0c8ddd77598fe870c3fb712c8a6f6c7474020ef6
sage <sage@29311d96-e01e-0410-9327-a35deaab8ce9>
2005-05-19 06:05:46 +0000

    *** empty log message ***
    git-svn-id: https://ceph.svn.sf.net/svnroot/ceph@234 29311d96-e01e-0410-9327-a35deaab8ce9

--- ceph/mds/OSDMonitor.cc
+++ ceph/mds/OSDMonitor.cc
@@ -4,12 +4,26 @@
 #include "msg/Message.h"
 #include "messages/MPing.h"
 
+#include "common/Timer.h"
+#include "common/Clock.h"
 
 /* to send a message,
 
 Message *messageptr = ......;
 mds->messenger->send_message(messageptr,
                              MSG_ADDR_OSD(osdnum), 0, MSG_PORT_OSDMON);
+
+
+timer example:
+
+class C_Test : public Context {
+public:
+  void finish(int r) {
+	cout << "C_Test->finish(" << r << ")" << endl;
+  }
+};
+
+g_timer.add_event_after(10, new C_Test);
 
 */
 
