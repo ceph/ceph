039b656f9ee151a0572827d0ae53685a5b8f6b78
sage <sage@29311d96-e01e-0410-9327-a35deaab8ce9>
2006-02-12 04:10:46 +0000

    *** empty log message ***
    git-svn-id: https://ceph.svn.sf.net/svnroot/ceph@617 29311d96-e01e-0410-9327-a35deaab8ce9

--- ceph/osd/OSD.cc
+++ ceph/osd/OSD.cc
@@ -1,3 +1,24 @@
+// -*- mode:C++; tab-width:4; c-basic-offset:2; indent-tabs-mode:t -*- 
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2004-2006 Sage Weil <sage@newdream.net>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 
 #include "include/types.h"
 
@@ -567,7 +588,7 @@ void OSD::update_map(bufferlist& state, bool mkfs)
   
   if (mkfs) {
 	// create PGs
-	for (int nrep = 2; nrep <= g_conf.osd_max_rep; nrep++) {
+	for (int nrep = 1; nrep <= g_conf.osd_max_rep; nrep++) {
 	  ps_t maxps = 1LL << osdmap->get_pg_bits();
 	  for (pg_t ps = 0; ps < maxps; ps++) {
 		pg_t pgid = osdmap->ps_nrep_to_pg(ps, nrep);
