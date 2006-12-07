1c9cb9c96760be3c8ecb3149d4a1f30c200892ac
sageweil <sageweil@29311d96-e01e-0410-9327-a35deaab8ce9>
2006-12-07 19:17:28 +0000

    exchange load (qlen) info
    git-svn-id: https://ceph.svn.sf.net/svnroot/ceph@986 29311d96-e01e-0410-9327-a35deaab8ce9

--- ceph/messages/MOSDPing.h
+++ ceph/messages/MOSDPing.h
@@ -11,8 +11,6 @@
  * 
  */
 
-
-
 #ifndef __MOSDPING_H
 #define __MOSDPING_H
 
@@ -23,8 +21,11 @@ class MOSDPing : public Message {
  public:
   epoch_t map_epoch;
   bool ack;
+  float avg_qlen;
 
-  MOSDPing(epoch_t e, bool a=false) : Message(MSG_OSD_PING), map_epoch(e), ack(a) {
+  MOSDPing(epoch_t e, 
+	   float aq,
+	   bool a=false) : Message(MSG_OSD_PING), map_epoch(e), ack(a), avg_qlen(aq) {
   }
   MOSDPing() {}
 
@@ -34,10 +35,13 @@ class MOSDPing : public Message {
     off += sizeof(map_epoch);
     payload.copy(off, sizeof(ack), (char*)&ack);
     off += sizeof(ack);
+    payload.copy(off, sizeof(avg_qlen), (char*)&avg_qlen);
+    off += sizeof(avg_qlen);
   }
   virtual void encode_payload() {
     payload.append((char*)&map_epoch, sizeof(map_epoch));
     payload.append((char*)&ack, sizeof(ack));
+    payload.append((char*)&avg_qlen, sizeof(avg_qlen));
   }
 
   virtual char *get_type_name() { return "oping"; }
