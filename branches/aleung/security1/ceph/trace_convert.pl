cb8a866095d27be96a991979b2be7c8e7070c2fb
anwleung <anwleung@29311d96-e01e-0410-9327-a35deaab8ce9>
2007-03-28 19:21:57 +0000

    does trace lseek stuff
    git-svn-id: https://ceph.svn.sf.net/svnroot/ceph@1312 29311d96-e01e-0410-9327-a35deaab8ce9

--- branches/aleung/security1/ceph/trace_convert.pl
+++ branches/aleung/security1/ceph/trace_convert.pl
@@ -8,20 +8,22 @@ my @files;
 my $file_handle;
 my @file_data;
 
-opendir(DIR, ".");
+#opendir(DIR, ".");
+my $pathname = "/projects/ssrc/traces-organized/LLNL-2003/tools/IOR2/traces_fileperproc";
+opendir(DIR, $pathname);
 @files = readdir(DIR);
 closedir(DIR);
 
 for $file_handle(@files) {
     if($file_handle =~ /IOR/ and $file_handle !~ /cephtrace/) {
-	open(FILE, $file_handle) or die "cannot_open_file: $!";
+	#print "$pathname/$file_handle";
+	open(FILE, "$pathname/$file_handle") or die "cannot_open_file: $!";
 	flock(FILE, 2);
-	open(OUT, ">$file_handle.cephtrace") or die "cannot_open_file: $!";
+	open(OUT, ">IOR2/$file_handle.cephtrace") or die "cannot_open_file: $!";
 	flock(OUT, 2);
 	@file_data = <FILE>;
 	my $line;
 	my @args;
-	my @unclosed_fds;
 	foreach $line (@file_data) {
 
 	    my $timestamp;
@@ -47,7 +49,6 @@ for $file_handle(@files) {
 		    ($op, $path, $rest) = split(/\"/, $op);
 		    print OUT "open\n$path\n102\n$ret_val\n";
 		}
-		push @unclosed_fds;
 	    }
 	    elsif ($op =~ /close\(/) {
 		my $nothing;
@@ -71,6 +72,7 @@ for $file_handle(@files) {
 
 		# FIXME hack so reads aren't empty
 		print OUT "write\n$ret_val\n$read_size\n-1\n";
+		print OUT "lseek\n$ret_val\n$read_size\n1\n";
 		print OUT "read\n$ret_val\n$read_size\n-1\n";
 	    }
 	    elsif ($op =~ /write/) {
