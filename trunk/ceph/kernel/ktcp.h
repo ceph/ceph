b49bf3f53befec576455432634cc293fa2d1f106
patiencew <patiencew@29311d96-e01e-0410-9327-a35deaab8ce9>
2007-10-31 17:35:42 +0000

    git-svn-id: https://ceph.svn.sf.net/svnroot/ceph@2008 29311d96-e01e-0410-9327-a35deaab8ce9

--- trunk/ceph/kernel/ktcp.h
+++ trunk/ceph/kernel/ktcp.h
@@ -1,6 +1,13 @@
 #ifndef _FS_CEPH_TCP_H
 #define _FS_CEPH_TCP_H
 
+/* prototype definitions */
+struct socket * _kconnect(struct sockaddr *);
+struct socket * _klisten(struct sockaddr *);
+struct socket *_kaccept(struct socket *);
+int _krecvmsg(struct socket *, void *, size_t , unsigned);
+int _ksendmsg(struct socket *, struct kvec *, size_t, size_t, unsigned);
+
 /* Well known port for ceph client listener.. */
 #define CEPH_PORT 2002
 /* Max number of outstanding connections in listener queueu */
