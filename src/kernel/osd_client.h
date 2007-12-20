c8181f8b12347a71d795ae9c914ee4a3a81c1768
Sage Weil <sage@newdream.net>
2007-12-19 18:05:42 -0800

    moved osdmap decoding code out of osd_client

--- src/kernel/osd_client.h
+++ src/kernel/osd_client.h
@@ -7,52 +7,31 @@
 #include <linux/radix-tree.h>
 #include <linux/completion.h>
 
+#include "osdmap.h"
+
 struct ceph_msg;
-
-struct ceph_osdmap {
-	struct ceph_fsid fsid;
-	ceph_epoch_t epoch;
-	ceph_epoch_t mon_epoch;
-	struct ceph_timeval ctime, mtime;
-	
-	__u32 pg_num, pg_num_mask;
-	__u32 localized_pg_num, localized_pg_num_mask;
-	
-	__u32 max_osd;
-	__u8 *osd_state;
-	__u32 *osd_offload;  /* 0 = normal, 0x10000 = 100% offload (failed) */
-	struct ceph_entity_addr *osd_addr;
-	struct crush_map *crush;
-
-	__u32 num_pg_swap_primary;
-	struct {
-		ceph_pg_t pg;
-		__u32 osd;
-	} *pg_swap_primary;
-};
 
 enum {
 	REQUEST_ACK, REQUEST_SAFE
 };
 
 struct ceph_osd_request {
-	__u64 r_tid;
-	ceph_pg_t r_pgid;
-	int r_flags;
-
-	atomic_t r_ref;
-	struct ceph_msg *r_request;
+	__u64             r_tid;
+	ceph_pg_t         r_pgid;
+	int               r_flags;
+	struct ceph_msg  *r_request;
+	struct ceph_msg  *r_reply;
+	atomic_t          r_ref;
 	struct completion r_completion;
 };
 
 struct ceph_osd_client {
-	struct ceph_osdmap *osdmap;  /* current osd map */
-
-	__u64 last_tid;              /* id of last mds request */
-	struct radix_tree_root request_tree;  /* pending mds requests */
-
-	__u64 last_requested_map;
-	struct completion map_waiters;
+	spinlock_t             lock;
+	struct ceph_osdmap     *osdmap;       /* current map */
+	__u64                  last_tid;      /* tid of last request */
+	struct radix_tree_root request_tree;  /* pending requests, by tid */
+	__u64                  last_requested_map;
+	struct completion      map_waiters;
 };
 
 extern void ceph_osdc_init(struct ceph_osd_client *osdc);
