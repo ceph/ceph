498acf21d27a5a0f9ca1cf91d88c90303f7f3f72
Sage Weil <sage@newdream.net>
2008-01-09 11:30:59 -0800

    todos, start.sh tweak

--- src/start.sh
+++ src/start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 ./stop.sh
-./mkmonmap `host \`hostname\`|cut -d ' ' -f 4`:12345  # your IP here; any unused port will do
+./mkmonmap `host \`hostname -f\`|cut -d ' ' -f 4`:12345  # your IP here; any unused port will do
 ./cmon --mkfs --mon 0 &
 ./cosd --mkfs --osd 0 &
 ./cosd --mkfs --osd 1 &
 ./cosd --mkfs --osd 2 &
 ./cosd --mkfs --osd 3 &
-./cmds --debug_ms 1 --debug_mds 10 &
+./cmds  &
