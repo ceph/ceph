a1b68dced072ba3ccefeb8bceaa09ba4bdfb587a
Sage Weil <sage@newdream.net>
2008-01-26 18:44:09 -0800

    smarter start.sh

--- src/start.sh
+++ src/start.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
-rm core*
 ./stop.sh
 
 test -d out || mkdir out
 rm out/*
 
-ARGS="--doutdir out -d"
-./mkmonmap `host \`hostname -f\`|cut -d ' ' -f 4`:12345  # your IP here
+# figure machine's ip
+HOSTNAME=`hostname -f`
+IP=`host $HOSTNAME | cut -d ' ' -f 4`
+echo hostname $HOSTNAME
+echo "ip $IP"
+
+if [ `echo $IP | grep '^127\\.'` ]
+then
+	echo
+	echo "WARNING: hostname resolves to loopback; remote hosts will not be able to"
+	echo "  connect.  either adjust /etc/hsots, or edit this script to use your"
+	echo "  machine's real IP."
+	echo
+fi
+
+ARGS="--bind $IP --doutdir out -d"
+./mkmonmap $IP:12345  # your IP here
 ./cmon --mkfs --mon 0 $ARGS
 ./cosd --mkfs --osd 0 $ARGS
 ./cosd --mkfs --osd 1 $ARGS
@@ -15,3 +29,4 @@ ARGS="--doutdir out -d"
 ./cosd --mkfs --osd 3 $ARGS
 ./cmds $ARGS
 echo "started.  stop.sh to stop.  see out/* (e.g. 'tail -f out/????') for debug output."
+
