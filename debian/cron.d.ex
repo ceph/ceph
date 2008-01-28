8adc9dac1d61f34ce85e3cbad1478e3e6ffe2b8e
Sage Weil <sage@newdream.net>
2008-01-28 15:33:37 -0800

    debian packing stuffs

---
+++ debian/cron.d.ex
@@ -0,0 +1,4 @@
+#
+# Regular cron jobs for the ceph package
+#
+0 4	* * *	root	ceph_maintenance
---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(ceph):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="ceph" command="/usr/bin/ceph"
