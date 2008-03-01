f3f3bf163027c32c866e4b2fdee9ef379d6c5f12
Sage Weil <sage@newdream.net>
2008-02-29 16:25:20 -0800

    manual merge of volker and dongsu's write stuff
    this is based on a diff of
        7469f26a33c072169d6ed929352eda06562ffe3f
    and 933226e5cdea16d311f15d47d5db852cf2107e57
    
    i left out the dcache locking bits in dir.c, for now.

--- src/kernel/addr.c
+++ src/kernel/addr.c
@@ -56,8 +56,154 @@ out_unlock:
 }
 #endif
 
+
+/*
+ * ceph_writepage:
+ *  clear dirty page, and set the writeback flag in the radix tree.
+ *  to actually write data to the remote OSDs.
+ */
+static int ceph_writepage(struct page *page, struct writeback_control *wbc)
+{
+       struct inode *inode = page->mapping->host;
+       struct ceph_inode_info *ci;
+       struct ceph_osd_client *osdc;
+       int err = 0;
+
+       if (!page->mapping || !page->mapping->host)
+               return -EFAULT;
+
+       ci = ceph_inode(inode);
+       osdc = &ceph_inode_to_client(inode)->osdc;
+
+       get_page(page);
+       set_page_writeback(page);
+       SetPageUptodate(page);
+
+       dout(10, "ceph_writepage inode %p page %p index %lu\n",
+               inode, page, page->index);
+
+       err = ceph_osdc_silly_write(osdc, inode->i_ino, &ci->i_layout,
+                               PAGE_SIZE, 0, (const char *)page->first_page);
+/*    err = ceph_osdc_writepage(osdc, inode->i_ino, &ci->i_layout,*/
+/*                page->index << PAGE_SHIFT, PAGE_SIZE, page);*/
+       if (err)
+               goto out_unlock;
+
+out_unlock:
+       unlock_page(page);
+       end_page_writeback(page);
+       put_page(page);
+
+       return err;
+}
+
+/*
+ * ceph_prepare_write:
+ *  allocate and initialize buffer heads for each page
+ */
+static int ceph_prepare_write(struct file *filp, struct page *page,
+       unsigned from, unsigned to)
+{
+/*    struct inode *inode = filp->f_dentry->d_inode;*/
+       struct inode *inode = page->mapping->host;
+       struct ceph_inode_info *ci = ceph_inode(inode);
+       struct ceph_osd_client *osdc = &ceph_inode_to_client(inode)->osdc;
+       int err = 0;
+       loff_t offset, i_size;
+
+       /*
+        * TODO:
+        *  1. check if page is up to date
+        *  2. If not, read a page to be up to date
+        */
+
+       if (PageUptodate(page))
+               return 0;
+
+       /* The given page is already up to date if it's a full page */
+       if ((to == PAGE_SIZE) && (from == 0)) {
+               SetPageUptodate(page);
+               return 0;
+       }
+
+       offset = (loff_t)page->index << PAGE_SHIFT;
+       i_size = i_size_read(inode);
+
+       if ((offset >= i_size) ||
+               ((from == 0) && (offset + to) >= i_size)) {
+               /* data beyond the file end doesn't need to be read */
+               simple_prepare_write(filp, page, from, to);
+               SetPageUptodate(page);
+               return 0;
+       }
+
+       /* Now it's clear that the page is not up to date */
+
+       err = ceph_osdc_prepare_write(osdc, inode->i_ino, &ci->i_layout,
+                                page->index << PAGE_SHIFT, PAGE_SIZE, page);
+       if (err)
+               goto out_unlock;
+
+out_unlock:
+       unlock_page(page);
+       return err;
+}
+
+/*
+ * ceph_commit_write:
+ *  mark the page as dirty, so that it is written to the disk later
+ */
+static int ceph_commit_write(struct file *filp, struct page *page,
+       unsigned from, unsigned to)
+{
+/*    struct inode *inode = filp->f_dentry->d_inode;*/
+       struct inode *inode = page->mapping->host;
+       struct ceph_inode_info *ci = ceph_inode(inode);
+       struct ceph_osd_client *osdc = &ceph_inode_to_client(inode)->osdc;
+       loff_t position = ((loff_t)page->index << PAGE_SHIFT) + to;
+       int err = 0;
+       char *page_data;
+
+       spin_lock(&inode->i_lock);
+       if (position > inode->i_size) {
+               i_size_write(inode, position);
+       }
+       spin_unlock(&inode->i_lock);
+
+       /*
+        *  1. check if page is up to date
+        *  2. If not, make the page up to date by writing a page
+        *  3. If yes, just set the page as dirty
+        */
+
+       if (!PageUptodate(page)) {
+               position = ((loff_t)page->index << PAGE_SHIFT) + from;
+
+               page_data = kmap(page);
+               err = ceph_osdc_commit_write(osdc, inode->i_ino, &ci->i_layout,
+                                page->index << PAGE_SHIFT, PAGE_SIZE, page);
+               if (err)
+                       err = 0;        /* FIXME: more sophisticated error handling */
+               kunmap(page);
+       }
+       else {
+               /* set the page as up-to-date and mark it as dirty */
+               SetPageUptodate(page);
+               set_page_dirty(page);
+       }
+
+/*out_unlock:*/
+       unlock_page(page);
+       return err;
+}
+
+
 const struct address_space_operations ceph_aops = {
 	.readpage = ceph_readpage,
 //	.readpages = ceph_readpages
 //	.writepage = ceph_writepage,
+	.prepare_write = ceph_prepare_write,
+	.commit_write = ceph_commit_write,
+	.writepage = ceph_writepage,
+//     .writepages = ceph_writepages,
 };
