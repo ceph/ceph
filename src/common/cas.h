08900c3bbb917ab01f982c152c09825bb4caca94
Sage Weil <sage@newdream.net>
2008-05-20 17:09:10 -0700

    cas stubs

---
+++ src/common/cas.h
@@ -0,0 +1,11 @@
+
+
+#ifndef __CEPH_CAS_H
+#define __CEPH_CAS_H
+
+#include "include/object.h"
+
+object_t calc_cas_name(bufferlist &bl);
+void chunk_buffer(bufferlist &bl, list<bufferlist> &chunks);
+
+#endif
