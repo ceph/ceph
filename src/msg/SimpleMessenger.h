80754a51d5a1e56f058b37b5a3bc34be7d5f2a4d
Sage Weil <sage@newdream.net>
2008-06-30 16:40:20 -0700

    msgr: half-assed fix to msgr race vs reset issue

--- src/msg/SimpleMessenger.h
+++ src/msg/SimpleMessenger.h
@@ -113,6 +113,8 @@ private:
     Mutex lock;
     int state;
 
+    bool did_zero_retry;
+
   protected:
 
     utime_t first_fault;   // time of original failure
@@ -167,7 +169,7 @@ private:
   public:
     Pipe(int st) : 
       sd(-1),
-      state(st), 
+      state(st), did_zero_retry(false),
       reader_running(false), writer_running(false),
       connect_seq(0),
       out_seq(0), in_seq(0), in_seq_acked(0),
