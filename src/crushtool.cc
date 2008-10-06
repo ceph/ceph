66c76af0bd00b9eaaa75fd4fa44ff287730ae61e
Sage Weil <sage@newdream.net>
2008-10-06 14:38:03 -0700

    crushtool: verify explicit item pos

--- src/crushtool.cc
+++ src/crushtool.cc
@@ -170,6 +170,10 @@ void parse_bucket(iter_t const& i, CrushWrapper &crush)
 	string tag = string_node(sub->children[q++]);
 	if (tag == "pos") {
 	  int pos = int_node(sub->children[q]);
+	  if (used_items.count(pos)) {
+	    cerr << "item '" << string_node(sub->children[1]) << "' in bucket '" << name << "' has explicit pos " << pos << ", which is occupied" << std::endl;
+	    exit(1);
+	  }
 	  used_items.insert(pos);
 	}
       }
