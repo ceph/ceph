b96b68f0f523093251fee34eb9fdd42cc2100598
Sage Weil <sage@newdream.net>
2008-11-13 11:06:03 -0800

    mds: keep inode multiversion if it has snapped old_inodes
    Once old_inodes gets cleaned out (snaps deleted), we can return
    to normalcy.

--- src/mds/CInode.h
+++ src/mds/CInode.h
@@ -147,7 +147,8 @@ class CInode : public MDSCacheObject {
   bool is_multiversion() {
     return snaprealm ||  // other snaprealms will link to me
       inode.is_dir() ||  // links to me in other snaps
-      inode.nlink > 1;   // there are remote links, possibly snapped, that will need to find me
+      inode.nlink > 1 || // there are remote links, possibly snapped, that will need to find me
+      old_inodes.size(); // once multiversion, always multiversion.  until old_inodes gets cleaned out.
   }
   snapid_t get_oldest_snap();
 
