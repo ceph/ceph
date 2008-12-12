1b773f5cd0bc765c13f349e8127bf2d92fa7b28e
Sage Weil <sage@newdream.net>
2008-12-12 15:12:31 -0800

    dstart: 3x replication

--- src/dstart.sh
+++ src/dstart.sh
@@ -32,12 +32,12 @@ ARGS="--dout_dir /data/`hostname`"
 if [ $debug -eq 0 ]; then
     CMON_ARGS="--debug_mon 10 --debug_ms 1"
     COSD_ARGS=""
-    CMDS_ARGS=""
+    CMDS_ARGS="--file_layout_pg_size 3"
 else
     echo "** going verbose **"
     CMON_ARGS="--lockdep 1 --debug_mon 20 --debug_ms 1 --debug_paxos 20"
     COSD_ARGS="--lockdep 1 --debug_osd 20 --debug_journal 20 --debug_filestore 15 --debug_ms 1" # --debug_journal 20 --debug_osd 20 --debug_filestore 20 --debug_ebofs 20
-    CMDS_ARGS="--lockdep 1 --mds_cache_size 500 --mds_log_max_segments 2 --debug_ms 1 --debug_mds 20 --mds_thrash_fragments 0 --mds_thrash_exports 0"
+    CMDS_ARGS="--file_layout_pg_size 3 --lockdep 1 --mds_cache_size 500 --mds_log_max_segments 2 --debug_ms 1 --debug_mds 20 --mds_thrash_fragments 0 --mds_thrash_exports 0"
 fi
 
 
