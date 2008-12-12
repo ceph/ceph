ab733782e2e8d5feb96122a3509c7f36021242c9
Yehuda Sadeh <yehuda@hq.newdream.net>
2008-12-12 14:51:40 -0800

    vstart.sh/stop.sh can start and stop specific modules

--- src/stop.sh
+++ src/stop.sh
@@ -1,2 +1,46 @@
 #!/bin/sh
-killall crun cmon cmds cosd
+
+do_killall() {
+	pg=`pgrep -f crun.*$1`
+	[ "$pg" != "" ] && kill $pg
+	killall $1
+}
+
+usage="usage: $0 [all] [mon] [mds] [osd]\n"
+
+let stop_all=1
+let stop_mon=0
+let stop_mds=0
+let stop_osd=0
+
+while [ $# -ge 1 ]; do
+    case $1 in
+	all )
+	    stop_all=1
+	    ;;
+	mon | cmon )
+	    stop_mon=1
+	    stop_all=0
+	    ;;
+	mds | cmds )
+	    stop_mds=1
+	    stop_all=0
+	    ;;
+	osd | cosd )
+	    stop_osd=1
+	    stop_all=0
+	    ;;
+	* )
+	    printf "$usage"
+	    exit
+        esac
+        shift
+done
+
+if [ $stop_all -eq 1 ]; then
+	killall crun cmon cmds cosd
+else
+	[ $stop_mon -eq 1 ] && do_killall cmon
+	[ $stop_mds -eq 1 ] && do_killall cmds
+	[ $stop_osd -eq 1 ] && do_killall cosd
+fi
--- src/vstart.sh
+++ src/vstart.sh
@@ -6,26 +6,58 @@
 
 let new=0
 let debug=0
+let start_all=1
+let start_mon=0
+let start_mds=0
+let start_osd=0
 norestart=""
 valgrind=""
 
+usage="usage: $0 [option]... [mon] [mds] [osd]\n"
+usage=$usage"options:\n"
+usage=$usage"\t-d, --debug\n"
+usage=$usage"\t-n, --new\n"
+usage=$usage"\t--norestart\n"
+usage=$usage"\t--valgrind\n"
+
 while [ $# -ge 1 ]; do
-    case $1 in
-        -d | --debug )
-            debug=1
-	    ;;
-        --new | -n )
-            new=1
-	    ;;
+case $1 in
+-d | --debug )
+debug=1
+	;;
+--new | -n )
+new=1
+	;;
 	--norestart )
-	    norestart="--norestart"
-	    ;;
+	norestart="--norestart"
+	;;
 	--valgrind )
-	    valgrind="--valgrind"
-	    ;;
-        esac
-        shift
+	valgrind="--valgrind"
+	;;
+	mon | cmon )
+	start_mon=1
+	start_all=0
+	;;
+	mds | cmds )
+	start_mds=1
+	start_all=0
+	;;
+	osd | cosd )
+	start_osd=1
+	start_all=0
+	;;
+	* )
+	printf "$usage"
+	exit
+esac
+shift
 done
+
+if [ $start_all -eq 1 ]; then
+	start_mon=1
+	start_mds=1
+	start_osd=1
+fi
 
 ARGS="-f"
 
@@ -48,7 +80,9 @@ export CEPH_ARGS="--lockdep 1"
 # sudo if btrfs
 test -d dev/osd0 && test -e dev/sudo && SUDO="sudo"
 
-$SUDO ./stop.sh
+if [ $start_all -eq 1 ]; then
+	$SUDO ./stop.sh
+fi
 $SUDO rm -f core*
 
 test -d out || mkdir out
@@ -64,66 +98,73 @@ IP=`host $HOSTNAME | grep $HOSTNAME | cut -d ' ' -f 4`
 echo hostname $HOSTNAME
 echo "ip $IP"
 
-if [ $new -eq 1 ]; then
-    if [ `echo $IP | grep '^127\\.'` ]
-    then
-	echo
-	echo "WARNING: hostname resolves to loopback; remote hosts will not be able to"
-	echo "  connect.  either adjust /etc/hosts, or edit this script to use your"
-	echo "  machine's real IP."
-	echo
-    fi
-    
-    # build a fresh fs monmap, mon fs
-    # $CEPH_BIN/monmaptool --create --clobber --print .ceph_monmap
-    str="$CEPH_BIN/monmaptool --create --clobber"
-    for f in `seq 0 $((CEPH_NUM_MON-1))`
-    do
-	str=$str" --add $IP:$(($CEPH_PORT+$f))"
-    done
-    str=$str" --print .ceph_monmap"
-    echo $str
-    $str
-    
-    for f in `seq 0 $((CEPH_NUM_MON-1))`
-    do
-	$CEPH_BIN/mkmonfs --clobber mondata/mon$f --mon $f --monmap .ceph_monmap
-    done
+if [ $start_mon -eq 1 ]; then
+	if [ $new -eq 1 ]; then
+		if [ `echo $IP | grep '^127\\.'` ]
+		then
+			echo
+			echo "WARNING: hostname resolves to loopback; remote hosts will not be able to"
+			echo "  connect.  either adjust /etc/hosts, or edit this script to use your"
+			echo "  machine's real IP."
+			echo
+		fi
+
+		# build a fresh fs monmap, mon fs
+		# $CEPH_BIN/monmaptool --create --clobber --print .ceph_monmap
+		str="$CEPH_BIN/monmaptool --create --clobber"
+		for f in `seq 0 $((CEPH_NUM_MON-1))`
+		do
+			str=$str" --add $IP:$(($CEPH_PORT+$f))"
+		done
+		str=$str" --print .ceph_monmap"
+		echo $str
+		$str
+
+		for f in `seq 0 $((CEPH_NUM_MON-1))`
+		do
+			$CEPH_BIN/mkmonfs --clobber mondata/mon$f --mon $f --monmap .ceph_monmap
+		done
+	fi
+
+	# start monitors
+	if [ $start_mon -ne 0 ]; then
+		for f in `seq 0 $((CEPH_NUM_MON-1))`; do
+			$CEPH_BIN/cmon $ARGS -d $CMON_ARGS mondata/mon$f
+		done
+	fi
+
+	if [ $new -eq 1 ]; then
+	# build and inject an initial osd map
+		$CEPH_BIN/osdmaptool --clobber --createsimple .ceph_monmap 4 .ceph_osdmap # --pgbits 2
+		$CEPH_BIN/cmonctl osd setmap -i .ceph_osdmap
+	fi
 fi
 
-# start monitors
-for f in `seq 0 $((CEPH_NUM_MON-1))`
-do
-    $CEPH_BIN/cmon $ARGS -d $CMON_ARGS mondata/mon$f
-done
-
-
-if [ $new -eq 1 ]; then
-    # build and inject an initial osd map
-    $CEPH_BIN/osdmaptool --clobber --createsimple .ceph_monmap 4 .ceph_osdmap # --pgbits 2
-    $CEPH_BIN/cmonctl osd setmap -i .ceph_osdmap
-fi
-
-for osd in `seq 0 $((CEPH_NUM_OSD-1))`
-do
-    if [ $new -eq 1 ]; then
-	echo mkfs osd$osd
-	$SUDO $CEPH_BIN/cosd --mkfs_for_osd $osd dev/osd$osd # --debug_journal 20 --debug_osd 20 --debug_filestore 20 --debug_ebofs 20
-    fi
-    echo start osd$osd
-    $CEPH_BIN/crun $norestart $valgrind $SUDO $CEPH_BIN/cosd -m $IP:$CEPH_PORT dev/osd$osd $ARGS $COSD_ARGS &
+#osd
+if [ $start_osd -eq 1 ]; then
+	for osd in `seq 0 $((CEPH_NUM_OSD-1))`
+	do
+		if [ $new -eq 1 ]; then
+			echo mkfs osd$osd
+			$SUDO $CEPH_BIN/cosd --mkfs_for_osd $osd dev/osd$osd # --debug_journal 20 --debug_osd 20 --debug_filestore 20 --debug_ebofs 20
+		fi
+		echo start osd$osd
+		$CEPH_BIN/crun $norestart $valgrind $SUDO $CEPH_BIN/cosd -m $IP:$CEPH_PORT dev/osd$osd $ARGS $COSD_ARGS &
 # echo valgrind --leak-check=full --show-reachable=yes $CEPH_BIN/cosd dev/osd$osd --debug_ms 1 --debug_osd 20 --debug_filestore 10 --debug_ebofs 20 #1>out/o$osd #& #--debug_osd 40
-done
+	done
+fi
 
 # mds
-for mds in `seq 0 $((CEPH_NUM_MDS-1))`
-do
-    $CEPH_BIN/crun $norestart $valgrind $CEPH_BIN/cmds $ARGS $CMDS_ARGS &
+if [ $start_mds -eq 1 ]; then
+	for mds in `seq 0 $((CEPH_NUM_MDS-1))`
+	do
+		$CEPH_BIN/crun $norestart $valgrind $CEPH_BIN/cmds $ARGS $CMDS_ARGS &
 
 #valgrind --tool=massif $CEPH_BIN/cmds $ARGS --mds_log_max_segments 2 --mds_thrash_fragments 0 --mds_thrash_exports 0 > m  #--debug_ms 20
 #$CEPH_BIN/cmds -d $ARGS --mds_thrash_fragments 0 --mds_thrash_exports 0 #--debug_ms 20
 #$CEPH_BIN/cmonctl mds set_max_mds 2
-done
+	done
+fi
 
 echo "started.  stop.sh to stop.  see out/* (e.g. 'tail -f out/????') for debug output."
 
