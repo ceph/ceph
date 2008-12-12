625e9502306a7fd97744d8611d9bfaf90b81ce9a
Sage Weil <sage@newdream.net>
2008-12-11 21:13:02 -0800

    todos

--- src/vstart.sh
+++ src/vstart.sh
@@ -36,7 +36,7 @@ if [ $debug -eq 0 ]; then
 else
 	echo "** going verbose **"
 	CMON_ARGS="--lockdep 1 --debug_mon 20 --debug_ms 1 --debug_paxos 20"
-	COSD_ARGS="--lockdep 1 --debug_osd 20 --debug_journal 20 --debug_ms 1" # --debug_journal 20 --debug_osd 20 --debug_filestore 20 --debug_ebofs 20
+	COSD_ARGS="--lockdep 1 --debug_osd 20 --debug_journal 20 --debug_filestore 10 --debug_ms 1" # --debug_journal 20 --debug_osd 20 --debug_filestore 20 --debug_ebofs 20
 	CMDS_ARGS="--lockdep 1 --mds_cache_size 500 --mds_log_max_segments 2 --debug_ms 1 --debug_mds 20 --mds_thrash_fragments 0 --mds_thrash_exports 0"
 fi
 
