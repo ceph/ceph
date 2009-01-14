8128eba717f04d4d38063464b441cb304ef006c6
Sage Weil <sage@newdream.net>
2009-01-14 12:47:53 -0800

    objectcacher: don't trim uncommitted objects

--- src/osdc/ObjectCacher.h
+++ src/osdc/ObjectCacher.h
@@ -181,7 +181,8 @@ class ObjectCacher {
     bool can_close() {
       return data.empty() && lock_state == LOCK_NONE &&
         waitfor_ack.empty() && waitfor_commit.empty() &&
-        waitfor_rd.empty() && waitfor_wr.empty();
+        waitfor_rd.empty() && waitfor_wr.empty() &&
+	!uncommitted_item.is_on_xlist();
     }
 
     // bh
