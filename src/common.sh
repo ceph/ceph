a6bb1d5f4381272c4ac67603e7d55ede6465b387
Yehuda Sadeh <yehuda@hq.newdream.net>
2009-01-29 15:02:34 -0800

    ceph.sh: add missing file

---
+++ src/common.sh
@@ -0,0 +1,47 @@
+
+[ "$CCONF_BIN" == "" ] && CCONF_BIN=`dirname $0`/../bin/cconf
+
+[ -e $CCONF_BIN ] || CCONF_BIN=`dirname $0`/cconf
+
+get_val() {
+  [ "$2" != "" ] && export $1=$2 || export $1=`$CCONF "$3" "$4" "$5"`
+}
+
+get_val_bool() {
+  if [ "$2" != "" ]; then
+	export $1=$2
+  else
+	tmp=`$CCONF "$3" "$4" "$5"`
+	export $1=$5
+
+	[ "$tmp" == "0" ] && export $1=0
+	[ "$tmp" == "false" ] && export $1=0
+	[ "$tmp" == "1" ] && export $1=1
+	[ "$tmp" == "true" ] && export $1=1
+  fi
+}
+
+get_conf() {
+	var=$1
+	def=$2
+	key=$3
+	shift; shift; shift
+
+	tmp=""
+	while [ $# -ge 1 ]; do
+		tmp=$tmp" -s $1"
+	shift
+	done
+	eval $var=`$CCONF $tmp "$key" "$def"`
+}
+
+get_conf_bool() {
+	get_conf "$@"
+
+	eval "val=$"$1
+	[ "$val" == "0" ] && export $1=0
+	[ "$val" == "false" ] && export $1=0
+	[ "$val" == "1" ] && export $1=1
+	[ "$val" == "true" ] && export $1=1
+}
+
