290b7d326dbf77a9470d50de26dc9069890f0757
Sage Weil <sage@newdream.net>
2009-02-24 16:58:29 -0800

    dstop: remove cosd hostname prefix

--- src/dstop.sh
+++ src/dstop.sh
@@ -4,5 +4,5 @@ killall cmon cmds crun
 
 for host in `cd dev/hosts ; ls`
 do
- ssh root@cosd$host killall crun cosd \; cd $HOME/ceph/src/dev/hosts/$host \; for f in \* \; do umount $HOME/ceph/src/devm/osd\$f \; done \; rmmod btrfs
+ ssh root@$host killall crun cosd \; cd $HOME/ceph/src/dev/hosts/$host \; for f in \* \; do umount $HOME/ceph/src/devm/osd\$f \; done \; rmmod btrfs
 done
