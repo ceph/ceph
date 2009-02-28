d73e0211420d859d34f824468e49f248dd73ea78
Sage Weil <sage@newdream.net>
2009-02-27 16:17:40 -0800

    ceph-daemons: generate list of items to stop/start in ceph_common.sh
    nothing -> everything
    mon -> all monitors (mon*)
    mds0 -> just mds0

--- src/ceph_common.sh
+++ src/ceph_common.sh
@@ -1,4 +1,38 @@
 
+CCONF="$BINDIR/cconf"
+
+conf=$ETCDIR"/startup.conf"
+cluster_conf=$ETCDIR"/cluster.conf"
+
+hostname=`hostname | cut -d . -f 1`
+
+
+get_name_list() {
+    orig=$1
+
+    if [[ $orig = "" ]]; then
+        # extract list of monitors, mdss, osds defined in startup.conf
+	what=`$CCONF -c $conf -l mon | egrep -v '^mon$' ; \
+	    $CCONF -c $conf -l mds | egrep -v '^mds$' ; \
+	    $CCONF -c $conf -l osd | egrep -v '^osd$'`
+	return
+    fi
+
+    what=""
+    for f in "$orig"; do
+	case $f in
+	    mon | osd | mds)
+		bit=`$CCONF -c $conf -l $f | egrep -v "^$f$"`
+		what="$what $bit"
+		;;
+	    *)
+		what="$what $f"
+		;;
+	esac
+    done
+}
+
+
 get_val() {
   [ "$2" != "" ] && export $1=$2 || export $1=`$CCONF -c $conf "$3" "$4" "$5"`
 }
