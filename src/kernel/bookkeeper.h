35bb1b98d3480b9f43c27a504fb2055706f76ad1
Sage Weil <sage@newdream.net>
2009-03-06 10:16:12 -0800

    kclient: fix bookkeeper ifdef

--- src/kernel/bookkeeper.h
+++ src/kernel/bookkeeper.h
@@ -1,13 +1,13 @@
 #ifndef _FS_CEPH_DEBUG_H
 #define _FS_CEPH_DEBUG_H
 
-#ifdef CONFIG_CEPH_LEAKCHECK
+#ifdef CONFIG_CEPH_BOOKKEEPER
 extern void ceph_bookkeeper_init(void);
 extern void ceph_bookkeeper_finalize(void);
 extern void *ceph_kmalloc(char *fname, int line, size_t size, gfp_t flags);
 extern void ceph_kfree(void *ptr);
 
-#ifndef CEPH_OVERRIDE_LEAKCHECK
+#ifndef CEPH_OVERRIDE_BOOKKEEPER
 #define kmalloc(size, flags)	ceph_kmalloc(__FILE__, __LINE__, size, flags)
 #define kzalloc(size, flags)	ceph_kmalloc(__FILE__, __LINE__, size, \
 					     flags | __GFP_ZERO)
