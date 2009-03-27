6e627075a2a17b25486da0c8fa2c06d048ee5416
Sage Weil <sage@newdream.net>
2009-03-27 10:33:57 -0700

    mds: expose projected inode to loners, even if they don't have EXCL yet
    If they are a (new) loner, they soon will!

--- src/mds/CInode.cc
+++ src/mds/CInode.cc
@@ -1392,8 +1392,8 @@ bool CInode::encode_inodestat(bufferlist& bl, Session *session,
 
 
   Capability *cap = get_client_cap(client);
-  bool pfile = filelock.is_xlocked_by_client(client) ||
-    (cap && (cap->issued() & CEPH_CAP_FILE_EXCL));
+  bool pfile = filelock.is_xlocked_by_client(client) || get_loner() == client;
+  //(cap && (cap->issued() & CEPH_CAP_FILE_EXCL));
   bool pauth = authlock.is_xlocked_by_client(client);
   bool plink = linklock.is_xlocked_by_client(client);
   bool pxattr = xattrlock.is_xlocked_by_client(client);
