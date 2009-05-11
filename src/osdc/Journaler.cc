ceph/ceph

commit 497ade3b90c973d34e04547e5f8748c4c1612314

journaler: store layout in journal header
This means we won't fall over if the 'default' layout changes.  Specify
the layout during journal creation, as you'd expect.

src/osdc/Journaler.cc
@@ -26,10 +26,16 @@
 
 
 
-void Journaler::reset()
+void Journaler::create(ceph_file_layout *l)
 {
-  dout(1) << "reset to blank journal" << dendl;
+  dout(1) << "create blank journal" << dendl;
   state = STATE_ACTIVE;
+
+  layout = *l;
+  assert(layout.fl_pg_pool == pg_pool);
+  last_written.layout = layout;
+  last_committed.layout = layout;
+
   write_pos = flush_pos = ack_pos = safe_pos =
     read_pos = requested_pos = received_pos =
     expire_pos = trimming_pos = trimmed_pos = ceph_file_layout_period(layout);
@@ -83,8 +89,10 @@ void Journaler::recover(Context *onread)
   state = STATE_READHEAD;
   C_ReadHead *fin = new C_ReadHead(this);
   vector<snapid_t> snaps;
-  filer.read(ino, &layout, CEPH_NOSNAP,
-	     0, 4096, &fin->bl, CEPH_OSD_FLAG_INCLOCK_FAIL, fin);
+
+  object_t oid(ino, 0);
+  ceph_object_layout ol = objecter->osdmap->make_object_layout(oid, pg_pool);
+  objecter->read_full(oid, ol, &fin->bl, 0, fin);
 }
 
 void Journaler::_finish_read_head(int r, bufferlist& bl)
@@ -114,6 +122,7 @@ void Journaler::_finish_read_head(int r, bufferlist& bl)
     return;
   }
 
+  layout = h.layout;
   write_pos = flush_pos = ack_pos = safe_pos = h.write_pos;
   read_pos = requested_pos = received_pos = h.read_pos;
   expire_pos = h.expire_pos;

src/osdc/Journaler.h
@@ -69,9 +69,11 @@ public:
     __s64 read_pos;
     __s64 write_pos;
     nstring magic;
+    ceph_file_layout layout;
 
-    Header(const char *m=0) : trimmed_pos(0), expire_pos(0), read_pos(0), write_pos(0),
-			      magic(m) {}
+    Header(const char *m=0) :
+      trimmed_pos(0), expire_pos(0), read_pos(0), write_pos(0),
+      magic(m) { }
 
     void encode(bufferlist &bl) const {
       ::encode(magic, bl);
@@ -79,6 +81,7 @@ public:
       ::encode(expire_pos, bl);
       ::encode(read_pos, bl);
       ::encode(write_pos, bl);
+      ::encode(layout, bl);
     }
     void decode(bufferlist::iterator &bl) {
       ::decode(magic, bl);
@@ -86,6 +89,7 @@ public:
       ::decode(expire_pos, bl);
       ::decode(read_pos, bl);
       ::decode(write_pos, bl);
+      ::decode(layout, bl);
     }
   } last_written, last_committed;
   WRITE_CLASS_ENCODER(Header)
@@ -93,7 +97,9 @@ public:
   private:
   // me
   inodeno_t ino;
+  unsigned pg_pool;
   ceph_file_layout layout;
+
   const char *magic;
   Objecter *objecter;
   Filer filer;
@@ -195,9 +201,9 @@ public:
   friend class C_Trim;
 
 public:
-  Journaler(inodeno_t ino_, ceph_file_layout *layout_, const char *mag, Objecter *obj, Logger *l, int lkey, Mutex *lk, __s64 fl=0, __s64 pff=0) : 
+  Journaler(inodeno_t ino_, int pool, const char *mag, Objecter *obj, Logger *l, int lkey, Mutex *lk, __s64 fl=0, __s64 pff=0) : 
     last_written(mag), last_committed(mag),
-    ino(ino_), layout(*layout_), magic(mag),
+    ino(ino_), pg_pool(pool), magic(mag),
     objecter(obj), filer(objecter), logger(l), logger_key_lat(lkey),
     lock(lk), timer(*lk), delay_flush_event(0),
     state(STATE_UNDEF), error(0),
@@ -224,7 +230,7 @@ public:
    * in our sequence do not exist.. e.g. after a MKFS.  this is _not_
    * an "erase" method.
    */
-  void reset();
+  void create(ceph_file_layout *layout);
   void recover(Context *onfinish);
   void write_head(Context *onsave=0);
 
@@ -237,6 +243,9 @@ public:
   __s64 get_read_pos() const { return read_pos; }
   __s64 get_expire_pos() const { return expire_pos; }
   __s64 get_trimmed_pos() const { return trimmed_pos; }
+
+  __s64 get_layout_period() const { return ceph_file_layout_period(layout); }
+  ceph_file_layout& get_layout() { return layout; }
 
   // write
   __s64 append_entry(bufferlist& bl);

src/dumpjournal.cc
@@ -94,19 +94,11 @@ int main(int argc, const char **argv, const char *envp[])
   messenger = rank.register_entity(entity_name_t::ADMIN());
   messenger->set_dispatcher(&dispatcher);
 
-  inode_t log_inode;
-  memset(&log_inode, 0, sizeof(log_inode));
-  log_inode.ino = MDS_INO_LOG_OFFSET + mds;
-  log_inode.layout.fl_stripe_unit = 1<<20;
-  log_inode.layout.fl_stripe_count = 1;
-  log_inode.layout.fl_object_size = 1<<20;
-  log_inode.layout.fl_cas_hash = 0;
-  log_inode.layout.fl_object_stripe_unit = 0;
-  log_inode.layout.fl_pg_preferred = -1;
-  log_inode.layout.fl_pg_pool = CEPH_METADATA_RULE;
+  inodeno_t ino = MDS_INO_LOG_OFFSET + mds;
+  unsigned pg_pool = CEPH_METADATA_RULE;
 
   objecter = new Objecter(messenger, &monmap, &osdmap, lock);
-  journaler = new Journaler(log_inode.ino, &log_inode.layout, CEPH_FS_ONDISK_MAGIC, objecter, 0, 0,  &lock);
+  journaler = new Journaler(ino, pg_pool, CEPH_FS_ONDISK_MAGIC, objecter, 0, 0,  &lock);
 
   objecter->set_client_incarnation(0);
 
@@ -124,7 +116,7 @@ int main(int argc, const char **argv, const char *envp[])
 
   Filer filer(objecter);
   bufferlist bl;
-  filer.read(log_inode.ino, &log_inode.layout, 0,
+  filer.read(ino, &journaler->get_layout(), 0,
 	     start, len, &bl, 0, new C_SafeCond(&lock, &cond, &done));
     lock.Lock();
   while (!done)

src/mds/MDLog.cc
@@ -81,22 +81,14 @@ void MDLog::reopen_logger(utime_t start, bool append)
 void MDLog::init_journaler()
 {
   // inode
-  memset(&log_inode, 0, sizeof(log_inode));
-  log_inode.ino = MDS_INO_LOG_OFFSET + mds->get_nodeid();
-  log_inode.layout.fl_stripe_unit = 1<<20;
-  log_inode.layout.fl_stripe_count = 1;
-  log_inode.layout.fl_object_size = 1<<20;
-  log_inode.layout.fl_cas_hash = 0;
-  log_inode.layout.fl_object_stripe_unit = 0;
-  log_inode.layout.fl_pg_preferred = -1;
-  log_inode.layout.fl_pg_pool = mds->mdsmap->get_metadata_pg_pool();
+  ino = MDS_INO_LOG_OFFSET + mds->get_nodeid();
   
-  if (g_conf.mds_local_osd) 
-    log_inode.layout.fl_pg_preferred = mds->get_nodeid() + g_conf.num_osd;  // hack
+  //if (g_conf.mds_local_osd) 
+  //log_inode.layout.fl_pg_preferred = mds->get_nodeid() + g_conf.num_osd;  // hack
   
   // log streamer
   if (journaler) delete journaler;
-  journaler = new Journaler(log_inode.ino, &log_inode.layout, CEPH_FS_ONDISK_MAGIC, mds->objecter, 
+  journaler = new Journaler(ino, mds->mdsmap->get_metadata_pg_pool(), CEPH_FS_ONDISK_MAGIC, mds->objecter, 
 			    logger, l_mdl_jlat,
 			    &mds->mds_lock);
 }
@@ -127,7 +119,7 @@ void MDLog::create(Context *c)
 {
   dout(5) << "create empty log" << dendl;
   init_journaler();
-  journaler->reset();
+  journaler->create(&mds->mdcache->default_dir_layout);
   write_head(c);
 
   logger->set(l_mdl_expos, journaler->get_expire_pos());
@@ -219,10 +211,11 @@ void MDLog::submit_entry( LogEvent *le, Context *c, bool wait_safe )
   // start a new segment?
   //  FIXME: should this go elsewhere?
   loff_t last_seg = get_last_segment_offset();
+  loff_t period = journaler->get_layout_period();
   if (!segments.empty() && 
       !writing_subtree_map &&
-      (journaler->get_write_pos() / ceph_file_layout_period(log_inode.layout) != (last_seg / ceph_file_layout_period(log_inode.layout)) &&
-       (journaler->get_write_pos() - last_seg > ceph_file_layout_period(log_inode.layout)/2))) {
+      (journaler->get_write_pos()/period != last_seg/period &&
+       journaler->get_write_pos() - last_seg > period/2)) {
     dout(10) << "submit_entry also starting new segment: last = " << last_seg
 	     << ", cur pos = " << journaler->get_write_pos() << dendl;
     start_new_segment();

src/mds/MDLog.h
@@ -73,7 +73,7 @@ class MDLog {
 
   bool capped;
 
-  inode_t log_inode;
+  inodeno_t ino;
   Journaler *journaler;
 
   Logger *logger;

commit 5d51f27e6432ce6ca6f18a13293291e403106c80

journaler: fix replay
Broken by commit 497ade3b90.

Use objecter to write header (not just to read it).

Also reset the prefetch values each time the layout is set.

src/osdc/Journaler.cc
@@ -31,14 +31,25 @@ void Journaler::create(ceph_file_layout *l)
   dout(1) << "create blank journal" << dendl;
   state = STATE_ACTIVE;
 
-  layout = *l;
-  assert(layout.fl_pg_pool == pg_pool);
-  last_written.layout = layout;
-  last_committed.layout = layout;
+  set_layout(l);
 
   write_pos = flush_pos = ack_pos = safe_pos =
     read_pos = requested_pos = received_pos =
     expire_pos = trimming_pos = trimmed_pos = ceph_file_layout_period(layout);
+}
+
+void Journaler::set_layout(ceph_file_layout *l)
+{
+  layout = *l;
+
+  assert(layout.fl_pg_pool == pg_pool);
+  last_written.layout = layout;
+  last_committed.layout = layout;
+
+  // prefetch intelligently.
+  // (watch out, this is big if you use big objects or weird striping)
+  fetch_len = ceph_file_layout_period(layout) * g_conf.journaler_prefetch_periods;
+  prefetch_from = fetch_len / 2;
 }
 
 
@@ -122,7 +133,8 @@ void Journaler::_finish_read_head(int r, bufferlist& bl)
     return;
   }
 
-  layout = h.layout;
+  set_layout(&h.layout);
+
   write_pos = flush_pos = ack_pos = safe_pos = h.write_pos;
   read_pos = requested_pos = received_pos = h.read_pos;
   expire_pos = h.expire_pos;
@@ -191,10 +203,12 @@ void Journaler::write_head(Context *oncommit)
   bufferlist bl;
   ::encode(last_written, bl);
   SnapContext snapc;
-  filer.write(ino, &layout, snapc,
-	      0, bl.length(), bl, g_clock.now(), CEPH_OSD_FLAG_INCLOCK_FAIL,
-	      NULL, 
-	      new C_WriteHead(this, last_written, oncommit));
+  
+  object_t oid(ino, 0);
+  ceph_object_layout ol = objecter->osdmap->make_object_layout(oid, pg_pool);
+  objecter->write_full(oid, ol, snapc, bl, g_clock.now(), 0, 
+		       NULL, 
+		       new C_WriteHead(this, last_written, oncommit));
 }
 
 void Journaler::_finish_write_head(Header &wrote, Context *oncommit)

src/osdc/Journaler.h
@@ -201,7 +201,7 @@ public:
   friend class C_Trim;
 
 public:
-  Journaler(inodeno_t ino_, int pool, const char *mag, Objecter *obj, Logger *l, int lkey, Mutex *lk, __s64 fl=0, __s64 pff=0) : 
+  Journaler(inodeno_t ino_, int pool, const char *mag, Objecter *obj, Logger *l, int lkey, Mutex *lk) : 
     last_written(mag), last_committed(mag),
     ino(ino_), pg_pool(pool), magic(mag),
     objecter(obj), filer(objecter), logger(l), logger_key_lat(lkey),
@@ -209,16 +209,10 @@ public:
     state(STATE_UNDEF), error(0),
     write_pos(0), flush_pos(0), ack_pos(0), safe_pos(0),
     read_pos(0), requested_pos(0), received_pos(0),
-    fetch_len(fl), prefetch_from(pff),
+    fetch_len(0), prefetch_from(0),
     read_bl(0), on_read_finish(0), on_readable(0),
     expire_pos(0), trimming_pos(0), trimmed_pos(0) 
   {
-    // prefetch intelligently.
-    // (watch out, this is big if you use big objects or weird striping)
-    if (!fetch_len)
-      fetch_len = ceph_file_layout_period(layout) * g_conf.journaler_prefetch_periods;
-    if (!prefetch_from)
-      prefetch_from = fetch_len / 2;
   }
 
   // me
@@ -233,6 +227,8 @@ public:
   void create(ceph_file_layout *layout);
   void recover(Context *onfinish);
   void write_head(Context *onsave=0);
+
+  void set_layout(ceph_file_layout *l);
 
   bool is_active() { return state == STATE_ACTIVE; }
   int get_error() { return error; }