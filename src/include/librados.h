b821a4bc1100aee884185d94c9df09cfb961dc6b
Sage Weil <sage@newdream.net>
2009-05-12 08:07:00 -0700

    testrados: C, not C++

--- src/include/librados.h
+++ src/include/librados.h
@@ -8,6 +8,7 @@ extern "C" {
 #include <netinet/in.h>
 #include <linux/types.h>
 #include <string.h>
+#include <stdbool.h>
 
 #include "include/msgr.h"
 #include "include/rados.h"
@@ -17,8 +18,8 @@ int rados_initialize(int argc, const char **argv); /* arguments are optional */
 void rados_deinitialize();
 
 /* read/write objects */
-int rados_write(ceph_object *oid, const char *buf, off_t off, size_t len);
-int rados_read(ceph_object *oid, char *buf, off_t off, size_t len);
+int rados_write(struct ceph_object *oid, const char *buf, off_t off, size_t len);
+int rados_read(struct ceph_object *oid, char *buf, off_t off, size_t len);
 
 #ifdef __cplusplus
 }
