b79983723afe98523352188136a170fcebc56dc8
Sage Weil <sage@newdream.net>
2009-05-14 22:34:55 -0700

    class: typedef method, use C style naming

--- src/objclass/objclass.h
+++ src/objclass/objclass.h
@@ -5,25 +5,27 @@
 extern "C" {
 #endif
 
-typedef void * ClsMethodHandle;
-
-
-#define OBJ_READ    0x1
-#define OBJ_WRITE   0x2
-
-typedef int ClsTrigger;
+typedef void *cls_method_handle_t;
+typedef int (*cls_method_call_t)(struct ceph_osd_op *op,
+				 char **indata, int datalen,
+				 char **outdata, int *outdatalen);
 
 /* class log */
 extern int cls_log(const char *format, ...);
 
 /* class registration api */
 extern int cls_register(const char *name, const char *method,
-                        int (*class_call)(struct ceph_osd_op *op, char **indata, int datalen, char **outdata, int *outdatalen),
-                        ClsMethodHandle *handle);
-extern int cls_unregister(ClsMethodHandle handle);
+                        cls_method_call_t class_call, cls_method_handle_t handle);
+extern int cls_unregister(cls_method_handle_t handle);
 
-extern int cls_link(ClsMethodHandle handle, int priority, ClsTrigger trigger);
-extern int cls_unlink(ClsMethodHandle handle);
+/* triggers */
+#define OBJ_READ    0x1
+#define OBJ_WRITE   0x2
+
+typedef int cls_trigger_t;
+
+extern int cls_link(cls_method_handle_t handle, int priority, cls_trigger_t trigger);
+extern int cls_unlink(cls_method_handle_t handle);
 
 #ifdef __cplusplus
 }
