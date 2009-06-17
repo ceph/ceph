9d6538ceb236e1ca33ffa48a049b8473cc25e729
Sage Weil <sage@newdream.net>
2009-06-17 12:50:06 -0700

    osd: fix pps calculation from pgid.ps() and pgid.pool()
    The final placement seed needs to factor in pool, but that can't be
    fed into stable_mod or you get weird results (for example, 1.ff and
    1.adff won't necessary map to the same thing because of the
    stable_mod).  Add pool to the stable_mod result, instead.  The seed
    itself doesn't need to be bounded; it's just an input for CRUSH.
    Just so long as there are a limited number of such inputs for a given
    pool.

--- src/osd/osd_types.h
+++ src/osd/osd_types.h
@@ -534,9 +534,9 @@ struct pg_pool_t {
    */
   ps_t raw_pg_to_pps(pg_t pg) const {
     if (pg.preferred() >= 0 && v.lpgp_num)
-      return ceph_stable_mod(pg.ps() + pg.pool(), v.lpgp_num, lpgp_num_mask);
+      return ceph_stable_mod(pg.ps(), v.lpgp_num, lpgp_num_mask + pg.pool());
     else
-      return ceph_stable_mod(pg.ps() + pg.pool(), v.pgp_num, pgp_num_mask);
+      return ceph_stable_mod(pg.ps(), v.pgp_num, pgp_num_mask) + pg.pool();
   }
 
   void encode(bufferlist& bl) const {
