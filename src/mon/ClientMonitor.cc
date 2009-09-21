ceph/ceph

commit b2c7655093821710aac2b4d35ccb1740429eb402

mon: fix bug with client id assignment of -1

src/mon/ClientMonitor.cc
@@ -63,7 +63,7 @@ bool ClientMonitor::update_from_paxos()
 
   paxos->stash_latest(paxosv, bl);
 
-  if (next_client < 0) {
+  if (next_client <= client_map.next_client) {
     dout(10) << "in-core next_client reset to " << client_map.next_client << dendl;
     next_client = client_map.next_client;
   }
@@ -73,8 +73,8 @@ bool ClientMonitor::update_from_paxos()
 
 void ClientMonitor::on_election_start()
 {
-  dout(10) << "in-core next_client cleared" << dendl;
-  next_client = -1;
+  dout(10) << "in-core next_client reset to " << client_map.next_client << dendl;
+  next_client = client_map.next_client;
 }
 
 void ClientMonitor::create_pending()

commit 35dc047356f0c18a8920eb63361a688d921edcf3

mon: fix client id preallocation
Broken by b2c7655093821710aac2b4d35ccb1740429eb402

src/mon/ClientMonitor.cc
@@ -63,7 +63,7 @@ bool ClientMonitor::update_from_paxos()
 
   paxos->stash_latest(paxosv, bl);
 
-  if (next_client <= client_map.next_client) {
+  if (next_client < 0) {
     dout(10) << "in-core next_client reset to " << client_map.next_client << dendl;
     next_client = client_map.next_client;
   }