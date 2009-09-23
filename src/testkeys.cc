315da4065ad1c954dea7e81ebda8cfd5543f85a0
Yehuda Sadeh <yehuda@hq.newdream.net>
2009-09-23 10:41:29 -0700

    auth: add missing file

---
+++ src/testkeys.cc
@@ -0,0 +1,58 @@
+#include "auth/KeysServer.h"
+
+#include "config.h"
+
+
+#define AES_KEY_LEN	16
+
+int main(int argc, char *argv[])
+{
+
+  KeysServer server;
+
+  dout(0) << "server created" << dendl;
+
+  getchar();
+
+#if 0
+  char aes_key[AES_KEY_LEN];
+  memset(aes_key, 0x77, sizeof(aes_key));
+  bufferptr keybuf(aes_key, sizeof(aes_key));
+  CryptoKey key(CEPH_SECRET_AES, g_clock.now(), keybuf);
+
+  const char *msg="hello! this is a message\n";
+  char pad[16];
+  memset(pad, 0, 16);
+  bufferptr ptr(msg, strlen(msg));
+  bufferlist enc_in;
+  enc_in.append(ptr);
+  enc_in.append(msg, strlen(msg));
+
+  bufferlist enc_out;
+  if (key.encrypt(enc_in, enc_out) < 0) {
+    derr(0) << "couldn't encode!" << dendl;
+    exit(1);
+  }
+
+  const char *enc_buf = enc_out.c_str();
+  for (unsigned i=0; i<enc_out.length(); i++) {
+    std::cout << hex << (int)(unsigned char)enc_buf[i] << dec << " ";
+    if (i && !(i%16))
+      std::cout << std::endl;
+  }
+
+  bufferlist dec_in, dec_out;
+
+  dec_in = enc_out;
+
+  if (key.decrypt(dec_in, dec_out) < 0) {
+    derr(0) << "couldn't decode!" << dendl;
+  }
+
+  dout(0) << "decoded len: " << dec_out.length() << dendl;
+  dout(0) << "decoded msg: " << dec_out.c_str() << dendl;
+
+  return 0;
+#endif
+}
+
