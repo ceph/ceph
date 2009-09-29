2f36c6276430ad61f52f9346d7fcb8d9fd034259
Greg Farnum <gregf@hq.newdream.net>
2009-09-29 16:37:28 -0700

    messages: Add an MMDSLoadTargets message for mds->monitor info.

---
+++ src/messages/MMDSLoadTargets.h
@@ -0,0 +1,46 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*- 
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2009 Sage Weil <sage@newdream.net>
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software 
+ * Foundation.  See file COPYING.
+ * 
+ */
+
+#ifndef __MMDSLoadTargets_H
+#define __MMDSLoadTargets_H
+
+#include "msg/Message.h"
+#include "include/types.h"
+
+#include <map>
+using std::map;
+
+class MMDSLoadTargets : public Message {
+ public:
+  map<int, double> targets;
+
+  MMDSLoadTargets() : Message(MSG_MDS_OFFLOAD_TARGETS) {}
+
+  MMDSLoadTargets(map<int, double>& mds_targets) :
+    Message(MSG_MDS_OFFLOAD_TARGETS),
+    targets(mds_targets) {}
+
+  const char* get_type_name() { return "mds_load_targets"; }
+
+  void decode_payload() {
+    bufferlist::iterator p = payload.begin();
+    ::decode(targets, p);
+  }
+
+  void encode_payload() {
+    ::encode(targets, payload);
+  }
+};
+
+#endif
