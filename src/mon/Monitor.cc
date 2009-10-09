c3916c78cc11f98f71e9657aad6b81aaeaf8917d
Sage Weil <sage@newdream.net>
2009-10-09 16:30:17 -0700

    mon: create session for all incoming monitor messages
    This code stolen from auth branch; hopefully git merge will be smart
    enough.

--- src/mon/Monitor.cc
+++ src/mon/Monitor.cc
@@ -354,7 +354,27 @@ void Monitor::stop_cluster()
 
 bool Monitor::ms_dispatch(Message *m)
 {
+  bool ret = true;
   lock.Lock();
+
+  Connection *connection = m->get_connection();
+  Session *s = NULL;
+
+  if (connection) {
+    s = (Session *)connection->get_priv();
+    if (s && s->closed) {
+      s->put();
+      s = NULL;
+    }
+    if (!s) {
+      s = session_map.new_session(m->get_source_inst());
+      m->get_connection()->set_priv(s->get());
+      dout(10) << "ms_dispatch new session " << s << " for " << s->inst << dendl;
+    } else {
+      dout(20) << "ms_dispatch existing session " << s << " for " << s->inst << dendl;
+    }
+  }
+
   {
     switch (m->get_type()) {
       
@@ -458,12 +478,15 @@ bool Monitor::ms_dispatch(Message *m)
       break;
       
     default:
-        return false;
+      ret = false;
     }
+  }
+  if (s) {
+    s->put();
   }
   lock.Unlock();
 
-  return true;
+  return ret;
 }
 
 void Monitor::handle_subscribe(MMonSubscribe *m)
