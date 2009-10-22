019e744d463579c495550311caa7318cd7f77089
Sage Weil <sage@newdream.net>
2009-10-22 14:27:47 -0700

    uclient: implement ms_get_authorizer

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -5953,3 +5953,10 @@ void Client::ms_handle_remote_reset(Connection *con)
   Mutex::Locker l(client_lock);
   objecter->ms_handle_remote_reset(con);
 }
+
+bool Client::ms_get_authorizer(int dest_type, AuthAuthorizer& authorizer, bool force_new)
+{
+  if (dest_type == CEPH_ENTITY_TYPE_MON)
+    return true;
+  return monclient->auth.build_authorizer(dest_type, authorizer);
+}
