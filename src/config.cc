f8b0e54c74f7f6151fb07d8ff253339b0f66dfc6
Sage Weil <sage@newdream.net>
2009-11-12 15:29:36 -0800

    conf: default to auth type 'none'

--- src/config.cc
+++ src/config.cc
@@ -341,7 +341,7 @@ static struct config_option config_optionsp[] = {
 	OPTION(debug_tp, 0, OPT_INT, 0),
 	OPTION(debug_auth, 0, OPT_INT, 1),
 	OPTION(keys_file, 'k', OPT_STR, "~/.ceph/keyring.bin, /etc/ceph/keyring.bin"),
-	OPTION(supported_auth, 0, OPT_STR, "cephx, none"),
+	OPTION(supported_auth, 0, OPT_STR, "none"),
 	OPTION(clock_lock, 0, OPT_BOOL, false),
 	OPTION(clock_tare, 0, OPT_BOOL, false),
 	OPTION(ms_tcp_nodelay, 0, OPT_BOOL, true),
