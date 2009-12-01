ceph/ceph

commit cd6f38171034ad14384f74ea12df7a471da98f1b

mds: update pending_for_mds during table server replay
Otherwise we our replay of AGREE after recovery is wrong; we just replay
what was saved, without taking additional journaled items into account.

src/mds/MDSTableServer.cc
@@ -46,9 +46,6 @@ void MDSTableServer::handle_prepare(MMDSTableRequest *req)
   bufferlist bl = req->bl;
 
   _prepare(req->bl, req->reqid, from);
-  pending_for_mds[version].mds = from;
-  pending_for_mds[version].reqid = req->reqid;
-  pending_for_mds[version].tid = version;
 
   ETableServer *le = new ETableServer(table, TABLESERVER_OP_PREPARE, req->reqid, from, version, version);
   le->mutation = bl;  // original request, NOT modified return value coming out of _prepare!
@@ -76,7 +73,6 @@ void MDSTableServer::handle_commit(MMDSTableRequest *req)
 
   if (pending_for_mds.count(tid)) {
     _commit(tid);
-    pending_for_mds.erase(tid);
     mds->mdlog->submit_entry(new ETableServer(table, TABLESERVER_OP_COMMIT, 0, -1, tid, version));
     mds->mdlog->wait_for_sync(new C_Commit(this, req));
   }

src/mds/MDSTableServer.h
@@ -78,6 +78,16 @@ private:
   virtual void _rollback(version_t tid) = 0;
   virtual void _server_update(bufferlist& bl) { assert(0); }
 
+  void _note_prepare(int mds, __u64 reqid) {
+    pending_for_mds[version].mds = mds;
+    pending_for_mds[version].reqid = reqid;
+    pending_for_mds[version].tid = version;
+  }
+  void _note_commit(__u64 tid) {
+    pending_for_mds.erase(tid);
+  }
+  
+
   MDSTableServer(MDS *m, int tab) : MDSTable(m, get_mdstable_name(tab), false), table(tab) {}
   virtual ~MDSTableServer() {}
 

src/mds/journal.cc
@@ -779,9 +779,11 @@ void ETableServer::replay(MDS *mds)
   switch (op) {
   case TABLESERVER_OP_PREPARE:
     server->_prepare(mutation, reqid, bymds);
+    server->_note_prepare(bymds, reqid);
     break;
   case TABLESERVER_OP_COMMIT:
     server->_commit(tid);
+    server->_note_commit(tid);
     break;
   case TABLESERVER_OP_SERVER_UPDATE:
     server->_server_update(mutation);

commit 6c34e6719046a037ce9f5956bc9469d7d0e3ec22

mds: track _note_prepared/committed in normal path, too
This was broken by cd6f3817.

src/mds/MDSTableClient.cc
@@ -103,7 +103,7 @@ void MDSTableClient::handle_request(class MMDSTableRequest *m)
 
 void MDSTableClient::_logged_ack(version_t tid)
 {
-  dout(10) << "_logged_ack" << dendl;
+  dout(10) << "_logged_ack " << tid << dendl;
 
   assert(pending_commit.count(tid));
   assert(pending_commit[tid]->pending_commit_tids[table].count(tid));
@@ -156,11 +156,13 @@ void MDSTableClient::commit(version_t tid, LogSegment *ls)
 
 void MDSTableClient::got_journaled_agree(version_t tid, LogSegment *ls)
 {
+  dout(10) << "got_journaled_agree " << tid << dendl;
   ls->pending_commit_tids[table].insert(tid);
   pending_commit[tid] = ls;
 }
 void MDSTableClient::got_journaled_ack(version_t tid)
 {
+  dout(10) << "got_journaled_ack " << tid << dendl;
   if (pending_commit.count(tid))
     pending_commit[tid]->pending_commit_tids[table].erase(tid);
   pending_commit.erase(tid);

src/mds/MDSTableServer.cc
@@ -46,6 +46,7 @@ void MDSTableServer::handle_prepare(MMDSTableRequest *req)
   bufferlist bl = req->bl;
 
   _prepare(req->bl, req->reqid, from);
+  _note_prepare(from, req->reqid);
 
   ETableServer *le = new ETableServer(table, TABLESERVER_OP_PREPARE, req->reqid, from, version, version);
   le->mutation = bl;  // original request, NOT modified return value coming out of _prepare!
@@ -73,6 +74,7 @@ void MDSTableServer::handle_commit(MMDSTableRequest *req)
 
   if (pending_for_mds.count(tid)) {
     _commit(tid);
+    _note_commit(tid);
     mds->mdlog->submit_entry(new ETableServer(table, TABLESERVER_OP_COMMIT, 0, -1, tid, version));
     mds->mdlog->wait_for_sync(new C_Commit(this, req));
   }