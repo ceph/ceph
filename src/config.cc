a26fec6886b28e744726acd689730a934a20fd92
Sage Weil <sage@newdream.net>
2010-01-14 11:52:20 -0800

    mds: disable cache dump on rejoin by default

--- src/config.cc
+++ src/config.cc
@@ -473,7 +473,7 @@ static struct config_option config_optionsp[] = {
 	OPTION(mds_thrash_exports, 0, OPT_INT, 0),
 	OPTION(mds_thrash_fragments, 0, OPT_INT, 0),
 	OPTION(mds_dump_cache_on_map, 0, OPT_BOOL, false),
-	OPTION(mds_dump_cache_after_rejoin, 0, OPT_BOOL, true),
+	OPTION(mds_dump_cache_after_rejoin, 0, OPT_BOOL, false),
 	OPTION(mds_hack_log_expire_for_better_stats, 0, OPT_BOOL, false),
 	OPTION(mds_kill_mdstable_at, 0, OPT_INT, 0),
 	OPTION(mds_kill_export_at, 0, OPT_INT, 0),
