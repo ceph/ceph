0f42df4685b81243d3f37137b0ec0e24bd7f075f
Sage Weil <sage@newdream.net>
2010-02-08 10:27:27 -0800

    debian: updated debian build scripts, changelog

--- build_upload_debian_packages.sh
+++ build_upload_debian_packages.sh
@@ -7,7 +7,6 @@ echo vers $vers
 
 repo=$1
 arch=$2
-snapshot=$3
 
 [ -z "$repo" ] && echo no repo && exit 1
 [ -z "$arch" ] && echo no arch && exit 1
@@ -28,8 +27,8 @@ fi
 echo final vers $finalvers
 
 echo cleanup
-rm *.deb *.tar.gz *.changes *.dsc || true
-rm -rf ceph-$vers* || true
+rm -rf ceph-* || true
+rm -rf debtmp || true
 
 echo generating git version stamp
 cd src
@@ -43,37 +42,53 @@ make dist
 echo extracting
 tar zxf ceph-$vers.tar.gz
 
+# mangle version
 if [ "$vers" != "$finalvers" ]; then
     echo "renaming ceph-$vers to ceph-$finalvers, rebuilding tarball"
     mv ceph-$vers ceph-$finalvers
     sed -i "s/ceph, $vers/ceph, $finalvers/" ceph-$finalvers/configure.ac
-
-    mv ceph-$finalvers/debian/changelog ceph-$finalvers/debian/changelog.tmp
-    cat <<EOF > ceph-$finalvers/debian/changelog
-ceph ($finalvers) unstable; urgency=low
-
-   * snapshot from git at $versuffix
-
- -- sage <sage@newdream.net>  $debdate
-
-EOF
-    cat ceph-$finalvers/debian/changelog.tmp >> ceph-$finalvers/debian/changelog
-    
+   
     tar zcf ceph-$finalvers.tar.gz ceph-$finalvers
 fi;
 
-if [ "$repo" = "stable" -a "$arch" = "amd64" ]; then
-    scp ceph-$vers.tar.gz sage@ceph.newdream.net:ceph.newdream.net/downloads
+
+## go
+
+echo creating debtmp with .orig.tar.gz
+mkdir -p debtmp
+cp ceph-$finalvers.tar.gz debtmp/ceph_$finalvers.orig.tar.gz
+cd debtmp
+
+echo extracting .orig.tar.gz
+tar zxf ceph_$finalvers.orig.tar.gz
+
+# copy in debian dir, fix up changelog
+echo setting up debian dir
+cp -aL ../debian ceph-$finalvers
+if [ "$vers" != "$finalvers" ]; then
+    cd ceph-$finalvers
+    DEBEMAIL="sage@newdream.net" dch -v $finalvers-1 'git snapshot'
+    cd ..
 fi
 
 cd ceph-$finalvers
-./autogen.sh
 dpkg-buildpackage -rfakeroot -us -uc
 cd ..
 
-# upload
-rsync -v --progress *$arch.{deb,changes} sage@ceph.newdream.net:debian/dists/$repo/main/binary-$arch
-rsync -v --progress ceph_* sage@ceph.newdream.net:debian/dists/$repo/main/source
+if [ "$3" = "upload" ]; then
 
-# rebuild index
-ssh sage@ceph.newdream.net build_debian_repo.sh
+    # upload
+    rsync -v --progress *$arch* sage@ceph.newdream.net:debian/dists/$repo/main/binary-$arch
+    rsync -v --progress ceph_$finalvers[.-]* sage@ceph.newdream.net:debian/dists/$repo/main/source
+
+    cd ..
+
+#exit 0
+
+    if [ "$repo" = "stable" -a "$arch" = "amd64" ]; then
+	scp ceph-$vers.tar.gz sage@ceph.newdream.net:ceph.newdream.net/downloads
+    fi
+
+    # rebuild index
+    ssh sage@ceph.newdream.net build_debian_repo.sh
+fi
