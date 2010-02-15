65f5123645079cc3467129682947c94599e9ebeb
Sage Weil <sage@newdream.net>
2010-02-15 15:28:02 -0800

    qa: fix snaptest1.sh

--- qa/workunits/snaptest1.sh
+++ qa/workunits/snaptest1.sh
@@ -5,6 +5,7 @@ echo 2 > file2
 echo 3 > file3
 mkdir .snap/snap1
 echo 4 > file4
-if [ `ls` -eq `ls .snap/snap1` ]
-    return -1
+if [ `ls` -eq `ls .snap/snap1` ]; then
+    echo live and snap contents are identical?
+    false
 fi
