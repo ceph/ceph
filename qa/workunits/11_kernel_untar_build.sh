038a8eba44d56c71fd7eb09f3653e07971a8903c
Sage Weil <sage@newdream.net>
2010-02-26 14:50:54 -0800

    qa: download linux tarball from ceph.newdream.net for kernel compile test

--- qa/workunits/11_kernel_untar_build.sh
+++ qa/workunits/11_kernel_untar_build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-tar jxvf /root/linux*
+wget http://ceph.newdream.net/qa/linux-2.6.33.tar.bz2
+mkdir t
+cd t
+tar jxvf ../linux*
 cd linux*
 make defconfig
 make
 cd ..
 rm -r linux*
+cd ..
+rm -r t linux*
