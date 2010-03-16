f059e49d5c07d72e8a592d019edefd910e0e590b
Sage Weil <sage@newdream.net>
2010-03-15 22:00:58 -0700

    qa: fix snaptest1.sh

--- qa/workunits/snaptest1.sh
+++ qa/workunits/snaptest1.sh
@@ -5,7 +5,10 @@ echo 2 > file2
 echo 3 > file3
 mkdir .snap/snap1
 echo 4 > file4
-if [ `ls` -eq `ls .snap/snap1` ]; then
+now=`ls`
+then=`ls .snap/snap1`
+rmdir .snap/snap1
+if [ "$now" = "$then" ]; then
     echo live and snap contents are identical?
     false
 fi
