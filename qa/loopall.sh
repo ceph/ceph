60677da9af4ce62f87e24551ebc2f4b18b042e99
Sage Weil <sage@newdream.net>
2010-03-17 11:13:53 -0700

    qa: add loopall.sh

---
+++ qa/loopall.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -x
+
+set -e
+
+basedir=`echo $0 | sed 's/[^/]*$//g'`.
+testdir="$1"
+[ -n "$2" ] && logdir=$2 || logdir=$1
+
+[ ${basedir:0:1} == "." ] && basedir=`pwd`/${basedir:1}
+
+[ -z "$testdir" ] || [ ! -d "$testdir" ] && echo "specify test dir" && exit 1
+cd $testdir
+
+while true
+do
+    for test in `cd $basedir/workunits && ls | grep .sh`
+    do
+	echo "------ running test $test ------"
+	pwd
+	[ -d $test ] && rm -r $test
+	mkdir -p $test
+	mkdir -p `dirname $logdir/$test.log`
+	test -e $logdir/$test.log && rm $logdir/$test.log
+	sh -c "cd $test && $basedir/workunits/$test" 2>&1 | tee $logdir/$test.log
+    done
+done
