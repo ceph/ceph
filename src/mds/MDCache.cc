ceph/ceph

commit b818f5bc8a9b925777687b2102f6b900dd9e50c3

mds: send resolve to resolve_set
Not RESOLVE|REJOIN|CLIENTREPLAY|ACTIVE|STOPPING, according to
the current MDSMap.

Fixes problem where the resolve_set is empty, but we send
resolves out, and got != needed because got is a superset.

src/mds/MDCache.cc
@@ -2009,13 +2009,14 @@ void MDCache::resolve_start()
       adjust_subtree_auth(rootdir, CDIR_AUTH_UNKNOWN);
   }
 
-  set<int> who;
+  /*set<int> who;
   mds->mdsmap->get_mds_set(who, MDSMap::STATE_RESOLVE);
   mds->mdsmap->get_mds_set(who, MDSMap::STATE_REJOIN);
   mds->mdsmap->get_mds_set(who, MDSMap::STATE_CLIENTREPLAY);
   mds->mdsmap->get_mds_set(who, MDSMap::STATE_ACTIVE);
   mds->mdsmap->get_mds_set(who, MDSMap::STATE_STOPPING);
-  for (set<int>::iterator p = who.begin(); p != who.end(); ++p) {
+  */
+  for (set<int>::iterator p = wants_resolve.begin(); p != wants_resolve.end(); ++p) {
     if (*p == mds->whoami)
       continue;
     send_resolve(*p);  // now.

commit d1c17442146b7917648df56a89398f1d641e3111

mds: fix resolve stage
This was recently broken by b818f5bc.

src/TODO
@@ -42,8 +42,8 @@ v0.19
 - debian: packaging fixes
 
 v0.20
-
 - new filestore, journaling
+
 - multiple mds fixes
 
 - qa: snap test.  maybe walk through 2.6.* kernel trees?
@@ -60,8 +60,6 @@ filestore
   - need an osdmap cache layer?
 
 bugs
-- mds: resolve problems.  restart all 3 mds's a few times (after vstart + a partial untar) and they won't come back up.
-
 - kclient: looping osd connection failures
 [ 3974.417106] ceph: osd11 10.3.14.138:6800 connection failed
 [ 3974.423295] ceph: osd11 10.3.14.138:6800 connection failed

src/mds/MDCache.cc
@@ -2011,14 +2011,7 @@ void MDCache::resolve_start()
       adjust_subtree_auth(rootdir, CDIR_AUTH_UNKNOWN);
   }
 
-  /*set<int> who;
-  mds->mdsmap->get_mds_set(who, MDSMap::STATE_RESOLVE);
-  mds->mdsmap->get_mds_set(who, MDSMap::STATE_REJOIN);
-  mds->mdsmap->get_mds_set(who, MDSMap::STATE_CLIENTREPLAY);
-  mds->mdsmap->get_mds_set(who, MDSMap::STATE_ACTIVE);
-  mds->mdsmap->get_mds_set(who, MDSMap::STATE_STOPPING);
-  */
-  for (set<int>::iterator p = wants_resolve.begin(); p != wants_resolve.end(); ++p) {
+  for (set<int>::iterator p = recovery_set.begin(); p != recovery_set.end(); ++p) {
     if (*p == mds->whoami)
       continue;
     send_resolve(*p);  // now.