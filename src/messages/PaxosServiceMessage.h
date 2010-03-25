c69a03391d9048e0821f97f4c95f0bb97921635f
Greg Farnum <gregf@hq.newdream.net>
2010-03-25 09:54:58 -0700

    Revert "msg: PaxosServiceMessage includes a MonCap*"
    This reverts commit 167b28a0d4cb2977f237be1972e40e04aed24207.

--- src/messages/PaxosServiceMessage.h
+++ src/messages/PaxosServiceMessage.h
@@ -10,14 +10,11 @@ class PaxosServiceMessage : public Message {
   version_t version;
   __s16 session_mon;
   __u64 session_mon_tid;
-  MonCaps *caps;
   
  PaxosServiceMessage() : Message(MSG_PAXOS),
-    version(0), session_mon(-1), session_mon_tid(0),
-    caps(NULL) { }
- PaxosServiceMessage(int type, version_t v) : Message(type),
-    version(v), session_mon(-1),
-    session_mon_tid(0), caps(NULL) { }
+			 version(0), session_mon(-1), session_mon_tid(0) { }
+  PaxosServiceMessage(int type, version_t v) : Message(type),
+					       version(v), session_mon(-1), session_mon_tid(0) { }
 
   void paxos_encode() {
     ::encode(version, payload);
