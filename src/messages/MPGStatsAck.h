a976fd69096d2ea8370af055730d896561dec63a
Greg Farnum <gregf@hq.newdream.net>
2010-03-30 12:57:07 -0700

    msg: MPGStatsAck has a private destructor

--- src/messages/MPGStatsAck.h
+++ src/messages/MPGStatsAck.h
@@ -23,6 +23,10 @@ public:
   
   MPGStatsAck() : Message(MSG_PGSTATSACK) {}
 
+private:
+  ~MPGStatsAck() {}
+
+public:
   const char *get_type_name() { return "pg_stats_ack"; }
   void print(ostream& out) {
     out << "pg_stats_ack(" << pg_stat.size() << " pgs)";
