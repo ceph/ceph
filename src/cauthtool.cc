94307c9ab03dfcfa02317cf9015281703c283e61
Greg Farnum <gregf@hq.newdream.net>
2010-04-06 11:08:45 -0700

    auth: cauthtool now identifies itself properly to common_init

--- src/cauthtool.cc
+++ src/cauthtool.cc
@@ -34,7 +34,7 @@ int main(int argc, const char **argv)
   argv_to_vec(argc, argv, args);
   env_to_vec(args);
   DEFINE_CONF_VARS(usage);
-  common_init(args, "osdmaptool", false, false);
+  common_init(args, "cauthtool", false, false);
 
   const char *me = argv[0];
 
