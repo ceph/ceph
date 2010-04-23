756b251bac81db92495d68c208e781c712ee3e2a
Sage Weil <sage@newdream.net>
2010-04-23 12:10:24 -0700

    v0.20

--- src/include/ceph_fs.h
+++ src/include/ceph_fs.h
@@ -19,8 +19,8 @@
  * Ceph release version
  */
 #define CEPH_VERSION_MAJOR 0
-#define CEPH_VERSION_MINOR 19
-#define CEPH_VERSION_PATCH 1
+#define CEPH_VERSION_MINOR 20
+#define CEPH_VERSION_PATCH 0
 
 #define _CEPH_STRINGIFY(x) #x
 #define CEPH_STRINGIFY(x) _CEPH_STRINGIFY(x)
