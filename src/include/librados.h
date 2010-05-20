16ab067c75da6aa4daecd62afc0a95568a22a941
Yehuda Sadeh <yehuda@hq.newdream.net>
2010-05-20 16:18:29 -0700

    librados: update librados to define CEPH_OSD_TMAP_SET

--- src/include/librados.h
+++ src/include/librados.h
@@ -9,6 +9,10 @@ extern "C" {
 #include <linux/types.h>
 #include <string.h>
 
+#ifndef CEPH_OSD_TMAP_SET
+#define CEPH_OSD_TMAP_SET 's'
+#endif
+
 /* initialization */
 int rados_initialize(int argc, const char **argv); /* arguments are optional */
 void rados_deinitialize();
