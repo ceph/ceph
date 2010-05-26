bee74a1e4c373d195aac47e74bab794c6bd6fa31
Christian Brunner <chb@muc.de>
2010-05-26 22:38:14 +0200

    ceph: add conversion to qemu coding style
    Hi Yehuda,
    
    I've added a small hack to make push_to_qemu.pl convert tabs to spaces.
    
    Christian

--- src/push_to_qemu.pl
+++ src/push_to_qemu.pl
@@ -15,7 +15,7 @@ until (-d "$dir/.git") {
 }
 
 print "pushing changed shared files from $dir to $qemu...\n";
-system "cat $dir/src/include/rbd_types.h | sed 's/__u32/uint32_t/g; s/__u8/uint8_t/g; s/__.*16/uint16_t/g; s/__.*32/uint32_t/g; s/__.*64/uint64_t/g; s/_FS_CEPH_RBD/QEMU_BLOCK_RBD_TYPES_H/g' | grep -v \"linux/types.h\" > $qemu/block/rbd_types.h";
+system "cat $dir/src/include/rbd_types.h | sed 's/__u32/uint32_t/g; s/__u8/uint8_t/g; s/__.*16/uint16_t/g; s/__.*32/uint32_t/g; s/__.*64/uint64_t/g; s/_FS_CEPH_RBD/QEMU_BLOCK_RBD_TYPES_H/g; s/^\t/    /g' | expand | grep -v \"linux/types.h\" > $qemu/block/rbd_types.h";
 
 print "done.\n";
 
