920b2d897495f3e1bfe892e6470657c711220b83
Sage Weil <sage@newdream.net>
2010-06-17 13:09:22 -0700

    qa: fix mdstable failure tests

--- qa/mds/test_mdstable_failures.sh
+++ qa/mds/test_mdstable_failures.sh
@@ -3,9 +3,9 @@
 for f in `seq 1 8`
 do
     echo testing failure point $f
-    $bindir/ceph -c $conf mds injectargs 0 "--mds_kill_mdstable_at $f"
+    pushd . ; cd $bindir ; ./ceph -c $conf mds injectargs 0 "--mds_kill_mdstable_at $f" ; popd
     sleep 1  # wait for mds command to go thru
-    pushd . ; cd $bindir ; ./init-ceph -c $conf start mds ; popd
+    bash -c "pushd . ; cd $bindir ; sleep 10 ; ./init-ceph -c $conf start mds ; popd" &
     touch $f
     ln $f $f.link
 done
