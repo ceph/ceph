3bfabd41ff9de805de7b00acf1c4450478b1ebfa
Sage Weil <sage@newdream.net>
2010-06-20 09:44:37 -0700

    mon: allow 10ms clock drift before complaining

--- src/config.cc
+++ src/config.cc
@@ -318,7 +318,7 @@ static struct config_option config_optionsp[] = {
 	OPTION(mon_lease, 0, OPT_FLOAT, 5),  		    // lease interval
 	OPTION(mon_lease_renew_interval, 0, OPT_FLOAT, 3), // on leader, to renew the lease
 	OPTION(mon_lease_ack_timeout, 0, OPT_FLOAT, 10.0), // on leader, if lease isn't acked by all peons
-	OPTION(mon_lease_wiggle_room, 0, OPT_FLOAT, .001), // allowed clock drive between monitors
+	OPTION(mon_lease_wiggle_room, 0, OPT_FLOAT, .010), // allowed clock drift between monitors
 	OPTION(mon_lease_timeout, 0, OPT_FLOAT, 10.0),     // on peon, if lease isn't extended
 	OPTION(mon_accept_timeout, 0, OPT_FLOAT, 10.0),    // on leader, if paxos update isn't accepted
 	OPTION(mon_stop_on_last_unmount, 0, OPT_BOOL, false),
