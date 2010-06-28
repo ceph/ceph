ceph/ceph

commit bd4188a02abff9efffb87a0a2031efe51c1b4d9a

filejournal: use buffer::write_fd() instead of open coding
Avoid dup code, and also handle short writes.

src/os/FileJournal.cc
@@ -488,20 +488,11 @@ void FileJournal::write_bl(off64_t& pos, bufferlist& bl)
   }
 
   ::lseek64(fd, pos, SEEK_SET);
-
-  for (list<bufferptr>::const_iterator it = bl.buffers().begin();
-       it != bl.buffers().end();
-       it++) {
-    if ((*it).length() == 0)
-      continue;  // blank buffer.
-    int r = ::write(fd, (char*)(*it).c_str(), (*it).length());
-    if (r < 0) {
-      char buf[80];
-      derr(0) << "do_write failed with " << errno << " " << strerror_r(errno, buf, sizeof(buf)) 
-	      << " with " << (void*)(*it).c_str() << " len " << (*it).length()
-	      << dendl;
-    }
-    pos += (*it).length();
+  int err = bl.write_fd(fd);
+  if (err) {
+    char buf[80];
+    derr(0) << "write_bl failed with " << err << " " << strerror_r(-err, buf, sizeof(buf)) 
+	    << dendl;
   }
 }
 

commit 50f254d0ce6c86a21140a4ce5a3ce08deb3d1d77

filejournal: fix journal write_pos advance
This was broken by bd4188a02abff9efffb87a0a2031efe51c1b4d9a.  @pos needs to
be advanced (it is pass by reference) or else we just overwrite the same
bytes at the journal start over and over again.

src/os/FileJournal.cc
@@ -425,6 +425,7 @@ int FileJournal::prepare_multi_write(bufferlist& bl, uint64_t& orig_ops, uint64_
     }
   }
 
+  dout(20) << "prepare_multi_write queue_pos now " << queue_pos << dendl;
   //assert(write_pos + bl.length() == queue_pos);
   return 0;
 }
@@ -511,6 +512,7 @@ void FileJournal::write_bl(off64_t& pos, bufferlist& bl)
     derr(0) << "write_bl failed with " << err << " " << strerror_r(-err, buf, sizeof(buf)) 
 	    << dendl;
   }
+  pos += bl.length();
 }
 
 void FileJournal::do_write(bufferlist& bl)