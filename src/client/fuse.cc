ef2ecdab6c1f079cb2f3ef2927be0b1d07456ff3
Sage Weil <sage@newdream.net>
2010-07-20 21:25:46 -0700

    fuse.cc: make license GPL
    Originally based on Miklos' fusexmp.c, which is GPL.  Keep that license.
    LGPL makes no sense here anyway.

--- src/client/fuse.cc
+++ src/client/fuse.cc
@@ -3,12 +3,10 @@
 /*
  * Ceph - scalable distributed file system
  *
- * Copyright (C) 2004-2006 Sage Weil <sage@newdream.net>
+ * Copyright (C) 2004-2010 Sage Weil <sage@newdream.net>
  *
  * This is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License version 2.1, as published by the Free Software 
- * Foundation.  See file COPYING.
+ * modify it under the terms of the GPL.
  * 
  */
 
