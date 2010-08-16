4e9ae5d97875defa7b795cd64e9d6dbf9c72bf9f
Sage Weil <sage@newdream.net>
2010-08-16 13:35:54 -0700

    qa: add test that client updates realm children on snap update

---
+++ qa/workunits/snaptest-upchildrealms.sh
@@ -0,0 +1,22 @@
+#!/bin/sh -x
+
+set -e
+
+#
+# verify that a snap update on a parent realm will induce
+# snap cap writeback for inodes child realms
+#
+
+mkdir a
+mkdir a/b
+mkdir a/.snap/a1
+mkdir a/b/.snap/b1
+echo asdf > a/b/foo
+mkdir a/.snap/a2
+# client _should_ have just queued a capsnap for writeback
+ln a/b/foo a/b/bar       # make the server cow the inode
+
+echo "this should not hang..."
+cat a/b/.snap/_a2_*/foo
+echo "good, it did not hang."
+
