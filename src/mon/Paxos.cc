6998372a8f3ca7d9b3180bbbeb628c8531865d31
Greg Farnum <gregf@hq.newdream.net>
2010-08-24 11:49:43 -0700

    mon: Adjust lease time checks.
    For some reason this fixes (apparently incorrect) warnings I saw
    while running a 2-mon system on my dev server.

--- src/mon/Paxos.cc
+++ src/mon/Paxos.cc
@@ -570,15 +570,17 @@ void Paxos::handle_lease(MMonPaxos *lease)
     lease->put();
     return;
   }
-
-  if (lease->sent_timestamp > g_clock.now() + g_conf.mon_clock_drift_allowed) {
+  utime_t allowed_time;
+  allowed_time.set_from_double(g_clock.now() + g_conf.mon_clock_drift_allowed);
+  if (lease->sent_timestamp > allowed_time) {
     utime_t warn_diff = g_clock.now() - last_clock_drift_warn;
     if ((last_clock_drift_warn == utime_t()) ||
-	(warn_diff > 
+	(warn_diff >
 	 pow(g_conf.mon_clock_drift_warn_backoff, clock_drift_warned))) {
       stringstream ss;
       ss << "lease_expire from mon" << lease->get_source().num()
 	 << " was sent from future time " << lease->sent_timestamp
+	 << " with expected time <=" << allowed_time
 	 << ", clocks not synchronized"
 	 << std::endl;
       mon->get_logclient()->log(LOG_WARN, ss);
@@ -648,10 +650,13 @@ void Paxos::handle_lease_ack(MMonPaxos *ack)
 	     << " dup (lagging!), ignoring" << dendl;
   }
 
-  if (ack->sent_timestamp > g_clock.now() + g_conf.mon_clock_drift_allowed) {
+  utime_t allowed_time;
+  allowed_time.set_from_double(g_clock.now() + g_conf.mon_clock_drift_allowed);
+  if (ack->sent_timestamp > allowed_time) {
     stringstream ss;
     ss << "lease_ack from mon" << from
        << " was sent from future time " << ack->sent_timestamp
+       << " with allowed time <=" << allowed_time
        << ", clocks not synchronized." << std::endl;
     mon->get_logclient()->log(LOG_WARN, ss);
   }
