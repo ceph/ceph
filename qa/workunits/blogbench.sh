54d661aca6964ba8ac8c7093a9005b33b0d47d4e
Greg Farnum <gregf@hq.newdream.net>
2010-08-25 16:10:57 -0700

    qa: add blogbench test to qa

---
+++ qa/workunits/blogbench.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+echo "getting blogbench"
+wget http://ceph.newdream.net/qa/blogbench-1.0.tar.bz2
+#cp /home/gregf/src/blogbench-1.0.tar.bz2 .
+tar -xvf blogbench-1.0.tar.bz2
+cd blogbench*
+echo "making blogbench"
+./configure
+make
+cd src
+mkdir blogtest_in
+echo "running blogbench"
+./blogbench -d blogtest_in
