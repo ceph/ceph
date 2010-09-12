eeeaf7a1c567bd680734a96f3b9b3978537a017a
Sage Weil <sage@newdream.net>
2010-09-12 09:18:13 -0700

    moncaps: fix array out of bounds in parsing

--- src/mon/MonCaps.cc
+++ src/mon/MonCaps.cc
@@ -8,14 +8,14 @@ bool MonCaps::get_next_token(string s, size_t& pos, string& token)
   int start = s.find_first_not_of(" \t", pos);
   int end;
 
+  if (start < 0) {
+    return false; 
+  }
+
   if (s[start] == '=' || s[start] == ',' || s[start] == ';') {
     end = start + 1;
   } else {
     end = s.find_first_of(";,= \t", start+1);
-  }
-
-  if (start < 0) {
-    return false; 
   }
 
   if (end < 0) {
