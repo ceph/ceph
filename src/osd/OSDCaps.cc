3695de1e6ecb957f3c3319b867e486c84a834e25
Sage Weil <sage@newdream.net>
2010-09-12 20:26:57 -0700

    osd: fix osdcaps parsing negative return value

--- src/osd/OSDCaps.cc
+++ src/osd/OSDCaps.cc
@@ -40,14 +40,14 @@ bool OSDCaps::get_next_token(string s, size_t& pos, string& token)
   int start = s.find_first_not_of(" \t", pos);
   int end;
 
+  if (start < 0) {
+    return false; 
+  }
+
   if (s[start] == '=' || s[start] == ',' || s[start] == ';') {
     end = start + 1;
   } else {
     end = s.find_first_of(";,= \t", start+1);
-  }
-
-  if (start < 0) {
-    return false; 
   }
 
   if (end < 0) {
