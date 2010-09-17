5cf850f8c1b1186cd84a90d1f77187678f95bb5c
Sage Weil <sage@newdream.net>
2010-09-17 15:52:43 -0700

    debian: make update_pbuilder.sh update a single dist at a time

--- update_pbuilder.sh
+++ update_pbuilder.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
 basedir=~/debian-base
+dist=$1
 
-for dist in sid squeeze lenny
-do
     pbuilder --clean
     
     if [ -e $basedir/$dist.tgz ]; then
 	echo updating $dist base.tgz
 	savelog -l -n  $basedir/$dist.tgz
 	cp $basedir/$dist.tgz.0 $basedir/$dist.tgz
-	pbuilder update --basetgz $basedir/$dist.tgz --distribution $dist
+	pbuilder update --basetgz $basedir/$dist.tgz --distribution $dist --mirror http://http.us.debian.org/debian
     else
 	echo building $dist base.tgz
 	pbuilder create --basetgz $basedir/$dist.tgz --distribution $dist --mirror http://http.us.debian.org/debian
     fi
-done
 
