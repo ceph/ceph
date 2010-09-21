381447d9f5396ea3b8ab406ec32cc95d156c7e6b
Sage Weil <sage@newdream.net>
2010-09-21 13:39:43 -0700

    qa: add snaptest-git-ceph.sh

---
+++ qa/workunits/snaptest-git-ceph.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -x
+
+set -e
+
+git clone git://ceph.newdream.net/git/ceph.git
+cd ceph
+
+versions=`seq 1 21`
+
+for v in $versions
+do
+    ver="v0.$v"
+    echo $ver
+    git reset --hard $ver
+    mkdir .snap/$ver
+done
+
+for v in $versions
+do
+    ver="v0.$v"
+    echo checking $ver
+    cd .snap/$ver
+    git diff --exit-code
+    cd ../..
+done
+
+echo OK
