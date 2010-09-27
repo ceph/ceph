b378cb4899e78d1d1e5b81f376a2536e56fe54c4
Wido den Hollander <wido@widodh.nl>
2010-09-27 21:21:25 +0200

    Add RGW_PRINT_CONTINUE to control wether we print the 100-continue header
    Signed-off-by: Yehuda Sadeh <yehuda@hq.newdream.net>

--- src/rgw/rgw_rest.cc
+++ src/rgw/rgw_rest.cc
@@ -649,8 +649,11 @@ void RGWHandler_REST::provider_init_state()
   s->copy_source = FCGX_GetParam("HTTP_X_AMZ_COPY_SOURCE", s->fcgx->envp);
   s->http_auth = FCGX_GetParam("HTTP_AUTHORIZATION", s->fcgx->envp);
 
-  const char *expect = FCGX_GetParam("HTTP_EXPECT", s->fcgx->envp);
-  s->expect_cont = (expect && !strcasecmp(expect, "100-continue"));
+  const char *cgi_env_continue = FCGX_GetParam("RGW_PRINT_CONTINUE", s->fcgx->envp);
+  if (cgi_env_continue != NULL && strcasecmp(cgi_env_continue, "On") == 0) {
+    const char *expect = FCGX_GetParam("HTTP_EXPECT", s->fcgx->envp);
+    s->expect_cont = (expect && !strcasecmp(expect, "100-continue"));
+  }
 }
 
 static bool is_acl_op(struct req_state *s)
