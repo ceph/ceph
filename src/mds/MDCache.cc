11a24f5e91ce9bb202c2106b87f6f222eb123b58
Sage Weil <sage@newdream.net>
2010-10-05 11:17:29 -0700

    mds: set root dir_layout during mkfs
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/mds/MDCache.cc
+++ src/mds/MDCache.cc
@@ -276,8 +276,8 @@ CInode *MDCache::create_system_inode(inodeno_t ino, int mode)
 CInode *MDCache::create_root_inode()
 {
   CInode *i = create_system_inode(MDS_INO_ROOT, S_IFDIR|0755);
-  i->default_layout = new default_file_layout;
-  *(i->default_layout) = default_file_layout;
+  i->default_layout = new struct default_file_layout;
+  i->default_layout->layout = default_file_layout;
   return i;
 }
 
