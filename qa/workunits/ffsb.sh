7f493a11cbe3f87d5746fa13923c34a2e413cca3
Sage Weil <sage@newdream.net>
2010-10-13 10:09:27 -0700

    qa: add ffsb

---
+++ qa/workunits/ffsb.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+mydir=`dirname $0`
+
+wget http://ceph.newdream.net/qa/ffsb.tar.bz2
+tar jxvf ffsb.tar.bz2
+cd ffsb-*
+./configure
+make
+cd ..
+mkdir tmp
+cd tmp
+
+for f in $mydir/*.ffsb
+do
+    ../ffsb-*/ffsb $f
+done
+cd ..
+rm -r tmp ffsb*
+
