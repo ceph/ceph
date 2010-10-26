e6df8074f317f5ac37751efff379121d14f8071c
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2010-10-26 10:04:12 -0700

    test: create test_unfound.sh
    Create test_unfound.sh to test handling unfound objects.
    
    Move more test functions into test/test_common.sh to facilitate code
    reuse.
    
    Allow the user to keep the temporary directory around after the test is
    over by setting KEEP_TEMPDIR.
    
    Signed-off-by: Colin McCabe <colinm@hq.newdream.net>

---
+++ src/test/test_unfound.sh
@@ -0,0 +1,56 @@
+#!/bin/bash -x
+
+#
+# Creates some unfound objects and then tests finding them.
+#
+
+# Includes
+source "`dirname $0`/test_common.sh"
+
+# Constants
+my_write_objects() {
+        write_objects $1 $2 50 1000000
+}
+
+setup() {
+        # Start ceph
+        ./stop.sh
+
+        # set recovery start to a really long time to ensure that we don't start recovery
+        CEPH_NUM_OSD=2 ./vstart.sh -d -n -o 'osd recovery delay start = 10000' || die "vstart failed"
+}
+
+do_test() {
+        # Write lots and lots of objects
+        my_write_objects 1 2
+
+        # Take down osd1
+        stop_osd 1
+
+        # Continue writing a lot of objects
+        my_write_objects 3 4
+
+        # Bring up osd1
+        restart_osd 1
+
+        # Finish peering.
+        sleep 15
+
+        # Stop osd0.
+        # At this point we have peered, but *NOT* recovered.
+        # Objects should be lost.
+        stop_osd 0
+
+        echo "There should be unfound objects."
+        continue_prompt "to test finding the unfound objects."
+
+        restart_osd 0
+}
+
+run() {
+        setup
+
+        do_test
+}
+
+$@
