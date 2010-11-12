ce6d63947fc9dc479376184e50683bdd9dd8cf81
Sage Weil <sage@newdream.net>
2010-11-11 16:38:02 -0800

    timer: rewrite mostly from scratch
    Just use the provided lock.  This _vastly_ reduces the complexity because
    we don't have to worry about races between our thread trying to fire off a
    timer that is being canceled.
    
    The old Timer class isn't used anywhere anymore.
    
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/common/Timer.cc
+++ src/common/Timer.cc
@@ -22,354 +22,164 @@
 
 #define DOUT_SUBSYS timer
 #undef dout_prefix
-#define dout_prefix *_dout << dbeginl << " TIMER "
-
-#define DBL 10
+#define dout_prefix *_dout << dbeginl << "timer(" << this << ")."
 
 #include <sstream>
 #include <signal.h>
 #include <sys/time.h>
 #include <math.h>
 
+
+class SafeTimerThread : public Thread {
+  SafeTimer *parent;
+public:
+  SafeTimerThread(SafeTimer *s) : parent(s) {}
+  void *entry() {
+    parent->timer_thread();
+    return NULL;
+  }
+} *thread;
+
+
+
 typedef std::multimap < utime_t, Context *> scheduled_map_t;
 typedef std::map < Context*, scheduled_map_t::iterator > event_lookup_map_t;
 
-class TimerThread : public Thread {
-public:
-  TimerThread(Timer &parent_)
-    : p(parent_)
-  {
-  }
-
-  void *entry()
-  {
-    p.lock.Lock();
-    while (true) {
-      /* Wait for a cond_signal */
-      scheduled_map_t::iterator s = p.scheduled.begin();
-      if (s == p.scheduled.end())
-	p.cond.Wait(p.lock);
-      else
-	p.cond.WaitUntil(p.lock, s->first);
-
-      if (p.exiting) {
-	dout(DBL) << "exiting TimerThread" << dendl;
-	p.lock.Unlock();
-	return NULL;
-      }
-
-      // Find out what callbacks we have to do
-      list <Context*> running;
-      utime_t now = g_clock.now();
-      p.pop_running(running, now);
-
-      if (running.empty()) {
-	dout(DBL) << "TimerThread: nothing to do." << dendl;
-	continue;
-      }
-
-      p.lock.Unlock();
-
-      lock_event_lock();
-      // p.running is protected by the event lock.
-      p.running.swap(running);
-      while (true) {
-	list <Context*>::const_iterator cit = p.running.begin();
-	if (cit == p.running.end())
-	  break;
-	Context *ctx = *cit;
-	p.running.pop_front();
-	dout(DBL) << "start callback " << ctx << dendl;
-	ctx->finish(0);
-	dout(DBL) << "deleting callback " << ctx << dendl;
-	delete ctx;
-	unlock_event_lock();
-	// Release the event_lock here to give other waiters a chance.
-	dout(DBL) << "finished callback " << ctx << dendl;
-	lock_event_lock();
-      }
-      unlock_event_lock();
-      p.lock.Lock();
-    }
-  }
-
-private:
-  inline void lock_event_lock()
-  {
-    if (p.event_lock)
-      p.event_lock->Lock();
-  }
-
-  inline void unlock_event_lock()
-  {
-    if (p.event_lock)
-      p.event_lock->Unlock();
-  }
-
-  Timer &p;
-};
-
-Timer::Timer()
-  : lock("Timer::lock"),
-    event_lock(NULL),
-    cond(),
+SafeTimer::SafeTimer(Mutex &l)
+  : lock(l),
     thread(NULL),
-    exiting(false)
+    stopping(false) 
 {
-  if (init()) {
-    assert(0);
-  }
+  thread = new SafeTimerThread(this);
+  thread->create();
 }
 
-Timer::Timer(Mutex *event_lock_)
-  : lock("Timer::lock"),
-    event_lock(event_lock_),
-    cond(),
-    thread(NULL),
-    exiting(false)
-{
-  if (init()) {
-    assert(0);
-  }
-}
-
-Timer::~Timer()
+SafeTimer::~SafeTimer()
 {
   shutdown();
 }
 
-void Timer::shutdown()
+void SafeTimer::shutdown()
 {
-  lock.Lock();
-  if (!thread) {
+  if (thread) {
+    lock.Lock();
+    cancel_all_events();
+    
+    stopping = true;
+    cond.Signal();
     lock.Unlock();
-    return;
+
+    thread->join();
+
+    delete thread;
+    thread = NULL;
   }
-  exiting = true;
-  cancel_all_events_impl(false);
-  cond.Signal();
-  lock.Unlock();
-
-  /* Block until the thread has exited.
-   * Only then do we know that no events are in progress. */
-  thread->join();
-
-  delete thread;
-  thread = NULL;
 }
 
-void Timer::add_event_after(double seconds,
-                            Context *callback)
+void SafeTimer::timer_thread()
 {
+  lock.Lock();
+  dout(10) << "timer_thread starting" << dendl;
+  while (!stopping) {
+    utime_t now = g_clock.now();
+    
+    while (!schedule.empty()) {
+      scheduled_map_t::iterator p = schedule.begin();
+
+      // is the future now?
+      if (p->first > now)
+	break;
+
+      Context *callback = p->second;
+      events.erase(callback);
+      schedule.erase(p);
+      dout(10) << "timer_thread executing " << callback << dendl;
+      
+      callback->finish(0);
+      delete callback;
+    }
+
+    dout(20) << "timer_thread going to sleep" << dendl;
+    if (schedule.empty())
+      cond.Wait(lock);
+    else
+      cond.WaitUntil(lock, schedule.begin()->first);
+    dout(20) << "timer_thread awake" << dendl;
+  }
+  dout(10) << "timer_thread exiting" << dendl;
+  lock.Unlock();
+}
+
+void SafeTimer::add_event_after(double seconds, Context *callback)
+{
+  assert(lock.is_locked());
+
   utime_t when = g_clock.now();
   when += seconds;
-  Timer::add_event_at(when, callback);
+  add_event_at(when, callback);
 }
 
-void Timer::add_event_at(utime_t when, Context *callback)
+void SafeTimer::add_event_at(utime_t when, Context *callback)
 {
-  lock.Lock();
-
-  /* Don't start using the timer until it's initialized */
-  assert(thread);
-
-  dout(DBL) << "add_event " << callback << " at " << when << dendl;
+  assert(lock.is_locked());
+  dout(10) << "add_event_at " << when << " -> " << callback << dendl;
 
   scheduled_map_t::value_type s_val(when, callback);
-  scheduled_map_t::iterator i = scheduled.insert(s_val);
+  scheduled_map_t::iterator i = schedule.insert(s_val);
 
   event_lookup_map_t::value_type e_val(callback, i);
   pair < event_lookup_map_t::iterator, bool > rval(events.insert(e_val));
-  dout(DBL) << "inserted events entry for " << callback << dendl;
 
   /* If you hit this, you tried to insert the same Context* twice. */
   assert(rval.second);
 
   /* If the event we have just inserted comes before everything else, we need to
    * adjust our timeout. */
-  if (i == scheduled.begin())
+  if (i == schedule.begin())
     cond.Signal();
 
-  dout(19) << show_all_events(__func__) << dendl;
-  lock.Unlock();
-}
-
-bool Timer::cancel_event(Context *callback)
-{
-  dout(DBL) << __PRETTY_FUNCTION__ << ": " << callback << dendl;
-
-  lock.Lock();
-  bool ret = cancel_event_impl(callback, false);
-  lock.Unlock();
-  return ret;
-}
-
-void Timer::cancel_all_events(void)
-{
-  dout(DBL) << __PRETTY_FUNCTION__ << dendl;
-
-  dout(19) << show_all_events(__func__) << dendl;
-
-  lock.Lock();
-  cancel_all_events_impl(false);
-  lock.Unlock();
-}
-
-int Timer::init()
-{
-  int ret = 0;
-  lock.Lock();
-  assert(exiting == false);
-  assert(!thread);
-
-  dout(DBL) << "Timer::init: starting thread" << dendl;
-  thread = new TimerThread(*this);
-  ret = thread->create();
-  lock.Unlock();
-  return ret;
-}
-
-bool Timer::cancel_event_impl(Context *callback, bool cancel_running)
-{
-  dout(19) << show_all_events(__func__) << dendl;
-
-  event_lookup_map_t::iterator e = events.find(callback);
-  if (e != events.end()) {
-    // Erase the item out of the scheduled map.
-    scheduled.erase(e->second);
-    events.erase(e);
-
-    delete callback;
-    return true;
-  }
-
-  // If we can't peek at the running list, we have to give up.
-  if (!cancel_running)
-    return false;
-
-  // Ok, we will check the running list. It's safe, because we're holding the
-  // event_lock.
-  list <Context*>::iterator cit =
-    std::find(running.begin(), running.end(), callback);
-  if (cit == running.end())
-    return false;
-  running.erase(cit);
-  delete callback;
-  return true;
-}
-
-void Timer::cancel_all_events_impl(bool clear_running)
-{
-  while (1) {
-    scheduled_map_t::iterator s = scheduled.begin();
-    if (s == scheduled.end())
-      break;
-    delete s->second;
-    scheduled.erase(s);
-  }
-  events.clear();
-
-  if (clear_running) {
-    running.clear();
-  }
-}
-
-void Timer::pop_running(list <Context*> &running_, const utime_t &now)
-{
-  while (true) {
-    std::multimap < utime_t, Context* >::iterator s = scheduled.begin();
-    if (s == scheduled.end())
-      return;
-    const utime_t &utime(s->first);
-    Context *cit(s->second);
-    if (utime > now)
-      return;
-    running_.push_back(cit);
-    event_lookup_map_t::iterator e = events.find(cit);
-    assert(e != events.end());
-    events.erase(e);
-    scheduled.erase(s);
-  }
-}
-
-std::string Timer::show_all_events(const char *caller) const
-{
-  ostringstream oss;
-  string sep;
-  oss << "show_all_events: from " << caller << ": scheduled [";
-  for (scheduled_map_t::const_iterator s = scheduled.begin();
-       s != scheduled.end();
-       ++s)
-  {
-    oss << sep << s->first << "->" << s->second;
-    sep = ",";
-  }
-  oss << "] ";
-
-  oss << "events [";
-  string sep2;
-  for (event_lookup_map_t::const_iterator e = events.begin();
-       e != events.end();
-       ++e)
-  {
-    oss << sep2 << e->first << "->" << e->second->first;
-    sep2 = ",";
-  }
-  oss << "]";
-  return oss.str();
-}
-
-/******************************************************************/
-SafeTimer::SafeTimer(Mutex &event_lock)
-  : t(&event_lock)
-{
-}
-
-SafeTimer::~SafeTimer()
-{
-  dout(DBL) << __PRETTY_FUNCTION__ << dendl;
-
-  t.shutdown();
-}
-
-void SafeTimer::shutdown()
-{
-  t.shutdown();
-}
-
-void SafeTimer::add_event_after(double seconds, Context *callback)
-{
-  assert(t.event_lock->is_locked());
-
-  t.add_event_after(seconds, callback);
-}
-
-void SafeTimer::add_event_at(utime_t when, Context *callback)
-{
-  assert(t.event_lock->is_locked());
-
-  t.add_event_at(when, callback);
 }
 
 bool SafeTimer::cancel_event(Context *callback)
 {
-  dout(DBL) << __PRETTY_FUNCTION__ << ": " << callback << dendl;
+  assert(lock.is_locked());
+  
+  std::map<Context*, std::multimap<utime_t, Context*>::iterator>::iterator p = events.find(callback);
+  if (p == events.end()) {
+    dout(10) << "cancel_event " << callback << " not found" << dendl;
+    return false;
+  }
 
-  assert(t.event_lock->is_locked());
+  dout(10) << "cancel_event " << p->second->first << " -> " << callback << dendl;
+  delete p->first;
 
-  t.lock.Lock();
-  bool ret = t.cancel_event_impl(callback, true);
-  t.lock.Unlock();
-  return ret;
+  schedule.erase(p->second);
+  events.erase(p);
+  return true;
 }
 
 void SafeTimer::cancel_all_events()
 {
-  dout(DBL) << __PRETTY_FUNCTION__ << dendl;
+  dout(10) << "cancel_all_events" << dendl;
+  assert(lock.is_locked());
+  
+  while (!events.empty()) {
+    std::map<Context*, std::multimap<utime_t, Context*>::iterator>::iterator p = events.begin();
+    dout(10) << " cancelled " << p->second->first << " -> " << p->first << dendl;
+    delete p->first;
+    schedule.erase(p->second);
+    events.erase(p);
+  }
+}
 
-  assert(t.event_lock->is_locked());
+void SafeTimer::dump(const char *caller) const
+{
+  if (!caller)
+    caller = "";
+  dout(10) << "dump " << caller << dendl;
 
-  t.lock.Lock();
-  t.cancel_all_events_impl(true);
-  t.lock.Unlock();
+  for (scheduled_map_t::const_iterator s = schedule.begin();
+       s != schedule.end();
+       ++s)
+    dout(10) << " " << s->first << "->" << s->second << dendl;
 }
--- src/common/Timer.h
+++ src/common/Timer.h
@@ -12,104 +12,40 @@
  *
  */
 
-
 #ifndef CEPH_TIMER_H
 #define CEPH_TIMER_H
 
-#include "Clock.h"
 #include "Cond.h"
 #include "Mutex.h"
-#include "include/types.h"
 
-#include <list>
 #include <map>
 
 class Context;
-class TimerThread;
+class SafeTimerThread;
 
-/* Timer
- *
- * An instance of the timer class holds a thread which executes callbacks at
- * predetermined times.
- */
-class Timer
-{
-public:
-  Timer();
-
-  /* Calls shutdown() */
-  ~Timer();
-
-  /* Cancel all events and stop the timer thread.
-   *
-   * This function might block for a while because it does a thread.join().
-   * */
-  void shutdown();
-
-  /* Schedule an event in the future */
-  void add_event_after(double seconds, Context *callback);
-  void add_event_at(utime_t when, Context *callback);
-
-  /* Cancel an event.
-   *
-   * If this function returns true, you know that the callback has been
-   * destroyed and is not currently running.
-   * If it returns false, either the callback is in progress, or you never addded
-   * the callback in the first place.
-   */
-  bool cancel_event(Context *callback);
-
-  /* Cancel all events.
-   *
-   * Even after this function returns, there may be events in progress.
-   * Use SafeTimer if you have to be sure that nothing is running after
-   * cancelling an event or events.
-   */
-  void cancel_all_events();
-
-private:
-  Timer(Mutex *event_lock_);
-
-  /* Starts the timer thread.
-   * Returns 0 on success; error code otherwise. */
-  int init();
-
-  bool cancel_event_impl(Context *callback, bool cancel_running);
-
-  void cancel_all_events_impl(bool clear_running);
-
-  void pop_running(std::list <Context*> &running_, const utime_t &now);
-
-  std::string show_all_events(const char *caller) const;
-
-  // This class isn't supposed to be copied
-  Timer(const Timer &rhs);
-  Timer& operator=(const Timer &rhs);
-
-  Mutex lock;
-  Mutex *event_lock;
-  Cond cond;
-  TimerThread *thread;
-  bool exiting;
-  std::multimap < utime_t, Context* > scheduled;
-  std::map < Context*, std::multimap < utime_t, Context* >::iterator > events;
-  std::list<Context*> running;
-
-  friend class TimerThread;
-  friend class SafeTimer;
-};
-
-/*
- * SafeTimer is a wrapper around the a Timer that protects event
- * execution with an existing mutex.  It provides for, among other
- * things, reliable event cancellation in cancel_event. Unlike in
- * Timer::cancel_event, the caller can be sure that once SafeTimer::cancel_event
- * returns, the callback will not be in progress.
- */
 class SafeTimer
 {
+  // This class isn't supposed to be copied
+  SafeTimer(const SafeTimer &rhs);
+  SafeTimer& operator=(const SafeTimer &rhs);
+
+  Mutex& lock;
+  Cond cond;
+
+  friend class SafeTimerThread;
+  SafeTimerThread *thread;
+
+  void timer_thread();
+  void _shutdown();
+
+  std::multimap<utime_t, Context*> schedule;
+  std::map<Context*, std::multimap<utime_t, Context*>::iterator> events;
+  bool stopping;
+
+  void dump(const char *caller = 0) const;
+
 public:
-  SafeTimer(Mutex &event_lock_);
+  SafeTimer(Mutex &l);
   ~SafeTimer();
 
   /* Call with the event_lock UNLOCKED.
@@ -141,11 +77,5 @@ public:
    */
   void cancel_all_events();
 
-private:
-  // This class isn't supposed to be copied
-  SafeTimer(const SafeTimer &rhs);
-  SafeTimer& operator=(const SafeTimer &rhs);
-
-  Timer t;
 };
 #endif
