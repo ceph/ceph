2f5502fabd773daa97638a8182f58674cfad04a4
Sage Weil <sage@newdream.net>
2010-11-19 13:41:58 -0800

    osdmap: fix printing, again
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/osd/OSDMap.cc
+++ src/osd/OSDMap.cc
@@ -59,14 +59,14 @@ void OSDMap::print(ostream& out) const
   for (int i=0; i<get_max_osd(); i++) {
     if (exists(i)) {
       out << "osd" << i;
-      out << (is_up(i) ? " up   ":" down ");
+      out << (is_up(i) ? " up  ":" down");
       out << (is_in(i) ? " in ":" out");
       if (is_in(i))
 	out << " weight " << get_weightf(i);
       const osd_info_t& info(get_info(i));
-      out << info << " ";
+      out << " " << info;
       if (is_up(i))
-	out << get_addr(i) << " " << get_hb_addr(i);
+	out << " " << get_addr(i) << " " << get_hb_addr(i);
       out << "\n";
     }
   }
