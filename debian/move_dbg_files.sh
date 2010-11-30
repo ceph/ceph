9360126943f214324d60a93a69218dab3a51819d
Sage Weil <sage@newdream.net>
2010-11-30 09:13:54 -0800

    debian: many many cleanups
    Signed-off-by: Laszlo Boszormenyi <gcs@debian.hu>

--- debian/move_dbg_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for f in ceph ceph-fuse ceph-client-tools libcrush1 librados1 libceph1 radosgw
-do
-    echo moving $f unstripped binaries into $f-dbg
-    mkdir -p debian/$f-dbg/usr/lib
-    mv debian/$f/usr/lib/debug debian/$f-dbg/usr/lib
-done
