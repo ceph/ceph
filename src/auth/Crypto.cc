e135e9245efcfd2c985a83af14bba5423d05a037
Yehuda Sadeh <yehuda@hq.newdream.net>
2010-12-03 13:06:42 -0800

    crypto: remove old openssl implementation

--- src/auth/Crypto.cc
+++ src/auth/Crypto.cc
@@ -11,17 +11,10 @@
  * 
  */
 
-#define CRYPTOPP
-
 #include "Crypto.h"
-#ifdef CRYPTOPP
 #include "cryptopp/modes.h"
 #include "cryptopp/aes.h"
 #include "cryptopp/filters.h"
-#else
-#include "openssl/evp.h"
-#include "openssl/aes.h"
-#endif
 
 #include "include/ceph_fs.h"
 #include "config.h"
@@ -29,9 +22,7 @@
 
 #include <errno.h>
 
-#ifdef CRYPTOPP
 using namespace CryptoPP;
-#endif
 
 int get_random_bytes(char *buf, int len)
 {
@@ -105,13 +96,8 @@ int CryptoNone::decrypt(bufferptr& secret, const bufferlist& in, bufferlist& out
 
 
 // ---------------------------------------------------
-#ifdef CRYPTOPP
 #define AES_KEY_LEN     ((size_t)AES::DEFAULT_KEYLENGTH)
 #define AES_BLOCK_LEN   ((size_t)AES::BLOCKSIZE)
-#else
-#define AES_KEY_LEN     AES_BLOCK_SIZE
-#define AES_BLOCK_LEN   AES_BLOCK_SIZE
-#endif
 
 class CryptoAES : public CryptoHandler {
 public:
@@ -154,7 +140,6 @@ int CryptoAES::encrypt(bufferptr& secret, const bufferlist& in, bufferlist& out)
     derr(0) << "key is too short" << dendl;
     return false;
   }
-#ifdef CRYPTOPP
   string ciphertext;
   CryptoPP::AES::Encryption aesEncryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
   CryptoPP::CBC_Mode_ExternalCipher::Encryption cbcEncryption( aesEncryption, aes_iv );
@@ -173,44 +158,11 @@ int CryptoAES::encrypt(bufferptr& secret, const bufferlist& in, bufferlist& out)
   out.append((const char *)ciphertext.c_str(), ciphertext.length());
 
   return true;
-#else
-  int in_len = in.length();
-  int max_out = (in_len + AES_BLOCK_LEN) & ~(AES_BLOCK_LEN - 1);
-  int total_out = 0;
-  int outlen;
-#define OUT_BUF_EXTRA 128
-  unsigned char outbuf[max_out + OUT_BUF_EXTRA];
-
-  EVP_CIPHER_CTX ctx;
-  EVP_CIPHER_CTX_init(&ctx);
-  EVP_EncryptInit_ex(&ctx, EVP_aes_128_cbc(), NULL, key, aes_iv);
-
-  bool ret = false;
-  for (std::list<bufferptr>::const_iterator it = in.buffers().begin(); 
-       it != in.buffers().end(); it++) {
-    outlen = max_out - total_out;
-    in_buf = (const unsigned char *)it->c_str();
-    if (!EVP_EncryptUpdate(&ctx, &outbuf[total_out], &outlen, in_buf, it->length()))
-      goto out;
-    total_out += outlen;
-  }
-  if (!EVP_EncryptFinal_ex(&ctx, outbuf + total_out, &outlen))
-    goto out;
-  total_out += outlen;
-  out.append((const char *)outbuf, total_out);
-  ret = true;
-out:
-  EVP_CIPHER_CTX_cleanup(&ctx);
-  return ret;
-#endif
 }
 
 int CryptoAES::decrypt(bufferptr& secret, const bufferlist& in, bufferlist& out)
 {
   const unsigned char *key = (const unsigned char *)secret.c_str();
-
-#ifdef CRYPTOPP
-  dout(0) << "CryptoPP!" << dendl;
 
   CryptoPP::AES::Decryption aesDecryption(key, CryptoPP::AES::DEFAULT_KEYLENGTH);
   CryptoPP::CBC_Mode_ExternalCipher::Decryption cbcDecryption( aesDecryption, aes_iv );
@@ -230,49 +182,6 @@ int CryptoAES::decrypt(bufferptr& secret, const bufferlist& in, bufferlist& out)
 
   out.append((const char *)decryptedtext.c_str(), decryptedtext.length());
   return decryptedtext.length();
-#else
-  int in_len = in.length();
-  int result = 0;
-
-  int dec_len = 0;
-  int total_dec_len = 0;
-  unsigned char dec_data[in_len];
-
-  EVP_CIPHER_CTX *ctx = EVP_CIPHER_CTX_new();
-  EVP_CIPHER_CTX_init(ctx);
-
-  int res = EVP_DecryptInit_ex(ctx, EVP_aes_128_cbc(), NULL, key, aes_iv);
-  if (res == 1) {
-    for (std::list<bufferptr>::const_iterator it = in.buffers().begin(); 
-       it != in.buffers().end(); it++) {
-      const unsigned char *in_buf = (const unsigned char *)it->c_str();
-      res = EVP_DecryptUpdate(ctx, &dec_data[total_dec_len],
-            &dec_len, in_buf, it->length());
-      total_dec_len += dec_len;
-
-      if (res != 1) {
-        dout(0) << "EVP_DecryptUpdate error" << dendl;
-        result = -1;
-      }
-    }
-  } else {
-    dout(0) << "EVP_DecryptInit_ex error" << dendl;
-    result = -1;
-  }
-
-  if (!result) {
-    EVP_DecryptFinal_ex(ctx,
-              &dec_data[total_dec_len],
-              &dec_len);
-
-    total_dec_len += dec_len;
-    out.append((const char *)dec_data, total_dec_len);
-  }
-
-  EVP_CIPHER_CTX_free(ctx);
-
-  return result;
-#endif
 }
 
 
