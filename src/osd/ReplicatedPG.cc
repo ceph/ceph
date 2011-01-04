ceph/ceph

commit 4a4a1e53c7d380cd0b582c1d0685fd0ef4ef1711

PG: Fix bug in scrub when checking clone sizes
Previosly, _scrub checked:
assert(p->second.size == snapset.clone_size[curclone])

curclone was, however, an index into snapset.clones rather than a
snapid_t.  For clarity, curclone is now an iterator.

Signed-off-by: Samuel Just <samuelj@hq.newdream.net>

src/osd/ReplicatedPG.cc
@@ -3985,7 +3985,7 @@ int ReplicatedPG::_scrub(ScrubMap& scrubmap, int& errors, int& fixed)
   // traverse in reverse order.
   sobject_t head;
   SnapSet snapset;
-  unsigned curclone = 0;
+  vector<snapid_t>::reverse_iterator curclone;
 
   pg_stat_t stat;
 
@@ -4021,7 +4021,7 @@ int ReplicatedPG::_scrub(ScrubMap& scrubmap, int& errors, int& fixed)
       if (snapset.clones.empty())
 	head = sobject_t();  // no clones.
       else {
-	curclone = snapset.clones.size()-1;
+	curclone = snapset.clones.rbegin();
 	head = p->first;
       }
 
@@ -4071,15 +4071,15 @@ int ReplicatedPG::_scrub(ScrubMap& scrubmap, int& errors, int& fixed)
 
       stat.num_object_clones++;
       
-      assert(soid.snap == snapset.clones[curclone]);
+      assert(soid.snap == *curclone);
 
-      assert(p->second.size == snapset.clone_size[curclone]);
+      assert(p->second.size == snapset.clone_size[*curclone]);
 
       // verify overlap?
       // ...
 
       // what's next?
-      if (curclone == 0)
+      if (curclone == snapset.clones.rend())
 	head = sobject_t();
       else
 	curclone--;

commit fab61391b7c662964e2498d31c1e1d0b65d4894e

PG: Fixes bug in _scrub with checking clones
I introduced this bug in
4a4a1e53c7d380cd0b582c1d0685fd0ef4ef1711.
curclone++ not curclone--.

Signed-off-by: Samuel Just <samuelj@hq.newdream.net>

src/osd/ReplicatedPG.cc
@@ -4079,10 +4079,11 @@ int ReplicatedPG::_scrub(ScrubMap& scrubmap, int& errors, int& fixed)
       // ...
 
       // what's next?
+      if (curclone != snapset.clones.rend())
+	curclone++;
+
       if (curclone == snapset.clones.rend())
 	head = sobject_t();
-      else
-	curclone--;
 
     } else {
       // it's unversioned.