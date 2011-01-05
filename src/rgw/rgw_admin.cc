bba356b86641085aeb8b76cea3526622265bf387
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-01-05 15:29:48 -0800

    rgw_admin: call common_set_defaults as non-daemon
    Signed-off-by: Colin McCabe <colinm@hq.newdream.net>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -82,7 +82,7 @@ int main(int argc, char **argv)
   argv_to_vec(argc, (const char **)argv, args);
   env_to_vec(args);
 
-  common_set_defaults(true);
+  common_set_defaults(false);
   common_init(args, "rgw", true);
 
   const char *user_id = 0;
