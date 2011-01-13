e7153f383f8a6e4b32f8a38809b2023799bab3b7
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-01-13 10:23:49 -0800

    unit: Add test/base64.cc
    Signed-off-by: Colin McCabe <colinm@hq.newdream.net>

---
+++ src/test/base64.cc
@@ -0,0 +1,35 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2011 Dreamhost
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
+#include "common/armor.h"
+#include "config.h"
+#include "include/buffer.h"
+#include "include/encoding.h"
+
+#include "gtest/gtest.h"
+
+TEST(CorrectBase64Encoding, StringSimple) {
+  static const int OUT_LEN = 4096;
+  const char * const original = "abracadabra";
+  const char * const correctly_encoded = "YWJyYWNhZGFicmE=";
+  char out[OUT_LEN];
+  memset(out, 0, sizeof(out));
+  int alen = ceph_armor(out, out + OUT_LEN, original, original + strlen(original));
+  ASSERT_STREQ(correctly_encoded, out);
+
+  char out2[OUT_LEN];
+  memset(out2, 0, sizeof(out2));
+  ceph_unarmor(out2, out2 + OUT_LEN, out, out + alen);
+  ASSERT_STREQ(original, out2);
+}
