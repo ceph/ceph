ceph/ceph

commit d1b85e06fb5ce1cfd5bbc74ba639811b92033909

ReplicatedPG.cc: update coi version and prior_version to match log
Caused error where oi on clone would not get updated version when snaps
was updated.  oi.version would lag behind the missing item's need field
during recovery.

Signed-off-by: Samuel Just <samuel.just@dreamhost.com>

src/osd/ReplicatedPG.cc
@@ -659,6 +659,8 @@ bool ReplicatedPG::snap_trimmer()
 	// save adjusted snaps for this object
 	dout(10) << coid << " snaps " << snaps << " -> " << newsnaps << dendl;
 	coi.snaps.swap(newsnaps);
+	coi.prior_version = coi.version;
+	coi.version = ctx->at_version;
 	bl.clear();
 	::encode(coi, bl);
 	t->setattr(coll_t(info.pgid), coid, OI_ATTR, bl);

commit 785bf0fcbfb69efa8dd97340c8ee0079bb5ad55e

ReplicatedPG: fix snap_trimmer log version bug
Previously, ctx->at_version would be the same as ctx->obs->oi.version
leading to the log entry having prior_version == version.
This bug was introduced in d1b85e06fb5ce1cfd5bbc74ba639811b92033909.

Signed-off-by: Samuel Just <samuel.just@dreamhost.com>

src/osd/ReplicatedPG.cc
@@ -674,7 +674,7 @@ bool ReplicatedPG::snap_trimmer()
 	    t->collection_add(coll_t(info.pgid, newsnaps[newsnaps.size()-1]), coll_t(info.pgid), coid);
 	}	      
 
-	ctx->log.push_back(Log::Entry(Log::Entry::MODIFY, coid, ctx->at_version, ctx->obs->oi.version,
+	ctx->log.push_back(Log::Entry(Log::Entry::MODIFY, coid, coi.version, coi.prior_version,
 				      osd_reqid_t(), ctx->mtime));
 	ctx->at_version.version++;
       }