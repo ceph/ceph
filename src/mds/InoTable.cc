cf59cdf7f2d2f331c896221927fc8ee6cd2f885e
Sage Weil <sage.weil@dreamhost.com>
2011-01-28 16:56:22 -0800

    mds: skip a few more inodes during journal reset
    To be safe...
    
    Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

--- src/mds/InoTable.cc
+++ src/mds/InoTable.cc
@@ -137,7 +137,7 @@ void InoTable::replay_release_ids(interval_set<inodeno_t>& ids)
 void InoTable::replay_reset()
 {
   dout(10) << "replay_reset " << free << dendl;
-  skip_inos(inodeno_t(1000000));  // a lot!
+  skip_inos(inodeno_t(10000000));  // a lot!
   projected_free = free;
   projected_version = ++version;
 }
