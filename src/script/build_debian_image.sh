b5f75927ca77af12a28c1544571a7dd24d9fc2dd
Sage Weil <sage.weil@dreamhost.com>
2011-02-06 21:17:08 -0800

    script: add script to build uml images
    Set up a tap interface in your bridge, and then
    
    ./linux ubda=image mem=512M eth0=tuntap,tap0 ip=<ip> netmask=<netmask> gw=<gw>
    
    Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

---
+++ src/script/build_debian_image.sh
@@ -0,0 +1,77 @@
+#!/bin/sh -x
+
+set -e
+
+image="$1"
+root=/tmp/$$
+dist=squeeze
+
+# image
+[ -e $image ] && echo $image exists && exit 1
+dd if=/dev/zero of=$image bs=1M seek=1023 count=1
+yes | mke2fs -j $image
+
+mkdir $root
+mount -o loop $image $root
+
+cleanup() {
+	umount $root/proc || true
+	umount $root/sys || true
+	umount $root
+}
+trap cleanup INT TERM EXIT
+
+debootstrap $dist $root http://http.us.debian.org/debian/
+
+cat <<EOF >> $root/etc/fstab
+none /dev/pts devpts gid=5,mode=620 0 0
+proc /proc proc defaults 0 0
+sysfs /sys sysfs defaults 0 0
+none /sys/kernel/debug debugfs defaults 0 0
+tmpfs /tmp tmpfs defaults,size=768M 0 0
+none /host hostfs defaults 0 0
+EOF
+
+echo "uml" > $root/etc/hostname
+mkdir $root/host
+
+# set up chroot
+mount -t proc proc $root/proc
+mount -t sysfs sysfs $root/sys
+
+# set up network
+cp network-from-cmdline $root/etc/init.d/network-from-cmdline
+chroot $root update-rc.d network-from-cmdline defaults 20
+
+# fix up consoles
+cat <<EOF >> $root/etc/securetty 
+
+# uml
+tty0
+vc/0
+EOF
+
+cat <<EOF >> $root/etc/inittab
+
+# uml just one console
+c0:1235:respawn:/sbin/getty 38400 tty0 linux
+EOF
+grep -v tty[23456] $root/etc/inittab > $root/etc/inittab.new
+mv $root/etc/inittab.new $root/etc/inittab
+
+# no root password
+chroot $root passwd -d root
+
+# copy creating user's authorized_keys
+mkdir $root/root/.ssh
+chmod 700 $root/root/.ssh
+cp ~/.ssh/authorized_keys $root/root/.ssh/authorized_keys
+chmod 600 $root/root/.ssh/authorized_keys
+
+# packages
+for p in ssh
+do
+	chroot $root apt-get -y --force-yes install $p
+done
+
+exit 0
