19afe11cc5d398de894ea5dd57b8ca2a8e5e45d9
Sage Weil <sage.weil@dreamhost.com>
2011-02-08 07:54:17 -0800

    osd: fix no missing inferance
    Add missing continue in last_update==last_complete (no missing) case.
    
    Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -1633,6 +1633,7 @@ void PG::do_peer(ObjectStore::Transaction& t, list<Context*>& tfin,
       if (pi.last_update == pi.last_complete) {
 	dout(10) << " infering no missing (last_update==last_complete) for osd" << peer << dendl;
 	peer_missing[peer].num_missing();  // just create the entry.
+	continue;
       } else {
 	dout(10) << " still need log+missing from osd" << peer << dendl;
 	have_all_missing = false;
