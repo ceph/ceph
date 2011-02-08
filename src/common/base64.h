a476cceb5805e7e98ddbf0a06f54adb1f49df24d
Tommi Virtanen <tv@hq.newdream.net>
2011-02-08 09:39:12 -0800

    Removing files that look completely unused.

--- src/common/base64.h
+++
@@ -1,9 +0,0 @@
-#ifndef CEPH_BASE64_H
-#define CEPH_BASE64_H
-
-extern "C" {
-  int encode_base64(const char *in, int in_len, char *out, int out_len);
-  int decode_base64(const char *in, int in_len, char *out, int out_len);
-}
-
-#endif
