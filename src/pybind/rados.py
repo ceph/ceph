061b8d976445c1036d8ce128b4b307fe7f2fafdb
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-02-09 09:39:45 -0800

    pybind/rados: expand on error handling a bit
    Signed-off-by: Colin McCabe <colin.mccabe@dreamhost.com>

--- src/pybind/rados.py
+++ src/pybind/rados.py
@@ -3,19 +3,45 @@ Copyright 2011, Hannu Valtonen <hannu.valtonen@ormod.com>
 """
 from ctypes import CDLL, c_char_p, c_size_t, c_void_p,\
     create_string_buffer, byref, Structure, c_uint64, pointer
+import errno
 import time
 
-class RadosError(Exception):
+class Error(Exception):
+    def __init__(self, code):
+        self.code = code
+
+class PermissionError(Exception):
     pass
 
 class ObjectNotFound(Exception):
     pass
 
-class WriteError(Exception):
+class ObjectExists(Exception):
+    pass
+
+class IOError(Exception):
+    pass
+
+class NoSpace(Exception):
     pass
 
 class IncompleteWriteError(Exception):
     pass
+
+def make_ex(ret, msg):
+    ret = abs(ret)
+    if (ret == errno.EPERM):
+        return PermissionError(msg)
+    elif (ret == errno.ENOENT):
+        return ObjectNotFound(msg)
+    elif (ret == errno.EIO):
+        return IOError(msg)
+    elif (ret == errno.ENOSPC):
+        return NoSpace(msg)
+    elif (ret == errno.EEXIST):
+        return ObjectExists(msg)
+    else:
+        return Error(msg + (": error code %d" % ret))
 
 class rados_pool_stat_t(Structure):
     _fields_ = [("num_bytes", c_uint64),
@@ -50,8 +76,7 @@ class Rados(object):
         self.librados = CDLL('librados.so')
         ret = self.librados.rados_initialize(None)
         if ret != 0:
-            raise RadosError("rados_initialize failed with error code: %d" \
-                    % ret)
+            raise Error("rados_initialize failed with error code: %d" % ret)
         self.initialized = True
 
     def __del__(self):
@@ -61,36 +86,29 @@ class Rados(object):
     def create_pool(self, pool_name):
         ret = self.librados.rados_create_pool(c_char_p(pool_name))
         if ret < 0:
-            raise RadosError("pool %s couldn't be created" % pool_name)
+            raise make_ex(ret, "error creating pool '%s'" % pool_name)
 
-    def delete_pool(self, pool):
-        ret = self.librados.rados_delete_pool(pool)
+    def delete_pool(self, pool_name):
+        ret = self.librados.rados_delete_pool(pool_name)
         if ret < 0:
-            raise RadosError("pool couldn't be deleted")
-
-    def get_pool(self, pool_name):
-        try:
-            pool = self.open_pool(pool_name)
-        except RadosError:
-            self.create_pool(pool_name)
-            pool = self.open_pool(pool_name)
-        return pool
+            raise make_ex(ret, "error deleting pool '%s'" % pool_name)
 
     def open_pool(self, pool_name):
         pool = c_void_p()
         ret = self.librados.rados_open_pool(c_char_p(pool_name), byref(pool))
         if ret < 0:
-            raise RadosError("pool %s couldn't be opened" % pool_name)
-        return Pool(self.librados, pool)
+            raise make_ex(ret, "error opening pool '%s'" % pool)
+        return Pool(pool_name, self.librados, pool)
 
     def close_pool(self, pool):
         ret = self.librados.rados_close_pool(pool)
         if ret < 0:
-            raise RadosError("pool couldn't be closed")
+            raise make_ex(ret, "error closing pool '%s'" % pool)
 
 class Pool(object):
     """Pool object"""
-    def __init__(self, librados, pool):
+    def __init__(self, name, librados, pool):
+        self.name = name
         self.librados = librados
         self.pool = pool
         self.deleted = False
@@ -108,11 +126,14 @@ class Pool(object):
         if ret == length:
             return ret
         elif ret < 0:
-            raise WriteError("Write failed completely")
+            raise make_ex(ret, "Pool.write(%s): failed to write %s" % \
+                (self.name, key))
         elif ret < length:
             raise IncompleteWriteError("Wrote only %ld/%ld bytes" % (ret, length))
         else:
-            raise RadosError("something weird happened while writing")
+            raise make_ex("Pool.write(%s): logic error: rados_write \
+returned %d, but %d was the maximum number of bytes it could have \
+written." % (self.name, ret, length))
 
     @object_deleted
     def read(self, key, offset = 0, length = 8192):
@@ -120,7 +141,7 @@ class Pool(object):
         ret = self.librados.rados_read(self.pool, c_char_p(key), c_size_t(offset),
                                         ret_buf, c_size_t(length))
         if ret < 0:
-            raise RadosError("Read failure, object doesn't exist?")
+            raise make_ex("Pool.read(%s): failed to read %s" % (self.name, key))
         return ret_buf.value
 
     @object_deleted
@@ -128,7 +149,7 @@ class Pool(object):
         stats = rados_pool_stat_t()
         ret = self.librados.rados_stat_pool(self.pool, byref(stats))
         if ret < 0:
-            raise RadosError("Couldn't get stats from pool")
+            raise make_ex(ret, "Pool.get_stats(%s): get_stats failed" % self.name)
         return {'num_bytes': stats.num_bytes,
                 'num_kb': stats.num_kb,
                 'num_objects': stats.num_objects,
@@ -146,7 +167,7 @@ class Pool(object):
     def remove_object(self, key):
         ret = self.librados.rados_remove(self.pool, c_char_p(key))
         if ret < 0:
-            raise RadosError("Delete failure")
+            raise make_ex(ret, "Failed to remove '%s'" % key)
         return True
 
     @object_deleted
@@ -158,7 +179,7 @@ class Pool(object):
         ret = self.librados.rados_stat(self.pool, c_char_p(key), pointer(psize),
                                         pointer(pmtime))
         if ret < 0:
-            raise RadosError("Failed to stat %r" % key)
+            raise make_ex(ret, "Failed to stat %r" % key)
         return psize.value, time.localtime(pmtime.value)
 
     @object_deleted
@@ -168,7 +189,7 @@ class Pool(object):
         ret = self.librados.rados_getxattr(self.pool, c_char_p(key),
                     c_char_p(xattr_name), ret_buf, c_size_t(ret_length))
         if ret < 0:
-            raise RadosError("Failed to get xattr %r" % xattr_name)
+            raise make_ex(ret, "Failed to get xattr %r" % xattr_name)
         return ret_buf.value
 
     @object_deleted
@@ -177,14 +198,15 @@ class Pool(object):
                     c_char_p(xattr_name), c_char_p(xattr_value),
                     c_size_t(len(xattr_value)))
         if ret < 0:
-            raise RadosError("Failed to set xattr %r" % xattr_name)
+            raise make_ex(ret, "Failed to set xattr %r" % xattr_name)
         return True
 
     @object_deleted
     def rm_xattr(self, key, xattr_name):
         ret = self.librados.rados_rmxattr(self.pool, c_char_p(key), c_char_p(xattr_name))
         if ret < 0:
-            raise RadosError("Failed to delete key %r xattr %r" % (key, xattr_name))
+            raise make_ex(ret, "Failed to delete key %r xattr %r" %
+                (key, xattr_name))
         return True
 
 class Object(object):
