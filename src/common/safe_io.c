a743e314b929cb2da012bb4f76ebf25962583f08
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-02-10 03:53:25 -0800

    common:safe_io: create exact and non-exact variant
    Create safe_read_exact, which fails with -EDOM unless it reads exactly
    the requested number of bytes. More often, what we want is the
    semantics of safe_read, which returns the number of bytes it read, or
    negative on error. Likewise for pread.
    
    Write doesn't need variants because failing to write all the bytes is
    always an error.
    
    Signed-off-by: Colin McCabe <colin.mccabe@dreamhost.com>

--- src/common/safe_io.c
+++ src/common/safe_io.c
@@ -1,3 +1,16 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2011 New Dream Network
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
 
 #define _XOPEN_SOURCE 500
 
@@ -7,21 +20,32 @@
 ssize_t safe_read(int fd, void *buf, size_t count)
 {
 	int r;
+	int cnt = 0;
 
-	while (count > 0) {
-		r = read(fd, buf, count);
+	while (cnt < count) {
+		r = read(fd, buf, count - cnt);
 		if (r <= 0) {
 			if (r == 0) {
 				// EOF
-				return -EDOM;
+				return cnt;
 			}
 			if (errno == EINTR)
 				continue;
 			return -errno;
 		}
-		count -= r;
+		cnt += r;
 		buf = (char *)buf + r;
 	}
+	return cnt;
+}
+
+ssize_t safe_read_exact(int fd, void *buf, size_t count)
+{
+	int ret = safe_read(fd, buf, count);
+	if (ret < 0)
+		return ret;
+	if (ret != count)
+		return -EDOM;
 	return 0;
 }
  
@@ -45,23 +69,33 @@ ssize_t safe_write(int fd, const void *buf, size_t count)
 ssize_t safe_pread(int fd, void *buf, size_t count, off_t offset)
 {
 	int r;
+	int cnt = 0;
+	char *b = (char*)buf;
 
-	while (count > 0) {
-		r = pread(fd, buf, count, offset);
+	while (cnt < count) {
+		r = pread(fd, b + cnt, count - cnt, offset + cnt);
 		if (r <= 0) {
 			if (r == 0) {
 				// EOF
-				return -EDOM;
+				return cnt;
 			}
 			if (errno == EINTR)
 				continue;
 			return -errno;
 		}
 
-		count -= r;
-		buf = (char *)buf + r;
-		offset += r;
+		cnt += r;
 	}
+	return cnt;
+}
+
+ssize_t safe_pread_exact(int fd, void *buf, size_t count, off_t offset)
+{
+	int ret = safe_pread(fd, buf, count, offset);
+	if (ret < 0)
+		return ret;
+	if (ret != count)
+		return -EDOM;
 	return 0;
 }
 
--- src/common/safe_io.h
+++ src/common/safe_io.h
@@ -9,8 +9,10 @@
 
 extern "C" {
   ssize_t safe_read(int fd, void *buf, size_t count);
+  ssize_t safe_read_exact(int fd, void *buf, size_t count);
   ssize_t safe_write(int fd, const void *buf, size_t count);
   ssize_t safe_pread(int fd, void *buf, size_t count, off_t offset);
+  ssize_t safe_pread_exact(int fd, void *buf, size_t count, off_t offset);
   ssize_t safe_pwrite(int fd, const void *buf, size_t count, off_t offset);
 }
 
