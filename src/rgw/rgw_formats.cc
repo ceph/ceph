543b9864d16b7fbd95b9ae6203a86e0ed520ca7c
Yehuda Sadeh <yehuda@hq.newdream.net>
2011-02-16 11:14:00 -0800

    rgw: add missing files

---
+++ src/rgw/rgw_formats.cc
@@ -0,0 +1,164 @@
+#include "rgw_common.h"
+#include "rgw_formats.h"
+
+/* Plain */
+
+void RGWFormatter_Plain::open_obj_section(const char *name)
+{
+}
+
+void RGWFormatter_Plain::open_array_section(const char *name)
+{
+}
+
+void RGWFormatter_Plain::close_section(const char *name)
+{
+}
+
+void RGWFormatter_Plain::dump_value_int(const char *name, const char *fmt, ...)
+{
+#define LARGE_SIZE 8192
+  char buf[LARGE_SIZE];
+  va_list ap;
+
+  va_start(ap, fmt);
+  int n = vsnprintf(buf, LARGE_SIZE, fmt, ap);
+  va_end(ap);
+  if (n >= LARGE_SIZE)
+    return;
+  CGI_PRINTF(s->fcgx->out, "%s\n", buf);
+}
+
+void RGWFormatter_Plain::dump_value_str(const char *name, const char *fmt, ...)
+{
+  char buf[LARGE_SIZE];
+  va_list ap;
+
+  va_start(ap, fmt);
+  int n = vsnprintf(buf, LARGE_SIZE, fmt, ap);
+  va_end(ap);
+  if (n >= LARGE_SIZE)
+    return;
+  CGI_PRINTF(s->fcgx->out, "%s\n", buf);
+}
+
+/* XML */
+
+void RGWFormatter_XML::formatter_init()
+{
+  indent = 0;
+}
+
+void RGWFormatter_XML::open_section(const char *name)
+{
+  CGI_PRINTF(s->fcgx->out, "<%s>", name);
+  ++indent;
+}
+
+void RGWFormatter_XML::close_section(const char *name)
+{
+  --indent;
+  CGI_PRINTF(s->fcgx->out, "</%s>", name);
+}
+
+void RGWFormatter_XML::dump_value_int(const char *name, const char *fmt, ...)
+{
+#define LARGE_SIZE 8192
+  char buf[LARGE_SIZE];
+  va_list ap;
+
+  va_start(ap, fmt);
+  int n = vsnprintf(buf, LARGE_SIZE, fmt, ap);
+  va_end(ap);
+  if (n >= LARGE_SIZE)
+    return;
+  CGI_PRINTF(s->fcgx->out, "<%s>%s</%s>", name, buf, name);
+}
+
+void RGWFormatter_XML::dump_value_str(const char *name, const char *fmt, ...)
+{
+  char buf[LARGE_SIZE];
+  va_list ap;
+
+  va_start(ap, fmt);
+  int n = vsnprintf(buf, LARGE_SIZE, fmt, ap);
+  va_end(ap);
+  if (n >= LARGE_SIZE)
+    return;
+  CGI_PRINTF(s->fcgx->out, "<%s>%s</%s>", name, buf, name);
+}
+
+/* JSON */
+
+void RGWFormatter_JSON::formatter_init()
+{
+  stack.clear();
+}
+
+void RGWFormatter_JSON::open_section(bool is_array)
+{
+  if (stack.size()) {
+    struct json_stack_entry& entry = stack.back();
+    CGI_PRINTF(s->fcgx->out, "%s\n", (entry.size ? "," : ""));
+    entry.size++;
+  }
+  CGI_PRINTF(s->fcgx->out, "%c", (is_array ? '[' : '{'));
+
+  struct json_stack_entry new_entry;
+  new_entry.is_array = is_array;
+  new_entry.size = 0;
+  stack.push_back(new_entry);
+}
+
+void RGWFormatter_JSON::open_obj_section(const char *name)
+{
+  open_section(false);
+}
+
+void RGWFormatter_JSON::open_array_section(const char *name)
+{
+  open_section(true);
+}
+
+void RGWFormatter_JSON::close_section(const char *name)
+{
+  struct json_stack_entry& entry = stack.back();
+
+  CGI_PRINTF(s->fcgx->out, "%c", (entry.is_array ? ']' : '}'));
+
+  stack.pop_back();
+}
+
+void RGWFormatter_JSON::dump_value_int(const char *name, const char *fmt, ...)
+{
+#define LARGE_SIZE 8192
+  char buf[LARGE_SIZE];
+  va_list ap;
+
+  struct json_stack_entry& entry = stack.back();
+
+  va_start(ap, fmt);
+  int n = vsnprintf(buf, LARGE_SIZE, fmt, ap);
+  va_end(ap);
+  if (n >= LARGE_SIZE)
+    return;
+  CGI_PRINTF(s->fcgx->out, "%s\"%s\":%s", (entry.size ? ", " : ""), name, buf);
+  entry.size++;
+}
+
+void RGWFormatter_JSON::dump_value_str(const char *name, const char *fmt, ...)
+{
+  char buf[LARGE_SIZE];
+  va_list ap;
+
+  struct json_stack_entry& entry = stack.back();
+
+  va_start(ap, fmt);
+  int n = vsnprintf(buf, LARGE_SIZE, fmt, ap);
+  va_end(ap);
+  if (n >= LARGE_SIZE)
+    return;
+  CGI_PRINTF(s->fcgx->out, "%s\"%s\":\"%s\"", (entry.size ? ", " : ""), name, buf);
+  entry.size++;
+}
+
