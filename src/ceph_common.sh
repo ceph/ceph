29b643910462f1e5d04abb35b6c2853db5be7cc4
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-02-22 08:48:53 -0800

    ceph_common.sh: remove get_val, get_val_bool
    get_val and get_val_bool are unused.
    
    Signed-off-by: Colin McCabe <colin.mccabe@dreamhost.com>

--- src/ceph_common.sh
+++ src/ceph_common.sh
@@ -140,25 +140,6 @@ get_name_list() {
     done
 }
 
-
-get_val() {
-  [ -n "$2" ] && export $1=$2 || export $1=`$CCONF -c $conf "$3" "$4" "$5"`
-}
-
-get_val_bool() {
-  if [ -n "$2" ]; then
-	export $1=$2
-  else
-	tmp=`$CCONF "$3" "$4" "$5"`
-	export $1=$5
-
-	[ "$tmp" = "0" ] && export $1=0
-	[ "$tmp" = "false" ] && export $1=0
-	[ "$tmp" = "1" ] && export $1=1
-	[ "$tmp" = "true" ] && export $1=1
-  fi
-}
-
 get_conf() {
 	var=$1
 	def=$2
