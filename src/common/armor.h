73671c84ccd8e4b2ecb59a4196b16475b7b89254
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2011-03-29 09:21:09 -0700

    common: Make armor.h safe to use from C.
    mount.ceph needs to base64-decode the secrets, so we can get rid of
    the kernel-side base64 decode, but it doesn't need all of common lib.
    And it is written in C.
    
    Signed-off-by: Tommi Virtanen <tommi.virtanen@dreamhost.com>

--- src/common/armor.h
+++ src/common/armor.h
@@ -1,11 +1,16 @@
 #ifndef CEPH_ARMOR_H
 #define CEPH_ARMOR_H
 
+#ifdef __cplusplus
 extern "C" {
+#endif
+
 int ceph_armor(char *dst, const char *dst_end,
 	       const char *src, const char *end);
 int ceph_unarmor(char *dst, const char *dst_end,
 		 const char *src, const char *end);
+#ifdef __cplusplus
 }
+#endif
 
 #endif
