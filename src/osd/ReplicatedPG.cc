ceph/ceph

commit f842bcf893e42a4cefda07d0bbf86303b17ddcd7

osd: simplify stat
We don't actually need to stat the object (again) since we have the size
(and mtime) in the object_info_t.  Keep the existing stat in there, tho,
and assert the answer matches for the time being, since I saw behavior
that suggested they were out of sync.  See #933.

Signed-off-by: Sage Weil <sage@newdream.net>

src/osd/ReplicatedPG.cc
@@ -1198,13 +1198,18 @@ int ReplicatedPG::do_osd_ops(OpContext *ctx, vector<OSDOp>& ops,
 
     case CEPH_OSD_OP_STAT:
       {
-	struct stat st;
-	memset(&st, 0, sizeof(st));
-	result = osd->store->stat(coll, soid, &st);
-	if (result >= 0) {
-	  uint64_t size = st.st_size;
-	  ::encode(size, odata);
-	  ::encode(oi.mtime, odata);
+	::encode(oi.size, odata);
+	::encode(oi.mtime, odata);
+	dout(10) << "stat oi has " << oi.size << " " << oi.mtime << dendl;
+	if (1) {  // REMOVE ME LATER!
+	  struct stat st;
+	  memset(&st, 0, sizeof(st));
+	  result = osd->store->stat(coll, soid, &st);
+	  if (result || st.st_size != oi.size) {
+	    osd->clog.error() << info.pgid << " " << soid << " oi.size " << oi.size
+			      << " but stat got " << result << " size " << st.st_size << "\n";
+	    assert(0 == "oi disagrees with stat");
+	  }
 	}
 	info.stats.num_rd++;
       }

commit 9599b66fba03e1737f84ff6b5de162d8f2ada466

ReplicatedPG: tolerate ENOENT when statting.
This got broken in commit f842bcf893e42a4cefda07d0bbf86303b17ddcd7
so that on any MDS restart the OSD would die. Bad OSD! Bad!

Signed-off-by: Greg Farnum <gregory.farnum@dreamhost.com>

src/osd/ReplicatedPG.cc
@@ -1205,10 +1205,11 @@ int ReplicatedPG::do_osd_ops(OpContext *ctx, vector<OSDOp>& ops,
 	  struct stat st;
 	  memset(&st, 0, sizeof(st));
 	  result = osd->store->stat(coll, soid, &st);
-	  if (result || (uint64_t)st.st_size != oi.size) {
+	  if ((result && result != -ENOENT) ||
+	      (uint64_t)st.st_size != oi.size) {
 	    osd->clog.error() << info.pgid << " " << soid << " oi.size " << oi.size
 			      << " but stat got " << result << " size " << st.st_size << "\n";
-	    assert(0 == "oi disagrees with stat");
+	    assert(0 == "oi disagrees with stat, or error code on stat");
 	  }
 	}
 	info.stats.num_rd++;