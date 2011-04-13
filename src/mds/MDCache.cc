ceph/ceph

commit a01fba175b646f67446b8cac4a20c3fbb306043b

mds: set dir_auth between EImport{Start,Finish}
This ensures that try_trim_non_auth_subtree() doesn't throw out a subtree
we're in the midst of importing (during journal replay).

Signed-off-by: Sage Weil <sage@newdream.net>

src/mds/MDCache.cc
@@ -2814,7 +2814,7 @@ void MDCache::disambiguate_imports()
 void MDCache::add_ambiguous_import(dirfrag_t base, vector<dirfrag_t>& bounds) 
 {
   assert(my_ambiguous_imports.count(base) == 0);
-  my_ambiguous_imports[base].swap( bounds );
+  my_ambiguous_imports[base].swap(bounds);
 }
 
 

src/mds/MDCache.h
@@ -696,6 +696,10 @@ public:
   bool have_ambiguous_import(dirfrag_t base) {
     return my_ambiguous_imports.count(base);
   }
+  void get_ambiguous_import_bounds(dirfrag_t base, vector<dirfrag_t>& bounds) {
+    assert(my_ambiguous_imports.count(base));
+    bounds = my_ambiguous_imports[base];
+  }
   void cancel_ambiguous_import(dirfrag_t dirino);
   void finish_ambiguous_import(dirfrag_t dirino);
   void resolve_start();

src/mds/journal.cc
@@ -1084,6 +1084,10 @@ void EImportStart::replay(MDS *mds)
   // put in ambiguous import list
   mds->mdcache->add_ambiguous_import(base, bounds);
 
+  // set auth partially to us so we don't trim it
+  CDir *dir = mds->mdcache->get_dirfrag(base);
+  mds->mdcache->adjust_bounded_subtree_auth(dir, bounds, pair<int,int>(mds->get_nodeid(), mds->get_nodeid()));
+
   // open client sessions?
   if (mds->sessionmap.version >= cmapv) {
     dout(10) << "EImportStart.replay sessionmap " << mds->sessionmap.version 
@@ -1110,8 +1114,14 @@ void EImportFinish::replay(MDS *mds)
     dout(10) << "EImportFinish.replay " << base << " success=" << success << dendl;
     if (success) 
       mds->mdcache->finish_ambiguous_import(base);
-    else
+    else {
+      CDir *dir = mds->mdcache->get_dirfrag(base);
+      vector<dirfrag_t> bounds;
+      mds->mdcache->get_ambiguous_import_bounds(base, bounds);
+      mds->mdcache->adjust_bounded_subtree_auth(dir, bounds, pair<int,int>(CDIR_AUTH_UNKNOWN, CDIR_AUTH_UNKNOWN));
       mds->mdcache->cancel_ambiguous_import(base);
+      mds->mdcache->try_trim_non_auth_subtree(dir);
+    }
   } else {
     dout(10) << "EImportFinish.replay " << base << " success=" << success
 	     << ", predates my subtree_map start point, ignoring" 

commit c9ce4cf95d78ca06d403e52bcfa0e0aad3c46f9c

mds: fix resolve
This was broken by a01fba175b646f6 when an ambiguous import was changed
from CDIR_AUTH_UNKNOWN to <whoami,whoami> and disambiguate_imports wasn't
updated accordingly.  The result was inconsistent results for subtree
ownership on different nodes.

This updates disambiguate_imports to match that EImportStart::replay
change.

Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

src/mds/MDCache.cc
@@ -2320,6 +2320,9 @@ void MDCache::send_resolve_now(int who)
     // only our subtrees
     if (dir->authority().first != mds->get_nodeid()) 
       continue;
+
+    if (mds->is_resolve() && my_ambiguous_imports.count(dir->dirfrag()))
+      continue;  // we'll add it below
     
     if (migrator->is_importing(dir->dirfrag())) {
       // ambiguous (mid-import)
@@ -2331,25 +2334,31 @@ void MDCache::send_resolve_now(int who)
       for (set<CDir*>::iterator p = bounds.begin(); p != bounds.end(); ++p)
 	dfls.push_back((*p)->dirfrag());
       m->add_ambiguous_import(dir->dirfrag(), dfls);
+      dout(10) << " ambig " << dir->dirfrag() << " " << dfls << dendl;
     } else {
       // not ambiguous.
       m->add_subtree(dir->dirfrag());
       
       // bounds too
+      vector<dirfrag_t> dfls;
       for (set<CDir*>::iterator q = subtrees[dir].begin();
 	   q != subtrees[dir].end();
 	   ++q) {
 	CDir *bound = *q;
 	m->add_subtree_bound(dir->dirfrag(), bound->dirfrag());
+	dfls.push_back(bound->dirfrag());
       }
+      dout(10) << " claim " << dir->dirfrag() << " " << dfls << dendl;
     }
   }
 
   // ambiguous
   for (map<dirfrag_t, vector<dirfrag_t> >::iterator p = my_ambiguous_imports.begin();
        p != my_ambiguous_imports.end();
-       ++p) 
+       ++p) {
     m->add_ambiguous_import(p->first, p->second);
+    dout(10) << " ambig " << p->first << " " << p->second << dendl;
+  }
   
 
   // list prepare requests lacking a commit
@@ -2374,7 +2383,6 @@ void MDCache::send_resolve_now(int who)
     dout(10) << " will need resolve ack from mds" << who << dendl;
     need_resolve_ack.insert(who);
   }
-
 
   // send
   mds->send_message_mds(m, who);
@@ -2767,8 +2775,7 @@ void MDCache::disambiguate_imports()
       CDir *dir = get_force_dirfrag(q->first);
       if (!dir) continue;
       
-      if (dir->authority().first == CDIR_AUTH_UNKNOWN ||   // if i am resolving
-	  dir->is_ambiguous_auth()) {                      // if i am a surviving bystander
+      if (dir->is_ambiguous_auth()) {     // works for me_ambig or if i am a surviving bystander
 	dout(10) << "  mds" << who << " did import " << *dir << dendl;
 	adjust_bounded_subtree_auth(dir, q->second, who);
 	try_subtree_merge(dir);
@@ -2780,18 +2787,19 @@ void MDCache::disambiguate_imports()
   other_ambiguous_imports.clear();
 
   // my ambiguous imports
+  pair<int,int> me_ambig(mds->whoami, mds->whoami);
   while (!my_ambiguous_imports.empty()) {
     map<dirfrag_t, vector<dirfrag_t> >::iterator q = my_ambiguous_imports.begin();
 
     CDir *dir = get_dirfrag(q->first);
     if (!dir) continue;
     
-    if (dir->authority().first != CDIR_AUTH_UNKNOWN) {
+    if (dir->authority() != me_ambig) {
       dout(10) << "ambiguous import auth known, must not be me " << *dir << dendl;
       cancel_ambiguous_import(q->first);
       mds->mdlog->start_submit_entry(new EImportFinish(dir, false));
     } else {
-      dout(10) << "ambiguous import auth unknown, must be me " << *dir << dendl;
+      dout(10) << "ambiguous import auth unclaimed, must be me " << *dir << dendl;
       finish_ambiguous_import(q->first);
       mds->mdlog->start_submit_entry(new EImportFinish(dir, true));
     }