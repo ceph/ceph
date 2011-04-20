3d97c33bea5144cbe5ed1b40704f74fff1ec0fb1
Greg Farnum <gregory.farnum@dreamhost.com>
2011-04-20 13:54:56 -0700

    PGMon: use the config observer framework
    We now register observers on mon_osd_full_ratio and nearfull_ratio
    to update the OSD full/nearfull lists.
    
    Signed-off-by: Greg Farnum <gregory.farnum@dreamhost.com>

--- src/mon/PGMonitor.h
+++ src/mon/PGMonitor.h
@@ -29,6 +29,7 @@ using namespace std;
 #include "include/types.h"
 #include "include/utime.h"
 #include "msg/Messenger.h"
+#include "common/config.h"
 
 class MPGStats;
 class MPGStatsAck;
@@ -59,6 +60,14 @@ private:
   bool prepare_pg_stats(MPGStats *stats);
   void _updated_stats(MPGStats *req, MPGStatsAck *ack);
 
+  void update_full_ratios(float full_ratio, int nearfull_ratio) {
+    if (full_ratio != 0)
+      pending_inc.full_ratio = full_ratio;
+    if (nearfull_ratio != 0)
+      pending_inc.nearfull_ratio = nearfull_ratio;
+    propose_pending();
+  }
+
   struct C_Stats : public Context {
     PGMonitor *pgmon;
     MPGStats *req;
@@ -86,8 +95,35 @@ private:
   void send_pg_creates();
 
  public:
-  PGMonitor(Monitor *mn, Paxos *p) : PaxosService(mn, p) { }
-  
+  class RatioMonitor : public md_config_obs_t {
+    PGMonitor *mon;
+  public:
+    RatioMonitor(PGMonitor *pgmon) : mon(pgmon) {}
+    virtual ~RatioMonitor() {}
+    virtual const char **get_tracked_conf_keys() const {
+      static const char *KEYS[] = { "mon_osd_full_ratio",
+                                    "mon_osd_nearfull_ratio", NULL };
+      return KEYS;
+    }
+    virtual void handle_conf_change(const md_config_t *conf,
+                                    const std::set<std::string>& changed) {
+      mon->update_full_ratios(((float)conf->mon_osd_full_ratio) / 100,
+                              ((float)conf->mon_osd_nearfull_ratio) / 100);
+    }
+  };
+
+  RatioMonitor *ratio_monitor;
+  friend class RatioMonitor;
+
+  PGMonitor(Monitor *mn, Paxos *p) : PaxosService(mn, p) {
+    ratio_monitor = new RatioMonitor(this);
+    g_conf.add_observer(ratio_monitor);
+  }
+
+  ~PGMonitor() {
+    delete ratio_monitor;
+  }
+
   virtual void on_election_start();
 
   void tick();  // check state, take actions
@@ -95,6 +131,7 @@ private:
   void check_osd_map(epoch_t epoch);
 
   enum health_status_t get_health(std::ostream &ss) const;
+
 };
 
 #endif
