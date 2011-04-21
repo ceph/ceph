66b84fa7863ffc132f553fba62b529cbf6a34cf8
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-04-20 17:29:23 -0700

    hadoop: whitespace fixes
    Signed-off-by: Colin McCabe <colin.mccabe@dreamhost.com>

--- src/client/hadoop/ceph/CephOutputStream.java
+++ src/client/hadoop/ceph/CephOutputStream.java
@@ -1,4 +1,5 @@
 // -*- mode:Java; tab-width:2; c-basic-offset:2; indent-tabs-mode:t -*- 
+
 /**
  *
  * Licensed under the Apache License, Version 2.0
@@ -20,11 +21,13 @@
 
 package org.apache.hadoop.fs.ceph;
 
+
 import java.io.IOException;
 import java.io.OutputStream;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.util.Progressable;
+
 
 /**
  * <p>
@@ -35,12 +38,12 @@ public class CephOutputStream extends OutputStream {
 
   private boolean closed;
 
-	private CephFS ceph;
+  private CephFS ceph;
 
   private int fileHandle;
 
-	private byte[] buffer;
-	private int bufUsed = 0;
+  private byte[] buffer;
+  private int bufUsed = 0;
 
   /**
    * Construct the CephOutputStream.
@@ -48,21 +51,24 @@ public class CephOutputStream extends OutputStream {
    * @param fh The Ceph filehandle to connect to.
    */
   public CephOutputStream(Configuration conf, CephFS cephfs,
-													int fh, int bufferSize) {
-		ceph = cephfs;
+      int fh, int bufferSize) {
+    ceph = cephfs;
     fileHandle = fh;
     closed = false;
-		buffer = new byte[bufferSize];
+    buffer = new byte[bufferSize];
   }
 
-  /**Ceph likes things to be closed before it shuts down,
+  /** Ceph likes things to be closed before it shuts down,
    *so closing the IOStream stuff voluntarily is good
    */
-  protected void finalize () throws Throwable {
+  protected void finalize() throws Throwable {
     try {
-      if (!closed) close();
+      if (!closed) {
+        close();
+      }
+    } finally {
+      super.finalize();
     }
-    finally { super.finalize();}
   }
 
   /**
@@ -80,20 +86,23 @@ public class CephOutputStream extends OutputStream {
    * write fails.
    */
   @Override
-	public synchronized void write(int b) throws IOException {
-      ceph.debug("CephOutputStream.write: writing a single byte to fd "
-								 + fileHandle, ceph.TRACE);
+  public synchronized void write(int b) throws IOException {
+    ceph.debug(
+        "CephOutputStream.write: writing a single byte to fd " + fileHandle,
+        ceph.TRACE);
 
-      if (closed) {
-				throw new IOException("CephOutputStream.write: cannot write " + 
-															"a byte to fd " + fileHandle + ": stream closed");
-      }
-      // Stick the byte in a buffer and write it
-      byte buf[] = new byte[1];
-      buf[0] = (byte) b;    
-      write(buf, 0, 1);
-      return;
+    if (closed) {
+      throw new IOException(
+          "CephOutputStream.write: cannot write " + "a byte to fd " + fileHandle
+          + ": stream closed");
     }
+    // Stick the byte in a buffer and write it
+    byte buf[] = new byte[1];
+
+    buf[0] = (byte) b;    
+    write(buf, 0, 1);
+    return;
+  }
 
   /**
    * Write a byte buffer into the Ceph file.
@@ -101,102 +110,110 @@ public class CephOutputStream extends OutputStream {
    * @param off the position in the file to start writing at.
    * @param len The number of bytes to actually write.
    * @throws IOException if you have closed the CephOutputStream, or
-	 * if buf is null or off + len > buf.length, or
-	 * if the write fails due to a Ceph error.
+   * if buf is null or off + len > buf.length, or
+   * if the write fails due to a Ceph error.
    */
   @Override
-	public synchronized void write(byte buf[], int off, int len) throws IOException {
-		ceph.debug("CephOutputStream.write: writing " + len + 
-							 " bytes to fd " + fileHandle, ceph.TRACE);
-		// make sure stream is open
-		if (closed) {
-			throw new IOException("CephOutputStream.write: cannot write " + len + 
-														"bytes to fd " + fileHandle + ": stream closed");
-		}
+  public synchronized void write(byte buf[], int off, int len) throws IOException {
+    ceph.debug(
+        "CephOutputStream.write: writing " + len + " bytes to fd " + fileHandle,
+        ceph.TRACE);
+    // make sure stream is open
+    if (closed) {
+      throw new IOException(
+          "CephOutputStream.write: cannot write " + len + "bytes to fd "
+          + fileHandle + ": stream closed");
+    }
 		
-		int result;
-		int write;
-		while (len>0) {
-			write = Math.min(len, buffer.length - bufUsed);
-			try {
-				System.arraycopy(buf, off, buffer, bufUsed, write);
-			}
-			catch (IndexOutOfBoundsException ie) {
-				throw new IOException("CephOutputStream.write: Indices out of bounds: "
-															+ "write length is " + len
-															+ ", buffer offset is " + off
-															+ ", and buffer size is " + buf.length);
-			}
-			catch (ArrayStoreException ae) {
-				throw new IOException("Uh-oh, CephOutputStream failed to do an array"
-															+ " copy due to type mismatch...");
-			}
-			catch (NullPointerException ne) {
-				throw new IOException("CephOutputStream.write: cannot write "
-															+ len + "bytes to fd " + fileHandle
-															+ ": buffer is null");
-			}
-			bufUsed += write;
-			len -= write;
-			off += write;
-			if (bufUsed == buffer.length) {
-				result = ceph.ceph_write(fileHandle, buffer, 0, bufUsed);
-				if (result < 0)
-					throw new IOException("CephOutputStream.write: Buffered write of "
-																+ bufUsed + " bytes failed!");
-				if (result != bufUsed)
-					throw new IOException("CephOutputStream.write: Wrote only "
-																+ result + " bytes of " + bufUsed
-																+ " in buffer! Data may be lost or written"
-																+ " twice to Ceph!");
-				bufUsed = 0;
-			}
+    int result;
+    int write;
 
-		}
-		return; 
-	}
+    while (len > 0) {
+      write = Math.min(len, buffer.length - bufUsed);
+      try {
+        System.arraycopy(buf, off, buffer, bufUsed, write);
+      } catch (IndexOutOfBoundsException ie) {
+        throw new IOException(
+            "CephOutputStream.write: Indices out of bounds: "
+                + "write length is " + len + ", buffer offset is " + off
+                + ", and buffer size is " + buf.length);
+      } catch (ArrayStoreException ae) {
+        throw new IOException(
+            "Uh-oh, CephOutputStream failed to do an array"
+                + " copy due to type mismatch...");
+      } catch (NullPointerException ne) {
+        throw new IOException(
+            "CephOutputStream.write: cannot write " + len + "bytes to fd "
+            + fileHandle + ": buffer is null");
+      }
+      bufUsed += write;
+      len -= write;
+      off += write;
+      if (bufUsed == buffer.length) {
+        result = ceph.ceph_write(fileHandle, buffer, 0, bufUsed);
+        if (result < 0) {
+          throw new IOException(
+              "CephOutputStream.write: Buffered write of " + bufUsed
+              + " bytes failed!");
+        }
+        if (result != bufUsed) {
+          throw new IOException(
+              "CephOutputStream.write: Wrote only " + result + " bytes of "
+              + bufUsed + " in buffer! Data may be lost or written"
+              + " twice to Ceph!");
+        }
+        bufUsed = 0;
+      }
+
+    }
+    return; 
+  }
    
   /**
    * Flush the buffered data.
    * @throws IOException if you've closed the stream or the write fails.
    */
   @Override
-	public synchronized void flush() throws IOException {
-			if (!closed) {
-				if (bufUsed == 0) return;
-				int result = ceph.ceph_write(fileHandle, buffer, 0, bufUsed);
-				if (result < 0) {
-					throw new IOException("CephOutputStream.write: Write of "
-																+ bufUsed + "bytes to fd "
-																+ fileHandle + " failed");
-				}
-				if (result != bufUsed) {
-					throw new IOException("CephOutputStream.write: Write of " + bufUsed
-																+ "bytes to fd " + fileHandle
-																+ "was incomplete:  only " + result + " of "
-																+ bufUsed + " bytes were written.");
-				}
-				bufUsed = 0;
-				return;
-			}
-	}
+  public synchronized void flush() throws IOException {
+    if (!closed) {
+      if (bufUsed == 0) {
+        return;
+      }
+      int result = ceph.ceph_write(fileHandle, buffer, 0, bufUsed);
+
+      if (result < 0) {
+        throw new IOException(
+            "CephOutputStream.write: Write of " + bufUsed + "bytes to fd "
+            + fileHandle + " failed");
+      }
+      if (result != bufUsed) {
+        throw new IOException(
+            "CephOutputStream.write: Write of " + bufUsed + "bytes to fd "
+            + fileHandle + "was incomplete:  only " + result + " of " + bufUsed
+            + " bytes were written.");
+      }
+      bufUsed = 0;
+      return;
+    }
+  }
   
   /**
    * Close the CephOutputStream.
    * @throws IOException if Ceph somehow returns an error. In current code it can't.
    */
   @Override
-	public synchronized void close() throws IOException {
-      ceph.debug("CephOutputStream.close:enter", ceph.TRACE);
-      if (!closed) {
-				flush();
-				int result = ceph.ceph_close(fileHandle);
-				if (result != 0) {
-					throw new IOException("Close failed!");
-				}
+  public synchronized void close() throws IOException {
+    ceph.debug("CephOutputStream.close:enter", ceph.TRACE);
+    if (!closed) {
+      flush();
+      int result = ceph.ceph_close(fileHandle);
+
+      if (result != 0) {
+        throw new IOException("Close failed!");
+      }
 				
-				closed = true;
-				ceph.debug("CephOutputStream.close:exit", ceph.TRACE);
-			}
-	}
+      closed = true;
+      ceph.debug("CephOutputStream.close:exit", ceph.TRACE);
+    }
+  }
 }
--- src/client/hadoop/ceph/CephTalker.java
+++ src/client/hadoop/ceph/CephTalker.java
@@ -1,4 +1,5 @@
 // -*- mode:Java; tab-width:2; c-basic-offset:2; indent-tabs-mode:t -*- 
+
 /**
  *
  * Licensed under the Apache License, Version 2.0
@@ -19,41 +20,70 @@
  */
 package org.apache.hadoop.fs.ceph;
 
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.commons.logging.Log;
 
+
 class CephTalker extends CephFS {
-	//we write a constructor so we can load the libraries
-	public CephTalker(Configuration conf, Log log) {
-		super(conf, log);
-		System.load(conf.get("fs.ceph.libDir")+"/libhadoopcephfs.so");
-		System.load(conf.get("fs.ceph.libDir")+"/libceph.so");
-	}
-	protected native boolean ceph_initializeClient(String arguments, int block_size);
-	protected native String ceph_getcwd();
-	protected native boolean ceph_setcwd(String path);
+  // we write a constructor so we can load the libraries
+  public CephTalker(Configuration conf, Log log) {
+    super(conf, log);
+    System.load(conf.get("fs.ceph.libDir") + "/libhadoopcephfs.so");
+    System.load(conf.get("fs.ceph.libDir") + "/libceph.so");
+  }
+
+  protected native boolean ceph_initializeClient(String arguments, int block_size);
+
+  protected native String ceph_getcwd();
+
+  protected native boolean ceph_setcwd(String path);
+
   protected native boolean ceph_rmdir(String path);
+
   protected native boolean ceph_unlink(String path);
+
   protected native boolean ceph_rename(String old_path, String new_path);
+
   protected native boolean ceph_exists(String path);
+
   protected native long ceph_getblocksize(String path);
+
   protected native boolean ceph_isdirectory(String path);
+
   protected native boolean ceph_isfile(String path);
+
   protected native String[] ceph_getdir(String path);
+
   protected native int ceph_mkdirs(String path, int mode);
+
   protected native int ceph_open_for_append(String path);
+
   protected native int ceph_open_for_read(String path);
+
   protected native int ceph_open_for_overwrite(String path, int mode);
+
   protected native int ceph_close(int filehandle);
+
   protected native boolean ceph_setPermission(String path, int mode);
+
   protected native boolean ceph_kill_client();
+
   protected native boolean ceph_stat(String path, CephFileSystem.Stat fill);
+
   protected native int ceph_statfs(String Path, CephFileSystem.CephStat fill);
+
   protected native int ceph_replication(String path);
+
   protected native String ceph_hosts(int fh, long offset);
+
   protected native int ceph_setTimes(String path, long mtime, long atime);
+
   protected native long ceph_getpos(int fh);
+
   protected native int ceph_write(int fh, byte[] buffer, int buffer_offset, int length);
-	protected native int ceph_read(int fh, byte[] buffer, int buffer_offset, int length);
-	protected native long ceph_seek_from_start(int fh, long pos);
+
+  protected native int ceph_read(int fh, byte[] buffer, int buffer_offset, int length);
+
+  protected native long ceph_seek_from_start(int fh, long pos);
 }
