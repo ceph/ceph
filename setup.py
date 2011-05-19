87d7192c8905b15b898395298f3d57acdb4aefc2
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2011-05-19 12:17:28 -0700

    Add setup.py, install in devel mode into virtualenv.

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+from setuptools import setup, find_packages, Extension
+
+setup(
+    name='orchestra',
+    version='0.0.1',
+    packages=find_packages(),
+
+    author='Tommi Virtanen',
+    author_email='tommi.virtanen@dreamhost.com',
+    description='Orchestration of multiple machines over SSH',
+    license='MIT',
+    keywords='ssh cluster',
+
+    install_requires=[
+        'gevent >=0.13.6',
+        'paramiko >=1.7.4',
+        'nose >=1.0.0',
+        'fudge >=1.0.3',
+        ],
+
+    )
