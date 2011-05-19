37df5b1c394d2db171eb1897735ab8fc2f313b99
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2011-05-19 10:20:32 -0700

    Refactor remote running to support more use cases.

--- orchestra/run.py
+++ orchestra/run.py
@@ -1,26 +1,115 @@
+from cStringIO import StringIO
+
 import gevent
 import pipes
+import logging
+import shutil
 
+log = logging.getLogger(__name__)
 
-def write_to_log(logger, f):
+class RemoteProcess(object):
+    __slots__ = ['stdin', 'stdout', 'stderr', '_get_exitstatus']
+    def __init__(self, stdin, stdout, stderr, get_exitstatus):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self._get_exitstatus = get_exitstatus
+
+    @property
+    def exitstatus(self):
+        """
+        Wait for exit and return exit status.
+
+        Will return None on signals and connection loss.
+
+        This will likely block until you've closed stdin and consumed
+        stdout and stderr.
+        """
+        status = self._get_exitstatus()
+        # -1 on connection loss *and* signals; map to more pythonic None
+        if status == -1:
+            status = None
+        return status
+
+def execute(client, args):
+    """
+    Execute a command remotely.
+
+    Caller needs to handle stdin etc.
+
+    :param client: SSHConnection to run the command with
+    :param args: command to run
+    :type args: list of string
+    """
+    cmd = ' '.join(pipes.quote(a) for a in args)
+    (in_, out, err) = client.exec_command(cmd)
+    r = RemoteProcess(
+        stdin=in_,
+        stdout=out,
+        stderr=err,
+        get_exitstatus=out.channel.recv_exit_status,
+        )
+    return r
+
+def copy_to_log(f, logger, loglevel=logging.INFO):
     # i can't seem to get fudge to fake an iterable, so using this old
     # api for now
     for line in f.xreadlines():
         line = line.rstrip()
-        logger.info(line)
+        logger.log(loglevel, line)
 
+def copyfileobj_and_close(fsrc, fdst):
+    shutil.copyfileobj(fsrc, fdst)
+    fdst.close()
 
-def run_and_log(client, logger, args):
-    cmd = ' '.join(pipes.quote(a) for a in args)
-    (in_, out, err) = client.exec_command(cmd)
-    in_.close()
-    log_out = logger.getChild('out')
-    log_err = logger.getChild('err')
-    g_err = gevent.spawn(write_to_log, log_err, err)
-    write_to_log(log_out, out)
+def copy_and_close(src, fdst):
+    if src is not None:
+        if isinstance(src, basestring):
+            src = StringIO(src)
+        shutil.copyfileobj(src, fdst)
+    fdst.close()
+
+def copy_file_to(f, dst):
+    if hasattr(dst, 'log'):
+        # looks like a Logger to me; not using isinstance to make life
+        # easier for unit tests
+        handler = copy_to_log
+    else:
+        handler = copyfileobj_and_close
+    return handler(f, dst)
+
+def run(
+    client, args,
+    stdin=None, stdout=None, stderr=None,
+    logger=None,
+    ):
+    """
+    Run a command remotely.
+
+    :param client: SSHConnection to run the command with
+    :param args: command to run
+    :type args: list of string
+    :param stdin: Standard input to send; either a string, a file-like object, or None.
+    :param stdout: What to do with standard output. Either a file-like object, a `logging.Logger`, or `None` for copying to default log.
+    :param stderr: What to do with standard error. See `stdout`.
+    :param logger: If logging, write stdout/stderr to "out" and "err" children of this logger. Defaults to logger named after this module.
+    """
+    r = execute(client, args)
+
+    g_in = gevent.spawn(copy_and_close, stdin, r.stdin)
+
+    if logger is None:
+        logger = log
+
+    if stderr is None:
+        stderr = logger.getChild('err')
+    g_err = gevent.spawn(copy_file_to, r.stderr, stderr)
+
+    if stdout is None:
+        stdout = logger.getChild('out')
+    copy_file_to(r.stdout, stdout)
+
     g_err.get()
-    # -1 on connection loss *and* signals; map to more pythonic None
-    status = out.channel.recv_exit_status()
-    if status == -1:
-        status = None
-    return status
+    g_in.get()
+
+    return r.exitstatus
--- orchestra/test/test_run.py
+++ orchestra/test/test_run.py
@@ -2,37 +2,36 @@ from nose.tools import eq_ as eq
 
 import fudge
 import nose
-
-from .util import assert_raises
+import logging
 
 from .. import run
 
 
 @nose.with_setup(fudge.clear_expectations)
 @fudge.with_fakes
-def test_run_and_log_simple():
+def test_run_log_simple():
     ssh = fudge.Fake('SSHConnection')
     cmd = ssh.expects('exec_command')
     cmd.with_args("foo 'bar baz'")
-    in_ = fudge.Fake('ChannelFile')
-    out = fudge.Fake('ChannelFile')
-    err = fudge.Fake('ChannelFile')
+    in_ = fudge.Fake('ChannelFile(stdin)')
+    out = fudge.Fake('ChannelFile(stdout)')
+    err = fudge.Fake('ChannelFile(stderr)')
     cmd.returns((in_, out, err))
     in_.expects('close').with_args()
-    out_iter = out.expects('xreadlines').with_args().returns(['foo', 'bar'])
-    err_iter = err.expects('xreadlines').with_args().returns(['bad'])
+    out.expects('xreadlines').with_args().returns(['foo', 'bar'])
+    err.expects('xreadlines').with_args().returns(['bad'])
     logger = fudge.Fake('logger')
-    log_out = fudge.Fake('log_out')
-    logger.expects('getChild').with_args('out').returns(log_out)
-    log_out.expects('info').with_args('foo')
-    log_out.expects('info').with_args('bar')
     log_err = fudge.Fake('log_err')
     logger.expects('getChild').with_args('err').returns(log_err)
-    log_err.expects('info').with_args('bad')
+    log_err.expects('log').with_args(logging.INFO, 'bad')
+    log_out = fudge.Fake('log_out')
+    logger.expects('getChild').with_args('out').returns(log_out)
+    log_out.expects('log').with_args(logging.INFO, 'foo')
+    log_out.expects('log').with_args(logging.INFO, 'bar')
     channel = fudge.Fake('channel')
     out.has_attr(channel=channel)
     channel.expects('recv_exit_status').with_args().returns(42)
-    got = run.run_and_log(
+    got = run.run(
         client=ssh,
         logger=logger,
         args=['foo', 'bar baz'],
@@ -42,7 +41,7 @@ def test_run_and_log_simple():
 
 @nose.with_setup(fudge.clear_expectations)
 @fudge.with_fakes
-def test_run_and_log_crash_status():
+def test_run_crash_status():
     ssh = fudge.Fake('SSHConnection')
     cmd = ssh.expects('exec_command')
     cmd.with_args("foo")
@@ -50,13 +49,13 @@ def test_run_and_log_crash_status():
     out = fudge.Fake('ChannelFile').is_a_stub()
     err = fudge.Fake('ChannelFile').is_a_stub()
     cmd.returns((in_, out, err))
-    out_iter = out.expects('xreadlines').with_args().returns([])
-    err_iter = err.expects('xreadlines').with_args().returns([])
+    out.expects('xreadlines').with_args().returns([])
+    err.expects('xreadlines').with_args().returns([])
     logger = fudge.Fake('logger').is_a_stub()
     channel = fudge.Fake('channel')
     out.has_attr(channel=channel)
     channel.expects('recv_exit_status').with_args().returns(-1)
-    got = run.run_and_log(
+    got = run.run(
         client=ssh,
         logger=logger,
         args=['foo'],
