efee7466c57fb118ab14696c614f491ab3faa5fa
Sage Weil <sage@newdream.net>
2011-05-31 14:28:52 -0700

    objecter, osd: clonerange operation
    Add a src_oids field to MOSDOp, referenced by a new CLONERANGE osd op type
    that will clone data from one object to another.
    
    - The src_oids will need to have the same locator as the destination object
      type to ensure this operation can succeed.
    - The OSD is going to have to do extra work to ensure the src object(s)
      are readable, etc., at the time of the clone.
    
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/include/ceph_strings.cc
+++ src/include/ceph_strings.cc
@@ -44,6 +44,7 @@ const char *ceph_osd_op_name(int op)
 	case CEPH_OSD_OP_TMAPGET: return "tmapget";
 	case CEPH_OSD_OP_TMAPPUT: return "tmapput";
 	case CEPH_OSD_OP_WATCH: return "watch";
+	case CEPH_OSD_OP_CLONERANGE: return "clonerange";
 
 	case CEPH_OSD_OP_GETXATTR: return "getxattr";
 	case CEPH_OSD_OP_GETXATTRS: return "getxattrs";
