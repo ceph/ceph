dc9aaacfb2308a03d8fc5b3aa183408c2e53cfb8
Tommi Virtanen <tommi.virtanen@dreamhost.com>
2011-05-31 14:33:00 -0700

    Add a pretty wrapper on top of Paramiko and run.run.
    Most importantly right now, it knows its name, and can
    prettyprint it.

---
+++ orchestra/remote.py
@@ -0,0 +1,34 @@
+from orchestra import run
+
+class Remote(object):
+    """
+    A connection to a remote host.
+
+    This is a higher-level wrapper around Paramiko's `SSHClient`.
+    """
+
+    # for unit tests to hook into
+    _runner = run.run
+
+    def __init__(self, name, ssh, shortname=None):
+        self.name = name
+        self._shortname = shortname
+        self.ssh = ssh
+
+    @property
+    def shortname(self):
+        name = self._shortname
+        if name is None:
+            name = self.name
+        return name
+
+    def __str__(self):
+        return self.shortname
+
+    def run(self, **kwargs):
+        """
+        This calls `orchestra.run.run` with our SSH client.
+
+        TODO refactor to move run.run here?
+        """
+        return self._runner(client=self.ssh, **kwargs)
---
+++ orchestra/test/test_remote.py
@@ -0,0 +1,52 @@
+from nose.tools import eq_ as eq
+
+import fudge
+import fudge.inspector
+import nose
+
+from .. import remote
+
+
+def test_shortname():
+    r = remote.Remote(
+        name='jdoe@xyzzy.example.com',
+        shortname='xyz',
+        ssh=fudge.Fake('SSHConnection'),
+        )
+    eq(r.shortname, 'xyz')
+    eq(str(r), 'xyz')
+
+
+def test_shortname_default():
+    r = remote.Remote(
+        name='jdoe@xyzzy.example.com',
+        ssh=fudge.Fake('SSHConnection'),
+        )
+    eq(r.shortname, 'jdoe@xyzzy.example.com')
+    eq(str(r), 'jdoe@xyzzy.example.com')
+
+
+@nose.with_setup(fudge.clear_expectations)
+@fudge.with_fakes
+def test_run():
+    ssh = fudge.Fake('SSHConnection')
+    run = fudge.Fake('run')
+    args = [
+        'something',
+        'more',
+        ]
+    foo = object()
+    ret = object()
+    run.expects_call().with_args(
+        client=fudge.inspector.arg.passes_test(lambda v: v is ssh),
+        args=fudge.inspector.arg.passes_test(lambda v: v is args),
+        foo=fudge.inspector.arg.passes_test(lambda v: v is foo),
+        ).returns(ret)
+    r = remote.Remote(name='jdoe@xyzzy.example.com', ssh=ssh)
+    # monkey patch ook ook
+    r._runner = run
+    got = r.run(
+        args=args,
+        foo=foo,
+        )
+    assert got is ret
