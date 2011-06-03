8e55e1861d5a6f65595aa8ca5e222b88acf2adf7
Yehuda Sadeh <yehuda@hq.newdream.net>
2011-06-03 14:31:20 -0700

    librados: remove useless reference holding

--- src/librados.cc
+++ src/librados.cc
@@ -346,14 +346,12 @@ void librados::IoCtxImpl::complete_aio_write(AioCompletionImpl *c)
 
 void librados::IoCtxImpl::flush_aio_writes()
 {
-  get();
   aio_write_list_lock.Lock();
   tid_t seq = aio_write_seq;
   while (!aio_write_list.empty() &&
 	 aio_write_list.front()->aio_write_seq <= seq)
     aio_write_cond.Wait(aio_write_list_lock);
   aio_write_list_lock.Unlock();
-  put();
 }
 
 struct librados::ObjListCtx {
