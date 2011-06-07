2969161b9e60a009e2a3884b5a1f46ca6338b2e9
Sage Weil <sage.weil@dreamhost.com>
2011-06-07 13:34:57 -0700

    qa: add rename dir checks
    Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

--- qa/mds/rename/pri_pri.sh
+++ qa/mds/rename/pri_pri.sh
@@ -1,13 +1,21 @@
 #!/bin/sh -ex
 
+# srcdn=destdn
 touch mnt/a/file1
 touch mnt/a/file2
-touch mnt/a/file3
-touch mnt/b/file4
-
-# srcdn=destdn
 mv mnt/a/file1 mnt/a/file2
 
 # different (srcdn != destdn)
+touch mnt/a/file3
+touch mnt/b/file4
 mv mnt/a/file3 mnt/b/file4
 
+# dir, srcdn=destdn
+mkdir mnt/a/dir1
+mkdir mnt/a/dir2
+mv -T mnt/a/dir1 mnt/a/dir2
+
+# dir, different
+mkdir mnt/a/dir3
+mkdir mnt/b/dir4
+mv -T mnt/a/dir3 mnt/b/dir4
