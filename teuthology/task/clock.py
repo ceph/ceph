7fe89b795f048074343beea4e3975250eb2813da
Sage Weil <sage@newdream.net>
2011-06-16 14:37:52 -0700

    add clock task to sync clocks

---
+++ teuthology/task/clock.py
@@ -0,0 +1,47 @@
+import contextlib
+import logging
+
+from orchestra import run
+
+log = logging.getLogger(__name__)
+
+def task(ctx, config):
+    """
+    Sync or skew clock
+
+    This will initially sync the clocks.  Eventually it should let us also
+    skew by some number of seconds.
+
+    example:
+
+    tasks:
+    - clock:
+    - ceph:
+    - interactive:
+
+    to sync.
+
+    To skew, we should allow something like:
+
+    tasks:
+    - clock:
+        mon.0: -2
+        client.1: 2
+    - ceph:
+    - interactive:
+
+    """
+
+    ctx.cluster.run(
+        args=[
+            'sudo',
+            'ntpdate',
+            'clock1.dreamhost.com',
+            'clock2.dreamhost.com',
+            'clock3.dreamhost.com',
+            ],
+        )
+
+    # TODO do the skew
+    #for role, skew in config.iteritems():
+
