b5510a6641fee07231c1e010a8d0c74e4cac7bea
Sage Weil <sage@newdream.net>
2011-06-29 09:51:36 -0700

    qa: move file_layout into workunits/misc
    Signed-off-by: Sage Weil <sage@newdream.net>

--- qa/workunits/misc/file_layout.sh
+++ qa/workunits/misc/file_layout.sh
@@ -20,21 +20,21 @@ layout.stripe_count:  2
 layout.preferred_osd: -1" > file2_layout
 
 mkdir layout_test
-./cephfs layout_test show_layout
-./cephfs layout_test set_layout -u $MB -c 1 -s $MB
+cephfs layout_test show_layout
+cephfs layout_test set_layout -u $MB -c 1 -s $MB
 touch layout_test/file1
-./cephfs layout_test/file1 show_layout > temp
+cephfs layout_test/file1 show_layout > temp
 diff new_layout temp || return 1
 `echo "hello, I'm a file" > layout_test/file1`
-./cephfs layout_test/file1 show_layout > temp
+cephfs layout_test/file1 show_layout > temp
 diff new_layout temp || return 1
 touch layout_test/file2
-./cephfs layout_test/file2 show_layout > temp
+cephfs layout_test/file2 show_layout > temp
 diff new_layout temp || return 1
-./cephfs layout_test/file2 set_layout -u $MB -c 2 -s $twoMB
-./cephfs layout_test/file2 show_layout > temp
+cephfs layout_test/file2 set_layout -u $MB -c 2 -s $twoMB
+cephfs layout_test/file2 show_layout > temp
 diff file2_layout temp || return 1
 
 echo "hello, I'm a file with a custom layout" > layout_test/file2
 sync
-echo "Completed all file layout tests!"
+echo "OK"
