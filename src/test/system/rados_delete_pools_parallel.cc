e6ff13d878c6c6623ff68c1b5e40c1aa7aa44dfb
Josh Durgin <josh.durgin@dreamhost.com>
2011-07-20 15:43:31 -0700

    systest: move pool deletion into its own file
    Signed-off-by: Josh Durgin <josh.durgin@dreamhost.com>

--- src/test/system/rados_delete_pools_parallel.cc
+++ src/test/system/rados_delete_pools_parallel.cc
@@ -15,6 +15,7 @@
 #include "cross_process_sem.h"
 #include "include/rados/librados.h"
 #include "st_rados_create_pool.h"
+#include "st_rados_delete_pool.h"
 #include "st_rados_list_objects.h"
 #include "systest_runnable.h"
 #include "systest_settings.h"
@@ -47,48 +48,6 @@ static int g_num_objects = 50;
  *
  * DO NOT EXPECT      * hangs, crashes
  */
-class StRadosDeletePool : public SysTestRunnable
-{
-public:
-  StRadosDeletePool(int argc, const char **argv,
-		  CrossProcessSem *pool_setup_sem, CrossProcessSem *delete_pool_sem,
-		  const std::string &pool_name)
-    : SysTestRunnable(argc, argv),
-      m_pool_setup_sem(pool_setup_sem), m_delete_pool_sem(delete_pool_sem),
-      m_pool_name(pool_name)
-  {
-  }
-
-  ~StRadosDeletePool()
-  {
-  }
-
-  int run()
-  {
-    rados_t cl;
-    RETURN1_IF_NONZERO(rados_create(&cl, NULL));
-    rados_conf_parse_argv(cl, m_argc, m_argv);
-    RETURN1_IF_NONZERO(rados_conf_read_file(cl, NULL));
-    RETURN1_IF_NONZERO(rados_connect(cl));
-    m_pool_setup_sem->wait();
-    m_pool_setup_sem->post();
-
-    rados_ioctx_t io_ctx;
-    RETURN1_IF_NOT_VAL(-EEXIST, rados_pool_create(cl, m_pool_name.c_str()));
-    RETURN1_IF_NONZERO(rados_ioctx_create(cl, m_pool_name.c_str(), &io_ctx));
-    rados_ioctx_destroy(io_ctx);
-    rados_pool_delete(cl, m_pool_name.c_str());
-    if (m_delete_pool_sem)
-      m_delete_pool_sem->post();
-    rados_shutdown(cl);
-    return 0;
-  }
-
-private:
-  CrossProcessSem *m_pool_setup_sem;
-  CrossProcessSem *m_delete_pool_sem;
-  std::string m_pool_name;
-};
 
 const char *get_id_str()
 {
