3f9c0d01db3a975a21274f02e337bfb6085c2d51
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-08-10 15:05:02 -0700

    test/rados-api/snapshots.cc: test Rollback
    Signed-off-by: Colin McCabe <colin.mccabe@dreamhost.com>

--- src/test/rados-api/snapshots.cc
+++ src/test/rados-api/snapshots.cc
@@ -53,3 +53,24 @@ TEST(LibRadosSnapshots, SnapRemove) {
   rados_ioctx_destroy(ioctx);
   ASSERT_EQ(0, destroy_one_pool(pool_name, &cluster));
 }
+
+TEST(LibRadosSnapshots, Rollback) {
+  char buf[128];
+  rados_t cluster;
+  rados_ioctx_t ioctx;
+  std::string pool_name = get_temp_pool_name();
+  ASSERT_EQ("", create_one_pool(pool_name, &cluster));
+  rados_ioctx_create(cluster, pool_name.c_str(), &ioctx);
+  memset(buf, 0xcc, sizeof(buf));
+  ASSERT_EQ((int)sizeof(buf), rados_write(ioctx, "foo", buf, sizeof(buf), 0));
+  ASSERT_EQ(0, rados_ioctx_snap_create(ioctx, "snap1"));
+  char buf2[sizeof(buf)];
+  memset(buf2, 0xdd, sizeof(buf2));
+  ASSERT_EQ(0, rados_write_full(ioctx, "foo", buf2, sizeof(buf2), 0));
+  ASSERT_EQ(0, rados_rollback(ioctx, "foo", "snap1"));
+  char buf3[sizeof(buf)];
+  ASSERT_EQ((int)sizeof(buf3), rados_read(ioctx, "foo", buf3, sizeof(buf3), 0));
+  ASSERT_EQ(0, memcmp(buf, buf3, sizeof(buf)));
+  rados_ioctx_destroy(ioctx);
+  ASSERT_EQ(0, destroy_one_pool(pool_name, &cluster));
+}
