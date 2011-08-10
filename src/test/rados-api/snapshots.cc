3dbf9d4ec7815ba34079d1526b5d4e41b76631f1
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-08-10 14:31:10 -0700

    test/rados-api/snapshots.cc: test SnapRemove
    Signed-off-by: Colin McCabe <colin.mccabe@dreamhost.com>

--- src/test/rados-api/snapshots.cc
+++ src/test/rados-api/snapshots.cc
@@ -5,8 +5,6 @@
 #include "gtest/gtest.h"
 
 ///* snapshots */
-//int rados_ioctx_snap_create(rados_ioctx_t io, const char *snapname);
-//int rados_ioctx_snap_remove(rados_ioctx_t io, const char *snapname);
 //int rados_rollback(rados_ioctx_t io, const char *oid,
 //		   const char *snapname);
 //void rados_ioctx_snap_set_read(rados_ioctx_t io, rados_snap_t snap);
@@ -34,6 +32,24 @@ TEST(LibRadosSnapshots, SnapList) {
   rados_snap_t rid;
   ASSERT_EQ(0, rados_ioctx_snap_lookup(ioctx, "snap1", &rid));
   ASSERT_EQ(rid, snaps[0]);
+  rados_ioctx_destroy(ioctx);
+  ASSERT_EQ(0, destroy_one_pool(pool_name, &cluster));
+}
+
+TEST(LibRadosSnapshots, SnapRemove) {
+  char buf[128];
+  rados_t cluster;
+  rados_ioctx_t ioctx;
+  std::string pool_name = get_temp_pool_name();
+  ASSERT_EQ("", create_one_pool(pool_name, &cluster));
+  rados_ioctx_create(cluster, pool_name.c_str(), &ioctx);
+  memset(buf, 0xcc, sizeof(buf));
+  ASSERT_EQ((int)sizeof(buf), rados_write(ioctx, "foo", buf, sizeof(buf), 0));
+  ASSERT_EQ(0, rados_ioctx_snap_create(ioctx, "snap1"));
+  rados_snap_t rid;
+  ASSERT_EQ(0, rados_ioctx_snap_lookup(ioctx, "snap1", &rid));
+  ASSERT_EQ(0, rados_ioctx_snap_remove(ioctx, "snap1"));
+  ASSERT_EQ(-ENOENT, rados_ioctx_snap_lookup(ioctx, "snap1", &rid));
   rados_ioctx_destroy(ioctx);
   ASSERT_EQ(0, destroy_one_pool(pool_name, &cluster));
 }
