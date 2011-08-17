a311715203ba0ae6339e6591f1d5b415813d113a
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-08-17 11:08:34 -0700

    src/perfglue/heap_profiler.cc: fix snprintf
    Signed-off-by: Colin McCabe <colin.mccabe@dreamhost.com>

--- src/perfglue/heap_profiler.cc
+++ src/perfglue/heap_profiler.cc
@@ -50,7 +50,7 @@ bool ceph_heap_profiler_running()
 static void get_profile_name(char *profile_name, int profile_name_len)
 {
   char path[PATH_MAX];
-  snprintf(path, sizeof(path), g_conf->log_file.c_str());
+  snprintf(path, sizeof(path), "%s", g_conf->log_file.c_str());
   char *last_slash = rindex(path, '/');
 
   if (last_slash == NULL) {
