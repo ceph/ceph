6d292397aa26ef78dc75b50c6c186f93da1d8b31
Sage Weil <sage@newdream.net>
2011-08-23 11:23:31 -0700

    mon: remove dead 'setmap' code
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -1408,24 +1408,6 @@ bool OSDMonitor::prepare_command(MMonCommand *m)
       paxos->wait_for_commit(new Monitor::C_Command(mon, m, 0, rs, paxos->get_version()));
       return true;
     }
-    /*else if (m->cmd[1] == "setmap" && m->cmd.size() == 3) {
-      OSDMap map;
-      map.decode(m->get_data());
-      epoch_t e = atoi(m->cmd[2].c_str());
-      if (ceph_fsid_compare(&map.fsid, &mon->monmap->fsid) == 0) {
-	if (pending_inc.epoch == e) {
-	  map.set_epoch(pending_inc.epoch);  // make sure epoch is correct
-	  map.encode(pending_inc.fullmap);
-	  string rs = "set osd map";
-	  paxos->wait_for_commit(new Monitor::C_Command(mon, m, 0, rs, paxos->get_version()));
-	  return true;
-	} else
-	  ss << "next osdmap epoch " << pending_inc.epoch << " != " << e;
-      } else
-	  ss << "osdmap fsid " << map.fsid << " does not match monitor fsid " << mon->monmap->fsid;
-      err = -EINVAL;
-    }
-    */
     else if (m->cmd[1] == "setmaxosd" && m->cmd.size() > 2) {
       int newmax = atoi(m->cmd[2].c_str());
       if (newmax < osdmap.crush.get_max_devices()) {
