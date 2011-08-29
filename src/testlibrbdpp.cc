8d64fee429832e0f05f10aabea55c062fe46b654
Colin Patrick McCabe <cmccabe@alumni.cmu.edu>
2011-08-29 11:33:36 -0700

    testlibrbdpp: test librbd::Image::copy
    Signed-off-by: Colin McCabe <colin.mccabe@dreamhost.com>

--- src/testlibrbdpp.cc
+++ src/testlibrbdpp.cc
@@ -31,6 +31,7 @@
 using namespace std;
 
 #define TEST_IMAGE "testimg"
+#define TEST_IMAGE2 "testimg2"
 #define TEST_POOL "librbdtest"
 #define TEST_SNAP "testsnap"
 #define TEST_IO_SIZE 513
@@ -237,6 +238,16 @@ void test_io(librados::IoCtx& io_ctx, librbd::Image& image)
 
 }
 
+void test_rbd_copy(librados::IoCtx& io_ctx, librbd::Image& image)
+{
+  int ret;
+  ret = image.copy(io_ctx, TEST_IMAGE2);
+  if (ret < 0) {
+    fprintf(stderr, "image.copy returned %d!\n", ret);
+    abort();
+  }
+}
+
 int main(int argc, const char **argv) 
 {
   librados::Rados rados;
@@ -275,6 +286,7 @@ int main(int argc, const char **argv)
   test_ls(io_ctx, 1, TEST_IMAGE "1");
   test_delete(io_ctx, TEST_IMAGE "1");
   test_ls(io_ctx, 0);
+  test_rbd_copy(io_ctx, image);
   delete rbd;
   return 0;
 }
