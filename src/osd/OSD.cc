ceph/ceph

commit cd2716c4151f5c0996bb4d0cf08961f8da281dac

OSD: use creating_pgs[pgid].history in get_or_create_pg for new pg
If info.pgid is in creating_pgs, we should use the history from
creating_pgs.  The history passed in will be an empty history from a
creation probe in that case.

Signed-off-by: Samuel Just <samuel.just@dreamhost.com>

src/osd/OSD.cc
@@ -1251,7 +1251,7 @@ PG *OSD::get_or_create_pg(const PG::Info& info, epoch_t epoch, int from, int& cr
     *pt = new ObjectStore::Transaction;
     *pfin = new C_Contexts(g_ceph_context);
     if (create) {
-      pg = _create_lock_new_pg(info.pgid, acting, **pt, history);
+      pg = _create_lock_new_pg(info.pgid, acting, **pt, creating_pgs[info.pgid].history);
     } else {
       pg = _create_lock_pg(info.pgid, **pt);
       pg->acting.swap(acting);

commit be8c16753d628314f2971b2d4d80ce49376db66d

OSD: info.pgid gets removed from creating_pgs before _create_lock_new_pg
Fix bug introduced in cd2716c4151f5c0996bb4d0cf08961f8da281dac.

Signed-off-by: Samuel Just <samuel.just@dreamhost.com>

src/osd/OSD.cc
@@ -1234,6 +1234,7 @@ PG *OSD::get_or_create_pg(const PG::Info& info, epoch_t epoch, int from, int& cr
 	  creating_pgs[info.pgid].prior.erase(from);
 	  if (!can_create_pg(info.pgid))
 	    return NULL;
+	  history = creating_pgs[info.pgid].history;
 	  create = true;
 	} else {
 	  dout(10) << "get_or_create_pg " << info.pgid
@@ -1251,7 +1252,7 @@ PG *OSD::get_or_create_pg(const PG::Info& info, epoch_t epoch, int from, int& cr
     *pt = new ObjectStore::Transaction;
     *pfin = new C_Contexts(g_ceph_context);
     if (create) {
-      pg = _create_lock_new_pg(info.pgid, acting, **pt, creating_pgs[info.pgid].history);
+      pg = _create_lock_new_pg(info.pgid, acting, **pt, history);
     } else {
       pg = _create_lock_pg(info.pgid, **pt);
       pg->acting.swap(acting);