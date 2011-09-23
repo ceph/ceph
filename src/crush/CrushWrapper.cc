e6eeaacc57ac036db2caa8bfd1227d103fea6e3f
Sage Weil <sage.weil@dreamhost.com>
2011-09-22 20:56:32 -0700

    Revert "crush: refuse to add item >= max_devices"
    This reverts commit 58f98e7afc364d28a385adc7ef0fee2291d70c17.
    
    max_devices only affects the size of the parent arrays, which aren't allocated
    until crush_finalize().  No problems here.

--- src/crush/CrushWrapper.cc
+++ src/crush/CrushWrapper.cc
@@ -64,10 +64,7 @@ int CrushWrapper::insert_item(int item, int weight, string name,
 				map<string,string>& loc)  // typename -> bucketname
 {
   cout << "insert_item item " << item << " weight " << weight
-       << " name " << name << " loc " << loc << std::endl;
-
-  if (item >= get_max_devices())
-    return -ERANGE;
+	  << " name " << name << " loc " << loc << std::endl;
 
   if (name_exists(name.c_str())) {
     cerr << "error: device name '" << name << "' already exists as id "
