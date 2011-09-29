4b8d72b3cf93a6d80b07c256e7a77ebd5575ab87
Sage Weil <sage@newdream.net>
2011-09-29 14:09:17 -0700

    radosgw: fix log retry after bucket creation
    We were clobbering the object name for some reason.. probably cruft from
    some old change.
    
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/rgw/rgw_log.cc
+++ src/rgw/rgw_log.cc
@@ -83,19 +83,19 @@ int rgw_log_op(struct req_state *s)
   gmtime_r(&t, &bdt);
   
   char buf[entry.bucket.size() + 16];
-  sprintf(buf, "%.4d-%.2d-%.2d-%lld-%s", (bdt.tm_year+1900), (bdt.tm_mon+1), bdt.tm_mday, (long long)s->bucket.bucket_id, entry.bucket.c_str());
+  sprintf(buf, "%.4d-%.2d-%.2d-%lld-%s", (bdt.tm_year+1900), (bdt.tm_mon+1), bdt.tm_mday,
+	  (long long)s->bucket.bucket_id, entry.bucket.c_str());
   string oid(buf);
   rgw_obj obj(log_bucket, oid);
 
   int ret = rgwstore->append_async(obj, bl.length(), bl);
-
   if (ret == -ENOENT) {
     string id;
     map<std::string, bufferlist> attrs;
     ret = rgw_create_bucket(id, log_bucket.name, log_bucket, attrs);
     if (ret < 0)
       goto done;
-    obj.object = entry.bucket;
+    // retry
     ret = rgwstore->append_async(obj, bl.length(), bl);
   }
 done:
@@ -119,7 +119,8 @@ int rgw_log_intent(struct req_state *s, rgw_obj& obj, RGWIntentEvent intent)
   gmtime_r(&t, &bdt);
 
   char buf[obj.bucket.name.size() + 16];
-  sprintf(buf, "%.4d-%.2d-%.2d-%lld-%s", (bdt.tm_year+1900), (bdt.tm_mon+1), bdt.tm_mday, (long long)s->bucket.bucket_id, obj.bucket.name.c_str());
+  sprintf(buf, "%.4d-%.2d-%.2d-%lld-%s", (bdt.tm_year+1900), (bdt.tm_mon+1), bdt.tm_mday,
+	  (long long)s->bucket.bucket_id, obj.bucket.name.c_str());
   string oid(buf);
   rgw_obj log_obj(intent_log_bucket, oid);
 
