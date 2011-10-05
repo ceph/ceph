ceph/ceph

commit 9c56070bc20878e87fcb4715b0a3559dd1aaf9ff

monclient: use an async finisher
Otherwise we can deadlock if any completions call back into MonClient.

Signed-off-by: Sage Weil <sage@newdream.net>

src/mon/MonClient.cc
@@ -50,7 +50,7 @@ MonClient::MonClient(CephContext *cct_) :
   messenger(NULL),
   cur_con(NULL),
   monc_lock("MonClient::monc_lock"),
-  timer(cct_, monc_lock),
+  timer(cct_, monc_lock), finisher(cct_),
   log_client(NULL),
   hunting(true),
   want_monmap(true),
@@ -355,6 +355,7 @@ int MonClient::init()
   
   Mutex::Locker l(monc_lock);
   timer.init();
+  finisher.start();
   schedule_tick();
 
   // seed rng so we choose a different monitor each time
@@ -380,9 +381,9 @@ int MonClient::init()
 
 void MonClient::shutdown()
 {
+  finisher.stop();
   monc_lock.Lock();
   timer.shutdown();
-
   if (cur_con) {
     cur_con->put();
     cur_con = NULL;
@@ -542,7 +543,7 @@ void MonClient::_reopen_session()
 
   // throw out version check requests
   while (!version_requests.empty()) {
-    version_requests.begin()->second->context->complete(-1);  // watch out for deadlock
+    finisher.queue(version_requests.begin()->second->context, -1);
     version_requests.erase(version_requests.begin());
   }
 
@@ -792,7 +793,7 @@ void MonClient::handle_get_version_reply(MMonGetVersionReply* m)
       *req->newest = m->version;
     if (req->oldest)
       *req->oldest = m->oldest_version;
-    req->context->complete(0);
+    finisher.queue(req->context, 0);
     delete req;
   }
 }

src/mon/MonClient.h
@@ -21,6 +21,7 @@
 #include "MonMap.h"
 
 #include "common/Timer.h"
+#include "common/Finisher.h"
 
 #include "auth/AuthClientHandler.h"
 #include "auth/RotatingKeyRing.h"
@@ -62,6 +63,7 @@ private:
 
   Mutex monc_lock;
   SafeTimer timer;
+  Finisher finisher;
 
   LogClient *log_client;
 

commit b5c27aec40fb39dff033a98c7cf2f59560285468

monclient: add an initialized bool to guard shutdown.
The addition of a Finisher in 9c56070bc20878e87fcb4715b0a3559dd1aaf9ff
broke shutdown in the case where MonClient::init() was never called, so
add a guard variable to keep track.

I'm not sure this is actually the best solution (Timer guard itself,
for instance; maybe Finisher should too?), but I don't want to change
the Finisher interface without looking at it more carefully than I'm
going to right now.

Signed-off-by: Greg Farnum <gregory.farnum@dreamhost.com>

src/mon/MonClient.cc
@@ -51,6 +51,7 @@ MonClient::MonClient(CephContext *cct_) :
   cur_con(NULL),
   monc_lock("MonClient::monc_lock"),
   timer(cct_, monc_lock), finisher(cct_),
+  initialized(false),
   log_client(NULL),
   hunting(true),
   want_monmap(true),
@@ -376,12 +377,16 @@ int MonClient::init()
       ldout(cct, 0) << "WARNING: unknown auth protocol defined: " << *iter << dendl;
     }
   }
+
+  initialized = true;
   return 0;
 }
 
 void MonClient::shutdown()
 {
-  finisher.stop();
+  if (initialized) {
+    finisher.stop();
+  }
   monc_lock.Lock();
   timer.shutdown();
   if (cur_con) {

src/mon/MonClient.h
@@ -64,6 +64,7 @@ private:
   Mutex monc_lock;
   SafeTimer timer;
   Finisher finisher;
+  bool initialized;
 
   LogClient *log_client;
 