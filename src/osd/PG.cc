29899de55089e54b5a225697256ee8b06fc20799
Sage Weil <sage@newdream.net>
2011-10-21 09:14:15 -0700

    osd: PriorSet: acting/up membership implies still alive
    If the osd is in the acting or up sets, we can assume they are still alive,
    even though we don't know that for sure, because if they are not, we will
    rebuild PriorSet.
    
    Note that we have a dependency here on up_thru that we could/should rebuild
    PriorSet based on, IF we think it might change the value of the CRASHED
    flag and IF we care enough.  Right now we don't.  Marking CRASHED when we
    don't need to is conservative, and not dangerous.
    
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -4774,10 +4774,12 @@ PG::PriorSet::PriorSet(const OSDMap &osdmap,
       // does this osd appear to have survived through the end of the
       // interval?
       if (pinfo && ((pinfo->up_from <= interval.first &&
-		     pinfo->up_thru > interval.last) ||            // most recently
+		     (pinfo->up_thru > interval.last ||                             // most recently
+		      std::find(acting.begin(), acting.end(), o) != acting.end() ||
+		      std::find(up.begin(), up.end(), o) != up.end())) ||           // or still
 		    (pinfo->up_from > interval.last &&
 		     pinfo->last_clean_first <= interval.first &&
-		     pinfo->last_clean_last >= interval.last))) {  // previous osd interval
+		     pinfo->last_clean_last >= interval.last))) {                   // previous osd interval
 	dout(10) << "build_prior  prior osd." << o
 		 << " up_from " << pinfo->up_from
 		 << " and last clean interval "
