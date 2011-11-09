303e863d32c077c17cd8a027172af8204434a66b
Sage Weil <sage@newdream.net>
2011-11-09 13:37:02 -0800

    add hammer.sh
    simple script to repeat a test until it fails.  can probably do something much more sophisticated
    here, but this works.

---
+++ hammer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh -ex
+
+job=$1
+test -e $1
+
+bin/teuthology-nuke $job
+
+title() {
+	echo '\[\033]0;hammer '$job' '$N' passes\007\]'
+}
+
+N=0
+title
+while bin/teuthology $job $2 $3 $4
+do
+	date
+	N=$(($N+1))
+	echo "$job: $N passes"
+	title
+done
+echo "$job: $N passes, then failure."
