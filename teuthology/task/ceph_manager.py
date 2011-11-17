4b53288b0c8f0d9524eca49878ca9a90558ec6a4
Sage Weil <sage@newdream.net>
2011-11-17 13:52:17 -0800

    ceph_manager: %

--- teuthology/task/ceph_manager.py
+++ teuthology/task/ceph_manager.py
@@ -298,7 +298,7 @@ class CephManager:
     def get_mon_quorum(self):
         out = self.raw_cluster_cmd('quorum_status')
         j = json.loads(out)
-        self.log('quorum_status is %s', out)
+        self.log('quorum_status is %s' % out)
         return j['quorum']
 
     def wait_for_mon_quorum_size(self, size, timeout=300):
