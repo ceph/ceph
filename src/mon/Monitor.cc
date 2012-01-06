ceph/ceph

commit 435c29448a10ec343f5a2b7195d94c72de5b1a25

mon: instrument elector so you can stop participating in the quorum
Add new monitor commands "quorum exit" and "quorum enter" to use it.

Signed-off-by: Greg Farnum <gregory.farnum@dreamhost.com>

src/mon/Monitor.cc
@@ -964,6 +964,24 @@ void Monitor::handle_command(MMonCommand *m)
       else
 	ceph_heap_profiler_handle_command(m->cmd, clog);
     }
+    if (m->cmd[0] == "quorum") {
+      if (m->cmd[1] == "exit") {
+        reset();
+        start_election();
+        elector.stop_participating();
+        rs = "stopped responding to quorum, initiated new election";
+        r = 0;
+      } else if (m->cmd[1] == "enter") {
+        elector.start_participating();
+        reset();
+        start_election();
+        rs = "started responding to quorum, initiated new election";
+        r = 0;
+      }
+    } else {
+      rs = "unknown quorum subcommand; use exit or enter";
+      r = -EINVAL;
+    }
   } else 
     rs = "no command";
 

src/mon/Elector.cc
@@ -62,6 +62,10 @@ void Elector::bump_epoch(epoch_t e)
 
 void Elector::start()
 {
+  if (!participating) {
+    dout(0) << "not starting new election -- not participating" << dendl;
+    return;
+  }
   dout(5) << "start -- can i be leader?" << dendl;
   
   // start by trying to elect me
@@ -285,6 +289,10 @@ void Elector::dispatch(Message *m)
     
   case MSG_MON_ELECTION:
     {
+      if (!participating) {
+        m->put();
+        return;
+      }
       MMonElection *em = (MMonElection*)m;
 
       // assume an old message encoding would have matched
@@ -341,6 +349,10 @@ void Elector::dispatch(Message *m)
   }
 }
 
-
-
-
+void Elector::start_participating()
+{
+  if (!participating) {
+    participating = true;
+    call_election();
+  }
+}

src/mon/Elector.h
@@ -40,6 +40,8 @@ class Elector {
 
   epoch_t epoch;   // latest epoch we've seen.  odd == election, even == stable, 
 
+  bool participating;
+
   // electing me
   bool     electing_me;
   utime_t  start_stamp;
@@ -73,6 +75,7 @@ class Elector {
   Elector(Monitor *m) : mon(m),
 			       expire_event(0),
 			       epoch(0),
+			       participating(true),
 			       electing_me(false),
 			       leader_acked(-1) { }
 
@@ -87,6 +90,8 @@ class Elector {
     start();
   }
 
+  void stop_participating() { participating = false; }
+  void start_participating();
 };
 
 

commit ca8df7eade76e24c69f9e9078c2ec582841aa4b2

mon: fix misplaced else
Broken by 435c29448a10ec343f5a2b7195d94c72de5b1a25.

Signed-off-by: Sage Weil <sage@newdream.net>

src/mon/Monitor.cc
@@ -977,10 +977,10 @@ void Monitor::handle_command(MMonCommand *m)
         start_election();
         rs = "started responding to quorum, initiated new election";
         r = 0;
+      } else {
+	rs = "unknown quorum subcommand; use exit or enter";
+	r = -EINVAL;
       }
-    } else {
-      rs = "unknown quorum subcommand; use exit or enter";
-      r = -EINVAL;
     }
   } else 
     rs = "no command";