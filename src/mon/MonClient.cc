ceph/ceph

commit def36668a13459d9c0851e4d4da440a288f9a34f

addr_parsing: rename mount_resolve_dest to resolve_addrs and don't modify its argument
Signed-off-by: Josh Durgin <josh.durgin@dreamhost.com>

src/mon/MonClient.cc
@@ -86,8 +86,7 @@ int MonClient::build_initial_monmap()
     } else { //maybe they passed us a DNS-resolvable name
       char *hosts = NULL;
       char *old_addrs = new char[g_conf.mon_host.size() + 1];
-      strcpy(old_addrs, g_conf.mon_host.c_str());
-      hosts = mount_resolve_dest(old_addrs);
+      hosts = resolve_addrs(old_addrs);
       delete [] old_addrs;
       if (!hosts)
         return -EINVAL;

src/include/addr_parsing.c
@@ -39,11 +39,13 @@ int safe_cat(char **pstr, int *plen, int pos, const char *str2)
   return pos + len2;
 }
 
-char *mount_resolve_dest(char *orig_str)
+char *resolve_addrs(const char *orig_str)
 {
   char *new_str;
   char *tok, *p, *port_str;
   int len, pos;
+  char buf[strlen(orig_str) + 1];
+  strcpy(buf, orig_str);
 
   len = BUF_SIZE;
   new_str = (char *)malloc(len);
@@ -51,7 +53,7 @@ char *mount_resolve_dest(char *orig_str)
   p = new_str;
   pos = 0;
 
-  tok = strtok(orig_str, ",");
+  tok = strtok(buf, ",");
 
   while (tok) {
     struct addrinfo hint;

src/include/addr_parsing.h
@@ -19,7 +19,7 @@ int safe_cat(char **pstr, int *plen, int pos, const char *str2);
 /*
  * returns a string allocated by malloc; caller must free
  */
-char *mount_resolve_dest(char *orig_str);
+char *resolve_addrs(const char *orig_str);
 
 #ifdef __cplusplus
 }

commit 5d9896084eb572903723da5d003a9cda2f9a4ab1

monclient: fix resolve_addrs() call
This was broken in def36668a13459d9c0851e4d4da440a288f9a34f it looks like.
Passing uninitialized memory to resolve_addrs(), and needlessly
allocating a buffer.

Signed-off-by: Sage Weil <sage@newdream.net>

src/mon/MonClient.cc
@@ -120,9 +120,7 @@ int MonClient::build_initial_monmap(CephContext *cct, MonMap &monmap)
       return 0;
     } else { //maybe they passed us a DNS-resolvable name
       char *hosts = NULL;
-      char *old_addrs = new char[conf->mon_host.size() + 1];
-      hosts = resolve_addrs(old_addrs);
-      delete [] old_addrs;
+      hosts = resolve_addrs(conf->mon_host.c_str());
       if (!hosts)
         return -EINVAL;
       bool success = parse_ip_port_vec(hosts, addrs);