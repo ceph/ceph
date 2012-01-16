51e402e3c2081cf995993e3ab75ebfdc0188e23e
Sage Weil <sage@newdream.net>
2012-01-16 10:25:00 -0800

    rados: fix load-gen 'max-ops'
    This was mixed up with min/max_op_len.  And max_ops wasn't being used
    the initial object creation stage, flooding the OSDs.  Or during run().
    
    Signed-off-by: Sage Weil <sage@newdream.net>

--- qa/workunits/rados/load-gen-big.sh
+++ qa/workunits/rados/load-gen-big.sh
@@ -4,7 +4,6 @@ rados -p data load-gen \
     --num-objects 10240 \
     --min-object-size 1048576 \
     --max-object-size 25600000 \
-    --min-ops 4 \
     --max-ops 1024 \
     --max-backlog 1024 \
     --percent 50 \
--- qa/workunits/rados/load-gen-mostlyread.sh
+++ qa/workunits/rados/load-gen-mostlyread.sh
@@ -4,7 +4,6 @@ rados -p data load-gen \
     --num-objects 102400 \
     --min-object-size 1 \
     --max-object-size 1048576 \
-    --min-ops 4 \
     --max-ops 128 \
     --max-backlog 128 \
     --percent 90 \
