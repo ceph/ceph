8fb115fe2c6292387a87eaaa4f28b12c062f6850
Sage Weil <sage@newdream.net>
2012-01-16 12:39:20 -0800

    include run duration in summary.yaml

--- teuthology/run.py
+++ teuthology/run.py
@@ -69,6 +69,7 @@ def main():
     from gevent import monkey; monkey.patch_all()
     from .orchestra import monkey; monkey.patch_all()
 
+    import time
     import logging
 
     log = logging.getLogger(__name__)
@@ -144,10 +145,16 @@ def main():
 
     ctx.config['tasks'][:0] = init_tasks
 
+    start_time = time.time()
+
     from teuthology.run_tasks import run_tasks
     try:
         run_tasks(tasks=ctx.config['tasks'], ctx=ctx)
     finally:
+        end_time = time.time()
+        duration = end_time - start_time
+        ctx.summary['duration'] = duration
+        log.info("Duration was %f seconds" % duration)
         if not ctx.summary.get('success') and ctx.config.get('nuke-on-error'):
             from teuthology.parallel import parallel
             with parallel() as p:
