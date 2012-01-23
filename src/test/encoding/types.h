58988920cc67b48bd568839cc23ce9adca11ce88
Sage Weil <sage.weil@dreamhost.com>
2012-01-22 21:20:14 -0800

    ceph-dencoder: reenable generated types
    Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

--- src/test/encoding/types.h
+++ src/test/encoding/types.h
@@ -6,15 +6,15 @@ TYPE(entity_addr_t)
 #include "osd/osd_types.h"
 TYPE(osd_stat_t)
 TYPE(OSDSuperblock)
-/*TYPE(pool_snap_info_t)
+TYPE(pool_snap_info_t)
 TYPE(pg_pool_t)
 TYPE(object_stat_sum_t)
 TYPE(object_stat_collection_t)
 TYPE(pg_stat_t)
 TYPE(pool_stat_t)
 TYPE(watch_info_t)
-TYPE(object_info_t)
+//TYPE(object_info_t)
 
 #include "os/ObjectStore.h"
 TYPE(ObjectStore::Transaction)
-*/
+
