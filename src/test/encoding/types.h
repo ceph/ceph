a5366c8b87c911916d415b29c255d9768913a0b9
Sage Weil <sage.weil@dreamhost.com>
2012-01-31 22:48:30 -0800

    ceph-dencoder: add all message types
    Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

--- src/test/encoding/types.h
+++ src/test/encoding/types.h
@@ -22,3 +22,200 @@ TYPE(ObjectStore::Transaction)
 // --- messages ---
 #include "messages/MPGStats.h"
 MESSAGE(MPGStats)
+#include "messages/MAuth.h"
+MESSAGE(MAuth)
+
+#include "messages/MAuth.h"
+MESSAGE(MAuth)
+#include "messages/MAuthReply.h"
+MESSAGE(MAuthReply)
+#include "messages/MCacheExpire.h"
+MESSAGE(MCacheExpire)
+#include "messages/MClientCapRelease.h"
+MESSAGE(MClientCapRelease)
+#include "messages/MClientCaps.h"
+MESSAGE(MClientCaps)
+#include "messages/MClientLease.h"
+MESSAGE(MClientLease)
+#include "messages/MClientReconnect.h"
+MESSAGE(MClientReconnect)
+#include "messages/MClientReply.h"
+MESSAGE(MClientReply)
+#include "messages/MClientRequest.h"
+MESSAGE(MClientRequest)
+#include "messages/MClientRequestForward.h"
+MESSAGE(MClientRequestForward)
+#include "messages/MClientSession.h"
+MESSAGE(MClientSession)
+#include "messages/MClientSnap.h"
+MESSAGE(MClientSnap)
+#include "messages/MCommand.h"
+MESSAGE(MCommand)
+#include "messages/MCommandReply.h"
+MESSAGE(MCommandReply)
+#include "messages/MDentryLink.h"
+MESSAGE(MDentryLink)
+#include "messages/MDentryUnlink.h"
+MESSAGE(MDentryUnlink)
+#include "messages/MDirUpdate.h"
+MESSAGE(MDirUpdate)
+#include "messages/MDiscover.h"
+MESSAGE(MDiscover)
+#include "messages/MDiscoverReply.h"
+MESSAGE(MDiscoverReply)
+#include "messages/MExportCaps.h"
+MESSAGE(MExportCaps)
+#include "messages/MExportCapsAck.h"
+MESSAGE(MExportCapsAck)
+#include "messages/MExportDir.h"
+MESSAGE(MExportDir)
+#include "messages/MExportDirAck.h"
+MESSAGE(MExportDirAck)
+#include "messages/MExportDirCancel.h"
+MESSAGE(MExportDirCancel)
+#include "messages/MExportDirDiscover.h"
+MESSAGE(MExportDirDiscover)
+#include "messages/MExportDirDiscoverAck.h"
+MESSAGE(MExportDirDiscoverAck)
+#include "messages/MExportDirFinish.h"
+MESSAGE(MExportDirFinish)
+#include "messages/MExportDirNotify.h"
+MESSAGE(MExportDirNotify)
+#include "messages/MExportDirNotifyAck.h"
+MESSAGE(MExportDirNotifyAck)
+#include "messages/MExportDirPrep.h"
+MESSAGE(MExportDirPrep)
+#include "messages/MExportDirPrepAck.h"
+MESSAGE(MExportDirPrepAck)
+#include "messages/MForward.h"
+MESSAGE(MForward)
+#include "messages/MGetPoolStats.h"
+MESSAGE(MGetPoolStats)
+#include "messages/MGetPoolStatsReply.h"
+MESSAGE(MGetPoolStatsReply)
+#include "messages/MHeartbeat.h"
+MESSAGE(MHeartbeat)
+#include "messages/MInodeFileCaps.h"
+MESSAGE(MInodeFileCaps)
+#include "messages/MLock.h"
+MESSAGE(MLock)
+#include "messages/MLog.h"
+MESSAGE(MLog)
+#include "messages/MLogAck.h"
+MESSAGE(MLogAck)
+#include "messages/MMDSBeacon.h"
+MESSAGE(MMDSBeacon)
+#include "messages/MMDSCacheRejoin.h"
+MESSAGE(MMDSCacheRejoin)
+#include "messages/MMDSFindIno.h"
+MESSAGE(MMDSFindIno)
+#include "messages/MMDSFindInoReply.h"
+MESSAGE(MMDSFindInoReply)
+#include "messages/MMDSFragmentNotify.h"
+MESSAGE(MMDSFragmentNotify)
+#include "messages/MMDSLoadTargets.h"
+MESSAGE(MMDSLoadTargets)
+#include "messages/MMDSMap.h"
+MESSAGE(MMDSMap)
+#include "messages/MMDSResolve.h"
+MESSAGE(MMDSResolve)
+#include "messages/MMDSResolveAck.h"
+MESSAGE(MMDSResolveAck)
+#include "messages/MMDSSlaveRequest.h"
+MESSAGE(MMDSSlaveRequest)
+#include "messages/MMDSTableRequest.h"
+MESSAGE(MMDSTableRequest)
+#include "messages/MMonCommand.h"
+MESSAGE(MMonCommand)
+#include "messages/MMonCommandAck.h"
+MESSAGE(MMonCommandAck)
+#include "messages/MMonElection.h"
+MESSAGE(MMonElection)
+#include "messages/MMonGetMap.h"
+MESSAGE(MMonGetMap)
+#include "messages/MMonGetVersion.h"
+MESSAGE(MMonGetVersion)
+#include "messages/MMonGetVersionReply.h"
+MESSAGE(MMonGetVersionReply)
+#include "messages/MMonGlobalID.h"
+MESSAGE(MMonGlobalID)
+#include "messages/MMonJoin.h"
+MESSAGE(MMonJoin)
+#include "messages/MMonMap.h"
+MESSAGE(MMonMap)
+#include "messages/MMonObserve.h"
+MESSAGE(MMonObserve)
+#include "messages/MMonObserveNotify.h"
+MESSAGE(MMonObserveNotify)
+#include "messages/MMonPaxos.h"
+MESSAGE(MMonPaxos)
+#include "messages/MMonProbe.h"
+MESSAGE(MMonProbe)
+#include "messages/MMonSubscribe.h"
+MESSAGE(MMonSubscribe)
+#include "messages/MMonSubscribeAck.h"
+MESSAGE(MMonSubscribeAck)
+#include "messages/MOSDAlive.h"
+MESSAGE(MOSDAlive)
+#include "messages/MOSDBoot.h"
+MESSAGE(MOSDBoot)
+#include "messages/MOSDFailure.h"
+MESSAGE(MOSDFailure)
+#include "messages/MOSDMap.h"
+MESSAGE(MOSDMap)
+#include "messages/MOSDOp.h"
+MESSAGE(MOSDOp)
+#include "messages/MOSDOpReply.h"
+MESSAGE(MOSDOpReply)
+#include "messages/MOSDPGBackfill.h"
+MESSAGE(MOSDPGBackfill)
+#include "messages/MOSDPGCreate.h"
+MESSAGE(MOSDPGCreate)
+#include "messages/MOSDPGInfo.h"
+MESSAGE(MOSDPGInfo)
+#include "messages/MOSDPGLog.h"
+MESSAGE(MOSDPGLog)
+#include "messages/MOSDPGMissing.h"
+MESSAGE(MOSDPGMissing)
+#include "messages/MOSDPGNotify.h"
+MESSAGE(MOSDPGNotify)
+#include "messages/MOSDPGQuery.h"
+MESSAGE(MOSDPGQuery)
+#include "messages/MOSDPGRemove.h"
+MESSAGE(MOSDPGRemove)
+#include "messages/MOSDPGScan.h"
+MESSAGE(MOSDPGScan)
+#include "messages/MOSDPGTemp.h"
+MESSAGE(MOSDPGTemp)
+#include "messages/MOSDPGTrim.h"
+MESSAGE(MOSDPGTrim)
+#include "messages/MOSDPing.h"
+MESSAGE(MOSDPing)
+#include "messages/MOSDRepScrub.h"
+MESSAGE(MOSDRepScrub)
+#include "messages/MOSDScrub.h"
+MESSAGE(MOSDScrub)
+#include "messages/MOSDSubOp.h"
+MESSAGE(MOSDSubOp)
+#include "messages/MOSDSubOpReply.h"
+MESSAGE(MOSDSubOpReply)
+#include "messages/MPGStats.h"
+MESSAGE(MPGStats)
+#include "messages/MPGStatsAck.h"
+MESSAGE(MPGStatsAck)
+#include "messages/MPing.h"
+MESSAGE(MPing)
+#include "messages/MPoolOp.h"
+MESSAGE(MPoolOp)
+#include "messages/MPoolOpReply.h"
+MESSAGE(MPoolOpReply)
+#include "messages/MRemoveSnaps.h"
+MESSAGE(MRemoveSnaps)
+#include "messages/MRoute.h"
+MESSAGE(MRoute)
+#include "messages/MStatfs.h"
+MESSAGE(MStatfs)
+#include "messages/MStatfsReply.h"
+MESSAGE(MStatfsReply)
+#include "messages/MWatchNotify.h"
+MESSAGE(MWatchNotify)
