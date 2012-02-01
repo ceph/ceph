d4d1b64fbe6889f49534b18ea909242af6d044b4
Sage Weil <sage.weil@dreamhost.com>
2012-02-01 15:41:34 -0800

    import-generated.sh: fix to use ceph-dencoder syntax
    Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

--- src/test/encoding/import-generated.sh
+++ src/test/encoding/import-generated.sh
@@ -16,11 +16,11 @@ for type in `./ceph-dencoder list_types`; do
 
     [ -d "$archive/$ver/$type" ] || mkdir "$archive/$ver/$type"
 
-    num=`./ceph-dencoder type $type count`
+    num=`./ceph-dencoder type $type count_tests`
     echo "$num\t$type"
     max=$(($num - 1))
     for n in `seq 0 $max`; do
-	./ceph-dencoder type $type select $n encode export $tmp1
+	./ceph-dencoder type $type select_test $n encode export $tmp1
 	md=`md5sum $tmp1 | awk '{print $1}'`
 	echo "\t$md"
 	[ -e "$archive/$ver/$md" ] || cp $tmp1 $archive/$ver/$type/$md
