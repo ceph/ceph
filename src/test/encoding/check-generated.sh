efe77a8ee7db3e43c9b0faff38e5d4dd1dd80a7e
Sage Weil <sage.weil@dreamhost.com>
2012-02-02 12:47:58 -0800

    check-generated.sh: nicer output
    Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

--- src/test/encoding/check-generated.sh
+++ src/test/encoding/check-generated.sh
@@ -7,6 +7,8 @@ set -e
 tmp1=`mktemp /tmp/typ-XXXXXXXXX`
 tmp2=`mktemp /tmp/typ-XXXXXXXXX`
 
+failed=0
+numtests=0
 echo "checking ceph-dencoder generated test instances..."
 echo "numgen type"
 for type in `./ceph-dencoder list_types`; do
@@ -16,16 +18,27 @@ for type in `./ceph-dencoder list_types`; do
 	./ceph-dencoder type $type select_test $n dump_json > $tmp1
 	./ceph-dencoder type $type select_test $n encode decode dump_json > $tmp2
 	if ! cmp $tmp1 $tmp2; then
-	    echo "$type test $n json check failed"
-	    exit 1
+	    echo "**** $type test $n dump_json check failed ****"
+	    echo "   ceph-dencoder type $type select_test $n dump_json > $tmp1"
+	    echo "   ceph-dencoder type $type select_test $n encode decode dump_json > $tmp2"
+	    echo "   diff $tmp1 $tmp2"
+	    failed=$(($failed + 1))
 	fi
 
 	./ceph-dencoder type $type select_test $n encode export $tmp1
 	./ceph-dencoder type $type select_test $n encode decode encode export $tmp2
 	if ! cmp $tmp1 $tmp2; then
-	    echo "$type test $n binary reencode check failed"
-	    exit 1
+	    echo "**** $type test $n binary reencode check failed ****"
+	    echo "   ceph-dencoder type $type select_test $n encode export $tmp1"
+	    echo "   ceph-dencoder type $type select_test $n encode decode encode export $tmp2"
+	    echo "   cmp $tmp1 $tmp2"
+	    failed=$(($failed + 1))
 	fi
+	numtests=$(($numtests + 1))
     done
 done
-echo "ok."
+if [ $failed -gt 0 ]; then
+    echo "FAILED $failed / $numtests tests."
+    exit 1
+fi
+echo "passed $numtests tests."
