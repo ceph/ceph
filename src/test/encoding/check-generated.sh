91073a6ab4062f9aa486f1948a7155a0684454da
Sage Weil <sage.weil@dreamhost.com>
2012-02-01 16:13:19 -0800

    check-generated.sh: run on 'make check'
    Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

--- src/test/encoding/check-generated.sh
+++ src/test/encoding/check-generated.sh
@@ -7,17 +7,25 @@ set -e
 tmp1=`mktemp /tmp/typ-XXXXXXXXX`
 tmp2=`mktemp /tmp/typ-XXXXXXXXX`
 
-echo "numgen\ttype"
+echo "checking ceph-dencoder generated test instances..."
+echo "numgen type"
 for type in `./ceph-dencoder list_types`; do
     num=`./ceph-dencoder type $type count_tests`
-    echo "$num\t$type"
+    echo "$num $type"
     for n in `seq 1 $num`; do
 	./ceph-dencoder type $type select_test $n dump_json > $tmp1
 	./ceph-dencoder type $type select_test $n encode decode dump_json > $tmp2
-	cmp $tmp1 $tmp2 || exit 1
+	if ! cmp $tmp1 $tmp2; then
+	    echo "$type test $n json check failed"
+	    exit 1
+	fi
 
 	./ceph-dencoder type $type select_test $n encode export $tmp1
 	./ceph-dencoder type $type select_test $n encode decode encode export $tmp2
-	cmp $tmp1 $tmp2 || exit 1
+	if ! cmp $tmp1 $tmp2; then
+	    echo "$type test $n binary reencode check failed"
+	    exit 1
+	fi
     done
 done
+echo "ok."
