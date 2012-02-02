6e62fc48b7b7894637fac7321848f13d85ec4d50
Sage Weil <sage.weil@dreamhost.com>
2012-02-02 13:56:55 -0800

    test/encoding/readable.sh: check all version
    Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

--- src/test/encoding/readable.sh
+++ src/test/encoding/readable.sh
@@ -7,19 +7,23 @@ set -e
 tmp1=`mktemp /tmp/typ-XXXXXXXXX`
 tmp2=`mktemp /tmp/typ-XXXXXXXXX`
 
-for type in `ls $dir`
+for vdir in $dir/*
 do
-    if ./ceph-dencoder type $type 2>/dev/null; then
-	echo "type $type";
-	for f in `ls $dir/$type`; do
-	    echo "\t$dir/$type/$f"
-            ./ceph-dencoder type $type import $dir/$type/$f decode dump_json > $tmp1
-            ./ceph-dencoder type $type import $dir/$type/$f decode encode decode dump_json > $tmp2
-            cmp $tmp1 $tmp2 || exit 1
-	done
-    else
-        echo "skip $type"
-    fi
+    echo $vdir
+    for type in `ls $vdir`
+    do
+	if ./ceph-dencoder type $type 2>/dev/null; then
+	    echo "type $type";
+	    for f in `ls $vdir/$type`; do
+		echo "\t$vdir/$type/$f"
+		./ceph-dencoder type $type import $vdir/$type/$f decode dump_json > $tmp1
+		./ceph-dencoder type $type import $vdir/$type/$f decode encode decode dump_json > $tmp2
+		cmp $tmp1 $tmp2 || exit 1
+	    done
+	else
+            echo "skip $type"
+	fi
+    done
 done
 
 rm -f $tmp1 $tmp2
