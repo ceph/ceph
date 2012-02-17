6e89d9ca06dca8b3cc836098fe31362431c62488
Sage Weil <sage.weil@dreamhost.com>
2012-02-17 15:26:37 -0800

    osd: update_stats() in GetInfo state start
    This is the first stage of peering.
    
    Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -4243,6 +4243,8 @@ PG::RecoveryState::GetInfo::GetInfo(my_context ctx)
   if (pg->need_up_thru)
     pg->osd->queue_want_up_thru(pg->info.history.same_interval_since);
 
+  pg->update_stats();
+
   get_infos();
 }
 
