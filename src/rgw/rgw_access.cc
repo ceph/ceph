2b3bfd0cf1a3ee9bb908a4f4bebf875736499e7c
Yehuda Sadeh <yehuda@hq.newdream.net>
2012-03-14 15:55:54 -0700

    rgw: remove fs backend
    was broken anyway
    
    Signed-off-by: Yehuda Sadeh <yehuda@hq.newdream.net>

--- src/rgw/rgw_access.cc
+++ src/rgw/rgw_access.cc
@@ -1,14 +1,11 @@
 #include <string.h>
 #include "rgw_access.h"
-#include "rgw_fs.h"
 #include "rgw_rados.h"
 #include "rgw_cache.h"
 
 #define DOUT_SUBSYS rgw
 
-static RGWCache<RGWFS> cached_fs_provider;
 static RGWCache<RGWRados> cached_rados_provider;
-static RGWFS fs_provider;
 static RGWRados rados_provider;
 
 void RGWObjManifestPart::generate_test_instances(std::list<RGWObjManifestPart*>& o)
@@ -75,14 +72,10 @@ RGWAccess *RGWAccess::init_storage_provider(const char *type, CephContext *cct)
   if (!use_cache) {
     if (strcmp(type, "rados") == 0) {
       store = &rados_provider;
-    } else if (strcmp(type, "fs") == 0) {
-      store = &fs_provider;
     }
   } else {
     if (strcmp(type, "rados") == 0) {
       store = &cached_rados_provider;
-    } else if (strcmp(type, "fs") == 0) {
-      store = &cached_fs_provider;
     }
   }
 
