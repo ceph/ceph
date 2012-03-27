ca1f79b50e494dac799589ca79df67168ffb7235
Sage Weil <sage.weil@dreamhost.com>
2012-03-27 16:37:59 -0700

    dout: no newlines on dout_emergency
    Preserve old behavior to avoid breaking all the cli tests.
    
    Signed-off-by: Sage Weil <sage.weil@dreamhost.com>

--- src/common/dout.cc
+++ src/common/dout.cc
@@ -3,10 +3,12 @@
 
 void dout_emergency(const char * const str)
 {
-  std::cerr << str << std::endl;
+  std::cerr << str;
+  std::cerr.flush();
 }
 
 void dout_emergency(const std::string &str)
 {
-  std::cerr << str << std::endl;
+  std::cerr << str;
+  std::cerr.flush();
 }
