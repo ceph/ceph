2878865463fa74fef4ea6dcd964425baa658f46f
Sage Weil <sage@newdream.net>
2012-03-30 16:11:31 -0700

    log: dump_recent in fatal signal handler
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/global/signal_handler.cc
+++ src/global/signal_handler.cc
@@ -91,6 +91,16 @@ static void handle_fatal_signal(int signum)
   bt.print(oss);
   dout_emergency(oss.str());
 
+  // dump to log.  this uses the heap extensively, but we're better
+  // off trying than not.
+  derr << buf << std::endl;
+  bt.print(*_dout);
+  *_dout << " NOTE: a copy of the executable, or `objdump -rdS <executable>` "
+	 << "is needed to interpret this.\n"
+	 << dendl;
+
+  g_ceph_context->_log->dump_recent();
+
   reraise_fatal(signum);
 }
 
