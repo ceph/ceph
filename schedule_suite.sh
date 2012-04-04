3adf2bf97534e5bb4efd003689ae60c380e899ce
Sage Weil <sage@newdream.net>
2012-04-04 13:54:43 -0700

    schedule_suite.sh: helper to schedule a suite
    There's a bunch of stuff hardcoded in here, similar to the nightly, but
    it's a useful starting point.

---
+++ schedule_suite.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+suite=$1
+ceph=$2
+kernel=$3
+email=$4
+
+if [ -z "$email" ]; then
+    echo "usage: $0 <suite> <ceph branch> <kernel branch> <email>"
+    exit 1
+fi
+
+stamp=`date +%Y-%m-%d_%H:%M:%S`
+
+fn="/tmp/schedule.suite.$$"
+
+cat <<EOF > $fn
+kernel:
+  branch: $kernel
+nuke-on-error: true
+overrides:
+  ceph:
+    btrfs: 1
+    log-whitelist:
+    - clocks not synchronized
+    - old request
+    branch: $ceph
+tasks:
+- chef:
+EOF
+
+bin/teuthology-suite -v $fn --collections /home/sage/src/ceph-qa-suite/suites/$suite/* --email $email --timeout 21600 --name $ceph-$stamp 
+
+rm $fn
