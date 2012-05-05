04ab289f94dc88a66e8d3bf8353b656d28a6791d
Sage Weil <sage@inktank.com>
2012-05-05 13:21:37 -0700

    mon: add safety checks for 'mds rm <gid>' command
    - make sure the gid exists
    - only remove it if it's inactive (state < 0)
    
    Fixes: #2188
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/mon/MDSMonitor.cc
+++ src/mon/MDSMonitor.cc
@@ -780,13 +780,23 @@ bool MDSMonitor::prepare_command(MMonCommand *m)
     }
     else if (m->cmd[1] == "rm" && m->cmd.size() == 3) {
       uint64_t gid = atoll(m->cmd[2].c_str());
-      pending_mdsmap.mds_info.erase(gid);
-      stringstream ss;
-      ss << "removed mds gid " << gid;
-      string rs;
-      getline(ss, rs);
-      paxos->wait_for_commit(new Monitor::C_Command(mon, m, 0, rs, paxos->get_version()));
-      return true;
+      int state = pending_mdsmap.get_state_gid(gid);
+      if (state == 0) {
+	ss << "mds gid " << gid << " dne";
+	r = 0;
+      } else if (state > 0) {
+	ss << "cannot remove active mds." << pending_mdsmap.get_info_gid(gid).name
+	   << " rank " << pending_mdsmap.get_info_gid(gid).rank;
+	r = -EBUSY;
+      } else {
+	pending_mdsmap.mds_info.erase(gid);
+	stringstream ss;
+	ss << "removed mds gid " << gid;
+	string rs;
+	getline(ss, rs);
+	paxos->wait_for_commit(new Monitor::C_Command(mon, m, 0, rs, paxos->get_version()));
+	return true;
+      }
     }
     else if (m->cmd[1] == "rmfailed" && m->cmd.size() == 3) {
       int w = atoi(m->cmd[2].c_str());
