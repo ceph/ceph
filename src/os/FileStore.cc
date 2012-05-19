ceph/ceph

commit 1314a00798ed4b7ef2f2686f0195c5c53c98c2ce

filestore: do not test xattrs on fsid file
This is sufficient to break our fcntl lock, which breaks the check for
other running daemons.

Broken by f03dc34f7e2fc1707fa00339b917c0d2c93dbdd3

Fixes: #2452
Backport: dho
Signed-off-by: Sage Weil <sage@inktank.com>

src/os/FileStore.cc
@@ -1312,7 +1312,7 @@ int FileStore::_detect_fs()
   char fn[PATH_MAX];
   int x = rand();
   int y = x+1;
-  snprintf(fn, sizeof(fn), "%s/fsid", basedir.c_str());
+  snprintf(fn, sizeof(fn), "%s/whoami", basedir.c_str());
   int ret = do_setxattr(fn, "user.test", &x, sizeof(x));
   if (ret >= 0)
     ret = do_getxattr(fn, "user.test", &y, sizeof(y));

commit 3f38605d19648c3d90db1b2e5c53f9441ea28554

filestore: test xattrs on temporary file
There isn't one we can rely on existing yet.  Broken by
1314a00798ed4b7ef2f2686f0195c5c53c98c2ce while fixing #2452.

Backport: dho
Signed-off-by: Sage Weil <sage@inktank.com>

src/os/FileStore.cc
@@ -1312,7 +1312,16 @@ int FileStore::_detect_fs()
   char fn[PATH_MAX];
   int x = rand();
   int y = x+1;
-  snprintf(fn, sizeof(fn), "%s/whoami", basedir.c_str());
+
+  snprintf(fn, sizeof(fn), "%s/xattr_test", basedir.c_str());
+
+  int tmpfd = ::open(fn, O_CREAT|O_WRONLY|O_TRUNC, 0700);
+  if (tmpfd < 0) {
+    int ret = -errno;
+    derr << "_detect_fs unable to create " << fn << ": " << cpp_strerror(ret) << dendl;
+    return ret;
+  }
+
   int ret = do_setxattr(fn, "user.test", &x, sizeof(x));
   if (ret >= 0)
     ret = do_getxattr(fn, "user.test", &y, sizeof(y));
@@ -1344,6 +1353,9 @@ int FileStore::_detect_fs()
   do_removexattr(fn, "user.test3");
   do_removexattr(fn, "user.test4");
   do_removexattr(fn, "user.test5");
+
+  ::unlink(fn);
+  TEMP_FAILURE_RETRY(::close(tmpfd));
 
   int fd = ::open(basedir.c_str(), O_RDONLY);
   if (fd < 0)