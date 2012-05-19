ceph/ceph

commit f03dc34f7e2fc1707fa00339b917c0d2c93dbdd3

filestore: first lock osd mount point, next detect fs type
Fixes #2353. Problem was that there were (at least) two osd processes
that were racing for the fs detection, which triggered some errors
in the btrfs create/remove snapshot.

Signed-off-by: Yehuda Sadeh <yehuda.sadeh@dreamhost.com>

src/os/FileStore.cc
@@ -1637,35 +1637,6 @@ int FileStore::mount()
     goto done;
   }
 
-  // test for btrfs, xattrs, etc.
-  ret = _detect_fs();
-  if (ret)
-    goto done;
-
-  uint32_t version_stamp;
-  ret = version_stamp_is_valid(&version_stamp);
-  if (ret < 0) {
-    derr << "FileStore::mount : error in version_stamp_is_valid: "
-	 << cpp_strerror(ret) << dendl;
-    goto done;
-  } else if (ret == 0) {
-    if (m_filestore_update_collections) {
-      derr << "FileStore::mount : stale version stamp detected: "
-	   << version_stamp 
-	   << ". Proceeding, m_filestore_update_collections "
-	   << "is set, DO NOT USE THIS OPTION IF YOU DO NOT KNOW WHAT IT DOES."
-	   << " More details can be found on the wiki."
-	   << dendl;
-    } else {
-      ret = -EINVAL;
-      derr << "FileStore::mount : stale version stamp " << version_stamp
-	   << ". Please run the FileStore update script before starting the "
-	   << "OSD."
-	   << dendl;
-      goto done;
-    }
-  }
-
   // get fsid
   snprintf(buf, sizeof(buf), "%s/fsid", basedir.c_str());
   fsid_fd = ::open(buf, O_RDWR, 0644);
@@ -1690,6 +1661,35 @@ int FileStore::mount()
   }
 
   dout(10) << "mount fsid is " << fsid << dendl;
+
+  // test for btrfs, xattrs, etc.
+  ret = _detect_fs();
+  if (ret)
+    goto close_fsid_fd;
+
+  uint32_t version_stamp;
+  ret = version_stamp_is_valid(&version_stamp);
+  if (ret < 0) {
+    derr << "FileStore::mount : error in version_stamp_is_valid: "
+	 << cpp_strerror(ret) << dendl;
+    goto close_fsid_fd;
+  } else if (ret == 0) {
+    if (m_filestore_update_collections) {
+      derr << "FileStore::mount : stale version stamp detected: "
+	   << version_stamp 
+	   << ". Proceeding, m_filestore_update_collections "
+	   << "is set, DO NOT USE THIS OPTION IF YOU DO NOT KNOW WHAT IT DOES."
+	   << " More details can be found on the wiki."
+	   << dendl;
+    } else {
+      ret = -EINVAL;
+      derr << "FileStore::mount : stale version stamp " << version_stamp
+	   << ". Please run the FileStore update script before starting the "
+	   << "OSD."
+	   << dendl;
+      goto close_fsid_fd;
+    }
+  }
 
   // open some dir handles
   basedir_fd = ::open(basedir.c_str(), O_RDONLY);

commit 122aac8400dbf4912da6838aac60f6f5925684d5

filestore: do not test xattrs on fsid file
This is sufficient to break our fcntl lock, which breaks the check for
other running daemons.

Broken by f03dc34f7e2fc1707fa00339b917c0d2c93dbdd3

Fixes: #2452
Backport: dho
Signed-off-by: Sage Weil <sage@inktank.com>

src/os/FileStore.cc
@@ -1312,7 +1312,7 @@ int FileStore::_detect_fs()
   char fn[PATH_MAX];
   int x = rand();
   int y = x+1;
-  snprintf(fn, sizeof(fn), "%s/fsid", basedir.c_str());
+  snprintf(fn, sizeof(fn), "%s/whoami", basedir.c_str());
   int ret = do_setxattr(fn, "user.test", &x, sizeof(x));
   if (ret >= 0)
     ret = do_getxattr(fn, "user.test", &y, sizeof(y));