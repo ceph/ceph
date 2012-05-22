6153f76c6541217f8f637abb4789cf81a9f9d646
Sage Weil <sage@inktank.com>
2012-05-22 10:16:42 -0700

    monclient: move keyring requirement where it belongs
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/auth/KeyRing.cc
+++ src/auth/KeyRing.cc
@@ -38,15 +38,6 @@ int KeyRing::from_ceph_context(CephContext *cct, KeyRing *keyring)
 {
   const md_config_t *conf = cct->_conf;
 
-  AuthMethodList supported(cct,
-			   cct->_conf->auth_client_required.length() ?
-			   cct->_conf->auth_client_required : cct->_conf->auth_supported);
-
-  if (!supported.is_supported_auth(CEPH_AUTH_CEPHX)) {
-    ldout(cct, 2) << "CephX auth is not supported." << dendl;
-    return 0;
-  }
-
   int ret = 0;
   string filename;
   if (ceph_resolve_file_search(conf->keyring, filename)) {
