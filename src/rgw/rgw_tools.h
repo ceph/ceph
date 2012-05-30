767664dcb87f46458b6434b57f4b5c14ed78575f
Yehuda Sadeh <yehuda@inktank.com>
2012-05-30 15:40:39 -0700

    rgw: put_bucket_info does not override attrs
    This fixes #2487. When writing bucket info we just
    wrote the object content, and were overriding any
    attrs that object contained (that is -- corrupted
    the ACLs).
    
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/rgw/rgw_tools.h
+++ src/rgw/rgw_tools.h
@@ -8,7 +8,7 @@
 
 
 int rgw_put_obj(string& uid, rgw_bucket& bucket, string& oid, const char *data, size_t size, bool exclusive, map<string, bufferlist> *pattrs = NULL);
-int rgw_get_obj(void *ctx, rgw_bucket& bucket, string& key, bufferlist& bl);
+int rgw_get_obj(void *ctx, rgw_bucket& bucket, string& key, bufferlist& bl, map<string, bufferlist> *pattrs = NULL);
 
 int rgw_tools_init(CephContext *cct);
 const char *rgw_find_mime_by_ext(string& ext);
