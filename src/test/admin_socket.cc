88ba5025878607d2c00fbca91bc1560fa952505d
Sage Weil <sage@inktank.com>
2012-05-30 22:22:18 -0700

    admin_socket: fix unit test
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/test/admin_socket.cc
+++ src/test/admin_socket.cc
@@ -64,14 +64,16 @@ TEST(AdminSocket, SendNoOp) {
   ASSERT_EQ(true, asoct.init(get_rand_socket_path()));
   AdminSocketClient client(get_rand_socket_path());
   string version;
-  ASSERT_EQ("", client.do_request("version", &version));
+  ASSERT_EQ("", client.do_request("0", &version));
   ASSERT_EQ(CEPH_ADMIN_SOCK_VERSION, version);
   ASSERT_EQ(true, asoct.shutdown());
 }
 
 class MyTest : public AdminSocketHook {
-  bool call(std::string command, bufferlist& result) {
-    result.append("yes");
+  bool call(std::string command, std::string args, bufferlist& result) {
+    result.append(command);
+    result.append("|");
+    result.append(args);
     return true;
   }
 };
@@ -86,13 +88,15 @@ TEST(AdminSocket, RegisterCommand) {
   ASSERT_EQ(0, asoct.m_asokc->register_command("test", new MyTest(), ""));
   string result;
   ASSERT_EQ("", client.do_request("test", &result));
-  ASSERT_EQ("yes", result);
+  ASSERT_EQ("test|", result);
   ASSERT_EQ(true, asoct.shutdown());
 }
 
 class MyTest2 : public AdminSocketHook {
-  bool call(std::string command, bufferlist& result) {
-    result.append("yessir");
+  bool call(std::string command, std::string args, bufferlist& result) {
+    result.append(command);
+    result.append("|");
+    result.append(args);
     return true;
   }
 };
@@ -108,19 +112,19 @@ TEST(AdminSocket, RegisterCommandPrefixes) {
   ASSERT_EQ(0, asoct.m_asokc->register_command("test command", new MyTest2(), ""));
   string result;
   ASSERT_EQ("", client.do_request("test", &result));
-  ASSERT_EQ("yes", result);
+  ASSERT_EQ("test|", result);
   ASSERT_EQ("", client.do_request("test command", &result));
-  ASSERT_EQ("yessir", result);
+  ASSERT_EQ("test command|", result);
   ASSERT_EQ("", client.do_request("test command post", &result));
-  ASSERT_EQ("yessir", result);
+  ASSERT_EQ("test command|post", result);
   ASSERT_EQ("", client.do_request("test command  post", &result));
-  ASSERT_EQ("yessir", result);
+  ASSERT_EQ("test command| post", result);
   ASSERT_EQ("", client.do_request("test this thing", &result));
-  ASSERT_EQ("yes", result);
+  ASSERT_EQ("test|this thing", result);
 
   ASSERT_EQ("", client.do_request("test  command post", &result));
-  ASSERT_EQ("yes", result);
+  ASSERT_EQ("test| command post", result);
   ASSERT_EQ("", client.do_request("test  this thing", &result));
-  ASSERT_EQ("yes", result);
+  ASSERT_EQ("test| this thing", result);
   ASSERT_EQ(true, asoct.shutdown());
 }
