17f433aa569a437638edd1decadd2fbc9f4878ef
Samuel Just <sam.just@inktank.com>
2012-06-26 10:38:20 -0700

    OSD: disconnect_session_watches: handle race with watch disconnect
    Signed-off-by: Samuel Just <sam.just@inktank.com>
    Reviewed-by: Sage Weil <sage@inktank.com>
    Tested-by: Stefan Priebe <s.priebe@profihost.ag>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -2028,6 +2028,7 @@ void OSD::disconnect_session_watches(Session *session)
 	dout(10) << " disconnected watch " << w << " by " << entity << " session " << session
 		 << ", expires " << expire << dendl;
         obc->watchers.erase(witer++);
+	pg->put_object_context(obc);
 	session->put();
       }
       if (witer == obc->watchers.end())
@@ -2035,8 +2036,6 @@ void OSD::disconnect_session_watches(Session *session)
       ++witer;
     }
     watch_lock.Unlock();
-    pg->put_object_context(obc);
-    /* now drop a reference to that obc */
     pg->unlock();
   }
 }
