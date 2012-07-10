7b0c71cca41b37a7b8920c65ad7a361cadb62388
Dan Mick <dan.mick@inktank.com>
2012-07-09 20:11:21 -0700

    librados: pool_get_name handles "not found" wrong
    Signed-off-by: Dan Mick <dan.mick@inktank.com>

--- src/librados/RadosClient.cc
+++ src/librados/RadosClient.cc
@@ -97,7 +97,7 @@ int librados::RadosClient::pool_get_name(uint64_t pool_id, std::string *s)
 {
   Mutex::Locker l(lock);
   const char *str = osdmap.get_pool_name(pool_id);
-  if (!s)
+  if (!str)
     return -ENOENT;
   *s = str;
   return 0;
