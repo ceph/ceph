b3d077c61e977e8ebb91288aa2294fb21c197fe7
Josh Durgin <josh.durgin@inktank.com>
2012-07-13 09:42:20 -0700

    qa: download tests from specified branch
    These python tests aren't installed, so they need to be downloaded
    
    Signed-off-by: Josh Durgin <josh.durgin@inktank.com>

--- qa/workunits/rbd/test_librbd_python.sh
+++ qa/workunits/rbd/test_librbd_python.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -ex
 
-wget -q https://raw.github.com/ceph/ceph/master/src/test/pybind/test_rbd.py
+CEPH_REF=${CEPH_REF:-master}
+wget -q https://raw.github.com/ceph/ceph/$CEPH_REF/src/test/pybind/test_rbd.py
 nosetests -v -e '.*test_remove_with_watcher' test_rbd
 exit 0
