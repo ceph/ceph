117b28680e56e10aac6c6489ba77963e7e8abd18
Samuel Just <sam.just@inktank.com>
2012-07-13 13:45:24 -0700

    OSD: add config options to fake missed pings
    In order to test monitor and osd failure detection and false
    positive correction, this patch adds the following options:
    
     1. osd_debug_drop_ping_probability: probability of dropping
        a string of pings from a client upon ping recipt.
     2. osd_debug_drop_ping_duration: number of pings to drop in
        a row.
    
    This should help with replicating some wrongly-marked-down
    thrashing cases.
    
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -336,6 +336,8 @@ OPTION(osd_op_complaint_time, OPT_FLOAT, 30) // how many seconds old makes an op
 OPTION(osd_command_max_records, OPT_INT, 256)
 OPTION(osd_op_log_threshold, OPT_INT, 5) // how many op log messages to show in one go
 OPTION(osd_verify_sparse_read_holes, OPT_BOOL, false)  // read fiemap-reported holes and verify they are zeros
+OPTION(osd_debug_drop_ping_probability, OPT_DOUBLE, 0)
+OPTION(osd_debug_drop_ping_duration, OPT_INT, 0)
 OPTION(filestore, OPT_BOOL, false)
 OPTION(filestore_debug_omap_check, OPT_BOOL, 0) // Expensive debugging check on sync
 // Use omap for xattrs for attrs over
