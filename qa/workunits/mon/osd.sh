42e4245c45bc251db1473b8cf9485a2cbd0eea2d
Sage Weil <sage@inktank.com>
2012-07-19 22:04:29 -0700

    qa: simple tests for 'ceph osd create|rm' commands
    Signed-off-by: Sage Weil <sage@inktank.com>

---
+++ qa/workunits/mon/osd.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -x
+
+set -e
+
+ua=`uuidgen`
+ub=`uuidgen`
+
+# shoudl get same id with same uuid
+na=`ceph osd create $ua`
+test $na -eq `ceph osd create $ua`
+
+nb=`ceph osd create $ub`
+test $nb -eq `ceph osd create $ub`
+test $nb -ne $na
+
+ceph osd rm $na
+ceph osd rm $na
+ceph osd rm $nb
+ceph osd rm 123123
+
+na2=`ceph osd create $ua`
+
+echo OK
+
