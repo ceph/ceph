b18c476dbb6356377015b87efcdb6b5fb759edca
Sage Weil <sage@inktank.com>
2012-07-19 21:27:37 -0700

    osd: defer boot if heartbeatmap indicates we are unhealthy
    If the OSD is bogged down or unresponsive, we should not try to join
    the cluster.  This was observed on congress (slow/clogged op_tp combined
    with osdmap thrashing).
    
    Fixes: #2502
    Signed-off-by: Sage Weil <sage@inktank.com>
    Reviewed-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -2174,6 +2174,8 @@ void OSD::_got_boot_version(epoch_t oldest, epoch_t newest)
   // if our map within recent history, try to add ourselves to the osdmap.
   if (osdmap->test_flag(CEPH_OSDMAP_NOUP)) {
     dout(5) << "osdmap NOUP flag is set, waiting for it to clear" << dendl;
+  } else if (!g_ceph_context->get_heartbeat_map()->is_healthy()) {
+    dout(1) << "internal heartbeats indicate we are not healthy; waiting to boot" << dendl;
   } else if (osdmap->get_epoch() >= oldest - 1 &&
 	     osdmap->get_epoch() < newest + g_conf->osd_map_message_max) {
     send_boot();
