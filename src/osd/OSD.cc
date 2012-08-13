71276c80da4b692ace6f911d89de3e9542959dff
Sage Weil <sage@inktank.com>
2012-08-13 10:44:08 -0700

    osd: require CRUSH_TUNABLES feature of peers, clients when used
    If the current crush map has non-default tunables, set our messengers to
    require the tunables feature bit.  If it does not, clear the bit.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -838,6 +838,8 @@ int OSD::init()
   osdmap = get_map(superblock.current_epoch);
   service.publish_map(osdmap);
 
+  check_osdmap_features();
+
   bind_epoch = osdmap->get_epoch();
 
   // load up pgs (as they previously existed)
@@ -3623,6 +3625,8 @@ void OSD::handle_osd_map(MOSDMap *m)
   clear_map_bl_cache_pins();
   map_lock.put_write();
 
+  check_osdmap_features();
+
   // yay!
   if (is_active())
     activate_map();
@@ -3642,6 +3646,47 @@ void OSD::handle_osd_map(MOSDMap *m)
     shutdown();
 
   m->put();
+}
+
+void OSD::check_osdmap_features()
+{
+  // adjust required feature bits?
+
+  // we have to be a bit careful here, because we are accessing the
+  // Policy structures without taking any lock.  in particular, only
+  // modify integer values that can safely be read by a racing CPU.
+  // since we are only accessing existing Policy structures a their
+  // current memory location, and setting or clearing bits in integer
+  // fields, and we are the only writer, this is not a problem.
+
+  Messenger::Policy p = client_messenger->get_default_policy();
+  if (osdmap->crush->has_nondefault_tunables()) {
+    if (!(p.features_required & CEPH_FEATURE_CRUSH_TUNABLES)) {
+      dout(0) << "crush map has non-default tunables, requiring CRUSH_TUNABLES feature for clients" << dendl;
+      p.features_required |= CEPH_FEATURE_CRUSH_TUNABLES;
+      client_messenger->set_default_policy(p);
+    }
+    if (!(cluster_messenger->get_policy(entity_name_t::TYPE_OSD).features_required &
+	  CEPH_FEATURE_CRUSH_TUNABLES)) {
+      dout(0) << "crush map has non-default tunables, requiring CRUSH_TUNABLES feature for osds" << dendl;
+      Messenger::Policy p = cluster_messenger->get_policy(entity_name_t::TYPE_OSD);
+      p.features_required |= CEPH_FEATURE_CRUSH_TUNABLES;
+      cluster_messenger->set_policy(entity_name_t::TYPE_OSD, p);
+    }
+  } else {
+    if (p.features_required & CEPH_FEATURE_CRUSH_TUNABLES) {
+      dout(0) << "crush map has default tunables, not requiring CRUSH_TUNABLES feature for clients" << dendl;
+      p.features_required &= ~CEPH_FEATURE_CRUSH_TUNABLES;
+      client_messenger->set_default_policy(p);
+    }
+    if (cluster_messenger->get_policy(entity_name_t::TYPE_OSD).features_required &
+	CEPH_FEATURE_CRUSH_TUNABLES) {
+      dout(0) << "crush map has default tunables, not requiring CRUSH_TUNABLES feature for osds" << dendl;
+      Messenger::Policy p = cluster_messenger->get_policy(entity_name_t::TYPE_OSD);
+      p.features_required &= ~CEPH_FEATURE_CRUSH_TUNABLES;
+      cluster_messenger->set_policy(entity_name_t::TYPE_OSD, p);
+    }
+  }
 }
 
 void OSD::advance_pg(epoch_t osd_epoch, PG *pg, PG::RecoveryCtx *rctx)
