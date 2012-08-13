ceph/ceph

commit 8453a8198c65712db968ff42be4fd10d2d216582

msg/Accepter: pass nonce on start
This lets us drop the otherwise awkward SimpleMessenger::get_nonce()
accessor.

Signed-off-by: Sage Weil <sage@inktank.com>

src/msg/Accepter.cc
@@ -134,7 +134,7 @@ int Accepter::bind(entity_addr_t &bind_addr, int avoid_port1, int avoid_port2)
     msgr->set_need_addr(true);
 
   if (msgr->get_myaddr().get_port() == 0) {
-    listen_addr.nonce = msgr->get_nonce();
+    listen_addr.nonce = nonce;
     msgr->set_myaddr(listen_addr);
   }
 
@@ -158,13 +158,14 @@ int Accepter::rebind(int avoid_port)
   ldout(msgr->cct,10) << " will try " << addr << dendl;
   int r = bind(addr, old_port, avoid_port);
   if (r == 0)
-    start();
+    start(nonce);
   return r;
 }
 
-int Accepter::start()
+int Accepter::start(uint64_t nonce)
 {
   ldout(msgr->cct,1) << "accepter.start" << dendl;
+  this->nonce = nonce;
 
   // start thread
   create();

src/msg/Accepter.h
@@ -28,6 +28,7 @@ class Accepter : public Thread {
   SimpleMessenger *msgr;
   bool done;
   int listen_sd;
+  uint64_t nonce;
 
 public:
   Accepter(SimpleMessenger *r) : msgr(r), done(false), listen_sd(-1) {}
@@ -36,7 +37,7 @@ public:
   void stop();
   int bind(entity_addr_t &bind_addr, int avoid_port1=0, int avoid_port2=0);
   int rebind(int avoid_port);
-  int start();
+  int start(uint64_t nonce);
 };
 
 

src/msg/SimpleMessenger.cc
@@ -285,7 +285,7 @@ int SimpleMessenger::start()
   lock.Unlock();
 
   if (did_bind)
-    accepter.start();
+    accepter.start(nonce);
 
   reaper_started = true;
   reaper_thread.create();

src/msg/SimpleMessenger.h
@@ -482,10 +482,6 @@ public:
   bool get_need_addr() const { return need_addr; }
   void set_need_addr(bool b) { need_addr = b; }
 
-  uint64_t get_nonce() const {
-    return nonce;
-  }
-
 private:
   /**
    *  false; set to true if the SimpleMessenger bound to a specific address;

commit 698b2368382b83032d08aa0d0f030e9f3d1f510d

msg/Accepter: fix nonce initialization
This needs to be provided to the Accepter at bind time, not by start().
Otherwise the nonce is effectively always 0, which is useless and breaks
all sorts of things.  Broken by 8453a8198c65712db968ff42be4fd10d2d216582.

Signed-off-by: Sage Weil <sage@inktank.com>

src/msg/Accepter.cc
@@ -36,11 +36,13 @@
  * Accepter
  */
 
-int Accepter::bind(entity_addr_t &bind_addr, int avoid_port1, int avoid_port2)
+int Accepter::bind(entity_addr_t &bind_addr, uint64_t _nonce, int avoid_port1, int avoid_port2)
 {
   const md_config_t *conf = msgr->cct->_conf;
   // bind to a socket
   ldout(msgr->cct,10) << "accepter.bind" << dendl;
+
+  nonce = _nonce;
   
   int family;
   switch (bind_addr.get_family()) {
@@ -161,14 +163,13 @@ int Accepter::rebind(int avoid_port)
   ldout(msgr->cct,10) << " will try " << addr << dendl;
   int r = bind(addr, old_port, avoid_port);
   if (r == 0)
-    start(nonce);
+    start();
   return r;
 }
 
-int Accepter::start(uint64_t nonce)
+int Accepter::start()
 {
   ldout(msgr->cct,1) << "accepter.start" << dendl;
-  this->nonce = nonce;
 
   // start thread
   create();

src/msg/Accepter.h
@@ -35,9 +35,9 @@ public:
     
   void *entry();
   void stop();
-  int bind(entity_addr_t &bind_addr, int avoid_port1=0, int avoid_port2=0);
+  int bind(entity_addr_t &bind_addr, uint64_t _nonce, int avoid_port1=0, int avoid_port2=0);
   int rebind(int avoid_port);
-  int start(uint64_t nonce);
+  int start();
 };
 
 

src/msg/SimpleMessenger.cc
@@ -254,7 +254,7 @@ int SimpleMessenger::bind(entity_addr_t bind_addr)
   lock.Unlock();
 
   // bind to a socket
-  int r = accepter.bind(bind_addr);
+  int r = accepter.bind(bind_addr, nonce);
   if (r >= 0)
     did_bind = true;
   return r;
@@ -285,7 +285,7 @@ int SimpleMessenger::start()
   lock.Unlock();
 
   if (did_bind)
-    accepter.start(nonce);
+    accepter.start();
 
   reaper_started = true;
   reaper_thread.create();