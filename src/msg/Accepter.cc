be72a83bbe432fe17ead7f377d413c2a3aebd4d1
Sage Weil <sage@inktank.com>
2012-08-24 16:45:29 -0700

    msg/Accepter: check setsockopt return value
    CCID 716856: Other violation (CHECKED_RETURN)
    At (7): Calling function "setsockopt(this->listen_sd, 1, 2, &on, 4U)" without checking return value. This library function may fail and return an error code.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/msg/Accepter.cc
+++ src/msg/Accepter.cc
@@ -78,7 +78,12 @@ int Accepter::bind(entity_addr_t &bind_addr, uint64_t _nonce, int avoid_port1, i
 
     // reuse addr+port when possible
     int on = 1;
-    ::setsockopt(listen_sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    rc = ::setsockopt(listen_sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+    if (rc < 0) {
+      ldout(msgr->cct,0) << "accepter.bind unable to setsockopt: "
+			 << cpp_strerror(errno) << dendl;
+      return -errno;
+    }
 
     rc = ::bind(listen_sd, (struct sockaddr *) &listen_addr.ss_addr(), listen_addr.addr_size());
     if (rc < 0) {
