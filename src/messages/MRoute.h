b4e461eab05d8f8a9acc732e93dcf01f5f33d853
Sage Weil <sage@inktank.com>
2012-08-31 18:23:20 -0700

    mon: send 'null' reply to requests we won't reply to
    This is a no-op if the client was talking to us, but in the forwarded
    request case will clean up the request state (and request message) on the
    forwarding monitor.  Otherwise, MOSDFailure messages (and probably others)
    can accumulate on the non-leader mon indefinitely.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/messages/MRoute.h
+++ src/messages/MRoute.h
@@ -43,12 +43,14 @@ public:
     bufferlist::iterator p = payload.begin();
     ::decode(session_mon_tid, p);
     ::decode(dest, p);
-    msg = decode_message(NULL, p);
+    if (!p.end())
+      msg = decode_message(NULL, p);
   }
   void encode_payload(uint64_t features) {
     ::encode(session_mon_tid, payload);
     ::encode(dest, payload);
-    encode_message(msg, features, payload);
+    if (msg)
+      encode_message(msg, features, payload);
   }
 
   const char *get_type_name() const { return "route"; }
@@ -56,7 +58,7 @@ public:
     if (msg)
       o << "route(" << *msg;
     else
-      o << "route(???";
+      o << "route(no-reply";
     if (session_mon_tid)
       o << " tid " << session_mon_tid << ")";
     else
