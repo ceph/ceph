bfa9a59f0e2b731a1b6ea752a90f46c220a2c1d2
Sage Weil <sage@inktank.com>
2012-09-01 14:39:28 -0700

    vstart.sh: -r to start radosgw
    Uses a fixed access/secret key for easier testing.  Starts a standalone
    apache2 process with basic config (based on the teuthology one).
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/stop.sh
+++ src/stop.sh
@@ -14,6 +14,7 @@ stop_all=1
 stop_mon=0
 stop_mds=0
 stop_osd=0
+stop_rgw=0
 
 while [ $# -ge 1 ]; do
     case $1 in
@@ -40,7 +41,7 @@ while [ $# -ge 1 ]; do
 done
 
 if [ $stop_all -eq 1 ]; then
-	killall ceph-mon ceph-mds ceph-osd
+	killall ceph-mon ceph-mds ceph-osd radosgw lt-radosgw apache2
 	pkill -f valgrind.bin.\*ceph-mon
 	$SUDO pkill -f valgrind.bin.\*ceph-osd
 	pkill -f valgrind.bin.\*ceph-mds
@@ -48,4 +49,5 @@ else
 	[ $stop_mon -eq 1 ] && do_killall ceph-mon
 	[ $stop_mds -eq 1 ] && do_killall ceph-mds
 	[ $stop_osd -eq 1 ] && do_killall ceph-osd
+	[ $stop_rgw -eq 1 ] && do_killall radosgw lt-radosgw apache2
 fi
