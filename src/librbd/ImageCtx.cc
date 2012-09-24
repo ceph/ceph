e1776809031c6dad441cfb2b9fac9612720b9083
Josh Durgin <josh.durgin@inktank.com>
2012-09-24 12:01:35 -0700

    librbd: ensure header is up to date after initial read
    Use an assert version op in combination with our watch, and re-read
    the header until it's not stale. Header updates are infrequent, so
    this should not cause any delay with normal use.
    
    Signed-off-by: Josh Durgin <josh.durgin@inktank.com>

--- src/librbd/ImageCtx.cc
+++ src/librbd/ImageCtx.cc
@@ -38,6 +38,7 @@ namespace librbd {
       wctx(NULL),
       refresh_seq(0),
       last_refresh(0),
+      last_header_version(0),
       md_lock("librbd::ImageCtx::md_lock"),
       cache_lock("librbd::ImageCtx::cache_lock"),
       snap_lock("librbd::ImageCtx::snap_lock"),
@@ -460,7 +461,20 @@ namespace librbd {
   int ImageCtx::register_watch() {
     assert(!wctx);
     wctx = new WatchCtx(this);
-    return md_ctx.watch(header_oid, 0, &(wctx->cookie), wctx);
+
+    int r = 0;
+    do {
+      md_lock.Lock();
+      if (r == -ERANGE)
+	ictx_refresh(this);
+      uint64_t last_read = last_header_version;
+      md_lock.Unlock();
+
+      md_ctx.set_assert_version(last_read);
+      r = md_ctx.watch(header_oid, 0, &(wctx->cookie), wctx);
+      ldout(cct, 20) << "watching header object returned " << r << dendl;
+    } while (r == -ERANGE);
+    return r;
   }
 
   void ImageCtx::unregister_watch() {
