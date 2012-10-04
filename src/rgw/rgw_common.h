f5501d0826886c675a61490ae22342c094037181
Yehuda Sadeh <yehuda@inktank.com>
2012-10-03 17:36:50 -0700

    rgw: handle content_type, meta
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -39,10 +39,12 @@ using ceph::crypto::MD5;
 
 #define RGW_ATTR_PREFIX  "user.rgw."
 
+#define RGW_AMZ_META_PREFIX "x-amz-meta-"
+
 #define RGW_ATTR_ACL		RGW_ATTR_PREFIX "acl"
 #define RGW_ATTR_ETAG    	RGW_ATTR_PREFIX "etag"
 #define RGW_ATTR_BUCKETS	RGW_ATTR_PREFIX "buckets"
-#define RGW_ATTR_META_PREFIX	RGW_ATTR_PREFIX "x-amz-meta-"
+#define RGW_ATTR_META_PREFIX	RGW_ATTR_PREFIX RGW_AMZ_META_PREFIX
 #define RGW_ATTR_CONTENT_TYPE	RGW_ATTR_PREFIX "content_type"
 #define RGW_ATTR_CACHE_CONTROL	RGW_ATTR_PREFIX "cache_control"
 #define RGW_ATTR_CONTENT_DISP	RGW_ATTR_PREFIX "content_disposition"
