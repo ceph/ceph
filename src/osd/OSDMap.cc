ceph/ceph

commit 13b841c74605d67a172d787079bcef007d1a8fa5

osdmap, mon: optional dump 'osd tree' in json
Signed-off-by: Sage Weil <sage@inktank.com>

src/osd/OSDMap.cc
@@ -1428,27 +1428,50 @@ void OSDMap::print(ostream& out) const
   // ignore pg_swap_primary
 }
 
-void OSDMap::print_osd_line(int cur, ostream& out) const
+void OSDMap::print_osd_line(int cur, ostream *out, Formatter *f) const
 {
-  out << "osd." << cur << "\t";
-  if (!exists(cur))
-    out << "DNE\t\t";
-  else {
-    if (is_up(cur))
-      out << "up\t";
-    else
-      out << "down\t";
-    std::streamsize p = out.precision();
-    out << std::setprecision(4) 
-	<< (exists(cur) ? get_weightf(cur) : 0)
-	<< std::setprecision(p)
-	<< "\t";
+  if (f) {
+    f->dump_unsigned("id", cur);
+    f->dump_stream("name") << "osd." << cur;
+    f->dump_unsigned("exists", (int)exists(cur));
+    f->dump_string("type", crush->get_type_name(0));
+    f->dump_int("type_id", 0);
+  }
+  if (out)
+    *out << "osd." << cur << "\t";
+  if (!exists(cur)) {
+    *out << "DNE\t\t";
+  } else {
+    if (is_up(cur)) {
+      if (out)
+	*out << "up\t";
+      if (f)
+	f->dump_string("status", "up");
+    } else {
+      if (out)
+	*out << "down\t";
+      if (f)
+	f->dump_string("status", "down");
+    }
+    if (out) {
+      std::streamsize p = out->precision();
+      *out << std::setprecision(4)
+	   << (exists(cur) ? get_weightf(cur) : 0)
+	   << std::setprecision(p)
+	   << "\t";
+    }
+    if (f) {
+      f->dump_float("reweight", get_weightf(cur));
+    }
   }
 }
 
-void OSDMap::print_tree(ostream& out) const
+void OSDMap::print_tree(ostream *out, Formatter *f) const
 {
-  out << "# id\tweight\ttype name\tup/down\treweight\n";
+  if (out)
+    *out << "# id\tweight\ttype name\tup/down\treweight\n";
+  if (f)
+    f->open_array_section("nodes");
   set<int> touched;
   set<int> roots;
   crush->find_roots(roots);
@@ -1461,29 +1484,63 @@ void OSDMap::print_tree(ostream& out) const
       float weight = q.front().weight;
       q.pop_front();
 
-      out << cur << "\t";
-      int oldprecision = out.precision();
-      out << std::setprecision(4) << weight << std::setprecision(oldprecision) << "\t";
+      if (out) {
+	*out << cur << "\t";
+	int oldprecision = out->precision();
+	*out << std::setprecision(4) << weight << std::setprecision(oldprecision) << "\t";
 
-      for (int k=0; k<depth; k++)
-	out << "\t";
-
+	for (int k=0; k<depth; k++)
+	  *out << "\t";
+      }
+      if (f) {
+	f->open_object_section("item");
+      }
       if (cur >= 0) {
-	print_osd_line(cur, out);
-	out << "\n";
+	print_osd_line(cur, out, f);
+	if (out)
+	  *out << "\n";
+	if (f) {
+	  f->close_section();
+	}
 	touched.insert(cur);
+      }
+      if (f) {
+	f->dump_float("crush_weight", weight);
+	f->dump_unsigned("depth", depth);
+      }
+      if (cur >= 0) {
 	continue;
       }
 
-      int type = crush->get_bucket_type(cur);
-      out << crush->get_type_name(type) << " " << crush->get_item_name(cur) << "\n";
-
       // queue bucket contents...
+      int type = crush->get_bucket_type(cur);
       int s = crush->get_bucket_size(cur);
-      for (int k=s-1; k>=0; k--)
-	q.push_front(qi(crush->get_bucket_item(cur, k), depth+1,
-			(float)crush->get_bucket_item_weight(cur, k) / (float)0x10000));
+      if (f) {
+	f->dump_int("id", cur);
+	f->dump_string("name", crush->get_item_name(cur));
+	f->dump_string("type", crush->get_type_name(type));
+	f->dump_int("type_id", type);
+	f->open_array_section("children");
+      }
+      for (int k=s-1; k>=0; k--) {
+	int item = crush->get_bucket_item(cur, k);
+	q.push_front(qi(item, depth+1, (float)crush->get_bucket_item_weight(cur, k) / (float)0x10000));
+	f->dump_int("child", item);
+      }
+      if (f)
+	f->close_section();
+
+      if (out)
+	*out << crush->get_type_name(type) << " " << crush->get_item_name(cur) << "\n";
+      if (f) {
+	f->close_section();
+      }
+
     }
+  }
+  if (f) {
+    f->close_section();
+    f->open_array_section("stray");
   }
 
   set<int> stray;
@@ -1492,14 +1549,22 @@ void OSDMap::print_tree(ostream& out) const
       stray.insert(i);
 
   if (!stray.empty()) {
-    out << "\n";
+    if (out)
+      *out << "\n";
+    if (f)
+      f->open_object_section("osd");
     for (set<int>::iterator p = stray.begin(); p != stray.end(); ++p) {
-      out << *p << "\t0\t";
-      print_osd_line(*p, out);
-      out << "\n";
+      if (out)
+	*out << *p << "\t0\t";
+      print_osd_line(*p, out, f);
+      if (out)
+	*out << "\n";
     }
+    if (f)
+      f->close_section();
   }
-
+  if (f)
+    f->close_section();
 }
 
 void OSDMap::print_summary(ostream& out) const

src/mon/OSDMonitor.cc
@@ -1677,10 +1677,23 @@ bool OSDMonitor::preprocess_command(MMonCommand *m)
 	  }
 	} else if (cmd == "tree") {
 	  stringstream ds;
-	  p->print_tree(ds);
-	  rdata.append(ds);
-	  ss << "dumped osdmap tree epoch " << p->get_epoch();
-	  r = 0;
+	  if (format == "json") {
+	    JSONFormatter jf(true);
+	    jf.open_object_section("tree");
+	    p->print_tree(NULL, &jf);
+	    jf.close_section();
+	    jf.flush(ds);
+	    r = 0;
+	  } else if (format == "plain") {
+	    p->print_tree(&ds, NULL);
+	    r = 0;
+	  } else {
+	    ss << "unrecognized format '" << format << "'";
+	    r = -EINVAL;
+	  }
+	  if (r == 0) {
+	    rdata.append(ds);
+	  }
 	} else if (cmd == "getmap") {
 	  p->encode(rdata);
 	  ss << "got osdmap epoch " << p->get_epoch();

src/osd/OSDMap.h
@@ -545,11 +545,11 @@ public:
 
 
 private:
-  void print_osd_line(int cur, ostream& out) const;
+  void print_osd_line(int cur, ostream *out, Formatter *f) const;
 public:
   void print(ostream& out) const;
   void print_summary(ostream& out) const;
-  void print_tree(ostream& out) const;
+  void print_tree(ostream *out, Formatter *f) const;
 
   string get_flag_string() const;
   static string get_flag_string(unsigned flags);

src/osdmaptool.cc
@@ -315,7 +315,7 @@ int main(int argc, const char **argv)
   if (print_json)
     osdmap.dump_json(cout);
   if (tree) 
-    osdmap.print_tree(cout);
+    osdmap.print_tree(&cout, NULL);
 
   if (modified) {
     bl.clear();

commit 0406d0c53a8cfb9e32c4c6df8901d536a43db18d

osdmap: fix osdmap dump null check
CID 731939 (#1 of 1): Dereference after null check (FORWARD_NULL)
At (5): Passing null pointer "out" to function "std::operator << <std::char_traits<char> >(std::basic_ostream<char, std::char_traits<char> > &, char const *)", which dereferences it.

Broken by 13b841c74605d67a172d787079bcef007d1a8fa5.

Signed-off-by: Sage Weil <sage@inktank.com>

src/osd/OSDMap.cc
@@ -1440,7 +1440,8 @@ void OSDMap::print_osd_line(int cur, ostream *out, Formatter *f) const
   if (out)
     *out << "osd." << cur << "\t";
   if (!exists(cur)) {
-    *out << "DNE\t\t";
+    if (out)
+      *out << "DNE\t\t";
   } else {
     if (is_up(cur)) {
       if (out)