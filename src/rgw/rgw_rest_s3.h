347bb4d7ada8f3f908ffa73df0408814329bb37f
Yehuda Sadeh <yehuda@inktank.com>
2012-10-11 14:27:16 -0700

    rgw: implement POST key can use ${filename}
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/rgw/rgw_rest_s3.h
+++ src/rgw/rgw_rest_s3.h
@@ -90,12 +90,13 @@ struct post_part_field {
 struct post_form_part {
   string name;
   string content_type;
-  map<string, struct post_part_field> fields;
+  map<string, struct post_part_field, ltstr_nocase> fields;
   bufferlist data;
 };
 
 class RGWPostObj_ObjStore_S3 : public RGWPostObj_ObjStore {
   string boundary;
+  string filename;
   bufferlist in_data;
   map<string, post_form_part, const ltstr_nocase> parts;  
   RGWPolicyEnv env;
@@ -115,6 +116,7 @@ class RGWPostObj_ObjStore_S3 : public RGWPostObj_ObjStore {
   bool part_bl(const string& name, bufferlist *pbl);
 
   int get_policy();
+  void rebuild_key(string& key);
 public:
   RGWPostObj_ObjStore_S3() {}
   ~RGWPostObj_ObjStore_S3() {}
