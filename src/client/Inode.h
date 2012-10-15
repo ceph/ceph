60f134bf8a1b6b20ce5dba577a94210e375b427b
Sage Weil <sage@inktank.com>
2012-10-15 14:20:51 -0700

    client: fix shadowing in inode ctor
    CID 728080 (#1 of 1): Incorrect sizeof expression (BAD_SIZEOF)
    Taking the size of pointer parameter "layout" is suspicious.
    
    At (2): Non-static class member field "layout.fl_stripe_unit" is not initialized in this constructor nor in any functions that it calls.
    At (4): Non-static class member field "layout.fl_stripe_count" is not initialized in this constructor nor in any functions that it calls.
    At (6): Non-static class member field "layout.fl_object_size" is not initialized in this constructor nor in any functions that it calls.
    At (8): Non-static class member field "layout.fl_cas_hash" is not initialized in this constructor nor in any functions that it calls.
    At (10): Non-static class member field "layout.fl_object_stripe_unit" is not initialized in this constructor nor in any functions that it calls.
    At (12): Non-static class member field "layout.fl_unused" is not initialized in this constructor nor in any functions that it calls.
    CID 717206 (#1 of 1): Uninitialized scalar field (UNINIT_CTOR)
    At (14): Non-static class member field "layout.fl_pg_pool" is not initialized in this constructor nor in any functions that it calls.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/client/Inode.h
+++ src/client/Inode.h
@@ -186,7 +186,7 @@ class Inode {
     ll_ref -= n;
   }
 
-  Inode(CephContext *cct_, vinodeno_t vino, ceph_file_layout *layout)
+  Inode(CephContext *cct_, vinodeno_t vino, ceph_file_layout *newlayout)
     : cct(cct_), ino(vino.ino), snapid(vino.snapid),
       rdev(0), mode(0), uid(0), gid(0), nlink(0),
       size(0), truncate_seq(1), truncate_size(-1),
@@ -198,7 +198,7 @@ class Inode {
       exporting_issued(0), exporting_mds(-1), exporting_mseq(0),
       cap_item(this), flushing_cap_item(this), last_flush_tid(0),
       snaprealm(0), snaprealm_item(this), snapdir_parent(0),
-      oset((void *)this, layout->fl_pg_pool, ino),
+      oset((void *)this, newlayout->fl_pg_pool, ino),
       reported_size(0), wanted_max_size(0), requested_max_size(0),
       _ref(0), ll_ref(0), 
       dir(0), dn_set()
