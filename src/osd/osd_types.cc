8459a1b41c4165480e205a42b4690951198c6a65
Sage Weil <sage@inktank.com>
2012-10-22 17:57:32 -0700

    osd/osd_types: make stat osd_op print nicely
    No 0~0 crap.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/osd/osd_types.cc
+++ src/osd/osd_types.cc
@@ -2677,6 +2677,7 @@ ostream& operator<<(ostream& out, const OSDOp& op)
   if (ceph_osd_op_type_data(op.op.op)) {
     // data extent
     switch (op.op.op) {
+    case CEPH_OSD_OP_STAT:
     case CEPH_OSD_OP_DELETE:
       break;
     case CEPH_OSD_OP_TRUNCATE:
