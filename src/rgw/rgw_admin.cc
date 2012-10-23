18731b4bb5015d601ebd40c9e385eaa407727824
caleb miles <caselim@gmail.com>
2012-10-23 13:36:53 -0400

    rgw_admin: do not throw error when start-date and end-date are not passed to usage::trim()
    Signed-off-by: caleb miles <caleb.miles@inktank.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -1584,18 +1584,25 @@ next:
       cerr << "do you really mean it? (requires --yes-i-really-mean-it)" << std::endl;
       return 1;
     }
+    int ret;
     uint64_t start_epoch = 0;
     uint64_t end_epoch = (uint64_t)-1;
 
-    int ret = parse_date(start_date, &start_epoch);
-    if (ret < 0) {
-      cerr << "ERROR: failed to parse start date" << std::endl;
-      return 1;
+
+    if (!start_date.empty()) {
+      ret = parse_date(start_date, &start_epoch);
+      if (ret < 0) {
+        cerr << "ERROR: failed to parse start date" << std::endl;
+        return 1;
+      }
     }
-    ret = parse_date(end_date, &end_epoch);
-    if (ret < 0) {
-      cerr << "ERROR: failed to parse end date" << std::endl;
-      return 1;
+
+    if (!end_date.empty()) {
+      ret = parse_date(end_date, &end_epoch);
+      if (ret < 0) {
+        cerr << "ERROR: failed to parse end date" << std::endl;
+        return 1;
+      }
     }
 
     ret = RGWUsage::trim(store, user_id, start_epoch, end_epoch);
