74b4f685512c7d7821ee3f7a1d8d03445e851ec6
Peter Reiher <reiher@inktank.com>
2012-10-29 14:36:08 -0700

    Fixed problem with checking authorizer in accept().
    Signed-off-by: Peter Reiher <reiher@inktank.com>

--- src/msg/Pipe.cc
+++ src/msg/Pipe.cc
@@ -312,32 +312,21 @@ int Pipe::accept()
 
     msgr->lock.Unlock();
 
-    // As it stands, this code is not totally secure, since the first monitor pipe that gets set up isn't ready to verify authorizers.
-    // As a result, we just skip over verifying the authorizer.  Fixing it requires some surgery in monitor initialization.  PLR
+    // Check the authorizer.  If not good, bail out.
 
-    if (msgr->verify_authorizer(connection_state, peer_type,
-				connect.authorizer_protocol, authorizer, authorizer_reply, authorizer_valid, session_key) ) {
-	if (!authorizer_valid) {
-          ldout(msgr->cct,0) << "accept: got bad authorizer" << dendl;
-          reply.tag = CEPH_MSGR_TAG_BADAUTHORIZER;
-          session_security = NULL;
-          goto reply;
-        } 
-	else {
-
-          // We've verified the authorizer for this pipe, so set up the session security structure.  PLR
-
-          session_security = get_auth_session_handler(msgr->cct, connect.authorizer_protocol, session_key);
-        }
-    }
-    else {
-
-      // verify_authorizer failed.  Don't set up session security, since we have no authorizer to get a key from.  PLR
-
-      ldout(msgr->cct,10) << "accept:  verify_authorizer failed, setting session_security to NULL." << dendl;
+    if (!msgr->verify_authorizer(connection_state, peer_type, connect.authorizer_protocol, authorizer, 
+        authorizer_reply, authorizer_valid, session_key) || !authorizer_valid ) {
+      ldout(msgr->cct,0) << "accept: got bad authorizer" << dendl;
+      reply.tag = CEPH_MSGR_TAG_BADAUTHORIZER;
       session_security = NULL;
-    }
-    
+      goto reply;
+    } 
+
+    // We've verified the authorizer for this pipe, so set up the session security structure.  PLR
+
+    ldout(msgr->cct,10) << "accept:  setting up session_security." << dendl;
+    session_security = get_auth_session_handler(msgr->cct, connect.authorizer_protocol, session_key);
+
     msgr->lock.Lock();
     if (msgr->dispatch_queue.stop)
       goto shutting_down;
