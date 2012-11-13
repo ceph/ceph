a301d02ae3f7e115a0059ad804c23e110dcdcdf9
Sage Weil <sage@inktank.com>
2012-11-12 16:31:35 -0800

    osdc/ObjectCacher: do not take Object ref for bh writes
    This reverts commit 5824795cf6a443f1ec57d32f2b00bba3e627882f.
    
    There is no reason to carry a ref for the writes; it just makes things
    more confusing because the refs aren't used for lifecycle, only for
    LRU pinning.  We would need to duplicate the close_object() conditional
    here for this to work right.
    
    This fixes #3431, in which a slow osd reply has an Object pinned, but when
    we try to truncate it away we hit the assert in can_close().
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/osdc/ObjectCacher.cc
+++ src/osdc/ObjectCacher.cc
@@ -684,8 +684,6 @@ void ObjectCacher::bh_read_finish(int64_t poolid, sobject_t oid, loff_t start,
 void ObjectCacher::bh_write(BufferHead *bh)
 {
   ldout(cct, 7) << "bh_write " << *bh << dendl;
-
-  bh->ob->get();
   
   // finishers
   C_WriteCommit *oncommit = new C_WriteCommit(this, bh->ob->oloc.pool,
@@ -794,9 +792,6 @@ void ObjectCacher::bh_write_commit(int64_t poolid, sobject_t oid, loff_t start,
 
     // is the entire object set now clean and fully committed?
     ObjectSet *oset = ob->oset;
-    ob->put();
-
-    // is the entire object set now clean?
     if (flush_set_callback &&
 	was_dirty_or_tx > 0 &&
 	oset->dirty_or_tx == 0) {        // nothing dirty/tx
