183953e14be91db0e7636d79851eee8e9e02aa06
Sage Weil <sage@inktank.com>
2012-11-17 16:01:13 -0800

    msg/SimpleMessenger: use put() on local_connection
    This aids leak debugging; not much else.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/msg/SimpleMessenger.cc
+++ src/msg/SimpleMessenger.cc
@@ -66,7 +66,7 @@ SimpleMessenger::~SimpleMessenger()
   assert(!did_bind); // either we didn't bind or we shut down the Accepter
   assert(rank_pipe.empty()); // we don't have any running Pipes.
   assert(reaper_stop && !reaper_started); // the reaper thread is stopped
-  delete local_connection;
+  local_connection->put();
 }
 
 void SimpleMessenger::ready()
