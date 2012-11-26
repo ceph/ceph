171eaa21010fd864a9f2fe28bcc10ab6e39a2043
Dan Mick <dan.mick@inktank.com>
2012-11-26 13:43:13 -0800

    test_lock_fence.sh, rbdrw.py: rbd lock/fence test
    qa/workunits/rbd/test_lock_fence.sh runs using test/rbdrw.py
    
    rbdrw.py creates an image, locks it, and runs an I/O loop;
    test_lock_fence.sh runs it, waits, and then blacklists that client,
    which causes rbdrw.py to get ESHUTDOWN on operations thereafter.
    Currently doesn't work with rbd caching enabled.
    
    rbd.py gets new exception type for ESHUTDOWN
    
    Fixes: #3190
    Signed-off-by: Dan Mick <dan.mick@inktank.com>
    Signed-off-by: Josh Durgin <josh.durgin@inktank.com>

---
+++ src/test/librbd/rbdrw.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""
+Loop writing/reading the first 4k of image argv[1] in pool rbd,
+after acquiring exclusive lock named argv[2].  When an exception
+happens, split off the last number in the exception 'args' string
+and use it as the process exit code, if it's convertible to a number.
+
+Designed to run against a blacklist operation and verify the
+ESHUTDOWN expected from the image operation.
+
+Note: this cannot be run with writeback caching on, currently, as
+writeback errors cause reads be marked dirty rather than error, and
+even if they were marked as errored, ObjectCacher would retry them
+rather than note them as errored.
+"""
+
+import rados, rbd, sys
+
+with rados.Rados(conffile='') as r:
+    with r.open_ioctx('rbd') as ioctx:
+        with rbd.Image(ioctx, sys.argv[1]) as image:
+            image.lock_exclusive(sys.argv[2])
+            while True:
+                try:
+                    image.write('A' * 4096, 0)
+                    r = image.read(0, 4096)
+                except rbd.ConnectionShutdown:
+                    # it so happens that the errno here is 108, but
+                    # anything recognizable would do
+                    exit(108)
+
