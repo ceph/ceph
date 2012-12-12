047aecd90f1dbfb172f48f9d10b67e82b3a8ce15
Samuel Just <sam.just@inktank.com>
2012-12-11 22:22:31 -0800

    PG,ReplicatedPG: handle_watch_timeout must not write during scrub/degraded
    Currently, handle_watch_timeout will gladly write to an object while
    that object is degraded or is being scrubbed.  Now, we queue a
    callback to be called on scrub completion or finish_degraded_object
    to recall handle_watch_timeout.  The callback mechanism assumes that
    the registered callbacks assume they will be called with the pg
    lock -- and no other locks -- already held.
    
    The callback will release the obc and pg refs unconditionally.  Thus,
    we need to replace the unconnected_watchers pointer with NULL to
    ensure that unregister_unconnected_watcher fails to cancel the
    event and does not release the resources a second time.
    
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -656,6 +656,8 @@ protected:
   list<OpRequestRef>            waiting_for_all_missing;
   map<hobject_t, list<OpRequestRef> > waiting_for_missing_object,
                                         waiting_for_degraded_object;
+  // Callbacks should assume pg (and nothing else) is locked
+  map<hobject_t, list<Context*> > callbacks_for_degraded_object;
   map<eversion_t,list<OpRequestRef> > waiting_for_ack, waiting_for_ondisk;
   map<eversion_t,OpRequestRef>   replay_queue;
   void split_ops(PG *child, unsigned split_bits);
@@ -857,6 +859,20 @@ public:
     // deep scrub
     bool deep;
 
+    list<Context*> callbacks;
+    void add_callback(Context *context) {
+      callbacks.push_back(context);
+    }
+    void run_callbacks() {
+      list<Context*> to_run;
+      to_run.swap(callbacks);
+      for (list<Context*>::iterator i = to_run.begin();
+	   i != to_run.end();
+	   ++i) {
+	(*i)->complete(0);
+      }
+    }
+
     static const char *state_string(const PG::Scrubber::State& state) {
       const char *ret = NULL;
       switch( state )
@@ -911,6 +927,7 @@ public:
       errors = 0;
       fixed = 0;
       deep = false;
+      run_callbacks();
     }
 
   } scrubber;
