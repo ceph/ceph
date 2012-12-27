bc4357dd00f1c20fd931baaa6f5cfe769673c674
Noah Watkins <noahwatkins@gmail.com>
2012-12-27 15:54:26 -0800

    libcephfs: document ERANGE rv for get_file_pool_name
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/include/cephfs/libcephfs.h
+++ src/include/cephfs/libcephfs.h
@@ -851,7 +851,7 @@ int ceph_get_file_pool(struct ceph_mount_info *cmount, int fh);
  * @param fh the open file descriptor referring to the file
  * @param buf buffer to store the name in
  * @param buflen size of the buffer
- * @returns length in bytes of the pool name
+ * @returns length in bytes of the pool name, or -ERANGE if the buffer is not large enough.
  */
 int ceph_get_file_pool_name(struct ceph_mount_info *cmount, int fh, char *buf, size_t buflen);
 
