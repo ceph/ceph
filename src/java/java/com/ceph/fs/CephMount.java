afb1c232d670fc97f4123d6088c42507e5f60e81
Noah Watkins <noahwatkins@gmail.com>
2013-01-05 11:12:25 -0800

    java: update javadoc comments
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- src/java/java/com/ceph/fs/CephMount.java
+++ src/java/java/com/ceph/fs/CephMount.java
@@ -666,9 +666,9 @@ public class CephMount {
   private static synchronized native int native_ceph_localize_reads(long mountp, boolean on);
 
   /**
-   * Pull the stripe unit granularity from the underlying ceph installation.
+   * Get file layout stripe unit granularity.
    *
-   * @return all block sizes must be a (non-zero) multiple of this value.
+   * @return Stripe unit granularity.
    */
   public int get_stripe_unit_granularity() {
     return native_ceph_get_stripe_unit_granularity(instance_ptr);
