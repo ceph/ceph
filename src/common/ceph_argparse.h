db48caf693192b9266abe3c38d8faa7b9272d6e6
David Zafman <david.zafman@inktank.com>
2013-01-14 16:37:09 -0800

    osd: debug support for omap deep-scrub
    Deep-scrub test support through admin socket
    
    Signed-off-by: David Zafman <david.zafman@inktank.com>
    Reviewed-by: Samuel Just <sam.just@inktank.com>

--- src/common/ceph_argparse.h
+++ src/common/ceph_argparse.h
@@ -43,6 +43,7 @@ public:
 };
 
 /////////////////////// Functions ///////////////////////
+extern void string_to_vec(std::vector<std::string>& args, std::string argstr);
 extern void env_to_vec(std::vector<const char*>& args, const char *name=NULL);
 extern void argv_to_vec(int argc, const char **argv,
                  std::vector<const char*>& args);
