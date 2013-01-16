3e72893152a9ef7c72b510460a6c80cda043ad48
Greg Farnum <greg@inktank.com>
2013-01-15 17:48:46 -0800

    client: rename client/SnapRealm files to avoid automake build conflict
    We are about to move the MDS' SnapRealm into its own files, which conflicts.
    The MDS is more important, so it wins the "SnapRealm" filename and we
    move the client stuff to ClientSnapRealm.[h|cc].
    
    Signed-off-by: Sage Weil <sage@inktank.com>
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- src/client/ClientSnapRealm.cc
+++ src/client/ClientSnapRealm.cc
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 
-#include "SnapRealm.h"
+#include "ClientSnapRealm.h"
 #include "common/Formatter.h"
 
 void SnapRealm::dump(Formatter *f) const
