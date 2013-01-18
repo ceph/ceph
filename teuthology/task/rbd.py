dbc38eff62173107df208f4ae36298d9df6aa23d
Alex Elder <elder@inktank.com>
2013-01-18 12:47:34 -0600

    rbd.py: update scratch and test image sizes
    Test 167 was failing due to running out of space on the scratch
    file system.  The test reserves 21MB in a file, and repeats 50
    times.  It required just over 1GB, so I bumped the default size
    for the testing device to 1200 MB.  I increased the test device
    size as well.
    
    This resolves http://tracker.newdream.net/issues/3864.
    
    Signed-off-by: Alex Elder <elder@inktank.com>

--- teuthology/task/rbd.py
+++ teuthology/task/rbd.py
@@ -531,10 +531,10 @@ def xfstests(ctx, config):
             properties = {}
 
         test_image = properties.get('test_image', 'test_image.{role}'.format(role=role))
-        test_size = properties.get('test_size', 1000)
+        test_size = properties.get('test_size', 1200)
         test_fmt = properties.get('test_format', 1)
         scratch_image = properties.get('scratch_image', 'scratch_image.{role}'.format(role=role))
-        scratch_size = properties.get('scratch_size', 1000)
+        scratch_size = properties.get('scratch_size', 1200)
         scratch_fmt = properties.get('scratch_format', 1)
 
         images_config[role] = dict(
