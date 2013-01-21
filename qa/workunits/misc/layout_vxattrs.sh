4990c2ff641494dc539011e989389f0eacafd8e8
Sage Weil <sage@inktank.com>
2013-01-20 21:53:37 -0800

    mds: parse ceph.*.layout vxattr key/value content
    Use qi to parse a strictly formatted set of key/value pairs.  Be picky
    about whitespace.  Any subset of recognized keys is allowed.  Parse the
    same set of keys as the ceph.*.layout.* vxattrs.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- qa/workunits/misc/layout_vxattrs.sh
+++ qa/workunits/misc/layout_vxattrs.sh
@@ -32,6 +32,18 @@ getfattr -n ceph.file.layout.stripe_unit file2 | grep -q 1048576
 getfattr -n ceph.file.layout.stripe_count file2 | grep -q 8
 getfattr -n ceph.file.layout.object_size file2 | grep -q 10485760
 
+setfattr -n ceph.file.layout -v "stripe_unit=4194304 stripe_count=16 object_size=41943040 pool=data" file2
+getfattr -n ceph.file.layout.stripe_unit file2 | grep -q 4194304
+getfattr -n ceph.file.layout.stripe_count file2 | grep -q 16
+getfattr -n ceph.file.layout.object_size file2 | grep -q 41943040
+getfattr -n ceph.file.layout.pool file2 | grep -q data
+
+setfattr -n ceph.file.layout -v "stripe_unit=1048576" file2
+getfattr -n ceph.file.layout.stripe_unit file2 | grep -q 1048576
+getfattr -n ceph.file.layout.stripe_count file2 | grep -q 16
+getfattr -n ceph.file.layout.object_size file2 | grep -q 41943040
+getfattr -n ceph.file.layout.pool file2 | grep -q data
+
 # dir
 rm -f dir/file || true
 rmdir dir || true
@@ -54,6 +66,11 @@ getfattr -n ceph.dir.layout.pool dir | grep -q data
 getfattr -n ceph.dir.layout.stripe_unit dir | grep -q 1048576
 getfattr -n ceph.dir.layout.stripe_count dir | grep -q 8
 getfattr -n ceph.dir.layout.object_size dir | grep -q 10485760
+
+setfattr -n ceph.file.layout -v "stripe_count=16" file2
+getfattr -n ceph.file.layout.stripe_count file2 | grep -q 16
+setfattr -n ceph.file.layout -v "object_size=10485760 stripe_count=8 stripe_unit=1048576 pool=data" file2
+getfattr -n ceph.file.layout.stripe_count file2 | grep -q 8
 
 touch dir/file
 getfattr -n ceph.file.layout.pool dir/file | grep -q data
