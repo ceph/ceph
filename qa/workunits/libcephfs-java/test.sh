586f84f4e8a2e79449f9dabef9ffdb0b6592a226
Sage Weil <sage@inktank.com>
2013-01-24 15:13:37 -0800

    libcephfs-java test: use provided environment
    Signed-off-by: Sage Weil <sage@inktank.com>

--- qa/workunits/libcephfs-java/test.sh
+++ qa/workunits/libcephfs-java/test.sh
@@ -1,12 +1,8 @@
 #!/bin/sh -e
 
-BASE=/tmp/cephtest
-TLIB=binary/usr/local/share/java
-
 echo "starting libcephfs-java tests"
 
-export LD_LIBRARY_PATH=$BASE/$TLIB 
-command="java -DCEPH_CONF_FILE=$BASE/ceph.conf -Djava.library.path=$LD_LIBRARY_PATH -cp /usr/share/java/junit4.jar:$BASE/$TLIB/libcephfs.jar:$BASE/$TLIB/libcephfs-test.jar org.junit.runner.JUnitCore com.ceph.fs.CephAllTests"
+command="java -DCEPH_CONF_FILE=$CEPH_CONF -Djava.library.path=$LD_LIBRARY_PATH -cp /usr/share/java/junit4.jar:$CEPH_JAVA_PATH/libcephfs.jar:$CEPH_JAVA_PATH/libcephfs-test.jar org.junit.runner.JUnitCore com.ceph.fs.CephAllTests"
 
 echo "----------------------"
 echo $command
