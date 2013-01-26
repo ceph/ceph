34ae1ae7ab0b1463998689b52a0f8cdee84fadf2
Sage Weil <sage@inktank.com>
2013-01-25 19:51:14 -0800

    mon/PGMap: report recovery rates
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/mon/PGMap.h
+++ src/mon/PGMap.h
@@ -73,6 +73,10 @@ public:
 
   utime_t stamp;
 
+  // delta since the previous map
+  pool_stat_t pg_sum_delta;
+  utime_t stamp_delta;
+
   set<pg_t> creating_pgs;   // lru: front = new additions, back = recently pinged
   map<int,set<pg_t> > creating_pgs_by_osd;
 
