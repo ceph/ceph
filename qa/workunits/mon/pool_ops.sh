bbb86ec79409bc906c08c896e855d2a7b556fb9f
Sage Weil <sage@inktank.com>
2013-01-26 13:45:12 -0800

    mon: safety interlock for pool deletion
    Require that the pool name be passed twice along with an force option
    before we irreversibly delete an entire pool of objects.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- qa/workunits/mon/pool_ops.sh
+++ qa/workunits/mon/pool_ops.sh
@@ -7,9 +7,13 @@ ceph osd pool create fooo 123
 
 ceph osd pool create foo 123 # idempotent
 
-ceph osd pool delete foo
-ceph osd pool delete foo
-ceph osd pool delete fuggg
+ceph osd pool delete foo && exit 1 || true    # should fail due to safety interlock
+ceph osd pool delete foo foo && exit 1 || true    # should fail due to safety interlock
+ceph osd pool delete foo foo --force && exit 1 || true    # should fail due to safety interlock
+
+ceph osd pool delete foo foo --yes-i-really-really-mean-it
+ceph osd pool delete foo foo --yes-i-really-really-mean-it
+ceph osd pool delete fuggg fugg --yes-i-really-really-mean-it
 
 ceph osd pool delete fooo
 
