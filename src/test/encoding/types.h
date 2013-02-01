deb0d45c084434b914cb95f186d12b5b8f868a85
Greg Farnum <greg@inktank.com>
2013-01-31 20:31:13 -0800

    mds: EMetaBlob::full_bit more modernization for dencoder
    While we're doing so, make the frag stream operator const!
    
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- src/test/encoding/types.h
+++ src/test/encoding/types.h
@@ -133,6 +133,8 @@ TYPE(EFragment)
 TYPE(EImportFinish)
 #include "mds/events/EImportStart.h"
 TYPE(EImportStart)
+#include "mds/events/EMetaBlob.h"
+TYPE(EMetaBlob::fullbit)
 
 #ifdef WITH_RADOSGW
 
