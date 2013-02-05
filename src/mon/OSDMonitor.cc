c7847c3433ff2e4d8760b1c6a318cf8acd373f67
Sage Weil <sage@inktank.com>
2013-02-05 14:31:10 -0800

    mon: 'osd crush rules list|ls'
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -1996,6 +1996,33 @@ bool OSDMonitor::preprocess_command(MMonCommand *m)
       ss << "listed " << osdmap.blacklist.size() << " entries";
       r = 0;
     }
+    else if (m->cmd.size() >= 4 && m->cmd[1] == "crush" && m->cmd[2] == "rule" && (m->cmd[3] == "list" ||
+										   m->cmd[3] == "ls")) {
+      JSONFormatter jf(true);
+      jf.open_array_section("rules");
+      for (int rule = 0; rule < osdmap.crush->get_max_rules(); rule++) {
+	if (!osdmap.crush->rule_exists(rule))
+	  continue;
+	jf.dump_string("name", osdmap.crush->get_rule_name(rule));
+      }
+      jf.close_section();
+      ostringstream rs;
+      jf.flush(rs);
+      rs << "\n";
+      rdata.append(rs.str());
+      r = 0;
+    }
+    else if (m->cmd.size() >= 4 && m->cmd[1] == "crush" && m->cmd[2] == "rule" && m->cmd[3] == "dump") {
+      JSONFormatter jf(true);
+      jf.open_array_section("rules");
+      osdmap.crush->dump_rules(&jf);
+      jf.close_section();
+      ostringstream rs;
+      jf.flush(rs);
+      rs << "\n";
+      rdata.append(rs.str());
+      r = 0;
+    }
     else if (m->cmd.size() == 3 && m->cmd[1] == "crush" && m->cmd[2] == "dump") {
       JSONFormatter jf(true);
       jf.open_object_section("crush_map");
