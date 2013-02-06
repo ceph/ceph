01e46804f297d485ff42f16029d8af173ff292a1
Greg Farnum <greg@inktank.com>
2013-02-06 13:32:53 -0800

    mds: add "mds traceless replies" debug option
    This option specifies (in the range 0-1) the percentage of modifying
    operations that should be responded to without including a trace
    for the dentries.
    In order to implement it, we add a "modify" parameter to
    set_trace_dist(), and the callers fill that with
    mdr->req->may_write().
    
    Signed-off-by: Greg Farnum <greg@inktank.com>
    Reviewed-by: Sam Lang <sam.lang@inktank.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -276,6 +276,8 @@ OPTION(mds_kill_export_at, OPT_INT, 0)
 OPTION(mds_kill_import_at, OPT_INT, 0)
 OPTION(mds_kill_link_at, OPT_INT, 0)
 OPTION(mds_kill_rename_at, OPT_INT, 0)
+OPTION(mds_traceless_replies, OPT_DOUBLE, 0) /* [0-1] amount of time MDS
+				should skip sending the client a trace */
 OPTION(mds_wipe_sessions, OPT_BOOL, 0)
 OPTION(mds_wipe_ino_prealloc, OPT_BOOL, 0)
 OPTION(mds_skip_ino, OPT_INT, 0)
