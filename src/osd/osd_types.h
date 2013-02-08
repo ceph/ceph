51d55999295bdb10add3715142e3e60a467452ec
David Zafman <david.zafman@inktank.com>
2013-02-08 14:43:50 -0800

    osd: move pg log into leveldb
    log from wip-pginfo
    Fix bugs in PG::read_log() handling
    Eliminate compiler warnings
    
    Feature #4075: osd: move pg log into leveldb
    
    Signed-off-by: Sage Weil <sage@inktank.com>
    Signed-off-by: David Zafman <david.zafman@inktank.com>

--- src/osd/osd_types.h
+++ src/osd/osd_types.h
@@ -1318,6 +1318,10 @@ struct pg_log_entry_t {
     return reqid != osd_reqid_t() && (op == MODIFY || op == DELETE);
   }
 
+  string get_key_name() const;
+  void encode_with_checksum(bufferlist& bl) const;
+  void decode_with_checksum(bufferlist::iterator& p);
+
   void encode(bufferlist &bl) const;
   void decode(bufferlist::iterator &bl);
   void dump(Formatter *f) const;
