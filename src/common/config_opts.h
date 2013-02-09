84500c22e4e27cdd70e6a370278c253d517d7a4b
Wido den Hollander <wido@widodh.nl>
2013-02-09 20:55:48 +0100

    config: Add small note about default number of PGs
    It's still not clear to end users this should go into the
    mon or global section of ceph.conf
    
    Until this gets resolved document it here as well for the people
    who look up their settings in the source code.
    
    Signed-off-by: Wido den Hollander <wido@42on.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -319,8 +319,8 @@ OPTION(osd_max_rep, OPT_INT, 10)
 OPTION(osd_pool_default_crush_rule, OPT_INT, 0)
 OPTION(osd_pool_default_size, OPT_INT, 2)
 OPTION(osd_pool_default_min_size, OPT_INT, 0)  // 0 means no specific default; ceph will use size-size/2
-OPTION(osd_pool_default_pg_num, OPT_INT, 8)
-OPTION(osd_pool_default_pgp_num, OPT_INT, 8)
+OPTION(osd_pool_default_pg_num, OPT_INT, 8) // number of PGs for new pools. Configure in global or mon section of ceph.conf
+OPTION(osd_pool_default_pgp_num, OPT_INT, 8) // number of PGs for placement purposes. Should be equal to pg_num
 OPTION(osd_map_dedup, OPT_BOOL, true)
 OPTION(osd_map_cache_size, OPT_INT, 500)
 OPTION(osd_map_message_max, OPT_INT, 100)  // max maps per MOSDMap message
