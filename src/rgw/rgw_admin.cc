f0878521dda7b14a6b646f5167b118ead6433b40
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2013-02-13 15:45:27 +0100

    rgw/rgw_admin.cc: use empty() instead of size() to check for emptiness
    Use empty() since it should be prefered as it has, following the
    standard, a constant time complexity regardless of the containter
    type. The same is not guaranteed for size().
    
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -750,7 +750,7 @@ static int remove_bucket(RGWRados *store, rgw_bucket& bucket, bool delete_childr
     if (ret < 0)
       return ret;
 
-    while (objs.size() > 0) {
+    while (!objs.empty()) {
       std::vector<RGWObjEnt>::iterator it = objs.begin();
       for (it = objs.begin(); it != objs.end(); it++) {
         ret = remove_object(store, bucket, (*it).name);
@@ -945,7 +945,7 @@ int main(int argc, char **argv)
     }
   }
 
-  if (args.size() == 0) {
+  if (args.empty()) {
     return usage();
   }
   else {
@@ -1558,7 +1558,7 @@ next:
     if (rgw_read_user_buckets(store, user_id, buckets, false) >= 0) {
       map<string, RGWBucketEnt>& m = buckets.get_buckets();
 
-      if (m.size() > 0 && purge_data) {
+      if (!m.empty() && purge_data) {
         for (std::map<string, RGWBucketEnt>::iterator it = m.begin(); it != m.end(); it++) {
           ret = remove_bucket(store, ((*it).second).bucket, true);
 
@@ -1567,7 +1567,7 @@ next:
         }
       }
 
-      if (m.size() > 0 && !purge_data) {
+      if (!m.empty() && !purge_data) {
         cerr << "ERROR: specify --purge-data to remove a user with a non-empty bucket list" << std::endl;
         return 1;
       }
