ceph/ceph

commit 7ae919770c4d162a1bc65fa05211361d139e6c36

libcephfs: fix for #4068
If client->init() fails in mount, then client->shutdown()
should not be called. This patch uses a bool to ensure
that shutdown is only called if init() succeeds.

Signed-off-by: Joe Buck <jbbuck@gmail.com>
Reviewed-by: Sam Lang <sam.lang@inktank.com>
Reviewed-by: Sage Weil <sage@inktank.com>

src/libcephfs.cc
@@ -37,6 +37,7 @@ public:
   ceph_mount_info(uint64_t msgr_nonce_, CephContext *cct_)
     : msgr_nonce(msgr_nonce_),
       mounted(false),
+      inited(false),
       client(NULL),
       monclient(NULL),
       messenger(NULL),
@@ -95,6 +96,8 @@ public:
     if (ret)
       goto fail;
 
+    inited = true;
+
     ret = client->mount(mount_root);
     if (ret)
       goto fail;
@@ -121,7 +124,7 @@ public:
       client->unmount();
       mounted = false;
     }
-    if (client) {
+    if (inited) {
       client->shutdown();
     }
     if (messenger) {
@@ -201,6 +204,7 @@ public:
 private:
   uint64_t msgr_nonce;
   bool mounted;
+  bool inited;
   Client *client;
   MonClient *monclient;
   Messenger *messenger;

commit c343e99da71d9a51aeb7c6f36665982413badb86

libcephfs:  Fix shutdown segfault
client->shutdown() should only be called once, but with
new inited flag, we were calling it twice.  This sets
the inited flag back to false after client->shutdown()
is called the first time.

Broken by 7ae919770c4d162a1bc65fa05211361d139e6c36.

Signed-off-by: Sam Lang <sam.lang@inktank.com>
Reviewed-by: Sage Weil <sage@inktank.com>

src/libcephfs.cc
@@ -126,6 +126,7 @@ public:
     }
     if (inited) {
       client->shutdown();
+      inited = false;
     }
     if (messenger) {
       messenger->shutdown();