e0cae41fe9bd649ee727ce2ef86594e00517c65a
Sage Weil <sage@inktank.com>
2013-02-15 09:20:14 -0800

    qa: pull qa stuff from ceph.com ceph.git mirror
    Signed-off-by: Sage Weil <sage@inktank.com>

--- qa/workunits/rbd/test_librbd_python.sh
+++ qa/workunits/rbd/test_librbd_python.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -ex
 
 CEPH_REF=${CEPH_REF:-master}
-wget -q https://raw.github.com/ceph/ceph/$CEPH_REF/src/test/pybind/test_rbd.py
+#wget -q https://raw.github.com/ceph/ceph/$CEPH_REF/src/test/pybind/test_rbd.py
+wget -q https://ceph.com/git/?p=ceph.git;a=blob_plain;hb=$CEPH_REF;f=src/test/pybind/test_rbd.py || \
+    wget -q https://ceph.com/git/?p=ceph.git;a=blob_plain;hb=ref/heads/$CEPH_REF;f=src/test/pybind/test_rbd.py
 nosetests -v -e '.*test_remove_with_watcher' test_rbd
 exit 0
