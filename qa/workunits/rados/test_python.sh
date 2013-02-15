b7950b5495a7ba407684abea4aecf0634077ce37
Sage Weil <sage@inktank.com>
2013-02-15 09:20:14 -0800

    qa: pull qa stuff from ceph.com ceph.git mirror
    Signed-off-by: Sage Weil <sage@inktank.com>

--- qa/workunits/rados/test_python.sh
+++ qa/workunits/rados/test_python.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -ex
 
 CEPH_REF=${CEPH_REF:-master}
-wget -q https://raw.github.com/ceph/ceph/$CEPH_REF/src/test/pybind/test_rados.py
+#wget -q https://raw.github.com/ceph/ceph/$CEPH_REF/src/test/pybind/test_rados.py
+wget -q https://ceph.com/git/?p=ceph.git;a=blob_plain;hb=$CEPH_REF;f=src/test/pybind/test_rados.py || \
+    wget -q https://ceph.com/git/?p=ceph.git;a=blob_plain;hb=ref/heads/$CEPH_REF;f=src/test/pybind/test_rados.py
 nosetests -v test_rados
 exit 0
