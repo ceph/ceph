c6cb9d7c6f090ab536d3ed51b7652b3eaf13601c
Sage Weil <sage@inktank.com>
2013-02-19 15:54:07 -0800

    qa: mon/pool_ops.sh: fix pool tests
    The '! command' doesn't fail properly, even with -e, in bash (wtf!).
    
    Also, the last pool deletion command succeeds because the pool
    '--yes-i-really-really-mean-it' doesn't exist.  So drop that test.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- qa/workunits/mon/pool_ops.sh
+++ qa/workunits/mon/pool_ops.sh
@@ -8,13 +8,11 @@ ceph osd pool create fooo 123
 ceph osd pool create foo 123 # idempotent
 
 # should fail due to safety interlock
-! ceph osd pool delete foo
-! ceph osd pool delete foo foo
-! ceph osd pool delete foo foo --force
-! ceph osd pool delete foo fooo --yes-i-really-mean-it
-! ceph osd pool delete foo --yes-i-really-mean-it foo
-! ceph osd pool delete --yes-i-really-mean-it foo foo
-
+ceph osd pool delete foo                               && exit 1 || true
+ceph osd pool delete foo foo                           && exit 1 || true
+ceph osd pool delete foo foo --force                   && exit 1 || true
+ceph osd pool delete foo fooo --yes-i-really-mean-it   && exit 1 || true
+ceph osd pool delete foo --yes-i-really-mean-it foo    && exit 1 || true
 
 ceph osd pool delete fooo fooo --yes-i-really-really-mean-it
 ceph osd pool delete foo foo --yes-i-really-really-mean-it
@@ -25,7 +23,7 @@ ceph osd pool delete fooo fooo --yes-i-really-really-mean-it
 ceph osd pool delete fooo fooo --yes-i-really-really-mean-it
 
 # non-existent pool
-! ceph osd pool delete fuggg fuggg --yes-i-really-really-mean-it
+ceph osd pool delete fuggg fuggg --yes-i-really-really-mean-it     && exit 1 || true
 
 echo OK
 
