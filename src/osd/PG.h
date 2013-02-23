0ba8db6b664205348d5499937759916eac0997bf
Sage Weil <sage@inktank.com>
2013-02-22 16:48:02 -0800

    osd: allow pg log trim during (non-classic) scrub
    Chunky (and deep) scrub do not care about PG log trimming.  Classic scrub
    still does.
    
    Deep scrub can take a long time, so not trimming the log during that period
    may eat lots of RAM; avoid that!
    
    Might fix: #4179
    Signed-off-by: Sage Weil <sage@inktank.com>
    Reviewed-by: Samuel Just <sam.just@inktank.com>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -834,6 +834,7 @@ public:
       block_writes(false), active(false), queue_snap_trim(false),
       waiting_on(0), errors(0), fixed(0), active_rep_scrub(0),
       must_scrub(false), must_deep_scrub(false), must_repair(false),
+      classic(false),
       finalizing(false), is_chunky(false), state(INACTIVE),
       deep(false)
     {
@@ -869,6 +870,7 @@ public:
     map<hobject_t, pair<ScrubMap::object, int> > authoritative;
 
     // classic scrub
+    bool classic;
     bool finalizing;
 
     // chunky scrub
@@ -940,6 +942,7 @@ public:
 
     // clear all state
     void reset() {
+      classic = false;
       finalizing = false;
       block_writes = false;
       active = false;
