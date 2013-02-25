c11468d6bdaaae530713252e8bf755628cedd963
Josh Durgin <josh.durgin@inktank.com>
2013-02-25 15:59:48 -0800

    qa: enable watch-notify dependent test
    This works now that watch-notify has been reworked a bit.
    
    Signed-off-by: Josh Durgin <josh.durgin@inktank.com>

--- qa/workunits/rbd/test_librbd_python.sh
+++ qa/workunits/rbd/test_librbd_python.sh
@@ -4,5 +4,5 @@ CEPH_REF=${CEPH_REF:-master}
 #wget -q https://raw.github.com/ceph/ceph/$CEPH_REF/src/test/pybind/test_rbd.py
 wget -O test_rbd.py "https://ceph.com/git/?p=ceph.git;a=blob_plain;hb=$CEPH_REF;f=src/test/pybind/test_rbd.py" || \
     wget -O test_rbd.py "https://ceph.com/git/?p=ceph.git;a=blob_plain;hb=ref/heads/$CEPH_REF;f=src/test/pybind/test_rbd.py"
-nosetests -v -e '.*test_remove_with_watcher' test_rbd
+nosetests -v test_rbd
 exit 0
