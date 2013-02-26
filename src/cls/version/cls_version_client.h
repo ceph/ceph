9ecfad5d1fd6231f42aff6965d99401992e621ef
Yehuda Sadeh <yehuda@inktank.com>
2013-02-26 14:53:49 -0800

    cls_version: create a new objclass
    New objclass to track and modify objects versions.
    
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

---
+++ src/cls/version/cls_version_client.h
@@ -0,0 +1,25 @@
+#ifndef CEPH_CLS_VERSION_CLIENT_H
+#define CEPH_CLS_VERSION_CLIENT_H
+
+#include "include/types.h"
+#include "include/rados/librados.hpp"
+
+/*
+ * version objclass
+ */
+
+void cls_version_set(librados::ObjectWriteOperation& op, obj_version& ver);
+
+/* increase anyway */
+void cls_version_inc(librados::ObjectWriteOperation& op);
+
+/* conditional increase, return -EAGAIN if condition fails */
+void cls_version_inc(librados::ObjectWriteOperation& op, obj_version& ver, VersionCond cond);
+
+void cls_version_read(librados::ObjectReadOperation& op, obj_version *objv);
+
+int cls_version_read(librados::IoCtx& io_ctx, string& oid, obj_version *ver);
+
+void cls_version_check(librados::ObjectOperation& op, obj_version& ver, VersionCond cond);
+
+#endif
