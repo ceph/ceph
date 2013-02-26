bf10e5e948856129551bc571d6a77aeedc40141f
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2013-02-26 15:54:19 +0100

    CephxProtocol.cc: catch error by reference
    Follow 'Throw by value, catch by reference' to void copying and
    potential slicing the exception.
    
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

--- src/auth/cephx/CephxProtocol.h
+++ src/auth/cephx/CephxProtocol.h
@@ -474,7 +474,7 @@ int decode_decrypt(CephContext *cct, T& t, const CryptoKey& key,
     ::decode(bl_enc, iter);
     decode_decrypt_enc_bl(cct, t, key, bl_enc, error);
   }
-  catch (buffer::error e) {
+  catch (buffer::error &e) {
     error = "error decoding block for decryption";
   }
   if (!error.empty())
