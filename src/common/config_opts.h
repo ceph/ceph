1e2864a0209ad5daa4a7cd58b6e7cbc9f46d3e94
Sage Weil <sage@inktank.com>
2013-03-07 10:31:27 -0800

    osd: increate default pg log size from 1000 -> 3000
    This reduces the probability that we will fail to detect a dup op.  See
    #4368.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -396,7 +396,7 @@ OPTION(osd_use_stale_snap, OPT_BOOL, false)
 OPTION(osd_rollback_to_cluster_snap, OPT_STR, "")
 OPTION(osd_default_notify_timeout, OPT_U32, 30) // default notify timeout in seconds
 OPTION(osd_kill_backfill_at, OPT_INT, 0)
-OPTION(osd_min_pg_log_entries, OPT_U32, 1000)  // number of entries to keep in the pg log when trimming it
+OPTION(osd_min_pg_log_entries, OPT_U32, 3000)  // number of entries to keep in the pg log when trimming it
 OPTION(osd_max_pg_log_entries, OPT_U32, 10000) // max entries, say when degraded, before we trim
 OPTION(osd_op_complaint_time, OPT_FLOAT, 30) // how many seconds old makes an op complaint-worthy
 OPTION(osd_command_max_records, OPT_INT, 256)
