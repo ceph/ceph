679bcd01c2a8fd50f63181897db348688f6d0d59
Sage Weil <sage@inktank.com>
2013-03-12 19:53:46 -0700

    mds: log session resets more cleanly
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/mds/MDS.cc
+++ src/mds/MDS.cc
@@ -2009,7 +2009,7 @@ bool MDS::_dispatch(Message *m)
 void MDS::ms_handle_connect(Connection *con) 
 {
   Mutex::Locker l(mds_lock);
-  dout(0) << "ms_handle_connect on " << con->get_peer_addr() << dendl;
+  dout(5) << "ms_handle_connect on " << con->get_peer_addr() << dendl;
   if (want_state == CEPH_MDS_STATE_DNE)
     return;
   objecter->ms_handle_connect(con);
@@ -2018,7 +2018,7 @@ void MDS::ms_handle_connect(Connection *con)
 bool MDS::ms_handle_reset(Connection *con) 
 {
   Mutex::Locker l(mds_lock);
-  dout(0) << "ms_handle_reset on " << con->get_peer_addr() << dendl;
+  dout(5) << "ms_handle_reset on " << con->get_peer_addr() << dendl;
   if (want_state == CEPH_MDS_STATE_DNE)
     return false;
 
@@ -2028,6 +2028,7 @@ bool MDS::ms_handle_reset(Connection *con)
     Session *session = static_cast<Session *>(con->get_priv());
     if (session) {
       if (session->is_closed()) {
+	dout(3) << "ms_handle_reset closing connection for session " << session->info.inst << dendl;
 	messenger->mark_down(con);
 	sessionmap.remove_session(session);
       }
@@ -2043,7 +2044,7 @@ bool MDS::ms_handle_reset(Connection *con)
 void MDS::ms_handle_remote_reset(Connection *con) 
 {
   Mutex::Locker l(mds_lock);
-  dout(0) << "ms_handle_remote_reset on " << con->get_peer_addr() << dendl;
+  dout(5) << "ms_handle_remote_reset on " << con->get_peer_addr() << dendl;
   if (want_state == CEPH_MDS_STATE_DNE)
     return;
   switch (con->get_peer_type()) {
@@ -2055,6 +2056,7 @@ void MDS::ms_handle_remote_reset(Connection *con)
     Session *session = static_cast<Session *>(con->get_priv());
     if (session) {
       if (session->is_closed()) {
+	dout(3) << "ms_handle_remote_reset closing connection for session " << session->info.inst << dendl;
 	messenger->mark_down(con);
 	sessionmap.remove_session(session);
       }
