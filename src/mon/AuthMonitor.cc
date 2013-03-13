702636b1189bd6ded538946e4f11030e7a438f52
Sage Weil <sage@inktank.com>
2013-03-13 13:13:12 -0700

    mon: increase max_global_id before we hit the wall
    Bump the max before we run out of IDs to allocate.  This avoids a stall in
    authentication every N new clients.
    
    Signed-off-by: Sage Weil <sage@inktank.com>
    Reviewed-by: Joao Luis <joao.luis@inktank.com>

--- src/mon/AuthMonitor.cc
+++ src/mon/AuthMonitor.cc
@@ -343,7 +343,7 @@ uint64_t AuthMonitor::assign_global_id(MAuth *m, bool should_increase_max)
   bool return_next = (next_global_id <= max_global_id);
 
   // bump the max?
-  while (next_global_id >= max_global_id) {
+  while (next_global_id >= max_global_id - g_conf->mon_globalid_prealloc / 2) {
     increase_max_global_id();
   }
 
