bac2c199a987dd9efe0f3412cf407c7253d41a35
Joao Eduardo Luis <joao.luis@inktank.com>
2013-03-17 18:40:58 +0000

    mon: HealthMonitor: Keep track of monitor cluster's health
    The HealthMonitor builds upon the QuorumService interface, and should be
    used to keep track of all and any relevant information about the monitor
    cluster (maybe even about all the cluster if need be).
    
    This patch also introduces the HealthService interface, used to define
    a HealthMonitor service, responsible for dispatching 'MMonHealth' messages
    (the QuorumService interface dispatches generic 'Message').
    
    Based on the HealthService interface, we introduce the DataHealthService
    class, a service that will track disk space consumption by the monitors,
    warn when a given threshold is crossed, and gracefully shutdown the monitor
    if disk space usage hits critical levels that might affect the correct
    monitor behavior.
    
    
    Signed-off-by: Joao Eduardo Luis <joao.luis@inktank.com>

---
+++ src/mon/HealthService.h
@@ -0,0 +1,53 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2013 Inktank, Inc
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+#ifndef CEPH_MON_HEALTH_SERVICE_H
+#define CEPH_MON_HEALTH_SERVICE_H
+
+#include <boost/intrusive_ptr.hpp>
+// Because intusive_ptr clobbers our assert...
+#include "include/assert.h"
+
+#include "mon/Monitor.h"
+#include "mon/QuorumService.h"
+
+#include "messages/MMonHealth.h"
+
+#include "common/config.h"
+
+struct HealthService : public QuorumService
+{
+  static const int SERVICE_HEALTH_DATA              = 0x01;
+  static const int SERVICE_HEALTH_TIMECHECK         = 0x02;
+
+  HealthService(Monitor *m, uint32_t flags) : QuorumService(m, flags) { }
+  virtual ~HealthService() { }
+
+  virtual bool service_dispatch(Message *m) {
+    return service_dispatch(static_cast<MMonHealth*>(m));
+  }
+
+  virtual bool service_dispatch(MMonHealth *m) = 0;
+
+public:
+  HealthService *get() {
+    return static_cast<HealthService *>(RefCountedObject::get());
+  }
+  virtual void get_health(Formatter *f,
+                          list<pair<health_status_t,string> > *detail) = 0;
+  virtual int get_type() = 0;
+  virtual string get_name() const = 0;
+};
+typedef boost::intrusive_ptr<HealthService> HealthServiceRef;
+
+#endif // CEPH_MON_HEALTH_SERVICE_H
