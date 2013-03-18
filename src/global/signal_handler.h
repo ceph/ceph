ee2b91768148e9c5b462750cb2bec77702fc54ae
Sage Weil <sage@inktank.com>
2013-03-18 13:10:36 -0700

    signal_handler: add func to queue an async signal
    For example, queue a SIGINT without actually sending the signal via the
    OS.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/global/signal_handler.h
+++ src/global/signal_handler.h
@@ -35,6 +35,9 @@ void init_async_signal_handler();
 /// shutdown async signal handler framework
 void shutdown_async_signal_handler();
 
+/// queue an async signal
+void queue_async_signal(int signum);
+
 /// install a safe, async, callback for the given signal
 void register_async_signal_handler(int signum, signal_handler_t handler);
 void register_async_signal_handler_oneshot(int signum, signal_handler_t handler);
