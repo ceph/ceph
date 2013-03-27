90341e4ddb200f0b9d8999a7efe92bac5b60c0cd
Josh Durgin <josh.durgin@inktank.com>
2013-03-27 15:48:17 -0700

    librbd: increment version for new functions is_complete() and aio_flush()
    This done in a separate commit since the increased version number
    should not be backported.
    
    Signed-off-by: Josh Durgin <josh.durgin@inktank.com>

--- src/include/rbd/librbd.h
+++ src/include/rbd/librbd.h
@@ -31,7 +31,7 @@ extern "C" {
 
 #define LIBRBD_VER_MAJOR 0
 #define LIBRBD_VER_MINOR 1
-#define LIBRBD_VER_EXTRA 7
+#define LIBRBD_VER_EXTRA 8
 
 #define LIBRBD_VERSION(maj, min, extra) ((maj << 16) + (min << 8) + extra)
 
