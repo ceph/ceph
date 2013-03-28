a14894762c5a7b3ab94c3a69d9fd494352514836
Sam Lang <sam.lang@inktank.com>
2013-03-28 11:02:49 -0500

    client: log inode when waiting on max size
    Logging the specific inode that the get_caps()
    is waiting for helps during debugging of stuck waiters.
    
    Signed-off-by: Sam Lang <sam.lang@inktank.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -2230,7 +2230,7 @@ int Client::get_caps(Inode *in, int need, int want, int *phave, loff_t endoff)
     }
     
     if (endoff >= 0 && endoff > (loff_t)in->max_size) {
-      ldout(cct, 10) << "waiting on max_size, endoff " << endoff << " max_size " << in->max_size << dendl;
+      ldout(cct, 10) << "waiting on max_size, endoff " << endoff << " max_size " << in->max_size << " on " << *in << dendl;
     } else if (!in->cap_snaps.empty() && in->cap_snaps.rbegin()->second->writing) {
       ldout(cct, 10) << "waiting on cap_snap write to complete" << dendl;
     } else {
