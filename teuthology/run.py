9f46f47b6b2ac7dec4c36c80d14e297f10ab2df3
Sage Weil <sage@inktank.com>
2013-03-29 12:16:39 -0700

    run: clean up machine_type thing
    Signed-off-by: Sage Weil <sage@inktank.com>

--- teuthology/run.py
+++ teuthology/run.py
@@ -103,9 +103,8 @@ def main():
 
     machine_type = ctx.machine_type
     if machine_type is None:
-        machine_type = ctx.config.get('machine-type')
-        if machine_type is None:
-            machine_type = ctx.config.get('machine_type', 'plana')
+        fallback_default = ctx.config.get('machine_type', 'plana')
+        machine_type = ctx.config.get('machine-type', fallback_default)
 
     if ctx.block:
         assert ctx.lock, \
