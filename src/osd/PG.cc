431c4c625a09b762a7d7d94dbc311d016160d09d
Samuel Just <sam.just@inktank.com>
2013-03-29 16:30:14 -0700

    PG: don't compare auth with itself
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -4337,6 +4337,8 @@ void PG::_compare_scrubmaps(const map<int,ScrubMap*> &maps,
     set<int> cur_missing;
     set<int> cur_inconsistent;
     for (j = maps.begin(); j != maps.end(); ++j) {
+      if (j == auth)
+	continue;
       if (j->second->objects.count(*k)) {
 	// Compare
 	stringstream ss;
