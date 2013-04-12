ceph/ceph

commit fa2049ffbbac67df7173cbb9098346d20267aaf9

misc: Use job id and make short path for testdir
Nightlies run on teuthology currently use a testdir of
/home/ubuntu/cephtest, but this causes stale job errors occasionally
from the previous tests not getting properly cleaned up, which prevents
the nightlies from running successfully.

The misc.py get_testdir() function can specify a testdir that is
specific to the job, but previously the path was too long and would
cause separate job failures.

This patch does two things to resolve that.  First, it uses the job id
from the teuthology run if one exists.  This should be a relatively
short number that will identify the job run effectively.  Second,
if the job id isn't available, it creates a shortened form of the
job's name, for example the job name:

teuthology-2013-04-09_23:51:49-rgw-next-testing-basic

becomes:

te1304092351rntb

Signed-off-by: Sam Lang <sam.lang@inktank.com>

teuthology/misc.py
@@ -13,12 +13,15 @@ import yaml
 import json
 
 from teuthology import safepath
+from teuthology import lock
 from .orchestra import run
 
 log = logging.getLogger(__name__)
 
 import datetime
-stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+stamp = datetime.datetime.now().strftime("%y%m%d%H%M")
+global_jobid = None
+checked_jobid = False
 
 def get_testdir(ctx):
     if 'test_path' in ctx.teuthology_config:
@@ -26,17 +29,51 @@ def get_testdir(ctx):
 
     basedir = ctx.teuthology_config.get('base_test_dir', '/tmp/cephtest')
 
-    if hasattr(ctx, 'name') and ctx.name:
+    global global_jobid
+    global checked_jobid
+
+    # check if a jobid exists in the machine status for all our targets
+    # and if its the same jobid, use that as the subdir for the test
+    if not checked_jobid:
+        jobids = {}
+        for machine in ctx.config['targets'].iterkeys():
+            status = lock.get_status(ctx, machine)
+            jid = status['description'].split('/')[-1]
+            jobids[jid] = 1
+            if len(jobids) > 1:
+                break
+        if len(jobids) == 1:
+            # same job id on all machines, use that as the test subdir
+            (jobid,) = jobids.iterkeys()
+            global_jobid = jobid
+        checked_jobid = True
+
+    # the subdir is chosen using the priority:
+    # 1. jobid chosen by the teuthology beanstalk queue
+    # 2. run name specified by teuthology schedule
+    # 3. user@timestamp
+    if global_jobid:
+        log.debug('with jobid basedir: {b}'.format(b=str(global_jobid)))
+        return '{basedir}/{jobid}'.format(
+                    basedir=basedir,
+                    jobid=global_jobid,
+                    )
+    elif hasattr(ctx, 'name') and ctx.name:
         log.debug('with name basedir: {b}'.format(b=basedir))
+        # we need a short string to keep the path short
+        import re
+        m = re.match(r"(.*)-(.*)-(.*)-(.*)_(.*)-(.*)-(.*)-(.*)-(.*)", ctx.name)
+        (u, y, m, d, hms, s, c, k, f) = m.groups()
+        short = u[0:2] + y[2:3] + m[0:2] + d[0:2] + hms[0:2] + hms[3:5] + s[0] + c[0] + k[0] + f[0]
         return '{basedir}/{rundir}'.format(
                     basedir=basedir,
-                    rundir=ctx.name.replace(':','-'),  # : breaks the $PATH list
+                    rundir=short,
                     )
     else:
         log.debug('basedir: {b}'.format(b=basedir))
-        return '{basedir}/{user}-{stamp}'.format(
+        return '{basedir}/{user}{stamp}'.format(
                     basedir=basedir,
-                    user=get_user(),
+                    user=get_user()[0:2],
                     stamp=stamp)
 
 def get_testdir_base(ctx):

commit a6b84a5613b2e734c822f2b88b784463356dd4b0

lock: Fix import cycle breakage
fa2049f caused an import cycle between lock.py and misc.py.  Move the
needed functions from lock.py to lockstatus.py so that we can avoid the
import cycle.

Signed-off-by: Sam Lang <sam.lang@inktank.com>

teuthology/lock.py
@@ -1,5 +1,4 @@
 import argparse
-import httplib2
 import json
 import logging
 import subprocess
@@ -7,29 +6,17 @@ import urllib
 import yaml
 import re
 import collections
-import os
 import tempfile
 
+from teuthology import lockstatus as ls
 from teuthology import misc as teuthology
 
 log = logging.getLogger(__name__)
 
-def _lock_url(ctx):
-    return ctx.teuthology_config['lock_server']
-
-def send_request(method, url, body=None, headers=None):
-    http = httplib2.Http()
-    resp, content = http.request(url, method=method, body=body, headers=headers)
-    if resp.status == 200:
-        return (True, content, resp.status)
-    log.info("%s request to '%s' with body '%s' failed with response code %d",
-             method, url, body, resp.status)
-    return (False, None, resp.status)
-
 def lock_many(ctx, num, machinetype, user=None, description=None):
     if user is None:
         user = teuthology.get_user()
-    success, content, status = send_request('POST', _lock_url(ctx),
+    success, content, status = ls.send_request('POST', ls._lock_url(ctx),
                                     urllib.urlencode(dict(
                 user=user,
                 num=num,
@@ -49,7 +36,7 @@ def lock_many(ctx, num, machinetype, user=None, description=None):
 def lock(ctx, name, user=None):
     if user is None:
         user = teuthology.get_user()
-    success, _, _ = send_request('POST', _lock_url(ctx) + '/' + name,
+    success, _, _ = ls.send_request('POST', ls._lock_url(ctx) + '/' + name,
                               urllib.urlencode(dict(user=user)))
     if success:
         log.debug('locked %s as %s', name, user)
@@ -60,7 +47,7 @@ def lock(ctx, name, user=None):
 def unlock(ctx, name, user=None):
     if user is None:
         user = teuthology.get_user()
-    success, _ , _ = send_request('DELETE', _lock_url(ctx) + '/' + name + '?' + \
+    success, _ , _ = ls.send_request('DELETE', ls._lock_url(ctx) + '/' + name + '?' + \
                                   urllib.urlencode(dict(user=user)))
     if success:
         log.debug('unlocked %s', name)
@@ -68,14 +55,8 @@ def unlock(ctx, name, user=None):
         log.error('failed to unlock %s', name)
     return success
 
-def get_status(ctx, name):
-    success, content, _ = send_request('GET', _lock_url(ctx) + '/' + name)
-    if success:
-        return json.loads(content)
-    return None
-
 def list_locks(ctx):
-    success, content, _ = send_request('GET', _lock_url(ctx))
+    success, content, _ = ls.send_request('GET', ls._lock_url(ctx))
     if success:
         return json.loads(content)
     return None
@@ -90,7 +71,7 @@ def update_lock(ctx, name, description=None, status=None, sshpubkey=None):
         updated['sshpubkey'] = sshpubkey
 
     if updated:
-        success, _, _ = send_request('PUT', _lock_url(ctx) + '/' + name,
+        success, _, _ = ls.send_request('PUT', ls._lock_url(ctx) + '/' + name,
                                   body=urllib.urlencode(updated),
                                   headers={'Content-type': 'application/x-www-form-urlencoded'})
         return success
@@ -283,7 +264,7 @@ Lock, unlock, or query lock status of machines.
         assert ctx.desc is None, '--desc does nothing with --list'
 
         if machines:
-            statuses = [get_status(ctx, machine) for machine in machines]
+            statuses = [ls.get_status(ctx, machine) for machine in machines]
         else:
             statuses = list_locks(ctx)
         vmachines = []
@@ -295,7 +276,7 @@ Lock, unlock, or query lock status of machines.
         if vmachines:
             scan_for_locks(ctx, vmachines)
             if machines:
-                statuses = [get_status(ctx, machine) for machine in machines]
+                statuses = [ls.get_status(ctx, machine) for machine in machines]
             else:
                 statuses = list_locks(ctx)
         if statuses:
@@ -349,7 +330,7 @@ Lock, unlock, or query lock status of machines.
                     return ret
             else:
                 machines_to_update.append(machine)
-                status_info = get_status(ctx,machine)
+                status_info = ls.get_status(ctx,machine)
                 if status_info['vpshost']:
                     do_create(ctx, machine, status_info['vpshost'])
     elif ctx.unlock:
@@ -360,7 +341,7 @@ Lock, unlock, or query lock status of machines.
                     return ret
             else:
                 machines_to_update.append(machine)
-                status_info = get_status(ctx,machine)
+                status_info = ls.get_status(ctx,machine)
                 if status_info['vpshost']:
                     do_destroy(machine, status_info['vpshost'])
     elif ctx.num_to_lock:

teuthology/lockstatus.py
@@ -1,0 +1,25 @@
+import json
+import httplib2
+import logging
+
+log = logging.getLogger(__name__)
+
+def _lock_url(ctx):
+    return ctx.teuthology_config['lock_server']
+
+def send_request(method, url, body=None, headers=None):
+    http = httplib2.Http()
+    resp, content = http.request(url, method=method, body=body, headers=headers)
+    if resp.status == 200:
+        return (True, content, resp.status)
+    log.info("%s request to '%s' with body '%s' failed with response code %d",
+             method, url, body, resp.status)
+    return (False, None, resp.status)
+
+def get_status(ctx, name):
+    success, content, _ = send_request('GET', _lock_url(ctx) + '/' + name)
+    if success:
+        return json.loads(content)
+    return None
+
+

teuthology/misc.py
@@ -13,7 +13,7 @@ import yaml
 import json
 
 from teuthology import safepath
-from teuthology import lock
+from teuthology import lockstatus
 from .orchestra import run
 
 log = logging.getLogger(__name__)
@@ -37,7 +37,7 @@ def get_testdir(ctx):
     if not checked_jobid:
         jobids = {}
         for machine in ctx.config['targets'].iterkeys():
-            status = lock.get_status(ctx, machine)
+            status = lockstatus.get_status(ctx, machine)
             jid = status['description'].split('/')[-1]
             jobids[jid] = 1
             if len(jobids) > 1:

teuthology/task/internal.py
@@ -8,6 +8,7 @@ import yaml
 import re
 import subprocess
 
+from teuthology import lockstatus
 from teuthology import lock
 from teuthology import misc as teuthology
 from teuthology.parallel import parallel
@@ -124,7 +125,7 @@ def check_lock(ctx, config):
         return
     log.info('Checking locks...')
     for machine in ctx.config['targets'].iterkeys():
-        status = lock.get_status(ctx, machine)
+        status = lockstatus.get_status(ctx, machine)
         log.debug('machine status is %s', repr(status))
         assert status is not None, \
             'could not read lock status for {name}'.format(name=machine)