34bc65136ab9e03bf6e340a5f869b386ff353b98
Joao Eduardo Luis <joao.luis@inktank.com>
2013-04-18 16:45:07 +0100

    mon: PaxosService: fix trim criteria so to avoid constantly trimming
    Say a service establishes it will only keep 500 versions once a given
    condition X is true.  Now say that said condition X only becomes true
    after said service committing some 800 versions.
    
    Once we decide to trim, this service would trim all 300 surplus versions
    in one go.  After that, each committed version would also trim the
    previous version.
    
    Trimming an unbounded number of versions is not a good practice
    as it will generate bigger transactions (thus a greater workload on
    leveldb) and therefore bigger messages too.
    
    Constantly trimming versions implies more frequent accesses to leveldb,
    and keeping around a couple more versions won't hurt us in any significant
    way, so let us put off trimming unless we go over a predefined minimum.
    
    This patch adds two new options:
    
     paxos service trim min - minimum amount of versions to trigger a trim
                              (default: 30, 0 disables it)
     paxos service trim max - maximum amount of versions to trim during a
                              single proposal
                              (default: 50, 0 disables it)
    
    Signed-off-by: Joao Eduardo Luis <joao.luis@inktank.com>

--- src/mon/PaxosService.cc
+++ src/mon/PaxosService.cc
@@ -152,7 +152,6 @@ void PaxosService::propose_pending()
 
   if (should_trim()) {
     encode_trim(&t);
-    set_trim_to(0);
   }
 
   encode_pending(&t);
@@ -327,7 +326,19 @@ void PaxosService::encode_trim(MonitorDBStore::Transaction *t)
   if (first_committed >= trim_to)
     return;
 
-  trim(t, first_committed, trim_to);
-  put_first_committed(t, trim_to);
+  version_t trim_to_max = trim_to;
+  if ((g_conf->paxos_service_trim_max > 0)
+      && (trim_to - first_committed > (size_t)g_conf->paxos_service_trim_max)) {
+    trim_to_max = first_committed + g_conf->paxos_service_trim_max;
+  }
+
+  dout(10) << __func__ << " trimming versions " << first_committed
+           << " to " << trim_to_max << dendl;
+
+  trim(t, first_committed, trim_to_max);
+  put_first_committed(t, trim_to_max);
+
+  if (trim_to_max == trim_to)
+    set_trim_to(0);
 }
 
