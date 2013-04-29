dc4ae8e31e9c268c551d34062a22a8a3a9de6943
David Zafman <david.zafman@inktank.com>
2013-04-29 14:36:18 -0700

    osd: Rename members and methods related to stat publish
    pg_stats_lock to pg_stats_publish_lock
    pg_stats_valid to pg_stats_publish_valid
    pg_stats_stable to pg_stats_publish
    update_stats() to publish_stats_to_osd()
    clear_stats() to clear_publish_stats()
    
    Signed-off-by: David Zafman <david.zafman@inktank.com>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -716,16 +716,16 @@ protected:
   void requeue_object_waiters(map<hobject_t, list<OpRequestRef> >& m);
   void requeue_ops(list<OpRequestRef> &l);
 
-  // stats
-  Mutex pg_stats_lock;
-  bool pg_stats_valid;
-  pg_stat_t pg_stats_stable;
+  // publish stats
+  Mutex pg_stats_publish_lock;
+  bool pg_stats_publish_valid;
+  pg_stat_t pg_stats_publish;
 
   // for ordering writes
   std::tr1::shared_ptr<ObjectStore::Sequencer> osr;
 
-  void update_stats();
-  void clear_stats();
+  void publish_stats_to_osd();
+  void clear_publish_stats();
 
 public:
   void clear_primary_state();
