0522556928f7abef27e1d4defc369b6d40fc10b7
Sage Weil <sage@inktank.com>
2013-04-30 10:26:24 -0700

    mon: change leveldb block size to 64K
    #leveldb on freenode says > 2MB is nonsense (it might explain the weird
    behavior we saw).  Riak tuning guide suggests 256KB for large data block
    environments.  Default is 8KB.  64KB seems sane for us.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -182,7 +182,7 @@ OPTION(mon_sync_provider_kill_at, OPT_INT, 0) // kill the sync provider at a spe
 OPTION(mon_sync_requester_kill_at, OPT_INT, 0) // kill the sync requester at a specific point in the work flow
 OPTION(mon_leveldb_write_buffer_size, OPT_U64, 32*1024*1024) // monitor's leveldb write buffer size
 OPTION(mon_leveldb_cache_size, OPT_U64, 0) // monitor's leveldb cache size
-OPTION(mon_leveldb_block_size, OPT_U64, 4*1024*1024) // monitor's leveldb block size
+OPTION(mon_leveldb_block_size, OPT_U64, 64*1024) // monitor's leveldb block size
 OPTION(mon_leveldb_bloom_size, OPT_INT, 0) // monitor's leveldb bloom bits per entry
 OPTION(mon_leveldb_max_open_files, OPT_INT, 0) // monitor's leveldb max open files
 OPTION(mon_leveldb_compression, OPT_BOOL, false) // monitor's leveldb uses compression
