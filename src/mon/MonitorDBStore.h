3b94f03ec58abe3d7a6d0359ff9b4d75826f3777
Samuel Just <sam.just@inktank.com>
2013-05-02 14:13:07 -0700

    mon: dump MonitorDBStore transactions to file
    Signed-off-by: Samuel Just <sam.just@inktank.com>
    (cherry picked from commit 797089ef082b99910eebfd9454c03d1f027c93bb)

--- src/mon/MonitorDBStore.h
+++ src/mon/MonitorDBStore.h
@@ -30,6 +30,8 @@
 class MonitorDBStore
 {
   boost::scoped_ptr<LevelDBStore> db;
+  bool do_dump;
+  int dump_fd;
 
  public:
 
@@ -184,6 +186,12 @@ class MonitorDBStore
 
   int apply_transaction(MonitorDBStore::Transaction& t) {
     KeyValueDB::Transaction dbt = db->get_transaction();
+
+    if (do_dump) {
+      bufferlist bl;
+      t.encode(bl);
+      bl.write_fd(dump_fd);
+    }
 
     list<string> compact_prefixes;
     for (list<Op>::iterator it = t.ops.begin(); it != t.ops.end(); ++it) {
@@ -486,7 +494,8 @@ class MonitorDBStore
     db->compact_prefix(prefix);
   }
 
-  MonitorDBStore(const string& path) : db(0) {
+  MonitorDBStore(const string& path) :
+    db(0), do_dump(false), dump_fd(-1) {
     string::const_reverse_iterator rit;
     int pos = 0;
     for (rit = path.rbegin(); rit != path.rend(); ++rit, ++pos) {
@@ -512,11 +521,27 @@ class MonitorDBStore
     db->options.max_open_files = g_conf->mon_leveldb_max_open_files;
     db->options.paranoid_checks = g_conf->mon_leveldb_paranoid;
     db->options.log_file = g_conf->mon_leveldb_log;
+
+    if (g_conf->mon_debug_dump_transactions) {
+      do_dump = true;
+      dump_fd = ::open(
+	g_conf->mon_debug_dump_location.c_str(),
+	O_CREAT|O_APPEND|O_WRONLY, 0644);
+      if (!dump_fd) {
+	dump_fd = -errno;
+	derr << "Could not open log file, got "
+	     << cpp_strerror(dump_fd) << dendl;
+      }
+    }
   }
-  MonitorDBStore(LevelDBStore *db_ptr) {
+  MonitorDBStore(LevelDBStore *db_ptr) :
+    db(0), do_dump(false), dump_fd(-1) {
     db.reset(db_ptr);
   }
-  ~MonitorDBStore() { }
+  ~MonitorDBStore() {
+    if (do_dump)
+      ::close(dump_fd);
+  }
 
 };
 
