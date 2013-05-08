c0c2e052f101d3a2a6dbb9fb717ab9c3e6474c45
Sage Weil <sage@inktank.com>
2013-05-08 11:08:16 -0700

    mon: set MonitorDBStore options on open
    So both ctors set the options.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/mon/MonitorDBStore.h
+++ src/mon/MonitorDBStore.h
@@ -481,6 +481,14 @@ class MonitorDBStore
   }
 
   int open(ostream &out) {
+    db->options.write_buffer_size = g_conf->mon_leveldb_write_buffer_size;
+    db->options.cache_size = g_conf->mon_leveldb_cache_size;
+    db->options.block_size = g_conf->mon_leveldb_block_size;
+    db->options.bloom_size = g_conf->mon_leveldb_bloom_size;
+    db->options.compression_enabled = g_conf->mon_leveldb_compression;
+    db->options.max_open_files = g_conf->mon_leveldb_max_open_files;
+    db->options.paranoid_checks = g_conf->mon_leveldb_paranoid;
+    db->options.log_file = g_conf->mon_leveldb_log;
     return db->open(out);
   }
 
@@ -515,14 +523,6 @@ class MonitorDBStore
       assert(0 != "MonitorDBStore: error initializing level db back storage");
     }
     db.reset(db_ptr);
-    db->options.write_buffer_size = g_conf->mon_leveldb_write_buffer_size;
-    db->options.cache_size = g_conf->mon_leveldb_cache_size;
-    db->options.block_size = g_conf->mon_leveldb_block_size;
-    db->options.bloom_size = g_conf->mon_leveldb_bloom_size;
-    db->options.compression_enabled = g_conf->mon_leveldb_compression;
-    db->options.max_open_files = g_conf->mon_leveldb_max_open_files;
-    db->options.paranoid_checks = g_conf->mon_leveldb_paranoid;
-    db->options.log_file = g_conf->mon_leveldb_log;
 
     if (g_conf->mon_debug_dump_transactions) {
       do_dump = true;
