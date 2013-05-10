6ece9185c8d9ea1b5e22fdc31c8477789c7d51c7
Dan Mick <dan.mick@inktank.com>
2013-05-09 17:09:18 -0700

    mon: add JSON command parsing framework
    Use json_spirit to read/parse JSON command input into a "cmdmap",
    which is a boost::variant typechecked discriminant union.
    This means that requests to read from the map must match the type
    given.  Types are string, bool, int64_t, double, and vector<string>,
    and that type is a "cmd_vartype"; a cmdmap is a map of name (string) to
    cmd_vartype.
    
    Also, an evolutionary deadend hack "build_fullcmd()", which reassembles
    certain commands from the cmdmap back into a string for the sole reason
    of running them through _allowed_command.  This is due to be replaced by
    a better allow mechanism very shortly.
    
    Signed-off-by: Dan Mick <dan.mick@inktank.com>

---
+++ src/common/cmdparse.h
@@ -0,0 +1,53 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*- 
+// vim: ts=8 sw=2 smarttab
+#include <string>
+#include <sstream>
+#include <map>
+#include <boost/variant.hpp>
+#include <vector>
+#include <stdexcept>
+#include "common/dout.h"
+
+typedef boost::variant<std::string, bool, int64_t, double, std::vector<std::string> > cmd_vartype;
+
+bool cmdmap_from_json(std::vector<std::string> cmd, std::map<std::string,
+		      cmd_vartype> *mapp, std::stringstream &ss);
+void build_fullcmd(string &prefix, map<string, cmd_vartype>&cmdmap,
+		   vector<string> *fullcmd);
+
+template <typename T>
+bool
+cmd_getval(CephContext *cct, std::map<std::string, cmd_vartype>& cmdmap, std::string k, T& val)
+{
+  // referencing a nonexistent key creates it, even as an rvalue;
+  // we don't want that behavior for get.
+  if (cmdmap.count(k)) {
+    try {
+      val = boost::get<T>(cmdmap[k]);
+      return true;
+    } catch (boost::bad_get) { 
+      std::ostringstream errstr;
+      errstr << "bad boost::get: key " << k << " is not type " << typeid(T).name();
+      lderr(cct) << errstr.str() << dendl;
+    }
+  }
+  // either not found or bad type, return false
+  return false;
+}
+
+// with default
+template <typename T>
+void
+cmd_getval(CephContext *cct, std::map<std::string, cmd_vartype>& cmdmap, std::string k, T& val,
+       T defval)
+{
+  if (!cmd_getval(cct, cmdmap, k, val))
+    val = defval;
+}
+
+template <typename T>
+void
+cmd_putval(CephContext *cct, std::map<std::string, cmd_vartype>& cmdmap, std::string k, T val)
+{
+  cmdmap[k] = val;
+}
