9c5a75a7eda6bb1b71bb2d17c01379091c10f69b
Dan Mick <dan.mick@inktank.com>
2013-05-09 17:00:17 -0700

    str_list: cleanup/enhancement
    - move #includes to str_list.h
    - remove redundant std::, add using for vector, set, list
    - add get_str_vec and get_str_set-no-delims
    
    Signed-off-by: Dan Mick <dan.mick@inktank.com>

--- src/include/str_list.h
+++ src/include/str_list.h
@@ -4,13 +4,22 @@
 #include <string>
 #include <list>
 #include <set>
+#include <vector>
 
 extern void get_str_list(const std::string& str,
 			 std::list<std::string>& str_list);
 extern void get_str_list(const std::string& str,
                          const char *delims,
 			 std::list<std::string>& str_list);
+extern void get_str_vec(const std::string& str,
+			 std::vector<std::string>& str_vec);
+extern void get_str_vec(const std::string& str,
+                         const char *delims,
+			 std::vector<std::string>& str_vec);
 extern void get_str_set(const std::string& str,
+			std::set<std::string>& str_list);
+extern void get_str_set(const std::string& str,
+                        const char *delims,
 			std::set<std::string>& str_list);
 
 
