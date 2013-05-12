62eb49f6ba46893851c79fdb94c7c5da58884819
Sage Weil <sage@inktank.com>
2013-05-11 17:07:14 -0700

    schedule_suite.sh: bump suite timeout from 6->8 hours
    This captures the current slow rbd tasks.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- schedule_suite.sh
+++ schedule_suite.sh
@@ -100,6 +100,6 @@ echo "name $name"
 ~/src/teuthology/virtualenv/bin/teuthology-suite -v $fn \
     --collections ~/src/ceph-qa-suite/suites/$suite/* \
     --email $email \
-    --timeout 21600 \
+    --timeout 28800 \
     --name $name \
     --branch $teuthology_branch
