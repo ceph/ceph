ab9b35184f1089ce990949b4dd813d606c525c8f
Josh Durgin <josh.durgin@inktank.com>
2013-05-16 15:19:46 -0700

    cls_rbd: make sure stripe_unit is not larger than object size
    Test a few other cases too.
    
    backport: cuttlefish, bobtail
    Signed-off-by: Josh Durgin <josh.durgin@inktank.com>

--- src/test/cls_rbd/test_cls_rbd.cc
+++ src/test/cls_rbd/test_cls_rbd.cc
@@ -906,6 +906,15 @@ TEST(cls_rbd, stripingv2)
   ASSERT_EQ(8192ull, su);
   ASSERT_EQ(456ull, sc);
 
+  // su must not be larger than an object
+  ASSERT_EQ(-EINVAL, set_stripe_unit_count(&ioctx, "bar", 1 << 23, 1));
+  // su must be a factor of object size
+  ASSERT_EQ(-EINVAL, set_stripe_unit_count(&ioctx, "bar", 511, 1));
+  // su and sc must be non-zero
+  ASSERT_EQ(-EINVAL, set_stripe_unit_count(&ioctx, "bar", 0, 1));
+  ASSERT_EQ(-EINVAL, set_stripe_unit_count(&ioctx, "bar", 1, 0));
+  ASSERT_EQ(-EINVAL, set_stripe_unit_count(&ioctx, "bar", 0, 0));
+
   ioctx.close();
   ASSERT_EQ(0, destroy_one_pool_pp(pool_name, rados));
 }
