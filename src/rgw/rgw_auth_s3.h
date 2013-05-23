f356070dbc1b5e792b47aaf2f22934568fce7174
Yehuda Sadeh <yehuda@inktank.com>
2013-05-23 12:29:42 -0700

    rgw: some code cleanup
    move s3 signing code to a different common file.
    
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

---
+++ src/rgw/rgw_auth_s3.h
@@ -0,0 +1,14 @@
+#ifndef CEPH_RGW_AUTH_S3_H
+#define CEPH_RGW_AUTH_S3_H
+
+
+#include "rgw_common.h"
+
+void rgw_create_s3_canonical_header(const char *method, const char *content_md5, const char *content_type, const char *date,
+                            map<string, string>& meta_map, const char *request_uri, map<string, string>& sub_resources,
+                            string& dest_str);
+int rgw_get_s3_header_digest(const string& auth_hdr, const string& key, string& dest);
+
+
+
+#endif
