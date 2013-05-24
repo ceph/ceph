22b5165b2aff1127dcf6ef1f0bf3e24ed63b3cba
Matt Benjamin <matt@linuxbox.com>
2013-05-23 19:13:54 -0700

    client: add barrier and types
    This gives us a framework to implement commit operations that are
    guaranteed to terminate even if unstable writes come in after commit
    is called.
    
    Adapted to use Boost interval classes by Adam Emerson
    <aemerson@linuxbox.com>.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/client/Client.h
+++ src/client/Client.h
@@ -34,6 +34,8 @@ using namespace __gnu_cxx;
 #include "include/interval_set.h"
 #include "include/lru.h"
 
+#include "barrier.h"
+
 #include "mds/mdstypes.h"
 
 #include "msg/Message.h"
@@ -301,6 +303,9 @@ protected:
   int num_flushing_caps;
   hash_map<inodeno_t,SnapRealm*> snap_realms;
 
+  /* async block write barrier support */
+  map<uint64_t, BarrierContext* > barriers;
+
   SnapRealm *get_snap_realm(inodeno_t r);
   SnapRealm *get_snap_realm_maybe(inodeno_t r);
   void put_snap_realm(SnapRealm *realm);
@@ -352,6 +357,7 @@ protected:
 
   friend class C_Client_PutInode; // calls put_inode()
   friend class C_Client_CacheInvalidate;  // calls ino_invalidate_cb
+  friend class C_Block_Sync; // Calls block map and protected helpers
 
   //int get_cache_size() { return lru.lru_get_size(); }
   //void set_cache_size(int m) { lru.lru_set_max(m); }
---
+++ src/client/barrier.h
@@ -0,0 +1,135 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ *
+ * Copyright (C) 2012 CohortFS, LLC.
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
+#ifndef BARRIER_H
+#define BARRIER_H
+
+#include "include/types.h"
+
+#include <string>
+#include <list>
+#include <set>
+#include <map>
+#include <boost/intrusive/list.hpp>
+#define BOOST_ICL_USE_STATIC_BOUNDED_INTERVALS
+#include <boost/icl/interval_set.hpp>
+#include <fstream>
+#include <exception>
+
+using std::list;
+using std::set;
+using std::map;
+using std::fstream;
+
+#include <ext/hash_map>
+
+#include "common/Mutex.h"
+#include "common/Cond.h"
+#include "common/config.h"
+
+class Client;
+
+typedef boost::icl::interval<uint64_t>::type barrier_interval;
+
+using namespace std;
+
+/*
+ * we keep count of uncommitted writes on the inode, so that
+ * ll_commit_blocks can do the right thing.
+ *
+ * This is just a hacked copy of Ceph's sync callback.
+ */
+
+enum CBlockSync_State
+{
+  CBlockSync_State_None, /* initial state */
+  CBlockSync_State_Unclaimed, /* outstanding write */
+  CBlockSync_State_Committing, /* commit in progress */
+  CBlockSync_State_Completed,
+};
+
+class Barrier;
+class BarrierContext;
+
+class C_Block_Sync : public Context {
+private:
+  Client *cl;
+  uint64_t ino;
+  barrier_interval iv;
+  enum CBlockSync_State state;
+  Barrier *barrier;
+  int *rval; /* see Cond.h */
+
+public:
+  boost::intrusive::list_member_hook<> intervals_hook;
+  C_Block_Sync(Client *c, uint64_t i, barrier_interval iv, int *r);
+  void finish(int rval);
+
+  friend class Barrier;
+  friend class BarrierContext;
+};
+
+typedef boost::intrusive::list< C_Block_Sync,
+				boost::intrusive::member_hook<
+				  C_Block_Sync,
+				  boost::intrusive::list_member_hook<>,
+				  &C_Block_Sync::intervals_hook >
+				> BlockSyncList;
+
+class Barrier
+{
+private:
+  Cond cond;
+  boost::icl::interval_set<uint64_t> span;
+  BlockSyncList write_list;
+
+public:
+  boost::intrusive::list_member_hook<> active_commits_hook;
+
+  Barrier();
+  ~Barrier();
+
+  friend class BarrierContext;
+};
+
+typedef boost::intrusive::list< Barrier,
+				boost::intrusive::member_hook<
+				  Barrier,
+				  boost::intrusive::list_member_hook<>,
+				  &Barrier::active_commits_hook >
+				> BarrierList;
+
+class BarrierContext
+{
+private:
+  Client *cl;
+  uint64_t ino;
+  Mutex lock;
+
+  // writes not claimed by a commit
+  BlockSyncList outstanding_writes;
+
+  // commits in progress, with their claimed writes
+  BarrierList active_commits;
+  boost::icl::interval_set<uint64_t> active_commit_interval;
+
+public:
+  BarrierContext(Client *c, uint64_t ino);
+  void write_nobarrier(C_Block_Sync &cbs);
+  void write_barrier(C_Block_Sync &cbs);
+  void commit_barrier(barrier_interval &civ);
+  void complete(C_Block_Sync &cbs);
+  ~BarrierContext();
+};
+
+#endif
