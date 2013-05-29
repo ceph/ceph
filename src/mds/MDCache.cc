77f2e8efea392d439979456c7d6da4c6663fba0d
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2013-05-29 15:24:39 +0200

    mds/MDCache.cc: fix dereference null return value
    Add assert to fix:
    
    CID 716994 (#1 of 1): Dereference null return value (NULL_RETURNS)
      dereference: Dereferencing a pointer that might be null "dir" when
      calling "CDir::mark_dirty(version_t, LogSegment *)".
    
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

--- src/mds/MDCache.cc
+++ src/mds/MDCache.cc
@@ -496,6 +496,7 @@ void MDCache::_create_system_file_finish(Mutation *mut, CDentry *dn, version_t d
   CDir *dir = 0;
   if (in->inode.is_dir()) {
     dir = in->get_dirfrag(frag_t());
+    assert(dir);
     dir->mark_dirty(1, mut->ls);
     dir->mark_new(mut->ls);
   }
