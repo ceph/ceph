a03ccf17b0df6e05120e77a52a6b85d56be7658f
Samuel Just <sam.just@inktank.com>
2013-05-31 15:11:02 -0700

    OSD: *inodes_hard_limit must be less than the fd limit
    Also add a comment explaining that.
    
    Fixes: #5224
    Signed-off-by: Samuel Just <sam.just@inktank.com>
    Reviewed-by: Greg Farnum <greg@inktank.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -489,12 +489,14 @@ OPTION(filestore_wbthrottle_btrfs_bytes_hard_limit, OPT_U64, 100<<20)
 OPTION(filestore_wbthrottle_btrfs_ios_start_flusher, OPT_U64, 100)
 OPTION(filestore_wbthrottle_btrfs_ios_hard_limit, OPT_U64, 1000)
 OPTION(filestore_wbthrottle_btrfs_inodes_start_flusher, OPT_U64, 100)
-OPTION(filestore_wbthrottle_btrfs_inodes_hard_limit, OPT_U64, 1000)
 OPTION(filestore_wbthrottle_xfs_bytes_start_flusher, OPT_U64, 10<<20)
 OPTION(filestore_wbthrottle_xfs_bytes_hard_limit, OPT_U64, 100<<20)
 OPTION(filestore_wbthrottle_xfs_ios_start_flusher, OPT_U64, 10)
 OPTION(filestore_wbthrottle_xfs_ios_hard_limit, OPT_U64, 100)
 OPTION(filestore_wbthrottle_xfs_inodes_start_flusher, OPT_U64, 10)
+
+/// These must be less than the fd limit
+OPTION(filestore_wbthrottle_btrfs_inodes_hard_limit, OPT_U64, 256)
 OPTION(filestore_wbthrottle_xfs_inodes_hard_limit, OPT_U64, 100)
 
 // Tests index failure paths
