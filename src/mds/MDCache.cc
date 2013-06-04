64b3e833f62f2538ffd7bd565d968decf6584691
Sage Weil <sage@inktank.com>
2013-06-04 11:07:58 -0700

    Revert "mds: allow purging "dirty parent" stray inode"
    This reverts commit b8f1cb978944a616b69150fdbb3a6b978d75b1dc.

--- src/mds/MDCache.cc
+++ src/mds/MDCache.cc
@@ -8584,7 +8584,7 @@ void MDCache::eval_stray(CDentry *dn)
       dout(20) << " pending recovery" << dendl;
       return;  // don't mess with file size probing
     }
-    if (in->get_num_ref() > (int)in->is_dirty() + (int)in->is_dirty_parent()) {
+    if (in->get_num_ref() > (int)in->is_dirty()) {
       dout(20) << " too many inode refs" << dendl;
       return;
     }
@@ -8865,9 +8865,6 @@ void MDCache::_purge_stray_logged(CDentry *dn, version_t pdv, LogSegment *ls)
   // drop inode
   if (in->is_dirty())
     in->mark_clean();
-  if (in->is_dirty_parent())
-    in->clear_dirty_parent();
-
   remove_inode(in);
 
   // drop dentry?
