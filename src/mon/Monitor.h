34cced3e16676b4749b13350c8760d2508d3f194
Sage Weil <sage@inktank.com>
2013-06-09 20:21:49 -0700

    msgr: use ConnectionRef throughout
    Make RefCountedObject a private parent of Connection so that users are
    forced to use ConnectionRef whenever references are taken.
    
    Many methods can still take a raw Connection* when they are using the
    caller's reference but not taking their own; this is cheaper than
    twiddling the reference count, and the lifetime is still well defined.
    Local variables generally use ConnectionRef, though.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/mon/Monitor.h
+++ src/mon/Monitor.h
@@ -109,7 +109,7 @@ public:
   string name;
   int rank;
   Messenger *messenger;
-  Connection *con_self;
+  ConnectionRef con_self;
   Mutex lock;
   SafeTimer timer;
   
@@ -1312,14 +1312,12 @@ public:
     uint64_t tid;
     bufferlist request_bl;
     MonSession *session;
-    Connection *con;
+    ConnectionRef con;
     entity_inst_t client_inst;
 
     ~RoutedRequest() {
       if (session)
 	session->put();
-      if (con)
-	con->put();
     }
   };
   uint64_t routed_request_tid;
