da5e443c5f263c3fa3bdde21783a715685da7e03
Yehuda Sadeh <yehuda@inktank.com>
2013-06-10 21:58:02 -0700

    rgw: head acls target can be quoted
    when passing x-amz-grant-* headers, the target (id, email,
    group) may be quoted.
    
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -1126,6 +1126,9 @@ extern int parse_time(const char *time_str, time_t *time);
 extern bool parse_rfc2616(const char *s, struct tm *t);
 extern bool parse_iso8601(const char *s, struct tm *t);
 extern int parse_date(const string& date, uint64_t *epoch, string *out_date = NULL, string *out_time = NULL);
+extern string rgw_trim_whitespace(const string& src);
+extern string rgw_trim_quotes(const string& val);
+
 
 /** Check if the req_state's user has the necessary permissions
  * to do the requested action */
