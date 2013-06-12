8bb7e2a157ba62809702ba98e8cc1bd0fd802552
Sage Weil <sage@inktank.com>
2013-06-12 13:26:03 -0700

    qa: multiple_rsync.sh: more output
    Trying to track down this failure:
    
    2013-06-12T06:11:13.430 INFO:teuthology.task.workunit.client.0.err:+ rsync -auv --exclude local/ /usr/ usr.2
    2013-06-12T06:11:13.430 INFO:teuthology.task.workunit.client.0.err:+ tee a
    2013-06-12T06:11:13.527 INFO:teuthology.task.workunit.client.0.out:sending incremental file list
    2013-06-12T06:11:46.206 INFO:teuthology.task.workunit.client.0.out:
    2013-06-12T06:11:46.208 INFO:teuthology.task.workunit.client.0.out:sent 1689627 bytes  received 8302 bytes  50684.45 bytes/sec
    2013-06-12T06:11:46.208 INFO:teuthology.task.workunit.client.0.out:total size is 3274130495  speedup is 1928.31
    2013-06-12T06:11:46.209 INFO:teuthology.task.workunit.client.0.err:+ wc -l a
    2013-06-12T06:11:46.209 INFO:teuthology.task.workunit.client.0.err:+ grep 4
    2013-06-12T06:11:46.211 INFO:teuthology.task.workunit:Stopping misc on client.0...
    
    ...and am perplexed!
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- qa/workunits/misc/multiple_rsync.sh
+++ qa/workunits/misc/multiple_rsync.sh
@@ -6,8 +6,10 @@ rsync -av --exclude local/ /usr/ usr.2
 # this shouldn't transfer any additional files
 echo we should get 4 here if no additional files are transfered
 rsync -auv --exclude local/ /usr/ usr.1 | tee a
+wc -l a
 wc -l a | grep 4
 rsync -auv --exclude local/ /usr/ usr.2 | tee a
+wc -l a
 wc -l a | grep 4
 
 echo OK
