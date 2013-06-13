e1d41901cde97a77fc1fda2d7f6e78a7cea61c5c
tamil <tamil.muthamizhan@inktank.com>
2013-06-13 13:50:56 -0700

    adding iogen.sh
    Signed-off-by: tamil <tamil.muthamizhan@inktank.com>

---
+++ qa/workunits/suites/iogen.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+echo "getting iogen"
+wget http://ceph.com/qa/iogen_3.1p0.tar
+tar -xvzf iogen_3.1p0.tar
+cd iogen*
+echo "making iogen"
+make
+echo "running iogen"
+./iogen -n 5 -s 2g
+echo "sleep for 10 min"
+sleep 600
+echo "stopping iogen"
+./iogen -k
+
+echo "OK"
+
