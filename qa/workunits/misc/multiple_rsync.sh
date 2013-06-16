ebb46c452eeca4102148becc543b1da83ea3e7a9
Sage Weil <sage@inktank.com>
2013-06-15 20:42:39 -0700

    qa/workunits/misc/multiple_rsync.sh: wtf
    2013-06-15T12:55:29.808 INFO:teuthology.task.workunit.client.0.err:+ rsync -auv --exclude local/ /usr/ usr.1
    2013-06-15T12:55:29.808 INFO:teuthology.task.workunit.client.0.err:+ tee a
    2013-06-15T12:55:29.820 INFO:teuthology.task.workunit.client.0.out:sending incremental file list
    2013-06-15T12:56:46.019 INFO:teuthology.task.workunit.client.0.out:
    2013-06-15T12:56:46.020 INFO:teuthology.task.workunit.client.0.out:sent 1452634 bytes  received 7485 bytes  19086.52 bytes/sec
    2013-06-15T12:56:46.020 INFO:teuthology.task.workunit.client.0.out:total size is 3205063225  speedup is 2195.07
    2013-06-15T12:56:46.020 INFO:teuthology.task.workunit.client.0.err:+ wc -l a
    2013-06-15T12:56:46.021 INFO:teuthology.task.workunit.client.0.out:4 a
    2013-06-15T12:56:46.022 INFO:teuthology.task.workunit.client.0.err:+ wc -l a
    2013-06-15T12:56:46.022 INFO:teuthology.task.workunit.client.0.err:+ grep 4
    2013-06-15T12:56:46.023 INFO:teuthology.task.workunit.client.0.out:4 a
    2013-06-15T12:56:46.024 INFO:teuthology.task.workunit.client.0.err:+ rsync -auv --exclude local/ /usr/ usr.2
    2013-06-15T12:56:46.024 INFO:teuthology.task.workunit.client.0.err:+ tee a
    2013-06-15T12:56:46.112 INFO:teuthology.task.workunit.client.0.out:sending incremental file list
    2013-06-15T12:57:17.172 INFO:teuthology.task.workunit.client.0.out:
    2013-06-15T12:57:17.174 INFO:teuthology.task.workunit.client.0.out:sent 1452634 bytes  received 7485 bytes  46352.98 bytes/sec
    2013-06-15T12:57:17.174 INFO:teuthology.task.workunit.client.0.out:total size is 3205063225  speedup is 2195.07
    2013-06-15T12:57:17.175 INFO:teuthology.task.workunit.client.0.err:+ wc -l a
    2013-06-15T12:57:17.175 INFO:teuthology.task.workunit.client.0.out:3 a
    
    Signed-off-by: Sage Weil <sage@inktank.com>
    (cherry picked from commit 21e85f90be3e4915376106dd384f6982086e2311)

--- qa/workunits/misc/multiple_rsync.sh
+++ qa/workunits/misc/multiple_rsync.sh
@@ -6,10 +6,10 @@ rsync -av --exclude local/ /usr/ usr.2
 # this shouldn't transfer any additional files
 echo we should get 4 here if no additional files are transfered
 rsync -auv --exclude local/ /usr/ usr.1 | tee a
-wc -l a
+hexdump -C a
 wc -l a | grep 4
 rsync -auv --exclude local/ /usr/ usr.2 | tee a
-wc -l a
+hexdump -C a
 wc -l a | grep 4
 
 echo OK
