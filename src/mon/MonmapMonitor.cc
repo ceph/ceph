f20f21cd613663a2479022b053c842514807fcb2
Sage Weil <sage@inktank.com>
2013-06-17 17:17:56 -0700

    mon: fix mkfs monmap cleanup
    exists_key(a,b) was looking for "monmap/mkfs/monmap".
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/mon/MonmapMonitor.cc
+++ src/mon/MonmapMonitor.cc
@@ -70,7 +70,7 @@ void MonmapMonitor::update_from_paxos(bool *need_bootstrap)
   dout(10) << "update_from_paxos got " << version << dendl;
   mon->monmap->decode(monmap_bl);
 
-  if (exists_key("mkfs", get_service_name())) {
+  if (mon->store->exists("mkfs", get_service_name())) {
     MonitorDBStore::Transaction t;
     erase_mkfs(&t);
     mon->store->apply_transaction(t);
