3b021386e703aa98bd8e019faeebcdac7c515195
Greg Farnum <greg@inktank.com>
2013-06-20 11:01:01 -0700

    cls_replica_log: integrate with RGWRados
    We introduce an implementation class RGWReplicaLogger, and two user
    classes RGWReplicaObjectLogger (for the data/metadata logs) and
    RGWReplicaBucketLogger (for the bucket logs).
    
    Signed-off-by: Greg Farnum <greg@inktank.com>

---
+++ src/rgw/rgw_replica_log.h
@@ -0,0 +1,113 @@
+/*
+ * Ceph - scalable distributed file system
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ */
+
+#ifndef RGW_REPLICA_LOG_H_
+#define RGW_REPLICA_LOG_H_
+
+#include <string>
+#include "cls/replica_log/cls_replica_log_types.h"
+#include "include/types.h"
+#include "include/utime.h"
+#include "include/rados/librados.hpp"
+#include "rgw_common.h"
+
+class RGWRados;
+class CephContext;
+
+using namespace std;
+
+#define META_REPLICA_LOG_OBJ_PREFIX "meta.replicalog."
+#define DATA_REPLICA_LOG_OBJ_PREFIX "data.replicalog."
+
+class RGWReplicaLogger {
+protected:
+  CephContext *cct;
+  RGWRados *store;
+  int open_ioctx(librados::IoCtx& ctx, const string& pool);
+
+  RGWReplicaLogger(RGWRados *_store);
+
+  int update_bound(const string& oid, const string& pool,
+                   const string& daemon_id, const string& marker,
+                   const utime_t& time,
+                   const list<pair<string, utime_t> > *entries);
+  int delete_bound(const string& oid, const string& pool,
+                   const string& daemon_id);
+  int get_bounds(const string& oid, const string& pool,
+                 string& marker, utime_t& oldest_time,
+                 list<cls_replica_log_progress_marker>& markers);
+
+public:
+  static void get_bound_info(const cls_replica_log_progress_marker& progress,
+                             string& entity, string& marker,
+                             utime_t time,
+                             list<pair<string, utime_t> >& entries);
+};
+
+class RGWReplicaObjectLogger : private RGWReplicaLogger {
+  string pool;
+  string prefix;
+
+  void get_shard_oid(int id, string& oid) {
+    char buf[16];
+    snprintf(buf, sizeof(buf), "%d", id);
+    oid = prefix + buf;
+  }
+
+public:
+  RGWReplicaObjectLogger(RGWRados *_store,
+                const string& _pool,
+                const string& _prefix);
+
+  int create_log_objects(int shards);
+  int update_bound(int shard, const string& daemon_id, const string& marker,
+                   const utime_t& time,
+                   const list<pair<string, utime_t> > *entries) {
+    string oid;
+    get_shard_oid(shard, oid);
+    return RGWReplicaLogger::update_bound(oid, pool,
+                                          daemon_id, marker, time, entries);
+  }
+  int delete_bound(int shard, const string& daemon_id) {
+    string oid;
+    get_shard_oid(shard, oid);
+    return RGWReplicaLogger::delete_bound(oid, pool,
+                                          daemon_id);
+  }
+  int get_bounds(int shard, string& marker, utime_t& oldest_time,
+                 list<cls_replica_log_progress_marker>& markers) {
+    string oid;
+    get_shard_oid(shard, oid);
+    return RGWReplicaLogger::get_bounds(oid, pool,
+                                        marker, oldest_time, markers);
+  }
+};
+
+class RGWReplicaBucketLogger : private RGWReplicaLogger {
+public:
+  RGWReplicaBucketLogger(RGWRados *_store) :
+    RGWReplicaLogger(_store) {}
+  int update_bound(const rgw_bucket& bucket, const string& daemon_id,
+                   const string& marker, const utime_t& time,
+                   const list<pair<string, utime_t> > *entries) {
+    return RGWReplicaLogger::update_bound(bucket.name, bucket.index_pool,
+                                          daemon_id, marker, time, entries);
+  }
+  int delete_bound(const rgw_bucket& bucket, const string& daemon_id) {
+    return RGWReplicaLogger::delete_bound(bucket.name, bucket.index_pool,
+                                          daemon_id);
+  }
+  int get_bounds(const rgw_bucket& bucket, string& marker, utime_t& oldest_time,
+                 list<cls_replica_log_progress_marker>& markers) {
+    return RGWReplicaLogger::get_bounds(bucket.name, bucket.index_pool,
+                                        marker, oldest_time, markers);
+  }
+};
+
+#endif /* RGW_REPLICA_LOG_H_ */
