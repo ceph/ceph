3dc49ab1e84716038e8bb7d1a61f0dff689c4fd7
Yan, Zheng <zheng.z.yan@intel.com>
2013-06-21 14:39:26 +0800

    mds: use "open-by-ino" helper to handle LOOKUPINO request
    Fixes #3541
    Signed-off-by: Yan, Zheng <zheng.z.yan@intel.com>

--- src/mds/Server.h
+++ src/mds/Server.h
@@ -146,7 +146,6 @@ public:
   void handle_client_lookup_ino(MDRequest *mdr);
   void _lookup_ino(MDRequest *mdr);
   void _lookup_ino_2(MDRequest *mdr, int r);
-  void _lookup_ino_3(MDRequest *mdr, int r);
   void handle_client_readdir(MDRequest *mdr);
   void handle_client_file_setlock(MDRequest *mdr);
   void handle_client_file_readlock(MDRequest *mdr);
