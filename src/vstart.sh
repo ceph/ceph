ed2d3a932e9288edcf682dea67328a152d0e57b7
Sage Weil <sage@inktank.com>
2013-06-28 18:26:31 -0700

    os/FileStore: automatically enable 'filestore xattr use omap' as needed
    Automatically enable the 'filestore xattr use omap' option if the fs
    does not appear to handle large xattrs on its own.
    
    This makes for a more pleasant use experience as they are not told to
    enable something that we already know they must enable in order to
    continue.
    
    Fixes: #5137
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/vstart.sh
+++ src/vstart.sh
@@ -329,7 +329,6 @@ $DAEMONOPTS
         osd class tmp = out
         osd class dir = .libs
         osd scrub load threshold = 5.0
-        filestore xattr use omap = true
         osd debug op order = true
 $COSDDEBUG
 $extra_conf
