150c46d0d3f63b2342e93dce0f118a4efd0b6df5
Yehuda Sadeh <yehuda@inktank.com>
2013-07-01 11:42:23 -0700

    rgw: metadata, data logs can be turned on / off
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/cls/rgw/cls_rgw_client.h
+++ src/cls/rgw/cls_rgw_client.h
@@ -11,11 +11,11 @@ void cls_rgw_bucket_init(librados::ObjectWriteOperation& o);
 void cls_rgw_bucket_set_tag_timeout(librados::ObjectWriteOperation& o, uint64_t tag_timeout);
 
 void cls_rgw_bucket_prepare_op(librados::ObjectWriteOperation& o, RGWModifyOp op, string& tag,
-                               string& name, string& locator);
+                               string& name, string& locator, bool log_op);
 
 void cls_rgw_bucket_complete_op(librados::ObjectWriteOperation& o, RGWModifyOp op, string& tag,
                                 rgw_bucket_entry_ver& ver, string& name, rgw_bucket_dir_entry_meta& dir_meta,
-				list<string> *remove_objs);
+				list<string> *remove_objs, bool log_op);
 
 int cls_rgw_list_op(librados::IoCtx& io_ctx, string& oid, string& start_obj,
                     string& filter_prefix, uint32_t num_entries,
