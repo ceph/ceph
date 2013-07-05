705a561e75f7a2d7104a5862eaa4e65f241de304
Sage Weil <sage@inktank.com>
2013-07-05 11:24:06 -0700

    qa: write a somewhat <1tb image
    1TB is enough to fill up 6 plana osds.  And it takes forever.  Write less.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- qa/workunits/rbd/simple_big.sh
+++ qa/workunits/rbd/simple_big.sh
@@ -2,7 +2,8 @@
 
 [ -d /sys/bus/rbd ] || sudo modprobe rbd
 
-rbd create foo --size 1000000
+#rbd create foo --size 1000000
+rbd create foo --size 100000    # ok, a bit smaller!
 sudo rbd map foo
 sudo dd if=/dev/zero of=/dev/rbd/rbd/foo bs=1M
 sudo dd if=/dev/rbd/rbd/foo of=/dev/null bs=1M
