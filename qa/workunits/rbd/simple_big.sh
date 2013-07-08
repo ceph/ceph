17343814a802849e816e14168fd8422782f67622
Sage Weil <sage@inktank.com>
2013-07-08 10:14:08 -0700

    qa/workunits/rbd/simple_big.sh: don't ENOSPC every time
    Set the count on the initial dd so we don't always ENOSPC.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- qa/workunits/rbd/simple_big.sh
+++ qa/workunits/rbd/simple_big.sh
@@ -2,11 +2,12 @@
 
 [ -d /sys/bus/rbd ] || sudo modprobe rbd
 
-#rbd create foo --size 1000000
-rbd create foo --size 100000    # ok, a bit smaller!
+mb=100000
+
+rbd create foo --size $mb
 sudo rbd map foo
-sudo dd if=/dev/zero of=/dev/rbd/rbd/foo bs=1M
-sudo dd if=/dev/rbd/rbd/foo of=/dev/null bs=1M
+sudo dd if=/dev/zero of=/dev/rbd/rbd/foo bs=1M count=$mb
+sudo dd if=/dev/rbd/rbd/foo of=/dev/null bs=1M count=$mb
 sudo rbd unmap foo
 rbd rm foo
 
