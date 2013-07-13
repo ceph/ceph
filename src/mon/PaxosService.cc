ceph/ceph

commit 39b71c582628d26e749c23343aa69db079ee40a1

mon/PaxosService: trim periodically instead of via propose_pending
We want to trim old states even if there is no update activity.  For
example, if a long-running rebalance finishes all osdmap updates will
stop and we won't trim out old maps to free space.

Instead, trim at the same time as tick().  Remove the trim during
propose_pending() to force all trims through this path and avoid
introducing a new and rarely-exercised behavior.

Signed-off-by: Sage Weil <sage@inktank.com>
Reviewed-by: Joao Eduardo Luis <joao.luis@inktank.com>

src/mon/PaxosService.cc
@@ -185,13 +185,8 @@ void PaxosService::propose_pending()
   MonitorDBStore::Transaction t;
   bufferlist bl;
 
-  update_trim();
   if (should_stash_full())
     encode_full(&t);
-
-  if (should_trim()) {
-    encode_trim(&t);
-  }
 
   encode_pending(&t);
   have_pending = false;
@@ -324,6 +319,20 @@ void PaxosService::shutdown()
   }
 
   finish_contexts(g_ceph_context, waiting_for_finished_proposal, -EAGAIN);
+}
+
+void PaxosService::maybe_trim()
+{
+  if (is_writeable() && should_trim()) {
+    dout(10) << __func__ << " trimming" << dendl;
+
+    MonitorDBStore::Transaction t;
+    encode_trim(&t);
+    bufferlist bl;
+    t.encode(bl);
+
+    paxos->propose_new_value(bl, new C_Committed(this));
+  }
 }
 
 bool PaxosService::should_trim()

src/mon/Monitor.cc
@@ -4139,6 +4139,7 @@ void Monitor::tick()
   
   for (vector<PaxosService*>::iterator p = paxos_service.begin(); p != paxos_service.end(); ++p) {
     (*p)->tick();
+    (*p)->maybe_trim();
   }
   
   // trim sessions

src/mon/PaxosService.h
@@ -469,6 +469,11 @@ public:
   virtual void tick() {}
 
   /**
+   * called at same interval as tick.  consider a trim.
+   */
+  void maybe_trim();
+
+  /**
    * Get health information
    *
    * @param summary list of summary strings and associated severity

commit bf4f8024ba39c7a44258b5035698efa796300727

mon/PaxosService: fix trim completion
Do not call C_Committed after trim or else we will prematurely clear
the bool proposing, propose something again using the same version, and
crash.  We do not in fact need anything to happen here aside from the
refresh_from_paxos() that happens on its own.

Broken by 39b71c582628d26e749c23343aa69db079ee40a1.

Signed-off-by: Sage Weil <sage@inktank.com>

src/mon/PaxosService.cc
@@ -357,7 +357,7 @@ void PaxosService::maybe_trim()
 
   bufferlist bl;
   t.encode(bl);
-  paxos->propose_new_value(bl, new C_Committed(this));
+  paxos->propose_new_value(bl, NULL);
 }
 
 void PaxosService::trim(MonitorDBStore::Transaction *t,