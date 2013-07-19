65f49beb3520d78e483d10a4f231d4c728e3912e
Alfredo Deza <alfredo@deza.pe>
2013-07-19 08:59:53 -0400

    safer get by falling back to a dictionary

--- teuthology/queue.py
+++ teuthology/queue.py
@@ -81,7 +81,7 @@ describe. One job is run at a time.
         log.debug('Config is: %s', job.body)
         job_config = yaml.safe_load(job.body)
         safe_archive = safepath.munge(job_config['name'])
-        teuthology_branch=job_config.get('config').get('teuthology_branch', 'master')
+        teuthology_branch = job_config.get('config', {}).get('teuthology_branch', 'master')
 
         teuth_path = os.path.join(os.getenv("HOME"), 'teuthology-' + teuthology_branch, 'virtualenv', 'bin')
         if not os.path.isdir(teuth_path):
