942f722e594f098764ffc04537bb72a9bb6bca3d
Sage Weil <sage@inktank.com>
2013-07-19 16:39:47 -0700

    Revert "mon/OSDMonitor: send_to_waiting() in on_active()"
    This reverts commit db8ba4c5d222492a24fd82bcc7401baf2667eef6.
    
    On peons, on_active() is only called when we *first* become active after an
    election.  Only on the leader is it called after each commit/update.  This
    makes this change cause other problems (broken subscriptions on peons, in
    particular).  We possibly should fix that, but there is also a simpler fix
    for the original problem we were trying to solve.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -185,8 +185,12 @@ void OSDMonitor::update_from_paxos(bool *need_bootstrap)
     mon->pgmon()->check_osd_map(osdmap.epoch);
   }
 
-  update_logger();
+  send_to_waiting();
+  check_subs();
+
   share_map_with_random_osd();
+  update_logger();
+
   process_failures();
 
   // make sure our feature bits reflect the latest map
@@ -293,9 +297,6 @@ bool OSDMonitor::thrash()
 void OSDMonitor::on_active()
 {
   update_logger();
-
-  send_to_waiting();
-  check_subs();
 
   if (thrash_map && thrash())
     propose_pending();
@@ -1305,7 +1306,6 @@ void OSDMonitor::_reply_map(PaxosServiceMessage *m, epoch_t e)
 {
   dout(7) << "_reply_map " << e
 	  << " from " << m->get_orig_source_inst()
-	  << " for " << m
 	  << dendl;
   send_latest(m, e);
 }
