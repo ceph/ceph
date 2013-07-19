fd8b957f5d79f94e18f43946873337e2acdc858f
Yehuda Sadeh <yehuda@inktank.com>
2013-07-19 11:19:05 -0700

    cls_rgw: quiet down verbose log message
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/cls/rgw/cls_rgw.cc
+++ src/cls/rgw/cls_rgw.cc
@@ -656,7 +656,7 @@ int rgw_bucket_complete_op(cls_method_context_t hctx, bufferlist *in, bufferlist
   }
 
   list<string>::iterator remove_iter;
-  CLS_LOG(0, "rgw_bucket_complete_op(): remove_objs.size()=%d\n", (int)op.remove_objs.size());
+  CLS_LOG(20, "rgw_bucket_complete_op(): remove_objs.size()=%d\n", (int)op.remove_objs.size());
   for (remove_iter = op.remove_objs.begin(); remove_iter != op.remove_objs.end(); ++remove_iter) {
     string& remove_oid_name = *remove_iter;
     CLS_LOG(1, "rgw_bucket_complete_op(): removing entries, read_index_entry name=%s\n", remove_oid_name.c_str());
