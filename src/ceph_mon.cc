1b5fd89c85a77869b1728f352df2f19f73c7e88e
Sage Weil <sage@inktank.com>
2013-07-20 08:49:48 -0700

    mon, mds, osd: add early SIGTERM injection
    This makes it easy to identify problems with (early) shutdown with a
    loop like
    
     while [ ! -e core ] ; do ./ceph-mds -i a -c ceph.conf -f ; done
    
    and a vstart cluster.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/ceph_mon.cc
+++ src/ceph_mon.cc
@@ -552,6 +552,9 @@ int main(int argc, const char **argv)
   register_async_signal_handler_oneshot(SIGINT, handle_mon_signal);
   register_async_signal_handler_oneshot(SIGTERM, handle_mon_signal);
 
+  if (g_conf->inject_early_sigterm)
+    kill(getpid(), SIGTERM);
+
   messenger->wait();
 
   unregister_async_signal_handler(SIGHUP, sighup_handler);
