ceph/ceph

commit 228c35ca622675d2f87d6f31eb920fd4a1ad0c04

global/signal_handler: use poll(2) instead of select(2)
Starting with commit 7d8a848a7fc07ffa3b17cd809ef90638d8e0d50d we delay the
signal handler setup until after lots of other initialization has happened,
which can result in us having very large (>1024) open fds, which will
break the FD_SET macros for select(2).  Use poll(2) instead.

Fixes: #5722
Signed-off-by: Sage Weil <sage@inktank.com>
Reviewed-by: Yehuda Sadeh <yehuda@inktank.com>

src/global/signal_handler.cc
@@ -19,6 +19,7 @@
 #include "global/pidfile.h"
 #include "global/signal_handler.h"
 
+#include <poll.h>
 #include <signal.h>
 #include <sstream>
 #include <stdlib.h>
@@ -189,25 +190,23 @@ struct SignalHandler : public Thread {
   // thread entry point
   void *entry() {
     while (!stop) {
-      // create fd set
-      fd_set rfds;
-      FD_ZERO(&rfds);
-      FD_SET(pipefd[0], &rfds);
-      int max_fd = pipefd[0];
+      // build fd list
+      struct pollfd fds[33];
 
       lock.Lock();
+      int num_fds = 0;
       for (unsigned i=0; i<32; i++) {
 	if (handlers[i]) {
-	  int fd = handlers[i]->pipefd[0];
-	  FD_SET(fd, &rfds);
-	  if (fd > max_fd)
-	    max_fd = fd;
+	  fds[num_fds].fd = handlers[i]->pipefd[0];
+	  fds[num_fds].events = POLLIN | POLLOUT | POLLERR;
+	  fds[num_fds].revents = 0;
+	  ++num_fds;
 	}
       }
       lock.Unlock();
 
       // wait for data on any of those pipes
-      int r = select(max_fd + 1, &rfds, NULL, NULL, NULL);
+      int r = poll(fds, num_fds, -1);
       if (stop)
 	break;
       if (r > 0) {

src/test/signals.cc
@@ -110,3 +110,33 @@ TEST(SignalHandler, Multiple)
   unregister_async_signal_handler(SIGUSR2, testhandler);
   shutdown_async_signal_handler();
 }
+
+/*
+TEST(SignalHandler, MultipleBigFd)
+{
+  int ret;
+
+  for (int i = 0; i < 1500; i++)
+    ::open(".", O_RDONLY);
+
+  reset();
+  init_async_signal_handler();
+  register_async_signal_handler(SIGUSR1, testhandler);
+  register_async_signal_handler(SIGUSR2, testhandler);
+  ASSERT_TRUE(usr1 == false);
+  ASSERT_TRUE(usr2 == false);
+
+  ret = kill(getpid(), SIGUSR1);
+  ASSERT_EQ(ret, 0);
+  ret = kill(getpid(), SIGUSR2);
+  ASSERT_EQ(ret, 0);
+
+  sleep(1);
+  ASSERT_TRUE(usr1 == true);
+  ASSERT_TRUE(usr2 == true);
+
+  unregister_async_signal_handler(SIGUSR1, testhandler);
+  unregister_async_signal_handler(SIGUSR2, testhandler);
+  shutdown_async_signal_handler();
+}
+*/

commit 345daa3e8d3b19e699b8d655639fba9aab6f3171

global/signal_handler: poll on the control pipe, too
We also need to poll the control fd/pipe so that we restart the poll loop
when new signal handlers are added.  This was broken by commit 228c35ca.

Fixes: #5742
Signed-off-by: Sage Weil <sage@inktank.com>
Reviewed-by: Yehuda Sadeh <yehuda@inktank.com>

src/global/signal_handler.cc
@@ -195,6 +195,10 @@ struct SignalHandler : public Thread {
 
       lock.Lock();
       int num_fds = 0;
+      fds[num_fds].fd = pipefd[0];
+      fds[num_fds].events = POLLIN | POLLOUT | POLLERR;
+      fds[num_fds].revents = 0;
+      ++num_fds;
       for (unsigned i=0; i<32; i++) {
 	if (handlers[i]) {
 	  fds[num_fds].fd = handlers[i]->pipefd[0];