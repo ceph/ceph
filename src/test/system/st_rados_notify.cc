ff22793f2872228de608ada5416d4db8add8391c
Sage Weil <sage@inktank.com>
2013-07-28 09:04:05 -0700

    test/system/*: parse CEPH_ARGS environment
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/test/system/st_rados_notify.cc
+++ src/test/system/st_rados_notify.cc
@@ -44,6 +44,8 @@ int StRadosNotify::run()
   RETURN1_IF_NONZERO(rados_create(&cl, NULL));
   rados_conf_parse_argv(cl, m_argc, m_argv);
   RETURN1_IF_NONZERO(rados_conf_read_file(cl, NULL));
+  rados_conf_parse_env(cl, NULL);
+
   if (m_setup_sem) {
     m_setup_sem->wait();
     m_setup_sem->post();
