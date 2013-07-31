cc4341b56a4430daa697a4ccaaaaebdbfcde24da
Sage Weil <sage@inktank.com>
2013-07-31 13:46:12 -0700

    qa/workunits/cephtool/test_daemon.sh: sudo
    Fixes: #5814
    Signed-off-by: Sage Weil <sage@inktank.com>

--- qa/workunits/cephtool/test_daemon.sh
+++ qa/workunits/cephtool/test_daemon.sh
@@ -10,17 +10,17 @@ expect_false()
 
 echo note: assuming mon.a is on the currenet host
 
-ceph daemon mon.a version | grep version
+sudo ceph daemon mon.a version | grep version
 
 # get debug_ms setting and strip it, painfully for reuse
-old_ms=$(ceph daemon mon.a config get debug_ms | grep debug_ms | \
+old_ms=$(sudo ceph daemon mon.a config get debug_ms | grep debug_ms | \
 	sed -e 's/.*: //' -e 's/["\}\\]//g')
-ceph daemon mon.a config set debug_ms 13
-new_ms=$(ceph daemon mon.a config get debug_ms | grep debug_ms | \
+sudo ceph daemon mon.a config set debug_ms 13
+new_ms=$(sudo ceph daemon mon.a config get debug_ms | grep debug_ms | \
 	sed -e 's/.*: //' -e 's/["\}\\]//g')
 [ "$new_ms" = "13/13" ]
-ceph daemon mon.a config set debug_ms $old_ms
-new_ms=$(ceph daemon mon.a config get debug_ms | grep debug_ms | \
+sudo ceph daemon mon.a config set debug_ms $old_ms
+new_ms=$(sudo ceph daemon mon.a config get debug_ms | grep debug_ms | \
 	sed -e 's/.*: //' -e 's/["\}\\]//g')
 [ "$new_ms" = "$old_ms" ]
 
