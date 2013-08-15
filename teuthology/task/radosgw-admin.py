558237e5f5981468aecdf2e146b6084de051b812
Joe Buck <jbbuck@gmail.com>
2013-08-15 12:51:51 -0700

    Detect multi-region test cases
    Enable multi-region calls and tests only if
    the configuration has specified a
    radosgw-agent tastk.
    
    Signed-off-by: Joe Buck <jbbuck@gmail.com>
    Reviewed-by: Josh Durgin <josh.durgin@inktank.com>

--- teuthology/task/radosgw-admin.py
+++ teuthology/task/radosgw-admin.py
@@ -32,8 +32,8 @@ def successful_ops(out):
 
 # simple test to indicate if multi-region testing should occur
 def multi_region_enabled(ctx):
-    # this is populated by the radosgw-agent task, seems reasonable to use that as an indicator that
-    # we're testing multi-region sync 
+    # this is populated by the radosgw-agent task, seems reasonable to
+    # use that as an indicator that we're testing multi-region sync
     return 'radosgw_agent' in ctx
 
 def task(ctx, config):
