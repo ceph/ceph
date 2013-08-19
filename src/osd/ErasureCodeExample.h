f480059fb27a18ba4100c440616575800134474b
Loic Dachary <loic@dachary.org>
2013-08-19 19:15:07 +0200

    default erasure code implementation : M=2 K=1
    This is the default erasure code implementation. Although it is fully
    functional and could be used on actual data, it is mainly provided for
    testing purposes. It splits data in two, computes an XOR parity and
    can sustain the loss of one chunk.
    
    http://tracker.ceph.com/issues/5878 refs #5878
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

---
+++ src/osd/ErasureCodeExample.h
@@ -0,0 +1,98 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*- 
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2013 Cloudwatt <libre.licensing@cloudwatt.com>
+ *
+ * Author: Loic Dachary <loic@dachary.org>
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ * 
+ */
+
+#ifndef CEPH_ERASURE_CODE_EXAMPLE_H
+#define CEPH_ERASURE_CODE_EXAMPLE_H
+
+#include <errno.h>
+#include "ErasureCodeInterface.h"
+
+#define DATA_CHUNKS 2u
+#define PARITY_CHUNKS 1u
+
+class ErasureCodeExample : public ErasureCodeInterface {
+public:
+  ErasureCodeExample(const map<std::string,std::string> &parameters) {}
+  virtual ~ErasureCodeExample() {}
+  
+  virtual int minimum_to_decode(const set<int> &want_to_read,
+				const set<int> &available_chunks,
+				set<int> *minimum) {
+    if (available_chunks.size() < DATA_CHUNKS)
+      return -EIO;
+    set<int>::iterator i;
+    unsigned j;
+    for (i = available_chunks.begin(), j = 0; j < DATA_CHUNKS; i++, j++)
+      minimum->insert(*i);
+    return 0;
+  }
+
+  virtual int minimum_to_decode_with_cost(const set<int> &want_to_read,
+                                          const map<int, int> &available,
+					  set<int> *minimum) {
+    return 0;
+  }
+
+  virtual int encode(const set<int> &want_to_encode,
+                     const bufferlist &in,
+		     map<int, bufferlist> *encoded) {
+    unsigned chunk_length = ( in.length() / DATA_CHUNKS ) + 1;
+    unsigned length = chunk_length * ( DATA_CHUNKS + PARITY_CHUNKS );
+    bufferlist out(in);
+    bufferptr pad(length - in.length());
+    pad.zero(0, DATA_CHUNKS);
+    out.push_back(pad);
+    char *p = out.c_str();
+    for (unsigned i = 0; i < chunk_length * DATA_CHUNKS; i++)
+      p[i + 2 * chunk_length] =
+        p[i + 0 * chunk_length] ^ p[i + 1 * chunk_length];
+    const bufferptr ptr = out.buffers().front();
+    for (set<int>::iterator j = want_to_encode.begin();
+         j != want_to_encode.end();
+         j++) {
+      bufferptr chunk(ptr, (*j) * chunk_length, chunk_length);
+      (*encoded)[*j].push_front(chunk);
+    }
+    return 0;
+  }
+
+  virtual int decode(const set<int> &want_to_read,
+                     const map<int, bufferlist> &chunks,
+		     map<int, bufferlist> *decoded) {
+    
+    unsigned chunk_length = (*chunks.begin()).second.length();
+    for (set<int>::iterator i = want_to_read.begin();
+         i != want_to_read.end();
+         i++) {
+      if (chunks.find(*i) != chunks.end())
+        (*decoded)[*i] = chunks.find(*i)->second;
+      else {
+        bufferptr chunk(chunk_length);
+        map<int, bufferlist>::const_iterator k = chunks.begin();
+        const char *a = k->second.buffers().front().c_str();
+        k++;
+        const char *b = k->second.buffers().front().c_str();
+        for (unsigned j = 0; j < chunk_length; j++) {
+          chunk[j] = a[j] ^ b[j];
+        }
+        (*decoded)[*i].push_front(chunk);
+      }
+    }
+    return 0;
+  }
+};
+
+#endif
