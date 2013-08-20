da7197a89eabc9189b102be32356150584245145
Sage Weil <sage@inktank.com>
2013-08-20 13:45:42 -0700

    crc32c: add intel sse4.2 detection
    Signed-off-by: Sage Weil <sage@inktank.com>

---
+++ src/common/crc32c_intel_detect.h
@@ -0,0 +1,14 @@
+#ifndef CEPH_COMMON_CRC32C_INTEL_DETECT_H
+#define CEPH_COMMON_CRC32C_INTEL_DETECT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern int ceph_detect_intel_sse42(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
