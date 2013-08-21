a65e2b62ae338b918bc94814a390104828d9a743
Sage Weil <sage@inktank.com>
2013-08-20 20:59:56 -0700

    crc32c: add intel baseline algorithm
    This is than the sctp code but probably slower.  We'll add it anywhere
    just as a reference and to have a baseline for comparing performance.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/common/crc32c.cc
+++ src/common/crc32c.cc
@@ -1,14 +1,18 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
 #include "include/crc32c.h"
 
 #include "common/sctp_crc32.h"
+#include "common/crc32c_intel_baseline.h"
 
 /*
  * choose best implementation based on the CPU architecture.
  */
 ceph_crc32c_func_t ceph_choose_crc32(void)
 {
-	/* default */
-	return ceph_crc32c_sctp;
+  /* default */
+  return ceph_crc32c_sctp;
 }
 
 /*
