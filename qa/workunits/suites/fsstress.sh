676a3aa5c1204b7ea4a972b744924929d359ae1e
Sandon Van Ness <sandon@inktank.com>
2013-08-22 19:44:40 -0700

    QA: Compile fsstress if missing on machine.
    Some distro's have a lack of ltp-kernel packages and all we need is
    fstress. This just modified the shell script to download/compile
    fstress from source and copy it to the right location if it doesn't
    currently exist where it is expected. It is a very small/quick
    compile and currently only SLES and debian do not have it already.
    
    Reviewed-by: Sage Weil <sage@inktank.com>
    Signed-off-by: Sandon Van Ness <sandon@inktank.com>

--- qa/workunits/suites/fsstress.sh
+++ qa/workunits/suites/fsstress.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+if [ ! -f /usr/lib/ltp/testcases/bin/fsstress ]
+then
+    mkdir -p /tmp/fsstress
+    cd /tmp/fsstress
+    wget -q -O /tmp/fsstress/ltp-full.tgz http://ceph.com/qa/ltp-full-20091231.tgz
+    tar xzf /tmp/fsstress/ltp-full.tgz
+    rm /tmp/fsstress/ltp-full.tgz
+    cd /tmp/fsstress/ltp-full-20091231/testcases/kernel/fs/fsstress
+    make
+    sudo mkdir -p /usr/lib/ltp/testcases/bin
+    sudo cp -avf /tmp/fsstress/ltp-full-20091231/testcases/kernel/fs/fsstress/fsstress /usr/lib/ltp/testcases/bin/fsstress
+    sudo chmod 755 /usr/lib/ltp/testcases/bin/fsstress
+    rm -Rf /tmp/fsstress
+fi
+
 command="/usr/lib/ltp/testcases/bin/fsstress -d fsstress-`hostname`$$ -l 1 -n 1000 -p 10 -v"
 
 echo "Starting fsstress $command"
