ceph/ceph

commit ace4cb07b2de99644c63f3ab90c21a663a384e69

Replace /tmp/cephtest/ with configurable path
Teuthology uses /tmp/cephtest/ as the scratch test directory for
a run.  This patch replaces /tmp/cephtest/ everywhere with a
per-run directory: {basedir}/{rundir} where {basedir} is a directory
configured in .teuthology.yaml (/tmp/cephtest if not specified),
and {rundir} is the name of the run, as given in --name.  If no name
is specified, {user}-{timestamp} is used.

To get the old behavior (/tmp/cephtest), set test_path: /tmp/cephtest
in .teuthology.yaml.

This change was modivated by #3782, which requires a test dir that
survives across reboots, but also resolves #3767.

Signed-off-by: Sam Lang <sam.lang@inktank.com>
Reviewed-by: Josh Durgin <josh.durgin@inktank.com>

teuthology/task/internal.py
@@ -15,15 +15,29 @@ log = logging.getLogger(__name__)
 @contextlib.contextmanager
 def base(ctx, config):
     log.info('Creating base directory...')
+    test_basedir = teuthology.get_testdir_base(ctx)
+    testdir = teuthology.get_testdir(ctx)
+    # make base dir if it doesn't exist
     run.wait(
         ctx.cluster.run(
             args=[
-                'mkdir', '-m0755', '--',
-                '/tmp/cephtest',
+                'mkdir', '-m0755', '-p', '--',
+                test_basedir,
                 ],
-            wait=False,
-            )
+                wait=False,
+                )
         )
+    # only create testdir if its not set to basedir
+    if test_basedir != testdir:
+        run.wait(
+            ctx.cluster.run(
+                args=[
+                    'mkdir', '-m0755', '--',
+                    testdir,
+                ],
+                wait=False,
+                )
+            )
 
     try:
         yield
@@ -36,7 +50,7 @@ def base(ctx, config):
                 args=[
                     'rmdir',
                     '--',
-                    '/tmp/cephtest',
+                    testdir,
                     ],
                 wait=False,
                 ),
@@ -154,9 +168,36 @@ def connect(ctx, config):
 
 def check_conflict(ctx, config):
     log.info('Checking for old test directory...')
+    test_basedir = teuthology.get_testdir_base(ctx)
     processes = ctx.cluster.run(
         args=[
-            'test', '!', '-e', '/tmp/cephtest',
+            'test', '!', '-e', test_basedir,
+            ],
+        wait=False,
+        )
+    for proc in processes:
+        assert isinstance(proc.exitstatus, gevent.event.AsyncResult)
+        try:
+            proc.exitstatus.get()
+        except run.CommandFailedError:
+            # base dir exists
+            r = proc.remote.run(
+                args=[
+                    'ls', test_basedir, run.Raw('|'), 'wc', '-l'
+                    ],
+                stdout=StringIO(),
+                )
+
+            if int(r.stdout.getvalue()) > 0:
+                log.error('WARNING: Host %s has stale test directories, these need to be investigated and cleaned up!',
+                          proc.remote.shortname)
+
+    # testdir might be the same as base dir (if test_path is set)
+    # need to bail out in that case if the testdir exists
+    testdir = teuthology.get_testdir(ctx)
+    processes = ctx.cluster.run(
+        args=[
+            'test', '!', '-e', testdir,
             ],
         wait=False,
         )
@@ -166,7 +207,7 @@ def check_conflict(ctx, config):
         try:
             proc.exitstatus.get()
         except run.CommandFailedError:
-            log.error('Host %s has stale cephtest directory, check your lock and reboot to clean up.', proc.remote.shortname)
+            log.error('Host %s has stale test directory %s, check lock and cleanup.', proc.remote.shortname, testdir)
             failed = True
     if failed:
         raise RuntimeError('Stale jobs detected, aborting.')
@@ -174,11 +215,12 @@ def check_conflict(ctx, config):
 @contextlib.contextmanager
 def archive(ctx, config):
     log.info('Creating archive directory...')
+    testdir = teuthology.get_testdir(ctx)
+    archive_dir = '{tdir}/archive'.format(tdir=testdir)
     run.wait(
         ctx.cluster.run(
             args=[
-                'install', '-d', '-m0755', '--',
-                '/tmp/cephtest/archive',
+                'install', '-d', '-m0755', '--', archive_dir,
                 ],
             wait=False,
             )
@@ -193,7 +235,7 @@ def archive(ctx, config):
             os.mkdir(logdir)
             for remote in ctx.cluster.remotes.iterkeys():
                 path = os.path.join(logdir, remote.shortname)
-                teuthology.pull_directory(remote, '/tmp/cephtest/archive', path)
+                teuthology.pull_directory(remote, archive_dir, path)
 
         log.info('Removing archive directory...')
         run.wait(
@@ -202,7 +244,7 @@ def archive(ctx, config):
                     'rm',
                     '-rf',
                     '--',
-                    '/tmp/cephtest/archive',
+                    archive_dir,
                     ],
                 wait=False,
                 ),
@@ -211,13 +253,14 @@ def archive(ctx, config):
 @contextlib.contextmanager
 def coredump(ctx, config):
     log.info('Enabling coredump saving...')
+    archive_dir = '{tdir}/archive'.format(tdir=teuthology.get_testdir(ctx))
     run.wait(
         ctx.cluster.run(
             args=[
                 'install', '-d', '-m0755', '--',
-                '/tmp/cephtest/archive/coredump',
+                '{adir}/coredump'.format(adir=archive_dir),
                 run.Raw('&&'),
-                'sudo', 'sysctl', '-w', 'kernel.core_pattern=/tmp/cephtest/archive/coredump/%t.%p.core',
+                'sudo', 'sysctl', '-w', 'kernel.core_pattern={adir}/coredump/%t.%p.core'.format(adir=archive_dir),
                 ],
             wait=False,
             )
@@ -235,7 +278,7 @@ def coredump(ctx, config):
                     'rmdir',
                     '--ignore-fail-on-non-empty',
                     '--',
-                    '/tmp/cephtest/archive/coredump',
+                    '{adir}/coredump'.format(adir=archive_dir),
                     ],
                 wait=False,
                 )
@@ -246,7 +289,7 @@ def coredump(ctx, config):
         for remote in ctx.cluster.remotes.iterkeys():
             r = remote.run(
                 args=[
-                    'if', 'test', '!', '-e', '/tmp/cephtest/archive/coredump', run.Raw(';'), 'then',
+                    'if', 'test', '!', '-e', '{adir}/coredump'.format(adir=archive_dir), run.Raw(';'), 'then',
                     'echo', 'OK', run.Raw(';'),
                     'fi',
                     ],
@@ -268,11 +311,12 @@ def syslog(ctx, config):
 
     log.info('Starting syslog monitoring...')
 
+    archive_dir = '{tdir}/archive'.format(tdir=teuthology.get_testdir(ctx))
     run.wait(
         ctx.cluster.run(
             args=[
                 'mkdir', '-m0755', '--',
-                '/tmp/cephtest/archive/syslog',
+                '{adir}/syslog'.format(adir=archive_dir),
                 ],
             wait=False,
             )
@@ -280,9 +324,9 @@ def syslog(ctx, config):
 
     CONF = '/etc/rsyslog.d/80-cephtest.conf'
     conf_fp = StringIO("""
-kern.* -/tmp/cephtest/archive/syslog/kern.log;RSYSLOG_FileFormat
-*.*;kern.none -/tmp/cephtest/archive/syslog/misc.log;RSYSLOG_FileFormat
-""")
+kern.* -{adir}/syslog/kern.log;RSYSLOG_FileFormat
+*.*;kern.none -{adir}/syslog/misc.log;RSYSLOG_FileFormat
+""".format(adir=archive_dir))
     try:
         for rem in ctx.cluster.remotes.iterkeys():
             teuthology.sudo_write_file(
@@ -336,7 +380,7 @@ kern.* -/tmp/cephtest/archive/syslog/kern.log;RSYSLOG_FileFormat
                 args=[
                     'egrep',
                     '\\bBUG\\b|\\bINFO\\b|\\bDEADLOCK\\b',
-                    run.Raw('/tmp/cephtest/archive/syslog/*.log'),
+                    run.Raw('{adir}/archive/syslog/*.log'.format(adir=archive_dir)),
                     run.Raw('|'),
                     'grep', '-v', 'task .* blocked for more than .* seconds',
                     run.Raw('|'),
@@ -377,7 +421,7 @@ kern.* -/tmp/cephtest/archive/syslog/kern.log;RSYSLOG_FileFormat
             ctx.cluster.run(
                 args=[
                     'find',
-                    '/tmp/cephtest/archive/syslog',
+                    '{adir}/archive/syslog'.format(adir=archive_dir),
                     '-name',
                     '*.log',
                     '-print0',

teuthology/ceph.conf
@@ -1,47 +1,0 @@
-[global]
-        keyring = /tmp/cephtest/ceph.keyring
-	log file = /tmp/cephtest/archive/log/$name.log
-	chdir = ""
-	pid file = $name.pid
-        auth supported = cephx
-	admin socket = /tmp/cephtest/asok.$name
-
-	filestore xattr use omap = true
-
-	mon clock drift allowed = .250
-
-	osd crush chooseleaf type = 0
-
-[mon]
-        mon data = /tmp/cephtest/data/mon.$id
-	mon cluster log file = /tmp/cephtest/archive/log/cluster.mon.$id.log
-
-[osd]
-        osd data = /tmp/cephtest/data/osd.$id.data
-        osd journal = /tmp/cephtest/data/osd.$id.journal
-        osd journal size = 100
-        keyring = /tmp/cephtest/data/osd.$id.keyring
-        osd class dir = /tmp/cephtest/binary/usr/local/lib/rados-classes
-
-        osd scrub load threshold = 5.0
-	osd scrub max interval = 600
-
-	osd recover clone overlap = true
-	osd recovery max chunk = 1048576
-
-[mds]
-        keyring = /tmp/cephtest/data/mds.$id.keyring
-        lockdep = 1
-        mds debug scatterstat = true
-        mds verify scatter = true
-        mds debug frag = true
-
-[client]
-        keyring = /tmp/cephtest/data/client.$id.keyring
-        rgw socket path = /tmp/cephtest/apache/tmp/fastcgi_sock/rgw_sock
-        rgw cache enabled = true
-	rgw enable ops log = true
-	rgw enable usage log = true
-
-[client.admin]
-        keyring = /tmp/cephtest/ceph.keyring

teuthology/ceph.conf.template
@@ -1,0 +1,47 @@
+[global]
+        keyring = {testdir}/ceph.keyring
+	log file = {testdir}/archive/log/$name.log
+	chdir = ""
+	pid file = $name.pid
+        auth supported = cephx
+	admin socket = {testdir}/asok.$name
+
+	filestore xattr use omap = true
+
+	mon clock drift allowed = .250
+
+	osd crush chooseleaf type = 0
+
+[mon]
+        mon data = {testdir}/data/mon.$id
+	mon cluster log file = {testdir}/archive/log/cluster.mon.$id.log
+
+[osd]
+        osd data = {testdir}/data/osd.$id.data
+        osd journal = {testdir}/data/osd.$id.journal
+        osd journal size = 100
+        keyring = {testdir}/data/osd.$id.keyring
+        osd class dir = {testdir}/binary/usr/local/lib/rados-classes
+
+        osd scrub load threshold = 5.0
+	osd scrub max interval = 600
+
+	osd recover clone overlap = true
+	osd recovery max chunk = 1048576
+
+[mds]
+        keyring = {testdir}/data/mds.$id.keyring
+        lockdep = 1
+        mds debug scatterstat = true
+        mds verify scatter = true
+        mds debug frag = true
+
+[client]
+        keyring = {testdir}/data/client.$id.keyring
+        rgw socket path = {testdir}/apache/tmp/fastcgi_sock/rgw_sock
+        rgw cache enabled = true
+	rgw enable ops log = true
+	rgw enable usage log = true
+
+[client.admin]
+        keyring = {testdir}/ceph.keyring

teuthology/misc.py
@@ -17,6 +17,32 @@ from .orchestra import run
 
 log = logging.getLogger(__name__)
 
+import datetime
+stamp = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+
+def get_testdir(ctx):
+    if 'test_path' in ctx.teuthology_config:
+        return ctx.teuthology_config['test_path']
+
+    basedir = ctx.teuthology_config.get('base_test_dir', '/tmp/cephtest')
+
+    if hasattr(ctx, 'name') and ctx.name:
+        log.debug('with name basedir: {b}'.format(b=basedir))
+        return '{basedir}/{rundir}'.format(
+                    basedir=basedir,
+                    rundir=ctx.name)
+    else:
+        log.debug('basedir: {b}'.format(b=basedir))
+        return '{basedir}/{user}-{stamp}'.format(
+                    basedir=basedir,
+                    user=get_user(),
+                    stamp=stamp)
+
+def get_testdir_base(ctx):
+    if 'test_path' in ctx.teuthology_config:
+        return ctx.teuthology_config['test_path']
+    return ctx.teuthology_config.get('base_test_dir', '/tmp/cephtest')
+
 def get_ceph_binary_url(package=None,
                         branch=None, tag=None, sha1=None, dist=None,
                         flavor=None, format=None, arch=None):
@@ -113,7 +139,7 @@ def generate_caps(type_):
         yield subsystem
         yield capability
 
-def skeleton_config(roles, ips):
+def skeleton_config(ctx, roles, ips):
     """
     Returns a ConfigObj that's prefilled with a skeleton config.
 
@@ -121,8 +147,10 @@ def skeleton_config(roles, ips):
 
     Use conf.write to write it out, override .filename first if you want.
     """
-    path = os.path.join(os.path.dirname(__file__), 'ceph.conf')
-    conf = configobj.ConfigObj(path, file_error=True)
+    path = os.path.join(os.path.dirname(__file__), 'ceph.conf.template')
+    t = open(path, 'r')
+    skconf = t.read().format(testdir=get_testdir(ctx))
+    conf = configobj.ConfigObj(StringIO(skconf), file_error=True)
     mons = get_mons(roles=roles, ips=ips)
     for role, addr in mons.iteritems():
         conf.setdefault(role, {})
@@ -175,7 +203,7 @@ def num_instances_of_type(cluster, type_):
     num = sum(sum(1 for role in hostroles if role.startswith(prefix)) for hostroles in roles)
     return num
 
-def create_simple_monmap(remote, conf):
+def create_simple_monmap(ctx, remote, conf):
     """
     Writes a simple monmap based on current ceph.conf into <tmpdir>/monmap.
 
@@ -196,11 +224,12 @@ def create_simple_monmap(remote, conf):
     assert addresses, "There are no monitors in config!"
     log.debug('Ceph mon addresses: %s', addresses)
 
+    testdir = get_testdir(ctx)
     args = [
-        '/tmp/cephtest/enable-coredump',
-        '/tmp/cephtest/binary/usr/local/bin/ceph-coverage',
-        '/tmp/cephtest/archive/coverage',
-        '/tmp/cephtest/binary/usr/local/bin/monmaptool',
+        '{tdir}/enable-coredump'.format(tdir=testdir),
+        '{tdir}/binary/usr/local/bin/ceph-coverage'.format(tdir=testdir),
+        '{tdir}/archive/coverage'.format(tdir=testdir),
+        '{tdir}/binary/usr/local/bin/monmaptool'.format(tdir=testdir),
         '--create',
         '--clobber',
         ]
@@ -208,7 +237,7 @@ def create_simple_monmap(remote, conf):
         args.extend(('--add', name, addr))
     args.extend([
             '--print',
-            '/tmp/cephtest/monmap',
+            '{tdir}/monmap'.format(tdir=testdir),
             ])
     remote.run(
         args=args,
@@ -379,16 +408,17 @@ def get_scratch_devices(remote):
             pass
     return retval
 
-def wait_until_healthy(remote):
+def wait_until_healthy(ctx, remote):
     """Wait until a Ceph cluster is healthy."""
+    testdir = get_testdir(ctx)
     while True:
         r = remote.run(
             args=[
-                '/tmp/cephtest/enable-coredump',
-                '/tmp/cephtest/binary/usr/local/bin/ceph-coverage',
-                '/tmp/cephtest/archive/coverage',
-                '/tmp/cephtest/binary/usr/local/bin/ceph',
-                '-c', '/tmp/cephtest/ceph.conf',
+                '{tdir}/enable-coredump'.format(tdir=testdir),
+                '{tdir}/binary/usr/local/bin/ceph-coverage'.format(tdir=testdir),
+                '{tdir}/archive/coverage'.format(tdir=testdir),
+                '{tdir}/binary/usr/local/bin/ceph'.format(tdir=testdir),
+                '-c', '{tdir}/ceph.conf'.format(tdir=testdir),
                 'health',
                 '--concise',
                 ],
@@ -401,17 +431,18 @@ def wait_until_healthy(remote):
             break
         time.sleep(1)
 
-def wait_until_osds_up(cluster, remote):
+def wait_until_osds_up(ctx, cluster, remote):
     """Wait until all Ceph OSDs are booted."""
     num_osds = num_instances_of_type(cluster, 'osd')
+    testdir = get_testdir(ctx)
     while True:
         r = remote.run(
             args=[
-                '/tmp/cephtest/enable-coredump',
-                '/tmp/cephtest/binary/usr/local/bin/ceph-coverage',
-                '/tmp/cephtest/archive/coverage',
-                '/tmp/cephtest/binary/usr/local/bin/ceph',
-                '-c', '/tmp/cephtest/ceph.conf',
+                '{tdir}/enable-coredump'.format(tdir=testdir),
+                '{tdir}/binary/usr/local/bin/ceph-coverage'.format(tdir=testdir),
+                '{tdir}/archive/coverage'.format(tdir=testdir),
+                '{tdir}/binary/usr/local/bin/ceph'.format(tdir=testdir),
+                '-c', '{tdir}/ceph.conf'.format(tdir=testdir),
                 '--concise',
                 'osd', 'dump', '--format=json'
                 ],
@@ -485,16 +516,17 @@ def reconnect(ctx, timeout):
         log.debug('waited {elapsed}'.format(elapsed=str(time.time() - starttime)))
         time.sleep(1)
 
-def write_secret_file(remote, role, filename):
+def write_secret_file(ctx, remote, role, filename):
+    testdir = get_testdir(ctx)
     remote.run(
         args=[
-            '/tmp/cephtest/enable-coredump',
-            '/tmp/cephtest/binary/usr/local/bin/ceph-coverage',
-            '/tmp/cephtest/archive/coverage',
-            '/tmp/cephtest/binary/usr/local/bin/ceph-authtool',
+            '{tdir}/enable-coredump'.format(tdir=testdir),
+            '{tdir}/binary/usr/local/bin/ceph-coverage'.format(tdir=testdir),
+            '{tdir}/archive/coverage'.format(tdir=testdir),
+            '{tdir}/binary/usr/local/bin/ceph-authtool'.format(tdir=testdir),
             '--name={role}'.format(role=role),
             '--print-key',
-            '/tmp/cephtest/data/{role}.keyring'.format(role=role),
+            '{tdir}/data/{role}.keyring'.format(tdir=testdir, role=role),
             run.Raw('>'),
             filename,
             ],
@@ -569,25 +601,25 @@ def deep_merge(a, b):
         return a
     return b
 
-def get_valgrind_args(name, v):
+def get_valgrind_args(testdir, name, v):
     if v is None:
         return []
     if not isinstance(v, list):
         v = [v]
-    val_path = '/tmp/cephtest/archive/log/valgrind'
+    val_path = '{tdir}/archive/log/valgrind'.format(tdir=testdir)
     if '--tool=memcheck' in v or '--tool=helgrind' in v:
         extra_args = [
-            '/tmp/cephtest/chdir-coredump',
+            '{tdir}/chdir-coredump'.format(tdir=testdir),
             'valgrind',
-            '--suppressions=/tmp/cephtest/valgrind.supp',
+            '--suppressions={tdir}/valgrind.supp'.format(tdir=testdir),
             '--xml=yes',
             '--xml-file={vdir}/{n}.log'.format(vdir=val_path, n=name)
             ]
     else:
         extra_args = [
-            '/tmp/cephtest/chdir-coredump',
+            '{tdir}/chdir-coredump'.format(tdir=testdir),
             'valgrind',
-            '--suppressions=/tmp/cephtest/valgrind.supp',
+            '--suppressions={tdir}/valgrind.supp'.format(tdir=testdir),
             '--log-file={vdir}/{n}.log'.format(vdir=val_path, n=name)
             ]
     extra_args.extend(v)

teuthology/nuke.py
@@ -1,6 +1,8 @@
 import argparse
 import yaml
 
+from teuthology import misc as teuthology
+
 def parse_args():
     from teuthology.run import config_file
     from teuthology.run import MergeConfig
@@ -150,7 +152,10 @@ def remove_osd_mounts(ctx, log):
     from .orchestra import run
     ctx.cluster.run(
         args=[
-            'grep', '/tmp/cephtest/data/', '/etc/mtab', run.Raw('|'),
+            'grep',
+            '{tdir}/data/'.format(tdir=teuthology.get_testdir(ctx)),
+            '/etc/mtab',
+            run.Raw('|'),
             'awk', '{print $2}', run.Raw('|'),
             'xargs', '-r',
             'sudo', 'umount', run.Raw(';'),
@@ -222,7 +227,8 @@ def remove_testing_tree(ctx, log):
     for remote in ctx.cluster.remotes.iterkeys():
         proc = remote.run(
             args=[
-                'sudo', 'rm', '-rf', '/tmp/cephtest',
+                'sudo', 'rm', '-rf',
+                teuthology.get_testdir(ctx),
                 ],
             wait=False,
             )

teuthology/task/admin_socket.py
@@ -62,20 +62,21 @@ def task(ctx, config):
         for client, tests in config.iteritems():
             p.spawn(_run_tests, ctx, client, tests)
 
-def _socket_command(remote, socket_path, command, args):
+def _socket_command(ctx, remote, socket_path, command, args):
     """
     Run an admin socket command and return the result as a string.
     """
     json_fp = StringIO()
+    testdir = teuthology.get_testdir(ctx)
     remote.run(
         args=[
-            'LD_LIBRARY_PATH=/tmp/cephtest/binary/usr/local/lib',
-            '/tmp/cephtest/enable-coredump',
-            '/tmp/cephtest/binary/usr/local/bin/ceph-coverage',
-            '/tmp/cephtest/archive/coverage',
-            '/tmp/cephtest/binary/usr/local/bin/ceph',
-            '-k', '/tmp/cephtest/ceph.keyring',
-            '-c', '/tmp/cephtest/ceph.conf',
+            'LD_LIBRARY_PATH={tdir}/binary/usr/local/lib'.format(tdir=testdir),
+            '{tdir}/enable-coredump'.format(tdir=testdir),
+            '{tdir}/binary/usr/local/bin/ceph-coverage'.format(tdir=testdir),
+            '{tdir}/archive/coverage'.format(tdir=testdir),
+            '{tdir}/binary/usr/local/bin/ceph'.format(tdir=testdir),
+            '-k', '{tdir}/ceph.keyring'.format(tdir=testdir),
+            '-c', '{tdir}/ceph.conf'.format(tdir=testdir),
             '--admin-daemon', socket_path,
             command,
             ] + args,
@@ -87,13 +88,14 @@ def _socket_command(remote, socket_path, command, args):
     return json.loads(out)
 
 def _run_tests(ctx, client, tests):
+    testdir = teuthology.get_testdir(ctx)
     log.debug('Running admin socket tests on %s', client)
     (remote,) = ctx.cluster.only(client).remotes.iterkeys()
-    socket_path = '/tmp/cephtest/asok.{name}'.format(name=client)
+    socket_path = '{tdir}/asok.{name}'.format(tdir=testdir, name=client)
 
     try:
         tmp_dir = os.path.join(
-            '/tmp/cephtest/',
+            testdir,
             'admin_socket_{client}'.format(client=client),
             )
         remote.run(
@@ -135,7 +137,7 @@ def _run_tests(ctx, client, tests):
             args = config.get('args', [])
             assert isinstance(args, list), \
                 'admin socket command args must be a list'
-            sock_out = _socket_command(remote, socket_path, command, args)
+            sock_out = _socket_command(ctx, remote, socket_path, command, args)
             if test_path is not None:
                 remote.run(
                     args=[

teuthology/task/apache.conf.template
@@ -5,14 +5,14 @@ LoadModule fastcgi_module /usr/lib/apache2/modules/mod_fastcgi.so
 Listen 7280
 ServerName rgwtest.example.com
 
-ServerRoot /tmp/cephtest/apache
-ErrorLog /tmp/cephtest/archive/apache/error.log
+ServerRoot {testdir}/apache
+ErrorLog {testdir}/archive/apache/error.log
 LogFormat "%h l %u %t \"%r\" %>s %b \"{Referer}i\" \"%{User-agent}i\"" combined
-CustomLog /tmp/cephtest/archive/apache/access.log combined
-PidFile /tmp/cephtest/apache/tmp/apache.pid
-DocumentRoot /tmp/cephtest/apache/htdocs
-FastCgiIPCDir /tmp/cephtest/apache/tmp/fastcgi_sock
-FastCgiExternalServer /tmp/cephtest/apache/htdocs/rgw.fcgi -socket rgw_sock
+CustomLog {testdir}/archive/apache/access.log combined
+PidFile {testdir}/apache/tmp/apache.pid
+DocumentRoot {testdir}/apache/htdocs
+FastCgiIPCDir {testdir}/apache/tmp/fastcgi_sock
+FastCgiExternalServer {testdir}/apache/htdocs/rgw.fcgi -socket rgw_sock
 RewriteEngine On
 
 RewriteRule ^/([a-zA-Z0-9-_.]*)([/]?.*) /rgw.fcgi?page=$1&params=$2&%{QUERY_STRING} [E=HTTP_AUTHORIZATION:%{HTTP:Authorization},L]
@@ -23,7 +23,7 @@ SetEnv RGW_LOG_LEVEL 20
 SetEnv RGW_PRINT_CONTINUE yes
 SetEnv RGW_SHOULD_LOG yes
 
-<Directory /tmp/cephtest/apache/htdocs>
+<Directory {testdir}/apache/htdocs>
   Options +ExecCGI
   AllowOverride All
   SetHandler fastcgi-script

teuthology/task/autotest.py
@@ -37,10 +37,11 @@ def task(ctx, config):
     assert isinstance(config, dict)
     config = teuthology.replace_all_with_clients(ctx.cluster, config)
     log.info('Setting up autotest...')
+    testdir = teuthology.get_testdir(ctx)
     with parallel() as p:
         for role in config.iterkeys():
             (remote,) = ctx.cluster.only(role).remotes.keys()
-            p.spawn(_download, remote)
+            p.spawn(_download, testdir, remote)
 
     log.info('Making a separate scratch dir for every client...')
     for role in config.iterkeys():
@@ -49,7 +50,7 @@ def task(ctx, config):
         assert role.startswith(PREFIX)
         id_ = role[len(PREFIX):]
         (remote,) = ctx.cluster.only(role).remotes.iterkeys()
-        mnt = os.path.join('/tmp/cephtest', 'mnt.{id}'.format(id=id_))
+        mnt = os.path.join(testdir, 'mnt.{id}'.format(id=id_))
         scratch = os.path.join(mnt, 'client.{id}'.format(id=id_))
         remote.run(
             args=[
@@ -66,16 +67,16 @@ def task(ctx, config):
     with parallel() as p:
         for role, tests in config.iteritems():
             (remote,) = ctx.cluster.only(role).remotes.keys()
-            p.spawn(_run_tests, remote, role, tests)
+            p.spawn(_run_tests, testdir, remote, role, tests)
 
-def _download(remote):
+def _download(testdir, remote):
     remote.run(
         args=[
             # explicitly does not support multiple autotest tasks
             # in a single run; the result archival would conflict
-            'mkdir', '/tmp/cephtest/archive/autotest',
+            'mkdir', '{tdir}/archive/autotest'.format(tdir=testdir),
             run.Raw('&&'),
-            'mkdir', '/tmp/cephtest/autotest',
+            'mkdir', '{tdir}/autotest'.format(tdir=testdir),
             run.Raw('&&'),
             'wget',
             '-nv',
@@ -84,7 +85,7 @@ def _download(remote):
             '-O-',
             run.Raw('|'),
             'tar',
-            '-C', '/tmp/cephtest/autotest',
+            '-C', '{tdir}/autotest'.format(tdir=testdir),
             '-x',
             '-z',
             '-f-',
@@ -92,12 +93,12 @@ def _download(remote):
             ],
         )
 
-def _run_tests(remote, role, tests):
+def _run_tests(testdir, remote, role, tests):
     assert isinstance(role, basestring)
     PREFIX = 'client.'
     assert role.startswith(PREFIX)
     id_ = role[len(PREFIX):]
-    mnt = os.path.join('/tmp/cephtest', 'mnt.{id}'.format(id=id_))
+    mnt = os.path.join(testdir, 'mnt.{id}'.format(id=id_))
     scratch = os.path.join(mnt, 'client.{id}'.format(id=id_))
 
     assert isinstance(tests, list)
@@ -109,7 +110,7 @@ def _run_tests(remote, role, tests):
             testname=testname,
             id=id_,
             )
-        control = '/tmp/cephtest/control.{tag}'.format(tag=tag)
+        control = '{tdir}/control.{tag}'.format(tdir=testdir, tag=tag)
         teuthology.write_file(
             remote=remote,
             path=control,
@@ -118,14 +119,14 @@ def _run_tests(remote, role, tests):
                         url=testname,
                         dir=scratch,
                         # TODO perhaps tag
-                        # results will be in /tmp/cephtest/autotest/client/results/dbench
-                        # or /tmp/cephtest/autotest/client/results/dbench.{tag}
+                        # results will be in {testdir}/autotest/client/results/dbench
+                        # or {testdir}/autotest/client/results/dbench.{tag}
                         )),
                 ),
             )
         remote.run(
             args=[
-                '/tmp/cephtest/autotest/client/bin/autotest',
+                '{tdir}/autotest/client/bin/autotest'.format(tdir=testdir),
                 '--verbose',
                 '--harness=simple',
                 '--tag={tag}'.format(tag=tag),
@@ -144,13 +145,13 @@ def _run_tests(remote, role, tests):
             args=[
                 'mv',
                 '--',
-                '/tmp/cephtest/autotest/client/results/{tag}'.format(tag=tag),
-                '/tmp/cephtest/archive/autotest/{tag}'.format(tag=tag),
+                '{tdir}/autotest/client/results/{tag}'.format(tdir=testdir, tag=tag),
+                '{tdir}/archive/autotest/{tag}'.format(tdir=testdir, tag=tag),
                 ],
             )
 
     remote.run(
         args=[
-            'rm', '-rf', '--', '/tmp/cephtest/autotest',
+            'rm', '-rf', '--', '{tdir}/autotest'.format(tdir=testdir),
             ],
         )

commit 711a02449f38f20786f869e4e0355413f4d496ca

internal: unbreak syslog BUG checks and gz
This broke back in ace4cb07b2de99644c63f3ab90c21a663a384e69

Signed-off-by: Sage Weil <sage@inktank.com>

teuthology/task/internal.py
@@ -453,7 +453,7 @@ kern.* -{adir}/syslog/kern.log;RSYSLOG_FileFormat
                 args=[
                     'egrep',
                     '\\bBUG\\b|\\bINFO\\b|\\bDEADLOCK\\b',
-                    run.Raw('{adir}/archive/syslog/*.log'.format(adir=archive_dir)),
+                    run.Raw('{adir}/syslog/*.log'.format(adir=archive_dir)),
                     run.Raw('|'),
                     'grep', '-v', 'task .* blocked for more than .* seconds',
                     run.Raw('|'),
@@ -494,7 +494,7 @@ kern.* -{adir}/syslog/kern.log;RSYSLOG_FileFormat
             ctx.cluster.run(
                 args=[
                     'find',
-                    '{adir}/archive/syslog'.format(adir=archive_dir),
+                    '{adir}/syslog'.format(adir=archive_dir),
                     '-name',
                     '*.log',
                     '-print0',