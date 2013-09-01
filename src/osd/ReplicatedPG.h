8f422a36e27539e04fe15bf84b30c4eded04436b
Samuel Just <sam.just@inktank.com>
2013-09-01 13:33:42 -0700

    ReplicatedPG: make log_subop_stats static
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/osd/ReplicatedPG.h
+++ src/osd/ReplicatedPG.h
@@ -855,8 +855,6 @@ protected:
   void sub_op_pull(OpRequestRef op);
   void handle_pull(int peer, PullOp &op, PushOp *reply);
 
-  void log_subop_stats(OpRequestRef op, int tag_inb, int tag_lat);
-
   // -- copyfrom --
   map<hobject_t, CopyOpRef> copy_ops;
 
