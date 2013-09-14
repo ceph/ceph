7a69efaa485eaa169dd869685f6d1e572ce682ac
Samuel Just <sam.just@inktank.com>
2013-09-14 00:45:00 -0700

    OSD,ReplicatedPG: queue pushes in a op_tp workqueue
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/osd/OSD.h
+++ src/osd/OSD.h
@@ -307,6 +307,7 @@ public:
   ThreadPool::WorkQueue<PG> &scrub_wq;
   ThreadPool::WorkQueue<PG> &scrub_finalize_wq;
   ThreadPool::WorkQueue<MOSDRepScrub> &rep_scrub_wq;
+  GenContextWQ push_wq;
   ClassHandler  *&class_handler;
 
   void dequeue_pg(PG *pg, list<OpRequestRef> *dequeued);
