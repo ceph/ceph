befdcfd65872ed7ce429439c80d64401ada18753
Sage Weil <sage@inktank.com>
2013-09-20 23:08:53 -0700

    mon/PGMonitor: health warn if pool has relatively high objects/pg
    If there is a pool that has a high objects/pg relative to the rest of the
    cluster, warn, as this suggests this particular pool may have too few
    PGs.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/mon/PGMonitor.cc
+++ src/mon/PGMonitor.cc
@@ -1873,6 +1873,24 @@ void PGMonitor::get_health(list<pair<health_status_t,string> >& summary,
 	if (detail)
 	  detail->push_back(make_pair(HEALTH_WARN, ss.str()));
       }
+      int average_objects_per_pg = pg_map.pg_sum.stats.sum.num_objects / pg_map.pg_stat.size();
+      if (average_objects_per_pg > 0) {
+	int objects_per_pg = p->second.stats.sum.num_objects / pi->get_pg_num();
+	float ratio = (float)objects_per_pg / (float)average_objects_per_pg;
+	if (g_conf->mon_pg_warn_max_object_skew > 0 &&
+	    ratio > g_conf->mon_pg_warn_max_object_skew) {
+	  ostringstream ss;
+	  ss << "pool " << mon->osdmon()->osdmap.get_pool_name(p->first) << " has too few pgs";
+	  summary.push_back(make_pair(HEALTH_WARN, ss.str()));
+	  if (detail) {
+	    ostringstream ss;
+	    ss << "pool " << mon->osdmon()->osdmap.get_pool_name(p->first) << " objects per pg ("
+	       << objects_per_pg << ") is more than " << ratio << " times cluster average ("
+	       << average_objects_per_pg << ")";
+	    detail->push_back(make_pair(HEALTH_WARN, ss.str()));
+	  }
+	}
+      }
     }
   }
 }
