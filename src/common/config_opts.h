8e1c57877f9c07909aac6a7637bae7e170cfe2d9
Sage Weil <sage@inktank.com>
2013-09-23 16:23:33 -0700

    osd: revert 'osd max xattr size' limit
    Set it to 0 (unlimited) for now.
    
    Backport: dumpling
    
    Signed-off-by: Sage Weil <sage@inktank.com>
    Reviewed-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -521,7 +521,7 @@ OPTION(osd_recovery_op_warn_multiple, OPT_U32, 16)
 OPTION(osd_mon_shutdown_timeout, OPT_DOUBLE, 5)
 
 OPTION(osd_max_object_size, OPT_U64, 100*1024L*1024L*1024L) // OSD's maximum object size
-OPTION(osd_max_attr_size, OPT_U64, 65536)
+OPTION(osd_max_attr_size, OPT_U64, 0)
 
 OPTION(filestore, OPT_BOOL, false)
 
