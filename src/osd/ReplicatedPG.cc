718673e730fd95ed4a4086c2d31eb8739f04565d
Sage Weil <sage@inktank.com>
2013-09-23 16:23:33 -0700

    osd: revert 'osd max xattr size' limit
    Set it to 0 (unlimited) for now.
    
    Backport: dumpling
    
    Signed-off-by: Sage Weil <sage@inktank.com>
    Reviewed-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -2898,7 +2898,8 @@ int ReplicatedPG::do_osd_ops(OpContext *ctx, vector<OSDOp>& ops)
     case CEPH_OSD_OP_SETXATTR:
       ++ctx->num_write;
       {
-	if (op.xattr.value_len > g_conf->osd_max_attr_size) {
+	if (g_conf->osd_max_attr_size > 0 &&
+	    op.xattr.value_len > g_conf->osd_max_attr_size) {
 	  result = -EFBIG;
 	  break;
 	}
