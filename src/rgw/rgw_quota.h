e5fa6d166cee0d5b8a6fd6c15f76ebfea6f6c61b
Yehuda Sadeh <yehuda@inktank.com>
2013-09-27 10:41:14 -0700

    rgw: more quota utility stuff
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/rgw/rgw_quota.h
+++ src/rgw/rgw_quota.h
@@ -12,6 +12,29 @@ struct RGWQuotaBucketStats {
   utime_t expiration;
 };
 
+struct RGWQuotaInfo {
+  uint64_t max_kb;
+  uint64_t max_objs;
+  bool is_set;
+
+  RGWQuotaInfo() : max_kb(0), max_objs(0), is_set(false) {}
+
+  void encode(bufferlist& bl) const {
+    ENCODE_START(1, 1, bl);
+    ::encode(max_kb, bl);
+    ::encode(max_objs, bl);
+    ::encode(is_set, bl);
+    ENCODE_FINISH(bl);
+  }
+  void decode(bufferlist::iterator& bl) {
+    DECODE_START(1, bl);
+    ::decode(max_kb, bl);
+    ::decode(max_objs, bl);
+    ::decode(is_set, bl);
+    DECODE_FINISH(bl);
+  }
+};
+WRITE_CLASS_ENCODER(RGWQuotaInfo)
 
 class RGWBucketStatsCache {
   RGWRados *store;
@@ -24,8 +47,8 @@ public:
   RGWBucketStatsCache(RGWRados *_store) : store(_store), stats_map(10000) {}
 
   int get_bucket_stats(rgw_bucket& bucket, RGWBucketStats& stats);
+  void adjust_bucket_stats(rgw_bucket& bucket, int objs_delta, uint64_t added_bytes, uint64_t removed_bytes);
 };
-
 
 
 
