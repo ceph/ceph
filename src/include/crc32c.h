0a1b80e318df8e8f588218176e9c151ca93b72b9
Sage Weil <sage@inktank.com>
2013-09-27 21:05:27 -0700

    common/crc32c: document interface
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/include/crc32c.h
+++ src/include/crc32c.h
@@ -14,8 +14,15 @@ extern ceph_crc32c_func_t ceph_crc32c_func;
 
 extern ceph_crc32c_func_t ceph_choose_crc32(void);
 
-/*
- * common entry point; use this!
+/**
+ * calculate crc32c
+ *
+ * Note: if the data pointer is NULL, we calculate a crc value as if
+ * it were zero-filled.
+ *
+ * @param crc initial value
+ * @param data pointer to data buffer
+ * @param length length of buffer
  */
 static inline uint32_t ceph_crc32c(uint32_t crc, unsigned char const *data, unsigned length)
 {
