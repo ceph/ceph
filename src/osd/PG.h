1f9e137b51d580bce6505773217bcca008225a47
Samuel Just <sam.just@inktank.com>
2013-10-03 22:00:46 -0700

    ReplicatedPG: remove the other backfill related flushes
    Signed-off-by: Samuel Just <sam.just@inktank.com>
    (cherry picked from commit 1f50750d0fd94ffddc14f3d7a1e95fa4449aa1b8)
    
    Conflicts:
    	src/osd/ReplicatedPG.cc

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -451,9 +451,7 @@ protected:
     
     /// clear content
     void clear() {
-      objects.clear();
-      begin = end = hobject_t();
-      version = eversion_t();
+      *this = BackfillInterval();
     }
 
     void reset(hobject_t start) {
