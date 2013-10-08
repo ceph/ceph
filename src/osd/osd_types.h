7279534047e49dd95ac8d558a772e47fa9c412df
Greg Farnum <greg@inktank.com>
2013-10-08 14:57:31 -0700

    osd: Add a new object_copy_data_t, and use it in the OSD/Objecter
    Right now this is very primitive, but we're about to extend it to
    deal with request versioning appropriately, and adding in some
    extra fields.
    Sadly we are doing a little extra copying in the Objecter as a result, but
    too bad -- being able to do updates will be worth it.
    
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- src/osd/osd_types.h
+++ src/osd/osd_types.h
@@ -1857,6 +1857,36 @@ struct object_copy_cursor_t {
 };
 WRITE_CLASS_ENCODER(object_copy_cursor_t)
 
+/**
+ * object_copy_data_t
+ *
+ * Return data from a copy request. The semantics are a little strange
+ * as a result of the encoding's heritage.
+ *
+ * In particular, the sender unconditionally fills in the cursor (from what
+ * it receives and sends), the size, and the mtime, but is responsible for
+ * figuring out whether it should put any data in the attrs, data, or
+ * omap members (corresponding to xattrs, object data, and the omap entries)
+ * based on external data (the client includes a max amount to return with
+ * the copy request). The client then looks into the attrs, data, and/or omap
+ * based on the contents of the cursor.
+ */
+struct object_copy_data_t {
+  object_copy_cursor_t cursor;
+  uint64_t size;
+  utime_t mtime;
+  map<string, bufferlist> attrs;
+  bufferlist data;
+  map<string, bufferlist> omap;
+public:
+  object_copy_data_t() : size((uint64_t)-1) {}
+
+  static void generate_test_instances(list<object_copy_data_t*>& o);
+  void encode(bufferlist& bl) const;
+  void decode(bufferlist::iterator& bl);
+  void dump(Formatter *f) const;
+};
+WRITE_CLASS_ENCODER(object_copy_data_t)
 
 /**
  * pg creation info
