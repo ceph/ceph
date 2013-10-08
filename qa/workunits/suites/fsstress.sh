102f63993cacf3b3404256e62a3096c5836f2282
Sandon Van Ness <sandon@inktank.com>
2013-10-08 12:08:08 -0700

    Go back to $PWD in fsstress.sh if compiling from source.
    Although fsstress was being called with a static path the directory
    it was writing to was in the current directory so doing a cd to the
    source directory that is made in /tmp and then removing it later
    caused it to be unable to write the files in a non-existent dir.
    
    This change gets the current path first and cd's back into it after
    it is done compiling fsstress.
    
    Issue #6479.
    
    Signed-off-by: Sandon Van Ness <sandon@inktank.com>
    Reviewed-by: Alfredo Deza <alfredo.deza@inktank.com>

--- qa/workunits/suites/fsstress.sh
+++ qa/workunits/suites/fsstress.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+if [ ! -f /usr/lib/ltp/testcases/bin/fsstress ]
+then
+    path=`pwd`
+    mkdir -p /tmp/fsstress
+    cd /tmp/fsstress
+    wget -q -O /tmp/fsstress/ltp-full.tgz http://ceph.com/qa/ltp-full-20091231.tgz
+    tar xzf /tmp/fsstress/ltp-full.tgz
+    rm /tmp/fsstress/ltp-full.tgz
+    cd /tmp/fsstress/ltp-full-20091231/testcases/kernel/fs/fsstress
+    make
+    sudo mkdir -p /usr/lib/ltp/testcases/bin
+    sudo cp -avf /tmp/fsstress/ltp-full-20091231/testcases/kernel/fs/fsstress/fsstress /usr/lib/ltp/testcases/bin/fsstress
+    sudo chmod 755 /usr/lib/ltp/testcases/bin/fsstress
+    rm -Rf /tmp/fsstress
+    cd $path
+fi
+
 command="/usr/lib/ltp/testcases/bin/fsstress -d fsstress-`hostname`$$ -l 1 -n 1000 -p 10 -v"
 
 echo "Starting fsstress $command"
