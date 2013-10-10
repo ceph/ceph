7e3894101a96eafb51192fc0663b7d89b1aecb66
Yehuda Sadeh <yehuda@inktank.com>
2013-10-10 10:50:39 -0700

    rgw: bucket quota threshold
    Add bucket quota threshold so that when we're passed that value we
    reread the bucket stats before every write and not rely on cached value.
    
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -716,6 +716,7 @@ OPTION(rgw_data_log_obj_prefix, OPT_STR, "data_log") //
 OPTION(rgw_replica_log_obj_prefix, OPT_STR, "replica_log") // 
 
 OPTION(rgw_bucket_quota_ttl, OPT_INT, 600) // time for cached bucket stats to be cached within rgw instance
+OPTION(rgw_bucket_quota_soft_threshold, OPT_DOUBLE, 0.95) // threshold from which we don't rely on cached info for quota decisions
 OPTION(rgw_bucket_quota_cache_size, OPT_INT, 10000) // number of entries in bucket quota cache
 
 OPTION(mutex_perf_counter, OPT_BOOL, false) // enable/disable mutex perf counter
