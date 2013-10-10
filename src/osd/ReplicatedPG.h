e1c0ea93c453f2757b07e2823a8550616a8b40d7
Greg Farnum <greg@inktank.com>
2013-10-10 09:34:02 -0700

    ReplicatedPG: promote: move the promote functionality into finish_promote
    This way we can have a couple of functions that handle each type of
    case, and let the PromoteCallback choose between them. That's much
    better than doing it all inline in the callback.
    
    Signed-off-by: Greg Farnum <greg@inktank.com>
    Reviewed-by: Sage Weil <sage@inktank.com>

--- src/osd/ReplicatedPG.h
+++ src/osd/ReplicatedPG.h
@@ -221,41 +221,8 @@ public:
 
     virtual void finish(CopyCallbackResults results) {
       CopyResults* results_data = results.get<1>();
-      vector<OSDOp> ops;
-      tid_t rep_tid = pg->osd->get_tid();
-      osd_reqid_t reqid(pg->osd->get_cluster_msgr_name(), 0, rep_tid);
-      OpContext *tctx = new OpContext(OpRequestRef(), reqid, ops, &obc->obs, obc->ssc, pg);
-      tctx->mtime = ceph_clock_now(g_ceph_context);
-      tctx->op_t.swap(results_data->final_tx);
-      if (results_data->started_temp_obj) {
-	tctx->discard_temp_oid = temp_obj;
-      }
-
-      RepGather *repop = pg->new_repop(tctx, obc, rep_tid);
-      C_KickBlockedObject *blockedcb = new C_KickBlockedObject(obc, pg);
-      repop->ondone = blockedcb;
-      object_stat_sum_t delta;
-      ++delta.num_objects;
-      obc->obs.exists = true;
-      delta.num_bytes += results_data->object_size;
-      obc->obs.oi.category = results_data->category;
-      pg->info.stats.stats.add(delta, obc->obs.oi.category);
-      tctx->at_version.epoch = pg->get_osdmap()->get_epoch();
-      tctx->at_version.version = pg->pg_log.get_head().version + 1;
-      tctx->user_at_version = results_data->user_version;
-
-      tctx->log.push_back(pg_log_entry_t(
-	  pg_log_entry_t::MODIFY,
-	  obc->obs.oi.soid,
-	  tctx->at_version,
-	  tctx->obs->oi.version,
-	  tctx->user_at_version,
-	  osd_reqid_t(),
-	  repop->ctx->mtime));
-      pg->append_log(tctx->log, eversion_t(), tctx->local_t);
-      pg->issue_repop(repop, repop->ctx->mtime);
-      pg->eval_repop(repop);
-      repop->put();
+      assert(results.get<0>() == 0); // we don't handle errors right now
+      pg->finish_promote(results_data, obc, temp_obj);
       delete results_data;
     }
   };
@@ -1016,6 +983,8 @@ protected:
   void _build_finish_copy_transaction(CopyOpRef cop,
                                       ObjectStore::Transaction& t);
   void finish_copyfrom(OpContext *ctx);
+  void finish_promote(CopyResults *results, ObjectContextRef obc,
+                      hobject_t& temp_obj);
   void cancel_copy(CopyOpRef cop, bool requeue);
   void cancel_copy_ops(bool requeue);
 
