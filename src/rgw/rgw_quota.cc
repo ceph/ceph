a48acffb1a9557d71e455db48aefd83aea29e8c8
Yehuda Sadeh <yehuda@inktank.com>
2013-10-11 16:43:00 -0700

    rgw_quoa.{h,cc}: add copyright notice
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/rgw/rgw_quota.cc
+++ src/rgw/rgw_quota.cc
@@ -1,3 +1,17 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2013 Inktank, Inc
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
 
 #include "include/utime.h"
 #include "common/lru_map.h"
