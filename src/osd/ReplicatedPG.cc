34b12b35ec29366c09e5e5eb7fbb6d15fdc1d578
Sage Weil <sage@inktank.com>
2013-10-24 21:38:30 -0700

    osd/ReplicatedPG: split off finish_ctx from execute_ctx
    The second part of execute_ctx() is doing some somewhat generic work to
    make the prepared updates in the ctx apply, updating the obc's cached
    values.  Factor it out.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -4264,6 +4264,16 @@ int ReplicatedPG::prepare_transaction(OpContext *ctx)
   // clone, if necessary
   make_writeable(ctx);
 
+  finish_ctx(ctx);
+
+  return result;
+}
+
+void ReplicatedPG::finish_ctx(OpContext *ctx)
+{
+  const hobject_t& soid = ctx->obs->oi.soid;
+  dout(20) << __func__ << " " << soid << " " << ctx << dendl;
+
   // snapset
   bufferlist bss;
   ::encode(ctx->new_snapset, bss);
@@ -4377,8 +4387,6 @@ int ReplicatedPG::prepare_transaction(OpContext *ctx)
     if (soid < scrubber.start)
       scrub_cstat.add(ctx->delta_stats, ctx->obs->oi.category);
   }
-
-  return result;
 }
 
 // ========================================================================
--- src/osd/ReplicatedPG.h
+++ src/osd/ReplicatedPG.h
@@ -794,6 +794,7 @@ protected:
 		   const hobject_t& head, const hobject_t& coid,
 		   object_info_t *poi);
   void execute_ctx(OpContext *ctx);
+  void finish_ctx(OpContext *ctx);
   void reply_ctx(OpContext *ctx, int err);
   void reply_ctx(OpContext *ctx, int err, eversion_t v, version_t uv);
   void make_writeable(OpContext *ctx);
