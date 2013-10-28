46dfd9197548a9d7cf4e5aa86497d73d8cc5d6ae
Samuel Just <sam.just@inktank.com>
2013-10-28 15:49:23 -0700

    PG::BackfillInterval: use trim() in pop_front()
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -479,10 +479,7 @@ protected:
     void pop_front() {
       assert(!objects.empty());
       objects.erase(objects.begin());
-      if (objects.empty())
-	begin = end;
-      else
-	begin = objects.begin()->first;
+      trim();
     }
 
     /// dump
