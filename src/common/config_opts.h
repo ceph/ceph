ae18becac1de8c8dc06f7109a73a96b9cc31cd26
Sage Weil <sage@inktank.com>
2013-10-30 17:21:05 -0700

    librbd: localize or distribute parent (snap) reads
    The parent is always a snapshot.  We may want to treat it differently
    than other snaps by virtue of it (likely) being a more highly-shared
    image.
    
    By default, localize parent reads.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -643,6 +643,8 @@ OPTION(rbd_cache_block_writes_upfront, OPT_BOOL, false) // whether to block writ
 OPTION(rbd_concurrent_management_ops, OPT_INT, 10) // how many operations can be in flight for a management operation like deleting or resizing an image
 OPTION(rbd_balance_snap_reads, OPT_BOOL, false)
 OPTION(rbd_localize_snap_reads, OPT_BOOL, false)
+OPTION(rbd_balance_parent_reads, OPT_BOOL, false)
+OPTION(rbd_localize_parent_reads, OPT_BOOL, true)
 
 /*
  * The following options change the behavior for librbd's image creation methods that
