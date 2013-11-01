cd30e5fbb1d2d29b4fc0c2b13fa3b9b4493c24f0
Samuel Just <sam.just@inktank.com>
2013-11-01 13:39:16 -0700

    common/buffer.cc: fix rebuild_page_aligned typo
    Introduced: 66a9fbe2c7ba59b7cd034c17865adce3432cd2cb
    Fixes: #6003
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/common/buffer.cc
+++ src/common/buffer.cc
@@ -854,7 +854,7 @@ void buffer::list::rebuild_page_aligned()
 	     (!p->is_page_aligned() ||
 	      !p->is_n_page_sized() ||
 	      (offset & ~CEPH_PAGE_MASK)));
-    ptr nb(buffer::create_page_aligned(_len));
+    ptr nb(buffer::create_page_aligned(unaligned._len));
     unaligned.rebuild(nb);
     _buffers.insert(p, unaligned._buffers.front());
   }
