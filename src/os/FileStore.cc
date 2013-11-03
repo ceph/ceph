ad601a34e3bbb12f6d88a9df9a945d57ac3784bb
Sage Weil <sage@inktank.com>
2013-11-02 19:29:11 -0700

    osd: move peek_journal_fsid() into ObjectStore (from OSD)
    This kills the last FileJournal reference in the OSD!
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/os/FileStore.cc
+++ src/os/FileStore.cc
@@ -104,6 +104,15 @@ static CompatSet get_fs_supported_compat_set() {
 }
 
 
+int FileStore::peek_journal_fsid(uuid_d *fsid)
+{
+  // make sure we don't try to use aio or direct_io (and get annoying
+  // error messages from failing to do so); performance implications
+  // should be irrelevant for this use
+  FileJournal j(*fsid, 0, 0, journalpath.c_str(), false, false);
+  return j.peek_fsid(*fsid);
+}
+
 void FileStore::FSPerfTracker::update_from_perfcounters(
   PerfCounters &logger)
 {
