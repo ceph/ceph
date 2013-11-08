40defcde3faeb90e58610e79e5305fa2ad0677ab
Samuel Just <sam.just@inktank.com>
2013-11-08 15:17:15 -0800

    OSD: remove wake_all_pg_waiters
    We shouldn't need this -- we check the pg waiters list on each
    map.
    
    Signed-off-by: Samuel Just <sam.just@inktank.com>
    Reviewed-by: Greg Farnum <greg@inktank.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -5947,8 +5947,6 @@ void OSD::activate_map()
 
   dout(7) << "activate_map version " << osdmap->get_epoch() << dendl;
 
-  wake_all_pg_waiters();   // the pg mapping may have shifted
-
   if (osdmap->test_flag(CEPH_OSDMAP_FULL)) {
     dout(10) << " osdmap flagged full, doing onetime osdmap subscribe" << dendl;
     osdmap_subscribe(osdmap->get_epoch() + 1, true);
@@ -7154,9 +7152,6 @@ void OSD::check_replay_queue()
       dout(10) << "check_replay_queue pgid " << pgid << " (not found)" << dendl;
     }
   }
-  
-  // wake up _all_ pg waiters; raw pg -> actual pg mapping may have shifted
-  wake_all_pg_waiters();
 }
 
 
