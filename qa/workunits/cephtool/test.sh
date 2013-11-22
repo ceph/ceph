99861e3b285d117a3588c9295b34b6477047489d
Greg Farnum <greg@inktank.com>
2013-11-22 15:24:07 -0800

    workunits: use integers instead of true/false for hashpspool command
    Backport: emperor
    
    Signed-off-by: Greg Farnum <greg@inktank.com>
    Reviewed-by: Samuel Just <sam.just@inktank.com

--- qa/workunits/cephtool/test.sh
+++ qa/workunits/cephtool/test.sh
@@ -327,8 +327,8 @@ ceph osd pool set data size 3
 ceph osd pool get data size | grep 'size: 3'
 ceph osd pool set data size 2
 
-ceph osd pool set data hashpspool true
-ceph osd pool set data hashpspool false
+ceph osd pool set data hashpspool 1
+ceph osd pool set data hashpspool 0
 
 ceph osd pool get rbd crush_ruleset | grep 'crush_ruleset: 2'
 
