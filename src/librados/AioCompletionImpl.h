ab1b35a5ce235453aac74066bbf95af299002c22
Rutger ter Borg <rutger@terborg.net>
2013-11-27 00:49:00 -0800

    librados: read into user's bufferlist for aio_read
    * The 'buf' argument to read() used to be passed into
      AioCompletionImpl, and the results would be copied back after
      reading. This is replaced with the creation of a static buffer of
      that buf.
    
    * The pbl argument in AioCompletionImpl is removed.
    
    The patch is tested against an application using librados. I've
    assumed that 'pbl' in
    
    aio_read( ...., pbl, )
    
    is allocated by the user. It may even speed things up: a buffer copy
    is prevented.
    
    Reviewed-by: Josh Durgin <josh.durgin@inktank.com>

--- src/librados/AioCompletionImpl.h
+++ src/librados/AioCompletionImpl.h
@@ -39,8 +39,7 @@ struct librados::AioCompletionImpl {
 
   // for read
   bool is_read;
-  bufferlist bl, *pbl;
-  char *buf;
+  bufferlist bl;
   unsigned maxlen;
 
   IoCtxImpl *io;
@@ -54,7 +53,7 @@ struct librados::AioCompletionImpl {
 			callback_safe(0),
 			callback_complete_arg(0),
 			callback_safe_arg(0),
-			is_read(false), pbl(0), buf(0), maxlen(0),
+			is_read(false), maxlen(0),
 			io(NULL), aio_write_seq(0), aio_write_list_item(this) { }
 
   int set_complete_callback(void *cb_arg, rados_callback_t cb) {
