c3eb7da79457255a4d0a50a1d001288484b50888
Sage Weil <sage@inktank.com>
2013-12-06 21:33:04 -0800

    osd/OSDMap: add feature bit for TUNABLES3 (== CRUSH_V2)
    Add a feature bit for the new tunables.  Re-use the same feature bit
    as CRUSH_V2 because the support is part of the same code update.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/include/ceph_features.h
+++ src/include/ceph_features.h
@@ -40,6 +40,7 @@
 #define CEPH_FEATURE_MON_SCRUB      (1ULL<<33)
 #define CEPH_FEATURE_OSD_PACKED_RECOVERY (1ULL<<34)
 #define CEPH_FEATURE_OSD_CACHEPOOL (1ULL<<35)
+#define CEPH_FEATURE_CRUSH_TUNABLES3 (1ULL<<36) /* same as V2 */
 #define CEPH_FEATURE_CRUSH_V2      (1ULL<<36)  /* new indep; SET_* steps */
 
 /*
@@ -115,6 +116,7 @@ static inline unsigned long long ceph_sanitize_features(unsigned long long f) {
 #define CEPH_FEATURES_CRUSH			\
 	(CEPH_FEATURE_CRUSH_TUNABLES |		\
 	 CEPH_FEATURE_CRUSH_TUNABLES2 |		\
+	 CEPH_FEATURE_CRUSH_TUNABLES3 |		\
 	 CEPH_FEATURE_CRUSH_V2)
 
 #endif
