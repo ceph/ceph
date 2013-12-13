99cee55c8aac6d64d7d28b2319dc0146331e60e7
Sage Weil <sage@inktank.com>
2013-12-13 13:40:01 -0800

    osd/osd_types: debug: include size in object_info_t operator<<
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/osd/osd_types.cc
+++ src/osd/osd_types.cc
@@ -3260,6 +3260,7 @@ ostream& operator<<(ostream& out, const object_info_t& oi)
     out << " " << oi.snaps;
   if (oi.flags)
     out << " " << oi.get_flag_string();
+  out << " s " << oi.size;
   out << " uv" << oi.user_version;
   out << ")";
   return out;
