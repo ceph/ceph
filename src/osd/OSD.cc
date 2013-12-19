b780f4bf3f2a9b56798fd9a55888291e22ef1352
David Zafman <david.zafman@inktank.com>
2013-12-19 10:35:39 -0800

    osd: Remove redundant incompat feature
    We can remove this CompatSet bit without worry because the only
    way it could have been set is if an erasure coded pool was create.
    This isn't supported as of yet.
    
    Signed-off-by: David Zafman <david.zafman@inktank.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -163,7 +163,6 @@ CompatSet OSD::get_osd_compat_set() {
   CompatSet compat =  get_osd_initial_compat_set();
   //Any features here can be set in code, but not in initial superblock
   compat.incompat.insert(CEPH_OSD_FEATURE_INCOMPAT_SHARDS);
-  compat.incompat.insert(CEPH_OSD_FEATURE_INCOMPAT_ERASURECODES);
   return compat;
 }
 
@@ -5380,9 +5379,9 @@ void OSD::check_osdmap_features()
   }
 
   if ((features & CEPH_FEATURE_OSD_ERASURE_CODES) &&
-      (!superblock.compat_features.incompat.contains(CEPH_OSD_FEATURE_INCOMPAT_ERASURECODES))) {
+      (!superblock.compat_features.incompat.contains(CEPH_OSD_FEATURE_INCOMPAT_SHARDS))) {
     dout(0) << __func__ << " enabling on-disk ERASURE CODES compat feature" << dendl;
-    superblock.compat_features.incompat.insert(CEPH_OSD_FEATURE_INCOMPAT_ERASURECODES);
+    superblock.compat_features.incompat.insert(CEPH_OSD_FEATURE_INCOMPAT_SHARDS);
     ObjectStore::Transaction t;
     write_superblock(t);
     int err = store->apply_transaction(t);
