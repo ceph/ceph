84423ec0e7d3fd6198625d34d582229a3b58b7ef
Sage Weil <sage@inktank.com>
2013-12-26 18:02:16 -0800

    osd/ReplicatedPG: adjust clone stats when promoting clones
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -5031,6 +5031,8 @@ void ReplicatedPG::finish_promote(int r, OpRequestRef op,
   tctx->at_version = get_next_version();
 
   ++tctx->delta_stats.num_objects;
+  if (soid.snap < CEPH_NOSNAP)
+    ++tctx->delta_stats.num_object_clones;
   tctx->new_obs.exists = true;
   tctx->new_snapset.head_exists = true;
 
