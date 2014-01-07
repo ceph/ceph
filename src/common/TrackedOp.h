e3a430e49ddba24a06cf618999bf5c1a77616d13
Greg Farnum <greg@inktank.com>
2014-01-07 10:05:57 -0800

    TrackedOp: optionally disable the actual tracking operations
    To avoid op contention on global locks, optionally disable the
    op tracking. Create an "osd_op_tracker" config to control it, then
    in the OpTracker constructor set a bool. If it's set, the OpTracker
    doesn't actually maintain its lists and avoids taking any locks. We
    maintain the within-op tracking for now since it shouldn't contend,
    but we can turn that off later on if we like.
    
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- src/common/TrackedOp.h
+++ src/common/TrackedOp.h
@@ -68,9 +68,10 @@ class OpTracker {
   int log_threshold;
 
 public:
+  bool tracking_enabled;
   CephContext *cct;
-  OpTracker(CephContext *cct_) : seq(0), ops_in_flight_lock("OpTracker mutex"),
-      history(this), complaint_time(0), log_threshold(0), cct(cct_) {}
+  OpTracker(CephContext *cct_, bool tracking) : seq(0), ops_in_flight_lock("OpTracker mutex"),
+      history(this), complaint_time(0), log_threshold(0), tracking_enabled(tracking_enabled), cct(cct_) {}
   void set_complaint_and_threshold(float time, int threshold) {
     complaint_time = time;
     log_threshold = threshold;
