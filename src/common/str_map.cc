7ff86afe59a3037d7e7b534fcfc294e322292cd6
Loic Dachary <loic@dachary.org>
2014-01-13 17:54:08 +0100

    common: s/stringstream/ostream/ in str_map
    There is no need to specialize more than ostream : it only makes it
    impossible to use cerr or cout as a parameter to str_map.
    
    Reviewed-By: Christophe Courtaut <christophe.courtaut@gmail.com>
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- src/common/str_map.cc
+++ src/common/str_map.cc
@@ -24,7 +24,7 @@
 using namespace std;
 
 int get_str_map(const string &str,
-                stringstream &ss,
+                ostream &ss,
                 map<string,string> *str_map)
 {
   json_spirit::mValue json;
