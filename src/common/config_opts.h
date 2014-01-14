7c6ef6a1afef0597ed3173df5532cc2f89c568a5
Yehuda Sadeh <yehuda@inktank.com>
2014-01-13 21:50:32 -0800

    rgw: sync bucket stats thread
    a new thread that periodically sync stats of recently modified buckets.
    
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -730,6 +730,7 @@ OPTION(rgw_replica_log_obj_prefix, OPT_STR, "replica_log") //
 OPTION(rgw_bucket_quota_ttl, OPT_INT, 600) // time for cached bucket stats to be cached within rgw instance
 OPTION(rgw_bucket_quota_soft_threshold, OPT_DOUBLE, 0.95) // threshold from which we don't rely on cached info for quota decisions
 OPTION(rgw_bucket_quota_cache_size, OPT_INT, 10000) // number of entries in bucket quota cache
+OPTION(rgw_user_quota_bucket_sync_interval, OPT_INT, 180) // time period for accumulating modified buckets before syncing stats
 
 OPTION(mutex_perf_counter, OPT_BOOL, false) // enable/disable mutex perf counter
 
