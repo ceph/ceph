53fd5861e3e632ab4781f75e2bd0b22f6a87920f
Moritz Möller <mm@mxs.de>
2014-01-16 16:09:36 +0100

    client: ceph-fuse: implement flock / setlk / getlk

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -5601,6 +5601,19 @@ int Client::_release_fh(Fh *f)
   Inode *in = f->inode;
   ldout(cct, 5) << "_release_fh " << f << " mode " << f->mode << " on " << *in << dendl;
 
+  // unlock if still locked
+  if (f->flock_locked) {
+    ll_flock(f, LOCK_UN);
+  }
+  if (f->fcntl_locked) {
+    struct flock lock;
+    lock.l_type = F_UNLCK;
+    lock.l_start = 0;
+    lock.l_whence = SEEK_SET;
+    lock.l_len = 0;
+    ll_setlk(f, &lock, 1);
+  }
+
   if (in->snapid == CEPH_NOSNAP) {
     if (in->put_open_ref(f->mode)) {
       _flush(in);
@@ -7806,6 +7819,117 @@ int Client::ll_fsync(Fh *fh, bool syncdataonly)
   return _fsync(fh, syncdataonly);
 }
 
+int Client::ll_flock(Fh *fh, int op, int uid, int gid, int pid)
+{
+  if (!cct->_conf->fuse_multithreaded) {
+    dout(0) << "WARNING: using setlk/getlk/flock without configuring fuse multithreading will result in a deadlock. please set 'fuse multithreaded=1' in your ceph.conf" << dendl;
+  }
+
+  Mutex::Locker lock(client_lock);
+  tout(cct) << "ll_flock" << std::endl;
+  tout(cct) << (unsigned long)fh << std::endl;
+
+  int wait = 1;
+  if (op & LOCK_NB) wait = 0;
+  int type;
+  switch (op & (LOCK_SH | LOCK_EX | LOCK_UN)) {
+    case LOCK_SH: type = CEPH_LOCK_SHARED; break;
+    case LOCK_EX: type = CEPH_LOCK_EXCL; break;
+    case LOCK_UN: type = CEPH_LOCK_UNLOCK; break;
+  }
+
+  MetaRequest *req = new MetaRequest(CEPH_MDS_OP_SETFILELOCK);
+  filepath path(fh->inode->ino);
+  req->set_filepath(path);
+  req->set_inode(fh->inode);
+  req->head.args.filelock_change.rule = CEPH_LOCK_FLOCK;
+  req->head.args.filelock_change.type = type;
+  req->head.args.filelock_change.pid = pid;
+  req->head.args.filelock_change.pid_namespace = 0; // some unique id for this host?
+  req->head.args.filelock_change.start = 0;
+  req->head.args.filelock_change.length = 0;
+  req->head.args.filelock_change.wait = wait;
+  int r = make_request(req, uid, gid);
+  if (r == 0 && type != CEPH_LOCK_UNLOCK) fh->flock_locked = true; 
+  if (r == 0 && type == CEPH_LOCK_UNLOCK) fh->flock_locked = false;
+  return r;
+}
+
+int Client::ll_setlk(Fh *fh, struct flock *lock, int sleep, int uid, int gid, int pid)
+{
+  if (!cct->_conf->fuse_multithreaded) {
+    dout(0) << "WARNING: using setlk/getlk/flock without configuring fuse multithreading will result in a deadlock. please set 'fuse multithreaded=1' in your ceph.conf" << dendl;
+  }
+
+  Mutex::Locker locker(client_lock);
+  tout(cct) << "ll_setlk" << std::endl;
+  tout(cct) << (unsigned long)fh << std::endl;
+
+  int type;
+  switch (lock->l_type) {
+    case F_RDLCK: type = CEPH_LOCK_SHARED; break;
+    case F_WRLCK: type = CEPH_LOCK_EXCL; break;
+    case F_UNLCK: type = CEPH_LOCK_UNLOCK; break;
+  }
+
+  if (lock->l_whence != SEEK_SET) {
+    dout(0) << "WARNING: using setlk with l_whence != SEEK_SET is not supported" << dendl;
+    return -EINVAL;
+  }
+
+  MetaRequest *req = new MetaRequest(CEPH_MDS_OP_SETFILELOCK);
+  filepath path(fh->inode->ino);
+  req->set_filepath(path);
+  req->set_inode(fh->inode);
+  req->head.args.filelock_change.rule = CEPH_LOCK_FCNTL;
+  req->head.args.filelock_change.type = type;
+  req->head.args.filelock_change.pid = pid; // or l_pid?
+  req->head.args.filelock_change.pid_namespace = 0; // some unique id for this host?
+  req->head.args.filelock_change.start = lock->l_start; // l_whence
+  req->head.args.filelock_change.length = lock->l_len;
+  req->head.args.filelock_change.wait = sleep;
+  int r = make_request(req, uid, gid);
+  if (r == 0 && type != CEPH_LOCK_UNLOCK) fh->fcntl_locked = true; 
+  if (r == 0 && type == CEPH_LOCK_UNLOCK) fh->fcntl_locked = false;
+  return r;
+}
+
+int Client::ll_getlk(Fh *fh, struct flock *lock, int uid, int gid, int pid)
+{
+  Mutex::Locker locker(client_lock);
+  tout(cct) << "ll_getlk" << std::endl;
+  tout(cct) << (unsigned long)fh << std::endl;
+
+  int type;
+  switch (lock->l_type) {
+    case F_RDLCK: type = CEPH_LOCK_SHARED; break;
+    case F_WRLCK: type = CEPH_LOCK_EXCL; break;
+    case F_UNLCK: type = CEPH_LOCK_UNLOCK; break;
+  }
+
+  if (lock->l_whence != SEEK_SET) {
+    dout(0) << "WARNING: using getlk with l_whence != SEEK_SET is not supported" << dendl;
+    return -EINVAL;
+  }
+
+  MetaRequest *req = new MetaRequest(CEPH_MDS_OP_GETFILELOCK);
+  filepath path(fh->inode->ino);
+  req->set_filepath(path);
+  req->set_inode(fh->inode);
+  req->head.args.filelock_change.rule = CEPH_LOCK_FCNTL;
+  req->head.args.filelock_change.type = type;
+  req->head.args.filelock_change.pid = pid; // or l_pid?
+  req->head.args.filelock_change.pid_namespace = 0; // some unique id for this host?
+  req->head.args.filelock_change.start = lock->l_start; // l_whence
+  req->head.args.filelock_change.length = lock->l_len;
+  req->head.args.filelock_change.wait = 0;
+  int r = make_request(req, uid, gid);
+  if (r == 0) {
+    derr << "GETFILELOCK completed" << dendl;
+  }
+  return r;
+}
+
 #ifdef FALLOC_FL_PUNCH_HOLE
 
 int Client::_fallocate(Fh *fh, int mode, int64_t offset, int64_t length)
@@ -7888,7 +8012,6 @@ int Client::_fallocate(Fh *fh, int mode, int64_t offset, int64_t length)
 }
 
 #endif
-
 
 int Client::ll_fallocate(Fh *fh, int mode, loff_t offset, loff_t length)
 {
