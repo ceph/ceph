5be4d9f1ba9eb1162ff352a6cc78417a0396717f
Somnath Roy <somnath.roy@sandisk.com>
2014-01-22 11:03:06 -0800

    cmp.h: boost tuple comparison is replaced by regular comparison
    Profiler was showing this boost tuple comparison is expensive.
    So, it is replaced by regular comparison.
    
    Signed-off-by: Somnath Roy <somnath.roy@sandisk.com>
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- src/include/cmp.h
+++ src/include/cmp.h
@@ -1,7 +1,5 @@
 #ifndef __CEPH_CMP_H
 #define __CEPH_CMP_H
-#include <boost/tuple/tuple.hpp>
-#include <boost/tuple/tuple_comparison.hpp>
 
 /*
  * macros to define comparison operators for classes with small numbers of members.
@@ -104,30 +102,48 @@
 						      (l.d == r.d && l.e <= r.e))))))); \
   }
 
-#define WRITE_EQ_OPERATORS_7(type, a, b, c, d, e, f, g)                                                       \
-	inline bool operator==(const type &l, const type &r) {                                                      \
-		return (boost::make_tuple(boost::cref(l.a), boost::cref(l.b), boost::cref(l.c), boost::cref(l.d), boost::cref(l.e), boost::cref(l.f), boost::cref(l.g)) == \
-						boost::make_tuple(boost::cref(r.a), boost::cref(r.b), boost::cref(r.c), boost::cref(r.d), boost::cref(r.e), boost::cref(r.f), boost::cref(r.g)));  \
-	}                                                                                                           \
-	inline bool operator!=(const type &l, const type &r) {                                                      \
-		return (boost::make_tuple(boost::cref(l.a), boost::cref(l.b), boost::cref(l.c), boost::cref(l.d), boost::cref(l.e), boost::cref(l.f), boost::cref(l.g)) != \
-						boost::make_tuple(boost::cref(r.a), boost::cref(r.b), boost::cref(r.c), boost::cref(r.d), boost::cref(r.e), boost::cref(r.f), boost::cref(r.g)));  \
-	}
-#define WRITE_CMP_OPERATORS_7(type, a, b, c, d, e, f, g)                                                      \
-	inline bool operator<=(const type &l, const type &r) {                                                      \
-		return (boost::make_tuple(boost::cref(l.a), boost::cref(l.b), boost::cref(l.c), boost::cref(l.d), boost::cref(l.e), boost::cref(l.f), boost::cref(l.g)) <= \
-						boost::make_tuple(boost::cref(r.a), boost::cref(r.b), boost::cref(r.c), boost::cref(r.d), boost::cref(r.e), boost::cref(r.f), boost::cref(r.g)));  \
-	}                                                                                                           \
-	inline bool operator>=(const type &l, const type &r) {                                                      \
-		return (boost::make_tuple(boost::cref(l.a), boost::cref(l.b), boost::cref(l.c), boost::cref(l.d), boost::cref(l.e), boost::cref(l.f), boost::cref(l.g)) >= \
-						boost::make_tuple(boost::cref(r.a), boost::cref(r.b), boost::cref(r.c), boost::cref(r.d), boost::cref(r.e), boost::cref(r.f), boost::cref(r.g)));  \
-	}                                                                                                           \
-	inline bool operator>(const type &l, const type &r) {                                                       \
-		return (boost::make_tuple(boost::cref(l.a), boost::cref(l.b), boost::cref(l.c), boost::cref(l.d), boost::cref(l.e), boost::cref(l.f), boost::cref(l.g)) >  \
-						boost::make_tuple(boost::cref(r.a), boost::cref(r.b), boost::cref(r.c), boost::cref(r.d), boost::cref(r.e), boost::cref(r.f), boost::cref(r.g)));  \
-	}                                                                                                           \
-	inline bool operator<(const type &l, const type &r) {                                                       \
-		return (boost::make_tuple(boost::cref(l.a), boost::cref(l.b), boost::cref(l.c), boost::cref(l.d), boost::cref(l.e), boost::cref(l.f), boost::cref(l.g)) <  \
-						boost::make_tuple(boost::cref(r.a), boost::cref(r.b), boost::cref(r.c), boost::cref(r.d), boost::cref(r.e), boost::cref(r.f), boost::cref(r.g)));  \
-	}
+#define WRITE_EQ_OPERATORS_7(type, a, b, c, d, e, f, g)			\
+  inline bool operator==(const type &l, const type &r) {		\
+    return l.a == r.a && l.b == r.b && l.c == r.c && l.d == r.d && l.e == r.e && l.f == r.f && l.g == r.g; \
+  }									\
+  inline bool operator!=(const type &l, const type &r) {		\
+    return l.a != r.a || l.b != r.b || l.c != r.c || l.d != r.d || l.e != r.e || l.f != r.f || l.g != r.g; \
+  }
+#define WRITE_CMP_OPERATORS_7(type, a, b, c, d, e, f, g)		\
+  inline bool operator<=(const type &l, const type &r) {		\
+    return l.a < r.a ||							\
+      (l.a == r.a && (l.b < r.b ||					\
+		      (l.b == r.b && (l.c < r.c ||			\
+				      (l.c == r.c && (l.d < r.d ||	\
+						      (l.d == r.d && (l.e < r.e || \
+								      (l.e == r.e && (l.f < r.f || \
+										      (l.f == r.f && l.g <= r.g))))))))))); \
+  }									\
+  inline bool operator>=(const type &l, const type &r) {		\
+    return l.a > r.a ||							\
+      (l.a == r.a && (l.b > r.b ||					\
+		      (l.b == r.b && (l.c > r.c ||			\
+				      (l.c == r.c && (l.d > r.d ||	\
+						      (l.d == r.d && (l.e > r.e || \
+								      (l.e == r.e && (l.f > r.f || \
+										      (l.f == r.f && l.g >= r.g))))))))))); \
+  }									\
+  inline bool operator>(const type &l, const type &r) {			\
+    return l.a > r.a ||							\
+      (l.a == r.a && (l.b > r.b ||					\
+		      (l.b == r.b && (l.c > r.c ||			\
+				      (l.c == r.c && (l.d > r.d ||	\
+						      (l.d == r.d && (l.e > r.e || \
+								      (l.e == r.e && (l.f > r.f || \
+										      (l.f == r.f && l.g > r.g))))))))))); \
+  }									\
+  inline bool operator<(const type &l, const type &r) {			\
+    return l.a < r.a ||							\
+      (l.a == r.a && (l.b < r.b ||					\
+		      (l.b == r.b && (l.c < r.c ||			\
+				      (l.c == r.c && (l.d < r.d ||	\
+						      (l.d == r.d && (l.e < r.e || \
+								      (l.e == r.e && (l.f < r.f || \
+										      (l.f == r.f && l.g < r.g))))))))))); \
+  }
 #endif
