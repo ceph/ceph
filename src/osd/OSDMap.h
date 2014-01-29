846dcf02e94110164eead7fb9cbdacf6d1d83d82
Sage Weil <sage@inktank.com>
2014-01-28 18:46:37 -0800

    OSDMap: fix deepish_copy_from
    Start with a shallow copy!
    
    Signed-off-by: Sage Weil <sage@inktank.com>
    (cherry picked from commit d0f13f54146694a197535795da15b8832ef4b56f)
    
    Conflicts:
    
    	src/osd/OSDMap.h

--- src/osd/OSDMap.h
+++ src/osd/OSDMap.h
@@ -245,6 +245,7 @@ public:
   */
 
   void deepish_copy_from(const OSDMap& o) {
+    *this = o;
     pg_temp.reset(new map<pg_t,vector<int> >(*o.pg_temp));
     osd_uuid.reset(new vector<uuid_d>(*o.osd_uuid));
 
