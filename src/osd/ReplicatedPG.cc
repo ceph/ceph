f548b60712ce577f4ce0fe494650a4a0d740fe48
Samuel Just <sam.just@inktank.com>
2014-02-08 11:24:37 -0800

    ReplicatedPG: handle xattr cache on rollback and delete_head
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -4393,6 +4393,7 @@ inline int ReplicatedPG::_delete_head(OpContext *ctx, bool no_whiteout)
     } else {
       t->remove(soid);
     }
+    ctx->obc->attr_cache.clear();
   } else {
     ctx->mod_desc.mark_unrollbackable();
     t->remove(soid);
@@ -4501,6 +4502,7 @@ int ReplicatedPG::_rollback_to(OpContext *ctx, ceph_osd_op& op)
 	    t->remove(soid);
 	  }
 	}
+	ctx->obc->attr_cache = rollback_to->attr_cache;
       } else {
 	if (obs.exists) {
 	  ctx->mod_desc.mark_unrollbackable();
