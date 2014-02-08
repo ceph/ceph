dcd0305e4715c4f2c9ea771bd6d3f80c39080032
Loic Dachary <loic@dachary.org>
2014-02-08 10:05:14 +0100

    tests: add rados functional tests
    Add rados tests that can successfully be run using a vstart.sh based
    cluster with one MON and three OSDs. Although they take a long time to
    complete (in the range of 30 minutes), it is much easier than running
    teuthology for the average developer.
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- src/test/vstart_wrapped_tests.sh
+++ src/test/vstart_wrapped_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2013 Cloudwatt <libre.licensing@cloudwatt.com>
+# Copyright (C) 2013,2014 Cloudwatt <libre.licensing@cloudwatt.com>
 #
 # Author: Loic Dachary <loic@dachary.org>
 #
@@ -19,12 +19,32 @@ echo "Run unit tests that need a cluster, using vstart.sh"
 while read line ; do
   echo "================ START ================"
   echo "$line"
-  echo "======================================="
   if ! test/vstart_wrapper.sh $line ; then
       code=1
   fi
   echo "================ STOP ================="  
 done <<EOF
 ../qa/workunits/cephtool/test.sh
+./ceph_test_librbd
+./ceph_test_cls_rbd
+./ceph_test_cls_refcount
+./ceph_test_cls_version
+./ceph_test_cls_log
+./ceph_test_cls_statelog
+./ceph_test_cls_replica_log
+./ceph_test_cls_lock
+./ceph_test_cls_hello
+./ceph_test_cls_rgw
+./ceph_test_rados_api_cmd
+./ceph_test_rados_api_io
+./ceph_test_rados_api_c_write_operations
+./ceph_test_rados_api_c_read_operations
+./ceph_test_rados_api_list
+./ceph_test_rados_api_pool
+./ceph_test_rados_api_stat
+./ceph_test_rados_api_watch_notify
+./ceph_test_rados_api_snapshots
+./ceph_test_rados_api_cls
+./ceph_test_rados_api_misc
 EOF
 exit $code
