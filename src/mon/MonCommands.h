4784a687c1c37d1390c1ff59c081014aff257695
Loic Dachary <loic@dachary.org>
2014-02-11 18:25:51 +0100

    mon: add ruleset name to crush rule dump
    The crush rule name is made an optional first argument. If not provided, it
    defaults to dumping all rules.
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- src/mon/MonCommands.h
+++ src/mon/MonCommands.h
@@ -364,8 +364,9 @@ COMMAND("osd blacklist ls", "show blacklisted clients", "osd", "r", "cli,rest")
 COMMAND("osd crush rule list", "list crush rules", "osd", "r", "cli,rest")
 COMMAND("osd crush rule ls", "list crush rules", "osd", "r", "cli,rest")
 COMMAND("osd crush rule dump " \
+	"name=name,type=CephString,goodchars=[A-Za-z0-9-_.],req=false " \
 	"name=format,type=CephChoices,strings=json|json-pretty|xml|xml-pretty,req=false", \
-	"dump crush rules formatted as <format> (default json-pretty)", \
+	"dump crush rule <name> (default all) formatted as <format> (default json-pretty)", \
 	"osd", "r", "cli,rest")
 COMMAND("osd crush dump " \
 	"name=format,type=CephChoices,strings=json|json-pretty|xml|xml-pretty,req=false", \
