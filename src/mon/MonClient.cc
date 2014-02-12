eed50e654bba60c1e49e0f7f48bffd36d98e7d7a
Greg Farnum <greg@inktank.com>
2014-02-11 17:53:56 -0800

    monc: let users specify a callback when they reopen their monitor session
    Then the callback is triggered when a new session is established, and the
    daemon can do whatever it likes. There are no guarantees about how long it
    might take to trigger, though. In particular we call the provided callback
    while not holding our own lock in order to avoid deadlock. This could lead
    to some funny ordering from the user's perspective if they call
    reopen_session() again before getting the callback, but there's no way around
    that, so they just have to use it appropriately.
    
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- src/mon/MonClient.cc
+++ src/mon/MonClient.cc
@@ -66,6 +66,7 @@ MonClient::MonClient(CephContext *cct_) :
   want_monmap(true),
   want_keys(0), global_id(0),
   authenticate_err(0),
+  session_established_context(NULL),
   auth(NULL),
   keyring(NULL),
   rotating_secrets(NULL),
@@ -77,6 +78,7 @@ MonClient::MonClient(CephContext *cct_) :
 MonClient::~MonClient()
 {
   delete auth_supported;
+  delete session_established_context;
   delete auth;
   delete keyring;
   delete rotating_secrets;
@@ -462,6 +464,7 @@ int MonClient::authenticate(double timeout)
 
 void MonClient::handle_auth(MAuthReply *m)
 {
+  Context *cb = NULL;
   bufferlist::iterator p = m->result_bl.begin();
   if (state == MC_STATE_NEGOTIATING) {
     if (!auth || (int)m->protocol != auth->get_protocol()) {
@@ -521,11 +524,20 @@ void MonClient::handle_auth(MAuthReply *m)
 	log_client->reset_session();
 	send_log();
       }
+      if (session_established_context) {
+        cb = session_established_context;
+        session_established_context = NULL;
+      }
     }
   
     _check_auth_tickets();
   }
   auth_cond.SignalAll();
+  if (cb) {
+    monc_lock.Unlock();
+    cb->complete(0);
+    monc_lock.Lock();
+  }
 }
 
 
