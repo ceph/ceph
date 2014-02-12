8f4c20bdab153d1603cc99186d8d3e3970aa8976
Greg Farnum <greg@inktank.com>
2014-02-12 13:37:50 -0800

    monc: set "hunting" to true when we reopen the mon session
    If we don't have a connecton to a monitor, we want to retry to another
    monitor regardless of whether it's the first time or not.
    
    Signed-off-by: Greg Farnum <greg@inktank.com>
    Reviewed-by: Sage Weil <sage@inktank.com>
    (cherry picked from commit 60da8abe0ebf17ce818d6fcc6391401878123bb7)

--- src/mon/MonClient.cc
+++ src/mon/MonClient.cc
@@ -543,6 +543,7 @@ void MonClient::_reopen_session(int rank, string name)
 
   // restart authentication handshake
   state = MC_STATE_NEGOTIATING;
+  hunting = true;
 
   MAuth *m = new MAuth;
   m->protocol = 0;
@@ -573,7 +574,6 @@ bool MonClient::ms_handle_reset(Connection *con)
 	return true;
       
       ldout(cct, 0) << "hunting for new mon" << dendl;
-      hunting = true;
       _reopen_session();
     }
   }
