a71d829e46e864905199c21c1737adc410099e3e
Sage Weil <sage@inktank.com>
2014-02-17 10:27:23 -0800

    client: disable barrier support
    The boost interval_set class is not available on centos6/rhel6.  Until that
    dependency is sorted out, fix the build.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -8222,7 +8222,7 @@ int Client::ll_write_block(Inode *in, uint64_t blockid,
   if (length == 0) {
     return -EINVAL;
   }
-  if (sync) {
+  if (true || sync) {
     /* if write is stable, the epilogue is waiting on
      * flock */
     onack = new C_NoopContext;
@@ -8232,8 +8232,9 @@ int Client::ll_write_block(Inode *in, uint64_t blockid,
     /* if write is unstable, we just place a barrier for
      * future commits to wait on */
     onack = new C_NoopContext;
-    onsafe = new C_Block_Sync(this, vino.ino,
+    /*onsafe = new C_Block_Sync(this, vino.ino,
 			      barrier_interval(offset, offset + length), &r);
+    */
     done = true;
   }
   object_t oid = file_object_t(vino.ino, blockid);
@@ -8282,6 +8283,7 @@ int Client::ll_commit_blocks(Inode *in,
 			     uint64_t length)
 {
     Mutex::Locker lock(client_lock);
+    /*
     BarrierContext *bctx;
     vinodeno_t vino = ll_get_vino(in);
     uint64_t ino = vino.ino;
@@ -8298,7 +8300,7 @@ int Client::ll_commit_blocks(Inode *in,
       barrier_interval civ(offset, offset + length);
       p->second->commit_barrier(civ);
     }
-
+    */
     return 0;
 }
 
