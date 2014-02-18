d43de8790be2b0485666a65af91d45787fa7a78f
Somnath Roy <somnath.roy@sandisk.com>
2014-02-18 11:06:09 -0800

    CPU Optimized the functions. Signed-off-by: Somnath Roy <somnath.roy@sandisk.com>

--- src/messages/MOSDOp.h
+++ src/messages/MOSDOp.h
@@ -58,10 +58,10 @@ public:
   friend class MOSDOpReply;
 
   // read
-  snapid_t get_snapid() { return snapid; }
+  const snapid_t& get_snapid() { return snapid; }
   void set_snapid(snapid_t s) { snapid = s; }
   // writ
-  snapid_t get_snap_seq() const { return snap_seq; }
+  const snapid_t& get_snap_seq() const { return snap_seq; }
   const vector<snapid_t> &get_snaps() const { return snaps; }
   void set_snaps(const vector<snapid_t>& i) {
     snaps = i;
@@ -78,9 +78,9 @@ public:
   
   object_t& get_oid() { return oid; }
 
-  pg_t     get_pg() const { return pgid; }
+  const pg_t&     get_pg() const { return pgid; }
 
-  object_locator_t get_object_locator() const {
+  const object_locator_t& get_object_locator() const {
     return oloc;
   }
 
--- src/msg/Pipe.cc
+++ src/msg/Pipe.cc
@@ -1640,7 +1640,7 @@ void Pipe::writer()
 	  }
 	}
 
-	bufferlist blist = m->get_payload();
+	bufferlist& blist = m->get_payload();
 	blist.append(m->get_middle());
 	blist.append(m->get_data());
 
