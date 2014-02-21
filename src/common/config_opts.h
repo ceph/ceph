a52d7cd03bb3264c15cc1b934677e752546c23cf
Haomai Wang <haomaiwang@gmail.com>
2014-02-21 22:48:00 +0800

    Add KeyValueStore op thread options
    Signed-off-by: Haomai Wang <haomaiwang@gmail.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -653,6 +653,9 @@ OPTION(journal_force_aio, OPT_BOOL, false)
 OPTION(keyvaluestore_queue_max_ops, OPT_INT, 50)
 OPTION(keyvaluestore_queue_max_bytes, OPT_INT, 100 << 20)
 OPTION(keyvaluestore_check_backend, OPT_BOOL, 0) // Expensive debugging check on sync
+OPTION(keyvaluestore_op_threads, OPT_INT, 2)
+OPTION(keyvaluestore_op_thread_timeout, OPT_INT, 60)
+OPTION(keyvaluestore_op_thread_suicide_timeout, OPT_INT, 180)
 
 // max bytes to search ahead in journal searching for corruption
 OPTION(journal_max_corrupt_search, OPT_U64, 10<<20)
