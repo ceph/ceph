58d193673801fcb4975da302290ec6bbe4598003
Haomai Wang <haomaiwang@gmail.com>
2014-02-22 23:57:02 +0800

    Rename keyvaluestore_check_backend to keyvaluestore_debug_check_backend
    Signed-off-by: Haomai Wang <haomaiwang@gmail.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -652,7 +652,7 @@ OPTION(journal_force_aio, OPT_BOOL, false)
 
 OPTION(keyvaluestore_queue_max_ops, OPT_INT, 50)
 OPTION(keyvaluestore_queue_max_bytes, OPT_INT, 100 << 20)
-OPTION(keyvaluestore_check_backend, OPT_BOOL, 0) // Expensive debugging check on sync
+OPTION(keyvaluestore_debug_check_backend, OPT_BOOL, 0) // Expensive debugging check on sync
 OPTION(keyvaluestore_op_threads, OPT_INT, 2)
 OPTION(keyvaluestore_op_thread_timeout, OPT_INT, 60)
 OPTION(keyvaluestore_op_thread_suicide_timeout, OPT_INT, 180)
