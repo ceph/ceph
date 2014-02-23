ceph/ceph

commit bc5045d5e3bce8fea85354308bd400ebb15f57fb

PGLog.h: prefer prefix ++operator for non-trivial iterator
[src/osd/PGLog.h:363]: (performance) Prefer prefix ++/-- operators for
  non-primitive types.
[src/osd/PGLog.h:375]: (performance) Prefer prefix ++/-- operators for
  non-primitive types.
[src/osd/PGLog.h:380]: (performance) Prefer prefix ++/-- operators for
  non-primitive types.
[src/osd/PGLog.h:382]: (performance) Prefer prefix ++/-- operators for
  non-primitive types.

Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

src/osd/PGLog.h
@@ -360,7 +360,7 @@ public:
 	  break;
 	if (info.last_complete < log.complete_to->version)
 	  info.last_complete = log.complete_to->version;
-	log.complete_to++;
+	++log.complete_to;
       }
     }
 
@@ -372,14 +372,14 @@ public:
     log.complete_to = log.log.begin();
     while (log.complete_to->version <
 	   missing.missing[missing.rmissing.begin()->second].need)
-      log.complete_to++;
+      ++log.complete_to;
     assert(log.complete_to != log.log.end());
     if (log.complete_to == log.log.begin()) {
       info.last_complete = eversion_t();
     } else {
-      log.complete_to--;
+      ++log.complete_to;
       info.last_complete = log.complete_to->version;
-      log.complete_to++;
+      ++log.complete_to;
     }
     log.last_requested = 0;
   }

commit a0d1f09d64ec1d594a12fc01b6c6f580ce82d59a

PGLog::activate_not_complete: fix log.complete_to increment typo
info.last_complete should be the entry before log.complete_to.

This appears to have been a typo introduced in
bc5045d5e3bce8fea85354308bd400ebb15f57fb.

Signed-off-by: Samuel Just <sam.just@inktank.com>

src/osd/PGLog.h
@@ -377,7 +377,7 @@ public:
     if (log.complete_to == log.log.begin()) {
       info.last_complete = eversion_t();
     } else {
-      ++log.complete_to;
+      --log.complete_to;
       info.last_complete = log.complete_to->version;
       ++log.complete_to;
     }