718cda6e95d85a6a159ca951b0257f0054689345
Samuel Just <sam.just@inktank.com>
2014-03-01 13:55:24 -0800

    TrackedOp: clear_payload as well in unregister_inflight_op
    We want to minimize the cost of maintaining the historic ops.
    
    Signed-off-by: Samuel Just <sam.just@inktank.com>

--- src/common/TrackedOp.cc
+++ src/common/TrackedOp.cc
@@ -120,8 +120,9 @@ void OpTracker::unregister_inflight_op(TrackedOp *i)
 {
   // caller checks;
   assert(tracking_enabled);
-  i->request->clear_data();
 
+  i->request->clear_data();
+  i->request->clear_payload();
 
   Mutex::Locker locker(ops_in_flight_lock);
   assert(i->xitem.get_list() == &ops_in_flight);
