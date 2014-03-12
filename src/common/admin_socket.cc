72bc1ef8b0067efcc5a1e54e984e8b21c11a64c8
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2014-03-12 21:03:25 +0100

    AdminSocket: initialize m_getdescs_hook in the constructor
    CID 717212 (#1 of 1): Uninitialized pointer field (UNINIT_CTOR)
     2. uninit_member: Non-static class member "m_getdescs_hook" is not
     initialized in this constructor nor in any functions that it calls.
    
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

--- src/common/admin_socket.cc
+++ src/common/admin_socket.cc
@@ -111,7 +111,8 @@ AdminSocket::AdminSocket(CephContext *cct)
     m_shutdown_wr_fd(-1),
     m_lock("AdminSocket::m_lock"),
     m_version_hook(NULL),
-    m_help_hook(NULL)
+    m_help_hook(NULL),
+    m_getdescs_hook(NULL)
 {
 }
 
