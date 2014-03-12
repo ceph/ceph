920fd6cac15676b20eeedfe45bcb2d2f1cc8226d
Greg Farnum <greg@inktank.com>
2014-03-12 14:20:52 -0700

    Locker: use a null_ref instead of NULL
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- src/mds/Locker.cc
+++ src/mds/Locker.cc
@@ -2539,7 +2539,8 @@ public:
     locker(l), client(c), item(it) { }
   void finish(int r) {
     string dname;
-    locker->process_request_cap_release(NULL, client, item, dname);
+    MDRequestRef null_ref;
+    locker->process_request_cap_release(null_ref, client, item, dname);
   }
 };
 
