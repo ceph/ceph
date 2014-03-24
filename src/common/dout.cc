ee0f281b3e10aa1235392e8e521c1ca8946bd78f
Marcus Watts <mdw@linuxbox.com>
2014-03-23 23:13:16 -0400

    common: add dout_format
    I don't want to have to rewrite a bunch of
    c style debugging printf statements as the corresponding c++ stuff.
    
    Signed-off-by: Marcus Watts <mdw@cohortfs.com>
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/common/dout.cc
+++ src/common/dout.cc
@@ -1,5 +1,8 @@
-
 #include <iostream>
+#include <memory.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 void dout_emergency(const char * const str)
 {
@@ -11,4 +14,26 @@ void dout_emergency(const std::string &str)
 {
   std::cerr << str;
   std::cerr.flush();
+}
+
+// adapted from
+// http://stackoverflow.com/questions/2342162/stdstring-formatting-like-sprintf
+// because the original req'd c++11
+std::string dout_format(const std::string fmt_str, ...) {
+  int final_n, n = fmt_str.size() * 2;
+  std::string str;
+  char *formatted;
+  va_list ap;
+  while(1) {
+    formatted = (char*)malloc(n+1);
+    va_start(ap, fmt_str);
+    final_n = vsnprintf(formatted, n, fmt_str.c_str(), ap);
+    va_end(ap);
+    if (final_n >= 0 && final_n < n)
+      break;
+    n += abs(final_n - n + 1);
+  }
+  str = std::string(formatted);
+  free(formatted);
+  return str;
 }
