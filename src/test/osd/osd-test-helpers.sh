1f86ebbbe72c60d793f2070d27e9cf115ba2a422
Loic Dachary <loic@dachary.org>
2014-03-26 23:57:28 +0100

    osd: increase osd verbosity during functional tests
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- src/test/osd/osd-test-helpers.sh
+++ src/test/osd/osd-test-helpers.sh
@@ -38,6 +38,7 @@ function run_osd() {
     ceph_args+=" --osd-data=$osd_data"
     ceph_args+=" --chdir="
     ceph_args+=" --run-dir=$dir"
+    ceph_args+=" --debug-osd=20"
     ceph_args+=" --log-file=$dir/osd-\$id.log"
     ceph_args+=" --pid-file=$dir/osd-\$id.pidfile"
     ceph_args+=" "
