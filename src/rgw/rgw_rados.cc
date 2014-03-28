dfd3cb51404050c916f5131b0179cede8ceb7b10
Yehuda Sadeh <yehuda@inktank.com>
2014-03-28 13:25:47 -0700

    rgw: only look at prefetched data if we actually prefetched
    Fixes: #7903
    Since we didn't prefetch data then we couldn't rely on the data to
    actually exist there. In that case just move on and read the object.
    
    Signed-off-by: Yehuda Sadeh <yehuda@inktank.com>

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -4559,7 +4559,7 @@ int RGWRados::get_obj(void *ctx, RGWObjVersionTracker *objv_tracker, void **hand
     if (r < 0)
       goto done_ret;
 
-    if (astate) {
+    if (astate && astate->prefetch_data) {
       if (!ofs && astate->data.length() >= len) {
         bl = astate->data;
         goto done;
