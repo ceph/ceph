352390153d8297a6078139b74c8883db9f40b347
Ilya Dryomov <ilya.dryomov@inktank.com>
2014-04-01 10:18:23 +0400

    stop.sh: indent 4 spaces universally
    Currently there is a mix between tabs and 4 spaces indent.  Switch to
    4 spaces indent.
    
    Signed-off-by: Ilya Dryomov <ilya.dryomov@inktank.com>

--- src/stop.sh
+++ src/stop.sh
@@ -19,9 +19,9 @@
 test -d dev/osd0/. && test -e dev/sudo && SUDO="sudo"
 
 do_killall() {
-	pg=`pgrep -f ceph-run.*$1`
-	[ -n "$pg" ] && kill $pg
-	$SUDO killall $1
+    pg=`pgrep -f ceph-run.*$1`
+    [ -n "$pg" ] && kill $pg
+    $SUDO killall $1
 }
 
 usage="usage: $0 [all] [mon] [mds] [osd]\n"
@@ -34,43 +34,43 @@ stop_rgw=0
 
 while [ $# -ge 1 ]; do
     case $1 in
-	all )
-	    stop_all=1
-	    ;;
-	mon | ceph-mon )
-	    stop_mon=1
-	    stop_all=0
-	    ;;
-	mds | ceph-mds )
-	    stop_mds=1
-	    stop_all=0
-	    ;;
-	osd | ceph-osd )
-	    stop_osd=1
-	    stop_all=0
-	    ;;
-	* )
-	    printf "$usage"
-	    exit
+        all )
+            stop_all=1
+            ;;
+        mon | ceph-mon )
+            stop_mon=1
+            stop_all=0
+            ;;
+        mds | ceph-mds )
+            stop_mds=1
+            stop_all=0
+            ;;
+        osd | ceph-osd )
+            stop_osd=1
+            stop_all=0
+            ;;
+        * )
+            printf "$usage"
+            exit
     esac
     shift
 done
 
 if [ $stop_all -eq 1 ]; then
-	for p in ceph-mon ceph-mds ceph-osd radosgw lt-radosgw apache2 ; do
-            for try in 0 1 1 1 1 ; do
-                if ! pkill $p ; then
-                    break
-                fi
-                sleep $try
-            done
+    for p in ceph-mon ceph-mds ceph-osd radosgw lt-radosgw apache2 ; do
+        for try in 0 1 1 1 1 ; do
+            if ! pkill $p ; then
+                break
+            fi
+            sleep $try
         done
-	pkill -f valgrind.bin.\*ceph-mon
-	$SUDO pkill -f valgrind.bin.\*ceph-osd
-	pkill -f valgrind.bin.\*ceph-mds
+    done
+    pkill -f valgrind.bin.\*ceph-mon
+    $SUDO pkill -f valgrind.bin.\*ceph-osd
+    pkill -f valgrind.bin.\*ceph-mds
 else
-	[ $stop_mon -eq 1 ] && do_killall ceph-mon
-	[ $stop_mds -eq 1 ] && do_killall ceph-mds
-	[ $stop_osd -eq 1 ] && do_killall ceph-osd
-	[ $stop_rgw -eq 1 ] && do_killall radosgw lt-radosgw apache2
+    [ $stop_mon -eq 1 ] && do_killall ceph-mon
+    [ $stop_mds -eq 1 ] && do_killall ceph-mds
+    [ $stop_osd -eq 1 ] && do_killall ceph-osd
+    [ $stop_rgw -eq 1 ] && do_killall radosgw lt-radosgw apache2
 fi
