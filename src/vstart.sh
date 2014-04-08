1cdb7381c71fcec00a2b5266c6cbce8f0c467b6d
Sage Weil <sage@inktank.com>
2014-04-08 10:58:53 -0700

    vstart.sh: make crush location match up with what init-ceph does
    This makes is to that ./init-ceph restart osd.0 won't modify the CRUSH
    tree.  And in any case, the localhost/localrack thing we were doing before
    was pretty useless.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/vstart.sh
+++ src/vstart.sh
@@ -460,7 +460,7 @@ EOF
 	    uuid=`uuidgen`
 	    echo "add osd$osd $uuid"
 	    $SUDO $CEPH_ADM osd create $uuid
-	    $SUDO $CEPH_ADM osd crush add osd.$osd 1.0 host=localhost rack=localrack root=default
+	    $SUDO $CEPH_ADM osd crush add osd.$osd 1.0 host=$HOSTNAME root=default
 	    $SUDO $CEPH_BIN/ceph-osd -i $osd $ARGS --mkfs --mkkey --osd-uuid $uuid
 
 	    key_fn=$CEPH_DEV_DIR/osd$osd/keyring
