8a596a4ca3be86caaa5b817b7c3ead84ede391a5
Samuel Just <sam.just@inktank.com>
2014-04-10 18:15:30 -0700

    ReplicatedPG: do not preserve op context during flush
    Any information stashed in the OpContext may be obsolete by the time we
    actually mark the object clean.  Instead, let the start_flush caller
    clean up its OpContext and in try_flush_mark_clean we'll create a new
    one.  The primary reason to keep the OpContext would have been locking,
    but we can set the obc as blocking without holding an OpContext, and
    that would allow trimming to happen in the mean time (which is good
    since trim_object does not respect rw locks since it doesn't change user
    visible state).  In try_flush_mark_clean, we requeue the fop->op along
    with (but ahead of) the fop->dup_ops.
    
    Fixes: #8068
    Signed-off-by: Samuel Just <sam.just@inktank.com>
    (cherry picked from commit d83b8f58513e86c68aa6c25d4f909d9a3c4a103e)

--- src/osd/ReplicatedPG.h
+++ src/osd/ReplicatedPG.h
@@ -198,17 +198,21 @@ public:
   friend class PromoteCallback;
 
   struct FlushOp {
-    OpContext *ctx;             ///< the parent OpContext
-    list<OpRequestRef> dup_ops; ///< dup flush requests
+    ObjectContextRef obc;       ///< obc we are flushing
+    OpRequestRef op;            ///< initiating op
+    list<OpRequestRef> dup_ops; ///< bandwagon jumpers
     version_t flushed_version;  ///< user version we are flushing
     ceph_tid_t objecter_tid;    ///< copy-from request tid
     int rval;                   ///< copy-from result
     bool blocking;              ///< whether we are blocking updates
     bool removal;               ///< we are removing the backend object
+    Context *on_flush;          ///< callback, may be null
 
     FlushOp()
-      : ctx(NULL), objecter_tid(0), rval(0),
-	blocking(false), removal(false) {}
+      : objecter_tid(0), rval(0),
+	blocking(false), removal(false),
+	on_flush(NULL) {}
+    ~FlushOp() { assert(!on_flush); }
   };
   typedef boost::shared_ptr<FlushOp> FlushOpRef;
 
@@ -1221,7 +1225,11 @@ protected:
   // -- flush --
   map<hobject_t, FlushOpRef> flush_ops;
 
-  int start_flush(OpContext *ctx, bool blocking, hobject_t *pmissing);
+  /// start_flush takes ownership of on_flush iff ret == -EINPROGRESS
+  int start_flush(
+    OpRequestRef op, ObjectContextRef obc,
+    bool blocking, hobject_t *pmissing,
+    Context *on_flush);
   void finish_flush(hobject_t oid, ceph_tid_t tid, int r);
   int try_flush_mark_clean(FlushOpRef fop);
   void cancel_flush(FlushOpRef fop, bool requeue);
