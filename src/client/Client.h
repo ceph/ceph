cdb0fac29693fb0250b515891804c4c7fd3045c7
Yan, Zheng <zheng.z.yan@intel.com>
2014-04-11 15:03:37 +0800

    client: add asok command to kick sessions that were remote reset
    Fixes: #8021
    Signed-off-by: Yan, Zheng <zheng.z.yan@intel.com>
    (cherry picked from commit 09a1bc5a4601d356b9cc69be8541e6515d763861)

--- src/client/Client.h
+++ src/client/Client.h
@@ -250,6 +250,7 @@ public:
   MetaSession *_open_mds_session(int mds);
   void _close_mds_session(MetaSession *s);
   void _closed_mds_session(MetaSession *s);
+  void _kick_stale_sessions();
   void handle_client_session(MClientSession *m);
   void send_reconnect(MetaSession *s);
   void resend_unsafe_requests(MetaSession *s);
