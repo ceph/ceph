091d1fe48d0d56288e8043b53b6be9d27f7e14fa
Zack Cerza <zack@cerza.org>
2014-05-05 16:57:13 -0500

    Revert "Revert "Clean up remote.py and misc.py changes.""
    This reverts commit 02504c3fd27d788e2e446369015b14cbf259a8d2.

--- teuthology/task/kernel.py
+++ teuthology/task/kernel.py
@@ -649,7 +649,7 @@ def update_grub_rpm(remote, newversion):
         newgrub = generate_legacy_grub_entry(remote, newversion)
         for line in newgrub:
             data += line + '\n'
-        temp_file_path = remote.remote_mktemp()
+        temp_file_path = remote.mktemp()
         teuthology.sudo_write_file(remote, temp_file_path, StringIO(data), '755')
         teuthology.move_file(remote, temp_file_path, '/boot/grub/grub.conf', True)
     else:
