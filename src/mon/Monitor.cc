894679513cf655440bbc98649224bb788ab0edc3
Matt Benjamin <matt@linuxbox.com>
2014-05-10 15:28:53 -0400

    Fix lifetime of ceph-mon's XioMessenger, and allow cluster_rdma.
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/mon/Monitor.cc
+++ src/mon/Monitor.cc
@@ -133,7 +133,7 @@ Monitor::Monitor(CephContext* cct_, string nm, MonitorDBStore *s,
 		 Messenger *m, XioMessenger *xm, MonMap *map) :
   Dispatcher(cct_),
   name(nm),
-  rank(-1), 
+  rank(-1),
   messenger(m),
   xmsgr(xm),
   con_self(m ? m->get_loopback_connection() : NULL),
