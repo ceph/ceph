90ebfc5f5ea99055453441c5d5aa05b26e7ed9b7
Matt Benjamin <matt@linuxbox.com>
2014-05-12 11:39:20 -0400

    Restore non-abort from Xio Mon integration.
    Missed this in grouped patch.
    
    Signed-off-by: Matt Benjamin <matt@linuxbox.com>

--- src/auth/none/AuthNoneServiceHandler.h
+++ src/auth/none/AuthNoneServiceHandler.h
@@ -32,7 +32,6 @@ public:
     return CEPH_AUTH_NONE;
   }
   int handle_request(bufferlist::iterator& indata, bufferlist& result_bl, uint64_t& global_id, AuthCapsInfo& caps, uint64_t *auid = NULL) {
-    assert(0);  // shouldn't get called
     return 0;
   }
   void build_cephx_response_header(int request_type, int status, bufferlist& bl) { }
