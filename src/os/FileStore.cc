249e1baa4a29e2a7346ebff81f548ad0ef693919
David Zafman <david.zafman@inktank.com>
2014-05-14 15:41:15 -0700

    os: Add optional flags to generic ObjectStore creation (SKIP_JOURNAL_REPLAY and SKIP_MOUNT_OMAP)
    Only FileStore cares about these flags, so passed on during create()
    
    Signed-off-by: David Zafman <david.zafman@inktank.com>

--- src/os/FileStore.cc
+++ src/os/FileStore.cc
@@ -419,10 +419,11 @@ int FileStore::lfn_unlink(coll_t cid, const ghobject_t& o,
   return index->unlink(o);
 }
 
-FileStore::FileStore(const std::string &base, const std::string &jdev, const char *name, bool do_update) :
+FileStore::FileStore(const std::string &base, const std::string &jdev, osflagbits_t flags, const char *name, bool do_update) :
   JournalingObjectStore(base),
   internal_name(name),
   basedir(base), journalpath(jdev),
+  generic_flags(flags),
   blk_size(0),
   fsid_fd(-1), op_fd(-1),
   basedir_fd(-1), current_fd(-1),
@@ -1124,7 +1125,7 @@ int FileStore::write_op_seq(int fd, uint64_t seq)
   return ret;
 }
 
-int FileStore::mount() 
+int FileStore::mount()
 {
   int ret;
   char buf[PATH_MAX];
@@ -1352,7 +1353,7 @@ int FileStore::mount()
     ::unlink(nosnapfn);
   }
 
-  {
+  if (!(generic_flags & SKIP_MOUNT_OMAP)) {
     KeyValueDB * omap_store = KeyValueDB::create(g_ceph_context,
 						 superblock.omap_backend,
 						 omap_dir);
@@ -1453,24 +1454,26 @@ int FileStore::mount()
   wbthrottle.start();
   sync_thread.create();
 
-  ret = journal_replay(initial_op_seq);
-  if (ret < 0) {
-    derr << "mount failed to open journal " << journalpath << ": " << cpp_strerror(ret) << dendl;
-    if (ret == -ENOTTY) {
-      derr << "maybe journal is not pointing to a block device and its size "
-	   << "wasn't configured?" << dendl;
+  if (!(generic_flags & SKIP_JOURNAL_REPLAY)) {
+    ret = journal_replay(initial_op_seq);
+    if (ret < 0) {
+      derr << "mount failed to open journal " << journalpath << ": " << cpp_strerror(ret) << dendl;
+      if (ret == -ENOTTY) {
+        derr << "maybe journal is not pointing to a block device and its size "
+	     << "wasn't configured?" << dendl;
+      }
+
+      // stop sync thread
+      lock.Lock();
+      stop = true;
+      sync_cond.Signal();
+      lock.Unlock();
+      sync_thread.join();
+
+      wbthrottle.stop();
+
+      goto close_current_fd;
     }
-
-    // stop sync thread
-    lock.Lock();
-    stop = true;
-    sync_cond.Signal();
-    lock.Unlock();
-    sync_thread.join();
-
-    wbthrottle.stop();
-
-    goto close_current_fd;
   }
 
   {
@@ -1523,6 +1526,8 @@ int FileStore::umount()
   op_tp.stop();
 
   journal_stop();
+  if (!(generic_flags & SKIP_JOURNAL_REPLAY))
+    journal_write_close();
 
   op_finisher.stop();
   ondisk_finisher.stop();
