fdbab46852e74d405b5c747da98564a5866ec8a7
Haomai Wang <haomaiwang@gmail.com>
2014-05-20 14:32:18 +0800

    Fix set_alloc_hint op cause KeyValueStore crash problem
    Now KeyValueStore doesn't support set_alloc_hit op, the implementation of
    _do_transaction need to consider decoding the arguments. Otherwise, the
    arguments will be regarded as the next op.
    
    Fix the same problem for MemStore.
    
    Fix #8381
    
    Reported-by: Xinxin Shu <xinxin.shu5040@gmail.com>
    Signed-off-by: Haomai Wang <haomaiwang@gmail.com>
    (cherry picked from commit c08adbc98ff5f380ecd215f8bd9cf3cab214913c)

--- src/os/MemStore.cc
+++ src/os/MemStore.cc
@@ -950,7 +950,12 @@ void MemStore::_do_transaction(Transaction& t)
       break;
 
     case Transaction::OP_SETALLOCHINT:
-      // nop
+      {
+        coll_t cid(i.get_cid());
+        ghobject_t oid = i.get_oid();
+        uint64_t expected_object_size = i.get_length();
+        uint64_t expected_write_size = i.get_length();
+      }
       break;
 
     default:
