3c675e73fa8977eb2df1775f6090c5ca39f74544
Loic Dachary <loic@dachary.org>
2014-05-28 15:36:57 +0200

    osd: loop over uint8_t instead of shard_id_t
    When looping in the range [0,number of shards[ using a uint8_t variable
    which is an integer instead of shard_id_t. The alternative would be
    define the ++ operator on shard_id_t but since the two are used
    interchangeably in a number of places, that seems unnecessary.
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -953,7 +953,7 @@ void PG::calc_ec_acting(
       ++i) {
     all_info_by_shard[i->first.shard].insert(i->first);
   }
-  for (shard_id_t i = 0; i < want.size(); ++i) {
+  for (uint8_t i = 0; i < want.size(); ++i) {
     ss << "For position " << (unsigned)i << ": ";
     if (up.size() > (unsigned)i && up[i] != CRUSH_ITEM_NONE &&
 	!all_info.find(pg_shard_t(up[i], i))->second.is_incomplete() &&
@@ -997,7 +997,7 @@ void PG::calc_ec_acting(
   }
 
   bool found_primary = false;
-  for (shard_id_t i = 0; i < want.size(); ++i) {
+  for (uint8_t i = 0; i < want.size(); ++i) {
     if (want[i] != CRUSH_ITEM_NONE) {
       acting_backfill->insert(pg_shard_t(want[i], i));
       if (!found_primary) {
