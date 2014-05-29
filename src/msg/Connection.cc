72b4d4e26f393c6607dea28e46bd0d720af76b6f
Sage Weil <sage@inktank.com>
2014-05-28 17:03:14 -0700

    msgr: add convenience Connection methods
    Signed-off-by: Sage Weil <sage@inktank.com>

---
+++ src/msg/Connection.cc
@@ -0,0 +1,31 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2014 Red Hat
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
+#include "Connection.h"
+#include "Messenger.h"
+
+void Connection::send_message(Message *m)
+{
+  msgr->send_message(m, this);
+}
+
+void Connection::send_keepalive()
+{
+  msgr->send_keepalive(this);
+}
+
+void Connection::mark_down()
+{
+  msgr->mark_down(this);
+}
