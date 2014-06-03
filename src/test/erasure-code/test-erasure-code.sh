c1bf5dc82f66c8b2a64729dea31d17195752d39f
Loic Dachary <loic@dachary.org>
2014-06-03 19:27:26 +0200

    erasure-code: remap chunks if not sequential
    If the remap vector is not empty, use it to figure out the sequence of
    data chunks.
    
    http://tracker.ceph.com/issues/9025 Fixes: #9025
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- src/test/erasure-code/test-erasure-code.sh
+++ src/test/erasure-code/test-erasure-code.sh
@@ -30,7 +30,7 @@ function run() {
     for id in $(seq 0 4) ; do
         run_osd $dir $id || return 1
     done
-    create_erasure_coded_pool || return 1
+    create_erasure_coded_pool ecpool || return 1
     FUNCTIONS=${FUNCTIONS:-$(set | sed -n -e 's/^\(TEST_[0-9a-z_]*\) .*/\1/p')}
     for TEST_function in $FUNCTIONS ; do
         if ! $TEST_function $dir ; then
@@ -38,13 +38,16 @@ function run() {
             return 1
         fi
     done
+    delete_pool ecpool || return 1
     teardown $dir || return 1
 }
 
 function create_erasure_coded_pool() {
+    local poolname=$1
+
     ./ceph osd erasure-code-profile set myprofile \
         ruleset-failure-domain=osd || return 1
-    ./ceph osd pool create ecpool 12 12 erasure myprofile \
+    ./ceph osd pool create $poolname 12 12 erasure myprofile \
         || return 1
 }
 
@@ -124,6 +127,77 @@ function TEST_alignment_constraints() {
     ./rados --block-size=$block_size \
         --pool ecpool put UNALIGNED $dir/ORIGINAL || return 1
     rm $dir/ORIGINAL
+}
+
+function get_osds() {
+    local poolname=$1
+    local objectname=$2
+
+    ./ceph osd map $poolname $objectname | \
+       perl -p -e 's/.*up \(\[(.*?)\].*/$1/; s/,/ /g'
+}
+
+function chunk_size() {
+    local stripe_width=$(./ceph-conf --show-config-value osd_pool_erasure_code_stripe_width)
+    eval local $(./ceph osd erasure-code-profile get default | grep k=)
+    echo $(($stripe_width / $k))
+}
+
+#
+# By default an object will be split in two (k=2) with the first part
+# of the object in the first OSD of the up set and the second part in
+# the next OSD in the up set. This layout is defined by the mapping
+# parameter and this function helps verify that the first and second
+# part of the object are located in the OSD where they should be.
+#
+function verify_chunk_mapping() {
+    local dir=$1
+    local poolname=$2
+    local first=$3
+    local second=$4
+
+    local payload=$(printf '%*s' $(chunk_size) FIRST$poolname ; printf '%*s' $(chunk_size) SECOND$poolname)
+    echo -n "$payload" > $dir/ORIGINAL
+
+    ./rados --pool $poolname put SOMETHING$poolname $dir/ORIGINAL || return 1
+    ./rados --pool $poolname get SOMETHING$poolname $dir/COPY || return 1
+    local -a osds=($(get_osds $poolname SOMETHING$poolname))
+    for (( i = 0; i < ${#osds[@]}; i++ )) ; do
+        ./ceph daemon osd.${osds[$i]} flush_journal
+    done
+    diff $dir/ORIGINAL $dir/COPY || return 1
+    rm $dir/COPY
+
+    local -a osds=($(get_osds $poolname SOMETHING$poolname))
+    grep --quiet --recursive --text FIRST$poolname $dir/${osds[$first]} || return 1
+    grep --quiet --recursive --text SECOND$poolname $dir/${osds[$second]} || return 1
+}
+
+function TEST_chunk_mapping() {
+    local dir=$1
+
+    #
+    # mapping=DD_ is the default:
+    #  first OSD (i.e. 0) in the up set has the first part of the object
+    #  second OSD (i.e. 1) in the up set has the second part of the object
+    #
+    verify_chunk_mapping $dir ecpool 0 1 || return 1
+
+    ./ceph osd erasure-code-profile set remap-profile \
+        ruleset-failure-domain=osd \
+        mapping='_DD' || return 1
+    ./ceph osd erasure-code-profile get remap-profile
+    ./ceph osd pool create remap-pool 12 12 erasure remap-profile \
+        || return 1
+
+    #
+    # mapping=_DD
+    #  second OSD (i.e. 1) in the up set has the first part of the object
+    #  third OSD (i.e. 2) in the up set has the second part of the object
+    #
+    verify_chunk_mapping $dir remap-pool 1 2 || return 1
+
+    delete_pool remap-pool
 }
 
 main test-erasure-code
