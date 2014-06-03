b561a257e5a87151eff8097d91b24a8fb9e3a40d
Loic Dachary <loic@dachary.org>
2014-06-03 23:17:59 +0200

    erasure-code: move to ErasureCode::{encode,decode}{,chunk}
    The bulk of ErasureCodeJerasure::encode and ErasureCodeJerasure::decode
    is dealing with alignment and buffer allocation. This is moved to
    ErasureCode::encode and ErasureCode::decode respectively.
    
    The jerasure specific code is isolated in the new
    ErasureCodeJerasure::encode_chunks and
    ErasureCodeJerasure::decode_chunks virtual functions.
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- src/erasure-code/ErasureCode.cc
+++ src/erasure-code/ErasureCode.cc
@@ -15,3 +15,95 @@
  */
 
 #include "ErasureCode.h"
+int ErasureCode::encode_prepare(const bufferlist &raw,
+                                bufferlist *prepared) const
+{
+  unsigned int k = get_data_chunk_count();
+  unsigned int m = get_chunk_count() - k;
+  unsigned blocksize = get_chunk_size(raw.length());
+  unsigned padded_length = blocksize * k;
+  *prepared = raw;
+  if (padded_length - raw.length() > 0) {
+    bufferptr pad(padded_length - raw.length());
+    pad.zero();
+    prepared->push_back(pad);
+  }
+  unsigned coding_length = blocksize * m;
+  bufferptr coding(buffer::create_page_aligned(coding_length));
+  prepared->push_back(coding);
+  prepared->rebuild_page_aligned();
+  return 0;
+}
+
+int ErasureCode::encode(const set<int> &want_to_encode,
+                        const bufferlist &in,
+                        map<int, bufferlist> *encoded)
+{
+  unsigned int k = get_data_chunk_count();
+  unsigned int m = get_chunk_count() - k;
+  bufferlist out;
+  int err = encode_prepare(in, &out);
+  if (err)
+    return err;
+  unsigned blocksize = get_chunk_size(in.length());
+  for (unsigned int i = 0; i < k + m; i++) {
+    bufferlist &chunk = (*encoded)[i];
+    chunk.substr_of(out, i * blocksize, blocksize);
+  }
+  encode_chunks(want_to_encode, encoded);
+  for (unsigned int i = 0; i < k + m; i++) {
+    if (want_to_encode.count(i) == 0)
+      encoded->erase(i);
+  }
+  return 0;
+}
+
+int ErasureCode::encode_chunks(const set<int> &want_to_encode,
+                               map<int, bufferlist> *encoded)
+{
+  assert("ErasureCode::encode_chunks not implemented" == 0);
+}
+ 
+int ErasureCode::decode(const set<int> &want_to_read,
+                        const map<int, bufferlist> &chunks,
+                        map<int, bufferlist> *decoded)
+{
+  vector<int> have;
+  have.reserve(chunks.size());
+  for (map<int, bufferlist>::const_iterator i = chunks.begin();
+       i != chunks.end();
+       ++i) {
+    have.push_back(i->first);
+  }
+  if (includes(
+	have.begin(), have.end(), want_to_read.begin(), want_to_read.end())) {
+    for (set<int>::iterator i = want_to_read.begin();
+	 i != want_to_read.end();
+	 ++i) {
+      (*decoded)[*i] = chunks.find(*i)->second;
+    }
+    return 0;
+  }
+  unsigned int k = get_data_chunk_count();
+  unsigned int m = get_chunk_count() - k;
+  unsigned blocksize = (*chunks.begin()).second.length();
+  for (unsigned int i =  0; i < k + m; i++) {
+    if (chunks.find(i) == chunks.end()) {
+      bufferptr ptr(buffer::create_page_aligned(blocksize));
+      (*decoded)[i].push_front(ptr);
+    } else {
+      (*decoded)[i] = chunks.find(i)->second;
+      (*decoded)[i].rebuild_page_aligned();
+    }
+  }
+  return decode_chunks(want_to_read, chunks, decoded);
+}
+
+int ErasureCode::decode_chunks(const set<int> &want_to_read,
+                               const map<int, bufferlist> &chunks,
+                               map<int, bufferlist> *decoded)
+{
+  assert("ErasureCode::decode_chunks not implemented" == 0);
+}
+
+
