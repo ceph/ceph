a164afae8575333cb426ee45b87d59da19fe7756
Ilya Dryomov <ilya.dryomov@inktank.com>
2014-06-05 10:08:42 +0400

    XfsFileStoreBackend: call ioctl(XFS_IOC_FSSETXATTR) less often
    No need to call ioctl(XFS_IOC_FSSETXATTR) if extsize is already set to
    the value we want or if any extents are allocated - XFS will refuse to
    change extsize in that's the case.
    
    Fixes: #8241
    Signed-off-by: Ilya Dryomov <ilya.dryomov@inktank.com>

--- src/os/XfsFileStoreBackend.cc
+++ src/os/XfsFileStoreBackend.cc
@@ -60,6 +60,14 @@ int XfsFileStoreBackend::set_extsize(int fd, unsigned int val)
     return ret;
   }
 
+  // already set?
+  if ((fsx.fsx_xflags & XFS_XFLAG_EXTSIZE) && fsx.fsx_extsize == val)
+    return 0;
+
+  // xfs won't change extent size if any extents are allocated
+  if (fsx.fsx_nextents != 0)
+    return 0;
+
   fsx.fsx_xflags |= XFS_XFLAG_EXTSIZE;
   fsx.fsx_extsize = val;
 
