754363f4563e7dbda1ef23fadc8d6ef1a3fdd0af
Loic Dachary <loic@dachary.org>
2014-06-13 14:41:39 +0200

    tests: prevent kill race condition
    When trying to kill a daemon, keep its pid in a variable instead of
    retrieving it from the pidfile multiple times. It prevents the following
    race condition:
    
      * try to kill ceph-mon
      * ceph-mon is in the process of dying and removed its pidfile
      * try to kill ceph-mon fails because the pidfile is not found
      * another ceph-mon is spawned and fails to bind the port
        because the previous ceph-mon is still holding it
    
    Signed-off-by: Loic Dachary <loic@dachary.org>
    (cherry picked from commit a1c13c57ba20fc329d943ea57523913e11067dc7)

--- src/test/mon/mkfs.sh
+++ src/test/mon/mkfs.sh
@@ -66,8 +66,9 @@ function mon_run() {
 
 function kill_daemons() {
     for pidfile in $(find $DIR -name pidfile) ; do
+        pid=$(cat $pidfile)
         for try in 0 1 1 1 2 3 ; do
-            kill $(cat $pidfile) || break
+            kill $pid || break
             sleep $try
         done
     done
