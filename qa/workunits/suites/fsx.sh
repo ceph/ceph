8649cbbc96a4de9de169b0203f35e0ac6c36a2ef
Greg Farnum <greg@inktank.com>
2014-07-01 15:19:21 -0700

    Revert "qa/workunits/suites/fsx.sh: don't use zero range"
    This reverts commit 583e6e3ef7f28bf34fe038e8a2391f9325a69adf.
    
    We're using a different fsx source, which doesn't support the
    same options as our git-based one does.
    
    Signed-off-by: Greg Farnum <greg@inktank.com>

--- qa/workunits/suites/fsx.sh
+++ qa/workunits/suites/fsx.sh
@@ -5,8 +5,6 @@ set -e
 wget http://ceph.com/qa/fsx.c
 gcc fsx.c -o fsx
 
-OPTIONS="-z"  # don't use zero range calls; not supported by cephfs
-
-./fsx $OPTIONS  1MB -N 50000 -p 10000 -l 1048576
-./fsx $OPTIONS  10MB -N 50000 -p 10000 -l 10485760
-./fsx $OPTIONS 100MB -N 50000 -p 10000 -l 104857600
+./fsx   1MB -N 50000 -p 10000 -l 1048576
+./fsx  10MB -N 50000 -p 10000 -l 10485760
+./fsx 100MB -N 50000 -p 10000 -l 104857600
