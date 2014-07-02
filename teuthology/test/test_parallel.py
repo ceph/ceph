43c4595ae26a7663538e712af37553c7a64fade7
Zack Cerza <zack@cerza.org>
2014-07-02 10:08:11 -0600

    Add a couple unit tests for teuthology.parallel
    Signed-off-by: Zack Cerza <zack.cerza@inktank.com>

---
+++ teuthology/test/test_parallel.py
@@ -0,0 +1,28 @@
+from ..parallel import parallel
+
+
+def identity(item, input_set=None, remove=False):
+    if input_set is not None:
+        assert item in input_set
+        if remove:
+            input_set.remove(item)
+    return item
+
+
+class TestParallel(object):
+    def test_basic(self):
+        in_set = set(range(10))
+        with parallel() as para:
+            for i in in_set:
+                para.spawn(identity, i, in_set, remove=True)
+                assert para.any_spawned is True
+            assert para.count == len(in_set)
+
+    def test_result(self):
+        in_set = set(range(10))
+        with parallel() as para:
+            for i in in_set:
+                para.spawn(identity, i, in_set)
+            for result in para:
+                in_set.remove(result)
+
