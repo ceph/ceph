657bd369d04f44c74fedfde52264ad49886dbb7d
Zack Cerza <zack@cerza.org>
2014-07-14 15:16:55 -0600

    Fix the incomplete substitute_placeholders() fix
    Signed-off-by: Zack Cerza <zack.cerza@inktank.com>

--- teuthology/suite.py
+++ teuthology/suite.py
@@ -631,15 +631,19 @@ def substitute_placeholders(input_dict, values_dict):
                         values to be substituted.
     :returns:           The modified input_dict
     """
-    input_dict = dict(input_dict)
-    for (key, value) in input_dict.iteritems():
-        if isinstance(value, dict):
-            substitute_placeholders(value, values_dict)
-        elif isinstance(value, Placeholder):
-            # If there is a Placeholder without a corresponding entry in
-            # values_dict, we will hit a KeyError - we want this.
-            input_dict[key] = values_dict[value.name]
-    return input_dict
+    input_dict = copy.deepcopy(input_dict)
+
+    def _substitute(input_dict, values_dict):
+        for (key, value) in input_dict.iteritems():
+            if isinstance(value, dict):
+                _substitute(value, values_dict)
+            elif isinstance(value, Placeholder):
+                # If there is a Placeholder without a corresponding entry in
+                # values_dict, we will hit a KeyError - we want this.
+                input_dict[key] = values_dict[value.name]
+        return input_dict
+
+    return _substitute(input_dict, values_dict)
 
 
 # Template for the config that becomes the base for each generated job config
