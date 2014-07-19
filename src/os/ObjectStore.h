2b9f0f601500a3ba17a12fa8aeea91da6b94cb53
Sage Weil <sage@redhat.com>
2014-07-19 11:00:04 -0700

    os/ObjectStore: remove collection_{add,remove}
    Move the add+remove a move normally translates to directly into
    that method.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/ObjectStore.h
+++ src/os/ObjectStore.h
@@ -870,33 +870,20 @@ public:
       ::encode(cid, tbl);
       ops++;
     }
-    /**
-     * Add object to another collection (DEPRECATED)
-     *
-     * The Object is added to the new collection. This is a virtual
-     * add, we now have two names for the same object.  This is only
-     * used for conversion of old stores to new stores and is not
-     * needed for new implementations unless they expect to make use
-     * of the conversion infrastructure.
-     */
-    void collection_add(coll_t cid, coll_t ocid, const ghobject_t& oid) {
+    void collection_move(coll_t cid, coll_t oldcid, const ghobject_t& oid) {
+      // NOTE: we encode this as a fixed combo of ADD + REMOVE.  they
+      // always appear together, so this is effectively a single MOVE.
       __u32 op = OP_COLL_ADD;
       ::encode(op, tbl);
       ::encode(cid, tbl);
-      ::encode(ocid, tbl);
+      ::encode(oldcid, tbl);
       ::encode(oid, tbl);
       ops++;
-    }
-    void collection_remove(coll_t cid, const ghobject_t& oid) {
-      __u32 op = OP_COLL_REMOVE;
+      op = OP_COLL_REMOVE;
       ::encode(op, tbl);
-      ::encode(cid, tbl);
+      ::encode(oldcid, tbl);
       ::encode(oid, tbl);
       ops++;
-    }
-    void collection_move(coll_t cid, coll_t oldcid, const ghobject_t& oid) {
-      collection_add(cid, oldcid, oid);
-      collection_remove(oldcid, oid);
       return;
     }
     void collection_move_rename(coll_t oldcid, const ghobject_t& oldoid,
