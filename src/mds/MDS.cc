3a94417ddad9e8b06466348494b6faaa85ab1462
Sage Weil <sage@redhat.com>
2014-07-20 14:10:41 -0700

    mds: use lock-safe OSDMap accessors; adjust Objecter wait_for_map call
    We need to handle a race between when we inspect the OSDMap and when we
    try to wait for the next map.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mds/MDS.cc
+++ src/mds/MDS.cc
@@ -885,10 +885,6 @@ void MDS::handle_mds_beacon(MMDSBeacon *m)
   m->put();
 }
 
-void MDS::request_osdmap(Context *c) {
-  objecter->wait_for_new_map(c, osdmap->get_epoch());
-}
-
 /* This function DOES put the passed message before returning*/
 void MDS::handle_command(MMonCommand *m)
 {
@@ -1506,18 +1502,21 @@ void MDS::replay_start()
 
   calc_recovery_set();
 
+  const OSDMap *osdmap = objecter->get_osdmap_read();
+  epoch_t e = osdmap->get_epoch();
+  objecter->put_osdmap_read();
+
   dout(1) << " need osdmap epoch " << mdsmap->get_last_failure_osd_epoch()
-	  <<", have " << osdmap->get_epoch()
-	  << dendl;
+	  << ", have " << e << dendl;
 
   // start?
-  if (osdmap->get_epoch() >= mdsmap->get_last_failure_osd_epoch()) {
+  if (e >= mdsmap->get_last_failure_osd_epoch()) {
     boot_start();
   } else {
     dout(1) << " waiting for osdmap " << mdsmap->get_last_failure_osd_epoch() 
 	    << " (which blacklists prior instance)" << dendl;
-    objecter->wait_for_new_map(new C_MDS_BootStart(this, MDS_BOOT_INITIAL),
-			       mdsmap->get_last_failure_osd_epoch());
+    objecter->wait_for_map(mdsmap->get_last_failure_osd_epoch(),
+			   new C_MDS_BootStart(this, MDS_BOOT_INITIAL));
   }
 }
 
@@ -1549,11 +1548,15 @@ inline void MDS::standby_replay_restart()
 {
   dout(1) << "standby_replay_restart" << (standby_replaying ? " (as standby)":" (final takeover pass)") << dendl;
 
-  if (!standby_replaying && osdmap->get_epoch() < mdsmap->get_last_failure_osd_epoch()) {
+  const OSDMap *osdmap = objecter->get_osdmap_read();
+  epoch_t e = osdmap->get_epoch();
+  objecter->put_osdmap_read();
+  
+  if (!standby_replaying && e < mdsmap->get_last_failure_osd_epoch()) {
     dout(1) << " waiting for osdmap " << mdsmap->get_last_failure_osd_epoch() 
 	    << " (which blacklists prior instance)" << dendl;
-    objecter->wait_for_new_map(new C_MDS_BootStart(this, MDS_BOOT_PREPARE_LOG),
-			       mdsmap->get_last_failure_osd_epoch());
+    objecter->wait_for_map(mdsmap->get_last_failure_osd_epoch(),
+			   new C_MDS_BootStart(this, MDS_BOOT_PREPARE_LOG));
   } else {
     mdlog->get_journaler()->reread_head_and_probe(
       new C_MDS_StandbyReplayRestartFinish(this, mdlog->get_journaler()->get_read_pos()));
