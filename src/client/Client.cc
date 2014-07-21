ca6ffe4c87d45becba4bbc1ecdb9788668014a49
Sage Weil <sage@redhat.com>
2014-07-20 20:49:19 -0700

    client: add a Finisher for the Objecter
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -157,6 +157,7 @@ Client::Client(Messenger *m, MonClient *mc)
     getgroups_cb_handle(NULL),
     async_ino_invalidator(m->cct),
     async_dentry_invalidator(m->cct),
+    objecter_finisher(m->cct),
     tick_event(NULL),
     monclient(mc), messenger(m), whoami(m->get_myname().num()),
     initialized(false), mounted(false), unmounting(false),
@@ -200,6 +201,7 @@ Client::Client(Messenger *m, MonClient *mc)
 				  cct->_conf->client_oc_target_dirty,
 				  cct->_conf->client_oc_max_dirty_age,
 				  true);
+  objecter_finisher.start();
   filer = new Filer(objecter);
 }
 
@@ -445,6 +447,10 @@ void Client::shutdown()
   timer.shutdown();
   objecter->shutdown();
   client_lock.Unlock();
+
+  objecter_finisher.wait_for_empty();
+  objecter_finisher.stop();
+
   monclient->shutdown();
 
   if (logger) {
