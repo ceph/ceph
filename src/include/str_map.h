844d767d954361706ab7ab0ea74ef2d8e5ff823d
Joao Eduardo Luis <joao.luis@inktank.com>
2014-07-28 23:09:24 +0100

    common: str_map: add helper methods to get values from maps
    Both methods obtain values for keys from a given map.  Main distinction
    is that one method will return a default value if key is not present and
    if the default value is specified (i.e., not NULL), while the other
    method will return the value of a fallback key if the key is not
    present.
    
    Signed-off-by: Joao Eduardo Luis <joao.luis@inktank.com>

--- src/include/str_map.h
+++ src/include/str_map.h
@@ -56,4 +56,42 @@ extern int get_str_map(const std::string &str,
 		       std::ostream &ss,
 		       std::map<std::string,std::string> *str_map);
 
+/**
+ * Returns the value of **key** in **str_map** if available.
+ *
+ * If **key** is not available in **str_map**, and if **def_val** is
+ * not-NULL then returns **def_val**. Otherwise checks if the value of
+ * **key** is an empty string and if so will return **key**.
+ * If the map contains **key**, the function returns the value of **key**.
+ *
+ * @param[in] str_map Map to obtain **key** from
+ * @param[in] key The key to search for in the map
+ * @param[in] def_val The value to return in case **key** is not present
+ */
+extern std::string get_str_map_value(
+    const std::map<std::string,std::string> &str_map,
+    const std::string &key,
+    const std::string *def_val = NULL);
+
+/**
+ * Returns the value of **key** in **str_map** if available.
+ *
+ * If **key** is available in **str_map** returns the value of **key**.
+ *
+ * If **key** is not available in **str_map**, and if **def_key**
+ * is not-NULL and available in **str_map**, then returns the value
+ * of **def_key**.
+ *
+ * Otherwise returns an empty string.
+ *
+ * @param[in] str_map Map to obtain **key** or **def_key** from
+ * @param[in] key Key to obtain the value of from **str_map**
+ * @param[in] def_key Key to fallback to if **key** is not present
+ *                    in **str_map**
+ */
+extern std::string get_str_map_key(
+    const std::map<std::string,std::string> &str_map,
+    const std::string &key,
+    const std::string *fallback_key = NULL);
+
 #endif
