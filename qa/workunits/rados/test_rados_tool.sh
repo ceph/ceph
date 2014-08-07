b6a80753f41e32fc2fcf16850fe6e8518a02c92f
Sage Weil <sage@redhat.com>
2014-08-06 17:00:57 -0700

    qa/workunits/rados/test_rados_tool: add a few xattr tests
    Signed-off-by: Sage Weil <sage@redhat.com>

--- qa/workunits/rados/test_rados_tool.sh
+++ qa/workunits/rados/test_rados_tool.sh
@@ -1,7 +1,13 @@
-#!/bin/sh -ex
+#!/bin/bash -ex
 
 OBJ=test_rados_obj
 POOL=rbd
+
+expect_false()
+{
+	set -x
+	if "$@"; then return 1; else return 0; fi
+}
 
 cleanup() {
     rados -p $POOL rm $OBJ || true
@@ -18,6 +24,29 @@ test_omap() {
     cleanup
 }
 
+test_xattr() {
+    cleanup
+    rados -p $POOL put $OBJ /etc/pass
+    V1=`mktemp fooattrXXXXXXX`
+    V2=`mktemp fooattrXXXXXXX`
+    echo -n fooval > $V1
+    expect_false rados -p $POOL setxattr $OBJ 2>/dev/null
+    expect_false rados -p $POOL setxattr $OBJ foo fooval extraarg 2>/dev/null
+    rados -p $POOL setxattr $OBJ foo fooval
+    rados -p $POOL getxattr $OBJ foo > $V2
+    cmp $V1 $V2
+    cat $V1 | rados -p $POOL setxattr $OBJ bar
+    rados -p $POOL getxattr $OBJ bar > $V2
+    cmp $V1 $V2
+    rados -p $POOL listxattr $OBJ > $V1
+    grep -q foo $V1
+    grep -q bar $V1
+    wc -l $V1 | grep -q "^2 "
+    rm $V1 $V2
+    cleanup
+}
+
+test_xattr
 test_omap
 
 echo OK
