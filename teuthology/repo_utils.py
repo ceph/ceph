35b82675a529802726132b8ce5e69d630ee36781
Zack Cerza <zack.cerza@inktank.com>
2014-08-08 13:11:52 -0600

    Disable pyc removal for now
    Signed-off-by: Zack Cerza <zack.cerza@inktank.com>

--- teuthology/repo_utils.py
+++ teuthology/repo_utils.py
@@ -37,7 +37,7 @@ def enforce_repo_state(repo_url, dest_path, branch, remove_on_error=True):
             log.info("%s was just updated; assuming it is current", branch)
 
         reset_repo(repo_url, dest_path, branch)
-        remove_pyc_files(dest_path)
+        #remove_pyc_files(dest_path)
     except BranchNotFoundError:
         if remove_on_error:
             shutil.rmtree(dest_path, ignore_errors=True)
