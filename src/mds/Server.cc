cf5e6ee84155d17acdc65c9c411829b50dcf30ef
Wido den Hollander <wido@42on.com>
2014-08-08 12:03:56 +0200

    mds: Also check min/max uid on snap removal
    Not only check if the uid is correct when creating a snapshot, but also
    verify if the UID is correct when trying to remove a snapshot.
    
    Fixes: #9029

--- src/mds/Server.cc
+++ src/mds/Server.cc
@@ -7373,7 +7373,7 @@ void Server::handle_client_mksnap(MDRequestRef& mdr)
   
   const string &snapname = req->get_filepath().last_dentry();
 
-  if (mdr->client_request->get_caller_uid() < g_conf->mds_snap_min_uid || mdr->client_request->get_caller_uid() > g_conf->mds_snap_max_uid) {
+  if (mdr->client_request->get_caller_uid() < g_conf->mds_mksnap_min_uid || mdr->client_request->get_caller_uid() > g_conf->mds_mksnap_max_uid) {
     dout(20) << "mksnap " << snapname << " on " << *diri << " denied to uid " << mdr->client_request->get_caller_uid() << dendl;
     reply_request(mdr, -EPERM);
     return;
@@ -7511,6 +7511,13 @@ void Server::handle_client_rmsnap(MDRequestRef& mdr)
   }
 
   const string &snapname = req->get_filepath().last_dentry();
+
+  if (mdr->client_request->get_caller_uid() < g_conf->mds_mksnap_min_uid || mdr->client_request->get_caller_uid() > g_conf->mds_mksnap_max_uid) {
+    dout(20) << "rmsnap " << snapname << " on " << *diri << " denied to uid " << mdr->client_request->get_caller_uid() << dendl;
+    reply_request(mdr, -EPERM);
+    return;
+  }
+
   dout(10) << "rmsnap " << snapname << " on " << *diri << dendl;
 
   // does snap exist?
