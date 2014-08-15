e29da3e91b2d021118ddb68b4927fabab23355e1
Loic Dachary <loic@dachary.org>
2014-08-15 15:56:52 +0200

    replace locally instantiated CephManager
    Use the ctx.manager instance created by ceph.py instead
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- tasks/divergent_priors.py
+++ tasks/divergent_priors.py
@@ -24,25 +24,16 @@ def task(ctx, config):
         config = {}
     assert isinstance(config, dict), \
         'divergent_priors task only accepts a dict for configuration'
-    first_mon = teuthology.get_first_mon(ctx, config)
-    (mon,) = ctx.cluster.only(first_mon).remotes.iterkeys()
 
-    manager = ceph_manager.CephManager(
-        mon,
-        ctx=ctx,
-        logger=log.getChild('ceph_manager'),
-        )
-    ctx.manager = manager
-
-    while len(manager.get_osd_status()['up']) < 3:
+    while len(ctx.manager.get_osd_status()['up']) < 3:
         time.sleep(10)
-    manager.raw_cluster_cmd('tell', 'osd.0', 'flush_pg_stats')
-    manager.raw_cluster_cmd('tell', 'osd.1', 'flush_pg_stats')
-    manager.raw_cluster_cmd('tell', 'osd.2', 'flush_pg_stats')
-    manager.raw_cluster_cmd('osd', 'set', 'noout')
-    manager.raw_cluster_cmd('osd', 'set', 'noin')
-    manager.raw_cluster_cmd('osd', 'set', 'nodown')
-    manager.wait_for_clean()
+    ctx.manager.raw_cluster_cmd('tell', 'osd.0', 'flush_pg_stats')
+    ctx.manager.raw_cluster_cmd('tell', 'osd.1', 'flush_pg_stats')
+    ctx.manager.raw_cluster_cmd('tell', 'osd.2', 'flush_pg_stats')
+    ctx.manager.raw_cluster_cmd('osd', 'set', 'noout')
+    ctx.manager.raw_cluster_cmd('osd', 'set', 'noin')
+    ctx.manager.raw_cluster_cmd('osd', 'set', 'nodown')
+    ctx.manager.wait_for_clean()
 
     # something that is always there
     dummyfile = '/etc/fstab'
@@ -50,14 +41,14 @@ def task(ctx, config):
 
     # create 1 pg pool
     log.info('creating foo')
-    manager.raw_cluster_cmd('osd', 'pool', 'create', 'foo', '1')
+    ctx.manager.raw_cluster_cmd('osd', 'pool', 'create', 'foo', '1')
 
     osds = [0, 1, 2]
     for i in osds:
-        manager.set_config(i, osd_min_pg_log_entries=1)
+        ctx.manager.set_config(i, osd_min_pg_log_entries=1)
 
     # determine primary
-    divergent = manager.get_pg_primary('foo', 0)
+    divergent = ctx.manager.get_pg_primary('foo', 0)
     log.info("primary and soon to be divergent is %d", divergent)
     non_divergent = [0,1,2]
     non_divergent.remove(divergent)
@@ -67,12 +58,12 @@ def task(ctx, config):
     for i in range(1000):
         rados(ctx, mon, ['-p', 'foo', 'put', 'existing_%d' % i, dummyfile])
 
-    manager.wait_for_clean()
+    ctx.manager.wait_for_clean()
 
     # blackhole non_divergent
     log.info("blackholing osds %s", str(non_divergent))
     for i in non_divergent:
-        manager.set_config(i, filestore_blackhole='')
+        ctx.manager.set_config(i, filestore_blackhole='')
 
     # write 1 (divergent) object
     log.info('writing divergent object existing_0')
@@ -88,57 +79,57 @@ def task(ctx, config):
     # kill all the osds
     log.info('killing all the osds')
     for i in osds:
-        manager.kill_osd(i)
+        ctx.manager.kill_osd(i)
     for i in osds:
-        manager.mark_down_osd(i)
+        ctx.manager.mark_down_osd(i)
     for i in osds:
-        manager.mark_out_osd(i)
+        ctx.manager.mark_out_osd(i)
 
     # bring up non-divergent
     log.info("bringing up non_divergent %s", str(non_divergent))
     for i in non_divergent:
-        manager.revive_osd(i)
+        ctx.manager.revive_osd(i)
     for i in non_divergent:
-        manager.mark_in_osd(i)
+        ctx.manager.mark_in_osd(i)
 
     log.info('making log long to prevent backfill')
     for i in non_divergent:
-        manager.set_config(i, osd_min_pg_log_entries=100000)
+        ctx.manager.set_config(i, osd_min_pg_log_entries=100000)
 
     # write 1 non-divergent object (ensure that old divergent one is divergent)
     log.info('writing non-divergent object existing_1')
     rados(ctx, mon, ['-p', 'foo', 'put', 'existing_1', dummyfile2])
 
-    manager.wait_for_recovery()
+    ctx.manager.wait_for_recovery()
 
     # ensure no recovery
     log.info('delay recovery')
     for i in non_divergent:
-        manager.set_config(i, osd_recovery_delay_start=100000)
+        ctx.manager.set_config(i, osd_recovery_delay_start=100000)
 
     # bring in our divergent friend
     log.info("revive divergent %d", divergent)
-    manager.revive_osd(divergent)
+    ctx.manager.revive_osd(divergent)
 
-    while len(manager.get_osd_status()['up']) < 3:
+    while len(ctx.manager.get_osd_status()['up']) < 3:
         time.sleep(10)
 
     log.info('delay recovery divergent')
-    manager.set_config(divergent, osd_recovery_delay_start=100000)
+    ctx.manager.set_config(divergent, osd_recovery_delay_start=100000)
     log.info('mark divergent in')
-    manager.mark_in_osd(divergent)
+    ctx.manager.mark_in_osd(divergent)
 
     log.info('wait for peering')
     rados(ctx, mon, ['-p', 'foo', 'put', 'foo', dummyfile])
 
     log.info("killing divergent %d", divergent)
-    manager.kill_osd(divergent)
+    ctx.manager.kill_osd(divergent)
     log.info("reviving divergent %d", divergent)
-    manager.revive_osd(divergent)
+    ctx.manager.revive_osd(divergent)
 
     log.info('allowing recovery')
     for i in non_divergent:
-        manager.set_config(i, osd_recovery_delay_start=0)
+        ctx.manager.set_config(i, osd_recovery_delay_start=0)
 
     log.info('reading existing_0')
     exit_status = rados(ctx, mon,
