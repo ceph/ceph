00c677b4b69f2a37ebeb663a1918d9ea1d974a59
Yehuda Sadeh <yehuda@redhat.com>
2014-08-21 16:51:16 -0700

    rgw: use a separate callback for civetweb access log
    Access log is separate from the regular civetweb logging. Also, changed
    the log level for the regular logging as it's used mostly for error
    messages.
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_civetweb_log.h
+++ src/rgw/rgw_civetweb_log.h
@@ -2,5 +2,6 @@
 #define CEPH_RGW_CIVETWEB_LOG_H
 
 int rgw_civetweb_log_callback(const struct mg_connection *conn, const char *buf);
+int rgw_civetweb_log_access_callback(const struct mg_connection *conn, const char *buf);
 
 #endif
