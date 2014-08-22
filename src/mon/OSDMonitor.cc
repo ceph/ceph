4b1dcaf026e4ebc30c01f5574f645d1dfd9fb8e9
Sage Weil <sage@redhat.com>
2014-08-21 17:35:15 -0700

    mon/OSDMonitor: verify full_crc when available
    Crash if it is wrong.  The monitors should *always* agree.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -223,6 +223,16 @@ void OSDMonitor::update_from_paxos(bool *need_bootstrap)
     osdmap.encode(full_bl, f | CEPH_FEATURE_RESERVED);
     tx_size += full_bl.length();
 
+    // verify the crc is as expected
+    if (inc.have_crc &&
+	inc.full_crc != osdmap.crc) {
+      derr << "inc for epoch " << osdmap.get_epoch() << " has full_crc "
+	   << inc.full_crc << " but actual is " << osdmap.crc
+	   << " features " << f
+	   << dendl;
+      assert(0 == "got mismatched crc encoding full map");
+    }
+
     put_version_full(t, osdmap.epoch, full_bl);
     put_version_latest_full(t, osdmap.epoch);
 
