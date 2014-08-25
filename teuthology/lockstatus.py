ad278fdc71140dfb4be27895e747356e668e3b6c
Zack Cerza <zack.cerza@inktank.com>
2014-08-25 14:07:29 -0600

    Remove the 'user@' prefix before checking status
    Signed-off-by: Zack Cerza <zack.cerza@inktank.com>

--- teuthology/lockstatus.py
+++ teuthology/lockstatus.py
@@ -1,9 +1,11 @@
 import requests
 import os
 from .config import config
+from .misc import canonicalize_hostname
 
 
 def get_status(name):
+    name = canonicalize_hostname(name, user=None)
     uri = os.path.join(config.lock_server, 'nodes', name, '')
     response = requests.get(uri)
     success = response.ok
