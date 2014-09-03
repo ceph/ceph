aa9037facf5d48ca44efe4090f2a2b37d1436002
Casey Bodley <casey@linuxbox.com>
2014-09-03 12:29:17 -0400

    msg: crc configuration in messenger
    Add new header_crc and data_crc configuration booleans, and use
    them consistently to govern whether CRC is performed in the
    Message encode, decode, and transit paths.
    
    Remove ms_nocrc, changes per Sage.
    Mimimally adapt AsyncMessenger for crcflags.
    
    Signed-off-by: Casey Bodley <casey@linuxbox.com>
    Signed-off-by: Matt Benjamin <matt@cohortfs.com>

--- src/msg/Messenger.cc
+++ src/msg/Messenger.cc
@@ -1,3 +1,5 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
 
 #include "include/types.h"
 #include "Messenger.h"
@@ -19,4 +21,19 @@ Messenger *Messenger::create(CephContext *cct, const string &type,
     return new AsyncMessenger(cct, name, lname, nonce);
   lderr(cct) << "unrecognized ms_type '" << type << "'" << dendl;
   return NULL;
+}
+
+/*
+ * Pre-calculate desired software CRC settings.  CRC computation may
+ * be disabled by default for some transports (e.g., those with strong
+ * hardware checksum support).
+ */
+int Messenger::get_default_crc_flags(md_config_t * conf)
+{
+  int r = 0;
+  if (conf->ms_crc_data)
+    r |= MSG_CRC_DATA;
+  if (conf->ms_crc_header)
+    r |= MSG_CRC_HEADER;
+  return r;
 }
