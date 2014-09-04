444f0a47da62e399dea472c24f8c02820fc4f789
John Spray <john.spray@redhat.com>
2014-09-04 16:47:38 +0100

    mds: add a health metric for failure to recall caps
    Fixes: #9284
    
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/mds/Beacon.cc
+++ src/mds/Beacon.cc
@@ -15,11 +15,13 @@
 
 #include "common/dout.h"
 #include "common/HeartbeatMap.h"
+#include "include/stringify.h"
 
 #include "messages/MMDSBeacon.h"
 #include "mon/MonClient.h"
 #include "mds/MDS.h"
 #include "mds/MDLog.h"
+#include "mds/Locker.h"
 
 #include "Beacon.h"
 
@@ -260,6 +262,32 @@ void Beacon::notify_health(MDS const *mds)
     m.metadata["num_segments"] = mds->mdlog->get_num_segments();
     m.metadata["max_segments"] = g_conf->mds_log_max_segments;
     health.metrics.push_back(m);
+  }
+
+  // Detect clients failing to generate cap releases from SESSION_RECALL messages
+  // May be due to buggy client or resource-hogging application.
+  set<Session*> sessions;
+  mds->sessionmap.get_client_session_set(sessions);
+  utime_t cutoff = ceph_clock_now(g_ceph_context);
+  cutoff -= g_conf->mds_recall_state_timeout;
+
+  for (set<Session*>::iterator i = sessions.begin(); i != sessions.end(); ++i) {
+    Session *session = *i;
+    if (!session->recalled_at.is_zero()) {
+      dout(20) << "Session servicing RECALL " << session->info.inst
+        << ": " << session->recalled_at << " " << session->recall_release_count
+        << "/" << session->recall_count << dendl;
+      if (session->recalled_at < cutoff) {
+        dout(20) << "  exceeded timeout " << session->recalled_at << " vs. " << cutoff << dendl;
+        std::ostringstream oss;
+        oss << "Client " << session->info.inst.name.num() << " failing to respond to cache pressure";
+        MDSHealthMetric m(MDS_HEALTH_CLIENT_RECALL, HEALTH_WARN, oss.str());
+        m.metadata["client_id"] = session->info.inst.name.num();
+        health.metrics.push_back(m);
+      } else {
+        dout(20) << "  within timeout " << session->recalled_at << " vs. " << cutoff << dendl;
+      }
+    }
   }
 }
 
