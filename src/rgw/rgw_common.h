7e13ac8e4e9912eed24680ec66eabc5d20ff8381
Ray Lv <xiangyulv@gmail.com>
2014-09-10 15:33:22 +0800

    rgw: Export user stats in get-user-info Adminops API
    Fixes: #9359
    
    Signed-off-by: Ray Lv <raylv@yahoo-inc.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -827,6 +827,8 @@ struct RGWStorageStats
   uint64_t num_objects;
 
   RGWStorageStats() : category(RGW_OBJ_CATEGORY_NONE), num_kb(0), num_kb_rounded(0), num_objects(0) {}
+
+  void dump(Formatter *f) const;
 };
 
 struct req_state;
