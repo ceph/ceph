ce384806358e02df73901d20341d35f709776e05
luowei <luowei@yahoo-inc.com>
2014-09-11 07:43:13 +0000

    Add reset_tp_timeout in long loop in add_source_info for suicide timeout
    Fixes: #9128
    
    Signed-off-by: luowei@yahoo-inc.com

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -7850,6 +7850,7 @@ void OSD::do_recovery(PG *pg, ThreadPool::TPHandle &handle)
 #endif
     
     PG::RecoveryCtx rctx = create_context();
+    rctx.handle = &handle;
 
     int started;
     bool more = pg->start_recovery_ops(max, &rctx, handle, &started);
@@ -8427,6 +8428,7 @@ void OSD::process_peering_events(
   epoch_t same_interval_since = 0;
   OSDMapRef curmap = service.get_osdmap();
   PG::RecoveryCtx rctx = create_context();
+  rctx.handle = &handle;
   for (list<PG*>::const_iterator i = pgs.begin();
        i != pgs.end();
        ++i) {
@@ -8456,6 +8458,7 @@ void OSD::process_peering_events(
     if (compat_must_dispatch_immediately(pg)) {
       dispatch_context(rctx, pg, curmap, &handle);
       rctx = create_context();
+      rctx.handle = &handle;
     } else {
       dispatch_context_transaction(rctx, pg, &handle);
     }
