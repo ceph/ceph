1a7482224c67fc101939807ffe91d8f262842ffe
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2014-09-17 18:56:00 +0200

    ErasureCodeLrc.h: fix data_chunk_count UNINIT_CTOR
    CID 1238903 (#1 of 1): Uninitialized scalar field (UNINIT_CTOR)
     uninit_member: Non-static class member data_chunk_count is not
     initialized in this constructor nor in any functions that it calls.
    
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>
    (cherry picked from commit 2bb35bba995af1983a81669711fda7855e1ce532)

--- src/erasure-code/lrc/ErasureCodeLrc.h
+++ src/erasure-code/lrc/ErasureCodeLrc.h
@@ -73,7 +73,7 @@ public:
   vector<Step> ruleset_steps;
 
   ErasureCodeLrc() :
-    ruleset_root("default")
+    data_chunk_count(0), ruleset_root("default")
   {
     ruleset_steps.push_back(Step("chooseleaf", "host", 0));
   }
