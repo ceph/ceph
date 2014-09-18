fe705c8fdef2371d3f5b11eb73f87a0cf6ef0f9e
Adam Crume <adamcrume@gmail.com>
2014-09-18 16:57:27 -0700

    common: Add cctid meta variable
    Fixes: #6228
    Signed-off-by: Adam Crume <adamcrume@gmail.com>
    (cherry picked from commit bb45621cb117131707a85154292a3b3cdd1c662a)

--- src/common/config.cc
+++ src/common/config.cc
@@ -947,7 +947,7 @@ int md_config_t::set_val_raw(const char *val, const config_option *opt)
 }
 
 static const char *CONF_METAVARIABLES[] =
-  { "cluster", "type", "name", "host", "num", "id", "pid" };
+  { "cluster", "type", "name", "host", "num", "id", "pid", "cctid" };
 static const int NUM_CONF_METAVARIABLES =
       (sizeof(CONF_METAVARIABLES) / sizeof(CONF_METAVARIABLES[0]));
 
@@ -1059,6 +1059,8 @@ bool md_config_t::expand_meta(std::string &origval,
 	  out += name.get_id().c_str();
 	else if (var == "pid")
 	  out += stringify(getpid());
+	else if (var == "cctid")
+	  out += stringify((unsigned long long)this);
 	else
 	  assert(0); // unreachable
 	expanded = true;
