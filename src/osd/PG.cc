f65668dc607ea59581a91541c1273d67ded3fbab
David Zafman <dzafman@redhat.com>
2014-09-24 16:02:21 -0700

    osd: Return EOPNOTSUPP if a set-alloc-hint occurs with OSDs that don't support
    Add CEPH_FEATURE_OSD_SET_ALLOC_HINT feature bit
    Collect the intersection of all peer feature bits during peering
    When handling CEPH_OSD_OP_SETALLOCHINT check that all OSDs support it
    by checking for CEPH_FEATURE_OSD_SET_ALLOC_HINT feature bit.
    
    Fixes: #9419
    Backport: firefly
    
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -202,7 +202,8 @@ PG::PG(OSDService *o, OSDMapRef curmap,
   scrub_after_recovery(false),
   active_pushes(0),
   recovery_state(this),
-  pg_id(p)
+  pg_id(p),
+  peer_features((uint64_t)-1)
 {
 #ifdef PG_DEBUG_REFS
   osd->add_pgid(p, this);
@@ -6693,6 +6694,7 @@ PG::RecoveryState::GetInfo::GetInfo(my_context ctx)
   if (!prior_set.get())
     pg->build_prior(prior_set);
 
+  pg->reset_peer_features();
   get_infos();
   if (peer_info_requested.empty() && !prior_set->pg_down) {
     post_event(GotInfo());
@@ -6768,6 +6770,9 @@ boost::statechart::result PG::RecoveryState::GetInfo::react(const MNotifyRec& in
       }
       get_infos();
     }
+    dout(20) << "Adding osd: " << infoevt.from.osd << " features: "
+      << hex << infoevt.features << dec << dendl;
+    pg->apply_peer_features(infoevt.features);
 
     // are we done getting everything?
     if (peer_info_requested.empty() && !prior_set->pg_down) {
@@ -6826,6 +6831,7 @@ boost::statechart::result PG::RecoveryState::GetInfo::react(const MNotifyRec& in
 	  break;
 	}
       }
+      dout(20) << "Common features: " << hex << pg->get_min_peer_features() << dec << dendl;
       post_event(GotInfo());
     }
   }
--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -3800,6 +3800,10 @@ int ReplicatedPG::do_osd_ops(OpContext *ctx, vector<OSDOp>& ops)
       ++ctx->num_write;
       {
 	tracepoint(osd, do_osd_op_pre_setallochint, soid.oid.name.c_str(), soid.snap.val, op.alloc_hint.expected_object_size, op.alloc_hint.expected_write_size);
+        if (!(get_min_peer_features() & CEPH_FEATURE_OSD_SET_ALLOC_HINT)) { 
+          result = -EOPNOTSUPP;
+          break;
+        }
         if (!obs.exists) {
           ctx->mod_desc.create();
           t->touch(soid);
