c39b7b67a503a15315dca92e21b8c585e507b639
Janne Grunau <j@jannau.net>
2014-09-29 14:34:32 +0200

    ceph_erasure_code_benchmark: use 32-byte aligned input
    The benchmark is supposed to measure the encoding/decoding speed and
    not the overhead of buffer realignments.
    
    Signed-off-by: Janne Grunau <j@jannau.net>

--- src/test/erasure-code/ceph_erasure_code_benchmark.cc
+++ src/test/erasure-code/ceph_erasure_code_benchmark.cc
@@ -132,6 +132,7 @@ int ErasureCodeBench::encode()
 
   bufferlist in;
   in.append(string(in_size, 'X'));
+  in.rebuild_aligned(32);
   set<int> want_to_encode;
   for (int i = 0; i < k + m; i++) {
     want_to_encode.insert(i);
@@ -160,6 +161,7 @@ int ErasureCodeBench::decode()
 
   bufferlist in;
   in.append(string(in_size, 'X'));
+  in.rebuild_aligned(32);
 
   set<int> want_to_encode;
   for (int i = 0; i < k + m; i++) {
