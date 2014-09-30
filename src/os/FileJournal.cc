8caed06efc82cf6afa06bd458855e7ac79f596ca
Loic Dachary <loic-201408@dachary.org>
2014-09-30 14:49:45 +0200

    os: fix confusing indentation in FileJournal::corrupt
    Signed-off-by: Loic Dachary <loic-201408@dachary.org>

--- src/os/FileJournal.cc
+++ src/os/FileJournal.cc
@@ -1883,19 +1883,19 @@ void FileJournal::corrupt(
   if (corrupt_at >= header.max_size)
     corrupt_at = corrupt_at + get_top() - header.max_size;
 
-    int64_t actual = ::lseek64(fd, corrupt_at, SEEK_SET);
-    assert(actual == corrupt_at);
+  int64_t actual = ::lseek64(fd, corrupt_at, SEEK_SET);
+  assert(actual == corrupt_at);
 
-    char buf[10];
-    int r = safe_read_exact(fd, buf, 1);
-    assert(r == 0);
+  char buf[10];
+  int r = safe_read_exact(fd, buf, 1);
+  assert(r == 0);
 
-    actual = ::lseek64(wfd, corrupt_at, SEEK_SET);
-    assert(actual == corrupt_at);
+  actual = ::lseek64(wfd, corrupt_at, SEEK_SET);
+  assert(actual == corrupt_at);
 
-    buf[0]++;
-    r = safe_write(wfd, buf, 1);
-    assert(r == 0);
+  buf[0]++;
+  r = safe_write(wfd, buf, 1);
+  assert(r == 0);
 }
 
 void FileJournal::corrupt_payload(
