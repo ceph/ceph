1901129402dfcbb129acd65606f912a232834092
Sage Weil <sage@redhat.com>
2014-10-01 13:02:23 -0700

    mon: put 'ceph status' quorum status on new line
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mon/Monitor.cc
+++ src/mon/Monitor.cc
@@ -2292,7 +2292,8 @@ void Monitor::get_cluster_status(stringstream &ss, Formatter *f)
     ss << "    cluster " << monmap->get_fsid() << "\n";
     ss << "     health " << joinify(health.begin(), health.end(), 
 				    string("\n            ")) << "\n";
-    ss << "     monmap " << *monmap << ", election epoch " << get_epoch()
+    ss << "     monmap " << *monmap << "\n";
+    ss << "            election epoch " << get_epoch()
        << ", quorum " << get_quorum() << " " << get_quorum_names() << "\n";
     if (mdsmon()->mdsmap.get_enabled())
       ss << "     mdsmap " << mdsmon()->mdsmap << "\n";
