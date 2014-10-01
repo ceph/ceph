0bad4d9daa8d7765c6cda65c426fc6b9001902c4
Alfredo Deza <alfredo.deza@inktank.com>
2014-10-01 15:17:36 -0400

    pybind: create a setup.py for the ceph python package
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

---
+++ src/pybind/ceph/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+
+def long_description():
+    readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+    return open(readme).read()
+
+
+setup(
+    name = 'ceph',
+    description = 'bindings to Ceph, the distributed file system',
+    packages=find_packages(),
+    author = 'Inktank',
+    author_email = 'ceph-devel@vger.kernel.org',
+    version = '0.80.6',
+    license = "LGPL2",
+    zip_safe = False,
+    keywords = "ceph, rest, bindings, api, cli",
+    install_requires = ['flask==0.10', 'rados'],
+    long_description = long_description(),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Topic :: System :: Filesystems',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
