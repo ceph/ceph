846272cd64d7332aefd03c9187d3724c143511f3
Alfredo Deza <alfredo.deza@inktank.com>
2014-10-01 15:56:39 -0400

    pybind: create the setup.py file for python-rados
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

---
+++ src/pybind/rados/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+
+def long_description():
+    readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+    return open(readme).read()
+
+
+setup(
+    name = 'python-rados',
+    description = 'Bindings for rados, the object storage utility from Ceph',
+    packages=find_packages(),
+    author = 'Inktank',
+    author_email = 'ceph-devel@vger.kernel.org',
+    version = '0.80.6',
+    license = "LGPL2",
+    zip_safe = False,
+    keywords = "ceph, rados, bindings, api, cli",
+    long_description = long_description(),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Topic :: System :: Filesystems',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
