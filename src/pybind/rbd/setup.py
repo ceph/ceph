fed205f604742d4c49490ce688db8ede46017678
Alfredo Deza <alfredo.deza@inktank.com>
2014-10-02 10:31:41 -0400

    pybind: add the setup.py for python-rbd
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

---
+++ src/pybind/rbd/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+
+def long_description():
+    readme = os.path.join(os.path.dirname(__file__), 'README.rst')
+    return open(readme).read()
+
+
+setup(
+    name = 'python-rbd',
+    description = 'Bindings for rbd [ceph]',
+    packages=find_packages(),
+    author = 'Inktank',
+    author_email = 'ceph-devel@vger.kernel.org',
+    version = '0.67',
+    license = "LGPL2",
+    zip_safe = False,
+    keywords = "ceph, rbd, bindings, api, cli",
+    long_description = long_description(),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Topic :: System :: Filesystems',
+        'Operating System :: POSIX',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
