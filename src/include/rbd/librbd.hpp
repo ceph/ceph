fa96bb4db47c500947efcab51892f45a04b03d30
Jason Dillaman <dillaman@redhat.com>
2014-10-07 14:17:50 -0400

    librbd: Stub out new exclusive image feature
    When an image is flagged with the exclusive feature, only a
    single client can modify the image.  This is groundwork for
    an RBD object map optimization and for RBD mirroring.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/include/rbd/librbd.hpp
+++ src/include/rbd/librbd.hpp
@@ -117,6 +117,10 @@ public:
   int size(uint64_t *size);
   int features(uint64_t *features);
   int overlap(uint64_t *overlap);
+
+  /* exclusive lock feature */
+  int is_exclusive_lock_owner(bool *is_owner);
+
   int copy(IoCtx& dest_io_ctx, const char *destname);
   int copy2(Image& dest);
   int copy_with_progress(IoCtx& dest_io_ctx, const char *destname,
