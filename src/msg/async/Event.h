e674cfbce5296334c5f6631be515bf5bfbe8cf88
Haomai Wang <haomaiwang@gmail.com>
2014-10-08 14:52:24 +0800

    AsyncMessenger: add copyright for async files
    Signed-off-by: Haomai Wang <haomaiwang@gmail.com>

--- src/msg/async/Event.h
+++ src/msg/async/Event.h
@@ -1,5 +1,19 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*- 
 // vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2014 UnitedStack <haomai@unitedstack.com>
+ *
+ * Author: Haomai Wang <haomaiwang@gmail.com>
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
 #ifndef CEPH_MSG_EVENT_H
 #define CEPH_MSG_EVENT_H
 
