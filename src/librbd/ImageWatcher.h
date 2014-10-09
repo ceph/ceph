3285b887e3c82866120ba7fae96bf9bbf029d7ef
Jason Dillaman <dillaman@redhat.com>
2014-10-08 23:25:28 -0400

    librbd: Add maintenance operation requests to ImageWatcher
    Snapshot, flatten, and resize maintenance operations can now
    be coordinated via the client holding the exclusive lock for the
    image.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/ImageWatcher.h
+++ src/librbd/ImageWatcher.h
@@ -22,9 +22,21 @@ namespace librbd {
 
   class AioCompletion;
   class ImageCtx;
+  class ProgressContext;
+
+  struct RemoteAsyncRequest {
+    uint64_t gid;
+    uint64_t handle;
+    uint64_t request_id;
+
+    RemoteAsyncRequest() : gid(), handle(), request_id() {}
+    RemoteAsyncRequest(uint64_t gid_, uint64_t handle_, uint64_t request_id_)
+      : gid(gid_), handle(handle_), request_id(request_id_) {}
+  };
 
   class ImageWatcher {
   public:
+
 
     ImageWatcher(ImageCtx& image_ctx);
     ~ImageWatcher();
@@ -44,6 +56,15 @@ namespace librbd {
 		     AioCompletion* c);
     int unlock();
 
+    int notify_async_progress(const RemoteAsyncRequest &remote_async_request,
+			      uint64_t offset, uint64_t total);
+    int notify_async_complete(const RemoteAsyncRequest &remote_async_request,
+			      int r);
+
+    int notify_flatten(ProgressContext &prog_ctx);
+    int notify_resize(uint64_t size, ProgressContext &prog_ctx);
+    int notify_snap_create(const std::string &snap_name);
+
     static void notify_header_update(librados::IoCtx &io_ctx,
 				     const std::string &oid);
 
@@ -55,6 +76,7 @@ namespace librbd {
       LOCK_OWNER_STATE_RELEASING
     };
 
+    typedef std::pair<Context *, ProgressContext *> AsyncRequest;
     typedef std::pair<boost::function<int(AioCompletion *)>,
 		      AioCompletion *> AioRequest;
 
@@ -88,6 +110,10 @@ namespace librbd {
     RWLock m_watch_lock;
     int m_watch_error;
 
+    RWLock m_async_request_lock;
+    uint64_t m_async_request_id;
+    std::map<uint64_t, AsyncRequest> m_async_requests;
+
     Mutex m_aio_request_lock;
     Cond m_aio_request_cond;
     std::vector<AioRequest> m_aio_requests;
@@ -108,16 +134,32 @@ namespace librbd {
     void cancel_retry_aio_requests();
     void finalize_retry_aio_requests();
     void retry_aio_requests();
+
+    void cancel_aio_requests(int result);
+    void cancel_async_requests(int result);
+
+    uint64_t encode_async_request(bufferlist &bl);
     static int decode_response_code(bufferlist &bl);
 
     void notify_released_lock();
     void notify_request_lock();
     int notify_lock_owner(bufferlist &bl, bufferlist &response);
 
+    int notify_async_request(uint64_t async_request_id, bufferlist &in,
+			     ProgressContext& prog_ctx);
+    void notify_request_leadership();
+    int notify_leader(bufferlist &bl, bufferlist &response);
+
     void handle_header_update();
     void handle_acquired_lock();
     void handle_released_lock();
     void handle_request_lock(bufferlist *out);
+
+    void handle_async_progress(bufferlist::iterator iter);
+    void handle_async_complete(bufferlist::iterator iter);
+    void handle_flatten(bufferlist::iterator iter, bufferlist *out);
+    void handle_resize(bufferlist::iterator iter, bufferlist *out);
+    void handle_snap_create(bufferlist::iterator iter, bufferlist *out);
     void handle_unknown_op(bufferlist *out);
     void handle_notify(uint64_t notify_id, uint64_t handle, bufferlist &bl);
     void handle_error(uint64_t cookie, int err);
