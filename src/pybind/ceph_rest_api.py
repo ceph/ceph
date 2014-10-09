b58380f30c45a4a1e34822fe72418ee084c86f5e
Alfredo Deza <alfredo.deza@inktank.com>
2014-10-09 12:24:52 -0400

    pybind: correct the ceph_rest path for the deprecation warning
    Signed-off-by: Alfredo Deza <alfredo.deza@inktank.com>

--- src/pybind/ceph_rest_api.py
+++ src/pybind/ceph_rest_api.py
@@ -3,9 +3,9 @@ import warnings
 warning_msg = """
 The 'ceph_rest_api' is deprecated and has been moved to a new module. Upgrade your imports to:
 
-    from ceph import rest
+    import ceph_rest
 """
 # Don't use ``DeprecationWarning`` as they are ignored by default in Python 2.7+
 warnings.warn(warning_msg, Warning)
 
-from ceph.rest import *
+from ceph_rest import *
