fc69e0c20a84959317a1049379bda15c8c7cf70d
Almightybeeij <almightybeeij@gmail.com>
2014-10-10 13:55:34 -0500

    Update libcephfs.cc
    Explained where ERRO_MON_MAP_BUILD, ERROR_NEW_CLIENT, and ERROR_MESSENGER_START were defined.

--- src/libcephfs.cc
+++ src/libcephfs.cc
@@ -75,7 +75,7 @@ public:
 
     //monmap
     monclient = new MonClient(cct);
-    ret = -ERROR_MON_MAP_BUILD;
+    ret = -ERROR_MON_MAP_BUILD; //defined in libcephfs.h
     if (monclient->build_initial_monmap() < 0)
       goto fail;
 
@@ -83,12 +83,12 @@ public:
     messenger = Messenger::create(cct, entity_name_t::CLIENT(), "client", msgr_nonce);
 
     //at last the client
-    ret = -ERROR_NEW_CLIENT;
+    ret = -ERROR_NEW_CLIENT; //defined in libcephfs.h
     client = new Client(messenger, monclient);
     if (!client)
       goto fail;
 
-    ret = -ERROR_MESSENGER_START;
+    ret = -ERROR_MESSENGER_START; //defined in libcephfs.h
     if (messenger->start() != 0)
       goto fail;
 
