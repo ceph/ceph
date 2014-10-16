236895eea65f8706baa5fdef96fb00ad5b82218c
Loic Dachary <loic-201408@dachary.org>
2014-10-15 17:02:58 -0700

    crush: improve constness of CrushWrapper methods
    A number of CrushWrapper get methods or predicates were not const
    because they need to maintain transparently the rmaps. Make the rmaps
    mutable and update the constness of the methods to match what the caller
    would expect.
    
    Signed-off-by: Loic Dachary <loic-201408@dachary.org>

--- src/crush/CrushWrapper.cc
+++ src/crush/CrushWrapper.cc
@@ -641,7 +641,7 @@ int CrushWrapper::update_item(CephContext *cct, int item, float weight, string n
   return ret;
 }
 
-int CrushWrapper::get_item_weight(int id)
+int CrushWrapper::get_item_weight(int id) const
 {
   for (int bidx = 0; bidx < crush->max_buckets; bidx++) {
     crush_bucket *b = crush->buckets[bidx];
@@ -703,7 +703,7 @@ int CrushWrapper::adjust_subtree_weight(CephContext *cct, int id, int weight)
   return changed;
 }
 
-bool CrushWrapper::check_item_present(int id)
+bool CrushWrapper::check_item_present(int id) const
 {
   bool found = false;
 
