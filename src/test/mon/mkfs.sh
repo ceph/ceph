7a6ca17f185283d135d9e7ef9789491318b9cfde
Loic Dachary <loic-201408@dachary.org>
2014-10-18 13:15:41 -0700

    tests: use different ports for each mon
    Run the mon on each test on a different port so they can run in
    parallel.
    
    http://tracker.ceph.com/issues/9815 Fixes: #9815
    
    Signed-off-by: Loic Dachary <loic-201408@dachary.org>

--- src/test/mon/mkfs.sh
+++ src/test/mon/mkfs.sh
@@ -23,8 +23,7 @@ export CEPH_CONF=/dev/null
 unset CEPH_ARGS
 MON_ID=a
 MON_DIR=$DIR/$MON_ID
-PORT=7451
-MONA=127.0.0.1:$PORT
+CEPH_MON=127.0.0.1:7110
 TIMEOUT=360
 
 function setup() {
@@ -47,7 +46,7 @@ function mon_mkfs() {
         --mkfs \
         --mon-data=$MON_DIR \
         --mon-initial-members=$MON_ID \
-        --mon-host=$MONA \
+        --mon-host=$CEPH_MON \
         "$@"
 }
 
@@ -63,7 +62,7 @@ function mon_run() {
         --mon-cluster-log-file=$MON_DIR/log \
         --run-dir=$MON_DIR \
         --pid-file=$MON_DIR/pidfile \
-        --public-addr $MONA \
+        --public-addr $CEPH_MON \
         "$@"
 }
 
@@ -94,7 +93,7 @@ function auth_none() {
 
     mon_run --auth-supported=none
     
-    timeout $TIMEOUT ./ceph --mon-host $MONA mon stat || return 1
+    timeout $TIMEOUT ./ceph --mon-host $CEPH_MON mon stat || return 1
 }
 
 function auth_cephx_keyring() {
@@ -113,7 +112,7 @@ EOF
     timeout $TIMEOUT ./ceph \
         --name mon. \
         --keyring $MON_DIR/keyring \
-        --mon-host $MONA mon stat || return 1
+        --mon-host $CEPH_MON mon stat || return 1
 }
 
 function auth_cephx_key() {
@@ -140,7 +139,7 @@ function auth_cephx_key() {
     timeout $TIMEOUT ./ceph \
         --name mon. \
         --keyring $MON_DIR/keyring \
-        --mon-host $MONA mon stat || return 1
+        --mon-host $CEPH_MON mon stat || return 1
 }
 
 function makedir() {
