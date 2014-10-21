a8ac9c1231417d065dc02b1f37a59750a89b2039
Sage Weil <sage@redhat.com>
2014-10-21 08:42:38 -0700

    msg/Messenger: allow ms_type 'random'
    Randomly select simple or async, for testing purposes.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/msg/Messenger.cc
+++ src/msg/Messenger.cc
@@ -10,9 +10,12 @@ Messenger *Messenger::create(CephContext *cct,
 			     string lname,
 			     uint64_t nonce)
 {
-  if (cct->_conf->ms_type == "simple")
+  int r = -1;
+  if (cct->_conf->ms_type == "random")
+    r = rand() % 2;
+  if (r == 0 || cct->_conf->ms_type == "simple")
     return new SimpleMessenger(cct, name, lname, nonce);
-  else if (cct->_conf->ms_type == "async")
+  else if (r == 1 || cct->_conf->ms_type == "async")
     return new AsyncMessenger(cct, name, lname, nonce);
   lderr(cct) << "unrecognized ms_type '" << cct->_conf->ms_type << "'" << dendl;
   return NULL;
