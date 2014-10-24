2ee29c110861cf269de5c7f382d20fcd8c46cda3
Sage Weil <sage@redhat.com>
2014-10-24 11:06:08 -0700

    Revert "rbd: ObjectCacher reads can hang when reading sparse files"
    This reverts commit d6f428e9f9ed51ea223df7a858d4ac4c607ae831.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osdc/ObjectCacher.cc
+++ src/osdc/ObjectCacher.cc
@@ -709,9 +709,6 @@ void ObjectCacher::bh_read_finish(int64_t poolid, sobject_t oid, ceph_tid_t tid,
       }
     }
 
-    ls.splice(ls.end(), waitfor_read);
-    waitfor_read.clear();
-
     // apply to bh's!
     loff_t opos = start;
     while (true) {
@@ -765,6 +762,9 @@ void ObjectCacher::bh_read_finish(int64_t poolid, sobject_t oid, ceph_tid_t tid,
 
       loff_t oldpos = opos;
       opos = bh->end();
+
+      ls.splice(ls.end(), waitfor_read);
+      waitfor_read.clear();
 
       if (r == -ENOENT) {
 	if (trust_enoent) {
