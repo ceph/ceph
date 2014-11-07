f66bf31b6743246fb1c88238cf18101238dee3a4
Greg Farnum <gfarnum@redhat.com>
2014-11-06 17:48:01 -0800

    qa: use sudo even more when rsyncing /usr
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>
    (cherry picked from commit 3aa7797741f9cff06053a2f31550fe6929039692)

--- qa/workunits/fs/misc/multiple_rsync.sh
+++ qa/workunits/fs/misc/multiple_rsync.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -ex
 
-rsync -av --exclude local/ /usr/ usr.1
-rsync -av --exclude local/ /usr/ usr.2
+sudo rsync -av --exclude local/ /usr/ usr.1
+sudo rsync -av --exclude local/ /usr/ usr.2
 
 # this shouldn't transfer any additional files
 echo we should get 4 here if no additional files are transfered
-rsync -auv --exclude local/ /usr/ usr.1 | tee /tmp/$$
+sudo rsync -auv --exclude local/ /usr/ usr.1 | tee /tmp/$$
 hexdump -C /tmp/$$
 wc -l /tmp/$$ | grep 4
 sudo rsync -auv --exclude local/ /usr/ usr.2 | tee /tmp/$$
