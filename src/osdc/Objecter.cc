4afa6088dbace462d867b1bda0c90efbaac3fdef
Sage Weil <sage@redhat.com>
2014-11-07 19:47:53 -0800

    osdc/Objecter: stop sending pings once we get an error
    If we get ENOTCONN or ETIMEDOUT we can stop pinging.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osdc/Objecter.cc
+++ src/osdc/Objecter.cc
@@ -1673,7 +1673,7 @@ void Objecter::tick()
         assert(op->session);
         ldout(cct, 10) << " pinging osd that serves lingering tid " << p->first << " (osd." << op->session->osd << ")" << dendl;
         toping.insert(op->session);
-	if (op->cookie)
+	if (op->cookie && !op->last_error)
 	  _send_linger_ping(op);
       }
       for (map<uint64_t,CommandOp*>::iterator p = s->command_ops.begin();
