32cfbbdbe062a7b82ac562d89426ab73bf84f8f2
Jason Dillaman <dillaman@redhat.com>
2014-11-10 12:25:50 -0500

    librbd: Create async versions of long-running maintenance operations
    Resize and flatten now have async versions. The existing resize
    and flatten operations now use the async versions internally. The
    async operations will be used by the client holding the exclusive
    lock when it receives maintenance requests from other clients.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

---
+++ src/librbd/AsyncObjectThrottle.cc
@@ -0,0 +1,84 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+#include "librbd/AsyncObjectThrottle.h"
+#include "include/rbd/librbd.hpp"
+
+namespace librbd
+{
+
+AsyncObjectThrottle::AsyncObjectThrottle(const ContextFactory& context_factory,
+				 	 Context *ctx, ProgressContext &prog_ctx,
+					 uint64_t object_no,
+					 uint64_t end_object_no)
+  : m_lock("librbd::AsyncThrottle::m_lock"),
+    m_context_factory(context_factory), m_ctx(ctx), m_prog_ctx(prog_ctx),
+    m_object_no(object_no), m_end_object_no(end_object_no), m_current_ops(0),
+    m_ret(0)
+{
+}
+
+int AsyncObjectThrottle::start_ops(uint64_t max_concurrent) {
+  bool complete;
+  {
+    Mutex::Locker l(m_lock);
+    for (uint64_t i = 0; i < max_concurrent; ++i) {
+      int r = start_next_op();
+      if (r < 0 && m_current_ops == 0) {
+        return r;
+      }
+    }
+    complete = (m_current_ops == 0);
+  }
+  if (complete) {
+    m_ctx->complete(m_ret);
+    delete this;
+  }
+  return 0;
+}
+
+void AsyncObjectThrottle::finish_op(int r) {
+  bool complete;
+  {
+    Mutex::Locker l(m_lock);
+    --m_current_ops;
+    if (r < 0 && r != -ENOENT && m_ret == 0) {
+      m_ret = r;
+    }
+
+    start_next_op();
+    complete = (m_current_ops == 0);
+  }
+  if (complete) {
+    m_ctx->complete(m_ret);
+    delete this;
+  }
+}
+
+int AsyncObjectThrottle::start_next_op() {
+  bool done = false;
+  while (!done) {
+    if (m_ret != 0 || m_object_no >= m_end_object_no) {
+      return m_ret;
+    }
+
+    uint64_t ono = m_object_no++;
+    C_AsyncObjectThrottle *ctx = m_context_factory(*this, ono);
+
+    int r = ctx->send();
+    if (r < 0) {
+      m_ret = r;
+      delete ctx;
+      return m_ret;
+    } else if (r > 0) {
+      // op completed immediately
+      delete ctx;
+    } else {
+      ++m_current_ops;
+      done = true;
+    }
+    m_prog_ctx.update_progress(ono, m_end_object_no);
+  }
+  return 0;
+}
+
+} // namespace librbd
