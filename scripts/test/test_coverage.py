22ef7fa8bde49aeb26e64ffe645438962c2674ff
Andrew Schoen <aschoen@redhat.com>
2014-11-14 10:07:18 -0600

    added a test to make sure docopt was parsing things correctly for teuthology-coverage
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- scripts/test/test_coverage.py
+++ scripts/test/test_coverage.py
@@ -1,5 +1,27 @@
+from docopt import docopt
+
 from script import Script
+from scripts import coverage
+
+
+doc = coverage.__doc__
 
 
 class TestCoverage(Script):
     script_name = 'teuthology-coverage'
+
+    def test_args(self):
+        args = docopt(doc, [
+            "--skip-init",
+            "--lcov-output=some/other/dir",
+            "--html-output=html/output/dir",
+            "--cov-tools-dir=cov/tools/dir",
+            "--verbose",
+            "some/test/dir"]
+        )
+        assert args["--skip-init"]
+        assert args["--lcov-output"] == "some/other/dir"
+        assert args["<test_dir>"] == "some/test/dir"
+        assert args["--html-output"] == "html/output/dir"
+        assert args["--cov-tools-dir"] == "cov/tools/dir"
+        assert args["--verbose"]
