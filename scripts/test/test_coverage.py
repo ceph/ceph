5376ecf4e4916dc7bb6b58c8197f0a60da22e94d
Andrew Schoen <aschoen@redhat.com>
2014-11-17 10:14:36 -0600

    refactored teuthology.coverage to use an dict from docopt instead of a namespace from argparse
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- scripts/test/test_coverage.py
+++ scripts/test/test_coverage.py
@@ -10,7 +10,7 @@ doc = coverage.__doc__
 class TestCoverage(Script):
     script_name = 'teuthology-coverage'
 
-    def test_args(self):
+    def test_all_args(self):
         args = docopt(doc, [
             "--skip-init",
             "--lcov-output=some/other/dir",
@@ -25,3 +25,13 @@ class TestCoverage(Script):
         assert args["--html-output"] == "html/output/dir"
         assert args["--cov-tools-dir"] == "cov/tools/dir"
         assert args["--verbose"]
+
+    def test_missing_optional_args(self):
+        args = docopt(doc, [
+            "--lcov-output=some/other/dir",
+            "some/test/dir"]
+        )
+        assert not args['--html-output']
+        assert not args['--skip-init']
+        assert not args['--verbose']
+        assert args['--cov-tools-dir'] == "../../coverage"
