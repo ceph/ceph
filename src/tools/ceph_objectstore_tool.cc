59b7fd844c49563790a097b7a96d483ee465e695
David Zafman <dzafman@redhat.com>
2014-11-20 13:00:10 -0800

    ceph_objectstore_tool: read_fd() doesn't handle ^D from tty stdin, don't allow
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/tools/ceph_objectstore_tool.cc
+++ src/tools/ceph_objectstore_tool.cc
@@ -2695,6 +2695,12 @@ int main(int argc, char **argv)
         } else {
           int fd;
           if (vm.count("arg1") == 0 || arg1 == "-") {
+            // Since read_fd() doesn't handle ^D from a tty stdin, don't allow it.
+            if (isatty(STDIN_FILENO)) {
+                cerr << "stdin is a tty and no file specified" << std::endl;
+                ret = 1;
+                goto out;
+            }
             fd = STDIN_FILENO;
 	  } else {
             fd = open(arg1.c_str(), O_RDONLY|O_LARGEFILE, 0666);
@@ -2724,6 +2730,12 @@ int main(int argc, char **argv)
 
 	int fd;
 	if (vm.count("arg2") == 0 || arg2 == "-") {
+          // Since read_fd() doesn't handle ^D from a tty stdin, don't allow it.
+          if (isatty(STDIN_FILENO)) {
+            cerr << "stdin is a tty and no file specified" << std::endl;
+            ret = 1;
+            goto out;
+          }
 	  fd = STDIN_FILENO;
 	} else {
 	  fd = open(arg2.c_str(), O_RDONLY|O_LARGEFILE, 0666);
@@ -2759,6 +2771,12 @@ int main(int argc, char **argv)
 
 	int fd;
 	if (vm.count("arg2") == 0 || arg2 == "-") {
+          // Since read_fd() doesn't handle ^D from a tty stdin, don't allow it.
+          if (isatty(STDIN_FILENO)) {
+            cerr << "stdin is a tty and no file specified" << std::endl;
+            ret = 1;
+            goto out;
+          }
 	  fd = STDIN_FILENO;
 	} else {
 	  fd = open(arg2.c_str(), O_RDONLY|O_LARGEFILE, 0666);
@@ -2794,6 +2812,12 @@ int main(int argc, char **argv)
 	  usage(desc);
 	int fd;
 	if (vm.count("arg1") == 0 || arg1 == "-") {
+          // Since read_fd() doesn't handle ^D from a tty stdin, don't allow it.
+          if (isatty(STDIN_FILENO)) {
+            cerr << "stdin is a tty and no file specified" << std::endl;
+            ret = 1;
+            goto out;
+          }
 	  fd = STDIN_FILENO;
 	} else {
 	  fd = open(arg1.c_str(), O_RDONLY|O_LARGEFILE, 0666);
