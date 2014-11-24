0113d98635884f242678b692c152b30e4c0d7bf2
Sage Weil <sage@redhat.com>
2014-11-23 21:11:59 -0800

    os/DBObjectMap: new version v2; drop support for upgrading from v1
    - Don't support upgrade from v1 anymore
    - Add upgrade for v1 -> v2
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/DBObjectMap.h
+++ src/os/DBObjectMap.h
@@ -207,7 +207,7 @@ public:
   int init(bool upgrade = false);
 
   /// Upgrade store to current version
-  int upgrade();
+  int upgrade_to_v2();
 
   /// Consistency check, debug, there must be no parallel writes
   bool check(std::ostream &out);
@@ -322,8 +322,7 @@ public:
   /// String munging (public for testing)
   static string ghobject_key(const ghobject_t &oid);
   static string ghobject_key_v0(coll_t c, const ghobject_t &oid);
-  static bool parse_ghobject_key_v0(const string &in,
-				   coll_t *c, ghobject_t *oid);
+  static int is_buggy_ghobject_key_v1(const string &in);
 private:
   /// Implicit lock on Header->seq
   typedef ceph::shared_ptr<_Header> Header;
