02e5006626b4780d958e40a1d6ec714aa6f46211
Ilya Dryomov <idryomov@redhat.com>
2014-12-03 13:10:17 +0300

    Don't fetch owner info from job_info when --owner given
    Commit 293c2bd49c0d ("Be more informative if owner info is missing")
    introduced --owner but it is still pulled from job_info no matter what.
    Fix it.
    
    Signed-off-by: Ilya Dryomov <idryomov@redhat.com>

--- teuthology/kill.py
+++ teuthology/kill.py
@@ -65,11 +65,12 @@ def kill_job(run_name, job_id, archive_base=None, owner=None,
              machine_type=None):
     serializer = report.ResultsSerializer(archive_base)
     job_info = serializer.job_info(run_name, job_id)
-    if not owner and 'owner' not in job_info:
-        raise RuntimeError(
-            "I could not figure out the owner of the requested job. "
-            "Please pass --owner <owner>.")
-    owner = job_info['owner']
+    if not owner:
+        if 'owner' not in job_info:
+            raise RuntimeError(
+                "I could not figure out the owner of the requested job. "
+                "Please pass --owner <owner>.")
+        owner = job_info['owner']
     kill_processes(run_name, [job_info.get('pid')])
     targets = dict(targets=job_info.get('targets', {}))
     nuke_targets(targets, owner)
