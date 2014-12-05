e1916794ec38f606515abf5e6af67dd641af7d30
Sage Weil <sage@redhat.com>
2014-12-04 17:07:32 -0800

    osd: move pg removal flag into a static helper
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -2722,11 +2722,11 @@ void OSD::load_pgs()
     spg_t pgid;
     snapid_t snap;
     uint64_t seq;
-    char val;
 
     if (it->is_temp(pgid) ||
 	it->is_removal(&seq, &pgid) ||
-	store->collection_getattr(*it, "remove", &val, 1) > 0) {
+	(it->is_pg(pgid, snap) &&
+	 PG::_has_removal_flag(store, pgid))) {
       dout(10) << "load_pgs " << *it << " clearing temp" << dendl;
       recursive_remove_collection(store, *it);
       continue;
