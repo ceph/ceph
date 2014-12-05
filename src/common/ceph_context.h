ceph/ceph

commit 7fed5dee4f96a83d1d6914f6fc0895bba2d15b99

CephContext: Add AssociatedSingletonObject to allow CephContext's singleton
If some objects associated to CephContext want to create a singleton object,
it can inherit AssociatedSingletonObject and implement destruction to get notified.

Signed-off-by: Haomai Wang <haomaiwang@gmail.com>

src/common/ceph_context.h
@@ -17,6 +17,8 @@
 
 #include <iostream>
 #include <stdint.h>
+#include <string>
+using namespace std;
 
 #include "include/buffer.h"
 #include "include/atomic.h"
@@ -58,6 +60,10 @@ private:
   ~CephContext();
   atomic_t nref;
 public:
+  class AssociatedSingletonObject {
+   public:
+    virtual ~AssociatedSingletonObject() {}
+  };
   CephContext *get() {
     nref.inc();
     return this;
@@ -102,6 +108,17 @@ public:
   void do_command(std::string command, cmdmap_t& cmdmap, std::string format,
 		  bufferlist *out);
 
+  template<typename T>
+  void lookup_or_create_singleton_object(T*& p, const string &name) {
+    ceph_spin_lock(&_associated_objs_lock);
+    if (!_associated_objs.count(name)) {
+      p = new T(this);
+      _associated_objs[name] = reinterpret_cast<AssociatedSingletonObject*>(p);
+    } else {
+      p = reinterpret_cast<T*>(_associated_objs[name]);
+    }
+    ceph_spin_unlock(&_associated_objs_lock);
+  }
   /**
    * get a crypto handler
    */
@@ -137,6 +154,9 @@ private:
   CephContextHook *_admin_hook;
 
   ceph::HeartbeatMap *_heartbeat_map;
+
+  ceph_spinlock_t _associated_objs_lock;
+  map<string, AssociatedSingletonObject*> _associated_objs;
 
   // crypto
   CryptoNone *_crypto_none;

src/common/ceph_context.cc
@@ -297,6 +297,7 @@ CephContext::CephContext(uint32_t module_type_)
     _crypto_aes(NULL)
 {
   ceph_spin_init(&_service_thread_lock);
+  ceph_spin_init(&_associated_objs_lock);
 
   _log = new ceph::log::Log(&_conf->subsys);
   _log->start();
@@ -332,6 +333,10 @@ CephContext::CephContext(uint32_t module_type_)
 CephContext::~CephContext()
 {
   join_service_thread();
+
+  for (map<string, AssociatedSingletonObject*>::iterator it = _associated_objs.begin();
+       it != _associated_objs.end(); it++)
+    delete it->second;
 
   if (_conf->lockdep) {
     lockdep_unregister_ceph_context(this);
@@ -371,6 +376,7 @@ CephContext::~CephContext()
 
   delete _conf;
   ceph_spin_destroy(&_service_thread_lock);
+  ceph_spin_destroy(&_associated_objs_lock);
 
   delete _crypto_none;
   delete _crypto_aes;

commit 43542fdb535c77f398cedb3519964f053b6b6014

common/ceph_context: don't import std namespace
This was broken by 7fed5dee4f96a83d1d6914f6fc0895bba2d15b99

Signed-off-by: Sage Weil <sage@redhat.com>

src/common/ceph_context.h
@@ -18,7 +18,6 @@
 #include <iostream>
 #include <stdint.h>
 #include <string>
-using namespace std;
 
 #include "include/buffer.h"
 #include "include/atomic.h"
@@ -109,7 +108,7 @@ public:
 		  bufferlist *out);
 
   template<typename T>
-  void lookup_or_create_singleton_object(T*& p, const string &name) {
+  void lookup_or_create_singleton_object(T*& p, const std::string &name) {
     ceph_spin_lock(&_associated_objs_lock);
     if (!_associated_objs.count(name)) {
       p = new T(this);
@@ -156,7 +155,7 @@ private:
   ceph::HeartbeatMap *_heartbeat_map;
 
   ceph_spinlock_t _associated_objs_lock;
-  map<string, AssociatedSingletonObject*> _associated_objs;
+  std::map<std::string, AssociatedSingletonObject*> _associated_objs;
 
   // crypto
   CryptoNone *_crypto_none;