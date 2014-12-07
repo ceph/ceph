826cfe8198cbf1c4a645de6352f332543dd80932
Sage Weil <sage@redhat.com>
2014-12-06 17:45:28 -0800

    osd/ReplicatedPG: improve proxy read cancelation
    Avoid taking the PG lock for a canceled read op (if we are lucky).  Recheck
    after the lock is taken for good measure.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -1944,7 +1944,11 @@ struct C_ProxyRead : public Context {
       tid(0), prdop(prd)
   {}
   void finish(int r) {
+    if (prdop->canceled)
+      return;
     pg->lock();
+    if (prdop->canceled)
+      return;
     if (last_peering_reset == pg->get_last_peering_reset()) {
       pg->finish_proxy_read(oid, tid, r);
     }
@@ -2046,6 +2050,7 @@ void ReplicatedPG::kick_proxy_read_blocked(hobject_t& soid)
 void ReplicatedPG::cancel_proxy_read(ProxyReadOpRef prdop)
 {
   dout(10) << __func__ << " " << prdop->soid << dendl;
+  prdop->canceled = true;
 
   // cancel objecter op, if we can
   if (prdop->objecter_tid) {
