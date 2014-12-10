ea6b8b3cf655a4dd2816a127a08485e3aa7e930e
Sage Weil <sage@redhat.com>
2014-12-10 09:32:50 -0800

    osd: scrub: set a min age before we update whole-object digest
    If an object is being actively updated, the whole-object digest will
    quickly be invalidated.  On deep scrub, only record that digest if the
    object is a few hours old.  Otherwise, we are wasting an IO.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -566,6 +566,7 @@ OPTION(osd_scrub_chunk_max, OPT_INT, 25)
 OPTION(osd_scrub_sleep, OPT_FLOAT, 0)   // sleep between [deep]scrub ops
 OPTION(osd_deep_scrub_interval, OPT_FLOAT, 60*60*24*7) // once a week
 OPTION(osd_deep_scrub_stride, OPT_INT, 524288)
+OPTION(osd_deep_scrub_update_digest_min_age, OPT_INT, 2*60*60)   // objects must be this old (seconds) before we update the whole-object digest on scrub
 OPTION(osd_scan_list_ping_tp_interval, OPT_U64, 100)
 OPTION(osd_auto_weight, OPT_BOOL, false)
 OPTION(osd_class_dir, OPT_STR, CEPH_LIBDIR "/rados-classes") // where rados plugins are stored
