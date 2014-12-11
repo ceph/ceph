b35886fcad859866202d792ad05a07864241a7bb
Zhiqiang Wang <zhiqiang.wang@intel.com>
2014-12-11 12:36:56 +0800

    osd/ReplicatedPG: remove the peer_type assertion in eval_repop
    When moving to proxy write, this assertion doesn't hold any more.
    
    Signed-off-by: Zhiqiang Wang <zhiqiang.wang@intel.com>
    
    Conflicts:
    	src/osd/ReplicatedPG.cc

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -7554,7 +7554,6 @@ void ReplicatedPG::eval_repop(RepGather *repop)
       }
       reply->add_flags(CEPH_OSD_FLAG_ACK);
       dout(10) << " sending ack on " << *repop << " " << reply << dendl;
-      assert(entity_name_t::TYPE_OSD != m->get_connection()->peer_type);
       osd->send_message_osd_client(reply, m->get_connection());
       repop->sent_ack = true;
     }
