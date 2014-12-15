b239dadc02fc8e09c5bf633a06c479494929e85c
David Zafman <dzafman@redhat.com>
2014-12-15 10:06:02 -0800

    ceph_manager: Fix Thrasher kill_osd() to ignore pool going away
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- tasks/ceph_manager.py
+++ tasks/ceph_manager.py
@@ -260,7 +260,10 @@ class Thrasher:
             cmd = (prefix + "--op import --file {file}")
             cmd = cmd.format(id=imp_osd, file=exp_path)
             proc = imp_remote.run(args=cmd, wait=True, check_status=False)
-            if proc.exitstatus:
+            if proc.exitstatus == 10:
+                self.log("Pool went away before processing an import"
+                         "...ignored");
+            elif proc.exitstatus:
                 raise Exception("ceph-objectstore-tool: "
                                 "import failure with status {ret}".
                                 format(ret=proc.exitstatus))
