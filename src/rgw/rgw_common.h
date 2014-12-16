b30a2de08864ef6238de27d25f8d6eec8e531d30
Yehuda Sadeh <yehuda@redhat.com>
2014-12-16 09:11:20 -0800

    rgw: return SignatureDoesNotMatch instead of AccessDenied
    Fixes: #10329
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -135,6 +135,7 @@ using ceph::crypto::MD5;
 #define ERR_PERMANENT_REDIRECT   2024
 #define ERR_LOCKED               2025
 #define ERR_QUOTA_EXCEEDED       2026
+#define ERR_SIGNATURE_NO_MATCH   2027
 #define ERR_USER_SUSPENDED       2100
 #define ERR_INTERNAL_ERROR       2200
 
