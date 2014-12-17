09343077c86334ab68dc9b43858489ae7c735d74
Andrew Schoen <aschoen@redhat.com>
2014-12-17 09:09:10 -0600

    Fixed a couple imports that were importing teuthology.misc as teuthology.
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- teuthology/test/test_get_distro.py
+++ teuthology/test/test_get_distro.py
@@ -1,6 +1,8 @@
-from .. import misc as teuthology
+from ..misc import get_distro
 
-class Mock: pass
+
+class Mock:
+    pass
 
 
 class TestGetDistro(object):
@@ -12,25 +14,25 @@ class TestGetDistro(object):
         self.fake_ctx.os_type = None
 
     def test_default_distro(self):
-        distro = teuthology.get_distro(self.fake_ctx)
+        distro = get_distro(self.fake_ctx)
         assert distro == 'ubuntu'
 
     def test_argument(self):
         self.fake_ctx.os_type = 'centos'
-        distro = teuthology.get_distro(self.fake_ctx)
+        distro = get_distro(self.fake_ctx)
         assert distro == 'centos'
 
     def test_teuth_config(self):
         self.fake_ctx.config = {'os_type': 'fedora'}
-        distro = teuthology.get_distro(self.fake_ctx)
+        distro = get_distro(self.fake_ctx)
         assert distro == 'fedora'
 
     def test_teuth_config_downburst(self):
         self.fake_ctx.config = {'downburst' : {'distro': 'sles'}}
-        distro = teuthology.get_distro(self.fake_ctx)
+        distro = get_distro(self.fake_ctx)
         assert distro == 'sles'
 
     def test_no_config_or_os_type(self):
         self.fake_ctx = Mock()
-        distro = teuthology.get_distro(self.fake_ctx)
+        distro = get_distro(self.fake_ctx)
         assert distro == 'ubuntu'
