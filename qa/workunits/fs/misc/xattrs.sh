b54e7bd4f1af417d9f589e7dbe4d7e6da0c18f11
Yan, Zheng <zyan@redhat.com>
2014-12-18 14:43:14 +0800

    qa: test zero size xattr
    Signed-off-by: Yan, Zheng <zyan@redhat.com>

--- qa/workunits/fs/misc/xattrs.sh
+++ qa/workunits/fs/misc/xattrs.sh
@@ -4,8 +4,11 @@ set -e
 
 touch file
 
-setfattr -n user.bare -v bare file
+setfattr -n user.foo -v foo file
 setfattr -n user.bar -v bar file
-getfattr -d file | grep bare
+setfattr -n user.empty file
+getfattr -d file | grep foo
+getfattr -d file | grep bar
+getfattr -d file | grep empty
 
 echo OK.
