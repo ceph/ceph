b0ebeba79cf349b9c5a6e387f8246d7b6d0ef576
Sage Weil <sage@redhat.com>
2014-12-23 10:28:22 -0800

    tasks/ceph_manager: dump raw cluster command at debug level
    Signed-off-by: Sage Weil <sage@redhat.com>

--- tasks/ceph_manager.py
+++ tasks/ceph_manager.py
@@ -589,6 +589,7 @@ class CephManager:
             args=ceph_args,
             stdout=StringIO(),
             )
+        log.debug('raw stdout "%s"', proc.stdout.getvalue())
         return proc.stdout.getvalue()
 
     def raw_cluster_cmd_result(self, *args):
