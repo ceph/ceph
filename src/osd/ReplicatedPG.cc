f8b6de4a1ced30f185dab78bd6b158b3076fed32
Sage Weil <sage@redhat.com>
2014-12-30 12:04:07 -0800

    osd: include registered version in watch_info_t
    Include the object version in which we were registered in the watch_info_t.
    This will later be used to tell if the registration is stable on disk.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -4328,7 +4328,8 @@ int ReplicatedPG::do_osd_ops(OpContext *ctx, vector<OSDOp>& ops)
 	  << ctx->op->get_req()->get_connection()->get_peer_addr() << dendl;
 
 	watch_info_t w(cookie, cct->_conf->osd_client_watch_timeout,
-	  ctx->op->get_req()->get_connection()->get_peer_addr());
+		       ctx->op->get_req()->get_connection()->get_peer_addr(),
+		       ctx->at_version);
 	if (op.watch.op == CEPH_OSD_WATCH_OP_WATCH ||
 	    op.watch.op == CEPH_OSD_WATCH_OP_LEGACY_WATCH) {
 	  if (oi.watchers.count(make_pair(cookie, entity))) {
