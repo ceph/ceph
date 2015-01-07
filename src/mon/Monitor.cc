e022385dd65489b9873ab838834779192c7fd27e
Jerry7X <875016668@qq.com>
2015-01-07 14:29:02 +0800

    mon: encode stashed monmap with all features
    latest_monmap that we stash is only used locally--the encoded bl is never shared. Which means we should just use CEPH_FEATURES_ALL all of the time.
    
    Fixes: #5203
    Backport: giant, firefly
    Signed-off-by: Xie Rui <875016668@qq.com>
    Reviewed-by: Sage Weil <sage@redhat.com>
    Reviewed-by: Joao Eduardo Luis <joao@redhat.com>

--- src/mon/Monitor.cc
+++ src/mon/Monitor.cc
@@ -1054,7 +1054,7 @@ void Monitor::sync_obtain_latest_monmap(bufferlist &bl)
 
   dout(1) << __func__ << " obtained monmap e" << latest_monmap.epoch << dendl;
 
-  latest_monmap.encode(bl, quorum_features);
+  latest_monmap.encode(bl, CEPH_FEATURES_ALL);
 }
 
 void Monitor::sync_reset_requester()
