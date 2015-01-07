ceph/ceph

commit 5b6977eb432438bc3d26c4bd108fe24dd2fe9f7b

librados: Fix operator= null ptr references
Fixes: #10082

dzafman-2014-11-13_10:42:58-rgw-wip-10082-testing-basic-multi

Reviewed-by: Sage Weil <sage@redhat.com>
Signed-off-by: David Zafman <dzafman@redhat.com>

src/librados/librados.cc
@@ -647,6 +647,7 @@ librados::NObjectIterator::~NObjectIterator()
 librados::NObjectIterator::NObjectIterator(const NObjectIterator &rhs)
 {
   if (rhs.impl == NULL) {
+    delete impl;
     impl = NULL;
     return;
   }
@@ -656,6 +657,11 @@ librados::NObjectIterator::NObjectIterator(const NObjectIterator &rhs)
 
 librados::NObjectIterator& librados::NObjectIterator::operator=(const librados::NObjectIterator &rhs)
 {
+  if (rhs.impl == NULL) {
+    delete impl;
+    impl = NULL;
+    return *this;
+  }
   if (impl == NULL)
     impl = new NObjectIteratorImpl();
   *impl = *(rhs.impl);
@@ -4507,6 +4513,7 @@ librados::ListObject::ListObject(librados::ListObjectImpl *i): impl(i)
 librados::ListObject::ListObject(const ListObject& rhs)
 {
   if (rhs.impl == NULL) {
+    delete impl;
     impl = NULL;
     return;
   }
@@ -4516,6 +4523,11 @@ librados::ListObject::ListObject(const ListObject& rhs)
 
 librados::ListObject& librados::ListObject::operator=(const ListObject& rhs)
 {
+  if (rhs.impl == NULL) {
+    delete impl;
+    impl = NULL;
+    return *this;
+  }
   if (impl == NULL)
     impl = new ListObjectImpl();
   *impl = *(rhs.impl);

commit f99f8484bda59364dbd702f3342fedda6f7ce479

librados/librados.cc: fix UNINIT issue
Fix for issues introduced with 5b6977eb4:

CID 1256099 (#1 of 1): Uninitialized pointer read (UNINIT)
 uninit_use: Using uninitialized value this->impl.

CID 1256100 (#1 of 1): Uninitialized pointer read (UNINIT)
 uninit_use: Using uninitialized value this->impl.

Don't delete a pointer in the constructor if the pointer
isn't initialized before.

Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

src/librados/librados.cc
@@ -668,7 +668,6 @@ librados::NObjectIterator::~NObjectIterator()
 librados::NObjectIterator::NObjectIterator(const NObjectIterator &rhs)
 {
   if (rhs.impl == NULL) {
-    delete impl;
     impl = NULL;
     return;
   }
@@ -4707,7 +4706,6 @@ librados::ListObject::ListObject(librados::ListObjectImpl *i): impl(i)
 librados::ListObject::ListObject(const ListObject& rhs)
 {
   if (rhs.impl == NULL) {
-    delete impl;
     impl = NULL;
     return;
   }