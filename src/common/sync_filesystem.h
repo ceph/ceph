c2fdbaef0206bfc0c276e50a64825efcf8cd6534
Jianpeng Ma <jianpeng.ma@intel.com>
2015-01-07 15:26:08 +0800

    common: Directly return the result of syncfs().
    In commit 808c644248e4, it will try sync() if syncfs() return error.
    No evidence prove this way can work. And sync() don't return result
    so make this function always return zero which cause filestore omit the
    error.
    
    Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

--- src/common/sync_filesystem.h
+++ src/common/sync_filesystem.h
@@ -34,12 +34,18 @@ inline int sync_filesystem(int fd)
 #ifdef HAVE_SYS_SYNCFS
   if (syncfs(fd) == 0)
     return 0;
+  else
+    return -errno;
 #elif defined(SYS_syncfs)
   if (syscall(SYS_syncfs, fd) == 0)
     return 0;
+  else
+    return -errno;
 #elif defined(__NR_syncfs)
   if (syscall(__NR_syncfs, fd) == 0)
     return 0;
+  else
+    return -errno;
 #endif
 
 #ifdef BTRFS_IOC_SYNC
