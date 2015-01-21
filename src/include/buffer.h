bf68864dda6e118f8f1bb20cc1a7e63ac2d8bd00
Jianpeng Ma <jianpeng.ma@intel.com>
2015-01-21 16:00:08 +0800

    bufferptr: add judgement in set_length() to make don't beyond the raw_length().
    Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

--- src/include/buffer.h
+++ src/include/buffer.h
@@ -237,7 +237,10 @@ public:
 
     // modifiers
     void set_offset(unsigned o) { _off = o; }
-    void set_length(unsigned l) { _len = l; }
+    void set_length(unsigned l) {
+      assert(raw_length() >= l);
+      _len = l;
+    }
 
     void append(char c);
     void append(const char *p, unsigned l);
