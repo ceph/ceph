aadec769b2255f88b70a00008ef9ef805c676494
Yan, Zheng <zyan@redhat.com>
2015-01-21 15:34:24 +0800

    mds: update client_need_snapflush when splitting snap inode
    Signed-off-by: Yan, Zheng <zyan@redhat.com>

--- src/mds/CInode.h
+++ src/mds/CInode.h
@@ -431,6 +431,7 @@ public:
 
   void add_need_snapflush(CInode *snapin, snapid_t snapid, client_t client);
   void remove_need_snapflush(CInode *snapin, snapid_t snapid, client_t client);
+  void split_need_snapflush(CInode *cowin, CInode *in);
 
 protected:
 
