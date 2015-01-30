77689f18ad1728b3d87646927eba190bb14d65c2
Samuel Just <sjust@redhat.com>
2015-01-30 12:54:47 -0800

    ReplicatedPG::C_ProxyRead: fix dropped lock
    Fixes: 10633
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -1951,8 +1951,10 @@ struct C_ProxyRead : public Context {
     if (prdop->canceled)
       return;
     pg->lock();
-    if (prdop->canceled)
+    if (prdop->canceled) {
+      pg->unlock();
       return;
+    }
     if (last_peering_reset == pg->get_last_peering_reset()) {
       pg->finish_proxy_read(oid, tid, r);
     }
