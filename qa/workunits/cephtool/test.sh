b2797973bfa87712423bb7d6104642f908542e2b
xinxin shu <xinxin.shu@intel.com>
2015-01-30 12:55:19 +0800

    add tests for ceph pg ls-by-osd
    Signed-off-by: xinxin shu <xinxin.shu@intel.com>

--- qa/workunits/cephtool/test.sh
+++ qa/workunits/cephtool/test.sh
@@ -1069,6 +1069,11 @@ function test_mon_pg()
   ceph pg ls-by-primary osd.0 active
   ceph pg ls-by-primary osd.0 active stale
   ceph pg ls-by-primary osd.0 0 active stale
+  ceph pg ls-by-osd osd.0
+  ceph pg ls-by-osd osd.0 0
+  ceph pg ls-by-osd osd.0 active
+  ceph pg ls-by-osd osd.0 active stale
+  ceph pg ls-by-osd osd.0 0 active stale
   # can't test this...
   # ceph pg force_create_pg
   ceph pg getmap -o $TMPDIR/map.$$
