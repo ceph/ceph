ceph/ceph

commit de2e5fa048639de6c9ee004a93ab295625fa3b94

rgw: finalize perfcounters after shutting down storage
Fixes: #10572
Backport: giant, firefly
First disable the storage subsystem, then disable perfcounters.

Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

src/rgw/rgw_main.cc
@@ -1198,8 +1198,6 @@ int main(int argc, const char **argv)
 
   delete olog;
 
-  rgw_perf_stop(g_ceph_context);
-
   RGWStoreManager::close_storage(store);
 
   rgw_tools_cleanup();
@@ -1210,6 +1208,8 @@ int main(int argc, const char **argv)
   g_ceph_context->put();
 
   ceph::crypto::shutdown();
+
+  rgw_perf_stop(g_ceph_context);
 
   signal_fd_finalize();
 

commit 0bab80c02f2a93a36608e9f7ff25ceaa133d7d1a

rgw: move perf cleanup before context cleanup
Fixes: #10722
Fixes: #10572
Backport: giant, firefly

This fixes a regression introduced in commit
de2e5fa048639de6c9ee004a93ab295625fa3b94.

Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

src/rgw/rgw_main.cc
@@ -1204,12 +1204,12 @@ int main(int argc, const char **argv)
   rgw_shutdown_resolver();
   curl_global_cleanup();
 
+  rgw_perf_stop(g_ceph_context);
+
   dout(1) << "final shutdown" << dendl;
   g_ceph_context->put();
 
   ceph::crypto::shutdown();
-
-  rgw_perf_stop(g_ceph_context);
 
   signal_fd_finalize();
 