9cdc3578e9000e6f0f49a9cfe62708847dc0b71a
Zack Cerza <zack@redhat.com>
2015-02-02 15:34:05 -0700

    Make 'branch' a tag in Sentry
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- teuthology/run_tasks.py
+++ teuthology/run_tasks.py
@@ -76,6 +76,8 @@ def run_tasks(tasks, ctx):
             }
             if 'teuthology_branch' in config:
                 tags['teuthology_branch'] = config['teuthology_branch']
+            if 'branch' in config:
+                tags['branch'] = config['branch']
 
             # Remove ssh keys from reported config
             if 'targets' in config:
@@ -86,7 +88,6 @@ def run_tasks(tasks, ctx):
             job_id = ctx.config.get('job_id')
             archive_path = ctx.config.get('archive_path')
             extra = dict(config=config,
-                         branch=ctx.config.get('branch'),
                          )
             if job_id:
                 extra['logs'] = get_http_log_path(archive_path, job_id)
