72a7d0c95388f9f7cfce873c6f72947aca5b81d0
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2015-02-03 21:29:01 +0100

    rgw_rest_s3.cc: prefer !empty() over size() for emptiness check
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -286,7 +286,7 @@ void RGWListBucket_ObjStore_S3::send_versioned_response()
       dump_owner(s, iter->owner, iter->owner_display_name);
       s->formatter->close_section();
     }
-    if (common_prefixes.size() > 0) {
+    if (!common_prefixes.empty()) {
       map<string, bool>::iterator pref_iter;
       for (pref_iter = common_prefixes.begin(); pref_iter != common_prefixes.end(); ++pref_iter) {
         s->formatter->open_array_section("CommonPrefixes");
