1ed2a0b1d2a0874342844c653e8b47e7b79f14fc
Samuel Just <sjust@redhat.com>
2015-02-03 09:11:44 -0800

    ReplicatedPG: block writes on degraded objects unless all peers support it
    Fixes: #10731
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/include/ceph_features.h
+++ src/include/ceph_features.h
@@ -61,6 +61,8 @@
 #define CEPH_FEATURE_MDS_QUOTA      (1ULL<<47)
 #define CEPH_FEATURE_CRUSH_V4      (1ULL<<48)  /* straw2 buckets */
 #define CEPH_FEATURE_OSD_MIN_SIZE_RECOVERY (1ULL<<49)
+// duplicated since it was introduced at the same time as MIN_SIZE_RECOVERY
+#define CEPH_FEATURE_OSD_DEGRADED_WRITES (1ULL<<49)
 
 #define CEPH_FEATURE_RESERVED2 (1ULL<<61)  /* slow down, we are almost out... */
 #define CEPH_FEATURE_RESERVED  (1ULL<<62)  /* DO NOT USE THIS ... last bit! */
@@ -146,6 +148,7 @@ static inline unsigned long long ceph_sanitize_features(unsigned long long f) {
 	 CEPH_FEATURE_MDS_QUOTA | \
          CEPH_FEATURE_CRUSH_V4 |	     \
          CEPH_FEATURE_OSD_MIN_SIZE_RECOVERY |		 \
+         CEPH_FEATURE_OSD_DEGRADED_WRITES |		 \
 	 0ULL)
 
 #define CEPH_FEATURES_SUPPORTED_DEFAULT  CEPH_FEATURES_ALL
