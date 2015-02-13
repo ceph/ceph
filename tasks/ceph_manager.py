5c1071b1039f224bb27c5b7afdc0350b61bdfbd1
John Spray <jspray@redhat.com>
2015-02-13 12:09:50 +0000

    ceph_manager: fix bad type assertions
    In python, isinstance(foo, str) will fail if
    a unicode string is passed in.  The correct check
    is basestring.
    
    Signed-off-by: John Spray <john.spray@redhat.com>

--- tasks/ceph_manager.py
+++ tasks/ceph_manager.py
@@ -1124,7 +1124,7 @@ class CephManager:
                                           erasure coded pool using the profile
         """
         with self.lock:
-            assert isinstance(pool_name, str)
+            assert isinstance(pool_name, basestring)
             assert isinstance(pg_num, int)
             assert pool_name not in self.pools
             self.log("creating pool_name %s" % (pool_name,))
@@ -1143,7 +1143,7 @@ class CephManager:
         :param pool_name: Pool to be removed
         """
         with self.lock:
-            assert isinstance(pool_name, str)
+            assert isinstance(pool_name, basestring)
             assert pool_name in self.pools
             self.log("removing pool_name %s" % (pool_name,))
             del self.pools[pool_name]
@@ -1163,7 +1163,7 @@ class CephManager:
         Return the number of pgs in the pool specified.
         """
         with self.lock:
-            assert isinstance(pool_name, str)
+            assert isinstance(pool_name, basestring)
             if pool_name in self.pools:
                 return self.pools[pool_name]
             return 0
@@ -1175,8 +1175,8 @@ class CephManager:
         :returns: property as an int value.
         """
         with self.lock:
-            assert isinstance(pool_name, str)
-            assert isinstance(prop, str)
+            assert isinstance(pool_name, basestring)
+            assert isinstance(prop, basestring)
             output = self.raw_cluster_cmd(
                 'osd',
                 'pool',
@@ -1194,8 +1194,8 @@ class CephManager:
         This routine retries if set operation fails.
         """
         with self.lock:
-            assert isinstance(pool_name, str)
-            assert isinstance(prop, str)
+            assert isinstance(pool_name, basestring)
+            assert isinstance(prop, basestring)
             assert isinstance(val, int)
             tries = 0
             while True:
@@ -1222,7 +1222,7 @@ class CephManager:
         Increase the number of pgs in a pool
         """
         with self.lock:
-            assert isinstance(pool_name, str)
+            assert isinstance(pool_name, basestring)
             assert isinstance(by, int)
             assert pool_name in self.pools
             if self.get_num_creating() > 0:
@@ -1239,7 +1239,7 @@ class CephManager:
         Set pgpnum property of pool_name pool.
         """
         with self.lock:
-            assert isinstance(pool_name, str)
+            assert isinstance(pool_name, basestring)
             assert pool_name in self.pools
             if self.get_num_creating() > 0:
                 return
