c03cd189f150e0f6b601b3322cdf2297ea76f6d8
David Zafman <dzafman@redhat.com>
2015-02-18 12:34:35 -0800

    osd: Add simple_dump() to FileJournal for unit testing
    Allow a dump when entries aren't ObjectStore::Transaction's
    
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/os/FileJournal.h
+++ src/os/FileJournal.h
@@ -297,6 +297,7 @@ private:
   void _close(int fd) const;
   void _check_disk_write_cache() const;
   int _open_file(int64_t oldsize, blksize_t blksize, bool create);
+  int _dump(ostream& out, bool simple);
   void print_header(const header_t &hdr) const;
   int read_header(header_t *hdr) const;
   bufferptr prepare_header();
@@ -411,6 +412,7 @@ private:
   int peek_fsid(uuid_d& fsid);
 
   int dump(ostream& out);
+  int simple_dump(ostream& out);
 
   void flush();
 
