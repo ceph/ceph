3530a25ae73bda2da63603373f1b3a56b63d8a00
Greg Farnum <gfarnum@redhat.com>
2015-03-02 22:29:10 -0800

    Client: do not require successful remount when unmounting
    Fixes: #10982
    
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>
    (cherry picked from commit 71f2686252ffe29d0234cc8be7304be1450d090e)

--- src/client/Client.h
+++ src/client/Client.h
@@ -421,6 +421,7 @@ protected:
   friend class C_C_Tick; // Asserts on client_lock
   friend class C_Client_SyncCommit; // Asserts on client_lock
   friend class C_Client_RequestInterrupt;
+  friend class C_Client_Remount;
 
   //int get_cache_size() { return lru.lru_get_size(); }
   //void set_cache_size(int m) { lru.lru_set_max(m); }
