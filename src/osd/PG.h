1f8bab468ac9318b42b977237da98861fc5214ed
Sage Weil <sage@redhat.com>
2015-03-04 15:16:27 -0800

    osd: pass map to write_info instead of txn
    No real change yet.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -2107,10 +2107,11 @@ public:
 		    spg_t pgid, const pg_pool_t *pool);
 
 private:
-  void write_info(ObjectStore::Transaction& t);
+  void prepare_write_info(map<string,bufferlist> *km);
 
 public:
-  static int _write_info(ObjectStore::Transaction& t, epoch_t epoch,
+  static int _prepare_write_info(map<string,bufferlist> *km,
+    epoch_t epoch,
     pg_info_t &info, coll_t coll,
     map<epoch_t,pg_interval_t> &past_intervals,
     ghobject_t &pgmeta_oid,
