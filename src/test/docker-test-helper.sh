3a8b8284ddd17ad5c6e27347433cdde6744d0737
Loic Dachary <ldachary@redhat.com>
2015-03-06 17:08:55 +0100

    doc,tests: force checkout of submodules
    When updating submodules, always checkout even if the HEAD is the
    desired commit hash (update --force) to avoid the following:
    
    * a directory gmock exists in hammer
    * a submodule gmock replaces the directory gmock in master
    * checkout master + submodule update : gmock/.git is created
    * checkout hammer : the gmock directory still contains the .git from
      master because it did not exist at the time and checkout won't
      remove untracked directories
    * checkout master + submodule update : git rev-parse HEAD is
      at the desired commit although the content of the gmock directory
      is from hammer
    
    Signed-off-by: Loic Dachary <ldachary@redhat.com>

--- src/test/docker-test-helper.sh
+++ src/test/docker-test-helper.sh
@@ -96,7 +96,7 @@ function setup_downstream() {
         cd $downstream
         git reset --hard $ref || return 1
         git submodule sync --recursive || return 1
-        git submodule update --init --recursive || return 1
+        git submodule update --force --init --recursive || return 1
     )
 }
 
