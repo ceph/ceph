53ae4323de49f5d994d8799b6708f710d09dc263
Rohan Mars <code@rohanmars.com>
2015-03-15 12:28:06 -0700

    rhel 5.9 port fixes to compile librados only Signed-off-by: Rohan Mars <code@rohanmars.com>

--- src/mon/MonCap.cc
+++ src/mon/MonCap.cc
@@ -13,10 +13,10 @@
  */
 
 #include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/qi_uint.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/fusion/include/std_pair.hpp>
 #include <boost/spirit/include/phoenix.hpp>
-#include <boost/spirit/include/qi_uint.hpp>
 #include <boost/fusion/adapted/struct/adapt_struct.hpp>
 #include <boost/fusion/include/adapt_struct.hpp>
 
