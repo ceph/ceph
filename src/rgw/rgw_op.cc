7aa1ae60cea17e0bd140c0cf2313d82f2f64554f
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2015-03-17 14:07:34 +0100

    rgw: send X-Copied-From-Last-Modified header of Swift API.
    Fixes: #10663
    Backport: hammer
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>
    (cherry picked from commit e7724a1d8c0872362c19f578fe30ac2cf3dada90)

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -2379,6 +2379,7 @@ void RGWCopyObj::execute()
                         src_obj,
                         dest_bucket_info,
                         src_bucket_info,
+                        &src_mtime,
                         &mtime,
                         mod_ptr,
                         unmod_ptr,
