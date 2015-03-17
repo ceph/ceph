ddbfbd25909225edd5839c9a71a7141998c81ece
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2015-03-17 14:06:37 +0100

    rgw: dump object metadata in response for COPY request of Swift API.
    Fixes: #10663
    Backport: hammer
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>
    (cherry picked from commit c52b75edeaeef19471b9aca772bf08055bf04031)

--- src/rgw/rgw_rest_swift.cc
+++ src/rgw/rgw_rest_swift.cc
@@ -634,14 +634,17 @@ void RGWCopyObj_ObjStore_SWIFT::dump_copy_info()
 void RGWCopyObj_ObjStore_SWIFT::send_response()
 {
   if (!sent_header) {
-   if (!ret)
+    string content_type;
+    if (!ret)
       ret = STATUS_CREATED;
     set_req_state_err(s, ret);
     dump_errno(s);
     dump_etag(s, etag.c_str());
     dump_last_modified(s, mtime);
     dump_copy_info();
-    end_header(s, this);
+    get_contype_from_attrs(attrs, content_type);
+    dump_object_metadata(s, attrs);
+    end_header(s, this, !content_type.empty() ? content_type.c_str() : "binary/octet-stream");
   } else {
     s->formatter->close_section();
     rgw_flush_formatter(s, s->formatter);
