0aaf6de606ac7a6c8a0209e458671b20b31e2e07
Jianpeng Ma <jianpeng.ma@intel.com>
2015-03-18 15:12:08 +0800

    osd/ReplicatedPG: For flush object, set the src object with fadvise sequential/nocache.
    Copy data from cache tier is copy whole data of object. So
    fadvise_sequential can make good readahead.
    From the base tier point, it don't hope left data in cache tier after
    copying, so use fadvise_nocache is good method.
    
    Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -7169,7 +7169,8 @@ int ReplicatedPG::start_flush(
 		CEPH_OSD_COPY_FROM_FLAG_IGNORE_OVERLAY |
 		CEPH_OSD_COPY_FROM_FLAG_IGNORE_CACHE |
 		CEPH_OSD_COPY_FROM_FLAG_MAP_SNAP_CLONE,
-		0);
+		LIBRADOS_OP_FLAG_FADVISE_SEQUENTIAL|LIBRADOS_OP_FLAG_FADVISE_NOCACHE);
+
     //mean the base tier don't cache data after this
     if (agent_state && agent_state->evict_mode != TierAgentState::EVICT_MODE_FULL)
       o.set_last_op_flags(LIBRADOS_OP_FLAG_FADVISE_DONTNEED);
