45638cb81a8cfd7be1aa415610e512df948f7a51
John Spray <john.spray@redhat.com>
2015-03-18 17:42:54 +0000

    mds: implement Beacon::send_and_wait
    For callers that want to send a beacon, and then
    wait up to some timeout for a response
    from the mon.  Specifically, shutdown handlers.
    
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/mds/Beacon.cc
+++ src/mds/Beacon.cc
@@ -32,7 +32,8 @@
 
 
 Beacon::Beacon(CephContext *cct_, MonClient *monc_, std::string name_) :
-  Dispatcher(cct_), lock("Beacon"), monc(monc_), timer(g_ceph_context, lock), name(name_)
+  Dispatcher(cct_), lock("Beacon"), monc(monc_), timer(g_ceph_context, lock),
+  name(name_), awaiting_seq(-1)
 {
   want_state = MDSMap::STATE_NULL;
   last_seq = 0;
@@ -131,6 +132,11 @@ void Beacon::handle_mds_beacon(MMDSBeacon *m)
     while (!seq_stamp.empty() &&
 	   seq_stamp.begin()->first <= seq)
       seq_stamp.erase(seq_stamp.begin());
+
+    // Wake a waiter up if present
+    if (awaiting_seq == seq) {
+      waiting_cond.Signal();
+    }
   } else {
     dout(10) << "handle_mds_beacon " << ceph_mds_state_name(m->get_state())
 	     << " seq " << m->get_seq() << " dne" << dendl;
@@ -142,6 +148,25 @@ void Beacon::send()
 {
   Mutex::Locker l(lock);
   _send();
+}
+
+
+void Beacon::send_and_wait(const double duration)
+{
+  Mutex::Locker l(lock);
+  _send();
+  awaiting_seq = last_seq;
+  dout(20) << __func__ << ": awaiting " << awaiting_seq
+           << " for up to " << duration << "s" << dendl;
+
+  utime_t timeout;
+  timeout.set_from_double(ceph_clock_now(cct) + duration);
+  while ((!seq_stamp.empty() && seq_stamp.begin()->first <= awaiting_seq)
+         && ceph_clock_now(cct) < timeout) {
+    waiting_cond.WaitUntil(lock, timeout);
+  }
+
+  awaiting_seq = -1;
 }
 
 
