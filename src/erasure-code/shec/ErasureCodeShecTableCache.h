6c02dcca78b2ea90fe3db16209f38bb0ffb64762
Takeshi Miyamae <miyamae.takeshi@jp.fujitsu.com>
2015-03-20 16:55:01 +0900

    erasure code: shec performance optimization by decoding cache
    Optimize shec's cpu utilization by caching decoding matrix
    
    Signed-off-by: Takeshi Miyamae <miyamae.takeshi@jp.fujitsu.com>

--- src/erasure-code/shec/ErasureCodeShecTableCache.h
+++ src/erasure-code/shec/ErasureCodeShecTableCache.h
@@ -25,17 +25,45 @@
 #include "erasure-code/ErasureCodeInterface.h"
 // -----------------------------------------------------------------------------
 #include <list>
+#include <stdint.h>
 // -----------------------------------------------------------------------------
 
 class ErasureCodeShecTableCache {
+  class DecodingCacheParameter {
+  public:
+    int* decoding_matrix; // size: k*k
+    int* dm_ids; // size: k
+    int* minimum; // size: k+m
+    DecodingCacheParameter(){
+      decoding_matrix = 0;
+      dm_ids = 0;
+      minimum = 0;
+    }
+    ~DecodingCacheParameter(){
+      if (decoding_matrix){
+	delete[] decoding_matrix;
+      }
+      if (dm_ids){
+	delete[] dm_ids;
+      }
+      if (minimum){
+	delete[] minimum;
+      }
+    }
+  };
+  
   // ---------------------------------------------------------------------------
   // This class implements a table cache for encoding and decoding matrices.
   // Encoding matrices are shared for the same (k,m,c,w) combination.
+  // It supplies a decoding matrix lru cache which is shared for identical
+  // matrix types e.g. there is one cache (lru-list + lru-map)
   // ---------------------------------------------------------------------------
 
  public:
 
-  typedef std::pair<std::list<std::string>::iterator, bufferptr> lru_entry_t;
+  static const int decoding_tables_lru_length = 2516;
+
+  typedef std::pair<std::list<uint64_t>::iterator, DecodingCacheParameter> lru_entry_t;
   typedef std::map< int, int** > codec_table_t;
   typedef std::map< int, codec_table_t > codec_tables_t__;
   typedef std::map< int, codec_tables_t__ > codec_tables_t_;
@@ -43,6 +71,9 @@ class ErasureCodeShecTableCache {
   typedef std::map< int, codec_tables_t > codec_technique_tables_t;
   // int** matrix = codec_technique_tables_t[technique][k][m][c][w]
   
+  typedef std::map< uint64_t, lru_entry_t > lru_map_t;
+  typedef std::list< uint64_t > lru_list_t;
+
  ErasureCodeShecTableCache() :
   codec_tables_guard("shec-lru-cache")
     {
@@ -52,13 +83,44 @@ class ErasureCodeShecTableCache {
   
   Mutex codec_tables_guard; // mutex used to protect modifications in encoding/decoding table maps
   
+  bool getDecodingTableFromCache(int* matrix,
+				 int* dm_ids,
+				 int* minimum,
+                                 int technique,
+                                 int k,
+                                 int m,
+				 int c,
+				 int w,
+				 int* erased,
+				 int* avails);
+
+  void putDecodingTableToCache(int* matrix,
+			       int* dm_ids,
+			       int* minimum,
+                               int technique,
+                               int k,
+                               int m,
+			       int c,
+			       int w,
+			       int* erased,
+			       int* avails);
+
   int** getEncodingTable(int technique, int k, int m, int c, int w);
   int** getEncodingTableNoLock(int technique, int k, int m, int c, int w);
   int* setEncodingTable(int technique, int k, int m, int c, int w, int*);
   
+  int getDecodingTableCacheSize(int technique);
+
  private:
-  codec_technique_tables_t encoding_table; // encoding coefficients accessed via table[technique][k][m]
-  
+  codec_technique_tables_t encoding_table; // encoding table accessed via table[matrix][k][m][c][w]
+  std::map<int, lru_map_t*> decoding_tables; // decoding table cache accessed via map[matrixtype]
+  std::map<int, lru_list_t*> decoding_tables_lru; // decoding table lru list accessed via list[matrixtype]
+
+  lru_map_t* getDecodingTables(int technique);
+  lru_list_t* getDecodingTablesLru(int technique);
+  uint64_t getDecodingCacheSignature(int k, int m, int c, int w,
+					       int *erased, int *avails);
+
   Mutex* getLock();
   
 };
