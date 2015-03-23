b84943cb459ae10f1b76f2024d4766165c3b084c
Samuel Just <sjust@redhat.com>
2015-03-23 11:50:10 -0700

    Revert "osd/: don't block writes on degraded objects"
    This reverts commit f9abffbb2461b5988c0435ee2f7771eca028f29f.

--- src/osd/ReplicatedPG.cc
+++ src/osd/ReplicatedPG.cc
@@ -1440,6 +1440,12 @@ void ReplicatedPG::do_op(OpRequestRef& op)
     return;
   }
 
+  // degraded object?
+  if (write_ordered && is_degraded_object(head)) {
+    wait_for_degraded_object(head, op);
+    return;
+  }
+
   // missing snapdir?
   hobject_t snapdir(m->get_oid(), m->get_object_locator().key,
 		    CEPH_SNAPDIR, m->get_pg().ps(), info.pgid.pool(),
@@ -8487,6 +8493,9 @@ void ReplicatedBackend::sub_op_modify_impl(OpRequestRef op)
   // sanity checks
   assert(m->map_epoch >= get_info().history.same_interval_since);
   
+  // we better not be missing this.
+  assert(!parent->get_log().get_missing().is_missing(soid));
+
   int ackerosd = m->get_source().num();
   
   op->mark_started();
