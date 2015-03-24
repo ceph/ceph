b2311597f1f73ef4e5d92ab92ec8866f678b8dbf
Zhiqiang Wang <zhiqiang.wang@intel.com>
2015-03-24 11:36:49 +0800

    osd: avoid inserting an op into hit set multiple times
    When an op is enqueued multiple times, it is inserted into the hit set
    every time. Only need to insert once.
    
    Signed-off-by: Zhiqiang Wang <zhiqiang.wang@intel.com>

--- src/osd/OpRequest.h
+++ src/osd/OpRequest.h
@@ -105,6 +105,7 @@ public:
   }
   bool send_map_update;
   epoch_t sent_epoch;
+  bool hitset_inserted;
   Message *get_req() const { return request; }
   bool been_queued_for_pg() { return hit_flag_points & flag_queued_for_pg; }
   bool been_reached_pg() { return hit_flag_points & flag_reached_pg; }
