db47d89a46068ad15be6842925383e93546b4ed0
Dmitry Yatsushkevich <dyatsushkevich@mirantis.com>
2015-03-25 13:42:06 -0700

    rbd cli: remove erroneous arg for ceph_argparse_witharg
    Remove erroneous arg for ceph_argparse_witharg call when '--io-pattern' parsed:
    the name look up will point the compiler to bool ceph_argparse_witharg(
        std::vector<const char*> &args,
        std::vector<const char*>::iterator &i, std::string *ret, ...) when compiler
    is resolving this function call. The &err argument will be wrongly interpreted
    as a char * variable to be compared with the argument name pointed by i.
    
    Signed-off-by: Dmitry Yatsushkevich <dyatsushkevich@mirantis.com>

--- src/rbd.cc
+++ src/rbd.cc
@@ -2752,7 +2752,7 @@ int main(int argc, const char **argv)
       }
     } else if (ceph_argparse_withlonglong(args, i, &bench_io_threads, &err, "--io-threads", (char*)NULL)) {
     } else if (ceph_argparse_withlonglong(args, i, &bench_bytes, &err, "--io-total", (char*)NULL)) {
-    } else if (ceph_argparse_witharg(args, i, &bench_pattern, &err, "--io-pattern", (char*)NULL)) {
+    } else if (ceph_argparse_witharg(args, i, &bench_pattern, "--io-pattern", (char*)NULL)) {
     } else if (ceph_argparse_witharg(args, i, &val, "--path", (char*)NULL)) {
       path = strdup(val.c_str());
     } else if (ceph_argparse_witharg(args, i, &val, "--dest", (char*)NULL)) {
