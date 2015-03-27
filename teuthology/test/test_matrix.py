05e6f146a1216f46cb34e0c59699576add192b26
Samuel Just <sjust@redhat.com>
2015-03-27 10:20:28 -0700

    suite: add --subset option to allow us to schedule subsets of a suite
    First, the old logic tended to do the following:
    
    [0,1,2,3] x [a,b,c,d] -> [(0, a), (0, b), (0, c), ...]
    
    which is a bummer since if we only schedule the first 1/4,
    we only get combinations with 0.
    
    Instead, we want:
    
    [0,1,2,3] x [a,b,c,d] -> [(0, a), (1, b), (2, c), ...]
    
    Second, the old logic tended to do the following:
    
    [0,1,2,3] + [a,b,c,d] -> [0,1,2,3,a,b,c,d]
    
    Which is a bummer since we need to all the way through the first
    set before we get examples from the second.
    
    Instead, we want:
    
    [0,1,2,3] + [a,b,c,d] -> [0,a,1,b,2,c,3,d]
    
    Lastly, if we have:
    
    [0,1,2] + ([0,1,2],[a,b,c])
    
    and want to schedule 1/3 of the suite, we don't want to
    just schedule 0, we probably want to schedule all of
    [0,1,2] and 1/3 of the second combo.  Thus, we want to
    detect such cases and multiply out [0,1,2] into
    [0,1,2,0,1,2,0,1,2].
    
    Signed-off-by: Samuel Just <sam.just@inktank.com>
    (cherry picked from commit 9a57759ec3df7a8b9490920538214ac189418c49)

---
+++ teuthology/test/test_matrix.py
@@ -0,0 +1,71 @@
+from .. import matrix
+
+def verify_matrix_output_diversity(res):
+    """
+    Verifies that the size of the matrix passed matches the number of unique
+    outputs from res.index
+    """
+    sz = res.size()
+    s = frozenset([matrix.generate_lists(res.index(i)) for i in range(sz)])
+    for i in range(res.size()):
+        assert sz == len(s)
+
+def mbs(num, l):
+    return matrix.Sum(num*10, [matrix.Base(i + (100*num)) for i in l])
+
+class TestMatrix(object):
+    def test_simple(self):
+        verify_matrix_output_diversity(mbs(1, range(6)))
+
+    def test_simple2(self):
+        verify_matrix_output_diversity(mbs(1, range(5)))
+
+    # The test_product* tests differ by the degree by which dimension
+    # sizes share prime factors
+    def test_product_simple(self):
+        verify_matrix_output_diversity(
+            matrix.Product(1, [mbs(1, range(6)), mbs(2, range(2))]))
+
+    def test_product_3_facets_2_prime_factors(self):
+        verify_matrix_output_diversity(matrix.Product(1, [
+                    mbs(1, range(6)),
+                    mbs(2, range(2)),
+                    mbs(3, range(3)),
+                    ]))
+
+    def test_product_3_facets_2_prime_factors_one_larger(self):
+        verify_matrix_output_diversity(matrix.Product(1, [
+                    mbs(1, range(2)),
+                    mbs(2, range(5)),
+                    mbs(4, range(4)),
+                    ]))
+
+    def test_product_4_facets_2_prime_factors(self):
+        verify_matrix_output_diversity(matrix.Sum(1, [
+                    mbs(1, range(6)),
+                    mbs(3, range(3)),
+                    mbs(2, range(2)),
+                    mbs(4, range(9)),
+                    ]))
+
+    def test_product_2_facets_2_prime_factors(self):
+        verify_matrix_output_diversity(matrix.Sum(1, [
+                    mbs(1, range(2)),
+                    mbs(2, range(5)),
+                    ]))
+
+    def test_product_with_sum(self):
+        verify_matrix_output_diversity(matrix.Sum(
+                9,
+                [
+                    mbs(10, range(6)),
+                    matrix.Product(1, [
+                            mbs(1, range(2)),
+                            mbs(2, range(5)),
+                            mbs(4, range(4))]),
+                    matrix.Product(8, [
+                            mbs(7, range(2)),
+                            mbs(6, range(5)),
+                            mbs(5, range(4))])
+                    ]
+                ))
