c1b7bd250063cb3e00dc6aed93572126808f6c95
Sage Weil <sage@redhat.com>
2015-03-27 10:04:00 -0700

    osd: fix condition for loggin scrub errors
    We may have an error to log even though the authoritative map is empty:
    when we have a digest that doesn't match all replicas.
    
    Signed-off-by: Sage Weil <sage@redhat.com>
    (cherry picked from commit d23766be902080ffb29528acb5a00d70beb97a9f)

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -4210,7 +4210,7 @@ void PG::scrub_compare_maps()
       ss);
     dout(2) << ss.str() << dendl;
 
-    if (!authoritative.empty()) {
+    if (!ss.str().empty()) {
       osd->clog->error(ss);
     }
 
