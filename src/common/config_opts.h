feb587175b3cc78a94c35a28111fce12dd00569f
Xiaoxi Chen <xiaoxi.chen@intel.com>
2015-03-30 19:56:59 +0800

    Add rbd_skip_partial_discard flag
    If we want to discard a range of an object, we will zero(use fallocate
    to punch a hole) the range now. In general this introduce some overhead(extra writes).
    
    If the filesystem ontop of RBD holding lots of small files, this
    behavior will bring big performance penalty.
    
    Adding a flag that allow user to control if they want to zero the
    range.
    
    Signed-off-by: Xiaoxi Chen <xiaoxi.chen@intel.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -892,6 +892,7 @@ OPTION(rbd_clone_copy_on_read, OPT_BOOL, false)
 OPTION(rbd_blacklist_on_break_lock, OPT_BOOL, true) // whether to blacklist clients whose lock was broken
 OPTION(rbd_blacklist_expire_seconds, OPT_INT, 0) // number of seconds to blacklist - set to 0 for OSD default
 OPTION(rbd_request_timed_out_seconds, OPT_INT, 30) // number of seconds before maint request times out
+OPTION(rbd_skip_partial_discard, OPT_BOOL, false) // when trying to discard a range inside an object, set to true to skip zeroing the range.
 
 /*
  * The following options change the behavior for librbd's image creation methods that
