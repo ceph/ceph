2a93dba9711fce518c7ef5192b797b9cbea21082
Sage Weil <sage@redhat.com>
2015-03-31 17:32:36 -0700

    os/ObjectStore: pass bits to create_collection
    This informs the backend how many bits of the hash range will be covered
    by the collection.  Those bits are implicitly matched against the spg_t in
    coll_t.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/tools/ceph_objectstore_tool.cc
+++ src/tools/ceph_objectstore_tool.cc
@@ -1343,7 +1343,8 @@ int ObjectStoreTool::do_import(ObjectStore *store, OSDSuperblock& sb,
 
   if (!dry_run) {
     ObjectStore::Transaction *t = new ObjectStore::Transaction;
-    PG::_create(*t, pgid);
+    PG::_create(*t, pgid,
+		pgid.get_split_bits(curmap.get_pg_pool(pgid.pool())->get_pg_num()));
     PG::_init(*t, pgid, NULL);
 
     // mark this coll for removal until we're done
