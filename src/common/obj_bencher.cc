a87ac4d36b86aa826afb990769831bfcf08ed3fa
Dmitry Yatsushkevich <dyatsushkevich@mirantis.com>
2015-04-02 11:21:05 -0700

    obj_bencher: aio_bench - rename op_size to object_size
    Rename aio_bench argument 'op_size' to 'object_size' to reflect the reality of
    how it is used.
    
    Signed-off-by: Dmitry Yatsushkevich <dyatsushkevich@mirantis.com>

--- src/common/obj_bencher.cc
+++ src/common/obj_bencher.cc
@@ -170,12 +170,11 @@ void *ObjBencher::status_printer(void *_bencher) {
 int ObjBencher::aio_bench(
   int operation, int secondsToRun,
   int maxObjectsToCreate,
-  int concurrentios, int op_size, bool cleanup, const char* run_name) {
+  int concurrentios, int object_size, bool cleanup, const char* run_name) {
 
   if (concurrentios <= 0) 
     return -EINVAL;
 
-  int object_size = op_size;
   int num_objects = 0;
   int r = 0;
   int prevPid = 0;
