5e9c64b4dd6b5fb9cab966a0e4ac7434804c3acc
Xiaoxi Chen <xiaoxi.chen@intel.com>
2015-04-08 23:35:50 +0800

    Implement get_omap_iterator
    implemented get_omap_iterator
    
    Signed-off-by: Xiaoxi Chen <xiaoxi.chen@intel.com>

--- src/os/newstore/NewStore.h
+++ src/os/newstore/NewStore.h
@@ -117,7 +117,7 @@ public:
     KeyValueDB::Iterator it;
     string head, tail;
   public:
-    OmapIteratorImpl(CollectionRef c, OnodeRef o);
+    OmapIteratorImpl(CollectionRef c, OnodeRef o, KeyValueDB::Iterator it);
     int seek_to_first();
     int upper_bound(const string &after);
     int lower_bound(const string &to);
