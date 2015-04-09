5f246a6196062d82c7d8fc3f0ab9abea5775b205
Jianpeng Ma <jianpeng.ma@intel.com>
2015-04-09 11:39:08 +0800

    common: make rados bench return correctly errno.
    Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>
    (cherry picked from commit e3d62a9d4c6da7bd48f7aefce91db050bdc9a637)

--- src/common/obj_bencher.cc
+++ src/common/obj_bencher.cc
@@ -506,7 +506,7 @@ int ObjBencher::write_bench(int secondsToRun, int maxObjectsToCreate,
   lock.Unlock();
   pthread_join(print_thread, NULL);
   delete newContents;
-  return -5;
+  return r;
 }
 
 int ObjBencher::seq_read_bench(int seconds_to_run, int num_objects, int concurrentios, int pid) {
@@ -695,7 +695,7 @@ int ObjBencher::seq_read_bench(int seconds_to_run, int num_objects, int concurre
   data.done = 1;
   lock.Unlock();
   pthread_join(print_thread, NULL);
-  return -5;
+  return r;
 }
 
 int ObjBencher::rand_read_bench(int seconds_to_run, int num_objects, int concurrentios, int pid)
@@ -888,7 +888,7 @@ int ObjBencher::rand_read_bench(int seconds_to_run, int num_objects, int concurr
   data.done = 1;
   lock.Unlock();
   pthread_join(print_thread, NULL);
-  return -5;
+  return r;
 }
 
 int ObjBencher::clean_up(const char* prefix, int concurrentios, const char* run_name) {
@@ -1045,7 +1045,7 @@ int ObjBencher::clean_up(int num_objects, int prevPid, int concurrentios) {
   lock.Lock();
   data.done = 1;
   lock.Unlock();
-  return -5;
+  return r;
 }
 
 /**
