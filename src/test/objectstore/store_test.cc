8112dc7a1f8ff8e2cf1fe89e5f632ee8ea81b4d4
Sage Weil <sage@redhat.com>
2015-04-10 17:08:06 -0700

    ceph_test_objectstore: drop useless sync_and_flush
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/test/objectstore/store_test.cc
+++ src/test/objectstore/store_test.cc
@@ -2182,7 +2182,6 @@ TEST_P(StoreTest, MoveRename) {
 
 TEST_P(StoreTest, BigRGWObjectName) {
   store->set_allow_sharded_objects();
-  store->sync_and_flush();
   coll_t cid(spg_t(pg_t(0,12),shard_id_t::NO_SHARD));
   ghobject_t oid(
     hobject_t(
