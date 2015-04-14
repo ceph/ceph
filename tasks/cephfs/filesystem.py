tchaikov/ceph-qa-suite

tasks/cephfs: better multiple-mds handling
Signed-off-by: John Spray <john.spray@redhat.com>

tasks/cephfs/filesystem.py
@@ -140,16 +140,22 @@ class Filesystem(object):
 
     def are_daemons_healthy(self):
         """
-        Return true if all daemons are in one of active, standby, standby-replay
+        Return true if all daemons are in one of active, standby, standby-replay, and
+        at least max_mds daemons are in 'active'.
+
         :return:
         """
+
+        active_count = 0
         status = self.mon_manager.get_mds_status_all()
         for mds_id, mds_status in status['info'].items():
             if mds_status['state'] not in ["up:active", "up:standby", "up:standby-replay"]:
                 log.warning("Unhealthy mds state {0}:{1}".format(mds_id, mds_status['state']))
                 return False
+            elif mds_status['state'] == 'up:active':
+                active_count += 1
 
-        return True
+        return active_count >= status['max_mds']
 
     def get_active_names(self):
         """
@@ -165,6 +171,22 @@ class Filesystem(object):
                 result.append(mds_status['name'])
 
         return result
+
+    def get_rank_names(self):
+        """
+        Return MDS daemon names of those daemons holding a rank,
+        sorted by rank.  This includes e.g. up:replay/reconnect
+        as well as active, but does not include standby or
+        standby-replay.
+        """
+        status = self.mon_manager.get_mds_status_all()
+        result = []
+        for mds_status in sorted(status['info'].values(), lambda a, b: cmp(a['rank'], b['rank'])):
+            if mds_status['rank'] != -1 and mds_status['state'] != 'up:standby-replay':
+                result.append(mds_status['name'])
+
+        return result
+
 
     def wait_for_daemons(self, timeout=None):
         """
@@ -187,10 +209,15 @@ class Filesystem(object):
                 raise RuntimeError("Timed out waiting for MDS daemons to become healthy")
 
     def get_lone_mds_id(self):
+        """
+        Get a single MDS ID: the only one if there is only one
+        configured, else the only one currently holding a rank,
+        else raise an error.
+        """
         if len(self.mds_ids) != 1:
-            active = self.get_active_names()
-            if len(active) == 1:
-                return active[0]
+            alive = self.get_rank_names()
+            if len(alive) == 1:
+                return alive[0]
             else:
                 raise ValueError("Explicit MDS argument required when multiple MDSs in use")
         else:
@@ -365,20 +392,34 @@ class Filesystem(object):
         Block until the MDS reaches a particular state, or a failure condition
         is met.
 
+        When there are multiple MDSs, succeed when exaclty one MDS is in the
+        goal state, or fail when any MDS is in the reject state.
+
         :param goal_state: Return once the MDS is in this state
         :param reject: Fail if the MDS enters this state before the goal state
         :param timeout: Fail if this many seconds pass before reaching goal
         :return: number of seconds waited, rounded down to integer
         """
 
-        if mds_id is None:
-            mds_id = self.get_lone_mds_id()
-
         elapsed = 0
         while True:
-            # mds_info is None if no daemon currently claims this rank
-            mds_info = self.mon_manager.get_mds_status(mds_id)
-            current_state = mds_info['state'] if mds_info else None
+
+            if mds_id is not None:
+                # mds_info is None if no daemon with this ID exists in the map
+                mds_info = self.mon_manager.get_mds_status(mds_id)
+                current_state = mds_info['state'] if mds_info else None
+                log.info("Looked up MDS state for {0}: {1}".format(mds_id, current_state))
+            else:
+                # In general, look for a single MDS
+                mds_status = self.mon_manager.get_mds_status_all()
+                states = [mds_status['state'] for mds_status in mds_status['info'].values()]
+                if [s for s in states if s == goal_state] == [goal_state]:
+                    current_state = goal_state
+                elif reject in states:
+                    current_state = reject
+                else:
+                    current_state = None
+                log.info("mapped states {0} to {1}".format(states, current_state))
 
             if current_state == goal_state:
                 log.info("reached state '{0}' in {1}s".format(current_state, elapsed))
@@ -386,6 +427,7 @@ class Filesystem(object):
             elif reject is not None and current_state == reject:
                 raise RuntimeError("MDS in reject state {0}".format(current_state))
             elif timeout is not None and elapsed > timeout:
+                log.error("MDS status at timeout: {0}".format(self.mon_manager.get_mds_status_all()))
                 raise RuntimeError(
                     "Reached timeout after {0} seconds waiting for state {1}, while in state {2}".format(
                         elapsed, goal_state, current_state

ceph/ceph

tasks/cephfs/filesystem.py
@@ -140,16 +140,22 @@ class Filesystem(object):
 
     def are_daemons_healthy(self):
         """
-        Return true if all daemons are in one of active, standby, standby-replay
+        Return true if all daemons are in one of active, standby, standby-replay, and
+        at least max_mds daemons are in 'active'.
+
         :return:
         """
+
+        active_count = 0
         status = self.mon_manager.get_mds_status_all()
         for mds_id, mds_status in status['info'].items():
             if mds_status['state'] not in ["up:active", "up:standby", "up:standby-replay"]:
                 log.warning("Unhealthy mds state {0}:{1}".format(mds_id, mds_status['state']))
                 return False
+            elif mds_status['state'] == 'up:active':
+                active_count += 1
 
-        return True
+        return active_count >= status['max_mds']
 
     def get_active_names(self):
         """
@@ -165,6 +171,22 @@ class Filesystem(object):
                 result.append(mds_status['name'])
 
         return result
+
+    def get_rank_names(self):
+        """
+        Return MDS daemon names of those daemons holding a rank,
+        sorted by rank.  This includes e.g. up:replay/reconnect
+        as well as active, but does not include standby or
+        standby-replay.
+        """
+        status = self.mon_manager.get_mds_status_all()
+        result = []
+        for mds_status in sorted(status['info'].values(), lambda a, b: cmp(a['rank'], b['rank'])):
+            if mds_status['rank'] != -1 and mds_status['state'] != 'up:standby-replay':
+                result.append(mds_status['name'])
+
+        return result
+
 
     def wait_for_daemons(self, timeout=None):
         """
@@ -187,10 +209,15 @@ class Filesystem(object):
                 raise RuntimeError("Timed out waiting for MDS daemons to become healthy")
 
     def get_lone_mds_id(self):
+        """
+        Get a single MDS ID: the only one if there is only one
+        configured, else the only one currently holding a rank,
+        else raise an error.
+        """
         if len(self.mds_ids) != 1:
-            active = self.get_active_names()
-            if len(active) == 1:
-                return active[0]
+            alive = self.get_rank_names()
+            if len(alive) == 1:
+                return alive[0]
             else:
                 raise ValueError("Explicit MDS argument required when multiple MDSs in use")
         else:
@@ -375,20 +402,34 @@ class Filesystem(object):
         Block until the MDS reaches a particular state, or a failure condition
         is met.
 
+        When there are multiple MDSs, succeed when exaclty one MDS is in the
+        goal state, or fail when any MDS is in the reject state.
+
         :param goal_state: Return once the MDS is in this state
         :param reject: Fail if the MDS enters this state before the goal state
         :param timeout: Fail if this many seconds pass before reaching goal
         :return: number of seconds waited, rounded down to integer
         """
 
-        if mds_id is None:
-            mds_id = self.get_lone_mds_id()
-
         elapsed = 0
         while True:
-            # mds_info is None if no daemon currently claims this rank
-            mds_info = self.mon_manager.get_mds_status(mds_id)
-            current_state = mds_info['state'] if mds_info else None
+
+            if mds_id is not None:
+                # mds_info is None if no daemon with this ID exists in the map
+                mds_info = self.mon_manager.get_mds_status(mds_id)
+                current_state = mds_info['state'] if mds_info else None
+                log.info("Looked up MDS state for {0}: {1}".format(mds_id, current_state))
+            else:
+                # In general, look for a single MDS
+                mds_status = self.mon_manager.get_mds_status_all()
+                states = [m['state'] for m in mds_status['info'].values()]
+                if [s for s in states if s == goal_state] == [goal_state]:
+                    current_state = goal_state
+                elif reject in states:
+                    current_state = reject
+                else:
+                    current_state = None
+                log.info("mapped states {0} to {1}".format(states, current_state))
 
             if current_state == goal_state:
                 log.info("reached state '{0}' in {1}s".format(current_state, elapsed))
@@ -396,6 +437,7 @@ class Filesystem(object):
             elif reject is not None and current_state == reject:
                 raise RuntimeError("MDS in reject state {0}".format(current_state))
             elif timeout is not None and elapsed > timeout:
+                log.error("MDS status at timeout: {0}".format(self.mon_manager.get_mds_status_all()))
                 raise RuntimeError(
                     "Reached timeout after {0} seconds waiting for state {1}, while in state {2}".format(
                         elapsed, goal_state, current_state