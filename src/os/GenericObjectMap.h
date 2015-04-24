715a16814956463d105138cfd3024c21e858c56b
Xiaoxi Chen <xiaoxi.chen@intel.com>
2015-04-24 22:37:32 +0800

    os/KeyValuestore:Refactor collection_list_range and collection_list_partial
    Add collection_list_impl which abstract the common process
    of collection list behavior.
    
    Signed-off-by: Xiaoxi Chen <xiaoxi.chen@intel.com>

--- src/os/GenericObjectMap.h
+++ src/os/GenericObjectMap.h
@@ -117,7 +117,7 @@ class GenericObjectMap {
   bool check(std::ostream &out);
 
   /// Util, list all objects, there must be no other concurrent access
-  int list_objects(const coll_t &cid, ghobject_t start, int max,
+  int list_objects(const coll_t &cid, ghobject_t start, ghobject_t end, int max,
                    vector<ghobject_t> *objs, ///< [out] objects
                    ghobject_t *next);
 
