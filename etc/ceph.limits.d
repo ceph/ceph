c09422a1b05e921fa9642fc1f913f31d54f21d31
Sage Weil <sage@redhat.com>
2015-04-24 15:26:39 -0700

    set nofile ulimit in /etc/security/limits.d/ceph only
    Specify the nofile ulimit in one standard place, where everyone expects it
    to be.  Drop it from the ceph-osd unit file.
    
    Leave upstart and sysvinit untouched for the time being to avoid compat
    issues.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

---
+++ etc/ceph.limits.d
@@ -0,0 +1,10 @@
+# /etc/security/limit.d/ceph
+#
+#<domain>        <type>  <item>  <value>
+#
+
+# We want a very large value for nofile for the ceph user as the ceph
+# clients and daemons consume lots and lots of file descriptors.
+
+ceph	  -	      nofile  4194304
+
