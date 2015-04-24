b3723bab58b7735b5f2ead2043ebe7da565e5e1a
Xiaoxi Chen <xiaoxi.chen@intel.com>
2015-04-24 15:32:36 +0800

    os/Filestore:Refactor collection_list_range and collection_list_partial
    Add collection_list_impl which abstract the common process
    of coleection list behavior.
    
    Todo: Refactor Index::collection_list_partial as
    well.
    
    Signed-off-by: Xiaoxi Chen <xiaoxi.chen@intel.com>

--- src/os/LFNIndex.cc
+++ src/os/LFNIndex.cc
@@ -155,13 +155,13 @@ int LFNIndex::pre_hash_collection(uint32_t pg_num, uint64_t expected_num_objs)
 
 
 int LFNIndex::collection_list_partial(const ghobject_t &start,
-				      int min_count,
+				      const ghobject_t end,
 				      int max_count,
 				      snapid_t seq,
 				      vector<ghobject_t> *ls,
 				      ghobject_t *next)
 {
-  return _collection_list_partial(start, min_count, max_count, seq, ls, next);
+  return _collection_list_partial(start, end, max_count, seq, ls, next);
 }
 
 /* Derived class utility methods */
