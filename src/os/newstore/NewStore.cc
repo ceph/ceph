87d7320844bcaf1086aed407d20070fb05324df8
Zhiqiang Wang <zhiqiang.wang@intel.com>
2015-04-27 16:27:21 +0800

    osd/NewStore: fix for skipping the overlay in _do_overlay_trim
    When the offset of the write starts at the end of the overlay, that is,
    p->first + p->second.length == offset, the overlay could be skipped as
    well.
    
    Signed-off-by: Zhiqiang Wang <zhiqiang.wang@intel.com>

--- src/os/newstore/NewStore.cc
+++ src/os/newstore/NewStore.cc
@@ -3058,7 +3058,7 @@ int NewStore::_do_overlay_trim(TransContext *txc,
 	       << dendl;
       break;
     }
-    if (p->first + p->second.length < offset) {
+    if (p->first + p->second.length <= offset) {
       dout(20) << __func__ << " skip " << p->first << " " << p->second
 	       << dendl;
       ++p;
