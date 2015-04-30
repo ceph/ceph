ed4116d091a92ba894d18cb6a9605bac9a140f4d
Yazen Ghannam <yazen.ghannam@linaro.org>
2015-04-29 23:46:37 -0400

    Workunits : suites/blogbench.sh : Get latest config.guess.
    Signed-off-by: Yazen Ghannam <yazen.ghannam@linaro.org>

--- qa/workunits/suites/blogbench.sh
+++ qa/workunits/suites/blogbench.sh
@@ -6,6 +6,7 @@ wget http://ceph.com/qa/blogbench-1.0.tar.bz2
 #cp /home/gregf/src/blogbench-1.0.tar.bz2 .
 tar -xvf blogbench-1.0.tar.bz2
 cd blogbench*
+wget -O config.guess "http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD"
 echo "making blogbench"
 ./configure
 make
