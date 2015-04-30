748ea57cfeca7b295afca5b8126d454eb85550c7
Yehuda Sadeh <yehuda@redhat.com>
2015-04-29 17:12:34 -0700

    rgw: iterate over linked objects, store them
    only keep part of the oid name if it is in a namespace.
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>
    (cherry picked from commit 75902fdb12ce0413d3beb455df47134f3d7386e7)

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -5010,6 +5010,7 @@ int RGWRados::Object::Stat::stat_async()
   RGWRados *store = source->get_store();
 
   RGWObjState *s = ctx.get_state(obj); /* calling this one directly because otherwise a sync request will be sent */
+  result.obj = obj;
   if (s->has_attrs) {
     state.ret = 0;
     result.size = s->size;
@@ -5074,6 +5075,7 @@ int RGWRados::Object::Stat::finish()
       ldout(store->ctx(), 0) << "ERROR: " << __func__ << ": failed to decode manifest"  << dendl;
       return -EIO;
     }
+    result.has_manifest = true;
   }
 
   return 0;
