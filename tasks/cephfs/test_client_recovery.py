9353d7a8788178f118c3f47ca5038763aa8e7977
Greg Farnum <gfarnum@redhat.com>
2015-04-30 18:15:05 -0700

    cephfs/test_client_recovery: check FUSE version before invoking flock
    flock only works properly on FUSE versions >=2.9, which is newer
    than eg Ubuntu Precise. So check the version on our client mounts and
    only test flock if it's at least that new.
    
    Fixes: #9995
    
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>

--- tasks/cephfs/test_client_recovery.py
+++ tasks/cephfs/test_client_recovery.py
@@ -5,9 +5,12 @@ Teuthology task for exercising CephFS client recovery
 
 import logging
 import time
+import distutils.version as version
+import re
 
 from teuthology.orchestra.run import CommandFailedError, ConnectionLostError
 from tasks.cephfs.cephfs_test_case import CephFSTestCase
+from teuthology.packaging import get_package_version
 
 
 log = logging.getLogger(__name__)
@@ -307,19 +310,41 @@ class TestClientRecovery(CephFSTestCase):
         self.assertLess(recovery_time, self.ms_max_backoff * 2)
         self.assert_session_state(client_id, "open")
 
+
     def test_filelock(self):
         """
         Check that file lock doesn't get lost after an MDS restart
         """
-        lock_holder = self.mount_a.lock_background()
+        a_version_str = get_package_version(self.mount_a.client_remote, "fuse")
+        b_version_str = get_package_version(self.mount_b.client_remote, "fuse")
+        flock_version_str = "2.9"
+
+        version_regex = re.compile(r"[0-9\.]+")
+        a_result = version_regex.match(a_version_str)
+        self.assertTrue(a_result)
+        b_result = version_regex.match(b_version_str)
+        self.assertTrue(b_result)
+        a_version = version.StrictVersion(a_result.group())
+        b_version = version.StrictVersion(b_result.group())
+        flock_version=version.StrictVersion(flock_version_str)
+
+        flockable = False
+        if (a_version >= flock_version and b_version >= flock_version):
+            log.info("testing flock locks")
+            flockable = True
+        else:
+            log.info("not testing flock locks, machines have versions {av} and {bv}".format(
+                av=a_version_str,bv=b_version_str))
+
+        lock_holder = self.mount_a.lock_background(do_flock=flockable)
 
         self.mount_b.wait_for_visible("background_file-2")
-        self.mount_b.check_filelock()
+        self.mount_b.check_filelock(do_flock=flockable)
 
         self.fs.mds_fail_restart()
         self.fs.wait_for_state('up:active', timeout=MDS_RESTART_GRACE)
 
-        self.mount_b.check_filelock()
+        self.mount_b.check_filelock(do_flock=flockable)
 
         # Tear down the background process
         lock_holder.stdin.close()
