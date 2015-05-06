91d4c217e32b8b76fcac49f37879a3f78088694d
Samuel Just <sjust@redhat.com>
2015-05-06 10:52:40 -0700

    OSD: add scrub_wq suicide timeout
    Signed-off-by: Samuel Just <sjust@redhat.com>
    (cherry picked from commit 547a7041edc833f3cc8e04d388574809e30a8af6)
    
    Conflicts:
            src/osd/OSD.cc
                firefly still has scrub_finalize_wq

--- src/osd/OSD.h
+++ src/osd/OSD.h
@@ -1756,8 +1756,8 @@ protected:
 
   struct ScrubWQ : public ThreadPool::WorkQueue<PG> {
     OSD *osd;
-    ScrubWQ(OSD *o, time_t ti, ThreadPool *tp)
-      : ThreadPool::WorkQueue<PG>("OSD::ScrubWQ", ti, 0, tp), osd(o) {}
+    ScrubWQ(OSD *o, time_t ti, time_t si, ThreadPool *tp)
+      : ThreadPool::WorkQueue<PG>("OSD::ScrubWQ", ti, si, tp), osd(o) {}
 
     bool _empty() {
       return osd->scrub_queue.empty();
@@ -1847,8 +1847,8 @@ protected:
     list<MOSDRepScrub*> rep_scrub_queue;
 
   public:
-    RepScrubWQ(OSD *o, time_t ti, ThreadPool *tp)
-      : ThreadPool::WorkQueue<MOSDRepScrub>("OSD::RepScrubWQ", ti, 0, tp), osd(o) {}
+    RepScrubWQ(OSD *o, time_t ti, time_t si, ThreadPool *tp)
+      : ThreadPool::WorkQueue<MOSDRepScrub>("OSD::RepScrubWQ", ti, si, tp), osd(o) {}
 
     bool _empty() {
       return rep_scrub_queue.empty();
