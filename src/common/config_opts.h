df4e5de819c30003cfbe50a071c49039cf534419
Samuel Just <sjust@redhat.com>
2015-05-06 10:49:00 -0700

    OSD: add command_wq suicide timeout
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -592,6 +592,7 @@ OPTION(osd_scrub_finalize_thread_timeout, OPT_INT, 60*10)
 OPTION(osd_scrub_invalid_stats, OPT_BOOL, true)
 OPTION(osd_remove_thread_timeout, OPT_INT, 60*60)
 OPTION(osd_command_thread_timeout, OPT_INT, 10*60)
+OPTION(osd_command_thread_suicide_timeout, OPT_INT, 15*60)
 OPTION(osd_heartbeat_addr, OPT_ADDR, entity_addr_t())
 OPTION(osd_heartbeat_interval, OPT_INT, 6)       // (seconds) how often we ping peers
 OPTION(osd_heartbeat_grace, OPT_INT, 20)         // (seconds) how long before we decide a peer has failed
