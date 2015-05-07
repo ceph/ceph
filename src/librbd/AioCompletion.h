b45c99401e91710cf8e91a2ec1924fd8651d1698
Jason Dillaman <dillaman@redhat.com>
2015-05-07 15:32:27 -0400

    librbd: disable lockdep on AioCompletion
    It is only used by clients and it causes a large slowdown
    in performance due to the rate at which the lock is constructed/
    destructed for each IO request.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/AioCompletion.h
+++ src/librbd/AioCompletion.h
@@ -64,7 +64,7 @@ namespace librbd {
 
     AsyncOperation async_op;
 
-    AioCompletion() : lock(unique_lock_name("AioCompletion::lock", this), true),
+    AioCompletion() : lock("AioCompletion::lock", true, false),
 		      done(false), rval(0), complete_cb(NULL),
 		      complete_arg(NULL), rbd_comp(NULL),
 		      pending_count(0), blockers(1),
