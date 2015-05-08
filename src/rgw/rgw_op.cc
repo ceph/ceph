76f1dcaf80732db2f9572745161327e8b874ea6c
Matt Benjamin <mbenjamin@redhat.com>
2015-05-08 20:07:01 +0200

    rgw: refactor rgw_main
    This change is an extended version of work by Orit Wasserman
    moving some request-processing class headers and definition to
    their own files (RGWProcess, RGWProcessEnv, RGWProcessFrontend),
    along with process_request.
    
    I have taken the refactoring further, so as to remove all class
    declarations and member definitions from rgw_main.cc, so that the
    file retains just:
    
    * the responsibility to parse arguments and supervise the daemon
    * the responsibility to select from front-ends/configure the RGW service
    * usage
    
    I have made small adjustments:
    * moving some processing out of headers, to remove circular deps
    * removed unused headers from rgw_main.cc
    * added editor boilerplate and tweaked include guards in some files
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -1878,7 +1878,7 @@ void RGWDeleteBucket::execute()
     }
   }
 
-  op_ret = rgw_bucket_sync_user_stats(store, s->user.user_id, s->bucket);
+  op_ret = rgw_bucket_sync_user_stats(store, s->user->user_id, s->bucket);
   if ( op_ret < 0) {
      ldout(s->cct, 1) << "WARNING: failed to sync user stats before bucket delete: op_ret= " << op_ret << dendl;
   }
