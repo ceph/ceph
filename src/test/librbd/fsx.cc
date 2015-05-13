124d154a1149dc35ac0bc6a76299e7a4fdaa21a8
Ilya Dryomov <idryomov@gmail.com>
2015-05-13 17:10:39 +0300

    tests: disable deep-flatten feature for the krbd fsx test
    krbd doesn't support fast-diff or deep-flatten yet.  Had to do this
    with two commits because fast-diff is in next and deep-flatten isn't.
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- src/test/librbd/fsx.cc
+++ src/test/librbd/fsx.cc
@@ -483,7 +483,8 @@ __librbd_clone(struct rbd_ctx *ctx, const char *src_snapname,
 	if (krbd) {
 		features &= ~(RBD_FEATURE_EXCLUSIVE_LOCK |
 		              RBD_FEATURE_OBJECT_MAP     |
-                              RBD_FEATURE_FAST_DIFF);
+                              RBD_FEATURE_FAST_DIFF      |
+                              RBD_FEATURE_DEEP_FLATTEN);
 	}
 	ret = rbd_clone2(ioctx, ctx->name, src_snapname, ioctx,
 			 dst_imagename, features, order,
