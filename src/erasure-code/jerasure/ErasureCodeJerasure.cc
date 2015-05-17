14a59e5a2cb5f1549c9b7ae0e166d02608c9c21b
Loic Dachary <ldachary@redhat.com>
2015-05-17 18:04:56 +0200

    erasure-code: ErasureCodeJerasure update default values to string
    Update the ErasureCodeJerasure::init function to use string default values
    and update the profile accordingly.
    
    The ErasureCodeJerasure::revert_to_default prototype is modified so it
    can update the profile.
    
    http://tracker.ceph.com/issues/9589 Fixes: #9589
    
    Signed-off-by: Loic Dachary <ldachary@redhat.com>

--- src/erasure-code/jerasure/ErasureCodeJerasure.cc
+++ src/erasure-code/jerasure/ErasureCodeJerasure.cc
@@ -56,13 +56,13 @@ int ErasureCodeJerasure::init(ErasureCodeProfile& profile, ostream *ss)
 {
   int err = 0;
   dout(10) << "technique=" << technique << dendl;
-  map<string,string>::const_iterator parameter;
-  parameter = profile.find("ruleset-root");
-  if (parameter != profile.end())
-    ruleset_root = parameter->second;
-  parameter = profile.find("ruleset-failure-domain");
-  if (parameter != profile.end())
-    ruleset_failure_domain = parameter->second;
+  profile["technique"] = technique;
+  err |= to_string("ruleset-root", profile,
+		   &ruleset_root,
+		   DEFAULT_RULESET_ROOT, ss);
+  err |= to_string("ruleset-failure-domain", profile,
+		   &ruleset_failure_domain,
+		   DEFAULT_RULESET_FAILURE_DOMAIN, ss);
   err |= parse(profile, ss);
   if (err)
     return err;
@@ -200,12 +200,13 @@ int ErasureCodeJerasureReedSolomonVandermonde::parse(ErasureCodeProfile &profile
   err |= ErasureCodeJerasure::parse(profile, ss);
   if (w != 8 && w != 16 && w != 32) {
     *ss << "ReedSolomonVandermonde: w=" << w
-	<< " must be one of {8, 16, 32} : revert to DEFAULT_W " << std::endl;
-    w = DEFAULT_W;
+	<< " must be one of {8, 16, 32} : revert to " << DEFAULT_W << std::endl;
+    profile["w"] = "8";
+    err |= to_int("w", profile, &w, DEFAULT_W, ss);
     err = -EINVAL;
   }
   err |= to_bool("jerasure-per-chunk-alignment", profile,
-		 &per_chunk_alignment, false, ss);
+		 &per_chunk_alignment, "false", ss);
   return err;
 }
 
@@ -248,11 +249,14 @@ int ErasureCodeJerasureReedSolomonRAID6::parse(ErasureCodeProfile &profile,
 					       ostream *ss)
 {
   int err = ErasureCodeJerasure::parse(profile, ss);
+  if (profile.find("m") != profile.end())
+    profile.erase("m");
   m = 2;
   if (w != 8 && w != 16 && w != 32) {
     *ss << "ReedSolomonRAID6: w=" << w
 	<< " must be one of {8, 16, 32} : revert to 8 " << std::endl;
-    w = 8;
+    profile["w"] = "8";
+    err |= to_int("w", profile, &w, DEFAULT_W, ss);
     err = -EINVAL;
   }
   return err;
@@ -305,7 +309,7 @@ int ErasureCodeJerasureCauchy::parse(ErasureCodeProfile &profile,
   int err = ErasureCodeJerasure::parse(profile, ss);
   err |= to_int("packetsize", profile, &packetsize, DEFAULT_PACKETSIZE, ss);
   err |= to_bool("jerasure-per-chunk-alignment", profile,
-		 &per_chunk_alignment, false, ss);
+		 &per_chunk_alignment, "false", ss);
   return err;
 }
 
@@ -412,13 +416,19 @@ bool ErasureCodeJerasureLiberation::check_packetsize(ostream *ss) const
   }
 }
 
-void ErasureCodeJerasureLiberation::revert_to_default(ostream *ss)
+int ErasureCodeJerasureLiberation::revert_to_default(ErasureCodeProfile &profile,
+						     ostream *ss)
 {
+  int err = 0;
   *ss << "reverting to k=" << DEFAULT_K << ", w="
       << DEFAULT_W << ", packetsize=" << DEFAULT_PACKETSIZE << std::endl;
-  k = DEFAULT_K;
-  w = DEFAULT_W;
-  packetsize = DEFAULT_PACKETSIZE;
+  profile["k"] = DEFAULT_K;
+  err |= to_int("k", profile, &k, DEFAULT_K, ss);
+  profile["w"] = DEFAULT_W;
+  err |= to_int("w", profile, &w, DEFAULT_W, ss);
+  profile["packetsize"] = DEFAULT_PACKETSIZE;
+  err |= to_int("packetsize", profile, &packetsize, DEFAULT_PACKETSIZE, ss);
+  return err;
 }
 
 int ErasureCodeJerasureLiberation::parse(ErasureCodeProfile &profile,
@@ -435,7 +445,7 @@ int ErasureCodeJerasureLiberation::parse(ErasureCodeProfile &profile,
   if (!check_packetsize_set(ss) || !check_packetsize(ss))
     error = true;
   if (error) {
-    revert_to_default(ss);
+    revert_to_default(profile, ss);
     err = -EINVAL;
   }
   return err;
@@ -478,8 +488,12 @@ int ErasureCodeJerasureLiber8tion::parse(ErasureCodeProfile &profile,
 					 ostream *ss)
 {
   int err = ErasureCodeJerasure::parse(profile, ss);
-  m = DEFAULT_M;
-  w = DEFAULT_W;
+  if (profile.find("m") != profile.end())
+    profile.erase("m");
+  err |= to_int("m", profile, &m, DEFAULT_M, ss);
+  if (profile.find("w") != profile.end())
+    profile.erase("w");
+  err |= to_int("w", profile, &w, DEFAULT_W, ss);
   err |= to_int("packetsize", profile, &packetsize, DEFAULT_PACKETSIZE, ss);
 
   bool error = false;
@@ -488,7 +502,7 @@ int ErasureCodeJerasureLiber8tion::parse(ErasureCodeProfile &profile,
   if (!check_packetsize_set(ss))
     error = true;
   if (error) {
-    revert_to_default(ss);
+    revert_to_default(profile, ss);
     err = -EINVAL;
   }
   return err;
