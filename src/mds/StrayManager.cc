a8a4cd6aa131f29347a9102f3781babe83b6e901
Yan, Zheng <zyan@redhat.com>
2015-05-18 18:53:25 +0800

    mds: avoid calling StrayManager::eval_stray() recursively
    StrayManager::eval_stray() may close dirfrags and unlink remote
    dentries. These operations drop inode's reference, which may call
    StrayManager::eval_stray() again.
    
    Signed-off-by: Yan, Zheng <zyan@redhat.com>

--- src/mds/StrayManager.cc
+++ src/mds/StrayManager.cc
@@ -520,7 +520,7 @@ struct C_MDC_EvalStray : public StrayManagerContext {
   }
 };
 
-bool StrayManager::eval_stray(CDentry *dn, bool delay)
+bool StrayManager::__eval_stray(CDentry *dn, bool delay)
 {
   dout(10) << "eval_stray " << *dn << dendl;
   CDentry::linkage_t *dnl = dn->get_projected_linkage();
@@ -642,6 +642,18 @@ bool StrayManager::eval_stray(CDentry *dn, bool delay)
     eval_remote_stray(dn, NULL);
     return false;
   }
+}
+
+bool StrayManager::eval_stray(CDentry *dn, bool delay)
+{
+  // avoid nested eval_stray
+  if (dn->state_test(CDentry::STATE_EVALUATINGSTRAY))
+      return false;
+
+  dn->state_set(CDentry::STATE_EVALUATINGSTRAY);
+  bool ret = __eval_stray(dn, delay);
+  dn->state_clear(CDentry::STATE_EVALUATINGSTRAY);
+  return ret;
 }
 
 void StrayManager::eval_remote_stray(CDentry *stray_dn, CDentry *remote_dn)
