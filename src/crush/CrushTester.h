957df6ce161a8b128795317621694c4809fa98bd
Kefu Chai <kchai@redhat.com>
2015-05-26 15:34:33 +0800

    crush/CrushTester: check if any item id is too large
    Signed-off-by: Kefu Chai <kchai@redhat.com>
    (cherry picked from commit e640d89240017956b8c7411babb86be0f1e2b172)

--- src/crush/CrushTester.h
+++ src/crush/CrushTester.h
@@ -335,9 +335,12 @@ public:
 
   /**
    * check if any bucket/nodes is referencing an unknown name or type
-   * @return false if an dangling name/type is referenced, true otherwise
+   * @param max_id rejects any non-bucket items with id less than this number,
+   *               pass 0 to disable this check
+   * @return false if an dangling name/type is referenced or an item id is too
+   *         large, true otherwise
    */
-  bool check_name_maps() const;
+  bool check_name_maps(unsigned max_id = 0) const;
   int test();
   int test_with_crushtool(const string& crushtool,
 			  int timeout);
