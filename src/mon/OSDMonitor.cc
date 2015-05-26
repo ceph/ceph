cb40460c190daf806c3577a000c23984bd13d023
Kefu Chai <kchai@redhat.com>
2015-05-26 18:11:59 +0800

    mon: add "--check" to CrushTester::test_with_crushtool()
    so we don't need to call CrushTester::check_name_maps() in OSDMonitor.cc
    anymore.
    
    Fixes: #11680
    Signed-off-by: Kefu Chai <kchai@redhat.com>
    (cherry picked from commit c6e634875316cf17368d497e6dc4f6f4b5dd65d2)
    (cherry picked from commit d5d8b5b1bb311f2c98e8158824821f5e97862884)

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -4530,12 +4530,8 @@ bool OSDMonitor::prepare_command_impl(MMonCommand *m,
     // then we would consistently trigger an election before the command
     // finishes, having a flapping monitor unable to hold quorum.
     CrushTester tester(crush, ess);
-    if (!tester.check_name_maps(osdmap.get_max_osd())) {
-      err = -EINVAL;
-      ss << ess.str();
-      goto reply;
-    }
     int r = tester.test_with_crushtool(g_conf->crushtool,
+				       osdmap.get_max_osd(),
 				       g_conf->mon_lease);
     if (r < 0) {
       if (r == -EINTR) {
