81faac75b5e337ed3b27574d5923934c833e7ef8
Haomai Wang <haomaiwang@gmail.com>
2015-05-27 13:25:50 +0800

    debian/copyright: update copyright for test/perf*
    Signed-off-by: Haomai Wang <haomaiwang@gmail.com>

--- src/test/perf_local.cc
+++ src/test/perf_local.cc
@@ -1,6 +1,7 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 /* Copyright (c) 2015 Haomai Wang <haomaiwang@gmail.com>
+ * Copyright (c) 2011-2014 Stanford University
  * Copyright (c) 2011 Facebook
  *
  * Permission to use, copy, modify, and distribute this software for any
