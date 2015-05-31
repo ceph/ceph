8e1bcf21f090fc98ac9b495d76614e3012f5872a
Loic Dachary <ldachary@redhat.com>
2015-05-31 19:42:45 +0200

    ceph-disk: always check zap is applied on a full device
    zap only makes sense on a full device and this is verified for
    
        ceph-disk prepare --zap-disk
    
    but not for
    
        ceph-disk zap
    
    Unify the two and add a test to check that it fails as expected when
    trying to zap a directory.
    
    http://tracker.ceph.com/issues/11272 Fixes: #11272
    
    Signed-off-by: Loic Dachary <ldachary@redhat.com>

--- src/test/ceph-disk.sh
+++ src/test/ceph-disk.sh
@@ -220,6 +220,15 @@ function test_mark_init() {
     $rm -fr $osd_data
 }
 
+function test_zap() {
+    local osd_data=$DIR/dir
+    $mkdir -p $osd_data
+
+    ./ceph-disk $CEPH_DISK_ARGS zap $osd_data 2>&1 | grep 'not full block device' || return 1
+
+    $rm -fr $osd_data
+}
+
 # ceph-disk prepare returns immediately on success if the magic file
 # exists in the --osd-data directory.
 function test_activate_dir_magic() {
@@ -613,6 +622,7 @@ function run() {
     default_actions+="test_activate_dir "
     default_actions+="test_keyring_path "
     default_actions+="test_mark_init "
+    default_actions+="test_zap "
     local actions=${@:-$default_actions}
     local status
     for action in $actions  ; do
