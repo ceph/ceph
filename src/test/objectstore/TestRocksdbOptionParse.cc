06391087fe60f0dfb6740629352617e06681c798
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2015-06-01 23:48:28 +0200

    TestRocksdbOptionParse.cc: prefer ++operator for non-primitive iter
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

--- src/test/objectstore/TestRocksdbOptionParse.cc
+++ src/test/objectstore/TestRocksdbOptionParse.cc
@@ -61,7 +61,7 @@ TEST(RocksDBOption, interpret) {
   int num_low_pri_threads = 0;
   for (vector<rocksdb::ThreadStatus>::iterator it = thread_list.begin();
 	it!= thread_list.end();
-	it++) {
+	++it) {
     if (it->thread_type == rocksdb::ThreadStatus::HIGH_PRIORITY)
       num_high_pri_threads++;
     if (it->thread_type == rocksdb::ThreadStatus::LOW_PRIORITY)
