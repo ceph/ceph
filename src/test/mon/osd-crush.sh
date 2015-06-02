ca3e9c57fb877f355f3d2cece6d6d623508dc0e9
Kefu Chai <kchai@redhat.com>
2015-06-02 23:52:22 +0800

    mon: add an "osd crush tree" command
    * to print crush buckets/items in a tree
    
    Fixes: #11833
    Signed-off-by: Kefu Chai <kchai@redhat.com>
    (cherry picked from commit 5436c290f3622feb8d4b279ed6552b2510e0cee9)
    
    Conflicts:
    	src/test/mon/osd-crush.sh: trivial resolution

--- src/test/mon/osd-crush.sh
+++ src/test/mon/osd-crush.sh
@@ -200,7 +200,15 @@ function TEST_crush_reject_empty() {
         ./ceph osd setcrushmap -i $empty_map.map || return 1
 }
 
-main osd-crush
+function TEST_crush_tree() {
+    local dir=$1
+    run_mon $dir a || return 1
+
+    ./ceph osd crush tree --format=xml | \
+        $XMLSTARLET val -e -r test/mon/osd-crush-tree.rng - || return 1
+}
+
+main osd-crush 
 
 # Local Variables:
 # compile-command: "cd ../.. ; make -j4 && test/mon/osd-crush.sh"
