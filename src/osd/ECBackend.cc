1e86c5e6087331f4c6e7e91a08b882d4c1a93fc2
Xinze Chi <xmdxcxz@gmail.com>
2015-06-03 20:31:55 +0800

    osd: bug fix when op.extent.length == 0 in ecbackend
    if no-ecbackend, if op.extent.length == 0, osd would return the range of
    (op.extent.offset ~ object.length) to client, but in ecbackend it would
    return empty data.
    
    Signed-off-by: Xinze Chi <xmdxcxz@gmail.com>

--- src/osd/ECBackend.cc
+++ src/osd/ECBackend.cc
@@ -1635,10 +1635,17 @@ struct CallClientContexts :
 	&bl);
       assert(i->second.second);
       assert(i->second.first);
-      i->second.first->substr_of(
-	bl,
-	i->first.get<0>() - adjusted.first,
-	MIN(i->first.get<1>(), bl.length() - (i->first.get<0>() - adjusted.first)));
+      if (i->first.get<1>()) {
+        i->second.first->substr_of(
+	  bl,
+	  i->first.get<0>() - adjusted.first,
+	  MIN(i->first.get<1>(), bl.length() - (i->first.get<0>() - adjusted.first)));
+      } else {
+        i->second.first->substr_of(
+	  bl,
+	  i->first.get<0>() - adjusted.first,
+	  bl.length() - (i->first.get<0>() - adjusted.first));
+      }
       if (i->second.second) {
 	i->second.second->complete(i->second.first->length());
       }
