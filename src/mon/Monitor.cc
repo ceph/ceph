6a0d61d5bb7a812fdf24fba39492e27125f4f3bd
Joao Eduardo Luis <joao@suse.de>
2015-06-05 16:11:57 +0100

    mon: Monitor: mark events
    Signed-off-by: Joao Eduardo Luis <joao@suse.de>

--- src/mon/Monitor.cc
+++ src/mon/Monitor.cc
@@ -3348,9 +3348,11 @@ void Monitor::waitlist_or_zap_client(MonOpRequestRef op)
       con->is_connected()) {
     dout(5) << "waitlisting message " << *m << dendl;
     maybe_wait_for_quorum.push_back(new C_RetryMessage(this, op));
+    op->mark_wait_for_quorum();
   } else {
     dout(5) << "discarding message " << *m << " and sending client elsewhere" << dendl;
     con->mark_down();
+    op->mark_zap();
   }
 }
 
@@ -3366,6 +3368,7 @@ void Monitor::_ms_dispatch(Message *m)
 
 void Monitor::dispatch(MonOpRequestRef op)
 {
+  op->mark_event("monitor_dispatch");
   ConnectionRef connection = op->get_connection();
   MonSession *s = NULL;
   MonCap caps;
@@ -3454,6 +3457,7 @@ void Monitor::dispatch(MonOpRequestRef op)
 
 void Monitor::dispatch_op(MonOpRequestRef op)
 {
+  op->mark_event("monitor_dispatch_op");
   /* deal with all messages that do not necessarily need caps */
   bool dealt_with = true;
   switch (op->get_req()->get_type()) {
