b90fc542d96051a8491c4b515a8da32e16961571
Zhiqiang Wang <zhiqiang.wang@intel.com>
2015-06-10 14:21:36 +0800

    osd: copy the reqids even if the object is deleted during promotion
    If the object is deleted on the base tier, and the reqids are not copied
    during promotion, this again leads to the 'ops not idempotent' problem.
    For the copy-get op, this fix copies the reqids even if the object doesn't
    exist.
    
    Signed-off-by: Zhiqiang Wang <zhiqiang.wang@intel.com>

--- src/osd/ReplicatedPG.h
+++ src/osd/ReplicatedPG.h
@@ -1354,6 +1354,8 @@ protected:
     OSDOp& op,
     ObjectContextRef& obc,
     bool classic);
+  void fill_in_copy_get_noent(OpRequestRef& op, hobject_t oid,
+                              OSDOp& osd_op, bool classic);
 
   /**
    * To copy an object, call start_copy.
