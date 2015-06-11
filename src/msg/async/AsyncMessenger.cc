0205b909c2b027acf54820b905d54a5893c98328
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2015-06-11 20:39:46 +0200

    msg/async/AsyncMessenger.cc: init listen_sd in ctor
    Fix for:
    
    CID 1249635 (#1 of 1): Uninitialized scalar field (UNINIT_CTOR)
     uninit_member: Non-static class member listen_sd is not
     initialized in this constructor nor in any functions that it calls.
    
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

--- src/msg/async/AsyncMessenger.cc
+++ src/msg/async/AsyncMessenger.cc
@@ -385,7 +385,7 @@ AsyncMessenger::AsyncMessenger(CephContext *cct, entity_name_t name,
   : SimplePolicyMessenger(cct, name,mname, _nonce),
     processor(this, cct, _nonce),
     lock("AsyncMessenger::lock"),
-    nonce(_nonce), need_addr(true), did_bind(false),
+    nonce(_nonce), need_addr(true), listen_sd(-1), did_bind(false),
     global_seq(0), deleted_lock("AsyncMessenger::deleted_lock"),
     cluster_protocol(0), stopped(true)
 {
