8daa5c3df33c1b34ec9e2935f61395c3ddaa570c
Vu Pham <vu@mellanox.com>
2015-06-12 09:50:28 -0700

    xio: fix to work with the merge of pull request #4707
    fix xio to work with the merge of pull request
    " Removed unnecessary inclusion of iostream in several files #4707"
    
    Signed-off-by: Vu Pham <vu@mellanox.com>

--- src/msg/xio/XioPool.cc
+++ src/msg/xio/XioPool.cc
@@ -12,6 +12,7 @@
  *
  */
 
+#include <iostream>
 #include "XioPool.h"
 
 XioPoolStats xp_stats;
@@ -19,4 +20,22 @@ XioPoolStats xp_stats;
 bool XioPool::trace_mempool = 0;
 bool XioPool::trace_msgcnt = 0;
 
-
+void XioPoolStats::dump(const char* tag, uint64_t serial)
+{
+  std::cout
+    << tag << " #" << serial << ": "
+    << "pool objs: "
+    << "64: " << ctr_set[SLAB_64].read() << " "
+    << "256: " << ctr_set[SLAB_256].read() << " "
+    << "1024: " << ctr_set[SLAB_1024].read() << " "
+    << "page: " << ctr_set[SLAB_PAGE].read() << " "
+    << "max: " << ctr_set[SLAB_MAX].read() << " "
+    << "overflow: " << ctr_set[SLAB_OVERFLOW].read() << " "
+    << std::endl;
+  std::cout
+    << tag << " #" << serial << ": "
+    << " msg objs: "
+    << "in: " << hook_cnt.read() << " "
+    << "out: " << msg_cnt.read() << " "
+    << std::endl;
+}
