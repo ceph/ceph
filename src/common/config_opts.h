350f43ef0d5a9ff8da447abedf468c53a3fd61c3
Haomai Wang <haomaiwang@gmail.com>
2015-06-17 22:04:52 +0800

    librbd: Add option to allow disabling issuing alloc hint
    For HDD osd backend, a alloc hint is good for local filesystem to ensure
    extent continuous. But for SSD, the available capacity is useful and
    precious, user may not expect lots of capacity is used by reserving.
    Then ssd is good for random rw.
    
    Signed-off-by: Haomai Wang <haomaiwang@gmail.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -897,6 +897,7 @@ OPTION(rbd_blacklist_on_break_lock, OPT_BOOL, true) // whether to blacklist clie
 OPTION(rbd_blacklist_expire_seconds, OPT_INT, 0) // number of seconds to blacklist - set to 0 for OSD default
 OPTION(rbd_request_timed_out_seconds, OPT_INT, 30) // number of seconds before maint request times out
 OPTION(rbd_skip_partial_discard, OPT_BOOL, false) // when trying to discard a range inside an object, set to true to skip zeroing the range.
+OPTION(rbd_enable_alloc_hint, OPT_BOOL, true) // when writing a object, it will issue a hint to osd backend to indicate the expected size object need
 
 /*
  * The following options change the behavior for librbd's image creation methods that
