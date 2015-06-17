d6931609521ee6016c9a7b805cbbba7bab21fe05
Sage Weil <sage@redhat.com>
2015-06-17 14:48:43 -0700

    os/FileStore: require upgrade to hammer before moving beyond
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/FileStore.cc
+++ src/os/FileStore.cc
@@ -1202,8 +1202,8 @@ int FileStore::upgrade()
   if (r == 1)
     return 0;
 
-  if (version < 3) {
-    derr << "ObjectStore is old at version " << version << ".  Please upgrade to firefly v0.80.x, convert your store, and then upgrade."  << dendl;
+  if (version < 4) {
+    derr << "ObjectStore is old at version " << version << ".  Please upgrade to hammer v0.94.x first."  << dendl;
     return -EINVAL;
   }
 
