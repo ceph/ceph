66f2e68fb046e1ebbfe1742a418bcaab213c903d
Jason Dillaman <dillaman@redhat.com>
2015-06-22 15:30:02 -0400

    lockdep: do not automatically collect all backtraces
    It is expensive to collect backtraces every time a lock is
    checked in order to provide cycle backtraces.  The backtraces
    can be forced on for specific locks or globally via the new
    config option "lockdep_force_backtrace".
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/common/Mutex.h
+++ src/common/Mutex.h
@@ -53,7 +53,7 @@ private:
     id = lockdep_register(name.c_str());
   }
   void _will_lock() { // about to lock
-    id = lockdep_will_lock(name.c_str(), id);
+    id = lockdep_will_lock(name.c_str(), id, backtrace);
   }
   void _locked() {    // just locked
     id = lockdep_locked(name.c_str(), id, backtrace);
