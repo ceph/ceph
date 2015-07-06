d7585c020de41f25e6a598661844169f2bdbb2b4
Jianpeng Ma <jianpeng.ma@intel.com>
2015-07-06 13:49:42 +0800

    common/TrackedOp: check tracking_enabled for event "initiated/done".
    Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

--- src/common/TrackedOp.h
+++ src/common/TrackedOp.h
@@ -161,7 +161,8 @@ protected:
     warn_interval_multiplier(1)
   {
     tracker->register_inflight_op(&xitem);
-    events.push_back(make_pair(initiated_at, "initiated"));
+    if (tracker->tracking_enabled)
+      events.push_back(make_pair(initiated_at, "initiated"));
   }
 
   /// output any type-specific data you want to get when dump() is called
