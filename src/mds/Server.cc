2b891c4737667ea24dffa373dd8f9aefde832ffe
Jashan Kamboj <jashank42@gmail.com>
2015-07-09 19:11:12 +0530

    mds: mkdir check_access
    Signed-off-by: Jashan Kamboj <jashank42@gmail.com>

--- src/mds/Server.cc
+++ src/mds/Server.cc
@@ -4537,6 +4537,10 @@ void Server::handle_client_mkdir(MDRequestRef& mdr)
   if (!mds->locker->acquire_locks(mdr, rdlocks, wrlocks, xlocks))
     return;
 
+  // mkdir check access
+  if (!check_access(mdr, diri, MAY_WRITE))
+    return;
+
   // new inode
   SnapRealm *realm = dn->get_dir()->inode->find_snaprealm();
   snapid_t follows = realm->get_newest_seq();
