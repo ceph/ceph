8ebcf3d15251e2098f44421d724ba28015a037cc
Sage Weil <sage@redhat.com>
2015-07-13 10:56:28 -0400

    os/LFNIndex: pass through sort_bitwise
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/LFNIndex.cc
+++ src/os/LFNIndex.cc
@@ -151,11 +151,12 @@ int LFNIndex::pre_hash_collection(uint32_t pg_num, uint64_t expected_num_objs)
 
 int LFNIndex::collection_list_partial(const ghobject_t &start,
 				      const ghobject_t &end,
+				      bool sort_bitwise,
 				      int max_count,
 				      vector<ghobject_t> *ls,
 				      ghobject_t *next)
 {
-  return _collection_list_partial(start, end, max_count, ls, next);
+  return _collection_list_partial(start, end, sort_bitwise, max_count, ls, next);
 }
 
 /* Derived class utility methods */
