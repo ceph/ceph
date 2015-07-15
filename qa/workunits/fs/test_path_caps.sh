b9c5f709da73c6ef71834eaf3ff4f62a376f2d94
Jashan Kamboj <jashank42@gmail.com>
2015-07-15 19:26:40 +0530

    add unlink, rmdir check_access test
    Signed-off-by: Jashan Kamboj <jashank42@gmail.com>

--- qa/workunits/fs/test_path_caps.sh
+++ qa/workunits/fs/test_path_caps.sh
@@ -3,7 +3,8 @@
 mkdir -p mnt.admin mnt.foo
 
 ./ceph-fuse -n client.admin mnt.admin
-mkdir -p mnt.admin/foo mnt.admin/bar
+mkdir -p mnt.admin/foo/p mnt.admin/bar/q
+touch mnt.admin/foo/r mnt.admin/bar/s
 
 ./ceph auth get-or-create client.foo mon 'allow r' mds 'allow r, allow rw path=/foo' osd 'allow rwx' >> keyring
 ./ceph-fuse -n client.foo mnt.foo
@@ -30,11 +31,15 @@ mkdir mnt.foo/foo/x
 mkdir mnt.foo/foo/y
 ln -s symlink mnt.foo/foo/x
 ln -s symlink mnt.foo/foo/y
+unlink mnt.foo/foo/r
+rmdir mnt.foo/foo/p
 
 # everything else is not
 expect_false mkdir mnt.foo/bar/x
 expect_false mkdir mnt.foo/food
 expect_false ln -s symlink mnt.foo/x
-expect_false ln -s symlink mnt.foo/foo
+expect_false ln -s symlink mnt.foo/food
+expect_false unlink mnt.foo/bar/s
+expect_false rmdir mnt.foo/bar/q
 
 echo PASS
