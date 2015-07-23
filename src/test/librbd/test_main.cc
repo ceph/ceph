81ded247cab040be69d7efe43b981b17faae2504
Jason Dillaman <dillaman@redhat.com>
2015-07-23 13:56:59 -0400

    tests: verify proper handling of librbd replay of IO journal events
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/test/librbd/test_main.cc
+++ src/test/librbd/test_main.cc
@@ -12,6 +12,7 @@ extern void register_test_librbd();
 #ifdef TEST_LIBRBD_INTERNALS
 extern void register_test_image_watcher();
 extern void register_test_internal();
+extern void register_test_journal_replay();
 extern void register_test_object_map();
 #endif // TEST_LIBRBD_INTERNALS
 
@@ -21,6 +22,7 @@ int main(int argc, char **argv)
 #ifdef TEST_LIBRBD_INTERNALS
   register_test_image_watcher();
   register_test_internal();
+  register_test_journal_replay();
   register_test_object_map();
 #endif // TEST_LIBRBD_INTERNALS
 
