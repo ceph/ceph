64c6bd1845d18e85c39ea8d1e0b464dace7396f7
Jason Dillaman <dillaman@redhat.com>
2015-07-30 14:34:57 -0400

    librbd: rename librbd::WatchNotify namespace to librbd::watch_notify
    The new namespace follows the expected naming conventions.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/test/encoding/types.h
+++ src/test/encoding/types.h
@@ -229,8 +229,8 @@ TYPE(EUpdate)
 #include "librbd/JournalTypes.h"
 TYPE(librbd::journal::EventEntry)
 #include "librbd/WatchNotifyTypes.h"
-TYPE(librbd::WatchNotify::NotifyMessage)
-TYPE(librbd::WatchNotify::ResponseMessage)
+TYPE(librbd::watch_notify::NotifyMessage)
+TYPE(librbd::watch_notify::ResponseMessage)
 
 #include "rbd_replay/ActionTypes.h"
 TYPE(rbd_replay::action::Dependency)
