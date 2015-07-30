ecebbdd65c96412c539d8180a90b161f0371d58c
Casey Bodley <cbodley@redhat.com>
2015-07-30 16:26:26 -0400

    c++11: remove references to the std::tr1 namespace
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/include/hash_namespace.h
+++ src/include/hash_namespace.h
@@ -3,22 +3,10 @@
 
 #include <ciso646>
 
-#ifdef _LIBCPP_VERSION
-
 #include <functional>
 
 #define CEPH_HASH_NAMESPACE_START namespace std {
 #define CEPH_HASH_NAMESPACE_END }
 #define CEPH_HASH_NAMESPACE std
-
-#else
-
-#include <tr1/functional>
-
-#define CEPH_HASH_NAMESPACE_START namespace std { namespace tr1 {
-#define CEPH_HASH_NAMESPACE_END }}
-#define CEPH_HASH_NAMESPACE std::tr1
-
-#endif
 
 #endif
