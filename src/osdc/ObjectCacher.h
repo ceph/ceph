909352794eb87b24c76a3ad190efbd7b7a4a7db8
Jianpeng Ma <jianpeng.ma@intel.com>
2015-08-07 13:18:11 +0800

    osdc/objectCacher: Remove the unsed field.
    Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

--- src/osdc/ObjectCacher.h
+++ src/osdc/ObjectCacher.h
@@ -63,7 +63,6 @@ class ObjectCacher {
   struct OSDRead {
     vector<ObjectExtent> extents;
     snapid_t snap;
-    map<object_t, bufferlist*> read_data;  // bits of data as they come back
     bufferlist *bl;
     int fadvise_flags;
     OSDRead(snapid_t s, bufferlist *b, int f)
