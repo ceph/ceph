97c66e3f3f17eea0a40d20619d171c85aec3a9ed
Yuan Zhou <yuan.zhou@intel.com>
2015-08-07 11:51:27 +0800

    erasure-code: Update ISA-L to 2.14
    Ceph requires all .s files to annotated to assert that stack
    should not be executable. see bug #10114, f60da6b and 06a245a.
    In ISA-L 2.14 this issue gets fixed.
    (see src/erasure-code/isa/isa-l/include/reg_sizes.asm:L102-L106)
    
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- src/erasure-code/isa/isa-l/erasure_code/gf_4vect_mad_sse.asm.s
+++ src/erasure-code/isa/isa-l/erasure_code/gf_4vect_mad_sse.asm.s
@@ -31,6 +31,8 @@
 ;;; gf_4vect_mad_sse(len, vec, vec_i, mul_array, src, dest);
 ;;;
 
+%include "reg_sizes.asm"
+
 %define PS 8
 
 %ifidn __OUTPUT_FORMAT__, win64
@@ -336,15 +338,5 @@ mask0f:
 constip16:
 	ddq 0xf0f1f2f3f4f5f6f7f8f9fafbfcfdfeff
 
-%macro slversion 4
-global %1_slver_%2%3%4
-global %1_slver
-%1_slver:
-%1_slver_%2%3%4:
-	dw 0x%4
-	db 0x%3, 0x%2
-%endmacro
 ;;;       func             core, ver, snum
-slversion gf_4vect_mad_sse, 00,  00,  0209
-; inform linker that this doesn't require executable stack
-section .note.GNU-stack noalloc noexec nowrite progbits
+slversion gf_4vect_mad_sse, 00,  01,  0209
