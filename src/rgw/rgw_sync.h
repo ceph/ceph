3482d5caf0e022262b9eab4d45498190ed9a4f65
Yehuda Sadeh <yehuda@redhat.com>
2015-08-07 14:17:33 -0700

    rgw: move coroutines code
    move the generic code to rgw_coroutine.{h,cc}
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_sync.h
+++ src/rgw/rgw_sync.h
@@ -1,16 +1,9 @@
 #ifndef CEPH_RGW_SYNC_H
 #define CEPH_RGW_SYNC_H
 
-#include "rgw_common.h"
-#include "rgw_rados.h"
-#include "rgw_metadata.h"
-#include "rgw_http_client.h"
+#include "rgw_coroutine.h"
 
 #include "common/RWLock.h"
-#include "common/RefCountedObj.h"
-
-
-#define dout_subsys ceph_subsys_rgw
 
 
 struct rgw_mdlog_info {
@@ -19,147 +12,6 @@ struct rgw_mdlog_info {
   rgw_mdlog_info() : num_shards(0) {}
 
   void decode_json(JSONObj *obj);
-};
-
-#define RGW_ASYNC_OPS_MGR_WINDOW 16
-
-class RGWCoroutinesStack;
-class RGWCoroutinesManager;
-class AioCompletionNotifier;
-
-struct RGWCoroutinesEnv {
-  RGWCoroutinesManager *manager;
-  list<RGWCoroutinesStack *> *stacks;
-  RGWCoroutinesStack *stack;
-
-  RGWCoroutinesEnv() : manager(NULL), stacks(NULL), stack(NULL) {}
-};
-
-class RGWCoroutine : public RefCountedObject {
-  friend class RGWCoroutinesStack;
-protected:
-  RGWCoroutinesEnv *env;
-  bool blocked;
-  int retcode;
-
-  stringstream error_stream;
-
-  void set_blocked(bool flag) { blocked = flag; }
-  int yield(int ret) {
-    set_blocked(true);
-    return ret;
-  }
-
-  int do_operate(RGWCoroutinesEnv *_env) {
-    env = _env;
-    return operate();
-  }
-
-  void call(RGWCoroutine *op);
-  void spawn(RGWCoroutine *op);
-
-public:
-  RGWCoroutine() : env(NULL), blocked(false), retcode(0) {}
-  virtual ~RGWCoroutine() {}
-
-  virtual int operate() = 0;
-
-  virtual bool is_done() = 0;
-  virtual bool is_error() = 0;
-
-  stringstream& log_error() { return error_stream; }
-  string error_str() {
-    return error_stream.str();
-  }
-
-  bool is_blocked() { return blocked; }
-
-  void set_retcode(int r) {
-    retcode = r;
-  }
-
-  int get_ret_status() {
-    return retcode;
-  }
-};
-
-class RGWCoroutinesStack {
-  CephContext *cct;
-
-  RGWCoroutinesManager *ops_mgr;
-
-  list<RGWCoroutine *> ops;
-  list<RGWCoroutine *>::iterator pos;
-
-  set<RGWCoroutinesStack *> blocked_by_stack;
-  set<RGWCoroutinesStack *> blocking_stacks;
-
-
-  bool done_flag;
-  bool error_flag;
-  bool blocked_flag;
-
-public:
-  RGWCoroutinesStack(CephContext *_cct, RGWCoroutinesManager *_ops_mgr, RGWCoroutine *start = NULL);
-
-  int operate(RGWCoroutinesEnv *env);
-
-  bool is_done() {
-    return done_flag;
-  }
-  bool is_error() {
-    return error_flag;
-  }
-  bool is_blocked_by_stack() {
-    return !blocked_by_stack.empty();
-  }
-  bool is_blocked() {
-    return blocked_flag || is_blocked_by_stack();
-  }
-
-  void set_blocked(bool flag);
-
-  string error_str();
-
-  int call(RGWCoroutine *next_op, int ret = 0);
-  int unwind(int retcode);
-
-  AioCompletionNotifier *create_completion_notifier();
-  RGWCompletionManager *get_completion_mgr();
-
-  void set_blocked_by(RGWCoroutinesStack *s) {
-    blocked_by_stack.insert(s);
-    s->blocking_stacks.insert(this);
-  }
-
-  bool unblock_stack(RGWCoroutinesStack **s);
-};
-
-class RGWCoroutinesManager {
-  CephContext *cct;
-
-  void handle_unblocked_stack(list<RGWCoroutinesStack *>& stacks, RGWCoroutinesStack *stack, int *waiting_count);
-protected:
-  RGWCompletionManager completion_mgr;
-
-  int ops_window;
-
-  void put_completion_notifier(AioCompletionNotifier *cn);
-public:
-  RGWCoroutinesManager(CephContext *_cct) : cct(_cct), ops_window(RGW_ASYNC_OPS_MGR_WINDOW) {}
-  virtual ~RGWCoroutinesManager() {}
-
-  int run(list<RGWCoroutinesStack *>& ops);
-  int run(RGWCoroutine *op);
-
-  virtual void report_error(RGWCoroutinesStack *op);
-
-  AioCompletionNotifier *create_completion_notifier(RGWCoroutinesStack *stack);
-  RGWCompletionManager *get_completion_mgr() { return &completion_mgr; }
-
-  RGWCoroutinesStack *allocate_stack() {
-    return new RGWCoroutinesStack(cct, this);
-  }
 };
 
 struct RGWMetaSyncGlobalStatus {
