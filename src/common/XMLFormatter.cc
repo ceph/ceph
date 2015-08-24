293c5a5b17e66cad78e30ae6cb5984e428ae5b73
Robin H. Johnson <robin.johnson@dreamhost.com>
2015-08-24 02:43:00 +0000

    common/*Formatters: handling of headers & footers.
    Formatters now gain first-class headers and footers, along with
    consistent handling of having header/footer only output ONCE.
    
    Signed-off-by: Robin H. Johnson <robin.johnson@dreamhost.com>

--- src/common/XMLFormatter.cc
+++ src/common/XMLFormatter.cc
@@ -46,8 +46,11 @@ XMLFormatter::XMLFormatter(bool pretty)
 void XMLFormatter::flush(std::ostream& os)
 {
   finish_pending_string();
-  os << m_ss.str();
-  if (m_pretty)
+  std::string m_ss_str = m_ss.str();
+  os << m_ss_str;
+  /* There is a small catch here. If the rest of the formatter had NO output,
+   * we should NOT output a newline. This primarily triggers on HTTP redirects */
+  if (m_pretty && !m_ss_str.empty())
     os << "\n";
   m_ss.clear();
   m_ss.str("");
@@ -61,6 +64,24 @@ void XMLFormatter::reset()
   m_pending_string.str("");
   m_sections.clear();
   m_pending_string_name.clear();
+  m_header_done = false;
+}
+
+void XMLFormatter::output_header()
+{
+  if(!m_header_done) {
+    m_header_done = true;
+    write_raw_data(XMLFormatter::XML_1_DTD);;
+    if (m_pretty)
+      m_ss << "\n";
+  }
+}
+
+void XMLFormatter::output_footer()
+{
+  while(!m_sections.empty()) {
+    close_section();
+  }
 }
 
 void XMLFormatter::open_object_section(const char *name)
