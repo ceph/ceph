95f712616334b495c1ee20a9416a4f2d8d7f2120
Orit Wasserman <owasserm@redhat.com>
2015-08-26 16:43:49 +0200

    rgw: RGWZoneGroup should inheirt from RGWSystemMetaObj
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- src/rgw/rgw_rest_conn.cc
+++ src/rgw/rgw_rest_conn.cc
@@ -14,7 +14,7 @@ RGWRESTConn::RGWRESTConn(CephContext *_cct, RGWRados *store, list<string>& remot
     endpoints[i] = *iter;
   }
   key = store->zone.system_key;
-  zone_group = store->zonegroup.name;
+  zone_group = store->zonegroup.get_id();
 }
 
 int RGWRESTConn::get_url(string& endpoint)
