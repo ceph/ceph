2bd6ab978a35550aa43cc440a1c11f0ae9a1bb5e
David Zafman <dzafman@redhat.com>
2015-08-26 13:58:09 -0700

    osd: Make the _scrub routine produce good output and detect errors properly
    Catch decode errors so osd doesn't crash on corrupt OI_ATTR or SS_ATTR
    Use boost::optional<> to make current state clearer
    Create next_clone as needed using head/curclone
    Add equivalent logic after getting to end of scrubmap.objects
    
    Fixes: #12738
    
    Signed-off-by: David Zafman <dzafman@redhat.com>
    (cherry picked from commit a23036c6fd7de5d1dbc2bd30c967c0be51d94ca5)
    
    Conflicts:
    	src/osd/ReplicatedPG.cc (no num_objects_pinned in hammer)
    	src/osd/ReplicatedPG.h (no get_temp_recovery_object() in hammer)

--- src/osd/ReplicatedPG.h
+++ src/osd/ReplicatedPG.h
@@ -1421,6 +1421,22 @@ private:
   uint64_t temp_seq; ///< last id for naming temp objects
   coll_t get_temp_coll(ObjectStore::Transaction *t);
   hobject_t generate_temp_object();  ///< generate a new temp object name
+  void log_missing(const boost::optional<hobject_t> &head,
+			LogChannelRef clog,
+			const spg_t &pgid,
+			const char *func,
+			const char *mode,
+			bool allow_incomplete_clones);
+  void process_clones_to(const boost::optional<hobject_t> &head,
+    const boost::optional<SnapSet> &snapset,
+    LogChannelRef clog,
+    const spg_t &pgid,
+    const char *mode,
+    bool &missing,
+    bool allow_incomplete_clones,
+    snapid_t target,
+    vector<snapid_t>::reverse_iterator *curclone);
+
 public:
   void get_colls(list<coll_t> *out) {
     out->push_back(coll);
