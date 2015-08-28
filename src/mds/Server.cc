e2d423c9a2926fbf2ced832079ceab216b6ddaf2
Sage Weil <sage@redhat.com>
2015-08-28 17:32:11 -0400

    mds/Server: skip auth check on session-less mdr's
    The mds internal requests don't have a session.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mds/Server.cc
+++ src/mds/Server.cc
@@ -2108,12 +2108,12 @@ void Server::handle_slave_auth_pin_ack(MDRequestRef& mdr, MMDSSlaveRequest *ack)
  */
 bool Server::check_access(MDRequestRef& mdr, CInode *in, unsigned mask)
 {
- if (!mdr->session->check_access(
-     in, mask,
-     mdr->client_request->get_caller_uid(),
-     mdr->client_request->get_caller_gid(),
-     mdr->client_request->head.args.setattr.uid,
-     mdr->client_request->head.args.setattr.gid)) {
+ if (mdr->session && !mdr->session->check_access(
+       in, mask,
+       mdr->client_request->get_caller_uid(),
+       mdr->client_request->get_caller_gid(),
+       mdr->client_request->head.args.setattr.uid,
+       mdr->client_request->head.args.setattr.gid)) {
     respond_to_request(mdr, -EACCES);
     return false;
   }
