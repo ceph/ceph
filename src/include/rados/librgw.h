ce5ba5d685e8d71b76b0c9ddae384bf7983553ab
Matt Benjamin <mbenjamin@redhat.com>
2015-08-30 16:28:48 -0400

    librgw: compile and implement more of rgw_file
    The rgw_file body wasn't being built, so had some impossible
    constructs.  More had been added by the commit which renamed
    struct nfs_handle to struct rgw_file_handle.
    
    Several key RGWLib methods hadn't been implemented (in progress).
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/include/rados/librgw.h
+++ src/include/rados/librgw.h
@@ -27,32 +27,8 @@ int librgw_acl_xml2bin(librgw_t rgw, const char *xml, char** bin,
 void librgw_free_bin(librgw_t rgw, char* bin);
 void librgw_shutdown(librgw_t rgw);
 
-/* librgw external interface */
 int librgw_init();
 int librgw_stop();
-
-/* User interface */
-int get_userinfo_by_uid(const string& uid);
-int get_user_acl();
-int set_user_permissions();
-int set_user_quota();
-int get_user_quota();
-
-/* buckets */
-int get_user_buckets_list();
-int get_bucket_objects_list();
-int create_bucket();
-int delete_bucket();
-int get_bucket_attributes();
-int set_bucket_attributes();
-
-/* objects */
-int create_object ();
-int delete_object();
-int rgw_write();
-int rgw_read();
-int get_object_attributes();
-int set_object_attributes();
 
 #ifdef __cplusplus
 }
