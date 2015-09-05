ad3c7d3a7a85ab0e32efca418ccd26565d400f8e
Yehuda Sadeh <yehuda@redhat.com>
2015-09-04 17:11:05 -0700

    ceph_json: add json encoder / decoder for std::set
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/common/ceph_json.h
+++ src/common/ceph_json.h
@@ -165,6 +165,21 @@ void decode_json_obj(list<T>& l, JSONObj *obj)
 }
 
 template<class T>
+void decode_json_obj(set<T>& l, JSONObj *obj)
+{
+  l.clear();
+
+  JSONObjIter iter = obj->find_first();
+
+  for (; !iter.end(); ++iter) {
+    T val;
+    JSONObj *o = *iter;
+    decode_json_obj(val, o);
+    l.insert(val);
+  }
+}
+
+template<class T>
 void decode_json_obj(vector<T>& l, JSONObj *obj)
 {
   l.clear();
@@ -347,6 +362,15 @@ static void encode_json(const char *name, const std::list<T>& l, ceph::Formatter
 {
   f->open_array_section(name);
   for (typename std::list<T>::const_iterator iter = l.begin(); iter != l.end(); ++iter) {
+    encode_json("obj", *iter, f);
+  }
+  f->close_section();
+}
+template<class T>
+static void encode_json(const char *name, const std::set<T>& l, ceph::Formatter *f)
+{
+  f->open_array_section(name);
+  for (typename std::set<T>::const_iterator iter = l.begin(); iter != l.end(); ++iter) {
     encode_json("obj", *iter, f);
   }
   f->close_section();
