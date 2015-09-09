4135d95efa1eef2c6446a1045e16c048a52b5ebf
Danny Al-Gaaf <danny.al-gaaf@bisect.de>
2015-09-09 16:54:07 +0200

    osdc/Objecter.cc: prefer ++operator for non-primitive iterators
    Signed-off-by: Danny Al-Gaaf <danny.al-gaaf@bisect.de>

--- src/osdc/Objecter.cc
+++ src/osdc/Objecter.cc
@@ -1019,7 +1019,7 @@ void Objecter::handle_osd_map(MOSDMap *m)
   bool was_pausewr = osdmap->test_flag(CEPH_OSDMAP_PAUSEWR) || cluster_full || _osdmap_has_pool_full();
   map<int64_t, bool> pool_full_map;
   for (map<int64_t, pg_pool_t>::const_iterator it = osdmap->get_pools().begin();
-       it != osdmap->get_pools().end(); it++)
+       it != osdmap->get_pools().end(); ++it)
     pool_full_map[it->first] = it->second.has_flag(pg_pool_t::FLAG_FULL);
 
   
@@ -2413,7 +2413,7 @@ bool Objecter::_osdmap_pool_full(const int64_t pool_id) const
 bool Objecter::_osdmap_has_pool_full() const
 {
   for (map<int64_t, pg_pool_t>::const_iterator it = osdmap->get_pools().begin();
-       it != osdmap->get_pools().end(); it++) {
+       it != osdmap->get_pools().end(); ++it) {
     if (it->second.has_flag(pg_pool_t::FLAG_FULL))
       return true;
   }
@@ -2432,7 +2432,7 @@ bool Objecter::_osdmap_full_flag() const
 void Objecter::update_pool_full_map(map<int64_t, bool>& pool_full_map)
 {
   for (map<int64_t, pg_pool_t>::const_iterator it = osdmap->get_pools().begin();
-       it != osdmap->get_pools().end(); it++) {
+       it != osdmap->get_pools().end(); ++it) {
     if (pool_full_map.find(it->first) == pool_full_map.end()) {
       pool_full_map[it->first] = it->second.has_flag(pg_pool_t::FLAG_FULL);
     } else {
