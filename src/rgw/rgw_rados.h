750074b2b6c1245a91b71e5361221434eb50b23b
Orit Wasserman <owasserm@redhat.com>
2015-09-09 10:40:01 +0200

    radosgw-admin: Add period list command
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -1677,7 +1677,7 @@ public:
   int list_regions(list<string>& regions);
   int list_zones(list<string>& zones);
   int list_realms(list<string>& realms);
-
+  int list_periods(list<string>& periods);
   void tick();
 
   CephContext *ctx() { return cct; }
