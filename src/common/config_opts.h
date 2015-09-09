f1100746e952ea39fa8220aebb1ac5b3c1927789
Yan, Zheng <zyan@redhat.com>
2015-09-09 14:14:33 +0800

    client: use faked inode number when sizeof(ino_t) < 8
    Cephfs uses 48-bits inode number. When ino_t is 32-bits, the upper 16
    bits of inode number gets lost for stat(2) and readdir(2).
    
    32-bits ino_t is even more problematic with ceph-fuse. In ceph-fuse
    case, we need to encode both cephfs inode number and snapid into
    fuse_ino_t. 32-bits are clearly not enough.
    
    The fix is assign a 32-bits faked inode number to each cached inode.
    Faked inode numbers are used for stat(2), readdir(2) and fuse. We
    maintain a data structure that maps faked inode number to cephfs's
    vinodeno_t. So we can find inode by the 32-bits faked inode number.
    This appoach should work as long as there are less than 4 billions
    cached inodes.
    
    Signed-off-by: Yan, Zheng <zyan@redhat.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -364,6 +364,7 @@ OPTION(fuse_multithreaded, OPT_BOOL, true)
 OPTION(client_try_dentry_invalidate, OPT_BOOL, true) // the client should try to use dentry invaldation instead of remounting, on kernels it believes that will work for
 OPTION(client_die_on_failed_remount, OPT_BOOL, true)
 OPTION(client_check_pool_perm, OPT_BOOL, true)
+OPTION(client_use_faked_inos, OPT_BOOL, false)
 
 OPTION(crush_location, OPT_STR, "")       // whitespace-separated list of key=value pairs describing crush location
 
