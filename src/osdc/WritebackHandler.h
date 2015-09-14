ac964259b4a1d90e1f87ce3f8108257a04726412
Adam C. Emerson <aemerson@redhat.com>
2015-09-14 12:19:50 -0400

    osdc: Whitespace
    Since I'm making a pervasive change anyway, knock off end-of-line spaces
    and wrap overly long lines.
    
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/osdc/WritebackHandler.h
+++ src/osdc/WritebackHandler.h
@@ -19,17 +19,19 @@ class WritebackHandler {
   /**
    * check if a given extent read result may change due to a write
    *
-   * Check if the content we see at the given read offset may change due to a write to
-   * this object.
+   * Check if the content we see at the given read offset may change
+   * due to a write to this object.
    *
    * @param oid object
    * @param read_off read offset
    * @param read_len read length
    * @param snapid read snapid
    */
-  virtual bool may_copy_on_write(const object_t& oid, uint64_t read_off, uint64_t read_len, snapid_t snapid) = 0;
+  virtual bool may_copy_on_write(const object_t& oid, uint64_t read_off,
+				 uint64_t read_len, snapid_t snapid) = 0;
   virtual ceph_tid_t write(const object_t& oid, const object_locator_t& oloc,
-			   uint64_t off, uint64_t len, const SnapContext& snapc,
+			   uint64_t off, uint64_t len,
+			   const SnapContext& snapc,
 			   const bufferlist &bl, utime_t mtime,
 			   uint64_t trunc_size, __u32 trunc_seq,
                            ceph_tid_t journal_tid, Context *oncommit) = 0;
