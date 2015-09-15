ea21ceaa5e649566e74c6be9b0ec8297fea20d88
Loic Dachary <ldachary@redhat.com>
2015-09-15 16:16:34 +0200

    buildpackages: pep8 compliance
    http://tracker.ceph.com/issues/13031 Fixes: #13031
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- tasks/buildpackages.py
+++ tasks/buildpackages.py
@@ -12,6 +12,7 @@ import urlparse
 
 log = logging.getLogger(__name__)
 
+
 @contextlib.contextmanager
 def task(ctx, config):
     """
@@ -49,7 +50,8 @@ def task(ctx, config):
         uri_reference = gitbuilder.uri_reference
         url = gitbuilder.base_url
         assert '/' + uri_reference in url, \
-            url + ' (from template ' + config.baseurl_template + ') does not contain /' + uri_reference
+            (url + ' (from template ' + config.baseurl_template +
+             ') does not contain /' + uri_reference)
         if 'ref/' in uri_reference:
             ref = os.path.basename(uri_reference)
         else:
@@ -63,7 +65,7 @@ def task(ctx, config):
             " CEPH_OS_VERSION=" + gitbuilder.os_version +
             " CEPH_DIST=" + gitbuilder.distro +
             " CEPH_ARCH=" + gitbuilder.arch +
-            " CEPH_SHA1=" + (sha1 or '')  +
+            " CEPH_SHA1=" + (sha1 or '') +
             " CEPH_TAG=" + (tag or '') +
             " CEPH_BRANCH=" + (branch or '') +
             " CEPH_REF=" + ref +
