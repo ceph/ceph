e38a6d69dfb25fae11902f537b6f2b752689236f
Loic Dachary <ldachary@redhat.com>
2015-09-15 18:43:06 +0200

    buildpackages: build the repository and the packages
    http://tracker.ceph.com/issues/13031 Fixes: #13031
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- tasks/buildpackages.py
+++ tasks/buildpackages.py
@@ -1,14 +1,14 @@
 """
 Build ceph packages
 """
-import contextlib
 import logging
 import os
 import subprocess
-from teuthology import contextutil, packaging
+from teuthology import packaging
 from teuthology import misc as teuthology
 from teuthology.config import config as teuth_config
 from teuthology.task import install
+from teuthology.openstack import OpenStack
 import urlparse
 
 log = logging.getLogger(__name__)
@@ -31,7 +31,6 @@ def get_install_config(ctx):
     log.debug('install config %s' % config)
     return config
 
-@contextlib.contextmanager
 def task(ctx, config):
     """
     Build Ceph packages. This task will automagically be run
@@ -63,6 +62,7 @@ def task(ctx, config):
         'task only accepts a dict for config not ' + str(config)
     d = os.path.join(os.path.dirname(__file__), 'buildpackages')
     install_config = get_install_config(ctx)
+    log.info('install_config ' + str(install_config))
     for remote in ctx.cluster.remotes.iterkeys():
         gitbuilder = install._get_gitbuilder_project(
             ctx, remote, install_config)
@@ -81,20 +81,29 @@ def task(ctx, config):
             ref = os.path.basename(uri_reference)
         else:
             ref = ''
-        url = urlparse.urlparse(url)
         subprocess.check_call(
-            "make -C " + d + " " + os.environ['HOME'] + "/.ssh-agent")
-        subprocess.check_call(
-            "source ~/.ssh-agent ; make -C " + d +
-            " CEPH_OS_TYPE=" + gitbuilder.os_type +
-            " CEPH_OS_VERSION=" + gitbuilder.os_version +
-            " CEPH_DIST=" + gitbuilder.distro +
-            " CEPH_ARCH=" + gitbuilder.arch +
-            " CEPH_SHA1=" + (sha1 or '') +
-            " CEPH_TAG=" + (tag or '') +
-            " CEPH_BRANCH=" + (branch or '') +
-            " CEPH_REF=" + ref +
-            " GITBUILDER_URL=" + url +
-            " packages-" + url.path.strip('/') +
-            " ", shell=True)
+            "make -C " + d + " " + os.environ['HOME'] + "/.ssh_agent",
+            shell=True)
+        target = os.path.dirname(urlparse.urlparse(url).path.strip('/'))
+        target = os.path.dirname(target) + '-' + sha1
+        openstack = OpenStack()
+        select = '^(vps|eg)-'
+        flavor = openstack.flavor(config['machine'], select)
+        cmd = (". " + os.environ['HOME'] + "/.ssh_agent ; make -C " + d +
+               " CEPH_GIT_BASE_URL=" + teuth_config.ceph_git_base_url + 
+               " CEPH_OS_TYPE=" + gitbuilder.os_type +
+               " CEPH_OS_VERSION=" + gitbuilder.os_version +
+               " CEPH_DIST=" + gitbuilder.distro +
+               " CEPH_ARCH=" + gitbuilder.arch +
+               " CEPH_SHA1=" + (sha1 or '')  +
+               " CEPH_TAG=" + (tag or '') +
+               " CEPH_BRANCH=" + (branch or '') +
+               " CEPH_REF=" + ref +
+               " GITBUILDER_URL=" + url +
+               " FLAVOR=" + flavor +
+               " " + target +
+               " ")
+        log.info("buildpackages: " + cmd)
+        subprocess.check_call(cmd, shell=True)
+    teuth_config.gitbuilder_host = openstack.get_ip('packages-repository', '')
     log.info('Finished buildpackages')
