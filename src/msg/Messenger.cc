de8da652b6c969aa0446f0d3351f63b012063c71
Josh Durgin <jdurgin@redhat.com>
2015-09-21 15:20:53 -0700

    msg: add simplified messenger constructor
    Several callers create messengers using exactly the same parameters:
    
    - reading the ms type from cct that is also passed in
    - a default entity_name_t::CLIENT
    - the default features
    
    Additionally, the nonce should be randomized and not depend on
    e.g. pid, as it does in several callers now. Clients running in
    containers can easily have pid collisions, leading to hangs, so
    randomize the nonce in this simplified constructor rather than
    duplicating that logic in every caller.
    
    Daemons have meaningful entity_name_ts, and monitors currently depend
    on using 0 as a nonce, so make this simple constructer
    client-specific.
    
    Related: #13032
    Signed-off-by: Josh Durgin <jdurgin@redhat.com>
    (cherry picked from commit e3785b0b5fbff870adbd5f121021b671912386cf)
    
    Conflicts:
    	src/msg/Messenger.cc
                - different #includes in firefly
                - different Messenger::create() arguments in firefly

--- src/msg/Messenger.cc
+++ src/msg/Messenger.cc
@@ -4,6 +4,13 @@
 
 #include "SimpleMessenger.h"
 
+Messenger *Messenger::create_client_messenger(CephContext *cct, string lname)
+{
+  uint64_t nonce = 0;
+  get_random_bytes((char*)&nonce, sizeof(nonce));
+  return Messenger::create(cct, entity_name_t::CLIENT(), lname, nonce);
+}
+
 Messenger *Messenger::create(CephContext *cct,
 			     entity_name_t name,
 			     string lname,
