4767a57fd62688203a0644f3bdc1b8e629d23533
Daniel Gryniewicz <dang@fprintf.net>
2015-09-21 11:08:19 -0400

    CMake - Add check for keyutils
    Signed-off-by: Daniel Gryniewicz <dang@fprintf.net>

---
+++ cmake/modules/Findkeyutils.cmake
@@ -0,0 +1,32 @@
+# Try to find Keyutils
+# Once done, this will define
+#
+# KEYUTILS_FOUND - system has Profiler
+# KEYUTILS_INCLUDE_DIR - the Profiler include directories
+# KEYUTILS_LIBRARIES - link these to use Profiler
+
+if(KEYUTILS_INCLUDE_DIR AND KEYUTILS_LIBRARIES)
+	set(KEYUTILS_FIND_QUIETLY TRUE)
+endif(KEYUTILS_INCLUDE_DIR AND KEYUTILS_LIBRARIES)
+
+INCLUDE(CheckCXXSymbolExists)
+
+# include dir
+
+find_path(KEYUTILS_INCLUDE_DIR keyutils.h NO_DEFAULT_PATH PATHS
+  /usr/include
+  /opt/local/include
+  /usr/local/include
+)
+
+
+# finally the library itself
+find_library(LIBKEYUTILS NAMES keyutils)
+set(KEYUTILS_LIBRARIES ${LIBKEYUTILS})
+
+# handle the QUIETLY and REQUIRED arguments and set KEYUTILS_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(keyutils DEFAULT_MSG KEYUTILS_LIBRARIES KEYUTILS_INCLUDE_DIR)
+
+mark_as_advanced(KEYUTILS_LIBRARIES KEYUTILS_INCLUDE_DIR)
