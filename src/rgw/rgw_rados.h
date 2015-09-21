4cb06cd8d31e214eb10edc68be14251ffefb55e5
Orit Wasserman <owasserm@redhat.com>
2015-09-21 16:21:57 +0200

    rgw: add realms to zonegroupmap
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -1129,8 +1129,8 @@ class RGWRealm;
 struct RGWZoneGroupMap {
 
   Mutex lock;
+  map<string, RGWRealm> realms;
   map<string, RGWPeriodMap> periods;
-
   RGWQuotaInfo bucket_quota;
   RGWQuotaInfo user_quota;
 
