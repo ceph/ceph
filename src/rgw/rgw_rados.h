ae7cb7d8926c0391cf2aa0b6d3e44dc0d0919421
Orit Wasserman <owasserm@redhat.com>
2015-09-21 17:32:06 +0200

    rgw: add zonegroup_map period update
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -1152,6 +1152,8 @@ struct RGWZoneGroupMap {
 
   int update(RGWZoneGroup& zonegroup);
   int update(RGWRealm& realm);
+  int update(CephContext *cct, RGWRados *store,
+	     const string& period_id);
   
   int get_master_zonegroup(const string& current_period,
 			   RGWZoneGroup& master_zonegroup);
@@ -1271,7 +1273,7 @@ class RGWPeriod
   epoch_t epoch;
   string predecessor_uuid;
   map<int, version_t> versions;
-
+  RGWPeriodMap period_map;
   string master_zone;
 
   string realm_id;
@@ -1300,6 +1302,7 @@ public:
   const string& get_predecessor() { return predecessor_uuid;}
   const string& get_master_zone() { return master_zone;}
   const string& get_realm() { return realm_id;}
+  const RGWPeriodMap& get_map() { return period_map;}
   const string& get_pool_name(CephContext *cct);
   const string& get_latest_epoch_oid();
   const string& get_info_oid_prefix();
