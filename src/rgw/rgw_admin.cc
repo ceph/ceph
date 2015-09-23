6aa9f5163802313cf8473641797790c71f75faaf
Orit Wasserman <owasserm@redhat.com>
2015-09-23 13:06:38 +0200

    radosgw-admin: add --rgw-zone for backward compatability
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -1656,6 +1656,8 @@ int main(int argc, char **argv)
       realm_id = val;
     } else if (ceph_argparse_witharg(args, i, &val, "--realm-new-name", (char*)NULL)) {
       realm_new_name = val;
+    } else if (ceph_argparse_witharg(args, i, &val, "--rgw-zone", (char*)NULL)) {
+      zone_name = val;
     } else if (ceph_argparse_witharg(args, i, &val, "--zonegroup-id", (char*)NULL)) {
       zonegroup_id = val;
     } else if (ceph_argparse_witharg(args, i, &val, "--zonegroup", (char*)NULL)) {
