e237d5a136f4e64022fa50de642a1a975d33ef71
Yehuda Sadeh <yehuda@redhat.com>
2015-09-24 14:56:52 -0700

    rgw: init shard_id param for bucket listing
    shard_id can now be specified in the bucket instance http param
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -1044,6 +1044,7 @@ struct req_state {
 
    string zonegroup_endpoint;
    string bucket_instance_id;
+   int bucket_instance_shard_id;
 
    RGWBucketInfo bucket_info;
    map<string, bufferlist> bucket_attrs;
