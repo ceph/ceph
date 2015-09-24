d4d65fbd6ad15320339751c604613ac63511e645
Sage Weil <sage@redhat.com>
2015-09-24 18:10:46 -0400

    qa: http://ceph.com/qa -> http://download.ceph.com/qa
    Signed-off-by: Sage Weil <sage@redhat.com>

--- qa/workunits/suites/blogbench.sh
+++ qa/workunits/suites/blogbench.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "getting blogbench"
-wget http://ceph.com/qa/blogbench-1.0.tar.bz2
+wget http://download.ceph.com/qa/blogbench-1.0.tar.bz2
 #cp /home/gregf/src/blogbench-1.0.tar.bz2 .
 tar -xvf blogbench-1.0.tar.bz2
 cd blogbench*
