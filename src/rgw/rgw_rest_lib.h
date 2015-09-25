9b1facaaa5cb2baf9a839b97f852b89240c4d669
Matt Benjamin <mbenjamin@redhat.com>
2015-09-25 14:27:09 -0400

    librgw: incremental rgw_rest_lib internals
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_rest_lib.h
+++ src/rgw/rgw_rest_lib.h
@@ -10,13 +10,38 @@ public:
   RGWRESTMgr_Lib() {}
   virtual ~RGWRESTMgr_Lib() {}
   virtual RGWHandler* get_handler(struct req_state* s);
-};
+}; /* RGWRESTMgr_Lib */
 
 class RGWHandler_ObjStore_Lib : public RGWHandler_User {
   friend class RGWRESTMgr_Lib;
 public:
   RGWHandler_ObjStore_Lib() {}
   virtual ~RGWHandler_ObjStore_Lib() {}
-};
+}; /* RGWHandler_ObjStore_Lib */
+
+class RGWListBuckets_ObjStore_Lib : public RGWListBuckets_ObjStore {
+public:
+  RGWListBuckets_ObjStore_Lib() {}
+  ~RGWListBuckets_ObjStore_Lib() {}
+
+  int get_params() {
+    limit = -1; /* no limit */
+    return 0;
+  }
+}; /* RGWListBuckets_ObjStore_Lib */
+
+class RGWListBucket_ObjStore_Lib : public RGWListBucket_ObjStore {
+public:
+  RGWListBucket_ObjStore_Lib() {
+    default_max = 1000;
+  }
+  ~RGWListBucket_ObjStore_Lib() {}
+}; /* RGWListBucket_ObjStore_Lib */
+
+class RGWGetBucketLogging_ObjStore_Lib : public RGWGetBucketLogging {
+public:
+  RGWGetBucketLogging_ObjStore_Lib() {}
+  ~RGWGetBucketLogging_ObjStore_Lib() {}
+}; /* RGWGetBucketLogging_ObjStore_Lib */
 
 #endif /* CEPH_RGW_REST_LIB_H */
