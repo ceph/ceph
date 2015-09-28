ea0ccbebc973cc1a45074900ca16b0ba7a8e02c8
David Zafman <dzafman@redhat.com>
2015-09-28 10:59:42 -0700

    osd: Don't crash if OI_ATTR attribute is missing or corrupt
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/osd/osd_types.h
+++ src/osd/osd_types.h
@@ -3143,6 +3143,10 @@ struct object_info_t {
   object_info_t(bufferlist& bl) {
     decode(bl);
   }
+  object_info_t operator=(bufferlist& bl) {
+    decode(bl);
+    return *this;
+  }
 };
 WRITE_CLASS_ENCODER(object_info_t)
 
