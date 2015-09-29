6330810182b3430abf471f890db86bdc4ca8dcf3
Jason Dillaman <dillaman@redhat.com>
2015-09-29 14:13:46 -0400

    lttng: move tracepoint probes to dynamic libraries
    LTTng-UST initializes itself at program load, which means it is
    currently always enabled.  This can lead to issues with SElinux
    and AppArmor which might restrict access to the necessary device
    files.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>
    (cherry picked from commit 4a5305e41677885d45b2b74299113a3d00189c66)
    
    Conflicts:
    	src/test/Makefile-client.am
    	src/tracing/Makefile.am

--- src/librbd/librbd.cc
+++ src/librbd/librbd.cc
@@ -36,7 +36,11 @@
 #include <vector>
 
 #ifdef WITH_LTTNG
+#define TRACEPOINT_DEFINE
+#define TRACEPOINT_PROBE_DYNAMIC_LINKAGE
 #include "tracing/librbd.h"
+#undef TRACEPOINT_PROBE_DYNAMIC_LINKAGE
+#undef TRACEPOINT_DEFINE
 #else
 #define tracepoint(...)
 #endif
