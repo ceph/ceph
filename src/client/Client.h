3739961e01ce7ef7a92fe4dd4424f9b4d60b2fad
Yan, Zheng <zyan@redhat.com>
2015-09-29 16:31:03 +0800

    client: permission check for setattr
    Signed-off-by: Yan, Zheng <zyan@redhat.com>

--- src/client/Client.h
+++ src/client/Client.h
@@ -783,6 +783,7 @@ private:
   };
 
   int inode_permission(Inode *in, uid_t uid, UserGroups& groups, unsigned want);
+  int may_setattr(Inode *in, struct stat *st, int mask, int uid=-1, int gid=-1);
   int may_open(Inode *in, int flags, int uid=-1, int gid=-1);
   int may_create(Inode *dir, int uid=-1, int gid=-1);
   int may_delete(Inode *dir, const char *name, int uid=-1, int gid=-1);
