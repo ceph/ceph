0a4fe99a087c54e4fc8163c8ff76000a14a4fa92
Matt Benjamin <mbenjamin@redhat.com>
2015-10-04 19:40:47 -0400

    librgw: untested RGWLibListBucketsRequest and caller.
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_file.h
+++ src/rgw/rgw_file.h
@@ -9,7 +9,7 @@
 /* internal header */
 
 /*
-  read directory content
+  read directory content (buckets)
 */
 
 class RGWListBucketsRequest : public RGWLibRequest,
@@ -67,5 +67,67 @@ public:
   }
 
 }; /* RGWListBucketsRequest */
+
+/*
+  read directory content (bucket objects)
+*/
+
+class RGWListBucketRequest : public RGWLibRequest,
+			     public RGWListBucket_ObjStore_Lib /* RGWOp */
+{
+public:
+  std::string user_id;
+  std::string& uri;
+  uint64_t* offset;
+  void* cb_arg;
+  rgw_readdir_cb rcb;
+
+  RGWListBucketRequest(CephContext* _cct, char *_user_id, std::string& _uri,
+		      rgw_readdir_cb _rcb, void* _cb_arg, uint64_t* _offset)
+    : RGWLibRequest(_cct), user_id(_user_id), uri(_uri), offset(_offset),
+      cb_arg(_cb_arg),
+      rcb(_rcb) {
+    magic = 72;
+    op = this;
+  }
+
+  virtual bool only_bucket() { return false; }
+
+  virtual int op_init() {
+    // assign store, s, and dialect_handler
+    RGWObjectCtx* rados_ctx
+      = static_cast<RGWObjectCtx*>(get_state()->obj_ctx);
+    // framework promises to call op_init after parent init
+    assert(rados_ctx);
+    RGWOp::init(rados_ctx->store, get_state(), this);
+    op = this; // assign self as op: REQUIRED
+    return 0;
+  }
+
+  virtual int header_init() {
+
+    struct req_state* s = get_state();
+    s->info.method = "GET";
+
+    /* XXX derp derp derp */
+    s->relative_uri = uri;
+    s->info.request_uri = uri; // XXX
+    s->info.effective_uri = uri;
+    s->info.request_params = "";
+    s->info.domain = ""; /* XXX ? */
+
+    /* XXX fake user_id (will fix) */
+    s->user.user_id = user_id;
+    s->user.display_name = user_id;
+
+    return 0;
+  }
+
+  int operator()(const std::string& name, const std::string& marker) {
+    rcb(name.c_str(), cb_arg, (*offset)++);
+    return 0;
+  }
+
+}; /* RGWListBucketRequest */
 
 #endif /* RGW_FILE_H */
