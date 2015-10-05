b03c716a5f2e1ece7d695ee266fe2c41522366e5
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2015-10-05 19:43:13 +0200

    rgw: add support for skipping manifest parsing during GET on Swift object.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -984,7 +984,7 @@ void RGWGetObj::execute()
     goto done_err;
 
   attr_iter = attrs.find(RGW_ATTR_USER_MANIFEST);
-  if (attr_iter != attrs.end()) {
+  if (attr_iter != attrs.end() && !skip_manifest) {
     ret = handle_user_manifest(attr_iter->second.c_str());
     if (ret < 0) {
       ldout(s->cct, 0) << "ERROR: failed to handle user manifest ret=" << ret << dendl;
