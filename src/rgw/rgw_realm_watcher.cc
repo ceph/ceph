31f0f7073c5e8b4e52862737e41c516fc3664371
Casey Bodley <cbodley@redhat.com>
2015-10-07 12:16:22 -0400

    rgw: refactor reconfigure out of RGWRealmWatcher
    add a RGWRealmReloader class to handle reconfiguration, and use a
    Watcher interface to get notifications from RGWRealmWatcher
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_realm_watcher.cc
+++ src/rgw/rgw_realm_watcher.cc
@@ -1,17 +1,10 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 
-#include <system_error>
-
 #include "common/errno.h"
 
 #include "rgw_realm_watcher.h"
 #include "rgw_rados.h"
-
-#include "rgw_bucket.h"
-#include "rgw_log.h"
-#include "rgw_rest.h"
-#include "rgw_user.h"
 
 #define dout_subsys ceph_subsys_rgw
 
@@ -19,128 +12,33 @@
 #define dout_prefix (*_dout << "rgw realm watcher: ")
 
 
-// safe callbacks from SafeTimer are unneccessary. reconfigure() can take a long
-// time, so we don't want to hold the mutex and block handle_notify() for the
-// duration
-static constexpr bool USE_SAFE_TIMER_CALLBACKS = false;
-
-
-RGWRealmWatcher::RGWRealmWatcher(CephContext *cct, RGWRados *&store,
-                                 FrontendPauser *frontends)
-  : cct(cct),
-    store(store),
-    frontends(frontends),
-    timer(cct, mutex, USE_SAFE_TIMER_CALLBACKS),
-    mutex("RGWRealmWatcher"),
-    reconfigure_scheduled(nullptr)
+RGWRealmWatcher::RGWRealmWatcher(CephContext* cct, RGWRealm& realm)
+  : cct(cct), watcher(nullptr)
 {
   // no default realm, nothing to watch
-  if (store->realm.get_id().empty()) {
+  if (realm.get_id().empty()) {
     ldout(cct, 4) << "No realm, disabling dynamic reconfiguration." << dendl;
     return;
   }
 
   // establish the watch on RGWRealm
-  int r = watch_start();
+  int r = watch_start(realm);
   if (r < 0) {
     lderr(cct) << "Failed to establish a watch on RGWRealm, "
         "disabling dynamic reconfiguration." << dendl;
     return;
   }
-
-  timer.init();
 }
 
 RGWRealmWatcher::~RGWRealmWatcher()
 {
   watch_stop();
-
-  Mutex::Locker lock(mutex);
-  timer.shutdown();
 }
 
-
-void RGWRealmWatcher::reconfigure()
+void RGWRealmWatcher::set_watcher(Watcher* watcher)
 {
-  ldout(cct, 1) << "Pausing frontends for realm update..." << dendl;
-
-  frontends->pause();
-
-  // destroy the existing store
-  RGWStoreManager::close_storage(store);
-  store = nullptr;
-
-  {
-    // allow a new notify to reschedule us. it's important that we do this
-    // before we start loading the new realm, or we could miss some updates
-    Mutex::Locker lock(mutex);
-    reconfigure_scheduled = nullptr;
-  }
-
-  while (!store) {
-    // recreate and initialize a new store
-    store = RGWStoreManager::get_storage(cct,
-                                         cct->_conf->rgw_enable_gc_threads,
-                                         cct->_conf->rgw_enable_quota_threads,
-                                         cct->_conf->rgw_run_sync_thread);
-
-    RGWRados *store_cleanup = nullptr;
-    {
-      Mutex::Locker lock(mutex);
-
-      // failure to recreate RGWRados is not a recoverable error, but we
-      // don't want to assert or abort the entire cluster.  instead, just
-      // sleep until we get another notification, and retry until we get
-      // a working configuration
-      if (store == nullptr) {
-        lderr(cct) << "Failed to reinitialize RGWRados after a realm "
-            "configuration update. Waiting for a new update." << dendl;
-
-        // sleep until another event is scheduled
-        while (!reconfigure_scheduled)
-          cond.Wait(mutex);
-
-        ldout(cct, 1) << "Woke up with a new configuration, retrying "
-            "RGWRados initialization." << dendl;
-      }
-
-      if (reconfigure_scheduled) {
-        // cancel the event; we'll handle it now
-        timer.cancel_event(reconfigure_scheduled);
-        reconfigure_scheduled = nullptr;
-
-        // if we successfully created a store, clean it up outside of the lock,
-        // then continue to loop and recreate another
-        std::swap(store, store_cleanup);
-      }
-    }
-
-    if (store_cleanup) {
-      ldout(cct, 4) << "Got another notification, restarting RGWRados "
-          "initialization." << dendl;
-
-      RGWStoreManager::close_storage(store_cleanup);
-    }
-  }
-
-  // finish initializing the new store
-  rgw_rest_init(cct, store->get_zonegroup());
-  rgw_user_init(store);
-  rgw_bucket_init(store->meta_mgr);
-  rgw_log_usage_init(cct, store);
-
-  ldout(cct, 1) << "Resuming frontends with new realm configuration." << dendl;
-
-  frontends->resume(store);
+  this->watcher = watcher;
 }
-
-
-class RGWRealmWatcher::C_Reconfigure : public Context {
-  RGWRealmWatcher *watcher;
- public:
-  C_Reconfigure(RGWRealmWatcher *watcher) : watcher(watcher) {}
-  void finish(int r) { watcher->reconfigure(); }
-};
 
 void RGWRealmWatcher::handle_notify(uint64_t notify_id, uint64_t cookie,
                                     uint64_t notifier_id, bufferlist& bl)
@@ -148,26 +46,12 @@ void RGWRealmWatcher::handle_notify(uint64_t notify_id, uint64_t cookie,
   if (cookie != watch_handle)
     return;
 
+  auto p = bl.begin();
+  watcher->handle_notify(p);
+
   // send an empty notify ack
-  bufferlist reply_bl;
-  pool_ctx.notify_ack(watch_oid, notify_id, cookie, reply_bl);
-
-  Mutex::Locker lock(mutex);
-  if (reconfigure_scheduled) {
-    ldout(cct, 4) << "Notification on " << watch_oid << ", reconfiguration "
-        "already scheduled" << dendl;
-    return;
-  }
-
-  reconfigure_scheduled = new C_Reconfigure(this);
-  cond.SignalOne(); // wake reconfigure() if it blocked on a bad configuration
-
-  // schedule reconfigure() with a delay so we can batch up changes
-  auto delay = cct->_conf->rgw_realm_reconfigure_delay;
-  timer.add_event_after(delay, reconfigure_scheduled);
-
-  ldout(cct, 4) << "Notification on " << watch_oid << ", reconfiguration "
-      "scheduled in " << delay << 's' << dendl;
+  bufferlist reply;
+  pool_ctx.notify_ack(watch_oid, notify_id, cookie, reply);
 }
 
 void RGWRealmWatcher::handle_error(uint64_t cookie, int err)
@@ -181,8 +65,7 @@ void RGWRealmWatcher::handle_error(uint64_t cookie, int err)
   }
 }
 
-
-int RGWRealmWatcher::watch_start()
+int RGWRealmWatcher::watch_start(RGWRealm& realm)
 {
   // initialize a Rados client
   int r = rados.init_with_context(cct);
@@ -199,7 +82,6 @@ int RGWRealmWatcher::watch_start()
   }
 
   // open an IoCtx for the realm's pool
-  auto& realm = store->realm;
   auto pool = realm.get_pool_name(cct);
   r = rados.ioctx_create(pool.c_str(), pool_ctx);
   if (r < 0) {
