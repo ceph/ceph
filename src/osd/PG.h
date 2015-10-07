14470ecdf53a967c55519e4b49979ec157029fe9
Guang Yang <yguang@yahoo-inc.com>
2015-10-07 04:34:34 +0000

    pg: add auto-repair for EC pool
    Fixes: #12754
    Signed-off-by: Guang Yang <yguang@yahoo-inc.com>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -1071,6 +1071,7 @@ public:
       active(false), queue_snap_trim(false),
       waiting_on(0), shallow_errors(0), deep_errors(0), fixed(0),
       must_scrub(false), must_deep_scrub(false), must_repair(false),
+      auto_repair(false),
       num_digest_updates_pending(0),
       state(INACTIVE),
       deep(false),
@@ -1098,6 +1099,9 @@ public:
 
     // flags to indicate explicitly requested scrubs (by admin)
     bool must_scrub, must_deep_scrub, must_repair;
+
+    // this flag indicates whether we would like to do auto-repair of the PG or not
+    bool auto_repair;
 
     // Maps from objects with errors to missing/inconsistent peers
     map<hobject_t, set<pg_shard_t>, hobject_t::BitwiseComparator> missing;
@@ -1187,6 +1191,7 @@ public:
       must_scrub = false;
       must_deep_scrub = false;
       must_repair = false;
+      auto_repair = false;
 
       state = PG::Scrubber::INACTIVE;
       start = hobject_t();
