c49d85b479ff0ce8f3092992355786580d77ed5e
Yehuda Sadeh <yehuda@redhat.com>
2015-10-08 16:43:05 -0700

    cls_rgw: reset dirent flag appropriately
    Need to mark dirent as versioned if it has instance.
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/cls/rgw/cls_rgw.cc
+++ src/cls/rgw/cls_rgw.cc
@@ -802,7 +802,7 @@ int rgw_bucket_complete_op(cls_method_context_t hctx, bufferlist *in, bufferlist
   }
 
   entry.index_ver = header.ver;
-  entry.flags = 0; /* resetting entry flags, entry might have been previously a delete marker */
+  entry.flags = (entry.key.instance.empty() ? 0 : RGW_BUCKET_DIRENT_FLAG_VER); /* resetting entry flags, entry might have been previously a delete marker */
 
   if (op.tag.size()) {
     map<string, struct rgw_bucket_pending_info>::iterator pinter = entry.pending_map.find(op.tag);
