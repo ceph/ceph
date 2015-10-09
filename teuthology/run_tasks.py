6b59975cd9661bc0f9cccec42f16096b79e6ba48
Dan Mick <dan.mick@redhat.com>
2015-10-08 18:22:25 -0700

    run_tasks.py: fix Sentry URL
    I don't know when or why it changed, but the existing URL format, which
    uses '/search?q=<id>', fails; what works, by observing the web UI's URL
    submission and by testing, is to omit the 'search' part of the path:
    '/?q=<id>'
    
    Signed-off-by: Dan Mick <dan.mick@redhat.com>

--- teuthology/run_tasks.py
+++ teuthology/run_tasks.py
@@ -96,7 +96,7 @@ def run_tasks(tasks, ctx):
                 tags=tags,
                 extra=extra,
             ))
-            event_url = "{server}/search?q={id}".format(
+            event_url = "{server}/?q={id}".format(
                 server=teuth_config.sentry_server.strip('/'), id=exc_id)
             log.exception(" Sentry event: %s" % event_url)
             ctx.summary['sentry_event'] = event_url
