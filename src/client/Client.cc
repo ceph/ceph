ff509b1e809e5be19557206aa4402d12aeed13c3
John Spray <john.spray@redhat.com>
2015-10-14 00:50:44 +0100

    client: a better check for MDS availability
    Requiring active daemons was pretty annoying in practice,
    because:
     * a client starting up in the middle of a failover event (e.g. a
     daemon was up and in replay, or a daemon was active+laggy but a
     standby was ready and waiting) would fail out instead of waiting
     for the cluster to progress.
     * a client starting up with a dead standby-less MDS would see and
     active rank and not care that it was actually laggy.
    
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -5035,14 +5035,16 @@ int Client::mount(const std::string &mount_root, bool require_mds)
   ldout(cct, 2) << "mounted: have mdsmap " << mdsmap->get_epoch() << dendl;
   if (require_mds) {
     while (1) {
-      if (mdsmap->get_epoch() > 0) {
-        if (mdsmap->get_num_mds(CEPH_MDS_STATE_ACTIVE) == 0) {
-          ldout(cct, 10) << "no mds up: epoch=" << mdsmap->get_epoch() << dendl;
-          return CEPH_FUSE_NO_MDS_UP;
-        } else {
-          break;
-        }
+      if (mdsmap->cluster_unavailable()) {
+        // If the cluster is stuck unavailable, error out
+        ldout(cct, 10) << "mds cluster unavailable: epoch=" << mdsmap->get_epoch() << dendl;
+        return CEPH_FUSE_NO_MDS_UP;
+      } else if (mdsmap->get_num_mds(CEPH_MDS_STATE_ACTIVE) > 0) {
+        // If somebody is active, continue to mount
+        break;
       } else {
+        // Else, wait.  MDSMonitor will update the map to bring
+        // us to a conclusion eventually.
         wait_on_list(waiting_for_mdsmap);
       }
     }
