d4f568274ab46a53cf8a8e562ffda038c63983ae
Yehuda Sadeh <yehuda@redhat.com>
2015-10-14 11:37:48 -0700

    rgw_admin: realm set-default -> realm default
    to be in line with the other default commands
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -91,7 +91,7 @@ void _usage()
   cerr << "  realm remove               remove a zonegroup from the realm\n";
   cerr << "  realm rename               rename a realm\n";
   cerr << "  realm set                  set realm info (requires infile)\n";
-  cerr << "  realm set-default          set realm as default\n";
+  cerr << "  realm default              set realm as default\n";
   cerr << "  zonegroup create           create a new zone group info\n";
   cerr << "  zonegroup default          set default zone group\n";
   cerr << "  zonegroup delete           delete a zone group info\n";
@@ -337,7 +337,7 @@ enum {
   OPT_REALM_REMOVE,
   OPT_REALM_RENAME,
   OPT_REALM_SET,
-  OPT_REALM_SET_DEFAULT,
+  OPT_REALM_DEFAULT,
   OPT_PERIOD_PREPARE,
   OPT_PERIOD_DELETE,
   OPT_PERIOD_GET,
@@ -541,8 +541,8 @@ static int get_cmd(const char *cmd, const char *prev_cmd, const char *prev_prev_
       return OPT_REALM_RENAME;
     if (strcmp(cmd, "set") == 0)
       return OPT_REALM_SET;
-    if (strcmp(cmd, "set-default") == 0)
-      return OPT_REALM_SET_DEFAULT;
+    if (strcmp(cmd, "default") == 0)
+      return OPT_REALM_DEFAULT;
   } else if (strcmp(prev_cmd, "zonegroup") == 0) {
     if (strcmp(cmd, "create")== 0)
       return OPT_ZONEGROUP_CREATE;
@@ -1749,7 +1749,7 @@ int main(int argc, char **argv)
 			 opt_cmd == OPT_REALM_LIST_PERIODS ||
 			 opt_cmd == OPT_REALM_GET_DEFAULT || opt_cmd == OPT_REALM_REMOVE ||
 			 opt_cmd == OPT_REALM_RENAME || opt_cmd == OPT_REALM_SET ||
-			 opt_cmd == OPT_REALM_SET_DEFAULT);
+			 opt_cmd == OPT_REALM_DEFAULT);
 
   if (raw_storage_op) {
     store = RGWStoreManager::get_raw_storage(g_ceph_context);
@@ -2152,7 +2152,7 @@ int main(int argc, char **argv)
       }
       break;
 
-    case OPT_REALM_SET_DEFAULT:
+    case OPT_REALM_DEFAULT:
       {
 	RGWRealm realm(realm_id, realm_name);
 	int ret = realm.init(g_ceph_context, store);
