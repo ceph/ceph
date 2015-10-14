f6fd37c4814403ec0f58660e6aa4540922ecb402
Yehuda Sadeh <yehuda@redhat.com>
2015-10-14 11:02:15 -0700

    rgw_admin: don't require zonegroup id on some commands
    should be able to get the default one
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -2170,7 +2170,7 @@ int main(int argc, char **argv)
     case OPT_ZONEGROUP_CREATE:
       {
 	if (zonegroup_name.empty()) {
-	  cerr << " Missing zonegroup name" << std::endl;
+	  cerr << "Missing zonegroup name" << std::endl;
 	  return -EINVAL;
 	}
 	RGWRealm realm(realm_id, realm_name);
@@ -2252,11 +2252,6 @@ int main(int argc, char **argv)
       break;
     case OPT_ZONEGROUP_GET:
       {
-	if (zonegroup_id.empty() && zonegroup_name.empty()) {
-	  cerr << "no zonegroup name or id provided" << std::endl;
-	  return -EINVAL;
-	}
-
 	RGWZoneGroup zonegroup(zonegroup_id, zonegroup_name);
 	int ret = zonegroup.init(g_ceph_context, store);
 	if (ret < 0) {
@@ -2299,11 +2294,6 @@ int main(int argc, char **argv)
       break;
     case OPT_ZONEGROUP_MODIFY:
       {
-	if (zonegroup_id.empty() && zonegroup_name.empty()) {
-	  cerr << "no zonegroup name or id provided" << std::endl;
-	  return -EINVAL;
-	}
-
 	RGWRealm realm(realm_id, realm_name);
 	int ret = realm.init(g_ceph_context, store);
 	if (ret < 0) {
