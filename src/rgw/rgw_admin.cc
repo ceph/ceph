9b74dcb3730edb4ae55276b5c22eef76b9c155d3
Orit Wasserman <owasserm@redhat.com>
2015-10-14 15:53:00 +0200

    radosgw-admin: check for valid input for zonegroup modify command
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -2298,6 +2298,11 @@ int main(int argc, char **argv)
       break;
     case OPT_ZONEGROUP_MODIFY:
       {
+	if (zonegroup_id.empty() && zonegroup_name.empty()) {
+	  cerr << "no zonegroup name or id provided" << std::endl;
+	  return -EINVAL;
+	}
+
 	RGWRealm realm(realm_id, realm_name);
 	int ret = realm.init(g_ceph_context, store);
 	if (ret < 0) {
