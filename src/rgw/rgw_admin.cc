a325dd5e46e0bf05e7e1a08ecaebadce0d9be3bb
Yehuda Sadeh <yehuda@redhat.com>
2015-10-14 14:32:19 -0700

    rgw: use --rgw-realm instead of --realm
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -184,7 +184,7 @@ void _usage()
   cerr << "   --master-url              master url\n";
   cerr << "   --master-zonegroup=<id>   master zonegroup id\n";
   cerr << "   --master-zone=<id>        master zone id\n";
-  cerr << "   --realm=<realm>           realm name\n";
+  cerr << "   --rgw-realm=<realm>       realm name\n";
   cerr << "   --realm-id=<realm id>     realm id\n";
   cerr << "   --realm-new-name=<realm new name> realm new name\n";
   cerr << "   --rgw-zonegroup=<zonegroup>   zonegroup name\n";
@@ -1675,8 +1675,6 @@ int main(int argc, char **argv)
       remote = val;
     } else if (ceph_argparse_witharg(args, i, &val, "--url", (char*)NULL)) {
       url = val;
-    } else if (ceph_argparse_witharg(args, i, &val, "--realm", (char*)NULL)) {
-      realm_name = val;
     } else if (ceph_argparse_witharg(args, i, &val, "--realm-id", (char*)NULL)) {
       realm_id = val;
     } else if (ceph_argparse_witharg(args, i, &val, "--realm-new-name", (char*)NULL)) {
@@ -1775,6 +1773,7 @@ int main(int argc, char **argv)
     return usage();
   }
 
+  realm_name = g_conf->rgw_realm;
   zone_name = g_conf->rgw_zone;
   zonegroup_name = g_conf->rgw_zonegroup;
 
@@ -1870,7 +1869,7 @@ int main(int argc, char **argv)
 	  epoch = atoi(period_epoch.c_str());
 	}
 	RGWPeriod period(period_id, epoch);
-	int ret = period.init(g_ceph_context, store);
+	int ret = period.init(g_ceph_context, store, realm_id, realm_name);
 	if (ret < 0) {
 	  cerr << "period init failed: " << cpp_strerror(-ret) << std::endl;
 	  return -ret;
@@ -1886,7 +1885,7 @@ int main(int argc, char **argv)
 	RGWRealm realm(realm_id, realm_name);
 	int ret = realm.init(g_ceph_context, store);
 	if (ret < 0 ) {
-	  cerr << "Error initing realm " << cpp_strerror(-ret) << std::endl;
+	  cerr << "Error initializing realm " << cpp_strerror(-ret) << std::endl;
 	  return ret;
 	}
 	string current_id = realm.get_current_period();
@@ -1909,7 +1908,7 @@ int main(int argc, char **argv)
 	RGWRealm realm(realm_id, realm_name);
 	int ret = realm.init(g_ceph_context, store);
 	if (ret < 0 ) {
-	  cerr << "Error initing realm " << cpp_strerror(-ret) << std::endl;
+	  cerr << "Error initializing realm " << cpp_strerror(-ret) << std::endl;
 	  return ret;
 	}
 	/* read latest sync data */
