cdaf9408a88d0bd249daee93d0a2f9fdec50809c
Yehuda Sadeh <yehuda@redhat.com>
2015-10-14 14:33:35 -0700

    rgw: period get does not require specifying period id
    Can return the current period
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -1837,10 +1837,6 @@ int main(int argc, char **argv)
       break;
     case OPT_PERIOD_GET:
       {
-	if (period_id.empty()) {
-	  cerr << "missing period id" << std::endl;
-	  return -EINVAL;
-	}
 	epoch_t epoch = 0;
 	if (!period_epoch.empty()) {
 	  epoch = atoi(period_epoch.c_str());
