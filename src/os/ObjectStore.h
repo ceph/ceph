7b9511b153e1746e91577b78a9c594b2d68ad6fb
Sage Weil <sage@redhat.com>
2015-10-19 14:37:04 -0400

    os/ObjectStore: helpers for validating map<string,string> and set<string> to bl
    Test/validate the encoding, and reference the resulting (encoded) data in
    a bufferlist.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/ObjectStore.h
+++ src/os/ObjectStore.h
@@ -84,6 +84,10 @@ static inline void encode(const map<string,bufferptr> *attrset, bufferlist &bl) 
   ::encode(*attrset, bl);
 }
 
+// this isn't the best place for these, but...
+void decode_str_str_map_to_bl(bufferlist::iterator& p, bufferlist *out);
+void decode_str_set_to_bl(bufferlist::iterator& p, bufferlist *out);
+
 // Flag bits
 typedef uint32_t osflagbits_t;
 const int SKIP_JOURNAL_REPLAY = 1 << 0;
@@ -836,8 +840,14 @@ public:
       void decode_attrset(map<string,bufferlist>& aset) {
         ::decode(aset, data_bl_p);
       }
+      void decode_attrset_bl(bufferlist *pbl) {
+	decode_str_str_map_to_bl(data_bl_p, pbl);
+      }
       void decode_keyset(set<string> &keys){
         ::decode(keys, data_bl_p);
+      }
+      void decode_keyset_bl(bufferlist *pbl){
+        decode_str_set_to_bl(data_bl_p, pbl);
       }
 
       const ghobject_t &get_oid(__le32 oid_id) {
