f24468d96758c4bd64cbe13212a40c7cd86292c4
Casey Bodley <cbodley@redhat.com>
2015-10-22 09:54:19 -0400

    rgw: add RGWPeriod::commit()
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -1381,6 +1381,9 @@ public:
   void fork();
   int update();
 
+  // commit a staging period; only for use on master zone
+  int commit(RGWRealm& realm, const RGWPeriod &current_period);
+
   void encode(bufferlist& bl) const {
     ENCODE_START(1, 1, bl);    
     ::encode(id, bl);
