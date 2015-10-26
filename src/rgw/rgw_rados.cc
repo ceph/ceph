1e60f59f506db2a9d7073e8171e18c946611317e
Orit Wasserman <owasserm@redhat.com>
2015-10-26 18:02:05 +0100

    rgw: update current_period after adding converted zonegroup
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -2997,6 +2997,7 @@ int RGWRados::replace_region_with_zonegroup()
     lderr(cct) << "failed to list regions: ret "<< ret << " " << cpp_strerror(-ret) << dendl;
     return ret;
   }
+
   list<string>::iterator iter;
   /* create zonegroups */
   for (iter = regions.begin(); iter != regions.end(); ++iter)
@@ -3056,6 +3057,11 @@ int RGWRados::replace_region_with_zonegroup()
 	ret = current_period.add_zonegroup(zonegroup);
 	if (ret < 0) {
 	  lderr(cct) << "failed to add zonegroup to current_period: " << cpp_strerror(-ret) << dendl;
+	  return ret;
+	}
+	ret = current_period.update();
+	if (ret < 0) {
+	  lderr(cct) << "failed to update current_period: " << cpp_strerror(-ret) << dendl;
 	  return ret;
 	}
       }
