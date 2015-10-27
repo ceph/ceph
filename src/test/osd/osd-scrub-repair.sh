f14144441bd2b78c5d71f41f26aae654743b1b12
Loic Dachary <ldachary@redhat.com>
2015-10-27 12:48:23 +0900

    tests: port uniqueness reminder
    When copy/pasting a tests, it is easy to forget (or not know) that the
    port used must be unique to allow for multiple tests to run in
    parallel (make -j8). Add a reminder next to each port.
    
    Signed-off-by: Loic Dachary <ldachary@redhat.com>

--- src/test/osd/osd-scrub-repair.sh
+++ src/test/osd/osd-scrub-repair.sh
@@ -20,7 +20,7 @@ function run() {
     local dir=$1
     shift
 
-    export CEPH_MON="127.0.0.1:7107"
+    export CEPH_MON="127.0.0.1:7107" # git grep '\<7107\>' : there must be only one
     export CEPH_ARGS
     CEPH_ARGS+="--fsid=$(uuidgen) --auth-supported=none "
     CEPH_ARGS+="--mon-host=$CEPH_MON "
