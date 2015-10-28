52e601b34730c5d67472851e202f5850cea4bada
Yehuda Sadeh <yehuda@redhat.com>
2015-10-28 14:03:00 -0700

    rgw: multiple fixes and adjustments following aws4 code review
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_rest_s3.h
+++ src/rgw/rgw_rest_s3.h
@@ -391,7 +391,7 @@ private:
   static int authorize_v2(RGWRados *store, struct req_state *s);
   static int authorize_v4(RGWRados *store, struct req_state *s);
   static int authorize_v4_complete(RGWRados *store, struct req_state *s,
-                                   string request_payload, bool unsigned_payload);
+                                   const string& request_payload, bool unsigned_payload);
 
 };
 
