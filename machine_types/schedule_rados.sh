90c1b5292b6c7352103b46c582cbaf5d5ddc5880
Yuri Weinstein <yweinste@redhat.com>
2015-11-02 08:01:50 -0800

    Added $4 for filter out "random.yaml\,async.yaml" tests for now
    Signed-off-by: Yuri Weinstein <yweinste@redhat.com>
    (cherry picked from commit 7af99f1ac3f2e103303d688c5ba0f7fd64433314)

--- machine_types/schedule_rados.sh
+++ machine_types/schedule_rados.sh
@@ -3,5 +3,6 @@
 # $1 - part
 # $2 - branch name
 # $3 - machine name
+# $4 - filter out
 
-teuthology-suite -v -c $2 -m $3 -k distro -s rados --subset $(echo "(($(date +%U) % 2) * 7) + $1" | bc)/14
+teuthology-suite -v -c $2 -m $3 -k distro -s rados --subset $(echo "(($(date +%U) % 2) * 7) + $1" | bc)/14 $4
