1d22f8f82491e37f502152bdfe7783890c9cca37
Marcus Watts <mwatts@redhat.com>
2015-11-05 05:01:13 -0500

    rgw/ more get rid of static data in headers.
    move rgw_http_error_to_errno from header to rgw_rest_client.cc,
    the only place it's used.
    move rgw_http_status_code and http_codes to rgw_rest.cc,
    the only place it's used.
    
    Signed-off-by: Marcus Watts <mwatts@redhat.com>

--- src/rgw/rgw_rest.cc
+++ src/rgw/rgw_rest.cc
@@ -27,6 +27,54 @@
 #define dout_subsys ceph_subsys_rgw
 
 
+struct rgw_http_status_code {
+  int code;
+  const char *name;
+};
+
+const static struct rgw_http_status_code http_codes[] = {
+  { 100, "Continue" },
+  { 200, "OK" },
+  { 201, "Created" },
+  { 202, "Accepted" },
+  { 204, "No Content" },
+  { 205, "Reset Content" },
+  { 206, "Partial Content" },
+  { 207, "Multi Status" },
+  { 208, "Already Reported" },
+  { 300, "Multiple Choices" },
+  { 301, "Moved Permanently" },
+  { 302, "Found" },
+  { 303, "See Other" },
+  { 304, "Not Modified" },
+  { 305, "User Proxy" },
+  { 306, "Switch Proxy" },
+  { 307, "Temporary Redirect" },
+  { 308, "Permanent Redirect" },
+  { 400, "Bad Request" },
+  { 401, "Unauthorized" },
+  { 402, "Payment Required" },
+  { 403, "Forbidden" },
+  { 404, "Not Found" },
+  { 405, "Method Not Allowed" },
+  { 406, "Not Acceptable" },
+  { 407, "Proxy Authentication Required" },
+  { 408, "Request Timeout" },
+  { 409, "Conflict" },
+  { 410, "Gone" },
+  { 411, "Length Required" },
+  { 412, "Precondition Failed" },
+  { 413, "Request Entity Too Large" },
+  { 414, "Request-URI Too Long" },
+  { 415, "Unsupported Media Type" },
+  { 416, "Requested Range Not Satisfiable" },
+  { 417, "Expectation Failed" },
+  { 422, "Unprocessable Entity" },
+  { 500, "Internal Server Error" },
+  { 501, "Not Implemented" },
+  { 0, NULL },
+};
+
 struct rgw_http_attr {
   const char *rgw_attr;
   const char *http_attr;
