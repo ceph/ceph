fb595a72a6e820fb6ce39e21a88ede0188d1e68e
Javier M. Mellid <jmunhoz@igalia.com>
2015-11-11 17:02:35 +0100

    rgw: AWS4 authorization must be completed
    Signed-off-by: Javier M. Mellid <jmunhoz@igalia.com>

--- src/rgw/rgw_op.h
+++ src/rgw/rgw_op.h
@@ -108,6 +108,7 @@ public:
   virtual void execute() = 0;
   virtual void send_response() {}
   virtual void complete() {
+    assert(!s->aws4_auth_needs_complete);
     send_response();
   }
   virtual const string name() = 0;
