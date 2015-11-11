82f4c28e367ccf9a74323ca58b99e7b14ce13d15
Wido den Hollander <wido@42on.com>
2015-11-11 19:01:15 +0100

    rgw: Do not send a Content-Length header on status 204
    RFC7230 says:
      A server MUST NOT send a Content-Length header field in any response with a
      status code of 1xx (Informational) or 204 (No Content).
    
    Fixes: #13582

--- src/rgw/rgw_fcgi.h
+++ src/rgw/rgw_fcgi.h
@@ -13,6 +13,9 @@ struct FCGX_Request;
 class RGWFCGX : public RGWClientIO
 {
   FCGX_Request *fcgx;
+
+  int status_num;
+
 protected:
   void init_env(CephContext *cct);
   int write_data(const char *buf, int len);
