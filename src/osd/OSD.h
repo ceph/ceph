9a14ff7eebf9eea898f4ac48187893cae931a720
Xiaoxi Chen <xiaoxi.chen@intel.com>
2015-11-16 16:47:13 +0800

    osd/OSD.cc: shutdown after flapping certain times
    OSD stauts may flapping due to some hardware/network issue.
    Although we tried our best to self healthing but still in some case
    the OSD is still flipping and require admin to operate.
    
    This patch try another approach that shutdown the OSD after being
    marked down certain times(flapping), thus speed up the convergence of cluster.
    
    Signed-off-by: Xiaoxi Chen <xiaoxi.chen@intel.com>

--- src/osd/OSD.h
+++ src/osd/OSD.h
@@ -1822,6 +1822,7 @@ private:
   utime_t         had_map_since;
   RWLock          map_lock;
   list<OpRequestRef>  waiting_for_osdmap;
+  deque<utime_t> osd_markdown_log;
 
   friend struct send_map_on_destruct;
 
