6f94bde44500cc4592ac9a842cbb150b8cabf96b
Jason Dillaman <dillaman@redhat.com>
2015-11-18 14:07:51 -0500

    librbd: ImageWatcher shouldn't block the notification thread
    Now that all RPC operations are asynchronous, blocking the notification
    thread will also result in librados async callbacks becoming blocked
    (since they use the same thread).
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/ImageWatcher.h
+++ src/librbd/ImageWatcher.h
@@ -181,6 +181,25 @@ private:
     ProgressContext *m_prog_ctx;
   };
 
+  struct C_NotifyAck : public Context {
+    ImageWatcher *image_watcher;
+    uint64_t notify_id;
+    uint64_t handle;
+    bufferlist out;
+
+    C_NotifyAck(ImageWatcher *image_watcher, uint64_t notify_id,
+                uint64_t handle);
+    virtual void finish(int r);
+  };
+
+  struct C_ResponseMessage : public Context {
+    C_NotifyAck *notify_ack;
+
+    C_ResponseMessage(C_NotifyAck *notify_ack) : notify_ack(notify_ack) {
+    }
+    virtual void finish(int r);
+  };
+
   struct HandlePayloadVisitor : public boost::static_visitor<void> {
     ImageWatcher *image_watcher;
     uint64_t notify_id;
@@ -192,17 +211,13 @@ private:
     {
     }
 
-    inline void operator()(const watch_notify::HeaderUpdatePayload &payload) const {
-      bufferlist out;
-      image_watcher->handle_payload(payload, &out);
-      image_watcher->acknowledge_notify(notify_id, handle, out);
-    }
-
     template <typename Payload>
     inline void operator()(const Payload &payload) const {
-      bufferlist out;
-      image_watcher->handle_payload(payload, &out);
-      image_watcher->acknowledge_notify(notify_id, handle, out);
+      C_NotifyAck *ctx = new C_NotifyAck(image_watcher, notify_id,
+                                                handle);
+      if (image_watcher->handle_payload(payload, ctx)) {
+        ctx->complete(0);
+      }
     }
   };
 
@@ -272,38 +287,38 @@ private:
                             bool* new_request, Context** ctx,
                             ProgressContext** prog_ctx);
 
-  void handle_payload(const watch_notify::HeaderUpdatePayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::AcquiredLockPayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::ReleasedLockPayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::RequestLockPayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::AsyncProgressPayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::AsyncCompletePayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::FlattenPayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::ResizePayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::SnapCreatePayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::SnapRenamePayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::SnapRemovePayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::SnapProtectPayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::SnapUnprotectPayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::RebuildObjectMapPayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::RenamePayload& payload,
-                      bufferlist *out);
-  void handle_payload(const watch_notify::UnknownPayload& payload,
-                      bufferlist *out);
+  bool handle_payload(const watch_notify::HeaderUpdatePayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::AcquiredLockPayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::ReleasedLockPayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::RequestLockPayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::AsyncProgressPayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::AsyncCompletePayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::FlattenPayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::ResizePayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::SnapCreatePayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::SnapRenamePayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::SnapRemovePayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::SnapProtectPayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::SnapUnprotectPayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::RebuildObjectMapPayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::RenamePayload& payload,
+                      C_NotifyAck *ctx);
+  bool handle_payload(const watch_notify::UnknownPayload& payload,
+                      C_NotifyAck *ctx);
 
   void handle_notify(uint64_t notify_id, uint64_t handle, bufferlist &bl);
   void handle_error(uint64_t cookie, int err);
