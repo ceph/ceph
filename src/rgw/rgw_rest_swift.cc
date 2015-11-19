1eeb8d84113ba246d16bc5c32c921c934cc88ce6
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2015-11-19 17:45:04 +0100

    rgw: add support for "end_marker" on listing Swift account.
    Fixes: #10682
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_rest_swift.cc
+++ src/rgw/rgw_rest_swift.cc
@@ -17,6 +17,7 @@
 int RGWListBuckets_ObjStore_SWIFT::get_params()
 {
   marker = s->info.args.get("marker");
+  end_marker = s->info.args.get("end_marker");
 
   string limit_str = s->info.args.get("limit");
   if (!limit_str.empty()) {
