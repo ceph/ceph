a14753e6bf87d8b7c5b66fd9df02bd7a540286d7
Loic Dachary <ldachary@redhat.com>
2015-11-19 11:53:07 +0100

    tests: teuthology-* --invalid-option is a sure fail
    Providing the INVALID argument may succeed, depending on how the script
    interprets arguments.
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- scripts/test/script.py
+++ scripts/test/script.py
@@ -11,6 +11,6 @@ class Script(object):
         assert out.startswith('usage')
 
     def test_invalid(self):
-        args = (self.script_name, 'INVALID')
+        args = (self.script_name, '--invalid-option')
         with raises(subprocess.CalledProcessError):
             subprocess.check_call(args)
