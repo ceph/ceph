d5eb9d5b4fd109c41e4d84216cf1a49dc9fd5c46
Yan, Zheng <zyan@redhat.com>
2015-11-20 12:13:16 +0800

    mds: repair fragstat/rstat errors in dirfrag
    Signed-off-by: Yan, Zheng <zyan@redhat.com>

--- src/common/ceph_strings.cc
+++ src/common/ceph_strings.cc
@@ -137,6 +137,7 @@ const char *ceph_mds_op_name(int op)
 	case CEPH_MDS_OP_EXPORTDIR: return "exportdir";
 	case CEPH_MDS_OP_VALIDATE: return "validate_path";
 	case CEPH_MDS_OP_FLUSH: return "flush_path";
+	case CEPH_MDS_OP_REPAIR_FRAGSTATS: return "repair_fragstats";
 	}
 	return "???";
 }
