ceph/ceph

commit 573d2cc6dc52158bcdb00f0c001518603b9bfed6

WBThrottle:Draw a common func beyond_limit which whether beyond limit.
Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

src/os/WBThrottle.cc
@@ -135,10 +135,7 @@ bool WBThrottle::get_next_should_flush(
 {
   assert(lock.is_locked());
   assert(next);
-  while (!stopping &&
-         cur_ios < io_limits.first &&
-         pending_wbs.size() < fd_limits.first &&
-         cur_size < size_limits.first)
+  while (!stopping && !beyond_limit())
          cond.Wait(lock);
   if (stopping)
     return false;
@@ -212,9 +209,7 @@ void WBThrottle::queue_wb(
 
   wbiter->second.first.add(nocache, len, 1);
   insert_object(hoid);
-  if (!(cur_ios < io_limits.first &&
-      pending_wbs.size() < fd_limits.first &&
-      cur_size < size_limits.first))
+  if (beyond_limit())
     cond.Signal();
 }
 
@@ -267,10 +262,7 @@ void WBThrottle::clear_object(const ghobject_t &hoid)
 void WBThrottle::throttle()
 {
   Mutex::Locker l(lock);
-  while (!stopping && !(
-	   cur_ios < io_limits.second &&
-	   pending_wbs.size() < fd_limits.second &&
-	   cur_size < size_limits.second)) {
+  while (!stopping && beyond_limit()) {
     cond.Wait(lock);
   }
 }

src/os/WBThrottle.h
@@ -130,6 +130,15 @@ private:
   FS fs;
 
   void set_from_conf();
+  bool beyond_limit() const {
+    if (cur_ios < io_limits.first &&
+	pending_wbs.size() < fd_limits.first &&
+	cur_size < size_limits.first)
+      return false;
+    else
+      return true;
+  }
+
 public:
   WBThrottle(CephContext *cct);
   ~WBThrottle();

commit 010fa494bbcff52b38b5e142df952b981799f329

WBThrottle: fix incorrect throttle
This bug is introduced in commit 573d2cc, that throttle()
incorrectly use the "start flusher" throttle value to block new
write requests. Fix this issue by reverting changes on throttle().

On a test environment of 2 storage nodes with 7 HDD each, the
overall write performance measured by iozone is doubled with this
patch (boosted from 160MB/s to 300MB/s).

Signed-off-by: Zhang Huan <zhanghuan@ict.ac.cn>

src/os/WBThrottle.cc
@@ -262,7 +262,10 @@ void WBThrottle::clear_object(const ghobject_t &hoid)
 void WBThrottle::throttle()
 {
   Mutex::Locker l(lock);
-  while (!stopping && beyond_limit()) {
+  while (!stopping && !(
+	   cur_ios < io_limits.second &&
+	   pending_wbs.size() < fd_limits.second &&
+	   cur_size < size_limits.second)) {
     cond.Wait(lock);
   }
 }