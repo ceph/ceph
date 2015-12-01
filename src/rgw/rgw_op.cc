5c89711d83acd08b90effbbe415c8c5ab9375b38
Yehuda Sadeh <yehuda@redhat.com>
2015-12-01 13:35:15 -0800

    rgw: forward bucket creation removal if it's not meta master
    We can be in the master zonegroup, but not the meta master -- in which case
    we still need to forward the request to the master.
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -1452,7 +1452,7 @@ void RGWCreateBucket::execute()
   rgw_bucket *pmaster_bucket;
   time_t creation_time;
 
-  if (!store->get_zonegroup().is_master) {
+  if (!store->is_meta_master()) {
     JSONParser jp;
     ret = forward_request_to_master(s, NULL, store, in_data, &jp);
     if (ret < 0)
@@ -1595,7 +1595,7 @@ void RGWDeleteBucket::execute()
     return;
   }
 
-  if (!store->get_zonegroup().is_master) {
+  if (!store->is_meta_master()) {
     bufferlist in_data;
     JSONParser jp;
     ret = forward_request_to_master(s, &ot.read_version, store, in_data, &jp);
