8946cb63402c1fecbb0ab0dbd3dfa87fedb02599
Ilya Dryomov <idryomov@gmail.com>
2015-12-01 15:07:48 +0100

    cmake: define STRERROR_R_CHAR_P for GNU-specific strerror_r
    This is what autotools does with AC_FUNC_STRERROR_R bit.  It takes us
    from
    
        $ monmaptool --print nonexistant
        monmaptool: monmap file nonexistant
        monmaptool: couldn't open nonexistant: (2)
    
    to
    
        $ monmaptool --print nonexistant
        monmaptool: monmap file nonexistant
        monmaptool: couldn't open nonexistant: (2) No such file or directory
    
    on cmake - otherwise we end up using GNU-specific version as if it was
    the POSIX version.
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- src/include/config-h.in.cmake
+++ src/include/config-h.in.cmake
@@ -176,6 +176,9 @@
 /* Define to 1 if you have the <utime.h> header file. */
 #cmakedefine HAVE_UTIME_H
 
+/* Define to 1 if strerror_r returns char *. */
+#cmakedefine STRERROR_R_CHAR_P 1
+
 /* Define to the sub-directory in which libtool stores uninstalled libraries.
    */
 #cmakedefine LT_OBJDIR
