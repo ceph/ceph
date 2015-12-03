9aa85e74a6233b64f1c38177d2ab25350a6becd0
Ning Yao <zay11022@gmail.com>
2015-12-03 14:44:25 +0800

    os: FileStore improve map usage in _setattrs map insertion and search are cpu costy operation on intel i5-4570 loop insertion for map 100k times will cost about 10ms with -O2 optimazation: for (int i=0; i < 100000; i++) {   map<string, A> mymap;   mymap.insert(pair<string,A>("test", A())); } so we need to use map carefully and reduce "insert, count, find" operations
    Signed-off-by: Ning Yao <zay11022@gmail.com>

--- src/os/FileStore.cc
+++ src/os/FileStore.cc
@@ -4179,7 +4179,7 @@ int FileStore::_setattrs(coll_t cid, const ghobject_t& oid, map<string,bufferptr
   map<string, bufferlist> omap_set;
   set<string> omap_remove;
   map<string, bufferptr> inline_set;
-  map<string, bufferptr> inline_to_set;
+  map<string,bufferptr>::iterator p;
   FDRef fd;
   int spill_out = -1;
   bool incomplete_inline = false;
@@ -4202,16 +4202,16 @@ int FileStore::_setattrs(coll_t cid, const ghobject_t& oid, map<string,bufferptr
   dout(15) << "setattrs " << cid << "/" << oid
     	   << (incomplete_inline ? " (incomplete_inline, forcing omap)" : "")
 	   << dendl;
-
-  for (map<string,bufferptr>::iterator p = aset.begin();
-       p != aset.end();
-       ++p) {
+ 
+  p = aset.begin();
+  while (p != aset.end()) {
     char n[CHAIN_XATTR_MAX_NAME_LEN];
     get_attrname(p->first.c_str(), n, CHAIN_XATTR_MAX_NAME_LEN);
 
     if (incomplete_inline) {
       chain_fremovexattr(**fd, n); // ignore any error
       omap_set[p->first].push_back(p->second);
+      aset.erase(p++);
       continue;
     }
 
@@ -4223,18 +4223,22 @@ int FileStore::_setattrs(coll_t cid, const ghobject_t& oid, map<string,bufferptr
 	    goto out_close;
 	}
 	omap_set[p->first].push_back(p->second);
+	aset.erase(p++);
 	continue;
     }
 
-    if (!inline_set.count(p->first) &&
-	  inline_set.size() >= m_filestore_max_inline_xattrs) {
+    if (!inline_set.count(p->first)) {
+      inline_set.insert(*p);
+      if(inline_set.size() >= m_filestore_max_inline_xattrs) {
 	omap_set[p->first].push_back(p->second);
+	aset.erase(p++);
 	continue;
+      }
     }
-    omap_remove.insert(p->first);
-    inline_set.insert(*p);
 
-    inline_to_set.insert(*p);
+    if (spill_out)
+      omap_remove.insert(p->first);
+    ++p;
   }
 
   if (spill_out != 1 && !omap_set.empty()) {
@@ -4242,7 +4246,7 @@ int FileStore::_setattrs(coll_t cid, const ghobject_t& oid, map<string,bufferptr
 		    sizeof(XATTR_SPILL_OUT));
   }
 
-  r = _fsetattrs(**fd, inline_to_set);
+  r = _fsetattrs(**fd, aset);
   if (r < 0)
     goto out_close;
 
