b04fc891a4b45f5ab3fbdb0cd87a70eb918d1dec
Dunrong Huang <dunrong.huang@eayun.com>
2015-12-04 18:48:56 +0800

    rgw: fix wrong checking for JSONParser::parse() return
    The type of return value from JSONParser::parse() is bool, it never returns
    a negative value.
    
    Signed-off-by: Dunrong Huang <dunrong.huang@eayun.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -695,10 +695,9 @@ static int read_decode_json(const string& infile, T& t)
     return ret;
   }
   JSONParser p;
-  ret = p.parse(bl.c_str(), bl.length());
-  if (ret < 0) {
+  if (!p.parse(bl.c_str(), bl.length())) {
     cout << "failed to parse JSON" << std::endl;
-    return ret;
+    return -EINVAL;
   }
 
   try {
@@ -720,10 +719,9 @@ static int read_decode_json(const string& infile, T& t, K *k)
     return ret;
   }
   JSONParser p;
-  ret = p.parse(bl.c_str(), bl.length());
-  if (ret < 0) {
+  if (!p.parse(bl.c_str(), bl.length())) {
     cout << "failed to parse JSON" << std::endl;
-    return ret;
+    return -EINVAL;
   }
 
   try {
