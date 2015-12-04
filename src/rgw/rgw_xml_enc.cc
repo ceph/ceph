278fc5fd14ae8244537fc01f8050f451c80025db
Robin H. Johnson <robin.johnson@dreamhost.com>
2015-12-03 16:46:37 -0800

    Copyright: add copyright blocks to new files, and update files with existing ones for website changes.
    Signed-off-by: Robin H. Johnson <robin.johnson@dreamhost.com>

--- src/rgw/rgw_xml_enc.cc
+++ src/rgw/rgw_xml_enc.cc
@@ -1,6 +1,17 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
-
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2015 Yehuda Sadeh <yehuda@redhat.com>
+ * Copyright (C) 2015 Robin H. Johnson <robin.johnson@dreamhost.com>
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software 
+ * Foundation.  See file COPYING.
+ * 
+ */
 #include "rgw_common.h"
 #include "rgw_xml.h"
 
