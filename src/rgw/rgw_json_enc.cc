184f26b21031f17b17fcaa078a4e9f53cb21c13c
Orit Wasserman <owasserm@redhat.com>
2015-12-08 11:26:00 +0100

    rgw: cleanup remove realm unused master_zonegroup and zonegroups
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- src/rgw/rgw_json_enc.cc
+++ src/rgw/rgw_json_enc.cc
@@ -1028,8 +1028,6 @@ void RGWDataChangesLogInfo::decode_json(JSONObj *obj)
 void RGWRealm::dump(Formatter *f) const
 {
   RGWSystemMetaObj::dump(f);
-  encode_json("master_zonegroup", master_zonegroup, f);
-  encode_json_map("zonegroups", zonegroups, f);
   encode_json("current_period", current_period, f);
 }
 
@@ -1037,8 +1035,6 @@ void RGWRealm::dump(Formatter *f) const
 void RGWRealm::decode_json(JSONObj *obj)
 {
   RGWSystemMetaObj::decode_json(obj);
-  JSONDecoder::decode_json("master_zonegroup", master_zonegroup, obj);
-  JSONDecoder::decode_json("zonegroups", zonegroups, decode_zonegroups, obj);
   JSONDecoder::decode_json("current_period", current_period, obj);
 }
 
