698423957cd32ca87280ddadcd2c72343960a125
Sage Weil <sage@redhat.com>
2015-12-14 21:02:52 -0500

    kv/LevelDBStore: log leveldb to ceph log
    This greatly eases debugging, and avoids the unbounded growth of
    the leveldb LOG file.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/kv/LevelDBStore.h
+++ src/kv/LevelDBStore.h
@@ -45,12 +45,17 @@ enum {
   l_leveldb_last,
 };
 
+extern leveldb::Logger *create_leveldb_ceph_logger();
+
+class CephLevelDBLogger;
+
 /**
  * Uses LevelDB to implement the KeyValueDB interface
  */
 class LevelDBStore : public KeyValueDB {
   CephContext *cct;
   PerfCounters *logger;
+  CephLevelDBLogger *ceph_logger;
   string path;
   boost::scoped_ptr<leveldb::Cache> db_cache;
 #ifdef HAVE_LEVELDB_FILTER_POLICY
@@ -148,6 +153,7 @@ public:
   LevelDBStore(CephContext *c, const string &path) :
     cct(c),
     logger(NULL),
+    ceph_logger(NULL),
     path(path),
     db_cache(NULL),
 #ifdef HAVE_LEVELDB_FILTER_POLICY
