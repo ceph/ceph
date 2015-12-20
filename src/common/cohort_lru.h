3453938c254979546698204a6d0800e37ba31d7f
Matt Benjamin <mbenjamin@redhat.com>
2015-12-19 20:27:31 -0500

    librgw: fix initialization of string_ref from temporary
    Found by valgrind.  The good news:  amounts to proof that the
    sref notation works.
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/common/cohort_lru.h
+++ src/common/cohort_lru.h
@@ -324,6 +324,7 @@ namespace cohort {
 	  if (csz) {
 	    p.csz = csz;
 	    p.cache = (T**) ::operator new(csz * sizeof(T*));
+	    memset(p.cache, 0, csz * sizeof(T*));
 	  }
 	  locks.push_back(&p.lock);
 	}
