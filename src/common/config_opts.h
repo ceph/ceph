0748de6d1845d733206668fd77777459585633a5
Sage Weil <sage@redhat.com>
2015-12-22 14:05:35 -0500

    os/bluestore/StupidAllocator: bluestore_debug_small_allocations
    Force small allocations for debugging purposes.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -878,6 +878,7 @@ OPTION(bluestore_o_direct, OPT_BOOL, true)
 OPTION(bluestore_clone_cow, OPT_BOOL, true)  // do copy-on-write for clones
 OPTION(bluestore_debug_misc, OPT_BOOL, false)
 OPTION(bluestore_debug_no_reuse_blocks, OPT_BOOL, false)
+OPTION(bluestore_debug_small_allocations, OPT_INT, 0)
 
 OPTION(kstore_max_ops, OPT_U64, 512)
 OPTION(kstore_max_bytes, OPT_U64, 64*1024*1024)
