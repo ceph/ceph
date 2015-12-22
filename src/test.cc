12f65dbb61518e002e9f307c1b3bb71e2a2e8167
J. Eric Ivancich <ivancich@redhat.com>
2015-12-22 14:40:58 -0500

    Protect output w mutex since under clang output was being mixed.

--- src/test.cc
+++ src/test.cc
@@ -23,9 +23,16 @@ TestServer* testServer;
 
 typedef std::unique_ptr<TestRequest> TestRequestRef;
 
+std::mutex cout_mtx;
+typedef typename std::lock_guard<std::mutex> Guard;
+
+
 
 dmc::ClientInfo getClientInfo(int c) {
-  std::cout << "getClientInfo called" << std::endl;
+  {
+    Guard g(cout_mtx);
+    std::cout << "getClientInfo called" << std::endl;
+  }
 
   static dmc::ClientInfo info[] = {
     {1.0, 100.0, 250.0},
@@ -52,12 +59,13 @@ void handleReq(std::unique_ptr<TestRequest>&& request_ref,
   std::unique_ptr<TestRequest> req(std::move(request_ref));
   int client = req->client;
   uint32_t op = req->op;
-  
+
   testServer->post(0.1,
 		   [=] {
 		     callback();
-		     std:: cout << "finished " << client << " / " << op <<
-		       std::endl;
+		     Guard g(cout_mtx);
+		     std::cout << "finished " << client << " / " <<
+		       op << std::endl;
 		   });
 }
 
@@ -72,27 +80,31 @@ int main(int argc, char* argv[]) {
   auto f3 = std::function<void(std::unique_ptr<TestRequest>&&,
 			       std::function<void()>)>(handleReq);
 
-  testServer = new TestServer(5);
+testServer = new TestServer(5);
 
-  dmc::PriorityQueue<int,TestRequest> priorityQueue(f1, f2, f3);
+dmc::PriorityQueue<int,TestRequest> priorityQueue(f1, f2, f3);
 
-  std::cout << "queue created" << std::endl;
+std::cout << "queue created" << std::endl;
 
-  for (uint32_t i = 0; i < 1000; ++i) {
-    static uint32_t op = 12;
-    int client = i % 4;
-    uint32_t epoch = i / 4;
-    priorityQueue.addRequest(TestRequest(client, epoch, op),
-			     client,
-			     dmc::getTime());
-  }
+for (uint32_t i = 0; i < 1000; ++i) {
+  static uint32_t op = 12;
+  int client = i % 4;
+  uint32_t epoch = i / 4;
+  priorityQueue.addRequest(TestRequest(client, epoch, op),
+			   client,
+			   dmc::getTime());
+}
 
+{
+  Guard g(cout_mtx);
   std::cout << "request added" << std::endl;
+}
 
-  priorityQueue.markAsIdle(1);
+priorityQueue.markAsIdle(1);
 
-  sleep(60);
-  delete testServer;
+sleep(10);
+delete testServer;
 
-  std::cout << "done" << std::endl;
+Guard g(cout_mtx);
+std::cout << "done" << std::endl;
 }
