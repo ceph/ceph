31a162935799f972a5f1e7ab2f3dd8318c4d332b
xinxin shu <xinxin.shu@intel.com>
2015-12-24 15:19:00 +0800

    include : cleanup compare functions
    Signed-off-by: xinxin shu <xinxin.shu@intel.com>

--- src/include/cmp.h
+++ src/include/cmp.h
@@ -92,34 +92,6 @@
     return l.a != r.a || l.b != r.b || l.c != r.c || l.d != r.d;	\
   }
 
-#define WRITE_CMP_OPERATORS_4(type, a, b, c, d)				\
-  inline bool operator>(const type &l, const type &r) {			\
-    return l.a > r.a ||							\
-      (l.a == r.a && (l.b > r.b ||					\
-		      (l.b == r.b && (l.c > r.c ||			\
-				      (l.c == r.c && (l.d > r.d))))));	\
-  }									\
-  inline bool operator<(const type &l, const type &r) {			\
-    return l.a < r.a ||							\
-      (l.a == r.a && (l.b < r.b ||					\
-		      (l.b == r.b && (l.c < r.c ||			\
-				      (l.c == r.c && (l.d < r.d))))));	\
-  }									\
-  inline bool operator>=(const type &l, const type &r) {		\
-    return l.a > r.a ||							\
-      (l.a == r.a && (l.b > r.b ||					\
-		      (l.b == r.b && (l.c > r.c ||			\
-				      (l.c == r.c && (l.d >= r.d))))));	\
-  }									\
-  inline bool operator<=(const type &l, const type &r) {		\
-    return l.a < r.a ||							\
-      (l.a == r.a && (l.b < r.b ||					\
-		      (l.b == r.b && (l.c < r.c ||			\
-				      (l.c == r.c && (l.d <= r.d)))))); \
-  }
-
-
-
 #define WRITE_EQ_OPERATORS_5(type, a, b, c, d, e)			\
   inline bool operator==(const type &l, const type &r) {		\
     return l.a == r.a && l.b == r.b && l.c == r.c && l.d == r.d && l.e == r.e; \
@@ -128,78 +100,11 @@
     return l.a != r.a || l.b != r.b || l.c != r.c || l.d != r.d || l.e != r.e; \
   }
 
-#define WRITE_CMP_OPERATORS_5(type, a, b, c, d, e)			\
-  inline bool operator>(const type &l, const type &r) {			\
-    return l.a > r.a ||							\
-      (l.a == r.a && (l.b > r.b ||					\
-		      (l.b == r.b && (l.c > r.c ||			\
-				      (l.c == r.c && (l.d > r.d ||	\
-						      (l.d == r.d && l.e > r.e))))))); \
-  }									\
-  inline bool operator<(const type &l, const type &r) {			\
-    return l.a < r.a ||							\
-      (l.a == r.a && (l.b < r.b ||					\
-		      (l.b == r.b && (l.c < r.c ||			\
-				      (l.c == r.c && (l.d < r.d ||	\
-						      (l.d == r.d && (l.e < r.e)))))))); \
-  }									\
-  inline bool operator>=(const type &l, const type &r) {		\
-    return l.a > r.a ||							\
-      (l.a == r.a && (l.b > r.b ||					\
-		      (l.b == r.b && (l.c > r.c ||			\
-				      (l.c == r.c && (l.d > r.d ||	\
-						      (l.d == r.d && l.e >= r.e))))))); \
-  }									\
-  inline bool operator<=(const type &l, const type &r) {		\
-    return l.a < r.a ||							\
-      (l.a == r.a && (l.b < r.b ||					\
-		      (l.b == r.b && (l.c < r.c ||			\
-				      (l.c == r.c && (l.d < r.d ||	\
-						      (l.d == r.d && l.e <= r.e))))))); \
-  }
-
 #define WRITE_EQ_OPERATORS_7(type, a, b, c, d, e, f, g)			\
   inline bool operator==(const type &l, const type &r) {		\
     return l.a == r.a && l.b == r.b && l.c == r.c && l.d == r.d && l.e == r.e && l.f == r.f && l.g == r.g; \
   }									\
   inline bool operator!=(const type &l, const type &r) {		\
     return l.a != r.a || l.b != r.b || l.c != r.c || l.d != r.d || l.e != r.e || l.f != r.f || l.g != r.g; \
-  }
-#define WRITE_CMP_OPERATORS_7(type, a, b, c, d, e, f, g)		\
-  inline bool operator<=(const type &l, const type &r) {		\
-    return l.a < r.a ||							\
-      (l.a == r.a && (l.b < r.b ||					\
-		      (l.b == r.b && (l.c < r.c ||			\
-				      (l.c == r.c && (l.d < r.d ||	\
-						      (l.d == r.d && (l.e < r.e || \
-								      (l.e == r.e && (l.f < r.f || \
-										      (l.f == r.f && l.g <= r.g))))))))))); \
-  }									\
-  inline bool operator>=(const type &l, const type &r) {		\
-    return l.a > r.a ||							\
-      (l.a == r.a && (l.b > r.b ||					\
-		      (l.b == r.b && (l.c > r.c ||			\
-				      (l.c == r.c && (l.d > r.d ||	\
-						      (l.d == r.d && (l.e > r.e || \
-								      (l.e == r.e && (l.f > r.f || \
-										      (l.f == r.f && l.g >= r.g))))))))))); \
-  }									\
-  inline bool operator>(const type &l, const type &r) {			\
-    return l.a > r.a ||							\
-      (l.a == r.a && (l.b > r.b ||					\
-		      (l.b == r.b && (l.c > r.c ||			\
-				      (l.c == r.c && (l.d > r.d ||	\
-						      (l.d == r.d && (l.e > r.e || \
-								      (l.e == r.e && (l.f > r.f || \
-										      (l.f == r.f && l.g > r.g))))))))))); \
-  }									\
-  inline bool operator<(const type &l, const type &r) {			\
-    return l.a < r.a ||							\
-      (l.a == r.a && (l.b < r.b ||					\
-		      (l.b == r.b && (l.c < r.c ||			\
-				      (l.c == r.c && (l.d < r.d ||	\
-						      (l.d == r.d && (l.e < r.e || \
-								      (l.e == r.e && (l.f < r.f || \
-										      (l.f == r.f && l.g < r.g))))))))))); \
   }
 #endif
