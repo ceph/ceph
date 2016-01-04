96914bb056b0711ca26bf594a3e4e5f732bc6691
Matt Benjamin <mbenjamin@redhat.com>
2016-01-04 17:00:42 -0500

    Fix copyright statements on new test driver files (Red Hat, Inc.)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/test/librgw_file_gp.cc
+++ src/test/librgw_file_gp.cc
@@ -3,7 +3,7 @@
 /*
  * Ceph - scalable distributed file system
  *
- * Copyright (C) 2015 New Dream Network
+ * Copyright (C) 2015 Red Hat, Inc.
  *
  * This is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
