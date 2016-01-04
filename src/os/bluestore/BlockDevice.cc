016b663cb76065759f872ac2c0e9d643c2a7bc61
Haomai Wang <haomai@xsky.com>
2016-01-04 22:30:05 +0800

    BlockDevice: Fix compile error
    Signed-off-by: Haomai Wang <haomai@xsky.com>

--- src/os/bluestore/BlockDevice.cc
+++ src/os/bluestore/BlockDevice.cc
@@ -19,6 +19,27 @@
 #include "NVMEDevice.h"
 #endif
 
+#include "common/debug.h"
+
+#define dout_subsys ceph_subsys_bdev
+#undef dout_prefix
+#define dout_prefix *_dout << "bdev "
+
+void IOContext::aio_wait()
+{
+  Mutex::Locker l(lock);
+  // see _aio_thread for waker logic
+  num_waiting.inc();
+  while (num_running.read() > 0 || num_reading.read() > 0) {
+    dout(10) << __func__ << " " << this
+	     << " waiting for " << num_running.read() << " aios and/or "
+	     << num_reading.read() << " readers to complete" << dendl;
+    cond.Wait(lock);
+  }
+  num_waiting.dec();
+  dout(20) << __func__ << " " << this << " done" << dendl;
+}
+
 BlockDevice *BlockDevice::create(const string& type, aio_callback_t cb, void *cbpriv)
 {
   if (type == "kernel") {
