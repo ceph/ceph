1f05011bd2622e46305b323fbb6d085281d0c1bc
xie xingguo <xie.xingguo@zte.com.cn>
2016-01-06 16:50:46 +0800

    BlueStore: remove unused local variable "empty"
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -6161,7 +6161,6 @@ int BlueStore::_remove_collection(TransContext *txc, coll_t cid,
 {
   dout(15) << __func__ << " " << cid << dendl;
   int r;
-  bufferlist empty;
 
   {
     RWLock::WLocker l(coll_lock);
