a84d3be6942eba5bba08318057868bdbe09db8cb
Sage Weil <sage@redhat.com>
2016-01-06 17:56:12 -0500

    osd: osd_objectstore_fuse
    Expose underlying ObjectStore via fuse at $osd_data/fuse/.  Can be enabled/
    disabled at runtime.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -833,6 +833,7 @@ OPTION(osd_objectstore_tracing, OPT_BOOL, false) // true if LTTng-UST tracepoint
 // Override maintaining compatibility with older OSDs
 // Set to true for testing.  Users should NOT set this.
 OPTION(osd_debug_override_acting_compat, OPT_BOOL, false)
+OPTION(osd_objectstore_fuse, OPT_BOOL, false)
 
 OPTION(osd_bench_small_size_max_iops, OPT_U32, 100) // 100 IOPS
 OPTION(osd_bench_large_size_max_throughput, OPT_U64, 100 << 20) // 100 MB/s
