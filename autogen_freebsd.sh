b761213767b25d2905e9afe593671f26d24a3549
Willem Jan Withagen <wjw@digiware.nl>
2016-01-07 02:29:26 +0100

    FreeBSD: Cleanup FreeBSD specific autogen file
    Signed-off-by: Willem Jan Withagen <wjw@digiware.nl>

--- autogen_freebsd.sh
+++ autogen_freebsd.sh
@@ -3,12 +3,11 @@ FREEBSD_CONFIGURE_FLAGS=
 if [ "$(uname)" = FreeBSD ]; then
     CC=clang
     CXX=clang++
-    #CC=gcc48
-    #CXX=g++48
+    CWARN=""
     CLANGWARN="-Wno-unused-local-typedef -Wno-mismatched-tags -Wno-macro-redefined -Wno-unused-function -Wno-unused-label -Wno-undefined-bool-conversion -Wno-unused-private-field -Wno-unused-local-typedef -Wno-uninitialized -Wno-gnu-designator -Wno-inconsistent-missing-override -Wno-deprecated-declarations"
-    CFLAGS="-I/usr/local/include ${CLANGWARN}"
-    CXXFLAGS="-DGTEST_USE_OWN_TR1_TUPLE -I/usr/local/include ${CLANGWARN}"
-    LDFLAGS="${LDFLAGS} -L/usr/local/lib -export-dynamic -luuid"
+    CFLAGS="-g -I/usr/local/include ${CWARN} ${CLANGWARN}"
+    CXXFLAGS="-g -DGTEST_USE_OWN_TR1_TUPLE=1 -I/usr/local/include ${CWARN} ${CLANGWARN}"
+    LDFLAGS="-g ${LDFLAGS} -L/usr/local/lib -export-dynamic -luuid"
     FREEBSD_CONFIGURE_FLAGS="
       --disable-silent-rules
       --with-debug
@@ -24,9 +23,9 @@ if [ "$(uname)" = FreeBSD ]; then
       --without-tcmalloc
       --without-libaio
       --without-libxfs
-      --with-libzfs=no
       --without-fuse
       --without-lttng
+      --with-libzfs=no
         "
 #      --with-cephfs
 #      --without-radosgw
