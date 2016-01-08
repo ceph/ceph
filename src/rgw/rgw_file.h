1251608f490e10a2d908537479a65b8d3dea3cdb
Matt Benjamin <mbenjamin@redhat.com>
2016-01-08 12:40:48 -0500

    librgw: conditionally send '.' and '..' (rgw_readdir)
    Ganesha traditionally does not expect them, but they may be needed
    when bypassing the MDCACHE (2.4).
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_file.h
+++ src/rgw/rgw_file.h
@@ -417,7 +417,8 @@ namespace rgw {
       return EPERM;
     }
 
-    int readdir(rgw_readdir_cb rcb, void *cb_arg, uint64_t *offset, bool *eof);
+    int readdir(rgw_readdir_cb rcb, void *cb_arg, uint64_t *offset, bool *eof,
+		uint32_t flags);
     int write(uint64_t off, size_t len, size_t *nbytes, void *buffer);
     int write_finish();
     int close();
