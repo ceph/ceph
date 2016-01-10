93818cdbdaf58f2987e191d033e2e0b7e55ac470
Willem Jan Withagen <wjw@digiware.nl>
2016-01-10 22:37:35 +0100

    Revert "FreeBSD: More static_cast fixes"
    This reverts commit c69a0604aca2a2fed117a54320831a5f081558bf.
    
    This is actually a pointer <> long int conversion....
    Which GCC doesn't liek to be statisc

--- src/test/librbd/test_mock_fixture.h
+++ src/test/librbd/test_mock_fixture.h
@@ -24,8 +24,7 @@ ACTION_P(CopyInBufferlist, str) {
 }
 
 ACTION_P2(CompleteContext, r, wq) {
-  // WjW Static <> reinterped
-  ContextWQ *context_wq = static_cast<ContextWQ *>(wq);
+  ContextWQ *context_wq = reinterpret_cast<ContextWQ *>(wq);
   if (context_wq != NULL) {
     context_wq->queue(arg0, r);
   } else {
