0b369e1aff14d17536566d62a60def418be37963
Sage Weil <sage@redhat.com>
2016-01-12 13:14:03 -0500

    rados: allow both object size and op size to be specified
    Instead of assuming 1 op per object, let you specify an op/block size *and*
    and object size.  If the object size isn't specified, it is assumed to
    be equal to the op size (legacy behavior).
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/tools/rados/rados.cc
+++ src/tools/rados/rados.cc
@@ -27,6 +27,7 @@ using namespace libradosstriper;
 #include "common/errno.h"
 #include "common/Formatter.h"
 #include "common/obj_bencher.h"
+#include "include/stringify.h"
 #include "mds/inode_backtrace.h"
 #include "auth/Crypto.h"
 #include <iostream>
@@ -159,6 +160,8 @@ void usage(ostream& out)
 "        select target pool by name\n"
 "   -b op_size\n"
 "        set the block size for put/get ops and for write benchmarking\n"
+"   -o object_size\n"
+"        set the object size for put/get ops and for write benchmarking\n"
 "   -s name\n"
 "   --snap name\n"
 "        select given snap name for (read) IO\n"
@@ -864,25 +867,29 @@ protected:
     completions[slot] = 0;
   }
 
-  int aio_read(const std::string& oid, int slot, bufferlist *pbl, size_t len) {
+  int aio_read(const std::string& oid, int slot, bufferlist *pbl, size_t len,
+	       size_t offset) {
     return io_ctx.aio_read(oid, completions[slot], pbl, len, 0);
   }
 
-  int aio_write(const std::string& oid, int slot, bufferlist& bl, size_t len) {
+  int aio_write(const std::string& oid, int slot, bufferlist& bl, size_t len,
+		size_t offset) {
     librados::ObjectWriteOperation op;
 
     if (write_destination & OP_WRITE_DEST_OBJ) {
-      op.write(0, bl);
+      op.write(offset, bl);
     }
 
     if (write_destination & OP_WRITE_DEST_OMAP) {
       std::map<std::string, librados::bufferlist> omap;
-      omap["bench-omap-key"] = bl;
+      omap[string("bench-omap-key-") + stringify(offset)] = bl;
       op.omap_set(omap);
     }
 
     if (write_destination & OP_WRITE_DEST_XATTR) {
-      op.setxattr("bench-xattr-key", bl);
+      char key[80];
+      snprintf(key, sizeof(key), "bench-xattr-key-%d", (int)offset);
+      op.setxattr(key, bl);
     }
 
     return io_ctx.aio_operate(oid, completions[slot], &op);
@@ -1214,6 +1221,7 @@ static int rados_tool_common(const std::map < std::string, std::string > &opts,
   string oloc, target_oloc, nspace, target_nspace;
   int concurrent_ios = 16;
   unsigned op_size = default_op_size;
+  unsigned object_size = 0;
   bool block_size_specified = false;
   int bench_write_dest = 0;
   bool cleanup = true;
@@ -1294,6 +1302,13 @@ static int rados_tool_common(const std::map < std::string, std::string > &opts,
   i = opts.find("block-size");
   if (i != opts.end()) {
     if (rados_sistrtoll(i, &op_size)) {
+      return -EINVAL;
+    }
+    block_size_specified = true;
+  }
+  i = opts.find("object-size");
+  if (i != opts.end()) {
+    if (rados_sistrtoll(i, &object_size)) {
       return -EINVAL;
     }
     block_size_specified = true;
@@ -2518,8 +2533,11 @@ static int rados_tool_common(const std::map < std::string, std::string > &opts,
         outstream = &cout;
       bencher.set_outstream(*outstream);
     }
+    if (!object_size)
+      object_size = op_size;
     ret = bencher.aio_bench(operation, seconds,
-			    concurrent_ios, op_size, cleanup, run_name, no_verify);
+			    concurrent_ios, op_size, object_size,
+			    cleanup, run_name, no_verify);
     if (ret != 0)
       cerr << "error during benchmark: " << ret << std::endl;
     if (formatter && output)
@@ -2996,6 +3014,10 @@ int main(int argc, const char **argv)
       opts["block-size"] = val;
     } else if (ceph_argparse_witharg(args, i, &val, "-b", (char*)NULL)) {
       opts["block-size"] = val;
+    } else if (ceph_argparse_witharg(args, i, &val, "--object-size", (char*)NULL)) {
+      opts["object-size"] = val;
+    } else if (ceph_argparse_witharg(args, i, &val, "-o", (char*)NULL)) {
+      opts["object-size"] = val;
     } else if (ceph_argparse_witharg(args, i, &val, "-s", "--snap", (char*)NULL)) {
       opts["snap"] = val;
     } else if (ceph_argparse_witharg(args, i, &val, "-S", "--snapid", (char*)NULL)) {
