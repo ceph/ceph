e98d7601745bd8603242eb2d60d3ed934e87bcf3
Sage Weil <sage@redhat.com>
2016-01-13 14:53:37 -0500

    os/bluestore/FreelistManager: use btree_map instead of map
    btree_map is about twice as memory efficient.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/bluestore/FreelistManager.h
+++ src/os/bluestore/FreelistManager.h
@@ -10,12 +10,17 @@
 #include <ostream>
 #include "kv/KeyValueDB.h"
 
+#include "include/cpp-btree/btree_map.h"
+
 class FreelistManager {
   std::string prefix;
   std::mutex lock;
   uint64_t total_free;
 
-  std::map<uint64_t, uint64_t> kv_free;    ///< mirrors our kv values in the db
+  typedef btree::btree_map<uint64_t,uint64_t> map_t;
+  static const bool map_t_has_stable_iterators = false;
+
+  map_t kv_free;    ///< mirrors our kv values in the db
 
   void _audit();
   void _dump();
@@ -35,7 +40,7 @@ public:
     return total_free;
   }
 
-  const std::map<uint64_t,uint64_t>& get_freelist() {
+  const map_t& get_freelist() {
     return kv_free;
   }
 
