d815f57afb5902f6854506c99a3c3e4eea8b9625
Mykola Golub <mgolub@mirantis.com>
2016-01-13 12:47:58 +0200

    rbd-nbd: correctly set exit status if map command failed
    - parent_wait() returns child exit status so test for "!= 0";
    - do_map() is expected to return a negative value on failure.
    
    Signed-off-by: Mykola Golub <mgolub@mirantis.com>

--- src/tools/rbd_nbd/rbd-nbd.cc
+++ src/tools/rbd_nbd/rbd-nbd.cc
@@ -486,15 +486,15 @@ static int do_map()
 
   if (global_init_prefork(g_ceph_context) >= 0) {
     std::string err;
-    if (forker.prefork(err) < 0) {
+    r = forker.prefork(err);
+    if (r < 0) {
       cerr << err << std::endl;
-      return EXIT_FAILURE;
+      return r;
     }
 
     if (forker.is_parent()) {
-      if (forker.parent_wait(err) < 0) {
-	cerr << err << std::endl;
-	return EXIT_FAILURE;
+      if (forker.parent_wait(err) != 0) {
+	return -ENXIO;
       }
       return 0;
     }
