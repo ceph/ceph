a76b70834e7ee5e85ddc099e20037ac84ab8212d
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-01-15 15:16:40 +0100

    rgw: improve support for Swift's URL schema with account name inside.
    Fixes: #11163
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_swift_auth.cc
+++ src/rgw/rgw_swift_auth.cc
@@ -213,6 +213,9 @@ void RGW_SWIFT_Auth_Get::execute()
   if (!g_conf->rgw_swift_tenant_name.empty()) {
     tenant_path = "/AUTH_";
     tenant_path.append(g_conf->rgw_swift_tenant_name);
+  } else if (g_conf->rgw_swift_account_in_url) {
+    tenant_path = "/AUTH_";
+    tenant_path.append(user_str);
   }
 
   STREAM_IO(s)->print("X-Storage-Url: %s/%s/v1%s\r\n", swift_url.c_str(),
