57f8d10465112ea18fdf380b1a5f9c2b18ae55d4
Willem Jan Withagen <wjw@digiware.nl>
2016-01-15 01:34:38 +0100

    FreeBSD More automake tool commits.
    -  Mainly removal of -ldl lib inclusion, since it already goes with
       the default path
    -  Install the required dependancies the FreeBSD way
    -  Organise of building files which have FreeBSD specfic code.
    	The actual files will be in a different pull request
    
    Signed-off-by: Willem Jan Withagen <wjw@digiware.nl>

--- do_freebsd.sh
+++ do_freebsd.sh
@@ -3,9 +3,9 @@
 	# sudo ./install-deps.sh
 	. ./autogen_freebsd.sh
 	./autogen.sh \
-	&& ./do_configure \
-	&& gmake -j8 ENABLE_GIT_VERSION=OFF | tee gmake.log \
+	&& ./configure ${CONFIGURE_FLAGS} \
 	&& ( cd src/gmock/gtest; patch < /usr/ports/devel/googletest/files/patch-bsd-defines ) \
+	&& gmake -j8 ENABLE_GIT_VERSION=OFF | tee gmake.log \
 	&& gmake check ENABLE_GIT_VERSION=OFF CEPH_BUFFER_NO_BENCH=yes | tee gmake_check.log
 
 
