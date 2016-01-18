d797a5b8f9982b630d8f14e24536fb57089acc25
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-01-18 12:18:03 +0100

    rgw: rework handling anonymous user.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -1653,7 +1653,7 @@ int RGWGetBucketLocation::verify_permission()
 
 int RGWCreateBucket::verify_permission()
 {
-  if (!rgw_user_is_authenticated(s->user)) {
+  if (!rgw_user_is_authenticated(s->auth_user)) {
     return -EACCES;
   }
 
@@ -2618,7 +2618,7 @@ int RGWPutMetadataAccount::handle_temp_url_update(const map<int, string>& temp_u
 
 int RGWPutMetadataAccount::verify_permission()
 {
-  if (!rgw_user_is_authenticated(s->user)) {
+  if (!rgw_user_is_authenticated(s->auth_user)) {
     return -EACCES;
   }
   // if ((s->perm_mask & RGW_PERM_WRITE) == 0) {
