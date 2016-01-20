46e29f0f1569cda4bf4626272bc10641a5bd4383
Mykola Golub <mgolub@mirantis.com>
2016-01-20 22:28:26 +0200

    rbd-replay: async open/close calls support
    It still uses sync open/close in replay, which can be improved later.
    
    Signed-off-by: Mykola Golub <mgolub@mirantis.com>

--- src/rbd_replay/ios.hpp
+++ src/rbd_replay/ios.hpp
@@ -291,6 +291,61 @@ private:
   imagectx_id_t m_imagectx;
 };
 
+class AioOpenImageIO : public IO {
+public:
+  AioOpenImageIO(action_id_t ionum,
+		 uint64_t start_time,
+		 thread_id_t thread_id,
+		 const io_set_t& deps,
+		 imagectx_id_t imagectx,
+		 const std::string& name,
+		 const std::string& snap_name,
+		 bool readonly)
+    : IO(ionum, start_time, thread_id, deps),
+      m_imagectx(imagectx),
+      m_name(name),
+      m_snap_name(snap_name),
+      m_readonly(readonly) {
+  }
+
+  virtual void encode(bufferlist &bl) const;
+
+  imagectx_id_t imagectx() const {
+    return m_imagectx;
+  }
+
+  void write_debug(std::ostream& out) const;
+
+private:
+  imagectx_id_t m_imagectx;
+  std::string m_name;
+  std::string m_snap_name;
+  bool m_readonly;
+};
+
+class AioCloseImageIO : public IO {
+public:
+  AioCloseImageIO(action_id_t ionum,
+		  uint64_t start_time,
+		  thread_id_t thread_id,
+		  const io_set_t& deps,
+		  imagectx_id_t imagectx)
+    : IO(ionum, start_time, thread_id, deps),
+      m_imagectx(imagectx) {
+  }
+
+  virtual void encode(bufferlist &bl) const;
+
+  imagectx_id_t imagectx() const {
+    return m_imagectx;
+  }
+
+  void write_debug(std::ostream& out) const;
+
+private:
+  imagectx_id_t m_imagectx;
+};
+
 }
 
 #endif
