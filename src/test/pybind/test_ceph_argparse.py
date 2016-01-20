c0797d6aa38f8f48c7ef8aa8405d6aab32ba2eab
Yan, Zheng <zyan@redhat.com>
2016-01-20 10:43:56 +0800

    pybind/ceph_argparse: update test for "ceph mds setmap"
    Signed-off-by: Yan, Zheng <zyan@redhat.com>

--- src/test/pybind/test_ceph_argparse.py
+++ src/test/pybind/test_ceph_argparse.py
@@ -381,7 +381,15 @@ class TestMDS(TestArgparse):
         self.check_1_natural_arg('mds', 'set_max_mds')
 
     def test_setmap(self):
-        self.check_1_natural_arg('mds', 'setmap')
+        self.assert_valid_command(['mds', 'setmap', '1'])
+        self.assert_valid_command(['mds', 'setmap', '1', '--yes-i-really-mean-it'])
+        assert_equal({}, validate_command(sigdict, ['mds', 'setmap',
+                                                    '--yes-i-really-mean-it']))
+        assert_equal({}, validate_command(sigdict, ['mds', 'setmap', '-1',
+                                                    '--yes-i-really-mean-it']))
+        assert_equal({}, validate_command(sigdict, ['mds', 'setmap', '1',
+                                                    '--yes-i-really-mean-it',
+                                                    'toomany']))
 
     def test_set_state(self):
         self.assert_valid_command(['mds', 'set_state', '1', '2'])
