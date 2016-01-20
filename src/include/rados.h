6db7fe7de32689c8185ceb1f350ebe94841f0781
Samuel Just <sjust@redhat.com>
2016-01-20 10:16:21 -0800

    introduce CEPH_FEATURE_SERVER_JEWEL feature bit
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/include/rados.h
+++ src/include/rados.h
@@ -144,6 +144,7 @@ extern const char *ceph_osd_state_name(int s);
 #define CEPH_OSDMAP_NOTIERAGENT (1<<13) /* disable tiering agent */
 #define CEPH_OSDMAP_NOREBALANCE (1<<14) /* block osd backfill unless pg is degraded */
 #define CEPH_OSDMAP_SORTBITWISE (1<<15) /* use bitwise hobject_t sort */
+#define CEPH_OSDMAP_REQUIRE_JEWEL (1<<16) /* require jewel for booting osds */
 
 /*
  * The error code to return when an OSD can't handle a write
