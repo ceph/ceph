056a10cca29763b4180bef89e88aa1a1b5bdcb57
Yehuda Sadeh <yehuda@redhat.com>
2016-01-21 16:35:29 -0800

    rgw: add a configurable to enable/disable static website
    rgw_enable_static_website
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -1245,6 +1245,8 @@ OPTION(rgw_objexp_time_step, OPT_U32, 4096) // number of seconds for rounding th
 OPTION(rgw_objexp_hints_num_shards, OPT_U32, 127) // maximum number of parts in which the hint index is stored in
 OPTION(rgw_objexp_chunk_size, OPT_U32, 100) // maximum number of entries in a single operation when processing objexp data
 
+OPTION(rgw_enable_static_website, OPT_BOOL, false) // enable static website feature
+
 OPTION(mutex_perf_counter, OPT_BOOL, false) // enable/disable mutex perf counter
 OPTION(throttler_perf_counter, OPT_BOOL, true) // enable/disable throttler perf counter
 
