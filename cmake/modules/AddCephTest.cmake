165ddf6333c02d52ddac246f14a7eb7fc1c910a5
Ali Maredia <amaredia@redhat.com>
2016-01-26 15:45:15 -0500

    cmake: Reorganized test dir, added cmake functions
    make check targets now use add_ceph_test, or
    add_ceph_unittest to be added to CTest. Renamed and
    reordered targets in tests/CMakeLists.txt
    
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

---
+++ cmake/modules/AddCephTest.cmake
@@ -0,0 +1,24 @@
+#AddCephTest is a module for adding tests to the "make check" target which runs CTest
+
+#adds makes target/script into a test, test to check target, sets necessary environment variables
+function(add_ceph_test test_name test_path)
+  add_test(NAME ${test_name} COMMAND ${test_path})
+  add_dependencies(check ${test_name})
+  set_property(TEST 
+    ${test_name}
+    PROPERTY ENVIRONMENT 
+    CEPH_ROOT=${CMAKE_SOURCE_DIR}
+    CEPH_BIN=${CMAKE_RUNTIME_OUTPUT_DIRECTORY}
+    CEPH_LIB=${CMAKE_LIBRARY_OUTPUT_DIRECTORY}
+    LD_LIBRARY_PATH=${CMAKE_BINARY_DIR}/lib
+    PATH=${CMAKE_RUNTIME_OUTPUT_DIRECTORY}:${CMAKE_SOURCE_DIR}/src:$ENV{PATH}
+    PYTHONPATH=${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/cython_modules/lib.linux-x86_64-2.7:${CMAKE_SOURCE_DIR}/src/pybind)
+endfunction()
+
+#sets uniform compiler flags and link libraries
+function(add_ceph_unittest unittest_name unittest_path)
+  add_ceph_test(${unittest_name} ${unittest_path})
+  target_link_libraries(${unittest_name} ${UNITTEST_LIBS})
+  set_target_properties(${unittest_name} PROPERTIES COMPILE_FLAGS ${UNITTEST_CXX_FLAGS})
+endfunction()
+
