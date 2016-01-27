bfea9f56d2ade74bf236f695e1bd2833633cd316
Haomai Wang <haomai@xsky.com>
2016-01-28 00:56:24 +0800

    AsyncConnection: add a debug option to test noninline way
    Signed-off-by: Haomai Wang <haomai@xsky.com>

--- src/msg/async/AsyncConnection.cc
+++ src/msg/async/AsyncConnection.cc
@@ -2061,7 +2061,7 @@ int AsyncConnection::send_message(Message *m)
     ldout(async_msgr->cct, 5) << __func__ << " clear encoded buffer, can_write=" << can_write << " previous "
                               << f << " != " << get_features() << dendl;
   }
-  if (!is_queued() && can_write == CANWRITE) {
+  if (!is_queued() && can_write == CANWRITE && async_msgr->cct->_conf->ms_async_send_inline) {
     if (!can_fast_prepare)
       prepare_send_message(get_features(), m, bl);
     logger->inc(l_msgr_send_messages_inline);
