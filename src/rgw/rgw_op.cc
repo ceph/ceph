ffca887c266c26b4f4bcc7a6ec71c5f9b55cacd5
root <root@gluster5.gfs.fsw.fujitsu.com>
2016-02-01 14:24:20 -0800

    Fix for rgw: user quota may not adjust on bucket removal
    Fixes: #14599
    Signed-off-by: Edward Yang eyang@us.fujitsu.com

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -1878,6 +1878,11 @@ void RGWDeleteBucket::execute()
     }
   }
 
+  op_ret = rgw_bucket_sync_user_stats(store, s->user.user_id, s->bucket);
+  if ( op_ret < 0) {
+     dout(1) << "WARNING: failed to sync user stats before bucket delete: ret= " << ret << dendl;
+  }
+
   op_ret = store->delete_bucket(s->bucket, ot);
   if (op_ret == 0) {
     op_ret = rgw_unlink_bucket(store, s->user.user_id, s->bucket.tenant,
