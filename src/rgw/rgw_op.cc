342999ed79e7d21c574d0f5e8ef017543b4fa658
root <root@gluster5.gfs.fsw.fujitsu.com>
2016-02-01 15:10:34 -0800

    2nd Fix for rgw: user quota may not adjust on bucket removal due to 'ret' not declared in this scope, correction made to op_ret.
    Fixes: #14599
    Signed-off-by: Edward Yang eyang@us.fujitsu.com

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -1880,7 +1880,7 @@ void RGWDeleteBucket::execute()
 
   op_ret = rgw_bucket_sync_user_stats(store, s->user.user_id, s->bucket);
   if ( op_ret < 0) {
-     dout(1) << "WARNING: failed to sync user stats before bucket delete: ret= " << ret << dendl;
+     dout(1) << "WARNING: failed to sync user stats before bucket delete: op_ret= " << op_ret << dendl;
   }
 
   op_ret = store->delete_bucket(s->bucket, ot);
