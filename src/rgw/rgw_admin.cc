9ab5fd9d67da43e986489e4f580a597dd8cb551e
Yehuda Sadeh <yehuda@redhat.com>
2016-02-01 16:33:55 -0800

    rgw-admin: document orphans commands in usage
    Fixes: #14516
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>
    (cherry picked from commit 105a76bf542e05b739d5a03ca8ae55432350f107)
    
    Conflicts:
    	src/rgw/rgw_admin.cc (trivial resolution)

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -112,6 +112,8 @@ void _usage()
   cerr << "  replicalog get             get replica metadata log entry\n";
   cerr << "  replicalog update          update replica metadata log entry\n";
   cerr << "  replicalog delete          delete replica metadata log entry\n";
+  cout << "  orphans find               init and run search for leaked rados objects\n";
+  cout << "  orphans finish             clean up search for leaked rados objects\n";
   cerr << "options:\n";
   cerr << "   --uid=<id>                user id\n";
   cerr << "   --subuser=<name>          subuser name\n";
@@ -174,6 +176,9 @@ void _usage()
   cerr << "   --max-objects             specify max objects (negative value to disable)\n";
   cerr << "   --max-size                specify max size (in bytes, negative value to disable)\n";
   cerr << "   --quota-scope             scope of quota (bucket, user)\n";
+  cout << "\nOrphans search options:\n";
+  cout << "   --pool                    data pool to scan for leaked rados objects in\n";
+  cout << "   --num-shards              num of shards to use for keeping the temporary scan info\n";
   cerr << "\n";
   generic_client_usage();
 }
