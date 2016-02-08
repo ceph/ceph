55cbc60d46a71563a0dff5ba7c63683a3f3d6c7a
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-02-08 16:41:36 +0100

    rgw: Keystone token parsing doesn't need to know API version.
    This patch targets an issue with S3Extension in Keystone:
    requested tokens always conform to Keystone API v2 - regardless
    of the version used to make the request.
    
    Previous implementation of KeystoneToken::parse() as well
    as KeystoneToken::decode_json() had to know the API version
    explicitly. Thus, they might be affected in the future by
    changes in S3-compatibility middleware of Keystone.
    
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_rest_s3.h
+++ src/rgw/rgw_rest_s3.h
@@ -356,11 +356,7 @@ private:
 
 public:
   explicit RGW_Auth_S3_Keystone_ValidateToken(CephContext *_cct)
-      : RGWHTTPClient(_cct),
-        /* This is really crazy but S3Extension in Keystone always
-         * returns token conforming to v2 - regardless whether you
-         * have requested v3 or not. */
-        response(KeystoneToken(KeystoneApiVersion::VER_2)) {
+      : RGWHTTPClient(_cct) {
     get_str_list(cct->_conf->rgw_keystone_accepted_roles, roles_list);
   }
 
