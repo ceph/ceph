058bd8df18656083776a997949f848902c9c32f7
Haomai Wang <haomai@xsky.com>
2016-02-11 01:07:47 +0800

    Event: don't wakeup dpdk driver since it's spinning on cpu
    Signed-off-by: Haomai Wang <haomai@xsky.com>

--- src/msg/async/Event.h
+++ src/msg/async/Event.h
@@ -78,6 +78,7 @@ class EventDriver {
   virtual int del_event(int fd, int cur_mask, int del_mask) = 0;
   virtual int event_wait(vector<FiredFileEvent> &fired_events, struct timeval *tp) = 0;
   virtual int resize_events(int newsize) = 0;
+  virtual bool need_wakeup() { return true; }
 };
 
 /*
--- src/msg/async/dpdk/EventDPDK.h
+++ src/msg/async/dpdk/EventDPDK.h
@@ -34,7 +34,7 @@ class DPDKDriver : public EventDriver {
   int del_event(int fd, int cur_mask, int del_mask) override;
   int resize_events(int newsize) override;
   int event_wait(vector<FiredFileEvent> &fired_events, struct timeval *tp) override;
-  bool wakeup_support() { return false; }
+  bool need_wakeup() { return false; }
 };
 
 #endif //CEPH_EVENTDPDK_H
