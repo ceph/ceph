484f47033b5081333606c15d13ae47f45afaadce
Javier M. Mellid <jmunhoz@igalia.com>
2016-02-14 13:26:02 +0100

    rgw: catch up aws4 with the new features
    Fix pending conflicts after massive merging. It catches up with SLO, bucket
    website, bulk deletes and payment request features.
    
    Signed-off-by: Javier M. Mellid <jmunhoz@igalia.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -381,6 +381,8 @@ enum RGWOpType {
   RGW_OP_GET_BUCKET_LOGGING,
   RGW_OP_GET_BUCKET_VERSIONING,
   RGW_OP_SET_BUCKET_VERSIONING,
+  RGW_OP_GET_BUCKET_WEBSITE,
+  RGW_OP_SET_BUCKET_WEBSITE,
   RGW_OP_STAT_BUCKET,
   RGW_OP_CREATE_BUCKET,
   RGW_OP_DELETE_BUCKET,
@@ -398,15 +400,18 @@ enum RGWOpType {
   RGW_OP_PUT_CORS,
   RGW_OP_DELETE_CORS,
   RGW_OP_OPTIONS_CORS,
+  RGW_OP_GET_REQUEST_PAYMENT,
+  RGW_OP_SET_REQUEST_PAYMENT,
   RGW_OP_INIT_MULTIPART,
   RGW_OP_COMPLETE_MULTIPART,
   RGW_OP_ABORT_MULTIPART,
   RGW_OP_LIST_MULTIPART,
   RGW_OP_LIST_BUCKET_MULTIPARTS,
   RGW_OP_DELETE_MULTI_OBJ,
+  RGW_OP_BULK_DELETE,
 
   /* rgw specific */
-  RGW_OP_ADMIN_SET_METADATA,
+  RGW_OP_ADMIN_SET_METADATA
 };
 
 class RGWAccessControlPolicy;
@@ -1771,7 +1776,7 @@ extern bool verify_object_permission(struct req_state *s, int perm);
  * by converting %-escaped strings into characters, etc*/
 extern void rgw_uri_escape_char(char c, string& dst);
 extern bool url_decode(const string& src_str, string& dest_str, bool in_query = false);
-extern void url_encode(const string& src, string& dst, bool in_query = false);
+extern void url_encode(const string& src, string& dst);
 
 /* destination should be CEPH_CRYPTO_HMACSHA1_DIGESTSIZE bytes long */
 extern void calc_hmac_sha1(const char *key, int key_len,
