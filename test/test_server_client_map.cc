a4c4903773f83547611f6503da4f79d0c212ffab
J. Eric Ivancich <ivancich@redhat.com>
2016-02-16 17:31:41 -0500

    Start adding some tests.

---
+++ test/test_server_client_map.cc
@@ -0,0 +1,13 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Copyright (C) 2016 Red Hat Inc.
+ */
+
+// #include <atomic>
+// #include <thread>
+// #include <chrono>
+// #include <iostream>
+
+#include "gtest/gtest.h"
+
