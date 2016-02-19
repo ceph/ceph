56dd09fc2a35733e0d4e4f0557fe314d3a0cf521
Yan, Zheng <zyan@redhat.com>
2016-02-19 18:39:49 +0800

    mds: Add posix file lock deadlock detection
    Signed-off-by: Yan, Zheng <zyan@redhat.com>

--- src/mds/CInode.h
+++ src/mds/CInode.h
@@ -542,7 +542,7 @@ protected:
 
   ceph_lock_state_t *get_fcntl_lock_state() {
     if (!fcntl_locks)
-      fcntl_locks = new ceph_lock_state_t(g_ceph_context);
+      fcntl_locks = new ceph_lock_state_t(g_ceph_context, CEPH_LOCK_FCNTL);
     return fcntl_locks;
   }
   void clear_fcntl_lock_state() {
@@ -551,7 +551,7 @@ protected:
   }
   ceph_lock_state_t *get_flock_lock_state() {
     if (!flock_locks)
-      flock_locks = new ceph_lock_state_t(g_ceph_context);
+      flock_locks = new ceph_lock_state_t(g_ceph_context, CEPH_LOCK_FLOCK);
     return flock_locks;
   }
   void clear_flock_lock_state() {
