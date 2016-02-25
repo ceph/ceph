e23fdcbbce47aaf041b9f8a0812c8a729aa5014b
Orit Wasserman <owasserm@redhat.com>
2016-02-25 16:02:15 +0100

    rgw: reset return code in when iterating over the bucket the objects
    Fixes: #14826
    
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -10859,6 +10859,7 @@ int RGWRados::cls_bucket_list(rgw_bucket& bucket, int shard_id, rgw_obj_key& sta
   map<string, bufferlist> updates;
   uint32_t count = 0;
   while (count < num_entries && !candidates.empty()) {
+    r = 0;
     // Select the next one
     int pos = candidates.begin()->second;
     const string& name = vcurrents[pos]->first;
