9215e3d7bf7b1e83b8ef13796592c492a1bbd96b
xie xingguo <xie.xingguo@zte.com.cn>
2016-02-25 06:57:15 +0000

    os/filestore: fix result code overwritten for snapshot()
    The create_checkpoint() is not a standard system-call, it may return
    some customized error code, thus no conversion is needed here.
    
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- src/os/filestore/FileStore.cc
+++ src/os/filestore/FileStore.cc
@@ -3907,7 +3907,6 @@ int FileStore::snapshot(const string& name)
 
   int r = backend->create_checkpoint(s, NULL);
   if (r) {
-    r = -errno;
     derr << "snapshot " << name << " failed: " << cpp_strerror(r) << dendl;
   }
 
