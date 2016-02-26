155f079118e7009fd71dcffa1ecfbf7f20020f79
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-02-26 22:17:09 +0100

    rgw: change naming of utility functions in rgw_keystone.cc.
    Slightly improve code formatting and const-correctness as well.
    
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_keystone.h
+++ src/rgw/rgw_keystone.h
@@ -6,11 +6,15 @@
 
 #include "rgw_common.h"
 
-int open_cms_envelope(CephContext *cct, string& src, string& dst);
-int decode_b64_cms(CephContext *cct, const string& signed_b64, bufferlist& bl);
-bool is_pki_token(const string& token);
-void get_token_id(const string& token, string& token_id);
-bool decode_pki_token(CephContext *cct, const string& token, bufferlist& bl);
+int rgw_open_cms_envelope(CephContext *cct, string& src, string& dst);
+int rgw_decode_b64_cms(CephContext *cct,
+                       const string& signed_b64,
+                       bufferlist& bl);
+bool rgw_is_pki_token(const string& token);
+void rgw_get_token_id(const string& token, string& token_id);
+bool rgw_decode_pki_token(CephContext *cct,
+                          const string& token,
+                          bufferlist& bl);
 
 enum class KeystoneApiVersion {
   VER_2,
