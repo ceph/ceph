ead55eb616db0afacccebd90ff97d74150f47fe4
Weijun Duan <duanweijun@h3c.com>
2016-02-27 04:28:14 -0500

    rgw:user email can modify to empty Fixes: #13286
    Signed-off-by: Weijun Duan <duanweijun@h3c.com>

--- src/rgw/rgw_user.cc
+++ src/rgw/rgw_user.cc
@@ -2048,8 +2048,17 @@ int RGWUser::execute_modify(RGWUserAdminOpState& op_state, std::string *err_msg)
       }
     }
     user_info.user_email = op_email;
-  }
+  } else if (op_email.empty() && op_state.user_email_specified) {
 
+    rgw_obj email_obj(store->zone.user_email_pool, user_info.user_email);
+    ldout(store->ctx(), 10) << "removing email index: " << user_info.user_email << dendl;
+    ret = store->delete_system_obj(email_obj);
+    if (ret < 0 && ret != -ENOENT) {
+      ldout(store->ctx(), 0) << "ERROR: could not remove " << user_info.user_id << ":" << email_obj << ", should be fixed (err=" << ret << ")" << dendl;
+      return ret;
+    }
+    user_info.user_email = "";
+  }
 
   // update the remaining user info
   if (!display_name.empty())
