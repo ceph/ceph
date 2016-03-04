ced5763019a5af9d79c1c92783ab6b617ca6486b
Yehuda Sadeh <yehuda@redhat.com>
2016-03-04 12:05:34 -0800

    librados: new stat2() that returns ceph::real_time
    Hiding ceph_real_time behind ceph_real_time_t
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/librados/IoCtxImpl.h
+++ src/librados/IoCtxImpl.h
@@ -138,6 +138,7 @@ struct librados::IoCtxImpl {
   int remove(const object_t& oid, int flags);
   int stat(const object_t& oid, uint64_t *psize, time_t *pmtime);
   int stat2(const object_t& oid, uint64_t *psize, struct timespec *pts);
+  int stat2(const object_t& oid, uint64_t *psize, ceph_real_time_t *pmtime);
   int trunc(const object_t& oid, uint64_t size);
 
   int tmap_update(const object_t& oid, bufferlist& cmdbl);
@@ -206,6 +207,7 @@ struct librados::IoCtxImpl {
 	       const char *method, bufferlist& inbl, bufferlist *outbl);
   int aio_stat(const object_t& oid, AioCompletionImpl *c, uint64_t *psize, time_t *pmtime);
   int aio_stat2(const object_t& oid, AioCompletionImpl *c, uint64_t *psize, struct timespec *pts);
+  int aio_stat2(const object_t& oid, AioCompletionImpl *c, uint64_t *psize, ceph_real_time_t *pmtime);
   int aio_cancel(AioCompletionImpl *c);
 
   int pool_change_auid(unsigned long long auid);
