b30655eeb673df447dc736e10b040b227911bbbc
Loic Dachary <ldachary@redhat.com>
2016-03-04 12:34:06 +0700

    buildpackages: do not requires the long tag version
    So that v10.1.0 is used instead of 10.1.0-0-g35ac827 when possible.
    
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- tasks/buildpackages/make-rpm.sh
+++ tasks/buildpackages/make-rpm.sh
@@ -46,7 +46,7 @@ releasedir=$base/$(lsb_release -si)/WORKDIR
 # c) compares higher than any previous commit
 # d) contains the short hash of the commit
 #
-vers=$(git describe --long --match "v*" | sed s/^v//)
+vers=$(git describe --match "v*" | sed s/^v//)
 ceph_dir=$(pwd)
 
 #
