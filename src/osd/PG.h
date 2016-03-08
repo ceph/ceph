66db4c89f7187321ea577f6ba24cfc31526aa954
xie xingguo <xie.xingguo@zte.com.cn>
2016-03-08 12:15:55 +0800

    osd: avoid implicit return type cast
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -1087,7 +1087,7 @@ public:
   void proc_primary_info(ObjectStore::Transaction &t, const pg_info_t &info);
 
   bool have_unfound() const { 
-    return missing_loc.num_unfound();
+    return missing_loc.num_unfound() > 0;
   }
   int get_num_unfound() const {
     return missing_loc.num_unfound();
