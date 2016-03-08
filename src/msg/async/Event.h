10ab2757c2daca502c74b36f66e7f82b2b90544a
Haomai Wang <haomai@xsky.com>
2016-03-08 14:35:17 +0800

    Event: replace Mutex with c++ mutex
    Signed-off-by: Haomai Wang <haomai@xsky.com>

--- src/msg/async/Event.h
+++ src/msg/async/Event.h
@@ -41,10 +41,10 @@
 #include <mutex>
 #include <condition_variable>
 
-#include "include/Context.h"
+#include "include/utime.h"
 #include "include/unordered_map.h"
 #include "common/ceph_time.h"
-#include "common/WorkQueue.h"
+#include "common/dout.h"
 #include "net_handler.h"
 
 #define EVENT_NONE 0
@@ -110,7 +110,7 @@ class EventCenter {
   CephContext *cct;
   int nevent;
   // Used only to external event
-  Mutex external_lock, file_lock;
+  std::mutex external_lock, file_lock;;
   std::atomic_ulong external_num_events;
   deque<EventCallbackRef> external_events;
   vector<FileEvent> file_events;
@@ -134,8 +134,6 @@ class EventCenter {
 
   explicit EventCenter(CephContext *c):
     cct(c), nevent(0),
-    external_lock("AsyncMessenger::external_lock"),
-    file_lock("AsyncMessenger::file_lock"),
     external_num_events(0),
     driver(NULL), time_event_next_id(1),
     notify_receive_fd(-1), notify_send_fd(-1), net(c),
