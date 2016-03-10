caba028557c620177134dee6cd0871ea885c4ccb
Samuel Just <sjust@redhat.com>
2016-03-10 11:06:06 -0800

    LFNIndex::created: fix return value with failure injection
    See comment for explanation.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/os/filestore/LFNIndex.cc
+++ src/os/filestore/LFNIndex.cc
@@ -89,8 +89,19 @@ int LFNIndex::created(const ghobject_t &oid, const char *path)
   if (r < 0)
     goto out;
   r = lfn_created(path_comp, oid, short_name);
-  if (r < 0)
+  if (r < 0) {
+    if (failed) {
+      /* This is hacky, but the only way we get ENOENT from lfn_created here is
+       * if we did a failure injection in _created below AND actually started the
+       * split or merge.  In that case, lfn_created already suceeded, and
+       * WRAP_RETRY already cleaned it up and we are actually done.  In a real
+       * failure, the filestore itself would have ended up calling this with
+       * the new path, not the old one, so we'd find it.
+       */
+      r = 0;
+    }
     goto out;
+  }
   r = _created(path_comp, oid, short_name);
   if (r < 0)
     goto out;
