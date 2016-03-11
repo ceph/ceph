cccf7b5a9e7479a9d4da89f8022be82d9727e20f
J. Eric Ivancich <ivancich@redhat.com>
2016-03-11 15:33:16 -0500

    Update all copyright notices to 2016.

--- src/crimson/heap.h
+++ src/crimson/heap.h
@@ -1,7 +1,8 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*- 
 // vim: ts=8 sw=2 smarttab
+
 /*
- * Copyright (C) 2015 Red Hat Inc.
+ * Copyright (C) 2016 Red Hat Inc.
  */
 
 
