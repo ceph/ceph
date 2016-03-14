26d4f2e11d68560a3c87c41a98e3ed5d9067eeb0
Sage Weil <sage@redhat.com>
2016-03-14 11:17:02 -0400

    mon/PGMonitor: reliably mark PGs state
    In beb99d708a5051405a32f46fc10cef473bfddce3 we partially addressed
    the case where a acting_primary update raced with an attemp to
    mark the pg state.  However, if the acting_primary changed, we
    didn't check the new acting_primary to ensure that it is also
    down and we should still mark things stale.
    
    Also, printing out the acting_primary aids debugging.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mon/PGMonitor.h
+++ src/mon/PGMonitor.h
@@ -141,7 +141,7 @@ private:
    * @return true if we updated pending_inc (and should propose)
    */
   bool check_down_pgs();
-  void _mark_pg_stale(pg_t pgid, const pg_stat_t& cur_stat);
+  void _try_mark_pg_stale(OSDMap *osdmap, pg_t pgid, const pg_stat_t& cur_stat);
 
 
   /**
