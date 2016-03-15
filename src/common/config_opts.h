cad057302fbb7a0730a649b4737897a62a2cfebf
Jianpeng Ma <jianpeng.ma@intel.com>
2016-03-15 23:51:05 +0800

    os/bluestore/BlueStore: add config option:bluestore_wal_async_read.
    In wal-op, for some reasons(aligned-write or copy) we need read data
    from disk. If bluestore_wal_async_read==ture, it mean we use async-read
    rather that sync-read.
    
    Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -951,6 +951,7 @@ OPTION(bluestore_open_by_handle, OPT_BOOL, true)
 OPTION(bluestore_o_direct, OPT_BOOL, true)
 OPTION(bluestore_clone_cow, OPT_BOOL, true)  // do copy-on-write for clones
 OPTION(bluestore_default_buffered_read, OPT_BOOL, false)
+OPTION(bluestore_wal_async_read, OPT_BOOL, false) //In wal-op, if need read somthing,  it use async mode
 OPTION(bluestore_debug_misc, OPT_BOOL, false)
 OPTION(bluestore_debug_no_reuse_blocks, OPT_BOOL, false)
 OPTION(bluestore_debug_small_allocations, OPT_INT, 0)
