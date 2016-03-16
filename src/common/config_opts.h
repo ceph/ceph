36fc0841cdaf57e8558851d4e282060f8dd10cd4
John Spray <john.spray@redhat.com>
2016-03-16 12:49:51 +0000

    mon: config setting to skip FSMap::sanity
    Only for use in extremis, where we have encountered
    a bug that generates an invalid FSMap, but the way
    to resolve it might be to proceed anyway and stay
    up long enough to use commands to fix it.
    
    Fixes: #15063
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -312,6 +312,7 @@ OPTION(mon_osd_min_down_reporters, OPT_INT, 2)   // number of OSDs from differen
 OPTION(mon_osd_reporter_subtree_level , OPT_STR, "host")   // in which level of parent bucket the reporters are counted
 OPTION(mon_osd_force_trim_to, OPT_INT, 0)   // force mon to trim maps to this point, regardless of min_last_epoch_clean (dangerous, use with care)
 OPTION(mon_mds_force_trim_to, OPT_INT, 0)   // force mon to trim mdsmaps to this point (dangerous, use with care)
+OPTION(mon_mds_skip_sanity, OPT_BOOL, false)  // skip safety assertions on FSMap (in case of bugs where we want to continue anyway)
 
 // monitor debug options
 OPTION(mon_debug_deprecated_as_obsolete, OPT_BOOL, false) // consider deprecated commands as obsolete
