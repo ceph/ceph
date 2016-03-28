00cb296a523996bbf52a7392a32462889b5aff7d
Kefu Chai <kchai@redhat.com>
2016-03-28 12:19:29 +0800

    mon: remove remove_legacy_versions()
    as it's required to upgrade to hammer before moving to jewel, and
    hammer is already using the single-paxos monitor. and we convert
    the store.db when starting up from an old monitor (bobtail). we
    stopped the conversion since 1d814b7.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/mon/PaxosService.cc
+++ src/mon/PaxosService.cc
@@ -144,26 +144,6 @@ void PaxosService::post_refresh()
   }
 }
 
-void PaxosService::remove_legacy_versions()
-{
-  dout(10) << __func__ << dendl;
-  if (!mon->store->exists(get_service_name(), "conversion_first"))
-    return;
-
-  version_t cf = mon->store->get(get_service_name(), "conversion_first");
-  version_t fc = get_first_committed();
-
-  dout(10) << __func__ << " conversion_first " << cf
-	   << " first committed " << fc << dendl;
-
-  MonitorDBStore::TransactionRef t(new MonitorDBStore::Transaction);
-  if (cf < fc) {
-    trim(t, cf, fc);
-  }
-  t->erase(get_service_name(), "conversion_first");
-  mon->store->apply_transaction(t);
-}
-
 bool PaxosService::should_propose(double& delay)
 {
   // simple default policy: quick startup, then some damping.
@@ -277,8 +257,6 @@ void PaxosService::_active()
     return;
   }
   dout(10) << "_active" << dendl;
-
-  remove_legacy_versions();
 
   // create pending state?
   if (mon->is_leader() && is_active()) {
