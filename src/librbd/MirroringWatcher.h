3748b8874ecfced0ae72bf924103187354cc5ab8
Jason Dillaman <dillaman@redhat.com>
2016-03-29 00:02:10 -0400

    librbd: helper methods for mirroring notifications
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/MirroringWatcher.h
+++ src/librbd/MirroringWatcher.h
@@ -5,6 +5,8 @@
 #define CEPH_LIBRBD_MIRRORING_WATCHER_H
 
 #include "include/int_types.h"
+#include "include/rados/librados.hpp"
+#include "cls/rbd/cls_rbd_types.h"
 #include "librbd/ImageCtx.h"
 #include "librbd/ObjectWatcher.h"
 #include "librbd/mirroring_watcher/Types.h"
@@ -17,6 +19,13 @@ public:
   typedef typename std::decay<decltype(*ImageCtxT::op_work_queue)>::type ContextWQT;
 
   MirroringWatcher(librados::IoCtx &io_ctx, ContextWQT *work_queue);
+
+  static int notify_mode_updated(librados::IoCtx &io_ctx,
+                                 cls::rbd::MirrorMode mirror_mode);
+  static int notify_image_updated(librados::IoCtx &io_ctx,
+                                  cls::rbd::MirrorImageState mirror_image_state,
+                                  const std::string &image_id,
+                                  const std::string &global_image_id);
 
 protected:
   virtual std::string get_oid() const;
