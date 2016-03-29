c4ee8a2a34179a61ed385f3fdc842e2430a9ca60
Jason Dillaman <dillaman@redhat.com>
2016-03-29 00:02:10 -0400

    librbd: helper methods for mirroring notifications
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/MirroringWatcher.cc
+++ src/librbd/MirroringWatcher.cc
@@ -3,12 +3,21 @@
 
 #include "librbd/MirroringWatcher.h"
 #include "include/rbd_types.h"
+#include "common/errno.h"
 
 #define dout_subsys ceph_subsys_rbd
 #undef dout_prefix
 #define dout_prefix *_dout << "librbd::MirroringWatcher: "
 
 namespace librbd {
+
+using namespace mirroring_watcher;
+
+namespace {
+
+static const uint64_t NOTIFY_TIMEOUT_MS = 5000;
+
+} // anonymous namespace
 
 template <typename I>
 MirroringWatcher<I>::MirroringWatcher(librados::IoCtx &io_ctx,
@@ -19,6 +28,45 @@ MirroringWatcher<I>::MirroringWatcher(librados::IoCtx &io_ctx,
 template <typename I>
 std::string MirroringWatcher<I>::get_oid() const {
   return RBD_MIRRORING;
+}
+
+template <typename I>
+int MirroringWatcher<I>::notify_mode_updated(librados::IoCtx &io_ctx,
+                                             cls::rbd::MirrorMode mirror_mode) {
+  CephContext *cct = reinterpret_cast<CephContext*>(io_ctx.cct());
+  ldout(cct, 20) << dendl;
+
+  bufferlist bl;
+  ::encode(NotifyMessage{ModeUpdatedPayload{mirror_mode}}, bl);
+
+  int r = io_ctx.notify2(RBD_MIRRORING, bl, NOTIFY_TIMEOUT_MS, nullptr);
+  if (r < 0) {
+    lderr(cct) << ": error encountered sending mode updated notification: "
+               << cpp_strerror(r) << dendl;
+    return r;
+  }
+  return 0;
+}
+
+template <typename I>
+int MirroringWatcher<I>::notify_image_updated(
+    librados::IoCtx &io_ctx, cls::rbd::MirrorImageState mirror_image_state,
+    const std::string &image_id, const std::string &global_image_id) {
+  CephContext *cct = reinterpret_cast<CephContext*>(io_ctx.cct());
+  ldout(cct, 20) << dendl;
+
+  bufferlist bl;
+  ::encode(NotifyMessage{ImageUpdatedPayload{mirror_image_state, image_id,
+                                             global_image_id}},
+           bl);
+
+  int r = io_ctx.notify2(RBD_MIRRORING, bl, NOTIFY_TIMEOUT_MS, nullptr);
+  if (r < 0) {
+    lderr(cct) << ": error encountered sending image updated notification: "
+               << cpp_strerror(r) << dendl;
+    return r;
+  }
+  return 0;
 }
 
 } // namespace librbd
