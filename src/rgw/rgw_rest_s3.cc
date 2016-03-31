ceph/ceph

commit a58b774e72cc1613d62e10b25322d6d15e9d2899

rgw: Multipart ListPartsResult ETag quotes
ListPartsResult output has always missed quotes on the ETag since it was
first committed.

Fixes: #15334
Backports: hammer, infernalis
Signed-off-by: Robin H. Johnson <robin.johnson@dreamhost.com>

src/rgw/rgw_rest_s3.cc
@@ -2428,7 +2428,7 @@ void RGWListMultipart_ObjStore_S3::send_response()
       dump_time(s, "LastModified", &info.modified);
 
       s->formatter->dump_unsigned("PartNumber", info.num);
-      s->formatter->dump_string("ETag", info.etag);
+      s->formatter->dump_format("ETag", "\"%s\"", info->etag.c_str());
       s->formatter->dump_unsigned("Size", info.size);
       s->formatter->close_section();
     }

commit 2b57411ff3aa483449e286135026055d4e8f01e6

rgw: fix compile error introduced in a58b774
Signed-off-by: Orit Wasserman <owasserm@redhat.com>

src/rgw/rgw_rest_s3.cc
@@ -2428,7 +2428,7 @@ void RGWListMultipart_ObjStore_S3::send_response()
       dump_time(s, "LastModified", &info.modified);
 
       s->formatter->dump_unsigned("PartNumber", info.num);
-      s->formatter->dump_format("ETag", "\"%s\"", info->etag.c_str());
+      s->formatter->dump_format("ETag", "\"%s\"", info.etag.c_str());
       s->formatter->dump_unsigned("Size", info.size);
       s->formatter->close_section();
     }