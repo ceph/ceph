3320f8fe3179f0e88809a1a363cad50c9d0c6297
Karol Mroz <kmroz@suse.com>
2016-04-08 16:44:34 +0200

    rgw-admin: remove unused iterator
    Signed-off-by: Karol Mroz <kmroz@suse.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -2293,7 +2293,6 @@ int main(int argc, char **argv)
     } else if (ceph_argparse_witharg(args, i, &val, "--zone-new-name", (char*)NULL)) {
       zone_new_name = val;
     } else if (ceph_argparse_witharg(args, i, &val, "--endpoints", (char*)NULL)) {
-      list<string>::iterator iter;
       get_str_list(val, endpoints);
     } else if (ceph_argparse_witharg(args, i, &val, "--source-zone", (char*)NULL)) {
       source_zone_name = val;
