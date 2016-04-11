9b39ec956c59c47fa8caa30cb3cd1360028b503d
J. Eric Ivancich <ivancich@redhat.com>
2016-04-11 15:18:35 -0400

    Split declarations for code that tests dmclock into a header file, so they can more easily be used with unit testing. Also put those declarations in a namespace. Reorganize code slightly.

--- src/test_dmclock.cc
+++ src/test_dmclock.cc
@@ -6,7 +6,7 @@
  */
 
 
-#include <sstream>
+// #include <sstream>
 
 #include "dmclock_recs.h"
 #include "dmclock_server.h"
@@ -18,35 +18,116 @@
 
 #include "simulate.h"
 
+#include "test_dmclock.h"
+
 
 using namespace std::placeholders;
+namespace test = test_dmc;
 
-namespace dmc = crimson::dmclock;
+const double client_reservation = 20.0;
+const double client_limit = 60.0;
+const double client_weight = 1.0;
 
-using DmcServerAddInfo = crimson::dmclock::PhaseType;
+test::dmc::ClientInfo client_info =
+{ client_weight, client_reservation, client_limit };
 
-struct DmcAccum {
-  uint64_t reservation_count = 0;
-  uint64_t proportion_count = 0;
-};
+void server_data(std::ostream& out,
+		 test::MySim* sim,
+		 test::MySim::ServerFilter server_disp_filter,
+		 int head_w, int data_w, int data_prec);
 
-using DmcServer = TestServer<dmc::PriorityQueue<ClientId,TestRequest>,
-			     dmc::ClientInfo,
-			     dmc::ReqParams<ClientId>,
-			     dmc::RespParams<ServerId>,
-			     DmcServerAddInfo,
-			     DmcAccum>;
-
-using DmcClient = TestClient<dmc::ServiceTracker<ServerId>,
-			     dmc::ReqParams<ClientId>,
-			     dmc::RespParams<ServerId>,
-			     DmcAccum>;
-
-using MySim = Simulation<ServerId,ClientId,DmcServer,DmcClient>;
+void client_data(std::ostream& out,
+		 test::MySim* sim,
+		 test::MySim::ClientFilter client_disp_filter,
+		 int head_w, int data_w, int data_prec);
 
 
-void dmc_server_accumulate_f(DmcAccum& a, const DmcServerAddInfo& add_info) {
-  if (dmc::PhaseType::reservation == add_info) {
+int main(int argc, char* argv[]) {
+  // server params
+
+  const uint server_count = 100;
+  const uint server_iops = 40;
+  const uint server_threads = 1;
+  const bool server_soft_limit = false;
+
+  // client params
+
+  const uint client_total_ops = 1000;
+  const uint client_count = 100;
+  const uint client_wait_count = 1;
+  const uint client_iops_goal = 50;
+  const uint client_outstanding_ops = 100;
+  const std::chrono::seconds client_wait(10);
+
+  auto client_disp_filter = [=] (const ClientId& i) -> bool {
+    return i < 3 || i >= (client_count - 3);
+  };
+
+  auto server_disp_filter = [=] (const ServerId& i) -> bool {
+    return i < 3 || i >= (server_count - 3);
+  };
+
+
+  test::MySim *simulation;
+
+  // lambda to post a request to the identified server; called by client
+  test::SubmitFunc server_post_f =
+    [&simulation](const ServerId& server,
+		  const TestRequest& request,
+		  const test::dmc::ReqParams<ClientId>& req_params) {
+    test::DmcServer& s = simulation->get_server(server);
+    s.post(request, req_params);
+  };
+
+  static std::vector<CliInst> no_wait =
+    { { req_op, client_total_ops, client_iops_goal, client_outstanding_ops } };
+  static std::vector<CliInst> wait =
+    { { wait_op, client_wait },
+      { req_op, client_total_ops, client_iops_goal, client_outstanding_ops } };
+
+  simulation = new test::MySim();
+
+  test::MySim::ClientBasedServerSelectFunc server_select_f =
+    simulation->make_server_select_alt_range(8);
+
+  test::DmcServer::ClientRespFunc client_response_f =
+    [&simulation](ClientId client_id,
+		  const TestResponse& resp,
+		  const test::dmc::RespParams<ServerId>& resp_params) {
+    simulation->get_client(client_id).receive_response(resp, resp_params);
+  };
+
+  auto create_server_f = [&](ServerId id) -> test::DmcServer* {
+    return new test::DmcServer(id,
+			       server_iops, server_threads,
+			       test::client_info_f,
+			       client_response_f,
+			       test::dmc_server_accumulate_f,
+			       server_soft_limit);
+  };
+
+  auto create_client_f = [&](ClientId id) -> test::DmcClient* {
+    return new test::DmcClient(id,
+			       server_post_f,
+			       std::bind(server_select_f, _1, id),
+			       test::dmc_client_accumulate_f,
+			       id < (client_count - client_wait_count)
+			       ? no_wait : wait);
+  };
+
+  simulation->add_servers(server_count, create_server_f);
+  simulation->add_clients(client_count, create_client_f);
+
+  simulation->run();
+  simulation->display_stats(std::cout,
+			    &server_data, &client_data,
+			    server_disp_filter, client_disp_filter);
+} // main
+
+
+void test::dmc_server_accumulate_f(test::DmcAccum& a,
+				   const test::DmcServerAddInfo& add_info) {
+  if (test::dmc::PhaseType::reservation == add_info) {
     ++a.reservation_count;
   } else {
     ++a.proportion_count;
@@ -54,8 +135,9 @@ void dmc_server_accumulate_f(DmcAccum& a, const DmcServerAddInfo& add_info) {
 }
 
 
-void dmc_client_accumulate_f(DmcAccum& a, const dmc::RespParams<ServerId>& r) {
-  if (dmc::PhaseType::reservation == r.phase) {
+void test::dmc_client_accumulate_f(test::DmcAccum& a,
+				   const test::dmc::RespParams<ServerId>& r) {
+  if (test::dmc::PhaseType::reservation == r.phase) {
     ++a.reservation_count;
   } else {
     ++a.proportion_count;
@@ -64,8 +146,8 @@ void dmc_client_accumulate_f(DmcAccum& a, const dmc::RespParams<ServerId>& r) {
 
 
 void client_data(std::ostream& out,
-		 MySim* sim,
-		 MySim::ClientFilter client_disp_filter,
+		 test::MySim* sim,
+		 test::MySim::ClientFilter client_disp_filter,
 		 int head_w, int data_w, int data_prec) {
   // report how many ops were done by reservation and proportion for
   // each client
@@ -97,8 +179,8 @@ void client_data(std::ostream& out,
 
 
 void server_data(std::ostream& out,
-		 MySim* sim,
-		 MySim::ServerFilter server_disp_filter,
+		 test::MySim* sim,
+		 test::MySim::ServerFilter server_disp_filter,
 		 int head_w, int data_w, int data_prec) {
   out << std::setw(head_w) << "res_ops:";
   int total_r = 0;
@@ -126,103 +208,6 @@ void server_data(std::ostream& out,
 }
 
 
-const double client_reservation = 20.0;
-const double client_limit = 60.0;
-const double client_weight = 1.0;
-
-dmc::ClientInfo client_info =
-{ client_weight, client_reservation, client_limit };
-
-
-// construct servers
-
-
-dmc::ClientInfo client_info_f(const ClientId& c) {
+test::dmc::ClientInfo test::client_info_f(const ClientId& c) {
   return client_info;
-}
-
-
-using SubmitFunc = DmcClient::SubmitFunc;
-
-
-int main(int argc, char* argv[]) {
-  // server params
-
-  const uint server_count = 100;
-  const uint server_iops = 40;
-  const uint server_threads = 1;
-  const bool server_soft_limit = false;
-
-  // client params
-
-  const uint client_total_ops = 1000;
-  const uint client_count = 100;
-  const uint client_wait_count = 1;
-  const uint client_iops_goal = 50;
-  const uint client_outstanding_ops = 100;
-  const std::chrono::seconds client_wait(10);
-
-  auto client_disp_filter = [=] (const ClientId& i) -> bool {
-    return i < 3 || i >= (client_count - 3);
-  };
-
-  auto server_disp_filter = [=] (const ServerId& i) -> bool {
-    return i < 3 || i >= (server_count - 3);
-  };
-
-
-  MySim *simulation;
-
-  // lambda to post a request to the identified server; called by client
-  SubmitFunc server_post_f =
-    [&simulation](const ServerId& server,
-		  const TestRequest& request,
-		  const dmc::ReqParams<ClientId>& req_params) {
-    DmcServer& s = simulation->get_server(server);
-    s.post(request, req_params);
-  };
-
-  static std::vector<CliInst> no_wait =
-    { { req_op, client_total_ops, client_iops_goal, client_outstanding_ops } };
-  static std::vector<CliInst> wait =
-    { { wait_op, client_wait },
-      { req_op, client_total_ops, client_iops_goal, client_outstanding_ops } };
-
-  simulation = new MySim();
-
-  MySim::ClientBasedServerSelectFunc server_select_f =
-    simulation->make_server_select_alt_range(8);
-
-  DmcServer::ClientRespFunc client_response_f =
-    [&simulation](ClientId client_id,
-		  const TestResponse& resp,
-		  const dmc::RespParams<ServerId>& resp_params) {
-    simulation->get_client(client_id).receive_response(resp, resp_params);
-  };
-
-  auto create_server_f = [&](ServerId id) -> DmcServer* {
-    return new DmcServer(id,
-			 server_iops, server_threads,
-			 client_info_f,
-			 client_response_f,
-			 dmc_server_accumulate_f,
-			 server_soft_limit);
-  };
-
-  auto create_client_f = [&](ClientId id) -> DmcClient* {
-    return new DmcClient(id,
-			 server_post_f,
-			 std::bind(server_select_f, _1, id),
-			 dmc_client_accumulate_f,
-			 id < (client_count - client_wait_count)
-			 ? no_wait : wait);
-  };
-
-  simulation->add_servers(server_count, create_server_f);
-  simulation->add_clients(client_count, create_client_f);
-
-  simulation->run();
-  simulation->display_stats(std::cout,
-			    &server_data, &client_data,
-			    server_disp_filter, client_disp_filter);
 }
