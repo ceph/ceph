ceph/ceph

commit 4a14baac81ae4c4e9d9d429ac1a68a5624061abc

osd: add option to disable startup check
Signed-off-by: Sage Weil <sage@redhat.com>

src/common/config_opts.h
@@ -556,6 +556,9 @@ OPTION(mds_max_scrub_ops_in_progress, OPT_INT, 5) // the number of simultaneous 
 // Maximum number of damaged frags/dentries before whole MDS rank goes damaged
 OPTION(mds_damage_table_max_entries, OPT_INT, 10000)
 
+// verify backend can support configured max object name length
+OPTION(osd_check_max_object_name_len_on_sthartup, OPT_BOOL, true)
+
 // If true, compact leveldb store on mount
 OPTION(osd_compact_leveldb_on_mount, OPT_BOOL, false)
 

src/osd/OSD.cc
@@ -2037,7 +2037,13 @@ int OSD::init()
       derr << "   osd max object namespace len = "
 	   << g_conf->osd_max_object_namespace_len << dendl;
       derr << cpp_strerror(r) << dendl;
-      goto out;
+      if (g_conf->osd_check_max_object_name_len_on_startup) {
+	goto out;
+      }
+      derr << "osd_check_max_object_name_len_on_startup = false, starting anyway"
+	   << dendl;
+    } else {
+      dout(20) << "configured osd_max_object_name[space]_len looks ok" << dendl;
     }
   }
 

commit 72077096469bf7680fc78cd5ba9f65b3ff9e1995

common: typo-fix for osd_check_max_object_name_len_on_startup
Fixes a typo introduced in 4a14baac81ae4c4e9d9d429ac1a68a5624061abc

Signed-off-by: Robin H. Johnson <robin.johnson@dreamhost.com>

src/common/config_opts.h
@@ -557,7 +557,7 @@ OPTION(mds_max_scrub_ops_in_progress, OPT_INT, 5) // the number of simultaneous 
 OPTION(mds_damage_table_max_entries, OPT_INT, 10000)
 
 // verify backend can support configured max object name length
-OPTION(osd_check_max_object_name_len_on_sthartup, OPT_BOOL, true)
+OPTION(osd_check_max_object_name_len_on_startup, OPT_BOOL, true)
 
 // If true, compact leveldb store on mount
 OPTION(osd_compact_leveldb_on_mount, OPT_BOOL, false)