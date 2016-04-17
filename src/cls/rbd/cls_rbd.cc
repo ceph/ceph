946155026ceda3c4d36d32ad1aa73f37870d2b31
runsisi <runsisi@hust.edu.cn>
2016-04-17 23:33:07 +0800

    cls_rbd: fix loop condition when listing image metadata
    if max_return is 0 (i.e., the rbd client wants to list all metadata of the
    image), then we should not use max_return as a part of the loop condition
    
    Signed-off-by: runsisi <runsisi@zte.com.cn>

--- src/cls/rbd/cls_rbd.cc
+++ src/cls/rbd/cls_rbd.cc
@@ -2563,10 +2563,13 @@ int metadata_list(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
     for (; it != raw_data.end(); ++it)
       data[metadata_name_from_key(it->first)].swap(it->second);
 
+    if (r < max_read)
+      break;
+
     last_read = raw_data.rbegin()->first;
     if (max_return)
       max_read = MIN(RBD_MAX_KEYS_READ, max_return-data.size());
-  } while (max_return && max_read);
+  } while (max_read);
 
   ::encode(data, *out);
   return 0;
