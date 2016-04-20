364f7459d2bddebff8d3686d6e5937c0bc6ca76b
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-04-20 13:54:44 +0200

    rgw: fix handling issues with memory allocation in RGWRados::init_rados().
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -3132,7 +3132,7 @@ int RGWRados::init_rados()
 
   num_rados_handles = cct->_conf->rgw_num_rados_handles;
 
-  rados = new librados::Rados *[num_rados_handles];
+  rados = new (std::nothrow) librados::Rados *[num_rados_handles];
   if (!rados) {
     ret = -ENOMEM;
     return ret;
@@ -3140,7 +3140,7 @@ int RGWRados::init_rados()
 
   for (uint32_t i=0; i < num_rados_handles; i++) {
 
-    rados[i] = new Rados();
+    rados[i] = new (std::nothrow) Rados();
     if (!rados[i]) {
       ret = -ENOMEM;
       goto fail;
@@ -3157,14 +3157,23 @@ int RGWRados::init_rados()
     }
   }
 
-  cr_registry = new RGWCoroutinesManagerRegistry(cct);
+  cr_registry = new (std::nothrow) RGWCoroutinesManagerRegistry(cct);
+  if (!cr_registry) {
+    ret = -ENOMEM;
+    goto fail;
+  }
+
   ret =  cr_registry->hook_to_admin_command("cr dump");
   if (ret < 0) {
     goto fail;
   }
 
-  meta_mgr = new RGWMetadataManager(cct, this);
-  data_log = new RGWDataChangesLog(cct, this);
+  meta_mgr = new (std::nothrow) RGWMetadataManager(cct, this);
+  data_log = new (std::nothrow) RGWDataChangesLog(cct, this);
+  if (!meta_mgr || !data_log) {
+    ret = -ENOMEM;
+    goto fail;
+  }
 
   return ret;
 
@@ -3180,6 +3189,9 @@ fail:
     delete[] rados;
     rados = NULL;
   }
+
+  delete cr_registry;
+  cr_registry = NULL;
 
   return ret;
 }
