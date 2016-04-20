489362876943bd8c9c484049ef5a80100517ce51
xie xingguo <xie.xingguo@zte.com.cn>
2016-04-20 14:59:44 +0800

    osd: kill PG_STATE_SCRUBQ state, which is never used
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- src/osd/osd_types.h
+++ src/osd/osd_types.h
@@ -905,7 +905,7 @@ inline ostream& operator<<(ostream& out, const osd_stat_t& s) {
 //#define PG_STATE_STRAY      (1<<6)  // i must notify the primary i exist.
 #define PG_STATE_SPLITTING    (1<<7)  // i am splitting
 #define PG_STATE_SCRUBBING    (1<<8)  // scrubbing
-#define PG_STATE_SCRUBQ       (1<<9)  // queued for scrub
+//#define PG_STATE_SCRUBQ       (1<<9)  // queued for scrub
 #define PG_STATE_DEGRADED     (1<<10) // pg contains objects with reduced redundancy
 #define PG_STATE_INCONSISTENT (1<<11) // pg replicas are inconsistent (but shouldn't be)
 #define PG_STATE_PEERING      (1<<12) // pg is (re)peering
