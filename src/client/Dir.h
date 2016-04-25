ba9fa11a235dc6c55b3b03e3a98b17316d06cf27
Yan, Zheng <zyan@redhat.com>
2016-04-25 23:24:46 +0800

    client: move dir_{release,ordered}_count into class Inode
    We close Inode::dir when it's empty. Once closing the dir, we lose
    track of {release,ordered}_count. This causes direcotry to be wrongly
    marked as complete. (dir is trimmed to empty in the middle of readdir)
    
    Signed-off-by: Yan, Zheng <zyan@redhat.com>
    (cherry picked from commit 235fcf68f5d96a4d9d6cc260d12da912fa7ea4a8)
    
    Signed-off-by: Greg Farnum <gfarnum@redhat.com

--- src/client/Dir.h
+++ src/client/Dir.h
@@ -8,10 +8,8 @@ class Dir {
   Inode    *parent_inode;  // my inode
   ceph::unordered_map<string, Dentry*> dentries;
   xlist<Dentry*> dentry_list;
-  uint64_t release_count;
-  uint64_t ordered_count;
 
-  explicit Dir(Inode* in) : release_count(0), ordered_count(0) { parent_inode = in; }
+  explicit Dir(Inode* in) { parent_inode = in; }
 
   bool is_empty() {  return dentries.empty(); }
 };
