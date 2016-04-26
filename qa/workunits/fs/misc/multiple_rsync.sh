49f1dae568f9cbc62abf5deac2d4d6829516cdf4
Li Peng <lip@dtdream.com>
2016-04-26 15:56:26 +0800

    qa: fix typo
    Signed-off-by: Li Peng <lip@dtdream.com>

--- qa/workunits/fs/misc/multiple_rsync.sh
+++ qa/workunits/fs/misc/multiple_rsync.sh
@@ -12,7 +12,7 @@ sudo rsync -av $PAYLOAD payload.1
 sudo rsync -av $PAYLOAD payload.2
 
 # this shouldn't transfer any additional files
-echo we should get 4 here if no additional files are transfered
+echo we should get 4 here if no additional files are transferred
 sudo rsync -auv $PAYLOAD payload.1 | tee /tmp/$$
 hexdump -C /tmp/$$
 wc -l /tmp/$$ | grep 4
