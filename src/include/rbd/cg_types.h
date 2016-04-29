5ec607718b2cb858cb3172d0330512e3c45df252
Victor Denisov <denisovenator@gmail.com>
2016-04-29 14:46:59 -0700

    Fix copyright

--- src/include/rbd/cg_types.h
+++ src/include/rbd/cg_types.h
@@ -3,7 +3,7 @@
 /*
  * Ceph - scalable distributed file system
  *
- * Copyright (C) 2004-2006 Sage Weil <sage@newdream.net>
+ * Copyright (C) 2016 Victor Denisov <vdenisov@mirantis.com>
  *
  * This is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
