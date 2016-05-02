3fea49becb7a760984be2d611c4c89f49e50ecc8
Sage Weil <sage@redhat.com>
2016-05-02 14:52:59 -0400

    test/mon/mon-created-time: put on unique port
    7122 is also used by test/osd/osd-reactivate.sh
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/test/mon/mon-created-time.sh
+++ src/test/mon/mon-created-time.sh
@@ -19,7 +19,7 @@ function run() {
     local dir=$1
     shift
 
-    export CEPH_MON="127.0.0.1:7122" # git grep '\<7122\>' : there must be only one
+    export CEPH_MON="127.0.0.1:7125" # git grep '\<7125\>' : there must be only one
     export CEPH_ARGS
     CEPH_ARGS+="--fsid=$(uuidgen) --auth-supported=none "
     CEPH_ARGS+="--mon-host=$CEPH_MON "
