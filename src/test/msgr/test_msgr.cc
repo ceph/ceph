bee9ca67475fc6e0d28dfa77de45b0358440b747
Haomai Wang <haomai@xsky.com>
2016-05-05 18:21:45 +0800

    test_msgr/async_networkstack: fix coremask option value
    Signed-off-by: Haomai Wang <haomai@xsky.com>

--- src/test/msgr/test_msgr.cc
+++ src/test/msgr/test_msgr.cc
@@ -62,7 +62,7 @@ class MessengerTest : public ::testing::TestWithParam<const char*> {
         g_ceph_context->_conf->set_val("ms_dpdk_debug_allow_loopback", "true", false, false);
         g_ceph_context->_conf->set_val("ms_async_op_threads", "2", false, false);
         g_ceph_context->_conf->set_val("ms_async_transport_type", "dpdk", false, false);
-        g_ceph_context->_conf->set_val("ms_dpdk_coremask", "3", false, false);
+        g_ceph_context->_conf->set_val("ms_dpdk_coremask", "0x7", false, false);
         g_ceph_context->_conf->set_val("ms_dpdk_host_ipv4_addr", "127.0.0.1", false, false);
         g_ceph_context->_conf->set_val("ms_dpdk_gateway_ipv4_addr", "127.0.0.1", false, false);
         g_ceph_context->_conf->set_val("ms_dpdk_netmask_ipv4_addr", "255.255.255.0", false, false);
