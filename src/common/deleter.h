cc2f16199e6b5f8c2eb31a4423637cf065b86f15
Haomai Wang <haomai@xsky.com>
2016-05-05 17:08:10 +0800

    deleter: if append the same impl just exit
    Signed-off-by: Haomai Wang <haomai@xsky.com>

--- src/common/deleter.h
+++ src/common/deleter.h
@@ -208,7 +208,8 @@ inline void deleter::append(deleter d) {
   impl* next_impl = _impl;
   deleter* next_d = this;
   while (next_impl) {
-    assert(next_impl != d._impl);
+    if (next_impl == d._impl)
+      return ;
     if (is_raw_object(next_impl)) {
       next_d->_impl = next_impl = new free_deleter_impl(to_raw_object(next_impl));
     }
