df144116171db92f56e3ce2b7a06438a8c019df3
Piotr Dałek <git@predictor.org.pl>
2016-05-05 21:03:37 +0200

    msg/simple: add ms_handle_refused callback
    Added new callback (ms_handle_refused) to dispatchers. It is called
    once connection attempt fails with ECONNREFUSED.
    Also added dummy ms_handle_refused handlers across codebase.
    
    Signed-off-by: Piotr Dałek <git@predictor.org.pl>

--- src/msg/DispatchQueue.h
+++ src/msg/DispatchQueue.h
@@ -90,7 +90,7 @@ class DispatchQueue {
 
   std::atomic<uint64_t> next_id;
     
-  enum { D_CONNECT = 1, D_ACCEPT, D_BAD_REMOTE_RESET, D_BAD_RESET, D_NUM_CODES };
+  enum { D_CONNECT = 1, D_ACCEPT, D_BAD_REMOTE_RESET, D_BAD_RESET, D_CONN_REFUSED, D_NUM_CODES };
 
   /**
    * The DispatchThread runs dispatch_entry to empty out the dispatch_queue.
@@ -183,6 +183,16 @@ class DispatchQueue {
       0,
       CEPH_MSG_PRIO_HIGHEST,
       QueueItem(D_BAD_RESET, con));
+    cond.Signal();
+  }
+  void queue_refused(Connection *con) {
+    Mutex::Locker l(lock);
+    if (stop)
+      return;
+    mqueue.enqueue_strict(
+      0,
+      CEPH_MSG_PRIO_HIGHEST,
+      QueueItem(D_CONN_REFUSED, con));
     cond.Signal();
   }
 
