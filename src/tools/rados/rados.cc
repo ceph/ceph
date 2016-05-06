4674092d0f264fd7c5c777b8e363c4c703535aaf
David Zafman <dzafman@redhat.com>
2016-05-06 16:47:55 -0700

    rados: Simplify do_get_inconsistent_cmd() logic
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/tools/rados/rados.cc
+++ src/tools/rados/rados.cc
@@ -1595,7 +1595,6 @@ static int do_get_inconsistent_cmd(const std::vector<const char*> &nargs,
   }
   uint32_t interval = 0, first_interval = 0;
   const unsigned max_item_num = 32;
-  bool opened = false;
   for (librados::object_id_t start;;) {
     std::vector<T> items;
     auto completion = librados::Rados::aio_create_completion();
@@ -1621,7 +1620,6 @@ static int do_get_inconsistent_cmd(const std::vector<const char*> &nargs,
       formatter.open_object_section("info");
       formatter.dump_int("epoch", interval);
       formatter.open_array_section("inconsistents");
-      opened = true;
     }
     for (auto& inc : items) {
       formatter.open_object_section("inconsistent");
@@ -1630,16 +1628,14 @@ static int do_get_inconsistent_cmd(const std::vector<const char*> &nargs,
     }
     if (items.size() < max_item_num) {
       formatter.close_section();
+      formatter.close_section();
+      formatter.flush(cout);
       break;
     }
     if (!items.empty()) {
       start = items.back().object;
     }
     items.clear();
-  }
-  if (opened) {
-    formatter.close_section();
-    formatter.flush(cout);
   }
   return ret;
 }
