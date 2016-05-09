aee6c52822b10e15979d9b49d2ca16a639666bad
Li Wang <li.wang@kylin-cloud.com>
2016-05-09 22:08:54 +0800

    librados: fix operate()
    ObjectOperationImpl has changed, no longer equal to ::ObjectOperation*,
    this patch fixes the wrong pointer conversion.
    
    Signed-off-by: Li Wang <li.wang@kylin-cloud.com>

--- src/librados/librados.cc
+++ src/librados/librados.cc
@@ -1430,13 +1430,13 @@ static int translate_flags(int flags)
 int librados::IoCtx::operate(const std::string& oid, librados::ObjectWriteOperation *o)
 {
   object_t obj(oid);
-  return io_ctx_impl->operate(obj, (::ObjectOperation*)o->impl, (ceph::real_time *)o->impl->prt);
+  return io_ctx_impl->operate(obj, &o->impl->o, (ceph::real_time *)o->impl->prt);
 }
 
 int librados::IoCtx::operate(const std::string& oid, librados::ObjectReadOperation *o, bufferlist *pbl)
 {
   object_t obj(oid);
-  return io_ctx_impl->operate_read(obj, (::ObjectOperation*)o->impl, pbl);
+  return io_ctx_impl->operate_read(obj, &o->impl->o, pbl);
 }
 
 int librados::IoCtx::aio_operate(const std::string& oid, AioCompletion *c,
