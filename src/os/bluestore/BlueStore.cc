7819dea85a30da5175ed38e5bfa7836c4e4940ee
Sage Weil <sage@redhat.com>
2016-05-09 10:36:14 -0400

    os/bluestore: fix bad iterator deref in _do_zero_tail_extent
    The extent_map may be empty, or not contain anything at this offset.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -5840,6 +5840,9 @@ void BlueStore::_do_zero_tail_extent(
   bp = o->onode.seek_lextent(offset);
   pp = o->onode.find_lextent(o->onode.size);
 
+  if (pp == o->onode.extent_map.end())
+    return;
+
   dout(10) << __func__ << " offset 0x" << std::hex << offset << " extent 0x"
 	   << pp->first << std::dec << ": " << pp->second << dendl;
   assert(offset > o->onode.size);
