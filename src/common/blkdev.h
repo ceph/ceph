ccfc93236a18a3a736e72e4a34ae853390193b50
Jianpeng Ma <jianpeng.ma@intel.com>
2016-05-16 19:27:57 +0800

    common/blkdev: Add new api block_device_is_rotational.
    It use to judge device whether rotational device.
    
    Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

--- src/common/blkdev.h
+++ src/common/blkdev.h
@@ -8,6 +8,7 @@ extern int get_block_device_base(const char *dev, char *out, size_t out_len);
 extern int get_block_device_size(int fd, int64_t *psize);
 extern int64_t get_block_device_int_property(const char *devname, const char *property);
 extern bool block_device_support_discard(const char *devname);
+extern bool block_device_is_rotational(const char *devname);
 extern int block_device_discard(int fd, int64_t offset, int64_t len);
 extern int get_device_by_uuid(uuid_d dev_uuid, const char* label,
 		char* partition, char* device);
