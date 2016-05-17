71cf733f6ce98805c1413fb67adcc3274bdcc1a6
John Spray <john.spray@redhat.com>
2016-05-17 17:53:56 +0100

    mon: tolerate missing osd metadata
    Just because one OSD's metadata is missing,
    don't give up on outputting all the other
    OSDs' metadata.
    
    Signed-off-by: John Spray <john.spray@redhat.com>
    (cherry picked from commit 2c7dd5f22288c4aee814573baa6af131421f36a4)

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -3279,8 +3279,15 @@ bool OSDMonitor::preprocess_command(MonOpRequestRef op)
           f->open_object_section("osd");
           f->dump_unsigned("id", i);
           r = dump_osd_metadata(i, f.get(), NULL);
-          if (r < 0)
+          if (r == -EINVAL || r == -ENOENT) {
+            // Drop error, continue to get other daemons' metadata
+            dout(4) << "No metadata for osd." << i << dendl;
+            r = 0;
+            continue;
+          } else if (r < 0) {
+            // Unexpected error
             goto reply;
+          }
           f->close_section();
         }
       }
