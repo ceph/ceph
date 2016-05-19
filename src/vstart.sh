afa95cbe3ab7fb62ae0ea4935fc6726f6b5145dd
Ved-vampir <akiselyova@mirantis.com>
2016-05-19 17:03:54 +0300

    vstart: fix for rgw restart without -n option Signed-of-by: Alyona Kiseleva <akiselyova@mirantis.com>

--- src/vstart.sh
+++ src/vstart.sh
@@ -750,43 +750,6 @@ do_hitsets $hitset
 
 do_rgw()
 {
-    # Create S3 user
-    local akey='0555b35654ad1656d804'
-    local skey='h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q=='
-    echo "setting up user testid"
-    $CEPH_BIN/radosgw-admin user create --uid testid --access-key $akey --secret $skey --display-name 'M. Tester' --email tester@ceph.com -c $conf_fn > /dev/null
-
-    # Create S3-test users
-    # See: https://github.com/ceph/s3-tests
-    echo "setting up s3-test users"
-    $CEPH_BIN/radosgw-admin user create \
-        --uid 0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef \
-        --access-key ABCDEFGHIJKLMNOPQRST \
-        --secret abcdefghijklmnopqrstuvwxyzabcdefghijklmn \
-        --display-name youruseridhere \
-        --email s3@example.com -c $conf_fn > /dev/null
-    $CEPH_BIN/radosgw-admin user create \
-        --uid 56789abcdef0123456789abcdef0123456789abcdef0123456789abcdef01234 \
-        --access-key NOPQRSTUVWXYZABCDEFG \
-        --secret nopqrstuvwxyzabcdefghijklmnabcdefghijklm \
-        --display-name john.doe \
-        --email john.doe@example.com -c $conf_fn > /dev/null
-
-    # Create Swift user
-    echo "setting up user tester"
-    $CEPH_BIN/radosgw-admin user create -c $conf_fn --subuser=test:tester --display-name=Tester-Subuser --key-type=swift --secret=testing --access=full > /dev/null
-
-    echo ""
-    echo "S3 User Info:"
-    echo "  access key:  $akey"
-    echo "  secret key:  $skey"
-    echo ""
-    echo "Swift User Info:"
-    echo "  account   : test"
-    echo "  user      : tester"
-    echo "  password  : testing"
-    echo ""
-
     # Start server
     echo start rgw on http://localhost:$CEPH_RGW_PORT
     RGWDEBUG=""
@@ -797,6 +760,51 @@ do_rgw()
     RGWSUDO=
     [ $CEPH_RGW_PORT -lt 1024 ] && RGWSUDO=sudo
     $RGWSUDO $CEPH_BIN/radosgw -c $conf_fn --log-file=${CEPH_OUT_DIR}/rgw.log ${RGWDEBUG} --debug-ms=1
+
+    # Create S3 user
+    local akey='0555b35654ad1656d804'
+    local skey='h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q=='
+    if [ "$new" -eq 1 ] || [ -z "$($CEPH_BIN/radosgw-admin user info --uid testid | grep "user_id")" ]; then
+        echo "setting up user testid"
+        $CEPH_BIN/radosgw-admin user create --uid testid --access-key $akey --secret $skey --display-name 'M. Tester' --email tester@ceph.com -c $conf_fn > /dev/null
+    fi
+
+    # Create S3-test users
+    # See: https://github.com/ceph/s3-tests
+    if [ "$new" -eq 1 ] || [ -z "$($CEPH_BIN/radosgw-admin user info --uid 0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef | grep "user_id")" ]; then
+        echo "setting up s3-test users"
+        $CEPH_BIN/radosgw-admin user create \
+            --uid 0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef \
+            --access-key ABCDEFGHIJKLMNOPQRST \
+            --secret abcdefghijklmnopqrstuvwxyzabcdefghijklmn \
+            --display-name youruseridhere \
+            --email s3@example.com -c $conf_fn > /dev/null
+    fi
+    if [ "$new" -eq 1 ] || [ -z "$($CEPH_BIN/radosgw-admin user info --uid 56789abcdef0123456789abcdef0123456789abcdef0123456789abcdef01234 | grep "user_id")" ]; then
+        $CEPH_BIN/radosgw-admin user create \
+            --uid 56789abcdef0123456789abcdef0123456789abcdef0123456789abcdef01234 \
+            --access-key NOPQRSTUVWXYZABCDEFG \
+            --secret nopqrstuvwxyzabcdefghijklmnabcdefghijklm \
+            --display-name john.doe \
+            --email john.doe@example.com -c $conf_fn > /dev/null
+    fi
+
+    # Create Swift user
+    if [ "$new" -eq 1 ] || [ -z "$($CEPH_BIN/radosgw-admin user info --subuser=test:tester | grep "user_id")" ]; then    
+        echo "setting up user tester"
+        $CEPH_BIN/radosgw-admin user create -c $conf_fn --subuser=test:tester --display-name=Tester-Subuser --key-type=swift --secret=testing --access=full > /dev/null
+    fi
+    
+    echo ""
+    echo "S3 User Info:"
+    echo "  access key:  $akey"
+    echo "  secret key:  $skey"
+    echo ""
+    echo "Swift User Info:"
+    echo "  account   : test"
+    echo "  user      : tester"
+    echo "  password  : testing"
+    echo ""
 }
 if [ "$start_rgw" -eq 1 ]; then
     do_rgw
