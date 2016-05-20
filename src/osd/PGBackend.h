6284f745157458439699c76e8616455c50d1eb71
David Zafman <dzafman@redhat.com>
2016-05-20 15:20:18 -0700

    osd: Handle recovery read errors
    Fixes: http://tracker.ceph.com/issues/13937
    
    Signed-off-by: David Zafman <dzafman@redhat.com>
    (cherry picked from commit c51d70e1e837c972e42ddd5fa66f7ca4477b95cc)
    
    Conflicts:
    	src/osd/ReplicatedPG.h (trivial)

--- src/osd/PGBackend.h
+++ src/osd/PGBackend.h
@@ -94,7 +94,7 @@ struct shard_info_wrapper;
        pg_shard_t peer,
        const hobject_t oid) = 0;
 
-     virtual void failed_push(pg_shard_t from, const hobject_t &soid) = 0;
+     virtual void failed_push(const list<pg_shard_t> &from, const hobject_t &soid) = 0;
      
      virtual void cancel_pull(const hobject_t &soid) = 0;
 
