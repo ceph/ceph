626d795139a6c5104be098780b70500c2de9b8f4
Yehuda Sadeh <yehuda@redhat.com>
2016-06-02 04:28:30 -0700

    rgw: can set negative max_buckets on RGWUserInfo
    This can be used to disable user's bucket creation
    
    Fixes: http://tracker.ceph.com/issues/14534
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -1793,6 +1793,9 @@ int RGWCreateBucket::verify_permission()
       << dendl;
     return -EACCES;
   }
+  if (s->user->max_buckets < 0) {
+    return -EPERM;
+  }
 
   if (s->user->max_buckets) {
     RGWUserBuckets buckets;
