6fe6a325b8542672bf61c2f8a67a04f948c7846c
Samuel Just <sjust@redhat.com>
2016-06-02 17:38:05 -0700

    hobject: enforce max canonical value
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/common/hobject.cc
+++ src/common/hobject.cc
@@ -112,6 +112,7 @@ void hobject_t::encode(bufferlist& bl) const
   ::encode(max, bl);
   ::encode(nspace, bl);
   ::encode(pool, bl);
+  assert(!max || (*this == hobject_t(hobject_t::get_max())));
   ENCODE_FINISH(bl);
 }
 
