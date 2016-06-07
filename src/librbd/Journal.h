5aa6eb2782bdb5cde6a050a5f7a4e683800414c0
Jason Dillaman <dillaman@redhat.com>
2016-06-07 12:42:52 -0400

    librbd: stop journal recorder before starting external replay
    Fixes: http://tracker.ceph.com/issues/16165
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/Journal.h
+++ src/librbd/Journal.h
@@ -157,7 +157,8 @@ public:
     return op_tid;
   }
 
-  int start_external_replay(journal::Replay<ImageCtxT> **journal_replay);
+  void start_external_replay(journal::Replay<ImageCtxT> **journal_replay,
+                             Context *on_finish);
   void stop_external_replay();
 
 private:
@@ -303,6 +304,8 @@ private:
 
   void complete_event(typename Events::iterator it, int r);
 
+  void start_append();
+
   void handle_initialized(int r);
   void handle_get_tags(int r);
 
@@ -310,6 +313,10 @@ private:
   void handle_replay_complete(int r);
   void handle_replay_process_ready(int r);
   void handle_replay_process_safe(ReplayEntry replay_entry, int r);
+
+  void handle_start_external_replay(int r,
+                                    journal::Replay<ImageCtxT> **journal_replay,
+                                    Context *on_finish);
 
   void handle_flushing_restart(int r);
   void handle_flushing_replay();
