abd84f4f1b804aa6a90ed5f8b94c1cfddc4e8956
Sage Weil <sage@redhat.com>
2016-06-08 15:02:55 -0400

    os/filestore: fix statfs
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/filestore/FileStore.cc
+++ src/os/filestore/FileStore.cc
@@ -713,9 +713,7 @@ int FileStore::statfs(struct store_statfs_t *buf0)
   }
   buf0->blocks = buf.f_blocks;
   buf0->bsize = buf.f_bsize;
-
-  buf0->allocated = buf.f_bavail;
-
+  buf0->available = buf.f_bavail * buf.f_bsize;
   return 0;
 }
 
