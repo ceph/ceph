c2824c5e96609a7dbb67354ab06833bdf19834de
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-06-13 16:12:44 +0200

    rgw: RGWGetObj_ObjStore_SWIFT delegates error handling.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_rest_swift.cc
+++ src/rgw/rgw_rest_swift.cc
@@ -1221,8 +1221,16 @@ int RGWGetObj_ObjStore_SWIFT::get_params()
 
 int RGWGetObj_ObjStore_SWIFT::send_response_data_error()
 {
-  bufferlist bl;
-  return send_response_data(bl, 0, 0);
+  std::string error_content;
+  op_ret = error_handler(op_ret, &error_content);
+  if (! op_ret) {
+    /* The error handler has taken care of the error. */
+    return 0;
+  }
+
+  bufferlist error_bl;
+  error_bl.append(error_content);
+  return send_response_data(error_bl, 0, error_bl.length());
 }
 
 int RGWGetObj_ObjStore_SWIFT::send_response_data(bufferlist& bl,
