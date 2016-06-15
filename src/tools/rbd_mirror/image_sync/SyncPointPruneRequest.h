08c28920d5434459de66e14386dfa154dda61de5
Jason Dillaman <dillaman@redhat.com>
2016-06-15 17:42:59 -0400

    rbd-mirror: prune sync points referencing missing snapshots
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>
    (cherry picked from commit 378914f6b8a8a8c5517fee8fa186268078fbcbfb)

--- src/tools/rbd_mirror/image_sync/SyncPointPruneRequest.h
+++ src/tools/rbd_mirror/image_sync/SyncPointPruneRequest.h
@@ -73,6 +73,8 @@ private:
   MirrorPeerClientMeta m_client_meta_copy;
   std::list<std::string> m_snap_names;
 
+  bool m_invalid_master_sync_point = false;
+
   void send_remove_snap();
   void handle_remove_snap(int r);
 
