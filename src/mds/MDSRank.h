92154c80a49cad7f77d562d93f7ad1b4f4a653fe
Zhi Zhang <willzzhang@tencent.com>
2016-06-15 11:28:34 +0800

    Ceph status outputs mds slow request for better monitoring
    Signed-off-by: Zhi Zhang <zhangz.david@outlook.com>

--- src/mds/MDSRank.h
+++ src/mds/MDSRank.h
@@ -248,6 +248,8 @@ class MDSRank {
      * Emit clog warnings for any ops reported as warnings by optracker
      */
     void check_ops_in_flight();
+  
+    int mds_slow_req_count;
 
     /**
      * Share MDSMap with clients
@@ -361,6 +363,8 @@ class MDSRank {
     MDSMap *get_mds_map() { return mdsmap; }
 
     int get_req_rate() { return logger->get(l_mds_request); }
+  
+    int get_mds_slow_req_count() const { return mds_slow_req_count; }
 
     void dump_status(Formatter *f) const;
 
