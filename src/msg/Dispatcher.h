aa6f4064741e539a2a27119cf880e2d5685dac8f
Sage Weil <sage@redhat.com>
2016-06-15 12:54:01 -0400

    msg/Dispatcher: add ms_get_auth1_authorizer_keystore
    This is there to provide the keyring used for authenticating msgr1
    authorizers.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/msg/Dispatcher.h
+++ src/msg/Dispatcher.h
@@ -26,6 +26,7 @@ class Connection;
 class AuthAuthorizer;
 class CryptoKey;
 class CephContext;
+class KeyStore;
 
 class Dispatcher {
 public:
@@ -171,6 +172,16 @@ public:
    */
   virtual int ms_handle_authentication(Connection *con) {
     return 0;
+  }
+
+  /**
+   * get authentication keyring
+   *
+   * Return the keyring to use for authentication with msgr1.  Remove me
+   * someday.
+   */
+  virtual KeyStore* ms_get_auth1_authorizer_keystore() {
+    return nullptr;
   }
 
   /**
