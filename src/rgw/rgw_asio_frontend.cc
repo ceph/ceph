c5543a495177461785c90a6bc322118b2da1d1ae
Casey Bodley <cbodley@redhat.com>
2016-06-17 22:58:05 -0400

    rgw: add skeleton for boost::asio frontend
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

---
+++ src/rgw/rgw_asio_frontend.cc
@@ -0,0 +1,38 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#include "rgw_asio_frontend.h"
+
+RGWAsioFrontend::RGWAsioFrontend(const RGWProcessEnv& env)
+{
+}
+
+RGWAsioFrontend::~RGWAsioFrontend()
+{
+}
+
+int RGWAsioFrontend::init()
+{
+  return 0;
+}
+
+int RGWAsioFrontend::run()
+{
+  return 0;
+}
+
+void RGWAsioFrontend::stop()
+{
+}
+
+void RGWAsioFrontend::join()
+{
+}
+
+void RGWAsioFrontend::pause_for_new_config()
+{
+}
+
+void RGWAsioFrontend::unpause_with_new_config(RGWRados *store)
+{
+}
---
+++ src/rgw/rgw_asio_frontend.h
@@ -0,0 +1,23 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef RGW_ASIO_FRONTEND_H
+#define RGW_ASIO_FRONTEND_H
+
+#include "rgw_frontend.h"
+
+class RGWAsioFrontend : public RGWFrontend {
+public:
+  RGWAsioFrontend(const RGWProcessEnv& env);
+  ~RGWAsioFrontend();
+
+  int init() override;
+  int run() override;
+  void stop() override;
+  void join() override;
+
+  void pause_for_new_config() override;
+  void unpause_with_new_config(RGWRados *store) override;
+};
+
+#endif // RGW_ASIO_FRONTEND_H
