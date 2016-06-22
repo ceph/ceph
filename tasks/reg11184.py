2fdbd38719cc536860faabd4f58a33cc8ec05e9f
Samuel Just <sjust@redhat.com>
2016-06-22 13:55:15 -0700

    divergent_priors: we no longer track divergent priors explicitely
    The rest of the test is still valuable to ensure that we detect missing
    items which are not in the log, but now that the missing set is
    explicitely persisted, the divergent priors set isn't a special case
    and won't have special log lines to check for.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- tasks/reg11184.py
+++ tasks/reg11184.py
@@ -230,13 +230,6 @@ def task(ctx, config):
 
     (remote,) = ctx.\
         cluster.only('osd.{o}'.format(o=divergent)).remotes.iterkeys()
-    msg = "dirty_divergent_priors: true, divergent_priors: %d" \
-          % (DIVERGENT_WRITE + DIVERGENT_REMOVE)
-    cmd = 'grep "{msg}" /var/log/ceph/ceph-osd.{osd}.log'\
-          .format(msg=msg, osd=divergent)
-    proc = remote.run(args=cmd, wait=True, check_status=False)
-    assert proc.exitstatus == 0
-
     cmd = 'rm {file}'.format(file=expfile)
     remote.run(args=cmd, wait=True)
     log.info("success")
