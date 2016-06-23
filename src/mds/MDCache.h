55367c5cda09167778839eb8474f86903857f53a
Yan, Zheng <zyan@redhat.com>
2016-06-23 16:59:46 +0800

    mds: rebuild the internal states that tracking pending snapflush
    Signed-off-by: Yan, Zheng <zyan@redhat.com>
    (cherry picked from commit 1b7d198f633a8608b704f82f01a3f4a4a3a4892b)

--- src/mds/MDCache.h
+++ src/mds/MDCache.h
@@ -560,11 +560,18 @@ public:
   }
 
   // [reconnect/rejoin caps]
-  map<CInode*,map<client_t, inodeno_t> >  reconnected_caps;   // inode -> client -> realmino
+  struct reconnected_cap_info_t {
+    inodeno_t realm_ino;
+    snapid_t snap_follows;
+    reconnected_cap_info_t() : realm_ino(0), snap_follows(0) {}
+  };
+  map<CInode*,map<client_t, reconnected_cap_info_t> >  reconnected_caps;   // inode -> client -> snap_follows,realmino
   map<inodeno_t,map<client_t, snapid_t> > reconnected_snaprealms;  // realmino -> client -> realmseq
 
-  void add_reconnected_cap(CInode *in, client_t client, inodeno_t realm) {
-    reconnected_caps[in][client] = realm;
+  void add_reconnected_cap(CInode *in, client_t client, const cap_reconnect_t& icr) {
+    reconnected_cap_info_t &info = reconnected_caps[in][client];
+    info.realm_ino = inodeno_t(icr.capinfo.snaprealm);
+    info.snap_follows = icr.snap_follows;
   }
   void add_reconnected_snaprealm(client_t client, inodeno_t ino, snapid_t seq) {
     reconnected_snaprealms[ino][client] = seq;
@@ -588,13 +595,15 @@ public:
 
   // cap imports.  delayed snap parent opens.
   //  realm inode -> client -> cap inodes needing to split to this realm
-  map<CInode*,map<client_t, set<inodeno_t> > > missing_snap_parents; 
+  map<CInode*,set<CInode*> > missing_snap_parents;
   map<client_t,set<CInode*> > delayed_imported_caps;
 
   void do_cap_import(Session *session, CInode *in, Capability *cap,
 		     uint64_t p_cap_id, ceph_seq_t p_seq, ceph_seq_t p_mseq,
 		     int peer, int p_flags);
   void do_delayed_cap_imports();
+  void rebuild_need_snapflush(CInode *head_in, SnapRealm *realm, client_t client,
+			      snapid_t snap_follows);
   void check_realm_past_parents(SnapRealm *realm);
   void open_snap_parents();
 
