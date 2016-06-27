265730aa5a139752ee1e284470a5301edcb8438a
Sage Weil <sage@redhat.com>
2016-06-27 13:09:22 -0700

    os/FuseStore: only flush if dirty
    No need to rewrite the object unless it was changed.
    
    This partially fixes truncate.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/FuseStore.h
+++ src/os/FuseStore.h
@@ -5,17 +5,31 @@
 #define CEPH_OS_FUSESTORE_H
 
 #include <string>
-#include "common/Thread.h"
+#include <map>
+#include <mutex>
 
-using std::string;
+#include "common/Thread.h"
+#include "include/buffer.h"
 
 class ObjectStore;
 
 class FuseStore {
 public:
   ObjectStore *store;
-  string mount_point;
+  std::string mount_point;
   struct fs_info *info;
+  std::mutex lock;
+
+  struct OpenFile {
+    std::string path;
+    bufferlist bl;
+    bool dirty = false;
+    int ref = 0;
+  };
+  std::map<std::string,OpenFile*> open_files;
+
+  int open_file(std::string p, struct fuse_file_info *fi,
+		std::function<int(bufferlist *bl)> f);
 
   class FuseThread : public Thread {
     FuseStore *fs;
@@ -27,7 +41,7 @@ public:
     }
   } fuse_thread;
 
-  FuseStore(ObjectStore *s, string p);
+  FuseStore(ObjectStore *s, std::string p);
   ~FuseStore();
 
   int main();
