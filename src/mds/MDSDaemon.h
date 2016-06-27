a35d72952c940b7834154c16897ebdc1179dbef9
Douglas Fuller <dfuller@redhat.com>
2016-06-27 12:30:11 -0700

    mds: Kill C_SaferCond in evict_sessions()
    MDSRankDispatcher::evict_sessions waits on a C_SaferCond for
    kill_session to complete on each of its victims. Change the
    command handling flow to pass command messages all the way down
    to MDSRankDispatcher. Extract the MDSDaemon's reply path into a
    static function callable from a new context in the MDSRankDispatcher.
    
    See: http://tracker.ceph.com/issues/16288
    Signed-off-by: Douglas Fuller <dfuller@redhat.com>

--- src/mds/MDSDaemon.h
+++ src/mds/MDSDaemon.h
@@ -187,13 +187,17 @@ protected:
   bool handle_core_message(Message *m);
   
   // special message types
+  friend class C_MDS_Send_Command_Reply;
+  static void send_command_reply(MCommand *m, MDSRank* mds_rank, int r,
+				 bufferlist outbl, const std::string& outs);
   int _handle_command_legacy(std::vector<std::string> args);
   int _handle_command(
       const cmdmap_t &cmdmap,
-      bufferlist const &inbl,
+      MCommand *m,
       bufferlist *outbl,
       std::string *outs,
-      Context **run_later);
+      Context **run_later,
+      bool *need_reply);
   void handle_command(class MMonCommand *m);
   void handle_command(class MCommand *m);
   void handle_mds_map(class MMDSMap *m);
