ac3c0940b7d13e68b632c386c382169922c090c7
Wanlong Gao <wanlong.gao@easystack.cn>
2016-06-28 15:51:52 +0800

    OSD: split out handle_pg_scrub() from handle_scrub()
    Signed-off-by: Wanlong Gao <wanlong.gao@easystack.cn>

--- src/osd/OSD.h
+++ src/osd/OSD.h
@@ -2456,6 +2456,7 @@ private:
   static int write_meta(ObjectStore *store,
 			uuid_d& cluster_fsid, uuid_d& osd_fsid, int whoami);
 
+  void handle_pg_scrub(struct MOSDScrub *m, PG* pg);
   void handle_scrub(struct MOSDScrub *m);
   void handle_osd_ping(class MOSDPing *m);
   void handle_op(OpRequestRef& op, OSDMapRef& osdmap);
