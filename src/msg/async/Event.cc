37629d587de3e5bca49be0f1fbf05a6c09824b0c
Haomai Wang <haomai@xsky.com>
2016-06-29 14:09:00 +0800

    msg/async: add assert to ensure create/delete file event within thread
    Signed-off-by: Haomai Wang <haomai@xsky.com>

--- src/msg/async/Event.cc
+++ src/msg/async/Event.cc
@@ -149,6 +149,7 @@ void EventCenter::set_owner()
 
 int EventCenter::create_file_event(int fd, int mask, EventCallbackRef ctxt)
 {
+  assert(in_thread());
   int r = 0;
   if (fd >= nevent) {
     int new_size = nevent << 2;
@@ -193,7 +194,7 @@ int EventCenter::create_file_event(int fd, int mask, EventCallbackRef ctxt)
 
 void EventCenter::delete_file_event(int fd, int mask)
 {
-  assert(fd >= 0);
+  assert(in_thread() && fd >= 0);
   if (fd >= nevent) {
     ldout(cct, 1) << __func__ << " delete event fd=" << fd << " is equal or greater than nevent=" << nevent
                   << "mask=" << mask << dendl;
