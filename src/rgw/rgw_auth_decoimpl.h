fd6e1c77efb91e55a245495cf14f77a789367534
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-07-07 19:42:24 +0200

    rgw: skip unneeded virtual specifiers in rgw_auth_decoimpl.h.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_auth_decoimpl.h
+++ src/rgw/rgw_auth_decoimpl.h
@@ -21,31 +21,31 @@ public:
       decoratee(decoratee) {
   }
 
-  virtual uint32_t get_perms_from_aclspec(const aclspec_t& aclspec) const override {
+  uint32_t get_perms_from_aclspec(const aclspec_t& aclspec) const override {
     return decoratee.get_perms_from_aclspec(aclspec);
   }
 
-  virtual bool is_admin_of(const rgw_user& uid) const override {
+  bool is_admin_of(const rgw_user& uid) const override {
     return decoratee.is_admin_of(uid);
   }
 
-  virtual bool is_owner_of(const rgw_user& uid) const override {
+  bool is_owner_of(const rgw_user& uid) const override {
     return decoratee.is_owner_of(uid);
   }
 
-  virtual uint32_t get_perm_mask() const override {
+  uint32_t get_perm_mask() const override {
     return decoratee.get_perm_mask();
   }
 
-  virtual void to_str(std::ostream& out) const override {
+  void to_str(std::ostream& out) const override {
     decoratee.to_str(out);
   }
 
-  virtual void load_acct_info(RGWUserInfo& user_info) const override {  /* out */
+  void load_acct_info(RGWUserInfo& user_info) const override {  /* out */
     return decoratee.load_acct_info(user_info);
   }
 
-  virtual void modify_request_state(req_state * s) const override {     /* in/out */
+  void modify_request_state(req_state * s) const override {     /* in/out */
     return decoratee.modify_request_state(s);
   }
 };
@@ -63,31 +63,31 @@ public:
       decoratee(std::move(decoratee)) {
   }
 
-  virtual uint32_t get_perms_from_aclspec(const aclspec_t& aclspec) const override {
+  uint32_t get_perms_from_aclspec(const aclspec_t& aclspec) const override {
     return decoratee->get_perms_from_aclspec(aclspec);
   }
 
-  virtual bool is_admin_of(const rgw_user& uid) const override {
+  bool is_admin_of(const rgw_user& uid) const override {
     return decoratee->is_admin_of(uid);
   }
 
-  virtual bool is_owner_of(const rgw_user& uid) const override {
+  bool is_owner_of(const rgw_user& uid) const override {
     return decoratee->is_owner_of(uid);
   }
 
-  virtual uint32_t get_perm_mask() const override {
+  uint32_t get_perm_mask() const override {
     return decoratee->get_perm_mask();
   }
 
-  virtual void to_str(std::ostream& out) const override {
+  void to_str(std::ostream& out) const override {
     decoratee->to_str(out);
   }
 
-  virtual void load_acct_info(RGWUserInfo& user_info) const override {  /* out */
+  void load_acct_info(RGWUserInfo& user_info) const override {  /* out */
     return decoratee->load_acct_info(user_info);
   }
 
-  virtual void modify_request_state(req_state * s) const override {     /* in/out */
+  void modify_request_state(req_state * s) const override {     /* in/out */
     return decoratee->modify_request_state(s);
   }
 };
@@ -110,8 +110,8 @@ public:
       acct_user_override(acct_user_override) {
   }
 
-  virtual void to_str(std::ostream& out) const override;
-  virtual void load_acct_info(RGWUserInfo& user_info) const override;   /* out */
+  void to_str(std::ostream& out) const override;
+  void load_acct_info(RGWUserInfo& user_info) const override;   /* out */
 };
 
 /* static declaration */
