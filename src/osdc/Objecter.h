wuxingyi/ceph-1

Objecter: add option for testing osd dup handling

src/osdc/Objecter.h
@@ -1979,7 +1979,8 @@ private:
     op_throttle_bytes(cct, "objecter_bytes",
 		      cct->_conf->objecter_inflight_op_bytes),
     op_throttle_ops(cct, "objecter_ops", cct->_conf->objecter_inflight_ops),
-    epoch_barrier(0)
+    epoch_barrier(0),
+    retry_writes_after_first_reply(cct_->conf->objecter_retry_writes_after_first_reply)
   { }
   ~Objecter();
 
@@ -2964,6 +2965,7 @@ public:
 
 private:
   epoch_t epoch_barrier;
+  bool retry_writes_after_first_reply;
 public:
   void set_epoch_barrier(epoch_t epoch);
 };

ceph/ceph

Signed-off-by: Josh Durgin <jdurgin@redhat.com>

src/osdc/Objecter.h
@@ -1951,7 +1951,8 @@ private:
     op_throttle_bytes(cct, "objecter_bytes",
 		      cct->_conf->objecter_inflight_op_bytes),
     op_throttle_ops(cct, "objecter_ops", cct->_conf->objecter_inflight_ops),
-    epoch_barrier(0)
+    epoch_barrier(0),
+    retry_writes_after_first_reply(cct->_conf->objecter_retry_writes_after_first_reply)
   { }
   ~Objecter();
 
@@ -2936,6 +2937,7 @@ public:
 
 private:
   epoch_t epoch_barrier;
+  bool retry_writes_after_first_reply;
 public:
   void set_epoch_barrier(epoch_t epoch);
 };