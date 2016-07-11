ceph/ceph

commit 61350db1bfd3f60112f976871767b820cfa9d272

suite: move config tmpfile management to Run
base_config changes over the course of the run, and
so cannot be written to a tmpfile until it's completely
finalized.  Move the create/write/rm out of main() and into
schedule_suite().

Signed-off-by: Dan Mick <dan.mick@redhat.com>

teuthology/suite/run.py
@@ -6,6 +6,7 @@ import time
 import yaml
 
 from datetime import datetime
+from tempfile import NamedTemporaryFile
 
 from ..config import config, JobConfig
 from ..exceptions import (
@@ -212,6 +213,12 @@ class Run(object):
         conf_dict = substitute_placeholders(dict_templ, self.config_input)
         conf_dict.update(self.kernel_dict)
         job_config = JobConfig.from_dict(conf_dict)
+        job_config.name = self.name
+        job_config.priority = self.args.priority
+        if self.args.results_email:
+            job_config.email = self.args.results_email
+        if self.args.owner:
+            job_config.owner = self.args.owner
         return job_config
 
     def build_base_args(self):
@@ -414,6 +421,17 @@ class Run(object):
         log.info('Suite %s in %s generated %d jobs (not yet filtered)' % (
             suite_name, suite_path, len(configs)))
 
+        if self.args.dry_run:
+            log.debug("Base job config:\n%s" % self.base_config)
+
+        # create, but do not write, the temp file here, so it can be
+        # added to the args in collect_jobs, but not filled until
+        # any backtracking is done
+        base_yaml_path = NamedTemporaryFile(
+            prefix='schedule_suite_', delete=False
+        ).name
+        self.base_yaml_paths.insert(0, base_yaml_path)
+
         # if newest, do this until there are no missing packages
         # if not, do it once
         backtrack = 0
@@ -444,7 +462,14 @@ class Run(object):
                     name=name,
                 )
 
+        if self.args.dry_run:
+            log.debug("Base job config:\n%s" % self.base_config)
+
+        with open(base_yaml_path, 'w+b') as base_yaml:
+            base_yaml.write(str(self.base_config))
         self.schedule_jobs(jobs_missing_packages, jobs_to_schedule, name)
+
+        os.remove(base_yaml_path)
 
         count = len(jobs_to_schedule)
         missing_count = len(jobs_missing_packages)

teuthology/suite/__init__.py
@@ -5,7 +5,6 @@
 import logging
 import os
 import time
-from tempfile import NamedTemporaryFile
 
 import teuthology
 from ..config import config, YamlConfig
@@ -63,26 +62,8 @@ def main(args):
         log.info('Will upload archives to ' + args['--archive-upload'])
 
     run = Run(fn)
-    job_config = run.base_config
     name = run.name
-
-    job_config.name = name
-    job_config.priority = fn.priority
-    if config.results_email:
-        job_config.email = config.results_email
-    if fn.owner:
-        job_config.owner = fn.owner
-
-    if fn.dry_run:
-        log.debug("Base job config:\n%s" % job_config)
-
-    with NamedTemporaryFile(prefix='schedule_suite_',
-                            delete=False) as base_yaml:
-        base_yaml.write(str(job_config))
-        base_yaml_path = base_yaml.name
-    run.base_yaml_paths.insert(0, base_yaml_path)
     run.prepare_and_schedule()
-    os.remove(base_yaml_path)
     if not fn.dry_run and args['--wait']:
         return wait(name, config.max_job_time,
                     args['--archive-upload-url'])

commit 506ce9375adbc914b5d01f37bcb2feb8eb97f8b3

Resume paying attention to --email
Change in 61350db caused email arg to be lost.  Fix, add test

Signed-off-by: Dan Mick <dan.mick@redhat.com>

teuthology/suite/run.py
@@ -215,8 +215,8 @@ class Run(object):
         job_config = JobConfig.from_dict(conf_dict)
         job_config.name = self.name
         job_config.priority = self.args.priority
-        if self.args.results_email:
-            job_config.email = self.args.results_email
+        if self.args.email:
+            job_config.email = self.args.email
         if self.args.owner:
             job_config.owner = self.args.owner
         return job_config

teuthology/suite/test/test_run_.py
@@ -18,6 +18,7 @@ class TestRun(object):
             suite_branch='suite_branch',
             ceph_branch='ceph_branch',
             ceph_sha1='ceph_sha1',
+            email='address@example.com',
             teuthology_branch='teuthology_branch',
             kernel_branch=None,
             kernel_flavor='kernel_flavor',
@@ -26,6 +27,20 @@ class TestRun(object):
             base_yaml_paths=list(),
         )
         self.args = YamlConfig.from_dict(self.args_dict)
+
+    @patch('teuthology.suite.run.util.git_ls_remote')
+    @patch('teuthology.suite.run.Run.choose_ceph_version')
+    @patch('teuthology.suite.run.util.git_validate_sha1')
+    def test_email_addr(self, m_git_validate_sha1, m_choose_ceph_version, m_git_ls_remote):
+        # neuter choose_X_branch
+        m_git_validate_sha1.return_value = self.args_dict['ceph_sha1']
+        m_choose_ceph_version.return_value = self.args_dict['ceph_sha1']
+        self.args_dict['teuthology_branch'] = 'master'
+        self.args_dict['suite_branch'] = 'master'
+        m_git_ls_remote.return_value = 'suite_sha1'
+
+        runobj = self.klass(self.args)
+        assert runobj.base_config.email == self.args_dict['email']
 
     @patch('teuthology.suite.run.util.fetch_repos')
     def test_name(self, m_fetch_repos):