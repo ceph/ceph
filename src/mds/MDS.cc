130fcd639d13884b458b202faec2203e637e8329
David Zafman <dzafman@redhat.com>
2016-07-11 17:58:22 -0700

    CLEANUP: Move locking into dump_ops_in_flight()/dump_historic_ops()
    Signed-off-by: David Zafman <dzafman@redhat.com>
    (cherry picked from commit 17c645f70708134f50374cadb5505b0babe2265a)
    
    Conflicts:
    	src/mds/MDSRank.cc (variation of code exists in mds/MDS.cc)
    	src/mon/Monitor.cc (no equivalent code here)

--- src/mds/MDS.cc
+++ src/mds/MDS.cc
@@ -242,18 +242,12 @@ bool MDS::asok_command(string command, cmdmap_t& cmdmap, string format,
       f->dump_string("error", "mds_not_active");
     } else if (command == "dump_ops_in_flight" ||
 	       command == "ops") {
-      RWLock::RLocker l(op_tracker.lock);
-      if (!op_tracker.tracking_enabled) {
+      if (!op_tracker.dump_ops_in_flight(f)) {
         ss << "op_tracker tracking is not enabled";
-      } else {
-        op_tracker.dump_ops_in_flight(f);
       }
     } else if (command == "dump_historic_ops") {
-      RWLock::RLocker l(op_tracker.lock);
-      if (!op_tracker.tracking_enabled) {
+      if (!op_tracker.dump_historic_ops(f)) {
 	ss << "op_tracker tracking is not enabled";
-      } else {
-        op_tracker.dump_historic_ops(f);
       }
     } else if (command == "osdmap barrier") {
       int64_t target_epoch = 0;
