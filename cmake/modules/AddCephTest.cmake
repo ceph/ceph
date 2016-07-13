ce5724effb270b0f57369fd1e9bd82d7cc872676
Kefu Chai <kchai@redhat.com>
2016-07-13 21:14:30 +0800

    cmake: add a "tests" target to build tests
    please note "make test" is used by cmake to run tests, so we cannot just
    repurpose it to *build* them.
    
    * AddCephTest.cmake: depends on "tests"
    * CMakeLists.txt: let "check" depend on "tests"
    * src/CMakeLists.txt: update the run-tox tests
    * run-make-check.sh: use "make tests" and "ctest" instead of "make check"
    * ceph-detect-init/CMakeLists.txt: let "tests" depend on
        "ceph-detect-init"
    * ceph-disk/CMakeLists.txt: let "tests" depend on "ceph-disk"
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- cmake/modules/AddCephTest.cmake
+++ cmake/modules/AddCephTest.cmake
@@ -3,8 +3,8 @@
 #adds makes target/script into a test, test to check target, sets necessary environment variables
 function(add_ceph_test test_name test_path)
   add_test(NAME ${test_name} COMMAND ${test_path})
-  add_dependencies(check ${test_name})
-  set_property(TEST 
+  add_dependencies(tests ${test_name})
+  set_property(TEST
     ${test_name}
     PROPERTY ENVIRONMENT 
     CEPH_ROOT=${CMAKE_SOURCE_DIR}
