9a97e1b454e6a826765105a43f6117718f81268d
Jason Dillaman <dillaman@redhat.com>
2016-07-13 14:49:06 -0400

    rbd-mirror: use pool id + global image id as deletion primary key
    Fixes: http://tracker.ceph.com/issues/16538
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>
    (cherry picked from commit 25203a8a9d59ff025d223ec1afaeb14946d54993)

--- src/test/rbd_mirror/test_ImageReplayer.cc
+++ src/test/rbd_mirror/test_ImageReplayer.cc
@@ -567,7 +567,7 @@ TEST_F(TestImageReplayer, Resync)
 
   C_SaferCond delete_ctx;
   m_image_deleter->wait_for_scheduled_deletion(
-    m_replayer->get_local_image_name(), &delete_ctx);
+    m_local_ioctx.get_id(), m_replayer->get_global_image_id(), &delete_ctx);
   EXPECT_EQ(0, delete_ctx.wait());
 
   C_SaferCond cond;
@@ -632,7 +632,7 @@ TEST_F(TestImageReplayer, Resync_While_Stop)
 
   C_SaferCond delete_ctx;
   m_image_deleter->wait_for_scheduled_deletion(
-    m_replayer->get_local_image_name(), &delete_ctx);
+    m_local_ioctx.get_id(), m_replayer->get_global_image_id(), &delete_ctx);
   EXPECT_EQ(0, delete_ctx.wait());
 
   C_SaferCond cond3;
@@ -673,7 +673,7 @@ TEST_F(TestImageReplayer, Resync_StartInterrupted)
 
   C_SaferCond delete_ctx;
   m_image_deleter->wait_for_scheduled_deletion(
-    m_replayer->get_local_image_name(), &delete_ctx);
+    m_local_ioctx.get_id(), m_replayer->get_global_image_id(), &delete_ctx);
   EXPECT_EQ(0, delete_ctx.wait());
 
   C_SaferCond cond2;
