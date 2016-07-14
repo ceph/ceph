0c221a95d729d779af4525618d112a9d9a0242db
Haomai Wang <haomai@xsky.com>
2016-07-15 01:00:20 +0800

    msg/async/Event: fix valgrind issue init "owner" to zero
    Fixes: http://tracker.ceph.com/issues/16685
    Signed-off-by: Haomai Wang <haomai@xsky.com>

--- src/msg/async/Event.h
+++ src/msg/async/Event.h
@@ -113,7 +113,7 @@ class EventCenter {
   CephContext *cct;
   int nevent;
   // Used only to external event
-  pthread_t owner;
+  pthread_t owner = 0;
   std::mutex external_lock;
   std::atomic_ulong external_num_events;
   deque<EventCallbackRef> external_events;
