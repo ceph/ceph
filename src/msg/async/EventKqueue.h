efd8d9d9bd26f4978affc728490d4a2e9cf193f7
Michal Jarzabek <stiopa@gmail.com>
2016-07-16 08:28:37 +0100

    msg/EventKqueue.h: add override to virtual func
    Signed-off-by: Michal Jarzabek <stiopa@gmail.com>

--- src/msg/async/EventKqueue.h
+++ src/msg/async/EventKqueue.h
@@ -39,11 +39,12 @@ class KqueueDriver : public EventDriver {
       free(events);
   }
 
-  int init(int nevent);
-  int add_event(int fd, int cur_mask, int add_mask);
-  int del_event(int fd, int cur_mask, int del_mask);
-  int resize_events(int newsize);
-  int event_wait(vector<FiredFileEvent> &fired_events, struct timeval *tp);
+  int init(int nevent) override;
+  int add_event(int fd, int cur_mask, int add_mask) override;
+  int del_event(int fd, int cur_mask, int del_mask) override;
+  int resize_events(int newsize) override;
+  int event_wait(vector<FiredFileEvent> &fired_events,
+		 struct timeval *tp) override;
 };
 
 #endif
