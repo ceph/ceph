942a532638653b469fe4e2064d37d08575ebfbd1
Kefu Chai <kchai@redhat.com>
2016-07-19 09:55:23 +0800

    [DNM] test: debug rados-striper.sh
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/test/libradosstriper/rados-striper.sh
+++ src/test/libradosstriper/rados-striper.sh
@@ -17,6 +17,17 @@
 source $(dirname $0)/../detect-build-env-vars.sh
 source $CEPH_ROOT/qa/workunits/ceph-helpers.sh
 
+function report_failure() {
+    local dir=$1
+
+    netstat -nlp
+    kill_daemons $dir TERM mon
+    echo "================ mon.a.log"
+    cat $dir/mon.a.log
+    echo "================ mon.a.log ====="
+    
+}
+
 function run() {
     local dir=$1
     shift
@@ -30,7 +41,10 @@ function run() {
     setup $dir || return 1
 
     # create a cluster with one monitor and three osds
-    run_mon $dir a || return 1
+    if ! run_mon $dir a; then
+        report_failure
+        return 1
+    fi
     run_osd $dir 0 || return 1
     run_osd $dir 1 || return 1
     run_osd $dir 2 || return 1
