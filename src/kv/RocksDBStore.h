58294411880f22c60077d3418dae0155abf3ded1
Jianjian Huo <jianjian.huo@ssi.samsung.com>
2016-07-20 14:28:21 -0700

    kv/RocksDBStore: add column family support, header file
    Signed-off-by: Jianjian Huo <jianjian.huo@ssi.samsung.com>

--- src/kv/RocksDBStore.h
+++ src/kv/RocksDBStore.h
@@ -47,7 +47,10 @@ namespace rocksdb{
   class WriteBatch;
   class Iterator;
   class Logger;
+  class ColumnFamilyHandle;
   struct Options;
+  struct DBOptions;
+  struct ColumnFamilyOptions;
 }
 
 extern rocksdb::Logger *create_rocksdb_ceph_logger();
@@ -63,7 +66,9 @@ class RocksDBStore : public KeyValueDB {
   rocksdb::DB *db;
   rocksdb::Env *env;
   string options_str;
-  int do_open(ostream &out, bool create_if_missing);
+  int create_db_dir();
+  int do_open(ostream &out, bool create_if_missing,
+	      const vector<ColumnFamily>* cfs = nullptr);
 
   // manage async compactions
   Mutex compact_queue_lock;
@@ -133,8 +138,13 @@ public:
   int open(ostream &out) {
     return do_open(out, false);
   }
+  int open(ostream &out, const vector<ColumnFamily>& cfs) {
+    return do_open(out, false, &cfs);
+  }
   /// Creates underlying db if missing and opens it
   int create_and_open(ostream &out);
+  int create_and_open(ostream &out, const vector<ColumnFamily>& cfs);
+  int drop_column_family(const string &cf_name);
 
   void close();
 
@@ -219,6 +229,39 @@ public:
     ~RocksDBSnapshotIteratorImpl();
   };
 
+  class RocksDBCFIteratorImpl :
+    public KeyValueDB::ColumnFamilyIteratorImpl{
+  protected:
+    rocksdb::Iterator *dbiter;
+  public:
+    explicit RocksDBCFIteratorImpl(rocksdb::Iterator *iter) :
+      dbiter(iter) { }
+    ~RocksDBCFIteratorImpl();
+
+    int seek_to_first();
+    int seek_to_last();
+    int upper_bound(const string &after);
+    int lower_bound(const string &to);
+    bool valid();
+    int next();
+    int prev();
+    string key();
+    bufferlist value();
+    bufferptr value_as_ptr();
+    int status();
+  };
+
+  class RocksDBCFSnapshotIteratorImpl : public RocksDBCFIteratorImpl{
+    rocksdb::DB *db;
+    const rocksdb::Snapshot *snapshot;
+  public:
+    RocksDBCFSnapshotIteratorImpl(rocksdb::DB *db, const rocksdb::Snapshot *s,
+				rocksdb::Iterator *iter) :
+      RocksDBCFIteratorImpl(iter), db(db), snapshot(s) { }
+
+    ~RocksDBCFSnapshotIteratorImpl();
+  };
+
   /// Utility
   static string combine_strings(const string &prefix, const string &value);
   static int split_key(rocksdb::Slice in, string *prefix, string *key);
@@ -300,9 +343,9 @@ err:
 
 protected:
   WholeSpaceIterator _get_iterator();
-
   WholeSpaceIterator _get_snapshot_iterator();
-
+  ColumnFamilyIterator _get_cf_iterator(const std::string& cf_name);
+  ColumnFamilyIterator _get_cf_snapshot_iterator(const std::string& cf_name);
 };
 
 
