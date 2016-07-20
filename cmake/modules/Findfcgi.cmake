b5607f88e8d5b49fa3189e7cf756715d8e35966e
Kefu Chai <kchai@redhat.com>
2016-07-20 16:20:59 +0800

    cmake: Findfcgi.cmake cleanups
    FindPackageHandleStandardArgs() takes care of the find_package()
    boilderplate stuff. so no need to repeat them. and remove the checkings
    in env variables.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- cmake/modules/Findfcgi.cmake
+++ cmake/modules/Findfcgi.cmake
@@ -4,35 +4,15 @@
 # and following variables are set:
 # FCGI_INCLUDE_DIR
 # FCGI_LIBRARY
-FIND_PATH(FCGI_INCLUDE_DIR
+find_path(FCGI_INCLUDE_DIR
   fcgio.h
   PATHS
   /usr/include
   /usr/local/include
-  /usr/include/fastcgi
-  "$ENV{LIB_DIR}/include"
-  $ENV{INCLUDE}
-  )
-FIND_LIBRARY(FCGI_LIBRARY NAMES fcgi libfcgi PATHS
+  /usr/include/fastcgi)
+find_library(FCGI_LIBRARY NAMES fcgi libfcgi PATHS
   /usr/local/lib
-  /usr/lib
-  "$ENV{LIB_DIR}/lib"
-  "$ENV{LIB}"
-  )
-
-IF (FCGI_INCLUDE_DIR AND FCGI_LIBRARY)
-   SET(FCGI_FOUND TRUE)
-ENDIF (FCGI_INCLUDE_DIR AND FCGI_LIBRARY)
-
-IF (FCGI_FOUND)
-   IF (NOT FCGI_FIND_QUIETLY)
-      MESSAGE(STATUS "Found FCGI: ${FCGI_LIBRARY}")
-   ENDIF (NOT FCGI_FIND_QUIETLY)
-ELSE (FCGI_FOUND)
-   IF (FCGI_FIND_REQUIRED)
-      MESSAGE(FATAL_ERROR "Could not find FCGI")
-   ENDIF (FCGI_FIND_REQUIRED)
-ENDIF (FCGI_FOUND)
+  /usr/lib)
 
 # handle the QUIETLY and REQUIRED arguments and set UUID_FOUND to TRUE if
 # all listed variables are TRUE
