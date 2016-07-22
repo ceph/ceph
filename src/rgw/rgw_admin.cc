b7cd4e0e8b879b5e528de75bea3307585b96cbf2
Casey Bodley <cbodley@redhat.com>
2016-07-21 23:46:20 -0400

    rgw: change read_sync_status interface
    RGWDataSyncStatusManager::read_sync_status() now operates on the given
    parameter, rather than its internal member variable. this allows
    multiple concurrent readers, which is needed for the rest interface
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -1787,13 +1787,12 @@ static void get_data_sync_status(const string& source_zone, list<string>& status
     return;
   }
 
-  ret = sync.read_sync_status();
+  rgw_data_sync_status sync_status;
+  ret = sync.read_sync_status(&sync_status);
   if (ret < 0 && ret != -ENOENT) {
     push_ss(ss, status, tab) << string("failed read sync status: ") + cpp_strerror(-ret);
     return;
   }
-
-  const rgw_data_sync_status& sync_status = sync.get_sync_status();
 
   string status_str;
   switch (sync_status.sync_info.state) {
@@ -5090,13 +5089,12 @@ next:
       return -ret;
     }
 
-    ret = sync.read_sync_status();
+    rgw_data_sync_status sync_status;
+    ret = sync.read_sync_status(&sync_status);
     if (ret < 0 && ret != -ENOENT) {
       cerr << "ERROR: sync.read_sync_status() returned ret=" << ret << std::endl;
       return -ret;
     }
-
-    rgw_data_sync_status& sync_status = sync.get_sync_status();
 
     formatter->open_object_section("summary");
     encode_json("sync_status", sync_status, formatter);
