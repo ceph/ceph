7eb4572944e048bee5f96f1b6c79c039d50b2cc8
John Spray <john.spray@redhat.com>
2016-07-26 11:01:40 +0100

    pybind/mgr: remove unused gevent code
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/pybind/mgr/calamari_rest/util.py
+++ src/pybind/mgr/calamari_rest/util.py
@@ -1,8 +1,6 @@
 import datetime
 
 from dateutil import tz
-import gevent.greenlet
-import gevent.event
 
 
 def now():
@@ -10,22 +8,6 @@ def now():
     A tz-aware now
     """
     return datetime.datetime.utcnow().replace(tzinfo=tz.tzutc())
-
-
-class Ticker(gevent.greenlet.Greenlet):
-    def __init__(self, period, callback, *args, **kwargs):
-        super(Ticker, self).__init__(*args, **kwargs)
-        self._period = period
-        self._callback = callback
-        self._complete = gevent.event.Event()
-
-    def stop(self):
-        self._complete.set()
-
-    def _run(self):
-        while not self._complete.is_set():
-            self._callback()
-            self._complete.wait(self._period)
 
 
 def memoize(function):
