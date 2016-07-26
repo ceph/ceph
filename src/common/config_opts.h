287de56c754a21ae9014f4cabdd6c07855b68ea4
oliveiradan <doliveirabrz@gmail.com>
2016-07-25 23:39:34 -0600

    bdev: Changes for issue #14934 - bdev related changes only
    Signed-off-by: Daniel Oliveira <doliveira@suse.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -905,12 +905,12 @@ OPTION(memstore_page_set, OPT_BOOL, true)
 OPTION(memstore_page_size, OPT_U64, 64 << 10)
 
 OPTION(bdev_debug_inflight_ios, OPT_BOOL, false)
-OPTION(bdev_inject_crash, OPT_INT, 0)  // if N>0, then ~ 1/N IOs will complete before we crash on flush.
-OPTION(bdev_inject_crash_flush_delay, OPT_INT, 2) // wait N more seconds on flush
+OPTION(bdev_inject_crash, OPT_U32, 0)  // if N>0, then ~ 1/N IOs will complete before we crash on flush.
+OPTION(bdev_inject_crash_flush_delay, OPT_U32, 2) // wait N more seconds on flush
 OPTION(bdev_aio, OPT_BOOL, true)
-OPTION(bdev_aio_poll_ms, OPT_INT, 250)  // milliseconds
-OPTION(bdev_aio_max_queue_depth, OPT_INT, 32)
-OPTION(bdev_block_size, OPT_INT, 4096)
+OPTION(bdev_aio_poll_ms, OPT_U32, 250)  // milliseconds
+OPTION(bdev_aio_max_queue_depth, OPT_U32, 32)
+OPTION(bdev_block_size, OPT_U32, 4096)
 
 // if yes, osd will unbind all NVMe devices from kernel driver and bind them
 // to the uio_pci_generic driver. The purpose is to prevent the case where
