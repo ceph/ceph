b32b5f1c8760bf73c436eaef2c83c6cf140db900
weiqiaomiao <wei.qiaomiao@zte.com.cn>
2016-07-26 18:03:31 +0800

    rgw: return "NoSuchLifecycleConfiguration" if lc config does not exist
    return error code "NoSuchLifecycleConfiguration" if lifecycle configuration does not exist
    as AmazonS3 latest API does.
    
    Signed-off-by: weiqiaomiao <wei.qiaomiao@zte.com.cn>

--- src/rgw/rgw_http_errors.h
+++ src/rgw/rgw_http_errors.h
@@ -50,6 +50,7 @@ const static struct rgw_http_errors RGW_HTTP_ERRORS[] = {
     { ERR_NO_SUCH_WEBSITE_CONFIGURATION, 404, "NoSuchWebsiteConfiguration" },
     { ERR_NO_SUCH_UPLOAD, 404, "NoSuchUpload" },
     { ERR_NOT_FOUND, 404, "Not Found"},
+    { ERR_NO_SUCH_LC, 404, "NoSuchLifecycleConfiguration"},
     { ERR_METHOD_NOT_ALLOWED, 405, "MethodNotAllowed" },
     { ETIMEDOUT, 408, "RequestTimeout" },
     { EEXIST, 409, "BucketAlreadyExists" },
