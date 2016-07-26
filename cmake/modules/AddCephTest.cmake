f3af334bf1230f7265dd82c485a1bdc84c187094
David Zafman <dzafman@redhat.com>
2016-07-26 13:23:52 -0700

    cmake: Support extra arguments to tests using add_ceph_test
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- cmake/modules/AddCephTest.cmake
+++ cmake/modules/AddCephTest.cmake
@@ -2,7 +2,7 @@
 
 #adds makes target/script into a test, test to check target, sets necessary environment variables
 function(add_ceph_test test_name test_path)
-  add_test(NAME ${test_name} COMMAND ${test_path})
+  add_test(NAME ${test_name} COMMAND ${test_path} ${ARGN})
   add_dependencies(tests ${test_name})
   set_property(TEST
     ${test_name}
