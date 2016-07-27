2a6e0be8f4f440f4d2090c953e7332c5cd7c0cef
Greg Farnum <gfarnum@redhat.com>
2016-07-27 15:26:07 -0700

    client: always pass a UserPerm to public setattr variants
    We also recast our private _setattr() stuff to be implemented in terms of
    UserPerm instead of uid/gid, and translate the other direction.
    
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>

--- src/libcephfs.cc
+++ src/libcephfs.cc
@@ -628,7 +628,8 @@ extern "C" int ceph_setattr(struct ceph_mount_info *cmount, const char *relpath,
 {
   if (!cmount->is_mounted())
     return -ENOTCONN;
-  return cmount->get_client()->setattr(relpath, attr, mask);
+  UserPerm perms = cmount->get_client()->pick_my_perms();
+  return cmount->get_client()->setattr(relpath, attr, mask, perms);
 }
 
 // *xattr() calls supporting samba/vfs
