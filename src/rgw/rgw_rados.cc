608430f36a92978a41e9c1038a5acbdc1051a3f4
Casey Bodley <cbodley@redhat.com>
2016-07-28 20:20:29 -0400

    rgw: pass cr registry to managers
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit ef4d7eab11fb5d2a41c9c28b9eb8b075aaff0d05)
    
    Conflicts:
    	src/rgw/rgw_data_sync.cc (this is a re-cherry-pick that includes
                    the RGWSyncLogTrimThread piece that was not available
                    as of 87cb8474f523be1b281882c46a8a3597977a51c9)

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -3078,7 +3078,8 @@ class RGWSyncLogTrimThread : public RGWSyncProcessorThread
   void stop_process() override { crs.stop(); }
 public:
   RGWSyncLogTrimThread(RGWRados *store, int interval)
-    : RGWSyncProcessorThread(store), crs(store->ctx(), nullptr), store(store),
+    : RGWSyncProcessorThread(store),
+      crs(store->ctx(), store->get_cr_registry()), store(store),
       http(store->ctx(), crs.get_completion_mgr()),
       trim_interval(interval, 0)
   {}
