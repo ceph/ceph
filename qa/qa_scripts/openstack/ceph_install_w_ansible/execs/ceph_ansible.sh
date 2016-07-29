123edd5f47ff77581deb9a8701f6bc3f2fc6ace7
Warren Usui <wusui@magna002.ceph.redhat.com>
2016-07-29 01:00:35 -0400

    Add checks to make sure that the subscription manager is not already enabled.
    Add ability to set monitor_interface and public_network values (as opposed
    to using hard coded values)
    
    This change should be squashed with the previous change before merging this
    pull request
    
    Signed-off-by: Warren Usui <wusui@magna002.ceph.redhat.com>

--- qa/qa_scripts/openstack/ceph_install_w_ansible/execs/ceph_ansible.sh
+++ qa/qa_scripts/openstack/ceph_install_w_ansible/execs/ceph_ansible.sh
@@ -6,6 +6,11 @@ cd
 sudo ./edit_ansible_hosts.sh $cephnodes
 mkdir ceph-ansible-keys
 cd /usr/share/ceph-ansible/group_vars/
+if [ -f ~/ip_info ]; then
+    source ~/ip_info
+fi
+mon_intf=${mon_intf:-'eno1'}
+pub_netw=${pub_netw:-'10.8.128.0\/21'}
 sudo cp all.sample all
 sudo sed -i 's/#ceph_origin:.*/ceph_origin: distro/' all
 sudo sed -i 's/#fetch_directory:.*/fetch_directory: ~\/ceph-ansible-keys/' all
@@ -13,9 +18,9 @@ sudo sed -i 's/#ceph_stable:.*/ceph_stable: true/' all
 sudo sed -i 's/#ceph_stable_rh_storage:.*/ceph_stable_rh_storage: false/' all
 sudo sed -i 's/#ceph_stable_rh_storage_cdn_install:.*/ceph_stable_rh_storage_cdn_install: true/' all
 sudo sed -i 's/#cephx:.*/cephx: true/' all
-sudo sed -i 's/#monitor_interface:.*/monitor_interface: eno1/' all
+sudo sed -i "s/#monitor_interface:.*/monitor_interface: ${mon_intf}/" all
 sudo sed -i 's/#journal_size:.*/journal_size: 1024/' all
-sudo sed -i 's/#public_network:.*/public_network: 10.8.128.0\/21/' all
+sudo sed -i "s/#public_network:.*/public_network: ${pub_netw}/" all
 sudo cp osds.sample osds
 sudo sed -i 's/#fetch_directory:.*/fetch_directory: ~\/ceph-ansible-keys/' osds
 sudo sed -i 's/#crush_location:/crush_location:/' osds
