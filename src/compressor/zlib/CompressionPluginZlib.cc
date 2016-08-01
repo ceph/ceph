a92eba65d887b770edbc1e800d1ceb948eed2a73
Igor Fedotov <ifedotov@mirantis.com>
2016-08-01 14:32:50 +0300

    os/bluestore: Move compression type enum and corresponding stuff to Compressor.h
    Signed-off-by: Igor Fedotov <ifedotov@mirantis.com>

--- src/compressor/zlib/CompressionPluginZlib.cc
+++ src/compressor/zlib/CompressionPluginZlib.cc
@@ -29,7 +29,7 @@ public:
   {}
 
   virtual int factory(CompressorRef *cs,
-                      ostream *ss)
+                      std::ostream *ss)
   {
     if (compressor == 0) {
       ZlibCompressor *interface = new ZlibCompressor();
