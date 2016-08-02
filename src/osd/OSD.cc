cf1dfd8ca4f64285aa316813a2b6c854ef51f8e2
xie xingguo <xie.xingguo@zte.com.cn>
2016-08-02 13:50:18 +0800

    osd: add sanity check for scrubs_active counter
    To make sure there is no underflow and the scrub logic
    works fine.
    
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -1115,6 +1115,7 @@ void OSDService::dec_scrubs_active()
   dout(20) << "dec_scrubs_active " << scrubs_active << " -> " << (scrubs_active-1)
 	   << " (max " << cct->_conf->osd_max_scrubs << ", pending " << scrubs_pending << ")" << dendl;
   --scrubs_active;
+  assert(scrubs_active >= 0);
   sched_scrub_lock.Unlock();
 }
 
