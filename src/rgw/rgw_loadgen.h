0ae4e1360cf7085989748a1614a8ba77ee6befa2
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-08-04 17:48:11 +0200

    rgw: cleanup in the LoadGen frontend.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_loadgen.h
+++ src/rgw/rgw_loadgen.h
@@ -4,21 +4,27 @@
 #ifndef CEPH_RGW_LOADGEN_H
 #define CEPH_RGW_LOADGEN_H
 
+#include <map>
+#include <string>
+
 #include "rgw_client_io.h"
 
 
 struct RGWLoadGenRequestEnv {
   int port;
   uint64_t content_length;
-  string content_type;
-  string request_method;
-  string uri;
-  string query_string;
-  string date_str;
+  std::string content_type;
+  std::string request_method;
+  std::string uri;
+  std::string query_string;
+  std::string date_str;
 
-  map<string, string> headers;
+  std::map<std::string, std::string> headers;
 
-  RGWLoadGenRequestEnv() : port(0), content_length(0) {}
+  RGWLoadGenRequestEnv()
+    : port(0),
+      content_length(0) {
+  }
 
   void set_date(utime_t& tm);
   int sign(RGWAccessKey& access_key);
@@ -29,26 +35,31 @@ struct RGWLoadGenRequestEnv {
 class RGWLoadGenIO : public RGWStreamIOEngine
 {
   uint64_t left_to_read;
-  RGWLoadGenRequestEnv *req;
+  RGWLoadGenRequestEnv* req;
   RGWEnv env;
-public:
-  void init_env(CephContext *cct);
 
-  int write_data(const char *buf, int len);
+public:
+  explicit RGWLoadGenIO(RGWLoadGenRequestEnv* const req)
+    : left_to_read(0),
+      req(req) {
+  }
+
+  void init_env(CephContext *cct);
   int read_data(char *buf, int len);
+  int write_data(const char *buf, int len);
 
   int send_status(int status, const char *status_name);
   int send_100_continue();
   int complete_header();
-  int complete_request();
   int send_content_length(uint64_t len);
 
-  explicit RGWLoadGenIO(RGWLoadGenRequestEnv *_re) : left_to_read(0), req(_re) {}
   void flush();
 
   RGWEnv& get_env() override {
     return env;
   }
+
+  int complete_request();
 };
 
 #endif
