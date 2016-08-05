e7b0428e0e8d8f5459311dc698d94a3ac4f04684
Josh Durgin <jdurgin@redhat.com>
2016-08-05 11:45:00 -0700

    ceph-objectstore-tool: add a way to split filestore directories offline
    Use the usual split code, and split each dir that meets the
    usual split criteria.
    
    This can be run with lower than usual split settings, to avoid costly
    online splits. To make sure the directories are not merged again, use
    a load merge threshold (e.g. filestore merge threshold = 1), and
    adjust the split multiplier accordingly.
    
    Fixes: http://tracker.ceph.com/issues/17220
    Signed-off-by: Josh Durgin <jdurgin@redhat.com>

--- src/os/filestore/FileStore.cc
+++ src/os/filestore/FileStore.cc
@@ -5695,6 +5695,21 @@ uint64_t FileStore::estimate_objects_overhead(uint64_t num_objects)
   return res;
 }
 
+int FileStore::apply_layout_settings(const coll_t &cid)
+{
+  dout(20) << __func__ << " " << cid << dendl;
+  Index index;
+  int r = get_index(cid, &index);
+  if (r < 0) {
+    dout(10) << "Error getting index for " << cid << ": " << cpp_strerror(r)
+	     << dendl;
+    return r;
+  }
+
+  return index->apply_layout_settings();
+}
+
+
 // -- FSSuperblock --
 
 void FSSuperblock::encode(bufferlist &bl) const
