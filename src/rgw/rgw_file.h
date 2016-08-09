94d1bc909a0926da245ea8fc0aa6a50f4ffde6bb
Matt Benjamin <mbenjamin@redhat.com>
2016-08-09 14:23:00 -0400

    rgw_file: unlock() must precede out label
    In lookup_handle(...).
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_file.h
+++ src/rgw/rgw_file.h
@@ -1000,9 +1000,9 @@ namespace rgw {
 	goto retry; /* !LATCHED */
       }
       /* LATCHED */
+      fh->mtx.unlock(); /* !LOCKED */
     out:
       lat.lock->unlock(); /* !LATCHED */
-      fh->mtx.unlock(); /* !LOCKED */
       return fh;
     }
 
