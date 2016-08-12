7f382c419cf96ba977ce35fbdff9485c03716e22
Jason Dillaman <dillaman@redhat.com>
2016-08-12 12:21:02 -0400

    librbd: new journal policy to disable initializing the journal
    This will be used in the case where the journal is being disabled.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/test/librbd/mock/MockJournalPolicy.h
+++ src/test/librbd/mock/MockJournalPolicy.h
@@ -12,6 +12,7 @@ namespace librbd {
 struct MockJournalPolicy : public journal::Policy {
 
   MOCK_CONST_METHOD0(append_disabled, bool());
+  MOCK_CONST_METHOD0(journal_disabled, bool());
   MOCK_METHOD1(allocate_tag_on_lock, void(Context*));
 
 };
