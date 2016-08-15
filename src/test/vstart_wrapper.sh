41069ed6f3dfd441be916d980d22f92d8b881b3e
Samuel Just <sjust@redhat.com>
2016-08-15 16:52:18 -0700

    test: add replicated and ec versions of ceph_test_rados to make check
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/test/vstart_wrapper.sh
+++ src/test/vstart_wrapper.sh
@@ -27,13 +27,18 @@ function vstart_setup()
 {
     rm -fr $CEPH_DEV_DIR $CEPH_OUT_DIR
     mkdir -p $CEPH_DEV_DIR
-    trap "teardown $CEPH_DIR" EXIT
+
+    if [ -z ${VSTART_WRAPPER_SKIP_TEARDOWN+x} ]
+    then
+      trap "teardown $CEPH_DIR" EXIT
+    fi
+
     export LC_ALL=C # some tests are vulnerable to i18n
     export PATH="$(pwd):${PATH}"
     $CEPH_ROOT/src/vstart.sh \
         --short \
         -o 'paxos propose interval = 0.01' \
-        -n -l $CEPH_START || return 1
+        -d -e -n -l $CEPH_START || return 1
     export CEPH_CONF=$CEPH_DIR/ceph.conf
 
     crit=$(expr 100 - $(ceph-conf --show-config-value mon_data_avail_crit))
@@ -49,13 +54,33 @@ This is a limit determined by
 EOF
         return 1
     fi
+
+
+    if [ -n ${CREATE_REP_CACHE+x} ]
+    then
+        ceph osd pool create rep-base 4
+        ceph osd pool create rep-cache 4
+        ceph osd tier add rep-base rep-cache
+        ceph osd tier cache-mode rep-cache writeback
+        ceph osd tier set-overlay rep-base rep-cache
+        ceph osd pool set rep-cache hit_set_type bloom
+        ceph osd pool set rep-cache hit_set_count 8
+        ceph osd pool set rep-cache hit_set_period 3600
+        ceph osd pool set rep-cache target_max_objects 250
+        ceph osd pool set rep-cache min_read_recency_for_promote 2
+    fi
 }
 
 function main()
 {
     teardown $CEPH_DIR
     vstart_setup || return 1
-    CEPH_CONF=$CEPH_DIR/ceph.conf "$@" || return 1
+    if [ -n ${VSTART_WRAPPER_SKIP_TEARDOWN+x} ]
+    then
+      CEPH_CONF=$CEPH_DIR/ceph.conf "$@" || return 1
+    else
+      ( CEPH_CONF=$CEPH_DIR/ceph.conf "$@" && teardown $CEPH_DIR ) || return 1
+    fi
 }
 
 main "$@"
