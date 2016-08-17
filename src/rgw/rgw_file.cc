cf1353c24917ecb9056272da7f6b3e73bba11fa8
Matt Benjamin <mbenjamin@redhat.com>
2016-08-17 10:28:48 -0400

    rgw_file: restore local definition of RGWLibFS gc interval
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit 31936caa09b46e86700faad969adfa9d43176206)
    
    Fixes: http://tracker.ceph.com/issues/17323

--- src/rgw/rgw_file.cc
+++ src/rgw/rgw_file.cc
@@ -684,7 +684,7 @@ namespace rgw {
 	  } /* rgw_fh */
 	} /* event::type::READDIR */
       } /* ev */
-      std::this_thread::sleep_for(gc_interval);
+      std::this_thread::sleep_for(std::chrono::seconds(120));
     } while (! stop);
   } /* RGWLibFS::gc */
 
