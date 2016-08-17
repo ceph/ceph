a5b8c9c1d81ed609e71a5a8abe4d0e90194408cc
Jason Dillaman <dillaman@redhat.com>
2016-08-17 14:02:32 -0400

    librbd: image state machine now has hooks for lock requests
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/ImageState.cc
+++ src/librbd/ImageState.cc
@@ -378,6 +378,37 @@ void ImageState<I>::snap_set(const std::string &snap_name, Context *on_finish) {
 }
 
 template <typename I>
+void ImageState<I>::prepare_lock(Context *on_ready) {
+  CephContext *cct = m_image_ctx->cct;
+  ldout(cct, 10) << __func__ << dendl;
+
+  m_lock.Lock();
+  if (is_closed()) {
+    m_lock.Unlock();
+    on_ready->complete(-ESHUTDOWN);
+    return;
+  }
+
+  Action action(ACTION_TYPE_LOCK);
+  action.on_ready = on_ready;
+  execute_action_unlock(action, nullptr);
+}
+
+template <typename I>
+void ImageState<I>::handle_prepare_lock_complete() {
+  CephContext *cct = m_image_ctx->cct;
+  ldout(cct, 10) << __func__ << dendl;
+
+  m_lock.Lock();
+  if (m_state != STATE_PREPARING_LOCK) {
+    m_lock.Unlock();
+    return;
+  }
+
+  complete_action_unlock(STATE_OPEN, 0);
+}
+
+template <typename I>
 int ImageState<I>::register_update_watcher(UpdateWatchCtx *watcher,
 					 uint64_t *handle) {
   CephContext *cct = m_image_ctx->cct;
@@ -426,6 +457,7 @@ bool ImageState<I>::is_transition_state() const {
   case STATE_CLOSING:
   case STATE_REFRESHING:
   case STATE_SETTING_SNAP:
+  case STATE_PREPARING_LOCK:
     break;
   }
   return true;
@@ -478,6 +510,9 @@ void ImageState<I>::execute_next_action_unlock() {
     return;
   case ACTION_TYPE_SET_SNAP:
     send_set_snap_unlock();
+    return;
+  case ACTION_TYPE_LOCK:
+    send_prepare_lock_unlock();
     return;
   }
   assert(false);
@@ -661,6 +696,30 @@ void ImageState<I>::handle_set_snap(int r) {
 
   m_lock.Lock();
   complete_action_unlock(STATE_OPEN, r);
+}
+
+template <typename I>
+void ImageState<I>::send_prepare_lock_unlock() {
+  CephContext *cct = m_image_ctx->cct;
+  ldout(cct, 10) << this << " " << __func__ << dendl;
+
+  assert(m_lock.is_locked());
+  m_state = STATE_PREPARING_LOCK;
+
+  assert(!m_actions_contexts.empty());
+  ActionContexts &action_contexts(m_actions_contexts.front());
+  assert(action_contexts.first.action_type == ACTION_TYPE_LOCK);
+
+  Context *on_ready = action_contexts.first.on_ready;
+  m_lock.Unlock();
+
+  if (on_ready == nullptr) {
+    complete_action_unlock(STATE_OPEN, 0);
+    return;
+  }
+
+  // wake up the lock handler now that its safe to proceed
+  on_ready->complete(0);
 }
 
 } // namespace librbd
