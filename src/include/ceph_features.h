6e7682b47e8d4d3be3dc3818d5bdd2b16bfc5a27
Jeff Layton <jlayton@redhat.com>
2016-08-29 07:16:39 -0400

    include/ceph_features: add FS_CHANGE_ATTR feature
    ...that is mapped to BTIME feature.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>

--- src/include/ceph_features.h
+++ src/include/ceph_features.h
@@ -85,6 +85,7 @@
 #define CEPH_FEATURE_NEW_OSDOPREPLY_ENCODING (1ULL<<58) /* New, v7 encoding */
 #define CEPH_FEATURE_FS_FILE_LAYOUT_V2       (1ULL<<58) /* file_layout_t */
 #define CEPH_FEATURE_FS_BTIME                (1ULL<<59) /* btime */
+#define CEPH_FEATURE_FS_CHANGE_ATTR          (1ULL<<59) /* change_attr */
 
 #define CEPH_FEATURE_RESERVED2 (1ULL<<61)  /* slow down, we are almost out... */
 #define CEPH_FEATURE_RESERVED  (1ULL<<62)  /* DO NOT USE THIS ... last bit! */
@@ -182,6 +183,7 @@ static inline unsigned long long ceph_sanitize_features(unsigned long long f) {
 	 CEPH_FEATURE_FS_FILE_LAYOUT_V2 |		 \
 	 CEPH_FEATURE_SERVER_KRAKEN |	\
 	 CEPH_FEATURE_FS_BTIME |			 \
+	 CEPH_FEATURE_FS_CHANGE_ATTR |			 \
 	 0ULL)
 
 #define CEPH_FEATURES_SUPPORTED_DEFAULT  CEPH_FEATURES_ALL
