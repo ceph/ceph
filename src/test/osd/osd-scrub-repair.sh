abcefc1395f76e5b05a988b970457a7f6dac6e8e
David Zafman <dzafman@redhat.com>
2016-08-30 12:11:44 -0700

    doc, test: Add schemas for list-inconsistent-* rados command output
    If jsonschema cmd is available use it to test output against schema
    
    Signed-off-by: David Zafman <dzafman@redhat.com>
    (cherry picked from commit eb73dd473600fbbb45fad00194f7c46b565d6b81)

--- src/test/osd/osd-scrub-repair.sh
+++ src/test/osd/osd-scrub-repair.sh
@@ -835,6 +835,11 @@ EOF
         jq '.' $dir/json > save1.json
     fi
 
+    if which jsonschema > /dev/null;
+    then
+      jsonschema -i $dir/json $CEPH_ROOT/doc/rados/command/list-inconsistent-obj.json || return 1
+    fi
+
     # Compute an old omap digest and save oi
     CEPH_ARGS='' ceph daemon $dir//ceph-osd.0.asok \
         config set osd_deep_scrub_update_digest_min_age 0
@@ -1471,6 +1476,10 @@ EOF
         jq '.' $dir/json > save2.json
     fi
 
+    if which jsonschema > /dev/null;
+    then
+      jsonschema -i $dir/json $CEPH_ROOT/doc/rados/command/list-inconsistent-obj.json || return 1
+    fi
 
     rados rmpool $poolname $poolname --yes-i-really-really-mean-it
     teardown $dir || return 1
@@ -1802,6 +1811,11 @@ EOF
         jq '.' $dir/json > save3.json
     fi
 
+    if which jsonschema > /dev/null;
+    then
+      jsonschema -i $dir/json $CEPH_ROOT/doc/rados/command/list-inconsistent-obj.json || return 1
+    fi
+
     pg_deep_scrub $pg
 
     rados list-inconsistent-pg $poolname > $dir/json || return 1
@@ -2116,6 +2130,11 @@ EOF
     if test $getjson = "yes"
     then
         jq '.' $dir/json > save4.json
+    fi
+
+    if which jsonschema > /dev/null;
+    then
+      jsonschema -i $dir/json $CEPH_ROOT/doc/rados/command/list-inconsistent-obj.json || return 1
     fi
 
     rados rmpool $poolname $poolname --yes-i-really-really-mean-it
