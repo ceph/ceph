2db0c0e76e194038d751d443a35c30a933a5c255
Roushan Ali <roushan.ali@sandisk.com>
2016-09-01 10:51:54 -0700

    Incorporated review comments

--- src/os/bluestore/bluestore_types.h
+++ src/os/bluestore/bluestore_types.h
@@ -584,13 +584,16 @@ struct bluestore_lextent_t {
   bluestore_blob_id_t blob;  ///< blob
   uint32_t offset;           ///< relative offset within the blob
   uint32_t length;           ///< length within the blob
+  uint32_t blob_depth;       /// How many blobs overlaps the lolgical offset
 
   bluestore_lextent_t(bluestore_blob_id_t _blob = 0,
 		      uint32_t o = 0,
-		      uint32_t l = 0)
+		      uint32_t l = 0,
+                      uint32_t d = 1)
     : blob(_blob),
       offset(o),
-      length(l) {}
+      length(l),
+      blob_depth(d) {}
 
   uint64_t end() const {
     return offset + length;
