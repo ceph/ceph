e9269bf12ebae291221dc94f3236d2a519229e1e
Roushan Ali <roushan.ali@sandisk.com>
2016-09-01 10:51:54 -0700

    Incorporated review comments

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -971,6 +971,7 @@ OPTION(bluestore_compression, OPT_STR, "none")  // force|aggressive|passive|none
 OPTION(bluestore_compression_algorithm, OPT_STR, "snappy")
 OPTION(bluestore_compression_min_blob_size, OPT_U32, 256*1024)
 OPTION(bluestore_compression_max_blob_size, OPT_U32, 4*1024*1024)
+OPTION(bluestore_blob_depth_for_gc, OPT_U32, 3)
 /*
  * Require the net gain of compression at least to be at this ratio,
  * otherwise we don't compress.
