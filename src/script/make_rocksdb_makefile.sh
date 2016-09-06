6e9662ac27b696a0f371eb3a5d68484e7bf32aaf
tianqing <tianqing@unitedstack.com>
2016-09-06 15:41:07 +0800

    Makfile: Fix rocksdb error when make dist
    Rocksdb delete some files, so we should change another way
    to get rocksdb's files.
    
    Currently, if we do not run configure in googletest/googlemock,
    we also will get a error when make dist. So fix it too.
    
    Signed-off-by: tianqing <tianqing@unitedstack.com>

--- src/script/make_rocksdb_makefile.sh
+++ src/script/make_rocksdb_makefile.sh
@@ -7,5 +7,4 @@ echo "EXTRA_DIST += \\" > /tmp/$$
 for f in `find rocksdb -type f | grep -v -e /.git$ -e ^rocksdb/tools/rdb | sort`; do
 		echo "  $f \\" >> /tmp/$$
 done
-echo "  rocksdb/AUTHORS" >> /tmp/$$
 mv /tmp/$$ Makefile-rocksdb.am
