5fb6d9c500bd3989c212b1be7f8de74834a1d11e
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-09-21 14:03:41 +0200

    rgw: add basic support for redirect in Swift's FormPost.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_rest_swift.cc
+++ src/rgw/rgw_rest_swift.cc
@@ -1927,9 +1927,17 @@ int RGWFormPost::get_data(ceph::bufferlist& bl)
 
 void RGWFormPost::send_response()
 {
+  std::string redirect = get_part_str(ctrl_parts, "redirect");
+  if (! redirect.empty()) {
+    op_ret = STATUS_REDIRECT;
+  }
+
   set_req_state_err(s, op_ret);
   s->err.s3_code = err_msg;
   dump_errno(s);
+  if (! redirect.empty()) {
+    dump_redirect(s, redirect);
+  }
   end_header(s, this);
 }
 
