ceph/ceph

commit c8d658bf355dd1e0285f76ce5a702053f202c94e

rgw: fix regression with handling double underscore
Fixes: http://tracker.ceph.com/issues/16856
Signed-off-by: Orit Wasserman <owasserm@redhat.com>

src/rgw/rgw_op.cc
@@ -308,7 +308,8 @@ static int read_policy(RGWRados *store,
     obj.init_ns(bucket, oid, mp_ns);
     obj.set_in_extra_data(true);
   } else {
-    obj = rgw_obj(bucket, object);
+    obj = rgw_obj(bucket, object.name);
+    obj.set_instance(s->object.instance);
   }
   int ret = get_policy_from_attr(s->cct, store, s->obj_ctx, bucket_info, bucket_attrs, policy, obj);
   if (ret == -ENOENT && !object.empty()) {
@@ -502,6 +503,7 @@ int rgw_build_object_policies(RGWRados *store, struct req_state *s,
     s->object_acl = new RGWAccessControlPolicy(s->cct);
 
     rgw_obj obj(s->bucket, s->object);
+      
     store->set_atomic(s->obj_ctx, obj);
     if (prefetch_data) {
       store->set_prefetch_data(s->obj_ctx, obj);
@@ -522,7 +524,8 @@ static void rgw_bucket_object_pre_exec(struct req_state *s)
 
 int RGWGetObj::verify_permission()
 {
-  obj = rgw_obj(s->bucket, s->object);
+  obj = rgw_obj(s->bucket, s->object.name);
+  obj.set_instance(s->object.instance);
   store->set_atomic(s->obj_ctx, obj);
   if (get_data) {
     store->set_prefetch_data(s->obj_ctx, obj);

commit c00f5087642cbf8b9214e1bc87bf04b77e415eb4

rgw: set correct instance on the object
Fixes: http://tracker.ceph.com/issues/17443

This was broken by commit c8d658bf355dd1e0285f76ce5a702053f202c94e

Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

src/rgw/rgw_op.cc
@@ -311,7 +311,7 @@ static int read_policy(RGWRados *store,
     obj.set_in_extra_data(true);
   } else {
     obj = rgw_obj(bucket, object.name);
-    obj.set_instance(s->object.instance);
+    obj.set_instance(object.instance);
   }
   int ret = get_policy_from_attr(s->cct, store, s->obj_ctx, bucket_info, bucket_attrs, policy, obj);
   if (ret == -ENOENT && !object.empty()) {