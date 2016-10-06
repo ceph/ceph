83014f4dfb5d10e51ad88ced2537659bc497d113
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2016-10-06 14:59:28 +0200

    rgw: introduce AWS_AUTHv4_IO.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_client_io.h
+++ src/rgw/rgw_client_io.h
@@ -212,12 +212,16 @@ public:
 
 /* Type conversions to work around lack of req_state type hierarchy matching
  * (e.g.) REST backends (may be replaced w/dynamic typed req_state). */
-static inline RGWRestfulIO* RESTFUL_IO(struct req_state* s) {
-  return static_cast<RGWRestfulIO*>(s->cio);
+static inline rgw::io::RestfulClient* RESTFUL_IO(struct req_state* s) {
+  return static_cast<rgw::io::RestfulClient*>(s->cio);
 }
 
 static inline rgw::io::Accounter* ACCOUNTING_IO(struct req_state* s) {
   return dynamic_cast<rgw::io::Accounter*>(s->cio);
+}
+
+static inline RGWRestfulIO* AWS_AUTHv4_IO(struct req_state* s) {
+  return static_cast<RGWRestfulIO*>(s->cio);
 }
 
 
