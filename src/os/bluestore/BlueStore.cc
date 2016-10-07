50884818e151c569cc0001053a5ade08cf9288ef
xie xingguo <xie.xingguo@zte.com.cn>
2016-10-07 09:49:28 +0800

    os/bluestore: simplify blob status checking for small writes
    If a blob is marked as compressed and still mutable, something
    is wrong.
    
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -7265,7 +7265,8 @@ void BlueStore::_do_write_small(
   if (ep != o->extent_map.extent_map.begin()) {
     --ep;
     b = ep->blob;
-    if (ep->logical_offset - ep->blob_offset + b->get_blob().get_ondisk_length() <= offset) {
+    if (ep->logical_offset - ep->blob_offset +
+      b->get_blob().get_ondisk_length() <= offset) {
       ++ep;
     }
   }
@@ -7274,7 +7275,7 @@ void BlueStore::_do_write_small(
       break;
     }
     b = ep->blob;
-    if (!b->get_blob().is_mutable() || b->get_blob().is_compressed()) {
+    if (!b->get_blob().is_mutable()) {
       dout(20) << __func__ << " ignoring immutable " << *b << dendl;
       ++ep;
       continue;
