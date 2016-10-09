bc618ae1ed4a9dd7d5c979e6e4c6fd1a4d577bb5
xie xingguo <xie.xingguo@zte.com.cn>
2016-10-09 10:52:09 +0800

    mgr/MgrMonitor: make period of sending digests configurable
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -1514,6 +1514,7 @@ OPTION(mgr_module_path, OPT_STR, CEPH_PKGLIBDIR "/mgr") // where to load python 
 OPTION(mgr_modules, OPT_STR, "rest")  // Which modules to load
 OPTION(mgr_data, OPT_STR, "/var/lib/ceph/mgr/$cluster-$id") // where to find keyring etc
 OPTION(mgr_beacon_period, OPT_INT, 5)  // How frequently to send beacon
+OPTION(mon_mgr_digest_period, OPT_INT, 5)  // How frequently to send digests
 OPTION(mon_mgr_beacon_grace, OPT_INT, 30)  // How long to wait to failover
 
 OPTION(rgw_list_bucket_min_readahead, OPT_INT, 1000) // minimum number of entries to read from rados for bucket listing
