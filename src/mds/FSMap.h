8a028b07d40b3e5f483de3fd896bf39e57f7b51f
Patrick Donnelly <pdonnell@redhat.com>
2016-10-10 18:14:18 -0400

    mds: group filesystem access methods
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>
    (cherry picked from commit 59de9919a0e8388c1b8e7968187de6b1b6492425)

--- src/mds/FSMap.h
+++ src/mds/FSMap.h
@@ -375,15 +375,22 @@ public:
     });
   }
 
-  size_t filesystem_count() const {return filesystems.size();}
-  bool filesystem_exists(fs_cluster_id_t fscid) const
-    {return filesystems.count(fscid) > 0;}
-
   epoch_t get_epoch() const { return epoch; }
   void inc_epoch() { epoch++; }
 
+  size_t filesystem_count() const {return filesystems.size();}
+  bool filesystem_exists(fs_cluster_id_t fscid) const {return filesystems.count(fscid) > 0;}
   std::shared_ptr<const Filesystem> get_filesystem(fs_cluster_id_t fscid) const {return std::const_pointer_cast<const Filesystem>(filesystems.at(fscid));}
   std::shared_ptr<const Filesystem> get_filesystem(void) const {return std::const_pointer_cast<const Filesystem>(filesystems.begin()->second);}
+  std::shared_ptr<const Filesystem> get_filesystem(const std::string &name) const
+  {
+    for (auto &i : filesystems) {
+      if (i.second->mds_map.fs_name == name) {
+        return std::const_pointer_cast<const Filesystem>(i.second);
+      }
+    }
+    return nullptr;
+  }
 
   int parse_filesystem(
       std::string const &ns_str,
@@ -417,17 +424,6 @@ public:
 
   void get_health(list<pair<health_status_t,std::string> >& summary,
 		  list<pair<health_status_t,std::string> > *detail) const;
-
-  std::shared_ptr<const Filesystem> get_filesystem(const std::string &name) const
-  {
-    for (auto &i : filesystems) {
-      if (i.second->mds_map.fs_name == name) {
-        return i.second;
-      }
-    }
-
-    return nullptr;
-  }
 
   /**
    * Assert that the FSMap, Filesystem, MDSMap, mds_info_t relations are
