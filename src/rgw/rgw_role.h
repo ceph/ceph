a6e5abf9a11bcc25e377805a7540db8edc0a7fa5
Pritha Srivastava <prsrivas@redhat.com>
2016-10-18 12:53:10 +0530

    rgw: Added code for manipulation of inline permission policies attached to roles.
    Implemented radosgw-admin commands for creating/ updating/ deleting/ reading/
    and listing of permission policies attached to roles.
    
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>

--- src/rgw/rgw_role.h
+++ src/rgw/rgw_role.h
@@ -13,6 +13,7 @@ class RGWRole
   string path;
   string creation_date;
   string trust_policy;
+  map<string, string> perm_policy_map;
 
   int store_info(bool exclusive);
   int store_name(bool exclusive);
@@ -53,6 +54,7 @@ public:
     ::encode(path, bl);
     ::encode(creation_date, bl);
     ::encode(trust_policy, bl);
+    ::encode(perm_policy_map, bl);
     ENCODE_FINISH(bl);
   }
 
@@ -63,6 +65,7 @@ public:
     ::decode(path, bl);
     ::decode(creation_date, bl);
     ::decode(trust_policy, bl);
+    ::decode(perm_policy_map, bl);
     DECODE_FINISH(bl);
   }
 
@@ -76,6 +79,10 @@ public:
   int get();
   int update();
   void update_trust_policy(string& trust_policy);
+  void set_perm_policy(const string& policy_name, const string& perm_policy);
+  vector<string> get_role_policy_names();
+  int get_role_policy(const string& policy_name, string& perm_policy);
+  int delete_policy(const string& policy_name);
   void dump(Formatter *f) const;
   void decode_json(JSONObj *obj);
 
