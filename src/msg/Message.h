d2f9b775fe58fcc9c5fe0f777987dd8cad95f88a
Sage Weil <sage@redhat.com>
2016-10-23 18:40:57 -0500

    msg: adjust byte_throttler from Message::encode
    Normally we never call encode on a message that has a byte_throttler set
    because we only use it for messages we received.  However, for forwarded
    messages that we clear_payload() before resending, we *do* reencode, and in
    that case we need to retake the appropriate number of bytes from the
    throttler--just like we release them in clear_payload().
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/msg/Message.h
+++ src/msg/Message.h
@@ -329,8 +329,9 @@ public:
    */
 
   void clear_payload() {
-    if (byte_throttler)
+    if (byte_throttler) {
       byte_throttler->put(payload.length() + middle.length());
+    }
     payload.clear();
     middle.clear();
   }
