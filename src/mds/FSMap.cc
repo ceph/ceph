c220eb99a394fe00fc4ccbb653ff6b338f7088e6
Michal Jarzabek <stiopa@gmail.com>
2016-11-05 19:06:16 +0000

    mds/FSMap.cc: prevent unneeded copy of map entry
    Signed-off-by: Michal Jarzabek <stiopa@gmail.com>

--- src/mds/FSMap.cc
+++ src/mds/FSMap.cc
@@ -50,7 +50,7 @@ void FSMap::dump(Formatter *f) const
   f->close_section();
 
   f->open_array_section("filesystems");
-  for (const auto fs : filesystems) {
+  for (const auto &fs : filesystems) {
     f->open_object_section("filesystem");
     fs.second->dump(f);
     f->close_section();
