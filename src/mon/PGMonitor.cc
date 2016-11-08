23904121618f04fbeacbf979a330cae8eabcc49f
xie xingguo <xie.xingguo@zte.com.cn>
2016-11-08 17:24:05 +0800

    osd: kill PG_STATE_SPLITTING
    The PG_STATE_SPLITTING is never recorded and therefore is not traceable,
    and removing the unnecessary code shall always be the preferred option.
    
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- src/mon/PGMonitor.cc
+++ src/mon/PGMonitor.cc
@@ -1678,8 +1678,6 @@ void PGMonitor::get_health(list<pair<health_status_t,string> >& summary,
       note["peering"] += p->second;
     if (p->first & PG_STATE_REPAIR)
       note["repair"] += p->second;
-    if (p->first & PG_STATE_SPLITTING)
-      note["splitting"] += p->second;
     if (p->first & PG_STATE_RECOVERING)
       note["recovering"] += p->second;
     if (p->first & PG_STATE_RECOVERY_WAIT)
@@ -1777,7 +1775,6 @@ void PGMonitor::get_health(list<pair<health_status_t,string> >& summary,
 	                        PG_STATE_INCONSISTENT |
 	                        PG_STATE_PEERING |
 	                        PG_STATE_REPAIR |
-	                        PG_STATE_SPLITTING |
 	                        PG_STATE_RECOVERING |
 	                        PG_STATE_RECOVERY_WAIT |
 	                        PG_STATE_INCOMPLETE |
