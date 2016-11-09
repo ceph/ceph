e60c0d1d639a8c60e0f6b67ecc2505575949c111
Nathan Cutler <ncutler@suse.com>
2016-11-09 15:23:51 +0100

    Revert "rgw file: remove busy-wait in RGWLibFS::gc()"
    This reverts commit 4ceb70eba4d777ecc34cce9d1d10002807a6e770.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- src/rgw/rgw_file.cc
+++ src/rgw/rgw_file.cc
@@ -679,7 +679,6 @@ namespace rgw {
 	  } /* rgw_fh */
 	} /* event::type::READDIR */
       } /* ev */
-      std::this_thread::sleep_for(gc_interval);
     } while (! stop);
   } /* RGWLibFS::gc */
 
