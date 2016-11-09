47dce82149b438697c2f4ca1bdcbe13718507181
Nathan Cutler <ncutler@suse.com>
2016-11-09 15:20:53 +0100

    Revert "rgw_file:  pre-assign times"
    This reverts commit 1a7faefd035aa8f31ab11ddfe1046989d0e69aac.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- src/rgw/rgw_file.h
+++ src/rgw/rgw_file.h
@@ -1809,10 +1809,6 @@ public:
     return (iter != attrs.end()) ? &(iter->second) : nullptr;
   }
 
-  real_time get_ctime() const {
-    return bucket.creation_time;
-  }
-
   virtual bool only_bucket() { return false; }
 
   virtual int op_init() {
@@ -1843,6 +1839,10 @@ public:
     s->user = user;
 
     return 0;
+  }
+
+  real_time get_ctime() const {
+    return bucket.creation_time;
   }
 
   virtual int get_params() {
