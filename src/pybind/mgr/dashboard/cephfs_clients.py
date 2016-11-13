ac43d5dc6d0c93c1fb77173bb9883a1014641f05
John Spray <john.spray@redhat.com>
2016-11-13 11:38:16 +0000

    pybind/mgr: web dashboard
    A read only display of some of the cluster's state, plus
    some cephfs/rbd/osd pages that demonstrate how to go
    read state remotely from daemons.
    
    Signed-off-by: John Spray <john.spray@redhat.com>

---
+++ src/pybind/mgr/dashboard/cephfs_clients.py
@@ -0,0 +1,25 @@
+
+import json
+
+from mgr_module import CommandResult
+from remote_view_cache import RemoteViewCache
+
+
+class CephFSClients(RemoteViewCache):
+    def __init__(self, module_inst, fscid):
+        super(CephFSClients, self).__init__(module_inst)
+
+        self.fscid = fscid
+
+    def _get(self):
+        mds_spec = "{0}:0".format(self.fscid)
+        result = CommandResult("")
+        self._module.send_command(result, "mds", mds_spec,
+               json.dumps({
+                   "prefix": "session ls",
+                   }),
+               "")
+        r, outb, outs = result.wait()
+        # TODO handle nonzero returns, e.g. when rank isn't active
+        assert r == 0
+        return json.loads(outb)
