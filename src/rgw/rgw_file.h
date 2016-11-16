1d197f8de4165704eef2a12c183fdcfa4885a177
Nathan Cutler <ncutler@suse.com>
2016-11-16 13:26:35 +0100

    Revert "Revert "rgw nfs: fix write continuations""
    This reverts commit 1bcb8591ba6e75988392d39d1a6489f4a768e8e1.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- src/rgw/rgw_file.h
+++ src/rgw/rgw_file.h
@@ -1932,8 +1932,7 @@ public:
   RGWPutObjProcessor *processor;
   buffer::list data;
   MD5 hash;
-  off_t last_off;
-  off_t next_off;
+  off_t real_ofs;
   size_t bytes_written;
   bool multipart;
   bool eio;
@@ -1941,8 +1940,8 @@ public:
   RGWWriteRequest(CephContext* _cct, RGWUserInfo *_user, RGWFileHandle* _fh,
 		  const std::string& _bname, const std::string& _oname)
     : RGWLibContinuedReq(_cct, _user), bucket_name(_bname), obj_name(_oname),
-      rgw_fh(_fh), processor(nullptr), last_off(0), next_off(0),
-      bytes_written(0), multipart(false), eio(false) {
+      rgw_fh(_fh), processor(nullptr), real_ofs(0), bytes_written(0),
+      multipart(false), eio(false) {
 
     int ret = header_init();
     if (ret == 0) {
@@ -2015,10 +2014,12 @@ public:
   }
 
   void put_data(off_t off, buffer::list& _bl) {
-    if (off && (off != (ofs+1)))
+    if (off != real_ofs) {
       eio = true;
-    ofs = off;
+    }
     data.claim(_bl);
+    real_ofs += data.length();
+    ofs = off; /* consumed in exec_continue() */
   }
 
   virtual int exec_start();
