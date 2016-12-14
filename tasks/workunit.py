1ae7aa17f4740e0a8b25ec9243035841c8a2c340
Sage Weil <sage@redhat.com>
2016-12-14 12:28:23 -0500

    Revert "tasks/workunit.py: depth 1 clone"
    This reverts commit 6e12e0327db656d203ae520346a7dc0010ab5b5a.

--- tasks/workunit.py
+++ tasks/workunit.py
@@ -314,8 +314,6 @@ def _run_tests(ctx, refspec, role, tests, env, subdir=None, timeout=None):
         args=[
             'git',
             'clone',
-            '--depth',
-            '1',
             git_url,
             clonedir,
             run.Raw(';'),
