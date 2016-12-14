bfce59e81b4d4f9bdb7b7679929b1e32b20c2d42
Zack Cerza <zack@redhat.com>
2016-12-14 12:43:59 -0700

    Default suite repo to ceph.git
    To reflect the move from ceph-qa-suite.git -> ceph.git
    
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- teuthology/config.py
+++ teuthology/config.py
@@ -183,7 +183,7 @@ class TeuthologyConfig(YamlConfig):
 
     def get_ceph_qa_suite_git_url(self):
         return (self.ceph_qa_suite_git_url or
-                self.ceph_git_base_url + 'ceph-qa-suite.git')
+                self.ceph_git_base_url + 'ceph.git')
 
     def get_ceph_git_url(self):
         return (self.ceph_git_url or
