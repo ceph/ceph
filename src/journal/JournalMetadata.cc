60f1b1a2f2a96cfe72acfc6855b9cc26b480732d
Jason Dillaman <dillaman@redhat.com>
2016-12-14 20:49:46 -0500

    journal: prevent repetitive error messages after being blacklisted
    Fixes: http://tracker.ceph.com/issues/18243
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/journal/JournalMetadata.cc
+++ src/journal/JournalMetadata.cc
@@ -848,6 +848,8 @@ void JournalMetadata::handle_watch_reset() {
   if (r < 0) {
     if (r == -ENOENT) {
       ldout(m_cct, 5) << __func__ << ": journal header not found" << dendl;
+    } else if (r == -EBLACKLISTED) {
+      ldout(m_cct, 5) << __func__ << ": client blacklisted" << dendl;
     } else {
       lderr(m_cct) << __func__ << ": failed to watch journal: "
                    << cpp_strerror(r) << dendl;
@@ -872,7 +874,7 @@ void JournalMetadata::handle_watch_error(int err) {
   if (err == -ENOTCONN) {
     ldout(m_cct, 5) << "journal watch error: header removed" << dendl;
   } else if (err == -EBLACKLISTED) {
-    ldout(m_cct, 5) << "journal watch error: client blacklisted" << dendl;
+    lderr(m_cct) << "journal watch error: client blacklisted" << dendl;
   } else {
     lderr(m_cct) << "journal watch error: " << cpp_strerror(err) << dendl;
   }
