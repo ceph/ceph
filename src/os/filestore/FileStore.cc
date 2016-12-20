ae498e84ffcff7424721f0d2704ec739d1cc092d
liuchang0812 <liuchang0812@gmail.com>
2016-12-20 13:21:40 +0800

    os/filestore: fix clang static check warn use-after-free
    Signed-off-by: liuchang0812 <liuchang0812@gmail.com>
    (cherry picked from commit 1d359455b3dd6abb383542ba596a03f14ac54dbd)
    See: http://tracker.ceph.com/issues/19311
    
    Signed-off-by: yaoning <yaoning@unitedstack.com>

--- src/os/filestore/FileStore.cc
+++ src/os/filestore/FileStore.cc
@@ -3103,12 +3103,15 @@ more:
     last = extent++;
   }
   const bool is_last = last->fe_flags & FIEMAP_EXTENT_LAST;
-  free(fiemap);
   if (!is_last) {
     uint64_t xoffset = last->fe_logical + last->fe_length - offset;
     offset = last->fe_logical + last->fe_length;
     len -= xoffset;
+    free(fiemap); /* fix clang warn: use-after-free */
     goto more;
+  }
+  else {
+    free(fiemap);
   }
 
   return r;
