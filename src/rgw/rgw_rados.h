7e51bec0dd77bedc248a595e1ab63f87d7a30458
Orit Wasserman <owasserm@redhat.com>
2016-12-25 12:36:34 +0200

    rgw: clear master_zonegroup when reseting RGWPeriodMap
    Fixes: http://tracker.ceph.com/issues/17239
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>
    (cherry picked from commit d8f42fe6be659c1d48bf04b30aa54ad616936145)

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -1230,6 +1230,7 @@ struct RGWPeriodMap
   void reset() {
     zonegroups.clear();
     zonegroups_by_api.clear();
+    master_zonegroup.clear();
   }
 
   uint32_t get_zone_short_id(const string& zone_id) const;
