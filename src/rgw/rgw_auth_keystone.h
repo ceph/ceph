096052d3fa5a2da99ff1a28bc74806ae534d8859
Radoslaw Zarzynski <rzarzynski@mirantis.com>
2017-01-04 20:22:13 +0100

    rgw: add rgw::auth::keystone::EC2Engine.
    Signed-off-by: Radoslaw Zarzynski <rzarzynski@mirantis.com>

--- src/rgw/rgw_auth_keystone.h
+++ src/rgw/rgw_auth_keystone.h
@@ -6,6 +6,7 @@
 #define CEPH_RGW_AUTH_KEYSTONE_H
 
 #include "rgw_auth.h"
+#include "rgw_rest_s3.h"
 #include "rgw_common.h"
 #include "rgw_keystone.h"
 
@@ -61,6 +62,55 @@ public:
     return authenticate(extractor->get_token(s));
   }
 }; /* class TokenEngine */
+
+
+class EC2Engine : public rgw::auth::s3::Version2ndEngine {
+  using acl_strategy_t = rgw::auth::RemoteApplier::acl_strategy_t;
+  using auth_info_t = rgw::auth::RemoteApplier::AuthInfo;
+  using result_t = rgw::auth::Engine::result_t;
+  using token_envelope_t = rgw::keystone::TokenEnvelope;
+
+  const rgw::auth::RemoteApplier::Factory* const apl_factory;
+  rgw::keystone::Config& config;
+  rgw::keystone::TokenCache& token_cache;
+
+  /* Helper methods. */
+  acl_strategy_t get_acl_strategy(const token_envelope_t& token) const;
+  auth_info_t get_creds_info(const token_envelope_t& token,
+                             const std::vector<std::string>& admin_roles
+                            ) const noexcept;
+  bool is_time_skew_ok(const utime_t& header_time,
+                       const bool qsr) const;
+  token_envelope_t get_from_keystone(const std::string& access_key_id,
+                                     const std::string& string_to_sign,
+                                     const std::string& signature) const;
+  result_t authenticate(std::string access_key_id,
+                        std::string signature,
+                        std::string expires,
+                        bool qsr,
+                        const req_info& info) const override;
+public:
+  EC2Engine(CephContext* const cct,
+            const rgw::auth::s3::Version2ndEngine::Extractor* const extractor,
+            const rgw::auth::RemoteApplier::Factory* const apl_factory,
+            rgw::keystone::Config& config,
+            /* The token cache is used ONLY for the retrieving admin token.
+             * Due to the architecture of AWS Auth S3 credentials cannot be
+             * cached at all. */
+            rgw::keystone::TokenCache& token_cache)
+    : Version2ndEngine(cct, *extractor),
+      apl_factory(apl_factory),
+      config(config),
+      token_cache(token_cache) {
+  }
+
+  using Version2ndEngine::authenticate;
+
+  const char* get_name() const noexcept override {
+    return "rgw::auth::keystone::EC2Engine";
+  }
+
+}; /* class EC2Engine */
 
 }; /* namespace keystone */
 }; /* namespace auth */
--- src/rgw/rgw_rest_s3.h
+++ src/rgw/rgw_rest_s3.h
@@ -398,56 +398,6 @@ public:
   void end_response();
 };
 
-class RGW_Auth_S3_Keystone_ValidateToken : public RGWHTTPClient {
-private:
-  bufferlist rx_buffer;
-  bufferlist tx_buffer;
-  bufferlist::iterator tx_buffer_it;
-  vector<string> accepted_roles;
-
-public:
-  rgw::keystone::TokenEnvelope response;
-
-private:
-  void set_tx_buffer(const string& d) {
-    tx_buffer.clear();
-    tx_buffer.append(d);
-    tx_buffer_it = tx_buffer.begin();
-    set_send_length(tx_buffer.length());
-  }
-
-public:
-  explicit RGW_Auth_S3_Keystone_ValidateToken(CephContext *_cct)
-      : RGWHTTPClient(_cct) {
-    get_str_vec(cct->_conf->rgw_keystone_accepted_roles, accepted_roles);
-  }
-
-  int receive_header(void *ptr, size_t len) {
-    return 0;
-  }
-  int receive_data(void *ptr, size_t len) {
-    rx_buffer.append((char *)ptr, len);
-    return 0;
-  }
-
-  int send_data(void *ptr, size_t len) {
-    if (!tx_buffer_it.get_remaining())
-      return 0; // nothing left to send
-
-    int l = MIN(tx_buffer_it.get_remaining(), len);
-    memcpy(ptr, tx_buffer_it.get_current_ptr().c_str(), l);
-    try {
-      tx_buffer_it.advance(l);
-    } catch (buffer::end_of_buffer &e) {
-      ceph_abort();
-    }
-
-    return l;
-  }
-
-  int validate_s3token(const string& auth_id, const string& auth_token, const string& auth_sign);
-
-};
 
 class RGW_Auth_S3 {
 private:
