d5fa6a65b52c3f2cf5363ebb55af7e96d8be90ab
Mykola Golub <mgolub@mirantis.com>
2017-01-09 09:23:19 +0100

    rbd-mirror: async request to test if image is primary
    Signed-off-by: Mykola Golub <mgolub@mirantis.com>
    (cherry picked from commit 0a1cb35caacdf85029f31a0364dc07a5d7462f5f)

--- src/tools/rbd_mirror/image_replayer/BootstrapRequest.h
+++ src/tools/rbd_mirror/image_replayer/BootstrapRequest.h
@@ -101,6 +101,9 @@ private:
    *    v                                               *
    * OPEN_REMOTE_IMAGE  * * * * * * * * * * * * * * * * *
    *    |                                               *
+   *    v                                               *
+   * IS_PRIMARY * * * * * * * * * * * * * * * * * * * * *
+   *    |                                               *
    *    | (remote image primary)                        *
    *    \----> OPEN_LOCAL_IMAGE * * * * * * * * * * * * *
    *    |         |   .   ^                             *
@@ -167,6 +170,7 @@ private:
   cls::journal::Client m_client;
   uint64_t m_remote_tag_class = 0;
   ImageCtxT *m_remote_image_ctx = nullptr;
+  bool m_primary = false;
   bool m_created_local_image = false;
   int m_ret_val = 0;
 
@@ -186,6 +190,9 @@ private:
 
   void open_remote_image();
   void handle_open_remote_image(int r);
+
+  void is_primary();
+  void handle_is_primary(int r);
 
   void update_client_state();
   void handle_update_client_state(int r);
