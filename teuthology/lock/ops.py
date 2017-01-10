77185fdd2d19b286263a6c3ad24aaff7e9208715
Zack Cerza <zack@redhat.com>
2017-01-10 12:47:21 -0700

    Don't unlock VMs when destroy fails
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- teuthology/lock/ops.py
+++ teuthology/lock/ops.py
@@ -163,6 +163,7 @@ def unlock_one(ctx, name, user, description=None):
     name = misc.canonicalize_hostname(name, user=None)
     if not teuthology.provision.destroy_if_vm(ctx, name, user, description):
         log.error('destroy failed for %s', name)
+        return False
     request = dict(name=name, locked=False, locked_by=user,
                    description=description)
     uri = os.path.join(config.lock_server, 'nodes', name, 'lock', '')
