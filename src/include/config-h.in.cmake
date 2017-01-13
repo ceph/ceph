b0ae5ae2adda63260d1eacc58aa0a798645f5965
wei xiao <wei.xiao@linaro.org>
2017-01-13 04:36:39 +0000

    crc32c: optimize aarch64 crc32c implementation
    ARMv8 defines PMULL crypto instruction.
    This patch optimizes crc32c calculate with the instruction when
    available rather than original linear crc instructions.
    
    ceph crc32c performance unit test shows that the optimization get
    ~ x3.90 speedups on ThunderX ARM Core@2.0GHz (Cavium)
    ~ x1.45 speedups on ARM Cortex-A57@2.1GHz (Huaiwei)
    ~ x1.16 speedups on ARM Cortex-A57@2.0GHz (Softiron)
    
    Jira: ENTLLT-358
    Change-Id: I657422cd20c9ca78237cd060210a5383f4122575
    Signed-off-by: wei xiao <wei.xiao@linaro.org>

--- src/include/config-h.in.cmake
+++ src/include/config-h.in.cmake
@@ -277,6 +277,12 @@
 /* Support ARMv8 CRC instructions */
 #cmakedefine HAVE_ARMV8_CRC
 
+/* Support ARMv8 CRYPTO instructions */
+#cmakedefine HAVE_ARMV8_CRYPTO
+
+/* Support ARMv8 CRC and CRYPTO intrinsics */
+#cmakedefine HAVE_ARMV8_CRC_CRYPTO_INTRINSICS
+
 /* Define if you have struct stat.st_mtimespec.tv_nsec */
 #cmakedefine HAVE_STAT_ST_MTIMESPEC_TV_NSEC
 
