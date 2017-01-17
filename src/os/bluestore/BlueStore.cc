b3fca67cc35cea2889c6ac70ba3541b0048317a4
Sage Weil <sage@redhat.com>
2017-01-17 11:17:38 -0500

    os/bluestore: prealloc/reserve extent vector for common alloc path
    No need to worry about the gift/reclaim path--those are very rare.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -8105,6 +8105,7 @@ int BlueStore::_do_alloc_write(
     }
 
     AllocExtentVector extents;
+    extents.reserve(4);  // 4 should be (more than) enough for most allocations
     int64_t got = alloc->allocate(final_length, min_alloc_size, max_alloc_size,
                             hint, &extents);
     assert(got == (int64_t)final_length);
