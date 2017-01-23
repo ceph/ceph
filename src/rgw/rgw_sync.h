aaab1ec2f736248b3557f42b4108e91b45f6b407
Casey Bodley <cbodley@redhat.com>
2017-01-23 16:38:48 -0500

    rgw: RGWSyncLogTrimThread runs mdlog trim
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_sync.h
+++ src/rgw/rgw_sync.h
@@ -450,4 +450,8 @@ public:
   int operate() override;
 };
 
+// MetaLogTrimCR factory function
+RGWCoroutine* create_meta_log_trim_cr(RGWRados *store, RGWHTTPManager *http,
+                                      int num_shards, utime_t interval);
+
 #endif
