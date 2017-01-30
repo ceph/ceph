08534bbadf94527f795cac3a20eda4e74dee83c9
Shinobu Kinjo <shinobu@redhat.com>
2017-01-30 00:38:45 -0500

    RadosStriperImpl.cc: Avoid assertion if object size is ZERO
    Do not assemble any piceces of ZERO size object
    
    Fixes: http://tracker.ceph.com/issues/14609
    
    Signed-off-by: Shinobu Kinjo <shinobu@redhat.com>

--- src/libradosstriper/RadosStriperImpl.cc
+++ src/libradosstriper/RadosStriperImpl.cc
@@ -764,17 +764,12 @@ static void rados_req_write_complete(rados_completion_t c, void *arg)
 
 int libradosstriper::RadosStriperImpl::internal_aio_write(
   const std::string& soid,
-<<<<<<< HEAD
   libradosstriper::MultiAioCompletionImpl *mac,
-=======
-  libradosstriper::MultiAioCompletionImpl *c,
->>>>>>> a211db19ddbb5c65b3152ba0154ab983220cbdc1
   const bufferlist& bl,
   size_t len,
   uint64_t off,
   const ceph_file_layout& layout)
 {
-<<<<<<< HEAD
 /// * Do not stripe object, if its size is zero byte
   if (len > 0) {
     vector<ObjectExtent> e;  /// * Extents to be written
@@ -807,51 +802,6 @@ int libradosstriper::RadosStriperImpl::internal_aio_write(
   mac->finish_adding_requests();
 
   return 0;  
-=======
-  int r = 0;
-
-  if (len > 0) {
-    // get list of extents to be written to
-    vector<ObjectExtent> extents;
-    std::string format = soid + RADOS_OBJECT_EXTENSION_FORMAT;
-    file_layout_t l;
-    l.from_legacy(layout);
-    Striper::file_to_extents(cct(), format.c_str(), &l, off, len, 0, extents);
-
-    // go through the extents
-    for (vector<ObjectExtent>::iterator p = extents.begin();
-      p != extents.end(); ++p) {
-      // assemble pieces of a given object into a single buffer list
-      bufferlist oid_bl;
-
-      for (vector<pair<uint64_t,uint64_t> >::iterator q =
-             p->buffer_extents.begin();
-        q != p->buffer_extents.end(); ++q) {
-        bufferlist buffer_bl;
-        buffer_bl.substr_of(bl, q->first, q->second);
-        oid_bl.append(buffer_bl);
-      }
-
-      // and write the object
-      c->add_request();
-      librados::AioCompletion *rados_completion =
-        m_radosCluster.aio_create_completion(
-          c, rados_req_write_complete, rados_req_write_safe);
-
-      r = m_ioCtx.aio_write(
-        p->oid.name, rados_completion, oid_bl, p->length, p->offset);
-
-      rados_completion->release();
-
-      if (r < 0)
-        break;
-    }
-  }
-
-  c->finish_adding_requests();
-
-  return r;
->>>>>>> a211db19ddbb5c65b3152ba0154ab983220cbdc1
 }
 
 int libradosstriper::RadosStriperImpl::extract_uint32_attr
