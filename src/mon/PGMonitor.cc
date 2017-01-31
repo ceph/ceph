3491892facc8fdbbb2129903f0dca37e299ed03b
Sage Weil <sage@redhat.com>
2017-01-31 12:31:42 -0500

    mon/PGMonitor: fix force_create_pg
    We weren't carefully registering the creating PG.  In
    particular, the current osd mappings (acting, up, etc.)
    weren't getting filled in, which meant the PG wasn't
    (necessarily) mapped to an OSD until the OSDMap updated
    and we recalculated mappings.
    
    Fix by sending us through the common code path so that
    all fields get updated properly.
    
    Fixes: http://tracker.ceph.com/issues/18298
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mon/PGMonitor.cc
+++ src/mon/PGMonitor.cc
@@ -1381,10 +1381,13 @@ bool PGMonitor::prepare_command(MonOpRequestRef op)
       goto reply;
     }
     {
-      pg_stat_t& s = pending_inc.pg_stat_updates[pgid];
-      s.state = PG_STATE_CREATING;
-      s.created = epoch;
-      s.last_change = ceph_clock_now();
+      PGMapUpdater::register_pg(
+	mon->osdmon()->osdmap,
+	pgid,
+	epoch,
+	true,
+	&pg_map,
+	&pending_inc);
     }
     ss << "pg " << pgidstr << " now creating, ok";
     goto update;
