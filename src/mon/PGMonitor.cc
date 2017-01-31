b79a85e80a1a660c5a5746270edb86d3f09cd983
Sage Weil <sage@redhat.com>
2017-01-31 12:31:12 -0500

    mon/PGMonitor: clean up some send_pg_creates cruft
    ff1fd4b4b69b52ef5ae734115b1d733aad03b03e already removed most of
    this; remove the now useless stub and clean up the unneede con
    check.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mon/PGMonitor.cc
+++ src/mon/PGMonitor.cc
@@ -236,8 +236,12 @@ void PGMonitor::upgrade_format()
 void PGMonitor::post_paxos_update()
 {
   dout(10) << __func__ << dendl;
-  if (mon->osdmon()->osdmap.get_epoch()) {
-    send_pg_creates();
+  OSDMap& osdmap = mon->osdmon()->osdmap;
+  if (osdmap.get_epoch()) {
+    if (osdmap.get_num_up_osds() > 0) {
+      assert(osdmap.get_up_osd_features() & CEPH_FEATURE_MON_STATEFUL_SUB);
+      check_subs();
+    }
   }
 }
 
@@ -879,17 +883,6 @@ void PGMonitor::check_osd_map(epoch_t epoch)
   propose_pending();
 }
 
-void PGMonitor::send_pg_creates()
-{
-  OSDMap& osdmap = mon->osdmon()->osdmap;
-  if (osdmap.get_num_up_osds() == 0)
-    return;
-
-  assert(osdmap.get_up_osd_features() & CEPH_FEATURE_MON_STATEFUL_SUB);
-  check_subs();
-  return;
-}
-
 epoch_t PGMonitor::send_pg_creates(int osd, Connection *con, epoch_t next)
 {
   dout(30) << __func__ << " " << pg_map.creating_pgs_by_osd_epoch << dendl;
@@ -927,12 +920,7 @@ epoch_t PGMonitor::send_pg_creates(int osd, Connection *con, epoch_t next)
     return next;
   }
 
-  if (con) {
-    con->send_message(m);
-  } else {
-    assert(mon->osdmon()->osdmap.is_up(osd));
-    mon->messenger->send_message(m, mon->osdmon()->osdmap.get_inst(osd));
-  }
+  con->send_message(m);
   last_sent_pg_create[osd] = ceph_clock_now();
 
   // sub is current through last + 1
