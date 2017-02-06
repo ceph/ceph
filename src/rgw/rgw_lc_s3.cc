2c3a7698bef836af9781c163cf2e1a235aa08648
Zhang Shaowen <zhangshaowen@cmss.chinamobile.com>
2017-02-06 13:45:24 +0800

    rgw: add support for noncurrentversion expiration in s3 lifecycle.
    Fixes: http://tracker.ceph.com/issues/18916
    
    Signed-off-by: Zhang Shaowen <zhangshaowen@cmss.chinamobile.com>

--- src/rgw/rgw_lc_s3.cc
+++ src/rgw/rgw_lc_s3.cc
@@ -23,6 +23,15 @@ bool LCExpiration_S3::xml_end(const char * el) {
   return true;
 }
 
+bool LCNoncurExpiration_S3::xml_end(const char *el) {
+  LCNoncurDays_S3 *lc_noncur_days = static_cast<LCNoncurDays_S3 *>(find_first("NoncurrentDays"));
+  if (!lc_noncur_days) {
+    return false;
+  }
+  days = lc_noncur_days->get_data();
+  return true;
+}
+
 bool RGWLifecycleConfiguration_S3::xml_end(const char *el) {
   XMLObjIter iter = find("Rule");
   LCRule_S3 *rule = static_cast<LCRule_S3 *>(iter.get_next());
@@ -38,6 +47,7 @@ bool LCRule_S3::xml_end(const char *el) {
   LCPrefix_S3 *lc_prefix;
   LCStatus_S3 *lc_status;
   LCExpiration_S3 *lc_expiration;
+  LCNoncurExpiration_S3 *lc_noncur_expiration;
 
   id.clear();
   prefix.clear();
@@ -61,20 +71,34 @@ bool LCRule_S3::xml_end(const char *el) {
     return false;
 
   lc_expiration = static_cast<LCExpiration_S3 *>(find_first("Expiration"));
-  if (!lc_expiration)
+  lc_noncur_expiration = static_cast<LCNoncurExpiration_S3 *>(find_first("NoncurrentVersionExpiration"));
+  if (!lc_expiration && !lc_noncur_expiration) {
     return false;
-  expiration = *lc_expiration;
+  } else {
+    if (lc_expiration) {
+      expiration = *lc_expiration;
+    }
+    if (lc_noncur_expiration) {
+      noncur_expiration = *lc_noncur_expiration;
+    }
+  }
 
   return true;
 }
 
 void LCRule_S3::to_xml(CephContext *cct, ostream& out) {
-  LCExpiration_S3& expir = static_cast<LCExpiration_S3&>(expiration);
   out << "<Rule>" ;
   out << "<ID>" << id << "</ID>";
   out << "<Prefix>" << prefix << "</Prefix>";
   out << "<Status>" << status << "</Status>";
-  expir.to_xml(out);
+  if (!expiration.empty()) {
+    LCExpiration_S3& expir = static_cast<LCExpiration_S3&>(expiration);
+    expir.to_xml(out);
+  }
+  if (!noncur_expiration.empty()) {
+    LCNoncurExpiration_S3& noncur_expir = static_cast<LCNoncurExpiration_S3&>(noncur_expiration);
+    noncur_expir.to_xml(out);
+  }
   out << "</Rule>";
 }
 
@@ -123,6 +147,10 @@ XMLObj *RGWLCXMLParser_S3::alloc_obj(const char *el)
     obj = new LCExpiration_S3();
   } else if (strcmp(el, "Days") == 0) {
     obj = new LCDays_S3();
+  } else if (strcmp(el, "NoncurrentVersionExpiration") == 0) {
+    obj = new LCNoncurExpiration_S3();
+  } else if (strcmp(el, "NoncurrentDays") == 0) {
+    obj = new LCNoncurDays_S3();
   }
   return obj;
 }
