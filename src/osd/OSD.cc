3dbfa4fd8b6b57bbe2e0676b059b10ce4862eb87
Sage Weil <sage@redhat.com>
2017-02-06 23:25:15 -0500

    osd: explicitly enumerate ops we can dispatch
    This prevents random messages from falling into and OpRequest and
    dispatch_op().
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -6519,7 +6519,15 @@ void OSD::_dispatch(Message *m)
 
     // -- need OSDMap --
 
-  default:
+  case MSG_OSD_PG_CREATE:
+  case MSG_OSD_PG_NOTIFY:
+  case MSG_OSD_PG_QUERY:
+  case MSG_OSD_PG_LOG:
+  case MSG_OSD_PG_REMOVE:
+  case MSG_OSD_PG_INFO:
+  case MSG_OSD_PG_TRIM:
+  case MSG_OSD_BACKFILL_RESERVE:
+  case MSG_OSD_RECOVERY_RESERVE:
     {
       OpRequestRef op = op_tracker.create_request<OpRequest, Message*>(m);
       // no map?  starting up?
