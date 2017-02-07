5ac7c2f972e1e75d975ac95e9fa8ae13ef2f43f4
Loic Dachary <ldachary@redhat.com>
2017-02-07 15:23:55 +0100

    connection: key_filename is a string, not an array
    Signed-off-by: Loic Dachary <ldachary@redhat.com>

--- teuthology/orchestra/connection.py
+++ teuthology/orchestra/connection.py
@@ -89,8 +89,7 @@ def connect(user_at_host, host_key=None, keep_alive=False, timeout=60,
             key_filename = opts['identityfile']
 
     if key_filename:
-        key_filename = [os.path.expanduser(f) for f in key_filename]
-        connect_args['key_filename'] = key_filename
+        connect_args['key_filename'] = os.path.expanduser(key_filename)
 
     log.debug(connect_args)
 
