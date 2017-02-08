e5e8eb962db6187ea19b96ba29ac83469c90b4ea
David Zafman <dzafman@redhat.com>
2017-02-08 10:02:40 -0800

    tools: Check for overlaps in internal "complete" table for DBObjectMap
    Changed check to return an error count and fix tool error message
    
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/os/filestore/DBObjectMap.h
+++ src/os/filestore/DBObjectMap.h
@@ -213,7 +213,7 @@ public:
   int upgrade_to_v2();
 
   /// Consistency check, debug, there must be no parallel writes
-  bool check(std::ostream &out) override;
+  int check(std::ostream &out) override;
 
   /// Ensure that all previous operations are durable
   int sync(const ghobject_t *oid=0, const SequencerPosition *spos=0) override;
