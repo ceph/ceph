1d6c72fe9a59561fb8f33889895b6708342b2856
Matt Benjamin <mbenjamin@redhat.com>
2017-02-10 17:14:16 -0500

    rgw_file: add pretty-print for RGWFileHandle
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit ef330f385d3407af5f470b5093145f59cc4dcc79)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_file.h
+++ src/rgw/rgw_file.h
@@ -636,6 +636,9 @@ namespace rgw {
 
     virtual ~RGWFileHandle();
 
+    friend std::ostream& operator<<(std::ostream &os,
+				    RGWFileHandle const &rgw_fh);
+
     class Factory : public cohort::lru::ObjectFactory
     {
     public:
