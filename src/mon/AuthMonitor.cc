7c6c3c753ccdd3baea834338e1a761f05b4e0a12
Vikhyat Umrao <vumrao@redhat.com>
2017-02-16 23:51:11 +0530

    auth: 'ceph auth import -i' overwrites caps, if caps are not specified in given keyring file, should alert user and should not allow this import. Because in 'ceph auth list' we keep all the keyrings with caps and importing 'client.admin' user keyring without caps locks the cluster with error[1] because admin keyring caps are missing in 'ceph auth'.
    [1] Error connecting to cluster: PermissionDeniedError
    
    Fixes: http://tracker.ceph.com/issues/18932
    
    Signed-off-by: Vikhyat Umrao <vumrao@redhat.com>
    (cherry picked from commit 90144aa64c11a685b6a7cb3aafea75d427f569be)

--- src/mon/AuthMonitor.cc
+++ src/mon/AuthMonitor.cc
@@ -652,11 +652,15 @@ void AuthMonitor::export_keyring(KeyRing& keyring)
   mon->key_server.export_keyring(keyring);
 }
 
-void AuthMonitor::import_keyring(KeyRing& keyring)
+int AuthMonitor::import_keyring(KeyRing& keyring)
 {
   for (map<EntityName, EntityAuth>::iterator p = keyring.get_keys().begin();
        p != keyring.get_keys().end();
        ++p) {
+    if (p->second.caps.empty()) {
+      dout(0) << "import: no caps supplied" << dendl;
+      return -EINVAL;
+    }
     KeyServerData::Incremental auth_inc;
     auth_inc.name = p->first;
     auth_inc.auth = p->second;
@@ -665,6 +669,7 @@ void AuthMonitor::import_keyring(KeyRing& keyring)
     dout(30) << "    " << auth_inc.auth << dendl;
     push_cephx_inc(auth_inc);
   }
+  return 0;
 }
 
 bool AuthMonitor::prepare_command(MonOpRequestRef op)
@@ -731,7 +736,13 @@ bool AuthMonitor::prepare_command(MonOpRequestRef op)
       rs = err;
       goto done;
     }
-    import_keyring(keyring);
+    err = import_keyring(keyring);
+    if (err < 0) {
+      ss << "auth import: no caps supplied";
+      getline(ss, rs);
+      mon->reply_command(op, -EINVAL, rs, get_last_committed());
+      return true;
+    }
     ss << "imported keyring";
     getline(ss, rs);
     err = 0;
