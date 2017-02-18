06b4f07041bce3cb1a0c6a3776d4976d603c31bf
Sage Weil <sage@redhat.com>
2017-02-18 16:13:53 -0500

    osd/OpRequest: add get_nonconst_req()
    (Soon we'll make get_req() const.)
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/OpRequest.h
+++ src/osd/OpRequest.h
@@ -111,6 +111,7 @@ public:
   epoch_t sent_epoch;
   bool hitset_inserted;
   Message *get_req() const { return request; }
+  Message *get_nonconst_req() { return request; }
 
   const char *state_string() const {
     switch(latest_flag_point) {
