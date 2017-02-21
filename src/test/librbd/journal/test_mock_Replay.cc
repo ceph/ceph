38e77899217eb58765ef4178cb9681e0a0f45b9d
Gui Hecheng <guihecheng@cmss.chinamobile.com>
2017-02-21 15:35:37 +0800

    test/librbd: handle writesame op in test_mock_Replay
    Based on pr: https://github.com/ceph/ceph/pull/10019.
    
    Signed-off-by: Gui Hecheng <guihecheng@cmss.chinamobile.com>
    Signed-off-by: Mingxin Liu <mingxin@xsky.com>

--- src/test/librbd/journal/test_mock_Replay.cc
+++ src/test/librbd/journal/test_mock_Replay.cc
@@ -50,6 +50,15 @@ struct ImageRequest<MockReplayImageCtx> {
     s_instance->aio_flush(c);
   }
 
+  MOCK_METHOD5(aio_writesame, void(AioCompletion *c, uint64_t off, uint64_t len,
+                                   const bufferlist &bl, int op_flags));
+  static void aio_writesame(MockReplayImageCtx *ictx, AioCompletion *c,
+                            uint64_t off, uint64_t len, bufferlist &&bl,
+                            int op_flags) {
+    assert(s_instance != nullptr);
+    s_instance->aio_writesame(c, off, len, bl, op_flags);
+  }
+
   ImageRequest() {
     s_instance = this;
   }
@@ -139,6 +148,14 @@ public:
                         uint64_t len, const char *data) {
     EXPECT_CALL(mock_io_image_request,
                 aio_write(_, io::Extents{{off, len}}, BufferlistEqual(data), _))
+                  .WillOnce(SaveArg<0>(aio_comp));
+  }
+
+  void expect_aio_writesame(MockIoImageRequest &mock_io_image_request,
+                            io::AioCompletion **aio_comp, uint64_t off,
+                            uint64_t len, const char *data) {
+    EXPECT_CALL(mock_io_image_request,
+                aio_writesame(_, off, len, BufferlistEqual(data), _))
                   .WillOnce(SaveArg<0>(aio_comp));
   }
 
@@ -385,6 +402,34 @@ TEST_F(TestMockJournalReplay, AioFlush) {
 
   ASSERT_EQ(0, when_shut_down(mock_journal_replay, false));
   ASSERT_EQ(0, on_ready.wait());
+}
+
+TEST_F(TestMockJournalReplay, AioWriteSame) {
+  REQUIRE_FEATURE(RBD_FEATURE_JOURNALING);
+
+  librbd::ImageCtx *ictx;
+  ASSERT_EQ(0, open_image(m_image_name, &ictx));
+
+  MockReplayImageCtx mock_image_ctx(*ictx);
+  MockJournalReplay mock_journal_replay(mock_image_ctx);
+  MockIoImageRequest mock_io_image_request;
+  expect_op_work_queue(mock_image_ctx);
+
+  InSequence seq;
+  io::AioCompletion *aio_comp;
+  C_SaferCond on_ready;
+  C_SaferCond on_safe;
+  expect_aio_writesame(mock_io_image_request, &aio_comp, 123, 456, "333");
+  when_process(mock_journal_replay,
+               EventEntry{AioWriteSameEvent(123, 456, to_bl("333"))},
+               &on_ready, &on_safe);
+
+  when_complete(mock_image_ctx, aio_comp, 0);
+  ASSERT_EQ(0, on_ready.wait());
+
+  expect_aio_flush(mock_image_ctx, mock_io_image_request, 0);
+  ASSERT_EQ(0, when_shut_down(mock_journal_replay, false));
+  ASSERT_EQ(0, on_safe.wait());
 }
 
 TEST_F(TestMockJournalReplay, IOError) {
