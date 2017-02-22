2be8903b41d401e152c7f6e0392fec7c3f4286b2
Yan, Zheng <zyan@redhat.com>
2017-02-22 15:38:06 +0800

    client: hold reference for newly updated snaprealm
    Client::update_snap_trace() may create new snaprealm, then update
    them. When Client::update_snap_trace() return, the newly created
    snaprealm get freed immediately. This is wrong because callers of
    Client::update_snap_trace() expects Client::get_snap_realm() return
    the updated snaprealm.
    
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

--- src/client/Client.h
+++ src/client/Client.h
@@ -443,8 +443,7 @@ protected:
   SnapRealm *get_snap_realm_maybe(inodeno_t r);
   void put_snap_realm(SnapRealm *realm);
   bool adjust_realm_parent(SnapRealm *realm, inodeno_t parent);
-  inodeno_t update_snap_trace(bufferlist& bl, bool must_flush=true);
-  inodeno_t _update_snap_trace(vector<SnapRealmInfo>& trace);
+  void update_snap_trace(bufferlist& bl, SnapRealm **realm_ret, bool must_flush=true);
   void invalidate_snaprealm_and_children(SnapRealm *realm);
 
   Inode *open_snapdir(Inode *diri);
