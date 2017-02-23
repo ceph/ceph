595c31f390fda700e6e346c208849d23c74bdf5f
Jing Wenjun <jingwenjun@cmss.chinamobile.com>
2017-02-24 04:45:04 +0800

    rgw: add the remove-x-delete feature to cancel swift object expiration
    In openstack swift, it also support the feature to cancel the object expiration,
    which could be found at last point in
    https://docs.openstack.org/user-guide/cli-swift-set-object-expiration.html. we
    can remove the object expiration by set 'X-Remove-Delete-At:'.
    
    This patch also could fix the bug that when we set the object expiration and
    then upload the same object to the container again. The previous object expiration
    also works, which is not compatible with the openstack swift.
    
    Fixes: http://tracker.ceph.com/issues/19074
    Signed-off-by: Jing Wenjun <jingwenjun@cmss.chinamobile.com>
    (cherry picked from commit 230429ebc4ac9b5840bb93c7e0eeb5edbb949106)

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -8250,6 +8250,8 @@ int RGWRados::Object::Delete::delete_obj()
       if (params.expiration_time != delete_at) {
         return -ERR_PRECONDITION_FAILED;
       }
+    } else {
+      return -ERR_PRECONDITION_FAILED;
     }
   }
 
