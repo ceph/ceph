af72bcfe41966a7a08eed855771c1f0eba1ca7fd
deepak-3386 <deepak.srinivasan@zohocorp.com>
2017-02-24 15:57:05 +0530

    bugfix

--- src/client/fs_action_scheduler.cc
+++ src/client/fs_action_scheduler.cc
@@ -12,7 +12,7 @@ char *unix_sock = "/var/run/ceph.sock";
 
 char* concat(const char *s1,const char *s2)
 {
-    char *result = malloc(strlen(s1)+strlen(s2)+1);//+1 for the zero-terminator
+    char *result = (char *)malloc(strlen(s1)+strlen(s2)+1);//+1 for the zero-terminator
     strcpy(result, s1);
     strcat(result, s2);
     return result;
