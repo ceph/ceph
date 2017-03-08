cf17ff2acb4db08ad2ff8a023acbabe4c08847ac
Orit Wasserman <owasserm@redhat.com>
2017-03-08 17:56:39 +0200

    rgw: add RGWReshard class
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -1731,3 +1731,6 @@ OPTION(internal_safe_to_start_threads, OPT_BOOL, false)
 OPTION(debug_deliberately_leak_memory, OPT_BOOL, false)
 
 OPTION(rgw_swift_custom_header, OPT_STR, "") // option to enable swift custom headers
+
+/* resharding tunables */
+OPTION(rgw_reshard_max_jobs, OPT_INT, 1024)
---
+++ src/rgw/rgw_reshard.h
@@ -0,0 +1,30 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef RGW_RESHARD_H
+#define RGW_RESHARD_H
+
+#include <vector>
+#include "include/rados/librados.hpp"
+#include "cls/rgw/cls_rgw_types.h"
+
+class CephContext;
+class RGWRados;
+
+class RGWReshard {
+    CephContext *cct;
+    RGWRados *store;
+    string lock_name;
+    int max_jobs;
+
+    int get_io_ctx(librados::IoCtx& io_ctx);
+  public:
+    RGWReshard(CephContext* cct, RGWRados* _store);
+    int add(cls_rgw_reshard_entry& entry);
+    int get_head(cls_rgw_reshard_entry& entry);
+    int get(cls_rgw_reshard_entry& entry);
+    int remove(cls_rgw_reshard_entry& entry);
+    int list(string& marker, uint32_t max, list<cls_rgw_reshard_entry>& entries, bool& is_truncated);
+};
+
+#endif
