25a68b8a9d31348460abd81634c51956df5c4ed9
Adam Kupczyk <akupczyk@mirantis.com>
2017-03-09 16:56:49 +0100

    Moved crypt_sanitize operator<< from namespace std to namespace rgw::crypt_sanitize.
    Signed-off-by: Adam Kupczyk <akupczyk@mirantis.com>

--- src/rgw/rgw_crypt_sanitize.h
+++ src/rgw/rgw_crypt_sanitize.h
@@ -9,7 +9,9 @@
 namespace rgw {
 namespace crypt_sanitize {
 
-
+/*
+ * Temporary container for suppressing printing if variable contains secret key.
+ */
 struct env {
   boost::string_ref name;
   boost::string_ref value;
@@ -18,6 +20,9 @@ struct env {
   : name(name), value(value) {}
 };
 
+/*
+ * Temporary container for suppressing printing if aws meta attributes contains secret key.
+ */
 struct x_meta_map {
   boost::string_ref name;
   boost::string_ref value;
@@ -25,6 +30,9 @@ struct x_meta_map {
   : name(name), value(value) {}
 };
 
+/*
+ * Temporary container for suppressing printing if s3_policy calculation variable contains secret key.
+ */
 struct s3_policy {
   boost::string_ref name;
   boost::string_ref value;
@@ -32,6 +40,9 @@ struct s3_policy {
   : name(name), value(value) {}
 };
 
+/*
+ * Temporary container for suppressing printing if auth string contains secret key.
+ */
 struct auth {
   const req_state* const s;
   boost::string_ref value;
@@ -39,20 +50,20 @@ struct auth {
   : s(s), value(value) {}
 };
 
+/*
+ * Temporary container for suppressing printing if log made from civetweb may contain secret key.
+ */
 struct log_content {
   const char* buf;
   log_content(const char* buf)
   : buf(buf) {}
 };
 
-}
-}
-
-namespace std {
 std::ostream& operator<<(std::ostream& out, const rgw::crypt_sanitize::env& e);
 std::ostream& operator<<(std::ostream& out, const rgw::crypt_sanitize::x_meta_map& x);
 std::ostream& operator<<(std::ostream& out, const rgw::crypt_sanitize::s3_policy& x);
 std::ostream& operator<<(std::ostream& out, const rgw::crypt_sanitize::auth& x);
 std::ostream& operator<<(std::ostream& out, const rgw::crypt_sanitize::log_content& x);
+}
 }
 #endif /* RGW_RGW_CRYPT_SANITIZE_H_ */
