44147a2ee535fa56e59798a6eb616e71813449e7
Haomai Wang <haomai@xsky.com>
2017-03-09 22:51:41 +0800

    msg/async/rdma: make sure no alive connection when Dispatcher exit
    Signed-off-by: Haomai Wang <haomai@xsky.com>

--- src/msg/async/rdma/RDMAStack.h
+++ src/msg/async/rdma/RDMAStack.h
@@ -73,6 +73,7 @@ class RDMADispatcher : public CephContext::ForkWatcher {
   EventCallbackRef async_handler;
   bool done = false;
   std::atomic<uint64_t> num_dead_queue_pair = {0};
+  std::atomic<uint64_t> num_qp_conn = {0};
   Mutex lock; // protect `qp_conns`, `dead_queue_pairs`
   // qp_num -> InfRcConnection
   // The main usage of `qp_conns` is looking up connection by qp_num,
