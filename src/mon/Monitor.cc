62e92e9f13e38dbd200767417328d8104a7b49b4
Greg Farnum <gfarnum@redhat.com>
2017-03-10 19:09:22 -0800

    mon: mgr: send a pgmap in reports
    This definitely isn't appropriate for long term but is a quick route to testing
    
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>

--- src/mon/Monitor.cc
+++ src/mon/Monitor.cc
@@ -2576,7 +2576,7 @@ void Monitor::get_cluster_status(stringstream &ss, Formatter *f)
     osdmon()->osdmap.print_summary(f, cout);
     f->close_section();
     f->open_object_section("pgmap");
-    pgmon()->pg_map.print_summary(f, NULL);
+    mgrmon()->pg_map.print_summary(f, NULL);
     f->close_section();
     f->open_object_section("fsmap");
     mdsmon()->get_fsmap().print_summary(f, NULL);
@@ -2620,7 +2620,7 @@ void Monitor::get_cluster_status(stringstream &ss, Formatter *f)
     osdmon()->osdmap.print_summary(NULL, ss);
 
     ss << "\n \n  data:\n";
-    pgmon()->pg_map.print_summary(NULL, &ss);
+    mgrmon()->pg_map.print_summary(NULL, &ss);
     ss << "\n ";
   }
 }
@@ -3086,10 +3086,10 @@ void Monitor::handle_command(MonOpRequestRef op)
       if (f)
         f->open_object_section("stats");
 
-      pgmon()->pg_map.dump_fs_stats(&ds, f.get(), verbose);
+      mgrmon()->pg_map.dump_fs_stats(&ds, f.get(), verbose);
       if (!f)
         ds << '\n';
-      pgmon()->pg_map.dump_pool_stats(osdmon()->osdmap, &ds, f.get(), verbose);
+      mgrmon()->pg_map.dump_pool_stats(osdmon()->osdmap, &ds, f.get(), verbose);
 
       if (f) {
         f->close_section();
