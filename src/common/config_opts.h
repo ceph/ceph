a6aa42f793e679272d4f4f2ed53c2b64366f5af6
Sage Weil <sage@redhat.com>
2017-03-13 11:59:07 -0400

    mon/OSDMonitor: add 'osd [rm-]pg-remap[-items] ...' commands
    Add commands to add and remove pg_remap mappings.
    
    Require that a mon config option is set before this is allowed so that
    users don't inadvertantly prevent older clients from interacting with the
    cluster.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -282,6 +282,7 @@ OPTION(mon_osd_max_op_age, OPT_DOUBLE, 32)     // max op age before we get conce
 OPTION(mon_osd_max_split_count, OPT_INT, 32) // largest number of PGs per "involved" OSD to let split create
 OPTION(mon_osd_allow_primary_temp, OPT_BOOL, false)  // allow primary_temp to be set in the osdmap
 OPTION(mon_osd_allow_primary_affinity, OPT_BOOL, false)  // allow primary_affinity to be set in the osdmap
+OPTION(mon_osd_allow_pg_remap, OPT_BOOL, false) // allow pg remap to be set in the osdmap
 OPTION(mon_osd_prime_pg_temp, OPT_BOOL, true)  // prime osdmap with pg mapping changes
 OPTION(mon_osd_prime_pg_temp_max_time, OPT_FLOAT, .5)  // max time to spend priming
 OPTION(mon_osd_prime_pg_temp_max_estimate, OPT_FLOAT, .25) // max estimate of pg total before we do all pgs in parallel
