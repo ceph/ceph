99c695828862397031136d18ccf461922db52d4d
Mykola Golub <mgolub@mirantis.com>
2017-03-15 10:47:00 +0100

    rbd-mirror A/A: proxy InstanceReplayer APIs via InstanceWatcher RPC
    Fixes: http://tracker.ceph.com/issues/18787
    Signed-off-by: Mykola Golub <mgolub@mirantis.com>

--- src/tools/rbd_mirror/InstanceWatcher.h
+++ src/tools/rbd_mirror/InstanceWatcher.h
@@ -4,20 +4,28 @@
 #ifndef CEPH_RBD_MIRROR_INSTANCE_WATCHER_H
 #define CEPH_RBD_MIRROR_INSTANCE_WATCHER_H
 
+#include <map>
+#include <set>
 #include <string>
 #include <vector>
-#include <boost/optional.hpp>
 
+#include "common/AsyncOpTracker.h"
 #include "librbd/Watcher.h"
 #include "librbd/managed_lock/Types.h"
+#include "tools/rbd_mirror/instance_watcher/Types.h"
 
 namespace librbd {
-  class ImageCtx;
-  template <typename> class ManagedLock;
+
+class ImageCtx;
+template <typename> class ManagedLock;
+
 }
 
 namespace rbd {
 namespace mirror {
+
+template <typename> class InstanceReplayer;
+template <typename> struct Threads;
 
 template <typename ImageCtxT = librbd::ImageCtx>
 class InstanceWatcher : protected librbd::Watcher {
@@ -25,22 +33,26 @@ public:
   static void get_instances(librados::IoCtx &io_ctx,
                             std::vector<std::string> *instance_ids,
                             Context *on_finish);
-  static void remove_instance(librados::IoCtx &io_ctx, ContextWQ *work_queue,
+  static void remove_instance(librados::IoCtx &io_ctx,
+                              ContextWQ *work_queue,
                               const std::string &instance_id,
                               Context *on_finish);
 
   static InstanceWatcher *create(
     librados::IoCtx &io_ctx, ContextWQ *work_queue,
-    const boost::optional<std::string> &id = boost::none) {
-    return new InstanceWatcher(io_ctx, work_queue, id);
-  }
+    InstanceReplayer<ImageCtxT> *instance_replayer);
   void destroy() {
     delete this;
   }
 
   InstanceWatcher(librados::IoCtx &io_ctx, ContextWQ *work_queue,
-                  const boost::optional<std::string> &id = boost::none);
+                  InstanceReplayer<ImageCtxT> *instance_replayer,
+                  const std::string &instance_id);
   ~InstanceWatcher() override;
+
+  inline std::string &get_instance_id() {
+    return m_instance_id;
+  }
 
   int init();
   void shut_down();
@@ -49,9 +61,18 @@ public:
   void shut_down(Context *on_finish);
   void remove(Context *on_finish);
 
-protected:
-  void handle_notify(uint64_t notify_id, uint64_t handle, uint64_t notifier_id,
-                     bufferlist &bl) override;
+  void notify_image_acquire(const std::string &instance_id,
+                            const std::string &global_image_id,
+                            const std::string &peer_mirror_uuid,
+                            const std::string &peer_image_id,
+                            Context *on_notify_ack);
+  void notify_image_release(const std::string &instance_id,
+                            const std::string &global_image_id,
+                            const std::string &peer_mirror_uuid,
+                            const std::string &peer_image_id,
+			    bool schedule_delete, Context *on_notify_ack);
+
+  void cancel_notify_requests(const std::string &instance_id);
 
 private:
   /**
@@ -63,8 +84,8 @@ private:
    *       GET_INSTANCE_LOCKER  * * *>|
    *          ^ (remove)              |
    *          |                       |
-   * <uninitialized> <----------------+--------\
-   *    | (init)         ^            |        |
+   * <uninitialized> <----------------+---- WAIT_FOR_NOTIFY_OPS
+   *    | (init)         ^            |        ^
    *    v        (error) *            |        |
    * REGISTER_INSTANCE * *     * * * *|* *> UNREGISTER_INSTANCE
    *    |                      *      |        ^
@@ -83,7 +104,43 @@ private:
    * @endverbatim
    */
 
-  bool m_owner;
+  struct C_NotifyInstanceRequest;
+
+  struct HandlePayloadVisitor : public boost::static_visitor<void> {
+    InstanceWatcher *instance_watcher;
+    std::string instance_id;
+    C_NotifyAck *on_notify_ack;
+
+    HandlePayloadVisitor(InstanceWatcher *instance_watcher,
+                         const std::string &instance_id,
+                         C_NotifyAck *on_notify_ack)
+      : instance_watcher(instance_watcher), instance_id(instance_id),
+        on_notify_ack(on_notify_ack) {
+    }
+
+    template <typename Payload>
+    inline void operator()(const Payload &payload) const {
+      instance_watcher->handle_payload(instance_id, payload, on_notify_ack);
+    }
+  };
+
+  struct Request {
+    std::string instance_id;
+    uint64_t request_id;
+    C_NotifyAck *on_notify_ack = nullptr;
+
+    Request(const std::string &instance_id, uint64_t request_id)
+      : instance_id(instance_id), request_id(request_id) {
+    }
+
+    inline bool operator<(const Request &rhs) const {
+      return instance_id < rhs.instance_id ||
+        (instance_id == rhs.instance_id && request_id < rhs.request_id);
+    }
+  };
+
+  Threads<ImageCtxT> *m_threads;
+  InstanceReplayer<ImageCtxT> *m_instance_replayer;
   std::string m_instance_id;
 
   mutable Mutex m_lock;
@@ -92,6 +149,10 @@ private:
   int m_ret_val = 0;
   bool m_removing = false;
   librbd::managed_lock::Locker m_instance_locker;
+  std::set<std::pair<std::string, C_NotifyInstanceRequest *>> m_notify_ops;
+  AsyncOpTracker m_notify_op_tracker;
+  uint64_t m_request_seq = 0;
+  std::set<Request> m_requests;
 
   void register_instance();
   void handle_register_instance(int r);
@@ -117,11 +178,39 @@ private:
   void unregister_instance();
   void handle_unregister_instance(int r);
 
+  void wait_for_notify_ops();
+  void handle_wait_for_notify_ops(int r);
+
   void get_instance_locker();
   void handle_get_instance_locker(int r);
 
   void break_instance_lock();
   void handle_break_instance_lock(int r);
+
+  Context *prepare_request(const std::string &instance_id, uint64_t request_id,
+                           C_NotifyAck *on_notify_ack);
+
+  void handle_notify(uint64_t notify_id, uint64_t handle,
+                     uint64_t notifier_id, bufferlist &bl) override;
+
+  void handle_image_acquire(const std::string &global_image_id,
+                            const std::string &peer_mirror_uuid,
+                            const std::string &peer_image_id,
+                            Context *on_finish);
+  void handle_image_release(const std::string &global_image_id,
+                            const std::string &peer_mirror_uuid,
+                            const std::string &peer_image_id,
+                            bool schedule_delete, Context *on_finish);
+
+  void handle_payload(const std::string &instance_id,
+                      const instance_watcher::ImageAcquirePayload &payload,
+                      C_NotifyAck *on_notify_ack);
+  void handle_payload(const std::string &instance_id,
+                      const instance_watcher::ImageReleasePayload &payload,
+                      C_NotifyAck *on_notify_ack);
+  void handle_payload(const std::string &instance_id,
+                      const instance_watcher::UnknownPayload &payload,
+                      C_NotifyAck *on_notify_ack);
 };
 
 } // namespace mirror
