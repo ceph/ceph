58b39cd1dddfada64593d66b307f2d2441a2731b
Adam Kupczyk <akupczyk@mirantis.com>
2017-03-24 19:31:03 +0100

    common, arch, intel: added recognition of AESNI features
    Signed-off-by: Adam Kupczyk <akupczyk@mirantis.com>

--- src/arch/intel.h
+++ src/arch/intel.h
@@ -11,6 +11,8 @@ extern int ceph_arch_intel_sse41;  /* true if we have sse 4.1 features */
 extern int ceph_arch_intel_ssse3;  /* true if we have ssse 3 features */
 extern int ceph_arch_intel_sse3;   /* true if we have sse 3 features */
 extern int ceph_arch_intel_sse2;   /* true if we have sse 2 features */
+extern int ceph_arch_intel_aesni;  /* true if we have aesni features */
+
 extern int ceph_arch_intel_probe(void);
 
 #ifdef __cplusplus
