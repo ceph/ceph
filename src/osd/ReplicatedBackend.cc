138a808afa7804b75da76dbde6ed2a98d9140d4b
Sage Weil <sage@redhat.com>
2017-03-28 16:07:29 -0400

    osd/ReplicatedBackend: set recovry_info.ss on non-head push
    Replicas need this in order to store the clones in SnapMapper.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/ReplicatedBackend.cc
+++ src/osd/ReplicatedBackend.cc
@@ -1497,6 +1497,7 @@ void ReplicatedBackend::prep_push_to_replica(
     SnapSetContext *ssc = obc->ssc;
     assert(ssc);
     dout(15) << "push_to_replica snapset is " << ssc->snapset << dendl;
+    pop->recovery_info.ss = ssc->snapset;
     map<pg_shard_t, pg_missing_t>::const_iterator pm =
       get_parent()->get_shard_missing().find(peer);
     assert(pm != get_parent()->get_shard_missing().end());
@@ -1568,6 +1569,7 @@ void ReplicatedBackend::prep_push(
   pi.recovery_info.clone_subset = clone_subsets;
   pi.recovery_info.soid = soid;
   pi.recovery_info.oi = obc->obs.oi;
+  pi.recovery_info.ss = pop->recovery_info.ss;
   pi.recovery_info.version = version;
   pi.lock_manager = std::move(lock_manager);
 
