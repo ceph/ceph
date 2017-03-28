d026a23d1f313ddf4897ab744cadb0dd57e50cff
Yehuda Sadeh <yehuda@redhat.com>
2017-03-28 15:43:52 -0700

    rgw: initial support for multiple types in es requests
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_es_query.h
+++ src/rgw/rgw_es_query.h
@@ -62,17 +62,43 @@ public:
 
 class ESQueryNode;
 
+struct ESEntityTypeMap {
+  enum EntityType {
+    ES_ENTITY_NONE,
+    ES_ENTITY_STR,
+    ES_ENTITY_INT,
+    ES_ENTITY_DATE,
+  };
+
+  map<string, EntityType> m;
+
+  ESEntityTypeMap(map<string, EntityType>& _m) : m(_m) {}
+
+  EntityType find(const string& entity) {
+    auto i = m.find(entity);
+    if (i != m.end()) {
+      return i->second;
+    }
+
+    return ES_ENTITY_NONE;
+  }
+};
+
 class ESQueryCompiler {
   ESInfixQueryParser parser;
   ESQueryStack stack;
   ESQueryNode *query_root{nullptr};
 
+  string custom_prefix;
+
   bool convert(list<string>& infix);
 
   list<pair<string, string> > eq_conds;
 
+  ESEntityTypeMap *custom_type_map{nullptr};
+
 public:
-  ESQueryCompiler(const string& query, list<pair<string, string> > *prepend_eq_conds) : parser(query) {
+  ESQueryCompiler(const string& query, list<pair<string, string> > *prepend_eq_conds, const string& _custom_prefix) : parser(query), custom_prefix(_custom_prefix) {
     if (prepend_eq_conds) {
       eq_conds = std::move(*prepend_eq_conds);
     }
@@ -81,6 +107,16 @@ public:
 
   bool compile();
   void dump(Formatter *f) const;
+  
+  const string& get_custom_prefix() { return custom_prefix; }
+
+  void set_custom_type_map(ESEntityTypeMap *entity_map) {
+    custom_type_map = entity_map;
+  }
+
+  ESEntityTypeMap *get_custom_type_map() {
+    return custom_type_map;
+  }
 };
 
 
