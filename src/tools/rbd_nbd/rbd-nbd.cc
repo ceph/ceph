ceph/ceph

commit ff4dcf029028e8a3636ac71a6c5ac5380bf274e4

rbd-nbd: polish the output info before and after ioctl NBD_DISCONNECT.
Signed-off-by: Pan Liu <liupan1111@gmail.com>

src/tools/rbd_nbd/rbd-nbd.cc
@@ -96,7 +96,12 @@ static void handle_signal(int signum)
 {
   assert(signum == SIGINT || signum == SIGTERM);
   derr << "*** Got signal " << sig_str(signum) << " ***" << dendl;
-  ioctl(nbd, NBD_DISCONNECT);
+  dout(20) << __func__ << ": " << "sending NBD_DISCONNECT" << dendl;
+  if (ioctl(nbd, NBD_DISCONNECT) < 0) {
+    derr << "rbd-nbd: disconnect failed: " << cpp_error(errno) << std::endl;
+  } else {
+    dout(20) << __func__ << ": " << "disconnected" << dendl;
+  }
 }
 
 class NBDServer
@@ -759,10 +764,13 @@ static int do_unmap()
     return nbd;
   }
 
-  // alert the reader thread to shut down
+  dout(20) << __func__ << ": " << "sending NBD_DISCONNECT" << dendl;
   if (ioctl(nbd, NBD_DISCONNECT) < 0) {
     cerr << "rbd-nbd: the device is not used" << std::endl;
+  } else {
+    dout(20) << __func__ << ": " << "disconnected" << dendl;
   }
+
   close(nbd);
 
   return 0;

commit c12ecd984df0f9ac0d7de81b9d689f6182a3646a

rbd-nbd: s/cpp_error/cpp_strerror/ to fix FTBFS
the build failure was introduced by ff4dcf0

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/tools/rbd_nbd/rbd-nbd.cc
@@ -26,7 +26,6 @@
 #include <string.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <assert.h>
 
 #include <linux/nbd.h>
 #include <linux/fs.h>
@@ -98,7 +97,7 @@ static void handle_signal(int signum)
   derr << "*** Got signal " << sig_str(signum) << " ***" << dendl;
   dout(20) << __func__ << ": " << "sending NBD_DISCONNECT" << dendl;
   if (ioctl(nbd, NBD_DISCONNECT) < 0) {
-    derr << "rbd-nbd: disconnect failed: " << cpp_error(errno) << std::endl;
+    derr << "rbd-nbd: disconnect failed: " << cpp_strerror(errno) << dendl;
   } else {
     dout(20) << __func__ << ": " << "disconnected" << dendl;
   }