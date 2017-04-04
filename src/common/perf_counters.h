d16ad589fd3ef9039188af4d3be993e5acdfb821
Sage Weil <sage@redhat.com>
2017-04-04 17:33:17 -0400

    common/perf_counters: replace suppress_nicks with prio adjustment
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/common/perf_counters.h
+++ src/common/perf_counters.h
@@ -180,8 +180,9 @@ public:
     m_name = s;
   }
 
-  void set_suppress_nicks(bool b) {
-    suppress_nicks = b;
+  /// adjust priority values by some value
+  void set_prio_adjust(int p) {
+    prio_adjust = p;
   }
 
 private:
@@ -200,7 +201,7 @@ private:
   std::string m_name;
   const std::string m_lock_name;
 
-  bool suppress_nicks = false;
+  int prio_adjust = 0;
 
   /** Protects m_data */
   mutable Mutex m_lock;
