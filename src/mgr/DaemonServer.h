c85554f86cf2e081b84175509e1b3d185e7e8adc
Kefu Chai <kchai@redhat.com>
2017-04-08 18:17:40 +0800

    mgr/DaemonServer: extract reply out of handle_command()
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/mgr/DaemonServer.h
+++ src/mgr/DaemonServer.h
@@ -73,6 +73,10 @@ protected:
     const map<string,string>& param_str_map,
     const MgrCommand *this_cmd);
 
+private:
+  bool _reply(MCommand* m,
+	      int ret, const std::string& s, const bufferlist& payload);
+
 public:
   int init(uint64_t gid, entity_addr_t client_addr);
   void shutdown();
