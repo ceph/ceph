aa3beea9104cb955a916c8b88e6e0464e5564f1c
Mykola Golub <mgolub@mirantis.com>
2017-04-11 11:55:59 +0200

    test/rbd_mirror: race in TestMockInstanceWatcher on destroy
    Signed-off-by: Mykola Golub <mgolub@mirantis.com>

--- src/test/rbd_mirror/test_mock_InstanceWatcher.cc
+++ src/test/rbd_mirror/test_mock_InstanceWatcher.cc
@@ -115,8 +115,14 @@ public:
     EXPECT_CALL(mock_managed_lock, shut_down(_)).WillOnce(CompleteContext(r));
   }
 
-  void expect_destroy_lock(MockManagedLock &mock_managed_lock) {
-    EXPECT_CALL(mock_managed_lock, destroy());
+  void expect_destroy_lock(MockManagedLock &mock_managed_lock,
+                           Context *ctx = nullptr) {
+    EXPECT_CALL(mock_managed_lock, destroy())
+      .WillOnce(Invoke([ctx]() {
+            if (ctx != nullptr) {
+              ctx->complete(0);
+            }
+          }));
   }
 
   void expect_get_locker(MockManagedLock &mock_managed_lock,
@@ -218,12 +224,14 @@ TEST_F(TestMockInstanceWatcher, Remove) {
   expect_get_locker(mock_managed_lock, locker, 0);
   expect_break_lock(mock_managed_lock, locker, 0);
   expect_unregister_instance(mock_io_ctx, 0);
-  expect_destroy_lock(mock_managed_lock);
+  C_SaferCond on_destroy;
+  expect_destroy_lock(mock_managed_lock, &on_destroy);
 
   C_SaferCond on_remove;
   MockInstanceWatcher::remove_instance(m_local_io_ctx, m_threads->work_queue,
                                        "instance_id", &on_remove);
   ASSERT_EQ(0, on_remove.wait());
+  ASSERT_EQ(0, on_destroy.wait());
 }
 
 TEST_F(TestMockInstanceWatcher, RemoveNoent) {
@@ -234,12 +242,14 @@ TEST_F(TestMockInstanceWatcher, RemoveNoent) {
 
   expect_get_locker(mock_managed_lock, librbd::managed_lock::Locker(), -ENOENT);
   expect_unregister_instance(mock_io_ctx, 0);
-  expect_destroy_lock(mock_managed_lock);
+  C_SaferCond on_destroy;
+  expect_destroy_lock(mock_managed_lock, &on_destroy);
 
   C_SaferCond on_remove;
   MockInstanceWatcher::remove_instance(m_local_io_ctx, m_threads->work_queue,
                                        "instance_id", &on_remove);
   ASSERT_EQ(0, on_remove.wait());
+  ASSERT_EQ(0, on_destroy.wait());
 }
 
 } // namespace mirror
