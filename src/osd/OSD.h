c2efeb4b62e6dc4e6975561eb8b20cfca66f7237
David Zafman <dzafman@redhat.com>
2017-04-17 14:58:02 -0700

    osd: Give requested scrub work a higher priority
    Once started we now queue scrub work at higher priority than
    scheduled scrubs.
    
    Fixes: http://tracker.ceph.com/issues/15789
    
    Signed-off-by: David Zafman <dzafman@redhat.com>
    (cherry picked from commit ebab8b1f4f67fbdec1e147c580329c1e2b5cf7cd)
    
    Conflicts:
            src/osd/OSD.h - in jewel, the PGScrub() call is enclosed within
                op_wq.queue(make_pair(...)) instead of enqueue_back()

--- src/osd/OSD.h
+++ src/osd/OSD.h
@@ -873,7 +873,7 @@ public:
 	PGQueueable(
 	  PGScrub(pg->get_osdmap()->get_epoch()),
 	  cct->_conf->osd_scrub_cost,
-	  pg->get_scrub_priority(),
+	  pg->scrubber.priority,
 	  ceph_clock_now(cct),
 	  entity_inst_t())));
   }
