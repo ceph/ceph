fb79932be69545b286497d0ee24c96d5e466344b
Sage Weil <sage@redhat.com>
2017-04-18 13:54:56 -0400

    mon/PGMonitor: clean up min/max span warning
    Clean up option naming.
    
    Signed-off-by: Sage Weil <sage@redhat.com>
    (cherry picked from commit 489e810c37ed6fb9d32d1015634041a577501ee4)
    
    Conflicts:
    	src/common/config_opts.h: Jewel's options are quite different from ones of master,
    	  s/mon_warn_osd_usage_percent/mon_warn_osd_usage_min_max_delta/

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -274,6 +274,7 @@ OPTION(mon_warn_on_crush_straw_calc_version_zero, OPT_BOOL, true) // warn if cru
 OPTION(mon_warn_on_osd_down_out_interval_zero, OPT_BOOL, true) // warn if 'mon_osd_down_out_interval == 0'
 OPTION(mon_warn_on_cache_pools_without_hit_sets, OPT_BOOL, true)
 OPTION(mon_warn_osd_usage_percent, OPT_FLOAT, .40) // warn if difference in usage percent between OSDs exceeds specified percent
+OPTION(mon_warn_osd_usage_min_max_delta, OPT_FLOAT, .40) // warn if difference between min and max OSD utilizations exceeds specified amount
 OPTION(mon_min_osdmap_epochs, OPT_INT, 500)
 OPTION(mon_max_pgmap_epochs, OPT_INT, 500)
 OPTION(mon_max_log_epochs, OPT_INT, 500)
