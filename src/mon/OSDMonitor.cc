2e7e4ebc2ebf58617ef83bf8217d569465451c88
Kefu Chai <kchai@redhat.com>
2017-04-22 16:42:11 +0800

    mon/OSDMonitor: update pg_creatings even the new acting set is empty
    there is chance that the acting set of a PG being created becomes empty,
    and then OSDs joins the acting set. in that case, we need also update
    the "create" epoch of the creating_pgs, so the new primary can be
    updated with the MOSDPCreate message.
    
    Fixes: http://tracker.ceph.com/issues/19744
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -3184,9 +3184,6 @@ void OSDMonitor::update_creating_pgs()
     auto pgid = pg.first;
     auto& created = pg.second.first;
     mapping.get(pgid, nullptr, nullptr, nullptr, &acting_primary);
-    if (acting_primary < 0) {
-      continue;
-    }
     // check the previous creating_pgs, look for the target to whom the pg was
     // previously mapped
     for (const auto& pgs_by_epoch : creating_pgs_by_osd_epoch) {
