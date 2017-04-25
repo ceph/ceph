a5d73a18f42164bb05b5c13843ee290e5ca04e4a
Jing Wenjun <jingwenjun@cmss.chinamobile.com>
2017-04-25 21:44:01 +0800

    rgw: add support container and object levels of swift bulkupload
    Signed-off-by: Jing Wenjun <jingwenjun@cmss.chinamobile.com>

--- src/rgw/rgw_rest_swift.cc
+++ src/rgw/rgw_rest_swift.cc
@@ -2103,6 +2103,9 @@ RGWOp *RGWHandler_REST_Bucket_SWIFT::op_put()
   if (is_acl_op()) {
     return new RGWPutACLs_ObjStore_SWIFT;
   }
+  if(s->info.args.exists("extract-archive")) {
+    return new RGWBulkUploadOp_ObjStore_SWIFT;
+  }
   return new RGWCreateBucket_ObjStore_SWIFT;
 }
 
@@ -2147,6 +2150,9 @@ RGWOp *RGWHandler_REST_Obj_SWIFT::op_put()
 {
   if (is_acl_op()) {
     return new RGWPutACLs_ObjStore_SWIFT;
+  }
+  if(s->info.args.exists("extract-archive")) {
+    return new RGWBulkUploadOp_ObjStore_SWIFT;
   }
   if (s->init_state.src_bucket.empty())
     return new RGWPutObj_ObjStore_SWIFT;
