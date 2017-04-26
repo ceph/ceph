8df9910aed4c6b3fd50e48cd74328be5a4536103
Dongsheng Yang <dongsheng.yang@easystack.cn>
2017-04-26 11:19:12 +0800

    librbd: add namespace_add api
    Signed-off-by: Dongsheng Yang <dongsheng.yang@easystack.cn>

--- src/librbd/librbd.cc
+++ src/librbd/librbd.cc
@@ -617,6 +617,10 @@ namespace librbd {
     return r;
   }
 
+  int RBD::namespace_add(IoCtx& io_ctx, const string &nspace) {
+    return librbd::namespace_add(io_ctx, nspace);
+  }
+
   int RBD::namespace_list(IoCtx& io_ctx, set<string>& namespaces) {
     return librbd::namespace_list(io_ctx, namespaces);
   }
