e35d66c6bbf40c65922ef758432375655772df6b
David Zafman <dzafman@redhat.com>
2017-04-27 14:51:13 -0700

    osd: cleanup: Rename _failed_push to _failed_pull for ReplicatedBackend
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/osd/ReplicatedBackend.cc
+++ src/osd/ReplicatedBackend.cc
@@ -1723,7 +1723,7 @@ bool ReplicatedBackend::handle_pull_response(
 	   << dendl;
   if (pop.version == eversion_t()) {
     // replica doesn't have it!
-    _failed_push(from, pop.soid);
+    _failed_pull(from, pop.soid);
     return false;
   }
 
@@ -2183,8 +2183,9 @@ void ReplicatedBackend::trim_pushed_data(
   }
 }
 
-void ReplicatedBackend::_failed_push(pg_shard_t from, const hobject_t &soid)
+void ReplicatedBackend::_failed_pull(pg_shard_t from, const hobject_t &soid)
 {
+  dout(20) << __func__ << ": " << soid << " from " << from << dendl;
   list<pg_shard_t> fl = { from };
   get_parent()->failed_push(fl, soid);
 
