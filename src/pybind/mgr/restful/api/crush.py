23b9e8d90cc441b2927d4b31366c16986c7bbeab
Boris Ranto <branto@redhat.com>
2017-05-09 10:52:15 +0200

    restful: Split api.py file into a module
    The patch makes the REST api being served by a module, not a single
    monolithic file.
    
    Signed-off-by: Boris Ranto <branto@redhat.com>

---
+++ src/pybind/mgr/restful/api/crush.py
@@ -0,0 +1,50 @@
+from pecan import expose
+from pecan.rest import RestController
+
+from restful import common, module
+from collections import defaultdict
+
+from restful.decorators import auth, catch
+
+
+class CrushRuleset(RestController):
+    @expose(template='json')
+    @catch
+    @auth
+    def get(self, **kwargs):
+        """
+        Show crush rulesets
+        """
+        rules = module.instance.get('osd_map_crush')['rules']
+        nodes = module.instance.get('osd_map_tree')['nodes']
+
+        ruleset = defaultdict(list)
+        for rule in rules:
+            rule['osd_count'] = len(common.crush_rule_osds(nodes, rule))
+            ruleset[rule['ruleset']].append(rule)
+
+        return ruleset
+
+
+
+class CrushRule(RestController):
+    @expose(template='json')
+    @catch
+    @auth
+    def get(self, **kwargs):
+        """
+        Show crush rules
+        """
+        rules = module.instance.get('osd_map_crush')['rules']
+        nodes = module.instance.get('osd_map_tree')['nodes']
+
+        for rule in rules:
+            rule['osd_count'] = len(common.crush_rule_osds(nodes, rule))
+
+        return rules
+
+
+
+class Crush(RestController):
+    rule = CrushRule()
+    ruleset = CrushRuleset()
---
+++ src/pybind/mgr/restful/api/mon.py
@@ -0,0 +1,46 @@
+from pecan import expose, response
+from pecan.rest import RestController
+
+from restful import module
+from restful.decorators import auth, catch
+
+
+class MonName(RestController):
+    def __init__(self, name):
+        self.name = name
+
+
+    @expose(template='json')
+    @catch
+    @auth
+    def get(self, **kwargs):
+        """
+        Show the information for the monitor name
+        """
+        mon = filter(
+            lambda x: x['name'] == self.name,
+            module.instance.get_mons()
+        )
+
+        if len(mon) != 1:
+            response.status = 500
+            return {'message': 'Failed to identify the monitor node "%s"' % self.name}
+
+        return mon[0]
+
+
+
+class Mon(RestController):
+    @expose(template='json')
+    @catch
+    @auth
+    def get(self, **kwargs):
+        """
+        Show the information for all the monitors
+        """
+        return module.instance.get_mons()
+
+
+    @expose()
+    def _lookup(self, name, *remainder):
+        return MonName(name), remainder
