09b9410c2e69a466b001d92fc14eb44d768009f1
Yan, Zheng <zyan@redhat.com>
2017-05-10 08:13:52 +0800

    pybind: fix open flags calculation
    (O_WRONLY | O_RDWR) is invaild open flags
    
    Fixes: http://tracker.ceph.com/issues/19890
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>
    (cherry picked from commit 2c25c99cb4572ffae97555a56b24a4c4097dcdec)

--- src/pybind/cephfs/cephfs.pyx
+++ src/pybind/cephfs/cephfs.pyx
@@ -618,16 +618,26 @@ cdef class LibCephFS(object):
             if flags == '':
                 cephfs_flags = os.O_RDONLY
             else:
+                access_flags = 0;
                 for c in flags:
                     if c == 'r':
-                        cephfs_flags |= os.O_RDONLY
+                        access_flags = 1;
                     elif c == 'w':
-                        cephfs_flags |= os.O_WRONLY | os.O_TRUNC | os.O_CREAT
-                    elif c == '+':
-                        cephfs_flags |= os.O_RDWR
+                        access_flags = 2;
+                        cephfs_flags |= os.O_TRUNC | os.O_CREAT
+                    elif access_flags > 0 and c == '+':
+                        access_flags = 3;
                     else:
                         raise OperationNotSupported(
                             "open flags doesn't support %s" % c)
+
+                if access_flags == 1:
+                    cephfs_flags |= os.O_RDONLY;
+                elif access_flags == 2:
+                    cephfs_flags |= os.O_WRONLY;
+                else:
+                    cephfs_flags |= os.O_RDWR;
+
         elif isinstance(flags, int):
             cephfs_flags = flags
         else:
