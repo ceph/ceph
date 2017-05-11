6d6e9a06e884f9b0a6a4d1710f9735ffa0e6d456
Sage Weil <sage@redhat.com>
2017-05-11 18:33:53 -0400

    msg/simple: honor client request for lossy session
    If our normal policy is stateful/non-lossy but the connecting peer
    request lossy, provide them with a stateless_server-style policy.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/msg/simple/Pipe.cc
+++ src/msg/simple/Pipe.cc
@@ -456,6 +456,10 @@ int Pipe::accept()
     // note peer's type, flags
     set_peer_type(connect.host_type);
     policy = msgr->get_policy(connect.host_type);
+    if ((connect.flags & CEPH_MSG_CONNECT_LOSSY) && !policy.lossy) {
+      ldout(msgr->cct, 10) << __func__ << " client forcing lossy session" << dendl;
+      policy = Messenger::Policy::stateless_server(policy.features_required);
+    }
     ldout(msgr->cct,10) << "accept of host_type " << connect.host_type
 			<< ", policy.lossy=" << policy.lossy
 			<< " policy.server=" << policy.server
