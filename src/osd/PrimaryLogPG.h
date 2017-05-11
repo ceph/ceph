1f61874a9efc4be3b7728be1e76692983b9b1bbf
runsisi <runsisi@zte.com.cn>
2017-05-11 21:45:08 +0800

    osd/PrimaryLogPG: drop parameter count_bytes for write_update_size_and_usage
    Signed-off-by: runsisi <runsisi@zte.com.cn>

--- src/osd/PrimaryLogPG.h
+++ src/osd/PrimaryLogPG.h
@@ -1099,7 +1099,7 @@ protected:
 
   void write_update_size_and_usage(object_stat_sum_t& stats, object_info_t& oi,
 				   interval_set<uint64_t>& modified, uint64_t offset,
-				   uint64_t length, bool count_bytes,
+				   uint64_t length,
 				   bool force_changesize=false);
   void add_interval_usage(interval_set<uint64_t>& s, object_stat_sum_t& st);
 
