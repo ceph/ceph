aa6138624c03b14a8a844b4ec37a3fe26555caf1
Sage Weil <sage@redhat.com>
2017-05-12 17:40:00 -0400

    mon/OSDMonitor: add 'osd crush swap-bucket' command
    Swap contents of two crush buckets.  Do it safely by default so that
    the origin bucket has to be an orphan (unless forced).
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mon/MonCommands.h
+++ src/mon/MonCommands.h
@@ -512,6 +512,12 @@ COMMAND("osd crush move " \
 	"name=args,type=CephString,n=N,goodchars=[A-Za-z0-9-_.=]", \
 	"move existing entry for <name> to location <args>", \
 	"osd", "rw", "cli,rest")
+COMMAND("osd crush swap-bucket " \
+	"name=source,type=CephString,goodchars=[A-Za-z0-9-_.] " \
+	"name=dest,type=CephString,goodchars=[A-Za-z0-9-_.] " \
+	"name=force,type=CephChoices,strings=--yes-i-really-mean-it,req=false", \
+	"swap existing bucket contents from (orphan) bucket <source> and <target>", \
+	"osd", "rw", "cli,rest")
 COMMAND("osd crush link " \
 	"name=name,type=CephString " \
 	"name=args,type=CephString,n=N,goodchars=[A-Za-z0-9-_.=]", \
