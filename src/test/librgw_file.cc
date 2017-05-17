7b0ad7433ea1ad5b20d45fb73877ecb03dbb5e5d
Gui Hecheng <guihecheng@cmss.chinamobile.com>
2017-05-17 11:15:04 +0800

    rgw_file/test: librgw_file cleanups
    - use userid instead of uid, to stay consistent with other tests
    
    Signed-off-by: Gui Hecheng <guihecheng@cmss.chinamobile.com>

--- src/test/librgw_file.cc
+++ src/test/librgw_file.cc
@@ -28,7 +28,7 @@
 
 namespace {
   librgw_t rgw = nullptr;
-  string uid("testuser");
+  string userid("testuser");
   string access_key("");
   string secret_key("");
   struct rgw_fs *fs = nullptr;
@@ -53,7 +53,7 @@ TEST(LibRGW, INIT) {
 }
 
 TEST(LibRGW, MOUNT) {
-  int ret = rgw_mount2(rgw, uid.c_str(), access_key.c_str(), secret_key.c_str(),
+  int ret = rgw_mount2(rgw, userid.c_str(), access_key.c_str(), secret_key.c_str(),
                        "/", &fs, RGW_MOUNT_FLAG_NONE);
   ASSERT_EQ(ret, 0);
   ASSERT_NE(fs, nullptr);
@@ -264,9 +264,9 @@ int main(int argc, char *argv[])
     } else if (ceph_argparse_witharg(args, arg_iter, &val, "--secret",
 				     (char*) nullptr)) {
       secret_key = val;
-    } else if (ceph_argparse_witharg(args, arg_iter, &val, "--uid",
+    } else if (ceph_argparse_witharg(args, arg_iter, &val, "--userid",
 				     (char*) nullptr)) {
-      uid = val;
+      userid = val;
     } else if (ceph_argparse_flag(args, arg_iter, "--getattr",
 					    (char*) nullptr)) {
       do_getattr = true;
