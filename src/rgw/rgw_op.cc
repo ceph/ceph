58f952f3640ace60e9ba1b3c8216d563bbff65a8
Orit Wasserman <owasserm@redhat.com>
2017-05-19 15:20:18 +0300

    rgw: check if buckets needs resharding on all ops that add new objects
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -3323,6 +3323,12 @@ void RGWPutObj::execute()
     goto done;
   }
 
+  op_ret = store->check_bucket_shards(s->bucket_info, s->bucket, bucket_quota);
+  if (op_ret < 0) {
+    ldout(s->cct, 20) << "check_bucket_shards() returned ret=" << op_ret << dendl;
+    goto done;
+  }
+
   hash.Final(m);
 
   if (compressor && compressor->is_compressed()) {
@@ -3492,6 +3498,11 @@ void RGWPostObj::execute()
       return;
     }
 
+    op_ret = store->check_bucket_shards(s->bucket_info, s->bucket, bucket_quota);
+    if (op_ret < 0) {
+      return;
+    }
+
     RGWPutObjProcessor_Atomic processor(*static_cast<RGWObjectCtx *>(s->obj_ctx),
                                         s->bucket_info,
                                         s->bucket,
@@ -3568,6 +3579,11 @@ void RGWPostObj::execute()
 
     op_ret = store->check_quota(s->bucket_owner.get_id(), s->bucket,
                                 user_quota, bucket_quota, s->obj_size);
+    if (op_ret < 0) {
+      return;
+    }
+
+    op_ret = store->check_bucket_shards(s->bucket_info, s->bucket, bucket_quota);
     if (op_ret < 0) {
       return;
     }
@@ -6010,6 +6026,11 @@ int RGWBulkUploadOp::handle_file(const boost::string_ref path,
     return op_ret;
   }
 
+  op_ret = store->check_bucket_shards(s->bucket_info, s->bucket, bucket_quota);
+  if (op_ret < 0) {
+    return op_ret;
+  }
+
   RGWPutObjProcessor_Atomic processor(obj_ctx,
                                       binfo,
                                       binfo.bucket,
@@ -6080,6 +6101,11 @@ int RGWBulkUploadOp::handle_file(const boost::string_ref path,
   if (op_ret < 0) {
     ldout(s->cct, 20) << "bulk upload: quota exceeded for path=" << path
                       << dendl;
+    return op_ret;
+  }
+
+  op_ret = store->check_bucket_shards(s->bucket_info, s->bucket, bucket_quota);
+  if (op_ret < 0) {
     return op_ret;
   }
 
