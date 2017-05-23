e9a183f015f3d0008481ba9e0bc17f0247fb5cd0
xie xingguo <xie.xingguo@zte.com.cn>
2017-05-23 09:53:55 +0800

    common/types: make numbers a bit nicer when displaying space usage
    Was:
    ----------------------------------------------------------------------------
    GLOBAL:
        SIZE       AVAIL      RAW USED     %RAW USED
        30911M     27050M        3861M         12.49
    POOLS:
        NAME                  ID     USED        %USED     MAX AVAIL     OBJECTS
        rbd                   0      101216k      1.10         8913M        1178
        cephfs_data_a         1            0         0         8913M           0
        cephfs_metadata_a     2          892         0         8913M          21
    ----------------------------------------------------------------------------
    
    Now:
    ----------------------------------------------------------------------------
    GLOBAL:
        SIZE      AVAIL     RAW USED     %RAW USED
        30.2G     26.4G        3.77G         12.50
    POOLS:
        NAME                  ID     USED      %USED     MAX AVAIL     OBJECTS
        rbd                   0      99.2M      1.10         8.70G        1180
        cephfs_data_a         1          0         0         8.70G           0
        cephfs_metadata_a     2        892         0         8.70G          21
    ----------------------------------------------------------------------------
    
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- qa/workunits/rbd/import_export.sh
+++ qa/workunits/rbd/import_export.sh
@@ -158,7 +158,7 @@ dd if=/dev/urandom bs=1M count=1 of=${TMPDIR}/sparse2; truncate ${TMPDIR}/sparse
 # 1M sparse, 1M data
 rbd rm sparse1 || true
 rbd import $RBD_CREATE_ARGS --order 20 ${TMPDIR}/sparse1
-rbd ls -l | grep sparse1 | grep -i '2048k'
+rbd ls -l | grep sparse1 | grep -i '2M'
 [ $tiered -eq 1 -o "$(objects sparse1)" = '1' ]
 
 # export, compare contents and on-disk size
@@ -170,7 +170,7 @@ rbd rm sparse1
 # 1M data, 1M sparse
 rbd rm sparse2 || true
 rbd import $RBD_CREATE_ARGS --order 20 ${TMPDIR}/sparse2
-rbd ls -l | grep sparse2 | grep -i '2048k'
+rbd ls -l | grep sparse2 | grep -i '2M'
 [ $tiered -eq 1 -o "$(objects sparse2)" = '0' ]
 rbd export sparse2 ${TMPDIR}/sparse2.out
 compare_files_and_ondisk_sizes ${TMPDIR}/sparse2 ${TMPDIR}/sparse2.out
@@ -181,7 +181,7 @@ rbd rm sparse2
 truncate ${TMPDIR}/sparse1 -s 10M
 # import from stdin just for fun, verify still sparse
 rbd import $RBD_CREATE_ARGS --order 20 - sparse1 < ${TMPDIR}/sparse1
-rbd ls -l | grep sparse1 | grep -i '10240k'
+rbd ls -l | grep sparse1 | grep -i '10M'
 [ $tiered -eq 1 -o "$(objects sparse1)" = '1' ]
 rbd export sparse1 ${TMPDIR}/sparse1.out
 compare_files_and_ondisk_sizes ${TMPDIR}/sparse1 ${TMPDIR}/sparse1.out
@@ -192,7 +192,7 @@ rbd rm sparse1
 dd if=/dev/urandom bs=2M count=1 of=${TMPDIR}/sparse2 oflag=append conv=notrunc
 # again from stding
 rbd import $RBD_CREATE_ARGS --order 20 - sparse2 < ${TMPDIR}/sparse2
-rbd ls -l | grep sparse2 | grep -i '4096k'
+rbd ls -l | grep sparse2 | grep -i '4M'
 [ $tiered -eq 1 -o "$(objects sparse2)" = '0 2 3' ]
 rbd export sparse2 ${TMPDIR}/sparse2.out
 compare_files_and_ondisk_sizes ${TMPDIR}/sparse2 ${TMPDIR}/sparse2.out
