ceph/ceph

commit af74dfb31f1f91b1243d3aeea8a1d31ffe293274

pybind/ceph_daemon.py: move _gettermsize() into Termsize
as the latter is the only consumer of _gettermsize(). and a little bit
refactor to improve the readability and be more pep8 compliant.

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/pybind/ceph_daemon.py
@@ -87,36 +87,37 @@ def admin_socket(asok_path, cmd, format=''):
     return ret
 
 
-def _gettermsize():
-    try:
-        rows, cols = struct.unpack('hhhh', ioctl(0, TIOCGWINSZ, 8*'\x00'))[0:2]
-    except IOError:
-        return 25, 80
-
-    return rows,cols
-
-
 class Termsize(object):
-
+    DEFAULT_SIZE = (25, 80)
     def __init__(self):
-        self.rows, self.cols = _gettermsize()
+        self.rows, self.cols = self._gettermsize()
         self.changed = False
 
+    def _gettermsize(self):
+        try:
+            fd = sys.stdin.fileno()
+            sz = struct.pack('hhhh', 0, 0, 0, 0)
+            rows, cols = struct.unpack('hhhh', ioctl(fd, TIOCGWINSZ, sz))[:2]
+            return rows, cols
+        except IOError:
+            return self.DEFAULT_SIZE
+
     def update(self):
-        rows, cols = _gettermsize()
-        self.changed = self.changed or (
-            (self.rows != rows) or (self.cols != cols)
-        )
+        rows, cols = self._gettermsize()
+        if not self.changed:
+            self.changed = (self.raw,self.col) != (rows, cols)
         self.rows, self.cols = rows, cols
 
     def reset_changed(self):
         self.changed = False
 
     def __str__(self):
-        return '%s(%dx%d, changed %s)' % (self.__class__, self.rows, self.cols, self.changed)
+        return '%s(%dx%d, changed %s)' % (self.__class__,
+                                          self.rows, self.cols, self.changed)
 
     def __repr__(self):
-        return 'Termsize(%d,%d,%s)' % (self.__class__, self.rows, self.cols, self.changed)
+        return 'Termsize(%d,%d,%s)' % (self.__class__,
+                                       self.rows, self.cols, self.changed)
 
 
 class DaemonWatcher(object):

commit 7fd5c17ad8eb96d44e168a6a1ee4f2822e4bf184

pybind/ceph_daemon.py: fix Termsize.update
it's a regression introduced by af74dfb

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/pybind/ceph_daemon.py
@@ -105,7 +105,7 @@ class Termsize(object):
     def update(self):
         rows, cols = self._gettermsize()
         if not self.changed:
-            self.changed = (self.raw,self.col) != (rows, cols)
+            self.changed = (self.rows, self.cols) != (rows, cols)
         self.rows, self.cols = rows, cols
 
     def reset_changed(self):