5e243c6537da8cb0be74cda99281d7b773fcbeb9
Greg Farnum <gfarnum@redhat.com>
2017-05-25 22:32:01 -0700

    osd: do not default-abort on leaked pg refs
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -3233,7 +3233,9 @@ int OSD::shutdown()
 #ifdef PG_DEBUG_REFS
 	p->second->dump_live_ids();
 #endif
-        ceph_abort();
+	if (cct->_conf->osd_shutdown_pgref_assert) {
+	  ceph_abort();
+	}
       }
       p->second->unlock();
       p->second->put("PGMap");
