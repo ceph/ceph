828c70b441a8b9a686e569a0c350bd054c49eb57
Kefu Chai <kchai@redhat.com>
2017-06-05 13:46:54 +0800

    radosstriper: use intrusive_ptr to manage lifecycle of MultiAioCompletionImpl
    this change also silence some cppcheck warnings: we fail to rlease
    references to MultiAioCompletionImpl in some of the error handling paths.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/libradosstriper/MultiAioCompletionImpl.cc
+++ src/libradosstriper/MultiAioCompletionImpl.cc
@@ -59,3 +59,13 @@ void libradosstriper::MultiAioCompletionImpl::finish_adding_requests()
     safe();
   lock.Unlock();
 }
+
+void intrusive_ptr_add_ref(libradosstriper::MultiAioCompletionImpl* ptr)
+{
+  ptr->get();
+}
+
+void intrusive_ptr_release(libradosstriper::MultiAioCompletionImpl* ptr)
+{
+  ptr->put();
+}
