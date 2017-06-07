6d5cf01144db74fb9b38bdf6350d95acf2cd2f11
Sage Weil <sage@redhat.com>
2017-06-06 23:33:45 -0400

    mon/OSDMonitor: report crush_version for get and set crush map
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -4201,7 +4201,7 @@ bool OSDMonitor::preprocess_command(MonOpRequestRef op)
       ss << "got osdmap epoch " << p->get_epoch();
     } else if (prefix == "osd getcrushmap") {
       p->crush->encode(rdata, mon->get_quorum_con_features());
-      ss << "got crush map from osdmap epoch " << p->get_epoch();
+      ss << p->get_crush_version();
     } else if (prefix == "osd ls-tree") {
       string bucket_name;
       cmd_getval(g_ceph_context, cmdmap, "name", bucket_name);
@@ -7273,7 +7273,7 @@ bool OSDMonitor::prepare_command_impl(MonOpRequestRef op,
     dout(10) << " result " << ess.str() << dendl;
 
     pending_inc.crush = data;
-    ss << "set crush map";
+    ss << osdmap.get_crush_version() + 1;
     goto update;
 
   } else if (prefix == "osd crush set-device-class") {
