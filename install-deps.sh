4c50dd545b3c3ae7e59e2b3e5e89eafff6146b8c
Willem Jan Withagen <wjw@digiware.nl>
2017-06-10 12:12:23 +0200

    core: Install Pecan for FreeBSD
    Signed-off-by: Willem Jan Withagen <wjw@digiware.nl>

--- install-deps.sh
+++ install-deps.sh
@@ -52,12 +52,16 @@ if [ x`uname`x = xFreeBSDx ]; then
         emulators/fuse \
         java/junit \
         lang/python27 \
+	devel/py-pip \
         devel/py-argparse \
         devel/py-nose \
         www/py-flask \
         www/fcgi \
         sysutils/flock \
         sysutils/fusefs-libs \
+
+	# Now use pip to install some extra python modules
+	pip install pecan
 
     exit
 else
