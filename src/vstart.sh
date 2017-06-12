1967350dc82dd419146ebbd30d7f22234d19127e
Adam Kupczyk <akupczyk@mirantis.com>
2017-06-12 14:47:49 +0200

    vstart: allow to start multiple radosgw on consecutive ports
    Signed-off-by: Adam Kupczyk <akupczyk@mirantis.com>

--- src/vstart.sh
+++ src/vstart.sh
@@ -447,6 +447,8 @@ EOF
         log file = $CEPH_OUT_DIR/\$name.\$pid.log
         admin socket = $CEPH_OUT_DIR/\$name.\$pid.asok
 
+[client.rgw]
+
 [mds]
 $DAEMONOPTS
 $CMDSDEBUG
@@ -544,6 +546,13 @@ start_mon() {
 			--cap mds 'allow *' \
 			--cap mgr 'allow *' \
 			"$keyring_fn"
+
+		prun $SUDO "$CEPH_BIN/ceph-authtool" --gen-key --name=client.rgw --set-uid=0 \
+		    --cap mon 'allow *' \
+		    --cap osd 'allow *' \
+		    --cap mds 'allow *' \
+		    --cap mgr 'allow *' \
+		    "$keyring_fn"
 
 		# build a fresh fs monmap, mon fs
 		local str=""
@@ -918,7 +927,7 @@ EOF
 }
 do_hitsets $hitset
 
-do_rgw()
+do_rgw_create_users()
 {
     # Create S3 user
     local akey='0555b35654ad1656d804'
@@ -963,9 +972,14 @@ do_rgw()
     echo "  user      : tester"
     echo "  password  : testing"
     echo ""
+}
 
+do_rgw()
+{
+    if [ "$new" -eq 1 ]; then
+	do_rgw_create_users
+    fi
     # Start server
-    echo start rgw on http://localhost:$CEPH_RGW_PORT
     RGWDEBUG=""
     if [ "$debug" -ne 0 ]; then
         RGWDEBUG="--debug-rgw=20"
@@ -977,7 +991,8 @@ do_rgw()
     i=0
     for rgw in j k l m n o p q r s t u v; do
 	ceph_adm auth get-or-create client.rgw.$rgw mon 'allow rw' osd 'allow rwx' mgr 'allow rw' -o $CEPH_DEV_DIR/rgw.$rgw.keyring
-	run 'rgw' $RGWSUDO $CEPH_BIN/radosgw -c $conf_fn --log-file=${CEPH_OUT_DIR}/rgw.$rgw.log ${RGWDEBUG} --debug-ms=1 -n client.rgw.$rgw -k $CEPH_DEV_DIR/rgw.$rgw.keyring
+	echo start rgw on http://localhost:$((CEPH_RGW_PORT + i))
+	run 'rgw' $RGWSUDO $CEPH_BIN/radosgw -c $conf_fn --log-file=${CEPH_OUT_DIR}/rgw.$rgw.log ${RGWDEBUG} --debug-ms=1 -n client.rgw "--rgw_frontends=${rgw_frontend} port=$((CEPH_RGW_PORT + i))"
 	i=$(($i + 1))
         [ $i -eq $CEPH_NUM_RGW ] && break
     done
