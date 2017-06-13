1d3e2dc35b553114d155bb1b7177e52b2e60d520
Sage Weil <sage@redhat.com>
2017-06-13 16:46:41 -0400

    osd/SnapMapper: debug keys inserted, removed, and iterated
    Hunting bug http://tracker.ceph.com/issues/19943
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/SnapMapper.cc
+++ src/osd/SnapMapper.cc
@@ -166,6 +166,9 @@ void SnapMapper::clear_snaps(
   assert(check(oid));
   set<string> to_remove;
   to_remove.insert(to_object_key(oid));
+  for (auto& i : to_remove) {
+    dout(20) << __func__ << " rm " << i << dendl;
+  }
   backend.remove_keys(to_remove, t);
 }
 
@@ -180,6 +183,9 @@ void SnapMapper::set_snaps(
   ::encode(in, bl);
   to_set[to_object_key(oid)] = bl;
   dout(20) << __func__ << " " << oid << " " << in.snaps << dendl;
+  for (auto& i : to_set) {
+    dout(20) << __func__ << " set " << i.first << dendl;
+  }
   backend.set_keys(to_set, t);
 }
 
@@ -214,6 +220,9 @@ int SnapMapper::update_snaps(
       to_remove.insert(to_raw_key(make_pair(*i, oid)));
     }
   }
+  for (auto& i : to_remove) {
+    dout(20) << __func__ << " rm " << i << dendl;
+  }
   backend.remove_keys(to_remove, t);
   return 0;
 }
@@ -239,6 +248,9 @@ void SnapMapper::add_oid(
        i != snaps.end();
        ++i) {
     to_add.insert(to_raw(make_pair(*i, oid)));
+  }
+  for (auto& i : to_add) {
+    dout(20) << __func__ << " set " << i.first << dendl;
   }
   backend.set_keys(to_add, t);
 }
@@ -270,6 +282,7 @@ int SnapMapper::get_next_objects_to_trim(
 
       assert(is_mapping(next.first));
 
+      dout(20) << __func__ << " " << next.first << dendl;
       pair<snapid_t, hobject_t> next_decoded(from_raw(next));
       assert(next_decoded.first == snap);
       assert(check(next_decoded.second));
@@ -312,6 +325,9 @@ int SnapMapper::_remove_oid(
        i != out.snaps.end();
        ++i) {
     to_remove.insert(to_raw_key(make_pair(*i, oid)));
+  }
+  for (auto& i : to_remove) {
+    dout(20) << __func__ << " rm " << i << dendl;
   }
   backend.remove_keys(to_remove, t);
   return 0;
