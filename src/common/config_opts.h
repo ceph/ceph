acbeb60980492763f4169e43c4a6f5b395557460
Sage Weil <sage@redhat.com>
2017-06-20 12:44:18 -0400

    mon/MgrMonitor: do not issue MGR_DOWN on new cluster
    It is normal for the initial cluster to lack a mgr.  Wait for some
    grace period before complaining about a missing mgr.
    
    Default to 30m.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/common/config_opts.h
+++ src/common/config_opts.h
@@ -1746,6 +1746,7 @@ OPTION(mgr_connect_retry_interval, OPT_DOUBLE, 1.0)
 OPTION(mon_mgr_digest_period, OPT_INT, 5)  // How frequently to send digests
 OPTION(mon_mgr_beacon_grace, OPT_INT, 30)  // How long to wait to failover
 OPTION(mon_mgr_inactive_grace, OPT_INT, 60) // How long before health WARN -> ERR
+OPTION(mon_mgr_mkfs_grace, OPT_INT, 60) // How long before we complain about MGR_DOWN
 OPTION(rgw_crypt_require_ssl, OPT_BOOL, true) // requests including encryption key headers must be sent over ssl
 OPTION(rgw_crypt_default_encryption_key, OPT_STR, "") // base64 encoded key for encryption of rgw objects
 OPTION(rgw_crypt_s3_kms_encryption_keys, OPT_STR, "") // extra keys that may be used for aws:kms
