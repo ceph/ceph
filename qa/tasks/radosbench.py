dfa95db57d2c73923918ecf7014c801a925ddfb7
Sage Weil <sage@redhat.com>
2017-06-23 11:12:01 -0400

    qa/tasks/radosbench: increase timeout
    The current timeout isn't enough in some cases (powercycle thrashing leaves
    osds down for a long time because rebooting is so slow).
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- qa/tasks/radosbench.py
+++ qa/tasks/radosbench.py
@@ -96,7 +96,7 @@ def task(ctx, config):
     try:
         yield
     finally:
-        timeout = config.get('time', 360) * 5 + 180
+        timeout = config.get('time', 360) * 10 + 180
         log.info('joining radosbench (timing out after %ss)', timeout)
         run.wait(radosbench.itervalues(), timeout=timeout)
 
