0564c13093da01f298d856d3a7e3f43d8184c887
Nathan Cutler <ncutler@suse.com>
2017-06-24 08:19:17 +0200

    Revert "build/ops: make sure which is installed in run-make-check.sh"
    This reverts downstream commit aa5a76856a4f18a83d1b07dd75e02caa3ecefbff
    which was for a bug that got fixed upstream in a different way.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- run-make-check.sh
+++ run-make-check.sh
@@ -51,7 +51,7 @@ function run() {
         exit 1
     fi
     if [ -n "$install_cmd" ]; then
-        $DRY_RUN sudo $install_cmd ccache jq which
+        $DRY_RUN sudo $install_cmd ccache jq
     else
         echo "WARNING: Don't know how to install packages" >&2
     fi
