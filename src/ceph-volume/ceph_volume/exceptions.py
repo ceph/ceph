a46a33b7df64475ee7011386d028d863a0d2a54f
Alfredo Deza <adeza@redhat.com>
2017-06-27 13:03:11 -0400

    ceph-volume: exceptions: create a MultipleLV error
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- src/ceph-volume/ceph_volume/exceptions.py
+++ src/ceph-volume/ceph_volume/exceptions.py
@@ -14,3 +14,14 @@ class SuperUserError(Exception):
 
     def __str__(self):
         return 'This command needs to be executed with sudo or as root'
+
+
+class MultipleLVsError(Exception):
+
+    def __init__(self, lv_name, lv_path):
+        self.lv_name = lv_name
+        self.lv_path = lv_path
+
+    def __str__(self):
+        msg = "Got more than 1 result looking for %s with path: %s" % (self.lv_name, self.lv_path)
+        return msg
