f42c4f78f64838c6495686d8770bf9a4ce14b0e8
Kefu Chai <kchai@redhat.com>
2017-06-28 13:18:09 +0800

    mon/MonClient: respect the priority in mon_info_t
    * common/dns_resolve: collect the priority in the SRV records
      also
    * mon/MonClient: only connect to the mon with highest priority
      (in context of SRV record, the lowest priority value), and prefer
      the mon with higher weight.
    
    Fixes: http://tracker.ceph.com/issues/5249
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/test/common/dns_resolve.cc
+++ src/test/common/dns_resolve.cc
@@ -135,25 +135,25 @@ TEST_F(DNSResolverTest, resolve_srv_hosts_empty_domain) {
 #endif
   }
 
-  map<string, entity_addr_t> addrs;
+  map<string, DNSResolver::Record> records;
   DNSResolver::get_instance(resolvH)->resolve_srv_hosts(g_ceph_context, "cephmon", 
-      DNSResolver::SRV_Protocol::TCP, &addrs);
+      DNSResolver::SRV_Protocol::TCP, &records);
 
-  ASSERT_EQ(addrs.size(), (unsigned int)3);
-  map<string,entity_addr_t>::iterator it = addrs.find("mon.a");
-  ASSERT_NE(it, addrs.end());
+  ASSERT_EQ(records.size(), (unsigned int)3);
+  auto it = records.find("mon.a");
+  ASSERT_NE(it, records.end());
   std::ostringstream os;
-  os << it->second;
+  os << it->second.addr;
   ASSERT_EQ(os.str(), "192.168.1.11:6789/0");
   os.str("");
-  it = addrs.find("mon.b");
-  ASSERT_NE(it, addrs.end());
-  os << it->second;
+  it = records.find("mon.b");
+  ASSERT_NE(it, records.end());
+  os << it->second.addr;
   ASSERT_EQ(os.str(), "192.168.1.12:6789/0");
   os.str("");
-  it = addrs.find("mon.c");
-  ASSERT_NE(it, addrs.end());
-  os << it->second;
+  it = records.find("mon.c");
+  ASSERT_NE(it, records.end());
+  os << it->second.addr;
   ASSERT_EQ(os.str(), "192.168.1.13:6789/0");
 }
 
@@ -205,25 +205,25 @@ TEST_F(DNSResolverTest, resolve_srv_hosts_full_domain) {
 #endif
   }
 
-  map<string, entity_addr_t> addrs;
+  map<string, DNSResolver::Record> records;
   DNSResolver::get_instance(resolvH)->resolve_srv_hosts(g_ceph_context, "cephmon", 
-      DNSResolver::SRV_Protocol::TCP, "ceph.com", &addrs);
+      DNSResolver::SRV_Protocol::TCP, "ceph.com", &records);
 
-  ASSERT_EQ(addrs.size(), (unsigned int)3);
-  map<string,entity_addr_t>::iterator it = addrs.find("mon.a");
-  ASSERT_NE(it, addrs.end());
+  ASSERT_EQ(records.size(), (unsigned int)3);
+  auto it = records.find("mon.a");
+  ASSERT_NE(it, records.end());
   std::ostringstream os;
-  os << it->second;
+  os << it->second.addr;
   ASSERT_EQ(os.str(), "192.168.1.11:6789/0");
   os.str("");
-  it = addrs.find("mon.b");
-  ASSERT_NE(it, addrs.end());
-  os << it->second;
+  it = records.find("mon.b");
+  ASSERT_NE(it, records.end());
+  os << it->second.addr;
   ASSERT_EQ(os.str(), "192.168.1.12:6789/0");
   os.str("");
-  it = addrs.find("mon.c");
-  ASSERT_NE(it, addrs.end());
-  os << it->second;
+  it = records.find("mon.c");
+  ASSERT_NE(it, records.end());
+  os << it->second.addr;
   ASSERT_EQ(os.str(), "192.168.1.13:6789/0");
 }
 
@@ -244,11 +244,11 @@ TEST_F(DNSResolverTest, resolve_srv_hosts_fail) {
 #endif
   }
 
-  map<string, entity_addr_t> addrs;
+  map<string, DNSResolver::Record> records;
   int ret = DNSResolver::get_instance(resolvH)->resolve_srv_hosts(
-      g_ceph_context, "noservice", DNSResolver::SRV_Protocol::TCP, "", &addrs);
+      g_ceph_context, "noservice", DNSResolver::SRV_Protocol::TCP, "", &records);
 
-  ASSERT_EQ(ret, 0);
-  ASSERT_EQ(addrs.size(), (unsigned int)0);
+  ASSERT_EQ(0, ret);
+  ASSERT_TRUE(records.empty());
 }
 
