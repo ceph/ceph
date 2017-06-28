21b557a55872f9f62c18e129813d5f6b000d0094
Josh Durgin <jdurgin@redhat.com>
2017-06-27 21:35:55 -0400

    osd/PGBackend: include min_epoch in RecoveryDelete messages
    This matches ordering with other recovery messages, and may speed up
    processing a bit.
    
    Signed-off-by: Josh Durgin <jdurgin@redhat.com>

--- src/messages/MOSDPGRecoveryDeleteReply.h
+++ src/messages/MOSDPGRecoveryDeleteReply.h
@@ -12,11 +12,14 @@ struct MOSDPGRecoveryDeleteReply : public MOSDFastDispatchOp {
 
   pg_shard_t from;
   spg_t pgid;
-  epoch_t map_epoch;
+  epoch_t map_epoch, min_epoch;
   list<pair<hobject_t, eversion_t> > objects;
 
   epoch_t get_map_epoch() const override {
     return map_epoch;
+  }
+  epoch_t get_min_epoch() const override {
+    return min_epoch;
   }
   spg_t get_spg() const override {
     return pgid;
@@ -30,6 +33,7 @@ struct MOSDPGRecoveryDeleteReply : public MOSDFastDispatchOp {
     bufferlist::iterator p = payload.begin();
     ::decode(pgid.pgid, p);
     ::decode(map_epoch, p);
+    ::decode(min_epoch, p);
     ::decode(objects, p);
     ::decode(pgid.shard, p);
     ::decode(from, p);
@@ -38,6 +42,7 @@ struct MOSDPGRecoveryDeleteReply : public MOSDFastDispatchOp {
   void encode_payload(uint64_t features) override {
     ::encode(pgid.pgid, payload);
     ::encode(map_epoch, payload);
+    ::encode(min_epoch, payload);
     ::encode(objects, payload);
     ::encode(pgid.shard, payload);
     ::encode(from, payload);
@@ -45,7 +50,7 @@ struct MOSDPGRecoveryDeleteReply : public MOSDFastDispatchOp {
 
   void print(ostream& out) const override {
     out << "MOSDPGRecoveryDeleteReply(" << pgid
-        << " e" << map_epoch << " " << objects << ")";
+        << " e" << map_epoch << "," << min_epoch << " " << objects << ")";
   }
 
   const char *get_type_name() const override { return "recovery_delete_reply"; }
