a4f4f88a257e99da9a91bed5ca42418cb101f271
Vasu Kulkarni <vasu@redhat.com>
2017-06-29 13:58:55 -0700

    Remove gzip as its done by pull_directory function
    Signed-off-by: Vasu Kulkarni <vasu@redhat.com>

--- teuthology/task/ceph_ansible.py
+++ teuthology/task/ceph_ansible.py
@@ -259,30 +259,6 @@ class CephAnsible(Task):
         ctx = self.ctx
         if ctx.archive is not None and \
                 not (ctx.config.get('archive-on-error') and ctx.summary['success']):
-            # collect logs
-            log.info('Compressing logs...')
-            run.wait(
-                ctx.cluster.run(
-                    args=[
-                        'sudo',
-                        'find',
-                        '/var/log/ceph',
-                        '-name',
-                        '*.log',
-                        '-print0',
-                        run.Raw('|'),
-                        'sudo',
-                        'xargs',
-                        '-0',
-                        '--no-run-if-empty',
-                        '--',
-                        'gzip',
-                        '--',
-                    ],
-                    check_status=False,
-                ),
-            )
-
             log.info('Archiving logs...')
             path = os.path.join(ctx.archive, 'remote')
             os.makedirs(path)
