227d28884696d8cc43e40432d73ce97ac556f61a
huanwen ren <rhwlyw@163.com>
2017-06-30 17:53:45 +0800

    MDS: tick_event repeated assignment
    "tick_event" will be assign in the MDSDaemon::tick(), so this place is unnecessary
    
    Signed-off-by: huanwen ren <ren.huanwen@zte.com.cn>

--- src/mds/MDSDaemon.cc
+++ src/mds/MDSDaemon.cc
@@ -78,8 +78,6 @@ public:
   explicit C_MDS_Tick(MDSDaemon *m) : mds_daemon(m) {}
   void finish(int r) override {
     assert(mds_daemon->mds_lock.is_locked_by_me());
-
-    mds_daemon->tick_event = 0;
     mds_daemon->tick();
   }
 };
