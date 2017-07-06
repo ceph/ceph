af0a6df270f3c469280cae877c177086ddf10245
Nathan Cutler <ncutler@suse.com>
2017-07-06 20:17:49 +0200

    rgw: lease_stack: use reset method instead of assignment
    It seems that the intent of 45877d38fd9a385b2f8b13e90be94d784898b0b3 was to
    change all instances of "lease_stack = ..." to "lease_stack.reset(...)", but
    this one was missed.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>
    (cherry picked from commit fc425afeb2e2a6ba3c98c612b3977aea619c9f73)
    
    Conflicts:
    	src/rgw/rgw_sync.cc (trivial)

--- src/rgw/rgw_sync.cc
+++ src/rgw/rgw_sync.cc
@@ -791,7 +791,7 @@ public:
                                                 sync_env->store->get_zone_params().log_pool,
                                                 sync_env->status_oid(), lock_name,
                                                 lock_duration, this));
-        lease_stack = spawn(lease_cr.get(), false);
+        lease_stack.reset(spawn(lease_cr.get(), false));
       }
       while (!lease_cr->is_locked()) {
         if (lease_cr->is_done()) {
