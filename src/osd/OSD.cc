f159a093ecab4aa53693ec106c00d5ecb383c467
Sage Weil <sage@redhat.com>
2017-07-20 13:07:32 -0400

    osd,mds,mgr: do not dereference null rotating_keys
    Immediately after we bind to a port, but before we have set up our
    auth infrastructure, we may get incoming connections.  Deny them.  Since
    we are not yet advertising ourselves these are peers trying to connect
    to old instances of daemons, not us.
    
    This triggers now because of bf4938567943c80345966f9c5a3bdc75a913175b.
    Previously, the peer would see we were a different addr and drop the
    connection.  Now, it continues.
    
    Fixes: http://tracker.ceph.com/issues/20667
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -6941,10 +6941,16 @@ bool OSD::ms_verify_authorizer(Connection *con, int peer_type,
   uint64_t global_id;
   uint64_t auid = CEPH_AUTH_UID_DEFAULT;
 
-  isvalid = authorize_handler->verify_authorizer(
-    cct, monc->rotating_secrets.get(),
-    authorizer_data, authorizer_reply, name, global_id, caps_info, session_key,
-    &auid);
+  RotatingKeyRing *keys = monc->rotating_secrets.get();
+  if (keys) {
+    isvalid = authorize_handler->verify_authorizer(
+      cct, keys,
+      authorizer_data, authorizer_reply, name, global_id, caps_info, session_key,
+      &auid);
+  } else {
+    dout(10) << __func__ << " no rotating_keys (yet), denied" << dendl;
+    isvalid = false;
+  }
 
   if (isvalid) {
     Session *s = static_cast<Session *>(con->get_priv());
