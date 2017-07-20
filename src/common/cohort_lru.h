5f838c05c72ad46ce01884a916f81275ef43dacd
Matt Benjamin <mbenjamin@redhat.com>
2017-07-20 08:18:22 -0400

    rgw_file: properly & |'d flags
    Found by "Supriti Singh" <Supriti.Singh@suse.com>.
    
    Fixes http://tracker.ceph.com/issues/20663
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/common/cohort_lru.h
+++ src/common/cohort_lru.h
@@ -388,7 +388,7 @@ namespace cohort {
 	  v = lat.p->cache[slot];
 	  if (v) {
 	    if (CEQ()(*v, k)) {
-	      if (flags & (FLAG_LOCK|FLAG_UNLOCK))
+	      if ((flags & FLAG_LOCK) && (flags & FLAG_UNLOCK))
 		lat.lock->unlock();
 	      return v;
 	    }
@@ -406,7 +406,7 @@ namespace cohort {
 	    lat.p->cache[slot] = v;
 	  }
 	}
-	if (flags & (FLAG_LOCK|FLAG_UNLOCK))
+	if ((flags & FLAG_LOCK) && (flags & FLAG_UNLOCK))
 	  lat.lock->unlock();
 	return v;
       } /* find_latch */
