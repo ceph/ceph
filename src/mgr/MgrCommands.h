396e2277d3287ac843899ac4e67f6d7fde2f2cfb
xie xingguo <xie.xingguo@zte.com.cn>
2017-07-20 16:00:46 +0800

    mgr: drop unnecessary write permission for test-reweight-by-* commands
    dry run does not ask for it
    
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- src/mgr/MgrCommands.h
+++ src/mgr/MgrCommands.h
@@ -82,7 +82,7 @@ COMMAND("osd test-reweight-by-utilization " \
 	"name=max_osds,type=CephInt,req=false "			\
 	"name=no_increasing,type=CephChoices,strings=--no-increasing,req=false",\
 	"dry run of reweight OSDs by utilization [overload-percentage-for-consideration, default 120]", \
-	"osd", "rw", "cli,rest")
+	"osd", "r", "cli,rest")
 COMMAND("osd reweight-by-pg " \
 	"name=oload,type=CephInt,req=false " \
 	"name=max_change,type=CephFloat,req=false "			\
@@ -96,7 +96,7 @@ COMMAND("osd test-reweight-by-pg " \
 	"name=max_osds,type=CephInt,req=false "			\
 	"name=pools,type=CephPoolname,n=N,req=false",			\
 	"dry run of reweight OSDs by PG distribution [overload-percentage-for-consideration, default 120]", \
-	"osd", "rw", "cli,rest")
+	"osd", "r", "cli,rest")
 
 COMMAND("osd scrub " \
 	"name=who,type=CephString", \
