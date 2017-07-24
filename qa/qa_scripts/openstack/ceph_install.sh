a0d3244ddf2742f8b8e20a71f2d8408217a0bcba
Alan Somers <asomers@gmail.com>
2017-07-24 17:33:02 -0600

    openstack: Fix shebangs on openstack scripts
    Many of the files in qa/qa_scripts/openstack had incorrect shebang
    lines: the bang was missing.  This means that those scripts would
    execute using the calling user's login shell, which is doubtless not
    what the author intended.  Now they'll always use bash.
    
    Two scripts do not need shebangs, because they contain only library
    functions and don't execute anything.  I removed their shebangs.
    
    Signed-off-by: Alan Somers <asomers@gmail.com>

--- qa/qa_scripts/openstack/ceph_install.sh
+++ qa/qa_scripts/openstack/ceph_install.sh
@@ -1,7 +1,8 @@
-#/bin/bash -fv
+#!/usr/bin/env bash
 #
 # Install a simple ceph cluster upon which openstack images will be stored.
 #
+set -fv
 ceph_node=${1}
 source copy_func.sh
 copy_file files/$OS_CEPH_ISO $ceph_node .
