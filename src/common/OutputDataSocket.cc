439aaf95150888da282e770d7af6221f1bd58ae4
Xinying Song <songxinying@cloudin.cn>
2017-07-25 17:48:04 +0800

    common: use std::move() for better performance
    use std::move() to avoid extra copy constructions in src/common/OutputDataSocket.cc.
    
    Signed-off-by: Xinying Song <songxinying@cloudin.cn>

--- src/common/OutputDataSocket.cc
+++ src/common/OutputDataSocket.cc
@@ -298,9 +298,7 @@ void OutputDataSocket::handle_connection(int fd)
 int OutputDataSocket::dump_data(int fd)
 {
   m_lock.Lock(); 
-  list<bufferlist> l;
-  l = data;
-  data.clear();
+  list<bufferlist> l = std::move(data);
   data_size = 0;
   m_lock.Unlock();
 
