e75465fdacd2fd137beab5af779e2d80d3e8f818
xie xingguo <xie.xingguo@zte.com.cn>
2017-07-25 19:23:34 +0800

    test/crush: kill dead class_is_in_use test
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- src/test/crush/CrushWrapper.cc
+++ src/test/crush/CrushWrapper.cc
@@ -1236,27 +1236,6 @@ TEST(CrushWrapper, populate_and_cleanup_classes) {
   ASSERT_FALSE(c.name_exists("default~ssd"));
 }
 
-TEST(CrushWrapper, class_is_in_use) {
-  CrushWrapper c;
-  c.create();
-  c.set_type_name(1, "root");
-
-  int weight = 1;
-  map<string,string> loc;
-  loc["root"] = "default";
-
-  ASSERT_FALSE(c.class_is_in_use(0));
-
-  int item = 1;
-  c.insert_item(g_ceph_context, item, weight, "osd.1", loc);
-  int class_id = c.get_or_create_class_id("ssd");
-  c.class_map[item] = class_id;
-
-  ASSERT_TRUE(c.class_is_in_use(c.get_class_id("ssd")));
-  ASSERT_EQ(0, c.remove_class_name("ssd"));
-  ASSERT_FALSE(c.class_is_in_use(c.get_class_id("ssd")));
-}
-
 TEST(CrushWrapper, remove_class_name) {
   CrushWrapper c;
   c.create();
