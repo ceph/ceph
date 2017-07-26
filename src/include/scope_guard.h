33a1da6eac1dbc338997c4950747cf5d0883820a
Adam C. Emerson <aemerson@redhat.com>
2017-07-26 18:09:27 -0400

    scope_guard: Support in-place construction and one-shots
    Kefu Chai just pointed out this exists so I just rolled the in-place variants of the
    'ward' function I'd written into it.
    
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/include/scope_guard.h
+++ src/include/scope_guard.h
@@ -17,6 +17,8 @@
 
 #include <utility>
 
+#include "common/backport14.h"
+
 template <typename F>
 struct scope_guard {
   F f;
@@ -25,15 +27,23 @@ struct scope_guard {
   scope_guard(scope_guard &&) = default;
   scope_guard & operator=(const scope_guard &) = delete;
   scope_guard & operator=(scope_guard &&) = default;
+  scope_guard(const F& f) : f(f) {}
   scope_guard(F &&f) : f(std::move(f)) {}
+  template<typename... Args>
+  scope_guard(ceph::in_place_t, Args&& ...args) : f(std::forward<Args>(args)...) {}
   ~scope_guard() {
-    f();
+    std::move(f)(); // Support at-most-once functions
   }
 };
 
 template <typename F>
 scope_guard<F> make_scope_guard(F &&f) {
   return scope_guard<F>(std::forward<F>(f));
+}
+
+template<typename F, typename... Args>
+scope_guard<F> make_scope_guard(ceph::in_place_type_t<F>, Args&& ...args) {
+  return { ceph::in_place, std::forward<Args>(args)... };
 }
 
 #endif
