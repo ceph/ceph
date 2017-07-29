419493adb866c4b606a0ab55acbe730f5b329bd8
xie xingguo <xie.xingguo@zte.com.cn>
2017-07-29 16:22:23 +0800

    crush: fix broken weight-set weight adjustment logic
    If osd_crush_update_weight_set is enabled, the crush item weight
    will always override the weight-set weights, which is aganist
    our original intention (of introducing weight-set).
    
    This patch now automatically adjusts weight-set only
    when osds are created or removed.
    
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- qa/standalone/crush/crush-choose-args.sh
+++ qa/standalone/crush/crush-choose-args.sh
@@ -61,8 +61,8 @@ choose_args 0 {
   {
     bucket_id -1
     weight_set [
-      [ 3.000 ]
-      [ 3.000 ]
+      [ 2.000 ]
+      [ 2.000 ]
     ]
     ids [ -10 ]
   }
@@ -91,67 +91,6 @@ EOF
     ceph osd getcrushmap > $dir/map-one-less || return 1
     crushtool -d $dir/map-one-less -o $dir/map-one-less.txt || return 1
     diff -u $dir/map-one-less.txt $dir/map.txt || return 1
-}
-
-function TEST_no_update_weight_set() {
-    #
-    # adding a zero weight OSD does not update the weight set at all
-    #
-    local dir=$1
-
-    ORIG_CEPH_ARGS="$CEPH_ARGS"
-    CEPH_ARGS+="--osd-crush-update-weight-set=false "
-
-    run_mon $dir a || return 1
-    run_osd $dir 0 || return 1
-
-    ceph osd set-require-min-compat-client luminous
-    ceph osd crush tree
-    ceph osd getcrushmap > $dir/map || return 1
-    crushtool -d $dir/map -o $dir/map.txt || return 1
-    sed -i -e '/end crush map/d' $dir/map.txt
-    cat >> $dir/map.txt <<EOF
-# choose_args
-choose_args 0 {
-  {
-    bucket_id -1
-    weight_set [
-      [ 2.000 ]
-      [ 1.000 ]
-    ]
-    ids [ -10 ]
-  }
-  {
-    bucket_id -2
-    weight_set [
-      [ 2.000 ]
-      [ 1.000 ]
-    ]
-    ids [ -20 ]
-  }
-}
-
-# end crush map
-EOF
-    crushtool -c $dir/map.txt -o $dir/map-new || return 1
-    ceph osd setcrushmap -i $dir/map-new || return 1
-    ceph osd crush tree
-
-
-    run_osd $dir 1 || return 1
-    ceph osd crush tree
-    ceph osd getcrushmap > $dir/map-one-more || return 1
-    crushtool -d $dir/map-one-more -o $dir/map-one-more.txt || return 1
-    cat $dir/map-one-more.txt
-    diff -u $dir/map-one-more.txt $CEPH_ROOT/src/test/crush/crush-choose-args-expected-one-more-0.txt || return 1
-
-    destroy_osd $dir 1 || return 1
-    ceph osd crush tree
-    ceph osd getcrushmap > $dir/map-one-less || return 1
-    crushtool -d $dir/map-one-less -o $dir/map-one-less.txt || return 1
-    diff -u $dir/map-one-less.txt $dir/map.txt || return 1
-
-    CEPH_ARGS="$ORIG_CEPH_ARGS"
 }
 
 main crush-choose-args "$@"
