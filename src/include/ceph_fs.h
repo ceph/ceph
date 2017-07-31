5bb80747ebcf920103e5bc419a3f5e5640b85de4
Douglas Fuller <dfuller@redhat.com>
2017-07-31 14:23:55 -0400

    cephfs: Kill allow_multimds
    With multi-mds now declared stable, allow_multimds now defaults to 1.
    Given the max_mds parameter, it is now redundant. Remove it, leaving a
    comment placeholder in the features bitmap.
    
    ceph fs set <fs> allow_multimds is now deprecated and prints a warning
    message.
    
    Signed-off-by: Douglas Fuller <dfuller@redhat.com>

--- src/include/ceph_fs.h
+++ src/include/ceph_fs.h
@@ -233,13 +233,13 @@ struct ceph_mon_subscribe_ack {
  */
 #define CEPH_MDSMAP_DOWN    (1<<0)  /* cluster deliberately down */
 #define CEPH_MDSMAP_ALLOW_SNAPS   (1<<1)  /* cluster allowed to create snapshots */
-#define CEPH_MDSMAP_ALLOW_MULTIMDS (1<<2) /* cluster allowed to have >1 active MDS */
+/* deprecated #define CEPH_MDSMAP_ALLOW_MULTIMDS (1<<2) cluster allowed to have >1 active MDS */
 #define CEPH_MDSMAP_ALLOW_DIRFRAGS (1<<3) /* cluster allowed to fragment directories */
 
-#define CEPH_MDSMAP_ALLOW_CLASSICS (CEPH_MDSMAP_ALLOW_SNAPS | CEPH_MDSMAP_ALLOW_MULTIMDS | \
+#define CEPH_MDSMAP_ALLOW_CLASSICS (CEPH_MDSMAP_ALLOW_SNAPS | \
 				    CEPH_MDSMAP_ALLOW_DIRFRAGS)
 
-#define CEPH_MDSMAP_DEFAULTS CEPH_MDSMAP_ALLOW_DIRFRAGS | CEPH_MDSMAP_ALLOW_MULTIMDS
+#define CEPH_MDSMAP_DEFAULTS CEPH_MDSMAP_ALLOW_DIRFRAGS
 
 /*
  * mds states
