bdc775fdd8acdad5c58ff3065a21396f80ce5db4
John Spray <john.spray@redhat.com>
2017-07-31 09:24:09 -0400

    mgr: apply a threshold to perf counter prios
    ...so that we can control the level of load
    we're putting on ceph-mgr with perf counters.  Don't collect
    anything below PRIO_USEFUL by default.
    
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/messages/MMgrConfigure.h
+++ src/messages/MMgrConfigure.h
@@ -23,25 +23,33 @@
  */
 class MMgrConfigure : public Message
 {
-  static const int HEAD_VERSION = 1;
+  static const int HEAD_VERSION = 2;
   static const int COMPAT_VERSION = 1;
 
 public:
   uint32_t stats_period;
+  
+  // Default 0 means if unspecified will include all stats
+  uint32_t stats_threshold = 0;
 
   void decode_payload() override
   {
     bufferlist::iterator p = payload.begin();
     ::decode(stats_period, p);
+    if (header.version >= 2) {
+      ::decode(stats_threshold, p);
+    }
   }
 
   void encode_payload(uint64_t features) override {
     ::encode(stats_period, payload);
+    ::encode(stats_threshold, payload);
   }
 
   const char *get_type_name() const override { return "mgrconfigure"; }
   void print(ostream& out) const override {
-    out << get_type_name() << "()";
+    out << get_type_name() << "(period=" << stats_period
+                           << ", threshold=" << stats_threshold << ")";
   }
 
   MMgrConfigure()
