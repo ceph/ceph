986e43af2659bad3e3f16c5394e7e99afc794e1f
Abhishek Lekshmanan <abhishek@suse.com>
2017-08-01 17:42:31 +0200

    rgw_lc: support for AWSv4 authentication
    adding support for AWSv4 authentication for Put Object LC, also adding
    types to all of the LC ops in the process
    
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>
    (cherry picked from commit cc51f32c22f294f59b369350e54b86892015cbab)

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -3821,6 +3821,7 @@ AWSGeneralAbstractor::get_auth_data_v4(const req_state* const s,
         case RGW_OP_SET_BUCKET_WEBSITE:
         case RGW_OP_PUT_BUCKET_POLICY:
         case RGW_OP_PUT_OBJ_TAGGING:
+        case RGW_OP_PUT_LC:
           break;
         default:
           dout(10) << "ERROR: AWS4 completion for this operation NOT IMPLEMENTED" << dendl;
