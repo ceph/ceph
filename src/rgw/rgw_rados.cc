5c45675fc719a8ec063fb50d47b9c133fc054021
Enming Zhang <enming.zhang@umcloud.com>
2017-08-03 00:16:55 +0800

    rgw: skip compress processing if synced object has been compressed
    To avoid to compress an object more than once, if the synced object
    has been compressed, we should skip the compress processing if the
    zone which need to put object has enabled compression function.
    
    Signed-off-by: Enming Zhang <enming.zhang@umcloud.com>

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -7290,7 +7290,8 @@ public:
       src_attrs.erase(RGW_ATTR_MANIFEST); // not interested in original object layout
     }
 
-    if (plugin && src_attrs.find(RGW_ATTR_CRYPT_MODE) == src_attrs.end()) {
+    if (plugin && src_attrs.find(RGW_ATTR_CRYPT_MODE) == src_attrs.end()
+               && src_attrs.find(RGW_ATTR_COMPRESSION) == src_attrs.end()) {
       //do not compress if object is encrypted
       compressor = boost::in_place(cct, plugin, filter);
       filter = &*compressor;
