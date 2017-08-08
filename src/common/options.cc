78ec64ea2ee141f1dbcd54923efab8a36ea12a36
Sage Weil <sage@redhat.com>
2017-08-08 12:14:07 -0400

    os/bluestore: default to bluestore_allocator = stupid
    Performance looks better, and code is simpler.
    
    Also fix config option annotations.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/common/options.cc
+++ src/common/options.cc
@@ -3396,10 +3396,10 @@ std::vector<Option> get_global_options() {
     .add_tag("mkfs")
     .set_description("Key value database to use for bluestore"),
 
-    Option("bluestore_allocator", Option::TYPE_STR, Option::LEVEL_DEV)
-    .set_default("bitmap")
-    .add_tag("mkfs")
-    .set_description(""),
+    Option("bluestore_allocator", Option::TYPE_STR, Option::LEVEL_ADVANCED)
+    .set_default("stupid")
+    .set_enum_allowed({"bitmap", "stupid"})
+    .set_description("Allocator policy"),
 
     Option("bluestore_freelist_blocks_per_key", Option::TYPE_INT, Option::LEVEL_DEV)
     .set_default(128)
