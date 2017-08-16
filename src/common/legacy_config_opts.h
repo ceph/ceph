d9a8b3e797252114c930117e6311f924ef0588b3
Patrick Donnelly <pdonnell@redhat.com>
2017-08-16 10:48:21 -0700

    ceph-fuse: load supplementary groups by default
    Partially addresses POSIX test failures in [1] due to the config setting being
    false by default.
    
    [1] https://bugzilla.redhat.com/show_bug.cgi?id=1480182#c6
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>
    (cherry picked from commit 08b077338cba1250c36232f91c708d189326bbb6)

--- src/common/legacy_config_opts.h
+++ src/common/legacy_config_opts.h
@@ -403,7 +403,6 @@ OPTION(fuse_debug, OPT_BOOL)
 OPTION(fuse_multithreaded, OPT_BOOL)
 OPTION(fuse_require_active_mds, OPT_BOOL) // if ceph_fuse requires active mds server
 OPTION(fuse_syncfs_on_mksnap, OPT_BOOL)
-OPTION(fuse_set_user_groups, OPT_BOOL) // if ceph_fuse fills in group lists or not
 
 OPTION(client_try_dentry_invalidate, OPT_BOOL) // the client should try to use dentry invaldation instead of remounting, on kernels it believes that will work for
 OPTION(client_die_on_failed_remount, OPT_BOOL)
