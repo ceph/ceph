8581349c8b1b1e06522707d0b6a3bd2d0c3e35b1
Sage Weil <sage@redhat.com>
2017-08-23 13:53:48 -0400

    qa/workunits/rados/test-upgrade-v11.0.0.sh: more tests skipped
    Signed-off-by: Sage Weil <sage@redhat.com>

--- qa/workunits/rados/test-upgrade-v11.0.0.sh
+++ qa/workunits/rados/test-upgrade-v11.0.0.sh
@@ -13,14 +13,14 @@ trap cleanup EXIT ERR HUP INT QUIT
 
 pids=""
 for f in \
-    'api_aio --gtest_filter=-LibRadosAio.RacingRemovePP:*WriteSame*:*CmpExt*:*RoundTrip3*:*Quota*' \
+    'api_aio --gtest_filter=-LibRadosAio.RacingRemovePP:*WriteSame*:*CmpExt*:*RoundTrip3*:*RoundTripPP3*:*Quota*' \
     'api_list --gtest_filter=-LibRadosList*.EnumerateObjects*' \
     'api_io --gtest_filter=-*Checksum*:*CmpExt*' \
     api_lock \
     'api_misc --gtest_filter=-*WriteSame*:*CmpExt*:*Checksum*:*CloneRange*' \
     'api_watch_notify --gtest_filter=-*WatchNotify3*' \
     api_tier api_pool api_snapshots api_stat api_cmd \
-    'api_c_write_operations --gtest_filter=-*WriteSame*' \
+    'api_c_write_operations --gtest_filter=-*WriteSame*:*CmpExt*' \
     api_c_read_operations \
     list_parallel \
     open_pools_parallel \
