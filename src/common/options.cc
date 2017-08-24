e406db59807e49effadef312ab568ec4b6f47bd8
Kefu Chai <kchai@redhat.com>
2017-08-24 17:08:00 +0800

    common/options: use user-defined literals for default sizes
    for better readablity.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/common/options.cc
+++ src/common/options.cc
@@ -129,6 +129,15 @@ void Option::dump(Formatter *f) const
   f->close_section();
 }
 
+constexpr unsigned long long operator"" _K (unsigned long long n) {
+  return n << 10;
+}
+constexpr unsigned long long operator"" _M (unsigned long long n) {
+  return n << 20;
+}
+constexpr unsigned long long operator"" _G (unsigned long long n) {
+  return n << 30;
+}
 
 std::vector<Option> get_global_options() {
   return std::vector<Option>({
@@ -655,7 +664,7 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("ms_rwthread_stack_bytes", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(1024 << 10)
+    .set_default(1_M)
     .set_description(""),
 
     Option("ms_tcp_read_timeout", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
@@ -728,11 +737,11 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("ms_async_rdma_buffer_size", Option::TYPE_INT, Option::LEVEL_ADVANCED)
-    .set_default(128 << 10)
+    .set_default(128_K)
     .set_description(""),
 
     Option("ms_async_rdma_send_buffers", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(1024)
+    .set_default(1_K)
     .set_description(""),
 
     Option("ms_async_rdma_receive_buffers", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
@@ -1152,7 +1161,7 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("mon_reweight_min_bytes_per_osd", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(100*1024*1024)
+    .set_default(100_M)
     .set_description(""),
 
     Option("mon_reweight_max_osds", Option::TYPE_INT, Option::LEVEL_ADVANCED)
@@ -1200,7 +1209,7 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("mon_data_size_warn", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(15ull*1024*1024*1024)
+    .set_default(15_G)
     .set_description(""),
 
     Option("mon_warn_not_scrubbed", Option::TYPE_INT, Option::LEVEL_ADVANCED)
@@ -1488,7 +1497,7 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("objecter_inflight_op_bytes", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(1024*1024*100)
+    .set_default(100_M)
     .set_description(""),
 
     Option("objecter_inflight_ops", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
@@ -1616,7 +1625,7 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("osd_client_message_size_cap", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(500*1024L*1024L)
+    .set_default(500_M)
     .set_description(""),
 
     Option("osd_client_message_cap", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
@@ -1764,7 +1773,7 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("osd_tier_promote_max_bytes_sec", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(5 * 1024*1024)
+    .set_default(5_M)
     .set_description(""),
 
     Option("osd_tier_default_cache_mode", Option::TYPE_STR, Option::LEVEL_ADVANCED)
@@ -2719,11 +2728,11 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("leveldb_write_buffer_size", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(8 *1024*1024)
+    .set_default(8_M)
     .set_description(""),
 
     Option("leveldb_cache_size", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(128 *1024*1024)
+    .set_default(128_M)
     .set_description(""),
 
     Option("leveldb_block_size", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
@@ -2788,7 +2797,7 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("rocksdb_cache_size", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(128*1024*1024)
+    .set_default(128_M)
     .set_description(""),
 
     Option("rocksdb_cache_row_ratio", Option::TYPE_FLOAT, Option::LEVEL_ADVANCED)
@@ -2804,7 +2813,7 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("rocksdb_block_size", Option::TYPE_INT, Option::LEVEL_ADVANCED)
-    .set_default(4*1024)
+    .set_default(4_K)
     .set_description(""),
 
     Option("rocksdb_perf", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
@@ -2914,7 +2923,7 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("osd_max_object_size", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(128*1024L*1024L)
+    .set_default(128_M)
     .set_description(""),
 
     Option("osd_max_object_name_len", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
@@ -2982,7 +2991,7 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("memstore_device_bytes", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(1024*1024*1024)
+    .set_default(1_G)
     .set_description(""),
 
     Option("memstore_page_set", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
@@ -3108,7 +3117,7 @@ std::vector<Option> get_global_options() {
     .set_description("Mirror bluefs data to file system for testing/validation"),
 
     Option("bluestore_bluefs_min", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(1*1024*1024*1024)
+    .set_default(1_G)
     .set_description("minimum disk space allocated to BlueFS (e.g., at mkfs)"),
 
     Option("bluestore_bluefs_min_ratio", Option::TYPE_FLOAT, Option::LEVEL_ADVANCED)
@@ -3149,7 +3158,7 @@ std::vector<Option> get_global_options() {
     .set_description("Path to block device/file"),
 
     Option("bluestore_block_size", Option::TYPE_UINT, Option::LEVEL_DEV)
-    .set_default(10ull * 1024*1024*1024)
+    .set_default(10_G)
     .add_tag("mkfs")
     .set_description("Size of file to create for backing bluestore"),
 
@@ -3182,7 +3191,7 @@ std::vector<Option> get_global_options() {
     .set_description("Path to block device/file backing bluefs wal"),
 
     Option("bluestore_block_wal_size", Option::TYPE_UINT, Option::LEVEL_DEV)
-    .set_default(96 * 1024*1024)
+    .set_default(96_M)
     .add_tag("mkfs")
     .set_description("Size of file to create for bluestore_block_wal_path"),
 
@@ -3213,7 +3222,7 @@ std::vector<Option> get_global_options() {
     .add_see_also("bluestore_csum_max_block"),
 
     Option("bluestore_csum_max_block", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(64*1024)
+    .set_default(64_K)
     .set_safe()
     .set_description("Maximum block size to checksum")
     .add_see_also("bluestore_csum_min_block"),
@@ -3225,12 +3234,12 @@ std::vector<Option> get_global_options() {
     .set_long_description("A smaller allocation size generally means less data is read and then rewritten when a copy-on-write operation is triggered (e.g., when writing to something that was recently snapshotted).  Similarly, less data is journaled before performing an overwrite (writes smaller than min_alloc_size must first pass through the BlueStore journal).  Larger values of min_alloc_size reduce the amount of metadata required to describe the on-disk layout and reduce overall fragmentation."),
 
     Option("bluestore_min_alloc_size_hdd", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(64*1024)
+    .set_default(64_K)
     .add_tag("mkfs")
     .set_description("Default min_alloc_size value for rotational media"),
 
     Option("bluestore_min_alloc_size_ssd", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(16*1024)
+    .set_default(16_K)
     .add_tag("mkfs")
     .set_description("Default min_alloc_size value for non-rotational (solid state)  media"),
 
@@ -3274,12 +3283,12 @@ std::vector<Option> get_global_options() {
     .set_description("Chunks smaller than this are never compressed"),
 
     Option("bluestore_compression_min_blob_size_hdd", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(128*1024)
+    .set_default(128_K)
     .set_safe()
     .set_description("Default value of bluestore_compression_min_blob_size for rotational media"),
 
     Option("bluestore_compression_min_blob_size_ssd", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(8*1024)
+    .set_default(8_K)
     .set_safe()
     .set_description("Default value of bluestore_compression_min_blob_size for non-rotational (solid state) media"),
 
@@ -3289,12 +3298,12 @@ std::vector<Option> get_global_options() {
     .set_description("Chunks larger than this are broken into smaller chunks before being compressed"),
 
     Option("bluestore_compression_max_blob_size_hdd", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(512*1024)
+    .set_default(512_K)
     .set_safe()
     .set_description("Default value of bluestore_compression_max_blob_size for rotational media"),
 
     Option("bluestore_compression_max_blob_size_ssd", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(64*1024)
+    .set_default(64_K)
     .set_safe()
     .set_description("Default value of bluestore_compression_max_blob_size for non-rotational (solid state) media"),
 
@@ -3314,12 +3323,12 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("bluestore_max_blob_size_hdd", Option::TYPE_UINT, Option::LEVEL_DEV)
-    .set_default(512*1024)
+    .set_default(512_K)
     .set_safe()
     .set_description(""),
 
     Option("bluestore_max_blob_size_ssd", Option::TYPE_UINT, Option::LEVEL_DEV)
-    .set_default(64*1024)
+    .set_default(64_K)
     .set_safe()
     .set_description(""),
 
@@ -3376,11 +3385,11 @@ std::vector<Option> get_global_options() {
     .set_long_description("This includes data and metadata cached by BlueStore as well as memory devoted to rocksdb's cache(s)."),
 
     Option("bluestore_cache_size_hdd", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(1ull*1024*1024*1024)
+    .set_default(1_G)
     .set_description("Default bluestore_cache_size for rotational media"),
 
     Option("bluestore_cache_size_ssd", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(3ull*1024*1024*1024)
+    .set_default(3_G)
     .set_description("Default bluestore_cache_size for non-rotational (solid state) media"),
 
     Option("bluestore_cache_meta_ratio", Option::TYPE_FLOAT, Option::LEVEL_ADVANCED)
@@ -3392,7 +3401,7 @@ std::vector<Option> get_global_options() {
     .set_description("Ratio of bluestore cache to devote to kv database (rocksdb)"),
 
     Option("bluestore_cache_kv_max", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(512*1024*1024)
+    .set_default(512_M)
     .set_description("Max memory (bytes) to devote to kv database (rocksdb)"),
 
     Option("bluestore_kvbackend", Option::TYPE_STR, Option::LEVEL_DEV)
@@ -3454,12 +3463,12 @@ std::vector<Option> get_global_options() {
     .set_description("Try to submit metadata transaction to rocksdb in queuing thread context"),
 
     Option("bluestore_throttle_bytes", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(64*1024*1024)
+    .set_default(64_M)
     .set_safe()
     .set_description("Maximum bytes in flight before we throttle IO submission"),
 
     Option("bluestore_throttle_deferred_bytes", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(128*1024*1024)
+    .set_default(128_M)
     .set_safe()
     .set_description("Maximum bytes for deferred writes before we throttle IO submission"),
 
@@ -3581,7 +3590,7 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("kstore_max_bytes", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(64*1024*1024)
+    .set_default(64_M)
     .set_description(""),
 
     Option("kstore_backend", Option::TYPE_STR, Option::LEVEL_ADVANCED)
@@ -4144,23 +4153,23 @@ std::vector<Option> get_rgw_options() {
     .set_description(""),
 
     Option("rgw_max_chunk_size", Option::TYPE_INT, Option::LEVEL_ADVANCED)
-    .set_default(4 * 1024 * 1024)
+    .set_default(4_M)
     .set_description(""),
 
     Option("rgw_put_obj_min_window_size", Option::TYPE_INT, Option::LEVEL_ADVANCED)
-    .set_default(16 * 1024 * 1024)
+    .set_default(16_M)
     .set_description(""),
 
     Option("rgw_put_obj_max_window_size", Option::TYPE_INT, Option::LEVEL_ADVANCED)
-    .set_default(64 * 1024 * 1024)
+    .set_default(64_M)
     .set_description(""),
 
     Option("rgw_max_put_size", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(5ULL*1024*1024*1024)
+    .set_default(5_G)
     .set_description(""),
 
     Option("rgw_max_put_param_size", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
-    .set_default(1 * 1024 * 1024)
+    .set_default(1_M)
     .set_description(""),
 
     Option("rgw_override_bucket_index_max_shards", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
@@ -4687,7 +4696,7 @@ std::vector<Option> get_rgw_options() {
     .set_description(""),
 
     Option("rgw_copy_obj_progress_every_bytes", Option::TYPE_INT, Option::LEVEL_ADVANCED)
-    .set_default(1024 * 1024)
+    .set_default(1_M)
     .set_description(""),
 
     Option("rgw_obj_tombstone_cache_size", Option::TYPE_INT, Option::LEVEL_ADVANCED)
@@ -4767,7 +4776,7 @@ std::vector<Option> get_rgw_options() {
     .set_description(""),
 
     Option("rgw_multipart_min_part_size", Option::TYPE_INT, Option::LEVEL_ADVANCED)
-    .set_default(5 * 1024 * 1024)
+    .set_default(5_M)
     .set_description(""),
 
     Option("rgw_multipart_part_upload_limit", Option::TYPE_INT, Option::LEVEL_ADVANCED)
@@ -5111,11 +5120,11 @@ static std::vector<Option> get_rbd_options() {
     .set_description("number of sequential requests necessary to trigger readahead"),
 
     Option("rbd_readahead_max_bytes", Option::TYPE_INT, Option::LEVEL_ADVANCED)
-    .set_default(512 * 1024)
+    .set_default(512_K)
     .set_description("set to 0 to disable readahead"),
 
     Option("rbd_readahead_disable_after_bytes", Option::TYPE_INT, Option::LEVEL_ADVANCED)
-    .set_default(50 * 1024 * 1024)
+    .set_default(50_M)
     .set_description("how many bytes are read in total before readahead is disabled"),
 
     Option("rbd_clone_copy_on_read", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
@@ -5818,15 +5827,15 @@ std::vector<Option> get_mds_client_options() {
     .set_description(""),
 
     Option("client_oc_size", Option::TYPE_INT, Option::LEVEL_ADVANCED)
-    .set_default(1024*1024* 200)
+    .set_default(200_M)
     .set_description(""),
 
     Option("client_oc_max_dirty", Option::TYPE_INT, Option::LEVEL_ADVANCED)
-    .set_default(1024*1024* 100)
+    .set_default(100_M)
     .set_description(""),
 
     Option("client_oc_target_dirty", Option::TYPE_INT, Option::LEVEL_ADVANCED)
-    .set_default(1024*1024* 8)
+    .set_default(8_M)
     .set_description(""),
 
     Option("client_oc_max_dirty_age", Option::TYPE_FLOAT, Option::LEVEL_ADVANCED)
