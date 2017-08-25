ceph/ceph

commit b739a11cad159efd9bb3666da75fe20caabe0974

osd/PGBackend: delete reply if fails to complete delete request
if any of the objects fails to be deleted due to pg reset after latest
osdmap, the pg recovery delete reply won't be sent to the primary OSD.
in that case, we should delete the reply.

Fixes: http://tracker.ceph.com/issues/20913
Signed-off-by: Kefu Chai <kchai@redhat.com>

src/osd/PGBackend.cc
@@ -140,6 +140,8 @@ void PGBackend::handle_recovery_delete(OpRequestRef op)
     [=](int r) {
       if (r != -EAGAIN) {
 	get_parent()->send_message_osd_cluster(reply, conn.get());
+      } else {
+	delete reply;
       }
     }));
   gather.activate();

commit 31a7cb5ddb34312b6ab823539af4dc538f6257f0

osd/PGBackend: release a msg using msg->put() not delete
fix the regression introduced by b739a11c

Fixes: http://tracker.ceph.com/issues/20913
Signed-off-by: Kefu Chai <kchai@redhat.com>

src/osd/PGBackend.cc
@@ -141,7 +141,7 @@ void PGBackend::handle_recovery_delete(OpRequestRef op)
       if (r != -EAGAIN) {
 	get_parent()->send_message_osd_cluster(reply, conn.get());
       } else {
-	delete reply;
+	reply->put();
       }
     }));
   gather.activate();
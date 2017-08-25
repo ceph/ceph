31a7cb5ddb34312b6ab823539af4dc538f6257f0
Kefu Chai <kchai@redhat.com>
2017-08-25 10:36:56 +0800

    osd/PGBackend: release a msg using msg->put() not delete
    fix the regression introduced by b739a11c
    
    Fixes: http://tracker.ceph.com/issues/20913
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/osd/PGBackend.cc
+++ src/osd/PGBackend.cc
@@ -141,7 +141,7 @@ void PGBackend::handle_recovery_delete(OpRequestRef op)
       if (r != -EAGAIN) {
 	get_parent()->send_message_osd_cluster(reply, conn.get());
       } else {
-	delete reply;
+	reply->put();
       }
     }));
   gather.activate();
