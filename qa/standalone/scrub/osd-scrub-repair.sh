84465bf5a583651436df7813d3b24857dc49460d
Sage Weil <sage@redhat.com>
2017-08-25 09:45:51 -0400

    qa/standalone/scrub/osd-scrub-repair: fix grep pattern
    PGMap shows
    
        ss << pg_sum.stats.sum.num_objects_unfound
           << "/" << pg_sum.stats.sum.num_objects << " objects unfound (" << b << "%)";
    
    but we were grepping for "1/1 unfound" instead of "1/1 objects
    unfound".
    
    Introduced by fe81b7e3a5034ce855303f93f3e413f3f2dc74a8.
    
    Fixes: http://tracker.ceph.com/issues/21127
    Signed-off-by: Sage Weil <sage@redhat.com>

--- qa/standalone/scrub/osd-scrub-repair.sh
+++ qa/standalone/scrub/osd-scrub-repair.sh
@@ -374,11 +374,11 @@ function unfound_erasure_coded() {
     #
     # it may take a bit to appear due to mon/mgr asynchrony
     for f in `seq 1 60`; do
-	ceph -s | grep "1/1 unfound" && break
+	ceph -s | grep "1/1 objects unfound" && break
 	sleep 1
     done
     ceph -s|grep "4 osds: 4 up, 4 in" || return 1
-    ceph -s|grep "1/1 unfound" || return 1
+    ceph -s|grep "1/1 objects unfound" || return 1
 
     teardown $dir || return 1
 }
