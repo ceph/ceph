35195a3d19482224d371c6b72688b367efc257cf
Sage Weil <sage@redhat.com>
2017-08-28 16:23:09 -0400

    mon/PGStatService: prune interface
    Remove now unused methods
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mon/PGStatService.h
+++ src/mon/PGStatService.h
@@ -32,47 +32,8 @@ class PGStatService {
 public:
   PGStatService() {}
   virtual ~PGStatService() {}
-  // FIXME: Kill this once we rip out PGMonitor post-luminous
-  /** returns true if the underlying data is readable. Always true
-   *  post-luminous, but not when we are redirecting to the yPGMonitor
-   */
-  virtual bool is_readable() const { return true; }
+
   virtual const pool_stat_t* get_pool_stat(int64_t poolid) const = 0;
-  virtual const osd_stat_t& get_osd_sum() const {
-    ceph_abort();
-  }
-  virtual const osd_stat_t *get_osd_stat(int osd) const {
-    ceph_abort();
-  }
-  virtual const mempool::pgmap::unordered_map<int32_t,osd_stat_t>& get_osd_stat() const {
-    ceph_abort();
-  }
-  virtual float get_full_ratio() const {
-    ceph_abort();
-  }
-  virtual float get_nearfull_ratio() const {
-    ceph_abort();
-  }
-  virtual bool have_creating_pgs() const {
-    ceph_abort();
-  }
-  virtual bool is_creating_pg(pg_t pgid) const {
-    ceph_abort();
-  }
-  virtual epoch_t get_min_last_epoch_clean() const {
-    ceph_abort();
-  }
-
-  virtual bool have_full_osds() const {
-    ceph_abort();
-  }
-  virtual bool have_nearfull_osds() const {
-    ceph_abort();
-  }
-
-  virtual size_t get_num_pg_by_osd(int osd) const {
-    ceph_abort();
-  }
   virtual ceph_statfs get_statfs(OSDMap &osd_map,
 				 boost::optional<int64_t> data_pool) const = 0;
   virtual void print_summary(Formatter *f, ostream *out) const = 0;
