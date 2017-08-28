141c21dc49e352c948faa7dd0dad5292b7217e19
Luo Kexue <luo.kexue@zte.com.cn>
2017-08-28 09:24:10 +0800

    osd, mds, tools: drop the invalid comment and some unused variables
    Signed-off-by: Luo Kexue <luo.kexue@zte.com.cn>

--- src/osd/ReplicatedBackend.h
+++ src/osd/ReplicatedBackend.h
@@ -58,7 +58,6 @@ public:
 
   void check_recovery_sources(const OSDMapRef& osdmap) override;
 
-  /// @see PGBackend::delay_message_until_active
   bool can_handle_while_inactive(OpRequestRef op) override;
 
   /// @see PGBackend::handle_message
