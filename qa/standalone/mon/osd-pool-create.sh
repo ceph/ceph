c9ffeeebebe84111ce3be51c04170804bf2dbc74
Sage Weil <sage@redhat.com>
2017-09-09 22:47:06 -0400

    qa/standalong/mon/osd-pool-create: fewer pgs in test
    This runs afoul of the new max pg per osd limit.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- qa/standalone/mon/osd-pool-create.sh
+++ qa/standalone/mon/osd-pool-create.sh
@@ -200,7 +200,7 @@ function TEST_utf8_cli() {
     # the fix for http://tracker.ceph.com/issues/7387.  If it turns out
     # to not be OK (when is the default encoding *not* UTF-8?), maybe
     # the character '黄' can be replaced with the escape $'\xe9\xbb\x84'
-    ceph osd pool create 黄 1024 || return 1
+    ceph osd pool create 黄 16 || return 1
     ceph osd lspools 2>&1 | \
         grep "黄" || return 1
     ceph -f json-pretty osd dump | \
