d704d5b4ecf454b0bb3fea54ae9835b89ba9427d
Zac Medico <zmedico@gmail.com>
2017-09-10 11:11:32 -0700

    Add array_interval_set
    This interval_set implementation, based on std::vector,
    is useful for cases where it provides better performance
    than the std::map based implementation. For example,
    it can be used to efficiently store a copy or the
    result of an intersection operation, while costing
    significantly less than it would to perform the same
    operations with std::map.
    
    The code is practically identical to interval_set, with
    only small modifications in order to adjust to the API
    differences between std::vector and std::map.
    
    Signed-off-by: Zac Medico <zmedico@gmail.com>

---
+++ src/include/array_interval_set.hpp
@@ -0,0 +1,455 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2004-2006 Sage Weil <sage@newdream.net>
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
+
+#ifndef CEPH_ARRAY_INTERVAL_SET_H
+#define CEPH_ARRAY_INTERVAL_SET_H
+
+#include <algorithm>
+#include <utility>
+#include <vector>
+
+#include "abs_interval_set.hpp"
+
+template<typename T>
+class array_interval_set: public abs_interval_set::interval_set<T> {
+ private:
+  typedef std::pair<T,T> map_value_t;
+  typedef std::vector<map_value_t> map_t;
+ public:
+  using typename abs_interval_set::interval_set<T>::value_type;
+  using typename abs_interval_set::interval_set<T>::iterator;
+  using typename abs_interval_set::interval_set<T>::const_iterator;
+  using abs_interval_set::interval_set<T>::operator==;
+  using abs_interval_set::interval_set<T>::intersection_of;
+
+  array_interval_set() : _size(0) {}
+
+  int num_intervals() const
+  {
+    return m.size();
+  }
+
+  typename array_interval_set<T>::iterator begin() {
+    return typename array_interval_set<T>::iterator(m.begin());
+  }
+
+  typename array_interval_set<T>::iterator lower_bound(T start) {
+    return typename array_interval_set<T>::iterator(find_inc_m(start));
+  }
+
+  typename array_interval_set<T>::iterator end() {
+    return typename array_interval_set<T>::iterator(m.end());
+  }
+
+  typename array_interval_set<T>::const_iterator begin() const {
+    return typename array_interval_set<T>::const_iterator(m.begin());
+  }
+
+  typename array_interval_set<T>::const_iterator lower_bound(T start) const {
+    return typename array_interval_set<T>::const_iterator(find_inc(start));
+  }
+
+  typename array_interval_set<T>::const_iterator end() const {
+    return typename array_interval_set<T>::const_iterator(m.end());
+  }
+
+  /*
+   * Allows template methods to access the underlying
+   * iterator type, for maximum performance.
+   */
+  const std::vector<map_value_t>& get_raw_data() const {
+    return m;
+  }
+
+ // base class calls protected helpers
+ friend class abs_interval_set::interval_set<T>;
+
+ // helpers
+ protected:
+  void insert(iterator &mi,
+        const typename abs_interval_set::interval_set<T>::value_type &value) {
+   /*
+    * An upper_bound call like the following would be redundant, since
+    * our intention is only to insert at the end position:
+    *
+    * auto pos = std::upper_bound(
+    *      *reinterpret_cast<const typename decltype(m)::iterator*>(
+    *      mi.get_impl()), m.end(), v);
+    */
+    auto pos = m.end();
+    _size += value.second;
+    typename decltype(m)::value_type v(value);
+    mi = m.insert(pos, std::move(v));
+  }
+
+  typename map_t::const_iterator find_inc(T start) const {
+    auto p = std::lower_bound<typename map_t::const_iterator,map_value_t>(
+        m.begin(), m.end(), {start, 0});  // p->first >= start
+    if (p != m.begin() &&
+        (p == m.end() || p->first > start)) {
+      --p;   // might overlap?
+      if (p->first + p->second <= start)
+        ++p; // it doesn't.
+    }
+    return p;
+  }
+
+ private:
+  typename map_t::iterator find_inc_m(T start) {
+    auto p = std::lower_bound<typename map_t::iterator,map_value_t>(
+        m.begin(), m.end(), {start, 0});
+    if (p != m.begin() &&
+        (p == m.end() || p->first > start)) {
+      --p;   // might overlap?
+      if (p->first + p->second <= start)
+        ++p; // it doesn't.
+    }
+    return p;
+  }
+
+  typename map_t::const_iterator find_adj(T start) const {
+    auto p = std::lower_bound<typename map_t::const_iterator,map_value_t>(
+        m.begin(), m.end(), {start, 0});
+    if (p != m.begin() &&
+        (p == m.end() || p->first > start)) {
+      --p;   // might touch?
+      if (p->first + p->second < start)
+        ++p; // it doesn't.
+    }
+    return p;
+  }
+
+  typename map_t::iterator find_adj_m(T start) {
+    auto p = std::lower_bound<typename map_t::iterator,map_value_t>(
+        m.begin(), m.end(), {start, 0});
+    if (p != m.begin() &&
+        (p == m.end() || p->first > start)) {
+      --p;   // might touch?
+      if (p->first + p->second < start)
+        ++p; // it doesn't.
+    }
+    return p;
+  }
+
+ public:
+  bool operator==(const array_interval_set& other) const {
+    return _size == other._size && m == other.m;
+  }
+
+  int64_t size() const {
+    return _size;
+  }
+
+  void clear() {
+    m.clear();
+    _size = 0;
+  }
+
+  void reserve(size_t new_cap) {
+    m.reserve(new_cap);
+  }
+
+  template <class ISet>
+  typename abs_interval_set::interval_set<T>& operator=(const ISet &rhs) {
+    reserve(rhs.num_intervals());
+    return abs_interval_set::interval_set<T>::operator=(rhs);
+  }
+
+  bool contains(T i, T *pstart=0, T *plen=0) const {
+    typename map_t::const_iterator p = find_inc(i);
+    if (p == m.end()) return false;
+    if (p->first > i) return false;
+    if (p->first+p->second <= i) return false;
+    assert(p->first <= i && p->first+p->second > i);
+    if (pstart)
+      *pstart = p->first;
+    if (plen)
+      *plen = p->second;
+    return true;
+  }
+  bool contains(T start, T len) const {
+    typename map_t::const_iterator p = find_inc(start);
+    if (p == m.end()) return false;
+    if (p->first > start) return false;
+    if (p->first+p->second <= start) return false;
+    assert(p->first <= start && p->first+p->second > start);
+    if (p->first+p->second < start+len) return false;
+    return true;
+  }
+  bool intersects(T start, T len) const {
+    array_interval_set a;
+    a.insert(start, len);
+    array_interval_set i;
+    i.intersection_of( *this, a );
+    if (i.empty()) return false;
+    return true;
+  }
+
+  // outer range of set
+  bool empty() const {
+    return m.empty();
+  }
+  T range_start() const {
+    assert(!empty());
+    typename map_t::const_iterator p = m.begin();
+    return p->first;
+  }
+  T range_end() const {
+    assert(!empty());
+    typename map_t::const_iterator p = m.end();
+    --p;
+    return p->first+p->second;
+  }
+
+  // interval start after p (where p not in set)
+  bool starts_after(T i) const {
+    assert(!contains(i));
+    typename map_t::const_iterator p = find_inc(i);
+    if (p == m.end()) return false;
+    return true;
+  }
+  T start_after(T i) const {
+    assert(!contains(i));
+    typename map_t::const_iterator p = find_inc(i);
+    return p->first;
+  }
+
+  // interval end that contains start
+  T end_after(T start) const {
+    assert(contains(start));
+    typename map_t::const_iterator p = find_inc(start);
+    return p->first+p->second;
+  }
+
+  void insert(T val) {
+    insert(val, 1);
+  }
+
+  void insert(T start, T len, T *pstart=0, T *plen=0) {
+    //cout << "insert " << start << "~" << len << endl;
+    assert(len > 0);
+    map_value_t interval(start, len);
+    _size += len;
+    typename map_t::iterator p = find_adj_m(start);
+    if (p == m.end()) {
+      m.insert(p, interval); // new interval
+      if (pstart)
+	*pstart = start;
+      if (plen)
+	*plen = len;
+    } else {
+      if (p->first < start) {
+
+        if (p->first + p->second != start) {
+          //cout << "p is " << p->first << "~" << p->second << ", start is " << start << ", len is " << len << endl;
+          ceph_abort();
+        }
+
+        p->second += len;               // append to end
+
+        typename map_t::iterator n = p;
+        ++n;
+	if (pstart)
+	  *pstart = p->first;
+        if (n != m.end() &&
+            start+len == n->first) {   // combine with next, too!
+          p->second += n->second;
+	  if (plen)
+	    *plen = p->second;
+          m.erase(n);
+        } else {
+	  if (plen)
+	    *plen = p->second;
+	}
+      } else {
+        if (start+len == p->first) {
+	  if (pstart)
+	    *pstart = start;
+	  if (plen)
+	    *plen = len + p->second;
+          interval.second += p->second; // append to front
+          m[p-m.begin()] = interval;
+        } else {
+          assert(p->first > start+len);
+	  if (pstart)
+	    *pstart = start;
+	  if (plen)
+	    *plen = len;
+          m.insert(p, interval);        // new interval
+        }
+      }
+    }
+  }
+
+  template <class ISet>
+  void swap(ISet &other) {
+    array_interval_set temp;
+    swap(temp);
+    *this = other;
+    other = temp;
+  }
+
+  void swap(array_interval_set<T>& other) {
+    m.swap(other.m);
+    std::swap(_size, other._size);
+  }
+
+  void erase(iterator &i) {
+    _size -= i.get_len();
+    assert(_size >= 0);
+    m.erase(*reinterpret_cast<const typename decltype(m)::iterator*>(
+        i.get_impl()));
+  }
+
+  void erase(T val) {
+    erase(val, 1);
+  }
+
+  void erase(T start, T len) {
+    typename map_t::iterator p = find_inc_m(start);
+
+    _size -= len;
+    assert(_size >= 0);
+
+    assert(p != m.end());
+    assert(p->first <= start);
+
+    T before = start - p->first;
+    assert(p->second >= before+len);
+    T after = p->second - before - len;
+
+    if (before)
+      p->second = before;        // shorten bit before
+    else
+      m.erase(p);
+    if (after) {
+      _size -= after;
+      insert(start+len, after);
+    }
+  }
+
+
+  void subtract(const array_interval_set &a) {
+    for (typename map_t::const_iterator p = a.m.begin();
+         p != a.m.end();
+         ++p)
+      erase(p->first, p->second);
+  }
+
+  template<class ISet>
+  void subtract(const ISet &a) {
+    const auto a_end = a.get_raw_data().end();
+    for (auto p = a.get_raw_data().begin();
+         p != a_end;
+         ++p)
+      erase(p->first, p->second);
+  }
+
+  void insert(const array_interval_set &a) {
+    for (typename map_t::const_iterator p = a.m.begin();
+         p != a.m.end();
+         ++p)
+      insert(p->first, p->second);
+  }
+
+  template <class ISetA, class ISetB>
+  void intersection_of(const ISetA &a, const ISetB &b) {
+    reserve(std::max(a.num_intervals(), b.num_intervals()));
+    abs_interval_set::interval_set<T>::intersection_of(a, b);
+  }
+
+  template <class ISet>
+  void intersection_of(const ISet &b) {
+    array_interval_set a;
+    swap(a);
+    intersection_of(a, b);
+  }
+
+  template <class ISetA, class ISetB>
+  void union_of(const ISetA &a, const ISetB &b) {
+    assert(&a != this);
+    assert(&b != this);
+
+    // a
+    *this = a;
+
+    // - (a*b)
+    array_interval_set ab;
+    ab.intersection_of(a, b);
+    subtract(ab);
+
+    // + b
+    insert(b);
+    return;
+  }
+
+  template <class ISet>
+  void union_of(const ISet &b) {
+    array_interval_set a;
+    swap(a);
+    union_of(a, b);
+  }
+
+  template <class ISet>
+  bool subset_of(const ISet &big) const {
+    for (typename map_t::const_iterator i = m.begin();
+         i != m.end();
+         ++i)
+      if (!big.contains(i->first, i->second)) return false;
+    return true;
+  }
+
+  /*
+   * build a subset of @other, starting at or after @start, and including
+   * @len worth of values, skipping holes.  e.g.,
+   *  span_of([5~10,20~5], 8, 5) -> [8~2,20~3]
+   */
+  void span_of(const array_interval_set &other, T start, T len) {
+    clear();
+    typename map_t::const_iterator p = other.find_inc(start);
+    if (p == other.m.end())
+      return;
+    if (p->first < start) {
+      if (p->first + p->second < start)
+	return;
+      if (p->first + p->second < start + len) {
+	T howmuch = p->second - (start - p->first);
+	insert(start, howmuch);
+	len -= howmuch;
+	++p;
+      } else {
+	insert(start, len);
+	return;
+      }
+    }
+    while (p != other.m.end() && len > 0) {
+      if (p->second < len) {
+	insert(p->first, p->second);
+	len -= p->second;
+	++p;
+      } else {
+	insert(p->first, len);
+	return;
+      }
+    }
+  }
+
+private:
+  // data
+  int64_t _size;
+  map_t m;   // map start -> len
+};
+
+#endif
