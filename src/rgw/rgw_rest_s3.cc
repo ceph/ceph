e23bae91682d8f83c33ac7991ca9857ed8422862
yuliyang <yuliyang@cmss.chinamobile.com>
2017-09-11 15:28:30 +0800

    rgw: return 'Access-Control-Allow-Origin' header when the set bucket versioning through XMLHttpRequest
    Signed-off-by: yuliyang <yuliyang@cmss.chinamobile.com>

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -962,7 +962,7 @@ void RGWSetBucketVersioning_ObjStore_S3::send_response()
   if (op_ret)
     set_req_state_err(s, op_ret);
   dump_errno(s);
-  end_header(s);
+  end_header(s, this, "application/xml");
 }
 
 int RGWSetBucketWebsite_ObjStore_S3::get_params()
