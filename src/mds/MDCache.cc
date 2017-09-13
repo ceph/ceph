ceph/ceph

commit 43abaeac50528153b5e1ff72fd5c4a971f29d927

Make standby-replay mds avoid initiating subtree export
Signed-off-by: Jianyu Li <joannyli@tencent.com>

src/mds/Migrator.cc
@@ -793,6 +793,10 @@ void Migrator::export_dir(CDir *dir, mds_rank_t dest)
     //ceph_abort();
     return;
   }
+  if (!mds->is_active()) {
+    dout(7) << "i'm not active, no exports for now" << dendl;
+    return;
+  }
 
   if (!dir->inode->is_base() && dir->inode->get_projected_parent_dir()->inode->is_stray() &&
       dir->inode->get_projected_parent_dir()->get_parent_dir()->ino() != MDS_INO_MDSDIR(dest)) {

commit ce4b1433f7d766ce11dbe4a6565c23292c42257b

ceph: fix "stopping mds can't export subtree"
caused by commit
43abaeac50 "Make standby-replay mds avoid initiating subtree export"

Fixes: http://tracker.ceph.com/issues/21378
Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

src/mds/MDCache.cc
@@ -6498,7 +6498,8 @@ bool MDCache::trim(int max, int count)
       if (!diri->is_auth() && !diri->is_base() &&
 	  dir->get_num_head_items() == 0) {
 	if (dir->state_test(CDir::STATE_EXPORTING) ||
-	    dir->is_freezing() || dir->is_frozen() || !mds->is_active())
+	    !(mds->is_active() || mds->is_stopping()) ||
+	    dir->is_freezing() || dir->is_frozen())
 	  continue;
 
 	migrator->export_empty_import(dir);

src/mds/Migrator.cc
@@ -776,6 +776,10 @@ void Migrator::export_dir(CDir *dir, mds_rank_t dest)
   assert(dir->is_auth());
   assert(dest != mds->get_nodeid());
    
+  if (!(mds->is_active() || mds->is_stopping())) {
+    dout(7) << "i'm not active, no exports for now" << dendl;
+    return;
+  }
   if (mds->mdcache->is_readonly()) {
     dout(7) << "read-only FS, no exports for now" << dendl;
     return;
@@ -791,10 +795,6 @@ void Migrator::export_dir(CDir *dir, mds_rank_t dest)
   if (dir->inode->is_system()) {
     dout(7) << "i won't export system dirs (root, mdsdirs, stray, /.ceph, etc.)" << dendl;
     //ceph_abort();
-    return;
-  }
-  if (!mds->is_active()) {
-    dout(7) << "i'm not active, no exports for now" << dendl;
     return;
   }
 