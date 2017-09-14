2b0b8d4c7b3b3ae3bd401848937564babe36f77d
Jeff Layton <jlayton@redhat.com>
2017-09-14 12:22:52 -0400

    client: fix signed/unsigned comparison compiler warning
    The build says:
    
    src/client/Client.cc: In member function ‘void Client::trim_caps(MetaSession*, int)’:
    src/client/Client.cc:4121:22: warning: comparison between signed and unsigned integer expressions [-Wsign-compare]
       if (s->caps.size() > max)
           ~~~~~~~~~~~~~~~^~~~~
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    (cherry picked from commit e057b6770b5545f327990cbaf17e7a391e3e8a50)

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -4113,7 +4113,8 @@ void Client::trim_caps(MetaSession *s, int max)
     }
   }
 
-  if (s->caps.size() > max)
+  caps_size = s->caps.size();
+  if (caps_size > max)
     _invalidate_kernel_dcache();
 }
 
