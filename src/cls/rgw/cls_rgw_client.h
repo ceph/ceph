aba2e0b64c0bc9e8dde20296a258b0d9850b5f72
Casey Bodley <cbodley@redhat.com>
2017-09-14 16:50:47 -0400

    rgw: add RGWRadosBILogTrimCR
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 0dea44dad3a572cd966d22dd345c1e226c073f6e)

--- src/cls/rgw/cls_rgw_client.h
+++ src/cls/rgw/cls_rgw_client.h
@@ -4,6 +4,7 @@
 #include "include/str_list.h"
 #include "include/rados/librados.hpp"
 #include "cls_rgw_ops.h"
+#include "cls_rgw_const.h"
 #include "common/RefCountedObj.h"
 #include "include/compat.h"
 #include "common/ceph_time.h"
