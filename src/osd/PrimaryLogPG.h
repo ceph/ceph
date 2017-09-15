b7463bafa60ed4ea7bef676a6ddf1f36cdf6f7f6
Sage Weil <sage@redhat.com>
2017-09-15 11:22:53 -0400

    osd/ECBackend: use Listener instead of PG interface to check undersized
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/PrimaryLogPG.h
+++ src/osd/PrimaryLogPG.h
@@ -415,6 +415,10 @@ public:
       assert(is_backfill_targets(peer));
     return should_send;
   }
+
+  bool pg_is_undersized() const override {
+    return is_undersized();
+  }
   
   void update_peer_last_complete_ondisk(
     pg_shard_t fromosd,
