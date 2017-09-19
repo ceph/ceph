ba106b6d32fb3138b7a61fe613b211e02f1e44aa
Sage Weil <sage@redhat.com>
2017-09-19 15:26:40 -0500

    osd/PG: allow local recovery reservations to be preempted
    If a PG has a higher recovery priority and a lower-priority item is in
    progress, allow it to be preempted.  This triggers the RecoveryCancel
    or BackfillCancel event with a 0 delay, which means it will immediately
    re-request a reservation (and presumably wait).
    
    Signed-off-by: Sage Weil <sage@redhat.com>
    (cherry picked from commit a8534cce1a1661375a93aff2314633bce63695ed)

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -6569,7 +6569,10 @@ PG::RecoveryState::WaitLocalBackfillReserved::WaitLocalBackfillReserved(my_conte
     new QueuePeeringEvt<LocalBackfillReserved>(
       pg, pg->get_osdmap()->get_epoch(),
       LocalBackfillReserved()),
-    pg->get_backfill_priority());
+    pg->get_backfill_priority(),
+    new QueuePeeringEvt<DeferBackfill>(
+      pg, pg->get_osdmap()->get_epoch(),
+      DeferBackfill(0.0)));
   pg->publish_stats_to_osd();
 }
 
@@ -6827,7 +6830,10 @@ PG::RecoveryState::WaitLocalRecoveryReserved::WaitLocalRecoveryReserved(my_conte
     new QueuePeeringEvt<LocalRecoveryReserved>(
       pg, pg->get_osdmap()->get_epoch(),
       LocalRecoveryReserved()),
-    pg->get_recovery_priority());
+    pg->get_recovery_priority(),
+    new QueuePeeringEvt<DeferRecovery>(
+      pg, pg->get_osdmap()->get_epoch(),
+      DeferRecovery(0.0)));
   pg->publish_stats_to_osd();
 }
 
