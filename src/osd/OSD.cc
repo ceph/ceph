579a000120d42606e740c372ecdf46694bb4a66a
Kefu Chai <kchai@redhat.com>
2017-09-26 15:54:14 +0800

    osd: add max-pg-per-osd limit
    osd will refused to create new pgs, until its pg number is lower
    than the max-pg-per-osd upper bound setting.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -4024,6 +4024,11 @@ int OSD::handle_pg_peering_evt(
       ceph_abort();
     }
 
+    const bool is_mon_create =
+      evt->get_event().dynamic_type() == PG::NullEvt::static_type();
+    if (maybe_wait_for_max_pg(pgid, is_mon_create)) {
+      return -EAGAIN;
+    }
     // do we need to resurrect a deleting pg?
     spg_t resurrected;
     PGRef old_pg_state;
@@ -4161,6 +4166,83 @@ int OSD::handle_pg_peering_evt(
   }
 }
 
+bool OSD::maybe_wait_for_max_pg(spg_t pgid, bool is_mon_create)
+{
+  const auto max_pgs_per_osd =
+    (cct->_conf->get_val<int64_t>("mon_max_pg_per_osd") *
+     cct->_conf->get_val<double>("osd_max_pg_per_osd_hard_ratio"));
+
+  RWLock::RLocker pg_map_locker{pg_map_lock};
+  if (pg_map.size() < max_pgs_per_osd) {
+    return false;
+  }
+  auto&& pending_creates_locker = guardedly_lock(pending_creates_lock);
+  if (is_mon_create) {
+    pending_creates_from_mon++;
+  } else {
+    pending_creates_from_osd.emplace(pgid.pgid);
+  }
+  dout(5) << __func__ << " withhold creation of pg " << pgid
+	  << ": " << pg_map.size() << " >= "<< max_pgs_per_osd << dendl;
+  return true;
+}
+
+// to re-trigger a peering, we have to twiddle the pg mapping a little bit,
+// see PG::should_restart_peering(). OSDMap::pg_to_up_acting_osds() will turn
+// to up set if pg_temp is empty. so an empty pg_temp won't work.
+static vector<int32_t> twiddle(const vector<int>& acting) {
+  if (acting.size() > 1) {
+    return {acting[0]};
+  } else {
+    vector<int32_t> twiddled(acting.begin(), acting.end());
+    twiddled.push_back(-1);
+    return twiddled;
+  }
+}
+
+void OSD::resume_creating_pg()
+{
+  assert(pg_map_lock.is_locked());
+
+  const auto max_pgs_per_osd =
+    (cct->_conf->get_val<int64_t>("mon_max_pg_per_osd") *
+     cct->_conf->get_val<double>("osd_max_pg_per_osd_hard_ratio"));
+  if (max_pgs_per_osd <= pg_map.size()) {
+    // this could happen if admin decreases this setting before a PG is removed
+    return;
+  }
+  unsigned spare_pgs = max_pgs_per_osd - pg_map.size();
+  assert(spare_pgs > 0);
+
+  auto&& locker = guardedly_lock(pending_creates_lock);
+  if (pending_creates_from_mon > 0) {
+    if (monc->sub_want("osd_pg_creates", last_pg_create_epoch, 0)) {
+      dout(4) << __func__ << ": resolicit pg creates from mon since "
+	      << last_pg_create_epoch << dendl;
+      monc->renew_subs();
+    }
+    if (spare_pgs <= pending_creates_from_mon) {
+      pending_creates_from_mon = 0;
+      return;
+    }
+    spare_pgs -= pending_creates_from_mon;
+    pending_creates_from_mon = 0;
+  }
+  if (!pending_creates_from_osd.empty()) {
+    auto m = new MOSDPGTemp(osdmap->get_epoch());
+    for (auto pg = pending_creates_from_osd.cbegin();
+	 pg != pending_creates_from_osd.cend();) {
+      vector<int> acting;
+      osdmap->pg_to_up_acting_osds(*pg, nullptr, nullptr, &acting, nullptr);
+      m->pg_temp[*pg] = twiddle(acting);
+      pg = pending_creates_from_osd.erase(pg);
+      if (--spare_pgs == 0)
+	break;
+    }
+    m->forced = true;
+    monc->send_mon_message(m);
+  }
+}
 
 void OSD::build_initial_pg_history(
   spg_t pgid,
@@ -7714,6 +7796,16 @@ void OSD::consume_map()
 
       pg->unlock();
     }
+
+    auto&& pending_create_locker = guardedly_lock(pending_creates_lock);
+    for (auto pg = pending_creates_from_osd.cbegin();
+	 pg != pending_creates_from_osd.cend();) {
+      if (osdmap->get_pg_acting_rank(*pg, whoami) < 0) {
+	pg = pending_creates_from_osd.erase(pg);
+      } else {
+	++pg;
+      }
+    }
   }
 
   for (list<PGRef>::iterator i = to_remove.begin();
@@ -8038,7 +8130,6 @@ void OSD::handle_pg_create(OpRequestRef op)
 	       << dendl;
       continue;
     }
-
     if (handle_pg_peering_evt(
           pgid,
           history,
@@ -8053,7 +8144,12 @@ void OSD::handle_pg_create(OpRequestRef op)
       service.send_pg_created(pgid.pgid);
     }
   }
-  last_pg_create_epoch = m->epoch;
+
+  with_unique_lock(pending_creates_lock, [=]() {
+      if (pending_creates_from_mon == 0) {
+	last_pg_create_epoch = m->epoch;
+      }
+    });
 
   maybe_update_heartbeat_peers();
 }
@@ -8719,6 +8815,8 @@ void OSD::_remove_pg(PG *pg)
   // remove from map
   pg_map.erase(pg->pg_id);
   pg->put("PGMap"); // since we've taken it out of map
+
+  resume_creating_pg();
 }
 
 
