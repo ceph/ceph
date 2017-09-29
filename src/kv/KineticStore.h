ca7f4881198f0291e84f959ecf7176fe2f22bc8f
Sage Weil <sage@redhat.com>
2017-09-29 16:54:50 -0500

    kv/KineticStore: override
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/kv/KineticStore.h
+++ src/kv/KineticStore.h
@@ -56,7 +56,7 @@ public:
     return do_open(out, false);
   }
   /// Creates underlying db if missing and opens it
-  int create_and_open(ostream &out) {
+  int create_and_open(ostream &out) override {
     return do_open(out, true);
   }
 
