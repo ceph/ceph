fc43e5a1115f72eaadeb74782d8641fc43a39e1f
Sage Weil <sage@redhat.com>
2017-10-02 17:11:38 -0500

    mgr/localpool: default to 3x
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/pybind/mgr/localpool/module.py
+++ src/pybind/mgr/localpool/module.py
@@ -18,7 +18,7 @@ class Module(MgrModule):
         subtree_type = self.get_config('subtree') or 'rack'
         failure_domain = self.get_config('failure_domain') or 'host'
         pg_num = self.get_config('pg_num') or '128'
-        num_rep = self.get_config('num_rep') or '2'
+        num_rep = self.get_config('num_rep') or '3'
         prefix = self.get_config('prefix') or 'by-' + subtree_type + '-'
 
         osdmap = self.get("osd_map")
