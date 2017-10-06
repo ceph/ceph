ceph/ceph

commit 31d20fc7de9e25e66d938687f477f4d229953388

osd/PGPool::update: optimize with deleting_snaps
Add a pg_pool_t.deleting_snaps member that tracks
the current set of snaps to be purged, in order to
avoid expensive operations involving the entire
set of removed snaps. The snaps are trimmed from
deleting snaps at the same time as they are added
to the pg_info_t.purged_snaps set.

Suggested-by: Greg Farnum <gfarnum@redhat.com>
Signed-off-by: Zac Medico <zmedico@gmail.com>

src/osd/osd_types.cc
@@ -1310,6 +1310,14 @@ bool pg_pool_t::maybe_updated_removed_snaps(const interval_set<snapid_t>& cached
   return true;
 }
 
+void pg_pool_t::purged_snap(const snapid_t& purged) {
+  deleting_snaps.erase(purged);
+}
+
+const interval_set<snapid_t>& pg_pool_t::get_deleting_snaps() const {
+  return deleting_snaps;
+}
+
 snapid_t pg_pool_t::snap_exists(const char *s) const
 {
   for (map<snapid_t,pool_snap_info_t>::const_iterator p = snaps.begin();
@@ -1343,6 +1351,7 @@ void pg_pool_t::add_unmanaged_snap(uint64_t& snapid)
 void pg_pool_t::remove_snap(snapid_t s)
 {
   assert(snaps.count(s));
+  deleting_snaps.insert(s);
   snaps.erase(s);
   snap_seq = snap_seq + 1;
 }
@@ -1350,6 +1359,7 @@ void pg_pool_t::remove_snap(snapid_t s)
 void pg_pool_t::remove_unmanaged_snap(snapid_t s)
 {
   assert(is_unmanaged_snaps_mode());
+  deleting_snaps.insert(s);
   removed_snaps.insert(s);
   snap_seq = snap_seq + 1;
   removed_snaps.insert(get_snap_seq());
@@ -1536,7 +1546,10 @@ void pg_pool_t::encode(bufferlist& bl, uint64_t features) const
     return;
   }
 
-  uint8_t v = 26;
+  uint8_t v = 27;
+  if (!HAVE_FEATURE(features, OSD_DELETING_SNAPS)) {
+    v = 26;
+  }
   if (!(features & CEPH_FEATURE_NEW_OSDOP_ENCODING)) {
     // this was the first post-hammer thing we added; if it's missing, encode
     // like hammer.
@@ -1612,6 +1625,9 @@ void pg_pool_t::encode(bufferlist& bl, uint64_t features) const
   }
   if (v >= 26) {
     ::encode(application_metadata, bl);
+  }
+  if (v >= 27) {
+    ::encode(deleting_snaps, bl);
   }
   ENCODE_FINISH(bl);
 }
@@ -1767,6 +1783,11 @@ void pg_pool_t::decode(bufferlist::iterator& bl)
   }
   if (struct_v >= 26) {
     ::decode(application_metadata, bl);
+  }
+  if (struct_v >= 27) {
+    ::decode(deleting_snaps, bl);
+  } else {
+    build_removed_snaps(deleting_snaps);
   }
   DECODE_FINISH(bl);
   calc_pg_masks();

src/include/ceph_features.h
@@ -91,6 +91,7 @@ DEFINE_CEPH_FEATURE(15, 1, MONENC)
 DEFINE_CEPH_FEATURE_RETIRED(16, 1, QUERY_T, JEWEL, LUMINOUS)
 DEFINE_CEPH_FEATURE(16, 3, SERVER_O)
 DEFINE_CEPH_FEATURE_RETIRED(17, 1, INDEP_PG_MAP, JEWEL, LUMINOUS)
+DEFINE_CEPH_FEATURE(17, 1, OSD_DELETING_SNAPS)
 
 DEFINE_CEPH_FEATURE(18, 1, CRUSH_TUNABLES)
 DEFINE_CEPH_FEATURE_RETIRED(19, 1, CHUNKY_SCRUB, JEWEL, LUMINOUS)
@@ -232,6 +233,7 @@ DEFINE_CEPH_FEATURE_DEPRECATED(63, 1, RESERVED_BROKEN, LUMINOUS) // client-facin
 	 CEPH_FEATURE_RADOS_BACKOFF |		\
 	 CEPH_FEATURE_OSD_RECOVERY_DELETES |	\
 	 CEPH_FEATURE_SERVER_MIMIC |		\
+	 CEPH_FEATURE_OSD_DELETING_SNAPS |	\
 	 0ULL)
 
 #define CEPH_FEATURES_SUPPORTED_DEFAULT  CEPH_FEATURES_ALL

src/osd/PG.cc
@@ -236,18 +236,18 @@ void PGPool::update(OSDMapRef map)
       (pi->get_snap_epoch() == map->get_epoch())) {
     updated = true;
     if (pi->maybe_updated_removed_snaps(cached_removed_snaps)) {
-      pi->build_removed_snaps(newly_removed_snaps);
-      if (cached_removed_snaps.subset_of(newly_removed_snaps)) {
-          interval_set<snapid_t> removed_snaps = newly_removed_snaps;
-          newly_removed_snaps.subtract(cached_removed_snaps);
-          cached_removed_snaps.swap(removed_snaps);
+      if (cached_deleting_snaps.subset_of(pi->get_deleting_snaps())) {
+          interval_set<snapid_t> difference = pi->get_deleting_snaps();
+          difference.subtract(cached_deleting_snaps);
+          newly_removed_snaps.swap(difference);
       } else {
           lgeneric_subdout(cct, osd, 0) << __func__
-            << " cached_removed_snaps shrank from " << cached_removed_snaps
-            << " to " << newly_removed_snaps << dendl;
-          cached_removed_snaps.swap(newly_removed_snaps);
+            << " cached_deleting_snaps shrank from " << cached_deleting_snaps
+            << " to " << pi->get_deleting_snaps() << dendl;
           newly_removed_snaps.clear();
       }
+      cached_deleting_snaps = pi->get_deleting_snaps();
+      pi->build_removed_snaps(cached_removed_snaps);
     } else
       newly_removed_snaps.clear();
     snapc = pi->get_snap_context();

src/osd/PG.h
@@ -229,6 +229,7 @@ struct PGPool {
 
   interval_set<snapid_t> cached_removed_snaps;      // current removed_snaps set
   interval_set<snapid_t> newly_removed_snaps;  // newly removed in the last epoch
+  interval_set<snapid_t> cached_deleting_snaps;  // current set to be purged
 
   PGPool(CephContext* cct, OSDMapRef map, int64_t i)
     : cct(cct),
@@ -241,6 +242,7 @@ struct PGPool {
     info = *pi;
     snapc = pi->get_snap_context();
     pi->build_removed_snaps(cached_removed_snaps);
+    cached_deleting_snaps = pi->get_deleting_snaps();
   }
 
   void update(OSDMapRef map);

src/osd/PrimaryLogPG.cc
@@ -13977,6 +13977,7 @@ boost::statechart::result PrimaryLogPG::AwaitAsyncWork::react(const DoSnapWork&)
 		       << dendl;
     pg->info.purged_snaps.insert(snap_to_trim);
     pg->snap_trimq.erase(snap_to_trim);
+    pg->pool.info.purged_snap(snap_to_trim);
     ldout(pg->cct, 10) << "purged_snaps now "
 		       << pg->info.purged_snaps << ", snap_trimq now "
 		       << pg->snap_trimq << dendl;

src/osd/osd_types.h
@@ -1288,6 +1288,7 @@ struct pg_pool_t {
   __u8 object_hash;         ///< hash mapping object name to ps
 private:
   __u32 pg_num, pgp_num;    ///< number of pgs
+  interval_set<snapid_t> deleting_snaps; ///< removed snaps to be been purged
 
 
 public:
@@ -1570,6 +1571,8 @@ public:
    */
   void build_removed_snaps(interval_set<snapid_t>& rs) const;
   bool maybe_updated_removed_snaps(const interval_set<snapid_t>& cached) const;
+  void purged_snap(const snapid_t& purged);
+  const interval_set<snapid_t>& get_deleting_snaps() const;
   snapid_t snap_exists(const char *s) const;
   void add_snap(const char *n, utime_t stamp);
   void add_unmanaged_snap(uint64_t& snapid);

commit 3b5bbdcb64bbada344cc707aeb28e50a5ec437c2

oss/pg_pool_t::purged_snap: ignore snaps not contained in deleting_snaps
This will fix the follwing crash during OSD startup:

 ceph version 12.2.0 (32ce2a3ae5239ee33d6150705cdb24d43bab910c) luminous (rc)
 1: (()+0x9fbb3e) [0x562b96be1b3e]
 2: (()+0x10ea0) [0x7f0753f13ea0]
 3: (gsignal()+0x38) [0x7f075328a148]
 4: (abort()+0x16a) [0x7f075328b5ca]
 5: (ceph::__ceph_assert_fail(char const*, char const*, int, char const*)+0x28e) [0x562b96c2489e]
 6: (pg_pool_t::purged_snap(snapid_t const&)+0x245) [0x562b968b64f5]
 7: (PrimaryLogPG::AwaitAsyncWork::react(PrimaryLogPG::DoSnapWork const&)+0x105a) [0x562b96803fca]
 8: (boost::statechart::simple_state<PrimaryLogPG::AwaitAsyncWork, PrimaryLogPG::Trimming, boost::mpl::list<mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na, mpl_::na>, (boost::statechart::history_mode)0>::react_impl(boost::statechart::event_base const&, void const*)+0xca) [0x562b96854a3a]
 9: (boost::statechart::state_machine<PrimaryLogPG::SnapTrimmer, PrimaryLogPG::NotTrimming, std::allocator<void>, boost::statechart::null_exception_translator>::process_event(boost::statechart::event_base const&)+0x56) [0x562b96835146]
 10: (PrimaryLogPG::snap_trimmer(unsigned int)+0x19c) [0x562b967a163c]
 11: (OSD::ShardedOpWQ::_process(unsigned int, ceph::heartbeat_handle_d*)+0x100c) [0x562b9666987c]
 12: (ShardedThreadPool::shardedthreadpool_worker(unsigned int)+0x884) [0x562b96c29504]
 13: (ShardedThreadPool::WorkThreadSharded::entry()+0x10) [0x562b96c2c540]
 14: (()+0x73e4) [0x7f0753f0a3e4]
 15: (clone()+0x6d) [0x7f075333ea9d]

Fixes: 31d20fc7de9e ("osd/PGPool::update: optimize with deleting_snaps")
Signed-off-by: Zac Medico <zmedico@gmail.com>

src/osd/osd_types.cc
@@ -1311,7 +1311,8 @@ bool pg_pool_t::maybe_updated_removed_snaps(const interval_set<snapid_t>& cached
 }
 
 void pg_pool_t::purged_snap(const snapid_t& purged) {
-  deleting_snaps.erase(purged);
+  if (deleting_snaps.contains(purged, 1))
+    deleting_snaps.erase(purged);
 }
 
 const interval_set<snapid_t>& pg_pool_t::get_deleting_snaps() const {