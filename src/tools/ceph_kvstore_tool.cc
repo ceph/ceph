1717b6b344304730c4f619f4aa31ab0bcfb11f11
Alan Somers <asomers@gmail.com>
2017-10-09 13:35:18 -0600

    Fix non-bluestore builds post-81e4560781c
    	Signed-off-by: Alan Somers <asomers@gmail.com>

--- src/tools/ceph_kvstore_tool.cc
+++ src/tools/ceph_kvstore_tool.cc
@@ -36,7 +36,9 @@ using namespace std;
 
 class StoreTool
 {
+#ifdef HAVE_LIBAIO
   boost::scoped_ptr<BlueStore> bluestore;
+#endif
 
   // TODO: make KeyValueDB enable_shared_from_this
   // bluestore will hold *db* also, use unique_ptr/shared_ptr will
@@ -74,10 +76,13 @@ class StoreTool
   }
 
   ~StoreTool() {
+#ifdef HAVE_LIBAIO
     if (bluestore) {
       bluestore->umount();   
     }
-    else {
+    else
+#endif
+    {
       if (db) {
         delete db;
       }
