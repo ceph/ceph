1fa314a2bc2e1c595c626f3dfc3a60b1ce4be033
Sage Weil <sage@redhat.com>
2017-10-10 11:05:32 -0500

    os/bluestore/BitmapFreelistManager: disable bluestore_debug_freelist
    This doesn't work as implemented.  We are doing _txc_finalize_kv() from
    queue_transactions, which calls into the freelist and does this verify
    code.  However, we have no assurance that a previous txc in the sequencer
    has applied its changes to the kv store, which means that a simple sequence
    like
    
     - write object
     - delete object
    
    can trigger if the write is waiting for aio.  This currently happens
    with ObjectStore/StoreTest.SimpleRemount/2.
    
    Comment out the verify, but leave _verify_range() helper in place in case
    we can use it in the future in some other context.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/bluestore/BitmapFreelistManager.cc
+++ src/os/bluestore/BitmapFreelistManager.cc
@@ -464,8 +464,6 @@ void BitmapFreelistManager::allocate(
 {
   dout(10) << __func__ << " 0x" << std::hex << offset << "~" << length
 	   << std::dec << dendl;
-  if (cct->_conf->bluestore_debug_freelist)
-    _verify_range(offset, length, 0);
   _xor(offset, length, txn);
 }
 
@@ -475,8 +473,6 @@ void BitmapFreelistManager::release(
 {
   dout(10) << __func__ << " 0x" << std::hex << offset << "~" << length
 	   << std::dec << dendl;
-  if (cct->_conf->bluestore_debug_freelist)
-    _verify_range(offset, length, 1);
   _xor(offset, length, txn);
 }
 
