1a44b3fbf371316153e545059fe019ca9cd39417
Jeff Layton <jlayton@redhat.com>
2017-10-11 11:16:39 -0400

    test: make the LibCephFS.ShutdownRacer test even more thrashy
    Have each thread do the startup and shutdown in a loop for a specified
    number of times.
    
    Tracker: http://tracker.ceph.com/issues/21512
    Signed-off-by: Jeff Layton <jlayton@redhat.com>

--- src/test/libcephfs/test.cc
+++ src/test/libcephfs/test.cc
@@ -1862,23 +1862,26 @@ TEST(LibCephFS, OperationsOnRoot)
   ceph_shutdown(cmount);
 }
 
-#define NTHREADS 128
-
 static void shutdown_racer_func()
 {
+  const int niter = 32;
   struct ceph_mount_info *cmount;
+  int i;
 
-  ASSERT_EQ(ceph_create(&cmount, NULL), 0);
-  ASSERT_EQ(ceph_conf_read_file(cmount, NULL), 0);
-  ASSERT_EQ(0, ceph_conf_parse_env(cmount, NULL));
-  ASSERT_EQ(ceph_mount(cmount, "/"), 0);
-  ceph_shutdown(cmount);
+  for (i = 0; i < niter; ++i) {
+    ASSERT_EQ(ceph_create(&cmount, NULL), 0);
+    ASSERT_EQ(ceph_conf_read_file(cmount, NULL), 0);
+    ASSERT_EQ(0, ceph_conf_parse_env(cmount, NULL));
+    ASSERT_EQ(ceph_mount(cmount, "/"), 0);
+    ceph_shutdown(cmount);
+  }
 }
 
 // See tracker #20988
 TEST(LibCephFS, ShutdownRace)
 {
-  std::thread threads[NTHREADS];
+  const int nthreads = 128;
+  std::thread threads[nthreads];
 
   // Need a bunch of fd's for this test
   struct rlimit rold, rnew;
@@ -1887,10 +1890,10 @@ TEST(LibCephFS, ShutdownRace)
   rnew.rlim_cur = rnew.rlim_max;
   ASSERT_EQ(setrlimit(RLIMIT_NOFILE, &rnew), 0);
 
-  for (int i = 0; i < NTHREADS; ++i)
+  for (int i = 0; i < nthreads; ++i)
     threads[i] = std::thread(shutdown_racer_func);
 
-  for (int i = 0; i < NTHREADS; ++i)
+  for (int i = 0; i < nthreads; ++i)
     threads[i].join();
   ASSERT_EQ(setrlimit(RLIMIT_NOFILE, &rold), 0);
 }
