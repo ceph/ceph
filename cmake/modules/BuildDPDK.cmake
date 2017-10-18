3b69b0b23914e3cec22dab308170d6070d051446
wanjun.lp <wanjun.lp@alibaba-inc.com>
2017-10-18 10:21:00 +0800

    cmake: WITH_SPDK=ON by default
    Signed-off-by: wanjun.lp <wanjun.lp@alibaba-inc.com>

--- cmake/modules/BuildDPDK.cmake
+++ cmake/modules/BuildDPDK.cmake
@@ -1,25 +1,26 @@
-if (CMAKE_SYSTEM_NAME MATCHES "Linux")
-  set(dpdk_env  "x86_64-native-linuxapp-gcc")
-  MESSAGE(STATUS "current platform: Linux ")
-elseif (CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
+if(CMAKE_SYSTEM_NAME MATCHES "Linux")
+  if(CMAKE_SYSTEM_PROCESSOR MATCHES "i386")
+    set(dpdk_env  "x86_x32-native-linuxapp-gcc")
+  else()
+    set(dpdk_env  "x86_64-native-linuxapp-gcc")
+  endif()
+elseif(CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
   set(dpdk_env "x86_64-native-bsdapp-gcc")
-  MESSAGE(STATUS "current platform: FreeBSD")
-else ()
+else()
   set(dpdk_env "x86_64-native-linuxapp-gcc")
-  MESSAGE(STATUS "other platform: ${CMAKE_SYSTEM_NAME}")
 endif (CMAKE_SYSTEM_NAME MATCHES "Linux")
 
 include(ExternalProject) 
 ExternalProject_Add(build_dpdk
   SOURCE_DIR ${CMAKE_SOURCE_DIR}/src/spdk/dpdk
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND make EXTRA_CFLAGS=-fPIC
+  BUILD_COMMAND $(MAKE) EXTRA_CFLAGS=-fPIC
   BUILD_IN_SOURCE 1
   INSTALL_COMMAND "")                                                                                                                                                                        
 
 ExternalProject_Add_Step(build_dpdk before_build
   DEPENDERS configure
   DEPENDERS build
-  COMMAND make config T=${dpdk_env}
+  COMMAND $(MAKE) config T=${dpdk_env}
   WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}/src/spdk/dpdk
   ALWAYS 1)
