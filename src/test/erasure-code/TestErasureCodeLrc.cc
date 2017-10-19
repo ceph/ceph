fdc01838715cbf0e2a5bb2d327b743bd0ab5d849
Kefu Chai <kchai@redhat.com>
2017-10-19 19:28:49 +0800

    mon/OSDMonitor: more options in new-style
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/test/erasure-code/TestErasureCodeLrc.cc
+++ src/test/erasure-code/TestErasureCodeLrc.cc
@@ -615,7 +615,7 @@ TEST(ErasureCodeLrc, encode_decode)
   profile["layers"] = description_string;
   EXPECT_EQ(0, lrc.init(profile, &cerr));
   EXPECT_EQ(4U, lrc.get_data_chunk_count());
-  unsigned int chunk_size = g_conf->osd_pool_erasure_code_stripe_unit;
+  unsigned int chunk_size = g_conf->get_val<uint64_t>("osd_pool_erasure_code_stripe_unit");
   unsigned int stripe_width = lrc.get_data_chunk_count() * chunk_size;
   EXPECT_EQ(chunk_size, lrc.get_chunk_size(stripe_width));
   set<int> want_to_encode;
@@ -745,7 +745,7 @@ TEST(ErasureCodeLrc, encode_decode_2)
   profile["layers"] = description_string;
   EXPECT_EQ(0, lrc.init(profile, &cerr));
   EXPECT_EQ(4U, lrc.get_data_chunk_count());
-  unsigned int chunk_size = g_conf->osd_pool_erasure_code_stripe_unit;
+  unsigned int chunk_size = g_conf->get_val<uint64_t>("osd_pool_erasure_code_stripe_unit");
   unsigned int stripe_width = lrc.get_data_chunk_count() * chunk_size;
   EXPECT_EQ(chunk_size, lrc.get_chunk_size(stripe_width));
   set<int> want_to_encode;
