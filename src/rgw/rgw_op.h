9c0abe6d638c92f1f29798afa846f8a80fa64814
Enming Zhang <enming.zhang@umcloud.com>
2017-10-25 02:36:47 +0800

    rgw: lc support Content-MD5 in request headers
    According to AWS S3, this header must be used
    as a message integrity check to verify that
    the request body was not corrupted in transit.
    
    Content-MD5 is the base64-encoded 128-bit MD5
    digest of the data
    
    Signed-off-by: Enming Zhang <enming.zhang@umcloud.com>

--- src/rgw/rgw_op.h
+++ src/rgw/rgw_op.h
@@ -1412,12 +1412,14 @@ class RGWPutLC : public RGWOp {
 protected:
   int len;
   char *data;
+  const char *content_md5;
   string cookie;
 
 public:
   RGWPutLC() {
     len = 0;
-    data = NULL;
+    data = nullptr;
+    content_md5 = nullptr;
   }
   ~RGWPutLC() override {
     free(data);
