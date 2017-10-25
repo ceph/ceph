8960e42ae07acce1bdbc79d3523fe76f3b64d3ef
Jianpeng Ma <jianpeng.ma@intel.com>
2017-10-26 00:03:36 +0800

    msg/async: batch handle out_seq.
    Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

--- src/msg/async/AsyncConnection.cc
+++ src/msg/async/AsyncConnection.cc
@@ -1940,13 +1940,13 @@ void AsyncConnection::requeue_sent()
     return;
 
   list<pair<bufferlist, Message*> >& rq = out_q[CEPH_MSG_PRIO_HIGHEST];
+  out_seq -= sent.size();
   while (!sent.empty()) {
     Message* m = sent.back();
     sent.pop_back();
     ldout(async_msgr->cct, 10) << __func__ << " " << *m << " for resend "
                                << " (" << m->get_seq() << ")" << dendl;
     rq.push_front(make_pair(bufferlist(), m));
-    out_seq--;
   }
 }
 
