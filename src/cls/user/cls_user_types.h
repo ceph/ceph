440fb101f37b089fa0cb14e5b8f74aedcab051d7
Casey Bodley <cbodley@redhat.com>
2017-10-31 16:56:01 -0400

    rgw: remove placement_rule from cls_user_bucket_entry
    placement_rule is no longer needed in cls_user_bucket_entry, because the
    only time that it's needed, we can read it from the bucket instance in
    RGWRados::update_containers_stats()
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/cls/user/cls_user_types.h
+++ src/cls/user/cls_user_types.h
@@ -105,15 +105,10 @@ struct cls_user_bucket_entry {
   uint64_t count;
   bool user_stats_sync;
 
-  /* The placement_rule is necessary to calculate per-storage-policy statics
-   * of the Swift API. Although the info available in RGWBucketInfo, we need
-   * to duplicate it here to not affect the performance of buckets listing. */
-  std::string placement_rule;
-
   cls_user_bucket_entry() : size(0), size_rounded(0), count(0), user_stats_sync(false) {}
 
   void encode(bufferlist& bl) const {
-    ENCODE_START(8, 5, bl);
+    ENCODE_START(9, 5, bl);
     uint64_t s = size;
     __u32 mt = ceph::real_clock::to_time_t(creation_time);
     string empty_str;  // originally had the bucket name here, but we encode bucket later
@@ -126,11 +121,11 @@ struct cls_user_bucket_entry {
     ::encode(s, bl);
     ::encode(user_stats_sync, bl);
     ::encode(creation_time, bl);
-    ::encode(placement_rule, bl);
+    //::encode(placement_rule, bl); removed in v9
     ENCODE_FINISH(bl);
   }
   void decode(bufferlist::iterator& bl) {
-    DECODE_START_LEGACY_COMPAT_LEN(6, 5, 5, bl);
+    DECODE_START_LEGACY_COMPAT_LEN(9, 5, 5, bl);
     __u32 mt;
     uint64_t s;
     string empty_str;  // backward compatibility
@@ -152,8 +147,10 @@ struct cls_user_bucket_entry {
       ::decode(user_stats_sync, bl);
     if (struct_v >= 7)
       ::decode(creation_time, bl);
-    if (struct_v >= 8)
+    if (struct_v == 8) { // added in v8, removed in v9
+      std::string placement_rule;
       ::decode(placement_rule, bl);
+    }
     DECODE_FINISH(bl);
   }
   void dump(Formatter *f) const;
