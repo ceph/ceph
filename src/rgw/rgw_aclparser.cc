9947db1ff3e6f39d76fe73eb77c9f9f080e0aa4f
gaosibei <gaosb@inspur.com>
2017-11-02 16:31:33 +0800

    rgw: fix null pointer exception
    Signed-off-by: Sibei Gao  <gaosb@inspur.com>

--- src/rgw/rgw_aclparser.cc
+++ src/rgw/rgw_aclparser.cc
@@ -43,18 +43,17 @@ int main(int argc, char **argv) {
     string id="79a59df900b949e55d96a1e698fbacedfd6e09d98eacf8f8d5218e7cd47ef2be";
     dout(10) << hex << policy->get_perm(g_ceph_context, id, RGW_PERM_ALL) << dec << dendl;
     policy->to_xml(cout);
+    cout << parser.get_xml() << endl;
+
+    bufferlist bl;
+    policy->encode(bl);
+
+    RGWAccessControlPolicy newpol;
+    bufferlist::iterator iter = bl.begin();
+    newpol.decode(iter);
+
+    newpol.to_xml(cout);
   }
-
-  cout << parser.get_xml() << endl;
-
-  bufferlist bl;
-  policy->encode(bl);
-
-  RGWAccessControlPolicy newpol;
-  bufferlist::iterator iter = bl.begin();
-  newpol.decode(iter);
-
-  newpol.to_xml(cout);
 
   exit(0);
 }
