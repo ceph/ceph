b08ae658e8d5b1516ad7b91f924c290265a7b5fe
Adam Kupczyk <akupczyk@redhat.com>
2017-11-05 21:18:28 +0100

    common/log: Added new version of PrebufferedStreambuf, tuned for log usage
    Signed-off-by: Adam Kupczyk <akupczyk@redhat.com>
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/log/Entry.h
+++ src/log/Entry.h
@@ -4,7 +4,7 @@
 #ifndef __CEPH_LOG_ENTRY_H
 #define __CEPH_LOG_ENTRY_H
 
-#include "common/PrebufferedStreambuf.h"
+#include "common/CachedPrebufferedStreambuf.h"
 #include <pthread.h>
 #include <string>
 #include "log/LogClock.h"
@@ -19,49 +19,43 @@ struct Entry {
   short m_prio, m_subsys;
   Entry *m_next;
 
-  PrebufferedStreambuf m_streambuf;
   size_t m_buf_len;
   size_t* m_exp_len;
   char m_static_buf[1];
 
+  prebuffered_data m_data;
+  CachedPrebufferedStreambuf* m_streambuf;
+
   Entry()
-    : m_thread(0), m_prio(0), m_subsys(0),
-      m_next(NULL),
-      m_streambuf(m_static_buf, sizeof(m_static_buf)),
-      m_buf_len(sizeof(m_static_buf)),
-      m_exp_len(NULL)
+    : Entry(log_time{}, 0, 0, 0, nullptr)
   {}
   Entry(log_time s, pthread_t t, short pr, short sub,
-  const char *msg = NULL)
-      : m_stamp(s), m_thread(t), m_prio(pr), m_subsys(sub),
-        m_next(NULL),
-        m_streambuf(m_static_buf, sizeof(m_static_buf)),
-        m_buf_len(sizeof(m_static_buf)),
-        m_exp_len(NULL)
-    {
-      if (msg) {
-	std::ostream os(&m_streambuf);
-        os << msg;
-      }
-    }
+	const char *msg = NULL)
+    : Entry(s, t, pr, sub, m_static_buf, sizeof(m_static_buf), nullptr,
+	    msg)
+  {}
   Entry(log_time s, pthread_t t, short pr, short sub, char* buf, size_t buf_len, size_t* exp_len,
 	const char *msg = NULL)
     : m_stamp(s), m_thread(t), m_prio(pr), m_subsys(sub),
       m_next(NULL),
-      m_streambuf(buf, buf_len),
       m_buf_len(buf_len),
-      m_exp_len(exp_len)
+      m_exp_len(exp_len),
+      m_data(buf, buf_len),
+      m_streambuf(CachedPrebufferedStreambuf::create(&m_data))
   {
     if (msg) {
-      std::ostream os(&m_streambuf);
-      os << msg;
+      get_ostream() << msg;
     }
+  }
+
+  std::ostream& get_ostream() {
+    return m_streambuf->get_ostream();
   }
 
   // function improves estimate for expected size of message
   void hint_size() {
     if (m_exp_len != NULL) {
-      size_t size = m_streambuf.size();
+      size_t size = m_data.size();
       if (size > __atomic_load_n(m_exp_len, __ATOMIC_RELAXED)) {
         //log larger then expected, just expand
         __atomic_store_n(m_exp_len, size + 10, __ATOMIC_RELAXED);
@@ -74,22 +68,25 @@ struct Entry {
   }
 
   void set_str(const std::string &s) {
-    std::ostream os(&m_streambuf);
-    os << s;
+    get_ostream() << s;
   }
 
   std::string get_str() const {
-    return m_streambuf.get_str();
+    return m_data.get_str();
   }
 
   // returns current size of content
   size_t size() const {
-    return m_streambuf.size();
+    return m_data.size();
   }
 
   // extracts up to avail chars of content
   int snprintf(char* dst, size_t avail) const {
-    return m_streambuf.snprintf(dst, avail);
+    return m_data.snprintf(dst, avail);
+  }
+
+  void finish() {
+    m_streambuf->finish();
   }
 };
 
