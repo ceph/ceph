9142833bec8adf3d4e0355c796d5c5f5c5fa115c
David Zafman <dzafman@redhat.com>
2017-11-14 13:00:06 -0800

    osd: cleanup: Remove unused const vars
    Signed-off-by: David Zafman <dzafman@redhat.com>
    (cherry picked from commit 9a66a6f517ea0c9c7963b2f3127f5efb78aea346)

--- src/osd/ECUtil.h
+++ src/osd/ECUtil.h
@@ -23,11 +23,6 @@
 
 namespace ECUtil {
 
-const uint64_t CHUNK_ALIGNMENT = 64;
-const uint64_t CHUNK_INFO = 8;
-const uint64_t CHUNK_PADDING = 8;
-const uint64_t CHUNK_OVERHEAD = 16; // INFO + PADDING
-
 class stripe_info_t {
   const uint64_t stripe_width;
   const uint64_t chunk_size;
