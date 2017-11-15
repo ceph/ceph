2339684467d739e6ba1dbbe939c5c5ba16fc807f
Sage Weil <sage@redhat.com>
2017-11-15 11:02:33 -0600

    os/bluestore: simplify Onode tracking for removed object
    Make the only caller of removed() not need to call note_modified_object
    separately, dropping the unneeded erase() call.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -10742,10 +10742,9 @@ int BlueStore::_do_remove(
     );
   }
   txc->t->rmkey(PREFIX_OBJ, o->key.c_str(), o->key.size());
-  txc->removed(o);
+  txc->note_removed_object(o);
   o->extent_map.clear();
   o->onode = bluestore_onode_t();
-  txc->note_modified_object(o);
   _debug_obj_on_delete(o->oid);
 
   if (!is_gen || maybe_unshared_blobs.empty()) {
