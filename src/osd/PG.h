64f6b63b7e73b4dfb737d9450891bb1b438031e0
Sage Weil <sage@redhat.com>
2017-11-16 08:57:13 -0600

    osd/PG: drop waiting_on, use waiting_on_whom
    Signed-off-by: Sage Weil <sage@redhat.com>
    (cherry picked from commit 004ee202fac7a2f3fba2b018426474eeae7f913b)
    
    Add changes to PG::sub_op_scrub_map() which exists in Luminous

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -1211,7 +1211,6 @@ public:
 
     // common to both scrubs
     bool active;
-    int waiting_on;
     set<pg_shard_t> waiting_on_whom;
     int shallow_errors;
     int deep_errors;
@@ -1313,7 +1312,6 @@ public:
     // clear all state
     void reset() {
       active = false;
-      waiting_on = 0;
       waiting_on_whom.clear();
       if (active_rep_scrub) {
         active_rep_scrub = OpRequestRef();
