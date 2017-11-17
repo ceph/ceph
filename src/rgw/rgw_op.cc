b2b7385f194def1025a8947bab876c9856b06400
Adam C. Emerson <aemerson@redhat.com>
2017-11-17 16:03:13 -0500

    rgw: Handle stale bucket info in RGWSetBucketWebsite
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -2153,10 +2153,14 @@ void RGWSetBucketWebsite::execute()
     }
   }
 
-  s->bucket_info.has_website = true;
-  s->bucket_info.website_conf = website_conf;
+  op_ret = retry_raced_bucket_write(store, s, [this] {
+      s->bucket_info.has_website = true;
+      s->bucket_info.website_conf = website_conf;
+      op_ret = store->put_bucket_instance_info(s->bucket_info, false,
+					       real_time(), &s->bucket_attrs);
+      return op_ret;
+    });
 
-  op_ret = store->put_bucket_instance_info(s->bucket_info, false, real_time(), &s->bucket_attrs);
   if (op_ret < 0) {
     ldout(s->cct, 0) << "NOTICE: put_bucket_info on bucket=" << s->bucket.name << " returned err=" << op_ret << dendl;
     return;
