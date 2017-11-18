344bd008f3d0dcbb36ec40f6cda6cec5db789123
Shinobu Kinjo <shinobu@redhat.com>
2017-11-18 11:19:30 -0500

    do_cmake.sh: Drop `hopefully`, pick a port cleverly
    Signed-off-by: Shinobu Kinjo <shinobu@redhat.com>

--- do_cmake.sh
+++ do_cmake.sh
@@ -22,7 +22,16 @@ plugin dir = lib
 erasure code dir = lib
 EOF
 
-# give vstart a (hopefully) unique mon port to start with
-echo $(( RANDOM % 1000 + 40000 )) > .ceph_port
+# give vstart a unique mon port to start with
+while [ true ]
+do
+  PORT=`echo $(( RANDOM % 1000 + 40000 ))`
+  ss | awk '{print $5}' | awk -F : '{print $2}' | grep ^[0-9] | grep ${PORT}
+  if [ "$?" -ne 0 ]
+  then
+    echo ${PORT} > .ceph_port
+    break
+  fi
+done
 
 echo done.
