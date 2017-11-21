5d8043698ae3eebb23b7ecabeeb3dc7850de78d4
Yehuda Sadeh <yehuda@redhat.com>
2017-11-21 15:11:34 -0800

    rgw: initial work to add otp as metadata provider
    To allow transparent multisite sync
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

---
+++ src/rgw/rgw_otp.h
@@ -0,0 +1,12 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef CEPH_RGW_OTP_H
+#define CEPH_RGW_OTP_H
+
+class RGWRados;
+
+void rgw_otp_init(RGWRados *store);
+
+#endif
+
