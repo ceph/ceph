0bf1fab7314abb20194d79bf666908e47fe3e775
Yehuda Sadeh <yehuda@redhat.com>
2017-11-22 15:49:32 -0800

    cls/otp: client api to set multiple keys
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/cls/otp/cls_otp_client.h
+++ src/cls/otp/cls_otp_client.h
@@ -17,7 +17,8 @@ namespace rados {
 
       class OTP {
       public:
-	static void create(librados::ObjectWriteOperation *op, const otp_info_t& config);
+        static void create(librados::ObjectWriteOperation *op, const otp_info_t& config);
+        static void set(librados::ObjectWriteOperation *op, const list<otp_info_t>& entries);
         static void remove(librados::ObjectWriteOperation *op, const string& id);
         static int get(librados::IoCtx& ioctx, const string& oid,
                        const list<string> *ids, bool get_all, list<otp_info_t> *result);
