313bb95c22cc056c4c5599f33b2c825ee480b717
Shinobu Kinjo <shinobu@redhat.com>
2017-11-24 21:05:41 -0500

    script/kcon_all: Add logging and disable dynamic debug
    Signed-off-by: Shinobu Kinjo <shinobu@redhat.com>

--- src/script/kcon_all.sh
+++ src/script/kcon_all.sh
@@ -8,3 +8,13 @@ echo 9 > /proc/sysrq-trigger
 p 'module ceph +p'
 p 'module libceph +p'
 p 'module rbd +p'
+
+## Logging for 5 min
+dmesg -w > $(hostname)_$(date +%Y%m%d).log
+sleep 300
+
+## disable debug
+echo 0 > /proc/sysrq-trigger
+p 'module ceph -p'
+p 'module libceph -p'
+p 'module rbd -p'
