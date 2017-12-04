bdba58f8f25c298cda0195f89a47fd7f11ce1be5
Adam C. Emerson <aemerson@redhat.com>
2017-12-04 15:30:01 -0500

    rgw: Handle stale bucket info in RGWDeleteBucketWebsite
    Fixes: http://tracker.ceph.com/issues/22517
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>
    (cherry picked from commit f4d274248e43cb38ff2b27782c010b2c35b12b2b)

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -1729,10 +1729,14 @@ void RGWDeleteBucketWebsite::pre_exec()
 
 void RGWDeleteBucketWebsite::execute()
 {
-  s->bucket_info.has_website = false;
-  s->bucket_info.website_conf = RGWBucketWebsiteConf();
+  op_ret = retry_raced_bucket_write(store, s, [this] {
+      s->bucket_info.has_website = false;
+      s->bucket_info.website_conf = RGWBucketWebsiteConf();
 
-  op_ret = store->put_bucket_instance_info(s->bucket_info, false, real_time(), &s->bucket_attrs);
+      op_ret = store->put_bucket_instance_info(s->bucket_info, false, real_time(), &s->bucket_attrs);
+
+      return op_ret;
+    });
   if (op_ret < 0) {
     ldout(s->cct, 0) << "NOTICE: put_bucket_info on bucket=" << s->bucket.name << " returned err=" << op_ret << dendl;
     return;
