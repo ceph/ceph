ef529f758c382d27f6f3dfd5e1a5c1938134d44d
Sage Weil <sage@redhat.com>
2017-12-04 16:37:14 -0600

    vstart.sh: less config noise
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/vstart.sh
+++ src/vstart.sh
@@ -447,13 +447,7 @@ EOF
         lockdep = true
 EOF
 	fi
-	if [ "$cephx" -eq 1 ] ; then
-		wconf <<EOF
-        auth cluster required = cephx
-        auth service required = cephx
-        auth client required = cephx
-EOF
-	else
+	if [ "$cephx" -ne 1 ] ; then
 		wconf <<EOF
 	auth cluster required = none
 	auth service required = none
@@ -533,7 +527,6 @@ $extra_conf
         mon osd allow primary affinity = true
         mon reweight min pgs per osd = 4
         mon osd prime pg temp = true
-        crushtool = $CEPH_BIN/crushtool
         mon allow pool delete = true
 $DAEMONOPTS
 $CMONDEBUG
