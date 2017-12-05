5e8c4f83f4f161a595e75340cecbdd41b1803f39
Mykola Golub <to.my.trociny@gmail.com>
2017-12-05 15:48:58 +0200

    pybind/rbd: raise KeyError when metadata does not exist
    Signed-off-by: Mykola Golub <to.my.trociny@gmail.com>
    (cherry picked from commit 7a1b13f8849ce7bd325316bafad5bab660c77825)

--- src/pybind/rbd/rbd.pyx
+++ src/pybind/rbd/rbd.pyx
@@ -2682,6 +2682,8 @@ written." % (self.name, ret, length))
                     ret = rbd_metadata_get(self.image, _key, value, &size)
                 if ret != -errno.ERANGE:
                     break
+            if ret == -errno.ENOENT:
+                raise KeyError('no metadata %s for image %s' % (key, self.name))
             if ret != 0:
                 raise make_ex(ret, 'error getting metadata %s for image %s' %
                               (key, self.name,))
@@ -2724,6 +2726,8 @@ written." % (self.name, ret, length))
         with nogil:
             ret = rbd_metadata_remove(self.image, _key)
 
+        if ret == -errno.ENOENT:
+            raise KeyError('no metadata %s for image %s' % (key, self.name))
         if ret != 0:
             raise make_ex(ret, 'error removing metadata %s for image %s' %
                           (key, self.name,))
