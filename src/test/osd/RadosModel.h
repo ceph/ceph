651e6cb27dfca44989d5539215a3f177b9cf8669
myoungwon oh <omwmw@sk.com>
2017-12-05 15:54:03 +0900

    src/test: remove version check and add data alignment for chunk_read test
    Signed-off-by: Myoungwon Oh <omwmw@sk.com>

--- src/test/osd/RadosModel.h
+++ src/test/osd/RadosModel.h
@@ -2133,7 +2133,6 @@ public:
 
     for (int i = 0; i < 2; i++) {
       assert(completions[i]->is_complete()); 
-      uint64_t version = completions[i]->get_version64();
       int err = completions[i]->get_return_value();
       if (err != retval) {
         cerr << num << ": Error: oid " << oid << " read returned different error codes: "
@@ -2146,11 +2145,6 @@ public:
                << err << std::endl;
           ceph_abort();
         }
-      }
-      if (version != tgt_value.version) {
-	cerr << num << ": oid " << oid << " version is " << version
-	     << " and expected " << tgt_value.version << std::endl;
-	assert(version == tgt_value.version);
       }
     }
 
