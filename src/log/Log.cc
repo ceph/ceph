c142ae3f1530067f35b8a1e1bac9ddeea4d81c09
Sage Weil <sage@redhat.com>
2017-12-07 16:39:30 -0600

    log: add log_stderr_prefix option
    Allows you to set a prefix for debug log messages send to stderr (e.g.,
    "debug ").
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/log/Log.cc
+++ src/log/Log.cc
@@ -133,6 +133,11 @@ void Log::set_log_file(string fn)
   m_log_file = fn;
 }
 
+void Log::set_log_stderr_prefix(const std::string& p)
+{
+  m_log_stderr_prefix = p;
+}
+
 void Log::reopen_log_file()
 {
   pthread_mutex_lock(&m_flush_mutex);
@@ -336,7 +341,7 @@ void Log::_flush(EntryQueue *t, EntryQueue *requeue, bool crash)
       }
 
       if (do_stderr) {
-        cerr << buf << std::endl;
+        cerr << m_log_stderr_prefix << buf << std::endl;
       }
       if (do_fd) {
         buf[buflen] = '\n';
