8d75fa70fe8207b91920fe3bdc453c638d93f43a
Neha Ojha <nojha@redhat.com>
2017-12-18 13:25:09 -0800

    osd: rename ECSubWrite parameter backfill to backfill_or_async_recovery
    Signed-off-by: Neha Ojha <nojha@redhat.com>

--- src/osd/ECMsgTypes.cc
+++ src/osd/ECMsgTypes.cc
@@ -30,7 +30,7 @@ void ECSubWrite::encode(bufferlist &bl) const
   encode(temp_removed, bl);
   encode(updated_hit_set_history, bl);
   encode(roll_forward_to, bl);
-  encode(backfill, bl);
+  encode(backfill_or_async_recovery, bl);
   ENCODE_FINISH(bl);
 }
 
@@ -57,10 +57,10 @@ void ECSubWrite::decode(bufferlist::iterator &bl)
     roll_forward_to = trim_to;
   }
   if (struct_v >= 4) {
-    decode(backfill, bl);
+    decode(backfill_or_async_recovery, bl);
   } else {
-    // The old protocol used an empty transaction to indicate backfill
-    backfill = t.empty();
+    // The old protocol used an empty transaction to indicate backfill or async_recovery
+    backfill_or_async_recovery = t.empty();
   }
   DECODE_FINISH(bl);
 }
@@ -75,8 +75,8 @@ std::ostream &operator<<(
       << ", roll_forward_to=" << rhs.roll_forward_to;
   if (rhs.updated_hit_set_history)
     lhs << ", has_updated_hit_set_history";
-  if (rhs.backfill)
-    lhs << ", backfill";
+  if (rhs.backfill_or_async_recovery)
+    lhs << ", backfill_or_async_recovery";
   return lhs <<  ")";
 }
 
@@ -89,7 +89,7 @@ void ECSubWrite::dump(Formatter *f) const
   f->dump_stream("roll_forward_to") << roll_forward_to;
   f->dump_bool("has_updated_hit_set_history",
       static_cast<bool>(updated_hit_set_history));
-  f->dump_bool("backfill", backfill);
+  f->dump_bool("backfill_or_async_recovery", backfill_or_async_recovery);
 }
 
 void ECSubWrite::generate_test_instances(list<ECSubWrite*> &o)
