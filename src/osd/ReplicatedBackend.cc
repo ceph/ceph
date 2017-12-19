ab29d817ca4efe281415bfe6dbb849a8838655a5
Neha Ojha <nojha@redhat.com>
2017-12-19 13:18:25 -0800

    PG: modify should_send_op to handle async recovery
    Signed-off-by: Neha Ojha <nojha@redhat.com>

--- src/osd/ReplicatedBackend.cc
+++ src/osd/ReplicatedBackend.cc
@@ -907,11 +907,6 @@ Message * ReplicatedBackend::generate_subop(
 
   // ship resulting transaction, log entries, and pg_stats
   if (!parent->should_send_op(peer, soid)) {
-    dout(10) << "issue_repop shipping empty opt to osd." << peer
-	     <<", object " << soid
-	     << " beyond std::max(last_backfill_started "
-	     << ", pinfo.last_backfill "
-	     << pinfo.last_backfill << ")" << dendl;
     ObjectStore::Transaction t;
     encode(t, wr->get_data());
   } else {
