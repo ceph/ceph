fae13b615282d863958dfaf8ee039fb2a05b027c
John Gibson <jgibson@mitre.org>
2017-12-24 15:44:54 -0500

    rgw: Fixed several bugs in policies related to IP Addresses.
    Comparisons of two individual IP addresses caused an assertion error.
    The text conversion of IPv4 addresses was using raw byte values instead of
    the converted number.
    NotIpAddress condition now works with multiple values.
    
    http://tracker.ceph.com/issues/20991
    
    Signed-off-by: John Gibson <jgibson@mitre.org>

--- src/rgw/rgw_iam_policy.h
+++ src/rgw/rgw_iam_policy.h
@@ -249,8 +249,9 @@ struct MaskedIP {
 std::ostream& operator <<(std::ostream& m, const MaskedIP& ip);
 
 inline bool operator ==(const MaskedIP& l, const MaskedIP& r) {
-  auto shift = std::max((l.v6 ? 128 : 32) - l.prefix,
-			(r.v6 ? 128 : 32) - r.prefix);
+  auto shift = std::max((l.v6 ? 128 : 32) - ((int) l.prefix),
+			(r.v6 ? 128 : 32) - ((int) r.prefix));
+  ceph_assert(shift >= 0);
   return (l.addr >> shift) == (r.addr >> shift);
 }
 
