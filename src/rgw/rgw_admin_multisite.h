d70a31ac716931c709217b7910bf3abcf948ae98
cache-nez <cache-nez@yandex.ru>
2018-01-07 23:51:07 +0500

    Split rgw_admin.cc: separate helper functions for multisite commands Signed-off-by: Ekaterina Goltsova <cache-nez@yandex.ru>

---
+++ src/rgw/rgw_admin_multisite.h
@@ -0,0 +1,39 @@
+//
+// Created by cache-nez on 07.01.18.
+//
+
+#ifndef CEPH_RGW_ADMIN_MULTISITE_H
+#define CEPH_RGW_ADMIN_MULTISITE_H
+
+#include <boost/optional.hpp>
+#include "rgw_rest_conn.h"
+
+// Period, realm, zone , zonegroup , data sync, metadata sync
+
+/// search each zonegroup for a connection
+boost::optional<RGWRESTConn> get_remote_conn(RGWRados *store,
+                                                    const RGWPeriodMap& period_map,
+                                                    const std::string& remote);
+
+int send_to_url(const string& url, const string& access,
+                       const string& secret, req_info& info,
+                       bufferlist& in_data, JSONParser& parser);
+
+int send_to_remote_or_url(RGWRESTConn *conn, const string& url,
+                                 const string& access, const string& secret,
+                                 req_info& info, bufferlist& in_data,
+                                 JSONParser& parser);
+
+int commit_period(RGWRados *store, RGWRealm& realm, RGWPeriod& period,
+                         string remote, const string& url,
+                         const string& access, const string& secret,
+                         bool force);
+
+int update_period(RGWRados *store, const string& realm_id, const string& realm_name,
+                         const string& period_id, const string& period_epoch,
+                         bool commit, const string& remote, const string& url,
+                         const string& access, const string& secret,
+                         Formatter *formatter, bool force);
+
+
+#endif //CEPH_RGW_ADMIN_MULTISITE_H
