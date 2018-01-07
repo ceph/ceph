01c80e94a7acbb0452c5b3e2471df5e8e826f7ff
cache-nez <cache-nez@yandex.ru>
2018-01-08 00:04:27 +0500

    Split rgw_admin.cc: pass arguments by const reference instead of by value

--- src/rgw/rgw_admin_multisite.h
+++ src/rgw/rgw_admin_multisite.h
@@ -11,29 +11,21 @@
 // Period, realm, zone , zonegroup , data sync, metadata sync
 
 /// search each zonegroup for a connection
-boost::optional<RGWRESTConn> get_remote_conn(RGWRados *store,
-                                                    const RGWPeriodMap& period_map,
-                                                    const std::string& remote);
+boost::optional<RGWRESTConn> get_remote_conn(RGWRados *store, const RGWPeriodMap& period_map,
+                                             const std::string& remote);
 
-int send_to_url(const string& url, const string& access,
-                       const string& secret, req_info& info,
-                       bufferlist& in_data, JSONParser& parser);
+int send_to_url(const string& url, const string& access, const string& secret, req_info& info, bufferlist& in_data,
+                JSONParser& parser);
 
-int send_to_remote_or_url(RGWRESTConn *conn, const string& url,
-                                 const string& access, const string& secret,
-                                 req_info& info, bufferlist& in_data,
-                                 JSONParser& parser);
+int send_to_remote_or_url(RGWRESTConn *conn, const string& url, const string& access, const string& secret,
+                          req_info& info, bufferlist& in_data, JSONParser& parser);
 
-int commit_period(RGWRados *store, RGWRealm& realm, RGWPeriod& period,
-                         string remote, const string& url,
-                         const string& access, const string& secret,
-                         bool force);
+int commit_period(RGWRados *store, RGWRealm& realm, RGWPeriod& period, string remote, const string& url,
+                  const string& access, const string& secret, bool force);
 
-int update_period(RGWRados *store, const string& realm_id, const string& realm_name,
-                         const string& period_id, const string& period_epoch,
-                         bool commit, const string& remote, const string& url,
-                         const string& access, const string& secret,
-                         Formatter *formatter, bool force);
+int update_period(RGWRados *store, const string& realm_id, const string& realm_name, const string& period_id,
+                  const string& period_epoch, bool commit, const string& remote, const string& url,
+                  const string& access, const string& secret, Formatter *formatter, bool force);
 
 
 #endif //CEPH_RGW_ADMIN_MULTISITE_H
