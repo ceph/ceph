4cf8601b03c8af6570785ce14c6d65b69222ed25
cache-nez <cache-nez@yandex.ru>
2018-01-08 12:00:00 +0500

    Split rgw_admin.cc: move widely used functions to rgw_admin_common

--- src/rgw/rgw_admin_argument_parsing.h
+++ src/rgw/rgw_admin_argument_parsing.h
@@ -218,9 +218,4 @@ int parse_commandline_parameters(vector<const char*>& args, rgw_user& user_id, s
                                  string& assume_role_doc, string& policy_name, string& perm_policy_doc,
                                  string& path_prefix);
 
-int read_input(const string& infile, bufferlist& bl);
-
-int init_bucket(RGWRados *store, const string& tenant_name, const string& bucket_name, const string& bucket_id,
-                RGWBucketInfo& bucket_info, rgw_bucket& bucket, map<string, bufferlist> *pattrs = nullptr);
-
 #endif //CEPH_RGW_ADMIN_COMMON_H
