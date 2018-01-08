2f4f2f4942239e48097b551b8b32584b1eb22c4b
cache-nez <cache-nez@yandex.ru>
2018-01-08 12:00:00 +0500

    Split rgw_admin.cc: move widely used functions to rgw_admin_common Signed-off-by: Ekaterina Goltsova <cache-nez@yandex.ru>

---
+++ src/rgw/rgw_admin_common.h
@@ -0,0 +1,18 @@
+//
+// Created by cache-nez on 08.01.18.
+//
+
+#ifndef CEPH_RGW_ADMIN_COMMON_H
+#define CEPH_RGW_ADMIN_COMMON_H
+
+#include "cls/rgw/cls_rgw_types.h"
+
+#include "rgw_common.h"
+#include "rgw_rados.h"
+
+int read_input(const string& infile, bufferlist& bl);
+
+int init_bucket(RGWRados *store, const string& tenant_name, const string& bucket_name, const string& bucket_id,
+                RGWBucketInfo& bucket_info, rgw_bucket& bucket, map<string, bufferlist> *pattrs = nullptr);
+
+#endif //CEPH_RGW_ADMIN_COMMON_H
