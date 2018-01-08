ccc5d97462363078c973a559f304c37c6c749096
cache-nez <cache-nez@yandex.ru>
2018-01-08 11:45:05 +0500

    Split rgw_admin.cc: separate argument parsing

--- src/rgw/rgw_admin_argument_parsing.cc
+++ src/rgw/rgw_admin_argument_parsing.cc
@@ -1,7 +1,7 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
 // vim: ts=8 sw=2 smarttab
 
-#include "rgw_admin_common.h"
+#include "rgw_admin_argument_parsing.h"
 
 #include <common/safe_io.h>
 
