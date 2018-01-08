5fa1a3eb208758cf365be31bf69436fe14ed5e00
Sage Weil <sage@redhat.com>
2018-01-08 16:27:51 -0600

    os/bluestore: fix data read error injection in bluestore
    Signed-off-by: Sage Weil <sage@redhat.com>
    (cherry picked from commit be32d15a04d9d900f604aa366e82791249f1bdb2)

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -6452,7 +6452,7 @@ int BlueStore::read(
   }
 
  out:
-  if (r == 0 && _debug_data_eio(oid)) {
+  if (r >= 0 && _debug_data_eio(oid)) {
     r = -EIO;
     derr << __func__ << " " << c->cid << " " << oid << " INJECT EIO" << dendl;
   } else if (cct->_conf->bluestore_debug_random_read_err &&
