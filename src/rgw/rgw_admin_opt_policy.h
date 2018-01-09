2f8b5bd670b30e9759834c6b6e1da80b1f2ea2e8
cache-nez <cache-nez@yandex.ru>
2018-01-09 13:47:33 +0500

    Split rgw_admin.cc: move handling of OPT_ROLE_* to a dedicated file Signed-off-by: Ekaterina Goltsova <cache-nez@yandex.ru>

--- src/rgw/rgw_admin_opt_policy.h
+++ src/rgw/rgw_admin_opt_policy.h
@@ -1,21 +1,38 @@
 #ifndef CEPH_RGW_ADMIN_OPT_POLICY_H
 #define CEPH_RGW_ADMIN_OPT_POLICY_H
 
-#include "rgw_rest_conn.h"
 
 #include "rgw_rest_conn.h"
 #include "rgw_realm_watcher.h"
 
 #include "common/ceph_json.h"
 #include "rgw_role.h"
+#include "rgw_admin_common.h"
 
+int handle_opt_role_get(const string& role_name, const string& tenant, CephContext *context,
+                        RGWRados *store, Formatter *formatter);
 
-void show_perm_policy(const string& perm_policy, Formatter* formatter);
+int handle_opt_role_create(const string& role_name, const string& assume_role_doc, const string& path,
+                           const string& tenant, CephContext *context, RGWRados *store, Formatter *formatter);
 
-void show_policy_names(const std::vector<string>& policy_names, Formatter* formatter);
+int handle_opt_role_delete(const string& role_name, const string& tenant, CephContext *context, RGWRados *store);
 
-void show_role_info(RGWRole& role, Formatter* formatter);
+int handle_opt_role_modify(const string& role_name, const string& assume_role_doc, const string& tenant,
+                           CephContext *context, RGWRados *store);
 
-void show_roles_info(const vector<RGWRole>& roles, Formatter* formatter);
+int handle_opt_role_list(const string& path_prefix, const string& tenant, CephContext *context,
+                         RGWRados *store, Formatter *formatter);
+
+int handle_opt_role_policy_put(const string& role_name, const string& policy_name, const string& perm_policy_doc,
+                               const string& tenant, CephContext *context, RGWRados *store);
+
+int handle_opt_role_policy_list(const string& role_name, const string& tenant, CephContext *context, 
+                                RGWRados *store, Formatter *formatter);
+
+int handle_opt_role_policy_get(const string& role_name, const string& policy_name, const string& tenant,
+                               CephContext *context, RGWRados *store, Formatter *formatter);
+
+int handle_opt_role_policy_delete(const string& role_name, const string& policy_name, const string& tenant,
+                           CephContext *context, RGWRados *store);
 
 #endif //CEPH_RGW_ADMIN_OPT_POLICY_H
