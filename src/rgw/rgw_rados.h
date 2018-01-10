0ce76720f7e4fc01741606bb44d6ccc60ee37416
Casey Bodley <cbodley@redhat.com>
2018-01-10 09:37:02 -0500

    rgw: rgw_put_system_obj takes bufferlist
    all callers were converting from bufferlist to const char*, then
    RGWRados::put_system_obj() copied that back into a new bufferlist
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -3050,16 +3050,14 @@ public:
   int aio_put_obj_data(void *ctx, rgw_raw_obj& obj, bufferlist& bl,
                         off_t ofs, bool exclusive, void **handle);
 
-  int put_system_obj(void *ctx, rgw_raw_obj& obj, const char *data, size_t len, bool exclusive,
+  int put_system_obj(void *ctx, rgw_raw_obj& obj, bufferlist& data, bool exclusive,
               ceph::real_time *mtime, map<std::string, bufferlist>& attrs, RGWObjVersionTracker *objv_tracker,
               ceph::real_time set_mtime) {
-    bufferlist bl;
-    bl.append(data, len);
     int flags = PUT_OBJ_CREATE;
     if (exclusive)
       flags |= PUT_OBJ_EXCL;
 
-    return put_system_obj_impl(obj, len, mtime, attrs, flags, bl, objv_tracker, set_mtime);
+    return put_system_obj_impl(obj, data.length(), mtime, attrs, flags, data, objv_tracker, set_mtime);
   }
   int aio_wait(void *handle);
   bool aio_completed(void *handle);
