8a6a1dd7a2acaf1b6bb26532a5f574a1257bec8f
Sage Weil <sage@redhat.com>
2018-01-17 11:22:55 -0600

    use stringsream::str(), not rdbuf()
    Fixes: http://tracker.ceph.com/issues/22715
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/common/admin_socket.cc
+++ src/common/admin_socket.cc
@@ -365,7 +365,7 @@ bool AdminSocket::do_accept()
   stringstream errss;
   cmdvec.push_back(cmd);
   if (!cmdmap_from_json(cmdvec, &cmdmap, errss)) {
-    ldout(m_cct, 0) << "AdminSocket: " << errss.rdbuf() << dendl;
+    ldout(m_cct, 0) << "AdminSocket: " << errss.str() << dendl;
     VOID_TEMP_FAILURE_RETRY(close(connection_fd));
     return false;
   }
