629152752bd7baffa90113333a3f96bf99a7e975
cache-nez <cache-nez@yandex.ru>
2018-01-18 17:35:25 +0500

    Split rgw_admin.cc: separate handling of period- and realm-related commands Signed-off-by: Ekaterina Goltsova <cache-nez@yandex.ru>

--- src/rgw/rgw_admin_multisite.h
+++ src/rgw/rgw_admin_multisite.h
@@ -31,131 +31,52 @@ int do_period_pull(RGWRados *store, RGWRESTConn *remote_conn, const string& url,
                    const string& period_id, const string& period_epoch,
                    RGWPeriod *period);
 
-int handle_opt_period_delete(const string& period_id, CephContext *context, RGWRados *store)
-{
-  if (period_id.empty()) {
-    cerr << "missing period id" << std::endl;
-    return EINVAL;
-  }
-  RGWPeriod period(period_id);
-  int ret = period.init(context, store);
-  if (ret < 0) {
-    cerr << "period.init failed: " << cpp_strerror(-ret) << std::endl;
-    return -ret;
-  }
-  ret = period.delete_obj();
-  if (ret < 0) {
-    cerr << "ERROR: couldn't delete period: " << cpp_strerror(-ret) << std::endl;
-    return -ret;
-  }
-  return 0;
-}
+int handle_opt_period_delete(const string& period_id, CephContext *context, RGWRados *store);
 
 int handle_opt_period_get(const string& period_epoch, string& period_id, bool staging, string& realm_id,
-                          string& realm_name, CephContext *context, RGWRados *store, Formatter *formatter)
-{
-  epoch_t epoch = 0;
-  if (!period_epoch.empty()) {
-    epoch = atoi(period_epoch.c_str());
-  }
-  if (staging) {
-    RGWRealm realm(realm_id, realm_name);
-    int ret = realm.init(context, store);
-    if (ret < 0 ) {
-      cerr << "Error initializing realm " << cpp_strerror(-ret) << std::endl;
-      return -ret;
-    }
-    realm_id = realm.get_id();
-    realm_name = realm.get_name();
-    period_id = RGWPeriod::get_staging_id(realm_id);
-    epoch = 1;
-  }
-  RGWPeriod period(period_id, epoch);
-  int ret = period.init(context, store, realm_id, realm_name);
-  if (ret < 0) {
-    cerr << "period init failed: " << cpp_strerror(-ret) << std::endl;
-    return -ret;
-  }
-  encode_json("period", period, formatter);
-  formatter->flush(cout);
-  return 0;
-}
+                          string& realm_name, CephContext *context, RGWRados *store, Formatter *formatter);
 
-int handle_opt_period_get_current(const string& realm_id, const string& realm_name, RGWRados *store, Formatter *formatter)
-{
-  string period_id;
-  int ret = read_current_period_id(store, realm_id, realm_name, &period_id);
-  if (ret < 0) {
-    return -ret;
-  }
-  formatter->open_object_section("period_get_current");
-  encode_json("current_period", period_id, formatter);
-  formatter->close_section();
-  formatter->flush(cout);
-  return 0;
-}
+int handle_opt_period_get_current(const string& realm_id, const string& realm_name, RGWRados *store, Formatter *formatter);
 
-int handle_opt_period_list(RGWRados *store, Formatter *formatter)
-{
-  list<string> periods;
-  int ret = store->list_periods(periods);
-  if (ret < 0) {
-    cerr << "failed to list periods: " << cpp_strerror(-ret) << std::endl;
-    return -ret;
-  }
-  formatter->open_object_section("periods_list");
-  encode_json("periods", periods, formatter);
-  formatter->close_section();
-  formatter->flush(cout);
-  return 0;
-}
+int handle_opt_period_list(RGWRados *store, Formatter *formatter);
 
 int handle_opt_period_pull(const string& period_id, const string& period_epoch, const string& realm_id,
                            const string& realm_name, const string& url, const string& access_key, const string& secret_key,
-                           string& remote, CephContext *context, RGWRados *store, Formatter *formatter)
-{
-  boost::optional<RGWRESTConn> conn;
-  RGWRESTConn *remote_conn = nullptr;
-  if (url.empty()) {
-    // load current period for endpoints
-    RGWRealm realm(realm_id, realm_name);
-    int ret = realm.init(g_ceph_context, store);
-    if (ret < 0) {
-      cerr << "failed to init realm: " << cpp_strerror(-ret) << std::endl;
-      return -ret;
-    }
-    RGWPeriod current_period(realm.get_current_period());
-    ret = current_period.init(g_ceph_context, store);
-    if (ret < 0) {
-      cerr << "failed to init current period: " << cpp_strerror(-ret) << std::endl;
-      return -ret;
-    }
-    if (remote.empty()) {
-      // use realm master zone as remote
-      remote = current_period.get_master_zone();
-    }
-    conn = get_remote_conn(store, current_period.get_map(), remote);
-    if (!conn) {
-      cerr << "failed to find a zone or zonegroup for remote "
-           << remote << std::endl;
-      return -ENOENT;
-    }
-    remote_conn = &*conn;
-  }
+                           string& remote, CephContext *context, RGWRados *store, Formatter *formatter);
 
-  RGWPeriod period;
-  int ret = do_period_pull(store, remote_conn, url, access_key, secret_key,
-                           realm_id, realm_name, period_id, period_epoch,
-                           &period);
-  if (ret < 0) {
-    cerr << "period pull failed: " << cpp_strerror(-ret) << std::endl;
-    return -ret;
-  }
+int handle_opt_period_push(const string& period_id, const string& period_epoch, const string& realm_id,
+                           const string& realm_name, const string& url, const string& access_key, const string& secret_key,
+                           CephContext *context, RGWRados *store);
 
-  encode_json("period", period, formatter);
-  formatter->flush(cout);
-  return 0;
-}
+int handle_opt_period_commit(const string& period_id, const string& period_epoch, const string& realm_id,
+                             const string& realm_name, const string& url, const string& access_key,
+                             const string& secret_key, const string& remote, bool yes_i_really_mean_it,
+                             CephContext *context, RGWRados *store, Formatter *formatter);
 
+int handle_opt_realm_create(const string& realm_name, bool set_default, CephContext *context, RGWRados *store,
+                            Formatter *formatter);
+
+int handle_opt_realm_delete(const string& realm_id, const string& realm_name, CephContext *context, RGWRados *store);
+
+int handle_opt_realm_get(const string& realm_id, const string& realm_name, CephContext *context, RGWRados *store,
+                         Formatter *formatter);
+
+int handle_opt_realm_get_default(CephContext *context, RGWRados *store);
+
+int handle_opt_realm_list(CephContext *context, RGWRados *store, Formatter *formatter);
+
+int handle_opt_realm_list_periods(const string& realm_id, const string& realm_name, RGWRados *store, Formatter *formatter);
+
+int handle_opt_realm_rename(const string& realm_id, const string& realm_name, const string& realm_new_name,
+                            CephContext *context, RGWRados *store);
+
+int handle_opt_realm_set(const string& realm_id, const string& realm_name, const string& infile,
+                         bool set_default, CephContext *context, RGWRados *store, Formatter *formatter);
+
+int handle_opt_realm_default(const string& realm_id, const string& realm_name, CephContext *context, RGWRados *store);
+
+int handle_opt_realm_pull(const string& realm_id, const string& realm_name, const string& url, const string& access_key,
+                          const string& secret_key, bool set_default, CephContext *context, RGWRados *store,
+                          Formatter *formatter);
 
 #endif //CEPH_RGW_ADMIN_MULTISITE_H
