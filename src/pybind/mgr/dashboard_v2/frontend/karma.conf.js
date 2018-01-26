d57b092f28a8beffc5e5976f78d7aace594f91d7
Ricardo Dias <rdias@suse.com>
2018-01-26 14:50:46 +0000

    mgr/dashboard_v2: add support for frontend CI coverage and unit testing
    Signed-off-by: Ricardo Dias <rdias@suse.com>

--- src/pybind/mgr/dashboard_v2/frontend/karma.conf.js
+++ src/pybind/mgr/dashboard_v2/frontend/karma.conf.js
@@ -10,19 +10,26 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-phantomjs-launcher'),
+      require('karma-junit-reporter')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
+      reports: [ 'html', 'lcovonly', 'cobertura' ],
       fixWebpackSourcePaths: true
     },
     angularCli: {
       environment: 'dev'
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'junit'],
+    junitReporter: {
+      'outputFile': 'junit.frontend.xml',
+      'suite': 'dashboard_v2',
+      'useBrowserName': false
+    },
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
