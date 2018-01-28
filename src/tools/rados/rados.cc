0159a9042aa191c7bfdfc34b51c665070c74e187
Nathan Cutler <ncutler@suse.com>
2018-01-28 12:15:18 +0100

    tools: rados: drop --force option
    The global -f / --force option was introduced by
    3425a8e5031a4f0c9c0eb85e8a329b02d05b9420 for use in import/export.
    
    After being rewritten by 3425a8e5031a4f0c9c0eb85e8a329b02d05b9420, the
    import/export no longer uses --force.
    
    I examined the rados tool source code and the test_rados_tool.sh
    script, with the conclusion that the rados tool does not use the
    --force option at all. So it's dead code ripe for pruning.
    
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- src/tools/rados/rados.cc
+++ src/tools/rados/rados.cc
@@ -3733,8 +3733,6 @@ int main(int argc, const char **argv)
     } else if (ceph_argparse_flag(args, i, "-h", "--help", (char*)NULL)) {
       usage(cout);
       exit(0);
-    } else if (ceph_argparse_flag(args, i, "-f", "--force", (char*)NULL)) {
-      opts["force"] = "true";
     } else if (ceph_argparse_flag(args, i, "--force-full", (char*)NULL)) {
       opts["force-full"] = "true";
     } else if (ceph_argparse_flag(args, i, "-d", "--delete-after", (char*)NULL)) {
