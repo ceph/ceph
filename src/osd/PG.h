4bf4cd937c6c3b67ce600203aa22f405033e8c19
David Zafman <dzafman@redhat.com>
2018-02-02 12:15:17 -0800

    osd: Fix _update_calc_stats() to use peer_missing and missing_by_count
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -2831,6 +2831,7 @@ protected:
   bool is_peered() const {
     return state_test(PG_STATE_ACTIVE) || state_test(PG_STATE_PEERED);
   }
+  bool is_recovering() const { return state_test(PG_STATE_RECOVERING); }
 
   bool is_empty() const { return info.last_update == eversion_t(0,0); }
 
