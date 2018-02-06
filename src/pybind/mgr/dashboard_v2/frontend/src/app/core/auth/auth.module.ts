d5bd806b32080dcaae306503d0b9369fd76a20c9
Volker Theile <vtheile@suse.com>
2018-02-06 12:02:50 +0100

    mgr/dashboard_v2: Add cdPasswordButton directive
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/pybind/mgr/dashboard_v2/frontend/src/app/core/auth/auth.module.ts
+++ src/pybind/mgr/dashboard_v2/frontend/src/app/core/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
 
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
@@ -8,7 +9,8 @@ import { LogoutComponent } from './logout/logout.component';
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ],
   declarations: [LoginComponent, LogoutComponent],
   exports: [LogoutComponent]
