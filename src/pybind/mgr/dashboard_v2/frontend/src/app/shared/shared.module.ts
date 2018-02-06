4cfd508c592c92d6ffc1c06ebe5c71c137b6e937
Volker Theile <vtheile@suse.com>
2018-02-06 12:02:50 +0100

    mgr/dashboard_v2: Add cdPasswordButton directive
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/pybind/mgr/dashboard_v2/frontend/src/app/shared/shared.module.ts
+++ src/pybind/mgr/dashboard_v2/frontend/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ComponentsModule } from './components/components.module';
+import { PasswordButtonDirective } from './directives/password-button.directive';
 import { PipesModule } from './pipes/pipes.module';
 import { AuthGuardService } from './services/auth-guard.service';
 import { AuthStorageService } from './services/auth-storage.service';
@@ -18,8 +19,14 @@ import { ServicesModule } from './services/services.module';
     ComponentsModule,
     ServicesModule
   ],
-  exports: [PipesModule, ServicesModule],
-  declarations: [],
+  exports: [
+    PipesModule,
+    ServicesModule,
+    PasswordButtonDirective
+  ],
+  declarations: [
+    PasswordButtonDirective
+  ],
   providers: [
     AuthService,
     AuthStorageService,
