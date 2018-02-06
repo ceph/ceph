1dd62fdf4e01721471adaef17237ee89d707a130
Ricardo Dias <rdias@suse.com>
2018-02-06 12:42:44 +0000

    mgr/dashboard_v2: clean rados connection on shutdown
    Signed-off-by: Ricardo Dias <rdias@suse.com>

--- src/pybind/mgr/dashboard_v2/module.py
+++ src/pybind/mgr/dashboard_v2/module.py
@@ -129,6 +129,7 @@ class Module(MgrModule):
         logger.info('Engine done')
 
     def shutdown(self):
+        super(Module, self).shutdown()
         logger.info('Stopping server...')
         NotificationQueue.stop()
         cherrypy.engine.exit()
