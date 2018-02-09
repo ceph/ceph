e6692ed853aacc1975e4a63bccf94c2b05149d11
Tiago Melo <tmelo@suse.com>
2018-02-09 19:49:55 +0000

    mgr/dashboard_v2: add 404 page
    Add a 404 page that will be triggered every time the user tries to
    access a route that does not exists or the servers responds with 404.
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard_v2/frontend/src/app/shared/services/auth-interceptor.service.ts
+++ src/pybind/mgr/dashboard_v2/frontend/src/app/shared/services/auth-interceptor.service.ts
@@ -26,6 +26,11 @@ export class AuthInterceptorService implements HttpInterceptor {
       }
     }, (err: any) => {
       if (err instanceof HttpErrorResponse) {
+        if (err.status === 404) {
+          this.router.navigate(['/404']);
+          return;
+        }
+
         this.toastr.error(err.error.detail || '', `${err.status} - ${err.statusText}`);
         if (err.status === 401) {
           this.authStorageService.remove();
