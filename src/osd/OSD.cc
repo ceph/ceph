221a16687a8f4dbee0e993d5fe4fb05232372752
Sage Weil <sage@redhat.com>
2018-02-09 13:16:19 -0600

    osd: remove created_pgs tracking in RecoveryCtx
    Not needed or used!
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -3931,7 +3931,6 @@ PGRef OSD::handle_pg_create_info(OSDMapRef osdmap, const PGCreateInfo *info)
 
   pg->handle_initialize(&rctx);
   pg->handle_activate_map(&rctx);
-  rctx.created_pgs.insert(pg);
 
   dispatch_context(rctx, pg.get(), osdmap, nullptr);
 
@@ -8013,7 +8012,6 @@ void OSD::split_pgs(
     child->lock(true);
     out_pgs->insert(child);
     child->ch = store->create_new_collection(child->coll);
-    rctx->created_pgs.insert(child);
 
     unsigned split_bits = i->get_split_bits(pg_num);
     dout(10) << " pg_num is " << pg_num
@@ -8163,11 +8161,9 @@ void OSD::dispatch_context_transaction(PG::RecoveryCtx &ctx, PG *pg,
       std::move(*ctx.transaction), TrackedOpRef(), handle);
     assert(tr == 0);
     delete (ctx.transaction);
-    ctx.created_pgs.clear();
     ctx.transaction = new ObjectStore::Transaction;
     ctx.on_applied = new C_Contexts(cct);
     ctx.on_safe = new C_Contexts(cct);
-    ctx.created_pgs.clear();
   }
 }
 
@@ -8188,12 +8184,10 @@ void OSD::dispatch_context(PG::RecoveryCtx &ctx, PG *pg, OSDMapRef curmap,
   delete ctx.info_map;
   if ((ctx.on_applied->empty() &&
        ctx.on_safe->empty() &&
-       ctx.transaction->empty() &&
-       ctx.created_pgs.empty()) || !pg) {
+       ctx.transaction->empty()) || !pg) {
     delete ctx.transaction;
     delete ctx.on_applied;
     delete ctx.on_safe;
-    assert(ctx.created_pgs.empty());
   } else {
     if (ctx.on_applied)
       ctx.transaction->register_on_applied(ctx.on_applied);
@@ -8203,10 +8197,8 @@ void OSD::dispatch_context(PG::RecoveryCtx &ctx, PG *pg, OSDMapRef curmap,
       pg->ch,
       std::move(*ctx.transaction), TrackedOpRef(),
       handle);
-    ctx.created_pgs.clear();
     delete (ctx.transaction);
     assert(tr == 0);
-    ctx.created_pgs.clear();
   }
 }
 
