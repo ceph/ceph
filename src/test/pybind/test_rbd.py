885390aaf8c589c4241e5b0307020d1d0c2bd927
Jason Dillaman <dillaman@redhat.com>
2018-02-12 15:02:42 -0500

    test/librbd: disable clone v2 for negative test cases
    Fixes: http://tracker.ceph.com/issues/22979
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/test/pybind/test_rbd.py
+++ src/test/pybind/test_rbd.py
@@ -1041,8 +1041,10 @@ class TestClone(object):
         self.image.create_snap('snap2')
         global features
         clone_name2 = get_temp_image_name()
+        rados.conf_set("rbd_default_clone_format", "1")
         assert_raises(InvalidArgument, self.rbd.clone, ioctx, image_name,
                       'snap2', ioctx, clone_name2, features)
+        rados.conf_set("rbd_default_clone_format", "auto")
         self.image.remove_snap('snap2')
 
     def test_unprotect_with_children(self):
@@ -1065,8 +1067,10 @@ class TestClone(object):
 
         # ...with a clone of the same parent
         other_clone_name = get_temp_image_name()
+        rados.conf_set("rbd_default_clone_format", "1")
         self.rbd.clone(ioctx, image_name, 'snap1', other_ioctx,
                        other_clone_name, features)
+        rados.conf_set("rbd_default_clone_format", "auto")
         self.other_clone = Image(other_ioctx, other_clone_name)
         # validate its parent info
         (pool, image, snap) = self.other_clone.parent_info()
