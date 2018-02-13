206a056a22f9d21aa59570a30dfd97f68b4b81a5
Yehuda Sadeh <yehuda@redhat.com>
2018-02-13 10:18:08 -0800

    rgw: parse old rgw_obj with namespace correctly
    Fixes: http://tracker.ceph.com/issues/22982
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>
    (cherry picked from commit c598e53c95ec09772353a12e9162464ccb76e0a8)

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -2045,7 +2045,7 @@ struct rgw_obj {
           if (pos < 0) {
             throw buffer::error();
           }
-          key.name = key.name.substr(pos);
+          key.name = key.name.substr(pos + 1);
         }
       }
     } else {
