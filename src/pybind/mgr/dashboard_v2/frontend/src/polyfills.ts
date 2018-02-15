69132e9cb4c6fc41ce166703fd681aabce96fb58
Tiago Melo <tmelo@suse.com>
2018-02-15 18:40:01 +0000

    mgr/dashboard_v2: add filesystem page
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard_v2/frontend/src/polyfills.ts
+++ src/pybind/mgr/dashboard_v2/frontend/src/polyfills.ts
@@ -33,6 +33,7 @@ import 'core-js/es6/set';
 import 'core-js/es6/string';
 import 'core-js/es6/symbol';
 import 'core-js/es6/weak-map';
+import 'core-js/es7/object';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
