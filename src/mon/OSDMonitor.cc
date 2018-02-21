6c12b6280f7472f6649310a791ea0e2ca22ed2c2
Greg Farnum <gfarnum@redhat.com>
2018-02-21 11:23:54 -0800

    mon: mark OSD beacons and pg_create messages as no_reply
    Fixes: http://tracker.ceph.com/issues/22114
    Reported-by: Hongpeng Lu <ludehp@163.com>
    
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>
    (cherry picked from commit be1f999f5e1e902f60c50433251c90926538f406)

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -2789,6 +2789,7 @@ bool OSDMonitor::preprocess_pg_created(MonOpRequestRef op)
   auto m = static_cast<MOSDPGCreated*>(op->get_req());
   dout(10) << __func__ << " " << *m << dendl;
   auto session = m->get_session();
+  mon->no_reply(op);
   if (!session) {
     dout(10) << __func__ << ": no monitor session!" << dendl;
     return true;
@@ -3050,6 +3051,7 @@ bool OSDMonitor::preprocess_beacon(MonOpRequestRef op)
   auto beacon = static_cast<MOSDBeacon*>(op->get_req());
   // check caps
   auto session = beacon->get_session();
+  mon->no_reply(op);
   if (!session) {
     dout(10) << __func__ << " no monitor session!" << dendl;
     return true;
