c206d79f54c658e4a83b864e7e99fd18043d3017
Mykola Golub <mgolub@suse.com>
2018-02-21 10:46:21 +0200

    test/librbd: fix required features for ViaLockOwner tests
    The rename operation is RPCed only when journaling is enabled.
    
    The snap_remove operation is RPCed only when either fast-diff or
    journaling is enabled.
    
    The rename, snap_create, and snap_remove do not require layering.
    
    Signed-off-by: Mykola Golub <mgolub@suse.com>
    (cherry picked from commit 56e762b13aae302e6ab3cddb43f277d13ba6f43c)

--- src/test/librbd/test_librbd.cc
+++ src/test/librbd/test_librbd.cc
@@ -3242,7 +3242,7 @@ TEST_F(TestLibRBD, RebuildObjectMapViaLockOwner)
 
 TEST_F(TestLibRBD, RenameViaLockOwner)
 {
-  REQUIRE_FEATURE(RBD_FEATURE_LAYERING | RBD_FEATURE_EXCLUSIVE_LOCK);
+  REQUIRE_FEATURE(RBD_FEATURE_JOURNALING);
 
   librados::IoCtx ioctx;
   ASSERT_EQ(0, _rados.ioctx_create(m_pool_name.c_str(), ioctx));
@@ -3274,7 +3274,7 @@ TEST_F(TestLibRBD, RenameViaLockOwner)
 
 TEST_F(TestLibRBD, SnapCreateViaLockOwner)
 {
-  REQUIRE_FEATURE(RBD_FEATURE_LAYERING | RBD_FEATURE_EXCLUSIVE_LOCK);
+  REQUIRE_FEATURE(RBD_FEATURE_EXCLUSIVE_LOCK);
 
   librados::IoCtx ioctx;
   ASSERT_EQ(0, _rados.ioctx_create(m_pool_name.c_str(), ioctx));
@@ -3318,7 +3318,7 @@ TEST_F(TestLibRBD, SnapCreateViaLockOwner)
 
 TEST_F(TestLibRBD, SnapRemoveViaLockOwner)
 {
-  REQUIRE_FEATURE(RBD_FEATURE_LAYERING | RBD_FEATURE_EXCLUSIVE_LOCK);
+  REQUIRE_FEATURE(RBD_FEATURE_FAST_DIFF);
 
   librados::IoCtx ioctx;
   ASSERT_EQ(0, _rados.ioctx_create(m_pool_name.c_str(), ioctx));
