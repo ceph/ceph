c9a8e3ead57da0f2213f7eacc61516bf48cda621
Yehuda Sadeh <yehuda@redhat.com>
2018-02-23 08:41:19 -0800

    rgw: use vector for remove_tags in gc aio
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>
    (cherry picked from commit 9d37caccd907a7e58a836c5bcd9fb00a3dda8216)

--- src/cls/rgw/cls_rgw_client.h
+++ src/cls/rgw/cls_rgw_client.h
@@ -548,7 +548,7 @@ void cls_rgw_gc_defer_entry(librados::ObjectWriteOperation& op, uint32_t expirat
 int cls_rgw_gc_list(librados::IoCtx& io_ctx, string& oid, string& marker, uint32_t max, bool expired_only,
                     list<cls_rgw_gc_obj_info>& entries, bool *truncated, string& next_marker);
 
-void cls_rgw_gc_remove(librados::ObjectWriteOperation& op, const list<string>& tags);
+void cls_rgw_gc_remove(librados::ObjectWriteOperation& op, const vector<string>& tags);
 
 /* lifecycle */
 int cls_rgw_lc_get_head(librados::IoCtx& io_ctx, string& oid, cls_rgw_lc_obj_head& head);
