9fa3748550198695fd76a397f151cddd62bad683
Mykola Golub <mgolub@suse.com>
2018-02-24 10:58:58 +0200

    pybind/rbd: pool id in rbd_group_image_info_t should be int64_t
    Signed-off-by: Mykola Golub <mgolub@suse.com>

--- src/pybind/rbd/rbd.pyx
+++ src/pybind/rbd/rbd.pyx
@@ -185,7 +185,7 @@ cdef extern from "rbd/librbd.h" nogil:
 
     ctypedef struct rbd_group_image_info_t:
         char *name
-        uint64_t pool
+        int64_t pool
         rbd_group_image_state_t state
 
     ctypedef enum rbd_group_snap_state_t:
