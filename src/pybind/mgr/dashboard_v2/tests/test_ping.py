93a223d7de21835786e81f34fe833ac5a724b875
Ricardo Dias <rdias@suse.com>
2018-02-27 11:45:56 +0000

    mgr/dashboard_v2: added first unit test example
    Signed-off-by: Ricardo Dias <rdias@suse.com>

---
+++ src/pybind/mgr/dashboard_v2/tests/test_ping.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from cherrypy.test import helper
+
+from ..module import Module, cherrypy
+
+class SimpleCPTest(helper.CPWebCase):
+    @staticmethod
+    def setup_server():
+        module = Module('attic', None, None)
+        cherrypy.tree.mount(Module.HelloWorld(module))
+
+    def test_ping(self):
+        self.getPage("/ping")
+        self.assertStatus('200 OK')
+        self.assertBody('"pong"')
+
