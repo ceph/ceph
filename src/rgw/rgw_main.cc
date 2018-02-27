8e0a3484539315575b0476a1875868cf2cd95efd
Marcus Watts <mwatts@redhat.com>
2018-02-27 02:53:37 -0500

    rgw: reuse CURL* for keystone.
    When using keystone with https (the recommended setting),
    it is inefficient to start up a new SSL connection for
    each and every operation.  Keeping a CURL* structure around
    should reduce the cost of doing this.  This logic tries
    to do so, but it also tries to free them fairly aggressively
    (5-10 seconds).  This should still greatly reduce load on
    keystone at peak times while not tying up excess resources.
    
    Fixes: https://tracker.ceph.com/issues/23162
    
    Signed-off-by: Marcus Watts <mwatts@redhat.com>
    (cherry picked from commit 0c5cee1977eac232045571f4850e64be93271aec)
    
     Conflicts:
      src/rgw/rgw_common.h
      trivial conflict as lowercase_dash_http_attr isnt a part of L

--- src/rgw/rgw_main.cc
+++ src/rgw/rgw_main.cc
@@ -321,6 +321,7 @@ int main(int argc, const char **argv)
   rgw_init_resolver();
   
   curl_global_init(CURL_GLOBAL_ALL);
+  rgw_setup_saved_curl_handles();
   
 #if defined(WITH_RADOSGW_FCGI_FRONTEND)
   FCGX_Init();
@@ -596,6 +597,7 @@ int main(int argc, const char **argv)
 
   rgw_tools_cleanup();
   rgw_shutdown_resolver();
+  rgw_release_all_curl_handles();
   curl_global_cleanup();
 
   rgw_perf_stop(g_ceph_context);
