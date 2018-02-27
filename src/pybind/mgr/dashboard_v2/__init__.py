bc84d1ae3598689d5e02d7b94ae50eea2bcc4a76
Ricardo Dias <rdias@suse.com>
2018-02-27 13:20:32 +0000

    mgr/dashboard_v2: Added pylint support and fixed linting errors
    Signed-off-by: Ricardo Dias <rdias@suse.com>

--- src/pybind/mgr/dashboard_v2/__init__.py
+++ src/pybind/mgr/dashboard_v2/__init__.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
+"""
+openATTIC module
+"""
 
 import os
 
 if 'UNITTEST' not in os.environ:
+    # pylint: disable=W0403,W0401
     from module import *  # NOQA
 else:
     import sys
+    # pylint: disable=W0403
     import ceph_module_mock
     sys.modules['ceph_module'] = ceph_module_mock
-
-
