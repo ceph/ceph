3389f868e773c15cd860bd09153101165759f921
Ricardo Marques <rimarques@suse.com>
2018-02-28 13:46:21 +0000

    mgr/dashboard: Add helper component
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/components.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/components.module.ts
@@ -2,8 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ChartsModule } from 'ng2-charts/ng2-charts';
-import { AlertModule } from 'ngx-bootstrap';
+import { AlertModule, PopoverModule } from 'ngx-bootstrap';
 
+import { HelperComponent } from './helper/helper.component';
 import { SparklineComponent } from './sparkline/sparkline.component';
 import { ViewCacheComponent } from './view-cache/view-cache.component';
 
@@ -11,16 +12,19 @@ import { ViewCacheComponent } from './view-cache/view-cache.component';
   imports: [
     CommonModule,
     AlertModule.forRoot(),
+    PopoverModule.forRoot(),
     ChartsModule
   ],
   declarations: [
     ViewCacheComponent,
-    SparklineComponent
+    SparklineComponent,
+    HelperComponent
   ],
   providers: [],
   exports: [
     ViewCacheComponent,
-    SparklineComponent
+    SparklineComponent,
+    HelperComponent
   ]
 })
 export class ComponentsModule { }
