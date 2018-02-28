24e216b519795ba6a5d427790399a595fbb19b27
Sebastian Wagner <sebastian.wagner@suse.com>
2018-02-28 10:08:55 +0100

    mgr/dashboard_v2: Add CMake target to build the frontend.
    * Added new CMake flag `WITH_MGR_DASHBOARD_V2_FRONTEND`:
      Build the mgr/dashboard_v2 frontend using `npm install && npm run build`
    
    * Set this flag to `OFF` when building packages.
    
    * Removed creation of the frontend from `vstart.sh`
    
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- src/vstart.sh
+++ src/vstart.sh
@@ -720,13 +720,10 @@ EOF
     fi
 
     # dashboard_v2
-    # build frontend
-    pushd $MGR_PYTHON_PATH/dashboard_v2/frontend
-    npm install
-    npm run build
-    popd
-    # setting login credentials for dashboard_v2
+    sleep 5  # when running with more than 1 mgrs, if we enable dashboard_v2
+             # immediately it will fail, so we just wait for a bit
     ceph_adm mgr module enable dashboard_v2
+    # setting login credentials for dashboard_v2
     ceph_adm tell mgr dashboard set-login-credentials admin admin
 }
 
