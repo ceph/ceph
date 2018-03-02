64a9e554a3bfbe55fb2bb4f12320830a7846f412
Tiago Melo <tmelo@suse.com>
2018-03-02 12:14:57 +0000

    mgr/dashboard_v2: add mocked service provider for TcmuIscsiService
    This removes the warnings shown when running the tests with PhantomJS.
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard_v2/frontend/src/app/ceph/block/iscsi/iscsi.component.spec.ts
+++ src/pybind/mgr/dashboard_v2/frontend/src/app/ceph/block/iscsi/iscsi.component.spec.ts
@@ -1,18 +1,29 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppModule } from '../../../app.module';
+import { TcmuIscsiService } from '../../../shared/services/tcmu-iscsi.service';
 import { IscsiComponent } from './iscsi.component';
 
 describe('IscsiComponent', () => {
   let component: IscsiComponent;
   let fixture: ComponentFixture<IscsiComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [AppModule]
+  const fakeService = {
+    tcmuiscsi: () => {
+      return new Promise(function(resolve, reject) {
+        return;
+      });
+    },
+  };
+
+  beforeEach(
+    async(() => {
+      TestBed.configureTestingModule({
+        imports: [AppModule],
+        providers: [{ provide: TcmuIscsiService, useValue: fakeService }]
+      }).compileComponents();
     })
-    .compileComponents();
-  }));
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IscsiComponent);
