f5ef947c9c0af676c9f59642202d9f626474f495
Radoslaw Zarzynski <rzarzyns@redhat.com>
2018-03-03 04:19:11 +0100

    common: optimize Ceph's spinlock with PAUSE.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/include/spinlock.h
+++ src/include/spinlock.h
@@ -18,52 +18,119 @@
 #define CEPH_SPINLOCK_HPP
 
 #include <atomic>
+#include <thread>
+
+#if defined(__i386__) || defined(__x86_64__)
+#  include <immintrin.h>
+#endif
+
+#include "common/likely.h"
 
 namespace ceph {
 inline namespace version_1_0 {
 
+static inline void emit_pause() {
+#if defined(__i386__) || defined(__x86_64__)
+  // The spinning part is indistinguishable for CPU without
+  // additional hint. On x86 there is PAUSE instruction for
+  // that. We definitely want to use it because of:
+  //   * not disturbing second thread on the same core (SMT),
+  //   * saving power.
+  // Although the instruction is available since P4, binary
+  // transcription into `rep; nop` allows its decoding even
+  // on i386, so no need for `cpuid` or other costly things.
+  // For details please refer to:
+  //  * "Long Duration Spin-wait Loops on Hyper-Threading
+  //     Technology Enabled Intel Processors",
+  //  * "Benefitting Power and Performance Sleep Loops".
+  _mm_pause();
+#endif
+}
+
 class spinlock;
 
-inline void spin_lock(std::atomic_flag& lock);
-inline void spin_unlock(std::atomic_flag& lock);
-inline void spin_lock(ceph::spinlock& lock);
-inline void spin_unlock(ceph::spinlock& lock);
+inline void spin_lock(std::atomic_bool& locked);
+inline void spin_unlock(std::atomic_bool& locked);
+inline void spin_lock(ceph::spinlock& locked);
+inline void spin_unlock(ceph::spinlock& locked);
 
 /* A pre-packaged spinlock type modelling BasicLockable: */
 class spinlock final
 {
-  std::atomic_flag af = ATOMIC_FLAG_INIT;
+  // Not using atomic_flag anymore because it doesn't
+  // provide the load nor store operation.
+  std::atomic_bool locked = false;
 
-  public:
+  // In contrast to atomic_flag, atomic_bool might be
+  // implemented on top of e.g. mutex. However, it is
+  // very unlikely we'll face such situation in Ceph.
+  static_assert(std::atomic_bool::is_always_lock_free);
+
+public:
   void lock() {
-    ceph::spin_lock(af);
+    ceph::spin_lock(locked);
   }
  
   void unlock() noexcept {
-    ceph::spin_unlock(af);
+    ceph::spin_unlock(locked);
   }
 };
 
 // Free functions:
-inline void spin_lock(std::atomic_flag& lock)
+inline void spin_lock(std::atomic_bool& locked)
 {
- while(lock.test_and_set(std::memory_order_acquire))
-  ;
+  bool expected = false;
+  if (likely(locked.compare_exchange_weak(expected, true,
+                                          std::memory_order_acquire,
+                                          std::memory_order_relaxed))) {
+    return;
+  }
+
+  // The contention part.
+  do {
+    std::size_t tries = 0;
+
+    // There is no need to constantly try LOCK CMPXCHG and enforce
+    // x86 CPUs to do costly mem fencing. The algorithm comes from
+    // NPTL's pthread_spin_lock() implementation of glibc.
+    do {
+      emit_pause();
+
+      if if (++tries == 32) {
+        // Oops, things went really bad. There was no state change
+        // for many iterations. This could happen when lock holder
+        // gets stuck because of e.g. being preempted. Most likely
+        // other waiters started spinning as well. The best we can
+        // do is to limit our losses and yield CPU. Luckily kernel
+        // (it's perfectly unaware about the whole situation) will
+        // switch to something useful or, at least, blocked holder
+        // will get its time quantum faster.
+        std::this_thread::yield();
+        tries = 0;
+      }
+    } while (locked.load(std::memory_order_relaxed));
+
+    // The specification of compare_exchange() does not say a word
+    // that the value of `expected` can't change, so let's refresh.
+    expected = false;
+  } while (!locked.compare_exchange_weak(expected, true,
+                                         std::memory_order_acquire,
+                                         std::memory_order_relaxed));
 }
 
-inline void spin_unlock(std::atomic_flag& lock)
+inline void spin_unlock(std::atomic_bool& locked)
 {
- lock.clear(std::memory_order_release);
+  locked.store(false, std::memory_order_release);
 }
 
-inline void spin_lock(std::atomic_flag *lock)
+inline void spin_lock(std::atomic_bool *locked)
 {
- spin_lock(*lock);
+ spin_lock(*locked);
 }
 
-inline void spin_unlock(std::atomic_flag *lock)
+inline void spin_unlock(std::atomic_bool *locked)
 {
- spin_unlock(*lock);
+ spin_unlock(*locked);
 }
 
 inline void spin_lock(ceph::spinlock& lock)
