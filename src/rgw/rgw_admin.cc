b53166c660ce32e9ac67e23605db8e93c6e7c53e
Abhishek Lekshmanan <abhishek@suse.com>
2018-03-05 17:59:33 +0100

    rgw: rgw_admin use raii for curl init/teardown
    Since a lot of rgw-admin commands can exit early
    
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>
    (cherry picked from commit 4667937e4cece63751d5406d75c140418910a823)

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -2745,7 +2745,15 @@ int main(int argc, char **argv)
 
   rgw_user_init(store);
   rgw_bucket_init(store->meta_mgr);
-  rgw::curl::setup_curl(boost::none);
+
+  struct rgw_curl_setup {
+    rgw_curl_setup() {
+      rgw::curl::setup_curl(boost::none);
+    }
+    ~rgw_curl_setup() {
+      rgw::curl::cleanup_curl();
+    }
+  } curl_cleanup;
 
   StoreDestructor store_destructor(store);
 
@@ -6404,6 +6412,5 @@ next:
     }
   }
 
-  rgw::curl::cleanup_curl();
   return 0;
 }
