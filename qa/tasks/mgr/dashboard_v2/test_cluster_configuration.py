59ff971d811a18cc933e4dab812bcacb75b2d5bb
Sebastian Wagner <sebastian.wagner@suse.com>
2018-03-07 18:18:31 +0100

    mgr/dashboard_v2: removed unused code that broke teuthology
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>
    (cherry picked from commit f3d60676583b7d6a53099fb9ef346392bb25b592)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- qa/tasks/mgr/dashboard_v2/test_cluster_configuration.py
+++ qa/tasks/mgr/dashboard_v2/test_cluster_configuration.py
@@ -10,37 +10,20 @@ class ClusterConfigurationTest(DashboardTestCase):
         self.assertStatus(200)
         self.assertIsInstance(data, list)
         self.assertGreater(len(data), 1000)
-
-        # service filter
-        data = self._get('/api/cluster_conf?service=mds')
-        self.assertTrue(all('mds' in e['services'] for e in data))
-        self.assertFalse(all('mon' in e['services'] for e in data))
-
-        # basic filter
-        data = self._get('/api/cluster_conf?level=basic')
-        self.assertTrue(all('basic' in e['level'] for e in data))
-
-        # advanced filter
-        data = self._get('/api/cluster_conf?level=advanced')
-        actual_levels = set([e['level'] for e in data])
-        self.assertTrue({'advanced', 'basic'}.issubset(actual_levels))
-
-        # developer filter
-        data = self._get('/api/cluster_conf?level=developer')
-        actual_levels = set([e['level'] for e in data])
-        self.assertTrue({'advanced', 'basic',
-                         'developer'}.issubset(actual_levels))
-
-        # two filters
-        data = self._get('/api/cluster_conf?level=advanced&service=mds')
-        actual_levels = set([e['level'] for e in data])
-        self.assertTrue({'advanced', 'basic'}.issubset(actual_levels))
-        self.assertTrue(all('mds' in e['services'] for e in data))
+        for conf in data:
+            self._validate_single(conf)
 
     @authenticate
     def test_get(self):
         data = self._get('/api/cluster_conf/admin_socket')
         self.assertStatus(200)
+        self._validate_single(data)
+        self.assertIn('enum_values', data)
+
+        data = self._get('/api/cluster_conf/fantasy_name')
+        self.assertStatus(404)
+
+    def _validate_single(self, data):
         self.assertIn('name', data)
         self.assertIn('daemon_default', data)
         self.assertIn('long_desc', data)
@@ -51,9 +34,6 @@ class ClusterConfigurationTest(DashboardTestCase):
         self.assertIn('min', data)
         self.assertIn('max', data)
         self.assertIn('services', data)
-        self.assertIn('enum_values', data)
         self.assertIn('type', data)
         self.assertIn('desc', data)
 
-        data = self._get('/api/cluster_conf/fantasy_name')
-        self.assertStatus(404)
