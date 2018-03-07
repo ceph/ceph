a9ada31d5f7efa40cb98f1fafbc4dcaa2b53ab36
Tim Lin <timlin@qnap.com>
2018-03-07 16:58:52 +0800

    Objecter: add ignore overlay flag if got redirect reply
    Objecter's target calculation should ignore overlays if client got redirect reply.
    
    Fixes: https://tracker.ceph.com/issues/23296
    
    Signed-off-by: Ting Yi Lin <wooertim@gmail.com>

--- src/osdc/Objecter.cc
+++ src/osdc/Objecter.cc
@@ -3411,7 +3411,7 @@ void Objecter::handle_osd_op_reply(MOSDOpReply *m)
     op->tid = 0;
     m->get_redirect().combine_with_locator(op->target.target_oloc,
 					   op->target.target_oid.name);
-    op->target.flags |= CEPH_OSD_FLAG_REDIRECTED;
+    op->target.flags |= (CEPH_OSD_FLAG_REDIRECTED | CEPH_OSD_FLAG_IGNORE_OVERLAY);
     _op_submit(op, sul, NULL);
     m->put();
     return;
