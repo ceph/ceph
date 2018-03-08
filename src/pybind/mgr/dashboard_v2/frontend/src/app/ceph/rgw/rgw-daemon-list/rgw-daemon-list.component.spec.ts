14076f37b6daa7a78dbeb7d9b94a79f36c44b44b
Tiago Melo <tmelo@suse.com>
2018-03-08 15:37:47 +0000

    mgr/dashboard_v2: fix and improve table details
    There was a problem with the current implementation of the table details, when
    compiled in production mode, where the method that created the details element
    would throw an exception.
    
    Besides fixing that, this commit also improves the way we define the details.
    Now instead of sending the component name, through the 'detailsComponent' attr,
    we need to get the current selection via 'updateSelection' attr and declare the
    detail component inside the cdTable and add the cdTableDetail attr to that
    element.
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>
    (cherry picked from commit ab780bafc2a13be461e37407f5db8c602b7741d1)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard_v2/frontend/src/app/ceph/rgw/rgw-daemon-list/rgw-daemon-list.component.spec.ts
+++ src/pybind/mgr/dashboard_v2/frontend/src/app/ceph/rgw/rgw-daemon-list/rgw-daemon-list.component.spec.ts
@@ -2,9 +2,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 import { DataTableModule } from '../../../shared/datatable/datatable.module';
+import { PerformanceCounterModule } from '../../performance-counter/performance-counter.module';
+import { RgwDaemonDetailsComponent } from '../rgw-daemon-details/rgw-daemon-details.component';
 import { RgwDaemonService } from '../services/rgw-daemon.service';
 import { RgwDaemonListComponent } from './rgw-daemon-list.component';
 
@@ -14,12 +16,13 @@ describe('RgwDaemonListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RgwDaemonListComponent ],
+      declarations: [ RgwDaemonListComponent, RgwDaemonDetailsComponent ],
       imports: [
         DataTableModule,
         HttpClientTestingModule,
-        BsDropdownModule.forRoot(),
-        HttpClientModule
+        HttpClientModule,
+        TabsModule.forRoot(),
+        PerformanceCounterModule
       ],
       providers: [ RgwDaemonService ]
     })
