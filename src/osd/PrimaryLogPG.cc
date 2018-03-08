ffaf1428bdfda3cec4fb30ee797678e6e30cd370
Josh Durgin <jdurgin@redhat.com>
2018-03-07 22:31:59 -0500

    PrimaryLogPG: only trim up to osd_pg_log_trim_max entries at once
    This prevents the fix for http://tracker.ceph.com/issues/22050 or
    potential future bugs from causing too much latency by trimming too
    many log entries at once.
    
    Signed-off-by: Josh Durgin <jdurgin@redhat.com>
    (cherry picked from commit 1c15458a00e64d1fd0127ae171305b8f8afa6619)

--- src/osd/PrimaryLogPG.cc
+++ src/osd/PrimaryLogPG.cc
@@ -1574,8 +1574,10 @@ void PrimaryLogPG::calc_trim_to()
   if (limit != eversion_t() &&
       limit != pg_trim_to &&
       pg_log.get_log().approx_size() > target) {
-    size_t num_to_trim = pg_log.get_log().approx_size() - target;
-    if (num_to_trim < cct->_conf->osd_pg_log_trim_min) {
+    size_t num_to_trim = MIN(pg_log.get_log().approx_size() - target,
+			     cct->_conf->osd_pg_log_trim_max);
+    if (num_to_trim < cct->_conf->osd_pg_log_trim_min &&
+	cct->_conf->osd_pg_log_trim_max >= cct->_conf->osd_pg_log_trim_min) {
       return;
     }
     list<pg_log_entry_t>::const_iterator it = pg_log.get_log().log.begin();
