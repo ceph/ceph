340005c1f1a14fef8d23069a908080afe59991b3
Yehuda Sadeh <yehuda@redhat.com>
2018-03-12 14:34:05 -0700

    test/rgw: initial work on cloud sync test
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/test/rgw/rgw_multi/zone_cloud.py
+++ src/test/rgw/rgw_multi/zone_cloud.py
@@ -7,11 +7,15 @@ import boto.s3.connection
 
 import dateutil.parser
 
+import re
+
 from nose.tools import eq_ as eq
 try:
     from itertools import izip_longest as zip_longest
 except ImportError:
     from itertools import zip_longest
+
+from urlparse import urlparse
 
 from .multisite import *
 from .tools import *
@@ -35,12 +39,12 @@ def check_object_eq(k1, k2, check_extra = True):
     # eq(k1.content_disposition, k2.content_disposition)
     # eq(k1.content_language, k2.content_language)
     eq(k1.etag, k2.etag)
-    mtime1 = dateutil.parser.parse(k1.last_modified)
-    mtime2 = dateutil.parser.parse(k2.last_modified)
-    assert abs((mtime1 - mtime2).total_seconds()) < 1 # handle different time resolution
-    if check_extra:
-        eq(k1.owner.id, k2.owner.id)
-        eq(k1.owner.display_name, k2.owner.display_name)
+    # mtime1 = dateutil.parser.parse(k1.last_modified)
+    # mtime2 = dateutil.parser.parse(k2.last_modified)
+    # assert abs((mtime1 - mtime2).total_seconds()) < 1 # handle different time resolution
+    # if check_extra:
+        # eq(k1.owner.id, k2.owner.id)
+        # eq(k1.owner.display_name, k2.owner.display_name)
     # eq(k1.storage_class, k2.storage_class)
     eq(k1.size, k2.size)
     eq(get_key_ver(k1), get_key_ver(k2))
@@ -60,7 +64,7 @@ def append_query_arg(s, n, v):
         return nv
     return '{s}&{nv}'.format(s=s, nv=nv)
 
-class MDSearch:
+class CloudList:
     def __init__(self, conn, bucket_name, query, query_args = None, marker = None):
         self.conn = conn
         self.bucket_name = bucket_name or ''
@@ -132,67 +136,95 @@ class MDSearch:
         return l
 
 
-class MDSearchConfig:
-    def __init__(self, conn, bucket_name):
-        self.conn = conn
-        self.bucket_name = bucket_name or ''
-        if bucket_name:
-            self.bucket = boto.s3.bucket.Bucket(name=bucket_name)
-        else:
-            self.bucket = None
-
-    def send_request(self, conf, method):
-        query_args = 'mdsearch'
-        headers = None
-        if conf:
-            headers = { 'X-Amz-Meta-Search': conf }
-
-        query_args = append_query_arg(query_args, 'format', 'json')
-
-        return make_request(self.conn, method, bucket=self.bucket_name, key='', query_args=query_args, headers=headers)
-
-    def get_config(self):
-        result = self.send_request(None, 'GET')
-        return json.loads(result.read())
-
-    def set_config(self, conf):
-        self.send_request(conf, 'POST')
-
-    def del_config(self):
-        self.send_request(None, 'DELETE')
-
-
-class ESZoneBucket:
-    def __init__(self, zone_conn, name, conn):
+class CloudZoneBucket:
+    def __init__(self, zone_conn, target_path, name):
         self.zone_conn = zone_conn
         self.name = name
-        self.conn = conn
+        self.cloud_conn = zone_conn.zone.cloud_conn
 
-        self.bucket = boto.s3.bucket.Bucket(name=name)
+        target_path = target_path[:]
+        if target_path[-1] != '/':
+            target_path += '/'
+        target_path = target_path.replace('${bucket}', name)
+
+        tp = target_path.split('/', 1)
+
+        if len(tp) == 1:
+            self.target_bucket = target_path
+            self.target_prefix = ''
+        else:
+            self.target_bucket = tp[0]
+            self.target_prefix = tp[1]
+
+        log.debug('target_path=%s target_bucket=%s target_prefix=%s', target_path, self.target_bucket, self.target_prefix)
+        self.bucket = self.cloud_conn.get_bucket(self.target_bucket)
 
     def get_all_versions(self):
-
-        marker = None
-        is_done = False
-
-        req = MDSearch(self.conn, self.name, 'bucket == ' + self.name, marker=marker)
-
-        for k in req.search():
-            yield k
+        for o in self.bucket.get_all_keys(prefix=self.target_prefix):
+            new_name = o.name[len(self.target_prefix):]
+            log.debug('bucket=%s obj=%s new_name=', self.bucket.name, new_name)
+            o.name = new_name
+            yield o
 
 
+def parse_endpoint(endpoint):
+    o = urlparse(endpoint)
+
+    netloc = o.netloc.split(':')
+    
+    host = netloc[0]
+
+    if len(netloc) > 1:
+        port = int(netloc[1])
+    else:
+        port = o.port
+
+    is_secure = False
+
+    if o.scheme == 'https':
+        is_secure = True
+
+    if not port:
+        if is_secure:
+            port = 443
+        else:
+            port = 80
+
+    return host, port, is_secure
 
 
-class ESZone(Zone):
-    def __init__(self, name, es_endpoint, zonegroup = None, cluster = None, data = None, zone_id = None, gateways = None):
-        self.es_endpoint = es_endpoint
-        super(ESZone, self).__init__(name, zonegroup, cluster, data, zone_id, gateways)
+class CloudZone(Zone):
+    def __init__(self, name, cloud_endpoint, credentials, source_bucket, target_path,
+            zonegroup = None, cluster = None, data = None, zone_id = None, gateways = None):
+        self.cloud_endpoint = cloud_endpoint
+        self.credentials = credentials
+        self.source_bucket = source_bucket
+        self.target_path = target_path
+
+        self.target_path = self.target_path.replace('${zone}', name)
+        # self.target_path = self.target_path.replace('${zone_id}', zone_id)
+        self.target_path = self.target_path.replace('${zonegroup}', zonegroup.name)
+        self.target_path = self.target_path.replace('${zonegroup_id}', zonegroup.id)
+
+        log.debug('target_path=%s', self.target_path)
+
+        host, port, is_secure = parse_endpoint(cloud_endpoint)
+
+        self.cloud_conn = boto.connect_s3(
+                aws_access_key_id = credentials.access_key,
+                aws_secret_access_key = credentials.secret,
+                host = host,
+                port = port,
+                is_secure = is_secure,
+                calling_format = boto.s3.connection.OrdinaryCallingFormat())
+        super(CloudZone, self).__init__(name, zonegroup, cluster, data, zone_id, gateways)
+
 
     def is_read_only(self):
         return True
 
     def tier_type(self):
-        return "elasticsearch"
+        return "aws"
 
     def create(self, cluster, args = None, check_retcode = True):
         """ create the object with the given arguments """
@@ -200,7 +232,10 @@ class ESZone(Zone):
         if args is None:
             args = ''
 
-        tier_config = ','.join([ 'endpoint=' + self.es_endpoint, 'explicit_custom_meta=false' ])
+        tier_config = ','.join([ 'connection.endpoint=' + self.cloud_endpoint,
+                                 'connection.access_key=' + self.credentials.access_key,
+                                 'connection.secret=' + self.credentials.secret,
+                                 'target_path=' + re.escape(self.target_path)])
 
         args += [ '--tier-type', self.tier_type(), '--tier-config', tier_config ] 
 
@@ -211,14 +246,14 @@ class ESZone(Zone):
 
     class Conn(ZoneConn):
         def __init__(self, zone, credentials):
-            super(ESZone.Conn, self).__init__(zone, credentials)
+            super(CloudZone.Conn, self).__init__(zone, credentials)
 
         def get_bucket(self, bucket_name):
-            return ESZoneBucket(self, bucket_name, self.conn)
+            return CloudZoneBucket(self, self.zone.target_path, bucket_name)
 
         def create_bucket(self, name):
             # should not be here, a bug in the test suite
-            log.critical('Conn.create_bucket() should not be called in ES zone')
+            log.critical('Conn.create_bucket() should not be called in cloud zone')
             assert False
 
         def check_bucket_eq(self, zone_conn, bucket_name):
@@ -254,7 +289,19 @@ class ESZone(Zone):
         return self.Conn(self, credentials)
 
 
-class ESZoneConfig:
+class CloudZoneConfig:
     def __init__(self, cfg, section):
         self.endpoint = cfg.get(section, 'endpoint')
+        access_key = cfg.get(section, 'access_key')
+        secret = cfg.get(section, 'secret')
+        self.credentials = Credentials(access_key, secret)
+        try:
+            self.target_path = cfg.get(section, 'target_path')
+        except:
+            self.target_path = 'rgw-${zonegroup_id}/${bucket}'
+
+        try:
+            self.source_bucket = cfg.get(section, 'source_bucket')
+        except:
+            self.source_bucket = '*'
 
