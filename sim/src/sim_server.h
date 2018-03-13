3f2e0b4bf4d93bc6af082b76475e44539a8c57ed
J. Eric Ivancich <ivancich@redhat.com>
2018-03-13 14:52:25 -0400

    Added LGPL2.1 license to all files.
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>

--- sim/src/sim_server.h
+++ sim/src/sim_server.h
@@ -3,6 +3,13 @@
 
 /*
  * Copyright (C) 2016 Red Hat Inc.
+ *
+ * Author: J. Eric Ivancich <ivancich@redhat.com>
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 2.1, as published by the Free Software Foundation.  See file
+ * COPYING.
  */
 
 
--- test/dmcPrCtl.h
+++ test/dmcPrCtl.h
@@ -1,3 +1,18 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2017 Red Hat Inc.
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
 // essentially the same as ceph's PrCtl.h, copied into the dmclock library
 
 #include <dmtest-config.h>
