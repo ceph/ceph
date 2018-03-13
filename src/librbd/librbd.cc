9e44b9f211d9b59dc66a43a797ab917ecf847a99
Hitoshi Kamei <hitoshi.kamei.xm@hitachi.com>
2018-03-13 17:25:13 +0900

    rbd: rbd_discard_on_zero_write_same option support
    Add the rbd_discard_on_zeroed_write_same option to
    the writesame method of librdb, if applied.
    
    If the rbd_discard_on_zero_write_same option is true,
    the data of rbd image are just discarded by writesame
    when librdb receives the write operation of zero data.
    Meanwhile, if the option is false, the zero data are
    actually written to the image.
    
    The default value of rbd_discard_on_zero_write_same
    is true.
    
    Signed-off-by: Hitoshi Kamei <hitoshi.kamei.xm@hitachi.com>
    Cc: Mitsuo Hayasaka <mitsuo.hayasaka.hu@hitachi.com>

--- src/librbd/librbd.cc
+++ src/librbd/librbd.cc
@@ -1800,7 +1800,8 @@ namespace librbd {
       return -EINVAL;
     }
 
-    if (mem_is_zero(bl.c_str(), bl.length())) {
+    bool discard_zero = ictx->cct->_conf->get_val<bool>("rbd_discard_on_zeroed_write_same");
+    if (discard_zero && mem_is_zero(bl.c_str(), bl.length())) {
       int r = ictx->io_work_queue->discard(ofs, len, false);
       tracepoint(librbd, writesame_exit, r);
       return r;
@@ -1931,7 +1932,8 @@ namespace librbd {
       return -EINVAL;
     }
 
-    if (mem_is_zero(bl.c_str(), bl.length())) {
+    bool discard_zero = ictx->cct->_conf->get_val<bool>("rbd_discard_on_zeroed_write_same");
+    if (discard_zero && mem_is_zero(bl.c_str(), bl.length())) {
       ictx->io_work_queue->aio_discard(get_aio_completion(c), off, len, false);
       tracepoint(librbd, aio_writesame_exit, 0);
       return 0;
@@ -3936,7 +3938,8 @@ extern "C" ssize_t rbd_writesame(rbd_image_t image, uint64_t ofs, size_t len,
     return -EINVAL;
   }
 
-  if (mem_is_zero(buf, data_len)) {
+  bool discard_zero = ictx->cct->_conf->get_val<bool>("rbd_discard_on_zeroed_write_same");
+  if (discard_zero && mem_is_zero(buf, data_len)) {
     int r = ictx->io_work_queue->discard(ofs, len, false);
     tracepoint(librbd, writesame_exit, r);
     return r;
@@ -4157,7 +4160,8 @@ extern "C" int rbd_aio_writesame(rbd_image_t image, uint64_t off, size_t len,
     return -EINVAL;
   }
 
-  if (mem_is_zero(buf, data_len)) {
+  bool discard_zero = ictx->cct->_conf->get_val<bool>("rbd_discard_on_zeroed_write_same");
+  if (discard_zero && mem_is_zero(buf, data_len)) {
     ictx->io_work_queue->aio_discard(get_aio_completion(comp), off, len, false);
     tracepoint(librbd, aio_writesame_exit, 0);
     return 0;
