71aba3a96ef8dd360129bf05c6a2f5b16f0310fe
Tiago Melo <tmelo@suse.com>
2018-03-14 11:11:55 +0000

    mgr/dashboard: remove node/npm system installation
    Node and npm are now being installed in a virtualenv, removing the need of
    having it installed in the system or as a node dependency.
    
    Now, if you want to use npm, you just need to activate the virtualenv created
    on 'build/src/pybind/mgr/dashboard/node-env', and then you can execute the
    same commands as you did before.
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>
    (cherry picked from commit d5f8713882b949610d02708e02488cec31256d04)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>
    
    Conflicts:
    	install-deps.sh

--- src/pybind/mgr/dashboard/run-frontend-unittests.sh
+++ src/pybind/mgr/dashboard/run-frontend-unittests.sh
@@ -4,6 +4,10 @@ set -e
 
 cd $CEPH_ROOT/src/pybind/mgr/dashboard/frontend
 
+.  $CEPH_ROOT/build/src/pybind/mgr/dashboard/node-env/bin/activate
+
 npm run build -- --prod
 npm run test -- --browsers PhantomJS --watch=false
 npm run lint
+
+deactivate
