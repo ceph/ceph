a377c66ed7f55d156bebb940a95d2bc6349b2741
David Galloway <dgallowa@redhat.com>
2018-03-14 13:12:06 -0400

    task.selinux: Whitelist rhsmd denials
    These started showing up once we added RHEL to Sepia.
    
    Fixes: https://tracker.ceph.com/issues/23343#note-5
    
    Signed-off-by: David Galloway <dgallowa@redhat.com>

--- teuthology/task/selinux.py
+++ teuthology/task/selinux.py
@@ -114,6 +114,7 @@ class SELinux(Task):
             'scontext=system_u:system_r:nrpe_t:s0',
             'scontext=system_u:system_r:pcp_pmlogger_t',
             'scontext=system_u:system_r:pcp_pmcd_t:s0',
+            'comm="rhsmd"',
         ]
         se_whitelist = self.config.get('whitelist', [])
         if se_whitelist:
