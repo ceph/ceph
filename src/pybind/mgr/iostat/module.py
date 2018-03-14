8ab5cbfb79c469bbe5d6439620553a963661ce79
Mohamad Gebai <mgebai@suse.com>
2018-03-14 12:07:09 -0400

    mgr/iostat: add self-test
    Signed-off-by: Mohamad Gebai <mgebai@suse.com>

--- src/pybind/mgr/iostat/module.py
+++ src/pybind/mgr/iostat/module.py
@@ -9,6 +9,11 @@ class Module(MgrModule):
             "desc": "Get IO rates",
             "perm": "r",
             "poll": "true"
+        },
+        {
+            "cmd": "iostat self-test",
+            "desc": "Run a self test the iostat module",
+            "perm": "r"
         }
     ]
 
@@ -21,6 +26,7 @@ class Module(MgrModule):
         rd = 0
         wr = 0
         ops = 0
+        ret = ''
 
         if command['prefix'] == 'iostat':
             r = self.get('io_rate')
@@ -31,6 +37,17 @@ class Module(MgrModule):
                 wr = int(r['pg_stats_delta']['stat_sum']['num_write_kb']) / stamp_delta
                 ops = ( int(r['pg_stats_delta']['stat_sum']['num_write']) + int(r['pg_stats_delta']['stat_sum']['num_read']) ) / stamp_delta
 
-        ret = "wr: {0} kB/s, rd: {1} kB/s, iops: {2}".format(str(int(wr)), str(int(rd)), str(int(ops)))
+            ret = "wr: {0} kB/s, rd: {1} kB/s, iops: {2}".format(int(wr), int(rd), int(ops))
+
+        elif command['prefix'] == 'iostat self-test':
+            r = self.get('io_rate')
+            assert('pg_stats_delta' in r)
+            assert('stamp_delta' in r['pg_stats_delta'])
+            assert('stat_sum' in r['pg_stats_delta'])
+            assert('num_read_kb' in r['pg_stats_delta']['stat_sum'])
+            assert('num_write_kb' in r['pg_stats_delta']['stat_sum'])
+            assert('num_write' in r['pg_stats_delta']['stat_sum'])
+            assert('num_read' in r['pg_stats_delta']['stat_sum'])
+            ret = 'iostat self-test OK'
 
         return 0, '', ret
