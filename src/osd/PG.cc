2f5a24d8f8fe7dd69dbbe3b3883d24d7ea0fdd41
Sage Weil <sage@redhat.com>
2018-03-15 08:16:51 -0500

    osd/PG: print debug if we fail to decode snapbl
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -3572,6 +3572,7 @@ void PG::update_snap_map(
 	try {
 	  decode(snaps, p);
 	} catch (...) {
+	  derr << __func__ << " decode snaps failure on " << *i << dendl;
 	  snaps.clear();
 	}
 	set<snapid_t> _snaps(snaps.begin(), snaps.end());
