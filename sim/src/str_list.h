b7c8aeb691c07f356d7cb042b51afd4759ff2f96
Casey Bodley <cbodley@redhat.com>
2018-03-21 13:34:16 -0400

    Squashed 'src/dmclock/' changes from 6f9250b4e9..5538352024
    5538352024 Merge pull request #49 from cbodley/wip-remove-by-ptr
    2dbd66d039 PriorityQueueBase uses RequestRef&& for remove_by callbacks
    729d8c0400 Merge pull request #48 from ivancich/wip-add-license
    3f2e0b4bf4 Added LGPL2.1 license to all files.
    
    git-subtree-dir: src/dmclock
    git-subtree-split: 5538352024bd6a774305218b4a27063ce5563162

--- sim/src/str_list.h
+++ sim/src/str_list.h
@@ -1,3 +1,18 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+/*
+ * Copyright (C) 2009 Red Hat Inc.
+ *
+ * Forked from Red Hat's Ceph project.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License version
+ * 2.1, as published by the Free Software Foundation.  See file
+ * COPYING.
+ */
+
+
 #ifndef CEPH_STRLIST_H
 #define CEPH_STRLIST_H
 
