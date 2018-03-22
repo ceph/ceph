e2d7dcb52c0819e1ba24d81944a66a37c8339aa2
Casey Bodley <cbodley@redhat.com>
2018-03-21 23:56:51 -0400

    rgw: add dmclock client/cost to RGWOp interface
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_rest.h
+++ src/rgw/rgw_rest.h
@@ -488,6 +488,7 @@ public:
   virtual int check_caps(RGWUserCaps& caps)
     { return -EPERM; } /* should to be implemented! */
   int verify_permission() override;
+  client_id dmclock_client() override { return client_id::admin; }
 };
 
 class RGWHandler_REST : public RGWHandler {
