04636376474e30a7cb05f9be004dd836b931bbcb
Simran Singhal <singhalsimran0@gmail.com>
2018-03-23 00:56:08 +0530

    Revert "mgr/dashboard: Add toBytes() method to FormatterService."
    This reverts commit 8fe761f735496bb2351e8860c6bb2fe946295e1d.

--- src/pybind/mgr/dashboard/frontend/src/app/shared/services/formatter.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/formatter.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 
-import * as _ from 'lodash';
-
 @Injectable()
 export class FormatterService {
   constructor() {}
@@ -49,52 +47,5 @@ export class FormatterService {
     }
 
     return truncatedFloat === '' ? '-' : (truncatedFloat + units[unit]);
-  }
-
-  /**
-   * Convert the given value into bytes.
-   * @param {string} value The value to be converted, e.g. 1024B, 10M, 300KiB or 1ZB.
-   * @returns Returns the given value in bytes without any appended unit or null in case
-   *   of an error.
-   */
-  toBytes(value: string): number | null {
-    const base = 1024;
-    const units = {
-      'b': 1,
-      'k': Math.pow(base, 1),
-      'kb': Math.pow(base, 1),
-      'kib': Math.pow(base, 1),
-      'm': Math.pow(base, 2),
-      'mb': Math.pow(base, 2),
-      'mib': Math.pow(base, 2),
-      'g': Math.pow(base, 3),
-      'gb': Math.pow(base, 3),
-      'gib': Math.pow(base, 3),
-      't': Math.pow(base, 4),
-      'tb': Math.pow(base, 4),
-      'tib': Math.pow(base, 4),
-      'p': Math.pow(base, 5),
-      'pb': Math.pow(base, 5),
-      'pib': Math.pow(base, 5),
-      'e': Math.pow(base, 6),
-      'eb': Math.pow(base, 6),
-      'eib': Math.pow(base, 6),
-      'z': Math.pow(base, 7),
-      'zb': Math.pow(base, 7),
-      'zib': Math.pow(base, 7),
-      'y': Math.pow(base, 8),
-      'yb': Math.pow(base, 8),
-      'yib': Math.pow(base, 8)
-    };
-    const m = RegExp('^(\\d+)\\s*(B|K(B|iB)?|M(B|iB)?|G(B|iB)?|T(B|iB)?|P(B|iB)?|' +
-      'E(B|iB)?|Z(B|iB)?|Y(B|iB)?)?$', 'i').exec(value);
-    if (m === null) {
-      return null;
-    }
-    let bytes = parseInt(m[1], 10);
-    if (_.isString(m[2])) {
-      bytes = bytes * units[m[2].toLowerCase()];
-    }
-    return bytes;
   }
 }
