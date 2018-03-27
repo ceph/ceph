7c37d8a06be60b66b030b7ab616ca075269ab70e
Tiago Melo <tmelo@suse.com>
2018-03-27 21:25:15 +0100

    mgr/dashboard: Add notification component
    This component is used on the navigation bar and will show the user the 10 most
    recent notifications.
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/core/navigation/navigation.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/navigation/navigation.module.ts
@@ -3,22 +3,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { PopoverModule } from 'ngx-bootstrap/popover';
 
 import { AppRoutingModule } from '../../app-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 import { AuthModule } from '../auth/auth.module';
 import { NavigationComponent } from './navigation/navigation.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 
 @NgModule({
   imports: [
     CommonModule,
     AuthModule,
     BsDropdownModule.forRoot(),
+    PopoverModule.forRoot(),
     AppRoutingModule,
     SharedModule,
     RouterModule
   ],
-  declarations: [NavigationComponent],
+  declarations: [NavigationComponent, NotificationsComponent],
   exports: [NavigationComponent]
 })
 export class NavigationModule {}
