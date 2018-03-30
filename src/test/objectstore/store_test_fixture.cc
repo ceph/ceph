7318826465db8f40317a621058d76a11b8994c40
Igor Fedotov <ifedotov@suse.com>
2018-03-30 20:16:58 +0300

    test/ceph_test_objectstore: make settings update and restore less error prone.
    Signed-off-by: Igor Fedotov <ifedotov@suse.com>

--- src/test/objectstore/store_test_fixture.cc
+++ src/test/objectstore/store_test_fixture.cc
@@ -1,16 +1,20 @@
 #include <stdlib.h>
 #include <string>
 #include <iostream>
+#include <assert.h>
 #include <gtest/gtest.h>
 
 #include "common/errno.h"
+#include "common/config.h"
 #include "os/ObjectStore.h"
+
 #if defined(WITH_BLUESTORE)
 #include "os/bluestore/BlueStore.h"
 #endif
 #include "store_test_fixture.h"
 
-static void rm_r(const string& path) {
+static void rm_r(const string& path)
+{
   string cmd = string("rm -r ") + path;
   cout << "==> " << cmd << std::endl;
   int r = ::system(cmd.c_str());
@@ -26,7 +30,8 @@ static void rm_r(const string& path) {
   }
 }
 
-void StoreTestFixture::SetUp() {
+void StoreTestFixture::SetUp()
+{
   int r = ::mkdir(data_dir.c_str(), 0777);
   if (r < 0) {
     r = -errno;
@@ -53,10 +58,37 @@ void StoreTestFixture::SetUp() {
   ASSERT_EQ(0, store->mount());
 }
 
-void StoreTestFixture::TearDown() {
+void StoreTestFixture::TearDown()
+{
+  PopSettings(0);
   if (store) {
     int r = store->umount();
     EXPECT_EQ(0, r);
     rm_r(data_dir);
+  }
+}
+
+void StoreTestFixture::SetVal(md_config_t* _conf, const char* key, const char* val)
+{
+  assert(!conf || conf == _conf);
+  conf = _conf;
+  std::string skey(key);
+  std::string prev_val;
+  conf->get_val(skey, &prev_val);
+  conf->set_val(key, val);
+  saved_settings.emplace(skey, prev_val);
+}
+
+void StoreTestFixture::PopSettings(size_t pos)
+{
+  if (conf) {
+    assert(pos == 0 || pos <= saved_settings.size()); // for sanity
+    while(pos < saved_settings.size())
+    {
+      auto& e = saved_settings.top();
+      conf->set_val(e.first, e.second);
+      saved_settings.pop();
+    }
+    conf->apply_changes(NULL);
   }
 }
--- src/test/objectstore/store_test_fixture.h
+++ src/test/objectstore/store_test_fixture.h
@@ -1,12 +1,17 @@
 #include <string>
+#include <stack>
 #include <boost/scoped_ptr.hpp>
 #include <gtest/gtest.h>
 
 class ObjectStore;
+struct md_config_t;
 
 class StoreTestFixture : virtual public ::testing::Test {
   const std::string type;
   const std::string data_dir;
+
+  std::stack<std::pair<std::string, std::string>> saved_settings;
+  md_config_t* conf = nullptr;
 
 public:
   boost::scoped_ptr<ObjectStore> store;
@@ -15,6 +20,23 @@ public:
   StoreTestFixture(const std::string& type)
     : type(type), data_dir(type + ".test_temp_dir")
   {}
+
   void SetUp() override;
   void TearDown() override;
+  void SetVal(md_config_t* conf, const char* key, const char* val);
+  struct SettingsBookmark {
+    StoreTestFixture& s;
+    size_t pos;
+
+    SettingsBookmark(StoreTestFixture& _s, size_t p) : s(_s), pos(p)
+    {}
+
+    ~SettingsBookmark() {
+      s.PopSettings(pos);
+    }
+  };
+  SettingsBookmark BookmarkSettings() {
+    return SettingsBookmark(*this, saved_settings.size());
+  }
+  void PopSettings(size_t);
 };
