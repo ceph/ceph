b3d7ac391670beb7094c0eb9e9e932ba641f535f
Sage Weil <sage@redhat.com>
2018-04-06 21:38:26 -0500

    osd/osd_types: add pg_pool_t FLAG_POOL_CREATING
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/osd_types.h
+++ src/osd/osd_types.h
@@ -1190,6 +1190,7 @@ struct pg_pool_t {
     FLAG_BACKFILLFULL = 1<<12, // pool is backfillfull
     FLAG_SELFMANAGED_SNAPS = 1<<13, // pool uses selfmanaged snaps
     FLAG_POOL_SNAPS = 1<<14,        // pool has pool snaps
+    FLAG_CREATING = 1<<15,          // initial pool PGs are being created
   };
 
   static const char *get_flag_name(int f) {
@@ -1209,6 +1210,7 @@ struct pg_pool_t {
     case FLAG_BACKFILLFULL: return "backfillfull";
     case FLAG_SELFMANAGED_SNAPS: return "selfmanaged_snaps";
     case FLAG_POOL_SNAPS: return "pool_snaps";
+    case FLAG_CREATING: return "creating";
     default: return "???";
     }
   }
@@ -1257,6 +1259,8 @@ struct pg_pool_t {
       return FLAG_SELFMANAGED_SNAPS;
     if (name == "pool_snaps")
       return FLAG_POOL_SNAPS;
+    if (name == "creating")
+      return FLAG_CREATING;
     return 0;
   }
 
