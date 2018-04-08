84c9d0ec2145c9049e9c8323b8e684a6e199a752
Niu Pengju <pengju.niu@xtaotech.com>
2018-04-08 11:08:04 +0800

    rgw: Fix multisite Synchronization failed when read and write delete at the same time
    This case is first write objA,then write and delete objA at the same
    time,write early than delete.
    When del objA, use information which stat  of first write objA, so the
    op should del the first write data.However when try to del objA, objA
    header is second write, so osd "do_xattr_cmp_str" has found idtag change
    and return -125(canceled),after rgw client receive the ret -125 , it
    will still do "complete_del", then do cls_obj_complete_del to write
    bilog。"complete_op" in cls_rgw module  will  write bilog with second
    write mtime and second ".ver.epoch". Finally, del op append behind the
    second write in bilog. And the slave rgw will merge write op and del op
    as del op, and del data,but master rgw complete second write and cancel
    del.
    This logic is problematic,  so bilog recording the del op should use
    cancel op. And squash_map should skip the cancel op.
    
    Fixes: http://tracker.ceph.com/issues/22804
    Signed-off-by: Niu Pengju <pengju.niu@xtaotech.com>
    (cherry picked from commit a8fd508915a0acf7768fe6ba7f7414e71813f1d7)

--- src/rgw/rgw_data_sync.cc
+++ src/rgw/rgw_data_sync.cc
@@ -2554,6 +2554,9 @@ int RGWBucketShardIncrementalSyncCR::operate()
           syncstopped = false;
           continue;
         }
+        if (e.op == CLS_RGW_OP_CANCEL) {
+          continue;
+        }
         if (e.state != CLS_RGW_STATE_COMPLETE) {
           continue;
         }
