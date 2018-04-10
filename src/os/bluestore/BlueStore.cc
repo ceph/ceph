fed965c20014f4c099029b5b5d9b20de6d7ab9bd
Sage Weil <sage@redhat.com>
2018-04-10 13:00:42 -0500

    os/bluestore: factor VDO into bluestore's 'allocated' value
    If we are on VDO, report the *raw* storage we've allocated, as reported
    by VDO.
    
    Signed-off-by: Sage Weil <sage@redhat.com>
    (cherry picked from commit 04b47939b84f388c3a8c0b09077fe56b27d2f9f2)

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -7170,6 +7170,15 @@ int BlueStore::statfs(struct store_statfs_t *buf)
     }
   }
 
+  {
+    std::lock_guard<std::mutex> l(vstatfs_lock);
+    buf->allocated = vstatfs.allocated();
+    buf->data_stored = vstatfs.stored();
+    buf->data_compressed = vstatfs.compressed();
+    buf->data_compressed_original = vstatfs.compressed_original();
+    buf->data_compressed_allocated = vstatfs.compressed_allocated();
+  }
+
   uint64_t thin_total, thin_avail;
   if (bdev->get_thin_utilization(&thin_total, &thin_avail)) {
     buf->total += thin_total;
@@ -7177,6 +7186,8 @@ int BlueStore::statfs(struct store_statfs_t *buf)
     // we are limited by both the size of the virtual device and the
     // underlying physical device.
     bfree = std::min(bfree, thin_avail);
+
+    buf->allocated = thin_total - thin_avail;
   } else {
     buf->total = bdev->get_size();
   }
@@ -7192,15 +7203,6 @@ int BlueStore::statfs(struct store_statfs_t *buf)
     buf->internal_metadata =
       std::max(bluefs->get_used(), (uint64_t)cct->_conf->bluestore_bluefs_min)
       - buf->omap_allocated;
-  }
-
-  {
-    std::lock_guard<std::mutex> l(vstatfs_lock);
-    buf->allocated = vstatfs.allocated();
-    buf->data_stored = vstatfs.stored();
-    buf->data_compressed = vstatfs.compressed();
-    buf->data_compressed_original = vstatfs.compressed_original();
-    buf->data_compressed_allocated = vstatfs.compressed_allocated();
   }
 
   dout(20) << __func__ << " " << *buf << dendl;
