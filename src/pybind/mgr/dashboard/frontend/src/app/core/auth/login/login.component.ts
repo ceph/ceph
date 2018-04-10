47173ed512061a96789dd6d1c8512b3f9125b090
Tiago Melo <tmelo@suse.com>
2018-04-10 16:01:42 +0100

    mgr/dashboard: Replace toastr calls with notification service
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/core/auth/login/login.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/auth/login/login.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, ViewContainerRef } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { ToastsManager } from 'ng2-toastr';
-
 import { AuthService } from '../../../shared/api/auth.service';
 import { Credentials } from '../../../shared/models/credentials';
 import { AuthStorageService } from '../../../shared/services/auth-storage.service';
@@ -18,10 +16,7 @@ export class LoginComponent implements OnInit {
 
   constructor(private authService: AuthService,
               private authStorageService: AuthStorageService,
-              private router: Router,
-              public toastr: ToastsManager,
-              private vcr: ViewContainerRef) {
-    this.toastr.setRootViewContainerRef(vcr);
+              private router: Router) {
   }
 
   ngOnInit() {
