c582bb254bd3225521c78f0ecdf1818225620a4c
Volker Theile <vtheile@suse.com>
2018-04-12 18:49:28 +0200

    mgr/dashboard: Rename auth-interceptor and refactor it to display notifications for more errors than 401 and 500.
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/services/api-interceptor.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/api-interceptor.service.ts
@@ -17,37 +17,28 @@ import { AuthStorageService } from './auth-storage.service';
 import { NotificationService } from './notification.service';
 
 @Injectable()
-export class AuthInterceptorService implements HttpInterceptor {
-  constructor(
-    private router: Router,
-    private authStorageService: AuthStorageService,
-    public notificationService: NotificationService
-  ) {}
+export class ApiInterceptorService implements HttpInterceptor {
 
-  _notify (resp) {
-    this.notificationService.show(
-      NotificationType.error,
-      resp.error.detail || '',
-      `${resp.status} - ${resp.statusText}`
-    );
-  }
+  constructor(private router: Router,
+              private authStorageService: AuthStorageService,
+              public notificationService: NotificationService) {}
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(request).catch(resp => {
       if (resp instanceof HttpErrorResponse) {
         switch (resp.status) {
-          case 404:
-            this.router.navigate(['/404']);
-            break;
           case 401:
             this.authStorageService.remove();
             this.router.navigate(['/login']);
-            this._notify(resp);
             break;
-          case 500:
-            this._notify(resp);
+          case 404:
+            this.router.navigate(['/404']);
             break;
         }
+        this.notificationService.show(
+          NotificationType.error,
+          resp.error.detail || '',
+          `${resp.status} - ${resp.statusText}`);
       }
       // Return the error to the method that called it.
       return Observable.throw(resp);
