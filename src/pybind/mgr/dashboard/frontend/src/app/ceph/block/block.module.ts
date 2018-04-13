e2054b6a1aaa98d36b57c74bb1c324301cf2cf83
Tiago Melo <tmelo@suse.com>
2018-04-13 01:21:01 +0100

    mgr/dashboard: Angular modules cleanup
    Removed the import of modules that were already being imported through the
    Shared module.
    
    Removed duplicated providers, those services were already provided in the api
    module.
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/block.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/block.module.ts
@@ -5,9 +5,6 @@ import { FormsModule } from '@angular/forms';
 import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 import { TabsModule } from 'ngx-bootstrap/tabs';
 
-import { ComponentsModule } from '../../shared/components/components.module';
-import { PipesModule } from '../../shared/pipes/pipes.module';
-import { ServicesModule } from '../../shared/services/services.module';
 import { SharedModule } from '../../shared/shared.module';
 import { IscsiComponent } from './iscsi/iscsi.component';
 import { MirrorHealthColorPipe } from './mirror-health-color.pipe';
@@ -20,10 +17,7 @@ import { PoolDetailComponent } from './pool-detail/pool-detail.component';
     FormsModule,
     TabsModule.forRoot(),
     ProgressbarModule.forRoot(),
-    SharedModule,
-    ComponentsModule,
-    PipesModule,
-    ServicesModule
+    SharedModule
   ],
   declarations: [
     PoolDetailComponent,
