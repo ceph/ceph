4c40168b8a7f2eb35824eaf9beaf5ee69e71a69a
Stephan Müller <smueller@suse.com>
2018-04-16 13:44:38 +0200

    mgr/dashboard: Change deletion link to modal only
    Due to CSS problems the link solution wasn't the best way, now it will
    represent only the modal content. The downside of this solution is that
    it put's the burden on the developer to use it the right way and import
    a view things to get it working. But on the upside CSS styles will work
    as expected.
    
    The unit test example was updated accordingly this way it should be easy
    to understand how it can be implemented the right way.
    
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/components.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/components.module.ts
@@ -9,7 +9,7 @@ import { PipesModule } from '../pipes/pipes.module';
 import {
   DeleteConfirmationComponent
 } from './delete-confirmation-modal/delete-confirmation-modal.component';
-import { DeletionLinkComponent } from './deletion-link/deletion-link.component';
+import { DeletionModalComponent } from './deletion-modal/deletion-modal.component';
 import { HelperComponent } from './helper/helper.component';
 import { ModalComponent } from './modal/modal.component';
 import { SparklineComponent } from './sparkline/sparkline.component';
@@ -38,7 +38,10 @@ import { ViewCacheComponent } from './view-cache/view-cache.component';
     UsageBarComponent,
     DeleteConfirmationComponent,
     ModalComponent,
-    DeletionLinkComponent
+    DeletionModalComponent
+  ],
+  entryComponents: [
+    DeletionModalComponent
   ],
   providers: [],
   exports: [
@@ -52,7 +55,7 @@ import { ViewCacheComponent } from './view-cache/view-cache.component';
   entryComponents: [
     DeleteConfirmationComponent,
     ModalComponent,
-    DeletionLinkComponent
+    DeletionModalComponent
   ]
 })
 export class ComponentsModule { }
