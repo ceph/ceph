7c1fccc3930e6da3ba178936fa7f5c0d5469a914
Yuri Weinstein <yweinste@redhat.com>
2018-04-16 12:28:27 -0700

    qa/tests - added tag: v12.2.2 to be used by client.1 under workload_luminous to avoid failures.
    added workunits/rados/test-upgrade-to-mimic.sh, and add --gtest_filter=-CReadOpsTest.Exec to api_c_read_operations
    
    Signed-off-by: Yuri Weinstein <yweinste@redhat.com>

---
+++ qa/workunits/rados/test-upgrade-to-mimic.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -ex
+
+parallel=1
+[ "$1" = "--serial" ] && parallel=0
+
+color=""
+[ -t 1 ] && color="--gtest_color=yes"
+
+function cleanup() {
+    pkill -P $$ || true
+}
+trap cleanup EXIT ERR HUP INT QUIT
+
+declare -A pids
+
+for f in \
+    api_aio api_io api_list api_lock api_misc \
+    api_tier api_pool api_snapshots api_stat api_watch_notify api_cmd \
+    api_service \
+    api_c_write_operations \
+    'api_c_read_operations --gtest_filter=-CReadOpsTest.Exec' \
+    list_parallel \
+    open_pools_parallel \
+    delete_pools_parallel \
+    watch_notify
+do
+    if [ $parallel -eq 1 ]; then
+	r=`printf '%25s' $f`
+	bash -o pipefail -exc "ceph_test_rados_$f $color 2>&1 | tee ceph_test_rados_$f.log | sed \"s/^/$r: /\"" &
+	pid=$!
+	echo "test $f on pid $pid"
+	pids[$f]=$pid
+    else
+	ceph_test_rados_$f
+    fi
+done
+
+ret=0
+if [ $parallel -eq 1 ]; then
+for t in "${!pids[@]}"
+do
+  pid=${pids[$t]}
+  if ! wait $pid
+  then
+    echo "error in $t ($pid)"
+    ret=1
+  fi
+done
+fi
+
+exit $ret
