a2c9e2a702abafba12918a2de10176b371f26c6c
Ricardo Dias <rdias@suse.com>
2018-04-20 16:35:44 +0100

    mgr/dashboard: task: fix bug in __eq__ method
    Signed-off-by: Ricardo Dias <rdias@suse.com>
    (cherry picked from commit f15b93e94d63ceb9cf0109ebc9cb4608c38e5d88)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/tools.py
+++ src/pybind/mgr/dashboard/tools.py
@@ -579,7 +579,7 @@ class Task(object):
         return hash((self.name, tuple(sorted(self.metadata.items()))))
 
     def __eq__(self, other):
-        return self.name == self.name and self.metadata == self.metadata
+        return self.name == other.name and self.metadata == other.metadata
 
     def __str__(self):
         return "Task(ns={}, md={})" \
