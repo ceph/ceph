1daa8b0ab382e745d23c2140bae0749d1a1dd4b1
Volker Theile <vtheile@suse.com>
2018-04-20 14:33:44 +0200

    mgr/dashboard: Add 'autofocus' directive
    Signed-off-by: Volker Theile <vtheile@suse.com>

---
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/directives/autofocus.directive.spec.ts
@@ -0,0 +1,8 @@
+import { AutofocusDirective } from './autofocus.directive';
+
+describe('AutofocusDirective', () => {
+  it('should create an instance', () => {
+    const directive = new AutofocusDirective(null);
+    expect(directive).toBeTruthy();
+  });
+});
