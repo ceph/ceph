7287ab67d9b8d74daed24b67acf6946c8f5fb584
Adam C. Emerson <aemerson@redhat.com>
2018-04-23 14:52:09 -0400

    finisher: Do some testing
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

---
+++ src/test/common/test_finisher.cc
@@ -0,0 +1,54 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2018 Red Hat <contact@redhat.com>
+ *
+ * LGPL2.1 (see COPYING-LGPL2.1) or later
+ */
+
+#include <atomic>
+
+#include <gtest/gtest.h>
+
+#include "include/Context.h"
+
+#include "common/ceph_time.h"
+#include "common/Cond.h"
+#include "common/Finisher.h"
+
+TEST(UpDown, Finisher) {
+  auto cct = (new CephContext(CEPH_ENTITY_TYPE_CLIENT))->get();
+  Finisher f(cct);
+  f.start();
+  f.stop();
+  cct->put();
+}
+
+TEST(Fire, Finisher) {
+  auto cct = (new CephContext(CEPH_ENTITY_TYPE_CLIENT))->get();
+  Finisher f(cct);
+  C_SaferCond c;
+  f.start();
+  f.queue(&c);
+  c.wait();
+  f.stop();
+  cct->put();
+}
+
+TEST(Bench, Finisher) {
+  using namespace std::literals;
+  auto cct = (new CephContext(CEPH_ENTITY_TYPE_CLIENT))->get();
+  Finisher f(cct);
+  std::vector<C_SaferCond> cs(1'000'000);
+  f.start();
+  auto begin = ceph::coarse_mono_clock::now();
+  for (auto& c : cs) f.queue(&c);
+  for (auto& c : cs) c.wait();
+  auto end = ceph::coarse_mono_clock::now();
+  f.stop();
+  cct->put();
+  std::cout << "Queued and completed 1,000,000 C_SaferCond objects in " << end - begin
+	    << std::endl;
+}
