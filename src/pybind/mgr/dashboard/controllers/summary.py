b9b1740ed3903e5050a41220fd9afbca69d776d8
Sebastian Wagner <sebastian.wagner@suse.com>
2018-04-24 18:20:59 +0200

    mgr/dashboard: Applied Exception Handling
    * Minor changes to CephFS, OSD, Pool, RbdMirroring, Summary and RGW
    
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- src/pybind/mgr/dashboard/controllers/summary.py
+++ src/pybind/mgr/dashboard/controllers/summary.py
@@ -5,9 +5,10 @@ import json
 
 import cherrypy
 
+from .. import mgr
 from . import AuthRequired, ApiController, BaseController
-from .. import logger, mgr
 from ..controllers.rbd_mirroring import get_daemons_and_pools
+from ..tools import ViewCacheNoDataException
 from ..services.ceph_service import CephService
 from ..tools import TaskManager
 
@@ -34,14 +35,13 @@ class Summary(BaseController):
         ]
 
     def _rbd_mirroring(self):
-        _, data = get_daemons_and_pools()
+        try:
+            _, data = get_daemons_and_pools()
+        except ViewCacheNoDataException:
+            return {}
 
-        if isinstance(data, Exception):
-            logger.exception("Failed to get rbd-mirror daemons and pools")
-            raise type(data)(str(data))
-        else:
-            daemons = data.get('daemons', [])
-            pools = data.get('pools', {})
+        daemons = data.get('daemons', [])
+        pools = data.get('pools', {})
 
         warnings = 0
         errors = 0
