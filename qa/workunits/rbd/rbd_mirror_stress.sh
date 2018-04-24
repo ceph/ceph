de4587012d2d0d35f9d201550d58ffc2faeff27b
Jason Dillaman <dillaman@redhat.com>
2018-04-24 09:00:18 -0400

    qa/suites/rbd: rbd-mirror fsx stress test
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- qa/workunits/rbd/rbd_mirror_stress.sh
+++ qa/workunits/rbd/rbd_mirror_stress.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -ex
 #
 # rbd_mirror_stress.sh - stress test rbd-mirror daemon
 #
@@ -12,6 +12,8 @@ IMAGE_COUNT=50
 export LOCKDEP=0
 
 . $(dirname $0)/rbd_mirror_helpers.sh
+
+setup
 
 create_snap()
 {
@@ -182,5 +184,3 @@ do
   purge_snapshots ${CLUSTER2} ${POOL} ${image}
   remove_image_retry ${CLUSTER2} ${POOL} ${image}
 done
-
-echo OK
