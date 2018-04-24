7d3bcf075acc94aa407866bedaba2fcf9eb5a0cb
John Spray <john.spray@redhat.com>
2018-04-24 15:05:44 -0400

    vstart: update for dashboard TLS
    Signed-off-by: John Spray <john.spray@redhat.com>
    (cherry picked from commit f521ffdb9089d920b155ae3464eb50f3cff8034e)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/vstart.sh
+++ src/vstart.sh
@@ -688,9 +688,9 @@ EOF
         if $with_mgr_dashboard ; then
             ceph_adm config-key set mgr/dashboard/$name/server_port $MGR_PORT
             if [ $mgr -eq 1 ]; then
-                DASH_URLS="http://$IP:$MGR_PORT"
+                DASH_URLS="https://$IP:$MGR_PORT"
             else
-                DASH_URLS+=", http://$IP:$MGR_PORT"
+                DASH_URLS+=", https://$IP:$MGR_PORT"
             fi
         fi
 	MGR_PORT=$(($MGR_PORT + 1000))
@@ -713,6 +713,9 @@ EOF
     # setting login credentials for dashboard
     if $with_mgr_dashboard; then
         ceph_adm tell mgr dashboard set-login-credentials admin admin
+        if ! ceph_adm tell mgr dashboard create-self-signed-cert;  then
+            echo dashboard module not working correctly!
+        fi
     fi
 
     if ceph_adm tell mgr restful create-self-signed-cert; then
