2b5842eb40c34bb064a92e2e616ffba6c6ef63b3
Stephan Müller <smueller@suse.com>
2018-04-26 15:25:04 +0200

    mgr/dashboard: Refactored formatter service
    Added tests which caused a refactorization of the service.
    
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/services/formatter.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/formatter.service.ts
@@ -6,48 +6,26 @@ import * as _ from 'lodash';
 export class FormatterService {
   constructor() {}
 
-  truncate(n, maxWidth) {
-    const stringized = n.toString();
-    const parts = stringized.split('.');
+  truncate(n: number | string, decimals: number): string {
+    const value = n.toString();
+    const parts = value.split('.');
     if (parts.length === 1) {
-      // Just an int
-      return stringized;
+      return value; // integer
     } else {
-      const fractionalDigits = maxWidth - parts[0].length - 1;
-      if (fractionalDigits <= 0) {
-        // No width available for the fractional part, drop
-        // it and the decimal point
-        return parts[0];
-      } else {
-        return stringized.substring(0, maxWidth);
-      }
+      return Number.parseFloat(value).toPrecision(decimals + parts[0].length)
+        .toString().replace(/0+$/, '');
     }
   }
 
-  format_number(n, divisor, units) {
-    const width = 4;
-    let unit = 0;
-
-    if (n == null) {
-      // People shouldn't really be passing null, but let's
-      // do something sensible instead of barfing.
+  format_number(n: any, divisor: number, units: string[], decimals: number = 4): string {
+    if (_.isString(n)) {
+      n = Number(n);
+    }
+    if (!(_.isNumber(n)) || n === 0) {
       return '-';
     }
-
-    while (Math.floor(n / divisor ** unit).toString().length > width - 1) {
-      unit = unit + 1;
-    }
-
-    let truncatedFloat;
-    if (unit > 0) {
-      truncatedFloat = this.truncate(
-        (n / Math.pow(divisor, unit)).toString(),
-        width
-      );
-    } else {
-      truncatedFloat = this.truncate(n, width);
-    }
-
+    const unit = Math.floor(Math.log(n) / Math.log(divisor));
+    const truncatedFloat = this.truncate((n / Math.pow(divisor, unit)), decimals);
     return truncatedFloat === '' ? '-' : (truncatedFloat + units[unit]);
   }
 
@@ -59,42 +37,15 @@ export class FormatterService {
    */
   toBytes(value: string): number | null {
     const base = 1024;
-    const units = {
-      'b': 1,
-      'k': Math.pow(base, 1),
-      'kb': Math.pow(base, 1),
-      'kib': Math.pow(base, 1),
-      'm': Math.pow(base, 2),
-      'mb': Math.pow(base, 2),
-      'mib': Math.pow(base, 2),
-      'g': Math.pow(base, 3),
-      'gb': Math.pow(base, 3),
-      'gib': Math.pow(base, 3),
-      't': Math.pow(base, 4),
-      'tb': Math.pow(base, 4),
-      'tib': Math.pow(base, 4),
-      'p': Math.pow(base, 5),
-      'pb': Math.pow(base, 5),
-      'pib': Math.pow(base, 5),
-      'e': Math.pow(base, 6),
-      'eb': Math.pow(base, 6),
-      'eib': Math.pow(base, 6),
-      'z': Math.pow(base, 7),
-      'zb': Math.pow(base, 7),
-      'zib': Math.pow(base, 7),
-      'y': Math.pow(base, 8),
-      'yb': Math.pow(base, 8),
-      'yib': Math.pow(base, 8)
-    };
-    const m = RegExp('^(\\d+(\.\\d+)?)\\s*(B|K(B|iB)?|M(B|iB)?|G(B|iB)?|T(B|iB)?|P(B|iB)?|' +
-      'E(B|iB)?|Z(B|iB)?|Y(B|iB)?)?$', 'i').exec(value);
+    const units = ['b', 'k', 'm', 'g', 't', 'p', 'e', 'z', 'y'];
+    const m = RegExp('^(\\d+(\.\\d+)?) ?(\[' + units.join('') + '\](b|ib)?)?$', 'i').exec(value);
     if (m === null) {
       return null;
     }
     let bytes = parseFloat(m[1]);
     if (_.isString(m[3])) {
-      bytes = bytes * units[m[3].toLowerCase()];
+      bytes = bytes * Math.pow(base, units.indexOf(m[3].toLowerCase()[0]));
     }
-    return Math.floor(bytes);
+    return Math.round(bytes);
   }
 }
