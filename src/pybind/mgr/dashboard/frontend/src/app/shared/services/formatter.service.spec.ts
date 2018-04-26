c24554c59f77cd8fd1548f05c2b6bc8ada993d33
Stephan Müller <smueller@suse.com>
2018-04-26 12:20:01 +0200

    mgr/dashboard: Convert floating values to bytes
    Now the 'formatter' service can convert floating values to bytes.
    
    Signed-off-by: Stephan Müller <smueller@suse.com>
    (cherry picked from commit 28cbd865b7c0d423430c086d83a5e2340d064b9d)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/services/formatter.service.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/formatter.service.spec.ts
@@ -1,36 +1,51 @@
-import { inject, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { FormatterService } from './formatter.service';
 
 describe('FormatterService', () => {
+  let service: FormatterService;
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [FormatterService]
     });
+    service = new FormatterService();
   });
 
-  it('should be created',
-    inject([FormatterService], (service: FormatterService) => {
-      expect(service).toBeTruthy();
-    }));
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 
-  it('should not convert 10xyz to bytes (failure)',
-    inject([FormatterService], (service: FormatterService) => {
-      const bytes = service.toBytes('10xyz');
-      expect(bytes).toBeNull();
-    }));
+  it('should not convert 10xyz to bytes (failure)', () => {
+    const bytes = service.toBytes('10xyz');
+    expect(bytes).toBeNull();
+  });
 
-  it('should convert 4815162342 to bytes',
-    inject([FormatterService], (service: FormatterService) => {
-      const bytes = service.toBytes('4815162342');
-      expect(bytes).toEqual(jasmine.any(Number));
-      expect(bytes).toBe(4815162342);
-    }));
+  it('should not convert 1.1.1KiB to bytes (failure)', () => {
+    const bytes = service.toBytes('1.1.1KiB');
+    expect(bytes).toBeNull();
+  });
 
-  it('should convert 100M to bytes',
-    inject([FormatterService], (service: FormatterService) => {
-      const bytes = service.toBytes('100M');
-      expect(bytes).toEqual(jasmine.any(Number));
-      expect(bytes).toBe(104857600);
-    }));
+  it('should convert 4815162342 to bytes', () => {
+    const bytes = service.toBytes('4815162342');
+    expect(bytes).toEqual(jasmine.any(Number));
+    expect(bytes).toBe(4815162342);
+  });
+
+  it('should convert 100M to bytes', () => {
+    const bytes = service.toBytes('100M');
+    expect(bytes).toEqual(jasmine.any(Number));
+    expect(bytes).toBe(104857600);
+  });
+
+  it('should convert 1.532KiB to bytes', () => {
+    const bytes = service.toBytes('1.532KiB');
+    expect(bytes).toEqual(jasmine.any(Number));
+    expect(bytes).toBe(Math.floor(1.532 * 1024));
+  });
+
+  it('should convert 0.000000000001TiB to bytes', () => {
+    const bytes = service.toBytes('0.000000000001TiB');
+    expect(bytes).toEqual(jasmine.any(Number));
+    expect(bytes).toBe(1);
+  });
 });
