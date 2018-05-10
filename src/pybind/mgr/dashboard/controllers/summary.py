ef7a930c1a23f410cb9000a485d4f4804647386d
Tiago Melo <tmelo@suse.com>
2018-05-10 10:36:57 +0100

    mgr/dashboard: Remove unused data from summary controller
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/controllers/summary.py
+++ src/pybind/mgr/dashboard/controllers/summary.py
@@ -7,30 +7,15 @@ from .. import mgr
 from . import AuthRequired, ApiController, Endpoint, BaseController
 from ..controllers.rbd_mirroring import get_daemons_and_pools
 from ..tools import ViewCacheNoDataException
-from ..services.ceph_service import CephService
 from ..tools import TaskManager
 
 
 @ApiController('/summary')
 @AuthRequired()
 class Summary(BaseController):
-    def _rbd_pool_data(self):
-        pool_names = [pool['pool_name'] for pool in CephService.get_pool_list('rbd')]
-        return sorted(pool_names)
-
     def _health_status(self):
         health_data = mgr.get("health")
         return json.loads(health_data["json"])['status']
-
-    def _filesystems(self):
-        fsmap = mgr.get("fs_map")
-        return [
-            {
-                "id": f['id'],
-                "name": f['mdsmap']['fs_name']
-            }
-            for f in fsmap['filesystems']
-        ]
 
     def _rbd_mirroring(self):
         try:
@@ -59,9 +44,7 @@ class Summary(BaseController):
     def __call__(self):
         executing_t, finished_t = TaskManager.list_serializable()
         return {
-            'rbd_pools': self._rbd_pool_data(),
             'health_status': self._health_status(),
-            'filesystems': self._filesystems(),
             'rbd_mirroring': self._rbd_mirroring(),
             'mgr_id': mgr.get_mgr_id(),
             'have_mon_connection': mgr.have_mon_connection(),
