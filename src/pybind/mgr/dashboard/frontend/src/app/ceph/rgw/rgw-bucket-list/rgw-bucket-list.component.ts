b2fa819fdfee93e6951666198720bce44c37b95d
Volker Theile <vtheile@suse.com>
2018-05-15 14:25:30 +0200

    mgr/dashboard: Handle errors during deletion
    - Take care that the deletion dialog gets notified about errors to stop the progress spinner and enable the delete button again.
    - Prettify JS code.
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-list/rgw-bucket-list.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-list/rgw-bucket-list.component.ts
@@ -7,9 +7,7 @@ import { Observable } from 'rxjs/Observable';
 import { Subscriber } from 'rxjs/Subscriber';
 
 import { RgwBucketService } from '../../../shared/api/rgw-bucket.service';
-import {
-  DeletionModalComponent
-} from '../../../shared/components/deletion-modal/deletion-modal.component';
+import { DeletionModalComponent } from '../../../shared/components/deletion-modal/deletion-modal.component';
 import { TableComponent } from '../../../shared/datatable/table/table.component';
 import { CdTableColumn } from '../../../shared/models/cd-table-column';
 import { CdTableSelection } from '../../../shared/models/cd-table-selection';
@@ -20,16 +18,17 @@ import { CdTableSelection } from '../../../shared/models/cd-table-selection';
   styleUrls: ['./rgw-bucket-list.component.scss']
 })
 export class RgwBucketListComponent {
-
   @ViewChild(TableComponent) table: TableComponent;
 
   columns: CdTableColumn[] = [];
   buckets: object[] = [];
   selection: CdTableSelection = new CdTableSelection();
 
-  constructor(private router: Router,
-              private rgwBucketService: RgwBucketService,
-              private bsModalService: BsModalService) {
+  constructor(
+    private router: Router,
+    private rgwBucketService: RgwBucketService,
+    private bsModalService: BsModalService
+  ) {
     this.columns = [
       {
         name: 'Name',
@@ -45,14 +44,16 @@ export class RgwBucketListComponent {
   }
 
   getBucketList() {
-    this.rgwBucketService.list()
-      .subscribe((resp: object[]) => {
+    this.rgwBucketService.list().subscribe(
+      (resp: object[]) => {
         this.buckets = resp;
-      }, () => {
+      },
+      () => {
         // Force datatable to hide the loading indicator in
         // case of an error.
         this.buckets = [];
-      });
+      }
+    );
   }
 
   updateSelection(selection: CdTableSelection) {
@@ -65,19 +66,30 @@ export class RgwBucketListComponent {
       metaType: this.selection.hasSingleSelection ? 'bucket' : 'buckets',
       deletionObserver: (): Observable<any> => {
         return new Observable((observer: Subscriber<any>) => {
+          // Delete all selected data table rows.
           Observable.forkJoin(
             this.selection.selected.map((bucket: any) => {
               return this.rgwBucketService.delete(bucket.bucket);
-            }))
-            .subscribe(null, null, () => {
-              observer.complete();
-              // Finally reload the data table content.
+            })
+          ).subscribe(
+            null,
+            (error) => {
+              // Forward the error to the observer.
+              observer.error(error);
+              // Reload the data table content because some deletions might
+              // have been executed successfully in the meanwhile.
               this.table.refreshBtn();
-            });
+            },
+            () => {
+              // Notify the observer that we are done.
+              observer.complete();
+              // Reload the data table content.
+              this.table.refreshBtn();
+            }
+          );
         });
       },
       modalRef: modalRef
     });
   }
-
 }
