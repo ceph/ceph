d0505df63c8e71d88e6c1493d45762d2883195ce
Tiago Melo <tmelo@suse.com>
2018-05-17 18:08:51 +0100

    mgr/dashboard: Use Jest in package.json scripts
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/run-frontend-unittests.sh
+++ src/pybind/mgr/dashboard/run-frontend-unittests.sh
@@ -7,7 +7,7 @@ cd $CEPH_ROOT/src/pybind/mgr/dashboard/frontend
 .  $CEPH_ROOT/build/src/pybind/mgr/dashboard/node-env/bin/activate
 
 npm run build -- --prod
-npm run test -- --browsers PhantomJS --watch=false
+npm run test:ci
 npm run lint
 
 deactivate
