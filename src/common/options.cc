86b3d7da02094ffd8f436d13df6fbce0814eb707
Igor Fedotov <ifedotov@suse.com>
2018-05-18 22:14:59 +0300

    os/bluestore: default to bitmap allocator for bluestore/bluefs
    Signed-off-by: Igor Fedotov <ifedotov@suse.com>
    (cherry picked from commit 231b7dd9c5dc1d22e93a8f81d07e772a6615f9f0)
    
    Conflicts:
    	src/common/options.cc: this a dev option in luminous

--- src/common/options.cc
+++ src/common/options.cc
@@ -3314,7 +3314,7 @@ std::vector<Option> get_global_options() {
     .set_description(""),
 
     Option("bluefs_allocator", Option::TYPE_STR, Option::LEVEL_DEV)
-    .set_default("stupid")
+    .set_default("bitmap")
     .set_description(""),
 
     Option("bluefs_preextend_wal_files", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
@@ -3655,7 +3655,7 @@ std::vector<Option> get_global_options() {
     .set_description("Key value database to use for bluestore"),
 
     Option("bluestore_allocator", Option::TYPE_STR, Option::LEVEL_DEV)
-    .set_default("stupid")
+    .set_default("bitmap")
     .set_enum_allowed({"bitmap", "stupid"})
     .set_description("Allocator policy"),
 
