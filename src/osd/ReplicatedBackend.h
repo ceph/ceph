06d5c05358b311e3c6d7955c3f8a7634413571a6
Sage Weil <sage@redhat.com>
2018-05-21 16:39:47 -0500

    osd/ReplicatedBackend: fix use-after-free on InProgressOp
    - op in flight to disk...
    - on_change() clears the InProgressOp
    - C_OSD_OnOpCommit calls op_commit() w/ bare pointer
    - crash!
    
    Fix by refcounting InProgressOp and clearing on_commit when it is
    canceled.
    
    Fixes: http://tracker.ceph.com/issues/24219
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/ReplicatedBackend.h
+++ src/osd/ReplicatedBackend.h
@@ -327,7 +327,7 @@ private:
   /**
    * Client IO
    */
-  struct InProgressOp {
+  struct InProgressOp : public RefCountedObject {
     ceph_tid_t tid;
     set<pg_shard_t> waiting_for_commit;
     Context *on_commit;
@@ -336,13 +336,15 @@ private:
     InProgressOp(
       ceph_tid_t tid, Context *on_commit,
       OpRequestRef op, eversion_t v)
-      : tid(tid), on_commit(on_commit),
+      : RefCountedObject(nullptr, 0),
+	tid(tid), on_commit(on_commit),
 	op(op), v(v) {}
     bool done() const {
       return waiting_for_commit.empty();
     }
   };
-  map<ceph_tid_t, InProgressOp> in_progress_ops;
+  typedef boost::intrusive_ptr<InProgressOp> InProgressOpRef;
+  map<ceph_tid_t, InProgressOpRef> in_progress_ops;
 public:
   friend class C_OSD_OnOpCommit;
 
@@ -395,7 +397,7 @@ private:
     boost::optional<pg_hit_set_history_t> &hset_history,
     InProgressOp *op,
     ObjectStore::Transaction &op_t);
-  void op_commit(InProgressOp *op);
+  void op_commit(InProgressOpRef& op);
   void do_repop_reply(OpRequestRef op);
   void do_repop(OpRequestRef op);
 
