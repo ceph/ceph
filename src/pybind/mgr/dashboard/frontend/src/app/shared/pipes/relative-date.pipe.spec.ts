133bedafbfbc42c943dddff079b0940ea7f76bb2
Tiago Melo <tmelo@suse.com>
2018-05-23 14:30:55 +0100

    mgr/dashboard: Add unit tests for all frontend pipes
    Signed-off-by: Tiago Melo <tmelo@suse.com>
    (cherry picked from commit b6305630c417bcb92c55a6b461c63ca0911a8b03)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/pipes/relative-date.pipe.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/pipes/relative-date.pipe.spec.ts
@@ -1,8 +1,26 @@
+import * as moment from 'moment';
+
 import { RelativeDatePipe } from './relative-date.pipe';
 
 describe('RelativeDatePipe', () => {
+  const pipe = new RelativeDatePipe();
+
   it('create an instance', () => {
-    const pipe = new RelativeDatePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('transforms without value', () => {
+    const value = undefined;
+    expect(pipe.transform(value)).toBe('unknown');
+  });
+
+  it('transforms "in 7 days"', () => {
+    const value = moment().add(7, 'days').unix();
+    expect(pipe.transform(value)).toBe('in 7 days');
+  });
+
+  it('transforms "7 days ago"', () => {
+    const value = moment().subtract(7, 'days').unix();
+    expect(pipe.transform(value)).toBe('7 days ago');
   });
 });
