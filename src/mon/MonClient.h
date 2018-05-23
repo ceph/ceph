2bdab228492bcc71c6ff071b8fe744a2aed6b8a9
Sage Weil <sage@redhat.com>
2018-05-23 17:37:00 -0500

    mon/MonClient: make connection check more robust
    Do no rely on the address comparison; just check the Connection*
    itself.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mon/MonClient.h
+++ src/mon/MonClient.h
@@ -216,6 +216,16 @@ private:
   void _add_conns(uint64_t global_id);
   void _send_mon_message(Message *m);
 
+  std::map<entity_addr_t, MonConnection>::iterator _find_pending_con(
+    const ConnectionRef& con) {
+    for (auto i = pending_cons.begin(); i != pending_cons.end(); ++i) {
+      if (i->second.get_con() == con) {
+	return i;
+      }
+    }
+    return pending_cons.end();
+  }
+
 public:
   void set_entity_name(EntityName name) { entity_name = name; }
 
