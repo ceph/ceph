3417cd73df631c937dab9dfb71a9cf70bc009b9a
Tiago Melo <tmelo@suse.com>
2018-05-28 14:19:26 +0100

    mgr/dashboard: Improve max-line-length tslint rule
    Prettier is not breaking strings if they are bigger
    then the defined max line length.
    
    With this rule we can make sure lines are not exceeding it,
    with the exception of import/export lines.
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>
    (cherry picked from commit fc67af05b67cb2bbb76fcbea35cf10b1284d25de)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/pipes/ceph-short-version.pipe.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/pipes/ceph-short-version.pipe.spec.ts
@@ -9,7 +9,8 @@ describe('CephShortVersionPipe', () => {
 
   it('transforms with correct version format', () => {
     const value =
-      'ceph version 13.1.0-534-g23d3751b89 (23d3751b897b31d2bda57aeaf01acb5ff3c4a9cd) nautilus (dev)';
+      'ceph version 13.1.0-534-g23d3751b89 \
+       (23d3751b897b31d2bda57aeaf01acb5ff3c4a9cd) nautilus (dev)';
     expect(pipe.transform(value)).toBe('13.1.0-534-g23d3751b89');
   });
 
