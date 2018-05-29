fb529ef6bbb4ed9cfad8c70e1fb5658606279356
Orit Wasserman <owasserm@redhat.com>
2018-05-29 18:24:16 +0300

    rgw: add os info to LocalRemote
    Signed-off-by: Orit Wasserman <owasserm@redhat.com>

--- qa/tasks/vstart_runner.py
+++ qa/tasks/vstart_runner.py
@@ -208,6 +208,15 @@ class LocalRemoteProcess(object):
 
         return FakeStdIn(self)
 
+class OS:
+    def __init__(self):
+        (os, ver ,  name) = platform.dist()
+        self.name = os
+        if self.name is 'ubuntu':
+            self.package_type = 'deb'
+        else:
+            self.package_type = 'rpm'
+
 
 class LocalRemote(object):
     """
@@ -221,6 +230,7 @@ class LocalRemote(object):
         self.name = "local"
         self.hostname = "localhost"
         self.user = getpass.getuser()
+        self.os = OS()
 
     def get_file(self, path, sudo, dest_dir):
         tmpfile = tempfile.NamedTemporaryFile(delete=False).name
