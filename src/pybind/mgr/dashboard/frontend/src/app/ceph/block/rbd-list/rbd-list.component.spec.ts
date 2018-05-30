cbaa518c0117777b7e985dfc2fde926766759855
Stephan Müller <smueller@suse.com>
2018-05-30 15:22:31 +0200

    mgr/dashboard: Use task wrapper in RBD list
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-list/rbd-list.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-list/rbd-list.component.spec.ts
@@ -1,21 +1,17 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ToastModule } from 'ng2-toastr';
 import {
   AlertModule,
   BsDropdownModule,
-  BsModalRef,
   ModalModule,
   TabsModule,
   TooltipModule
 } from 'ngx-bootstrap';
-import { throwError as observableThrowError } from 'rxjs';
 
-import { RbdService } from '../../../shared/api/rbd.service';
 import { ComponentsModule } from '../../../shared/components/components.module';
-import { NotificationService } from '../../../shared/services/notification.service';
 import { SharedModule } from '../../../shared/shared.module';
 import { configureTestBed } from '../../../shared/unit-test-helper';
 import { RbdDetailsComponent } from '../rbd-details/rbd-details.component';
@@ -50,42 +46,5 @@ describe('RbdListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  describe('api delete request', () => {
-    let called;
-    let rbdService: RbdService;
-    let notificationService: NotificationService;
-
-    beforeEach(() => {
-      called = false;
-      rbdService = new RbdService(null);
-      notificationService = new NotificationService(null, null);
-      component = new RbdListComponent(
-        rbdService,
-        null,
-        null,
-        null,
-        null,
-        notificationService,
-        null,
-        null
-      );
-      spyOn(rbdService, 'delete').and.returnValue(observableThrowError({ status: 500 }));
-      spyOn(notificationService, 'notifyTask').and.stub();
-      component.modalRef = new BsModalRef();
-      component.modalRef.content = {
-        stopLoadingSpinner: () => (called = true)
-      };
-    });
-
-    it('should make sure that if the deletion fails stopLoadingSpinner is called', <any>fakeAsync(
-      () => {
-        expect(called).toBe(false);
-        component.deleteRbd('sth', 'test');
-        tick(500);
-        expect(called).toBe(true);
-      }
-    ));
   });
 });
