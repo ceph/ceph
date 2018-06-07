6d7d01ef99f290b58ec1cd27f8f99fde3d690ebe
Sage Weil <sage@redhat.com>
2018-06-07 08:33:46 -0500

    osd/PG: normalize query processing in Stray and ReplicaActive
    A stray PG may end up in ReplicaActive if it is participating in backfill.
    However, whether it is or isn't, we should treat queries the same.
    Otherwise we end up with weird behaviors like:
    
     - osd's stray pg moves to ReplicaActive (gets info+log from primary)
     - osd goes down and back up
     - primary restarts peering, request FULLLOG to find missing objects
     - osd ignores FULLLOG because it is ReplicaActive and not Stray
    
    Fixes: http://tracker.ceph.com/issues/24373
    Reported-by: Kouya Shimura <kouya@jp.fujitsu.com>
    Signed-off-by: Sage Weil <sage@redhat.com>
    (cherry picked from commit 4e14a380becd61d823a1c86e2dbb41dc8e3a5834)

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -2939,7 +2939,7 @@ protected:
   void fulfill_info(pg_shard_t from, const pg_query_t &query,
 		    pair<pg_shard_t, pg_info_t> &notify_info);
   void fulfill_log(pg_shard_t from, const pg_query_t &query, epoch_t query_epoch);
-
+  void fulfill_query(const MQuery& q, RecoveryCtx *rctx);
   void check_full_transition(OSDMapRef lastmap, OSDMapRef osdmap);
 
   bool should_restart_peering(
