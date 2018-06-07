ceph/ceph

commit 87b256632efbf46c67ace60eebdffcf163051dc4

mgr: use entity_addrvec_t for MgrMap
This does several entity_addr_t -> entity_addrvec_t conversions across the
mgr code.

(Note that entity_addr_t and entity_addrvec_t already encode in an
interoperable way.)

Signed-off-by: Sage Weil <sage@redhat.com>

src/mon/MgrMap.h
@@ -132,7 +132,7 @@ public:
   /// global_id of the ceph-mgr instance selected as a leader
   uint64_t active_gid = 0;
   /// server address reported by the leader once it is active
-  entity_addr_t active_addr;
+  entity_addrvec_t active_addrs;
   /// whether the nominated leader is active (i.e. has initialized its server)
   bool available = false;
   /// the name (foo in mgr.<foo>) of the active daemon
@@ -151,7 +151,7 @@ public:
   std::map<std::string, std::string> services;
 
   epoch_t get_epoch() const { return epoch; }
-  entity_addr_t get_active_addr() const { return active_addr; }
+  entity_addrvec_t get_active_addrs() const { return active_addrs; }
   uint64_t get_active_gid() const { return active_gid; }
   bool get_available() const { return available; }
   const std::string &get_active_name() const { return active_name; }
@@ -235,9 +235,9 @@ public:
 
   void encode(bufferlist& bl, uint64_t features) const
   {
-    ENCODE_START(4, 1, bl);
+    ENCODE_START(5, 1, bl);
     encode(epoch, bl);
-    encode(active_addr, bl, features);
+    encode(active_addrs, bl, features);
     encode(active_gid, bl);
     encode(available, bl);
     encode(active_name, bl);
@@ -259,9 +259,9 @@ public:
 
   void decode(bufferlist::const_iterator& p)
   {
-    DECODE_START(4, p);
+    DECODE_START(5, p);
     decode(epoch, p);
-    decode(active_addr, p);
+    decode(active_addrs, p);
     decode(active_gid, p);
     decode(available, p);
     decode(active_name, p);
@@ -295,7 +295,7 @@ public:
     f->dump_int("epoch", epoch);
     f->dump_int("active_gid", get_active_gid());
     f->dump_string("active_name", get_active_name());
-    f->dump_stream("active_addr") << active_addr;
+    f->dump_object("active_addrs", active_addrs);
     f->dump_bool("available", available);
     f->open_array_section("standbys");
     for (const auto &i : standbys) {

src/messages/MMgrBeacon.h
@@ -30,7 +30,7 @@ class MMgrBeacon : public PaxosServiceMessage {
 
 protected:
   uint64_t gid;
-  entity_addr_t server_addr;
+  entity_addrvec_t server_addrs;
   bool available;
   std::string name;
   uuid_d fsid;
@@ -54,17 +54,17 @@ public:
   }
 
   MMgrBeacon(const uuid_d& fsid_, uint64_t gid_, const std::string &name_,
-             entity_addr_t server_addr_, bool available_,
+             entity_addrvec_t server_addrs_, bool available_,
 	     std::vector<MgrMap::ModuleInfo>&& modules_,
 	     map<string,string>&& metadata_)
     : PaxosServiceMessage(MSG_MGR_BEACON, 0, HEAD_VERSION, COMPAT_VERSION),
-      gid(gid_), server_addr(server_addr_), available(available_), name(name_),
+      gid(gid_), server_addrs(server_addrs_), available(available_), name(name_),
       fsid(fsid_), modules(std::move(modules_)), metadata(std::move(metadata_))
   {
   }
 
   uint64_t get_gid() const { return gid; }
-  entity_addr_t get_server_addr() const { return server_addr; }
+  entity_addrvec_t get_server_addrs() const { return server_addrs; }
   bool get_available() const { return available; }
   const std::string& get_name() const { return name; }
   const uuid_d& get_fsid() const { return fsid; }
@@ -106,7 +106,7 @@ public:
 
   void print(ostream& out) const override {
     out << get_type_name() << " mgr." << name << "(" << fsid << ","
-	<< gid << ", " << server_addr << ", " << available
+	<< gid << ", " << server_addrs << ", " << available
 	<< ")";
   }
 
@@ -114,7 +114,7 @@ public:
     using ceph::encode;
     paxos_encode();
 
-    encode(server_addr, payload, features);
+    encode(server_addrs, payload, features);
     encode(gid, payload);
     encode(available, payload);
     encode(name, payload);
@@ -137,7 +137,7 @@ public:
   void decode_payload() override {
     auto p = payload.cbegin();
     paxos_decode(p);
-    decode(server_addr, p);
+    decode(server_addrs, p);
     decode(gid, p);
     decode(available, p);
     decode(name, p);

src/mgr/DaemonServer.cc
@@ -138,9 +138,9 @@ int DaemonServer::init(uint64_t gid, entity_addr_t client_addr)
   return 0;
 }
 
-entity_addr_t DaemonServer::get_myaddr() const
+entity_addrvec_t DaemonServer::get_myaddrs() const
 {
-  return msgr->get_myaddr();
+  return msgr->get_myaddrs();
 }
 
 

src/mgr/DaemonServer.h
@@ -107,7 +107,7 @@ public:
   int init(uint64_t gid, entity_addr_t client_addr);
   void shutdown();
 
-  entity_addr_t get_myaddr() const;
+  entity_addrvec_t get_myaddrs() const;
 
   DaemonServer(MonClient *monc_,
                Finisher &finisher_,

src/mgr/Mgr.cc
@@ -202,7 +202,7 @@ void Mgr::init()
     // systemd restart us.
     exit(1);
   }
-  dout(4) << "Initialized server at " << server.get_myaddr() << dendl;
+  dout(4) << "Initialized server at " << server.get_myaddrs() << dendl;
 
   // Preload all daemon metadata (will subsequently keep this
   // up to date by watching maps, so do the initial load before

src/mgr/Mgr.h
@@ -78,7 +78,9 @@ public:
   ~Mgr();
 
   bool is_initialized() const {return initialized;}
-  entity_addr_t get_server_addr() const { return server.get_myaddr(); }
+  entity_addrvec_t get_server_addrs() const {
+    return server.get_myaddrs();
+  }
 
   void handle_mgr_digest(MMgrDigest* m);
   void handle_fs_map(MFSMap* m);

src/mgr/MgrClient.cc
@@ -149,15 +149,13 @@ void MgrClient::reconnect()
     connect_retry_callback = nullptr;
   }
 
-  ldout(cct, 4) << "Starting new session with " << map.get_active_addr()
+  ldout(cct, 4) << "Starting new session with " << map.get_active_addrs()
 		<< dendl;
-  entity_inst_t inst;
-  inst.addr = map.get_active_addr();
-  inst.name = entity_name_t::MGR(map.get_active_gid());
   last_connect_attempt = ceph_clock_now();
 
   session.reset(new MgrSessionState());
-  session->con = msgr->get_connection(inst);
+  session->con = msgr->connect_to(CEPH_ENTITY_TYPE_MGR,
+				  map.get_active_addrs());
 
   if (service_daemon) {
     daemon_dirty_status = true;
@@ -208,11 +206,11 @@ bool MgrClient::handle_mgr_map(MMgrMap *m)
   ldout(cct, 4) << "Got map version " << map.epoch << dendl;
   m->put();
 
-  ldout(cct, 4) << "Active mgr is now " << map.get_active_addr() << dendl;
+  ldout(cct, 4) << "Active mgr is now " << map.get_active_addrs() << dendl;
 
   // Reset session?
   if (!session ||
-      session->con->get_peer_addr() != map.get_active_addr()) {
+      session->con->get_peer_addrs() != map.get_active_addrs()) {
     reconnect();
   }
 

src/mgr/MgrStandby.cc
@@ -199,7 +199,7 @@ void MgrStandby::send_beacon()
   // as available in the map)
   bool available = active_mgr != nullptr && active_mgr->is_initialized();
 
-  auto addr = available ? active_mgr->get_server_addr() : entity_addr_t();
+  auto addrs = available ? active_mgr->get_server_addrs() : entity_addrvec_t();
   dout(10) << "sending beacon as gid " << monc.get_global_id() << dendl;
 
   map<string,string> metadata;
@@ -209,7 +209,7 @@ void MgrStandby::send_beacon()
   MMgrBeacon *m = new MMgrBeacon(monc.get_fsid(),
 				 monc.get_global_id(),
                                  g_conf->name.get_id(),
-                                 addr,
+                                 addrs,
                                  available,
 				 std::move(module_info),
 				 std::move(metadata));

commit d991c44db598a8ba39656298882931179add496a

mon/MgrMap: fix pre-addrvec compat
This was broken by 87b256632efbf46c67ace60eebdffcf163051dc4.  Old code
does not decode an addrvec as an addr.

Take the opportunity to remove the old_available_modules field.

Signed-off-by: Sage Weil <sage@redhat.com>

src/mon/MgrMap.h
@@ -235,7 +235,30 @@ public:
 
   void encode(bufferlist& bl, uint64_t features) const
   {
-    ENCODE_START(5, 1, bl);
+    if (!HAVE_FEATURE(features, SERVER_NAUTILUS)) {
+      ENCODE_START(5, 1, bl);
+      encode(epoch, bl);
+      encode(active_addrs.legacy_addr(), bl, features);
+      encode(active_gid, bl);
+      encode(available, bl);
+      encode(active_name, bl);
+      encode(standbys, bl);
+      encode(modules, bl);
+
+      // Pre-version 4 string list of available modules
+      // (replaced by direct encode of ModuleInfo below)
+      std::set<std::string> old_available_modules;
+      for (const auto &i : available_modules) {
+	old_available_modules.insert(i.name);
+      }
+      encode(old_available_modules, bl);
+
+      encode(services, bl);
+      encode(available_modules, bl);
+      ENCODE_FINISH(bl);
+      return;
+    }
+    ENCODE_START(6, 6, bl);
     encode(epoch, bl);
     encode(active_addrs, bl, features);
     encode(active_gid, bl);
@@ -243,23 +266,15 @@ public:
     encode(active_name, bl);
     encode(standbys, bl);
     encode(modules, bl);
-
-    // Pre-version 4 string list of available modules
-    // (replaced by direct encode of ModuleInfo below)
-    std::set<std::string> old_available_modules;
-    for (const auto &i : available_modules) {
-      old_available_modules.insert(i.name);
-    }
-    encode(old_available_modules, bl);
-
     encode(services, bl);
     encode(available_modules, bl);
     ENCODE_FINISH(bl);
+    return;
   }
 
   void decode(bufferlist::const_iterator& p)
   {
-    DECODE_START(5, p);
+    DECODE_START(6, p);
     decode(epoch, p);
     decode(active_addrs, p);
     decode(active_gid, p);
@@ -269,17 +284,19 @@ public:
     if (struct_v >= 2) {
       decode(modules, p);
 
-      // Reconstitute ModuleInfos from names
-      std::set<std::string> module_name_list;
-      decode(module_name_list, p);
-      // Only need to unpack this field if we won't have the full
-      // MgrMap::ModuleInfo structures added in v4
-      if (struct_v < 4) {
-        for (const auto &i : module_name_list) {
-          MgrMap::ModuleInfo info;
-          info.name = i;
-          available_modules.push_back(std::move(info));
-        }
+      if (struct_v < 6) {
+	// Reconstitute ModuleInfos from names
+	std::set<std::string> module_name_list;
+	decode(module_name_list, p);
+	// Only need to unpack this field if we won't have the full
+	// MgrMap::ModuleInfo structures added in v4
+	if (struct_v < 4) {
+	  for (const auto &i : module_name_list) {
+	    MgrMap::ModuleInfo info;
+	    info.name = i;
+	    available_modules.push_back(std::move(info));
+	  }
+	}
       }
     }
     if (struct_v >= 3) {