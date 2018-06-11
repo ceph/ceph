d0b797ef3d5b805eb4ffe4cef9fd5e5d113bccbc
Erwan Velu <erwan@redhat.com>
2018-06-11 16:09:53 +0200

    test/encoding/types.h: Adjusting src/mds entries
    This patch is about:
    - Adding the missing headers from src/mds
    - Adding the missing types from the existing headers
    - Adding blank lines to keep includes and types in a visual block.
      That is much easier to read and keep the same visual as the rest of the file.
    
    Signed-off-by: Erwan Velu <erwan@redhat.com>

--- src/test/encoding/types.h
+++ src/test/encoding/types.h
@@ -217,6 +217,7 @@ TYPE(sr_t)
 #include "mds/mdstypes.h"
 TYPE(frag_info_t)
 TYPE(nest_info_t)
+TYPE(quota_info_t)
 TYPE(client_writeable_range_t)
 TYPE_FEATUREFUL(inode_t<std::allocator>)
 TYPE_FEATUREFUL(old_inode_t<std::allocator>)
@@ -226,13 +227,13 @@ TYPE_FEATUREFUL(session_info_t)
 TYPE(string_snap_t)
 TYPE(MDSCacheObjectInfo)
 TYPE(mds_table_pending_t)
+TYPE(cap_reconnect_t)
 TYPE(inode_load_vec_t)
 TYPE(dirfrag_load_vec_t)
 TYPE(mds_load_t)
-TYPE(cap_reconnect_t)
+TYPE(MDSCacheObjectInfo)
 TYPE(inode_backtrace_t)
 TYPE(inode_backpointer_t)
-TYPE(quota_info_t)
 
 #include "mds/CInode.h"
 TYPE_FEATUREFUL(InodeStore)
@@ -249,6 +250,10 @@ TYPE_FEATUREFUL(FSMap)
 #include "mds/Capability.h"
 TYPE_NOCOPY(Capability)
 
+#include "mds/inode_backtrace.h"
+TYPE(inode_backpointer_t)
+TYPE(inode_backtrace_t)
+
 #include "mds/InoTable.h"
 TYPE(InoTable)
 
@@ -257,40 +262,54 @@ TYPE_STRAYDATA(SnapServer)
 
 #include "mds/events/ECommitted.h"
 TYPE_FEATUREFUL(ECommitted)
+
 #include "mds/events/EExport.h"
 TYPE_FEATUREFUL(EExport)
+
 #include "mds/events/EFragment.h"
 TYPE_FEATUREFUL(EFragment)
+
 #include "mds/events/EImportFinish.h"
 TYPE_FEATUREFUL(EImportFinish)
+
 #include "mds/events/EImportStart.h"
 TYPE_FEATUREFUL(EImportStart)
+
 #include "mds/events/EMetaBlob.h"
 TYPE_FEATUREFUL_NOCOPY(EMetaBlob::fullbit)
 TYPE(EMetaBlob::remotebit)
 TYPE(EMetaBlob::nullbit)
 TYPE_FEATUREFUL(EMetaBlob::dirlump)
 TYPE_FEATUREFUL(EMetaBlob)
+
 #include "mds/events/EOpen.h"
 TYPE_FEATUREFUL(EOpen)
+
 #include "mds/events/EResetJournal.h"
 TYPE_FEATUREFUL(EResetJournal)
+
 #include "mds/events/ESession.h"
 TYPE_FEATUREFUL(ESession)
+
 #include "mds/events/ESessions.h"
 TYPE_FEATUREFUL(ESessions)
+
 #include "mds/events/ESlaveUpdate.h"
 TYPE(link_rollback)
 TYPE(rmdir_rollback)
 TYPE(rename_rollback::drec)
 TYPE(rename_rollback)
 TYPE_FEATUREFUL(ESlaveUpdate)
+
 #include "mds/events/ESubtreeMap.h"
 TYPE_FEATUREFUL(ESubtreeMap)
+
 #include "mds/events/ETableClient.h"
 TYPE_FEATUREFUL(ETableClient)
+
 #include "mds/events/ETableServer.h"
 TYPE_FEATUREFUL(ETableServer)
+
 #include "mds/events/EUpdate.h"
 TYPE_FEATUREFUL(EUpdate)
 
