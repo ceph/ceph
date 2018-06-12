5b3a54004521bb0c3c464813cc40a0e3a6f019fe
Alfredo Deza <adeza@redhat.com>
2018-06-12 09:42:33 -0400

    qa/standalone remove ceph-disk from activate_osd helper
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- qa/standalone/ceph-helpers.sh
+++ qa/standalone/ceph-helpers.sh
@@ -553,9 +553,8 @@ function run_mgr() {
 # The CEPH_CONF variable is expected to be set to /dev/null to
 # only rely on arguments for configuration.
 #
-# The run_osd function creates the OSD data directory with ceph-disk
-# prepare on the **dir**/**id** directory and relies on the
-# activate_osd function to run the daemon.
+# The run_osd function creates the OSD data directory on the **dir**/**id**
+# directory and relies on the activate_osd function to run the daemon.
 #
 # Examples:
 #
@@ -750,7 +749,7 @@ function test_destroy_osd() {
 # The activate_osd function expects a valid OSD data directory
 # in **dir**/**id**, either just created via run_osd or re-using
 # one left by a previous run of ceph-osd. The ceph-osd daemon is
-# run indirectly via ceph-disk activate.
+# run directly on the foreground
 #
 # The activate_osd function blocks until the monitor reports the osd
 # up. If it fails to do so within $TIMEOUT seconds, activate_osd
@@ -774,17 +773,12 @@ function activate_osd() {
     shift
     local osd_data=$dir/$id
 
-    local ceph_disk_args
-    ceph_disk_args+=" --verbose"
-    ceph_disk_args+=" --statedir=$dir"
-    ceph_disk_args+=" --sysconfdir=$dir"
-    ceph_disk_args+=" --prepend-to-path="
-
     local ceph_args="$CEPH_ARGS"
     ceph_args+=" --osd-failsafe-full-ratio=.99"
     ceph_args+=" --osd-journal-size=100"
     ceph_args+=" --osd-scrub-load-threshold=2000"
     ceph_args+=" --osd-data=$osd_data"
+    ceph_args+=" --osd-journal=${osd_data}/journal"
     ceph_args+=" --chdir="
     ceph_args+=$EXTRA_OPTS
     ceph_args+=" --run-dir=$dir"
@@ -798,10 +792,9 @@ function activate_osd() {
     ceph_args+=" "
     ceph_args+="$@"
     mkdir -p $osd_data
-    CEPH_ARGS="$ceph_args " ceph-disk $ceph_disk_args \
-        activate \
-        --mark-init=none \
-        $osd_data || return 1
+
+    echo start osd.$id
+    ceph-osd -i $id $ceph_args &
 
     [ "$id" = "$(cat $osd_data/whoami)" ] || return 1
 
