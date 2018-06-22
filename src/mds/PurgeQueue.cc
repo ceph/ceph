462258079b2935cde9191088123cfaf2da74edd9
Venky Shankar <vshankar@redhat.com>
2018-06-22 01:43:37 -0400

    mds: reorganize and curate perf log counters
    Categorize perf counters into high (PRIO_{CRITICAL/INTERESTING}),
    medium (PRIO_USEFUL) and low priority (PRIO_DEBUGONLY). High and
    medium priority stats are transmitted to ceph mgr and included in
    prometheus output.
    
    Signed-off-by: Venky Shankar <vshankar@redhat.com>
    Fixes: http://tracker.ceph.com/issues/24004
    (cherry picked from commit 48a33f6f06802fee998f60a2ec35d9041a7c51a6)
    
    Conflicts:
    	src/mds/MDSRank.cc

--- src/mds/PurgeQueue.cc
+++ src/mds/PurgeQueue.cc
@@ -97,12 +97,14 @@ PurgeQueue::~PurgeQueue()
 
 void PurgeQueue::create_logger()
 {
-  PerfCountersBuilder pcb(g_ceph_context,
-          "purge_queue", l_pq_first, l_pq_last);
+  PerfCountersBuilder pcb(g_ceph_context, "purge_queue", l_pq_first, l_pq_last);
+
+  pcb.add_u64_counter(l_pq_executed, "pq_executed", "Purge queue tasks executed",
+                      "purg", PerfCountersBuilder::PRIO_INTERESTING);
+
+  pcb.set_prio_default(PerfCountersBuilder::PRIO_USEFUL);
   pcb.add_u64(l_pq_executing_ops, "pq_executing_ops", "Purge queue ops in flight");
   pcb.add_u64(l_pq_executing, "pq_executing", "Purge queue tasks in flight");
-  pcb.add_u64_counter(l_pq_executed, "pq_executed", "Purge queue tasks executed", "purg",
-      PerfCountersBuilder::PRIO_INTERESTING);
 
   logger.reset(pcb.create_perf_counters());
   g_ceph_context->get_perfcounters_collection()->add(logger.get());
