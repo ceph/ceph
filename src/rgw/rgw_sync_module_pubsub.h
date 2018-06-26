4a89cb71242e9bad32afbd28d112b37e29f53a48
Yehuda Sadeh <yehuda@redhat.com>
2018-06-26 16:02:14 -0700

    rgw: initial pub sub work
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

---
+++ src/rgw/rgw_sync_module_pubsub.h
@@ -0,0 +1,26 @@
+#ifndef CEPH_RGW_SYNC_MODULE_PUBSUB_H
+#define CEPH_RGW_SYNC_MODULE_PUBSUB_H
+
+#include "rgw_sync_module.h"
+
+class RGWPSSyncModule : public RGWSyncModule {
+public:
+  RGWPSSyncModule() {}
+  bool supports_data_export() override {
+    return false;
+  }
+  int create_instance(CephContext *cct, const JSONFormattable& config, RGWSyncModuleInstanceRef *instance) override;
+};
+
+class RGWPSDataSyncModule;
+class RGWRESTConn;
+
+class RGWPSSyncModuleInstance : public RGWSyncModuleInstance {
+  std::unique_ptr<RGWPSDataSyncModule> data_handler;
+public:
+  RGWPSSyncModuleInstance(CephContext *cct, const JSONFormattable& config);
+  RGWDataSyncModule *get_data_handler() override;
+  RGWRESTMgr *get_rest_filter(int dialect, RGWRESTMgr *orig) override;
+};
+
+#endif
