610ac4ea0fafbf2d3f9bee6fd92d9f2cfba85d3e
Tsung-Ju Lii <usefulalgorithm@gmail.com>
2018-06-26 10:57:51 +0800

    mds/Mantle: Fixed Mantle's obsolete Lua C++ API
    Changed Mantle's Lua C++ API from using several `luaopen_*` calls to
    one `luaL_openlibs` call, since the former has been deprecated from the
    release of Lua 5.1.
    
    Signed-off-by: Tsung-Ju (Andy Lii) <usefulalgorithm@gmail.com>

--- src/mds/Mantle.cc
+++ src/mds/Mantle.cc
@@ -119,12 +119,7 @@ Mantle::Mantle (void)
   }
 
   /* balancer policies can use basic Lua functions */
-  luaopen_base(L);
-  luaopen_coroutine(L);
-  luaopen_string(L);
-  luaopen_math(L);
-  luaopen_table(L);
-  luaopen_utf8(L);
+  luaL_openlibs(L);
 
   /* setup debugging */
   lua_register(L, "BAL_LOG", dout_wrapper);
