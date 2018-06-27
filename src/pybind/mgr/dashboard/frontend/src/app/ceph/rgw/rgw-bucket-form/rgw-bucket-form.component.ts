87bc49a91699947f2a712d222caa2b345161b606
Stephan Müller <smueller@suse.com>
2018-06-27 11:33:09 +0200

    mgr/dashboard: Use forms in RGW bucket form
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-form/rgw-bucket-form.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-form/rgw-bucket-form.component.ts
@@ -1,18 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  AbstractControl,
-  AsyncValidatorFn,
-  FormBuilder,
-  FormGroup,
-  ValidationErrors,
-  Validators
-} from '@angular/forms';
+import { AbstractControl, AsyncValidatorFn, ValidationErrors, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import * as _ from 'lodash';
 
 import { RgwBucketService } from '../../../shared/api/rgw-bucket.service';
 import { RgwUserService } from '../../../shared/api/rgw-user.service';
+import { CdFormBuilder } from '../../../shared/forms/cd-form-builder';
+import { CdFormGroup } from '../../../shared/forms/cd-form-group';
 
 @Component({
   selector: 'cd-rgw-bucket-form',
@@ -20,16 +15,16 @@ import { RgwUserService } from '../../../shared/api/rgw-user.service';
   styleUrls: ['./rgw-bucket-form.component.scss']
 })
 export class RgwBucketFormComponent implements OnInit {
-  bucketForm: FormGroup;
+  bucketForm: CdFormGroup;
   editing = false;
   error = false;
   loading = false;
   owners = null;
 
   constructor(
-    private formBuilder: FormBuilder,
     private route: ActivatedRoute,
     private router: Router,
+    private formBuilder: CdFormBuilder,
     private rgwBucketService: RgwBucketService,
     private rgwUserService: RgwUserService
   ) {
