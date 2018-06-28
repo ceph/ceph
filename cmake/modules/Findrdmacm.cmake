63656af5f224b2c07cd74ed0197984bc94760014
Kefu Chai <kchai@redhat.com>
2018-06-28 17:05:53 +0800

    cmake: extract the RDMA libraries interface properties out
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- cmake/modules/Findrdmacm.cmake
+++ cmake/modules/Findrdmacm.cmake
@@ -11,6 +11,16 @@ find_library(RDMACM_LIBRARIES rdmacm)
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(rdmacm DEFAULT_MSG RDMACM_LIBRARIES RDMACM_INCLUDE_DIR)
 
+if(RDMACM_FOUND)
+  if(NOT TARGET RDMA::RDMAcm)
+    add_library(RDMA::RDMAcm UNKNOWN IMPORTED)
+  endif()
+  set_target_properties(RDMA::RDMAcm PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${RDMACM_INCLUDE_DIR}"
+    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+    IMPORTED_LOCATION "${RDMACM_LIBRARIES}")
+endif()
+
 mark_as_advanced(
   RDMACM_LIBRARIES
 )
