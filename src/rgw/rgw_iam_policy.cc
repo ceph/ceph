b864b242df40188e40e4bed3d808b92304461ce2
Pritha Srivastava <prsrivas@redhat.com>
2018-06-28 11:25:21 +0530

    rgw: Corrected code for ARNs for entities that have a path.
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>

--- src/rgw/rgw_iam_policy.cc
+++ src/rgw/rgw_iam_policy.cc
@@ -208,13 +208,14 @@ ARN::ARN(const rgw_bucket& b, const string& o)
   resource.append(o);
 }
 
-ARN::ARN(const string& resource_name, const string& type, const string& tenant)
+ARN::ARN(const string& resource_name, const string& type, const string& tenant, bool has_path)
   : partition(Partition::aws),
     service(Service::iam),
     region(),
     account(tenant),
     resource(type) {
-  resource.push_back('/');
+  if (! has_path)
+    resource.push_back('/');
   resource.append(resource_name);
 }
 
