3204893c0ea7bc462842f5d16f8eb1709008d684
Ricardo Marques <rimarques@suse.com>
2018-06-28 16:11:36 +0100

    mgr/dashboard: Add "About" modal
    Fixes: https://tracker.ceph.com/issues/24646
    
    Signed-off-by: Ricardo Marques <rimarques@suse.com>

---
+++ src/pybind/mgr/dashboard/frontend/src/app/core/navigation/about/about.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+import { BsModalRef } from 'ngx-bootstrap';
+import { Subscription } from 'rxjs';
+
+import { SummaryService } from '../../../shared/services/summary.service';
+
+@Component({
+  selector: 'cd-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.scss']
+})
+export class AboutComponent implements OnInit, OnDestroy {
+  versionNumber: string;
+  versionHash: string;
+  versionName: string;
+  subs: Subscription;
+
+  constructor(public modalRef: BsModalRef, private summaryService: SummaryService) {}
+
+  ngOnInit() {
+    this.subs = this.summaryService.summaryData$.subscribe((summary: any) => {
+      if (!summary) {
+        return;
+      }
+      const version = summary.version.replace('ceph version ', '').split(' ');
+      this.versionNumber = version[0];
+      this.versionHash = version[1];
+      this.versionName = version.slice(2, version.length).join(' ');
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subs.unsubscribe();
+  }
+}
