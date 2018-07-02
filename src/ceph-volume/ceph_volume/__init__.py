526096ba07b1c255c0bb8278b99732b3272ad35d
Andrew Schoen <aschoen@redhat.com>
2018-07-02 15:58:44 -0500

    ceph-volume: show a nice error message when ceph.conf is not loaded
    If the ceph configuration file is not loaded correctly and then values
    from it are used then an undescript error message is shown, e.g.
    
    AttributeError: 'property' object has no attribute 'get'
    
    With this change that same error condition shows the message:
    
    RuntimeError: No valid ceph configuration file was loaded.
    
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- src/ceph-volume/ceph_volume/__init__.py
+++ src/ceph-volume/ceph_volume/__init__.py
@@ -1,5 +1,16 @@
 from collections import namedtuple
 
+
+class UnloadedConfig(object):
+    """
+    This class is used as the default value for conf.ceph so that if
+    a configuration file is not successfully loaded then it will give
+    a nice error message when values from the config are used.
+    """
+    def __getattr__(self, *a):
+        raise RuntimeError("No valid ceph configuration file was loaded.")
+
 conf = namedtuple('config', ['ceph', 'cluster', 'verbosity', 'path', 'log_path'])
+conf.ceph = UnloadedConfig()
 
 __version__ = "1.0.0"
