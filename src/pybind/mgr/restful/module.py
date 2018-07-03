93f95459cde99b9b7c627274d8f9e3cdefc00744
Abhishek Lekshmanan <abhishek@suse.com>
2018-07-03 14:01:20 +0200

    mgr: devicehealth: dont error on dict iteritems
    Since iteritems are not a part of py3, avoid health errors like following:
    
    ```
    Module 'devicehealth' has failed: 'dict' object has no attribute 'iteritems
    ```
    
    Using six iteritems in devicehealth and restful modules
    
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- src/pybind/mgr/restful/module.py
+++ src/pybind/mgr/restful/module.py
@@ -20,16 +20,11 @@ from uuid import uuid4
 from pecan import jsonify, make_app
 from OpenSSL import crypto
 from pecan.rest import RestController
+from six import iteritems
 from werkzeug.serving import make_server, make_ssl_devcert
 
 from .hooks import ErrorHook
 from mgr_module import MgrModule, CommandResult
-
-
-try:
-    iteritems = dict.iteritems
-except:
-    iteritems = dict.items
 
 
 class CannotServe(Exception):
