4ab8e827820cff0b32dd9b49dde2c9112c30305d
Tiago Melo <tmelo@suse.com>
2018-07-03 14:38:35 +0100

    mgr/dashboard: Fix regression on rbd form component
    'decodeURIComponent' method was decoding an undefined variable to the string
    'undefined', which would later cause problems.
    
    This regression was introduced in057d6025894800c1b4a422b081fe02ab82b044fe.
    
    Fixes: http://tracker.ceph.com/issues/24757
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>

---
+++ src/pybind/mgr/dashboard/frontend/src/testing/activated-route-stub.ts
@@ -0,0 +1,23 @@
+import { ReplaySubject } from 'rxjs';
+
+/**
+ * An ActivateRoute test double with a `params` observable.
+ * Use the `setParams()` method to add the next `params` value.
+ */
+export class ActivatedRouteStub {
+  // Use a ReplaySubject to share previous values with subscribers
+  // and pump new values into the `params` observable
+  private subject = new ReplaySubject<object>();
+
+  constructor(initialParams?: object) {
+    this.setParams(initialParams);
+  }
+
+  /** The mock params observable */
+  readonly params = this.subject.asObservable();
+
+  /** Set the params observables's next value */
+  setParams(params?: object) {
+    this.subject.next(params);
+  }
+}
