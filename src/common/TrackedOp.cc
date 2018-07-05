b45a97aeff57cc63214d7847bcf7a2ee4a11c12e
Radoslaw Zarzynski <rzarzyns@redhat.com>
2018-07-05 14:39:31 +0200

    common: fix races when visiting a TrackedOp.
    Before the commit following race can happen:
      ```
      A           : OpTracker::visit_ops_in_flight(..., callable leaking TrackedOpRef outside)
      A           : Mutex::Locker::Locker(sdata->ops_in_flight_lock_sharded)
      A with lock : (nref > 0) == true
    
      B           : TrackedOp::put(), nref := 0 // updating the counter is done without the lock
      B           : OpTracker::unregister_inflight_op()
      B           : Mutex::Locker::Locker(sdata->ops_in_flight_lock_sharded)
    
      A with lock : visit() -> TrackedOp::get(), nref := 1
      A with lock : Mutex::Locker::~Locker()
    
      B with lock : boost::intrusive::list::iterator_to(op)
      B with lock : boost::intrusive::list::erase(iter)
      B with lock : Mutex::Locker::~Locker()
    
      A           : TrackedOp::put(), nref := 0
      A           : OpTracker::unregister_inflight_op()
      A           : Mutex::Locker::Locker(sdata->ops_in_flight_lock_sharded)
      A with lock : boost::intrusive::list::iterator_to(op) // oops as op doesn't belong to the list anymore
      ```
    
    Fixes: https://tracker.ceph.com/issues/24664
    Related-To: https://tracker.ceph.com/issues/24037
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/common/TrackedOp.cc
+++ src/common/TrackedOp.cc
@@ -265,7 +265,7 @@ bool OpTracker::register_inflight_op(TrackedOp *i)
   return true;
 }
 
-void OpTracker::unregister_inflight_op(TrackedOp *i)
+void OpTracker::unregister_inflight_op(TrackedOp* const i)
 {
   // caller checks;
   assert(i->state);
@@ -278,16 +278,12 @@ void OpTracker::unregister_inflight_op(TrackedOp *i)
     auto p = sdata->ops_in_flight_sharded.iterator_to(*i);
     sdata->ops_in_flight_sharded.erase(p);
   }
-  i->_unregistered();
+}
 
-  if (!tracking_enabled)
-    delete i;
-  else {
-    RWLock::RLocker l(lock);
-    i->state = TrackedOp::STATE_HISTORY;
-    utime_t now = ceph_clock_now();
-    history.insert(now, TrackedOpRef(i));
-  }
+void OpTracker::record_history_op(TrackedOpRef&& i)
+{
+  RWLock::RLocker l(lock);
+  history.insert(ceph_clock_now(), std::move(i));
 }
 
 bool OpTracker::visit_ops_in_flight(utime_t* oldest_secs,
@@ -328,7 +324,7 @@ bool OpTracker::visit_ops_in_flight(utime_t* oldest_secs,
     assert(NULL != sdata);
     Mutex::Locker locker(sdata->ops_in_flight_lock_sharded);
     for (auto& op : sdata->ops_in_flight_sharded) {
-      if (op.nref > 0 && !visit(op))
+      if (!visit(op))
 	break;
     }
   }
