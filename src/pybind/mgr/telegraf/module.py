ab001e134238f609823d0aad25f5e758107d9369
Nathan Cutler <ncutler@suse.com>
2018-07-05 17:24:43 +0200

    mgr: replace iteritems with six.iteritems (telegraf)
    Fixes: http://tracker.ceph.com/issues/24779
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- src/pybind/mgr/telegraf/module.py
+++ src/pybind/mgr/telegraf/module.py
@@ -1,6 +1,7 @@
 import errno
 import json
 import itertools
+import six
 import socket
 import time
 from threading import Event
@@ -102,7 +103,7 @@ class Module(MgrModule):
                 }
 
     def get_daemon_stats(self):
-        for daemon, counters in self.get_all_perf_counters().iteritems():
+        for daemon, counters in six.iteritems(self.get_all_perf_counters()):
             svc_type, svc_id = daemon.split('.', 1)
             metadata = self.get_metadata(svc_type, svc_id)
 
