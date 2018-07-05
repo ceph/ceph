c02c6601755efbe5fb06021d314d5b2d8aa6c79e
Nathan Cutler <ncutler@suse.com>
2018-07-05 17:22:16 +0200

    mgr: replace iteritems with six.iteritems (restful)
    Fixes: http://tracker.ceph.com/issues/24779
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- src/pybind/mgr/restful/module.py
+++ src/pybind/mgr/restful/module.py
@@ -11,6 +11,7 @@ import inspect
 import tempfile
 import threading
 import traceback
+import six
 import socket
 
 from . import common
@@ -24,12 +25,6 @@ from werkzeug.serving import make_server, make_ssl_devcert
 
 from .hooks import ErrorHook
 from mgr_module import MgrModule, CommandResult
-
-
-try:
-    iteritems = dict.iteritems
-except:
-    iteritems = dict.items
 
 
 class CannotServe(Exception):
@@ -270,7 +265,7 @@ class Module(MgrModule):
     def refresh_keys(self):
         self.keys = {}
         rawkeys = self.get_store_prefix('keys/') or {}
-        for k, v in iteritems(rawkeys):
+        for k, v in six.iteritems(rawkeys):
             self.keys[k[5:]] = v  # strip of keys/ prefix
 
     def _serve(self):
