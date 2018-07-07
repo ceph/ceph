b76c9854d94716e3f1671e816b7d594cff7d292a
Nathan Cutler <ncutler@suse.com>
2018-07-07 09:04:39 +0200

    run-make-check.sh: gtest-parallel only with Python 2
    Fixes: http://tracker.ceph.com/issues/24817
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- run-make-check.sh
+++ run-make-check.sh
@@ -92,12 +92,14 @@ function run() {
     test "$BUILD_MAKEOPTS" && echo "make will run with option(s) $BUILD_MAKEOPTS"
     CHECK_MAKEOPTS=${CHECK_MAKEOPTS:-$DEFAULT_MAKEOPTS}
 
-    CMAKE_PYTHON_OPTS=
-    if ! type python2 > /dev/null 2>&1 ; then
-        CMAKE_PYTHON_OPTS="-DWITH_PYTHON2=OFF -DWITH_PYTHON3=ON -DMGR_PYTHON_VERSION=3"
+    if type python2 > /dev/null 2>&1 ; then
+        # gtest-parallel requires Python 2
+        CMAKE_PYTHON_OPTS="-DWITH_GTEST_PARALLEL=ON"
+    else
+        CMAKE_PYTHON_OPTS="-DWITH_PYTHON2=OFF -DWITH_PYTHON3=ON -DMGR_PYTHON_VERSION=3 -DWITH_GTEST_PARALLEL=OFF"
     fi
 
-    CMAKE_BUILD_OPTS="-DWITH_FIO=ON -DWITH_GTEST_PARALLEL=ON"
+    CMAKE_BUILD_OPTS="-DWITH_FIO=ON"
 
     cat <<EOM
 Note that the binaries produced by this script do not contain correct time
