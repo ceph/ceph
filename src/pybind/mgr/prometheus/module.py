7b5af2857cd3f1c7867d0186d1c12f0ece6d52da
Boris Ranto <branto@redhat.com>
2018-07-12 00:55:18 +0200

    prometheus: Make the cache timeout configurable
    Signed-off-by: Boris Ranto <branto@redhat.com>

--- src/pybind/mgr/prometheus/module.py
+++ src/pybind/mgr/prometheus/module.py
@@ -311,6 +311,7 @@ class Module(MgrModule):
     OPTIONS = [
             {'name': 'server_addr'},
             {'name': 'server_port'},
+            {'name': 'scrape_interval'},
     ]
 
     def __init__(self, *args, **kwargs):
@@ -668,6 +669,9 @@ class Module(MgrModule):
                         return inst.collect_cache
                 else:
                     raise cherrypy.HTTPError(503, 'No MON connection')
+
+        # Make the cache timeout for collecting configurable
+        self.collect_timeout = self.get_localized_config('scrape_interval', 5.0)
 
         server_addr = self.get_localized_config('server_addr', DEFAULT_ADDR)
         server_port = self.get_localized_config('server_port', DEFAULT_PORT)
