35a86d9c45b1cfd74956bf53f9e28186d33a3f5b
Yan, Zheng <zyan@redhat.com>
2018-07-12 11:19:05 +0800

    mds: change default mds_max_export_size to 20M
    1G seems too large. mds may speed long time on processing
    imported/expored subtree.
    
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>
    (cherry picked from commit 07ef150d3c552b7781347e19caa2dae2850368b9)
    
     Conflicts:
    	src/common/options.cc

--- src/common/options.cc
+++ src/common/options.cc
@@ -6818,7 +6818,7 @@ std::vector<Option> get_mds_options() {
     .set_description(""),
 
     Option("mds_max_export_size", Option::TYPE_UINT, Option::LEVEL_DEV)
-    .set_default(1_G)
+    .set_default(20_M)
     .set_description(""),
 
     Option("mds_kill_export_at", Option::TYPE_INT, Option::LEVEL_DEV)
