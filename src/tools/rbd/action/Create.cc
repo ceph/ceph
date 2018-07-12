ceph/ceph

commit d88bc78608d3a026d6c74c613dc875384d333f7c

common: move ConfigValues out of md_config_t
by moving ConfigValues out of md_config_t, we are able to point
md_config_t to a new ConfigValues instance at runtime, without
destroying the old one.

also remove ConfigReader, because all md_config_obs_t will be updated
with ConfigProxy now. as md_config_t is not able to offer the *data*
anymore.

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/tools/rbd/action/Create.cc
@@ -64,7 +64,7 @@ struct thick_provision_writer {
     assert(r >= 0);
     uint64_t order;
     if (info.order == 0) {
-      order = g_conf->get_val<int64_t>("rbd_default_order");
+      order = g_conf().get_val<int64_t>("rbd_default_order");
     } else {
       order = info.order;
     }
@@ -73,7 +73,7 @@ struct thick_provision_writer {
       chunk_size = image->get_stripe_unit();
     }
 
-    concurr = g_conf->get_val<int64_t>("rbd_concurrent_management_ops");
+    concurr = g_conf().get_val<int64_t>("rbd_concurrent_management_ops");
     io_status.in_flight = 0;
     io_status.io_error = 0;
   }
@@ -181,7 +181,7 @@ int thick_write(const std::string &image_name,librados::IoCtx &io_ctx,
   // To prevent writesame from discarding data, thick_write sets
   // the rbd_discard_on_zeroed_write_same option to false.
   assert(g_conf != nullptr);
-  r = g_conf->set_val("rbd_discard_on_zeroed_write_same", "false");
+  r = g_conf().set_val("rbd_discard_on_zeroed_write_same", "false");
   assert(r == 0);
   r = utils::open_image(io_ctx, image_name, false, &image);
   if (r < 0) {

commit 1794412b3bc4b4b6a7e37edcd5abcb08974ea644

tools/rbd: assert(g_ceph_context) not g_conf
this addresses a warning introduced by d88bc786:

src/tools/rbd/action/Create.cc:59:19: warning: the address of
'ConfigProxy& g_conf()' will never be NULL [-Waddress]
     assert(g_conf != nullptr);
            ~~~~~~~^~~~

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/tools/rbd/action/Create.cc
@@ -56,7 +56,7 @@ struct thick_provision_writer {
   {
     // If error cases occur, the code is aborted, because
     // constructor cannot return error value.
-    assert(g_conf != nullptr);
+    assert(g_ceph_context != nullptr);
     bl.append_zero(block_size);
 
     librbd::image_info_t info;