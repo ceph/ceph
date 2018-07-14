35ed68a8b239c69c879a065772e3a7df289b4391
Sage Weil <sage@redhat.com>
2018-07-14 13:26:31 -0500

    common/options: make osd_skip_data_digest a DEV option
    And document/warn to never turn it off after it's been on.
    
    Signed-off-by: Sage Weil <sage@redhat.com>
    (cherry picked from commit c854ed9b33a5a293c8ace15718e26f74237e3332)

--- src/common/options.cc
+++ src/common/options.cc
@@ -2316,9 +2316,9 @@ std::vector<Option> get_global_options() {
     .set_flag(Option::FLAG_STARTUP)
     .set_description(""),
 
-    Option("osd_skip_data_digest", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
+    Option("osd_skip_data_digest", Option::TYPE_BOOL, Option::LEVEL_DEV)
     .set_default(true)
-    .set_description(""),
+    .set_description("Do not store full-object checksums if the backend (bluestore) does its own checksums.  Do not ever turn this off if it has ever been turned on."),
 
     Option("osd_op_queue", Option::TYPE_STR, Option::LEVEL_ADVANCED)
     .set_default("wpq")
