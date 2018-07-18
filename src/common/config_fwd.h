c8b80d605a5343c2cdfca771758186854bc5804e
Kefu Chai <kchai@redhat.com>
2018-07-18 15:55:55 +0800

    common/config: change md_config_impl<> back to plain class
    now that all of the variables that change depending on seastar/alien
    have been extracted out of md_config_impl<>, there is no need to make it
    a template anymore.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/common/config_fwd.h
+++ src/common/config_fwd.h
@@ -5,12 +5,10 @@
 #include "lock_policy.h"
 
 namespace ceph::internal {
-template<LockPolicy lp> struct md_config_impl;
 template<LockPolicy lp> class md_config_obs_impl;
 }
 
-using md_config_t =
-  ceph::internal::md_config_impl<ceph::internal::LockPolicy::MUTEX>;
+class md_config_t;
 using md_config_obs_t =
   ceph::internal::md_config_obs_impl<ceph::internal::LockPolicy::MUTEX>;
 class ConfigProxy;
