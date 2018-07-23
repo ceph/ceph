bfbe23bfbd3ea54b4bd756dca065d9780d3e3330
Jason Dillaman <dillaman@redhat.com>
2018-07-23 10:57:44 -0400

    librbd: clone detach should attempt to remove trashed snapshots
    Fixes: http://tracker.ceph.com/issues/23398
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>
    (cherry picked from commit 59676fee0b4e330fe5d74c0cc5d5b3e79cad9b15)
    
    Conflicts:
    	src/librbd/image/DetachChildRequest.cc: trivial resolution

--- src/librbd/image/DetachChildRequest.h
+++ src/librbd/image/DetachChildRequest.h
@@ -27,6 +27,7 @@ public:
   DetachChildRequest(ImageCtxT& image_ctx, Context* on_finish)
     : m_image_ctx(image_ctx), m_on_finish(on_finish) {
   }
+  ~DetachChildRequest();
 
   void send();
 
@@ -34,13 +35,28 @@ private:
   /**
    * @verbatim
    *
-   * <start>
-   *    |
-   *    v (skip if v1)
-   * CHILD_DETACH
-   *    |
-   *    v (skip if v2)
-   * REMOVE_CHILD
+   *                 <start>
+   *                    |
+   *     (v1)           | (v2)
+   *    /--------------/ \--------------\
+   *    |                               |
+   *    v                               v
+   * REMOVE_CHILD                   CHILD_DETACH
+   *    |                               |
+   *    |                               v
+   *    |                           GET_SNAPSHOT
+   *    |  (snapshot in-use)          . |
+   *    |/. . . . . . . . . . . . . . . |
+   *    |                               v
+   *    |                           OPEN_PARENT
+   *    |                               |
+   *    |                               v
+   *    |                           REMOVE_SNAPSHOT
+   *    |                               |
+   *    |                               v
+   *    |                           CLOSE_PARENT
+   *    |                               |
+   *    |/------------------------------/
    *    |
    *    v
    * <finish>
@@ -53,11 +69,29 @@ private:
 
   librados::IoCtx m_parent_io_ctx;
   ParentSpec m_parent_spec;
+  std::string m_parent_header_name;
+
+  cls::rbd::SnapshotNamespace m_parent_snap_namespace;
+  std::string m_parent_snap_name;
+
+  ImageCtxT* m_parent_image_ctx = nullptr;
 
   ceph::bufferlist m_out_bl;
 
   void clone_v2_child_detach();
   void handle_clone_v2_child_detach(int r);
+
+  void clone_v2_get_snapshot();
+  void handle_clone_v2_get_snapshot(int r);
+
+  void clone_v2_open_parent();
+  void handle_clone_v2_open_parent(int r);
+
+  void clone_v2_remove_snapshot();
+  void handle_clone_v2_remove_snapshot(int r);
+
+  void clone_v2_close_parent();
+  void handle_clone_v2_close_parent(int r);
 
   void clone_v1_remove_child();
   void handle_clone_v1_remove_child(int r);
