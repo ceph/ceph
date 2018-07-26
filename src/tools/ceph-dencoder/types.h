aa08323409fa92fa6e934c246eada59d933d1a89
Jason Dillaman <dillaman@redhat.com>
2018-07-26 12:20:46 -0400

    librbd: new ParentImageSpec type to support parent namespaces
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/tools/ceph-dencoder/types.h
+++ src/tools/ceph-dencoder/types.h
@@ -465,6 +465,7 @@ TYPE(cls_rbd_parent)
 TYPE(cls_rbd_snap)
 
 #include "cls/rbd/cls_rbd_types.h"
+TYPE(cls::rbd::ParentImageSpec)
 TYPE(cls::rbd::ChildImageSpec)
 TYPE(cls::rbd::MigrationSpec)
 TYPE(cls::rbd::MirrorPeer)
