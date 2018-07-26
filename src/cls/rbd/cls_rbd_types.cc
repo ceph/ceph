48c0bcd01119b1d0f95b5ac2428669ccba87fae6
Jason Dillaman <dillaman@redhat.com>
2018-07-26 12:20:46 -0400

    librbd: new ParentImageSpec type to support parent namespaces
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/cls/rbd/cls_rbd_types.cc
+++ src/cls/rbd/cls_rbd_types.cc
@@ -212,6 +212,37 @@ std::ostream& operator<<(std::ostream& os, const MirrorImageStatus& status) {
   return os;
 }
 
+void ParentImageSpec::encode(bufferlist& bl) const {
+  ENCODE_START(1, 1, bl);
+  encode(pool_id, bl);
+  encode(pool_namespace, bl);
+  encode(image_id, bl);
+  encode(snap_id, bl);
+  ENCODE_FINISH(bl);
+}
+
+void ParentImageSpec::decode(bufferlist::const_iterator& bl) {
+  DECODE_START(1, bl);
+  decode(pool_id, bl);
+  decode(pool_namespace, bl);
+  decode(image_id, bl);
+  decode(snap_id, bl);
+  DECODE_FINISH(bl);
+}
+
+void ParentImageSpec::dump(Formatter *f) const {
+  f->dump_int("pool_id", pool_id);
+  f->dump_string("pool_namespace", pool_namespace);
+  f->dump_string("image_id", image_id);
+  f->dump_unsigned("snap_id", snap_id);
+}
+
+void ParentImageSpec::generate_test_instances(std::list<ParentImageSpec*>& o) {
+  o.push_back(new ParentImageSpec{});
+  o.push_back(new ParentImageSpec{1, "", "foo", 3});
+  o.push_back(new ParentImageSpec{1, "ns", "foo", 3});
+}
+
 void ChildImageSpec::encode(bufferlist &bl) const {
   ENCODE_START(1, 1, bl);
   encode(pool_id, bl);
