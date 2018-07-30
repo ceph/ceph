969645efeee9e2e79d05fa1433e8707d40723d94
Volker Theile <vtheile@suse.com>
2018-07-30 17:22:41 +0200

    mgr/dashboard: Modal dialogs are still open when UI is redirected to the login screen
    Fixes https://tracker.ceph.com/issues/24570
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/core/auth/login/login.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+import { BsModalService } from 'ngx-bootstrap';
+
 import { AuthService } from '../../../shared/api/auth.service';
 import { Credentials } from '../../../shared/models/credentials';
 import { AuthStorageService } from '../../../shared/services/auth-storage.service';
@@ -16,12 +18,21 @@ export class LoginComponent implements OnInit {
   constructor(
     private authService: AuthService,
     private authStorageService: AuthStorageService,
+    private bsModalService: BsModalService,
     private router: Router
   ) {}
 
   ngOnInit() {
     if (this.authStorageService.isLoggedIn()) {
       this.router.navigate(['']);
+    } else {
+      // Make sure all open modal dialogs are closed. This might be
+      // necessary when the logged in user is redirected to the login
+      // page after a 401.
+      const modalsCount = this.bsModalService.getModalsCount();
+      for (let i = 1; i <= modalsCount; i++) {
+        this.bsModalService.hide(i);
+      }
     }
   }
 
