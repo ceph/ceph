1872cdca7bc98131c207e90822ed005225f168f6
Kefu Chai <tchaikov@gmail.com>
2018-08-01 15:00:34 +0800

    cmake: Revert "cmake: fix "WITH_STATIC_LIBSTDCXX""
    Fixes: http://tracker.ceph.com/issues/25209
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- cmake/modules/Findsnappy.cmake
+++ cmake/modules/Findsnappy.cmake
@@ -5,21 +5,13 @@
 # SNAPPY_LIBRARIES - List of libraries when using snappy.
 # SNAPPY_FOUND - True if snappy found.
 
-find_package(PkgConfig)
-pkg_search_module(PC_snappy
-  QUIET snappy)
-
 find_path(SNAPPY_INCLUDE_DIR
   NAMES snappy.h
-  HINTS
-    ${PC_snappy_INCLUDE_DIRS}
-    ${SNAPPY_ROOT_DIR}/include)
+  HINTS ${SNAPPY_ROOT_DIR}/include)
 
 find_library(SNAPPY_LIBRARIES
   NAMES snappy
-  HINTS
-    ${PC_snappy_LIBRARY_DIRS}
-    ${SNAPPY_ROOT_DIR}/lib)
+  HINTS ${SNAPPY_ROOT_DIR}/lib)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(snappy DEFAULT_MSG SNAPPY_LIBRARIES SNAPPY_INCLUDE_DIR)
