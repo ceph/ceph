ceph/ceph

commit a91159c29781f55f1ac632fca9327f3c48287d8e

common/options: convert many TYPE_[U]INT -> TYPE_SIZE
Note that the _cost options are in fact in units of bytes.

Signed-off-by: Sage Weil <sage@redhat.com>

src/mds/MDCache.cc
@@ -210,7 +210,7 @@ MDCache::MDCache(MDSRank *m, PurgeQueue &purge_queue_) :
   opening_root = open = false;
 
   cache_inode_limit = g_conf().get_val<int64_t>("mds_cache_size");
-  cache_memory_limit = g_conf().get_val<uint64_t>("mds_cache_memory_limit");
+  cache_memory_limit = g_conf().get_val<Option::size_t>("mds_cache_memory_limit");
   cache_reservation = g_conf().get_val<double>("mds_cache_reservation");
   cache_health_threshold = g_conf().get_val<double>("mds_health_cache_threshold");
 

src/ceph_osd.cc
@@ -518,7 +518,7 @@ flushjournal_out:
        << std::endl;
 
   uint64_t message_size =
-    g_conf().get_val<uint64_t>("osd_client_message_size_cap");
+    g_conf().get_val<Option::size_t>("osd_client_message_size_cap");
   boost::scoped_ptr<Throttle> client_byte_throttler(
     new Throttle(g_ceph_context, "osd_client_bytes", message_size));
 

src/client/fuse_ll.cc
@@ -1103,7 +1103,7 @@ int CephFuse::Handle::init(int argc, const char *argv[])
     "fuse_atomic_o_trunc");
   auto fuse_debug = client->cct->_conf.get_val<bool>(
     "fuse_debug");
-  auto fuse_max_write = client->cct->_conf.get_val<uint64_t>(
+  auto fuse_max_write = client->cct->_conf.get_val<Option::size_t>(
     "fuse_max_write");
 
   if (fuse_allow_other) {

src/kv/RocksDBStore.cc
@@ -435,8 +435,8 @@ int RocksDBStore::load_rocksdb_options(bool create_if_missing, rocksdb::Options&
       g_conf().get_val<bool>("rocksdb_pin_l0_filter_and_index_blocks_in_cache");
   }
   bbt_opts.partition_filters = g_conf().get_val<bool>("rocksdb_partition_filters");
-  if (g_conf().get_val<uint64_t>("rocksdb_metadata_block_size") > 0)
-    bbt_opts.metadata_block_size = g_conf().get_val<uint64_t>("rocksdb_metadata_block_size");
+  if (g_conf().get_val<Option::size_t>("rocksdb_metadata_block_size") > 0)
+    bbt_opts.metadata_block_size = g_conf().get_val<Option::size_t>("rocksdb_metadata_block_size");
 
   opt.table_factory.reset(rocksdb::NewBlockBasedTableFactory(bbt_opts));
   dout(10) << __func__ << " block size " << g_conf()->rocksdb_block_size

src/librbd/image/CreateRequest.cc
@@ -156,7 +156,7 @@ CreateRequest<I>::CreateRequest(IoCtx &ioctx, const std::string &image_name,
 
   if (image_options.get(RBD_IMAGE_OPTION_STRIPE_UNIT, &m_stripe_unit) != 0 ||
       m_stripe_unit == 0) {
-    m_stripe_unit = m_cct->_conf.get_val<uint64_t>("rbd_default_stripe_unit");
+    m_stripe_unit = m_cct->_conf.get_val<Option::size_t>("rbd_default_stripe_unit");
   }
   if (image_options.get(RBD_IMAGE_OPTION_STRIPE_COUNT, &m_stripe_count) != 0 ||
       m_stripe_count == 0) {

src/mds/Migrator.cc
@@ -1217,7 +1217,7 @@ void Migrator::check_export_size(CDir *dir, export_state_t& stat, set<client_t>&
   const unsigned link_size = 10;
   const unsigned null_size = 1;
 
-  uint64_t max_size = g_conf().get_val<uint64_t>("mds_max_export_size");
+  uint64_t max_size = g_conf().get_val<Option::size_t>("mds_max_export_size");
   uint64_t approx_size = 0;
 
   list<CDir*> dfs;

src/mon/Monitor.cc
@@ -3103,7 +3103,7 @@ void Monitor::handle_command(MonOpRequestRef op)
   if (mon_cmd->is_mgr()) {
     const auto& hdr = m->get_header();
     uint64_t size = hdr.front_len + hdr.middle_len + hdr.data_len;
-    uint64_t max = g_conf().get_val<uint64_t>("mon_client_bytes")
+    uint64_t max = g_conf().get_val<Option::size_t>("mon_client_bytes")
                  * g_conf().get_val<double>("mon_mgr_proxy_client_bytes_ratio");
     if (mgr_proxy_bytes + size > max) {
       dout(10) << __func__ << " current mgr proxy bytes " << mgr_proxy_bytes

commit 398c2fa50b71cda99f7e1b682947d03f605fe5ad

mds/MDCache: fix mds_cache_memory_limit get_val<>
Fixes: a91159c29781f55f1ac632fca9327f3c48287d8e
Fixes: http://tracker.ceph.com/issues/25215
Signed-off-by: Sage Weil <sage@redhat.com>

src/mds/MDCache.cc
@@ -239,7 +239,7 @@ void MDCache::handle_conf_change(const ConfigProxy& conf,
   if (changed.count("mds_cache_size"))
     cache_inode_limit = g_conf().get_val<int64_t>("mds_cache_size");
   if (changed.count("mds_cache_memory_limit"))
-    cache_memory_limit = g_conf().get_val<uint64_t>("mds_cache_memory_limit");
+    cache_memory_limit = g_conf().get_val<Option::size_t>("mds_cache_memory_limit");
   if (changed.count("mds_cache_reservation"))
     cache_reservation = g_conf().get_val<double>("mds_cache_reservation");
   if (changed.count("mds_health_cache_threshold"))