7602f256c47fd9782e86ba21e6ef2edc8925b87e
John Spray <john.spray@redhat.com>
2018-08-02 11:42:50 +0100

    mgr: tweaks to command permissions
    Upgrade all selftest ops to rw, downgrade restful
    cert list to r.
    
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/pybind/mgr/restful/module.py
+++ src/pybind/mgr/restful/module.py
@@ -219,7 +219,7 @@ class Module(MgrModule):
         {
             "cmd": "restful list-keys",
             "desc": "List all API keys",
-            "perm": "rw"
+            "perm": "r"
         },
         {
             "cmd": "restful create-self-signed-cert",
