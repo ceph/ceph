f9bb53d8d90cca22671f328e218d3c7955dc7b0d
John Spray <john.spray@redhat.com>
2018-08-02 11:42:50 +0100

    mgr: tweaks to command permissions
    Upgrade all selftest ops to rw, downgrade restful
    cert list to r.
    
    Signed-off-by: John Spray <john.spray@redhat.com>

--- src/pybind/mgr/selftest/module.py
+++ src/pybind/mgr/selftest/module.py
@@ -63,12 +63,12 @@ class Module(MgrModule):
             {
                 "cmd": "mgr self-test remote",
                 "desc": "Test inter-module calls",
-                "perm": "r"
+                "perm": "rw"
             },
             {
                 "cmd": "mgr self-test module name=module,type=CephString",
                 "desc": "Run another module's self_test() method",
-                "perm": "r"
+                "perm": "rw"
             },
             ]
 
