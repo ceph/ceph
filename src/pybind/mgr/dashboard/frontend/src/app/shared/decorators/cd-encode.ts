cf77b3c6d1307ed58550c114bb5d7e36725d7feb
Tiago Melo <tspmelo@gmail.com>
2018-08-03 17:58:21 +0100

    mgr/dashboard: Add decorator to skip parameter encoding
    By enconding all parameters of api services we were also encoding parameters
    that were being sent in the body of the request.
    Those parameters don't need to be enconded and the server never decodes them.
    
    With this new decorator you can specify if you don't want a parameter to be
    enconded.
    
    This is a regression introduced in f21d0da5a3e19e3f9bbde084e71b4a09f8dcb0a1.
    
    Fixes: http://tracker.ceph.com/issues/26856
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>
    (cherry picked from commit 45e645b7701178ad3c711c6804df273e2907d9d3)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/decorators/cd-encode.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/decorators/cd-encode.ts
@@ -9,58 +9,71 @@ import * as _ from 'lodash';
  * @param {Function} [target=null]
  * @returns {*}
  */
-export function cdEncode(target: Function = null): any {
-  if (target) {
-    encodeClass(target);
+export function cdEncode(...args: any[]): any {
+  switch (args.length) {
+    case 1:
+      return encodeClass.apply(this, args);
+    case 3:
+      return encodeMethod.apply(this, args);
+    default:
+      throw new Error();
+  }
+}
+
+/**
+ * This decorator can be used in parameters only.
+ * It will exclude the parameter from being encode.
+ * This should be used in parameters that are going
+ * to be sent in the request's body.
+ *
+ * @export
+ * @param {Object} target
+ * @param {string} propertyKey
+ * @param {number} index
+ */
+export function cdEncodeNot(target: Object, propertyKey: string, index: number) {
+  const metadataKey = `__ignore_${propertyKey}`;
+  if (Array.isArray(target[metadataKey])) {
+    target[metadataKey].push(index);
   } else {
-    return encodeMethod();
+    target[metadataKey] = [index];
   }
 }
 
 function encodeClass(target: Function) {
   for (const propertyName of Object.keys(target.prototype)) {
     const descriptor = Object.getOwnPropertyDescriptor(target.prototype, propertyName);
+
     const isMethod = descriptor.value instanceof Function;
     if (!isMethod) {
       continue;
     }
 
-    const originalMethod = descriptor.value;
-    descriptor.value = function(...args: any[]) {
-      args.forEach((arg, i, argsArray) => {
-        if (_.isString(arg)) {
-          argsArray[i] = encodeURIComponent(arg);
-        }
-      });
-
-      const result = originalMethod.apply(this, args);
-      return result;
-    };
-
+    encodeMethod(target.prototype, propertyName, descriptor);
     Object.defineProperty(target.prototype, propertyName, descriptor);
   }
 }
 
-function encodeMethod() {
-  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-    if (descriptor === undefined) {
-      descriptor = Object.getOwnPropertyDescriptor(target, propertyKey);
-    }
-    const originalMethod = descriptor.value;
+function encodeMethod(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  if (descriptor === undefined) {
+    descriptor = Object.getOwnPropertyDescriptor(target, propertyKey);
+  }
+  const originalMethod = descriptor.value;
 
-    descriptor.value = function() {
-      const args = [];
+  descriptor.value = function() {
+    const metadataKey = `__ignore_${propertyKey}`;
+    const indices: number[] = target[metadataKey] || [];
+    const args = [];
 
-      for (let i = 0; i < arguments.length; i++) {
-        if (_.isString(arguments[i])) {
-          args[i] = encodeURIComponent(arguments[i]);
-        } else {
-          args[i] = arguments[i];
-        }
+    for (let i = 0; i < arguments.length; i++) {
+      if (_.isString(arguments[i]) && indices.indexOf(i) === -1) {
+        args[i] = encodeURIComponent(arguments[i]);
+      } else {
+        args[i] = arguments[i];
       }
+    }
 
-      const result = originalMethod.apply(this, args);
-      return result;
-    };
+    const result = originalMethod.apply(this, args);
+    return result;
   };
 }
