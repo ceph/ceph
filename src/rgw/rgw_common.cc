c97ee932f380a673b52ffcb1986b015e06246844
yuliyang <yuliyang@cmss.chinamobile.com>
2018-08-06 10:33:12 +0800

    rgw: cors rules num limit According to AWS S3, an cors rules can have up to 100 rules.
    Signed-off-by: yuliyang <yuliyang@cmss.chinamobile.com>

--- src/rgw/rgw_common.cc
+++ src/rgw/rgw_common.cc
@@ -75,6 +75,7 @@ rgw_http_errors rgw_http_s3_errors({
     { ERR_MALFORMED_DOC, {400, "MalformedPolicyDocument"}},
     { ERR_INVALID_TAG, {400, "InvalidTag"}},
     { ERR_MALFORMED_ACL_ERROR, {400, "MalformedACLError" }},
+    { ERR_INVALID_CORS_RULES_ERROR, {400, "InvalidRequest" }},
     { ERR_INVALID_ENCRYPTION_ALGORITHM, {400, "InvalidEncryptionAlgorithmError" }},
     { ERR_LENGTH_REQUIRED, {411, "MissingContentLength" }},
     { EACCES, {403, "AccessDenied" }},
