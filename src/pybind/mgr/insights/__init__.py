66977290a0de08b4c8701b1b3e372d046dd6e3de
Noah Watkins <nwatkins@redhat.com>
2018-08-08 11:24:59 -0700

    mgr/insights: add unit tests
    wired up to make check
    
    Signed-off-by: Noah Watkins <nwatkins@redhat.com>

--- src/pybind/mgr/insights/__init__.py
+++ src/pybind/mgr/insights/__init__.py
@@ -1,1 +1,9 @@
-from .module import Module
+from __future__ import absolute_import
+import os
+
+if 'UNITTEST' not in os.environ:
+    from .module import Module
+else:
+    import sys
+    import mock
+    sys.modules['ceph_module'] = mock.Mock()
