a5eb5e6fdd1949f4e4299c612b0e760bc7cc60ea
Sage Weil <sage@redhat.com>
2018-08-16 12:22:57 -0500

    osd: make project_pg_history handle concurrent osdmap publish
    The class's osdmap may be updated while we are in our loop.  Pass it in
    explicitly instead.
    
    Fixes: http://tracker.ceph.com/issues/26970
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/OSD.h
+++ src/osd/OSD.h
@@ -1926,6 +1926,7 @@ protected:
   /// project pg history from from to now
   bool project_pg_history(
     spg_t pgid, pg_history_t& h, epoch_t from,
+    const OSDMap &osdmap,
     const vector<int>& lastup,
     int lastupprimary,
     const vector<int>& lastacting,
