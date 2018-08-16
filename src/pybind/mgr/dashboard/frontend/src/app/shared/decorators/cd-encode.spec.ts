ceph/ceph

commit e24f597e4d1ad18cb75612264a049d744f9c0d01

mgr/dashboard: Add support for URI encode
Created a decorator and pipe to help encode special URI components in the
frontend.

Modified the backend request handler to decode all the string args.

fixes: http://tracker.ceph.com/issues/24621

Signed-off-by: Tiago Melo <tmelo@suse.com>

src/pybind/mgr/dashboard/frontend/src/app/shared/api/rbd.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+import { cdEncode } from '../decorators/cd-encode';
 import { ApiModule } from './api.module';
 
+@cdEncode
 @Injectable({
   providedIn: ApiModule
 })

src/pybind/mgr/dashboard/frontend/src/app/shared/decorators/cd-encode.ts
@@ -1,0 +1,66 @@
+import * as _ from 'lodash';
+
+/**
+ * This decorator can be used in a class or method.
+ * It will encode all the string parameters of all the methods of a class
+ * or, if applied on a method, the specified method.
+ *
+ * @export
+ * @param {Function} [target=null]
+ * @returns {*}
+ */
+export function cdEncode(target: Function = null): any {
+  if (target) {
+    encodeClass(target);
+  } else {
+    return encodeMethod();
+  }
+}
+
+function encodeClass(target: Function) {
+  for (const propertyName of Object.keys(target.prototype)) {
+    const descriptor = Object.getOwnPropertyDescriptor(target.prototype, propertyName);
+    const isMethod = descriptor.value instanceof Function;
+    if (!isMethod) {
+      continue;
+    }
+
+    const originalMethod = descriptor.value;
+    descriptor.value = function(...args: any[]) {
+      args.forEach((arg, i, argsArray) => {
+        if (_.isString(arg)) {
+          argsArray[i] = encodeURIComponent(arg);
+        }
+      });
+
+      const result = originalMethod.apply(this, args);
+      return result;
+    };
+
+    Object.defineProperty(target.prototype, propertyName, descriptor);
+  }
+}
+
+function encodeMethod() {
+  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+    if (descriptor === undefined) {
+      descriptor = Object.getOwnPropertyDescriptor(target, propertyKey);
+    }
+    const originalMethod = descriptor.value;
+
+    descriptor.value = function() {
+      const args = [];
+
+      for (let i = 0; i < arguments.length; i++) {
+        if (_.isString(arguments[i])) {
+          args[i] = encodeURIComponent(arguments[i]);
+        } else {
+          args[i] = arguments[i];
+        }
+      }
+
+      const result = originalMethod.apply(this, args);
+      return result;
+    };
+  };
+}

src/pybind/mgr/dashboard/frontend/src/app/shared/api/rbd.service.spec.ts
@@ -126,4 +126,12 @@ describe('RbdService', () => {
     const req = httpTesting.expectOne('api/block/image/poolName/rbdName/snap/snapshotName');
     expect(req.request.method).toBe('DELETE');
   });
+
+  describe('Encode decorator', () => {
+    it('should encode the imageName', () => {
+      service.get('poolName', 'rbd/name').subscribe();
+      const req = httpTesting.expectOne('api/block/image/poolName/rbd%2Fname');
+      expect(req.request.method).toBe('GET');
+    });
+  });
 });

qa/tasks/mgr/dashboard/test_rbd.py
@@ -580,3 +580,22 @@ class RbdTest(DashboardTestCase):
         self.assertEqual(default_features, ['deep-flatten', 'exclusive-lock',
                                              'fast-diff', 'layering',
                                              'object-map'])
+
+    def test_image_with_special_name(self):
+        rbd_name = 'test/rbd'
+        rbd_name_encoded = 'test%2Frbd'
+
+        self.create_image('rbd', rbd_name, 10240)
+        self.assertStatus(201)
+
+        img = self._get("/api/block/image/rbd/" + rbd_name_encoded)
+        self.assertStatus(200)
+
+        self._validate_image(img, name=rbd_name, size=10240,
+                             num_objs=1, obj_size=4194304,
+                             features_name=['deep-flatten',
+                                            'exclusive-lock',
+                                            'fast-diff', 'layering',
+                                            'object-map'])
+
+        self.remove_image('rbd', rbd_name_encoded)

src/pybind/mgr/dashboard/controllers/__init__.py
@@ -9,9 +9,15 @@ import json
 import os
 import pkgutil
 import sys
-
-import cherrypy
 from six import add_metaclass
+
+if sys.version_info >= (3, 0):
+    from urllib.parse import unquote  # pylint: disable=no-name-in-module,import-error
+else:
+    from urllib import unquote  # pylint: disable=no-name-in-module
+
+# pylint: disable=wrong-import-position
+import cherrypy
 
 from .. import logger
 from ..security import Scope, Permission
@@ -521,6 +527,12 @@ class BaseController(object):
     def _request_wrapper(func, method, json_response):
         @wraps(func)
         def inner(*args, **kwargs):
+            for key, value in kwargs.items():
+                # pylint: disable=undefined-variable
+                if (sys.version_info < (3, 0) and isinstance(value, unicode)) \
+                        or isinstance(value, str):
+                    kwargs[key] = unquote(value)
+
             if method in ['GET', 'DELETE']:
                 ret = func(*args, **kwargs)
 

src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-form/rbd-form.component.ts
@@ -206,9 +206,9 @@ export class RbdFormComponent implements OnInit {
       this.mode === this.rbdFormMode.copying
     ) {
       this.route.params.subscribe((params: { pool: string; name: string; snap: string }) => {
-        const poolName = params.pool;
-        const rbdName = params.name;
-        this.snapName = params.snap;
+        const poolName = decodeURIComponent(params.pool);
+        const rbdName = decodeURIComponent(params.name);
+        this.snapName = decodeURIComponent(params.snap);
         this.rbdService.get(poolName, rbdName).subscribe((resp: RbdFormResponseModel) => {
           this.setResponse(resp, this.snapName);
         });

src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-list/rbd-list.component.html
@@ -32,8 +32,9 @@
               class="btn btn-sm btn-primary"
               *ngIf="permission.update && (!permission.create || permission.create && selection.hasSingleSelection)"
               [ngClass]="{'disabled': !selection.hasSingleSelection || selection.first().executing}"
-              routerLink="/rbd/edit/{{ selection.first()?.pool_name }}/{{ selection.first()?.name }}">
-        <i class="fa fa-fw fa-pencil"></i><span i18n>Edit</span>
+              routerLink="/rbd/edit/{{ selection.first()?.pool_name | encodeUri }}/{{ selection.first()?.name | encodeUri }}">
+        <i class="fa fa-fw fa-pencil"></i>
+        <span i18n>Edit</span>
       </button>
       <button type="button"
               class="btn btn-sm btn-primary"
@@ -64,7 +65,7 @@
             *ngIf="permission.update"
             [ngClass]="{'disabled': !selection.hasSingleSelection || selection.first().executing}">
           <a class="dropdown-item"
-             routerLink="/rbd/edit/{{ selection.first()?.pool_name }}/{{ selection.first()?.name }}">
+             routerLink="/rbd/edit/{{ selection.first()?.pool_name | encodeUri }}/{{ selection.first()?.name | encodeUri }}">
             <i class="fa fa-fw fa-pencil"></i>
             <span i18n>Edit</span>
           </a>
@@ -73,7 +74,7 @@
             *ngIf="permission.create"
             [ngClass]="{'disabled': !selection.hasSingleSelection || selection.first().executing}">
           <a class="dropdown-item"
-             routerLink="/rbd/copy/{{ selection.first()?.pool_name }}/{{ selection.first()?.name }}">
+             routerLink="/rbd/copy/{{ selection.first()?.pool_name | encodeUri }}/{{ selection.first()?.name | encodeUri }}">
             <i class="fa fa-fw fa-copy"></i>
             <span i18n>Copy</span>
           </a>

src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-snapshot-list/rbd-snapshot-list.component.html
@@ -73,7 +73,7 @@
             *ngIf="permission.create"
             [ngClass]="{'disabled': !selection.hasSingleSelection || selection.first().executing}">
           <a class="dropdown-item"
-             routerLink="/rbd/clone/{{ poolName }}/{{ rbdName }}/{{ selection.first()?.name }}">
+             routerLink="/rbd/clone/{{ poolName | encodeUri }}/{{ rbdName | encodeUri }}/{{ selection.first()?.name | encodeUri }}">
             <i class="fa fa-fw fa-clone"></i>
             <span i18n>Clone</span>
           </a>
@@ -81,8 +81,10 @@
         <li role="menuitem"
             *ngIf="permission.create"
             [ngClass]="{'disabled': !selection.hasSingleSelection || selection.first().executing}">
-          <a class="dropdown-item" routerLink="/rbd/copy/{{ poolName }}/{{ rbdName }}/{{ selection.first()?.name }}">
-            <i class="fa fa-fw fa-copy"></i><span i18n>Copy</span>
+          <a class="dropdown-item"
+             routerLink="/rbd/copy/{{ poolName | encodeUri }}/{{ rbdName | encodeUri }}/{{ selection.first()?.name | encodeUri }}">
+            <i class="fa fa-fw fa-copy"></i>
+            <span i18n>Copy</span>
           </a>
         </li>
         <li role="menuitem"

commit 7fb0220ffaa449af568249a083f3edd3f99e8827

mgr/dashboard: Add decorator to skip parameter encoding
By enconding all parameters of api services we were also encoding parameters
that were being sent in the body of the request.
Those parameters don't need to be enconded and the server never decodes them.

With this new decorator you can specify if you don't want a parameter to be
enconded.

This is a regression introduced in e24f597e4d1ad18cb75612264a049d744f9c0d01.

Fixes: http://tracker.ceph.com/issues/26856

Signed-off-by: Tiago Melo <tmelo@suse.com>

src/pybind/mgr/dashboard/frontend/src/app/shared/api/rbd.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-import { cdEncode } from '../decorators/cd-encode';
+import { cdEncode, cdEncodeNot } from '../decorators/cd-encode';
 import { ApiModule } from './api.module';
 
 @cdEncode
@@ -47,7 +47,7 @@ export class RbdService {
     return this.http.get('api/block/image/default_features');
   }
 
-  createSnapshot(poolName, rbdName, snapshotName) {
+  createSnapshot(poolName, rbdName, @cdEncodeNot snapshotName) {
     const request = {
       snapshot_name: snapshotName
     };
@@ -56,7 +56,7 @@ export class RbdService {
     });
   }
 
-  renameSnapshot(poolName, rbdName, snapshotName, newSnapshotName) {
+  renameSnapshot(poolName, rbdName, snapshotName, @cdEncodeNot newSnapshotName) {
     const request = {
       new_snap_name: newSnapshotName
     };
@@ -65,7 +65,7 @@ export class RbdService {
     });
   }
 
-  protectSnapshot(poolName, rbdName, snapshotName, isProtected) {
+  protectSnapshot(poolName, rbdName, snapshotName, @cdEncodeNot isProtected) {
     const request = {
       is_protected: isProtected
     };

src/pybind/mgr/dashboard/frontend/src/app/shared/decorators/cd-encode.ts
@@ -9,58 +9,71 @@ import * as _ from 'lodash';
  * @param {Function} [target=null]
  * @returns {*}
  */
-export function cdEncode(target: Function = null): any {
-  if (target) {
-    encodeClass(target);
+export function cdEncode(...args: any[]): any {
+  switch (args.length) {
+    case 1:
+      return encodeClass.apply(this, args);
+    case 3:
+      return encodeMethod.apply(this, args);
+    default:
+      throw new Error();
+  }
+}
+
+/**
+ * This decorator can be used in parameters only.
+ * It will exclude the parameter from being encode.
+ * This should be used in parameters that are going
+ * to be sent in the request's body.
+ *
+ * @export
+ * @param {Object} target
+ * @param {string} propertyKey
+ * @param {number} index
+ */
+export function cdEncodeNot(target: Object, propertyKey: string, index: number) {
+  const metadataKey = `__ignore_${propertyKey}`;
+  if (Array.isArray(target[metadataKey])) {
+    target[metadataKey].push(index);
   } else {
-    return encodeMethod();
+    target[metadataKey] = [index];
   }
 }
 
 function encodeClass(target: Function) {
   for (const propertyName of Object.keys(target.prototype)) {
     const descriptor = Object.getOwnPropertyDescriptor(target.prototype, propertyName);
+
     const isMethod = descriptor.value instanceof Function;
     if (!isMethod) {
       continue;
     }
 
-    const originalMethod = descriptor.value;
-    descriptor.value = function(...args: any[]) {
-      args.forEach((arg, i, argsArray) => {
-        if (_.isString(arg)) {
-          argsArray[i] = encodeURIComponent(arg);
-        }
-      });
-
-      const result = originalMethod.apply(this, args);
-      return result;
-    };
-
+    encodeMethod(target.prototype, propertyName, descriptor);
     Object.defineProperty(target.prototype, propertyName, descriptor);
   }
 }
 
-function encodeMethod() {
-  return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
-    if (descriptor === undefined) {
-      descriptor = Object.getOwnPropertyDescriptor(target, propertyKey);
-    }
-    const originalMethod = descriptor.value;
+function encodeMethod(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+  if (descriptor === undefined) {
+    descriptor = Object.getOwnPropertyDescriptor(target, propertyKey);
+  }
+  const originalMethod = descriptor.value;
 
-    descriptor.value = function() {
-      const args = [];
+  descriptor.value = function() {
+    const metadataKey = `__ignore_${propertyKey}`;
+    const indices: number[] = target[metadataKey] || [];
+    const args = [];
 
-      for (let i = 0; i < arguments.length; i++) {
-        if (_.isString(arguments[i])) {
-          args[i] = encodeURIComponent(arguments[i]);
-        } else {
-          args[i] = arguments[i];
-        }
+    for (let i = 0; i < arguments.length; i++) {
+      if (_.isString(arguments[i]) && indices.indexOf(i) === -1) {
+        args[i] = encodeURIComponent(arguments[i]);
+      } else {
+        args[i] = arguments[i];
       }
+    }
 
-      const result = originalMethod.apply(this, args);
-      return result;
-    };
+    const result = originalMethod.apply(this, args);
+    return result;
   };
 }

src/pybind/mgr/dashboard/frontend/src/app/shared/api/rbd.service.spec.ts
@@ -126,12 +126,4 @@ describe('RbdService', () => {
     const req = httpTesting.expectOne('api/block/image/poolName/rbdName/snap/snapshotName');
     expect(req.request.method).toBe('DELETE');
   });
-
-  describe('Encode decorator', () => {
-    it('should encode the imageName', () => {
-      service.get('poolName', 'rbd/name').subscribe();
-      const req = httpTesting.expectOne('api/block/image/poolName/rbd%2Fname');
-      expect(req.request.method).toBe('GET');
-    });
-  });
 });

src/pybind/mgr/dashboard/frontend/src/app/shared/decorators/cd-encode.spec.ts
@@ -1,0 +1,41 @@
+import { cdEncode, cdEncodeNot } from './cd-encode';
+
+describe('cdEncode', () => {
+  @cdEncode
+  class ClassA {
+    x2: string;
+    y2: string;
+
+    methodA(x1: string, @cdEncodeNot y1: string) {
+      this.x2 = x1;
+      this.y2 = y1;
+    }
+  }
+
+  class ClassB {
+    x2: string;
+    y2: string;
+
+    @cdEncode
+    methodB(x1: string, @cdEncodeNot y1: string) {
+      this.x2 = x1;
+      this.y2 = y1;
+    }
+  }
+
+  const word = 'a+b/c-d';
+
+  it('should encode all params of ClassA, with exception of y1', () => {
+    const a = new ClassA();
+    a.methodA(word, word);
+    expect(a.x2).toBe('a%2Bb%2Fc-d');
+    expect(a.y2).toBe(word);
+  });
+
+  it('should encode all params of methodB, with exception of y1', () => {
+    const b = new ClassB();
+    b.methodB(word, word);
+    expect(b.x2).toBe('a%2Bb%2Fc-d');
+    expect(b.y2).toBe(word);
+  });
+});