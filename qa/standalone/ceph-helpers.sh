d0b260c272cf2732437c838c80ab380b4bb8d433
David Zafman <dzafman@redhat.com>
2018-08-17 17:25:38 -0700

    test: Fix test to use -gt instead of creating an empty file "0"
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- qa/standalone/ceph-helpers.sh
+++ qa/standalone/ceph-helpers.sh
@@ -1928,7 +1928,7 @@ function test_flush_pg_stats()
     local jq_filter='.pools | .[] | select(.name == "rbd") | .stats'
     raw_bytes_used=`ceph df detail --format=json | jq "$jq_filter.raw_bytes_used"`
     bytes_used=`ceph df detail --format=json | jq "$jq_filter.bytes_used"`
-    test $raw_bytes_used > 0 || return 1
+    test $raw_bytes_used -gt 0 || return 1
     test $raw_bytes_used == $bytes_used || return 1
     teardown $dir
 }
