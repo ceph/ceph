e3242088abddb0d3c813705a674820c989d0b350
Kefu Chai <kchai@redhat.com>
2018-08-20 17:38:57 +0800

    common,test: update mClockPriorityQueue to accomodate the change in dmc
    * cost is required to be a positive number
    * PullPriorityQueue's construct uses an enum instead of a bool for
      specifying the behavior when request breaches the limit.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/test/common/test_mclock_priority_queue.cc
+++ src/test/common/test_mclock_priority_queue.cc
@@ -74,8 +74,8 @@ TEST(mClockPriorityQueue, Sizes)
   q.enqueue_strict(c1, 1, Request(1));
   q.enqueue_strict(c2, 2, Request(2));
   q.enqueue_strict(c1, 2, Request(3));
-  q.enqueue(c2, 1, 0, Request(4));
-  q.enqueue(c1, 2, 0, Request(5));
+  q.enqueue(c2, 1, 1u, Request(4));
+  q.enqueue(c1, 2, 1u, Request(5));
   q.enqueue_strict(c2, 1, Request(6));
 
   ASSERT_FALSE(q.empty());
@@ -150,10 +150,10 @@ TEST(mClockPriorityQueue, JustNotStrict)
 
   // non-strict queue ignores priorites, but will divide between
   // clients evenly and maintain orders between clients
-  q.enqueue(c1, 1, 0, Request(1));
-  q.enqueue(c1, 2, 0, Request(2));
-  q.enqueue(c2, 3, 0, Request(3));
-  q.enqueue(c2, 4, 0, Request(4));
+  q.enqueue(c1, 1, 1u, Request(1));
+  q.enqueue(c1, 2, 1u, Request(2));
+  q.enqueue(c2, 3, 1u, Request(3));
+  q.enqueue(c2, 4, 1u, Request(4));
 
   Request r1, r2;
 
@@ -184,10 +184,10 @@ TEST(mClockPriorityQueue, EnqueuFront)
 
   // non-strict queue ignores priorites, but will divide between
   // clients evenly and maintain orders between clients
-  q.enqueue(c1, 1, 0, Request(1));
-  q.enqueue(c1, 2, 0, Request(2));
-  q.enqueue(c2, 3, 0, Request(3));
-  q.enqueue(c2, 4, 0, Request(4));
+  q.enqueue(c1, 1, 1u, Request(1));
+  q.enqueue(c1, 2, 1u, Request(2));
+  q.enqueue(c2, 3, 1u, Request(3));
+  q.enqueue(c2, 4, 1u, Request(4));
   q.enqueue_strict(c2, 6, Request(6));
   q.enqueue_strict(c1, 7, Request(7));
 
@@ -237,15 +237,15 @@ TEST(mClockPriorityQueue, RemoveByClass)
   Client c2(2);
   Client c3(3);
 
-  q.enqueue(c1, 1, 0, Request(1));
-  q.enqueue(c2, 1, 0, Request(2));
-  q.enqueue(c3, 1, 0, Request(4));
+  q.enqueue(c1, 1, 1u, Request(1));
+  q.enqueue(c2, 1, 1u, Request(2));
+  q.enqueue(c3, 1, 1u, Request(4));
   q.enqueue_strict(c1, 2, Request(8));
   q.enqueue_strict(c2, 1, Request(16));
   q.enqueue_strict(c3, 3, Request(32));
-  q.enqueue(c3, 1, 0, Request(64));
-  q.enqueue(c2, 1, 0, Request(128));
-  q.enqueue(c1, 1, 0, Request(256));
+  q.enqueue(c3, 1, 1u, Request(64));
+  q.enqueue(c2, 1, 1u, Request(128));
+  q.enqueue(c1, 1, 1u, Request(256));
 
   int out_mask = 2 | 16 | 128;
   int in_mask = 1 | 8 | 256;
@@ -281,15 +281,15 @@ TEST(mClockPriorityQueue, RemoveByFilter)
   Client c2(2);
   Client c3(3);
 
-  q.enqueue(c1, 1, 0, Request(1));
-  q.enqueue(c2, 1, 0, Request(2));
-  q.enqueue(c3, 1, 0, Request(3));
+  q.enqueue(c1, 1, 1u, Request(1));
+  q.enqueue(c2, 1, 1u, Request(2));
+  q.enqueue(c3, 1, 1u, Request(3));
   q.enqueue_strict(c1, 2, Request(4));
   q.enqueue_strict(c2, 1, Request(5));
   q.enqueue_strict(c3, 3, Request(6));
-  q.enqueue(c3, 1, 0, Request(7));
-  q.enqueue(c2, 1, 0, Request(8));
-  q.enqueue(c1, 1, 0, Request(9));
+  q.enqueue(c3, 1, 1u, Request(7));
+  q.enqueue(c2, 1, 1u, Request(8));
+  q.enqueue(c1, 1, 1u, Request(9));
 
   std::list<Request> filtered;
 
