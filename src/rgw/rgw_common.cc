f950a7aed2e8f783d46a3b1944d933281c3ef422
ashitakasam <694240887@qq.com>
2018-08-20 16:49:52 +0800

    rgw: the error code returned by rgw is different from amz s3 when getting cors from the bucket without cors configuration
    Fixes: http://tracker.ceph.com/issues/26964
    
    Signed-off-by: ashitakasam <694240887@qq.com>
    (cherry picked from commit 009a67fe4d09c4904bb9711f9bd4c60f353199ec)
    
    Conflicts:
    	src/rgw/rgw_common.cc
    - luminous has no ERR_NO_ROLE_FOUND
    	src/rgw/rgw_op.cc
    - mimic uses dout(2) instead of ldpp_dout(this, 2)

--- src/rgw/rgw_common.cc
+++ src/rgw/rgw_common.cc
@@ -92,6 +92,7 @@ rgw_http_errors rgw_http_s3_errors({
     { ERR_NO_SUCH_LC, {404, "NoSuchLifecycleConfiguration"}},
     { ERR_NO_SUCH_BUCKET_POLICY, {404, "NoSuchBucketPolicy"}},
     { ERR_NO_SUCH_USER, {404, "NoSuchUser"}},
+    { ERR_NO_CORS_FOUND, {404, "NoSuchCORSConfiguration"}},
     { ERR_NO_SUCH_SUBUSER, {404, "NoSuchSubUser"}},
     { ERR_NO_SUCH_CORS_CONFIGURATION, {404, "NoSuchCORSConfiguration"}},
     { ERR_METHOD_NOT_ALLOWED, {405, "MethodNotAllowed" }},
