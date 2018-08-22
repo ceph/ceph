b2ea6e37d408541819b5700625e3c79b627a539f
myoungwon oh <omwmw@sk.com>
2018-08-22 19:19:07 +0900

    common/buffer.cc: add sha1 fingerprint
    Signed-off-by: Myoungwon Oh <omwmw@sk.com>

--- src/common/buffer.cc
+++ src/common/buffer.cc
@@ -2464,6 +2464,32 @@ void buffer::list::invalidate_crc()
   }
 }
 
+#include "common/ceph_crypto.h"
+using ceph::crypto::SHA1;
+
+boost::optional<sha1_digest_info_t> buffer::list::sha1()
+{
+  ptr nb;
+  unsigned pos = 0;
+  unsigned char fingerprint[CEPH_CRYPTO_SHA1_DIGESTSIZE];
+  if (_len == 0) {
+    return boost::none;
+  }
+  nb = buffer::create(_len);
+  for (std::list<ptr>::iterator it = _buffers.begin();
+       it != _buffers.end();
+       ++it) {
+    nb.copy_in(pos, it->length(), it->c_str(), false);
+    pos += it->length();
+  }
+  int size = length();
+  SHA1 sha1_gen;
+  sha1_gen.Update((const unsigned char *)nb.c_str(), size);
+  sha1_gen.Final(fingerprint);
+  sha1_digest_info_t fp_t(fingerprint);
+  return fp_t;
+}
+
 /**
  * Binary write all contents to a C++ stream
  */
