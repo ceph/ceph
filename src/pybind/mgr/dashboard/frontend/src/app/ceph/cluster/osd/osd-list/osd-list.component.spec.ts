2ad3ff6f7c85878bd80bac9b65c7d2d1a8693a06
Stephan Müller <smueller@suse.com>
2018-08-22 08:45:08 +0200

    mgr/dashboard: Use table actions component for OSDs
    Signed-off-by: Stephan Müller <smueller@suse.com>
    (cherry picked from commit 46a8ea67896b82bb60dfef138401c91b221307f3)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/osd/osd-list/osd-list.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/osd/osd-list/osd-list.component.spec.ts
@@ -1,11 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { TabsModule } from 'ngx-bootstrap/tabs';
 
-import { configureTestBed } from '../../../../../testing/unit-test-helper';
+import { configureTestBed, PermissionHelper } from '../../../../../testing/unit-test-helper';
 import { ComponentsModule } from '../../../../shared/components/components.module';
 import { DataTableModule } from '../../../../shared/datatable/datatable.module';
+import { TableActionsComponent } from '../../../../shared/datatable/table-actions/table-actions.component';
 import { Permissions } from '../../../../shared/models/permissions';
 import { DimlessPipe } from '../../../../shared/pipes/dimless.pipe';
 import { AuthStorageService } from '../../../../shared/services/auth-storage.service';
@@ -49,6 +51,44 @@ describe('OsdListComponent', () => {
   });
 
   it('should create', () => {
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  describe('show table actions as defined', () => {
+    let tableActions: TableActionsComponent;
+    let scenario: { fn; empty; single };
+    let permissionHelper: PermissionHelper;
+
+    const getTableActionComponent = () => {
+      fixture.detectChanges();
+      return fixture.debugElement.query(By.directive(TableActionsComponent)).componentInstance;
+    };
+
+    beforeEach(() => {
+      permissionHelper = new PermissionHelper(component.permission, () =>
+        getTableActionComponent()
+      );
+      scenario = {
+        fn: () => tableActions.getCurrentButton(),
+        single: undefined,
+        empty: undefined
+      };
+      tableActions = permissionHelper.setPermissionsAndGetActions(1, 1, 1);
+    });
+
+    it('shows no action button', () => permissionHelper.testScenarios(scenario));
+
+    it('shows all actions', () => {
+      expect(tableActions.tableActions.length).toBe(2);
+      expect(tableActions.tableActions).toEqual(component.tableActions);
+    });
+
+    it(`shows 'Perform task' as drop down`, () => {
+      expect(
+        fixture.debugElement.query(By.directive(TableActionsComponent)).query(By.css('button'))
+          .nativeElement.textContent
+      ).toBe('Perform Task');
+    });
   });
 });
