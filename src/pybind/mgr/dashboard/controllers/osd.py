1c4d30f806a7784fa85c7360373adcb0c6057e96
Patrick Nawracay <pnawracay@suse.com>
2018-08-27 15:14:23 +0200

    mgr/dashboard: Improve readability
    Signed-off-by: Patrick Nawracay <pnawracay@suse.com>
    (cherry picked from commit bca8eae2225fd293e257a56d821ea495073b1cee)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/controllers/osd.py
+++ src/pybind/mgr/dashboard/controllers/osd.py
@@ -12,20 +12,24 @@ from ..tools import str_to_bool
 class Osd(RESTController):
     def list(self):
         osds = self.get_osd_map()
+
         # Extending by osd stats information
         for s in mgr.get('osd_stats')['osd_stats']:
             osds[str(s['osd'])].update({'osd_stats': s})
+
         # Extending by osd node information
         nodes = mgr.get('osd_map_tree')['nodes']
         osd_tree = [(str(o['id']), o) for o in nodes if o['id'] >= 0]
         for o in osd_tree:
             osds[o[0]].update({'tree': o[1]})
+
         # Extending by osd parent node information
         hosts = [(h['name'], h) for h in nodes if h['id'] < 0]
         for h in hosts:
             for o_id in h[1]['children']:
                 if o_id >= 0:
                     osds[str(o_id)]['host'] = h[1]
+
         # Extending by osd histogram data
         for o_id in osds:
             o = osds[o_id]
@@ -39,6 +43,7 @@ class Osd(RESTController):
             # Gauge stats
             for s in ['osd.numpg', 'osd.stat_bytes', 'osd.stat_bytes_used']:
                 o['stats'][s.split('.')[1]] = mgr.get_latest('osd', osd_spec, s)
+
         return list(osds.values())
 
     def get_osd_map(self):
