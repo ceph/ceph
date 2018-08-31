db5c9d937f594f5cf7b6c3d62a52c5b0bfa16e8e
Jason Dillaman <dillaman@redhat.com>
2018-08-31 14:17:51 -0400

    objclass: expose the required OSD release setting
    This can be utilized to determine when it's safe to enable new
    cls features.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/objclass/objclass.h
+++ src/objclass/objclass.h
@@ -156,6 +156,7 @@ extern uint64_t cls_current_version(cls_method_context_t hctx);
 extern int cls_current_subop_num(cls_method_context_t hctx);
 extern uint64_t cls_get_features(cls_method_context_t hctx);
 extern uint64_t cls_get_client_features(cls_method_context_t hctx);
+extern int8_t cls_get_required_osd_release(cls_method_context_t hctx);
 
 /* helpers */
 extern void cls_cxx_subop_version(cls_method_context_t hctx, string *s);
