ee0e388516d899239a5e75a2e6cfe592cb0a3047
Warren Usui <wusui@redhat.com>
2018-08-31 02:28:25 +0000

    Add ldap_server.
    First pass of ldap_server.  Still need workunit and possibly ldap_client.

---
+++ qa/tasks/ldap_server.py
@@ -0,0 +1,69 @@
+"""
+ldap_server
+"""
+import logging
+import time
+
+from teuthology.orchestra import run
+from teuthology import misc
+
+log = logging.getLogger(__name__)
+
+def task(ctx, config):
+    """
+    Start up an ldap_server in order to test ldap rgw authentication 
+    
+    Usage
+       tasks:
+       - ldap_server:
+           [client.0]
+
+    """
+
+    log.info('in ldap_server')
+    assert isinstance(config, list)
+    (client,) = ctx.cluster.only(config[0]).remotes
+    system_type = misc.get_system_type(client)
+    if system_type == 'rpm':
+        install_cmd = ['sudo', 'yum', '-y', 'install', 'ipa-server', 'ipa-server-dns']
+        fix_mod_ssl = ['sudo', 'yum', '-y', 'remove', 'mod_ssl']
+    else:
+        install_cmd = ['sudo', 'apt-get', '-y', 'install', 'freeipa-server', 'freeipa-server-dns']
+        fix_mod_ssl = []
+    client.run(args=install_cmd)
+    if fix_mod_ssl:
+        client.run(args=fix_mod_ssl)
+        client.run(args=['sudo', 'systemctl', 'restart', 'messagebus'])
+    client.run(args=['sudo',
+                'ipa-server-install',
+                '--realm',
+                'ceph.redhat.com',
+                '--ds-password',
+                't0pSecret',
+                '--admin-password',
+                't0pSecret',
+                '--unattended'])
+    time.sleep(120)
+    client.run(args=['echo', 
+                     't0pSecret',
+                     run.Raw('|'),
+                     'kinit',
+                     'admin'])
+    client.run(args=['ipa', 'user-add', 'rgw',
+                     '--first', 'rados', '--last', 'gateway'])
+    client.run(args=['ipa', 'user-add', 'newuser',
+                     '--first', 'new', '--last', 'user'])
+    client.run(args=['echo',
+                     't0pSecret\nt0pSecret',
+                     run.Raw('|'),
+                     'ipa', 
+                     'user-mod',
+                     'rgw',
+                     '--password'])
+    client.run(args=['echo',
+                     't0pSecret\nt0pSecret',
+                     run.Raw('|'),
+                     'ipa', 
+                     'user-mod',
+                     'newuser',
+                     '--password'])
