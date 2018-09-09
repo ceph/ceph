9f8227cb085cb089d9ac503290652c6ed5076503
Nathan Cutler <ncutler@suse.com>
2018-09-10 00:10:34 +0200

    qa: refactor deepsea suite yaml (deepsea/tier2/nfs-ganesha)
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

---
+++ qa/workunits/deepsea/suites/nfs-ganesha/cephfs.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -x
+WORKUNITS_DIR=$CEPH_BASE/qa/workunits
+sudo $WORKUNITS_DIR/deepsea/health-ok.sh --client-nodes=1 --mds --min-nodes=2 --nfs-ganesha
---
+++ qa/workunits/deepsea/suites/nfs-ganesha/rgw.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -x
+WORKUNITS_DIR=$CEPH_BASE/qa/workunits
+sudo $WORKUNITS_DIR/deepsea/health-ok.sh --client-nodes=1 --min-nodes=2 --nfs-ganesha --rgw
