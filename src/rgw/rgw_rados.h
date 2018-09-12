samchoraria/ceph-ci

rgw: fetch_remote_obj() takes optional olh epoch
Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/rgw_rados.h
@@ -3188,7 +3188,7 @@ public:
                        bool copy_if_newer,
                        map<string, bufferlist>& attrs,
                        RGWObjCategory category,
-                       uint64_t olh_epoch,
+                       std::optional<uint64_t> olh_epoch,
 		       ceph::real_time delete_at,
                        string *version_id,
                        string *ptag,

ceph/ceph

(cherry picked from commit ff0280b30180b73a53f08fc919febc03410e4c1e)
Conflicts:
    src/rgw/rgw_cr_rados.h
    src/rgw/rgw_rados.cc
    src/rgw/rgw_rados.h
- use boost::optional instead of std::optional

src/rgw/rgw_rados.h
@@ -3156,7 +3156,7 @@ public:
                        bool copy_if_newer,
                        map<string, bufferlist>& attrs,
                        RGWObjCategory category,
-                       uint64_t olh_epoch,
+                       boost::optional<uint64_t> olh_epoch,
 		       ceph::real_time delete_at,
                        string *version_id,
                        string *ptag,