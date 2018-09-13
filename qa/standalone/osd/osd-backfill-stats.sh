ef6940fbb6c084b7f9abe893a18faef75d3a5680
David Zafman <dzafman@redhat.com>
2018-09-13 13:15:09 -0700

    test: osd-backfill-stats.sh: Fix subtests to get primary which can change
    Fixes: http://tracker.ceph.com/issues/35982
    
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- qa/standalone/osd/osd-backfill-stats.sh
+++ qa/standalone/osd/osd-backfill-stats.sh
@@ -345,8 +345,11 @@ function TEST_backfill_out2() {
     ceph osd pool set $poolname size 3
     ceph osd out osd.${otherosd}
     ceph osd out osd.${primary}
+    # Primary might change before backfill starts
+    sleep 2
+    primary=$(get_primary $poolname obj1)
     ceph osd unset nobackfill
-    ceph tell osd.$(get_primary $poolname obj1) debug kick_recovery_wq 0
+    ceph tell osd.$primary debug kick_recovery_wq 0
     sleep 2
 
     wait_for_clean || return 1
@@ -399,8 +402,11 @@ function TEST_backfill_sizeup4_allout() {
     ceph osd out osd.$otherosd
     ceph osd out osd.$primary
     ceph osd pool set $poolname size 4
+    # Primary might change before backfill starts
+    sleep 2
+    primary=$(get_primary $poolname obj1)
     ceph osd unset nobackfill
-    ceph tell osd.$(get_primary $poolname obj1) debug kick_recovery_wq 0
+    ceph tell osd.$primary debug kick_recovery_wq 0
     sleep 2
 
     wait_for_clean || return 1
@@ -461,10 +467,10 @@ function TEST_backfill_remapped() {
 
     # primary may change due to invalidating the old pg_temp, which was [1,2,0],
     # but up_primary (3) chooses [0,1] for acting.
-    local new_primary=$(get_primary $poolname obj1)
+    primary=$(get_primary $poolname obj1)
 
     ceph osd unset nobackfill
-    ceph tell osd.$(get_primary $poolname obj1) debug kick_recovery_wq 0
+    ceph tell osd.$primary debug kick_recovery_wq 0
 
     sleep 2
 
@@ -472,7 +478,7 @@ function TEST_backfill_remapped() {
 
     local misplaced=$(expr $objects \* 2)
 
-    check $dir $PG $new_primary replicated 0 0 $misplaced $objects || return 1
+    check $dir $PG $primary replicated 0 0 $misplaced $objects || return 1
 
     delete_pool $poolname
     kill_daemons $dir || return 1
@@ -520,8 +526,11 @@ function TEST_backfill_ec_all_out() {
     do
         ceph osd out osd.$o
     done
+    # Primary might change before backfill starts
+    sleep 2
+    primary=$(get_primary $poolname obj1)
     ceph osd unset nobackfill
-    ceph tell osd.$(get_primary $poolname obj1) debug kick_recovery_wq 0
+    ceph tell osd.$primary debug kick_recovery_wq 0
     sleep 2
 
     wait_for_clean || return 1
@@ -567,8 +576,11 @@ function TEST_backfill_ec_prim_out() {
 
     ceph osd set nobackfill
     ceph osd out osd.$primary
+    # Primary might change before backfill starts
+    sleep 2
+    primary=$(get_primary $poolname obj1)
     ceph osd unset nobackfill
-    ceph tell osd.$(get_primary $poolname obj1) debug kick_recovery_wq 0
+    ceph tell osd.$primary debug kick_recovery_wq 0
     sleep 2
 
     wait_for_clean || return 1
@@ -622,8 +634,11 @@ function TEST_backfill_ec_down_all_out() {
     do
         ceph osd out osd.$o
     done
+    # Primary might change before backfill starts
+    sleep 2
+    primary=$(get_primary $poolname obj1)
     ceph osd unset nobackfill
-    ceph tell osd.$(get_primary $poolname obj1) debug kick_recovery_wq 0
+    ceph tell osd.$primary debug kick_recovery_wq 0
     sleep 2
     flush_pg_stats
 
@@ -703,8 +718,11 @@ function TEST_backfill_ec_down_out() {
     kill $(cat $dir/osd.${otherosd}.pid)
     ceph osd down osd.${otherosd}
     ceph osd out osd.${otherosd}
+    # Primary might change before backfill starts
+    sleep 2
+    primary=$(get_primary $poolname obj1)
     ceph osd unset nobackfill
-    ceph tell osd.$(get_primary $poolname obj1) debug kick_recovery_wq 0
+    ceph tell osd.$primary debug kick_recovery_wq 0
     sleep 2
 
     wait_for_clean || return 1
