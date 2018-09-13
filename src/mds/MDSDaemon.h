6296573ef1763e8e15998a12f9c4785a66a9c73c
Sage Weil <sage@redhat.com>
2018-09-13 14:03:21 -0500

    mds: expose keyring for authenticater verification
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mds/MDSDaemon.h
+++ src/mds/MDSDaemon.h
@@ -116,6 +116,7 @@ class MDSDaemon : public Dispatcher, public md_config_obs_t {
 			    bool& isvalid, CryptoKey& session_key,
 			    std::unique_ptr<AuthAuthorizerChallenge> *challenge) override;
   int ms_handle_authentication(Connection *con) override;
+  KeyStore *ms_get_auth1_authorizer_keystore() override;
   void ms_handle_accept(Connection *con) override;
   void ms_handle_connect(Connection *con) override;
   bool ms_handle_reset(Connection *con) override;
