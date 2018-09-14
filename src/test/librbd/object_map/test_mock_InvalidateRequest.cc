b7bb92d729505555201925f0d30280c6b1fdfc45
Jason Dillaman <dillaman@redhat.com>
2018-09-14 09:59:35 -0400

    librbd: test_flags helper should require snap id parameter
    The HEAD and snapshots have potentially different flag states
    since object maps get invalidated per revision.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>
    (cherry picked from commit 862082792d9c2ff23823e46937b7de9a42830cfd)
    
    Conflicts:
    	src/librbd/ObjectMap.cc: trivial resolution
    	src/librbd/operation/SnapshotRemoveRequest.cc: trivial resolution
    	src/test/librbd/test_DeepCopy.cc: DNE
    	src/test/librbd/test_Migration.cc: DNE

--- src/test/librbd/object_map/test_mock_InvalidateRequest.cc
+++ src/test/librbd/object_map/test_mock_InvalidateRequest.cc
@@ -27,7 +27,8 @@ TEST_F(TestMockObjectMapInvalidateRequest, UpdatesInMemoryFlag) {
   librbd::ImageCtx *ictx;
   ASSERT_EQ(0, open_image(m_image_name, &ictx));
   bool flags_set;
-  ASSERT_EQ(0, ictx->test_flags(RBD_FLAG_OBJECT_MAP_INVALID, &flags_set));
+  ASSERT_EQ(0, ictx->test_flags(CEPH_NOSNAP,
+                                RBD_FLAG_OBJECT_MAP_INVALID, &flags_set));
   ASSERT_FALSE(flags_set);
 
   C_SaferCond cond_ctx;
@@ -44,7 +45,8 @@ TEST_F(TestMockObjectMapInvalidateRequest, UpdatesInMemoryFlag) {
   }
   ASSERT_EQ(0, cond_ctx.wait());
 
-  ASSERT_EQ(0, ictx->test_flags(RBD_FLAG_OBJECT_MAP_INVALID, &flags_set));
+  ASSERT_EQ(0, ictx->test_flags(CEPH_NOSNAP,
+                                RBD_FLAG_OBJECT_MAP_INVALID, &flags_set));
   ASSERT_TRUE(flags_set);
 }
 
