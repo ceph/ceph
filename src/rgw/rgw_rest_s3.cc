d313d4de94da517b747179353c338e24ae84997d
Zhang Shaowen <zhangshaowen@cmss.chinamobile.com>
2018-09-19 10:04:44 +0800

    rgw: remove repetitive conditional statement in RGWHandler_REST_Obj_S3
    Signed-off-by: Zhang Shaowen <zhangshaowen@cmss.chinamobile.com>

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -3137,9 +3137,6 @@ RGWOp *RGWHandler_REST_Bucket_S3::op_options()
 
 RGWOp *RGWHandler_REST_Obj_S3::get_obj_op(bool get_data)
 {
-  if (is_acl_op()) {
-    return new RGWGetACLs_ObjStore_S3;
-  }
   RGWGetObj_ObjStore_S3 *get_obj_op = new RGWGetObj_ObjStore_S3;
   get_obj_op->set_get_data(get_data);
   return get_obj_op;
