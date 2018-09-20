b40121767e46b44a2ac27d8f93c4921c5716c595
Casey Bodley <cbodley@redhat.com>
2018-09-20 11:37:06 -0400

    rgw: remove BucketChangeObserver from data sync thread
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit f05db89637d280505321708683182f0f2c886208)
    
    Conflicts:
    	src/rgw/rgw_data_sync.cc
    	src/rgw/rgw_data_sync.h
    - argument lists are different in luminous, compared to master

--- src/rgw/rgw_data_sync.cc
+++ src/rgw/rgw_data_sync.cc
@@ -649,7 +649,7 @@ int RGWRemoteDataLog::read_source_log_shards_next(map<int, string> shard_markers
 int RGWRemoteDataLog::init(const string& _source_zone, RGWRESTConn *_conn, RGWSyncErrorLogger *_error_logger, RGWSyncModuleInstanceRef& _sync_module)
 {
   sync_env.init(store->ctx(), store, _conn, async_rados, &http_manager, _error_logger,
-                _source_zone, _sync_module, observer);
+                _source_zone, _sync_module);
 
   if (initialized) {
     return 0;
@@ -1069,9 +1069,6 @@ public:
           ldout(sync_env->store->ctx(), 0) << "ERROR: failed to remove omap key from error repo ("
              << error_repo->get_obj() << " retcode=" << retcode << dendl;
         }
-      }
-      if (sync_env->observer) {
-        sync_env->observer->on_bucket_changed(bs.bucket.get_key());
       }
       /* FIXME: what do do in case of error */
       if (marker_tracker && !entry_marker.empty()) {
@@ -1788,7 +1785,7 @@ int RGWRemoteBucketLog::init(const string& _source_zone, RGWRESTConn *_conn,
   bs.shard_id = shard_id;
 
   sync_env.init(store->ctx(), store, conn, async_rados, http_manager,
-                _error_logger, source_zone, _sync_module, nullptr);
+                _error_logger, source_zone, _sync_module);
 
   return 0;
 }
@@ -3238,7 +3235,7 @@ int rgw_bucket_sync_status(RGWRados *store, const std::string& source_zone,
   RGWDataSyncEnv env;
   RGWSyncModuleInstanceRef module; // null sync module
   env.init(store->ctx(), store, nullptr, store->get_async_rados(),
-           nullptr, nullptr, source_zone, module, nullptr);
+           nullptr, nullptr, source_zone, module);
 
   RGWCoroutinesManager crs(store->ctx(), store->get_cr_registry());
   return crs.run(new RGWCollectBucketSyncStatusCR(store, &env, num_shards,
