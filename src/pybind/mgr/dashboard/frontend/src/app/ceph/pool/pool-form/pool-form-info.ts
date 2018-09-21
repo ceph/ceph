3542dcf7a5f90e623fc95eb140290a5fd207c2ec
Stephan Müller <smueller@suse.com>
2018-09-21 10:08:19 +0200

    mgr/dashboard: Unset any compression setting for pools in UI
    The dashboard API now can delete all compression options if the
    compression mode is set to 'unset' during edit. Other dismissed options
    will be overwritten with the reset value on submit, which will cause
    the deletion of the previously set settings.
    
    Fixes: https://tracker.ceph.com/issues/36355
    Signed-off-by: Stephan Müller <smueller@suse.com>
    (cherry picked from commit ce341cfba931468fb64ee069817832c5dee5eee3)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/pool/pool-form/pool-form-info.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/pool/pool-form/pool-form-info.ts
@@ -4,6 +4,7 @@ export class PoolFormInfo {
   pool_names: string[];
   osd_count: number;
   is_all_bluestore: boolean;
+  bluestore_compression_algorithm: string;
   compression_algorithms: string[];
   compression_modes: string[];
   crush_rules_replicated: CrushRule[];
