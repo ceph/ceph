ceph/ceph

commit 1b4f86cb044bead252f22c427417bea643eb19ca

PG: add async recovery and backfill to pg dout() prefix
Signed-off-by: Neha Ojha <nojha@redhat.com>

src/osd/PG.cc
@@ -6047,6 +6047,10 @@ ostream& operator<<(ostream& out, const PG& pg)
     out << "/" << pg.acting;
   if (pg.is_ec_pg())
     out << "p" << pg.get_primary();
+  if (!pg.async_recovery_targets.empty())
+    out << " async=[" << pg.async_recovery_targets << "]";
+  if (!pg.backfill_targets.empty())
+    out << " backfill=[" << pg.backfill_targets << "]";
   out << " r=" << pg.get_role();
   out << " lpr=" << pg.get_last_peering_reset();
 

commit 6847d3fd6f7a6cd671cb7b4fa14240f0e6df3c09

osd: Remove old bft= which has been superceded by backfill=
Fixes: http://tracker.ceph.com/issues/36170
Caused by: 1b4f86cb0

Signed-off-by: David Zafman <dzafman@redhat.com>

src/osd/PG.cc
@@ -6327,8 +6327,6 @@ ostream& operator<<(ostream& out, const PG& pg)
     }
   }
 
-  if (!pg.backfill_targets.empty())
-    out << " bft=" << pg.backfill_targets;
   out << " crt=" << pg.pg_log.get_can_rollback_to();
 
   if (pg.last_complete_ondisk != pg.info.last_complete)