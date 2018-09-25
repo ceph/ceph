a296b56bcc380ec3adf5f3e0bc5bb9927b99d8a9
Ali Maredia <amaredia@redhat.com>
2018-09-25 15:54:58 -0400

    rgw: no DoutPrefixProvider as arg for RGWRemoteDataLog::init
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- src/rgw/rgw_data_sync.h
+++ src/rgw/rgw_data_sync.h
@@ -286,7 +286,7 @@ public:
       http_manager(store->ctx(), completion_mgr),
       lock("RGWRemoteDataLog::lock"), data_sync_cr(NULL),
       initialized(false) {}
-  int init(const DoutPrefixProvider *_dpp, const string& _source_zone, RGWRESTConn *_conn, RGWSyncErrorLogger *_error_logger,
+  int init(const string& _source_zone, RGWRESTConn *_conn, RGWSyncErrorLogger *_error_logger,
            RGWSyncTraceManager *_sync_tracer, RGWSyncModuleInstanceRef& module);
   void finish();
 
