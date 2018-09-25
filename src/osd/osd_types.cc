d38f6a11701ec788e4d384aa5b0ae65b8e57da64
Dan van der Ster <daniel.vanderster@cern.ch>
2018-09-25 10:39:37 +0200

    osd: add creating to pg_string_state
    Fixes: http://tracker.ceph.com/issues/36174
    Signed-off-by: Dan van der Ster <daniel.vanderster@cern.ch>

--- src/osd/osd_types.cc
+++ src/osd/osd_types.cc
@@ -968,6 +968,8 @@ boost::optional<uint64_t> pg_string_state(const std::string& state)
     type = PG_STATE_SNAPTRIM_WAIT;
   else if (state == "snaptrim_error")
     type = PG_STATE_SNAPTRIM_ERROR;
+  else if (state == "creating")
+    type = PG_STATE_CREATING;
   else if (state == "unknown")
     type = 0;
   else
