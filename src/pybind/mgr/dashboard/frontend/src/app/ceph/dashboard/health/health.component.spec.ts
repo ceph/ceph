5ee883dc5f52e37759d29704c75dd6861e37abbb
alfonsomthd <almartin@redhat.com>
2018-10-03 13:59:50 +0200

    mgr/dashboard: New Landing Page: Milestone 2
    Added tests for Health component to ensure that
    
    all groups and cards are rendered.
    
    Fixes: https://tracker.ceph.com/issues/27050
    
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>
    (cherry picked from commit 509380c74ac5a10fcec3ff2aeac76e06ec7c5c9a)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/health/health.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/health/health.component.spec.ts
@@ -3,6 +3,7 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PopoverModule } from 'ngx-bootstrap/popover';
+import { of } from 'rxjs';
 
 import { configureTestBed } from '../../../../testing/unit-test-helper';
 import { DashboardService } from '../../../shared/api/dashboard.service';
@@ -19,6 +20,22 @@ import { HealthComponent } from './health.component';
 describe('HealthComponent', () => {
   let component: HealthComponent;
   let fixture: ComponentFixture<HealthComponent>;
+  let getHealthSpy;
+  const healthPayload = {
+    health: { status: 'HEALTH_OK', checks: [] },
+    mon_status: { monmap: { mons: [] }, quorum: [] },
+    osd_map: { osds: [] },
+    mgr_map: { standbys: [] },
+    hosts: 0,
+    rgw: 0,
+    fs_map: { filesystems: [] },
+    iscsi_daemons: 0,
+    client_perf: {},
+    scrub_status: 'Inactive',
+    pools: [],
+    df: { stats: { total_objects: 0 } },
+    pg_info: {}
+  };
 
   configureTestBed({
     providers: [DashboardService],
@@ -39,10 +56,38 @@ describe('HealthComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HealthComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    getHealthSpy = spyOn(fixture.debugElement.injector.get(DashboardService), 'getHealth');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render all info groups and all info cards', () => {
+    getHealthSpy.and.returnValue(of(healthPayload));
+    fixture.detectChanges();
+
+    const infoGroups = fixture.debugElement.nativeElement.querySelectorAll('cd-info-group');
+    expect(infoGroups.length).toBe(3);
+
+    const infoCards = fixture.debugElement.nativeElement.querySelectorAll('cd-info-card');
+    expect(infoCards.length).toBe(18);
+  });
+
+  // @TODO: remove this test when logs are no longer in landing page
+  // See https://tracker.ceph.com/issues/24571 & https://github.com/ceph/ceph/pull/23834
+  it('should render Logs group & cards in addition to the other ones', () => {
+    const payload = healthPayload;
+    payload['clog'] = [];
+    payload['audit_log'] = [];
+
+    getHealthSpy.and.returnValue(of(payload));
+    fixture.detectChanges();
+
+    const infoGroups = fixture.debugElement.nativeElement.querySelectorAll('cd-info-group');
+    expect(infoGroups.length).toBe(4);
+
+    const infoCards = fixture.debugElement.nativeElement.querySelectorAll('cd-info-card');
+    expect(infoCards.length).toBe(20);
   });
 });
