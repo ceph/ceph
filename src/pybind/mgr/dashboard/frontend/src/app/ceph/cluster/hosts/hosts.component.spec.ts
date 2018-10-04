12174a3eb6ee40b30264d21e1037176021e172f0
Kanika Murarka <kmurarka@redhat.com>
2018-10-04 21:23:22 +0530

    mgr/dashboard: Adding Grafana componenets to views
    Fixes: https://tracker.ceph.com/issues/24999
    Signed-off-by: Kanika Murarka <kmurarka@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/hosts/hosts.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/hosts/hosts.component.spec.ts
@@ -3,12 +3,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { BsDropdownModule } from 'ngx-bootstrap';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 import { configureTestBed } from '../../../../testing/unit-test-helper';
 import { ComponentsModule } from '../../../shared/components/components.module';
 import { Permissions } from '../../../shared/models/permissions';
 import { AuthStorageService } from '../../../shared/services/auth-storage.service';
 import { SharedModule } from '../../../shared/shared.module';
+import { HostDetailsComponent } from './host-details/host-details.component';
 import { HostsComponent } from './hosts.component';
 
 describe('HostsComponent', () => {
@@ -25,12 +27,13 @@ describe('HostsComponent', () => {
     imports: [
       SharedModule,
       HttpClientTestingModule,
+      TabsModule.forRoot(),
       ComponentsModule,
       BsDropdownModule.forRoot(),
       RouterTestingModule
     ],
     providers: [{ provide: AuthStorageService, useValue: fakeAuthStorageService }],
-    declarations: [HostsComponent]
+    declarations: [HostsComponent, HostDetailsComponent]
   });
 
   beforeEach(() => {
