ad17e733c88927fce8b7762513d715b4fdd247f8
Kanika Murarka <kmurarka@redhat.com>
2018-10-08 14:59:31 +0530

    mgr/dashboard: Adds dynamic documentation link
    Fixes: https://tracker.ceph.com/issues/24999
    Signed-off-by: Kanika Murarka <kmurarka@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/pool/pool-list/pool-list.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/pool/pool-list/pool-list.component.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { TabsModule } from 'ngx-bootstrap/tabs/tabs.module';
 
@@ -13,7 +14,7 @@ describe('PoolListComponent', () => {
 
   configureTestBed({
     declarations: [PoolListComponent],
-    imports: [SharedModule, TabsModule.forRoot(), HttpClientTestingModule]
+    imports: [SharedModule, TabsModule.forRoot(), HttpClientTestingModule, RouterTestingModule]
   });
 
   beforeEach(() => {
