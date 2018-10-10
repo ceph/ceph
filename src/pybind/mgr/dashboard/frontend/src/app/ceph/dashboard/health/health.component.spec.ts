0a98846e425aec656e5a56e04fbfbd0e229757db
alfonsomthd <almartin@redhat.com>
2018-10-10 10:02:18 +0200

    mgr/dashboard: Landing Page: info group visibility Do not display info groups not containing any card. Fixes: https://tracker.ceph.com/issues/27047
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>
    (cherry picked from commit 83b8cebe4cbb0993acc3b4b59d2effa3fffdd0ec)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/health/health.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/health/health.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import * as _ from 'lodash';
 import { PopoverModule } from 'ngx-bootstrap/popover';
 import { of } from 'rxjs';
 
@@ -22,7 +23,7 @@ describe('HealthComponent', () => {
   let fixture: ComponentFixture<HealthComponent>;
   let getHealthSpy;
   const healthPayload = {
-    health: { status: 'HEALTH_OK', checks: [] },
+    health: { status: 'HEALTH_OK' },
     mon_status: { monmap: { mons: [] }, quorum: [] },
     osd_map: { osds: [] },
     mgr_map: { standbys: [] },
@@ -56,7 +57,7 @@ describe('HealthComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HealthComponent);
     component = fixture.componentInstance;
-    getHealthSpy = spyOn(fixture.debugElement.injector.get(DashboardService), 'getHealth');
+    getHealthSpy = spyOn(TestBed.get(DashboardService), 'getHealth');
   });
 
   it('should create', () => {
@@ -74,10 +75,76 @@ describe('HealthComponent', () => {
     expect(infoCards.length).toBe(18);
   });
 
+  it('should render all except "Status" group and cards', () => {
+    const payload = _.cloneDeep(healthPayload);
+    payload.health.status = '';
+    payload.mon_status = null;
+    payload.osd_map = null;
+    payload.mgr_map = null;
+    payload.hosts = null;
+    payload.rgw = null;
+    payload.fs_map = null;
+    payload.iscsi_daemons = null;
+
+    getHealthSpy.and.returnValue(of(payload));
+    fixture.detectChanges();
+
+    const infoGroups = fixture.debugElement.nativeElement.querySelectorAll('cd-info-group');
+    expect(infoGroups.length).toBe(2);
+
+    const infoCards = fixture.debugElement.nativeElement.querySelectorAll('cd-info-card');
+    expect(infoCards.length).toBe(10);
+  });
+
+  it('should render all except "Performance" group and cards', () => {
+    const payload = _.cloneDeep(healthPayload);
+    payload.scrub_status = '';
+    payload.client_perf = null;
+
+    getHealthSpy.and.returnValue(of(payload));
+    fixture.detectChanges();
+
+    const infoGroups = fixture.debugElement.nativeElement.querySelectorAll('cd-info-group');
+    expect(infoGroups.length).toBe(2);
+
+    const infoCards = fixture.debugElement.nativeElement.querySelectorAll('cd-info-card');
+    expect(infoCards.length).toBe(13);
+  });
+
+  it('should render all except "Capacity" group and cards', () => {
+    const payload = _.cloneDeep(healthPayload);
+    payload.pools = null;
+    payload.df = null;
+    payload.pg_info = null;
+
+    getHealthSpy.and.returnValue(of(payload));
+    fixture.detectChanges();
+
+    const infoGroups = fixture.debugElement.nativeElement.querySelectorAll('cd-info-group');
+    expect(infoGroups.length).toBe(2);
+
+    const infoCards = fixture.debugElement.nativeElement.querySelectorAll('cd-info-card');
+    expect(infoCards.length).toBe(13);
+  });
+
+  it('should render all groups and 1 card per group', () => {
+    const payload = { hosts: 0, scrub_status: 'Inactive', pools: [] };
+
+    getHealthSpy.and.returnValue(of(payload));
+    fixture.detectChanges();
+
+    const infoGroups = fixture.debugElement.nativeElement.querySelectorAll('cd-info-group');
+    expect(infoGroups.length).toBe(3);
+
+    _.each(infoGroups, (infoGroup) => {
+      expect(infoGroup.querySelectorAll('cd-info-card').length).toBe(1);
+    });
+  });
+
   // @TODO: remove this test when logs are no longer in landing page
   // See https://tracker.ceph.com/issues/24571 & https://github.com/ceph/ceph/pull/23834
   it('should render Logs group & cards in addition to the other ones', () => {
-    const payload = healthPayload;
+    const payload = _.cloneDeep(healthPayload);
     payload['clog'] = [];
     payload['audit_log'] = [];
 
