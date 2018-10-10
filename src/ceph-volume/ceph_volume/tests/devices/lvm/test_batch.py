c7be0592bee94e73e5a68b932c67dc98ef4391f7
Andrew Schoen <aschoen@redhat.com>
2018-10-10 15:28:29 -0400

    ceph-volume: extracts batch.filter_devices from Batch._get_strategy
    This allows us to easily provide tests for that method.
    
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

---
+++ src/ceph-volume/ceph_volume/tests/devices/lvm/test_batch.py
@@ -0,0 +1,61 @@
+from ceph_volume.devices.lvm import batch
+
+
+class TestFilterDevices(object):
+
+    def test_filter_used_device(self, factory):
+        device1 = factory(used_by_ceph=True, abspath="/dev/sda")
+        args = factory(devices=[device1], filtered_devices={})
+        result = batch.filter_devices(args)
+        assert not result
+        assert device1.abspath in args.filtered_devices
+
+    def test_has_unused_devices(self, factory):
+        device1 = factory(
+            used_by_ceph=False,
+            abspath="/dev/sda",
+            rotational=False,
+            is_lvm_member=False
+        )
+        args = factory(devices=[device1], filtered_devices={})
+        result = batch.filter_devices(args)
+        assert device1 in result
+        assert not args.filtered_devices
+
+    def test_filter_device_used_as_a_journal(self, factory):
+        hdd1 = factory(
+            used_by_ceph=True,
+            abspath="/dev/sda",
+            rotational=True,
+            is_lvm_member=True,
+        )
+        lv = factory(tags={"ceph.type": "journal"})
+        ssd1 = factory(
+            used_by_ceph=False,
+            abspath="/dev/nvme0n1",
+            rotational=False,
+            is_lvm_member=True,
+            lvs=[lv],
+        )
+        args = factory(devices=[hdd1, ssd1], filtered_devices={})
+        result = batch.filter_devices(args)
+        assert not result
+        assert ssd1.abspath in args.filtered_devices
+
+    def test_last_device_is_not_filtered(self, factory):
+        hdd1 = factory(
+            used_by_ceph=True,
+            abspath="/dev/sda",
+            rotational=True,
+            is_lvm_member=True,
+        )
+        ssd1 = factory(
+            used_by_ceph=False,
+            abspath="/dev/nvme0n1",
+            rotational=False,
+            is_lvm_member=False,
+        )
+        args = factory(devices=[hdd1, ssd1], filtered_devices={})
+        result = batch.filter_devices(args)
+        assert result
+        assert len(args.filtered_devices) == 1
