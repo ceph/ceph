70c8c8110c339fcb6bc68ad2f2bd0166a144692d
Casey Bodley <cbodley@redhat.com>
2018-10-10 15:54:13 -0400

    rgw: encryption filter uses rgw::putobj::Pipe
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_crypt.h
+++ src/rgw/rgw_crypt.h
@@ -9,6 +9,7 @@
 #include <rgw/rgw_op.h>
 #include <rgw/rgw_rest.h>
 #include <rgw/rgw_rest_s3.h>
+#include "rgw_putobj.h"
 #include <boost/utility/string_view.hpp>
 
 /**
@@ -111,28 +112,19 @@ public:
 }; /* RGWGetObj_BlockDecrypt */
 
 
-class RGWPutObj_BlockEncrypt : public RGWPutObj_Filter
+class RGWPutObj_BlockEncrypt : public rgw::putobj::Pipe
 {
   CephContext* cct;
   std::unique_ptr<BlockCrypt> crypt; /**< already configured stateless BlockCrypt
                                           for operations when enough data is accumulated */
-  off_t ofs; /**< stream offset of data we expect to show up next through \ref handle_data */
   bufferlist cache; /**< stores extra data that could not (yet) be processed by BlockCrypt */
-  size_t block_size; /**< snapshot of \ref BlockCrypt.get_block_size() */
+  const size_t block_size; /**< snapshot of \ref BlockCrypt.get_block_size() */
 public:
   RGWPutObj_BlockEncrypt(CephContext* cct,
-                         RGWPutObjDataProcessor* next,
+                         rgw::putobj::DataProcessor *next,
                          std::unique_ptr<BlockCrypt> crypt);
-  virtual ~RGWPutObj_BlockEncrypt();
-  virtual int handle_data(bufferlist& bl,
-                          off_t ofs,
-                          void **phandle,
-                          rgw_raw_obj *pobj,
-                          bool *again) override;
-  virtual int throttle_data(void *handle,
-                            const rgw_raw_obj& obj,
-                            uint64_t size,
-                            bool need_to_wait) override;
+
+  int process(bufferlist&& data, uint64_t logical_offset) override;
 }; /* RGWPutObj_BlockEncrypt */
 
 
