93ed8adb33a1e996878862124f8db6fefc46d0e8
Casey Bodley <cbodley@redhat.com>
2018-10-10 15:11:57 -0400

    rgw: add rgw::putobj::HeadObjectProcessor
    a striped object processor with special handling for the first chunk of
    data
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_putobj_processor.h
+++ src/rgw/rgw_putobj_processor.h
@@ -14,6 +14,8 @@
 
 #pragma once
 
+#include <optional>
+
 #include "rgw_putobj.h"
 #include "rgw_rados.h"
 
@@ -33,6 +35,34 @@ class ObjectProcessor : public DataProcessor {
                        const char *if_match, const char *if_nomatch,
                        const std::string *user_data,
                        rgw_zone_set *zones_trace, bool *canceled) = 0;
+};
+
+// an object processor with special handling for the first chunk of the head.
+// the virtual process_first_chunk() function returns a processor to handle the
+// rest of the object
+class HeadObjectProcessor : public ObjectProcessor {
+  uint64_t head_chunk_size;
+  // buffer to capture the first chunk of the head object
+  bufferlist head_data;
+  // initialized after process_first_chunk() to process everything else
+  DataProcessor *processor = nullptr;
+  uint64_t data_offset = 0; // maximum offset of data written (ie compressed)
+ protected:
+  uint64_t get_actual_size() const { return data_offset; }
+
+  // process the first chunk of data and return a processor for the rest
+  virtual int process_first_chunk(bufferlist&& data,
+                                  DataProcessor **processor) = 0;
+ public:
+  HeadObjectProcessor(uint64_t head_chunk_size)
+    : head_chunk_size(head_chunk_size)
+  {}
+
+  void set_head_chunk_size(uint64_t size) { head_chunk_size = size; }
+
+  // cache first chunk for process_first_chunk(), then forward everything else
+  // to the returned processor
+  int process(bufferlist&& data, uint64_t logical_offset) final override;
 };
 
 } // namespace rgw::putobj
