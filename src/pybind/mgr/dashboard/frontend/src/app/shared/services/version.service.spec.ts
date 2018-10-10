b890a691fcb57e7087532d1e4c915367363173f6
Zack Cerza <zack@redhat.com>
2018-10-10 15:29:02 -0400

    Implement version service
    Much of this was adapted from the summary service. I do wonder, though,
    if the async bits are necessary at all since for the version to change
    the mgr will have to have been restarted anyway.

--- src/pybind/mgr/dashboard/frontend/src/app/shared/services/version.service.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/version.service.spec.ts
@@ -1,15 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { TestBed, inject } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { of as observableOf, Subscriber } from 'rxjs';
+
+import { configureTestBed } from '../../../testing/unit-test-helper';
 
 import { VersionService } from './version.service';
 
 describe('VersionService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [VersionService]
-    });
+  let versionService: VersionService;
+
+  const version = {
+    raw: "ceph version 14.0.0-4116-gbd6b4b4 (bd6b4b46e5932d8827687e1e8ed624293daa5d60) nautilus (dev)",
+    hash: "bd6b4b46e5932d8827687e1e8ed624293daa5d60",
+    name: "nautilus",
+    number: "14.0.0-4116-gbd6b4b4"
+  };
+
+  const httpClientSpy = {
+    get: () => observableOf(version)
+  };
+
+  configureTestBed({
+    imports: [RouterTestingModule],
+    providers: [
+      VersionService,
+      { provide: HttpClient, useValue: httpClientSpy }
+    ]
   });
 
-  it('should be created', inject([VersionService], (service: VersionService) => {
-    expect(service).toBeTruthy();
-  }));
+  beforeEach(() => {
+    versionService = TestBed.get(VersionService);
+  });
+
+  it('should be created', () => {
+    expect(versionService).toBeTruthy();
+  });
+
+  it('should call getCurrentVersion', () => {
+    expect(versionService.getCurrentVersion()).toEqual(version);
+  });
 });
--- src/pybind/mgr/dashboard/frontend/src/app/shared/services/version.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/version.service.ts
@@ -1,9 +1,85 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+
+import * as _ from 'lodash';
+import { BehaviorSubject, Subscription } from 'rxjs';
+
+import { ExecutingTask } from '../models/executing-task';
+import { ServicesModule } from './services.module';
+
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: ServicesModule
 })
 export class VersionService {
+  // Observable sources
+  private versionDataSource = new BehaviorSubject(null);
 
-  constructor() { }
+  // Observable streams
+  versionData$ = this.versionDataSource.asObservable();
+
+  constructor(private http: HttpClient, private router: Router, private ngZone: NgZone) {
+    this.refresh();
+  }
+
+  refresh() {
+    this.http.get('api/public/version').subscribe((data) => {
+      this.versionDataSource.next(data);
+    });
+
+    this.ngZone.runOutsideAngular(() => {
+      setTimeout(() => {
+        this.ngZone.run(() => {
+          this.refresh();
+        });
+      }, 5000);
+    });
+  }
+
+  /**
+   * Returns the current value of versionData
+   *
+   * @returns {object}
+   * @memberof VersionService
+   */
+  getCurrentVersion() {
+    return this.versionDataSource.getValue();
+  }
+
+  /**
+   * Subscribes to the versionData,
+   * which is updated once every 5 seconds or when a new task is created.
+   *
+   * @param {(version: any) => void} call
+   * @param {(error: any) => void} error
+   * @returns {Subscription}
+   * @memberof VersionService
+   */
+  subscribe(call: (version: any) => void, error?: (error: any) => void): Subscription {
+    return this.versionData$.subscribe(call, error);
+  }
+
+  /**
+   * Inserts a newly created task to the local list of executing tasks.
+   * After that, it will automatically push that new information
+   * to all subscribers.
+   *
+   * @param {ExecutingTask} task
+   * @memberof VersionService
+   */
+  addRunningTask(task: ExecutingTask) {
+    const current = this.versionDataSource.getValue();
+    if (!current) {
+      return;
+    }
+
+    if (_.isArray(current.executing_tasks)) {
+      current.executing_tasks.push(task);
+    } else {
+      current.executing_tasks = [task];
+    }
+
+    this.versionDataSource.next(current);
+  }
 }
