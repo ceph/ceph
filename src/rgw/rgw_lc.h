20fcb73b4736abe663cb6d4014b91230c99a2aaa
Ali Maredia <amaredia@redhat.com>
2018-10-10 11:14:40 -0400

    rgw: refactor logging in gc and lc
    RGWGC and RGWLC are now DoutPrefixProviders
    
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- src/rgw/rgw_lc.h
+++ src/rgw/rgw_lc.h
@@ -336,7 +336,7 @@ public:
 };
 WRITE_CLASS_ENCODER(RGWLifecycleConfiguration)
 
-class RGWLC {
+class RGWLC : public DoutPrefixProvider {
   CephContext *cct;
   RGWRados *store;
   int max_objs{0};
@@ -345,13 +345,14 @@ class RGWLC {
   string cookie;
 
   class LCWorker : public Thread {
+    const DoutPrefixProvider *dpp;
     CephContext *cct;
     RGWLC *lc;
     Mutex lock;
     Cond cond;
 
   public:
-    LCWorker(CephContext *_cct, RGWLC *_lc) : cct(_cct), lc(_lc), lock("LCWorker") {}
+    LCWorker(const DoutPrefixProvider* _dpp, CephContext *_cct, RGWLC *_lc) : dpp(_dpp), cct(_cct), lc(_lc), lock("LCWorker") {}
     void *entry() override;
     void stop();
     bool should_work(utime_t& now);
@@ -379,6 +380,10 @@ class RGWLC {
   bool going_down();
   void start_processor();
   void stop_processor();
+
+  CephContext *get_cct() const override { return store->ctx(); }
+  unsigned get_subsys() const;
+  std::ostream& gen_prefix(std::ostream& out) const;
 
   private:
   int remove_expired_obj(RGWBucketInfo& bucket_info, rgw_obj_key obj_key, const string& owner, const string& owner_display_name, bool remove_indeed = true);
