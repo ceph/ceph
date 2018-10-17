0c8b9adaa7307512d964bb17e64907eba513e3f1
David Zafman <dzafman@redhat.com>
2018-10-17 12:31:59 -0700

    osd: Deny reservation if expected backfill size would put us over backfill_full_ratio
    Replicated pools
    
    Fixes: http://tracker.ceph.com/issues/19753
    
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/osd/osd_types.h
+++ src/osd/osd_types.h
@@ -2164,6 +2164,9 @@ struct store_statfs_t
   uint64_t kb() const {
     return total >> 10;
   }
+  uint64_t kb_used() const {
+    return (total - available - internally_reserved) >> 10;
+  }
   uint64_t kb_used_raw() const {
     return get_used_raw() >> 10;
   }
