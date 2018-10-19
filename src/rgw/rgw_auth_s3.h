480f0877d15520c045331ca591f91e0f6a36bbb5
Matt Benjamin <mbenjamin@redhat.com>
2018-10-19 15:59:29 -0400

    rgw: handle S3 version 2 pre-signed urls with meta-data
    Add CanonicalizedAmzHeaders to the v2 signing document, as required
    in https://docs.aws.amazon.com/AmazonS3/latest/dev/RESTAuthentication.html
    
    Also lift qs metadata into x_meta_map in RGWPutObj_ObjStore_S3::get_parms(),
    so they are applied to the stored object.
    
    Fixes: https://tracker.ceph.com/issues/23470
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit 3b0480ed136cebd3fba2322a77c7d047ef6ad5e3)

--- src/rgw/rgw_auth_s3.h
+++ src/rgw/rgw_auth_s3.h
@@ -345,6 +345,7 @@ void rgw_create_s3_canonical_header(
   const char *content_type,
   const char *date,
   const std::map<std::string, std::string>& meta_map,
+  const std::map<std::string, std::string>& qs_map,
   const char *request_uri,
   const std::map<std::string, std::string>& sub_resources,
   std::string& dest_str);
