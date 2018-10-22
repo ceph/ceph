85570639cbfed76cf5c9221b37d14e6d20ecbd37
alfonsomthd <almartin@redhat.com>
2018-10-22 19:38:38 +0200

    mgr/dashboard: Add TSLint rule - TSlint no-unused-variable rule added. - Cleanup: unused imports and variables.
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/configuration/configuration-form/configuration-form.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/configuration/configuration-form/configuration-form.component.spec.ts
@@ -14,7 +14,6 @@ import { ConfigFormModel } from './configuration-form.model';
 describe('ConfigurationFormComponent', () => {
   let component: ConfigurationFormComponent;
   let fixture: ComponentFixture<ConfigurationFormComponent>;
-  let activatedRoute: ActivatedRoute;
 
   configureTestBed({
     imports: [
@@ -35,7 +34,6 @@ describe('ConfigurationFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ConfigurationFormComponent);
     component = fixture.componentInstance;
-    activatedRoute = TestBed.get(ActivatedRoute);
   });
 
   it('should create', () => {
