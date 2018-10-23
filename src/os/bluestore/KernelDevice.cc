127309264ce2b278b527b85ad0fa41ba7c0cb292
Radoslaw Zarzynski <rzarzyns@redhat.com>
2018-10-23 15:36:48 +0200

    os/bluestore: KernelDevice creates buffer::hangable_ptr directly.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/os/bluestore/KernelDevice.cc
+++ src/os/bluestore/KernelDevice.cc
@@ -807,7 +807,8 @@ int KernelDevice::read(uint64_t off, uint64_t len, bufferlist *pbl,
 
   _aio_log_start(ioc, off, len);
 
-  bufferptr p = buffer::create_small_page_aligned(len);
+  auto& p = buffer::hangable_ptr::create(
+    buffer::create_small_page_aligned(len));
   int r = ::pread(buffered ? fd_buffered : fd_direct,
 		  p.c_str(), len, off);
   if (r < 0) {
@@ -861,7 +862,8 @@ int KernelDevice::direct_read_unaligned(uint64_t off, uint64_t len, char *buf)
 {
   uint64_t aligned_off = align_down(off, block_size);
   uint64_t aligned_len = align_up(off+len, block_size) - aligned_off;
-  bufferptr p = buffer::create_small_page_aligned(aligned_len);
+  auto& p = buffer::hangable_ptr::create(
+    buffer::create_small_page_aligned(aligned_len));
   int r = 0;
 
   r = ::pread(fd_direct, p.c_str(), aligned_len, aligned_off);
