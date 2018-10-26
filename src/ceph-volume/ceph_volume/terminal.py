9b55e664c9c5a5bd93ade7e01859eae26a47a98b
Mehdi Abaakouk <sileht@sileht.net>
2018-10-26 10:34:16 +0200

    ceph-volume: reduce complexity of colorize
    Signed-off-by: Mehdi Abaakouk <sileht@sileht.net>

--- src/ceph-volume/ceph_volume/terminal.py
+++ src/ceph-volume/ceph_volume/terminal.py
@@ -2,75 +2,32 @@ import logging
 import sys
 
 
-class colorize(str):
+class Colorizer(object):
     """
     Pretty simple to use::
 
-        colorize.make('foo').bold
-        colorize.make('foo').green
-        colorize.make('foo').yellow
-        colorize.make('foo').red
-        colorize.make('foo').blue
-
-    Otherwise you could go the long way (for example if you are
-    testing this class)::
-
-        string = colorize('foo')
-        string._set_attributes()
-        string.red
+        Colorizer('\033[33m')("my test")
 
     """
 
-    def __init__(self, string):
-        self.stdout = sys.__stdout__
-        self.appends = ''
-        self.prepends = ''
-        self.isatty = self.stdout.isatty()
+    def __init__(self, color):
+        self.color = color
+        self.isatty = sys.__stdout__.isatty()
 
-    def _set_attributes(self):
-        """
-        Sets the attributes here because the str class does not
-        allow to pass in anything other than a string to the constructor
-        so we can't really mess with the other attributes.
-        """
-        for k, v in self.__colors__.items():
-            setattr(self, k, self.make_color(v))
-
-    def make_color(self, color):
-        if not self.isatty:
-            return self
-        return color + self + '\033[0m' + self.appends
-
-    @property
-    def __colors__(self):
-        return dict(
-            blue='\033[34m',
-            green='\033[92m',
-            yellow='\033[33m',
-            red='\033[91m',
-            bold='\033[1m',
-            ends='\033[0m'
-        )
-
-    @classmethod
-    def make(cls, string):
-        """
-        A helper method to return itself and workaround the fact that
-        the str object doesn't allow extra arguments passed in to the
-        constructor
-        """
-        obj = cls(string)
-        obj._set_attributes()
-        return obj
+    def __call__(self, string):
+        if self.isatty:
+            return self.color + string + '\033[0m'
+        else:
+            return string
 
 #
 # Common string manipulations
 #
-yellow = lambda x: colorize.make(x).yellow  # noqa
-blue = lambda x: colorize.make(x).blue  # noqa
-green = lambda x: colorize.make(x).green  # noqa
-red = lambda x: colorize.make(x).red  # noqa
-bold = lambda x: colorize.make(x).bold  # noqa
+yellow = Colorizer('\033[33m')
+blue = Colorizer('\033[34m')
+green = Colorizer('\033[92m')
+red = Colorizer('\033[91m')
+bold = Colorizer('\033[1m')
 red_arrow = red('--> ')
 blue_arrow = blue('--> ')
 green_arrow = green('--> ')
