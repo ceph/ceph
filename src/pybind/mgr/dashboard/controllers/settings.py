samchoraria/ceph-ci

mgr/dashboard/backend: API to manage dashboard settings
Enables to change (set/unset) values of settings of the dashboard using
the REST API.

Fixes: https://tracker.ceph.com/issues/24273

Signed-off-by: Patrick Nawracay <pnawracay@suse.com>

src/pybind/mgr/dashboard/controllers/settings.py
@@ -1,0 +1,69 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from contextlib import contextmanager
+
+import cherrypy
+
+
+from . import ApiController, RESTController
+from ..settings import Settings, Options
+from ..security import Scope
+
+
+@ApiController('/settings', Scope.CONFIG_OPT)
+class SettingsController(RESTController):
+    """
+    Enables to manage the settings of the dashboard (not the Ceph cluster).
+    """
+    @contextmanager
+    def _attribute_handler(self, name):
+        """
+        :type name: str|dict[str, str]
+        :rtype: str|dict[str, str]
+        """
+        if isinstance(name, dict):
+            result = {self._to_native(key): value
+                      for key, value in name.items()}
+        else:
+            result = self._to_native(name)
+
+        try:
+            yield result
+        except AttributeError:
+            raise cherrypy.NotFound(result)
+
+    @staticmethod
+    def _to_native(setting):
+        return setting.upper().replace('-', '_')
+
+    def list(self):
+        return [
+            self._get(name) for name in Options.__dict__
+            if name.isupper() and not name.startswith('_')
+        ]
+
+    def _get(self, name):
+        with self._attribute_handler(name) as sname:
+            default, data_type = getattr(Options, sname)
+        return {
+            'name': sname,
+            'default': default,
+            'type': data_type.__name__,
+            'value': getattr(Settings, sname)
+        }
+
+    def get(self, name):
+        return self._get(name)
+
+    def set(self, name, value):
+        with self._attribute_handler(name) as sname:
+            setattr(Settings, self._to_native(sname), value)
+
+    def delete(self, name):
+        with self._attribute_handler(name) as sname:
+            delattr(Settings, self._to_native(sname))
+
+    def bulk_set(self, **kwargs):
+        with self._attribute_handler(kwargs) as data:
+            for name, value in data.items():
+                setattr(Settings, self._to_native(name), value)

ceph/ceph

(cherry picked from commit 7340e70d7f77907a23a51ab8177ea40a4f921928)
Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

src/pybind/mgr/dashboard/controllers/settings.py
@@ -1,0 +1,69 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from contextlib import contextmanager
+
+import cherrypy
+
+
+from . import ApiController, RESTController
+from ..settings import Settings as SettingsModule, Options
+from ..security import Scope
+
+
+@ApiController('/settings', Scope.CONFIG_OPT)
+class Settings(RESTController):
+    """
+    Enables to manage the settings of the dashboard (not the Ceph cluster).
+    """
+    @contextmanager
+    def _attribute_handler(self, name):
+        """
+        :type name: str|dict[str, str]
+        :rtype: str|dict[str, str]
+        """
+        if isinstance(name, dict):
+            result = {self._to_native(key): value
+                      for key, value in name.items()}
+        else:
+            result = self._to_native(name)
+
+        try:
+            yield result
+        except AttributeError:
+            raise cherrypy.NotFound(result)
+
+    @staticmethod
+    def _to_native(setting):
+        return setting.upper().replace('-', '_')
+
+    def list(self):
+        return [
+            self._get(name) for name in Options.__dict__
+            if name.isupper() and not name.startswith('_')
+        ]
+
+    def _get(self, name):
+        with self._attribute_handler(name) as sname:
+            default, data_type = getattr(Options, sname)
+        return {
+            'name': sname,
+            'default': default,
+            'type': data_type.__name__,
+            'value': getattr(SettingsModule, sname)
+        }
+
+    def get(self, name):
+        return self._get(name)
+
+    def set(self, name, value):
+        with self._attribute_handler(name) as sname:
+            setattr(SettingsModule, self._to_native(sname), value)
+
+    def delete(self, name):
+        with self._attribute_handler(name) as sname:
+            delattr(SettingsModule, self._to_native(sname))
+
+    def bulk_set(self, **kwargs):
+        with self._attribute_handler(kwargs) as data:
+            for name, value in data.items():
+                setattr(SettingsModule, self._to_native(name), value)