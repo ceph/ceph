5a1d2f173372cb6a092be38108304978f728b3ce
Jianpeng Ma <jianpeng.ma@intel.com>
2018-10-29 23:12:14 +0800

    os/bluefs: set logfile w/ WRITE_LIFE_MEDIUM
    Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

--- src/os/bluestore/BlueFS.h
+++ src/os/bluestore/BlueFS.h
@@ -140,6 +140,9 @@ public:
       ++file->num_writers;
       iocv.fill(nullptr);
       dirty_devs.fill(false);
+      if (f->fnode.ino == 1) {
+	write_hint = WRITE_LIFE_MEDIUM;
+      }
     }
     // NOTE: caller must call BlueFS::close_writer()
     ~FileWriter() {
