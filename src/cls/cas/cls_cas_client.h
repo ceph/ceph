af55b21afea7e77828b233dc5855f8c10a9b72c8
myoungwon oh <omwmw@sk.com>
2018-11-03 17:13:41 +0900

    src/cls/cas: add has_chunk op
    Signed-off-by: Myoungwon Oh <omwmw@sk.com>

--- src/cls/cas/cls_cas_client.h
+++ src/cls/cas/cls_cas_client.h
@@ -13,4 +13,5 @@ void cls_chunk_refcount_get(librados::ObjectWriteOperation& op, const hobject_t&
 void cls_chunk_refcount_put(librados::ObjectWriteOperation& op, const hobject_t& soid);
 void cls_chunk_refcount_set(librados::ObjectWriteOperation& op, set<hobject_t>& refs);
 int cls_chunk_refcount_read(librados::IoCtx& io_ctx, string& oid, set<hobject_t> *refs);
+int cls_chunk_has_chunk(librados::IoCtx& io_ctx, string& oid, string& fp_oid);
 #endif
