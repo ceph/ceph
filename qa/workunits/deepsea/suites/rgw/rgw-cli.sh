46b171032c664515afcd377bdfd715c804285e69
Nathan Cutler <ncutler@suse.com>
2018-11-03 16:04:55 +0100

    qa/suites/deepsea: make RGW CLI test cases really pass --cli
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- qa/workunits/deepsea/suites/rgw/rgw-cli.sh
+++ qa/workunits/deepsea/suites/rgw/rgw-cli.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 WORKUNITS_DIR=$CEPH_BASE/qa/workunits
-sudo $WORKUNITS_DIR/deepsea/health-ok.sh --rgw
+sudo $WORKUNITS_DIR/deepsea/health-ok.sh --cli --rgw
--- qa/workunits/deepsea/suites/rgw/rgw-ssl-cli.sh
+++ qa/workunits/deepsea/suites/rgw/rgw-ssl-cli.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 WORKUNITS_DIR=$CEPH_BASE/qa/workunits
-sudo $WORKUNITS_DIR/deepsea/health-ok.sh --rgw --ssl
+sudo $WORKUNITS_DIR/deepsea/health-ok.sh --cli --rgw --ssl
