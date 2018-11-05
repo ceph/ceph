8087d9d4d71997dc33515cf153b2e0ae62d6e872
Venky Shankar <vshankar@redhat.com>
2018-11-05 00:49:44 -0500

    mds: generate random scrub tag when empty
    With this, scrub operations are tagged with a random
    uuid if tag is unspecified. This also helps to show
    in-progress scrub operations via "scrub status" command
    (introduced in later commits).
    
    Signed-off-by: Venky Shankar <vshankar@redhat.com>

--- src/mds/ScrubHeader.h
+++ src/mds/ScrubHeader.h
@@ -26,10 +26,10 @@ class CInode;
  */
 class ScrubHeader {
 public:
-  ScrubHeader(std::string_view tag_, bool force_, bool recursive_,
-              bool repair_, Formatter *f_)
-      : tag(tag_), force(force_), recursive(recursive_), repair(repair_),
-        formatter(f_), origin(nullptr)
+  ScrubHeader(std::string_view tag_, bool is_tag_internal_, bool force_,
+              bool recursive_, bool repair_, Formatter *f_)
+    : tag(tag_), is_tag_internal(is_tag_internal_), force(force_),
+      recursive(recursive_), repair(repair_), formatter(f_), origin(nullptr)
   {
     ceph_assert(formatter != nullptr);
   }
@@ -41,6 +41,7 @@ public:
   bool get_recursive() const { return recursive; }
   bool get_repair() const { return repair; }
   bool get_force() const { return force; }
+  bool is_internal_tag() const { return is_tag_internal; }
   CInode *get_origin() const { return origin; }
   std::string_view get_tag() const { return tag; }
   Formatter &get_formatter() const { return *formatter; }
@@ -50,6 +51,7 @@ public:
 
 protected:
   const std::string tag;
+  bool is_tag_internal;
   const bool force;
   const bool recursive;
   const bool repair;
