8f10df8ba17cfe04cc64d67c8a83fb1d1af502ba
Andrew Schoen <aschoen@redhat.com>
2018-11-13 09:35:21 -0600

    ceph-volume: add PTTYPE to blkid parser
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>
    
    Resolves: rm#27062

--- src/ceph-volume/ceph_volume/util/disk.py
+++ src/ceph-volume/ceph_volume/util/disk.py
@@ -51,6 +51,7 @@ def _blkid_parser(output):
         'TYPE': 'TYPE',
         'PART_ENTRY_NAME': 'PARTLABEL',
         'PART_ENTRY_UUID': 'PARTUUID',
+        'PTTYPE': 'PTTYPE',
     }
 
     for pair in pairs:
