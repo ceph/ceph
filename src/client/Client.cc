017a6936de49d9e64a6d9150bd3cf0df0b7c2991
Tang Junhui <tangjunhui@sangfor.com>
2018-11-15 15:16:44 +0800

    client: fix failure in quota size limitation when using samba
    In samba, Client::_write may be called with the offset a
    negative number, and use the f->pos as the real write offset,
    in such condition, quota size can not limite the writes
    enven it exceeds the size limitation.
    
    Signed-off-by: Junhui Tang <tangjunhui@sangfor.com.cn>
    Fixes: http://tracker.ceph.com/issues/37547
    (cherry picked from commit c14ca19ed42cde552ec8d1f933aa209e891040e8)

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -9196,13 +9196,6 @@ int64_t Client::_write(Fh *f, int64_t offset, uint64_t size, const char *buf,
   if ((f->mode & CEPH_FILE_MODE_WR) == 0)
     return -EBADF;
 
-  // check quota
-  uint64_t endoff = offset + size;
-  if (endoff > in->size && is_quota_bytes_exceeded(in, endoff - in->size,
-						   f->actor_perms)) {
-    return -EDQUOT;
-  }
-
   // use/adjust fd pos?
   if (offset < 0) {
     lock_fh_pos(f);
@@ -9220,6 +9213,13 @@ int64_t Client::_write(Fh *f, int64_t offset, uint64_t size, const char *buf,
     offset = f->pos;
     f->pos = offset+size;
     unlock_fh_pos(f);
+  }
+
+  // check quota
+  uint64_t endoff = offset + size;
+  if (endoff > in->size && is_quota_bytes_exceeded(in, endoff - in->size,
+						   f->actor_perms)) {
+    return -EDQUOT;
   }
 
   //bool lazy = f->mode == CEPH_FILE_MODE_LAZY;
