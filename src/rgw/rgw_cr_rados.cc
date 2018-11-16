ceph/ceph

commit 05583ebff36780d7d3d5ba4dc807a01fc84d5c60

rgw: sysobj_core: filter sysobj attrs
Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

src/rgw/services/svc_sys_obj_core.cc
@@ -203,8 +203,10 @@ int RGWSI_SysObj_Core::read(RGWSysObjectCtxBase& obj_ctx,
   ldout(cct, 20) << "rados->read ofs=" << ofs << " len=" << len << dendl;
   op.read(ofs, len, bl, nullptr);
 
+  map<string, bufferlist> unfiltered_attrset;
+
   if (attrs) {
-    op.getxattrs(attrs, nullptr);
+    op.getxattrs(&unfiltered_attrset, nullptr);
   }
 
   RGWSI_RADOS::Obj rados_obj;
@@ -226,6 +228,10 @@ int RGWSI_SysObj_Core::read(RGWSysObjectCtxBase& obj_ctx,
       read_state.last_ver != op_ver) {
     ldout(cct, 5) << "raced with an object write, abort" << dendl;
     return -ECANCELED;
+  }
+
+  if (attrs) {
+    rgw_filter_attrset(unfiltered_attrset, RGW_ATTR_PREFIX, attrs);
   }
 
   read_state.last_ver = op_ver;

commit 8cfa4fe5ceecf7add1783869fb11a816491c1462

rgw: change the way sysobj filters raw attributes, fix bucket sync state xattrs
Fixes: http://tracker.ceph.com/issues/37281

This is a regression, introduced in 05583ebff36780d7d3d5ba4dc807a01fc84d5c60.
The problem was that the original change fixed was that we didn't filter
out sys object attributes that needed to be filtered (e.g., ver objclass
private attributes that were set on the object). We shouldn't know about
these attributes. RGW mostly prefixes all its attributes with "user.rgw."
so in most cases just filtering out everything that doesn't start with
that is fine. However, prior to refactoring, we had multiple ways to read
sys object, and apparently there was at least one case where we didn't
prefix the attrs correctly (bucket sync state).

This commit adds the option to read all of sys object's xattrs, and uses
that in the case where we needed to do it. However, it also modifies the
bucket sync state xattrs to be prefixed (in a backward compatible way).

Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

src/rgw/rgw_cr_rados.cc
@@ -98,14 +98,15 @@ int RGWAsyncGetSystemObj::_send_request()
   return sysobj.rop()
                .set_objv_tracker(&objv_tracker)
                .set_attrs(pattrs)
+	       .set_raw_attrs(raw_attrs)
                .read(&bl);
 }
 
 RGWAsyncGetSystemObj::RGWAsyncGetSystemObj(RGWCoroutine *caller, RGWAioCompletionNotifier *cn, RGWSI_SysObj *_svc,
                        RGWObjVersionTracker *_objv_tracker, const rgw_raw_obj& _obj,
-                       bool want_attrs)
+                       bool want_attrs, bool raw_attrs)
   : RGWAsyncRadosRequest(caller, cn), obj_ctx(_svc),
-    obj(_obj), want_attrs(want_attrs)
+    obj(_obj), want_attrs(want_attrs), raw_attrs(raw_attrs)
 {
   if (_objv_tracker) {
     objv_tracker = *_objv_tracker;
@@ -115,7 +116,7 @@ RGWAsyncGetSystemObj::RGWAsyncGetSystemObj(RGWCoroutine *caller, RGWAioCompletio
 int RGWSimpleRadosReadAttrsCR::send_request()
 {
   req = new RGWAsyncGetSystemObj(this, stack->create_completion_notifier(),
-			         svc, nullptr, obj, true);
+			         svc, nullptr, obj, true, raw_attrs);
   async_rados->queue(req);
   return 0;
 }

src/rgw/rgw_cr_rados.h
@@ -107,12 +107,13 @@ class RGWAsyncGetSystemObj : public RGWAsyncRadosRequest {
   RGWObjVersionTracker objv_tracker;
   rgw_raw_obj obj;
   const bool want_attrs;
+  const bool raw_attrs;
 protected:
   int _send_request() override;
 public:
   RGWAsyncGetSystemObj(RGWCoroutine *caller, RGWAioCompletionNotifier *cn, RGWSI_SysObj *_svc,
                        RGWObjVersionTracker *_objv_tracker, const rgw_raw_obj& _obj,
-                       bool want_attrs);
+                       bool want_attrs, bool raw_attrs);
 
   bufferlist bl;
   map<string, bufferlist> attrs;
@@ -221,7 +222,7 @@ template <class T>
 int RGWSimpleRadosReadCR<T>::send_request()
 {
   req = new RGWAsyncGetSystemObj(this, stack->create_completion_notifier(), svc,
-			         objv_tracker, obj, false);
+			         objv_tracker, obj, false, false);
   async_rados->queue(req);
   return 0;
 }
@@ -262,15 +263,17 @@ class RGWSimpleRadosReadAttrsCR : public RGWSimpleCoroutine {
 
   rgw_raw_obj obj;
   map<string, bufferlist> *pattrs;
+  bool raw_attrs;
   RGWAsyncGetSystemObj *req;
 
 public:
   RGWSimpleRadosReadAttrsCR(RGWAsyncRadosProcessor *_async_rados, RGWSI_SysObj *_svc,
 		      const rgw_raw_obj& _obj,
-		      map<string, bufferlist> *_pattrs) : RGWSimpleCoroutine(_svc->ctx()),
+		      map<string, bufferlist> *_pattrs, bool _raw_attrs) : RGWSimpleCoroutine(_svc->ctx()),
                                                 async_rados(_async_rados), svc(_svc),
 						obj(_obj),
                                                 pattrs(_pattrs),
+						raw_attrs(_raw_attrs),
                                                 req(NULL) {}
   ~RGWSimpleRadosReadAttrsCR() override {
     request_cleanup();

src/rgw/rgw_data_sync.cc
@@ -1942,13 +1942,15 @@ RGWCoroutine *RGWRemoteBucketLog::init_sync_status_cr()
   return new RGWInitBucketShardSyncStatusCoroutine(&sync_env, bs, init_status);
 }
 
+#define BUCKET_SYNC_ATTR_PREFIX RGW_ATTR_PREFIX "bucket-sync."
+
 template <class T>
-static void decode_attr(CephContext *cct, map<string, bufferlist>& attrs, const string& attr_name, T *val)
+static bool decode_attr(CephContext *cct, map<string, bufferlist>& attrs, const string& attr_name, T *val)
 {
   map<string, bufferlist>::iterator iter = attrs.find(attr_name);
   if (iter == attrs.end()) {
     *val = T();
-    return;
+    return false;
   }
 
   auto biter = iter->second.cbegin();
@@ -1956,14 +1958,22 @@ static void decode_attr(CephContext *cct, map<string, bufferlist>& attrs, const 
     decode(*val, biter);
   } catch (buffer::error& err) {
     ldout(cct, 0) << "ERROR: failed to decode attribute: " << attr_name << dendl;
+    return false;
   }
+  return true;
 }
 
 void rgw_bucket_shard_sync_info::decode_from_attrs(CephContext *cct, map<string, bufferlist>& attrs)
 {
-  decode_attr(cct, attrs, "state", &state);
-  decode_attr(cct, attrs, "full_marker", &full_marker);
-  decode_attr(cct, attrs, "inc_marker", &inc_marker);
+  if (!decode_attr(cct, attrs, BUCKET_SYNC_ATTR_PREFIX "state", &state)) {
+    decode_attr(cct, attrs, "state", &state);
+  }
+  if (!decode_attr(cct, attrs, BUCKET_SYNC_ATTR_PREFIX "full_marker", &full_marker)) {
+    decode_attr(cct, attrs, "full_marker", &full_marker);
+  }
+  if (!decode_attr(cct, attrs, BUCKET_SYNC_ATTR_PREFIX "inc_marker", &inc_marker)) {
+    decode_attr(cct, attrs, "inc_marker", &inc_marker);
+  }
 }
 
 void rgw_bucket_shard_sync_info::encode_all_attrs(map<string, bufferlist>& attrs)
@@ -1976,19 +1986,19 @@ void rgw_bucket_shard_sync_info::encode_all_attrs(map<string, bufferlist>& attrs
 void rgw_bucket_shard_sync_info::encode_state_attr(map<string, bufferlist>& attrs)
 {
   using ceph::encode;
-  encode(state, attrs["state"]);
+  encode(state, attrs[BUCKET_SYNC_ATTR_PREFIX "state"]);
 }
 
 void rgw_bucket_shard_full_sync_marker::encode_attr(map<string, bufferlist>& attrs)
 {
   using ceph::encode;
-  encode(*this, attrs["full_marker"]);
+  encode(*this, attrs[BUCKET_SYNC_ATTR_PREFIX "full_marker"]);
 }
 
 void rgw_bucket_shard_inc_sync_marker::encode_attr(map<string, bufferlist>& attrs)
 {
   using ceph::encode;
-  encode(*this, attrs["inc_marker"]);
+  encode(*this, attrs[BUCKET_SYNC_ATTR_PREFIX "inc_marker"]);
 }
 
 class RGWReadBucketSyncStatusCoroutine : public RGWCoroutine {
@@ -2011,8 +2021,8 @@ int RGWReadBucketSyncStatusCoroutine::operate()
 {
   reenter(this) {
     yield call(new RGWSimpleRadosReadAttrsCR(sync_env->async_rados, sync_env->store->svc.sysobj,
-                                                   rgw_raw_obj(sync_env->store->svc.zone->get_zone_params().log_pool, oid),
-                                                   &attrs));
+                                             rgw_raw_obj(sync_env->store->svc.zone->get_zone_params().log_pool, oid),
+                                             &attrs, true));
     if (retcode == -ENOENT) {
       *status = rgw_bucket_shard_sync_info();
       return set_cr_done();

src/rgw/services/svc_sys_obj.cc
@@ -27,7 +27,8 @@ int RGWSI_SysObj::Obj::ROp::stat()
   RGWSI_SysObj_Core *svc = source.core_svc;
   rgw_raw_obj& obj = source.obj;
 
-  return svc->stat(source.get_ctx(), state, obj, attrs,
+  return svc->stat(source.get_ctx(), state, obj,
+		   attrs, raw_attrs,
                    lastmod, obj_size,
                    objv_tracker);
 }
@@ -41,6 +42,7 @@ int RGWSI_SysObj::Obj::ROp::read(int64_t ofs, int64_t end, bufferlist *bl)
                    objv_tracker,
                    obj, bl, ofs, end,
                    attrs,
+		   raw_attrs,
                    cache_info,
                    refresh_version);
 }

src/rgw/services/svc_sys_obj.h
@@ -50,6 +50,7 @@ public:
       
       RGWObjVersionTracker *objv_tracker{nullptr};
       map<string, bufferlist> *attrs{nullptr};
+      bool raw_attrs{false};
       boost::optional<obj_version> refresh_version{boost::none};
       ceph::real_time *lastmod{nullptr};
       uint64_t *obj_size{nullptr};
@@ -73,6 +74,11 @@ public:
       ROp& set_attrs(map<string, bufferlist> *_attrs) {
         attrs = _attrs;
         return *this;
+      }
+
+      ROp& set_raw_attrs(bool ra) {
+	raw_attrs = ra;
+	return *this;
       }
 
       ROp& set_refresh_version(boost::optional<obj_version>& rf) {

src/rgw/services/svc_sys_obj_cache.cc
@@ -3,6 +3,7 @@
 #include "svc_notify.h"
 
 #include "rgw/rgw_zone.h"
+#include "rgw/rgw_tools.h"
 
 #define dout_subsys ceph_subsys_rgw
 
@@ -90,6 +91,7 @@ int RGWSI_SysObj_Cache::read(RGWSysObjectCtxBase& obj_ctx,
                              const rgw_raw_obj& obj,
                              bufferlist *obl, off_t ofs, off_t end,
                              map<string, bufferlist> *attrs,
+			     bool raw_attrs,
                              rgw_cache_entry_info *cache_info,
                              boost::optional<obj_version> refresh_version)
 {
@@ -97,7 +99,7 @@ int RGWSI_SysObj_Cache::read(RGWSysObjectCtxBase& obj_ctx,
   string oid;
   if (ofs != 0) {
     return RGWSI_SysObj_Core::read(obj_ctx, read_state, objv_tracker,
-                          obj, obl, ofs, end, attrs,
+                          obj, obl, ofs, end, attrs, true,
                           cache_info, refresh_version);
   }
 
@@ -126,13 +128,22 @@ int RGWSI_SysObj_Cache::read(RGWSysObjectCtxBase& obj_ctx,
     i.copy_all(*obl);
     if (objv_tracker)
       objv_tracker->read_version = info.version;
-    if (attrs)
-      *attrs = info.xattrs;
+    if (attrs) {
+      if (raw_attrs) {
+	*attrs = info.xattrs;
+      } else {
+	rgw_filter_attrset(info.xattrs, RGW_ATTR_PREFIX, attrs);
+      }
+    }
     return obl->length();
   }
+
+  map<string, bufferlist> unfiltered_attrset;
   int r = RGWSI_SysObj_Core::read(obj_ctx, read_state, objv_tracker,
                          obj, obl, ofs, end,
-                         attrs, cache_info,
+			 (attrs ? &unfiltered_attrset : nullptr),
+			 true, /* cache unfiltered attrs */
+			 cache_info,
                          refresh_version);
   if (r < 0) {
     if (r == -ENOENT) { // only update ENOENT, we'd rather retry other errors
@@ -158,7 +169,12 @@ int RGWSI_SysObj_Cache::read(RGWSysObjectCtxBase& obj_ctx,
     info.version = objv_tracker->read_version;
   }
   if (attrs) {
-    info.xattrs = *attrs;
+    info.xattrs = std::move(unfiltered_attrset);
+    if (raw_attrs) {
+      *attrs = info.xattrs;
+    } else {
+      rgw_filter_attrset(info.xattrs, RGW_ATTR_PREFIX, attrs);
+    }
   }
   cache.put(name, info, cache_info);
   return r;

src/rgw/services/svc_sys_obj_cache.h
@@ -43,6 +43,7 @@ protected:
            const rgw_raw_obj& obj,
            bufferlist *bl, off_t ofs, off_t end,
            map<string, bufferlist> *attrs,
+	   bool raw_attrs,
            rgw_cache_entry_info *cache_info,
            boost::optional<obj_version>) override;
 

src/rgw/services/svc_sys_obj_core.cc
@@ -105,7 +105,6 @@ int RGWSI_SysObj_Core::raw_stat(const rgw_raw_obj& obj, uint64_t *psize, real_ti
     return r;
   }
 
-  map<string, bufferlist> unfiltered_attrset;
   uint64_t size = 0;
   struct timespec mtime_ts;
 
@@ -113,9 +112,7 @@ int RGWSI_SysObj_Core::raw_stat(const rgw_raw_obj& obj, uint64_t *psize, real_ti
   if (objv_tracker) {
     objv_tracker->prepare_op_for_read(&op);
   }
-  if (attrs) {
-    op.getxattrs(&unfiltered_attrset, nullptr);
-  }
+  op.getxattrs(attrs, nullptr);
   if (psize || pmtime) {
     op.stat2(&size, &mtime_ts, nullptr);
   }
@@ -136,9 +133,6 @@ int RGWSI_SysObj_Core::raw_stat(const rgw_raw_obj& obj, uint64_t *psize, real_ti
     *psize = size;
   if (pmtime)
     *pmtime = ceph::real_clock::from_timespec(mtime_ts);
-  if (attrs) {
-    rgw_filter_attrset(unfiltered_attrset, RGW_ATTR_PREFIX, attrs);
-  }
 
   return 0;
 }
@@ -147,6 +141,7 @@ int RGWSI_SysObj_Core::stat(RGWSysObjectCtxBase& obj_ctx,
                             GetObjState& state,
                             const rgw_raw_obj& obj,
                             map<string, bufferlist> *attrs,
+			    bool raw_attrs,
                             real_time *lastmod,
                             uint64_t *obj_size,
                             RGWObjVersionTracker *objv_tracker)
@@ -162,7 +157,11 @@ int RGWSI_SysObj_Core::stat(RGWSysObjectCtxBase& obj_ctx,
   }
 
   if (attrs) {
-    *attrs = astate->attrset;
+    if (raw_attrs) {
+      *attrs = astate->attrset;
+    } else {
+      rgw_filter_attrset(astate->attrset, RGW_ATTR_PREFIX, attrs);
+    }
     if (cct->_conf->subsys.should_gather<ceph_subsys_rgw, 20>()) {
       map<string, bufferlist>::iterator iter;
       for (iter = attrs->begin(); iter != attrs->end(); ++iter) {
@@ -185,6 +184,7 @@ int RGWSI_SysObj_Core::read(RGWSysObjectCtxBase& obj_ctx,
                             const rgw_raw_obj& obj,
                             bufferlist *bl, off_t ofs, off_t end,
                             map<string, bufferlist> *attrs,
+			    bool raw_attrs,
                             rgw_cache_entry_info *cache_info,
                             boost::optional<obj_version>)
 {
@@ -231,7 +231,11 @@ int RGWSI_SysObj_Core::read(RGWSysObjectCtxBase& obj_ctx,
   }
 
   if (attrs) {
-    rgw_filter_attrset(unfiltered_attrset, RGW_ATTR_PREFIX, attrs);
+    if (raw_attrs) {
+      attrs->swap(unfiltered_attrset);
+    } else {
+      rgw_filter_attrset(unfiltered_attrset, RGW_ATTR_PREFIX, attrs);
+    }
   }
 
   read_state.last_ver = op_ver;

src/rgw/services/svc_sys_obj_core.h
@@ -134,6 +134,7 @@ protected:
                    const rgw_raw_obj& obj,
                    bufferlist *bl, off_t ofs, off_t end,
                    map<string, bufferlist> *attrs,
+		   bool raw_attrs,
                    rgw_cache_entry_info *cache_info,
                    boost::optional<obj_version>);
 
@@ -184,6 +185,7 @@ protected:
            GetObjState& state,
            const rgw_raw_obj& obj,
            map<string, bufferlist> *attrs,
+	   bool raw_attrs,
            real_time *lastmod,
            uint64_t *obj_size,
            RGWObjVersionTracker *objv_tracker);