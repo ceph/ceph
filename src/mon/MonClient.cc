65c9c1f4bdaf5f36781a69b1fe776c6cfdb0c68e
Sage Weil <sage@redhat.com>
2018-11-17 06:50:17 -0600

    mon/MonClient: add callback for (any) config change
    This is a coarse "there was some config update".  It's fired whether or
    not it values are observed.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mon/MonClient.cc
+++ src/mon/MonClient.cc
@@ -374,6 +374,9 @@ void MonClient::handle_config(MConfig *m)
   ldout(cct,10) << __func__ << " " << *m << dendl;
   finisher.queue(new FunctionContext([this, m](int r) {
 	cct->_conf.set_mon_vals(cct, m->config, config_cb);
+	if (config_notify_cb) {
+	  config_notify_cb();
+	}
 	m->put();
       }));
   got_config = true;
