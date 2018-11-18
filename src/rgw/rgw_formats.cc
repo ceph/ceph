50cecccf9bcad19692fb41dccd0de7632dd3bf71
Jesse Williamson <jwilliamson@suse.de>
2018-11-17 21:37:15 -0800

    rgw: squish VLAs in rgw_formats
    Signed-off-by: Jesse Williamson <jwilliamson@suse.de>

--- src/rgw/rgw_formats.cc
+++ src/rgw/rgw_formats.cc
@@ -184,9 +184,9 @@ void RGWFormatter_Plain::write_raw_data(const char *data)
 
 void RGWFormatter_Plain::write_data(const char *fmt, ...)
 {
-#define LARGE_ENOUGH_LEN 128
+  constexpr int LARGE_ENOUGH_LEN = 128;
   int n, size = LARGE_ENOUGH_LEN;
-  char s[size + 8];
+  char s[LARGE_ENOUGH_LEN + 8];
   char *p, *np;
   bool p_on_stack;
   va_list ap;
