d8b094c81c6d9bdca39b96336a00ce1e8391e3c8
Mykola Golub <mgolub@suse.com>
2018-11-25 12:35:15 +0200

    mgr/crash: add process name to 'ceph crash ls' output
    Signed-off-by: Mykola Golub <mgolub@suse.com>

--- src/pybind/mgr/crash/module.py
+++ src/pybind/mgr/crash/module.py
@@ -79,8 +79,11 @@ class Module(MgrModule):
 
     def do_ls(self, cmd, inbuf):
         keys = []
-        for key in six.iterkeys(self.get_store_prefix('crash/')):
-            keys.append(key.replace('crash/', ''))
+        for k, meta in self.timestamp_filter(lambda ts: True):
+            process_name = meta.get('process_name', 'unknown')
+            if not process_name:
+                process_name = 'unknown'
+            keys.append("%s %s" % (k.replace('crash/', ''), process_name))
         keys.sort()
         return 0, '\n'.join(keys), ''
 
