039995d9ad2ee95b39dc317b6ebf0d50209fcc1f
Enming.Zhang <zvampirem77@gmail.com>
2018-11-27 06:41:56 -0500

    rgw: cleanup for RGWDeleteObj::verify_permission()
    - if the 'Effect' field of user policy and bucket policy
      is set to 'Allow', RGWDeleteObj::verify_permission()
      should return 0 instead of true;
    
    - as the same with bucket policy, if the 'Effect' field
      of user policy is set to 'Deny', RGWDeleteObj::verify_permission()
      should return -EACCES instead of false;
    
    Signed-off-by: Enming.Zhang <zvampirem77@gmail.com>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -4450,7 +4450,7 @@ int RGWDeleteObj::verify_permission()
                                               rgw::IAM::s3DeleteObjectVersion,
                                               ARN(s->bucket, s->object.name));
     if (usr_policy_res == Effect::Deny) {
-      return false;
+      return -EACCES;
     }
 
     rgw::IAM::Effect r = Effect::Pass;
@@ -4462,11 +4462,11 @@ int RGWDeleteObj::verify_permission()
 				 ARN(s->bucket, s->object.name));
     }
     if (r == Effect::Allow)
-      return true;
+      return 0;
     else if (r == Effect::Deny)
       return -EACCES;
     else if (usr_policy_res == Effect::Allow)
-      return true;
+      return 0;
   }
 
   if (!verify_bucket_permission_no_policy(this, s, RGW_PERM_WRITE)) {
