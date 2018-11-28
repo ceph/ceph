dfff1ed366a7a5f67869b08bdd126dadb138a15a
J. Eric Ivancich <ivancich@redhat.com>
2018-11-28 12:55:35 -0500

    rgw: fix typo
    This fixes a typo in a log message. It's a separate commit so
    downstream commits point to the right upstream commits via
    cherry-pick.
    
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>
    (cherry picked from commit 7d1768f2a332c5ac389cbb4bbeda28752e4f17bc)

--- src/rgw/rgw_reshard.cc
+++ src/rgw/rgw_reshard.cc
@@ -723,7 +723,7 @@ int RGWBucketReshard::execute(int num_shards, int max_op_entries,
     lderr(store->ctx()) << "Error: " << __func__ <<
       " failed to clean old bucket info object \"" <<
       bucket_info.bucket.get_key() <<
-      "\"created after successufl resharding with error " << ret << dendl;
+      "\"created after successful resharding with error " << ret << dendl;
   }
 
   ldout(store->ctx(), 1) << __func__ <<
