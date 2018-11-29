3d49b73cbb06621256bbc7830836411e565e48ca
Joao Eduardo Luis <joao@suse.de>
2018-11-29 01:05:31 +0000

    rgw: fix issues with 'enforce bounds' patch
    The patch to enforce bounds on max-keys/max-uploads/max-parts had a few
    issues that would prevent us from compiling it. Instead of changing the
    code provided by the submitter, we're addressing them in a separate
    commit to maintain the DCO.
    
    Signed-off-by: Joao Eduardo Luis <joao@suse.de>
    (cherry picked from commit 29bc434a6a81a2e5c5b8cfc4c8d5c82ca5bf538a)

--- src/rgw/rgw_op.h
+++ src/rgw/rgw_op.h
@@ -2214,11 +2214,16 @@ public:
   virtual const string name() { return "get_cluster_stat"; }
 };
 
-static inline int parse_value_and_bound(const string &input, long *output, const long lower_bound, const long upper_bound, const long default_val)
+static inline int parse_value_and_bound(
+    const string &input,
+    int &output,
+    const long lower_bound,
+    const long upper_bound,
+    const long default_val)
 {
   if (!input.empty()) {
     char *endptr;
-    *output = strtol(input.c_str(), &endptr, 10);
+    output = strtol(input.c_str(), &endptr, 10);
     if (endptr) {
       if (endptr == input.c_str()) return -EINVAL;
       while (*endptr && isspace(*endptr)) // ignore white space
@@ -2227,14 +2232,14 @@ static inline int parse_value_and_bound(const string &input, long *output, const
         return -EINVAL;
       }
     }
-    if(*output > upper_bound) {
-      *output = upper_bound;
+    if(output > upper_bound) {
+      output = upper_bound;
     }
-    if(*output < lower_bound) {
-      *output = lower_bound;
+    if(output < lower_bound) {
+      output = lower_bound;
     }
   } else {
-    *output = default_val;
+    output = default_val;
   }
 
   return 0;
