f3d62d54d3c8f96c7f8d98c59ee1be299a691a67
Kanika Murarka <kmurarka@redhat.com>
2018-12-02 23:19:54 +0530

    mgr/dashboard: About modal proposed changes
    Fixes: http://tracker.ceph.com/issues/35693
    Signed-off-by: Kanika Murarka <kmurarka@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/core/navigation/about/about.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/navigation/about/about.component.ts
@@ -1,8 +1,13 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
+import { detect } from 'detect-browser';
 import { BsModalRef } from 'ngx-bootstrap/modal';
 import { Subscription } from 'rxjs';
-
+import { environment } from '../../../../environments/environment';
+import { UserService } from '../../../shared/api/user.service';
+import { AppConstants } from '../../../shared/constants/app.constants';
+import { Permission } from '../../../shared/models/permissions';
+import { AuthStorageService } from '../../../shared/services/auth-storage.service';
 import { SummaryService } from '../../../shared/services/summary.service';
 
 @Component({
@@ -11,19 +16,36 @@ import { SummaryService } from '../../../shared/services/summary.service';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit, OnDestroy {
+  modalVariables: any;
   versionNumber: string;
   versionHash: string;
   versionName: string;
   subs: Subscription;
+  userPermission: Permission;
+  projectConstants: typeof AppConstants;
+  hostAddr: string;
+  copyright: string;
 
-  constructor(public modalRef: BsModalRef, private summaryService: SummaryService) {}
+  constructor(
+    public modalRef: BsModalRef,
+    private summaryService: SummaryService,
+    private userService: UserService,
+    private authStorageService: AuthStorageService
+  ) {
+    this.userPermission = this.authStorageService.getPermissions().user;
+  }
 
   ngOnInit() {
+    this.copyright = 'Copyright(c) ' + environment.year + ' Ceph contributors.';
+    this.projectConstants = AppConstants;
+    this.hostAddr = window.location.hostname;
+    this.modalVariables = this.setVariables();
     this.subs = this.summaryService.subscribe((summary: any) => {
       if (!summary) {
         return;
       }
       const version = summary.version.replace('ceph version ', '').split(' ');
+      this.hostAddr = summary.mgr_host.replace(/(^\w+:|^)\/\//, '').replace(/\/$/, '');
       this.versionNumber = version[0];
       this.versionHash = version[1];
       this.versionName = version.slice(2, version.length).join(' ');
@@ -32,5 +54,21 @@ export class AboutComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subs.unsubscribe();
+  }
+
+  setVariables() {
+    const project = {} as any;
+    project.user = localStorage.getItem('dashboard_username');
+    project.role = 'user';
+    if (this.userPermission.read) {
+      this.userService.get(project.user).subscribe((data: any) => {
+        project.role = data.roles;
+      });
+    }
+    const browser = detect();
+    project.browserName = browser && browser.name ? browser.name : 'Not detected';
+    project.browserVersion = browser && browser.version ? browser.version : 'Not detected';
+    project.browserOS = browser && browser.os ? browser.os : 'Not detected';
+    return project;
   }
 }
--- src/pybind/mgr/dashboard/frontend/src/environments/environment.tpl.ts
+++ src/pybind/mgr/dashboard/frontend/src/environments/environment.tpl.ts
@@ -4,5 +4,6 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: '{PRODUCTION}',
+  year: '{COPYRIGHT_YEAR}'
 };
