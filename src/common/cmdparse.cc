ceph/ceph

commit 5a0f2340427527b32bc6eb1637de5010fa4ffd2d

ceph_argparse: introduce CephBool arguments
This replaces use of CephChoices for cases like
--yes-i-really-really-mean-it.

It's a relatively invasive change, because both
client and server need to handle backward compatibility:
 - Clients are easy, they just cope with the server's
   use of CephChoices/CephString with a few special case
   strings to recognise --yes-i-really-mean-it and similar
 - Servers are harder, they have to convert CephBool arguments
   into a similar CephChoices argument that older clients will
   understand.  This involves propagating feature flags into
   some code paths that couldn't see them before.

Signed-off-by: John Spray <john.spray@redhat.com>

src/common/cmdparse.cc
@@ -72,7 +72,7 @@ arg_desc_t cmddesc_get_args(const String& cmddesc)
  */
 
 void
-dump_cmd_to_json(Formatter *f, const string& cmd)
+dump_cmd_to_json(Formatter *f, uint64_t features, const string& cmd)
 {
   // put whole command signature in an already-opened container
   // elements are: "name", meaning "the typeless name that means a literal"
@@ -91,6 +91,23 @@ dump_cmd_to_json(Formatter *f, const string& cmd)
     auto desckv = cmddesc_get_args(word);
     // name the individual desc object based on the name key
     f->open_object_section(string(desckv["name"]).c_str());
+
+    // Compatibility for pre-nautilus clients that don't know about CephBool
+    if (!HAVE_FEATURE(features, SERVER_NAUTILUS)) {
+      auto i = desckv.find("type");
+      if (i != desckv.end() && i->second == "CephBool") {
+        // Instruct legacy clients to send --foo-bar string in place
+        // of a 'true'/'false' value
+        std::ostringstream oss;
+        oss << std::string("--") << desckv["name"];
+        std::string val = oss.str();
+        std::replace(val.begin(), val.end(), '_', '-');
+
+        desckv["type"] = "CephChoices";
+        desckv["strings"] = val;
+      }
+    }
+
     // dump all the keys including name into the array
     for (auto [key, value] : desckv) {
       f->dump_string(string(key).c_str(), string(value));
@@ -101,13 +118,14 @@ dump_cmd_to_json(Formatter *f, const string& cmd)
 
 void
 dump_cmd_and_help_to_json(Formatter *jf,
+			  uint64_t features,
 			  const string& secname,
 			  const string& cmdsig,
 			  const string& helptext)
 {
       jf->open_object_section(secname.c_str());
       jf->open_array_section("sig");
-      dump_cmd_to_json(jf, cmdsig);
+      dump_cmd_to_json(jf, features, cmdsig);
       jf->close_section(); // sig array
       jf->dump_string("help", helptext.c_str());
       jf->close_section(); // cmd
@@ -115,6 +133,7 @@ dump_cmd_and_help_to_json(Formatter *jf,
 
 void
 dump_cmddesc_to_json(Formatter *jf,
+		     uint64_t features,
 		     const string& secname,
 		     const string& cmdsig,
 		     const string& helptext,
@@ -124,7 +143,7 @@ dump_cmddesc_to_json(Formatter *jf,
 {
       jf->open_object_section(secname.c_str());
       jf->open_array_section("sig");
-      dump_cmd_to_json(jf, cmdsig);
+      dump_cmd_to_json(jf, features, cmdsig);
       jf->close_section(); // sig array
       jf->dump_string("help", helptext.c_str());
       jf->dump_string("module", module.c_str());
@@ -560,3 +579,37 @@ bool validate_cmd(CephContext* cct,
     }
   });
 }
+
+bool cmd_getval(CephContext *cct, const cmdmap_t& cmdmap,
+		const std::string& k, bool& val)
+{
+  /*
+   * Specialized getval for booleans.  CephBool didn't exist before Nautilus,
+   * so earlier clients are sent a CephChoices argdesc instead, and will
+   * send us a "--foo-bar" value string for boolean arguments.
+   */
+  if (cmdmap.count(k)) {
+    try {
+      val = boost::get<bool>(cmdmap.find(k)->second);
+      return true;
+    } catch (boost::bad_get&) {
+      try {
+        std::string expected = "--" + k;
+        std::replace(expected.begin(), expected.end(), '_', '-');
+
+        std::string v_str = boost::get<std::string>(cmdmap.find(k)->second);
+        if (v_str == expected) {
+          val = true;
+          return true;
+        } else {
+          throw bad_cmd_get(k, cmdmap);
+        }
+      } catch (boost::bad_get&) {
+        throw bad_cmd_get(k, cmdmap);
+      }
+    }
+  }
+  return false;
+}
+
+

src/common/admin_socket.cc
@@ -566,6 +566,7 @@ public:
       ostringstream secname;
       secname << "cmd" << setfill('0') << std::setw(3) << cmdnum;
       dump_cmd_and_help_to_json(&jf,
+                                CEPH_FEATURES_ALL,
 				secname.str().c_str(),
 				info.desc,
 				info.help);

src/common/cmdparse.h
@@ -23,12 +23,15 @@ typedef boost::variant<std::string,
 typedef std::map<std::string, cmd_vartype, std::less<>> cmdmap_t;
 
 std::string cmddesc_get_prefix(const std::string &cmddesc);
-void dump_cmd_to_json(ceph::Formatter *f, const std::string& cmd);
+void dump_cmd_to_json(ceph::Formatter *f, uint64_t features,
+                      const std::string& cmd);
 void dump_cmd_and_help_to_json(ceph::Formatter *f,
+			       uint64_t features,
 			       const std::string& secname,
 			       const std::string& cmd,
 			       const std::string& helptext);
 void dump_cmddesc_to_json(ceph::Formatter *jf,
+		          uint64_t features,
 		          const std::string& secname,
 		          const std::string& cmdsig,
 		          const std::string& helptext,
@@ -51,6 +54,9 @@ struct bad_cmd_get : public std::exception {
     return desc.c_str();
   }
 };
+
+bool cmd_getval(CephContext *cct, const cmdmap_t& cmdmap,
+		const std::string& k, bool& val);
 
 template <typename T>
 bool cmd_getval(CephContext *cct, const cmdmap_t& cmdmap,

src/mds/MDSDaemon.cc
@@ -727,7 +727,8 @@ int MDSDaemon::_handle_command(
     for (auto& c : get_commands()) {
       ostringstream secname;
       secname << "cmd" << setfill('0') << std::setw(3) << cmdnum;
-      dump_cmddesc_to_json(f.get(), secname.str(), c.cmdstring, c.helpstring,
+      dump_cmddesc_to_json(f.get(), m->get_connection()->get_features(),
+                           secname.str(), c.cmdstring, c.helpstring,
 			   c.module, "*", 0);
       cmdnum++;
     }

src/mgr/DaemonServer.cc
@@ -830,10 +830,11 @@ bool DaemonServer::_handle_command(
     JSONFormatter f;
     f.open_object_section("command_descriptions");
 
-    auto dump_cmd = [&cmdnum, &f](const MonCommand &mc){
+    auto dump_cmd = [&cmdnum, &f, m](const MonCommand &mc){
       ostringstream secname;
       secname << "cmd" << setfill('0') << std::setw(3) << cmdnum;
-      dump_cmddesc_to_json(&f, secname.str(), mc.cmdstring, mc.helpstring,
+      dump_cmddesc_to_json(&f, m->get_connection()->get_features(),
+                           secname.str(), mc.cmdstring, mc.helpstring,
                            mc.module, mc.req_perms, 0);
       cmdnum++;
     };

src/mon/MonCommands.h
@@ -738,7 +738,8 @@ COMMAND("osd pause", "pause osd", "osd", "rw")
 COMMAND("osd unpause", "unpause osd", "osd", "rw")
 COMMAND("osd erasure-code-profile set " \
 	"name=name,type=CephString,goodchars=[A-Za-z0-9-_.] " \
-	"name=profile,type=CephString,n=N,req=false", \
+	"name=profile,type=CephString,n=N,req=false " \
+	"name=force,type=CephBool,req=false", \
 	"create erasure code profile <name> with [<key[=value]> ...] pairs. Add a --force at the end to override an existing profile (VERY DANGEROUS)", \
 	"osd", "rw")
 COMMAND("osd erasure-code-profile get " \
@@ -942,16 +943,16 @@ COMMAND("osd pool create " \
 COMMAND_WITH_FLAG("osd pool delete " \
 	"name=pool,type=CephPoolname " \
 	"name=pool2,type=CephPoolname,req=false " \
-	"name=sure,type=CephChoices,strings=--yes-i-really-really-mean-it|" \
-        "--yes-i-really-really-mean-it-not-faking,req=false", \
+	"name=yes_i_really_really_mean_it,type=CephBool,req=false "
+	"name=yes_i_really_really_mean_it_not_faking,type=CephBool,req=false ", \
 	"delete pool", \
 	"osd", "rw", \
     FLAG(DEPRECATED))
 COMMAND("osd pool rm " \
 	"name=pool,type=CephPoolname " \
 	"name=pool2,type=CephPoolname,req=false " \
-	"name=sure,type=CephChoices,strings=--yes-i-really-really-mean-it|" \
-        "--yes-i-really-really-mean-it-not-faking,req=false", \
+	"name=yes_i_really_really_mean_it,type=CephBool,req=false "
+	"name=yes_i_really_really_mean_it_not_faking,type=CephBool,req=false ", \
 	"remove pool", \
 	"osd", "rw")
 COMMAND("osd pool rename " \

src/mon/Monitor.cc
@@ -2869,6 +2869,7 @@ bool Monitor::_allowed_command(MonSession *s, const string &module,
 
 void Monitor::format_command_descriptions(const std::vector<MonCommand> &commands,
 					  Formatter *f,
+					  uint64_t features,
 					  bufferlist *rdata)
 {
   int cmdnum = 0;
@@ -2877,7 +2878,7 @@ void Monitor::format_command_descriptions(const std::vector<MonCommand> &command
     unsigned flags = cmd.flags;
     ostringstream secname;
     secname << "cmd" << setfill('0') << std::setw(3) << cmdnum;
-    dump_cmddesc_to_json(f, secname.str(),
+    dump_cmddesc_to_json(f, features, secname.str(),
 			 cmd.cmdstring, cmd.helpstring, cmd.module,
 			 cmd.req_perms, flags);
     cmdnum++;
@@ -2979,7 +2980,8 @@ void Monitor::handle_command(MonOpRequestRef op)
       }
     }
 
-    format_command_descriptions(commands, f, &rdata);
+    auto features = m->get_connection()->get_features();
+    format_command_descriptions(commands, f, features, &rdata);
     delete f;
     reply_command(op, 0, "", rdata, 0);
     return;

src/mon/Monitor.h
@@ -963,6 +963,7 @@ private:
 public:
   static void format_command_descriptions(const std::vector<MonCommand> &commands,
 					  Formatter *f,
+					  uint64_t features,
 					  bufferlist *rdata);
 
   const std::vector<MonCommand> &get_local_commands(mon_feature_t f) {

commit f106104515e6eb6d83aba4b88e39491ee6494123

ceph_argparse: fix CephBool to CephChoices conversion
Commit 5a0f234 has introduced a regression that makes Ceph clients of
versions prior to Nautilus to receive invalid unicode characters in the
commands list JSON sent by the monitors.

The unicode characters are caused by memory corruption in a dangling
pointer stored by a std::string_view. The dangling pointer belonged to
an std::string object that was already destructed when the
std::string_view is used.
The fix in this PR makes the std::string object to live enough time for
the std::string_view to be used safely.

Signed-off-by: Ricardo Dias <rdias@suse.com>

src/common/cmdparse.cc
@@ -93,6 +93,7 @@ dump_cmd_to_json(Formatter *f, uint64_t features, const string& cmd)
     f->open_object_section(string(desckv["name"]).c_str());
 
     // Compatibility for pre-nautilus clients that don't know about CephBool
+    std::string val;
     if (!HAVE_FEATURE(features, SERVER_NAUTILUS)) {
       auto i = desckv.find("type");
       if (i != desckv.end() && i->second == "CephBool") {
@@ -100,7 +101,7 @@ dump_cmd_to_json(Formatter *f, uint64_t features, const string& cmd)
         // of a 'true'/'false' value
         std::ostringstream oss;
         oss << std::string("--") << desckv["name"];
-        std::string val = oss.str();
+        val = oss.str();
         std::replace(val.begin(), val.end(), '_', '-');
 
         desckv["type"] = "CephChoices";