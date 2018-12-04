a16917813561e5af6d519682e48f738c9ea2a113
Zhi Zhang <willzzhang@tencent.com>
2018-12-04 14:49:17 +0800

    mds: remove duplicated l_mdc_num_strays perfcounter set
    Fixes: http://tracker.ceph.com/issues/37516
    
    Signed-off-by: Zhi Zhang <zhangz.david@outlook.com>
    (cherry picked from commit 5901a116311549b67d584e0940a7d10fe57c24b5)

--- src/mds/StrayManager.cc
+++ src/mds/StrayManager.cc
@@ -229,8 +229,6 @@ void StrayManager::_purge_stray_purged(
 
     mds->mdlog->submit_entry(le, new C_PurgeStrayLogged(this, dn, pdv,
           mds->mdlog->get_current_segment()));
-
-    logger->set(l_mdc_num_strays, num_strays);
   }
 }
 
