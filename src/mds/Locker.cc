b67ab596a75df050e1ef9c5fe57d97b2696209c7
Yan, Zheng <zyan@redhat.com>
2018-12-10 11:37:32 +0800

    mds: optimize resuming stale caps
    If client doesn't want any cap, there is no need to re-issue stale
    caps.
    
    A special case is that client wants some caps, but skipped updating
    'wanted'. For this case, client needs to update 'wanted' when stale
    session get renewed.
    
    Fixes: http://tracker.ceph.com/issues/38043
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

--- src/mds/Locker.cc
+++ src/mds/Locker.cc
@@ -2170,8 +2170,13 @@ void Locker::resume_stale_caps(Session *session)
 {
   dout(10) << "resume_stale_caps for " << session->info.inst.name << dendl;
 
-  for (xlist<Capability*>::iterator p = session->caps.begin(); !p.end(); ++p) {
+  bool lazy = session->info.has_feature(CEPHFS_FEATURE_LAZY_CAP_WANTED);
+  for (xlist<Capability*>::iterator p = session->caps.begin(); !p.end(); ) {
     Capability *cap = *p;
+    ++p;
+    if (lazy && !cap->is_notable())
+      break; // see revoke_stale_caps()
+
     CInode *in = cap->get_inode();
     ceph_assert(in->is_head());
     dout(10) << " clearing stale flag on " << *in << dendl;
