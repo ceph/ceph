48c33e68d8e3d4568559020a9dd7c92efcaadab6
Ernesto Puerta <epuertat@redhat.com>
2018-12-21 19:23:56 +0100

    mgr/dashboard: feature-toggle: add HTTP headers
    Features toggles will be announced to the front-end by means of an HTTP
    header X-Dashboard-FeatureToggles. For this to work without performance
    impact, Feature Toggles, which are obtained from module Options, should
    be TTL-cached (5 secs seems a good trade off).
    
    Fixes: http://tracker.ceph.com/issues/37530
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/services/feature_toggle.py
+++ src/pybind/mgr/dashboard/services/feature_toggle.py
@@ -1,15 +1,30 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 import cherrypy
+import json
 
 from .. import logger
 from ..settings import Settings
 
 
 class FeatureToggleTool(cherrypy.Tool):
+    FEATURE_TOGGLES = [
+        'ENABLE_RBD_IMAGES',
+        'ENABLE_RBD_MIRRORING',
+        'ENABLE_RBD_ISCSI',
+        'ENABLE_CEPHFS',
+        'ENABLE_RGW'
+    ]
+
     def __init__(self):
         super(FeatureToggleTool, self).__init__(
             'before_handler', self.is_disabled, priority=10)
+
+    def _setup(self):
+        super(FeatureToggleTool, self)._setup()
+        cherrypy.request.hooks.attach('before_finalize',
+                                      self.announce_feature_toggles,
+                                      priority=10)
 
     def is_disabled(self):
         controller = cherrypy.request.handler.callable.__self__
@@ -20,3 +35,8 @@ class FeatureToggleTool(cherrypy.Tool):
                         type(controller).__name__, feature_enable_option)
             raise cherrypy.HTTPError(501, "Feature disabled by option '{}'".format(
                 feature_enable_option))
+
+    def announce_feature_toggles(self):
+        cherrypy.response.headers["X-Dashboard-FeatureToggles"] = {
+            feature_enable_option: getattr(Settings, feature_enable_option, True) for
+            feature_enable_option in self.FEATURE_TOGGLES}
