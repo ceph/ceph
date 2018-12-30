c92bba23b26fc3ea858de054beb7b774c2360056
Kefu Chai <kchai@redhat.com>
2018-12-30 21:57:04 +0800

    osd: unlock osd_lock when tweaking osd settings
    unlock osd_lock when serving "debug kick_recovery_wq" command
    
    we need to unlock osd_lock temporarily when updating the osd settings,
    otherwise we will run into assert failure. because
    OSD::handle_conf_change() acquires the osd_lock which is not a recursive
    lock.
    
    Fixes: http://tracker.ceph.com/issues/37761
    Signed-off-by: Kefu Chai <kchai@redhat.com>
    (cherry picked from commit 8e94b081506fa9fbdbea09113f1549772bb6ec04)
    
     Conflicts:
    	src/osd/OSD.cc

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -6583,6 +6583,7 @@ void OSD::do_command(Connection *con, ceph_tid_t tid, vector<string>& cmd, buffe
     cmd_getval(cct, cmdmap, "delay", delay);
     ostringstream oss;
     oss << delay;
+    unlock_guard unlock{osd_lock};
     r = cct->_conf->set_val("osd_recovery_delay_start", oss.str().c_str());
     if (r != 0) {
       ss << "kick_recovery_wq: error setting "
