202f537bb05db7e17676c0253ee68559b01ead2e
Yuval Lifshitz <yuvalif@yahoo.com>
2019-01-01 19:37:59 +0200

    rgw: pubsub add amqp unit-tests
    Signed-off-by: Yuval Lifshitz <yuvalif@yahoo.com>

---
+++ src/test/rgw/amqp_mock.h
@@ -0,0 +1,14 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+#pragma once
+
+#include <string>
+
+namespace amqp_mock {
+extern int VALID_PORT;              // default 5672
+extern std::string VALID_HOST;      // default "localhost"
+extern std::string VALID_VHOST;     // default "/"
+extern std::string VALID_USER;      // default "guest"
+extern std::string VALID_PASSWORD;  // default "guest"
+}
+
