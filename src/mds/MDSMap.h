74297b6fecc990579f706697122fc6d0f7cea9e9
IvanGuan <yunfei.guan@xtaotech.com>
2019-01-04 12:22:27 +0800

    client: fix fuse client hang because its pipe to mds is not ok
    If fuse client session had been killed by mds and the mds daemon restart
    or hot-standby switch happens right away but the client did not receive
    any message from monitor due to network or other whatever reason untill
    the mds become active again.Thus cause client didn't do closed_mds_session
    lead the seession still is STATE_OPEN but client can't send any message to
    mds because its pipe is not ok.So we should close the stale session so that
    it can be reopened again.
    
    Fixes: http://tracker.ceph.com/issues/36079
    Signed-off-by: Guan yunfei <yunfei.guan@xtaotech.com>

--- src/mds/MDSMap.h
+++ src/mds/MDSMap.h
@@ -616,6 +616,16 @@ public:
     }
   }
 
+  /**
+   * Get MDS rank incarnation if the rank is up, else -1
+   */
+  mds_gid_t get_incarnation(mds_rank_t m) const {
+    std::map<mds_rank_t, mds_gid_t>::const_iterator u = up.find(m);
+    if (u == up.end())
+      return MDS_GID_NONE;
+    return (mds_gid_t)get_inc_gid(u->second);
+  }
+
   int get_inc_gid(mds_gid_t gid) const {
     auto mds_info_entry = mds_info.find(gid);
     if (mds_info_entry != mds_info.end())
