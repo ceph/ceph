8f6409d3acc29c3309ac4bc6f08fb58feaee4cbf
hsiang41 <rick.chen@prophetstor.com>
2019-01-04 09:49:52 +0800

    mgr: diskprediction_cloud Add nvme devcie support
    Parse nvme device information from the device health output.
    
    Signed-off-by: Rick Chen <rick.chen@prophetstor.com>

--- src/pybind/mgr/diskprediction_cloud/agent/metrics/sai_cluster.py
+++ src/pybind/mgr/diskprediction_cloud/agent/metrics/sai_cluster.py
@@ -30,7 +30,6 @@ class SAICluserAgent(MetricsAgent):
         c_data.tags['domain_id'] = str(cluster_id)
         c_data.tags['host_domain_id'] = '%s_%s' % (str(cluster_id), str(socket.gethostname()))
         c_data.fields['agenthost'] = str(socket.gethostname())
-        c_data.tags['agenthost_domain_id'] = \
-            str('%s_%s' % (cluster_id, c_data.fields['agenthost']))
+        c_data.tags['agenthost_domain_id'] = cluster_id
         c_data.fields['name'] = 'Ceph mgr plugin'
         self.data.append(c_data)
