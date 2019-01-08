a591dfbbae23ab9b6cdecfc277185da8cc4c8e5f
Sage Weil <sage@redhat.com>
2019-01-08 09:54:04 -0600

    common/numa: remove unused _mask variants
    These parse the 'local_cpus' file that has a hex bitmask, but I didn't end
    up using them.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/test/common/test_numa.cc
+++ src/test/common/test_numa.cc
@@ -60,92 +60,13 @@ TEST(cpu_set, round_trip_list)
 	CPU_SET(i, &cpu_set);
       }
     }
-    std::string v = cpu_set_to_str_mask(size, &cpu_set);
+    std::string v = cpu_set_to_str_list(size, &cpu_set);
     cpu_set_t cpu_set_2;
     size_t size2;
-    ASSERT_EQ(0, parse_cpu_set_mask(v.c_str(), &size2, &cpu_set_2));
+    ASSERT_EQ(0, parse_cpu_set_list(v.c_str(), &size2, &cpu_set_2));
     for (unsigned i = 0; i < 32; ++i) {
       ASSERT_TRUE(CPU_ISSET(i, &cpu_set) == CPU_ISSET(i, &cpu_set_2));
     }
-  }
-}
-
-
-
-TEST(cpu_set, parse_mask) {
-  cpu_set_t cpu_set;
-  size_t size;
-
-  ASSERT_EQ(0, parse_cpu_set_mask("f", &size, &cpu_set));
-  ASSERT_EQ(size, 4u);
-  for (unsigned i = 0; i < size; ++i) {
-    ASSERT_TRUE(CPU_ISSET(i, &cpu_set));
-  }
-
-  ASSERT_EQ(0, parse_cpu_set_mask("0f", &size, &cpu_set));
-  ASSERT_EQ(size, 8u);
-  for (unsigned i = 0; i < 4; ++i) {
-    ASSERT_TRUE(CPU_ISSET(i, &cpu_set));
-  }
-
-  ASSERT_EQ(0, parse_cpu_set_mask("ffff", &size, &cpu_set));
-  ASSERT_EQ(size, 16u);
-  for (unsigned i = 0; i < size; ++i) {
-    ASSERT_TRUE(CPU_ISSET(i, &cpu_set));
-  }
-
-  ASSERT_EQ(0, parse_cpu_set_mask("0000", &size, &cpu_set));
-  ASSERT_EQ(size, 16u);
-  for (unsigned i = 0; i < size; ++i) {
-    ASSERT_FALSE(CPU_ISSET(i, &cpu_set));
-  }
-
-  ASSERT_EQ(0, parse_cpu_set_mask("00ff00ff", &size, &cpu_set));
-  ASSERT_EQ(size, 32u);
-  for (unsigned i = 0; i < 8; ++i) {
-    ASSERT_TRUE(CPU_ISSET(i, &cpu_set));
-  }
-  for (unsigned i = 8; i < 16; ++i) {
-    ASSERT_FALSE(CPU_ISSET(i, &cpu_set));
-  }
-  for (unsigned i = 16; i < 24; ++i) {
-    ASSERT_TRUE(CPU_ISSET(i, &cpu_set));
-  }
-  for (unsigned i = 24; i < 32; ++i) {
-    ASSERT_FALSE(CPU_ISSET(i, &cpu_set));
-  }
-
-  ASSERT_EQ(0, parse_cpu_set_mask("ffffffffffffffffffffffffffffffff",
-				  &size, &cpu_set));
-  ASSERT_EQ(size, 128u);
-  for (unsigned i = 0; i < size; ++i) {
-    ASSERT_TRUE(CPU_ISSET(i, &cpu_set));
-  }
-}
-
-TEST(cpu_set, to_str_mask)
-{
-  cpu_set_t cpu_set;
-  CPU_ZERO(&cpu_set);
-  CPU_SET(0, &cpu_set);
-  CPU_SET(1, &cpu_set);
-  CPU_SET(2, &cpu_set);
-  CPU_SET(3, &cpu_set);
-  ASSERT_EQ(std::string("f"), cpu_set_to_str_mask(4, &cpu_set));
-  ASSERT_EQ(std::string("0f"), cpu_set_to_str_mask(8, &cpu_set));
-}
-
-TEST(cpu_set, round_trip_mask)
-{
-  for (unsigned i = 0; i < 100; ++i) {
-    char buf[50];
-    snprintf(buf, sizeof(buf), "%08llx", (long long unsigned)rand());
-    cpu_set_t cpu_set;
-    size_t size;
-    ASSERT_EQ(0, parse_cpu_set_mask(buf, &size, &cpu_set));
-    ASSERT_EQ(32u, size);
-    std::string v = cpu_set_to_str_mask(size, &cpu_set);
-    ASSERT_EQ(v, buf);
   }
 }
 
