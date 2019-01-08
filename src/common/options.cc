yehudasa/ceph-pr-archive

dmclock: optional enable of rgw dmclock
Adding a configurable rgw_enable_dmclock based on which we enable/disable the
scheduler. A TODO to tackle is enabling the throttler for beast frontend when
dmclock scheduler isn't enabled

Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

src/common/options.cc
@@ -6356,6 +6356,10 @@ std::vector<Option> get_rgw_options() {
     .add_tag("performance")
     .add_see_also("rgw_frontends"),
 
+    Option("rgw_enable_dmclock", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
+    .set_default(true)
+    .set_description("Enable dmclock scheduler for rgw requests"),
+
     Option("rgw_dmclock_admin_res", Option::TYPE_FLOAT, Option::LEVEL_ADVANCED)
     .set_default(100.0)
     .set_description("mclock reservation for admin requests")

ceph/ceph

src/common/options.cc
@@ -6676,6 +6676,10 @@ std::vector<Option> get_rgw_options() {
     .add_tag("performance")
     .add_see_also("rgw_frontends"),
 
+    Option("rgw_dmclock_enabled", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
+    .set_default(true)
+    .set_description("Enable dmclock scheduler for rgw requests"),
+
     Option("rgw_dmclock_admin_res", Option::TYPE_FLOAT, Option::LEVEL_ADVANCED)
     .set_default(100.0)
     .set_description("mclock reservation for admin requests")