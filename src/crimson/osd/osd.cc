f847f6ac5d922fc9b4af707a49da54738b85324b
Kefu Chai <kchai@redhat.com>
2019-01-11 18:21:25 +0800

    crimson: integrate mon::Client into OSD
    and register them as dispatchers using ChainedDispatchers
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/crimson/osd/osd.cc
+++ src/crimson/osd/osd.cc
@@ -1,5 +1,6 @@
 #include "osd.h"
 
+#include "crimson/net/Connection.h"
 #include "crimson/net/SocketMessenger.h"
 
 namespace {
@@ -26,6 +27,8 @@ OSD::OSD(int id, uint32_t nonce)
       msgr->set_crc_header();
     }
   }
+  dispatchers.push_front(this);
+  dispatchers.push_front(&monc);
 }
 
 OSD::~OSD() = default;
@@ -33,10 +36,48 @@ OSD::~OSD() = default;
 seastar::future<> OSD::start()
 {
   logger().info("start");
-  return seastar::now();
+  return client_msgr.start(&dispatchers).then([this] {
+    return monc.start();
+  }).then([this] {
+    monc.sub_want("mgrmap", 0, 0);
+    monc.sub_want("osdmap", 0, 0);
+    return monc.renew_subs();
+  });
 }
 
 seastar::future<> OSD::stop()
 {
-  return gate.close();
+  return gate.close().then([this] {
+    return monc.stop();
+  }).then([this] {
+    return client_msgr->shutdown();
+  });
+}
+
+seastar::future<> OSD::ms_dispatch(ceph::net::ConnectionRef conn, MessageRef m)
+{
+  logger().info("ms_dispatch {}", *m);
+  return seastar::now();
+}
+
+seastar::future<> OSD::ms_handle_connect(ceph::net::ConnectionRef conn)
+{
+  if (conn->get_peer_type() != CEPH_ENTITY_TYPE_MON) {
+    return seastar::now();
+  } else {
+    return seastar::now();
+  }
+}
+
+seastar::future<> OSD::ms_handle_reset(ceph::net::ConnectionRef conn)
+{
+  // TODO: cleanup the session attached to this connection
+  logger().warn("ms_handle_reset");
+  return seastar::now();
+}
+
+seastar::future<> OSD::ms_handle_remote_reset(ceph::net::ConnectionRef conn)
+{
+  logger().warn("ms_handle_remote_reset");
+  return seastar::now();
 }
