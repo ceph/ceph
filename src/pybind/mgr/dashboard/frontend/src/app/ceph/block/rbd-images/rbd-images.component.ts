6e261774ce238bfa82a351043fbb2044cecc85c2
Jason Dillaman <dillaman@redhat.com>
2019-01-11 16:30:52 -0500

    mgr/dashboard: incorporate RBD overall performance grafana dashboard
    Fixes: http://tracker.ceph.com/issues/37867
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-images/rbd-images.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-images/rbd-images.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Permission } from '../../../shared/models/permissions';
+import { AuthStorageService } from '../../../shared/services/auth-storage.service';
+
 @Component({
   selector: 'cd-rbd-images',
   templateUrl: './rbd-images.component.html',
   styleUrls: ['./rbd-images.component.scss']
 })
 export class RbdImagesComponent implements OnInit {
-  constructor() {}
+  grafanaPermission: Permission;
+
+  constructor(private authStorageService: AuthStorageService) {
+    this.grafanaPermission = this.authStorageService.getPermissions().grafana;
+  }
 
   ngOnInit() {}
 }
