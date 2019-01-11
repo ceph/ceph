a4c5236e165390bcab1874f9b18b603a106c2773
Kefu Chai <kchai@redhat.com>
2019-01-11 18:21:25 +0800

    crimson: integrate mon::Client into OSD
    and register them as dispatchers using ChainedDispatchers
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/crimson/osd/osd.h
+++ src/crimson/osd/osd.h
@@ -3,7 +3,9 @@
 #include <seastar/core/future.hh>
 #include <seastar/core/gate.hh>
 
+#include "crimson/mon/MonClient.h"
 #include "crimson/net/Dispatcher.h"
+#include "crimson/osd/chained_dispatchers.h"
 
 namespace ceph::net {
   class Messenger;
@@ -16,6 +18,14 @@ class OSD : public ceph::net::Dispatcher {
   std::unique_ptr<ceph::net::Messenger> cluster_msgr;
   // talk with mon/mgr
   std::unique_ptr<ceph::net::Messenger> client_msgr;
+  ChainedDispatchers dispatchers;
+  ceph::mon::Client monc;
+
+  // Dispatcher methods
+  seastar::future<> ms_dispatch(ceph::net::ConnectionRef conn, MessageRef m) override;
+  seastar::future<> ms_handle_connect(ceph::net::ConnectionRef conn) override;
+  seastar::future<> ms_handle_reset(ceph::net::ConnectionRef conn) override;
+  seastar::future<> ms_handle_remote_reset(ceph::net::ConnectionRef conn) override;
 
 public:
   OSD(int id, uint32_t nonce);
