57c72346c71dbcb82be80f34884029c8034d5a01
Sage Weil <sage@redhat.com>
2019-01-14 16:13:53 -0600

    auth: clean up AuthServiceHandler::start_session()
    - return error code, not type (which never changes)
    - take const ref for input args
    - pointers for output args
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/auth/AuthServiceHandler.h
+++ src/auth/AuthServiceHandler.h
@@ -35,7 +35,9 @@ public:
 
   virtual ~AuthServiceHandler() { }
 
-  virtual int start_session(EntityName& name, bufferlist::const_iterator& indata, bufferlist& result, AuthCapsInfo& caps) = 0;
+  virtual int start_session(const EntityName& name,
+			    bufferlist *result,
+			    AuthCapsInfo *caps) = 0;
   virtual int handle_request(bufferlist::const_iterator& indata, bufferlist& result, uint64_t& global_id, AuthCapsInfo& caps) = 0;
 
   EntityName& get_entity_name() { return entity_name; }
