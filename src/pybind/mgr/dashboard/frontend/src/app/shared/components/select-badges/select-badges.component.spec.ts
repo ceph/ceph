d7135998104464c525939c207a81669d192e99ee
Tiago Melo <tmelo@suse.com>
2019-01-15 15:28:00 +0000

    mgr/dashboard: Add generic Select component
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/select-badges/select-badges.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/select-badges/select-badges.component.spec.ts
@@ -1,25 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule, Validators } from '@angular/forms';
 
+import { I18n } from '@ngx-translate/i18n-polyfill';
 import { PopoverModule } from 'ngx-bootstrap/popover';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { configureTestBed, i18nProviders } from '../../../../testing/unit-test-helper';
-import { SelectBadgesOption } from './select-badges-option.model';
+import { SelectMessages } from '../select/select-messages.model';
+import { SelectComponent } from '../select/select.component';
 import { SelectBadgesComponent } from './select-badges.component';
 
 describe('SelectBadgesComponent', () => {
   let component: SelectBadgesComponent;
   let fixture: ComponentFixture<SelectBadgesComponent>;
 
-  const selectOption = (filter: string) => {
-    component.filter.setValue(filter);
-    component.updateFilter();
-    component.selectOption();
-  };
-
   configureTestBed({
-    declarations: [SelectBadgesComponent],
+    declarations: [SelectBadgesComponent, SelectComponent],
     imports: [PopoverModule.forRoot(), TooltipModule, ReactiveFormsModule],
     providers: i18nProviders
   });
@@ -28,252 +24,38 @@ describe('SelectBadgesComponent', () => {
     fixture = TestBed.createComponent(SelectBadgesComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    component.options = [
-      { name: 'option1', description: '', selected: false },
-      { name: 'option2', description: '', selected: false },
-      { name: 'option3', description: '', selected: false }
-    ];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should add item', () => {
-    component.data = [];
-    component.triggerSelection(component.options[1]);
-    expect(component.data).toEqual(['option2']);
-  });
+  it('should reflect the attributes into CdSelect', () => {
+    const data = ['a', 'b'];
+    const options = [
+      { name: 'option1', description: '', selected: false },
+      { name: 'option2', description: '', selected: false }
+    ];
+    const i18n = TestBed.get(I18n);
+    const messages = new SelectMessages({ empty: 'foo bar' }, i18n);
+    const selectionLimit = 2;
+    const customBadges = true;
+    const customBadgeValidators = [Validators.required];
 
-  it('should update selected', () => {
-    component.data = ['option2'];
-    component.ngOnChanges();
-    expect(component.options[0].selected).toBe(false);
-    expect(component.options[1].selected).toBe(true);
-  });
+    component.data = data;
+    component.options = options;
+    component.messages = messages;
+    component.selectionLimit = selectionLimit;
+    component.customBadges = customBadges;
+    component.customBadgeValidators = customBadgeValidators;
 
-  it('should remove item', () => {
-    component.options.map((option) => {
-      option.selected = true;
-      return option;
-    });
-    component.data = ['option1', 'option2', 'option3'];
-    component.removeItem('option1');
-    expect(component.data).toEqual(['option2', 'option3']);
-  });
+    fixture.detectChanges();
 
-  it('should not remove item that is not selected', () => {
-    component.options[0].selected = true;
-    component.data = ['option1'];
-    component.removeItem('option2');
-    expect(component.data).toEqual(['option1']);
-  });
-
-  describe('filter values', () => {
-    beforeEach(() => {
-      component.ngOnInit();
-    });
-
-    it('shows all options with no value set', () => {
-      expect(component.filteredOptions).toEqual(component.options);
-    });
-
-    it('shows one option that it filtered for', () => {
-      component.filter.setValue('2');
-      component.updateFilter();
-      expect(component.filteredOptions).toEqual([component.options[1]]);
-    });
-
-    it('shows all options after selecting something', () => {
-      component.filter.setValue('2');
-      component.updateFilter();
-      component.selectOption();
-      expect(component.filteredOptions).toEqual(component.options);
-    });
-
-    it('is not able to create by default with no value set', () => {
-      component.updateFilter();
-      expect(component.isCreatable()).toBeFalsy();
-    });
-
-    it('is not able to create by default with a value set', () => {
-      component.filter.setValue('2');
-      component.updateFilter();
-      expect(component.isCreatable()).toBeFalsy();
-    });
-  });
-
-  describe('automatically add selected options if not in options array', () => {
-    beforeEach(() => {
-      component.data = ['option1', 'option4'];
-      expect(component.options.length).toBe(3);
-    });
-
-    const expectedResult = () => {
-      expect(component.options.length).toBe(4);
-      expect(component.options[3]).toEqual(new SelectBadgesOption(true, 'option4', ''));
-    };
-
-    it('with no extra settings', () => {
-      component.ngOnInit();
-      expectedResult();
-    });
-
-    it('with custom badges', () => {
-      component.customBadges = true;
-      component.ngOnInit();
-      expectedResult();
-    });
-
-    it('with limit higher than selected', () => {
-      component.selectionLimit = 3;
-      component.ngOnInit();
-      expectedResult();
-    });
-
-    it('with limit equal to selected', () => {
-      component.selectionLimit = 2;
-      component.ngOnInit();
-      expectedResult();
-    });
-
-    it('with limit lower than selected', () => {
-      component.selectionLimit = 1;
-      component.ngOnInit();
-      expectedResult();
-    });
-  });
-
-  describe('sorted array and options', () => {
-    beforeEach(() => {
-      component.customBadges = true;
-      component.customBadgeValidators = [Validators.pattern('[A-Za-z0-9_]+')];
-      component.data = ['c', 'b'];
-      component.options = [
-        new SelectBadgesOption(true, 'd', ''),
-        new SelectBadgesOption(true, 'a', '')
-      ];
-      component.ngOnInit();
-    });
-
-    it('has a sorted selection', () => {
-      expect(component.data).toEqual(['a', 'b', 'c', 'd']);
-    });
-
-    it('has a sorted options', () => {
-      const sortedOptions = [
-        new SelectBadgesOption(true, 'a', ''),
-        new SelectBadgesOption(true, 'b', ''),
-        new SelectBadgesOption(true, 'c', ''),
-        new SelectBadgesOption(true, 'd', '')
-      ];
-      expect(component.options).toEqual(sortedOptions);
-    });
-
-    it('has a sorted selection after adding an item', () => {
-      selectOption('block');
-      expect(component.data).toEqual(['a', 'b', 'block', 'c', 'd']);
-    });
-
-    it('has a sorted options after adding an item', () => {
-      selectOption('block');
-      const sortedOptions = [
-        new SelectBadgesOption(true, 'a', ''),
-        new SelectBadgesOption(true, 'b', ''),
-        new SelectBadgesOption(true, 'block', ''),
-        new SelectBadgesOption(true, 'c', ''),
-        new SelectBadgesOption(true, 'd', '')
-      ];
-      expect(component.options).toEqual(sortedOptions);
-    });
-  });
-
-  describe('with custom options', () => {
-    beforeEach(() => {
-      component.customBadges = true;
-      component.customBadgeValidators = [Validators.pattern('[A-Za-z0-9_]+')];
-      component.ngOnInit();
-    });
-
-    it('is not able to create with no value set', () => {
-      component.updateFilter();
-      expect(component.isCreatable()).toBeFalsy();
-    });
-
-    it('is able to create with a valid value set', () => {
-      component.filter.setValue('2');
-      component.updateFilter();
-      expect(component.isCreatable()).toBeTruthy();
-    });
-
-    it('is not able to create with a value set that already exist', () => {
-      component.filter.setValue('option2');
-      component.updateFilter();
-      expect(component.isCreatable()).toBeFalsy();
-    });
-
-    it('adds custom option', () => {
-      selectOption('customOption');
-      expect(component.options[0]).toEqual({
-        name: 'customOption',
-        description: '',
-        selected: true
-      });
-      expect(component.options.length).toBe(4);
-      expect(component.data).toEqual(['customOption']);
-    });
-
-    it('will not add an option that did not pass the validation', () => {
-      selectOption(' this does not pass ');
-      expect(component.options.length).toBe(3);
-      expect(component.data).toEqual([]);
-      expect(component.filter.invalid).toBeTruthy();
-    });
-
-    it('removes custom item selection by name', () => {
-      selectOption('customOption');
-      component.removeItem('customOption');
-      expect(component.data).toEqual([]);
-      expect(component.options.length).toBe(4);
-      expect(component.options[0]).toEqual({
-        name: 'customOption',
-        description: '',
-        selected: false
-      });
-    });
-
-    it('will not add an option that is already there', () => {
-      selectOption('option2');
-      expect(component.options.length).toBe(3);
-      expect(component.data).toEqual(['option2']);
-    });
-
-    it('will not add an option twice after each other', () => {
-      selectOption('onlyOnce');
-      expect(component.data).toEqual(['onlyOnce']);
-      selectOption('onlyOnce');
-      expect(component.data).toEqual([]);
-      selectOption('onlyOnce');
-      expect(component.data).toEqual(['onlyOnce']);
-      expect(component.options.length).toBe(4);
-    });
-  });
-
-  describe('if the selection limit is reached', function() {
-    beforeEach(() => {
-      component.selectionLimit = 2;
-      component.triggerSelection(component.options[0]);
-      component.triggerSelection(component.options[1]);
-    });
-
-    it('will not select more options', () => {
-      component.triggerSelection(component.options[2]);
-      expect(component.data).toEqual(['option1', 'option2']);
-    });
-
-    it('will unselect options that are selected', () => {
-      component.triggerSelection(component.options[1]);
-      expect(component.data).toEqual(['option1']);
-    });
+    expect(component.cdSelect.data).toEqual(data);
+    expect(component.cdSelect.options).toEqual(options);
+    expect(component.cdSelect.messages).toEqual(messages);
+    expect(component.cdSelect.selectionLimit).toEqual(selectionLimit);
+    expect(component.cdSelect.customBadges).toEqual(customBadges);
+    expect(component.cdSelect.customBadgeValidators).toEqual(customBadgeValidators);
   });
 });
--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/select-badges/select-badges.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/select-badges/select-badges.component.ts
@@ -1,140 +1,33 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
-import { FormControl, ValidatorFn } from '@angular/forms';
+import { Component, Input, ViewChild } from '@angular/core';
+import { ValidatorFn } from '@angular/forms';
 
 import { I18n } from '@ngx-translate/i18n-polyfill';
 import * as _ from 'lodash';
 
-import { CdFormGroup } from '../../forms/cd-form-group';
-import { SelectBadgesMessages } from './select-badges-messages.model';
-import { SelectBadgesOption } from './select-badges-option.model';
+import { SelectMessages } from '../select/select-messages.model';
+import { SelectOption } from '../select/select-option.model';
 
 @Component({
   selector: 'cd-select-badges',
   templateUrl: './select-badges.component.html',
   styleUrls: ['./select-badges.component.scss']
 })
-export class SelectBadgesComponent implements OnInit, OnChanges {
+export class SelectBadgesComponent {
   @Input()
   data: Array<string> = [];
   @Input()
-  options: Array<SelectBadgesOption> = [];
+  options: Array<SelectOption> = [];
   @Input()
-  messages = new SelectBadgesMessages({}, this.i18n);
+  messages = new SelectMessages({}, this.i18n);
   @Input()
   selectionLimit: number;
   @Input()
   customBadges = false;
   @Input()
   customBadgeValidators: ValidatorFn[] = [];
-  form: CdFormGroup;
-  filter: FormControl;
-  Object = Object;
-  filteredOptions: Array<SelectBadgesOption> = [];
+
+  @ViewChild('cdSelect')
+  cdSelect;
 
   constructor(private i18n: I18n) {}
-
-  ngOnInit() {
-    this.initFilter();
-    if (this.data.length > 0) {
-      this.initMissingOptions();
-    }
-    this.options = _.sortBy(this.options, ['name']);
-    this.updateOptions();
-  }
-
-  private initFilter() {
-    this.filter = new FormControl('', { validators: this.customBadgeValidators });
-    this.form = new CdFormGroup({ filter: this.filter });
-    this.filteredOptions = [...this.options];
-  }
-
-  private initMissingOptions() {
-    const options = this.options.map((option) => option.name);
-    const needToCreate = this.data.filter((option) => options.indexOf(option) === -1);
-    needToCreate.forEach((option) => this.addOption(option));
-    this.forceOptionsToReflectData();
-  }
-
-  private addOption(name: string) {
-    this.options.push(new SelectBadgesOption(false, name, ''));
-    this.options = _.sortBy(this.options, ['name']);
-    this.triggerSelection(this.options.find((option) => option.name === name));
-  }
-
-  triggerSelection(option: SelectBadgesOption) {
-    if (
-      !option ||
-      (this.selectionLimit && !option.selected && this.data.length >= this.selectionLimit)
-    ) {
-      return;
-    }
-    option.selected = !option.selected;
-    this.updateOptions();
-  }
-
-  private updateOptions() {
-    this.data.splice(0, this.data.length);
-    this.options.forEach((option: SelectBadgesOption) => {
-      if (option.selected) {
-        this.data.push(option.name);
-      }
-    });
-    this.updateFilter();
-  }
-
-  updateFilter() {
-    this.filteredOptions = this.options.filter((option) => option.name.includes(this.filter.value));
-  }
-
-  private forceOptionsToReflectData() {
-    this.options.forEach((option) => {
-      if (this.data.indexOf(option.name) !== -1) {
-        option.selected = true;
-      }
-    });
-  }
-
-  ngOnChanges() {
-    if (!this.options || !this.data || this.data.length === 0) {
-      return;
-    }
-    this.forceOptionsToReflectData();
-  }
-
-  selectOption() {
-    if (this.filteredOptions.length === 0) {
-      this.addCustomOption();
-    } else {
-      this.triggerSelection(this.filteredOptions[0]);
-      this.resetFilter();
-    }
-  }
-
-  addCustomOption() {
-    if (!this.isCreatable()) {
-      return;
-    }
-    this.addOption(this.filter.value);
-    this.resetFilter();
-  }
-
-  isCreatable() {
-    return (
-      this.customBadges &&
-      this.filter.valid &&
-      this.filter.value.length > 0 &&
-      this.filteredOptions.every((option) => option.name !== this.filter.value)
-    );
-  }
-
-  private resetFilter() {
-    this.filter.setValue('');
-    this.updateFilter();
-  }
-
-  removeItem(item: string) {
-    this.triggerSelection(
-      this.options.find((option: SelectBadgesOption) => option.name === item && option.selected)
-    );
-  }
 }
