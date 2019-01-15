9985f369dd6b93d81668288726bad0d8a87c3f3a
Patrick Donnelly <pdonnell@redhat.com>
2019-01-15 13:45:14 -0800

    MDSMonitor: add missing OSDMonitor writeable check
    Fixes: http://tracker.ceph.com/issues/37929
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>
    (cherry picked from commit 8ef68663d311f71d1598297c14fad39274ada41d)

--- src/mon/FSCommands.cc
+++ src/mon/FSCommands.cc
@@ -620,6 +620,13 @@ class RemoveFilesystemHandler : public FileSystemCommandHandler
       map<string, cmd_vartype> &cmdmap,
       std::stringstream &ss) override
   {
+    /* We may need to blacklist ranks. */
+    if (!mon->osdmon()->is_writeable()) {
+      // not allowed to write yet, so retry when we can
+      mon->osdmon()->wait_for_writeable(op, new PaxosService::C_RetryMessage(mon->mdsmon(), op));
+      return -EAGAIN;
+    }
+
     // Check caller has correctly named the FS to delete
     // (redundant while there is only one FS, but command
     //  syntax should apply to multi-FS future)
@@ -662,6 +669,9 @@ class RemoveFilesystemHandler : public FileSystemCommandHandler
       // Standby replays don't write, so it isn't important to
       // wait for an osdmap propose here: ignore return value.
       mon->mdsmon()->fail_mds_gid(fsmap, gid);
+    }
+    if (!to_fail.empty()) {
+      mon->osdmon()->propose_pending(); /* maybe new blacklists */
     }
 
     fsmap.erase_filesystem(fs->fscid);
