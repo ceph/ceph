d9dd67c5b7b0c5cc37e4532163dc1d1e98f777a9
Yehuda Sadeh <yehuda@redhat.com>
2019-01-15 19:31:18 -0800

    rgw: check if default placement target is set correctly
    Don't check if rule is empty. Rule might consist of non empty
    storage class, but in this case we only care about the placement
    id.
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/services/svc_zone.cc
+++ src/rgw/services/svc_zone.cc
@@ -967,7 +967,7 @@ int RGWSI_Zone::select_new_bucket_location(const RGWUserInfo& user_info, const s
                     << " within zonegroup " << dendl;
       return -ERR_INVALID_LOCATION_CONSTRAINT;
     }
-  } else if (!user_info.default_placement.empty()) {
+  } else if (!user_info.default_placement.name.empty()) {
     used_rule = &user_info.default_placement;
     titer = zonegroup.placement_targets.find(user_info.default_placement.name);
     if (titer == zonegroup.placement_targets.end()) {
