fc90a084c4eccad2dc677caf7b9fe4ca1a1de305
Sage Weil <sage@redhat.com>
2019-01-16 15:00:59 -0600

    auth/AuthClientHandler: pass in session_key, connection_secret pointers
    No functional change.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/auth/cephx/CephxClientHandler.h
+++ src/auth/cephx/CephxClientHandler.h
@@ -54,7 +54,9 @@ public:
   }
   void prepare_build_request() override;
   int build_request(bufferlist& bl) const override;
-  int handle_response(int ret, bufferlist::const_iterator& iter) override;
+  int handle_response(int ret, bufferlist::const_iterator& iter,
+		      CryptoKey *session_key,
+		      CryptoKey *connection_secret) override;
   bool build_rotating_request(bufferlist& bl) const override;
 
   int get_protocol() const override { return CEPH_AUTH_CEPHX; }
