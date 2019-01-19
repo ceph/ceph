301a64212f0a38a3b5db4bd1bd0f15e26ff055cf
Kefu Chai <kchai@redhat.com>
2019-01-20 00:01:55 +0800

    tools/ceph_kvstore_tool: extract StoreTool into kvstore_tool.cc
    to workaround n bug in GCC 7.3, which leads to ICE like
    
    build/workspace/ceph-dev-new-build/ARCH/x86_64/AVAILABLE_ARCH/x86_64/AVAILABLE_DIST/centos7/DIST/centos7/MACHINE_SIZE/huge/release/14.0.1-2751-gb268822/rpm/el7/BUILD/ceph-14.0.1-2751-gb268822/src/tools/ceph_kvstore_tool.cc:625:1:
    internal compiler error: Segmentation fault
     }
     ^
    Please submit a full bug report,
    with preprocessed source if appropriate.
    See <http://bugzilla.redhat.com/bugzilla> for instructions.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

---
+++ src/tools/kvstore_tool.h
@@ -0,0 +1,73 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#pragma once
+
+#include <memory>
+#include <ostream>
+#include <string>
+
+#include "acconfig.h"
+#include "include/buffer_fwd.h"
+#ifdef WITH_BLUESTORE
+#include "os/bluestore/BlueStore.h"
+#endif
+
+class KeyValueDB;
+
+class StoreTool
+{
+#ifdef WITH_BLUESTORE
+  struct Deleter {
+    BlueStore *bluestore;
+    Deleter()
+      : bluestore(nullptr) {}
+    Deleter(BlueStore *store)
+      : bluestore(store) {}
+    void operator()(KeyValueDB *db) {
+      if (bluestore) {
+	bluestore->umount();
+	delete bluestore;
+      } else {
+	delete db;
+      }
+    }
+  };
+  std::unique_ptr<KeyValueDB, Deleter> db;
+#else
+  std::unique_ptr<KeyValueDB> db;
+#endif
+
+  const std::string store_path;
+
+public:
+  StoreTool(const std::string& type,
+	    const std::string& path,
+	    bool need_open_db=true);
+  uint32_t traverse(const std::string& prefix,
+                    const bool do_crc,
+                    const bool do_value_dump,
+                    ostream *out);
+  void list(const std::string& prefix,
+	    const bool do_crc,
+	    const bool do_value_dump);
+  bool exists(const std::string& prefix);
+  bool exists(const std::string& prefix, const std::string& key);
+  ceph::bufferlist get(const std::string& prefix,
+		       const std::string& key,
+		       bool& exists);
+  uint64_t get_size();
+  bool set(const std::string& prefix,
+	   const std::string& key,
+	   ceph::bufferlist& val);
+  bool rm(const std::string& prefix, const std::string& key);
+  bool rm_prefix(const std::string& prefix);
+  int copy_store_to(const std::string& type, const std::string& other_path,
+                    const int num_keys_per_tx, const std::string& other_type);
+  void compact();
+  void compact_prefix(const std::string& prefix);
+  void compact_range(const std::string& prefix,
+		     const std::string& start,
+		     const std::string& end);
+  int destructive_repair();
+};
