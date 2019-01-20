861d2f794f42279be0acaad5e962ab0ab49d8869
Sage Weil <sage@redhat.com>
2019-01-20 17:06:23 -0600

    auth/AuthClientHandler: add build_initial_request hook
    With msgr2 the initial kickoff of an authentication handshake is client ->
    server, while with msgr1 it was server -> client.  So existing
    implementations have an empty initial message (outside of the messenger's
    envelope).  Future auth implementations that are msgr2 only (e.g., krb)
    may want to make use of this initial payload.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/auth/AuthClientHandler.h
+++ src/auth/AuthClientHandler.h
@@ -48,6 +48,9 @@ public:
 
   virtual void reset() = 0;
   virtual void prepare_build_request() = 0;
+  virtual void build_initial_request(bufferlist *bl) const {
+    // this is empty for methods cephx and none.
+  }
   virtual int build_request(bufferlist& bl) const = 0;
   virtual int handle_response(int ret, bufferlist::const_iterator& iter,
 			      CryptoKey *session_key,
