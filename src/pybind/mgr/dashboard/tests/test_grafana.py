e9f9d031f99dc290895124338b9eed52fc18b60f
Zack Cerza <zack@redhat.com>
2019-01-21 17:30:44 -0700

    mgr/dashboard: Push Grafana dashboards on startup
    Because we didn't have viable way to populate Grafana dashboards in a
    fully containerized context, we needed a different method. This will
    work in packaged and containerized deployments.
    
    We provide three methods to push the dashboard JSON files to the Grafana
    instance: a command (ceph dashboard update-grafana-dashboards); an API
    endpoint (/api/grafana/update_dashboards); and an automatic on-startup
    feature that is disabled by default (ceph dashboard
    set-grafana-update-dashboards true). The on-startup method will also
    retry periodically to avoid racing with the startup of Grafana itself.
    
    Signed-off-by: Zack Cerza <zack@redhat.com>
    (cherry picked from commit c0bc7dafa777cfb349f372237ec91c5285dd4df2)

--- src/pybind/mgr/dashboard/tests/test_grafana.py
+++ src/pybind/mgr/dashboard/tests/test_grafana.py
@@ -6,19 +6,45 @@ from .. import mgr
 class GrafanaTest(ControllerTestCase):
     @classmethod
     def setup_server(cls):
-        settings = {
-            'GRAFANA_API_URL': 'http://localhost:3000'
-        }
-        mgr.get_module_option.side_effect = settings.get
+        cls.server_settings()
         # pylint: disable=protected-access
         Grafana._cp_config['tools.authenticate.on'] = False
         cls.setup_controllers([Grafana])
 
+    @classmethod
+    def server_settings(
+            cls,
+            url='http://localhost:3000',
+            user='admin',
+            password='admin',
+    ):
+        settings = dict()
+        if url is not None:
+            settings['GRAFANA_API_URL'] = url
+        if user is not None:
+            settings['GRAFANA_API_USERNAME'] = user
+        if password is not None:
+            settings['GRAFANA_API_PASSWORD'] = password
+        mgr.get_module_option.side_effect = settings.get
+
     def test_url(self):
+        self.server_settings()
         self._get('/api/grafana/url')
         self.assertStatus(200)
         self.assertJsonBody({'instance': 'http://localhost:3000'})
 
     def test_validation(self):
+        self.server_settings()
         self._get('/api/grafana/validation/foo')
+        self.assertStatus(500)
+
+    def test_dashboards(self):
+        self.server_settings(url=None)
+        self._post('/api/grafana/dashboards')
+        self.assertStatus(500)
+        self.server_settings(user=None)
+        self._post('/api/grafana/dashboards')
+        self.assertStatus(500)
+        self.server_settings(password=None)
+        self._post('/api/grafana/dashboards')
         self.assertStatus(500)
