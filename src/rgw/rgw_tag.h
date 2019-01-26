66d2ed38b3414023cfad8eb111ba56a88bece43b
Matt Benjamin <mbenjamin@redhat.com>
2019-01-26 17:43:51 -0500

    rgw: project and return lc expiration from GET/HEAD and PUT ops
    https://tracker.ceph.com/issues/38055
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_tag.h
+++ src/rgw/rgw_tag.h
@@ -8,12 +8,12 @@
 #include <include/types.h>
 #include <boost/container/flat_map.hpp>
 
-#include "rgw_common.h"
-
 class RGWObjTags
 {
- protected:
+public:
   using tag_map_t = boost::container::flat_map <std::string, std::string>;
+
+protected:
   tag_map_t tag_map;
 
   uint32_t max_obj_tags{10};
@@ -23,7 +23,6 @@ class RGWObjTags
  public:
   RGWObjTags() = default;
   RGWObjTags(uint32_t max_obj_tags):max_obj_tags(max_obj_tags) {}
-
 
   void encode(bufferlist& bl) const {
     ENCODE_START(1,1,bl);
