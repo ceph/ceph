4c5b8b6c94e4a8f34f38435f6ac52223c8837026
shangdehao1 <dehao.shang@intel.com>
2019-01-27 04:53:15 +0800

    tools: follow current codes to modify UT
    Signed-off-by: Dehao Shang <dehao.shang@intel.com>

--- src/test/immutable_object_cache/test_message.cc
+++ src/test/immutable_object_cache/test_message.cc
@@ -5,14 +5,16 @@ using namespace ceph::immutable_obj_cache;
 
 TEST(test_for_message, test_1) 
 {
+  std::string pool_nspace("this is a pool namespace");
   std::string oid_name("this is a oid name");
+  std::string cache_file_path("/temp/ceph_immutable_object_cache");
 
   ObjectCacheMsgHeader header;
   header.seq = 1;
   header.type = 2;
   header.version =3;
-  header.data_len = 5;
-  header.reserved = 6;
+  header.data_len = 0;
+  header.reserved = 5;
 
   ObjectCacheRequest req;
 
@@ -23,28 +25,46 @@ TEST(test_for_message, test_1)
 
   req.m_data.m_read_offset = 222222;
   req.m_data.m_read_len = 333333;
+  req.m_data.m_pool_id = 444444;
+  req.m_data.m_snap_id = 555555;
   req.m_data.m_oid = oid_name;
+  req.m_data.m_pool_namespace = pool_nspace;
+  req.m_data.m_cache_path = cache_file_path;
 
+  // ObjectRequest --> bufferlist
   req.encode();
 
   ASSERT_EQ(req.m_head_buffer.length(), sizeof(req.m_head));
 
-
   ObjectCacheRequest* req_decode;
 
-  auto x = req.get_head_buffer();
-  auto z = req.get_data_buffer();
+  auto head_bl = req.get_head_buffer();
+  auto data_bl = req.get_data_buffer();
 
-  req_decode = decode_object_cache_request(x, z);
+  // bufferlist --> ObjectCacheRequest
+  req_decode = decode_object_cache_request(head_bl, data_bl);
 
   ASSERT_EQ(req_decode->m_head.seq, header.seq);
+  ASSERT_EQ(req_decode->m_head.seq, 1);
   ASSERT_EQ(req_decode->m_head.type, header.type);
+  ASSERT_EQ(req_decode->m_head.type, 2);
   ASSERT_EQ(req_decode->m_head.version, header.version);
+  ASSERT_EQ(req_decode->m_head.version, 3);
   ASSERT_EQ(req_decode->m_head.data_len, req.m_data_buffer.length());
+  ASSERT_EQ(req_decode->m_head.data_len, data_bl.length());
   ASSERT_EQ(req_decode->m_head.reserved, header.reserved);
+  ASSERT_EQ(req_decode->m_head.reserved, 5);
 
-
+  ASSERT_EQ(req_decode->m_data.m_read_offset, req.m_data.m_read_offset);
   ASSERT_EQ(req_decode->m_data.m_read_offset, 222222);
+  ASSERT_EQ(req_decode->m_data.m_read_len, req.m_data.m_read_len);
   ASSERT_EQ(req_decode->m_data.m_read_len, 333333);
+  ASSERT_EQ(req_decode->m_data.m_pool_namespace, req.m_data.m_pool_namespace);
+  ASSERT_EQ(req_decode->m_data.m_pool_namespace, pool_nspace);
+  ASSERT_EQ(req_decode->m_data.m_cache_path, req.m_data.m_cache_path);
+  ASSERT_EQ(req_decode->m_data.m_cache_path, cache_file_path);
+  ASSERT_EQ(req_decode->m_data.m_oid, req.m_data.m_oid);
   ASSERT_EQ(req_decode->m_data.m_oid, oid_name);
+
+  delete req_decode;
 }
