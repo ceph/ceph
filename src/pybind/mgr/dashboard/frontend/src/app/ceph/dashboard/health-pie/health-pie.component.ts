e03d37f52a0aeecbae562a0ee11494bd6be3e182
alfonsomthd <almartin@redhat.com>
2019-01-30 15:27:47 +0100

    mgr/dashboard: Updated colors in PG Status chart
    * For consistency:
      Set 'Clean' status color to 'HEALTH_OK' color
      (Cluster Status card).
    
      Set 'Warning' status color to 'HEALTH_WARN' color.
    
      'Working' (blue) & 'Unknown' (red) are kept due to
      previous consensus about these complementary colors
      in doughnut/pie charts.
    
    * Renamed Health Pie colors for the sake of clarity.
    
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/health-pie/health-pie.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/health-pie/health-pie.component.ts
@@ -126,11 +126,11 @@ export class HealthPieComponent implements OnChanges, OnInit {
     this.chartConfig.colors = [
       {
         backgroundColor: [
-          HealthPieColor.MEDIUM_LIGHT_SHADE_PINK_RED,
-          HealthPieColor.MEDIUM_DARK_SHADE_CYAN_BLUE,
-          HealthPieColor.LIGHT_SHADE_BROWN,
-          HealthPieColor.SHADE_GREEN_CYAN,
-          HealthPieColor.MEDIUM_DARK_SHADE_BLUE_MAGENTA
+          HealthPieColor.DEFAULT_RED,
+          HealthPieColor.DEFAULT_BLUE,
+          HealthPieColor.DEFAULT_ORANGE,
+          HealthPieColor.DEFAULT_GREEN,
+          HealthPieColor.DEFAULT_MAGENTA
         ]
       }
     ];
