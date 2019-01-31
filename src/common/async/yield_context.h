6b36f1a4fba8b766ee21e4e4b0ff0e158206a05d
Adam C. Emerson <aemerson@redhat.com>
2019-01-31 17:37:37 -0500

    common/async: Change static constexpr null_yield to inline
    Inline ensuring that all references in any linked object point to a
    single object.
    
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/common/async/yield_context.h
+++ src/common/async/yield_context.h
@@ -50,4 +50,4 @@ class optional_yield {
 };
 
 // type tag object to construct an empty optional_yield
-static constexpr optional_yield::empty_t null_yield{};
+inline constexpr optional_yield::empty_t null_yield{};
