e1f6eb8a7ad49f1eb0bc1424bdc199d84d4c0bbb
Sage Weil <sage@redhat.com>
2019-02-01 12:17:07 -0600

    auth: document Auth{Client,Server} interfaces
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/auth/AuthServer.h
+++ src/auth/AuthServer.h
@@ -17,12 +17,15 @@ public:
   AuthServer(CephContext *cct) : auth_registry(cct) {}
   virtual ~AuthServer() {}
 
+  /// Get authentication methods and connection modes for the given peer type
   virtual void get_supported_auth_methods(
     int peer_type,
     std::vector<uint32_t> *methods,
     std::vector<uint32_t> *modes = nullptr) {
     auth_registry.get_supported_methods(peer_type, methods, modes);
   }
+
+  /// Get support connectino modes for the given peer type and auth method
   virtual void get_supported_con_modes(
     int peer_type,
     uint32_t auth_method,
@@ -30,16 +33,18 @@ public:
     auth_registry.get_supported_modes(peer_type, auth_method, modes);
   }
 
+  /// return an AuthAuthorizeHandler for hte given peer type and auth method
   AuthAuthorizeHandler *get_auth_authorize_handler(
     int peer_type,
     int auth_method) {
     return auth_registry.get_handler(peer_type, auth_method);
   }
 
+  /// Handle an authentication request on an incoming connection
   virtual int handle_auth_request(
     Connection *con,
     AuthConnectionMeta *auth_meta,
-    bool more,
+    bool more,           ///< true if this is not the first part of the handshake
     uint32_t auth_method,
     const bufferlist& bl,
     bufferlist *reply) = 0;
