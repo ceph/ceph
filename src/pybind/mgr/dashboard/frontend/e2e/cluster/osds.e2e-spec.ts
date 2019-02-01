2b8ed2cf1808773347be4fbcea8eefada57a03ed
Nathan Weinberg <nathan2@stwmd.net>
2019-02-01 08:55:37 -0500

    mgr/dashboard: Added additional breadcrumb and tab tests to Cluster menu
    Fixes: https://tracker.ceph.com/issues/38050
    Signed-off-by: Nathan Weinberg <nweinber@redhat.com>

--- src/pybind/mgr/dashboard/frontend/e2e/cluster/osds.e2e-spec.ts
+++ src/pybind/mgr/dashboard/frontend/e2e/cluster/osds.e2e-spec.ts
@@ -16,4 +16,19 @@ describe('OSDs page', () => {
     page.navigateTo();
     expect(Helper.getBreadcrumbText()).toEqual('OSDs');
   });
+
+  it('should show two tabs', () => {
+    page.navigateTo();
+    expect(Helper.getTabsCount()).toEqual(2);
+  });
+
+  it('should show OSDs list tab at first', () => {
+    page.navigateTo();
+    expect(Helper.getTabText(0)).toEqual('OSDs List');
+  });
+
+  it('should show overall performance as a second tab', () => {
+    page.navigateTo();
+    expect(Helper.getTabText(1)).toEqual('Overall Performance');
+  });
 });
