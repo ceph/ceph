d83f82947e30333014d7dad8838b9efc386de110
Abhishek Lekshmanan <abhishek@suse.com>
2019-02-06 15:12:19 +0100

    rgw_dmclock: add TODO note and copyright/editor headers
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- src/rgw/rgw_dmclock.h
+++ src/rgw/rgw_dmclock.h
@@ -1,8 +1,25 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2019 Red Hat, Inc.
+ * Copyright (C) 2019 SUSE LLC
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
 #ifndef RGW_DMCLOCK_H
 #define RGW_DMCLOCK_H
 #include "dmclock/src/dmclock_server.h"
 
 namespace rgw::dmclock {
+// TODO: implement read vs write
 enum class client_id {
                       admin, //< /admin apis
                       auth, //< swift auth, sts
