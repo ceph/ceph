236420108d525fdda4092dc97b31123fd1eb1215
Brad Hubbard <bhubbard@redhat.com>
2019-02-22 12:16:38 +1000

    qa/workunits/rest: Better detection of rest url
    Signed-off-by: Brad Hubbard <bhubbard@redhat.com>

--- qa/workunits/rest/test-restful.sh
+++ qa/workunits/rest/test-restful.sh
@@ -3,14 +3,8 @@
 mydir=`dirname $0`
 
 secret=`ceph config-key get mgr/restful/keys/admin`
-active=`ceph mgr dump | jq -r .active_name`
-echo "active $active  admin secret $secret"
-
-prefix="mgr/restful/$active"
-addr=`ceph config-key get $prefix/server_addr || echo 127.0.0.1`
-port=`ceph config-key get $prefix/server_port || echo 8003`
-url="https://$addr:$port"
-echo "prefix $prefix url $url"
+url=$(ceph mgr dump|jq -r .services.restful|sed -e 's/\/$//')
+echo "url $url secret $secret"
 $mydir/test_mgr_rest_api.py $url $secret
 
 echo $0 OK
