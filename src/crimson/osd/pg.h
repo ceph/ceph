f5817f3288cf790661d47b657775179ff09f1f5c
Kefu Chai <kchai@redhat.com>
2019-02-22 16:16:58 +0800

    crimson/osd: pass pgid, osdmap and msgr to PG's ctor
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/crimson/osd/pg.h
+++ src/crimson/osd/pg.h
@@ -5,17 +5,41 @@
 
 #include <boost/intrusive_ptr.hpp>
 #include <boost/smart_ptr/intrusive_ref_counter.hpp>
+#include <boost/smart_ptr/local_shared_ptr.hpp>
 #include <seastar/core/future.hh>
 
 #include "osd/osd_types.h"
 
 template<typename T> using Ref = boost::intrusive_ptr<T>;
+class OSDMap;
+
+namespace ceph::net {
+  class Messenger;
+}
 
 class PG : public boost::intrusive_ref_counter<
   PG,
   boost::thread_unsafe_counter>
 {
   using ec_profile_t = std::map<std::string,std::string>;
+  using cached_map_t = boost::local_shared_ptr<OSDMap>;
+
 public:
-  PG(pg_pool_t&& pool, std::string&& name, ec_profile_t&& ec_profile);
+  PG(spg_t pgid,
+     pg_shard_t pg_shard,
+     pg_pool_t&& pool,
+     std::string&& name,
+     ec_profile_t&& ec_profile,
+     cached_map_t osdmap,
+     ceph::net::Messenger* msgr);
+
+  epoch_t get_osdmap_epoch() const;
+  pg_shard_t get_whoami() const;
+
+private:
+  const spg_t pgid;
+  pg_shard_t whoami;
+  pg_pool_t pool;
+  cached_map_t osdmap;
+  ceph::net::Messenger* msgr = nullptr;
 };
