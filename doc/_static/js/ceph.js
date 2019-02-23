2af76ff72e0f288e69077b8b1fc0a830433c2841
Ilya Sukhoplyuev <i.sukhoplyuev@innopolis.university>
2019-02-24 01:43:46 +0300

    add ceph releses select to the documentation

--- doc/_static/js/ceph.js
+++ doc/_static/js/ceph.js
@@ -1,41 +1,70 @@
 $(function() {
-  var releases_url = "http://docs.ceph.com/docs/master/releases.json";
+  var releases_url = "/docs/master/releases.json";
 
-  function show_edit(branch, data) {
-    if (branch) {
-      if (branch === "master") {
-        $("#dev-warning").show();
-        return true;
+  function is_eol(branch, data) {
+    if (data && data.releases && branch in data.releases) {
+      var eol = ("actual_eol" in data.releases[branch]);
+      if (eol) {
+        $("#eol-warning").show();
       }
-      if (data && data.releases && branch in data.releases) {
-        var eol = ("actual_eol" in data.releases[branch]);
-        if (eol) {
-          $("#eol-warning").show();
-        }
-        return !eol;
-      }
+      return !eol;
     }
-    $("#dev-warning").show();
     return false;
   }
 
   function get_branch() {
-    var url = window.location.href;
-    var res = url.match(/docs.ceph.com\/docs\/([a-z]+)\/?/i)
+    var path = window.location.pathname;
+    var res = path.match(/\/docs\/([a-z]+)\/?/i)
     if (res) {
       return res[1]
     }
     return null;
   }
 
+  function show_releases_select(branch, data) {
+
+    var select = $("#ceph-release-select");
+    for (var release in data.releases) {
+      var option = '<option value="' + release + '">' + release + '</option>';
+      select.append(option);
+    }
+    select.append('<option value="master">master</option>');
+    select.val(branch);
+
+    select.change(function (){
+      var tgt_branch = select.val();
+      var pathname = window.location.pathname;
+
+      var tgt_path = pathname.replace(/^\/docs\/([a-z]+)\/?/i, '/docs/' + tgt_branch + '/');
+
+      window.location.pathname = tgt_path;
+    });
+
+    $("#ceph-releases").show()
+  }
+
   $.getJSON(releases_url, function(data) {
     var branch = get_branch();
-    if (show_edit(branch, data)) {
+
+    if (branch === null) {
+      $("#dev-warning").show();
+      return;
+    }
+    if (branch === "master") {
+      $("#dev-warning").show();
+    }
+
+    // show select regardless of eol release
+    show_releases_select(branch, data);
+
+    if (is_eol(branch, data)) {
       // patch the edit-on-github URL for correct branch
       var url = $("#edit-on-github").attr("href");
       url = url.replace("master", branch);
       $("#edit-on-github").attr("href", url);
+
       $("#docubetter").show();
     }
+
   });
 });
