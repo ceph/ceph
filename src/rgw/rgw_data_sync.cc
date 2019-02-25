d9b844b8db76a401386acbfb24988930e8a1b5f7
Casey Bodley <cbodley@redhat.com>
2019-02-25 14:10:39 -0500

    rgw: data sync drains lease stack on lease failure
    on lease failure, call drain_all() to collect the spawned lease stack.
    otherwise, the parent coroutine will inherit this spawned stack and may
    not drain or collect it manually. RGWDataSyncShardControlCR, for
    example, does not drain/collect because it only uses call() to run a
    single RGWDataSyncShardCR, so all lease stacks from RGWDataSyncShardCR
    just pile up in the RGWDataSyncShardControlCR
    
    Fixes: http://tracker.ceph.com/issues/38479
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 11ff8653dcaf7663a0553e3175dec75843b3ccea)

--- src/rgw/rgw_data_sync.cc
+++ src/rgw/rgw_data_sync.cc
@@ -1304,6 +1304,7 @@ public:
         if (lease_cr->is_done()) {
           tn->log(5, "failed to take lease");
           set_status("lease lock failed, early abort");
+          drain_all();
           return set_cr_error(lease_cr->get_ret_status());
         }
         set_sleeping(true);
@@ -1396,6 +1397,7 @@ public:
           if (lease_cr->is_done()) {
             tn->log(5, "failed to take lease");
             set_status("lease lock failed, early abort");
+            drain_all();
             return set_cr_error(lease_cr->get_ret_status());
           }
           set_sleeping(true);
@@ -3186,6 +3188,7 @@ int RGWRunBucketSyncCoroutine::operate()
       if (lease_cr->is_done()) {
         tn->log(5, "failed to take lease");
         set_status("lease lock failed, early abort");
+        drain_all();
         return set_cr_error(lease_cr->get_ret_status());
       }
       set_sleeping(true);
