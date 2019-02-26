sunnyku/ceph-ci

message/MMonMgrReport: conditionally reencode PGMapDigest
The encoding changed between luminous and mimic.  Reencode the
structure if the target doesn't have all the features.

Fixes: http://tracker.ceph.com/issues/38295
Signed-off-by: Sage Weil <sage@redhat.com>

src/messages/MMonMgrReport.h
@@ -19,8 +19,10 @@
 #include "include/types.h"
 #include "include/health.h"
 #include "mon/health_check.h"
+#include "mon/PGMap.h"
 
-class MMonMgrReport : public MessageInstance<MMonMgrReport, PaxosServiceMessage> {
+class MMonMgrReport
+  : public MessageInstance<MMonMgrReport, PaxosServiceMessage> {
 public:
   friend factory;
 private:
@@ -53,6 +55,23 @@ public:
     encode(health_checks, payload);
     encode(service_map_bl, payload);
     encode(progress_events, payload);
+
+    if (!HAVE_FEATURE(features, SERVER_NAUTILUS) ||
+	!HAVE_FEATURE(features, SERVER_MIMIC)) {
+      // PGMapDigest had a backwards-incompatible change between
+      // luminous and mimic, and conditionally encodes based on
+      // provided features, so reencode the one in our data payload.
+      // The mgr isn't able to do this at the time the encoded
+      // PGMapDigest is constructed because we don't know which mon we
+      // will target.  Note that this only triggers if the user
+      // upgrades ceph-mgr before ceph-mon (tsk tsk).
+      PGMapDigest digest;
+      auto p = data.cbegin();
+      decode(digest, p);
+      bufferlist bl;
+      encode(digest, bl, features);
+      data.swap(bl);
+    }
   }
   void decode_payload() override {
     auto p = payload.cbegin();

ceph/ceph

(cherry picked from commit e4ae368ff7a5396194f8bdd5692429af5457998b)
Conflicts:
	src/CMakeLists.txt
        src/messages/MMonMgrReport.h
- mimic has msgr V1
- do not check SERVER_NAUTILUS on mimic
- use begin() instead of cbegin() on mimic

src/messages/MMonMgrReport.h
@@ -19,6 +19,7 @@
 #include "include/types.h"
 #include "include/health.h"
 #include "mon/health_check.h"
+#include "mon/PGMap.h"
 
 class MMonMgrReport : public PaxosServiceMessage {
 
@@ -48,6 +49,22 @@ public:
     paxos_encode();
     encode(health_checks, payload);
     encode(service_map_bl, payload);
+
+    if (!HAVE_FEATURE(features, SERVER_MIMIC)) {
+      // PGMapDigest had a backwards-incompatible change between
+      // luminous and mimic, and conditionally encodes based on
+      // provided features, so reencode the one in our data payload.
+      // The mgr isn't able to do this at the time the encoded
+      // PGMapDigest is constructed because we don't know which mon we
+      // will target.  Note that this only triggers if the user
+      // upgrades ceph-mgr before ceph-mon (tsk tsk).
+      PGMapDigest digest;
+      auto p = data.begin();
+      decode(digest, p);
+      bufferlist bl;
+      encode(digest, bl, features);
+      data.swap(bl);
+    }
   }
   void decode_payload() override {
     bufferlist::iterator p = payload.begin();