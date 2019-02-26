8110d2981ff127b1e66a968b1edec90e762fba19
huangjun <huangjun@xsky.com>
2019-02-26 22:06:22 +0800

    osd: clear osd op reply output only when writes success
    As described in https://github.com/ceph/ceph/commit/18f08cd9c0a3972149eef635fb1035dfc4fcb662,
    if the op is failed, we should return data.
    
    Fixes: http://tracker.ceph.com/issues/38492
    
    Signed-off-by: huangjun <huangjun@xsky.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -1714,7 +1714,7 @@ void OSDService::reply_op_error(OpRequestRef op, int err, eversion_t v,
   int flags;
   flags = m->get_flags() & (CEPH_OSD_FLAG_ACK|CEPH_OSD_FLAG_ONDISK);
 
-  MOSDOpReply *reply = new MOSDOpReply(m, err, osdmap->get_epoch(), flags, true);
+  MOSDOpReply *reply = new MOSDOpReply(m, err, osdmap->get_epoch(), flags, err >= 0);
   reply->set_reply_versions(v, uv);
   m->get_connection()->send_message(reply);
 }
