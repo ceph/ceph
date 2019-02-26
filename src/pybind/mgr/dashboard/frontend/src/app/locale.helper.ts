678e522f549d6046830ff2d4287bdf51d999ac34
Tiago Melo <tmelo@suse.com>
2019-02-26 12:11:59 +0000

    mgr/dashboard: Update I18N translation
    Add support for Chinese (Simplified), Czech, French, Indonesian and Spanish.
    
    Fixed Portuguese (PT) locale source.
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/locale.helper.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/locale.helper.ts
@@ -1,6 +1,11 @@
-import localeDe from '@angular/common/locales/de';
-import localeEn from '@angular/common/locales/en';
-import localePt from '@angular/common/locales/pt';
+import locale_cs from '@angular/common/locales/cs';
+import locale_de from '@angular/common/locales/de';
+import locale_en from '@angular/common/locales/en';
+import locale_es from '@angular/common/locales/es';
+import locale_fr from '@angular/common/locales/fr';
+import locale_id from '@angular/common/locales/id';
+import locale_pt_PT from '@angular/common/locales/pt-PT';
+import locale_zh_Hans from '@angular/common/locales/zh-Hans';
 import { LOCALE_ID, TRANSLATIONS, TRANSLATIONS_FORMAT } from '@angular/core';
 
 declare const require;
@@ -9,12 +14,22 @@ export class LocaleHelper {
   static getBrowserLang(): string {
     const lang = navigator.language;
 
-    if (lang.includes('en')) {
-      return 'en-US';
-    } else if (lang.includes('pt')) {
-      return 'pt-PT';
+    if (lang.includes('cs')) {
+      return 'cs';
     } else if (lang.includes('de')) {
       return 'de-DE';
+    } else if (lang.includes('en')) {
+      return 'en-US';
+    } else if (lang.includes('es')) {
+      return 'es-ES';
+    } else if (lang.includes('fr')) {
+      return 'fr-FR';
+    } else if (lang.includes('id')) {
+      return 'id-ID';
+    } else if (lang.includes('pt')) {
+      return 'pt-PT';
+    } else if (lang.includes('zh')) {
+      return 'zh-CN';
     } else {
       return undefined;
     }
@@ -29,13 +44,28 @@ export class LocaleHelper {
   }
 
   static getLocaleData() {
-    let localeData = localeEn;
+    let localeData = locale_en;
     switch (this.getLocale()) {
-      case 'pt-PT':
-        localeData = localePt;
+      case 'cs':
+        localeData = locale_cs;
         break;
       case 'de-DE':
-        localeData = localeDe;
+        localeData = locale_de;
+        break;
+      case 'es-ES':
+        localeData = locale_es;
+        break;
+      case 'fr-FR':
+        localeData = locale_fr;
+        break;
+      case 'id-ID':
+        localeData = locale_id;
+        break;
+      case 'pt-PT':
+        localeData = locale_pt_PT;
+        break;
+      case 'zh-CN':
+        localeData = locale_zh_Hans;
         break;
     }
     return localeData;
