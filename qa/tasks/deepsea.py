b71ca8af2ba63606871b2db540f293cc1e2476c3
Nathan Cutler <ncutler@suse.com>
2019-02-26 12:45:59 +0100

    qa/tier1/health-ok: add prometheus and grafana roles
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- qa/tasks/deepsea.py
+++ qa/tasks/deepsea.py
@@ -1408,6 +1408,8 @@ class Policy(DeepSea):
             self._build_x('rgw')
             self._build_x('igw')
             self._build_x('ganesha')
+            self._build_x('prometheus')
+            self._build_x('grafana')
             self._build_storage_profile()
             self._write_policy_cfg()
             self._cat_policy_cfg()
