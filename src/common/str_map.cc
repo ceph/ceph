9ae76f13df2a8f773944b7ec4d849700228d567f
Sage Weil <sage@redhat.com>
2019-02-28 10:38:54 -0600

    common/str_map: fix trim() on empty string
    If was pass an empty string (e.g., with something like
    get_str_map("M= T= P=")) we end up with a (size_t)-1 for end.
    
    Fixes: http://tracker.ceph.com/issues/38329
    Signed-off-by: Sage Weil <sage@redhat.com>
    (cherry picked from commit 16ffca6f0a23a96284a7d17c890047121d32058e)

--- src/common/str_map.cc
+++ src/common/str_map.cc
@@ -57,6 +57,9 @@ int get_json_str_map(
   return 0;
 }
 string trim(const string& str) {
+  if (str.empty()) {
+    return str;
+  }
   size_t start = 0;
   size_t end = str.size() - 1;
   while (isspace(str[start]) != 0 && start <= end) {
