b6075fdaa833c3bc7ac42437e750394808617c16
Kefu Chai <kchai@redhat.com>
2019-03-04 13:09:40 +0800

    crimson/mon: implement Dispatcher::ms_get_authorizer()
    we need this method for supporting cephx authentication
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/crimson/mon/MonClient.h
+++ src/crimson/mon/MonClient.h
@@ -90,6 +90,7 @@ private:
   seastar::future<> ms_dispatch(ceph::net::ConnectionRef conn,
 				MessageRef m) override;
   seastar::future<> ms_handle_reset(ceph::net::ConnectionRef conn) override;
+  AuthAuthorizer* ms_get_authorizer(peer_type_t peer) const override;
 
   seastar::future<> handle_monmap(ceph::net::ConnectionRef conn,
 				  Ref<MMonMap> m);
