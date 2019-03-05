2a4fe208256eb3a7d40b15fd71e8c2073e53e30a
Abhishek Lekshmanan <abhishek@suse.com>
2019-03-05 13:35:50 +0100

    cls_rgw: alias the LC entries as  rgw_lc_entry_t
    instead of a naked std::pair<std::string, int> everywhere
    
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>
    (cherry picked from commit 9e1d2a28807fc709b5c9f86132609841e453ae45)

--- src/cls/rgw/cls_rgw_ops.h
+++ src/cls/rgw/cls_rgw_ops.h
@@ -1000,9 +1000,10 @@ struct cls_rgw_lc_get_next_entry_op {
 };
 WRITE_CLASS_ENCODER(cls_rgw_lc_get_next_entry_op)
 
-struct cls_rgw_lc_get_next_entry_ret {
-  pair<string, int> entry;
+using rgw_lc_entry_t = std::pair<std::string, int>;
 
+struct cls_rgw_lc_get_next_entry_ret {
+  rgw_lc_entry_t entry;
   cls_rgw_lc_get_next_entry_ret() {}
 
   void encode(bufferlist& bl) const {
@@ -1021,7 +1022,7 @@ struct cls_rgw_lc_get_next_entry_ret {
 WRITE_CLASS_ENCODER(cls_rgw_lc_get_next_entry_ret)
 
 struct cls_rgw_lc_rm_entry_op {
-  pair<string, int> entry;
+  rgw_lc_entry_t entry;
   cls_rgw_lc_rm_entry_op() {}
 
   void encode(bufferlist& bl) const {
@@ -1039,7 +1040,7 @@ struct cls_rgw_lc_rm_entry_op {
 WRITE_CLASS_ENCODER(cls_rgw_lc_rm_entry_op)
 
 struct cls_rgw_lc_set_entry_op {
-  pair<string, int> entry;
+  rgw_lc_entry_t entry;
   cls_rgw_lc_set_entry_op() {}
 
   void encode(bufferlist& bl) const {
