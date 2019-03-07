e68f1acb695a6b94ddd799b06a472bdf93d71813
Yan, Zheng <zyan@redhat.com>
2019-03-07 16:30:32 +0800

    mds: avoid revoking Fsx from loner during directory fragmentation
    MDS wrlock filelock during directory fragmentation and subtree migration.
    The purpose of the wrlock is preventing scatter gather process. MDS does
    not modify directory contents after filelock is locked. MDS can pretend
    that wrlock is being acquired by loner. So it has a higher change to get
    the wrlock.
    
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

--- src/mds/Locker.h
+++ src/mds/Locker.h
@@ -110,7 +110,7 @@ public:
   void rdlock_take_set(MutationImpl::LockOpVec& lov, MutationRef& mut);
 
   void wrlock_force(SimpleLock *lock, MutationRef& mut);
-  bool wrlock_start(SimpleLock *lock, MDRequestRef& mut, bool nowait=false);
+  bool wrlock_start(const MutationImpl::LockOp &op, MDRequestRef& mut, bool nowait=false);
   void wrlock_finish(const MutationImpl::lock_iterator& it, MutationImpl *mut, bool *pneed_issue);
 
   void remote_wrlock_start(SimpleLock *lock, mds_rank_t target, MDRequestRef& mut);
