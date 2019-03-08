0385ee4db1ca04b3b971f55951a21fa81cdeaeb8
Sage Weil <sage@redhat.com>
2019-03-08 17:06:05 -0600

    qa/standalone/osd/osd-rep-recov-eio: fix better
    - no need for the default pool size
    - no initial osds or it will collide with setup_osds later
    - no need for rbd pool at all
    
    Signed-off-by: Sage Weil <sage@redhat.com>
    (cherry picked from commit bf74c1adc4c5df7b11217eebfbfa2f6d81bdd357)
    
    Conflicts:
    	qa/standalone/osd/osd-rep-recov-eio.sh (no TEST_rep_read_unfound() test)

--- qa/standalone/osd/osd-rep-recov-eio.sh
+++ qa/standalone/osd/osd-rep-recov-eio.sh
@@ -27,15 +27,14 @@ function run() {
     export CEPH_ARGS
     CEPH_ARGS+="--fsid=$(uuidgen) --auth-supported=none "
     CEPH_ARGS+="--mon-host=$CEPH_MON "
-    CEPH_ARGS+="--osd-objectstore=filestore --osd_pool_default_size=1 "
+    CEPH_ARGS+="--osd-objectstore=filestore "
 
     local funcs=${@:-$(set | sed -n -e 's/^\(TEST_[0-9a-z_]*\) .*/\1/p')}
     for func in $funcs ; do
         setup $dir || return 1
         run_mon $dir a || return 1
 	run_mgr $dir x || return 1
-	run_osd $dir 0 || return 1
-	create_rbd_pool || return 1
+	ceph osd pool create foo 8 || return 1
 
         $func $dir || return 1
         teardown $dir || return 1
