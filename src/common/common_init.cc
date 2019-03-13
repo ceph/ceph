3ebd59b872e3a77430b93bb51f9232868975b3bb
Sage Weil <sage@redhat.com>
2019-03-13 13:53:52 -0500

    msg: default to debug_ms=0
    These have a measurable impact on performance and have a limited (although
    not non-existent!) track record of providing debugging value.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/common/common_init.cc
+++ src/common/common_init.cc
@@ -66,10 +66,6 @@ CephContext *common_preinit(const CephInitParameters &iparams,
     conf.set_val_default("err_to_stderr", "false");
     conf.set_val_default("log_flush_on_exit", "false");
   }
-  if (code_env != CODE_ENVIRONMENT_DAEMON) {
-    // NOTE: disable ms subsystem gathering in clients by default
-    conf.set_val_default("debug_ms", "0/0");
-  }
 
   return cct;
 }
