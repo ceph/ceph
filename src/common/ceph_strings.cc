241d00b9e8732584e0555c252a441abb1adb4330
Sage Weil <sage@redhat.com>
2019-03-16 15:01:40 -0500

    ceph_release: the next release will be octopus
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/common/ceph_strings.cc
+++ src/common/ceph_strings.cc
@@ -101,6 +101,8 @@ const char *ceph_release_name(int r)
 		return "mimic";
 	case CEPH_RELEASE_NAUTILUS:
 		return "nautilus";
+	case CEPH_RELEASE_OCTOPUS:
+		return "octopus";
 	default:
 		if (r < 0)
 			return "unspecified";
@@ -112,6 +114,9 @@ int ceph_release_from_name(const char *s)
 {
 	if (!s) {
 		return -1;
+	}
+	if (strcmp(s, "octopus") == 0) {
+		return CEPH_RELEASE_OCTOPUS;
 	}
 	if (strcmp(s, "nautilus") == 0) {
 		return CEPH_RELEASE_NAUTILUS;
