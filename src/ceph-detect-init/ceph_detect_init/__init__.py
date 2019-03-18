05d6b9f5b2f5d9cf9215af3aa27c1eb90b50a038
Andrey Bychkov <mrdrew@altlinux.org>
2019-03-18 12:47:48 +0300

    ceph_detect_init: Add support for ALT Linux
    Signed-off-by: Andrey Bychkov <mrdrew@altlinux.org>

--- src/ceph-detect-init/ceph_detect_init/__init__.py
+++ src/ceph-detect-init/ceph_detect_init/__init__.py
@@ -14,6 +14,7 @@
 # GNU Library Public License for more details.
 #
 from ceph_detect_init import alpine
+from ceph_detect_init import alt
 from ceph_detect_init import arch
 from ceph_detect_init import centos
 from ceph_detect_init import debian
@@ -61,6 +62,7 @@ def _get_distro(distro, use_rhceph=False):
     distro = _normalized_distro_name(distro)
     distributions = {
         'alpine': alpine,
+        'alt': alt,
         'arch': arch,
         'debian': debian,
         'ubuntu': debian,
@@ -104,6 +106,8 @@ def _normalized_distro_name(distro):
         return 'gentoo'
     elif distro.startswith('virtuozzo'):
         return 'virtuozzo'
+    elif distro.startswith(('alt', 'altlinux', 'basealt', 'alt linux')):
+        return 'alt'
     return distro
 
 
