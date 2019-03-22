1e4ccc4e7dcb9dc9d37d49a93174cc42a3dbcf98
Stephan Müller <smueller@suse.com>
2019-03-22 17:46:35 +0100

    mgr/dashboard: Localization for date picker module
    Fixes: https://tracker.ceph.com/issues/39037
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/language-selector/language-selector.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/language-selector/language-selector.component.spec.ts
@@ -1,8 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 
+import { listLocales } from 'ngx-bootstrap/chronos';
+import { BsLocaleService } from 'ngx-bootstrap/datepicker';
+
 import { configureTestBed } from '../../../../testing/unit-test-helper';
-import { LocaleHelper } from '../../../locale.helper';
 import { LanguageSelectorComponent } from './language-selector.component';
 
 describe('LanguageSelectorComponent', () => {
@@ -11,6 +13,7 @@ describe('LanguageSelectorComponent', () => {
 
   configureTestBed({
     declarations: [LanguageSelectorComponent],
+    providers: [BsLocaleService],
     imports: [FormsModule]
   });
 
@@ -18,6 +21,7 @@ describe('LanguageSelectorComponent', () => {
     fixture = TestBed.createComponent(LanguageSelectorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    spyOn(window.location, 'reload').and.callFake(() => component.ngOnInit());
   });
 
   it('should create', () => {
@@ -26,14 +30,44 @@ describe('LanguageSelectorComponent', () => {
 
   it('should read current language', () => {
     expect(component.selectedLanguage).toBe('en-US');
+    expect(listLocales()).toEqual([]);
   });
 
-  it('should change language', () => {
-    // Removes error in jsdom
-    window.location.reload = jest.fn();
+  const expectLanguageChange = (lang) => {
+    component.changeLanguage(lang);
+    expect(component.selectedLanguage).toBe(lang);
+    expect(listLocales().includes(lang.slice(0, 2))).toBe(true);
+  };
 
-    expect(LocaleHelper.getLocale()).toBe('en-US');
-    component.changeLanguage('pt-PT');
-    expect(LocaleHelper.getLocale()).toBe('pt-PT');
+  it('should change to cs', () => {
+    expectLanguageChange('cs');
+  });
+
+  it('should change to de-DE', () => {
+    expectLanguageChange('de-DE');
+  });
+
+  it('should change to es-ES', () => {
+    expectLanguageChange('es-ES');
+  });
+
+  it('should change to fr-FR', () => {
+    expectLanguageChange('fr-FR');
+  });
+
+  it('should change to id-ID', () => {
+    expectLanguageChange('id-ID');
+  });
+
+  it('should change to pl-PL', () => {
+    expectLanguageChange('pl-PL');
+  });
+
+  it('should change to pt-PT', () => {
+    expectLanguageChange('pt-PT');
+  });
+
+  it('should change to zh-CN', () => {
+    expectLanguageChange('zh-CN');
   });
 });
--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/language-selector/supported-languages.enum.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/language-selector/supported-languages.enum.ts
@@ -1,3 +1,16 @@
+import {
+  csLocale,
+  deLocale,
+  esLocale,
+  frLocale,
+  idLocale,
+  plLocale,
+  ptBrLocale,
+  zhCnLocale
+} from 'ngx-bootstrap/chronos';
+
+// When adding a new supported language make sure to add a test for it in:
+// language-selector.component.spec.ts
 export enum SupportedLanguages {
   'cs' = 'Čeština',
   'de-DE' = 'Deutsch',
@@ -9,3 +22,16 @@ export enum SupportedLanguages {
   'es-ES' = 'Español',
   'zh-CN' = '中文'
 }
+
+// Supported languages:
+// https://github.com/valor-software/ngx-bootstrap/tree/development/src/chronos/i18n
+export let languageBootstrapMapping = {
+  cs: csLocale,
+  de: deLocale,
+  es: esLocale,
+  fr: frLocale,
+  id: idLocale,
+  pl: plLocale,
+  pt: ptBrLocale,
+  zh: zhCnLocale
+};
