ceph/ceph

commit 6cb23f9ce6b3848a7fd93d135acc143f2ae7cba1

pybind: encode flattened dict
in python3, the keys and values in dict are unicode strings, so we need
to encode them before passing them to underlying librados' C API.

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/pybind/rados/rados.pyx
@@ -558,7 +558,7 @@ def decode_cstr(val, encoding="utf-8"):
 
 def flatten_dict(d):
     items = chain.from_iterable(d.items())
-    return ''.join(i + '\0' for i in items)
+    return cstr(''.join(i + '\0' for i in items))
 
 
 cdef char* opt_str(s) except? NULL:

commit dba8326bdf5c9cda08d5bc70640371220bb18073

pybind/rados: should pass "name" to cstr()
it's a regression introduced by 6cb23f9c

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/pybind/rados/rados.pyx
@@ -556,9 +556,9 @@ def decode_cstr(val, encoding="utf-8"):
     return val.decode(encoding)
 
 
-def flatten_dict(d):
+def flatten_dict(d, name):
     items = chain.from_iterable(d.items())
-    return cstr(''.join(i + '\0' for i in items))
+    return cstr(''.join(i + '\0' for i in items), name)
 
 
 cdef char* opt_str(s) except? NULL:
@@ -1532,7 +1532,7 @@ Rados object in state %s." % self.state)
         """
         service = cstr(service, 'service')
         daemon = cstr(daemon, 'daemon')
-        metadata_dict = flatten_dict(metadata)
+        metadata_dict = flatten_dict(metadata, 'metadata')
         cdef:
             char *_service = service
             char *_daemon = daemon
@@ -1545,7 +1545,7 @@ Rados object in state %s." % self.state)
 
     @requires(('metadata', dict))
     def service_daemon_update(self, status):
-        status_dict = flatten_dict(status)
+        status_dict = flatten_dict(status, 'status')
         cdef:
             char *_status = status_dict
 