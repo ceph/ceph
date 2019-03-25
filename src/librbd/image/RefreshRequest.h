7999933d06bae006eeaf6abd4fd4b8f8a02d640f
Mykola Golub <mgolub@suse.com>
2019-03-25 06:54:46 +0000

    librbd: look for pool metadata in default namespace
    when applying pool level config overrides
    
    Fixes: https://tracker.ceph.com/issues/38928
    Signed-off-by: Mykola Golub <mgolub@suse.com>

--- src/librbd/image/RefreshRequest.h
+++ src/librbd/image/RefreshRequest.h
@@ -145,6 +145,7 @@ private:
   uint64_t m_flags = 0;
   uint64_t m_op_features = 0;
 
+  librados::IoCtx m_pool_metadata_io_ctx;
   std::string m_last_metadata_key;
   std::map<std::string, bufferlist> m_metadata;
 
