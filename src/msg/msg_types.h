sunnyku/ceph-ci

msg/msg_types: make legacy_equals() symmetrical
The prior version would return true for

 [v2,v1] vs [v2,v1]
 [v2,v1] vs [v1]

but not

 [v1] vs [v2,v1]

Now that final case is also true, which means a.legacy_equals(b) ==
b.legacy_equals(a) for any combination.  This is what you would expect
for an equality operator... even a fuzzy one.

Signed-off-by: Sage Weil <sage@redhat.com>

src/msg/msg_types.h
@@ -669,6 +669,11 @@ struct entity_addrvec_t {
 	front() == o.legacy_addr()) {
       return true;
     }
+    if (o.v.size() == 1 &&
+	v.front().is_legacy() &&
+	v.front() == legacy_addr()) {
+      return true;
+    }
     return false;
   }
 

ceph/ceph

src/msg/msg_types.h
@@ -669,6 +669,11 @@ struct entity_addrvec_t {
 	front() == o.legacy_addr()) {
       return true;
     }
+    if (o.v.size() == 1 &&
+	o.front().is_legacy() &&
+	o.front() == legacy_addr()) {
+      return true;
+    }
     return false;
   }
 