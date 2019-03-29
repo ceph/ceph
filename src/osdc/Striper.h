3903cdfe71e7aa07490b0fcbf97706ea27abb4ad
Adam C. Emerson <aemerson@redhat.com>
2019-03-28 20:31:17 -0400

    osdc: Update Striper to work without using namespace
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/osdc/Striper.h
+++ src/osdc/Striper.h
@@ -25,28 +25,28 @@ class CephContext;
   class Striper {
   public:
     /*
-     * map (ino, layout, offset, len) to a (list of) ObjectExtents (byte
+     * std::map (ino, layout, offset, len) to a (list of) ObjectExtents (byte
      * ranges in objects on (primary) osds)
      */
     static void file_to_extents(CephContext *cct, const char *object_format,
 				const file_layout_t *layout,
 				uint64_t offset, uint64_t len,
 				uint64_t trunc_size,
-				map<object_t, vector<ObjectExtent> >& extents,
+				std::map<object_t, std::vector<ObjectExtent> >& extents,
 				uint64_t buffer_offset=0);
 
     static void file_to_extents(CephContext *cct, const char *object_format,
 				const file_layout_t *layout,
 				uint64_t offset, uint64_t len,
 				uint64_t trunc_size,
-				vector<ObjectExtent>& extents,
+				std::vector<ObjectExtent>& extents,
 				uint64_t buffer_offset=0);
 
     static void file_to_extents(CephContext *cct, inodeno_t ino,
 				const file_layout_t *layout,
 				uint64_t offset, uint64_t len,
 				uint64_t trunc_size,
-				vector<ObjectExtent>& extents) {
+				std::vector<ObjectExtent>& extents) {
       // generate prefix/format
       char buf[32];
       snprintf(buf, sizeof(buf), "%llx.%%08llx", (long long unsigned)ino);
@@ -55,15 +55,15 @@ class CephContext;
     }
 
     static void assimilate_extents(
-      map<object_t, vector<ObjectExtent> >& object_extents,
-      vector<ObjectExtent>& extents);
+      std::map<object_t, std::vector<ObjectExtent> >& object_extents,
+      std::vector<ObjectExtent>& extents);
 
     /**
-     * reverse map an object extent to file extents
+     * reverse std::map an object extent to file extents
      */
     static void extent_to_file(CephContext *cct, file_layout_t *layout,
 			       uint64_t objectno, uint64_t off, uint64_t len,
-			       vector<pair<uint64_t, uint64_t> >& extents);
+			       std::vector<std::pair<uint64_t, uint64_t> >& extents);
 
     static uint64_t object_truncate_size(
       CephContext *cct, const file_layout_t *layout,
@@ -76,28 +76,28 @@ class CephContext;
      */
     class StripedReadResult {
       // offset -> (data, intended length)
-      map<uint64_t, pair<bufferlist, uint64_t> > partial;
+      std::map<uint64_t, std::pair<ceph::buffer::list, uint64_t> > partial;
       uint64_t total_intended_len = 0; //sum of partial.second.second
 
     public:
       void add_partial_result(
-	CephContext *cct, bufferlist& bl,
-	const vector<pair<uint64_t,uint64_t> >& buffer_extents);
+	CephContext *cct, ceph::buffer::list& bl,
+	const std::vector<std::pair<uint64_t,uint64_t> >& buffer_extents);
       /**
        * add sparse read into results
        *
        * @param bl buffer
-       * @param bl_map map of which logical source extents this covers
+       * @param bl_map std::map of which logical source extents this covers
        * @param bl_off logical buffer offset (e.g., first bl_map key
        *               if the buffer is not sparse)
        * @param buffer_extents output buffer extents the data maps to
        */
       void add_partial_sparse_result(
-	CephContext *cct, bufferlist& bl,
-	const map<uint64_t, uint64_t>& bl_map, uint64_t bl_off,
-	const vector<pair<uint64_t,uint64_t> >& buffer_extents);
+	CephContext *cct, ceph::buffer::list& bl,
+	const std::map<uint64_t, uint64_t>& bl_map, uint64_t bl_off,
+	const std::vector<std::pair<uint64_t,uint64_t> >& buffer_extents);
 
-      void assemble_result(CephContext *cct, bufferlist& bl, bool zero_tail);
+      void assemble_result(CephContext *cct, ceph::buffer::list& bl, bool zero_tail);
 
       /**
        * @buffer copy read data into buffer
