8fcdb4d628f3a55e4783f0a3df7ac7c0a36eaa43
Jan Fajerski <jfajerski@suse.com>
2019-04-11 16:58:32 +0200

    ceph-volume: batch ensure device lists are disjoint
    Fixes: https://tracker.ceph.com/issues/41018
    
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>

--- src/ceph-volume/ceph_volume/devices/lvm/batch.py
+++ src/ceph-volume/ceph_volume/devices/lvm/batch.py
@@ -254,7 +254,7 @@ class Batch(object):
         self.args = parser.parse_args(argv)
         self.parser = parser
         for dev_list in ['', 'db_', 'wal_', 'journal_']:
-            setattr(self, '{}usable'.format(dev_list), [])
+            setattr(self, '{}usable'.format(dev_list), set())
 
     def get_devices(self):
         # remove devices with partitions
@@ -327,6 +327,7 @@ class Batch(object):
     def _get_explicit_strategy(self):
         # TODO assert that none of the device lists overlap?
         self._filter_devices()
+        self._ensure_disjoint_device_lists()
         if self.args.bluestore:
             if self.db_usable or self.wal_usable:
                 self.strategy = strategies.bluestore.MixedType(
@@ -360,8 +361,17 @@ class Batch(object):
             dev_list_prop = '{}devices'.format(dev_list)
             if hasattr(self.args, dev_list_prop):
                 usable_dev_list_prop = '{}usable'.format(dev_list)
-                usable = [d for d in getattr(self.args, dev_list_prop) if d.available]
+                usable = set([d for d in getattr(self.args, dev_list_prop) if
+                              d.available])
                 setattr(self, usable_dev_list_prop, usable)
                 self.filtered_devices.update({d: used_reason for d in
                                               getattr(self.args, dev_list_prop)
                                               if d.used_by_ceph})
+
+    def _ensure_disjoint_device_lists(self):
+        # check that all device lists are disjoint with each other
+        if not(self.usable.isdisjoint(self.db_usable) and
+               self.usable.isdisjoint(self.wal_usable) and
+               self.usable.isdisjoint(self.journal_usable) and
+               self.db_usable.isdisjoint(self.wal_usable)):
+            raise Exception('Device lists are not disjoint')
