ba1c64bad15fe26ca26253bc14c063ed740ea567
Sage Weil <sage@redhat.com>
2019-04-12 08:55:45 -0500

    osd/osd_types: fix object_stat_sum_t fast-path decode
    This needs to match the latest object version number.
    
    Fixes d2ca3d2feb442f97ca89023c7d01178d96f517a6
    
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/osd_types.cc
+++ src/osd/osd_types.cc
@@ -2325,7 +2325,7 @@ void object_stat_sum_t::decode(ceph::buffer::list::const_iterator& bl)
   bool decode_finish = false;
   DECODE_START(20, bl);  // make sure to also update fast decode below
 #if defined(CEPH_LITTLE_ENDIAN)
-  if (struct_v >= 19) {  // this must match newest decode version
+  if (struct_v >= 20) {  // this must match newest decode version
     bl.copy(sizeof(object_stat_sum_t), (char*)(&num_bytes));
     decode_finish = true;
   }
