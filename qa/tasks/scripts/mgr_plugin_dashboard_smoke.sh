0a50d7c06eec566723c824551803b8ce502785e2
Shyukri Shyukriev <shshyukriev@suse.com>
2019-04-17 17:43:58 +0300

    qa:mgr:dashboard - simple check for SUSE branding
    Signed-off-by: Shyukri Shyukriev <shshyukriev@suse.com>

--- qa/tasks/scripts/mgr_plugin_dashboard_smoke.sh
+++ qa/tasks/scripts/mgr_plugin_dashboard_smoke.sh
@@ -9,4 +9,6 @@ URL=$(ceph mgr services 2>/dev/null | jq .dashboard | sed -e 's/"//g')
 curl --insecure --silent $URL 2>&1 > dashboard.html
 test -s dashboard.html
 file dashboard.html | grep "HTML document"
+# verify SUSE branding (in title and suse_favicon)
+grep -i "suse" dashboard.html
 echo "OK" >/dev/null
