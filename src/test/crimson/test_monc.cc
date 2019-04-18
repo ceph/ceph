5952e9809d013f08d1327fd8088d1f866dc27a50
Kefu Chai <kchai@redhat.com>
2019-04-18 22:48:01 +0800

    crimson: add Messenger::{set,get}_require_authorizer()
    This flag is used for compatibility with pre-nautilus OSDs, which do not
    send authorizers on the OSD heartbeat connections.  However, because the
    AuthServer is implemented by MonClient, which is shared across all
    OSD messengers, we can't set this to false for the OSD without disabling
    all auth.  Instead, make it a Messenger property, and set it only on the
    heartbeat server messengers.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/test/crimson/test_monc.cc
+++ src/test/crimson/test_monc.cc
@@ -15,7 +15,8 @@ class DummyAuthHandler : public ceph::common::AuthHandler {
 public:
   void handle_authentication(const EntityName& name,
                              uint64_t global_id,
-                             const AuthCapsInfo& caps) override {}
+                             const AuthCapsInfo& caps) final
+  {}
 };
 
 DummyAuthHandler dummy_handler;
