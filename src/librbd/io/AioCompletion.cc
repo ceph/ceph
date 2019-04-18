be419a1d6fd1e176ab4fdebfaa17726bb63d6ac8
Jason Dillaman <dillaman@redhat.com>
2019-04-18 13:17:56 -0400

    librbd: async open/close should free ImageCtx before issuing callback
    The destructor for ImageCtx attempts to access librados::IoCtx objects
    that might have been destroyed immediately after the async open (failure)
    and close actions completed.
    
    Fixes: http://tracker.ceph.com/issues/39031
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/io/AioCompletion.cc
+++ src/librbd/io/AioCompletion.cc
@@ -82,6 +82,13 @@ void AioCompletion::complete() {
     }
   }
 
+  if ((aio_type == AIO_TYPE_CLOSE) ||
+      (aio_type == AIO_TYPE_OPEN && rval < 0)) {
+    // must destroy ImageCtx prior to invoking callback
+    delete ictx;
+    ictx = nullptr;
+  }
+
   state = AIO_STATE_CALLBACK;
   if (complete_cb) {
     lock.Unlock();
@@ -89,7 +96,7 @@ void AioCompletion::complete() {
     lock.Lock();
   }
 
-  if (event_notify && ictx->event_socket.is_valid()) {
+  if (ictx != nullptr && event_notify && ictx->event_socket.is_valid()) {
     ictx->completed_reqs_lock.Lock();
     ictx->completed_reqs.push_back(&m_xlist_item);
     ictx->completed_reqs_lock.Unlock();
@@ -119,6 +126,12 @@ void AioCompletion::start_op(bool ignore_type) {
   Mutex::Locker locker(lock);
   ceph_assert(ictx != nullptr);
   ceph_assert(!async_op.started());
+
+  if (aio_type == AIO_TYPE_OPEN || aio_type == AIO_TYPE_CLOSE) {
+    // no need to track async open/close operations
+    return;
+  }
+
   if (state == AIO_STATE_PENDING &&
       (ignore_type || aio_type != AIO_TYPE_FLUSH)) {
     async_op.start_op(*ictx);
