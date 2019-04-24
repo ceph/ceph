71f6aef81eb15ac733a92c390c557f7b1e9ee305
Radoslaw Zarzynski <rzarzyns@redhat.com>
2019-04-24 18:17:44 -0400

    common: add initialization code for old libcrypto versions.
    This is based on Civetweb's. Modern OpenSSLs don't require
    the extra calls as they are self-initializing [1][2].
    
    [1]: https://wiki.openssl.org/index.php/Library_Initialization
    [2]: https://www.openssl.org/docs/faq.html#PROG1
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/common/ceph_crypto.cc
+++ src/common/ceph_crypto.cc
@@ -26,16 +26,166 @@
 
 #ifdef USE_OPENSSL
 #include <openssl/evp.h>
+
+# if OPENSSL_VERSION_NUMBER < 0x10100000L
+#   include <openssl/conf.h>
+#   include <openssl/engine.h>
+#   include <openssl/err.h>
+# endif /* OPENSSL_VERSION_NUMBER < 0x10100000L */
+
+namespace ceph::crypto::ssl {
+
+# if OPENSSL_VERSION_NUMBER < 0x10100000L
+static std::atomic_uint32_t crypto_refs;
+
+// XXX: vector instead?
+static pthread_mutex_t* ssl_mutexes;
+static size_t ssl_num_locks;
+
+static struct {
+  // we could use e.g. unordered_set instead at the price of providing
+  // std::hash<...> specialization. However, we can live with duplicates
+  // quite well while the benefit is not worth the effort.
+  std::vector<CRYPTO_THREADID> tids;
+  ceph::mutex lock = ceph::make_mutex("crypto::ssl::init_records::lock");;
+} init_records;
+
+static void
+ssl_locking_callback(int mode, int mutex_num, const char *file, int line)
+{
+  static_cast<void>(line);
+  static_cast<void>(file);
+
+  if (mode & 1) {
+    /* 1 is CRYPTO_LOCK */
+    [[maybe_unused]] auto r = pthread_mutex_lock(&ssl_mutexes[mutex_num]);
+  } else {
+    [[maybe_unused]] auto r = pthread_mutex_unlock(&ssl_mutexes[mutex_num]);
+  }
+}
+
+static unsigned long
+ssl_get_thread_id(void)
+{
+  static_assert(sizeof(unsigned long) >= sizeof(pthread_t));
+  /* pthread_t may be any data type, so a simple cast to unsigned long
+   * can rise a warning/error, depending on the platform.
+   * Here memcpy is used as an anything-to-anything cast. */
+  unsigned long ret = 0;
+  pthread_t t = pthread_self();
+  memcpy(&ret, &t, sizeof(pthread_t));
+  return ret;
+}
+# endif /* not OPENSSL_VERSION_NUMBER < 0x10100000L */
+
+static void init() {
+#if OPENSSL_VERSION_NUMBER < 0x10100000L
+  if (++crypto_refs == 1) {
+    // according to
+    // https://wiki.openssl.org/index.php/Library_Initialization#libcrypto_Initialization
+    OpenSSL_add_all_algorithms();
+    ERR_load_crypto_strings();
+
+    ssl_num_locks = std::max(CRYPTO_num_locks(), 0);
+    if (ssl_num_locks > 0) {
+      try {
+        ssl_mutexes = new pthread_mutex_t[ssl_num_locks];
+      } catch (...) {
+        ceph_assert_always("can't allocate memory for OpenSSL init" == nullptr);
+      }
+    }
+
+    pthread_mutexattr_t pthread_mutex_attr;
+    pthread_mutexattr_init(&pthread_mutex_attr);
+    pthread_mutexattr_settype(&pthread_mutex_attr, PTHREAD_MUTEX_RECURSIVE);
+
+    for (size_t i = 0; i < ssl_num_locks; i++) {
+      pthread_mutex_init(&ssl_mutexes[i], &pthread_mutex_attr);
+    }
+
+    // initialize locking callbacks, needed for thread safety.
+    // http://www.openssl.org/support/faq.html#PROG1
+    CRYPTO_set_locking_callback(&ssl_locking_callback);
+    CRYPTO_set_id_callback(&ssl_get_thread_id);
+
+    OPENSSL_config(nullptr);
+  }
+
+  // we need to record IDs of all threads calling the initialization in
+  // order to *manually* free per-thread memory OpenSSL *automagically*
+  // allocated in ERR_get_state().
+  // XXX: this solution/nasty hack is IMPERFECT. A leak will appear when
+  // a client init()ializes the crypto subsystem with one thread and then
+  // uses it from another one in a way that results in ERR_get_state().
+  // XXX: for discussion about another approaches please refer to:
+  // https://www.mail-archive.com/openssl-users@openssl.org/msg59070.html
+  {
+    std::lock_guard l(init_records.lock);
+    CRYPTO_THREADID tmp;
+    CRYPTO_THREADID_current(&tmp);
+    init_records.tids.emplace_back(std::move(tmp));
+  }
+# endif /* OPENSSL_VERSION_NUMBER < 0x10100000L */
+}
+
+static void shutdown() {
+# if OPENSSL_VERSION_NUMBER < 0x10100000L
+  if (--crypto_refs != 0) {
+    return;
+  }
+
+  // drop error queue for each thread that called the init() function to
+  // satisfy valgrind.
+  {
+    std::lock_guard l(init_records.lock);
+
+    // NOTE: in OpenSSL 1.0.2g the signature is:
+    //    void ERR_remove_thread_state(const CRYPTO_THREADID *tid);
+    // but in 1.1.0j it has been changed to
+    //    void ERR_remove_thread_state(void *);
+    // We're basing on the OPENSSL_VERSION_NUMBER check to preserve
+    // const-correctness without failing builds on modern envs.
+    for (const auto& tid : init_records.tids) {
+      ERR_remove_thread_state(&tid);
+    }
+  }
+
+  // Shutdown according to
+  // https://wiki.openssl.org/index.php/Library_Initialization#Cleanup
+  // http://stackoverflow.com/questions/29845527/how-to-properly-uninitialize-openssl
+  //
+  // The call to CONF_modules_free() has been introduced after a valgring run.
+  CRYPTO_set_locking_callback(nullptr);
+  CRYPTO_set_id_callback(nullptr);
+  ENGINE_cleanup();
+  CONF_modules_free();
+  CONF_modules_unload(1);
+  ERR_free_strings();
+  EVP_cleanup();
+  CRYPTO_cleanup_all_ex_data();
+
+  for (size_t i = 0; i < ssl_num_locks; i++) {
+    pthread_mutex_destroy(&ssl_mutexes[i]);
+  }
+  delete[] ssl_mutexes;
+  ssl_mutexes = nullptr;
+# endif /* OPENSSL_VERSION_NUMBER < 0x10100000L */
+}
+
+} // namespace ceph::crypto::openssl
 #endif /*USE_OPENSSL*/
 
 #ifdef USE_NSS
+
+namespace ceph::crypto::nss {
 
 static pthread_mutex_t crypto_init_mutex = PTHREAD_MUTEX_INITIALIZER;
 static uint32_t crypto_refs = 0;
 static NSSInitContext *crypto_context = NULL;
 static pid_t crypto_init_pid = 0;
 
-void ceph::crypto::init(CephContext *cct)
+
+static void init(CephContext *cct)
 {
   pid_t pid = getpid();
   pthread_mutex_lock(&crypto_init_mutex);
@@ -62,7 +212,7 @@ void ceph::crypto::init(CephContext *cct)
   ceph_assert_always(crypto_context != NULL);
 }
 
-void ceph::crypto::shutdown(bool shared)
+static void shutdown(bool shared)
 {
   pthread_mutex_lock(&crypto_init_mutex);
   ceph_assert_always(crypto_refs > 0);
@@ -84,9 +234,31 @@ ceph::crypto::nss::HMAC::~HMAC()
   PK11_FreeSlot(slot);
 }
 
+} // namespace ceph::crypto::nss
 #else
 # error "No supported crypto implementation found."
 #endif /*USE_NSS*/
+
+
+void ceph::crypto::init(CephContext* const cct) {
+#ifdef USE_NSS
+  ceph::crypto::nss::init(cct);
+#endif
+
+#ifdef USE_OPENSSL
+  ceph::crypto::ssl::init();
+#endif
+}
+
+void ceph::crypto::shutdown(const bool shared) {
+#ifdef USE_NSS
+  ceph::crypto::nss::shutdown(shared);
+#endif
+
+#ifdef USE_OPENSSL
+  ceph::crypto::ssl::shutdown();
+#endif
+}
 
 #ifdef USE_OPENSSL
 
