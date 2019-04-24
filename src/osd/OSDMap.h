67c779fa5a3ef03f910673eb226caa273b1dd3c1
xie xingguo <xie.xingguo@zte.com.cn>
2019-04-24 08:57:17 +0800

    osd: add no{out,down,in,out} flags by device class
    This works as a good supplement of https://github.com/ceph/ceph/pull/27563.
    
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- src/osd/OSDMap.h
+++ src/osd/OSDMap.h
@@ -402,6 +402,7 @@ public:
     mempool::osdmap::map<int64_t, snap_interval_set_t> new_purged_snaps;
 
     mempool::osdmap::map<int32_t,uint32_t> new_crush_node_flags;
+    mempool::osdmap::map<int32_t,uint32_t> new_device_class_flags;
 
     std::string cluster_snapshot;
 
@@ -519,6 +520,7 @@ private:
   std::vector<uint32_t> osd_state;
 
   mempool::osdmap::map<int32_t,uint32_t> crush_node_flags; // crush node -> CEPH_OSD_* flags
+  mempool::osdmap::map<int32_t,uint32_t> device_class_flags; // device class -> CEPH_OSD_* flags
 
   utime_t last_up_change, last_in_change;
 
@@ -839,6 +841,7 @@ public:
 
   unsigned get_osd_crush_node_flags(int osd) const;
   unsigned get_crush_node_flags(int id) const;
+  unsigned get_device_class_flags(int id) const;
 
   bool is_noup(int osd) const {
     return exists(osd) && (osd_state[osd] & CEPH_OSD_NOUP);
