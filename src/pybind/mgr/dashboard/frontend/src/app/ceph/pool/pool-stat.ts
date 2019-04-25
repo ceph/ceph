a0f46bb49c2d61808c9275931724e37ba8da18b3
Stephan Müller <smueller@suse.com>
2019-04-25 14:34:00 +0200

    mgr/dashboard: Make all columns sortable
    Fixes: https://tracker.ceph.com/issues/39483
    Signed-off-by: Stephan Müller <smueller@suse.com>
    (cherry picked from commit ba6d0f96dbdd2ccb4824a6377e741a9534d46287)

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/pool/pool-stat.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/pool/pool-stat.ts
@@ -3,3 +3,12 @@ export class PoolStat {
   rate: number;
   series: number[];
 }
+
+export class PoolStats {
+  bytes_used?: PoolStat;
+  max_avail?: PoolStat;
+  rd_bytes?: PoolStat;
+  wr_bytes?: PoolStat;
+  rd?: PoolStat;
+  wr?: PoolStat;
+}
