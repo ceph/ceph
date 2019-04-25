f793bab872ad3a630d2e879f0aa2bcb07806ea23
Sage Weil <sage@redhat.com>
2019-04-25 17:17:45 -0500

    common/ceph_time: stringify in ISO 8601 format
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/test/common/test_time.cc
+++ src/test/common/test_time.cc
@@ -18,6 +18,7 @@
 #include "common/ceph_time.h"
 #include "include/rados.h"
 #include "gtest/gtest.h"
+#include "include/stringify.h"
 
 
 using ceph::real_clock;
@@ -175,4 +176,19 @@ TEST(TimePoints, SignedSubtraciton) {
   ASSERT_LT((cmta - cmtb).count(), 0);
   ASSERT_GT(cmtb - cmta, ceph::signedspan::zero());
   ASSERT_GT((cmtb - cmta).count(), 0);
+}
+
+TEST(TimePoints, stringify) {
+  ceph::real_clock::time_point tp(seconds(1556122013) + nanoseconds(39923122));
+  string s = stringify(tp);
+  ASSERT_EQ(s.size(), strlen("2019-04-24T11:06:53.039923-0500"));
+  ASSERT_TRUE(s[26] == '-' || s[26] == '+');
+  ASSERT_EQ(s.substr(0, 9), "2019-04-2");
+
+  ceph::coarse_real_clock::time_point ctp(seconds(1556122013) +
+					  nanoseconds(399000000));
+  s = stringify(ctp);
+  ASSERT_EQ(s.size(), strlen("2019-04-24T11:06:53.399000-0500"));
+  ASSERT_TRUE(s[26] == '-' || s[26] == '+');
+  ASSERT_EQ(s.substr(0, 9), "2019-04-2");
 }
