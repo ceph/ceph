da3803f59fcf904ec32e6e03d2dedc23d0a127f8
Casey Bodley <cbodley@redhat.com>
2019-04-25 10:01:30 -0400

    rgw: data/bilogs are trimmed when no peers are reading them
    if no peers zones are configured to sync from us, we can safely trim
    our entire log. start with the marker '99999999' (which compares larger
    than any key based on a timestamp) instead of an empty marker, then take
    the minimum of that with any peer markers
    
    Fixes: http://tracker.ceph.com/issues/39487
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_trim_datalog.cc
+++ src/rgw/rgw_trim_datalog.cc
@@ -64,7 +64,7 @@ class DataLogTrimCR : public RGWCoroutine {
       num_shards(num_shards),
       zone_id(store->svc.zone->get_zone().id),
       peer_status(store->svc.zone->get_zone_data_notify_to_map().size()),
-      min_shard_markers(num_shards),
+      min_shard_markers(num_shards, "99999999"),
       last_trim(last_trim)
   {}
 
