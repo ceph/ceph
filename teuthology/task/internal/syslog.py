shyukri/teuthology

task: syslog: salt INFO messages generate false negatives
Signed-off-by: Nathan Cutler <ncutler@suse.com>

teuthology/task/internal/syslog.py
@@ -128,6 +128,8 @@ def syslog(ctx, config):
                     run.Raw('|'),
                     'grep', '-v', 'ceph-create-keys: INFO',
                     run.Raw('|'),
+                    'grep', '-v', '\\bsalt-master\\b|\\bsalt-minion\\b',
+                    run.Raw('|'),
                     'head', '-n', '1',
                 ],
                 stdout=StringIO(),

ceph/ceph

Conflicts:
	teuthology/task/internal/syslog.py

teuthology/task/internal/syslog.py
@@ -134,6 +134,8 @@ def syslog(ctx, config):
                     run.Raw('|'),
                     'grep', '-v', 'container-storage-setup: INFO: Volume group backing root filesystem could not be determined',  # noqa
                     run.Raw('|'),
+                    'egrep', '-v', '\\bsalt-master\\b|\\bsalt-minion\\b',
+                    run.Raw('|'),
                     'grep', '-v', 'ceph-crash',
                     run.Raw('|'),
                     'head', '-n', '1',