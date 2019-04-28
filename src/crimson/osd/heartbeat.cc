ceph/ceph

commit fabc0ffc1fb230d26210997c430be8bf119ececa

crimson: pass `Connection*` to Dispatch::ms_dispatch()
currently, we use a `with_gate()` in `ProtocolV2::read_message()` for
ensuring that `this` (or `Connection` holding this protocol instance)
will outlive the continuation of `dispatcher.ms_dispatch()` which
references `this->dispatcher`. but we also pass a strong reference of
connection to dispatcher. in short, we have *two* safeguards for the
same purpose.

in this change, one of these safeguards is removed -- to pass the raw
pointer of `Connection` to `Dispatch::ms_dispatch()`. the reason why
the `with_gate()` is kept is that, if we have removed `with_gate()` in
Protocol, we need to

1. let `Dispatcher::ms_dispatch()` return `void`, as it should not block
any succeeding calls.
2. add a `with_gate()` in `Dispatcher::ms_dispatch()` to ensure that
`this` is alive during the lifecycle of the continuation(s) in
`Dispatcher::ms_dispatch()`.

Signed-off-by: Yingxin Cheng <yingxincheng@gmail.com>
Signed-off-by: Kefu Chai <kchai@redhat.com>

src/crimson/osd/heartbeat.cc
@@ -201,7 +201,7 @@ seastar::future<> Heartbeat::remove_peer(osd_id_t peer)
     });
 }
 
-seastar::future<> Heartbeat::ms_dispatch(ceph::net::ConnectionRef conn,
+seastar::future<> Heartbeat::ms_dispatch(ceph::net::Connection* conn,
                                          MessageRef m)
 {
   switch (m->get_type()) {
@@ -229,7 +229,7 @@ seastar::future<> Heartbeat::ms_handle_reset(ceph::net::ConnectionRef conn)
   });
 }
 
-seastar::future<> Heartbeat::handle_osd_ping(ceph::net::ConnectionRef conn,
+seastar::future<> Heartbeat::handle_osd_ping(ceph::net::Connection* conn,
                                              Ref<MOSDPing> m)
 {
   switch (m->op) {
@@ -244,7 +244,7 @@ seastar::future<> Heartbeat::handle_osd_ping(ceph::net::ConnectionRef conn,
   }
 }
 
-seastar::future<> Heartbeat::handle_ping(ceph::net::ConnectionRef conn,
+seastar::future<> Heartbeat::handle_ping(ceph::net::Connection* conn,
                                          Ref<MOSDPing> m)
 {
   auto min_message = static_cast<uint32_t>(
@@ -258,7 +258,7 @@ seastar::future<> Heartbeat::handle_ping(ceph::net::ConnectionRef conn,
   return conn->send(reply);
 }
 
-seastar::future<> Heartbeat::handle_reply(ceph::net::ConnectionRef conn,
+seastar::future<> Heartbeat::handle_reply(ceph::net::Connection* conn,
                                           Ref<MOSDPing> m)
 {
   const osd_id_t from = m->get_source().num();

src/crimson/mgr/client.cc
@@ -49,7 +49,7 @@ seastar::future<> Client::stop()
   });
 }
 
-seastar::future<> Client::ms_dispatch(ceph::net::ConnectionRef conn,
+seastar::future<> Client::ms_dispatch(ceph::net::Connection* conn,
                                       MessageRef m)
 {
   switch(m->get_type()) {
@@ -90,7 +90,7 @@ seastar::future<> Client::reconnect()
   });
 }
 
-seastar::future<> Client::handle_mgr_map(ceph::net::ConnectionRef,
+seastar::future<> Client::handle_mgr_map(ceph::net::Connection*,
                                          Ref<MMgrMap> m)
 {
   mgrmap = m->get_map();
@@ -104,7 +104,7 @@ seastar::future<> Client::handle_mgr_map(ceph::net::ConnectionRef,
   }
 }
 
-seastar::future<> Client::handle_mgr_conf(ceph::net::ConnectionRef conn,
+seastar::future<> Client::handle_mgr_conf(ceph::net::Connection* conn,
                                           Ref<MMgrConfigure> m)
 {
   logger().info("{} {}", __func__, *m);

src/crimson/mgr/client.h
@@ -37,12 +37,12 @@ public:
   seastar::future<> start();
   seastar::future<> stop();
 private:
-  seastar::future<> ms_dispatch(ceph::net::ConnectionRef conn,
+  seastar::future<> ms_dispatch(ceph::net::Connection* conn,
 				Ref<Message> m) override;
   seastar::future<> ms_handle_reset(ceph::net::ConnectionRef conn) override;
-  seastar::future<> handle_mgr_map(ceph::net::ConnectionRef conn,
+  seastar::future<> handle_mgr_map(ceph::net::Connection* conn,
 				   Ref<MMgrMap> m);
-  seastar::future<> handle_mgr_conf(ceph::net::ConnectionRef conn,
+  seastar::future<> handle_mgr_conf(ceph::net::Connection* conn,
 				    Ref<MMgrConfigure> m);
   seastar::future<> reconnect();
   void report();

src/crimson/mon/MonClient.cc
@@ -433,7 +433,7 @@ bool Client::is_hunting() const {
 }
 
 seastar::future<>
-Client::ms_dispatch(ceph::net::ConnectionRef conn, MessageRef m)
+Client::ms_dispatch(ceph::net::Connection* conn, MessageRef m)
 {
   // we only care about these message types
   switch (m->get_type()) {
@@ -689,7 +689,7 @@ int Client::handle_auth_bad_method(ceph::net::ConnectionRef conn,
   }
 }
 
-seastar::future<> Client::handle_monmap(ceph::net::ConnectionRef conn,
+seastar::future<> Client::handle_monmap(ceph::net::Connection* conn,
                                         Ref<MMonMap> m)
 {
   monmap.decode(m->monmapbl);
@@ -707,7 +707,7 @@ seastar::future<> Client::handle_monmap(ceph::net::ConnectionRef conn,
   }
 }
 
-seastar::future<> Client::handle_auth_reply(ceph::net::ConnectionRef conn,
+seastar::future<> Client::handle_auth_reply(ceph::net::Connection* conn,
                                                Ref<MAuthReply> m)
 {
   logger().info("mon {} => {} returns {}: {}",

src/crimson/mon/MonClient.h
@@ -143,14 +143,14 @@ private:
 private:
   void tick();
 
-  seastar::future<> ms_dispatch(ceph::net::ConnectionRef conn,
+  seastar::future<> ms_dispatch(ceph::net::Connection* conn,
 				MessageRef m) override;
   seastar::future<> ms_handle_reset(ceph::net::ConnectionRef conn) override;
   AuthAuthorizer* ms_get_authorizer(peer_type_t peer) const override;
 
-  seastar::future<> handle_monmap(ceph::net::ConnectionRef conn,
+  seastar::future<> handle_monmap(ceph::net::Connection* conn,
 				  Ref<MMonMap> m);
-  seastar::future<> handle_auth_reply(ceph::net::ConnectionRef conn,
+  seastar::future<> handle_auth_reply(ceph::net::Connection* conn,
 				      Ref<MAuthReply> m);
   seastar::future<> handle_subscribe_ack(Ref<MMonSubscribeAck> m);
   seastar::future<> handle_get_version_reply(Ref<MMonGetVersionReply> m);

src/crimson/net/Dispatcher.h
@@ -27,7 +27,7 @@ class Dispatcher {
  public:
   virtual ~Dispatcher() {}
 
-  virtual seastar::future<> ms_dispatch(ConnectionRef conn, MessageRef m) {
+  virtual seastar::future<> ms_dispatch(Connection* conn, MessageRef m) {
     return seastar::make_ready_future<>();
   }
 

src/crimson/net/ProtocolV1.cc
@@ -733,10 +733,7 @@ seastar::future<> ProtocolV1::read_message()
       seastar::with_gate(pending_dispatch, [this, msg = std::move(msg_ref)] {
           logger().debug("{} <= {}@{} === {}", messenger,
                 msg->get_source(), conn.peer_addr, *msg);
-          return dispatcher.ms_dispatch(
-              seastar::static_pointer_cast<SocketConnection>(
-                conn.shared_from_this()),
-              std::move(msg))
+          return dispatcher.ms_dispatch(&conn, std::move(msg))
             .handle_exception([this] (std::exception_ptr eptr) {
               logger().error("{} ms_dispatch caught exception: {}", conn, eptr);
               ceph_assert(false);

src/crimson/net/ProtocolV2.cc
@@ -1466,11 +1466,8 @@ seastar::future<> ProtocolV2::read_message(utime_t throttle_stamp)
     // TODO: change MessageRef with seastar::shared_ptr
     auto msg_ref = MessageRef{message, false};
     seastar::with_gate(pending_dispatch, [this, msg = std::move(msg_ref)] {
-      return dispatcher.ms_dispatch(
-          seastar::static_pointer_cast<SocketConnection>(
-            conn.shared_from_this()),
-          std::move(msg))
-      .handle_exception([this] (std::exception_ptr eptr) {
+      return dispatcher.ms_dispatch(&conn, std::move(msg))
+	.handle_exception([this] (std::exception_ptr eptr) {
         logger().error("{} ms_dispatch caught exception: {}", conn, eptr);
         ceph_assert(false);
       });

commit 54795c461e71ac482d7fd10491b97c5d98ce4eba

crimson/osd: pass `Connection*` to Dispatch::ms_dispatch()
it's a regression introduced by fabc0ffc1fb230d26210997c430be8bf119ececa

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/crimson/osd/heartbeat.cc
@@ -276,10 +276,10 @@ seastar::future<> Heartbeat::handle_reply(ceph::net::Connection* conn,
   }
   const auto now = clock::now();
   auto& unacked = ping->second.unacknowledged;
-  if (conn == peer.con_back) {
+  if (conn == peer.con_back.get()) {
     peer.last_rx_back = now;
     unacked--;
-  } else if (conn == peer.con_front) {
+  } else if (conn == peer.con_front.get()) {
     peer.last_rx_front = now;
     unacked--;
   }

src/crimson/osd/osd.cc
@@ -359,7 +359,7 @@ seastar::future<Ref<PG>> OSD::load_pg(spg_t pgid)
   });
 }
 
-seastar::future<> OSD::ms_dispatch(ceph::net::ConnectionRef conn, MessageRef m)
+seastar::future<> OSD::ms_dispatch(ceph::net::Connection* conn, MessageRef m)
 {
   if (state.is_stopping()) {
     return seastar::now();
@@ -520,7 +520,7 @@ seastar::future<> OSD::osdmap_subscribe(version_t epoch, bool force_request)
   }
 }
 
-seastar::future<> OSD::handle_osd_map(ceph::net::ConnectionRef conn,
+seastar::future<> OSD::handle_osd_map(ceph::net::Connection* conn,
                                       Ref<MOSDMap> m)
 {
   logger().info("handle_osd_map {}", *m);
@@ -651,7 +651,7 @@ seastar::future<> OSD::committed_osd_maps(version_t first,
   });
 }
 
-seastar::future<> OSD::handle_osd_op(ceph::net::ConnectionRef conn,
+seastar::future<> OSD::handle_osd_op(ceph::net::Connection* conn,
                                      Ref<MOSDOp> m)
 {
   return wait_for_map(m->get_map_epoch()).then([=](epoch_t epoch) {
@@ -740,7 +740,7 @@ void OSD::update_heartbeat_peers()
   heartbeat->update_peers(whoami);
 }
 
-seastar::future<> OSD::handle_pg_notify(ceph::net::ConnectionRef conn,
+seastar::future<> OSD::handle_pg_notify(ceph::net::Connection* conn,
                                         Ref<MOSDPGNotify> m)
 {
   // assuming all pgs reside in a single shard
@@ -769,7 +769,7 @@ seastar::future<> OSD::handle_pg_notify(ceph::net::ConnectionRef conn,
   });
 }
 
-seastar::future<> OSD::handle_pg_info(ceph::net::ConnectionRef conn,
+seastar::future<> OSD::handle_pg_info(ceph::net::Connection* conn,
                                       Ref<MOSDPGInfo> m)
 {
   // assuming all pgs reside in a single shard
@@ -789,7 +789,7 @@ seastar::future<> OSD::handle_pg_info(ceph::net::ConnectionRef conn,
   });
 }
 
-seastar::future<> OSD::handle_pg_query(ceph::net::ConnectionRef conn,
+seastar::future<> OSD::handle_pg_query(ceph::net::Connection* conn,
                                        Ref<MOSDPGQuery> m)
 {
   const int from = m->get_source().num();
@@ -805,7 +805,7 @@ seastar::future<> OSD::handle_pg_query(ceph::net::ConnectionRef conn,
   });
 }
 
-seastar::future<> OSD::handle_pg_log(ceph::net::ConnectionRef conn,
+seastar::future<> OSD::handle_pg_log(ceph::net::Connection* conn,
                                        Ref<MOSDPGLog> m)
 {
   const int from = m->get_source().num();

src/crimson/osd/osd.h
@@ -86,7 +86,7 @@ class OSD : public ceph::net::Dispatcher,
   OSDSuperblock superblock;
 
   // Dispatcher methods
-  seastar::future<> ms_dispatch(ceph::net::ConnectionRef conn, MessageRef m) override;
+  seastar::future<> ms_dispatch(ceph::net::Connection* conn, MessageRef m) override;
   seastar::future<> ms_handle_connect(ceph::net::ConnectionRef conn) override;
   seastar::future<> ms_handle_reset(ceph::net::ConnectionRef conn) override;
   seastar::future<> ms_handle_remote_reset(ceph::net::ConnectionRef conn) override;
@@ -137,17 +137,17 @@ private:
   void write_superblock(ceph::os::Transaction& t);
   seastar::future<> read_superblock();
 
-  seastar::future<> handle_osd_map(ceph::net::ConnectionRef conn,
+  seastar::future<> handle_osd_map(ceph::net::Connection* conn,
                                    Ref<MOSDMap> m);
-  seastar::future<> handle_osd_op(ceph::net::ConnectionRef conn,
+  seastar::future<> handle_osd_op(ceph::net::Connection* conn,
 				  Ref<MOSDOp> m);
-  seastar::future<> handle_pg_log(ceph::net::ConnectionRef conn,
+  seastar::future<> handle_pg_log(ceph::net::Connection* conn,
 				  Ref<MOSDPGLog> m);
-  seastar::future<> handle_pg_notify(ceph::net::ConnectionRef conn,
+  seastar::future<> handle_pg_notify(ceph::net::Connection* conn,
 				     Ref<MOSDPGNotify> m);
-  seastar::future<> handle_pg_info(ceph::net::ConnectionRef conn,
+  seastar::future<> handle_pg_info(ceph::net::Connection* conn,
 				   Ref<MOSDPGInfo> m);
-  seastar::future<> handle_pg_query(ceph::net::ConnectionRef conn,
+  seastar::future<> handle_pg_query(ceph::net::Connection* conn,
 				    Ref<MOSDPGQuery> m);
 
   seastar::future<> committed_osd_maps(version_t first,

src/crimson/osd/pg.cc
@@ -1018,7 +1018,7 @@ seastar::future<Ref<MOSDOpReply>> PG::do_osd_ops(Ref<MOSDOp> m)
   });
 }
 
-seastar::future<> PG::handle_op(ceph::net::ConnectionRef conn,
+seastar::future<> PG::handle_op(ceph::net::Connection* conn,
                                 Ref<MOSDOp> m)
 {
   return wait_for_active().then([conn, m, this] {

src/crimson/osd/pg.h
@@ -114,7 +114,7 @@ public:
   seastar::future<> handle_activate_map();
   seastar::future<> share_pg_info();
   void reply_pg_query(const MQuery& query, recovery::Context* ctx);
-  seastar::future<> handle_op(ceph::net::ConnectionRef conn,
+  seastar::future<> handle_op(ceph::net::Connection* conn,
 			      Ref<MOSDOp> m);
   void print(ostream& os) const;
 private: