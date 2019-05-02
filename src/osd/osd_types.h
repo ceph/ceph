00f279db37fc648584c4c741e0453b1d4f4320e4
David Zafman <dzafman@redhat.com>
2019-05-01 19:47:55 -0700

    osd: Include dups in copy_after() and copy_up_to()
    Client saw out of order results in a test with a very small
    pg_log because a backfill/recovery sent a small number of
    log entries for duplicate checks.
    
    Since these copy operations are equivalent to both a copy
    and trim (an argument controls how many log entries
    transfer), we need to include any new dups with enough
    existing dups up to the configured maximum.
    
    Fixes: http://tracker.ceph.com/issues/39304
    
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/osd/osd_types.h
+++ src/osd/osd_types.h
@@ -4110,7 +4110,7 @@ public:
    * @param other pg_log_t to copy from
    * @param from copy entries after this version
    */
-  void copy_after(const pg_log_t &other, eversion_t from);
+  void copy_after(CephContext* cct, const pg_log_t &other, eversion_t from);
 
   /**
    * copy up to N entries
@@ -4118,7 +4118,7 @@ public:
    * @param other source log
    * @param max max number of entries to copy
    */
-  void copy_up_to(const pg_log_t &other, int max);
+  void copy_up_to(CephContext* cct, const pg_log_t &other, int max);
 
   std::ostream& print(std::ostream& out) const;
 
