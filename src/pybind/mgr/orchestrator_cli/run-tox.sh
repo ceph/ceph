63c045d23d596af11509e77fb35f74511df54860
Nathan Cutler <ncutler@suse.com>
2019-05-03 12:58:59 +0200

    pybind/tox: pass additional command line arguments through to tox
    Fixes: 9426f1f2045d0ae0f319530c3dc3a9240d838d07
    Fixes: https://tracker.ceph.com/issues/39579
    Signed-off-by: Nathan Cutler <ncutler@suse.com>
    (cherry picked from commit d04b44816028079ee65564d8244fe2b56c49047e)

--- src/pybind/mgr/orchestrator_cli/run-tox.sh
+++ src/pybind/mgr/orchestrator_cli/run-tox.sh
@@ -40,7 +40,7 @@ ENV_LIST=$(echo "$ENV_LIST" | sed -e 's/,$//')
 # use bash string manipulation to strip off any trailing comma
 ENV_LIST=${ENV_LIST%,}
 
-tox -c "${TOX_PATH}" -e "${ENV_LIST}"
+tox -c "${TOX_PATH}" -e "${ENV_LIST}" "$@"
 TOX_STATUS="$?"
 test "$TOX_STATUS" -ne "0" && dump_envvars
 exit $TOX_STATUS
