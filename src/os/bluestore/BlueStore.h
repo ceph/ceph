5180a14d1f39a37f4ff28e31ff1ec06e5c435f34
Igor Fedotov <ifedotov@suse.com>
2019-05-03 16:01:47 +0300

    os/bluestore: fix origin reference in logging slow ops.
    Signed-off-by: Igor Fedotov <ifedotov@suse.com>

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -2682,7 +2682,8 @@ public:
     uint64_t size,
     PExtentVector* extents);
 
-  void log_latency_fn(int idx,
+  void log_latency_fn(const char* name,
+		      int idx,
 		      const ceph::timespan& lat,
 		      std::function<string (const ceph::timespan& lat)> fn);
 
