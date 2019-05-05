438a1ff329dd0f0c3ee1693c49702ad4a83500ba
Mykola Golub <mgolub@suse.com>
2019-05-05 10:00:10 +0100

    common: make extra memory allocation for pri cache optional
    Not all caches may want this.
    
    Signed-off-by: Mykola Golub <mgolub@suse.com>

--- src/common/PriorityCache.h
+++ src/common/PriorityCache.h
@@ -116,9 +116,11 @@ namespace PriorityCache {
     uint64_t max_mem = 0;
     uint64_t target_mem = 0;
     uint64_t tuned_mem = 0;
+    bool reserve_extra;
 
   public:
-    Manager(CephContext *c, uint64_t min, uint64_t max, uint64_t target);
+    Manager(CephContext *c, uint64_t min, uint64_t max, uint64_t target,
+            bool reserve_extra);
     ~Manager();
     void set_min_memory(uint64_t min) {
       min_mem = min;
