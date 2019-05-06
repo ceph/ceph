a3f4827bb8c864c3aedf6e6b82a4d9194c5a5c6a
zjh <jhzeng93@foxmail.com>
2019-05-06 14:35:54 +0800

    qa/standalone: remove osd_pool_default_size in test_wait_for_health_ok
    Signed-off-by: zjh <jhzeng93@foxmail.com>

--- qa/standalone/ceph-helpers.sh
+++ qa/standalone/ceph-helpers.sh
@@ -1626,9 +1626,11 @@ function test_wait_for_health_ok() {
     local dir=$1
 
     setup $dir || return 1
-    run_mon $dir a --osd_pool_default_size=1 --osd_failsafe_full_ratio=.99 --mon_pg_warn_min_per_osd=0 || return 1
+    run_mon $dir a --osd_failsafe_full_ratio=.99 --mon_pg_warn_min_per_osd=0 || return 1
     run_mgr $dir x --mon_pg_warn_min_per_osd=0 || return 1
     run_osd $dir 0 || return 1
+    run_osd $dir 1 || return 1
+    run_osd $dir 2 || return 1
     kill_daemons $dir TERM osd || return 1
     ceph osd down 0 || return 1
     ! TIMEOUT=1 wait_for_health_ok || return 1
