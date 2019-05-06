939cd2dcd46ac1fe48f0ffd6c072cc1dc78efba8
Casey Bodley <cbodley@redhat.com>
2019-05-06 15:01:07 -0400

    rgw: delete_obj_index() takes mtime for bilog
    writing an empty timestamp to the bilog prevents other zones from
    applying the delete. this means that the --bypass-gc flag for
    'radosgw-admin bucket rm' doesn't work in multisite
    
    Fixes: http://tracker.ceph.com/issues/24991
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 0316a81875ae93a06841d532bf3548c81370296e)

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -3274,7 +3274,7 @@ public:
   virtual int delete_system_obj(rgw_raw_obj& src_obj, RGWObjVersionTracker *objv_tracker = NULL);
 
   /** Remove an object from the bucket index */
-  int delete_obj_index(const rgw_obj& obj);
+  int delete_obj_index(const rgw_obj& obj, ceph::real_time mtime);
 
   /**
    * Get an attribute for a system object.
