80c567a9f1300acf2e6056145a6711759011d2c7
shangdehao1 <dehao.shang@intel.com>
2019-05-08 13:10:23 +0800

    librbd: change RO hook naming.
    change SharedReadOnlyCache to ParentCache
    
    Signed-off-by: Dehao Shang <dehao.shang@intel.com>

--- src/librbd/ImageCtx.h
+++ src/librbd/ImageCtx.h
@@ -195,7 +195,7 @@ namespace librbd {
     uint64_t mtime_update_interval;
     uint64_t atime_update_interval;
 
-    bool shared_cache_enabled;
+    bool parent_cache_enabled;
     std::string shared_cache_path;
 
     LibrbdAdminSocketHook *asok_hook;
