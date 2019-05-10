4363f7848501639339238ffb699e0056684c995a
Sage Weil <sage@redhat.com>
2019-05-10 14:45:12 -0500

    mon/PGMap: enable/disable TOO_FEW_OSDS warning with an option
    Signed-off-by: Sage Weil <sage@redhat.com>
    (cherry picked from commit 3b74fbc93d32d6cc367049d3bcbe83d3837abba5)

--- src/common/options.cc
+++ src/common/options.cc
@@ -1727,6 +1727,11 @@ std::vector<Option> get_global_options() {
     .add_service("mgr")
     .set_description("Issue a health warning if there are misplaced objects"),
 
+    Option("mon_warn_on_too_few_osds", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
+    .set_default(true)
+    .add_service("mgr")
+    .set_description("Issue a health warning if there are fewer OSDs than osd_pool_default_size"),
+
     Option("mon_max_snap_prune_per_epoch", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
     .set_default(100)
     .add_service("mon")
