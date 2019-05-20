2d15a1024f6cf2b58bc5719cec2996e9cc263c05
Rishabh Dave <ridave@redhat.com>
2019-05-20 11:30:07 +0200

    test_volume_client: simplify test_get_authorized_ids()
    Signed-off-by: Rishabh Dave <ridave@redhat.com>
    (cherry picked from commit 79d62dd87461c43dd6df3775f21962be9042a7b1)

--- qa/tasks/cephfs/test_volume_client.py
+++ qa/tasks/cephfs/test_volume_client.py
@@ -664,8 +664,7 @@ vc.disconnect()
             volume_id=volume_id,
         )))
         # Check the list of authorized IDs for the volume.
-        expected_result = None
-        self.assertEqual(str(expected_result), auths)
+        self.assertEqual('None', auths)
 
         # Allow two auth IDs access to the volume.
         auths = self._volume_client_python(volumeclient_mount, dedent("""
@@ -698,8 +697,7 @@ vc.disconnect()
             guest_entity_2=guest_entity_2,
         )))
         # Check the list of authorized IDs for the volume.
-        expected_result = None
-        self.assertItemsEqual(str(expected_result), auths)
+        self.assertEqual('None', auths)
 
     def test_multitenant_volumes(self):
         """
