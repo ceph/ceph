d97c574e411f82a001d1359191651b4c4e439989
Chang Liu <liuchang0812@gmail.com>
2019-05-28 15:38:43 +0800

    rgw: request that delete bucket website should be forward to master zone
    Signed-off-by: Chang Liu <liuchang0812@gmail.com>
    (cherry picked from commit 52cb0915630ff490352a4733d8a24af2f88f784b)
    
    Conflicts:
        src/rgw/rgw_op.cc
    - ldpp_dout, store->svc.zone

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -2299,6 +2299,16 @@ void RGWDeleteBucketWebsite::pre_exec()
 
 void RGWDeleteBucketWebsite::execute()
 {
+
+  if (!store->is_meta_master()) {
+    bufferlist in_data;
+    op_ret = forward_request_to_master(s, nullptr, store, in_data, nullptr);
+    if (op_ret < 0) {
+      ldout(s->cct, 0) << "NOTICE: forward_to_master failed on bucket=" << s->bucket.name 
+	               << "returned err=" << op_ret << dendl;
+      return;
+    }
+  }
   op_ret = retry_raced_bucket_write(store, s, [this] {
       s->bucket_info.has_website = false;
       s->bucket_info.website_conf = RGWBucketWebsiteConf();
