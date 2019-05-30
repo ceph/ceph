7ec6bd809923f11a1f389b4c6f24faa5b24556f8
alfonsomthd <almartin@redhat.com>
2019-05-30 09:57:54 +0200

    mgr/dashboard: show degraded/misplaced/unfound objects.
    * Landing Page 'Objects' card now is a chart that shows more info about objects.
    * Fix: Dimless/dimlessBinary pipe applied to amount displayed in
      chart slice tooltip body (if shown).
    * Refactoring: simplified way of setting chart initial config
      via 'config' @Input; erased redundant @Inputs.
      Updated chart component default config (for the sake of simplicity).
    
    Fixes: https://tracker.ceph.com/issues/39613
    
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/health/health.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/health/health.component.ts
@@ -7,6 +7,7 @@ import { Subscription } from 'rxjs/Subscription';
 import { HealthService } from '../../../shared/api/health.service';
 import { Permissions } from '../../../shared/models/permissions';
 import { DimlessBinaryPipe } from '../../../shared/pipes/dimless-binary.pipe';
+import { DimlessPipe } from '../../../shared/pipes/dimless.pipe';
 import { AuthStorageService } from '../../../shared/services/auth-storage.service';
 import {
   FeatureTogglesMap$,
@@ -27,6 +28,39 @@ export class HealthComponent implements OnInit, OnDestroy {
   permissions: Permissions;
   enabledFeature$: FeatureTogglesMap$;
 
+  rawCapacityChartConfig = {
+    options: {
+      title: { display: true, position: 'bottom' }
+    }
+  };
+  objectsChartConfig = {
+    options: {
+      title: { display: true, position: 'bottom' }
+    },
+    colors: [
+      {
+        backgroundColor: [
+          HealthPieColor.DEFAULT_GREEN,
+          HealthPieColor.DEFAULT_MAGENTA,
+          HealthPieColor.DEFAULT_ORANGE,
+          HealthPieColor.DEFAULT_RED
+        ]
+      }
+    ]
+  };
+  pgStatusChartConfig = {
+    colors: [
+      {
+        backgroundColor: [
+          HealthPieColor.DEFAULT_GREEN,
+          HealthPieColor.DEFAULT_BLUE,
+          HealthPieColor.DEFAULT_ORANGE,
+          HealthPieColor.DEFAULT_RED
+        ]
+      }
+    ]
+  };
+
   constructor(
     private healthService: HealthService,
     private i18n: I18n,
@@ -34,7 +68,8 @@ export class HealthComponent implements OnInit, OnDestroy {
     private pgCategoryService: PgCategoryService,
     private featureToggles: FeatureTogglesService,
     private refreshIntervalService: RefreshIntervalService,
-    private dimlessBinary: DimlessBinaryPipe
+    private dimlessBinary: DimlessBinaryPipe,
+    private dimless: DimlessPipe
   ) {
     this.permissions = this.authStorageService.getPermissions();
     this.enabledFeature$ = this.featureToggles.get();
@@ -63,12 +98,20 @@ export class HealthComponent implements OnInit, OnDestroy {
 
     const total =
       this.healthData.client_perf.write_op_per_sec + this.healthData.client_perf.read_op_per_sec;
-    const calcPercentage = (status) =>
-      Math.round(((this.healthData.client_perf[status] || 0) / total) * 100);
 
-    ratioLabels.push(`${this.i18n('Writes')} (${calcPercentage('write_op_per_sec')}%)`);
+    ratioLabels.push(
+      `${this.i18n('Writes')} (${this.calcPercentage(
+        this.healthData.client_perf.write_op_per_sec,
+        total
+      )}%)`
+    );
     ratioData.push(this.healthData.client_perf.write_op_per_sec);
-    ratioLabels.push(`${this.i18n('Reads')} (${calcPercentage('read_op_per_sec')}%)`);
+    ratioLabels.push(
+      `${this.i18n('Reads')} (${this.calcPercentage(
+        this.healthData.client_perf.read_op_per_sec,
+        total
+      )}%)`
+    );
     ratioData.push(this.healthData.client_perf.read_op_per_sec);
 
     chart.dataset[0].data = ratioData;
@@ -76,20 +119,17 @@ export class HealthComponent implements OnInit, OnDestroy {
   }
 
   prepareRawUsage(chart, data) {
-    const percentAvailable = Math.round(
-      100 *
-        ((data.df.stats.total_bytes - data.df.stats.total_used_raw_bytes) /
-          data.df.stats.total_bytes)
+    const percentAvailable = this.calcPercentage(
+      data.df.stats.total_bytes - data.df.stats.total_used_raw_bytes,
+      data.df.stats.total_bytes
     );
-
-    const percentUsed = Math.round(
-      100 * (data.df.stats.total_used_raw_bytes / data.df.stats.total_bytes)
+    const percentUsed = this.calcPercentage(
+      data.df.stats.total_used_raw_bytes,
+      data.df.stats.total_bytes
     );
 
     chart.dataset[0].data = [data.df.stats.total_used_raw_bytes, data.df.stats.total_avail_bytes];
-    if (chart === 'doughnut') {
-      chart.options.cutoutPercentage = 65;
-    }
+
     chart.labels = [
       `${this.dimlessBinary.transform(data.df.stats.total_used_raw_bytes)} ${this.i18n(
         'Used'
@@ -99,25 +139,13 @@ export class HealthComponent implements OnInit, OnDestroy {
       )} ${this.i18n('Avail.')} (${percentAvailable}%)`
     ];
 
-    chart.options.title = {
-      display: true,
-      text: `${this.dimlessBinary.transform(data.df.stats.total_bytes)} total`,
-      position: 'bottom'
-    };
+    chart.options.title.text = `${this.dimlessBinary.transform(
+      data.df.stats.total_bytes
+    )} ${this.i18n('total')}`;
   }
 
   preparePgStatus(chart, data) {
     const categoryPgAmount = {};
-    chart.colors = [
-      {
-        backgroundColor: [
-          HealthPieColor.DEFAULT_GREEN,
-          HealthPieColor.DEFAULT_BLUE,
-          HealthPieColor.DEFAULT_ORANGE,
-          HealthPieColor.DEFAULT_RED
-        ]
-      }
-    ];
 
     _.forEach(data.pg_info.statuses, (pgAmount, pgStatesText) => {
       const categoryType = this.pgCategoryService.getTypeByStates(pgStatesText);
@@ -132,15 +160,62 @@ export class HealthComponent implements OnInit, OnDestroy {
       .getAllTypes()
       .map((categoryType) => categoryPgAmount[categoryType]);
 
-    const calcPercentage = (status) =>
-      Math.round(((categoryPgAmount[status] || 0) / data.pg_info.pgs_per_osd) * 100) || 0;
+    chart.labels = [
+      `${this.i18n('Clean')} (${this.calcPercentage(
+        categoryPgAmount['clean'],
+        data.pg_info.pgs_per_osd
+      )}%)`,
+      `${this.i18n('Working')} (${this.calcPercentage(
+        categoryPgAmount['working'],
+        data.pg_info.pgs_per_osd
+      )}%)`,
+      `${this.i18n('Warning')} (${this.calcPercentage(
+        categoryPgAmount['warning'],
+        data.pg_info.pgs_per_osd
+      )}%)`,
+      `${this.i18n('Unknown')} (${this.calcPercentage(
+        categoryPgAmount['unknown'],
+        data.pg_info.pgs_per_osd
+      )}%)`
+    ];
+  }
+
+  prepareObjects(chart, data) {
+    const totalReplicas = data.pg_info.object_stats.num_object_copies;
+    const healthy =
+      totalReplicas -
+      data.pg_info.object_stats.num_objects_misplaced -
+      data.pg_info.object_stats.num_objects_degraded -
+      data.pg_info.object_stats.num_objects_unfound;
 
     chart.labels = [
-      `${this.i18n('Clean')} (${calcPercentage('clean')}%)`,
-      `${this.i18n('Working')} (${calcPercentage('working')}%)`,
-      `${this.i18n('Warning')} (${calcPercentage('warning')}%)`,
-      `${this.i18n('Unknown')} (${calcPercentage('unknown')}%)`
+      `${this.i18n('Healthy')} (${this.calcPercentage(healthy, totalReplicas)}%)`,
+      `${this.i18n('Misplaced')} (${this.calcPercentage(
+        data.pg_info.object_stats.num_objects_misplaced,
+        totalReplicas
+      )}%)`,
+      `${this.i18n('Degraded')} (${this.calcPercentage(
+        data.pg_info.object_stats.num_objects_degraded,
+        totalReplicas
+      )}%)`,
+      `${this.i18n('Unfound')} (${this.calcPercentage(
+        data.pg_info.object_stats.num_objects_unfound,
+        totalReplicas
+      )}%)`
     ];
+
+    chart.dataset[0].data = [
+      healthy,
+      data.pg_info.object_stats.num_objects_misplaced,
+      data.pg_info.object_stats.num_objects_degraded,
+      data.pg_info.object_stats.num_objects_unfound
+    ];
+
+    chart.options.title.text = `${this.dimless.transform(
+      data.pg_info.object_stats.num_objects
+    )} ${this.i18n('total')} (${this.dimless.transform(totalReplicas)} ${this.i18n('replicas')})`;
+
+    chart.options.maintainAspectRatio = window.innerWidth >= 375;
   }
 
   isClientReadWriteChartShowable() {
@@ -148,5 +223,13 @@ export class HealthComponent implements OnInit, OnDestroy {
     const writeOps = this.healthData.client_perf.write_op_per_sec || 0;
 
     return readOps + writeOps > 0;
+  }
+
+  private calcPercentage(dividend: number, divisor: number) {
+    if (!_.isNumber(dividend) || !_.isNumber(divisor) || divisor === 0) {
+      return 0;
+    }
+
+    return Math.round((dividend / divisor) * 100);
   }
 }
