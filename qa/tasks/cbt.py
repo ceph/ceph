d9d473e4307897ec2b588a20495b5692fd91ef4f
Neha Ojha <nojha@redhat.com>
2019-06-06 17:21:38 -0700

    qa/tasks/cbt.py: change port to work with client_endpoints
    hack to work with https://github.com/ceph/cbt/pull/160/
    
    Signed-off-by: Neha Ojha <nojha@redhat.com>

--- qa/tasks/cbt.py
+++ qa/tasks/cbt.py
@@ -61,7 +61,7 @@ class CBT(Task):
             remotes_and_roles = self.ctx.cluster.remotes.items()
             ips = [host for (host, port) in
                    (remote.ssh.get_transport().getpeername() for (remote, role_list) in remotes_and_roles)]
-            benchmark_config['cosbench']['auth'] = "username=cosbench:operator;password=intel2012;url=http://%s:7280/auth/v1.0;retry=9" %(ips[0])
+            benchmark_config['cosbench']['auth'] = "username=cosbench:operator;password=intel2012;url=http://%s:80/auth/v1.0;retry=9" %(ips[0])
 
         return dict(
             cluster=cluster_config,
