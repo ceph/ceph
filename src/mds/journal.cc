ebb174ad6d56c1466d4a81eeccf432b8b84dd36f
Yan, Zheng <zyan@redhat.com>
2019-06-08 13:08:21 +0800

    mds: fix corner case of replaying open sessions
    Marking a session dirty may flush all existing dirty sessions. MDS
    calls Server::finish_force_open_sessions() for log event that opens
    multiple sessions. The function marks sessions dirty one by one. So
    sessions opened by a log event may get flushed partially.
    
    When replaying a log event that opens multiple sessions, mds need to
    check if some of these sessions have already been flushed.
    
    Fixes: https://tracker.ceph.com/issues/40211
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

--- src/mds/journal.cc
+++ src/mds/journal.cc
@@ -1778,8 +1778,7 @@ void ESessions::replay(MDSRank *mds)
   } else {
     dout(10) << "ESessions.replay sessionmap " << mds->sessionmap.get_version()
 	     << " < " << cmapv << dendl;
-    mds->sessionmap.replay_open_sessions(client_map, client_metadata_map);
-    ceph_assert(mds->sessionmap.get_version() == cmapv);
+    mds->sessionmap.replay_open_sessions(cmapv, client_map, client_metadata_map);
   }
   update_segment();
 }
@@ -2062,9 +2061,7 @@ void EUpdate::replay(MDSRank *mds)
       decode(cm, blp);
       if (!blp.end())
 	decode(cmm, blp);
-      mds->sessionmap.replay_open_sessions(cm, cmm);
-
-      ceph_assert(mds->sessionmap.get_version() == cmapv);
+      mds->sessionmap.replay_open_sessions(cmapv, cm, cmm);
     }
   }
   update_segment();
@@ -2910,15 +2907,7 @@ void EImportStart::replay(MDSRank *mds)
     decode(cm, blp);
     if (!blp.end())
       decode(cmm, blp);
-    mds->sessionmap.replay_open_sessions(cm, cmm);
-
-    if (mds->sessionmap.get_version() != cmapv) {
-      derr << "sessionmap version " << mds->sessionmap.get_version()
-           << " != cmapv " << cmapv << dendl;
-      mds->clog->error() << "failure replaying journal (EImportStart)";
-      mds->damaged();
-      ceph_abort();  // Should be unreachable because damaged() calls respawn()
-    }
+    mds->sessionmap.replay_open_sessions(cmapv, cm, cmm);
   }
   update_segment();
 }
