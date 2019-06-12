ac5aca48c7d427f9cd06b2421d5f1e05d202caad
Greg Farnum <gfarnum@redhat.com>
2019-06-12 15:55:14 -0700

    elector: rename handle_propose_logic -> receive_propose
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>
    (cherry picked from commit ba37ca9ccb84391b747b6f15ce1357031b7b88f6)
    
    Resolves: rhbz#1800382

--- src/mon/Elector.h
+++ src/mon/Elector.h
@@ -43,7 +43,7 @@ public:
   void start();
   void defer(int who);
   void end_election_period();
-  void handle_propose_logic(epoch_t mepoch, int from);
+  void receive_propose(epoch_t mepoch, int from);
   void receive_ack(int from, epoch_t from_epoch);
   bool receive_victory_claim(int from, epoch_t from_epoch);
   void declare_victory();
