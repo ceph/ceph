bc0553147b229855def6da92e4dd2b58a32ba9ad
Sage Weil <sage@redhat.com>
2019-06-12 16:47:53 -0500

    mon/OSDMonitor: do not bother reporting gaps in removed_snaps
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -3936,9 +3936,6 @@ void OSDMonitor::send_incremental(epoch_t first,
     m->oldest_map = get_first_committed();
     m->newest_map = osdmap.get_epoch();
 
-    // share removed snaps during the gap
-    get_removed_snaps_range(first, m->oldest_map, &m->gap_removed_snaps);
-
     first = get_first_committed();
     bufferlist bl;
     int err = get_version_full(first, features, bl);
@@ -3975,28 +3972,6 @@ void OSDMonitor::send_incremental(epoch_t first,
     session->osd_epoch = last;
     if (onetime || req)
       break;
-  }
-}
-
-void OSDMonitor::get_removed_snaps_range(
-  epoch_t start, epoch_t end,
-  mempool::osdmap::map<int64_t,snap_interval_set_t> *gap_removed_snaps)
-{
-  // we only care about pools that exist now.
-  for (auto& p : osdmap.get_pools()) {
-    auto& t = (*gap_removed_snaps)[p.first];
-    for (epoch_t epoch = start; epoch < end; ++epoch) {
-      string k = make_removed_snap_epoch_key(p.first, epoch);
-      bufferlist v;
-      mon->store->get(OSD_SNAP_PREFIX, k, v);
-      if (v.length()) {
-	auto q = v.cbegin();
-	snap_interval_set_t snaps;
-	decode(snaps, q);
-	t.union_of(snaps);
-      }
-    }
-    dout(10) << __func__ << " " << p.first << " " << t << dendl;
   }
 }
 
