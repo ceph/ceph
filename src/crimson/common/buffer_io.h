6c7b3ea216f50b20eadce7d8a52c3063b98554f0
Kefu Chai <kchai@redhat.com>
2019-06-14 11:49:47 +0800

    crimson: add buffer_io helpers
    Signed-off-by: Kefu Chai <kchai@redhat.com>

---
+++ src/crimson/common/buffer_io.h
@@ -0,0 +1,19 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#pragma once
+
+#include <seastar/core/future.hh>
+#include <seastar/core/file-types.hh>
+
+#include "include/buffer_fwd.h"
+
+namespace ceph::buffer {
+  seastar::future<> write_file(ceph::buffer::list&& bl,
+                               seastar::sstring fn,
+                               seastar::file_permissions= // 0644
+                                 (seastar::file_permissions::user_read |
+                                  seastar::file_permissions::user_write |
+                                  seastar::file_permissions::group_read |
+                                  seastar::file_permissions::others_read));
+}
