d1bbc48d8a6f534882169808e424fba1c166e905
Sage Weil <sage@redhat.com>
2019-06-17 13:55:56 -0500

    osd: track clock delta between peer OSDs
    We need to keep track of the monotonic clock deltas between peer OSDs in
    order to be able to exchange timestamps across messages.  We need an
    upper and lower bound on this delta, depending on the context where it is
    used.
    
    We can use the existing ping messages to get this by assuming that a ping
    message is sent instantly to get a bound, and to share our delta in a
    follow-up reply to share the other bound.  The ping sender will get both
    bounds with a single ping + ping_reply exchange.  The ping receiver will
    get the delta value from the next round's ping.
    
    Include up_from in the ping messages to ensure we don't mix up different
    instances of the same OSD.
    
    Signed-off-by: Sage Weil <sage@inktank.com>

--- src/osd/OSD.h
+++ src/osd/OSD.h
@@ -850,6 +850,15 @@ public:
 
   void request_osdmap_update(epoch_t e);
 
+  // -- heartbeats --
+  ceph::mutex hb_stamp_lock = ceph::make_mutex("OSDServce::hb_stamp_lock");
+
+  /// osd -> heartbeat stamps
+  vector<HeartbeatStampsRef> hb_stamps;
+
+  /// get or create a ref for a peer's HeartbeatStamps
+  HeartbeatStampsRef get_hb_stamps(unsigned osd);
+
   // -- stopping --
   ceph::mutex is_stopping_lock = ceph::make_mutex("OSDService::is_stopping_lock");
   ceph::condition_variable is_stopping_cond;
@@ -1436,11 +1445,7 @@ private:
       return !is_unhealthy(now);
     }
   };
-  /// state attached to outgoing heartbeat connections
-  struct HeartbeatSession : public RefCountedObject {
-    int peer;
-    explicit HeartbeatSession(int p) : peer(p) {}
-  };
+
   ceph::mutex heartbeat_lock = ceph::make_mutex("OSD::heartbeat_lock");
   map<int, int> debug_heartbeat_drops_remaining;
   ceph::condition_variable heartbeat_cond;
