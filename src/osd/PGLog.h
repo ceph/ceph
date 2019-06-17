3a2ac07c90d33909248c2c35720ed1f4003ce1ae
xie xingguo <xie.xingguo@zte.com.cn>
2019-06-17 11:05:31 +0800

    osd: do not invalidate clear_regions of missing item at boot
    Seems we'll always mark clear_regions as all dirty
    when reading pg logs and missing items off the disk,
    which as a result turns incremental recovery off by default.
    Also using std::move seems to be a bit more efficient
    and robust here.
    
    Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

--- src/osd/PGLog.h
+++ src/osd/PGLog.h
@@ -1405,7 +1405,7 @@ public:
 	  if (item.is_delete()) {
 	    ceph_assert(missing.may_include_deletes);
 	  }
-	  missing.add(oid, item.need, item.have, item.is_delete());
+	  missing.add(oid, std::move(item));
 	} else if (p->key().substr(0, 4) == string("dup_")) {
 	  pg_log_dup_t dup;
 	  decode(dup, bp);
@@ -1653,7 +1653,7 @@ public:
 	if (item.is_delete()) {
 	  ceph_assert(missing.may_include_deletes);
 	}
-	missing.add(oid, item.need, item.have, item.is_delete());
+	missing.add(oid, std::move(item));
       } else if (p.first.substr(0, 4) == string("dup_")) {
 	pg_log_dup_t dup;
 	decode(dup, bp);
