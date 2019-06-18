7cbdf312374f45dc4766cc205d9ce867a45cd189
Kefu Chai <kchai@redhat.com>
2019-06-18 20:59:17 +0800

    src/: parser returns up to one error
    since config parser bails out at seeing the first error, there is no
    need to offer the interfaces to report multiple errors.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/common/common_init.cc
+++ src/common/common_init.cc
@@ -71,25 +71,13 @@ CephContext *common_preinit(const CephInitParameters &iparams,
 }
 #endif	// #ifndef WITH_SEASTAR
 
-void complain_about_parse_errors(CephContext *cct,
-				 std::deque<std::string> *parse_errors)
+void complain_about_parse_error(CephContext *cct,
+				const string& parse_error)
 {
-  if (parse_errors->empty())
+  if (parse_error.empty())
     return;
   lderr(cct) << "Errors while parsing config file!" << dendl;
-  int cur_err = 0;
-  static const int MAX_PARSE_ERRORS = 20;
-  for (std::deque<std::string>::const_iterator p = parse_errors->begin();
-       p != parse_errors->end(); ++p)
-  {
-    lderr(cct) << *p << dendl;
-    if (cur_err == MAX_PARSE_ERRORS) {
-      lderr(cct) << "Suppressed " << (parse_errors->size() - MAX_PARSE_ERRORS)
-	   << " more errors." << dendl;
-      break;
-    }
-    ++cur_err;
-  }
+  lderr(cct) << parse_error << dendl;
 }
 
 #ifndef WITH_SEASTAR
