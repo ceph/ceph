b4a8d496153425323a60aa78e85b715ce2f1990c
Dziomdziora, El?bieta <elzbieta.dziomdziora@ts.fujitsu.coom>
2019-06-21 18:44:36 +0200

    mgr/dashboard: Support minimum password complexity rules
    Fixes: https://tracker.ceph.com/issues/25232
    Signed-off-by: Dziomdziora, Elżbieta <elzbieta.dziomdziora@ts.fujitsu.coom>

--- src/pybind/mgr/dashboard/controllers/user.py
+++ src/pybind/mgr/dashboard/controllers/user.py
@@ -10,8 +10,40 @@ from ..exceptions import DashboardException, UserAlreadyExists, \
 from ..security import Scope
 from ..services.access_control import SYSTEM_ROLES
 from ..services.auth import JwtManager
+import re
 
 
+# minimum password complexity rules
+def check_password_complexity(password, username):
+    """
+    Suggested rules for password complexity
+    - at least 6 chars in length
+    - must not be the same as the user account name
+    - consist of characters from the following groups
+    - alphabetic a-z, A-Z
+    - numbers 0-9
+    - special chars: !_@
+    - must use at least 1 special char
+    """
+    if password == username:
+        raise DashboardException(msg='Password is the same as the username.\
+                                      It has to be different',
+                                 code='password-the-same-as-username',
+                                 component='Update/Create user')
+
+    password_min_complex = re.compile('^(?=\\S{6,20}$)(?=.*[!_@])(?=.*[a-z])'
+                                      '(?=.*[A-Z])(?=.*[0-9])')
+
+    if not password_min_complex.match(password):
+        raise DashboardException(msg='Password is not strong enough. <br/>\
+                                      It has to contains at least 6 chars. <br/>\
+                                      It must use at least 1 special char (!_@). <br/>\
+                                      It has to consist of alphabetic\
+                                       (a-z and A-Z) and numeric (0-9) chars.',
+                                 code='not-strong-enough-password',
+                                 component='Update/Create user')
+
+                                 
 @ApiController('/user', Scope.USER)
 class User(RESTController):
     @staticmethod
@@ -52,6 +84,8 @@ class User(RESTController):
         if roles:
             user_roles = User._get_user_roles(roles)
         try:
+            if password:
+                check_password_complexity(password, username)
             user = mgr.ACCESS_CTRL_DB.create_user(username, password, name, email)
         except UserAlreadyExists:
             raise DashboardException(msg='Username already exists',
@@ -79,10 +113,15 @@ class User(RESTController):
             user = mgr.ACCESS_CTRL_DB.get_user(username)
         except UserDoesNotExist:
             raise cherrypy.HTTPError(404)
+        if not password and not name and not email and (user.to_dict())['roles'] == roles:
+            raise DashboardException(msg='All fields are empty, cannot update the user',
+                                     code='cannot_update_user',
+                                     component='user')
         user_roles = []
         if roles:
             user_roles = User._get_user_roles(roles)
         if password:
+            check_password_complexity(password, username)
             user.set_password(password)
         user.name = name
         user.email = email
