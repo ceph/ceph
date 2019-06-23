5c00cc30a1ad49b01e4ab84a17b62def88fc83c4
Ramana Raja <rraja@redhat.com>
2019-06-23 17:39:36 +0530

    mgr/volumes: allow setting mode
    ... of fs subvolumes and subvolume groups during their creation.
    
    Fixes: https://tracker.ceph.com/issues/40299
    Signed-off-by: Ramana Raja <rraja@redhat.com>

--- src/pybind/mgr/volumes/module.py
+++ src/pybind/mgr/volumes/module.py
@@ -47,9 +47,10 @@ class Module(orchestrator.OrchestratorClientMixin, MgrModule):
             'cmd': 'fs subvolumegroup create '
                    'name=vol_name,type=CephString '
                    'name=group_name,type=CephString '
-                   'name=pool_layout,type=CephString,req=false ',
+                   'name=pool_layout,type=CephString,req=false '
+                   'name=mode,type=CephString,req=false ',
             'desc': "Create a CephFS subvolume group in a volume, and optionally, "
-                    "with a specific data pool layout",
+                    "with a specific data pool layout, and a specific numeric mode",
             'perm': 'rw'
         },
         {
@@ -66,10 +67,11 @@ class Module(orchestrator.OrchestratorClientMixin, MgrModule):
                    'name=sub_name,type=CephString '
                    'name=size,type=CephInt,req=false '
                    'name=group_name,type=CephString,req=false '
-                   'name=pool_layout,type=CephString,req=false ',
+                   'name=pool_layout,type=CephString,req=false '
+                   'name=mode,type=CephString,req=false ',
             'desc': "Create a CephFS subvolume in a volume, and optionally, "
-                    "with a specific size (in bytes), a specific data pool layout "
-                    "and in a specific subvolume group",
+                    "with a specific size (in bytes), a specific data pool layout, "
+                    "a specific mode, and in a specific subvolume group",
             'perm': 'rw'
         },
         {
@@ -201,8 +203,9 @@ class Module(orchestrator.OrchestratorClientMixin, MgrModule):
         vol_name = cmd['vol_name']
         group_name = cmd['group_name']
         pool_layout = cmd.get('pool_layout', None)
+        mode = cmd.get('mode', '755')
 
-        return self.vc.create_subvolume_group(vol_name, group_name, pool=pool_layout)
+        return self.vc.create_subvolume_group(vol_name, group_name, mode=mode, pool=pool_layout)
 
     def _cmd_fs_subvolumegroup_rm(self, inbuf, cmd):
         """
@@ -223,8 +226,9 @@ class Module(orchestrator.OrchestratorClientMixin, MgrModule):
         size = cmd.get('size', None)
         group_name = cmd.get('group_name', None)
         pool_layout = cmd.get('pool_layout', None)
+        mode = cmd.get('mode', '755')
 
-        return self.vc.create_subvolume(vol_name, sub_name, group_name, size, pool=pool_layout)
+        return self.vc.create_subvolume(vol_name, sub_name, group_name, size, mode=mode, pool=pool_layout)
 
     def _cmd_fs_subvolume_rm(self, inbuf, cmd):
         """
