f49c9952eb881ec62241d9183d50cfd03ae97b05
Jos Collin <jcollin@redhat.com>
2019-07-01 14:32:33 +0530

    rgw: Silence warning: control reaches end of non-void function
    Build shows:
    [ 53%] Building CXX object src/mds/CMakeFiles/mds.dir/JournalPointer.cc.o
    ceph/src/rgw/rgw_rest_s3.cc: In member function ‘RGWOp* RGWHandler_REST_Bucket_S3::get_obj_op(bool)’:
    ceph/src/rgw/rgw_rest_s3.cc:3588:5: warning: control reaches end of non-void function [-Wreturn-type]
       }   }
         ^
    Fixes: 4ffc765c4c5debc665ade7769c4647c3a7278fd2
    Fixes: http://tracker.ceph.com/issues/40747
    Signed-off-by: Jos Collin <jcollin@redhat.com>
    (cherry picked from commit abb2451dd5164e6b610589207b900a6464e21282)
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>
    
    Conflicts:
    	src/rgw/rgw_rest_s3.cc

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -2662,7 +2662,7 @@ void RGWGetLC_ObjStore_S3::execute()
 void RGWGetLC_ObjStore_S3::send_response()
 {
   if (op_ret) {
-    if (op_ret == -ENOENT) {	
+    if (op_ret == -ENOENT) {
       set_req_state_err(s, ERR_NO_SUCH_LC);
     } else {
       set_req_state_err(s, op_ret);
@@ -3527,14 +3527,20 @@ RGWOp *RGWHandler_REST_Bucket_S3::get_obj_op(bool get_data)
   s->info.args.get_int("list-type", &list_type, 1);
 
    // Non-website mode    // Non-website mode
-  if (get_data) {   
-    if (list_type == 1) {
-       return new RGWListBucket_ObjStore_S3;     
-    } else if(list_type == 2) {
-      return new RGWListBucket_ObjStore_S3v2;
-    } } else {
-    return new RGWStatBucket_ObjStore_S3;    
-  }   }
+  if (get_data) {
+    switch (list_type) {
+      case 1:
+        return new RGWListBucket_ObjStore_S3;
+      case 2:
+        return new RGWListBucket_ObjStore_S3v2;
+      default:
+        ldpp_dout(s, 5) << __func__ << ": unsupported list-type " << list_type << dendl;
+        return new RGWListBucket_ObjStore_S3;
+    }
+  } else {
+    return new RGWStatBucket_ObjStore_S3;
+  }
+}
 
 RGWOp *RGWHandler_REST_Bucket_S3::op_get()
 {
