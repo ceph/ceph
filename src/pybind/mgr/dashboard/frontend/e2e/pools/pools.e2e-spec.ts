9a03376603e9536b9ce213585f988d8289b02015
Patrick Nawracay <pnawracay@suse.com>
2019-07-01 17:42:55 +0200

    mgr/dashboard: Add rudimentary pool create/exist/delete test
    Fixes: http://tracker.ceph.com/issues/38093
    
    Signed-off-by: Patrick Nawracay <pnawracay@suse.com>

--- src/pybind/mgr/dashboard/frontend/e2e/pools/pools.e2e-spec.ts
+++ src/pybind/mgr/dashboard/frontend/e2e/pools/pools.e2e-spec.ts
@@ -3,9 +3,13 @@ import { PoolPageHelper } from './pools.po';
 
 describe('Pools page', () => {
   let page: PoolPageHelper;
+  let helper: Helper;
+  const poolName = 'pool_e2e_pool_test';
 
   beforeAll(() => {
     page = new PoolPageHelper();
+    helper = new Helper();
+    page.navigateTo();
   });
 
   afterEach(() => {
@@ -13,10 +17,6 @@ describe('Pools page', () => {
   });
 
   describe('breadcrumb and tab tests', () => {
-    beforeAll(() => {
-      page.navigateTo();
-    });
-
     it('should open and show breadcrumb', () => {
       expect(PoolPageHelper.getBreadcrumbText()).toEqual('Pools');
     });
@@ -31,6 +31,24 @@ describe('Pools page', () => {
 
     it('should show overall performance as a second tab', () => {
       expect(PoolPageHelper.getTabText(1)).toEqual('Overall Performance');
+    });
+  });
+
+  it('should create a pool', () => {
+    helper.pools.exist(poolName, false).then(() => {
+      helper.pools.navigateTo('create');
+      helper.pools.create(poolName, 8).then(() => {
+        helper.pools.navigateTo();
+        helper.pools.exist(poolName, true);
+      });
+    });
+  });
+
+  it('should delete a pool', () => {
+    helper.pools.exist(poolName);
+    helper.pools.delete(poolName).then(() => {
+      helper.pools.navigateTo();
+      helper.pools.exist(poolName, false);
     });
   });
 });
