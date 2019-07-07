d5cbb94bcb7d9343efbab217a800d84414adcf8a
Kefu Chai <kchai@redhat.com>
2019-07-07 11:20:13 +0800

    common: s/Mutex/ceph::mutex/
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/common/Cond.h
+++ src/common/Cond.h
@@ -126,17 +126,17 @@ class Cond {
  * assume the caller is holding the appropriate lock.
  */
 class C_Cond : public Context {
-  Cond *cond;   ///< Cond to signal
+  ceph::condition_variable& cond;   ///< Cond to signal
   bool *done;   ///< true if finish() has been called
   int *rval;    ///< return value
 public:
-  C_Cond(Cond *c, bool *d, int *r) : cond(c), done(d), rval(r) {
+  C_Cond(ceph::condition_variable &c, bool *d, int *r) : cond(c), done(d), rval(r) {
     *done = false;
   }
   void finish(int r) override {
     *done = true;
     *rval = r;
-    cond->Signal();
+    cond.notify_all();
   }
 };
 
@@ -148,21 +148,21 @@ public:
  * already hold it.
  */
 class C_SafeCond : public Context {
-  Mutex *lock;    ///< Mutex to take
-  Cond *cond;     ///< Cond to signal
+  ceph::mutex& lock;    ///< Mutex to take
+  ceph::condition_variable& cond;     ///< Cond to signal
   bool *done;     ///< true after finish() has been called
   int *rval;      ///< return value (optional)
 public:
-  C_SafeCond(Mutex *l, Cond *c, bool *d, int *r=0) : lock(l), cond(c), done(d), rval(r) {
+  C_SafeCond(ceph::mutex& l, ceph::condition_variable& c, bool *d, int *r=0)
+    : lock(l), cond(c), done(d), rval(r) {
     *done = false;
   }
   void finish(int r) override {
-    lock->lock();
+    std::lock_guard l{lock};
     if (rval)
       *rval = r;
     *done = true;
-    cond->Signal();
-    lock->unlock();
+    cond.notify_all();
   }
 };
 
@@ -173,13 +173,16 @@ public:
  * until wait() returns.
  */
 class C_SaferCond : public Context {
-  Mutex lock;    ///< Mutex to take
-  Cond cond;     ///< Cond to signal
-  bool done;     ///< true after finish() has been called
-  int rval;      ///< return value
+  ceph::mutex lock;  ///< Mutex to take
+  ceph::condition_variable cond;     ///< Cond to signal
+  bool done = false; ///< true after finish() has been called
+  int rval = 0;      ///< return value
 public:
-  C_SaferCond() : lock("C_SaferCond"), done(false), rval(0) {}
-  explicit C_SaferCond(const std::string &name) : lock(name), done(false), rval(0) {}
+  C_SaferCond() :
+    C_SaferCond("C_SaferCond")
+  {}
+  explicit C_SaferCond(const std::string &name)
+    : lock(ceph::make_mutex(name)) {}
   void finish(int r) override { complete(r); }
 
   /// We overload complete in order to not delete the context
@@ -187,27 +190,27 @@ public:
     std::lock_guard l(lock);
     done = true;
     rval = r;
-    cond.Signal();
+    cond.notify_all();
   }
 
   /// Returns rval once the Context is called
   int wait() {
-    std::lock_guard l(lock);
-    while (!done)
-      cond.Wait(lock);
+    std::unique_lock l{lock};
+    cond.wait(l, [this] { return done;});
     return rval;
   }
 
   /// Wait until the \c secs expires or \c complete() is called
   int wait_for(double secs) {
-    utime_t interval;
-    interval.set_from_double(secs);
-    std::lock_guard l{lock};
+    std::unique_lock l{lock};
     if (done) {
       return rval;
     }
-    cond.WaitInterval(lock, interval);
-    return done ? rval : ETIMEDOUT;
+    if (cond.wait_for(l, ceph::make_timespan(secs), [this] { return done; })) {
+      return rval;
+    } else {
+      return ETIMEDOUT;
+    }
   }
 };
 
