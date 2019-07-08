37a79dd20996abffc8e7cbdbc477043637b0537b
Sage Weil <sage@redhat.com>
2019-07-08 17:28:01 -0500

    osd: record startup_time
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -503,6 +503,7 @@ void OSDService::request_osdmap_update(epoch_t e)
   osd->osdmap_subscribe(e, false);
 }
 
+
 class AgentTimeoutCB : public Context {
   PGRef pg;
 public:
@@ -2798,6 +2799,8 @@ int OSD::init()
     r = -EINVAL;
     goto out;
   }
+
+  startup_time = ceph::mono_clock::now();
 
   // load up "current" osdmap
   assert_warn(!osdmap);
