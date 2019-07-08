cb12633200785245cd7e25a1cb0abfbb0f848556
Patrick Donnelly <pdonnell@redhat.com>
2019-07-08 10:30:26 -0700

    mds: apply configuration changes through MDSRank
    This avoids the need to acquire the mds_lock prior to responding to config
    changes. The MDSRank can create a finisher context that acquires the lock
    later.
    
    Fixes: https://tracker.ceph.com/issues/40694
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- src/mds/MDSDaemon.h
+++ src/mds/MDSDaemon.h
@@ -40,7 +40,7 @@
 class Messenger;
 class MonClient;
 
-class MDSDaemon : public Dispatcher, public md_config_obs_t {
+class MDSDaemon : public Dispatcher {
  public:
   /* Global MDS lock: every time someone takes this, they must
    * also check the `stopping` flag.  If stopping is true, you
@@ -92,11 +92,6 @@ class MDSDaemon : public Dispatcher, public md_config_obs_t {
    * such that deleting xlists doesn't assert.
    */
   bool is_clean_shutdown();
-
-  // config observer bits
-  const char** get_tracked_conf_keys() const override;
-  void handle_conf_change(const ConfigProxy& conf,
-			  const std::set <std::string> &changed) override;
  protected:
   // tick and other timer fun
   Context *tick_event = nullptr;
