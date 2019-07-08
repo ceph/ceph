b66560d3b274eb3108a6a2abb14fb80fcbb34461
Igor Fedotov <ifedotov@suse.com>
2019-07-08 17:32:10 +0300

    os/kv: add static method to parse RocksDB options
    Signed-off-by: Igor Fedotov <ifedotov@suse.com>
    (cherry picked from commit 518faf426ddd3142813963901d31e5675ee34eec)

--- src/kv/RocksDBStore.h
+++ src/kv/RocksDBStore.h
@@ -114,6 +114,7 @@ class RocksDBStore : public KeyValueDB {
 
   void compact_range(const string& start, const string& end);
   void compact_range_async(const string& start, const string& end);
+  int tryInterpret(const string& key, const string& val, rocksdb::Options& opt);
 
 public:
   /// compact the underlying rocksdb store
@@ -127,8 +128,12 @@ public:
     compact_range_async(string(), string());
   }
 
-  int tryInterpret(const string& key, const string& val, rocksdb::Options &opt);
-  int ParseOptionsFromString(const string& opt_str, rocksdb::Options &opt);
+  int ParseOptionsFromString(const string& opt_str, rocksdb::Options& opt);
+  static int ParseOptionsFromStringStatic(
+    CephContext* cct,
+    const string& opt_str,
+    rocksdb::Options &opt,
+    function<int(const string&, const string&, rocksdb::Options&)> interp);
   static int _test_init(const string& dir);
   int init(string options_str) override;
   /// compact rocksdb for all keys with a given prefix
