e84c4474d2edbb4ea906d2b1ed951e1251e96199
Jason Dillaman <dillaman@redhat.com>
2019-07-16 13:54:16 -0400

    mon/mgr: add 'rbd_support' to list of always-on mgr modules
    The 'rbd_support' mgr module provides many background functions that
    are required by the 'rbd' CLI performance queries, as well as future
    background long-running maintenance tasks.
    
    Fixes: http://tracker.ceph.com/issues/40790
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>
    (cherry picked from commit 7231aab4b844729c1c35da37515dce58026c26a2)
    
    Conflicts:
    	src/mon/MgrMonitor.cc: trivial resolution

--- src/mon/MgrMonitor.cc
+++ src/mon/MgrMonitor.cc
@@ -48,6 +48,7 @@ const static std::map<uint32_t, std::set<std::string>> always_on_modules = {
       "balancer",
       "devicehealth",
       "orchestrator_cli",
+      "rbd_support",
       "volumes",
     }
   }
