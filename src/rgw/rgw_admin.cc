04c2177fbe8b5d2d09e2dc3a409dd7e61cdf9227
Casey Bodley <cbodley@redhat.com>
2019-07-17 13:59:19 -0400

    radosgw-admin: bucket sync status not 'caught up' during full sync
    the 'radosgw-admin bucket sync status' command will incorrectly report
    that the "bucket is caught up with source" if some shards are still on
    full sync
    
    Fixes: http://tracker.ceph.com/issues/40806
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 9e05341a53d4cbb6f5d2018b8f29c1a32e0506a9)

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -2415,11 +2415,11 @@ static int bucket_source_sync_status(RGWRados *store, const RGWZone& zone,
       shards_behind.insert(shard_id);
     }
   }
-  if (shards_behind.empty()) {
-    out << indented{width} << "bucket is caught up with source\n";
-  } else {
+  if (!shards_behind.empty()) {
     out << indented{width} << "bucket is behind on " << shards_behind.size() << " shards\n";
     out << indented{width} << "behind shards: [" << shards_behind << "]\n" ;
+  } else if (!num_full) {
+    out << indented{width} << "bucket is caught up with source\n";
   }
   return 0;
 }
