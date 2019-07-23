5ce14ded9a9c5b6c1ac93afbfeb757e6b2ca75af
Adam King <kingamk3@gmail.com>
2019-07-23 15:51:21 -0400

    mgr/dashboard: RGW user E2E Tests
    Tests creating, editing and deleting RGW users.
    Tests providing invalid input to user creation and editing forms.
    
    Fixes: https://tracker.ceph.com/issues/40364
    Fixes: https://tracker.ceph.com/issues/40908
    
    Signed-off-by: Adam King <adking@redhat.com>
    Signed-off-by: Rafael Quintero <rquinter@redhat.com>

--- src/pybind/mgr/dashboard/frontend/e2e/rgw/users.e2e-spec.ts
+++ src/pybind/mgr/dashboard/frontend/e2e/rgw/users.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Helper } from '../helper.po';
 
 describe('RGW users page', () => {
   let users;
+  const user_name = '000user_create_edit_delete';
 
   beforeAll(() => {
     users = new Helper().users;
@@ -18,6 +19,41 @@ describe('RGW users page', () => {
 
     it('should open and show breadcrumb', () => {
       expect(users.getBreadcrumbText()).toEqual('Users');
+    });
+  });
+
+  describe('create, edit & delete user test', () => {
+    beforeAll(() => {
+      users.navigateTo();
+    });
+
+    it('should create user', () => {
+      users.create(user_name, 'Some Name', 'original@website.com', '1200');
+      expect(users.getTableCell(user_name).isPresent()).toBe(true);
+    });
+
+    it('should edit users full name, email and max buckets', () => {
+      users.edit(user_name, 'Another Identity', 'changed@othersite.com', '1969');
+      // checks for succsessful editing are done within edit function
+    });
+
+    it('should delete user', () => {
+      users.delete(user_name);
+      expect(users.getTableCell(user_name).isPresent()).toBe(false);
+    });
+  });
+
+  describe('Invalid input test', () => {
+    beforeAll(() => {
+      users.navigateTo();
+    });
+
+    it('should put invalid input into user creation form and check fields are marked invalid', () => {
+      users.invalidCreate();
+    });
+
+    it('should put invalid input into user edit form and check fields are marked invalid', () => {
+      users.invalidEdit();
     });
   });
 });
