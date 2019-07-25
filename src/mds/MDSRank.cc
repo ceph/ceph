ff475d963491b07c0b3a7ef4cfea79a7f73686ec
Venky Shankar <vshankar@redhat.com>
2019-07-25 09:16:18 -0400

    mds: send scrub state changes to cluster log.
    ... also log new and completed scrubs.
    
    Signed-off-by: Venky Shankar <vshankar@redhat.com>

--- src/mds/MDSRank.cc
+++ src/mds/MDSRank.cc
@@ -529,7 +529,7 @@ MDSRank::MDSRank(
   mdlog = new MDLog(this);
   balancer = new MDBalancer(this, messenger, monc);
 
-  scrubstack = new ScrubStack(mdcache, finisher);
+  scrubstack = new ScrubStack(mdcache, clog, finisher);
 
   inotable = new InoTable(this);
   snapserver = new SnapServer(this, monc);
