2e68902810c5a3f1779731bf790b5f1d3aa78cf4
Rafael Quintero <rafaelq@bu.edu>
2019-07-29 09:02:12 -0400

    mgr/dashboard: E2E Test for RBD Mirroring and Images
    Fixes: https://tracker.ceph.com/issues/38701
    Fixes: https://tracker.ceph.com/issues/40581
    Fixes: https://tracker.ceph.com/issues/40956
    
    Signed-off-by: Adam King <adking@redhat.com>
    Signed-off-by: Rafael Quintero <rquinter@redhat.com>

--- src/pybind/mgr/dashboard/frontend/e2e/block/mirroring.e2e-spec.ts
+++ src/pybind/mgr/dashboard/frontend/e2e/block/mirroring.e2e-spec.ts
@@ -2,9 +2,11 @@ import { Helper } from '../helper.po';
 
 describe('Mirroring page', () => {
   let mirroring: Helper['mirroring'];
+  let pools: Helper['pools'];
 
   beforeAll(() => {
     mirroring = new Helper().mirroring;
+    pools = new Helper().pools;
   });
 
   afterEach(() => {
@@ -28,6 +30,36 @@ describe('Mirroring page', () => {
       expect(mirroring.getTabText(0)).toEqual('Issues');
       expect(mirroring.getTabText(1)).toEqual('Syncing');
       expect(mirroring.getTabText(2)).toEqual('Ready');
+    });
+  });
+
+  describe('checks that edit mode functionality shows in the pools table', () => {
+    const poolName = 'mirrorpoolrq';
+
+    beforeAll(() => {
+      pools.navigateTo('create'); // Need pool for mirroring testing
+      pools.create(poolName, 8, 'rbd').then(() => {
+        pools.navigateTo();
+        pools.exist(poolName, true);
+      });
+    });
+
+    it('tests editing mode for pools', () => {
+      mirroring.navigateTo();
+      expect(mirroring.editMirror(poolName, 'Pool'));
+      expect(mirroring.getFirstTableCellWithText('pool').isPresent()).toBe(true);
+      expect(mirroring.editMirror(poolName, 'Image'));
+      expect(mirroring.getFirstTableCellWithText('image').isPresent()).toBe(true);
+      expect(mirroring.editMirror(poolName, 'Disabled'));
+      expect(mirroring.getFirstTableCellWithText('disabled').isPresent()).toBe(true);
+    });
+
+    afterAll(() => {
+      pools.navigateTo(); // Deletes mirroring test pool
+      pools.delete(poolName).then(() => {
+        pools.navigateTo();
+        pools.exist(poolName, false);
+      });
     });
   });
 });
