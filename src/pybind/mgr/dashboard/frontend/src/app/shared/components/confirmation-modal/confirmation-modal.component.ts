cc4b3e0bda680ff92f55c4101da3bcfcfaf45bf0
Stephan Müller <smueller@suse.com>
2019-07-30 17:55:04 +0200

    mgr/dashboard: Add tests for confirmation modal
    Fixes: https://tracker.ceph.com/issues/40828
    Signed-off-by: Stephan Müller <smueller@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/confirmation-modal/confirmation-modal.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/confirmation-modal/confirmation-modal.component.ts
@@ -8,18 +8,21 @@ import { BsModalRef } from 'ngx-bootstrap/modal';
   templateUrl: './confirmation-modal.component.html',
   styleUrls: ['./confirmation-modal.component.scss']
 })
-export class ConfirmationModalComponent implements OnInit {
-  bodyData: object;
+export class ConfirmationModalComponent implements OnInit, OnDestroy {
+  // Needed
   bodyTpl: TemplateRef<any>;
   buttonText: string;
-  onSubmit: Function;
-  onCancel: Function;
   titleText: string;
+  onSubmit: Function;
 
-  bodyContext: object;
-  confirmationForm: FormGroup;
+  // Optional
+  bodyData?: object;
+  onCancel?: Function;
+  bodyContext?: object;
 
+  // Component only
   boundCancel = this.cancel.bind(this);
+  confirmationForm: FormGroup;
 
   constructor(public modalRef: BsModalRef) {
     this.confirmationForm = new FormGroup({});
@@ -28,6 +31,15 @@ export class ConfirmationModalComponent implements OnInit {
   ngOnInit() {
     this.bodyContext = this.bodyContext || {};
     this.bodyContext['$implicit'] = this.bodyData;
+    if (!this.onSubmit) {
+      throw new Error('No submit action defined');
+    } else if (!this.buttonText) {
+      throw new Error('No action name defined');
+    } else if (!this.titleText) {
+      throw new Error('No title defined');
+    } else if (!this.bodyTpl) {
+      throw new Error('No description defined');
+    }
   }
 
   cancel() {
