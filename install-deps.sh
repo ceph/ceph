605814e2c396d413885d1e331dce8e0141325b38
Ernesto Puerta <epuertat@redhat.com>
2019-07-30 18:45:44 +0200

    install-deps.sh: remove failing error catching
    Fixes: https://tracker.ceph.com/issues/41013
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- install-deps.sh
+++ install-deps.sh
@@ -374,7 +374,8 @@ else
 	if [ -n "$dts_ver" ]; then
             ensure_decent_gcc_on_rh $dts_ver
 	fi
-        ! grep -q -i error: $DIR/yum-builddep.out || exit 1
+        IGNORE_YUM_BUILDEP_ERRORS="ValueError: SELinux policy is not managed or store cannot be accessed."
+        sed "/$IGNORE_YUM_BUILDEP_ERRORS/d" $DIR/yum-builddep.out | grep -qi "error:" && exit 1
         # for building python-saml and its dependencies
         $SUDO $yumdnf install -y xmlsec1 xmlsec1-nss xmlsec1-openssl xmlsec1-devel xmlsec1-openssl-devel libtool-ltdl-devel
         ;;
