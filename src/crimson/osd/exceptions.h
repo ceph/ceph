2407f576ef6b48285972243a84b247292badd2c1
Radoslaw Zarzynski <rzarzyns@redhat.com>
2019-07-31 19:38:30 +0200

    crimson/osd: exceptions derive from std::system_error now.
    This change is be useful especially for CEPH_OSD_OP_CALL
    which will be brought by further commits. The issue here
    is that the Ceph Classes will be based on existing iface
    handling errors with usual, int-based ret codes. These
    codes needs to be glued with the existing handling  in
    pg::do_osd_ops().
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/crimson/osd/exceptions.h
+++ src/crimson/osd/exceptions.h
@@ -4,12 +4,33 @@
 #pragma once
 
 #include <exception>
+#include <system_error>
 
-class object_not_found : public std::exception {
+class crimson_error : private std::system_error {
+public:
+  crimson_error(const std::errc ec)
+    : system_error(std::make_error_code(ec)) {
+  }
+
+  using system_error::code;
+  using system_error::what;
+
+private:
+  crimson_error(const int ret) noexcept
+    : system_error(ret, std::system_category()) {
+  }
 };
 
-class object_corrupted : public std::exception {
+
+struct object_not_found : public crimson_error {
+  object_not_found() : crimson_error(std::errc::no_such_file_or_directory) {}
 };
 
-class invalid_argument : public std::exception {
+struct object_corrupted : public crimson_error {
+  object_corrupted() : crimson_error(std::errc::illegal_byte_sequence) {}
 };
+
+struct invalid_argument : public crimson_error {
+  invalid_argument() : crimson_error(std::errc::invalid_argument) {}
+};
+
