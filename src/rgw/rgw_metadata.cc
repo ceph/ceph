48fffc3c30277376b4bd0160b982cbc2387401ed
Casey Bodley <cbodley@redhat.com>
2019-07-31 17:06:13 -0400

    radosgw-admin: 'mdlog trim' loops until done
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 91eef6712ad29d6c15907f1663e6143fc4144dec)
    
    Conflicts: store->time_log_trim() instead of svc.cls->timelog.trim()
    	src/rgw/rgw_metadata.cc no null_yield

--- src/rgw/rgw_metadata.cc
+++ src/rgw/rgw_metadata.cc
@@ -226,14 +226,8 @@ int RGWMetadataLog::trim(int shard_id, const real_time& from_time, const real_ti
   string oid;
   get_shard_oid(shard_id, oid);
 
-  int ret;
-
-  ret = store->time_log_trim(oid, from_time, end_time, start_marker, end_marker);
-
-  if (ret == -ENOENT || ret == -ENODATA)
-    ret = 0;
-
-  return ret;
+  return store->time_log_trim(oid, from_time, end_time, start_marker,
+                              end_marker, nullptr);
 }
   
 int RGWMetadataLog::lock_exclusive(int shard_id, timespan duration, string& zone_id, string& owner_id) {
