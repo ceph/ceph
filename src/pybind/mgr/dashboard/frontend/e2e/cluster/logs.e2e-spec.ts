4d75de9b96561eb2b4cbe11ffce404aaa0fbf047
Adam King <kingamk3@gmail.com>
2019-07-31 14:10:31 -0400

    mgr/dashboard: Logs Page Tests
    Tests that logs page creates audit logs in response to pool creation/deletion
    Tests that logs page creates audit logs in response to configuration changes
    Alter configuration po to put config name in filter box so you can edit configs not on first page (first 10)
    
    Fixes: https://tracker.ceph.com/issues/41035
    
    Signed-off-by: Adam King <adking@redhat.com>
    Signed-off-by: Rafael Quintero <rquinter@redhat.com>

--- src/pybind/mgr/dashboard/frontend/e2e/cluster/logs.e2e-spec.ts
+++ src/pybind/mgr/dashboard/frontend/e2e/cluster/logs.e2e-spec.ts
@@ -2,9 +2,22 @@ import { Helper } from '../helper.po';
 
 describe('Logs page', () => {
   let logs: Helper['logs'];
+  let pools: Helper['pools'];
+  let configuration: Helper['configuration'];
+
+  const poolname = 'logs_e2e_test_pool';
+  const configname = 'log_graylog_port';
+  const today = new Date();
+  let hour = today.getHours();
+  if (hour > 12) {
+    hour = hour - 12;
+  }
+  const minute = today.getMinutes();
 
   beforeAll(() => {
     logs = new Helper().logs;
+    pools = new Helper().pools;
+    configuration = new Helper().configuration;
   });
 
   afterEach(() => {
@@ -30,6 +43,43 @@ describe('Logs page', () => {
 
     it('should show audit logs as a second tab', () => {
       expect(logs.getTabText(1)).toEqual('Audit Logs');
+    });
+  });
+
+  describe('audit logs respond to pool creation and deletion test', () => {
+    it('should create pool and check audit logs reacted', () => {
+      pools.navigateTo('create');
+      pools.create(poolname, 8);
+
+      pools.navigateTo();
+      pools.exist(poolname, true);
+
+      logs.navigateTo();
+      logs.checkAuditForPoolFunction(poolname, 'create', hour, minute);
+    });
+
+    it('should delete pool and check audit logs reacted', () => {
+      pools.navigateTo();
+      pools.delete(poolname);
+
+      pools.navigateTo();
+      pools.exist(poolname, false);
+
+      logs.navigateTo();
+      logs.checkAuditForPoolFunction(poolname, 'delete', hour, minute);
+    });
+  });
+
+  describe('audit logs respond to editing configuration setting test', () => {
+    it('should change config settings and check audit logs reacted', () => {
+      configuration.navigateTo();
+      configuration.edit(configname, ['global', '5']);
+
+      logs.navigateTo();
+      logs.checkAuditForConfigChange(configname, 'global', hour, minute);
+
+      configuration.navigateTo();
+      configuration.configClear(configname);
     });
   });
 });
