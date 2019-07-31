b3e1c58b0ee70630d9e41269c181166c0b7ce703
David Zafman <dzafman@redhat.com>
2019-07-31 00:15:32 +0000

    osd: Replace active/pending scrub tracking for local/remote
    This is similar to how recovery reservations are split between
    local and remote.
    
    It was the case that scrubs_pending was used for reservations at
    the replicas as well as at the primary while requesting reservations
    from the replicas.  There was no need for scrubs_pending to turn
    into scrubs_active at the primary as nothing treated that value
    as special.  scrubber.active = true when scrubbing is
    actually going.
    
    Now scurbber.local_reserved indicates scrubs_local incremented
    Now scrubber.remote_reserved indicates scrubs_remote incremented
    
    Fixes: https://tracker.ceph.com/issues/41669
    
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/osd/PG.h
+++ src/osd/PG.h
@@ -1091,7 +1091,7 @@ public:
 
     // metadata
     set<pg_shard_t> reserved_peers;
-    bool reserved, reserve_failed;
+    bool local_reserved, remote_reserved, reserve_failed;
     epoch_t epoch_start;
 
     // common to both scrubs
