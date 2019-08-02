42ac34736a7ccc8317982e602c33b30fa5225c21
Patrick Donnelly <pdonnell@redhat.com>
2019-08-02 13:32:15 -0700

    mds: simplify method definition
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- src/mds/SessionMap.h
+++ src/mds/SessionMap.h
@@ -601,8 +601,7 @@ public:
   // sessions
   void decode_legacy(bufferlist::const_iterator& blp) override;
   bool empty() const { return session_map.empty(); }
-  const ceph::unordered_map<entity_name_t, Session*>& get_sessions() const
-  {
+  const auto& get_sessions() const {
     return session_map;
   }
 
