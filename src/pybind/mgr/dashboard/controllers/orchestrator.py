27fdf80e353bb43ec5171f8858e17c59fb244b83
Kiefer Chang <kiefer.chang@suse.com>
2019-08-06 17:55:40 +0800

    mgr/dashboard: Add backend unit tests for orchestrator controllers
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

--- src/pybind/mgr/dashboard/controllers/orchestrator.py
+++ src/pybind/mgr/dashboard/controllers/orchestrator.py
@@ -1,14 +1,27 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+import cherrypy
+
 from . import ApiController, Endpoint, ReadPermission
 from . import RESTController, Task
 from ..security import Scope
 from ..services.orchestrator import OrchClient
+from ..tools import wraps
 
 
 def orchestrator_task(name, metadata, wait_for=2.0):
     return Task("orchestrator/{}".format(name), metadata, wait_for)
+
+
+def raise_if_no_orchestrator(method):
+    @wraps(method)
+    def inner(self, *args, **kwargs):
+        orch = OrchClient.instance()
+        if not orch.available():
+            raise cherrypy.HTTPError(503)
+        return method(self, *args, **kwargs)
+    return inner
 
 
 @ApiController('/orchestrator')
@@ -23,24 +36,18 @@ class Orchestrator(RESTController):
 @ApiController('/orchestrator/inventory', Scope.HOSTS)
 class OrchestratorInventory(RESTController):
 
+    @raise_if_no_orchestrator
     def list(self, hostname=None):
         orch = OrchClient.instance()
-        result = []
-
-        if orch.available():
-            hosts = [hostname] if hostname else None
-            inventory_nodes = orch.inventory.list(hosts)
-            result = [node.to_json() for node in inventory_nodes]
-        return result
+        hosts = [hostname] if hostname else None
+        inventory_nodes = orch.inventory.list(hosts)
+        return [node.to_json() for node in inventory_nodes]
 
 
 @ApiController('/orchestrator/service', Scope.HOSTS)
 class OrchestratorService(RESTController):
-    def list(self, service_type=None, service_id=None, hostname=None):
-        orch = OrchClient.instance()
-        services = []
 
-        if orch.available():
-            services = [service.to_json() for service in orch.services.list(
-                service_type, service_id, hostname)]
-        return services
+    @raise_if_no_orchestrator
+    def list(self, hostname=None):
+        orch = OrchClient.instance()
+        return [service.to_json() for service in orch.services.list(None, None, hostname)]
