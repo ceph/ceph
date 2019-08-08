57887f6a364e06ade69fdf2277122c496ab8a134
Casey Bodley <cbodley@redhat.com>
2019-08-08 10:06:17 -0400

    rgw: http client drops mutex before suspending coroutine
    the lock can't be held over async_wait(), because the call to finish()
    won't be able to reacquire the lock in order to fire the completion.
    coroutines may also resume on a different thread, and you can't unlock a
    mutex that was locked on another thread
    
    Reported-by: Yuval Lifshitz <yuvalif@yahoo.com>
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_http_client.cc
+++ src/rgw/rgw_http_client.cc
@@ -60,12 +60,14 @@ struct rgw_http_req_data : public RefCountedObject {
   auto async_wait(ExecutionContext& ctx, CompletionToken&& token) {
     boost::asio::async_completion<CompletionToken, Signature> init(token);
     auto& handler = init.completion_handler;
-    completion = Completion::create(ctx.get_executor(), std::move(handler));
+    {
+      std::unique_lock l{lock};
+      completion = Completion::create(ctx.get_executor(), std::move(handler));
+    }
     return init.result.get();
   }
 
   int wait(optional_yield y) {
-    std::unique_lock l{lock};
     if (done) {
       return ret;
     }
@@ -82,6 +84,7 @@ struct rgw_http_req_data : public RefCountedObject {
       dout(20) << "WARNING: blocking http request" << dendl;
     }
 #endif
+    std::unique_lock l{lock};
     cond.wait(l);
     return ret;
   }
