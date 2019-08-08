b3ecc18fc80428ecf58067b0806069319b5430e9
Rishabh Dave <ridave@redhat.com>
2019-08-08 21:22:58 +0530

    qa/cephfs: add tests for ACLs
    Add code to run tests for ACLs from xfstests-dev against kernel and
    FUSE CephFS mounts.
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

---
+++ qa/tasks/cephfs/test_acls.py
@@ -0,0 +1,27 @@
+import logging
+from StringIO import StringIO
+
+from xfstests_dev import XFSTestsDev
+
+log = logging.getLogger(__name__)
+
+class TestACLs(XFSTestsDev):
+
+    def test_acls(self):
+        from tasks.cephfs.fuse_mount import FuseMount
+        from tasks.cephfs.kernel_mount import KernelMount
+
+        # TODO: make xfstests-dev compatible with ceph-fuse. xfstests-dev
+        # remounts CephFS before running tests using kernel, so ceph-fuse
+        # mounts are never actually testsed.
+        if isinstance(self.mount_a, FuseMount):
+            log.info('client is fuse mounted')
+            self.skipTest('Requires kernel client; xfstests-dev not '\
+                          'compatible with ceph-fuse ATM.')
+        elif isinstance(self.mount_a, KernelMount):
+            log.info('client is kernel mounted')
+
+        self.mount_a.client_remote.run(args=['sudo', './check',
+            'generic/099'], cwd=self.repo_path, stdout=StringIO(),
+            stderr=StringIO(), timeout=30, check_status=True,
+            label='running tests for ACLs from xfstests-dev')
