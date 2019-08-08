1868cde38e7e55377d5a6829b7cda39ccc77c798
Yingxin Cheng <yingxin.cheng@intel.com>
2019-08-08 17:03:39 +0800

    crimson/net: handle fault for READY, CONNECTING and ACCEPTING
    Signed-off-by: Yingxin Cheng <yingxin.cheng@intel.com>

--- src/crimson/net/ProtocolV2.h
+++ src/crimson/net/ProtocolV2.h
@@ -121,7 +121,7 @@ class ProtocolV2 final : public Protocol {
   seastar::future<> write_frame(F &frame, bool flush=true);
 
  private:
-  seastar::future<> fault();
+  void fault(bool backoff);
   void dispatch_reset();
   void reset_session(bool full);
   seastar::future<entity_type_t, entity_addr_t> banner_exchange();
