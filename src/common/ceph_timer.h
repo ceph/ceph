f63463d1b208f4f2fc533b2475084be251c44e44
Adam C. Emerson <aemerson@redhat.com>
2019-08-13 12:53:02 -0400

    common: Add a threadpool for Asio
    Just to package up running starting, stopping, and running everything.
    
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/common/ceph_timer.h
+++ src/common/ceph_timer.h
@@ -19,14 +19,9 @@
 #include <thread>
 #include <boost/intrusive/set.hpp>
 
+#include "common/detail/construct_suspended.h"
+
 namespace ceph {
-
-  /// Newly constructed timer should be suspended at point of
-  /// construction.
-
-  struct construct_suspended_t { };
-  constexpr construct_suspended_t construct_suspended { };
-
   namespace timer_detail {
     using boost::intrusive::member_hook;
     using boost::intrusive::set_member_hook;
