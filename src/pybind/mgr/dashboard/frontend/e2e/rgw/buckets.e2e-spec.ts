4511bf17c9de90acb86f5fab8de1d48d49b9264a
Patrick Seidensal <pseidensal@suse.com>
2019-08-16 14:39:54 +0200

    mgr/dashboard: migrate E2E buckets to async/await
    Fixes: https://tracker.ceph.com/issues/40693
    
    Signed-off-by: Patrick Seidensal <pseidensal@suse.com>

--- src/pybind/mgr/dashboard/frontend/e2e/rgw/buckets.e2e-spec.ts
+++ src/pybind/mgr/dashboard/frontend/e2e/rgw/buckets.e2e-spec.ts
@@ -1,57 +1,63 @@
+import { $ } from 'protractor';
 import { Helper } from '../helper.po';
 
 describe('RGW buckets page', () => {
   let buckets: Helper['buckets'];
 
-  beforeAll(() => {
-    buckets = new Helper().buckets;
-    buckets.navigateTo();
+  beforeAll(async () => {
+    const helper = new Helper();
+    buckets = helper.buckets;
   });
 
-  afterEach(() => {
-    Helper.checkConsole();
+  afterEach(async () => {
+    await Helper.checkConsole();
   });
 
-  describe('breadcrumb test', () => {
-    it('should open and show breadcrumb', () => {
-      expect(buckets.getBreadcrumbText()).toEqual('Buckets');
-    });
+  it('should open and show breadcrumb', async () => {
+    await buckets.navigateTo();
+    expect(await $('.breadcrumb-item.active').getText()).toBe('Buckets');
   });
 
-  describe('create, edit & delete bucket test', () => {
-    it('should create bucket', () => {
-      buckets.create(
-        '000test',
-        '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef',
-        'default-placement'
-      );
-      expect(buckets.getTableCell('000test').isPresent()).toBe(true);
-    });
+  it('should create bucket', async () => {
+    await buckets.navigateTo('create');
+    await buckets.create(
+      '000test',
+      '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef',
+      'default-placement'
+    );
+    expect(await buckets.getTableCell('000test').isPresent()).toBe(true);
+  });
 
-    it('should edit bucket', () => {
-      buckets.edit('000test', 'dev');
-      expect(buckets.getTable().getText()).toMatch('dev');
-    });
+  it('should edit bucket', async () => {
+    await buckets.navigateTo();
+    await buckets.edit('000test', 'dev');
+    expect(await buckets.getTable().getText()).toMatch('dev');
+  });
 
-    it('should delete bucket', () => {
-      buckets.delete('000test');
-      expect(buckets.getTableCell('000test').isPresent()).toBe(false);
-    });
+  it('should delete bucket', async () => {
+    await buckets.navigateTo();
+    await buckets.delete('000test');
+    expect(buckets.getTableCell('000test').isPresent()).toBe(false);
   });
 
   describe('Invalid Input in Create and Edit tests', () => {
-    it('should test invalid inputs in create fields', () => {
-      buckets.invalidCreate();
+    it('should test invalid inputs in create fields', async () => {
+      await buckets.navigateTo('create');
+      await buckets.testInvalidCreate();
     });
 
-    it('should test invalid input in edit owner field', () => {
-      buckets.create(
+    it('should test invalid input in edit owner field', async () => {
+      await buckets.navigateTo('create');
+      await buckets.create(
         '000rq',
         '0123456789abcdef0123456789abcdef0123456789abcdef0123456789abcdef',
         'default-placement'
       );
-      buckets.invalidEdit('000rq');
-      buckets.delete('000rq');
+
+      await buckets.testInvalidEdit('000rq');
+
+      await buckets.navigateTo();
+      await buckets.delete('000rq');
     });
   });
 });
