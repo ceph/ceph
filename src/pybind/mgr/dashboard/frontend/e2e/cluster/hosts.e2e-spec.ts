bdaf0a89f28d7a8fd2bc4527e2f75fa6e420d62a
Patrick Seidensal <pseidensal@suse.com>
2019-08-19 13:07:05 +0200

    mgr/dashboard: migrate E2E hosts to async/await
    Fixes: https://tracker.ceph.com/issues/40693
    
    Signed-off-by: Patrick Seidensal <pseidensal@suse.com>

--- src/pybind/mgr/dashboard/frontend/e2e/cluster/hosts.e2e-spec.ts
+++ src/pybind/mgr/dashboard/frontend/e2e/cluster/hosts.e2e-spec.ts
@@ -7,39 +7,39 @@ describe('Hosts page', () => {
     hosts = new Helper().hosts;
   });
 
-  afterEach(() => {
-    Helper.checkConsole();
+  afterEach(async () => {
+    await Helper.checkConsole();
   });
 
   describe('breadcrumb and tab tests', () => {
-    beforeAll(() => {
-      hosts.navigateTo();
+    beforeAll(async () => {
+      await hosts.navigateTo();
     });
 
-    it('should open and show breadcrumb', () => {
-      expect(hosts.getBreadcrumbText()).toEqual('Hosts');
+    it('should open and show breadcrumb', async () => {
+      expect(await hosts.getBreadcrumbText()).toEqual('Hosts');
     });
 
-    it('should show two tabs', () => {
-      expect(hosts.getTabsCount()).toEqual(2);
+    it('should show two tabs', async () => {
+      expect(await hosts.getTabsCount()).toEqual(2);
     });
 
-    it('should show hosts list tab at first', () => {
-      expect(hosts.getTabText(0)).toEqual('Hosts List');
+    it('should show hosts list tab at first', async () => {
+      expect(await hosts.getTabText(0)).toEqual('Hosts List');
     });
 
-    it('should show overall performance as a second tab', () => {
-      expect(hosts.getTabText(1)).toEqual('Overall Performance');
+    it('should show overall performance as a second tab', async () => {
+      expect(await hosts.getTabText(1)).toEqual('Overall Performance');
     });
   });
 
   describe('services link test', () => {
-    it('should check at least one host is present', () => {
-      hosts.check_for_host();
+    it('should check at least one host is present', async () => {
+      await hosts.check_for_host();
     });
 
-    it('should check services link(s) work for first host', () => {
-      hosts.check_services_links();
+    it('should check services link(s) work for first host', async () => {
+      await hosts.check_services_links();
     });
   });
 });
