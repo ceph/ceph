d53cfb48c74f36cdd12cd8344264f5750ecd0f26
Patrick Seidensal <pseidensal@suse.com>
2019-08-19 14:07:27 +0200

    mgr/dashboard: migrate E2E images to async/await
    Fixes: https://tracker.ceph.com/issues/40693
    
    Signed-off-by: Patrick Seidensal <pseidensal@suse.com>

--- src/pybind/mgr/dashboard/frontend/e2e/block/images.e2e-spec.ts
+++ src/pybind/mgr/dashboard/frontend/e2e/block/images.e2e-spec.ts
@@ -9,65 +9,63 @@ describe('Images page', () => {
     pools = new Helper().pools;
   });
 
-  afterEach(() => {
-    Helper.checkConsole();
+  afterEach(async () => {
+    await Helper.checkConsole();
   });
 
   describe('breadcrumb and tab tests', () => {
-    beforeAll(() => {
-      images.navigateTo();
+    beforeAll(async () => {
+      await images.navigateTo();
     });
 
-    it('should open and show breadcrumb', () => {
-      expect(images.getBreadcrumbText()).toEqual('Images');
+    it('should open and show breadcrumb', async () => {
+      expect(await images.getBreadcrumbText()).toEqual('Images');
     });
 
-    it('should show three tabs', () => {
-      expect(images.getTabsCount()).toEqual(3);
+    it('should show three tabs', async () => {
+      expect(await images.getTabsCount()).toEqual(3);
     });
 
-    it('should show text for all tabs', () => {
-      expect(images.getTabText(0)).toEqual('Images');
-      expect(images.getTabText(1)).toEqual('Trash');
-      expect(images.getTabText(2)).toEqual('Overall Performance');
+    it('should show text for all tabs', async () => {
+      expect(await images.getTabText(0)).toEqual('Images');
+      expect(await images.getTabText(1)).toEqual('Trash');
+      expect(await images.getTabText(2)).toEqual('Overall Performance');
     });
   });
 
-  describe('create, edit & delete image test', () => {
+  describe('create, edit & delete image test', async () => {
     const poolName = 'e2e_images_pool';
     const imageName = 'e2e_images_image';
     const newImageName = 'e2e_images_image_new';
 
-    beforeAll(() => {
-      pools.navigateTo('create'); // Need pool for image testing
-      pools.create(poolName, 8, 'rbd').then(() => {
-        pools.navigateTo();
-        pools.exist(poolName, true);
-      });
-      images.navigateTo();
+    beforeAll(async () => {
+      await pools.navigateTo('create'); // Need pool for image testing
+      await pools.create(poolName, 8, 'rbd');
+      await pools.navigateTo();
+      await pools.exist(poolName, true);
+      await images.navigateTo();
     });
 
-    it('should create image', () => {
-      images.createImage(imageName, poolName, '1');
-      expect(images.getTableCell(imageName).isPresent()).toBe(true);
+    it('should create image', async () => {
+      await images.createImage(imageName, poolName, '1');
+      expect(await images.getTableCell(imageName).isPresent()).toBe(true);
     });
 
-    it('should edit image', () => {
-      images.editImage(imageName, poolName, newImageName, '2');
-      expect(images.getTableCell(newImageName).isPresent()).toBe(true);
+    it('should edit image', async () => {
+      await images.editImage(imageName, poolName, newImageName, '2');
+      expect(await images.getTableCell(newImageName).isPresent()).toBe(true);
     });
 
-    it('should delete image', () => {
-      images.deleteImage(newImageName);
-      expect(images.getTableCell(newImageName).isPresent()).toBe(false);
+    it('should delete image', async () => {
+      await images.deleteImage(newImageName);
+      expect(await images.getTableCell(newImageName).isPresent()).toBe(false);
     });
 
-    afterAll(() => {
-      pools.navigateTo(); // Deletes images test pool
-      pools.delete(poolName).then(() => {
-        pools.navigateTo();
-        pools.exist(poolName, false);
-      });
+    afterAll(async () => {
+      await pools.navigateTo(); // Deletes images test pool
+      await pools.delete(poolName);
+      await pools.navigateTo();
+      await pools.exist(poolName, false);
     });
   });
 });
