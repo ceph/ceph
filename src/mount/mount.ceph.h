867c2dfea9f7f308aa70beb08b712ee06ba0c503
Jeff Layton <jlayton@redhat.com>
2019-08-20 14:25:55 -0400

    mount.ceph: add name and secret to ceph_mount_info
    ...and move the length defines into mount.ceph.h. Note that we use a
    fixed-length buffer for the secret, as it cuts down a copy when we
    read_secret_from_file.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    (cherry picked from commit f560fbc1e19caec9d4522fdc4ae67955cbf46390)

--- src/mount/mount.ceph.h
+++ src/mount/mount.ceph.h
@@ -5,6 +5,25 @@
 extern "C" {
 #endif
 
+/*
+ * See class CryptoKey
+ *
+ * 2 (for the type of secret) +
+ * 8 (for the timestamp) +
+ * 2 (for the length of secret) +
+ * 16 (for an AES-128 key)
+ */
+#define MAX_RAW_SECRET_LEN (2 + 8 + 2 + 16)
+
+/* Max length of base64 encoded secret. 4/3 original size (rounded up) */
+#define MAX_SECRET_LEN ((MAX_RAW_SECRET_LEN + (3 - 1)) * 4 / 3)
+
+/* Max Including null terminator */
+#define SECRET_BUFSIZE (MAX_SECRET_LEN + 1)
+
+/* Buffer size for secret= option */
+#define SECRET_OPTION_BUFSIZE (sizeof("secret=") + MAX_SECRET_LEN + 1)
+
 void mount_ceph_debug(const char *fmt, ...);
 
 #ifdef __cplusplus
