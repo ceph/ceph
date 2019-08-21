8ec0fa5dc95ccca7ba6467dda688939ba7761b19
Joao Eduardo Luis <joao@suse.com>
2019-08-21 15:39:49 +0000

    mon: create monmon (ro) command wrappers
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

--- src/mon/commands/monmapmon_cmds.h
+++ src/mon/commands/monmapmon_cmds.h
@@ -76,4 +76,222 @@ struct MonMonWriteCommand :
 typedef std::shared_ptr<MonMonReadCommand> MonMonReadCommandRef;
 typedef std::shared_ptr<MonMonWriteCommand> MonMonWriteCommandRef;
 
+// preprocess commands (RO)
+
+/* MonMonStatCommand - handle 'mon stat' */
+struct MonMonStatCommand : public MonMonReadCommand
+{
+  explicit MonMonStatCommand(
+      Monitor *_mon,
+      MonmapMonitor *_monmon,
+      CephContext *_cct) :
+    MonMonReadCommand(_mon, _monmon, _cct)
+  { }
+
+  bool handles_command(const string &prefix) {
+    return (prefix == "mon stat");
+  }
+
+  bool do_preprocess(
+      MonOpRequestRef op,
+      const string &prefix,
+      const cmdmap_t &cmdmap,
+      stringstream &ss,
+      bufferlist rdata,
+      FormatterRef f,
+      const MonMap &monmap) {
+
+    ceph_assert(handles_command(prefix));
+
+    mon->monmap->print_summary(ss);
+    ss << ", election epoch " << mon->get_epoch() << ", leader "
+      << mon->get_leader() << " " << mon->get_leader_name()
+      << ", quorum " << mon->get_quorum() << " " << mon->get_quorum_names();
+    rdata.append(ss);
+    ss.str("");
+    reply_with_data(op, 0, ss, rdata, service->get_last_committed());
+    return true;
+  }
+};
+
+struct MonMonGetMap : public MonMonReadCommand
+{
+  explicit MonMonGetMap(
+      Monitor *_mon,
+      MonmapMonitor *_monmon,
+      CephContext *_cct) :
+    MonMonReadCommand(_mon, _monmon, _cct)
+  { }
+
+  bool handles_command(const string &prefix) {
+    return (prefix == "mon getmap" || prefix == "mon dump");
+  }
+
+  virtual bool do_preprocess(
+      MonOpRequestRef op,
+      const string &prefix,
+      const cmdmap_t &cmdmap,
+      stringstream &ss,
+      bufferlist rdata,
+      FormatterRef f,
+      const MonMap &monmap) final {
+
+    ceph_assert(handles_command(prefix));
+
+    MMonCommand *m = static_cast<MMonCommand*>(op->get_req());
+
+    epoch_t epoch;
+    int64_t epochnum;
+    cmd_getval(cct, cmdmap, "epoch", epochnum, (int64_t)0);
+    epoch = epochnum;
+
+    MonMap *p = mon->monmap;
+    if (epoch) {
+      bufferlist bl;
+      int r = service->get_version(epoch, bl);
+      if (r == -ENOENT) {
+        ss << "there is no map for epoch " << epoch;
+	reply(op, -ENOENT, ss, service->get_last_committed());
+      }
+      ceph_assert(r == 0);
+      ceph_assert(bl.length() > 0);
+      p = new MonMap;
+      p->decode(bl);
+    }
+
+    ceph_assert(p);
+
+    if (prefix == "mon getmap") {
+      p->encode(rdata, m->get_connection()->get_features());
+      ss << "got monmap epoch " << p->get_epoch();
+    } else if (prefix == "mon dump") {
+      stringstream ds;
+      if (f) {
+        f->open_object_section("monmap");
+        p->dump(f.get());
+        f->open_array_section("quorum");
+        for (set<int>::iterator q = mon->get_quorum().begin();
+            q != mon->get_quorum().end(); ++q) {
+          f->dump_int("mon", *q);
+        }
+        f->close_section();
+        f->close_section();
+        f->flush(ds);
+      } else {
+        p->print(ds);
+      }
+      rdata.append(ds);
+      ss << "dumped monmap epoch " << p->get_epoch();
+    }
+    if (p != mon->monmap) {
+       delete p;
+       p = nullptr;
+    }
+
+    reply_with_data(op, 0, ss, rdata, service->get_last_committed());
+    return true;
+  }
+};
+
+
+struct MonMonFeatureLs : public MonMonReadCommand
+{
+  explicit MonMonFeatureLs(
+      Monitor *_mon,
+      MonmapMonitor *_monmon,
+      CephContext *_cct) :
+    MonMonReadCommand(_mon, _monmon, _cct)
+  { }
+
+  bool handles_command(const string &prefix) {
+    return (prefix == "mon feature ls");
+  }
+
+  bool do_preprocess(
+      MonOpRequestRef op,
+      const string &prefix,
+      const cmdmap_t &cmdmap,
+      stringstream &ss,
+      bufferlist rdata,
+      FormatterRef f,
+      const MonMap &monmap) {
+
+    ceph_assert(handles_command(prefix));
+
+    bool list_with_value = false;
+    string with_value;
+    if (cmd_getval(g_ceph_context, cmdmap, "with_value", with_value) &&
+        with_value == "--with-value") {
+      list_with_value = true;
+    }
+
+    // list features
+    mon_feature_t supported = ceph::features::mon::get_supported();
+    mon_feature_t persistent = ceph::features::mon::get_persistent();
+    mon_feature_t required = monmap.get_required_features();
+
+    stringstream ds;
+    auto print_feature = [&](mon_feature_t& m_features, const char* m_str) {
+      if (f) {
+        if (list_with_value)
+          m_features.dump_with_value(f.get(), m_str);
+        else
+          m_features.dump(f.get(), m_str);
+      } else {
+        if (list_with_value)
+          m_features.print_with_value(ds);
+        else
+          m_features.print(ds);
+      }
+    };
+
+    mon_feature_t mon_persistent = monmap.persistent_features;
+    mon_feature_t mon_optional = monmap.optional_features;
+
+    if (f) {
+      f->open_object_section("features");
+
+      f->open_object_section("all");
+      print_feature(supported, "supported");
+      print_feature(persistent, "persistent");
+      f->close_section(); // all
+
+      f->open_object_section("monmap");
+      print_feature(mon_persistent, "persistent");
+      print_feature(mon_optional, "optional");
+      print_feature(required, "required");
+      f->close_section(); // monmap 
+
+      f->close_section(); // features
+      f->flush(ds);
+
+    } else {
+      ds << "all features" << std::endl
+        << "\tsupported: ";
+      print_feature(supported, nullptr);
+      ds << std::endl
+        << "\tpersistent: ";
+      print_feature(persistent, nullptr);
+      ds << std::endl
+        << std::endl;
+
+      ds << "on current monmap (epoch "
+         << monmap.get_epoch() << ")" << std::endl
+         << "\tpersistent: ";
+      print_feature(mon_persistent, nullptr);
+      ds << std::endl
+        // omit optional features in plain-text
+        // makes it easier to read, and they're, currently, empty.
+	 << "\trequired: ";
+      print_feature(required, nullptr);
+      ds << std::endl;
+    }
+    rdata.append(ds);
+
+    reply_with_data(op, 0, ss, rdata, service->get_last_committed());
+    return true;
+  }
+
+};
+
 #endif // CEPH_MONMAPMONITOR_CMDS_H
