a8b1fbf59894ea46b643e236ce9ca7190d9705f8
Mark Nelson <mnelson@redhat.com>
2019-08-26 07:16:31 -0400

    rgw/rgw_reshard: Don't dump RGWBucketReshard JSON in process_single_logshard
    Signed-off-by: Mark Nelson <mnelson@redhat.com>

--- src/rgw/rgw_reshard.cc
+++ src/rgw/rgw_reshard.cc
@@ -993,11 +993,8 @@ int RGWReshard::process_single_logshard(int logshard_num)
 	}
 
 	RGWBucketReshard br(store, bucket_info, attrs, nullptr);
-
-	Formatter* formatter = new JSONFormatter(false);
-	auto formatter_ptr = std::unique_ptr<Formatter>(formatter);
-	ret = br.execute(entry.new_num_shards, max_entries, true, nullptr,
-			 formatter, this);
+	ret = br.execute(entry.new_num_shards, max_entries, false, nullptr,
+			 nullptr, this);
 	if (ret < 0) {
 	  ldout (store->ctx(), 0) <<  __func__ <<
 	    "ERROR in reshard_bucket " << entry.bucket_name << ":" <<
