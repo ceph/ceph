c2e0540bbb1d594504fd118a0051a0a275f66858
Tiago Melo <tmelo@suse.com>
2019-08-29 16:52:29 +0000

    mgr/dashboard: Split user-mgmt e2e files
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/frontend/e2e/ui/user-mgmt.e2e-spec.ts
+++ src/pybind/mgr/dashboard/frontend/e2e/ui/user-mgmt.e2e-spec.ts
@@ -2,12 +2,11 @@ import { Helper } from '../helper.po';
 import { UserMgmtPageHelper } from './user-mgmt.po';
 
 describe('User Management page', () => {
-  let userManagement: UserMgmtPageHelper;
+  let userMgmt: UserMgmtPageHelper;
   const user_name = 'user_mgmt_create_edit_delete_user';
-  const role_name = 'user_mgmt_create_edit_delete_role';
 
   beforeAll(() => {
-    userManagement = new UserMgmtPageHelper();
+    userMgmt = new UserMgmtPageHelper();
   });
 
   afterEach(async () => {
@@ -16,56 +15,27 @@ describe('User Management page', () => {
 
   describe('breadcrumb tests', () => {
     it('should check breadcrumb on users tab of user management page', async () => {
-      await userManagement.navigateTo('users');
-      await userManagement.waitTextToBePresent(userManagement.getBreadcrumb(), 'Users');
-    });
-
-    it('should check breadcrumb on roles tab of user management page', async () => {
-      await userManagement.navigateTo('roles');
-      await userManagement.waitTextToBePresent(userManagement.getBreadcrumb(), 'Roles');
+      await userMgmt.navigateTo();
+      await userMgmt.waitTextToBePresent(userMgmt.getBreadcrumb(), 'Users');
     });
 
     it('should check breadcrumb on user creation page', async () => {
-      await userManagement.navigateTo('userCreate');
-      await userManagement.waitTextToBePresent(userManagement.getBreadcrumb(), 'Create');
-    });
-
-    it('should check breadcrumb on role creation page', async () => {
-      await userManagement.navigateTo('roleCreate');
-      await userManagement.waitTextToBePresent(userManagement.getBreadcrumb(), 'Create');
+      await userMgmt.navigateTo('create');
+      await userMgmt.waitTextToBePresent(userMgmt.getBreadcrumb(), 'Create');
     });
   });
 
   describe('user create, edit & delete test', () => {
     it('should create a user', async () => {
-      await userManagement.userCreate(
-        user_name,
-        'cool_password',
-        'Jeff',
-        'realemail@realwebsite.com'
-      );
+      await userMgmt.create(user_name, 'cool_password', 'Jeff', 'realemail@realwebsite.com');
     });
 
     it('should edit a user', async () => {
-      await userManagement.userEdit(user_name, 'cool_password_number_2', 'Geoff', 'w@m');
+      await userMgmt.edit(user_name, 'cool_password_number_2', 'Geoff', 'w@m');
     });
 
     it('should delete a user', async () => {
-      await userManagement.userDelete(user_name);
-    });
-  });
-
-  describe('role create, edit & delete test', () => {
-    it('should create a role', async () => {
-      await userManagement.roleCreate(role_name, 'An interesting description');
-    });
-
-    it('should edit a role', async () => {
-      await userManagement.roleEdit(role_name, 'A far more interesting description');
-    });
-
-    it('should delete a role', async () => {
-      await userManagement.roleDelete(role_name);
+      await userMgmt.userDelete(user_name);
     });
   });
 });
--- src/pybind/mgr/dashboard/frontend/e2e/ui/user-mgmt.po.ts
+++ src/pybind/mgr/dashboard/frontend/e2e/ui/user-mgmt.po.ts
@@ -3,15 +3,12 @@ import { PageHelper } from '../page-helper.po';
 
 export class UserMgmtPageHelper extends PageHelper {
   pages = {
-    index: '/#/user-management',
-    users: '/#/user-management/users',
-    userCreate: '/#/user-management/users/create',
-    roles: '/#/user-management/roles',
-    roleCreate: '/#/user-management/roles/create'
+    index: '/#/user-management/users',
+    create: '/#/user-management/users/create'
   };
 
-  async userCreate(username, password, name, email): Promise<void> {
-    await this.navigateTo('userCreate');
+  async create(username, password, name, email): Promise<void> {
+    await this.navigateTo('create');
 
     // fill in fields
     await element(by.id('username')).sendKeys(username);
@@ -26,8 +23,8 @@ export class UserMgmtPageHelper extends PageHelper {
     await this.waitPresence(this.getTableCell(username));
   }
 
-  async userEdit(username, password, name, email): Promise<void> {
-    await this.navigateTo('users');
+  async edit(username, password, name, email): Promise<void> {
+    await this.navigateTo();
 
     await this.getTableCell(username).click(); // select user from table
     await element(by.cssContainingText('button', 'Edit')).click(); // click button to move to edit page
@@ -50,7 +47,7 @@ export class UserMgmtPageHelper extends PageHelper {
   }
 
   async userDelete(username): Promise<void> {
-    await this.navigateTo('users');
+    await this.navigateTo();
 
     await this.getTableCell(username).click(); // select user from table
     await $('.table-actions button.dropdown-toggle').click(); // click toggle menu
@@ -60,49 +57,5 @@ export class UserMgmtPageHelper extends PageHelper {
     await $('.custom-control-label').click(); // click confirmation checkbox
     await element(by.cssContainingText('button', 'Delete User')).click();
     await this.waitStaleness(this.getFirstTableCellWithText(username));
-  }
-
-  async roleCreate(name, description): Promise<void> {
-    await this.navigateTo('roleCreate');
-
-    // fill in fields
-    await element(by.id('name')).sendKeys(name);
-    await element(by.id('description')).sendKeys(description);
-
-    // Click the create button and wait for user to be made
-    const createButton = element(by.cssContainingText('button', 'Create Role'));
-    await createButton.click();
-    await this.waitPresence(this.getTableCell(name));
-  }
-
-  async roleEdit(name, description): Promise<void> {
-    await this.navigateTo('roles');
-
-    await this.getTableCell(name).click(); // select role from table
-    await element(by.cssContainingText('button', 'Edit')).click(); // click button to move to edit page
-
-    // fill in fields with new values
-    await element(by.id('description')).clear();
-    await element(by.id('description')).sendKeys(description);
-
-    // Click the edit button and check new values are present in table
-    const editButton = element(by.cssContainingText('button', 'Edit Role'));
-    await editButton.click();
-
-    await this.waitPresence(this.getTableCell(name));
-    await this.waitPresence(this.getTableCell(description));
-  }
-
-  async roleDelete(name) {
-    await this.navigateTo('roles');
-
-    await this.getTableCell(name).click(); // select role from table
-    await $('.table-actions button.dropdown-toggle').click(); // click toggle menu
-    await $('li.delete a').click(); // click delete
-
-    await this.waitVisibility($('.custom-control-label'));
-    await $('.custom-control-label').click(); // click confirmation checkbox
-    await element(by.cssContainingText('button', 'Delete Role')).click();
-    await this.waitStaleness(this.getFirstTableCellWithText(name));
   }
 }
