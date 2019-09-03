2021399c3203482f2080701b882df0be30732ff1
chenxuqiang <chenxuqiang3@hisilicon.com>
2019-09-03 11:55:10 +0800

    test/bench_log: Modify some errors
       if(argc < 3) {
    	  ...;
    43	  return EXIT_FAILURE;  //return 0;
       }
    
    Signed-off-by: Xuqiang Chen <chenxuqiang3@hisilicon.com>

--- src/test/bench_log.cc
+++ src/test/bench_log.cc
@@ -32,15 +32,15 @@ struct T : public Thread {
 
 void usage(const char* program_name) {
   cout << program_name << " <threads> <lines>" << std::endl;
-  cout << "\t threads: the number of thtreads for this test." << std::endl;
-  cout << "\t lines: the number of the lines per thread." << std::endl;
+  cout << "\t threads: the number of threads for this test." << std::endl;
+  cout << "\t lines: the number of log entries per thread." << std::endl;
 }
 
 int main(int argc, const char **argv)
 {
   if (argc < 3) {
     usage(argv[0]);
-    return 0;
+    return EXIT_FAILURE;
   }
 
   int threads = atoi(argv[1]);
