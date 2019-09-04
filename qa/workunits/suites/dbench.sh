eeeebafdeec71a8d09ac1e00819b6023c90f791c
zhangbingyi <zhangbingyi@umcloud.com>
2019-09-04 22:13:53 +0800

    qa/workunits/suites/dbench.sh : before test we should download the tool first Fixes: https://tracker.ceph.com/issues/41651 Signed-off-by: Bingyi Zhang <zhangbingyi@umcloud.com>

--- qa/workunits/suites/dbench.sh
+++ qa/workunits/suites/dbench.sh
@@ -2,5 +2,15 @@
 
 set -e
 
+echo "getting dbench"
+git clone git://git.samba.org/sahlberg/dbench.git dbench
+cd dbench
+./autogen.sh
+./configure
+make
+make install
+cp loadfiles/client.txt /usr/local/share/
+
+echo "running dbench"
 dbench 1
 dbench 10
