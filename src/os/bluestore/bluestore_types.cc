yehudasa/ceph-pr-archive

os/bluestore: implement tiny object keeping at db only.
Signed-off-by: Igor Fedotov <ifedotov@suse.com>

src/os/bluestore/bluestore_types.cc
@@ -608,6 +608,11 @@ string bluestore_blob_t::get_flags_string(unsigned flags)
       s += '+';
     s += "shared";
   }
+  if (flags & FLAG_DB_OFFLOADED) {
+    if (s.length())
+      s += '+';
+    s += "@db";
+  }
 
   return s;
 }
@@ -875,7 +880,7 @@ bool bluestore_blob_t::release_extents(bool all,
       }
       pos += e.length;
     }
-    ceph_assert(is_compressed() || get_logical_length() == pos);
+    ceph_assert(is_compressed() || is_db_offloaded() || get_logical_length() == pos);
     extents.resize(1);
     extents[0].offset = bluestore_pextent_t::INVALID_OFFSET;
     extents[0].length = pos;

ceph/ceph

src/os/bluestore/bluestore_types.cc
@@ -640,6 +640,11 @@ string bluestore_blob_t::get_flags_string(unsigned flags)
       s += '+';
     s += "shared";
   }
+  if (flags & FLAG_KV) {
+    if (s.length())
+      s += '+';
+    s += "@kv";
+  }
 
   return s;
 }
@@ -907,7 +912,7 @@ bool bluestore_blob_t::release_extents(bool all,
       }
       pos += e.length;
     }
-    ceph_assert(is_compressed() || get_logical_length() == pos);
+    ceph_assert(is_compressed() || is_at_kv() || get_logical_length() == pos);
     extents.resize(1);
     extents[0].offset = bluestore_pextent_t::INVALID_OFFSET;
     extents[0].length = pos;