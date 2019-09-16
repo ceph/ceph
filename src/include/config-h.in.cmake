1264ef6556df0ed9f5dc822efae0ccf414fcc9ee
Ulrich Weigand <ulrich.weigand@de.ibm.com>
2019-09-16 17:23:40 +0200

    headers: Remove __le16/__le32/__le64
    These types should no longer be used; use ceph_le16/32/64 instead.
    
    Also removes the cmake check whether those types are provided by
    kernel headers (likewise for the __be types, which already were
    not used anywhere).
    
    Signed-off-by: Ulrich Weigand <ulrich.weigand@de.ibm.com>

--- src/include/config-h.in.cmake
+++ src/include/config-h.in.cmake
@@ -39,24 +39,6 @@
 /* Define to 1 if you have the <execinfo.h> header file. */
 #cmakedefine HAVE_EXECINFO_H 1
 
-/* Define to 1 if the system has the type `__be16'. */
-#cmakedefine HAVE___BE16 1
-
-/* Define to 1 if the system has the type `__be32'. */
-#cmakedefine HAVE___BE32 1
-
-/* Define to 1 if the system has the type `__be64'. */
-#cmakedefine HAVE___BE64 1
-
-/* Define to 1 if the system has the type `__le16'. */
-#cmakedefine HAVE___LE16 1
-
-/* Define to 1 if the system has the type `__le32'. */
-#cmakedefine HAVE___LE32 1
-
-/* Define to 1 if the system has the type `__le64'. */
-#cmakedefine HAVE___LE64 1
-
 /* Define to 1 if the system has the type `__s16'. */
 #cmakedefine HAVE___S16 1
 
