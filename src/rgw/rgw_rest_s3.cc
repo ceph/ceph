41ebfb259d37988ff40ee2b5f7a0008716ec1f39
Chang Liu <liuchang0812@gmail.com>
2019-09-19 14:56:02 +0800

    rgw: the http response code of delete bucket should not be 204-no-content
    Fixes: https://tracker.ceph.com/issues/41925
    
    Signed-off-by: Chang Liu <liuchang0812@gmail.com>
    (cherry picked from commit 710d8d31d27af05de1f17f7a44f9f0b5862b00a2)

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -1566,15 +1566,6 @@ void RGWDeleteBucket_ObjStore_S3::send_response()
   set_req_state_err(s, r);
   dump_errno(s);
   end_header(s, this);
-
-  if (s->system_request) {
-    JSONFormatter f; /* use json formatter for system requests output */
-
-    f.open_object_section("info");
-    encode_json("object_ver", objv_tracker.read_version, &f);
-    f.close_section();
-    rgw_flush_formatter_and_reset(s, &f);
-  }
 }
 
 static inline void map_qs_metadata(struct req_state* s)
