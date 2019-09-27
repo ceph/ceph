035303f704765e21ca90bf5d74b2d113c0761dc1
Sage Weil <sage@redhat.com>
2019-09-27 11:41:32 -0500

    ceph-daemon: add mons with 'deploy mon.x ...'
    Signed-off-by: Sage Weil <sage@redhat.com>

--- test_ceph_daemon.sh
+++ test_ceph_daemon.sh
@@ -5,10 +5,30 @@ fsid=0a464092-dfd0-11e9-b903-002590e526e8
 ../src/ceph-daemon rm-cluster --fsid $fsid --force
 
 ../src/ceph-daemon bootstrap \
+		   --mon-id a \
+		   --mgr-id x \
 		   --fsid $fsid \
 		   --mon-ip 10.3.64.23 \
 		   --output-keyring k \
 		   --output-conf c
 chmod 644 k c
+
+# mon.b
+../src/ceph-daemon deploy --name mon.b \
+		   --fsid $fsid \
+		   --mon-ip 10.3.64.27 \
+		   --keyring /var/lib/ceph/$fsid/mon.a/keyring \
+		   --conf c
+
+# mgr.b
+bin/ceph -c c -k k auth get-or-create mgr.y \
+	 mon 'allow profile mgr' \
+	 osd 'allow *' \
+	 mds 'allow *' > k-mgr.y
+../src/ceph-daemon deploy --name mgr.y \
+		   --fsid $fsid \
+		   --keyring k-mgr.y \
+		   --conf c
+
 
 bin/ceph -c c -k k -s
