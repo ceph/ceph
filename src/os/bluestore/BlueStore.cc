ceph/ceph

commit f1346a0ce1e9c35d44d016380c6a962d23288de4

os/bluestore: remove duplicate logging
Signed-off-by: Igor Fedotov <ifedotov@suse.com>

src/os/bluestore/BlueStore.cc
@@ -7685,9 +7685,6 @@ void BlueStore::_fsck_check_objects(FSCKDepth depth,
         pool_id = c->cid.is_pg(&pgid) ? pgid.pool() : META_POOL_ID;
         dout(20) << __func__ << "  collection " << c->cid << " " << c->cnode
           << dendl;
-
-        dout(20) << __func__ << "  collection " << c->cid << " " << c->cnode
-          << dendl;
       }
 
       if (depth != FSCK_SHALLOW &&

commit 14c1461cbfab7717d15b4ba7a95834b19d7ae6fa

os/bluestore: fix invalid stray shared blob detection in fsck.
This is a regression, caused by https://github.com/ceph/ceph/pull/30264

Signed-off-by: Igor Fedotov <ifedotov@suse.com>

src/os/bluestore/BlueStore.cc
@@ -7619,7 +7619,7 @@ void BlueStore::_fsck_check_objects(FSCKDepth depth,
   auto used_per_pool_omap_head = ctx.used_per_pool_omap_head;
   auto used_pgmeta_omap_head = ctx.used_pgmeta_omap_head;
   auto sb_info_lock = ctx.sb_info_lock;
-  auto sb_info = ctx.sb_info;
+  auto& sb_info = ctx.sb_info;
   auto repairer = ctx.repairer;
 
   uint64_t_btree_t used_nids;