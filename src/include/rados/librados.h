0587ef90eb96fb388574f2d3d37fd9bfa1abc235
Kefu Chai <kchai@redhat.com>
2019-09-27 15:32:43 +0800

    librados: deprecate rados_{set,unset}_osdmap_full_try()
    will add rados_{set,unset}_pool_full_try() in a follow-up change, which
    reflect the change of d61d20d0dc4d9f76f4e802e945a4b32a1207c6b6
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/include/rados/librados.h
+++ src/include/rados/librados.h
@@ -3647,9 +3647,11 @@ CEPH_RADOS_API int rados_blacklist_add(rados_t cluster,
 				       char *client_address,
 				       uint32_t expire_seconds);
 
-CEPH_RADOS_API void rados_set_osdmap_full_try(rados_ioctx_t io);
+CEPH_RADOS_API void rados_set_osdmap_full_try(rados_ioctx_t io)
+  __attribute__((deprecated));
 
-CEPH_RADOS_API void rados_unset_osdmap_full_try(rados_ioctx_t io);
+CEPH_RADOS_API void rados_unset_osdmap_full_try(rados_ioctx_t io)
+  __attribute__((deprecated));
 
 /**
  * Enable an application on a pool
