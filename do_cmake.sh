3d15f1c1f84a4816aeddb52ae2ca324821bd3539
Kefu Chai <kchai@redhat.com>
2019-09-28 22:15:15 +0800

    do_cmake.sh: set WITH_RADOSGW_AMQP_ENDPOINT using ARGS
    so we can extend ARGS in a more unified way
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>
    (cherry picked from commit 612376a74964c79af5b4029c650c8add5fd50120)
    
    Conflicts:
    	do_cmake.sh
    - nautilus does not have 9a47b328b80e5e502bd67cb94f925efe0c53c755

--- do_cmake.sh
+++ do_cmake.sh
@@ -23,13 +23,13 @@ if [ -r /etc/os-release ]; then
           ;;
       opensuse*|suse|sles)
           PYBUILD="3"
-          WITH_RADOSGW_AMQP_ENDPOINT="OFF"
+          ARGS+=" -DWITH_RADOSGW_AMQP_ENDPOINT=OFF"
           WITH_RADOSGW_KAFKA_ENDPOINT="OFF"
           ;;
   esac
 elif [ "$(uname)" == FreeBSD ] ; then
   PYBUILD="3"
-  WITH_RADOSGW_AMQP_ENDPOINT="OFF"
+  ARGS+=" -DWITH_RADOSGW_AMQP_ENDPOINT=OFF"
   WITH_RADOSGW_KAFKA_ENDPOINT="OFF"
 else
   echo Unknown release
@@ -37,15 +37,12 @@ else
 fi
 
 if [ "$PYBUILD" = "3" ] ; then
-    ARGS="$ARGS -DWITH_PYTHON2=OFF -DWITH_PYTHON3=ON -DMGR_PYTHON_VERSION=3"
+    ARGS+=" -DWITH_PYTHON2=OFF -DWITH_PYTHON3=ON -DMGR_PYTHON_VERSION=3"
 fi
 
 if type ccache > /dev/null 2>&1 ; then
     echo "enabling ccache"
-    ARGS="$ARGS -DWITH_CCACHE=ON"
-fi
-if [ -n "$WITH_RADOSGW_AMQP_ENDPOINT" ] ; then
-    ARGS="$ARGS -DWITH_RADOSGW_AMQP_ENDPOINT=$WITH_RADOSGW_AMQP_ENDPOINT"
+    ARGS+=" -DWITH_CCACHE=ON"
 fi
 
 mkdir build
