2ba80e73cb3e47fee057aaa007eb2d86b3b49d32
Sage Weil <sage@redhat.com>
2019-09-30 17:01:51 -0500

    test_ceph_daemon: specify image name
    Signed-off-by: Sage Weil <sage@redhat.com>

--- test_ceph_daemon.sh
+++ test_ceph_daemon.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -ex
 
 fsid=2a833e3f-53e4-49a7-a7a0-bd89d193ab62
+image=ceph/daemon-base:latest-master-devel
 
 ../src/ceph-daemon rm-cluster --fsid $fsid --force
 
@@ -9,32 +10,38 @@ cat <<EOF > c
 log to file = true
 EOF
 
-../src/ceph-daemon bootstrap \
-		   --mon-id a \
-		   --mgr-id x \
-		   --fsid $fsid \
-		   --mon-ip 10.3.64.23 \
-		   --config c \
-		   --output-keyring k \
-		   --output-config c
+../src/ceph-daemon \
+    --image $image \
+    bootstrap \
+    --mon-id a \
+    --mgr-id x \
+    --fsid $fsid \
+    --mon-ip 10.3.64.23 \
+    --config c \
+    --output-keyring k \
+    --output-config c
 chmod 644 k c
 
 # mon.b
-../src/ceph-daemon deploy --name mon.b \
-		   --fsid $fsid \
-		   --mon-ip 10.3.64.27 \
-		   --keyring /var/lib/ceph/$fsid/mon.a/keyring \
-		   --config c
+../src/ceph-daemon \
+    --image $image \
+    deploy --name mon.b \
+    --fsid $fsid \
+    --mon-ip 10.3.64.27 \
+    --keyring /var/lib/ceph/$fsid/mon.a/keyring \
+    --config c
 
 # mgr.b
 bin/ceph -c c -k k auth get-or-create mgr.y \
 	 mon 'allow profile mgr' \
 	 osd 'allow *' \
 	 mds 'allow *' > k-mgr.y
-../src/ceph-daemon deploy --name mgr.y \
-		   --fsid $fsid \
-		   --keyring k-mgr.y \
-		   --config c
+../src/ceph-daemon \
+    --image $image \
+    deploy --name mgr.y \
+    --fsid $fsid \
+    --keyring k-mgr.y \
+    --config c
 
 # mds.{k,j}
 for id in k j; do
@@ -43,10 +50,12 @@ for id in k j; do
 	     mgr 'allow profile mds' \
 	     osd 'allow *' \
 	     mds 'allow *' > k-mds.$id
-    ../src/ceph-daemon deploy --name mds.$id \
-		       --fsid $fsid \
-		       --keyring k-mds.$id \
-		       --config c
+    ../src/ceph-daemon \
+	--image $image \
+	deploy --name mds.$id \
+	--fsid $fsid \
+	--keyring k-mds.$id \
+	--config c
 done
 
 bin/ceph -c c -k k -s
