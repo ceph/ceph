9e382138f8ba4cf8931790f383bac4b51778e071
Yehuda Sadeh <yehuda@redhat.com>
2019-10-01 11:55:29 -0700

    test/cls/fifo: test FIFO::open() functionality
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/test/cls_fifo/test_cls_fifo.cc
+++ src/test/cls_fifo/test_cls_fifo.cc
@@ -134,5 +134,80 @@ TEST(ClsFIFO, TestGetInfo) {
                .objv(objv),
                &info));
 
+  ASSERT_EQ(0, destroy_one_pool_pp(pool_name, cluster));
+}
+
+TEST(FIFO, TestOpenDefault) {
+  Rados cluster;
+  std::string pool_name = get_temp_pool_name();
+  ASSERT_EQ("", create_one_pool_pp(pool_name, cluster));
+  IoCtx ioctx;
+  cluster.ioctx_create(pool_name.c_str(), ioctx);
+
+  string fifo_id = "fifo";
+
+  FIFO fifo(g_ceph_context, fifo_id, &ioctx);
+
+  /* pre-open ops that should fail */
+  ASSERT_EQ(-EINVAL, fifo.read_meta());
+
+  bufferlist bl;
+  ASSERT_EQ(-EINVAL, fifo.push(bl));
+
+  ASSERT_EQ(-EINVAL, fifo.list(100, nullopt, nullptr, nullptr));
+  ASSERT_EQ(-EINVAL, fifo.trim(string()));
+
+  ASSERT_EQ(-ENOENT, fifo.open(false));
+
+  /* first successful create */
+  ASSERT_EQ(0, fifo.open(true));
+
+  ASSERT_EQ(0, fifo.read_meta()); /* force reading from backend */
+
+  auto info = fifo.get_meta();
+
+  ASSERT_EQ(info.id, fifo_id);
+
+  ASSERT_EQ(0, destroy_one_pool_pp(pool_name, cluster));
+}
+
+TEST(FIFO, TestOpenParams) {
+  Rados cluster;
+  std::string pool_name = get_temp_pool_name();
+  ASSERT_EQ("", create_one_pool_pp(pool_name, cluster));
+  IoCtx ioctx;
+  cluster.ioctx_create(pool_name.c_str(), ioctx);
+
+  string fifo_id = "fifo";
+
+  FIFO fifo(g_ceph_context, fifo_id, &ioctx);
+
+  uint64_t max_part_size = 10 * 1024;
+  uint64_t max_entry_size = 128;
+  string oid_prefix = "foo.123.";
+
+  fifo_objv_t objv;
+  objv.instance = "fooz";
+  objv.ver = 10;
+
+
+  /* first successful create */
+  ASSERT_EQ(0, fifo.open(true,
+                         ClsFIFO::MetaCreateParams()
+                         .max_part_size(max_part_size)
+                         .max_entry_size(max_entry_size)
+                         .oid_prefix(oid_prefix)
+                         .objv(objv)));
+
+  ASSERT_EQ(0, fifo.read_meta()); /* force reading from backend */
+
+  auto info = fifo.get_meta();
+
+  ASSERT_EQ(info.id, fifo_id);
+  ASSERT_EQ(info.data_params.max_part_size, max_part_size);
+  ASSERT_EQ(info.data_params.max_entry_size, max_entry_size);
+  ASSERT_EQ(info.objv, objv);
+
+  ASSERT_EQ(0, destroy_one_pool_pp(pool_name, cluster));
 }
 
