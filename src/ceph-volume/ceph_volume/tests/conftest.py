ab8f2d68357355c05637e05f9b28ea9805287570
Rishabh Dave <ridave@redhat.com>
2019-10-03 17:00:38 +0530

    ceph-volume: update tests since PVolumes.filter returns a list
    ...returns a list of filtered PVs instead of removing the items that do
    not match the filters from itself.
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- src/ceph-volume/ceph_volume/tests/conftest.py
+++ src/ceph-volume/ceph_volume/tests/conftest.py
@@ -167,6 +167,12 @@ def pvolumes(monkeypatch):
     pvolumes = lvm_api.PVolumes()
     pvolumes._purge()
     return pvolumes
+@pytest.fixture
+def pvolumes_empty(monkeypatch):
+    monkeypatch.setattr('ceph_volume.process.call', lambda x, **kw: ('', '', 0))
+    pvolumes = lvm_api.PVolumes(populate=False)
+    return pvolumes
+
 
 
 @pytest.fixture
