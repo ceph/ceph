283e00262ee8cfc0562bb4b8a889ee1972a52448
Rishabh Dave <ridave@redhat.com>
2019-10-03 17:39:37 +0530

    ceph-volume: VolumeGroups.filter shouldn't purge itself
    VolumeGroups.filter remove VGs from the list that do no match filter.
    Instead of doing that, return a new list that contains VGs that match
    the fiter so that VolumeGroups object held by code calling it is not
    modified.
    
    Fixes: https://tracker.ceph.com/issues/42171
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- src/ceph-volume/ceph_volume/api/lvm.py
+++ src/ceph-volume/ceph_volume/api/lvm.py
@@ -764,15 +764,10 @@ class VolumeGroups(list):
         """
         if not any([vg_name, vg_tags]):
             raise TypeError('.filter() requires vg_name or vg_tags (none given)')
-        # first find the filtered volumes with the values in self
-        filtered_groups = self._filter(
-            vg_name=vg_name,
-            vg_tags=vg_tags
-        )
-        # then purge everything
-        self._purge()
-        # and add the filtered items
-        self.extend(filtered_groups)
+
+        filtered_vgs = VolumeGroups(populate=False)
+        filtered_vgs.extend(self._filter(vg_name, vg_tags))
+        return filtered_vgs
 
     def get(self, vg_name=None, vg_tags=None):
         """
