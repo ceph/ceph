029abbdf3319c7806c5fa4f9fc74b7e8ca613ad2
Jeff Layton <jlayton@redhat.com>
2019-10-09 10:06:07 -0400

    mds: add infrastructure for delegating inos from the prealloc set
    Add a new interval_set to Session to track which preallocated inos have
    been delegated to the client. Alter take_ino to also remove inos from
    that set if they are present there. Add a new routine to refill the
    delegated_inos set from the prealloc_set.
    
    Signed-off-by: Jeff Layton <jlayton@redhat.com>

--- src/mds/SessionMap.h
+++ src/mds/SessionMap.h
@@ -176,20 +176,63 @@ public:
     ceph_assert(!info.prealloc_inos.empty());
 
     if (ino) {
-      if (info.prealloc_inos.contains(ino))
+      if (info.prealloc_inos.contains(ino)) {
 	info.prealloc_inos.erase(ino);
-      else
+	if (delegated_inos.contains(ino))
+	  delegated_inos.erase(ino);
+      } else {
 	ino = 0;
+      }
     }
     if (!ino) {
-      ino = info.prealloc_inos.range_start();
-      info.prealloc_inos.erase(ino);
+      /* Grab first prealloc_ino that isn't delegated */
+      for (const auto& [start, len] : info.prealloc_inos) {
+	for (auto i = start ; i < start + len ; i += 1) {
+	  inodeno_t dstart, dlen;
+	  if (!delegated_inos.contains(i, &dstart, &dlen)) {
+	    ino = i;
+	    info.prealloc_inos.erase(ino);
+	    break;
+	  }
+	  /* skip to end of delegated interval */
+	  i = dstart + dlen - 1;
+	}
+	if (ino)
+	  break;
+      }
     }
+    ceph_assert(ino);
     info.used_inos.insert(ino, 1);
     return ino;
   }
+  void delegate_inos(int want, interval_set<inodeno_t>& newinos) {
+    want -= (int)delegated_inos.size();
+    if (want <= 0)
+      return;
+
+    for (const auto& [start, len] : info.prealloc_inos) {
+      for (auto i = start ; i < start + len ; i += 1) {
+	inodeno_t dstart, dlen;
+	if (!delegated_inos.contains(i, &dstart, &dlen)) {
+	  delegated_inos.insert(i);
+	  newinos.insert(i);
+	  if (--want == 0)
+	     return;
+	} else {
+	  /* skip to end of delegated interval */
+	  i = dstart + dlen - 1;
+	}
+      }
+    }
+  }
+
+  // sans any delegated ones
+  int get_num_prealloc_inos() const {
+    return info.prealloc_inos.size() - delegated_inos.size();
+  }
+
   int get_num_projected_prealloc_inos() const {
-    return info.prealloc_inos.size() + pending_prealloc_inos.size();
+    return get_num_prealloc_inos() + pending_prealloc_inos.size();
   }
 
   client_t get_client() const {
@@ -362,6 +405,7 @@ public:
 
   void clear() {
     pending_prealloc_inos.clear();
+    delegated_inos.clear();
     info.clear_meta();
 
     cap_push_seq = 0;
@@ -382,6 +426,7 @@ public:
   mutable elist<MDRequestImpl*> requests;
 
   interval_set<inodeno_t> pending_prealloc_inos; // journaling prealloc, will be added to prealloc_inos
+  interval_set<inodeno_t> delegated_inos; // hand these out to client
 
   xlist<Capability*> caps;     // inodes with caps; front=most recently used
   xlist<ClientLease*> leases;  // metadata leases to clients
