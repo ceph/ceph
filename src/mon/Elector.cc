b12970e7a45166771de441e612cc12e30b4ad986
Greg Farnum <gfarnum@redhat.com>
2019-10-10 17:11:55 -0700

    test: elector: make macros to run test scenarios against each ElectionStrategy
    Switch the ElectionLogic to require an ElectionStrategy input on construction.
    
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>

--- src/mon/Elector.cc
+++ src/mon/Elector.cc
@@ -58,7 +58,7 @@ static ostream& _prefix(std::ostream *_dout, Monitor *mon, epoch_t epoch) {
 		<< ").elector(" << epoch << ") ";
 }
 
-Elector::Elector(Monitor *m) : logic(this,
+Elector::Elector(Monitor *m) : logic(this, ElectionLogic::CLASSIC,
 				     &peer_tracker, m->cct),
 			       peer_tracker(this, 12*60*60), // TODO: make configurable
 			       ping_timeout(2),
