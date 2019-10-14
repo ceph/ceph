09b4b5fa5924959dff5f8ae8c3326fa8446c2574
GaryHyg <huygbj@inspur.com>
2019-10-14 15:12:46 +0800

    update max_buckets with proper value
    Signed-off-by: GaryHyg <huygbj@inspur.com>

--- src/rgw/rgw_rest_user.cc
+++ src/rgw/rgw_rest_user.cc
@@ -183,6 +183,9 @@ void RGWOp_User_Create::execute()
   }
 
   if (max_buckets != default_max_buckets)
+    if (max_buckets < 0)  {
+      max_buckets == -1;
+    }
     op_state.set_max_buckets(max_buckets);
 
   if (s->info.args.exists("suspended"))
@@ -274,8 +277,12 @@ void RGWOp_User_Modify::execute()
   op_state.set_access_key(access_key);
   op_state.set_secret_key(secret_key);
 
-  if (quota_set)
+  if (quota_set) {
+    if (max_buckets < 0) {
+      max_buckets = -1;
+    }
     op_state.set_max_buckets(max_buckets);
+  }
 
   if (gen_key)
     op_state.set_generate_key();
