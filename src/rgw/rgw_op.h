2fe8891f31b74c8672fbb64fc940aaba5c92d5a6
Abhishek Lekshmanan <abhishek@suse.com>
2019-10-16 13:22:09 +0200

    rgw: PublicAccessConfiguration -> PublicAccessBlockConfiguration
    also drop iam namespace
    
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- src/rgw/rgw_op.h
+++ src/rgw/rgw_op.h
@@ -2400,7 +2400,7 @@ public:
 class RGWPutBucketPublicAccessBlock : public RGWOp {
 protected:
   bufferlist data;
-  rgw::IAM::PublicAccessConfiguration access_conf;
+  PublicAccessBlockConfiguration access_conf;
 public:
   int verify_permission() override;
   const char* name() const override { return "put_bucket_public_access_block";}
@@ -2413,7 +2413,7 @@ public:
 
 class RGWGetBucketPublicAccessBlock : public RGWOp {
 protected:
-  rgw::IAM::PublicAccessConfiguration access_conf;
+  PublicAccessBlockConfiguration access_conf;
 public:
   int verify_permission() override;
   const char* name() const override { return "get_bucket_public_access_block";}
@@ -2426,7 +2426,7 @@ public:
 
 class RGWDeleteBucketPublicAccessBlock : public RGWOp {
 protected:
-  rgw::IAM::PublicAccessConfiguration access_conf;
+  PublicAccessBlockConfiguration access_conf;
 public:
   int verify_permission() override;
   const char* name() const override { return "delete_bucket_public_access_block";}
