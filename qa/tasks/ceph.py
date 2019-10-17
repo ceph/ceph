be13ba09a7a89d6dcc9f6ff8ce032e328a70f0d2
Kefu Chai <kchai@redhat.com>
2019-10-17 22:07:18 +0800

    qa/tasks/ceph_manager: do not panic of "pg_num_target" is missing
    we don't have "pg_num_target" in "osd dump" back in mimic, so we don't
    need to check it if it is missing when performing upgrade test.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- qa/tasks/ceph.py
+++ qa/tasks/ceph.py
@@ -1264,7 +1264,11 @@ def osd_scrub_pgs(ctx, config):
         unclean = [stat['pgid'] for stat in stats if 'active+clean' not in stat['state']]
         split_merge = []
         osd_dump = manager.get_osd_dump_json()
-        split_merge = [i['pool_name'] for i in osd_dump['pools'] if i['pg_num'] != i['pg_num_target']]
+        try:
+            split_merge = [i['pool_name'] for i in osd_dump['pools'] if i['pg_num'] != i['pg_num_target']]
+        except KeyError:
+            # we don't support pg_num_target before nautilus
+            pass
         if not unclean and not split_merge:
             all_clean = True
             break
