3f5afc9386b84388d145c85f912e879c56bed508
Kefu Chai <kchai@redhat.com>
2019-10-22 15:07:35 +0800

    rgw: s/wait_for_safe/wait_for_complete/
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -5272,7 +5272,7 @@ int RGWRados::Object::Stat::wait()
     return state.ret;
   }
 
-  state.completion->wait_for_safe();
+  state.completion->wait_for_complete();
   state.ret = state.completion->get_return_value();
   state.completion->release();
 
