5816bb6699752f971b375320a55f9246a9ccc1fa
Yehuda Sadeh <yehuda@redhat.com>
2019-10-22 15:18:14 -0700

    rgw: sync policy: cleanups and fixes
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_sync_policy.h
+++ src/rgw/rgw_sync_policy.h
@@ -17,161 +17,6 @@
 
 #include "rgw_common.h"
 
-#if 0
-struct rgw_sync_flow_directional_rule {
-  string source_zone;
-  string target_zone;
-
-  void encode(bufferlist& bl) const {
-    ENCODE_START(1, 1, bl);
-    encode(source_zone, bl);
-    encode(target_zone, bl);
-    ENCODE_FINISH(bl);
-  }
-
-  void decode(bufferlist::const_iterator& bl) {
-    DECODE_START(1, bl);
-    decode(source_zone, bl);
-    decode(target_zone, bl);
-    DECODE_FINISH(bl);
-  }
-
-  void dump(ceph::Formatter *f) const;
-  void decode_json(JSONObj *obj);
-};
-WRITE_CLASS_ENCODER(rgw_sync_flow_directional_rule)
-
-struct rgw_sync_flow_rule {
-  string id;
-  std::optional<rgw_sync_flow_directional_rule> directional;
-  std::optional<std::set<string> > symmetrical;
-
-  void encode(bufferlist& bl) const {
-    ENCODE_START(1, 1, bl);
-    encode(id, bl);
-    encode(directional, bl);
-    encode(symmetrical, bl);
-    ENCODE_FINISH(bl);
-  }
-
-  void decode(bufferlist::const_iterator& bl) {
-    DECODE_START(1, bl);
-    decode(id, bl);
-    decode(directional, bl);
-    decode(symmetrical, bl);
-    DECODE_FINISH(bl);
-  }
-
-  void dump(ceph::Formatter *f) const;
-  void decode_json(JSONObj *obj);
-
-  void get_zone_peers(const string& zone_id, std::set<string> *sources, std::set<string> *targets) const;
-};
-WRITE_CLASS_ENCODER(rgw_sync_flow_rule)
-
-struct rgw_sync_source {
-  string id;
-  string type;
-  std::optional<string> zone;
-  std::optional<rgw_bucket> bucket;
-  /* FIXME: config */
-
-  void encode(bufferlist& bl) const {
-    ENCODE_START(1, 1, bl);
-    encode(id, bl);
-    encode(type, bl);
-    encode(zone, bl);
-    encode(bucket, bl);
-    ENCODE_FINISH(bl);
-  }
-
-  void decode(bufferlist::const_iterator& bl) {
-    DECODE_START(1, bl);
-    decode(id, bl);
-    decode(type, bl);
-    decode(zone, bl);
-    decode(bucket, bl);
-    DECODE_FINISH(bl);
-  }
-
-  void dump(ceph::Formatter *f) const;
-  void decode_json(JSONObj *obj);
-};
-WRITE_CLASS_ENCODER(rgw_sync_source)
-
-struct rgw_sync_target {
-  string id;
-  string type;
-  std::optional<std::vector<rgw_sync_flow_rule> > flow_rules; /* flow rules for trivial sources,
-                                                                if set then needs to be a subset of higher level rules */
-  std::set<string> zones;  /* target zones. Can be wildcard */
-  /* FIXME: add config */
-
-  std::vector<rgw_sync_source> sources; /* non-trivial sources */
-  std::optional<rgw_bucket> bucket; /* can be explicit, or not set. If not set then depending
-                                       on the context */
-  
-  void encode(bufferlist& bl) const {
-    ENCODE_START(1, 1, bl);
-    encode(id, bl);
-    encode(type, bl);
-    encode(flow_rules, bl);
-    encode(zones, bl);
-    encode(sources, bl);
-    encode(bucket, bl);
-    ENCODE_FINISH(bl);
-  }
-
-  void decode(bufferlist::const_iterator& bl) {
-    DECODE_START(1, bl);
-    decode(id, bl);
-    decode(type, bl);
-    decode(flow_rules, bl);
-    decode(zones, bl);
-    decode(sources, bl);
-    decode(bucket, bl);
-    DECODE_FINISH(bl);
-  }
-
-  void dump(ceph::Formatter *f) const;
-  void decode_json(JSONObj *obj);
-};
-WRITE_CLASS_ENCODER(rgw_sync_target)
-
-
-struct rgw_sync_policy_info {
-  std::optional<std::vector<rgw_sync_flow_rule> > flow_rules;
-  std::optional<std::vector<rgw_sync_source> > sources;
-  std::optional<std::vector<rgw_sync_target> > targets;
-
-  void encode(bufferlist& bl) const {
-    ENCODE_START(1, 1, bl);
-    encode(flow_rules, bl);
-    encode(sources, bl);
-    encode(targets, bl);
-    ENCODE_FINISH(bl);
-  }
-
-  void decode(bufferlist::const_iterator& bl) {
-    DECODE_START(1, bl);
-    decode(flow_rules, bl);
-    decode(sources, bl);
-    decode(targets, bl);
-    DECODE_FINISH(bl);
-  }
-
-  void dump(ceph::Formatter *f) const;
-  void decode_json(JSONObj *obj);
-
-  bool empty() const {
-    return (!flow_rules || flow_rules->empty()) &&
-           (!targets || targets->empty());
-  }
-
-};
-WRITE_CLASS_ENCODER(rgw_sync_policy_info)
-
-#endif
 
 struct rgw_sync_symmetric_group {
   string id;
@@ -342,14 +187,6 @@ public:
 WRITE_CLASS_ENCODER(rgw_sync_bucket_pipe)
 
 struct rgw_sync_bucket_entities {
-private:
-  bool match_str(const string& s1, const string& s2) const { /* empty string is wildcard */
-    return (s1.empty() ||
-            s2.empty() ||
-            s1 == s2);
-  }
-
-public:
   std::optional<rgw_bucket> bucket; /* define specific bucket */
   std::optional<std::set<string> > zones; /* define specific zones, if not set then all zones */
 
@@ -384,9 +221,9 @@ public:
       return true;
     }
 
-    return (match_str(bucket->tenant, b->tenant) &&
-            match_str(bucket->name, b->name) &&
-            match_str(bucket->bucket_id, b->bucket_id));
+    return (rgw_sync_bucket_entity::match_str(bucket->tenant, b->tenant) &&
+            rgw_sync_bucket_entity::match_str(bucket->name, b->name) &&
+            rgw_sync_bucket_entity::match_str(bucket->bucket_id, b->bucket_id));
   }
 
   void add_zones(const std::vector<string>& new_zones);
@@ -419,16 +256,6 @@ public:
 WRITE_CLASS_ENCODER(rgw_sync_bucket_entities)
 
 struct rgw_sync_bucket_pipes {
-private:
-  void symmetrical_copy_if_empty(string& s1, string& s2) const {
-    if (s1.empty()) {
-      s1 = s2;
-    } else if (s2.empty()) {
-      s2 = s1;
-    }
-  }
-
-public:
   string id;
   rgw_sync_bucket_entities source;
   rgw_sync_bucket_entities dest;
@@ -447,13 +274,6 @@ public:
     decode(source, bl);
     decode(dest, bl);
     DECODE_FINISH(bl);
-  }
-
-  bool contains_bucket(std::optional<rgw_bucket> b) const {
-    return (source.match_bucket(b) || dest.match_bucket(b));
-  }
-  bool contains_zone(const string& zone) const {
-    return (source.match_zone(zone) || dest.match_zone(zone));
   }
 
   bool match_source(const string& zone, std::optional<rgw_bucket> b) const {
