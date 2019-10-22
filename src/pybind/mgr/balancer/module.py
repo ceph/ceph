aa9f4e7893fda2dbd186f9a491d986009a90faf8
David Zafman <dzafman@redhat.com>
2019-10-22 15:24:05 -0700

    mgr: Release GIL before calling OSDMap::calc_pg_upmaps()
    Prevent optimize and execute commands from running with active balancer
    
    Fixes: https://tracker.ceph.com/issues/42432
    
    Signed-off-by: David Zafman <dzafman@redhat.com>
    (cherry picked from commit e2a35e8c8e9f381a635ad0ca01326a2c512590a9)

--- src/pybind/mgr/balancer/module.py
+++ src/pybind/mgr/balancer/module.py
@@ -403,6 +403,7 @@ class Module(MgrModule):
     run = True
     plans = {}
     mode = ''
+    optimizing = False
 
     def __init__(self, *args, **kwargs):
         super(Module, self).__init__(*args, **kwargs)
@@ -515,6 +516,11 @@ class Module(MgrModule):
                                   'current cluster')
             return (0, self.evaluate(ms, pools, verbose=verbose), '')
         elif command['prefix'] == 'balancer optimize':
+            # The GIL can be release by the active balancer, so disallow when active
+            if self.active:
+                return (-errno.EINVAL, '', 'Balancer enabled, disable to optimize manually')
+            if self.optimizing:
+                return (-errno.EINVAL, '', 'Balancer finishing up....try again')
             pools = []
             if 'pools' in command:
                 pools = command['pools']
@@ -528,10 +534,9 @@ class Module(MgrModule):
                 return (-errno.EINVAL, '', 'pools %s not found' % invalid_pool_names)
             plan = self.plan_create(command['plan'], osdmap, pools)
             r, detail = self.optimize(plan)
-            # remove plan if we are currently unable to find an optimization
-            # or distribution is already perfect
-            if r:
-                self.plan_rm(command['plan'])
+            # Add plan if an optimization was created
+            if not r:
+                self.plans[command['plan']] = plan
             return (r, '', detail)
         elif command['prefix'] == 'balancer rm':
             self.plan_rm(command['plan'])
@@ -552,6 +557,11 @@ class Module(MgrModule):
                 return (-errno.ENOENT, '', 'plan %s not found' % command['plan'])
             return (0, plan.show(), '')
         elif command['prefix'] == 'balancer execute':
+            # The GIL can be release by the active balancer, so disallow when active
+            if self.active:
+                return (-errno.EINVAL, '', 'Balancer enabled, disable to execute a plan')
+            if self.optimizing:
+                return (-errno.EINVAL, '', 'Balancer finishing up....try again')
             plan = self.plans.get(command['plan'])
             if not plan:
                 return (-errno.ENOENT, '', 'plan %s not found' % command['plan'])
@@ -621,10 +631,11 @@ class Module(MgrModule):
                     final = [int(p) for p in final]
                     final = [pool_name_by_id[p] for p in final if p in pool_name_by_id]
                 plan = self.plan_create(name, osdmap, final)
+                self.optimizing = True
                 r, detail = self.optimize(plan)
                 if r == 0:
                     self.execute(plan)
-                self.plan_rm(name)
+                self.optimizing = False
             self.log.debug('Sleeping for %d', sleep_interval)
             self.event.wait(sleep_interval)
             self.event.clear()
@@ -635,7 +646,6 @@ class Module(MgrModule):
                                  self.get("pg_dump"),
                                  'plan %s initial' % name),
                     pools)
-        self.plans[name] = plan
         return plan
 
     def plan_rm(self, name):
