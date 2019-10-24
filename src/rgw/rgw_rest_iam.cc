ceph/ceph

commit 446f28db4f2f0de01ebefe368a0d6edccad1652e

rgw/pubsub: allow pubsub REST API on master
Signed-off-by: Yuval Lifshitz <yuvalif@yahoo.com>

src/rgw/rgw_rest_iam.cc
@@ -26,12 +26,13 @@ void RGWHandler_REST_IAM::rgw_iam_parse_input()
       for (const auto& t : tokens) {
         auto pos = t.find("=");
         if (pos != string::npos) {
-          std::string key = t.substr(0, pos);
-          std::string value = t.substr(pos + 1, t.size() - 1);
-          if (key == "AssumeRolePolicyDocument" || key == "Path" || key == "PolicyDocument") {
-            value = url_decode(value);
+          const auto key = t.substr(0, pos);
+          if (key == "Action") {
+            s->info.args.append(key, t.substr(pos + 1, t.size() - 1));
+          } else if (key == "AssumeRolePolicyDocument" || key == "Path" || key == "PolicyDocument") {
+            const auto value = url_decode(t.substr(pos + 1, t.size() - 1));
+            s->info.args.append(key, value);
           }
-          s->info.args.append(key, value);
         }
       }
     }

src/common/options.cc
@@ -5716,7 +5716,7 @@ std::vector<Option> get_rgw_options() {
         "will be located in the path that is specified here. "),
 
     Option("rgw_enable_apis", Option::TYPE_STR, Option::LEVEL_ADVANCED)
-    .set_default("s3, s3website, swift, swift_auth, admin, sts, iam")
+    .set_default("s3, s3website, swift, swift_auth, admin, sts, iam, pubsub")
     .set_description("A list of set of RESTful APIs that rgw handles."),
 
     Option("rgw_cache_enabled", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)

src/mrun
@@ -21,4 +21,10 @@ fi
 
 shift 2
 
-$CEPH_BIN/$command -c $CEPH_CONF_PATH/ceph.conf "$@"
+if [ "$RGW_VALGRIND" == 'yes' ] && [ $command == 'radosgw' ]; then
+    valgrind --trace-children=yes --tool=memcheck --max-threads=1024 $CEPH_BIN/$command -c $CEPH_CONF_PATH/ceph.conf "$@"
+    sleep 10
+else
+    $CEPH_BIN/$command -c $CEPH_CONF_PATH/ceph.conf "$@"
+fi
+

src/rgw/CMakeLists.txt
@@ -113,6 +113,8 @@ set(librgw_common_srcs
   rgw_rest_conn.cc
   rgw_rest_log.cc
   rgw_rest_metadata.cc
+  rgw_rest_pubsub.cc
+  rgw_rest_pubsub_common.cc
   rgw_rest_realm.cc
   rgw_rest_role.cc
   rgw_rest_s3.cc

src/rgw/rgw_basic_types.cc
@@ -6,6 +6,7 @@
 #include <string>
 
 #include "rgw_basic_types.h"
+#include "rgw_xml.h"
 #include "common/ceph_json.h"
 
 using std::string;
@@ -13,14 +14,17 @@ using std::stringstream;
 
 void decode_json_obj(rgw_user& val, JSONObj *obj)
 {
-  string s = obj->get_data();
-  val.from_str(s);
+  val.from_str(obj->get_data());
 }
 
 void encode_json(const char *name, const rgw_user& val, Formatter *f)
 {
-  string s = val.to_str();
-  f->dump_string(name, s);
+  f->dump_string(name, val.to_str());
+}
+
+void encode_xml(const char *name, const rgw_user& val, Formatter *f)
+{
+  encode_xml(name, val.to_str(), f);
 }
 
 namespace rgw {

src/rgw/rgw_basic_types.h
@@ -13,8 +13,7 @@ struct rgw_user {
   std::string id;
 
   rgw_user() {}
-  // cppcheck-suppress noExplicitConstructor
-  rgw_user(const std::string& s) {
+  explicit rgw_user(const std::string& s) {
     from_str(s);
   }
   rgw_user(const std::string& tenant, const std::string& id)
@@ -203,6 +202,7 @@ class JSONObj;
 
 void decode_json_obj(rgw_user& val, JSONObj *obj);
 void encode_json(const char *name, const rgw_user& val, Formatter *f);
+void encode_xml(const char *name, const rgw_user& val, Formatter *f);
 
 inline ostream& operator<<(ostream& out, const rgw_user &u) {
   string s;

src/rgw/rgw_bucket.cc
@@ -1389,7 +1389,7 @@ int RGWBucketAdminOp::limit_check(RGWRadosStore *store,
     do {
       RGWUserBuckets buckets;
 
-      ret = rgw_read_user_buckets(store, user_id, buckets,
+      ret = rgw_read_user_buckets(store, rgw_user(user_id), buckets,
 				  marker, string(), max_entries, false,
 				  &is_truncated);
       if (ret < 0)

src/rgw/rgw_cr_rados.cc
@@ -589,7 +589,7 @@ int RGWAsyncFetchRemoteObj::_send_request()
 
   std::optional<uint64_t> bytes_transferred;
   int r = store->getRados()->fetch_remote_obj(obj_ctx,
-                       user_id,
+                       rgw_user(user_id),
                        NULL, /* req_info */
                        source_zone,
                        dest_obj,
@@ -646,7 +646,7 @@ int RGWAsyncStatRemoteObj::_send_request()
   rgw_obj dest_obj(src_obj);
 
   int r = store->getRados()->stat_remote_obj(obj_ctx,
-                       user_id,
+                       rgw_user(user_id),
                        nullptr, /* req_info */
                        source_zone,
                        src_obj,
@@ -722,7 +722,7 @@ int RGWAsyncRemoveObj::_send_request()
   }
   del_op.params.olh_epoch = versioned_epoch;
   del_op.params.marker_version_id = marker_version_id;
-  del_op.params.obj_owner.set_id(owner);
+  del_op.params.obj_owner.set_id(rgw_user(owner));
   del_op.params.obj_owner.set_name(owner_display_name);
   del_op.params.mtime = timestamp;
   del_op.params.high_precision_time = true;

commit 0b469266241fc7ab9085d392e93923cbb4d63cdc

rgw: iam: add all http args to req_info
parsing a iam post request arguments were broken after
446f28db4f2f0de01ebefe368a0d6edccad1652e as all the key values weren't in http
args anymore. Since the if conditional were specifically to url_decode POST args
which are by default url encoded anyway, just add all the key value pairs to the
req_info args as these are used later by the respective process requests

Fixes: https://tracker.ceph.com/issues/42470
Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

src/rgw/rgw_rest_iam.cc
@@ -26,13 +26,8 @@ void RGWHandler_REST_IAM::rgw_iam_parse_input()
       for (const auto& t : tokens) {
         auto pos = t.find("=");
         if (pos != string::npos) {
-          const auto key = t.substr(0, pos);
-          if (key == "Action") {
-            s->info.args.append(key, t.substr(pos + 1, t.size() - 1));
-          } else if (key == "AssumeRolePolicyDocument" || key == "Path" || key == "PolicyDocument") {
-            const auto value = url_decode(t.substr(pos + 1, t.size() - 1));
-            s->info.args.append(key, value);
-          }
+          s->info.args.append(t.substr(0,pos),
+                              url_decode(t.substr(pos+1, t.size() -1)));
         }
       }
     }