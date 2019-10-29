d3098c82552991ef0ce7a19285a58697c8cf973a
weiyingze-git <weiyingze@cmss.chinamobile.com>
2019-10-30 03:22:26 +0800

    test:add test_remove_snap_ImageNotFound interface in remove snap part
    Signed-off-by: Yingze Wei <weiyingze@cmss.chinamobile.com>

--- src/test/pybind/test_rbd.py
+++ src/test/pybind/test_rbd.py
@@ -762,6 +762,9 @@ class TestImage(object):
         self.image.remove_snap('snap1')
         eq([], list(self.image.list_snaps()))
 
+    def test_remove_snap_not_found(self):
+        assert_raises(ImageNotFound, self.image.remove_snap, 'snap1')
+
     def test_remove_snap_by_id(self):
 	eq([], list(self.image.list_snaps()))
 	self.image.create_snap('snap1')
