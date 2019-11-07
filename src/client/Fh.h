aa9db2ef73ca4c5e8e09637ddcbbe26df4922797
Yan, Zheng <zyan@redhat.com>
2019-11-07 17:17:35 +0800

    client: invalidate all write mode filp after reconnect
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

--- src/client/Fh.h
+++ src/client/Fh.h
@@ -17,6 +17,7 @@ struct Fh {
   loff_t    pos;
   int       mds;        // have to talk to mds we opened with (for now)
   int       mode;       // the mode i opened the file with
+  uint64_t  gen;
 
   int flags;
   bool pos_locked;           // pos is currently in use
@@ -50,7 +51,7 @@ struct Fh {
   }
 
   Fh() = delete;
-  Fh(InodeRef in, int flags, int cmode, const UserPerm &perms);
+  Fh(InodeRef in, int flags, int cmode, uint64_t gen, const UserPerm &perms);
   ~Fh();
 
   void get() { ++_ref; }
