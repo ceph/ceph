b0e9d2ac6de3e68eb281680c6a66b46a228fd8ae
zhang Shaowen <zhangshaowen@cmss.chinamobile.com>
2019-11-08 17:50:23 +0800

    rgw: fix a bug that bucket instance obj can't be removed after resharding completed.
    Fixes: https://tracker.ceph.com/issues/42691
    
    Signed-off-by: zhang Shaowen <zhangshaowen@cmss.chinamobile.com>

--- src/rgw/rgw_reshard.cc
+++ src/rgw/rgw_reshard.cc
@@ -375,7 +375,7 @@ int RGWBucketReshard::cancel()
 class BucketInfoReshardUpdate
 {
   rgw::sal::RGWRadosStore *store;
-  RGWBucketInfo bucket_info;
+  RGWBucketInfo& bucket_info;
   std::map<string, bufferlist> bucket_attrs;
 
   bool in_progress{false};
