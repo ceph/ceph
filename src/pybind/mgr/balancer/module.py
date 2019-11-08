ceph/ceph

commit 86444dbfe5d8478530182116507d034d9e180f5e

mgr/balancer: add per pool concurrent optimization limit
the main point is that in general we have physical isolated storage
pools in our products, so the cluster-wide target_max_misplaced_ratio
option would not make much sense in such case.
Introducing a fine-grained alternate throttling mechanism instead make
things much better now!

Signed-off-by: xie xingguo <xie.xingguo@zte.com.cn>

src/pybind/mgr/balancer/module.py
@@ -901,7 +901,6 @@ class Module(MgrModule):
                 detail = 'Unrecognized mode %s' % plan.mode
                 self.log.info(detail)
                 return -errno.EINVAL, detail
-        ##
 
     def do_upmap(self, plan):
         self.log.info('do_upmap')
@@ -941,12 +940,29 @@ class Module(MgrModule):
         # shuffle so all pools get equal (in)attention
         random.shuffle(classified_pools)
         for it in classified_pools:
-            did = ms.osdmap.calc_pg_upmaps(inc, max_deviation, left, it)
+            pool_dump = osdmap_dump.get('pools', [])
+            num_pg = 0
+            for p in pool_dump:
+                if p['pool_name'] in it:
+                    num_pg += p['pg_num']
+
+            # note that here we deliberately exclude any scrubbing pgs too
+            # since scrubbing activities have significant impacts on performance
+            pool_ids = list(p['pool'] for p in pool_dump if p['pool_name'] in it)
+            num_pg_active_clean = 0
+            pg_dump = self.get('pg_dump')
+            for p in pg_dump['pg_stats']:
+                pg_pool = p['pgid'].split('.')[0]
+                if len(pool_ids) and int(pg_pool) not in pool_ids:
+                    continue
+                if p['state'] == 'active+clean':
+                    num_pg_active_clean += 1
+
+            available = max_optimizations - (num_pg - num_pg_active_clean)
+            did = ms.osdmap.calc_pg_upmaps(inc, max_deviation, available, it)
+            self.log.info('prepared %d changes for pool(s) %s' % (did, it))
             total_did += did
-            left -= did
-            if left <= 0:
-                break
-        self.log.info('prepared %d/%d changes' % (total_did, max_optimizations))
+        self.log.info('prepared %d changes in total' % total_did)
         if total_did == 0:
             return -errno.EALREADY, 'Unable to find further optimization, ' \
                                     'or pool(s)\' pg_num is decreasing, ' \

commit 33dbbe260b95d0cc7998b1e22fd0987dad4bf1bf

mgr/balancer: Fix max_optimizations per pool
This change didn't account for prior optimizations because
doesn't decrement how many optimizations are left.

In addition, it isn't clear why the code was subtracting
the count of non-active+clean (num_pg - num_pg_active_clean),
so I changed that to limit optimizations to active+clean pgs.

Caused by: 86444dbfe5d8478530182116507d034d9e180f5e

Signed-off-by: David Zafman <dzafman@redhat.com>

src/pybind/mgr/balancer/module.py
@@ -984,9 +984,16 @@ class Module(MgrModule):
                 if p['state'] == 'active+clean':
                     num_pg_active_clean += 1
 
-            available = max_optimizations - (num_pg - num_pg_active_clean)
+            # Don't need more optimizations than active+clean pgs
+            if (left > num_pg_active_clean)
+              available = num_pg_active_clean;
+            else
+              available = left;
             did = ms.osdmap.calc_pg_upmaps(inc, max_deviation, available, [pool])
             total_did += did
+            left -= did
+            if left <= 0:
+                break
         self.log.info('prepared %d changes in total' % total_did)
         if total_did == 0:
             return -errno.EALREADY, 'Unable to find further optimization, ' \