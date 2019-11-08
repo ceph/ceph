f0ff49238d1ab42786bbec3c6127d09b9177559a
Yehuda Sadeh <yehuda@redhat.com>
2019-11-07 16:56:52 -0800

    rgw: data sync: use hints when determining sync pipes
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/services/svc_bucket_sync_sobj.h
+++ src/rgw/services/svc_bucket_sync_sobj.h
@@ -29,6 +29,8 @@ class RGWSI_Bucket_SObj;
 template <class T>
 class RGWChainedCacheImpl;
 
+class RGWSI_Bucket_Sync_SObj_HintIndexManager;
+
 class RGWSI_Bucket_Sync_SObj : public RGWSI_Bucket_Sync
 {
   struct bucket_sync_policy_cache_entry {
@@ -38,33 +40,10 @@ class RGWSI_Bucket_Sync_SObj : public RGWSI_Bucket_Sync
   using RGWChainedCacheImpl_bucket_sync_policy_cache_entry = RGWChainedCacheImpl<bucket_sync_policy_cache_entry>;
   unique_ptr<RGWChainedCacheImpl_bucket_sync_policy_cache_entry> sync_policy_cache;
 
-  class HintIndexManager {
-    struct {
-      RGWSI_Zone *zone;
-      RGWSI_SysObj *sysobj;
-    } svc;
-
-  public:
-    HintIndexManager() {}
-
-    void init(RGWSI_Zone *_zone_svc,
-              RGWSI_SysObj *_sysobj_svc) {
-      svc.zone = _zone_svc;
-      svc.sysobj = _sysobj_svc;
-    }
-
-    rgw_raw_obj get_sources_obj(const rgw_bucket& bucket) const;
-    rgw_raw_obj get_dests_obj(const rgw_bucket& bucket) const;
-  } hint_index_mgr;
+  std::unique_ptr<RGWSI_Bucket_Sync_SObj_HintIndexManager> hint_index_mgr;
 
   int do_start() override;
 
-  int do_update_hints(const RGWBucketInfo& bucket_info,
-                      std::vector<rgw_bucket>& added_dests,
-                      std::vector<rgw_bucket>& removed_dests,
-                      std::vector<rgw_bucket>& added_sources,
-                      std::vector<rgw_bucket>& removed_sources,
-                      optional_yield y);
 public:
   struct Svc {
     RGWSI_Zone *zone{nullptr};
@@ -73,7 +52,7 @@ public:
     RGWSI_Bucket_SObj *bucket_sobj{nullptr};
   } svc;
 
-  RGWSI_Bucket_Sync_SObj(CephContext *cct) : RGWSI_Bucket_Sync(cct) {}
+  RGWSI_Bucket_Sync_SObj(CephContext *cct);
   ~RGWSI_Bucket_Sync_SObj();
 
   void init(RGWSI_Zone *_zone_svc,
