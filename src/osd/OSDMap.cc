a0a4aef448a6c87653011f0e59297ce747fef911
Neha Ojha <nojha@redhat.com>
2019-11-11 13:32:15 -0800

    osd/OSDMap.cc: don't output over/underfull messages to lderr
    There can be cases where overfull and underfull(see example in
    https://tracker.ceph.com/issues/42756) will be empty, which is not
    necessarily an error. These error messages can end up spamming
    the ceph-mgr log.
    
    Fixes: https://tracker.ceph.com/issues/42756
    Signed-off-by: Neha Ojha <nojha@redhat.com>
    (cherry picked from commit 3cf28f09a2ebdd922fc5c1d29c65469f8331e741)

--- src/osd/OSDMap.cc
+++ src/osd/OSDMap.cc
@@ -4560,7 +4560,7 @@ int OSDMap::calc_pg_upmaps(
                      << dendl;
     }
     if (overfull.empty()) {
-      lderr(cct) << __func__ << " failed to build overfull" << dendl;
+      ldout(cct, 20) << __func__ << " failed to build overfull" << dendl;
       break;
     }
 
@@ -4584,7 +4584,7 @@ int OSDMap::calc_pg_upmaps(
                      << dendl;
     }
     if (underfull.empty()) {
-      lderr(cct) << __func__ << " failed to build underfull" << dendl;
+      ldout(cct, 20) << __func__ << " failed to build underfull" << dendl;
       break;
     }
 
