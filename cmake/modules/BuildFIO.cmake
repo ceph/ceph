e85d4b6366a2353222660a8ecdc7ef04b95c3fdb
Igor Fedotov <ifedotov@suse.com>
2019-11-12 01:19:29 +0300

    test/fio: bump to fio 3.15
    Signed-off-by: Igor Fedotov <ifedotov@suse.com>

--- cmake/modules/BuildFIO.cmake
+++ cmake/modules/BuildFIO.cmake
@@ -9,7 +9,7 @@ function(build_fio)
     DOWNLOAD_DIR ${CMAKE_BINARY_DIR}/src/
     UPDATE_COMMAND "" # this disables rebuild on each run
     GIT_REPOSITORY "https://github.com/axboe/fio.git"
-    GIT_TAG "fio-3.10"
+    GIT_TAG "fio-3.15"
     SOURCE_DIR ${CMAKE_BINARY_DIR}/src/fio
     BUILD_IN_SOURCE 1
     CONFIGURE_COMMAND <SOURCE_DIR>/configure
