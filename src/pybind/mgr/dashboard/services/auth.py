sunnyku/ceph-ci

mgr/dashboard: use python logging pkg with named loggers
Signed-off-by: Ricardo Dias <rdias@suse.com>

src/pybind/mgr/dashboard/services/auth.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 
 from base64 import b64encode
 import json
+import logging
 import os
 import threading
 import time
@@ -12,7 +13,7 @@ import cherrypy
 import jwt
 
 from .access_control import LocalAuthenticator, UserDoesNotExist
-from .. import mgr, logger
+from .. import mgr
 
 
 class JwtManager(object):
@@ -30,6 +31,7 @@ class JwtManager(object):
 
     @classmethod
     def init(cls):
+        cls.logger = logging.getLogger('jtw')
         # generate a new secret if it does not exist
         secret = mgr.get_store('jwt_secret')
         if secret is None:
@@ -88,16 +90,16 @@ class JwtManager(object):
                 user = AuthManager.get_user(dtoken['username'])
                 if user.last_update <= dtoken['iat']:
                     return user
-                logger.debug("AMT: user info changed after token was issued, iat=%s last_update=%s",
-                             dtoken['iat'], user.last_update)
+                cls.logger.debug("user info changed after token was issued, iat=%s last_update=%s",
+                                 dtoken['iat'], user.last_update)
             else:
-                logger.debug('AMT: Token is black-listed')
+                cls.logger.debug('Token is black-listed')
         except jwt.exceptions.ExpiredSignatureError:
-            logger.debug("AMT: Token has expired")
+            cls.logger.debug("Token has expired")
         except jwt.exceptions.InvalidTokenError:
-            logger.debug("AMT: Failed to decode token")
+            cls.logger.debug("Failed to decode token")
         except UserDoesNotExist:
-            logger.debug("AMT: Invalid token: user %s does not exist", dtoken['username'])
+            cls.logger.debug("Invalid token: user %s does not exist", dtoken['username'])
         return None
 
     @classmethod
@@ -153,23 +155,24 @@ class AuthManagerTool(cherrypy.Tool):
     def __init__(self):
         super(AuthManagerTool, self).__init__(
             'before_handler', self._check_authentication, priority=20)
+        self.logger = logging.getLogger('auth')
 
     def _check_authentication(self):
         JwtManager.reset_user()
         token = JwtManager.get_token_from_header()
-        logger.debug("AMT: token: %s", token)
+        self.logger.debug("token: %s", token)
         if token:
             user = JwtManager.get_user(token)
             if user:
                 self._check_authorization(user.username)
                 return
-        logger.debug('AMT: Unauthorized access to %s',
-                     cherrypy.url(relative='server'))
+        self.logger.debug('Unauthorized access to %s',
+                          cherrypy.url(relative='server'))
         raise cherrypy.HTTPError(401, 'You are not authorized to access '
                                       'that resource')
 
     def _check_authorization(self, username):
-        logger.debug("AMT: checking authorization...")
+        self.logger.debug("checking authorization...")
         username = username
         handler = cherrypy.request.handler.callable
         controller = handler.__self__
@@ -181,12 +184,12 @@ class AuthManagerTool(cherrypy.Tool):
             # controller does not define any authorization restrictions
             return
 
-        logger.debug("AMT: checking '%s' access to '%s' scope", sec_perms,
-                     sec_scope)
+        self.logger.debug("checking '%s' access to '%s' scope", sec_perms,
+                          sec_scope)
 
         if not sec_perms:
-            logger.debug("Fail to check permission on: %s:%s", controller,
-                         handler)
+            self.logger.debug("Fail to check permission on: %s:%s", controller,
+                              handler)
             raise cherrypy.HTTPError(403, "You don't have permissions to "
                                           "access that resource")
 

src/pybind/mgr/dashboard/services/sso.py
@@ -5,17 +5,21 @@ from __future__ import absolute_import
 import os
 import errno
 import json
+import logging
 import threading
 import warnings
 
 import six
 from six.moves.urllib import parse
 
-from .. import mgr, logger
+from .. import mgr
 from ..tools import prepare_url_prefix
 
 if six.PY2:
     FileNotFoundError = IOError  # pylint: disable=redefined-builtin
+
+
+logger = logging.getLogger('sso')
 
 try:
     from onelogin.saml2.settings import OneLogin_Saml2_Settings as Saml2Settings
@@ -71,17 +75,17 @@ class SsoDB(object):
         return "{}{}".format(cls.SSODB_CONFIG_KEY, version)
 
     def check_and_update_db(self):
-        logger.debug("SSO: Checking for previews DB versions")
+        logger.debug("Checking for previews DB versions")
         if self.VERSION != 1:
             raise NotImplementedError()
 
     @classmethod
     def load(cls):
-        logger.info("SSO: Loading SSO DB version=%s", cls.VERSION)
+        logger.info("Loading SSO DB version=%s", cls.VERSION)
 
         json_db = mgr.get_store(cls.ssodb_config_key(), None)
         if json_db is None:
-            logger.debug("SSO: No DB v%s found, creating new...", cls.VERSION)
+            logger.debug("No DB v%s found, creating new...", cls.VERSION)
             db = cls(cls.VERSION, '', Saml2({}))
             # check if we can update from a previous version database
             db.check_and_update_db()

ceph/ceph

src/pybind/mgr/dashboard/services/auth.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 
 from base64 import b64encode
 import json
+import logging
 import os
 import threading
 import time
@@ -12,7 +13,7 @@ import cherrypy
 import jwt
 
 from .access_control import LocalAuthenticator, UserDoesNotExist
-from .. import mgr, logger
+from .. import mgr
 
 
 class JwtManager(object):
@@ -30,6 +31,7 @@ class JwtManager(object):
 
     @classmethod
     def init(cls):
+        cls.logger = logging.getLogger('jwt')
         # generate a new secret if it does not exist
         secret = mgr.get_store('jwt_secret')
         if secret is None:
@@ -88,16 +90,16 @@ class JwtManager(object):
                 user = AuthManager.get_user(dtoken['username'])
                 if user.last_update <= dtoken['iat']:
                     return user
-                logger.debug("AMT: user info changed after token was issued, iat=%s last_update=%s",
-                             dtoken['iat'], user.last_update)
+                cls.logger.debug("user info changed after token was issued, iat=%s last_update=%s",
+                                 dtoken['iat'], user.last_update)
             else:
-                logger.debug('AMT: Token is black-listed')
+                cls.logger.debug('Token is black-listed')
         except jwt.exceptions.ExpiredSignatureError:
-            logger.debug("AMT: Token has expired")
+            cls.logger.debug("Token has expired")
         except jwt.exceptions.InvalidTokenError:
-            logger.debug("AMT: Failed to decode token")
+            cls.logger.debug("Failed to decode token")
         except UserDoesNotExist:
-            logger.debug("AMT: Invalid token: user %s does not exist", dtoken['username'])
+            cls.logger.debug("Invalid token: user %s does not exist", dtoken['username'])
         return None
 
     @classmethod
@@ -153,23 +155,24 @@ class AuthManagerTool(cherrypy.Tool):
     def __init__(self):
         super(AuthManagerTool, self).__init__(
             'before_handler', self._check_authentication, priority=20)
+        self.logger = logging.getLogger('auth')
 
     def _check_authentication(self):
         JwtManager.reset_user()
         token = JwtManager.get_token_from_header()
-        logger.debug("AMT: token: %s", token)
+        self.logger.debug("token: %s", token)
         if token:
             user = JwtManager.get_user(token)
             if user:
                 self._check_authorization(user.username)
                 return
-        logger.debug('AMT: Unauthorized access to %s',
-                     cherrypy.url(relative='server'))
+        self.logger.debug('Unauthorized access to %s',
+                          cherrypy.url(relative='server'))
         raise cherrypy.HTTPError(401, 'You are not authorized to access '
                                       'that resource')
 
     def _check_authorization(self, username):
-        logger.debug("AMT: checking authorization...")
+        self.logger.debug("checking authorization...")
         username = username
         handler = cherrypy.request.handler.callable
         controller = handler.__self__
@@ -181,12 +184,12 @@ class AuthManagerTool(cherrypy.Tool):
             # controller does not define any authorization restrictions
             return
 
-        logger.debug("AMT: checking '%s' access to '%s' scope", sec_perms,
-                     sec_scope)
+        self.logger.debug("checking '%s' access to '%s' scope", sec_perms,
+                          sec_scope)
 
         if not sec_perms:
-            logger.debug("Fail to check permission on: %s:%s", controller,
-                         handler)
+            self.logger.debug("Fail to check permission on: %s:%s", controller,
+                              handler)
             raise cherrypy.HTTPError(403, "You don't have permissions to "
                                           "access that resource")
 

src/pybind/mgr/dashboard/services/sso.py
@@ -5,17 +5,21 @@ from __future__ import absolute_import
 import os
 import errno
 import json
+import logging
 import threading
 import warnings
 
 import six
 from six.moves.urllib import parse
 
-from .. import mgr, logger
+from .. import mgr
 from ..tools import prepare_url_prefix
 
 if six.PY2:
     FileNotFoundError = IOError  # pylint: disable=redefined-builtin
+
+
+logger = logging.getLogger('sso')
 
 try:
     from onelogin.saml2.settings import OneLogin_Saml2_Settings as Saml2Settings
@@ -71,17 +75,17 @@ class SsoDB(object):
         return "{}{}".format(cls.SSODB_CONFIG_KEY, version)
 
     def check_and_update_db(self):
-        logger.debug("SSO: Checking for previews DB versions")
+        logger.debug("Checking for previous DB versions")
         if self.VERSION != 1:
             raise NotImplementedError()
 
     @classmethod
     def load(cls):
-        logger.info("SSO: Loading SSO DB version=%s", cls.VERSION)
+        logger.info("Loading SSO DB version=%s", cls.VERSION)
 
         json_db = mgr.get_store(cls.ssodb_config_key(), None)
         if json_db is None:
-            logger.debug("SSO: No DB v%s found, creating new...", cls.VERSION)
+            logger.debug("No DB v%s found, creating new...", cls.VERSION)
             db = cls(cls.VERSION, '', Saml2({}))
             # check if we can update from a previous version database
             db.check_and_update_db()