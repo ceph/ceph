d615118102b1be77fc10b4ec908e5b5889258868
Sidharth Anupkrishnan <sanupkri@redhat.com>
2019-11-14 21:35:00 +0530

    Added MemoryWatchdog to monitor the memory usage of the MDSs

--- qa/tasks/fs.py
+++ qa/tasks/fs.py
@@ -7,9 +7,55 @@ import logging
 import re
 import time
 
+from gevent import sleep
+from gevent.greenlet import Greenlet
+from gevent.event import Event
+
 from tasks.cephfs.filesystem import Filesystem
 
 log = logging.getLogger(__name__)
+
+class MemoryWatchdog(Greenlet):
+    def __init__(self, ctx, config):
+        super(MemoryWatchdog, self).__init__()
+        self.ctx = ctx
+        self.config = config
+        self.e = None
+        self.logger = log.getChild('memory_watch')
+        self.cluster = config.get('cluster', 'ceph')
+        self.name = 'memorywatchdog'
+        self.stopping = Event()
+
+    def _run(self):
+        try:
+            self.watch()
+        except Exception as e:
+            self.e = e
+            self.logger.exception("exception:")
+            # allow successful completion so gevent doesn't see an exception...
+
+    def log(self, x):
+        """Write data to logger"""
+        self.logger.info(x)
+
+    def stop(self):
+        self.stopping.set()
+
+    def watch(self):
+        self.log("MemoryWatchdog starting")
+
+        while not self.stopping.is_set():
+            status = self.fs.status()
+            max_mds = status.get_fsmap(self.fs.id)['mdsmap']['max_mds']
+            ranks = list(status.get_ranks(self.fs.id))
+            actives = filter(lambda info: "up:active" == info['state'] and "laggy_since" not in info, ranks)
+            for rank in actives:
+                mem_info = self.fs.rank_asok(["perf", "dump"], rank)["mds_mem"]
+                self.log("Usage statistic for mds ", self.get_rank(rank=rank, status=status)["name"])
+                self.log(mem_info)
+                assertGreaterEqual(((int(self.fs.get_config("mds_memory_target")) * 125)/100), mem_info["rss"], "rss usage has exceeded the mds_memory_target")
+
+        self.log("MemoryWatchdog finished")
 
 def clients_evicted(ctx, config):
     """
