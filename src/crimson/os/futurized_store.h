samchoraria/ceph-ci

crimson/os: alias errorator for CyanStore::get_attrs().
Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

src/crimson/os/futurized_store.h
@@ -45,9 +45,13 @@ public:
 				   uint64_t offset,
 				   size_t len,
 				   uint32_t op_flags = 0) = 0;
-  virtual ceph::errorator<ceph::ct_error::enoent,
-                          ceph::ct_error::enodata>::future<ceph::bufferptr>
-  get_attr(CollectionRef c, const ghobject_t& oid, std::string_view name) const = 0;
+  using get_attr_errorator = ceph::errorator<
+    ceph::ct_error::enoent,
+    ceph::ct_error::enodata>;
+  virtual get_attr_errorator::future<ceph::bufferptr> get_attr(
+    CollectionRef c,
+    const ghobject_t& oid,
+    std::string_view name) const = 0;
 
   using attrs_t = std::map<std::string, ceph::bufferptr, std::less<>>;
   virtual seastar::future<attrs_t> get_attrs(CollectionRef c,

src/crimson/osd/pg_backend.h
@@ -84,8 +84,8 @@ public:
   seastar::future<> getxattr(
     const ObjectState& os,
     OSDOp& osd_op) const;
-  ceph::errorator<ceph::ct_error::enoent,
-                  ceph::ct_error::enodata>::future<ceph::bufferptr> getxattr(
+  using get_attr_errorator = ceph::os::FuturizedStore::get_attr_errorator;
+  get_attr_errorator::future<ceph::bufferptr> getxattr(
     const hobject_t& soid,
     std::string_view key) const;
 

ceph/ceph

src/crimson/os/futurized_store.h
@@ -47,9 +47,13 @@ public:
 				   uint64_t offset,
 				   size_t len,
 				   uint32_t op_flags = 0) = 0;
-  virtual crimson::errorator<crimson::ct_error::enoent,
-                             crimson::ct_error::enodata>::future<ceph::bufferptr>
-  get_attr(CollectionRef c, const ghobject_t& oid, std::string_view name) const = 0;
+  using get_attr_errorator = crimson::errorator<
+    crimson::ct_error::enoent,
+    crimson::ct_error::enodata>;
+  virtual get_attr_errorator::future<ceph::bufferptr> get_attr(
+    CollectionRef c,
+    const ghobject_t& oid,
+    std::string_view name) const = 0;
 
   using attrs_t = std::map<std::string, ceph::bufferptr, std::less<>>;
   virtual seastar::future<attrs_t> get_attrs(CollectionRef c,

src/crimson/osd/pg_backend.h
@@ -87,8 +87,8 @@ public:
   seastar::future<> getxattr(
     const ObjectState& os,
     OSDOp& osd_op) const;
-  crimson::errorator<crimson::ct_error::enoent,
-                     crimson::ct_error::enodata>::future<ceph::bufferptr> getxattr(
+  using get_attr_errorator = crimson::os::FuturizedStore::get_attr_errorator;
+  get_attr_errorator::future<ceph::bufferptr> getxattr(
     const hobject_t& soid,
     std::string_view key) const;
 