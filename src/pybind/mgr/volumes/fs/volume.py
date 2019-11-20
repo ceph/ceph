8233ea58da2289b462f552796f507016c10d7db4
Joshua Schmid <jschmid@suse.de>
2019-11-20 14:58:47 +0100

    mgr/volumes: move up 'confirm' validation
    Instead of checking if the --yes-i-really-mean-it
    flag was set _after_ removing the MDS daemon, we
    need to check if before starting any removal operation.
    
    Fixes: https://tracker.ceph.com/issues/42931
    Signed-off-by: Joshua Schmid <jschmid@suse.de>
    (cherry picked from commit c1db7f8b42f238f5875386a71d30bc34ae29171c)
    
     Conflicts:
    	src/pybind/mgr/volumes/fs/volume.py

--- src/pybind/mgr/volumes/fs/volume.py
+++ src/pybind/mgr/volumes/fs/volume.py
@@ -230,13 +230,7 @@ class VolumeClient(object):
                    'data': data_pool}
         return self.mgr.mon_command(command)
 
-    def remove_filesystem(self, fs_name, confirm):
-        if confirm != "--yes-i-really-mean-it":
-            return -errno.EPERM, "", "WARNING: this will *PERMANENTLY DESTROY* all data " \
-                "stored in the filesystem '{0}'. If you are *ABSOLUTELY CERTAIN* " \
-                "that is what you want, re-issue the command followed by " \
-                "--yes-i-really-mean-it.".format(fs_name)
-
+    def remove_filesystem(self, fs_name):
         command = {'prefix': 'fs fail', 'fs_name': fs_name}
         r, outb, outs = self.mgr.mon_command(command)
         if r != 0:
@@ -287,6 +281,15 @@ class VolumeClient(object):
         """
         delete the given module (tear down mds, remove filesystem)
         """
+        if self.stopping.isSet():
+            return -errno.ESHUTDOWN, "", "shutdown in progress"
+
+        if confirm != "--yes-i-really-mean-it":
+            return -errno.EPERM, "", "WARNING: this will *PERMANENTLY DESTROY* all data " \
+                "stored in the filesystem '{0}'. If you are *ABSOLUTELY CERTAIN* " \
+                "that is what you want, re-issue the command followed by " \
+                "--yes-i-really-mean-it.".format(volname)
+
         self.purge_queue.cancel_purge_job(volname)
         self.connection_pool.del_fs_handle(volname)
         # Tear down MDS daemons
@@ -305,7 +308,7 @@ class VolumeClient(object):
         # In case orchestrator didn't tear down MDS daemons cleanly, or
         # there was no orchestrator, we force the daemons down.
         if self.volume_exists(volname):
-            r, outb, outs = self.remove_filesystem(volname, confirm)
+            r, outb, outs = self.remove_filesystem(volname)
             if r != 0:
                 return r, outb, outs
         else:
