ceph/ceph

commit b4b8f54a0c8a9c5b8baf44ae1c3df5a33978de96

ceph-volume util.system allow skipping restorecon calls
Signed-off-by: Alfredo Deza <adeza@redhat.com>

src/ceph-volume/ceph_volume/util/system.py
@@ -275,7 +275,33 @@ def get_mounts(devices=False, paths=False, realpath=False):
         return paths_mounted
 
 
-def set_context(path, recursive = False):
+def set_context(path, recursive=False):
+    """
+    Calls ``restorecon`` to set the proper context on SELinux systems. Only if
+    the ``restorecon`` executable is found anywhere in the path it will get
+    called.
+
+    If the ``CEPH_VOLUME_SKIP_RESTORECON`` environment variable is set to
+    any of: "1", "true", "yes" the call will be skipped as well.
+
+    Finally, if SELinux is not enabled, or not available in the system,
+    ``restorecon`` will not be called. This is checked by calling out to the
+    ``selinuxenabled`` executable. If that tool is not installed or returns
+    a non-zero exit status then no further action is taken and this function
+    will return.
+    """
+    skip = os.environ.get('CEPH_VOLUME_SKIP_RESTORECON', '')
+    if skip.lower() in ['1', 'true', 'yes']:
+        logger.info(
+            'CEPH_VOLUME_SKIP_RESTORECON environ is set, will not call restorecon'
+        )
+        return
+
+    stdout, stderr, code = process.call(['selinuxenabled'], verbose_on_failure=False)
+    if code != 0:
+        logger.info('SELinux is not enabled, will not call restorecon')
+        return
+
     # restore selinux context to default policy values
     if which('restorecon').startswith('/'):
         if recursive:

commit a6a1c52833d2fdd4a92e5536336d1a6715fe7115

ceph-volume: don't assume SELinux
Expect selinux related binaries to not be present.

Fixes: b4b8f54a0c8a9c5b8baf44ae1c3df5a33978de96
Fixes: https://tracker.ceph.com/issues/42957

Signed-off-by: Jan Fajerski <jfajerski@suse.com>

src/ceph-volume/ceph_volume/util/system.py
@@ -297,7 +297,13 @@ def set_context(path, recursive=False):
         )
         return
 
-    stdout, stderr, code = process.call(['selinuxenabled'], verbose_on_failure=False)
+    try:
+        stdout, stderr, code = process.call(['selinuxenabled'],
+                                            verbose_on_failure=False)
+    except FileNotFoundError:
+        logger.info('No SELinux found, skipping call to restorecon')
+        return
+
     if code != 0:
         logger.info('SELinux is not enabled, will not call restorecon')
         return