d02afa652fdf7c5620863a3bf518f6aa23811e21
Radoslaw Zarzynski <rzarzyns@redhat.com>
2019-11-27 17:21:31 +0100

    crimson: introduce watchers to ObjectContext.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/crimson/osd/object_context.h
+++ src/crimson/osd/object_context.h
@@ -14,6 +14,7 @@
 #include "osd/object_state.h"
 #include "crimson/common/config_proxy.h"
 #include "crimson/osd/osd_operation.h"
+#include "crimson/osd/watch.h"
 
 namespace crimson::osd {
 
@@ -35,6 +36,12 @@ public:
   ObjectState obs;
   std::optional<SnapSet> ss;
   bool loaded : 1;
+  // the watch / notify machinery rather stays away from the hot and
+  // frequented paths. std::map is used mostly because of developer's
+  // convenience.
+  using watch_key_t = std::pair<uint64_t, entity_name_t>;
+  std::map<watch_key_t, crimson::osd::WatchRef> watchers;
+
   ObjectContext(const hobject_t &hoid) : obs(hoid), loaded(false) {}
 
   const hobject_t &get_oid() const {
