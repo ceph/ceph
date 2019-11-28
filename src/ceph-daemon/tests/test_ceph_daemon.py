45bae8219a0501a7cbd456e55102580635e9333b
Thomas Bechtold <tbechtold@suse.com>
2019-11-28 06:08:42 +0100

    ceph-daemon: Allow env var for setting the used image
    Instead of always adding "--image my-custom-image" when calling
    ceph-daemon with a non-standard image, allow to set the environment
    variable called CEPH_DAEMON_IMAGE which will adjust the --image
    default.
    That way, the command line arguments when using ceph-daemon with a
    custom image are a bit shorter.
    
    Signed-off-by: Thomas Bechtold <tbechtold@suse.com>

--- src/ceph-daemon/tests/test_ceph_daemon.py
+++ src/ceph-daemon/tests/test_ceph_daemon.py
@@ -1,5 +1,7 @@
 import imp
 import unittest
+import mock
+import os
 
 
 cd = imp.load_source("ceph-daemon", "ceph-daemon")
@@ -8,3 +10,14 @@ cd = imp.load_source("ceph-daemon", "ceph-daemon")
 class TestCephDaemon(unittest.TestCase):
     def test_is_fsid(self):
         self.assertFalse(cd.is_fsid('no-uuid'))
+
+    def test__get_parser_image(self):
+        p = cd._get_parser()
+        args = p.parse_args(['--image', 'foo', 'version'])
+        assert args.image == 'foo'
+
+    @mock.patch.dict(os.environ,{'CEPH_DAEMON_IMAGE':'bar'})
+    def test__get_parser_image_with_envvar(self):
+        p = cd._get_parser()
+        args = p.parse_args(['version'])
+        assert args.image == 'bar'
