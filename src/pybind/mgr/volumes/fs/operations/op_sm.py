294eed3d3f1442d17680562ad29b981fac9c6667
Venky Shankar <vshankar@redhat.com>
2019-12-02 01:27:55 -0500

    mgr/volumes: add operation state machine table
    ... and fetch creation state from state machine table.
    
    Signed-off-by: Venky Shankar <vshankar@redhat.com>

---
+++ src/pybind/mgr/volumes/fs/operations/op_sm.py
@@ -0,0 +1,50 @@
+from ..exception import OpSmException
+
+class OpSm(object):
+    INIT_STATE_KEY = 'init'
+
+    FAILED_STATE = 'failed'
+    FINAL_STATE  = 'complete'
+
+    OP_SM_SUBVOLUME = {
+        INIT_STATE_KEY : FINAL_STATE,
+    }
+
+    OP_SM_CLONE = {
+        INIT_STATE_KEY : 'pending',
+        'pending'           : ('in-progress', FAILED_STATE),
+        'in-progress'       : (FINAL_STATE, FAILED_STATE),
+    }
+
+    STATE_MACHINES_TYPES = {
+        "subvolume" : OP_SM_SUBVOLUME,
+        "clone"     : OP_SM_CLONE,
+    }
+
+    @staticmethod
+    def is_final_state(state):
+        return state == OpSm.FINAL_STATE
+
+    @staticmethod
+    def is_failed_state(state):
+        return state == OpSm.FAILED_STATE
+
+    @staticmethod
+    def get_init_state(stm_type):
+        stm = OpSm.STATE_MACHINES_TYPES.get(stm_type, None)
+        if not stm:
+            raise OpSmException(-errno.ENOENT, "state machine type '{0}' not found".format(stm_type))
+        init_state = stm.get(OpSm.INIT_STATE_KEY, None)
+        if not init_state:
+            raise OpSmException(-errno.ENOENT, "initial state unavailable for state machine '{0}'".format(stm_type))
+        return init_state
+
+    @staticmethod
+    def get_next_state(stm_type, current_state, ret):
+        stm = OpSm.STATE_MACHINES_TYPES.get(stm_type, None)
+        if not stm:
+            raise OpSmException(-errno.ENOENT, "state machine type '{0}' not found".format(stm_type))
+        next_state = stm.get(current_state, None)
+        if not next_state:
+            raise OpSmException(-errno.EINVAL, "invalid current state '{0}'".format(current_state))
+        return next_state[0] if ret == 0 else next_state[1]
--- src/pybind/mgr/volumes/fs/operations/versions/subvolume_v1.py
+++ src/pybind/mgr/volumes/fs/operations/versions/subvolume_v1.py
@@ -6,9 +6,10 @@ import logging
 import cephfs
 
 from .subvolume_base import SubvolumeBase
+from ..op_sm import OpSm
 from ..template import SubvolumeTemplate
 from ..snapshot_util import mksnap, rmsnap
-from ...exception import VolumeException, MetadataMgrException
+from ...exception import OpSmException, VolumeException, MetadataMgrException
 from ...fs_util import listdir
 
 log = logging.getLogger(__name__)
@@ -29,6 +30,12 @@ class SubvolumeV1(SubvolumeBase, SubvolumeTemplate):
             raise VolumeException(-errno.EINVAL, "error fetching subvolume metadata")
 
     def create(self, size, isolate_nspace, pool, mode, uid, gid):
+        subvolume_type = SubvolumeBase.SUBVOLUME_TYPE_NORMAL
+        try:
+            initial_state = OpSm.get_init_state(subvolume_type)
+        except OpSmException as oe:
+            raise VolumeException(-errno.EINVAL, "subvolume creation failed: internal error")
+
         subvol_path = os.path.join(self.base_path, str(uuid.uuid4()).encode('utf-8'))
         try:
             # create directory and set attributes
@@ -37,7 +44,7 @@ class SubvolumeV1(SubvolumeBase, SubvolumeTemplate):
 
             # persist subvolume metadata
             qpath = subvol_path.decode('utf-8')
-            self.init_config(SubvolumeV1.VERSION, "subvolume", qpath, "complete")
+            self.init_config(SubvolumeV1.VERSION, subvolume_type, qpath, initial_state)
         except (VolumeException, MetadataMgrException, cephfs.Error) as e:
             try:
                 log.info("cleaning up subvolume with path: {0}".format(self.subvolname))
