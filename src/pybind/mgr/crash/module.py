ceph/ceph

commit 9ba60f09bb7c4cb2ef27d5ead40c1fed8bfabb4a

mgr/crash: make 'crash ls' a nice table with a NEW column
Signed-off-by: Sage Weil <sage@redhat.com>

src/pybind/mgr/crash/module.py
@@ -4,6 +4,7 @@ import errno
 import json
 import six
 from collections import defaultdict
+from prettytable import PrettyTable
 
 
 DATEFMT = '%Y-%m-%dT%H:%M:%S.%f'
@@ -83,12 +84,23 @@ class Module(MgrModule):
         return 0, '', ''
 
     def do_ls(self, cmd, inbuf):
-        keys = []
+        r = []
         for k, meta in self.timestamp_filter(lambda ts: True):
-            entity_name = meta.get('entity_name', 'unknown')
-            keys.append("%s %s" % (k.replace('crash/', ''), entity_name))
-        keys.sort()
-        return 0, '\n'.join(keys), ''
+            r.append(meta)
+        if cmd.get('format') == 'json' or cmd.get('format') == 'json-pretty':
+            return 0, json.dumps(r, indent=4), ''
+        else:
+            table = PrettyTable(['ID', 'ENTITY', 'NEW'],
+                                border=False)
+            table.left_padding_width = 0
+            table.right_padding_width = 1
+            table.align['ID'] = 'l'
+            table.align['ENTITY'] = 'l'
+            for c in r:
+                table.add_row([c.get('crash_id'),
+                               c.get('entity_name','unknown'),
+                               '' if 'archived' in c else '*'])
+            return 0, table.get_string(), ''
 
     def do_rm(self, cmd, inbuf):
         crashid = cmd['id']

qa/workunits/rados/test_crash.sh
@@ -29,5 +29,5 @@ sudo systemctl restart ceph-crash
 sleep 30
 
 # must be 3 crashdumps registered and moved to crash/posted
-[ $(ceph crash ls | wc -l) = 3 ]  || exit 1
+[ $(ceph crash ls | wc -l) = 4 ]  || exit 1   # 4 here bc of the table header
 [ $(sudo find /var/lib/ceph/crash/posted/ -name meta | wc -l) = 3 ] || exit 1

commit a2e5f01cceb7b6396643a59d1244018edf4f6ead

mgr/crash: fix 'crash ls[-new]' sorting
Broken by 9ba60f09bb7c4cb2ef27d5ead40c1fed8bfabb4a

Signed-off-by: Sage Weil <sage@redhat.com>

src/pybind/mgr/crash/module.py
@@ -200,10 +200,11 @@ class Module(MgrModule):
 
     def do_ls(self, cmd, inbuf):
         if cmd['prefix'] == 'crash ls':
-            r = self.crashes.values()
+            t = self.crashes.values()
         else:
-            r = [crash for crashid, crash in self.crashes.items()
+            t = [crash for crashid, crash in self.crashes.items()
                  if 'archived' not in crash]
+        r = sorted(t, key=lambda i: i.get('crash_id'))
         if cmd.get('format') == 'json' or cmd.get('format') == 'json-pretty':
             return 0, json.dumps(r, indent=4), ''
         else: