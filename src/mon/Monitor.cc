2130a6f41063c48a1c5560b5152cb36dd2595105
Brad Hubbard <bhubbard@redhat.com>
2019-12-03 16:54:03 +1000

    mon: Don't put session during feature change
    The session gets put as result of the set_session call in the next
    block.
    
    Fixes: https://tracker.ceph.com/issues/38345
    
    Signed-off-by: Brad Hubbard <bhubbard@redhat.com>

--- src/mon/Monitor.cc
+++ src/mon/Monitor.cc
@@ -4304,7 +4304,6 @@ void Monitor::_ms_dispatch(Message *m)
         // exist only while the op is being handled.
         remove_session(s);
       }
-      s->put();
       s = nullptr;
     }
   }
