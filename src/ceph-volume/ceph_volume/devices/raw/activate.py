1383e930a0274e1ca3dca5806695ddfab3acd8e5
Sage Weil <sage@redhat.com>
2019-12-06 14:08:12 -0600

    ceph-volume: add raw mode
    list, prepare, activate, create (prepare+activate) basic bluestore OSDs
    on a single device.
    
    No support (or plans to ever support) dmcrypt.
    
    I think support for db/wal could be added later without much trouble.
    
    For now, we require --no-systemd and --bluestore flags because we haven't
    implemented support for anything else but suspect we will in the future,
    and would like to ensure users are being explicit about what they're
    asking for.
    
    Signed-off-by: Sage Weil <sage@redhat.com>

---
+++ src/ceph-volume/ceph_volume/devices/raw/activate.py
@@ -0,0 +1,151 @@
+from __future__ import print_function
+import argparse
+import logging
+import os
+from textwrap import dedent
+from ceph_volume import process, conf, decorators, terminal, __release__, configuration
+from ceph_volume.util import system, disk
+from ceph_volume.util import prepare as prepare_utils
+from ceph_volume.util import encryption as encryption_utils
+from ceph_volume.systemd import systemctl
+from ceph_volume.api import lvm as api
+from .list import direct_report
+
+
+logger = logging.getLogger(__name__)
+
+def activate_bluestore(meta, tmpfs, systemd):
+    # find the osd
+    osd_id = meta['osd_id']
+    osd_fsid = meta['osd_uuid']
+
+    # mount on tmpfs the osd directory
+    osd_path = '/var/lib/ceph/osd/%s-%s' % (conf.cluster, osd_id)
+    if not system.path_is_mounted(osd_path):
+        # mkdir -p and mount as tmpfs
+        prepare_utils.create_osd_path(osd_id, tmpfs=tmpfs)
+
+    # XXX This needs to be removed once ceph-bluestore-tool can deal with
+    # symlinks that exist in the osd dir
+    for link_name in ['block', 'block.db', 'block.wal']:
+        link_path = os.path.join(osd_path, link_name)
+        if os.path.exists(link_path):
+            os.unlink(os.path.join(osd_path, link_name))
+
+    # Once symlinks are removed, the osd dir can be 'primed again. chown first,
+    # regardless of what currently exists so that ``prime-osd-dir`` can succeed
+    # even if permissions are somehow messed up
+    system.chown(osd_path)
+    prime_command = [
+        'ceph-bluestore-tool',
+        'prime-osd-dir', '--dev', meta['device'],
+        '--path', osd_path,
+        '--no-mon-config']
+    process.run(prime_command)
+
+    # always re-do the symlink regardless if it exists, so that the block,
+    # block.wal, and block.db devices that may have changed can be mapped
+    # correctly every time
+    process.run(['ln', '-snf', meta['device'], os.path.join(osd_path, 'block')])
+    system.chown(os.path.join(osd_path, 'block'))
+    system.chown(osd_path)
+
+#    if systemd:
+        # write me
+        # enable the OSD
+        #systemctl.enable_osd(osd_id)
+
+        # start the OSD
+        #systemctl.start_osd(osd_id)
+
+    terminal.success("ceph-volume raw activate successful for osd ID: %s" % osd_id)
+
+
+class Activate(object):
+
+    help = 'Discover and prepare a data directory for a (BlueStore) OSD on a raw device'
+
+    def __init__(self, argv):
+        self.argv = argv
+        self.args = None
+
+    @decorators.needs_root
+    def activate(self, devices=None, only_osd_id=None, only_osd_fsid=None):
+        """
+        :param args: The parsed arguments coming from the CLI
+        """
+        if devices:
+            found = direct_report(devices)
+        else:
+            found = direct_report(None)
+
+        for osd_id, meta in found.items():
+            if only_osd_id and only_osd_fsid and \
+               (only_osd_id != osd_id or only_osd_fsid != meta['osd_uuid']):
+                logger.debug('ignoring osd.%d uuid %s cluster %s' % (
+                    osd_id, meta['osd_uuid'], meta['ceph_fsid']))
+                continue
+
+            logger.info('Activating osd.%s uuid %s cluster %s' % (
+                    osd_id, meta['osd_uuid'], meta['ceph_fsid']))
+            activate_bluestore(meta,
+                               tmpfs=not self.args.no_tmpfs,
+                               systemd=not self.args.no_systemd)
+
+    def main(self):
+        sub_command_help = dedent("""
+        Activate (BlueStore) OSD on a raw block device based on the 
+        device label (normally the first block of the device).
+
+            ceph-volume raw activate --device /dev/sdb
+            ceph-volume raw activate --osd-id 1 --osd-uuid f0327efd-c28e-40bb-9199-f2e61e54c12a
+
+        The device(s) associated with the OSD needs to have been prepared
+        previously, so that all needed tags and metadata exist.
+        """)
+        parser = argparse.ArgumentParser(
+            prog='ceph-volume raw activate',
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            description=sub_command_help,
+        )
+
+        parser.add_argument(
+            '--osd-id',
+            help='The ID of the OSD, usually an integer, like 0'
+        )
+        parser.add_argument(
+            '--osd-uuid',
+            help='The UUID of the OSD'
+        )
+        parser.add_argument(
+            '--device',
+            nargs='*',
+            help='The device(s) for the OSD to start')
+        parser.add_argument(
+            '--all',
+            dest='activate_all',
+            action='store_true',
+            help='Activate all OSDs found in the system',
+        )
+        parser.add_argument(
+            '--no-systemd',
+            dest='no_systemd',
+            action='store_true',
+            help='Skip creating and enabling systemd units and starting OSD services',
+        )
+        parser.add_argument(
+            '--no-tmpfs',
+            action='store_true',
+            help='Do not use a tmpfs mount for OSD data dir')
+        if len(self.argv) == 0:
+            print(sub_command_help)
+            return
+        args = parser.parse_args(self.argv)
+        self.args = args
+        if not args.device and not args.activate_all and not (args.osd_id and args.osd_uuid):
+            terminal.error('must specify one of --device, --activate-all, or --osd-id and --osd-uuid')
+            raise SystemExit(1)
+        if not args.no_systemd:
+            terminal.error('systemd support not yet implemented')
+            raise SystemExit(1)
+        self.activate(args.device, args.osd_id, args.osd_fsid, not args.no_tmpfs)
---
+++ src/ceph-volume/ceph_volume/devices/raw/list.py
@@ -0,0 +1,118 @@
+from __future__ import print_function
+import argparse
+import base64
+import json
+import logging
+import os
+from textwrap import dedent
+from ceph_volume import decorators, terminal, conf, process
+from ceph_volume.api import lvm
+from ceph_volume.systemd import systemctl
+from ceph_volume.util import arg_validators, system, disk, encryption
+from ceph_volume.util.device import Device
+
+
+logger = logging.getLogger(__name__)
+
+def direct_report(devices):
+    """
+    Other non-cli consumers of listing information will want to consume the
+    report without the need to parse arguments or other flags. This helper
+    bypasses the need to deal with the class interface which is meant for cli
+    handling.
+    """
+    _list = List([])
+    return _list.generate(devices)
+
+
+class List(object):
+
+    help = 'list BlueStore OSDs on raw devices'
+
+    def __init__(self, argv):
+        self.argv = argv
+
+    def generate(self, devs=None):
+        if not devs:
+            logger.debug('Listing block devices via lsblk...')
+            devs = []
+            out, err, ret = process.call([
+                'lsblk', '--paths', '--nodeps', '--output=NAME', '--noheadings'
+            ])
+            assert not ret
+            r = json.loads(''.join(out))
+            for dev in r.get('blockdevices', []):
+                devs.append('/dev/' + dev['name'])
+        result = {}
+        for dev in devs:
+            logger.debug('Examining %s' % dev)
+            # bluestore?
+            out, err, ret = process.call([
+                'ceph-bluestore-tool', 'show-label',
+                '--dev', dev])
+            if ret:
+                logger.debug('No label on %s' % dev)
+                continue
+            oj = json.loads(''.join(out))
+            if dev not in oj:
+                continue
+            if oj[dev]['description'] != 'main':
+                # ignore non-main devices, for now
+                continue
+            whoami = oj[dev]['whoami']
+            result[whoami] = {
+                'type': 'bluestore',
+                'osd_id': int(whoami),
+            }
+            for f in ['osd_uuid', 'ceph_fsid']:
+                result[whoami][f] = oj[dev][f]
+            result[whoami]['device'] = dev
+        return result
+
+    @decorators.needs_root
+    def list(self, args):
+        report = self.generate(args.device)
+        if args.format == 'json':
+            print(json.dumps(report, indent=4, sort_keys=True))
+        else:
+            if not report:
+                raise SystemExit('No valid Ceph devices found')
+            raise RuntimeError('not implemented yet')
+
+    def main(self):
+        sub_command_help = dedent("""
+        List OSDs on raw devices with raw device labels (usually the first
+        block of the device).
+
+        Full listing of all identifiable (currently, BlueStore) OSDs
+        on raw devices:
+
+            ceph-volume raw list
+
+        List a particular device, reporting all metadata about it::
+
+            ceph-volume raw list /dev/sda1
+
+        """)
+        parser = argparse.ArgumentParser(
+            prog='ceph-volume raw list',
+            formatter_class=argparse.RawDescriptionHelpFormatter,
+            description=sub_command_help,
+        )
+
+        parser.add_argument(
+            'device',
+            metavar='DEVICE',
+            nargs='*',
+            help='Path to a device like /dev/sda1'
+        )
+
+        parser.add_argument(
+            '--format',
+            help='output format, defaults to "pretty"',
+            default='json',
+            choices=['json', 'pretty'],
+        )
+
+        args = parser.parse_args(self.argv)
+        self.list(args)
