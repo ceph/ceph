73e6265e8afac96dbc690f0343a418ca301d4495
yangjun <yangjun@cmss.chinamobile.com>
2019-12-08 09:09:53 +0800

    librbd: fix rbd_open_by_id, rbd_open_by_id_read_only
    These methods incorrectly delete ImageCtx on error, resulting
    in double-free heap corruption.
    
    Fixes: https://tracker.ceph.com/issues/43178
    Signed-off-by: yangjun <yangjun@cmss.chinamobile.com>
    (cherry picked from commit 3457192c24a66ba499a7c9b1747bc29c79b34636)

--- src/librbd/librbd.cc
+++ src/librbd/librbd.cc
@@ -3973,9 +3973,7 @@ extern "C" int rbd_open_by_id(rados_ioctx_t p, const char *id,
              ictx->id.c_str(), ictx->snap_name.c_str(), ictx->read_only);
 
   int r = ictx->state->open(0);
-  if (r < 0) {
-    delete ictx;
-  } else {
+  if (r >= 0) {
     *image = (rbd_image_t)ictx;
   }
   tracepoint(librbd, open_image_exit, r);
@@ -4048,9 +4046,7 @@ extern "C" int rbd_open_by_id_read_only(rados_ioctx_t p, const char *id,
              ictx->id.c_str(), ictx->snap_name.c_str(), ictx->read_only);
 
   int r = ictx->state->open(0);
-  if (r < 0) {
-    delete ictx;
-  } else {
+  if (r >= 0) {
     *image = (rbd_image_t)ictx;
   }
   tracepoint(librbd, open_image_exit, r);
