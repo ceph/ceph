sunnyku/ceph-ci

mgr/telemetry: fix 'telemetry {on,off}'
Signed-off-by: Sage Weil <sage@redhat.com>

src/pybind/mgr/telemetry/module.py
@@ -272,10 +272,10 @@ class Module(MgrModule):
                 r[opt['name']] = getattr(self, opt['name'])
             return 0, json.dumps(r, indent=4), ''
         elif command['prefix'] == 'telemetry on':
-            self.set_config('active', True)
+            self.set_module_option('active', True)
             return 0, '', ''
         elif command['prefix'] == 'telemetry off':
-            self.set_config('active', False)
+            self.set_module_option('active', False)
             return 0, '', ''
         elif command['prefix'] == 'telemetry send':
             self.last_report = self.compile_report()

ceph/ceph

(cherry picked from commit 497e00c4dadd9e29a792413a425671c061fa44c6)

src/pybind/mgr/telemetry/module.py
@@ -315,10 +315,10 @@ class Module(MgrModule):
             self.set_config(key, value)
             return 0, 'Configuration option {0} updated'.format(key), ''
         elif command['prefix'] == 'telemetry on':
-            self.set_config('active', True)
+            self.set_config('enabled', True)
             return 0, '', ''
         elif command['prefix'] == 'telemetry off':
-            self.set_config('active', False)
+            self.set_config('enabled', False)
             return 0, '', ''
         elif command['prefix'] == 'telemetry send':
             self.last_report = self.compile_report()