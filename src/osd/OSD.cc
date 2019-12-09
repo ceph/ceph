11811e612efdcb1f9138b8b22e0e580d53390324
Sage Weil <sage@redhat.com>
2019-12-09 13:48:20 +0100

    nautilus: osd: set collection pool opts on collection create, pg load
    We need to tell the ObjectStore's Collections what the pool options are
    for things like the bluestore compression mode and so on to take effect.
    
    Apply these
    
    - when we create new collections, due to a new pg or a split
    - when we start up and open an existing collection
    
    We already apply these changes when there is a pool change.
    
    (This is a manual backport of 512d89af9ac0d19df44dedc67fe349022c656907).
    
    Fixes: https://tracker.ceph.com/issues/40483
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -4757,6 +4757,8 @@ PGRef OSD::handle_pg_create_info(const OSDMapRef& osdmap,
     false,
     rctx.transaction);
 
+  pg->init_collection_pool_opts();
+
   if (pg->is_primary()) {
     Mutex::Locker locker(m_perf_queries_lock);
     pg->set_dynamic_perf_stats_queries(m_perf_queries);
@@ -9405,6 +9407,8 @@ void OSD::split_pgs(
       i->pgid,
       child,
       split_bits);
+
+    child->init_collection_pool_opts();
 
     child->finish_split_stats(*stat_iter, rctx->transaction);
     child->unlock();
