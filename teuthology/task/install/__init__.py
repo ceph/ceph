c10378318712c92c3c2d9ba96607248034dcf275
shangdehao1 <dehao.shang@intel.com>
2019-12-14 14:28:43 +0800

    task/install: fix UnboundLocalError of local variable repos
    If overrides is None, repos will don't be assignmented.
    In this case, will occur error "UnboundLocalError: local variable
    repos referenced before assignment"
    
    Signed-off-by: dehao.shang@intel.com

--- teuthology/task/install/__init__.py
+++ teuthology/task/install/__init__.py
@@ -541,6 +541,7 @@ def task(ctx, config):
     project, = config.get('project', 'ceph'),
     log.debug('project %s' % project)
     overrides = ctx.config.get('overrides')
+    repos = None
     if overrides:
         install_overrides = overrides.get('install', {})
         teuthology.deep_merge(config, install_overrides.get(project, {}))
