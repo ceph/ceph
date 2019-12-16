105307f1bb4b3f05575cfde912ce1ba4c7eb5f37
Kyr Shatskyy <kyrylo.shatskyy@suse.com>
2019-12-16 02:20:11 +0100

    qa/tasks/cephfs: get rid of StringIO for py3
    Use io.BytesIO and six.ensure_str for py3 compatibility
    
    Signed-off-by: Kyr Shatskyy <kyrylo.shatskyy@suse.com>
    (cherry picked from commit 9f6c764f10f99cbf5ca164c2681b001d29505d9d)

--- qa/tasks/cephfs/mount.py
+++ qa/tasks/cephfs/mount.py
@@ -1,4 +1,5 @@
 from contextlib import contextmanager
+from io import BytesIO
 import json
 import logging
 import datetime
@@ -190,7 +191,7 @@ class CephFSMount(object):
         return self.client_remote.run(
                args=['sudo', 'adjust-ulimits', 'daemon-helper', 'kill',
                      py_version, '-c', pyscript], wait=False, stdin=run.PIPE,
-               stdout=StringIO())
+               stdout=BytesIO())
 
     def run_python(self, pyscript, py_version='python3'):
         p = self._run_python(pyscript, py_version)
@@ -203,8 +204,8 @@ class CephFSMount(object):
             args = args.split()
 
         args = ["cd", self.mountpoint, run.Raw('&&'), "sudo"] + args
-        return self.client_remote.run(args=args, stdout=StringIO(),
-                                      stderr=StringIO(), wait=wait,
+        return self.client_remote.run(args=args, stdout=BytesIO(),
+                                      stderr=BytesIO(), wait=wait,
                                       stdin=stdin, check_status=check_status,
                                       omit_sudo=omit_sudo)
 
