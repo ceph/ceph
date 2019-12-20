38d88fef38a3b2f37f6166df106f3bb2b7512cbd
Sebastian Wagner <sebastian.wagner@suse.com>
2019-12-20 13:10:05 +0100

    mgr/cephadm: Add progress capability to completions
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- src/pybind/mgr/cephadm/tests/test_cephadm.py
+++ src/pybind/mgr/cephadm/tests/test_cephadm.py
@@ -9,11 +9,10 @@ try:
 except ImportError:
     pass
 
-from orchestrator import ServiceDescription, raise_if_exception, Completion, InventoryNode, \
-    StatelessServiceSpec, PlacementSpec, RGWSpec, parse_host_specs, StatefulServiceSpec
-from ..module import CephadmOrchestrator
+from orchestrator import ServiceDescription, InventoryNode, \
+    StatelessServiceSpec, PlacementSpec, RGWSpec, StatefulServiceSpec
 from tests import mock
-from .fixtures import cephadm_module
+from .fixtures import cephadm_module, wait
 
 
 """
@@ -35,35 +34,12 @@ def mon_command(*args, **kwargs):
 
 
 class TestCephadm(object):
-    def _wait(self, m, c):
-        # type: (CephadmOrchestrator, Completion) -> Any
-        m.process([c])
-
-        try:
-            import pydevd  # if in debugger
-            while True:    # don't timeout
-                if c.is_finished:
-                    raise_if_exception(c)
-                    return c.result
-                time.sleep(0.1)
-        except ImportError:  # not in debugger
-            for i in range(30):
-                if i % 10 == 0:
-                    m.process([c])
-                if c.is_finished:
-                    raise_if_exception(c)
-                    return c.result
-                time.sleep(0.1)
-        assert False, "timeout" + str(c._state)
-
-        m.process([c])
-        assert False, "timeout" + str(c._state)
 
     @contextmanager
     def _with_host(self, m, name):
-        self._wait(m, m.add_host(name))
+        wait(m, m.add_host(name))
         yield
-        self._wait(m, m.remove_host(name))
+        wait(m, m.remove_host(name))
 
     def test_get_unique_name(self, cephadm_module):
         existing = [
@@ -75,21 +51,21 @@ class TestCephadm(object):
 
     def test_host(self, cephadm_module):
         with self._with_host(cephadm_module, 'test'):
-            assert self._wait(cephadm_module, cephadm_module.get_hosts()) == [InventoryNode('test')]
+            assert wait(cephadm_module, cephadm_module.get_hosts()) == [InventoryNode('test')]
         c = cephadm_module.get_hosts()
-        assert self._wait(cephadm_module, c) == []
+        assert wait(cephadm_module, c) == []
 
     @mock.patch("cephadm.module.CephadmOrchestrator._run_cephadm", _run_cephadm('[]'))
     def test_service_ls(self, cephadm_module):
         with self._with_host(cephadm_module, 'test'):
             c = cephadm_module.describe_service()
-            assert self._wait(cephadm_module, c) == []
+            assert wait(cephadm_module, c) == []
 
     @mock.patch("cephadm.module.CephadmOrchestrator._run_cephadm", _run_cephadm('[]'))
     def test_device_ls(self, cephadm_module):
         with self._with_host(cephadm_module, 'test'):
             c = cephadm_module.get_inventory()
-            assert self._wait(cephadm_module, c) == [InventoryNode('test')]
+            assert wait(cephadm_module, c) == [InventoryNode('test')]
 
     @mock.patch("cephadm.module.CephadmOrchestrator._run_cephadm", _run_cephadm(
         json.dumps([
@@ -111,11 +87,11 @@ class TestCephadm(object):
         cephadm_module.service_cache_timeout = 10
         with self._with_host(cephadm_module, 'test'):
             c = cephadm_module.service_action('redeploy', 'rgw', service_id='myrgw.foobar')
-            assert self._wait(cephadm_module, c) == ["Deployed rgw.myrgw.foobar on host 'test'"]
+            assert wait(cephadm_module, c) == ["Deployed rgw.myrgw.foobar on host 'test'"]
 
             for what in ('start', 'stop', 'restart'):
                 c = cephadm_module.service_action(what, 'rgw', service_id='myrgw.foobar')
-                assert self._wait(cephadm_module, c) == [what + " rgw.myrgw.foobar from host 'test'"]
+                assert wait(cephadm_module, c) == [what + " rgw.myrgw.foobar from host 'test'"]
 
 
     @mock.patch("cephadm.module.CephadmOrchestrator._run_cephadm", _run_cephadm('[]'))
@@ -126,7 +102,7 @@ class TestCephadm(object):
         with self._with_host(cephadm_module, 'test'):
             ps = PlacementSpec(hosts=['test:0.0.0.0=a'], count=1)
             c = cephadm_module.update_mons(StatefulServiceSpec(placement=ps))
-            assert self._wait(cephadm_module, c) == ["Deployed mon.a on host 'test'"]
+            assert wait(cephadm_module, c) == ["Deployed mon.a on host 'test'"]
 
     @mock.patch("cephadm.module.CephadmOrchestrator._run_cephadm", _run_cephadm('[]'))
     @mock.patch("cephadm.module.CephadmOrchestrator.send_command")
@@ -136,7 +112,7 @@ class TestCephadm(object):
         with self._with_host(cephadm_module, 'test'):
             ps = PlacementSpec(hosts=['test:0.0.0.0=a'], count=1)
             c = cephadm_module.update_mgrs(StatefulServiceSpec(placement=ps))
-            [out] = self._wait(cephadm_module, c)
+            [out] = wait(cephadm_module, c)
             assert "Deployed mgr." in out
             assert " on host 'test'" in out
 
@@ -148,7 +124,7 @@ class TestCephadm(object):
         with self._with_host(cephadm_module, 'test'):
             dg = DriveGroupSpec('test', DeviceSelection(paths=['']))
             c = cephadm_module.create_osds(dg)
-            assert self._wait(cephadm_module, c) == "Created osd(s) on host 'test'"
+            assert wait(cephadm_module, c) == "Created osd(s) on host 'test'"
 
     @mock.patch("cephadm.module.CephadmOrchestrator._run_cephadm", _run_cephadm(
         json.dumps([
@@ -166,7 +142,7 @@ class TestCephadm(object):
         cephadm_module._cluster_fsid = "fsid"
         with self._with_host(cephadm_module, 'test'):
             c = cephadm_module.remove_osds(['0'])
-            out = self._wait(cephadm_module, c)
+            out = wait(cephadm_module, c)
             assert out == ["Removed osd.0 from host 'test'"]
 
     @mock.patch("cephadm.module.CephadmOrchestrator._run_cephadm", _run_cephadm('{}'))
@@ -177,7 +153,7 @@ class TestCephadm(object):
         with self._with_host(cephadm_module, 'test'):
             ps = PlacementSpec(hosts=['test'], count=1)
             c = cephadm_module.add_mds(StatelessServiceSpec('name', placement=ps))
-            [out] = self._wait(cephadm_module, c)
+            [out] = wait(cephadm_module, c)
             assert "Deployed mds.name." in out
             assert " on host 'test'" in out
 
@@ -189,7 +165,7 @@ class TestCephadm(object):
         with self._with_host(cephadm_module, 'test'):
             ps = PlacementSpec(hosts=['test'], count=1)
             c = cephadm_module.add_rgw(RGWSpec('realm', 'zone', placement=ps))
-            [out] = self._wait(cephadm_module, c)
+            [out] = wait(cephadm_module, c)
             assert "Deployed rgw.realm.zone." in out
             assert " on host 'test'" in out
 
@@ -209,7 +185,7 @@ class TestCephadm(object):
         cephadm_module._cluster_fsid = "fsid"
         with self._with_host(cephadm_module, 'test'):
             c = cephadm_module.remove_rgw('myrgw')
-            out = self._wait(cephadm_module, c)
+            out = wait(cephadm_module, c)
             assert out == ["Removed rgw.myrgw.foobar from host 'test'"]
 
     @mock.patch("cephadm.module.CephadmOrchestrator._run_cephadm", _run_cephadm('{}'))
@@ -220,7 +196,7 @@ class TestCephadm(object):
         with self._with_host(cephadm_module, 'test'):
             ps = PlacementSpec(hosts=['test'], count=1)
             c = cephadm_module.add_rbd_mirror(StatelessServiceSpec(name='name', placement=ps))
-            [out] = self._wait(cephadm_module, c)
+            [out] = wait(cephadm_module, c)
             assert "Deployed rbd-mirror." in out
             assert " on host 'test'" in out
 
@@ -231,5 +207,5 @@ class TestCephadm(object):
     def test_blink_device_light(self, _send_command, _get_connection, cephadm_module):
         with self._with_host(cephadm_module, 'test'):
             c = cephadm_module.blink_device_light('ident', True, [('test', '', '')])
-            assert self._wait(cephadm_module, c) == ['Set ident light for test: on']
+            assert wait(cephadm_module, c) == ['Set ident light for test: on']
 
