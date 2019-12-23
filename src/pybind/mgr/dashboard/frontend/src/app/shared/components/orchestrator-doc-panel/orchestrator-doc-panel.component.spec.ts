610c5b3fa6798ac5cc2147d810cf05f764e4a5e8
Kiefer Chang <kiefer.chang@suse.com>
2019-12-23 16:03:10 +0800

    mgr/dashboard: Add Orchestrator doc components
    Create two components for redirecting to Orchestrator Documents:
    - cd-orchestrator-doc-panel: For displaying an information panel
    - cd-orchestrator-doc-modal: For displaying an modal contains the
    information panel
    
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

---
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/orchestrator-doc-panel/orchestrator-doc-panel.component.spec.ts
@@ -0,0 +1,29 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { configureTestBed, i18nProviders } from '../../../../testing/unit-test-helper';
+import { CephReleaseNamePipe } from '../../pipes/ceph-release-name.pipe';
+import { SummaryService } from '../../services/summary.service';
+import { ComponentsModule } from '../components.module';
+import { OrchestratorDocPanelComponent } from './orchestrator-doc-panel.component';
+
+describe('OrchestratorDocPanelComponent', () => {
+  let component: OrchestratorDocPanelComponent;
+  let fixture: ComponentFixture<OrchestratorDocPanelComponent>;
+
+  configureTestBed({
+    imports: [ComponentsModule, HttpClientTestingModule, RouterTestingModule],
+    providers: [CephReleaseNamePipe, SummaryService, i18nProviders]
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OrchestratorDocPanelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
