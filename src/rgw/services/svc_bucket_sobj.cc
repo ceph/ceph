c1da1259741f66211d877af423af712f51c1481a
Kefu Chai <kchai@redhat.com>
2019-12-27 11:33:37 +0800

    rgw: svc.bucket: move _orig_info out of "if" block
    so `*orig_info` is valid out of the `if` block if it's set to
    `&_orig_info`.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/rgw/services/svc_bucket_sobj.cc
+++ src/rgw/services/svc_bucket_sobj.cc
@@ -488,18 +488,16 @@ int RGWSI_Bucket_SObj::store_bucket_instance_info(RGWSI_Bucket_BI_Ctx& ctx,
   /*
    * we might need some special handling if overwriting
    */
-
+  RGWBucketInfo shared_bucket_info;
   if (!orig_info && !exclusive) {  /* if exclusive, we're going to fail when try
                                       to overwrite, so the whole check here is moot */
-    /* we're here because orig_info wasn't passed in */
-    RGWBucketInfo _orig_info;
-
     /*
+     * we're here because orig_info wasn't passed in
      * we don't have info about what was there before, so need to fetch first
      */
     int r  = read_bucket_instance_info(ctx,
                                        key,
-                                       &_orig_info,
+                                       &shared_bucket_info,
                                        nullptr, nullptr,
                                        y,
                                        nullptr, boost::none);
@@ -509,7 +507,7 @@ int RGWSI_Bucket_SObj::store_bucket_instance_info(RGWSI_Bucket_BI_Ctx& ctx,
         return r;
       }
     } else {
-      *orig_info = &_orig_info;
+      *orig_info = &shared_bucket_info;
     }
   }
 
