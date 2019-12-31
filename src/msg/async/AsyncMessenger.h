ceph/ceph

commit db89e165776d38c352539d4c97bbbff41037fca6

msg/async: Don't dec(msgr_active_connections) if conn still in accepting_conns.
I found this bug during test:
"AsyncMessenger::Worker-0": {
        "msgr_active_connections": 18446744073709313353,
    },

This because before AsyncMessenger::accept_conn, the conn met fault and
stop this conn. In unregister_conn, it will
dec(msgr_active_connections). But the conn it don't inc(msgr_active_connections).
Only erase from accepting_conns, it will inc(msgr_active_connections).

Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

src/msg/async/AsyncMessenger.h
@@ -403,7 +403,8 @@ public:
    */
   void unregister_conn(const AsyncConnectionRef& conn) {
     std::lock_guard l{deleted_lock};
-    conn->get_perf_counter()->dec(l_msgr_active_connections);
+    if (!accepting_conns.count(conn))
+      conn->get_perf_counter()->dec(l_msgr_active_connections);
     deleted_conns.emplace(std::move(conn));
 
     if (deleted_conns.size() >= ReapDeadConnectionThreshold) {

commit 64be2f2509b8ea3cfee96a55cce96507ce44b1d1

msg/async: dec(l_msgr_active_connections) when unregister anonymous connection
When do all in one test,  l_msgr_active_connection don't decrease when
client shutdown.
This bug introduced by db89e165776d3.

Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

src/msg/async/AsyncMessenger.h
@@ -397,7 +397,7 @@ public:
    */
   void unregister_conn(const AsyncConnectionRef& conn) {
     std::lock_guard l{deleted_lock};
-    if (!accepting_conns.count(conn))
+    if (!accepting_conns.count(conn) || anon_conns.count(conn))
       conn->get_perf_counter()->dec(l_msgr_active_connections);
     deleted_conns.emplace(std::move(conn));
     conn->unregister();