9507c196333e0eccc3bd61736f2fe650430c921f
Kefu Chai <kchai@redhat.com>
2020-01-05 22:17:42 +0800

    crimson: do not use auto in param list of function prototype
    src/crimson/net/Socket.cc:114:57: error: 'auto' not allowed in function
    prototype
    static inline seastar::future<> close_and_handle_errors(auto& out) {
                                                            ^~~~
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/crimson/net/Socket.cc
+++ src/crimson/net/Socket.cc
@@ -111,7 +111,9 @@ void Socket::shutdown() {
   socket.shutdown_output();
 }
 
-static inline seastar::future<> close_and_handle_errors(auto& out) {
+static inline seastar::future<>
+close_and_handle_errors(seastar::output_stream<char>& out)
+{
   return out.close().handle_exception_type([] (const std::system_error& e) {
     if (e.code() != error::broken_pipe &&
         e.code() != error::connection_reset) {
