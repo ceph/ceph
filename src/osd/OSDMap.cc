ceph/ceph

commit 180e3bbe2e319d4e5407ff0656dc5b0decfb8a4f

osd/OSDMap.cc: add more info in json output of osd stat
Fixes: https://tracker.ceph.com/issues/41718

Signed-off-by: Shen Hang <harryshen18@gmail.com>

src/osd/OSDMap.cc
@@ -3969,7 +3969,11 @@ void OSDMap::print_summary(Formatter *f, ostream& out,
     f->dump_int("epoch", get_epoch());
     f->dump_int("num_osds", get_num_osds());
     f->dump_int("num_up_osds", get_num_up_osds());
+    f->dump_int("osd_up_since", last_up_change.to_msec() / 1000);
     f->dump_int("num_in_osds", get_num_in_osds());
+    f->dump_int("osd_in_since", last_in_change.to_msec() / 1000);
+    f->dump_bool("full", test_flag(CEPH_OSDMAP_FULL));
+    f->dump_bool("nearfull", test_flag(CEPH_OSDMAP_NEARFULL));
     f->dump_unsigned("num_remapped_pgs", get_num_pg_temp());
   } else {
     utime_t now = ceph_clock_now();

commit 9f158635b44e4c9f12ea9bc82aafab18190c55da

osd: Again remove deprecated full/nearfull from osdmap
Fixes: https://tracker.ceph.com/issues/43485

Caused by: 180e3bbe2e319d4e5407ff0656dc5b0decfb8a4f

Signed-off-by: David Zafman <dzafman@redhat.com>

src/osd/OSDMap.cc
@@ -3983,8 +3983,6 @@ void OSDMap::print_summary(Formatter *f, ostream& out,
     f->dump_int("osd_up_since", last_up_change.to_msec() / 1000);
     f->dump_int("num_in_osds", get_num_in_osds());
     f->dump_int("osd_in_since", last_in_change.to_msec() / 1000);
-    f->dump_bool("full", test_flag(CEPH_OSDMAP_FULL));
-    f->dump_bool("nearfull", test_flag(CEPH_OSDMAP_NEARFULL));
     f->dump_unsigned("num_remapped_pgs", get_num_pg_temp());
   } else {
     utime_t now = ceph_clock_now();