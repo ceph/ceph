4dc057b82ab23d4cc78f69dae832ba91ebfe6382
Casey Bodley <cbodley@redhat.com>
2020-01-13 14:15:01 -0500

    qa/rgw: s3a-hadoop task defaults to maven-version 3.6.3
    binaries for v3.6.2 are no longer available under
    http://www-us.apache.org/dist/maven/maven-3/
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- qa/tasks/s3a_hadoop.py
+++ qa/tasks/s3a_hadoop.py
@@ -14,8 +14,8 @@ def task(ctx, config):
       -tasks:
          ceph-ansible:
          s3a-hadoop:
-           maven-version: '3.3.9' (default)
-           hadoop-version: '2.7.3'
+           maven-version: '3.6.3' (default)
+           hadoop-version: '2.9.2'
            bucket-name: 's3atest' (default)
            access-key: 'anykey' (uses a default value)
            secret-key: 'secretkey' ( uses a default value)
@@ -40,7 +40,7 @@ def task(ctx, config):
 
     # get versions
     maven_major = config.get('maven-major', 'maven-3')
-    maven_version = config.get('maven-version', '3.6.2')
+    maven_version = config.get('maven-version', '3.6.3')
     hadoop_ver = config.get('hadoop-version', '2.9.2')
     bucket_name = config.get('bucket-name', 's3atest')
     access_key = config.get('access-key', 'EGAQRD2ULOIFKFSKCT4F')
