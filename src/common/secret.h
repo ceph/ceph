80f6f924a3b391f33004fa5fb734746fd1ce0351
Jeff Layton <jlayton@redhat.com>
2020-01-17 07:48:12 -0500

    mount.ceph: remove arbitrary limit on size of name= option
    Aaron was getting back -ERANGE errors when trying to mount using
    a long name= option. The issue is that the destination buffer for the
    "key=" string is not big enough to hold long names.
    
    When I overhauled the mount.ceph code recently, I made this buffer much
    smaller than before figuring that it didn't need to be any larger than
    the length of "secret=<base64 encoded key>".
    
    In the case where the secret is set in the keyring though, this buffer
    needs to be able to hold a string like "key=client.<cephx name>". The
    cephx name can be of arbitrary length, however.
    
    Rework the code to just safe_cat the strings directly into the options
    buffer, eliminating an extra copy and the need for an arbitrary limit.
    This also allows us to remove get_secret_option() from the the common
    code as well.
    
    Fixes: https://tracker.ceph.com/issues/43649
    Reported-by: Aaron <aarongmldt@gmail.com>
    Signed-off-by: Jeff Layton <jlayton@redhat.com>
    (cherry picked from commit 8db2a44c9438749be98d41fb309f10d5084805df)
    
    Conflicts:
    	src/common/secret.c

--- src/common/secret.h
+++ src/common/secret.h
@@ -7,13 +7,6 @@ extern "C" {
 
 int read_secret_from_file(const char *filename, char *secret, size_t max_len);
 
-/*
- * Attempts to add the secret to the kernel, but falls back to
- * the old secret= option if the kernel is too old.
- */
-int get_secret_option(const char *secret, const char *key_name,
-		      char *secret_option, size_t secret_option_len);
-
 int set_kernel_secret(const char *secret, const char *key_name);
 
 int is_kernel_secret(const char *key_name);
