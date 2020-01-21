a871ef4150f1946fbc357c5b98bb591c61af3407
Sage Weil <sage@redhat.com>
2020-01-21 10:43:04 -0600

    pybind/mgr/*: fix config_notify handling of default values
    The default values are handled by mgr_module.py's _get_module_option();
    the or here means that we break any non-true (0, false, none) value and
    override it with the default.
    
    https://tracker.ceph.com/issues/43746
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/pybind/mgr/diskprediction_local/module.py
+++ src/pybind/mgr/diskprediction_local/module.py
@@ -54,7 +54,7 @@ class Module(MgrModule):
         for opt in self.MODULE_OPTIONS:
             setattr(self,
                     opt['name'],
-                    self.get_module_option(opt['name']) or opt['default'])
+                    self.get_module_option(opt['name']))
             self.log.debug(' %s = %s', opt['name'], getattr(self, opt['name']))
         if self.get_ceph_option('device_failure_prediction_mode') == 'local':
             self._event.set()
