ebdc3121d0f3ceded8fa2974e0a1950b7226d0c7
Sage Weil <sage@redhat.com>
2020-01-21 10:43:04 -0600

    pybind/mgr/*: fix config_notify handling of default values
    The default values are handled by mgr_module.py's _get_module_option();
    the or here means that we break any non-true (0, false, none) value and
    override it with the default.
    
    https://tracker.ceph.com/issues/43746
    Signed-off-by: Sage Weil <sage@redhat.com>
    (cherry picked from commit 5ace82e65c72847fb875fc01c419937a26a59d70)
    
    Conflicts:
    	src/pybind/mgr/cephadm/module.py
    - nautilus has no "cephadm" module. It does have an "orchestrator_cli"
      module but it doesn't contain the code being patched
    	src/pybind/mgr/hello/module.py
    - nautilus has a "hello" module, but it doesn't contain the code being
      patched

--- src/pybind/mgr/alerts/module.py
+++ src/pybind/mgr/alerts/module.py
@@ -105,7 +105,7 @@ class Alerts(MgrModule):
         for opt in self.MODULE_OPTIONS:
             setattr(self,
                     opt['name'],
-                    self.get_module_option(opt['name']) or opt['default'])
+                    self.get_module_option(opt['name']))
             self.log.debug(' mgr option %s = %s',
                            opt['name'], getattr(self, opt['name']))
         # Do the same for the native options.
