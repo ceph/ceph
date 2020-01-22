ceph/ceph

commit 16aacb8a7ffeef7a22e4c654dc6a5475d606080d

rgw: cr_rest: allow a error type for wait with bufferlists
This is the version that will not apply json decoding if the type is a
bufferlist

Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>
(cherry picked from commit 8a7044d0c1716b9df321bb7c94278f719a25a67c)

Conflicts:
	src/rgw/rgw_rest_conn.h
conflicts due to optional_yield_context in master

src/rgw/rgw_rest_conn.h
@@ -422,45 +422,35 @@ public:
     return req.get_http_status();
   }
 
-  template <class E = std::nullptr_t>
+  template <class E = int>
   int wait(bufferlist *pbl, E *err_result = nullptr) {
-    int ret = req.wait(y);
+    int ret = req.wait();
     *pbl = bl;
 
-    if (ret >=0) {
-      ret = req.get_status();
+    if (ret < 0 && err_result ) {
+      ret = parse_decode_json(*err_result, bl);
     }
 
-    if (ret < 0) {
-      if constexpr (!std::is_same_v<E, std::nullptr_t>) {
-        if (err_result) {
-          ret = parse_decode_json(*err_result, bl);
-        }
-      }
-    }
-
-    return ret;
+    return req.get_status();
   }
 
   template <class T, class E = int>
   int wait(T *dest, E *err_result = nullptr);
 };
 
-template <class T, class E=std::nullptr_t>
-int RGWRESTSendResource::wait(T *dest, optional_yield y, E *err_result)
+template <class T, class E=int>
+int RGWRESTSendResource::wait(T *dest, E *err_result)
 {
-  int ret = req.wait(y);
-  if (ret >=0) {
+  int ret = req.wait();
+  if (ret >= 0) {
     ret = req.get_status();
   }
 
-  if constexpr (!std::is_same_v<E, std::nullptr_t>) {
-    if (ret <0 && err_result) {
-      ret = parse_decode_json(*err_result, bl);
-    }
+  if (ret < 0 && err_result) {
+    ret = parse_decode_json(*err_result, bl);
   }
 
-  if (ret < 0){
+  if (ret < 0) {
     return ret;
   }
 

commit 9f69935ab2502b1bb367792ed815f764f8c55b2c

src/rgw/rgw_rest_conn.h: fix build with clang
Building with clang requires this little fix for

[ 60%] Building CXX object src/rgw/CMakeFiles/rgw_common.dir/services/svc_zone.cc.o
cd /<<PKGBUILDDIR>>/obj-arm-linux-gnueabihf/src/rgw && /usr/bin/clang++  -D_FILE_OFFSET_BITS=64 -D_GNU_SOURCE -D__linux__ -I/<<PKGBUILDDIR>>/obj-arm-linux-gnueabihf/src/include -I/<<PKGBUILDDIR>>/src -I/usr/include/nss -I/usr/include/nspr -I/<<PKGBUILDDIR>>/src/dmclock/support/src -isystem /<<PKGBUILDDIR>>/obj-arm-linux-gnueabihf/include -isystem /<<PKGBUILDDIR>>/src/xxHash -isystem /<<PKGBUILDDIR>>/src/rapidjson/include -isystem /<<PKGBUILDDIR>>/src/rgw/services  -g -O2 -fdebug-prefix-map=/<<PKGBUILDDIR>>=. -fstack-protector-strong -Wformat -Werror=format-security -Wdate-time -D_FORTIFY_SOURCE=2 -Wall -Wtype-limits -Wignored-qualifiers -Winit-self -Wpointer-arith -Werror=format-security -fno-strict-aliasing -fsigned-char -Wno-unknown-pragmas -Wno-unused-function -Wno-unused-local-typedef -Wno-varargs -Wno-gnu-designator -Wno-missing-braces -Wno-parentheses -Wno-deprecated-register -g -O2 -fdebug-prefix-map=/<<PKGBUILDDIR>>=. -fstack-protector-strong -Wformat -Werror=format-security -Wdate-time -D_FORTIFY_SOURCE=2 -ftemplate-depth-1024 -Wnon-virtual-dtor -Wno-unknown-pragmas -Wno-ignored-qualifiers -Wno-inconsistent-missing-override -Wno-mismatched-tags -Wno-unused-private-field -Wno-address-of-packed-member -fdiagnostics-color=auto -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free -fPIC   -DHAVE_CONFIG_H -D__CEPH__ -D_REENTRANT -D_THREAD_SAFE -D__STDC_FORMAT_MACROS -std=c++17 -o CMakeFiles/rgw_common.dir/services/svc_zone.cc.o -c /<<PKGBUILDDIR>>/src/rgw/services/svc_zone.cc
In file included from /<<PKGBUILDDIR>>/src/rgw/services/svc_zone.cc:7:
/<<PKGBUILDDIR>>/src/rgw/rgw_rest_conn.h:441:28: error: template parameter redefines default argument
template <class T, class E=int>
                           ^
/<<PKGBUILDDIR>>/src/rgw/rgw_rest_conn.h:437:32: note: previous default template argument defined here
  template <class T, class E = int>
                               ^
1 error generated.

This fixes the backport in 16aacb8a7ffeef7a22e4c654dc6a5475d606080d

Fixes: 16aacb8a7ffeef7a22e4c654dc6a5475d606080d

src/rgw/rgw_rest_conn.h
@@ -438,7 +438,7 @@ public:
   int wait(T *dest, E *err_result = nullptr);
 };
 
-template <class T, class E=int>
+template <class T, class E>
 int RGWRESTSendResource::wait(T *dest, E *err_result)
 {
   int ret = req.wait();