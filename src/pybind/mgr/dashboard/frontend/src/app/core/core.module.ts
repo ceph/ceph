ad2d19f2d5a6255e5774b66c2a89785940c7061e
Tiago Melo <tmelo@suse.com>
2020-01-22 08:46:30 -0100

    mgr/dashboard: Improve Notification sidebar
    Remove the external package and add custom scrollbar.
    
    Fix random e2e tests.
    
    Fixes: https://tracker.ceph.com/issues/43804
    Fixes: https://tracker.ceph.com/issues/43593
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/core/core.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/core.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { BlockUIModule } from 'ng-block-ui';
-import { SidebarModule } from 'ng-sidebar';
 
 import { SharedModule } from '../shared/shared.module';
 import { ForbiddenComponent } from './forbidden/forbidden.component';
@@ -13,14 +12,7 @@ import { NavigationModule } from './navigation/navigation.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  imports: [
-    BlockUIModule.forRoot(),
-    CommonModule,
-    NavigationModule,
-    RouterModule,
-    SharedModule,
-    SidebarModule.forRoot()
-  ],
+  imports: [BlockUIModule.forRoot(), CommonModule, NavigationModule, RouterModule, SharedModule],
   exports: [NavigationModule],
   declarations: [
     NotFoundComponent,
