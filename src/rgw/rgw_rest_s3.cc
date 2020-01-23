9d7633dccef29a2d0d7d7adea0246725483a5fd5
Yehuda Sadeh <yehuda@redhat.com>
2020-01-22 18:45:26 -0800

    rgw: bucket sync: don't allow replication api on legacy config
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -48,6 +48,7 @@
 #include "rgw_crypt_sanitize.h"
 #include "rgw_rest_user_policy.h"
 #include "rgw_zone.h"
+#include "rgw_bucket_sync.h"
 
 #include "services/svc_zone.h"
 #include "services/svc_cls.h"
@@ -4321,6 +4322,12 @@ RGWOp *RGWHandler_REST_Bucket_S3::op_put()
   } else if (is_notification_op()) {
     return RGWHandler_REST_PSNotifs_S3::create_put_op();
   } else if (is_replication_op()) {
+    auto sync_policy_handler = store->svc()->zone->get_sync_policy_handler(nullopt);
+    if (!sync_policy_handler ||
+        sync_policy_handler->is_legacy_config()) {
+      return nullptr;
+    }
+
     return new RGWPutBucketReplication_ObjStore_S3;
   }
   return new RGWCreateBucket_ObjStore_S3;
