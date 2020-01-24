acd66617546d8dec36fdf0f8e7f18c68a89074a6
Igor Fedotov <ifedotov@suse.com>
2020-01-25 02:00:29 +0300

    os/bluestore: get rid off OnodeShardCache::_touch.
    Onode pinning does the same for us.
    
    Signed-off-by: Igor Fedotov <ifedotov@suse.com>

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -1216,7 +1216,6 @@ public:
                                    PerfCounters *logger);
     virtual void _add(Onode* o, int level) = 0;
     virtual void _rm(Onode* o) = 0;
-    virtual void _touch(Onode* o) = 0;
 
     void pin(Onode* o, std::function<bool ()> validator) {
       std::lock_guard l(lock);
