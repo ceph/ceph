ceph/ceph

commit 9a7f76a7bd88cd79d47cad92d4dd4ab786262fa0

Set appropriate bucket quota value (when quota value is less than 0)
Signed-off-by: GaryHyg <huygbj@inspur.com>

src/rgw/rgw_rest_user.cc
@@ -183,6 +183,9 @@ void RGWOp_User_Create::execute()
   }
 
   if (max_buckets != default_max_buckets)
+    if (max_buckets < 0) {
+      max_buckets = -1;
+    }
     op_state.set_max_buckets(max_buckets);
 
   if (s->info.args.exists("suspended"))
@@ -275,6 +278,9 @@ void RGWOp_User_Modify::execute()
   op_state.set_secret_key(secret_key);
 
   if (quota_set)
+    if (max_buckets < 0 ) {
+      max_buckets = -1;
+    }
     op_state.set_max_buckets(max_buckets);
 
   if (gen_key)

commit d1f8717988dafbdff77c73a1359fec6ebb42c789

rgw: set bucket quota only when necessary
this change fixes the regression introduced by 9a7f76a7

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/rgw/rgw_rest_user.cc
@@ -182,12 +182,12 @@ void RGWOp_User_Create::execute()
     op_state.set_key_type(key_type);
   }
 
-  if (max_buckets != default_max_buckets)
+  if (max_buckets != default_max_buckets) {
     if (max_buckets < 0) {
       max_buckets = -1;
     }
     op_state.set_max_buckets(max_buckets);
-
+  }
   if (s->info.args.exists("suspended"))
     op_state.set_suspension(suspended);
 
@@ -277,12 +277,12 @@ void RGWOp_User_Modify::execute()
   op_state.set_access_key(access_key);
   op_state.set_secret_key(secret_key);
 
-  if (quota_set)
+  if (quota_set) {
     if (max_buckets < 0 ) {
       max_buckets = -1;
     }
     op_state.set_max_buckets(max_buckets);
-
+  }
   if (gen_key)
     op_state.set_generate_key();
 