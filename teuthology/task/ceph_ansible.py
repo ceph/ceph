22832b263dbca873fa4f38b11008f8b07d9224ab
Brad Hubbard <bhubbard@redhat.com>
2020-01-28 16:54:59 +1000

    ceph_ansible: Make sure cryptography version is compatible
    Fixes: https://tracker.ceph.com/issues/43843
    
    Signed-off-by: Brad Hubbard <bhubbard@redhat.com>

--- teuthology/task/ceph_ansible.py
+++ teuthology/task/ceph_ansible.py
@@ -432,6 +432,11 @@ class CephAnsible(Task):
             run.Raw(';'),
             'pip',
             'install',
+            '--upgrade',
+            'cryptography>=2.5',
+            run.Raw(';'),
+            'pip',
+            'install',
             run.Raw('setuptools>=11.3'),
             run.Raw('notario>=0.0.13'), # FIXME: use requirements.txt
             run.Raw('netaddr'),
