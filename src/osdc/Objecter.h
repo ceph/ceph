74b2cd6d9d42d0a98db4a6c310c0963737d8468f
Sage Weil <sage@redhat.com>
2020-01-31 12:01:19 -0600

    osdc/Objecter: inline pool full check
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/osdc/Objecter.h
+++ src/osdc/Objecter.h
@@ -1968,6 +1968,7 @@ public:
   bool osdmap_full_flag() const;
   bool osdmap_pool_full(const int64_t pool_id) const;
 
+
  private:
 
   /**
@@ -1977,7 +1978,9 @@ public:
    *         the global full flag is set, else false
    */
   bool _osdmap_pool_full(const int64_t pool_id) const;
-  bool _osdmap_pool_full(const pg_pool_t &p) const;
+  bool _osdmap_pool_full(const pg_pool_t &p) const {
+    return p.has_flag(pg_pool_t::FLAG_FULL) && honor_pool_full;
+  }
   void update_pool_full_map(std::map<int64_t, bool>& pool_full_map);
 
   std::map<uint64_t, LingerOp*> linger_ops;
