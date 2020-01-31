6fb79af05c08fa4f1c77ae121ed8f9a38e494eba
Sage Weil <sage@redhat.com>
2020-01-31 11:51:23 -0600

    osdc/Objecter: move respects_full() to op_target_t
    Signed-off-by: Sage Weil <sage@redhat.com>
    (cherry picked from commit 3ef8c4210fbac6621d5c86ae54ab12e38bc31b8f)
    
     Conflicts:
    	src/osdc/Objecter.h

--- src/osdc/Objecter.h
+++ src/osdc/Objecter.h
@@ -1336,7 +1336,13 @@ public:
       return r == 0 || (r > 0 && h < end);
     }
 
-    void dump(Formatter *f) const;
+    bool respects_full() const {
+      return
+	(flags & (CEPH_OSD_FLAG_WRITE | CEPH_OSD_FLAG_RWORDERED)) &&
+	!(flags & (CEPH_OSD_FLAG_FULL_TRY | CEPH_OSD_FLAG_FULL_FORCE));
+    }
+
+    void dump(ceph::Formatter *f) const;
   };
 
   struct Op : public RefCountedObject {
@@ -1433,12 +1439,6 @@ public:
 
     bool operator<(const Op& other) const {
       return tid < other.tid;
-    }
-
-    bool respects_full() const {
-      return
-	(target.flags & (CEPH_OSD_FLAG_WRITE | CEPH_OSD_FLAG_RWORDERED)) &&
-	!(target.flags & (CEPH_OSD_FLAG_FULL_TRY | CEPH_OSD_FLAG_FULL_FORCE));
     }
 
   private:
