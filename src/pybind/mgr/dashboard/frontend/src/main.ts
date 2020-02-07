90a950c3cc5086731bcbfd9c2d758ea943011b94
Tiago Melo <tmelo@suse.com>
2020-02-07 14:37:13 -0100

    mgr/dashboard: Temporary fix for Chrome 80
    There is a bug in Chrome 80 that breaks the reduce method.
    This solution seems to fix the problem:
    https://bugs.chromium.org/p/chromium/issues/detail?id=1049982#c7
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/main.ts
+++ src/pybind/mgr/dashboard/frontend/src/main.ts
@@ -11,3 +11,26 @@ if (environment.production) {
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.log(err));
+
+/*
+ * Temporary fix for Chrome 80 bug regarding reduce method
+ * source: https://bugs.chromium.org/p/chromium/issues/detail?id=1049982#c7
+ */
+(function() {
+  function getChromeVersion() {
+    const raw = navigator.userAgent.match(/Chrom(e|ium)\/([0-9]+)\./);
+    return raw ? parseInt(raw[2], 10) : false;
+  }
+
+  const chromeVersion = getChromeVersion();
+  if (chromeVersion && chromeVersion >= 80) {
+    const arrayReduce = Array.prototype.reduce;
+    let callback;
+    Object.defineProperty(Array.prototype, 'reduce', {
+      value: function(cb: any, ...args: any[]) {
+        callback = cb;
+        return arrayReduce.call(this, callback, ...args);
+      }
+    });
+  }
+})();
