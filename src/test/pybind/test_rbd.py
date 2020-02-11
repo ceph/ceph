cd6cfa5b084626826a1cd87a0da4946afd53d144
Jason Dillaman <dillaman@redhat.com>
2020-02-11 16:08:30 -0500

    test/pybind/rbd: demoted images cannot be deleted
    Now that we don't delete the last mirror snapshot, the test is failing
    because you cannot delete a demoted image.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/test/pybind/test_rbd.py
+++ src/test/pybind/test_rbd.py
@@ -2132,6 +2132,7 @@ class TestMirroring(object):
 
         self.rbd.mirror_peer_remove(ioctx, peer1_uuid)
         self.rbd.mirror_peer_remove(ioctx, peer2_uuid)
+        self.image.mirror_image_promote(False)
 
 class TestTrash(object):
 
