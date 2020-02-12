6556ff7ae21b11b93dc9fb7865620c5df4774a8e
harishmunjulur <harishmunjulur@gmail.com>
2020-02-12 12:25:45 -0800

    qa:Using python3 -m nose for "nosetests:command not found" error
    On bionic "nosetests3" and on el "nosetests-3/nosetests-3.6" use neither.
    Rhel 7.7 does not support python3 using nosetests.
    
    Signed-off-by: harishmunjulur <harishmunjulur@gmail.com>

--- qa/workunits/rbd/test_librbd_python.sh
+++ qa/workunits/rbd/test_librbd_python.sh
@@ -2,11 +2,19 @@
 
 relpath=$(dirname $0)/../../../src/test/pybind
 
+source /etc/os-release
+
+pycmd=$"python3 -m nose"
+
+if [ "$VERSION_ID" == "7.7" ]; then
+        pycmd=$"nosetests"
+fi
+
 if [ -n "${VALGRIND}" ]; then
   valgrind ${VALGRIND} --suppressions=${TESTDIR}/valgrind.supp \
     --errors-for-leak-kinds=definite --error-exitcode=1 \
-    python3 -m nose -v $relpath/test_rbd.py
+    $pycmd -v $relpath/test_rbd.py
 else
-  python3 -m nose -v $relpath/test_rbd.py
+  $pycmd -v $relpath/test_rbd.py
 fi
 exit 0
