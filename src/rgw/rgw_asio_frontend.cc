0b1c81c5e9818393157cbc9fe8586e5d41f56c6c
Yehuda Sadeh <yehuda@redhat.com>
2020-02-17 15:24:01 -0800

    rgw: beast frontend: handle default ssl configurables
    Also don't exit immediately if can't init ssl cert, only if cert is actually
    needed (if ssl_port/ssl_endpoint is configured).
    
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_asio_frontend.cc
+++ src/rgw/rgw_asio_frontend.cc
@@ -590,36 +590,40 @@ int AsioFrontend::init_ssl()
   auto& config = conf->get_config_map();
 
   // ssl configuration
-  auto cert = config.find("ssl_certificate");
-  const bool have_cert = cert != config.end();
-  if (have_cert) {
+  std::optional<string> cert = conf->get_val("ssl_certificate");
+  if (cert) {
     // only initialize the ssl context if it's going to be used
     ssl_context = boost::in_place(ssl::context::tls);
   }
 
-  auto key = config.find("ssl_private_key");
-  const bool have_private_key = key != config.end();
-  if (have_private_key) {
-    if (!have_cert) {
-      lderr(ctx()) << "no ssl_certificate configured for ssl_private_key" << dendl;
-      return -EINVAL;
-    }
-    int r = ssl_set_private_key(key->second);
-    if (r < 0) {
-      return r;
-    }
+  std::optional<string> key = conf->get_val("ssl_private_key");
+  bool have_cert = false;
+
+  if (key && !cert) {
+    lderr(ctx()) << "no ssl_certificate configured for ssl_private_key" << dendl;
+    return -EINVAL;
   }
-  if (have_cert) {
-    int r = ssl_set_certificate_chain(cert->second);
-    if (r < 0) {
-      return r;
+
+  bool key_is_cert = false;
+
+  if (cert) {
+    if (!key) {
+      key = cert;
+      key_is_cert = true;
     }
-    if (!have_private_key) {
-      // attempt to use it as a private key if a separate one wasn't provided
-      r = ssl_set_private_key(cert->second);
-      if (r < 0) {
-        return r;
+
+    int r = ssl_set_private_key(*key);
+    bool have_private_key = (r >= 0);
+    if (r < 0) {
+      if (!key_is_cert) {
+        r = ssl_set_private_key(*cert, true);
+        have_private_key = (r >= 0);
       }
+    }
+
+    if (have_private_key) {
+      int r = ssl_set_certificate_chain(*cert);
+      have_cert = (r >= 0);
     }
   }
 
