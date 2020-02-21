ceph/ceph

commit 1072a7e3b5026c1e605dd45967fa555d2e6a0d14

mgr/cephadm: fix upgrade wait loop
This was succeeding after 4 tries even though it wasn't ok-to-stop.

Signed-off-by: Sage Weil <sage@redhat.com>

src/pybind/mgr/cephadm/module.py
@@ -579,9 +579,7 @@ class CephadmOrchestrator(MgrModule, orchestrator.OrchestratorClientMixin):
                 self.log.info('Upgrade: It is safe to stop %s.%s' %
                               (s.daemon_type, s.daemon_id))
                 return True
-        self.log.info('Upgrade: It is safe to stop %s.%s' %
-                      (s.daemon_type, s.daemon_id))
-        return True
+        return False
 
     def _clear_upgrade_health_checks(self):
         for k in ['UPGRADE_NO_STANDBY_MGR',

commit a0879e3c86b191ba9595b03f88ff7664988b225c

mgr/cephadm: fix upgrade ok-to-stop condition check
Broke this in 1072a7e3b5026c1e605dd45967fa555d2e6a0d14

Signed-off-by: Sage Weil <sage@redhat.com>

src/pybind/mgr/cephadm/module.py
@@ -563,7 +563,9 @@ class CephadmOrchestrator(MgrModule, orchestrator.OrchestratorClientMixin):
         tries = 4
         while tries > 0:
             if s.daemon_type not in ['mon', 'osd', 'mds']:
-                break
+                self.log.info('Upgrade: It is presumed safe to stop %s.%s' %
+                              (s.daemon_type, s.daemon_id))
+                return True
             ret, out, err = self.mon_command({
                 'prefix': '%s ok-to-stop' % s.daemon_type,
                 'ids': [s.daemon_id],