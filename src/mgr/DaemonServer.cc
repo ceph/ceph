ceph/ceph

commit cb48be5a69fe6482cbe3bff1b53ba090e077de0d

mgr: drop session with Ceph daemon when not ready
If the mgr is waiting on daemon metadata from the mons, it has no
DaemonState associated with the daemon yet. If we try to process this
MgrOpen, the metadata sent by the daemon (like its config) will not be
recorded.

Fixes: https://tracker.ceph.com/issues/43037
Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>
(cherry picked from commit 16a1deb5b3f276efe7dd3904c26786e6d837ddee)

src/mgr/DaemonServer.cc
@@ -429,12 +429,22 @@ bool DaemonServer::handle_open(MMgrOpen *m)
     dout(20) << "updating existing DaemonState for " << key << dendl;
     daemon = daemon_state.get(key);
   }
-  if (m->service_daemon && !daemon) {
-    dout(4) << "constructing new DaemonState for " << key << dendl;
-    daemon = std::make_shared<DaemonState>(daemon_state.types);
-    daemon->key = key;
-    daemon->service_daemon = true;
-    daemon_state.insert(daemon);
+  if (!daemon) {
+    if (m->service_daemon) {
+      dout(4) << "constructing new DaemonState for " << key << dendl;
+      daemon = std::make_shared<DaemonState>(daemon_state.types);
+      daemon->key = key;
+      daemon->service_daemon = true;
+      daemon_state.insert(daemon);
+    } else {
+      /* A normal Ceph daemon has connected but we are or should be waiting on
+       * metadata for it. Close the session so that it tries to reconnect.
+       */
+      dout(2) << "ignoring open from " << key << " " << con->get_peer_addr()
+              << "; not ready for session (expect reconnect)" << dendl;
+      con->mark_down();
+      return true;
+    }
   }
   if (daemon) {
     if (m->service_daemon) {

commit 24c0e72d4daab35fddcd474ae107886a31a70fa6

mgr: drop reference to msg on return
Caused by backport commit cb48be5a69fe6482cbe3bff1b53ba090e077de0d which
did not account for the explicit drop of the message reference, only in
Nautilus-.

Fixes: https://tracker.ceph.com/issues/44245
Fixes: cb48be5a69fe6482cbe3bff1b53ba090e077de0d
Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

src/mgr/DaemonServer.cc
@@ -443,6 +443,7 @@ bool DaemonServer::handle_open(MMgrOpen *m)
       dout(2) << "ignoring open from " << key << " " << con->get_peer_addr()
               << "; not ready for session (expect reconnect)" << dendl;
       con->mark_down();
+      m->put();
       return true;
     }
   }