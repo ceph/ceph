43d06374970177918cf24a6bb485bdb9b6767762
Matt Benjamin <mbenjamin@redhat.com>
2020-02-24 17:56:49 -0500

    rgw: fail when get/set-bucket-versioning attempted on a non-existent bucket
    These should not succeed, but fail w/HTTP 404 and NoSuchBucket as
    the API error.
    
    Fixes: https://tracker.ceph.com/issues/44280
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -1824,6 +1824,8 @@ void RGWGetBucketLocation_ObjStore_S3::send_response()
 
 void RGWGetBucketVersioning_ObjStore_S3::send_response()
 {
+  if (op_ret)
+    set_req_state_err(s, op_ret);
   dump_errno(s);
   end_header(s, this, "application/xml");
   dump_start(s);
