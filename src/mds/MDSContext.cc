ceph/ceph

commit 09c6ef48c6eecbdb62e7355629f1a8064109d6e7

mds: make MDSIOContextBase delete itself when shutting down
Fixes: https://tracker.ceph.com/issues/41346
Signed-off-by: Xuehan Xu <xxhdx1985126@163.com>

src/mds/MDSContext.cc
@@ -95,6 +95,7 @@ void MDSIOContextBase::complete(int r) {
   if (mds->is_daemon_stopping()) {
     dout(4) << "MDSIOContextBase::complete: dropping for stopping "
             << typeid(*this).name() << dendl;
+    MDSContext::complete(r);
     return;
   }
 

commit 4e1b81427ec40a4647029d33e635a32f84fb209d

mds: just delete MDSIOContextBase during shutdown
We don't want the finisher called during shutdown.

Fixes: https://tracker.ceph.com/issues/44295
Fixes: 09c6ef48c6eecbdb62e7355629f1a8064109d6e7
Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

src/mds/MDSContext.cc
@@ -98,11 +98,8 @@ void MDSIOContextBase::complete(int r) {
   if (mds->is_daemon_stopping()) {
     dout(4) << "MDSIOContextBase::complete: dropping for stopping "
             << typeid(*this).name() << dendl;
-    MDSContext::complete(r);
-    return;
-  }
-
-  if (r == -EBLACKLISTED) {
+    delete this;
+  } else if (r == -EBLACKLISTED) {
     derr << "MDSIOContextBase: blacklisted!  Restarting..." << dendl;
     mds->respawn();
   } else {