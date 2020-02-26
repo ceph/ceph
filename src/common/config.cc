ceph/ceph

commit c81754af907188be13ec43dcf9456ea52c2ba6b3

common/config :remove duplicaed code.
In fact, in func _get_val call normalize_key_name.

Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

src/common/config.cc
@@ -1031,8 +1031,7 @@ Option::value_t md_config_t::get_val_generic(
   const ConfigValues& values,
   const std::string_view key) const
 {
-  string k(ConfFile::normalize_key_name(key));
-  return _get_val(values, k);
+  return _get_val(values, key);
 }
 
 Option::value_t md_config_t::_get_val(

commit 0be9985d6e781d906a11827de84400fbe5650bb1

common/config: fix lack of normalize_key_name() apply.
This is a fix to regression caused by:
c81754af907188be13ec43dcf9456ea52c2ba6b3

In fact md_config_t::_get_val was lacking of proper normalize_key_name()
call.

Signed-off-by: Igor Fedotov <ifedotov@suse.com>

src/common/config.cc
@@ -1047,7 +1047,7 @@ Option::value_t md_config_t::_get_val(
   // In key names, leading and trailing whitespace are not significant.
   string k(ConfFile::normalize_key_name(key));
 
-  const Option *o = find_option(key);
+  const Option *o = find_option(k);
   if (!o) {
     // not a valid config option
     return Option::value_t(boost::blank());