100065a6d41f3b2079074f5d5492ac25560099d5
Kefu Chai <kchai@redhat.com>
2020-03-02 16:55:39 +0800

    qa/tasks/mgr/test_orchestrator_cli: s/CommandFailedError/DriveGroupValidationError/
    `DriveGroupValidationError` is raised by `DriveGroupSpec.validate()`
    when an implementation of `Orchestrator.create_osds()` validates the
    given driver group with the hosts it manages before creating OSDs
    from given driver group spec.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- qa/tasks/mgr/test_orchestrator_cli.py
+++ qa/tasks/mgr/test_orchestrator_cli.py
@@ -3,7 +3,7 @@ import json
 import logging
 from time import sleep
 
-from teuthology.exceptions import CommandFailedError
+from ceph.deployment.drive_group import DriveGroupValidationError
 
 from mgr_test_case import MgrTestCase
 
@@ -95,8 +95,8 @@ class TestOrchestratorCli(MgrTestCase):
         res = self._orch_cmd_result("osd", "create", "-i", "-", stdin=json.dumps(drive_groups))
         self.assertEqual(res, 0)
 
-        #with self.assertRaises(Exception):
-        #    self._orch_cmd("osd", "create", "notfound:device")
+        with self.assertRaises(DriveGroupValidationError):
+            self._orch_cmd("osd", "create", "notfound:device")
 
     def test_blink_device_light(self):
         def _ls_lights(what):
