f88fd694ef5d68765db902b79e3c59cba7129305
Shyukri Shyukriev <shshyukriev@suse.com>
2020-03-05 14:42:39 +0200

    mrg/dashboard: update protractor to fix chromedriver errors
    E/launcher - session not created: Chrome version must be between 71 and 75
    (Driver info: chromedriver=2.46.628388 (4a34a70827ac54148e092aafb70504c4ea7ae926),platform=Linux 4.15.0-72-generic x86_64)
    
    Disable pylint warning
    
    Signed-off-by: Shyukri Shyukriev <shshyukriev@suse.com>

--- src/pybind/mgr/dashboard/module.py
+++ src/pybind/mgr/dashboard/module.py
@@ -438,6 +438,7 @@ class StandbyModule(MgrStandbyModule, CherryPyConfig):
         module = self
 
         class Root(object):
+            # pylint: disable=inconsistent-return-statements
             @cherrypy.expose
             def default(self, *args, **kwargs):
                 if module.get_module_option('standby_behaviour', 'redirect') == 'redirect':
@@ -445,12 +446,11 @@ class StandbyModule(MgrStandbyModule, CherryPyConfig):
                     if active_uri:
                         module.log.info("Redirecting to active '%s'", active_uri)
                         raise cherrypy.HTTPRedirect(active_uri)
-                    else:
-                        template = """
+                    template = """
                     <html>
                         <!-- Note: this is only displayed when the standby
-                             does not know an active URI to redirect to, otherwise
-                             a simple redirect is returned instead -->
+                                does not know an active URI to redirect to, otherwise
+                                a simple redirect is returned instead -->
                         <head>
                             <title>Ceph</title>
                             <meta http-equiv="refresh" content="{delay}">
@@ -462,7 +462,7 @@ class StandbyModule(MgrStandbyModule, CherryPyConfig):
                         </body>
                     </html>
                         """
-                        return template.format(delay=5)
+                    return template.format(delay=5)
 
         cherrypy.tree.mount(Root(), "{}/".format(self.url_prefix), {})
         self.log.info("Starting engine...")
