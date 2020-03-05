4179b960a4b1aa9c72ff51302eb6a83193b860b4
Matthew Oliver <moliver@suse.com>
2020-03-05 13:55:47 +1100

    cephadm: ceph-iscsi first draft
    This if the first draft of the ceph-iscsi in cephadm.
    There are a few gotchas when running `rbd-target-api` in a container:
    
     1. We need both the ceph.conf and iscsi-gateway.cfg, so needed to
    ability to pass extra config. This latter is based off the spec, so now
    the daemon config func api allows you to return a dict of configs:
    
      { 'config': '<str>' # will be appended to the ceph.conf
        '<conf name>': 'str', # Will be dumped in datadir/<conf name>
        ...
      }
    
    It will be up to cephadm to know to bind mount it to the right location.
    The first 'config' isn't used by this patch, but makes it possible for
    specs or config funcs to append anything? maybe it's overkill.
    
     2. We need the kernel's configfs in the container so we can configure
    LIO. There is a chicken and egg problem, configfs isn't mounted on the
    host to bind mount when the container starts. So now a check is added to
    the `unit.run` and cleanup in the `unit.poststop` scripts for
    daemon_type iscsi.
    
     3. rbd-target-api is python and hardcodes a few things, like logging
    through `/dev/log` which happens to be a domain socket. So `/dev/log`
    also needed to be bind mounted into the continer.
    
     4. The daemon expects the keyring to be in `/etc/ceph` so this needed to
    be specifically bind mounted to the correct location too.
    
    As this currently stands this is deploying and starting the api on port
    5000, so seems to be "working", also gateway.conf does exist in the
    pool. I have yet to set up an iscsi device, but will test that next.
    
    The `rbd-target-api` daemon expects the ssl key and cert to be named a
    certain name in the contianer. So SSL isn't working yet. However, I do
    hav a PR in ceph-iscsi to look in the mon config-key store for them[0].
    
    [0] - https://github.com/ceph/ceph-iscsi/pull/173
    
    Signed-off-by: Matthew Oliver <moliver@suse.com>

--- src/pybind/mgr/cephadm/tests/test_cephadm.py
+++ src/pybind/mgr/cephadm/tests/test_cephadm.py
@@ -13,7 +13,7 @@ except ImportError:
     pass
 
 from ceph.deployment.service_spec import ServiceSpec, PlacementSpec, RGWSpec, \
-    NFSServiceSpec
+    NFSServiceSpec, IscsiServiceSpec
 from orchestrator import ServiceDescription, DaemonDescription, InventoryHost, \
     HostSpec, OrchestratorError
 from tests import mock
@@ -322,6 +322,15 @@ class TestCephadm(object):
             match_glob(out, "Deployed nfs.name.* on host 'test'")
 
     @mock.patch("cephadm.module.CephadmOrchestrator._run_cephadm", _run_cephadm('{}'))
+    def test_iscsi(self, cephadm_module):
+        with self._with_host(cephadm_module, 'test'):
+            ps = PlacementSpec(hosts=['test'], count=1)
+            spec = IscsiServiceSpec('name', pool='pool', placement=ps)
+            c = cephadm_module.add_iscsi(spec)
+            [out] = wait(cephadm_module, c)
+            match_glob(out, "Deployed iscsi.name.* on host 'test'")
+
+    @mock.patch("cephadm.module.CephadmOrchestrator._run_cephadm", _run_cephadm('{}'))
     def test_prometheus(self, cephadm_module):
         with self._with_host(cephadm_module, 'test'):
             ps = PlacementSpec(hosts=['test'], count=1)
@@ -406,6 +415,15 @@ class TestCephadm(object):
             spec = NFSServiceSpec('name', pool='pool', namespace='namespace', placement=ps)
             c = cephadm_module.apply_nfs(spec)
             assert wait(cephadm_module, c) == 'Scheduled nfs update...'
+            assert [d.spec for d in wait(cephadm_module, cephadm_module.describe_service())] == [spec]
+
+    @mock.patch("cephadm.module.CephadmOrchestrator._run_cephadm", _run_cephadm('{}'))
+    def test_apply_iscsi_save(self, cephadm_module):
+        with self._with_host(cephadm_module, 'test'):
+            ps = PlacementSpec(hosts=['test'], count=1)
+            spec = IscsiServiceSpec('name', pool='pool', placement=ps)
+            c = cephadm_module.apply_iscsi(spec)
+            assert wait(cephadm_module, c) == 'Scheduled iscsi update...'
             assert [d.spec for d in wait(cephadm_module, cephadm_module.describe_service())] == [spec]
 
     @mock.patch("cephadm.module.CephadmOrchestrator._run_cephadm", _run_cephadm('{}'))
