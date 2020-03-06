8c080afbd3fbb9f44110d1f79fdc44ec0b2ab084
Xiubo Li <xiubli@redhat.com>
2020-03-05 20:10:47 -0500

    qa/vstart_runner.py: add os and arch properties support
    Fixes: https://tracker.ceph.com/issues/44456
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- qa/tasks/vstart_runner.py
+++ qa/tasks/vstart_runner.py
@@ -51,6 +51,7 @@ import platform
 from teuthology import misc
 from teuthology.orchestra.run import Raw, quote
 from teuthology.orchestra.daemon import DaemonGroup
+from teuthology.orchestra.remote import Remote
 from teuthology.config import config as teuth_config
 import six
 import logging
@@ -238,6 +239,9 @@ class LocalRemote(object):
 
     Run this inside your src/ dir!
     """
+
+    os = Remote.os
+    arch = Remote.arch
 
     def __init__(self):
         self.name = "local"
