52bc4c723e33d1c71c5e76bd5d7a1b73c81fcdab
Kefu Chai <kchai@redhat.com>
2020-03-08 13:39:59 +0800

    qa/tasks/ceph_manager: capture stderr for COT
    as we are expecting the error message written to stderr, and we need to
    check for the error messages in it.
    
    this change addresses the regression introduced by
    0b474433ce33c0c7d5fcc5b599cee90bca8acbf6
    
    Fixes: https://tracker.ceph.com/issues/44500
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- qa/tasks/ceph_manager.py
+++ qa/tasks/ceph_manager.py
@@ -306,7 +306,8 @@ class OSDThrasher(Thrasher):
                         ])
                     if proc.exitstatus == 0:
                         break
-                    elif proc.exitstatus == 1 and proc.stderr == "OSD has the store locked":
+                    elif (proc.exitstatus == 1 and
+                          six.ensure_str(proc.stderr.getvalue()) == "OSD has the store locked"):
                         continue
                     else:
                         raise Exception("ceph-objectstore-tool: "
@@ -418,7 +419,7 @@ class OSDThrasher(Thrasher):
                 ])
             if proc.exitstatus == 1:
                 bogosity = "The OSD you are using is older than the exported PG"
-                if bogosity in proc.stderr.getvalue():
+                if bogosity in six.ensure_str(proc.stderr.getvalue()):
                     self.log("OSD older than exported PG"
                              "...ignored")
             elif proc.exitstatus == 10:
@@ -453,8 +454,10 @@ class OSDThrasher(Thrasher):
                                '--journal-path', JPATH.format(id=imp_osd),
                            ])
                            + " --op apply-layout-settings --pool " + pool).format(id=osd)
-                    output = imp_remote.sh(cmd, wait=True, check_status=False)
-                    if 'Couldn\'t find pool' in output:
+                    proc = imp_remote.run(args=cmd,
+                                          wait=True, check_status=False,
+                                          stderr=BytesIO)
+                    if 'Couldn\'t find pool' in six.ensure_str(proc.stderr.getvalue()):
                         continue
                     if proc.exitstatus:
                         raise Exception("ceph-objectstore-tool apply-layout-settings"
