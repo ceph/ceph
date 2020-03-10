5489fd3b05f1a87f42ccf2c72e3eb23db7db1498
Sebastian Wagner <sebastian.wagner@suse.com>
2020-03-10 10:38:04 +0100

    python-common: don't run flake8 on tests.
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- src/python-common/ceph/tests/test_disk_selector.py
+++ src/python-common/ceph/tests/test_disk_selector.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 import pytest
 
 from ceph.deployment.inventory import Devices, Device
--- src/python-common/ceph/tests/test_drive_group.py
+++ src/python-common/ceph/tests/test_drive_group.py
@@ -1,3 +1,4 @@
+# flake8: noqa
 import pytest
 
 from ceph.deployment import drive_selection, translate
