34c7a596e1d1478af455230539c17060a65f10e0
Jianpeng Ma <jianpeng.ma@intel.com>
2020-03-11 09:49:23 +0800

    osd/OSD: remove usless ceph_assert.
    At the beginning of func advance_pg, it already check last epoch of pg.
    
    Signed-off-by: Jianpeng Ma <jianpeng.ma@intel.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -8449,7 +8449,6 @@ bool OSD::advance_pg(
   }
   ceph_assert(pg->is_locked());
   OSDMapRef lastmap = pg->get_osdmap();
-  ceph_assert(lastmap->get_epoch() < osd_epoch);
   set<PGRef> new_pgs;  // any split children
   bool ret = true;
 
