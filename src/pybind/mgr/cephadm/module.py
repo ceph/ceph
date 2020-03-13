f569125213caf6aa101879d0810aa489c44575e0
Michael Fritch <mfritch@suse.com>
2020-03-13 14:19:23 -0600

    mgr/cephadm: ensure nfs uses the correct entity
    mon entity needs to start with `client.`
    
    Signed-off-by: Michael Fritch <mfritch@suse.com>
    (cherry picked from commit fd6322f7ec51f3dc57168bbd08ffab3c832a4598)

--- src/pybind/mgr/cephadm/module.py
+++ src/pybind/mgr/cephadm/module.py
@@ -102,7 +102,7 @@ def name_to_config_section(name):
     Map from daemon names to ceph entity names (as seen in config)
     """
     daemon_type = name.split('.', 1)[0]
-    if daemon_type in ['rgw', 'rbd-mirror', 'crash']:
+    if daemon_type in ['rgw', 'rbd-mirror', 'nfs', 'crash']:
         return 'client.' + name
     elif daemon_type in ['mon', 'osd', 'mds', 'mgr', 'client']:
         return name
