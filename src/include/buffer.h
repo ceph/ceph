b9ce3911443d5e143d5f572119727a7ecc6b545d
Casey Bodley <cbodley@redhat.com>
2020-03-16 11:58:51 -0400

    buffer: add missing != operator for bufferlist
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/include/buffer.h
+++ src/include/buffer.h
@@ -1257,6 +1257,9 @@ inline bool operator==(const bufferlist &l, const bufferlist &r) {
   }
   return true;
 }
+inline bool operator!=(const bufferlist &l, const bufferlist &r) {
+  return !(l == r);
+}
 inline bool operator<(const bufferlist& l, const bufferlist& r) {
   return r > l;
 }
