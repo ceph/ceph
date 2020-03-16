f3a2f6620f81ed55752c5b48d0aaefa4118ede47
Sage Weil <sage@redhat.com>
2020-03-16 13:54:27 -0400

    python-common/ceph/deployment/service_spec: add ssl to RGWSpec
    Signed-off-by: Sage Weil <sage@redhat.com>

--- src/python-common/ceph/deployment/service_spec.py
+++ src/python-common/ceph/deployment/service_spec.py
@@ -419,6 +419,7 @@ class RGWSpec(ServiceSpec):
                  service_type='rgw',
                  rgw_frontend_port=None,  # type: Optional[int]
                  unmanaged=False,  # type: bool
+                 ssl=False,   # type: bool
                  ):
         assert service_type == 'rgw'
         if service_id:
@@ -432,3 +433,12 @@ class RGWSpec(ServiceSpec):
         self.rgw_realm = rgw_realm
         self.rgw_zone = rgw_zone
         self.rgw_frontend_port = rgw_frontend_port
+        self.ssl = ssl
+
+    def get_port(self):
+        if self.rgw_frontend_port:
+            return self.rgw_frontend_port
+        if self.ssl:
+            return 443
+        else:
+            return 80
