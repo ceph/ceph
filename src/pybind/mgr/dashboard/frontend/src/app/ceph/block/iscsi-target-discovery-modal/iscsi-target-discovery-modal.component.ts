532b271572f7b6410c099362dc046148f5a686a1
Tiago Melo <tmelo@suse.com>
2020-03-16 20:23:47 -0100

    mgr/dashboard: Fix iSCSI's username and password validation
    When using regex, instead of strings, angular doesn't assume the pattern must
    validate the entire string. 
    
    Add validation to the backend.
    
    Fixes: https://tracker.ceph.com/issues/44624
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/iscsi-target-discovery-modal/iscsi-target-discovery-modal.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/iscsi-target-discovery-modal/iscsi-target-discovery-modal.component.ts
@@ -22,8 +22,8 @@ export class IscsiTargetDiscoveryModalComponent implements OnInit {
   permission: Permission;
   hasPermission: boolean;
 
-  USER_REGEX = /[\w\.:@_-]{8,64}/;
-  PASSWORD_REGEX = /[\w@\-_\/]{12,16}/;
+  USER_REGEX = /^[\w\.:@_-]{8,64}$/;
+  PASSWORD_REGEX = /^[\w@\-_\/]{12,16}$/;
 
   constructor(
     private authStorageService: AuthStorageService,
