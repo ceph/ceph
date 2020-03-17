ceph/ceph

commit 50ba25b2aa4db34319a7b95e789065540347f895

rgw/pubsub: add kafka notification endpoint
Signed-off-by: Yuval Lifshitz <yuvalif@yahoo.com>
(cherry picked from commit 354d775e04e3f979b4742d963b8788bce3874a5f)
Signed-off-by: Yuval Lifshitz <yuvalif@yahoo.com>

Conflicts:
	ceph.spec.in
	debian/control
	do_cmake.sh
	doc/radosgw/notifications.rst
	src/test/rgw/rgw_multi/tests_ps.py

do_cmake.sh
@@ -8,6 +8,12 @@ fi
 
 PYBUILD="2"
 source /etc/os-release
+
+if [ "$(uname)" == FreeBSD ] ; then
+  DWITH_RADOSGW_AMQP_ENDPOINT="OFF"
+  DWITH_RADOSGW_KAFKA_ENDPOINT="OFF"
+fi
+
 case "$ID" in
     fedora)
         if [ "$VERSION_ID" -ge "29" ] ; then
@@ -23,6 +29,7 @@ case "$ID" in
     opensuse*|suse|sles)
         PYBUILD="3"
         WITH_RADOSGW_AMQP_ENDPOINT="OFF"
+        WITH_RADOSGW_KAFKA_ENDPOINT="OFF"
         ;;
 esac
 if [ "$PYBUILD" = "3" ] ; then

CMakeLists.txt
@@ -445,6 +445,7 @@ option(WITH_RADOSGW_FCGI_FRONTEND "Rados Gateway's FCGI frontend is enabled" OFF
 option(WITH_RADOSGW_BEAST_FRONTEND "Rados Gateway's Beast frontend is enabled" ON)
 option(WITH_RADOSGW_BEAST_OPENSSL "Rados Gateway's Beast frontend uses OpenSSL" ON)
 option(WITH_RADOSGW_AMQP_ENDPOINT "Rados Gateway's pubsub support for AMQP push endpoint" ON)
+option(WITH_RADOSGW_KAFKA_ENDPOINT "Rados Gateway's pubsub support for Kafka push endpoint" ON)
 
 if(WITH_RADOSGW)
   find_package(EXPAT REQUIRED)

ceph.spec.in
@@ -35,12 +35,14 @@
 %bcond_without libradosstriper
 %bcond_without ocf
 %bcond_without amqp_endpoint
+%bcond_without kafka_endpoint
 %global _remote_tarball_prefix https://download.ceph.com/tarballs/
 %endif
 %if 0%{?suse_version}
 %bcond_with selinux
 %bcond_with cephfs_java
 %bcond_with amqp_endpoint
+%bcond_with kafka_endpoint
 #Compat macro for new _fillupdir macro introduced in Nov 2017
 %if ! %{defined _fillupdir}
 %global _fillupdir /var/adm/fillup-templates
@@ -193,6 +195,9 @@ BuildRequires:	xmlstarlet
 BuildRequires:	yasm
 %if 0%{with amqp_endpoint}
 BuildRequires:  librabbitmq-devel
+%endif
+%if 0%{with kafka_endpoint}
+BuildRequires:  librdkafka-devel
 %endif
 %if 0%{with make_check}
 BuildRequires:  jq
@@ -1216,6 +1221,11 @@ ${CMAKE} .. \
     -DWITH_RADOSGW_AMQP_ENDPOINT=ON \
 %else
     -DWITH_RADOSGW_AMQP_ENDPOINT=OFF \
+%endif
+%if 0%{with kafka_endpoint}
+    -DWITH_RADOSGW_KAFKA_ENDPOINT=ON \
+%else
+    -DWITH_RADOSGW_KAFKA_ENDPOINT=OFF \
 %endif
     -DBOOST_J=$CEPH_SMP_NCPUS \
     -DWITH_GRAFANA=ON

cmake/modules/FindRDKafka.cmake
@@ -1,0 +1,19 @@
+find_path(rdkafka_INCLUDE_DIR
+  NAMES librdkafka/rdkafka.h)
+
+find_library(rdkafka_LIBRARY
+  NAMES rdkafka)
+
+include(FindPackageHandleStandardArgs)
+
+find_package_handle_standard_args(RDKafka DEFAULT_MSG
+  rdkafka_INCLUDE_DIR
+  rdkafka_LIBRARY)
+
+if(RDKafka_FOUND AND NOT (TARGET RDKafka::RDKafka))
+  add_library(RDKafka::RDKafka UNKNOWN IMPORTED)
+  set_target_properties(RDKafka::RDKafka PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES "${rdkafka_INCLUDE_DIR}"
+    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+    IMPORTED_LOCATION "${rdkafka_LIBRARY}")
+endif()

debian/control
@@ -50,6 +50,7 @@ Build-Depends: cmake (>= 3.5),
                libnl-genl-3-dev,
                libxml2-dev,
                librabbitmq-dev,
+               librdkafka-dev,
                lsb-release,
                parted,
                patch,

do_freebsd.sh
@@ -36,6 +36,7 @@ fi
 	-D CMAKE_C_FLAGS_DEBUG="$C_FLAGS_DEBUG" \
 	-D ENABLE_GIT_VERSION=OFF \
 	-D WITH_RADOSGW_AMQP_ENDPOINT=OFF \
+	-D WITH_RADOSGW_KAFKA_ENDPOINT=OFF \
 	-D WITH_SYSTEM_BOOST=ON \
 	-D WITH_SYSTEM_NPM=ON \
 	-D WITH_LTTNG=OFF \

src/include/config-h.in.cmake
@@ -345,6 +345,9 @@
 /* Defined if rabbitmq-c is available for rgw amqp push endpoint */
 #cmakedefine WITH_RADOSGW_AMQP_ENDPOINT
 
+/* Defined if libedkafka is available for rgw kafka push endpoint */
+#cmakedefine WITH_RADOSGW_KAFKA_ENDPOINT
+
 /* Defined if std::map::merge() is supported */
 #cmakedefine HAVE_STDLIB_MAP_SPLICING
 

src/rgw/CMakeLists.txt
@@ -125,6 +125,9 @@ set(librgw_common_srcs
 if(WITH_RADOSGW_AMQP_ENDPOINT)
   list(APPEND librgw_common_srcs rgw_amqp.cc)
 endif()
+if(WITH_RADOSGW_KAFKA_ENDPOINT)
+  list(APPEND librgw_common_srcs rgw_kafka.cc)
+endif()
 
 add_library(rgw_common OBJECT ${librgw_common_srcs})
 
@@ -186,6 +189,9 @@ target_include_directories(rgw_a SYSTEM PUBLIC "../rapidjson/include")
 if(WITH_RADOSGW_AMQP_ENDPOINT)
   find_package(RabbitMQ REQUIRED)
 endif()
+if(WITH_RADOSGW_KAFKA_ENDPOINT)
+  find_package(RDKafka REQUIRED)
+endif()
 
 target_link_libraries(rgw_a
   PRIVATE
@@ -210,6 +216,10 @@ set(rgw_libs rgw_a)
 if(WITH_RADOSGW_AMQP_ENDPOINT)
   # used by rgw_amqp.cc
   list(APPEND rgw_libs RabbitMQ::RabbitMQ)
+endif()
+if(WITH_RADOSGW_KAFKA_ENDPOINT)
+  # used by rgw_kafka.cc
+  list(APPEND rgw_libs RDKafka::RDKafka)
 endif()
 
 set(radosgw_srcs
@@ -318,6 +328,10 @@ if(WITH_RADOSGW_AMQP_ENDPOINT)
   target_link_libraries(rgw PRIVATE RabbitMQ::RabbitMQ)
 endif()
 
+if(WITH_RADOSGW_KAFKA_ENDPOINT)
+  target_link_libraries(rgw PRIVATE RDKafka::RDKafka)
+endif()
+
 set_target_properties(rgw PROPERTIES OUTPUT_NAME rgw VERSION 2.0.0
   SOVERSION 2)
 install(TARGETS rgw DESTINATION ${CMAKE_INSTALL_LIBDIR})
@@ -352,6 +366,9 @@ set_target_properties(rgw_admin_user PROPERTIES OUTPUT_NAME rgw_admin_user VERSI
 install(TARGETS rgw_admin_user DESTINATION ${CMAKE_INSTALL_LIBDIR})
 if(WITH_RADOSGW_AMQP_ENDPOINT)
   target_link_libraries(rgw_admin_user PRIVATE RabbitMQ::RabbitMQ)
+endif()
+if(WITH_RADOSGW_KAFKA_ENDPOINT)
+  target_link_libraries(rgw_admin_user PRIVATE RDKafka::RDKafka)
 endif()
 if(WITH_BOOST_CONTEXT)
   target_link_libraries(rgw_admin_user PRIVATE Boost::coroutine Boost::context)

commit a40c42bd4838fb65f890e4a9abe8105b4c4a4e24

do_cmake.sh: fix application of -DWITH_RADOSGW_KAFKA_ENDPOINT=OFF
50ba25b2aa4db34319a7b95e789065540347f895 broke do_cmake.sh in nautilus
and the backport of 46b3d671b4408a766aaed379274242ad4b38afb3 (which
was included in the same PR) did not fix it enough.

This commit cannot be backported from master. It is needed to fix
a partially broken backport.

After this commit is applied, the relevant part of do_cmake.sh in
nautilus will look exactly like it does now in master.

Fixes: 50ba25b2aa4db34319a7b95e789065540347f895
Fixes: https://tracker.ceph.com/issues/44641
Signed-off-by: Nathan Cutler <ncutler@suse.com>

do_cmake.sh
@@ -24,13 +24,13 @@ if [ -r /etc/os-release ]; then
       opensuse*|suse|sles)
           PYBUILD="3"
           ARGS+=" -DWITH_RADOSGW_AMQP_ENDPOINT=OFF"
-          WITH_RADOSGW_KAFKA_ENDPOINT="OFF"
+          ARGS+=" -DWITH_RADOSGW_KAFKA_ENDPOINT=OFF"
           ;;
   esac
 elif [ "$(uname)" == FreeBSD ] ; then
   PYBUILD="3"
   ARGS+=" -DWITH_RADOSGW_AMQP_ENDPOINT=OFF"
-  WITH_RADOSGW_KAFKA_ENDPOINT="OFF"
+  ARGS+=" -DWITH_RADOSGW_KAFKA_ENDPOINT=OFF"
 else
   echo Unknown release
   exit 1