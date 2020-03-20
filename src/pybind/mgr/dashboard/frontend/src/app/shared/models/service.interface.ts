479f6b69b5322800e3b429b2bb0f6f4afbcd7729
Sebastian Wagner <sebastian.wagner@suse.com>
2020-03-20 11:27:04 +0100

    mgr/dashboard: adapt to new `ServiceDescription.to_json()`
    `ServiceDescription.to_json()` is now based on `ServiceSpec.to_json()`
    
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/models/service.interface.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/models/service.interface.ts
@@ -1,8 +1,17 @@
-export interface CephService {
+export interface CephServiceStatus {
   container_image_id: string;
   container_image_name: string;
-  service_name: string;
   size: number;
   running: number;
   last_refresh: Date;
+  created: Date;
+}
+
+// This will become handy when creating arbitrary services
+export interface CephServiceSpec {
+  service_name: string;
+  service_type: string;
+  service_id: string;
+  unmanaged: boolean;
+  status: CephServiceStatus;
 }
