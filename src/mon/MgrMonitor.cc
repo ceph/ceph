ceph/ceph

commit a7677c76a54bdbc68ea9ba60efe7cec013d5afda

mon,mgr: improve 'mgr module disable/enable' cmds
when running those cmds, check if the module do exist
or is disabled already.
also check if the module is enabled already.

Signed-off-by: Gu Zhongyan <guzhongyan@360.cn>

src/mon/MgrMonitor.cc
@@ -897,6 +897,11 @@ bool MgrMonitor::prepare_command(MonOpRequestRef op)
       goto out;
     }
 
+    if (pending_map.module_enabled(module)) {
+      ss << "module '" << module << "' is already enabled";
+      r = 0;
+      goto out;
+    }
     pending_map.modules.insert(module);
   } else if (prefix == "mgr module disable") {
     string module;
@@ -904,6 +909,14 @@ bool MgrMonitor::prepare_command(MonOpRequestRef op)
     if (module.empty()) {
       r = -EINVAL;
       goto out;
+    }
+    if (!pending_map.module_enabled(module)) {
+      ss << "module '" << module << "' is already disabled";
+      r = 0;
+      goto out;
+    }
+    if (!pending_map.any_supports_module(module)) {
+      ss << "module '" << module << "' does not exist";
     }
     pending_map.modules.erase(module);
   } else {

src/mon/MgrMap.h
@@ -193,6 +193,23 @@ public:
     throw std::logic_error(oss.str());
   }
 
+  bool module_enabled(const std::string& module_name) const
+  {
+    return modules.find(module_name) != modules.end();
+  }
+
+  bool any_supports_module(const std::string& module) const {
+    if (have_module(module)) {
+      return true;
+    }
+    for (auto& p : standbys) {
+      if (p.second.have_module(module)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   bool have_name(const string& name) const {
     if (active_name == name) {
       return true;

commit 4bb2566ef5ca5ac02a5e6efa301ae9aaa3a99ce1

mon/MgrMonitor: show different error message when disabling a dne module
this is a regression introduced by
a7677c76a54bdbc68ea9ba60efe7cec013d5afda

per Sage, we can enable a module when the mgr is not started yet using
"--force" command, but we should be allowed to disable it if the
non-existent module is added. this change just changes the condition and
error message so that the error message is printed if the module is not
loaded at all. because the operation should be idempotent, so the
operation is always finished without returning an error.

Fixes: https://tracker.ceph.com/issues/44728
Signed-off-by: Kefu Chai <kchai@redhat.com>

src/mon/MgrMonitor.cc
@@ -1158,8 +1158,8 @@ bool MgrMonitor::prepare_command(MonOpRequestRef op)
       r = 0;
       goto out;
     }
-    if (!pending_map.any_supports_module(module)) {
-      ss << "module '" << module << "' does not exist";
+    if (!pending_map.modules.count(module)) {
+      ss << "module '" << module << "' is not enabled";
     }
     pending_map.modules.erase(module);
   } else {