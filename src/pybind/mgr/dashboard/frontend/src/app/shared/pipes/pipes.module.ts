d62f83638df763c9546814d59e0ecb8a6db911a6
Volker Theile <vtheile@suse.com>
2020-03-27 13:10:46 +0100

    mgr/dashboard: Unify Tasks and Notifications into a sidebar
    Fixes: https://tracker.ceph.com/issues/37402
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>
    (cherry picked from commit 7fdb3c1504a96a991041934c8abd6453d762857d)

--- src/pybind/mgr/dashboard/frontend/src/app/shared/pipes/pipes.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/pipes/pipes.module.ts
@@ -8,6 +8,7 @@ import { CephShortVersionPipe } from './ceph-short-version.pipe';
 import { DimlessBinaryPerSecondPipe } from './dimless-binary-per-second.pipe';
 import { DimlessBinaryPipe } from './dimless-binary.pipe';
 import { DimlessPipe } from './dimless.pipe';
+import { DurationPipe } from './duration.pipe';
 import { EmptyPipe } from './empty.pipe';
 import { EncodeUriPipe } from './encode-uri.pipe';
 import { FilterPipe } from './filter.pipe';
@@ -48,7 +49,8 @@ import { UpperFirstPipe } from './upper-first.pipe';
     NotAvailablePipe,
     IopsPipe,
     UpperFirstPipe,
-    RbdConfigurationSourcePipe
+    RbdConfigurationSourcePipe,
+    DurationPipe
   ],
   exports: [
     BooleanTextPipe,
@@ -72,7 +74,8 @@ import { UpperFirstPipe } from './upper-first.pipe';
     NotAvailablePipe,
     IopsPipe,
     UpperFirstPipe,
-    RbdConfigurationSourcePipe
+    RbdConfigurationSourcePipe,
+    DurationPipe
   ],
   providers: [
     BooleanTextPipe,
