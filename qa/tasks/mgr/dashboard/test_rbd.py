1c3d6539d93961f9f7bd5308771725410bde32ec
Kiefer Chang <kiefer.chang@suse.com>
2020-03-27 13:14:09 +0800

    qa/tasks/mgr/dashboard/test_rbd: wait longer when purging
    Fixes: https://tracker.ceph.com/issues/44743
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>
    Co-authored-by: Kefu Chai <kefu@redhat.com>
    Co-authored-by: Volker Theile <vtheile@suse.com>

--- qa/tasks/mgr/dashboard/test_rbd.py
+++ qa/tasks/mgr/dashboard/test_rbd.py
@@ -844,15 +844,14 @@ class RbdTest(DashboardTestCase):
         time.sleep(1)
 
         self.purge_trash('rbd')
-        self.assertStatus(200)
+        self.assertStatus([200, 201])
 
         time.sleep(1)
 
         trash_not_expired = self.get_trash('rbd', id_not_expired)
         self.assertIsNotNone(trash_not_expired)
 
-        trash_expired = self.get_trash('rbd', id_expired)
-        self.assertIsNone(trash_expired)
+        self.wait_until_equal(lambda: self.get_trash('rbd', id_expired), None, 60)
 
     def test_list_namespaces(self):
         self.create_namespace('rbd', 'ns')
