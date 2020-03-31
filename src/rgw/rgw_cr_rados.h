8d715d181403ae65172fb855b5f06c8885f360aa
Casey Bodley <cbodley@redhat.com>
2020-03-31 09:23:20 -0400

    rgw: SimpleRadosReadAttrsCR takes objv_tracker
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_cr_rados.h
+++ src/rgw/rgw_cr_rados.h
@@ -296,7 +296,6 @@ private:
 
 class RGWAsyncGetSystemObj : public RGWAsyncRadosRequest {
   RGWSysObjectCtx obj_ctx;
-  RGWObjVersionTracker objv_tracker;
   rgw_raw_obj obj;
   const bool want_attrs;
   const bool raw_attrs;
@@ -309,6 +308,7 @@ public:
 
   bufferlist bl;
   map<string, bufferlist> attrs;
+  RGWObjVersionTracker objv_tracker;
 };
 
 class RGWAsyncPutSystemObj : public RGWAsyncRadosRequest {
@@ -456,17 +456,20 @@ class RGWSimpleRadosReadAttrsCR : public RGWSimpleCoroutine {
   rgw_raw_obj obj;
   map<string, bufferlist> *pattrs;
   bool raw_attrs;
-  RGWAsyncGetSystemObj *req;
+  RGWObjVersionTracker* objv_tracker;
+  RGWAsyncGetSystemObj *req = nullptr;
 
 public:
   RGWSimpleRadosReadAttrsCR(RGWAsyncRadosProcessor *_async_rados, RGWSI_SysObj *_svc,
-		      const rgw_raw_obj& _obj,
-		      map<string, bufferlist> *_pattrs, bool _raw_attrs) : RGWSimpleCoroutine(_svc->ctx()),
-                                                async_rados(_async_rados), svc(_svc),
-						obj(_obj),
-                                                pattrs(_pattrs),
-						raw_attrs(_raw_attrs),
-                                                req(NULL) {}
+                            const rgw_raw_obj& _obj, map<string, bufferlist> *_pattrs,
+                            bool _raw_attrs, RGWObjVersionTracker* objv_tracker = nullptr)
+    : RGWSimpleCoroutine(_svc->ctx()),
+      async_rados(_async_rados), svc(_svc),
+      obj(_obj),
+      pattrs(_pattrs),
+      raw_attrs(_raw_attrs),
+      objv_tracker(objv_tracker)
+  {}
   ~RGWSimpleRadosReadAttrsCR() override {
     request_cleanup();
   }
