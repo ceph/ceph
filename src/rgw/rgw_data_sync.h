c191ce1b6eec170140a233b5e2f8ebe673b15bbc
Casey Bodley <cbodley@redhat.com>
2020-03-31 09:23:23 -0400

    rgw: use objv_tracker to read/write bucket sync status
    use cls_version on bucket sync status to detect racing writes - whether
    from other gateways, or from radosgw-admin commands like 'bucket sync'
    or 'bucket sync init'
    
    classes that require a non-null version tracker take it by reference
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_data_sync.h
+++ src/rgw/rgw_data_sync.h
@@ -603,7 +603,7 @@ public:
             const rgw_bucket& dest_bucket);
 
   RGWCoroutine *read_sync_status_cr(int num, rgw_bucket_shard_sync_info *sync_status);
-  RGWCoroutine *init_sync_status_cr(int num);
+  RGWCoroutine *init_sync_status_cr(int num, RGWObjVersionTracker& objv_tracker);
   RGWCoroutine *run_sync_cr(int num);
 
   int num_pipes() {
