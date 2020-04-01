3947375260ccadc89f041a2cf0db0a64bcec55fc
Kefu Chai <kchai@redhat.com>
2020-04-01 20:20:13 +0800

    pybind: s/xrange/range/
    fortunately, cython does not complain at seeing xrange, but let's drop
    the last bit of python2.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>
    (cherry picked from commit ad2bbbc1edb29d3623299fd25f72458623415a96)

--- src/pybind/rados/rados.pyx
+++ src/pybind/rados/rados.pyx
@@ -573,7 +573,7 @@ cdef size_t * to_csize_t_array(list_int):
     cdef size_t *ret = <size_t *>malloc(len(list_int) * sizeof(size_t))
     if ret == NULL:
         raise MemoryError("malloc failed")
-    for i in xrange(len(list_int)):
+    for i in range(len(list_int)):
         ret[i] = <size_t>list_int[i]
     return ret
 
@@ -582,7 +582,7 @@ cdef char ** to_bytes_array(list_bytes):
     cdef char **ret = <char **>malloc(len(list_bytes) * sizeof(char *))
     if ret == NULL:
         raise MemoryError("malloc failed")
-    for i in xrange(len(list_bytes)):
+    for i in range(len(list_bytes)):
         ret[i] = <char *>list_bytes[i]
     return ret
 
