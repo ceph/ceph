3b3689901a96be2c2e8b04a0d5748a18cdf98efd
Rishabh Dave <ridave@redhat.com>
2020-04-01 16:51:06 +0530

    qa/cephfs: add methods to read/write on CephFS mounts
    Signed-off-by: Rishabh Dave <ridave@redhat.com>
    (cherry picked from commit 3f0284f272231c3b62b0f3f201cbaaecfa405bcd)
    
    Conflicts:
        qa/tasks/cephfs/mount.py: get_file and IP module is not present in
            nautilus

--- qa/tasks/cephfs/mount.py
+++ qa/tasks/cephfs/mount.py
@@ -8,6 +8,7 @@ import time
 from six import StringIO
 from textwrap import dedent
 import os
+from teuthology.misc import sudo_write_file
 from teuthology.orchestra import run
 from teuthology.orchestra.run import CommandFailedError, ConnectionLostError, Raw
 from tasks.cephfs.filesystem import Filesystem
@@ -141,6 +142,29 @@ class CephFSMount(object):
             ], check_status=False)
             if r.exitstatus != 0:
                 raise RuntimeError("Expected file {0} not found".format(suffix))
+
+    def write_file(self, path, data, perms=None):
+        """
+        Write the given data at the given path and set the given perms to the
+        file on the path.
+        """
+        if path.find(self.mountpoint) == -1:
+            path = os.path.join(self.mountpoint, path)
+
+        sudo_write_file(self.client_remote, path, data)
+
+        if perms:
+            self.run_shell(args=f'chmod {perms} {path}')
+
+    def read_file(self, path):
+        """
+        Return the data from the file on given path.
+        """
+        if path.find(self.mountpoint) == -1:
+            path = os.path.join(self.mountpoint, path)
+
+        return self.run_shell(args=['sudo', 'cat', path], omit_sudo=False).\
+            stdout.getvalue().strip()
 
     def create_destroy(self):
         assert(self.is_mounted())
