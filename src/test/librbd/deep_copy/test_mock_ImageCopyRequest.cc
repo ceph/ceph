7aec4cb6fd6ff23eb7c5a1fa72833eea2d6a0168
Jason Dillaman <dillaman@redhat.com>
2020-04-03 15:24:14 -0400

    librbd: pass bytes-read statistics from deep-copy through handler
    These simple stats will be utilized by rbd-mirror to compute throughput
    metrics for snapshot-based mirroring.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/test/librbd/deep_copy/test_mock_ImageCopyRequest.cc
+++ src/test/librbd/deep_copy/test_mock_ImageCopyRequest.cc
@@ -55,7 +55,8 @@ struct ObjectCopyRequest<librbd::MockTestImageCtx> {
       librados::snap_t src_snap_id_start,
       librados::snap_t dst_snap_id_start,
       const SnapMap &snap_map,
-      uint64_t object_number, bool flatten, Context *on_finish) {
+      uint64_t object_number, bool flatten, Handler* handler,
+      Context *on_finish) {
     ceph_assert(s_instance != nullptr);
     std::lock_guard locker{s_instance->lock};
     s_instance->snap_map = &snap_map;
