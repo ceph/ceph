7d357a274e575051d182f901e08f03eeec6c6ad4
Yuan Lu <yuan.y.lu@intel.com>
2020-04-14 14:57:23 +0800

    librbd: add ReadRequest to handle read
    Signed-off-by: Peterson, Scott <scott.d.peterson@intel.com>
    Signed-off-by: Li, Xiaoyan <xiaoyan.li@intel.com>
    Signed-off-by: Lu, Yuan <yuan.y.lu@intel.com>
    Signed-off-by: Chamarthy, Mahati <mahati.chamarthy@intel.com>

--- src/librbd/cache/rwl/Request.h
+++ src/librbd/cache/rwl/Request.h
@@ -231,6 +231,31 @@ private:
                                   const C_FlushRequest<U> &req);
 };
 
+class C_ReadRequest : public Context {
+public:
+  io::Extents miss_extents; // move back to caller
+  ImageExtentBufs read_extents;
+  bufferlist miss_bl;
+
+  C_ReadRequest(CephContext *cct, utime_t arrived, PerfCounters *perfcounter, bufferlist *out_bl, Context *on_finish)
+    : m_cct(cct), m_on_finish(on_finish), m_out_bl(out_bl),
+      m_arrived_time(arrived), m_perfcounter(perfcounter) {}
+  ~C_ReadRequest() {}
+
+  void finish(int r) override;
+
+  const char *get_name() const {
+    return "C_ReadRequest";
+  }
+
+private:
+  CephContext *m_cct;
+  Context *m_on_finish;
+  bufferlist *m_out_bl;
+  utime_t m_arrived_time;
+  PerfCounters *m_perfcounter;
+};
+
 struct BlockGuardReqState {
   bool barrier = false; /* This is a barrier request */
   bool current_barrier = false; /* This is the currently active barrier */
