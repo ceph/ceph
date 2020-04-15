f1a360428c0ad19847899617f581f6bfe5da5356
Yan, Zheng <zyan@redhat.com>
2020-04-15 18:49:36 +0800

    mds: don't shallow copy when decoding xattr map
    Otherwise inodes' xattr maps may reference a large shared buffer
    (from omap fetch or journal read). If mds trims and loads inode
    repeatly, each inode can reference different large buffer in the
    worst case.
    
    Fixes: https://tracker.ceph.com/issues/45090
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>
    (cherry picked from commit deff94c8f43bb3734b688ccb828d942b8f150638)

--- src/mds/CInode.h
+++ src/mds/CInode.h
@@ -104,6 +104,12 @@ public:
   damage_flags_t damage_flags = 0;
 };
 
+inline void decode_noshare(InodeStoreBase::mempool_xattr_map& xattrs,
+                          ceph::buffer::list::const_iterator &p)
+{
+  decode_noshare<mempool::mds_co::pool_allocator>(xattrs, p);
+}
+
 class InodeStore : public InodeStoreBase {
 public:
   void encode(bufferlist &bl, uint64_t features) const {
