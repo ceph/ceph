561985d30fe6a725205c20cdff6b22e7d6ebd1d2
Jan Fajerski <jfajerski@suse.com>
2020-04-16 11:49:31 +0200

    pybing/mgr/mgr_module: allow keyword arguments
    If a mgr module encounters an argument values containing a '=' parsing
    switches to kwargs style parsing independent of arg position. If a
    non-kw argument is encountered after the first kw argument an EINVAL
    error is returned.
    
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>
    (cherry picked from commit ee9dea6cbf9879208ca88786e7f3a944d479e9ed)
    (cherry picked from commit d65ea64bb48faad3656c7a88cf8eee438caf5dc2)

--- src/pybind/mgr/mgr_module.py
+++ src/pybind/mgr/mgr_module.py
@@ -310,10 +310,21 @@ class CLICommand(object):
 
     def call(self, mgr, cmd_dict, inbuf):
         kwargs = {}
+        kwargs_switch = False
         for a, d in self.args_dict.items():
             if 'req' in d and d['req'] == "false" and a not in cmd_dict:
                 continue
-            kwargs[a.replace("-", "_")] = cmd_dict[a]
+            if kwargs_switch or (isinstance(cmd_dict[a], str) and '=' in cmd_dict[a]):
+                mgr.log.debug('found kwarg, assuming all following args are kw style')
+                kwargs_switch = True
+                try:
+                    k, arg = cmd_dict[a].split('=')
+                except ValueError as e:
+                    mgr.log.error('found positional arg after switching to kwarg parsing')
+                    return -errno.EINVAL, '', 'Error EINVAL: postitional arg not allowed after kwarg'
+                kwargs[k.replace("-", "_")] = arg
+            else:
+                kwargs[a.replace("-", "_")] = cmd_dict[a]
         if inbuf:
             kwargs['inbuf'] = inbuf
         assert self.func
