222435319f2d7c457193c8e14c6123ccb0b2be28
Casey Bodley <cbodley@redhat.com>
2020-04-17 10:49:53 -0400

    rgw: combine calls to global_init/pre_init
    calling global_init() without 'run_pre_init=false' will call
    global_pre_init() for us
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit d12c1bed88bc93846de022bcdd55d596199247e2)

--- src/rgw/rgw_main.cc
+++ src/rgw/rgw_main.cc
@@ -203,17 +203,9 @@ int radosgw_Main(int argc, const char **argv)
   }
 
   int flags = CINIT_FLAG_UNPRIVILEGED_DAEMON_DEFAULTS;
-  global_pre_init(
-    &defaults, args, CEPH_ENTITY_TYPE_CLIENT, CODE_ENVIRONMENT_DAEMON,
-    flags);
-
-  // Now that we've determined which frontend(s) to use, continue with global
-  // initialization. Passing false as the final argument ensures that
-  // global_pre_init() is not invoked twice.
-  // claim the reference and release it after subsequent destructors have fired
   auto cct = global_init(&defaults, args, CEPH_ENTITY_TYPE_CLIENT,
 			 CODE_ENVIRONMENT_DAEMON,
-			 flags, "rgw_data", false);
+			 flags, "rgw_data");
 
   // First, let's determine which frontends are configured.
   list<string> frontends;
