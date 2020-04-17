476b718e9b201a68a3fada1117542f14ee2620a8
Jason Dillaman <dillaman@redhat.com>
2020-04-17 10:02:10 -0400

    common: add helper C_TrackerOp context class
    This wraps the functionality of starting and finishing a tracked op
    into the standard context interface.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>
    (cherry picked from commit 4bd9d1501f3832206ef12155cec3f008e3160822)

--- src/common/AsyncOpTracker.h
+++ src/common/AsyncOpTracker.h
@@ -5,8 +5,7 @@
 #define CEPH_ASYNC_OP_TRACKER_H
 
 #include "common/ceph_mutex.h"
-
-struct Context;
+#include "include/Context.h"
 
 class AsyncOpTracker {
 public:
@@ -25,6 +24,25 @@ private:
   uint32_t m_pending_ops = 0;
   Context *m_on_finish = nullptr;
 
+};
+
+class C_TrackedOp : public Context {
+public:
+  C_TrackedOp(AsyncOpTracker& async_op_tracker, Context* on_finish)
+    : m_async_op_tracker(async_op_tracker), m_on_finish(on_finish) {
+    m_async_op_tracker.start_op();
+  }
+
+  void finish(int r) override {
+    if (m_on_finish != nullptr) {
+      m_on_finish->complete(r);
+    }
+    m_async_op_tracker.finish_op();
+  }
+
+private:
+  AsyncOpTracker& m_async_op_tracker;
+  Context* m_on_finish;
 };
 
 #endif // CEPH_ASYNC_OP_TRACKER_H
