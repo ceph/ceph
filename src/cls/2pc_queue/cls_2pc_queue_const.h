ad56f9e826e79b5d78493950438c39fb2717b242
Yuval Lifshitz <ylifshit@redhat.com>
2020-04-21 19:15:42 +0300

    cls/2pc_queue: remove the dependency of cls_2pc_queue with cls_queue
    both queues share the same code base, however, cls_2pc_queue should work
    even if cls_queue objectclass is not loaded into the osd
    
    Signed-off-by: Yuval Lifshitz <ylifshit@redhat.com>

--- src/cls/2pc_queue/cls_2pc_queue_const.h
+++ src/cls/2pc_queue/cls_2pc_queue_const.h
@@ -3,8 +3,11 @@
 #define TPC_QUEUE_CLASS "2pc_queue"
 
 #define TPC_QUEUE_INIT "2pc_queue_init"
+#define TPC_QUEUE_GET_CAPACITY "2pc_queue_get_capacity"
 #define TPC_QUEUE_RESERVE "2pc_queue_reserve"
 #define TPC_QUEUE_COMMIT "2pc_queue_commit"
 #define TPC_QUEUE_ABORT "2pc_queue_abort"
 #define TPC_QUEUE_LIST_RESERVATIONS "2pc_queue_list_reservations"
+#define TPC_QUEUE_LIST_ENTRIES "2pc_queue_list_entries"
+#define TPC_QUEUE_REMOVE_ENTRIES "2pc_queue_remove_entries"
 
