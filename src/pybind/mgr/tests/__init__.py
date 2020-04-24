6b029c09951d002fa6bf04390920f1232390fb2f
Sebastian Wagner <sebastian.wagner@suse.com>
2020-04-24 11:22:28 +0200

    mgr/dashboard: Add dashboard to mon_command reference docs
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- src/pybind/mgr/tests/__init__.py
+++ src/pybind/mgr/tests/__init__.py
@@ -36,3 +36,22 @@ if 'UNITTEST' in os.environ:
     cm.BaseMgrModule = M
     cm.BaseMgrStandbyModule = M
     sys.modules['ceph_module'] = cm
+
+    def mock_ceph_modules():
+        class MockRadosError(Exception):
+            def __init__(self, message, errno=None):
+                super(MockRadosError, self).__init__(message)
+                self.errno = errno
+
+            def __str__(self):
+                msg = super(MockRadosError, self).__str__()
+                if self.errno is None:
+                    return msg
+                return '[errno {0}] {1}'.format(self.errno, msg)
+
+
+        sys.modules.update({
+            'rados': mock.Mock(Error=MockRadosError, OSError=MockRadosError),
+            'rbd': mock.Mock(),
+            'cephfs': mock.Mock(),
+        })
