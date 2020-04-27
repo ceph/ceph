884d9f3c925059b5e0fc7746f2fc6af306f2b633
Kiefer Chang <kiefer.chang@suse.com>
2020-04-27 16:08:22 +0800

    mgr/dashboard: allow getting fresh inventory data from the orchestrator
    When there is a device change, a `ceph orch device ls --refresh` command
    needs to be called so the orchestrator can invalidate its cache and
    refresh all devices on all nodes. Currently, the call is asynchronous and
    there is no way to determine is a refresh is done or not.
    
    To allow doing a refresh in the Dashboard:
    - The inventory device list is periodically updated with cached data.
    - If the user clicks the refresh button, a refresh call is sent to the
      orchestrator. Thus if there are device changes, it will be revealed soon
      because of the periodical update.
    
    Fixes: https://tracker.ceph.com/issues/44803
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>
    (cherry picked from commit 1147d6dca04fe6e45b385b549db37775fce54edd)
    
     Conflicts:
    	src/pybind/mgr/dashboard/controllers/orchestrator.py
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/inventory/inventory.component.ts
      Do not remove wraps decorator

--- src/pybind/mgr/dashboard/controllers/orchestrator.py
+++ src/pybind/mgr/dashboard/controllers/orchestrator.py
@@ -10,8 +10,13 @@ from .. import mgr
 from ..exceptions import DashboardException
 from ..security import Scope
 from ..services.exception import handle_orchestrator_error
-from ..services.orchestrator import OrchClient
-from ..tools import TaskManager, wraps
+from ..services.orchestrator import OrchClient, OrchFeature
+from ..tools import TaskManager, str_to_bool, wraps
+
+STATUS_SCHEMA = {
+    "available": (bool, "Orchestrator status"),
+    "description": (str, "Description")
+}
 
 
 def get_device_osd_map():
@@ -101,11 +106,14 @@ class Orchestrator(RESTController):
 @ApiController('/orchestrator/inventory', Scope.HOSTS)
 class OrchestratorInventory(RESTController):
 
-    @raise_if_no_orchestrator
-    def list(self, hostname=None):
+    @raise_if_no_orchestrator([OrchFeature.DEVICE_LIST])
+    def list(self, hostname=None, refresh=None):
         orch = OrchClient.instance()
         hosts = [hostname] if hostname else None
-        inventory_hosts = [host.to_json() for host in orch.inventory.list(hosts)]
+        do_refresh = False
+        if refresh is not None:
+            do_refresh = str_to_bool(refresh)
+        inventory_hosts = [host.to_json() for host in orch.inventory.list(hosts, do_refresh)]
         device_osd_map = get_device_osd_map()
         for inventory_host in inventory_hosts:
             host_osds = device_osd_map.get(inventory_host['name'])
