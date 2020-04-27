74e52ea24f626df7717c3843b5963d549a22b85a
Jan Fajerski <jfajerski@suse.com>
2020-04-27 11:35:51 +0200

    ceph-volume: Device - available_lvm if 10 extents are free.
    This changes the available_lvm notion to only require 10 free extents
    instead of 5GB.
    
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>
    (cherry picked from commit 6cb0841658ae45f23c2457a8f6a489457012d93e)

--- src/ceph-volume/ceph_volume/util/device.py
+++ src/ceph-volume/ceph_volume/util/device.py
@@ -477,9 +477,9 @@ class Device(object):
     def _check_lvm_reject_reasons(self):
         rejected = []
         if self.vgs:
-            available_vgs = [vg for vg in self.vgs if vg.free >= 5368709120]
+            available_vgs = [vg for vg in self.vgs if int(vg.vg_free_count) > 10]
             if not available_vgs:
-                rejected.append('Insufficient space (<5GB) on vgs')
+                rejected.append('Insufficient space (<10 extents) on vgs')
         else:
             # only check generic if no vgs are present. Vgs might hold lvs and
             # that might cause 'locked' to trigger
