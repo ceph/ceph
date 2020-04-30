7618b75d81617d2d90c8c38ad3dff095d3c870d4
Vasu Kulkarni <vasu@redhat.com>
2020-04-30 13:23:45 -0700

    Fixes to install task to handle extra system pkgs
    Signed-off-by: Vasu Kulkarni <vasu@redhat.com>
    
    Fix unit test cases
    
    Signed-off-by: Vasu Kulkarni <vasu@redhat.com>

--- teuthology/task/install/redhat.py
+++ teuthology/task/install/redhat.py
@@ -57,6 +57,10 @@ def install(ctx, config):
     log.info("using yaml path %s", yaml_path)
     downstream_config = yaml.safe_load(open(yaml_path))
     rh_versions = downstream_config.get('versions', dict()).get('supported', [])
+    external_config = dict(extra_system_packages=config.get('extra_system_packages', {}),
+                           extra_packages=config.get('extra_packages', {}),
+                           )
+    downstream_config.update(external_config)
     version = config['rhbuild']
     if version in rh_versions:
         log.info("%s is a supported version", version)
@@ -70,8 +74,7 @@ def install(ctx, config):
             else:
                 log.info("Install on Ubuntu node: %s", remote.shortname)
                 p.spawn(install_deb_pkgs, ctx, remote, version,
-                        downstream_config, config['deb-repo'],
-                        config['deb-gpg-key'])
+                        downstream_config)
     try:
         yield
     finally:
@@ -92,7 +95,9 @@ def install_pkgs(ctx, remote, version, downstream_config):
     :param downstream_config the dict object that has downstream pkg info
     """
     rh_version_check = downstream_config.get('versions').get('rpm').get('mapped')
-    rh_rpm_pkgs = downstream_config.get('pkgs').get('rpm')
+    rh_rpm_pkgs = downstream_config.get('pkgs').get('rpm') + \
+                  downstream_config.get('extra_system_packages').get('rpm', []) + \
+                  downstream_config.get('extra_packages').get('rpm', [])
     pkgs = str.join(' ', rh_rpm_pkgs)
     log.info("Remove any epel packages installed on node %s", remote.shortname)
     # below packages can come from epel and still work, ensure we use cdn pkgs
@@ -104,6 +109,7 @@ def install_pkgs(ctx, remote, version, downstream_config):
             run.Raw("leveldb xmlstarlet fcgi"),
             '-y'],
         check_status=False)
+
     log.info("Installing redhat ceph packages")
     remote.run(args=['sudo', 'yum', '-y', 'install',
                      run.Raw(pkgs)])
@@ -157,21 +163,18 @@ def install_deb_pkgs(
         ctx,
         remote,
         version,
-        downstream_config,
-        deb_repo,
-        deb_gpg_key):
+        downstream_config):
     """
     Setup debian repo, Install gpg key
     and Install on debian packages
     : param ctx
     : param remote
     : param downstream_config the dict object that has downstream pkg info
-    : deb_repo - http path of downstream ubuntu repo
-    : deb_gpg_key - gpg key for the ubuntu pkg
     """
-    set_deb_repo(remote, deb_repo, deb_gpg_key)
     rh_version_check = downstream_config.get('versions').get('deb').get('mapped')
-    rh_deb_pkgs = downstream_config.get('pkgs').get('deb')
+    rh_deb_pkgs = downstream_config.get('pkgs').get('deb') + \
+                  downstream_config.get('extra_system_packages').get('deb', []) + \
+                  downstream_config.get('extra_packages').get('deb', [])
     pkgs = str.join(' ', rh_deb_pkgs)
     log.info("Installing redhat ceph packages")
     remote.run(args=['sudo', 'apt-get', '-y', 'install',
