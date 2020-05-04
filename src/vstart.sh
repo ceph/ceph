f81fe362456865265f79b7b13b726fc0fdb6ef54
Varsha Rao <varao@redhat.com>
2020-05-04 19:35:47 +0530

    vstart: Update details about ganesha packages
    Signed-off-by: Varsha Rao <varao@redhat.com>

--- src/vstart.sh
+++ src/vstart.sh
@@ -1064,11 +1064,12 @@ EOF
 }
 
 # Ganesha Daemons requires nfs-ganesha nfs-ganesha-ceph nfs-ganesha-rados-grace
-# (version 2.7.6-2 and above) packages installed. On Fedora>=30 these packages
-# can be installed directly with 'dnf'. For CentOS>=8 the packages need to be
-# downloaded first from  https://download.nfs-ganesha.org/2.7/2.7.6/CentOS/ and
-# then install it. Similarly for Ubuntu 16.04 follow the instructions on
-# https://launchpad.net/~nfs-ganesha/+archive/ubuntu/nfs-ganesha-2.7
+# nfs-ganesha-rados-urls (version 2.8.3 and above) packages installed. On
+# Fedora>=31 these packages can be installed directly with 'dnf'. For CentOS>=8
+# the packages need to be downloaded first from
+# https://download.nfs-ganesha.org/2.8/2.8.3/CentOS and then installed.
+# Similarly for Ubuntu 16.04 follow the instructions on
+# https://launchpad.net/~nfs-ganesha/+archive/ubuntu/nfs-ganesha-2.8
 
 start_ganesha() {
     GANESHA_PORT=$(($CEPH_PORT + 4000))
