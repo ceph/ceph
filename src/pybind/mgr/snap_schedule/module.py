6c63b353ea9ce1b2e6e5081e9b087cf07650ac06
Jan Fajerski <jfajerski@suse.com>
2020-05-06 17:09:33 +0200

    pybind/mgr/snap-schedule: list - print ws separated results
    This should make it easy to consume for cli tools.
    Also simplifies some internals.
    
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>

--- src/pybind/mgr/snap_schedule/module.py
+++ src/pybind/mgr/snap_schedule/module.py
@@ -6,10 +6,9 @@ LGPL2.1.  See file COPYING.
 import errno
 import json
 import sqlite3
-from .fs.schedule import SnapSchedClient, Schedule
+from .fs.schedule import SnapSchedClient
 from mgr_module import MgrModule, CLIReadCommand, CLIWriteCommand
 from mgr_util import CephfsConnectionException
-from rados import ObjectNotFound
 from threading import Event
 
 
@@ -76,7 +75,7 @@ class Module(MgrModule):
             return e.to_tuple()
         if not scheds:
             return errno.ENOENT, '', f'SnapSchedule for {path} not found'
-        return 0, json.dumps([[sched[1], sched[2]] for sched in scheds]), ''
+        return 0, '\n'.join([str(sched) for sched in scheds]), ''
 
     @CLIWriteCommand('fs snap-schedule add',
                      'name=path,type=CephString '
@@ -96,9 +95,9 @@ class Module(MgrModule):
         try:
             use_fs = fs if fs else self.default_fs
             abs_path = self.resolve_subvolume_path(fs, subvol, path)
-            sched = Schedule(abs_path, snap_schedule, retention_policy,
-                             start, use_fs, subvol, path)
-            self.client.store_snap_schedule(use_fs, sched)
+            self.client.store_snap_schedule(use_fs, (abs_path, snap_schedule,
+                                                     retention_policy, start,
+                                                     use_fs, subvol, path))
             suc_msg = f'Schedule set for path {path}'
         except sqlite3.IntegrityError:
             existing_scheds = self.client.get_snap_schedules(use_fs, path)
