30aff90e8d5aeee80b800a5bc091927f07138190
Sebastian Wagner <sebastian.wagner@suse.com>
2020-05-08 13:00:36 +0200

    mgr/cephadm: add extra function for the auth entity
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

--- src/pybind/mgr/cephadm/utils.py
+++ src/pybind/mgr/cephadm/utils.py
@@ -13,3 +13,18 @@ def name_to_config_section(name):
         return name
     else:
         return 'mon'
+
+
+def name_to_auth_entity(name) -> str:
+    """
+    Map from daemon names to ceph entity names (as seen in config)
+    """
+    daemon_type = name.split('.', 1)[0]
+    if daemon_type in ['rgw', 'rbd-mirror', 'nfs', 'crash', 'iscsi']:
+        return 'client.' + name
+    elif daemon_type == 'mon':
+        return 'mon.'
+    elif daemon_type in ['osd', 'mds', 'mgr', 'client']:
+        return name
+    else:
+        raise OrchestratorError("unknown auth entity name")
