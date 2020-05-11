119851ec89745026c273334efbcbf2d50128c99d
Ernesto Puerta <epuertat@redhat.com>
2020-05-11 20:33:25 +0200

    mgr/dashboard: work with RBD images v1
    Add support for RBD Image Format v1:
    - This format lacks ID field, required for dashboard. Instead,
    RBD image `block_name_prefix` is used as unique ID (together with pool
    id and namespace)
    - Additionally, `image_format` is now exposed.
    - In the front-end side:
      - Copy action on a v1 image will cause the image to be copied to v2
    format.
      - List doesn't allow Move to Trash on v1 images,
      - Details section now shows `image_format` for images,
      - Edit Form disables flags not supported for v1 (`deep-flatten`,
    `layering`, `exclusive-lock`).
      - Protect does not work on v1 images or v2 images created from v1
    ones.
    
    Fixes: https://tracker.ceph.com/issues/36354
    Signed-off-by: Ernesto Puerta <epuertat@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-list/rbd-model.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/rbd-list/rbd-model.ts
@@ -1,8 +1,15 @@
 export class RbdModel {
   id: string;
+  unique_id: string;
   name: string;
   pool_name: string;
   namespace: string;
+  image_format: RBDImageFormat;
 
   cdExecuting: string;
+}
+
+export enum RBDImageFormat {
+  V1 = 1,
+  V2 = 2
 }
