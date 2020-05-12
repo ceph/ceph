efef459f90b28e9c87ce31a6d9ffaf6378d37340
Yuan Lu <yuan.y.lu@intel.com>
2020-05-12 17:23:13 +0800

    librbd: split ReadRequest
    Signed-off-by: Peterson, Scott <scott.d.peterson@intel.com>
    Signed-off-by: Li, Xiaoyan <xiaoyan.li@intel.com>
    Signed-off-by: Lu, Yuan <yuan.y.lu@intel.com>
    Signed-off-by: Chamarthy, Mahati <mahati.chamarthy@intel.com>

--- src/librbd/cache/ReplicatedWriteLog.cc
+++ src/librbd/cache/ReplicatedWriteLog.cc
@@ -17,6 +17,7 @@
 #include "librbd/asio/ContextWQ.h"
 #include "librbd/cache/rwl/ImageCacheState.h"
 #include "librbd/cache/rwl/LogEntry.h"
+#include "librbd/cache/rwl/ReadRequest.h"
 #include "librbd/cache/rwl/Types.h"
 #include <map>
 #include <vector>
