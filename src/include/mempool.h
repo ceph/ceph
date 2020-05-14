bb488df5e4ffa1ec2b5d25d9af0ce7f5093d7470
Adam Kupczyk <akupczyk@redhat.com>
2020-05-14 07:24:15 -0400

    common/mempool: More detailed bluestore stats
    Splits bluestore bluestore_cache_other, exposes accounting of some internals.
    
    Signed-off-by: Adam Kupczyk <akupczyk@redhat.com>

--- src/include/mempool.h
+++ src/include/mempool.h
@@ -153,6 +153,11 @@ namespace mempool {
   f(bluestore_cache_onode)	      \
   f(bluestore_cache_meta)	      \
   f(bluestore_cache_other)	      \
+  f(bluestore_Buffer)		      \
+  f(bluestore_Extent)		      \
+  f(bluestore_Blob)		      \
+  f(bluestore_SharedBlob)	      \
+  f(bluestore_inline_bl)	      \
   f(bluestore_fsck)		      \
   f(bluestore_txc)		      \
   f(bluestore_writing_deferred)      \
