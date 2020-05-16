6219c4433f25d9320d38ead7e0bd3fbc67433b44
Changcheng Liu <changcheng.liu@aliyun.com>
2020-05-16 14:30:43 +0800

    common/base64: restrucutre ceph_armor/unarmor
    Only restructure the original code structure to preapre
    to add architecture specific implementation.
    
    Signed-off-by: Changcheng Liu <changcheng.liu@intel.com>

---
+++ src/common/armor.cc
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2020 Intel Corporation.
+ * All rights reserved.
+ *
+ * Author: Changcheng Liu<changcheng.liu@intel.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "armor.h"
+#include "base64/base64_plain.h"
+
+base64_encode_func_t arch_encode_base64 = nullptr;
+base64_decode_func_t arch_decode_base64 = nullptr;
+
+/* choose best implementation based on the CPU architecture. */
+__attribute__ ((constructor))
+static void choose_base64_encode_decode(void) {
+  arch_encode_base64 = plain_armor;
+  arch_decode_base64 = plain_unarmor;
+}
