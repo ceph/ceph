004c1b18c148d355e4bb1e720cc27e50188d0fd4
Changcheng Liu <changcheng.liu@aliyun.com>
2020-05-18 14:27:32 +0800

    arch/intel: refine ISA SIMD feature detection
    1. cpuid.h has the corresponding MACRO to check SIMD feature.
    2. use __get_cpuid_max to detect usable instruction.
    3. use __cpuid_count to get ebx/ecx/edx
    4. replace '\t' indent with space
    
    Signed-off-by: Changcheng Liu <changcheng.liu@aliyun.com>

--- src/arch/intel.c
+++ src/arch/intel.c
@@ -13,6 +13,7 @@
  * 
  */
 #include <stdio.h>
+#include <stdint.h>
 #include "arch/probe.h"
 
 /* flags we export */
@@ -27,54 +28,47 @@ int ceph_arch_intel_aesni = 0;
 #ifdef __x86_64__
 #include <cpuid.h>
 
-/* http://en.wikipedia.org/wiki/CPUID#EAX.3D1:_Processor_Info_and_Feature_Bits */
-
-#define CPUID_PCLMUL	(1 << 1)
-#define CPUID_SSE42	(1 << 20)
-#define CPUID_SSE41	(1 << 19)
-#define CPUID_SSSE3	(1 << 9)
-#define CPUID_SSE3	(1)
-#define CPUID_SSE2	(1 << 26)
-#define CPUID_AESNI (1 << 25)
-
 int ceph_arch_intel_probe(void)
 {
-	/* i know how to check this on x86_64... */
-	unsigned int eax, ebx, ecx = 0, edx = 0;
-	if (!__get_cpuid(1, &eax, &ebx, &ecx, &edx)) {
-	  return 1;
-	}
-	if ((ecx & CPUID_PCLMUL) != 0) {
-		ceph_arch_intel_pclmul = 1;
-	}
-	if ((ecx & CPUID_SSE42) != 0) {
-		ceph_arch_intel_sse42 = 1;
-	}
-	if ((ecx & CPUID_SSE41) != 0) {
-		ceph_arch_intel_sse41 = 1;
-	}
-	if ((ecx & CPUID_SSSE3) != 0) {
-	        ceph_arch_intel_ssse3 = 1;
-	}
-	if ((ecx & CPUID_SSE3) != 0) {
-	        ceph_arch_intel_sse3 = 1;
-	}
-	if ((edx & CPUID_SSE2) != 0) {
-	        ceph_arch_intel_sse2 = 1;
-	}
-  if ((ecx & CPUID_AESNI) != 0) {
-          ceph_arch_intel_aesni = 1;
-  }
+    uint32_t eax = 0, ebx = 0, ecx = 0, edx = 0;
 
-	return 0;
+    uint32_t max_level = __get_cpuid_max(0, NULL);
+    if (max_level == 0) {
+        return 0;
+    }
+
+    __cpuid_count(1, 0, eax, ebx, ecx, edx);
+    if ((ecx & bit_PCLMUL) != 0) {
+        ceph_arch_intel_pclmul = 1;
+    }
+    if ((ecx & bit_SSE4_2) != 0) {
+        ceph_arch_intel_sse42 = 1;
+    }
+    if ((ecx & bit_SSE4_1) != 0) {
+        ceph_arch_intel_sse41 = 1;
+    }
+    if ((ecx & bit_SSSE3) != 0) {
+        ceph_arch_intel_ssse3 = 1;
+    }
+    if ((ecx & bit_SSE3) != 0) {
+        ceph_arch_intel_sse3 = 1;
+    }
+    if ((edx & bit_SSE2) != 0) {
+        ceph_arch_intel_sse2 = 1;
+    }
+    if ((ecx & bit_AES) != 0) {
+        ceph_arch_intel_aesni = 1;
+    }
+
+    return 0;
 }
 
 #else // __x86_64__
 
 int ceph_arch_intel_probe(void)
 {
-	/* no features */
-	return 0;
+    /* no features */
+    return 0;
 }
 
 #endif // __x86_64__
