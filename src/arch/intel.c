31737b0dd5bd9f7ee3e51d0f36394c27a2ee8ea9
Changcheng Liu <changcheng.liu@aliyun.com>
2020-05-18 19:34:29 +0800

    arch/intel: detect avx/avx2/avx512 feature
    Signed-off-by: Changcheng Liu <changcheng.liu@aliyun.com>

--- src/arch/intel.c
+++ src/arch/intel.c
@@ -24,9 +24,126 @@ int ceph_arch_intel_ssse3 = 0;
 int ceph_arch_intel_sse3 = 0;
 int ceph_arch_intel_sse2 = 0;
 int ceph_arch_intel_aesni = 0;
+int ceph_arch_intel_avx = 0;
+int ceph_arch_intel_avx2 = 0;
+int ceph_arch_intel_avx512f = 0;
+int ceph_arch_intel_avx512er = 0;
+int ceph_arch_intel_avx512pf = 0;
+int ceph_arch_intel_avx512vl = 0;
+int ceph_arch_intel_avx512cd = 0;
+int ceph_arch_intel_avx512dq = 0;
+int ceph_arch_intel_avx512bw = 0;
 
 #ifdef __x86_64__
 #include <cpuid.h>
+
+#ifndef _XCR_XFEATURE_ENABLED_MASK
+#define _XCR_XFEATURE_ENABLED_MASK 0
+#endif
+
+#ifndef _XCR_XMM_YMM_STATE_ENABLED_BY_OS
+#define _XCR_XMM_YMM_STATE_ENABLED_BY_OS 0x6
+#endif
+
+#ifndef _XCR_XMM_YMM_ZMM_STATE_ENABLED_BY_OS
+#define _XCR_XMM_YMM_ZMM_STATE_ENABLED_BY_OS \
+        ((0x3 << 5) | 0x6)
+#endif
+
+static inline int64_t _xgetbv(uint32_t index) {
+    uint32_t eax, edx;
+    __asm__ __volatile__("xgetbv" : "=a"(eax), "=d"(edx) : "c"(index));
+    return ((uint64_t)edx << 32) | eax;
+}
+
+static void detect_avx(void) {
+    uint32_t eax = 0, ebx = 0, ecx = 0, edx = 0;
+
+    uint32_t max_level = __get_cpuid_max(0, NULL);
+    if (max_level == 0) {
+        return;
+    }
+    __cpuid_count(1, 0, eax, ebx, ecx, edx);
+    if ((ecx & bit_OSXSAVE) == 0 || (ecx & bit_AVX) == 0) {
+        return;
+    }
+
+    int64_t xcr_mask = _xgetbv(_XCR_XFEATURE_ENABLED_MASK);
+    if (xcr_mask & _XCR_XMM_YMM_STATE_ENABLED_BY_OS) {
+        ceph_arch_intel_avx = 1;
+    }
+}
+
+static void detect_avx2(void) {
+    uint32_t eax = 0, ebx = 0, ecx = 0, edx = 0;
+
+    uint32_t max_level = __get_cpuid_max(0, NULL);
+    if (max_level == 0) {
+        return;
+    }
+    __cpuid_count(1, 0, eax, ebx, ecx, edx);
+    if ((ecx & bit_OSXSAVE) == 0 || (ecx & bit_AVX) == 0) {
+        return;
+    }
+    if (max_level < 7) {
+        return;
+    }
+    __cpuid_count(7, 0, eax, ebx, ecx, edx);
+    if ((ebx & bit_AVX2) == 0) {
+        return;
+    }
+    int64_t xcr_mask = _xgetbv(_XCR_XFEATURE_ENABLED_MASK);
+    if (xcr_mask & _XCR_XMM_YMM_STATE_ENABLED_BY_OS) {
+        ceph_arch_intel_avx2 = 1;
+    }
+}
+
+static void detect_avx512(void) {
+    uint32_t eax = 0, ebx = 0, ecx = 0, edx = 0;
+
+    uint32_t max_level = __get_cpuid_max(0, NULL);
+    if (max_level == 0) {
+        return;
+    }
+    __cpuid_count(1, 0, eax, ebx, ecx, edx);
+    if ((ecx & bit_OSXSAVE) == 0) {
+        return;
+    }
+    int64_t xcr_mask = _xgetbv(_XCR_XFEATURE_ENABLED_MASK);
+    if ((xcr_mask & _XCR_XMM_YMM_ZMM_STATE_ENABLED_BY_OS) == 0) {
+        return;
+    }
+    if (max_level < 7) {
+        return;
+    }
+    __cpuid_count(7, 0, eax, ebx, ecx, edx);
+    if ((ebx & bit_AVX512F) == 0) {
+        return;
+    }
+    ceph_arch_intel_avx512f = 1;
+
+    if(ebx & bit_AVX512ER) {
+        ceph_arch_intel_avx512er = 1;
+    }
+    if(ebx & bit_AVX512PF) {
+        ceph_arch_intel_avx512pf = 1;
+    }
+
+    if ((ebx & bit_AVX512VL)== 0) {
+        return;
+    }
+    ceph_arch_intel_avx512vl = 1;
+
+    if(ebx & bit_AVX512CD) {
+        ceph_arch_intel_avx512cd = 1;
+    }
+    if(ebx & bit_AVX512DQ) {
+        ceph_arch_intel_avx512dq = 1;
+    }
+    if (ebx & bit_AVX512BW) {
+        ceph_arch_intel_avx512bw = 1;
+    }
+}
 
 int ceph_arch_intel_probe(void)
 {
@@ -36,8 +153,8 @@ int ceph_arch_intel_probe(void)
     if (max_level == 0) {
         return 0;
     }
-
     __cpuid_count(1, 0, eax, ebx, ecx, edx);
+
     if ((ecx & bit_PCLMUL) != 0) {
         ceph_arch_intel_pclmul = 1;
     }
@@ -59,6 +176,10 @@ int ceph_arch_intel_probe(void)
     if ((ecx & bit_AES) != 0) {
         ceph_arch_intel_aesni = 1;
     }
+
+    detect_avx();
+    detect_avx2();
+    detect_avx512();
 
     return 0;
 }
--- src/arch/intel.h
+++ src/arch/intel.h
@@ -12,6 +12,15 @@ extern int ceph_arch_intel_ssse3;  /* true if we have ssse 3 features */
 extern int ceph_arch_intel_sse3;   /* true if we have sse 3 features */
 extern int ceph_arch_intel_sse2;   /* true if we have sse 2 features */
 extern int ceph_arch_intel_aesni;  /* true if we have aesni features */
+extern int ceph_arch_intel_avx;    /* ture if we have avx features */
+extern int ceph_arch_intel_avx2;   /* ture if we have avx2 features */
+extern int ceph_arch_intel_avx512f;  /* ture if we have avx512 foudation features */
+extern int ceph_arch_intel_avx512er; /* ture if we have 28-bit precision RCP, RSQRT and EXP transcendentals features */
+extern int ceph_arch_intel_avx512pf; /* ture if we have avx512 prefetch features */
+extern int ceph_arch_intel_avx512vl; /* ture if we have avx512 variable length features */
+extern int ceph_arch_intel_avx512cd; /* ture if we have avx512 conflictdetection features */
+extern int ceph_arch_intel_avx512dq; /* ture if we have new 32-bit and 64-bit AVX-512 instructions */
+extern int ceph_arch_intel_avx512bw; /* ture if we have new 8-bit and 16-bit AVX-512 instructions */
 
 extern int ceph_arch_intel_probe(void);
 
