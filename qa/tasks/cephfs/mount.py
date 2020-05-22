f8d9be1300f617a1d371bef86712045f1fca554a
Patrick Donnelly <pdonnell@redhat.com>
2020-05-22 14:15:19 -0700

    qa: return a string via getfattr
    Fixes: https://tracker.ceph.com/issues/45666
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>
    (cherry picked from commit 3c1330795add81126424e9e0a49cc07881fbdf8f)

--- qa/tasks/cephfs/mount.py
+++ qa/tasks/cephfs/mount.py
@@ -714,7 +714,7 @@ class CephFSMount(object):
             else:
                 raise
 
-        return p.stdout.getvalue()
+        return str(p.stdout.getvalue())
 
     def df(self):
         """
