77bfb235776358a0e29b4aa96c16226ede5ec7f8
Tiago Melo <tmelo@suse.com>
2020-05-22 15:41:58 +0000

    mgr/dashboard: Migrate Tabs from ngx-bootstrap to ng-bootstrap
    Using ng-bootstrap for Tabs will allow us to easily implement some new features
    like only loading 1 tab at a time (already implemented here) and
    saving/restoring last opened tab.
    
    Modified the table component to use a clone of the columns list.
    Making changes directly to columns var was causing problem when the table was
    loaded a second time.
    
    Fixes: https://tracker.ceph.com/issues/45017
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/app.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/app.module.ts
@@ -14,7 +14,6 @@ import { I18n } from '@ngx-translate/i18n-polyfill';
 import { NgBootstrapFormValidationModule } from 'ng-bootstrap-form-validation';
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -47,7 +46,6 @@ export function jwtTokenGetter() {
     SharedModule,
     CephModule,
     BsDropdownModule.forRoot(),
-    TabsModule.forRoot(),
     JwtModule.forRoot({
       config: {
         tokenGetter: jwtTokenGetter
