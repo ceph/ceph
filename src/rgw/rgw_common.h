ideepika/ceph-ci

rgw: Make system objects use new RADOS
Rather than trying to convert RGWSI_RADOS over and force all its users
to convert, add the new RADOS library alongside it and convert only
the system object services. This is the core of the new system and
lets us have a working piece to work out from.

Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

src/rgw/rgw_common.h
@@ -37,6 +37,7 @@
 #include "cls/user/cls_user_types.h"
 #include "cls/rgw/cls_rgw_types.h"
 #include "include/rados/librados.hpp"
+#include "include/RADOS/RADOS.hpp"
 
 namespace ceph {
   class Formatter;
@@ -1046,6 +1047,9 @@ struct RGWObjVersionTracker {
 
   void prepare_op_for_read(librados::ObjectReadOperation *op);
   void prepare_op_for_write(librados::ObjectWriteOperation *op);
+
+  void prepare_op_for_read(RADOS::ReadOp& op);
+  void prepare_op_for_write(RADOS::WriteOp& op);
 
   void apply_write() {
     read_version = write_version;

ceph/ceph

src/rgw/rgw_common.h
@@ -39,6 +39,7 @@
 #include "cls/rgw/cls_rgw_types.h"
 #include "include/rados/librados.hpp"
 #include "rgw_public_access.h"
+#include "include/neorados/RADOS.hpp"
 
 namespace ceph {
   class Formatter;
@@ -1056,6 +1057,9 @@ struct RGWObjVersionTracker {
 
   void prepare_op_for_read(librados::ObjectReadOperation *op);
   void prepare_op_for_write(librados::ObjectWriteOperation *op);
+
+  void prepare_op_for_read(neorados::ReadOp& op);
+  void prepare_op_for_write(neorados::WriteOp& op);
 
   void apply_write() {
     read_version = write_version;