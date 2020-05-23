ideepika/ceph-ci

rgw: Put throttle for new RADOS alongside throttle for old RADOS
Garbage collect the old version when everything's using the new one.

Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

src/rgw/rgw_aio.cc
@@ -19,6 +19,8 @@
 
 #include "rgw_aio.h"
 
+namespace ba = boost::asio;
+
 namespace rgw {
 
 namespace {
@@ -121,3 +123,95 @@ Aio::OpFunc Aio::librados_op(librados::ObjectWriteOperation&& op,
 }
 
 } // namespace rgw
+
+
+namespace neo {
+
+namespace {
+
+struct state {
+  Aio* aio;
+  AioResult& r;
+
+  void operator ()(boost::system::error_code ec) {
+    static_assert(sizeof(AioResult::user_data) >= sizeof(state));
+    static_assert(std::is_trivially_destructible_v<state>);
+    r.result = ec;
+    aio->put(r);
+  }
+
+  state(Aio* aio, AioResult& r)
+    : aio(aio), r(r) {}
+};
+
+
+template <typename Op>
+Aio::OpFunc aio_abstract(Op&& op) {
+  return [op = std::move(op)] (Aio* aio, AioResult& r) mutable {
+      auto s = new (&r.user_data) state(aio, r);
+      if constexpr (std::is_same_v<std::decay_t<Op>, RADOS::ReadOp>) {
+	r.obj.aio_operate(std::move(op), &r.data, std::ref(*s));
+      } else {
+	r.obj.aio_operate(std::move(op), std::ref(*s));
+      }
+    };
+}
+
+#ifdef HAVE_BOOST_CONTEXT
+struct Handler {
+  Aio* throttle = nullptr;
+  AioResult& r;
+  // callback
+  void operator()(boost::system::error_code ec) {
+    r.result = ec;
+    throttle->put(r);
+  }
+};
+
+template <typename Op>
+Aio::OpFunc aio_abstract(Op&& op, ba::io_context& context,
+                         spawn::yield_context yield) {
+  return [op = std::move(op), yield] (Aio* aio, AioResult& r) mutable {
+      // arrange for the completion Handler to run on the yield_context's strand
+      // executor so it can safely call back into Aio without locking
+      using namespace boost::asio;
+      async_completion<spawn::yield_context, void()> init(yield);
+      auto ex = get_associated_executor(init.completion_handler);
+
+      if constexpr (std::is_same_v<std::decay_t<Op>, RADOS::ReadOp>) {
+	r.obj.aio_operate(std::move(op), &r.data,
+			  bind_executor(ex, Handler{aio, r}));
+      } else {
+	r.obj.aio_operate(std::move(op),
+			  bind_executor(ex, Handler{aio, r}));
+      }
+    };
+}
+#endif // HAVE_BOOST_CONTEXT
+
+template <typename Op>
+Aio::OpFunc aio_abstract(Op&& op, optional_yield y) {
+  static_assert(std::is_base_of_v<RADOS::Op, std::decay_t<Op>>);
+  static_assert(!std::is_lvalue_reference_v<Op>);
+  static_assert(!std::is_const_v<Op>);
+#ifdef HAVE_BOOST_CONTEXT
+  if (y) {
+    return aio_abstract(std::move(op), y.get_io_context(),
+                        y.get_yield_context());
+  }
+#endif
+  return aio_abstract(std::move(op));
+}
+
+} // anonymous namespace
+
+Aio::OpFunc Aio::rados_op(RADOS::ReadOp&& op,
+			  optional_yield y) {
+  return aio_abstract(std::move(op), y);
+}
+Aio::OpFunc Aio::rados_op(RADOS::WriteOp&& op,
+			  optional_yield y) {
+  return aio_abstract(std::move(op), y);
+}
+
+} // namespace neo

src/rgw/rgw_aio.h
@@ -20,14 +20,17 @@
 #include <type_traits>
 
 #include <boost/intrusive/list.hpp>
+#include <boost/system/error_code.hpp>
 #include "include/rados/librados_fwd.hpp"
 #include "common/async/yield_context.h"
 
 #include "services/svc_rados.h" // cant forward declare RGWSI_RADOS::Obj
 
 #include "rgw_common.h"
+#include "rgw_rados.h"
 
 #include "include/function2.hpp"
+#include "include/RADOS/RADOS.hpp"
 
 namespace rgw {
 
@@ -98,3 +101,78 @@ class Aio {
 };
 
 } // namespace rgw
+
+// Just so we can have both versions sit side-by-side.
+
+namespace bs = boost::system;
+namespace R = RADOS;
+
+namespace neo {
+
+struct AioResult {
+  neo_obj_ref obj;
+  uint64_t id = 0; // id allows caller to associate a result with its request
+  bs::error_code result;
+  ceph::buffer::list data;
+  std::aligned_storage_t<3 * sizeof(void*)> user_data;
+
+  AioResult(neo_obj_ref obj)
+    : obj(std::move(obj)) {}
+  AioResult(const AioResult&) = delete;
+  AioResult& operator =(const AioResult&) = delete;
+  AioResult(AioResult&&) = delete;
+  AioResult& operator =(AioResult&&) = delete;
+};
+struct AioResultEntry : AioResult, boost::intrusive::list_base_hook<> {
+  AioResultEntry(neo_obj_ref obj) : AioResult(std::move(obj)) {}
+  virtual ~AioResultEntry() {}
+};
+// a list of polymorphic entries that frees them on destruction
+template <typename T, typename ...Args>
+struct OwningList : boost::intrusive::list<T, Args...> {
+  OwningList() = default;
+  ~OwningList() { this->clear_and_dispose(std::default_delete<T>{}); }
+  OwningList(OwningList&&) = default;
+  OwningList& operator=(OwningList&&) = default;
+  OwningList(const OwningList&) = delete;
+  OwningList& operator=(const OwningList&) = delete;
+};
+using AioResultList = OwningList<AioResultEntry>;
+
+// returns the first error code or 0 if all succeeded
+inline bs::error_code check_for_errors(const AioResultList& results) {
+  for (auto& e : results) {
+    if (e.result) {
+      return e.result;
+    }
+  }
+  return {};
+}
+
+// interface to submit async RADOS operations and wait on their completions.
+// each call returns a list of results from prior completions
+class Aio {
+public:
+  using OpFunc = fu2::unique_function<void(Aio*, AioResult&) &&>;
+
+  Aio() = default;
+  virtual ~Aio() {}
+
+  virtual AioResultList get(neo_obj_ref obj,
+			    OpFunc&& f,
+			    uint64_t cost, uint64_t id) = 0;
+  virtual void put(AioResult& r) = 0;
+
+  // poll for any ready completions without waiting
+  virtual AioResultList poll() = 0;
+
+  // return any ready completions. if there are none, wait for the next
+  virtual AioResultList wait() = 0;
+
+  // wait for all outstanding completions and return their results
+  virtual AioResultList drain() = 0;
+
+  static OpFunc rados_op(R::ReadOp&& op, optional_yield y);
+  static OpFunc rados_op(R::WriteOp&& op, optional_yield y);
+};
+}

ceph/ceph

src/rgw/rgw_aio.cc
@@ -19,6 +19,8 @@
 
 #include "rgw_aio.h"
 
+namespace ba = boost::asio;
+
 namespace rgw {
 
 namespace {
@@ -121,3 +123,95 @@ Aio::OpFunc Aio::librados_op(librados::ObjectWriteOperation&& op,
 }
 
 } // namespace rgw
+
+
+namespace neo {
+
+namespace {
+
+struct state {
+  Aio* aio;
+  AioResult& r;
+
+  void operator ()(boost::system::error_code ec) {
+    static_assert(sizeof(AioResult::user_data) >= sizeof(state));
+    static_assert(std::is_trivially_destructible_v<state>);
+    r.result = ec;
+    aio->put(r);
+  }
+
+  state(Aio* aio, AioResult& r)
+    : aio(aio), r(r) {}
+};
+
+
+template <typename Op>
+Aio::OpFunc aio_abstract(Op&& op) {
+  return [op = std::move(op)] (Aio* aio, AioResult& r) mutable {
+      auto s = new (&r.user_data) state(aio, r);
+      if constexpr (std::is_same_v<std::decay_t<Op>, R::ReadOp>) {
+	r.obj.aio_operate(std::move(op), &r.data, std::ref(*s));
+      } else {
+	r.obj.aio_operate(std::move(op), std::ref(*s));
+      }
+    };
+}
+
+#ifdef HAVE_BOOST_CONTEXT
+struct Handler {
+  Aio* throttle = nullptr;
+  AioResult& r;
+  // callback
+  void operator()(boost::system::error_code ec) {
+    r.result = ec;
+    throttle->put(r);
+  }
+};
+
+template <typename Op>
+Aio::OpFunc aio_abstract(Op&& op, ba::io_context& context,
+                         spawn::yield_context yield) {
+  return [op = std::move(op), yield] (Aio* aio, AioResult& r) mutable {
+      // arrange for the completion Handler to run on the yield_context's strand
+      // executor so it can safely call back into Aio without locking
+      using namespace boost::asio;
+      async_completion<spawn::yield_context, void()> init(yield);
+      auto ex = get_associated_executor(init.completion_handler);
+
+      if constexpr (std::is_same_v<std::decay_t<Op>, R::ReadOp>) {
+	r.obj.aio_operate(std::move(op), &r.data,
+			  bind_executor(ex, Handler{aio, r}));
+      } else {
+	r.obj.aio_operate(std::move(op),
+			  bind_executor(ex, Handler{aio, r}));
+      }
+    };
+}
+#endif // HAVE_BOOST_CONTEXT
+
+template <typename Op>
+Aio::OpFunc aio_abstract(Op&& op, optional_yield y) {
+  static_assert(std::is_base_of_v<R::Op, std::decay_t<Op>>);
+  static_assert(!std::is_lvalue_reference_v<Op>);
+  static_assert(!std::is_const_v<Op>);
+#ifdef HAVE_BOOST_CONTEXT
+  if (y) {
+    return aio_abstract(std::move(op), y.get_io_context(),
+                        y.get_yield_context());
+  }
+#endif
+  return aio_abstract(std::move(op));
+}
+
+} // anonymous namespace
+
+Aio::OpFunc Aio::rados_op(R::ReadOp&& op,
+			  optional_yield y) {
+  return aio_abstract(std::move(op), y);
+}
+Aio::OpFunc Aio::rados_op(R::WriteOp&& op,
+			  optional_yield y) {
+  return aio_abstract(std::move(op), y);
+}
+
+} // namespace neo

src/rgw/rgw_aio.h
@@ -20,14 +20,17 @@
 #include <type_traits>
 
 #include <boost/intrusive/list.hpp>
+#include <boost/system/error_code.hpp>
 #include "include/rados/librados_fwd.hpp"
 #include "common/async/yield_context.h"
 
 #include "services/svc_rados.h" // cant forward declare RGWSI_RADOS::Obj
 
 #include "rgw_common.h"
+#include "rgw_rados.h"
 
 #include "include/function2.hpp"
+#include "include/neorados/RADOS.hpp"
 
 namespace rgw {
 
@@ -98,3 +101,78 @@ class Aio {
 };
 
 } // namespace rgw
+
+// Just so we can have both versions sit side-by-side.
+
+namespace bs = boost::system;
+namespace R = neorados;
+
+namespace neo {
+
+struct AioResult {
+  neo_obj_ref obj;
+  uint64_t id = 0; // id allows caller to associate a result with its request
+  bs::error_code result;
+  ceph::buffer::list data;
+  std::aligned_storage_t<3 * sizeof(void*)> user_data;
+
+  AioResult(neo_obj_ref obj)
+    : obj(std::move(obj)) {}
+  AioResult(const AioResult&) = delete;
+  AioResult& operator =(const AioResult&) = delete;
+  AioResult(AioResult&&) = delete;
+  AioResult& operator =(AioResult&&) = delete;
+};
+struct AioResultEntry : AioResult, boost::intrusive::list_base_hook<> {
+  AioResultEntry(neo_obj_ref obj) : AioResult(std::move(obj)) {}
+  virtual ~AioResultEntry() {}
+};
+// a list of polymorphic entries that frees them on destruction
+template <typename T, typename ...Args>
+struct OwningList : boost::intrusive::list<T, Args...> {
+  OwningList() = default;
+  ~OwningList() { this->clear_and_dispose(std::default_delete<T>{}); }
+  OwningList(OwningList&&) = default;
+  OwningList& operator=(OwningList&&) = default;
+  OwningList(const OwningList&) = delete;
+  OwningList& operator=(const OwningList&) = delete;
+};
+using AioResultList = OwningList<AioResultEntry>;
+
+// returns the first error code or 0 if all succeeded
+inline bs::error_code check_for_errors(const AioResultList& results) {
+  for (auto& e : results) {
+    if (e.result) {
+      return e.result;
+    }
+  }
+  return {};
+}
+
+// interface to submit async RADOS operations and wait on their completions.
+// each call returns a list of results from prior completions
+class Aio {
+public:
+  using OpFunc = fu2::unique_function<void(Aio*, AioResult&) &&>;
+
+  Aio() = default;
+  virtual ~Aio() {}
+
+  virtual AioResultList get(neo_obj_ref obj,
+			    OpFunc&& f,
+			    uint64_t cost, uint64_t id) = 0;
+  virtual void put(AioResult& r) = 0;
+
+  // poll for any ready completions without waiting
+  virtual AioResultList poll() = 0;
+
+  // return any ready completions. if there are none, wait for the next
+  virtual AioResultList wait() = 0;
+
+  // wait for all outstanding completions and return their results
+  virtual AioResultList drain() = 0;
+
+  static OpFunc rados_op(R::ReadOp&& op, optional_yield y);
+  static OpFunc rados_op(R::WriteOp&& op, optional_yield y);
+};
+}