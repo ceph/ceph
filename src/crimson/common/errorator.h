7aeab3077b679a61810ff987fa1ab63d48d4d20b
Radoslaw Zarzynski <rzarzyns@redhat.com>
2020-05-25 14:26:32 +0200

    crimson/common: discard_all::operator() returns void now.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/crimson/common/errorator.h
+++ src/crimson/common/errorator.h
@@ -724,7 +724,7 @@ public:
 
   struct discard_all {
     template <class ErrorT, EnableIf<ErrorT>...>
-    decltype(auto) operator()(ErrorT&&) {
+    void operator()(ErrorT&&) {
       static_assert(contains_once_v<std::decay_t<ErrorT>>,
                     "discarding disallowed ErrorT");
     }
@@ -971,7 +971,7 @@ namespace ct_error {
 
   struct discard_all {
     template <class ErrorT>
-    decltype(auto) operator()(ErrorT&&) {
+    void operator()(ErrorT&&) {
     }
   };
 
