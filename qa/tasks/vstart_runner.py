e1656fadeafddfc1ae35a8fb3e48aa3e65ac24f7
Xiubo Li <xiubli@redhat.com>
2020-05-27 21:18:53 -0400

    qa/tasks/vstart_runner.py: add kwargs parameter to ignore the ones it does not understand
    INFO:__main__:test_replay_create (tasks.cephfs.test_cap_flush.TestCapFlush) ... ERROR
    INFO:__main__:Stopped test: test_replay_create (tasks.cephfs.test_cap_flush.TestCapFlush) in 22.251901s
    INFO:__main__:
    INFO:__main__:======================================================================
    INFO:__main__:ERROR: test_replay_create (tasks.cephfs.test_cap_flush.TestCapFlush)
    INFO:__main__:----------------------------------------------------------------------
    INFO:__main__:Traceback (most recent call last):
    INFO:__main__:  File "/data/ceph/qa/tasks/cephfs/cephfs_test_case.py", line 145, in setUp
    INFO:__main__:    self.mounts[i].mount_wait()
    INFO:__main__:  File "/data/ceph/qa/tasks/cephfs/mount.py", line 420, in mount_wait
    INFO:__main__:    mount_options=mount_options)
    INFO:__main__:TypeError: mount() got an unexpected keyword argument 'mountpoint'
    INFO:__main__:
    INFO:__main__:----------------------------------------------------------------------
    INFO:__main__:Ran 1 test in 22.253s
    INFO:__main__:
    INFO:__main__:FAILED (errors=1)
    INFO:__main__:
    
    Fixes: https://tracker.ceph.com/issues/45300
    Signed-off-by: Xiubo Li <xiubli@redhat.com>
    (cherry picked from commit 9f757c18a88a3c0d970e5c9ffda37f79f49fbe8d)

--- qa/tasks/vstart_runner.py
+++ qa/tasks/vstart_runner.py
@@ -622,7 +622,7 @@ class LocalKernelMount(KernelMount):
         log.info("I think my launching pid was {0}".format(self.fuse_daemon.subproc.pid))
         return path
 
-    def mount(self, mount_path=None, mount_fs_name=None, mount_options=[]):
+    def mount(self, mount_path=None, mount_fs_name=None, mount_options=[], **kwargs):
         self.setupfs(name=mount_fs_name)
 
         log.info('Mounting kclient client.{id} at {remote} {mnt}...'.format(
