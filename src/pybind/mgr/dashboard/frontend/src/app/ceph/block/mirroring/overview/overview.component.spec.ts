fc2796dfc5bc3699c12131845a8c1845201830e6
Tiago Melo <tmelo@suse.com>
2020-05-28 19:12:59 +0000

    mgr/dashboard: Use ng-bootstrap for Progressbar
    Fixes: https://tracker.ceph.com/issues/45752
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/mirroring/overview/overview.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/mirroring/overview/overview.component.spec.ts
@@ -3,9 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbNavModule, NgbProgressbarModule } from '@ng-bootstrap/ng-bootstrap';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { ProgressbarModule } from 'ngx-bootstrap/progressbar';
 import { ToastrModule } from 'ngx-toastr';
 
 import { configureTestBed, i18nProviders } from '../../../../../testing/unit-test-helper';
@@ -33,7 +32,7 @@ describe('OverviewComponent', () => {
       SharedModule,
       BsDropdownModule.forRoot(),
       NgbNavModule,
-      ProgressbarModule.forRoot(),
+      NgbProgressbarModule,
       HttpClientTestingModule,
       RouterTestingModule,
       ToastrModule.forRoot()
