97882c29252addcc7312d0a868d73283e4e08c68
Abhishek Lekshmanan <abhishek@suse.com>
2020-05-28 22:37:53 +0200

    svc: svc_account: add `get_meta_key` function
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- src/rgw/services/svc_account.cc
+++ src/rgw/services/svc_account.cc
@@ -2,9 +2,14 @@
 // vim: ts=8 sw=2 smarttab ft=cpp
 
 #include "svc_account.h"
+#include "rgw/rgw_account.h"
 
 RGWSI_Account::RGWSI_Account(CephContext *cct): RGWServiceInstance(cct) {
 }
 
 RGWSI_Account::~RGWSI_Account() {
+}
+
+std::string RGWSI_Account::get_meta_key(const RGWAccountInfo& info) {
+  return info.get_id();
 }
--- src/rgw/services/svc_account.h
+++ src/rgw/services/svc_account.h
@@ -26,6 +26,8 @@ public:
   RGWSI_Account(CephContext *cct);
   virtual ~RGWSI_Account();
 
+  static string get_meta_key(const RGWAccountInfo& info);
+
   virtual int read_account_info(RGWSI_MetaBackend::Context *ctx,
 				const std::string& account_id,
 				RGWAccountInfo *info,
