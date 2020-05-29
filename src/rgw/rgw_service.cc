e41c9bc7403a453152f7c2db7074bfd401a78d37
Abhishek Lekshmanan <abhishek@suse.com>
2020-05-29 16:02:44 +0200

    rgw_service: add account service
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- src/rgw/rgw_service.cc
+++ src/rgw/rgw_service.cc
@@ -27,6 +27,7 @@
 #include "services/svc_sys_obj_cache.h"
 #include "services/svc_sys_obj_core.h"
 #include "services/svc_user_rados.h"
+#include "services/svc_account_rados.h"
 
 #include "common/errno.h"
 
@@ -34,6 +35,7 @@
 #include "rgw_user.h"
 #include "rgw_bucket.h"
 #include "rgw_otp.h"
+#include "rgw_account.h"
 
 #define dout_subsys ceph_subsys_rgw
 
@@ -71,6 +73,7 @@ int RGWServices_Def::init(CephContext *cct,
   sysobj = std::make_unique<RGWSI_SysObj>(cct);
   sysobj_core = std::make_unique<RGWSI_SysObj_Core>(cct);
   user_rados = std::make_unique<RGWSI_User_RADOS>(cct);
+  account_rados = std::make_unique<RGWSI_Account_RADOS>(cct);
 
   if (have_cache) {
     sysobj_cache = std::make_unique<RGWSI_SysObj_Cache>(cct);
@@ -111,6 +114,7 @@ int RGWServices_Def::init(CephContext *cct,
   }
   user_rados->init(rados.get(), zone.get(), sysobj.get(), sysobj_cache.get(),
                    meta.get(), meta_be_sobj.get(), sync_modules.get());
+  account_rados->init(zone.get(), meta.get(), meta_be_sobj.get());
 
   can_shutdown = true;
 
@@ -240,6 +244,13 @@ int RGWServices_Def::init(CephContext *cct,
       ldout(cct, 0) << "ERROR: failed to start otp service (" << cpp_strerror(-r) << dendl;
       return r;
     }
+
+    r = account_rados->start();
+    if (r < 0) {
+      ldout(cct, 0) << "ERROR: failed to start account service (" << cpp_strerror(-r) << dendl;
+      return r;
+    }
+
   }
 
   /* cache or core services will be started by sysobj */
@@ -311,6 +322,7 @@ int RGWServices::do_init(CephContext *_cct, bool have_cache, bool raw, bool run_
   cache = _svc.sysobj_cache.get();
   core = _svc.sysobj_core.get();
   user = _svc.user_rados.get();
+  account = _svc.account_rados.get();
 
   return 0;
 }
@@ -363,6 +375,7 @@ int RGWCtlDef::init(RGWServices& svc)
                                 svc.bucket_sync,
                                 svc.bi));
   otp.reset(new RGWOTPCtl(svc.zone, svc.otp));
+  account.reset(new RGWAccountCtl(svc.zone, svc.account));
 
   RGWBucketMetadataHandlerBase *bucket_meta_handler = static_cast<RGWBucketMetadataHandlerBase *>(meta.bucket.get());
   RGWBucketInstanceMetadataHandlerBase *bi_meta_handler = static_cast<RGWBucketInstanceMetadataHandlerBase *>(meta.bucket_instance.get());
@@ -400,10 +413,12 @@ int RGWCtl::init(RGWServices *_svc)
   meta.bucket = _ctl.meta.bucket.get();
   meta.bucket_instance = _ctl.meta.bucket_instance.get();
   meta.otp = _ctl.meta.otp.get();
+  //meta.account = _ctl.meta.account.get();
 
   user = _ctl.user.get();
   bucket = _ctl.bucket.get();
   otp = _ctl.otp.get();
+  account = _ctl.account.get();
 
   r = meta.user->attach(meta.mgr);
   if (r < 0) {
@@ -428,6 +443,12 @@ int RGWCtl::init(RGWServices *_svc)
     ldout(cct, 0) << "ERROR: failed to start init otp ctl (" << cpp_strerror(-r) << dendl;
     return r;
   }
+
+  // r = meta.account->attach(meta.mgr);
+  // if (r < 0) {
+  //   ldout(cct, 0) << "ERROR: failed to start init account ctl (" << cpp_strerror(-r) << dendl;
+  //   return r;
+  // }
 
   return 0;
 }
