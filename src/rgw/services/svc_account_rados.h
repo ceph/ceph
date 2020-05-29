75937363c0651b098fc407b590a9f59eca23dd1f
Abhishek Lekshmanan <abhishek@suse.com>
2020-05-29 16:03:13 +0200

    rgw: svc_account_rados: mark functions as not implemented
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- src/rgw/services/svc_account_rados.h
+++ src/rgw/services/svc_account_rados.h
@@ -47,12 +47,37 @@ public:
 	    RGWSI_MetaBackend *_meta_be_svc);
 
   int store_account_info(RGWSI_MetaBackend::Context *ctx,
-			 const RGWAccountInfo& info,
-			 RGWObjVersionTracker * const objv_tracker,
-			 const real_time& mtime,
-			 bool exclusive,
-			 map<std::string, bufferlist> * const pattrs,
-			 optional_yield y) override;
+  			 const RGWAccountInfo& info,
+  			 RGWObjVersionTracker * const objv_tracker,
+  			 const real_time& mtime,
+  			 bool exclusive,
+  			 map<std::string, bufferlist> * const pattrs,
+  			 optional_yield y) override;
+
+  int read_account_info(RGWSI_MetaBackend::Context *ctx,
+  			const std::string& account_id,
+  			RGWAccountInfo *info,
+  			RGWObjVersionTracker * const objv_tracker,
+  			rgw_cache_entry_info * const cache_info,
+  			map<std::string, bufferlist> * const pattrs,
+  			optional_yield y) override
+  { return -ERR_NOT_IMPLEMENTED; }
+
+  int remove_account_info(RGWSI_MetaBackend::Context *ctx,
+  			  const RGWAccountInfo& info,
+  			  RGWObjVersionTracker *objv_tracker,
+  			  optional_yield y) override
+  { return -ERR_NOT_IMPLEMENTED; }
+
+  int add_user(RGWSI_MetaBackend::Context *ctx,
+  	       const std::string& account_id,
+  	       const rgw_user& rgw_user) override
+  { return -ERR_NOT_IMPLEMENTED; }
+
+  int remove_user(RGWSI_MetaBackend::Context *ctx,
+  		  const std::string& account_id,
+  		  const rgw_user& rgw_user) override
+  { return -ERR_NOT_IMPLEMENTED; }
 
 private:
   RGWSI_MetaBackend_Handler *be_handler;
