f81d3b34b3d7ba7dd70f50154e3f4b89d4948879
Rishabh Dave <ridave@redhat.com>
2020-06-02 17:32:47 +0530

    vstart_runner: make LocalKernelMount.mount() accept argument mountpoint
    Unlike all other mount() methods in qa/ LocalKernelMount.mount() doesn't
    accept mountpoint as an argument which leads to a crash when
    mount_wait() is called.
    
    Fixes: https://tracker.ceph.com/issues/45813

--- qa/tasks/vstart_runner.py
+++ qa/tasks/vstart_runner.py
@@ -589,7 +589,8 @@ class LocalKernelMount(KernelMount):
         log.info("I think my launching pid was {0}".format(self.fuse_daemon.subproc.pid))
         return path
 
-    def mount(self, mount_path=None, mount_fs_name=None, mount_options=[]):
+    def mount(self, mount_path=None, mount_fs_name=None, mountpoint=None,
+              mount_options=[]):
         self.setupfs(name=mount_fs_name)
         self.setup_netns()
 
