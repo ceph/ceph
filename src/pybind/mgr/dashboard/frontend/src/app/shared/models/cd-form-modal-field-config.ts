a7e4cbac8520a6bc4d682ddcc47645e6d8e16f23
Volker Theile <vtheile@suse.com>
2020-06-04 15:57:34 +0200

    mgr/dashboard: Add host labels in UI
    Fixes: https://tracker.ceph.com/issues/45897
    
    Signed-off-by: Volker Theile <vtheile@suse.com>
    
    s
    
    s
    
    s

--- src/pybind/mgr/dashboard/frontend/src/app/shared/models/cd-form-modal-field-config.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/models/cd-form-modal-field-config.ts
@@ -1,19 +1,32 @@
 import { ValidatorFn } from '@angular/forms';
 
 export class CdFormModalFieldConfig {
+  // --- Generic field properties ---
   name: string;
   // 'binary' will use cdDimlessBinary directive on input element
   // 'select' will use select element
-  type: 'number' | 'text' | 'binary' | 'select';
+  type: 'number' | 'text' | 'binary' | 'select' | 'select-badges';
   label?: string;
   required?: boolean;
   value?: any;
   errors?: { [errorName: string]: string };
   validators: ValidatorFn[];
-  // only for type select
-  placeholder?: string;
-  options?: Array<{
-    text: string;
-    value: any;
-  }>;
+
+  // --- Specific field properties ---
+  typeConfig?: {
+    [prop: string]: any;
+    // 'select':
+    // ---------
+    // placeholder?: string;
+    // options?: Array<{
+    //   text: string;
+    //   value: any;
+    // }>;
+    //
+    // 'select-badges':
+    // ----------------
+    // customBadges: boolean;
+    // options: Array<SelectOption>;
+    // messages: SelectMessages;
+  };
 }
