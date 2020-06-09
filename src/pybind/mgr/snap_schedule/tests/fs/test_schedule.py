8ebf967c9af5778ccb3713467c64bf588f68616c
Jan Fajerski <jfajerski@suse.com>
2020-06-09 14:49:55 +0200

    pybing/mgr/snap_schedule: add separate retention interface
    Signed-off-by: Jan Fajerski <jfajerski@suse.com>

--- src/pybind/mgr/snap_schedule/tests/fs/test_schedule.py
+++ src/pybind/mgr/snap_schedule/tests/fs/test_schedule.py
@@ -1,12 +1,34 @@
 import datetime
-from fs.schedule import Schedule
+import json
+import pytest
+import random
+import sqlite3
+from fs.schedule import Schedule, parse_retention
 
 SELECT_ALL = ('select * from schedules s'
               ' INNER JOIN schedules_meta sm'
               ' ON sm.schedule_id = s.id')
 
 
+def assert_updated(new, old, update_expected={}):
+    '''
+    This helper asserts that an object new has been updated in the
+    attributes in the dict updated AND has not changed in other attributes
+    compared to old.
+    if update expected is the empty dict, equality is checked
+    '''
+
+    for var in vars(new):
+        if var in update_expected:
+            assert getattr(new, var) == update_expected.get(var), f'new did not update value for {var}'
+        else:
+            assert getattr(new, var) == getattr(old, var), f'new changed unexpectedly in value for {var}'
+
+
 class TestSchedule(object):
+    '''
+    Test the schedule class basics and that its methods update self as expected
+    '''
 
     def test_start_default_midnight(self, simple_schedule):
         now = datetime.datetime.now(datetime.timezone.utc)
@@ -37,10 +59,9 @@ class TestSchedule(object):
         with db:
             row = db.execute(SELECT_ALL).fetchone()
 
-        db_schedule = Schedule._from_get_query(row, simple_schedule.fs)
+        db_schedule = Schedule._from_db_row(row, simple_schedule.fs)
 
-        for var in vars(db_schedule):
-            assert getattr(simple_schedule, var) == getattr(db_schedule, var)
+        assert_updated(db_schedule, simple_schedule)
 
     def test_store_multiple(self, db, simple_schedules):
         [s.store_schedule(db) for s in simple_schedules]
@@ -54,66 +75,162 @@ class TestSchedule(object):
     def test_update_last(self, db, simple_schedule):
         simple_schedule.store_schedule(db)
 
-        row = ()
         with db:
-            row = db.execute(SELECT_ALL).fetchone()
-        assert row['last'] is None
+            _ = db.execute(SELECT_ALL).fetchone()
 
         first_time = datetime.datetime.now(datetime.timezone.utc)
         simple_schedule.update_last(first_time, db)
 
         with db:
-            row = db.execute(SELECT_ALL).fetchone()
-        assert datetime.datetime.fromisoformat(row['last']) == first_time
+            after = db.execute(SELECT_ALL).fetchone()
+        assert_updated(Schedule._from_db_row(after, simple_schedule.fs),
+                       simple_schedule)
 
         second_time = datetime.datetime.now(datetime.timezone.utc)
         simple_schedule.update_last(second_time, db)
 
         with db:
-            row = db.execute(SELECT_ALL).fetchone()
-        assert datetime.datetime.fromisoformat(row['last']) == second_time
+            after2 = db.execute(SELECT_ALL).fetchone()
+        assert_updated(Schedule._from_db_row(after2, simple_schedule.fs),
+                       simple_schedule)
 
-    def test_update_created_count(self, db, simple_schedule):
+    def test_set_inactive_active(self, db, simple_schedule):
         simple_schedule.store_schedule(db)
 
-        row = ()
         with db:
-            row = db.execute(SELECT_ALL).fetchone()
-        assert row['created_count'] == 0
+            _ = db.execute(SELECT_ALL).fetchone()
 
-        first_time = datetime.datetime.now(datetime.timezone.utc)
-        simple_schedule.update_last(first_time, db)
+        simple_schedule.set_inactive(db)
 
         with db:
-            row = db.execute(SELECT_ALL).fetchone()
-        assert row['created_count'] == 1
+            after = db.execute(SELECT_ALL).fetchone()
+        assert_updated(Schedule._from_db_row(after, simple_schedule.fs),
+                       simple_schedule)
 
-        second_time = datetime.datetime.now(datetime.timezone.utc)
-        simple_schedule.update_last(second_time, db)
+        simple_schedule.set_active(db)
 
         with db:
-            row = db.execute(SELECT_ALL).fetchone()
-        assert row['created_count'] == 2
+            after2 = db.execute(SELECT_ALL).fetchone()
+        assert_updated(Schedule._from_db_row(after2, simple_schedule.fs),
+                       simple_schedule)
 
-    def test_update_first(self, db, simple_schedule):
+    def test_update_pruned(self, db, simple_schedule):
         simple_schedule.store_schedule(db)
 
-        row = ()
         with db:
-            row = db.execute(SELECT_ALL).fetchone()
-        assert row['first'] is None
+            _ = db.execute(SELECT_ALL).fetchone()
+
+        now = datetime.datetime.now(datetime.timezone.utc)
+        pruned_count = random.randint(1, 1000)
+
+        simple_schedule.update_pruned(now, db, pruned_count)
+
+        with db:
+            after = db.execute(SELECT_ALL).fetchone()
+
+        assert_updated(Schedule._from_db_row(after, simple_schedule.fs),
+                       simple_schedule)
+
+    # TODO test get_schedules and list_schedules
+
+
+class TestScheduleDB(object):
+    '''
+    This class tests that Schedules methods update the DB correctly
+    '''
+
+    def test_update_last(self, db, simple_schedule):
+        simple_schedule.store_schedule(db)
+
+        with db:
+            before = db.execute(SELECT_ALL).fetchone()
 
         first_time = datetime.datetime.now(datetime.timezone.utc)
         simple_schedule.update_last(first_time, db)
 
         with db:
-            row = db.execute(SELECT_ALL).fetchone()
-        assert datetime.datetime.fromisoformat(row['first']) == first_time
+            after = db.execute(SELECT_ALL).fetchone()
+        assert_updated(Schedule._from_db_row(after, simple_schedule.fs),
+                       Schedule._from_db_row(before, simple_schedule.fs),
+                       {'created_count': 1,
+                        'last': first_time,
+                        'first': first_time})
 
         second_time = datetime.datetime.now(datetime.timezone.utc)
         simple_schedule.update_last(second_time, db)
 
         with db:
-            row = db.execute(SELECT_ALL).fetchone()
-        assert datetime.datetime.fromisoformat(row['first']) == first_time
+            after2 = db.execute(SELECT_ALL).fetchone()
+        assert_updated(Schedule._from_db_row(after2, simple_schedule.fs),
+                       Schedule._from_db_row(after, simple_schedule.fs),
+                       {'created_count': 2, 'last': second_time})
 
+    def test_set_inactive_active(self, db, simple_schedule):
+        simple_schedule.store_schedule(db)
+
+        with db:
+            before = db.execute(SELECT_ALL).fetchone()
+
+        simple_schedule.set_inactive(db)
+
+        with db:
+            after = db.execute(SELECT_ALL).fetchone()
+        assert_updated(Schedule._from_db_row(after, simple_schedule.fs),
+                       Schedule._from_db_row(before, simple_schedule.fs),
+                       {'active': 0})
+
+        simple_schedule.set_active(db)
+
+        with db:
+            after2 = db.execute(SELECT_ALL).fetchone()
+        assert_updated(Schedule._from_db_row(after2, simple_schedule.fs),
+                       Schedule._from_db_row(after, simple_schedule.fs),
+                       {'active': 1})
+
+    def test_update_pruned(self, db, simple_schedule):
+        simple_schedule.store_schedule(db)
+
+        with db:
+            before = db.execute(SELECT_ALL).fetchone()
+
+        now = datetime.datetime.now(datetime.timezone.utc)
+        pruned_count = random.randint(1, 1000)
+
+        simple_schedule.update_pruned(now, db, pruned_count)
+
+        with db:
+            after = db.execute(SELECT_ALL).fetchone()
+
+        assert_updated(Schedule._from_db_row(after, simple_schedule.fs),
+                       Schedule._from_db_row(before, simple_schedule.fs),
+                       {'last_pruned': now, 'pruned_count': pruned_count})
+
+    def test_add_retention(self, db, simple_schedule):
+        simple_schedule.store_schedule(db)
+
+        with db:
+            before = db.execute(SELECT_ALL).fetchone()
+
+        retention = "7d12m"
+        simple_schedule.add_retention(db, simple_schedule.path, retention)
+
+        with db:
+            after = db.execute(SELECT_ALL).fetchone()
+
+        assert after['retention'] == json.dumps(parse_retention(retention))
+
+        retention2 = "4w"
+        simple_schedule.add_retention(db, simple_schedule.path, retention2)
+
+        with db:
+            after = db.execute(SELECT_ALL).fetchone()
+
+        assert after['retention'] == json.dumps(parse_retention(retention + retention2))
+
+    def test_per_path_and_repeat_uniqness(self, db):
+        s1 = Schedule(*('/foo', '24h', 'fs_name', '/foo'))
+        s2 = Schedule(*('/foo', '1d', 'fs_name', '/foo'))
+
+        s1.store_schedule(db)
+        with pytest.raises(sqlite3.IntegrityError):
+            s2.store_schedule(db)
