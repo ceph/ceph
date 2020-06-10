3bcf9d600598114df8fc53771db48c0dcefcb980
Shilpa Jagannath <smanjara@redhat.com>
2020-06-10 19:39:55 +0530

    rgw: fetch updated bucketinfo rather than bucket_id in guard_reshard
    To be able to read the updated layout.
    
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>

--- src/rgw/rgw_reshard.cc
+++ src/rgw/rgw_reshard.cc
@@ -668,6 +668,12 @@ int RGWBucketReshard::do_reshard(int num_shards,
       return ret;
   }
 
+  ret = bucket_info_updater.complete();
+  if (ret < 0) {
+    ldout(store->ctx(), 0) << __func__ << ": failed to update bucket info ret=" << ret << dendl;
+    /* don't error out, reshard process succeeded */
+  }
+
   return 0;
   // NB: some error clean-up is done by ~BucketInfoReshardUpdate
 } // RGWBucketReshard::do_reshard
