c43769b08621958446d6ae98cc3205822eabda76
Ramana Raja <rraja@redhat.com>
2020-06-14 22:03:16 +0530

    mgr/volumes/nfs: list exports within a ganesha cluster
    List the pseudo paths of the exports within a ganesha cluster
    with `nfs export ls <cluster ID>` command.
    
    List all the details of the exports within a ganesha cluster
    with `nfs export ls <cluster ID> --detailed` command.
    
    Partially-fixes: https://tracker.ceph.com/issues/45741
    Signed-off-by: Ramana Raja <rraja@redhat.com>

--- src/pybind/mgr/volumes/module.py
+++ src/pybind/mgr/volumes/module.py
@@ -271,6 +271,13 @@ class Module(orchestrator.OrchestratorClientMixin, MgrModule):
             'perm': 'rw'
         },
         {
+            'cmd': 'nfs export ls '
+                   'name=clusterid,type=CephString '
+                   'name=detailed,type=CephBool,req=false ',
+            'desc': "List exports of a NFS cluster",
+            'perm': 'r'
+        },
+        {
             'cmd': 'nfs cluster create '
                    'name=type,type=CephString '
                    'name=clusterid,type=CephString,goodchars=[A-Za-z0-9-_.] '
@@ -487,6 +494,9 @@ class Module(orchestrator.OrchestratorClientMixin, MgrModule):
 
     def _cmd_nfs_export_delete(self, inbuf, cmd):
         return self.fs_export.delete_export(cluster_id=cmd['attach'], pseudo_path=cmd['binding'])
+
+    def _cmd_nfs_export_ls(self, inbuf, cmd):
+        return self.fs_export.list_exports(cluster_id=cmd['clusterid'], detailed=cmd.get('detailed', False))
 
     def _cmd_nfs_cluster_create(self, inbuf, cmd):
         return self.nfs.create_nfs_cluster(cluster_id=cmd['clusterid'], export_type=cmd['type'],
