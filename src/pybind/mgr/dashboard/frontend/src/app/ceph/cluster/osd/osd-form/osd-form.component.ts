2a7b392d873f224e71c50d2916dbd1a63e4360f9
Kiefer Chang <kiefer.chang@suse.com>
2020-06-15 15:16:57 +0800

    mgr/dashboard: fix wal/db slots controls in the OSD form
    Controls should be updated immediately after changing.
    
    Fixes: https://tracker.ceph.com/issues/44985
    Signed-off-by: Kiefer Chang <kiefer.chang@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/osd/osd-form/osd-form.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/osd/osd-form/osd-form.component.ts
@@ -107,11 +107,9 @@ export class OsdFormComponent extends CdForm implements OnInit {
   createForm() {
     this.form = new CdFormGroup({
       walSlots: new FormControl(0, {
-        updateOn: 'blur',
         validators: [Validators.min(0)]
       }),
       dbSlots: new FormControl(0, {
-        updateOn: 'blur',
         validators: [Validators.min(0)]
       }),
       features: new CdFormGroup(
