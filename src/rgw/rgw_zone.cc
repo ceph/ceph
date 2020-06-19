d60711019dee494cd03a876398b221c826722b41
Abhishek Lekshmanan <abhishek@suse.com>
2020-06-19 15:37:58 +0200

    rgw zone: fix account pool name
    Signed-off-by: Abhishek Lekshmanan <abhishek@suse.com>

--- src/rgw/rgw_zone.cc
+++ src/rgw/rgw_zone.cc
@@ -1713,6 +1713,7 @@ int get_zones_pool_set(const DoutPrefixProvider *dpp,
       pool_names.insert(zone.roles_pool);
       pool_names.insert(zone.reshard_pool);
       pool_names.insert(zone.notif_pool);
+      pool_names.insert(zone.account_pool);
       for(auto& iter : zone.placement_pools) {
 	pool_names.insert(iter.second.index_pool);
         for (auto& pi : iter.second.storage_classes.get_all()) {
@@ -1790,6 +1791,7 @@ int RGWZoneParams::fix_pool_names(const DoutPrefixProvider *dpp, optional_yield 
   otp_pool = fix_zone_pool_dup(pools, name, ".rgw.otp", otp_pool);
   oidc_pool = fix_zone_pool_dup(pools, name, ".rgw.meta:oidc", oidc_pool);
   notif_pool = fix_zone_pool_dup(pools, name ,".rgw.log:notif", notif_pool);
+  account_pool = fix_zone_pool_dup(pools, name, ".rgw.meta:account", account_pool);
 
   for(auto& iter : placement_pools) {
     iter.second.index_pool = fix_zone_pool_dup(pools, name, "." + default_bucket_index_pool_suffix,
