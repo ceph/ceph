6b28b8b91f2d3cea5315db16134c127aa1e80f9c
Fabrizio D'Angelo <fdangelo@redhat.com>
2020-06-26 09:50:15 -0400

    mgr/dashboard: Address API inconsistencies
    Cephfs endpoints are inconsistencies with other endpoints.
    
    * Change rm endpoints from GET to DELETE requests
    * Change set_quotas from POST to PUT
    * Renames endpoints accordingly
    * Changes related angular files
    * update a few lines related to mypy
    * remove future import as the python2 compatibility is not needed
    
    Fixes: https://tracker.ceph.com/issues/46160
    Signed-off-by: Fabrizio D'Angelo <fdangelo@redhat.com>
    (cherry picked from commit 222286b08bde3600d50a6deb25e7425c17e9e482)

--- src/pybind/mgr/dashboard/frontend/src/app/shared/api/cephfs.service.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/api/cephfs.service.spec.ts
@@ -66,33 +66,33 @@ describe('CephfsService', () => {
 
   it('should call mkSnapshot', () => {
     service.mkSnapshot(3, '/some/path').subscribe();
-    const req = httpTesting.expectOne('api/cephfs/3/mk_snapshot?path=%252Fsome%252Fpath');
+    const req = httpTesting.expectOne('api/cephfs/3/snapshot?path=%252Fsome%252Fpath');
     expect(req.request.method).toBe('POST');
 
     service.mkSnapshot(4, '/some/other/path', 'snap').subscribe();
-    httpTesting.expectOne('api/cephfs/4/mk_snapshot?path=%252Fsome%252Fother%252Fpath&name=snap');
+    httpTesting.expectOne('api/cephfs/4/snapshot?path=%252Fsome%252Fother%252Fpath&name=snap');
   });
 
   it('should call rmSnapshot', () => {
     service.rmSnapshot(1, '/some/path', 'snap').subscribe();
-    const req = httpTesting.expectOne('api/cephfs/1/rm_snapshot?path=%252Fsome%252Fpath&name=snap');
-    expect(req.request.method).toBe('POST');
+    const req = httpTesting.expectOne('api/cephfs/1/snapshot?path=%252Fsome%252Fpath&name=snap');
+    expect(req.request.method).toBe('DELETE');
   });
 
   it('should call updateQuota', () => {
-    service.updateQuota(1, '/some/path', { max_bytes: 1024 }).subscribe();
-    let req = httpTesting.expectOne('api/cephfs/1/set_quotas?path=%252Fsome%252Fpath');
-    expect(req.request.method).toBe('POST');
+    service.quota(1, '/some/path', { max_bytes: 1024 }).subscribe();
+    let req = httpTesting.expectOne('api/cephfs/1/quota?path=%252Fsome%252Fpath');
+    expect(req.request.method).toBe('PUT');
     expect(req.request.body).toEqual({ max_bytes: 1024 });
 
-    service.updateQuota(1, '/some/path', { max_files: 10 }).subscribe();
-    req = httpTesting.expectOne('api/cephfs/1/set_quotas?path=%252Fsome%252Fpath');
-    expect(req.request.method).toBe('POST');
+    service.quota(1, '/some/path', { max_files: 10 }).subscribe();
+    req = httpTesting.expectOne('api/cephfs/1/quota?path=%252Fsome%252Fpath');
+    expect(req.request.method).toBe('PUT');
     expect(req.request.body).toEqual({ max_files: 10 });
 
-    service.updateQuota(1, '/some/path', { max_bytes: 1024, max_files: 10 }).subscribe();
-    req = httpTesting.expectOne('api/cephfs/1/set_quotas?path=%252Fsome%252Fpath');
-    expect(req.request.method).toBe('POST');
+    service.quota(1, '/some/path', { max_bytes: 1024, max_files: 10 }).subscribe();
+    req = httpTesting.expectOne('api/cephfs/1/quota?path=%252Fsome%252Fpath');
+    expect(req.request.method).toBe('PUT');
     expect(req.request.body).toEqual({ max_bytes: 1024, max_files: 10 });
   });
 });
