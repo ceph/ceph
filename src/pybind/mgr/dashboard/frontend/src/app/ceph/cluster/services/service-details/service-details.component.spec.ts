7a8861cc0a801fb12f9898ac198c48fb58d102b9
Volker Theile <vtheile@suse.com>
2020-06-30 11:26:22 +0200

    mgr/dashboard: stay on active tab
    * Remember the selected tab and restore the state on tabset init.
    * Remove tab headers where only one tab is shown.
    
    Fixes: https://tracker.ceph.com/issues/43120
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/services/service-details/service-details.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/services/service-details/service-details.component.spec.ts
@@ -4,11 +4,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 
-import {
-  configureTestBed,
-  i18nProviders,
-  TabHelper
-} from '../../../../../testing/unit-test-helper';
+import { configureTestBed, i18nProviders } from '../../../../../testing/unit-test-helper';
 import { CdTableSelection } from '../../../../shared/models/cd-table-selection';
 import { SummaryService } from '../../../../shared/services/summary.service';
 import { SharedModule } from '../../../../shared/shared.module';
@@ -42,22 +38,5 @@ describe('ServiceDetailsComponent', () => {
   it('should create', () => {
     fixture.detectChanges();
     expect(component).toBeTruthy();
-  });
-
-  describe('Service details tabset', () => {
-    beforeEach(() => {
-      component.selection.selected = [{ serviceName: 'osd' }];
-      fixture.detectChanges();
-    });
-
-    it('should have a NgbNav', () => {
-      const ngbNav = TabHelper.getNgbNav(fixture);
-      expect(ngbNav).toBeDefined();
-    });
-
-    it('should show tabs', () => {
-      const ngbNavItems = TabHelper.getTextContents(fixture);
-      expect(ngbNavItems).toEqual(['Daemons']);
-    });
   });
 });
--- src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-details/rgw-bucket-details.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-details/rgw-bucket-details.component.spec.ts
@@ -1,7 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { configureTestBed, i18nProviders } from '../../../../testing/unit-test-helper';
 import { CdTableSelection } from '../../../shared/models/cd-table-selection';
 import { SharedModule } from '../../../shared/shared.module';
@@ -13,7 +11,7 @@ describe('RgwBucketDetailsComponent', () => {
 
   configureTestBed({
     declarations: [RgwBucketDetailsComponent],
-    imports: [SharedModule, NgbNavModule],
+    imports: [SharedModule],
     providers: [i18nProviders]
   });
 
