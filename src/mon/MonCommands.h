6c40de86f37b16df91568a664cd0663c3a257e67
Greg Farnum <gfarnum@redhat.com>
2020-07-02 15:37:45 +0000

    mon: require all pools be in stretch mode; remove explicit listing from command
    There are a lot of UX issues and difficult failure modes that come up when
    some pools are in stretch mode and some aren't. We can deal with them later;
    for now just require users put everything into stretch mode.
    
    Signed-off-by: Greg Farnum <gfarnum@redhat.com>

--- src/mon/MonCommands.h
+++ src/mon/MonCommands.h
@@ -488,10 +488,9 @@ COMMAND("mon set_location " \
 COMMAND("mon enable_stretch_mode " \
 	"name=tiebreaker_mon,type=CephString, "
 	"name=new_crush_rule,type=CephString, "
-	"name=dividing_bucket,type=CephString, "
-	"name=pools,type=CephString,n=N",
+	"name=dividing_bucket,type=CephString, ",
 	"enable stretch mode, changing the peering rules and "
-	"failure handling on the named <pools> with <tiebreaker_mon> "
+	"failure handling on all pools with <tiebreaker_mon> "
 	"as the tiebreaker and setting <dividing_bucket> locations "
 	"as the units for stretching across",
 	"mon", "rw")
