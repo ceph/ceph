ceph/ceph

commit 8737042da19c190a802ac33635d43c423484d140

crimson/osd: use operator=(...) instead of claim()
to silence warnings like

objclass.cc:300:26: warning: 'void ceph::buffer::v15_2_0::list::claim(ceph::buffer::v15_2_0::list&)' is deprecated: in favor of operator=(list&&) [-Wdeprecated-declarations]
  300 |   outbl->claim(op.outdata);
      |                          ^

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/crimson/osd/objclass.cc
@@ -297,7 +297,7 @@ int cls_cxx_map_read_header(cls_method_context_t hctx, bufferlist *outbl)
   if (const auto ret = execute_osd_op(hctx, op); ret < 0) {
     return ret;
   }
-  outbl->claim(op.outdata);
+  outbl = std::move(op.outdata);
   return 0;
 }
 
@@ -357,7 +357,7 @@ int cls_cxx_map_clear(cls_method_context_t hctx)
 int cls_cxx_map_write_header(cls_method_context_t hctx, bufferlist *inbl)
 {
   OSDOp op{CEPH_OSD_OP_OMAPSETHEADER};
-  op.indata.claim(*inbl);
+  op.indata = std::move(*inbl);
   return execute_osd_op(hctx, op);
 }
 

commit 45b97d4b2c8b9da3abf748b5ee70d1130b28685e

crimson/osd: assign to bufferlist not to bufferlist*
this change addresses a regression introduced by 8737042da1

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/crimson/osd/objclass.cc
@@ -297,7 +297,7 @@ int cls_cxx_map_read_header(cls_method_context_t hctx, bufferlist *outbl)
   if (const auto ret = execute_osd_op(hctx, op); ret < 0) {
     return ret;
   }
-  outbl = std::move(op.outdata);
+  *outbl = std::move(op.outdata);
   return 0;
 }
 