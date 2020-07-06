d3eb060edcfe6b4ad18dad5b687041de867a8c42
Radoslaw Zarzynski <rzarzyns@redhat.com>
2020-07-06 18:50:11 +0200

    crimson/common: switch errorator to use std::apply directly.
    This is needed as newer versions of the framework have
    the `seastar::apply` compatibility layer stripped.
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/crimson/common/errorator.h
+++ src/crimson/common/errorator.h
@@ -1038,8 +1038,8 @@ struct futurize<Container<::crimson::errorated_future_marker<Values...>>> {
   [[gnu::always_inline]]
   static inline type apply(Func&& func, std::tuple<FuncArgs...>&& args) noexcept {
     try {
-      return ::seastar::apply(std::forward<Func>(func),
-                              std::forward<std::tuple<FuncArgs...>>(args));
+      return std::apply(std::forward<Func>(func),
+                        std::forward<std::tuple<FuncArgs...>>(args));
     } catch (...) {
       return make_exception_future(std::current_exception());
     }
