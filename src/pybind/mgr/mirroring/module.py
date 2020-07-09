f1cb9317e5b8e57f046dbec8042b0d2d2f0027e2
Venky Shankar <vshankar@redhat.com>
2020-07-09 06:43:47 -0400

    pybind/mgr/mirroring: interface to mirror CephFS directory snapshots
    Signed-off-by: Venky Shankar <vshankar@redhat.com>
    (cherry picked from commit b7acf7fc77d8c7865935643c3eb484be2f9999a1)

---
+++ src/pybind/mgr/mirroring/module.py
@@ -0,0 +1,70 @@
+from typing import List, Optional
+
+from mgr_module import MgrModule, CLIReadCommand, CLIWriteCommand, Option
+
+from .fs.snapshot_mirror import FSSnapshotMirror
+
+class Module(MgrModule):
+    MODULE_OPTIONS: List[Option] = []
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fs_snapshot_mirror = FSSnapshotMirror(self)
+
+    def notify(self, notify_type, notify_id):
+        self.fs_snapshot_mirror.notify(notify_type)
+
+    @CLIWriteCommand('fs snapshot mirror enable')
+    def snapshot_mirror_enable(self,
+                               fs_name: str):
+        """Enable snapshot mirroring for a filesystem"""
+        return self.fs_snapshot_mirror.enable_mirror(fs_name)
+
+    @CLIWriteCommand('fs snapshot mirror disable')
+    def snapshot_mirror_disable(self,
+                                fs_name: str):
+        """Disable snapshot mirroring for a filesystem"""
+        return self.fs_snapshot_mirror.disable_mirror(fs_name)
+
+    @CLIWriteCommand('fs snapshot mirror peer_add')
+    def snapshot_mirorr_peer_add(self,
+                                 fs_name: str,
+                                 remote_cluster_spec: str,
+                                 remote_fs_name: Optional[str] = None):
+        """Add a remote filesystem peer"""
+        return self.fs_snapshot_mirror.peer_add(fs_name, remote_cluster_spec,
+                                                remote_fs_name)
+
+    @CLIWriteCommand('fs snapshot mirror peer_remove')
+    def snapshot_mirror_peer_remove(self,
+                                    fs_name: str,
+                                    peer_uuid: str):
+        """Remove a filesystem peer"""
+        return self.fs_snapshot_mirror.peer_remove(fs_name, peer_uuid)
+
+    @CLIWriteCommand('fs snapshot mirror add')
+    def snapshot_mirror_add_dir(self,
+                                fs_name: str,
+                                path: str):
+        """Add a directory for snapshot mirroring"""
+        return self.fs_snapshot_mirror.add_dir(fs_name, path)
+
+    @CLIWriteCommand('fs snapshot mirror remove')
+    def snapshot_mirror_remove_dir(self,
+                                   fs_name: str,
+                                   path: str):
+        """Remove a snapshot mirrored directory"""
+        return self.fs_snapshot_mirror.remove_dir(fs_name, path)
+
+    @CLIReadCommand('fs snapshot mirror dirmap')
+    def snapshot_mirror_dirmap(self,
+                               fs_name: str,
+                               path: str):
+        """Get current mirror instance map for a directory"""
+        return self.fs_snapshot_mirror.status(fs_name, path)
+
+    @CLIReadCommand('fs snapshot mirror show distribution')
+    def snapshot_mirror_distribution(self,
+                                     fs_name: str):
+        """Get current instance to directory map for a filesystem"""
+        return self.fs_snapshot_mirror.show_distribution(fs_name)
