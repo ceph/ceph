88d6b69d3de20d6338d061019eb26ed3147007cb
Mykola Golub <mgolub@suse.com>
2020-07-09 14:36:39 +0100

    librbd: properly unregister failed async_complete request
    Signed-off-by: Mykola Golub <mgolub@suse.com>
    (cherry picked from commit 143179ffa92ce64d89fc98d6d6c9e7a3722a12f2)
    
    Conflicts:
    	src/librbd/ImageWatcher.cc: RWLock vs std::unique_lock

--- src/librbd/ImageWatcher.cc
+++ src/librbd/ImageWatcher.cc
@@ -158,12 +158,13 @@ void ImageWatcher<I>::handle_async_complete(const AsyncRequestId &request,
 			   << cpp_strerror(ret_val) << dendl;
     if (ret_val == -ETIMEDOUT && !is_unregistered()) {
       schedule_async_complete(request, r);
+      m_async_op_tracker.finish_op();
+      return;
     }
-  } else {
-    RWLock::WLocker async_request_locker(m_async_request_lock);
-    m_async_pending.erase(request);
   }
 
+  RWLock::WLocker async_request_locker(m_async_request_lock);
+  m_async_pending.erase(request);
   m_async_op_tracker.finish_op();
 }
 
