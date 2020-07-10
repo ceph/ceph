03a4d18c1b5a16e6bcd79bc44aa9a1a0eb643c35
Yan, Zheng <zyan@redhat.com>
2020-07-10 17:56:19 +0800

    mds: explicitly track files with client writeable ranges
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>
    (cherry picked from commit 44f67be02b8e8aa8fc69283bf12107c2df3f8277)
    
     Conflicts:
    	src/mds/CInode.cc
    	src/mds/Locker.cc
    	src/mds/MDCache.cc
    	src/mds/Server.cc
    	src/mds/StrayManager.cc

--- src/mds/Server.cc
+++ src/mds/Server.cc
@@ -4391,6 +4391,7 @@ void Server::handle_client_openc(MDRequestRef& mdr)
     in->inode.client_ranges[client].range.first = 0;
     in->inode.client_ranges[client].range.last = in->inode.layout.stripe_unit;
     in->inode.client_ranges[client].follows = follows;
+    in->mark_clientwriteable();
     cap->mark_clientwriteable();
   }
   
@@ -5055,6 +5056,7 @@ void Server::do_open_truncate(MDRequestRef& mdr, int cmode)
     pi.inode.client_ranges[client].range.last = pi.inode.get_layout_size_increment();
     pi.inode.client_ranges[client].follows = realm->get_newest_seq();
     changed_ranges = true;
+    in->mark_clientwriteable();
     cap->mark_clientwriteable();
   }
   
@@ -6055,6 +6057,7 @@ void Server::handle_client_mknod(MDRequestRef& mdr)
       newi->inode.client_ranges[client].range.first = 0;
       newi->inode.client_ranges[client].range.last = newi->inode.layout.stripe_unit;
       newi->inode.client_ranges[client].follows = follows;
+      newi->mark_clientwriteable();
       cap->mark_clientwriteable();
     }
   }
