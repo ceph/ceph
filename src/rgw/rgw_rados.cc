fbe68421656650e7d77e64491096587f8f21337d
Shilpa Jagannath <smanjara@redhat.com>
2020-07-14 13:03:31 +0530

    rgw: allow clean_index to take const bucket_index_layout_generation&
    fix bi_get() to get objects after being resharded
    
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -2337,8 +2337,8 @@ int RGWRados::create_bucket(const RGWUserInfo& owner, rgw_bucket& bucket,
 
       /* only remove it if it's a different bucket instance */
       if (orig_info.bucket.bucket_id != bucket.bucket_id) {
-        int r = svc.bi->clean_index(dpp, info, info.layout.current_index.gen);
-        if (r < 0) {
+	int r = svc.bi->clean_index(dpp, info, info.layout.current_index);
+	if (r < 0) {
 	  ldpp_dout(dpp, 0) << "WARNING: could not remove bucket index (r=" << r << ")" << dendl;
 	}
         r = ctl.bucket->remove_bucket_instance_info(info.bucket, info, null_yield, dpp);
