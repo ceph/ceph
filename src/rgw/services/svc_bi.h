acd3347263382df7129ecbdd1f894feadff359f5
Shilpa Jagannath <smanjara@redhat.com>
2020-07-14 13:03:31 +0530

    rgw: allow clean_index to take const bucket_index_layout_generation&
    fix bi_get() to get objects after being resharded
    
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>

--- src/rgw/services/svc_bi.h
+++ src/rgw/services/svc_bi.h
@@ -30,7 +30,7 @@ public:
   virtual ~RGWSI_BucketIndex() {}
 
   virtual int init_index(const DoutPrefixProvider *dpp, RGWBucketInfo& bucket_info, const rgw::bucket_index_layout_generation& idx_layout) = 0;
-  virtual int clean_index(const DoutPrefixProvider *dpp, RGWBucketInfo& bucket_info, uint64_t gen) = 0;
+  virtual int clean_index(const DoutPrefixProvider *dpp, RGWBucketInfo& bucket_info, const rgw::bucket_index_layout_generation& idx_layout) = 0;
 
   virtual int read_stats(const DoutPrefixProvider *dpp,
                          const RGWBucketInfo& bucket_info,
