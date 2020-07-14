2884c8cf2f519d20487e983f07fd9d41016782df
Shilpa Jagannath <smanjara@redhat.com>
2020-07-14 13:03:31 +0530

    rgw: allow clean_index to take const bucket_index_layout_generation&
    fix bi_get() to get objects after being resharded
    
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>

--- src/rgw/services/svc_bi_rados.h
+++ src/rgw/services/svc_bi_rados.h
@@ -57,7 +57,7 @@ class RGWSI_BucketIndex_RADOS : public RGWSI_BucketIndex
   int get_bucket_index_object(const std::string& bucket_oid_base,
 			      const std::string& obj_key,
                               uint32_t num_shards, rgw::BucketHashType hash_type,
-                              std::string* bucket_obj, int* shard_id);
+                              uint64_t gen_id, std::string *bucket_obj, int *shard_id);
 
   int cls_bucket_head(const DoutPrefixProvider *dpp,
 		      const RGWBucketInfo& bucket_info,
@@ -98,10 +98,8 @@ public:
     return rgw_shards_mod(sid2, num_shards);
   }
 
-  int init_index(const DoutPrefixProvider *dpp, RGWBucketInfo& bucket_info,
-		 const rgw::bucket_index_layout_generation& idx_layout) override;
-  int clean_index(const DoutPrefixProvider *dpp, RGWBucketInfo& bucket_info,
-		  uint64_t gen) override;
+  int init_index(const DoutPrefixProvider *dpp, RGWBucketInfo& bucket_info,const rgw::bucket_index_layout_generation& idx_layout);
+  int clean_index(const DoutPrefixProvider *dpp, RGWBucketInfo& bucket_info, const rgw::bucket_index_layout_generation& idx_layout);
 
   /* RADOS specific */
 
