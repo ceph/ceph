ae61c21349a77f8cc770213537e099158ffd600d
Michael Fritch <mfritch@suse.com>
2020-07-15 16:23:59 -0600

    mgr/cephadm: assert the service_type during config/create
    ensure the passed ServiceSpec type matches the CephadmService type
    
    Signed-off-by: Michael Fritch <mfritch@suse.com>
    (cherry picked from commit f8186bd72f807df3c8a31b8c515d2a9e4bb69d09)

--- src/pybind/mgr/cephadm/services/monitoring.py
+++ src/pybind/mgr/cephadm/services/monitoring.py
@@ -13,9 +13,11 @@ class GrafanaService(CephadmService):
     DEFAULT_SERVICE_PORT = 3000
 
     def create(self, daemon_spec: CephadmDaemonSpec) -> str:
+        assert self.TYPE == daemon_spec.daemon_type
         return self.mgr._create_daemon(daemon_spec)
 
     def generate_config(self, daemon_spec: CephadmDaemonSpec) -> Tuple[Dict[str, Any], List[str]]:
+        assert self.TYPE == daemon_spec.daemon_type
         deps = []  # type: List[str]
 
         prom_services = []  # type: List[str]
@@ -76,9 +78,11 @@ class AlertmanagerService(CephadmService):
     DEFAULT_SERVICE_PORT = 9093
 
     def create(self, daemon_spec: CephadmDaemonSpec) -> str:
+        assert self.TYPE == daemon_spec.daemon_type
         return self.mgr._create_daemon(daemon_spec)
 
     def generate_config(self, daemon_spec: CephadmDaemonSpec) -> Tuple[Dict[str, Any], List[str]]:
+        assert self.TYPE == daemon_spec.daemon_type
         deps = [] # type: List[str]
 
         # dashboard(s)
@@ -143,9 +147,11 @@ class PrometheusService(CephadmService):
     DEFAULT_SERVICE_PORT = 9095
 
     def create(self, daemon_spec: CephadmDaemonSpec) -> str:
+        assert self.TYPE == daemon_spec.daemon_type
         return self.mgr._create_daemon(daemon_spec)
 
     def generate_config(self, daemon_spec: CephadmDaemonSpec) -> Tuple[Dict[str, Any], List[str]]:
+        assert self.TYPE == daemon_spec.daemon_type
         deps = []  # type: List[str]
 
         # scrape mgrs
@@ -230,7 +236,9 @@ class NodeExporterService(CephadmService):
     TYPE = 'node-exporter'
 
     def create(self, daemon_spec: CephadmDaemonSpec) -> str:
+        assert self.TYPE == daemon_spec.daemon_type
         return self.mgr._create_daemon(daemon_spec)
 
     def generate_config(self, daemon_spec: CephadmDaemonSpec) -> Tuple[Dict[str, Any], List[str]]:
+        assert self.TYPE == daemon_spec.daemon_type
         return {}, []
