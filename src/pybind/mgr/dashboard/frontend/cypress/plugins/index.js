6ba9325b38a8baa9be3b2cb675f7e0ef7ee7b64b
Tiago Melo <tmelo@suse.com>
2020-07-20 10:47:08 +0000

    mgr/dashboard: E2E: Disable GPU for Chrome Headless
    Fixes: https://tracker.ceph.com/issues/46624
    
    Signed-off-by: Tiago Melo <tmelo@suse.com>

---
+++ src/pybind/mgr/dashboard/frontend/cypress/plugins/index.js
@@ -0,0 +1,8 @@
+module.exports = (on, _config) => {
+  on('before:browser:launch', (browser, launchOptions) => {
+    if (browser.name === 'chrome' && browser.isHeadless) {
+      launchOptions.args.push('--disable-gpu');
+      return launchOptions;
+    }
+  });
+};
