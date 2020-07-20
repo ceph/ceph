95409d0dd3cc38ce9d713fbae1d4158e3e5ea636
Kyr Shatskyy <kyrylo.shatskyy@suse.com>
2020-07-20 11:25:11 +0200

    qa/tasks/cephadm: deprecate misc.sudo_write_file
    Signed-off-by: Kyr Shatskyy <kyrylo.shatskyy@suse.com>

--- qa/tasks/cephadm.py
+++ qa/tasks/cephadm.py
@@ -343,8 +343,7 @@ def ceph_bootstrap(ctx, config, registry):
         conf_fp = BytesIO()
         seed_config = build_initial_config(ctx, config)
         seed_config.write(conf_fp)
-        teuthology.write_file(
-            remote=bootstrap_remote,
+        bootstrap_remote.write_file(
             path='{}/seed.{}.conf'.format(testdir, cluster_name),
             data=conf_fp.getvalue())
         log.debug('Final config:\n' + conf_fp.getvalue().decode())
@@ -448,12 +447,10 @@ def ceph_bootstrap(ctx, config, registry):
             if remote == bootstrap_remote:
                 continue
             log.info('Writing (initial) conf and keyring to %s' % remote.shortname)
-            teuthology.write_file(
-                remote=remote,
+            remote.write_file(
                 path='/etc/ceph/{}.conf'.format(cluster_name),
                 data=ctx.ceph[cluster_name].config_file)
-            teuthology.write_file(
-                remote=remote,
+            remote.write_file(
                 path='/etc/ceph/{}.client.admin.keyring'.format(cluster_name),
                 data=ctx.ceph[cluster_name].admin_keyring)
 
@@ -813,12 +810,7 @@ def ceph_clients(ctx, config):
                 stdout=StringIO(),
             )
             keyring = r.stdout.getvalue()
-            teuthology.sudo_write_file(
-                remote=remote,
-                path=client_keyring,
-                data=keyring,
-                perms='0644'
-            )
+            remote.sudo_write_file(client_keyring, keyring, mode='0644')
     yield
 
 @contextlib.contextmanager
@@ -984,14 +976,14 @@ def distribute_config_and_admin_keyring(ctx, config):
     cluster_name = config['cluster']
     log.info('Distributing (final) config and client.admin keyring...')
     for remote, roles in ctx.cluster.remotes.items():
-        teuthology.sudo_write_file(
-            remote=remote,
-            path='/etc/ceph/{}.conf'.format(cluster_name),
-            data=ctx.ceph[cluster_name].config_file)
-        teuthology.sudo_write_file(
-            remote=remote,
+        remote.write_file(
+            '/etc/ceph/{}.conf'.format(cluster_name),
+            ctx.ceph[cluster_name].config_file,
+            sudo=True)
+        remote.write_file(
             path='/etc/ceph/{}.client.admin.keyring'.format(cluster_name),
-            data=ctx.ceph[cluster_name].admin_keyring)
+            data=ctx.ceph[cluster_name].admin_keyring,
+            sudo=True)
     try:
         yield
     finally:
@@ -1280,11 +1272,7 @@ def add_mirror_to_cluster(ctx, mirror):
             )
             new_config = toml.dumps(registries_add_mirror_to_docker_io(config.decode('utf-8'), mirror))
 
-            teuthology.sudo_write_file(
-                remote=remote,
-                path=registries_conf,
-                data=new_config,
-            )
+            remote.sudo_write_file(registries_conf, new_config)
         except IOError as e:  # py3: use FileNotFoundError instead.
             if e.errno != errno.ENOENT:
                 raise
