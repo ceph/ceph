81daeee02296ea8e03d89bec49477a8fd923500a
Radoslaw Zarzynski <rzarzyns@redhat.com>
2020-07-24 08:13:41 +0000

    kv: drop the MemDB backend.
    It's an experimental feature since 2016. It doesn't seem to be
    used anywhere nor actively maintained. However, it's the sole
    major user of `ceph::buffer::ptr::clone()` which could have
    been dropped otherwise.
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/kv/KeyValueDB.cc
+++ src/kv/KeyValueDB.cc
@@ -2,7 +2,6 @@
 // vim: ts=8 sw=2 smarttab
 
 #include "KeyValueDB.h"
-#include "MemDB.h"
 #include "RocksDBStore.h"
 
 using std::map;
@@ -16,10 +15,6 @@ KeyValueDB *KeyValueDB::create(CephContext *cct, const string& type,
   if (type == "rocksdb") {
     return new RocksDBStore(cct, dir, options, p);
   }
-  if ((type == "memdb") && 
-    cct->check_experimental_feature_enabled("memdb")) {
-    return new MemDB(cct, dir, p);
-  }
   return NULL;
 }
 
@@ -27,9 +22,6 @@ int KeyValueDB::test_init(const string& type, const string& dir)
 {
   if (type == "rocksdb") {
     return RocksDBStore::_test_init(dir);
-  }
-  if (type == "memdb") {
-    return MemDB::_test_init(dir);
   }
   return -EINVAL;
 }
