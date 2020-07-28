4db92f523c705b9d4e9ef26824982fdc1a309651
Jason Dillaman <dillaman@redhat.com>
2020-07-28 15:00:36 -0400

    librbd: invoke AioCompletion external callbacks via the op_work_queue
    The op_work_queue is single-threaded and will ensure clients won't
    receive concurrent callbacks. It is also a simpler approach than
    the original lock-free queue implementation that had issues with
    potential race conditions.
    
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>

--- src/librbd/io/AioCompletion.cc
+++ src/librbd/io/AioCompletion.cc
@@ -240,28 +240,13 @@ ssize_t AioCompletion::get_return_value() {
 void AioCompletion::complete_external_callback() {
   // ensure librbd external users never experience concurrent callbacks
   // from multiple librbd-internal threads.
-  ictx->external_callback_completions.push(this);
-
-  while (true) {
-    if (ictx->external_callback_in_progress.exchange(true)) {
-      // another thread is concurrently invoking external callbacks
-      break;
-    }
-
-    AioCompletion* aio_comp;
-    while (ictx->external_callback_completions.pop(aio_comp)) {
-      aio_comp->complete_cb(aio_comp->rbd_comp, aio_comp->complete_arg);
-      aio_comp->complete_event_socket();
-      aio_comp->notify_callbacks_complete();
-    }
-
-    ictx->external_callback_in_progress.store(false);
-    if (ictx->external_callback_completions.empty()) {
-      // queue still empty implies we didn't have a race between the last failed
-      // pop and resetting the in-progress state
-      break;
-    }
-  }
+  get();
+  ictx->op_work_queue->queue(new LambdaContext([this](int r) {
+      complete_cb(rbd_comp, complete_arg);
+      complete_event_socket();
+      notify_callbacks_complete();
+      put();
+    }));
 }
 
 void AioCompletion::complete_event_socket() {
