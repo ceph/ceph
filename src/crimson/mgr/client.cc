ceph/ceph

commit 1dfe1801724b50d9fef729549459e0f6721d1ad3

crimson/mon: only pick the addr of the same type
to avoid the attempts to connect an OSD which is bound to a v2 address
to a v1 addrss of a monitor.

in general, osd is bound to both v1 and v2 addresses, but crimson msgr
does not support multiple bound address at the time of writing, so to
avoid the failures when trying to connect to incompatible addresses,
let's filter out them when connecting to monitor. this change silence
warnings like:

peer_addr_for_me v1:172.21.15.106:60008/0 type doesn't match myaddr
v2:0.0.0.0:6802/26710

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/crimson/mon/MonClient.cc
@@ -957,7 +957,19 @@ seastar::future<> Client::reopen_session(int rank)
   pending_conns.reserve(mons.size());
   return seastar::parallel_for_each(mons, [this](auto rank) {
 #warning fixme
-    auto peer = monmap.get_addrs(rank).front();
+    auto peer = [&] {
+      auto& mon_addrs = monmap.get_addrs(rank);
+      if (msgr.get_myaddr().is_legacy()) {
+        return mon_addrs.legacy_addr();
+      } else {
+        return mon_addrs.msgr2_addr();
+      }
+    }();
+    if (peer == entity_addr_t{}) {
+      // crimson msgr only uses the first bound addr
+      logger().warn("mon.{} does not have an addr compatible with me", rank);
+      return seastar::now();
+    }
     logger().info("connecting to mon.{}", rank);
     return seastar::futurize_invoke(
         [peer, this] () -> seastar::future<Connection::AuthResult> {

commit b0ecb1fc2a8fbd6b8eb81007fb1a0d08ef27c7e6

crimson: connect to v2 address if myaddr.is_any()
if myaddr is any, both v1 and v2 would work for me.
this change addresses the regression introduced by 1dfe1801.

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/crimson/mgr/client.cc
@@ -105,10 +105,11 @@ seastar::future<> Client::reconnect()
   return seastar::sleep(a_while).then([this] {
     auto peer = [&] {
       auto& mgr_addrs = mgrmap.get_active_addrs();
-      if (msgr.get_myaddr().is_legacy()) {
-        return mgr_addrs.legacy_addr();
-      } else {
+      if (msgr.get_myaddr().is_any() ||
+          msgr.get_myaddr().is_msgr2()) {
         return mgr_addrs.msgr2_addr();
+      } else {
+        return mgr_addrs.legacy_addr();
       }
     }();
     if (peer == entity_addr_t{}) {

src/crimson/mon/MonClient.cc
@@ -959,10 +959,11 @@ seastar::future<> Client::reopen_session(int rank)
 #warning fixme
     auto peer = [&] {
       auto& mon_addrs = monmap.get_addrs(rank);
-      if (msgr.get_myaddr().is_legacy()) {
-        return mon_addrs.legacy_addr();
-      } else {
+      if (msgr.get_myaddr().is_any() ||
+          msgr.get_myaddr().is_msgr2()) {
         return mon_addrs.msgr2_addr();
+      } else {
+        return mon_addrs.legacy_addr();
       }
     }();
     if (peer == entity_addr_t{}) {