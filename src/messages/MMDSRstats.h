ideepika/ceph-ci

mds: gather status of rstats progagation from multiple mds
Signed-off-by: "Yan, Zheng" <zyan@redhat.com>

src/messages/MMDSRstats.h
@@ -1,0 +1,53 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef CEPH_MMDSRSTATS_H
+#define CEPH_MMDSRSTATS_H
+
+#include "msg/Message.h"
+
+class MMDSRstats : public SafeMessage {
+  static constexpr int HEAD_VERSION = 1;
+  static constexpr int COMPAT_VERSION = 1;
+
+public:
+  std::string_view get_type_name() const override { return "mds_rstats"; }
+  void print(ostream& o) const override {
+    o << "mds_rstats(e" << epoch << " f " << flushed_to << " a " << advance_to << ")";
+  }
+
+  unsigned get_epoch() const { return epoch; }
+  utime_t get_flushed_to() const { return flushed_to; }
+  utime_t get_advance_to() const { return advance_to; }
+
+  void encode_payload(uint64_t features) override {
+    using ceph::encode;
+    encode(epoch, payload);
+    encode(flushed_to, payload);
+    encode(advance_to, payload);
+  }
+  void decode_payload() override {
+    using ceph::decode;
+    auto p = payload.cbegin();
+    decode(epoch, p);
+    decode(flushed_to, p);
+    decode(advance_to, p);
+  }
+
+protected:
+  MMDSRstats() : SafeMessage(MSG_MDS_RSTATS, HEAD_VERSION, COMPAT_VERSION) {}
+  MMDSRstats(unsigned e, const utime_t& f, const utime_t& a) :
+    SafeMessage(MSG_MDS_RSTATS, HEAD_VERSION, COMPAT_VERSION),
+    epoch(e), flushed_to(f), advance_to(a) {}
+  ~MMDSRstats() override {}
+
+private:
+  unsigned epoch;
+  utime_t flushed_to;
+  utime_t advance_to;
+
+  template<class T, typename... Args>
+  friend boost::intrusive_ptr<T> ceph::make_message(Args&&... args);
+};
+
+#endif

src/msg/Message.h
@@ -171,6 +171,7 @@
 #define MSG_MDS_FRAGMENTNOTIFYACK  0x212
 #define MSG_MDS_LOCK               0x300
 #define MSG_MDS_INODEFILECAPS      0x301
+#define MSG_MDS_RSTATS             0x302
 
 #define MSG_MDS_EXPORTDIRDISCOVER     0x449
 #define MSG_MDS_EXPORTDIRDISCOVERACK  0x450

ceph/ceph

src/messages/MMDSRstats.h
@@ -1,0 +1,53 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#ifndef CEPH_MMDSRSTATS_H
+#define CEPH_MMDSRSTATS_H
+
+#include "messages/MMDSOp.h"
+
+class MMDSRstats : public MMDSOp {
+  static constexpr int HEAD_VERSION = 1;
+  static constexpr int COMPAT_VERSION = 1;
+
+public:
+  std::string_view get_type_name() const override { return "mds_rstats"; }
+  void print(ostream& o) const override {
+    o << "mds_rstats(e" << epoch << " f " << flushed_to << " a " << advance_to << ")";
+  }
+
+  unsigned get_epoch() const { return epoch; }
+  utime_t get_flushed_to() const { return flushed_to; }
+  utime_t get_advance_to() const { return advance_to; }
+
+  void encode_payload(uint64_t features) override {
+    using ceph::encode;
+    encode(epoch, payload);
+    encode(flushed_to, payload);
+    encode(advance_to, payload);
+  }
+  void decode_payload() override {
+    using ceph::decode;
+    auto p = payload.cbegin();
+    decode(epoch, p);
+    decode(flushed_to, p);
+    decode(advance_to, p);
+  }
+
+protected:
+  MMDSRstats() : MMDSOp(MSG_MDS_RSTATS, HEAD_VERSION, COMPAT_VERSION) {}
+  MMDSRstats(unsigned e, const utime_t& f, const utime_t& a) :
+    MMDSOp(MSG_MDS_RSTATS, HEAD_VERSION, COMPAT_VERSION),
+    epoch(e), flushed_to(f), advance_to(a) {}
+  ~MMDSRstats() override {}
+
+private:
+  unsigned epoch;
+  utime_t flushed_to;
+  utime_t advance_to;
+
+  template<class T, typename... Args>
+  friend boost::intrusive_ptr<T> ceph::make_message(Args&&... args);
+};
+
+#endif

src/msg/Message.h
@@ -192,6 +192,7 @@
 #define MSG_MDS_HEARTBEAT          0x500  // for mds load balancer
 #define MSG_MDS_METRICS            0x501  // for mds metric aggregator
 #define MSG_MDS_PING               0x502  // for mds pinger
+#define MSG_MDS_RSTATS             0x503
 
 // *** generic ***
 #define MSG_TIMECHECK             0x600