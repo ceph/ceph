f94c276accb8c9f4096b3bffc95362a921f2a2a5
Jason Dillaman <dillaman@redhat.com>
2020-08-05 09:12:41 -0400

    librbd: migration abort should revert data back to the original image
    If the migration destination image was modified and then the migration
    was aborted, we need to copy the data back to the source image to avoid
    losing data. For simplicity we will only revert the HEAD revision state
    and will not attempt to copy new snapshots on the destination image
    back to the source.
    
    Fixes: https://tracker.ceph.com/issues/41394
    Signed-off-by: Jason Dillaman <dillaman@redhat.com>
    (cherry picked from commit 5bd15da8be09a4e7644d411a0b0c132e5b795393)
    
    Conflicts:
    	src/librbd/api/Migration.cc: trivial resolution

--- src/test/librbd/test_Migration.cc
+++ src/test/librbd/test_Migration.cc
@@ -1116,6 +1116,28 @@ TEST_F(TestMigration, AbortInUseImage) {
                                                     no_op));
 }
 
+TEST_F(TestMigration, AbortWithoutSnapshots) {
+  test_no_snaps();
+  migration_prepare(m_ioctx, m_image_name);
+  migration_status(RBD_IMAGE_MIGRATION_STATE_PREPARED);
+  test_no_snaps();
+  migration_abort(m_ioctx, m_image_name);
+}
+
+TEST_F(TestMigration, AbortWithSnapshots) {
+  test_snaps();
+  migration_prepare(m_ioctx, m_image_name);
+  migration_status(RBD_IMAGE_MIGRATION_STATE_PREPARED);
+
+  test_no_snaps();
+  flush();
+  ASSERT_EQ(0, TestFixture::snap_create(*m_ictx, "dst-only-snap"));
+
+  test_no_snaps();
+
+  migration_abort(m_ioctx, m_image_name);
+}
+
 TEST_F(TestMigration, CloneV1Parent)
 {
   const uint32_t CLONE_FORMAT = 1;
