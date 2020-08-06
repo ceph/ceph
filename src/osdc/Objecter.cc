b761ca9105aa50eec0095e3c9de4f9009439c1c1
Simon Gao <simon29rock@gmail.com>
2020-08-06 21:38:18 +0800

    client : additional processing of remove_snaps on the latest map  the original processing scheme doesn't handle the removed snaps of latest osdmap
    Signed-off-by: Simon Gao <simon29rock@gmail.com>

--- src/osdc/Objecter.cc
+++ src/osdc/Objecter.cc
@@ -1165,6 +1165,7 @@ void Objecter::handle_osd_map(MOSDMap *m)
 		  << m->get_first() << "," << m->get_last()
 		  << "] > " << osdmap->get_epoch() << dendl;
 
+    mempool::osdmap::map<int64_t, snap_interval_set_t> removed_snaps;
     if (osdmap->get_epoch()) {
       bool skipped_map = false;
       // we want incrementals
@@ -1213,10 +1214,11 @@ void Objecter::handle_osd_map(MOSDMap *m)
 	cluster_full = cluster_full || _osdmap_full_flag();
 	update_pool_full_map(pool_full_map);
 
-	// check all outstanding requests on every epoch
-	for (auto& i : need_resend) {
-	  _prune_snapc(osdmap->get_new_removed_snaps(), i.second);
+	// collect removed snap in every past osdmaps
+	for (auto& [pool, snaps] : osdmap->get_new_removed_snaps()) {
+	  removed_snaps[pool].insert(snaps);
 	}
+
 	_scan_requests(homeless_session, skipped_map, cluster_full,
 		       &pool_full_map, need_resend,
 		       need_resend_linger, need_resend_command, sul);
@@ -1238,6 +1240,9 @@ void Objecter::handle_osd_map(MOSDMap *m)
 	ceph_assert(e == osdmap->get_epoch());
       }
 
+      for (auto& i : need_resend) {
+	_prune_snapc(removed_snaps, i.second);
+      }
     } else {
       // first map.  we want the full thing.
       if (m->maps.count(m->get_last())) {
