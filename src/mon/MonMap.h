2f075704073ff80f94c70cf79516028d2754ae4f
Shyamsundar Ranganathan <srangana@redhat.com>
2020-08-09 20:47:43 -0400

    mon: store mon updates in ceph context for future MonMap instantiation
    MonMap builds initial mon list using provided sources, like
    mon-host or monmap.
    
    For future instantiations of MonClient, if mon addresses are
    updated, stale information from the provided sources are used.
    
    This commit retains mon updates that are processed by the
    MonClient in CephContext, for use in MonMap instantiations
    and hence uses updated information as required.
    
    This is helpful in cases where librados or libcephfs
    instantiate MonClient in the ceph-mgr deamon as required.
    
    Fixes: https://tracker.ceph.com/issues/46645
    Signed-off-by: Shyamsundar Ranganathan <srangana@redhat.com>
    (cherry picked from commit 7a1f02acfe6b5d8a760efd16bb594a0656b39eac)
    Conflicts:
        src/mon/MonMap.h
    Retain crimson namespace as ceph::common
        src/mon/MonMap.cc
    Address merge conflict due to linespace removed
        src/common/ceph_context.cc
    Remove WITH_ALIEN latch
        src/common/ceph_context/h
    Remove WITH_ALIEN latch

--- src/mon/MonMap.h
+++ src/mon/MonMap.h
@@ -437,6 +437,18 @@ public:
   static void generate_test_instances(list<MonMap*>& o);
 protected:
   /**
+   * build a monmap from a list of entity_addrvec_t's
+   *
+   * Give mons dummy names.
+   *
+   * @param addrs  list of entity_addrvec_t's
+   * @param prefix prefix to prepend to generated mon names
+   * @return 0 for success, -errno on error
+   */
+  int init_with_addrs(const std::vector<entity_addrvec_t>& addrs,
+        bool for_mkfs,
+        std::string_view prefix);
+  /**
    * build a monmap from a list of ips
    *
    * Give mons dummy names.
@@ -447,7 +459,7 @@ protected:
    */
   int init_with_ips(const std::string& ips,
 		    bool for_mkfs,
-		    const std::string &prefix);
+		    std::string_view prefix);
   /**
    * build a monmap from a list of hostnames
    *
@@ -459,7 +471,7 @@ protected:
    */
   int init_with_hosts(const std::string& hostlist,
 		      bool for_mkfs,
-		      const std::string& prefix);
+		      std::string_view prefix);
   int init_with_config_file(const ConfigProxy& conf, std::ostream& errout);
 #if WITH_SEASTAR
   seastar::future<> read_monmap(const std::string& monmap);
