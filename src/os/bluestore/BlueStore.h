4cb8b58df1260cdbf6c22ab84492c3ee25b9208a
Abutalib Aghayev <agayev@cs.cmu.edu>
2020-08-26 10:36:27 -0400

    os/bluestore: Pass in zoned device parameters to allocator creation.
    A new Allocator::create call was added, which did not pass in zoned device
    parameters.  As a result, BlueStore stopped working on zoned devices.  This
    patch passes in the zoned device parameters and restores BlueStore functionality
    on zoned devices.
    
    Signed-off-by: Abutalib Aghayev <agayev@cs.cmu.edu>

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -2393,18 +2393,8 @@ private:
 				   bool create);
 
   // Functions related to zoned storage.
-
-  // For now, to avoid interface changes we piggyback zone_size (in MiB) and the
-  // first sequential zone number onto min_alloc_size and pass it to functions
-  // Allocator::create and FreelistManager::create.
-  uint64_t _piggyback_zoned_device_parameters_onto(uint64_t min_alloc_size) {
-    uint64_t zone_size = bdev->get_zone_size();
-    uint64_t zone_size_mb = zone_size / (1024 * 1024);
-    uint64_t first_seq_zone = bdev->get_conventional_region_size() / zone_size;
-    min_alloc_size |= (zone_size_mb << 32);
-    min_alloc_size |= (first_seq_zone << 48);
-    return min_alloc_size;
-  }
+  uint64_t _zoned_piggyback_device_parameters_onto(uint64_t min_alloc_size);
+  int _zoned_check_config_settings();
 
 public:
   utime_t get_deferred_last_submitted() {
