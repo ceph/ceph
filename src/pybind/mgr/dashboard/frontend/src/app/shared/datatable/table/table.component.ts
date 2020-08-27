ceph/ceph

commit 31e9bbb9b308277d2f89390d5ff62f13457d69cb

mgr/dashboard: Datatable catches select events from other datatables
Ensure we do not process DOM 'select' events, see https://github.com/swimlane/ngx-datatable/issues/899.

Fixes: https://tracker.ceph.com/issues/46903
Signed-off-by: Volker Theile <vtheile@suse.com>

src/pybind/mgr/dashboard/frontend/src/app/shared/datatable/table/table.component.ts
@@ -696,8 +696,12 @@ export class TableComponent implements AfterContentChecked, OnInit, OnChanges, O
   }
 
   onSelect($event: any) {
-    this.selection.selected = $event['selected'];
-    this.updateSelection.emit(_.clone(this.selection));
+    // Ensure we do not process DOM 'select' events.
+    // https://github.com/swimlane/ngx-datatable/issues/899
+    if (_.has($event, 'selected')) {
+      this.selection.selected = $event['selected'];
+      this.updateSelection.emit(_.clone(this.selection));
+    }
   }
 
   toggleColumn(column: CdTableColumn) {

commit c9a73185a9d8348d0a5e72b76b6bcabd5ac34da8

mgr/dashboard: Fix stale table actions
If we don't emit the select event, the action button can become stale and not
update when the data changes.

This fixes a regression introduced in 31e9bbb9b308277d2f89390d5ff62f13457d69cb

Fixes: https://tracker.ceph.com/issues/47175

Signed-off-by: Tiago Melo <tmelo@suse.com>

src/pybind/mgr/dashboard/frontend/src/app/shared/datatable/table/table.component.ts
@@ -700,8 +700,8 @@ export class TableComponent implements AfterContentChecked, OnInit, OnChanges, O
     // https://github.com/swimlane/ngx-datatable/issues/899
     if (_.has($event, 'selected')) {
       this.selection.selected = $event['selected'];
-      this.updateSelection.emit(_.clone(this.selection));
     }
+    this.updateSelection.emit(_.clone(this.selection));
   }
 
   toggleColumn(column: CdTableColumn) {