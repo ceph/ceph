39058d75ebfa52ab273cc9961732baf6b6ada146
Michael Fritch <mfritch@suse.com>
2020-09-01 12:32:04 -0600

    mgr/cephadm: move `name_to_auth_entity` to `get_auth_entity`
    refactor the logic for getting the keyring auth entity from the utils
    module into the CephadmService class
    
    Signed-off-by: Michael Fritch <mfritch@suse.com>

--- src/pybind/mgr/cephadm/utils.py
+++ src/pybind/mgr/cephadm/utils.py
@@ -13,7 +13,6 @@ T = TypeVar('T')
 logger = logging.getLogger(__name__)
 
 ConfEntity = NewType('ConfEntity', str)
-AuthEntity = NewType('AuthEntity', str)
 
 
 class CephadmNoImage(Enum):
@@ -35,29 +34,6 @@ def name_to_config_section(name: str) -> ConfEntity:
         return ConfEntity(name)
     else:
         return ConfEntity('mon')
-
-
-def name_to_auth_entity(daemon_type: str,
-                        daemon_id: str,
-                        host: str = "",
-                        ) -> AuthEntity:
-    """
-    Map from daemon names/host to ceph entity names (as seen in config)
-    """
-    if daemon_type in ['rgw', 'rbd-mirror', 'nfs', "iscsi"]:
-        return AuthEntity('client.' + daemon_type + "." + daemon_id)
-    elif daemon_type == 'crash':
-        if host == "":
-            raise OrchestratorError("Host not provided to generate <crash> auth entity name")
-        return AuthEntity('client.' + daemon_type + "." + host)
-    elif daemon_type == 'mon':
-        return AuthEntity('mon.')
-    elif daemon_type == 'mgr':
-        return AuthEntity(daemon_type + "." + daemon_id)
-    elif daemon_type in ['osd', 'mds', 'client']:
-        return AuthEntity(daemon_type + "." + daemon_id)
-    else:
-        raise OrchestratorError("unknown auth entity name")
 
 
 def forall_hosts(f: Callable[..., T]) -> Callable[..., List[T]]:
