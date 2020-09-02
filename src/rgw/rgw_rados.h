529527efa58adaac5771f73361c29562fdeb2b26
Radoslaw Zarzynski <rzarzyns@redhat.com>
2020-09-02 23:59:24 +0200

    rgw: drop UpdateIndex::prepared & co.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -826,6 +826,7 @@ public:
       int _do_write_meta(uint64_t size, uint64_t accounted_size,
                      map<std::string, bufferlist>& attrs,
                      bool modify_tail, bool assume_noent,
+                     RGWObjState *state,
                      void *index_op, optional_yield y);
       int write_meta(uint64_t size, uint64_t accounted_size,
                      map<std::string, bufferlist>& attrs, optional_yield y);
@@ -927,7 +928,6 @@ public:
       BucketShard bs;
       bool bs_initialized{false};
       bool blind;
-      bool prepared{false};
       rgw_zone_set *zones_trace{nullptr};
 
       int init_bs() {
@@ -981,8 +981,6 @@ public:
       int cancel();
 
       const string *get_optag() { return &optag; }
-
-      bool is_prepared() { return prepared; }
     }; // class UpdateIndex
 
     class List {
