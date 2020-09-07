4b5a5c33e63ac4294563b2c0b5135f1f4f3e8bdf
Radoslaw Zarzynski <rzarzyns@redhat.com>
2020-09-07 20:24:42 +0200

    cls/rgw: unaccount_entry() takes entry by const ref.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/cls/rgw/cls_rgw.cc
+++ src/cls/rgw/cls_rgw.cc
@@ -951,7 +951,7 @@ int rgw_bucket_prepare_op(cls_method_context_t hctx, bufferlist *in, bufferlist 
 } // rgw_bucket_prepare_op
 
 static void unaccount_entry(rgw_bucket_dir_header& header,
-			    rgw_bucket_dir_entry& entry)
+			    const rgw_bucket_dir_entry& entry)
 {
   if (entry.exists) {
     rgw_bucket_category_stats& stats = header.stats[entry.meta.category];
