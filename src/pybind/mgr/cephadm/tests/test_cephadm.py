3bffddb6411d5d7a690c2199706f99655035bd2f
Sebastian Wagner <sebastian.wagner@suse.com>
2020-09-11 13:26:46 +0200

    mgr/cephadm: move _apply_service to serve.py
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>
    (cherry picked from commit 6b2664a9e3dd531d99b5cc7f01384298e0bdc055)

--- src/pybind/mgr/cephadm/tests/test_cephadm.py
+++ src/pybind/mgr/cephadm/tests/test_cephadm.py
@@ -324,7 +324,7 @@ class TestCephadm(object):
     def test_mgr_update(self, cephadm_module):
         with with_host(cephadm_module, 'test'):
             ps = PlacementSpec(hosts=['test:0.0.0.0=a'], count=1)
-            r = cephadm_module._apply_service(ServiceSpec('mgr', placement=ps))
+            r = CephadmServe(cephadm_module)._apply_service(ServiceSpec('mgr', placement=ps))
             assert r
 
             assert_rm_daemon(cephadm_module, 'mgr.a', 'test')
@@ -527,7 +527,7 @@ class TestCephadm(object):
                 match_glob(out, "Deployed rgw.realm.zone1.host1.* on host 'host1'")
 
                 ps = PlacementSpec(hosts=['host1', 'host2'], count=2)
-                r = cephadm_module._apply_service(
+                r = CephadmServe(cephadm_module)._apply_service(
                     RGWSpec(rgw_realm='realm', rgw_zone='zone1', placement=ps))
                 assert r
 
