b5e106a0ef84426e0d04b8b24a7e499afcc07908
David Zafman <dzafman@redhat.com>
2020-09-11 21:46:55 +0000

    mon: Add osd version collector
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- src/mon/Monitor.cc
+++ src/mon/Monitor.cc
@@ -5363,6 +5363,8 @@ void Monitor::get_all_versions(std::map<string, list<string> > &versions)
 {
   // mon
   get_versions(versions);
+  // osd
+  osdmon()->get_versions(versions);
   dout(20) << __func__ << " all versions=" << versions << dendl;
 }
 
