111a145371bc503bd8384f937010fe533c865f4a
Kefu Chai <kchai@redhat.com>
2020-09-14 14:22:40 +0800

    crimson/osd: add ObjectContext::wait_recovery_read()
    instead of reusing ObjectContext::get_recovery_read() for both
    sync call and async call. just add a new method for the async call
    for better readability
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/crimson/osd/object_context.h
+++ src/crimson/osd/object_context.h
@@ -203,14 +203,16 @@ public:
   void drop_read() {
     return put_lock_type(RWState::RWREAD);
   }
-  seastar::future<bool> get_recovery_read(bool can_wait = false) {
-    if (bool acquired = rwstate.get_recovery_read();
-        acquired || !can_wait) {
-      return seastar::make_ready_future<bool>(acquired);
+  bool get_recovery_read() {
+    return rwstate.get_recovery_read();
+  }
+  seastar::future<> wait_recovery_read() {
+    if (rwstate.get_recovery_read()) {
+      return seastar::make_ready_future<>();
     }
     return with_queue([this] {
       return rwstate.get_recovery_read();
-    }).then([] {return seastar::make_ready_future<bool>(true); });
+    });
   }
   void drop_recovery_read() {
     ceph_assert(rwstate.recovery_read_marker);
