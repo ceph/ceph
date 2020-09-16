3b55e33e13cbe2a080a0c40356acc51e39436c20
Radoslaw Zarzynski <rzarzyns@redhat.com>
2020-09-16 23:25:16 +0200

    cls/rgw: move op_type to CLSRGWBilogOp.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/cls/rgw/cls_rgw_ops.h
+++ src/cls/rgw/cls_rgw_ops.h
@@ -1527,11 +1527,10 @@ struct CLSRGWBilogOp {
   const std::string& op_tag;
   const rgw_zone_set* const zones_trace;
   const uint16_t bilog_flags;
+  const enum RGWModifyOp op_type;
 };
 
 struct CLSRGWCompleteModifyOpBase : CLSRGWBilogOp {
-  const enum RGWModifyOp op_type;
-
   void complete_op(librados::ObjectWriteOperation& o,
                    const rgw_bucket_entry_ver& ver,
                    const rgw_bucket_dir_entry_meta& dir_meta,
@@ -1547,8 +1546,6 @@ struct CLSRGWCompleteModifyOp : CLSRGWCompleteModifyOpBase {
 };
 
 struct CLSRGWLinkOLHBase : CLSRGWBilogOp {
-  const enum RGWModifyOp op_type;
-
   static RGWModifyOp get_bilog_op_type(const bool delete_marker) {
      return delete_marker ? CLS_RGW_OP_LINK_OLH_DM
                           : CLS_RGW_OP_LINK_OLH;
@@ -1575,6 +1572,11 @@ struct CLSRGWLinkOLH : CLSRGWLinkOLHBase {
 };
 
 struct CLSRGWUnlinkInstance : CLSRGWBilogOp {
+  template <class... Args>
+  CLSRGWUnlinkInstance(Args&&... args)
+    : CLSRGWBilogOp { std::forward<Args>(args)..., get_bilog_op_type() } {
+  }
+
   constexpr static enum RGWModifyOp get_bilog_op_type() {
     return CLS_RGW_OP_UNLINK_INSTANCE;
   }
