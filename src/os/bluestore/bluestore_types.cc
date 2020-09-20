7b44d545a3ed77fcf99ab4211cde08fd0771acd8
Igor Fedotov <ifedotov@suse.com>
2020-09-20 12:22:05 +0300

    os/bluestore: add missing structs to bluestore_cache_other mempool
    Fixes: https://tracker.ceph.com/issues/47551
    Signed-off-by: Igor Fedotov <ifedotov@suse.com>
    (cherry picked from commit 4592c5df554190065fa9a994f7b6dc3fed7ea214)

--- src/os/bluestore/bluestore_types.cc
+++ src/os/bluestore/bluestore_types.cc
@@ -358,6 +358,10 @@ void bluestore_blob_use_tracker_t::allocate()
 {
   ceph_assert(num_au != 0);
   bytes_per_au = new uint32_t[num_au];
+  mempool::get_pool(
+    mempool::pool_index_t(mempool::mempool_bluestore_cache_other)).
+      adjust_count(1, sizeof(uint32_t) * num_au);
+
   for (uint32_t i = 0; i < num_au; ++i) {
     bytes_per_au[i] = 0;
   }
@@ -1000,6 +1004,8 @@ void bluestore_blob_t::split(uint32_t blob_offset, bluestore_blob_t& rb)
 }
 
 // bluestore_shared_blob_t
+MEMPOOL_DEFINE_OBJECT_FACTORY(bluestore_shared_blob_t, bluestore_shared_blob_t,
+	          bluestore_cache_other);
 
 void bluestore_shared_blob_t::dump(Formatter *f) const
 {
