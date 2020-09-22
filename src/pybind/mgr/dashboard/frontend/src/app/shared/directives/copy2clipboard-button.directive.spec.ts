54a0638e7ec0a2d1e9f27f061b3e49f6d3c89df5
Volker Theile <vtheile@suse.com>
2020-09-22 14:09:52 +0200

    mgr/dashboard: Copy to clipboard does not work in Firefox
    Fixes: https://tracker.ceph.com/issues/47578
    
    Signed-off-by: Volker Theile <vtheile@suse.com>
    (cherry picked from commit 8b9d881a63aac8ed3d7d1a7fc80be5e3d3d0b3b1)
    
    Conflicts:
      src/pybind/mgr/dashboard/frontend/src/app/shared/directives/copy2clipboard-button.directive.spec.ts

--- src/pybind/mgr/dashboard/frontend/src/app/shared/directives/copy2clipboard-button.directive.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/directives/copy2clipboard-button.directive.spec.ts
@@ -1,18 +1,69 @@
 import { TestBed } from '@angular/core/testing';
 
 import { I18n } from '@ngx-translate/i18n-polyfill';
+import * as BrowserDetect from 'detect-browser';
+import { ToastrService } from 'ngx-toastr';
 
 import { configureTestBed, i18nProviders } from '../../../testing/unit-test-helper';
 import { Copy2ClipboardButtonDirective } from './copy2clipboard-button.directive';
 
 describe('Copy2clipboardButtonDirective', () => {
+  let directive: Copy2ClipboardButtonDirective;
+
   configureTestBed({
-    providers: [i18nProviders]
+    providers: [
+      i18nProviders,
+      {
+        provide: ToastrService,
+        useValue: {
+          error: () => true,
+          success: () => true
+        }
+      }
+    ]
   });
 
   it('should create an instance', () => {
     const i18n = TestBed.get(I18n);
-    const directive = new Copy2ClipboardButtonDirective(null, null, null, i18n);
+    directive = new Copy2ClipboardButtonDirective(null, null, null, i18n);
     expect(directive).toBeTruthy();
+  });
+
+  describe('test onClick behaviours', () => {
+    let toastrService: ToastrService;
+    let queryFn: jasmine.Spy;
+    let writeTextFn: jasmine.Spy;
+
+    beforeEach(() => {
+      const i18n = TestBed.get(I18n);
+      toastrService = TestBed.get(ToastrService);
+      directive = new Copy2ClipboardButtonDirective(null, null, toastrService, i18n);
+      spyOn<any>(directive, 'getText').and.returnValue('foo');
+      Object.assign(navigator, {
+        permissions: { query: jest.fn() },
+        clipboard: {
+          writeText: jest.fn()
+        }
+      });
+      queryFn = spyOn(navigator.permissions, 'query');
+    });
+
+    it('should not call permissions API', () => {
+      spyOn(BrowserDetect, 'detect').and.returnValue({ name: 'firefox' });
+      writeTextFn = spyOn(navigator.clipboard, 'writeText').and.returnValue(
+        new Promise<void>((resolve, _) => {
+          resolve();
+        })
+      );
+      directive.onClick();
+      expect(queryFn).not.toHaveBeenCalled();
+      expect(writeTextFn).toHaveBeenCalledWith('foo');
+    });
+
+    it('should call permissions API', () => {
+      spyOn(BrowserDetect, 'detect').and.returnValue({ name: 'chrome' });
+      directive.onClick();
+      expect(queryFn).toHaveBeenCalled();
+    });
   });
 });
