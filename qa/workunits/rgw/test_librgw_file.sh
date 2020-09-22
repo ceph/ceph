b035477d093c712b85731961a1f3ccb84c0b5e44
Casey Bodley <cbodley@redhat.com>
2020-09-22 10:38:22 -0400

    qa/rgw: librgw workunit creates a test user
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 12b5b9506c19133d10823ca6d2041eda92618765)

--- qa/workunits/rgw/test_librgw_file.sh
+++ qa/workunits/rgw/test_librgw_file.sh
@@ -1,5 +1,14 @@
 #!/bin/sh -e
 
+export AWS_ACCESS_KEY_ID=`openssl rand -base64 20`
+export AWS_SECRET_ACCESS_KEY=`openssl rand -base64 40`
+
+radosgw-admin user create --uid ceph-test-librgw-file \
+       --access-key $AWS_ACCESS_KEY_ID \
+       --secret $AWS_SECRET_ACCESS_KEY \
+       --display-name "librgw test user" \
+       --email librgw@example.com || echo "librgw user exists"
+
 ceph_test_librgw_file
 ceph_test_librgw_file_aw
 ceph_test_librgw_file_cd
