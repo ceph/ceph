sunnyku/ceph-ci

pybind / mirroring: interface to mirror CephFS directory snapshots
Signed-off-by: Venky Shankar <vshankar@redhat.com>

src/pybind/mgr/mirroring/fs/dir_map/load.py
@@ -1,0 +1,69 @@
+import errno
+import pickle
+import logging
+
+import rados
+
+from ..exception import MirrorException
+from ..utils import MIRROR_OBJECT_NAME, DIRECTORY_MAP_PREFIX, \
+    INSTANCE_ID_PREFIX
+
+log = logging.getLogger(__name__)
+
+MAX_RETURN = 256
+
+def handle_dir_load(dir_mapping, dir_map):
+    for directory_str, encoded_map in dir_map.items():
+        dir_name = directory_str[len(DIRECTORY_MAP_PREFIX):]
+        decoded_map = pickle.loads(encoded_map)
+        log.debug(f'{dir_name} -> {decoded_map}')
+        dir_mapping[dir_name] = decoded_map
+
+def load_dir_map(ioctx):
+    dir_mapping = {}
+    log.info('loading dir map...')
+    try:
+        with rados.ReadOpCtx() as read_op:
+            start = ""
+            while True:
+                iter, ret = ioctx.get_omap_vals(read_op, start, DIRECTORY_MAP_PREFIX, MAX_RETURN)
+                if not ret == 0:
+                    raise MirrorException(-errno.EINVAL, f'rados operation failed')
+                ioctx.operate_read_op(read_op, MIRROR_OBJECT_NAME)
+                dir_map = dict(iter)
+                if not dir_map:
+                    break
+                handle_dir_load(dir_mapping, dir_map)
+                start = dir_map.popitem()[0]
+        log.info("loaded {0} directory mapping(s) from disk".format(len(dir_mapping)))
+        return dir_mapping
+    except rados.Error as e:
+        log.error(f'loading directory map exception: {e}')
+
+def handle_instance_load(instance_mapping, instance_map):
+    for instance, e_data in instance_map.items():
+        instance_id = instance[len(INSTANCE_ID_PREFIX):]
+        d_data = pickle.loads(e_data)
+        log.debug(f'{instance_id} -> {d_data}')
+        instance_mapping[instance_id] = d_data
+
+def load_instances(ioctx):
+    instance_mapping = {}
+    log.info('loading instances...')
+    try:
+        with rados.ReadOpCtx() as read_op:
+            start = ""
+            while True:
+                iter, ret = ioctx.get_omap_vals(read_op, start, INSTANCE_ID_PREFIX, MAX_RETURN)
+                if not ret == 0:
+                    raise MirrorException(-errno.EINVAL, f'rados operation failed')
+                ioctx.operate_read_op(read_op, MIRROR_OBJECT_NAME)
+                instance_map = dict(iter)
+                if not instance_map:
+                    break
+                handle_instance_load(instance_mapping, instance_map)
+                start = instance_map.popitem()[0]
+        log.info("loaded {0} instance(s) from disk".format(len(instance_mapping)))
+        return instance_mapping
+    except rados.Error as e:
+        log.error(f'loading instances exception: {e}')

src/pybind/mgr/mirroring/fs/notify.py
@@ -1,0 +1,115 @@
+import errno
+import json
+import logging
+import threading
+import time
+
+import rados
+
+from .utils import MIRROR_OBJECT_PREFIX, AsyncOpTracker
+
+log = logging.getLogger(__name__)
+
+class Notifier():
+    def __init__(self, ioctx):
+        self.ioctx = ioctx
+
+    @staticmethod
+    def instance_object(instance_id):
+        return f'{MIRROR_OBJECT_PREFIX}.{instance_id}'
+
+    def notify_cbk(self, dir_name, callback):
+        def cbk(_, r, acks, timeouts):
+            log.debug(f'Notifier.notify_cbk: ret {r} acks: {acks} timeouts: {timeouts}')
+            callback(dir_name, r)
+        return cbk
+
+    def notify(self, dir_name, message, callback):
+        try:
+            instance_id = message[0]
+            message = message[1]
+            log.debug(f'Notifier.notify: {instance_id} {message} for {dir_name}')
+            self.ioctx.aio_notify(
+                Notifier.instance_object(
+                    instance_id), self.notify_cbk(dir_name, callback), msg=message)
+        except rados.Error as e:
+            log.error(f'Notifier exception: {e}')
+            raise e
+
+class InstanceWatcher():
+    INSTANCE_TIMEOUT = 30
+    NOTIFY_INTERVAL = 1
+
+    class Listener(object):
+        def handle_instances(self, added, removed):
+            raise NotImplementedError()
+
+    def __init__(self, ioctx, instances, listener):
+        self.ioctx = ioctx
+        self.listener = listener
+        self.instances = {}
+        for instance_id, data in instances.items():
+            self.instances[instance_id] = {'addr': data['addr'], 'seen': time.time()}
+        self.lock = threading.Lock()
+        self.stopping = threading.Event()
+        self.op_tracker = AsyncOpTracker()
+        self.notify_task = None
+        self.schedule_notify_task()
+
+    def schedule_notify_task(self):
+        assert self.notify_task == None
+        self.notify_task = threading.Timer(InstanceWatcher.NOTIFY_INTERVAL, self.notify)
+        self.notify_task.start()
+
+    def stop(self):
+        with self.lock:
+            log.debug('InstanceWatcher.stop')
+            self.stopping.set()
+            if self.notify_task:
+                self.notify_task.cancel()
+        self.op_tracker.wait_for_ops()
+        log.debug('InstanceWatcher.stop done')
+
+    def handle_notify(self, _, r, acks, timeouts):
+        log.debug(f'InstanceWatcher.handle_notify r={r} acks={acks} timeouts={timeouts}')
+        with self.lock:
+            try:
+                added = {}
+                removed = {}
+                for ack in acks:
+                    instance_id = str(ack[0])
+                    notifier_data = ack[2]
+                    log.debug(f'InstanceWatcher.handle_notify: {instance_id}: {notifier_data}')
+                    if not instance_id in self.instances:
+                        added[instance_id] = notifier_data
+                        self.instances[instance_id] = {'addr': notifier_data}
+                    self.instances[instance_id]['seen'] = time.time()
+                # gather non responders
+                now = time.time()
+                for instance_id in list(self.instances.keys()):
+                    data = self.instances[instance_id]
+                    if now - data['seen'] > InstanceWatcher.INSTANCE_TIMEOUT:
+                        removed[instance_id] = data['addr']
+                        self.instances.pop(instance_id)
+                if added or removed:
+                    self.listener.handle_instances(added, removed)
+            except Exception as e:
+                log.warn(f'InstanceWatcher.handle_notify exception: {e}')
+            finally:
+                self.op_tracker.finish_async_op()
+                if not self.stopping.is_set():
+                    self.schedule_notify_task()
+
+    def notify(self):
+        with self.lock:
+            self.notify_task = None
+            if self.stopping.is_set():
+                log.debug('InstanceWatcher: exiting')
+                return
+            try:
+                self.op_tracker.start_async_op()
+                log.debug('InstanceWatcher.notify')
+                self.ioctx.aio_notify(MIRROR_OBJECT_PREFIX, self.handle_notify)
+            except rados.Error as e:
+                log.warn(f'InstanceWatcher exception: {e}')
+                self.schedule_notify_task()

ceph/ceph

src/pybind/mgr/mirroring/fs/dir_map/load.py
@@ -1,0 +1,73 @@
+import errno
+import pickle
+import logging
+
+import rados
+
+from ..exception import MirrorException
+from ..utils import MIRROR_OBJECT_NAME, DIRECTORY_MAP_PREFIX, \
+    INSTANCE_ID_PREFIX
+
+log = logging.getLogger(__name__)
+
+MAX_RETURN = 256
+
+def handle_dir_load(dir_mapping, dir_map):
+    for directory_str, encoded_map in dir_map.items():
+        dir_path = directory_str[len(DIRECTORY_MAP_PREFIX):]
+        decoded_map = pickle.loads(encoded_map)
+        log.debug(f'{dir_path} -> {decoded_map}')
+        dir_mapping[dir_path] = decoded_map
+
+def load_dir_map(ioctx):
+    dir_mapping = {}
+    log.info('loading dir map...')
+    try:
+        with rados.ReadOpCtx() as read_op:
+            start = ""
+            while True:
+                iter, ret = ioctx.get_omap_vals(read_op, start, DIRECTORY_MAP_PREFIX, MAX_RETURN)
+                if not ret == 0:
+                    log.error(f'failed to fetch dir mapping omap')
+                    raise Exception(-errno.EINVAL)
+                ioctx.operate_read_op(read_op, MIRROR_OBJECT_NAME)
+                dir_map = dict(iter)
+                if not dir_map:
+                    break
+                handle_dir_load(dir_mapping, dir_map)
+                start = dir_map.popitem()[0]
+        log.info("loaded {0} directory mapping(s) from disk".format(len(dir_mapping)))
+        return dir_mapping
+    except rados.Error as e:
+        log.error(f'exception when loading directory mapping: {e}')
+        raise Exception(-e.errno)
+
+def handle_instance_load(instance_mapping, instance_map):
+    for instance, e_data in instance_map.items():
+        instance_id = instance[len(INSTANCE_ID_PREFIX):]
+        d_data = pickle.loads(e_data)
+        log.debug(f'{instance_id} -> {d_data}')
+        instance_mapping[instance_id] = d_data
+
+def load_instances(ioctx):
+    instance_mapping = {}
+    log.info('loading instances...')
+    try:
+        with rados.ReadOpCtx() as read_op:
+            start = ""
+            while True:
+                iter, ret = ioctx.get_omap_vals(read_op, start, INSTANCE_ID_PREFIX, MAX_RETURN)
+                if not ret == 0:
+                    log.error(f'failed to fetch instance omap')
+                    raise Exception(-errno.EINVAL)
+                ioctx.operate_read_op(read_op, MIRROR_OBJECT_NAME)
+                instance_map = dict(iter)
+                if not instance_map:
+                    break
+                handle_instance_load(instance_mapping, instance_map)
+                start = instance_map.popitem()[0]
+        log.info("loaded {0} instance(s) from disk".format(len(instance_mapping)))
+        return instance_mapping
+    except rados.Error as e:
+        log.error(f'exception when loading instances: {e}')
+        raise Exception(-e.errno)

src/pybind/mgr/mirroring/fs/notify.py
@@ -1,0 +1,117 @@
+import errno
+import json
+import logging
+import threading
+import time
+
+import rados
+
+from .utils import MIRROR_OBJECT_PREFIX, AsyncOpTracker
+
+log = logging.getLogger(__name__)
+
+class Notifier():
+    def __init__(self, ioctx):
+        self.ioctx = ioctx
+
+    @staticmethod
+    def instance_object(instance_id):
+        return f'{MIRROR_OBJECT_PREFIX}.{instance_id}'
+
+    def notify_cbk(self, dir_path, callback):
+        def cbk(_, r, acks, timeouts):
+            log.debug(f'Notifier.notify_cbk: ret {r} acks: {acks} timeouts: {timeouts}')
+            callback(dir_path, r)
+        return cbk
+
+    def notify(self, dir_path, message, callback):
+        try:
+            instance_id = message[0]
+            message = message[1]
+            log.debug(f'Notifier.notify: {instance_id} {message} for {dir_path}')
+            self.ioctx.aio_notify(
+                Notifier.instance_object(
+                    instance_id), self.notify_cbk(dir_path, callback), msg=message)
+        except rados.Error as e:
+            log.error(f'Notifier exception: {e}')
+            raise e
+
+class InstanceWatcher():
+    INSTANCE_TIMEOUT = 30
+    NOTIFY_INTERVAL = 1
+
+    class Listener(object):
+        def handle_instances(self, added, removed):
+            raise NotImplementedError()
+
+    def __init__(self, ioctx, instances, listener):
+        self.ioctx = ioctx
+        self.listener = listener
+        self.instances = {}
+        for instance_id, data in instances.items():
+            self.instances[instance_id] = {'addr': data['addr'], 'seen': time.time()}
+        self.lock = threading.Lock()
+        self.stopping = threading.Event()
+        self.op_tracker = AsyncOpTracker()
+        self.notify_task = None
+        self.schedule_notify_task()
+
+    def schedule_notify_task(self):
+        assert self.notify_task == None
+        self.notify_task = threading.Timer(InstanceWatcher.NOTIFY_INTERVAL, self.notify)
+        self.notify_task.start()
+
+    def stop(self):
+        with self.lock:
+            log.debug('InstanceWatcher.stop')
+            self.stopping.set()
+            if self.notify_task:
+                self.notify_task.cancel()
+        self.op_tracker.wait_for_ops()
+        log.debug('InstanceWatcher.stop done')
+
+    def handle_notify(self, _, r, acks, timeouts):
+        log.debug(f'InstanceWatcher.handle_notify r={r} acks={acks} timeouts={timeouts}')
+        with self.lock:
+            try:
+                added = {}
+                removed = {}
+                if acks is None:
+                    acks = []
+                for ack in acks:
+                    instance_id = str(ack[0])
+                    notifier_data = ack[2]
+                    log.debug(f'InstanceWatcher.handle_notify: {instance_id}: {notifier_data}')
+                    if not instance_id in self.instances:
+                        added[instance_id] = notifier_data
+                        self.instances[instance_id] = {'addr': notifier_data}
+                    self.instances[instance_id]['seen'] = time.time()
+                # gather non responders
+                now = time.time()
+                for instance_id in list(self.instances.keys()):
+                    data = self.instances[instance_id]
+                    if now - data['seen'] > InstanceWatcher.INSTANCE_TIMEOUT:
+                        removed[instance_id] = data['addr']
+                        self.instances.pop(instance_id)
+                if added or removed:
+                    self.listener.handle_instances(added, removed)
+            except Exception as e:
+                log.warn(f'InstanceWatcher.handle_notify exception: {e}')
+            finally:
+                self.op_tracker.finish_async_op()
+                if not self.stopping.is_set():
+                    self.schedule_notify_task()
+
+    def notify(self):
+        with self.lock:
+            self.notify_task = None
+            if self.stopping.is_set():
+                log.debug('InstanceWatcher: exiting')
+                return
+            try:
+                self.op_tracker.start_async_op()
+                log.debug('InstanceWatcher.notify')
+                self.ioctx.aio_notify(MIRROR_OBJECT_PREFIX, self.handle_notify)
+            except rados.Error as e:
+                log.warn(f'InstanceWatcher exception: {e}')
+                self.schedule_notify_task()