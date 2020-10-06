3ed4a9de771f36f27e8e6fbef39f1cfb88b119a1
Casey Bodley <cbodley@redhat.com>
2020-10-06 17:59:07 -0400

    rgw: remove unused RGWRunBucketsSyncBySourceCR
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_data_sync.cc
+++ src/rgw/rgw_data_sync.cc
@@ -1180,26 +1180,6 @@ struct rgw_sync_pipe_info_set {
   }
 };
 
-class RGWRunBucketsSyncBySourceCR : public RGWCoroutine {
-  RGWDataSyncCtx *sc;
-  RGWDataSyncEnv *sync_env;
-
-  rgw_bucket_shard source;
-
-  RGWSyncTraceNodeRef tn;
-
-public:
-  RGWRunBucketsSyncBySourceCR(RGWDataSyncCtx *_sc, const rgw_bucket_shard& _source, const RGWSyncTraceNodeRef& _tn_parent)
-    : RGWCoroutine(_sc->cct), sc(_sc), sync_env(_sc->env), source(_source),
-      tn(sync_env->sync_tracer->add_node(_tn_parent, "source",
-                                         SSTR(bucket_shard_str{_source} ))) {
-  }
-  ~RGWRunBucketsSyncBySourceCR() override {
-  }
-
-  int operate() override;
-};
-
 class RGWRunBucketSourcesSyncCR : public RGWCoroutine {
   RGWDataSyncCtx *sc;
   RGWDataSyncEnv *sync_env;
@@ -4667,15 +4647,6 @@ int RGWGetBucketPeersCR::operate()
       pipes->update_empty_bucket_info(buckets_info);
     }
 
-    return set_cr_done();
-  }
-
-  return 0;
-}
-
-int RGWRunBucketsSyncBySourceCR::operate()
-{
-  reenter(this) {
     return set_cr_done();
   }
 
