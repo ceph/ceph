bebabfe8e16ef94c5655403a8a8c7dd61253256f
Kotresh HR <khiremat@redhat.com>
2020-10-07 17:03:48 +0530

    mds: throttle cap acquisition via readdir
    A trivial "find" command on a large directory hierarchy will cause the
    client to receive caps significantly faster than it will release. The
    MDS will try to have the client reduce its caps below the
    mds_max_caps_per_client limit but the recall throttles prevent it from
    catching up to the pace of acquisition. The solution is to throttle
    readdir from client. This patch does the same.
    
    The readdir is throttled on the condition that the number of caps
    acquired is greater than certain percentage of mds_max_caps_per_client
    (default is 10%) and cap acquisition via readdir is certain percentage
    of mds_max_caps_per_client (the default is 50%). When the above
    condition is met, the readdir request is retried after
    'mds_cap_acquisition_throttle_retry_request_timeout' (default is 0.5)
    seconds.
    
    Fixes: https://tracker.ceph.com/issues/47307
    Signed-off-by: Kotresh HR <khiremat@redhat.com>
    (cherry picked from commit c0de657d3f99f8a3a0d89576dff2f8e98f5f8974)
    
    Conflicts:
      src/mds/MDSRank.cc: mds_heartbeat_grace config is not tracked by MDSRankDispatcher in octopus
      src/mds/Server.h: Per session client metrics is not available in octopus

--- src/mds/Server.h
+++ src/mds/Server.h
@@ -77,6 +77,7 @@ enum {
   l_mdss_req_symlink_latency,
   l_mdss_req_unlink_latency,
   l_mdss_cap_revoke_eviction,
+  l_mdss_cap_acquisition_throttle,
   l_mdss_last,
 };
 
@@ -348,6 +349,12 @@ private:
 
   DecayCounter recall_throttle;
   time last_recall_state;
+
+  // Cache cap acquisition throttle configs
+  uint64_t max_caps_per_client;
+  uint64_t cap_acquisition_throttle;
+  double max_caps_throttle_ratio;
+  double caps_throttle_retry_request_timeout;
 };
 
 static inline constexpr auto operator|(Server::RecallFlags a, Server::RecallFlags b) {
