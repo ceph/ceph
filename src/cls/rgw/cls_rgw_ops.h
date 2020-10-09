a7ef4305df3ad76f1e95117f55bd1ed4b765972b
Radoslaw Zarzynski <rzarzyns@redhat.com>
2020-10-09 17:21:30 +0200

    rgw: CLSRGWLinkOLHBase inherits from cls_rgw_bi_log_related_op privately.
    The motivation here is to ensure that users of the class
    always pick up the proper variant of `add_maybe_flush()`
    of the `bilog_handler`.
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/cls/rgw/cls_rgw_ops.h
+++ src/cls/rgw/cls_rgw_ops.h
@@ -1511,10 +1511,19 @@ struct CLSRGWCompleteModifyOp : CLSRGWCompleteModifyOpBase {
 };
 
 // rgw_cls_link_olh_op
-struct CLSRGWLinkOLHBase : cls_rgw_bi_log_related_op {
+struct CLSRGWLinkOLHBase : private cls_rgw_bi_log_related_op {
+  template <class... Args>
+  CLSRGWLinkOLHBase(Args&&... args)
+    : cls_rgw_bi_log_related_op{ std::forward<Args>(args)... } {
+  }
+
   static RGWModifyOp get_bilog_op_type(const bool delete_marker) {
      return delete_marker ? CLS_RGW_OP_LINK_OLH_DM
                           : CLS_RGW_OP_LINK_OLH;
+  }
+
+  std::string& get_op_tag_ref() {
+    return op_tag;
   }
 
   void link_olh(librados::ObjectWriteOperation& op,
@@ -1527,6 +1536,8 @@ struct CLSRGWLinkOLHBase : cls_rgw_bi_log_related_op {
 
 template <bool DeleteMarkerV>
 struct CLSRGWLinkOLH : CLSRGWLinkOLHBase {
+  using CLSRGWLinkOLHBase::CLSRGWLinkOLHBase;
+
   constexpr static enum RGWModifyOp get_bilog_op_type() {
     return CLSRGWLinkOLHBase::get_bilog_op_type(DeleteMarkerV);
   }
--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -7460,7 +7460,7 @@ int RGWRados::set_olh(RGWObjectCtx& obj_ctx, const RGWBucketInfo& bucket_info, c
           return ret;
         }
  
-        ret = olh_init_modification(bucket_info, *state, olh_obj, &op_issuer.op_tag);
+        ret = olh_init_modification(bucket_info, *state, olh_obj, &op_issuer.get_op_tag_ref());
         if (ret < 0) {
           ldout(cct, 20) << "olh_init_modification() target_obj=" << target_obj << " delete_marker=" << (int)DeleteMarkerV << " returned " << ret << dendl;
           if (ret == -ECANCELED) {
