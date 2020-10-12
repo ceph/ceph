sunnyku/ceph-ci

rgw/lua: run lua scripts in s3 requests context
for more details on design and
remaining work see:
https://gist.github.com/yuvalif/60d5984c28af89ba17443ce947540c1f

Signed-off-by: Yuval Lifshitz <ylifshit@redhat.com>

src/rgw/rgw_lua.cc
@@ -1,0 +1,139 @@
+#include <lua.hpp>
+#include "services/svc_zone.h"
+#include "services/svc_sys_obj.h"
+#include "common/dout.h"
+#include "rgw_lua_utils.h"
+#include "rgw_sal_rados.h"
+#include "rgw_lua.h"
+
+#define dout_subsys ceph_subsys_rgw
+
+namespace rgw::lua {
+
+context to_context(const std::string& s) 
+{
+  if (strcasecmp(s.c_str(), "pres3") == 0) {
+    return context::preS3;
+  }
+  if (strcasecmp(s.c_str(), "posts3") == 0) {
+    return context::postS3;
+  }
+  return context::none;
+}
+
+std::string to_string(context ctx) 
+{
+  switch (ctx) {
+    case context::preS3:
+      return "pres3";
+    case context::postS3:
+      return "posts3";
+    case context::none:
+      break;
+  }
+  return "none";
+}
+
+bool verify(const std::string& script, std::string& err_msg) 
+{
+  lua_State *L = luaL_newstate();
+  lua_state_guard guard(L);
+  luaL_openlibs(L);
+  try {
+    if (luaL_loadstring(L, script.c_str()) != LUA_OK) {
+      err_msg.assign(lua_tostring(L, -1));
+      return false;
+    }
+  } catch (const std::runtime_error& e) {
+    err_msg = e.what();
+    return false;
+  }
+  err_msg = "";
+  return true;
+}
+
+static const std::string SCRIPT_OID_PREFIX("script.");
+
+int read_script(rgw::sal::RGWRadosStore* store, const std::string& tenant, optional_yield y, context ctx, std::string& script)
+{
+  RGWSysObjectCtx obj_ctx(store->svc()->sysobj->init_obj_ctx());
+  RGWObjVersionTracker objv_tracker;
+
+  const auto script_oid = SCRIPT_OID_PREFIX + to_string(ctx) + tenant;
+  rgw_raw_obj obj(store->svc()->zone->get_zone_params().log_pool, script_oid);
+
+  bufferlist bl;
+  
+  const auto rc = rgw_get_system_obj(
+      obj_ctx,
+      obj.pool, 
+      obj.oid,
+      bl,
+      &objv_tracker,
+      nullptr, 
+      y, 
+      nullptr, 
+      nullptr);
+
+  if (rc < 0) {
+    return rc;
+  }
+
+  auto iter = bl.cbegin();
+  try {
+    ceph::decode(script, iter);
+  } catch (buffer::error& err) {
+    return -EIO;
+  }
+
+  return 0;
+}
+
+int write_script(rgw::sal::RGWRadosStore* store, const std::string& tenant, optional_yield y, context ctx, const std::string& script)
+{
+  RGWSysObjectCtx obj_ctx(store->svc()->sysobj->init_obj_ctx());
+  RGWObjVersionTracker objv_tracker;
+
+  const auto script_oid = SCRIPT_OID_PREFIX + to_string(ctx) + tenant;
+  rgw_raw_obj obj(store->svc()->zone->get_zone_params().log_pool, script_oid);
+
+  bufferlist bl;
+  ceph::encode(script, bl);
+
+  const auto rc = rgw_put_system_obj(
+      obj_ctx, 
+      obj.pool, 
+      obj.oid,
+      bl,
+      false, 
+      &objv_tracker,
+      real_time());
+
+  if (rc < 0) {
+    return rc;
+  }
+
+  return 0;
+}
+
+int delete_script(rgw::sal::RGWRadosStore* store, const std::string& tenant, optional_yield y, context ctx)
+{
+  RGWObjVersionTracker objv_tracker;
+
+  const auto script_oid = SCRIPT_OID_PREFIX + to_string(ctx) + tenant;
+  rgw_raw_obj obj(store->svc()->zone->get_zone_params().log_pool, script_oid);
+
+  const auto rc = rgw_delete_system_obj(
+      store->svc()->sysobj, 
+      obj.pool, 
+      obj.oid,
+      &objv_tracker);
+
+  if (rc < 0 && rc != -ENOENT) {
+    return rc;
+  }
+
+  return 0;
+}
+}
+

src/rgw/rgw_lua_utils.h
@@ -1,0 +1,168 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <ctime>
+#include <lua.hpp>
+
+class CephContext;
+
+namespace rgw::lua {
+
+// push ceph time in string format: "%Y-%m-%d %H:%M:%S"
+template <typename CephTime>
+void lua_pushtime(lua_State* L, const CephTime& tp) 
+{
+  const auto tt = CephTime::clock::to_time_t(tp);
+  const auto tm = *std::localtime(&tt);
+  char buff[64];
+  std::strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", &tm);
+  lua_pushstring(L, buff);
+}
+
+// push std::string
+void lua_pushstring(lua_State* L, const std::string& str);
+
+// dump the lua stack to stdout
+void stack_dump(lua_State* L);
+
+class lua_state_guard {
+  lua_State* l;
+public:
+  lua_state_guard(lua_State* _l) : l(_l) {}
+  ~lua_state_guard() {lua_close(l);}
+  void reset(lua_State* _l=nullptr) {l = _l;}
+};
+
+constexpr auto ONE_UPVAL    = 1;
+constexpr auto TWO_UPVALS   = 2;
+constexpr auto THREE_UPVALS = 3;
+constexpr auto FOUR_UPVALS  = 4;
+
+constexpr auto ONE_RETURNVAL    = 1;
+constexpr auto TWO_RETURNVALS   = 2;
+constexpr auto THREE_RETURNVALS = 3;
+constexpr auto FOUR_RETURNVALS  = 4;
+// utility functions to create a metatable
+// and tie it to an unnamed table
+//
+// add an __index method to it, to allow reading values
+// if "readonly" parameter is set to "false", it will also add
+// a __newindex method to it, to allow writing values
+// if the "toplevel" parameter is set to "true", it will name the
+// table as well as the metatable, this would allow direct access from
+// the lua script.
+//
+// The MetaTable is expected to be a class with the following members:
+// Name (static function returning the unique name of the metatable)
+// TableName (static function returning the unique name of the table - needed only for "toplevel" tables)
+// Type (typename) - the type of the "upvalue" (the type that the meta table represent)
+// IndexClosure (static function return "int" and accept "lua_State*") 
+// NewIndexClosure (static function return "int" and accept "lua_State*") 
+// e.g.
+// struct MyStructMetaTable {
+//   static std::string TableName() {
+//     return "MyStruct";
+//   }
+//
+//   using Type = MyStruct;
+//
+//   static int IndexClosure(lua_State* L) {
+//     const auto value = reinterpret_cast<const Type*>(lua_touserdata(L, lua_upvalueindex(1)));
+//     ...
+//   }
+
+//   static int NewIndexClosure(lua_State* L) {
+//     auto value = reinterpret_cast<Type*>(lua_touserdata(L, lua_upvalueindex(1)));
+//     ...
+//   }
+// };
+//
+
+template<typename MetaTable>
+void create_metatable(lua_State* L, void* upvalue, bool toplevel=false)
+{
+  if (upvalue) {
+    // create table
+    lua_newtable(L);
+    if (toplevel) {
+      // duplicate the table to make sure it remain in the stack
+      lua_pushvalue(L, -1);
+      // give table a name (in cae of "toplevel")
+      lua_setglobal(L, MetaTable::TableName().c_str());
+    }
+    // create metatable
+    [[maybe_unused]] const auto rc = luaL_newmetatable(L, MetaTable::Name().c_str());
+    lua_pushstring(L, "__index");
+    lua_pushlightuserdata(L, upvalue);
+    lua_pushcclosure(L, MetaTable::IndexClosure, ONE_UPVAL);
+    lua_rawset(L, -3);
+    lua_pushstring(L, "__newindex");
+    lua_pushlightuserdata(L, upvalue);
+    lua_pushcclosure(L, MetaTable::NewIndexClosure, ONE_UPVAL);
+    lua_rawset(L, -3);
+    lua_pushstring(L, "__pairs");
+    lua_pushlightuserdata(L, upvalue);
+    lua_pushcclosure(L, MetaTable::PairsClosure, ONE_UPVAL);
+    lua_rawset(L, -3);
+    lua_pushstring(L, "__len");
+    lua_pushlightuserdata(L, upvalue);
+    lua_pushcclosure(L, MetaTable::LenClosure, ONE_UPVAL);
+    lua_rawset(L, -3);
+    // tie metatable and table
+    lua_setmetatable(L, -2);
+  } else {
+    lua_pushnil(L);
+  }
+}
+
+template<typename MetaTable>
+void create_metatable(lua_State* L, std::unique_ptr<typename MetaTable::Type>& ptr, bool toplevel=false)
+{
+  if (ptr) {
+    create_metatable<MetaTable>(L, reinterpret_cast<void*>(ptr.get()), toplevel);
+  } else {
+    lua_pushnil(L);
+  }
+}
+
+// following struct may be used as a base class for other MetaTable classes
+// note, however, this is not mandatory to use it as a base
+struct EmptyMetaTable {
+  // by default everythinmg is "readonly"
+  // to change, overload this function in the derived
+  static int NewIndexClosure(lua_State* L) {
+    throw std::runtime_error("trying to write to readonly field");
+    return 1;
+  }
+  
+  // by default nothing is iterable
+  // to change, overload this function in the derived
+  static int PairsClosure(lua_State* L) {
+    throw std::runtime_error("trying to iterate over non-iterable field");
+    return 1;
+  }
+  
+  // by default nothing is iterable
+  // to change, overload this function in the derived
+  static int LenClosure(lua_State* L) {
+    throw std::runtime_error("trying to get length of non-iterable field");
+    return 1;
+  }
+
+  static void throw_unknown_field(const std::string& index, const std::string& table) {
+    throw std::runtime_error("unknown field name: " + index + " provided to: " + table);
+  }
+};
+
+// create a debug log action
+// it expects CephContext to be captured
+// it expects one string parameter, which is the message to log
+// could be executed from any context that has CephContext
+// e.g.
+//    RGWDebugLog("hello world from lua")
+//
+void create_debug_action(lua_State* L, CephContext* cct);
+
+}
+

src/test/cli/radosgw-admin/help.t
@@ -172,6 +172,9 @@
     subscription rm            remove a pubsub subscription
     subscription pull          show events in a pubsub subscription
     subscription ack           ack (remove) an events in a pubsub subscription
+    script put                 upload a lua script to a context
+    script get                 get the lua script of a context
+    script rm                  remove the lua scripts of a context
   options:
      --tenant=<tenant>         tenant name
      --uid=<id>                user id
@@ -326,6 +329,9 @@
      --topic                   bucket notifications/pubsub topic name
      --subscription            pubsub subscription name
      --event-id                event id in a pubsub subscription
+  
+  Script options:
+     --context                 context in which the script runs. one of: PreS3Request, PostS3Request
   
     --conf/-c FILE    read configuration from the given configuration file
     --id ID           set ID portion of my name

ceph/ceph

src/rgw/rgw_lua.cc
@@ -1,0 +1,139 @@
+#include <lua.hpp>
+#include "services/svc_zone.h"
+#include "services/svc_sys_obj.h"
+#include "common/dout.h"
+#include "rgw_lua_utils.h"
+#include "rgw_sal_rados.h"
+#include "rgw_lua.h"
+
+#define dout_subsys ceph_subsys_rgw
+
+namespace rgw::lua {
+
+context to_context(const std::string& s) 
+{
+  if (strcasecmp(s.c_str(), "prerequest") == 0) {
+    return context::preRequest;
+  }
+  if (strcasecmp(s.c_str(), "postrequest") == 0) {
+    return context::postRequest;
+  }
+  return context::none;
+}
+
+std::string to_string(context ctx) 
+{
+  switch (ctx) {
+    case context::preRequest:
+      return "prerequest";
+    case context::postRequest:
+      return "postrequest";
+    case context::none:
+      break;
+  }
+  return "none";
+}
+
+bool verify(const std::string& script, std::string& err_msg) 
+{
+  lua_State *L = luaL_newstate();
+  lua_state_guard guard(L);
+  luaL_openlibs(L);
+  try {
+    if (luaL_loadstring(L, script.c_str()) != LUA_OK) {
+      err_msg.assign(lua_tostring(L, -1));
+      return false;
+    }
+  } catch (const std::runtime_error& e) {
+    err_msg = e.what();
+    return false;
+  }
+  err_msg = "";
+  return true;
+}
+
+static const std::string SCRIPT_OID_PREFIX("script.");
+
+int read_script(rgw::sal::RGWRadosStore* store, const std::string& tenant, optional_yield y, context ctx, std::string& script)
+{
+  RGWSysObjectCtx obj_ctx(store->svc()->sysobj->init_obj_ctx());
+  RGWObjVersionTracker objv_tracker;
+
+  const auto script_oid = SCRIPT_OID_PREFIX + to_string(ctx) + tenant;
+  rgw_raw_obj obj(store->svc()->zone->get_zone_params().log_pool, script_oid);
+
+  bufferlist bl;
+  
+  const auto rc = rgw_get_system_obj(
+      obj_ctx,
+      obj.pool, 
+      obj.oid,
+      bl,
+      &objv_tracker,
+      nullptr, 
+      y, 
+      nullptr, 
+      nullptr);
+
+  if (rc < 0) {
+    return rc;
+  }
+
+  auto iter = bl.cbegin();
+  try {
+    ceph::decode(script, iter);
+  } catch (buffer::error& err) {
+    return -EIO;
+  }
+
+  return 0;
+}
+
+int write_script(rgw::sal::RGWRadosStore* store, const std::string& tenant, optional_yield y, context ctx, const std::string& script)
+{
+  RGWSysObjectCtx obj_ctx(store->svc()->sysobj->init_obj_ctx());
+  RGWObjVersionTracker objv_tracker;
+
+  const auto script_oid = SCRIPT_OID_PREFIX + to_string(ctx) + tenant;
+  rgw_raw_obj obj(store->svc()->zone->get_zone_params().log_pool, script_oid);
+
+  bufferlist bl;
+  ceph::encode(script, bl);
+
+  const auto rc = rgw_put_system_obj(
+      obj_ctx, 
+      obj.pool, 
+      obj.oid,
+      bl,
+      false, 
+      &objv_tracker,
+      real_time());
+
+  if (rc < 0) {
+    return rc;
+  }
+
+  return 0;
+}
+
+int delete_script(rgw::sal::RGWRadosStore* store, const std::string& tenant, optional_yield y, context ctx)
+{
+  RGWObjVersionTracker objv_tracker;
+
+  const auto script_oid = SCRIPT_OID_PREFIX + to_string(ctx) + tenant;
+  rgw_raw_obj obj(store->svc()->zone->get_zone_params().log_pool, script_oid);
+
+  const auto rc = rgw_delete_system_obj(
+      store->svc()->sysobj, 
+      obj.pool, 
+      obj.oid,
+      &objv_tracker);
+
+  if (rc < 0 && rc != -ENOENT) {
+    return rc;
+  }
+
+  return 0;
+}
+}
+

src/rgw/rgw_lua_utils.h
@@ -1,0 +1,175 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <ctime>
+#include <lua.hpp>
+
+class CephContext;
+
+namespace rgw::lua {
+
+// push ceph time in string format: "%Y-%m-%d %H:%M:%S"
+template <typename CephTime>
+void lua_pushtime(lua_State* L, const CephTime& tp) 
+{
+  const auto tt = CephTime::clock::to_time_t(tp);
+  const auto tm = *std::localtime(&tt);
+  char buff[64];
+  std::strftime(buff, sizeof(buff), "%Y-%m-%d %H:%M:%S", &tm);
+  lua_pushstring(L, buff);
+}
+
+// push std::string
+void lua_pushstring(lua_State* L, const std::string& str);
+
+// dump the lua stack to stdout
+void stack_dump(lua_State* L);
+
+class lua_state_guard {
+  lua_State* l;
+public:
+  lua_state_guard(lua_State* _l) : l(_l) {}
+  ~lua_state_guard() {lua_close(l);}
+  void reset(lua_State* _l=nullptr) {l = _l;}
+};
+
+constexpr auto ONE_UPVAL    = 1;
+constexpr auto TWO_UPVALS   = 2;
+constexpr auto THREE_UPVALS = 3;
+constexpr auto FOUR_UPVALS  = 4;
+constexpr auto FIVE_UPVALS  = 5;
+
+constexpr auto ONE_RETURNVAL    = 1;
+constexpr auto TWO_RETURNVALS   = 2;
+constexpr auto THREE_RETURNVALS = 3;
+constexpr auto FOUR_RETURNVALS  = 4;
+// utility functions to create a metatable
+// and tie it to an unnamed table
+//
+// add an __index method to it, to allow reading values
+// if "readonly" parameter is set to "false", it will also add
+// a __newindex method to it, to allow writing values
+// if the "toplevel" parameter is set to "true", it will name the
+// table as well as the metatable, this would allow direct access from
+// the lua script.
+//
+// The MetaTable is expected to be a class with the following members:
+// Name (static function returning the unique name of the metatable)
+// TableName (static function returning the unique name of the table - needed only for "toplevel" tables)
+// Type (typename) - the type of the "upvalue" (the type that the meta table represent)
+// IndexClosure (static function return "int" and accept "lua_State*") 
+// NewIndexClosure (static function return "int" and accept "lua_State*") 
+// e.g.
+// struct MyStructMetaTable {
+//   static std::string TableName() {
+//     return "MyStruct";
+//   }
+//
+//   using Type = MyStruct;
+//
+//   static int IndexClosure(lua_State* L) {
+//     const auto value = reinterpret_cast<const Type*>(lua_touserdata(L, lua_upvalueindex(1)));
+//     ...
+//   }
+
+//   static int NewIndexClosure(lua_State* L) {
+//     auto value = reinterpret_cast<Type*>(lua_touserdata(L, lua_upvalueindex(1)));
+//     ...
+//   }
+// };
+//
+
+template<typename MetaTable, typename... Upvalues>
+void create_metatable(lua_State* L, bool toplevel, Upvalues... upvalues)
+{
+  constexpr auto upvals_size = sizeof...(upvalues);
+  const std::array<void*, upvals_size> upvalue_arr = {upvalues...};
+  // create table
+  lua_newtable(L);
+  if (toplevel) {
+    // duplicate the table to make sure it remain in the stack
+    lua_pushvalue(L, -1);
+    // give table a name (in cae of "toplevel")
+    lua_setglobal(L, MetaTable::TableName().c_str());
+  }
+  // create metatable
+  [[maybe_unused]] const auto rc = luaL_newmetatable(L, MetaTable::Name().c_str());
+  lua_pushstring(L, "__index");
+  for (const auto upvalue : upvalue_arr) {
+    lua_pushlightuserdata(L, upvalue);
+  }
+  lua_pushcclosure(L, MetaTable::IndexClosure, upvals_size);
+  lua_rawset(L, -3);
+  lua_pushstring(L, "__newindex");
+  for (const auto upvalue : upvalue_arr) {
+    lua_pushlightuserdata(L, upvalue);
+  }
+  lua_pushcclosure(L, MetaTable::NewIndexClosure, upvals_size);
+  lua_rawset(L, -3);
+  lua_pushstring(L, "__pairs");
+  for (const auto upvalue : upvalue_arr) {
+    lua_pushlightuserdata(L, upvalue);
+  }
+  lua_pushcclosure(L, MetaTable::PairsClosure, upvals_size);
+  lua_rawset(L, -3);
+  lua_pushstring(L, "__len");
+  for (const auto upvalue : upvalue_arr) {
+    lua_pushlightuserdata(L, upvalue);
+  }
+  lua_pushcclosure(L, MetaTable::LenClosure, upvals_size);
+  lua_rawset(L, -3);
+  // tie metatable and table
+  lua_setmetatable(L, -2);
+}
+
+template<typename MetaTable>
+void create_metatable(lua_State* L, bool toplevel, std::unique_ptr<typename MetaTable::Type>& ptr)
+{
+  if (ptr) {
+    create_metatable<MetaTable>(L, toplevel, reinterpret_cast<void*>(ptr.get()));
+  } else {
+    lua_pushnil(L);
+  }
+}
+
+// following struct may be used as a base class for other MetaTable classes
+// note, however, this is not mandatory to use it as a base
+struct EmptyMetaTable {
+  // by default everythinmg is "readonly"
+  // to change, overload this function in the derived
+  static int NewIndexClosure(lua_State* L) {
+    throw std::runtime_error("trying to write to readonly field");
+    return 1;
+  }
+  
+  // by default nothing is iterable
+  // to change, overload this function in the derived
+  static int PairsClosure(lua_State* L) {
+    throw std::runtime_error("trying to iterate over non-iterable field");
+    return 1;
+  }
+  
+  // by default nothing is iterable
+  // to change, overload this function in the derived
+  static int LenClosure(lua_State* L) {
+    throw std::runtime_error("trying to get length of non-iterable field");
+    return 1;
+  }
+
+  static void throw_unknown_field(const std::string& index, const std::string& table) {
+    throw std::runtime_error("unknown field name: " + index + " provided to: " + table);
+  }
+};
+
+// create a debug log action
+// it expects CephContext to be captured
+// it expects one string parameter, which is the message to log
+// could be executed from any context that has CephContext
+// e.g.
+//    RGWDebugLog("hello world from lua")
+//
+void create_debug_action(lua_State* L, CephContext* cct);
+
+}
+

src/test/cli/radosgw-admin/help.t
@@ -173,6 +173,9 @@
     subscription rm            remove a pubsub subscription
     subscription pull          show events in a pubsub subscription
     subscription ack           ack (remove) an events in a pubsub subscription
+    script put                 upload a lua script to a context
+    script get                 get the lua script of a context
+    script rm                  remove the lua scripts of a context
   options:
      --tenant=<tenant>         tenant name
      --user_ns=<namespace>     namespace of user (oidc in case of users authenticated with oidc provider)
@@ -328,6 +331,9 @@
      --topic                   bucket notifications/pubsub topic name
      --subscription            pubsub subscription name
      --event-id                event id in a pubsub subscription
+  
+  Script options:
+     --context                 context in which the script runs. one of: preRequest, postRequest
   
     --conf/-c FILE    read configuration from the given configuration file
     --id ID           set ID portion of my name