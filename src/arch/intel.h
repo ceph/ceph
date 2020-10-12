5a9c108c3a496d6e5b04e6d1d2476daa42e60375
Changcheng Liu <changcheng.liu@aliyun.com>
2020-10-12 17:03:25 +0800

    arch/intel: refine IA SIMD feature detection
    1. cpuid.h has the corresponding MACRO to check SIMD feature.
       https://github.com/gcc-mirror/gcc/blob/master/gcc/config/i386/cpuid.h
    2. use __get_cpuid_max to detect usable instruction.
    3. use __cpuid_count to get ebx/ecx/edx
    4. replace '\t' indent with space
    
    Signed-off-by: Changcheng Liu <changcheng.liu@aliyun.com>

--- src/arch/intel.h
+++ src/arch/intel.h
@@ -5,13 +5,13 @@
 extern "C" {
 #endif
 
-extern int ceph_arch_intel_pclmul; /* true if we have PCLMUL features */
-extern int ceph_arch_intel_sse42;  /* true if we have sse 4.2 features */
-extern int ceph_arch_intel_sse41;  /* true if we have sse 4.1 features */
-extern int ceph_arch_intel_ssse3;  /* true if we have ssse 3 features */
-extern int ceph_arch_intel_sse3;   /* true if we have sse 3 features */
-extern int ceph_arch_intel_sse2;   /* true if we have sse 2 features */
-extern int ceph_arch_intel_aesni;  /* true if we have aesni features */
+extern int ceph_arch_intel_pclmul; /* true: have PCLMUL features */
+extern int ceph_arch_intel_sse42;  /* true: have sse 4.2 features */
+extern int ceph_arch_intel_sse41;  /* true: have sse 4.1 features */
+extern int ceph_arch_intel_ssse3;  /* true: have ssse 3 features */
+extern int ceph_arch_intel_sse3;   /* true: have sse 3 features */
+extern int ceph_arch_intel_sse2;   /* true: have sse 2 features */
+extern int ceph_arch_intel_aesni;  /* true: have aesni features */
 
 extern int ceph_arch_intel_probe(void);
 
