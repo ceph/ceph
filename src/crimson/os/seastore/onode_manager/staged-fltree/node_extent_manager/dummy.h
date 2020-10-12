15037569f8c53af7e785195167f954d9f68d433f
Yingxin Cheng <yingxin.cheng@intel.com>
2020-10-12 13:15:56 +0800

    crimson/onode-staged-tree: proper logging and dump
    Signed-off-by: Yingxin Cheng <yingxin.cheng@intel.com>

--- src/crimson/os/seastore/onode_manager/staged-fltree/node_extent_manager/dummy.h
+++ src/crimson/os/seastore/onode_manager/staged-fltree/node_extent_manager/dummy.h
@@ -8,6 +8,7 @@
 
 #include "include/buffer_raw.h"
 
+#include "crimson/common/log.h"
 #include "crimson/os/seastore/onode_manager/staged-fltree/node_extent_manager.h"
 
 namespace crimson::os::seastore::onode {
@@ -19,7 +20,14 @@ class DummySuper final: public Super {
   ~DummySuper() override = default;
  protected:
   laddr_t get_root_laddr() const override { return *p_root_laddr; }
-  void write_root_laddr(context_t, laddr_t addr) override { *p_root_laddr = addr; }
+  void write_root_laddr(context_t, laddr_t addr) override {
+    logger().info("OTree::Dummy: update root {:#x} ...", addr);
+    *p_root_laddr = addr;
+  }
+ private:
+  static seastar::logger& logger() {
+    return crimson::get_logger(ceph_subsys_filestore);
+  }
   laddr_t* p_root_laddr;
 };
 
@@ -52,6 +60,7 @@ class DummyNodeExtentManager final: public NodeExtentManager {
 
   tm_future<NodeExtentRef> read_extent(
       Transaction& t, laddr_t addr, extent_len_t len) override {
+    logger().trace("OTree::Dummy: reading {}B at {:#x} ...", len, addr);
     if constexpr (SYNC) {
       return read_extent_sync(t, addr, len);
     } else {
@@ -64,6 +73,7 @@ class DummyNodeExtentManager final: public NodeExtentManager {
 
   tm_future<NodeExtentRef> alloc_extent(
       Transaction& t, extent_len_t len) override {
+    logger().trace("OTree::Dummy: allocating {}B ...", len);
     if constexpr (SYNC) {
       return alloc_extent_sync(t, len);
     } else {
@@ -76,6 +86,7 @@ class DummyNodeExtentManager final: public NodeExtentManager {
 
   tm_future<Super::URef> get_super(
       Transaction& t, RootNodeTracker& tracker) override {
+    logger().trace("OTree::Dummy: get root ...");
     if constexpr (SYNC) {
       return get_super_sync(t, tracker);
     } else {
@@ -91,8 +102,12 @@ class DummyNodeExtentManager final: public NodeExtentManager {
       Transaction& t, laddr_t addr, extent_len_t len) {
     auto iter = allocate_map.find(addr);
     assert(iter != allocate_map.end());
-    assert(iter->second->get_length() == len);
-    return tm_ertr::make_ready_future<NodeExtentRef>(iter->second);
+    auto extent = iter->second;
+    logger().trace("OTree::Dummy: read {}B at {:#x}",
+                   extent->get_length(), extent->get_laddr());
+    assert(extent->get_laddr() == addr);
+    assert(extent->get_length() == len);
+    return tm_ertr::make_ready_future<NodeExtentRef>(extent);
   }
 
   tm_future<NodeExtentRef> alloc_extent_sync(
@@ -105,13 +120,21 @@ class DummyNodeExtentManager final: public NodeExtentManager {
     extent->set_laddr(addr);
     assert(allocate_map.find(extent->get_laddr()) == allocate_map.end());
     allocate_map.insert({extent->get_laddr(), extent});
+    logger().debug("OTree::Dummy: allocated {}B at {:#x}",
+                   extent->get_length(), extent->get_laddr());
+    assert(extent->get_length() == len);
     return tm_ertr::make_ready_future<NodeExtentRef>(extent);
   }
 
   tm_future<Super::URef> get_super_sync(
       Transaction& t, RootNodeTracker& tracker) {
+    logger().debug("OTree::Dummy: got root {:#x}", root_laddr);
     return tm_ertr::make_ready_future<Super::URef>(
         Super::URef(new DummySuper(t, tracker, &root_laddr)));
+  }
+
+  static seastar::logger& logger() {
+    return crimson::get_logger(ceph_subsys_filestore);
   }
 
   std::map<laddr_t, Ref<DummyNodeExtent>> allocate_map;
