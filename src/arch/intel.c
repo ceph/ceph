29590849ddbd99e846eff9dd476ea812107f5182
Changcheng Liu <changcheng.liu@aliyun.com>
2020-10-12 17:12:17 +0800

    arch/intel: detect IA AVX2 instruction
    Signed-off-by: Changcheng Liu <changcheng.liu@aliyun.com>

--- src/arch/intel.c
+++ src/arch/intel.c
@@ -25,6 +25,7 @@ int ceph_arch_intel_sse3 = 0;
 int ceph_arch_intel_sse2 = 0;
 int ceph_arch_intel_aesni = 0;
 int ceph_arch_intel_avx = 0;
+int ceph_arch_intel_avx2 = 0;
 
 #ifdef __x86_64__
 #include <cpuid.h>
@@ -61,6 +62,30 @@ static void detect_avx(void) {
     }
 }
 
+static void detect_avx2(void) {
+    uint32_t eax = 0, ebx = 0, ecx = 0, edx = 0;
+
+    uint32_t max_level = __get_cpuid_max(0, NULL);
+    if (max_level == 0) {
+        return;
+    }
+    __cpuid_count(1, 0, eax, ebx, ecx, edx);
+    if ((ecx & bit_OSXSAVE) == 0 || (ecx & bit_AVX) == 0) {
+        return;
+    }
+    if (max_level < 7) {
+        return;
+    }
+    __cpuid_count(7, 0, eax, ebx, ecx, edx);
+    if ((ebx & bit_AVX2) == 0) {
+        return;
+    }
+    int64_t xcr_mask = _xgetbv(_XCR_XFEATURE_ENABLED_MASK);
+    if (xcr_mask & _XCR_XMM_YMM_STATE_ENABLED_BY_OS) {
+        ceph_arch_intel_avx2 = 1;
+    }
+}
+
 int ceph_arch_intel_probe(void)
 {
     uint32_t eax = 0, ebx = 0, ecx = 0, edx = 0;
@@ -94,6 +119,7 @@ int ceph_arch_intel_probe(void)
     }
 
     detect_avx();
+    detect_avx2();
 
     return 0;
 }
--- src/arch/intel.h
+++ src/arch/intel.h
@@ -13,6 +13,7 @@ extern int ceph_arch_intel_sse3;   /* true: have sse 3 features */
 extern int ceph_arch_intel_sse2;   /* true: have sse 2 features */
 extern int ceph_arch_intel_aesni;  /* true: have aesni features */
 extern int ceph_arch_intel_avx;    /* ture: have avx features */
+extern int ceph_arch_intel_avx2;   /* ture: have avx2 features */
 
 extern int ceph_arch_intel_probe(void);
 
