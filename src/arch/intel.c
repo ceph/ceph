0755a7870f4875a0a2f73e23532ee8c9c74d204d
Changcheng Liu <changcheng.liu@aliyun.com>
2020-10-12 17:09:25 +0800

    arch/intel: detect IA avx instruction
    Signed-off-by: Changcheng Liu <changcheng.liu@aliyun.com>

--- src/arch/intel.c
+++ src/arch/intel.c
@@ -24,9 +24,42 @@ int ceph_arch_intel_ssse3 = 0;
 int ceph_arch_intel_sse3 = 0;
 int ceph_arch_intel_sse2 = 0;
 int ceph_arch_intel_aesni = 0;
+int ceph_arch_intel_avx = 0;
 
 #ifdef __x86_64__
 #include <cpuid.h>
+
+#ifndef _XCR_XFEATURE_ENABLED_MASK
+#define _XCR_XFEATURE_ENABLED_MASK 0
+#endif
+
+#ifndef _XCR_XMM_YMM_STATE_ENABLED_BY_OS
+#define _XCR_XMM_YMM_STATE_ENABLED_BY_OS 0x6
+#endif
+
+static inline int64_t _xgetbv(uint32_t index) {
+    uint32_t eax, edx;
+    __asm__ __volatile__("xgetbv" : "=a"(eax), "=d"(edx) : "c"(index));
+    return ((uint64_t)edx << 32) | eax;
+}
+
+static void detect_avx(void) {
+    uint32_t eax = 0, ebx = 0, ecx = 0, edx = 0;
+
+    uint32_t max_level = __get_cpuid_max(0, NULL);
+    if (max_level == 0) {
+        return;
+    }
+    __cpuid_count(1, 0, eax, ebx, ecx, edx);
+    if ((ecx & bit_OSXSAVE) == 0 || (ecx & bit_AVX) == 0) {
+        return;
+    }
+
+    int64_t xcr_mask = _xgetbv(_XCR_XFEATURE_ENABLED_MASK);
+    if (xcr_mask & _XCR_XMM_YMM_STATE_ENABLED_BY_OS) {
+        ceph_arch_intel_avx = 1;
+    }
+}
 
 int ceph_arch_intel_probe(void)
 {
@@ -36,8 +69,8 @@ int ceph_arch_intel_probe(void)
     if (max_level == 0) {
         return 0;
     }
-
     __cpuid_count(1, 0, eax, ebx, ecx, edx);
+
     if ((ecx & bit_PCLMUL) != 0) {
         ceph_arch_intel_pclmul = 1;
     }
@@ -59,6 +92,8 @@ int ceph_arch_intel_probe(void)
     if ((ecx & bit_AES) != 0) {
         ceph_arch_intel_aesni = 1;
     }
+
+    detect_avx();
 
     return 0;
 }
