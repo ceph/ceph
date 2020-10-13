935f336ffc27e5f31afa0f3b2e7a976247386e70
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2020-10-13 10:26:10 +0000

    common,librbd: add portable ceph_memzero_s
    We need a portable way of safely zero-ing out a buffer.
    
    We're adding ceph_memzero_s, which will use one of the
    following functions, dependning which one is available:
    memset_s, SecureZeroMemory and explicit_bzero.
    
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- src/librbd/crypto/openssl/DataCryptor.cc
+++ src/librbd/crypto/openssl/DataCryptor.cc
@@ -5,6 +5,7 @@
 #include <openssl/err.h>
 #include <string.h>
 #include "include/ceph_assert.h"
+#include "include/compat.h"
 
 namespace librbd {
 namespace crypto {
@@ -46,7 +47,8 @@ int DataCryptor::init(const char* cipher_name, const unsigned char* key,
 
 DataCryptor::~DataCryptor() {
   if (m_key != nullptr) {
-    explicit_bzero(m_key, EVP_CIPHER_key_length(m_cipher));
+    ceph_memzero_s(m_key, EVP_CIPHER_key_length(m_cipher),
+                   EVP_CIPHER_key_length(m_cipher));
     delete m_key;
     m_key = nullptr;
   }
