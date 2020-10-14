4f66ad018a31c7a896f98fc7b0c5b7ae19ff2307
Yehuda Sadeh <yehuda@redhat.com>
2020-10-14 16:51:15 -0700

    rgw: data sync: remove warning
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/rgw/rgw_data_sync.cc
+++ src/rgw/rgw_data_sync.cc
@@ -4477,8 +4477,8 @@ public:
 
         e.mtime = be.mtime;
 
-#warning FIXME link olh, olh_dm
-        e.op = be.get_modify_op();
+        e.op = be.get_modify_op(); /* CLS_RGW_OP_ADD or CLS_RGW_OP_LINK_OLH or CLS_RGW_OP_LINK_OLH_DM,
+                                      depending on be */
         e.is_versioned = be.is_versioned();
         e.owner = { be.owner.id, be.owner.display_name };
 
