5d80c545a95a343c5cf8b07ad40af4a955136e36
Kefu Chai <kchai@redhat.com>
2020-10-16 14:11:52 +0800

    crimson/common: do not take from a future twice
    before this change, in our specialization of seastar::do_until(),
    we access `f` after calling `f.get()`, this is not correct. as `f.get()`
    actually moves `f._state` away and detaches the associated promise if any.
    so we cannot call `f._then()` anymore after calling `f.get()`. as
    `f._then()` schedules `f` by detaching the future from promise and
    attaching the scheduled task to the promise. but `future_base::detach_promise()`
    does not check `_promise` before accessing it, hence the segfault.
    
    after this change, the order of the checks is rearranged so that
    `f.get()` is called at the end. and also use `f.get0()` to be more
    explicit, as we are accessing the only element of the returned
    value.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

---
+++ src/test/crimson/test_errorator.cc
@@ -0,0 +1,32 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:nil -*-
+// vim: ts=8 sw=2 smarttab
+
+#include "test/crimson/gtest_seastar.h"
+
+#include "crimson/common/errorator.h"
+#include "crimson/common/log.h"
+
+struct do_until_test_t : public seastar_test_suite_t {
+  using ertr = crimson::errorator<crimson::ct_error::invarg>;
+  template <class... ValuesT>
+  using future = ertr::future<ValuesT...>;
+  future<> test() {
+    return crimson::do_until([this]() -> future<bool> {
+      if (i < 5) {
+        ++i;
+        return ertr::make_ready_future<bool>(false);
+      } else {
+        return ertr::make_ready_future<bool>(true);
+      }
+    });
+  }
+
+  int i = 0;
+};
+
+TEST_F(do_until_test_t, basic)
+{
+  run_async([this] {
+    test().unsafe_get0();
+  });
+}
