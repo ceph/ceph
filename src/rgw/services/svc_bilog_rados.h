d047b5ffde89c85536a4ff9cdbf3ff1e6b63053e
Radoslaw Zarzynski <rzarzyns@redhat.com>
2020-10-20 18:15:53 +0200

    rgw: delegate the primitive max_marker to RGWSI_BILog_RADOS.
    The idea here is to make the `BucketIndexShardsManager`-based
    policy for primitive `max_marker` calculation to the InIndex
    backend BILog.
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/rgw/services/svc_bilog_rados.h
+++ src/rgw/services/svc_bilog_rados.h
@@ -51,6 +51,10 @@ public:
                                  const std::map<int, rgw_bucket_dir_header>& headers,
                                  int shard_id,
                                  std::map<int, std::string> *max_markers) = 0;
+  virtual int log_get_max_marker(const RGWBucketInfo& bucket_info,
+                                 const std::map<int, rgw_bucket_dir_header>& headers,
+                                 int shard_id,
+                                 std::string *max_marker) = 0;
 };
 
 class RGWSI_BILog_RADOS_InIndex : public RGWSI_BILog_RADOS
@@ -81,6 +85,10 @@ public:
                          const std::map<int, rgw_bucket_dir_header>& headers,
                          int shard_id,
                          std::map<int, std::string> *max_markers) override;
+  int log_get_max_marker(const RGWBucketInfo& bucket_info,
+                         const std::map<int, rgw_bucket_dir_header>& headers,
+                         int shard_id,
+                         std::string *max_marker) override;
 };
 
 // RGWSI_BILog_RADOS_FIFO -- the reader part of the cls_fifo-based backend
@@ -123,4 +131,8 @@ public:
                          const std::map<int, rgw_bucket_dir_header>& headers,
                          int shard_id,
                          std::map<int, string> *max_markers) override;
+  int log_get_max_marker(const RGWBucketInfo& bucket_info,
+                         const std::map<int, rgw_bucket_dir_header>& headers,
+                         int shard_id,
+                         std::string *max_marker) override;
 };
