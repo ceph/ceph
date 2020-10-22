d637c1b344174b0926fc9eb2c74bd14bd14f3c41
Volker Theile <vtheile@suse.com>
2020-10-22 11:17:31 +0200

    mgr/dashboard: displaying time in human-readable format
    Fixes: https://tracker.ceph.com/issues/47884
    
    Signed-off-by: Volker Theile <vtheile@suse.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/pipes/not-available.pipe.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/pipes/not-available.pipe.spec.ts
@@ -11,12 +11,20 @@ describe('NotAvailablePipe', () => {
     expect(pipe).toBeTruthy();
   });
 
-  it('transforms not available', () => {
+  it('transforms not available (1)', () => {
     expect(pipe.transform('')).toBe('n/a');
   });
 
-  it('transforms number', () => {
+  it('transforms not available (2)', () => {
+    expect(pipe.transform('', 'Unknown')).toBe('Unknown');
+  });
+
+  it('transform not necessary (1)', () => {
     expect(pipe.transform(0)).toBe(0);
     expect(pipe.transform(1)).toBe(1);
+  });
+
+  it('transform not necessary (2)', () => {
+    expect(pipe.transform('foo')).toBe('foo');
   });
 });
