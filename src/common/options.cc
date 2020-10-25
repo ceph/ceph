75768c25e260cf2f175cf17605e45bfa323a3a3c
Venky Shankar <vshankar@redhat.com>
2020-10-25 03:01:03 -0400

    cephfs-mirror: remove `cephfs_mirror_directory_choose_policy` option
    Basically, the mirror daemon would process directory list in in a
    round-robin fashion. Randomly choosing directories probably does not
    make much sense right now.
    
    Signed-off-by: Venky Shankar <vshankar@redhat.com>

--- src/common/options.cc
+++ src/common/options.cc
@@ -8898,11 +8898,6 @@ std::vector<Option> get_cephfs_mirror_options() {
     .set_description("maximum number of concurrent snapshot synchronization threads")
     .set_long_description("maximum number of directory snapshots that can be synchronized concurrently by cephfs-mirror daemon. Controls the number of synchronization threads."),
 
-    Option("cephfs_mirror_directory_choose_policy", Option::TYPE_STR, Option::LEVEL_ADVANCED)
-    .set_default("random")
-    .set_description("policy for choosing directories to mirror snapshots")
-    .set_long_description("policy used by cephfs-mirror daemon to choose directories for snapshot mirroring"),
-
     Option("cephfs_mirror_action_update_interval", Option::TYPE_SECS, Option::LEVEL_ADVANCED)
     .set_default(2)
     .set_min(1)
