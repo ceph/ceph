d7357663199ff0eef329832dafdbafdd271e4708
Changcheng Liu <changcheng.liu@aliyun.com>
2020-10-28 15:26:41 +0800

    cmake: remove yasm since it's been replaced by nasm
    Signed-off-by: Changcheng Liu <changcheng.liu@aliyun.com>

--- install-deps.sh
+++ install-deps.sh
@@ -221,7 +221,6 @@ if [ x$(uname)x = xFreeBSDx ]; then
         devel/gperf \
         devel/gmake \
         devel/cmake \
-        devel/yasm \
         devel/nasm \
         devel/boost-all \
         devel/boost-python-libs \
