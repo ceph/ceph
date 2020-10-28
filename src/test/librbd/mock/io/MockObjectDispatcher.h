435ed8c89619931ef6b7109d0149e178433b1d29
Or Ozeri <oro@il.ibm.com>
2020-10-28 14:22:06 +0200

    librbd: add LUKS support
    This commit introduces internal (not yet part of the api) librbd functions for:
    1. formating an RBD image in LUKS format
    2. parsing an RBD image in LUKS format
    
     The actual implementation of the LUKS format is done via libcryptsetup, which is added as a new dependency.
    
    Signed-off-by: Or Ozeri <oro@il.ibm.com>

--- src/test/librbd/mock/io/MockObjectDispatcher.h
+++ src/test/librbd/mock/io/MockObjectDispatcher.h
@@ -22,6 +22,7 @@ public:
   MOCK_METHOD1(shut_down, void(Context*));
 
   MOCK_METHOD1(register_dispatch, void(ObjectDispatchInterface*));
+  MOCK_METHOD1(exists, bool(ObjectDispatchLayer));
   MOCK_METHOD2(shut_down_dispatch, void(ObjectDispatchLayer, Context*));
 
   MOCK_METHOD2(flush, void(FlushSource, Context*));
