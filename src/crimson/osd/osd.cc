3638447f5d893672c5de669e5351afdbe80a0abc
Xuehan Xu <xxhdx1985126@gmail.com>
2020-11-01 11:29:57 +0800

    crimson/osd: rename heartbeat_timer to tick_timer
    Signed-off-by: Xuehan Xu <xxhdx1985126@gmail.com>

--- src/crimson/osd/osd.cc
+++ src/crimson/osd/osd.cc
@@ -89,7 +89,7 @@ OSD::OSD(int id, uint32_t nonce,
     shard_services{*this, whoami, *cluster_msgr, *public_msgr, *monc, *mgrc, *store},
     heartbeat{new Heartbeat{whoami, shard_services, *monc, hb_front_msgr, hb_back_msgr}},
     // do this in background
-    heartbeat_timer{[this] { update_heartbeat_peers(); }},
+    tick_timer{[this] { update_heartbeat_peers(); }},
     asok{seastar::make_lw_shared<crimson::admin::AdminSocket>()},
     osdmap_gate("OSD::osdmap_gate", std::make_optional(std::ref(shard_services)))
 {
@@ -1038,7 +1038,7 @@ seastar::future<> OSD::committed_osd_maps(version_t first,
         state.set_active();
         beacon_timer.arm_periodic(
           std::chrono::seconds(local_conf()->osd_beacon_report_interval));
-        heartbeat_timer.arm_periodic(
+        tick_timer.arm_periodic(
           std::chrono::seconds(TICK_INTERVAL));
       }
     } else if (!osdmap->is_up(whoami)) {
@@ -1205,7 +1205,7 @@ bool OSD::should_restart() const
 seastar::future<> OSD::restart()
 {
   beacon_timer.cancel();
-  heartbeat_timer.cancel();
+  tick_timer.cancel();
   up_epoch = 0;
   bind_epoch = osdmap->get_epoch();
   // TODO: promote to shutdown if being marked down for multiple times
