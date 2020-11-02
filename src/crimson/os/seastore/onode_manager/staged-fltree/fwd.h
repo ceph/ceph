744c2b7d8deae33e2bf5ab1844c0a5a83ee83ac4
Yingxin Cheng <yingxin.cheng@intel.com>
2020-11-02 13:24:18 +0800

    crimson/onode-staged-tree: implement string_view_masked_t
    Use string_view_masked_t to consolidate a string_key_view_t with type, a
    string/string_view, or a masked string. And leverage the consistant
    class to do compare, print, convert and append operations.
    
    Signed-off-by: Yingxin Cheng <yingxin.cheng@intel.com>

--- src/crimson/os/seastore/onode_manager/staged-fltree/fwd.h
+++ src/crimson/os/seastore/onode_manager/staged-fltree/fwd.h
@@ -73,35 +73,9 @@ MatchKindCMP toMatchKindCMP(const Type& l, const Type& r) {
   return toMatchKindCMP(match);
 }
 
-template <>
-inline MatchKindCMP toMatchKindCMP<std::string>(
-    const std::string& l, const std::string& r) {
+inline MatchKindCMP toMatchKindCMP(
+    std::string_view l, std::string_view r) {
   return toMatchKindCMP(l.compare(r));
-}
-
-inline MatchKindCMP toMatchKindCMP(
-    const char* l, size_t l_len, const char* r, size_t r_len) {
-  assert(l && l_len);
-  assert(r && r_len);
-  auto min_len = std::min(l_len, r_len);
-  auto match = toMatchKindCMP(std::strncmp(l, r, min_len));
-  if (match == MatchKindCMP::EQ) {
-    return toMatchKindCMP(l_len, r_len);
-  } else {
-    return match;
-  }
-}
-
-inline MatchKindCMP toMatchKindCMP(
-    const std::string& l, const char* r, size_t r_len) {
-  assert(r && r_len);
-  return toMatchKindCMP(l.compare(0u, l.length(), r, r_len));
-}
-
-inline MatchKindCMP toMatchKindCMP(
-    const char* l, size_t l_len, const std::string& r) {
-  assert(l && l_len);
-  return toMatchKindCMP(-r.compare(0u, r.length(), l, l_len));
 }
 
 inline MatchKindCMP reverse(MatchKindCMP cmp) {
