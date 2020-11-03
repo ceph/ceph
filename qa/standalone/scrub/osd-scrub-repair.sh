ef47a3e708da1b18865f2151e637ea12efdad8c1
David Zafman <dzafman@redhat.com>
2020-11-03 21:23:42 +0000

    test: set mon_allow_pool_size_one for consistency with original test intention
    Signed-off-by: David Zafman <dzafman@redhat.com>

--- qa/standalone/scrub/osd-scrub-repair.sh
+++ qa/standalone/scrub/osd-scrub-repair.sh
@@ -5804,7 +5804,7 @@ function TEST_scrub_warning() {
     local pool_overdue_seconds=$(calc $i14_days + $i1_day + \( $i14_days \* $overdue \) )
 
     setup $dir || return 1
-    run_mon $dir a --osd_pool_default_size=1 || return 1
+    run_mon $dir a --osd_pool_default_size=1 --mon_allow_pool_size_one=true || return 1
     run_mgr $dir x --mon_warn_pg_not_scrubbed_ratio=${overdue} --mon_warn_pg_not_deep_scrubbed_ratio=${overdue} || return 1
     run_osd $dir 0 $ceph_osd_args --osd_scrub_backoff_ratio=0 || return 1
 
@@ -6152,7 +6152,7 @@ function TEST_request_scrub_priority() {
     local PGS=8
 
     setup $dir || return 1
-    run_mon $dir a --osd_pool_default_size=1 || return 1
+    run_mon $dir a --osd_pool_default_size=1 --mon_allow_pool_size_one=true || return 1
     run_mgr $dir x || return 1
     local ceph_osd_args="--osd-scrub-interval-randomize-ratio=0 --osd-deep-scrub-randomize-ratio=0 "
     ceph_osd_args+="--osd_scrub_backoff_ratio=0"
