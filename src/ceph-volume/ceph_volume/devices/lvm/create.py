d4790f6749a7290495aed960e4c1381117f64e13
ZhenLiu94 <zhenliu94@163.com>
2020-11-12 10:11:06 +0800

    lvm/create.py: fix a typo in the help message ceph_volume/devices/lvm/create.py:corrected typo of the word when using ceph-volume lvm create -h
    Fixes: https://tracker.ceph.com/issues/48273
    Signed-off-by: ZhenLiu94 <zhenliu94@163.com>

--- src/ceph-volume/ceph_volume/devices/lvm/create.py
+++ src/ceph-volume/ceph_volume/devices/lvm/create.py
@@ -42,7 +42,7 @@ class Create(object):
         Create an OSD by assigning an ID and FSID, registering them with the
         cluster with an ID and FSID, formatting and mounting the volume, adding
         all the metadata to the logical volumes using LVM tags, and starting
-        the OSD daemon. This is a convinience command that combines the prepare
+        the OSD daemon. This is a convenience command that combines the prepare
         and activate steps.
 
         Encryption is supported via dmcrypt and the --dmcrypt flag.
