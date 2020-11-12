111b6cc36da15dea598c1337698bbcc48861d2f6
Igor Fedotov <ifedotov@suse.com>
2020-11-12 20:04:45 +0300

    os/bluestore: detect and fix "zombie" spanning blobs using fsck.
    Signed-off-by: Igor Fedotov <ifedotov@suse.com>
    (cherry picked from commit 18d120dc2c15c0cf8d3b27ddba3c7f2057856555)
    
     Conflicts:
     (trivial)      src/os/bluestore/BlueStore.cc
     (trivial)      src/os/bluestore/BlueStore.h

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -2886,6 +2886,7 @@ public:
   void inject_misreference(coll_t cid1, ghobject_t oid1,
 			   coll_t cid2, ghobject_t oid2,
 			   uint64_t offset);
+  void inject_zombie_spanning_blob(coll_t cid, ghobject_t oid, int16_t blob_id);
   // resets global per_pool_omap in DB
   void inject_legacy_omap();
   // resets per_pool_omap | pgmeta_omap for onode
@@ -3533,6 +3534,7 @@ public:
 		      FreelistManager* fm,
 		      uint64_t offset, uint64_t len);
   bool fix_bluefs_extents(std::atomic<uint64_t>& out_of_sync_flag);
+  KeyValueDB::Transaction fix_spanning_blobs(KeyValueDB* db);
 
   void init(uint64_t total_space, uint64_t lres_tracking_unit_size);
 
@@ -3571,6 +3573,7 @@ private:
   KeyValueDB::Transaction fix_shared_blob_txn;
 
   KeyValueDB::Transaction fix_misreferences_txn;
+  KeyValueDB::Transaction fix_onode_txn;
 
   StoreSpaceTracker space_usage_tracker;
 
