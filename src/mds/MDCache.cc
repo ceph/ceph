02ca2d84b9a90887208fb9226abdc1bef701a4aa
Yan, Zheng <zyan@redhat.com>
2020-11-16 22:44:00 +0800

    mds: dir->mark_new() should together with dir->mark_dirty()
    Otherwise, the 'new' flag won't get cleared after dir is committed.
    The leftover 'new' flag will confuse later journal and dir commit.
    
    Fixes: https://tracker.ceph.com/issues/48249
    Signed-off-by: "Yan, Zheng" <zyan@redhat.com>
    (cherry picked from commit be4710a1496c46f3307966ddfa416e932d878603)
    
    Conflicts:
            src/mds/MDCache.cc
    - in octopus, CDir::mark_dirty() takes a version_t argument in addition to the
      LogSegment* pointer

--- src/mds/MDCache.cc
+++ src/mds/MDCache.cc
@@ -11995,6 +11995,7 @@ void MDCache::_fragment_logged(MDRequestRef& mdr)
   for (const auto& dir : info.resultfrags) {
     dout(10) << " storing result frag " << *dir << dendl;
 
+    dir->mark_dirty(dir->pre_dirty(), mdr->ls);
     dir->mark_new(mdr->ls);
 
     // freeze and store them too
