04d876ced756ca86580bdff4ac116333dbb102e5
Kotresh HR <khiremat@redhat.com>
2020-11-18 15:43:25 +0530

    mgr/volumes: Persist auth and subvolume metadata
    1. Subvolume create and delete operations create and delete subvolume
       metadata file respectively.
    2. Subvolume authorize creates the auth meta file and persists the
       required metadata on subvolume metadata file and auth metdata file
       on disk. Subvolume deauthorize clears the required metadata on
       both metadata files.
    
    Fixes: https://tracker.ceph.com/issues/44931
    Signed-off-by: Kotresh HR <khiremat@redhat.com>

--- src/pybind/mgr/volumes/fs/operations/versions/subvolume_base.py
+++ src/pybind/mgr/volumes/fs/operations/versions/subvolume_base.py
@@ -15,6 +15,7 @@ from ..trash import create_trashcan, open_trashcan
 from ...fs_util import get_ancestor_xattr
 from ...exception import MetadataMgrException, VolumeException
 from .op_sm import SubvolumeOpSm
+from .auth_metadata import AuthMetadataManager
 
 log = logging.getLogger(__name__)
 
@@ -24,6 +25,7 @@ class SubvolumeBase(object):
     def __init__(self, mgr, fs, vol_spec, group, subvolname, legacy=False):
         self.mgr = mgr
         self.fs = fs
+        self.auth_mdata_mgr = AuthMetadataManager(fs)
         self.cmode = None
         self.user_id = None
         self.group_id = None
