ceph/ceph

commit f994925908c720f8e0b3fda2f1fc51ef6e757de3

mon/OSDMonitor: remove osd_epochs if osd is marked in or out
this is a cleanup. if we don't take the osd_epochs of osd marked out,
there is no need to keep those osd_epochs around anymore. so, in this
change, once the osd is marked in or out, we just drop its osd_epochs
right away.

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/mon/OSDMonitor.cc
@@ -891,9 +891,9 @@ void OSDMonitor::update_from_paxos(bool *need_bootstrap)
 	// could be marked up *or* down, but we're too lazy to check which
 	last_osd_report.erase(osd_state.first);
       }
-      if (osd_state.second & CEPH_OSD_EXISTS) {
-	// could be created *or* destroyed, but we can safely drop it
-	osd_epochs.erase(osd_state.first);
+      if (osd_state.second & CEPH_OSD_OUT) {
+        // could be marked in *or* out, but we can safely drop it
+        osd_epochs.erase(osd_state.first);
       }
     }
   }
@@ -2281,8 +2281,7 @@ epoch_t OSDMonitor::get_min_last_epoch_clean() const
   // also scan osd epochs
   // don't trim past the oldest reported osd epoch
   for (auto [osd, epoch] : osd_epochs) {
-    if (epoch < floor &&
-        osdmap.is_in(osd)) {
+    if (epoch < floor) {
       floor = epoch;
     }
   }

commit 0ecef75210113ffba1b89c061cd470ee321d7a45

mon/OSDMonitor: drop osd_epoch of manually marked out osd
this change addresses the regression introduced by
f994925908c720f8e0b3fda2f1fc51ef6e757de3.

in f994925908c720f8e0b3fda2f1fc51ef6e757de3, instead of ignoring the
osd_epoch from non-in OSD, the osd_epochs from osds marked out using
CEPH_OSD_OUT in its osd_state are dropped once they are marked out.

but the change failed to take those OSDs which are marked out manually
into consideration. we mark them out by changing inc.new_weight to
CEPH_OSD_OUT. and mon-last-epoch-clean.sh marks osd.2 out manually for
testing osdmap trimming. hence we have the regression after that change.

in this change, the manually marked out OSDs are also considered, so
their osd_epochs are also dropped in OSDMonitor::update_from_paxos().

Fixes: https://tracker.ceph.com/issues/48320
Signed-off-by: Kefu Chai <kchai@redhat.com>

src/mon/OSDMonitor.cc
@@ -896,6 +896,12 @@ void OSDMonitor::update_from_paxos(bool *need_bootstrap)
         osd_epochs.erase(osd_state.first);
       }
     }
+    for (const auto [osd, weight] : inc.new_weight) {
+      if (weight == CEPH_OSD_OUT) {
+        // manually marked out, so drop it
+        osd_epochs.erase(osd);
+      }
+    }
   }
 
   if (t) {