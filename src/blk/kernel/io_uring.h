da7cc003f84149b660ff21df6982d4fbe9c9ed2f
JiangYu <lnsyyj@hotmail.com>
2020-11-24 03:19:39 +0000

    blk/kernel: expose IORING_SETUP_{IOPOLL,SQPOLL} as options
    Signed-off-by: JiangYu <lnsyyj@hotmail.com>

--- src/blk/kernel/io_uring.h
+++ src/blk/kernel/io_uring.h
@@ -13,13 +13,15 @@ struct ioring_data;
 struct ioring_queue_t final : public io_queue_t {
   std::unique_ptr<ioring_data> d;
   unsigned iodepth = 0;
+  bool hipri = false;
+  bool sq_thread = false;
 
   typedef std::list<aio_t>::iterator aio_iter;
 
   // Returns true if arch is x86-64 and kernel supports io_uring
   static bool supported();
 
-  ioring_queue_t(unsigned iodepth_);
+  ioring_queue_t(unsigned iodepth_, bool hipri_, bool sq_thread_);
   ~ioring_queue_t() final;
 
   int init(std::vector<int> &fds) final;
