be26c566e5aaddb4370f032f813a517e62ebc291
Kefu Chai <kchai@redhat.com>
2020-12-01 13:43:40 +0800

    doc: remove edit_on_github sphinx extension
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- doc/_static/js/ceph.js
+++ doc/_static/js/ceph.js
@@ -1,24 +1,6 @@
 $(function() {
   var releases_url = DOCUMENTATION_OPTIONS.URL_ROOT + '_static/releases.json';
 
-  function show_edit(branch, data) {
-    if (branch) {
-      if (branch === "master") {
-        $("#dev-warning").show();
-        return true;
-      }
-      if (data && data.releases && branch in data.releases) {
-        var eol = ("actual_eol" in data.releases[branch]);
-        if (eol) {
-          $("#eol-warning").show();
-        }
-        return !eol;
-      }
-    }
-    $("#dev-warning").show();
-    return false;
-  }
-
   function get_branch() {
     var pathname = window.location.pathname;
     var res = pathname.match(/en\/([a-z]+)\/?/i)
@@ -30,14 +12,15 @@ $(function() {
 
   $.getJSON(releases_url, function(data) {
     var branch = get_branch();
-    if (show_edit(branch, data)) {
-      // patch the edit-on-github URL for correct branch
-      var url = $("#edit-on-github").attr("href");
-      if (url)  {
-        url = url.replace("master", branch);
-        $("#edit-on-github").attr("href", url);
+    if (branch === "master") {
+      $("#dev-warning").show();
+    } else if (data && data.releases && branch in data.releases) {
+      var eol = ("actual_eol" in data.releases[branch]);
+      if (eol) {
+        $("#eol-warning").show();
       }
-      $("#docubetter").show();
+    } else {
+      $("#dev-warning").show();
     }
   });
 });
