0a109346bda864f60fce1f0d66463b1849d5ba79
Shilpa Jagannath <smanjara@redhat.com>
2020-12-03 11:53:40 +0530

    rgw: add generation support to BILog admin APIs
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>

--- src/rgw/rgw_sal_dbstore.h
+++ src/rgw/rgw_sal_dbstore.h
@@ -187,12 +187,14 @@ protected:
       virtual RGWAccessControlPolicy& get_acl(void) override { return acls; }
       virtual int set_acl(const DoutPrefixProvider *dpp, RGWAccessControlPolicy& acl, optional_yield y) override;
       virtual int load_bucket(const DoutPrefixProvider *dpp, optional_yield y, bool get_stats = false) override;
-      virtual int read_stats(const DoutPrefixProvider *dpp, int shard_id,
+      virtual int read_stats(const DoutPrefixProvider *dpp,
+			     const bucket_index_layout_generation& idx_layout,
+			     int shard_id,
           std::string *bucket_ver, std::string *master_ver,
           std::map<RGWObjCategory, RGWStorageStats>& stats,
           std::string *max_marker = nullptr,
           bool *syncstopped = nullptr) override;
-      virtual int read_stats_async(const DoutPrefixProvider *dpp, int shard_id, RGWGetBucketStats_CB* ctx) override;
+      virtual int read_stats_async(const DoutPrefixProvider *dpp, const bucket_index_layout_generation& idx_layout, int shard_id, RGWGetBucketStats_CB* ctx) override;
       virtual int sync_user_stats(const DoutPrefixProvider *dpp, optional_yield y) override;
       virtual int update_container_stats(const DoutPrefixProvider *dpp) override;
       virtual int check_bucket_shards(const DoutPrefixProvider *dpp) override;
