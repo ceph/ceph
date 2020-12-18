c5b0fd2ebacca3008f19397c10cf87129d8ff2d4
Mykola Golub <mgolub@suse.com>
2020-12-18 10:15:11 +0000

    librbd: don't send 'image updated' notifications
    when enabling/disabling group mirroring
    
    Signed-off-by: Mykola Golub <mgolub@suse.com>
    Signed-off-by: Prasanna Kumar Kalever <prasanna.kalever@redhat.com>

--- src/librbd/mirror/ImageRemoveRequest.cc
+++ src/librbd/mirror/ImageRemoveRequest.cc
@@ -28,6 +28,90 @@ ImageRemoveRequest<I>::ImageRemoveRequest(
 
 template <typename I>
 void ImageRemoveRequest<I>::send() {
+  get_group();
+}
+
+template <typename I>
+void ImageRemoveRequest<I>::get_group() {
+  ldout(m_cct, 10) << dendl;
+  librados::ObjectReadOperation op;
+  cls_client::image_group_get_start(&op);
+
+  auto comp = create_rados_callback<
+    ImageRemoveRequest<I>, &ImageRemoveRequest<I>::handle_get_group>(this);
+  m_out_bl.clear();
+  int r = m_io_ctx.aio_operate(util::header_name(m_image_id), comp, &op,
+                               &m_out_bl);
+  ceph_assert(r == 0);
+  comp->release();
+}
+
+template <typename I>
+void ImageRemoveRequest<I>::handle_get_group(int r) {
+  ldout(m_cct, 10) << "r=" << r << dendl;
+
+  if (r == 0) {
+    auto iter = m_out_bl.cbegin();
+    r = cls_client::image_group_get_finish(&iter, &m_group_spec);
+  }
+
+  if (r < 0) {
+    lderr(m_cct) << "failed to retrieve image group: " << cpp_strerror(r)
+                 << dendl;
+    finish(r);
+    return;
+  }
+
+  get_mirror_group();
+}
+
+template <typename I>
+void ImageRemoveRequest<I>::get_mirror_group() {
+  if (!m_group_spec.is_valid()) {
+    m_mirror_group.state = cls::rbd::MIRROR_GROUP_STATE_DISABLED;
+    remove_mirror_image();
+    return;
+  }
+
+  ldout(m_cct, 10) << dendl;
+
+  int r = util::create_ioctx(m_io_ctx, "group", m_group_spec.pool_id, {},
+                             &m_group_io_ctx);
+  if (r < 0) {
+    finish(r);
+    return;
+  }
+
+  librados::ObjectReadOperation op;
+  cls_client::mirror_group_get_start(&op, m_group_spec.group_id);
+
+  auto comp = create_rados_callback<
+    ImageRemoveRequest<I>,
+    &ImageRemoveRequest<I>::handle_get_mirror_group>(this);
+  m_out_bl.clear();
+  r = m_group_io_ctx.aio_operate(RBD_MIRRORING, comp, &op, &m_out_bl);
+  ceph_assert(r == 0);
+  comp->release();
+}
+
+template <typename I>
+void ImageRemoveRequest<I>::handle_get_mirror_group(int r) {
+  ldout(m_cct, 10) << "r=" << r << dendl;
+
+  if (r == 0) {
+    auto iter = m_out_bl.cbegin();
+    r = cls_client::mirror_group_get_finish(&iter, &m_mirror_group);
+  }
+
+  if (r == -ENOENT) {
+    m_mirror_group.state = cls::rbd::MIRROR_GROUP_STATE_DISABLED;
+  } else if (r < 0) {
+    lderr(m_cct) << "failed to retrieve group mirroring state: "
+                 << cpp_strerror(r) << dendl;
+    finish(r);
+    return;
+  }
+
   remove_mirror_image();
 }
 
@@ -62,6 +146,12 @@ void ImageRemoveRequest<I>::handle_remove_mirror_image(int r) {
 
 template <typename I>
 void ImageRemoveRequest<I>::notify_mirroring_watcher() {
+  // skip image notification if mirroring for the image group is disabling
+  if (m_mirror_group.state == cls::rbd::MIRROR_GROUP_STATE_DISABLING) {
+    finish(0);
+    return;
+  }
+
   ldout(m_cct, 10) << dendl;
 
   auto ctx = util::create_context_callback<
