45e6779da6ed5a9bfc9e4bd42be0a9de42a3527a
Mykola Golub <mgolub@suse.com>
2020-12-18 10:15:11 +0000

    librbd: don't send 'image updated' notifications
    when enabling/disabling group mirroring
    
    Signed-off-by: Mykola Golub <mgolub@suse.com>
    Signed-off-by: Prasanna Kumar Kalever <prasanna.kalever@redhat.com>

--- src/librbd/mirror/ImageStateUpdateRequest.cc
+++ src/librbd/mirror/ImageStateUpdateRequest.cc
@@ -39,7 +39,12 @@ void ImageStateUpdateRequest<I>::send() {
 template <typename I>
 void ImageStateUpdateRequest<I>::get_mirror_image() {
   if (!m_mirror_image.global_image_id.empty()) {
-    set_mirror_image();
+    if (m_mirror_image.state == m_mirror_image_state) {
+      finish(0);
+      return;
+    }
+
+    get_group();
     return;
   }
 
@@ -75,13 +80,54 @@ void ImageStateUpdateRequest<I>::handle_get_mirror_image(int r) {
     return;
   }
 
-  set_mirror_image();
+  if (m_mirror_image.state == m_mirror_image_state) {
+    finish(0);
+    return;
+  }
+
+  get_group();
 }
 
 template <typename I>
-void ImageStateUpdateRequest<I>::set_mirror_image() {
-  if (m_mirror_image.state == m_mirror_image_state) {
-    finish(0);
+void ImageStateUpdateRequest<I>::get_group() {
+  ldout(m_cct, 10) << dendl;
+  librados::ObjectReadOperation op;
+  cls_client::image_group_get_start(&op);
+
+  auto comp = create_rados_callback<
+    ImageStateUpdateRequest<I>,
+    &ImageStateUpdateRequest<I>::handle_get_group>(this);
+  m_out_bl.clear();
+  int r = m_io_ctx.aio_operate(util::header_name(m_image_id), comp, &op,
+                               &m_out_bl);
+  ceph_assert(r == 0);
+  comp->release();
+}
+
+template <typename I>
+void ImageStateUpdateRequest<I>::handle_get_group(int r) {
+  ldout(m_cct, 10) << "r=" << r << dendl;
+
+  if (r == 0) {
+    auto iter = m_out_bl.cbegin();
+    r = cls_client::image_group_get_finish(&iter, &m_group_spec);
+  }
+
+  if (r < 0) {
+    lderr(m_cct) << "failed to retrieve image group: " << cpp_strerror(r)
+                 << dendl;
+    finish(r);
+    return;
+  }
+
+  get_mirror_group();
+}
+
+template <typename I>
+void ImageStateUpdateRequest<I>::get_mirror_group() {
+  if (!m_group_spec.is_valid()) {
+    m_mirror_group.state = cls::rbd::MIRROR_GROUP_STATE_DISABLED;
+    set_mirror_image();
     return;
   } else if (m_mirror_group.state == cls::rbd::MIRROR_GROUP_STATE_ENABLED ||
              m_mirror_group.state == cls::rbd::MIRROR_GROUP_STATE_ENABLING) {
@@ -89,6 +135,50 @@ void ImageStateUpdateRequest<I>::set_mirror_image() {
     m_mirror_image.group_spec = m_group_spec;
   }
 
+  ldout(m_cct, 10) << dendl;
+
+  int r = util::create_ioctx(m_io_ctx, "group", m_group_spec.pool_id, {},
+                             &m_group_io_ctx);
+  if (r < 0) {
+    finish(r);
+    return;
+  }
+
+  librados::ObjectReadOperation op;
+  cls_client::mirror_group_get_start(&op, m_group_spec.group_id);
+
+  auto comp = create_rados_callback<
+    ImageStateUpdateRequest<I>,
+    &ImageStateUpdateRequest<I>::handle_get_mirror_group>(this);
+  m_out_bl.clear();
+  r = m_group_io_ctx.aio_operate(RBD_MIRRORING, comp, &op, &m_out_bl);
+  ceph_assert(r == 0);
+  comp->release();
+}
+
+template <typename I>
+void ImageStateUpdateRequest<I>::handle_get_mirror_group(int r) {
+  ldout(m_cct, 10) << "r=" << r << dendl;
+
+  if (r == 0) {
+    auto iter = m_out_bl.cbegin();
+    r = cls_client::mirror_group_get_finish(&iter, &m_mirror_group);
+  }
+
+  if (r == -ENOENT) {
+    m_mirror_group.state = cls::rbd::MIRROR_GROUP_STATE_DISABLED;
+  } else if (r < 0) {
+    lderr(m_cct) << "failed to retrieve group mirroring state: "
+                 << cpp_strerror(r) << dendl;
+    finish(r);
+    return;
+  }
+
+  set_mirror_image();
+}
+
+template <typename I>
+void ImageStateUpdateRequest<I>::set_mirror_image() {
   ldout(m_cct, 10) << dendl;
   m_mirror_image.state = m_mirror_image_state;
 
@@ -108,7 +198,7 @@ void ImageStateUpdateRequest<I>::handle_set_mirror_image(int r) {
   ldout(m_cct, 10) << "r=" << r << dendl;
 
   if (r < 0) {
-    lderr(m_cct) << "failed to disable mirroring image: " << cpp_strerror(r)
+    lderr(m_cct) << "failed to set mirror image: " << cpp_strerror(r)
                  << dendl;
     finish(r);
     return;
@@ -119,6 +209,12 @@ void ImageStateUpdateRequest<I>::handle_set_mirror_image(int r) {
 
 template <typename I>
 void ImageStateUpdateRequest<I>::notify_mirroring_watcher() {
+  // skip image notification if mirroring for the image group is disabled
+  if (m_mirror_group.state != cls::rbd::MIRROR_GROUP_STATE_DISABLED) {
+    finish(0);
+    return;
+  }
+
   ldout(m_cct, 10) << dendl;
 
   auto ctx = util::create_context_callback<
