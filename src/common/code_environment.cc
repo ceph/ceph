sunilangadi2/ceph-ci

common: Add get_process_name implementation for Windows
This patch adds get_process_name for Windows based on
GetModuleFileName.

Signed-off-by: Alin Gabriel Serdean <aserdean@cloudbasesolutions.com>

src/common/code_environment.cc
@@ -79,6 +79,35 @@ int get_process_name(char *buf, int len)
   return 0;
 }
 
+#elif defined(_WIN32)
+
+int get_process_name(char *buf, int len)
+{
+  if (len <= 0) {
+    return -EINVAL;
+  }
+
+  char location[MAX_PATH + 1];
+  int length = GetModuleFileNameA(nullptr, location, sizeof(location) - 1);
+  if (length <= 0)
+    return -ENOSYS;
+  if (length == sizeof(location) - 1)
+    location[length] = '\0';
+
+  char* start = strrchr(location, '\\');
+  if (!start)
+    return -ENOSYS;
+  start++;
+  char* end = strstr(start, ".exe");
+  if (!end)
+    return -ENOSYS;
+  *end = '\0';
+
+  strncpy(buf, start, len - 1);
+  buf[len - 1] = '\0';
+  return 0;
+}
+
 #else
 
 int get_process_name(char *buf, int len)

ceph/ceph

src/common/code_environment.cc
@@ -79,6 +79,31 @@ int get_process_name(char *buf, int len)
   return 0;
 }
 
+#elif defined(_WIN32)
+
+int get_process_name(char *buf, int len)
+{
+  if (len <= 0) {
+    return -EINVAL;
+  }
+
+  int length = GetModuleFileNameA(nullptr, buf, len);
+  if (length <= 0)
+    return -ENOSYS;
+
+  char* start = strrchr(buf, '\\');
+  if (!start)
+    return -ENOSYS;
+  start++;
+  char* end = strstr(start, ".exe");
+  if (!end)
+    return -ENOSYS;
+
+  memmove(buf, start, end - start);
+  buf[end - start] = '\0';
+  return 0;
+}
+
 #else
 
 int get_process_name(char *buf, int len)