4fea0ed76ecf8145a71a74ad3292c95b03ec9ded
Mykola Golub <mgolub@suse.com>
2020-12-30 16:37:31 +0000

    rbd-mirror: introduce generalized mirror entity (type, global_id, size)
    where type may be image or group. Make pool watcher and image
    mapper use it currently for images. The group support is coming.
    
    Signed-off-by: Mykola Golub <mgolub@suse.com>
    Signed-off-by: Prasanna Kumar Kalever <prasanna.kalever@redhat.com>

--- src/tools/rbd_mirror/image_map/Policy.h
+++ src/tools/rbd_mirror/image_map/Policy.h
@@ -27,53 +27,53 @@ public:
   }
 
   // init -- called during initialization
-  void init(
-      const std::map<std::string, cls::rbd::MirrorImageMap> &image_mapping);
+  void init(const std::map<GlobalId, cls::rbd::MirrorImageMap> &image_mapping);
 
   // lookup an image from the map
-  LookupInfo lookup(const std::string &global_image_id);
+  LookupInfo lookup(const GlobalId &global_id);
 
   // add, remove
-  bool add_image(const std::string &global_image_id);
-  bool remove_image(const std::string &global_image_id);
+  bool add_entity(const GlobalId &global_id, uint64_t weight);
+  bool remove_entity(const GlobalId &global_id);
 
   // shuffle images when instances are added/removed
-  void add_instances(const InstanceIds &instance_ids,
-                     GlobalImageIds* global_image_ids);
-  void remove_instances(const InstanceIds &instance_ids,
-                        GlobalImageIds* global_image_ids);
+  void add_instances(const InstanceIds &instance_ids, GlobalIds* global_ids);
+  void remove_instances(const InstanceIds &instance_ids, GlobalIds* global_ids);
 
-  ActionType start_action(const std::string &global_image_id);
-  bool finish_action(const std::string &global_image_id, int r);
+  ActionType start_action(const GlobalId &global_id);
+  bool finish_action(const GlobalId &global_id, int r);
 
 protected:
-  typedef std::map<std::string, std::set<std::string> > InstanceToImageMap;
+  typedef std::map<std::string, GlobalIds> InstanceToImageMap;
 
-  bool is_dead_instance(const std::string instance_id) {
+  bool is_dead_instance(const std::string instance_id) const {
     ceph_assert(ceph_mutex_is_locked(m_map_lock));
     return m_dead_instances.find(instance_id) != m_dead_instances.end();
   }
 
-  bool is_image_shuffling(const std::string &global_image_id);
-  bool can_shuffle_image(const std::string &global_image_id);
+  bool is_entity_shuffling(const GlobalId &global_id) const;
+  bool can_shuffle_entity(const GlobalId &global_id) const;
+  uint64_t get_weight(const GlobalId &global_id) const;
 
-  // map an image (global image id) to an instance
+  // map an entity (global id) to an instance
   virtual std::string do_map(const InstanceToImageMap& map,
-                             const std::string &global_image_id) = 0;
+                             const GlobalId &global_id) = 0;
 
   // shuffle images when instances are added/removed
   virtual void do_shuffle_add_instances(
       const InstanceToImageMap& map, size_t image_count,
-      std::set<std::string> *remap_global_image_ids) = 0;
+      GlobalIds *remap_global_ids) = 0;
 
 private:
   struct ImageState {
+    uint64_t weight = 1;
     std::string instance_id = UNMAPPED_INSTANCE_ID;
     utime_t mapped_time;
 
     ImageState() {}
-    ImageState(const std::string& instance_id, const utime_t& mapped_time)
-      : instance_id(instance_id), mapped_time(mapped_time) {
+    ImageState(uint64_t weight, const std::string& instance_id,
+               const utime_t& mapped_time)
+      : weight(weight), instance_id(instance_id), mapped_time(mapped_time) {
     }
 
     // active state and action
@@ -84,7 +84,7 @@ private:
     boost::optional<StateTransition::State> next_state = boost::none;
   };
 
-  typedef std::map<std::string, ImageState> ImageStates;
+  typedef std::map<GlobalId, ImageState> ImageStates;
 
   librados::IoCtx &m_ioctx;
 
@@ -98,17 +98,16 @@ private:
 
   void remove_instances(const ceph::shared_mutex& lock,
 			const InstanceIds &instance_ids,
-                        GlobalImageIds* global_image_ids);
+                        GlobalIds* global_ids);
 
   bool set_state(ImageState* image_state, StateTransition::State state,
                  bool ignore_current_state);
 
-  void execute_policy_action(const std::string& global_image_id,
-                             ImageState* image_state,
+  void execute_policy_action(const GlobalId& global_id, ImageState* image_state,
                              StateTransition::PolicyAction policy_action);
 
-  void map(const std::string& global_image_id, ImageState* image_state);
-  void unmap(const std::string &global_image_id, ImageState* image_state);
+  void map(const GlobalId &global_id, ImageState* image_state);
+  void unmap(const GlobalId &global_id, ImageState* image_state);
 
   bool is_state_scheduled(const ImageState& image_state,
                           StateTransition::State state) const;
