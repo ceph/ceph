13dec07f39c53d333fffed7c93c56a8a0bb509e9
Mykola Golub <mgolub@suse.com>
2020-12-30 16:37:31 +0000

    rbd-mirror: introduce generalized mirror entity (type, global_id, size)
    where type may be image or group. Make pool watcher and image
    mapper use it currently for images. The group support is coming.
    
    Signed-off-by: Mykola Golub <mgolub@suse.com>
    Signed-off-by: Prasanna Kumar Kalever <prasanna.kalever@redhat.com>

--- src/tools/rbd_mirror/ImageMap.cc
+++ src/tools/rbd_mirror/ImageMap.cc
@@ -27,6 +27,8 @@ namespace mirror {
 
 using ::operator<<;
 using image_map::Policy;
+using image_map::GlobalId;
+using image_map::GlobalIds;
 
 using librbd::util::unique_lock_name;
 using librbd::util::create_async_context_callback;
@@ -34,21 +36,21 @@ using librbd::util::create_async_context_callback;
 template <typename I>
 struct ImageMap<I>::C_NotifyInstance : public Context {
   ImageMap* image_map;
-  std::string global_image_id;
+  GlobalId global_id;
   bool acquire_release;
 
-  C_NotifyInstance(ImageMap* image_map, const std::string& global_image_id,
+  C_NotifyInstance(ImageMap* image_map, const GlobalId& global_id,
                    bool acquire_release)
-    : image_map(image_map), global_image_id(global_image_id),
+    : image_map(image_map), global_id(global_id),
       acquire_release(acquire_release) {
     image_map->start_async_op();
   }
 
   void finish(int r) override {
     if (acquire_release) {
-      image_map->handle_peer_ack(global_image_id, r);
+      image_map->handle_peer_ack(global_id, r);
     } else {
-      image_map->handle_peer_ack_remove(global_image_id, r);
+      image_map->handle_peer_ack_remove(global_id, r);
     }
     image_map->finish_async_op();
   }
@@ -72,8 +74,7 @@ ImageMap<I>::~ImageMap() {
 }
 
 template <typename I>
-void ImageMap<I>::continue_action(const std::set<std::string> &global_image_ids,
-                                  int r) {
+void ImageMap<I>::continue_action(const GlobalIds &global_ids, int r) {
   dout(20) << dendl;
 
   {
@@ -82,10 +83,10 @@ void ImageMap<I>::continue_action(const std::set<std::string> &global_image_ids,
       return;
     }
 
-    for (auto const &global_image_id : global_image_ids) {
-      bool schedule = m_policy->finish_action(global_image_id, r);
+    for (auto const &global_id : global_ids) {
+      bool schedule = m_policy->finish_action(global_id, r);
       if (schedule) {
-        schedule_action(global_image_id);
+        schedule_action(global_id);
       }
     }
   }
@@ -95,24 +96,22 @@ void ImageMap<I>::continue_action(const std::set<std::string> &global_image_ids,
 
 template <typename I>
 void ImageMap<I>::handle_update_request(
-    const Updates &updates,
-    const std::set<std::string> &remove_global_image_ids, int r) {
+    const Updates &updates, const GlobalIds &remove_global_ids, int r) {
   dout(20) << "r=" << r << dendl;
 
-  std::set<std::string> global_image_ids;
+  GlobalIds global_ids;
 
-  global_image_ids.insert(remove_global_image_ids.begin(),
-                          remove_global_image_ids.end());
+  global_ids.insert(remove_global_ids.begin(), remove_global_ids.end());
   for (auto const &update : updates) {
-    global_image_ids.insert(update.global_image_id);
+    global_ids.insert({update.entity.type, update.entity.global_id});
   }
 
-  continue_action(global_image_ids, r);
+  continue_action(global_ids, r);
 }
 
 template <typename I>
-void ImageMap<I>::update_image_mapping(Updates&& map_updates,
-                                       std::set<std::string>&& map_removals) {
+void ImageMap<I>::update_image_mapping(Updates &&map_updates,
+                                       GlobalIds &&map_removals) {
   if (map_updates.empty() && map_removals.empty()) {
     return;
   }
@@ -130,15 +129,15 @@ void ImageMap<I>::update_image_mapping(Updates&& map_updates,
   // empty meta policy for now..
   image_map::PolicyMetaNone policy_meta;
 
-  bufferlist bl;
-  encode(image_map::PolicyData(policy_meta), bl);
-
   // prepare update map
-  std::map<std::string, cls::rbd::MirrorImageMap> update_mapping;
+  std::map<GlobalId, cls::rbd::MirrorImageMap> update_mapping;
   for (auto const &update : map_updates) {
+    bufferlist bl;
+    encode(image_map::PolicyData(update.entity.count, policy_meta), bl);
+
     update_mapping.emplace(
-      update.global_image_id, cls::rbd::MirrorImageMap(update.instance_id,
-      update.mapped_time, bl));
+        GlobalId{update.entity.type, update.entity.global_id},
+        cls::rbd::MirrorImageMap(update.instance_id, update.mapped_time, bl));
   }
 
   start_async_op();
@@ -155,18 +154,17 @@ void ImageMap<I>::process_updates() {
   ceph_assert(m_timer_task == nullptr);
 
   Updates map_updates;
-  std::set<std::string> map_removals;
+  GlobalIds map_removals;
   Updates acquire_updates;
   Updates release_updates;
 
   // gather updates by advancing the state machine
   m_lock.lock();
-  for (auto const &global_image_id : m_global_image_ids) {
-    image_map::ActionType action_type =
-      m_policy->start_action(global_image_id);
-    image_map::LookupInfo info = m_policy->lookup(global_image_id);
+  for (auto const &global_id : m_global_ids) {
+    image_map::ActionType action_type = m_policy->start_action(global_id);
+    image_map::LookupInfo info = m_policy->lookup(global_id);
 
-    dout(15) << "global_image_id=" << global_image_id << ", "
+    dout(15) << "global_id=" << global_id << ", "
              << "action=" << action_type << ", "
              << "instance=" << info.instance_id << dendl;
     switch (action_type) {
@@ -174,23 +172,28 @@ void ImageMap<I>::process_updates() {
       continue;
     case image_map::ACTION_TYPE_MAP_UPDATE:
       ceph_assert(info.instance_id != image_map::UNMAPPED_INSTANCE_ID);
-      map_updates.emplace_back(global_image_id, info.instance_id,
-                               info.mapped_time);
+      map_updates.emplace_back(
+          MirrorEntity{global_id.type, global_id.id, info.weight},
+          info.instance_id, info.mapped_time);
       break;
     case image_map::ACTION_TYPE_MAP_REMOVE:
-      map_removals.emplace(global_image_id);
+      map_removals.emplace(global_id);
       break;
     case image_map::ACTION_TYPE_ACQUIRE:
       ceph_assert(info.instance_id != image_map::UNMAPPED_INSTANCE_ID);
-      acquire_updates.emplace_back(global_image_id, info.instance_id);
+      acquire_updates.emplace_back(
+          MirrorEntity{global_id.type, global_id.id, info.weight},
+          info.instance_id);
       break;
     case image_map::ACTION_TYPE_RELEASE:
       ceph_assert(info.instance_id != image_map::UNMAPPED_INSTANCE_ID);
-      release_updates.emplace_back(global_image_id, info.instance_id);
+      release_updates.emplace_back(
+          MirrorEntity{global_id.type, global_id.id, info.weight},
+          info.instance_id);
       break;
     }
   }
-  m_global_image_ids.clear();
+  m_global_ids.clear();
   m_lock.unlock();
 
   // notify listener (acquire, release) and update on-disk map. note
@@ -203,11 +206,14 @@ void ImageMap<I>::process_updates() {
 template <typename I>
 void ImageMap<I>::schedule_update_task() {
   std::lock_guard timer_lock{m_threads->timer_lock};
-  schedule_update_task(m_threads->timer_lock);
+  CephContext *cct = reinterpret_cast<CephContext *>(m_ioctx.cct());
+  double after = cct->_conf.get_val<double>("rbd_mirror_image_policy_update_throttle_interval");
+  schedule_update_task(m_threads->timer_lock, after);
 }
 
 template <typename I>
-void ImageMap<I>::schedule_update_task(const ceph::mutex &timer_lock) {
+void ImageMap<I>::schedule_update_task(const ceph::mutex &timer_lock,
+                                       double after) {
   ceph_assert(ceph_mutex_is_locked(m_threads->timer_lock));
 
   schedule_rebalance_task();
@@ -218,7 +224,7 @@ void ImageMap<I>::schedule_update_task(const ceph::mutex &timer_lock) {
 
   {
     std::lock_guard locker{m_lock};
-    if (m_global_image_ids.empty()) {
+    if (m_global_ids.empty()) {
       return;
     }
   }
@@ -229,9 +235,6 @@ void ImageMap<I>::schedule_update_task(const ceph::mutex &timer_lock) {
 
       process_updates();
     });
-
-  CephContext *cct = reinterpret_cast<CephContext *>(m_ioctx.cct());
-  double after = cct->_conf.get_val<double>("rbd_mirror_image_policy_update_throttle_interval");
 
   dout(20) << "scheduling image check update (" << m_timer_task << ")"
            << " after " << after << " second(s)" << dendl;
@@ -244,19 +247,19 @@ void ImageMap<I>::rebalance() {
 
   {
     std::lock_guard locker{m_lock};
-    if (m_async_op_tracker.empty() && m_global_image_ids.empty()){
+    if (m_async_op_tracker.empty() && m_global_ids.empty()){
       dout(20) << "starting rebalance" << dendl;
 
-      std::set<std::string> remap_global_image_ids;
-      m_policy->add_instances({}, &remap_global_image_ids);
+      GlobalIds remap_global_ids;
+      m_policy->add_instances({}, &remap_global_ids);
 
-      for (auto const &global_image_id : remap_global_image_ids) {
-        schedule_action(global_image_id);
+      for (auto const &global_id : remap_global_ids) {
+        schedule_action(global_id);
       }
     }
   }
 
-  schedule_update_task(m_threads->timer_lock);
+  schedule_update_task();
 }
 
 template <typename I>
@@ -290,11 +293,11 @@ void ImageMap<I>::schedule_rebalance_task() {
 }
 
 template <typename I>
-void ImageMap<I>::schedule_action(const std::string &global_image_id) {
-  dout(20) << "global_image_id=" << global_image_id << dendl;
+void ImageMap<I>::schedule_action(const GlobalId &global_id) {
+  dout(20) << "global_id=" << global_id << dendl;
   ceph_assert(ceph_mutex_is_locked(m_lock));
 
-  m_global_image_ids.emplace(global_image_id);
+  m_global_ids.emplace(global_id);
 }
 
 template <typename I>
@@ -308,19 +311,35 @@ void ImageMap<I>::notify_listener_acquire_release_images(
           << "release=[" << release << "]" << dendl;
 
   for (auto const &update : acquire) {
-    m_listener.acquire_image(
-      update.global_image_id, update.instance_id,
-      create_async_context_callback(
-        m_threads->work_queue,
-        new C_NotifyInstance(this, update.global_image_id, true)));
+    auto global_id = GlobalId(update.entity.type, update.entity.global_id);
+    if (update.entity.type == MIRROR_ENTITY_TYPE_IMAGE) {
+      m_listener.acquire_image(
+        update.entity.global_id, update.instance_id,
+        create_async_context_callback(
+          m_threads->work_queue,
+          new C_NotifyInstance(this, global_id, true)));
+    } else if (update.entity.type == MIRROR_ENTITY_TYPE_GROUP) {
+      // TODO
+    } else {
+      ceph_abort_msgf("invalid mirror entity type: %d",
+                      (int)update.entity.type);
+    }
   }
 
   for (auto const &update : release) {
-    m_listener.release_image(
-      update.global_image_id, update.instance_id,
-      create_async_context_callback(
-        m_threads->work_queue,
-        new C_NotifyInstance(this, update.global_image_id, true)));
+    auto global_id = GlobalId(update.entity.type, update.entity.global_id);
+    if (update.entity.type == MIRROR_ENTITY_TYPE_IMAGE || update.entity.count != 0) {
+      m_listener.release_image(
+        update.entity.global_id, update.instance_id,
+        create_async_context_callback(
+          m_threads->work_queue,
+          new C_NotifyInstance(this, global_id, true)));
+    } else if (update.entity.type == MIRROR_ENTITY_TYPE_GROUP) {
+      // TODO
+    } else {
+      ceph_abort_msgf("invalid mirror entity type: %d",
+                      (int)update.entity.type);
+    }
   }
 }
 
@@ -331,16 +350,24 @@ void ImageMap<I>::notify_listener_remove_images(const std::string &mirror_uuid,
           << "remove=[" << remove << "]" << dendl;
 
   for (auto const &update : remove) {
-    m_listener.remove_image(
-      mirror_uuid, update.global_image_id, update.instance_id,
-      create_async_context_callback(
-        m_threads->work_queue,
-        new C_NotifyInstance(this, update.global_image_id, false)));
+    auto global_id = GlobalId(update.entity.type, update.entity.global_id);
+    if (update.entity.type == MIRROR_ENTITY_TYPE_IMAGE) {
+      m_listener.remove_image(
+        mirror_uuid, update.entity.global_id, update.instance_id,
+        create_async_context_callback(
+          m_threads->work_queue,
+          new C_NotifyInstance(this, global_id, false)));
+    } else if (update.entity.type == MIRROR_ENTITY_TYPE_GROUP) {
+      // TODO
+    } else {
+      ceph_abort_msgf("invalid mirror entity type: %d",
+                      (int)update.entity.type);
+    }
   }
 }
 
 template <typename I>
-void ImageMap<I>::handle_load(const std::map<std::string,
+void ImageMap<I>::handle_load(const std::map<GlobalId,
                               cls::rbd::MirrorImageMap> &image_mapping) {
   dout(20) << dendl;
 
@@ -356,16 +383,15 @@ void ImageMap<I>::handle_load(const std::map<std::string,
 }
 
 template <typename I>
-void ImageMap<I>::handle_peer_ack_remove(const std::string &global_image_id,
-                                         int r) {
+void ImageMap<I>::handle_peer_ack_remove(const GlobalId &global_id, int r) {
   std::lock_guard locker{m_lock};
-  dout(5) << "global_image_id=" << global_image_id << dendl;
+  dout(5) << "global_id=" << global_id << dendl;
 
   if (r < 0) {
-    derr << "failed to remove global_image_id=" << global_image_id << dendl;
+    derr << "failed to remove global_id=" << global_id << dendl;
   }
 
-  auto peer_it = m_peer_map.find(global_image_id);
+  auto peer_it = m_peer_map.find(global_id);
   if (peer_it == m_peer_map.end()) {
     return;
   }
@@ -376,16 +402,17 @@ void ImageMap<I>::handle_peer_ack_remove(const std::string &global_image_id,
 template <typename I>
 void ImageMap<I>::update_images_added(
     const std::string &mirror_uuid,
-    const std::set<std::string> &global_image_ids) {
+    const MirrorEntities &entities) {
   dout(5) << "mirror_uuid=" << mirror_uuid << ", "
-          << "global_image_ids=[" << global_image_ids << "]" << dendl;
+          << "entities=[" << entities << "]" << dendl;
   ceph_assert(ceph_mutex_is_locked(m_lock));
 
-  for (auto const &global_image_id : global_image_ids) {
-    auto result = m_peer_map[global_image_id].insert(mirror_uuid);
-    if (result.second && m_peer_map[global_image_id].size() == 1) {
-      if (m_policy->add_image(global_image_id)) {
-        schedule_action(global_image_id);
+  for (auto &entity : entities) {
+    auto global_id = GlobalId(entity.type, entity.global_id);
+    auto result = m_peer_map[global_id].insert(mirror_uuid);
+    if ((result.second || entity.type == MIRROR_ENTITY_TYPE_GROUP)) {
+      if (m_policy->add_entity(global_id, entity.count)) {
+        schedule_action(global_id);
       }
     }
   }
@@ -394,34 +421,35 @@ void ImageMap<I>::update_images_added(
 template <typename I>
 void ImageMap<I>::update_images_removed(
     const std::string &mirror_uuid,
-    const std::set<std::string> &global_image_ids) {
+    const MirrorEntities &entities) {
   dout(5) << "mirror_uuid=" << mirror_uuid << ", "
-          << "global_image_ids=[" << global_image_ids << "]" << dendl;
+          << "entities=[" << entities << "]" << dendl;
   ceph_assert(ceph_mutex_is_locked(m_lock));
 
   Updates to_remove;
-  for (auto const &global_image_id : global_image_ids) {
-    image_map::LookupInfo info = m_policy->lookup(global_image_id);
-    bool image_mapped = (info.instance_id != image_map::UNMAPPED_INSTANCE_ID);
+  for (auto &entity : entities) {
+    auto global_id = GlobalId(entity.type, entity.global_id);
+    image_map::LookupInfo info = m_policy->lookup(global_id);
+    bool entity_mapped = (info.instance_id != image_map::UNMAPPED_INSTANCE_ID);
 
-    bool image_removed = image_mapped;
+    bool entity_removed = entity_mapped;
     bool peer_removed = false;
-    auto peer_it = m_peer_map.find(global_image_id);
+    auto peer_it = m_peer_map.find(global_id);
     if (peer_it != m_peer_map.end()) {
       auto& peer_set = peer_it->second;
       peer_removed = peer_set.erase(mirror_uuid);
-      image_removed = peer_removed && peer_set.empty();
+      entity_removed = peer_removed && peer_set.empty();
     }
 
-    if (image_mapped && peer_removed && !mirror_uuid.empty()) {
+    if (entity_mapped && peer_removed && !mirror_uuid.empty()) {
       // peer image has been deleted
-      to_remove.emplace_back(global_image_id, info.instance_id);
+      to_remove.emplace_back(entity, info.instance_id);
     }
 
-    if (image_removed) {
+    if (entity_removed) {
       // local and peer images have been deleted
-      if (m_policy->remove_image(global_image_id)) {
-        schedule_action(global_image_id);
+      if (m_policy->remove_entity(global_id)) {
+        schedule_action(global_id);
       }
     }
   }
@@ -450,11 +478,11 @@ void ImageMap<I>::update_instances_added(
 
     dout(20) << "instance_ids=" << filtered_instance_ids << dendl;
 
-    std::set<std::string> remap_global_image_ids;
-    m_policy->add_instances(filtered_instance_ids, &remap_global_image_ids);
+    GlobalIds remap_global_ids;
+    m_policy->add_instances(filtered_instance_ids, &remap_global_ids);
 
-    for (auto const &global_image_id : remap_global_image_ids) {
-      schedule_action(global_image_id);
+    for (auto const &global_id : remap_global_ids) {
+      schedule_action(global_id);
     }
   }
 
@@ -478,11 +506,11 @@ void ImageMap<I>::update_instances_removed(
 
     dout(20) << "instance_ids=" << filtered_instance_ids << dendl;
 
-    std::set<std::string> remap_global_image_ids;
-    m_policy->remove_instances(filtered_instance_ids, &remap_global_image_ids);
+    GlobalIds remap_global_ids;
+    m_policy->remove_instances(filtered_instance_ids, &remap_global_ids);
 
-    for (auto const &global_image_id : remap_global_image_ids) {
-      schedule_action(global_image_id);
+    for (auto const &global_id : remap_global_ids) {
+      schedule_action(global_id);
     }
   }
 
@@ -491,11 +519,11 @@ void ImageMap<I>::update_instances_removed(
 
 template <typename I>
 void ImageMap<I>::update_images(const std::string &mirror_uuid,
-                                std::set<std::string> &&added_global_image_ids,
-                                std::set<std::string> &&removed_global_image_ids) {
+                                MirrorEntities &&added_entities,
+                                MirrorEntities &&removed_entities) {
   dout(5) << "mirror_uuid=" << mirror_uuid << ", " << "added_count="
-          << added_global_image_ids.size() << ", " << "removed_count="
-          << removed_global_image_ids.size() << dendl;
+          << added_entities.size() << ", " << "removed_count="
+          << removed_entities.size() << dendl;
 
   {
     std::lock_guard locker{m_lock};
@@ -503,11 +531,11 @@ void ImageMap<I>::update_images(const std::string &mirror_uuid,
       return;
     }
 
-    if (!removed_global_image_ids.empty()) {
-      update_images_removed(mirror_uuid, removed_global_image_ids);
+    if (!removed_entities.empty()) {
+      update_images_removed(mirror_uuid, removed_entities);
     }
-    if (!added_global_image_ids.empty()) {
-      update_images_added(mirror_uuid, added_global_image_ids);
+    if (!added_entities.empty()) {
+      update_images_added(mirror_uuid, added_entities);
     }
   }
 
@@ -515,11 +543,10 @@ void ImageMap<I>::update_images(const std::string &mirror_uuid,
 }
 
 template <typename I>
-void ImageMap<I>::handle_peer_ack(const std::string &global_image_id, int r) {
-  dout (20) << "global_image_id=" << global_image_id << ", r=" << r
-            << dendl;
+void ImageMap<I>::handle_peer_ack(const GlobalId &global_id, int r) {
+  dout (20) << "global_id=" << global_id << ", r=" << r << dendl;
 
-  continue_action({global_image_id}, r);
+  continue_action({global_id}, r);
 }
 
 template <typename I>
