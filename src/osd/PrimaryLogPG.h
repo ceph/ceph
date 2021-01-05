bdcedd4e9f2efed59b1e5c2d40171e6da595a259
myoungwon oh <ohmyoungwon@gmail.com>
2021-01-05 21:40:36 +0900

    osd: add has_manifest_chunk() to check chunks in snapshot
    cls_has_chunk does not cover snapshotted manifest object.
    This leads to unexpected behavior during chunk scrub.
    
    Signed-off-by: Myoungwon Oh <myoungwon.oh@samsung.com>

--- src/osd/PrimaryLogPG.h
+++ src/osd/PrimaryLogPG.h
@@ -1848,6 +1848,8 @@ public:
   void maybe_kick_recovery(const hobject_t &soid);
   void wait_for_unreadable_object(const hobject_t& oid, OpRequestRef op);
 
+  bool has_manifest_chunk(ObjectContextRef obc, std::string& fp_oid);
+
   bool check_laggy(OpRequestRef& op);
   bool check_laggy_requeue(OpRequestRef& op);
   void recheck_readable() override;
