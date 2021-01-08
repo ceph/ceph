sunilangadi2/ceph-ci

mgr/dashboard: CLI commands: read passwords from file
Fixes: https://tracker.ceph.com/issues/48355

Signed-off-by: Alfonso Martínez <almartin@redhat.com>
Signed-off-by: Juan Miguel Olmo Martínez <jolmomar@redhat.com>

src/pybind/mgr/dashboard/services/access_control.py
@@ -13,7 +13,7 @@ from datetime import datetime, timedelta
 from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 
 import bcrypt
-from mgr_module import CLIReadCommand, CLIWriteCommand
+from mgr_module import CLICheckNonemptyFileInput, CLIReadCommand, CLIWriteCommand
 
 from .. import mgr
 from ..exceptions import PasswordPolicyException, PermissionNotValid, \
@@ -561,10 +561,11 @@ def load_access_control_db():
 # CLI dashboard access control scope commands
 
 @CLIWriteCommand('dashboard set-login-credentials',
-                 'name=username,type=CephString '
-                 'name=password,type=CephString',
-                 'Set the login credentials')
-def set_login_credentials_cmd(_, username, password):
+                 'name=username,type=CephString',
+                 'Set the login credentials. Password read from -i <file>')
+@CLICheckNonemptyFileInput
+def set_login_credentials_cmd(_, username, inbuf):
+    password = inbuf
     try:
         user = mgr.ACCESS_CTRL_DB.get_user(username)
         user.set_password(password)
@@ -694,7 +695,6 @@ def ac_user_show_cmd(_, username=None):
 
 @CLIWriteCommand('dashboard ac-user-create',
                  'name=username,type=CephString '
-                 'name=password,type=CephString,req=false '
                  'name=rolename,type=CephString,req=false '
                  'name=name,type=CephString,req=false '
                  'name=email,type=CephString,req=false '
@@ -702,10 +702,12 @@ def ac_user_show_cmd(_, username=None):
                  'name=force_password,type=CephBool,req=false '
                  'name=pwd_expiration_date,type=CephInt,req=false '
                  'name=pwd_update_required,type=CephBool,req=false',
-                 'Create a user')
-def ac_user_create_cmd(_, username, password=None, rolename=None, name=None,
+                 'Create a user. Password read from -i <file>')
+@CLICheckNonemptyFileInput
+def ac_user_create_cmd(_, username, inbuf, rolename=None, name=None,
                        email=None, enabled=True, force_password=False,
                        pwd_expiration_date=None, pwd_update_required=False):
+    password = inbuf
     try:
         role = mgr.ACCESS_CTRL_DB.get_role(rolename) if rolename else None
     except RoleDoesNotExist as ex:
@@ -844,10 +846,11 @@ def ac_user_del_roles_cmd(_, username, roles):
 
 @CLIWriteCommand('dashboard ac-user-set-password',
                  'name=username,type=CephString '
-                 'name=password,type=CephString '
                  'name=force_password,type=CephBool,req=false',
-                 'Set user password')
-def ac_user_set_password(_, username, password, force_password=False):
+                 'Set user password from -i <file>')
+@CLICheckNonemptyFileInput
+def ac_user_set_password(_, username, inbuf, force_password=False):
+    password = inbuf
     try:
         user = mgr.ACCESS_CTRL_DB.get_user(username)
         if not force_password:
@@ -863,10 +866,11 @@ def ac_user_set_password(_, username, password, force_password=False):
 
 
 @CLIWriteCommand('dashboard ac-user-set-password-hash',
-                 'name=username,type=CephString '
-                 'name=hashed_password,type=CephString',
-                 'Set user password bcrypt hash')
-def ac_user_set_password_hash(_, username, hashed_password):
+                 'name=username,type=CephString',
+                 'Set user password bcrypt hash from -i <file>')
+@CLICheckNonemptyFileInput
+def ac_user_set_password_hash(_, username, inbuf):
+    hashed_password = inbuf
     try:
         # make sure the hashed_password is actually a bcrypt hash
         bcrypt.checkpw(b'', hashed_password.encode('utf-8'))

src/pybind/mgr/dashboard/tests/test_settings.py
@@ -4,6 +4,8 @@ from __future__ import absolute_import
 import errno
 import unittest
 
+from mgr_module import ERROR_MSG_EMPTY_INPUT_FILE
+
 from .. import settings
 from ..controllers.settings import Settings as SettingsController
 from ..settings import Settings, handle_option_command
@@ -43,7 +45,9 @@ class SettingsTest(unittest.TestCase, KVStoreMockMixin):
 
     def test_get_cmd(self):
         r, out, err = handle_option_command(
-            {'prefix': 'dashboard get-grafana-api-port'})
+            {'prefix': 'dashboard get-grafana-api-port'},
+            None
+        )
         self.assertEqual(r, 0)
         self.assertEqual(out, '3000')
         self.assertEqual(err, '')
@@ -51,14 +55,35 @@ class SettingsTest(unittest.TestCase, KVStoreMockMixin):
     def test_set_cmd(self):
         r, out, err = handle_option_command(
             {'prefix': 'dashboard set-grafana-api-port',
-             'value': '4000'})
+             'value': '4000'},
+            None
+        )
         self.assertEqual(r, 0)
         self.assertEqual(out, 'Option GRAFANA_API_PORT updated')
         self.assertEqual(err, '')
 
+    def test_set_secret_empty(self):
+        r, out, err = handle_option_command(
+            {'prefix': 'dashboard set-grafana-api-password'},
+            None
+        )
+        self.assertEqual(r, -errno.EINVAL)
+        self.assertEqual(out, '')
+        self.assertEqual(err, ERROR_MSG_EMPTY_INPUT_FILE)
+
+    def test_set_secret(self):
+        r, out, err = handle_option_command(
+            {'prefix': 'dashboard set-grafana-api-password'},
+            'my-secret'
+        )
+        self.assertEqual(r, 0)
+        self.assertEqual(out, 'Option GRAFANA_API_PASSWORD updated')
+        self.assertEqual(err, '')
+
     def test_reset_cmd(self):
         r, out, err = handle_option_command(
-            {'prefix': 'dashboard reset-grafana-enabled'}
+            {'prefix': 'dashboard reset-grafana-enabled'},
+            None
         )
         self.assertEqual(r, 0)
         self.assertEqual(out, 'Option {} reset to default value "{}"'.format(
@@ -67,7 +92,9 @@ class SettingsTest(unittest.TestCase, KVStoreMockMixin):
 
     def test_inv_cmd(self):
         r, out, err = handle_option_command(
-            {'prefix': 'dashboard get-non-existent-option'})
+            {'prefix': 'dashboard get-non-existent-option'},
+            None
+        )
         self.assertEqual(r, -errno.ENOSYS)
         self.assertEqual(out, '')
         self.assertEqual(err, "Command not found "
@@ -76,13 +103,17 @@ class SettingsTest(unittest.TestCase, KVStoreMockMixin):
     def test_sync(self):
         Settings.GRAFANA_API_PORT = 5000
         r, out, err = handle_option_command(
-            {'prefix': 'dashboard get-grafana-api-port'})
+            {'prefix': 'dashboard get-grafana-api-port'},
+            None
+        )
         self.assertEqual(r, 0)
         self.assertEqual(out, '5000')
         self.assertEqual(err, '')
         r, out, err = handle_option_command(
             {'prefix': 'dashboard set-grafana-api-host',
-             'value': 'new-local-host'})
+             'value': 'new-local-host'},
+            None
+        )
         self.assertEqual(r, 0)
         self.assertEqual(out, 'Option GRAFANA_API_HOST updated')
         self.assertEqual(err, '')

ceph/ceph

(cherry picked from commit 5d7ee7c1f0ad971fd0079f917e2b44cdef1d6f9f)
 Conflicts:
	doc/mgr/dashboard.rst
	qa/tasks/mgr/dashboard/helper.py
	qa/tasks/mgr/dashboard/test_auth.py
	qa/tasks/mgr/dashboard/test_rgw.py
	qa/tasks/mgr/dashboard/test_user.py
	qa/workunits/cephadm/test_dashboard_e2e.sh
	src/cephadm/cephadm
	src/pybind/mgr/cephadm/services/cephadmservice.py
	src/pybind/mgr/cephadm/services/iscsi.py
	src/pybind/mgr/cephadm/tests/test_cephadm.py
	src/pybind/mgr/cephadm/tests/test_services.py
	src/pybind/mgr/dashboard/run-frontend-e2e-tests.sh
	src/pybind/mgr/dashboard/services/access_control.py
	src/pybind/mgr/dashboard/services/iscsi_cli.py
	src/pybind/mgr/dashboard/tests/test_access_control.py
	src/pybind/mgr/dashboard/tests/test_iscsi.py
	src/pybind/mgr/dashboard/tests/test_settings.py
	src/pybind/mgr/mgr_module.py
	src/pybind/mgr/tests/__init__.py
	src/test/mgr/mgr-dashboard-smoke.sh
	src/vstart.sh
  - Remove cephadm files and related code (does not apply to nautilus).
  - Remove code related to non-existing functionality in nautilus.
  - Adapt code to be py2 compatible.
  - Resolve conflicts related to code divergence.

src/pybind/mgr/dashboard/services/access_control.py
@@ -10,7 +10,7 @@ import time
 
 import bcrypt
 
-from mgr_module import CLIReadCommand, CLIWriteCommand
+from mgr_module import CLICheckNonemptyFileInput, CLIReadCommand, CLIWriteCommand
 
 from .. import mgr, logger
 from ..security import Scope, Permission
@@ -367,10 +367,11 @@ def load_access_control_db():
 # CLI dashboard access control scope commands
 
 @CLIWriteCommand('dashboard set-login-credentials',
-                 'name=username,type=CephString '
-                 'name=password,type=CephString',
-                 'Set the login credentials')
-def set_login_credentials_cmd(_, username, password):
+                 'name=username,type=CephString',
+                 'Set the login credentials. Password read from -i <file>')
+@CLICheckNonemptyFileInput
+def set_login_credentials_cmd(_, username, inbuf):
+    password = inbuf
     try:
         user = mgr.ACCESS_CTRL_DB.get_user(username)
         user.set_password(password)
@@ -500,13 +501,14 @@ def ac_user_show_cmd(_, username=None):
 
 @CLIWriteCommand('dashboard ac-user-create',
                  'name=username,type=CephString '
-                 'name=password,type=CephString,req=false '
                  'name=rolename,type=CephString,req=false '
                  'name=name,type=CephString,req=false '
                  'name=email,type=CephString,req=false',
-                 'Create a user')
-def ac_user_create_cmd(_, username, password=None, rolename=None, name=None,
+                 'Create a user. Password read from -i <file>')
+@CLICheckNonemptyFileInput
+def ac_user_create_cmd(_, username, inbuf, rolename=None, name=None,
                        email=None):
+    password = inbuf
     try:
         role = mgr.ACCESS_CTRL_DB.get_role(rolename) if rolename else None
     except RoleDoesNotExist as ex:
@@ -609,10 +611,11 @@ def ac_user_del_roles_cmd(_, username, roles):
 
 
 @CLIWriteCommand('dashboard ac-user-set-password',
-                 'name=username,type=CephString '
-                 'name=password,type=CephString',
-                 'Set user password')
-def ac_user_set_password(_, username, password):
+                 'name=username,type=CephString',
+                 'Set user password from -i <file>')
+@CLICheckNonemptyFileInput
+def ac_user_set_password(_, username, inbuf):
+    password = inbuf
     try:
         user = mgr.ACCESS_CTRL_DB.get_user(username)
         user.set_password(password)

src/pybind/mgr/dashboard/tests/test_settings.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import
 
 import errno
 import unittest
+
+from mgr_module import ERROR_MSG_EMPTY_INPUT_FILE
+
 from . import KVStoreMockMixin, ControllerTestCase
 from .. import settings
 from ..controllers.settings import Settings as SettingsController
@@ -42,7 +45,9 @@ class SettingsTest(unittest.TestCase, KVStoreMockMixin):
 
     def test_get_cmd(self):
         r, out, err = handle_option_command(
-            {'prefix': 'dashboard get-grafana-api-port'})
+            {'prefix': 'dashboard get-grafana-api-port'},
+            None
+        )
         self.assertEqual(r, 0)
         self.assertEqual(out, '3000')
         self.assertEqual(err, '')
@@ -50,14 +55,35 @@ class SettingsTest(unittest.TestCase, KVStoreMockMixin):
     def test_set_cmd(self):
         r, out, err = handle_option_command(
             {'prefix': 'dashboard set-grafana-api-port',
-             'value': '4000'})
+             'value': '4000'},
+            None
+        )
         self.assertEqual(r, 0)
         self.assertEqual(out, 'Option GRAFANA_API_PORT updated')
         self.assertEqual(err, '')
 
+    def test_set_secret_empty(self):
+        r, out, err = handle_option_command(
+            {'prefix': 'dashboard set-rgw-api-secret-key'},
+            None
+        )
+        self.assertEqual(r, -errno.EINVAL)
+        self.assertEqual(out, '')
+        self.assertEqual(err, ERROR_MSG_EMPTY_INPUT_FILE)
+
+    def test_set_secret(self):
+        r, out, err = handle_option_command(
+            {'prefix': 'dashboard set-rgw-api-secret-key'},
+            'my-secret'
+        )
+        self.assertEqual(r, 0)
+        self.assertEqual(out, 'Option RGW_API_SECRET_KEY updated')
+        self.assertEqual(err, '')
+
     def test_reset_cmd(self):
         r, out, err = handle_option_command(
-            {'prefix': 'dashboard reset-grafana-enabled'}
+            {'prefix': 'dashboard reset-grafana-enabled'},
+            None
         )
         self.assertEqual(r, 0)
         self.assertEqual(out, 'Option {} reset to default value "{}"'.format(
@@ -66,7 +92,9 @@ class SettingsTest(unittest.TestCase, KVStoreMockMixin):
 
     def test_inv_cmd(self):
         r, out, err = handle_option_command(
-            {'prefix': 'dashboard get-non-existent-option'})
+            {'prefix': 'dashboard get-non-existent-option'},
+            None
+        )
         self.assertEqual(r, -errno.ENOSYS)
         self.assertEqual(out, '')
         self.assertEqual(err, "Command not found "
@@ -75,13 +103,17 @@ class SettingsTest(unittest.TestCase, KVStoreMockMixin):
     def test_sync(self):
         Settings.GRAFANA_API_PORT = 5000
         r, out, err = handle_option_command(
-            {'prefix': 'dashboard get-grafana-api-port'})
+            {'prefix': 'dashboard get-grafana-api-port'},
+            None
+        )
         self.assertEqual(r, 0)
         self.assertEqual(out, '5000')
         self.assertEqual(err, '')
         r, out, err = handle_option_command(
             {'prefix': 'dashboard set-grafana-api-host',
-             'value': 'new-local-host'})
+             'value': 'new-local-host'},
+            None
+        )
         self.assertEqual(r, 0)
         self.assertEqual(out, 'Option GRAFANA_API_HOST updated')
         self.assertEqual(err, '')