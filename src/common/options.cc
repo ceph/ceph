be38fd3bcc9dcfe5b528d389deaeba694757f682
Mahati Chamarthy <mahati.chamarthy@intel.com>
2021-01-12 13:38:02 +0530

    librbd/cache/pwl: Rename config options
    Signed-off-by: Mahati Chamarthy <mahati.chamarthy@intel.com>

--- src/common/options.cc
+++ src/common/options.cc
@@ -7734,16 +7734,16 @@ static std::vector<Option> get_rbd_options() {
     .set_enum_allowed({"disabled", "rwl", "ssd"})
     .set_description("enable persistent write back cache for this volume"),
 
-    Option("rbd_rwl_log_periodic_stats", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
+    Option("rbd_persistent_cache_log_periodic_stats", Option::TYPE_BOOL, Option::LEVEL_ADVANCED)
     .set_default(false)
     .set_description("emit periodic perf stats to debug log"),
 
-    Option("rbd_rwl_size", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
+    Option("rbd_persistent_cache_size", Option::TYPE_UINT, Option::LEVEL_ADVANCED)
     .set_default(1073741824)
     .set_min(1073741824)
     .set_description("size of the persistent write back cache for this volume"),
 
-    Option("rbd_rwl_path", Option::TYPE_STR, Option::LEVEL_ADVANCED)
+    Option("rbd_persistent_cache_path", Option::TYPE_STR, Option::LEVEL_ADVANCED)
     .set_default("/tmp")
     .set_description("location of the persistent write back cache in a DAX-enabled filesystem on persistent memory"),
 
