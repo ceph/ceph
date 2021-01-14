ceph/ceph

commit 96a2613471f2649038c264923c5acf99ee44c3bb

pybind/mgr/cephadm: limit rgw osd caps
Using tagged pools ensures RGW only can access pools used for RGW.

Fixes: https://tracker.ceph.com/issues/48594
Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

src/pybind/mgr/cephadm/services/cephadmservice.py
@@ -614,7 +614,7 @@ class RgwService(CephService):
             'entity': self.get_auth_entity(rgw_id),
             'caps': ['mon', 'allow *',
                      'mgr', 'allow rw',
-                     'osd', 'allow rwx'],
+                     'osd', 'allow rwx tag rgw'],
         })
         return keyring
 

commit 8802223d543a0d2033f3244f33aa433a7e6e223f

cephadm: fix rgw osd cap tag
The syntax is "allow rwx tag rgw *=*'.

Sorry, I thought this would have gotten caught in testing :(

Fixes: 96a2613471f2649038c264923c5acf99ee44c3bb
Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

src/pybind/mgr/cephadm/services/cephadmservice.py
@@ -617,7 +617,7 @@ class RgwService(CephService):
             'entity': self.get_auth_entity(rgw_id),
             'caps': ['mon', 'allow *',
                      'mgr', 'allow rw',
-                     'osd', 'allow rwx tag rgw'],
+                     'osd', 'allow rwx tag rgw *=*'],
         })
         return keyring
 