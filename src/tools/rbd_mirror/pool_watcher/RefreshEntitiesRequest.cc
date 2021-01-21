c7b5ba7a77f304ee01c1edeee86f5fc5dd925bfd
Mykola Golub <mgolub@suse.com>
2021-01-21 09:18:36 +0000

    rbd-mirror: make pool watcher also refresh groups
    Signed-off-by: Mykola Golub <mgolub@suse.com>
    Signed-off-by: Prasanna Kumar Kalever <prasanna.kalever@redhat.com>

--- src/tools/rbd_mirror/pool_watcher/RefreshEntitiesRequest.cc
+++ src/tools/rbd_mirror/pool_watcher/RefreshEntitiesRequest.cc
@@ -71,7 +71,111 @@ void RefreshEntitiesRequest<I>::handle_mirror_image_list(int r) {
     return;
   }
 
-  finish(0);
+  m_start_after = {};
+  mirror_group_list();
+}
+
+template <typename I>
+void RefreshEntitiesRequest<I>::mirror_group_list() {
+  dout(10) << dendl;
+
+  librados::ObjectReadOperation op;
+  librbd::cls_client::mirror_group_list_start(&op, m_start_after, MAX_RETURN);
+
+  m_out_bl.clear();
+  librados::AioCompletion *aio_comp = create_rados_callback<
+    RefreshEntitiesRequest<I>,
+    &RefreshEntitiesRequest<I>::handle_mirror_group_list>(this);
+  int r = m_remote_io_ctx.aio_operate(RBD_MIRRORING, aio_comp, &op, &m_out_bl);
+  ceph_assert(r == 0);
+  aio_comp->release();
+}
+
+template <typename I>
+void RefreshEntitiesRequest<I>::handle_mirror_group_list(int r) {
+  dout(10) << "r=" << r << dendl;
+
+  std::map<std::string, cls::rbd::MirrorGroup> groups;
+  if (r == 0) {
+    auto it = m_out_bl.cbegin();
+    r = librbd::cls_client::mirror_group_list_finish(&it, &groups);
+  }
+
+  if (r < 0 && r != -ENOENT) {
+    derr << "failed to list mirrored groups: " << cpp_strerror(r) << dendl;
+    finish(r);
+    return;
+  }
+
+  m_groups.insert(groups.begin(), groups.end());
+
+  if (groups.size() == MAX_RETURN) {
+    m_start_after = groups.rbegin()->first;
+    mirror_group_list();
+    return;
+  }
+
+  // XXXMG: should we just provide group_size field in cls::rbd::MirrorGroup
+  // instead of listing group images just to learn their count?
+  group_image_list();
+}
+
+template <typename I>
+void RefreshEntitiesRequest<I>::group_image_list() {
+  if (m_groups.empty()) {
+    finish(0);
+    return;
+  }
+
+  auto &group_id = m_groups.begin()->first;
+
+  dout(10) << group_id << dendl;
+
+  librados::ObjectReadOperation op;
+  librbd::cls_client::group_image_list_start(
+      &op, m_start_group_image_list_after, MAX_RETURN);
+  m_out_bl.clear();
+  librados::AioCompletion *aio_comp = create_rados_callback<
+    RefreshEntitiesRequest<I>,
+    &RefreshEntitiesRequest<I>::handle_group_image_list>(this);
+  int r = m_remote_io_ctx.aio_operate(librbd::util::group_header_name(group_id),
+                                      aio_comp, &op, &m_out_bl);
+  ceph_assert(r == 0);
+  aio_comp->release();
+}
+
+template <typename I>
+void RefreshEntitiesRequest<I>::handle_group_image_list(int r) {
+  dout(10) << "r=" << r << dendl;
+
+  std::vector<cls::rbd::GroupImageStatus> images;
+  if (r == 0) {
+    auto iter = m_out_bl.cbegin();
+    r = librbd::cls_client::group_image_list_finish(&iter, &images);
+  }
+
+  if (r < 0) {
+    derr << "error listing remote group: " << cpp_strerror(r) << dendl;
+    finish(r);
+    return;
+  }
+
+  auto image_count = images.size();
+  m_group_size += image_count;
+  if (image_count == MAX_RETURN) {
+    m_start_group_image_list_after = images.rbegin()->spec;
+  } else {
+    auto group_it = m_groups.begin();
+    auto &global_group_id = group_it->second.global_group_id;
+    m_entities->insert(
+      {{MIRROR_ENTITY_TYPE_GROUP, global_group_id, m_group_size},
+       group_it->first});
+    m_groups.erase(group_it);
+    m_start_group_image_list_after = {};
+    m_group_size = 0;
+  }
+
+  group_image_list();
 }
 
 template <typename I>
