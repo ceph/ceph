2c1886b72395b648e664ed9e678eda576c90446b
Casey Bodley <cbodley@redhat.com>
2021-01-22 18:28:50 -0500

    rgw: add generations to error repo with binary format
    adds a backward-compatible binary encoding for error repo keys that can
    contain a generation number along with the bucket and shard
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_sync_error_repo.h
+++ src/rgw/rgw_sync_error_repo.h
@@ -14,6 +14,7 @@
 
 #pragma once
 
+#include <optional>
 #include "include/rados/librados_fwd.hpp"
 #include "include/buffer_fwd.h"
 #include "common/ceph_time.h"
@@ -21,25 +22,38 @@
 class RGWSI_RADOS;
 class RGWCoroutine;
 struct rgw_raw_obj;
+struct rgw_bucket_shard;
+
+namespace rgw::error_repo {
+
+// binary-encode a bucket/shard/gen and return it as a string
+std::string encode_key(const rgw_bucket_shard& bs,
+                       std::optional<uint64_t> gen);
+
+// try to decode a key. returns -EINVAL if not in binary format
+int decode_key(std::string encoded,
+               rgw_bucket_shard& bs,
+               std::optional<uint64_t>& gen);
 
 // decode a timestamp as a uint64_t for CMPXATTR_MODE_U64
-ceph::real_time rgw_error_repo_decode_value(const ceph::bufferlist& bl);
+ceph::real_time decode_value(const ceph::bufferlist& bl);
 
 // write an omap key iff the given timestamp is newer
-int rgw_error_repo_write(librados::ObjectWriteOperation& op,
-                         const std::string& key,
-                         ceph::real_time timestamp);
-RGWCoroutine* rgw_error_repo_write_cr(RGWSI_RADOS* rados,
-                                      const rgw_raw_obj& obj,
-                                      const std::string& key,
-                                      ceph::real_time timestamp);
+int write(librados::ObjectWriteOperation& op,
+          const std::string& key,
+          ceph::real_time timestamp);
+RGWCoroutine* write_cr(RGWSI_RADOS* rados,
+                       const rgw_raw_obj& obj,
+                       const std::string& key,
+                       ceph::real_time timestamp);
 
 // remove an omap key iff there isn't a newer timestamp
-int rgw_error_repo_remove(librados::ObjectWriteOperation& op,
-                          const std::string& key,
-                          ceph::real_time timestamp);
-RGWCoroutine* rgw_error_repo_remove_cr(RGWSI_RADOS* rados,
-                                       const rgw_raw_obj& obj,
-                                       const std::string& key,
-                                       ceph::real_time timestamp);
+int remove(librados::ObjectWriteOperation& op,
+           const std::string& key,
+           ceph::real_time timestamp);
+RGWCoroutine* remove_cr(RGWSI_RADOS* rados,
+                        const rgw_raw_obj& obj,
+                        const std::string& key,
+                        ceph::real_time timestamp);
 
+} // namespace rgw::error_repo
