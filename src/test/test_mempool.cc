e4e07de8f36d0d44b341d5784a8bf56192201b95
Adam Kupczyk <akupczyk@redhat.com>
2021-01-25 11:33:28 +0100

    common/mempool: Add test for mempool shards
    Add test that checks quality of mempool sharing.
    It refuses to accept cases when variance is too large.
    
    Signed-off-by: Adam Kupczyk <akupczyk@redhat.com>

--- src/test/test_mempool.cc
+++ src/test/test_mempool.cc
@@ -402,6 +402,33 @@ TEST(mempool, btree_map_test)
   ASSERT_EQ(0, mempool::osd::allocated_bytes());
 }
 
+TEST(mempool, check_shard_select)
+{
+  const size_t samples = 100;
+  std::atomic_int shards[mempool::num_shards] = {0};
+  std::vector<std::thread> workers;
+  for (size_t i = 0; i < samples; i++) {
+    workers.push_back(
+      std::thread([&](){
+          size_t i = mempool::pool_t::pick_a_shard_int();
+          shards[i]++;
+        }));
+  }
+  for (auto& t:workers) {
+    t.join();
+  }
+  workers.clear();
+
+  double EX = (double)samples / (double)mempool::num_shards;
+  double VarX = 0;
+  for (size_t i = 0; i < mempool::num_shards; i++) {
+    VarX += (EX - shards[i]) * (EX - shards[i]);
+  }
+  //random gives VarX below 200
+  //when half slots are 0, we get ~300
+  //when all samples go into one slot, we get ~9000
+  EXPECT_LT(VarX, 200);
+}
 
 
 int main(int argc, char **argv)
