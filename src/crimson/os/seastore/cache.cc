e73181fbecfcd71abe246e9f11152996b1bd12ab
Samuel Just <sjust@redhat.com>
2021-01-28 23:57:59 +0000

    crimson/os/seastore/onode_manager: remove simple-fltree
    Doesn't actually have a TransactionManager implementation, and
    it needs substantial changes to adapt to changes in Onode.
    We can resurrect it in the future if we want to.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/os/seastore/cache.cc
+++ src/crimson/os/seastore/cache.cc
@@ -9,7 +9,7 @@
 #include "crimson/os/seastore/extentmap_manager/btree/extentmap_btree_node_impl.h"
 #include "crimson/os/seastore/lba_manager/btree/lba_btree_node_impl.h"
 #include "crimson/os/seastore/omap_manager/btree/omap_btree_node_impl.h"
-#include "crimson/os/seastore/onode_manager/simple-fltree/onode_block.h"
+#include "crimson/os/seastore/collection_manager/collection_flat_node.h"
 #include "crimson/os/seastore/onode_manager/staged-fltree/node_extent_manager/seastore.h"
 #include "test/crimson/seastore/test_block.h"
 
@@ -141,8 +141,6 @@ CachedExtentRef Cache::alloc_new_extent_by_type(
     return alloc_new_extent<lba_manager::btree::LBAInternalNode>(t, length);
   case extent_types_t::LADDR_LEAF:
     return alloc_new_extent<lba_manager::btree::LBALeafNode>(t, length);
-  case extent_types_t::ONODE_BLOCK:
-    return alloc_new_extent<OnodeBlock>(t, length);
   case extent_types_t::ONODE_BLOCK_STAGED:
     return alloc_new_extent<onode::SeastoreNodeExtent>(t, length);
   case extent_types_t::EXTMAP_INNER:
@@ -546,11 +544,6 @@ Cache::get_extent_ertr::future<CachedExtentRef> Cache::get_extent_by_type(
       return get_extent<collection_manager::CollectionNode>(offset, length
       ).safe_then([](auto extent) {
         return CachedExtentRef(extent.detach(), false /* add_ref */);
-      });
-    case extent_types_t::ONODE_BLOCK:
-      return get_extent<OnodeBlock>(offset, length
-      ).safe_then([](auto extent) {
-	return CachedExtentRef(extent.detach(), false /* add_ref */);
       });
     case extent_types_t::ONODE_BLOCK_STAGED:
       return get_extent<onode::SeastoreNodeExtent>(offset, length
