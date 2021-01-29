6bfe15044f3ceace662135394cf2e17ec9a6af2f
Kefu Chai <kchai@redhat.com>
2021-01-29 10:31:04 +0800

    pybind/mgr/iostat: add typing annotations
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/pybind/mgr/iostat/module.py
+++ src/pybind/mgr/iostat/module.py
@@ -1,11 +1,13 @@
+from typing import Any
+
 from mgr_module import CLIReadCommand, HandleCommandResult, MgrModule
 
 
 class Module(MgrModule):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         super().__init__(*args, **kwargs)
 
-    def self_test(self):
+    def self_test(self) -> None:
         r = self.get('io_rate')
         assert('pg_stats_delta' in r)
         assert('stamp_delta' in r['pg_stats_delta'])
@@ -16,7 +18,7 @@ class Module(MgrModule):
         assert('num_read' in r['pg_stats_delta']['stat_sum'])
 
     @CLIReadCommand('iostat', poll=True)
-    def iostat(self, width: int = 80, print_header: bool = False):
+    def iostat(self, width: int = 80, print_header: bool = False) -> HandleCommandResult:
         """
         Get IO rates
         """
