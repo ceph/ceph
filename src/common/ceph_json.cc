be51f8a1a0507f903c57bb21640ff8e834698bcf
Casey Bodley <cbodley@redhat.com>
2021-02-01 14:34:54 -0500

    json: add encode_json() overload for string_view
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/common/ceph_json.cc
+++ src/common/ceph_json.cc
@@ -519,6 +519,11 @@ void decode_json_obj(ceph_dir_layout& i, JSONObj *obj){
     i.dl_unused3 = tmp;
 }
 
+void encode_json(const char *name, std::string_view val, Formatter *f)
+{
+  f->dump_string(name, val);
+}
+
 void encode_json(const char *name, const string& val, Formatter *f)
 {
   f->dump_string(name, val);
