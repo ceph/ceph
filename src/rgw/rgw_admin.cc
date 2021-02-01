96e4289b7fa54d420cc555d3d081526fa4918447
Casey Bodley <cbodley@redhat.com>
2021-02-01 12:02:44 -0500

    radosgw-admin: try reshard even if bucket is resharding
    allow reshard in case a previous reshard failed. if the reshard is
    actually still in progress, we'll fail to get the reshard lock
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -2928,13 +2928,6 @@ int check_reshard_bucket_params(rgw::sal::RadosStore* store,
     return ret;
   }
 
-  if ((*bucket)->get_info().layout.resharding != rgw::BucketReshardState::None) {
-    // if in_progress or done then we have an old BucketInfo
-    cerr << "ERROR: the bucket is currently undergoing resharding and "
-      "cannot be added to the reshard list at this time" << std::endl;
-    return -EBUSY;
-  }
-
   int num_source_shards = rgw::current_num_shards((*bucket)->get_info().layout);
 
   if (num_shards <= num_source_shards && !yes_i_really_mean_it) {
