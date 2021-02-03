c53d9f6bff018f6eabb0ac69793ca15322824815
Aashish Sharma <aashishsharma@localhost.localdomain>
2021-02-03 12:53:56 +0530

    mgr/dashboard:test prometheus rules through promtool
    This PR intends to add unit testing for prometheus rules using promtool. To run the tests run 'run-promtool-unittests.sh' file.
    
    Fixes: https://tracker.ceph.com/issues/45415
    Signed-off-by: Aashish Sharma <aasharma@redhat.com>

--- install-deps.sh
+++ install-deps.sh
@@ -296,6 +296,9 @@ else
     case "$ID" in
     debian|ubuntu|devuan|elementary)
         echo "Using apt-get to install dependencies"
+        $SUDO apt install -y docker.io
+        $SUDO systemctl start docker
+        $SUDO systemctl enable docker
         $SUDO apt-get install -y devscripts equivs
         $SUDO apt-get install -y dpkg-dev
         ensure_python3_sphinx_on_ubuntu
@@ -336,6 +339,9 @@ else
         case "$ID" in
             fedora)
                 $SUDO dnf install -y dnf-utils
+                $SUDO dnf install -y docker-ce docker-ce-cli containerd.io
+                $SUDO systemctl start docker
+                $SUDO systemctl enable docker
                 ;;
             centos|rhel|ol|virtuozzo)
                 MAJOR_VERSION="$(echo $VERSION_ID | cut -d. -f1)"
@@ -430,6 +436,7 @@ function preload_wheels_for_tox() {
         mv $wip_wheelhouse wheelhouse
         md5sum $require_files $constraint_files > $md5
     fi
+
     popd > /dev/null
 }
 
