4fdd451edd71713b9aaa1d571bc6bbd977fc79c2
chunmei-liu <chunmei.liu@intel.com>
2021-02-05 18:13:37 -0800

    crimson/seastore: add collection manager implementation
    Signed-off-by: chunmei-liu <chunmei.liu@intel.com>
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/os/seastore/seastore_types.cc
+++ src/crimson/os/seastore/seastore_types.cc
@@ -63,6 +63,8 @@ std::ostream &operator<<(std::ostream &out, extent_types_t t)
     return out << "OMAP_INNER";
   case extent_types_t::OMAP_LEAF:
     return out << "OMAP_LEAF";
+  case extent_types_t::COLL_BLOCK:
+    return out << "COLL_BLOCK";
   case extent_types_t::TEST_BLOCK:
     return out << "TEST_BLOCK";
   case extent_types_t::TEST_BLOCK_PHYSICAL:
