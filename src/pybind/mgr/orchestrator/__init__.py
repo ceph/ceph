29e4e6c1bf88c9ec805961b23b8d54ef2eb4957c
Sebastian Wagner <sebastian.wagner@suse.com>
2021-02-08 01:01:08 +0100

    mgr/orch: replace Completion with OrchResult(Generic[T])
    Greatly simplify the orchestrator interface
    
    Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>
    (cherry picked from commit fcf75136634aa7750a242c60b81b86619713fbbe)

--- src/pybind/mgr/orchestrator/__init__.py
+++ src/pybind/mgr/orchestrator/__init__.py
@@ -4,7 +4,7 @@ from .module import OrchestratorCli
 
 # usage: E.g. `from orchestrator import StatelessServiceSpec`
 from ._interface import \
-    Completion, TrivialReadCompletion, raise_if_exception, ProgressReference, pretty_print, _Promise, \
+    OrchResult, raise_if_exception, \
     CLICommand, _cli_write_command, _cli_read_command, CLICommandMeta, \
     Orchestrator, OrchestratorClientMixin, \
     OrchestratorValidationError, OrchestratorError, NoOrchestrator, \
