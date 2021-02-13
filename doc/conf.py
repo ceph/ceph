3922bbeef461f28beeac5f9ab17ce2bdadc7b047
Kefu Chai <kchai@redhat.com>
2021-02-13 12:57:19 +0800

    doc/_theme: customize sphinx_rtd_theme
    * move the breadcrumbs to the top
    * add border around admonition elements
    * use different colors and fonts for section headers
    * add decoration lines at the bottom of breadcrumbs
    * remove left and right borders in tables
    * override the injected versions, the name of theme
      is different from "sphinx_rtd_theme", but the
      versions element is still displayed at the
      bottom-left corner as "versions.html" defines.
      without overriding .rst-badge CSS styling,
      readthedocs puts the injected versions at
      the default bottom-right corner, see
      https://github.com/readthedocs/readthedocs.org/blob/2a519f1146142d18f6a63b61c2f08984067280e0/readthedocs/api/v2/templates/restapi/footer.html
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- doc/conf.py
+++ doc/conf.py
@@ -50,7 +50,7 @@ html_theme_options = {
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
     'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
+    'style_nav_header_background': '#eee',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
