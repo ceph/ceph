275c6bd1b7884c3a82ec210d77d27dd608fc392c
Kefu Chai <kchai@redhat.com>
2021-02-13 11:23:23 +0800

    doc: switch to sphinx_rtd_theme
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- doc/conf.py
+++ doc/conf.py
@@ -44,6 +44,20 @@ pygments_style = 'sphinx'
 
 # HTML output options
 html_theme = 'ceph'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 html_theme_path = ['_themes']
 html_title = "Ceph Documentation"
 html_logo = 'logo.png'
