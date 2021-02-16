c10586f79901ce09473a56d973252f40d9d43741
Sage Weil <sage@newdream.net>
2021-02-16 16:50:18 -0500

    mon/ConfigMap: fix stray option leak
    The const Option* needs to remain alive only until the next clear().  Keep
    the reference in ConfigMap and clean it up then.
    
    Signed-off-by: Sage Weil <sage@newdream.net>
    (cherry picked from commit 9397a46aec88e287d56a6286ed4319f65d9c1f31)
    
    Fixes: https://tracker.ceph.com/issues/48381
    Conflicts:
    	src/mon/ConfigMap.h: trivial resolution

--- src/mon/ConfigMap.h
+++ src/mon/ConfigMap.h
@@ -99,6 +99,7 @@ struct ConfigMap {
   Section global;
   std::map<std::string,Section> by_type;
   std::map<std::string,Section> by_id;
+  std::list<std::unique_ptr<Option>> stray_options;
 
   Section *find_section(const std::string& name) {
     if (name == "global") {
@@ -118,6 +119,7 @@ struct ConfigMap {
     global.clear();
     by_type.clear();
     by_id.clear();
+    stray_options.clear();
   }
   void dump(Formatter *f) const;
   void generate_entity_map(
