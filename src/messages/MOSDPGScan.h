8d52d48adf01710ff125d9399289e9b000e6de01
Kefu Chai <kchai@redhat.com>
2021-02-19 01:06:50 +0800

    messages: drop compatibility for pre-octopus versions
    no need to be compatible with pre-octopus versions when it comes to
    in-cluster communications.
    
    * MMgrBeacon: octopus encodes MMgrBeacon with version 8
    * MMonJoin: octopus encodes MMonJoin with version 2
    * MOSDBoot: octopus encodes MOSDBoot with version 7
    * MOSDFailure: octopus encodes MOSDFailure with version 4
    * MOSDMarkMeDown: octopus encodes MOSDMarkMeDown with version 3
    * MOSDPGLog: octopus encodes MOSDPGLog with version 6
    * MOSDPGScan: octopus encodes MOSDPGScan with version 2
    * MOSDPGInfo: octopus encodes MOSDPGInfo with version 6
    * MOSDPGNotify: octopus encodes MOSDPGNotify with version 7
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/messages/MOSDPGScan.h
+++ src/messages/MOSDPGScan.h
@@ -75,12 +75,8 @@ public:
     using ceph::encode;
     encode(op, payload);
     encode(map_epoch, payload);
-    if (!HAVE_FEATURE(features, SERVER_NAUTILUS)) {
-      // pre-nautilus OSDs do not set last_peering_reset properly
-      encode(map_epoch, payload);
-    } else {
-      encode(query_epoch, payload);
-    }
+    assert(HAVE_FEATURE(features, SERVER_NAUTILUS));
+    encode(query_epoch, payload);
     encode(pgid.pgid, payload);
     encode(begin, payload);
     encode(end, payload);
