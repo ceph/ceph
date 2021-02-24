892bfa097eaf3e1e83cb3fa2ab4b0161bf987851
Kefu Chai <kchai@redhat.com>
2021-02-24 12:33:33 +0800

    async/PosixStack: do not reference worker id in ctor
    as it's not needed and confusing.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/msg/async/PosixStack.h
+++ src/msg/async/PosixStack.h
@@ -48,8 +48,7 @@ class PosixNetworkStack : public NetworkStack {
   explicit PosixNetworkStack(CephContext *c);
 
   void spawn_worker(unsigned i, std::function<void ()> &&func) override {
-    threads.resize(i+1);
-    threads[i] = std::thread(func);
+    threads.emplace_back(std::move(func));
   }
   void join_worker(unsigned i) override {
     ceph_assert(threads.size() > i && threads[i].joinable());
