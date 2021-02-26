ceph/ceph

commit c5648d2fd7c8a05c2fb181844f80cd896bb00ba1

mon: convert ConfigKeyService -> KVMonitor
Convert this into a normal PaxosService.  This gives us (1) code uniformity
and also (2) a changelog in the form of the paxos commits for recently
changed keys that we can use to allow clients to subscribe to changes.

For upgrades this is pretty painless:
 - the actual kv data is in the same place
 - an old mon will still see updates made by a new monitor
 - a new mon will still see updates made by an old monitor, but won't get
   a paxosservice version bump.

Signed-off-by: Sage Weil <sage@newdream.net>

src/mon/ConfigMonitor.cc
@@ -5,6 +5,7 @@
 
 #include "mon/Monitor.h"
 #include "mon/ConfigMonitor.h"
+#include "mon/KVMonitor.h"
 #include "mon/MgrMonitor.h"
 #include "mon/OSDMonitor.h"
 #include "messages/MConfig.h"
@@ -112,7 +113,7 @@ void ConfigMonitor::encode_pending(MonitorDBStore::TransactionRef t)
     bufferlist metabl;
     ::encode(ceph_clock_now(), metabl);
     ::encode(pending_description, metabl);
-    t->put(CONFIG_PREFIX, history, metabl);
+    t->put(KV_PREFIX, history, metabl);
   }
   for (auto& p : pending) {
     string key = KEY_PREFIX + p.first;
@@ -121,17 +122,17 @@ void ConfigMonitor::encode_pending(MonitorDBStore::TransactionRef t)
       if (p.second && *p.second == q->second) {
 	continue;
       }
-      t->put(CONFIG_PREFIX, history + "-" + p.first, q->second);
+      t->put(KV_PREFIX, history + "-" + p.first, q->second);
     } else if (!p.second) {
       continue;
     }
     if (p.second) {
       dout(20) << __func__ << " set " << key << dendl;
-      t->put(CONFIG_PREFIX, key, *p.second);
-      t->put(CONFIG_PREFIX, history + "+" + p.first, *p.second);
+      t->put(KV_PREFIX, key, *p.second);
+      t->put(KV_PREFIX, history + "+" + p.first, *p.second);
     } else {
       dout(20) << __func__ << " rm " << key << dendl;
-      t->erase(CONFIG_PREFIX, key);
+      t->erase(KV_PREFIX, key);
     }
   }
 }
@@ -764,7 +765,7 @@ void ConfigMonitor::load_config()
   };
 
   unsigned num = 0;
-  KeyValueDB::Iterator it = mon.store->get_iterator(CONFIG_PREFIX);
+  KeyValueDB::Iterator it = mon.store->get_iterator(KV_PREFIX);
   it->lower_bound(KEY_PREFIX);
   config_map.clear();
   current.clear();
@@ -873,7 +874,7 @@ void ConfigMonitor::load_changeset(version_t v, ConfigChangeSet *ch)
 {
   ch->version = v;
   string prefix = HISTORY_PREFIX + stringify(v) + "/";
-  KeyValueDB::Iterator it = mon.store->get_iterator(CONFIG_PREFIX);
+  KeyValueDB::Iterator it = mon.store->get_iterator(KV_PREFIX);
   it->lower_bound(prefix);
   while (it->valid() && it->key().find(prefix) == 0) {
     if (it->key() == prefix) {

src/mon/KVMonitor.h
@@ -1,0 +1,52 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#pragma once
+
+#include <boost/optional.hpp>
+
+#include "mon/PaxosService.h"
+
+class MonSession;
+
+extern const std::string KV_PREFIX;
+
+class KVMonitor : public PaxosService
+{
+  version_t version = 0;
+  std::map<std::string,boost::optional<ceph::buffer::list>> pending;
+
+  bool _have_prefix(const string &prefix);
+
+public:
+  KVMonitor(Monitor &m, Paxos &p, const std::string& service_name);
+
+  void init() override;
+
+  void get_store_prefixes(set<string>& s) const override;
+
+  bool preprocess_command(MonOpRequestRef op);
+  bool prepare_command(MonOpRequestRef op);
+  
+  bool preprocess_query(MonOpRequestRef op) override;
+  bool prepare_update(MonOpRequestRef op) override;
+
+  void create_initial() override;
+  void update_from_paxos(bool *need_bootstrap) override;
+  void create_pending() override;
+  void encode_pending(MonitorDBStore::TransactionRef t) override;
+  version_t get_trim_to() const override;
+
+  void encode_full(MonitorDBStore::TransactionRef t) override { }
+
+  void on_active() override;
+  void tick() override;
+
+  int validate_osd_destroy(const int32_t id, const uuid_d& uuid);
+  void do_osd_destroy(int32_t id, uuid_d& uuid);
+  int validate_osd_new(
+      const uuid_d& uuid,
+      const std::string& dmcrypt_key,
+      std::stringstream& ss);
+  void do_osd_new(const uuid_d& uuid, const std::string& dmcrypt_key);
+};

src/mon/AuthMonitor.cc
@@ -17,7 +17,6 @@
 #include "mon/AuthMonitor.h"
 #include "mon/Monitor.h"
 #include "mon/MonitorDBStore.h"
-#include "mon/ConfigKeyService.h"
 #include "mon/OSDMonitor.h"
 #include "mon/MDSMonitor.h"
 #include "mon/ConfigMonitor.h"

src/mon/CMakeLists.txt
@@ -20,7 +20,7 @@ set(lib_mon_srcs
   ElectionLogic.cc
   ConnectionTracker.cc
   HealthMonitor.cc
-  ConfigKeyService.cc
+  KVMonitor.cc
   ../mds/MDSAuthCaps.cc
   ../mgr/mgr_commands.cc
   ../osd/OSDCap.cc

src/mon/ConfigKeyService.cc
@@ -1,414 +1,0 @@
-// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
-// vim: ts=8 sw=2 smarttab
-/*
- * Ceph - scalable distributed file system
- *
- * Copyright (C) 2013 Inktank, Inc
- *
- * This is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License version 2.1, as published by the Free Software
- * Foundation.  See file COPYING.
- *
- */
-
-#include <sstream>
-#include <stdlib.h>
-#include <limits.h>
-
-#include "mon/Monitor.h"
-#include "mon/ConfigKeyService.h"
-#include "mon/MonitorDBStore.h"
-#include "mon/OSDMonitor.h"
-#include "common/errno.h"
-#include "include/stringify.h"
-
-#include "include/ceph_assert.h" // re-clobber ceph_assert()
-#define dout_subsys ceph_subsys_mon
-#undef dout_prefix
-#define dout_prefix _prefix(_dout, mon, this)
-using namespace TOPNSPC::common;
-
-using namespace std::literals;
-using std::cerr;
-using std::cout;
-using std::dec;
-using std::hex;
-using std::list;
-using std::map;
-using std::make_pair;
-using std::ostream;
-using std::ostringstream;
-using std::pair;
-using std::set;
-using std::setfill;
-using std::string;
-using std::stringstream;
-using std::to_string;
-using std::vector;
-using std::unique_ptr;
-
-using ceph::bufferlist;
-using ceph::decode;
-using ceph::encode;
-using ceph::Formatter;
-using ceph::JSONFormatter;
-using ceph::mono_clock;
-using ceph::mono_time;
-using ceph::parse_timespan;
-using ceph::timespan_str;
-
-static ostream& _prefix(std::ostream *_dout, const Monitor &mon,
-                        const ConfigKeyService *service)
-{
-  return *_dout << "mon." << mon.name << "@" << mon.rank
-		<< "(" << mon.get_state_name() << ").config_key";
-}
-
-const string CONFIG_PREFIX = "mon_config_key";
-
-ConfigKeyService::ConfigKeyService(Monitor &m, Paxos &p)
-  : mon(m),
-    paxos(p)
-{}
-
-bool ConfigKeyService::in_quorum() const
-{
-  return (mon.is_leader() || mon.is_peon());
-}
-
-int ConfigKeyService::store_get(const string &key, bufferlist &bl)
-{
-  return mon.store->get(CONFIG_PREFIX, key, bl);
-}
-
-void ConfigKeyService::get_store_prefixes(set<string>& s) const
-{
-  s.insert(CONFIG_PREFIX);
-}
-
-void ConfigKeyService::store_put(const string &key, bufferlist &bl, Context *cb)
-{
-  MonitorDBStore::TransactionRef t = paxos.get_pending_transaction();
-  t->put(CONFIG_PREFIX, key, bl);
-  if (cb)
-    paxos.queue_pending_finisher(cb);
-  paxos.trigger_propose();
-}
-
-void ConfigKeyService::store_delete(const string &key, Context *cb)
-{
-  MonitorDBStore::TransactionRef t = paxos.get_pending_transaction();
-  store_delete(t, key);
-  if (cb)
-    paxos.queue_pending_finisher(cb);
-  paxos.trigger_propose();
-}
-
-void ConfigKeyService::store_delete(
-    MonitorDBStore::TransactionRef t,
-    const string &key)
-{
-  t->erase(CONFIG_PREFIX, key);
-}
-
-bool ConfigKeyService::store_exists(const string &key)
-{
-  return mon.store->exists(CONFIG_PREFIX, key);
-}
-
-void ConfigKeyService::store_list(stringstream &ss)
-{
-  KeyValueDB::Iterator iter =
-    mon.store->get_iterator(CONFIG_PREFIX);
-
-  JSONFormatter f(true);
-  f.open_array_section("keys");
-
-  while (iter->valid()) {
-    string key(iter->key());
-    f.dump_string("key", key);
-    iter->next();
-  }
-  f.close_section();
-  f.flush(ss);
-}
-
-bool ConfigKeyService::store_has_prefix(const string &prefix)
-{
-  KeyValueDB::Iterator iter =
-    mon.store->get_iterator(CONFIG_PREFIX);
-
-  while (iter->valid()) {
-    string key(iter->key());
-    size_t p = key.find(prefix);
-    if (p != string::npos && p == 0) {
-      return true;
-    }
-    iter->next();
-  }
-  return false;
-}
-
-static bool is_binary_string(const string& s)
-{
-  for (auto c : s) {
-    // \n and \t are escaped in JSON; other control characters are not.
-    if ((c < 0x20 && c != '\n' && c != '\t') || c >= 0x7f) {
-      return true;
-    }
-  }
-  return false;
-}
-
-void ConfigKeyService::store_dump(stringstream &ss, const string& prefix)
-{
-  KeyValueDB::Iterator iter =
-    mon.store->get_iterator(CONFIG_PREFIX);
-
-  dout(10) << __func__ << " prefix '" << prefix << "'" << dendl;
-  if (prefix.size()) {
-    iter->lower_bound(prefix);
-  }
-
-  JSONFormatter f(true);
-  f.open_object_section("config-key store");
-
-  while (iter->valid()) {
-    if (prefix.size() &&
-	iter->key().find(prefix) != 0) {
-      break;
-    }
-    string s = iter->value().to_str();
-    if (is_binary_string(s)) {
-      ostringstream ss;
-      ss << "<<< binary blob of length " << s.size() << " >>>";
-      f.dump_string(iter->key().c_str(), ss.str());
-    } else {
-      f.dump_string(iter->key().c_str(), s);
-    }
-    iter->next();
-  }
-  f.close_section();
-  f.flush(ss);
-}
-
-void ConfigKeyService::store_delete_prefix(
-    MonitorDBStore::TransactionRef t,
-    const string &prefix)
-{
-  KeyValueDB::Iterator iter =
-    mon.store->get_iterator(CONFIG_PREFIX);
-
-  while (iter->valid()) {
-    string key(iter->key());
-
-    size_t p = key.find(prefix);
-    if (p != string::npos && p == 0) {
-      store_delete(t, key);
-    }
-    iter->next();
-  }
-}
-
-bool ConfigKeyService::dispatch(MonOpRequestRef op)
-{
-  Message *m = op->get_req();
-  ceph_assert(m != NULL);
-  dout(10) << __func__ << " " << *m << dendl;
-
-  if (!in_quorum()) {
-    dout(1) << __func__ << " not in quorum -- waiting" << dendl;
-    paxos.wait_for_readable(op, new Monitor::C_RetryMessage(&mon, op));
-    return false;
-  }
-
-  ceph_assert(m->get_type() == MSG_MON_COMMAND);
-
-  MMonCommand *cmd = static_cast<MMonCommand*>(m);
-
-  ceph_assert(!cmd->cmd.empty());
-
-  int ret = 0;
-  stringstream ss;
-  bufferlist rdata;
-
-  string prefix;
-  cmdmap_t cmdmap;
-
-  if (!TOPNSPC::common::cmdmap_from_json(cmd->cmd, &cmdmap, ss)) {
-    return false;
-  }
-
-  cmd_getval(cmdmap, "prefix", prefix);
-  string key;
-  cmd_getval(cmdmap, "key", key);
-
-  if (prefix == "config-key get") {
-    ret = store_get(key, rdata);
-    if (ret < 0) {
-      ceph_assert(!rdata.length());
-      ss << "error obtaining '" << key << "': " << cpp_strerror(ret);
-      goto out;
-    }
-    ss << "obtained '" << key << "'";
-
-  } else if (prefix == "config-key put" ||
-	     prefix == "config-key set") {
-    if (!mon.is_leader()) {
-      mon.forward_request_leader(op);
-      // we forward the message; so return now.
-      return true;
-    }
-
-    bufferlist data;
-    string val;
-    if (cmd_getval(cmdmap, "val", val)) {
-      // they specified a value in the command instead of a file
-      data.append(val);
-    } else if (cmd->get_data_len() > 0) {
-      // they specified '-i <file>'
-      data = cmd->get_data();
-    }
-    if (data.length() > (size_t) g_conf()->mon_config_key_max_entry_size) {
-      ret = -EFBIG; // File too large
-      ss << "error: entry size limited to "
-         << g_conf()->mon_config_key_max_entry_size << " bytes. "
-         << "Use 'mon config key max entry size' to manually adjust";
-      goto out;
-    }
-
-    ss << "set " << key;
-
-    // we'll reply to the message once the proposal has been handled
-    store_put(key, data,
-	      new Monitor::C_Command(mon, op, 0, ss.str(), 0));
-    // return for now; we'll put the message once it's done.
-    return true;
-
-  } else if (prefix == "config-key del" ||
-             prefix == "config-key rm") {
-    if (!mon.is_leader()) {
-      mon.forward_request_leader(op);
-      return true;
-    }
-
-    if (!store_exists(key)) {
-      ret = 0;
-      ss << "no such key '" << key << "'";
-      goto out;
-    }
-    store_delete(key, new Monitor::C_Command(mon, op, 0, "key deleted", 0));
-    // return for now; we'll put the message once it's done
-    return true;
-
-  } else if (prefix == "config-key exists") {
-    bool exists = store_exists(key);
-    ss << "key '" << key << "'";
-    if (exists) {
-      ss << " exists";
-      ret = 0;
-    } else {
-      ss << " doesn't exist";
-      ret = -ENOENT;
-    }
-
-  } else if (prefix == "config-key list" ||
-	     prefix == "config-key ls") {
-    stringstream tmp_ss;
-    store_list(tmp_ss);
-    rdata.append(tmp_ss);
-    ret = 0;
-
-  } else if (prefix == "config-key dump") {
-    string prefix;
-    cmd_getval(cmdmap, "key", prefix);
-    stringstream tmp_ss;
-    store_dump(tmp_ss, prefix);
-    rdata.append(tmp_ss);
-    ret = 0;
-
-  }
-
-out:
-  if (!cmd->get_source().is_mon()) {
-    string rs = ss.str();
-    mon.reply_command(op, ret, rs, rdata, 0);
-  }
-
-  return (ret == 0);
-}
-
-string _get_dmcrypt_prefix(const uuid_d& uuid, const string k)
-{
-  return "dm-crypt/osd/" + stringify(uuid) + "/" + k;
-}
-
-int ConfigKeyService::validate_osd_destroy(
-    const int32_t id,
-    const uuid_d& uuid)
-{
-  string dmcrypt_prefix = _get_dmcrypt_prefix(uuid, "");
-  string daemon_prefix =
-    "daemon-private/osd." + stringify(id) + "/";
-
-  if (!store_has_prefix(dmcrypt_prefix) &&
-      !store_has_prefix(daemon_prefix)) {
-    return -ENOENT;
-  }
-  return 0;
-}
-
-void ConfigKeyService::do_osd_destroy(int32_t id, uuid_d& uuid)
-{
-  string dmcrypt_prefix = _get_dmcrypt_prefix(uuid, "");
-  string daemon_prefix =
-    "daemon-private/osd." + stringify(id) + "/";
-
-  MonitorDBStore::TransactionRef t = paxos.get_pending_transaction();
-  for (auto p : { dmcrypt_prefix, daemon_prefix }) {
-    store_delete_prefix(t, p);
-  }
-
-  paxos.trigger_propose();
-}
-
-int ConfigKeyService::validate_osd_new(
-    const uuid_d& uuid,
-    const string& dmcrypt_key,
-    stringstream& ss)
-{
-  string dmcrypt_prefix = _get_dmcrypt_prefix(uuid, "luks");
-  bufferlist value;
-  value.append(dmcrypt_key);
-
-  if (store_exists(dmcrypt_prefix)) {
-    bufferlist existing_value;
-    int err = store_get(dmcrypt_prefix, existing_value);
-    if (err < 0) {
-      dout(10) << __func__ << " unable to get dm-crypt key from store (r = "
-               << err << ")" << dendl;
-      return err;
-    }
-    if (existing_value.contents_equal(value)) {
-      // both values match; this will be an idempotent op.
-      return EEXIST;
-    }
-    ss << "dm-crypt key already exists and does not match";
-    return -EEXIST;
-  }
-  return 0;
-}
-
-void ConfigKeyService::do_osd_new(
-    const uuid_d& uuid,
-    const string& dmcrypt_key)
-{
-  ceph_assert(paxos.is_plugged());
-
-  string dmcrypt_key_prefix = _get_dmcrypt_prefix(uuid, "luks");
-  bufferlist dmcrypt_key_value;
-  dmcrypt_key_value.append(dmcrypt_key);
-  // store_put() will call trigger_propose
-  store_put(dmcrypt_key_prefix, dmcrypt_key_value, nullptr);
-}

src/mon/ConfigKeyService.h
@@ -1,63 +1,0 @@
-// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
-// vim: ts=8 sw=2 smarttab
-/*
- * Ceph - scalable distributed file system
- *
- * Copyright (C) 2013 Inktank, Inc
- *
- * This is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License version 2.1, as published by the Free Software
- * Foundation.  See file COPYING.
- *
- */
-#ifndef CEPH_MON_CONFIG_KEY_SERVICE_H
-#define CEPH_MON_CONFIG_KEY_SERVICE_H
-
-#include "include/Context.h"
-#include "mon/MonOpRequest.h"
-#include "mon/MonitorDBStore.h"
-
-class Paxos;
-class Monitor;
-
-class ConfigKeyService
-{
-public:
-  ConfigKeyService(Monitor &m, Paxos &p);
-  ~ConfigKeyService() {}
-
-  bool dispatch(MonOpRequestRef op);
-
-  int validate_osd_destroy(const int32_t id, const uuid_d& uuid);
-  void do_osd_destroy(int32_t id, uuid_d& uuid);
-  int validate_osd_new(
-      const uuid_d& uuid,
-      const std::string& dmcrypt_key,
-      std::stringstream& ss);
-  void do_osd_new(const uuid_d& uuid, const std::string& dmcrypt_key);
-
-  void get_store_prefixes(std::set<std::string>& s) const;
-
-private:
-  Monitor &mon;
-  Paxos &paxos;
-
-  bool in_quorum() const;
-
-  int store_get(const std::string &key, ceph::buffer::list &bl);
-  void store_put(const std::string &key, ceph::buffer::list &bl, Context *cb = NULL);
-  void store_delete(MonitorDBStore::TransactionRef t, const std::string &key);
-  void store_delete(const std::string &key, Context *cb = NULL);
-  void store_delete_prefix(
-      MonitorDBStore::TransactionRef t,
-      const std::string &prefix);
-  void store_list(std::stringstream &ss);
-  void store_dump(std::stringstream &ss, const std::string& prefix);
-  bool store_exists(const std::string &key);
-  bool store_has_prefix(const std::string &prefix);
-
-  static const std::string STORE_PREFIX;
-};
-
-#endif // CEPH_MON_CONFIG_KEY_SERVICE_H

commit 2b5801bc9eba0539adbbdaccd67f8fa61f3dda07

mon/ConfigMonitor: make config changes via KVMonitor's pending set
We need to ensure that changes we make to the kv store (config/...)
are proposed via KVMonitor so that they are properly versioned there
and shared with subscribers (notably, the mgr).

Fixes: c5648d2fd7c8a05c2fb181844f80cd896bb00ba1
Signed-off-by: Sage Weil <sage@newdream.net>

src/mon/ConfigMonitor.cc
@@ -96,7 +96,14 @@ void ConfigMonitor::encode_pending(MonitorDBStore::TransactionRef t)
 {
   dout(10) << " " << (version+1) << dendl;
   put_last_committed(t, version+1);
+  // NOTE: caller should have done encode_pending_to_kvmon() and
+  // kvmon->propose_pending() to commit the actual config changes.
+}
 
+void ConfigMonitor::encode_pending_to_kvmon()
+{
+  // we need to pass our data through KVMonitor so that it is properly
+  // versioned and shared with subscribers.
   for (auto& [key, value] : pending_cleanup) {
     if (pending.count(key) == 0) {
       derr << __func__ << " repair: adjusting config key '" << key << "'"
@@ -113,7 +120,7 @@ void ConfigMonitor::encode_pending(MonitorDBStore::TransactionRef t)
     bufferlist metabl;
     ::encode(ceph_clock_now(), metabl);
     ::encode(pending_description, metabl);
-    t->put(KV_PREFIX, history, metabl);
+    mon.kvmon()->enqueue_set(history, metabl);
   }
   for (auto& p : pending) {
     string key = KEY_PREFIX + p.first;
@@ -122,17 +129,17 @@ void ConfigMonitor::encode_pending(MonitorDBStore::TransactionRef t)
       if (p.second && *p.second == q->second) {
 	continue;
       }
-      t->put(KV_PREFIX, history + "-" + p.first, q->second);
+      mon.kvmon()->enqueue_set(history + "-" + p.first, q->second);
     } else if (!p.second) {
       continue;
     }
     if (p.second) {
       dout(20) << __func__ << " set " << key << dendl;
-      t->put(KV_PREFIX, key, *p.second);
-      t->put(KV_PREFIX, history + "+" + p.first, *p.second);
-    } else {
+      mon.kvmon()->enqueue_set(key, *p.second);
+      mon.kvmon()->enqueue_set(history + "+" + p.first, *p.second);
+   } else {
       dout(20) << __func__ << " rm " << key << dendl;
-      t->erase(KV_PREFIX, key);
+      mon.kvmon()->enqueue_rm(key);
     }
   }
 }
@@ -513,6 +520,13 @@ bool ConfigMonitor::prepare_command(MonOpRequestRef op)
   std::stringstream ss;
   int err = -EINVAL;
 
+  // make sure kv is writeable.
+  if (!mon.kvmon()->is_writeable()) {
+    dout(10) << __func__ << " waiting for kv mon to be writeable" << dendl;
+    mon.kvmon()->wait_for_writeable(op, new C_RetryMessage(this, op));
+    return false;
+  }
+
   cmdmap_t cmdmap;
   if (!cmdmap_from_json(m->cmd, &cmdmap, ss)) {
     string rs = ss.str();
@@ -724,7 +738,12 @@ update:
     err = 0;
     goto reply;
   }
-  force_immediate_propose();  // faster response
+  // immediately propose *with* KV mon
+  encode_pending_to_kvmon();
+  paxos.plug();
+  mon.kvmon()->propose_pending();
+  paxos.unplug();
+  force_immediate_propose();
   wait_for_finished_proposal(
     op,
     new Monitor::C_Command(
@@ -743,7 +762,11 @@ void ConfigMonitor::tick()
   if (!pending_cleanup.empty()) {
     changed = true;
   }
-  if (changed) {
+  if (changed && mon.kvmon()->is_writeable()) {
+    paxos.plug();
+    encode_pending_to_kvmon();
+    mon.kvmon()->propose_pending();
+    paxos.unplug();
     propose_pending();
   }
 }

src/mon/ConfigMonitor.h
@@ -20,6 +20,8 @@ class ConfigMonitor : public PaxosService
 
   std::map<std::string,ceph::buffer::list> current;
 
+  void encode_pending_to_kvmon();
+
 public:
   ConfigMonitor(Monitor &m, Paxos &p, const std::string& service_name);
 

src/mon/KVMonitor.h
@@ -55,4 +55,15 @@ public:
   void check_all_subs();
 
   bool maybe_send_update(Subscription *sub);
+
+
+  // used by other services to adjust kv content; note that callers MUST ensure that
+  // propose_pending() is called and a commit is forced to provide atomicity and
+  // proper subscriber notifications.
+  void enqueue_set(const std::string& key, bufferlist &v) {
+    pending[key] = v;
+  }
+  void enqueue_rm(const std::string& key) {
+    pending[key] = boost::none;
+  }
 };