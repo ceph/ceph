db9b319aab84e47ecc9ba6aa04b40c36eb627c3b
Kefu Chai <kchai@redhat.com>
2021-03-02 19:03:38 +0800

    crimson/mon: do not use futurize_invoke
    flatten the call in seastar::futurize_invoke().
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/crimson/mon/MonClient.h
+++ src/crimson/mon/MonClient.h
@@ -171,7 +171,7 @@ private:
   // @param rank, rank of the monitor to be connected, if it is less than 0,
   //              try to connect to all monitors in monmap, until one of them
   //              is connected.
-  // @return true if a connection is established to a monitor
+  // @return true if a connection to monitor is established
   seastar::future<bool> reopen_session(int rank);
   std::vector<unsigned> get_random_mons(unsigned n) const;
   seastar::future<> _add_conn(unsigned rank, uint64_t global_id);
