91e720d37c73719ab66be0947e6b828bb2853d7b
Ali Maredia <amaredia@redhat.com>
2021-03-02 00:24:14 -0500

    rgw: add latency to the request summary of an op
    Latency of a request added at the end of request
    summary rgw log line. This summary line also contains
    information about the request like the op, bucket,
    object, http status.
    
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- src/rgw/rgw_civetweb_frontend.cc
+++ src/rgw/rgw_civetweb_frontend.cc
@@ -66,10 +66,11 @@ int RGWCivetWebFrontend::process(struct mg_connection*  const conn)
 
   RGWRequest req(env.store->getRados()->get_new_req_id());
   int http_ret = 0;
+  ceph::coarse_real_clock::duration latency{};
   //assert (scheduler != nullptr);
   int ret = process_request(env.store, env.rest, &req, env.uri_prefix,
                             *env.auth_registry, &client_io, env.olog,
-                            null_yield, scheduler.get(), nullptr, &http_ret);
+                            null_yield, scheduler.get(), nullptr, &latency, &http_ret);
   if (ret < 0) {
     /* We don't really care about return code. */
     dout(20) << "process_request() returned " << ret << dendl;
