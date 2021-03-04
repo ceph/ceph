d10659b44336651281fb4d082afb9ad538124c09
Patrick Donnelly <pdonnell@redhat.com>
2021-03-04 13:01:01 -0800

    qa: schedule random subset
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- qa/machine_types/schedule_subset.sh
+++ qa/machine_types/schedule_subset.sh
@@ -2,8 +2,6 @@
 
 #command line => CEPH_BRANCH=<branch>; MACHINE_NAME=<machine_type>; SUITE_NAME=<suite>; ../schedule_subset.sh <day_of_week> $CEPH_BRANCH $MACHINE_NAME $SUITE_NAME $CEPH_QA_EMAIL $KERNEL <$FILTER>
 
-partition="$1"
-shift
 partitions="$1"
 shift
 branch="$1"
@@ -19,4 +17,4 @@ shift
 # rest of arguments passed directly to teuthology-suite
 
 echo "Scheduling $branch branch"
-teuthology-suite -v -c "$branch" -m "$machine" -k "$kernel" -s "$suite" --subset $(echo "$(date +%j) + $partition" | bc)/"$partitions" --newest 100 -e "$email" "$@"
+teuthology-suite -v -c "$branch" -m "$machine" -k "$kernel" -s "$suite" --subset "$((RANDOM % partitions))/$partitions" --newest 100 -e "$email" "$@"
