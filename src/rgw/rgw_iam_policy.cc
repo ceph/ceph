e1bacb193b917aba1a6a98f5ade12ac6fa119eff
Pritha Srivastava <prsrivas@redhat.com>
2021-03-04 14:27:46 +0530

    rgw/sts: adding code for aws:RequestTags as part of session tags implementation.
    Session Tags can be passed in the web token in
    AssumeRoleWithWebIdentity call by configuring
    them in the IDP. These tags can be used as Conditions
    in the trust policy of a role as aws:RequestTag, based
    on which a federated user is allowed to assume a role.
    The trust policy should have a statement for 'sts:TagSession'
    alongwith 'sts:AssumeRoleWithWebIdentity' in case principal
    tags are passed in the web token.
    
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>

--- src/rgw/rgw_iam_policy.cc
+++ src/rgw/rgw_iam_policy.cc
@@ -156,6 +156,7 @@ static const actpair actpairs[] =
  { "sts:AssumeRole", stsAssumeRole},
  { "sts:AssumeRoleWithWebIdentity", stsAssumeRoleWithWebIdentity},
  { "sts:GetSessionToken", stsGetSessionToken},
+ { "sts:TagSession", stsTagSession},
 };
 
 struct PolicyParser;
@@ -1312,6 +1313,9 @@ const char* action_bit_string(uint64_t action) {
 
   case stsGetSessionToken:
     return "sts:GetSessionToken";
+
+  case stsTagSession:
+    return "sts:TagSession";
   }
   return "s3Invalid";
 }
