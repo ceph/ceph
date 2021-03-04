242cf248d1953da9b9ff39f325bea34d43520b7f
Nathan Cutler <ncutler@suse.com>
2021-03-04 19:12:06 +0100

    cmake: eliminate find_package_handle_standard_args warning
    This commit eliminates the following unsightly warning:
    
         -- Found pmem: /usr/lib64/libpmem.so
         CMake Warning (dev) at /usr/share/cmake/Modules/FindPackageHandleStandardArgs.cmake:426 (message):
           The package name passed to 'find_package_handle_standard_args' (pemobj)
           does not match the name of the calling package (pmem).  This can lead
    
           problems in calling code that expects 'find_package' result variables
           (e.g., '_FOUND') to follow a certain pattern.
         Call Stack (most recent call first):
           cmake/modules/Findpmem.cmake:32 (find_package_handle_standard_args)
           src/CMakeLists.txt:511 (find_package)
         This warning is for project developers.  Use -Wno-dev to suppress it.
    
         -- Found pmemobj: /usr/lib64/libpmemobj.so
    
    Fixes: 242d391408127ded975c24490af9347011db0174
    Signed-off-by: Nathan Cutler <ncutler@suse.com>

--- cmake/modules/Findpmem.cmake
+++ cmake/modules/Findpmem.cmake
@@ -3,9 +3,6 @@
 # PMEM_INCLUDE_DIR - Where to find libpmem.h
 # PMEM_LIBRARIES - List of libraries when using pmdk.
 # pmem_FOUND - True if pmem found.
-# PMEMOBJ_INCLUDE_DIR - Where to find libpmemobj.h
-# PMEMOBJ_LIBRARIES - List of libraries when using pmdk obj.
-# pmemobj_FOUND - True if pmemobj found.
 
 find_path(PMEM_INCLUDE_DIR libpmem.h)
 find_library(PMEM_LIBRARIES pmem)
@@ -24,22 +21,4 @@ if(pmem_FOUND AND NOT TARGET pmem::pmem)
     INTERFACE_INCLUDE_DIRECTORIES "${PMEM_INCLUDE_DIR}"
     IMPORTED_LINK_INTERFACE_LANGUAGES "C"
     IMPORTED_LOCATION "${PMEM_LIBRARIES}")
-endif()
-
-find_path(PMEMOBJ_INCLUDE_DIR libpmemobj.h)
-find_library(PMEMOBJ_LIBRARIES pmemobj)
-
-find_package_handle_standard_args(pmemobj
-  DEFAULT_MSG PMEMOBJ_LIBRARIES PMEMOBJ_INCLUDE_DIR)
-
-mark_as_advanced(
-  PMEMOBJ_INCLUDE_DIR
-  PMEMOBJ_LIBRARIES)
-
-if(pmemobj_FOUND AND NOT TARGET pmem::pmemobj)
-  add_library(pmem::pmemobj UNKNOWN IMPORTED)
-  set_target_properties(pmem::pmemobj PROPERTIES
-    INTERFACE_INCLUDE_DIRECTORIES "${PMEMOBJ_INCLUDE_DIR}"
-    IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-    IMPORTED_LOCATION "${PMEMOBJ_LIBRARIES}")
 endif()
