442904bf2da3b83c14d262a5673b81476a537364
Michael Fritch <mfritch@suse.com>
2021-03-08 07:28:24 -0700

    cephadm: run containers using `--init` by default
    generally all ceph containers need an init process to both reap any
    zombie pids and/or perform signal handling (e.g. coredumps, etc.)
    
    Signed-off-by: Michael Fritch <mfritch@suse.com>
    (cherry picked from commit 9ac2ab85cb1b828bd35b88c5ac2b477fd6f8e8e2)

--- src/pybind/mgr/cephadm/module.py
+++ src/pybind/mgr/cephadm/module.py
@@ -225,7 +225,7 @@ class CephadmOrchestrator(orchestrator.Orchestrator, MgrModule):
         {
             'name': 'container_init',
             'type': 'bool',
-            'default': False,
+            'default': True,
             'desc': 'Run podman/docker with `--init`',
         },
         {
@@ -310,7 +310,7 @@ class CephadmOrchestrator(orchestrator.Orchestrator, MgrModule):
             self.warn_on_stray_daemons = True
             self.warn_on_failed_host_check = True
             self.allow_ptrace = False
-            self.container_init = False
+            self.container_init = True
             self.prometheus_alerts_path = ''
             self.migration_current: Optional[int] = None
             self.config_dashboard = True
@@ -1104,22 +1104,27 @@ To check that the host is reachable:
 
             final_args = []
 
+            # global args
             if env_vars:
                 for env_var_pair in env_vars:
                     final_args.extend(['--env', env_var_pair])
 
             if image:
                 final_args.extend(['--image', image])
+
+            if not self.container_init:
+                final_args += ['--no-container-init']
+
+            # subcommand
             final_args.append(command)
 
+            # subcommand args
             if not no_fsid:
                 final_args += ['--fsid', self._cluster_fsid]
 
-            if self.container_init:
-                final_args += ['--container-init']
-
             final_args += args
 
+            # exec
             self.log.debug('args: %s' % (' '.join(final_args)))
             if self.mode == 'root':
                 if stdin:
