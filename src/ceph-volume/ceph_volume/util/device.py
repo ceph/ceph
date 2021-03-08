9780d28028a40ecbfc327dab779e8a37c9aaed51
Guillaume Abrioux <gabrioux@redhat.com>
2021-03-08 09:59:26 +0100

    ceph-volume: `get_first_*()` refactor
    As indicated by commit 17957d9beb42a04b8f180ccb7ba07d43179a41d3 those
    fuctions were meant to avoid writing something like following:
    
    ```
    lvs = get_lvs()
    if len(lvs) >= 1:
    	lvs = lv[0]
    ```
    
    Those functions should return `None` if 0 or more than 1 item is returned.
    The current name of these functions are confusing and can lead to thinking that
    we just want the first item returned, even though it returns more than 1
    item, let's rename them to `get_single_pv()`, `get_single_vg()` and
    `get_single_lv()`
    
    Closes: https://tracker.ceph.com/issues/49643
    
    Signed-off-by: Guillaume Abrioux <gabrioux@redhat.com>
    (cherry picked from commit a5e4216b49704783c55fb83b3ae6dde35b0082ad)

--- src/ceph-volume/ceph_volume/util/device.py
+++ src/ceph-volume/ceph_volume/util/device.py
@@ -162,11 +162,11 @@ class Device(object):
         # if the path is not absolute, we have 'vg/lv', let's use LV name
         # to get the LV.
         if self.path[0] == '/':
-            lv = lvm.get_first_lv(filters={'lv_path': self.path})
+            lv = lvm.get_single_lv(filters={'lv_path': self.path})
         else:
             vgname, lvname = self.path.split('/')
-            lv = lvm.get_first_lv(filters={'lv_name': lvname,
-                                           'vg_name': vgname})
+            lv = lvm.get_single_lv(filters={'lv_name': lvname,
+                                            'vg_name': vgname})
         if lv:
             self.lv_api = lv
             self.lvs = [lv]
