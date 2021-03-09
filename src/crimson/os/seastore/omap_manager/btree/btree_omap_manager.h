cbd31b4cbe389812c11c745310c48b4be1a0dde9
Kefu Chai <kchai@redhat.com>
2021-03-09 22:30:56 +0800

    crimson: pass by `const` or `const&` if possible
    more constness, and better performance.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/crimson/os/seastore/omap_manager/btree/btree_omap_manager.h
+++ src/crimson/os/seastore/omap_manager/btree/btree_omap_manager.h
@@ -36,7 +36,7 @@ class BtreeOMapManager : public OMapManager {
    */
   using get_root_ertr = base_ertr;
   using get_root_ret = get_root_ertr::future<OMapNodeRef>;
-  get_root_ret get_omap_root(
+  static get_root_ret get_omap_root(
     omap_context_t c,
     const omap_root_t &omap_root);
 
@@ -49,7 +49,7 @@ class BtreeOMapManager : public OMapManager {
   handle_root_split_ret handle_root_split(
     omap_context_t c,
     omap_root_t &omap_root,
-    OMapNode::mutation_result_t mresult);
+    const OMapNode::mutation_result_t& mresult);
 
   /* handle_root_merge
    *
