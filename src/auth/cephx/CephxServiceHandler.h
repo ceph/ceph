7a224bad5bf5f7bbc1e805b012c4882d37a120be
Ilya Dryomov <idryomov@gmail.com>
2021-03-09 14:36:39 +0100

    auth/AuthServiceHandler: build_cephx_response_header() is cephx-specific
    Make the one in CephxServiceHandler private and drop the stub in
    AuthNoneServiceHandler.
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- src/auth/cephx/CephxServiceHandler.h
+++ src/auth/cephx/CephxServiceHandler.h
@@ -41,7 +41,9 @@ public:
     CryptoKey *session_key,
     std::string *connection_secret) override;
 
-  void build_cephx_response_header(int request_type, int status, ceph::buffer::list& bl);
+private:
+  void build_cephx_response_header(int request_type, int status,
+				   ceph::buffer::list& bl);
 };
 
 #endif
