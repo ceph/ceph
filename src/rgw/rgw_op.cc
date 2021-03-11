48d35660c0ea629fe698627438d91564e9033ec9
zhouyunkai <498549175@qq.com>
2021-03-11 14:42:12 +0800

    rgw:fix the bug of file decompression error which is larger than 15M.
    Last patch of compressor_message has a bug released. This patch is to fix the bug of file decompression, when the file is larger than 15M.
    I see the last patch of compressor_message is merged in v16.1.0. As this patch is based on the last one, so it should be merged after the last patch.
    
    Signed-off-by: zhouyunkai <498549175@qq.com>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -6136,7 +6136,7 @@ void RGWCompleteMultipart::execute()
         if (!compressed)
           cs_info.compression_type = obj_part.cs_info.compression_type;
         cs_info.orig_size += obj_part.cs_info.orig_size;
-        cs_info.compressor_message = obj_part.cs_info.compressor_message;
+        cs_info.compressor_message = obj_part.cs_info.compressor_message; 
         compressed = true;
       }
 
