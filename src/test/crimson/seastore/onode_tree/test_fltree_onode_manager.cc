fe0178aa345e78e0c70dd57f15bf79ed3198aa7b
Yingxin Cheng <yingxin.cheng@intel.com>
2021-03-12 16:12:14 +0800

    crimson/seastore/onode_tree: implement FLTreeOnodeManager unittest
    Signed-off-by: Yingxin Cheng <yingxin.cheng@intel.com>

--- src/test/crimson/seastore/onode_tree/test_fltree_onode_manager.cc
+++ src/test/crimson/seastore/onode_tree/test_fltree_onode_manager.cc
@@ -1,16 +1,14 @@
-// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:nil -*-
 // vim: ts=8 sw=2 smarttab
 
-#include <string>
-#include <iostream>
-#include <sstream>
+#include <boost/range/combine.hpp>
 
 #include "test/crimson/gtest_seastar.h"
 
 #include "test/crimson/seastore/transaction_manager_test_state.h"
 
-#include "crimson/os/futurized_collection.h"
 #include "crimson/os/seastore/onode_manager/staged-fltree/fltree_onode_manager.h"
+#include "crimson/os/seastore/onode_manager/staged-fltree/tree_utils.h"
 
 using namespace crimson;
 using namespace crimson::os;
@@ -25,19 +23,41 @@ namespace {
   }
 }
 
-ghobject_t make_oid(unsigned i) {
-  stringstream ss;
-  ss << "object_" << i;
-  auto ret = ghobject_t(
-    hobject_t(
-      sobject_t(ss.str(), CEPH_NOSNAP)));
-  ret.hobj.nspace = "asdf";
-  return ret;
-}
+struct onode_item_t {
+  uint32_t size;
+  uint64_t id;
+  uint32_t cnt_modify = 0;
 
-struct fltree_onode_manager_test_t :
-  public seastar_test_suite_t,
-  TMTestState {
+  void initialize(Transaction& t, Onode& value) const {
+    auto& layout = value.get_mutable_layout(t);
+    layout.size = size;
+    layout.omap_root.update(omap_root_t(id, cnt_modify));
+    validate(value);
+  }
+
+  void validate(Onode& value) const {
+    auto& layout = value.get_layout();
+    ceph_assert(laddr_t(layout.size) == laddr_t{size});
+    ceph_assert(layout.omap_root.get().addr == id);
+    ceph_assert(layout.omap_root.get().depth == cnt_modify);
+  }
+
+  void modify(Transaction& t, Onode& value) {
+    validate(value);
+    ++cnt_modify;
+    initialize(t, value);
+  }
+
+  static onode_item_t create(std::size_t size, std::size_t id) {
+    ceph_assert(size <= std::numeric_limits<uint32_t>::max());
+    return {(uint32_t)size, id};
+  }
+};
+
+struct fltree_onode_manager_test_t
+    : public seastar_test_suite_t, TMTestState {
+  using iterator_t = typename KVPool<onode_item_t>::iterator_t;
+
   FLTreeOnodeManagerRef manager;
 
   seastar::future<> set_up_fut() final {
@@ -63,73 +83,143 @@ struct fltree_onode_manager_test_t :
     return TMTestState::_mkfs(
     ).then([this] {
       return seastar::do_with(
-	tm->create_transaction(),
-	[this](auto &t) {
-	  return manager->mkfs(*t
-	  ).safe_then([this, &t] {
-	    return tm->submit_transaction(std::move(t));
-	  }).handle_error(
-	    crimson::ct_error::assert_all{
-	      "Invalid error in _mkfs"
-	    }
-	  );
-	});
+        tm->create_transaction(),
+        [this](auto &t) {
+          return manager->mkfs(*t
+          ).safe_then([this, &t] {
+            return tm->submit_transaction(std::move(t));
+          }).handle_error(
+            crimson::ct_error::assert_all{
+              "Invalid error in _mkfs"
+            }
+          );
+        });
+    });
+  }
+
+  template <typename F>
+  void with_transaction(F&& f) {
+    auto t = tm->create_transaction();
+    std::invoke(f, *t);
+    tm->submit_transaction(std::move(t)).unsafe_get0();
+  }
+
+  template <typename F>
+  void with_onode_write(iterator_t& it, F&& f) {
+    with_transaction([this, &it, f=std::move(f)] (auto& t) {
+      auto p_kv = *it;
+      auto onode = manager->get_or_create_onode(
+          t, p_kv->key).unsafe_get0();
+      std::invoke(f, t, *onode, p_kv->value);
+      manager->write_dirty(t, {onode}).unsafe_get0();
+    });
+  }
+
+  void validate_onode(iterator_t& it) {
+    with_transaction([this, &it] (auto& t) {
+      auto p_kv = *it;
+      auto onode = manager->get_onode(
+          t, p_kv->key).unsafe_get0();
+      p_kv->value.validate(*onode);
+    });
+  }
+
+  template <typename F>
+  void with_onodes_process(
+      iterator_t& start, iterator_t& end, F&& f) {
+    std::vector<ghobject_t> oids;
+    std::vector<onode_item_t*> items;
+    auto it = start;
+    while(it != end) {
+      auto p_kv = *it;
+      oids.emplace_back(p_kv->key);
+      items.emplace_back(&p_kv->value);
+      ++it;
+    }
+    with_transaction([this, &oids, &items, f=std::move(f)] (auto& t) mutable {
+      std::invoke(f, t, oids, items);
     });
   }
 
   template <typename F>
   void with_onodes_write(
-    std::vector<ghobject_t> oids,
-    F &&f) {
-    auto t = tm->create_transaction();
-    auto onodes = manager->get_or_create_onodes(
-      *t,
-      oids).unsafe_get0();
-    std::invoke(f, *t, onodes);
-    manager->write_dirty(
-      *t,
-      onodes
-    ).unsafe_get0();
-    tm->submit_transaction(std::move(t)).unsafe_get0();
+      iterator_t& start, iterator_t& end, F&& f) {
+    with_onodes_process(start, end,
+        [this, f=std::move(f)] (auto& t, auto& oids, auto& items) {
+      auto onodes = manager->get_or_create_onodes(
+          t, oids).unsafe_get0();
+      for (auto tup : boost::combine(onodes, items)) {
+        OnodeRef onode;
+        onode_item_t* p_item;
+        boost::tie(onode, p_item) = tup;
+        std::invoke(f, t, *onode, *p_item);
+      }
+      manager->write_dirty(t, onodes).unsafe_get0();
+    });
   }
 
-  template <typename F>
-  void with_onodes_write_range(
-    unsigned start,
-    unsigned end,
-    unsigned stride,
-    F &&f) {
-    std::vector<ghobject_t> oids;
-    for (unsigned i = start; i < end; i += stride) {
-      oids.emplace_back(make_oid(i));
-    }
-    with_onodes_write(std::move(oids), std::forward<F>(f));
-  }
-
-  template <typename F>
-  void with_onode_write(unsigned i, F &&f) {
-    with_onodes_write_range(
-      i,
-      i + 1,
-      1, [this, f=std::forward<F>(f)](auto &t, auto &onodes) {
-	assert(onodes.size() == 1);
-	std::invoke(f, t, *onodes[0]);
-      });
+  void validate_onodes(
+      iterator_t& start, iterator_t& end) {
+    with_onodes_process(start, end,
+        [this] (auto& t, auto& oids, auto& items) {
+      for (auto tup : boost::combine(oids, items)) {
+        ghobject_t oid;
+        onode_item_t* p_item;
+        boost::tie(oid, p_item) = tup;
+        auto onode = manager->get_onode(t, oid).unsafe_get0();
+        p_item->validate(*onode);
+      }
+    });
   }
 
   fltree_onode_manager_test_t() {}
 };
 
-TEST_F(fltree_onode_manager_test_t, touch)
+TEST_F(fltree_onode_manager_test_t, 1_single)
 {
   run_async([this] {
-    with_onode_write(0, [](auto &t, auto &onode) {
-      onode.get_mutable_layout(t);
+    auto pool = KVPool<onode_item_t>::create_range({0, 1}, {128, 256});
+    auto iter = pool.begin();
+    with_onode_write(iter, [](auto& t, auto& onode, auto& item) {
+      item.initialize(t, onode);
     });
-    /* Disabled pending fix
-    with_onode_write(0, [](auto &t, auto &onode) {
-      onode.get_mutable_layout(t);
+    validate_onode(iter);
+
+    with_onode_write(iter, [](auto& t, auto& onode, auto& item) {
+      item.modify(t, onode);
     });
-    */
+    validate_onode(iter);
+  });
+}
+
+TEST_F(fltree_onode_manager_test_t, 2_synthetic)
+{
+  run_async([this] {
+    auto pool = KVPool<onode_item_t>::create_range(
+        {0, 100}, {32, 64, 128, 256, 512});
+    auto start = pool.begin();
+    auto end = pool.end();
+    with_onodes_write(start, end,
+        [](auto& t, auto& onode, auto& item) {
+      item.initialize(t, onode);
+    });
+    validate_onodes(start, end);
+
+    auto rd_start = pool.random_begin();
+    auto rd_end = rd_start + 50;
+    with_onodes_write(rd_start, rd_end,
+        [](auto& t, auto& onode, auto& item) {
+      item.modify(t, onode);
+    });
+    validate_onodes(start, end);
+
+    pool.shuffle();
+    rd_start = pool.random_begin();
+    rd_end = rd_start + 50;
+    with_onodes_write(rd_start, rd_end,
+        [](auto& t, auto& onode, auto& item) {
+      item.modify(t, onode);
+    });
+    validate_onodes(start, end);
   });
 }
