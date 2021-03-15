ceph/ceph

commit fa5df9a6ce1bf4707a4f4f1d82043a0d289c6861

mgr/cephadm: tolerate failure to update daemon caps
If we're upgrading from 15.2.0, we may fail to update caps.  Instead of
failing the upgrade hard, warn to the log and continue.  This is less
than ideal, but the caps will get corrected the next time the daemon is
redeployed on the next upgrade, and most likely the previous caps will
continue to work (given they were presumably working before the upgrade).

Signed-off-by: Sage Weil <sage@newdream.net>

src/pybind/mgr/cephadm/services/cephadmservice.py
@@ -147,11 +147,13 @@ class CephadmService(metaclass=ABCMeta):
             'caps': caps,
         })
         if err:
-            ret, out, err = self.mgr.check_mon_command({
+            ret, out, err = self.mgr.mon_command({
                 'prefix': 'auth caps',
                 'entity': entity,
                 'caps': caps,
             })
+            if err:
+                self.mgr.log.warning(f"Unable to update caps for {entity}")
         return keyring
 
     def _inventory_get_addr(self, hostname: str) -> str:

src/pybind/mgr/cephadm/tests/test_services.py
@@ -19,7 +19,9 @@ class FakeMgr:
     def __init__(self):
         self.config = ''
         self.check_mon_command = MagicMock(side_effect=self._check_mon_command)
+        self.mon_command = MagicMock(side_effect=self._check_mon_command)
         self.template = MagicMock()
+        self.log = MagicMock()
 
     def _check_mon_command(self, cmd_dict, inbuf=None):
         prefix = cmd_dict.get('prefix')
@@ -114,7 +116,7 @@ class TestCephadmService:
                                'caps': expected_caps})
 
         assert expected_call in mgr.check_mon_command.mock_calls
-        assert expected_call2 in mgr.check_mon_command.mock_calls
+        assert expected_call2 in mgr.mon_command.mock_calls
 
     def test_get_auth_entity(self):
         mgr = FakeMgr()

commit d6b992bc72871691c1f8a27acb70efaf23cf7b0f

mgr/cephadm: fix 'auth caps' fallback
The first get-or-create attempt also needs to tolerate failure.

Fixes: fa5df9a6ce1bf4707a4f4f1d82043a0d289c6861

Signed-off-by: Sage Weil <sage@newdream.net>

src/pybind/mgr/cephadm/services/cephadmservice.py
@@ -144,7 +144,7 @@ class CephadmService(metaclass=ABCMeta):
         return DaemonDescription()
 
     def get_keyring_with_caps(self, entity: AuthEntity, caps: List[str]) -> str:
-        ret, keyring, err = self.mgr.check_mon_command({
+        ret, keyring, err = self.mgr.mon_command({
             'prefix': 'auth get-or-create',
             'entity': entity,
             'caps': caps,

src/pybind/mgr/cephadm/tests/test_services.py
@@ -115,7 +115,7 @@ class TestCephadmService:
                                'entity': 'client.iscsi.a',
                                'caps': expected_caps})
 
-        assert expected_call in mgr.check_mon_command.mock_calls
+        assert expected_call in mgr.mon_command.mock_calls
         assert expected_call2 in mgr.mon_command.mock_calls
 
     def test_get_auth_entity(self):