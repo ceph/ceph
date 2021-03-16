sunilangadi2/ceph-ci

osd: Try other PGs when reservation failures occur
Fixes: https://tracker.ceph.com/issues/48843

Signed-off-by: David Zafman <dzafman@redhat.com>

src/osd/OSD.cc
@@ -7611,6 +7611,13 @@ void OSD::sched_scrub()
 	continue;
       }
 
+      // If this one couldn't reserve, skip for now
+      if (pg->get_reserve_failed()) {
+	pg->unlock();
+	dout(20) << __func__ << " pg  " << scrub_job.pgid << " reserve failed, skipped" << dendl;
+        continue;
+      }
+
       // This has already started, so go on to the next scrub job
       if (pg->is_scrub_active()) {
 	pg->unlock();
@@ -7630,7 +7637,7 @@ void OSD::sched_scrub()
       if (pg->m_scrubber->is_reserving()) {
 	pg->unlock();
 	dout(10) << __func__ << ": reserve in progress pgid " << scrub_job.pgid << dendl;
-	break;
+	goto out;
       }
       dout(15) << "sched_scrub scrubbing " << scrub_job.pgid << " at " << scrub_job.sched_time
 	       << (pg->get_must_scrub() ? ", explicitly requested" :
@@ -7639,11 +7646,34 @@ void OSD::sched_scrub()
       if (pg->sched_scrub()) {
 	pg->unlock();
         dout(10) << __func__ << " scheduled a scrub!" << " (~" << scrub_job.pgid << "~)" << dendl;
-	break;
+	goto out;
       }
+      // If this is set now we must have had a local reserve failure, so can't scrub anything right now
+      if (pg->get_reserve_failed()) {
+	pg->unlock();
+	dout(20) << __func__ << " pg  " << scrub_job.pgid << " local reserve failed, nothing to be done now" << dendl;
+        goto out;
+      }
+
       pg->unlock();
     } while (service.next_scrub_stamp(scrub_job, &scrub_job));
+
+    // Clear reserve_failed from all pending PGs, so we try again
+    if (service.first_scrub_stamp(&scrub_job)) {
+      do {
+        if (scrub_job.sched_time > now)
+	  break;
+        PGRef pg = _lookup_lock_pg(scrub_job.pgid);
+	// If we can't lock, it's ok we can get it next time
+        if (!pg)
+	  continue;
+        pg->clear_reserve_failed();
+        pg->unlock();
+      } while (service.next_scrub_stamp(scrub_job, &scrub_job));
+    }
   }
+
+out:
   dout(20) << "sched_scrub done" << dendl;
 }
 

ceph/ceph

(cherry picked from commit 08c3ede0844507074de2b43638423c094976b493)
Conflicts:
	PendingReleaseNotes (trivial)
	src/osd/OSD.cc (trivial)
	src/osd/PG.cc (set_reserve_failed() in appropriate places)
	src/osd/PG.h (add reserve_failed to scrubber)
	src/osd/pg_scrubber.h (not present)
	src/osd/scrub_machine.cc (not present)
	src/osd/scrub_machine_lstnr.h (not present)
	src/osd/scrubber_common.h (not present)
Conflicts for Octopus to Nautilus:
	src/osd/PG.cc (code moved around in Octopus)
	src/osd/PG.h (trivial)

src/osd/OSD.cc
@@ -7979,6 +7979,14 @@ void OSD::sched_scrub()
       PGRef pg = _lookup_lock_pg(scrub.pgid);
       if (!pg)
 	continue;
+
+      // If this one couldn't reserve, skip for now
+      if (pg->get_reserve_failed()) {
+	pg->unlock();
+	dout(20) << __func__ << " pg  " << scrub.pgid << " reserve failed, skipped" << dendl;
+        continue;
+      }
+
       // This has already started, so go on to the next scrub job
       if (pg->scrubber.active) {
 	pg->unlock();
@@ -7996,8 +8004,8 @@ void OSD::sched_scrub()
       // If it is reserving, let it resolve before going to the next scrub job
       if (pg->scrubber.local_reserved && !pg->scrubber.active) {
 	pg->unlock();
-	dout(30) << __func__ << ": reserve in progress pgid " << scrub.pgid << dendl;
-	break;
+	dout(10) << __func__ << ": reserve in progress pgid " << scrub.pgid << dendl;
+	goto out;
       }
       dout(10) << "sched_scrub scrubbing " << scrub.pgid << " at " << scrub.sched_time
 	       << (pg->get_must_scrub() ? ", explicitly requested" :
@@ -8005,11 +8013,35 @@ void OSD::sched_scrub()
 	       << dendl;
       if (pg->sched_scrub()) {
 	pg->unlock();
-	break;
+        dout(10) << __func__ << " scheduled a scrub!" << " (~" << scrub.pgid << "~)" << dendl;
+	goto out;
       }
+      // If this is set now we must have had a local reserve failure, so can't scrub anything right now
+      if (pg->get_reserve_failed()) {
+	pg->unlock();
+	dout(20) << __func__ << " pg  " << scrub.pgid << " local reserve failed, nothing to be done now" << dendl;
+        goto out;
+      }
+
       pg->unlock();
     } while (service.next_scrub_stamp(scrub, &scrub));
+
+    // Clear reserve_failed from all pending PGs, so we try again
+    if (service.first_scrub_stamp(&scrub)) {
+      do {
+        if (scrub.sched_time > now)
+	  break;
+        PGRef pg = _lookup_lock_pg(scrub.pgid);
+	// If we can't lock, it's ok we can get it next time
+        if (!pg)
+	  continue;
+        pg->clear_reserve_failed();
+        pg->unlock();
+      } while (service.next_scrub_stamp(scrub, &scrub));
+    }
   }
+
+out:
   dout(20) << "sched_scrub done" << dendl;
 }
 