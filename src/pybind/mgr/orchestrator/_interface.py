sunilangadi2/ceph-ci

mgr/orch: replace def add_{type}(...) with generic add_daemon()
Signed-off-by: Sebastian Wagner <sebastian.wagner@suse.com>

src/pybind/mgr/orchestrator/_interface.py
@@ -1039,32 +1039,20 @@ class Orchestrator(object):
         """Zap/Erase a device (DESTROYS DATA)"""
         raise NotImplementedError()
 
-    def add_mon(self, spec: ServiceSpec) -> Completion[List[str]]:
-        """Create mon daemon(s)"""
+    def add_daemon(self, spec: ServiceSpec) -> Completion[List[str]]:
+        """Create daemons daemon(s) for unmanaged services"""
         raise NotImplementedError()
 
     def apply_mon(self, spec: ServiceSpec) -> Completion[str]:
         """Update mon cluster"""
         raise NotImplementedError()
 
-    def add_mgr(self, spec: ServiceSpec) -> Completion[List[str]]:
-        """Create mgr daemon(s)"""
-        raise NotImplementedError()
-
     def apply_mgr(self, spec: ServiceSpec) -> Completion[str]:
         """Update mgr cluster"""
         raise NotImplementedError()
 
-    def add_mds(self, spec: ServiceSpec) -> Completion[List[str]]:
-        """Create MDS daemon(s)"""
-        raise NotImplementedError()
-
     def apply_mds(self, spec: ServiceSpec) -> Completion[str]:
         """Update MDS cluster"""
-        raise NotImplementedError()
-
-    def add_rgw(self, spec: RGWSpec) -> Completion[List[str]]:
-        """Create RGW daemon(s)"""
         raise NotImplementedError()
 
     def apply_rgw(self, spec: RGWSpec) -> Completion[str]:
@@ -1075,72 +1063,36 @@ class Orchestrator(object):
         """Update ha-rgw daemons"""
         raise NotImplementedError()
 
-    def add_rbd_mirror(self, spec: ServiceSpec) -> Completion[List[str]]:
-        """Create rbd-mirror daemon(s)"""
-        raise NotImplementedError()
-
     def apply_rbd_mirror(self, spec: ServiceSpec) -> Completion[str]:
         """Update rbd-mirror cluster"""
-        raise NotImplementedError()
-
-    def add_nfs(self, spec: NFSServiceSpec) -> Completion[List[str]]:
-        """Create NFS daemon(s)"""
         raise NotImplementedError()
 
     def apply_nfs(self, spec: NFSServiceSpec) -> Completion[str]:
         """Update NFS cluster"""
         raise NotImplementedError()
 
-    def add_iscsi(self, spec: IscsiServiceSpec) -> Completion[List[str]]:
-        """Create iscsi daemon(s)"""
-        raise NotImplementedError()
-
     def apply_iscsi(self, spec: IscsiServiceSpec) -> Completion[str]:
         """Update iscsi cluster"""
-        raise NotImplementedError()
-
-    def add_prometheus(self, spec: ServiceSpec) -> Completion[List[str]]:
-        """Create new prometheus daemon"""
         raise NotImplementedError()
 
     def apply_prometheus(self, spec: ServiceSpec) -> Completion[str]:
         """Update prometheus cluster"""
         raise NotImplementedError()
 
-    def add_node_exporter(self, spec: ServiceSpec) -> Completion[List[str]]:
-        """Create a new Node-Exporter service"""
-        raise NotImplementedError()
-
     def apply_node_exporter(self, spec: ServiceSpec) -> Completion[str]:
         """Update existing a Node-Exporter daemon(s)"""
-        raise NotImplementedError()
-
-    def add_crash(self, spec: ServiceSpec) -> Completion[List[str]]:
-        """Create a new crash service"""
         raise NotImplementedError()
 
     def apply_crash(self, spec: ServiceSpec) -> Completion[str]:
         """Update existing a crash daemon(s)"""
         raise NotImplementedError()
 
-    def add_grafana(self, spec: ServiceSpec) -> Completion[List[str]]:
-        """Create a new grafana service"""
-        raise NotImplementedError()
-
     def apply_grafana(self, spec: ServiceSpec) -> Completion[str]:
         """Update existing a grafana service"""
         raise NotImplementedError()
 
-    def add_alertmanager(self, spec: ServiceSpec) -> Completion[List[str]]:
-        """Create a new AlertManager service"""
-        raise NotImplementedError()
-
     def apply_alertmanager(self, spec: ServiceSpec) -> Completion[str]:
         """Update an existing AlertManager daemon(s)"""
-        raise NotImplementedError()
-
-    def add_cephadm_exporter(self, spec: ServiceSpec) -> Completion[List[str]]:
-        """Create a new cephadm exporter daemon"""
         raise NotImplementedError()
 
     def apply_cephadm_exporter(self, spec: ServiceSpec) -> Completion[str]:

ceph/ceph

(cherry picked from commit 175ee8390985ae12b1b67d602338a4d1b1c81b52)

src/pybind/mgr/orchestrator/_interface.py
@@ -576,32 +576,20 @@ class Orchestrator(object):
         """Zap/Erase a device (DESTROYS DATA)"""
         raise NotImplementedError()
 
-    def add_mon(self, spec: ServiceSpec) -> OrchResult[List[str]]:
-        """Create mon daemon(s)"""
+    def add_daemon(self, spec: ServiceSpec) -> OrchResult[List[str]]:
+        """Create daemons daemon(s) for unmanaged services"""
         raise NotImplementedError()
 
     def apply_mon(self, spec: ServiceSpec) -> OrchResult[str]:
         """Update mon cluster"""
         raise NotImplementedError()
 
-    def add_mgr(self, spec: ServiceSpec) -> OrchResult[List[str]]:
-        """Create mgr daemon(s)"""
-        raise NotImplementedError()
-
     def apply_mgr(self, spec: ServiceSpec) -> OrchResult[str]:
         """Update mgr cluster"""
         raise NotImplementedError()
 
-    def add_mds(self, spec: ServiceSpec) -> OrchResult[List[str]]:
-        """Create MDS daemon(s)"""
-        raise NotImplementedError()
-
     def apply_mds(self, spec: ServiceSpec) -> OrchResult[str]:
         """Update MDS cluster"""
-        raise NotImplementedError()
-
-    def add_rgw(self, spec: RGWSpec) -> OrchResult[List[str]]:
-        """Create RGW daemon(s)"""
         raise NotImplementedError()
 
     def apply_rgw(self, spec: RGWSpec) -> OrchResult[str]:
@@ -612,72 +600,36 @@ class Orchestrator(object):
         """Update ha-rgw daemons"""
         raise NotImplementedError()
 
-    def add_rbd_mirror(self, spec: ServiceSpec) -> OrchResult[List[str]]:
-        """Create rbd-mirror daemon(s)"""
-        raise NotImplementedError()
-
     def apply_rbd_mirror(self, spec: ServiceSpec) -> OrchResult[str]:
         """Update rbd-mirror cluster"""
-        raise NotImplementedError()
-
-    def add_nfs(self, spec: NFSServiceSpec) -> OrchResult[List[str]]:
-        """Create NFS daemon(s)"""
         raise NotImplementedError()
 
     def apply_nfs(self, spec: NFSServiceSpec) -> OrchResult[str]:
         """Update NFS cluster"""
         raise NotImplementedError()
 
-    def add_iscsi(self, spec: IscsiServiceSpec) -> OrchResult[List[str]]:
-        """Create iscsi daemon(s)"""
-        raise NotImplementedError()
-
     def apply_iscsi(self, spec: IscsiServiceSpec) -> OrchResult[str]:
         """Update iscsi cluster"""
-        raise NotImplementedError()
-
-    def add_prometheus(self, spec: ServiceSpec) -> OrchResult[List[str]]:
-        """Create new prometheus daemon"""
         raise NotImplementedError()
 
     def apply_prometheus(self, spec: ServiceSpec) -> OrchResult[str]:
         """Update prometheus cluster"""
         raise NotImplementedError()
 
-    def add_node_exporter(self, spec: ServiceSpec) -> OrchResult[List[str]]:
-        """Create a new Node-Exporter service"""
-        raise NotImplementedError()
-
     def apply_node_exporter(self, spec: ServiceSpec) -> OrchResult[str]:
         """Update existing a Node-Exporter daemon(s)"""
-        raise NotImplementedError()
-
-    def add_crash(self, spec: ServiceSpec) -> OrchResult[List[str]]:
-        """Create a new crash service"""
         raise NotImplementedError()
 
     def apply_crash(self, spec: ServiceSpec) -> OrchResult[str]:
         """Update existing a crash daemon(s)"""
         raise NotImplementedError()
 
-    def add_grafana(self, spec: ServiceSpec) -> OrchResult[List[str]]:
-        """Create a new grafana service"""
-        raise NotImplementedError()
-
     def apply_grafana(self, spec: ServiceSpec) -> OrchResult[str]:
         """Update existing a grafana service"""
         raise NotImplementedError()
 
-    def add_alertmanager(self, spec: ServiceSpec) -> OrchResult[List[str]]:
-        """Create a new AlertManager service"""
-        raise NotImplementedError()
-
     def apply_alertmanager(self, spec: ServiceSpec) -> OrchResult[str]:
         """Update an existing AlertManager daemon(s)"""
-        raise NotImplementedError()
-
-    def add_cephadm_exporter(self, spec: ServiceSpec) -> OrchResult[List[str]]:
-        """Create a new cephadm exporter daemon"""
         raise NotImplementedError()
 
     def apply_cephadm_exporter(self, spec: ServiceSpec) -> OrchResult[str]: