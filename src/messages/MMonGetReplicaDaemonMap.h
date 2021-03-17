390c3c4ef3c0c7e450fe18b9d9f4fdfe4475ea9b
Changcheng Liu <changcheng.liu@aliyun.com>
2021-03-17 14:01:28 +0800

    ReqReplicaDaemonInfo: support client to request daemon info
    Signed-off-by: Changcheng Liu <changcheng.liu@aliyun.com>

--- src/messages/MMonGetReplicaDaemonMap.h
+++ src/messages/MMonGetReplicaDaemonMap.h
@@ -14,15 +14,52 @@
 #include "msg/Message.h"
 #include "messages/PaxosServiceMessage.h"
 
+struct ReqReplicaDaemonInfo {
+  int32_t replicas;
+  uint64_t replica_size;
+
+  ReqReplicaDaemonInfo(int32_t replicas = 0, uint64_t replica_size = 0):
+    replicas(replicas), replica_size(replica_size) {
+  }
+
+  void encode(bufferlist& req_bl, uint64_t features = 0) const {
+    using ceph::encode;
+    ENCODE_START(0, 0, req_bl);
+
+    encode(replicas, req_bl);
+    encode(replica_size, req_bl);
+
+    ENCODE_FINISH(req_bl);
+  }
+
+  void decode(bufferlist::const_iterator& req_bl_it) {
+    using ceph::encode;
+    DECODE_START(0, req_bl_it);
+
+    decode(replicas, req_bl_it);
+    decode(replica_size, req_bl_it);
+
+    DECODE_FINISH(req_bl_it);
+  }
+
+  void decode(bufferlist& req_bl) {
+    auto bl_it = req_bl.cbegin();
+    decode(bl_it);
+  }
+};
+WRITE_CLASS_ENCODER(ReqReplicaDaemonInfo)
+
 class MMonGetReplicaDaemonMap : public PaxosServiceMessage {
 private:
   static constexpr int HEADER_VERSION = 0;
   static constexpr int COMPAT_VERSION = 0;
 
 public:
-  MMonGetReplicaDaemonMap()
+  ReqReplicaDaemonInfo req_daemon_info;
+  MMonGetReplicaDaemonMap(int32_t replicas = 0, uint64_t replica_size = 0)
     : PaxosServiceMessage{CEPH_MSG_MON_GET_REPLICADAEMONMAP, 0,
-                          HEADER_VERSION, COMPAT_VERSION} {
+                          HEADER_VERSION, COMPAT_VERSION},
+      req_daemon_info(replicas, replica_size) {
   }
 
   ~MMonGetReplicaDaemonMap() override {
@@ -37,13 +74,17 @@ public:
   }
 
   void encode_payload(uint64_t features) override {
+    header.version = HEADER_VERSION;
+    header.compat_version = COMPAT_VERSION;
     using ceph::encode;
     paxos_encode();
+    encode(req_daemon_info, payload);
   }
   void decode_payload() override {
     using ceph::decode;
     auto p = payload.cbegin();
     paxos_decode(p);
+    decode(req_daemon_info, p);
   }
 private:
   template<class T, typename... Args>
