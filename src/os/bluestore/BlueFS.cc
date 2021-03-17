90e24ee455bcca04fa650f0e66b7ffcd8ae18de5
Sven Wegener <sven.wegener@stealer.net>
2021-03-17 09:49:44 +0100

    bluefs: fix assert always being true
    Signed-off-by: Sven Wegener <sven.wegener@stealer.net>

--- src/os/bluestore/BlueFS.cc
+++ src/os/bluestore/BlueFS.cc
@@ -1692,7 +1692,7 @@ int BlueFS::device_migrate_to_new(
 
   dout(10) << __func__ << " devs_source " << devs_source
 	   << " dev_target " << dev_target << dendl;
-  assert(dev_target == (int)BDEV_NEWDB || (int)BDEV_NEWWAL);
+  assert(dev_target == (int)BDEV_NEWDB || dev_target == (int)BDEV_NEWWAL);
 
   int flags = 0;
 
