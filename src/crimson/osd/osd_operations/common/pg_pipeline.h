76d24a995c94e55a9e5276e698ef04c4004d95c3
Radoslaw Zarzynski <rzarzyns@redhat.com>
2021-03-18 09:41:55 +0000

    crimson/osd: split ClientRequest::PGPipeline into CommonPGPipeline.
    This is another step towards the `InternalClientRequst`.
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

---
+++ src/crimson/osd/osd_operations/common/pg_pipeline.h
@@ -0,0 +1,27 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#pragma once
+
+#include "osd/osd_op_util.h"
+#include "crimson/osd/osd_operation.h"
+
+namespace crimson::osd {
+
+class CommonPGPipeline {
+protected:
+  OrderedExclusivePhase wait_for_active = {
+    "CommonPGPipeline:::wait_for_active"
+  };
+  OrderedExclusivePhase recover_missing = {
+    "CommonPGPipeline::recover_missing"
+  };
+  OrderedExclusivePhase get_obc = {
+    "CommonPGPipeline::get_obc"
+  };
+  OrderedExclusivePhase process = {
+    "CommonPGPipeline::process"
+  };
+};
+
+} // namespace crimson::osd
