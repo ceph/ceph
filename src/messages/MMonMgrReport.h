8d53b058ff5c76615cd375d1cc3ac7e2b6c0bba1
Sage Weil <sage@newdream.net>
2021-03-18 11:45:48 -0500

    mon/MgrStatMonitor: ignore MMgrReport from non-active mgr
    If it's not the active mgr, we should ignore it.
    
    Since the mgr instance is best identified by the gid, add that to the
    message.  (We can't use the source_addrs for the message since that is
    the MgrStandby monc addr, not the active mgr addrs in the MgrMap.)
    
    This fixes a problem where a just-demoted mgr report gets processed and a
    new mgr gets a ServiceMap with an epoch >= its pending map.  (At least,
    that is my theory!)
    
    Fixes: https://tracker.ceph.com/issues/48022
    Signed-off-by: Sage Weil <sage@newdream.net>
    (cherry picked from commit 4d447092c3542bf57dfb4942db766adf2923c069)
    
    Conflicts:
    	src/messages/MMonMgrReport.h
    	src/mon/MgrStatMonitor.cc

--- src/messages/MMonMgrReport.h
+++ src/messages/MMonMgrReport.h
@@ -23,7 +23,7 @@
 
 class MMonMgrReport : public PaxosServiceMessage {
 private:
-  static constexpr int HEAD_VERSION = 2;
+  static constexpr int HEAD_VERSION = 3;
   static constexpr int COMPAT_VERSION = 1;
 
 public:
@@ -31,6 +31,7 @@ public:
   health_check_map_t health_checks;
   bufferlist service_map_bl;  // encoded ServiceMap
   std::map<std::string,ProgressEvent> progress_events;
+  uint64_t gid = 0;
 
   MMonMgrReport()
     : PaxosServiceMessage{MSG_MON_MGR_REPORT, 0, HEAD_VERSION, COMPAT_VERSION}
@@ -41,8 +42,9 @@ private:
 public:
   std::string_view get_type_name() const override { return "monmgrreport"; }
 
-  void print(ostream& out) const override {
-    out << get_type_name() << "(" << health_checks.checks.size() << " checks, "
+  void print(std::ostream& out) const override {
+    out << get_type_name() << "(gid " << gid
+	<< ", " << health_checks.checks.size() << " checks, "
 	<< progress_events.size() << " progress events)";
   }
 
@@ -52,6 +54,7 @@ public:
     encode(health_checks, payload);
     encode(service_map_bl, payload);
     encode(progress_events, payload);
+    encode(gid, payload);
 
     if (!HAVE_FEATURE(features, SERVER_NAUTILUS) ||
 	!HAVE_FEATURE(features, SERVER_MIMIC)) {
@@ -77,6 +80,9 @@ public:
     decode(service_map_bl, p);
     if (header.version >= 2) {
       decode(progress_events, p);
+    }
+    if (header.version >= 3) {
+      decode(gid, p);
     }
   }
 private:
