fe30427f6a97ecf844bdde8ccefea13dc645c445
Yan, Zheng <yanzheng03@kuaishou.com>
2021-03-24 14:30:27 +0800

    mds: optimize sending cap messages
    Signed-off-by: "Yan, Zheng" <yanzheng03@kuaishou.com>

--- src/mds/Locker.cc
+++ src/mds/Locker.cc
@@ -2443,7 +2443,7 @@ void Locker::issue_truncate(CInode *in)
                                        cap->get_mseq(),
                                        mds->get_osd_epoch_barrier());
     in->encode_cap_message(m, cap);			     
-    mds->send_message_client_counted(m, p.first);
+    mds->send_message_client_counted(m, cap->get_session());
   }
 
   // should we increase max_size?
@@ -2931,7 +2931,7 @@ void Locker::share_inode_max_size(CInode *in, Capability *only_cap)
                                          cap->get_mseq(),
                                          mds->get_osd_epoch_barrier());
       in->encode_cap_message(m, cap);
-      mds->send_message_client_counted(m, client);
+      mds->send_message_client_counted(m, cap->get_session());
     }
     if (only_cap)
       break;
