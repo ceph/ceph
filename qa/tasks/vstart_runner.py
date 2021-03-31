50f050194a03b51e9afcd45ecb8f03d53767b96b
Alfonso Martínez <almartin@redhat.com>
2021-03-31 10:11:50 +0200

    qa/tasks/vstart_runner.py: start max required mgrs
    Pass environment copy with max required mgrs when shell kwarg is True.
    
    Fixes: https://tracker.ceph.com/issues/50077
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>
    (cherry picked from commit 45e1134e3b36ca103cef727103905d3db2960758)

--- qa/tasks/vstart_runner.py
+++ qa/tasks/vstart_runner.py
@@ -373,6 +373,7 @@ class LocalRemote(object):
                                        stderr=subprocess.PIPE,
                                        stdin=subprocess.PIPE,
                                        cwd=cwd,
+                                       env=env,
                                        shell=True)
         else:
             # Sanity check that we've got a list of strings
