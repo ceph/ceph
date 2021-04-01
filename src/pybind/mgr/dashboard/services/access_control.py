ceph/ceph

commit 2cd94293268116838c3ddcebdedde4fbd9cb93aa

mgr/dashboard: ensure password is a string before encoding it
otherwise we have following failure:

AttributeError: 'bytes' object has no attribute 'encode'

this change is not cherry-picked from master, as master has dropped
python2 support.

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/pybind/mgr/dashboard/services/access_control.py
@@ -7,7 +7,6 @@ import errno
 import json
 import threading
 import time
-import six
 
 import bcrypt
 
@@ -15,6 +14,7 @@ from mgr_module import CLICheckNonemptyFileInput, CLIReadCommand, CLIWriteComman
 
 from .. import mgr, logger
 from ..security import Scope, Permission
+from ..tools import ensure_str
 from ..exceptions import RoleAlreadyExists, RoleDoesNotExist, ScopeNotValid, \
                          PermissionNotValid, RoleIsAssociatedWithUser, \
                          UserAlreadyExists, UserDoesNotExist, ScopeNotInRole, \
@@ -25,8 +25,7 @@ from ..exceptions import RoleAlreadyExists, RoleDoesNotExist, ScopeNotValid, \
 def password_hash(password, salt_password=None):
     if not password:
         return None
-    if six.PY2:
-        password = unicode(password, 'utf-8') if isinstance(password, str) else password
+    password = ensure_str(password)
     if not salt_password:
         salt_password = bcrypt.gensalt()
     else:

commit e2614de5d3ebb8b240facbc5e7ef3fc6293fbe12

mgr/dashboard: do not import tools in access_control
this addresses a regression introduced by
2cd94293268116838c3ddcebdedde4fbd9cb93aa. which

from ..tools import ensure_str

and it causes recursive import.

so, in this change, an copy of ensure_str() is added
to access_control.py

this change is not cherry-picked from master, as the offending commit
which is fixed by this change is not included in master.

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/pybind/mgr/dashboard/services/access_control.py
@@ -7,6 +7,7 @@ import errno
 import json
 import threading
 import time
+import six
 
 import bcrypt
 
@@ -14,11 +15,23 @@ from mgr_module import CLICheckNonemptyFileInput, CLIReadCommand, CLIWriteComman
 
 from .. import mgr, logger
 from ..security import Scope, Permission
-from ..tools import ensure_str
 from ..exceptions import RoleAlreadyExists, RoleDoesNotExist, ScopeNotValid, \
                          PermissionNotValid, RoleIsAssociatedWithUser, \
                          UserAlreadyExists, UserDoesNotExist, ScopeNotInRole, \
                          RoleNotInUser
+
+
+# replicates tools.ensure_str() to avoid recursive import:
+# ..tools -> .services.auth -> .access_control -> ..tools
+def ensure_str(s, encoding='utf-8', errors='strict'):
+    """Ported from six."""
+    if not isinstance(s, (six.text_type, six.binary_type)):
+        raise TypeError("not expecting type '%s'" % type(s))
+    if six.PY2 and isinstance(s, six.text_type):
+        s = s.encode(encoding, errors)
+    elif six.PY3 and isinstance(s, six.binary_type):
+        s = s.decode(encoding, errors)
+    return s
 
 
 # password hashing algorithm