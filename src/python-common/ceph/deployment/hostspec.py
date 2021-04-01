be79dc5704c507f4d4081e1619bd93ea91fe59e4
Daniel Pivonka <dpivonka@redhat.com>
2021-04-01 13:56:48 -0400

    mgr/orchestrator: validate lists in spec jsons
    Signed-off-by: Daniel Pivonka <dpivonka@redhat.com>
    (cherry picked from commit 7844ce0785595c951f5822d2c38d1381dc13c8c1)

--- src/python-common/ceph/deployment/hostspec.py
+++ src/python-common/ceph/deployment/hostspec.py
@@ -1,7 +1,20 @@
+import errno
 try:
     from typing import Optional, List, Any
 except ImportError:
     pass  # just for type checking
+
+
+class SpecValidationError(Exception):
+    """
+    Defining an exception here is a bit problematic, cause you cannot properly catch it,
+    if it was raised in a different mgr module.
+    """
+    def __init__(self,
+                 msg: str,
+                 errno: int = -errno.EINVAL):
+        super(SpecValidationError, self).__init__(msg)
+        self.errno = errno
 
 
 class HostSpec(object):
@@ -38,11 +51,24 @@ class HostSpec(object):
 
     @classmethod
     def from_json(cls, host_spec: dict) -> 'HostSpec':
+        host_spec = cls.normalize_json(host_spec)
         _cls = cls(host_spec['hostname'],
                    host_spec['addr'] if 'addr' in host_spec else None,
                    list(set(host_spec['labels'])) if 'labels' in host_spec else None,
                    host_spec['status'] if 'status' in host_spec else None)
         return _cls
+
+    @staticmethod
+    def normalize_json(host_spec: dict) -> dict:
+        labels = host_spec.get('labels')
+        if labels is None:
+            return host_spec
+        if isinstance(labels, list):
+            return host_spec
+        if not isinstance(labels, str):
+            raise SpecValidationError(f'Labels ({labels}) must be a string or list of strings')
+        host_spec['labels'] = [labels]
+        return host_spec
 
     def __repr__(self) -> str:
         args = [self.hostname]  # type: List[Any]
