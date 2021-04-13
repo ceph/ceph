2d9bdaa477f8d0370a8c56ad8bd843e066cc2d3d
Kamoltat <ksirivad@redhat.com>
2021-04-13 21:12:40 +0000

    pybind/mgr/pg_autoscaler: Added autoscale-profile feature
    The autoscaler by default will start out each pool with minimal
    pgs and `scale-up` the pgs when there is more usage in each pool.
    
    Users can now use the commands:
    
    `osd pool set autoscale-profile scale-down` to make the pools
    start out with a full complement of pgs and only `scale-down`
    when usage ratio across the pools are not even.
    
    `osd pool set autoscale-profile scale-up` (by default) to make the pools
    start out with minimal pgs and `scale-up` the pgs when there
    is more usage in each pool.
    
    Edited KVMonitor.cc file to make the `autoscale_profile` variable
    persistent.
    
    Edited tests/test_cal_final_pg_target.py so that it takes into account
    the new `profile` argument when calling cal_final_pg_target(). Also,
    added some new test cases for when profile is `scale-up`
    
    Renamed tests/test_autoscaler.py to a more appropriate name:
    tests/test_cal_ratio.py
    
    Signed-off-by: Kamoltat <ksirivad@redhat.com>
    (cherry picked from commit 0e49683b538026e4c51a040950da3dbb9c7e92d5)

--- src/pybind/mgr/pg_autoscaler/tests/test_cal_final_pg_target.py
+++ src/pybind/mgr/pg_autoscaler/tests/test_cal_final_pg_target.py
@@ -17,30 +17,107 @@ class RootMapItem:
 class TestPgAutoscaler(object):
 
     def setup(self):
-        # a bunch of attributes for testing
+        # a bunch of attributes for testing.
         self.autoscaler = module.PgAutoscaler('module_name', 0, 0)
 
-    def helper_test(self, pools, root_map, bias):
-
+    def helper_test(self, pools, root_map, bias, profile):
+        # Here we simulate how _calc_pool_target() works.
         even_pools = {}
         for pool_name, p in pools.items():
-            final_ratio, pool_pg_target, final_pg_target = self.autoscaler._calc_final_pg_target(p, pool_name, root_map, p['root_id'], p['capacity_ratio'], even_pools, bias, True)
-            
+            final_ratio, pool_pg_target, final_pg_target = self.autoscaler._calc_final_pg_target(p, pool_name, root_map,
+                p['root_id'], p['capacity_ratio'], even_pools, bias, True, profile)
+
             if final_ratio == None:
+                # no final_ratio means current pool is an even pool
+                # and we do not have to do any assertion on it.
+                # You will never hit this case with a scale up profile. 
                 continue
 
             assert p['expected_final_pg_target'] == final_pg_target
             assert p['expected_final_ratio'] == final_ratio
-            assert not p['even_pools'] and pool_name not in even_pools  
 
-        for pool_name, p in even_pools.items():
-            final_ratio, pool_pg_target, final_pg_target = self.autoscaler._calc_final_pg_target(p, pool_name, root_map, p['root_id'], p['capacity_ratio'], even_pools, bias, False) 
+            if profile == "scale-down":
+                # We only care about even_pools when profile is a scale-down
+                assert not p['even_pools'] and pool_name not in even_pools 
 
-            assert p['expected_final_pg_target'] == final_pg_target
-            assert p['expected_final_ratio'] == final_ratio
-            assert p['even_pools'] and pool_name in even_pools
+        if profile == "scale-down":
+            for pool_name, p in even_pools.items():
+                final_ratio, pool_pg_target, final_pg_target = self.autoscaler._calc_final_pg_target(p, pool_name, root_map,
+                    p['root_id'], p['capacity_ratio'], even_pools, bias, False, profile)
 
-    def test_all_even_pools(self):
+                assert p['expected_final_pg_target'] == final_pg_target
+                assert p['expected_final_ratio'] == final_ratio
+                assert p['even_pools'] and pool_name in even_pools
+
+    def test_all_even_pools_scale_up(self):
+        pools = {
+
+                "test0":{
+
+                    "pool": 0,
+                    "pool_name": "test0",
+                    "pg_num_target": 32,
+                    "capacity_ratio": 0.2,
+                    "root_id":"0",
+                    "expected_final_pg_target": 64,
+                    "expected_final_ratio": 0.2,
+                    "even_pools": True,
+                    "size": 1,
+                    },
+
+                "test1":{
+
+                    "pool": 1,
+                    "pool_name": "test1",
+                    "pg_num_target": 32,
+                    "capacity_ratio": 0.2,
+                    "root_id":"0",
+                    "expected_final_pg_target": 64,
+                    "expected_final_ratio": 0.2,
+                    "even_pools": True,
+                    "size": 1,
+                    },
+
+                "test2":{
+
+                    "pool": 2,
+                    "pool_name": "test2",
+                    "pg_num_target": 32,
+                    "capacity_ratio": 0.2,
+                    "root_id":"0",
+                    "expected_final_pg_target": 64,
+                    "expected_final_ratio": 0.2,
+                    "even_pools": True,
+                    "size": 1,
+                    },
+
+                "test3":{
+
+                    "pool": 3,
+                    "pool_name": "test3",
+                    "pg_num_target": 32,
+                    "capacity_ratio": 0.1,
+                    "root_id": "0",
+                    "expected_final_pg_target": 32,
+                    "expected_final_ratio": 0.1,
+                    "even_pools": True,
+                    "size": 1,
+                    },
+
+                }
+
+        root_map = {
+
+                "0": RootMapItem(4, 400, 400),
+                "1": RootMapItem(4, 400, 400),
+
+                }
+
+        profile = "scale-up"
+        bias = 1
+        self.helper_test(pools, root_map, bias, profile)
+
+    def test_all_even_pools_scale_down(self):
         pools = {
 
                 "test0":{
@@ -104,10 +181,79 @@ class TestPgAutoscaler(object):
 
                 }
 
+        profile = "scale-down" 
         bias = 1
-        self.helper_test(pools, root_map, bias)
+        self.helper_test(pools, root_map, bias, profile)
 
-    def test_uneven_pools(self):
+    def test_uneven_pools_scale_up(self):
+        pools = {
+
+                "test0":{
+
+                    "pool": 0,
+                    "pool_name": "test0",
+                    "pg_num_target": 32,
+                    "capacity_ratio": 0.1,
+                    "root_id":"0",
+                    "expected_final_pg_target": 32,
+                    "expected_final_ratio": 0.1,
+                    "even_pools": True,
+                    "size": 1,
+                    },
+
+                "test1":{
+
+                    "pool": 1,
+                    "pool_name": "test1",
+                    "pg_num_target": 32,
+                    "capacity_ratio": 0.5,
+                    "root_id":"0",
+                    "expected_final_pg_target": 256,
+                    "expected_final_ratio": 0.5,
+                    "even_pools": False,
+                    "size": 1,
+                    },
+
+                "test2":{
+
+                    "pool": 2,
+                    "pool_name": "test2",
+                    "pg_num_target": 32,
+                    "capacity_ratio": 0.1,
+                    "root_id":"0",
+                    "expected_final_pg_target": 32,
+                    "expected_final_ratio": 0.1,
+                    "even_pools": True,
+                    "size": 1,
+                    },
+
+                "test3":{
+
+                    "pool": 3,
+                    "pool_name": "test3",
+                    "pg_num_target": 32,
+                    "capacity_ratio": 0.1,
+                    "root_id": "0",
+                    "expected_final_pg_target": 32,
+                    "expected_final_ratio": 0.1,
+                    "even_pools": True,
+                    "size": 1,
+                    },
+
+                }
+
+        root_map = {
+
+                "0": RootMapItem(4, 400, 400),
+                "1": RootMapItem(4, 400, 400),
+
+                }
+
+        profile = "scale-up"
+        bias = 1
+        self.helper_test(pools, root_map, bias, profile)
+
+    def test_uneven_pools_scale_down(self):
         pools = {
 
                 "test0":{
@@ -171,10 +317,92 @@ class TestPgAutoscaler(object):
 
                 }
 
+        profile = "scale-down" 
         bias = 1
-        self.helper_test(pools, root_map, bias)
+        self.helper_test(pools, root_map, bias, profile)
 
-    def test_uneven_pools_with_diff_roots(self):
+    def test_uneven_pools_with_diff_roots_scale_up(self):
+        pools = {
+
+                "test0":{
+
+                    "pool": 0,
+                    "pool_name": "test0",
+                    "pg_num_target": 32,
+                    "capacity_ratio": 0.4,
+                    "root_id":"0",
+                    "expected_final_pg_target": 2048,
+                    "expected_final_ratio": 0.4,
+                    "even_pools": False,
+                    "size": 1,
+                    },
+
+                "test1":{
+
+                    "pool": 1,
+                    "pool_name": "test1",
+                    "pg_num_target": 32,
+                    "capacity_ratio": 0.6,
+                    "root_id":"1",
+                    "expected_final_pg_target": 2048,
+                    "expected_final_ratio": 0.6,
+                    "even_pools": False,
+                    "size": 1,
+                    },
+
+                "test2":{
+
+                    "pool": 2,
+                    "pool_name": "test2",
+                    "pg_num_target": 32,
+                    "capacity_ratio": 0.5,
+                    "root_id":"0",
+                    "expected_final_pg_target": 2048,
+                    "expected_final_ratio": 0.5,
+                    "even_pools": False,
+                    "size": 1,
+                    },
+
+                "test3":{
+
+                    "pool": 3,
+                    "pool_name": "test3",
+                    "pg_num_target": 32,
+                    "capacity_ratio": 0.1,
+                    "root_id": "0",
+                    "expected_final_pg_target": 512,
+                    "expected_final_ratio": 0.1,
+                    "even_pools": True,
+                    "size": 1,
+                    },
+
+                "test4":{
+
+                    "pool": 4,
+                    "pool_name": "test4",
+                    "pg_num_target": 32,
+                    "capacity_ratio": 0.4,
+                    "root_id": "1",
+                    "expected_final_pg_target": 2048,
+                    "expected_final_ratio": 0.4,
+                    "even_pools": True,
+                    "size": 1,
+                    },
+
+                }
+
+        root_map = {
+
+                "0": RootMapItem(3, 5000, 5000),
+                "1": RootMapItem(2, 5000, 5000),
+
+                }
+
+        profile = "scale-up"
+        bias = 1
+        self.helper_test(pools, root_map, bias, profile)
+
+    def test_uneven_pools_with_diff_roots_scale_down(self):
         pools = {
 
                 "test0":{
@@ -251,5 +479,6 @@ class TestPgAutoscaler(object):
 
                 }
 
+        profile = "scale-down"
         bias = 1
-        self.helper_test(pools, root_map, bias)
+        self.helper_test(pools, root_map, bias, profile)
