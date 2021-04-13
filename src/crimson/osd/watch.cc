31cab8821d2129128c6fc69b47ae13cb0846f905
Amnon Hanuhov <ahanukov@redhat.com>
2021-04-13 15:27:15 +0300

    crimson/osd: Use crimson::net::make_message() in Watch
    Signed-off-by: Amnon Hanuhov <ahanukov@redhat.com>

--- src/crimson/osd/watch.cc
+++ src/crimson/osd/watch.cc
@@ -36,7 +36,7 @@ seastar::future<> Watch::connect(crimson::net::ConnectionRef conn, bool)
 seastar::future<> Watch::send_notify_msg(NotifyRef notify)
 {
   logger().info("{} for notify(id={})", __func__, notify->ninfo.notify_id);
-  return conn->send(make_message<MWatchNotify>(
+  return conn->send(crimson::net::make_message<MWatchNotify>(
     winfo.cookie,
     notify->user_version,
     notify->ninfo.notify_id,
@@ -75,7 +75,7 @@ seastar::future<> Watch::send_disconnect_msg()
     return seastar::now();
   }
   ceph::bufferlist empty;
-  return conn->send(make_message<MWatchNotify>(
+  return conn->send(crimson::net::make_message<MWatchNotify>(
     winfo.cookie,
     0,
     0,
@@ -183,7 +183,7 @@ seastar::future<> Notify::send_completion(
   logger().debug("{} sending notify replies: {}", __func__, notify_replies);
 
   ceph::bufferlist empty;
-  auto reply = make_message<MWatchNotify>(
+  auto reply = crimson::net::make_message<MWatchNotify>(
     ninfo.cookie,
     user_version,
     ninfo.notify_id,
