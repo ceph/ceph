bfdca7fa014dcc405f75a97381aceb773e049aab
Kefu Chai <kchai@redhat.com>
2021-04-13 09:15:17 +0800

    common/options: s/immutable-objet-cache/immutable-object-cache/
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/common/options/build_options.cc
+++ src/common/options/build_options.cc
@@ -26,7 +26,7 @@ std::vector<Option> build_options()
   ingest(get_rgw_options(), "rgw");
   ingest(get_rbd_options(), "rbd");
   ingest(get_rbd_mirror_options(), "rbd-mirror");
-  ingest(get_immutable_object_cache_options(), "immutable-objet-cache");
+  ingest(get_immutable_object_cache_options(), "immutable-object-cache");
   ingest(get_mds_options(), "mds");
   ingest(get_mds_client_options(), "mds_client");
   ingest(get_cephfs_mirror_options(), "cephfs-mirror");
