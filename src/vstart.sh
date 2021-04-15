88d140ef8f0c73bbd083f06117f12f1c59e9a908
Kefu Chai <kchai@redhat.com>
2021-04-15 21:07:53 +0800

    vstart.sh: disable "auth_allow_insecure_global_id_reclaim"
    to silence the health warning of "mons are allowing insecure global_id
    reclaim", which prevents the cluster from being active+clean. couple
    tests are expecting a warning free cluster before they starts.
    
    as this option is enabled by default for appeasing the old clients, but when it
    comes to most of upstream testing, we can just disable it.
    
    Fixes: https://tracker.ceph.com/issues/50374
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/vstart.sh
+++ src/vstart.sh
@@ -741,6 +741,7 @@ $CMONDEBUG
         $(format_conf "${extra_conf}")
         mon cluster log file = $CEPH_OUT_DIR/cluster.mon.\$id.log
         osd pool default erasure code profile = plugin=jerasure technique=reed_sol_van k=2 m=1 crush-failure-domain=osd
+        auth allow insecure global id reclaim = false
 EOF
 }
 
