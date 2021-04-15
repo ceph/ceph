7c9032a07f6c60afb9422e73bf82d781a771892c
Kefu Chai <kchai@redhat.com>
2021-04-15 13:31:25 +0800

    rgw: cast rgw_max_chunk_size to off_t
    as the left-hand operator is promoted to off_t which is a signed
    integer, while rgw_max_chunk_size will be an unsigned after the
    yaml-to-cxx migration. so let's cast it to `off_t` before comparing
    them.
    
    the same applies to rgw_copy_obj_progress_every_bytes.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -5053,8 +5053,10 @@ void RGWCopyObj::progress_cb(off_t ofs)
   if (!s->cct->_conf->rgw_copy_obj_progress)
     return;
 
-  if (ofs - last_ofs < s->cct->_conf->rgw_copy_obj_progress_every_bytes)
+  if (ofs - last_ofs <
+      static_cast<off_t>(s->cct->_conf->rgw_copy_obj_progress_every_bytes)) {
     return;
+  }
 
   send_partial_response(ofs);
 
