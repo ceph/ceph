ceph/ceph

commit c3bbb05deb3b224aad5fca57d7994d53f1931ba0

common: generate legacy_config_opts.h from .yaml.in files
* add a setting named "with_legacy" to .yaml.in files, so
  each option with a true "with_legacy" will have an entry
  in legacy_config_opts.h.
* preserve the comments from legacy_config_opts.h to .yaml.in,
  some of them are solely for developers, but some of them are
  good reading for users as well. we can use them for "desc"
  field in a follow-up change.
* move common/legacy_config_opts.h to common/options/legacy_config_opts.h
  as legacy_config_opts.h is "closer" to the options directory
  than other sources files under src/common.
* update y2c.py to generate separate .h files which are in turn
  included by legacy_config_opts.h
* add a target named "legacy-option-headers", and let
  some targets depend on it so that these headers generated by
  y2c.py can be generated before the .cc files including them
  are compiled.

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/common/options/y2c.py
@@ -6,6 +6,7 @@ import math
 import os
 import sys
 
+# flake8: noqa: E127
 
 def type_to_cxx(t):
     return f'Option::TYPE_{t.upper()}'
@@ -185,6 +186,21 @@ def yaml_to_cxx(opt, indent):
     return cxx
 
 
+def type_to_h(t):
+    if t == 'uint':
+        return 'OPT_U32'
+    return f'OPT_{t.upper()}'
+
+
+def yaml_to_h(opt):
+    if opt.get('with_legacy', False):
+        name = opt['name']
+        typ = opt['type']
+        htyp = type_to_h(typ)
+        return f'OPTION({name}, {htyp})'
+    else:
+        return ''
+
 TEMPLATE_CC = '''#include "common/options.h"
 {headers}
 
@@ -208,21 +224,24 @@ def translate(opts):
         name = os.path.split(opts.input)[-1]
         name = name.rsplit('.', 1)[0]
     name = name.replace('-', '_')
-    with open(opts.input) as infile, open(opts.output, 'w') as outfile:
+    # noqa: E127
+    with open(opts.input) as infile, \
+         open(opts.output, 'w') as cc_file, \
+         open(opts.legacy, 'w') as h_file:
         yml = yaml.safe_load(infile)
         headers = yml.get('headers', '')
-        outfile.write(prelude.format(name=name, headers=headers))
+        cc_file.write(prelude.format(name=name, headers=headers))
         options = yml['options']
         for option in options:
             try:
-                cxx = yaml_to_cxx(option, opts.indent)
-                outfile.write(cxx)
-                outfile.write('\n')
+                cc_file.write(yaml_to_cxx(option, opts.indent) + '\n')
+                if option.get('with_legacy', False):
+                    h_file.write(yaml_to_h(option) + '\n')
             except ValueError as e:
                 print(f'failed to translate option "{name}": {e}',
                       file=sys.stderr)
                 return 1
-        outfile.write(epilogue.replace("}}", "}"))
+        cc_file.write(epilogue.replace("}}", "}"))
 
 
 def readable_size(value, typ):
@@ -302,6 +321,9 @@ def main():
     parser.add_argument('-o', '--output', dest='output',
                         default='options',
                         help='the path to the generated .cc file')
+    parser.add_argument('--legacy', dest='legacy',
+                        default='legacy_options',
+                        help='the path to the generated legacy .h file')
     parser.add_argument('--indent', type=int,
                         default=4,
                         help='the number of spaces added before each line')

src/CMakeLists.txt
@@ -399,6 +399,7 @@ endif()
 set_source_files_properties(ceph_ver.c
   APPEND PROPERTY OBJECT_DEPENDS ${CMAKE_BINARY_DIR}/src/include/ceph_ver.h)
 add_library(common-objs OBJECT ${libcommon_files})
+add_dependencies(common-objs legacy-option-headers)
 
 if(WITH_JAEGER)
   find_package(yaml-cpp 0.6.0)

src/auth/CMakeLists.txt
@@ -22,3 +22,4 @@ endif()
 
 add_library(common-auth-objs OBJECT ${auth_srcs})
 target_include_directories(common-auth-objs PRIVATE ${OPENSSL_INCLUDE_DIR})
+add_dependencies(common-auth-objs legacy-option-headers)

src/common/CMakeLists.txt
@@ -6,6 +6,7 @@ add_library(common_texttable_obj OBJECT
 
 add_library(common_prioritycache_obj OBJECT
   PriorityCache.cc)
+add_dependencies(common_prioritycache_obj legacy-option-headers)
 
 if(WIN32)
   add_library(dlfcn_win32 STATIC win32/dlfcn.cc win32/errno.cc)
@@ -188,6 +189,7 @@ target_compile_definitions(common-common-objs PRIVATE
   "CMAKE_INSTALL_LIBDIR=\"${CMAKE_INSTALL_LIBDIR}\""
   "CEPH_INSTALL_FULL_PKGLIBDIR=\"${CEPH_INSTALL_FULL_PKGLIBDIR}\""
   "CEPH_INSTALL_DATADIR=\"${CEPH_INSTALL_DATADIR}\"")
+add_dependencies(common-common-objs legacy-option-headers)
 
 set(common_mountcephfs_srcs
   armor.c

src/common/config.cc
@@ -185,7 +185,7 @@ md_config_t::md_config_t(ConfigValues& values,
 #define OPTION(name, type) \
     {STRINGIFY(name), &ConfigValues::name},
 #define SAFE_OPTION(name, type) OPTION(name, type)
-#include "common/legacy_config_opts.h"
+#include "options/legacy_config_opts.h"
 #undef OPTION
 #undef SAFE_OPTION
   };

src/common/config_values.h
@@ -56,7 +56,7 @@ public:
 #define SAFE_OPTION(name, ty)       \
   protected:                        \
     OPTION_##ty(name)               
-#include "common/legacy_config_opts.h"
+#include "common/options/legacy_config_opts.h"
 #undef OPTION_OPT_INT
 #undef OPTION_OPT_LONGLONG
 #undef OPTION_OPT_STR

src/common/options/CMakeLists.txt
@@ -1,4 +1,5 @@
 set(common_options_srcs build_options.cc)
+set(legacy_options_headers)
 
 # to mimic the behavior of file(CONFIGURE ...)
 file(GENERATE OUTPUT configure_file.cmake
@@ -25,17 +26,20 @@ function(add_options name)
   set(yaml_file ${CMAKE_CURRENT_BINARY_DIR}/${name}.yaml)
   file_configure("${yaml_in_file}"
     "${yaml_file}" @ONLY)
-  set(src_file "${name}_options.cc")
-  add_custom_command(
-    OUTPUT ${src_file}
+  set(cc_file "${name}_options.cc")
+  set(h_file "${PROJECT_BINARY_DIR}/include/${name}_legacy_options.h")
+  add_custom_command(PRE_BUILD
+    OUTPUT ${cc_file} ${h_file}
     COMMAND ${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/y2c.py
       --input ${yaml_file}
-      --output ${src_file}
+      --output ${cc_file}
+      --legacy ${h_file}
       --name ${name}
-    DEPENDS ${yaml_file})
-  list(APPEND common_options_srcs
-    ${src_file})
+      DEPENDS ${yaml_file})
+  list(APPEND common_options_srcs ${cc_file})
   set(common_options_srcs ${common_options_srcs} PARENT_SCOPE)
+  list(APPEND legacy_options_headers ${h_file})
+  set(legacy_options_headers ${legacy_options_headers} PARENT_SCOPE)
 endfunction()
 
 set(osd_erasure_code_plugins "jerasure" "lrc")
@@ -92,3 +96,5 @@ add_options(rgw)
 
 add_library(common-options-objs OBJECT
   ${common_options_srcs})
+add_custom_target(legacy-option-headers
+  DEPENDS ${legacy_options_headers})

commit a7d843b195680d05fbdac401cb27b2e73bf286ca

common: make y2c.py choke on duplicate keys
Commit c3bbb05deb3b ("common: generate legacy_config_opts.h from
.yaml.in files") inadvertently reverted a change of a default value by
adding a second "default" key with the old value.  This was corrected
in commit 041d749dba16 ("common/options/global: correct default of
auth_mon_ticket_ttl"), but highlights that mis-merging a yaml file
is rather easy.

To prevent this happening again, fail the build if duplicate keys
exist in any of src/common/options/*.yaml.in files.

Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

src/common/options/y2c.py
@@ -201,6 +201,7 @@ def yaml_to_h(opt):
     else:
         return ''
 
+
 TEMPLATE_CC = '''#include "common/options.h"
 {headers}
 
@@ -210,6 +211,26 @@ std::vector<Option> get_{name}_options() {{
   }});
 }}
 '''
+
+
+# PyYAML doesn't check for duplicates even though the YAML spec says
+# that mapping keys must be unique and that duplicates must be treated
+# as an error.  See https://github.com/yaml/pyyaml/issues/165.
+#
+# This workaround breaks merge keys -- in "<<: *xyz", duplicate keys
+# from xyz mapping raise an error instead of being discarded.
+class UniqueKeySafeLoader(yaml.SafeLoader):
+    def construct_mapping(self, node, deep=False):
+        mapping = super().construct_mapping(node, deep)
+        keys = set()
+        for key_node, _ in node.value:
+            key = self.construct_object(key_node, deep=deep)
+            if key in keys:
+                raise yaml.constructor.ConstructorError(None, None,
+                                                        "found duplicate key",
+                                                        key_node.start_mark)
+            keys.add(key)
+        return mapping
 
 
 def translate(opts):
@@ -228,7 +249,7 @@ def translate(opts):
     with open(opts.input) as infile, \
          open(opts.output, 'w') as cc_file, \
          open(opts.legacy, 'w') as h_file:
-        yml = yaml.safe_load(infile)
+        yml = yaml.load(infile, Loader=UniqueKeySafeLoader)
         headers = yml.get('headers', '')
         cc_file.write(prelude.format(name=name, headers=headers))
         options = yml['options']
@@ -292,7 +313,7 @@ def readable_millisecs(value, typ):
 
 def readable(opts):
     with open(opts.input) as infile, open(opts.output, 'w') as outfile:
-        yml = yaml.safe_load(infile)
+        yml = yaml.load(infile, Loader=UniqueKeySafeLoader)
         options = yml['options']
         for option in options:
             typ = option['type']