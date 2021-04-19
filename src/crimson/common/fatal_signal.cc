cebb36aa84bae084a10e7d0e33c3f83f307d1a42
Radoslaw Zarzynski <rzarzyns@redhat.com>
2021-04-19 14:32:05 +0000

    crimson/common: don't use Seastar's handle_signal() for fatal signals.
    The `handle_signal()` way requires going through the reactor, and
    thus is unsuitable for situations when it's no longer operational.
    SIGSEGV generated as a result of actual fault (not `kill()`) is
    the intended audience of this change.
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/crimson/common/fatal_signal.cc
+++ src/crimson/common/fatal_signal.cc
@@ -3,7 +3,7 @@
 
 #include "fatal_signal.h"
 
-#include <signal.h>
+#include <csignal>
 #include <iostream>
 #include <string_view>
 
@@ -11,21 +11,43 @@
 
 FatalSignal::FatalSignal()
 {
-  auto fatal_signals = {SIGSEGV,
-                        SIGABRT,
-                        SIGBUS,
-                        SIGILL,
-                        SIGFPE,
-                        SIGXCPU,
-                        SIGXFSZ,
-                        SIGSYS};
-
-  for (auto signum : fatal_signals) {
-    seastar::engine().handle_signal(signum, [signum, this] {
-      signaled(signum);
-    });
-  }
+  install_oneshot_signals_handler<SIGSEGV,
+                                  SIGABRT,
+                                  SIGBUS,
+                                  SIGILL,
+                                  SIGFPE,
+                                  SIGXCPU,
+                                  SIGXFSZ,
+                                  SIGSYS>();
 }
+
+template <int... SigNums>
+void FatalSignal::install_oneshot_signals_handler()
+{
+  (install_oneshot_signal_handler<SigNums>() , ...);
+}
+
+template <int SigNum>
+void FatalSignal::install_oneshot_signal_handler()
+{
+  struct sigaction sa;
+  sa.sa_sigaction = [](int sig, siginfo_t *info, void *p) {
+    static std::atomic_bool handled = false;
+    if (handled.exchange(true)) {
+      return;
+    }
+    FatalSignal::signaled(sig);
+    ::signal(sig, SIG_DFL);
+  };
+  sigfillset(&sa.sa_mask);
+  sa.sa_flags = SA_SIGINFO | SA_RESTART;
+  if constexpr (SigNum == SIGSEGV) {
+      sa.sa_flags |= SA_ONSTACK;
+  }
+  [[maybe_unused]] auto r = ::sigaction(SigNum, &sa, nullptr);
+  assert(r);
+}
+
 
 static void print_with_backtrace(std::string_view cause) {
   std::cerr << cause;
@@ -43,9 +65,6 @@ static void print_with_backtrace(std::string_view cause) {
 
 void FatalSignal::signaled(int signum)
 {
-  if (handled.exchange(true)) {
-    return;
-  }
   switch (signum) {
   case SIGSEGV:
     print_with_backtrace("Aborting");
@@ -57,5 +76,4 @@ void FatalSignal::signaled(int signum)
     print_with_backtrace(fmt::format("Signal {}", signum));
     break;
   }
-  signal(signum, SIG_DFL);
 }
