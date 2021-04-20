c07b54a6b72af8d687408f65e47608fea4e5c28f
Kalpesh <kalpesh@localhost.localdomain>
2021-04-20 14:44:04 +0530

    qa/tasks: Adding RabbitMQ task for bucket notification tests
    This commit majorly consists of the RabbitMQ task which is a required and supported endpoint in bucket notification tests.
    And some related changes in the AMQP tests. Major changes are:
    1. Addition of RabbitMQ task
    2. Documentation update for the steps to execute AMQP tests
    3. Addition of attributes to the tests
    4. Tox dependency removal from kafka.py
    
    Signed-off-by: Kalpesh Pandya <kapandya@redhat.com>
    (cherry picked from commit 2e0b8a2a1ffa450ee6b276b5cbe0f735b7488b88)
    
     Conflicts:
    	qa/suites/rgw/notifications/tasks/test_kafka.yaml
    	src/test/rgw/bucket_notification/test_bn.py
    
     The changes made are: removing git_remote from test_kafka.yaml, changing the force-branch name to master, removal of SkipTest parameter from AMQP tests, and some spacing changes (mostly addition of new line characters) that were in the changes

--- qa/tasks/kafka.py
+++ qa/tasks/kafka.py
@@ -7,7 +7,6 @@ import logging
 from teuthology import misc as teuthology
 from teuthology import contextutil
 from teuthology.orchestra import run
-from teuthology.exceptions import ConfigError
 
 log = logging.getLogger(__name__)
 
@@ -22,12 +21,6 @@ def get_kafka_dir(ctx, config):
     current_version = 'kafka-' + kafka_version + '-src'
     return '{tdir}/{ver}'.format(tdir=teuthology.get_testdir(ctx),ver=current_version)
 
-def get_toxvenv_dir(ctx):
-    return ctx.tox.venv_path
-
-def toxvenv_sh(ctx, remote, args, **kwargs):
-    activate = get_toxvenv_dir(ctx) + '/bin/activate'
-    return remote.sh(['source', activate, run.Raw('&&')] + args, **kwargs)
 
 @contextlib.contextmanager
 def install_kafka(ctx, config):
@@ -43,10 +36,14 @@ def install_kafka(ctx, config):
         current_version = get_kafka_version(config)
 
         link1 = 'https://archive.apache.org/dist/kafka/' + current_version + '/kafka-' + current_version + '-src.tgz'
-        toxvenv_sh(ctx, remote, ['cd', '{tdir}'.format(tdir=test_dir), run.Raw('&&'), 'wget', link1])
+        ctx.cluster.only(client).run(
+            args=['cd', '{tdir}'.format(tdir=test_dir), run.Raw('&&'), 'wget', link1],
+        )
 
         file1 = 'kafka-' + current_version + '-src.tgz'
-        toxvenv_sh(ctx, remote, ['cd', '{tdir}'.format(tdir=test_dir), run.Raw('&&'), 'tar', '-xvzf', file1])
+        ctx.cluster.only(client).run(
+            args=['cd', '{tdir}'.format(tdir=test_dir), run.Raw('&&'), 'tar', '-xvzf', file1],
+        )
 
     try:
         yield
@@ -54,7 +51,7 @@ def install_kafka(ctx, config):
         log.info('Removing packaged dependencies of Kafka...')
         test_dir=get_kafka_dir(ctx, config)
         current_version = get_kafka_version(config)
-        for client in config:
+        for (client,_) in config.items():
             ctx.cluster.only(client).run(
                 args=['rm', '-rf', test_dir],
             )
@@ -77,23 +74,23 @@ def run_kafka(ctx,config):
     for (client,_) in config.items():
         (remote,) = ctx.cluster.only(client).remotes.keys()
 
-        toxvenv_sh(ctx, remote,
-            ['cd', '{tdir}'.format(tdir=get_kafka_dir(ctx, config)), run.Raw('&&'),
+        ctx.cluster.only(client).run(
+            args=['cd', '{tdir}'.format(tdir=get_kafka_dir(ctx, config)), run.Raw('&&'),
              './gradlew', 'jar', 
              '-PscalaVersion=2.13.2'
             ],
         )
 
-        toxvenv_sh(ctx, remote,
-            ['cd', '{tdir}/bin'.format(tdir=get_kafka_dir(ctx, config)), run.Raw('&&'),
+        ctx.cluster.only(client).run(
+            args=['cd', '{tdir}/bin'.format(tdir=get_kafka_dir(ctx, config)), run.Raw('&&'),
              './zookeeper-server-start.sh',
              '{tir}/config/zookeeper.properties'.format(tir=get_kafka_dir(ctx, config)),
              run.Raw('&'), 'exit'
             ],
         )
 
-        toxvenv_sh(ctx, remote,
-            ['cd', '{tdir}/bin'.format(tdir=get_kafka_dir(ctx, config)), run.Raw('&&'),
+        ctx.cluster.only(client).run(
+            args=['cd', '{tdir}/bin'.format(tdir=get_kafka_dir(ctx, config)), run.Raw('&&'),
              './kafka-server-start.sh',
              '{tir}/config/server.properties'.format(tir=get_kafka_dir(ctx, config)),
              run.Raw('&'), 'exit'
@@ -108,18 +105,18 @@ def run_kafka(ctx,config):
         for (client, _) in config.items():
             (remote,) = ctx.cluster.only(client).remotes.keys()
 
-            toxvenv_sh(ctx, remote, 
-                ['cd', '{tdir}/bin'.format(tdir=get_kafka_dir(ctx, config)), run.Raw('&&'),
+            ctx.cluster.only(client).run(
+                args=['cd', '{tdir}/bin'.format(tdir=get_kafka_dir(ctx, config)), run.Raw('&&'),
                  './kafka-server-stop.sh',  
                  '{tir}/config/kafka.properties'.format(tir=get_kafka_dir(ctx, config)),
-                ]
+                ],
             )
 
-            toxvenv_sh(ctx, remote, 
-                ['cd', '{tdir}/bin'.format(tdir=get_kafka_dir(ctx, config)), run.Raw('&&'), 
+            ctx.cluster.only(client).run(
+                args=['cd', '{tdir}/bin'.format(tdir=get_kafka_dir(ctx, config)), run.Raw('&&'), 
                  './zookeeper-server-stop.sh',
                  '{tir}/config/zookeeper.properties'.format(tir=get_kafka_dir(ctx, config)),
-                ]
+                ],
             )
 
 @contextlib.contextmanager
@@ -132,29 +129,32 @@ def run_admin_cmds(ctx,config):
     for (client,_) in config.items():
         (remote,) = ctx.cluster.only(client).remotes.keys()
 
-        toxvenv_sh(ctx, remote,
-            [
+        ctx.cluster.only(client).run(
+            args=[
                 'cd', '{tdir}/bin'.format(tdir=get_kafka_dir(ctx, config)), run.Raw('&&'), 
                 './kafka-topics.sh', '--create', '--topic', 'quickstart-events',
                 '--bootstrap-server', 'localhost:9092'
-            ])
+            ],
+        )
 
-        toxvenv_sh(ctx, remote,
-            [
+        ctx.cluster.only(client).run(
+            args=[
                 'cd', '{tdir}/bin'.format(tdir=get_kafka_dir(ctx, config)), run.Raw('&&'),
                 'echo', "First", run.Raw('|'),
                 './kafka-console-producer.sh', '--topic', 'quickstart-events',
                 '--bootstrap-server', 'localhost:9092'
-            ])
+            ],
+        )
 
-        toxvenv_sh(ctx, remote,
-            [
+        ctx.cluster.only(client).run(
+            args=[
                 'cd', '{tdir}/bin'.format(tdir=get_kafka_dir(ctx, config)), run.Raw('&&'),
                 './kafka-console-consumer.sh', '--topic', 'quickstart-events',
                 '--from-beginning',
                 '--bootstrap-server', 'localhost:9092',
                 run.Raw('&'), 'exit'
-            ])
+            ],
+        )
 
     try:
         yield
@@ -165,19 +165,14 @@ def run_admin_cmds(ctx,config):
 @contextlib.contextmanager
 def task(ctx,config):
     """
-    To run kafka the prerequisite is to run the tox task. Following is the way how to run
-    tox and then kafka::
+    Following is the way how to run kafka::
     tasks:
-    - tox: [ client.0 ]
     - kafka:
         client.0:
     """
     assert config is None or isinstance(config, list) \
         or isinstance(config, dict), \
         "task kafka only supports a list or dictionary for configuration"
-
-    if not hasattr(ctx, 'tox'):
-        raise ConfigError('kafka must run after the tox task')
 
     all_clients = ['client.{id}'.format(id=id_)
                    for id_ in teuthology.all_roles_of_type(ctx.cluster, 'client')]
