sunilangadi2/ceph-ci

RGW Zipper - Permenant User storage API
The load/store/remove APIs for User are needed, so make them consistent
and permenant.

Signed-off-by: Daniel Gryniewicz <dang@redhat.com>

src/rgw/rgw_sal.h
@@ -285,10 +285,9 @@ class User {
     virtual Attrs& get_attrs() { return attrs; }
     virtual void set_attrs(Attrs& _attrs) { attrs = _attrs; }
 
-    /* Placeholders */
     virtual int load_by_id(const DoutPrefixProvider* dpp, optional_yield y) = 0;
-    virtual int store_info(const DoutPrefixProvider* dpp, optional_yield y, bool exclusive, RGWUserInfo* old_info = nullptr) = 0;
-    virtual int remove_info(const DoutPrefixProvider* dpp, optional_yield y, const RGWUserCtl::RemoveParams& params = {}) = 0;
+    virtual int store_by_id(const DoutPrefixProvider* dpp, optional_yield y, bool exclusive, RGWUserInfo* old_info = nullptr) = 0;
+    virtual int remove_by_id(const DoutPrefixProvider* dpp, optional_yield y) = 0;
 
     /* dang temporary; will be removed when User is complete */
     RGWUserInfo& get_info() { return info; }

src/rgw/rgw_sal_rados.h
@@ -65,8 +65,8 @@ class RadosUser : public User {
 
     /* Placeholders */
     virtual int load_by_id(const DoutPrefixProvider* dpp, optional_yield y) override;
-    virtual int store_info(const DoutPrefixProvider* dpp, optional_yield y, bool exclusive, RGWUserInfo* old_info = nullptr) override;
-    virtual int remove_info(const DoutPrefixProvider* dpp, optional_yield y, const RGWUserCtl::RemoveParams& params = {}) override;
+    virtual int store_by_id(const DoutPrefixProvider* dpp, optional_yield y, bool exclusive, RGWUserInfo* old_info = nullptr) override;
+    virtual int remove_by_id(const DoutPrefixProvider* dpp, optional_yield y) override;
 
     friend class RadosBucket;
 };

ceph/ceph

src/rgw/rgw_sal.h
@@ -285,10 +285,9 @@ class User {
     virtual Attrs& get_attrs() { return attrs; }
     virtual void set_attrs(Attrs& _attrs) { attrs = _attrs; }
 
-    /* Placeholders */
-    virtual int load_by_id(const DoutPrefixProvider* dpp, optional_yield y) = 0;
-    virtual int store_info(const DoutPrefixProvider* dpp, optional_yield y, bool exclusive, RGWUserInfo* old_info = nullptr) = 0;
-    virtual int remove_info(const DoutPrefixProvider* dpp, optional_yield y, const RGWUserCtl::RemoveParams& params = {}) = 0;
+    virtual int load_user(const DoutPrefixProvider* dpp, optional_yield y) = 0;
+    virtual int store_user(const DoutPrefixProvider* dpp, optional_yield y, bool exclusive, RGWUserInfo* old_info = nullptr) = 0;
+    virtual int remove_user(const DoutPrefixProvider* dpp, optional_yield y) = 0;
 
     /* dang temporary; will be removed when User is complete */
     RGWUserInfo& get_info() { return info; }

src/rgw/rgw_sal_rados.h
@@ -63,10 +63,9 @@ class RadosUser : public User {
 			   map<rgw_user_bucket, rgw_usage_log_entry>& usage) override;
     virtual int trim_usage(uint64_t start_epoch, uint64_t end_epoch) override;
 
-    /* Placeholders */
-    virtual int load_by_id(const DoutPrefixProvider* dpp, optional_yield y) override;
-    virtual int store_info(const DoutPrefixProvider* dpp, optional_yield y, bool exclusive, RGWUserInfo* old_info = nullptr) override;
-    virtual int remove_info(const DoutPrefixProvider* dpp, optional_yield y, const RGWUserCtl::RemoveParams& params = {}) override;
+    virtual int load_user(const DoutPrefixProvider* dpp, optional_yield y) override;
+    virtual int store_user(const DoutPrefixProvider* dpp, optional_yield y, bool exclusive, RGWUserInfo* old_info = nullptr) override;
+    virtual int remove_user(const DoutPrefixProvider* dpp, optional_yield y) override;
 
     friend class RadosBucket;
 };