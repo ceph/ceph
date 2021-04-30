5b684353cd385cb11b2841eab483da1ce7a72bc8
Samuel Just <sjust@redhat.com>
2021-04-30 07:31:45 +0000

    crimson/os/seastore/seastore.cc: update to use new debug macros
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/os/seastore/seastore.cc
+++ src/crimson/os/seastore/seastore.cc
@@ -21,12 +21,6 @@
 #include "crimson/os/seastore/onode_manager.h"
 #include "crimson/os/seastore/object_data_handler.h"
 
-namespace {
-  seastar::logger& logger() {
-    return crimson::get_logger(ceph_subsys_filestore);
-  }
-}
-
 using crimson::common::local_conf;
 
 namespace crimson::os::seastore {
@@ -92,7 +86,8 @@ seastar::future<> SeaStore::mkfs(uuid_d new_osd_fsid)
 
 seastar::future<store_statfs_t> SeaStore::stat() const
 {
-  logger().debug("{}", __func__);
+  LOG_PREFIX(SeaStore::stat);
+  DEBUG("");
   store_statfs_t st;
   return seastar::make_ready_future<store_statfs_t>(st);
 }
@@ -109,6 +104,8 @@ SeaStore::list_objects(CollectionRef ch,
 
 seastar::future<CollectionRef> SeaStore::create_new_collection(const coll_t& cid)
 {
+  LOG_PREFIX(SeaStore::create_new_collection);
+  DEBUG("{}", cid);
   auto c = _get_collection(cid);
   return repeat_with_internal_context(
     c,
@@ -128,6 +125,8 @@ seastar::future<CollectionRef> SeaStore::create_new_collection(const coll_t& cid
 
 seastar::future<CollectionRef> SeaStore::open_collection(const coll_t& cid)
 {
+  LOG_PREFIX(SeaStore::open_collection);
+  DEBUG("{}", cid);
   return seastar::make_ready_future<CollectionRef>(_get_collection(cid));
 }
 
@@ -170,6 +169,8 @@ SeaStore::read_errorator::future<ceph::bufferlist> SeaStore::read(
   size_t len,
   uint32_t op_flags)
 {
+  LOG_PREFIX(SeaStore::read);
+  DEBUG("oid {} offset {} len {}", oid, offset, len);
   return repeat_with_onode<ceph::bufferlist>(
     ch,
     oid,
@@ -202,8 +203,8 @@ SeaStore::get_attr_errorator::future<ceph::bufferlist> SeaStore::get_attr(
   std::string_view name) const
 {
   auto c = static_cast<SeastoreCollection*>(ch.get());
-  logger().debug("{} {} {}",
-		 __func__, c->get_cid(), oid);
+  LOG_PREFIX(SeaStore::get_attr);
+  DEBUG("{} {}", c->get_cid(), oid);
   using get_attr_ertr = TransactionManager::base_ertr::extend<
     crimson::ct_error::enodata>;
   return repeat_with_onode<ceph::bufferlist>(
@@ -237,8 +238,8 @@ SeaStore::get_attr_errorator::future<ceph::bufferlist> SeaStore::get_attr(
 	return seastar::make_ready_future<ceph::bufferlist>(std::move(*opt));
       });
     });
-  }).handle_error(crimson::ct_error::input_output_error::handle([this] {
-    logger().error("EIO when getting attrs");
+  }).handle_error(crimson::ct_error::input_output_error::handle([FNAME] {
+    ERROR("EIO when getting attrs");
     abort();
   }), crimson::ct_error::pass_further_all{});
 }
@@ -247,9 +248,9 @@ SeaStore::get_attrs_ertr::future<SeaStore::attrs_t> SeaStore::get_attrs(
   CollectionRef ch,
   const ghobject_t& oid)
 {
+  LOG_PREFIX(SeaStore::get_attrs);
   auto c = static_cast<SeastoreCollection*>(ch.get());
-  logger().debug("{} {} {}",
-		 __func__, c->get_cid(), oid);
+  DEBUG("{} {}", c->get_cid(), oid);
   return repeat_with_onode<attrs_t>(
     c, oid, [=](auto &t, auto& onode) {
     auto& layout = onode.get_layout();
@@ -269,8 +270,8 @@ SeaStore::get_attrs_ertr::future<SeaStore::attrs_t> SeaStore::get_attrs(
       }
       return seastar::make_ready_future<omap_values_t>(std::move(attrs));
     });
-  }).handle_error(crimson::ct_error::input_output_error::handle([this] {
-    logger().error("EIO when getting attrs");
+  }).handle_error(crimson::ct_error::input_output_error::handle([FNAME] {
+    ERROR("EIO when getting attrs");
     abort();
   }), crimson::ct_error::pass_further_all{});
 }
@@ -279,16 +280,18 @@ seastar::future<struct stat> SeaStore::stat(
   CollectionRef c,
   const ghobject_t& oid)
 {
+  LOG_PREFIX(SeaStore::stat);
   return repeat_with_onode<struct stat>(
     c,
     oid,
-    [=](auto &t, auto &onode) {
+    [=, &oid](auto &t, auto &onode) {
       struct stat st;
       auto &olayout = onode.get_layout();
       st.st_size = olayout.size;
       st.st_blksize = transaction_manager->get_block_size();
       st.st_blocks = (st.st_size + st.st_blksize - 1) / st.st_blksize;
       st.st_nlink = 1;
+      DEBUGT("cid {}, oid {}, return size {}", t, c->get_cid(), oid, st.st_size);
       return seastar::make_ready_future<struct stat>(st);
     }).handle_error(
       crimson::ct_error::assert_all{
@@ -388,9 +391,8 @@ SeaStore::omap_get_values_ret_t SeaStore::omap_list(
   OMapManager::omap_list_config_t config)
 {
   auto c = static_cast<SeastoreCollection*>(ch.get());
-  logger().debug(
-    "{} {} {}",
-    __func__, c->get_cid(), oid);
+  LOG_PREFIX(SeaStore::omap_list);
+  DEBUG("{} {}", c->get_cid(), oid);
   using ret_bare_t = std::tuple<bool, SeaStore::omap_values_t>;
   using int_ret_t = omap_int_ertr_t::future<ret_bare_t>;
   return repeat_with_onode<ret_bare_t>(
@@ -526,14 +528,15 @@ seastar::future<std::map<uint64_t, uint64_t>> SeaStore::fiemap(
 }
 
 void SeaStore::on_error(ceph::os::Transaction &t) {
-  logger().error(" transaction dump:\n");
+  LOG_PREFIX(SeaStore::on_error);
+  ERROR(" transaction dump:\n");
   JSONFormatter f(true);
   f.open_object_section("transaction");
   t.dump(&f);
   f.close_section();
   std::stringstream str;
   f.flush(str);
-  logger().error("{}", str.str());
+  ERROR("{}", str.str());
   abort();
 }
 
@@ -581,6 +584,7 @@ SeaStore::tm_ret SeaStore::_do_transaction_step(
   std::vector<OnodeRef> &onodes,
   ceph::os::Transaction::iterator &i)
 {
+  LOG_PREFIX(SeaStore::_do_transaction_step);
   auto get_onode = [&onodes](size_t i) -> OnodeRef& {
     ceph_assert(i < onodes.size());
     return onodes[i];
@@ -678,11 +682,11 @@ SeaStore::tm_ret SeaStore::_do_transaction_step(
       return tm_ertr::now();
     }
     default:
-      logger().error("bad op {}", static_cast<unsigned>(op->op));
+      ERROR("bad op {}", static_cast<unsigned>(op->op));
       return crimson::ct_error::input_output_error::make();
     }
   } catch (std::exception &e) {
-    logger().error("{} got exception {}", __func__, e);
+    ERROR("got exception {}", e);
     return crimson::ct_error::input_output_error::make();
   }
 }
@@ -691,8 +695,8 @@ SeaStore::tm_ret SeaStore::_remove(
   internal_context_t &ctx,
   OnodeRef &onode)
 {
-  logger().debug("{} onode={}",
-                __func__, *onode);
+  LOG_PREFIX(SeaStore::_remove);
+  DEBUGT("onode={}", *ctx.transaction, *onode);
   return tm_ertr::now();
 }
 
@@ -700,8 +704,8 @@ SeaStore::tm_ret SeaStore::_touch(
   internal_context_t &ctx,
   OnodeRef &onode)
 {
-  logger().debug("{} onode={}",
-                __func__, *onode);
+  LOG_PREFIX(SeaStore::_touch);
+  DEBUGT("onode={}", *ctx.transaction, *onode);
   return tm_ertr::now();
 }
 
@@ -712,8 +716,8 @@ SeaStore::tm_ret SeaStore::_write(
   ceph::bufferlist &&_bl,
   uint32_t fadvise_flags)
 {
-  logger().debug("SeaStore::{}: {} {} ~ {}",
-                __func__, *onode, offset, len);
+  LOG_PREFIX(SeaStore::_write);
+  DEBUGT("onode={} {}~{}", *ctx.transaction, *onode, offset, len);
   {
     auto &object_size = onode->get_mutable_layout(*ctx.transaction).size;
     object_size = std::max<uint64_t>(
@@ -771,9 +775,8 @@ SeaStore::tm_ret SeaStore::_omap_set_values(
   OnodeRef &onode,
   std::map<std::string, ceph::bufferlist> &&aset)
 {
-  logger().debug(
-    "{}: {} {} keys",
-    __func__, *onode, aset.size());
+  LOG_PREFIX(SeaStore::_omap_set_values);
+  DEBUGT("{} {} keys", *ctx.transaction, *onode, aset.size());
   return __omap_set_kvs(
     onode->get_layout().omap_root,
     *ctx.transaction,
@@ -786,9 +789,8 @@ SeaStore::tm_ret SeaStore::_omap_set_header(
   OnodeRef &onode,
   ceph::bufferlist &&header)
 {
-  logger().debug(
-    "{}: {} {} bytes",
-    __func__, *onode, header.length());
+  LOG_PREFIX(SeaStore::_omap_set_header);
+  DEBUGT("{} {} bytes", *ctx.transaction, *onode, header.length());
   assert(0 == "not supported yet");
   return tm_ertr::now();
 }
@@ -798,9 +800,8 @@ SeaStore::tm_ret SeaStore::_omap_rmkeys(
   OnodeRef &onode,
   omap_keys_t &&keys)
 {
-  logger().debug(
-    "{} {} {} keys",
-    __func__, *onode, keys.size());
+  LOG_PREFIX(SeaStore::_omap_rmkeys);
+  DEBUGT("{} {} keys", *ctx.transaction, *onode, keys.size());
   auto omap_root = onode->get_layout().omap_root.get();
   if (omap_root.is_null()) {
     return seastar::now();
@@ -837,9 +838,8 @@ SeaStore::tm_ret SeaStore::_omap_rmkeyrange(
   std::string first,
   std::string last)
 {
-  logger().debug(
-    "{} {} first={} last={}",
-    __func__, *onode, first, last);
+  LOG_PREFIX(SeaStore::_omap_rmkeyrange);
+  DEBUGT("{} first={} last={}", *ctx.transaction, *onode, first, last);
   assert(0 == "not supported yet");
   return tm_ertr::now();
 }
@@ -849,8 +849,8 @@ SeaStore::tm_ret SeaStore::_truncate(
   OnodeRef &onode,
   uint64_t size)
 {
-  logger().debug("SeaStore::{} onode={} size={}",
-                __func__, *onode, size);
+  LOG_PREFIX(SeaStore::_truncate);
+  DEBUGT("onode={} size={}", *ctx.transaction, *onode, size);
   onode->get_mutable_layout(*ctx.transaction).size = size;
   return ObjectDataHandler().truncate(
     ObjectDataHandler::context_t{
@@ -866,8 +866,8 @@ SeaStore::tm_ret SeaStore::_setattrs(
   OnodeRef &onode,
   std::map<std::string, bufferlist>&& aset)
 {
-  logger().debug("{} onode={}",
-                __func__, *onode);
+  LOG_PREFIX(SeaStore::_setattrs);
+  DEBUGT("onode={}", *ctx.transaction, *onode);
   auto& layout = onode->get_mutable_layout(*ctx.transaction);
   if (auto it = aset.find(OI_ATTR); it != aset.end()) {
     auto& val = it->second;
@@ -980,7 +980,8 @@ boost::intrusive_ptr<SeastoreCollection> SeaStore::_get_collection(const coll_t&
 seastar::future<> SeaStore::write_meta(const std::string& key,
 					const std::string& value)
 {
-  logger().debug("{}, key: {}; value: {}", __func__, key, value);
+  LOG_PREFIX(SeaStore::write_meta);
+  DEBUG("key: {}; value: {}", key, value);
   return seastar::do_with(key, value,
     [this](auto& key, auto& value) {
     return repeat_eagain([this, &key, &value] {
@@ -998,7 +999,8 @@ seastar::future<> SeaStore::write_meta(const std::string& key,
 
 seastar::future<std::tuple<int, std::string>> SeaStore::read_meta(const std::string& key)
 {
-  logger().debug("{}, key: {}", __func__, key);
+  LOG_PREFIX(SeaStore::read_meta);
+  DEBUG("key: {}", key);
   return seastar::do_with(transaction_manager->create_transaction(), key,
     [this](auto& t, auto& key) {
     return transaction_manager->get_root(*t).safe_then(
