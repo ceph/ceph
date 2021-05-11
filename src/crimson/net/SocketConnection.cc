0a1d158a82a63f6c4f1176ec211344bd3dbd7335
Amnon Hanuhov <ahanukov@redhat.com>
2021-05-11 15:07:35 +0300

    crimson/net: Set add_ref to false when creating a MessageRef in conn::send()
    we should not increase the ref count when transferring the ownership from a
    MessageURef instance to a MessageRef instance.
    
    to address the memory leak introduced by
    6913f5712bd3a466987a07c8ac36f942a37033b4.
    
    Signed-off-by: Amnon Hanuhov <ahanukov@redhat.com>

--- src/crimson/net/SocketConnection.cc
+++ src/crimson/net/SocketConnection.cc
@@ -72,7 +72,7 @@ bool SocketConnection::peer_wins() const
 seastar::future<> SocketConnection::send(MessageURef msg)
 {
   assert(seastar::this_shard_id() == shard_id());
-  return protocol->send(MessageRef{msg.release()});
+  return protocol->send(MessageRef{msg.release(), false});
 }
 
 seastar::future<> SocketConnection::send(MessageRef msg)
