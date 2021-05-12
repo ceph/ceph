9059c72d39a2e3203abd146a183cc705914dd72f
Mykola Golub <mgolub@suse.com>
2021-05-12 07:51:15 +0100

    osd/ECBackend: don't remove in-flight backfill when missing is not primary
    This is a direct commit to nautilus branch to recreate the
    behaviour we have post-nautilus, after refactoring made in
    8a8947d2a32d6390cb17099398e7f2212660c9a1, which seems to be
    correct.
    
    The in-flight backfill prevents updating of "backfill complete"
    position, which remains on the object before the missing oid. So
    when pg is re-peered it retries backfill from this position
    instead of entering the clean state.
    
    Fixes: https://tracker.ceph.com/issues/50747
    Signed-off-by: Mykola Golub <mgolub@suse.com>

--- src/osd/PrimaryLogPG.h
+++ src/osd/PrimaryLogPG.h
@@ -300,7 +300,8 @@ public:
     const hobject_t &soid,
     const object_stat_sum_t &delta_stats) override;
   void on_primary_error(const hobject_t &oid, eversion_t v) override;
-  void backfill_add_missing(const hobject_t &oid, eversion_t v) override;
+  void backfill_add_missing(const hobject_t &oid, eversion_t v,
+                            bool primary_missing) override;
   void remove_missing_object(const hobject_t &oid,
 			     eversion_t v,
 			     Context *on_complete) override;
