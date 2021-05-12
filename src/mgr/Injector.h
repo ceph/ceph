864d32561d23926557986b2b6bef3979460af8ec
Pere Diaz Bou <pdiazbou@redhat.com>
2021-05-12 13:05:02 +0200

    mgr: use build_simple
    Signed-off-by: Pere Diaz Bou <pdiazbou@redhat.com>

--- src/mgr/Injector.h
+++ src/mgr/Injector.h
@@ -3,12 +3,14 @@
 
 #pragma once
 
-#include "ActivePyModule.h"
+#include "osd/OSDMap.h"
+
 #include "PyUtil.h"
 
 class Injector {
 private:
   static int64_t get_num_osds();
+  static void mark_exists_osds(OSDMap* osdmap);
 
 public:
   static PyObject *get_python(const std::string &what);
