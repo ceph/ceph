64486bea6bb7c7c2108b851b953fca136a141a63
Pere Diaz Bou <pdiazbou@redhat.com>
2021-05-12 12:00:15 +0200

    mgr: inject enable option and inject_num_osds option
    Signed-off-by: Pere Diaz Bou <pdiazbou@redhat.com>

--- src/mgr/Injector.cc
+++ src/mgr/Injector.cc
@@ -1,12 +1,27 @@
-#include "PyFormatter.h"
-#include "PyUtil.h"
-#include "osd/OSDMap.h"
-#include "mgr/inject.h"
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
 
-PyObject* Injector::get_python(const std::string &what) {
+#include "ActivePyModule.h"
+
+#include "mgr/Injector.h"
+
+#include "PyFormatter.h"
+
+#include "PyUtil.h"
+
+#include "osd/OSDMap.h"
+
+using namespace std;
+
+int64_t Injector::get_num_osds() {
+  return g_conf().get_val<int64_t>("inject_num_osds");
+}
+
+PyObject* Injector::get_python(const std::string& what) {
   PyFormatter f;
-  OSDMap *osdmap = new OSDMap;
-  int num_osds = 1000;
+  OSDMap* osdmap = new OSDMap;
+  int64_t num_osds = Injector::get_num_osds();
+
   osdmap->set_max_osd(num_osds);
   for (int i = 0; i < num_osds; i++) {
     osdmap->set_state(i, CEPH_OSD_EXISTS);
