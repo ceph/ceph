fba399b123276d00d278acaba661bf1a8cf8faa0
Xiubo Li <xiubli@redhat.com>
2021-05-19 15:54:12 +0800

    mds: progress the recover queue immediately after the inode is enqueued
    Fixes: https://tracker.ceph.com/issues/50840
    Signed-off-by: "Yan, Zheng" <ukernel@gmail.com>
    (cherry picked from commit 616ebc3f04620c22439a62f3af35845fa98e508f)

--- src/mds/Locker.cc
+++ src/mds/Locker.cc
@@ -5415,6 +5415,7 @@ void Locker::file_eval(ScatterLock *lock, bool *need_issue)
   }
   else if (in->state_test(CInode::STATE_NEEDSRECOVER)) {
     mds->mdcache->queue_file_recover(in);
+    mds->mdcache->do_file_recover();
   }
 }
 
