f9fca8a762f69eed96d78577433e9170ef62ec95
Venky Shankar <vshankar@redhat.com>
2021-05-19 01:19:34 -0400

    mgr/stats: include average latencies and stdev in `perf stat` dump
    Signed-off-by: Venky Shankar <vshankar@redhat.com>
    (cherry picked from commit b2bc45223b02ded7a5cc921980b3961c5e1d5893)

--- src/pybind/mgr/stats/fs/perf_stats.py
+++ src/pybind/mgr/stats/fs/perf_stats.py
@@ -37,7 +37,13 @@ MDS_PERF_QUERY_COUNTERS_MAP = OrderedDict({'cap_hit': 0,
                                            'pinned_icaps': 6,
                                            'opened_inodes': 7,
                                            'read_io_sizes': 8,
-                                           'write_io_sizes': 9})
+                                           'write_io_sizes': 9,
+                                           'avg_read_latency': 10,
+                                           'stdev_read_latency': 11,
+                                           'avg_write_latency': 12,
+                                           'stdev_write_latency': 13,
+                                           'avg_metadata_latency': 14,
+                                           'stdev_metadata_latency': 15})
 MDS_PERF_QUERY_COUNTERS = [] # type: List[str]
 MDS_GLOBAL_PERF_QUERY_COUNTERS = list(MDS_PERF_QUERY_COUNTERS_MAP.keys())
 
