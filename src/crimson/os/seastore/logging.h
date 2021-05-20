ceph/ceph

commit 39dfae03e5652db8c840c0b575b82ec8dc9f9c44

crimson/os: pass log level to LOG()
instead of passing function name to the underlying macro, pass log
level for better readability.

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/crimson/os/seastore/logging.h
@@ -12,23 +12,23 @@
 
 #ifdef NDEBUG
 
-#define LOG(LEVEL, MSG, ...) LOGGER . LEVEL("{}: " MSG, FNAME __VA_OPT__(,) __VA_ARGS__)
-#define LOGT(LEVEL, MSG, t, ...) LOGGER . LEVEL("{}({}): " MSG, FNAME, (void*)&t __VA_OPT__(,) __VA_ARGS__)
+#define LOG(level_, MSG, ...) LOGGER.log(level_, "{}: " MSG, FNAME __VA_OPT__(,) __VA_ARGS__))
+#define LOGT(level_, MSG, t, ...) LOGGER.log(level_, "{}({}): " MSG, FNAME, (void*)&t __VA_OPT__(,) __VA_ARGS__))
 
-#define TRACE(...) LOG(trace, __VA_ARGS__)
-#define TRACET(...) LOGT(trace, __VA_ARGS__)
+#define TRACE(...) LOG(seastar::log_level::trace, __VA_ARGS__)
+#define TRACET(...) LOGT(seastar::log_level::trace, __VA_ARGS__)
 
-#define DEBUG(...) LOG(debug, __VA_ARGS__)
-#define DEBUGT(...) LOGT(debug, __VA_ARGS__)
+#define DEBUG(...) LOG(seastar::log_level::debug, __VA_ARGS__)
+#define DEBUGT(...) LOGT(seastar::log_level::debug, __VA_ARGS__)
 
-#define INFO(...) LOG(info, __VA_ARGS__)
-#define INFOT(...) LOGT(info, __VA_ARGS__)
+#define INFO(...) LOG(seastar::log_level::info, __VA_ARGS__)
+#define INFOT(...) LOGT(seastar::log_level::info, __VA_ARGS__)
 
-#define WARN(...) LOG(warn, __VA_ARGS__)
-#define WARNT(...) LOGT(warn, __VA_ARGS__)
+#define WARN(...) LOG(seastar::log_level::warn, __VA_ARGS__)
+#define WARNT(...) LOGT(seastar::log_level::warn, __VA_ARGS__)
 
-#define ERROR(...) LOG(error, __VA_ARGS__)
-#define ERRORT(...) LOGT(error, __VA_ARGS__)
+#define ERROR(...) LOG(seastar::log_level::error, __VA_ARGS__)
+#define ERRORT(...) LOGT(seastar::log_level::error, __VA_ARGS__)
 
 #else
 // do compile-time format string validation

commit 913a58a2e0d36ba3ffb509975fb73624b6270eec

crimson/os/seastore/logging: remove comma
to address the FTBFS of the release builds.

fix a regression introduced by 39dfae03e5652db8c840c0b575b82ec8dc9f9c44

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/crimson/os/seastore/logging.h
@@ -13,7 +13,7 @@
 #ifdef NDEBUG
 
 #define LOG(level_, MSG, ...) LOGGER.log(level_, "{}: " MSG, FNAME __VA_OPT__(,) __VA_ARGS__))
-#define LOGT(level_, MSG, t, ...) LOGGER.log(level_, "{}({}): " MSG, FNAME, (void*)&t __VA_OPT__(,) __VA_ARGS__))
+#define LOGT(level_, MSG, t, ...) LOGGER.log(level_, "{}({}): " MSG, FNAME (void*)&t __VA_OPT__(,) __VA_ARGS__))
 
 #define TRACE(...) LOG(seastar::log_level::trace, __VA_ARGS__)
 #define TRACET(...) LOGT(seastar::log_level::trace, __VA_ARGS__)