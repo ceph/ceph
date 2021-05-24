fdef04c749f04b769e6f5b95a094672f397ad3e5
Jinyong Ha <jy200.ha@samsung.com>
2021-05-24 18:45:24 +0900

    seastore: move implementation of TestMemory to nvmeblockdevice.cc
    Signed-off-by: Jinyong Ha <jy200.ha@samsung.com>

--- src/crimson/os/seastore/nvmedevice/nvmedevice.cc
+++ src/crimson/os/seastore/nvmedevice/nvmedevice.cc
@@ -91,4 +91,61 @@ seastar::future<> PosixNVMeDevice::close() {
   logger().debug(" close ");
   return device.close();
 }
+
+open_ertr::future<> TestMemory::open(
+  const std::string &in_path,
+   seastar::open_flags mode) {
+  if (buf) {
+    return open_ertr::now();
+  }
+
+  logger().debug(
+    "Initializing test memory divice {}",
+    size);
+
+  void* addr = ::mmap(
+    nullptr,
+    size,
+    PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS,
+    -1,
+    0);
+
+  buf = (char*)addr;
+
+  ::memset(buf, 0, size);
+  return open_ertr::now();
+}
+
+write_ertr::future<> TestMemory::write(
+  uint64_t offset,
+  bufferptr &bptr,
+  uint16_t stream) {
+  ceph_assert(buf);
+  logger().debug(
+    "TestMemory: write offset {} len {}",
+    offset,
+    bptr.length());
+
+  ::memcpy(buf + offset, bptr.c_str(), bptr.length());
+
+  return write_ertr::now();
+}
+
+read_ertr::future<> TestMemory::read(
+  uint64_t offset,
+  bufferptr &bptr) {
+  ceph_assert(buf);
+  logger().debug(
+    "TestMemory: read offset {} len {}",
+    offset,
+    bptr.length());
+
+  bptr.copy_in(0, bptr.length(), buf + offset);
+  return read_ertr::now();
+}
+
+seastar::future<> TestMemory::close() {
+  logger().debug(" close ");
+  return seastar::now();
+}
 }
