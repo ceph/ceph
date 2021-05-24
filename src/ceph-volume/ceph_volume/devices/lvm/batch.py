47715c835fe5f0934f33c0d33175ddf9d67b9c9d
Andrew Schoen <aschoen@redhat.com>
2021-05-24 12:19:38 -0500

    ceph-volume: respect the value of bluestore_block_db_size from ceph.conf
    If --block-db-size is not given args.block_db_size is set to None,
    so we should check for it's value in ceph.conf
    
    Resolves: RHBZ#1962744
    Fixes: https://tracker.ceph.com/issues/50958
    
    Signed-off-by: Andrew Schoen <aschoen@redhat.com>

--- src/ceph-volume/ceph_volume/devices/lvm/batch.py
+++ src/ceph-volume/ceph_volume/devices/lvm/batch.py
@@ -106,7 +106,7 @@ def get_physical_fast_allocs(devices, type_, fast_slots_per_device, new_osds, ar
         requested_slots = fast_slots_per_device
 
     requested_size = getattr(args, '{}_size'.format(type_), 0)
-    if requested_size == 0:
+    if not requested_size or requested_size == 0:
         # no size argument was specified, check ceph.conf
         get_size_fct = getattr(prepare, 'get_{}_size'.format(type_))
         requested_size = get_size_fct(lv_format=False)
