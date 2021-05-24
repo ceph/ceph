6fe2c1b67399876ad8b4c25126172809b015e639
Michael Fritch <mfritch@suse.com>
2021-05-24 08:03:55 -0600

    cephadm: add pyfakefs fixture
    to mock the cephadm filesystem
    
    Signed-off-by: Michael Fritch <mfritch@suse.com>

--- src/cephadm/tests/fixtures.py
+++ src/cephadm/tests/fixtures.py
@@ -3,6 +3,8 @@ import os
 import pytest
 import time
 
+from pyfakefs import fake_filesystem
+
 
 with mock.patch('builtins.open', create=True):
     from importlib.machinery import SourceFileLoader
@@ -38,3 +40,25 @@ def exporter():
         exporter = cd.CephadmDaemon(ctx, fsid='foobar', daemon_id='test')
         assert exporter.token == 'MyAccessToken' 
         yield exporter
+
+
+@pytest.fixture()
+def cephadm_fs(
+    fs: fake_filesystem.FakeFilesystem,
+):
+    """
+    use pyfakefs to stub filesystem calls
+    """
+    uid = os.getuid()
+    gid = os.getgid()
+
+    with mock.patch('os.fchown'), \
+         mock.patch('cephadm.extract_uid_gid', return_value=(uid, gid)):
+
+            fs.create_dir(cd.DATA_DIR)
+            fs.create_dir(cd.LOG_DIR)
+            fs.create_dir(cd.LOCK_DIR)
+            fs.create_dir(cd.LOGROTATE_DIR)
+            fs.create_dir(cd.UNIT_DIR)
+
+            yield fs
