d20af990e85d4c4e007eca1b18cd41b7748266fd
Kefu Chai <kchai@redhat.com>
2021-05-24 11:51:09 +0800

    pybind/mgr/dashboard: drop "from __future__ import absolute_import"
    since we've migrated to python3, there is no need to import
    absolute_import for accessing absolute import anymore.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/pybind/mgr/dashboard/controllers/task.py
+++ src/pybind/mgr/dashboard/controllers/task.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import
 
 from ..services import progress
 from ..tools import TaskManager
--- src/pybind/mgr/dashboard/services/sso.py
+++ src/pybind/mgr/dashboard/services/sso.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=too-many-return-statements,too-many-branches
-from __future__ import absolute_import
 
 import errno
 import json
