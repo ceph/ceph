ceph/ceph

commit 3ea5242e381a850c080ee9edbaeea28059ad4da9

vstart: define helper for parsing block_devs options
for better readability, and to simplify the code

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/vstart.sh
@@ -248,6 +248,18 @@ usage_exit() {
     exit
 }
 
+parse_block_devs() {
+    local opt_name=$1
+    shift
+    IFS=',' read -r -a block_devs <<< "$2"
+    for dev in "${block_devs[@]}"; do
+        if [ ! -b $dev -o ! -w $dev ]; then
+            echo "All $opt_name must refer to writable block devices"
+            exit 1
+        fi
+    done
+}
+
 while [ $# -ge 1 ]; do
 case $1 in
     -d | --debug)
@@ -437,13 +449,7 @@ case $1 in
         with_mgr_dashboard=false
         ;;
     --seastore-devs)
-        IFS=',' read -r -a block_devs <<< "$2"
-        for dev in "${block_devs[@]}"; do
-            if [ ! -b $dev -o ! -w $dev ]; then
-                echo "All --seastore-devs must refer to writable block devices"
-                exit 1
-            fi
-        done
+        parse_block_devs --seastore-devs
         shift
         ;;
     --bluestore-spdk)
@@ -453,13 +459,7 @@ case $1 in
         shift
         ;;
     --bluestore-devs)
-        IFS=',' read -r -a block_devs <<< "$2"
-        for dev in "${block_devs[@]}"; do
-            if [ ! -b $dev -o ! -w $dev ]; then
-                echo "All --bluestore-devs must refer to writable block devices"
-                exit 1
-            fi
-        done
+        parse_block_devs --bluestore-devs
         shift
         ;;
     --bluestore-zoned)

commit f9a3921eff1e749a4e9a8709ce9d1b9caf47c6a3

vstart.sh: pass the addition option to parse_block_devs()
to address the regression introduced by
3ea5242e381a850c080ee9edbaeea28059ad4da9

Signed-off-by: Kefu Chai <kchai@redhat.com>

src/vstart.sh
@@ -251,7 +251,9 @@ usage_exit() {
 parse_block_devs() {
     local opt_name=$1
     shift
-    IFS=',' read -r -a block_devs <<< "$2"
+    local devs=$1
+    shift
+    IFS=',' read -r -a block_devs <<< "$devs"
     for dev in "${block_devs[@]}"; do
         if [ ! -b $dev -o ! -w $dev ]; then
             echo "All $opt_name must refer to writable block devices"
@@ -449,7 +451,7 @@ case $1 in
         with_mgr_dashboard=false
         ;;
     --seastore-devs)
-        parse_block_devs --seastore-devs
+        parse_block_devs --seastore-devs "$2"
         shift
         ;;
     --bluestore-spdk)
@@ -459,7 +461,7 @@ case $1 in
         shift
         ;;
     --bluestore-devs)
-        parse_block_devs --bluestore-devs
+        parse_block_devs --bluestore-devs "$2"
         shift
         ;;
     --bluestore-zoned)