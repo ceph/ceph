ef91f619e336a3115fc5519ea16615a2163227bc
Pere Diaz Bou <pdiazbou@redhat.com>
2021-05-31 14:42:27 +0200

    mgr: move injection to ClusterState
    Signed-off-by: Pere Diaz Bou <pdiazbou@redhat.com>

--- src/mgr/Injector.h
+++ src/mgr/Injector.h
@@ -10,9 +10,10 @@
 class Injector {
 private:
   static int64_t get_num_osds();
-  static void mark_exists_osds(OSDMap* osdmap);
+  static void mark_exists_osds(OSDMap *osdmap);
 
 public:
   static PyObject *get_python(const std::string &what);
+  static OSDMap *get_osdmap();
 };
 
