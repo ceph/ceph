c351c47f7c700a113781519e2bc88dc6bb5f9044
Kefu Chai <kchai@redhat.com>
2021-05-31 11:13:55 +0800

    test/librados/list: print reason why test fails
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/test/librados/list.cc
+++ src/test/librados/list.cc
@@ -480,8 +480,12 @@ TEST_F(LibRadosList, EnumerateObjectsSplit) {
 
   // Ensure a non-power-of-two PG count to avoid only
   // touching the easy path.
-  ASSERT_TRUE(set_pg_num(&s_cluster, pool_name, 11).empty());
-  ASSERT_TRUE(set_pgp_num(&s_cluster, pool_name, 11).empty());
+  if (auto error = set_pg_num(&s_cluster, pool_name, 11); !error.empty()) {
+    GTEST_FAIL() << error;
+  }
+  if (auto error = set_pgp_num(&s_cluster, pool_name, 11); !error.empty()) {
+    GTEST_FAIL() << error;
+  }
 
   rados_object_list_cursor begin = rados_object_list_begin(ioctx);
   rados_object_list_cursor end = rados_object_list_end(ioctx);
