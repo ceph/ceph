8e3292c04a3c28b28bf93e98ce57c98a2938bf64
Avan Thakkar <athakkar@localhost.localdomain>
2021-06-01 18:25:15 +0530

    mgr/dashboard: "Create Cluster" workflow (e2e tests)
    Fixes: https://tracker.ceph.com/issues/50336
    Signed-off-by: Avan Thakkar <athakkar@redhat.com>

---
+++ src/pybind/mgr/dashboard/frontend/cypress/integration/cluster/cluster-welcome-page.po.ts
@@ -0,0 +1,27 @@
+import { PageHelper } from '../page-helper.po';
+import { NotificationSidebarPageHelper } from '../ui/notification.po';
+
+export class CreateClusterWelcomePageHelper extends PageHelper {
+  pages = {
+    index: { url: '#/create-cluster', id: 'cd-create-cluster' }
+  };
+
+  createCluster() {
+    cy.get('cd-create-cluster').should('contain.text', 'Welcome to Ceph');
+    cy.get('[name=create-cluster]').click();
+
+    cy.get('cd-dashboard').should('exist');
+    const notification = new NotificationSidebarPageHelper();
+    notification.open();
+    notification.getNotifications().should('contain', 'Cluster creation feature not implemented');
+  }
+
+  doSkip() {
+    cy.get('[name=skip-cluster-creation]').click();
+
+    cy.get('cd-dashboard').should('exist');
+    const notification = new NotificationSidebarPageHelper();
+    notification.open();
+    notification.getNotifications().should('contain', 'Cluster creation skipped by user');
+  }
+}
