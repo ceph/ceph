ceph/ceph

commit b1b44d775df3160d937c068d5e1079e24199ed6b

mds: do not assert when receiving a unknow metric type
Fixes: https://tracker.ceph.com/issues/50822
Signed-off-by: Xiubo Li <xiubli@redhat.com>

src/include/cephfs/metrics/Types.h
@@ -53,7 +53,7 @@ inline std::ostream &operator<<(std::ostream &os, const ClientMetricType &type) 
     os << "OPENED_INODES";
     break;
   default:
-    ceph_abort();
+    os << "Unknown metric type: " << type;
   }
 
   return os;

commit 20a622a87ea4cd9671d975d09d33363726f4fc69

mds: do not infinitely recursively print a metric
Fixes: b1b44d775df3160d937c068d5e1079e24199ed6b
Fixes: https://tracker.ceph.com/issues/51067
Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

src/include/cephfs/metrics/Types.h
@@ -53,7 +53,8 @@ inline std::ostream &operator<<(std::ostream &os, const ClientMetricType &type) 
     os << "OPENED_INODES";
     break;
   default:
-    os << "Unknown metric type: " << type;
+    os << "(UNKNOWN:" << static_cast<std::underlying_type<ClientMetricType>::type>(type) << ")";
+    break;
   }
 
   return os;