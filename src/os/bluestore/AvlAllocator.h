19c36e2024c00bed4720dd56af49940484db42c6
Kefu Chai <kchai@redhat.com>
2021-06-02 15:57:04 +0800

    os/bluestore/AvlAllocator: introduce bluestore_avl_alloc_ff_max_search_count
    so AvlAllocator can switch from the first-first mode to best-fit mode
    without walking through the whole space map tree. in the
    highly-fragmented system, iterating the whole tree could hurt the
    performance of fast storage system a lot.
    
    the idea comes from openzfs's metaslab allocator.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>
    (cherry picked from commit 40f05b971f5a8064cf9819f80fc3bbf21d5206da)
    Signed-off-by: Mauricio Faria de Oliveira <mfo@canonical.com>
    
     Conflicts:
    	src/common/options/global.yaml.in
    	- Moved new option into src/common/options.cc

--- src/os/bluestore/AvlAllocator.h
+++ src/os/bluestore/AvlAllocator.h
@@ -163,7 +163,12 @@ private:
    * switch to using best-fit allocations.
    */
   int range_size_alloc_free_pct = 0;
-
+  /*
+   * Maximum number of segments to check in the first-fit mode, without this
+   * limit, fragmented device can see lots of iterations and _block_picker()
+   * becomes the performance limiting factor on high-performance storage.
+   */
+  const uint32_t max_search_count;
   /*
   * Max amount of range entries allowed. 0 - unlimited
   */
