fb2a7d768490a83218590cb63aa9b3e2a1471ec5
Xiubo Li <xiubli@redhat.com>
2021-06-03 13:25:36 +0800

    mds: fix the comment in add_inode
    Signed-off-by: Xiubo Li <xiubli@redhat.com>
    (cherry picked from commit e02c3295534bc771fee79afbab7bf32abff33cb7)

--- src/mds/MDCache.cc
+++ src/mds/MDCache.cc
@@ -257,9 +257,9 @@ bool MDCache::shutdown()
 // ====================================================================
 // some inode functions
 
-void MDCache::add_inode(CInode *in) 
+void MDCache::add_inode(CInode *in)
 {
-  // add to lru, inode map
+  // add to inode map
   if (in->last == CEPH_NOSNAP) {
     auto &p = inode_map[in->ino()];
     ceph_assert(!p); // should be no dup inos!
