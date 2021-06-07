d2b0382ec6f67c4f6f2eb422c676db4ea561e490
Kefu Chai <kchai@redhat.com>
2021-06-07 16:39:22 +0800

    cmake: stop detecting <experimental/filesystem>
    since we've dropped the support of GCC older than v8.0, there is no need
    to detect <experimental/filesystem>
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- cmake/modules/FindStdFilesystem_test.cc
+++ cmake/modules/FindStdFilesystem_test.cc
@@ -1,12 +1,6 @@
-#if __has_include(<filesystem>)
 #include <filesystem>
+
 namespace fs = std::filesystem;
-#elif __has_include(<experimental/filesystem>)
-#include <experimental/filesystem>
-namespace fs = std::experimental::filesystem;
-#else
-#error std::filesystem not available!
-#endif
 
 int main() {
     fs::create_directory("sandbox");
