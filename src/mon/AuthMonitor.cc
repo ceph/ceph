fa337b159ce34fd48e39360c6884e4184d0eccfa
Rishabh Dave <ridave@redhat.com>
2021-06-09 13:39:12 +0530

    AuthMonitor: update caps when no caps are present in keyring
    If "fs authorize" subcommand is executed for a client that already has
    a keyring but with no caps present in it, the command should update the
    keyring with the caps supplied instead of quitting with an error message.
    
    Example -
    
    $ ./bin/ceph auth add client.x
    added key for client.x
    $ ./bin/ceph auth get client.x
    [client.x]
            key = AQCqOrJgtsJDHBAARGzbd1sj+ycRtWHOAcWz1w==
    exported keyring for client.x
    
    Before this commit -
    
    $ ./bin/ceph fs authorize a client.x / rw
    Error EINVAL: client.x already has fs capabilities that differ from those supplied. To generate a new auth key for client.x, first remove client.x from configuration files, execute 'ceph auth rm client.x', then execute this command again.
    
    After this commit -
    
    $ ./bin/ceph fs authorize a client.x1 / rw
    updated caps for client.x1
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- src/mon/AuthMonitor.cc
+++ src/mon/AuthMonitor.cc
@@ -1657,19 +1657,6 @@ bool AuthMonitor::prepare_command(MonOpRequestRef op)
 
     EntityAuth e_auth;
     if (mon.key_server.get_auth(entity, e_auth)) {
-      for (auto it = newcaps.begin(); it != newcaps.end(); it += 2) {
-	string& cap_entity = *it;
-        if (entity_auth.caps.count(cap_entity) == 0) {
-          ss << entity << " already has fs capabilities that differ from "
-             << "those supplied. To generate a new auth key for " << entity
-             << ", first remove " << entity << " from configuration files, "
-             << "execute 'ceph auth rm " << entity << "', then execute this "
-             << "command again.";
-          err = -EINVAL;
-          goto done;
-        }
-      }
-
       bool all_caps_same = _gen_wanted_caps(e_auth, newcaps, filesystem);
 
       if (all_caps_same) {
@@ -1763,6 +1750,11 @@ bool AuthMonitor::_gen_wanted_caps(EntityAuth& e_auth, vector<string>& newcaps,
   for (auto it = newcaps.begin(); it != newcaps.end(); it += 2) {
     string& cap_entity = *it;
     string& ncap = *(it + 1);
+
+    if (e_auth.caps.count(cap_entity) == 0) {
+      all_caps_same = false;
+      continue;
+    }
 
     string ecap; // ecap = entity cap
     auto i = e_auth.caps[cap_entity].cbegin();
