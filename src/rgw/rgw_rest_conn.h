1e48366c7bc9610abceb4b91377807a67935886e
Soumya Koduri <skoduri@redhat.com>
2021-06-23 21:16:24 +0530

    rgw/RGWRESTConn: Define a wrapper to send PUT/POST stream request
    Similar to "get_resource()", add an API "send_resource()" to send
    PUT/POST/DELETE Stream request on RGWRestConn
    
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/rgw/rgw_rest_conn.h
+++ src/rgw/rgw_rest_conn.h
@@ -200,6 +200,16 @@ public:
                    RGWHTTPManager *mgr,
                    optional_yield y);
 
+  int send_resource(const DoutPrefixProvider *dpp,
+                   const string& method,
+                   const string& resource,
+		           rgw_http_param_pair *extra_params,
+                   map<string, string>* extra_headers,
+                   bufferlist& bl,
+                   bufferlist *send_data,
+                   RGWHTTPManager *mgr,
+                   optional_yield y);
+
   template <class T>
   int get_json_resource(const DoutPrefixProvider *dpp, const std::string& resource, param_vec_t *params,
                         bufferlist *in_data, optional_yield y, T& t);
