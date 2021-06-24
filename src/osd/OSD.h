0a5f87cc8a52febd98c4115e7ba78743a2636eb2
Sridhar Seshasayee <sseshasa@redhat.com>
2021-06-24 13:14:28 +0530

    osd: Add method to store config option key/value on the MON store
    Add method mon_cmd_set_config() to save config option key and
    value to the MON store. The ConfigMonitor command, 'config set' is
    used to achieve this.
    
    A corresponding get method is unnecessary since any config option
    found on the MON store is loaded during OSD boot-up and set using
    the md_config_t::set_mon_vals() method. Therefore, the existing
    versions of ConfigProxy::get_val() method are sufficient to get
    the latest value for the config option.
    
    Fixes: https://tracker.ceph.com/issues/51464
    Signed-off-by: Sridhar Seshasayee <sseshasa@redhat.com>
    (cherry picked from commit 1fca4bdfd4c919907247f8f31a3f9e6ca7a11653)

--- src/osd/OSD.h
+++ src/osd/OSD.h
@@ -2071,6 +2071,7 @@ private:
                          int64_t onum,
                          double *elapsed,
                          std::ostream& ss);
+  int mon_cmd_set_config(const std::string &key, const std::string &val);
 
   void scrub_purged_snaps();
   void probe_smart(const std::string& devid, std::ostream& ss);
