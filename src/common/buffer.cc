e7cd86c624f99a967b872bff91357cacf208965f
Kefu Chai <kchai@redhat.com>
2021-06-29 13:52:04 +0800

    common/buffers: check _num directly in list::c_str()
    no need to create temporary iterator for comparing it with cend().
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/common/buffer.cc
+++ src/common/buffer.cc
@@ -1519,16 +1519,18 @@ static ceph::spinlock debug_lock;
    */
   char *buffer::list::c_str()
   {
-    if (_buffers.empty())
-      return 0;                         // no buffers
-
-    auto iter = std::cbegin(_buffers);
-    ++iter;
-
-    if (iter != std::cend(_buffers)) {
+    switch (get_num_buffers()) {
+    case 0:
+      // no buffers
+      return nullptr;
+    case 1:
+      // good, we're already contiguous.
+      break;
+    default:
       rebuild();
+      break;
     }
-    return _buffers.front().c_str();  // good, we're already contiguous.
+    return _buffers.front().c_str();
   }
 
   string buffer::list::to_str() const {
