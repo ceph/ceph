4dc4f216a6f2d6c1ed837f8bc810cd962b3f023a
Matt Benjamin <mbenjamin@redhat.com>
2021-06-29 13:54:35 -0400

    rgw: introduce new /{admin}/info api resource
    The new resource returns an array of informational data, which
    currently consists of the RADOS cluster fsid as "cluster_fsid."
    
    Fixes: https://tracker.ceph.com/issues/51432
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_common.cc
+++ src/rgw/rgw_common.cc
@@ -1991,6 +1991,7 @@ bool RGWUserCaps::is_valid_cap_type(const string& tp)
                                     "users",
                                     "buckets",
                                     "metadata",
+				    "info",
                                     "usage",
                                     "zone",
                                     "bilog",
