865d13e9fa043954dce5110f4f8ae693af9604ce
mengxiangrui <mengxr@chinatelecom.cn>
2021-07-17 05:22:42 +0800

    rgw: avoid occuring radosgw daemon crash when access a conditionally redirected static website
    Fixes: https://tracker.ceph.com/issues/51715
    Signed-off-by: xiangrui meng <mengxr@chinatelecom.cn>
    Signed-off-by: yupeng chen <chenyupeng@chinatelecom.cn>

--- src/rgw/rgw_website.cc
+++ src/rgw/rgw_website.cc
@@ -45,7 +45,9 @@ void RGWBWRoutingRule::apply_rule(const string& default_protocol, const string& 
 
   if (!redirect_info.replace_key_prefix_with.empty()) {
     *new_url += redirect_info.replace_key_prefix_with;
-    *new_url += key.substr(condition.key_prefix_equals.size());
+    if (key.size() > condition.key_prefix_equals.size()) {
+      *new_url += key.substr(condition.key_prefix_equals.size());
+    }
   } else if (!redirect_info.replace_key_with.empty()) {
     *new_url += redirect_info.replace_key_with;
   } else {
