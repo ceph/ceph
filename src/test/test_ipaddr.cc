ce5eaa122f2bc8458059544431343a4931f8d3ec
Kefu Chai <kchai@redhat.com>
2021-07-19 11:26:50 +0800

    test/test_ipaddr: avoid reinterpret cast
    drop couple occurrences of C-style cast, for better readability.
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/test/test_ipaddr.cc
+++ src/test/test_ipaddr.cc
@@ -215,13 +215,13 @@ TEST(CommonIPAddr, TestV4_SkipLoopback)
     find_ip_in_subnet_list(nullptr, (struct ifaddrs*)&one,
                            CEPH_PICK_ADDRESS_IPV4 | CEPH_PICK_ADDRESS_IPV6,
                            "", "");
-  ASSERT_EQ((struct sockaddr*)&a_three, result);
+  ASSERT_EQ(three.ifa_addr, result);
   // the subnet criteria leaves us no choice but the UP loopback address
   result =
     find_ip_in_subnet_list(nullptr, (struct ifaddrs*)&one,
                            CEPH_PICK_ADDRESS_IPV4 | CEPH_PICK_ADDRESS_IPV6,
                            "127.0.0.0/8", "");
-  ASSERT_EQ((struct sockaddr*)&a_two, result);
+  ASSERT_EQ(two.ifa_addr, result);
 }
 
 TEST(CommonIPAddr, TestV6_Simple)
@@ -346,13 +346,13 @@ TEST(CommonIPAddr, TestV6_SkipLoopback)
     find_ip_in_subnet_list(nullptr, (struct ifaddrs*)&one,
                            CEPH_PICK_ADDRESS_IPV4 | CEPH_PICK_ADDRESS_IPV6,
                            "", "");
-  ASSERT_EQ((struct sockaddr*)&a_three, result);
+  ASSERT_EQ(three.ifa_addr, result);
   // the subnet criteria leaves us no choice but the UP loopback address
   result =
     find_ip_in_subnet_list(nullptr, (struct ifaddrs*)&one,
                            CEPH_PICK_ADDRESS_IPV4 | CEPH_PICK_ADDRESS_IPV6,
                            "::1/128", "");
-  ASSERT_EQ((struct sockaddr*)&a_two, result);
+  ASSERT_EQ(two.ifa_addr, result);
 }
 
 TEST(CommonIPAddr, ParseNetwork_Empty)
@@ -710,7 +710,7 @@ TEST(pick_address, find_ip_in_subnet_list)
     CEPH_PICK_ADDRESS_IPV4,
     "10.1.0.0/16",
     "eth0");
-  ASSERT_EQ((struct sockaddr*)&a_one, result);
+  ASSERT_EQ(one.ifa_addr, result);
 
   result = find_ip_in_subnet_list(
     cct.get(),
@@ -718,7 +718,7 @@ TEST(pick_address, find_ip_in_subnet_list)
     CEPH_PICK_ADDRESS_IPV4,
     "10.2.0.0/16",
     "eth1");
-  ASSERT_EQ((struct sockaddr*)&a_two, result);
+  ASSERT_EQ(two.ifa_addr, result);
 
   // match by eth name
   result = find_ip_in_subnet_list(
@@ -727,7 +727,7 @@ TEST(pick_address, find_ip_in_subnet_list)
     CEPH_PICK_ADDRESS_IPV4,
     "10.0.0.0/8",
     "eth0");
-  ASSERT_EQ((struct sockaddr*)&a_one, result);
+  ASSERT_EQ(one.ifa_addr, result);
 
   result = find_ip_in_subnet_list(
     cct.get(),
@@ -735,7 +735,7 @@ TEST(pick_address, find_ip_in_subnet_list)
     CEPH_PICK_ADDRESS_IPV4,
     "10.0.0.0/8",
     "eth1");
-  ASSERT_EQ((struct sockaddr*)&a_two, result);
+  ASSERT_EQ(two.ifa_addr, result);
 
   result = find_ip_in_subnet_list(
     cct.get(),
@@ -743,7 +743,7 @@ TEST(pick_address, find_ip_in_subnet_list)
     CEPH_PICK_ADDRESS_IPV6,
     "2001::/16",
     "eth1");
-  ASSERT_EQ((struct sockaddr*)&a_three, result);
+  ASSERT_EQ(three.ifa_addr, result);
 }
 
 TEST(pick_address, filtering)
