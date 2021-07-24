636faf933c615f3a0c8752ed875b2c503468096d
Kefu Chai <kchai@redhat.com>
2021-07-24 17:39:55 +0800

    cmake: define OpenLDAP::OpenLDAP library
    * define OpenLDAP::OpenLDAP, so this library can be consumed in a simpler way.
    * use OpenLDAP::OpenLDAP instead of OpenLDAP_LIBRARIES when appropriate
    * do not link against unused ${OpenLDAP_LIBRARIES}
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- cmake/modules/FindOpenLDAP.cmake
+++ cmake/modules/FindOpenLDAP.cmake
@@ -16,7 +16,26 @@ include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(OpenLDAP DEFAULT_MSG
   OpenLDAP_INCLUDE_DIR LDAP_LIBRARY LBER_LIBRARY)
 
-set(OpenLDAP_LIBRARIES ${LDAP_LIBRARY} ${LBER_LIBRARY})
-
 mark_as_advanced(
   OpenLDAP_INCLUDE_DIR LDAP_LIBRARY LBER_LIBRARY)
+
+if(OpenLDAP_FOUND)
+  set(OpenLDAP_LIBRARIES ${LDAP_LIBRARY} ${LBER_LIBRARY})
+  if(NOT TARGET OpenLDAP::OpenLDAP)
+    add_library(OpenLDAP::LDAP UNKNOWN IMPORTED)
+    set_target_properties(OpenLDAP::LDAP PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${OpenLDAP_INCLUDE_DIR}"
+      IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+      IMPORTED_LOCATION "${LDAP_LIBRARY}")
+    add_library(OpenLDAP::BER UNKNOWN IMPORTED)
+    set_target_properties(OpenLDAP::BER PROPERTIES
+      INTERFACE_INCLUDE_DIRECTORIES "${OpenLDAP_INCLUDE_DIR}"
+      IMPORTED_LINK_INTERFACE_LANGUAGES "C"
+      IMPORTED_LOCATION "${LBER_LIBRARY}")
+    add_library(openldap INTERFACE)
+    target_link_libraries(openldap INTERFACE
+      OpenLDAP::LDAP
+      OpenLDAP::BER)
+    add_library(OpenLDAP::OpenLDAP ALIAS openldap)
+  endif()
+endif()
