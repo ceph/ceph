419a43a0a8a6c6fe18f7cd4700b4e7796d37aa9e
Alfonso Martínez <almartin@redhat.com>
2021-07-27 10:51:18 +0200

    mgr/dashboard: cephadm-e2e script: improvements
    Improvements and some adaptations related to the jenkins job.
    
    Fixes: https://tracker.ceph.com/issues/51612
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>
    (cherry picked from commit 65b75000b7694cb3cbe617bbec28c513a2522be8)

--- src/pybind/mgr/dashboard/ci/cephadm/bootstrap-cluster.sh
+++ src/pybind/mgr/dashboard/ci/cephadm/bootstrap-cluster.sh
@@ -2,21 +2,14 @@
 
 export PATH=/root/bin:$PATH
 mkdir /root/bin
-{% if ceph_dev_folder is defined %}
-  cp /mnt/{{ ceph_dev_folder }}/src/cephadm/cephadm /root/bin/cephadm
-{% else %}
-  cd /root/bin
-  curl --silent --remote-name --location https://raw.githubusercontent.com/ceph/ceph/master/src/cephadm/cephadm
-{% endif %}
+
+cp /mnt/{{ ceph_dev_folder }}/src/cephadm/cephadm /root/bin/cephadm
 chmod +x /root/bin/cephadm
 mkdir -p /etc/ceph
 mon_ip=$(ifconfig eth0  | grep 'inet ' | awk '{ print $2}')
-{% if ceph_dev_folder is defined %}
-  cephadm bootstrap --mon-ip $mon_ip --initial-dashboard-password {{ admin_password }} --allow-fqdn-hostname --dashboard-password-noupdate --shared_ceph_folder /mnt/{{ ceph_dev_folder }}
-{% else %}
-  cephadm bootstrap --mon-ip $mon_ip --initial-dashboard-password {{ admin_password }} --allow-fqdn-hostname --dashboard-password-noupdate
-{% endif %}
-fsid=$(cat /etc/ceph/ceph.conf | grep fsid | awk '{ print $3}')
+
+cephadm bootstrap --mon-ip $mon_ip --initial-dashboard-password {{ admin_password }} --allow-fqdn-hostname --dashboard-password-noupdate --shared_ceph_folder /mnt/{{ ceph_dev_folder }}
+
 {% for number in range(1, nodes) %}
   ssh-copy-id -f -i /etc/ceph/ceph.pub  -o StrictHostKeyChecking=no root@{{ prefix }}-node-0{{ number }}.{{ domain }}
 {% endfor %}
