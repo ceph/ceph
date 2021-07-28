191f50e05a41a91926129b56112659f83eb752d5
J. Eric Ivancich <ivancich@redhat.com>
2021-07-28 13:52:29 -0400

    rgw: user stats showing 0 value for "size_utilized" and "size_kb_utilized" fields
    When accumulating user stats, the "utilized" fields are not looked
    at. Updates RGWStorageStats::dump so it only outputs the "utilized"
    data if they're updated.
    
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -7483,12 +7483,13 @@ next:
       }
     }
 
-    RGWStorageStats stats;
+    constexpr bool omit_utilized_stats = false;
+    RGWStorageStats stats(omit_utilized_stats);
     ceph::real_time last_stats_sync;
     ceph::real_time last_stats_update;
     int ret = static_cast<rgw::sal::RadosStore*>(store)->ctl()->user->read_stats(dpp(), user->get_id(), &stats, null_yield,
-					     &last_stats_sync,
-					     &last_stats_update);
+										 &last_stats_sync,
+										 &last_stats_update);
     if (ret < 0) {
       if (ret == -ENOENT) { /* in case of ENOENT */
         cerr << "User has not been initialized or user does not exist" << std::endl;
