c05447fd13ec87414d74cef5c6d6924e6d06906b
Kefu Chai <kchai@redhat.com>
2021-07-31 20:44:57 +0800

    script/run_tox.sh: drop support of python2
    since we don't support python2 anymore, there is not need to mention or
    support python2 when performing tests
    
    Signed-off-by: Kefu Chai <kchai@redhat.com>

--- src/script/run_tox.sh
+++ src/script/run_tox.sh
@@ -25,13 +25,13 @@ options:
 
 example:
 
-following command will run tox with envlist of "py27,py3" using the "tox.ini" in current directory.
+following command will run tox with envlist of "py3,mypy" using the "tox.ini" in current directory.
 
-  $prog_name --tox-envs py27,py3
+  $prog_name --tox-envs py3,mypy
 
-following command will run tox with envlist of "py27" using "/ceph/src/python-common/tox.ini"
+following command will run tox with envlist of "py3" using "/ceph/src/python-common/tox.ini"
 
-  $prog_name --tox-envs py27 --tox-path /ceph/src/python-common
+  $prog_name --tox-envs py3 --tox-path /ceph/src/python-common
 EOF
 }
 
@@ -116,17 +116,7 @@ function main() {
             echo "$PWD already exists, but it's not a virtualenv. test_name empty?"
             exit 1
         fi
-        # try to use the prefered python for creating the virtual env for
-        # bootstrapping tox.
-        case $tox_envs in
-            py3*)
-                virtualenv_python=python3;;
-            py2*)
-                virtualenv_python=python2;;
-            *)
-                virtualenv_python=python3;;
-        esac
-        $source_dir/src/tools/setup-virtualenv.sh --python=${virtualenv_python} ${venv_path}
+        $source_dir/src/tools/setup-virtualenv.sh ${venv_path}
     fi
     source ${venv_path}/bin/activate
     pip install tox
