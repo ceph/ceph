c4ac53ef43739a6485081908a8eae0336124f3c6
Benoît Knecht <bknecht@protonmail.ch>
2021-08-05 16:08:49 +0200

    rgw: Use 100-continue in OPA requests to reduce latency
    When we send a POST request to Open Policy Agent, we send the headers first,
    wait for TCP ACK from the server, and then send the body. But due to [delayed
    ACK], the TCP ACK can be delayed by up to 500 ms (but typically 40 ms on
    Linux), which in this case is huge as OPA typically takes only 1 ms to process
    a request.
    
    This commit adds the
    
    ```
    Expect: 100-continue
    ```
    
    header to the `POST` requests sent to OPA, which makes the server reply with
    
    ```
    HTTP/1.1 100 Continue
    ```
    
    immediately after receiving the headers, thereby avoiding the 40 ms delay.
    
    [delayed ACK]: https://en.wikipedia.org/wiki/TCP_delayed_acknowledgment
    
    Fixes: https://tracker.ceph.com/issues/52067
    Signed-off-by: Benoît Knecht <bknecht@protonmail.ch>
    (cherry picked from commit 871d18040b7b23deb96c2eefcd1fbf2039690816)

--- src/rgw/rgw_opa.cc
+++ src/rgw/rgw_opa.cc
@@ -31,6 +31,7 @@ int rgw_opa_authorize(RGWOp *& op,
   /* set required headers for OPA request */
   req.append_header("X-Auth-Token", opa_token);
   req.append_header("Content-Type", "application/json");
+  req.append_header("Expect", "100-continue");
 
   /* check if we want to verify OPA server SSL certificate */
   req.set_verify_ssl(s->cct->_conf->rgw_opa_verify_ssl);
