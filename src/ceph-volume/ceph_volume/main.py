13cd2e4039a32915a6f697c31533ec63bef5e923
Sage Weil <sage@newdream.net>
2021-08-05 13:29:17 -0400

    ceph-volume: top-level 'activate' command
    First try raw, then lvm.
    
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/ceph-volume/ceph_volume/main.py
+++ src/ceph-volume/ceph_volume/main.py
@@ -6,7 +6,7 @@ import sys
 import logging
 
 from ceph_volume.decorators import catches
-from ceph_volume import log, devices, configuration, conf, exceptions, terminal, inventory, drive_group
+from ceph_volume import log, devices, configuration, conf, exceptions, terminal, inventory, drive_group, activate
 
 
 class Volume(object):
@@ -29,6 +29,7 @@ Ceph Conf: {ceph_path}
             'simple': devices.simple.Simple,
             'raw': devices.raw.Raw,
             'inventory': inventory.Inventory,
+            'activate': activate.Activate,
             'drive-group': drive_group.Deploy,
         }
         self.plugin_help = "No plugins found/loaded"
