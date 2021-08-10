98b6a010d36ed6387e978974563f2d974400a9a9
Adam Kupczyk <akupczyk@redhat.com>
2021-08-10 15:15:52 +0200

    os/bluestore/bluefs: Rearrange locks in prealocate
    Rearranged locks in preallocate to avoid possible deadlock with
    compact_log_async_dump_metadata_NF.
    Cycle was:
    L->N rename/mkdir
    N->F compact_log_async_dump_metadata_NF
    F->L preallocate
    
    Signed-off-by: Adam Kupczyk <akupczyk@redhat.com>

--- src/os/bluestore/BlueFS.h
+++ src/os/bluestore/BlueFS.h
@@ -711,8 +711,8 @@ public:
  * log        L |     <   <         
  * dirty      D |                   
  * nodes      N |             <     
- * File       F | <                 
- * FileWriter W | <  <        <      
+ * File       F |                  
+ * FileWriter W | <   <       <      
  * 
  * Claim: Deadlock is possible IFF graph contains cycles.
  */
