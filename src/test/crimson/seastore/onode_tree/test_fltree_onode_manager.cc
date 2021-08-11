1c0e6e7c8d2181e6cc2f69cb1ab8c988f5279b96
Chunmei Liu <chunmei.liu@intel.com>
2021-08-10 20:22:17 -0700

    crimson/seastore: change unit test using hint
    Signed-off-by: Chunmei Liu <chunmei.liu@intel.com>

--- src/test/crimson/seastore/onode_tree/test_fltree_onode_manager.cc
+++ src/test/crimson/seastore/onode_tree/test_fltree_onode_manager.cc
@@ -31,15 +31,15 @@ struct onode_item_t {
   void initialize(Transaction& t, Onode& value) const {
     auto& layout = value.get_mutable_layout(t);
     layout.size = size;
-    layout.omap_root.update(omap_root_t(id, cnt_modify));
+    layout.omap_root.update(omap_root_t(id, cnt_modify, value.get_hint()));
     validate(value);
   }
 
   void validate(Onode& value) const {
     auto& layout = value.get_layout();
     ceph_assert(laddr_t(layout.size) == laddr_t{size});
-    ceph_assert(layout.omap_root.get().addr == id);
-    ceph_assert(layout.omap_root.get().depth == cnt_modify);
+    ceph_assert(layout.omap_root.get(value.get_hint()).addr == id);
+    ceph_assert(layout.omap_root.get(value.get_hint()).depth == cnt_modify);
   }
 
   void modify(Transaction& t, Onode& value) {
