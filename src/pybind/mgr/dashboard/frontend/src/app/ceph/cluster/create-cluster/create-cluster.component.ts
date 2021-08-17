12886bdab1940f24edf7f6cedfbd6ba4c81c56be
Nizamudeen A <nia@redhat.com>
2021-08-17 19:04:31 +0530

    mgr/dashboard: Cluster Creation Create OSDs Section
    Create OSDs section in cluster creation wizard
    
    Fixes: https://tracker.ceph.com/issues/51991
    Fixes: https://tracker.ceph.com/issues/52298
    Signed-off-by: Nizamudeen A <nia@redhat.com>
    Signed-off-by: Aashish Sharma <aasharma@redhat.com>
    (cherry picked from commit ad1535eea465f5e9f096600e01df1e95813ebb18)

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/create-cluster/create-cluster.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/create-cluster/create-cluster.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnDestroy, TemplateRef, ViewChild } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, Output, TemplateRef, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import _ from 'lodash';
 import { forkJoin, Subscription } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 
 import { ClusterService } from '~/app/shared/api/cluster.service';
 import { HostService } from '~/app/shared/api/host.service';
+import { OsdService } from '~/app/shared/api/osd.service';
 import { ConfirmationModalComponent } from '~/app/shared/components/confirmation-modal/confirmation-modal.component';
-import { ActionLabelsI18n, AppConstants } from '~/app/shared/constants/app.constants';
+import { ActionLabelsI18n, AppConstants, URLVerbs } from '~/app/shared/constants/app.constants';
 import { NotificationType } from '~/app/shared/enum/notification-type.enum';
+import { FinishedTask } from '~/app/shared/models/finished-task';
 import { Permissions } from '~/app/shared/models/permissions';
 import { WizardStepModel } from '~/app/shared/models/wizard-steps';
 import { AuthStorageService } from '~/app/shared/services/auth-storage.service';
 import { ModalService } from '~/app/shared/services/modal.service';
 import { NotificationService } from '~/app/shared/services/notification.service';
+import { TaskWrapperService } from '~/app/shared/services/task-wrapper.service';
 import { WizardStepsService } from '~/app/shared/services/wizard-steps.service';
+import { DriveGroup } from '../osd/osd-form/drive-group.model';
 
 @Component({
   selector: 'cd-create-cluster',
@@ -29,10 +34,15 @@ export class CreateClusterComponent implements OnDestroy {
   currentStepSub: Subscription;
   permissions: Permissions;
   projectConstants: typeof AppConstants = AppConstants;
-  stepTitles = ['Add Hosts', 'Review'];
+  stepTitles = ['Add Hosts', 'Create OSDs', 'Review'];
   startClusterCreation = false;
   observables: any = [];
   modalRef: NgbModalRef;
+  driveGroup = new DriveGroup();
+  driveGroups: Object[] = [];
+
+  @Output()
+  submitAction = new EventEmitter();
 
   constructor(
     private authStorageService: AuthStorageService,
@@ -42,7 +52,10 @@ export class CreateClusterComponent implements OnDestroy {
     private notificationService: NotificationService,
     private actionLabels: ActionLabelsI18n,
     private clusterService: ClusterService,
-    private modalService: ModalService
+    private modalService: ModalService,
+    private taskWrapper: TaskWrapperService,
+    private osdService: OsdService,
+    private wizardStepService: WizardStepsService
   ) {
     this.permissions = this.authStorageService.getPermissions();
     this.currentStepSub = this.stepsService.getCurrentStep().subscribe((step: WizardStepModel) => {
@@ -95,17 +108,42 @@ export class CreateClusterComponent implements OnDestroy {
       .subscribe({
         error: (error) => error.preventDefault()
       });
+
+    this.taskWrapper
+      .wrapTaskAroundCall({
+        task: new FinishedTask('osd/' + URLVerbs.CREATE, {
+          tracking_id: _.join(_.map(this.driveGroups, 'service_id'), ', ')
+        }),
+        call: this.osdService.create(this.driveGroups)
+      })
+      .subscribe({
+        error: (error) => error.preventDefault(),
+        complete: () => {
+          this.submitAction.emit();
+        }
+      });
   }
 
   onNextStep() {
     if (!this.stepsService.isLastStep()) {
       this.hostService.list().subscribe((hosts) => {
         hosts.forEach((host) => {
-          if (host['status'] === 'maintenance') {
-            this.observables.push(this.hostService.update(host['hostname'], false, [], true));
+          const index = host['labels'].indexOf('_no_schedule', 0);
+          if (index > -1) {
+            host['labels'].splice(index, 1);
+            this.observables.push(this.hostService.update(host['hostname'], true, host['labels']));
           }
         });
       });
+      this.driveGroup = this.wizardStepService.sharedData;
+      this.stepsService.getCurrentStep().subscribe((step: WizardStepModel) => {
+        this.currentStep = step;
+      });
+      if (this.currentStep.stepIndex === 2 && this.driveGroup) {
+        const user = this.authStorageService.getUsername();
+        this.driveGroup.setName(`dashboard-${user}-${_.now()}`);
+        this.driveGroups.push(this.driveGroup.spec);
+      }
       this.stepsService.moveToNextStep();
     } else {
       this.onSubmit();
