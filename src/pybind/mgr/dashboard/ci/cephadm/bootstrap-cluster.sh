f5382aba124ff207e0d280810d20572ae2057dc2
Nizamudeen A <nia@redhat.com>
2021-08-17 22:26:52 +0530

    mgr/dashboard: Move force maintenance test to the workflow test suite
    Fixes: https://tracker.ceph.com/issues/52276
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- src/pybind/mgr/dashboard/ci/cephadm/bootstrap-cluster.sh
+++ src/pybind/mgr/dashboard/ci/cephadm/bootstrap-cluster.sh
@@ -8,7 +8,7 @@ chmod +x /root/bin/cephadm
 mkdir -p /etc/ceph
 mon_ip=$(ifconfig eth0  | grep 'inet ' | awk '{ print $2}')
 
-cephadm bootstrap --mon-ip $mon_ip --initial-dashboard-password {{ admin_password }} --allow-fqdn-hostname --dashboard-password-noupdate --shared_ceph_folder /mnt/{{ ceph_dev_folder }}
+cephadm bootstrap --mon-ip $mon_ip --initial-dashboard-password {{ admin_password }} --allow-fqdn-hostname --skip-monitoring-stack --dashboard-password-noupdate --shared_ceph_folder /mnt/{{ ceph_dev_folder }}
 
 fsid=$(cat /etc/ceph/ceph.conf | grep fsid | awk '{ print $3}')
 
