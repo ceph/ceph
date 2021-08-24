e1cd1d91abb487349183043526c4d232b71d049e
Sebastian Wagner <sewagner@redhat.com>
2021-08-24 14:31:56 +0200

    python-common: Remove duplicated DriveGroupSpec.__repr__ and __eq__
    Signed-off-by: Sebastian Wagner <sewagner@redhat.com>

--- src/python-common/ceph/deployment/drive_group.py
+++ src/python-common/ceph/deployment/drive_group.py
@@ -330,21 +330,5 @@ class DriveGroupSpec(ServiceSpec):
         if self.method == 'raw' and self.objectstore == 'filestore':
             raise DriveGroupValidationError('method raw only supports bluestore')
 
-    def __repr__(self) -> str:
-        keys = [
-            key for key in self._supported_features if getattr(self, key) is not None
-        ]
-        if 'encrypted' in keys and not self.encrypted:
-            keys.remove('encrypted')
-        if 'objectstore' in keys and self.objectstore == 'bluestore':
-            keys.remove('objectstore')
-        return "DriveGroupSpec(name={}->{})".format(
-            self.service_id,
-            ', '.join('{}={}'.format(key, repr(getattr(self, key))) for key in keys)
-        )
-
-    def __eq__(self, other: Any) -> bool:
-        return repr(self) == repr(other)
-
 
 yaml.add_representer(DriveGroupSpec, DriveGroupSpec.yaml_representer)
