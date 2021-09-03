f3a21877e62776595ecf30bc51965d3615a342cd
Sage Weil <sage@newdream.net>
2021-09-03 16:41:27 -0500

    os/bluestore: properly reset zoned allocator on startup
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -6649,6 +6649,7 @@ int BlueStore::mkfs()
     freelist_type = "zoned";
     zone_size = bdev->get_zone_size();
     first_sequential_zone = bdev->get_conventional_region_size() / zone_size;
+    bdev->reset_all_zones();
   } else
 #endif
   {
