27648953c273597d6bd7a7de1ef7fa264d808d37
krunerge <krunerge@tencent.com>
2021-09-07 10:13:28 +0800

    cls/rbd: fix log text for children list

--- src/cls/rbd/cls_rbd.cc
+++ src/cls/rbd/cls_rbd.cc
@@ -4138,7 +4138,7 @@ int children_list(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
     return -EINVAL;
   }
 
-  CLS_LOG(20, "child_detach snap_id=%" PRIu64, snap_id);
+  CLS_LOG(20, "children_list snap_id=%" PRIu64, snap_id);
 
   cls_rbd_snap snap;
   std::string snapshot_key;
