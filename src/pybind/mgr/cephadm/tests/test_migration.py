02c942a093a28376301b9b4c66d9c712345ff953
Sebastian Wagner <sewagner@redhat.com>
2021-09-13 16:03:02 +0200

    mgr/cephadm: Add client.admin keyring when upgrading from older version
    Signed-off-by: Sebastian Wagner <sewagner@redhat.com>

--- src/pybind/mgr/cephadm/tests/test_migration.py
+++ src/pybind/mgr/cephadm/tests/test_migration.py
@@ -4,6 +4,7 @@ from ceph.deployment.service_spec import PlacementSpec, ServiceSpec, HostPlaceme
 from ceph.utils import datetime_to_str, datetime_now
 from cephadm import CephadmOrchestrator
 from cephadm.inventory import SPEC_STORE_PREFIX
+from cephadm.migrations import LAST_MIGRATION
 from cephadm.tests.fixtures import _run_cephadm, wait, with_host, receive_agent_metadata_all_hosts
 from cephadm.serve import CephadmServe
 from tests import mock
@@ -182,7 +183,7 @@ def test_migrate_nfs_initial(cephadm_module: CephadmOrchestrator):
         assert cephadm_module.migration_current == 2
 
         cephadm_module.migration.migrate()
-        assert cephadm_module.migration_current == 3
+        assert cephadm_module.migration_current == LAST_MIGRATION
 
 
 @mock.patch("cephadm.serve.CephadmServe._run_cephadm", _run_cephadm('[]'))
@@ -215,4 +216,15 @@ def test_migrate_nfs_initial_octopus(cephadm_module: CephadmOrchestrator):
         assert cephadm_module.migration_current == 2
 
         cephadm_module.migration.migrate()
-        assert cephadm_module.migration_current == 3
+        assert cephadm_module.migration_current == LAST_MIGRATION
+
+
+@mock.patch("cephadm.serve.CephadmServe._run_cephadm", _run_cephadm('[]'))
+def test_migrate_admin_client_keyring(cephadm_module: CephadmOrchestrator):
+    assert 'client.admin' not in cephadm_module.keys.keys
+
+    cephadm_module.migration_current = 3
+    cephadm_module.migration.migrate()
+    assert cephadm_module.migration_current == LAST_MIGRATION
+
+    assert cephadm_module.keys.keys['client.admin'].placement.label == '_admin'
