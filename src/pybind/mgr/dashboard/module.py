ceph/ceph

commit d79b9b196c255166483823aad898e9cf175159dd

mgr/dashboard,prometheus: new method of getting mgr IP
- Use a centralized method get_mgr_ip()
- Look up the hostname via DNS.  This is a bit more reliable than
getfqdn() since it will work even when podman adds the container
name to /etc/hosts.

Signed-off-by: Sage Weil <sage@newdream.net>

src/pybind/mgr/dashboard/module.py
@@ -6,7 +6,6 @@ import collections
 import errno
 import logging
 import os
-import socket
 import ssl
 import sys
 import tempfile
@@ -107,6 +106,8 @@ class CherryPyConfig(object):
         else:
             server_port = self.get_localized_module_option('ssl_server_port', 8443)  # type: ignore
 
+        if server_addr == '::':
+            server_addr = self.get_mgr_ip()  # type: ignore
         if server_addr is None:
             raise ServerConfigException(
                 'no server_addr configured; '
@@ -191,7 +192,7 @@ class CherryPyConfig(object):
 
         uri = "{0}://{1}:{2}{3}/".format(
             'https' if use_ssl else 'http',
-            socket.getfqdn(server_addr if server_addr != '::' else ''),
+            server_addr,
             server_port,
             self.url_prefix
         )

src/pybind/mgr/prometheus/module.py
@@ -5,7 +5,6 @@ import json
 import math
 import os
 import re
-import socket
 import threading
 import time
 from mgr_module import CLIReadCommand, MgrModule, MgrStandbyModule, PG_STATES, Option, ServiceInfoT
@@ -1383,10 +1382,9 @@ class Module(MgrModule):
 
         # Publish the URI that others may use to access the service we're
         # about to start serving
-        self.set_uri('http://{0}:{1}/'.format(
-            socket.getfqdn() if server_addr in ['::', '0.0.0.0'] else server_addr,
-            server_port
-        ))
+        if server_addr in ['::', '0.0.0.0']:
+            server_addr = self.get_mgr_ip()
+        self.set_uri('http://{0}:{1}/'.format(server_addr, server_port))
 
         cherrypy.config.update({
             'server.socket_host': server_addr,

src/pybind/mgr/mgr_module.py
@@ -19,6 +19,7 @@ from collections import defaultdict
 from enum import IntEnum
 import rados
 import re
+import socket
 import sys
 import time
 from ceph_argparse import CephArgtype
@@ -791,6 +792,21 @@ class MgrStandbyModule(ceph_module.BaseMgrStandbyModule, MgrModuleLoggingMixin):
     def get_active_uri(self) -> str:
         return self._ceph_get_active_uri()
 
+    def get_mgr_ip(self) -> str:
+        hostname = socket.gethostname()
+        try:
+            r = socket.getaddrinfo(hostname, None, flags=socket.AI_CANONNAME,
+                                   type=socket.SOCK_STREAM)
+            # pick first v4 IP, if present, as long as it is not 127.0.{0,1}.1
+            for a in r:
+                if a[4][0] in ['127.0.1.1', '127.0.0.1']:
+                    continue
+                if a[0] == socket.AF_INET:
+                    return a[4][0]
+        except socket.gaierror as e:
+            pass
+        return hostname
+
     def get_localized_module_option(self, key: str, default: OptionValue = None) -> OptionValue:
         r = self._ceph_get_module_option(key, self.get_mgr_id())
         if r is None:
@@ -1371,6 +1387,21 @@ class MgrModule(ceph_module.BaseMgrModule, MgrModuleLoggingMixin):
 
     def get_ceph_conf_path(self) -> str:
         return self._ceph_get_ceph_conf_path()
+
+    def get_mgr_ip(self) -> str:
+        hostname = socket.gethostname()
+        try:
+            r = socket.getaddrinfo(hostname, None, flags=socket.AI_CANONNAME,
+                                   type=socket.SOCK_STREAM)
+            # pick first v4 IP, if present, as long as it is not 127.0.{0,1}.1
+            for a in r:
+                if a[4][0] in ['127.0.1.1', '127.0.0.1']:
+                    continue
+                if a[0] == socket.AF_INET:
+                    return a[4][0]
+        except socket.gaierror as e:
+            pass
+        return hostname
 
     def get_ceph_option(self, key: str) -> OptionValue:
         return self._ceph_get_option(key)

commit 58bde61b0c34e5304acc5d5cb9f86b2d45cdb0d3

mgr/dashboard,prometheus: fix handling of server_addr
Commit d79b9b1 corrected the URI published by dashboard/prometheus
to use get_mgr_ip() if the address was the wildcard (eg ::).
However, the change also affected the cherrypy server.socket_host
config value, so the modules could no longer bind to a wildcard.

This patch corrects the commit to not affect the cherrypy config.

It also further corrects the dashboard URI fix to handle the 0.0.0.0
wildcard case (prometheus already had this behavior).

Fixes: d79b9b196c255166483823aad898e9cf175159dd
Fixes: http://tracker.ceph.com/issues/52002
Signed-off-by: Scott Shambarger <devel@shambarger.net>

src/pybind/mgr/dashboard/module.py
@@ -110,8 +110,6 @@ class CherryPyConfig(object):
         else:
             server_port = self.get_localized_module_option('ssl_server_port', 8443)  # type: ignore
 
-        if server_addr == '::':
-            server_addr = self.get_mgr_ip()  # type: ignore
         if server_addr is None:
             raise ServerConfigException(
                 'no server_addr configured; '
@@ -194,6 +192,8 @@ class CherryPyConfig(object):
         self._url_prefix = prepare_url_prefix(self.get_module_option(  # type: ignore
             'url_prefix', default=''))
 
+        if server_addr in ['::', '0.0.0.0']:
+            server_addr = self.get_mgr_ip()  # type: ignore
         base_url = build_url(
             scheme='https' if use_ssl else 'http',
             host=server_addr,

src/pybind/mgr/prometheus/module.py
@@ -1394,17 +1394,17 @@ class Module(MgrModule):
         else:
             self.log.info('Cache disabled')
 
+        cherrypy.config.update({
+            'server.socket_host': server_addr,
+            'server.socket_port': server_port,
+            'engine.autoreload.on': False
+        })
         # Publish the URI that others may use to access the service we're
         # about to start serving
         if server_addr in ['::', '0.0.0.0']:
             server_addr = self.get_mgr_ip()
         self.set_uri(build_url(scheme='http', host=server_addr, port=server_port, path='/'))
 
-        cherrypy.config.update({
-            'server.socket_host': server_addr,
-            'server.socket_port': server_port,
-            'engine.autoreload.on': False
-        })
         cherrypy.tree.mount(Root(), "/")
         self.log.info('Starting engine...')
         cherrypy.engine.start()