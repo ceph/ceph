f01add8c3461d51816e8b28fb75db29578174f51
Radoslaw Zarzynski <rzarzyns@redhat.com>
2021-09-14 13:48:58 +0000

    crimson/os: allow to build crimson when WITH_BLUESTORE=OFF.
    This lets to ensure nobody is accidentally linking with the alienized
    version of `common`.
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/crimson/os/futurized_store.cc
+++ src/crimson/os/futurized_store.cc
@@ -1,6 +1,8 @@
 #include "futurized_store.h"
 #include "cyanstore/cyan_store.h"
+#ifdef WITH_BLUESTORE
 #include "alienstore/alien_store.h"
+#endif
 #include "seastore/seastore.h"
 
 namespace crimson::os {
@@ -15,9 +17,14 @@ FuturizedStore::create(const std::string& type,
   } else if (type == "seastore") {
     return crimson::os::seastore::make_seastore(data, values);
   } else {
+#ifdef WITH_BLUESTORE
     // use AlienStore as a fallback. It adapts e.g. BlueStore.
     return std::make_unique<crimson::os::AlienStore>(
       type, data, values);
+#else
+    ceph_abort_msgf("unsupported objectstore type: %s", type.c_str());
+    return {};
+#endif
   }
 }
 
