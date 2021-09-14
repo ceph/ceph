92974b300c3da5fda6be404d76d90c7a55a9e7a5
Radoslaw Zarzynski <rzarzyns@redhat.com>
2021-09-14 15:05:32 +0000

    crimson/os: add the preamble to futurized_store.cc.
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/crimson/os/futurized_store.cc
+++ src/crimson/os/futurized_store.cc
@@ -1,3 +1,6 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
 #include "futurized_store.h"
 #include "cyanstore/cyan_store.h"
 #ifdef WITH_BLUESTORE
