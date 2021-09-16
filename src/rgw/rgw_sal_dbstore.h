eaaaa09312bb0c88d7a8e0be4d2bb86a80b42ce8
Matt Benjamin <mbenjamin@redhat.com>
2021-09-16 11:11:16 -0400

    rgw:sal: implement (placeholder) get_cluster_fsid in DBStore
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_sal_dbstore.h
+++ src/rgw/rgw_sal_dbstore.h
@@ -511,6 +511,7 @@ protected:
       virtual int get_user_by_email(const DoutPrefixProvider *dpp, const std::string& email, optional_yield y, std::unique_ptr<User>* user) override;
       virtual int get_user_by_swift(const DoutPrefixProvider *dpp, const std::string& user_str, optional_yield y, std::unique_ptr<User>* user) override;
       virtual std::unique_ptr<Object> get_object(const rgw_obj_key& k) override;
+      virtual std::string get_cluster_id(const DoutPrefixProvider* dpp, optional_yield y);
       virtual int get_bucket(const DoutPrefixProvider *dpp, User* u, const rgw_bucket& b, std::unique_ptr<Bucket>* bucket, optional_yield y) override;
       virtual int get_bucket(User* u, const RGWBucketInfo& i, std::unique_ptr<Bucket>* bucket) override;
       virtual int get_bucket(const DoutPrefixProvider *dpp, User* u, const std::string& tenant, const std::string&name, std::unique_ptr<Bucket>* bucket, optional_yield y) override;
