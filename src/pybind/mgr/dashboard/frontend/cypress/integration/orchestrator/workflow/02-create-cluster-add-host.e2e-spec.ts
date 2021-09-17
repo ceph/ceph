3276147049511a9a8d45d84c95154a4ec014cd83
Nizamudeen A <nia@redhat.com>
2021-09-17 18:43:01 +0530

    mgr/dashboard: Cluster expansion e2e cleanups
    Fixes: https://tracker.ceph.com/issues/52645
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- src/pybind/mgr/dashboard/frontend/cypress/integration/orchestrator/workflow/02-create-cluster-add-host.e2e-spec.ts
+++ src/pybind/mgr/dashboard/frontend/cypress/integration/orchestrator/workflow/02-create-cluster-add-host.e2e-spec.ts
@@ -1,15 +1,20 @@
-import { CreateClusterWizardHelper } from 'cypress/integration/cluster/create-cluster.po';
+import {
+  CreateClusterHostPageHelper,
+  CreateClusterWizardHelper
+} from 'cypress/integration/cluster/create-cluster.po';
 
 describe('Create cluster add host page', () => {
   const createCluster = new CreateClusterWizardHelper();
+  const createClusterHostPage = new CreateClusterHostPageHelper();
   const hostnames = [
     'ceph-node-00.cephlab.com',
     'ceph-node-01.cephlab.com',
     'ceph-node-02.cephlab.com'
   ];
   const addHost = (hostname: string, exist?: boolean) => {
-    createCluster.add(hostname, exist, false);
-    createCluster.checkExist(hostname, true);
+    cy.get('.btn.btn-accent').first().click({ force: true });
+    createClusterHostPage.add(hostname, exist, false);
+    createClusterHostPage.checkExist(hostname, true);
   };
 
   beforeEach(() => {
@@ -19,35 +24,37 @@ describe('Create cluster add host page', () => {
     createCluster.createCluster();
   });
 
-  it('should check if nav-link and title contains Add Hosts', () => {
+  it('should check if title contains Add Hosts', () => {
     cy.get('.nav-link').should('contain.text', 'Add Hosts');
 
     cy.get('.title').should('contain.text', 'Add Hosts');
   });
 
   it('should check existing host and add new hosts', () => {
-    createCluster.checkExist(hostnames[0], true);
+    createClusterHostPage.checkExist(hostnames[0], true);
 
     addHost(hostnames[1], false);
     addHost(hostnames[2], false);
   });
 
+  it('should delete a host and add it back', () => {
+    createClusterHostPage.delete(hostnames[1]);
+    addHost(hostnames[1], false);
+  });
+
   it('should verify "_no_schedule" label is added', () => {
-    createCluster.checkLabelExists(hostnames[1], ['_no_schedule'], true);
-    createCluster.checkLabelExists(hostnames[2], ['_no_schedule'], true);
+    createClusterHostPage.checkLabelExists(hostnames[1], ['_no_schedule'], true);
+    createClusterHostPage.checkLabelExists(hostnames[2], ['_no_schedule'], true);
   });
 
   it('should not add an existing host', () => {
-    createCluster.add(hostnames[0], true);
+    cy.get('.btn.btn-accent').first().click({ force: true });
+    createClusterHostPage.add(hostnames[0], true);
   });
 
   it('should edit host labels', () => {
     const labels = ['foo', 'bar'];
-    createCluster.editLabels(hostnames[0], labels, true);
-    createCluster.editLabels(hostnames[0], labels, false);
-  });
-
-  it('should delete a host', () => {
-    createCluster.delete(hostnames[1]);
+    createClusterHostPage.editLabels(hostnames[0], labels, true);
+    createClusterHostPage.editLabels(hostnames[0], labels, false);
   });
 });
