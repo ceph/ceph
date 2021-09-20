fdcdeae2a26927b51ab8a480d48a52b896b5532b
Ilya Dryomov <idryomov@gmail.com>
2021-09-20 22:36:10 +0200

    rbd-mirror: fix a couple of brainos in log messages
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- src/tools/rbd_mirror/RemotePoolPoller.cc
+++ src/tools/rbd_mirror/RemotePoolPoller.cc
@@ -16,7 +16,7 @@
 #define dout_context g_ceph_context
 #define dout_subsys ceph_subsys_rbd_mirror
 #undef dout_prefix
-#define dout_prefix *_dout << "rbd::mirror::RemotePollPoller: " << this << " " \
+#define dout_prefix *_dout << "rbd::mirror::RemotePoolPoller: " << this << " " \
                            << __func__ << ": "
 
 namespace rbd {
