28e68f379316752fe34b302da7edea71dc2fff2e
Sage Weil <sage@newdream.net>
2021-10-01 09:44:34 -0500

    os/bluestore: expect smr fields IFF device is smr
    Signed-off-by: Sage Weil <sage@newdream.net>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -11863,6 +11863,9 @@ int BlueStore::_open_super_meta()
       auto p = bl.cbegin();
       decode(zone_size, p);
       dout(1) << __func__ << " zone_size 0x" << std::hex << zone_size << std::dec << dendl;
+      ceph_assert(bdev->is_smr());
+    } else {
+      ceph_assert(!bdev->is_smr());
     }
   }
   {
@@ -11873,6 +11876,9 @@ int BlueStore::_open_super_meta()
       decode(first_sequential_zone, p);
       dout(1) << __func__ << " first_sequential_zone 0x" << std::hex
 	      << first_sequential_zone << std::dec << dendl;
+      ceph_assert(bdev->is_smr());
+    } else {
+      ceph_assert(!bdev->is_smr());
     }
   }
 
