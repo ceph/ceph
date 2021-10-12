b39839b78f8ac0ab2cab0a7931f1a69b679f1da2
Adam King <adking@redhat.com>
2021-10-12 10:08:23 -0400

    cephadm: agent: cache ls output
    ls is really slow, but we can cache the info and only run
    the full thing if we find a new or removed container or if
    one of the daemons' container id changes
    
    Signed-off-by: Adam King <adking@redhat.com>

--- src/pybind/mgr/cephadm/agent.py
+++ src/pybind/mgr/cephadm/agent.py
@@ -261,6 +261,7 @@ class AgentMessageThread(threading.Thread):
                 secure_agent_socket.sendall(msg.encode('utf-8'))
                 agent_response = secure_agent_socket.recv(1024).decode()
                 self.mgr.log.info(f'Received "{agent_response}" from agent on host {self.host}')
+                self.mgr.cache.sending_agent_message[self.host] = False
                 return
             except ConnectionError as e:
                 # if it's a connection error, possibly try to connect again.
