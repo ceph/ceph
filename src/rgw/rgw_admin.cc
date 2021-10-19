f7237ed806cddc708bd3b2c3dba38b0936b376ce
Casey Bodley <cbodley@redhat.com>
2021-10-19 13:09:15 -0400

    radosgw-admin: 'bi purge' doesn't require entrypoint
    we may get -ENOENT looking up cur_bucket here. we look up cur_bucket so
    we can avoid purging the 'current' bucket instance. but if that
    entrypoint doesn't exist, there is no current instance and that
    shouldn't prevent us from purging
    
    Fixes: https://tracker.ceph.com/issues/52976
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -6843,12 +6843,13 @@ next:
 
     std::unique_ptr<rgw::sal::Bucket> cur_bucket;
     ret = init_bucket(user.get(), tenant, bucket_name, string(), &cur_bucket);
-    if (ret < 0) {
+    if (ret == -ENOENT) {
+      // no bucket entrypoint
+    } else if (ret < 0) {
       cerr << "ERROR: could not init current bucket info for bucket_name=" << bucket_name << ": " << cpp_strerror(-ret) << std::endl;
       return -ret;
-    }
-
-    if (cur_bucket->get_bucket_id() == bucket->get_bucket_id() && !yes_i_really_mean_it) {
+    } else if (cur_bucket->get_bucket_id() == bucket->get_bucket_id() &&
+               !yes_i_really_mean_it) {
       cerr << "specified bucket instance points to a current bucket instance" << std::endl;
       cerr << "do you really mean it? (requires --yes-i-really-mean-it)" << std::endl;
       return EINVAL;
