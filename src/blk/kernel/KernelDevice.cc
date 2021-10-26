7e77432cacd79d27419d6ba92a1fe0b93085cc36
Sheng Qiu <herbert1984106@gmail.com>
2021-10-26 12:10:10 -0700

    src/blk/kernel: fix KernelDevice's flush target device fd
    change it to the buffered device fd instead of the directio device fd.
    because it's the buffered device fd that may have dirty cached data in kernel memory
    
    Signed-off-by: Sheng Qiu <herbert1984106@gmail.com>

--- src/blk/kernel/KernelDevice.cc
+++ src/blk/kernel/KernelDevice.cc
@@ -457,7 +457,7 @@ int KernelDevice::flush()
     _exit(1);
   }
   utime_t start = ceph_clock_now();
-  int r = ::fdatasync(fd_directs[WRITE_LIFE_NOT_SET]);
+  int r = ::fdatasync(fd_buffereds[WRITE_LIFE_NOT_SET]);
   utime_t end = ceph_clock_now();
   utime_t dur = end - start;
   if (r < 0) {
