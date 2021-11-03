40cb3f610d842e13d79d10b281aebb72dde3b588
Paul Cuzner <pcuzner@redhat.com>
2021-11-03 15:24:20 +1300

    mgr/prometheus: Update rule format and enhance SNMP support
    Rules now adhere to the format defined by Prometheus.io.
    This changes alert naming and each alert now includes a
    a summary description to provide a quick one-liner.
    
    In addition to reformatting some missing alerts for MDS and
    cephadm have been added, and corresponding tests added.
    
    The MIB has also been refactored, so it now passes standard
    lint tests and a README included for devs to understand the
    OID schema.
    
    Fixes: https://tracker.ceph.com/issues/53111
    
    Signed-off-by: Paul Cuzner <pcuzner@redhat.com>

--- monitoring/prometheus/tests/validate_rules.py
+++ monitoring/prometheus/tests/validate_rules.py
@@ -8,15 +8,20 @@
 #  8 .. Missing fields in YAML
 # 12 .. Invalid YAML - unable to load
 # 16 .. Missing input files
-
-# TODO: logging for debug
+#
+# Externals
+# snmptranslate .. used to determine the oid's in the MIB to verify the rule -> MIB is correct
+#
 
 import re
 import os
 import sys
 import yaml
+import shutil
+import string
 from bs4 import BeautifulSoup
 from typing import List, Any, Dict, Set, Optional, Tuple
+import subprocess
 
 import urllib.request
 import urllib.error
@@ -25,6 +30,15 @@ from urllib.parse import urlparse
 DOCLINK_NAME = 'documentation'
 DEFAULT_RULES_FILENAME = '../alerts/ceph_default_alerts.yml'
 DEFAULT_TEST_FILENAME = 'test_alerts.yml'
+MIB_FILE = '../../snmp/CEPH-MIB.txt'
+
+
+def isascii(s: str) -> bool:
+    try:
+        s.encode('ascii')
+    except UnicodeEncodeError:
+        return False
+    return True
 
 
 def read_file(file_name: str) -> Tuple[str, str]:
@@ -50,6 +64,14 @@ def load_yaml(file_name: str) -> Tuple[Dict[str, Any], str]:
             errs = f"filename '{file_name} is not a valid YAML file"
 
     return data, errs
+
+
+def run_command(command: str):
+    c = command.split()
+    completion = subprocess.run(c, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return (completion.returncode,
+            completion.stdout.decode('utf-8').split('\n'),
+            completion.stderr.decode('utf-8').split('\n'))
 
 
 class HTMLCache:
@@ -93,7 +115,6 @@ class PrometheusRule:
     expected_attrs = [
         'alert',
         'expr',
-        'for',
         'labels',
         'annotations'
     ]
@@ -106,11 +127,30 @@ class PrometheusRule:
         self.rule = rule_data
         self.errors: List[str] = []
         self.warnings: List[str] = []
-
         self.validate()
 
+    @property
+    def has_oid(self):
+        return True if self.rule.get('labels', {}).get('oid', '') else False
+
+    @property
+    def labels(self) -> Dict[str, str]:
+        return self.rule.get('labels', {})
+
+    @property
+    def annotations(self) -> Dict[str, str]:
+        return self.rule.get('annotations', {})
+
     def _check_alert_name(self):
-        pass
+        # this is simplistic, but works in the context of the alert name
+        if self.name[0] in string.ascii_uppercase and \
+          self.name != self.name.lower() and \
+          self.name != self.name.upper() and \
+          " " not in self.name and \
+          "_" not in self.name:
+            return
+
+        self.warnings.append("Alert name is not in CamelCase format")
 
     def _check_structure(self):
         rule_attrs = self.rule.keys()
@@ -123,17 +163,16 @@ class PrometheusRule:
 
     def _check_labels(self):
         for rqd in ['severity', 'type']:
-            if rqd not in self.rule.get('labels', ''):
+            if rqd not in self.labels.keys():
                 self.errors.append(f"rule is missing {rqd} label definition")
 
     def _check_annotations(self):
-        for rqd in ['description']:
-                if rqd not in self.rule.get('annotations', ''):
+        for rqd in ['summary', 'description']:
+                if rqd not in self.annotations:
                     self.errors.append(f"rule is missing {rqd} annotation definition")
 
     def _check_doclink(self):
-        annotations = self.rule.get('annotations', {})
-        doclink = annotations.get(DOCLINK_NAME, '')
+        doclink = self.annotations.get(DOCLINK_NAME, '')
 
         if doclink:
             url = urlparse(doclink)
@@ -148,20 +187,36 @@ class PrometheusRule:
                 self.errors.append(f"documentation link error: {status} {content}")
 
     def _check_snmp(self):
-        labels = self.rule.get('labels', {})
-        oid = labels.get('oid', '')
-        if labels.get('severity', '') == 'critical' and not oid:
+        oid = self.labels.get('oid', '')
+
+        if self.labels.get('severity', '') == 'critical' and not oid:
             self.warnings.append("critical level alert is missing an SNMP oid entry")
-        if oid and not re.search('^1.3.6.1.4.1.50495.15.1.2.\\d+.\\d+$', oid):
-            self.errors.append("invalid OID provided")
+        if oid and not re.search('^1.3.6.1.4.1.50495.1.2.\\d+.\\d+.\\d+$', oid):
+            self.errors.append("invalid OID format provided")
+        if self.group.get_oids():
+            if oid and oid not in self.group.get_oids():
+                self.errors.append(f"rule defines an OID {oid} that is missing from the MIB file({os.path.basename(MIB_FILE)})")
+
+    def _check_ascii(self):
+        if 'oid' not in self.labels:
+            return
+
+        desc = self.annotations.get('description', '')
+        summary = self.annotations.get('summary', '')
+        if not isascii(desc):
+            self.errors.append(f"non-ascii characters found in 'description' field will cause issues in associated snmp trap.")
+        if not isascii(summary):
+            self.errors.append(f"non-ascii characters found in 'summary' field will cause issues in associated snmp trap.")
 
     def validate(self):
+
         self._check_alert_name()
         self._check_structure()
         self._check_labels()
         self._check_annotations()
         self._check_doclink()
         self._check_snmp()
+        self._check_ascii()
         char = '.'
 
         if self.errors:
@@ -200,6 +255,9 @@ class RuleGroup:
     def fetch_html_page(self, url):
         return self.rule_file.fetch_html_page(url)
 
+    def get_oids(self):
+        return self.rule_file.oid_list
+
     @property
     def error_count(self):
         return len(self.problems['error'])
@@ -214,10 +272,11 @@ class RuleGroup:
 
 class RuleFile:
 
-    def __init__(self, parent, file_name, rules):
+    def __init__(self, parent, file_name, rules, oid_list):
         self.parent = parent
         self.file_name = file_name
         self.rules: Dict[str, Any] = rules
+        self.oid_list = oid_list
         self.problems: Set[str] = set()
         self.group: Dict[str, RuleGroup] = {}
         self.alert_names_seen: Set[str] = set()
@@ -242,9 +301,18 @@ class RuleFile:
     @property
     def rule_count(self):
         rule_count = 0
-        for group_name, rule_group in self.group.items():
+        for _group_name, rule_group in self.group.items():
             rule_count += rule_group.count
         return rule_count
+
+    @property
+    def oid_count(self):
+        oid_count = 0
+        for _group_name, rule_group in self.group.items():
+            for _rule_name, rule in rule_group.rules.items():
+                if rule.has_oid:
+                    oid_count += 1
+        return oid_count
 
     @property
     def group_names(self):
@@ -278,23 +346,23 @@ class RuleFile:
                     # skipped recording rule
                     pass
 
-    def error_report(self):
-        def max_width(item_list: List[str]) -> int:
-            return max([len(i) for i in item_list])
+    def report(self):
+        def max_width(item_list: Set[str], min_width: int = 0) -> int:
+            return max([len(i) for i in item_list] + [min_width])
 
         if not self.problems and not self.duplicate_alert_names:
-            print("\nNo problems detected in rule file")
+            print("\nNo problems detected in the rule file")
             return
 
         print("\nProblem Report\n")
 
-        group_width = max_width(self.problems)
+        group_width = max_width(self.problems, 5)
         alert_names = set()
         for g in self.problems:
             group = self.group[g]
             alert_names.update(group.problems.get('error', []))
             alert_names.update(group.problems.get('warning', []))
-        alert_width = max_width(alert_names)
+        alert_width = max_width(alert_names, 10)
 
         template = "  {group:<{group_width}}  {severity:<8}  {alert_name:<{alert_width}}  {description}"
 
@@ -382,7 +450,7 @@ class UnitTests:
     def process(self, defined_alert_names: List[str]):
         self._check_alert_names(defined_alert_names)
 
-    def error_report(self) -> None:
+    def report(self) -> None:
 
         if not self.problems:
             print("\nNo problems detected in unit tests file")
@@ -404,6 +472,26 @@ class RuleChecker:
         self.warnings = {}
         self.error_count = 0
         self.warning_count = 0
+        self.oid_count = 0
+
+        self.oid_list = self.build_oid_list()
+
+    def build_oid_list(self) -> List[str]:
+
+        cmd = shutil.which('snmptranslate')
+        if not cmd:
+            return []
+
+        rc, stdout, stderr = run_command(f"{cmd} -Pu -Tz -M ../../snmp:/usr/share/snmp/mibs -m CEPH-MIB")
+        if rc != 0:
+            return []
+
+        oid_list: List[str] = []
+        for line in stdout[:-1]:
+            _label, oid = line.replace('"', '').replace('\t', ' ').split()
+            oid_list.append(oid)
+
+        return oid_list
 
     @property
     def status(self):
@@ -448,36 +536,34 @@ class RuleChecker:
             print(errs)
             sys.exit(12)
 
-        self.rule_file = RuleFile(self, self.rules_filename, rules)
+        self.rule_file = RuleFile(self, self.rules_filename, rules, self.oid_list)
         self.summarise_rule_file()
 
         self.unit_tests = UnitTests(self.test_filename)
         self.unit_tests.process(self.rule_file.alert_names_seen)
 
-    def error_report(self):
+    def report(self):
         print("\n\nSummary\n")
         print(f"Rule file             : {self.rules_filename}")
         print(f"Unit Test file        : {self.test_filename}")
         print(f"\nRule groups processed : {self.rule_file.group_count:>3}")
         print(f"Rules processed       : {self.rule_file.rule_count:>3}")
+        print(f"SNMP OIDs declared    : {self.rule_file.oid_count:>3} {'(snmptranslate missing, unable to cross check)' if not self.oid_list else ''}")
         print(f"Rule errors           : {self.error_count:>3}")
         print(f"Rule warnings         : {self.warning_count:>3}")
         print(f"Rule name duplicates  : {len(self.rule_file.duplicate_alert_names):>3}")
         print(f"Unit tests missing    : {len(self.unit_tests.problems):>3}")
 
-        if self.rule_file_problems:
-            self.rule_file.error_report()
-        if self.unit_tests.problems:
-            self.unit_tests.error_report()
+        self.rule_file.report()
+        self.unit_tests.report()
 
 
 def main():
     checker = RuleChecker()
 
     checker.run()
-    if checker.status > 0:
-        checker.error_report()
-        print()
+    checker.report()
+    print()
 
     sys.exit(checker.status)
 
