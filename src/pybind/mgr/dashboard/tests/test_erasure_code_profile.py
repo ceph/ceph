9926dd24a2a8217cdcdf2eacb6c00baa8448ab47
Alfonso Martínez <almartin@redhat.com>
2021-11-03 10:17:21 +0100

    mgr/dashboard: python unit tests refactoring
    * Controller tests: cherrypy config: authentication disabled by default; ability to pass custom config (e.g. enable authentication).
    * Auth controller: add tests; test that unauthorized request fails when authentication is enabled.
    * DocsTest: clear ENDPOINT_MAP so the test_gen_tags test becomes deterministic.
    * pylint: disable=no-name-in-module: fix imports in tests.
    
    Fixes: https://tracker.ceph.com/issues/53083
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>

--- src/pybind/mgr/dashboard/tests/test_erasure_code_profile.py
+++ src/pybind/mgr/dashboard/tests/test_erasure_code_profile.py
@@ -2,7 +2,7 @@
 
 from .. import mgr
 from ..controllers.erasure_code_profile import ErasureCodeProfile
-from . import ControllerTestCase  # pylint: disable=no-name-in-module
+from ..tests import ControllerTestCase
 
 
 class ErasureCodeProfileTest(ControllerTestCase):
@@ -21,8 +21,6 @@ class ErasureCodeProfileTest(ControllerTestCase):
             'fs_map': {'filesystems': []},
 
         }[key]
-        # pylint: disable=protected-access
-        ErasureCodeProfile._cp_config['tools.authenticate.on'] = False
         cls.setup_controllers([ErasureCodeProfile])
 
     def test_list(self):
