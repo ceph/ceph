ce49448ee2475111b2867046e172865ce5331713
Aashish Sharma <aashishsharma@localhost.localdomain>
2021-11-08 13:01:02 +0530

    mgr/dashboard: Cluster Expansion - Review Section: fixes and improvements
    Ensure "Storage capacity" keeps the "Description : Value" approach ("Number of devices: X" and "Raw Capacity: Y" in different lines).Correct issue with "host by services" host count
    
    Fixes: https://tracker.ceph.com/issues/53128
    Signed-off-by: Aashish Sharma <aasharma@redhat.com>
    (cherry picked from commit 2ba1a5a2666674a935c9288f5b547532ba214f54)

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/create-cluster/create-cluster-review.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/create-cluster/create-cluster-review.component.spec.ts
@@ -3,11 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import _ from 'lodash';
 import { ToastrModule } from 'ngx-toastr';
-import { of } from 'rxjs';
 
 import { CephModule } from '~/app/ceph/ceph.module';
 import { CoreModule } from '~/app/core/core.module';
-import { CephServiceService } from '~/app/shared/api/ceph-service.service';
 import { SharedModule } from '~/app/shared/shared.module';
 import { configureTestBed } from '~/testing/unit-test-helper';
 import { CreateClusterReviewComponent } from './create-cluster-review.component';
@@ -15,8 +13,6 @@ import { CreateClusterReviewComponent } from './create-cluster-review.component'
 describe('CreateClusterReviewComponent', () => {
   let component: CreateClusterReviewComponent;
   let fixture: ComponentFixture<CreateClusterReviewComponent>;
-  let cephServiceService: CephServiceService;
-  let serviceListSpy: jasmine.Spy;
 
   configureTestBed({
     imports: [HttpClientTestingModule, SharedModule, ToastrModule.forRoot(), CephModule, CoreModule]
@@ -25,31 +21,9 @@ describe('CreateClusterReviewComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CreateClusterReviewComponent);
     component = fixture.componentInstance;
-    cephServiceService = TestBed.inject(CephServiceService);
-    serviceListSpy = spyOn(cephServiceService, 'list');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
-  });
-
-  it('should verify host metadata calculations', () => {
-    const hostnames = ['ceph.test1', 'ceph.test2'];
-    const payload = [
-      {
-        hostname: hostnames[0],
-        service_type: ['mgr', 'mon']
-      },
-      {
-        hostname: hostnames[1],
-        service_type: ['mgr', 'alertmanager']
-      }
-    ];
-    serviceListSpy.and.callFake(() => of(payload));
-    fixture.detectChanges();
-    expect(serviceListSpy).toHaveBeenCalled();
-
-    expect(component.serviceCount).toBe(2);
-    expect(component.uniqueServices.size).toBe(2);
   });
 });
