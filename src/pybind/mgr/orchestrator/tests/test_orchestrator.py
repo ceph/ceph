1a30da0b23149dfbdd9f958028bbe10cb43a30e0
Sebastian Wagner <sewagner@redhat.com>
2021-11-10 15:54:42 +0100

    python-common: Don't valiate ServiceSpec.from_json() in `orch ls`
    unfortunately `ceph orch ls` may return invalid OSD specs for
    OSDs not associated to and specs.
    
    Signed-off-by: Sebastian Wagner <sewagner@redhat.com>
    (cherry picked from commit 3f38583b7189d99be360d8475fe6ef8cd53dee7c)
    
    Conflicts:
    	src/pybind/mgr/orchestrator/module.py

--- src/pybind/mgr/orchestrator/tests/test_orchestrator.py
+++ src/pybind/mgr/orchestrator/tests/test_orchestrator.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import json
+import textwrap
 
 import pytest
 import yaml
@@ -155,6 +156,24 @@ def test_orch_ls(_describe_service):
     r = m._handle_command(None, cmd)
     out = 'NAME  PORTS  RUNNING  REFRESHED  AGE  PLACEMENT  \n' \
           'osd              123  -          -               '
+    assert r == HandleCommandResult(retval=0, stdout=out, stderr='')
+
+    cmd = {
+        'prefix': 'orch ls',
+        'format': 'yaml',
+    }
+    m = OrchestratorCli('orchestrator', 0, 0)
+    r = m._handle_command(None, cmd)
+    out = textwrap.dedent("""
+        service_type: osd
+        service_name: osd
+        spec:
+          filter_logic: AND
+          objectstore: bluestore
+        status:
+          running: 123
+          size: 0
+        """).lstrip()
     assert r == HandleCommandResult(retval=0, stdout=out, stderr='')
 
 
