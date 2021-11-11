3a7bc71d68d67e56e9d50100090d4334a9ffe098
Casey Bodley <cbodley@redhat.com>
2021-11-11 12:01:06 -0500

    rgw/beast: reference count Connections for timeout_handler
    resolves a use-after-free in the timeout_handler, where a timeout fires
    and schedules the timeout_handler for execution, but the coroutine exits
    and destroys the socket before asio executes the timeout_handler
    
    timeout_handler now holds a reference on the Connection to extend its
    lifetime
    
    now that the Connection is allocated on the heap, we can include the
    parse_buffer in this memory instead of allocating it separately
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_asio_frontend_timer.h
+++ src/rgw/rgw_asio_frontend_timer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <boost/asio/basic_waitable_timer.hpp>
+#include <boost/intrusive_ptr.hpp>
 
 #include "common/ceph_time.h"
 
@@ -9,9 +10,12 @@ namespace rgw {
 // a WaitHandler that closes a stream if the timeout expires
 template <typename Stream>
 struct timeout_handler {
-  Stream* stream;
+  // this handler may outlive the timer/stream, so we need to hold a reference
+  // to keep the stream alive
+  boost::intrusive_ptr<Stream> stream;
 
-  explicit timeout_handler(Stream* stream) noexcept : stream(stream) {}
+  explicit timeout_handler(boost::intrusive_ptr<Stream> stream) noexcept
+      : stream(std::move(stream)) {}
 
   void operator()(boost::system::error_code ec) {
     if (!ec) { // wait was not canceled
@@ -22,22 +26,24 @@ struct timeout_handler {
 };
 
 // a timeout timer for stream operations
-template <typename Clock, typename Executor>
+template <typename Clock, typename Executor, typename Stream>
 class basic_timeout_timer {
  public:
   using clock_type = Clock;
   using duration = typename clock_type::duration;
   using executor_type = Executor;
 
-  explicit basic_timeout_timer(const executor_type& ex) : timer(ex) {}
+  explicit basic_timeout_timer(const executor_type& ex, duration dur,
+                               boost::intrusive_ptr<Stream> stream)
+      : timer(ex), dur(dur), stream(std::move(stream))
+  {}
 
   basic_timeout_timer(const basic_timeout_timer&) = delete;
   basic_timeout_timer& operator=(const basic_timeout_timer&) = delete;
 
-  template <typename Stream>
-  void expires_after(Stream& stream, duration dur) {
+  void start() {
     timer.expires_after(dur);
-    timer.async_wait(timeout_handler{&stream});
+    timer.async_wait(timeout_handler{stream});
   }
 
   void cancel() {
@@ -48,6 +54,8 @@ class basic_timeout_timer {
   using Timer = boost::asio::basic_waitable_timer<clock_type,
         boost::asio::wait_traits<clock_type>, executor_type>;
   Timer timer;
+  duration dur;
+  boost::intrusive_ptr<Stream> stream;
 };
 
 } // namespace rgw
