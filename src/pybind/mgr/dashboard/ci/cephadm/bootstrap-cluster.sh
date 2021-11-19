5fa8116171e0002a02783cc8fea520eb3040f081
Alfonso Martínez <almartin@redhat.com>
2021-11-19 08:57:06 +0100

    mgr/dashboard: cephadm e2e start script: deploy monitoring stack when --expanded
    Fixes: https://tracker.ceph.com/issues/53333
    Signed-off-by: Alfonso Martínez <almartin@redhat.com>

--- src/pybind/mgr/dashboard/ci/cephadm/bootstrap-cluster.sh
+++ src/pybind/mgr/dashboard/ci/cephadm/bootstrap-cluster.sh
@@ -8,7 +8,13 @@ chmod +x /root/bin/cephadm
 mkdir -p /etc/ceph
 mon_ip=$(ifconfig eth0  | grep 'inet ' | awk '{ print $2}')
 
-cephadm bootstrap --mon-ip $mon_ip --initial-dashboard-password {{ admin_password }} --allow-fqdn-hostname --skip-monitoring-stack --dashboard-password-noupdate --shared_ceph_folder /mnt/{{ ceph_dev_folder }}
+bootstrap_extra_options='--allow-fqdn-hostname --dashboard-password-noupdate'
+bootstrap_extra_options_not_expanded='--skip-monitoring-stack'
+{% if expanded_cluster is not defined %}
+  bootstrap_extra_options+=" ${bootstrap_extra_options_not_expanded}"
+{% endif %}
+
+cephadm bootstrap --mon-ip $mon_ip --initial-dashboard-password {{ admin_password }} --shared_ceph_folder /mnt/{{ ceph_dev_folder }} ${bootstrap_extra_options}
 
 fsid=$(cat /etc/ceph/ceph.conf | grep fsid | awk '{ print $3}')
 cephadm_shell="cephadm shell --fsid ${fsid} -c /etc/ceph/ceph.conf -k /etc/ceph/ceph.client.admin.keyring"
