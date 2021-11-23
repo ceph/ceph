4128106de3c8270419f371f147e9a8719fdfc81c
Deepika Upadhyay <dupadhya@redhat.com>
2021-11-23 22:40:56 +0530

    debian: remove libjaeger packaging
    we no longer need to package libjaeger, as we would be using
    libopentelemetry static libraries(until we support shared).
    
    Signed-off-by: Deepika Upadhyay <dupadhya@redhat.com>

--- cmake/modules/BuildOpentelemetry.cmake
+++ cmake/modules/BuildOpentelemetry.cmake
@@ -13,7 +13,7 @@ function(build_opentelemetry)
                                -DWITH_JAEGER=ON
                                -DBUILD_TESTING=OFF
                                -DWITH_EXAMPLES=OFF
-                               -DBoost_INCLUDE_DIRS=${CMAKE_BINARY_DIR}/boost/include)
+                               -DBoost_INCLUDE_DIR=${CMAKE_BINARY_DIR}/boost/include)
 
   set(opentelemetry_libs
       ${opentelemetry_BINARY_DIR}/sdk/src/trace/libopentelemetry_trace.a
