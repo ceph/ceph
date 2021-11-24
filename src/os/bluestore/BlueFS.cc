ac4c119e562ef0c5bae565a07eadfab0394e5c06
Adam Kupczyk <akupczyk@redhat.com>
2021-11-24 18:55:05 +0100

    os/bluestore/bluefs: Sync BlueFS log with its allocation delta
    BlueFS log is the only file that we can append to.
    When we append to file we must take into consideration previously commited allocations,
    otherwise update will be miscalculated.
    
    Signed-off-by: Adam Kupczyk <akupczyk@redhat.com>

--- src/os/bluestore/BlueFS.cc
+++ src/os/bluestore/BlueFS.cc
@@ -927,6 +927,7 @@ int BlueFS::mount()
   log_writer = _create_writer(_get_file(1));
   ceph_assert(log_writer->file->fnode.ino == 1);
   log_writer->pos = log_writer->file->fnode.size;
+  log_writer->file->fnode.reset_delta();
   dout(10) << __func__ << " log write pos set to 0x"
            << std::hex << log_writer->pos << std::dec
            << dendl;
