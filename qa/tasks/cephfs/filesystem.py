3b44f20ac0a889fdfc763d7ff1c824020aa784a4
Xiubo Li <xiubli@redhat.com>
2021-11-25 16:29:21 +0800

    qa: move the optional client_config parameter to the end
    Fixes: https://tracker.ceph.com/issues/53216
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- qa/tasks/cephfs/filesystem.py
+++ qa/tasks/cephfs/filesystem.py
@@ -720,7 +720,7 @@ class Filesystem(MDSCluster):
         # avoid circular dep by importing here:
         from tasks.cephfs.fuse_mount import FuseMount
         d = misc.get_testdir(self._ctx)
-        m = FuseMount(self._ctx, {}, d, "admin", self.client_remote, cephfs_name=self.name)
+        m = FuseMount(self._ctx, d, "admin", self.client_remote, cephfs_name=self.name)
         m.mount_wait()
         m.run_shell_payload(cmd)
         m.umount_wait(require_clean=True)
