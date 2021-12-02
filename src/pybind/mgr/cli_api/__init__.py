e699de49f2b9b8136f21eac92bcb8d06ba642563
Pere Diaz Bou <pdiazbou@redhat.com>
2021-12-02 09:32:56 +0100

    mgr/cli: fix __init__ CLI import
    Signed-off-by: Pere Diaz Bou <pdiazbou@redhat.com>

--- src/pybind/mgr/cli_api/__init__.py
+++ src/pybind/mgr/cli_api/__init__.py
@@ -1,6 +1,10 @@
-import os
+from .module import CLI
 
+__all__ = [
+    "CLI",
+]
+
+import os
 if 'UNITTEST' in os.environ:
     import tests  # noqa # pylint: disable=unused-import
-else:
-    from .module import CLI  # noqa # pylint: disable=unused-import
+    __all__.append(tests.__name__)
