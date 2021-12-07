123f508a8069b6c9f5d3871d77385b4d2edc8114
Pritha Srivastava <prsrivas@redhat.com>
2021-12-07 16:31:25 +0530

    rgw: deleting objects inline in case bypass_gc is specified for bucket remove command.
    fixes: https://tracker.ceph.com/issues/53512
    
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -482,6 +482,10 @@ public:
     return lc;
   }
 
+  RGWGC *get_gc() {
+    return gc;
+  }
+
   RGWRados& set_run_gc_thread(bool _use_gc_thread) {
     use_gc_thread = _use_gc_thread;
     return *this;
