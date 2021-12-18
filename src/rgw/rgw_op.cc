508f60539de43c800e3629fc8aa2af8cf58d6e38
Marcus Watts <mwatts@redhat.com>
2021-12-17 23:16:09 -0500

    rgw/sse-s3: +get_encryption_defaults, use new crypt_attribute_map
    putobj and postobj: get_encryption_defaults
    	this fetches bucketencryption policy and resolves defaults.
    	also errors for various conflicts between parameters (& policy).
    verify_permisions
    	fetch encryption attributes from crypt_attribute_map not x_meta_map
    	for postobj, x_meta_map only gets meta attributes, not sse.
    	if bucketencryption policy exists, it *may* be correct to
    	prepopulate this before bucket policy sees it.
    
    map_qs_metadata
    	for putobj it now also copies sse attributes into crypt_attribute_map.
    
    Signed-off-by: Marcus Watts <mwatts@redhat.com>
    (cherry picked from commit 464fda8bcd2e3b605d94d68a9e530148846e9297)

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -3637,15 +3637,15 @@ int RGWPutObj::verify_permission(optional_yield y)
 
     constexpr auto encrypt_attr = "x-amz-server-side-encryption";
     constexpr auto s3_encrypt_attr = "s3:x-amz-server-side-encryption";
-    auto enc_header = s->info.x_meta_map.find(encrypt_attr);
-    if (enc_header != s->info.x_meta_map.end()){
+    auto enc_header = s->info.crypt_attribute_map.find(encrypt_attr);
+    if (enc_header != s->info.crypt_attribute_map.end()){
       rgw_add_to_iam_environment(s->env, s3_encrypt_attr, enc_header->second);
     }
 
     constexpr auto kms_attr = "x-amz-server-side-encryption-aws-kms-key-id";
     constexpr auto s3_kms_attr = "s3:x-amz-server-side-encryption-aws-kms-key-id";
-    auto kms_header = s->info.x_meta_map.find(kms_attr);
-    if (kms_header != s->info.x_meta_map.end()){
+    auto kms_header = s->info.crypt_attribute_map.find(kms_attr);
+    if (kms_header != s->info.crypt_attribute_map.end()){
       rgw_add_to_iam_environment(s->env, s3_kms_attr, kms_header->second);
     }
 
