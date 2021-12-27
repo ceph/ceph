7f6e108223c0b97d4006ef017bb04fb489912c86
Yin Congmin <congmin.yin@intel.com>
2021-12-27 15:06:49 +0800

    rbd: add persistent-cache flush command
    Add a flush command so that users can manually flush cache.
    
    [ idryomov: error messages, incorporate doc and help.t hunks, drop
      do_persistent_cache_flush() ]
    
    Signed-off-by: Yin Congmin <congmin.yin@intel.com>
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    (cherry picked from commit 644fbc9fcc8f12eb93d5cc20054cd8598ab001b7)

--- src/tools/rbd/action/PersistentCache.cc
+++ src/tools/rbd/action/PersistentCache.cc
@@ -59,10 +59,63 @@ int execute_invalidate(const po::variables_map &vm,
   return 0;
 }
 
+void get_arguments_flush(po::options_description *positional,
+                         po::options_description *options) {
+  at::add_image_spec_options(positional, options, at::ARGUMENT_MODIFIER_NONE);
+  at::add_image_id_option(options);
+}
+
+int execute_flush(const po::variables_map &vm,
+                  const std::vector<std::string> &ceph_global_init_args) {
+  size_t arg_index = 0;
+  std::string pool_name;
+  std::string namespace_name;
+  std::string image_name;
+  std::string snap_name;
+  int r = utils::get_pool_image_snapshot_names(
+    vm, at::ARGUMENT_MODIFIER_NONE, &arg_index, &pool_name, &namespace_name,
+    &image_name, &snap_name, true, utils::SNAPSHOT_PRESENCE_NONE,
+    utils::SPEC_VALIDATION_NONE);
+  if (r < 0) {
+    return r;
+  }
+
+  librados::Rados rados;
+  librados::IoCtx io_ctx;
+  librbd::Image image;
+  r = utils::init_and_open_image(pool_name, namespace_name, image_name, "", "",
+                                 false, &rados, &io_ctx, &image);
+  if (r < 0) {
+    return r;
+  }
+
+  uint64_t features;
+  r = image.features(&features);
+  if (r < 0) {
+    return r;
+  }
+
+  if (features & RBD_FEATURE_DIRTY_CACHE) {
+    r = image.flush();
+    if (r < 0) {
+      std::cerr << "rbd: flushing persistent cache failed: "
+                << cpp_strerror(r) << std::endl;
+      return r;
+    }
+  } else {
+    std::cout << "rbd: persistent cache is clean or disabled" << std::endl;
+  }
+
+  return 0;
+}
+
 Shell::Action action_invalidate(
   {"persistent-cache", "invalidate"}, {},
   "Invalidate (discard) existing / dirty persistent cache.", "",
   &get_arguments_invalidate, &execute_invalidate);
+Shell::Action action_flush(
+  {"persistent-cache", "flush"}, {}, "Flush persistent cache.", "",
+  &get_arguments_flush, &execute_flush);
 
 } // namespace persistent_cache
 } // namespace action
