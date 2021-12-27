b55c32566c3163907164edcdddbf1197be994613
Yuval Lifshitz <ylifshit@redhat.com>
2021-12-27 17:19:53 +0200

    rgw/multisite/test: allow passing rgw parameter to multisite script
    this allow changing rgw conf when sratting the test scrip. e.g.
    MON=1 OSD=1 MDS=0 MGR=1 ../src/test/rgw/test-rgw-multisite.sh 2
    --rgw_max_objs_per_shard=50
    
    Signed-off-by: Yuval Lifshitz <ylifshit@redhat.com>

--- src/test/rgw/test-rgw-common.sh
+++ src/test/rgw/test-rgw-common.sh
@@ -76,9 +76,13 @@ function rgw_admin {
 }
 
 function rgw {
-  [ $# -ne 2 ] && echo "rgw() needs 2 params" && exit 1
+  [ $# -lt 2 ] && echo "rgw() needs at least 2 params" && exit 1
 
-  echo "$mrgw $1 $2 $rgw_flags"
+  name=$1
+  port=$2
+  shift 2
+
+  echo "$mrgw $name $port $rgw_flags $@"
 }
 
 function init_first_zone {
