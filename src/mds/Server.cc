9269c181dd3da3090f29d5c6f206217d7ae132df
Xiubo Li <xiubli@redhat.com>
2022-01-04 11:18:53 +0800

    mds: directly return just after responding the link request
    Fixes: https://tracker.ceph.com/issues/53750
    Signed-off-by: Xiubo Li <xiubli@redhat.com>
    (cherry picked from commit 14f9840dbf5ad288ae71af3311e3283f8f3964a6)

--- src/mds/Server.cc
+++ src/mds/Server.cc
@@ -6642,6 +6642,7 @@ void Server::handle_client_link(MDRequestRef& mdr)
   if (targeti->get_projected_inode()->nlink == 0) {
     dout(7) << "target has no link, failing..." << dendl;
     respond_to_request(mdr, -CEPHFS_ENOENT);
+    return;
   }
 
   if ((!mdr->has_more() || mdr->more()->witnessed.empty())) {
