041703547ccd769e3fead396527a33fcbdb761a2
J. Eric Ivancich <ivancich@redhat.com>
2022-01-07 12:32:48 -0500

    rgw: resharding causes bucket attributes to be lost
    With the new resharding code, some bucket metadata that is stored as
    xattrs (e.g., ACLs, life-cycle policies) were not sent with the
    updated bucket instance data when resharding completed. As a result,
    resharding has a regression where that metadata is lost after a
    successful reshard.
    
    This commit restores the variable in the RGWBucketReshard class that
    maintains the bucket attributes, so they can be saved when the bucket
    instance object is updated.
    
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -7695,7 +7695,9 @@ next:
       return -EINVAL;
     }
 
-    RGWBucketReshard br(static_cast<rgw::sal::RadosStore*>(store), bucket->get_info(), nullptr /* no callback */);
+    RGWBucketReshard br(static_cast<rgw::sal::RadosStore*>(store),
+			bucket->get_info(), bucket->get_attrs(),
+			nullptr /* no callback */);
 
 #define DEFAULT_RESHARD_MAX_ENTRIES 1000
     if (max_entries < 1) {
@@ -7796,7 +7798,9 @@ next:
       return -ret;
     }
 
-    RGWBucketReshard br(static_cast<rgw::sal::RadosStore*>(store), bucket->get_info(), nullptr /* no callback */);
+    RGWBucketReshard br(static_cast<rgw::sal::RadosStore*>(store),
+			bucket->get_info(), bucket->get_attrs(),
+			nullptr /* no callback */);
     list<cls_rgw_bucket_instance_entry> status;
     int r = br.get_status(dpp(), &status);
     if (r < 0) {
@@ -7841,7 +7845,9 @@ next:
 
     if (bucket_initable) {
       // we did not encounter an error, so let's work with the bucket
-      RGWBucketReshard br(static_cast<rgw::sal::RadosStore*>(store), bucket->get_info(), nullptr /* no callback */);
+	RGWBucketReshard br(static_cast<rgw::sal::RadosStore*>(store),
+			    bucket->get_info(), bucket->get_attrs(),
+			    nullptr /* no callback */);
       int ret = br.cancel(dpp());
       if (ret < 0) {
         if (ret == -EBUSY) {
