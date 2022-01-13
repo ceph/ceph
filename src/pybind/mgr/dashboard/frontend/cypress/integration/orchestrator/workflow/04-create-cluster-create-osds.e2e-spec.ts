bd026a95428f5b033d4461df045061b19a6bec95
Nizamudeen A <nia@redhat.com>
2022-01-13 18:28:56 +0530

    mgr/dashboard: Refactoring dashboard cephadm checks
    I isolated all the tests suites into there respective files
    so that in future it is easier to add more tests to it.
    
    I also given priority to the host actions.
    
    Create OSD checks are now written in a way that OSDs
    are created only on the intended hosts. This will make
    the host draining process easier and less time consuming.
    
    Also tried to address the flaky force maintenance checks.
    
    Removed some duplicated codes
    
    Service creation part improved to reduce the time taken
    for its completion
    
    Fixes: https://tracker.ceph.com/issues/53905
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- src/pybind/mgr/dashboard/frontend/cypress/integration/orchestrator/workflow/04-create-cluster-create-osds.e2e-spec.ts
+++ src/pybind/mgr/dashboard/frontend/cypress/integration/orchestrator/workflow/04-create-cluster-create-osds.e2e-spec.ts
@@ -20,21 +20,24 @@ describe('Create cluster create osds page', () => {
 
   describe('when Orchestrator is available', () => {
     it('should create OSDs', () => {
-      osds.navigateTo();
-      osds.getTableCount('total').as('initOSDCount');
+      const hostnames = [
+        'ceph-node-00.cephlab.com',
+        'ceph-node-02.cephlab.com',
+        'ceph-node-03.cephlab.com'
+      ];
+      for (const hostname of hostnames) {
+        osds.create('hdd', hostname, true);
 
-      createCluster.navigateTo();
-      createCluster.createCluster();
-      cy.get('.nav-link').contains('Create OSDs').click();
-
-      createCluster.createOSD('hdd');
-
-      // Go to the Review section and Expand the cluster
-      // because the drive group spec is only stored
-      // in frontend and will be lost when refreshed
-      cy.get('.nav-link').contains('Review').click();
-      cy.get('button[aria-label="Next"]').click();
-      cy.get('cd-dashboard').should('exist');
+        // Go to the Review section and Expand the cluster
+        // because the drive group spec is only stored
+        // in frontend and will be lost when refreshed
+        cy.get('.nav-link').contains('Review').click();
+        cy.get('button[aria-label="Next"]').click();
+        cy.get('cd-dashboard').should('exist');
+        createCluster.navigateTo();
+        createCluster.createCluster();
+        cy.get('.nav-link').contains('Create OSDs').click();
+      }
     });
   });
 });
---
+++ src/pybind/mgr/dashboard/frontend/cypress/integration/orchestrator/workflow/08-hosts.e2e-spec.ts
@@ -0,0 +1,42 @@
+import { HostsPageHelper } from 'cypress/integration/cluster/hosts.po';
+import { ServicesPageHelper } from 'cypress/integration/cluster/services.po';
+
+describe('Host Page', () => {
+  const hosts = new HostsPageHelper();
+  const services = new ServicesPageHelper();
+
+  const hostnames = [
+    'ceph-node-00.cephlab.com',
+    'ceph-node-01.cephlab.com',
+    'ceph-node-02.cephlab.com',
+    'ceph-node-03.cephlab.com'
+  ];
+
+  beforeEach(() => {
+    cy.login();
+    Cypress.Cookies.preserveOnce('token');
+    hosts.navigateTo();
+  });
+
+  // rgw is needed for testing the force maintenance
+  it('should check if rgw daemon is running on all hosts', () => {
+    for (const hostname of hostnames) {
+      hosts.clickTab('cd-host-details', hostname, 'Daemons');
+      cy.get('cd-host-details').within(() => {
+        services.checkServiceStatus('rgw');
+      });
+    }
+  });
+
+  it('should force maintenance and exit', { retries: 2 }, () => {
+    hosts.maintenance(hostnames[1], true, true);
+  });
+
+  it('should drain, remove and add the host back', () => {
+    hosts.drain(hostnames[1]);
+    hosts.remove(hostnames[1]);
+    hosts.navigateTo('add');
+    hosts.add(hostnames[1]);
+    hosts.checkExist(hostnames[1], true);
+  });
+});
