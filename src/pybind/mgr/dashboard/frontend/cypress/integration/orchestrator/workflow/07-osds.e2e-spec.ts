b6759b75c9fc4d3fb565201aa6bbe0c2473fd3d4
Nizamudeen A <nia@redhat.com>
2022-01-13 18:28:56 +0530

    mgr/dashboard: Refactoring dashboard cephadm checks
    I isolated all the tests suites into there respective files
    so that in future it is easier to add more tests to it.
    
    I also given priority to the host actions.
    
    Create OSD checks are now written in a way that OSDs
    are created only on the intended hosts. This will make
    the host draining process easier and less time consuming.
    
    Also tried to address the flaky force maintenance checks.
    
    Removed some duplicated codes
    
    Service creation part improved to reduce the time taken
    for its completion
    
    Fixes: https://tracker.ceph.com/issues/53905
    Signed-off-by: Nizamudeen A <nia@redhat.com>

---
+++ src/pybind/mgr/dashboard/frontend/cypress/integration/orchestrator/workflow/07-osds.e2e-spec.ts
@@ -0,0 +1,22 @@
+import { OSDsPageHelper } from 'cypress/integration/cluster/osds.po';
+
+describe('OSDs page', () => {
+  const osds = new OSDsPageHelper();
+
+  beforeEach(() => {
+    cy.login();
+    Cypress.Cookies.preserveOnce('token');
+    osds.navigateTo();
+  });
+
+  it('should check if atleast 3 osds are created', { retries: 3 }, () => {
+    // we have created a total of more than 3 osds throughout
+    // the whole tests so ensuring that atleast
+    // 3 osds are listed in the table. Since the OSD
+    // creation can take more time going with
+    // retry of 3
+    for (let id = 0; id < 3; id++) {
+      osds.checkStatus(id, ['in', 'up']);
+    }
+  });
+});
