3fd443a05e48e89778e318617ac3e07792760676
Nitzan Mordechai <nmordech@redhat.com>
2022-01-24 12:55:23 +0200

    pybind/mgr: ceph osd status crash with ZeroDivisionError
    If stats-update is called a second time within a second, get_rate() fails with
    a 'divide by 0' error.
    Change the check before the computation, taking into account the fact that
    two f.p numbers may differ, but still their diff - when cast into an int - might
    be zero.
    
    Fixed: https://tracker.ceph.com/issues/53538
    Signed-off-by: Nitzan Mordechai <nmordech@redhat.com>

--- src/pybind/mgr/status/module.py
+++ src/pybind/mgr/status/module.py
@@ -24,7 +24,7 @@ class Module(MgrModule):
 
     def get_rate(self, daemon_type: str, daemon_name: str, stat: str) -> int:
         data = self.get_counter(daemon_type, daemon_name, stat)[stat]
-        if data and len(data) > 1 and data[-1][0] != data[-2][0]:
+        if data and len(data) > 1 and (int(data[-1][0] - data[-2][0]) != 0):
             return (data[-1][1] - data[-2][1]) // int(data[-1][0] - data[-2][0])
         else:
             return 0
