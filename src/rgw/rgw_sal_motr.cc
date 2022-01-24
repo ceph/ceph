a17789091f7fd0c6916518bba525bde50bd30c28
Pritha Srivastava <prsrivas@redhat.com>
2022-01-24 11:01:08 +0530

    rgw: implementation of forward_request_to_master() for role REST APIs.
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>

--- src/rgw/rgw_sal_motr.cc
+++ src/rgw/rgw_sal_motr.cc
@@ -2936,6 +2936,14 @@ int MotrStore::forward_request_to_master(const DoutPrefixProvider *dpp, User* us
   return 0;
 }
 
+int MotrStore::forward_iam_request_to_master(const DoutPrefixProvider *dpp, const RGWAccessKey& key, obj_version* objv,
+					     bufferlist& in_data,
+					     RGWXMLDecoder::XMLParser* parser, req_info& info,
+					     optional_yield y)
+{
+    return 0;
+}
+
 std::string MotrStore::zone_unique_id(uint64_t unique_num)
 {
   return "";
