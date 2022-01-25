f2180afd1be53e88c9c782f19154b09342f020c0
Zack Cerza <zack@redhat.com>
2022-01-25 11:47:28 -0700

    teuthology.sh: Use absolute paths
    Signed-off-by: Zack Cerza <zack@redhat.com>

--- docs/docker-compose/teuthology.sh
+++ docs/docker-compose/teuthology.sh
@@ -2,8 +2,9 @@
 set -e
 # We don't want -x yet, in case the private key is sensitive
 echo "$SSH_PRIVKEY" > $HOME/.ssh/id_ed25519
+source /teuthology/virtualenv/bin/activate
 set -x
-./virtualenv/bin/teuthology-suite -v \
+teuthology-suite -v \
     --ceph-repo https://github.com/ceph/ceph.git \
     --suite-repo https://github.com/ceph/ceph.git \
     -c master \
@@ -14,6 +15,7 @@ set -x
     --suite-branch master \
     --subset 9000/100000 \
     -p 75 \
-    --force-priority
-./virtualenv/bin/teuthology-dispatcher --log-dir ./log --tube testnode
+    --force-priority \
+    /teuthology/custom_conf.yaml
+teuthology-dispatcher --log-dir /teuthology/log --tube testnode
 tail -f /dev/null
