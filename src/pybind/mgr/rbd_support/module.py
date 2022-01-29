c33be6bb549f8c861055cb5ebc95cdba1ac44f3b
Ilya Dryomov <idryomov@gmail.com>
2022-01-29 15:01:27 +0100

    mgr/rbd_support: level_spec is optional for schedule list/status
    Commit 6cd10dc7cd9b ("mgr/rbd_support: level_spec passed to some
    commands is not optional") is wrong.  While it is true that a valid
    level_spec is needed to create a LevelSpec instance, an empty string
    is very much a valid level spec -- it signifies "all levels".
    
    This wasn't caught because within Ceph these commands are wrapped by
    rbd CLI which injects an empty string in get_level_spec_args().
    
    Fixes: https://tracker.ceph.com/issues/54058
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- src/pybind/mgr/rbd_support/module.py
+++ src/pybind/mgr/rbd_support/module.py
@@ -108,7 +108,7 @@ class Module(MgrModule):
     @CLIReadCommand('rbd mirror snapshot schedule list')
     @with_latest_osdmap
     def mirror_snapshot_schedule_list(self,
-                                      level_spec: str) -> Tuple[int, str, str]:
+                                      level_spec: str = '') -> Tuple[int, str, str]:
         """
         List rbd mirror snapshot schedule
         """
@@ -118,7 +118,7 @@ class Module(MgrModule):
     @CLIReadCommand('rbd mirror snapshot schedule status')
     @with_latest_osdmap
     def mirror_snapshot_schedule_status(self,
-                                        level_spec: str) -> Tuple[int, str, str]:
+                                        level_spec: str = '') -> Tuple[int, str, str]:
         """
         Show rbd mirror snapshot schedule status
         """
@@ -248,7 +248,7 @@ class Module(MgrModule):
     @CLIReadCommand('rbd trash purge schedule list')
     @with_latest_osdmap
     def trash_purge_schedule_list(self,
-                                  level_spec: str) -> Tuple[int, str, str]:
+                                  level_spec: str = '') -> Tuple[int, str, str]:
         """
         List rbd trash purge schedule
         """
@@ -258,7 +258,7 @@ class Module(MgrModule):
     @CLIReadCommand('rbd trash purge schedule status')
     @with_latest_osdmap
     def trash_purge_schedule_status(self,
-                                    level_spec: str) -> Tuple[int, str, str]:
+                                    level_spec: str = '') -> Tuple[int, str, str]:
         """
         Show rbd trash purge schedule status
         """
