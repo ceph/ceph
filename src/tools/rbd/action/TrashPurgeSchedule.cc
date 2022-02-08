70f2d5e2968ba2dae0763efe5a005d3d0b4aa0ac
Ilya Dryomov <idryomov@gmail.com>
2022-02-08 10:11:49 +0100

    rbd: mark optional positional arguments as such in help output
    Currently at least five commands have optional positional arguments.
    
    Overloading po::value<std::string>()->default_value("") for this
    is a bit sneaky but nothing better fits into the existing Shell.cc
    framework.
    
    Note that strictly speaking "[<interval>] [<start-time>]" should be
    "[<interval> [<start-time>]]" but we aren't doing that here because
    "ceph" command doesn't do it either.
    
    Fixes: https://tracker.ceph.com/issues/54191
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- src/tools/rbd/action/TrashPurgeSchedule.cc
+++ src/tools/rbd/action/TrashPurgeSchedule.cc
@@ -151,7 +151,7 @@ std::ostream& operator<<(std::ostream& os, ScheduleStatus &s) {
 void get_arguments_add(po::options_description *positional,
                        po::options_description *options) {
   add_level_spec_options(options, false);
-  add_schedule_options(positional);
+  add_schedule_options(positional, true);
 }
 
 int execute_add(const po::variables_map &vm,
@@ -186,7 +186,7 @@ int execute_add(const po::variables_map &vm,
 void get_arguments_remove(po::options_description *positional,
                           po::options_description *options) {
   add_level_spec_options(options, false);
-  add_schedule_options(positional);
+  add_schedule_options(positional, false);
 }
 
 int execute_remove(const po::variables_map &vm,
