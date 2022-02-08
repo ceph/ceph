58a909203cc171622ea78e7741148922fa5f7b24
Nizamudeen A <nia@redhat.com>
2022-02-08 11:50:29 +0530

    mgr/dashboard: set appropriate baseline branch for applitools
    All the dashboard PRs are checked against a baseline branch called
    'default' in the visual regresstion testing. This will cause issues when
    testing PRs in different branches. For eg: currently our master and
    pacific has to save two different screenshots since the two of them
    differ slightly.
    
    Disabling the applitools logs as well because its too 'noisy'
    
    Fixes: https://tracker.ceph.com/issues/54190
    Signed-off-by: Nizamudeen A <nia@redhat.com>
    (cherry picked from commit 40c902ac59b758a314f6a123d71cb59342523dac)

--- src/pybind/mgr/dashboard/frontend/applitools.config.js
+++ src/pybind/mgr/dashboard/frontend/applitools.config.js
@@ -1,3 +1,12 @@
+const fs = require('fs')
+var branch = new String();
+
+// Read the contents of the ceph_release file to retrieve
+// the branch
+fs.readFile('../../../../ceph_release', (_, data) => {
+    branch = data.toString().split('\n')[1];
+});
+
 module.exports = {
   appName: 'Ceph Dashboard',
   apiKey: process.env.APPLITOOLS_API_KEY,
@@ -7,8 +16,9 @@ module.exports = {
     { width: 800, height: 600, name: 'chrome' },
     { width: 800, height: 600, name: 'firefox' }
   ],
-  showLogs: true,
+  showLogs: false,
   saveDebugData: true,
   failCypressOnDiff: true,
-  concurrency: 4
+  concurrency: 4,
+  baselineBranchName: branch
 };
