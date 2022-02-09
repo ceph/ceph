c7a9d2d5b401d571b90188b75c45f2938dc1d2fe
Casey Bodley <cbodley@redhat.com>
2022-02-09 16:55:38 -0500

    rgw: prevent 'radosgw-admin bucket reshard' if zonegroup reshard is disabled
    dynamic reshard was gated behind the zonegroup resharding flag with
    RGWSI_Zone::can_reshard(), but manual reshard was only calling
    RGWBucketReshard::can_reshard()
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -7709,6 +7709,12 @@ next:
     }
 
     auto zone_svc = static_cast<rgw::sal::RadosStore*>(store)->svc()->zone;
+    if (!zone_svc->can_reshard()) {
+      const auto& zonegroup = zone_svc->get_zonegroup();
+      std::cerr << "The zonegroup '" << zonegroup.get_name() << "' does not "
+          "have the resharding feature enabled." << std::endl;
+      return ENOTSUP;
+    }
     if (!RGWBucketReshard::can_reshard(bucket->get_info(), zone_svc) &&
         !yes_i_really_mean_it) {
       std::cerr << "Bucket '" << bucket->get_name() << "' already has too many "
@@ -7716,7 +7722,7 @@ next:
           "from previous reshards that peer zones haven't finished syncing. "
           "Resharding is not recommended until the old generations sync, but "
           "you can force a reshard with --yes-i-really-mean-it." << std::endl;
-      return -EINVAL;
+      return EINVAL;
     }
 
     RGWBucketReshard br(static_cast<rgw::sal::RadosStore*>(store),
