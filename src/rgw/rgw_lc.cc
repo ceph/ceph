66ee3a77c68f6b30c072be02bb016e530b5caede
Matt Benjamin <mbenjamin@redhat.com>
2022-02-18 08:33:35 -0500

    rgwlc:  pretty-print status in RGWLC::process(int...)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_lc.cc
+++ src/rgw/rgw_lc.cc
@@ -280,9 +280,9 @@ static inline std::ostream& operator<<(std::ostream &os, rgw::sal::Lifecycle::LC
   os << "; start_time=";
   os << rgw_to_asctime(utime_t(time_t(ent.start_time), 0));
   os << "; status=";
-    os << ent.status;
-    os << ">";
-    return os;
+  os << LC_STATUS[ent.status];
+  os << ">";
+  return os;
 }
 
 static bool obj_has_expired(const DoutPrefixProvider *dpp, CephContext *cct, ceph::real_time mtime, int days,
