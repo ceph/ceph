b167988ade8bd21a4af75cf6e1117be91e57ed6a
Pere Diaz Bou <pdiazbou@redhat.com>
2022-02-22 15:00:51 +0100

    exporter daemon
    Signed-off-by: Pere Diaz Bou <pdiazbou@redhat.com>

---
+++ src/ceph_exporter.cc
@@ -0,0 +1,19 @@
+#include "mgr/DaemonMetricCollector.h"
+#include <iostream>
+#include "global/global_init.h"
+#include "common/ceph_argparse.h"
+#include <map>
+
+
+int main(int argc, char** argv) {
+  // TODO: daemonize
+  std::cout << "inside exporter" << std::endl;
+  std::map<std::string,std::string> defaults = {
+    { "keyring", "$mgr_data/keyring" }
+  };
+  auto args = argv_to_vec(argc, argv);
+  auto cct = global_init(&defaults, args, CEPH_ENTITY_TYPE_EXPORTER,
+			 CODE_ENVIRONMENT_DAEMON, 0);
+  DaemonMetricCollector collector;
+  collector.main();
+}
---
+++ src/mgr/DaemonMetricCollector.cc
@@ -0,0 +1,7 @@
+#include "DaemonMetricCollector.h"
+#include <iostream>
+
+
+void DaemonMetricCollector::main() {
+  std::cout << "metric" << std::endl;
+}
