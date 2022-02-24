ceph/ceph

commit 3bb8a7210a647ae0f074277d5c2a73f2719741e7

osd: requeue ops when PG is no longer laggy
When we get a lease_ack, leave the laggy state.

Signed-off-by: Sage Weil <sage@redhat.com>

src/osd/PeeringState.cc
@@ -1155,10 +1155,11 @@ void PeeringState::proc_lease_ack(int from, const pg_lease_ack_t& a)
     }
   }
   if (was_min) {
+    auto old_ru = readable_until;
     recalc_readable_until();
-//    if (pl->is_laggy() && readable_until > now) {
-#warning fixme: wake up?
-    //}
+    if (now < old_ru) {
+      pl->recheck_readable();
+    }
   }
 }
 

src/osd/PeeringState.h
@@ -273,6 +273,7 @@ public:
     virtual ceph::signedspan get_mnow() = 0;
     virtual HeartbeatStampsRef get_hb_stamps(int peer) = 0;
     virtual void schedule_renew_lease(epoch_t plr, ceph::timespan delay) = 0;
+    virtual void recheck_readable() = 0;
 
     // ============ Flush state ==================
     /**

src/osd/PrimaryLogPG.cc
@@ -786,6 +786,21 @@ bool PrimaryLogPG::check_laggy_requeue(OpRequestRef& op)
   return false;
 }
 
+void PrimaryLogPG::recheck_readable()
+{
+  if (is_laggy()) {
+    auto ru = recovery_state.get_readable_until();
+    auto mnow = osd->get_mnow();
+    if (ru > mnow) {
+      dout(10) << __func__ << " no longer laggy (ru " << ru << " > mnow " << mnow
+	       << ")" << dendl;
+      state_clear(PG_STATE_LAGGY);
+      publish_stats_to_osd();
+      requeue_ops(waiting_for_readable);
+    }
+  }
+}
+
 bool PrimaryLogPG::pgls_filter(const PGLSFilter& filter, const hobject_t& sobj)
 {
   bufferlist bl;

src/osd/PrimaryLogPG.h
@@ -1875,6 +1875,7 @@ public:
 
   bool check_laggy(OpRequestRef& op);
   bool check_laggy_requeue(OpRequestRef& op);
+  void recheck_readable() override;
 
   bool is_backfill_target(pg_shard_t osd) const {
     return recovery_state.is_backfill_target(osd);

commit caeca396e8b149cfa09ed99eda4f7a7186b005b4

osd/PeeringState: fix missed `recheck_readable` from laggy
Previously, the first `pg_lease_ack_t` after becoming laggy would not
trigger `recheck_readable`. However, every other ack would trigger it.
The logic is inverted, causing unnecessarily long laggy PG state.

Fixes: 3bb8a7210a6 (osd: requeue ops when PG is no longer laggy)
Fixes: https://tracker.ceph.com/issues/53806
Signed-off-by: 胡玮文 <huww98@outlook.com>

src/osd/PeeringState.cc
@@ -1220,7 +1220,7 @@ void PeeringState::proc_lease_ack(int from, const pg_lease_ack_t& a)
   if (was_min) {
     auto old_ru = readable_until;
     recalc_readable_until();
-    if (now < old_ru) {
+    if (now >= old_ru) {
       pl->recheck_readable();
     }
   }