d2a14aa5161adb792333151b7d197ea238d37213
Xiubo Li <xiubli@redhat.com>
2022-03-09 15:42:56 +0800

    mds: notify clients if the session has already opened
    If the connection was accidently closed due to the socket issue or
    something else the client will try to open the opened sessions, for
    now the MDS will just discard the session open request.
    
    But the client will keep waiting the reply from the mds forever.
    
    We need to tell the clients what has happened instead of discard it
    directly. And when the client get the session open reply, it can
    do what needed.
    
    Fixes: https://tracker.ceph.com/issues/53911
    Signed-off-by: Xiubo Li <xiubli@redhat.com>
    (cherry picked from commit f232b020fc36ba33d6ef144bdb9e607beb135763)

--- src/mds/cephfs_features.cc
+++ src/mds/cephfs_features.cc
@@ -23,6 +23,7 @@ static const std::array feature_names
   "deleg_ino",
   "metric_collect",
   "alternate_name",
+  "notify_session_state",
 };
 static_assert(feature_names.size() == CEPHFS_FEATURE_MAX + 1);
 
