51aa4c7a7784d1918ecba022a0c3944bdbe60d5a
Xuehan Xu <xxhdx1985126@gmail.com>
2022-03-15 11:37:28 +0800

    crimson/os/seastore/cache: record backref extents placement update in Cache::backref_extents
    Signed-off-by: Xuehan Xu <xxhdx1985126@gmail.com>

--- src/crimson/os/seastore/cache.h
+++ src/crimson/os/seastore/cache.h
@@ -938,6 +938,59 @@ public:
     }
   }
 
+  struct backref_extent_buf_entry_t {
+    backref_extent_buf_entry_t(
+      paddr_t paddr,
+      extent_types_t type)
+      : paddr(paddr), type(type) {}
+    paddr_t paddr = P_ADDR_NULL;
+    extent_types_t type = extent_types_t::ROOT;
+    struct cmp_t {
+      using is_transparent = paddr_t;
+      bool operator()(
+	const backref_extent_buf_entry_t &l,
+	const backref_extent_buf_entry_t &r) const {
+	return l.paddr < r.paddr;
+      }
+      bool operator()(
+	const paddr_t &l,
+	const backref_extent_buf_entry_t &r) const {
+	return l < r.paddr;
+      }
+      bool operator()(
+	const backref_extent_buf_entry_t &l,
+	const paddr_t &r) const {
+	return l.paddr < r;
+      }
+    };
+  };
+
+  void add_backref_extent(paddr_t paddr, extent_types_t type) {
+    assert(!paddr.is_relative());
+    auto [iter, inserted] = backref_extents.emplace(paddr, type);
+    assert(inserted);
+  }
+
+  void remove_backref_extent(paddr_t paddr) {
+    auto iter = backref_extents.find(paddr);
+    if (iter != backref_extents.end())
+      backref_extents.erase(iter);
+  }
+
+  std::set<
+    backref_extent_buf_entry_t,
+    backref_extent_buf_entry_t::cmp_t> get_backref_extents_in_range(
+    paddr_t start,
+    paddr_t end) {
+    auto start_iter = backref_extents.lower_bound(start);
+    auto end_iter = backref_extents.upper_bound(end);
+    std::set<
+      backref_extent_buf_entry_t,
+      backref_extent_buf_entry_t::cmp_t> res;
+    res.insert(start_iter, end_iter);
+    return res;
+  }
+
 private:
   ExtentPlacementManager& epm;
   RootBlockRef root;               ///< ref to current root
@@ -951,6 +1004,10 @@ private:
    * holds refs to dirty extents.  Ordered by CachedExtent::get_dirty_from().
    */
   CachedExtent::list dirty;
+
+  std::set<
+    backref_extent_buf_entry_t,
+    backref_extent_buf_entry_t::cmp_t> backref_extents;
 
   /**
    * lru
