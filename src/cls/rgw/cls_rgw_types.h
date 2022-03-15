cbdeb349b3e841fb55ed8506680e30787c3985ed
Iqbal Khan <iqkhan@redhat.com>
2022-03-15 22:51:18 +0530

    In "cls_rgw_ls_entry" class generate_test_instances() function is made static Fixes: https://tracker.ceph.com/issues/54054
    Signed-off-by: Iqbal Khan <iqkhan@redhat.com>

--- src/cls/rgw/cls_rgw_types.h
+++ src/cls/rgw/cls_rgw_types.h
@@ -1300,7 +1300,7 @@ struct cls_rgw_lc_entry {
     DECODE_FINISH(bl);
   }
   void dump(Formatter *f) const;
-  void generate_test_instances(std::list<cls_rgw_lc_entry*>& ls);
+  static void generate_test_instances(std::list<cls_rgw_lc_entry*>& ls);
 };
 WRITE_CLASS_ENCODER(cls_rgw_lc_entry);
 
