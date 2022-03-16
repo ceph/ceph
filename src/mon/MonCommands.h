77ad5a810c7e21d7e12947a07a5ba9b079dd1081
tancz1 <tancz1@lenovo.com>
2022-03-16 17:20:40 +0800

    mon: add proxy to cache tier options
    When we set the proxy mode to remove a writeback cache according to
    the ceph official documentation an error occurred:
    
    [root@controller-1 root]# ceph osd tier cache-mode cachepool proxy
    Invalid command: proxy not in writeback|readproxy|readonly|none
    osd tier cache-mode writeback|readproxy|readonly|none [--yes-i-really-mean-it]:
    specify the caching mode for cache tier
    
    According to the description of the official website document: since
    a writeback cache may have modified data, you must take steps to ensure
    that you do not lose any recent changes to objects in the cache before
    you disable and remove it. Change the cache mode to proxy so that new and
    modified objects will flush to the backing storage pool.
    
    Fixes: https://tracker.ceph.com/issues/54576
    Signed-off-by: tan changzhi <544463199@qq.com>

--- src/mon/MonCommands.h
+++ src/mon/MonCommands.h
@@ -1169,7 +1169,7 @@ COMMAND_WITH_FLAG("osd tier remove "
     FLAG(DEPRECATED))
 COMMAND("osd tier cache-mode "
 	"name=pool,type=CephPoolname "
-	"name=mode,type=CephChoices,strings=writeback|readproxy|readonly|none "
+	"name=mode,type=CephChoices,strings=writeback|proxy|readproxy|readonly|none "
 	"name=yes_i_really_mean_it,type=CephBool,req=false",
 	"specify the caching mode for cache tier <pool>", "osd", "rw")
 COMMAND("osd tier set-overlay "
