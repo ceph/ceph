613cf906649f7a3cf505e9e2e3029c632e328949
NitzanMordhai <nmordech@redhat.com>
2022-03-17 07:30:10 +0000

    osd/PGLog: Add DENC encode\decode\bound for pg_log_entry and subclasses
    in order to find entries length we will use the DENC encode\decode, that commit will
    add the DENC functions
    
    Signed-off-by: Nitzan Mordechai <nmordech@redhat.com>

--- src/crimson/osd/ops_executer.cc
+++ src/crimson/osd/ops_executer.cc
@@ -1310,7 +1310,7 @@ static PG::interruptible_future<> do_pgnls_filtered(
 
   hobject_t lower_bound;
   try {
-    lower_bound.decode(bp);
+    ceph::decode(lower_bound, bp);
   } catch (const buffer::error&) {
     throw std::invalid_argument("unable to decode PGNLS_FILTER description");
   }
@@ -1400,7 +1400,7 @@ static PG::interruptible_future<> do_pgls(
   hobject_t lower_bound;
   auto bp = osd_op.indata.cbegin();
   try {
-    lower_bound.decode(bp);
+    ceph::decode(lower_bound, bp);
   } catch (const buffer::error&) {
     throw std::invalid_argument{"unable to decode PGLS handle"};
   }
@@ -1439,7 +1439,7 @@ static PG::interruptible_future<> do_pgls_filtered(
 
   hobject_t lower_bound;
   try {
-    lower_bound.decode(bp);
+    ceph::decode(lower_bound, bp);
   } catch (const buffer::error&) {
     throw std::invalid_argument("unable to decode PGLS_FILTER description");
   }
