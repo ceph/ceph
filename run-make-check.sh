e6e3f5b4a300a742846e16f9946a8f0909243731
David Galloway <dgallowa@redhat.com>
2022-03-25 17:29:44 -0400

    build: Add some debugging messages
    Having a unique string like "CI_DEBUG" will help me know where we are in the build process in Jenkins logs.
    
    Signed-off-by: David Galloway <dgallowa@redhat.com>
    (cherry picked from commit 57edb76ea46893294a70aa080916bc723fb35f9e)

--- run-make-check.sh
+++ run-make-check.sh
@@ -74,7 +74,10 @@ function main() {
     if [ $WITH_ZBD ]; then
         cmake_opts+=" -DWITH_ZBD=ON"
     fi
+    in_jenkins && echo "CI_DEBUG: Our cmake_opts are: $cmake_opts
+                        CI_DEBUG: Running ./configure"
     configure $cmake_opts $@
+    in_jenkins && echo "CI_DEBUG: Running 'build tests'"
     build tests
     echo "make check: successful build on $(git rev-parse HEAD)"
     run
