4465bcf6202deeb90ef3357d532c9b0f864be4c8
luo rixin <luorixin@huawei.com>
2022-03-31 11:23:14 +0800

    common: let thread_shard_index use static init to avoid dynamic cmp
    let thread_shard_index use static init to avoid dynamic cmp
    
    Signed-off-by: luo rixin <luorixin@huawei.com>

--- src/common/mempool.cc
+++ src/common/mempool.cc
@@ -15,10 +15,6 @@
 #include "include/mempool.h"
 #include "include/demangle.h"
 
-// Thread local variables should save index, not &shard[index],
-// because shard[] is defined in the class
-static thread_local size_t thread_shard_index = mempool::num_shards;
-
 // default to debug_mode off
 bool mempool::debug_mode = false;
 
@@ -95,7 +91,7 @@ size_t mempool::pool_t::allocated_items() const
 
 void mempool::pool_t::adjust_count(ssize_t items, ssize_t bytes)
 {
-  thread_shard_index = (thread_shard_index == num_shards) ? pick_a_shard_int() : thread_shard_index;
+  static thread_local size_t thread_shard_index = pick_a_shard_int();
   shard[thread_shard_index].items += items;
   shard[thread_shard_index].bytes += bytes;
 }
