685012733de142a56a6c62819cda1a1a9402899e
Xiubo Li <xiubli@redhat.com>
2022-04-01 22:15:39 +0800

    mds: try to open the ino if couldn't be found in MDCache in all peers
    There has one case that when the MDS crashes and the openfiletable
    journal couldn't be flushed and then the replacing MDS is possibly
    won't load some already opened CInodes into the MDCache. And if the
    clients will retry some requests after reconnected, the MDS will
    return -ESTALE after failing to find the ino in all active peers.
    
    As a workaround users can run `ls -R ${mountpoint}` to list all the
    sub-files or sub-direcotries from the mountpoint.
    
    We need try to open the ino and try it again.
    
    Fixes: https://tracker.ceph.com/issues/53504
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- src/mds/Server.h
+++ src/mds/Server.h
@@ -187,6 +187,7 @@ public:
   void journal_allocated_inos(MDRequestRef& mdr, EMetaBlob *blob);
   void apply_allocated_inos(MDRequestRef& mdr, Session *session);
 
+  void _try_open_ino(MDRequestRef& mdr, int r, inodeno_t ino);
   CInode* rdlock_path_pin_ref(MDRequestRef& mdr, bool want_auth,
 			      bool no_want_auth=false);
   CDentry* rdlock_path_xlock_dentry(MDRequestRef& mdr, bool create,
