929238dbd268fe6c94a622141dffeda4dd846dbd
Yuval Lifshitz <ylifshit@redhat.com>
2022-04-15 14:45:49 +0300

    rgw/multisite: ignore sync init state when doing bilog trimming
    regardless of the sync state, we take the marker from the
    incremental sync object
    
    Signed-off-by: Yuval Lifshitz <ylifshit@redhat.com>

--- src/rgw/rgw_rados.cc
+++ src/rgw/rgw_rados.cc
@@ -6204,7 +6204,8 @@ int RGWRados::Bucket::UpdateIndex::guard_reshard(const DoutPrefixProvider *dpp, 
       obj_instance.key << dendl;
     r = store->block_while_resharding(bs, obj_instance, target->bucket_info, null_yield, dpp);
     if (r == -ERR_BUSY_RESHARDING) {
-      ldpp_dout(dpp, 0) << "ERROR: block_while_resharding() still busy. obj=" << dendl;
+      ldpp_dout(dpp, 0) << "ERROR: block_while_resharding() still busy. obj=" <<
+        obj_instance.key << dendl;
       continue;
     }
     if (r < 0) {
