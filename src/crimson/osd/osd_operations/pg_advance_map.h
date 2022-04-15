ac91787db5b7868c98d2e33830a325af766c7bfa
Radosław Zarzyński <rzarzyns@redhat.com>
2022-04-15 18:10:20 +0200

    crimson/osd: migrate PGAdvanceMap to new tracking infra.
    Signed-off-by: Radosław Zarzyński <rzarzyns@redhat.com>

--- src/crimson/osd/osd_operations/pg_advance_map.h
+++ src/crimson/osd/osd_operations/pg_advance_map.h
@@ -7,9 +7,9 @@
 #include <seastar/core/future.hh>
 
 #include "crimson/osd/osd_operation.h"
+#include "crimson/osd/osd_operations/peering_event.h"
 #include "osd/osd_types.h"
 #include "crimson/common/type_helpers.h"
-#include "osd/PeeringState.h"
 
 namespace ceph {
   class Formatter;
@@ -43,6 +43,10 @@ public:
   void print(std::ostream &) const final;
   void dump_detail(ceph::Formatter *f) const final;
   seastar::future<> start();
+
+  std::tuple<
+    PeeringEvent::PGPipeline::Process::BlockingEvent
+  > tracking_events;
 };
 
 }
