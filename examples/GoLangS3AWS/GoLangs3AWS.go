e885106eff1e8b0387d6e9f076690f82a5be5572
Ochuwa-sophie <sophiecodes@gmail.com>
2022-04-17 01:00:36 +0000

    first version of golang sdk

---
+++ examples/GoLangS3AWS/GoLangs3AWS.go
@@ -0,0 +1,137 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+var (
+	s3session *s3.S3
+)
+
+const (
+	BUCKET_NAME = "cephDonkey"
+	REGION      = "default"
+)
+
+func init() {
+	s3session = s3.New(session.Must(session.NewSession(&aws.Config{
+		Region: aws.String(REGION),
+	})))
+}
+
+func listBuckets() (resp *s3.ListBucketsOutput) {
+	resp, err := s3session.ListBuckets(&s3.ListBucketsInput{})
+	if err != nil {
+		panic(err)
+	}
+
+	return resp
+}
+
+func createBucket() (resp *s3.CreateBucketOutput) {
+	resp, err := s3session.CreateBucket(&s3.CreateBucketInput{
+		// ACL: aws.String(s3.BucketCannedACLPrivate),
+		// ACL: aws.String(s3.BucketCannedACLPublicRead),
+		Bucket: aws.String(BUCKET_NAME),
+		CreateBucketConfiguration: &s3.CreateBucketConfiguration{
+			LocationConstraint: aws.String(REGION),
+		},
+	})
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case s3.ErrCodeBucketAlreadyExists:
+				fmt.Println("Bucket name already in use!")
+				panic(err)
+			case s3.ErrCodeBucketAlreadyOwnedByYou:
+				fmt.Println("Bucket exists and is owned by you!")
+			default:
+				panic(err)
+			}
+		}
+	}
+
+	return resp
+}
+
+func uploadObject(filename string) (resp *s3.PutObjectOutput) {
+	f, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Uploading:", filename)
+	resp, err = s3session.PutObject(&s3.PutObjectInput{
+		Body:   f,
+		Bucket: aws.String(BUCKET_NAME),
+		Key:    aws.String(strings.Split(filename, "/")[1]),
+		ACL:    aws.String(s3.BucketCannedACLPublicRead),
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return resp
+}
+
+func listObjects() (resp *s3.ListObjectsV2Output) {
+	resp, err := s3session.ListObjectsV2(&s3.ListObjectsV2Input{
+		Bucket: aws.String(BUCKET_NAME),
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	return resp
+}
+
+func getObject(filename string) {
+	fmt.Println("Downloading: ", filename)
+
+	resp, err := s3session.GetObject(&s3.GetObjectInput{
+		Bucket: aws.String(BUCKET_NAME),
+		Key:    aws.String(filename),
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	err = ioutil.WriteFile(filename, body, 0644)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	folder := "files"
+
+	files, _ := ioutil.ReadDir(folder)
+	fmt.Println(files)
+	for _, file := range files {
+		if file.IsDir() {
+			continue
+		} else {
+			uploadObject(folder + "/" + file.Name())
+		}
+	}
+
+	fmt.Println(listObjects())
+
+	for _, object := range listObjects().Contents {
+		getObject(*object.Key)
+	}
+
+	fmt.Println(listObjects())
+}
---
+++ server.py.1
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+"""
+Very simple HTTP server in python for logging requests
+Usage::
+    ./server.py [<port>]
+"""
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+
+class S(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
