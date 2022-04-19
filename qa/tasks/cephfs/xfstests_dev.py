e3f95998a5b2dbd93eda10f37dd75ed71bb67a14
Rishabh Dave <ridave@redhat.com>
2022-04-19 14:54:31 +0530

    qa/cephfs: use StringIO instead of BytesIO
    StringIO can be operated on without extra hassles of converting. So
    replace BytesIO by StringIO in test_acls.py and xfstests_dev.py
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/tasks/cephfs/xfstests_dev.py
+++ qa/tasks/cephfs/xfstests_dev.py
@@ -1,4 +1,4 @@
-from io import BytesIO
+from io import StringIO
 from logging import getLogger
 
 from tasks.cephfs.cephfs_test_case import CephFSTestCase
@@ -29,11 +29,11 @@ class XFSTestsDev(CephFSTestCase):
         # NOTE: On teuthology machines it's necessary to run "make" as
         # superuser since the repo is cloned somewhere in /tmp.
         self.mount_a.client_remote.run(args=['sudo', 'make'],
-                                       cwd=self.repo_path, stdout=BytesIO(),
-                                       stderr=BytesIO())
+                                       cwd=self.repo_path, stdout=StringIO(),
+                                       stderr=StringIO())
         self.mount_a.client_remote.run(args=['sudo', 'make', 'install'],
                                        cwd=self.repo_path, omit_sudo=False,
-                                       stdout=BytesIO(), stderr=BytesIO())
+                                       stdout=StringIO(), stderr=StringIO())
 
     def get_repo(self):
         """
