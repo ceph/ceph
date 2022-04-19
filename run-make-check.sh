0ab4901be5ef84bb64c3745c2e58d7f9ea449b65
Ilya Dryomov <idryomov@gmail.com>
2022-04-19 11:21:06 +0200

    run-make-check.sh: enable RBD persistent caches
    This was attempted in commit 2877c460f372 ("run-make-check: enable
    WITH_RBD_RWL when WITH_PMEM is true") but never completed.  We soon
    bumped the requirement on libpmem, so WITH_SYSTEM_PMDK=ON wouldn't
    have worked anyway.
    
    Enable the RWL mode conditionally based on WITH_RBD_RWL variable.
    Enable the SSD mode unconditionally as it has no special dependencies
    and can be built on any architecture.
    
    Fixes: https://tracker.ceph.com/issues/55285
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- run-make-check.sh
+++ run-make-check.sh
@@ -97,9 +97,10 @@ function main() {
     if [ $WITH_ZBD ]; then
         cmake_opts+=" -DWITH_ZBD=ON"
     fi
-    if [ $WITH_PMEM ]; then
-        cmake_opts+=" -DWITH_RBD_RWL=ON -DWITH_SYSTEM_PMDK=ON"
+    if [ $WITH_RBD_RWL ]; then
+        cmake_opts+=" -DWITH_RBD_RWL=ON"
     fi
+    cmake_opts+=" -DWITH_RBD_SSD_CACHE=ON"
     in_jenkins && echo "CI_DEBUG: Our cmake_opts are: $cmake_opts
                         CI_DEBUG: Running ./configure"
     configure "$cmake_opts" "$@"
