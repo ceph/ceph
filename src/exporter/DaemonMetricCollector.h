5394b0266f65582764ee0b00780f0e35b5aff0aa
Pere Diaz Bou <pdiazbou@redhat.com>
2022-04-19 13:19:38 +0200

    exporter: Cleanups on DaemonMetricCollector and google styling
    1. Formatted all exporter files to google c++ styling.
    2. Refactored send_requests a bit to increase readabilty.
    3. Removed unused functions + variables.
    4. Moved fs::path socketdir to a static const char* SOCKETDIR.
    
    Signed-off-by: Pere Diaz Bou <pdiazbou@redhat.com>

--- src/exporter/DaemonMetricCollector.h
+++ src/exporter/DaemonMetricCollector.h
@@ -1,36 +1,37 @@
 #pragma once
 
 #include "common/admin_socket_client.h"
-#include <string>
 #include <map>
+#include <string>
 #include <vector>
 
-#include<filesystem>
-#include <string>
-#include <map>
-#include <vector>
 #include <boost/asio.hpp>
-
-namespace fs = std::filesystem;
+#include <boost/json/object.hpp>
+#include <filesystem>
+#include <map>
+#include <string>
+#include <vector>
 
 class DaemonMetricCollector {
- public:
-  int i;
+public:
   void main();
   std::string get_metrics();
+  static const char *SOCKETDIR;
 
 private:
-  // TODO: add clients
-  //       check removed sockets
-  //       list dir of sockets
   std::map<std::string, AdminSocketClient> clients;
   std::string metrics;
   int stats_period; // time to wait before sending requests again
-  fs::path socketdir = "/var/run/ceph/";
   void update_sockets();
   void request_loop(boost::asio::deadline_timer &timer);
-  void send_requests();
-  void start_mgr_connection();
+
+  void dump_asok_metrics();
+  void dump_asok_metric(std::stringstream &ss, boost::json::object perf_info,
+                        boost::json::value perf_values, std::string name,
+                        std::string labels);
+  std::pair<std::string, std::string>
+  get_labels_and_metric_name(std::string daemon_name, std::string metric_name);
+  std::string asok_request(AdminSocketClient &asok, std::string command);
 };
 
-DaemonMetricCollector& collector_instance();
+DaemonMetricCollector &collector_instance();
