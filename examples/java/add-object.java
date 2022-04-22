4d9f4e746a924b0eea74d277adf3ff837553f16a
Miracle1504 <miracle15042000@gmail.com>
2022-04-22 09:26:16 +0100

    initial logic of how the app should work
    Signed-off-by: Miracle1504 <miracle15042000@gmail.com>

---
+++ examples/java/add-object.java
@@ -0,0 +1,134 @@
+package com.ceph.rgw.example;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Random;
+import software.amazon.awssdk.core.waiters.WaiterResponse;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.paginators.ListObjectsV2Iterable;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Request;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
+import software.amazon.awssdk.services.s3.model.S3Object;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.CreateBucketRequest;
+import software.amazon.awssdk.services.s3.model.CreateBucketConfiguration;
+import software.amazon.awssdk.services.s3.waiters.S3Waiter;
+import software.amazon.awssdk.services.s3.model.HeadBucketRequest;
+import software.amazon.awssdk.services.s3.model.HeadBucketResponse;
+
+public class S3ObjectOperations {
+
+    private static S3Client s3;
+
+    public static void main(String[] args) throws IOException {
+
+        final String USAGE = "\n" +
+                "Usage:\n" +
+                "    <bucketName> <key>\n\n" +
+                "Where:\n" +
+                "    bucketName - the Amazon S3 bucket to create.\n\n" +
+                "    key - the key to use.\n\n" ;
+
+        if (args.length != 2) {
+            System.out.println(USAGE);
+            System.exit(1);
+        }
+
+        String bucketName = args[0];
+        String key = args[1];
+
+        Region region = defaultRegion();
+        s3 = S3Client.builder()
+                .region(region)
+                .build();
+
+        createBucket(s3, bucketName, region);
+
+        PutObjectRequest objectRequest = PutObjectRequest.builder()
+                .bucket(bucketName)
+                .key(key)
+                .build();
+
+        s3.putObject(objectRequest, RequestBody.fromByteBuffer(getRandomByteBuffer(10_000)));
+
+        ListObjectsV2Request listObjectsReqManual = ListObjectsV2Request.builder()
+                .bucket(bucketName)
+                .maxKeys(1)
+                .build();
+
+        boolean done = false;
+        while (!done) {
+            ListObjectsV2Response listObjResponse = s3.listObjectsV2(listObjectsReqManual);
+            for (S3Object content : listObjResponse.contents()) {
+                System.out.println(content.key());
+            }
+
+            if (listObjResponse.nextContinuationToken() == null) {
+                done = true;
+            }
+
+            listObjectsReqManual = listObjectsReqManual.toBuilder()
+                    .continuationToken(listObjResponse.nextContinuationToken())
+                    .build();
+        }
+        ListObjectsV2Request listReq = ListObjectsV2Request.builder()
+                .bucket(bucketName)
+                .maxKeys(1)
+                .build();
+
+        ListObjectsV2Iterable listRes = s3.listObjectsV2Paginator(listReq);
+        for (ListObjectsV2Response listObjResponse : listRes.contents()) {
+            for (S3Object content : listObjResponse.contents()) {
+                System.out.println(content.key());
+            }
+        }
+        listRes.stream()
+                .flatMap(r -> r.contents().stream())
+                .forEach(content -> System.out.println(" Key: " + content.key() + " size = " + content.size()));
+
+        listRes.contents().stream()
+                .forEach(content -> System.out.println(" Key: " + content.key() + " size = " + content.size()));
+
+        for (S3Object content : listRes.contents()) {
+            System.out.println(" Key: " + content.key() + " size = " + content.size());
+        }
+    }
+
+    public static void createBucket( S3Client s3Client, String bucketName, Region region) {
+
+        S3Waiter s3Waiter = s3Client.waiter();
+
+        try {
+            CreateBucketRequest bucketRequest = CreateBucketRequest.builder()
+                    .bucket(bucketName)
+                    .createBucketConfiguration(
+                            CreateBucketConfiguration.builder()
+                                    .locationConstraint(region.id())
+                                   .build())
+                    .build();
+
+            s3Client.createBucket(bucketRequest);
+            HeadBucketRequest bucketRequestWait = HeadBucketRequest.builder()
+                    .bucket(bucketName)
+                    .build();
+
+           
+            WaiterResponse<HeadBucketResponse> waiterResponse = s3Waiter.waitUntilBucketExists(bucketRequestWait);
+            waiterResponse.matched().response().ifPresent(System.out::println);
+            System.out.println(bucketName +" is ready");
+
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+    }
+    private static ByteBuffer getRandomByteBuffer(int size) throws IOException {
+        byte[] b = new byte[size];
+        new Random().nextBytes(b);
+        return ByteBuffer.wrap(b);
+    }
+}
