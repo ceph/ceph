39d9cd1355d6e318c37525129285b678f8211331
Avan Thakkar <athakkar@redhat.com>
2022-04-26 00:24:54 +0530

    exporter: fix run-tox-cephadm/mgr
    Signed-off-by: Avan Thakkar <athakkar@redhat.com>

--- src/pybind/mgr/cephadm/tests/test_services.py
+++ src/pybind/mgr/cephadm/tests/test_services.py
@@ -291,6 +291,7 @@ class TestMonitoring:
 
         with with_host(cephadm_module, 'test'):
             with with_service(cephadm_module, MonitoringSpec('node-exporter')) as _, \
+                    with_service(cephadm_module, ServiceSpec('ceph-exporter')) as _, \
                     with_service(cephadm_module, MonitoringSpec('prometheus')) as _:
 
                 y = dedent("""
@@ -312,6 +313,11 @@ class TestMonitoring:
                     - targets: ['[1::4]:9100']
                       labels:
                         instance: 'test'
+
+                  - job_name: 'ceph-exporter'
+                    static_configs:
+                    - targets:
+                      - '[1::4]:9926'
 
                 """).lstrip()
 
