ea8595ffe4b8b591d85b9599fa952a4872ff8026
Miracle1504 <miracle15042000@gmail.com>
2022-04-28 19:31:48 +0100

    refactored flags and access configurations
    Signed-off-by: Miracle1504 <miracle15042000@gmail.com>

--- examples/golang/add-object.go
+++ examples/golang/add-object.go
@@ -1,85 +1,82 @@
 package main
 
 import (
-	"bufio"
-	"context"
-	"flag"
-	"fmt"
-	"log"
-	"os"
+    "bufio"
+    "bytes"
+    "context"
+    "flag"
+    "fmt"
+    "log"
+    "os"
 
-	"github.com/aws/aws-sdk-go-v2/aws"
-	"github.com/aws/aws-sdk-go-v2/config"
-	"github.com/aws/aws-sdk-go-v2/service/s3"
-)
-
-const (
-	id  = "AWS_ACCESS_KEY_ID"
-	key = "AWS_SECRET_ACCESS_KEY"
+    "github.com/aws/aws-sdk-go-v2/aws"
+    "github.com/aws/aws-sdk-go-v2/config"
+    "github.com/aws/aws-sdk-go-v2/credentials"
+    "github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 func main() {
-	endPoint := flag.String("endpoint", "", "region of request")
-	accessId := flag.String("accessId", "", "Access Id")
-	accessKey := flag.String("accessKey", "", "Access Key")
-	ObjectName := flag.String("objectPath", "", "Object Path")
-	bucketName := flag.String("bucketName", "", "The name of the s3 bucket")
-	flag.Parse()
-	if flag.NFlag() < 4 {
-		log.Fatalf("Usage error: EndPoint, Access ID, Access Key, and Object Path not set.")
-	}
-	// I want it to be a one way flow for the app user thats why I used flags for access key and Id instead of
-	// a direct environment variable.
-	setEnv(id, *accessId)
-	setEnv(key, *accessKey)
 
-	f, err := os.Open(*ObjectName)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	file := bufio.NewReader(f)
+    endPoint := flag.String("endpoint", "", "EndPoint of request")
+    accessId := flag.String("access_key", "", "Access Id")
+    accessKey := flag.String("secret_key", "", "Access Key")
+    objectPath := flag.String("objectpath", "", "Object Path")
+    bucketName := flag.String("bucketName", "", "The name of the s3 bucket")
+    flag.Parse()
+    if flag.NFlag() < 4 {
+        log.Fatalf("Usage error: EndPoint, Access ID, Access Key, and Object Path not set.")
+    }
 
-	cfg, err := config.LoadDefaultConfig(context.TODO(),
-		config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(
-			aws.EndpointResolverWithOptionsFunc(
-				func(service, region string, options ...interface{}) (aws.Endpoint, error) {
-					return aws.Endpoint{
-						URL: *endPoint,
-					}, nil
-				}))))
-	if err != nil {
-		log.Fatal("Error loading config ", err)
-	}
+    f, err := os.Open(*objectPath)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer f.Close()
 
-	params := s3.PutObjectInput{
-		Bucket: aws.String(*bucketName),
-		Key:    aws.String(*ObjectName),
-		Body:   file,
-	}
+    fileReader := bufio.NewReader(f)
 
-	svc := createClient(cfg)
-	putObject(svc, &params, ObjectName)
+    fileBytes, err := fileReader.ReadBytes('\n')
+    if err != nil {
+        log.Fatal(err)
+    }
 
-}
+    cfg, err := config.LoadDefaultConfig(context.TODO(),
+        config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(*accessId, *accessKey, "")),
+        config.WithEndpointResolverWithOptions(aws.EndpointResolverWithOptionsFunc(
+            aws.EndpointResolverWithOptionsFunc(
+                func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+                    return aws.Endpoint{
+                        URL:               *endPoint,
+                        HostnameImmutable: true,
+                    }, nil
+                }))))
 
-func setEnv(id, accessValue string) {
-	err := os.Setenv(id, accessValue)
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
+    if err != nil {
+        log.Fatal("Error loading config ", err)
+    }
+
+    svc := createClient(cfg)
+    fmt.Println(*bucketName)
+
+    params := &s3.PutObjectInput{
+        Bucket: aws.String(*bucketName),
+        Key:    aws.String(*objectPath),
+        Body:   bytes.NewReader(fileBytes),
+    }
+
+    putObject(svc, params, objectPath, bucketName)
+
 }
 
 func createClient(config aws.Config) *s3.Client {
-	svc := s3.NewFromConfig(config)
-	return svc
-}
+    svc := s3.NewFromConfig(config)
 
-func putObject(svc *s3.Client, params *s3.PutObjectInput, ObjectName *string) {
-	_, err := svc.PutObject(context.TODO(), params)
-	if err != nil {
-		log.Printf("Error uploading object to s3: %v", err)
-	}
-	fmt.Printf("Successfully uploaded object %v to bucket", ObjectName)
-	return
+    return svc
+}
+func putObject(svc *s3.Client, params *s3.PutObjectInput, ObjectName, bucketName *string) {
+    _, err := svc.PutObject(context.TODO(), params)
+    if err != nil {
+        log.Fatalf("Error uploading object to s3: %v", err)
+    }
+    fmt.Printf("Successfully uploaded object '%v' to bucket '%v' \n", *ObjectName, *bucketName)
 }
--- examples/golang/go.mod
+++ examples/golang/go.mod
@@ -5,12 +5,12 @@ go 1.17
 require (
 	github.com/aws/aws-sdk-go-v2 v1.16.3
 	github.com/aws/aws-sdk-go-v2/config v1.15.4
+	github.com/aws/aws-sdk-go-v2/credentials v1.12.0
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.26.7
 )
 
 require (
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.1 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.12.0 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.4 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.10 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.4.4 // indirect
