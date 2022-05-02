34d226b63c386271556a3efaad791ca6218d6fce
Diwakar92 <diwakar.kumar@seagate.com>
2022-05-02 09:16:20 +0530

    Added Storage Stats as part of usage API to get capacity used by a user. (#179)
    Problem : Storage stats for a user was not there as part of usage API.
    Solution : Updated stats for a user and added those fields as part of usage API.
    Signed-off-by: Diwakar92 <diwakar.kumar@seagate.com>

--- src/rgw/rgw_usage.cc
+++ src/rgw/rgw_usage.cc
@@ -145,6 +145,41 @@ int RGWUsage::show(const DoutPrefixProvider *dpp, rgw::sal::Store* store,
 
     formatter->close_section(); // summary
   }
+  
+  // Added storage stats for a user as part of this API
+  // New section StorageStats added as part of usage
+  // Steps - 1. Update user stats for the user
+  //         2. Read those stats and add in StorageStats section
+  ret = rgw_user_sync_all_stats(dpp, store, user, null_yield);
+
+  if (ret < 0) {
+      return ret;
+  }
+
+  constexpr bool omit_utilized_stats = false;
+  RGWStorageStats stats(omit_utilized_stats);
+  ceph::real_time last_stats_sync;
+  ceph::real_time last_stats_update;
+
+  ret = user->read_stats(dpp, null_yield, &stats, &last_stats_sync, &last_stats_update);
+
+  if (ret < 0) {
+      return ret;
+  }
+
+  formatter->open_array_section("StorageStats");
+  encode_json("size", stats.size, formatter);
+  encode_json("size_actual", stats.size_rounded, formatter);
+  encode_json("size_kb", rgw_rounded_kb(stats.size), formatter);
+  encode_json("size_kb_actual", rgw_rounded_kb(stats.size_rounded), formatter);
+  encode_json("num_objects", stats.num_objects, formatter);
+  utime_t last_sync_ut(last_stats_sync);
+  encode_json("last_stats_sync", last_sync_ut, formatter);
+  utime_t last_update_ut(last_stats_update);
+  encode_json("last_stats_update", last_update_ut, formatter);
+
+  flusher.flush();
+  formatter->close_section(); // CapacityUsed
 
   formatter->close_section(); // usage
   flusher.flush();
