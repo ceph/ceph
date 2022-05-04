b2fa8d4f2bb873971eaaa32474f1b12f9a654cae
Casey Bodley <cbodley@redhat.com>
2022-05-04 16:19:01 -0400

    radosgw-admin: don't crash on --placement-id without --storage-class
    if --storage-class is omitted, avoid dereferencing an empty optional
    
    Fixes: https://tracker.ceph.com/issues/55548
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 91931a38ce280132e270c80822f3f3b2cc0a3bf9)
    
    Conflicts:
      - path: src/rgw/rgw_admin.cc
        comment: resolve minor conflict

--- src/rgw/rgw_admin.cc
+++ src/rgw/rgw_admin.cc
@@ -6159,11 +6159,10 @@ int main(int argc, const char **argv)
   else if (opt_cmd == OPT::USER_SUSPEND)
     user_op.set_suspension(true);
 
-  if (!placement_id.empty() ||
-      (opt_storage_class && !opt_storage_class->empty())) {
+  if (!placement_id.empty()) {
     rgw_placement_rule target_rule;
     target_rule.name = placement_id;
-    target_rule.storage_class = *opt_storage_class;
+    target_rule.storage_class = opt_storage_class.value_or("");
     if (!store->get_zone()->get_params().valid_placement(target_rule)) {
       cerr << "NOTICE: invalid dest placement: " << target_rule.to_str() << std::endl;
       return EINVAL;
