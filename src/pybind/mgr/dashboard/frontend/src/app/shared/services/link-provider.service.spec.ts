9241c11ea621341b5b6b14bddfcc11a7f6db08c8
Emmanuel Opatola <emmanuelopatola99@gmail.com>
2022-05-05 22:44:11 +0000

    Fix issue 47338 Created a centralized service for links/urls from source code for easy iteration and testing
    Signed-off-by: Emmanuel Opatola <emmanuelopatola99@gmail.com>

---
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/link-provider.service.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from '@angular/core/testing';
+
+import { LinkProviderService } from './link-provider.service';
+
+describe('LinkProviderService', () => {
+  let service: LinkProviderService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(LinkProviderService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it ('should test for correct links', () => {
+    expect(service.telemetrycomponent1()) .toBe (`https://telemetry-public.ceph.com/`);
+  });
+  it ('should test for correct links', () => {
+    expect(service.telemetrycomponent2()) .toBe (`https://cdla.io/sharing-1.0/`);
+  });
+  it ('should test for correct links', () => {
+    expect(service.docservice1()) .toBe ('https://docs.ceph.com/en/${docVersion}/');
+  });
+  it ('should test for correct links', () => {
+    expect(service.docservice2()) .toBe (`https://ceph.io/`);
+  });
+  it ('should test for correct links', () => {
+    expect(service.telemetrynotificationcomponent()) .toBe (`https://docs.ceph.com/en/latest/mgr/telemetry/`);
+  });
+  it ('should test for correct links', () => {
+    expect(service.iscsitargetformcomponent()) .toBe (`https://en.wikipedia.org/wiki/ISCSI#Addressing`);
+  });
+  
+});
---
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/link-provider.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LinkProviderService {
+
+  constructor() { }
+
+  telemetrycomponent1 () {
+     const url = `https://telemetry-public.ceph.com/`
+    return url;
+  };
+
+  telemetrycomponent2 () {
+   const url =  `https://cdla.io/sharing-1.0/`
+  return url;
+  };
+
+  docservice1 () {
+    const url =  "https://docs.ceph.com/en/${docVersion}/"
+    return url;
+  };
+
+  docservice2 () {
+    const url = `https://ceph.io/`
+    return url;
+  };
+
+  telemetrynotificationcomponent () {
+    const url =  `https://docs.ceph.com/en/latest/mgr/telemetry/`
+    return url;
+  };
+
+  iscsitargetformcomponent() {
+    const url = `https://en.wikipedia.org/wiki/ISCSI#Addressing`
+    return url;
+  };
+}
