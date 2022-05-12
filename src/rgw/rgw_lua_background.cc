5077d8aec9f24f15d416f80848fbd39d63f8703a
Yuval Lifshitz <ylifshit@redhat.com>
2022-05-13 01:55:44 +0300

    rgw: fix lua background crash on zone reload
    Signed-off-by: Yuval Lifshitz <ylifshit@redhat.com>

--- src/rgw/rgw_lua_background.cc
+++ src/rgw/rgw_lua_background.cc
@@ -5,16 +5,28 @@
 #include "include/ceph_assert.h"
 #include <lua.hpp>
 
+#define dout_subsys ceph_subsys_rgw
+
 namespace rgw::lua {
 
+Background::Background(rgw::sal::Store* store,
+    CephContext* cct,
+      const std::string& luarocks_path,
+      int execute_interval) :
+    execute_interval(execute_interval),
+    dp(cct, dout_subsys, "lua background: "),
+    store(store),
+    cct(cct),
+    luarocks_path(luarocks_path) {}
+
 void Background::shutdown(){
-  this->stop();
+  stopped = true;
+  cond.notify_all();
   if (runner.joinable()) {
     runner.join();
   }
-}
-void Background::stop(){
-  stopped = true;
+  started = false;
+  stopped = false;
 }
 
 void Background::start() {
@@ -29,12 +41,46 @@ void Background::start() {
   ceph_assert(rc == 0);
 }
 
-int Background::read_script() {
-  std::string tenant;
-  return rgw::lua::read_script(dpp, store, tenant, null_yield, rgw::lua::context::background, rgw_script);
+void Background::pause() {
+  {
+    std::unique_lock cond_lock(pause_mutex);
+    paused = true;
+  }
+  cond.notify_all();
 }
 
-//(1) Loads the script from the object
+void Background::resume(rgw::sal::Store* _store) {
+  store = _store;
+  paused = false;
+  cond.notify_all();
+}
+
+int Background::read_script() {
+  std::unique_lock cond_lock(pause_mutex);
+  if (paused) {
+    return -EAGAIN;
+  }
+  std::string tenant;
+  return rgw::lua::read_script(&dp, store, tenant, null_yield, rgw::lua::context::background, rgw_script);
+}
+
+const std::string Background::empty_table_value;
+
+const std::string& Background::get_table_value(const std::string& key) const {
+  std::unique_lock cond_lock(table_mutex);
+  const auto it = rgw_map.find(key);
+  if (it == rgw_map.end()) {
+    return empty_table_value;
+  }
+  return it->second;
+}
+
+void Background::put_table_value(const std::string& key, const std::string& value) {
+  std::unique_lock cond_lock(table_mutex);
+  rgw_map[key] = value;
+}
+
+//(1) Loads the script from the object if not paused
 //(2) Executes the script
 //(3) Sleep (configurable)
 void Background::run() {
@@ -44,11 +90,22 @@ void Background::run() {
   set_package_path(L, luarocks_path);
   create_debug_action(L, cct);
   create_background_metatable(L);
+  const DoutPrefixProvider* const dpp = &dp;
 
   while (!stopped) {
+    if (paused) {
+      ldpp_dout(dpp, 10) << "Lua background thread paused" << dendl;
+      std::unique_lock cond_lock(cond_mutex);
+      cond.wait(cond_lock, [this]{return !paused || stopped;}); 
+      if (stopped) {
+        ldpp_dout(dpp, 10) << "Lua background thread stopped" << dendl;
+        return;
+      }
+      ldpp_dout(dpp, 10) << "Lua background thread resumed" << dendl;
+    }
     const auto rc = read_script();
-    if (rc == -ENOENT) {
-      // no script, nothing to do
+    if (rc == -ENOENT || rc == -EAGAIN) {
+      // either no script or paused, nothing to do
     } else if (rc < 0) {
       ldpp_dout(dpp, 1) << "WARNING: failed to read background script. error " << rc << dendl;
     } else {
@@ -68,12 +125,14 @@ void Background::run() {
         perfcounter->inc((failed ? l_rgw_lua_script_fail : l_rgw_lua_script_ok), 1);
       }
     }
-    std::this_thread::sleep_for(std::chrono::seconds(execute_interval));
+    std::unique_lock cond_lock(cond_mutex);
+    cond.wait_for(cond_lock, std::chrono::seconds(execute_interval), [this]{return stopped;}); 
   }
+  ldpp_dout(dpp, 10) << "Lua background thread stopped" << dendl;
 }
 
 void Background::create_background_metatable(lua_State* L) {
-  create_metatable<rgw::lua::RGWTable>(L, true, &rgw_map, &m_mutex);
+  create_metatable<rgw::lua::RGWTable>(L, true, &rgw_map, &table_mutex);
 }
 
 } //namespace lua
