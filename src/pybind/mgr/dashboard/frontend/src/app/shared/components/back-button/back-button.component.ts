7b6f7f932ffff11493a6375e6c8cc3abb3c38032
Seremba Patrick <pserembae@gmail.com>
2022-05-12 15:36:03 +0300

    mgr/dashboard:Cancel button leads to the previous page in browser history
    This commit changes the behavior of the Cancel button from relying on browser history to rely on the
    router hierarchy.
    
    Fixes: https://tracker.ceph.com/issues/39121
    Signed-off-by: Seremba Patrick <pserembae@gmail.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/back-button/back-button.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/back-button/back-button.component.ts
@@ -1,4 +1,5 @@
-import { Location } from '@angular/common';
+
+import { Router } from '@angular/router';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 import { ActionLabelsI18n } from '~/app/shared/constants/app.constants';
@@ -11,12 +12,16 @@ import { ActionLabelsI18n } from '~/app/shared/constants/app.constants';
 export class BackButtonComponent {
   @Output() backAction = new EventEmitter();
   @Input() name: string = this.actionLabels.CANCEL;
+  
+  previousNavigation: string; 
 
-  constructor(private location: Location, private actionLabels: ActionLabelsI18n) {}
+  constructor(private router: Router, private actionLabels: ActionLabelsI18n) {
+    this.previousNavigation = this.router.getCurrentNavigation().previousNavigation.finalUrl.toString();
+  }
 
   back() {
-    if (this.backAction.observers.length === 0) {
-      this.location.back();
+    if (this.backAction.observers.length === 0) {   
+      this.router.navigateByUrl(this.previousNavigation);     
     } else {
       this.backAction.emit();
     }
