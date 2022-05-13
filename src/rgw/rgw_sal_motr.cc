6b615e1408e30dfae9bd4abbbf8eb8fc92bbf4a3
Selvakumar <selva.k.nambi@seagate.com>
2022-05-13 17:16:02 +0530

    CORTX-31196:rgw:PutBucketTagging: Update inline comments
    Signed-off-by: Selvakumar <selva.k.nambi@seagate.com>

--- src/rgw/rgw_sal_motr.cc
+++ src/rgw/rgw_sal_motr.cc
@@ -900,8 +900,8 @@ int MotrBucket::merge_and_store_attrs(const DoutPrefixProvider *dpp, Attrs& new_
   for (auto& it : new_attrs)
     attrs[it.first] = it.second;
 
-  // "put_info" is meant to update existing metadata, by
-  // explicitly passing false. Passing "yield" was incorrect
+  // "put_info" second bool argument is meant to update existing metadata,
+  // which is not needed here. So explicitly passing false.
   return put_info(dpp, false, ceph::real_time());
 }
 
