10174460fa3756a787243c1400d20f188e18d454
Adam Kupczyk <akupczyk@redhat.com>
2022-05-19 13:20:12 +0200

    crimson/os/alienstore: Limit list_object reserve
    Do not reserve() if limit is large. This prevents problem when caller wants MAXINT.
    
    Signed-off-by: Adam Kupczyk <akupczyk@redhat.com>

--- src/crimson/os/alienstore/alien_store.cc
+++ src/crimson/os/alienstore/alien_store.cc
@@ -194,7 +194,10 @@ AlienStore::list_objects(CollectionRef ch,
   assert(tp);
   return do_with_op_gate(std::vector<ghobject_t>(), ghobject_t(),
                          [=] (auto &objects, auto &next) {
-    objects.reserve(limit);
+    if (limit <= 100) {
+      //reserve if limit is reasonably small
+      objects.reserve(limit);
+    }
     return tp->submit(ch->get_cid().hash_to_shard(tp->size()),
       [=, &objects, &next] {
       auto c = static_cast<AlienCollection*>(ch.get());
