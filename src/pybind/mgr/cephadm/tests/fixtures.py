scantist-ossops-m2/ceph-ci

mgr/cephadm: Add client.admin keyring when upgrading from older version
Signed-off-by: Sebastian Wagner <sewagner@redhat.com>

src/pybind/mgr/cephadm/tests/fixtures.py
@@ -124,15 +124,14 @@ def wait(m, c):
 
 
 @contextmanager
-def with_host(m: CephadmOrchestrator, name, addr='1::4', refresh_hosts=True):
-    # type: (CephadmOrchestrator, str) -> None
+def with_host(m: CephadmOrchestrator, name, addr='1::4', refresh_hosts=True, force=True):
     with mock.patch("cephadm.utils.resolve_ip", return_value=addr):
         wait(m, m.add_host(HostSpec(hostname=name)))
         if refresh_hosts:
             CephadmServe(m)._refresh_hosts_and_daemons()
             receive_agent_metadata(m, name)
         yield
-        wait(m, m.remove_host(name))
+        wait(m, m.remove_host(name, force=force))
 
 
 def assert_rm_service(cephadm: CephadmOrchestrator, srv_name):

src/pybind/mgr/cephadm/tests/test_cephadm.py
@@ -1241,6 +1241,8 @@ class TestCephadm(object):
                             assert len(cephadm_module.cache.get_daemons_by_type('mgr')) == 3
                             assert len(cephadm_module.cache.get_daemons_by_type('crash')) == 1
 
+                        cephadm_module.offline_hosts = {}
+
     @mock.patch("cephadm.serve.CephadmServe._run_cephadm")
     @mock.patch("cephadm.CephadmOrchestrator._host_ok_to_stop")
     @mock.patch("cephadm.module.HostCache.get_daemon_types")
@@ -1610,3 +1612,12 @@ Traceback (most recent call last):
                 mock.call('test', 'osd', 'ceph-volume',
                           ['--', 'raw', 'list', '--format', 'json'], no_fsid=False, image=''),
             ]
+
+    @mock.patch("cephadm.serve.CephadmServe._run_cephadm", _run_cephadm('[]'))
+    def test_host_rm_last_admin(self, cephadm_module: CephadmOrchestrator):
+        with pytest.raises(OrchestratorError):
+            with with_host(cephadm_module, 'test', refresh_hosts=False, force=False):
+                pass
+            assert False
+        with with_host(cephadm_module, 'test', refresh_hosts=False, force=False):
+            cephadm_module.inventory.add_label('test', '_admin')

ceph/ceph

(cherry picked from commit 02c942a093a28376301b9b4c66d9c712345ff953)
Conflicts:
	src/pybind/mgr/cephadm/tests/test_migration.py

src/pybind/mgr/cephadm/tests/fixtures.py
@@ -77,14 +77,13 @@ def wait(m, c):
 
 
 @contextmanager
-def with_host(m: CephadmOrchestrator, name, addr='1::4', refresh_hosts=True):
-    # type: (CephadmOrchestrator, str) -> None
+def with_host(m: CephadmOrchestrator, name, addr='1::4', refresh_hosts=True, rm_with_force=True):
     with mock.patch("cephadm.utils.resolve_ip", return_value=addr):
         wait(m, m.add_host(HostSpec(hostname=name)))
         if refresh_hosts:
             CephadmServe(m)._refresh_hosts_and_daemons()
         yield
-        wait(m, m.remove_host(name))
+        wait(m, m.remove_host(name, force=rm_with_force))
 
 
 def assert_rm_service(cephadm: CephadmOrchestrator, srv_name):

src/pybind/mgr/cephadm/tests/test_cephadm.py
@@ -1372,6 +1372,8 @@ class TestCephadm(object):
                             assert len(cephadm_module.cache.get_daemons_by_type('mgr')) == 3
                             assert len(cephadm_module.cache.get_daemons_by_type('crash')) == 1
 
+                        cephadm_module.offline_hosts = {}
+
     @mock.patch("cephadm.serve.CephadmServe._run_cephadm")
     @mock.patch("cephadm.CephadmOrchestrator._host_ok_to_stop")
     @mock.patch("cephadm.module.HostCache.get_daemon_types")
@@ -1716,3 +1718,14 @@ Traceback (most recent call last):
 
             with with_osd_daemon(cephadm_module, _run_cephadm, 'test', 1, ceph_volume_lvm_list=_ceph_volume_list):
                 pass
+
+    @mock.patch("cephadm.serve.CephadmServe._run_cephadm", _run_cephadm('[]'))
+    def test_host_rm_last_admin(self, cephadm_module: CephadmOrchestrator):
+        with pytest.raises(OrchestratorError):
+            with with_host(cephadm_module, 'test', refresh_hosts=False, rm_with_force=False):
+                cephadm_module.inventory.add_label('test', '_admin')
+                pass
+            assert False
+        with with_host(cephadm_module, 'test1', refresh_hosts=False, rm_with_force=True):
+            with with_host(cephadm_module, 'test2', refresh_hosts=False, rm_with_force=False):
+                cephadm_module.inventory.add_label('test2', '_admin')