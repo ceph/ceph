6b67b68858a1e2d97058edc2a097e9ae074d8f34
dparmar18 <dparmar@redhat.com>
2022-05-23 14:45:40 +0530

    qa/cephfs: fallback to older way of get_op_read_count
    Fixes: https://tracker.ceph.com/issues/55538
    
    Signed-off-by: Dhairya Parmar <dparmar@redhat.com>
    (cherry picked from commit 0aca27d2fcb17e4e5b5ef339b2d1d011d52dbd6c)

--- qa/tasks/cephfs/kernel_mount.py
+++ qa/tasks/cephfs/kernel_mount.py
@@ -1,3 +1,4 @@
+import errno
 import json
 import logging
 import os
@@ -297,8 +298,23 @@ class KernelMount(CephFSMount):
         return epoch, barrier
 
     def get_op_read_count(self):
-        buf = self.read_debug_file("metrics")
-        if buf is None:
-            return 0
-        else:
-            return int(re.findall(r'read.*', buf)[0].split()[1])
+        stdout = StringIO()
+        stderr = StringIO()
+        try:
+            path = os.path.join(self._get_debug_dir(), "metrics/size")
+            self.run_shell(f"sudo stat {path}", stdout=stdout,
+                           stderr=stderr, cwd=None)
+            buf = self.read_debug_file("metrics/size")
+        except CommandFailedError:
+            if 'no such file or directory' in stderr.getvalue().lower() \
+                    or 'not a directory' in stderr.getvalue().lower():
+                try:
+                    path = os.path.join(self._get_debug_dir(), "metrics")
+                    self.run_shell(f"sudo stat {path}", stdout=stdout,
+                                   stderr=stderr, cwd=None)
+                    buf = self.read_debug_file("metrics")
+                except CommandFailedError:
+                    return errno.ENOENT
+            else:
+                return 0
+        return int(re.findall(r'read.*', buf)[0].split()[1])
