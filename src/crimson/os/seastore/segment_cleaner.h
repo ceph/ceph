9acb49d379db76e64fd5eb05037bab9702df5e4e
Yingxin Cheng <yingxin.cheng@intel.com>
2022-05-30 13:27:30 +0800

    crimson/os/seastore/segment_cleaner: delay reclaim until near full
    It should be generically better to delay reclaim as much as possible, so
    that:
    * unalive/unavailable can higher to reduce reclaim efforts;
    * less conflicts between mutate and reclaim transactions;
    
    Signed-off-by: Yingxin Cheng <yingxin.cheng@intel.com>

--- src/crimson/os/seastore/segment_cleaner.h
+++ src/crimson/os/seastore/segment_cleaner.h
@@ -516,10 +516,10 @@ public:
 	  12,   // target_journal_segments
 	  16,   // max_journal_segments
 	  2,	// target_backref_inflight_segments
-	  .9,   // available_ratio_gc_max
-	  .2,   // available_ratio_hard_limit
-	  .8,   // reclaim_ratio_hard_limit
-	  .6,   // reclaim_ratio_gc_threshold
+	  .1,   // available_ratio_gc_max
+	  .05,  // available_ratio_hard_limit
+	  .9,   // reclaim_ratio_hard_limit
+	  .1,   // reclaim_ratio_gc_threshold
 	  1<<20,// reclaim_bytes_per_cycle
 	  1<<17,// rewrite_dirty_bytes_per_cycle
 	  1<<24 // rewrite_backref_bytes_per_cycle
