fab6f37052a527ec710d8d6ea48eaadd05aa72d7
Nizamudeen A <nia@redhat.com>
2022-06-06 11:21:29 +0530

    mgr/dashboard: configure rbd mirroring
    One-click button in the case of an orch cluster for configuring the
    rbd-mirroring when its not properly setup. This button will create an
    rbd-mirror service and also an rbd labelled pool(replicated: size-3) (if they are not
    existing)
    
    Fixes: https://tracker.ceph.com/issues/55646
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/core/error/error.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/error/error.component.ts
@@ -1,10 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, Router, RouterEvent } from '@angular/router';
 
 import { Subscription } from 'rxjs';
 import { filter } from 'rxjs/operators';
 
+import { NotificationType } from '~/app/shared/enum/notification-type.enum';
 import { DocService } from '~/app/shared/services/doc.service';
+import { NotificationService } from '~/app/shared/services/notification.service';
 
 @Component({
   selector: 'cd-error',
@@ -16,14 +19,18 @@ export class ErrorComponent implements OnDestroy, OnInit {
   message: string;
   section: string;
   section_info: string;
-  button_name: string;
-  button_route: string;
   icon: string;
   docUrl: string;
   source: string;
   routerSubscription: Subscription;
+  bootstrap: string;
+  uiApiPath: string;
+  button_route: string;
+  button_name: string;
 
-  constructor(private router: Router, private docService: DocService) {}
+  constructor(private router: Router, private docService: DocService,
+    private http: HttpClient,
+    private notificationService: NotificationService, ) {}
 
   ngOnInit() {
     this.fetchData();
@@ -32,6 +39,32 @@ export class ErrorComponent implements OnDestroy, OnInit {
       .subscribe(() => {
         this.fetchData();
       });
+  }
+
+  doBootstrap() {
+    this.http.post(`ui-api/${this.uiApiPath}/configure`, {}).subscribe({
+      next: () => {
+        this.notificationService.show(
+          NotificationType.info,
+          'Configuring RBD Mirroring'
+        );
+      },
+      error: (error: any) => {
+        this.notificationService.show(
+          NotificationType.error,
+          error
+        );
+      },
+      complete: () => {
+        setTimeout(() => {
+          this.router.navigate([this.uiApiPath]);
+          this.notificationService.show(
+            NotificationType.success,
+            'Configured RBD Mirroring'
+          );
+        }, 3000);
+      }
+    });
   }
 
   @HostListener('window:beforeunload', ['$event']) unloadHandler(event: Event) {
@@ -45,10 +78,12 @@ export class ErrorComponent implements OnDestroy, OnInit {
       this.header = history.state.header;
       this.section = history.state.section;
       this.section_info = history.state.section_info;
-      this.button_name = history.state.button_name;
-      this.button_route = history.state.button_route;
       this.icon = history.state.icon;
       this.source = history.state.source;
+      this.bootstrap = history.state.bootstrap;
+      this.uiApiPath = history.state.uiApiPath;
+      this.button_route = history.state.button_route;
+      this.button_name = history.state.button_name;
       this.docUrl = this.docService.urlGenerator(this.section);
     } catch (error) {
       this.router.navigate(['/error']);
