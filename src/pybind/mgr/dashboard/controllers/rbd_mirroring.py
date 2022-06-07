453446104b8fe86b3f561b99a7b5a5838ee89478
Pere Diaz Bou <pdiazbou@redhat.com>
2022-06-07 19:57:44 +0200

    mgr/dashboard: add byte info, move state, add idle state
    Idle substate added from snapshot mode.
    Instead of seconds info we display bytes and entries info.
    
    Signed-off-by: Pere Diaz Bou <pdiazbou@redhat.com>

--- src/pybind/mgr/dashboard/controllers/rbd_mirroring.py
+++ src/pybind/mgr/dashboard/controllers/rbd_mirroring.py
@@ -4,7 +4,7 @@ import json
 import logging
 import re
 from functools import partial
-from typing import no_type_check
+from typing import NamedTuple, Optional, no_type_check
 
 import cherrypy
 import rbd
@@ -199,6 +199,13 @@ def get_daemons_and_pools():  # pylint: disable=R0915
     }
 
 
+class ReplayingData(NamedTuple):
+    bytes_per_second: Optional[int] = None
+    seconds_until_synced: Optional[int] = None
+    syncing_percent: Optional[float] = None
+    entries_behind_primary: Optional[int] = None
+
+
 @ViewCache()
 @no_type_check
 def _get_pool_datum(pool_name):
@@ -250,8 +257,9 @@ def _get_pool_datum(pool_name):
         rbd.MIRROR_IMAGE_STATUS_STATE_STOPPED: {
             'health': 'ok',
             'state_color': 'info',
-            'state': 'Primary'
+            'state': 'Stopped'
         }
+
     }
 
     rbdctx = rbd.RBD()
@@ -271,6 +279,29 @@ def _get_pool_datum(pool_name):
         raise
 
     return data
+
+
+def _update_syncing_image_data(mirror_image, image):
+    if mirror_image['state'] == 'Replaying':
+        p = re.compile("replaying, ({.*})")
+        replaying_data = p.findall(mirror_image['description'])
+        assert len(replaying_data) == 1
+        replaying_data = json.loads(replaying_data[0])
+        if 'replay_state' in replaying_data and replaying_data['replay_state'] == 'idle':
+            image.update({
+                'state_color': 'info',
+                'state': 'Idle'
+            })
+        for field in ReplayingData._fields:
+            try:
+                image[field] = replaying_data[field]
+            except KeyError:
+                pass
+    else:
+        p = re.compile("bootstrapping, IMAGE_COPY/COPY_OBJECT (.*)%")
+        image.update({
+            'progress': (p.findall(mirror_image['description']) or [0])[0]
+        })
 
 
 @ViewCache()
@@ -309,22 +340,7 @@ def _get_content_data():  # pylint: disable=R0914
                 })
                 image_ready.append(image)
             elif mirror_image['health'] == 'syncing':
-                if mirror_image['state'] == 'Replaying':
-                    p = re.compile("replaying, ({.*})")
-                    replaying_data = p.findall(mirror_image['description'])
-                    assert len(replaying_data) == 1
-                    replaying_data = json.loads(replaying_data[0])
-                    seconds_until_synced = 0
-                    if 'seconds_until_synced' in replaying_data:
-                        seconds_until_synced = replaying_data['seconds_until_synced']
-                    image.update({
-                        'seconds_until_synced': seconds_until_synced
-                    })
-                else:
-                    p = re.compile("bootstrapping, IMAGE_COPY/COPY_OBJECT (.*)%")
-                    image.update({
-                        'progress': (p.findall(mirror_image['description']) or [0])[0]
-                    })
+                _update_syncing_image_data(mirror_image, image)
                 image_syncing.append(image)
             else:
                 image.update({
