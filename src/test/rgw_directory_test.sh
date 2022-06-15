8b62997d257d939db517188ad61861d31e6c63dd
samarah <suriarte@redhat.com>
2022-06-15 13:56:58 -0400

    Time out added for getValue issue
    Signed-off-by: samarah <suriarte@redhat.com>

--- src/test/rgw_directory_test.sh
+++ src/test/rgw_directory_test.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
-#PORT_NUM=$(./port_scanner.sh 127.0.0.1 40000 65535 | grep open | grep [0-9]* -o | cut -d ' ' -f1)
-PORT_NUM=50104
+#PORT_NUM=$(./port_scanner.sh 127.0.0.1 40000 65535 | grep closed | awk '{ print $1 }' | head -1)
+PORT_NUM=6379
 echo "-----------Port $PORT_NUM Discovered-----------"
 redis-server --port $PORT_NUM --daemonize yes
 echo "-----------Redis Server Started-----------"
-../../build/bin/ceph_test_rgw_directory $PORT_NUM
-printf "\n-----------Directory Test Executed; Showing Directory Values-----------\n"
-echo "keys *" | redis-cli -p $PORT_NUM
-redis-cli -h 127.0.0.1 -p ${PORT_NUM[0]} FLUSHALL
-echo "-----------Redis Server Flushed-----------"
-REDIS_PID=$(lsof -i4TCP:$PORT_NUM -sTCP:LISTEN -t)
-kill $REDIS_PID
-echo "-----------Redis Server Stopped-----------"
+
+timeout 2s ../../build/bin/ceph_test_rgw_directory $PORT_NUM
+
+if (($? == 124)); then
+  echo "getValue timed out; restarting script"
+  
+  redis-cli -h 127.0.0.1 -p $PORT_NUM FLUSHALL
+  REDIS_PID=$(lsof -i4TCP:$PORT_NUM -sTCP:LISTEN -t)
+  kill $REDIS_PID
+  source ./rgw_directory_test.sh
+else
+  printf "\n-----------Directory Test Executed; Showing Directory Values-----------\n"
+  echo "keys *" | redis-cli -p $PORT_NUM
+  redis-cli -h 127.0.0.1 -p $PORT_NUM FLUSHALL
+  echo "-----------Redis Server Flushed-----------"
+  REDIS_PID=$(lsof -i4TCP:$PORT_NUM -sTCP:LISTEN -t)
+  kill $REDIS_PID
+  echo "-----------Redis Server Stopped-----------"
+fi
