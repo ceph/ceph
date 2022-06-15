2a1d17ec549f515660af23928852e87a40fb71ac
Soumya Koduri <skoduri@redhat.com>
2022-06-15 17:50:09 +0530

    rgw/dbstore: Atomically update version number for versioned objects
    VersionNum is used to track the latest version available of the versioned
    object. In order to handle racing updates/overwrites, have used sqlite
    trigger which atomically updates the version number for every object insert.
    
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/rgw/rgw_sal_dbstore.cc
+++ src/rgw/rgw_sal_dbstore.cc
@@ -1148,6 +1148,7 @@ namespace rgw::sal {
 
     obj_op.meta.owner = owner.get_id();
     obj_op.meta.flags = PUT_OBJ_CREATE;
+    obj_op.meta.category = RGWObjCategory::Main;
     obj_op.meta.modify_tail = true;
     obj_op.meta.completeMultipart = true;
 
@@ -1525,6 +1526,7 @@ namespace rgw::sal {
     parent_op.meta.if_nomatch = if_nomatch;
     parent_op.meta.user_data = user_data;
     parent_op.meta.zones_trace = zones_trace;
+    parent_op.meta.category = RGWObjCategory::Main;
     
     /* XXX: handle accounted size */
     accounted_size = total_data_size;
