e74d26e348fd2424f2e1697408597f4287b60bfd
samarah <suriarte@redhat.com>
2022-06-17 16:18:28 -0400

    Added sleep command to the end of the script
    Signed-off-by: samarah <suriarte@redhat.com>

--- src/test/rgw_directory_test.sh
+++ src/test/rgw_directory_test.sh
@@ -1,26 +1,11 @@
 #!/bin/bash
-PORT_NUM=$(./port_scanner.sh 127.0.0.1 40000 65535 | grep closed | awk '{ print $1 }' | head -1)
-#PORT_NUM = 6379
-echo "-----------Port $PORT_NUM Discovered-----------"
-redis-server --port $PORT_NUM --daemonize yes
+redis-server --daemonize yes
 echo "-----------Redis Server Started-----------"
-
-#timeout 2s ../../build/bin/ceph_test_rgw_directory $PORT_NUM
-../../build/bin/ceph_test_rgw_directory $PORT_NUM
-
-#if (($? == 124)); then
-#  echo "getValue timed out; restarting script"
-  
-#  redis-cli -h 127.0.0.1 -p $PORT_NUM FLUSHALL
-#  REDIS_PID=$(lsof -i4TCP:$PORT_NUM -sTCP:LISTEN -t)
-#  kill $REDIS_PID
-#  ./rgw_directory_test.sh
-#else
-  printf "\n-----------Directory Test Executed; Showing Directory Values-----------\n"
-  echo "keys *" | redis-cli -p $PORT_NUM
-  redis-cli -h 127.0.0.1 -p $PORT_NUM FLUSHALL
-  echo "-----------Redis Server Flushed-----------"
-  REDIS_PID=$(lsof -i4TCP:$PORT_NUM -sTCP:LISTEN -t)
-  kill $REDIS_PID
-  echo "-----------Redis Server Stopped-----------"
-#fi
+timeout 5s ../../build/bin/ceph_test_rgw_directory || echo "Failed from timeout"
+printf "\n-----------Directory Test Executed-----------\n"
+redis-cli FLUSHALL
+echo "-----------Redis Server Flushed-----------"
+REDIS_PID=$(lsof -i4TCP:6379 -sTCP:LISTEN -t)
+kill $REDIS_PID
+sleep 0.5
+echo "-----------Redis Server Stopped-----------"
