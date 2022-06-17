6629098148ea220b9feda01ce5e77799d697a37a
samarah <suriarte@redhat.com>
2022-06-17 16:14:50 -0400

    Slimmed down code
    Signed-off-by: samarah <suriarte@redhat.com>

--- src/rgw/rgw_directory.h
+++ src/rgw/rgw_directory.h
@@ -1,4 +1,3 @@
-
 #ifndef CEPH_RGWDIRECTORY_H
 #define CEPH_RGWDIRECTORY_H
 
@@ -15,89 +14,36 @@
 
 using namespace std;
 
-class RGWDirectory{
-	public:
-	RGWDirectory() {}
-	virtual ~RGWDirectory(){ cout << "RGW Directory is destroyed!";}
-	CephContext *cct;
-
-private:
-
-};
-
-class RGWObjectDirectory: public RGWDirectory {
-public:
-
-	RGWObjectDirectory() {}
-	//cpp_redis::client *client;
-	//cpp_redis::client client1;
-	//cpp_redis::client client2;
-	//cpp_redis::client client3;
-	void init(CephContext *_cct) {
-      		cct = _cct;
-		//client1.connect(cct->_conf->rgw_directory_address1, cct->_conf->rgw_directory_port1);
-		//client2.connect(cct->_conf->rgw_directory_address2, cct->_conf->rgw_directory_port2);
-		//client3.connect(cct->_conf->rgw_directory_address3, cct->_conf->rgw_directory_port3);
-    	
-    	}
-
-	virtual ~RGWObjectDirectory() { cout << "RGWObject Directory is destroyed!";}
-
-	//void findClient(string key, cpp_redis::client *client);
-	//int delKey(string key);
-	//int existKey(string key, cpp_redis::client *client);
-	//int setValue(cache_obj *ptr);
-	//int getValue(cache_obj *ptr);
-	//int updateField(cache_obj *ptr, string field, string value);
-	//int delValue(cache_obj *ptr);
-	//int setTTL(cache_obj *ptr, int seconds);
-	//vector<pair<vector<string>, time_t>> get_aged_keys(time_t startTime_t, time_t endTime_t);
-
-private:
-	//string buildIndex(cache_obj *ptr);
-	
-
+class RGWDirectory {
+  public:
+    RGWDirectory() {}
+    virtual ~RGWDirectory() { cout << "RGW Directory is destroyed!"; }
+    CephContext *cct;
 };
 
 class RGWBlockDirectory: RGWDirectory {
-public:
-
-	RGWBlockDirectory() {}
-	void init(CephContext *_cct) {
-		cct = _cct;
-	}
-	virtual ~RGWBlockDirectory() { 
-		cout << "RGWObject Directory is destroyed!";
-		client.disconnect(true);
-	}
-
-	void findClient(string key, cpp_redis::client *client, int port);
-	//int delKey(string key);
-	int existKey(string key, cpp_redis::client *client);
-	int setValue(cache_block *ptr, int port);
-	int setValue(cache_block *ptr);
-	int getValue(cache_block *ptr, int port);
-	int getValue(cache_block *ptr);
-	//int getValue(cache_block *ptr, string key);
-	//int updateField(cache_block *ptr, string field, string value);
-	//int updateField(string key, string field, string value);
-	//int updateGlobalWeight(string key,  size_t weight , bool evict);
-	//int resetGlobalWeight(string key);
-	//int delValue(cache_block *ptr);
-	//int delValue(string key);
-	//int updateAccessCount(string key, int incr);
-	//int setTTL(cache_block *ptr, int seconds);
-	//int getHosts(string key, string hosts);
-  	/* //////// Unused for directory ///////
-	int setAvgCacheWeight(int64_t avg_weight);
-	int getAvgCacheWeight(string endpoint);
-  	///////////////////////////////////// */
-private:
-	string buildIndex(cache_block *ptr);
-	cpp_redis::client client;
+  public:
+    RGWBlockDirectory() {}
+    
+    void init(CephContext *_cct) {
+      cct = _cct;
+    }
+	
+    virtual ~RGWBlockDirectory() { 
+      cout << "RGWObject Directory is destroyed!";
+      client.disconnect(true);
+    }
+    
+    void findClient(string key, cpp_redis::client *client, int port);
+    int existKey(string key, cpp_redis::client *client);
+    int setValue(cache_block *ptr, int port);
+    int setValue(cache_block *ptr);
+    int getValue(cache_block *ptr, int port);
+    int getValue(cache_block *ptr);
+  
+  private:
+    string buildIndex(cache_block *ptr);
+    cpp_redis::client client;
 };
-
-
-
 
 #endif
