d6d8f634c6e795039eddb251961b6c18e7c13643
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-06-22 22:33:23 -0400

    spelling: meaningful
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -11121,7 +11121,7 @@ bool OSDMonitor::prepare_command_impl(MonOpRequestRef op,
 
     if (osdmap.crush->rule_exists(name)) {
       // The name is uniquely associated to a ruleid and the rule it contains
-      // From the user point of view, the rule is more meaningfull.
+      // From the user point of view, the rule is more meaningful.
       ss << "rule " << name << " already exists";
       err = 0;
       goto reply_no_propose;
@@ -11131,7 +11131,7 @@ bool OSDMonitor::prepare_command_impl(MonOpRequestRef op,
 
     if (newcrush.rule_exists(name)) {
       // The name is uniquely associated to a ruleid and the rule it contains
-      // From the user point of view, the rule is more meaningfull.
+      // From the user point of view, the rule is more meaningful.
       ss << "rule " << name << " already exists";
       err = 0;
     } else {
@@ -11159,7 +11159,7 @@ bool OSDMonitor::prepare_command_impl(MonOpRequestRef op,
 
     if (osdmap.crush->rule_exists(name)) {
       // The name is uniquely associated to a ruleid and the rule it contains
-      // From the user point of view, the rule is more meaningfull.
+      // From the user point of view, the rule is more meaningful.
       ss << "rule " << name << " already exists";
       err = 0;
       goto reply_no_propose;
@@ -11169,7 +11169,7 @@ bool OSDMonitor::prepare_command_impl(MonOpRequestRef op,
 
     if (newcrush.rule_exists(name)) {
       // The name is uniquely associated to a ruleid and the rule it contains
-      // From the user point of view, the rule is more meaningfull.
+      // From the user point of view, the rule is more meaningful.
       ss << "rule " << name << " already exists";
       err = 0;
     } else {
