94975df594d6a72874ecc6aa622acc9a1ddbba19
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-06-22 23:14:48 -0400

    spelling: session
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -2423,7 +2423,7 @@ bool PG::can_discard_replica_op(OpRequestRef& op)
   // before the pg advances to this new osdmap, the repop replies before this
   // repop can be discarded by that replica OSD, because the primary resets the
   // connection to it when handling the new osdmap marking it down, and also
-  // resets the messenger sesssion when the replica reconnects. to avoid the
+  // resets the messenger session when the replica reconnects. to avoid the
   // out-of-order replies, the messages from that replica should be discarded.
   OSDMapRef next_map = osd->get_next_osdmap();
   if (next_map->is_down(from)) {
