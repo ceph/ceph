b6ed813cc5d481016a7d917a2759d6175c76d577
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-06-22 23:23:18 -0400

    spelling: unexpected
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/os/bluestore/HybridAllocator.cc
+++ src/os/bluestore/HybridAllocator.cc
@@ -156,7 +156,7 @@ void HybridAllocator::init_rm_free(uint64_t offset, uint64_t length)
           bmap_alloc->init_rm_free(o, l);
         } else {
           lderr(cct) << "init_rm_free lambda " << std::hex
-            << "Uexpected extent: "
+            << "Unexpected extent: "
             << " 0x" << o << "~" << l
             << std::dec << dendl;
           ceph_assert(false);
