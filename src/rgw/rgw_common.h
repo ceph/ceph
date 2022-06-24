c7c004dbe9f2d3280e12649b8be1ad5f43c8ab0a
Samarah Uriarte <suriarte@redhat.com>
2022-06-24 12:48:14 -0400

    Removed unnecessary comment and reinitialized submodules"
    Signed-off-by: Samarah Uriarte <suriarte@redhat.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -2428,4 +2428,3 @@ rgw_global_init(const std::map<std::string,std::string> *defaults,
 		    std::vector < const char* >& args,
 		    uint32_t module_type, code_environment_t code_env,
 		    int flags);
-/* datacache */
