a8aefb4b13e0c751b356f78d92d77196ae3a9296
Adam King <adking@redhat.com>
2022-06-27 14:26:37 -0400

    src/python_common: Add CustomConfig class
    Signed-off-by: Adam King <adking@redhat.com>
    (cherry picked from commit 61a6ec0399ac33843d39c9b4bd9ca23ad72e4154)
    
    Conflicts:
    	src/python-common/ceph/deployment/drive_group.py

--- src/python-common/ceph/deployment/drive_group.py
+++ src/python-common/ceph/deployment/drive_group.py
@@ -2,7 +2,7 @@ import enum
 import yaml
 
 from ceph.deployment.inventory import Device
-from ceph.deployment.service_spec import ServiceSpec, PlacementSpec
+from ceph.deployment.service_spec import ServiceSpec, PlacementSpec, CustomConfig
 from ceph.deployment.hostspec import SpecValidationError
 
 try:
@@ -178,13 +178,15 @@ class DriveGroupSpec(ServiceSpec):
                  data_allocate_fraction=None,  # type: Optional[float]
                  method=None,  # type: Optional[OSDMethod]
                  crush_device_class=None,  # type: Optional[str]
+                 custom_configs=None,  # type: Optional[List[CustomConfig]]
                  ):
         assert service_type is None or service_type == 'osd'
         super(DriveGroupSpec, self).__init__('osd', service_id=service_id,
                                              placement=placement,
                                              unmanaged=unmanaged,
                                              preview_only=preview_only,
-                                             extra_container_args=extra_container_args)
+                                             extra_container_args=extra_container_args,
+                                             custom_configs=custom_configs)
 
         #: A :class:`ceph.deployment.drive_group.DeviceSelection`
         self.data_devices = data_devices
