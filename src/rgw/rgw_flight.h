eac2905d9f64e174570ee25129112ed7ed53993c
J. Eric Ivancich <ivancich@redhat.com>
2022-06-28 15:35:18 -0400

    rgw: implement initial flight server functionality
    Implements the ability for a flight to be created when the object is
    retrieved by an S3 get.
    
    Adds FlightServer abilities ListFlights, GetFlightInfo, GetSchema, and
    DoGet. Adds an interface for a store for flight information and adds
    an in-memory implemtation of it.
    
    This code is functionality is early-stage and lacks some planned
    efficiencies.
    
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>

--- src/rgw/rgw_flight.h
+++ src/rgw/rgw_flight.h
@@ -14,88 +14,200 @@
 #include "rgw_frontend.h"
 #include "arrow/type.h"
 #include "arrow/flight/server.h"
+#include "arrow/util/string_view.h"
 
 #include "rgw_flight_frontend.h"
 
+
+#define INFO_F(dp)   ldpp_dout(&dp, 20) << "INFO: " << __func__ << ": "
+#define STATUS_F(dp) ldpp_dout(&dp, 10) << "STATUS: " << __func__ << ": "
+#define WARN_F(dp)   ldpp_dout(&dp,  0) << "WARNING: " << __func__ << ": "
+#define ERROR_F(dp)  ldpp_dout(&dp,  0) << "ERROR: " << __func__ << ": "
+
+#define INFO   INFO_F(dp)
+#define STATUS STATUS_F(dp)
+#define WARN   WARN_F(dp)
+#define ERROR  ERROR_F(dp)
+
+
 namespace arw = arrow;
 namespace flt = arrow::flight;
+
 
 struct req_state;
 
 namespace rgw::flight {
 
-  static const coarse_real_clock::duration lifespan = std::chrono::hours(1);
+static const coarse_real_clock::duration lifespan = std::chrono::hours(1);
 
-  struct FlightData {
-    FlightKey key;
-    coarse_real_clock::time_point expires;
+struct FlightData {
+  FlightKey key;
+  // coarse_real_clock::time_point expires;
+  std::string uri;
+  std::string tenant_name;
+  std::string bucket_name;
+  rgw_obj_key object_key;
+  // NB: what about object's namespace and instance?
+  uint64_t num_records;
+  uint64_t obj_size;
+  std::shared_ptr<arw::Schema> schema;
+  std::shared_ptr<const arw::KeyValueMetadata> kv_metadata;
 
-    rgw::sal::Bucket* bucket;
-#if 0
-    rgw::sal::Object object;
-    rgw::sal::User user;
-#endif
+  rgw_user user_id; // TODO: this should be removed when we do
+  // proper flight authentication
 
+  FlightData(const std::string& _uri,
+	     const std::string& _tenant_name,
+	     const std::string& _bucket_name,
+	     const rgw_obj_key& _object_key,
+	     uint64_t _num_records,
+	     uint64_t _obj_size,
+	     std::shared_ptr<arw::Schema>& _schema,
+	     std::shared_ptr<const arw::KeyValueMetadata>& _kv_metadata,
+	     rgw_user _user_id);
+};
 
-    FlightData(const req_state* state);
-  };
+// stores flights that have been created and helps expire them
+class FlightStore {
 
-  // stores flights that have been created and helps expire them
-  class FlightStore {
-  public:
-    virtual ~FlightStore();
-    virtual FlightKey add_flight(FlightData&& flight) = 0;
-    virtual int get_flight(const FlightKey& key) = 0;
-    virtual int remove_flight(const FlightKey& key) = 0;
-    virtual int expire_flights() = 0;
-  };
+protected:
 
-  class MemoryFlightStore : public FlightStore {
-    std::map<FlightKey, FlightData> map;
+  const DoutPrefix& dp;
 
-  public:
+public:
 
-    virtual ~MemoryFlightStore();
-    FlightKey add_flight(FlightData&& flight) override;
-    int get_flight(const FlightKey& key) override;
-    int remove_flight(const FlightKey& key) override;
-    int expire_flights() override;
-  };
+  FlightStore(const DoutPrefix& dp);
+  virtual ~FlightStore();
+  virtual FlightKey add_flight(FlightData&& flight) = 0;
 
-  class FlightServer : public flt::FlightServerBase {
+  // TODO consider returning const shared pointers to FlightData in
+  // the following two functions
+  virtual arw::Result<FlightData> get_flight(const FlightKey& key) const = 0;
+  virtual std::optional<FlightData> after_key(const FlightKey& key) const = 0;
 
-    using Data1 = std::vector<std::shared_ptr<arw::RecordBatch>>;
+  virtual int remove_flight(const FlightKey& key) = 0;
+  virtual int expire_flights() = 0;
+};
 
-    boost::intrusive_ptr<ceph::common::CephContext> cct;
-    const DoutPrefix dp;
-    rgw::sal::Store* store;
-    FlightStore* flight_store;
+class MemoryFlightStore : public FlightStore {
+  std::map<FlightKey, FlightData> map;
+  mutable std::mutex mtx; // for map
 
-    std::map<std::string, Data1> data;
+public:
 
-  public:
+  MemoryFlightStore(const DoutPrefix& dp);
+  virtual ~MemoryFlightStore();
+  FlightKey add_flight(FlightData&& flight) override;
+  arw::Result<FlightData> get_flight(const FlightKey& key) const override;
+  std::optional<FlightData> after_key(const FlightKey& key) const override;
+  int remove_flight(const FlightKey& key) override;
+  int expire_flights() override;
+};
 
-    static constexpr int default_port = 8077;
+class FlightServer : public flt::FlightServerBase {
 
-    FlightServer(boost::intrusive_ptr<ceph::common::CephContext>& _cct,
-		 rgw::sal::Store* _store,
-		 FlightStore* _flight_store);
-    ~FlightServer() override;
+  using Data1 = std::vector<std::shared_ptr<arw::RecordBatch>>;
 
-    FlightStore* get_flight_store() {
-      return flight_store;
+  RGWProcessEnv& env;
+  rgw::sal::Driver* driver;
+  const DoutPrefix& dp;
+  FlightStore* flight_store;
+
+  std::map<std::string, Data1> data;
+
+public:
+
+  static constexpr int default_port = 8077;
+
+  FlightServer(RGWProcessEnv& env,
+	       FlightStore* flight_store,
+	       const DoutPrefix& dp);
+  ~FlightServer() override;
+
+  FlightStore* get_flight_store() {
+    return flight_store;
+  }
+
+  arw::Status ListFlights(const flt::ServerCallContext& context,
+			  const flt::Criteria* criteria,
+			  std::unique_ptr<flt::FlightListing>* listings) override;
+
+  arw::Status GetFlightInfo(const flt::ServerCallContext &context,
+			    const flt::FlightDescriptor &request,
+			    std::unique_ptr<flt::FlightInfo> *info) override;
+
+  arw::Status GetSchema(const flt::ServerCallContext &context,
+			const flt::FlightDescriptor &request,
+			std::unique_ptr<flt::SchemaResult> *schema) override;
+
+  arw::Status DoGet(const flt::ServerCallContext &context,
+		    const flt::Ticket &request,
+		    std::unique_ptr<flt::FlightDataStream> *stream) override;
+}; // class FlightServer
+
+class OwningStringView : public arw::util::string_view {
+
+  uint8_t* buffer;
+  int64_t capacity;
+  int64_t consumed;
+
+  OwningStringView(uint8_t* _buffer, int64_t _size) :
+    arw::util::string_view((const char*) _buffer, _size),
+    buffer(_buffer),
+    capacity(_size),
+    consumed(_size)
+    { }
+
+  OwningStringView(OwningStringView&& from, int64_t new_size) :
+    buffer(nullptr),
+    capacity(from.capacity),
+    consumed(new_size)
+    {
+      // should be impossible due to static function check
+      ceph_assertf(consumed <= capacity, "new size cannot exceed capacity");
+
+      std::swap(buffer, from.buffer);
+      from.capacity = 0;
+      from.consumed = 0;
     }
 
-    arw::Status ListFlights(const flt::ServerCallContext& context,
-			    const flt::Criteria* criteria,
-			    std::unique_ptr<flt::FlightListing>* listings) override;
-  }; // class FlightServer
+public:
 
-  // GLOBAL
+  OwningStringView(OwningStringView&&) = default;
+  OwningStringView& operator=(OwningStringView&&) = default;
 
-  void set_flight_server(FlightServer* _server);
-  FlightServer* get_flight_server();
-  FlightStore* get_flight_store();
-  FlightKey propose_flight(const req_state* request);
+  uint8_t* writeable_data() {
+    return buffer;
+  }
+
+  ~OwningStringView() {
+    if (buffer) {
+      delete[] buffer;
+    }
+  }
+
+  static arw::Result<OwningStringView> make(int64_t size) {
+    uint8_t* buffer = new uint8_t[size];
+    if (!buffer) {
+      return arw::Status::OutOfMemory("could not allocated buffer of size %" PRId64, size);
+    }
+    return OwningStringView(buffer, size);
+  }
+
+  static arw::Result<OwningStringView> shrink(OwningStringView&& from,
+					      int64_t new_size) {
+    if (new_size > from.capacity) {
+      return arw::Status::Invalid("new size cannot exceed capacity");
+    } else {
+      return OwningStringView(std::move(from), new_size);
+    }
+  }
+
+};
+
+// GLOBAL
+
+flt::Ticket FlightKeyToTicket(const FlightKey& key);
+arw::Status TicketToFlightKey(const flt::Ticket& t, FlightKey& key);
 
 } // namespace rgw::flight
--- src/rgw/rgw_flight_frontend.h
+++ src/rgw/rgw_flight_frontend.h
@@ -8,62 +8,71 @@
 #include "rgw_frontend.h"
 #include "rgw_op.h"
 
+#include "arrow/status.h"
+
 
 namespace rgw::flight {
 
-  using FlightKey = uint32_t;
+using FlightKey = uint32_t;
+extern const FlightKey null_flight_key;
 
-  class FlightServer;
+class FlightServer;
 
-  class FlightFrontend : public RGWFrontend {
+class FlightFrontend : public RGWFrontend {
 
-    static constexpr std::string_view server_thread_name =
-      "Arrow Flight Server thread";
+  static constexpr std::string_view server_thread_name =
+    "Arrow Flight Server thread";
 
-    boost::intrusive_ptr<ceph::common::CephContext>& cct;
-    const DoutPrefix dp;
-    FlightServer* flight_server; // pointer so header file doesn't need to pull in too much
-    std::thread flight_thread;
-    RGWFrontendConfig* config;
-    int port;
+  RGWProcessEnv& env;
+  std::thread flight_thread;
+  RGWFrontendConfig* config;
+  int port;
 
-  public:
+  const DoutPrefix dp;
 
-    // port <= 0 -> let server decide; typically 8077
-    FlightFrontend(boost::intrusive_ptr<ceph::common::CephContext>& cct,
-		   RGWFrontendConfig* config,
-		   rgw::sal::Store* store,
-		   int port = -1);
-    ~FlightFrontend() override;
-    int init() override;
-    int run() override;
-    void stop() override;
-    void join() override;
+public:
 
-    void pause_for_new_config() override;
-    void unpause_with_new_config(rgw::sal::Store* store,
-				 rgw_auth_registry_ptr_t auth_registry) override;
+  // port <= 0 means let server decide; typically 8077
+  FlightFrontend(RGWProcessEnv& env,
+		 RGWFrontendConfig* config,
+		 int port = -1);
+  ~FlightFrontend() override;
+  int init() override;
+  int run() override;
+  void stop() override;
+  void join() override;
 
-  }; // class FlightFrontend
+  void pause_for_new_config() override;
+  void unpause_with_new_config() override;
+}; // class FlightFrontend
 
-  class FlightGetObj_Filter : public RGWGetObj_Filter {
+class FlightGetObj_Filter : public RGWGetObj_Filter {
 
-    FlightKey key;
+  const RGWProcessEnv& penv;
+  const DoutPrefix dp;
+  FlightKey key;
+  uint64_t current_offset;
+  uint64_t expected_size;
+  std::string uri;
+  std::string tenant_name;
+  std::string bucket_name;
+  rgw_obj_key object_key;
+  std::string temp_file_name;
+  std::ofstream temp_file;
+  arrow::Status schema_status;
+  rgw_user user_id; // TODO: this should be removed when we do
+  // proper flight authentication
 
-  public:
+public:
 
-    FlightGetObj_Filter(const FlightKey& _key, RGWGetObj_Filter* next) :
-      RGWGetObj_Filter(next),
-      key(_key)
-    {
-      // empty
-    }
+  FlightGetObj_Filter(const req_state* request, RGWGetObj_Filter* next);
+  ~FlightGetObj_Filter();
 
-    int handle_data(bufferlist& bl, off_t bl_ofs, off_t bl_len) override;
+  int handle_data(bufferlist& bl, off_t bl_ofs, off_t bl_len) override;
 #if 0
-    // this would allow the range to be modified if necessary;
-    int fixup_range(off_t& ofs, off_t& end) override;
+  // this would allow the range to be modified if necessary;
+  int fixup_range(off_t& ofs, off_t& end) override;
 #endif
-  };
+};
 
 } // namespace rgw::flight
