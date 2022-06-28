a1f8351997e547761ec4b91e4a850cfdd1508995
myoungwon oh <ohmyoungwon@gmail.com>
2022-06-28 13:42:21 +0900

    osd: return ENOENT if pool information is invalid during tier-flush
    During tier-flush, OSD sends reference increase message to target OSD.
    At this point, sending message with invalid pool information (e.g., deleted pool)
    causes unexpected behavior.
    
    Therefore, this commit return ENOENT early before sending the message
    
    fixes: https://tracker.ceph.com/issues/53294
    
    Signed-off-by: Myoungwon Oh <myoungwon.oh@samsung.com>
    (cherry picked from commit 3de27b25cb6104d810f7af81809a0813df6c46a4)

--- src/osd/PrimaryLogPG.h
+++ src/osd/PrimaryLogPG.h
@@ -1458,7 +1458,7 @@ protected:
   int do_cdc(const object_info_t& oi, std::map<uint64_t, chunk_info_t>& chunk_map,
 	     std::map<uint64_t, bufferlist>& chunks);
   int start_dedup(OpRequestRef op, ObjectContextRef obc);
-  hobject_t get_fpoid_from_chunk(const hobject_t soid, bufferlist& chunk);
+  std::pair<int, hobject_t> get_fpoid_from_chunk(const hobject_t soid, bufferlist& chunk);
   int finish_set_dedup(hobject_t oid, int r, ceph_tid_t tid, uint64_t offset);
   int finish_set_manifest_refcount(hobject_t oid, int r, ceph_tid_t tid, uint64_t offset);
 
