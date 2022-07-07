fe4ddb324cd63b8573f5a6db515393bfade31488
bryanmontalvan <68972382+bryanmontalvan@users.noreply.github.com>
2022-07-07 12:36:15 -0400

    Start of landing-page dashboard revamp v3
    This commit add the following
    - A new dashboard component, which will exist in parallel with in the
      current landing-page
    - Created a route for this dashboard `/dashboard_3`
    - Created a bare-bones bootstrap grid with mock-up card components
    
    Signed-off-by: bryanmontalvan <bmontalv@redhat.com>
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@redhat.com>

---
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/new-dashboard/dashboard/dashboard.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cd-new-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class NewDashboardComponent {}
