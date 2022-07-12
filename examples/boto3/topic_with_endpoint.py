0d43ec8c52016e3ce6657ea8af55090829cc81a5
dux <faithuniterh@tutanota.com>
2022-07-12 13:29:37 +0000

    examples/boto3: Updating Python examples to work with Python 3
    Signed-off-by: dux <faithuniterh@tutanota.com>

--- examples/boto3/topic_with_endpoint.py
+++ examples/boto3/topic_with_endpoint.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python
-
 import boto3
-import sys
-import urlparse
 from botocore.client import Config
+import sys
 
-if len(sys.argv) == 3:
+if len(sys.argv) == 2:
     # topic name as first argument
     topic_name = sys.argv[1]
-    # region name as second argument
-    region_name = sys.argv[2]
-elif len(sys.argv) == 2:
-    # topic name as first argument
-    topic_name = sys.argv[1]
-    region_name = ""
 else:
-    print('Usage: ' + sys.argv[0] + ' <topic name> [region name]')
+    print('Usage: ' + sys.argv[0] + ' <topic name> ')
     sys.exit(1)
 
 # endpoint and keys from vstart
@@ -26,7 +18,6 @@ secret_key='h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q=='
 client = boto3.client('sns',
         endpoint_url=endpoint,
         aws_access_key_id=access_key,
-        region_name=region_name,
         aws_secret_access_key=secret_key,
         config=Config(signature_version='s3'))
 
@@ -34,8 +25,6 @@ client = boto3.client('sns',
 # radosgw-admin realm zonegroup list
 
 # this is standard AWS services call, using custom attributes to add AMQP endpoint information to the topic
-
-endpoint_args = 'push-endpoint=amqp://127.0.0.1:5672&amqp-exchange=ex1&amqp-ack-level=broker'
-attributes = {nvp[0] : nvp[1] for nvp in urlparse.parse_qsl(endpoint_args, keep_blank_values=True)}
+attributes = {"push-endpoint": "amqp://localhost:5672", "amqp-exchange": "ex1", "amqp-ack-level": "broker"}
 
 print(client.create_topic(Name=topic_name, Attributes=attributes))
