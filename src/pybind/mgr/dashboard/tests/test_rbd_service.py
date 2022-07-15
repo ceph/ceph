bd56f0187fc9335b5106b595fc3438b113a317fd
Pere Diaz Bou <pdiazbou@redhat.com>
2022-07-15 16:30:15 +0200

    mgr/dashboard: fix _get_rbd_image_refs caching
    We were cahing _get_rbd_image_refs by Ioctx but the namespace to which
    a ioctx is set is also relevant information regarding the references
    of a pool.
    
    Signed-off-by: Pere Diaz Bou <pdiazbou@redhat.com>
    (cherry picked from commit 950e66b1207f50e3bc91382e07540181d5059511)

--- src/pybind/mgr/dashboard/tests/test_rbd_service.py
+++ src/pybind/mgr/dashboard/tests/test_rbd_service.py
@@ -23,6 +23,11 @@ class ImageNotFoundStub(Exception):
 
 class RbdServiceTest(unittest.TestCase):
 
+    def setUp(self):
+        # pylint: disable=protected-access
+        RbdService._rbd_inst = mock.Mock()
+        self.rbd_inst_mock = RbdService._rbd_inst
+
     def test_compose_image_spec(self):
         self.assertEqual(get_image_spec('mypool', 'myns', 'myimage'), 'mypool/myns/myimage')
         self.assertEqual(get_image_spec('mypool', None, 'myimage'), 'mypool/myimage')
@@ -54,11 +59,9 @@ class RbdServiceTest(unittest.TestCase):
         config = RbdConfiguration('good-pool')
         self.assertEqual(config.list(), [1, 2, 3])
 
-    @mock.patch('dashboard.services.rbd.rbd.RBD')
-    def test_rbd_image_stat_removing(self, rbd_mock):
+    def test_rbd_image_stat_removing(self):
         time = datetime.utcnow()
-        rbd_inst_mock = rbd_mock.return_value
-        rbd_inst_mock.trash_get.return_value = {
+        self.rbd_inst_mock.trash_get.return_value = {
             'id': '3c1a5ee60a88',
             'name': 'test_rbd',
             'source': 'REMOVING',
@@ -82,10 +85,8 @@ class RbdServiceTest(unittest.TestCase):
         })
 
     @mock.patch('dashboard.services.rbd.rbd.ImageNotFound', new_callable=lambda: ImageNotFoundStub)
-    @mock.patch('dashboard.services.rbd.rbd.RBD')
-    def test_rbd_image_stat_filter_source_user(self, rbd_mock, _):
-        rbd_inst_mock = rbd_mock.return_value
-        rbd_inst_mock.trash_get.return_value = {
+    def test_rbd_image_stat_filter_source_user(self, _):
+        self.rbd_inst_mock.trash_get.return_value = {
             'id': '3c1a5ee60a88',
             'name': 'test_rbd',
             'source': 'USER'
@@ -99,21 +100,21 @@ class RbdServiceTest(unittest.TestCase):
                       str(ctx.exception))
 
     @mock.patch('dashboard.services.rbd.rbd.ImageNotFound', new_callable=lambda: ImageNotFoundStub)
+    @mock.patch('dashboard.services.rbd.RbdService._pool_namespaces')
     @mock.patch('dashboard.services.rbd.RbdService._rbd_image_stat_removing')
     @mock.patch('dashboard.services.rbd.RbdService._rbd_image_stat')
     @mock.patch('dashboard.services.rbd.RbdService._rbd_image_refs')
-    @mock.patch('dashboard.services.rbd.rbd.RBD')
-    def test_rbd_pool_list(self, rbd_mock, rbd_image_ref_mock, rbd_image_stat_mock,
-                           rbd_image_stat_removing_mock, _):
+    def test_rbd_pool_list(self, rbd_image_ref_mock, rbd_image_stat_mock,
+                           rbd_image_stat_removing_mock, pool_namespaces, _):
         time = datetime.utcnow()
 
         ioctx_mock = MagicMock()
         mgr.rados = MagicMock()
         mgr.rados.open_ioctx.return_value = ioctx_mock
 
-        rbd_inst_mock = rbd_mock.return_value
-        rbd_inst_mock.namespace_list.return_value = []
+        self.rbd_inst_mock.namespace_list.return_value = []
         rbd_image_ref_mock.return_value = [{'name': 'test_rbd', 'id': '3c1a5ee60a88'}]
+        pool_namespaces.return_value = ['']
 
         rbd_image_stat_mock.side_effect = mock.Mock(side_effect=ImageNotFoundStub(
             'RBD image not found test_pool/3c1a5ee60a88'))
@@ -158,3 +159,15 @@ class RbdServiceTest(unittest.TestCase):
                     RBDSchedulerInterval(interval)
             else:
                 self.assertEqual(str(RBDSchedulerInterval(interval)), interval)
+
+    def test_rbd_image_refs_cache(self):
+        ioctx_mock = MagicMock()
+        mgr.rados = MagicMock()
+        mgr.rados.open_ioctx.return_value = ioctx_mock
+        images = [{'image': str(i), 'id': str(i)} for i in range(10)]
+        for i in range(5):
+            self.rbd_inst_mock.list2.return_value = images[i*2:(i*2)+2]
+            ioctx_mock = MagicMock()
+            # pylint: disable=protected-access
+            res = RbdService._rbd_image_refs(ioctx_mock, str(i))
+            self.assertEqual(res, images[i*2:(i*2)+2])
