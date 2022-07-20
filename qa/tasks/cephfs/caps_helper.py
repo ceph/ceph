VallariAg/ceph-ci

qa/cephfs: allow checking for multiple error messages while...
while negative testing.

And update negtestcmd() to pass process's return value and error message
instead of passing the object representing the process.

Signed-off-by: Rishabh Dave <ridave@redhat.com>

qa/tasks/cephfs/caps_helper.py
@@ -60,6 +60,7 @@ class CapsHelper(CephFSTestCase):
                     self.assertEqual(data, contents1)
 
     def conduct_neg_test_for_write_caps(self, filepaths, mounts):
+        possible_errmsgs = ('permission denied', 'operation not permitted')
         cmdargs = ['echo', 'some random data', Raw('|'), 'tee']
 
         for mount in mounts:
@@ -67,7 +68,7 @@ class CapsHelper(CephFSTestCase):
                 if path.find(mount.hostfs_mntpt) != -1:
                     cmdargs.append(path)
                     mount.negtestcmd(args=cmdargs, retval=1,
-                                     errmsg='permission denied')
+                                     errmsg=possible_errmsgs)
                     cmdargs.pop(len(cmdargs)-1)
 
     def get_mon_cap_from_keyring(self, client_name):

ceph/ceph

qa/tasks/cephfs/caps_helper.py
@@ -59,6 +59,7 @@ class CapsHelper(CephFSTestCase):
                     self.assertEqual(data, contents1)
 
     def conduct_neg_test_for_write_caps(self, filepaths, mounts):
+        possible_errmsgs = ('permission denied', 'operation not permitted')
         cmdargs = ['echo', 'some random data', Raw('|'), 'tee']
 
         for mount in mounts:
@@ -66,7 +67,7 @@ class CapsHelper(CephFSTestCase):
                 if path.find(mount.hostfs_mntpt) != -1:
                     cmdargs.append(path)
                     mount.negtestcmd(args=cmdargs, retval=1,
-                                     errmsg='permission denied')
+                                     errmsgs=possible_errmsgs)
                     cmdargs.pop(-1)
 
     def get_mon_cap_from_keyring(self, client_name):