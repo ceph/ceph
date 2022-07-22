8aadfa2ba1fef9daae784fbf0ae7ffa8713a5e8e
Pedro Gonzalez Gomez <pegonzal@redhat.com>
2022-07-22 07:37:11 +0200

    mgr/dashboard: changes to first layout
    CHANGES:
    
     - Renamed dashboardcomponents
    
     - Removed unnecesary styling
    
     - Added unit tests
    
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/new-dashboard/dashboard.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/new-dashboard/dashboard.module.ts
@@ -9,7 +9,10 @@ import { ChartsModule } from 'ng2-charts';
 import { SharedModule } from '~/app/shared/shared.module';
 import { CephSharedModule } from '../shared/ceph-shared.module';
 import { CardComponent } from './card/card.component';
+<<<<<<< HEAD
 import { DashboardPieComponent } from './dashboard-pie/dashboard-pie.component';
+=======
+>>>>>>> 59d734f8b53 (mgr/dashboard: changes to first layout)
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
@@ -25,6 +28,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     ReactiveFormsModule
   ],
 
+<<<<<<< HEAD
   declarations: [DashboardComponent, CardComponent, DashboardPieComponent]
+=======
+  declarations: [DashboardComponent, CardComponent]
+>>>>>>> 59d734f8b53 (mgr/dashboard: changes to first layout)
 })
 export class NewDashboardModule {}
