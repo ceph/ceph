2572e1c03ce3dc0f6f1463f1ef8a35192c04856a
Pedro Gonzalez Gomez <pegonzal@redhat.com>
2022-07-22 07:37:11 +0200

    mgr/dashboard: changes to first layout
    CHANGES:
    
     - Renamed dashboardcomponents
    
     - Removed unnecesary styling
    
     - Added unit tests
    
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/dashboard/dashboard.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/dashboard/dashboard.component.ts
@@ -5,6 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DeprecatedDashboardComponent {
   hasGrafana = false; // TODO: Temporary var, remove when grafana is implemented
 }
