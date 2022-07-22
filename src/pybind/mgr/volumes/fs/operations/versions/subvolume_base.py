373a04cf734d0ed7f4f2451d99826a9517cbe651
Kotresh HR <khiremat@redhat.com>
2022-07-22 13:23:39 +0530

    mgr/volumes: Fix subvolume discover during upgrade
    Fixes the subvolume discover to use the correct
    metadata file after an upgrade from legacy subvolume
    to v1. The fix makes sure, it doesn't use the
    handcrafted metadata file placed in the subvolume
    root of legacy subvolume.
    
    Co-authored-by: Arthur Outhenin-Chalandre <arthur.outhenin-chalandre@cern.ch>
    Co-authored-by: Dan van der Ster <daniel.vanderster@cern.ch>
    Co-authored-by: Ramana Raja <rraja@redhat.com>
    Signed-off-by: Kotresh HR <khiremat@redhat.com>

--- src/pybind/mgr/volumes/fs/operations/versions/subvolume_base.py
+++ src/pybind/mgr/volumes/fs/operations/versions/subvolume_base.py
@@ -5,6 +5,7 @@ import errno
 import logging
 from hashlib import md5
 from typing import Dict, Union
+from pathlib import Path
 
 import cephfs
 
@@ -128,6 +129,15 @@ class SubvolumeBase(object):
         raise NotImplementedError
 
     def load_config(self):
+        try:
+            self.fs.stat(self.legacy_config_path)
+            self.legacy_mode = True
+        except cephfs.Error as e:
+            pass
+
+        log.debug("loading config "
+                  "'{0}' [mode: {1}]".format(self.subvolname, "legacy"
+                                             if self.legacy_mode else "new"))
         if self.legacy_mode:
             self.metadata_mgr = MetadataManager(self.fs,
                                                 self.legacy_config_path,
@@ -318,8 +328,13 @@ class SubvolumeBase(object):
             self.fs.stat(self.base_path)
             self.metadata_mgr.refresh()
             log.debug("loaded subvolume '{0}'".format(self.subvolname))
+            subvolpath = self.metadata_mgr.get_global_option(MetadataManager.GLOBAL_META_KEY_PATH)
+            if not self.legacy_mode and self.base_path.decode('utf-8') != str(Path(subvolpath).parent):
+                raise MetadataMgrException(-errno.ENOENT, 'fabricated .meta')
         except MetadataMgrException as me:
-            if me.errno == -errno.ENOENT and not self.legacy_mode:
+            if me.errno in (-errno.ENOENT, -errno.EINVAL) and not self.legacy_mode:
+                log.warn("subvolume '{0}', {1}, "
+                          "assuming legacy_mode".format(self.subvolname, me.error_str))
                 self.legacy_mode = True
                 self.load_config()
                 self.discover()
