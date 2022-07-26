11224e30c73ab42679727acead35e29a6f87719d
Radoslaw Zarzynski <rzarzyns@redhat.com>
2022-07-26 12:14:05 +0000

    monc: improve auth-related debugs in MonClient
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/mon/MonClient.cc
+++ src/mon/MonClient.cc
@@ -1025,6 +1025,7 @@ void MonClient::handle_subscribe_ack(MMonSubscribeAck *m)
 
 int MonClient::_check_auth_tickets()
 {
+  ldout(cct, 10) << __func__ << dendl;
   ceph_assert(ceph_mutex_is_locked(monc_lock));
   if (active_con && auth) {
     if (auth->need_tickets()) {
@@ -1830,6 +1831,7 @@ int MonConnection::_negotiate(MAuthReply *m,
 			      uint32_t want_keys,
 			      RotatingKeyRing* keyring)
 {
+  ldout(cct, 10) << __func__ << dendl;
   int r = _init_auth(m->protocol, entity_name, want_keys, keyring, false);
   if (r == -ENOTSUP) {
     if (m->result == -ENOTSUP) {
