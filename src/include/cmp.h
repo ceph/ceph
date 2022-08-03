06c641b8c6aed33aa2b455bc14f30ec8c1dc8cba
Kefu Chai <tchaikov@gmail.com>
2022-08-03 11:07:17 +0800

    common: drop WRITE_{EQ,CMP}_OPERATORS_5()
    the default-generated comparison operators can fulfill our needs.
    
    Signed-off-by: Kefu Chai <tchaikov@gmail.com>

--- src/include/cmp.h
+++ src/include/cmp.h
@@ -4,44 +4,6 @@
 /*
  * macros to define comparison operators for classes with small numbers of members.
  */
-#define WRITE_EQ_OPERATORS_5(type, a, b, c, d, e)			\
-  inline bool operator==(const type &l, const type &r) {		\
-    return l.a == r.a && l.b == r.b && l.c == r.c && l.d == r.d && l.e == r.e; \
-  }									\
-  inline bool operator!=(const type &l, const type &r) {		\
-    return l.a != r.a || l.b != r.b || l.c != r.c || l.d != r.d || l.e != r.e; \
-  }
-
-#define WRITE_CMP_OPERATORS_5(type, a, b, c, d, e)			\
-  inline bool operator>(const type &l, const type &r) {			\
-    return l.a > r.a ||							\
-      (l.a == r.a && (l.b > r.b ||					\
-		      (l.b == r.b && (l.c > r.c ||			\
-				      (l.c == r.c && (l.d > r.d ||	\
-						      (l.d == r.d && l.e > r.e))))))); \
-  }									\
-  inline bool operator<(const type &l, const type &r) {			\
-    return l.a < r.a ||							\
-      (l.a == r.a && (l.b < r.b ||					\
-		      (l.b == r.b && (l.c < r.c ||			\
-				      (l.c == r.c && (l.d < r.d ||	\
-						      (l.d == r.d && (l.e < r.e)))))))); \
-  }									\
-  inline bool operator>=(const type &l, const type &r) {		\
-    return l.a > r.a ||							\
-      (l.a == r.a && (l.b > r.b ||					\
-		      (l.b == r.b && (l.c > r.c ||			\
-				      (l.c == r.c && (l.d > r.d ||	\
-						      (l.d == r.d && l.e >= r.e))))))); \
-  }									\
-  inline bool operator<=(const type &l, const type &r) {		\
-    return l.a < r.a ||							\
-      (l.a == r.a && (l.b < r.b ||					\
-		      (l.b == r.b && (l.c < r.c ||			\
-				      (l.c == r.c && (l.d < r.d ||	\
-						      (l.d == r.d && l.e <= r.e))))))); \
-  }
-
 #define WRITE_EQ_OPERATORS_7(type, a, b, c, d, e, f, g)			\
   inline bool operator==(const type &l, const type &r) {		\
     return l.a == r.a && l.b == r.b && l.c == r.c && l.d == r.d && l.e == r.e && l.f == r.f && l.g == r.g; \
