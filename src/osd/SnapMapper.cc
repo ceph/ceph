e611cd5995102e3b0cf7f74e6f8fc6d2143c4263
Ronen Friedman <rfriedma@redhat.com>
2022-08-04 16:24:53 +0000

    osd: faster versions of SnapMapper::get_legacy_prefix() & make_purged_snap_key()
    Using fmtlib, and treating the size of uint64_t as a known
    quantity.
    
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- src/osd/SnapMapper.cc
+++ src/osd/SnapMapper.cc
@@ -419,10 +419,10 @@ int SnapMapper::get_snaps(
 
 string SnapMapper::make_purged_snap_key(int64_t pool, snapid_t last)
 {
-  char k[80];
-  snprintf(k, sizeof(k), "%s_%llu_%016llx", PURGED_SNAP_PREFIX,
-	   (unsigned long long)pool, (unsigned long long)last);
-  return k;
+  return fmt::sprintf("%s_%lld_%016llx",
+		      PURGED_SNAP_PREFIX,
+		      pool,
+		      last);
 }
 
 void SnapMapper::make_purged_snap_key_value(
@@ -639,12 +639,9 @@ void SnapMapper::Scrubber::run()
 
 string SnapMapper::get_legacy_prefix(snapid_t snap)
 {
-  char buf[100];
-  int len = snprintf(
-    buf, sizeof(buf),
-    "%.*X_",
-    (int)(sizeof(snap)*2), static_cast<unsigned>(snap));
-  return LEGACY_MAPPING_PREFIX + string(buf, len);
+  return fmt::sprintf("%s%.16X_",
+		      LEGACY_MAPPING_PREFIX,
+		      snap);
 }
 
 string SnapMapper::to_legacy_raw_key(
