ceph/ceph

commit 308e64a72af09f3ff531c045a593743190682525

cmake: set WITH_JAEGER=ON by default
Signed-off-by: Omri Zeneva <ozeneva@redhat.com>

src/rgw/rgw_process.cc
@@ -401,7 +401,7 @@ int process_request(rgw::sal::Store* const store,
   }
 
 done:
-  if (op) {
+  if (op && s->trace) {
     s->trace->SetAttribute(tracing::rgw::RETURN, op->get_ret());
     if (s->user) {
       s->trace->SetAttribute(tracing::rgw::USER_ID, s->user->get_id().id);

CMakeLists.txt
@@ -408,7 +408,7 @@ if(WITH_BLKIN)
   include_directories(SYSTEM src/blkin/blkin-lib)
 endif(WITH_BLKIN)
 
-option(WITH_JAEGER "Enable jaegertracing and it's dependent libraries" OFF)
+option(WITH_JAEGER "Enable jaegertracing and it's dependent libraries" ON)
 if(WITH_JAEGER)
   set(HAVE_JAEGER TRUE)
 endif()

commit ad5116494e158af3bdce1c78347b069e651a93e4

rgw/lua: post request context does not work
regression introduced by: 308e64a72af09f3ff531c045a593743190682525

Signed-off-by: Yuval Lifshitz <ylifshit@redhat.com>

src/rgw/rgw_process.cc
@@ -401,16 +401,18 @@ int process_request(rgw::sal::Store* const store,
   }
 
 done:
-  if (op && s->trace) {
-    s->trace->SetAttribute(tracing::rgw::RETURN, op->get_ret());
-    if (s->user) {
-      s->trace->SetAttribute(tracing::rgw::USER_ID, s->user->get_id().id);
-    }
-    if (s->bucket) {
-      s->trace->SetAttribute(tracing::rgw::BUCKET_NAME, s->bucket->get_name());
-    }
-    if (s->object) {
-      s->trace->SetAttribute(tracing::rgw::OBJECT_NAME, s->object->get_name());
+  if (op) {
+    if (s->trace) {
+      s->trace->SetAttribute(tracing::rgw::RETURN, op->get_ret());
+      if (s->user) {
+        s->trace->SetAttribute(tracing::rgw::USER_ID, s->user->get_id().id);
+      }
+      if (s->bucket) {
+        s->trace->SetAttribute(tracing::rgw::BUCKET_NAME, s->bucket->get_name());
+      }
+      if (s->object) {
+        s->trace->SetAttribute(tracing::rgw::OBJECT_NAME, s->object->get_name());
+      }
     }
     std::string script;
     auto rc = rgw::lua::read_script(s, store, s->bucket_tenant, s->yield, rgw::lua::context::postRequest, script);