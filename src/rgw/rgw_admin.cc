ekmixon/ceph-ci

rgw/lua: don't create the script manager per request
Fixes: https://tracker.ceph.com/issues/53810

Signed-off-by: yuval Lifshitz <ylifshit@redhat.com>

src/rgw/rgw_admin.cc
@@ -10353,7 +10353,8 @@ next:
       cerr << "ERROR: cannot specify tenant in background context" << std::endl;
       return EINVAL;
     }
-    rc = rgw::lua::write_script(dpp(), store, tenant, null_yield, script_ctx, script);
+    auto lua_script_manager = store->get_lua_script_manager();
+    rc = rgw::lua::write_script(dpp(), lua_script_manager.get(), tenant, null_yield, script_ctx, script);
     if (rc < 0) {
       cerr << "ERROR: failed to put script. error: " << rc << std::endl;
       return -rc;
@@ -10370,8 +10371,9 @@ next:
       cerr << "ERROR: invalid script context: " << *str_script_ctx << ". must be one of: preRequest, postRequest, background" << std::endl;
       return EINVAL;
     }
+    auto lua_script_manager = store->get_lua_script_manager();
     std::string script;
-    const auto rc = rgw::lua::read_script(dpp(), store, tenant, null_yield, script_ctx, script);
+    const auto rc = rgw::lua::read_script(dpp(), lua_script_manager.get(), tenant, null_yield, script_ctx, script);
     if (rc == -ENOENT) {
       std::cout << "no script exists for context: " << *str_script_ctx << 
         (tenant.empty() ? "" : (" in tenant: " + tenant)) << std::endl;
@@ -10393,7 +10395,8 @@ next:
       cerr << "ERROR: invalid script context: " << *str_script_ctx << ". must be one of: preRequest, postRequest, background" << std::endl;
       return EINVAL;
     }
-    const auto rc = rgw::lua::delete_script(dpp(), store, tenant, null_yield, script_ctx);
+    auto lua_script_manager = store->get_lua_script_manager();
+    const auto rc = rgw::lua::delete_script(dpp(), lua_script_manager.get(), tenant, null_yield, script_ctx);
     if (rc < 0) {
       cerr << "ERROR: failed to remove script. error: " << rc << std::endl;
       return -rc;

src/rgw/rgw_lua.cc
@@ -68,25 +68,20 @@ std::string script_oid(context ctx, const std::string& tenant) {
 }
 
 
-int read_script(const DoutPrefixProvider *dpp, rgw::sal::Store* store, const std::string& tenant, optional_yield y, context ctx, std::string& script)
+int read_script(const DoutPrefixProvider *dpp, sal::LuaScriptManager* manager, const std::string& tenant, optional_yield y, context ctx, std::string& script)
 {
-  auto lua_script = store->get_lua_script_manager();
-
-  return lua_script->get(dpp, y, script_oid(ctx, tenant), script);
+  return manager->get(dpp, y, script_oid(ctx, tenant), script);
 }
 
-int write_script(const DoutPrefixProvider *dpp, rgw::sal::Store* store, const std::string& tenant, optional_yield y, context ctx, const std::string& script)
+int write_script(const DoutPrefixProvider *dpp, sal::LuaScriptManager* manager, const std::string& tenant, optional_yield y, context ctx, const std::string& script)
 {
-  auto lua_script = store->get_lua_script_manager();
 
-  return lua_script->put(dpp, y, script_oid(ctx, tenant), script);
+  return manager->put(dpp, y, script_oid(ctx, tenant), script);
 }
 
-int delete_script(const DoutPrefixProvider *dpp, rgw::sal::Store* store, const std::string& tenant, optional_yield y, context ctx)
+int delete_script(const DoutPrefixProvider *dpp, sal::LuaScriptManager* manager, const std::string& tenant, optional_yield y, context ctx)
 {
-  auto lua_script = store->get_lua_script_manager();
-
-  return lua_script->del(dpp, y, script_oid(ctx, tenant));
+  return manager->del(dpp, y, script_oid(ctx, tenant));
 }
 
 #ifdef WITH_RADOSGW_LUA_PACKAGES

src/rgw/rgw_sal_rados.cc
@@ -2946,13 +2946,17 @@ const std::string& RadosZone::get_realm_id()
   return store->svc()->zone->get_realm().get_id();
 }
 
-RadosLuaScriptManager::RadosLuaScriptManager(RadosStore* _s) : store(_s)
-{
-  pool = store->svc()->zone->get_zone_params().log_pool;
-}
+RadosLuaScriptManager::RadosLuaScriptManager(RadosStore* _s) : 
+  store(_s),
+  pool((store->svc() && store->svc()->zone) ? store->svc()->zone->get_zone_params().log_pool : rgw_pool())
+{ }
 
 int RadosLuaScriptManager::get(const DoutPrefixProvider* dpp, optional_yield y, const std::string& key, std::string& script)
 {
+  if (pool.empty()) {
+    ldpp_dout(dpp, 10) << "WARNING: missing pool when reading lua script " << dendl;
+    return 0;
+  }
   auto obj_ctx = store->svc()->sysobj->init_obj_ctx();
   bufferlist bl;
 
@@ -2973,6 +2977,10 @@ int RadosLuaScriptManager::get(const DoutPrefixProvider* dpp, optional_yield y, 
 
 int RadosLuaScriptManager::put(const DoutPrefixProvider* dpp, optional_yield y, const std::string& key, const std::string& script)
 {
+  if (pool.empty()) {
+    ldpp_dout(dpp, 10) << "WARNING: missing pool when writing lua script " << dendl;
+    return 0;
+  }
   auto obj_ctx = store->svc()->sysobj->init_obj_ctx();
   bufferlist bl;
   ceph::encode(script, bl);
@@ -2987,6 +2995,10 @@ int RadosLuaScriptManager::put(const DoutPrefixProvider* dpp, optional_yield y, 
 
 int RadosLuaScriptManager::del(const DoutPrefixProvider* dpp, optional_yield y, const std::string& key)
 {
+  if (pool.empty()) {
+    ldpp_dout(dpp, 10) << "WARNING: missing pool when deleting lua script " << dendl;
+    return 0;
+  }
   int r = rgw_delete_system_obj(dpp, store->svc()->sysobj, pool, key, nullptr, y);
   if (r < 0 && r != -ENOENT) {
     return r;

ceph/ceph

src/rgw/rgw_admin.cc
@@ -10394,7 +10394,8 @@ next:
       cerr << "ERROR: cannot specify tenant in background context" << std::endl;
       return EINVAL;
     }
-    rc = rgw::lua::write_script(dpp(), store, tenant, null_yield, script_ctx, script);
+    auto lua_manager = store->get_lua_manager();
+    rc = rgw::lua::write_script(dpp(), lua_manager.get(), tenant, null_yield, script_ctx, script);
     if (rc < 0) {
       cerr << "ERROR: failed to put script. error: " << rc << std::endl;
       return -rc;
@@ -10411,8 +10412,9 @@ next:
       cerr << "ERROR: invalid script context: " << *str_script_ctx << ". must be one of: preRequest, postRequest, background" << std::endl;
       return EINVAL;
     }
+    auto lua_manager = store->get_lua_manager();
     std::string script;
-    const auto rc = rgw::lua::read_script(dpp(), store, tenant, null_yield, script_ctx, script);
+    const auto rc = rgw::lua::read_script(dpp(), lua_manager.get(), tenant, null_yield, script_ctx, script);
     if (rc == -ENOENT) {
       std::cout << "no script exists for context: " << *str_script_ctx << 
         (tenant.empty() ? "" : (" in tenant: " + tenant)) << std::endl;
@@ -10434,7 +10436,8 @@ next:
       cerr << "ERROR: invalid script context: " << *str_script_ctx << ". must be one of: preRequest, postRequest, background" << std::endl;
       return EINVAL;
     }
-    const auto rc = rgw::lua::delete_script(dpp(), store, tenant, null_yield, script_ctx);
+    auto lua_manager = store->get_lua_manager();
+    const auto rc = rgw::lua::delete_script(dpp(), lua_manager.get(), tenant, null_yield, script_ctx);
     if (rc < 0) {
       cerr << "ERROR: failed to remove script. error: " << rc << std::endl;
       return -rc;

src/rgw/rgw_lua.cc
@@ -67,25 +67,21 @@ std::string script_oid(context ctx, const std::string& tenant) {
 }
 
 
-int read_script(const DoutPrefixProvider *dpp, rgw::sal::Store* store, const std::string& tenant, optional_yield y, context ctx, std::string& script)
+int read_script(const DoutPrefixProvider *dpp, sal::LuaManager* manager, const std::string& tenant, optional_yield y, context ctx, std::string& script)
 {
-  auto lua_mgr = store->get_lua_manager();
 
-  return lua_mgr->get_script(dpp, y, script_oid(ctx, tenant), script);
+  return manager->get_script(dpp, y, script_oid(ctx, tenant), script);
+
 }
 
-int write_script(const DoutPrefixProvider *dpp, rgw::sal::Store* store, const std::string& tenant, optional_yield y, context ctx, const std::string& script)
+int write_script(const DoutPrefixProvider *dpp, sal::LuaManager* manager, const std::string& tenant, optional_yield y, context ctx, const std::string& script)
 {
-  auto lua_mgr = store->get_lua_manager();
-
-  return lua_mgr->put_script(dpp, y, script_oid(ctx, tenant), script);
+  return manager->put_script(dpp, y, script_oid(ctx, tenant), script);
 }
 
-int delete_script(const DoutPrefixProvider *dpp, rgw::sal::Store* store, const std::string& tenant, optional_yield y, context ctx)
+int delete_script(const DoutPrefixProvider *dpp, sal::LuaManager* manager, const std::string& tenant, optional_yield y, context ctx)
 {
-  auto lua_mgr = store->get_lua_manager();
-
-  return lua_mgr->del_script(dpp, y, script_oid(ctx, tenant));
+  return manager->del_script(dpp, y, script_oid(ctx, tenant));
 }
 
 #ifdef WITH_RADOSGW_LUA_PACKAGES

src/rgw/rgw_sal_rados.cc
@@ -3009,13 +3009,17 @@ const std::string_view RadosZone::get_tier_type()
   return store->svc()->zone->get_zone().tier_type;
 }
 
-RadosLuaManager::RadosLuaManager(RadosStore* _s) : store(_s)
-{
-  pool = store->svc()->zone->get_zone_params().log_pool;
-}
+RadosLuaManager::RadosLuaManager(RadosStore* _s) : 
+  store(_s),
+  pool((store->svc() && store->svc()->zone) ? store->svc()->zone->get_zone_params().log_pool : rgw_pool())
+{ }
 
 int RadosLuaManager::get_script(const DoutPrefixProvider* dpp, optional_yield y, const std::string& key, std::string& script)
 {
+  if (pool.empty()) {
+    ldpp_dout(dpp, 10) << "WARNING: missing pool when reading lua script " << dendl;
+    return 0;
+  }
   bufferlist bl;
 
   int r = rgw_get_system_obj(store->svc()->sysobj, pool, key, bl, nullptr, nullptr, y, dpp);
@@ -3035,6 +3039,10 @@ int RadosLuaManager::get_script(const DoutPrefixProvider* dpp, optional_yield y,
 
 int RadosLuaManager::put_script(const DoutPrefixProvider* dpp, optional_yield y, const std::string& key, const std::string& script)
 {
+  if (pool.empty()) {
+    ldpp_dout(dpp, 10) << "WARNING: missing pool when writing lua script " << dendl;
+    return 0;
+  }
   bufferlist bl;
   ceph::encode(script, bl);
 
@@ -3048,6 +3056,10 @@ int RadosLuaManager::put_script(const DoutPrefixProvider* dpp, optional_yield y,
 
 int RadosLuaManager::del_script(const DoutPrefixProvider* dpp, optional_yield y, const std::string& key)
 {
+  if (pool.empty()) {
+    ldpp_dout(dpp, 10) << "WARNING: missing pool when deleting lua script " << dendl;
+    return 0;
+  }
   int r = rgw_delete_system_obj(dpp, store->svc()->sysobj, pool, key, nullptr, y);
   if (r < 0 && r != -ENOENT) {
     return r;