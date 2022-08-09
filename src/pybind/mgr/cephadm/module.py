7ad668158c76c498c08ca584a27d288de0ac1e3b
Adam King <adking@redhat.com>
2022-08-09 16:13:38 -0400

    mgr/cephadm: reconfig iscsi daemons if trusted_ip_list changes
    This is necessary as iscsi daemons expect the iscsi-gateway.cfg
    which includes this list to match across the iscsi daemons
    
    Fixes: https://tracker.ceph.com/issues/57078
    
    Signed-off-by: Adam King <adking@redhat.com>
    (cherry picked from commit 8a6e67c6a32f71f38dd3dafe84e6a5e7b2a99f5a)
    
    Conflicts:
    	src/pybind/mgr/cephadm/module.py

--- src/pybind/mgr/cephadm/module.py
+++ src/pybind/mgr/cephadm/module.py
@@ -26,7 +26,7 @@ from ceph.deployment.drive_group import DriveGroupSpec
 from ceph.deployment.service_spec import \
     ServiceSpec, PlacementSpec, \
     HostPlacementSpec, IngressSpec, \
-    TunedProfileSpec
+    TunedProfileSpec, IscsiServiceSpec
 from ceph.utils import str_to_datetime, datetime_to_str, datetime_now
 from cephadm.serve import CephadmServe
 from cephadm.services.cephadmservice import CephadmDaemonDeploySpec
@@ -540,6 +540,7 @@ class CephadmOrchestrator(orchestrator.Orchestrator, MgrModule,
 
         self.mgr_service: MgrService = cast(MgrService, self.cephadm_services['mgr'])
         self.osd_service: OSDService = cast(OSDService, self.cephadm_services['osd'])
+        self.iscsi_service: IscsiService = cast(IscsiService, self.cephadm_services['iscsi'])
 
         self.template = TemplateMgr(self)
 
@@ -2360,7 +2361,11 @@ Then run the following:
             deps = sorted([self.get_mgr_ip(), server_port, root_cert,
                            str(self.device_enhanced_scan)])
         elif daemon_type == 'iscsi':
-            deps = [self.get_mgr_ip()]
+            if spec:
+                iscsi_spec = cast(IscsiServiceSpec, spec)
+                deps = [self.iscsi_service.get_trusted_ips(iscsi_spec)]
+            else:
+                deps = [self.get_mgr_ip()]
         else:
             need = {
                 'prometheus': ['mgr', 'alertmanager', 'node-exporter', 'ingress'],
