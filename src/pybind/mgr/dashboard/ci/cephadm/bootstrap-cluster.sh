0b1ac17f5cefea060c3e121bc9c7da15f6cc8ed2
Nizamudeen A <nia@redhat.com>
2022-08-16 19:47:13 +0530

    mgr/dashboard: fix "can't read .ssh/known_hosts: No such file or directory"
    Updates the fedora version too
    
    Fixes: https://tracker.ceph.com/issues/57365
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- src/pybind/mgr/dashboard/ci/cephadm/bootstrap-cluster.sh
+++ src/pybind/mgr/dashboard/ci/cephadm/bootstrap-cluster.sh
@@ -25,7 +25,7 @@ fsid=$(cat /etc/ceph/ceph.conf | grep fsid | awk '{ print $3}')
 cephadm_shell="cephadm shell --fsid ${fsid} -c /etc/ceph/ceph.conf -k /etc/ceph/ceph.client.admin.keyring"
 
 {% for number in range(1, nodes) %}
-  ssh-copy-id -f -i /etc/ceph/ceph.pub  -o StrictHostKeyChecking=no root@{{ prefix }}-node-0{{ number }}
+  ssh-copy-id -f -i /etc/ceph/ceph.pub  -o StrictHostKeyChecking=no root@192.168.100.10{{ number }}
   {% if expanded_cluster is defined %}
     ${cephadm_shell} ceph orch host add {{ prefix }}-node-0{{ number }}
   {% endif %}
