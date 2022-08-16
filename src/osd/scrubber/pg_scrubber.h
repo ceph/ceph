95dd59ba873cd0e04e3cbf97e74e7d3919c41c4c
Ronen Friedman <rfriedma@redhat.com>
2022-08-16 15:48:04 +0300

    osd/scrub: reduce the amount of log lines
    Following call chains, and removing repeated data.
    
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- src/osd/scrubber/pg_scrubber.h
+++ src/osd/scrubber/pg_scrubber.h
@@ -359,9 +359,8 @@ class PgScrubber : public ScrubPgIF,
 
   void rm_from_osd_scrubbing() final;
 
-  void on_primary_change(const requested_scrub_t& request_flags) final;
-
-  void on_maybe_registration_change(
+  void on_primary_change(
+    std::string_view caller,
     const requested_scrub_t& request_flags) final;
 
   void scrub_requested(scrub_level_t scrub_level,
