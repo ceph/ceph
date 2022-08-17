a8a7ad74fd37995f3a9e13f436ef6108c1ced41c
Casey Bodley <cbodley@redhat.com>
2022-08-17 19:55:25 -0400

    common: ceph::perf_counters::cache_key_insert()
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/common/perf_counters_cache_key.h
+++ src/common/perf_counters_cache_key.h
@@ -1,9 +1,6 @@
 #pragma once
 
-#include <algorithm>
-#include <iterator>
-#include <numeric>
-#include <string_view>
+#include <string>
 #include <utility>
 
 namespace ceph::perf_counters {
@@ -13,54 +10,52 @@ using label_pair = std::pair<std::string_view, std::string_view>;
 
 
 /// construct a cache key for a perf counter and set of labels. returns a string
-/// of the form "counter_name\0key1\0value1\0key2\0value2\0", where label pairs
-/// are insertion-sorted by key then value
+/// of the form "counter_name\0key1\0val1\0key2\0val2\0", where label pairs
+/// are sorted by key with duplicates removed
 template <std::size_t Count>
-constexpr std::string make_cache_key(std::string_view counter_name,
-                                     label_pair (&&sorted)[Count])
+std::string cache_key(std::string_view counter_name,
+                      label_pair (&&labels)[Count]);
+
+/// \overload
+std::string cache_key(std::string_view counter_name);
+
+/// insert additional labels to an existing label set. this returns a new
+/// string without modifying the existing one. the returned string has labels
+/// in sorted order and no duplicate keys
+template <std::size_t Count>
+std::string cache_key_insert(std::string_view key,
+                             label_pair (&&labels)[Count]);
+
+
+namespace detail {
+
+std::string create(std::string_view counter_name,
+                   label_pair* begin, label_pair* end);
+
+std::string insert(const char* begin1, const char* end1,
+                   label_pair* begin2, label_pair* end2);
+
+} // namespace detail
+
+template <std::size_t Count>
+std::string cache_key(std::string_view counter_name,
+                      label_pair (&&labels)[Count])
 {
-  std::sort(std::begin(sorted), std::end(sorted));
-
-  // use a null character to delimit strings
-  constexpr char delimiter = '\0';
-
-  // calculate the total size and preallocate the buffer
-  auto size = std::accumulate(std::begin(sorted), std::end(sorted),
-                              counter_name.size() + sizeof(delimiter),
-                              [] (std::size_t sum, const label_pair& l) {
-                                return sum + l.first.size() + sizeof(delimiter)
-                                    + l.second.size() + sizeof(delimiter);
-                              });
-
-  std::string result;
-  result.resize(size);
-
-  auto pos = result.begin();
-  pos = std::copy(counter_name.begin(), counter_name.end(), pos);
-  *(pos++) = delimiter;
-
-  for (const auto& label : sorted) {
-    pos = std::copy(label.first.begin(), label.first.end(), pos);
-    *(pos++) = delimiter;
-    pos = std::copy(label.second.begin(), label.second.end(), pos);
-    *(pos++) = delimiter;
-  }
-
-  return result;
+  return detail::create(counter_name, std::begin(labels), std::end(labels));
 }
 
-constexpr std::string make_cache_key(std::string_view counter_name)
+std::string cache_key(std::string_view counter_name)
 {
-  constexpr char delimiter = '\0';
-  const auto size = counter_name.size() + sizeof(delimiter);
-  std::string result;
-  result.resize(size);
-  auto pos = result.begin();
-  pos = std::copy(counter_name.begin(), counter_name.end(), pos);
-  *(pos++) = delimiter;
-  return result;
+  label_pair* end = nullptr;
+  return detail::create(counter_name, end, end);
 }
 
-// TODO: cache_key_insert() to add more labels to an existing string
+template <std::size_t Count>
+std::string cache_key_insert(std::string_view key,
+                             label_pair (&&labels)[Count])
+{
+  return detail::insert(key.begin(), key.end(),
+                        std::begin(labels), std::end(labels));
+}
 
 } // namespace ceph::perf_counters
