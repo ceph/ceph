b2e6f4b457c39f2a4d539d8d015300ebf67a907c
Adam C. Emerson <aemerson@redhat.com>
2022-08-24 15:24:09 -0400

    rgw: `SimpleRadosWriteAttrsCR` uses an async RADOS call
    Don't go through the 'system object' cache. This also saves us the use
    of the RADOS async completion processor.
    
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_data_sync.cc
+++ src/rgw/rgw_data_sync.cc
@@ -3054,7 +3054,7 @@ public:
 
         map<string, bufferlist> attrs;
         status.encode_all_attrs(attrs);
-        call(new RGWSimpleRadosWriteAttrsCR(dpp, sync_env->async_rados, sync_env->svc->sysobj,
+        call(new RGWSimpleRadosWriteAttrsCR(dpp, sync_env->store,
                                             obj, attrs, &objv_tracker, exclusive));
       }
 
@@ -3907,7 +3907,7 @@ class RGWWriteBucketShardIncSyncStatus : public RGWCoroutine {
     reenter(this) {
       sync_marker.encode_attr(attrs);
 
-      yield call(new RGWSimpleRadosWriteAttrsCR(sync_env->dpp, sync_env->async_rados, sync_env->svc->sysobj,
+      yield call(new RGWSimpleRadosWriteAttrsCR(sync_env->dpp, sync_env->store,
                                                 obj, attrs, &objv_tracker));
       if (retcode < 0) {
         return set_cr_error(retcode);
