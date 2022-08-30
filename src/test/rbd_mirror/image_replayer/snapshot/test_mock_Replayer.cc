5567d271163aa2928427795f1e906f9ac1e740a1
Ilya Dryomov <idryomov@gmail.com>
2022-08-30 11:45:44 +0200

    rbd-mirror: skip setting error code on snapshot replayer shutdown
    This is regarding failures in unregister_remote_update_watcher() and
    unregister_local_update_watcher().  handle_replay_complete() can't be
    called in these cases anymore as it would blindly attempt to unregister
    watchers from scratch again.  Dropping handle_replay_complete() calls
    there means that these failures would only be logged and would not be
    surfaced by snapshot replayer.  But the only caller ignores them
    anyway:
    
      void ImageReplayer<I>::shut_down(int r) {
        ...
        // close the replayer
        if (m_replayer != nullptr) {
          ctx = new LambdaContext([this, ctx](int r) {
            m_replayer->destroy();
            m_replayer = nullptr;
            ctx->complete(0);             <------
          });
          ctx = new LambdaContext([this, ctx](int r) {
            m_replayer->shut_down(ctx);
          });
        }
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- src/test/rbd_mirror/image_replayer/snapshot/test_mock_Replayer.cc
+++ src/test/rbd_mirror/image_replayer/snapshot/test_mock_Replayer.cc
@@ -1908,7 +1908,7 @@ TEST_F(TestMockImageReplayerSnapshotReplayer, UnregisterRemoteUpdateWatcherError
 
   C_SaferCond shutdown_ctx;
   mock_replayer.shut_down(&shutdown_ctx);
-  ASSERT_EQ(-EINVAL, shutdown_ctx.wait());
+  ASSERT_EQ(0, shutdown_ctx.wait());
 }
 
 TEST_F(TestMockImageReplayerSnapshotReplayer, UnregisterLocalUpdateWatcherError) {
@@ -1950,7 +1950,7 @@ TEST_F(TestMockImageReplayerSnapshotReplayer, UnregisterLocalUpdateWatcherError)
 
   C_SaferCond shutdown_ctx;
   mock_replayer.shut_down(&shutdown_ctx);
-  ASSERT_EQ(-EINVAL, shutdown_ctx.wait());
+  ASSERT_EQ(0, shutdown_ctx.wait());
 }
 
 TEST_F(TestMockImageReplayerSnapshotReplayer, LoadImageMetaError) {
