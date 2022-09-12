dff8a2dfa1f1933ee04eff55d90efa0ffd11cdd1
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2022-09-12 13:00:39 +0000

    test/libcephfs: use CEPHFS_E* errors
    The cephfs client now uses CEPHFS_E errors (based on Linux)
    instead of the platform specific errno values.
    
    This change updates the libcephfs tests accordingly.
    
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>

--- src/test/libcephfs/newops.cc
+++ src/test/libcephfs/newops.cc
@@ -19,6 +19,7 @@
 #include "gmock/gmock-more-matchers.h"
 #include "include/compat.h"
 #include "include/cephfs/libcephfs.h"
+#include "include/fs_types.h"
 #include "mds/mdstypes.h"
 #include "include/stat.h"
 #include <errno.h>
@@ -62,8 +63,8 @@ TEST(LibCephFS, NewOPs)
   {
     char value[1024] = "";
     int r = ceph_getxattr(cmount, test_path, "ceph.dir.pin.random", (void*)value, sizeof(value));
-    // Clients will return -ENODATA if new getvxattr op not support yet.
-    EXPECT_THAT(r, AnyOf(Gt(0), Eq(-ENODATA)));
+    // Clients will return -CEPHFS_ENODATA if new getvxattr op not support yet.
+    EXPECT_THAT(r, AnyOf(Gt(0), Eq(-CEPHFS_ENODATA)));
   }
 
   {
@@ -71,13 +72,13 @@ TEST(LibCephFS, NewOPs)
     std::stringstream ss;
     ss << val;
     int r = ceph_setxattr(cmount, test_path, "ceph.dir.pin.random", (void*)ss.str().c_str(), strlen(ss.str().c_str()), XATTR_CREATE);
-    // Old cephs will return -EINVAL if not support "ceph.dir.pin.random" yet.
-    EXPECT_THAT(r, AnyOf(Eq(0), Eq(-EINVAL)));
+    // Old cephs will return -CEPHFS_EINVAL if not support "ceph.dir.pin.random" yet.
+    EXPECT_THAT(r, AnyOf(Eq(0), Eq(-CEPHFS_EINVAL)));
 
     char value[1024] = "";
     r = ceph_getxattr(cmount, test_path, "ceph.dir.pin.random", (void*)value, sizeof(value));
-    // Clients will return -ENODATA if new getvxattr op not support yet.
-    EXPECT_THAT(r, AnyOf(Gt(0), Eq(-ENODATA)));
+    // Clients will return -CEPHFS_ENODATA if new getvxattr op not support yet.
+    EXPECT_THAT(r, AnyOf(Gt(0), Eq(-CEPHFS_ENODATA)));
   }
 
   ASSERT_EQ(0, ceph_rmdir(cmount, test_path));
