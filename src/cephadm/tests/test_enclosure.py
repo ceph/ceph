a5133c69043710d97fa5863163cf69cacd61c8a5
John Mulligan <jmulligan@redhat.com>
2022-09-27 14:50:24 -0400

    cephadm: rename import from cd to _cephadm
    The `cd` name is an abbreviation of an obsolete name for cephadm.
    As this is not widely known it could be confusing or conflict with
    other common terms like `cd` for "change dir" or "compact disc".
    Therefore we rename it to something much clearer.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- src/cephadm/tests/test_enclosure.py
+++ src/cephadm/tests/test_enclosure.py
@@ -3,12 +3,12 @@ import pytest
 from unittest import mock
 from tests.fixtures import host_sysfs, import_cephadm
 
-cd = import_cephadm()
+_cephadm = import_cephadm()
 
 
 @pytest.fixture
 def enclosure(host_sysfs):
-    e = cd.Enclosure(
+    e = _cephadm.Enclosure(
         enc_id='1',
         enc_path='/sys/class/scsi_generic/sg2/device/enclosure/0:0:1:0',
         dev_path='/sys/class/scsi_generic/sg2')
