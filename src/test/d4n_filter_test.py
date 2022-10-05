0f7ad6d20eb5e30aa1e17f4c0e23cbd66f7460d9
samarah <suriarte@redhat.com>
2022-10-05 11:39:58 -0400

    RGW: D4N python test updated to exclude cache tests since they do not currently work
    Signed-off-by: samarah <suriarte@redhat.com>

--- src/test/d4n_filter_test.py
+++ src/test/d4n_filter_test.py
@@ -114,6 +114,7 @@ class D4NFilterTestCase(unittest.TestCase):
         self.assertEqual(response_put.get('ResponseMetadata').get('HTTPStatusCode'), 200) 
 
         data = r.hgetall('rgw-object:test.txt:cache')
+        print(data)
         output = subprocess.check_output(['./bin/radosgw-admin', 'object', 'stat', '--bucket=bkt', '--object=test.txt'])
         attrs = json.loads(output)
         
