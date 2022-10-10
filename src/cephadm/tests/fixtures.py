0f1c7a6d78d641886fb92169ed6f1ee0479fc7c4
Kefu Chai <tchaikov@gmail.com>
2022-10-10 22:43:23 +0800

    cephadm: fix the indent
    Signed-off-by: Kefu Chai <tchaikov@gmail.com>

--- src/cephadm/tests/fixtures.py
+++ src/cephadm/tests/fixtures.py
@@ -77,17 +77,17 @@ def cephadm_fs(
          mock.patch('platform.processor', return_value='x86_64'), \
          mock.patch('cephadm.extract_uid_gid', return_value=(uid, gid)):
 
-            if not fake_filesystem.is_root():
-                fake_filesystem.set_uid(0)
+        if not fake_filesystem.is_root():
+            fake_filesystem.set_uid(0)
 
-            fs.create_dir(_cephadm.DATA_DIR)
-            fs.create_dir(_cephadm.LOG_DIR)
-            fs.create_dir(_cephadm.LOCK_DIR)
-            fs.create_dir(_cephadm.LOGROTATE_DIR)
-            fs.create_dir(_cephadm.UNIT_DIR)
-            fs.create_dir('/sys/block')
+        fs.create_dir(_cephadm.DATA_DIR)
+        fs.create_dir(_cephadm.LOG_DIR)
+        fs.create_dir(_cephadm.LOCK_DIR)
+        fs.create_dir(_cephadm.LOGROTATE_DIR)
+        fs.create_dir(_cephadm.UNIT_DIR)
+        fs.create_dir('/sys/block')
 
-            yield fs
+        yield fs
 
 
 @pytest.fixture()
