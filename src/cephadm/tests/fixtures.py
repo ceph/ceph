b136b409e44acffe0ecc484a49538b3a661d01c9
Kefu Chai <tchaikov@gmail.com>
2022-10-10 21:57:57 +0800

    cephadm: run tests as root
    as we will be create/access directories which are only accessible by
    root with pyfakefs, and pyfake respects the uid of current effective
    user, so if we run the test using non-root user, these tests would fail
    after the fix for https://github.com/pytest-dev/pyfakefs/issues/489
    is addressed in the pyfakefs we are using.
    
    in this change, we will run the test on behalf of root, so we can
    create and access the directories as we did before.
    
    Signed-off-by: Kefu Chai <tchaikov@gmail.com>
    (cherry picked from commit ed19416067a7ef3b69310d22bf4e3b573e3011f0)
    
     Conflicts:
    	src/cephadm/tests/fixtures.py
      - Replace _cephadm with cd

--- src/cephadm/tests/fixtures.py
+++ src/cephadm/tests/fixtures.py
@@ -68,11 +68,15 @@ def cephadm_fs(
          mock.patch('platform.processor', return_value='x86_64'), \
          mock.patch('cephadm.extract_uid_gid', return_value=(uid, gid)):
 
+            if not fake_filesystem.is_root():
+                fake_filesystem.set_uid(0)
+
             fs.create_dir(cd.DATA_DIR)
             fs.create_dir(cd.LOG_DIR)
             fs.create_dir(cd.LOCK_DIR)
             fs.create_dir(cd.LOGROTATE_DIR)
             fs.create_dir(cd.UNIT_DIR)
+            fs.create_dir('/sys/block')
 
             yield fs
 
