37c3466af237ca938964d2548b6262ed32eeb89c
John Mulligan <jmulligan@redhat.com>
2022-10-13 10:37:10 -0400

    script: add jsonnet to support running tests in container
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- src/script/run-make.sh
+++ src/script/run-make.sh
@@ -55,7 +55,7 @@ function prepare() {
 
     if test -f ./install-deps.sh ; then
         ci_debug "Running install-deps.sh"
-        INSTALL_EXTRA_PACKAGES="ccache git $which_pkg clang lvm2"
+        INSTALL_EXTRA_PACKAGES="ccache git $which_pkg clang lvm2 jsonnet"
         $DRY_RUN source ./install-deps.sh || return 1
         trap clean_up_after_myself EXIT
     fi
