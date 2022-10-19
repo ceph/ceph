b35de88a3a7d7e3498a939264566dcca44ff1cfa
Yin Congmin <congmin.yin@intel.com>
2022-10-19 10:47:35 +0800

    librbd/cache/pwl:fix clean vs bytes_dirty cache state inconsistency
    The space of the pwl is pre allocated. In check_allocation(), the space
    is reserved, and then the op is completed after a period of process.
    When checking resource is successful and the resource is reserved,
    dirty_bytes has been increased. But new entry will not be added
    to m_dirty_log_entries until the op is finished. This time gap
    leads to inconsistencies.
    
    Put update_image_cache_state() in check_allocation() to fix this
    issue. It is now considered that as long as space is reserved
    and the op is started, the cache is dirty.
    
    Fixes: https://tracker.ceph.com/issues/57872
    
    Signed-off-by: Yin Congmin <congmin.yin@intel.com>
    (cherry picked from commit e2b079137473d4c819d58ef6466450a5c4a8b8af)
    
    Conflicts:
    	src/librbd/cache/pwl/AbstractWriteLog.cc [ commit 6290446b819b
    	  ("librbd/cache/pwl/: remove IO waited state") not in pacific ]

--- src/librbd/cache/pwl/AbstractWriteLog.cc
+++ src/librbd/cache/pwl/AbstractWriteLog.cc
@@ -1308,7 +1308,6 @@ void AbstractWriteLog<I>::complete_op_log_entries(GenericLogOperations &&ops,
 {
   GenericLogEntries dirty_entries;
   int published_reserves = 0;
-  bool need_update_state = false;
   ldout(m_image_ctx.cct, 20) << __func__ << ": completing" << dendl;
   for (auto &op : ops) {
     utime_t now = ceph_clock_now();
@@ -1328,11 +1327,6 @@ void AbstractWriteLog<I>::complete_op_log_entries(GenericLogOperations &&ops,
       std::lock_guard locker(m_lock);
       m_unpublished_reserves -= published_reserves;
       m_dirty_log_entries.splice(m_dirty_log_entries.end(), dirty_entries);
-      if (m_cache_state->clean && !this->m_dirty_log_entries.empty()) {
-        m_cache_state->clean = false;
-        update_image_cache_state();
-        need_update_state = true;
-      }
     }
     op->complete(result);
     m_perfcounter->tinc(l_librbd_pwl_log_op_dis_to_app_t,
@@ -1346,10 +1340,6 @@ void AbstractWriteLog<I>::complete_op_log_entries(GenericLogOperations &&ops,
     m_perfcounter->hinc(l_librbd_pwl_log_op_app_to_appc_t_hist, app_lat.to_nsec(),
                       log_entry->ram_entry.write_bytes);
     m_perfcounter->tinc(l_librbd_pwl_log_op_app_to_cmp_t, now - op->log_append_start_time);
-  }
-  if (need_update_state) {
-    std::unique_lock locker(m_lock);
-    write_image_cache_state(locker);
   }
   // New entries may be flushable
   {
@@ -1545,7 +1535,7 @@ bool AbstractWriteLog<I>::check_allocation(
   }
 
   if (alloc_succeeds) {
-    std::lock_guard locker(m_lock);
+    std::unique_lock locker(m_lock);
     /* We need one free log entry per extent (each is a separate entry), and
      * one free "lane" for remote replication. */
     if ((m_free_lanes >= num_lanes) &&
@@ -1559,6 +1549,11 @@ bool AbstractWriteLog<I>::check_allocation(
       m_bytes_dirty += bytes_dirtied;
       if (req->has_io_waited_for_buffers()) {
         req->set_io_waited_for_buffers(false);
+      }
+      if (m_cache_state->clean && bytes_dirtied > 0) {
+        m_cache_state->clean = false;
+        update_image_cache_state();
+        write_image_cache_state(locker);
       }
     } else {
       alloc_succeeds = false;
