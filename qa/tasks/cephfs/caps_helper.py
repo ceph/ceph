9e07568fc3ea8123ce890b19c115b0908348d76b
Xiubo Li <xiubli@redhat.com>
2022-11-02 09:12:16 +0800

    qa: add test for checking access in client side of root_squash
    Test the 'chown' and 'truncate', which will call the setattr and
    'cat' will open the files. Before each testing will open the file
    by non-root user and keep it to make sure the Fxw caps are issued,
    and then user the 'sudo' do to the tests, which will set the uid/gid
    to 0/0.
    
    Fixes: https://tracker.ceph.com/issues/57154
    Signed-off-by: Xiubo Li <xiubli@redhat.com>
    (cherry picked from commit 28023f84d714488a5dfd17b2191790ed15909fb3)
    
    Conflicts:
    	qa/tasks/cephfs/caps_helper.py
    	qa/tasks/cephfs/test_admin.py
    	missed dependency commit 370449ac9d0("qa/cephfs: upgrade and
    	                                     rename class CapsHelper")

--- qa/tasks/cephfs/caps_helper.py
+++ qa/tasks/cephfs/caps_helper.py
@@ -1,9 +1,13 @@
 """
 Helper methods to test that MON and MDS caps are enforced properly.
 """
+import logging
+
 from tasks.cephfs.cephfs_test_case import CephFSTestCase
 
 from teuthology.orchestra.run import Raw
+
+log = logging.getLogger(__name__)
 
 class CapsHelper(CephFSTestCase):
 
@@ -29,21 +33,24 @@ class CapsHelper(CephFSTestCase):
 
     def run_mds_cap_tests(self, filepaths, filedata, mounts, perm):
         self.conduct_pos_test_for_read_caps(filepaths, filedata, mounts)
+        self.conduct_pos_test_for_open_caps(filepaths, filedata, mounts)
 
         if perm == 'rw':
             self.conduct_pos_test_for_write_caps(filepaths, mounts)
         elif perm == 'r':
+            self.conduct_neg_test_for_chown_caps(filepaths, filedata, mounts)
+            self.conduct_neg_test_for_truncate_caps(filepaths, filedata, mounts)
             self.conduct_neg_test_for_write_caps(filepaths, mounts)
         else:
             raise RuntimeError(f'perm = {perm}\nIt should be "r" or "rw".')
 
-    def conduct_pos_test_for_read_caps(self, filepaths, filedata, mounts):
+    def conduct_pos_test_for_read_caps(self, filepaths, filedata, mounts, sudo_read=False):
         for mount in mounts:
             for path, data in zip(filepaths, filedata):
                 # XXX: conduct tests only if path belongs to current mount; in
                 # teuth tests client are located on same machines.
                 if path.find(mount.hostfs_mntpt) != -1:
-                    contents = mount.read_file(path)
+                    contents = mount.read_file(path, sudo_read)
                     self.assertEqual(data, contents)
 
     def conduct_pos_test_for_write_caps(self, filepaths, mounts):
@@ -77,3 +84,35 @@ class CapsHelper(CephFSTestCase):
 
         raise RuntimeError('get_save_mon_cap: mon cap not found in keyring. '
                            'keyring -\n' + keyring)
+
+    def _conduct_neg_test_for_root_squash_caps(self, _cmdargs, filepaths, filedata, mounts, sudo_write=False):
+        cmdargs = ['sudo'] if sudo_write else ['']
+        cmdargs += _cmdargs
+
+        for mount in mounts:
+            for path in filepaths:
+                log.info(f'test absence of {_cmdargs[0]} perm: expect failure {path}.')
+
+                # open the file and hold it. The MDS will issue CEPH_CAP_EXCL_*
+                # to mount
+                proc = mount.open_background(path, write=False)
+
+                cmdargs.append(path)
+                mount.negtestcmd(args=cmdargs, retval=1, errmsg="permission denied")
+                cmdargs.pop(-1)
+
+                mount._kill_background(proc)
+
+                log.info(f'absence of {_cmdargs[0]} perm was tested successfully')
+
+    def conduct_neg_test_for_chown_caps(self, filepaths, filedata, mounts, sudo_write=True):
+        # flip ownership to nobody. assumption: nobody's id is 65534
+        cmdargs = ['chown', '-h', '65534:65534']
+        self._conduct_neg_test_for_root_squash_caps(cmdargs, filepaths, filedata, mounts, sudo_write)
+
+    def conduct_neg_test_for_truncate_caps(self, filepaths, filedata, mounts, sudo_write=True):
+        cmdargs = ['truncate', '-s', '10GB']
+        self._conduct_neg_test_for_root_squash_caps(cmdargs, filepaths, filedata, mounts, sudo_write)
+
+    def conduct_pos_test_for_open_caps(self, filepaths, filedata, mounts, sudo_read=True):
+        self.conduct_pos_test_for_read_caps(filepaths, filedata, mounts, sudo_read)
