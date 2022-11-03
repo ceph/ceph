5e33c262e2c4d8fd9387c43e8908e5a8a8e89ea5
Matan Breizman <mbreizma@redhat.com>
2022-11-03 08:59:11 +0000

    osd: Remove oldest_stored_osdmap()
    The only usage was for identyfing map gaps on new intervals.
    We should use max_oldest_stored_osdmap() instead, since a specific
    osd's oldest_map may lag behind.
    
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>

--- src/osd/PG.cc
+++ src/osd/PG.cc
@@ -1745,10 +1745,6 @@ void PG::on_new_interval()
   m_scrubber->on_primary_change(__func__, m_planned_scrub);
 }
 
-epoch_t PG::oldest_stored_osdmap() {
-  return osd->get_superblock().oldest_map;
-}
-
 epoch_t PG::max_oldest_stored_osdmap() {
   return osd->get_superblock().max_oldest_map;
 }
