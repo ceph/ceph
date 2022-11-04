e315587c09d619b86440c53df52d3836fb93eef2
Radoslaw Zarzynski <rzarzyns@redhat.com>
2022-11-04 13:07:13 +0000

    crimson/osd: drop the residue of CompoundPeeringRequest
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/crimson/osd/osd_operation.h
+++ src/crimson/osd/osd_operation.h
@@ -36,7 +36,6 @@ struct ConnectionPipeline {
 enum class OperationTypeCode {
   client_request = 0,
   peering_event,
-  compound_peering_request,
   pg_advance_map,
   pg_creation,
   replicated_request,
@@ -53,7 +52,6 @@ enum class OperationTypeCode {
 static constexpr const char* const OP_NAMES[] = {
   "client_request",
   "peering_event",
-  "compound_peering_request",
   "pg_advance_map",
   "pg_creation",
   "replicated_request",
