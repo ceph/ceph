b49f3d2cdcc7954ea1dc2ce14d2688eb15a97032
Igor Fedotov <igor.fedotov@croit.io>
2022-11-09 17:58:52 +0300

    os/bluestore: output cosmetics for BlueFS
    This includes finer position specification during replay
    and logging read size in hex.
    
    Signed-off-by: Igor Fedotov <igor.fedotov@croit.io>

--- src/os/bluestore/BlueFS.cc
+++ src/os/bluestore/BlueFS.cc
@@ -1310,7 +1310,9 @@ int BlueFS::_replay(bool noop, bool to_stdout)
     }
 
     auto p = t.op_bl.cbegin();
+    auto pos0 = pos;
     while (!p.end()) {
+      pos = pos0 + p.get_off();
       __u8 op;
       decode(op, p);
       switch (op) {
@@ -2132,7 +2134,9 @@ int64_t BlueFS::_read_random(
       buf->pos += r;
     }
   }
-  dout(20) << __func__ << " got " << ret << dendl;
+  dout(20) << __func__ << std::hex
+           << " got 0x" << ret
+           << std::dec  << dendl;
   --h->file->num_reading;
   return ret;
 }
@@ -2257,7 +2261,9 @@ int64_t BlueFS::_read(
     buf->pos += r;
   }
 
-  dout(20) << __func__ << " got " << ret << dendl;
+  dout(20) << __func__ << std::hex
+           << " got 0x" << ret
+           << std::dec  << dendl;
   ceph_assert(!outbl || (int)outbl->length() == ret);
   --h->file->num_reading;
   return ret;
