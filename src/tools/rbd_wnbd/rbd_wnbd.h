4ae4379e83be59395a0743ca154a88d0c33400a6
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2022-11-16 13:24:34 +0200

    rbd-wnbd: optionally handle wnbd adapter restart events
    The WNBD adapter may be reset in certain situations (e.g. driver
    upgrade, MS WHQL tests, etc).
    
    We're going to monitor the WNBD adapter using WMI[1] events, restarting
    the rbd-wnbd disk mappings whenever necessary. Adapter monitoring can be
    enabled by passing the --adapter-monitoring-enabled flag to the service.
    
    This feature is optional for the following reasons:
    
    * it's mainly used during development / driver certification
    * we had to use a relatively small polling interval, which might imply
      additional resource usage. WMI quotas also have to be considered.
    
    While at it, we're updating two lambdas that are submitted to thread pools,
    avoiding default reference capturing and explicitly specifying the variables
    that get copied.
    
    [1] https://learn.microsoft.com/en-us/windows/win32/wmisdk/wmi-start-page
    
    Signed-off-by: Lucian Petrut <lpetrut@cloudbasesolutions.com>
    (cherry picked from commit 0c25ca656430b4d5280ab7d033495ad8ce4e776c)

--- src/tools/rbd_wnbd/rbd_wnbd.h
+++ src/tools/rbd_wnbd/rbd_wnbd.h
@@ -67,6 +67,7 @@ struct Config {
   int service_start_timeout = DEFAULT_SERVICE_START_TIMEOUT;
   int image_map_timeout = DEFAULT_IMAGE_MAP_TIMEOUT;
   bool remap_failure_fatal = false;
+  bool adapter_monitoring_enabled = false;
 
   // TODO: consider moving those fields to a separate structure. Those
   // provide connection information without actually being configurable.
