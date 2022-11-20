0f6f921dd2e4e26b7a411c9969999e4d544ffabb
Matan Breizman <mbreizma@redhat.com>
2022-11-20 10:02:04 +0000

    mon/MonCommands: Support dump_historic_slow_ops
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>
    (cherry picked from commit 8cddd92e153a2e47dadcce1e7e6aa419ea2e4682)

--- src/mon/MonCommands.h
+++ src/mon/MonCommands.h
@@ -1402,6 +1402,10 @@ COMMAND_WITH_FLAG("sessions",
             "mon", "r",
             FLAG(TELL))
 COMMAND_WITH_FLAG("dump_historic_ops",
-            "dump_historic_ops",
+            "show recent ops",
+            "mon", "r",
+            FLAG(TELL))
+COMMAND_WITH_FLAG("dump_historic_slow_ops",
+            "show recent slow ops",
             "mon", "r",
             FLAG(TELL))
