89f4564a1e2041e6b40df824426ecee3fdbd62f9
Vasishta <vipin.indiasmg@gmail.com>
2022-11-22 18:19:24 +0530

    rados: Removing extra tabs to avoid confusions
    Current o/p
    WARNING: pool copy does not preserve user_version, which some     apps may rely on.
    
    Signed-off-by: Vasishta <vipin.indiasmg@gmail.com>

--- src/tools/rados/rados.cc
+++ src/tools/rados/rados.cc
@@ -3118,11 +3118,11 @@ static int rados_tool_common(const std::map < std::string, std::string > &opts,
     }
 
     cerr << "WARNING: pool copy does not preserve user_version, which some "
-	 << "    apps may rely on." << std::endl;
+	 << "apps may rely on." << std::endl;
 
     if (rados.get_pool_is_selfmanaged_snaps_mode(src_pool)) {
       cerr << "WARNING: pool " << src_pool << " has selfmanaged snaps, which are not preserved\n"
-	   << "    by the cppool operation.  This will break any snapshot user."
+	   << "    by the cppool operation. This will break any snapshot user."
 	   << std::endl;
       if (!force) {
 	cerr << "    If you insist on making a broken copy, you can pass\n"
