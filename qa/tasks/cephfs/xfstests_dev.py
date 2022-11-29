231ba4c77391c91c5d3c077b2ee60e5ac5668ac6
Xiubo Li <xiubli@redhat.com>
2022-11-29 21:24:21 +0800

    qa: switch to https protocol for ffsb and xfstests-dev repos
    Since the git protocol is not reachable any more, just switch it
    to https.
    
    Fixes: https://tracker.ceph.com/issues/58133
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- qa/tasks/cephfs/xfstests_dev.py
+++ qa/tasks/cephfs/xfstests_dev.py
@@ -43,12 +43,11 @@ class XFSTestsDev(CephFSTestCase):
 
         # TODO: make sure that repo is not cloned for every test. it should
         # happen only once.
-        remoteurl = 'git://git.ceph.com/xfstests-dev.git'
+        remoteurl = 'https://git.ceph.com/xfstests-dev.git'
         self.repo_path = self.mount_a.client_remote.mkdtemp(suffix=
                                                             'xfstests-dev')
-        self.mount_a.run_shell(['git', 'archive', '--remote=' + remoteurl,
-                                'HEAD', run.Raw('|'),
-                                'tar', '-C', self.repo_path, '-x', '-f', '-'])
+        self.mount_a.run_shell(['git', 'clone', remoteurl, '--depth', '1',
+                                self.repo_path])
 
     def get_admin_key(self):
         import configparser
