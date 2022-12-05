9eb1c903e39472274aed6567c9af68a072984362
Casey Bodley <cbodley@redhat.com>
2022-12-05 17:40:10 -0500

    rgw/sync: add a BidManager to metadata sync
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/store/rados/rgw_sync.h
+++ src/rgw/store/rados/rgw_sync.h
@@ -16,6 +16,7 @@
 #include "rgw_sal_rados.h"
 #include "rgw_sync_trace.h"
 #include "rgw_mdlog.h"
+#include "sync_fairness.h"
 
 #define ERROR_LOGGER_SHARDS 32
 #define RGW_SYNC_ERROR_LOG_SHARD_PREFIX "sync.error-log"
@@ -181,6 +182,7 @@ struct RGWMetaSyncEnv {
   RGWHTTPManager *http_manager{nullptr};
   RGWSyncErrorLogger *error_logger{nullptr};
   RGWSyncTraceManager *sync_tracer{nullptr};
+  rgw::sync_fairness::BidManager* bid_manager{nullptr};
 
   RGWMetaSyncEnv() {}
 
@@ -225,7 +227,7 @@ public:
       http_manager(store->ctx(), completion_mgr),
       status_manager(_sm) {}
 
-  virtual ~RGWRemoteMetaLog() override;
+  ~RGWRemoteMetaLog() override;
 
   int init();
   void finish();
