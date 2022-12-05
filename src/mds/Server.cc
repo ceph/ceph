4fdc59e782ebad0d283b4440f9e686b3717dc3fc
Xiubo Li <xiubli@redhat.com>
2022-12-05 15:49:26 +0800

    mds: send the cap_auths to clients when openning the sessions
    Fixes: https://tracker.ceph.com/issues/57154
    Signed-off-by: Xiubo Li <xiubli@redhat.com>
    (cherry picked from commit c1cbb9acadc311f155b176d6108975835a616a5f)
    
    Conflicts:
    	src/mds/MDSAuthCaps.h: missed dependency commit 23f1fdbcb3d
    	("AuthMonitor: allow "fs authorize" to update caps")

--- src/mds/Server.cc
+++ src/mds/Server.cc
@@ -610,6 +610,7 @@ void Server::handle_client_session(const cref_t<MClientSession> &m)
                                                     session->get_push_seq());
           if (session->info.has_feature(CEPHFS_FEATURE_MIMIC))
             reply->supported_features = supported_features;
+          session->auth_caps.get_cap_auths(&reply->cap_auths);
           mds->send_message_client(reply, session);
           if (mdcache->is_readonly()) {
             auto m = make_message<MClientSession>(CEPH_SESSION_FORCE_RO);
@@ -900,6 +901,7 @@ void Server::_session_logged(Session *session, uint64_t state_seq, bool open, ve
       reply->supported_features = supported_features;
       reply->metric_spec = supported_metric_spec;
     }
+    session->auth_caps.get_cap_auths(&reply->cap_auths);
     mds->send_message_client(reply, session);
     if (mdcache->is_readonly()) {
       auto m = make_message<MClientSession>(CEPH_SESSION_FORCE_RO);
@@ -1056,6 +1058,7 @@ void Server::finish_force_open_sessions(const map<client_t,pair<Session*,uint64_
 	  reply->supported_features = supported_features;
           reply->metric_spec = supported_metric_spec;
 	}
+	session->auth_caps.get_cap_auths(&reply->cap_auths);
 	mds->send_message_client(reply, session);
 
 	if (mdcache->is_readonly())
@@ -1557,6 +1560,7 @@ void Server::handle_client_reconnect(const cref_t<MClientReconnect> &m)
       reply->supported_features = supported_features;
       reply->metric_spec = supported_metric_spec;
     }
+    session->auth_caps.get_cap_auths(&reply->cap_auths);
     mds->send_message_client(reply, session);
     mds->clog->debug() << "reconnect by " << session->info.inst << " after " << delay;
   }
