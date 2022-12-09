scantist-ossops-m2/ceph-ci

rgw: auth_registry uses unique_ptr instead of shared_ptr
Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/rgw_appmain.cc
@@ -390,8 +390,6 @@ int rgw::AppMain::init_frontends2(RGWLib* rgwlib)
    * the dynamic reconfiguration. */
   implicit_tenant_context.reset(new rgw::auth::ImplicitTenants{g_conf()});
   g_conf().add_observer(implicit_tenant_context.get());
-  auto auth_registry =
-    rgw::auth::StrategyRegistry::create(dpp->get_cct(), *(implicit_tenant_context.get()), env.store);
 
   /* allocate a mime table (you'd never guess that from the name) */
   rgw_tools_init(dpp, dpp->get_cct());
@@ -406,7 +404,8 @@ int rgw::AppMain::init_frontends2(RGWLib* rgwlib)
   // initialize RGWProcessEnv
   env.rest = &rest;
   env.olog = olog;
-  env.auth_registry = auth_registry;
+  env.auth_registry = rgw::auth::StrategyRegistry::create(
+      dpp->get_cct(), *implicit_tenant_context, env.store);
   env.ratelimiting = ratelimiter.get();
   env.lua_background = lua_background.get();
 

src/rgw/rgw_auth_registry.h
@@ -84,11 +84,11 @@ public:
     return sts_strategy;
   }
 
-  static std::shared_ptr<StrategyRegistry>
+  static std::unique_ptr<StrategyRegistry>
   create(CephContext* const cct,
          const ImplicitTenants& implicit_tenant_context,
          rgw::sal::Store* store) {
-    return std::make_shared<StrategyRegistry>(cct, implicit_tenant_context, store);
+    return std::make_unique<StrategyRegistry>(cct, implicit_tenant_context, store);
   }
 };
 
@@ -96,6 +96,6 @@ public:
 } /* namespace rgw */
 
 using rgw_auth_registry_t = rgw::auth::StrategyRegistry;
-using rgw_auth_registry_ptr_t = std::shared_ptr<rgw_auth_registry_t>;
+using rgw_auth_registry_ptr_t = std::unique_ptr<rgw_auth_registry_t>;
 
 #endif /* CEPH_RGW_AUTH_REGISTRY_H */

ceph/ceph

src/rgw/rgw_appmain.cc
@@ -390,8 +390,6 @@ int rgw::AppMain::init_frontends2(RGWLib* rgwlib)
    * the dynamic reconfiguration. */
   implicit_tenant_context.reset(new rgw::auth::ImplicitTenants{g_conf()});
   g_conf().add_observer(implicit_tenant_context.get());
-  auto auth_registry =
-    rgw::auth::StrategyRegistry::create(dpp->get_cct(), *implicit_tenant_context, env.driver);
 
   /* allocate a mime table (you'd never guess that from the name) */
   rgw_tools_init(dpp, dpp->get_cct());
@@ -406,7 +404,8 @@ int rgw::AppMain::init_frontends2(RGWLib* rgwlib)
   // initialize RGWProcessEnv
   env.rest = &rest;
   env.olog = olog;
-  env.auth_registry = auth_registry;
+  env.auth_registry = rgw::auth::StrategyRegistry::create(
+      dpp->get_cct(), *implicit_tenant_context, env.driver);
   env.ratelimiting = ratelimiter.get();
   env.lua_background = lua_background.get();
 

src/rgw/rgw_auth_registry.h
@@ -84,11 +84,11 @@ public:
     return sts_strategy;
   }
 
-  static std::shared_ptr<StrategyRegistry>
+  static std::unique_ptr<StrategyRegistry>
   create(CephContext* const cct,
          const ImplicitTenants& implicit_tenant_context,
          rgw::sal::Driver* driver) {
-    return std::make_shared<StrategyRegistry>(cct, implicit_tenant_context, driver);
+    return std::make_unique<StrategyRegistry>(cct, implicit_tenant_context, driver);
   }
 };
 
@@ -96,6 +96,6 @@ public:
 } /* namespace rgw */
 
 using rgw_auth_registry_t = rgw::auth::StrategyRegistry;
-using rgw_auth_registry_ptr_t = std::shared_ptr<rgw_auth_registry_t>;
+using rgw_auth_registry_ptr_t = std::unique_ptr<rgw_auth_registry_t>;
 
 #endif /* CEPH_RGW_AUTH_REGISTRY_H */