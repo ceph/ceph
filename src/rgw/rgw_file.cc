scantist-ossops-m2/ceph-ci

rgw: req_state takes const ref to RGWProcessEnv
make the RGWProcessEnv accessible to any call paths that have a
req_state

Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/rgw_file.cc
@@ -1659,10 +1659,12 @@ namespace rgw {
 	return -EIO;
       }
 
+      const RGWProcessEnv& penv = g_rgwlib->get_fe()->get_process()->get_env();
+
       /* start */
       std::string object_name = relative_object_name();
       f->write_req =
-	new RGWWriteRequest(g_rgwlib->get_store(),
+	new RGWWriteRequest(g_rgwlib->get_store(), penv,
 			    g_rgwlib->get_store()->get_user(fs->get_user()->user_id),
 			    this, bucket_name(), object_name);
       rc = g_rgwlib->get_fe()->start_req(f->write_req);

src/rgw/rgw_file.h
@@ -2489,11 +2489,11 @@ public:
   size_t bytes_written;
   bool eio;
 
-  RGWWriteRequest(rgw::sal::Store* store,
+  RGWWriteRequest(rgw::sal::Store* store, const RGWProcessEnv& penv,
 		  std::unique_ptr<rgw::sal::User> _user,
 		  RGWFileHandle* _fh, const std::string& _bname,
 		  const std::string& _oname)
-    : RGWLibContinuedReq(store->ctx(), std::move(_user)),
+    : RGWLibContinuedReq(store->ctx(), penv, std::move(_user)),
       bucket_name(_bname), obj_name(_oname),
       rgw_fh(_fh), filter(nullptr), timer_id(0), real_ofs(0),
       bytes_written(0), eio(false) {

ceph/ceph

src/rgw/rgw_file.cc
@@ -1659,10 +1659,12 @@ namespace rgw {
 	return -EIO;
       }
 
+      const RGWProcessEnv& penv = g_rgwlib->get_fe()->get_process()->get_env();
+
       /* start */
       std::string object_name = relative_object_name();
       f->write_req =
-	new RGWWriteRequest(g_rgwlib->get_driver(),
+	new RGWWriteRequest(g_rgwlib->get_driver(), penv,
 			    g_rgwlib->get_driver()->get_user(fs->get_user()->user_id),
 			    this, bucket_name(), object_name);
       rc = g_rgwlib->get_fe()->start_req(f->write_req);

src/rgw/rgw_file.h
@@ -2489,11 +2489,11 @@ public:
   size_t bytes_written;
   bool eio;
 
-  RGWWriteRequest(rgw::sal::Driver* driver,
+  RGWWriteRequest(rgw::sal::Driver* driver, const RGWProcessEnv& penv,
 		  std::unique_ptr<rgw::sal::User> _user,
 		  RGWFileHandle* _fh, const std::string& _bname,
 		  const std::string& _oname)
-    : RGWLibContinuedReq(driver->ctx(), std::move(_user)),
+    : RGWLibContinuedReq(driver->ctx(), penv, std::move(_user)),
       bucket_name(_bname), obj_name(_oname),
       rgw_fh(_fh), filter(nullptr), timer_id(0), real_ofs(0),
       bytes_written(0), eio(false) {