scantist-ossops-m2/ceph-ci

rgw: RGWRealmReloader reloads auth_registry
Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/rgw_asio_frontend.cc
@@ -446,7 +446,7 @@ class AsioFrontend {
   void stop();
   void join();
   void pause();
-  void unpause(rgw::sal::Store* store, rgw_auth_registry_ptr_t);
+  void unpause();
 };
 
 unsigned short parse_port(const char *input, boost::system::error_code& ec)
@@ -1114,12 +1114,9 @@ void AsioFrontend::pause()
   }
 }
 
-void AsioFrontend::unpause(rgw::sal::Store* const store,
-                           rgw_auth_registry_ptr_t auth_registry)
+void AsioFrontend::unpause()
 {
-  env.store = store;
-  env.auth_registry = std::move(auth_registry);
-  lua_manager = store->get_lua_manager();
+  lua_manager = env.store->get_lua_manager();
 
   // unpause to unblock connections
   pause_mutex.unlock();
@@ -1178,9 +1175,7 @@ void RGWAsioFrontend::pause_for_new_config()
   impl->pause();
 }
 
-void RGWAsioFrontend::unpause_with_new_config(
-  rgw::sal::Store* const store,
-  rgw_auth_registry_ptr_t auth_registry
-) {
-  impl->unpause(store, std::move(auth_registry));
+void RGWAsioFrontend::unpause_with_new_config()
+{
+  impl->unpause();
 }

src/rgw/rgw_process.h
@@ -115,11 +115,8 @@ public:
     m_tp.pause();
   }
 
-  void unpause_with_new_config(rgw::sal::Store* const store,
-                               rgw_auth_registry_ptr_t auth_registry) {
-    env.store = store;
-    env.auth_registry = std::move(auth_registry);
-    lua_manager = store->get_lua_manager();
+  void unpause_with_new_config() {
+    lua_manager = env.store->get_lua_manager();
     m_tp.unpause();
   }
 

src/rgw/rgw_realm_reloader.cc
@@ -27,9 +27,12 @@
 static constexpr bool USE_SAFE_TIMER_CALLBACKS = false;
 
 
-RGWRealmReloader::RGWRealmReloader(RGWProcessEnv& env, std::map<std::string, std::string>& service_map_meta,
+RGWRealmReloader::RGWRealmReloader(RGWProcessEnv& env,
+                                   const rgw::auth::ImplicitTenants& implicit_tenants,
+                                   std::map<std::string, std::string>& service_map_meta,
                                    Pauser* frontends)
   : env(env),
+    implicit_tenants(implicit_tenants),
     service_map_meta(service_map_meta),
     frontends(frontends),
     timer(env.store->ctx(), mutex, USE_SAFE_TIMER_CALLBACKS),
@@ -171,6 +174,11 @@ void RGWRealmReloader::reload()
   rgw_rest_init(cct, env.store->get_zone()->get_zonegroup());
   ldpp_dout(&dp, 1) << " - usage subsystem init" << dendl;
   rgw_log_usage_init(cct, env.store);
+
+  /* Initialize the registry of auth strategies which will coordinate
+   * the dynamic reconfiguration. */
+  env.auth_registry = rgw::auth::StrategyRegistry::create(
+      cct, implicit_tenants, env.store);
 
   ldpp_dout(&dp, 1) << "Resuming frontends with new realm configuration." << dendl;
 

ceph/ceph

src/rgw/rgw_asio_frontend.cc
@@ -450,7 +450,7 @@ class AsioFrontend {
   void stop();
   void join();
   void pause();
-  void unpause(rgw::sal::Driver* driver, rgw_auth_registry_ptr_t);
+  void unpause();
 };
 
 unsigned short parse_port(const char *input, boost::system::error_code& ec)
@@ -1118,12 +1118,9 @@ void AsioFrontend::pause()
   }
 }
 
-void AsioFrontend::unpause(rgw::sal::Driver* const driver,
-                           rgw_auth_registry_ptr_t auth_registry)
+void AsioFrontend::unpause()
 {
-  env.driver = driver;
-  env.auth_registry = std::move(auth_registry);
-  lua_manager = driver->get_lua_manager();
+  lua_manager = env.driver->get_lua_manager();
 
   // unpause to unblock connections
   pause_mutex.unlock();
@@ -1182,9 +1179,7 @@ void RGWAsioFrontend::pause_for_new_config()
   impl->pause();
 }
 
-void RGWAsioFrontend::unpause_with_new_config(
-  rgw::sal::Driver* const driver,
-  rgw_auth_registry_ptr_t auth_registry
-) {
-  impl->unpause(driver, std::move(auth_registry));
+void RGWAsioFrontend::unpause_with_new_config()
+{
+  impl->unpause();
 }

src/rgw/rgw_process.h
@@ -115,11 +115,8 @@ public:
     m_tp.pause();
   }
 
-  void unpause_with_new_config(rgw::sal::Driver* const driver,
-                               rgw_auth_registry_ptr_t auth_registry) {
-    env.driver = driver;
-    env.auth_registry = std::move(auth_registry);
-    lua_manager = driver->get_lua_manager();
+  void unpause_with_new_config() {
+    lua_manager = env.driver->get_lua_manager();
     m_tp.unpause();
   }
 

src/rgw/rgw_realm_reloader.cc
@@ -27,9 +27,12 @@
 static constexpr bool USE_SAFE_TIMER_CALLBACKS = false;
 
 
-RGWRealmReloader::RGWRealmReloader(RGWProcessEnv& env, std::map<std::string, std::string>& service_map_meta,
+RGWRealmReloader::RGWRealmReloader(RGWProcessEnv& env,
+                                   const rgw::auth::ImplicitTenants& implicit_tenants,
+                                   std::map<std::string, std::string>& service_map_meta,
                                    Pauser* frontends)
   : env(env),
+    implicit_tenants(implicit_tenants),
     service_map_meta(service_map_meta),
     frontends(frontends),
     timer(env.driver->ctx(), mutex, USE_SAFE_TIMER_CALLBACKS),
@@ -171,6 +174,11 @@ void RGWRealmReloader::reload()
   rgw_rest_init(cct, env.driver->get_zone()->get_zonegroup());
   ldpp_dout(&dp, 1) << " - usage subsystem init" << dendl;
   rgw_log_usage_init(cct, env.driver);
+
+  /* Initialize the registry of auth strategies which will coordinate
+   * the dynamic reconfiguration. */
+  env.auth_registry = rgw::auth::StrategyRegistry::create(
+      cct, implicit_tenants, env.driver);
 
   ldpp_dout(&dp, 1) << "Resuming frontends with new realm configuration." << dendl;
 