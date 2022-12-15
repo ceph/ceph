9c14979d0d897c143e7f24ee2ee437678b947dc3
dparmar18 <dparmar@redhat.com>
2022-12-15 17:30:07 +0530

    qa: added test case test_nfs_export_with_invalid_path
    Fixes: https://tracker.ceph.com/issues/58228
    Signed-off-by: Dhairya Parmar <dparmar@redhat.com>
    (cherry picked from commit b0bb25024d8fad2c06138571b830afcea4994be0)

--- qa/tasks/cephfs/test_nfs.py
+++ qa/tasks/cephfs/test_nfs.py
@@ -744,3 +744,17 @@ class TestNFS(MgrTestCase):
         except CommandFailedError as e:
             if e.exitstatus != errno.ENOENT:
                 raise
+
+    def test_nfs_export_with_invalid_path(self):
+        """
+        Test that nfs exports can't be created with invalid path
+        """
+        self._test_create_cluster()
+        try:
+            self._create_export(export_id='123',
+                                extra_cmd=['--pseudo-path', self.pseudo_path,
+                                           '--path', '/non_existent_dir'])
+        except CommandFailedError as e:
+            if e.exitstatus != errno.ENOENT:
+                raise
+        self._test_delete_cluster()
