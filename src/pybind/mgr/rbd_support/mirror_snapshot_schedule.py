9a159fc6398d3707f1ffa313fa079d473342c0df
Ramana Raja <rraja@redhat.com>
2023-01-16 22:04:08 -0500

    mgr/rbd_support: notify the thread waiting on pending snapshot
    ... requests to be completed.
    
    Signed-off-by: Ramana Raja <rraja@redhat.com>
    (cherry picked from commit 38a7e3715f0cee225aa49f3331d85ad37e2b7422)

--- src/pybind/mgr/rbd_support/mirror_snapshot_schedule.py
+++ src/pybind/mgr/rbd_support/mirror_snapshot_schedule.py
@@ -288,6 +288,7 @@ class CreateSnapshotRequests:
 
         with self.lock:
             self.pending.remove(image_spec)
+            self.condition.notify()
             if not self.queue:
                 return
             image_spec = self.queue.pop(0)
