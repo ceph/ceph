af01e206fed46a45153cefff3d006727daee88fc
Pedro Gonzalez Gomez <pegonzal@redhat.com>
2023-01-24 17:19:52 +0100

    mgr/dashboard: added recovery throghput graph, improve promqls, fix inventory's card lines
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/new-dashboard/dashboard-time-selector/dashboard-time-selector.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/new-dashboard/dashboard-time-selector/dashboard-time-selector.component.ts
@@ -18,53 +18,53 @@ export class DashboardTimeSelectorComponent {
     this.times = [
       {
         name: $localize`Last 5 minutes`,
-        value: this.timeToDate(5 * 60)
+        value: this.timeToDate(5 * 60, 1)
       },
       {
         name: $localize`Last 15 minutes`,
-        value: this.timeToDate(15 * 60)
+        value: this.timeToDate(15 * 60, 3)
       },
       {
         name: $localize`Last 30 minutes`,
-        value: this.timeToDate(30 * 60)
+        value: this.timeToDate(30 * 60, 6)
       },
       {
         name: $localize`Last 1 hour`,
-        value: this.timeToDate(3600)
+        value: this.timeToDate(3600, 12)
       },
       {
         name: $localize`Last 3 hours`,
-        value: this.timeToDate(3 * 3600)
+        value: this.timeToDate(3 * 3600, 36)
       },
       {
         name: $localize`Last 6 hours`,
-        value: this.timeToDate(6 * 3600, 30)
+        value: this.timeToDate(6 * 3600, 72)
       },
       {
         name: $localize`Last 12 hours`,
-        value: this.timeToDate(12 * 3600, 60)
+        value: this.timeToDate(12 * 3600, 144)
       },
       {
         name: $localize`Last 24 hours`,
-        value: this.timeToDate(24 * 3600, 120)
+        value: this.timeToDate(24 * 3600, 288)
       },
       {
         name: $localize`Last 2 days`,
-        value: this.timeToDate(48 * 3600, 300)
+        value: this.timeToDate(48 * 3600, 576)
       },
       {
         name: $localize`Last 7 days`,
-        value: this.timeToDate(168 * 3600, 900)
+        value: this.timeToDate(168 * 3600, 2016)
       }
     ];
     this.time = this.times[3].value;
   }
 
   emitTime() {
-    this.selectedTime.emit(this.time);
+    this.selectedTime.emit(this.timeToDate(this.time.end - this.time.start, this.time.step));
   }
 
-  private timeToDate(secondsAgo: number, step: number = 30): any {
+  private timeToDate(secondsAgo: number, step: number): any {
     const date: number = moment().unix() - secondsAgo;
     const dateNow: number = moment().unix();
     const formattedDate: any = {
