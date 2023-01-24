61449f9fe2c168c01b23ae519d04673d855d7d3a
Pedro Gonzalez Gomez <pegonzal@redhat.com>
2023-01-24 17:19:52 +0100

    mgr/dashboard: added recovery throghput graph
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/enum/dashboard-promqls.enum.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/enum/dashboard-promqls.enum.ts
@@ -5,5 +5,6 @@ export enum Promqls {
   READLATENCY = 'avg(ceph_osd_apply_latency_ms)',
   WRITELATENCY = 'avg(ceph_osd_commit_latency_ms)',
   READCLIENTTHROUGHPUT = 'sum(irate(ceph_pool_rd_bytes[1m]))',
-  WRITECLIENTTHROUGHPUT = 'sum(irate(ceph_pool_wr_bytes[1m]))'
+  WRITECLIENTTHROUGHPUT = 'sum(irate(ceph_pool_wr_bytes[1m]))',
+  RECOVERYBYTES = 'ceph_osd_recovery_bytes'
 }
