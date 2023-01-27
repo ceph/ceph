37207b773c1faca2bb36e1ab2b4fcecc65ad510c
dparmar18 <dparmar@redhat.com>
2023-01-27 13:07:47 +0530

    mds: make `scrub status` print flag `scrub_mdsdir`
    and make sure it does not dispose JSON for recursive scrub
    on ~mdsdir while scrubbing with flag 'scrub_mdsdir'
    
    Signed-off-by: Dhairya Parmar <dparmar@redhat.com>

--- src/mds/ScrubStack.cc
+++ src/mds/ScrubStack.cc
@@ -655,6 +655,11 @@ void ScrubStack::scrub_status(Formatter *f) {
     have_more = false;
     auto& header = p.second;
 
+    if (mdcache->get_inode(header->get_origin())->is_mdsdir() 
+    && header->get_scrub_mdsdir() && header->get_tag().empty()) {
+      continue;
+    }
+
     std::string tag(header->get_tag());
     f->open_object_section(tag.c_str()); // scrub id
 
@@ -682,6 +687,12 @@ void ScrubStack::scrub_status(Formatter *f) {
         *optcss << ",";
       }
       *optcss << "force";
+    }
+    if (header->get_scrub_mdsdir()) {
+      if (have_more) {
+        *optcss << ",";
+      }
+      *optcss << "scrub_mdsdir";
     }
 
     f->dump_string("options", optcss->strv());
