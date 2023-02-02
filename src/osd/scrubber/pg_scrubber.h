5aa9b7e517e8738a83e1c4f5a72c35116049235f
Ronen Friedman <rfriedma@redhat.com>
2023-02-02 17:06:46 +0200

    osd/scrub: remove PgScrubber::unregister_from_osd()
    and inline its functionality into its sole caller
    (PgScrubber::rm_from_osd_scrubbing())
    
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- src/osd/scrubber/pg_scrubber.h
+++ src/osd/scrubber/pg_scrubber.h
@@ -887,8 +887,6 @@ class PgScrubber : public ScrubPgIF,
    */
   void request_rescrubbing(requested_scrub_t& req_flags);
 
-  void unregister_from_osd();
-
   /*
    * Select a range of objects to scrub.
    *
