e4bc823b04cede9d0391a9cf328303f466c666c5
Ronen Friedman <rfriedma@redhat.com>
2023-02-02 17:06:46 +0200

    osd/scrub: remove PgScrubber::unregister_from_osd()
    and inline its functionality into its sole caller
    (PgScrubber::rm_from_osd_scrubbing())
    
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- src/osd/scrubber/osd_scrub_sched.h
+++ src/osd/scrubber/osd_scrub_sched.h
@@ -276,6 +276,12 @@ class ScrubQueue {
     }
 
     /**
+     * access the 'state' directly, for when a distinction between 'registered'
+     * and 'unregistering' is needed (both have in_queues() == true)
+     */
+    bool is_state_registered() const { return state == qu_state_t::registered; }
+
+    /**
      * a text description of the "scheduling intentions" of this PG:
      * are we already scheduled for a scrub/deep scrub? when?
      */
--- src/osd/scrubber/pg_scrubber.h
+++ src/osd/scrubber/pg_scrubber.h
@@ -893,8 +893,6 @@ class PgScrubber : public ScrubPgIF,
    */
   void request_rescrubbing(requested_scrub_t& req_flags);
 
-  void unregister_from_osd();
-
   /*
    * Select a range of objects to scrub.
    *
