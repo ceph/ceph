caf5c611151f004364165d23cab42841654b21d7
Pedro Gonzalez Gomez <pegonzal@redhat.com>
2023-02-14 11:21:57 +0100

    mgr/dashboard: add button toggle to switch between new and old landing page
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/dashboard/dashboard.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/dashboard/dashboard.component.spec.ts
@@ -1,23 +1,26 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { FeatureTogglesService } from '~/app/shared/services/feature-toggles.service';
 
 import { configureTestBed } from '~/testing/unit-test-helper';
-import { DeprecatedDashboardComponent } from './dashboard.component';
+import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
-  let component: DeprecatedDashboardComponent;
-  let fixture: ComponentFixture<DeprecatedDashboardComponent>;
+  let component: DashboardComponent;
+  let fixture: ComponentFixture<DashboardComponent>;
 
   configureTestBed({
-    imports: [NgbNavModule],
-    declarations: [DeprecatedDashboardComponent],
+    imports: [NgbNavModule, HttpClientTestingModule],
+    declarations: [DashboardComponent],
+    providers: [FeatureTogglesService],
     schemas: [NO_ERRORS_SCHEMA]
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DeprecatedDashboardComponent);
+    fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
