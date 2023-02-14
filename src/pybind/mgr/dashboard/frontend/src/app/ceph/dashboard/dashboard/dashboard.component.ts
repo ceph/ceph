21dc93ae11db02bc8e0a2175f21fa7dd5e423b00
Pedro Gonzalez Gomez <pegonzal@redhat.com>
2023-02-14 11:21:57 +0100

    mgr/dashboard: add button toggle to switch between new and old landing page
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/dashboard/dashboard.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/dashboard/dashboard/dashboard.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { FeatureTogglesService } from '~/app/shared/services/feature-toggles.service';
 
 @Component({
   selector: 'cd-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DeprecatedDashboardComponent {
-  hasGrafana = false; // TODO: Temporary var, remove when grafana is implemented
+export class DashboardComponent {
+  enabledFeature$: Observable<Object>;
+
+  constructor(private featureToggles: FeatureTogglesService) {
+    this.enabledFeature$ = this.featureToggles.get();
+  }
 }
