780f6f55cd605302e13c0022ef62766edac299f8
Matan Breizman <mbreizma@redhat.com>
2023-03-09 15:09:12 +0000

    qa/tasks/cbt.py: Install collectl manually
    See: d4a04809fd7fd8aaf447005f76a1090db99d75c5
    
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>

--- qa/tasks/cbt.py
+++ qa/tasks/cbt.py
@@ -74,7 +74,19 @@ class CBT(Task):
 
         if system_type == 'rpm':
             install_cmd = ['sudo', 'yum', '-y', 'install']
-            cbt_depends = ['python3-yaml', 'python3-lxml', 'librbd-devel', 'pdsh', 'collectl']
+            cbt_depends = ['python3-yaml', 'python3-lxml', 'librbd-devel', 'pdsh', 'pdsh-rcmd-ssh']
+            self.log.info('Installing collectl')
+            collectl_location = "https://sourceforge.net/projects/collectl/files/collectl/collectl-4.3.1/collectl-4.3.1.src.tar.gz/download"
+            self.first_mon.run(
+                args=[
+                    'sudo', 'mkdir', 'collectl', run.Raw('&&'),
+                    'cd', 'collectl', run.Raw('&&'),
+                    'sudo', 'wget', collectl_location, '-O', 'collectl.tar.gz', run.Raw('&&'),
+                    'sudo', 'tar', '-xvf', 'collectl.tar.gz' , run.Raw('&&'),
+                    'cd', 'collectl-4.3.1', run.Raw('&&'),
+                    'sudo', './INSTALL'
+                ]
+            )
         else:
             install_cmd = ['sudo', 'apt-get', '-y', '--force-yes', 'install']
             cbt_depends = ['python3-yaml', 'python3-lxml', 'librbd-dev', 'collectl']
