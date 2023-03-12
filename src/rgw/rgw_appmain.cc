scantist-ossops-m2/ceph-ci

rgw/main: init_storage() creates a ConfigStore and loads SiteConfig
Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/rgw_appmain.cc
@@ -27,7 +27,8 @@
 #include "include/stringify.h"
 #include "rgw_main.h"
 #include "rgw_common.h"
-#include "rgw_sal_rados.h"
+#include "rgw_sal.h"
+#include "rgw_sal_config.h"
 #include "rgw_period_pusher.h"
 #include "rgw_realm_reloader.h"
 #include "rgw_rest.h"
@@ -87,6 +88,11 @@ namespace {
 }
 
 OpsLogFile* rgw::AppMain::ops_log_file;
+
+rgw::AppMain::AppMain(const DoutPrefixProvider* dpp) : dpp(dpp)
+{
+}
+rgw::AppMain::~AppMain() = default;
 
 void rgw::AppMain::init_frontends1(bool nfs) 
 {
@@ -193,9 +199,20 @@ void rgw::AppMain::init_numa()
   }
 } /* init_numa */
 
-void rgw::AppMain::init_storage()
+int rgw::AppMain::init_storage()
 {
-    auto run_gc =
+  auto config_store_type = g_conf().get_val<std::string>("rgw_config_store");
+  cfgstore = StoreManager::create_config_store(dpp, config_store_type);
+  if (!cfgstore) {
+    return -EIO;
+  }
+
+  int r = site.load(dpp, null_yield, cfgstore.get());
+  if (r < 0) {
+    return r;
+  }
+
+  auto run_gc =
     (g_conf()->rgw_enable_gc_threads &&
       ((!nfs) || (nfs && g_conf()->rgw_nfs_run_gc_threads)));
 
@@ -220,7 +237,10 @@ void rgw::AppMain::init_storage()
           run_sync,
           g_conf().get_val<bool>("rgw_dynamic_resharding"),
           g_conf()->rgw_cache_enabled);
-
+  if (!store) {
+    return -EIO;
+  }
+  return 0;
 } /* init_storage */
 
 void rgw::AppMain::init_perfcounters()
@@ -578,6 +598,7 @@ void rgw::AppMain::shutdown(std::function<void(void)> finalize_async_signals)
     lua_background->shutdown();
   }
 
+  cfgstore.reset(); // deletes
   StoreManager::close_storage(store);
 
   rgw_tools_cleanup();

ceph/ceph

src/rgw/rgw_appmain.cc
@@ -27,7 +27,8 @@
 #include "include/stringify.h"
 #include "rgw_main.h"
 #include "rgw_common.h"
-#include "rgw_sal_rados.h"
+#include "rgw_sal.h"
+#include "rgw_sal_config.h"
 #include "rgw_period_pusher.h"
 #include "rgw_realm_reloader.h"
 #include "rgw_rest.h"
@@ -90,6 +91,11 @@ namespace {
 }
 
 OpsLogFile* rgw::AppMain::ops_log_file;
+
+rgw::AppMain::AppMain(const DoutPrefixProvider* dpp) : dpp(dpp)
+{
+}
+rgw::AppMain::~AppMain() = default;
 
 void rgw::AppMain::init_frontends1(bool nfs) 
 {
@@ -196,9 +202,22 @@ void rgw::AppMain::init_numa()
   }
 } /* init_numa */
 
-void rgw::AppMain::init_storage()
+int rgw::AppMain::init_storage()
 {
-    auto run_gc =
+  auto config_store_type = g_conf().get_val<std::string>("rgw_config_store");
+  cfgstore = DriverManager::create_config_store(dpp, config_store_type);
+  if (!cfgstore) {
+    return -EIO;
+  }
+  env.cfgstore = cfgstore.get();
+
+  int r = site.load(dpp, null_yield, cfgstore.get());
+  if (r < 0) {
+    return r;
+  }
+  env.site = &site;
+
+  auto run_gc =
     (g_conf()->rgw_enable_gc_threads &&
       ((!nfs) || (nfs && g_conf()->rgw_nfs_run_gc_threads)));
 
@@ -224,7 +243,10 @@ void rgw::AppMain::init_storage()
           g_conf().get_val<bool>("rgw_dynamic_resharding"),
           true, // run notification thread
           g_conf()->rgw_cache_enabled);
-
+  if (!env.driver) {
+    return -EIO;
+  }
+  return 0;
 } /* init_storage */
 
 void rgw::AppMain::init_perfcounters()
@@ -586,6 +608,7 @@ void rgw::AppMain::shutdown(std::function<void(void)> finalize_async_signals)
     lua_background->shutdown();
   }
 
+  cfgstore.reset(); // deletes
   DriverManager::close_storage(env.driver);
 
   rgw_tools_cleanup();