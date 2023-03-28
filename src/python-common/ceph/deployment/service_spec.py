9426edb9c94f625179c54fca808d3cab788ab7d8
Teoman ONAY <tonay@ibm.com>
2023-03-28 14:18:14 +0200

    mgr/cephadm: add idmap.conf customization for  NFS server
    Allows idmapd.conf to be customized to control the NFSv4.x server configuration
    
    Fixes: https://tracker.ceph.com/issues/64577
    
    Signed-off-by: Teoman ONAY <tonay@ibm.com>
    (cherry picked from commit 416425c5fde39a9b42c5d70bb6bda3bd576456c3)

--- src/python-common/ceph/deployment/service_spec.py
+++ src/python-common/ceph/deployment/service_spec.py
@@ -1093,6 +1093,7 @@ class NFSServiceSpec(ServiceSpec):
                  enable_haproxy_protocol: bool = False,
                  extra_container_args: Optional[GeneralArgList] = None,
                  extra_entrypoint_args: Optional[GeneralArgList] = None,
+                 idmap_conf: Optional[Dict[str, Dict[str, str]]] = None,
                  custom_configs: Optional[List[CustomConfig]] = None,
                  ):
         assert service_type == 'nfs'
@@ -1105,6 +1106,7 @@ class NFSServiceSpec(ServiceSpec):
         self.port = port
         self.virtual_ip = virtual_ip
         self.enable_haproxy_protocol = enable_haproxy_protocol
+        self.idmap_conf = idmap_conf
 
     def get_port_start(self) -> List[int]:
         if self.port:
