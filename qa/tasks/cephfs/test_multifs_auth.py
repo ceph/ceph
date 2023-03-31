3ac6ffac72643d9bcb003b5667e5315d8cd0739a
Rishabh Dave <ridave@redhat.com>
2023-04-01 00:44:52 +0530

    qa/cephfs: improve caps_helper.CapTester
    Improvement #1:
    
    CapTester.write_test_files() not only creates the test file but also
    does the following for every mount object it receives in parameters -
    
    * carefully produces the path for the test file as per parameters
      received
    * generates the unique data for each test file on a CephFS mount
    * creates a data structure -- list of lists -- that holds all this
      information along with mount object itself for each mount object so
      that tests can be conducted at a later point
    
    Untangle this mess of code by splitting this method into 3 separate
    methods -
    
    1. To produce the path for test file (as per user's need).
    2. To generate the data that will be written into the test file.
    3. To actually create the test file on CephFS.
    
    Improvement #2:
    
    Remove the internal data structure used for testing -- self.test_set --
    and use separate class attributes to store all the data required for
    testing instead of a tuple. This serves two purpose -
    
    One, it makes it easy to manipulate all this data from helper methods
    and during debugging session, especially while using a PDB session.
    
    And two, make it impossible to have multiple mounts/multiple "test sets"
    within same CapTester instance for the sake of simplicity. Users can
    instead create two instances of CapTester instances if needed.
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>
    (cherry picked from commit 008dbe91e2994a654cba37e07ca6a9ae7c98fa2f)

--- qa/tasks/cephfs/test_multifs_auth.py
+++ qa/tasks/cephfs/test_multifs_auth.py
@@ -23,8 +23,6 @@ class TestMultiFS(CephFSTestCase):
     def setUp(self):
         super(TestMultiFS, self).setUp()
 
-        self.captester = CapTester()
-
         # we might have it - the client - if the same cluster was used for a
         # different vstart_runner.py run.
         self.run_cluster_cmd(f'auth rm {self.client_name}')
@@ -42,18 +40,21 @@ class TestMultiFS(CephFSTestCase):
 class TestMONCaps(TestMultiFS):
 
     def test_moncap_with_one_fs_names(self):
+        self.captester = CapTester()
         moncap = gen_mon_cap_str((('r', self.fs1.name),))
         self.create_client(self.client_id, moncap)
 
         self.captester.run_mon_cap_tests(self.fs1, self.client_id)
 
     def test_moncap_with_multiple_fs_names(self):
+        self.captester = CapTester()
         moncap = gen_mon_cap_str((('r', self.fs1.name), ('r', self.fs2.name)))
         self.create_client(self.client_id, moncap)
 
         self.captester.run_mon_cap_tests(self.fs1, self.client_id)
 
     def test_moncap_with_blanket_allow(self):
+        self.captester = CapTester()
         moncap = 'allow r'
         self.create_client(self.client_id, moncap)
 
@@ -69,53 +70,55 @@ class TestMDSCaps(TestMultiFS):
     3. Remount the current mounts with this new client.
     4. Test read and write on both FSs.
     """
-    def setUp(self):
-        super(self.__class__, self).setUp()
-        self.mounts = (self.mount_a, self.mount_b)
-
     def test_rw_with_fsname_and_no_path_in_cap(self):
         PERM = 'rw'
-        self.captester.write_test_files(self.mounts)
+        self.captesters = (CapTester(self.mount_a), CapTester(self.mount_b))
 
         moncap, osdcap, mdscap = self._gen_caps(PERM, both_fsnames=True)
         keyring = self.create_client(self.client_id, moncap, osdcap, mdscap)
         self.remount_with_new_client(keyring)
 
-        self.captester.run_mds_cap_tests(PERM)
+        self.captesters[0].run_mds_cap_tests(PERM)
+        self.captesters[1].run_mds_cap_tests(PERM)
 
     def test_r_with_fsname_and_no_path_in_cap(self):
         PERM = 'r'
-        self.captester.write_test_files(self.mounts)
+        self.captesters = (CapTester(self.mount_a), CapTester(self.mount_b))
 
         moncap, osdcap, mdscap = self._gen_caps(PERM, both_fsnames=True)
         keyring = self.create_client(self.client_id, moncap, osdcap, mdscap)
         self.remount_with_new_client(keyring)
 
-        self.captester.run_mds_cap_tests(PERM)
+        self.captesters[0].run_mds_cap_tests(PERM)
+        self.captesters[1].run_mds_cap_tests(PERM)
 
     def test_rw_with_fsname_and_path_in_cap(self):
         PERM, CEPHFS_MNTPT = 'rw', 'dir1'
         self.mount_a.run_shell(f'mkdir {CEPHFS_MNTPT}')
         self.mount_b.run_shell(f'mkdir {CEPHFS_MNTPT}')
-        self.captester.write_test_files(self.mounts, CEPHFS_MNTPT)
+        self.captesters = (CapTester(self.mount_a, CEPHFS_MNTPT),
+                           CapTester(self.mount_b, CEPHFS_MNTPT))
 
         moncap, osdcap, mdscap = self._gen_caps(PERM, True, CEPHFS_MNTPT)
         keyring = self.create_client(self.client_id, moncap, osdcap, mdscap)
         self.remount_with_new_client(keyring, CEPHFS_MNTPT)
 
-        self.captester.run_mds_cap_tests(PERM, CEPHFS_MNTPT)
+        self.captesters[0].run_mds_cap_tests(PERM, CEPHFS_MNTPT)
+        self.captesters[1].run_mds_cap_tests(PERM, CEPHFS_MNTPT)
 
     def test_r_with_fsname_and_path_in_cap(self):
         PERM, CEPHFS_MNTPT = 'r', 'dir1'
         self.mount_a.run_shell(f'mkdir {CEPHFS_MNTPT}')
         self.mount_b.run_shell(f'mkdir {CEPHFS_MNTPT}')
-        self.captester.write_test_files(self.mounts, CEPHFS_MNTPT)
+        self.captesters = (CapTester(self.mount_a, CEPHFS_MNTPT),
+                           CapTester(self.mount_b, CEPHFS_MNTPT))
 
         moncap, osdcap, mdscap = self._gen_caps(PERM, True, CEPHFS_MNTPT)
         keyring = self.create_client(self.client_id, moncap, osdcap, mdscap)
         self.remount_with_new_client(keyring, CEPHFS_MNTPT)
 
-        self.captester.run_mds_cap_tests(PERM, CEPHFS_MNTPT)
+        self.captesters[0].run_mds_cap_tests(PERM, CEPHFS_MNTPT)
+        self.captesters[1].run_mds_cap_tests(PERM, CEPHFS_MNTPT)
 
     # XXX: this tests the backward compatibility; "allow rw path=<dir1>" is
     # treated as "allow rw fsname=* path=<dir1>"
@@ -123,13 +126,15 @@ class TestMDSCaps(TestMultiFS):
         PERM, CEPHFS_MNTPT = 'rw', 'dir1'
         self.mount_a.run_shell(f'mkdir {CEPHFS_MNTPT}')
         self.mount_b.run_shell(f'mkdir {CEPHFS_MNTPT}')
-        self.captester.write_test_files(self.mounts, CEPHFS_MNTPT)
+        self.captesters = (CapTester(self.mount_a, CEPHFS_MNTPT),
+                           CapTester(self.mount_b, CEPHFS_MNTPT))
 
         moncap, osdcap, mdscap = self._gen_caps(PERM, False, CEPHFS_MNTPT)
         keyring = self.create_client(self.client_id, moncap, osdcap, mdscap)
         self.remount_with_new_client(keyring, CEPHFS_MNTPT)
 
-        self.captester.run_mds_cap_tests(PERM, CEPHFS_MNTPT)
+        self.captesters[0].run_mds_cap_tests(PERM, CEPHFS_MNTPT)
+        self.captesters[1].run_mds_cap_tests(PERM, CEPHFS_MNTPT)
 
     # XXX: this tests the backward compatibility; "allow r path=<dir1>" is
     # treated as "allow r fsname=* path=<dir1>"
@@ -137,33 +142,37 @@ class TestMDSCaps(TestMultiFS):
         PERM, CEPHFS_MNTPT = 'r', 'dir1'
         self.mount_a.run_shell(f'mkdir {CEPHFS_MNTPT}')
         self.mount_b.run_shell(f'mkdir {CEPHFS_MNTPT}')
-        self.captester.write_test_files(self.mounts, CEPHFS_MNTPT)
+        self.captesters = (CapTester(self.mount_a, CEPHFS_MNTPT),
+                           CapTester(self.mount_b, CEPHFS_MNTPT))
 
         moncap, osdcap, mdscap = self._gen_caps(PERM, False, CEPHFS_MNTPT)
         keyring = self.create_client(self.client_id, moncap, osdcap, mdscap)
         self.remount_with_new_client(keyring, CEPHFS_MNTPT)
 
-        self.captester.run_mds_cap_tests(PERM, CEPHFS_MNTPT)
+        self.captesters[0].run_mds_cap_tests(PERM, CEPHFS_MNTPT)
+        self.captesters[1].run_mds_cap_tests(PERM, CEPHFS_MNTPT)
 
     def test_rw_with_no_fsname_and_no_path(self):
         PERM = 'rw'
-        self.captester.write_test_files(self.mounts)
+        self.captesters = (CapTester(self.mount_a), CapTester(self.mount_b))
 
         moncap, osdcap, mdscap = self._gen_caps(PERM)
         keyring = self.create_client(self.client_id, moncap, osdcap, mdscap)
         self.remount_with_new_client(keyring)
 
-        self.captester.run_mds_cap_tests(PERM)
+        self.captesters[0].run_mds_cap_tests(PERM)
+        self.captesters[1].run_mds_cap_tests(PERM)
 
     def test_r_with_no_fsname_and_no_path(self):
         PERM = 'r'
-        self.captester.write_test_files(self.mounts)
+        self.captesters = (CapTester(self.mount_a), CapTester(self.mount_b))
 
         moncap, osdcap, mdscap = self._gen_caps(PERM)
         keyring = self.create_client(self.client_id, moncap, osdcap, mdscap)
         self.remount_with_new_client(keyring)
 
-        self.captester.run_mds_cap_tests(PERM)
+        self.captesters[0].run_mds_cap_tests(PERM)
+        self.captesters[1].run_mds_cap_tests(PERM)
 
     def tearDown(self):
         self.mount_a.umount_wait()
