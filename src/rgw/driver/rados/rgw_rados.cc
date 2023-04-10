a66f8283ac4a07a8cf17ff1d2085c6a6e7deb672
J. Eric Ivancich <ivancich@redhat.com>
2023-04-10 13:40:32 -0400

    rgw: dencode "removed" flag in RGWOLHInfo
    When an OLH structure was dencoded the "removed" flag was not
    included. That flag specifies whether the OLH is a delete marker. This
    adds that flag to the dencoding.
    
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>

--- src/rgw/driver/rados/rgw_rados.cc
+++ src/rgw/driver/rados/rgw_rados.cc
@@ -10073,6 +10073,7 @@ void RGWOLHInfo::generate_test_instances(list<RGWOLHInfo*> &o)
 void RGWOLHInfo::dump(Formatter *f) const
 {
   encode_json("target", target, f);
+  encode_json("removed", removed, f);
 }
 
 void RGWOLHPendingInfo::dump(Formatter *f) const
