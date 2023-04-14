1d3c55463b16f203f7f4c7dcad6999abc3d63797
Nizamudeen A <nia@redhat.com>
2023-04-14 11:33:16 +0530

    mgr/dashboard: fix cephadm e2e expression changed error
    tried to fix this issue from the daemon component sometime ago several
    times but it didn't work. So force ignoring the error
    
    Fixes: https://tracker.ceph.com/issues/59444
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- src/pybind/mgr/dashboard/frontend/cypress/support/index.ts
+++ src/pybind/mgr/dashboard/frontend/cypress/support/index.ts
@@ -10,7 +10,8 @@ afterEach(() => {
 Cypress.on('uncaught:exception', (err: Error) => {
   if (
     err.message.includes('ResizeObserver loop limit exceeded') ||
-    err.message.includes('api/prometheus/rules')
+    err.message.includes('api/prometheus/rules') ||
+    err.message.includes('NG0100: ExpressionChangedAfterItHasBeenCheckedError')
   ) {
     return false;
   }
