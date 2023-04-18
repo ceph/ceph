cdec4196c4c72a0a109ee34c0e72b1a708d31dc6
Pere Diaz Bou <pere-altea@hotmail.com>
2023-04-18 08:41:10 +0200

    mgr/prometheus: remove jeje log
    Probably the greatest log ever assembled in ceph is being deprecated in
    this PR.
    
    RIP 2022/11/11 - 2023/04/18
    
    Signed-off-by: Pere Diaz Bou <pere-altea@hotmail.com>

--- src/pybind/mgr/prometheus/module.py
+++ src/pybind/mgr/prometheus/module.py
@@ -1613,7 +1613,6 @@ class Module(MgrModule):
 
     def get_all_daemon_health_metrics(self) -> None:
         daemon_metrics = self.get_daemon_health_metrics()
-        self.log.debug('metrics jeje %s' % (daemon_metrics))
         for daemon_name, health_metrics in daemon_metrics.items():
             for health_metric in health_metrics:
                 path = f'daemon_health_metrics{daemon_name}{health_metric["type"]}'
