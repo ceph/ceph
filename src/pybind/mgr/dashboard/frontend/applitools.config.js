f31053b5fa149082d656b4c7e403183bc0124003
Nizamudeen A <nia@redhat.com>
2023-04-20 13:05:46 +0530

    mgr/dashboard: ignore Details card in applitools vrt
    because the details card includes the FSID and Ceph Version which can
    change on different clusters.
    
    also don't run the 800x600 resolution because for new dashboard it looks
    really weird.
    
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- src/pybind/mgr/dashboard/frontend/applitools.config.js
+++ src/pybind/mgr/dashboard/frontend/applitools.config.js
@@ -10,9 +10,7 @@ module.exports = {
   apiKey: process.env.APPLITOOLS_API_KEY,
   browser: [
     { width: 1920, height: 1080, name: 'chrome' },
-    { width: 1920, height: 1080, name: 'firefox' },
-    { width: 800, height: 600, name: 'chrome' },
-    { width: 800, height: 600, name: 'firefox' }
+    { width: 1920, height: 1080, name: 'firefox' }
   ],
   showLogs: false,
   saveDebugData: true,
