scantist-ossops-m2/ceph-ci

cirmson/os: seperate futurizedStore interface
into futurizedStore and futurizedShardStore
and modification the other parts in osd and objectstore

Signed-off-by: chunmei <chunmei.liu@intel.com>

src/crimson/osd/shard_services.cc
@@ -40,7 +40,7 @@ PerShardState::PerShardState(
   PerfCounters *recoverystate_perf,
   crimson::os::FuturizedStore &store)
   : whoami(whoami),
-    store(store),
+    store(store.get_local_shard_store()),
     perf(perf), recoverystate_perf(recoverystate_perf),
     throttler(crimson::common::local_conf()),
     next_tid(

ceph/ceph

src/crimson/osd/shard_services.cc
@@ -40,7 +40,7 @@ PerShardState::PerShardState(
   PerfCounters *recoverystate_perf,
   crimson::os::FuturizedStore &store)
   : whoami(whoami),
-    store(store),
+    store(store.get_sharded_store()),
     perf(perf), recoverystate_perf(recoverystate_perf),
     throttler(crimson::common::local_conf()),
     next_tid(