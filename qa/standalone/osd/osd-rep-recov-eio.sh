4c22fcfbe8df5dc60b53330097158ca62a0f358b
Sridhar Seshasayee <sseshasa@redhat.com>
2023-04-29 10:46:58 +0530

    qa/: Override mClock profile to 'high_recovery_ops' for qa tests
    The qa tests are not client I/O centric and mostly focus on triggering
    recovery/backfills and monitor them for completion within a finite amount
    of time. The same holds true for scrub operations.
    
    Therefore, an mClock profile that optimizes background operations is a
    better fit for qa related tests. The osd_mclock_profile is therefore
    globally overriden to 'high_recovery_ops' profile for the Rados suite as
    it fits the requirement.
    
    Also, many standalone tests expect recovery and scrub operations to
    complete within a finite time. To ensure this, the osd_mclock_profile
    options is set to 'high_recovery_ops' as part of the run_osd() function
    in ceph-helpers.sh.
    
    A subset of standalone tests explicitly used 'high_recovery_ops' profile.
    Since the profile is now set as part of run_osd(), the earlier overrides
    are redundant and therefore removed from the tests.
    
    Signed-off-by: Sridhar Seshasayee <sseshasa@redhat.com>

--- qa/standalone/osd/osd-rep-recov-eio.sh
+++ qa/standalone/osd/osd-rep-recov-eio.sh
@@ -29,7 +29,6 @@ function run() {
     export CEPH_ARGS
     CEPH_ARGS+="--fsid=$(uuidgen) --auth-supported=none "
     CEPH_ARGS+="--mon-host=$CEPH_MON "
-    CEPH_ARGS+="--osd-mclock-profile=high_recovery_ops "
 
 
     local funcs=${@:-$(set | sed -n -e 's/^\(TEST_[0-9a-z_]*\) .*/\1/p')}
