e39ca6b6b8a9a3c36820b099d7e3798a9fc257e5
Yuval Lifshitz <ylifshit@redhat.com>
2023-04-30 13:31:19 +0000

    Revert "tracer/osd/librados/build/rgw: rgw and osd end2end tracing using opentelemetry"
    This reverts commit 5d4c5e65be94a346e62b9f8ac63241779c4c7a6a.
    
    Signed-off-by: Yuval Lifshitz <ylifshit@redhat.com>

--- cmake/modules/BuildOpentelemetry.cmake
+++ cmake/modules/BuildOpentelemetry.cmake
@@ -82,5 +82,4 @@ function(build_opentelemetry)
     PROPERTIES
       INTERFACE_LINK_LIBRARIES "${opentelemetry_deps}"
       INTERFACE_INCLUDE_DIRECTORIES "${opentelemetry_include_dir}")
-  include_directories(SYSTEM "${opentelemetry_include_dir}")
 endfunction()
--- src/rgw/rgw_aio.h
+++ src/rgw/rgw_aio.h
@@ -96,7 +96,7 @@ class Aio {
                             optional_yield y);
   static OpFunc librados_op(librados::IoCtx ctx,
                             librados::ObjectWriteOperation&& op,
-                            optional_yield y, jspan_context *trace_ctx = nullptr);
+                            optional_yield y);
   static OpFunc d3n_cache_op(const DoutPrefixProvider *dpp, optional_yield y,
                              off_t read_ofs, off_t read_len, std::string& location);
 };
