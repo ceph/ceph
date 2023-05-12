4992e8f0300860ee4bb58f2cc186373aaad252f5
Yehuda Sadeh <yehuda@redhat.com>
2023-05-12 15:09:24 -0400

    fscrypt: hkdf calc function
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

--- src/client/FSCrypt.h
+++ src/client/FSCrypt.h
@@ -3,6 +3,10 @@
 #include "fscrypt_uapi.h"
 
 
+int fscrypt_calc_hkdf(const char *salt, int salt_len,
+                      const char *key, int key_len,
+                      char *dest, int dest_len);
+
 class FSCryptKey {
 public:
   int init(const struct fscrypt_key_specifier& key_spec);
