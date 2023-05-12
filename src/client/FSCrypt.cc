391fbdb6ab03f166f7d2a773c7a97c26c95ca6f6
Yehuda Sadeh <yehuda@redhat.com>
2023-05-12 13:24:31 -0400

    fscypt: userspace client support for add key ioctl (initial)
    Signed-off-by: Yehuda Sadeh <yehuda@redhat.com>

---
+++ src/client/FSCrypt.cc
@@ -0,0 +1,20 @@
+
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2023 IBM
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+#include "common/errno.h"
+#include "common/safe_io.h"
+#include "common/config.h"
+#include "include/ceph_assert.h"
+
+#include "client/FSCrypt.h"
