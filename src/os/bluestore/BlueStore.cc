784600930d1a4bd72d0a8eb446f5cd549af26b5d
Radosław Zarzyński <rzarzyns@redhat.com>
2023-05-14 14:12:37 +0200

    os/bluestore: get rid of inclusion-in-the-middle from BlueStore.cc
    Signed-off-by: Radosław Zarzyński <rzarzyns@redhat.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -49,6 +49,7 @@
 #include "common/blkdev.h"
 #include "common/numa.h"
 #include "common/pretty_binary.h"
+#include "common/WorkQueue.h"
 #include "kv/KeyValueHistogram.h"
 
 #ifdef HAVE_LIBZBD
@@ -9551,8 +9552,6 @@ BlueStore::OnodeRef BlueStore::fsck_check_objects_shallow(
 
   return o;
 }
-
-#include "common/WorkQueue.h"
 
 class ShallowFSCKThreadPool : public ThreadPool
 {
@@ -20328,8 +20327,6 @@ int BlueStore::read_allocation_from_drive_on_startup()
 // Not meant to be run by customers
 #ifdef CEPH_BLUESTORE_TOOL_RESTORE_ALLOCATION
 
-#include <stdlib.h>
-#include <algorithm>
 //---------------------------------------------------------
 int cmpfunc (const void * a, const void * b)
 {
