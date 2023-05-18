3524be227158f05f406679d409862832f68c7a87
Pedro Gonzalez Gomez <pegonzal@redhat.com>
2023-05-18 13:47:16 +0200

    mgr/dashboard: fix Nan undefined values and add empty pipe on expand-cluster
    Fixes: https://tracker.ceph.com/issues/61207
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@redhat.com>
    (cherry picked from commit 764a1b67392f09a288a6f38d9d4f89d2a203cf57)

--- src/pybind/mgr/dashboard/frontend/src/app/shared/services/formatter.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/formatter.service.ts
@@ -13,6 +13,9 @@ export class FormatterService {
     if (!_.isNumber(n)) {
       return '-';
     }
+    if (_.isNaN(n)) {
+      return 'N/A';
+    }
     let unit = n < 1 ? 0 : Math.floor(Math.log(n) / Math.log(divisor));
     unit = unit >= units.length ? units.length - 1 : unit;
     let result = _.round(n / Math.pow(divisor, unit), decimals).toString();
