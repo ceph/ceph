769b7cb8dcc355e35ef80c7de73879afc2db57c3
Samarah <samarah.uriarte@ibm.com>
2023-05-25 14:58:26 -0400

    rgw/d4n: This commit squashes the following commits related to d4n policy and filter driver.
    RGW: Add cache driver base class - add d4n filter driver.
    RGW: Update cache and policy files; add RedisDriver into D4N filter
    RGW: Add D4N classes and unit testing; update cpp_redis submodule
    QA: Add D4N teuthology suite
    RGW: Improve D4N readability and structure
    RGW: Add base D4N policy and test
    RGW: Add GWF policy to D4N
    RGW: Add global weight property
    RGW: Added D4N namespace
    RGW: Update policy driver interface
    RGW: Update unit tests
    RGW: Add Address struct to all files
    RGW: Update D4N names and structure
    RGW: Update structure and fix errors
    RGW: Add more features to policy driver
    RGW: Access local weight in policy code
    RGW: Work on D4N workflows
    RGW: Add object directory class and remove copy_value method
    RGW: Establish object directory in read workflow
    RGW: Update cache and policy files; add RedisDriver into D4N filter
    RGW: Switch out D4N cache methods with Redis driver methods
    RGW: Update RedisDriver to match new CacheDriver structure; define set_attrs method
    RGW: Fix D4N read workflow crashes
    RGW: Update D4N files to match CacheDriver changes
    RGW: fix key_exists method for RedisDriver and clean up rgw_sal_d4n.cc
    RGW: Use correct get_block method
    RGW: Make CachePolicy a virtual class
    RGW: Initialize localWeight if not found and develop find_victim method
    RGW: Debugging network failure
    RGW: Rebase and debugging network failure
    RGW: Update RedisDriver::list_entries and usage in D4N policy driver
    RGW: Fix network failure issue; add entries and entry methods
    RGW: Update D4N test files to match rebase
    RGW: Update D4N policy and RedisDriver with entries
    RGW: make localWeight an attribute
    rgw/cache: commit to fix compilation issues.
    
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/rgw/rgw_redis_driver.h
+++ src/rgw/rgw_redis_driver.h
@@ -10,21 +10,39 @@
 
 namespace rgw { namespace cache {
 
+class RedisDriver;
+
+class RedisCacheAioRequest: public CacheAioRequest {
+public:
+  RedisCacheAioRequest(RedisDriver* cache_driver) : cache_driver(cache_driver) {}
+  virtual ~RedisCacheAioRequest() = default;
+  virtual void cache_aio_read(const DoutPrefixProvider* dpp, optional_yield y, const std::string& key, off_t ofs, uint64_t len, rgw::Aio* aio, rgw::AioResult& r) override;
+  virtual void cache_aio_write(const DoutPrefixProvider* dpp, optional_yield y, const std::string& key, bufferlist& bl, uint64_t len, rgw::Aio* aio, rgw::AioResult& r) override;
+private:
+  RedisDriver* cache_driver;
+};
+
 class RedisDriver : public CacheDriver {
   private:
     cpp_redis::client client;
+    rgw::d4n::Address addr;
     std::unordered_map<std::string, Entry> entries;
 
+    int find_client(const DoutPrefixProvider* dpp);
     int insert_entry(const DoutPrefixProvider* dpp, std::string key, off_t offset, uint64_t len);
     int remove_entry(const DoutPrefixProvider* dpp, std::string key);
     std::optional<Entry> get_entry(const DoutPrefixProvider* dpp, std::string key);
 
   public:
-    RedisDriver(Partition& _partition_info, std::string host, int port) : CacheDriver() {}
+    RedisDriver(Partition& _partition_info, std::string host, int port) : CacheDriver() {
+      addr.host = host;
+      addr.port = port;
+    }
 
     virtual int initialize(CephContext* cct, const DoutPrefixProvider* dpp) override;
     virtual int put(const DoutPrefixProvider* dpp, const std::string& key, bufferlist& bl, uint64_t len, rgw::sal::Attrs& attrs) override;
     virtual int get(const DoutPrefixProvider* dpp, const std::string& key, off_t offset, uint64_t len, bufferlist& bl, rgw::sal::Attrs& attrs) override;
+    virtual rgw::AioResultList get_async (const DoutPrefixProvider* dpp, optional_yield y, rgw::Aio* aio, const std::string& key, off_t ofs, uint64_t len, uint64_t cost, uint64_t id) override;
     virtual int append_data(const DoutPrefixProvider* dpp, const::std::string& key, bufferlist& bl_data) override;
     virtual int delete_data(const DoutPrefixProvider* dpp, const::std::string& key) override;
     virtual int get_attrs(const DoutPrefixProvider* dpp, const std::string& key, rgw::sal::Attrs& attrs) override;
@@ -38,10 +56,13 @@ class RedisDriver : public CacheDriver {
     virtual bool key_exists(const DoutPrefixProvider* dpp, const std::string& key) override;
     virtual std::vector<Entry> list_entries(const DoutPrefixProvider* dpp) override;
     virtual size_t get_num_entries(const DoutPrefixProvider* dpp) override;
+    int update_local_weight(const DoutPrefixProvider* dpp, std::string key, int localWeight); // may need to exist for base class -Sam
 
     /* Partition */
     virtual Partition get_current_partition_info(const DoutPrefixProvider* dpp) override;
     virtual uint64_t get_free_space(const DoutPrefixProvider* dpp) override;
+
+    virtual std::unique_ptr<CacheAioRequest> get_cache_aio_request_ptr(const DoutPrefixProvider* dpp) override;
 };
 
 } } // namespace rgw::cal
