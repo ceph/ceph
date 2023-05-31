464dd4813cbd6ea33e19c7b2e28cbf11d640d75d
Cory Snyder <csnyder@1111systems.com>
2023-05-31 18:01:02 +0000

    rgw: cancel olh modification by removing pending xattrs in set_olh error scenarios
    Makes request threads clean up after themselves and remove their pending
    xattrs from OLH objects before they return an error from set_olh.
    
    Fixes: https://tracker.ceph.com/issues/59663
    Signed-off-by: Cory Snyder <csnyder@1111systems.com>

--- src/rgw/driver/rados/rgw_rados.h
+++ src/rgw/driver/rados/rgw_rados.h
@@ -1315,6 +1315,7 @@ public:
                              const DoutPrefixProvider *dpp);
 
   void bucket_index_guard_olh_op(const DoutPrefixProvider *dpp, RGWObjState& olh_state, librados::ObjectOperation& op);
+  void olh_cancel_modification(const DoutPrefixProvider *dpp, const RGWBucketInfo& bucket_info, RGWObjState& state, const rgw_obj& olh_obj, const std::string& op_tag, optional_yield y);
   int olh_init_modification(const DoutPrefixProvider *dpp, const RGWBucketInfo& bucket_info, RGWObjState& state, const rgw_obj& olh_obj, std::string *op_tag, optional_yield y);
   int olh_init_modification_impl(const DoutPrefixProvider *dpp, const RGWBucketInfo& bucket_info, RGWObjState& state, const rgw_obj& olh_obj, std::string *op_tag, optional_yield y);
   int bucket_index_link_olh(const DoutPrefixProvider *dpp,
