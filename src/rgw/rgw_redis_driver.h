aa25b2456f1dbbaeb2ed136e45ba4cd7872cbdb4
Samarah <samarah.uriarte@ibm.com>
2023-05-31 14:56:04 -0400

    RGW: Update cache driver and create redis cache driver files
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

---
+++ src/rgw/rgw_redis_driver.h
@@ -0,0 +1,35 @@
+#ifndef CEPH_REDISDRIVER_H
+#define CEPH_REDISDRIVER_H
+
+#include <string>
+#include <iostream>
+#include <cpp_redis/cpp_redis>
+#include "rgw_common.h"
+#include "rgw_cache_driver.h"
+#include "driver/d4n/d4n_directory.h"
+
+class RedisDriver : public CacheDriver {
+  private:
+    cpp_redis::client client;
+    rgw::d4n::Address addr;
+
+  public:
+    RedisDriver(Partition& _partition_info, std::string host, int port) : CacheDriver(_partition_info) {
+      addr.host = host;
+      addr.port = port;
+    }
+
+    virtual int initialize(CephContext* cct, const DoutPrefixProvider* dpp) override;
+    virtual bool key_exists(const DoutPrefixProvider* dpp, std::string& key) override;
+    virtual int put(const DoutPrefixProvider* dpp, const std::string& key, bufferlist& bl, uint64_t len, rgw::sal::Attrs* attrs) override;
+    virtual int get(const DoutPrefixProvider* dpp, const std::string& key, off_t offset, uint64_t len, bufferlist& bl, rgw::sal::Attrs* attrs) override;
+    virtual int append_data(const DoutPrefixProvider* dpp, const::std::string& key, bufferlist& bl_data) override;
+    virtual int delete_data(const DoutPrefixProvider* dpp, const::std::string& key) override;
+    virtual int get_attrs(const DoutPrefixProvider* dpp, const std::string& key, rgw::sal::Attrs& attrs) override;
+    virtual int update_attrs(const DoutPrefixProvider* dpp, const std::string& key, rgw::sal::Attrs& attrs) override;
+    virtual int delete_attrs(const DoutPrefixProvider* dpp, const std::string& key, rgw::sal::Attrs& del_attrs) override;
+    virtual int get_attr(const DoutPrefixProvider* dpp, const std::string& key, const std::string& attr_name, std::string& attr_value) override;
+    virtual int set_attr(const DoutPrefixProvider* dpp, const std::string& key, const std::string& attr_name, const std::string& attr_val) override;
+};
+
+#endif
