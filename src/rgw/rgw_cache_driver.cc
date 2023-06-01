51e653a8b96c91dd16c716848e2e5dca0f179ef3
Samarah <samarah.uriarte@ibm.com>
2023-06-01 11:30:45 -0400

    RGW: Update Cache Driver structure
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/rgw/rgw_cache_driver.cc
+++ src/rgw/rgw_cache_driver.cc
@@ -1,5 +1,7 @@
 #include "rgw_cache_driver.h"
 
+namespace rgw { 
+
 int CacheDriver::initialize(CephContext* cct, const DoutPrefixProvider* dpp) {
   return 0;
 }
@@ -8,11 +10,11 @@ bool CacheDriver::key_exists(const DoutPrefixProvider* dpp, std::string& key) {
   return false;
 }
 
-int CacheDriver::put(const DoutPrefixProvider* dpp, const std::string& key, bufferlist& bl, uint64_t len, rgw::sal::Attrs* attrs) {
+int CacheDriver::put(const DoutPrefixProvider* dpp, const std::string& key, bufferlist& bl, uint64_t len, rgw::sal::Attrs& attrs) {
   return 0;
 }
 
-int CacheDriver::get(const DoutPrefixProvider* dpp, const std::string& key, off_t offset, uint64_t len, bufferlist& bl, rgw::sal::Attrs* attrs) {
+int CacheDriver::get(const DoutPrefixProvider* dpp, const std::string& key, off_t offset, uint64_t len, bufferlist& bl, rgw::sal::Attrs& attrs) {
   return 0;
 }
 
@@ -45,9 +47,11 @@ int CacheDriver::delete_attrs(const DoutPrefixProvider* dpp, const std::string& 
 }
 
 std::string CacheDriver::get_attr(const DoutPrefixProvider* dpp, const std::string& key, const std::string& attr_name) {
-  return std::string;
+  return {};
 }
 
 int CacheDriver::set_attr(const DoutPrefixProvider* dpp, const std::string& key, const std::string& attr_name, const std::string& attr_val) {
   return 0;
 }
+
+} // namespace rgw::sal
--- src/rgw/rgw_redis_driver.h
+++ src/rgw/rgw_redis_driver.h
@@ -8,6 +8,8 @@
 #include "rgw_cache_driver.h"
 #include "driver/d4n/d4n_directory.h"
 
+namespace rgw {
+
 class RedisDriver : public CacheDriver {
   private:
     cpp_redis::client client;
@@ -21,8 +23,8 @@ class RedisDriver : public CacheDriver {
 
     virtual int initialize(CephContext* cct, const DoutPrefixProvider* dpp) override;
     virtual bool key_exists(const DoutPrefixProvider* dpp, std::string& key) override;
-    virtual int put(const DoutPrefixProvider* dpp, const std::string& key, bufferlist& bl, uint64_t len, rgw::sal::Attrs* attrs) override;
-    virtual int get(const DoutPrefixProvider* dpp, const std::string& key, off_t offset, uint64_t len, bufferlist& bl, rgw::sal::Attrs* attrs) override;
+    virtual int put(const DoutPrefixProvider* dpp, const std::string& key, bufferlist& bl, uint64_t len, rgw::sal::Attrs& attrs) override;
+    virtual int get(const DoutPrefixProvider* dpp, const std::string& key, off_t offset, uint64_t len, bufferlist& bl, rgw::sal::Attrs& attrs) override;
     virtual int append_data(const DoutPrefixProvider* dpp, const::std::string& key, bufferlist& bl_data) override;
     virtual int delete_data(const DoutPrefixProvider* dpp, const::std::string& key) override;
     virtual int get_attrs(const DoutPrefixProvider* dpp, const std::string& key, rgw::sal::Attrs& attrs) override;
@@ -32,4 +34,6 @@ class RedisDriver : public CacheDriver {
     virtual int set_attr(const DoutPrefixProvider* dpp, const std::string& key, const std::string& attr_name, const std::string& attr_val) override;
 };
 
+} // namespace rgw::sal
+    
 #endif
