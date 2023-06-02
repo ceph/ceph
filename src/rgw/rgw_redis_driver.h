35f8b9869676a26fc460f7c4985835cdb477ceb2
Samarah <samarah.uriarte@ibm.com>
2023-06-02 10:28:42 -0400

    RGW: Update RedisDriver to match new CacheDriver structure; define set_attrs method
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/rgw/rgw_redis_driver.h
+++ src/rgw/rgw_redis_driver.h
@@ -8,7 +8,7 @@
 #include "rgw_cache_driver.h"
 #include "driver/d4n/d4n_directory.h"
 
-namespace rgw {
+namespace rgw { namespace cal { //cal stands for Cache Abstraction Layer
 
 class RedisDriver : public CacheDriver {
   private:
@@ -22,18 +22,26 @@ class RedisDriver : public CacheDriver {
     }
 
     virtual int initialize(CephContext* cct, const DoutPrefixProvider* dpp) override;
-    virtual bool key_exists(const DoutPrefixProvider* dpp, std::string& key) override;
     virtual int put(const DoutPrefixProvider* dpp, const std::string& key, bufferlist& bl, uint64_t len, rgw::sal::Attrs& attrs) override;
     virtual int get(const DoutPrefixProvider* dpp, const std::string& key, off_t offset, uint64_t len, bufferlist& bl, rgw::sal::Attrs& attrs) override;
     virtual int append_data(const DoutPrefixProvider* dpp, const::std::string& key, bufferlist& bl_data) override;
     virtual int delete_data(const DoutPrefixProvider* dpp, const::std::string& key) override;
     virtual int get_attrs(const DoutPrefixProvider* dpp, const std::string& key, rgw::sal::Attrs& attrs) override;
+    virtual int set_attrs(const DoutPrefixProvider* dpp, const std::string& key, rgw::sal::Attrs& attrs) override;
     virtual int update_attrs(const DoutPrefixProvider* dpp, const std::string& key, rgw::sal::Attrs& attrs) override;
     virtual int delete_attrs(const DoutPrefixProvider* dpp, const std::string& key, rgw::sal::Attrs& del_attrs) override;
     virtual std::string get_attr(const DoutPrefixProvider* dpp, const std::string& key, const std::string& attr_name) override;
     virtual int set_attr(const DoutPrefixProvider* dpp, const std::string& key, const std::string& attr_name, const std::string& attr_val) override;
+
+    /* Entry */
+    virtual bool key_exists(const DoutPrefixProvider* dpp, const std::string& key) override;
+    virtual uint64_t get_num_entries(const DoutPrefixProvider* dpp) override;
+
+    /* Partition */
+    virtual CacheDriver::Partition get_current_partition_info() override;
+    virtual uint64_t get_free_space() override;
 };
 
-} // namespace rgw::sal
+} } // namespace rgw::cal
     
 #endif
