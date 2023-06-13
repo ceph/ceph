430a8666b89e84f89d8fd534e146331a2aa04d27
Xiubo Li <xiubli@redhat.com>
2023-06-13 18:30:34 +0800

    mds: fix the description for inotable testing only options
    The description text are mixed for mds_kill_skip_replaying_inotable
    and mds_inject_skip_replaying_inotable.
    
    At the same time rename "mds_kill_skip_replaying_inotable", which
    is a bit confusing to "mds_kill_after_journal_logs_flushed".
    
    Fixes: https://tracker.ceph.com/issues/61660
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- src/mds/Server.cc
+++ src/mds/Server.cc
@@ -4557,7 +4557,7 @@ public:
     ceph_assert(r == 0);
 
     // crash current MDS and the replacing MDS will test the journal
-    ceph_assert(!g_conf()->mds_kill_skip_replaying_inotable);
+    ceph_assert(!g_conf()->mds_kill_after_journal_logs_flushed);
 
     dn->pop_projected_linkage();
 
@@ -6874,7 +6874,7 @@ public:
     ceph_assert(r == 0);
 
     // crash current MDS and the replacing MDS will test the journal
-    ceph_assert(!g_conf()->mds_kill_skip_replaying_inotable);
+    ceph_assert(!g_conf()->mds_kill_after_journal_logs_flushed);
 
     // link the inode
     dn->pop_projected_linkage();
@@ -7166,7 +7166,7 @@ void Server::handle_client_symlink(MDRequestRef& mdr)
   mds->balancer->maybe_fragment(dir, false);
 
   // flush the journal as soon as possible
-  if (g_conf()->mds_kill_skip_replaying_inotable) {
+  if (g_conf()->mds_kill_after_journal_logs_flushed) {
     mdlog->flush();
   }
 }
