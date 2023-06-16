5445cd3ce55a609e31050aa7499169a3bd8aef5f
Xuehan Xu <xxhdx1985126@gmail.com>
2023-06-16 14:42:38 +0800

    crimson/os/seastore/btree: should add left's size when merging levels if donor is left
    Fixes: https://tracker.ceph.com/issues/61709
    Signed-off-by: Xuehan Xu <xxhdx1985126@gmail.com>
    (cherry picked from commit 918d26bf979814c158744979e16fdfaf1ac4ed3d)

--- src/crimson/os/seastore/btree/fixed_kv_btree.h
+++ src/crimson/os/seastore/btree/fixed_kv_btree.h
@@ -2039,7 +2039,7 @@ private:
 
         pos.node = replacement;
         if (donor_is_left) {
-          pos.pos += r->get_size();
+          pos.pos += l->get_size();
           parent_pos.pos--;
         }
 
