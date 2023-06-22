77bf36fa5a9ed1bc40be78504f5945c50f3468be
Patrick Donnelly <pdonnell@redhat.com>
2023-06-21 22:39:13 -0400

    qa: stop overriding ceph_w prefix in vstart_runner
    These tools are now available in the $PATH so it's no longer necessary
    to remove them.
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- qa/tasks/vstart_runner.py
+++ qa/tasks/vstart_runner.py
@@ -805,7 +805,6 @@ class LocalCephManager(CephManager):
         self.cephadm = False
         self.rook = False
         self.testdir = None
-        self.run_ceph_w_prefix = self.run_cluster_cmd_prefix = [CEPH_CMD]
         self.CEPH_CMD = [CEPH_CMD]
         self.RADOS_CMD = [RADOS_CMD]
 
