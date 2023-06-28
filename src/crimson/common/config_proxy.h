62e11dec8af8630c6ab5d6c50574dd505b86f689
Aishwarya Mathuria <amathuri@redhat.com>
2023-06-28 16:55:03 +0530

    crimson/mgr: Fix config show command
    Update the MMgrOpen message to include config_bl and config_defaults_bl
    
    Fixes: https://tracker.ceph.com/issues/61846
    Signed-off-by: Aishwarya Mathuria <amathuri@redhat.com>

--- src/crimson/common/config_proxy.h
+++ src/crimson/common/config_proxy.h
@@ -96,6 +96,15 @@ public:
     return values.get();
   }
 
+  void get_config_bl(uint64_t have_version,
+		     ceph::buffer::list *bl,
+		     uint64_t *got_version) {
+    get_config().get_config_bl(get_config_values(), have_version,
+                               bl, got_version);
+  }
+  void get_defaults_bl(ceph::buffer::list *bl) {
+    get_config().get_defaults_bl(get_config_values(), bl);
+  }
   // required by sharded<>
   seastar::future<> start();
   seastar::future<> stop() {
