d8c93617bd18a192ddc79dca126e2fe3657aa637
ericqzhao <125110732+ericqzhao@users.noreply.github.com>
2023-07-05 11:10:22 +0800

    "mgr: fix the problem of inaccurate throttle"

--- src/mgr/DaemonServer.cc
+++ src/mgr/DaemonServer.cc
@@ -123,7 +123,14 @@ int DaemonServer::init(uint64_t gid, entity_addrvec_t client_addrs)
 			   "mgr",
 			   Messenger::get_random_nonce());
   msgr->set_default_policy(Messenger::Policy::stateless_server(0));
-
+  msgr->set_policy(entity_name_t::TYPE_OSD,
+                   Messenger::Policy::stateless_server(
+		     CEPH_FEATURE_SERVER_LUMINOUS));
+  msgr->set_policy(entity_name_t::TYPE_MON,
+                   Messenger::Policy::lossy_client(CEPH_FEATURE_UID |
+                                                   CEPH_FEATURE_PGID64));
+  msgr->set_policy(entity_name_t::TYPE_MDS,
+                   Messenger::Policy::lossless_peer(CEPH_FEATURE_UID));  
   msgr->set_auth_client(monc);
 
   // throttle clients
