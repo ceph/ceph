c1808e6dfe2590848ebca0dcc74548807d5fd596
Igor Fedotov <igor.fedotov@croit.io>
2023-07-11 00:22:58 +0300

    os/bluestore: fix btree allocator
    Fixes: https://tracker.ceph.com/issues/61949
    
    Signed-off-by: Igor Fedotov <igor.fedotov@croit.io>
    (cherry picked from commit c4c3e34f819ef13a476093969bb634658b84e026)

--- src/test/objectstore/Allocator_bench.cc
+++ src/test/objectstore/Allocator_bench.cc
@@ -365,4 +365,4 @@ TEST_P(AllocTest, mempoolAccounting)
 INSTANTIATE_TEST_SUITE_P(
   Allocator,
   AllocTest,
-  ::testing::Values("stupid", "bitmap", "avl", "btree", "hybrid"));
+  ::testing::Values("stupid", "bitmap", "avl", "hybrid", "btree"));
