d9aabb8c09a08ea92d804c1751395eb72436212f
Pritha Srivastava <prsrivas@redhat.com>
2023-07-11 11:31:04 +0530

    rgw/cache: implementation of put_async method in ssd driver and redis driver and squashes the following commits.
    rgw/cache: implementation of async put. the call
    does not take into account throttling for now.
    rgw/cache: dummy implementation of put_async in redis
    driver to fix compilation error.
    
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>

--- src/rgw/rgw_ssd_driver.h
+++ src/rgw/rgw_ssd_driver.h
@@ -27,6 +27,7 @@ public:
   virtual int put(const DoutPrefixProvider* dpp, const std::string& key, bufferlist& bl, uint64_t len, rgw::sal::Attrs& attrs) override;
   virtual int get(const DoutPrefixProvider* dpp, const std::string& key, off_t offset, uint64_t len, bufferlist& bl, rgw::sal::Attrs& attrs) override;
   virtual rgw::AioResultList get_async (const DoutPrefixProvider* dpp, optional_yield y, rgw::Aio* aio, const std::string& key, off_t ofs, uint64_t len, uint64_t cost, uint64_t id) override;
+  virtual int put_async(const DoutPrefixProvider* dpp, const std::string& key, bufferlist& bl, uint64_t len, rgw::sal::Attrs& attrs) override;
   virtual int append_data(const DoutPrefixProvider* dpp, const::std::string& key, bufferlist& bl_data) = 0;
   virtual int delete_data(const DoutPrefixProvider* dpp, const::std::string& key) override;
   virtual int get_attrs(const DoutPrefixProvider* dpp, const std::string& key, rgw::sal::Attrs& attrs) override;
@@ -63,7 +64,6 @@ public:
   template <typename ExecutionContext, typename CompletionToken>
   auto get_async(const DoutPrefixProvider *dpp, ExecutionContext& ctx, const std::string& key,
                   off_t read_ofs, off_t read_len, CompletionToken&& token);
-
 protected:
   static std::unordered_map<std::string, Partition> partitions;
   std::unordered_map<std::string, Entry> entries;
@@ -79,6 +79,7 @@ protected:
   std::optional<Entry> get_entry(const DoutPrefixProvider* dpp, std::string key);
 
 private:
+
 // unique_ptr with custom deleter for struct aiocb
 struct libaio_aiocb_deleter {
   void operator()(struct aiocb* c) {
@@ -93,17 +94,38 @@ struct libaio_aiocb_deleter {
 using unique_aio_cb_ptr = std::unique_ptr<struct aiocb, libaio_aiocb_deleter>;
 
 struct AsyncReadOp {
-    bufferlist result;
-    unique_aio_cb_ptr aio_cb;
-    using Signature = void(boost::system::error_code, bufferlist);
-    using Completion = ceph::async::Completion<Signature, AsyncReadOp>;
+  bufferlist result;
+  unique_aio_cb_ptr aio_cb;
+  using Signature = void(boost::system::error_code, bufferlist);
+  using Completion = ceph::async::Completion<Signature, AsyncReadOp>;
 
-    int init(const DoutPrefixProvider *dpp, CephContext* cct, const std::string& file_path, off_t read_ofs, off_t read_len, void* arg);
-    static void libaio_cb_aio_dispatch(sigval sigval);
+  int init(const DoutPrefixProvider *dpp, CephContext* cct, const std::string& file_path, off_t read_ofs, off_t read_len, void* arg);
+  static void libaio_cb_aio_dispatch(sigval sigval);
 
-    template <typename Executor1, typename CompletionHandler>
-    static auto create(const Executor1& ex1, CompletionHandler&& handler);
-  };
+  template <typename Executor1, typename CompletionHandler>
+  static auto create(const Executor1& ex1, CompletionHandler&& handler);
+};
+
+struct AsyncWriteRequest {
+  const DoutPrefixProvider* dpp;
+	std::string key;
+	void *data;
+	int fd;
+	struct aiocb *cb;
+  SSDDriver *priv_data;
+
+	AsyncWriteRequest(const DoutPrefixProvider* dpp) : dpp(dpp) {}
+	int prepare_libaio_write_op(const DoutPrefixProvider *dpp, bufferlist& bl, unsigned int len, std::string key, std::string cache_location);
+  static void libaio_write_cb(sigval sigval);
+
+  ~AsyncWriteRequest() {
+    ::close(fd);
+		cb->aio_buf = nullptr;
+		delete(cb);
+  }
+};
+
+void libaio_write_completion_cb(AsyncWriteRequest* c);
 
 };
 
