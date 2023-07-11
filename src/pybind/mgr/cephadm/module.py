e4dc09ce211b247f04ca833733e4cf27f9401522
Redouane Kachach <rkachach@redhat.com>
2023-07-11 10:01:39 +0200

    Addressing reviewers comments
    Signed-off-by: Redouane Kachach <rkachach@redhat.com>
    (cherry picked from commit 5a043fe6d25fd986a5ccb55b4920922c1fcd0f22)

--- src/pybind/mgr/cephadm/module.py
+++ src/pybind/mgr/cephadm/module.py
@@ -2278,7 +2278,7 @@ Then run the following:
 
         if action == 'rotate-key':
             if d.daemon_type not in ['mgr', 'osd', 'mds',
-                                     'rgw', 'crash', 'nfs', 'rbd-mirror', 'iscsi', 'nvmeof']:
+                                     'rgw', 'crash', 'nfs', 'rbd-mirror', 'iscsi']:
                 raise OrchestratorError(
                     f'key rotation not supported for {d.daemon_type}'
                 )
@@ -2653,8 +2653,6 @@ Then run the following:
                 deps = [self.iscsi_service.get_trusted_ips(iscsi_spec)]
             else:
                 deps = [self.get_mgr_ip()]
-        elif daemon_type == 'nvmeof':
-            deps = []  # TODO(redo)
         elif daemon_type == 'prometheus':
             # for prometheus we add the active mgr as an explicit dependency,
             # this way we force a redeploy after a mgr failover
