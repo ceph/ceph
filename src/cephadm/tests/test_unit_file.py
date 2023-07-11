6f3b16cd6c97e7f90f947b9dc0ca90d777952c68
John Mulligan <jmulligan@redhat.com>
2023-07-11 15:51:19 -0400

    cephadm: add additional test cases for unit file contents
    Asserts more about the unit files.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- src/cephadm/tests/test_unit_file.py
+++ src/cephadm/tests/test_unit_file.py
@@ -16,6 +16,8 @@ from tests.fixtures import (
     with_cephadm_ctx,
 )
 
+from cephadmlib.constants import CGROUPS_SPLIT_PODMAN_VERSION
+
 _cephadm = import_cephadm()
 
 
@@ -31,3 +33,24 @@ def test_podman_engine_does_not_req_docker():
     ctx.container_engine = mock_podman()
     r = _cephadm.get_unit_file(ctx, '9b9d7609-f4d5-4aba-94c8-effa764d96c9')
     assert 'Requires=docker.service' not in r
+
+
+def test_podman_engine_forking_service():
+    # verity that the podman service uses the forking service type
+    # and related parameters
+    ctx = _cephadm.CephadmContext()
+    ctx.container_engine = mock_podman()
+    r = _cephadm.get_unit_file(ctx, '9b9d7609-f4d5-4aba-94c8-effa764d96c9')
+    assert 'Type=forking' in r
+    assert 'PIDFile=' in r
+    assert 'ExecStartPre' in r
+    assert 'ExecStopPost' in r
+
+
+def test_podman_with_split_cgroups_sets_delegate():
+    ctx = _cephadm.CephadmContext()
+    ctx.container_engine = mock_podman()
+    ctx.container_engine.version = CGROUPS_SPLIT_PODMAN_VERSION
+    r = _cephadm.get_unit_file(ctx, '9b9d7609-f4d5-4aba-94c8-effa764d96c9')
+    assert 'Type=forking' in r
+    assert 'Delegate=yes' in r
