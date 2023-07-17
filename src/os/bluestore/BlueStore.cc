15440fa5aaaa2a68389135b91cf608b195cf8c22
Adam Kupczyk <akupczyk@ibm.com>
2023-07-17 07:31:05 +0000

    os/bluestore: Fix split_cache
    Fix cache was broken as it moved blobs and extents between cache shards
    without changing atomic num_blobs, num_extent counters.
    
    Now tracking counters correctly.
    
    Signed-off-by: Adam Kupczyk <akupczyk@ibm.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -5137,6 +5137,8 @@ void BlueStore::Collection::split_cache(
 				 << dendl;
 	  }
 	}
+	cache->rm_blob();
+	dest->cache->add_blob();
 	SharedBlob* sb = b->shared_blob.get();
 	if (sb->coll == dest) {
 	  ldout(store->cct, 20) << __func__ << "  already moved " << *sb
@@ -5160,6 +5162,8 @@ void BlueStore::Collection::split_cache(
 	b.second->last_encoded_id = -1;
       }
       for (auto& e : o->extent_map.extent_map) {
+	cache->rm_extent();
+	dest->cache->add_extent();
 	Blob* tb = e.blob.get();
 	if (tb->last_encoded_id == -1) {
 	  rehome_blob(tb);
