cf9fc7e3c4693e7f107b5a01dff649eaabb37ac8
Patrick Donnelly <pdonnell@redhat.com>
2023-07-17 11:07:14 -0400

    qa: create qa subvolumes in named subvolumegroup
    So we can twiddle the subvolumegroup settings (like pins).
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- qa/tasks/cephfs/filesystem.py
+++ qa/tasks/cephfs/filesystem.py
@@ -773,17 +773,30 @@ class Filesystem(MDSCluster):
                 assert(isinstance(subvols['create'], int))
                 assert(subvols['create'] > 0)
 
+                self.run_ceph_cmd('fs', 'subvolumegroup', 'create', self.name, 'qa')
+                subvol_options = self.fs_config.get('subvol_options', '')
+
                 for sv in range(0, subvols['create']):
                     sv_name = f'sv_{sv}'
-                    self.run_ceph_cmd('fs', 'subvolume', 'create', self.name,
-                                      sv_name,
-                                      self.fs_config.get('subvol_options', ''))
+                    cmd = [
+                      'fs',
+                      'subvolume',
+                      'create',
+                      self.name,
+                      sv_name,
+                      '--group_name', 'qa',
+                    ]
+                    if subvol_options:
+                        cmd.append(subvol_options)
+                    self.run_ceph_cmd(*cmd)
 
                     if self.name not in self._ctx.created_subvols:
                         self._ctx.created_subvols[self.name] = []
                     
                     subvol_path = self.get_ceph_cmd_stdout(
-                        'fs', 'subvolume', 'getpath', self.name, sv_name)
+                        'fs', 'subvolume', 'getpath', self.name,
+                        '--group_name', 'qa',
+                        sv_name)
                     subvol_path = subvol_path.strip()
                     self._ctx.created_subvols[self.name].append(subvol_path)
             else:
