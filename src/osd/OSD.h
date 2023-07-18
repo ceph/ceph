e615d1fb178ad21310dae5b16481d2c4f59e4614
Matan Breizman <mbreizma@redhat.com>
2023-07-18 16:08:23 +0000

    osd/OSD: remove `skip_maps`
    the superblock now stores an interval_set which supports non-contiguous osdmap history.
    
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>

--- src/osd/OSD.h
+++ src/osd/OSD.h
@@ -1673,7 +1673,7 @@ protected:
 
   void handle_osd_map(class MOSDMap *m);
   void _committed_osd_maps(epoch_t first, epoch_t last, class MOSDMap *m);
-  void trim_maps(epoch_t oldest, bool skip_maps);
+  void trim_maps(epoch_t oldest);
   void note_down_osd(int osd);
   void note_up_osd(int osd);
   friend struct C_OnMapCommit;
