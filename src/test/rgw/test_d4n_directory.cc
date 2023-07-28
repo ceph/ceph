cc4439a2fffe0187dc1e6623c6d591906a3ae12f
Samarah <samarah.uriarte@ibm.com>
2023-07-28 09:44:03 -0400

    test/d4n: this commit squashes the following commits related to tests for d4n.
    test/redis: Add temporary `boost::redis` unit test program for experimentation
    test/rgw: Add/update unit test programs for D4N classes + RedisDriver; switch to `boost::redis` usage
    test/d4n: Begin `cpp_redis` removal from D4N filter test program; needs
    to be fixed
    test/d4n: Temporarily remove D4N filter test program until D4N workflows
    are fully implemented
    
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/test/rgw/test_d4n_directory.cc
+++ src/test/rgw/test_d4n_directory.cc
@@ -1,206 +1,482 @@
-#include "../rgw/driver/d4n/d4n_directory.h" // Fix -Sam
-#include "rgw_process_env.h"
-#include <cpp_redis/cpp_redis>
-#include <iostream>
-#include <string>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/redis/connection.hpp>
+
 #include "gtest/gtest.h"
+#include "common/ceph_argparse.h"
+#include "rgw_auth_registry.h"
+#include "driver/d4n/d4n_directory.h"
 
-using namespace std;
+namespace net = boost::asio;
+using boost::redis::config;
+using boost::redis::connection;
+using boost::redis::request;
+using boost::redis::response;
 
-string portStr;
-string hostStr;
-string redisHost = "";
-string oid = "testName";
-string bucketName = "testBucket";
-int blockSize = 123;
+class Environment* env;
 
-class DirectoryFixture: public ::testing::Test {
+class Environment : public ::testing::Environment {
+  public:
+    Environment() {}
+
+    virtual ~Environment() {}
+
+    void SetUp() override {
+      std::vector<const char*> args;
+      std::string conf_file_list;
+      std::string cluster = "";
+      CephInitParameters iparams = ceph_argparse_early_args(
+	args, CEPH_ENTITY_TYPE_CLIENT,
+	&cluster, &conf_file_list);
+
+      cct = common_preinit(iparams, CODE_ENVIRONMENT_UTILITY, {}); 
+      dpp = new DoutPrefix(cct->get(), dout_subsys, "D4N Object Directory Test: ");
+      
+      redisHost = cct->_conf->rgw_d4n_host + ":" + std::to_string(cct->_conf->rgw_d4n_port); 
+    }
+    
+    void TearDown() override {
+      delete dpp;
+    }
+
+    std::string redisHost;
+    CephContext* cct;
+    DoutPrefixProvider* dpp;
+};
+
+class ObjectDirectoryFixture: public ::testing::Test {
   protected:
     virtual void SetUp() {
-      blockDir = new rgw::d4n::BlockDirectory(hostStr, stoi(portStr));
-      cacheBlock = new rgw::d4n::CacheBlock();
+      dir = new rgw::d4n::ObjectDirectory{io};
+      obj = new rgw::d4n::CacheObj{
+	.objName = "testName",
+	.bucketName = "testBucket",
+	.creationTime = 0,
+	.dirty = false,
+	.hostsList = { env->redisHost }
+      };
 
-      cacheBlock->hostsList.push_back(redisHost);
-      cacheBlock->size = blockSize; 
-      cacheBlock->cacheObj.bucketName = bucketName;
-      cacheBlock->cacheObj.objName = oid;
+      conn = new connection{boost::asio::make_strand(io)};
+
+      ASSERT_NE(obj, nullptr);
+      ASSERT_NE(dir, nullptr);
+      ASSERT_NE(conn, nullptr);
+
+      dir->init(env->cct, env->dpp);
+
+      /* Run fixture's connection */
+      config cfg;
+      cfg.addr.host = env->cct->_conf->rgw_d4n_host;
+      cfg.addr.port = std::to_string(env->cct->_conf->rgw_d4n_port);
+
+      conn->async_run(cfg, {}, net::detached);
     } 
 
     virtual void TearDown() {
-      delete blockDir;
-      blockDir = nullptr;
-
-      delete cacheBlock;
-      cacheBlock = nullptr;
+      delete conn;
+      delete obj;
+      delete dir;
     }
 
-    rgw::d4n::BlockDirectory* blockDir;
-    rgw::d4n::CacheBlock* cacheBlock;
+    rgw::d4n::CacheObj* obj;
+    rgw::d4n::ObjectDirectory* dir;
+
+    net::io_context io;
+    connection* conn;
+
+    std::vector<std::string> vals{"testName", "testBucket", "0", "0", env->redisHost};
+    std::vector<std::string> fields{"objName", "bucketName", "creationTime", "dirty", "objHosts"};
 };
 
-/* Successful initialization */
-TEST_F(DirectoryFixture, DirectoryInit) {
-  ASSERT_NE(blockDir, nullptr);
-  ASSERT_NE(cacheBlock, nullptr);
-  ASSERT_NE(redisHost.length(), (long unsigned int)0);
+class BlockDirectoryFixture: public ::testing::Test {
+  protected:
+    virtual void SetUp() {
+      dir = new rgw::d4n::BlockDirectory{io};
+      block = new rgw::d4n::CacheBlock{
+        .cacheObj = {
+	  .objName = "testName",
+	  .bucketName = "testBucket",
+	  .creationTime = 0,
+	  .dirty = false,
+	  .hostsList = { env->redisHost }
+	},
+        .blockID = 0,
+	.version = "",
+	.size = 0,
+	.hostsList = { env->redisHost }
+      };
+
+      conn = new connection{boost::asio::make_strand(io)};
+
+      ASSERT_NE(block, nullptr);
+      ASSERT_NE(dir, nullptr);
+      ASSERT_NE(conn, nullptr);
+
+      dir->init(env->cct, env->dpp);
+
+      /* Run fixture's connection */
+      config cfg;
+      cfg.addr.host = env->cct->_conf->rgw_d4n_host;
+      cfg.addr.port = std::to_string(env->cct->_conf->rgw_d4n_port);
+
+      conn->async_run(cfg, {}, net::detached);
+    } 
+
+    virtual void TearDown() {
+      delete conn;
+      delete block;
+      delete dir;
+    }
+
+    rgw::d4n::CacheBlock* block;
+    rgw::d4n::BlockDirectory* dir;
+
+    net::io_context io;
+    connection* conn;
+
+    std::vector<std::string> vals{"0", "", "0", "0", env->redisHost, 
+                                   "testName", "testBucket", "0", "0", env->redisHost};
+    std::vector<std::string> fields{"blockID", "version", "size", "globalWeight", "blockHosts", 
+				     "objName", "bucketName", "creationTime", "dirty", "objHosts"};
+};
+
+TEST_F(ObjectDirectoryFixture, SetYield)
+{
+  spawn::spawn(io, [this] (spawn::yield_context yield) {
+    ASSERT_EQ(0, dir->set(obj, optional_yield{io, yield}));
+    dir->shutdown();
+
+    boost::system::error_code ec;
+    request req;
+    req.push_range("HMGET", "testBucket_testName", fields);
+    req.push("FLUSHALL");
+
+    response< std::vector<std::string>,
+	      boost::redis::ignore_t > resp;
+
+    conn->async_exec(req, resp, yield[ec]);
+
+    ASSERT_EQ((bool)ec, false);
+    EXPECT_EQ(std::get<0>(resp).value(), vals);
+    conn->cancel();
+  });
+
+  io.run();
 }
 
-/* Successful set_value Call and Redis Check */
-TEST_F(DirectoryFixture, SetValueTest) {
-  cpp_redis::client client;
-  int key_exist = -1;
-  string key;
-  string hosts;
-  string size;
-  string bucketName;
-  string objName;
-  std::vector<std::string> fields;
-  int setReturn = blockDir->set_value(cacheBlock);
+TEST_F(ObjectDirectoryFixture, GetYield)
+{
+  spawn::spawn(io, [this] (spawn::yield_context yield) {
+    ASSERT_EQ(0, dir->set(obj, optional_yield{io, yield}));
 
-  ASSERT_EQ(setReturn, 0);
+    {
+      boost::system::error_code ec;
+      request req;
+      req.push("HSET", "testBucket_testName", "objName", "newoid");
+      response<int> resp;
 
-  fields.push_back("key");
-  fields.push_back("hosts");
-  fields.push_back("size");
-  fields.push_back("bucketName");
-  fields.push_back("objName");
+      conn->async_exec(req, resp, yield[ec]);
 
-  client.connect(hostStr, stoi(portStr), nullptr, 0, 5, 1000);
-  ASSERT_EQ((bool)client.is_connected(), (bool)1);
-
-  client.hmget("rgw-object:" + oid + ":block-directory", fields, [&key, &hosts, &size, &bucketName, &objName, &key_exist](cpp_redis::reply& reply) {
-    auto arr = reply.as_array();
-
-    if (!arr[0].is_null()) {
-      key_exist = 0;
-      key = arr[0].as_string();
-      hosts = arr[1].as_string();
-      size = arr[2].as_string();
-      bucketName = arr[3].as_string();
-      objName = arr[4].as_string();
+      ASSERT_EQ((bool)ec, false);
+      EXPECT_EQ(std::get<0>(resp).value(), 0);
     }
+
+    ASSERT_EQ(0, dir->get(obj, optional_yield{io, yield}));
+    EXPECT_EQ(obj->objName, "newoid");
+    dir->shutdown();
+
+    {
+      boost::system::error_code ec;
+      request req;
+      req.push("FLUSHALL");
+      response<boost::redis::ignore_t> resp;
+
+      conn->async_exec(req, resp, yield[ec]);
+    }
+
+    conn->cancel();
   });
 
-  client.sync_commit();
-
-  EXPECT_EQ(key_exist, 0);
-  EXPECT_EQ(key, "rgw-object:" + oid + ":block-directory");
-  EXPECT_EQ(hosts, redisHost);
-  EXPECT_EQ(size, to_string(blockSize));
-  EXPECT_EQ(bucketName, bucketName);
-  EXPECT_EQ(objName, oid);
-
-  client.flushall();
+  io.run();
 }
 
-/* Successful get_value Calls and Redis Check */
-TEST_F(DirectoryFixture, GetValueTest) {
-  cpp_redis::client client;
-  int key_exist = -1;
-  string key;
-  string hosts;
-  string size;
-  string bucketName;
-  string objName;
-  std::vector<std::string> fields;
-  int setReturn = blockDir->set_value(cacheBlock);
+TEST_F(ObjectDirectoryFixture, CopyYield)
+{
+  spawn::spawn(io, [this] (spawn::yield_context yield) {
+    ASSERT_EQ(0, dir->set(obj, optional_yield{io, yield}));
+    ASSERT_EQ(0, dir->copy(obj, "copyTestName", "copyBucketName", optional_yield{io, yield}));
+    dir->shutdown();
 
-  ASSERT_EQ(setReturn, 0);
+    boost::system::error_code ec;
+    request req;
+    req.push("EXISTS", "copyBucketName_copyTestName");
+    req.push_range("HMGET", "copyBucketName_copyTestName", fields);
+    req.push("FLUSHALL");
 
-  fields.push_back("key");
-  fields.push_back("hosts");
-  fields.push_back("size");
-  fields.push_back("bucketName");
-  fields.push_back("objName");
+    response<int, std::vector<std::string>, 
+	     boost::redis::ignore_t> resp;
 
-  client.connect(hostStr, stoi(portStr), nullptr, 0, 5, 1000);
-  ASSERT_EQ((bool)client.is_connected(), (bool)1);
+    conn->async_exec(req, resp, yield[ec]);
 
-  client.hmget("rgw-object:" + oid + ":block-directory", fields, [&key, &hosts, &size, &bucketName, &objName, &key_exist](cpp_redis::reply& reply) {
-    auto arr = reply.as_array();
+    ASSERT_EQ((bool)ec, false);
+    EXPECT_EQ(std::get<0>(resp).value(), 1);
 
-    if (!arr[0].is_null()) {
-      key_exist = 0;
-      key = arr[0].as_string();
-      hosts = arr[1].as_string();
-      size = arr[2].as_string();
-      bucketName = arr[3].as_string();
-      objName = arr[4].as_string();
-    }
+    auto copyVals = vals;
+    copyVals[0] = "copyTestName";
+    copyVals[1] = "copyBucketName";
+    EXPECT_EQ(std::get<1>(resp).value(), copyVals);
+
+    conn->cancel();
   });
 
-  client.sync_commit();
-
-  EXPECT_EQ(key_exist, 0);
-  EXPECT_EQ(key, "rgw-object:" + oid + ":block-directory");
-  EXPECT_EQ(hosts, redisHost);
-  EXPECT_EQ(size, to_string(blockSize));
-  EXPECT_EQ(bucketName, bucketName);
-  EXPECT_EQ(objName, oid);
-
-  /* Check if object name in directory instance matches redis update */
-  client.hset("rgw-object:" + oid + ":block-directory", "objName", "newoid", [](cpp_redis::reply& reply) {
-    if (!reply.is_null()) {
-      ASSERT_EQ(reply.as_integer(), 0);
-    }
-  });
-
-  client.sync_commit();
-
-  int getReturn = blockDir->get_value(cacheBlock);
-
-  ASSERT_EQ(getReturn, 0);
-  EXPECT_EQ(cacheBlock->cacheObj.objName, "newoid");
-
-  client.flushall();
+  io.run();
 }
 
-/* Successful del_value Call and Redis Check */
-TEST_F(DirectoryFixture, DelValueTest) {
-  cpp_redis::client client;
-  vector<string> keys;
-  int setReturn = blockDir->set_value(cacheBlock);
+TEST_F(ObjectDirectoryFixture, DelYield)
+{
+  spawn::spawn(io, [this] (spawn::yield_context yield) {
+    ASSERT_EQ(0, dir->set(obj, optional_yield{io, yield}));
 
-  ASSERT_EQ(setReturn, 0);
+    {
+      boost::system::error_code ec;
+      request req;
+      req.push("EXISTS", "testBucket_testName");
+      response<int> resp;
 
-  /* Ensure entry exists in directory before deletion */
-  keys.push_back("rgw-object:" + oid + ":block-directory");
+      conn->async_exec(req, resp, yield[ec]);
 
-  client.exists(keys, [](cpp_redis::reply& reply) {
-    if (reply.is_integer()) {
-      ASSERT_EQ(reply.as_integer(), 1);
+      ASSERT_EQ((bool)ec, false);
+      EXPECT_EQ(std::get<0>(resp).value(), 1);
     }
+
+    ASSERT_EQ(0, dir->del(obj, optional_yield{io, yield}));
+    dir->shutdown();
+
+    {
+      boost::system::error_code ec;
+      request req;
+      req.push("EXISTS", "testBucket_testName");
+      req.push("FLUSHALL");
+      response<int, boost::redis::ignore_t> resp;
+
+      conn->async_exec(req, resp, yield[ec]);
+
+      ASSERT_EQ((bool)ec, false);
+      EXPECT_EQ(std::get<0>(resp).value(), 0);
+    }
+
+    conn->cancel();
   });
 
-  int delReturn = blockDir->del_value(cacheBlock);
+  io.run();
+}
 
-  ASSERT_EQ(delReturn, 0);
+TEST_F(BlockDirectoryFixture, SetYield)
+{
+  spawn::spawn(io, [this] (spawn::yield_context yield) {
+    ASSERT_EQ(0, dir->set(block, optional_yield{io, yield}));
+    dir->shutdown();
 
-  client.exists(keys, [](cpp_redis::reply& reply) {
-    if (reply.is_integer()) {
-      ASSERT_EQ(reply.as_integer(), 0);  /* Zero keys exist */
-    }
+    boost::system::error_code ec;
+    request req;
+    req.push_range("HMGET", "testBucket_testName_0", fields);
+    req.push("FLUSHALL");
+
+    response< std::vector<std::string>,
+	      boost::redis::ignore_t > resp;
+
+    conn->async_exec(req, resp, yield[ec]);
+
+    ASSERT_EQ((bool)ec, false);
+    EXPECT_EQ(std::get<0>(resp).value(), vals);
+    conn->cancel();
   });
 
-  client.flushall();
+  io.run();
+}
+
+TEST_F(BlockDirectoryFixture, GetYield)
+{
+  spawn::spawn(io, [this] (spawn::yield_context yield) {
+    ASSERT_EQ(0, dir->set(block, optional_yield{io, yield}));
+
+    {
+      boost::system::error_code ec;
+      request req;
+      req.push("HSET", "testBucket_testName_0", "objName", "newoid");
+      response<int> resp;
+
+      conn->async_exec(req, resp, yield[ec]);
+
+      ASSERT_EQ((bool)ec, false);
+      EXPECT_EQ(std::get<0>(resp).value(), 0);
+    }
+
+    ASSERT_EQ(0, dir->get(block, optional_yield{io, yield}));
+    EXPECT_EQ(block->cacheObj.objName, "newoid");
+    dir->shutdown();
+
+    {
+      boost::system::error_code ec;
+      request req;
+      req.push("FLUSHALL");
+      response<boost::redis::ignore_t> resp;
+
+      conn->async_exec(req, resp, yield[ec]);
+    }
+
+    conn->cancel();
+  });
+
+  io.run();
+}
+
+TEST_F(BlockDirectoryFixture, CopyYield)
+{
+  spawn::spawn(io, [this] (spawn::yield_context yield) {
+    ASSERT_EQ(0, dir->set(block, optional_yield{io, yield}));
+    ASSERT_EQ(0, dir->copy(block, "copyTestName", "copyBucketName", optional_yield{io, yield}));
+    dir->shutdown();
+
+    boost::system::error_code ec;
+    request req;
+    req.push("EXISTS", "copyBucketName_copyTestName_0");
+    req.push_range("HMGET", "copyBucketName_copyTestName_0", fields);
+    req.push("FLUSHALL");
+
+    response<int, std::vector<std::string>, 
+	     boost::redis::ignore_t> resp;
+
+    conn->async_exec(req, resp, yield[ec]);
+
+    ASSERT_EQ((bool)ec, false);
+    EXPECT_EQ(std::get<0>(resp).value(), 1);
+
+    auto copyVals = vals;
+    copyVals[5] = "copyTestName";
+    copyVals[6] = "copyBucketName";
+    EXPECT_EQ(std::get<1>(resp).value(), copyVals);
+
+    conn->cancel();
+  });
+
+  io.run();
+}
+
+TEST_F(BlockDirectoryFixture, DelYield)
+{
+  spawn::spawn(io, [this] (spawn::yield_context yield) {
+    ASSERT_EQ(0, dir->set(block, optional_yield{io, yield}));
+
+    {
+      boost::system::error_code ec;
+      request req;
+      req.push("EXISTS", "testBucket_testName_0");
+      response<int> resp;
+
+      conn->async_exec(req, resp, yield[ec]);
+
+      ASSERT_EQ((bool)ec, false);
+      EXPECT_EQ(std::get<0>(resp).value(), 1);
+    }
+
+    ASSERT_EQ(0, dir->del(block, optional_yield{io, yield}));
+    dir->shutdown();
+
+    {
+      boost::system::error_code ec;
+      request req;
+      req.push("EXISTS", "testBucket_testName_0");
+      req.push("FLUSHALL");
+      response<int, boost::redis::ignore_t> resp;
+
+      conn->async_exec(req, resp, yield[ec]);
+
+      ASSERT_EQ((bool)ec, false);
+      EXPECT_EQ(std::get<0>(resp).value(), 0);
+    }
+
+    conn->cancel();
+  });
+
+  io.run();
+}
+
+TEST_F(BlockDirectoryFixture, UpdateFieldYield)
+{
+  spawn::spawn(io, [this] (spawn::yield_context yield) {
+    ASSERT_EQ(0, dir->set(block, optional_yield{io, yield}));
+    ASSERT_EQ(0, dir->update_field(block, "objName", "newTestName", optional_yield{io, yield}));
+    ASSERT_EQ(0, dir->update_field(block, "blockHosts", "127.0.0.1:5000", optional_yield{io, yield}));
+    dir->shutdown();
+
+    boost::system::error_code ec;
+    request req;
+    req.push("HMGET", "testBucket_testName_0", "objName", "blockHosts");
+    req.push("FLUSHALL");
+    response< std::vector<std::string>, 
+	      boost::redis::ignore_t> resp;
+
+    conn->async_exec(req, resp, yield[ec]);
+
+    ASSERT_EQ((bool)ec, false);
+    EXPECT_EQ(std::get<0>(resp).value()[0], "newTestName");
+    EXPECT_EQ(std::get<0>(resp).value()[1], "127.0.0.1:6379_127.0.0.1:5000");
+
+    conn->cancel();
+  });
+
+  io.run();
+}
+
+TEST_F(BlockDirectoryFixture, RemoveHostYield)
+{
+  spawn::spawn(io, [this] (spawn::yield_context yield) {
+    block->hostsList.push_back("127.0.0.1:6000");
+    ASSERT_EQ(0, dir->set(block, optional_yield{io, yield}));
+    ASSERT_EQ(0, dir->remove_host(block, "127.0.0.1:6379", optional_yield{io, yield}));
+
+    {
+      boost::system::error_code ec;
+      request req;
+      req.push("HEXISTS", "testBucket_testName_0", "blockHosts");
+      req.push("HGET", "testBucket_testName_0", "blockHosts");
+      response<int, std::string> resp;
+
+      conn->async_exec(req, resp, yield[ec]);
+
+      ASSERT_EQ((bool)ec, false);
+      EXPECT_EQ(std::get<0>(resp).value(), 1);
+      EXPECT_EQ(std::get<1>(resp).value(), "127.0.0.1:6000");
+    }
+
+    ASSERT_EQ(0, dir->remove_host(block, "127.0.0.1:6000", optional_yield{io, yield}));
+    dir->shutdown();
+
+    {
+      boost::system::error_code ec;
+      request req;
+      req.push("EXISTS", "testBucket_testName_0");
+      req.push("FLUSHALL");
+      response<int, boost::redis::ignore_t> resp;
+
+      conn->async_exec(req, resp, yield[ec]);
+
+      ASSERT_EQ((bool)ec, false);
+      EXPECT_EQ(std::get<0>(resp).value(), 0);
+    }
+
+    conn->cancel();
+  });
+
+  io.run();
 }
 
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
 
-  /* Other ports can be passed to the program */
-  if (argc == 1) {
-    portStr = "6379";
-    hostStr = "127.0.0.1";
-  } else if (argc == 3) {
-    hostStr = argv[1];
-    portStr = argv[2];
-  } else {
-    cout << "Incorrect number of arguments." << std::endl;
-    return -1;
-  }
-
-  redisHost = hostStr + ":" + portStr;
+  env = new Environment();
+  ::testing::AddGlobalTestEnvironment(env);
 
   return RUN_ALL_TESTS();
 }
