2cba10f8412da39c76461515bfd43dd51f526d18
Samarah <samarah.uriarte@ibm.com>
2023-07-31 13:32:49 -0400

    rgw/redis: Add `del` method to RedisDriver
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/rgw/rgw_redis_driver.h
+++ src/rgw/rgw_redis_driver.h
@@ -35,6 +35,8 @@ class RedisDriver : public CacheDriver {
     virtual int initialize(CephContext* cct, const DoutPrefixProvider* dpp) override;
     virtual int put(const DoutPrefixProvider* dpp, const std::string& key, bufferlist& bl, uint64_t len, rgw::sal::Attrs& attrs, optional_yield y) override;
     virtual int get(const DoutPrefixProvider* dpp, const std::string& key, off_t offset, uint64_t len, bufferlist& bl, rgw::sal::Attrs& attrs, optional_yield y) override;
+    virtual int del(const DoutPrefixProvider* dpp, const std::string& key, optional_yield y) override;
+    virtual rgw::AioResultList get_async(const DoutPrefixProvider* dpp, optional_yield y, rgw::Aio* aio, const std::string& key, off_t ofs, uint64_t len, uint64_t cost, uint64_t id) override;
     virtual int append_data(const DoutPrefixProvider* dpp, const::std::string& key, bufferlist& bl_data, optional_yield y) override;
     virtual int delete_data(const DoutPrefixProvider* dpp, const::std::string& key, optional_yield y) override;
     virtual int get_attrs(const DoutPrefixProvider* dpp, const std::string& key, rgw::sal::Attrs& attrs, optional_yield y) override;
