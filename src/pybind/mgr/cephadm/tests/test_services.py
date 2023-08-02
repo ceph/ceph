75327c5b56591c6a29ad47745df24d16320f5a99
Jiffin Tony Thottan <thottanjiffin@gmail.com>
2023-08-02 13:23:12 +0530

    cephadm: adjust the ingress ha proxy health check interval
    Currently health checker uses default value of 2s, it is send list
    bucket request for every 2s. This seems to be frequent and need to
    adjust properly. Hence introducing new setting health_check_interval in
    the ingress spec for haproxy.
    
    Signed-off-by: Jiffin Tony Thottan <thottanjiffin@gmail.com>
    
    Apply suggestions from code review
    
    Co-authored-by: Adam King <47704447+adk3798@users.noreply.github.com>
    Signed-off-by: Jiffin Tony Thottan  <thottanjiffin@gmail.com>

--- src/pybind/mgr/cephadm/tests/test_services.py
+++ src/pybind/mgr/cephadm/tests/test_services.py
@@ -1854,7 +1854,7 @@ class TestIngressService:
                                 'balance static-rr\n    '
                                 'option httpchk HEAD / HTTP/1.0\n    '
                                 'server '
-                                + haproxy_generated_conf[1][0] + ' 1.2.3.7:80 check weight 100\n'
+                                + haproxy_generated_conf[1][0] + ' 1.2.3.7:80 check weight 100 inter 2s\n'
                         }
                 }
 
@@ -1979,7 +1979,7 @@ class TestIngressService:
                                 'balance static-rr\n    '
                                 'option httpchk HEAD / HTTP/1.0\n    '
                                 'server '
-                                + haproxy_generated_conf[1][0] + ' 1::4:443 check weight 100\n'
+                                + haproxy_generated_conf[1][0] + ' 1::4:443 check weight 100 inter 2s\n'
                         }
                 }
 
@@ -2103,7 +2103,7 @@ class TestIngressService:
                                 'balance static-rr\n    '
                                 'option httpchk HEAD / HTTP/1.0\n    '
                                 'server '
-                                + haproxy_generated_conf[1][0] + ' 1.2.3.7:80 check weight 100\n'
+                                + haproxy_generated_conf[1][0] + ' 1.2.3.7:80 check weight 100 inter 2s\n'
                         }
                 }
 
