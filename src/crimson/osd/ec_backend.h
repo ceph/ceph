ed5dc44a8d44030c9c9e7ad77fdd252a3ab213c8
Radosław Zarzyński <rzarzyns@redhat.com>
2023-08-02 16:43:56 +0200

    crimson/osd: add support for error handling around MOSDECSubOpRead
    Signed-off-by: Radosław Zarzyński <rzarzyns@redhat.com>

--- src/crimson/osd/ec_backend.h
+++ src/crimson/osd/ec_backend.h
@@ -23,6 +23,8 @@ public:
 	    crimson::osd::ShardServices& shard_services,
 	    const ec_profile_t& ec_profile,
 	    uint64_t stripe_width,
+	    bool fast_read,
+	    bool allows_ecoverwrites,
 	    DoutPrefixProvider &dpp);
   seastar::future<> stop() final {
     return seastar::now();
@@ -61,4 +63,7 @@ private:
 
   ceph::ErasureCodeInterfaceRef ec_impl;
   const ECUtil::stripe_info_t sinfo;
+
+  const bool fast_read;
+  const bool allows_ecoverwrites;
 };
