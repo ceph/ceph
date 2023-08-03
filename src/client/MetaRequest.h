c829fe1a1f5b18431e61d581cf23f9e665ec458b
Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
2023-08-03 14:15:28 +0200

    ceph_fs.h: add separate owner_{u,g}id fields
    This patch adds separate fields to pass inode owner's UID/GID
    for operations which create new inodes:
    CEPH_MDS_OP_CREATE, CEPH_MDS_OP_MKNOD, CEPH_MDS_OP_RENAME,
    CEPH_MDS_OP_MKDIR, CEPH_MDS_OP_SYMLINK.
    
    Before we have used caller_{u,g}id fields for two purposes:
    - for MDS permission checks
    - to set UID/GID for new inodes
    
    but during a long discussion around adding idmapped mounts
    support to cephfs kernel client we decided to extend wire protocol [1].
    
    Originally, Christian Brauner pointed to this problem [2], but
    a few initial versions of "ceph: support idmapped mounts" patchset
    were based on a compromise between bringing idmapped mounts support
    and changing on-wire data structures. The only problem with this approach
    is that if we have UID/GID-based permission checks on the MDS side they
    won't work as intended. Xiubo Li pointed to this issue and said that
    it's critical enough to not being ignored.
    
    This patch does not change behavior for client or MDS, because
    in the current client implementation we do:
    req->set_inode_owner_uid_gid(perms.uid(), perms.gid());
    so, these new fields contain the same value as caller_{u,g}id.
    
    This new extension will be used in a non-trivial way in the Linux kernel client.
    
    [1] https://lore.kernel.org/lkml/CAEivzxcipdTQ9-b2zk-7-AMDfwPk2Brtp=X6H8xXsHMEtQJKFQ@mail.gmail.com/
    [2] https://lore.kernel.org/all/20220104140414.155198-3-brauner@kernel.org/
    
    Fixes: https://tracker.ceph.com/issues/62217
    
    Signed-off-by: Alexander Mikhalitsyn <aleksandr.mikhalitsyn@canonical.com>
    (cherry picked from commit 46cb244b9c8397870b99037434d627a63dcb1363)

--- src/client/MetaRequest.h
+++ src/client/MetaRequest.h
@@ -78,6 +78,8 @@ public:
     unsafe_target_item(this) {
     memset(&head, 0, sizeof(head));
     head.op = op;
+    head.owner_uid = -1;
+    head.owner_gid = -1;
   }
   ~MetaRequest();
 
@@ -149,6 +151,13 @@ public:
   bool _put() {
     int v = --ref;
     return v == 0;
+  }
+
+  void set_inode_owner_uid_gid(unsigned u, unsigned g) {
+    /* it makes sense to set owner_{u,g}id only for OPs which create inodes */
+    ceph_assert(IS_CEPH_MDS_OP_NEWINODE(head.op));
+    head.owner_uid = u;
+    head.owner_gid = g;
   }
 
   // normal fields
--- src/mds/cephfs_features.cc
+++ src/mds/cephfs_features.cc
@@ -26,6 +26,8 @@ static const std::array feature_names
   "notify_session_state",
   "op_getvxattr",
   "32bits_retry_fwd",
+  "new_snaprealm_info",
+  "has_owner_uidgid",
 };
 static_assert(feature_names.size() == CEPHFS_FEATURE_MAX + 1);
 
