336260c7b2482d5312c5838694a669fc0eadcdda
Adam Kupczyk <akupczyk@ibm.com>
2023-08-05 17:22:18 +0000

    os/bluestore: Protect AvlAllocator against malicious input
    AvlAlloctor now is fortified with proper asserts against inserting a range
    that would otherwise corrupt internal trees.
    When same region was added twice it could be requested twice.
    
    Signed-off-by: Adam Kupczyk <akupczyk@ibm.com>

--- src/os/bluestore/AvlAllocator.cc
+++ src/os/bluestore/AvlAllocator.cc
@@ -105,6 +105,12 @@ void AvlAllocator::_add_to_tree(uint64_t start, uint64_t size)
     rs_before = std::prev(rs_after);
   }
 
+  // It is illegal if region before is in collision with new region
+  ceph_assert(rs_before == range_tree.end() || rs_before->end <= start);
+  // It is illegal if region after is in collision with new region
+  // the below assertion seems to be necessary, but in fact it only check upper_bound()
+  ceph_assert(rs_after == range_tree.end() || end <= rs_after->start);
+
   bool merge_before = (rs_before != range_tree.end() && rs_before->end == start);
   bool merge_after = (rs_after != range_tree.end() && rs_after->start == end);
 
