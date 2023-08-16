ac6688729d328d9c853eef85be9bb035b369e1ee
John Mulligan <jmulligan@redhat.com>
2023-08-16 14:03:50 -0400

    cephadm: move executable discovery funcs to exe_utils.py
    Signed-off-by: John Mulligan <jmulligan@redhat.com>
    Pair-programmed-with: Adam King <adking@redhat.com>
    Co-authored-by: Adam King <adking@redhat.com>

--- src/cephadm/tests/fixtures.py
+++ src/cephadm/tests/fixtures.py
@@ -148,7 +148,7 @@ def with_cephadm_ctx(
     with mock.patch('cephadm.attempt_bind'), \
          mock.patch('cephadm.call', return_value=('', '', 0)), \
          mock.patch('cephadm.call_timeout', return_value=0), \
-         mock.patch('cephadm.find_executable', return_value='foo'), \
+         mock.patch('cephadmlib.exe_utils.find_executable', return_value='foo'), \
          mock.patch('cephadm.get_container_info', return_value=None), \
          mock.patch('cephadm.is_available', return_value=True), \
          mock.patch('cephadm.json_loads_retry', return_value={'epoch' : 1}), \
