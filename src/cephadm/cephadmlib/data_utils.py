646630afe8a743039b46967e0c439e1667d10072
John Mulligan <jmulligan@redhat.com>
2023-08-17 14:27:09 -0400

    cephadm: move byte string manipulation funcs to data_utils.py
    Signed-off-by: John Mulligan <jmulligan@redhat.com>
    Pair-programmed-with: Adam King <adking@redhat.com>
    Co-authored-by: Adam King <adking@redhat.com>

--- src/cephadm/cephadmlib/data_utils.py
+++ src/cephadm/cephadmlib/data_utils.py
@@ -39,3 +39,48 @@ def dict_get_join(d: Dict[str, Any], key: str) -> Any:
     if isinstance(value, list):
         value = '\n'.join(map(str, value))
     return value
+
+
+def bytes_to_human(num, mode='decimal'):
+    # type: (float, str) -> str
+    """Convert a bytes value into it's human-readable form.
+
+    :param num: number, in bytes, to convert
+    :param mode: Either decimal (default) or binary to determine divisor
+    :returns: string representing the bytes value in a more readable format
+    """
+    unit_list = ['', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB']
+    divisor = 1000.0
+    yotta = 'YB'
+
+    if mode == 'binary':
+        unit_list = ['', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB']
+        divisor = 1024.0
+        yotta = 'YiB'
+
+    for unit in unit_list:
+        if abs(num) < divisor:
+            return '%3.1f%s' % (num, unit)
+        num /= divisor
+    return '%.1f%s' % (num, yotta)
+
+
+def with_units_to_int(v: str) -> int:
+    if v.endswith('iB'):
+        v = v[:-2]
+    elif v.endswith('B'):
+        v = v[:-1]
+    mult = 1
+    if v[-1].upper() == 'K':
+        mult = 1024
+        v = v[:-1]
+    elif v[-1].upper() == 'M':
+        mult = 1024 * 1024
+        v = v[:-1]
+    elif v[-1].upper() == 'G':
+        mult = 1024 * 1024 * 1024
+        v = v[:-1]
+    elif v[-1].upper() == 'T':
+        mult = 1024 * 1024 * 1024 * 1024
+        v = v[:-1]
+    return int(float(v) * mult)
