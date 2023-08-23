07221bb4a245cca1937940dc6249b96528229f29
Ali Maredia <amaredia@redhat.com>
2023-08-23 01:05:37 -0400

    exporter: check key exists before json::object at()
    Signed-off-by: Ali Maredia <amaredia@redhat.com>

--- src/exporter/DaemonMetricCollector.cc
+++ src/exporter/DaemonMetricCollector.cc
@@ -160,8 +160,10 @@ void DaemonMetricCollector::dump_asok_metrics() {
             labels.insert(multisite_labels_and_name.first.begin(), multisite_labels_and_name.first.end());
             counter_name = multisite_labels_and_name.second;
           }
-          auto perf_values = counters_values.at(counter_name_init);
-          dump_asok_metric(counter_group, perf_values, counter_name, labels);
+          if (counters_values.find(counter_name_init) != counters_values.end()) {
+            auto perf_values = counters_values.at(counter_name_init);
+            dump_asok_metric(counter_group, perf_values, counter_name, labels);
+          }
         }
       }
     }
