a069fd4f754ffc58f41b6d764a1e92b09b917b8b
Samarah <samarah.uriarte@ibm.com>
2023-08-24 14:20:22 +0000

    test/d4n: Bad file descriptor error
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/test/rgw/test_d4n_directory.cc
+++ src/test/rgw/test_d4n_directory.cc
@@ -20,9 +20,6 @@ using boost::redis::response;
 string portStr;
 string hostStr;
 string redisHost = "";
-string oid = "testName";
-string bucketName = "testBucket";
-int blockSize = 123;
 
 class DirectoryFixture: public ::testing::Test {
   protected:
@@ -35,29 +32,32 @@ class DirectoryFixture: public ::testing::Test {
       work = new std::optional<Work>(io.get_executor());
       worker = new std::thread([&] { io.run(); });
 
+      /* Create block directory and cache block */
       blockDir = new rgw::d4n::BlockDirectory(io, hostStr, stoi(portStr));
-      cacheBlock = new rgw::d4n::CacheBlock();
+      block = new rgw::d4n::CacheBlock();
 
-      cacheBlock->hostsList.push_back(redisHost);
-      cacheBlock->size = blockSize; 
-      cacheBlock->cacheObj.bucketName = bucketName;
-      cacheBlock->cacheObj.objName = oid;
+      block->version = 0;
+      block->size = 0; 
+      block->globalWeight = 0; 
+      block->hostsList.push_back(redisHost);
 
-     // blockDir->init();
-      
+      block->cacheObj.objName = "testName";
+      block->cacheObj.bucketName = "testBucket";
+      block->cacheObj.creationTime = 0;
+      block->cacheObj.dirty = false;
+      block->cacheObj.hostsList.push_back(redisHost);
+
+      /* Run fixture's connection */
       config cfg;
-      cfg.addr.host = "127.0.0.1";
-      cfg.addr.port = "6379";
+      cfg.addr.host = hostStr;
+      cfg.addr.port = portStr;
 
       conn->async_run(cfg, {}, net::detached);
     } 
 
     virtual void TearDown() {
       delete blockDir;
-      blockDir = nullptr;
-
-      delete cacheBlock;
-      cacheBlock = nullptr;
+      delete block;
 
       io.stop();
 
@@ -67,7 +67,7 @@ class DirectoryFixture: public ::testing::Test {
     }
 
     rgw::d4n::BlockDirectory* blockDir;
-    rgw::d4n::CacheBlock* cacheBlock;
+    rgw::d4n::CacheBlock* block;
 
     net::io_context io;
     connection* conn;
@@ -76,12 +76,17 @@ class DirectoryFixture: public ::testing::Test {
     using Work = net::executor_work_guard<Executor>;
     std::optional<Work>* work;
     std::thread* worker;
+
+    std::vector<std::string> vals{"0", "0", "0", redisHost, 
+                                   "testName", "testBucket", "0", "0", redisHost};
+    std::vector<std::string> fields{"version", "size", "globalWeight", "blockHosts", 
+                                   "objName", "bucketName", "creationTime", "dirty", "objHosts"};
 };
 
 /* Successful initialization */
 TEST_F(DirectoryFixture, DirectoryInit) {
   ASSERT_NE(blockDir, nullptr);
-  ASSERT_NE(cacheBlock, nullptr);
+  ASSERT_NE(block, nullptr);
   ASSERT_NE(redisHost.length(), (long unsigned int)0);
 
   conn->cancel();
@@ -89,128 +94,100 @@ TEST_F(DirectoryFixture, DirectoryInit) {
   worker->join();
 }
 
-#if 0
 /* Successful set_value Call and Redis Check */
 TEST_F(DirectoryFixture, SetValueTest) {
-  cpp_redis::client client;
-  int key_exist = -1;
-  string key;
-  string hosts;
-  string size;
-  string bucketName;
-  string objName;
-  std::vector<std::string> fields;
-  int setReturn = blockDir->set_value(cacheBlock, null_yield);
+  blockDir->init();
+  int setReturn = blockDir->set_value(block, null_yield);
 
   ASSERT_EQ(setReturn, 0);
 
-  fields.push_back("key");
-  fields.push_back("hosts");
-  fields.push_back("size");
-  fields.push_back("bucketName");
-  fields.push_back("objName");
+  #if 0
+  vector<std::string> results;
+  request req;
+  req.push_range("HGETALL", "testBucket_testName_0");
+  //req.push_range("HMGET", "testBucket_testName_0", fields);
+  //req.push("FLUSHALL");
 
-  client.connect(hostStr, stoi(portStr), nullptr, 0, 5, 1000);
-  ASSERT_EQ((bool)client.is_connected(), (bool)1);
+  response< std::map<std::string, std::string>,
+            boost::redis::ignore_t > resp;
 
-  client.hmget("rgw-object:" + oid + ":block-directory", fields, [&key, &hosts, &size, &bucketName, &objName, &key_exist](cpp_redis::reply& reply) {
-    auto arr = reply.as_array();
+  conn->async_exec(req, resp, [&](auto ec, auto) {
+    ASSERT_EQ((bool)ec, false);
 
-    if (!arr[0].is_null()) {
-      key_exist = 0;
-      key = arr[0].as_string();
-      hosts = arr[1].as_string();
-      size = arr[2].as_string();
-      bucketName = arr[3].as_string();
-      objName = arr[4].as_string();
+    /*for (auto const& pair : std::get<0>(resp).value()) {
+      results.push_back(pair.second);
     }
+    EXPECT_EQ(results, vals);*/
+
+
+    for (auto const& pair : std::get<0>(resp).value()) {
+      if (pair.first == "version")
+	EXPECT_EQ(pair.second, vals[0]);
+      else if (pair.first == "size")
+	EXPECT_EQ(pair.second, vals[1]);
+      else if (pair.first == "globalWeight")
+	EXPECT_EQ(pair.second, vals[2]);
+      else if (pair.first == "blockHosts")
+	EXPECT_EQ(pair.second, vals[3]);
+      else if (pair.first == "objName")
+	EXPECT_EQ(pair.second, vals[4]);
+      else if (pair.first == "bucketName")
+	EXPECT_EQ(pair.second, vals[5]);
+      else if (pair.first == "creationTime")
+	EXPECT_EQ(pair.second, vals[6]);
+      else if (pair.first == "dirty")
+	EXPECT_EQ(pair.second, vals[7]);
+      else if (pair.first == "objHosts")
+	EXPECT_EQ(pair.second, vals[8]);
+      else
+        EXPECT_EQ(false, true); /* Redundant fail; this statement should not be entered */
+    }
+
+    conn->cancel();
   });
-
-  client.sync_commit();
-
-  EXPECT_EQ(key_exist, 0);
-  EXPECT_EQ(key, "rgw-object:" + oid + ":block-directory");
-  EXPECT_EQ(hosts, redisHost);
-  EXPECT_EQ(size, to_string(blockSize));
-  EXPECT_EQ(bucketName, bucketName);
-  EXPECT_EQ(objName, oid);
-
-  client.flushall();
+  #endif
 
   *work = std::nullopt;
   worker->join();
 }
 
+#if 0
 /* Successful get_value Calls and Redis Check */
 TEST_F(DirectoryFixture, GetValueTest) {
-  cpp_redis::client client;
-  int key_exist = -1;
-  string key;
-  string hosts;
-  string size;
-  string bucketName;
-  string objName;
-  std::vector<std::string> fields;
-  int setReturn = blockDir->set_value(cacheBlock, null_yield);
+  blockDir->init();
+  int setReturn = blockDir->set_value(block, null_yield);
 
   ASSERT_EQ(setReturn, 0);
 
-  fields.push_back("key");
-  fields.push_back("hosts");
-  fields.push_back("size");
-  fields.push_back("bucketName");
-  fields.push_back("objName");
-
-  client.connect(hostStr, stoi(portStr), nullptr, 0, 5, 1000);
-  ASSERT_EQ((bool)client.is_connected(), (bool)1);
-
-  client.hmget("rgw-object:" + oid + ":block-directory", fields, [&key, &hosts, &size, &bucketName, &objName, &key_exist](cpp_redis::reply& reply) {
-    auto arr = reply.as_array();
-
-    if (!arr[0].is_null()) {
-      key_exist = 0;
-      key = arr[0].as_string();
-      hosts = arr[1].as_string();
-      size = arr[2].as_string();
-      bucketName = arr[3].as_string();
-      objName = arr[4].as_string();
-    }
-  });
-
-  client.sync_commit();
-
-  EXPECT_EQ(key_exist, 0);
-  EXPECT_EQ(key, "rgw-object:" + oid + ":block-directory");
-  EXPECT_EQ(hosts, redisHost);
-  EXPECT_EQ(size, to_string(blockSize));
-  EXPECT_EQ(bucketName, bucketName);
-  EXPECT_EQ(objName, oid);
-
   /* Check if object name in directory instance matches redis update */
-  client.hset("rgw-object:" + oid + ":block-directory", "objName", "newoid", [](cpp_redis::reply& reply) {
-    if (!reply.is_null()) {
-      ASSERT_EQ(reply.as_integer(), 0);
-    }
+  request req;
+  req.push("HSET", "testBucket_testName_0");
+  req.push("FLUSHALL");
+
+  response<std::string, boost::redis::ignore_t> resp;
+
+  conn->async_exec(req, resp, [&](auto ec, auto) {
+    ASSERT_EQ((bool)ec, false);
+    EXPECT_EQ(std::get<0>(resp).value(), "OK");
+
+    conn->cancel();
   });
 
-  client.sync_commit();
-
-  int getReturn = blockDir->get_value(cacheBlock);
+  int getReturn = blockDir->get_value(block, null_yield);
 
   ASSERT_EQ(getReturn, 0);
-  EXPECT_EQ(cacheBlock->cacheObj.objName, "newoid");
-
-  client.flushall();
 
   *work = std::nullopt;
   worker->join();
+
+  EXPECT_EQ(block->cacheObj.objName, "newoid");
 }
 
 /* Successful del_value Call and Redis Check */
 TEST_F(DirectoryFixture, DelValueTest) {
   cpp_redis::client client;
   vector<string> keys;
-  int setReturn = blockDir->set_value(cacheBlock, null_yield);
+  int setReturn = blockDir->set_value(block, null_yield);
 
   ASSERT_EQ(setReturn, 0);
 
@@ -223,7 +200,7 @@ TEST_F(DirectoryFixture, DelValueTest) {
     }
   });
 
-  int delReturn = blockDir->del_value(cacheBlock, null_yield);
+  int delReturn = blockDir->del_value(block, null_yield);
 
   ASSERT_EQ(delReturn, 0);
 
