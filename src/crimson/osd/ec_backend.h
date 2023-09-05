ac358b25838470209c5f4a9d15f496dc4ea284c4
Radosław Zarzyński <rzarzyns@redhat.com>
2023-09-05 15:21:21 +0200

    crimson: drop store from ECBackend to not shadow PGBackend::store
    This problem was leading to crashes like this one:
    
    ```
    ../src/crimson/os/futurized_store.h:113:41: runtime error: member access within misaligned address 0xbebebebebebebebe for type 'struct Shard', which requires 8 byte alignment
    0xbebebebebebebebe: note: pointer points here
    <memory cannot be printed>
    --Type <RET> for more, q to quit, c to continue without paging--
    
    Thread 1 "crimson-osd" received signal SIGSEGV, Segmentation fault.
    0x000055558e7a1dca in crimson::os::FuturizedStore::Shard::do_transaction (txn=..., ch=..., this=0xbebebebebebebebe) at ../src/crimson/os/futurized_store.h:113
    113	      return do_transaction_no_callbacks(
    (gdb) bt
    ```
    
    Signed-off-by: Radosław Zarzyński <rzarzyns@redhat.com>

--- src/crimson/osd/ec_backend.h
+++ src/crimson/osd/ec_backend.h
@@ -33,7 +33,6 @@ private:
 		      epoch_t min_epoch, epoch_t max_epoch,
 		      std::vector<pg_log_entry_t>&& log_entries) final;
   CollectionRef coll;
-  crimson::os::FuturizedStore::Shard* store;
   seastar::future<> request_committed(const osd_reqid_t& reqid,
 				       const eversion_t& version) final {
     return seastar::now();
