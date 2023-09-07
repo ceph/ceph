05f1ea9416e4db17e9b18d58f8d1ec370b9c1d46
Samarah <samarah.uriarte@ibm.com>
2023-09-07 21:38:20 +0000

    test/d4n: Add tests to policy test program
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/test/rgw/test_d4n_policy.cc
+++ src/test/rgw/test_d4n_policy.cc
@@ -40,29 +40,15 @@ class Environment : public ::testing::Environment {
     DoutPrefixProvider* dpp;
 };
 
-class PolicyFixture: public ::testing::Test {
+class LFUDAPolicyFixture: public ::testing::Test {
   protected:
     virtual void SetUp() {
       rgw::cache::Partition partition_info{ .location = "RedisCache" };
       cacheDriver = new rgw::cache::RedisDriver{io, partition_info};
       policyDriver = new rgw::d4n::PolicyDriver(io, "lfuda");
       dir = new rgw::d4n::BlockDirectory{io};
-      block = new rgw::d4n::CacheBlock{
-        .cacheObj = {
-          .objName = "testName",
-          .bucketName = "testBucket",
-          .creationTime = 0,
-          .dirty = false,
-          .hostsList = { env->redisHost }
-        },
-        .version = 0,
-        .size = 0,
-        .hostsList = { env->redisHost }
-      };
-
       conn = new connection{boost::asio::make_strand(io)};
 
-      ASSERT_NE(block, nullptr);
       ASSERT_NE(dir, nullptr);
       ASSERT_NE(cacheDriver, nullptr);
       ASSERT_NE(policyDriver, nullptr);
@@ -70,6 +56,8 @@ class PolicyFixture: public ::testing::Test {
 
       dir->init(env->cct, env->dpp);
       cacheDriver->initialize(env->cct, env->dpp);
+      policyDriver->init();
+      policyDriver->get_cache_policy()->init(env->cct, env->dpp);
 
       bl.append("test data");
       bufferlist attrVal;
@@ -86,13 +74,11 @@ class PolicyFixture: public ::testing::Test {
 
     virtual void TearDown() {
       delete conn;
-      delete block;
       delete dir;
       delete cacheDriver;
       delete policyDriver;
     }
 
-    rgw::d4n::CacheBlock* block;
     rgw::d4n::BlockDirectory* dir;
     rgw::d4n::PolicyDriver* policyDriver;
     rgw::cache::RedisDriver* cacheDriver;
@@ -102,31 +88,140 @@ class PolicyFixture: public ::testing::Test {
 
     bufferlist bl;
     rgw::sal::Attrs attrs;
+    std::string key = "testName";
 };
 
-TEST_F(PolicyFixture, GetBlockYield)
+TEST_F(LFUDAPolicyFixture, LocalGetBlockYield)
 {
   spawn::spawn(io, [this] (yield_context yield) {
-    ASSERT_EQ(0, cacheDriver->put(env->dpp, "testName", bl, bl.length(), attrs, optional_yield{io, yield}));
-    cacheDriver->shutdown();
+    rgw::d4n::CacheBlock block = rgw::d4n::CacheBlock{
+      .cacheObj = {
+	.objName = "testName",
+	.bucketName = "testBucket",
+	.creationTime = 0,
+	.dirty = false,
+	.hostsList = { env->redisHost }
+      },
+      .version = 0,
+      .size = 0,
+      .hostsList = { env->redisHost }
+    };
 
-    ASSERT_EQ(0, dir->set(block, optional_yield{io, yield}));
+    ASSERT_EQ(0, cacheDriver->put(env->dpp, key, bl, bl.length(), attrs, optional_yield{io, yield}));
+    policyDriver->get_cache_policy()->insert(env->dpp, key, 0, bl.length(), cacheDriver, optional_yield{io, yield});
+
+    /* Change cache age for testing purposes */
+    { 
+      boost::system::error_code ec;
+      request req;
+      req.push("HSET", "lfuda", "age", "5");
+      response<int> resp;
+
+      conn->async_exec(req, resp, yield[ec]);
+
+      ASSERT_EQ((bool)ec, false);
+      EXPECT_EQ(std::get<0>(resp).value(), 0);
+    }
+
+    ASSERT_GE(policyDriver->get_cache_policy()->get_block(env->dpp, &block, cacheDriver, optional_yield{io, yield}), 0);
+
     dir->shutdown();
-
-    ASSERT_EQ(0, policyDriver->get_cache_policy()->get_block(env->dpp, cacheDriver, optional_yield{io, yield}));
+    cacheDriver->shutdown();
     policyDriver->get_cache_policy()->shutdown();
 
-/*    boost::system::error_code ec;
+    boost::system::error_code ec;
     request req;
-    req.push_range("HMGET", "testBucket_testName", fields);
+    req.push("HGET", "RedisCache/testName", "localWeight");
     req.push("FLUSHALL");
 
-    response< std::vector<std::string>,
-	      boost::redis::ignore_t > resp;
+    response<std::string, boost::redis::ignore_t> resp;
 
     conn->async_exec(req, resp, yield[ec]);
 
     ASSERT_EQ((bool)ec, false);
+    EXPECT_EQ(std::get<0>(resp).value(), "5");
+    conn->cancel();
+  });
+
+  io.run();
+}
+
+TEST_F(LFUDAPolicyFixture, RemoteGetBlockYield) // not completely supported yet
+{
+  spawn::spawn(io, [this] (yield_context yield) {
+    rgw::d4n::CacheBlock block = rgw::d4n::CacheBlock{
+      .cacheObj = {
+	.objName = "testName",
+	.bucketName = "testBucket",
+	.creationTime = 0,
+	.dirty = false,
+	.hostsList = { "127.0.0.1:6000" }
+      },
+      .version = 0,
+      .size = 0,
+      .hostsList = { "127.0.0.1:6000" }
+    };
+
+    ASSERT_EQ(0, dir->set(&block, optional_yield{io, yield}));
+    ASSERT_GE(policyDriver->get_cache_policy()->get_block(env->dpp, &block, cacheDriver, optional_yield{io, yield}), 0);
+
+    dir->shutdown();
+    cacheDriver->shutdown();
+    policyDriver->get_cache_policy()->shutdown();
+
+    boost::system::error_code ec;
+    request req;
+//    req.push_range("HMGET", "testBucket_testName", fields);
+    req.push("FLUSHALL");
+
+    response<boost::redis::ignore_t> resp;
+  //  response< std::vector<std::string>,
+//	      boost::redis::ignore_t > resp;
+
+    conn->async_exec(req, resp, yield[ec]);
+
+  /*  ASSERT_EQ((bool)ec, false);
+    EXPECT_EQ(std::get<0>(resp).value(), vals);*/
+    conn->cancel();
+  });
+
+  io.run();
+}
+
+TEST_F(LFUDAPolicyFixture, BackendGetBlockYield)
+{
+  spawn::spawn(io, [this] (yield_context yield) {
+    rgw::d4n::CacheBlock block = rgw::d4n::CacheBlock{
+      .cacheObj = {
+	.objName = "testName",
+	.bucketName = "testBucket",
+	.creationTime = 0,
+	.dirty = false,
+	.hostsList = { "127.0.0.1:6000" }
+      },
+      .version = 0,
+      .size = 0,
+      .hostsList = { "127.0.0.1:6000" }
+    };
+
+    ASSERT_GE(policyDriver->get_cache_policy()->get_block(env->dpp, &block, cacheDriver, optional_yield{io, yield}), 0);
+
+    dir->shutdown();
+    cacheDriver->shutdown();
+    policyDriver->get_cache_policy()->shutdown();
+
+    boost::system::error_code ec;
+    request req;
+//    req.push_range("HMGET", "testBucket_testName", fields);
+    req.push("FLUSHALL");
+
+    response<boost::redis::ignore_t> resp;
+  //  response< std::vector<std::string>,
+//	      boost::redis::ignore_t > resp;
+
+    conn->async_exec(req, resp, yield[ec]);
+
+  /*  ASSERT_EQ((bool)ec, false);
     EXPECT_EQ(std::get<0>(resp).value(), vals);*/
     conn->cancel();
   });
