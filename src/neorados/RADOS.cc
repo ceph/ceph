b5ea320cd41d551b548de7d994137a8cd376032b
Adam Emerson <aemerson@redhat.com>
2023-09-08 16:50:46 -0400

    neorados: Notify fixes
    1. Objecter expects a count of seconds, not milliseconds
    2. Actually return result
    
    Signed-off-by: Adam Emerson <aemerson@redhat.com>

--- src/neorados/RADOS.cc
+++ src/neorados/RADOS.cc
@@ -1451,8 +1451,9 @@ struct NotifyHandler : std::enable_shared_from_this<NotifyHandler> {
 		  bufferlist&& bl) {
     asio::post(
       strand,
-      [this, ec, p = shared_from_this()]() mutable {
+      [this, ec, bl = std::move(bl), p = shared_from_this()]() mutable {
 	finished = true;
+	rbl = std::move(bl);
 	maybe_cleanup(ec);
       });
   }
@@ -1470,7 +1471,7 @@ struct NotifyHandler : std::enable_shared_from_this<NotifyHandler> {
 };
 
 void RADOS::notify_(Object o, IOContext _ioc, bufferlist bl,
-		    std::optional<std::chrono::milliseconds> timeout,
+		    std::optional<std::chrono::seconds> timeout,
 		    NotifyComp c)
 {
   auto oid = reinterpret_cast<const object_t*>(&o.impl);
@@ -1490,9 +1491,11 @@ void RADOS::notify_(Object o, IOContext _ioc, bufferlist bl,
       });
   ObjectOperation rd;
   bufferlist inbl;
+  // 30s is the default in librados. Use that rather than borrowing from CephFS.
+  // TODO add a config option later.
   rd.notify(
     linger_op->get_cookie(), 1,
-    timeout ? timeout->count() : impl->cct->_conf->client_notify_timeout,
+    timeout.value_or(30s).count(),
     bl, &inbl);
 
   impl->objecter->linger_notify(
