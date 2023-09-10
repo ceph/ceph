46d291a7fcd45874b0d7a1998e2aadc88d2464a7
Ronen Friedman <rfriedma@redhat.com>
2023-09-10 13:07:31 -0500

    osd/scrub: introducing random_bool_with_probability()
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -7599,12 +7599,11 @@ void OSD::handle_fast_scrub(MOSDScrub2 *m)
 
 bool OSD::scrub_random_backoff()
 {
-  bool coin_flip = (rand() / (double)RAND_MAX >=
-		    cct->_conf->osd_scrub_backoff_ratio);
-  if (!coin_flip) {
-    dout(20) << "scrub_random_backoff lost coin flip, randomly backing off (ratio: "
-	     << cct->_conf->osd_scrub_backoff_ratio << ")" << dendl;
-    return true;
+  if (random_bool_with_probability(cct->_conf->osd_scrub_backoff_ratio)) {
+    dout(20)
+	<< "scrub_random_backoff lost coin flip, randomly backing off (ratio: "
+	<< cct->_conf->osd_scrub_backoff_ratio << ")" << dendl;
+    return true; // backing off
   }
   return false;
 }
