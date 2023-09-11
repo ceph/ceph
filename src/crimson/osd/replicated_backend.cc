e8502a44319712a07361623e8a3867b205443d55
Radosław Zarzyński <rzarzyns@redhat.com>
2023-09-11 16:24:27 +0200

    crimson/osd: PG::log_operation() is now able to update hset
    Signed-off-by: Radosław Zarzyński <rzarzyns@redhat.com>

--- src/crimson/osd/replicated_backend.cc
+++ src/crimson/osd/replicated_backend.cc
@@ -96,6 +96,7 @@ ReplicatedBackend::submit_transaction(const std::set<pg_shard_t>& pg_shards,
 
   pg.log_operation(
     std::move(log_entries),
+    std::nullopt,
     osd_op_p.pg_trim_to,
     osd_op_p.at_version,
     osd_op_p.pg_committed_to,
