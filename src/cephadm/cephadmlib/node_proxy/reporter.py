22b96bc26f29d69042bf71783d1a6543f4d0d0a7
Guillaume Abrioux <gabrioux@ibm.com>
2023-09-14 16:08:26 +0000

    node-proxy: update reporter agent
    This commit introduces the required changes in order to make
    the reporter agent query the new mgr endpoint '/node-proxy/data'
    
    Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>
    (cherry picked from commit 9a305c5c8e94e12b6c103b3b3f4201f4fc3616c9)

--- src/cephadm/cephadmlib/node_proxy/reporter.py
+++ src/cephadm/cephadmlib/node_proxy/reporter.py
@@ -2,16 +2,17 @@ from threading import Thread
 import requests
 import time
 from .util import Logger
-from typing import Any
+from typing import Dict, Any
 
 log = Logger(__name__)
 
 
 class Reporter:
-    def __init__(self, system: Any, observer_url: str) -> None:
+    def __init__(self, system: Any, data: Dict[str, Any], observer_url: str) -> None:
         self.system = system
         self.observer_url = observer_url
         self.finish = False
+        self.data = data
 
     def stop(self) -> None:
         self.finish = True
@@ -35,11 +36,13 @@ class Reporter:
                 log.logger.info('data ready to be sent to the mgr.')
                 if not self.system.get_system() == self.system.previous_data:
                     log.logger.info('data has changed since last iteration.')
-                    d = self.system.get_system()
+                    self.data['data'] = self.system.get_system()
                     try:
                         # TODO: add a timeout parameter to the reporter in the config file
-                        requests.post(f"{self.observer_url}/", json=d, timeout=5)
-                    except requests.exceptions.RequestException as e:
+                        log.logger.info(f"sending data to {self.observer_url}")
+                        r = requests.post(f"{self.observer_url}", json=self.data, timeout=5, verify=False)
+                    except (requests.exceptions.RequestException,
+                            requests.exceptions.ConnectionError) as e:
                         log.logger.error(f"The reporter couldn't send data to the mgr: {e}")
                         # Need to add a new parameter 'max_retries' to the reporter if it can't
                         # send the data for more than x times, maybe the daemon should stop altogether
