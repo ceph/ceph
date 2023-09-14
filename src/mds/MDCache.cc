ccb968430fbe49a19186010058b0f08997f88e6b
Christopher Hoffman <choffman@redhat.com>
2023-09-14 15:11:26 +0000

    mds: Add fragment to scrub
    Add dirfrag split and merge to scrub process.
    
    Fixes: https://tracker.ceph.com/issues/57481
    Signed-off-by: Christopher Hoffman <choffman@redhat.com>
    (cherry picked from commit eadab41eab7220bd749a8217944157050c9513b4)

--- src/mds/MDCache.cc
+++ src/mds/MDCache.cc
@@ -12514,6 +12514,9 @@ void MDCache::force_readonly()
   mds->mdlog->flush();
 }
 
+void MDCache::maybe_fragment(CDir *dir) {
+  mds->balancer->maybe_fragment(dir, false);
+}
 
 // ==============================================================
 // debug crap
