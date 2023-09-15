53d9e657e42bdf2440cd93a8273eb3ce79d8bd33
Xiubo Li <xiubli@redhat.com>
2023-09-15 08:41:35 +0800

    mds: fix issuing redundant reintegrate/migrate_stray requests
    Just in case a CInode's nlink is 1, and then a unlink request comes
    and then early replies and submits to the MDLogs, but just before
    the MDlogs are flushed a link request comes, and the link request
    also succeeds and early replies to client.
    
    Later when the unlink/link requests' MDLog events are flushed and
    the callbacks are called, which will fire a stray denty reintegration.
    But it will pick the new dentry, which is from the link's request
    and is a remote dentry, to do the reintegration. While in the
    'rename' code when traversing the path it will trigger to call the
    'dn->link_remote()', which later will fire a new stray dentry
    reintegration.
    
    The problem is if the first 'rename' request is retried several
    times, and in each time it will fire a new reintegration, which
    makes no sense and maybe blocked for a very long time dues to some
    reasons and then will be reported as slow request warning.
    
    Fixes: https://tracker.ceph.com/issues/62702
    Signed-off-by: Xiubo Li <xiubli@redhat.com>

--- src/mds/StrayManager.h
+++ src/mds/StrayManager.h
@@ -19,15 +19,30 @@
 #include <list>
 #include "Mutation.h"
 #include "PurgeQueue.h"
+#include "MDSMetaRequest.h"
+#include "CDentry.h"
 
 class MDSRank;
 class CInode;
-class CDentry;
 
 class StrayManager
 {
   // My public interface is for consumption by MDCache
 public:
+  struct StrayEvalRequest : public MDSMetaRequest {
+    CDentry *dentry;
+  public:
+    explicit StrayEvalRequest(int o, ceph_tid_t t, CDentry *d) :
+      MDSMetaRequest(o, t), dentry(d) {
+      dentry->get(CDentry::PIN_PURGING);
+      dentry->reintegration_reqid = t;
+    }
+    ~StrayEvalRequest() {
+      dentry->reintegration_reqid = 0;
+      dentry->put(CDentry::PIN_PURGING);
+    }
+  };
+
   explicit StrayManager(MDSRank *mds, PurgeQueue &purge_queue_);
   void set_logger(PerfCounters *l) {logger = l;}
   void activate();
