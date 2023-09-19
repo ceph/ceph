a7138a690acd1b7aa06981451132c23ab15619f1
Ronen Friedman <rfriedma@redhat.com>
2023-09-19 07:16:03 -0500

    osd/scrub: handle configuration changes in OsdScrub
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- src/osd/OSD.cc
+++ src/osd/OSD.cc
@@ -9839,10 +9839,17 @@ void OSD::handle_conf_change(const ConfigProxy& conf,
   }
 
   if (changed.count("osd_scrub_min_interval") ||
-      changed.count("osd_scrub_max_interval")) {
-    resched_all_scrubs();
-    dout(0) << __func__ << ": scrub interval change" << dendl;
+      changed.count("osd_scrub_max_interval") ||
+      changed.count("osd_deep_scrub_interval")) {
+    service.get_scrub_services().on_config_change();
+    dout(0) << fmt::format(
+		   "{}: scrub interval change (min:{} deep:{} max:{})",
+		   __func__, cct->_conf->osd_scrub_min_interval,
+		   cct->_conf->osd_deep_scrub_interval,
+		   cct->_conf->osd_scrub_max_interval)
+	    << dendl;
   }
+
   check_config();
   if (changed.count("osd_asio_thread_count")) {
     service.poolctx.stop();
