aad54d5ee54abb4231dd8c9020d98f6f256bde75
Cory Snyder <csnyder@1111systems.com>
2023-09-22 08:35:16 +0000

    rgw: radosgw-admin bucket check should only print index entries with --check-objects flag
    Printing all index entries can be very time consuming for large
    buckets and the inability to switch this behavior off makes it
    cumbersome to use the command for fixing bucket stats. This was
    also preventing the command from outputting recalculated bucket
    stats when the --fix flag wasn't specified.
    
    Signed-off-by: Cory Snyder <csnyder@1111systems.com>
    (cherry picked from commit 6b057fe55413c0eaf9959f006584cba6cc4c192a)

--- src/rgw/driver/rados/rgw_bucket.cc
+++ src/rgw/driver/rados/rgw_bucket.cc
@@ -1226,9 +1226,11 @@ int RGWBucketAdminOp::check_index(rgw::sal::Driver* driver, RGWBucketAdminOpStat
   if (ret < 0)
     return ret;
 
-  ret = bucket.check_object_index(dpp, op_state, flusher, y);
-  if (ret < 0)
-    return ret;
+  if (op_state.will_check_objects()) {
+    ret = bucket.check_object_index(dpp, op_state, flusher, y);
+    if (ret < 0)
+      return ret;
+  }
 
   ret = bucket.check_index(dpp, op_state, existing_stats, calculated_stats);
   if (ret < 0)
