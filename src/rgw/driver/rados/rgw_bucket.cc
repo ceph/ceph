d18c2a3c9663bdea4fd5a657d217f84459899e1c
Cory Snyder <csnyder@1111systems.com>
2023-09-25 10:06:41 +0000

    rgw: fix output formatting of bucket index check admin api
    The bucket index check admin API was previously returning invalid
    JSON.
    
    Signed-off-by: Cory Snyder <csnyder@1111systems.com>

--- src/rgw/driver/rados/rgw_bucket.cc
+++ src/rgw/driver/rados/rgw_bucket.cc
@@ -1223,6 +1223,8 @@ int RGWBucketAdminOp::check_index(rgw::sal::Driver* driver, RGWBucketAdminOpStat
   Formatter *formatter = flusher.get_formatter();
   flusher.start(0);
 
+  formatter->open_object_section("bucket_check");
+
   ret = bucket.check_bad_index_multipart(op_state, flusher, dpp, y);
   if (ret < 0)
     return ret;
@@ -1238,6 +1240,8 @@ int RGWBucketAdminOp::check_index(rgw::sal::Driver* driver, RGWBucketAdminOpStat
     return ret;
 
   dump_index_check(existing_stats, calculated_stats, formatter);
+  
+  formatter->close_section();
   flusher.flush();
 
   return 0;
