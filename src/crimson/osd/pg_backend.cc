a1a186e4c3341dd59b6980c042224a451dac1c33
Radosław Zarzyński <rzarzyns@redhat.com>
2023-09-26 17:42:55 +0200

    crimson/osd: settle RMWPipeline and RMWPipeline within ECBackend
    Signed-off-by: Radosław Zarzyński <rzarzyns@redhat.com>

--- src/crimson/osd/pg_backend.cc
+++ src/crimson/osd/pg_backend.cc
@@ -53,7 +53,8 @@ PGBackend::create(pg_t pgid,
 		  crimson::os::CollectionRef coll,
 		  crimson::osd::ShardServices& shard_services,
 		  const ec_profile_t& ec_profile,
-		  DoutPrefixProvider &dpp)
+		  DoutPrefixProvider &dpp,
+		  ECListener &eclistener)
 {
   switch (pool.type) {
   case pg_pool_t::TYPE_REPLICATED:
@@ -66,7 +67,8 @@ PGBackend::create(pg_t pgid,
                                        pool.stripe_width,
 				       pool.fast_read,
 				       pool.allows_ecoverwrites(),
-				       dpp);
+				       dpp,
+				       eclistener);
   default:
     throw runtime_error(seastar::format("unsupported pool type '{}'",
                                         pool.type));
