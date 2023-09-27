ae0ea83aef35d540ccfb6cf8e7255f7de25e2d5c
cloudbehl <cloudbehl@gmail.com>
2023-09-28 04:03:29 +0530

    mgr/dashboard: Add bucket connection details
    Signed-off-by: cloudbehl <cloudbehl@gmail.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-details/rgw-bucket-details.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-details/rgw-bucket-details.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input, OnChanges } from '@angular/core';
+import _ from 'lodash';
+import { Observable } from 'rxjs';
 
 import { RgwBucketService } from '~/app/shared/api/rgw-bucket.service';
+import { RgwDaemonService } from '~/app/shared/api/rgw-daemon.service';
+import { RgwUserService } from '~/app/shared/api/rgw-user.service';
+import { RgwDaemon } from '../models/rgw-daemon';
 
 @Component({
   selector: 'cd-rgw-bucket-details',
@@ -10,15 +15,49 @@ import { RgwBucketService } from '~/app/shared/api/rgw-bucket.service';
 export class RgwBucketDetailsComponent implements OnChanges {
   @Input()
   selection: any;
+  user: any;
 
-  constructor(private rgwBucketService: RgwBucketService) {}
+  // Keys tab
+  keys: any = [];
+  selectedDaemon: Observable<RgwDaemon>;
+  endpoint: string;
+  zonegroupName: string;
+
+  constructor(
+    private rgwBucketService: RgwBucketService,
+    private rgwUserService: RgwUserService,
+    private rgwDaemonService: RgwDaemonService
+  ) {}
 
   ngOnChanges() {
     if (this.selection) {
-      this.rgwBucketService.get(this.selection.bid).subscribe((bucket: object) => {
+      this.selectedDaemon = this.rgwDaemonService.selectedDaemon$;
+      this.rgwBucketService.get(this.selection.bid).subscribe((bucket: any) => {
         bucket['lock_retention_period_days'] = this.rgwBucketService.getLockDays(bucket);
         this.selection = bucket;
+
+        this.rgwUserService.get(bucket.owner).subscribe((resp: object) => {
+          this.user = resp;
+          this.keys = [];
+          if (this.user?.keys) {
+            this.user.keys.forEach((key: any) => {
+              this.keys.push({
+                id: this.keys.length + 1, // Create an unique identifier
+                username: key.user,
+                ref: key
+              });
+            });
+          }
+          this.keys = _.sortBy(this.keys, 'user');
+          getHostNamePort();
+        });
       });
     }
+
+    const getHostNamePort = () =>
+      this.selectedDaemon.subscribe((values) => {
+        this.zonegroupName = values?.zonegroup_name;
+        this.endpoint = 'http://' + values?.server_hostname + ':' + values?.port;
+      });
   }
 }
