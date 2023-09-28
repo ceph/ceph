3389cd58c8d9d44fbf27f3567afea4cfa5b0cce8
Redouane Kachach <rkachach@redhat.com>
2023-09-28 14:21:38 +0200

    mgr/rook: some enhancements on the main script + new files
    Signed-off-by: Redouane Kachach <rkachach@redhat.com>

--- src/pybind/mgr/rook/ci/create-rook-cluster.sh
+++ src/pybind/mgr/rook/ci/create-rook-cluster.sh
@@ -4,6 +4,20 @@ set -eEx
 
 export CEPH_DEV_FOLDER=/home/redo/workspaces/ceph
 
+kcli_run_cmd() {
+    local remote_host="$1"
+    local ssh_command="$2"
+
+    # Run the SSH command and capture its exit status
+    output=$(kcli ssh -u root -- "$remote_host" "$ssh_command" 2>&1)
+    ssh_exit_status=$?
+
+    # Check if the SSH command was successful
+    if [ "$ssh_exit_status" -ne 0 ]; then
+        exit $?
+    fi
+}
+
 cleanup() {
     set +x
     if [[ -n "$JENKINS_HOME" ]]; then
@@ -26,108 +40,113 @@ on_error() {
         echo "Saving MGR logs:"
         echo
         mkdir -p ${CEPH_DEV_FOLDER}/logs
-        active_manager=$(kcli ssh -u root -- cephkube-ctlplane-0 "kubectl rook-ceph ceph mgr dump | grep active_name | awk 'NF>1{print $NF}'")
+        active_manager=$(kcli_run_cmd cephkube-ctlplane-0 "kubectl rook-ceph ceph mgr dump | grep active_name | awk 'NF>1{print $NF}'")
         active_manager=$(echo $active_manager | tr -d \",\')
 
-        kcli ssh -u root -- cephkube-ctlplane-0 'kubectl logs -n rook-ceph -l ceph_daemon_id='${active_manager} > ${CEPH_DEV_FOLDER}/logs/mgr.${active_manager}.rook.log
+        kcli_run_cmd cephkube-ctlplane-0 'kubectl logs -n rook-ceph -l ceph_daemon_id='${active_manager} > ${CEPH_DEV_FOLDER}/logs/mgr.${active_manager}.rook.log
         for vm in cephkube-ctlplane-0 cephkube-worker-0 cephkube-worker-1
         do
             echo "Saving journalctl from VM ${vm}:"
             echo
-            kcli ssh -u root -- ${vm} 'journalctl --no-tail --no-pager -t cloud-init' > ${CEPH_DEV_FOLDER}/logs/journal.ceph-node-0${vm_id}.log || true
+            kcli_run_cmd ${vm} 'journalctl --no-tail --no-pager -t cloud-init' > ${CEPH_DEV_FOLDER}/logs/journal.ceph-node-0${vm_id}.log || true
             echo "Saving container logs:"
             echo
-            kcli ssh -u root -- ${vm} 'podman logs --names --since 30s \$(podman ps -aq)' > ${CEPH_DEV_FOLDER}/logs/container.ceph-node-0${vm_id}.log || true
+            kcli_run_cmd ${vm} 'podman logs --names --since 30s \$(podman ps -aq)' > ${CEPH_DEV_FOLDER}/logs/container.ceph-node-0${vm_id}.log || true
         done
         echo "TEST FAILED."
     fi
 }
 
+build_ceph_image() {
+    : ${REPOTAG:='rkachach/ceph:redo-rook'}
+    cd ${CEPH_DEV_FOLDER}/src/pybind/mgr/rook/ci
+    mkdir -p tmpBuild/rook
+    mkdir -p tmpBuild/orchestrator
+    cp ./../../orchestrator/*.py tmpBuild/orchestrator
+    cp ../*.py tmpBuild/rook
+    docker build --tag ${REPOTAG} .
+    docker push ${REPOTAG}
+    sed "s#testimage#${REPOTAG}#g" rook_cluster_basic.yaml > tmpBuild/testcluster.yaml
+    kcli scp -u root tmpBuild/testcluster.yaml cephkube-ctlplane-0:/root
+    rm -rf tmpBuild
+    cd ${CEPH_DEV_FOLDER}
+}
+
+install_rook_operator() {
+    kcli_run_cmd cephkube-ctlplane-0 'git clone --single-branch --branch master https://github.com/rook/rook.git'
+    kcli_run_cmd cephkube-ctlplane-0 'kubectl create -f /root/rook/deploy/examples/crds.yaml'
+    kcli_run_cmd cephkube-ctlplane-0 'kubectl create -f /root/rook/deploy/examples/common.yaml'
+    kcli_run_cmd cephkube-ctlplane-0 'kubectl create -f /root/rook/deploy/examples/operator.yaml'
+    # wait some time for the operator to start running
+    sleep 20 # use some check instead of just waiting a fixed amount of time
+    kcli_run_cmd cephkube-ctlplane-0 'kubectl -n rook-ceph get pods'
+}
+
+wait_for_rook_operator() {
+    : ${WAIT_OPERATOR_RUNNING:=5}
+    while [[ $(kcli ssh -u root -- cephkube-ctlplane-0 "kubectl get pods --selector=app=rook-ceph-operator -n rook-ceph -o jsonpath='{.items[*].status.phase}'")  != "Running" ]]; do
+	echo "Waiting for operator pod running..."
+	sleep ${WAIT_OPERATOR_RUNNING}
+    done
+    echo "Rook operator running"
+}
+
+install_krew() {
+    kcli scp -u root ${CEPH_DEV_FOLDER}/src/pybind/mgr/rook/ci/install_krew.sh cephkube-ctlplane-0:/root
+    kcli_run_cmd cephkube-ctlplane-0 'chmod +x /root/install_krew.sh'
+    kcli_run_cmd cephkube-ctlplane-0 '/root/install_krew.sh'
+}
+
+prepare_kcli() {
+    : ${VM_IMAGE:='fedora36'}
+    : ${VM_IMAGE_URL:='https://download.fedoraproject.org/pub/fedora/linux/releases/36/Cloud/x86_64/images/Fedora-Cloud-Base-36-1.5.x86_64.qcow2'}
+    kcli create pool cephkubePool -p /tmp/kcli_pool || true
+    kcli delete plan -y cephkube || true
+    kcli download image -p cephkubePool -u ${VM_IMAGE_URL} ${VM_IMAGE}
+    kcli delete plan -y cephkube || true
+    kcli create kube generic --paramfile=./k8cluster.yaml cephkube
+    #export KUBECONFIG=$HOME/.kcli/clusters/cephkube/auth/kubeconfig
+
+    : ${WAIT_MASTER_READY:=30}
+    while [[ -z $(kcli_run_cmd cephkube-ctlplane-0 'journalctl --no-tail --no-pager -t cloud-init' | grep "kcli boot finished") ]]; do
+	echo "Waiting for master node ready..."
+	sleep ${WAIT_MASTER_READY}
+    done
+
+    : ${k8NODESREADY_CHECK_INTERVAL:=30}
+    : ${MIN_READY_NODES:=3}
+    while [[  $(kcli_run_cmd cephkube-ctlplane-0 'kubectl get nodes | grep "Ready" | wc -l') != ${MIN_READY_NODES} ]]; do
+        echo "Waiting for $MIN_READY_NODES nodes ready..."
+        sleep ${k8NODESREADY_CHECK_INTERVAL}
+    done
+    echo "$MIN_READY_NODES nodes ready"
+}
+
+####################################################################
+####################################################################
+
 trap 'on_error $? $LINENO' ERR
 trap 'cleanup $? $LINENO' EXIT
 
-kcli create pool cephkubePool -p /tmp/kcli_pool || true
-kcli delete plan -y cephkube || true
-
 : ${CEPH_DEV_FOLDER:=${PWD}}
-: ${VM_IMAGE:='fedora36'}
-: ${VM_IMAGE_URL:='https://download.fedoraproject.org/pub/fedora/linux/releases/36/Cloud/x86_64/images/Fedora-Cloud-Base-36-1.5.x86_64.qcow2'}
-kcli download image -p cephkubePool -u ${VM_IMAGE_URL} ${VM_IMAGE}
-kcli delete plan -y cephkube || true
-kcli create kube generic --paramfile=./k8cluster.yaml cephkube
-#export KUBECONFIG=$HOME/.kcli/clusters/cephkube/auth/kubeconfig
+prepare_kcli
+install_krew
+install_rook_operator
+build_ceph_image
+wait_for_rook_operator
 
-
-: ${WAIT_MASTER_READY:=30}
-while [[ -z $(kcli ssh -u root -- cephkube-ctlplane-0 'journalctl --no-tail --no-pager -t cloud-init' | grep "kcli boot finished") ]]; do
-    echo "Waiting for master node ready..."
-    sleep ${WAIT_MASTER_READY}
-done
-
-#
-: ${k8NODESREADY_CHECK_INTERVAL:=30}
-: ${MIN_READY_NODES:=3}
-while [[  $(kcli ssh -u root -- cephkube-ctlplane-0 'kubectl get nodes | grep "Ready" | wc -l') != ${MIN_READY_NODES} ]]; do
-        echo "Waiting for $MIN_READY_NODES nodes ready..."
-        sleep ${k8NODESREADY_CHECK_INTERVAL}
-done
-echo "$MIN_READY_NODES nodes ready"
-
-
-# Install krew
-kcli scp -u root ${CEPH_DEV_FOLDER}/src/pybind/mgr/rook/ci/install_krew.sh cephkube-ctlplane-0:/root
-kcli ssh -u root -- cephkube-ctlplane-0 'chmod +x /root/install_krew.sh'
-kcli ssh -u root -- cephkube-ctlplane-0 '/root/install_krew.sh'
-if [[ $? != 0 ]]; then
-        echo "Krew plugin not available. Not possible to continue test"
-        exit 1
-fi
-
-# Install rook operator and ceph cluster
-kcli ssh -u root -- cephkube-ctlplane-0 'git clone --single-branch --branch master https://github.com/rook/rook.git'
-kcli ssh -u root -- cephkube-ctlplane-0 'kubectl create -f /root/rook/deploy/examples/crds.yaml'
-kcli ssh -u root -- cephkube-ctlplane-0 'kubectl create -f /root/rook/deploy/examples/common.yaml'
-kcli ssh -u root -- cephkube-ctlplane-0 'kubectl create -f /root/rook/deploy/examples/operator.yaml'
-# wait some time for the operator to start running
-sleep 20
-kcli ssh -u root -- cephkube-ctlplane-0 'kubectl -n rook-ceph get pods'
-
-# Meanwhile the operator is installed, build the ceph image to test and copy it to the vm cluster
-: ${REPOTAG:='rkachach/ceph:redo-rook'}
-cd ${CEPH_DEV_FOLDER}/src/pybind/mgr/rook/ci
-mkdir -p tmpBuild/rook
-mkdir -p tmpBuild/orchestrator
-cp ./../../orchestrator/*.py tmpBuild/orchestrator
-cp ../*.py tmpBuild/rook
-docker build --tag ${REPOTAG} .
-docker push ${REPOTAG}
-sed "s#testimage#${REPOTAG}#g" rook_cluster_basic.yaml > tmpBuild/testcluster.yaml
-kcli scp -u root tmpBuild/testcluster.yaml cephkube-ctlplane-0:/root
-rm -rf tmpBuild
-cd ${CEPH_DEV_FOLDER}
-
-# wait for operator running
-: ${WAIT_OPERATOR_RUNNING:=5}
-while [[ $(kcli ssh -u root -- cephkube-ctlplane-0 "kubectl get pods --selector=app=rook-ceph-operator -n rook-ceph -o jsonpath='{.items[*].status.phase}'")  != "Running" ]]; do
-    echo "Waiting for operator pod running..."
-    sleep ${WAIT_OPERATOR_RUNNING}
-done
-echo "Rook operator running"
-
-# Deploy ceph cluster
-kcli ssh -u root -- cephkube-ctlplane-0 'kubectl create -f /root/testcluster.yaml'
-
-# Install rook ceph krew plugin
-kcli ssh -u root -- cephkube-ctlplane-0 'kubectl krew install rook-ceph'
+# Deploy ceph cluster and Install rook ceph krew plugin
+kcli_run_cmd cephkube-ctlplane-0 'kubectl create -f /root/testcluster.yaml'
+kcli_run_cmd cephkube-ctlplane-0 'kubectl krew install rook-ceph'
 
 # Wait for ceph cluster ready
 : ${WAIT_CEPH_CLUSTER_RUNNING:=20}
-until [[ $(kcli ssh -u root -- cephkube-ctlplane-0 'kubectl rook-ceph ceph status') =~ .*'health: HEALTH_OK'.* ]]; do
+until [[ $(kcli_run_cmd cephkube-ctlplane-0 'kubectl rook-ceph ceph status') =~ .*'health: HEALTH_OK'.* ]]; do
       echo "Waiting for Ceph cluster installed"
       sleep ${WAIT_CEPH_CLUSTER_RUNNING}
 done
 echo "Ceph cluster installed and running"
 
 # Set rook orchestrator as backend
-kcli ssh -u root -- cephkube-ctlplane-0 'kubectl rook-ceph ceph orch set backend rook'
-kcli ssh -u root -- cephkube-ctlplane-0 'kubectl rook-ceph ceph orch status'
+kcli_run_cmd cephkube-ctlplane-0 'kubectl rook-ceph ceph orch set backend rook'
+kcli_run_cmd cephkube-ctlplane-0 'kubectl rook-ceph ceph orch status'
---
+++ src/pybind/mgr/rook/ci/install_krew.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"'>>~/.bashrc
+source ~/.bashrc
+kubectl krew version
+if [[ $? != 0 ]]; then
+        echo "krew plugin not available. Not possible to continue test"
+        exit 1
+fi
