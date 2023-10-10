9c70adf8080c053950e89b220195e3aa47991f86
avanthakkar <avanjohn@gmail.com>
2023-10-10 22:36:28 +0530

    mgr/nfs: add cmount_path
    Implementing necessary changes for the NFS module to align with the new export block format introduced in nfs-ganesha-V5.6.
    The purpose of these changes is to enhance memory efficiency for exports. To achieve this goal, we have introduced a new field
    called cmount_path under the FSAL block of export. Initially, this is applicable only to CephFS-based exports.
    
    Furthermore, the newly created CephFS exports will now share the same user_id and secret_access_key, which are determined based
    on the NFS cluster name and filesystem name. This results in each export on the same filesystem using a shared connection,
    thereby optimizing resource usage.
    
    Signed-off-by: avanthakkar <avanjohn@gmail.com>
    
    mgr/nfs: fix a unit test failure
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>
    
    mgr/nfs: fix a unit test failure
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>
    
    mgr/nfs: fix a unit test failure
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>
    
    mgr/nfs: enable user management on a per-fs basis
    
    Add back the ability to create a user for a cephfs export but do
    it only for a cluster+filesystem combination. According to the
    ganesha devs this ought to continue sharing a cephfs client connection
    across multiple exports.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>
    
    mgr/nfs: add more unit tests with cmount_path
    
    Add more unit tests for CEPHFS based NFS exports with
    newly added cmount_path field under FSAL.
    Signed-off-by: avanthakkar <avanjohn@gmail.com>
    
    mgr/nfs: fix rgw nfs export when no existing exports
    
    Signed-off-by: avanthakkar <avanjohn@gmail.com>
    
    mgr/nfs: generate user_id & access_key for apply_export(CEPHFS)
    
    Generate user_id & secret_access_key for CephFS based exports
    for apply export. Also ensure the export FSAL block has
    `cmount_path`.
    
    Fixes: https://tracker.ceph.com/issues/63377
    Signed-off-by: avanthakkar <avanjohn@gmail.com>
    
    mgr/nfs: simplify validation, update f-strings, add generate_user_id function
    
    - Improved validation to check cmount_path as a superset of path
    - Replaced format method with f-strings
    - Added generate_user_id function using SHA-1 hash
    - Enhanced error handling and integrated new user_id generation
    
    Signed-off-by: Avan Thakkar <athakkar@redhat.com>

--- src/pybind/mgr/nfs/ganesha_conf.py
+++ src/pybind/mgr/nfs/ganesha_conf.py
@@ -179,8 +179,12 @@ class GaneshaConfParser:
 
 
 class FSAL(object):
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, cmount_path: Optional[str] = "/") -> None:
+        # By default, cmount_path is set to "/", allowing the export to mount at the root level.
+        # This ensures that the export path can be any complete path hierarchy within the Ceph filesystem.
+        # If multiple exports share the same cmount_path and FSAL options, they will share a single CephFS client.
         self.name = name
+        self.cmount_path = cmount_path
 
     @classmethod
     def from_dict(cls, fsal_dict: Dict[str, Any]) -> 'FSAL':
@@ -211,9 +215,11 @@ class CephFSFSAL(FSAL):
                  user_id: Optional[str] = None,
                  fs_name: Optional[str] = None,
                  sec_label_xattr: Optional[str] = None,
-                 cephx_key: Optional[str] = None) -> None:
+                 cephx_key: Optional[str] = None,
+                 cmount_path: Optional[str] = "/") -> None:
         super().__init__(name)
         assert name == 'CEPH'
+        self.cmount_path = cmount_path
         self.fs_name = fs_name
         self.user_id = user_id
         self.sec_label_xattr = sec_label_xattr
@@ -225,7 +231,8 @@ class CephFSFSAL(FSAL):
                    fsal_block.values.get('user_id'),
                    fsal_block.values.get('filesystem'),
                    fsal_block.values.get('sec_label_xattr'),
-                   fsal_block.values.get('secret_access_key'))
+                   fsal_block.values.get('secret_access_key'),
+                   cmount_path=fsal_block.values.get('cmount_path'))
 
     def to_fsal_block(self) -> RawBlock:
         result = RawBlock('FSAL', values={'name': self.name})
@@ -238,6 +245,8 @@ class CephFSFSAL(FSAL):
             result.values['sec_label_xattr'] = self.sec_label_xattr
         if self.cephx_key:
             result.values['secret_access_key'] = self.cephx_key
+        if self.cmount_path:
+            result.values['cmount_path'] = self.cmount_path
         return result
 
     @classmethod
@@ -246,7 +255,8 @@ class CephFSFSAL(FSAL):
                    fsal_dict.get('user_id'),
                    fsal_dict.get('fs_name'),
                    fsal_dict.get('sec_label_xattr'),
-                   fsal_dict.get('cephx_key'))
+                   fsal_dict.get('cephx_key'),
+                   fsal_dict.get('cmount_path'))
 
     def to_dict(self) -> Dict[str, str]:
         r = {'name': self.name}
@@ -256,6 +266,8 @@ class CephFSFSAL(FSAL):
             r['fs_name'] = self.fs_name
         if self.sec_label_xattr:
             r['sec_label_xattr'] = self.sec_label_xattr
+        if self.cmount_path:
+            r['cmount_path'] = self.cmount_path
         return r
 
 
