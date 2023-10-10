915447fd12d7c8f7214b13cd62345023da360a73
Guillaume Abrioux <gabrioux@ibm.com>
2023-10-10 12:42:42 +0000

    node-proxy: rename server.py -> main.py
    This is going to be the entrypoint of node-proxy, let's rename
    this file to main.py
    
    Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>
    (cherry picked from commit d08006f487abd3cecb957b4e82330d0b4ff27d6e)

--- src/cephadm/cephadm.py
+++ src/cephadm/cephadm.py
@@ -40,7 +40,7 @@ from threading import Thread, Event
 from urllib.error import HTTPError, URLError
 from urllib.request import urlopen, Request
 from pathlib import Path
-from cephadmlib.node_proxy.server import NodeProxy
+from cephadmlib.node_proxy.main import NodeProxy
 
 FuncT = TypeVar('FuncT', bound=Callable)
 
