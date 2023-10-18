b35be54ed9f23b7fc7859f054902e37cb88cefd8
Nizamudeen A <nia@redhat.com>
2023-10-18 23:16:09 +0530

    mgr/dashboard: cephfs subvolume list snapshots
    Added a tab for displaying the subvolume snapshots
    - this tab will show an info alert when there are no subvolumes present
    - if the subvolume is present, then it'll be auto-selected by default
    
    Implemented a filter to search the groups and subvolumes by its name.
    Also added a scrollbar when there are too many items in the nav list
    
    Modified the REST APIs to fetch only the names of the resources and
    fetch the info when an API call is requesting for it.
    
    Added unit tests
    
    Fixes: https://tracker.ceph.com/issues/63237
    Signed-off-by: Nizamudeen A <nia@redhat.com>

---
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cephfs/cephfs-subvolume-snapshots-list/cephfs-subvolume-snapshots-list.component.ts
@@ -0,0 +1,148 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Observable, ReplaySubject, forkJoin, of } from 'rxjs';
+import { catchError, shareReplay, switchMap, tap } from 'rxjs/operators';
+import { CephfsSubvolumeGroupService } from '~/app/shared/api/cephfs-subvolume-group.service';
+import { CephfsSubvolumeService } from '~/app/shared/api/cephfs-subvolume.service';
+import { CellTemplate } from '~/app/shared/enum/cell-template.enum';
+import { CdTableColumn } from '~/app/shared/models/cd-table-column';
+import { CdTableFetchDataContext } from '~/app/shared/models/cd-table-fetch-data-context';
+import { CephfsSubvolume, SubvolumeSnapshot } from '~/app/shared/models/cephfs-subvolume.model';
+
+@Component({
+  selector: 'cd-cephfs-subvolume-snapshots-list',
+  templateUrl: './cephfs-subvolume-snapshots-list.component.html',
+  styleUrls: ['./cephfs-subvolume-snapshots-list.component.scss']
+})
+export class CephfsSubvolumeSnapshotsListComponent implements OnInit, OnChanges {
+  @Input() fsName: string;
+
+  context: CdTableFetchDataContext;
+  columns: CdTableColumn[] = [];
+
+  subVolumes$: Observable<CephfsSubvolume[]>;
+  snapshots$: Observable<any[]>;
+  snapshotSubject = new ReplaySubject<SubvolumeSnapshot[]>();
+  subVolumeSubject = new ReplaySubject<CephfsSubvolume[]>();
+
+  subvolumeGroupList: string[] = [];
+  subVolumesList: string[];
+
+  activeGroupName = '';
+  activeSubVolumeName = '';
+
+  isSubVolumesAvailable = false;
+  isLoading = true;
+
+  observables: any = [];
+
+  constructor(
+    private cephfsSubvolumeGroupService: CephfsSubvolumeGroupService,
+    private cephfsSubvolumeService: CephfsSubvolumeService
+  ) {}
+
+  ngOnInit(): void {
+    this.columns = [
+      {
+        name: $localize`Name`,
+        prop: 'name',
+        flexGrow: 1
+      },
+      {
+        name: $localize`Created`,
+        prop: 'info.created_at',
+        flexGrow: 1,
+        cellTransformation: CellTemplate.timeAgo
+      },
+      {
+        name: $localize`Pending Clones`,
+        prop: 'info.has_pending_clones',
+        flexGrow: 0.5,
+        cellTransformation: CellTemplate.badge,
+        customTemplateConfig: {
+          map: {
+            no: { class: 'badge-success' },
+            yes: { class: 'badge-info' }
+          }
+        }
+      }
+    ];
+
+    this.cephfsSubvolumeGroupService
+      .get(this.fsName)
+      .pipe(
+        switchMap((groups) => {
+          // manually adding the group 'default' to the list.
+          groups.unshift({ name: '' });
+
+          const observables = groups.map((group) =>
+            this.cephfsSubvolumeService.existsInFs(this.fsName, group.name).pipe(
+              switchMap((resp) => {
+                if (resp) {
+                  this.subvolumeGroupList.push(group.name);
+                }
+                return of(resp); // Emit the response
+              })
+            )
+          );
+
+          return forkJoin(observables);
+        })
+      )
+      .subscribe(() => {
+        if (this.subvolumeGroupList.length) {
+          this.isSubVolumesAvailable = true;
+        }
+        this.isLoading = false;
+      });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.fsName) {
+      this.subVolumeSubject.next();
+    }
+  }
+
+  selectSubVolumeGroup(subVolumeGroupName: string) {
+    this.activeGroupName = subVolumeGroupName;
+    this.getSubVolumes();
+  }
+
+  selectSubVolume(subVolumeName: string) {
+    this.activeSubVolumeName = subVolumeName;
+    this.getSubVolumesSnapshot();
+  }
+
+  getSubVolumes() {
+    this.subVolumes$ = this.subVolumeSubject.pipe(
+      switchMap(() =>
+        this.cephfsSubvolumeService.get(this.fsName, this.activeGroupName, false).pipe(
+          tap((resp) => {
+            this.subVolumesList = resp.map((subVolume) => subVolume.name);
+            this.activeSubVolumeName = resp[0].name;
+            this.getSubVolumesSnapshot();
+          })
+        )
+      )
+    );
+  }
+
+  getSubVolumesSnapshot() {
+    this.snapshots$ = this.snapshotSubject.pipe(
+      switchMap(() =>
+        this.cephfsSubvolumeService
+          .getSnapshots(this.fsName, this.activeSubVolumeName, this.activeGroupName)
+          .pipe(
+            catchError(() => {
+              this.context.error();
+              return of(null);
+            })
+          )
+      ),
+      shareReplay(1)
+    );
+  }
+
+  fetchData() {
+    this.snapshotSubject.next();
+  }
+}
