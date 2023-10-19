213ad1833187c4994c4987d6183f55feafb07a65
John Mulligan <jmulligan@redhat.com>
2023-10-19 16:19:21 -0400

    cephadm: move tracing get_daemon_args logic to class
    Move the logic used to get deamon args into a method of the
    Tracing class.
    This requires an update to save a reference to ctx.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- src/cephadm/cephadm.py
+++ src/cephadm/cephadm.py
@@ -1663,6 +1663,11 @@ class Tracing(ContainerDaemonForm):
     def uid_gid(self, ctx: CephadmContext) -> Tuple[int, int]:
         return 65534, 65534
 
+    def get_daemon_args(self) -> List[str]:
+        return self.components[self.identity.daemon_type].get(
+            'daemon_args', []
+        )
+
 ##################################
 
 
@@ -2325,7 +2330,8 @@ def get_daemon_args(ctx: CephadmContext, ident: 'DaemonIdentity') -> List[str]:
         monitoring = Monitoring.create(ctx, ident)
         r += monitoring.get_daemon_args()
     elif daemon_type == 'jaeger-agent':
-        r.extend(Tracing.components[daemon_type]['daemon_args'])
+        tracing = Tracing.create(ctx, ident)
+        r += tracing.get_daemon_args()
     elif daemon_type == NFSGanesha.daemon_type:
         nfs_ganesha = NFSGanesha.init(ctx, ident.fsid, ident.daemon_id)
         r += nfs_ganesha.get_daemon_args()
