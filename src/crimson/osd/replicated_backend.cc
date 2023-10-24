d5c5a9da632dc291cb5f4f0eb765c69567a09d77
Radosław Zarzyński <rzarzyns@redhat.com>
2023-10-24 12:25:41 +0200

    crimson/osd: PCBackend::_submit_transaction() takes ObjectContext
    For the sake of ECBackend.
    
    Signed-off-by: Radosław Zarzyński <rzarzyns@redhat.com>

--- src/crimson/osd/replicated_backend.cc
+++ src/crimson/osd/replicated_backend.cc
@@ -37,13 +37,14 @@ ReplicatedBackend::_read(const hobject_t& hoid,
 
 ReplicatedBackend::rep_op_fut_t
 ReplicatedBackend::submit_transaction(const std::set<pg_shard_t>& pg_shards,
-                                      const hobject_t& hoid,
+                                      crimson::osd::ObjectContextRef &&obc,
                                       ceph::os::Transaction&& t,
                                       osd_op_params_t&& opp,
                                       epoch_t min_epoch, epoch_t map_epoch,
 				      std::vector<pg_log_entry_t>&& logv)
 {
   LOG_PREFIX(ReplicatedBackend::submit_transaction);
+  const hobject_t& hoid = obc->obs.oi.soid;
   DEBUGDPP("object {}", dpp, hoid);
   auto log_entries = std::move(logv);
   auto txn = std::move(t);
