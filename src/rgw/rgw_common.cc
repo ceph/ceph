ce17cdb4221adf7a7ce7f1f8621814ba119f4e88
Casey Bodley <cbodley@redhat.com>
2023-11-01 18:27:42 -0400

    rgw: add /admin/account rest apis
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_common.cc
+++ src/rgw/rgw_common.cc
@@ -133,6 +133,7 @@ rgw_http_errors rgw_http_s3_errors({
     { ERR_NO_SUCH_TAG_SET, {404, "NoSuchTagSet"}},
     { ERR_NO_SUCH_BUCKET_ENCRYPTION_CONFIGURATION, {404, "ServerSideEncryptionConfigurationNotFoundError"}},
     { ERR_NO_SUCH_PUBLIC_ACCESS_BLOCK_CONFIGURATION, {404, "NoSuchPublicAccessBlockConfiguration"}},
+    { ERR_ACCOUNT_EXISTS, {409, "AccountAlreadyExists"}},
 });
 
 rgw_http_errors rgw_http_swift_errors({
