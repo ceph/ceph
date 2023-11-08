454a2b9285319d561046be0b482029f5130adfc7
Matan Breizman <mbreizma@redhat.com>
2023-11-08 13:07:29 +0000

    crimson/osd/pg: extend pg lifetime on snap_trimq iteration
    Beacuse the loop's returned future is ignored,
    we should cover the scenario where the pg is removed and the
    snap_trimq iteration didn't complete yet.
    
    Fixes: https://tracker.ceph.com/issues/61653
    
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>

--- src/crimson/osd/pg.cc
+++ src/crimson/osd/pg.cc
@@ -471,6 +471,7 @@ void PG::on_active_actmap()
   logger().debug("{}: {} snap_trimq={}", *this, __func__, snap_trimq);
   peering_state.state_clear(PG_STATE_SNAPTRIM_ERROR);
   // loops until snap_trimq is empty or SNAPTRIM_ERROR.
+  Ref<PG> pg_ref = this;
   std::ignore = seastar::do_until(
     [this] { return snap_trimq.empty()
                     || peering_state.state_test(PG_STATE_SNAPTRIM_ERROR);
@@ -505,7 +506,7 @@ void PG::on_active_actmap()
       }).then([this, trimmed=to_trim] {
         logger().debug("{}: trimmed snap={}", *this, trimmed);
       });
-    }).finally([this] {
+    }).finally([this, pg_ref] {
       logger().debug("{}: PG::on_active_actmap() finished trimming",
                      *this);
       peering_state.state_clear(PG_STATE_SNAPTRIM);
