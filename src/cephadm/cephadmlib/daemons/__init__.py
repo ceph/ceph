312c9203af232b25014b1ab44b34d7046af05f54
John Mulligan <jmulligan@redhat.com>
2023-11-08 19:42:27 -0500

    cephadm: move iscsi class to a new file
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- src/cephadm/cephadmlib/daemons/__init__.py
+++ src/cephadm/cephadmlib/daemons/__init__.py
@@ -2,8 +2,10 @@ from .custom import CustomContainer
 from .tracing import Tracing
 from .ingress import HAproxy, Keepalived
 from .nvmeof import CephNvmeof
+from .iscsi import CephIscsi
 
 __all__ = [
+    'CephIscsi',
     'CephNvmeof',
     'CustomContainer',
     'HAproxy',
