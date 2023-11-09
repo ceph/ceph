de4a373360b73253f3beb414d541179ae1c177f7
John Mulligan <jmulligan@redhat.com>
2023-11-08 19:23:20 -0500

    cephadm: move haproxy and keepalived classes to a new file
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- src/cephadm/cephadmlib/daemons/__init__.py
+++ src/cephadm/cephadmlib/daemons/__init__.py
@@ -1,4 +1,5 @@
 from .custom import CustomContainer
 from .tracing import Tracing
+from .ingress import HAproxy, Keepalived
 
-__all__ = ['CustomContainer', 'Tracing']
+__all__ = ['CustomContainer', 'Tracing', 'HAproxy', 'Keepalived']
