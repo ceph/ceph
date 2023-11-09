cb1190ae9474792ff37a2e056fa1a09b057e5636
John Mulligan <jmulligan@redhat.com>
2023-11-08 19:34:58 -0500

    cephadm: move nvmeof class to a new file
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- src/cephadm/cephadmlib/daemons/__init__.py
+++ src/cephadm/cephadmlib/daemons/__init__.py
@@ -1,5 +1,12 @@
 from .custom import CustomContainer
 from .tracing import Tracing
 from .ingress import HAproxy, Keepalived
+from .nvmeof import CephNvmeof
 
-__all__ = ['CustomContainer', 'Tracing', 'HAproxy', 'Keepalived']
+__all__ = [
+    'CephNvmeof',
+    'CustomContainer',
+    'HAproxy',
+    'Keepalived',
+    'Tracing',
+]
