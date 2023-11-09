98048efa88c858b848dad9b48f74ad53f3b9f7b1
Joshua Baergen <jbaergen@digitalocean.com>
2023-11-09 09:43:19 -0700

    test/librbd: Add a stress test that reproduces a crash during discard journaling
    See the comments in DiscardWithPruneWriteOverlap for details.
    
    Signed-off-by: Joshua Baergen <jbaergen@digitalocean.com>

--- src/test/librbd/test_main.cc
+++ src/test/librbd/test_main.cc
@@ -17,6 +17,7 @@ extern void register_test_image_watcher();
 extern void register_test_internal();
 extern void register_test_journal_entries();
 extern void register_test_journal_replay();
+extern void register_test_journal_stress();
 extern void register_test_migration();
 extern void register_test_mirroring();
 extern void register_test_mirroring_watcher();
@@ -37,6 +38,7 @@ int main(int argc, char **argv)
   register_test_internal();
   register_test_journal_entries();
   register_test_journal_replay();
+  register_test_journal_stress();
   register_test_migration();
   register_test_mirroring();
   register_test_mirroring_watcher();
