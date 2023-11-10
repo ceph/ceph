e4b9ea64b0a8935e91c3350372c1ac1973e7862f
Ilya Dryomov <idryomov@gmail.com>
2023-11-10 11:14:42 +0100

    librbd: resurrect "exists" assert in simple_diff_cb()
    This effectively reverts commit d1472e1b1211 ("librbd: diff_iterate
    needs to handle holes in parent images") which just dropped the assert
    instead of addressing the root cause of reported crashes.
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- src/librbd/api/DiffIterate.cc
+++ src/librbd/api/DiffIterate.cc
@@ -150,11 +150,12 @@ private:
 };
 
 int simple_diff_cb(uint64_t off, size_t len, int exists, void *arg) {
-  // it's possible for a discard to create a hole in the parent image -- ignore
-  if (exists) {
-    interval_set<uint64_t> *diff = static_cast<interval_set<uint64_t> *>(arg);
-    diff->insert(off, len);
-  }
+  // This reads the existing extents in a parent from the beginning
+  // of time.  Since images are thin-provisioned, the extents will
+  // always represent data, not holes.
+  ceph_assert(exists);
+  auto diff = static_cast<interval_set<uint64_t>*>(arg);
+  diff->insert(off, len);
   return 0;
 }
 
