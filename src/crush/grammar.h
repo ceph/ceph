caa9e7a45edc74a7e8eaecb0f300db9c0e3b68bc
Samuel Just <sjust@redhat.com>
2023-11-16 20:09:45 -0800

    crush: add msr rule types and CrushWrapper infrastructure
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crush/grammar.h
+++ src/crush/grammar.h
@@ -170,7 +170,7 @@ struct crush_grammar : public boost::spirit::grammar<crush_grammar>
 				step_emit );
       crushrule = str_p("rule") >> !name >> '{'
 				>> (str_p("id") | str_p("ruleset")) >> posint
-				>> str_p("type") >> ( str_p("replicated") | str_p("erasure") )
+				>> str_p("type") >> ( str_p("replicated") | str_p("erasure") | str_p("msr_firstn") | str_p("msr_indep") )
 				>> !(str_p("min_size") >> posint)
 				>> !(str_p("max_size") >> posint)
 			   >> +step
--- src/osd/OSDMap.cc
+++ src/osd/OSDMap.cc
@@ -1789,6 +1789,8 @@ uint64_t OSDMap::get_features(int entity_type, uint64_t *pmask) const
 	features |= CEPH_FEATURE_CRUSH_TUNABLES3;
       if (crush->is_v5_rule(ruleid))
 	features |= CEPH_FEATURE_CRUSH_TUNABLES5;
+      if (crush->is_msr_rule(ruleid))
+	features |= CEPH_FEATURE_CRUSH_MSR;
     }
   }
   mask |= CEPH_FEATURE_OSDHASHPSPOOL | CEPH_FEATURE_OSD_CACHEPOOL;
@@ -1843,6 +1845,9 @@ ceph_release_t OSDMap::get_min_compat_client() const
 {
   uint64_t f = get_features(CEPH_ENTITY_TYPE_CLIENT, nullptr);
 
+  if (HAVE_FEATURE(f, CRUSH_MSR)) {
+    return ceph_release_t::squid;        // v19.2.0
+  }
   if (HAVE_FEATURE(f, OSDMAP_PG_UPMAP) ||      // v12.0.0-1733-g27d6f43
       HAVE_FEATURE(f, CRUSH_CHOOSE_ARGS)) {    // v12.0.1-2172-gef1ef28
     return ceph_release_t::luminous;  // v12.2.0
