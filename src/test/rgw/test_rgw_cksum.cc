819d397f69adcc6c712d6e113b972fdbdda3135a
Matt Benjamin <mbenjamin@redhat.com>
2023-11-20 18:09:46 -0500

    rgw_cksum: add trival test vectors for sha-format digests
    Computed digests match those produced by sha1sum, sha256sum,
    and sha512sum utilities.
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/test/rgw/test_rgw_cksum.cc
+++ src/test/rgw/test_rgw_cksum.cc
@@ -15,6 +15,7 @@
 
 #include <errno.h>
 #include <iostream>
+#include <fstream>
 #include <string>
 
 #include "gtest/gtest.h"
@@ -23,6 +24,8 @@
 #include "common/ceph_argparse.h"
 #include "common/debug.h"
 #include "rgw/rgw_cksum.h"
+#include <openssl/sha.h>
+#include "rgw/rgw_hex.h"
 
 #define dout_subsys ceph_subsys_rgw
 
@@ -31,6 +34,8 @@ namespace {
   using namespace rgw;
   using namespace rgw::cksum;
 
+  bool verbose = false;
+
   cksum::Type t1 = cksum::Type::blake3;
   cksum::Type t2 = cksum::Type::sha1;
   cksum::Type t3 = cksum::Type::sha256;
@@ -38,18 +43,27 @@ namespace {
   cksum::Type t5 = cksum::Type::crc32;
   cksum::Type t6 = cksum::Type::crc32c;
 
+  std::string lorem =
+    "Lorem ipsum dolor sit amet";
+
   std::string dolor =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod "
-    "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim "
-    "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea "
-    "commodo consequat. Duis aute irure dolor in reprehenderit in voluptate "
-    "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint "
-    "occaecat cupidatat non proident, sunt in culpa qui officia deserunt "
-    "mollit anim id est laborum.";
+    R"(Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.)";
+
+TEST(RGWCksum, Output)
+{
+  auto o_mode = std::ios::out|std::ios::trunc;
+  std::ofstream of;
+  of.open("/tmp/lorem", o_mode);
+  of << lorem;
+  of.close();
+
+  of.open("/tmp/dolor", o_mode);
+  of << dolor;
+  of.close();
+}
 
 TEST(RGWCksum, DigestCRC32)
 {
-
   auto t = cksum::Type::crc32;
   DigestVariant dv = rgw::cksum::digest_factory(t);
   Digest* digest = get_digest(dv);
@@ -69,7 +83,6 @@ TEST(RGWCksum, DigestCRC32)
 
 TEST(RGWCksum, DigestCRC32c)
 {
-
   auto t = cksum::Type::crc32c;
   DigestVariant dv = rgw::cksum::digest_factory(t);
   Digest* digest = get_digest(dv);
@@ -86,6 +99,116 @@ TEST(RGWCksum, DigestCRC32c)
   ASSERT_EQ(cksum.to_base64(), "NGIyZWRjOTU=");
 }
 
+TEST(RGWCksum, DigestSha1)
+{
+  auto t = cksum::Type::sha1;
+  for (const auto input_str : {&lorem, &dolor}) {
+    DigestVariant dv = rgw::cksum::digest_factory(t);
+    Digest *digest = get_digest(dv);
+
+    ASSERT_NE(digest, nullptr);
+
+    std::cout << "input_str:" << input_str << std::endl;
+    digest->Update((const unsigned char *)input_str->c_str(),
+		   input_str->length());
+
+    /* try by hand */
+    unsigned char sha1_hash[SHA_DIGEST_LENGTH]; // == 20
+    ::SHA1((unsigned char *)input_str->c_str(), input_str->length(), sha1_hash);
+    // do some stuff with the hash
+
+    char buf[20 * 2 + 1];
+    memset(buf, 0, sizeof(buf));
+    buf_to_hex(sha1_hash, SHA_DIGEST_LENGTH, buf);
+    std::cout << "byhand sha1 " << buf << std::endl;
+
+    auto cksum = rgw::cksum::finalize_digest(digest, t);
+    std::cout << "computed sha1: " << cksum.hex() << std::endl;
+
+    /* check match with direct OpenSSL mech */
+    ASSERT_TRUE(memcmp(buf, cksum.hex().c_str(),
+		       cksum.hex().length()) == 0);
+
+    if (input_str == &lorem) {
+      /* compare w/known value, openssl sha1 */
+      ASSERT_EQ(cksum.hex(), "38f00f8738e241daea6f37f6f55ae8414d7b0219");
+      /* compare w/known value https://www.base64encode.org/ */
+      ASSERT_EQ(cksum.to_base64(),
+		"MzhmMDBmODczOGUyNDFkYWVhNmYzN2Y2ZjU1YWU4NDE0ZDdiMDIxOQ==");
+    } else { // &dolor
+      /* compare w/known value, openssl sha1 */
+      ASSERT_EQ(cksum.hex(), "cd36b370758a259b34845084a6cc38473cb95e27");
+      /* compare w/known value https://www.base64encode.org/ */
+      ASSERT_EQ(cksum.to_base64(),
+		"Y2QzNmIzNzA3NThhMjU5YjM0ODQ1MDg0YTZjYzM4NDczY2I5NWUyNw==");
+    }
+  }
+}
+
+TEST(RGWCksum, DigestSha256)
+{
+  auto t = cksum::Type::sha256;
+  for (const auto input_str : {&lorem, &dolor}) {
+    DigestVariant dv = rgw::cksum::digest_factory(t);
+    Digest *digest = get_digest(dv);
+
+    ASSERT_NE(digest, nullptr);
+
+    std::cout << "input_str:" << input_str << std::endl;
+    digest->Update((const unsigned char *)input_str->c_str(),
+		   input_str->length());
+
+    auto cksum = rgw::cksum::finalize_digest(digest, t);
+    std::cout << "computed sha1: " << cksum.hex() << std::endl;
+
+    if (input_str == &lorem) {
+      /* compare w/known value, openssl sha1 */
+      ASSERT_EQ(cksum.hex(), "16aba5393ad72c0041f5600ad3c2c52ec437a2f0c7fc08fadfc3c0fe9641d7a3");
+      /* compare w/known value https://www.base64encode.org/ */
+      ASSERT_EQ(cksum.to_base64(),
+		"MTZhYmE1MzkzYWQ3MmMwMDQxZjU2MDBhZDNjMmM1MmVjNDM3YTJmMGM3ZmMwOGZhZGZjM2MwZmU5NjQxZDdhMw==");
+    } else { // &dolor
+      /* compare w/known value, openssl sha1 */
+      ASSERT_EQ(cksum.hex(), "2d8c2f6d978ca21712b5f6de36c9d31fa8e96a4fa5d8ff8b0188dfb9e7c171bb");
+      /* compare w/known value https://www.base64encode.org/ */
+      ASSERT_EQ(cksum.to_base64(),
+		"MmQ4YzJmNmQ5NzhjYTIxNzEyYjVmNmRlMzZjOWQzMWZhOGU5NmE0ZmE1ZDhmZjhiMDE4OGRmYjllN2MxNzFiYg==");
+    }
+  }
+}
+
+TEST(RGWCksum, DigestSha512)
+{
+  auto t = cksum::Type::sha512;
+  for (const auto input_str : {&lorem, &dolor}) {
+    DigestVariant dv = rgw::cksum::digest_factory(t);
+    Digest *digest = get_digest(dv);
+
+    ASSERT_NE(digest, nullptr);
+
+    std::cout << "input_str:" << input_str << std::endl;
+    digest->Update((const unsigned char *)input_str->c_str(),
+		   input_str->length());
+
+    auto cksum = rgw::cksum::finalize_digest(digest, t);
+    std::cout << "computed sha1: " << cksum.hex() << std::endl;
+
+    if (input_str == &lorem) {
+      /* compare w/known value, openssl sha1 */
+      ASSERT_EQ(cksum.hex(), "b1f4aaa6b51c19ffbe4b1b6fa107be09c8acafd7c768106a3faf475b1e27a940d3c075fda671eadf46c68f93d7eabcf604bcbf7055da0dc4eae6743607a2fc3f");
+      /* compare w/known value https://www.base64encode.org/ */
+      ASSERT_EQ(cksum.to_base64(),
+		"YjFmNGFhYTZiNTFjMTlmZmJlNGIxYjZmYTEwN2JlMDljOGFjYWZkN2M3NjgxMDZhM2ZhZjQ3NWIxZTI3YTk0MGQzYzA3NWZkYTY3MWVhZGY0NmM2OGY5M2Q3ZWFiY2Y2MDRiY2JmNzA1NWRhMGRjNGVhZTY3NDM2MDdhMmZjM2Y=");
+    } else { // &dolor
+      /* compare w/known value, openssl sha1 */
+      ASSERT_EQ(cksum.hex(), "8ba760cac29cb2b2ce66858ead169174057aa1298ccd581514e6db6dee3285280ee6e3a54c9319071dc8165ff061d77783100d449c937ff1fb4cd1bb516a69b9");
+      /* compare w/known value https://www.base64encode.org/ */
+      ASSERT_EQ(cksum.to_base64(),
+		"OGJhNzYwY2FjMjljYjJiMmNlNjY4NThlYWQxNjkxNzQwNTdhYTEyOThjY2Q1ODE1MTRlNmRiNmRlZTMyODUyODBlZTZlM2E1NGM5MzE5MDcxZGM4MTY1ZmYwNjFkNzc3ODMxMDBkNDQ5YzkzN2ZmMWZiNGNkMWJiNTE2YTY5Yjk=");
+    }
+  }
+}
+ 
 TEST(RGWCksum, DigestSTR)
 {
   for (auto t : {t1, t2, t3, t4, t5, t6}) {
