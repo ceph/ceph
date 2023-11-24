0e548b3d610e649e0ed57b9ba53cd96ac53468e5
Casey Bodley <cbodley@redhat.com>
2023-11-24 12:23:57 -0500

    cmake: add WITH_QATLIB to build against qatlib
    FindQAT.cmake adds library targets QAT::qat and QAT::usdm for qatlib,
    and FindQatDrv.cmake adds alias targets to the same
    
    src/compressor and src/crypto/qat link against these common QAT targets
    
    src/crypto/qat/qcccrypto.h uses new HAVE_QATDRV define to determine
    which headers to include
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/crypto/qat/qcccrypto.h
+++ src/crypto/qat/qcccrypto.h
@@ -20,6 +20,7 @@
 #include <boost/asio/thread_pool.hpp>
 #include <boost/asio/use_future.hpp>
 extern "C" {
+#ifdef HAVE_QATDRV
 #include "cpa.h"
 #include "cpa_cy_sym_dp.h"
 #include "cpa_cy_im.h"
@@ -29,6 +30,15 @@ extern "C" {
 #include "icp_sal_user.h"
 #include "icp_sal_poll.h"
 #include "qae_mem_utils.h"
+#else
+#include <qat/cpa.h>
+#include <qat/cpa_cy_im.h>
+#include <qat/cpa_cy_sym_dp.h>
+#include <qat/cpa_cy_sym.h>
+#include <qat/qae_mem.h>
+#include <qat/icp_sal_user.h>
+#include <qat/icp_sal_poll.h>
+#endif
 }
 
 class QccCrypto {
