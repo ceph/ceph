edf4bce948477ebd57eaeb9eecdd4beae6a2c6a7
Leonid Usov <leonid.usov@ibm.com>
2023-11-30 16:42:22 +0200

    mds/quiesce: MDSRankQuiesce - integration of the quiesce db manager
    * create an instance of the QuiesceDbManager in the rank
    * update membership with a new mdsmap
    * add an admin socket command for sending requests to the manager
    
    Signed-off-by: Leonid Usov <leonid.usov@ibm.com>

--- src/mds/MDSRank.h
+++ src/mds/MDSRank.h
@@ -151,6 +151,8 @@ class MgrClient;
 class Finisher;
 class ScrubStack;
 class C_ExecAndReply;
+class QuiesceDbManager;
+class QuiesceAgent;
 
 /**
  * The public part of this class's interface is what's exposed to all
@@ -437,6 +439,9 @@ class MDSRank {
 
     bool cluster_degraded = false;
 
+    std::shared_ptr<QuiesceDbManager> quiesce_db_manager;
+    std::shared_ptr<QuiesceAgent> quiesce_agent;
+
     Finisher *finisher;
   protected:
     typedef enum {
@@ -528,6 +533,7 @@ class MDSRank {
     void command_dump_inode(Formatter *f, const cmdmap_t &cmdmap, std::ostream &ss);
     void command_dump_dir(Formatter *f, const cmdmap_t &cmdmap, std::ostream &ss);
     void command_cache_drop(uint64_t timeout, Formatter *f, Context *on_finish);
+    void command_quiesce_db(const cmdmap_t& cmdmap, std::function<void(int, const std::string&, bufferlist&)> on_finish);
 
     // FIXME the state machine logic should be separable from the dispatch
     // logic that calls it.
@@ -565,6 +571,9 @@ class MDSRank {
 
     void handle_mds_recovery(mds_rank_t who);
     void handle_mds_failure(mds_rank_t who);
+
+    void quiesce_cluster_update();
+    void quiesce_agent_setup();
 
     /* Update MDSMap export_targets for this rank. Called on ::tick(). */
     void update_targets();
