dfaec0154527ded6954ead9dd56750249958c61c
Ivo Almeida <ialmeida@redhat.com>
2023-12-10 17:44:33 +0000

    mgr/dashboard: snap schedule module detect/eneable
    Fixes: https://tracker.ceph.com/issues/63768
    Signed-off-by: Ivo Almeida <ialmeida@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cephfs/cephfs-snapshotschedule-list/cephfs-snapshotschedule-list.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cephfs/cephfs-snapshotschedule-list/cephfs-snapshotschedule-list.component.ts
@@ -1,7 +1,15 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges, ViewChild } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { finalize, shareReplay, switchMap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, Subscription, of, timer } from 'rxjs';
+import { finalize, map, shareReplay, switchMap } from 'rxjs/operators';
 import { CephfsSnapshotScheduleService } from '~/app/shared/api/cephfs-snapshot-schedule.service';
 import { CdForm } from '~/app/shared/forms/cd-form';
 import { CdTableAction } from '~/app/shared/models/cd-table-action';
@@ -14,21 +22,31 @@ import { AuthStorageService } from '~/app/shared/services/auth-storage.service';
 import { ModalService } from '~/app/shared/services/modal.service';
 import { Icons } from '~/app/shared/enum/icons.enum';
 import { CellTemplate } from '~/app/shared/enum/cell-template.enum';
+import { MgrModuleService } from '~/app/shared/api/mgr-module.service';
+import { NotificationService } from '~/app/shared/services/notification.service';
+import { BlockUI, NgBlockUI } from 'ng-block-ui';
+import { NotificationType } from '~/app/shared/enum/notification-type.enum';
 
 @Component({
   selector: 'cd-cephfs-snapshotschedule-list',
   templateUrl: './cephfs-snapshotschedule-list.component.html',
   styleUrls: ['./cephfs-snapshotschedule-list.component.scss']
 })
-export class CephfsSnapshotscheduleListComponent extends CdForm implements OnInit, OnChanges {
+export class CephfsSnapshotscheduleListComponent
+  extends CdForm
+  implements OnInit, OnChanges, OnDestroy {
   @Input() fsName!: string;
 
   @ViewChild('pathTpl', { static: true })
   pathTpl: any;
 
+  @BlockUI()
+  blockUI: NgBlockUI;
+
   snapshotSchedules$!: Observable<SnapshotSchedule[]>;
   subject$ = new BehaviorSubject<SnapshotSchedule[]>([]);
-  isLoading$ = new BehaviorSubject<boolean>(true);
+  snapScheduleModuleStatus$ = new BehaviorSubject<boolean>(false);
+  moduleServiceListSub!: Subscription;
   columns: CdTableColumn[] = [];
   tableActions: CdTableAction[] = [];
   context!: CdTableFetchDataContext;
@@ -39,10 +57,15 @@ export class CephfsSnapshotscheduleListComponent extends CdForm implements OnIni
   selectedName: string = '';
   icons = Icons;
 
+  MODULE_NAME = 'snap_schedule';
+  ENABLE_MODULE_TIMER = 2 * 1000;
+
   constructor(
     private snapshotScheduleService: CephfsSnapshotScheduleService,
     private authStorageService: AuthStorageService,
-    private modalService: ModalService
+    private modalService: ModalService,
+    private mgrModuleService: MgrModuleService,
+    private notificationService: NotificationService
   ) {
     super();
     this.permissions = this.authStorageService.getPermissions();
@@ -55,13 +78,27 @@ export class CephfsSnapshotscheduleListComponent extends CdForm implements OnIni
   }
 
   ngOnInit(): void {
+    this.moduleServiceListSub = this.mgrModuleService
+      .list()
+      .pipe(
+        map((modules: any[]) => modules.find((module) => module?.['name'] === this.MODULE_NAME))
+      )
+      .subscribe({
+        next: (module: any) => this.snapScheduleModuleStatus$.next(module?.enabled)
+      });
+
     this.snapshotSchedules$ = this.subject$.pipe(
       switchMap(() =>
-        this.snapshotScheduleService
-          .getSnapshotScheduleList('/', this.fsName)
-          .pipe(finalize(() => this.isLoading$.next(false)))
-      ),
-      shareReplay(1)
+        this.snapScheduleModuleStatus$.pipe(
+          switchMap((status) => {
+            if (!status) {
+              return of([]);
+            }
+            return this.snapshotScheduleService.getSnapshotScheduleList('/', this.fsName);
+          }),
+          shareReplay(1)
+        )
+      )
     );
 
     this.columns = [
@@ -76,6 +113,10 @@ export class CephfsSnapshotscheduleListComponent extends CdForm implements OnIni
     ];
 
     this.tableActions = [];
+  }
+
+  ngOnDestroy(): void {
+    this.moduleServiceListSub.unsubscribe();
   }
 
   fetchData() {
@@ -94,6 +135,50 @@ export class CephfsSnapshotscheduleListComponent extends CdForm implements OnIni
         isEdit: edit
       },
       { size: 'lg' }
+    );
+  }
+
+  enableSnapshotSchedule() {
+    let $obs;
+    const fnWaitUntilReconnected = () => {
+      timer(this.ENABLE_MODULE_TIMER).subscribe(() => {
+        // Trigger an API request to check if the connection is
+        // re-established.
+        this.mgrModuleService.list().subscribe(
+          () => {
+            // Resume showing the notification toasties.
+            this.notificationService.suspendToasties(false);
+            // Unblock the whole UI.
+            this.blockUI.stop();
+            // Reload the data table content.
+            this.notificationService.show(
+              NotificationType.success,
+              $localize`Enabled Snapshot Schedule Module`
+            );
+            // Reload the data table content.
+          },
+          () => {
+            fnWaitUntilReconnected();
+          }
+        );
+      });
+    };
+
+    if (!this.snapScheduleModuleStatus$.value) {
+      $obs = this.mgrModuleService
+        .enable(this.MODULE_NAME)
+        .pipe(finalize(() => this.snapScheduleModuleStatus$.next(true)));
+    }
+    $obs.subscribe(
+      () => undefined,
+      () => {
+        // Suspend showing the notification toasties.
+        this.notificationService.suspendToasties(true);
+        // Block the whole UI to prevent user interactions until
+        // the connection to the backend is reestablished
+        this.blockUI.start($localize`Reconnecting, please wait ...`);
+        fnWaitUntilReconnected();
+      }
     );
   }
 }
