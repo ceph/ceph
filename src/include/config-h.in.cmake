d098334b7c9027c263a40a2bf02b4d299b06497e
Yao guotao <yaoguot@gmail.com>
2023-12-11 17:55:22 +0800

    The compilation of ISAL compress in the current code depends on the macro HAVE_NASM_X64_AVX2. However, the macro HAVE_NASM_X64_AVX2 has been removed, resulting in the compression not using ISAL even if the compressor_zlib_isal parameter is set to true.
    fix: https://tracker.ceph.com/issues/63780
    
    Signed-off-by: Yao guotao <yaoguot@gmail.com>
    (cherry picked from commit b31fbab5bef3f5a18dfc7ab6a7f41ed78b5af1a8)

--- src/include/config-h.in.cmake
+++ src/include/config-h.in.cmake
@@ -250,6 +250,9 @@
 /* we have a recent nasm and are x86_64 */
 #cmakedefine HAVE_NASM_X64
 
+/* nasm can also build the isa-l:avx2 */
+#cmakedefine HAVE_NASM_X64_AVX2
+
 /* nasm can also build the isa-l:avx512 */
 #cmakedefine HAVE_NASM_X64_AVX512
 
