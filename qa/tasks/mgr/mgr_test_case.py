7431992501cd7376f447f63c91729069784fbcf7
Patrick Donnelly <pdonnell@redhat.com>
2023-12-21 11:11:03 -0500

    qa: allow failing whatever the active mgr is
    Vanilla `mgr fail` will just fail the active.
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>
    (cherry picked from commit a066467c9dc6f34a58e4085d05238a5db3f45cba)

--- qa/tasks/mgr/mgr_test_case.py
+++ qa/tasks/mgr/mgr_test_case.py
@@ -29,8 +29,11 @@ class MgrCluster(CephCluster):
     def mgr_stop(self, mgr_id):
         self.mgr_daemons[mgr_id].stop()
 
-    def mgr_fail(self, mgr_id):
-        self.mon_manager.raw_cluster_cmd("mgr", "fail", mgr_id)
+    def mgr_fail(self, mgr_id=None):
+        if mgr_id is None:
+            self.mon_manager.raw_cluster_cmd("mgr", "fail")
+        else:
+            self.mon_manager.raw_cluster_cmd("mgr", "fail", mgr_id)
 
     def mgr_restart(self, mgr_id):
         self.mgr_daemons[mgr_id].restart()
