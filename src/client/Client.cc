2a2066acdcd5606a2315d307850597ffd461740f
Anoop C S <anoopcs@cryptolab.net>
2024-01-08 17:17:59 +0530

    client: Add exception to POSIX ACL xattrs in removexattr
    With an exception to POSIX ACL xattrs all others from "system."
    namespace are found to return ENOTSUP during a removal attempt.
    Hence "system.posix_acl_access" and "system.posix_acl_default"
    are exempted thereby returning success despite their presence
    on files.
    
    Signed-off-by: Anoop C S <anoopcs@cryptolab.net>
    (cherry picked from commit a17dccebba0fbc46ac0d3d57fca3f671b6a99bec)

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -13312,7 +13312,9 @@ int Client::_removexattr(Inode *in, const char *name, const UserPerm& perms)
   if (strncmp(name, "user.", 5) &&
       strncmp(name, "security.", 9) &&
       strncmp(name, "trusted.", 8) &&
-      strncmp(name, "ceph.", 5))
+      strncmp(name, "ceph.", 5) &&
+      strcmp(name, ACL_EA_ACCESS) &&
+      strcmp(name, ACL_EA_DEFAULT))
     return -CEPHFS_EOPNOTSUPP;
 
   const VXattr *vxattr = _match_vxattr(in, name);
@@ -13327,6 +13329,11 @@ int Client::_removexattr(Inode *in, const char *name, const UserPerm& perms)
   req->set_inode(in);
  
   int res = make_request(req, perms);
+
+  if ((!strcmp(name, ACL_EA_ACCESS) ||
+      !strcmp(name, ACL_EA_DEFAULT)) &&
+      res == -CEPHFS_ENODATA)
+    res = 0;
 
   trim_cache();
   ldout(cct, 8) << "_removexattr(" << in->ino << ", \"" << name << "\") = " << res << dendl;
