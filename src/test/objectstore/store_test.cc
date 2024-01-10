scantist-ossops-m2/ceph-ci

os/bluestore: get rid off resulting lba alignment in allocators
Fixes: https://tracker.ceph.com/issues/62815

Signed-off-by: Igor Fedotov <igor.fedotov@croit.io>

src/test/objectstore/store_test.cc
@@ -9524,9 +9524,9 @@ TEST_P(StoreTestSpecificAUSize, BluestoreRepairSharedBlobTest) {
     string key;
     _key_encode_u64(1, &key);
     bluestore_shared_blob_t sb(1);
-    sb.ref_map.get(0x2000, block_size);
-    sb.ref_map.get(0x4000, block_size);
-    sb.ref_map.get(0x4000, block_size);
+    sb.ref_map.get(0x822000, block_size);
+    sb.ref_map.get(0x824000, block_size);
+    sb.ref_map.get(0x824000, block_size);
     bufferlist bl;
     encode(sb, bl);
     bstore->inject_broken_shared_blob_key(key, bl);

ceph/ceph

(cherry picked from commit b0cb41a761dd7c32bde8e29eb12d44519a2988a5)

src/test/objectstore/store_test.cc
@@ -9524,9 +9524,9 @@ TEST_P(StoreTestSpecificAUSize, BluestoreRepairSharedBlobTest) {
     string key;
     _key_encode_u64(1, &key);
     bluestore_shared_blob_t sb(1);
-    sb.ref_map.get(0x2000, block_size);
-    sb.ref_map.get(0x4000, block_size);
-    sb.ref_map.get(0x4000, block_size);
+    sb.ref_map.get(0x412000, block_size);
+    sb.ref_map.get(0x414000, block_size);
+    sb.ref_map.get(0x414000, block_size);
     bufferlist bl;
     encode(sb, bl);
     bstore->inject_broken_shared_blob_key(key, bl);