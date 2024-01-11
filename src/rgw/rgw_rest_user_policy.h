8befc4b0009c20cf9a79896fa5aac70219331cb2
Casey Bodley <cbodley@redhat.com>
2024-01-11 18:06:36 -0500

    rgw/rest: iam user policy api cleanup
    make get_params() virtual and protected. base class always validates
    UserName
    
    add common init_processing() function that calls get_params() and loads
    the user by UserName. this step happens before verify_permission()
    
    set s->err.message in several error paths
    
    add the xmlns="https://iam.amazonaws.com/doc/2010-05-08/" part to the
    responses
    
    return ERR_LIMIT_EXCEEDED instead of ERR_INVALID_REQUEST when
    RGWPutUserPolicy exceeds the policy limit
    
    where RGW_ATTR_USER_POLICY is missing, treat it the same way we treat an
    empty map of policies. this avoids separate error paths
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_rest_user_policy.h
+++ src/rgw/rgw_rest_user_policy.h
@@ -2,22 +2,27 @@
 // vim: ts=8 sw=2 smarttab ft=cpp
 
 #pragma once
+
+#include "rgw_arn.h"
 #include "rgw_rest.h"
+#include "rgw_sal_fwd.h"
 
 class RGWRestUserPolicy : public RGWRESTOp {
 protected:
-  static constexpr int MAX_POLICY_NAME_LEN = 128;
+  std::unique_ptr<rgw::sal::User> user;
+  rgw::ARN user_arn;
   std::string policy_name;
   std::string user_name;
   std::string policy;
 
+  virtual int get_params();
   bool validate_input();
 
 public:
+  int init_processing(optional_yield y) override;
   int verify_permission(optional_yield y) override;
   virtual uint64_t get_op() = 0;
   void send_response() override;
-  void dump(Formatter *f) const;
 };
 
 class RGWUserPolicyRead : public RGWRestUserPolicy {
@@ -33,20 +38,20 @@ public:
 };
 
 class RGWPutUserPolicy : public RGWUserPolicyWrite {
+  int get_params() override;
 public:
   RGWPutUserPolicy() = default;
   void execute(optional_yield y) override;
-  int get_params();
-  const char* name() const override { return "put_user-policy"; }
+  const char* name() const override { return "put_user_policy"; }
   uint64_t get_op() override;
   RGWOpType get_type() override { return RGW_OP_PUT_USER_POLICY; }
 };
 
 class RGWGetUserPolicy : public RGWUserPolicyRead {
+  int get_params() override;
 public:
   RGWGetUserPolicy() = default;
   void execute(optional_yield y) override;
-  int get_params();
   const char* name() const override { return "get_user_policy"; }
   uint64_t get_op() override;
   RGWOpType get_type() override { return RGW_OP_GET_USER_POLICY; }
@@ -56,17 +61,16 @@ class RGWListUserPolicies : public RGWUserPolicyRead {
 public:
   RGWListUserPolicies() = default;
   void execute(optional_yield y) override;
-  int get_params();
   const char* name() const override { return "list_user_policies"; }
   uint64_t get_op() override;
   RGWOpType get_type() override { return RGW_OP_LIST_USER_POLICIES; }
 };
 
 class RGWDeleteUserPolicy : public RGWUserPolicyWrite {
+  int get_params() override;
 public:
   RGWDeleteUserPolicy() = default;
   void execute(optional_yield y) override;
-  int get_params();
   const char* name() const override { return "delete_user_policy"; }
   uint64_t get_op() override;
   RGWOpType get_type() override { return RGW_OP_DELETE_USER_POLICY; }
