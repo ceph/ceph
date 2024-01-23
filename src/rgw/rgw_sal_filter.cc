d42a3760be55dd3c4e3b07944532a082159b7ec0
Casey Bodley <cbodley@redhat.com>
2024-01-23 09:47:58 -0500

    rgw/sal: remove load_account_role_by_name()
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_sal_filter.cc
+++ src/rgw/rgw_sal_filter.cc
@@ -243,15 +243,6 @@ int FilterDriver::load_owner_by_email(const DoutPrefixProvider* dpp,
   return next->load_owner_by_email(dpp, y, email, owner);
 }
 
-int FilterDriver::load_account_role_by_name(const DoutPrefixProvider* dpp,
-                                            optional_yield y,
-                                            std::string_view account_id,
-                                            std::string_view name,
-                                            std::unique_ptr<RGWRole>* role)
-{
-  return next->load_account_role_by_name(dpp, y, account_id, name, role);
-}
-
 int FilterDriver::count_account_roles(const DoutPrefixProvider* dpp,
                                       optional_yield y,
                                       std::string_view account_id,
