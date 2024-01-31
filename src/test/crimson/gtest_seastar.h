fbd9777055171be5e51434af94e600130cb2fef4
Samuel Just <sjust@redhat.com>
2024-01-31 15:34:34 -0800

    crimson/common: add coroutine integration for crimson futures
    Adds coroutine machinery for crimson errorated and interruptible futures.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>
    (cherry picked from commit 1126ec3cf6285abbf6a875ab27daff397f09220b)

--- src/test/crimson/gtest_seastar.h
+++ src/test/crimson/gtest_seastar.h
@@ -3,6 +3,7 @@
 
 #pragma once
 
+#include "crimson/common/errorator.h"
 #include "gtest/gtest.h"
 
 #include "seastar_runner.h"
@@ -16,11 +17,37 @@ struct seastar_test_suite_t : public ::testing::Test {
   }
 
   template <typename Func>
+  void run_ertr(Func &&func) {
+    return run(
+      [func=std::forward<Func>(func)]() mutable {
+	return std::invoke(std::move(func)).handle_error(
+	  crimson::ct_error::assert_all("error"));
+      });
+  }
+
+  template <typename Func>
   void run_async(Func &&func) {
     run(
       [func=std::forward<Func>(func)]() mutable {
 	return seastar::async(std::forward<Func>(func));
       });
+  }
+
+  template <typename F>
+  auto scl(F &&f) {
+    return [fptr = std::make_unique<F>(std::forward<F>(f))]() mutable {
+      return std::invoke(*fptr).finally([fptr=std::move(fptr)] {});
+    };
+  }
+
+  auto run_scl(auto &&f) {
+    return run([this, f=std::forward<decltype(f)>(f)]() mutable {
+      return std::invoke(scl(std::move(f)));
+    });
+  }
+
+  auto run_ertr_scl(auto &&f) {
+    return run_ertr(scl(std::forward<decltype(f)>(f)));
   }
 
   virtual seastar::future<> set_up_fut() { return seastar::now(); }
