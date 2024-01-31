1f152bf9861dd04ace70a62ba8a82c33136b5fb0
Samuel Just <sjust@redhat.com>
2024-01-31 15:33:33 -0800

    crimson/common/interruptible_future: add interruptor::future convenience declaration
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/common/interruptible_future.h
+++ src/crimson/common/interruptible_future.h
@@ -1096,6 +1096,9 @@ struct interruptor
 public:
   using condition = InterruptCond;
 
+  template <typename T>
+  using future = interruptible_future<InterruptCond, T>;
+
   static const void *get_interrupt_cond() {
     return (const void*)interrupt_cond<InterruptCond>.interrupt_cond.get();
   }
