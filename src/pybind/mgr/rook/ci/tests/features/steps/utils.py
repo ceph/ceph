a7a64160cc7f1f733baac12215c369a8d0ca4cba
Redouane Kachach <rkachach@redhat.com>
2024-02-01 10:41:17 +0100

    backport mgr/rook: adding some basic rook e2e testing Fixes: https://tracker.ceph.com/issues/64176
    Signed-off-by: Redouane Kachach <rkachach@redhat.com>

--- src/pybind/mgr/rook/ci/tests/features/steps/utils.py
+++ src/pybind/mgr/rook/ci/tests/features/steps/utils.py
@@ -1,4 +1,5 @@
 import subprocess
+from difflib import unified_diff
 
 ROOK_CEPH_COMMAND = "minikube kubectl -- -n rook-ceph exec -it deploy/rook-ceph-tools -- "
 CLUSTER_COMMAND = "minikube kubectl -- "
@@ -27,3 +28,25 @@ def run_commands(commands: str) -> str:
         output = execute_command(command)
 
     return output.strip("\n")
+
+def run_k8s_commands(commands: str) -> str:
+    commands_list = commands.split("\n")
+    output = ""
+    for cmd in commands_list:
+        command = CLUSTER_COMMAND + cmd
+        output = execute_command(command)
+
+    return output.strip("\n")
+
+def run_ceph_commands(commands: str) -> str:
+    commands_list = commands.split("\n")
+    output = ""
+    for cmd in commands_list:
+        command = ROOK_CEPH_COMMAND + cmd
+        output = execute_command(command)
+
+    return output.strip("\n")
+
+def display_side_by_side(expected, got):
+    diff = unified_diff(expected.splitlines(), got.splitlines(), lineterm='')
+    print('\n'.join(diff))
