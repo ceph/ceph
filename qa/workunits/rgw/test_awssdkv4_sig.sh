73e939011192a07988add463f95ea1d45e25b288
Matt Benjamin <mbenjamin@redhat.com>
2024-02-02 14:59:20 -0500

    rgw_sigv4: fixes to bootstrap maven/junit5 suite
    The junit5 suite in fact chooses selects transport security (SSL)
    strictly from the endpoint URL.  The test_awssdkv4_sig.sh (or its
    caller?) only needs to export RGW_HTTP_ENDPOINT_URL appropriately
    to get one or the other.
    
    Fix several mistakes in refactoring caught by Ali Maredia.
    Print AccessKey, SecretKey and EndpointURL on startup
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- qa/workunits/rgw/test_awssdkv4_sig.sh
+++ qa/workunits/rgw/test_awssdkv4_sig.sh
@@ -5,18 +5,18 @@
 # $KEYRING need to be set as the path for a vstart clusters Ceph keyring.
 #
 # Example when ceph source is cloned into $HOME and a vstart cluster is already running with a radosgw:
-# $ PATH=~/ceph/build/bin/:$PATH KEYRING=~/ceph/build/keyring ~/ceph/qa/workunits/rgw/test_librgw_file.sh
+# $ PATH=~/ceph/build/bin/:$PATH KEYRING=~/ceph/build/keyring ~/ceph/qa/workunits/rgw/test_awssdkv4_sig.sh
 
 if [ -z ${AWS_ACCESS_KEY_ID} ]
 then
     export AWS_ACCESS_KEY_ID=`openssl rand -base64 20`
     export AWS_SECRET_ACCESS_KEY=`openssl rand -base64 40`
 
-    radosgw-admin user create --uid ceph-test-librgw-file \
+    radosgw-admin user create --uid ceph-test-maven \
        --access-key $AWS_ACCESS_KEY_ID \
        --secret $AWS_SECRET_ACCESS_KEY \
        --display-name "maven test user" \
-       --email librgw@example.com || echo "maven user exists"
+       --email sigv4@example.com || echo "sigv4 maven user exists"
 
     # keyring override for teuthology env
     if [ -z ${KEYRING} ]
@@ -29,9 +29,14 @@ fi
 # the required S3 access_key and secret_key are already exported above, but
 # we need to hook up the S3 endpoints
 
-# the following are taken from 
+# the following are taken from
+
+# XXXX ok, so I think there should be only RGW_HTTP_ENDPOINT_URL and that
+# it may already be set before this script runs, though if we can figure it
+# out, it's ok if we set it...
+
 export RGW_HTTP_ENDPOINT_URL="http://localhost:80"
-export RGW_HTTPS_ENDPOINT_URL="https://localhost:443"
+#export RGW_HTTPS_ENDPOINT_URL="https://localhost:443"
 
 # rgw/test_awssdkv4_sig.sh
 pushd jcksum
