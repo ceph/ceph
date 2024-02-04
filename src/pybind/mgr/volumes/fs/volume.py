849da56546e833d45372f3c9f51732aa5b424cc1
Rishabh Dave <ridave@redhat.com>
2024-02-05 00:29:54 +0530

    mgr/vol: show clone progress in "ceph status" output
    Print a progress bar for ongoing clone job in output of "ceph status".
    
    When multiple clones are ongoing, show 1 progress bar in output of
    "ceph status" shows average of progress made by each clone.
    
    When number of clone job is more than number of clone threads, print 2
    progress bars in output of "ceph status"  command; one for ongoing clone
    jobs and other for ongoing+pending clone jobs.
    
    Fixes: https://tracker.ceph.com/issues/61904
    Signed-off-by: Rishabh Dave <ridave@redhat.com>
    (cherry picked from commit 65b789edfb507fe11db1052b67a703f7d85a4101)

--- src/pybind/mgr/volumes/fs/volume.py
+++ src/pybind/mgr/volumes/fs/volume.py
@@ -30,6 +30,7 @@ from .exception import VolumeException, ClusterError, ClusterTimeout, \
 from .async_cloner import Cloner
 from .purge_queue import ThreadPoolPurgeQueueMixin
 from .operations.template import SubvolumeOpType
+from .stats_util import CloneProgressReporter
 
 if TYPE_CHECKING:
     from volumes import Module
@@ -63,6 +64,8 @@ class VolumeClient(CephfsClient["Module"]):
         self.volspec = VolSpec(mgr.rados.conf_get('client_snapdir'))
         self.cloner = Cloner(self, self.mgr.max_concurrent_clones, self.mgr.snapshot_clone_delay,
                              self.mgr.snapshot_clone_no_wait)
+        self.clone_progress_reporter = CloneProgressReporter(self,
+                                                             self.volspec)
         self.purge_queue = ThreadPoolPurgeQueueMixin(self, 4)
         # on startup, queue purge job for available volumes to kickstart
         # purge for leftover subvolume entries in trash. note that, if the
@@ -937,6 +940,7 @@ class VolumeClient(CephfsClient["Module"]):
                 else:
                     s_subvolume.attach_snapshot(s_snapname, t_subvolume)
                 self.cloner.queue_job(volname)
+                self.clone_progress_reporter.initiate_reporting()
             except VolumeException as ve:
                 try:
                     t_subvolume.remove()
