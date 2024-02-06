f80b1249edb2e003a9c2e026cd38c0d36651e047
Afreen <afreen23.git@gmail.com>
2024-02-06 15:13:58 +0530

    mgr/dashboard: Internal Server Error while accessing the RGW roles tab when policy are attached to roles
    Fixes https://tracker.ceph.com/issues/64270
    
    Issue:
    ======
    Accessing Object->Users-Roles tab causing 500 internal servor error.
    This is due to the "PermissionPolicies" which are attached to role and
    backend was not handling this field for rgw roles.
    
    Fix:
    ====
    Added "PermissionPolicies" as the valid field in backend and updated
    frontend to render the attached policy in formatted JSON
    
    Signed-off-by: Afreen <afreen23.git@gmail.com>

--- src/pybind/mgr/dashboard/controllers/rgw.py
+++ src/pybind/mgr/dashboard/controllers/rgw.py
@@ -850,9 +850,10 @@ edit_role_form = Form(path='/edit',
         "CreateDate": {'cellTemplate': 'date'},
         "MaxSessionDuration": {'cellTemplate': 'duration'},
         "RoleId": {'isHidden': True},
-        "AssumeRolePolicyDocument": {'isHidden': True}
+        "AssumeRolePolicyDocument": {'isHidden': True},
+        "PermissionPolicies": {'isHidden': True}
     },
-    detail_columns=['RoleId', 'AssumeRolePolicyDocument'],
+    detail_columns=['RoleId', 'AssumeRolePolicyDocument', 'PermissionPolicies'],
     meta=CRUDMeta()
 )
 class RgwUserRole(NamedTuple):
@@ -863,6 +864,7 @@ class RgwUserRole(NamedTuple):
     CreateDate: str
     MaxSessionDuration: int
     AssumeRolePolicyDocument: str
+    PermissionPolicies: List
 
 
 @APIRouter('/rgw/realm', Scope.RGW)
