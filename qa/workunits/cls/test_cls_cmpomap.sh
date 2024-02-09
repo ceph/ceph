8e290c03193bfba743b1b110dad614748494d97e
Casey Bodley <cbodley@redhat.com>
2024-02-09 10:26:38 -0500

    qa/upgrade: disable a failing ceph_test_cls_cmpomap test case
    Fixes: https://tracker.ceph.com/issues/52590
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- qa/workunits/cls/test_cls_cmpomap.sh
+++ qa/workunits/cls/test_cls_cmpomap.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 
-ceph_test_cls_cmpomap
+# this test case changed in 16.2.6 so had been failing in pacific-p2p upgrades since
+ceph_test_cls_cmpomap --gtest_filter=-CmpOmap.cmp_vals_u64_invalid_default
 
 exit 0
