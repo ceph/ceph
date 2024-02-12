89f756258fcca820a8ef30898540a31d67407b35
Casey Bodley <cbodley@redhat.com>
2024-02-12 16:44:00 -0500

    rgw/pubsub: topic apis forward post body
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 9d46c21bc6e522b1d85056e1e545125743aace65)

--- src/rgw/driver/rados/rgw_rest_pubsub.h
+++ src/rgw/driver/rados/rgw_rest_pubsub.h
@@ -25,11 +25,13 @@ public:
 // AWS compliant topics handler factory
 class RGWHandler_REST_PSTopic_AWS : public RGWHandler_REST {
   const rgw::auth::StrategyRegistry& auth_registry;
+  bufferlist bl_post_body;
 protected:
   RGWOp* op_post() override;
 public:
-  RGWHandler_REST_PSTopic_AWS(const rgw::auth::StrategyRegistry& _auth_registry) : 
-      auth_registry(_auth_registry) {}
+  RGWHandler_REST_PSTopic_AWS(const rgw::auth::StrategyRegistry& _auth_registry,
+                              bufferlist bl_post_body)
+    :  auth_registry(_auth_registry), bl_post_body(std::move(bl_post_body)) {}
   virtual ~RGWHandler_REST_PSTopic_AWS() = default;
   int postauth_init(optional_yield) override { return 0; }
   int authorize(const DoutPrefixProvider* dpp, optional_yield y) override;
