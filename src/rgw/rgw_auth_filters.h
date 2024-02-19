8b1a8bdf6702d80d13ad75b2fc2017f8dbf0c229
Casey Bodley <cbodley@redhat.com>
2024-02-19 17:31:55 -0500

    rgw: forward_to_master() passes rgw_owner for effective uid header
    when s3 requests get forwarded to the master zone in multisite, we sign
    them as the multisite system user because we need to extend the s3
    protocol. for example, CreateBucket requests issues by a system user
    include an extra response body that encodes the RGWBucketInfo. this way,
    the secondary zone can recreate exactly the same bucket that the master
    zone did
    
    these forwarded requests include a header like "rgwx-uid: myuserid" to
    request that the system user impersonate the given uid. this isn't
    necessary for authorization, because the system user overrides
    permission checks already. but it's important for resource ownership -
    the result of a forwarded CreateBucket request should be a bucket owned
    by "myuserid", not the system user
    
    because this "rgwx-uid" header is concerned with ownership, we pass the
    string encoding of rgw_owner instead of rgw_user. on the receiving side,
    we parse this header in SysReqApplier and override get_aclowner() to
    expose it
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>
    (cherry picked from commit 8d50f6103ff481a6ef4bf7c894e1d4329eefcf49)

--- src/rgw/rgw_auth_filters.h
+++ src/rgw/rgw_auth_filters.h
@@ -3,10 +3,10 @@
 
 #pragma once
 
+#include <optional>
 #include <type_traits>
 
 #include <boost/logic/tribool.hpp>
-#include <boost/optional.hpp>
 
 #include "rgw_service.h"
 #include "rgw_common.h"
@@ -228,6 +228,7 @@ class SysReqApplier : public DecoratedApplier<T> {
   rgw::sal::Driver* driver;
   const RGWHTTPArgs& args;
   mutable boost::tribool is_system;
+  mutable std::optional<ACLOwner> effective_owner;
 
 public:
   template <typename U>
@@ -245,12 +246,23 @@ public:
   void to_str(std::ostream& out) const override;
   void load_acct_info(const DoutPrefixProvider* dpp, RGWUserInfo& user_info) const override;   /* out */
   void modify_request_state(const DoutPrefixProvider* dpp, req_state* s) const override;       /* in/out */
+
+  ACLOwner get_aclowner() const override {
+    if (effective_owner) {
+      return *effective_owner;
+    }
+    return DecoratedApplier<T>::get_aclowner();
+  }
 };
 
 template <typename T>
 void SysReqApplier<T>::to_str(std::ostream& out) const
 {
-  out << "rgw::auth::SysReqApplier" << " -> ";
+  out << "rgw::auth::SysReqApplier";
+  if (effective_owner) {
+    out << '(' << effective_owner->id << ')';
+  }
+  out << " -> ";
   DecoratedApplier<T>::to_str(out);
 }
 
@@ -263,17 +275,19 @@ void SysReqApplier<T>::load_acct_info(const DoutPrefixProvider* dpp, RGWUserInfo
   if (is_system) {
     //ldpp_dout(dpp, 20) << "system request" << dendl;
 
-    rgw_user effective_uid(args.sys_get(RGW_SYS_PARAM_PREFIX "uid"));
-    if (! effective_uid.empty()) {
-      /* We aren't writing directly to user_info for consistency and security
-       * reasons. rgw_get_user_info_by_uid doesn't trigger the operator=() but
-       * calls ::decode instead. */
-      std::unique_ptr<rgw::sal::User> user = driver->get_user(effective_uid);
-      if (user->load_user(dpp, null_yield) < 0) {
-        //ldpp_dout(dpp, 0) << "User lookup failed!" << dendl;
-        throw -EACCES;
+    std::string str = args.sys_get(RGW_SYS_PARAM_PREFIX "uid");
+    if (!str.empty()) {
+      effective_owner.emplace();
+      effective_owner->id = parse_owner(str);
+
+      if (const auto* uid = std::get_if<rgw_user>(&effective_owner->id); uid) {
+        std::unique_ptr<rgw::sal::User> user = driver->get_user(*uid);
+        if (user->load_user(dpp, null_yield) < 0) {
+          //ldpp_dout(dpp, 0) << "User lookup failed!" << dendl;
+          throw -EACCES;
+        }
+        effective_owner->display_name = user->get_display_name();
       }
-      user_info = user->get_info();
     }
   }
 }
