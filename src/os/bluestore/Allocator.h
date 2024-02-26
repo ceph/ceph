e2c64ab649ac4a7408cffd3ae2dc30ebfc31e76a
Igor Fedotov <igor.fedotov@croit.io>
2024-02-26 21:16:46 +0300

    os/bluestore: relocate some allocmap stuff methods
    Signed-off-by: Igor Fedotov <igor.fedotov@croit.io>

--- src/os/bluestore/Allocator.h
+++ src/os/bluestore/Allocator.h
@@ -15,6 +15,8 @@
 #include <functional>
 #include <ostream>
 #include "include/ceph_assert.h"
+#include "global/global_context.h"
+#include "simple_bitmap.h"
 #include "bluestore_types.h"
 
 class Allocator {
@@ -117,6 +119,14 @@ public:
 
   typedef std::vector<free_state_hist_bucket> FreeStateHistogram;
   void build_free_state_histogram(size_t alloc_unit, FreeStateHistogram& hist);
+
+  void copy_in(SimpleBitmap* sbmap, uint64_t alloc_size);
+
+  static bool compare_allocators(Allocator* alloc1, Allocator* alloc2);
+  static Allocator* create_bitmap_allocator(uint64_t bdev_size,
+                                            uint64_t min_alloc_size) {
+    return create(g_ceph_context, "bitmap", bdev_size, min_alloc_size, "recovery");
+  }
 
 private:
   class SocketHook;
