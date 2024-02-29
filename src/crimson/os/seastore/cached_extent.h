01776a081c041ee5b949de0f1a5841ca31baa8b2
Xuehan Xu <xuxuehan@qianxin.com>
2024-02-29 20:17:33 +0800

    crimson/os/seastore/cached_extent: rename get_crc32c to calc_crc32c
    Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

--- src/crimson/os/seastore/cached_extent.h
+++ src/crimson/os/seastore/cached_extent.h
@@ -542,7 +542,7 @@ public:
   }
 
   /// Returns crc32c of buffer
-  virtual uint32_t get_crc32c() const {
+  virtual uint32_t calc_crc32c() const {
     return ceph_crc32c(
       1,
       reinterpret_cast<const unsigned char *>(get_bptr().c_str()),
@@ -1259,7 +1259,7 @@ public:
   void apply_delta_and_adjust_crc(
     paddr_t base, const ceph::bufferlist &bl) final {
     apply_delta(bl);
-    set_last_committed_crc(get_crc32c());
+    set_last_committed_crc(calc_crc32c());
   }
 
   bool is_logical() const final {
