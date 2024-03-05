bddd3c72014dc7aa4bb4f16e4633111a0f2e0bb0
Patrick Donnelly <pdonnell@redhat.com>
2024-03-05 12:19:42 -0500

    mds: prevent new wrlocks on LocalLock if there exists any xlock waiter
    Otherwise, an xlock waiter can become starved as a LocalLock supports multiple
    writers.
    
    Strictly speaking, a new lock state would be appropriate for this but we cheat
    frequently with the LocalLock -- there is only one state. All transition checks
    are already manually performed by the Locker.
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- src/mds/LocalLockC.h
+++ src/mds/LocalLockC.h
@@ -34,7 +34,7 @@ public:
   }
 
   bool can_wrlock() const {
-    return !is_xlocked();
+    return !is_xlocked() && !is_waiter_for(SimpleLock::WAIT_XLOCK);
   }
   void get_wrlock(client_t client) {
     ceph_assert(can_wrlock());
