bbc6e005eb3f3a8e7b3d58e58af54158709203ff
John Mulligan <jmulligan@redhat.com>
2024-03-09 11:37:14 -0500

    cephadm: black format daemons/ceph.py
    Signed-off-by: John Mulligan <jmulligan@redhat.com>
    (cherry picked from commit 3cd5c237837aaa1841f2675cfe08886606b4dc62)
    
    Conflicts:
    	src/cephadm/cephadmlib/daemons/ceph.py

--- src/cephadm/cephadmlib/daemons/ceph.py
+++ src/cephadm/cephadmlib/daemons/ceph.py
@@ -329,7 +329,9 @@ class CephExporter(ContainerDaemonForm):
 
     def validate(self) -> None:
         if not os.path.isdir(self.sock_dir):
-            raise Error(f'Directory does not exist. Got: {self.sock_dir}')
+            raise Error(
+                f'Desired sock dir for ceph-exporter is not directory: {self.sock_dir}'
+            )
 
     def container(self, ctx: CephadmContext) -> CephContainer:
         ctr = daemon_to_container(ctx, self)
