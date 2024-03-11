acbc26ec1273e5d4a6fa95501d94c54b84ed858b
Patrick Donnelly <pdonnell@redhat.com>
2024-03-11 10:46:05 -0400

    qa/crontab: simplify and minimize argument specification
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- qa/nightlies/schedule_subset.sh
+++ qa/nightlies/schedule_subset.sh
@@ -1,20 +1,20 @@
-#!/bin/bash -e
+#!/bin/bash
 
-#command line => CEPH_BRANCH=<branch>; MACHINE_NAME=<machine_type>; SUITE_NAME=<suite>; ../schedule_subset.sh <day_of_week> $CEPH_BRANCH $MACHINE_NAME $SUITE_NAME $CEPH_QA_EMAIL $KERNEL <$FILTER>
+set -e
+
+function prun {
+  printf '%s\n' "$*" >&2
+  "$@"
+}
 
 partitions="$1"
 shift
-branch="$1"
-shift
-machine="$1"
-shift
-suite="$1"
-shift
-email="$1"
-shift
-kernel="$1"
-shift
-# rest of arguments passed directly to teuthology-suite
 
-echo "Scheduling $branch branch"
-teuthology-suite -v -c "$branch" -m "$machine" -k "$kernel" -s "$suite" --ceph-repo https://git.ceph.com/ceph.git --suite-repo https://git.ceph.com/ceph.git --subset "$((RANDOM % partitions))/$partitions" --newest 100 -e "$email" "$@"
+ARGS=()
+ARGS+=("--subset=$((RANDOM % partitions))/$partitions")
+
+if [ -n "$CEPH_QA_EMAIL" ]; then
+  ARGS+=("--email=$CEPH_QA_EMAIL")
+fi
+
+prun teuthology-suite "${ARGS[@]}" $TEUTHOLOGY_SUITE_ARGS "$@"
