9685a2a4f3b747969fb99834d0ea274bac808e12
WeiGuo Ren <weiguo.ren@xtaotech.com>
2024-03-14 11:39:46 +0000

    async/rdma: handle failed qp gracefully
    1.When qp is null,  not access to qp->qp_num.
    2.when qp failed, pending_conn not exist, so could skip remove_pending_conn.
    3.when qp failed, accept() and connect() must delete RDMAConnectedSocketImpl, otherwise memory leak.
    
    Signed-off-by: WeiGuo Ren <weiguo.ren@xtaotech.com>

--- src/msg/async/rdma/Infiniband.cc
+++ src/msg/async/rdma/Infiniband.cc
@@ -1260,9 +1260,9 @@ Infiniband::CompletionQueue* Infiniband::create_comp_queue(
 
 Infiniband::QueuePair::~QueuePair()
 {
-  ldout(cct, 20) << __func__ << " destroy Queue Pair, qp number: " << qp->qp_num << " left SQ WR " << recv_queue.size() << dendl;
+  ldout(cct, 20) << __func__ << " destroy Queue Pair: " << this  << " left SQ WR " << recv_queue.size() << dendl;
   if (qp) {
-    ldout(cct, 20) << __func__ << " destroy qp=" << qp << dendl;
+    ldout(cct, 20) << __func__ << " destroy qp=" << qp << " qp number=" << qp->qp_num  << dendl;
     ceph_assert(!ibv_destroy_qp(qp));
   }
 
