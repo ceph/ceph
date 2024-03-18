f985a87adcf6502729dfc28a3d8eb3cca937595f
nmordech@redhat.com <nmordech@redhat.com>
2024-03-18 05:53:01 +0000

    cmake: add asan

--- cmake/modules/CTestScript.cmake
+++ cmake/modules/CTestScript.cmake
@@ -6,3 +6,9 @@ function(build_tests)
   set(MEMORYCHECK_SUPPRESSIONS_FILE  
     "${CMAKE_SOURCE_DIR}/qa/valgrind.supp")
 endfunction()
+
+function(build_test_asan)
+  set(CTEST_MEMORYCHECK_TYPE "AddressSanitizer")
+	set(CTEST_MEMORYCHECK_SUPPRESSIONS_FILE "${CMAKE_SOURCE_DIR}/qa/asan.supp")
+endfunction(build_test_asan)
+
