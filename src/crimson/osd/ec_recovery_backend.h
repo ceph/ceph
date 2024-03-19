637b3195582479f76961ac3f2562d3685cdfe691
Radoslaw Zarzynski <rzarzyns@redhat.com>
2024-03-19 16:49:06 +0000

    crimson/osd: bring ECRecoveryBackend, phase 0
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

---
+++ src/crimson/osd/ec_recovery_backend.h
@@ -0,0 +1,49 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#pragma once
+
+#include <seastar/core/future.hh>
+
+#include "crimson/common/type_helpers.h"
+#include "crimson/os/futurized_store.h"
+#include "crimson/os/futurized_collection.h"
+#include "crimson/osd/object_context.h"
+#include "crimson/osd/pg_interval_interrupt_condition.h"
+#include "crimson/osd/recovery_backend.h"
+#include "crimson/osd/shard_services.h"
+
+
+#include "messages/MOSDPGBackfill.h"
+#include "messages/MOSDPGBackfillRemove.h"
+#include "messages/MOSDPGScan.h"
+#include "osd/recovery_types.h"
+#include "osd/osd_types.h"
+
+namespace crimson::osd{
+  class PG;
+}
+
+class PGBackend;
+
+class ECRecoveryBackend : public RecoveryBackend {
+public:
+  ECRecoveryBackend(crimson::osd::PG& pg,
+		    crimson::osd::ShardServices& shard_services,
+		    crimson::os::CollectionRef coll,
+		    PGBackend* backend)
+    : RecoveryBackend(pg, shard_services, coll, backend)
+  {}
+
+  interruptible_future<> handle_recovery_op(
+    Ref<MOSDFastDispatchOp> m,
+    crimson::net::ConnectionXcoreRef conn) final;
+
+  interruptible_future<> recover_object(
+    const hobject_t& soid,
+    eversion_t need) final;
+
+  seastar::future<> on_stop() final {
+    return seastar::now();
+  }
+};
