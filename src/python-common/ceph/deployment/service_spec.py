29331919df222e136ce7b0788376859b3a9cb2aa
Teoman ONAY <tonay@ibm.com>
2024-03-26 09:34:16 +0100

    mgr/cephadm: Allows enabling NFS Ganesha NLM
    Enable_NLM was harcoded to "false" which prevented enabling file
    locking. It can now be enabled using enable_nlm in spec file
    
    Fixes: https://tracker.ceph.com/issues/65144
    
    Signed-off-by: Teoman ONAY <tonay@ibm.com>

--- src/python-common/ceph/deployment/service_spec.py
+++ src/python-common/ceph/deployment/service_spec.py
@@ -1093,6 +1093,7 @@ class NFSServiceSpec(ServiceSpec):
                  networks: Optional[List[str]] = None,
                  port: Optional[int] = None,
                  virtual_ip: Optional[str] = None,
+                 enable_nlm: bool = False,
                  enable_haproxy_protocol: bool = False,
                  extra_container_args: Optional[GeneralArgList] = None,
                  extra_entrypoint_args: Optional[GeneralArgList] = None,
@@ -1110,6 +1111,7 @@ class NFSServiceSpec(ServiceSpec):
         self.virtual_ip = virtual_ip
         self.enable_haproxy_protocol = enable_haproxy_protocol
         self.idmap_conf = idmap_conf
+        self.enable_nlm = enable_nlm
 
     def get_port_start(self) -> List[int]:
         if self.port:
