42f0031fee27a0d43f7e549484dc15cc8f1ab529
John Mulligan <jmulligan@redhat.com>
2024-03-27 18:45:15 -0400

    cephadm: fix host-maintenance command always exiting with a failure
    The host-maintenance command would always fail because
    command_maintenance always returns a string. This string is passed to
    sys.exit and thus always gets printed and causes a non-zero exit code.
    Fix the command line behavior by renaming the original function and
    adding a new command_maintenance that prints the string and returns an
    int like other command_* functions do.
    
    Fixes: https://tracker.ceph.com/issues/65122
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>
    (cherry picked from commit 0a137b140e47d02b03d133adb55e62aa0de7b984)

--- src/cephadm/tests/test_cephadm.py
+++ src/cephadm/tests/test_cephadm.py
@@ -1164,7 +1164,7 @@ class TestMaintenance:
         ctx: _cephadm.CephadmContext = _cephadm.cephadm_init_ctx(
             ['host-maintenance', 'enter', '--fsid', TestMaintenance.fsid])
         ctx.container_engine = mock_podman()
-        retval = _cephadm.command_maintenance(ctx)
+        retval = _cephadm.change_maintenance_mode(ctx)
         assert retval.startswith('failed')
 
     @mock.patch('os.listdir', return_value=[])
@@ -1177,7 +1177,7 @@ class TestMaintenance:
         ctx: _cephadm.CephadmContext = _cephadm.cephadm_init_ctx(
             ['host-maintenance', 'enter', '--fsid', TestMaintenance.fsid])
         ctx.container_engine = mock_podman()
-        retval = _cephadm.command_maintenance(ctx)
+        retval = _cephadm.change_maintenance_mode(ctx)
         assert retval.startswith('failed')
 
     @mock.patch('os.listdir', return_value=[])
@@ -1192,7 +1192,7 @@ class TestMaintenance:
         ctx: _cephadm.CephadmContext = _cephadm.cephadm_init_ctx(
             ['host-maintenance', 'exit', '--fsid', TestMaintenance.fsid])
         ctx.container_engine = mock_podman()
-        retval = _cephadm.command_maintenance(ctx)
+        retval = _cephadm.change_maintenance_mode(ctx)
         assert retval.startswith('failed')
 
     @mock.patch('os.listdir', return_value=[])
@@ -1207,7 +1207,7 @@ class TestMaintenance:
         ctx: _cephadm.CephadmContext = _cephadm.cephadm_init_ctx(
             ['host-maintenance', 'exit', '--fsid', TestMaintenance.fsid])
         ctx.container_engine = mock_podman()
-        retval = _cephadm.command_maintenance(ctx)
+        retval = _cephadm.change_maintenance_mode(ctx)
         assert retval.startswith('failed')
 
 
