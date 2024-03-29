ceph/ceph

commit f68248944815acbf5fb08ef66f36da6d5baef824

pybind: use LDFLAGS in env variable when check for building env
when building python bindings extensions using Cython, we first perform
sanity checks by building an executable and linking it against the
shared library of the C language binding. if the executable builds,
we consider the sanity test passes.

before this change, we don't add `LDFLAGS` specified in environmental
variable to the ldflags when building the executable. but we need to
link against libasan, so that the symbols used by, for instance,
librados.so, can be found by the executable.

in this change, we always check `LDFLAGS`, and add it to the ldflags
when building the executable for testing, if it's set.

Signed-off-by: Kefu Chai <tchaikov@gmail.com>

src/pybind/cephfs/setup.py
@@ -117,10 +117,15 @@ def check_sanity():
             extra_preargs=['-iquote{path}'.format(path=os.path.join(CEPH_SRC_DIR, 'include'))]
         )
 
+        if ldflags := os.environ.get('LDFLAGS'):
+            extra_postargs = ldflags.split()
+        else:
+            extra_postargs = None
         compiler.link_executable(
             objects=link_objects,
             output_progname=os.path.join(tmp_dir, 'cephfs_dummy'),
             libraries=['cephfs'],
+            extra_postargs=extra_postargs,
             output_dir=tmp_dir,
         )
 

src/pybind/rados/setup.py
@@ -112,10 +112,15 @@ def check_sanity():
             sources=[tmp_file],
             output_dir=tmp_dir
         )
+        if ldflags := os.environ.get('LDFLAGS'):
+            extra_postargs = ldflags.split()
+        else:
+            extra_postargs = None
         compiler.link_executable(
             objects=link_objects,
             output_progname=os.path.join(tmp_dir, 'rados_dummy'),
             libraries=['rados'],
+            extra_postargs=extra_postargs,
             output_dir=tmp_dir,
         )
 

src/pybind/rbd/setup.py
@@ -116,10 +116,15 @@ def check_sanity():
             output_dir=tmp_dir
         )
 
+        if ldflags := os.environ.get('LDFLAGS'):
+            extra_postargs = ldflags.split()
+        else:
+            extra_postargs = None
         compiler.link_executable(
             objects=link_objects,
             output_progname=os.path.join(tmp_dir, 'rbd_dummy'),
             libraries=['rbd', 'rados'],
+            extra_postargs=extra_postargs,
             output_dir=tmp_dir,
         )
 

src/pybind/rgw/setup.py
@@ -116,10 +116,15 @@ def check_sanity():
             output_dir=tmp_dir,
         )
 
+        if ldflags := os.environ.get('LDFLAGS'):
+            extra_postargs = ldflags.split()
+        else:
+            extra_postargs = None
         compiler.link_executable(
             objects=link_objects,
             output_progname=os.path.join(tmp_dir, 'rgw_dummy'),
             libraries=['rgw', 'rados'],
+            extra_postargs=extra_postargs,
             output_dir=tmp_dir,
         )
 

commit fd5488ed2c64726fb36321a836cdff4de92454bd

pybind: use syntax supported by python3.6
Fixes: f68248944815acbf5fb08ef66f36da6d5baef824

It seems the shaman CentOS8 builders, which serve as the base for container images, aren't current with the ":=" syntax. Amend to support RHEL8, whose default python is python 3.6.8.

Error example from https://shaman.ceph.com/builds/ceph/ceph-nvmeof-mon/27188b78bdbaa3c33de518b8514906de629e774a/default/388913/

  File "/home/jenkins-build/build/workspace/ceph-dev-new-build/ARCH/x86_64/AVAILABLE_ARCH/x86_64/AVAILABLE_DIST/centos8/DIST/centos8/MACHINE_SIZE/gigantic/release/19.0.0-2505-g27188b78/rpm/el8/BUILD/ceph-19.0.0-2505-g27188b78/src/pybind/cephfs/setup.py", line 120
    if ldflags := os.environ.get('LDFLAGS'):
                ^
SyntaxError: invalid syntax

Signed-off-by: Alexander Indenbaum <aindenba@redhat.com>

src/pybind/cephfs/setup.py
@@ -117,7 +117,8 @@ def check_sanity():
             extra_preargs=['-iquote{path}'.format(path=os.path.join(CEPH_SRC_DIR, 'include'))]
         )
 
-        if ldflags := os.environ.get('LDFLAGS'):
+        ldflags = os.environ.get('LDFLAGS')
+        if ldflags:
             extra_postargs = ldflags.split()
         else:
             extra_postargs = None

src/pybind/rados/setup.py
@@ -112,7 +112,8 @@ def check_sanity():
             sources=[tmp_file],
             output_dir=tmp_dir
         )
-        if ldflags := os.environ.get('LDFLAGS'):
+        ldflags = os.environ.get('LDFLAGS')
+        if ldflags:
             extra_postargs = ldflags.split()
         else:
             extra_postargs = None

src/pybind/rbd/setup.py
@@ -116,7 +116,8 @@ def check_sanity():
             output_dir=tmp_dir
         )
 
-        if ldflags := os.environ.get('LDFLAGS'):
+        ldflags = os.environ.get('LDFLAGS')
+        if ldflags:
             extra_postargs = ldflags.split()
         else:
             extra_postargs = None

src/pybind/rgw/setup.py
@@ -116,7 +116,8 @@ def check_sanity():
             output_dir=tmp_dir,
         )
 
-        if ldflags := os.environ.get('LDFLAGS'):
+        ldflags = os.environ.get('LDFLAGS')
+        if ldflags:
             extra_postargs = ldflags.split()
         else:
             extra_postargs = None