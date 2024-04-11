bf448e3f637f8c8e42f095b1b5ff709e3633dc11
Guillaume Abrioux <gabrioux@ibm.com>
2024-04-11 09:43:47 +0200

    ceph-volume: update functional testing
    various changes for fixing ceph-volume functional testing.
    cleaning up deploy.yml (drop py2 references)
    
    Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>
    (cherry picked from commit 90bfaaa2f0ab7e28c0f78189ccb83635e944e758)

--- src/ceph-volume/ceph_volume/tests/functional/scripts/vagrant_up.sh
+++ src/ceph-volume/ceph_volume/tests/functional/scripts/vagrant_up.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+CEPH_ANSIBLE_VAGRANT_BOX="${CEPH_ANSIBLE_VAGRANT_BOX:-centos/stream9}"
+
+if [[ "${CEPH_ANSIBLE_VAGRANT_BOX}" =~ "centos/stream" ]]; then
+  EL_VERSION="${CEPH_ANSIBLE_VAGRANT_BOX: -1}"
+  LATEST_IMAGE="$(curl -s https://cloud.centos.org/centos/${EL_VERSION}-stream/x86_64/images/CHECKSUM | sed -nE 's/^SHA256.*\((.*-([0-9]+).*vagrant-libvirt.box)\).*$/\1/p' | sort -u | tail -n1)"
+  vagrant box remove "${CEPH_ANSIBLE_VAGRANT_BOX}" --all --force || true
+  vagrant box add --force --provider libvirt --name "${CEPH_ANSIBLE_VAGRANT_BOX}" "https://cloud.centos.org/centos/${EL_VERSION}-stream/x86_64/images/${LATEST_IMAGE}" --force
+fi
+
 retries=0
 until [ $retries -ge 5 ]
 do
