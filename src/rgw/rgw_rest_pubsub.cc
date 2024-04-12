roysahar/ceph-ci

rgw/pubsub: CreateTopic consults identity policies when topic doesn't exist
Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/rgw_rest_pubsub.cc
@@ -312,12 +312,20 @@ class RGWPSCreateTopicOp : public RGWOp {
       return 0;
     }
 
-    if (!existing) {
-      return 0;
-    }
-    if (!verify_topic_permission(this, s, *existing, topic_arn,
-                                 rgw::IAM::snsCreateTopic)) {
-      return -ERR_AUTHORIZATION; // AuthorizationError is sns for AccessDenied
+    if (existing) {
+      // consult topic policy for overwrite permission
+      if (!verify_topic_permission(this, s, *existing, topic_arn,
+                                   rgw::IAM::snsCreateTopic)) {
+        return -ERR_AUTHORIZATION;
+      }
+    } else {
+      // if no topic policy exists, just check identity policies for denies
+      constexpr bool mandatory_policy = false;
+      if (!verify_user_permission(this, s, topic_arn,
+                                  rgw::IAM::snsCreateTopic,
+                                  mandatory_policy)) {
+        return -ERR_AUTHORIZATION;
+      }
     }
     return 0;
   }

ceph/ceph

(cherry picked from commit 8896b227226fad92d280e72cd08210685dd445af)

src/rgw/rgw_rest_pubsub.cc
@@ -329,9 +329,20 @@ class RGWPSCreateTopicOp : public RGWOp {
       return 0;
     }
 
-    if (topic && !verify_topic_permission(this, s, *topic, topic_arn,
-                                          rgw::IAM::snsCreateTopic)) {
-      return -ERR_AUTHORIZATION;
+    if (topic) {
+      // consult topic policy for overwrite permission
+      if (!verify_topic_permission(this, s, *topic, topic_arn,
+                                   rgw::IAM::snsCreateTopic)) {
+        return -ERR_AUTHORIZATION;
+      }
+    } else {
+      // if no topic policy exists, just check identity policies for denies
+      constexpr bool mandatory_policy = false;
+      if (!verify_user_permission(this, s, topic_arn,
+                                  rgw::IAM::snsCreateTopic,
+                                  mandatory_policy)) {
+        return -ERR_AUTHORIZATION;
+      }
     }
     return 0;
   }