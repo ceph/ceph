7ae46186760746f239d88ed804e08732f645373f
Samuel Just <sjust@redhat.com>
2024-04-18 15:19:31 -0700

    os/: modify getattrs to clear attrs out param before populating
    Passing in a non-empty map would otherwise exhibit quite unexpected
    behavior.  For the bufferptr overload, any preexisting entries would
    not be overwritten due to how std::map::emplace behaves.  For the
    bufferlist overload, it would result in appending to any pre-existing
    entries.
    
    The prior commit cleans up one such inadvertent caller which resulted
    in the below bug.
    
    Fixes: https://tracker.ceph.com/issues/65185
    Signed-off-by: Samuel Just <sjust@redhat.com>
    (cherry picked from commit 915f92ba9dc8e192bdcebcb92e7d06a93a401f5d)

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -12426,6 +12426,7 @@ int BlueStore::getattrs(
       r = -ENOENT;
       goto out;
     }
+    aset.clear();
     for (auto& i : o->onode.attrs) {
       aset.emplace(i.first.c_str(), i.second);
     }
