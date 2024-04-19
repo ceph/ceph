3ee850473c0b75bfe836cff0aae5c0827b98421a
Patrick Donnelly <pdonnell@redhat.com>
2024-04-19 14:31:09 -0400

    mgr: allow specifying module option level
    Some are for development purposes and should be filtered out by the dashboard.
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- src/pybind/mgr/mgr_module.py
+++ src/pybind/mgr/mgr_module.py
@@ -583,6 +583,11 @@ if TYPE_CHECKING:
 # common/options.h: value_t
 OptionValue = Optional[Union[bool, int, float, str]]
 
+class OptionLevel(IntEnum):
+    BASIC = 0
+    ADVANCED = 1
+    DEV = 2
+    UNKNOWN = 3
 
 class Option(Dict):
     """
@@ -593,6 +598,7 @@ class Option(Dict):
     def __init__(
             self,
             name: str,
+            level: OptionLevel = OptionLevel.ADVANCED,
             default: OptionValue = None,
             type: 'OptionTypeLabel' = 'str',
             desc: Optional[str] = None,
