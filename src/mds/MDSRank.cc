b05f9ba13e8764322b2dde860de8e34be5897ad3
Patrick Donnelly <pdonnell@redhat.com>
2024-04-24 20:44:58 -0400

    mds: drop client metrics during recovery
    Fixes: https://tracker.ceph.com/issues/65660
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>
    (cherry picked from commit 83f445c54fe2c61bb9239abdb961be001e34c750)

--- src/mds/MDSRank.cc
+++ src/mds/MDSRank.cc
@@ -2103,6 +2103,8 @@ void MDSRank::active_start()
 {
   dout(1) << "active_start" << dendl;
 
+  m_is_active = true;
+
   if (last_state == MDSMap::STATE_CREATING ||
       last_state == MDSMap::STATE_STARTING) {
     mdcache->open_root();
