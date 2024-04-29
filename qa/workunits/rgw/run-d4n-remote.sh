dfb98b63ad0646c60b20734c896b66a0a4949afa
Samarah <samarah.uriarte@ibm.com>
2024-04-29 16:29:32 +0000

    qa/d4n: Begin adding suite for D4N remote cache API
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

---
+++ qa/workunits/rgw/run-d4n-remote.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -ex
+mydir=`dirname $0`
+
+python3 -m venv $mydir
+source $mydir/bin/activate
+pip install pip --upgrade
+pip install redis
+pip install configobj
+pip install boto3
+
+#mount -t tmpfs -o size=15 tmpfs /tmp/rgw_d4n_datacache
+
+# run test
+$mydir/bin/python3 $mydir/test_rgw_d4n_remote.py
+
+deactivate
+echo OK.
---
+++ qa/workunits/rgw/test_rgw_d4n_remote.py
@@ -0,0 +1,199 @@
+#!/usr/bin/python3
+
+'''
+This workunits tests the functionality of the D4N read workflow on a small object of size 4.
+'''
+
+import logging as log
+from configobj import ConfigObj
+import boto3
+import redis
+import subprocess
+import json
+import os
+import hashlib
+import string
+import random
+
+log.basicConfig(level=log.DEBUG)
+
+""" Constants """
+ACCESS_KEY = 'test3'
+SECRET_KEY = 'test3'
+
+def exec_cmd(cmd):
+    log.debug("exec_cmd(%s)", cmd)
+    try:
+        proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+        out, err = proc.communicate()
+        if proc.returncode == 0:
+            log.info('command succeeded')
+            if out is not None: log.info(out)
+            return out
+        else:
+            raise Exception("error: %s \nreturncode: %s" % (err, proc.returncode))
+    except Exception as e:
+        log.error('command failed')
+        log.error(e)
+        return False
+
+def get_radosgw_endpoint():
+    out = exec_cmd('sudo netstat -nltp | egrep "rados|valgr"')  # short for radosgw/valgrind
+    x = out.decode('utf8').split(" ")
+    port = [i for i in x if ':' in i][0].split(':')[1]
+    log.info('radosgw port: %s' % port)
+    proto = "http"
+    hostname = '127.0.0.1'
+
+    if port == '443':
+        proto = "https"
+
+    endpoint = "%s://%s:%s" % (proto, hostname, port)
+
+    log.info("radosgw endpoint is: %s", endpoint)
+    return endpoint, proto
+
+def create_s3cmd_config(path, proto):
+    """
+    Creates a minimal config file for s3cmd
+    """
+    log.info("Creating s3cmd config...")
+
+    use_https_config = "False"
+    log.info("proto for s3cmd config is %s", proto)
+    if proto == "https":
+        use_https_config = "True"
+
+    s3cmd_config = ConfigObj(
+        indent_type='',
+        infile={
+            'default':
+                {
+                'host_bucket': 'no.way.in.hell',
+                'use_https': use_https_config,
+                },
+            }
+    )
+
+    f = open(path, 'wb')
+    s3cmd_config.write(f)
+    f.close()
+    log.info("s3cmd config written")
+
+def get_cmd_output(cmd_out):
+    out = cmd_out.decode('utf8')
+    out = out.strip('\n')
+    return out
+
+def get_body(response):
+    body = response['Body']
+    got = body.read()
+    if type(got) is bytes:
+        got = got.decode()
+    return got
+
+def test_remote_cache_api(r, client, s3):
+    # cause eviction
+    test_txt = 'hello world'
+
+    response_put = obj.put(Body=test_txt)
+    assert(response_put.get('ResponseMetadata').get('HTTPStatusCode') == 200)
+
+    assert(os.path.exists('/tmp/rgw_d4n_datacache/D_bkt_test.txt_0_11') == 'true')
+
+    data = r.hgetall('bkt_test.txt_0_11')
+
+    # directory entry comparisons
+    assert(data.get('blockID') == '0')
+    assert(data.get('version') == '')
+    assert(data.get('size') == '11')
+    assert(data.get('globalWeight') == '0')
+    assert(data.get('blockHosts') == '127.0.0.1:8001') 
+    assert(data.get('objName') == 'test.txt')
+    assert(data.get('bucketName') == 'bkt')
+    assert(data.get('creationTime') == '')
+    assert(data.get('dirty') == '1')
+    assert(data.get('objHosts') == '')
+
+    # Allow cleaning cycle to pass
+    time.sleep(6)
+
+    os.path.exists('/tmp/rgw_d4n_datacache/bkt_test.txt_0_11')
+    os.path.exists('/tmp/rgw_d4n_datacache/RD_bkt_test.txt_0_11')
+
+    # Check contents of both files
+    out = subprocess.check_output(['cat', '/tmp/rgw_d4n_datacache/bkt_test.txt_0_11']).decode('latin-1')
+    assert(out == "test")
+
+    out = subprocess.check_output(['cat', '/tmp/rgw_d4n_datacache/RD_bkt_test.txt_0_11']).decode('latin-1')
+    assert(out == "test")
+
+    data = r.hgetall('bkt_test.txt_0_11')
+
+    # directory entry comparisons
+    assert(data.get('blockID') == '0')
+    assert(data.get('version') == '')
+    assert(data.get('size') == '11')
+    assert(data.get('globalWeight') == '0')
+    assert(data.get('blockHosts') == '127.0.0.1:8001_127.0.0.1:8002') 
+    assert(data.get('objName') == 'test.txt')
+    assert(data.get('bucketName') == 'bkt')
+    assert(data.get('creationTime') == '')
+    assert(data.get('dirty') == 'false')
+    assert(data.get('objHosts') == '')
+
+    r.flushall()
+     
+def main():
+    """
+    execute the d4n test
+    """
+
+    # Setup for test
+    log.info("D4NFilterTest setup.")
+
+    out = exec_cmd('pwd')
+    pwd = get_cmd_output(out)
+    log.debug("pwd is: %s", pwd)
+
+    endpoint, proto = get_radosgw_endpoint()
+
+    client = boto3.client(service_name='s3',
+                aws_access_key_id=ACCESS_KEY,
+                aws_secret_access_key=SECRET_KEY,
+                endpoint_url=endpoint,
+                use_ssl=False,
+                verify=False)
+
+    s3 = boto3.resource('s3', 
+                aws_access_key_id=ACCESS_KEY,
+                aws_secret_access_key=SECRET_KEY,
+                endpoint_url=endpoint, 
+                use_ssl=False,
+                verify=False)
+
+    bucket = s3.Bucket('bkt')
+    bucket.create()
+    obj = s3.Object(bucket_name='bkt', key='test.txt')
+
+    # Check for Redis instance
+    try:
+        connection = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+        connection.ping() 
+    except:
+        log.debug("ERROR: Redis instance not running.")
+        raise
+
+    # Create s3cmd config
+    s3cmd_config_path = pwd + '/s3cfg'
+    create_s3cmd_config(s3cmd_config_path, proto)
+
+    r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
+    # Run remote cache API test 
+    test_remote_cache_api(r, client, s3)
+
+    log.info("D4NFilterTest completed.")
+
+main()
+log.info("Completed D4N tests")
