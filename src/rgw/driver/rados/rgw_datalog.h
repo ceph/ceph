e67f51fb3b3765918152b383df701c4557a8dccb
Adam Emerson <aemerson@redhat.com>
2024-04-29 19:49:16 -0400

    rgw/multisite/datalog: Parallelize recovery
    Signed-off-by: Adam Emerson <aemerson@redhat.com>

--- src/rgw/driver/rados/rgw_datalog.h
+++ src/rgw/driver/rados/rgw_datalog.h
@@ -502,6 +502,7 @@ public:
   asio::awaitable<void>
   decrement_sems(int index,
 		 bc::flat_map<std::string, uint64_t>&& semcount);
+  asio::awaitable<void> recover_shard(const DoutPrefixProvider* dpp, int index);
   asio::awaitable<void> recover(const DoutPrefixProvider* dpp,
 				decltype(recovery_signal));
 };
