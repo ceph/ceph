541cd7a563da1f27e464591cad41dd717c7315c1
Adam King <adking@redhat.com>
2024-04-30 13:49:46 -0400

    mgr/cephadm: clean up nvmeof keyring when removing daemon
    The parent class' (CephService) post_remove handles
    keyring cleanup
    
    Fixes: https://tracker.ceph.com/issues/65717
    
    Signed-off-by: Adam King <adking@redhat.com>
    (cherry picked from commit 04065e34d74421fd1f85b52b87efaf357fe88505)
    
    Conflicts:
    	src/pybind/mgr/cephadm/services/nvmeof.py

--- src/pybind/mgr/cephadm/services/nvmeof.py
+++ src/pybind/mgr/cephadm/services/nvmeof.py
@@ -84,6 +84,8 @@ class NvmeofService(CephService):
         Called after the daemon is removed.
         """
         logger.debug(f'Post remove daemon {self.TYPE}.{daemon.daemon_id}')
+        # to clean the keyring up
+        super().post_remove(daemon, is_failed_deploy=is_failed_deploy)
         # TODO: remove config for dashboard nvmeof gateways if any
         # and any certificates being used for mTLS
 
