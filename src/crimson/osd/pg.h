811ace178114247ac05274feec20905b9c5e2ba4
junxiang Mu <1948535941@qq.com>
2024-04-30 02:44:58 -0400

    crimson/osd.cc: crimson support SnapMapper::Scrubber
    fixes: https://tracker.ceph.com/issues/65478
    Signed-off-by: junxiang Mu <1948535941@qq.com>

--- src/crimson/osd/pg.h
+++ src/crimson/osd/pg.h
@@ -425,6 +425,10 @@ public:
   void set_ready_to_merge_target(eversion_t lu, epoch_t les, epoch_t lec) final {}
   void set_ready_to_merge_source(eversion_t lu) final {}
 
+  // seastar::future<> scrub_purged_snaps();
+  void queue_snap_retrim(snapid_t snap);
+  void kick_snap_trim();
+
   void on_active_actmap() final;
   void on_active_advmap(const OSDMapRef &osdmap) final;
 
