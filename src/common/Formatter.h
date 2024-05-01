b550bf60adf14b38db2b29adb565e1469afdc2cb
Patrick Donnelly <pdonnell@redhat.com>
2024-05-01 15:09:33 -0400

    common,ceph: add output file switch to dump json to
    The `ceph tell mds.X cache dump` and `ceph tell mds.X ops` commands have a
    useful `--path` argument that directs the daemon to save the dump of the output
    to a file local the daemon. This has several advantages:
    
    * We don't need to construct the JSON output in memory, which may be many gigabytes.
    * Streaming writes to a local file is significantly faster than sending the data over the ceph messenger.
    * The command spends as little time as possible holding relevant locks.
    
    However, only some commands support this and we could make it generic to the
    admin socket interface. So, add a generic --daemon-output-file argument to
    achieve this.
    
    The main concern with this is security (telling the daemon to write to
    arbitrary files) but this is gated by the session caps which require
    "allow_all" and that's typically only valid for the client.admin.
    
    Fixes: https://tracker.ceph.com/issues/65747
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>
    (cherry picked from commit a3414684974380c1d335f1fcafc5245dbde1c0d6)

--- src/common/Formatter.h
+++ src/common/Formatter.h
@@ -272,6 +272,9 @@ public:
     int get_len() const override {
       return file.tellp();
     }
+    std::ofstream const& get_ofstream() const {
+      return file;
+    }
 
 protected:
     std::ostream& get_ss() override {
