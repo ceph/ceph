3631692b835c644818f2aec428d24274653e992f
John Mulligan <jmulligan@redhat.com>
2024-05-08 10:49:20 -0400

    mgr/smb: add test coverage for all utils.py functions
    Signed-off-by: John Mulligan <jmulligan@redhat.com>

--- src/pybind/mgr/smb/tests/test_utils.py
+++ src/pybind/mgr/smb/tests/test_utils.py
@@ -26,3 +26,18 @@ def test_rand_name():
     assert len(name) == 18
     name = smb.utils.rand_name('')
     assert len(name) == 8
+
+
+def test_checked():
+    assert smb.utils.checked('foo') == 'foo'
+    assert smb.utils.checked(77) == 77
+    assert smb.utils.checked(0) == 0
+    with pytest.raises(smb.utils.IsNoneError):
+        smb.utils.checked(None)
+
+
+def test_ynbool():
+    assert smb.utils.ynbool(True) == 'Yes'
+    assert smb.utils.ynbool(False) == 'No'
+    # for giggles
+    assert smb.utils.ynbool(0) == 'No'
