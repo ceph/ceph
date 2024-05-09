53901f412d47e3ef59ab0a5b9eff7507e38b81bf
Rishabh Dave <ridave@redhat.com>
2024-05-09 11:48:25 +0530

    mon/MDSMonitor: rectify errmsg for "mds fail" cmd
    Rectify the error message printed when a non-existent MDS's name is
    passed to "ceph mds fail" command.
    
    Fixes: https://tracker.ceph.com/issues/65875
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- src/mon/MDSMonitor.cc
+++ src/mon/MDSMonitor.cc
@@ -1409,8 +1409,6 @@ mds_gid_t MDSMonitor::gid_from_arg(const FSMap &fsmap, const string &arg, ostrea
     // Not a role or a GID, try as a daemon name
     const MDSMap::mds_info_t *mds_info = fsmap.find_by_name(arg);
     if (!mds_info) {
-      ss << "MDS named '" << arg
-	 << "' does not exist, or is not up";
       return MDS_GID_NONE;
     }
     dout(10) << __func__ << ": resolved MDS name '" << arg
