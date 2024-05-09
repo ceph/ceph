9420d0038bce145c8aba7628270f69eb668e30f0
Radoslaw Zarzynski <rzarzyns@redhat.com>
2024-05-09 17:37:51 +0000

    crimson/osd: settle ECRecoveryBackend -- local MOSDPGPush
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/crimson/osd/pg.h
+++ src/crimson/osd/pg.h
@@ -157,6 +157,8 @@ public:
       });
     });
   }
+  void send_message_osd_cluster(
+    int osd, MOSDPGPush* msg, epoch_t from_epoch) override;
   std::ostream& gen_dbg_prefix(std::ostream& out) const override final {
     return gen_prefix(out);
   }
