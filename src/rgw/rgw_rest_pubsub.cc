bef65f456a2593d7fe0731df20d147f966e8c41f
kchheda3 <kchheda3@bloomberg.net>
2024-05-17 11:04:13 -0400

    rgw/notification: Store the value of `persistent_queue` for existing persistent topic.
    Fixes https://tracker.ceph.com/issues/66097
    
    Signed-off-by: kchheda3 <kchheda3@bloomberg.net>
    (cherry picked from commit c676c31042e4edbcf7acd7a58fc804cf205038d7)

--- src/rgw/rgw_rest_pubsub.cc
+++ src/rgw/rgw_rest_pubsub.cc
@@ -402,6 +402,8 @@ void RGWPSCreateTopicOp::execute(optional_yield y) {
                          << op_ret << dendl;
       return;
     }
+  } else if (already_persistent) {  // redundant call to CreateTopic
+    dest.persistent_queue = topic->dest.persistent_queue;
   }
   const RGWPubSub ps(driver, get_account_or_tenant(s->owner.id), *s->penv.site);
   op_ret = ps.create_topic(this, topic_name, dest, topic_arn.to_string(),
@@ -861,7 +863,7 @@ void RGWPSSetTopicAttributesOp::execute(optional_yield y) {
           << op_ret << dendl;
       return;
     }
-  } else if (already_persistent) {
+  } else if (already_persistent && !topic_needs_queue(dest)) {
     // changing the persistent topic to non-persistent.
     op_ret = rgw::notify::remove_persistent_topic(result.dest.persistent_queue, s->yield);
     if (op_ret != -ENOENT && op_ret < 0) {
