1440904ddb9300e749f1a74186f47d32fc0ec111
Ivo Almeida <ialmeida@redhat.com>
2024-05-20 13:36:30 +0100

    implemented telemetry-notification notification component

--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/components.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/components.module.ts
@@ -15,7 +15,7 @@ import {
 import { ClickOutsideModule } from 'ng-click-outside';
 import { NgChartsModule } from 'ng2-charts';
 import { SimplebarAngularModule } from 'simplebar-angular';
-import { UIShellModule, ButtonModule } from 'carbon-components-angular';
+import { UIShellModule, ButtonModule, NotificationModule } from 'carbon-components-angular';
 
 import { MotdComponent } from '~/app/shared/components/motd/motd.component';
 import { DirectivesModule } from '../directives/directives.module';
@@ -79,7 +79,8 @@ import { UpgradableComponent } from './upgradable/upgradable.component';
     NgbDatepickerModule,
     NgbTimepickerModule,
     UIShellModule,
-    ButtonModule
+    ButtonModule,
+    NotificationModule,
   ],
   declarations: [
     SparklineComponent,
