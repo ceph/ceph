ea30bd53a3cb9c5b96f7bd6b61da3edd252edbba
Soumya Koduri <skoduri@redhat.com>
2024-05-20 13:25:19 -0400

    rgwlc: non-current expiration sends LifecycleExpirationDelete
    Missed when the new event types were added.
    
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

--- src/rgw/rgw_lc.cc
+++ src/rgw/rgw_lc.cc
@@ -1249,7 +1249,8 @@ public:
   int process(lc_op_ctx& oc) override {
     auto& o = oc.o;
     int r = remove_expired_obj(oc.dpp, oc, true,
-                               {rgw::notify::ObjectExpirationNonCurrent});
+                               {rgw::notify::LifecycleExpirationDelete,
+				rgw::notify::ObjectExpirationNoncurrent});
     if (r < 0) {
       ldpp_dout(oc.dpp, 0) << "ERROR: remove_expired_obj (non-current expiration) " 
 			   << oc.bucket << ":" << o.key
