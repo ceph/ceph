28fb158a6294c183c3e04cb5f2ddf10f36fc40c6
Samuel Just <sjust@redhat.com>
2024-05-31 11:42:58 -0700

    test/librados/testcase_cxx: set_allow_ec_overwrites wait for overwrite to work
    The mon command to enable ec overwrites succeeds once the mon commits
    the update.  However, actual IOs won't suceed until the map is actually
    propagated to OSDs.  Update set_allow_ec_overwrites to try performing
    overwrites until it actually succeeds (with a 120s timelimit) before
    returning.
    
    Fixes: https://tracker.ceph.com/issues/66226
    Signed-off-by: Samuel Just <sjust@redhat.com>
    (cherry picked from commit 426f225d4e6ec7e037260a234896ef9c8d6aec82)

--- src/test/librados/testcase_cxx.cc
+++ src/test/librados/testcase_cxx.cc
@@ -3,6 +3,9 @@
 
 #include "testcase_cxx.h"
 
+#include <chrono>
+#include <thread>
+
 #include <errno.h>
 #include <fmt/format.h>
 #include "test_cxx.h"
@@ -406,13 +409,26 @@ void RadosTestECPP::TearDown()
 
 void RadosTestECPP::recreate_pool()
 {
-  SKIP_IF_CRIMSON();
-  ASSERT_EQ(0, destroy_one_ec_pool_pp(pool_name, s_cluster));
-  ASSERT_EQ("", create_one_ec_pool_pp(pool_name, s_cluster));
-  SetUp();
+  ec_overwrites_set = true;
+  ASSERT_EQ("", set_allow_ec_overwrites_pp(pool_name, cluster, true));
+
+  char buf[128];
+  memset(buf, 0xcc, sizeof(buf));
+  bufferlist bl;
+  bl.append(buf, sizeof(buf));
+
+  const std::string objname = "RadosTestECPP::set_allow_ec_overwrites:test_obj";
+  ASSERT_EQ(0, ioctx.write(objname, bl, sizeof(buf), 0));
+  const auto end = std::chrono::steady_clock::now() + std::chrono::seconds(120);
+  while (true) {
+    if (0 == ioctx.write(objname, bl, sizeof(buf), 0)) {
+      break;
+    }
+    ASSERT_LT(std::chrono::steady_clock::now(), end);
+    std::this_thread::sleep_for(std::chrono::seconds(2));
+  }
 }
 
 void RadosTestECPP::set_allow_ec_overwrites(std::string pool, bool allow)
 {
   ASSERT_EQ("", set_allow_ec_overwrites_pp(pool, cluster, allow));
-}
