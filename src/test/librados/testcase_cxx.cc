ce2378108f8630ec6ea195d66ff89cb71497b767
Samuel Just <sjust@redhat.com>
2024-05-31 17:19:51 +0000

    test/librados/io_cxx: fix ec overwrites cleanup in CrcZeroWrite
    We can't rely on recreating the pool at the end of the test case because
    failing an assert in CrcZeroWrite would skip it.  Instead, recreate the
    pool in TearDown with other cleanup.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/test/librados/testcase_cxx.cc
+++ src/test/librados/testcase_cxx.cc
@@ -401,18 +401,16 @@ void RadosTestECPP::TearDown()
     cleanup_default_namespace(ioctx);
     cleanup_namespace(ioctx, nspace);
   }
+  if (ec_overwrites_set) {
+    ASSERT_EQ(0, destroy_one_ec_pool_pp(pool_name, s_cluster));
+    ASSERT_EQ("", create_one_ec_pool_pp(pool_name, s_cluster));
+    ec_overwrites_set = false;
+  }
   ioctx.close();
 }
 
-void RadosTestECPP::recreate_pool()
+void RadosTestECPP::set_allow_ec_overwrites()
 {
-  SKIP_IF_CRIMSON();
-  ASSERT_EQ(0, destroy_one_ec_pool_pp(pool_name, s_cluster));
-  ASSERT_EQ("", create_one_ec_pool_pp(pool_name, s_cluster));
-  SetUp();
-}
-
-void RadosTestECPP::set_allow_ec_overwrites(std::string pool, bool allow)
-{
-  ASSERT_EQ("", set_allow_ec_overwrites_pp(pool, cluster, allow));
+  ASSERT_EQ("", set_allow_ec_overwrites_pp(pool_name, cluster, true));
+  ec_overwrites_set = true;
 }
