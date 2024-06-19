95054a4e286c2bd66893eb7afa3de8c475e4902b
Garry Drankovich <garry.drankovich@clyso.com>
2024-06-19 20:17:52 +0300

    os/bluestore: measure op latency for data reformatting.
    Signed-off-by: Garry Drankovich <garry.drankovich@clyso.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -6711,6 +6711,9 @@ void BlueStore::_init_logger()
 
   // reformatting counters
   //****************************************
+  b.add_time_avg(l_bluestore_reformat_lat, "reformat_lat",
+    "Average reformatting latency",
+    "rf_l", PerfCountersBuilder::PRIO_CRITICAL);
   b.add_u64_counter(l_bluestore_reformat_compress_attempted,
     "reformat_compress_attempted",
     "Recompression attempts done",
@@ -12866,8 +12869,24 @@ int BlueStore::read(
     if (r == -EIO) {
       logger->inc(l_bluestore_read_eio);
     }
+    if (op_flags & CEPH_OSD_OP_FLAG_SCRUB) {
+      log_latency_scrub(__func__,
+        l_bluestore_read_lat,
+        mono_clock::now() - start,
+        cct->_conf->bluestore_log_scrub_op_age);
+    } else {
+      log_latency(__func__,
+        l_bluestore_read_lat,
+        mono_clock::now() - start,
+        cct->_conf->bluestore_log_op_age);
+    }
     if (might_need_reformatting) {
+      auto start2 = mono_clock::now();
       _maybe_reformat_object(c, o, offset, length, bl, op_flags, span_stat);
+      log_latency(__func__,
+        l_bluestore_reformat_lat,
+        mono_clock::now() - start2,
+        cct->_conf->bluestore_log_op_age);
     }
   }
 
@@ -12885,18 +12904,6 @@ int BlueStore::read(
   dout(10) << __func__ << " " << cid << " " << oid
 	   << " 0x" << std::hex << offset << "~" << length << std::dec
 	   << " = " << r << dendl;
-
-  if (op_flags & CEPH_OSD_OP_FLAG_SCRUB) {
-    log_latency_scrub(__func__,
-      l_bluestore_read_lat,
-      mono_clock::now() - start,
-      cct->_conf->bluestore_log_scrub_op_age);
-  } else {
-    log_latency(__func__,
-      l_bluestore_read_lat,
-      mono_clock::now() - start,
-      cct->_conf->bluestore_log_op_age);
-  }
   return r;
 }
 
