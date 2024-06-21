ceph/ceph-ci

rgw/archive: Correct the bucket sync status cmd output
Signed-off-by: Soumya Koduri <skoduri@redhat.com>

src/rgw/rgw_admin.cc
@@ -2509,7 +2509,7 @@ static int bucket_source_sync_status(const DoutPrefixProvider *dpp, rgw::sal::Ra
   out << indented{width, "source zone"} << source.id << " (" << source.name << ")" << std::endl;
 
   // syncing from this zone?
-  if (!zone.syncs_from(source.name)) {
+  if (!driver->svc()->zone->zone_syncs_from(zone, source)) {
     out << indented{width} << "does not sync from zone\n";
     return 0;
   }

ceph/ceph

(cherry picked from commit d7af6c228253d05a16dd3be993f150518f1b6f66)
(cherry picked from commit ede6e61a65e2618477315a7fc37a50ccfda65391)

src/rgw/rgw_admin.cc
@@ -2491,7 +2491,7 @@ static int bucket_source_sync_status(const DoutPrefixProvider *dpp, rgw::sal::Ra
   out << indented{width, "source zone"} << source.id << " (" << source.name << ")" << std::endl;
 
   // syncing from this zone?
-  if (!zone.syncs_from(source.name)) {
+  if (!store->svc()->zone->zone_syncs_from(zone, source)) {
     out << indented{width} << "does not sync from zone\n";
     return 0;
   }