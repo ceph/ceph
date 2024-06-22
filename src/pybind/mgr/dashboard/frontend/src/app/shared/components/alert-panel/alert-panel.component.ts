7d55db51b44accc46fe9a716c7af2406b31d4281
Nizamudeen A <nia@redhat.com>
2024-06-22 20:59:23 +0530

    mgr/dashboard: carbonize general components and styles
    alert-panel and some other general components
    
    Fixes: https://tracker.ceph.com/issues/67231
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/alert-panel/alert-panel.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/alert-panel/alert-panel.component.ts
@@ -19,27 +19,25 @@ import { Icons } from '~/app/shared/enum/icons.enum';
 export class AlertPanelComponent implements OnInit {
   @ViewChild('content', { static: true })
   alertContent: TemplateRef<any>;
-  @ViewChild('closeTpl', { static: true })
-  closeTpl: TemplateRef<any>;
+  @ViewChild('actionTpl', { static: true })
+  actionTpl: TemplateRef<any>;
 
   @Input()
   title = '';
   @Input()
-  bootstrapClass = '';
-  @Input()
   type: 'warning' | 'error' | 'info' | 'success' | 'danger';
   @Input()
-  typeIcon: Icons | string;
+  showTitle = true;
   @Input()
   size: 'slim' | 'normal' = 'normal';
-  @Input()
-  showIcon = true;
-  @Input()
-  showTitle = true;
   @Input()
   dismissible = false;
   @Input()
   spacingClass = '';
+  @Input()
+  actionName = '';
+  @Input()
+  lowContrast = true;
 
   /**
    * The event that is triggered when the close button (x) has been
@@ -48,50 +46,52 @@ export class AlertPanelComponent implements OnInit {
   @Output()
   dismissed = new EventEmitter();
 
+  /**
+   * The event that is triggered when the action button has been
+   * pressed.
+   */
+  @Output()
+  action = new EventEmitter();
+
   icons = Icons;
 
   notificationContent: NotificationContent;
 
   ngOnInit() {
     const type: NotificationType = this.type === 'danger' ? 'error' : this.type;
-    this.notificationContent = {
-      type: type,
-      template: this.alertContent,
-      actionsTemplate: this.closeTpl,
-      showClose: false,
-      title: this.title
-    };
-
     switch (this.type) {
       case 'warning':
         this.title = this.title || $localize`Warning`;
-        this.typeIcon = this.typeIcon || Icons.warning;
-        this.bootstrapClass = this.bootstrapClass || 'warning';
         break;
       case 'error':
         this.title = this.title || $localize`Error`;
-        this.typeIcon = this.typeIcon || Icons.destroyCircle;
-        this.bootstrapClass = this.bootstrapClass || 'danger';
         break;
       case 'info':
         this.title = this.title || $localize`Information`;
-        this.typeIcon = this.typeIcon || Icons.infoCircle;
-        this.bootstrapClass = this.bootstrapClass || 'info';
         break;
       case 'success':
         this.title = this.title || $localize`Success`;
-        this.typeIcon = this.typeIcon || Icons.check;
-        this.bootstrapClass = this.bootstrapClass || 'success';
         break;
       case 'danger':
         this.title = this.title || $localize`Danger`;
-        this.typeIcon = this.typeIcon || Icons.warning;
-        this.bootstrapClass = this.bootstrapClass || 'danger';
         break;
     }
+
+    this.notificationContent = {
+      type: type,
+      template: this.alertContent,
+      actionsTemplate: this.actionTpl,
+      showClose: this.dismissible,
+      title: this.showTitle ? this.title : '',
+      lowContrast: this.lowContrast
+    };
   }
 
   onClose(): void {
     this.dismissed.emit();
+  }
+
+  onAction(): void {
+    this.action.emit();
   }
 }
--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/components.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/components.module.ts
@@ -19,7 +19,12 @@ import {
   UIShellModule,
   ButtonModule,
   NotificationModule,
-  IconModule
+  IconModule,
+  IconService,
+  TooltipModule,
+  GridModule,
+  AccordionModule,
+  LoadingModule
 } from 'carbon-components-angular';
 
 import { MotdComponent } from '~/app/shared/components/motd/motd.component';
@@ -64,6 +69,9 @@ import { HelpTextComponent } from './help-text/help-text.component';
 import { FormAdvancedFieldsetComponent } from './form-advanced-fieldset/form-advanced-fieldset.component';
 import { UpgradableComponent } from './upgradable/upgradable.component';
 
+// Icons
+import InfoIcon from '@carbon/icons/es/information/16';
+
 @NgModule({
   imports: [
     CommonModule,
@@ -86,7 +94,12 @@ import { UpgradableComponent } from './upgradable/upgradable.component';
     UIShellModule,
     ButtonModule,
     NotificationModule,
-    IconModule
+    IconModule,
+    TooltipModule,
+    IconModule,
+    GridModule,
+    AccordionModule,
+    LoadingModule
   ],
   declarations: [
     SparklineComponent,
@@ -168,4 +181,8 @@ import { UpgradableComponent } from './upgradable/upgradable.component';
     UpgradableComponent
   ]
 })
-export class ComponentsModule {}
+export class ComponentsModule {
+  constructor(private iconService: IconService) {
+    this.iconService.registerAll([InfoIcon]);
+  }
+}
