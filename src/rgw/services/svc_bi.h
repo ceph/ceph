58b282309edbd8e6293251ca44cad90e53d008fc
Dan Mick <dmick@redhat.com>
2024-06-25 19:07:41 -0700

    Add Containerfile and build.sh to build it.
    The intent is to replace ceph-container.git, at first for ci containers
    only, and eventually production containers as well.
    
    There is code present for production containers, including
    a separate "make-manifest-list.py" to scan for and glue the two
    arch-specific containers into a 'manifest-list' 'fat' container,
    but that code is not yet fully tested.
    
    This code will not be used until a corresponding change to the
    Jenkins jobs in ceph-build.git is pushed.
    
    Note that this tooling does not authenticate to the container repo;
    it is assumed that will be done elsewhere.  Authentication is
    verified by pushing a minimal image to the requested repo.
    
    Signed-off-by: Dan Mick <dmick@redhat.com>

---
+++ src/rgw/services/svc_bi.h
@@ -0,0 +1,49 @@
+
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab ft=cpp
+
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2019 Red Hat, Inc.
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation. See file COPYING.
+ *
+ */
+
+
+#pragma once
+
+#include "rgw_service.h"
+
+class RGWBucketInfo;
+struct RGWBucketEnt;
+
+
+class RGWSI_BucketIndex : public RGWServiceInstance
+{
+public:
+  RGWSI_BucketIndex(CephContext *cct) : RGWServiceInstance(cct) {}
+  virtual ~RGWSI_BucketIndex() {}
+
+  virtual int init_index(const DoutPrefixProvider *dpp,
+                         const RGWBucketInfo& bucket_info,
+                         const rgw::bucket_index_layout_generation& idx_layout,
+                         bool judge_support_logrecord = false) = 0;
+  virtual int clean_index(const DoutPrefixProvider *dpp,
+                          const RGWBucketInfo& bucket_info,
+                          const rgw::bucket_index_layout_generation& idx_layout) = 0;
+
+  virtual int read_stats(const DoutPrefixProvider *dpp,
+                         const RGWBucketInfo& bucket_info,
+                         RGWBucketEnt *stats,
+                         optional_yield y) = 0;
+
+  virtual int handle_overwrite(const DoutPrefixProvider *dpp,
+                               const RGWBucketInfo& info,
+                               const RGWBucketInfo& orig_info,
+                               optional_yield y) = 0;
+};
