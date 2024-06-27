ceph/ceph

commit 728cdc349d0442926ab4f4401bd457d6ae75ca07

mds: rework FSMap to avoid shared_ptrs
There's zero reason to complicate the interface with
shared_ptr<Filesystem> when a Filesystem's lifetime is tied to the
FSMap.

Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

src/tools/cephfs/JournalTool.cc
@@ -138,9 +138,8 @@ int JournalTool::main(std::vector<const char*> &argv)
     return r;
   }
  
-  auto fs = fsmap->get_filesystem(role_selector.get_ns());
-  ceph_assert(fs != nullptr);
-  int64_t const pool_id = fs->mds_map.get_metadata_pool();
+  auto& fs = fsmap->get_filesystem(role_selector.get_ns());
+  int64_t const pool_id = fs.get_mds_map().get_metadata_pool();
   dout(4) << "JournalTool: resolving pool " << pool_id << dendl;
   std::string pool_name;
   r = rados.pool_reverse_lookup(pool_id, &pool_name);
@@ -1201,9 +1200,9 @@ int JournalTool::consume_inos(const std::set<inodeno_t> &inos)
   int r = 0;
 
   // InoTable is a per-MDS structure, so iterate over assigned ranks
-  auto fs = fsmap->get_filesystem(role_selector.get_ns());
+  auto& fs = fsmap->get_filesystem(role_selector.get_ns());
   std::set<mds_rank_t> in_ranks;
-  fs->mds_map.get_mds_set(in_ranks);
+  fs.get_mds_map().get_mds_set(in_ranks);
 
   for (std::set<mds_rank_t>::iterator rank_i = in_ranks.begin();
       rank_i != in_ranks.end(); ++rank_i)

src/client/Client.cc
@@ -6134,7 +6134,8 @@ int Client::resolve_mds(
   int role_r = fsmap->parse_role(mds_spec, &role, *css);
   if (role_r == 0) {
     // We got a role, resolve it to a GID
-    auto& info = fsmap->get_filesystem(role.fscid)->mds_map.get_info(role.rank);
+    const auto& mdsmap = fsmap->get_filesystem(role.fscid).get_mds_map();
+    auto& info = mdsmap.get_info(role.rank);
     ldout(cct, 10) << __func__ << ": resolved " << mds_spec << " to role '"
       << role << "' aka " << info.human_name() << dendl;
     targets->push_back(info.global_id);

src/mds/FSMap.h
@@ -189,14 +189,7 @@ WRITE_CLASS_ENCODER(MirrorInfo)
 class Filesystem
 {
 public:
-  using ref = std::shared_ptr<Filesystem>;
-  using const_ref = std::shared_ptr<Filesystem const>;
-
-  template<typename... Args>
-  static ref create(Args&&... args)
-  {
-    return std::make_shared<Filesystem>(std::forward<Args>(args)...);
-  }
+  Filesystem() = default;
 
   void encode(ceph::buffer::list& bl, uint64_t features) const;
   void decode(ceph::buffer::list::const_iterator& p);
@@ -228,6 +221,32 @@ public:
     return false;
   }
 
+  const auto& get_mds_map() const
+  {
+    return mds_map;
+  }
+  auto& get_mds_map()
+  {
+    return mds_map;
+  }
+
+  const auto& get_mirror_info() const
+  {
+    return mirror_info;
+  }
+  auto& get_mirror_info()
+  {
+    return mirror_info;
+  }
+
+  auto get_fscid() const
+  {
+    return fscid;
+  }
+
+private:
+  friend class FSMap;
+
   fs_cluster_id_t fscid = FS_CLUSTER_ID_NONE;
   MDSMap mds_map;
   MirrorInfo mirror_info;
@@ -236,9 +255,10 @@ WRITE_CLASS_ENCODER_FEATURES(Filesystem)
 
 class FSMap {
 public:
-  friend class MDSMonitor;
-  friend class PaxosFSMap;
   using mds_info_t = MDSMap::mds_info_t;
+  using fsmap = typename std::map<fs_cluster_id_t, Filesystem>;
+  using const_iterator = typename fsmap::const_iterator;
+  using iterator = typename fsmap::iterator;
 
   static const version_t STRUCT_VERSION = 7;
   static const version_t STRUCT_VERSION_TRIM_TO = 7;
@@ -259,15 +279,20 @@ public:
       struct_version(rhs.struct_version)
   {
     filesystems.clear();
-    for (const auto &i : rhs.filesystems) {
-      const auto &fs = i.second;
-      filesystems[fs->fscid] = std::make_shared<Filesystem>(*fs);
-    }
+    filesystems = rhs.filesystems;
   }
 
   FSMap &operator=(const FSMap &rhs);
 
-  const CompatSet &get_default_compat() const {return default_compat;}
+  const_iterator begin() const {
+    return filesystems.begin();
+  }
+  const_iterator end() const {
+    return filesystems.end();
+  }
+
+  const CompatSet& get_default_compat() const {return default_compat;}
+  CompatSet& get_default_compat() {return default_compat;}
 
   void filter(const std::vector<std::string>& allowed)
   {
@@ -276,7 +301,7 @@ public:
     }
 
     erase_if(filesystems, [&](const auto& f) {
-      return std::find(allowed.begin(), allowed.end(), f.second->mds_map.get_fs_name()) == allowed.end();
+      return std::find(allowed.begin(), allowed.end(), f.second.mds_map.get_fs_name()) == allowed.end();
     });
 
     erase_if(mds_roles, [&](const auto& r) {
@@ -381,7 +406,7 @@ public:
    */
   void promote(
       mds_gid_t standby_gid,
-      Filesystem& filesystem,
+      fs_cluster_id_t fscid,
       mds_rank_t assigned_rank);
 
   /**
@@ -417,10 +442,9 @@ public:
    * Caller must already have validated all arguments vs. the existing
    * FSMap and OSDMap contents.
    */
-  Filesystem::ref create_filesystem(
-      std::string_view name, int64_t metadata_pool,
-      int64_t data_pool, uint64_t features,
-      fs_cluster_id_t fscid, bool recover);
+  const Filesystem& create_filesystem(
+      std::string_view name, int64_t metadata_pool, int64_t data_pool,
+      uint64_t features, fs_cluster_id_t fscid, bool recover);
 
   /**
    * Remove the filesystem (it must exist).  Caller should already
@@ -444,7 +468,8 @@ public:
   {
     auto& fs = filesystems.at(fscid);
     fn(fs);
-    fs->mds_map.epoch = epoch;
+    fs.mds_map.epoch = epoch;
+    fs.mds_map.modified = ceph_clock_now();
   }
 
   /**
@@ -462,9 +487,10 @@ public:
       standby_epochs[who] = epoch;
     } else {
       auto& fs = filesystems.at(fscid);
-      auto& info = fs->mds_map.mds_info.at(who);
+      auto& info = fs.mds_map.mds_info.at(who);
       fn(info);
-      fs->mds_map.epoch = epoch;
+      fs.mds_map.epoch = epoch;
+      fs.mds_map.modified = ceph_clock_now();
     }
   }
 
@@ -478,7 +504,7 @@ public:
     if (fscid == FS_CLUSTER_ID_NONE) {
       return standby_daemons.at(gid);
     } else {
-      return filesystems.at(fscid)->mds_map.mds_info.at(gid);
+      return filesystems.at(fscid).mds_map.mds_info.at(gid);
     }
   }
 
@@ -488,26 +514,26 @@ public:
     if (fscid == FS_CLUSTER_ID_NONE or !filesystem_exists(fscid)) {
       return std::string_view();
     } else {
-      return get_filesystem(fscid)->mds_map.get_fs_name();
+      return filesystems.at(fscid).mds_map.get_fs_name();
     }
   }
 
   bool is_standby_replay(mds_gid_t who) const
   {
-    return filesystems.at(mds_roles.at(who))->is_standby_replay(who);
+    return filesystems.at(mds_roles.at(who)).is_standby_replay(who);
   }
 
   mds_gid_t get_standby_replay(mds_gid_t who) const
   {
-    return filesystems.at(mds_roles.at(who))->get_standby_replay(who);
+    return filesystems.at(mds_roles.at(who)).get_standby_replay(who);
   }
 
-  Filesystem::const_ref get_legacy_filesystem()
+  const Filesystem* get_legacy_filesystem() const
   {
     if (legacy_client_fscid == FS_CLUSTER_ID_NONE) {
       return nullptr;
     } else {
-      return filesystems.at(legacy_client_fscid);
+      return &filesystems.at(legacy_client_fscid);
     }
   }
 
@@ -518,7 +544,7 @@ public:
   {
     auto fscid = mds_roles.at(who);
     modify_filesystem(fscid, [who, &targets](auto&& fs) {
-      fs->mds_map.mds_info.at(who).export_targets = targets;
+      fs.mds_map.mds_info.at(who).export_targets = targets;
     });
   }
 
@@ -530,25 +556,24 @@ public:
     return struct_version < STRUCT_VERSION_TRIM_TO;
   }
 
-  size_t filesystem_count() const {return filesystems.size();}
-  bool filesystem_exists(fs_cluster_id_t fscid) const {return filesystems.count(fscid) > 0;}
-  Filesystem::const_ref get_filesystem(fs_cluster_id_t fscid) const {return std::const_pointer_cast<const Filesystem>(filesystems.at(fscid));}
-  Filesystem::ref get_filesystem(fs_cluster_id_t fscid) {return filesystems.at(fscid);}
-  Filesystem::ref get_filesystem(mds_gid_t gid) {
-    return filesystems.at(mds_roles.at(gid));
+  size_t filesystem_count() const {
+    return filesystems.size();
   }
-  Filesystem::const_ref get_filesystem(void) const {return std::const_pointer_cast<const Filesystem>(filesystems.begin()->second);}
-  Filesystem::const_ref get_filesystem(std::string_view name) const;
-  Filesystem::const_ref get_filesystem(mds_gid_t gid) const {
+  bool filesystem_exists(fs_cluster_id_t fscid) const {
+    return filesystems.count(fscid) > 0;
+  }
+  const Filesystem& get_filesystem(fs_cluster_id_t fscid) const {
+    return filesystems.at(fscid);
+  }
+  const Filesystem& get_filesystem(void) const {
+    return filesystems.at(filesystems.begin()->second.fscid);
+  }
+  Filesystem const* get_filesystem(std::string_view name) const;
+  const Filesystem& get_filesystem(mds_gid_t gid) const {
     return filesystems.at(mds_roles.at(gid));
   }
 
-  std::vector<Filesystem::const_ref> get_filesystems(void) const;
-
-  int parse_filesystem(
-      std::string_view ns_str,
-      Filesystem::const_ref *result
-      ) const;
+  int parse_filesystem(std::string_view ns_str, Filesystem const** result) const;
 
   int parse_role(
       std::string_view role_str,
@@ -575,6 +600,18 @@ public:
 
   bool check_health(void);
 
+  const auto& get_mds_roles() const {
+    return mds_roles;
+  }
+
+  const auto& get_standby_daemons() const {
+    return standby_daemons;
+  }
+
+  const auto& get_standby_epochs() const {
+    return standby_epochs;
+  }
+
   /**
    * Assert that the FSMap, Filesystem, MDSMap, mds_info_t relations are
    * all self-consistent.
@@ -598,6 +635,13 @@ public:
   static void generate_test_instances(std::list<FSMap*>& ls);
 
 protected:
+  iterator begin() {
+    return filesystems.begin();
+  }
+  iterator end() {
+    return filesystems.end();
+  }
+
   epoch_t epoch = 0;
   uint64_t next_filesystem_id = FS_CLUSTER_ID_ANONYMOUS + 1;
   fs_cluster_id_t legacy_client_fscid = FS_CLUSTER_ID_NONE;
@@ -605,7 +649,7 @@ protected:
   bool enable_multiple = true;
   bool ever_enabled_multiple = true; // < the cluster had multiple FS enabled once
 
-  std::map<fs_cluster_id_t, Filesystem::ref> filesystems;
+  fsmap filesystems;
 
   // Remember which Filesystem an MDS daemon's info is stored in
   // (or in standby_daemons for FS_CLUSTER_ID_NONE)

src/mds/MDSMap.cc
@@ -818,7 +818,6 @@ void MDSMap::decode(bufferlist::const_iterator& p)
 {
   std::map<mds_rank_t,int32_t> inc;  // Legacy field, parse and drop
 
-  cached_up_features = 0;
   DECODE_START_LEGACY_COMPAT_LEN_16(5, 4, 4, p);
   decode(epoch, p);
   decode(flags, p);
@@ -1099,24 +1098,20 @@ void MDSMap::get_up_mds_set(std::set<mds_rank_t>& s) const {
     s.insert(p->first);
 }
 
-uint64_t MDSMap::get_up_features() {
-  if (!cached_up_features) {
-    bool first = true;
-    for (std::map<mds_rank_t, mds_gid_t>::const_iterator p = up.begin();
-         p != up.end();
-         ++p) {
-      std::map<mds_gid_t, mds_info_t>::const_iterator q =
-        mds_info.find(p->second);
-      ceph_assert(q != mds_info.end());
-      if (first) {
-        cached_up_features = q->second.mds_features;
-        first = false;
-      } else {
-        cached_up_features &= q->second.mds_features;
-      }
+uint64_t MDSMap::get_up_features() const {
+  uint64_t features = 0;
+  bool first = true;
+  for ([[maybe_unused]] auto& [rank, gid] : up) {
+    auto it = mds_info.find(gid);
+    ceph_assert(it != mds_info.end());
+    if (first) {
+      features = it->second.mds_features;
+      first = false;
+    } else {
+      features &= it->second.mds_features;
     }
   }
-  return cached_up_features;
+  return features;
 }
 
 void MDSMap::get_recovery_mds_set(std::set<mds_rank_t>& s) const {

src/mds/MDSMap.h
@@ -381,7 +381,7 @@ public:
   }
 
   // features
-  uint64_t get_up_features();
+  uint64_t get_up_features() const;
 
   /**
    * Get MDS ranks which are in but not up.
@@ -668,7 +668,6 @@ protected:
 
   bool inline_data_enabled = false;
 
-  uint64_t cached_up_features = 0;
 private:
   inline static const std::map<int, std::string> flag_display = {
     {CEPH_MDSMAP_NOT_JOINABLE, "joinable"}, //inverse for user display

src/mon/AuthMonitor.cc
@@ -1705,15 +1705,16 @@ bool AuthMonitor::prepare_command(MonOpRequestRef op)
     string mds_cap_string, osd_cap_string;
     string osd_cap_wanted = "r";
 
-    std::shared_ptr<const Filesystem> fs;
+    const Filesystem* fs = nullptr;
     if (filesystem != "*" && filesystem != "all") {
-      fs = mon.mdsmon()->get_fsmap().get_filesystem(filesystem);
+      const auto& fsmap = mon.mdsmon()->get_fsmap();
+      fs = fsmap.get_filesystem(filesystem);
       if (fs == nullptr) {
 	ss << "filesystem " << filesystem << " does not exist.";
 	err = -EINVAL;
 	goto done;
       } else {
-	mon_cap_string += " fsname=" + std::string(fs->mds_map.get_fs_name());
+	mon_cap_string += " fsname=" + std::string(fs->get_mds_map().get_fs_name());
       }
     }
 
@@ -1760,7 +1761,7 @@ bool AuthMonitor::prepare_command(MonOpRequestRef op)
       mds_cap_string += "allow " + cap;
 
       if (filesystem != "*" && filesystem != "all" && fs != nullptr) {
-	mds_cap_string += " fsname=" + std::string(fs->mds_map.get_fs_name());
+	mds_cap_string += " fsname=" + std::string(fs->get_mds_map().get_fs_name());
       }
 
       if (path != "/") {

commit 495f6b151ad5bb2d55b160a0734a3dc89a06791e

cephfs-journal-tool: Add warning messages during 'journal reset' and prevent execution on active fs
Fixes: https://tracker.ceph.com/issues/62925
Signed-off-by: Jos Collin <jcollin@redhat.com>
(cherry picked from commit 57f7d8b70f6989dee09a2adf5ff99b2917589488)

 Conflicts:
	src/tools/cephfs/JournalTool.cc
        - Resolved conflicts caused by 728cdc349d0442926ab4f4401bd457d6ae75ca07 is not backported to reef.

src/tools/cephfs/JournalTool.cc
@@ -47,7 +47,7 @@ void JournalTool::usage()
     << "      inspect\n"
     << "      import <path> [--force]\n"
     << "      export <path>\n"
-    << "      reset [--force]\n"
+    << "      reset [--force] <--yes-i-really-really-mean-it>\n"
     << "  cephfs-journal-tool [options] header <get|set> <field> <value>\n"
     << "    <field>: [trimmed_pos|expire_pos|write_pos|pool_id]\n"
     << "  cephfs-journal-tool [options] event <effect> <selector> <output> [special options]\n"
@@ -139,7 +139,12 @@ int JournalTool::main(std::vector<const char*> &argv)
   }
  
   auto fs = fsmap->get_filesystem(role_selector.get_ns());
-  ceph_assert(fs != nullptr);
+  stringstream (rank_str.substr(rank_str.find(':') + 1)) >> rank;
+  if (fs->mds_map.is_active(rank)) {
+    derr << "Cannot run cephfs-journal-tool on an active file system!" << dendl;
+    return -CEPHFS_EPERM;
+  }
+
   int64_t const pool_id = fs->mds_map.get_metadata_pool();
   dout(4) << "JournalTool: resolving pool " << pool_id << dendl;
   std::string pool_name;
@@ -197,7 +202,7 @@ int JournalTool::validate_type(const std::string &type)
   if (type == "mdlog" || type == "purge_queue") {
     return 0;
   }
-  return -1;
+  return -CEPHFS_EPERM;
 }
 
 std::string JournalTool::gen_dump_file_path(const std::string &prefix) {
@@ -251,14 +256,36 @@ int JournalTool::main_journal(std::vector<const char*> &argv)
     }
   } else if (command == "reset") {
     bool force = false;
-    if (argv.size() == 2) {
+    if (argv.size() == 1) {
+        std::cerr << "warning: this operation resets the journal!!!\n"
+                  << "Do not run this operation if you do not understand CephFS' internal storage mechanisms or have received specific instructions from those who do.\n"
+                  << "If you want to continue, please add --yes-i-really-really-mean-it!!!"
+                  << std::endl;
+        return -EINVAL;
+    } else if (argv.size() == 2) {
+      if (std::string(argv[1]) == "--force") {
+        std::cerr << "warning: this operation resets the journal!!!\n"
+                  << "Do not run this operation if you do not understand CephFS' internal storage mechanisms or have received specific instructions from those who do.\n"
+                  << "If you want to continue, please add --yes-i-really-really-mean-it!!!"
+                  << std::endl;
+        return -EINVAL;
+      } else if (std::string(argv[1]) != "--yes-i-really-really-mean-it") {
+        std::cerr << "Unknown argument " << argv[1] << std::endl;
+        return -EINVAL;
+      }
+    } else if (argv.size() == 3) {
       if (std::string(argv[1]) == "--force") {
         force = true;
       } else {
         std::cerr << "Unknown argument " << argv[1] << std::endl;
         return -EINVAL;
       }
-    } else if (argv.size() > 2) {
+
+      if (std::string(argv[2]) != "--yes-i-really-really-mean-it") {
+	std::cerr << "Unknown argument " << argv[2] << std::endl;
+        return -EINVAL;
+      }
+    } else if (argv.size() > 3) {
       std::cerr << "Too many arguments!" << std::endl;
       return -EINVAL;
     }

src/tools/cephfs/first-damage.py
@@ -25,7 +25,7 @@
 #
 # 4b) If all good so far, reset the journal:
 #
-#    cephfs-journal-tool --rank=<fs_name>:0 journal reset
+#    cephfs-journal-tool --rank=<fs_name>:0 journal reset --yes-i-really-really-mean-it
 #
 # 5) Run this tool to see list of damaged dentries:
 #