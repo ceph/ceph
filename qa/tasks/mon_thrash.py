514396211f59b4fca5f3da0615f459df5208df7a
Nitzan Mordechai <nmordech@redhat.com>
2024-07-02 08:31:38 +0000

    thrashers: standardize stop and join method names
    Thrashers that do not inherit from ThrasherGreenlet previously used a
    method called do_join, which combined stop and join functionality. To
    ensure consistency and clarity, we want all thrashers to use separate
    stop, join, and stop_and_join methods.
    
    This commit renames methods and implements missing stop and stop_and_join
    methods in thrashers that did not inherit from ThrasherGreenlet.
    
    Fixes: https://tracker.ceph.com/issues/66698
    Signed-off-by: Nitzan Mordechai <nmordech@redhat.com>
    (cherry picked from commit a035b5a22fb87794e65d112c0d21dcbd28b728ad)

--- qa/tasks/mon_thrash.py
+++ qa/tasks/mon_thrash.py
@@ -149,12 +149,25 @@ class MonitorThrasher(Thrasher):
         """
         self.logger.info(x)
 
-    def do_join(self):
+    def stop(self):
+        """
+        Stop the thrashing process.
+        """
+        self.stopping = True
+
+    def join(self):
         """
         Break out of this processes thrashing loop.
         """
         self.stopping = True
         self.thread.get()
+
+    def stop_and_join(self):
+        """
+        Stop the thrashing process and join the thread.
+        """
+        self.stop()
+        return self.join()
 
     def should_thrash_store(self):
         """
@@ -415,6 +428,6 @@ def task(ctx, config):
         yield
     finally:
         log.info('joining mon_thrasher')
-        thrash_proc.do_join()
+        thrash_proc.stop_and_join()
         mons = _get_mons(ctx)
         manager.wait_for_mon_quorum_size(len(mons))
