e8638a13255afd5a8849538b9f1b180f7a5b2420
Hoai-Thu Vuong <thuvh87@gmail.com>
2024-07-09 23:56:51 +0700

    rgw: allow bucket notification send message to kafka with multiple brokers
    - add new parameter to define list of broker
    - update document
    - change nose to nose-py3
    - add test case for multiple brokers (happy case)
    
    Signed-off-by: Hoai-Thu Vuong <thuvh87@gmail.com>
    (cherry picked from commit 1a34cd1be9f2c10a8262372e0a5bab396655bdab)
    
    Conflicts:
    	doc/radosgw/notifications.rst
    	qa/tasks/notification_tests.py
    	src/rgw/driver/rados/rgw_pubsub_push.cc
    	src/rgw/rgw_kafka.cc
    	src/rgw/rgw_kafka.h
    	src/test/rgw/bucket_notification/test_bn.py
    - update doc with new params
    - add only kafka-failover
    - update param in connect function of rgw_kafka
    - resolve conflict by keep two function:
        - test_ps_s3_notification_push_amqp_on_master
        - notification_push

--- src/rgw/rgw_kafka.cc
+++ src/rgw/rgw_kafka.cc
@@ -13,6 +13,7 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
+#include <boost/algorithm/string.hpp>
 #include <boost/functional/hash.hpp>
 #include <boost/lockfree/queue.hpp>
 #include "common/dout.h"
@@ -594,13 +595,14 @@ public:
 
   // connect to a broker, or reuse an existing connection if already connected
   bool connect(connection_id_t& conn_id,
-               const std::string& url,
-               bool use_ssl,
-               bool verify_ssl,
-               boost::optional<const std::string&> ca_location,
-               boost::optional<const std::string&> mechanism,
-               boost::optional<const std::string&> topic_user_name,
-               boost::optional<const std::string&> topic_password) {
+          const std::string& url, 
+          bool use_ssl,
+          bool verify_ssl,
+          boost::optional<const std::string&> ca_location,
+          boost::optional<const std::string&> mechanism,
+          boost::optional<const std::string&> topic_user_name,
+          boost::optional<const std::string&> topic_password,
+          boost::optional<const std::string&> brokers) {
     if (stopped) {
       ldout(cct, 1) << "Kafka connect: manager is stopped" << dendl;
       return false;
@@ -608,8 +610,8 @@ public:
 
     std::string user;
     std::string password;
-    std::string broker;
-    if (!parse_url_authority(url, broker, user, password)) {
+    std::string broker_list;
+    if (!parse_url_authority(url, broker_list, user, password)) {
       // TODO: increment counter
       ldout(cct, 1) << "Kafka connect: URL parsing failed" << dendl;
       return false;
@@ -637,7 +639,13 @@ public:
       ldout(cct, 1) << "Kafka connect: user/password are only allowed over secure connection" << dendl;
       return false;
     }
-    connection_id_t tmp_id(broker, user, password, ca_location, mechanism,
+
+    if (brokers.has_value()) {
+      broker_list.append(",");
+      broker_list.append(brokers.get());
+    }
+
+    connection_id_t tmp_id(broker_list, user, password, ca_location, mechanism,
                            use_ssl);
     std::lock_guard lock(connections_lock);
     const auto it = connections.find(tmp_id);
@@ -657,7 +665,7 @@ public:
       return false;
     }
 
-    auto conn = std::make_unique<connection_t>(cct, broker, use_ssl, verify_ssl, ca_location, user, password, mechanism);
+    auto conn = std::make_unique<connection_t>(cct, broker_list, use_ssl, verify_ssl, ca_location, user, password, mechanism);
     if (!new_producer(conn.get())) {
       ldout(cct, 10) << "Kafka connect: producer creation failed in new connection" << dendl;
       return false;
@@ -775,11 +783,12 @@ bool connect(connection_id_t& conn_id,
              boost::optional<const std::string&> ca_location,
              boost::optional<const std::string&> mechanism,
              boost::optional<const std::string&> user_name,
-             boost::optional<const std::string&> password) {
+             boost::optional<const std::string&> password,
+             boost::optional<const std::string&> brokers) {
   std::shared_lock lock(s_manager_mutex);
   if (!s_manager) return false;
   return s_manager->connect(conn_id, url, use_ssl, verify_ssl, ca_location,
-                            mechanism, user_name, password);
+                            mechanism, user_name, password, brokers);
 }
 
 int publish(const connection_id_t& conn_id,
