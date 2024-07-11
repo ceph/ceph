09f4c06375409ebd38442863db83b9c1a81b0aaf
Garry Drankovich <garry.drankovich@clyso.com>
2024-07-11 18:08:25 +0300

    test/store_test: enforce ssd settings for reformatting cases.
    This is required to avoid buffer caching which would prevent content
    reformatting.
    
    Signed-off-by: Garry Drankovich <garry.drankovich@clyso.com>

--- src/test/objectstore/store_test.cc
+++ src/test/objectstore/store_test.cc
@@ -11334,7 +11334,18 @@ TEST_P(StoreTestOmapUpgrade, LargeLegacyToPG) {
   }
 }
 
-TEST_P(StoreTest, BasicReformattingTest) {
+TEST_P(StoreTestSpecificAUSize, BasicReformattingTest) {
+  if (string(GetParam()) != "bluestore")
+    return;
+
+  // enforce 'ssd' settings to avoid deferred writes
+  // which result in cached data blocks and hence
+  // prevents from reformatting
+  SetVal(g_conf(), "bluestore_debug_enforce_settings", "ssd");
+  g_conf().apply_changes(nullptr);
+
+  StartDeferred(0x1000);
+
   int r;
   coll_t cid;
   ghobject_t obj(hobject_t(sobject_t("Object 1", CEPH_NOSNAP)));
@@ -11580,7 +11591,18 @@ TEST_P(StoreTest, BasicReformattingTest) {
   }
 }
 
-TEST_P(StoreTest, CompressedReformattingTest) {
+TEST_P(StoreTestSpecificAUSize, CompressedReformattingTest) {
+  if (string(GetParam()) != "bluestore")
+    return;
+
+  // enforce 'ssd' settings to avoid deferred writes
+  // which result in cached data blocks and hence
+  // prevents from reformatting
+  SetVal(g_conf(), "bluestore_debug_enforce_settings", "ssd");
+  g_conf().apply_changes(nullptr);
+
+  StartDeferred(0x1000);
+
   int r;
   coll_t cid;
 
@@ -11856,7 +11878,18 @@ TEST_P(StoreTest, CompressedReformattingTest) {
   }
 }
 
-TEST_P(StoreTest, LazyCompressionReformattingTest) {
+TEST_P(StoreTestSpecificAUSize, LazyCompressionReformattingTest) {
+  if (string(GetParam()) != "bluestore")
+    return;
+
+  // enforce 'ssd' settings to avoid deferred writes
+  // which result in cached data blocks and hence
+  // prevents from reformatting
+  SetVal(g_conf(), "bluestore_debug_enforce_settings", "ssd");
+  g_conf().apply_changes(nullptr);
+
+  StartDeferred(0x1000);
+
   int r;
   coll_t cid;
 
