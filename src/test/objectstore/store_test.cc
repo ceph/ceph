5dcfa156b4499d920ba928a663fa2cca2772c82b
Garry Drankovich <garry.drankovich@clyso.com>
2024-07-11 18:08:25 +0300

    test/store_test: enforce ssd settings for reformatting cases.
    This is required to avoid buffer caching which would prevent content
    reformatting.
    
    Signed-off-by: Garry Drankovich <garry.drankovich@clyso.com>

--- src/test/objectstore/store_test.cc
+++ src/test/objectstore/store_test.cc
@@ -12002,7 +12002,18 @@ TEST_P(StoreTestSpecificAUSize, BlueFSReservedTest) {
             g_conf()->bluefs_alloc_size + wal_extra);
 }
 
-TEST_P(StoreTest, BasicReformattingTest) {
+TEST_P(StoreTestSpecificAUSize, BasicReformattingTest) {
+  if (string(GetParam()) != "bluestore")
+    return;
+
+  // enforce 'ssd' settings to avoid deferred writes
+  // which result in cached data blocks and hence
+  // prevents from reformatting
+  SetVal(g_conf(), "bluestore_debug_enforce_settings", "ssd");
+  g_conf().apply_changes(nullptr);
+
+  StartDeferred(0x1000);
+
   int r;
   coll_t cid;
   ghobject_t obj(hobject_t(sobject_t("Object 1", CEPH_NOSNAP)));
@@ -12248,7 +12259,18 @@ TEST_P(StoreTest, BasicReformattingTest) {
   }
 }
 
-TEST_P(StoreTest, CompressedReformattingTest) {
+TEST_P(StoreTestSpecificAUSize, CompressedReformattingTest) {
+  if (string(GetParam()) != "bluestore")
+    return;
+
+  // enforce 'ssd' settings to avoid deferred writes
+  // which result in cached data blocks and hence
+  // prevents from reformatting
+  SetVal(g_conf(), "bluestore_debug_enforce_settings", "ssd");
+  g_conf().apply_changes(nullptr);
+
+  StartDeferred(0x1000);
+
   int r;
   coll_t cid;
 
@@ -12524,7 +12546,18 @@ TEST_P(StoreTest, CompressedReformattingTest) {
   }
 }
 
-TEST_P(StoreTest, LazyCompressionReformattingTest) {
+TEST_P(StoreTestSpecificAUSize, LazyCompressionReformattingTest) {
+  if (string(GetParam()) != "bluestore")
+    return;
+
+  // enforce 'ssd' settings to avoid deferred writes
+  // which result in cached data blocks and hence
+  // prevents from reformatting
+  SetVal(g_conf(), "bluestore_debug_enforce_settings", "ssd");
+  g_conf().apply_changes(nullptr);
+
+  StartDeferred(0x1000);
+
   int r;
   coll_t cid;
 
