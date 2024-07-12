a258a361bb01a7d106ca296a4965d85e034dadcb
Dhairya Parmar <dparmar@redhat.com>
2024-07-12 20:50:47 +0530

    client: restrict bufferlist to total write size
    In linux, systems calls like write() anyways don't allow writes > 2GiB,
    the total write size passed to the Client::_write is clamped to INT_MAX
    but bufferlist is not handled. This edge case is patched here.
    
    bug that arises due to buffer list beyond INT_MAX stalls async i/o due to:
    unknown file: Failure
    C++ exception with description "End of buffer [buffer:2]" thrown in the test body.
    2024-05-28T16:17:06.854+0530 7f9a5d24c9c0  2 client.4311 unmount
    2024-05-28T16:17:06.854+0530 7f9a5d24c9c0  2 client.4311 unmounting
    
    which results in disconnected inode and cap leaks:
    2024-05-28T16:17:11.855+0530 7f9a5d24c9c0  1 client.4311 dump_inode: DISCONNECTED inode 0x10000000001 #0x10000000001 ref 3 0x10000000001.head(faked_ino=0 nref=3 ll_ref=0 cap_refs={4=0,1024=1,4096=1,8192=2} open={3=0} mode=100666 size=0/4294967296 nlink=1 btime=2024-05-28T16:17:03.387546+0530 mtime=2024-05-28T16:17:03.387546+0530 ctime=2024-05-28T16:17:03.387546+0530 change_attr=0 caps=pAsxLsXsxFsxcrwb(0=pAsxLsXsxFsxcrwb) objectset[0x10000000001 ts 0/0 objects 1 dirty_or_tx 0] 0x7f9a2c009530)
    2024-05-28T16:17:11.855+0530 7f9a5d24c9c0  2 client.4311 cache still has 0+1 items, waiting (for caps to release?)
    
    Fixes: https://tracker.ceph.com/issues/66245
    Signed-off-by: Dhairya Parmar <dparmar@redhat.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -11603,13 +11603,20 @@ int64_t Client::_write(Fh *f, int64_t offset, uint64_t size, const char *buf,
 
   // copy into fresh buffer (since our write may be resub, async)
   bufferlist bl;
+  size_t total_appended = 0;
   if (buf) {
     if (size > 0)
       bl.append(buf, size);
   } else if (iov){
     for (int i = 0; i < iovcnt; i++) {
       if (iov[i].iov_len > 0) {
-        bl.append((const char *)iov[i].iov_base, iov[i].iov_len);
+        if (total_appended + iov[i].iov_len >= static_cast<size_t>(size)) {
+          bl.append((const char *)iov[i].iov_base, size - total_appended);
+          break;
+        } else {
+          bl.append((const char *)iov[i].iov_base, iov[i].iov_len);
+          total_appended += iov[i].iov_len;
+        }
       }
     }
   }
