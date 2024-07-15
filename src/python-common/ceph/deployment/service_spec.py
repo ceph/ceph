3333cef8196296f8c0b6e81f7a36f3a24fcf1c41
Adam King <adking@redhat.com>
2024-07-15 11:57:47 -0400

    mgr/cephadm: add tcp mode for ingress over RGW
    In order to allow TLS for the full path client -> haproxy
    -> RGW, give the option for the ingress service to just
    serve in tcp mode and pass received data directly
    to the backend servers. Makes no difference for the
    ingress over nfs case that already used tcp mode.
    
    Signed-off-by: Adam King <adking@redhat.com>

--- src/python-common/ceph/deployment/service_spec.py
+++ src/python-common/ceph/deployment/service_spec.py
@@ -1690,6 +1690,7 @@ class IngressSpec(ServiceSpec):
                  extra_entrypoint_args: Optional[GeneralArgList] = None,
                  custom_configs: Optional[List[CustomConfig]] = None,
                  health_check_interval: Optional[str] = None,
+                 use_tcp_mode_over_rgw: bool = False,
                  ):
         assert service_type == 'ingress'
 
@@ -1724,6 +1725,7 @@ class IngressSpec(ServiceSpec):
         self.enable_haproxy_protocol = enable_haproxy_protocol
         self.health_check_interval = health_check_interval.strip(
         ) if health_check_interval else None
+        self.use_tcp_mode_over_rgw = use_tcp_mode_over_rgw
 
     def get_port_start(self) -> List[int]:
         ports = []
