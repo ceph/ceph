b3357159a448598153fa6ef92c08d5b3ff84462a
Rishabh Dave <ridave@redhat.com>
2024-07-26 01:29:30 +0530

    DNM: mgr/vol: use SubvolumeOpType.CLONE_SOURCE
    Use SubVolumeOpType.CLONE_SOURCE to get the path of the snapshot that
    is being cloned instead of SubvolumeOpType.GET_PATH.
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- src/pybind/mgr/volumes/fs/volume.py
+++ src/pybind/mgr/volumes/fs/volume.py
@@ -855,6 +855,7 @@ class VolumeClient(CephfsClient["Module"]):
         src_subvol_details = dst_subvol._get_clone_source()
         src_group_name = src_subvol_details.get('group', dst_group.NO_GROUP_NAME)
         src_subvol_name = src_subvol_details['subvolume']
+        src_snap_name = src_subvol_details['snapshot']
 
         # We exercise op type checks on subvolume but not on subvolumegroups and we don't allow
         # internal directories (including "_nogroup" to be opened). To do that we need to pass
@@ -867,14 +868,14 @@ class VolumeClient(CephfsClient["Module"]):
         if src_group_name != dst_group.groupname:
             with open_subvol_in_group(self.mgr, vol_handle, self.volspec,
                                       src_group_name, src_subvol_name,
-                                      SubvolumeOpType.GETPATH) as src_subvol:
-                src_path = src_subvol.base_path.decode('utf-8')
+                                      SubvolumeOpType.CLONE_SOURCE) as src_subvol:
+                src_path = src_subvol.snapshot_data_path(src_snap_name).decode('utf-8')
 
         else:
             with open_subvol(self.mgr, vol_handle, self.volspec, dst_group,
-                             src_subvol_name, SubvolumeOpType.GETPATH) as \
+                             src_subvol_name, SubvolumeOpType.CLONE_SOURCE) as \
                              src_subvol:
-                src_path = src_subvol.base_path.decode('utf-8')
+                src_path = src_subvol.snapshot_data_path(src_snap_name).decode('utf-8')
 
         return src_path
 
