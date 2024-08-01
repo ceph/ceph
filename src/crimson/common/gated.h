b2bffe5695f6c54f1e9c60e1e6c5580734b84f86
Nitzan Mordechai <nmordech@redhat.com>
2024-08-01 10:41:25 +0000

    crimson: use gate per shard for AlienStore and OSD
    This change modifies AlienStore and OSD to use a gate per seastar
    shard instead of a shared gate.
    It introduces a new `gate_per_shard` class in `Gated` to manage
    a vector of gates, ensuring proper gate handling across shards.
    
    Fixes: https://tracker.ceph.com/issues/64332
    Signed-off-by: NitzanMordhai <nmordech@redhat.com>

--- src/crimson/common/gated.h
+++ src/crimson/common/gated.h
@@ -6,6 +6,8 @@
 #include <seastar/core/gate.hh>
 #include <seastar/core/future.hh>
 #include <seastar/core/future-util.hh>
+#include <type_traits>
+#include <vector>
 
 #include "crimson/common/exception.h"
 #include "crimson/common/log.h"
@@ -15,13 +17,22 @@ namespace crimson::common {
 
 class Gated {
  public:
+  Gated() = default;
+  Gated(const Gated&) = delete;
+  Gated& operator=(const Gated&) = delete;
+  Gated(Gated&&) = default;
+  Gated& operator=(Gated&&) = default;
+  virtual ~Gated() = default;
+  
   static seastar::logger& gated_logger() {
     return crimson::get_logger(ceph_subsys_osd);
   }
+
   template <typename Func, typename T>
   inline void dispatch_in_background(const char* what, T& who, Func&& func) {
-    (void) dispatch(what, who, func);
+    (void) dispatch(what, who, std::forward<Func>(func));
   }
+
   template <typename Func, typename T>
   inline seastar::future<> dispatch(const char* what, T& who, Func&& func) {
     return seastar::with_gate(pending_dispatch, std::forward<Func>(func)
@@ -42,7 +53,12 @@ class Gated {
     });
   }
 
-  seastar::future<> close() {
+  template <typename Func>
+  auto submit_with_gate(const char* what, Func&& func) {
+    return seastar::with_gate(pending_dispatch, std::forward<Func>(func));
+  }
+
+  virtual seastar::future<> close() {
     return pending_dispatch.close();
   }
   bool is_closed() const {
@@ -52,4 +68,43 @@ class Gated {
   seastar::gate pending_dispatch;
 };
 
-}// namespace crimson::common
+class gate_per_shard : public Gated {
+ public:
+  gate_per_shard() : gates(seastar::smp::count) {}
+  explicit gate_per_shard(size_t shard_count) : gates(shard_count) {}
+  gate_per_shard(const gate_per_shard&) = delete;
+  gate_per_shard& operator=(const gate_per_shard&) = delete;
+  gate_per_shard(gate_per_shard&&) = default;
+  gate_per_shard& operator=(gate_per_shard&&) = default;
+  ~gate_per_shard() override = default;
+
+  template <typename Func, typename T>
+  inline auto dispatch_in_background(const char* what, T& who, Func&& func) {
+    return dispatch(what, who, std::forward<Func>(func));
+  }
+
+  template <typename Func, typename T>
+  inline auto dispatch(const char* what, T& who, Func&& func) {
+    return gates[seastar::this_shard_id()].dispatch(what, who, std::forward<Func>(func));
+  }
+
+  template <typename Func>
+  auto submit_with_gate(const char* what, Func&& func) {
+    return gates[seastar::this_shard_id()].submit_with_gate(what, std::forward<Func>(func));
+  }
+
+  bool is_closed() const {
+    return gates[seastar::this_shard_id()].is_closed();
+  }
+
+  seastar::future<> close_all() {
+    return seastar::parallel_for_each(gates, [](Gated& gated) {
+      return gated.close();
+    });
+  }
+
+ private:
+  std::vector<Gated> gates;
+};
+
+} // namespace crimson::common
