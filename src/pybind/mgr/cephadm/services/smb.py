1a603d0e5308e4c04f22405eb2a08ff14bfd6a95
Shachar Sharon <ssharon@redhat.com>
2024-08-08 08:55:05 +0300

    cephadm: propagate smbmetrics image via config_blobs
    Current code assumes single image for each daemon-service. However,
    Samba's metrics-exporter requires different image then the one used by
    main samba server. Use the auxiliary 'config_blobs' map to propagate
    the metrics image downwards to samba-service, plus metrics port number.
    
    Signed-off-by: Shachar Sharon <ssharon@redhat.com>

--- src/pybind/mgr/cephadm/services/smb.py
+++ src/pybind/mgr/cephadm/services/smb.py
@@ -16,6 +16,7 @@ logger = logging.getLogger(__name__)
 
 class SMBService(CephService):
     TYPE = 'smb'
+    DEFAULT_EXPORTER_PORT = 9922
 
     def config(self, spec: ServiceSpec) -> None:
         assert self.TYPE == spec.service_type
@@ -56,6 +57,11 @@ class SMBService(CephService):
                 smb_spec, daemon_spec.daemon_id, ceph_users
             )
         )
+        config_blobs['metrics_image'] = (
+            self.mgr.container_image_samba_metrics
+        )
+        config_blobs['metrics_port'] = SMBService.DEFAULT_EXPORTER_PORT
+
         logger.debug('smb generate_config: %r', config_blobs)
         return config_blobs, []
 
