3c9a869883d1f795aa9d34b44915748828f7d882
Leonid Chernin <leonidc@il.ibm.com>
2024-08-13 04:45:54 +0000

    indentations

--- src/mon/NVMeofGwMap.h
+++ src/mon/NVMeofGwMap.h
@@ -38,63 +38,78 @@ class Monitor;
 class NVMeofGwMap
 {
 public:
-    Monitor*                            mon           = NULL;
-    epoch_t                             epoch         = 0;      // epoch is for Paxos synchronization  mechanizm
-    bool                                delay_propose = false;
-    std::map<NvmeGroupKey, NvmeGwMonStates>  created_gws;
-    std::map<NvmeGroupKey, NvmeGwTimers> fsm_timers;// map that handles timers started by all Gateway FSMs
-    void to_gmap(std::map<NvmeGroupKey, NvmeGwMonClientStates>& Gmap, bool gw_version_last) const;
+  Monitor* mon = NULL;
+  epoch_t epoch = 0;      // epoch is for Paxos synchronization  mechanizm
+  std::map<NvmeGroupKey, NvmeGwMonStates>  created_gws;
+  std::map<NvmeGroupKey, NvmeGwTimers> fsm_timers;// map that handles timers started by all Gateway FSMs
+  void to_gmap(std::map<NvmeGroupKey, NvmeGwMonClientStates>& Gmap, bool gw_version_last) const;
 
-    int   cfg_add_gw                    (const NvmeGwId &gw_id, const NvmeGroupKey& group_key);
-    int   cfg_delete_gw                 (const NvmeGwId &gw_id, const NvmeGroupKey& group_key);
-    void  process_gw_map_ka             (const NvmeGwId &gw_id, const NvmeGroupKey& group_key, epoch_t& last_osd_epoch,  bool &propose_pending);
-    int   process_gw_map_gw_down        (const NvmeGwId &gw_id, const NvmeGroupKey& group_key, bool &propose_pending);
-    void  update_active_timers          (bool &propose_pending);
-    void  handle_abandoned_ana_groups   (bool &propose_pending);
-    void  handle_removed_subsystems     (const NvmeGwId &gw_id, const NvmeGroupKey& group_key, const std::vector<NvmeNqnId> &current_subsystems, bool &propose_pending);
-    void  start_timer (const NvmeGwId &gw_id, const NvmeGroupKey& group_key, NvmeAnaGrpId anagrpid, uint8_t value);
+  int   cfg_add_gw(const NvmeGwId &gw_id, const NvmeGroupKey& group_key);
+  int   cfg_delete_gw(const NvmeGwId &gw_id, const NvmeGroupKey& group_key);
+  void  process_gw_map_ka(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+                     epoch_t& last_osd_epoch,  bool &propose_pending);
+  int   process_gw_map_gw_down(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+                     bool &propose_pending);
+  void  update_active_timers(bool &propose_pending);
+  void  handle_abandoned_ana_groups(bool &propose_pending);
+  void  handle_removed_subsystems(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+            const std::vector<NvmeNqnId> &current_subsystems, bool &propose_pending);
+  void  start_timer(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+                   NvmeAnaGrpId anagrpid, uint8_t value);
 private:
-    void add_grp_id   (const NvmeGwId &gw_id, const NvmeGroupKey& group_key, const NvmeAnaGrpId grpid);
-    void remove_grp_id(const NvmeGwId &gw_id, const NvmeGroupKey& group_key, const NvmeAnaGrpId grpid);
-    void fsm_handle_gw_down    (const NvmeGwId &gw_id, const NvmeGroupKey& group_key,  gw_states_per_group_t state, NvmeAnaGrpId grpid,  bool &map_modified);
-    void fsm_handle_gw_delete  (const NvmeGwId &gw_id, const NvmeGroupKey& group_key,  gw_states_per_group_t state, NvmeAnaGrpId grpid,  bool &map_modified);
-    void fsm_handle_gw_alive   (const NvmeGwId &gw_id, const NvmeGroupKey& group_key,  NvmeGwMonState & gw_state, gw_states_per_group_t state,
-                                                                                   NvmeAnaGrpId grpid, epoch_t& last_osd_epoch, bool &map_modified);
-    void fsm_handle_to_expired (const NvmeGwId &gw_id, const NvmeGroupKey& group_key,  NvmeAnaGrpId grpid,  bool &map_modified);
+  void add_grp_id(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+               const NvmeAnaGrpId grpid);
+  void remove_grp_id(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+               const NvmeAnaGrpId grpid);
+  void fsm_handle_gw_down(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+               gw_states_per_group_t state, NvmeAnaGrpId grpid, bool &map_modified);
+  void fsm_handle_gw_delete(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+               gw_states_per_group_t state, NvmeAnaGrpId grpid, bool &map_modified);
+  void fsm_handle_gw_alive(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+               NvmeGwMonState & gw_state, gw_states_per_group_t state,
+               NvmeAnaGrpId grpid, epoch_t& last_osd_epoch, bool &map_modified);
+  void fsm_handle_to_expired(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+               NvmeAnaGrpId grpid,  bool &map_modified);
 
-    void find_failover_candidate(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,  NvmeAnaGrpId grpid, bool &propose_pending);
-    void find_failback_gw       (const NvmeGwId &gw_id, const NvmeGroupKey& group_key,  bool &propose_pending);
-    void set_failover_gw_for_ANA_group (const NvmeGwId &failed_gw_id, const NvmeGroupKey& group_key, const NvmeGwId &gw_id,
-                                                                                                     NvmeAnaGrpId groupid);
+  void find_failover_candidate(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+               NvmeAnaGrpId grpid, bool &propose_pending);
+  void find_failback_gw(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+               bool &propose_pending);
+  void set_failover_gw_for_ANA_group(const NvmeGwId &failed_gw_id,
+             const NvmeGroupKey& group_key, const NvmeGwId &gw_id, NvmeAnaGrpId groupid);
 
 
-    int  get_timer   (const NvmeGwId &gw_id, const NvmeGroupKey& group_key, NvmeAnaGrpId anagrpid);
-    void cancel_timer(const NvmeGwId &gw_id, const NvmeGroupKey& group_key, NvmeAnaGrpId anagrpid);
-    void validate_gw_map(const NvmeGroupKey& group_key);
+  int  get_timer(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+            NvmeAnaGrpId anagrpid);
+  void cancel_timer(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+       NvmeAnaGrpId anagrpid);
+  void validate_gw_map(const NvmeGroupKey& group_key);
 
 public:
-    int  blocklist_gw(const NvmeGwId &gw_id, const NvmeGroupKey& group_key, NvmeAnaGrpId ANA_groupid, epoch_t &epoch, bool failover);
+  int  blocklist_gw(const NvmeGwId &gw_id, const NvmeGroupKey& group_key,
+        NvmeAnaGrpId ANA_groupid, epoch_t &epoch, bool failover);
 
-    void encode(ceph::buffer::list &bl, uint64_t features) const {
-      uint8_t version = 1;
-      ENCODE_START(version, 1, bl);
-      dout(20) << "encode version " << version  << version << " features " << features << dendl;
-      using ceph::encode;
-      encode(epoch, bl);// global map epoch
-      encode(created_gws, bl, features); //Encode created GWs
-      encode(fsm_timers, bl, features);
-      ENCODE_FINISH(bl);
-    }
+  void encode(ceph::buffer::list &bl, uint64_t features) const {
+    uint8_t version = 1;
+    ENCODE_START(version, 1, bl);
+    dout(20) << "encode version " << version  << version
+        << " features " << features << dendl;
+    using ceph::encode;
+    encode(epoch, bl);// global map epoch
+    encode(created_gws, bl, features); //Encode created GWs
+    encode(fsm_timers, bl, features);
+    ENCODE_FINISH(bl);
+  }
 
-    void decode(ceph::buffer::list::const_iterator &bl) {
-      using ceph::decode;
-      DECODE_START(2, bl);
-      dout(20) << "decode version " << struct_v   << dendl;
-      decode(epoch, bl);
-      decode(created_gws, bl);
-      decode(fsm_timers, bl);
-      DECODE_FINISH(bl);
-    }
+  void decode(ceph::buffer::list::const_iterator &bl) {
+    using ceph::decode;
+    DECODE_START(2, bl);
+    dout(20) << "decode version " << struct_v   << dendl;
+    decode(epoch, bl);
+    decode(created_gws, bl);
+    decode(fsm_timers, bl);
+    DECODE_FINISH(bl);
+  }
 };
 
 #include "NVMeofGwSerialize.h"
