b20e89dc201158302ae98d5a144af351f1da417a
Patrick Donnelly <pdonnell@redhat.com>
2024-08-22 09:58:01 -0400

    mds: move fscrypt_auth inode_t metadata to mds_co mempool
    https://tracker.ceph.com/issues/67687
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>
    (cherry picked from commit ed9481c1ac69b0069849d250470d9fb2d1ae06b4)

--- src/mds/CInode.cc
+++ src/mds/CInode.cc
@@ -4224,7 +4224,7 @@ void CInode::encode_cap_message(const ref_t<MClientCaps> &m, Capability *cap)
   m->truncate_seq = i->truncate_seq;
   m->truncate_size = i->truncate_size;
   m->fscrypt_file = i->fscrypt_file;
-  m->fscrypt_auth = i->fscrypt_auth;
+  m->fscrypt_auth.assign(i->fscrypt_auth.begin(), i->fscrypt_auth.end());
   m->mtime = i->mtime;
   m->atime = i->atime;
   m->ctime = i->ctime;
