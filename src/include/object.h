8fbdf57aee4ea2a615e804e64e843fba7378c963
Xuehan Xu <xuxuehan@qianxin.com>
2024-08-27 22:04:52 +0800

    crimson/osd/pg: clear backfill_state when the PG goes clean
    Fixes: https://tracker.ceph.com/issues/67731
    Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

---
+++ src/include/object.h
@@ -0,0 +1,229 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*- 
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2004-2006 Sage Weil <sage@newdream.net>
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software 
+ * Foundation.  See file COPYING.
+ * 
+ */
+
+#ifndef CEPH_OBJECT_H
+#define CEPH_OBJECT_H
+
+#include <cstdint>
+#include <cstdio>
+#include <iomanip>
+#include <iosfwd>
+#include <string>
+#include <string>
+#include <string_view>
+
+#include <fmt/compile.h>
+#include <fmt/format.h>
+
+#include "include/rados.h"
+#include "include/unordered_map.h"
+#include "common/Formatter.h"
+
+#include "hash.h"
+#include "encoding.h"
+#include "ceph_hash.h"
+
+struct object_t {
+  std::string name;
+
+  object_t() {}
+  // cppcheck-suppress noExplicitConstructor
+  object_t(const char *s) : name(s) {}
+  // cppcheck-suppress noExplicitConstructor
+  object_t(const std::string& s) : name(s) {}
+  object_t(std::string&& s) : name(std::move(s)) {}
+  object_t(std::string_view s) : name(s) {}
+
+  auto operator<=>(const object_t&) const noexcept = default;
+
+  void swap(object_t& o) {
+    name.swap(o.name);
+  }
+  void clear() {
+    name.clear();
+  }
+
+  void encode(ceph::buffer::list &bl) const {
+    using ceph::encode;
+    encode(name, bl);
+  }
+  void decode(ceph::buffer::list::const_iterator &bl) {
+    using ceph::decode;
+    decode(name, bl);
+  }
+
+  void dump(ceph::Formatter *f) const {
+    f->dump_string("name", name);
+  }
+
+  static void generate_test_instances(std::list<object_t*>& o) {
+    o.push_back(new object_t);
+    o.push_back(new object_t("myobject"));
+  }
+};
+WRITE_CLASS_ENCODER(object_t)
+
+inline std::ostream& operator<<(std::ostream& out, const object_t& o) {
+  return out << o.name;
+}
+
+namespace std {
+template<> struct hash<object_t> {
+  size_t operator()(const object_t& r) const {
+    //static hash<string> H;
+    //return H(r.name);
+    return ceph_str_hash_linux(r.name.c_str(), r.name.length());
+  }
+};
+} // namespace std
+
+
+struct file_object_t {
+  uint64_t ino, bno;
+  mutable char buf[34];
+
+  file_object_t(uint64_t i=0, uint64_t b=0) : ino(i), bno(b) {
+    buf[0] = 0;
+  }
+  
+  const char *c_str() const {
+    if (!buf[0])
+      snprintf(buf, sizeof(buf), "%llx.%08llx", (long long unsigned)ino, (long long unsigned)bno);
+    return buf;
+  }
+
+  operator object_t() {
+    return object_t(c_str());
+  }
+};
+
+
+// ---------------------------
+// snaps
+
+struct snapid_t {
+  uint64_t val;
+  // cppcheck-suppress noExplicitConstructor
+  constexpr snapid_t(uint64_t v=0) : val(v) {}
+  snapid_t operator+=(snapid_t o) { val += o.val; return *this; }
+  snapid_t operator++() { ++val; return *this; }
+  constexpr operator uint64_t() const { return val; }
+};
+
+inline void encode(snapid_t i, ceph::buffer::list &bl) {
+  using ceph::encode;
+  encode(i.val, bl);
+}
+inline void decode(snapid_t &i, ceph::buffer::list::const_iterator &p) {
+  using ceph::decode;
+  decode(i.val, p);
+}
+
+template<>
+struct denc_traits<snapid_t> {
+  static constexpr bool supported = true;
+  static constexpr bool featured = false;
+  static constexpr bool bounded = true;
+  static constexpr bool need_contiguous = true;
+  static void bound_encode(const snapid_t& o, size_t& p) {
+    denc(o.val, p);
+  }
+  static void encode(const snapid_t &o, ceph::buffer::list::contiguous_appender& p) {
+    denc(o.val, p);
+  }
+  static void decode(snapid_t& o, ceph::buffer::ptr::const_iterator &p) {
+    denc(o.val, p);
+  }
+};
+
+inline std::ostream& operator<<(std::ostream& out, const snapid_t& s) {
+  if (s == CEPH_NOSNAP)
+    return out << "head";
+  else if (s == CEPH_SNAPDIR)
+    return out << "snapdir";
+  else
+    return out << std::hex << s.val << std::dec;
+}
+
+namespace fmt {
+template <>
+struct formatter<snapid_t> {
+
+  constexpr auto parse(format_parse_context& ctx) { return ctx.begin(); }
+
+  template <typename FormatContext>
+  auto format(const snapid_t& snp, FormatContext& ctx) const
+  {
+    if (snp == CEPH_NOSNAP) {
+      return fmt::format_to(ctx.out(), "head");
+    }
+    if (snp == CEPH_SNAPDIR) {
+      return fmt::format_to(ctx.out(), "snapdir");
+    }
+    return fmt::format_to(ctx.out(), FMT_COMPILE("{:x}"), snp.val);
+  }
+};
+} // namespace fmt
+
+struct sobject_t {
+  object_t oid;
+  snapid_t snap;
+
+  sobject_t() : snap(0) {}
+  sobject_t(object_t o, snapid_t s) : oid(o), snap(s) {}
+
+  auto operator<=>(const sobject_t&) const noexcept = default;
+
+  void swap(sobject_t& o) {
+    oid.swap(o.oid);
+    snapid_t t = snap;
+    snap = o.snap;
+    o.snap = t;
+  }
+
+  void encode(ceph::buffer::list& bl) const {
+    using ceph::encode;
+    encode(oid, bl);
+    encode(snap, bl);
+  }
+  void decode(ceph::buffer::list::const_iterator& bl) {
+    using ceph::decode;
+    decode(oid, bl);
+    decode(snap, bl);
+  }
+  void dump(ceph::Formatter *f) const {
+    f->dump_stream("oid") << oid;
+    f->dump_stream("snap") << snap;
+  }
+  static void generate_test_instances(std::list<sobject_t*>& o) {
+    o.push_back(new sobject_t);
+    o.push_back(new sobject_t(object_t("myobject"), 123));
+  }
+};
+WRITE_CLASS_ENCODER(sobject_t)
+
+inline std::ostream& operator<<(std::ostream& out, const sobject_t &o) {
+  return out << o.oid << "/" << o.snap;
+}
+namespace std {
+template<> struct hash<sobject_t> {
+  size_t operator()(const sobject_t &r) const {
+    static hash<object_t> H;
+    static rjhash<uint64_t> I;
+    return H(r.oid) ^ I(r.snap);
+  }
+};
+} // namespace std
+
+#endif
---
+++ src/rgw/rgw_main.h
@@ -0,0 +1,142 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab ft=cpp
+
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2022 Red Hat, Inc
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
+#pragma once
+
+#include <vector>
+#include <map>
+#include <string>
+
+#include "common/async/context_pool.h"
+
+#include "rgw_common.h"
+#include "rgw_rest.h"
+#include "rgw_frontend.h"
+#include "rgw_period_pusher.h"
+#include "rgw_realm_reloader.h"
+#include "rgw_ldap.h"
+#include "rgw_lua.h"
+#include "rgw_dmclock_scheduler_ctx.h"
+#include "rgw_ratelimit.h"
+
+
+class RGWPauser : public RGWRealmReloader::Pauser {
+  std::vector<Pauser*> pausers;
+
+public:
+  ~RGWPauser() override = default;
+
+  void add_pauser(Pauser* pauser) {
+    pausers.push_back(pauser);
+  }
+
+  void pause() override {
+    std::for_each(pausers.begin(), pausers.end(), [](Pauser* p){p->pause();});
+  }
+  void resume(rgw::sal::Driver* driver) override {
+    std::for_each(pausers.begin(), pausers.end(), [driver](Pauser* p){p->resume(driver);});
+  }
+
+};
+
+namespace rgw {
+
+namespace lua { class Background; }
+namespace sal { class ConfigStore; }
+
+class RGWLib;
+class AppMain {
+  /* several components should be initalized only if librgw is
+    * also serving HTTP */
+  bool have_http_frontend{false};
+  bool nfs{false};
+
+  std::vector<RGWFrontend*> fes;
+  std::vector<RGWFrontendConfig*> fe_configs;
+  std::multimap<string, RGWFrontendConfig*> fe_map;
+  std::unique_ptr<rgw::LDAPHelper> ldh;
+  OpsLogSink* olog = nullptr;
+  RGWREST rest;
+  std::unique_ptr<rgw::lua::Background> lua_background;
+  std::unique_ptr<rgw::auth::ImplicitTenants> implicit_tenant_context;
+  std::unique_ptr<rgw::dmclock::SchedulerCtx> sched_ctx;
+  std::unique_ptr<ActiveRateLimiter> ratelimiter;
+  std::map<std::string, std::string> service_map_meta;
+  // wow, realm reloader has a lot of parts
+  std::unique_ptr<RGWRealmReloader> reloader;
+  std::unique_ptr<RGWPeriodPusher> pusher;
+  std::unique_ptr<RGWFrontendPauser> fe_pauser;
+  std::unique_ptr<RGWRealmWatcher> realm_watcher;
+  std::unique_ptr<RGWPauser> rgw_pauser;
+  std::unique_ptr<sal::ConfigStore> cfgstore;
+  SiteConfig site;
+  const DoutPrefixProvider* dpp;
+  RGWProcessEnv env;
+  void need_context_pool();
+  std::optional<ceph::async::io_context_pool> context_pool;
+public:
+  AppMain(const DoutPrefixProvider* dpp);
+  ~AppMain();
+
+  void shutdown(std::function<void(void)> finalize_async_signals
+	       = []() { /* nada */});
+
+  sal::ConfigStore* get_config_store() const {
+    return cfgstore.get();
+  }
+  rgw::sal::Driver* get_driver() {
+    return env.driver;
+  }
+
+  rgw::LDAPHelper* get_ldh() {
+    return ldh.get();
+  }
+
+  void init_frontends1(bool nfs = false);
+  void init_numa();
+  int init_storage();
+  void init_perfcounters();
+  void init_http_clients();
+  void cond_init_apis();
+  void init_ldap();
+  void init_opslog();
+  int init_frontends2(RGWLib* rgwlib = nullptr);
+  void init_tracepoints();
+  void init_lua();
+
+  bool have_http() {
+    return have_http_frontend;
+  }
+
+  static OpsLogFile* ops_log_file;
+}; /* AppMain */
+} // namespace rgw
+
+static inline RGWRESTMgr *set_logging(RGWRESTMgr* mgr)
+{
+  mgr->set_logging(true);
+  return mgr;
+}
+
+static inline RGWRESTMgr *rest_filter(rgw::sal::Driver* driver, int dialect, RGWRESTMgr* orig)
+{
+  RGWSyncModuleInstanceRef sync_module = driver->get_sync_module();
+  if (sync_module) {
+    return sync_module->get_rest_filter(dialect, orig);
+  } else {
+    return orig;
+  }
+}
+
