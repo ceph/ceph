9c65adeb23fbb03b31bb0455b601af2c517baad5
Yite Gu <yitegu0@gmail.com>
2024-08-28 17:48:40 +0800

    blk/KernelDevice: using join() to wait thread end is more safe
    Using join() to wait discard thread end is more safe, it can
    ensure that resource releases are sequential, to avoid potential
    race conditions.
    
    Signed-off-by: Yite Gu <yitegu0@gmail.com>

--- src/blk/kernel/KernelDevice.h
+++ src/blk/kernel/KernelDevice.h
@@ -58,7 +58,6 @@ private:
   aio_callback_t discard_callback;
   void *discard_callback_priv;
   bool aio_stop;
-  bool discard_stop;
   std::unique_ptr<PerfCounters> logger;
 
   ceph::mutex discard_lock = ceph::make_mutex("KernelDevice::discard_lock");
@@ -100,7 +99,7 @@ private:
   int _aio_start();
   void _aio_stop();
 
-  void _discard_update_threads();
+  void _discard_update_threads(bool discard_stop = false);
   void _discard_stop();
   bool _discard_started();
 
