540a5e7448e391631e4dac3e85b0cb4b796e0da8
Radoslaw Zarzynski <rzarzyns@redhat.com>
2024-08-28 19:18:47 +0000

    buffer: copy_shallow() of bl's iterator doesn't take ptr as in/out param
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/include/denc.h
+++ src/include/denc.h
@@ -1703,9 +1703,8 @@ inline std::enable_if_t<traits::supported && !traits::need_contiguous> decode(
     // ceph::buffer::list.  we don't really know how much we'll need here,
     // unfortunately.  hopefully it is already contiguous and we're just
     // bumping the raw ref and initializing the ptr tmp fields.
-    ceph::buffer::ptr tmp;
     auto t = p;
-    t.copy_shallow(remaining, tmp);
+    ceph::buffer::ptr tmp = t.copy_shallow(remaining);
     auto cp = std::cbegin(tmp);
     traits::decode(o, cp);
     p += cp.get_offset();
@@ -1724,9 +1723,8 @@ inline std::enable_if_t<traits::supported && traits::need_contiguous> decode(
   // ceph::buffer::list.  we don't really know how much we'll need here,
   // unfortunately.  hopefully it is already contiguous and we're just
   // bumping the raw ref and initializing the ptr tmp fields.
-  ceph::buffer::ptr tmp;
   auto t = p;
-  t.copy_shallow(p.get_bl().length() - p.get_off(), tmp);
+  ceph::buffer::ptr tmp = t.copy_shallow(p.get_bl().length() - p.get_off());
   auto cp = std::cbegin(tmp);
   traits::decode(o, cp);
   p += cp.get_offset();
@@ -1762,9 +1760,9 @@ inline std::enable_if_t<traits::supported && !traits::featured> decode_nohead(
       size_t element_size = 0;
       typename T::value_type v;
       denc_traits<typename T::value_type>::bound_encode(v, element_size);
-      t.copy_shallow(num * element_size, tmp);
+      tmp = t.copy_shallow(num * element_size);
     } else {
-      t.copy_shallow(p.get_bl().length() - p.get_off(), tmp);
+      tmp = t.copy_shallow(p.get_bl().length() - p.get_off());
     }
     auto cp = std::cbegin(tmp);
     traits::decode_nohead(num, o, cp);
