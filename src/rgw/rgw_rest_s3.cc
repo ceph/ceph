d4941a77dbdbe8915bec28272d5c9edd3d217fda
Marcus Watts <mwatts@redhat.com>
2024-08-28 11:42:33 -0400

    rgw/etags: etags should be quoted in CompleteMultipartUploadResult xml.
    Etags in other parts of ceph are returned as quoted objects (""),
    where they are returned as header fields in the response.
    Comppleting a multipart upload returns the etag in the xml
    output, and it turns out they should be quoted here as well.
    
    There are clients in the field (such as R2) that depend on this behavior.
    
    Fixes: https://tracker.ceph.com/issues/67688
    
    Signed-off-by: Marcus Watts <mwatts@redhat.com>

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -4479,7 +4479,7 @@ void RGWCompleteMultipart_ObjStore_S3::send_response()
     }
     s->formatter->dump_string("Bucket", s->bucket_name);
     s->formatter->dump_string("Key", s->object->get_name());
-    s->formatter->dump_string("ETag", etag);
+    s->formatter->dump_format_unquoted("ETag", "\"%s\"", etag.c_str());
     if (armored_cksum) {
       s->formatter->dump_string(cksum->element_name(), *armored_cksum);
     }
