792a23725fe864eba6b28f51c04037acba6ce00d
Radoslaw Zarzynski <rzarzyns@redhat.com>
2024-08-29 13:33:46 +0000

    os/memstore: rework users of buffer library due to the bptr rw|ro split
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/os/memstore/MemStore.cc
+++ src/os/memstore/MemStore.cc
@@ -1667,7 +1667,7 @@ int MemStore::PageSetObject::read(uint64_t offset, uint64_t len, ceph::buffer::l
   data.get_range(offset, len, tls_pages);
 
   // allocate a buffer for the data
-  ceph::buffer::ptr buf(len);
+  ceph::buffer::ptr_rw buf(len);
 
   auto p = tls_pages.begin();
   while (remaining) {
