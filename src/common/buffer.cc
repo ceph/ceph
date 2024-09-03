29c58c46c3982ae443d1b31feb7fc867a53100c4
Radoslaw Zarzynski <rzarzyns@redhat.com>
2024-09-03 14:38:54 +0000

    buffer, test: bl::append_zero2() deduplicates zeros, introduce bptr::is_zero_fast()
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>
    Signed-off-by: Alex Ainscow <aainscow@uk.ibm.com>

--- src/common/buffer.cc
+++ src/common/buffer.cc
@@ -96,10 +96,9 @@ static ceph::spinlock debug_lock;
       : raw(dataptr, l, mempool) {
     }
 
-    static ceph::unique_leakable_ptr<buffer::raw>
-    create(unsigned len,
-	   unsigned align,
-	   int mempool = mempool::mempool_buffer_anon)
+    static std::pair<char*, size_t> alloc_data_n_controlblock(
+      unsigned len,
+      unsigned align)
     {
       // posix_memalign() requires a multiple of sizeof(void *)
       align = std::max<unsigned>(align, sizeof(void *));
@@ -117,7 +116,15 @@ static ceph::spinlock debug_lock;
 #endif /* DARWIN */
       if (!ptr)
 	throw bad_alloc();
+      return {ptr, datalen};
+    }
 
+    static ceph::unique_leakable_ptr<buffer::raw>
+    create(unsigned len,
+	   unsigned align,
+	   int mempool = mempool::mempool_buffer_anon)
+    {
+      const auto [ptr, datalen] = alloc_data_n_controlblock(len, align);
       // actual data first, since it has presumably larger alignment restriction
       // then put the raw_combined at the end
       return ceph::unique_leakable_ptr<buffer::raw>(
@@ -127,6 +134,26 @@ static ceph::spinlock debug_lock;
     static void operator delete(void *ptr) {
       raw_combined *raw = (raw_combined *)ptr;
       aligned_free((void *)raw->data);
+    }
+  };
+
+  class buffer::raw_zeros : public buffer::raw_combined {
+    raw_zeros(char *dataptr, unsigned l, int mempool)
+      : raw_combined(dataptr, l, mempool) {
+      memset(dataptr, 0, l);
+    }
+
+    static constexpr unsigned ZERO_AREA_NUM_PAGES = 4;
+
+  public:
+    static ceph::unique_leakable_ptr<buffer::raw>
+    create(int mempool = mempool::mempool_buffer_anon)
+    {
+      const auto ZERO_AREA_SIZE = ZERO_AREA_NUM_PAGES * CEPH_PAGE_SIZE;
+      const auto [ptr, datalen] = alloc_data_n_controlblock(
+        ZERO_AREA_SIZE, /* align to */CEPH_PAGE_SIZE);
+      return ceph::unique_leakable_ptr<buffer::raw>(
+	new (ptr + datalen) raw_zeros(ptr, ZERO_AREA_SIZE, mempool));
     }
   };
 
@@ -521,9 +548,14 @@ static ceph::spinlock debug_lock;
     return 0;
   }
 
+  bool buffer::ptr::is_zero_fast() const
+  {
+    return dynamic_cast<const buffer::raw_zeros*>(_raw) != nullptr;
+  }
+
   bool buffer::ptr::is_zero() const
   {
-    return mem_is_zero(c_str(), _len);
+    return /*is_zero_fast() ||*/ mem_is_zero(c_str(), _len);
   }
 
   unsigned buffer::ptr::append(char c)
@@ -1292,6 +1324,14 @@ static ceph::spinlock debug_lock;
     return _buffers.back();
   }
 
+  buffer::ptr buffer::list::always_zeroed_bptr() {
+    // See https://en.cppreference.com/w/cpp/language/storage_duration.html
+    // Section on static block variables states that since C++11 this is
+    // lazily evaluated and thread safe.
+    static ptr always_zeroed_bptr = raw_zeros::create();
+    return always_zeroed_bptr;
+  }
+
   void buffer::list::append(const char *data, unsigned len)
   {
     _len += len;
@@ -1427,6 +1467,18 @@ static ceph::spinlock debug_lock;
     _buffers.push_front(*bp.release());
   }
   
+  void buffer::list::append_zero2(unsigned len)
+  {
+    _len += len;
+    while (len > 0) {
+      const auto round_size = std::min(len, always_zeroed_bptr().length());
+      auto bptr = ptr_node::create(always_zeroed_bptr(), 0, round_size);
+      _buffers.push_back(*bptr.release());
+      _num += 1;
+      len -= round_size;
+    }
+  }
+
   void buffer::list::append_zero(unsigned len)
   {
     _len += len;
