5dd2b8cc856d8adf9a33670dac93bc05cdf0b3dc
Radoslaw Zarzynski <rzarzyns@redhat.com>
2024-09-03 14:38:54 +0000

    buffer: bl::append_zero() deduplicates zeros
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/include/buffer.h
+++ src/include/buffer.h
@@ -132,6 +132,7 @@ struct error_code;
   class raw_claimed_char;
   class raw_unshareable; // diagnostic, unshareable char buffer
   class raw_combined;
+  class raw_zeros;
   class raw_claim_buffer;
 
 
@@ -988,6 +989,8 @@ struct error_code;
     ptr& get_append_buffer() {
       return *_carriage;
     }
+
+    static ptr always_zeroed_bptr;
 
   public:
     // cons/des
