98a922150b23588522ec5017527ba896a12ffdd4
Alexander H Ainscow <aainscow@uk.ibm.com>
2024-09-06 20:04:10 +0100

    interval_map: Add "contains" for interval_map.const_iterator.
    This method returns true if the interval entirely contains the specified range, without gaps.
    
    Signed-off-by: Alexander H Ainscow <aainscow@uk.ibm.com>

--- src/common/interval_map.h
+++ src/common/interval_map.h
@@ -245,6 +245,11 @@ public:
     const_iterator &operator*() {
       return *this;
     }
+    const bool contains(K _off, K _len) const {
+      K off = get_off();
+      K len = get_len();
+      return off <= _off && off + len >= _off + _len;
+    }
   };
   const_iterator begin() const {
     return const_iterator(m.begin());
