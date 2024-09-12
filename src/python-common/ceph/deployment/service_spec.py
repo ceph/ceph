64b1811ce8492f9ecbab9b066e9c788ae172d6ea
Alexander Indenbaum <aindenba@redhat.com>
2024-09-12 09:26:34 +0000

    pybind/mgr/cephadm/services/nvmeof.py: allow setting '0.0.0.0' as address in the spec file
    - Partial revert of https://github.com/ceph/ceph/pull/59738/commits/9eb3b99b99b0b8c6c4e5f75e59c2e4835a9d6796
    - Part of https://github.com/ceph/ceph/pull/59738
    
    (cherry picked from commit 62a4247353dd85417303995cda7e9b8ffae1e929)
    
    python-common/ceph/deployment/service_spec.py: Allow the cephadm deployment to determine the default addresses
    
    Signed-off-by: Alexander Indenbaum <aindenba@redhat.com>
    (cherry picked from commit 0997e4cd51edfe954148bfb8b7f42be9205c237c)
    
    Resolves: rhbz#2311996
    (cherry picked from commit 2db75595f1c44f5fb973a612a08406b5630ad1e5)

--- src/python-common/ceph/deployment/service_spec.py
+++ src/python-common/ceph/deployment/service_spec.py
@@ -1377,7 +1377,7 @@ class NvmeofServiceSpec(ServiceSpec):
         #: RADOS pool where ceph-nvmeof config data is stored.
         self.pool = pool
         #: ``addr`` address of the nvmeof gateway
-        self.addr = addr or "0.0.0.0"
+        self.addr = addr
         #: ``port`` port of the nvmeof gateway
         self.port = port or 5500
         #: ``name`` name of the nvmeof gateway
