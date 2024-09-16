33e3f8d6362075d0f58c5ee5ff6022c7ff2f0434
Patrick Donnelly <pdonnell@redhat.com>
2024-09-16 14:33:15 -0400

    arch/s390x: add license notices, use LGPL
    These are original code files introduced by Aliaksei Makarau for s390x
    enablement. With his permission, we've licensed these under LGPL.
    
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- src/arch/s390x.h
+++ src/arch/s390x.h
@@ -1,3 +1,15 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright 2024 IBM Corporation
+ *
+ * This is free software; you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License version 2.1, as published by
+ * the Free Software Foundation.  See file COPYING.
+ */
+
 #ifndef CEPH_ARCH_S390X_H
 #define CEPH_ARCH_S390X_H
 
