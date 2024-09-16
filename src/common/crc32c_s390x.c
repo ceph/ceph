95cdae7f39bf173894defa895d7e7c9a2b112dc3
Patrick Donnelly <pdonnell@redhat.com>
2024-09-16 14:30:31 -0400

    COPYING,common/*s390x*: distribute under Apache 2 license for s390x
    The Ceph project cannot distributed GPL code as it is incompatible with the
    LGPL that all Ceph code is licensed under. Since s390x is dual-licensed, we
    choose to use the Apache 2 license.
    
    Fixes: https://tracker.ceph.com/issues/68084
    Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

--- src/common/crc32c_s390x.c
+++ src/common/crc32c_s390x.c
@@ -1,14 +1,29 @@
 /*
- * CRC-32 algorithm implemented with the z/Architecture
- * Vector Extension Facility.
+ * CRC-32 algorithm implemented with the z/Architecture Vector Extension
+ * Facility.
  *
- * Copyright IBM Corp. 2024
+ * Copyright 2024 IBM Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ *
  * Author(s): Hendrik Brueckner <brueckner@linux.vnet.ibm.com>
  *            Anton Blanchard <anton@au.ibm.com>
  *            Bryan Chan <bryan.chan@ca.ibm.com>
  *            Chris Zou <chriszou@ca.ibm.com>
  *            Aliaksei Makarau <aliaksei.makarau@ibm.com>
  */
+
 #include <sys/types.h>
 #include <endian.h>
 #include "crc32c_s390x.h"
