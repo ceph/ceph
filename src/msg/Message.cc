8010c9a543dc6a00599c1fd6f3534f63910049ff
Radoslaw Zarzynski <rzarzyns@redhat.com>
2024-09-17 12:31:16 +0000

    msg: wire up passing of feature bits to decoders
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/msg/Message.cc
+++ src/msg/Message.cc
@@ -319,7 +319,8 @@ Message *decode_message(CephContext *cct,
                         ceph::bufferlist&& front,
                         ceph::bufferlist&& middle,
                         const ceph::bufferlist& data,
-                        Message::ConnectionRef conn)
+                        Message::ConnectionRef conn,
+                        uint64_t features)
 {
 #ifdef WITH_SEASTAR
   // In crimson, conn is independently maintained outside Message.
@@ -1000,7 +1001,7 @@ Message *decode_message(CephContext *cct,
   m->set_data(data);
 
   try {
-    m->decode_payload(0);
+    m->decode_payload(features);
   }
   catch (const ceph::buffer::error &e) {
     if (cct) {
@@ -1116,5 +1117,6 @@ Message *decode_message(CephContext *cct, int crcflags, ceph::bufferlist::const_
   decode(fr, p);
   decode(mi, p);
   decode(da, p);
-  return decode_message(cct, crcflags, h, f, std::move(fr), std::move(mi), std::move(da), nullptr);
+  return decode_message(
+    cct, crcflags, h, f, std::move(fr), std::move(mi), std::move(da), nullptr, 0);
 }
