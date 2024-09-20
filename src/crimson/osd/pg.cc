7a826eb86c423e895345557632091a934f7c7d7e
Samuel Just <sjust@redhat.com>
2024-09-19 19:39:08 -0700

    crimson: PG::submit_error_log returns eversion_t rather than optional
    It seems like the motivation here was to allow do_osd_ops_execute to
    communicate that it didn't submit an error log by making
    maybe_submit_error_log a std::optional<eversion_t>.  However,
    submit_error_log itself always returns a version.  Fix submit_error_log
    and compensate in do_osd_ops_execute.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/osd/pg.cc
+++ src/crimson/osd/pg.cc
@@ -1122,7 +1122,11 @@ PG::do_osd_ops_execute(
     if constexpr (!std::is_same_v<Ret, void>) {
       if(op_info.may_write()) {
         maybe_submit_error_log =
-          submit_error_log(m, op_info, obc, e, rep_tid);
+          submit_error_log(
+	    m, op_info, obc, e, rep_tid
+	  ).then_interruptible([](auto &&e) {
+	    return std::make_optional<eversion_t>(std::move(e));
+	  });
       }
     }
     return maybe_submit_error_log.then_interruptible(
@@ -1175,7 +1179,7 @@ PG::interruptible_future<> PG::complete_error_log(const ceph_tid_t& rep_tid,
   return result;
 }
 
-PG::interruptible_future<std::optional<eversion_t>> PG::submit_error_log(
+PG::interruptible_future<eversion_t> PG::submit_error_log(
   Ref<MOSDOp> m,
   const OpInfo &op_info,
   ObjectContextRef obc,
@@ -1241,7 +1245,7 @@ PG::interruptible_future<std::optional<eversion_t>> PG::submit_error_log(
         get_collection_ref(), std::move(t)
       ).then([this] {
         peering_state.update_trim_to();
-        return seastar::make_ready_future<std::optional<eversion_t>>(projected_last_update);
+        return seastar::make_ready_future<eversion_t>(projected_last_update);
       });
     });
   });
