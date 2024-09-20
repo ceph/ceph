14778bb58a582f2cd2281986a0262c07c7de114e
Marcel Lauhoff <marcel.lauhoff@clyso.com>
2024-09-20 17:39:50 +0200

    msg: Add dump() to Protocol{V1,V2}
    Add dump() methods to the async messenger protocol layer. Export cipher
    and compressor name from the respective handlers to be able to export
    a human understandable information here.
    
    Signed-off-by: Marcel Lauhoff <marcel.lauhoff@clyso.com>

--- src/msg/async/crypto_onwire.h
+++ src/msg/async/crypto_onwire.h
@@ -86,6 +86,8 @@ struct TxHandler {
   // Generates authentication signature and returns bufferlist crafted
   // basing on plaintext from preceding call to _update().
   virtual ceph::bufferlist authenticated_encrypt_final() = 0;
+
+  virtual std::string_view cipher_name() const = 0;
 };
 
 class RxHandler {
@@ -109,6 +111,8 @@ public:
   // for overall decryption sequence.
   // Throws on integrity/authenticity checks
   virtual void authenticated_decrypt_update_final(ceph::bufferlist& bl) = 0;
+
+  virtual std::string_view cipher_name() const = 0;
 };
 
 struct rxtx_t {
