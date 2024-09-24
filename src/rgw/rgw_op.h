43a6f123e24a772cc48a3a045d2136e13355c2a5
Shilpa Jagannath <smanjara@redhat.com>
2024-09-24 17:12:02 -0400

    rgw/multisite: the create_bucket forward request omits the the request body, thus missing some data if specified inside CreateBucketConfiguration xml on the non-master zone. also, now that we perform cksum validation against empty payloads, such a request would fail with -ERR_AMZ_CONTENT_SHA256_MISMATCH due to a zero content-length but a non-empty payload hash. this fix ensures that request body is forwarded during create_bucket
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>

--- src/rgw/rgw_op.h
+++ src/rgw/rgw_op.h
@@ -1117,6 +1117,7 @@ class RGWCreateBucket : public RGWOp {
   bool relaxed_region_enforcement = false;
   RGWCORSConfiguration cors_config;
   std::set<std::string> rmattr_names;
+  bufferlist in_data;
 
   virtual bool need_metadata_upload() const { return false; }
 
