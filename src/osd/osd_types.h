c1fdc53bc7b5c472cd734ed4fe6b98fd3cf5af63
Max Kellermann <max.kellermann@ionos.com>
2024-10-04 15:10:51 +0200

    osd/osd_types: use std::map::insert_or_assign()
    This eliminates the overhead for constructing an empty value if the
    key does not yet exist.
    
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/osd/osd_types.h
+++ src/osd/osd_types.h
@@ -1153,7 +1153,7 @@ public:
   template<typename T>
   void set(key_t key, const T &val) {
     value_t value = val;
-    opts[key] = value;
+    opts.insert_or_assign(key, value);
   }
 
   template<typename T>
