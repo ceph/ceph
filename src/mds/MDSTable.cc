64692f08b49da2d3b61fe1ac4be4f55265a542fc
Max Kellermann <max.kellermann@ionos.com>
2024-10-04 22:27:52 +0200

    mds/MDSTable: include cleanup
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/mds/MDSTable.cc
+++ src/mds/MDSTable.cc
@@ -13,16 +13,12 @@
  */
 
 #include "MDSTable.h"
-
 #include "MDSRank.h"
-#include "MDLog.h"
 
-#include "osdc/Filer.h"
+#include "osdc/Objecter.h"
 
-#include "include/types.h"
-
-#include "common/config.h"
-#include "common/errno.h"
+#include "common/debug.h"
+#include "common/errno.h" // for cpp_strerror()
 #include "common/Finisher.h"
 
 #include "include/ceph_assert.h"
