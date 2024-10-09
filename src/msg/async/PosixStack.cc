bf6d6e3248dd80cc83c4b7cdd70f01bb2ff47d6c
Max Kellermann <max.kellermann@ionos.com>
2024-10-09 20:14:40 +0200

    msg/async/Stack: add (optional) virtual method readv()
    This wraps the recvmsg() system call where it is available, with a
    default implementation that falls back to read().  This new method
    will allow some new optimizations.
    
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/msg/async/PosixStack.cc
+++ src/msg/async/PosixStack.cc
@@ -70,6 +70,33 @@ class PosixConnectedSocketImpl final : public ConnectedSocketImpl {
     return r;
   }
 
+#ifndef _WIN32
+  ssize_t readv(std::span<const std::span<char>> dest) override {
+#ifndef IOV_MAX
+    static constexpr std::size_t IOV_MAX = 1024;
+#endif
+
+    std::array<struct iovec, IOV_MAX> iov;
+
+    if (dest.size() > iov.size())
+      dest = dest.first(iov.size());
+
+    std::transform(dest.begin(), dest.end(), iov.begin(), [](auto i) -> struct iovec {
+      return {
+	.iov_base = i.data(),
+	.iov_len = i.size(),
+      };
+    });
+
+    struct msghdr msg{
+      .msg_iov = iov.data(),
+      .msg_iovlen = dest.size(),
+    };
+
+    return recvmsg(_fd, &msg, 0);
+  }
+#endif
+
   // return the sent length
   // < 0 means error occurred
   #ifndef _WIN32
