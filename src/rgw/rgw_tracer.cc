ee5d1548105c57d6d4415d20604e3a45efcb6229
Casey Bodley <cbodley@redhat.com>
2024-10-14 14:44:19 -0400

    common/async: use asio::associator for Forwarding/CompletionHandler
    asio keeps adding new types of associations. since this was written,
    get_associated_cancellation_slot() and
    get_associated_immediate_executor() were added. we can now specialize
    the asio::associator<> template to forward all of them
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

---
+++ src/rgw/rgw_tracer.cc
@@ -0,0 +1,12 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+
+#include "rgw_tracer.h"
+
+namespace tracing {
+namespace rgw {
+
+tracing::Tracer tracer;
+
+} // namespace rgw
+} // namespace tracing
