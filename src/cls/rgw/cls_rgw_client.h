c6a98b2e33f44c4233b18136521112d64235efff
Casey Bodley <cbodley@redhat.com>
2024-10-15 10:05:37 -0400

    cls/rgw: remove unused 'oid' from cls_rgw_bi_put()
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/cls/rgw/cls_rgw_client.h
+++ src/cls/rgw/cls_rgw_client.h
@@ -385,7 +385,7 @@ int cls_rgw_bi_get(librados::IoCtx& io_ctx, const std::string oid,
                    BIIndexType index_type, const cls_rgw_obj_key& key,
                    rgw_cls_bi_entry *entry);
 int cls_rgw_bi_put(librados::IoCtx& io_ctx, const std::string oid, const rgw_cls_bi_entry& entry);
-void cls_rgw_bi_put(librados::ObjectWriteOperation& op, const std::string oid, const rgw_cls_bi_entry& entry);
+void cls_rgw_bi_put(librados::ObjectWriteOperation& op, const rgw_cls_bi_entry& entry);
 // Write the given array of index entries and update bucket stats accordingly.
 // If existing entries may be overwritten, pass check_existing=true to decrement
 // their stats first.
