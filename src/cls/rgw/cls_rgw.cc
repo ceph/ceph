bc8034438cf9c40433888b4edd95c3be18c90930
Radoslaw Zarzynski <rzarzyns@redhat.com>
2024-10-17 14:33:33 +0000

    cls/rgw, rgw: validate in compile-time users of rgw_cls_list_op
    This is just to facilitate review; the commit is not supposed
    to be merged.
    
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/cls/rgw/cls_rgw.cc
+++ src/cls/rgw/cls_rgw.cc
@@ -537,7 +537,7 @@ int rgw_bucket_list(cls_method_context_t hctx, bufferlist *in, bufferlist *out)
 
   auto iter = in->cbegin();
 
-  rgw_cls_list_op op;
+  rgw_cls_list_op op(false);
   try {
     decode(op, iter);
   } catch (ceph::buffer::error& err) {
