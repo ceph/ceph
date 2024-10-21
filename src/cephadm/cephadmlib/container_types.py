e1b54a39e3510b6ab9a0a0c8aa16987b5f611790
Shweta Bhosale <Shweta.Bhosale1@ibm.com>
2024-10-21 10:42:24 +0530

    cephadm: Modify the structure of the default container images
    Fixes: https://tracker.ceph.com/issues/68605?next_issue_id=68599
    
    Signed-off-by: Shweta Bhosale <Shweta.Bhosale1@ibm.com>

--- src/cephadm/cephadmlib/container_types.py
+++ src/cephadm/cephadmlib/container_types.py
@@ -9,7 +9,7 @@ from typing import Dict, List, Optional, Any, Union, Tuple, Iterable, cast
 
 from .call_wrappers import call, call_throws, CallVerbosity
 from .constants import DEFAULT_TIMEOUT
-import ceph.cephadm.images as default_images
+from ceph.cephadm.images import DefaultImages
 from .container_engines import Docker, Podman
 from .context import CephadmContext
 from .daemon_identity import DaemonIdentity, DaemonSubIdentity
@@ -665,14 +665,8 @@ def enable_shared_namespaces(
 
 def get_mgr_images() -> dict:
     """Return dict of default mgr images"""
-    mgr_prefix = 'mgr/cephadm/container_image_'
-    mgr_images = {}
-    images = vars(default_images)
-    for key, value in images.items():
-        if key.startswith('DEFAULT_') and key.endswith('_IMAGE'):
-            # flake8 and black disagree about spaces around ":" hence the noqa comment
-            suffix = key[
-                len('DEFAULT_') : -len('_IMAGE')  # noqa: E203
-            ].lower()
-            mgr_images[mgr_prefix + suffix] = value
+    mgr_prefix = 'mgr/cephadm/'
+    mgr_images = {
+        f'{mgr_prefix}{image.key}': image.image_ref for image in DefaultImages
+    }
     return mgr_images
--- src/python-common/ceph/cephadm/images.py
+++ src/python-common/ceph/cephadm/images.py
@@ -1,19 +1,57 @@
 # Default container images -----------------------------------------------------
-DEFAULT_PROMETHEUS_IMAGE = 'quay.io/prometheus/prometheus:v2.51.0'
-DEFAULT_NODE_EXPORTER_IMAGE = 'quay.io/prometheus/node-exporter:v1.7.0'
-DEFAULT_NVMEOF_IMAGE = 'quay.io/ceph/nvmeof:1.3.3'
-DEFAULT_LOKI_IMAGE = 'quay.io/ceph/loki:3.0.0'
-DEFAULT_PROMTAIL_IMAGE = 'quay.io/ceph/promtail:3.0.0'
-DEFAULT_ALERTMANAGER_IMAGE = 'quay.io/prometheus/alertmanager:v0.27.0'
-DEFAULT_GRAFANA_IMAGE = 'quay.io/ceph/grafana:10.4.8'
-DEFAULT_HAPROXY_IMAGE = 'quay.io/ceph/haproxy:2.3'
-DEFAULT_KEEPALIVED_IMAGE = 'quay.io/ceph/keepalived:2.2.4'
-DEFAULT_SNMP_GATEWAY_IMAGE = 'quay.io/ceph/snmp-notifier:v1.2.1'
-DEFAULT_ELASTICSEARCH_IMAGE = 'quay.io/omrizeneva/elasticsearch:6.8.23'
-DEFAULT_JAEGER_COLLECTOR_IMAGE = 'quay.io/jaegertracing/jaeger-collector:1.29'
-DEFAULT_JAEGER_AGENT_IMAGE = 'quay.io/jaegertracing/jaeger-agent:1.29'
-DEFAULT_NGINX_IMAGE = 'quay.io/ceph/nginx:sclorg-nginx-126'
-DEFAULT_OAUTH2_PROXY_IMAGE = 'quay.io/oauth2-proxy/oauth2-proxy:v7.6.0'
-DEFAULT_JAEGER_QUERY_IMAGE = 'quay.io/jaegertracing/jaeger-query:1.29'
-DEFAULT_SAMBA_IMAGE = 'quay.io/samba.org/samba-server:devbuilds-centos-amd64'
-DEFAULT_SAMBA_METRICS_IMAGE = 'quay.io/samba.org/samba-metrics:latest'
+
+from typing import NamedTuple
+from enum import Enum
+
+
+class ContainerImage(NamedTuple):
+    image_ref: str  # reference to default container image
+    key: str  # image key
+    desc: str  # description of image
+
+    def __repr__(self) -> str:
+        return self.image_ref
+
+
+def _create_image(image_ref: str, key: str) -> ContainerImage:
+    _img_prefix = 'container_image_'
+    description = key.replace('_', ' ').capitalize()
+    return ContainerImage(
+        image_ref,
+        f'{_img_prefix}{key}',
+        f'{description} container image'
+    )
+
+
+class DefaultImages(Enum):
+    PROMETHEUS = _create_image('quay.io/prometheus/prometheus:v2.51.0', 'prometheus')
+    LOKI = _create_image('docker.io/grafana/loki:3.0.0', 'loki')
+    PROMTAIL = _create_image('docker.io/grafana/promtail:3.0.0', 'promtail')
+    NODE_EXPORTER = _create_image('quay.io/prometheus/node-exporter:v1.7.0', 'node_exporter')
+    ALERTMANAGER = _create_image('quay.io/prometheus/alertmanager:v0.27.0', 'alertmanager')
+    GRAFANA = _create_image('quay.io/ceph/grafana:10.4.8', 'grafana')
+    HAPROXY = _create_image('quay.io/ceph/haproxy:2.3', 'haproxy')
+    KEEPALIVED = _create_image('quay.io/ceph/keepalived:2.2.4', 'keepalived')
+    NVMEOF = _create_image('quay.io/ceph/nvmeof:1.2.17', 'nvmeof')
+    SNMP_GATEWAY = _create_image('docker.io/maxwo/snmp-notifier:v1.2.1', 'snmp_gateway')
+    ELASTICSEARCH = _create_image('quay.io/omrizeneva/elasticsearch:6.8.23', 'elasticsearch')
+    JAEGER_COLLECTOR = _create_image('quay.io/jaegertracing/jaeger-collector:1.29',
+                                     'jaeger_collector')
+    JAEGER_AGENT = _create_image('quay.io/jaegertracing/jaeger-agent:1.29', 'jaeger_agent')
+    JAEGER_QUERY = _create_image('quay.io/jaegertracing/jaeger-query:1.29', 'jaeger_query')
+    SAMBA = _create_image('quay.io/samba.org/samba-server:devbuilds-centos-amd64', 'samba')
+    SAMBA_METRICS = _create_image('quay.io/samba.org/samba-metrics:latest', 'samba_metrics')
+    NGINX = _create_image('quay.io/ceph/nginx:sclorg-nginx-126', 'nginx')
+    OAUTH2_PROXY = _create_image('quay.io/oauth2-proxy/oauth2-proxy:v7.6.0', 'oauth2_proxy')
+
+    @property
+    def image_ref(self) -> str:
+        return self.value.image_ref
+
+    @property
+    def key(self) -> str:
+        return self.value.key
+
+    @property
+    def desc(self) -> str:
+        return self.value.desc
