b4c53fc36f37660f909e3dd16b1747d993406948
Naman Munet <namanmunet@li-ff83bccc-26af-11b2-a85c-a4b04bfb1003.ibm.com>
2024-10-21 22:25:41 +0530

    mgr/dashboard: replace usage or progress bar with carbon progress bar
    Fixes: https://tracker.ceph.com/issues/68258
    
    Changes affect the following files:
    - rbd-list.component.html
    - cephfs-detail.component.html
    - cephfs-subvolume-group.component.html
    - cephfs-subvolume-list.componenet.html
    - multi-cluster.component.html
    - osd-list.component.html
    - service-daemon-list.component.html
    - pool-list.component.html
    - rgw-bucket-list.component.html
    - rgw-user-list.component.html
    
    Signed-off-by: Naman Munet <namanmunet@li-ff83bccc-26af-11b2-a85c-a4b04bfb1003.ibm.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/usage-bar/usage-bar.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/usage-bar/usage-bar.component.ts
@@ -1,53 +1,62 @@
-import { Component, Input, OnChanges } from '@angular/core';
-
-import _ from 'lodash';
+import { Component, Input, ElementRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cd-usage-bar',
   templateUrl: './usage-bar.component.html',
   styleUrls: ['./usage-bar.component.scss']
 })
-export class UsageBarComponent implements OnChanges {
-  @Input()
-  total: number;
-  @Input()
-  used: any;
-  @Input()
-  warningThreshold?: number;
-  @Input()
-  errorThreshold?: number;
-  @Input()
-  isBinary = true;
-  @Input()
-  decimals = 0;
-  @Input()
-  calculatePerc = true;
-  @Input()
-  title = $localize`usage`;
-  @Input()
-  customLegend?: string;
-  @Input()
-  customLegendValue?: string;
-  @Input()
-  showFreeToolTip = true;
-  @Input()
-  showMultisiteTooltip = false;
+export class UsageBarComponent implements OnInit {
+  @Input() total: number = 0;
+  @Input() used: number = 0;
+  @Input() warningThreshold?: number = 90;
+  @Input() errorThreshold?: number = 95;
+  @Input() isBinary = true;
+  @Input() decimals: number = 0;
+  @Input() title = $localize`usage`;
+  data: { group: string; value: number }[] = [{ group: $localize`Capacity`, value: 0 }];
+  options;
 
-  usedPercentage: number;
-  freePercentage: number;
-
-  ngOnChanges() {
-    if (this.calculatePerc) {
-      this.usedPercentage = this.total > 0 ? (this.used / this.total) * 100 : 0;
-      this.freePercentage = 100 - this.usedPercentage;
-    } else {
-      if (this.used) {
-        this.used = this.used.slice(0, -1);
-        this.usedPercentage = Number(this.used);
-        this.freePercentage = 100 - this.usedPercentage;
-      } else {
-        this.usedPercentage = 0;
+  constructor(private elementRef: ElementRef) {
+    this.options = {
+      resizable: false,
+      meter: {
+        showLabels: false
+      },
+      tooltip: {
+        enabled: true
+      },
+      color: {
+        scale: {
+          Capacity: this.getCssVariableValue('cds-support-info')
+        }
+      },
+      height: '1rem',
+      width: '12rem',
+      toolbar: {
+        enabled: false
       }
+    };
+  }
+
+  ngOnInit() {
+    let color = 'cds-support-info'; // Default color for 'Capacity'
+    const usedPercentage = this.calculateUsedPercentage(this.used, this.total);
+    if (this.warningThreshold >= 0 && usedPercentage >= this.warningThreshold) {
+      color = 'cds-support-warning'; // Warning threshold exceeded
     }
+    if (this.errorThreshold >= 0 && usedPercentage >= this.errorThreshold) {
+      color = 'cds-support-error'; // Error threshold exceeded
+    }
+    this.data[0].value = this.total > 0 ? (this.used / this.total) * 100 : 0;
+    this.options.color.scale.Capacity = this.getCssVariableValue(color);
+  }
+
+  getCssVariableValue(variableName: string): string {
+    const nativeElement = this.elementRef.nativeElement;
+    return getComputedStyle(nativeElement).getPropertyValue(`--${variableName}`);
+  }
+
+  calculateUsedPercentage(used: number, total: number): number {
+    return total > 0 ? used / total : 0;
   }
 }
