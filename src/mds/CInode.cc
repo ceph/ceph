88e16394186134a788ba622ab90b89a7d213b113
Patrick Donnelly <pdonnell@ibm.com>
2024-10-23 14:00:35 -0400

    mds: encode optmetadata in InodeStat sent to clients
    Deliberately do not dump the entire struct. Some metadata may not be
    appropriate to share to clients.
    
    Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>
    Fixes: https://tracker.ceph.com/issues/66373
    (cherry picked from commit 6c891f96aaef5be7bc90b166d5a728516370349b)
    
    Conflicts:
    	src/mds/CInode.cc: no tuple encoding

--- src/mds/CInode.cc
+++ src/mds/CInode.cc
@@ -3938,7 +3938,24 @@ int CInode::encode_inodestat(bufferlist& bl, Session *session,
   } else {
     xattr_version = 0;
   }
-  
+
+  bufferlist optmdbl;
+  {
+    decltype(InodeStat::optmetadata) optmetadata;
+    using kind_t = decltype(optmetadata)::optkind_t;
+
+    auto* csp = get_charmap();
+    if (csp) {
+      dout(25) << *csp << dendl;
+      auto& opt = optmetadata.get_or_create_opt(kind_t::CHARMAP);
+      auto& cs = opt.template get_meta< charmap_md_t >();
+      cs = *csp;
+      dout(25) << "cs now " << cs << dendl;
+    }
+
+    encode(optmetadata, optmdbl);
+  }
+
   // do we have room?
   if (max_bytes) {
     unsigned bytes =
@@ -3952,6 +3969,7 @@ int CInode::encode_inodestat(bufferlist& bl, Session *session,
       sizeof(struct ceph_dir_layout) // dir_layout
       + 4 + file_i->fscrypt_auth.size() // len + data
       + 4 + file_i->fscrypt_file.size() // len + data
+      + optmdbl.length()
       ;
 
     if (xattr_version) {
@@ -4106,7 +4124,7 @@ int CInode::encode_inodestat(bufferlist& bl, Session *session,
    * note: encoding matches MClientReply::InodeStat
    */
   if (session->info.has_feature(CEPHFS_FEATURE_REPLY_ENCODING)) {
-    ENCODE_START(7, 1, bl);
+    ENCODE_START(8, 1, bl);
     encode(oi->ino, bl);
     encode(snapid, bl);
     encode(oi->rdev, bl);
@@ -4154,6 +4172,8 @@ int CInode::encode_inodestat(bufferlist& bl, Session *session,
     encode(!file_i->fscrypt_auth.empty(), bl);
     encode(file_i->fscrypt_auth, bl);
     encode(file_i->fscrypt_file, bl);
+    encode_nohead(optmdbl, bl);
+    // encode inodestat
     ENCODE_FINISH(bl);
   }
   else {
