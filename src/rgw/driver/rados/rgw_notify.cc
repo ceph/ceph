7de5af59c2741b4203cc17f619ca53096c97b8ff
Patrick Donnelly <pdonnell@ibm.com>
2024-10-23 23:16:03 -0400

    common: cache pthread names
    This provides common ceph entrypoints for the pthread_[gs]name functions which
    will also cache a thread_local copy. This also removes the pthread_t parameter
    which precipitated the bug i50743.
    
    Obviously, the overall goal here is to avoid system calls.
    
    See-also: https://tracker.ceph.com/issues/50743
    Fixes: 0be8d01c9ddde0d7d24edd34dc75f6cfc861b5ba
    Fixes: https://tracker.ceph.com/issues/68691
    Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>

--- src/rgw/driver/rados/rgw_notify.cc
+++ src/rgw/driver/rados/rgw_notify.cc
@@ -21,6 +21,7 @@
 #include "common/dout.h"
 #include "rgw_url.h"
 #include <chrono>
+#include <fmt/format.h>
 
 #define dout_subsys ceph_subsys_rgw_notification
 
@@ -769,9 +770,10 @@ public:
         });
 
     // start the worker threads to do the actual queue processing
-    const std::string WORKER_THREAD_NAME = "notif-worker";
     for (auto worker_id = 0U; worker_id < worker_count; ++worker_id) {
-      workers.emplace_back([this]() {
+      workers.emplace_back([this,worker_id]() {
+        const auto thread_name = fmt::format("notif-worker-{}", worker_id);
+        ceph_pthread_setname(thread_name.c_str());
         try {
           io_context.run(); 
         } catch (const std::exception& err) {
@@ -779,11 +781,6 @@ public:
           throw err;
         }
       });
-      const auto thread_name = WORKER_THREAD_NAME+std::to_string(worker_id);
-      if (const auto rc = ceph_pthread_setname(workers.back().native_handle(), thread_name.c_str()); rc != 0) {
-        ldpp_dout(this, 1) << "ERROR: failed to set notification manager thread name to: " << thread_name
-          << ". error: " << rc << dendl;
-      }
     }
     ldpp_dout(this, 10) << "INfO: started notification manager with: " << worker_count << " workers" << dendl;
   }
