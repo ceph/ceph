bebd657500d7fc8846ca431427f7078ab5e8e5cf
Samuel Just <sjust@redhat.com>
2024-10-24 16:39:18 -0700

    crimson: manage log submission atomicity independently of pipeline stages
    It's important to construct and submit log entries atomically because
    the submission order needs to match the entry numbering.  Previously,
    this occurred under the pg-wide exclusive process stage.  Shortly, each
    obc will have its own process stage, so we need to ensure that atomicity
    seperately from the pipeline stages.  Introduce a simple mutex.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/osd/pg.h
+++ src/crimson/osd/pg.h
@@ -664,6 +664,7 @@ private:
     const OpInfo &op_info,
     std::vector<OSDOp>& ops);
 
+  seastar::shared_mutex submit_lock;
   using submit_executer_ret = std::tuple<
     interruptible_future<>,
     interruptible_future<>>;
