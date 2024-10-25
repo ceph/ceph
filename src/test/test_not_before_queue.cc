89356d4778428c08f8d73cb3ecfddd1e0fa75097
Max Kellermann <max.kellermann@ionos.com>
2024-10-25 06:58:39 +0200

    include/types.h: move iostream functions to container_ios.h
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/test/test_not_before_queue.cc
+++ src/test/test_not_before_queue.cc
@@ -5,6 +5,7 @@
 #include <string_view>
 
 #include "common/not_before_queue.h"
+#include "include/container_ios.h"
 #include "gtest/gtest.h"
 
 // Just to have a default constructor that sets it to 0
