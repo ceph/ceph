7fe6d8470c6a6aebd38ee3f223546fa2c56d9ccc
Pritha Srivastava <prsrivas@redhat.com>
2024-10-25 10:26:05 +0530

    rgw/d4n: squashing following commits to address concurrency in filter driver and ssd backed cache.
    1. rgw/d4n: addressing concurrency issues by adding a refcount
    to each block.
    Blocks with positive refcounts are pinned (similar to dirty blocks)
    and not eligible for eviction or deletion. Updating unit tests also.
    2. rgw/cache: addressing concurrency issues while directories creation,
    deletion, updating xattrs, get_attr and put.
    
    Signed-off-by: Pritha Srivastava <prsrivas@redhat.com>

--- src/test/rgw/test_d4n_policy.cc
+++ src/test/rgw/test_d4n_policy.cc
@@ -107,7 +107,7 @@ class LFUDAPolicyFixture : public ::testing::Test {
       std::string oid = build_index(block->cacheObj.bucketName, block->cacheObj.objName, block->blockID, block->size);
 
       if (this->policyDriver->get_cache_policy()->exist_key(build_index(block->cacheObj.bucketName, block->cacheObj.objName, block->blockID, block->size))) { /* Local copy */
-	policyDriver->get_cache_policy()->update(env->dpp, oid, 0, bl.length(), "", false, y);
+	policyDriver->get_cache_policy()->update(env->dpp, oid, 0, bl.length(), "", false, rgw::d4n::REFCOUNT_NOOP, y);
         return 0;
       } else {
 	if (this->policyDriver->get_cache_policy()->eviction(dpp, block->size, y) < 0)
@@ -135,7 +135,7 @@ class LFUDAPolicyFixture : public ::testing::Test {
 	  if (dir->set(env->dpp, block, y) < 0)
 	    return -1;
 
-	  this->policyDriver->get_cache_policy()->update(dpp, oid, 0, bl.length(), "", false, y);
+	  this->policyDriver->get_cache_policy()->update(dpp, oid, 0, bl.length(), "", false, rgw::d4n::REFCOUNT_NOOP, y);
 	  if (cacheDriver->put(dpp, oid, bl, bl.length(), attrs, y) < 0)
             return -1;
 	  return cacheDriver->set_attr(dpp, oid, "localWeight", std::to_string(age), y);
@@ -171,7 +171,7 @@ TEST_F(LFUDAPolicyFixture, LocalGetBlockYield)
 
     std::string key = block->cacheObj.bucketName + "#" + block->cacheObj.objName + "#" + std::to_string(block->blockID) + "#" + std::to_string(block->size);
     ASSERT_EQ(0, cacheDriver->put(env->dpp, key, bl, bl.length(), attrs, optional_yield{yield}));
-    policyDriver->get_cache_policy()->update(env->dpp, key, 0, bl.length(), "", false, optional_yield{yield});
+    policyDriver->get_cache_policy()->update(env->dpp, key, 0, bl.length(), "", false, rgw::d4n::REFCOUNT_NOOP, optional_yield{yield});
 
     ASSERT_EQ(lfuda(env->dpp, block, cacheDriver, yield), 0);
 
@@ -231,12 +231,12 @@ TEST_F(LFUDAPolicyFixture, RemoteGetBlockYield)
     std::string victimKey = victim.cacheObj.bucketName + "_version_" + victim.cacheObj.objName + "_" + std::to_string(victim.blockID) + "_" + std::to_string(victim.size);
     std::string victimKeyInCache = victim.cacheObj.bucketName + "#version#" + victim.cacheObj.objName + "#" + std::to_string(victim.blockID) + "#" + std::to_string(victim.size);
     ASSERT_EQ(0, cacheDriver->put(env->dpp, victimKeyInCache, bl, bl.length(), attrs, optional_yield{yield}));
-    policyDriver->get_cache_policy()->update(env->dpp, victimKeyInCache, 0, bl.length(), "", false, optional_yield{yield});
+    policyDriver->get_cache_policy()->update(env->dpp, victimKeyInCache, 0, bl.length(), "", false, rgw::d4n::REFCOUNT_NOOP, optional_yield{yield});
 
     /* Set head blocks */
     std::string victimHeadObj = victim.cacheObj.bucketName + "#version#" + victim.cacheObj.objName + "#0#0";
     ASSERT_EQ(0, cacheDriver->put(env->dpp, victimHeadObj, bl, bl.length(), attrs, optional_yield{yield}));
-    policyDriver->get_cache_policy()->update(env->dpp, victimHeadObj, 0, bl.length(), "", false, optional_yield{yield});
+    policyDriver->get_cache_policy()->update(env->dpp, victimHeadObj, 0, bl.length(), "", false, rgw::d4n::REFCOUNT_NOOP, optional_yield{yield});
 
     /* Remote block */
     block->size = cacheDriver->get_free_space(env->dpp) + 1; /* To trigger eviction */
