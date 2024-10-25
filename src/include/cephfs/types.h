0161ba422003a31ae1f0437b324c2f7a01e93c6f
Max Kellermann <max.kellermann@ionos.com>
2024-10-25 13:51:44 +0200

    include/cephfs/types.h: include cleanup
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/include/cephfs/types.h
+++ src/include/cephfs/types.h
@@ -12,32 +12,26 @@
  */
 #ifndef CEPH_CEPHFS_TYPES_H
 #define CEPH_CEPHFS_TYPES_H
-#include "include/int_types.h"
 
-#include <ostream>
-#include <set>
+#include <cstdint>
+#include <iosfwd>
 #include <map>
+#include <string>
 #include <string_view>
+#include <vector>
 
 #include "common/config.h"
-#include "common/Clock.h"
-#include "common/DecayCounter.h"
-#include "common/StackStringStream.h"
 #include "common/entity_name.h"
 
 #include "include/compat.h"
-#include "include/Context.h"
 #include "include/frag.h"
-#include "include/xlist.h"
-#include "include/interval_set.h"
 #include "include/compact_set.h"
 #include "include/fs_types.h"
 #include "include/ceph_fs.h"
-
-#include "mds/inode_backtrace.h"
+#include "include/types.h" // for version_t
+#include "include/utime.h"
 
 #include <boost/spirit/include/qi.hpp>
-#include <boost/pool/pool.hpp>
 #include "include/ceph_assert.h"
 #include <boost/serialization/strong_typedef.hpp>
 #include "common/ceph_json.h"
