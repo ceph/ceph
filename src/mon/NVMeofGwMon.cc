901cbc97ff6da4316bc1147980a0e3386b61a0e5
Leonid Chernin <leonidc@il.ibm.com>
2024-10-29 11:30:15 +0000

    mon/nvmeofgw*: fix no-listeners FSM, fix detection of no-listeners condition
    Signed-off-by: Leonid Chernin <leonidc@il.ibm.com>
    (cherry picked from commit 66ca80e7d7db9c47fcfe2f56bb86c008aad2dc9a)
    
    restore  proper no-listeners logic
    
    Signed-off-by: leonidc <leonidc@il.ibm.com>

--- src/mon/NVMeofGwMon.cc
+++ src/mon/NVMeofGwMon.cc
@@ -729,6 +729,7 @@ bool NVMeofGwMon::prepare_beacon(MonOpRequestRef op)
 
   if (sub.size() == 0) {
     avail = gw_availability_t::GW_CREATED;
+    dout(20) << "No-subsystems condition detected for GW " << gw_id <<dendl;
   } else {
     bool listener_found = false;
     for (auto &subs: sub) {
@@ -738,6 +739,7 @@ bool NVMeofGwMon::prepare_beacon(MonOpRequestRef op)
       }
     }
     if (!listener_found) {
+     dout(10) << "No-listeners condition detected for GW " << gw_id  << dendl;
      avail = gw_availability_t::GW_CREATED;
     }
   }// for HA no-subsystems and no-listeners are same usecases
