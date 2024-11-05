8dfc5e292fc34f87c8e4ed7555accf531a89bfc8
Kamoltat Sirivadhna <ksirivad@redhat.com>
2024-11-05 20:53:41 +0000

    [3AZ Stretch pool]: Allow user to specify values when unsetting pools
    Problem:
    
    When we enable stretched mode on the pools,
    we modify 6 configs on the pool,
    namely peering_crush_bucket_count,
    peering_crush_bucket_target,
    peering_crush_bucket_barrier,
    crush_rule, size, min_size.
    
    Out of these, only 3 configs,
    namely peering_crush_bucket_count,
    peering_crush_bucket_target,
    peering_crush_bucket_barrier is reset to 0.
    
    The remaining 3 configs,
    namely crush_rule, size,
    min_size are not reverted back,
    and are still the values that were set with stretch set command.
    
    Solution:
    
    The unset command now is required
    to specify `crush_rule`, `size`, `min_size`.
    
    Fixes: https://tracker.ceph.com/issues/68842
    
    Signed-off-by: Kamoltat Sirivadhna <ksirivad@redhat.com>

--- src/mon/MonCommands.h
+++ src/mon/MonCommands.h
@@ -1234,7 +1234,10 @@ COMMAND("osd pool stretch set "
         "make the pool stretched across the specified number of CRUSH buckets",
         "osd", "rw")
 COMMAND("osd pool stretch unset "
-		"name=pool,type=CephPoolname",
+		"name=pool,type=CephPoolname "
+		"name=crush_rule,type=CephString "
+		"name=size,type=CephInt,range=0 "
+		"name=min_size,type=CephInt,range=0 ",
 		"unset the stretch mode for the pool",
 		"osd", "rw")
 COMMAND("osd utilization",
