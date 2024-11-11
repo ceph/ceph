anshuman-agarwala/ceph-ci

qa/cephadm: adding new testcase for cephadm oauth2-proxy service
Signed-off-by: Redouane Kachach <rkachach@ibm.com>

qa/workunits/cephadm/test_start_dex_server.sh
@@ -1,0 +1,49 @@
+
+MGMT_GW_HOST=$(sudo cephadm shell -- ceph orch ps --daemon-type mgmt-gateway -f json | jq -e '.[]' | jq -r '.hostname')
+sudo tee -a /tmp/dex.conf > /dev/null << EOF
+        issuer: http://${HOSTNAME}:5556/dex
+
+        storage:
+          type: sqlite3
+          config:
+            file: /var/dex/dex.db
+
+        web:
+          http: 0.0.0.0:5556
+
+        expiry:
+          deviceRequests: 5h
+          signingKeys: 6h
+          idTokens: 24h
+          authRequests: 24h
+
+        logger:
+          level: info
+          format: text
+
+        oauth2:
+          responseTypes: [code]
+          skipApprovalScreen: false
+          alwaysShowLoginScreen: false
+
+        enablePasswordDB: true
+
+        staticClients:
+          - id: oauth2-proxy
+            redirectURIs:
+              - 'https://${MGMT_GW_HOST}/oauth2/callback'
+            name: 'oauth2-proxy'
+            secret: proxy
+
+        connectors:
+          - type: mockCallback
+            id: mock
+            name: Example
+
+        staticPasswords:
+          - email: "admin@example.com"
+            hash: "$2a$10$2b2cU8CPhOTaGrs1HRQuAueS7JTT5ZHsHSzYiFPm1leZck7Mc8T4W"
+            username: "admin"
+            userID: "08a8684b-db88-4b73-90a9-3cd1661f5466"
+EOF
+podman run -d --rm -p 5556:5556 -p 5555:5555 -v /tmp/dex.conf:/etc/dex/config.docker.yaml --name dex-container ghcr.io/dexidp/dex

ceph/ceph

qa/workunits/cephadm/test_start_dex_server.sh
@@ -1,0 +1,49 @@
+set -ex
+HOST_IP=$(hostname -i)
+sudo tee -a /tmp/dex.conf > /dev/null << EOF
+        issuer: http://${HOST_IP}:5556/dex
+
+        storage:
+          type: sqlite3
+          config:
+            file: /var/dex/dex.db
+
+        web:
+          http: 0.0.0.0:5556
+
+        expiry:
+          deviceRequests: 5h
+          signingKeys: 6h
+          idTokens: 24h
+          authRequests: 24h
+
+        logger:
+          level: info
+          format: text
+
+        oauth2:
+          responseTypes: [code]
+          skipApprovalScreen: false
+          alwaysShowLoginScreen: false
+
+        enablePasswordDB: true
+
+        staticClients:
+          - id: oauth2-proxy
+            redirectURIs:
+              - 'https://${MGMT_GW_HOST}/oauth2/callback'
+            name: 'oauth2-proxy'
+            secret: proxy
+
+        connectors:
+          - type: mockCallback
+            id: mock
+            name: Example
+
+        staticPasswords:
+          - email: "admin@example.com"
+            hash: "$2a$10$2b2cU8CPhOTaGrs1HRQuAueS7JTT5ZHsHSzYiFPm1leZck7Mc8T4W"
+            username: "admin"
+            userID: "08a8684b-db88-4b73-90a9-3cd1661f5466"
+EOF
+podman run -d --rm -p 5556:5556 -p 5555:5555 -v /tmp/dex.conf:/etc/dex/config.docker.yaml --name dex-container ghcr.io/dexidp/dex