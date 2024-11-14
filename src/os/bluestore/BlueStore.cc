f1f9368608895e46582aad6ae14d9c2fb3cf0ef9
Adam Kupczyk <akupczyk@ibm.com>
2024-11-14 11:23:25 +0000

    os/bluestore: Add debug hooks
    Added debug hooks for:
    - init_alloc
    - deferred start/stop/operation
    
    This created framework for specific unit tests.
    These functions are as name suggests debug level, only for unittests.
    
    Signed-off-by: Adam Kupczyk <akupczyk@ibm.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -7416,7 +7416,7 @@ int BlueStore::_init_alloc()
           << ", free 0x" << alloc->get_free()
           << ", fragmentation " << alloc->get_fragmentation()
           << std::dec << dendl;
-
+  dtr_init_alloc_done();
   return 0;
 }
 
@@ -15546,6 +15546,7 @@ int BlueStore::_deferred_replay()
     fake_ch = true;
   }
   OpSequencer *osr = static_cast<OpSequencer*>(ch->osr.get());
+  dtr_deferred_replay_start();
   KeyValueDB::Iterator it = db->get_iterator(PREFIX_DEFERRED);
   for (it->lower_bound(string()); it->valid(); it->next(), ++count) {
     dout(20) << __func__ << " replay " << pretty_binary_string(it->key())
@@ -15565,6 +15566,7 @@ int BlueStore::_deferred_replay()
     }
     bool has_some = _eliminate_outdated_deferred(deferred_txn, bluefs_extents);
     if (has_some) {
+      dtr_deferred_replay_track(*deferred_txn);
       TransContext *txc = _txc_create(ch.get(), osr,  nullptr);
       txc->deferred_txn = deferred_txn;
       txc->set_state(TransContext::STATE_KV_DONE);
@@ -15577,6 +15579,7 @@ int BlueStore::_deferred_replay()
   dout(20) << __func__ << " draining osr" << dendl;
   _osr_register_zombie(osr);
   _osr_drain_all();
+  dtr_deferred_replay_end();
   if (fake_ch) {
     new_coll_map.clear();
   }
--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -3560,6 +3560,7 @@ public:
   void debug_set_prefer_deferred_size(uint64_t s) {
     prefer_deferred_size = s;
   }
+
   inline void log_latency(const char* name,
     int idx,
     const ceph::timespan& lat,
@@ -3573,6 +3574,12 @@ public:
     double lat_threshold,
     std::function<std::string (const ceph::timespan& lat)> fn,
     int idx2 = l_bluestore_first);
+
+  debug_point_t<std::function<void()>> dtr_deferred_replay_start;
+  debug_point_t<std::function<void()>> dtr_deferred_replay_end;
+  debug_point_t<std::function<void()>> dtr_init_alloc_done;
+  debug_point_t<std::function<void(const bluestore_deferred_transaction_t&)>>
+    dtr_deferred_replay_track;
 
 private:
   bool _debug_data_eio(const ghobject_t& o) {
