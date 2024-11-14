a4f9dea6a732d5c2fff64030e4b92581fdb1793c
Patrick Donnelly <pdonnell@ibm.com>
2024-11-14 12:36:14 -0500

    client: load changes to fuse_default_permissions
    Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>
    Fixes: https://tracker.ceph.com/issues/66373

--- src/client/Client.h
+++ src/client/Client.h
@@ -942,7 +942,6 @@ public:
   std::unique_ptr<PerfCounters> logger;
   std::unique_ptr<MDSMap> mdsmap;
 
-  bool fuse_default_permissions;
   bool _collect_and_send_global_metrics;
 
 protected:
@@ -1953,6 +1952,8 @@ private:
   std::vector<MDSCapAuth> cap_auths;
 
   feature_bitset_t myfeatures;
+
+  bool fuse_default_permissions;
 };
 
 /**
