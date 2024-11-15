c1424eb546f46aaee6b72e6bd58515267fb0d606
Seena Fallah <seenafallah@gmail.com>
2024-11-15 23:45:20 +0100

    rgw: consider hint sources in RGWGetBucketPeersCR
    When running `radosgw-admin bucket sync run` with only the target
    bucket specified and no source bucket, RGWGetBucketPeersCR doesn't
    account for resolved sources from the sync pipe, resulting in no
    pipes being returned and causing the command to fail. This change
    ensures that hint sources are considered to avoid this issue.
    
    Signed-off-by: Seena Fallah <seenafallah@gmail.com>

--- src/test/rgw/rgw_multi/tests.py
+++ src/test/rgw/rgw_multi/tests.py
@@ -1866,7 +1866,6 @@ def test_sync_single_bucket_to_multiple_log_trim_zonegroups(zonegroup):
     assert(len(test_bilog) == 0)
 
 
-@attr('fails_with_rgw')  # RGWGetBucketPeersCR doesn't respect the resolved_sources when only target_bucket is passed.
 @attr('bucket_reshard')
 @allow_zonegroups_replication
 @run_per_zonegroup
@@ -1906,7 +1905,6 @@ def test_bucket_sync_run_basic_incremental_zonegroups(source_zone, dest_zone, so
     zone_bucket_checkpoint(dest_zone.zone, source_zone.zone, dest_bucket.name)
 
 
-@attr('fails_with_rgw')  # RGWGetBucketPeersCR doesn't respect the resolved_sources when only target_bucket is passed.
 @attr('bucket_reshard')
 @allow_zonegroups_replication
 @run_per_zonegroup
