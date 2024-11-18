d2d04d7d864ce76178ff8754e7535bea72871e47
Kritik Sachdeva <sachdevakritik.440@gmail.com>
2024-11-18 14:12:35 +0530

    mgr/orchestrator: Adding new command to list available parameters available to use in service specification for respective Ceph service includes RGW, NFS, NVMEoF, ISCSI, Ingress, Mon, SNMPGateway, & SMB
    Fixes: https://tracker.ceph.com/issues/67982
    
    Signed-off-by: Kritik Sachdeva <sachdevakritik.440@gmail.com>

--- src/pybind/mgr/orchestrator/_interface.py
+++ src/pybind/mgr/orchestrator/_interface.py
@@ -45,7 +45,7 @@ from ceph.deployment.service_spec import (
     MgmtGatewaySpec,
     OAuth2ProxySpec,
     ServiceSpec,
-    TunedProfileSpec
+    TunedProfileSpec,
 )
 from ceph.deployment.drive_group import DriveGroupSpec
 from ceph.deployment.hostspec import HostSpec, SpecValidationError
