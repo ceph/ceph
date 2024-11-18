ceph/ceph

commit 1da6ef237fc70ddd64152d029cd6e0cf8f0c808e

include/ceph_fs: correct ceph_mds_cap_peer field name
The peer seq is used as the issue_seq. Use that name for consistency.

Fixes: 4fdeb00df20ccd36c1e53c6ea234c63c18a9ff5a
Signed-off-by: Patrick Donnelly <pdonnell@redhat.com>

src/client/Client.cc
@@ -5518,10 +5518,10 @@ void Client::handle_cap_export(MetaSession *session, Inode *in, const MConstRef<
         if (it != in->caps.end()) {
 	  Cap &tcap = it->second;
 	  if (tcap.cap_id == m->peer.cap_id &&
-	      ceph_seq_cmp(tcap.seq, m->peer.seq) < 0) {
+	      ceph_seq_cmp(tcap.seq, m->peer.issue_seq) < 0) {
 	    tcap.cap_id = m->peer.cap_id;
-	    tcap.seq = m->peer.seq - 1;
-	    tcap.issue_seq = tcap.seq;
+	    tcap.seq = m->peer.issue_seq - 1;
+	    tcap.issue_seq = tcap.issue_seq;
 	    tcap.issued |= cap.issued;
 	    tcap.implemented |= cap.issued;
 	    if (&cap == in->auth_cap)
@@ -5531,7 +5531,7 @@ void Client::handle_cap_export(MetaSession *session, Inode *in, const MConstRef<
 	  }
         } else {
 	  add_update_cap(in, tsession.get(), m->peer.cap_id, cap.issued, 0,
-		         m->peer.seq - 1, m->peer.mseq, (uint64_t)-1,
+		         m->peer.issue_seq - 1, m->peer.mseq, (uint64_t)-1,
 		         &cap == in->auth_cap ? CEPH_CAP_FLAG_AUTH : 0,
 		         cap.latest_perms);
         }

src/include/ceph_fs.h
@@ -1005,7 +1005,7 @@ extern const char *ceph_cap_op_name(int op);
 /* extra info for cap import/export */
 struct ceph_mds_cap_peer {
 	__le64 cap_id;
-	__le32 seq;
+	__le32 issue_seq;
 	__le32 mseq;
 	__le32 mds;
 	__u8   flags;

src/messages/MClientCaps.h
@@ -117,9 +117,9 @@ private:
   void set_ctime(const utime_t &t) { ctime = t; }
   void set_atime(const utime_t &t) { atime = t; }
 
-  void set_cap_peer(uint64_t id, ceph_seq_t seq, ceph_seq_t mseq, int mds, int flags) {
+  void set_cap_peer(uint64_t id, ceph_seq_t issue_seq, ceph_seq_t mseq, int mds, int flags) {
     peer.cap_id = id;
-    peer.seq = seq;
+    peer.issue_seq = issue_seq;
     peer.mseq = mseq;
     peer.mds = mds;
     peer.flags = flags;

commit 1cb07f9cba00a2bbc7eeb648debefcfb262f37dc

client: resolve bogus self-assignment
Credit to  Ilya Dryomov for spotting this.

[1] https://github.com/ceph/ceph/pull/60283#discussion_r1846415092

Fixes: 1da6ef237fc70ddd64152d029cd6e0cf8f0c808e
Fixes: https://tracker.ceph.com/issues/68973
Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>

src/client/Client.cc
@@ -5524,7 +5524,7 @@ void Client::handle_cap_export(MetaSession *session, Inode *in, const MConstRef<
 	      ceph_seq_cmp(tcap.seq, m->peer.issue_seq) < 0) {
 	    tcap.cap_id = m->peer.cap_id;
 	    tcap.seq = m->peer.issue_seq - 1;
-	    tcap.issue_seq = tcap.issue_seq;
+	    tcap.issue_seq = m->peer.issue_seq - 1;
 	    tcap.issued |= cap.issued;
 	    tcap.implemented |= cap.issued;
 	    if (&cap == in->auth_cap)