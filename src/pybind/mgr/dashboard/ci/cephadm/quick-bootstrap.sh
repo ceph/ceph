46b82ed18567aa3b88969e7258e3f48c3734e11c
Nizamudeen A <nia@redhat.com>
2024-11-22 23:55:45 +0530

    mgr/dashboard: adapt kcli plan for multi-clusters
    to start multiple clusters we can simply do
    ```
    ./quick-bootstrap.sh -dir=/home/nia/projects/ceph -u -e -n=2 -c=2
    
    -c = number of clusters
    -n = number of nodes per cluster
    ```
    
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- src/pybind/mgr/dashboard/ci/cephadm/quick-bootstrap.sh
+++ src/pybind/mgr/dashboard/ci/cephadm/quick-bootstrap.sh
@@ -11,14 +11,21 @@ show_help() {
   echo "  -u, --use-cached-image     Uses the existing podman image in local. Only use this if there is such an image present."
   echo "  -dir, --ceph-dir             Use this to provide the local ceph directory. eg. --ceph-dir=/path/to/ceph"
   echo "  -e, --expanded-cluster     To add all the hosts and deploy OSDs on top of it."
+  echo "  -c, --clusters          Number of clusters to be created. Default is 1."
+  echo "  -n, --nodes           Number of nodes to be created per cluster. Default is 3."
   echo "  -h, --help             Display this help message."
   echo ""
   echo "Example:"
   echo "  ./quick-bootstrap.sh --use-cached-image"
 }
 
+GREEN='\033[0;32m'
+RESET='\033[0m'
+
 use_cached_image=false
 extra_args="-P quick_install=True"
+CLUSTERS=1
+NODES=3
 
 for arg in "$@"; do
   case "$arg" in
@@ -30,6 +37,12 @@ for arg in "$@"; do
       ;;
     -e|--expanded-cluster)
       extra_args+=" -P expanded_cluster=True"
+      ;;
+    -n=*|--nodes=*)
+      NODES="${arg#*=}"
+      ;;
+    -c=*|--clusters=*)
+      CLUSTERS="${arg#*=}"
       ;;
     -h|--help)
       show_help
@@ -44,8 +57,8 @@ for arg in "$@"; do
 done
 
 image_name=$(echo "$CEPHADM_IMAGE")
-ceph_cluster_yml='ceph_cluster.yml'
-node_count=$(awk '/nodes:/ {print $2}' "${ceph_cluster_yml}")
+
+extra_args+=" -P nodes=${NODES}"
 
 if [[ ${use_cached_image} == false ]]; then
     printf "Pulling the image: %s\n" "$image_name"
@@ -57,30 +70,44 @@ rm -f ceph_image.tar
 printf "Saving the image: %s\n" "$image_name"
 podman save -o ceph_image.tar quay.ceph.io/ceph-ci/ceph:main
 
-printf "Creating the plan\n"
-kcli create plan -f ceph_cluster.yml ${extra_args} ceph
+NODE_IP_OFFSET=100
+PREFIX="ceph"
+for cluster in $(seq 1 $CLUSTERS); do
+    if [[ $cluster -gt 1 ]]; then
+        PREFIX="ceph${cluster}"
+    fi
+    printf "\nCreating cluster: %s\n" "${PREFIX}"
+    kcli create plan -f ceph_cluster.yml ${extra_args} -P node_ip_offset=${NODE_IP_OFFSET} \
+      -P prefix=${PREFIX} ${PREFIX}
+    NODE_IP_OFFSET=$((NODE_IP_OFFSET + 10))
+done
 
 attempt=0
 
 MAX_ATTEMPTS=10
 SLEEP_INTERVAL=5
 
-printf "Waiting for the host to be reachable\n"
-while [[ ${attempt} -lt ${MAX_ATTEMPTS} ]]; do
-    if ssh -o StrictHostKeyChecking=no -o BatchMode=yes -o ConnectTimeout=10 root@192.168.100.100 exit; then
-        break
-    else
-        echo "Waiting for ssh connection to be available..., attempt: ${attempt}"
-        ((attempt++))
-        sleep ${SLEEP_INTERVAL}
-    fi
+NODE_IP_OFFSET=100
+PREFIX="ceph"
+for cluster in $(seq 1 $CLUSTERS); do
+  if [[ $cluster -gt 1 ]]; then
+      PREFIX="ceph${cluster}"
+  fi
+  printf "\nWaiting for the host to be reachable on cluster ${PREFIX}...\n"
+
+  while [[ ${attempt} -lt ${MAX_ATTEMPTS} ]]; do
+      if ssh -o StrictHostKeyChecking=no -o BatchMode=yes -o ConnectTimeout=10 root@192.168.100."${NODE_IP_OFFSET}" exit; then
+          printf "\n${GREEN}Host is reachable on cluster %s\n${RESET}" "${PREFIX}"
+          break
+      else
+          echo "Waiting for ssh connection to be available..., attempt: ${attempt}"
+          ((attempt++))
+          sleep ${SLEEP_INTERVAL}
+      fi
+  done
+
+  NODE_IP_OFFSET=$((NODE_IP_OFFSET + 10))
 done
 
-printf "Copying the image to the hosts\n"
-
-for node in $(seq 0 $((node_count - 1))); do
-    scp -o StrictHostKeyChecking=no ceph_image.tar root@192.168.100.10"${node}":/root/
-done
-
-rm -f ceph_image.tar
+printf "\nOpening logs for first cluster"
 kcli ssh -u root -- ceph-node-00 'journalctl -n all -ft cloud-init'
