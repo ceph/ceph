67c599e067edbe7247604997a7df50af5e1ff6ab
Shweta Bhosale <Shweta.Bhosale1@ibm.com>
2024-11-28 15:02:35 +0530

    Squid: cephadm: bootstap should not have "This is a development version of cephadm" message
    Fixes: https://tracker.ceph.com/issues/69075
    Signed-off-by: Shweta Bhosale <Shweta.Bhosale1@ibm.com>

--- src/cephadm/cephadmlib/constants.py
+++ src/cephadm/cephadmlib/constants.py
@@ -2,7 +2,7 @@
 
 # Default container images -----------------------------------------------------
 DEFAULT_IMAGE = 'quay.ceph.io/ceph-ci/ceph:main'
-DEFAULT_IMAGE_IS_MAIN = True
+DEFAULT_IMAGE_IS_MAIN = False
 DEFAULT_IMAGE_RELEASE = 'squid'
 DEFAULT_REGISTRY = 'quay.io'  # normalize unqualified digests to this
 # ------------------------------------------------------------------------------
