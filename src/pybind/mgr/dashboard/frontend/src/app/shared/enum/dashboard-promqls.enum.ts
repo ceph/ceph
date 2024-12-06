9bede61c0fca7a29bc624c85f6ff474765e89de6
Aashish Sharma <aasharma@li-e74156cc-2f67-11b2-a85c-e98659a63c5c.ibm.com>
2024-12-06 15:27:25 +0530

    mgr/dashboard: Fix Latency chart data units in rgw overview page
    Issue: The Latency chart in the rgw overview page shows incorrect data
    unit as the unit that we are passing as an input to the dashboard-area
    chart component is `ms` whereas the data that we get from the metrics is
    in seconds. Due to this if we pass a value like 0.725s to
    the dashboard chart component it will show the value in the chart as
    0.725ms whereas it should be 725ms.
    
    Fix: Pass the value in ms as expected to the dashboard area chart
    component
    
    Fixes: https://tracker.ceph.com/issues/69144
    
    Signed-off-by: Aashish Sharma <aasharma@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/enum/dashboard-promqls.enum.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/enum/dashboard-promqls.enum.ts
@@ -11,8 +11,8 @@ export enum Promqls {
 
 export enum RgwPromqls {
   RGW_REQUEST_PER_SECOND = 'sum(rate(ceph_rgw_req[1m]))',
-  AVG_GET_LATENCY = 'sum(rate(ceph_rgw_op_get_obj_lat_sum[1m])) / sum(rate(ceph_rgw_op_get_obj_lat_count[1m]))',
-  AVG_PUT_LATENCY = 'sum(rate(ceph_rgw_op_put_obj_lat_sum[1m])) / sum(rate(ceph_rgw_op_put_obj_lat_count[1m]))',
+  AVG_GET_LATENCY = '(sum(rate(ceph_rgw_op_get_obj_lat_sum[1m])) / sum(rate(ceph_rgw_op_get_obj_lat_count[1m]))) * 1000',
+  AVG_PUT_LATENCY = '(sum(rate(ceph_rgw_op_put_obj_lat_sum[1m])) / sum(rate(ceph_rgw_op_put_obj_lat_count[1m]))) * 1000',
   GET_BANDWIDTH = 'sum(rate(ceph_rgw_op_get_obj_bytes[1m]))',
   PUT_BANDWIDTH = 'sum(rate(ceph_rgw_op_put_obj_bytes[1m]))'
 }
