3965acb820055fca2f2892642ee71898525243b2
Max Kellermann <max.kellermann@ionos.com>
2024-12-09 22:48:46 +0100

    crimson/os/cyanstore: add "using ceph::decode" to fix build failure
    This decode() overhead is really in the `std` namespace.  The best way
    to solve this is to import the primary function with `using` line.
    
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/crimson/os/cyanstore/cyan_store.cc
+++ src/crimson/os/cyanstore/cyan_store.cc
@@ -154,6 +154,8 @@ CyanStore::get_default_device_class()
 
 CyanStore::mount_ertr::future<> CyanStore::Shard::mount()
 {
+  using ceph::decode;
+
   static const char read_file_errmsg[]{"read_file"};
   ceph::bufferlist bl;
   std::string fn =
@@ -165,7 +167,7 @@ CyanStore::mount_ertr::future<> CyanStore::Shard::mount()
 
   std::set<coll_t> collections;
   auto p = bl.cbegin();
-  ceph::decode(collections, p);
+  decode(collections, p);
 
   for (auto& coll : collections) {
     std::string fn = fmt::format("{}/{}{}", path, coll,
