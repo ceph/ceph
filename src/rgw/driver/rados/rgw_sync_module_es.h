e7264e5f506cef2b093c1534314dbb37a1e0ce69
Seena Fallah <seenafallah@gmail.com>
2024-12-09 14:32:35 +0100

    rgw: control full sync on bucket replication enable
    Introduced a "skip-existing-object-replication-policy" feature,
    allowing bucket replication to start incrementally for new changes
    and objects, bypassing a full sync of existing objects. The feature
    applies to both intra- and cross-zonegroup replication policies,
    aligning with AWS behavior after the deprecation of their
    ExistingObjectReplication option.
    
    Fixes: https://tracker.ceph.com/issues/69159
    Signed-off-by: Seena Fallah <seenafallah@gmail.com>

--- src/rgw/driver/rados/rgw_sync_module_es.h
+++ src/rgw/driver/rados/rgw_sync_module_es.h
@@ -38,7 +38,7 @@ public:
   bool supports_data_export() override {
     return false;
   }
-  int create_instance(const DoutPrefixProvider *dpp, CephContext *cct, const JSONFormattable& config, RGWSyncModuleInstanceRef *instance) override;
+  int create_instance(const DoutPrefixProvider *dpp, CephContext *cct, const JSONFormattable& config, const RGWZoneGroup& zonegroup, RGWSyncModuleInstanceRef *instance) override;
 };
 
 class RGWElasticDataSyncModule;
