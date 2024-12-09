f638cfccf89c360a6ca1ead2486eaed9a0174099
Max Kellermann <max.kellermann@ionos.com>
2024-12-09 22:34:43 +0100

    messages/*: include cleanup
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/messages/MTimeCheck.h
+++ src/messages/MTimeCheck.h
@@ -39,8 +39,8 @@ public:
   version_t round = 0;
 
   utime_t timestamp;
-  map<entity_inst_t, double> skews;
-  map<entity_inst_t, double> latencies;
+  std::map<entity_inst_t, double> skews;
+  std::map<entity_inst_t, double> latencies;
 
   MTimeCheck() : Message{MSG_TIMECHECK, HEAD_VERSION} {}
   MTimeCheck(int op) :
