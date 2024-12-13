210d151d103161b0259285e6a0e320b9a440e825
pujaoshahu <pshahu@redhat.com>
2024-12-13 10:41:13 +0530

    mgr/dashboard: Add RGW topics endpoint creation for create ,delete and list in dashboard
    Fixes: https://tracker.ceph.com/issues/69229
    
    Signed-off-by: Puja Shahu<pshahu@redhat.com>
    Signed-off-by: pujaoshahu <pshahu@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-topic-details/rgw-topic-details.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-topic-details/rgw-topic-details.component.ts
@@ -1,14 +1,25 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges, OnChanges } from '@angular/core';
+import { TopicDetailModel } from './topic-detail.model';
 
 @Component({
   selector: 'cd-rgw-topic-details',
   templateUrl: './rgw-topic-details.component.html',
   styleUrls: ['./rgw-topic-details.component.scss']
 })
-export class RgwTopicDetailsComponent {
+export class RgwTopicDetailsComponent implements OnChanges {
   @Input()
-  selection: any;
+  selection: TopicDetailModel;
   tenant: string;
   topicname: string;
+  policy?: {};
   constructor() {}
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['selection'] && this.selection) {
+      if (typeof this.selection.policy === 'string') {
+        this.policy = JSON.parse(this.selection.policy);
+      } else {
+        this.policy = {};
+      }
+    }
+  }
 }
