fac004c0532ecfd85a5475a9e4b860c2e5cb16b2
pujaoshahu <pshahu@redhat.com>
2024-12-13 10:41:13 +0530

    mgr/dashboard: Add RGW topics endpoint creation for create ,delete and list in dashboard
    Fixes: https://tracker.ceph.com/issues/69229
    
    Signed-off-by: Puja Shahu<pshahu@redhat.com>
    Signed-off-by: pujaoshahu <pshahu@redhat.com>

---
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-create-topic-form/create-topic.model.ts
@@ -0,0 +1,28 @@
+export interface CreateTopicModel {
+  owner: string;
+  name: string;
+  push_endpoint: string;
+  opaque_data: string;
+  persistent?: string;
+  time_to_live?: string;
+  max_retries?: string;
+  retry_sleep_duration?: string;
+  policy: {} | string;
+  verify_ssl?: boolean;
+  cloud_events?: string;
+  ca_location?: string;
+  amqp_exchange?: string;
+  amqp_ack_level?: string;
+  use_ssl?: boolean;
+  kafka_ack_level?: string;
+  kafka_brokers?: string;
+  mechanism?: string;
+}
+
+export const KAFKA_MECHANISM = {
+  PLAIN: 'PLAIN',
+  SCRAM256: 'SCRAM-SHA-256',
+  SCRAM512: 'SCRAM-SHA-512',
+  GSSAPI: 'GSSAPI',
+  OAUTHBEARER: 'OAUTHBEARER'
+};
