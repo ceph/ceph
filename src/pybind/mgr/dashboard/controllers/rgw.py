ca99b08283beea86eafd3b1abbacfe2bd26e9647
Achintk1491 <achintk1491@gmail.com>
2024-12-13 13:08:33 +0530

    Fixes: https://tracker.ceph.com/issues/69233
    Signed-Off-by: Achint Kaur<ackaur@redhat.com>

--- src/pybind/mgr/dashboard/controllers/rgw.py
+++ src/pybind/mgr/dashboard/controllers/rgw.py
@@ -15,7 +15,7 @@ from ..security import Permission, Scope
 from ..services.auth import AuthManager, JwtManager
 from ..services.ceph_service import CephService
 from ..services.rgw_client import _SYNC_GROUP_ID, NoRgwDaemonsException, \
-    RgwClient, RgwMultisite, RgwMultisiteAutomation
+    RgwClient, RgwMultisite, RgwMultisiteAutomation, RgwRateLimit
 from ..services.rgw_iam import RgwAccounts
 from ..services.service import RgwServiceManager, wait_for_daemon_to_start
 from ..tools import json_str_to_object, str_to_bool
@@ -945,7 +945,29 @@ class RgwUser(RgwRESTController):
             'purge-keys': purge_keys
         }, json_response=False)
 
+    @Endpoint(method='GET', path='/ratelimit')
+    @EndpointDoc("Get the global rate limit")
+    @ReadPermission 
+    def get_global_rate_limit(self):
+        rgwRateLimit_instance = RgwRateLimit()
+        return rgwRateLimit_instance.get_global_rateLimit()
 
+    @Endpoint(method='GET', path='/ratelimit')
+    @EndpointDoc("Get the rate limit")
+    @ReadPermission 
+    def get_rate_limit(self, scope:str, name: str ):
+        rgwRateLimit_instance = RgwRateLimit()
+        return rgwRateLimit_instance.get_rateLimit(scope, name)
+    
+    @Endpoint(method='PUT', path='/ratelimit')
+    @UpdatePermission
+    @allow_empty_body
+    @EndpointDoc("Update the rate limit")
+    def set_rate_limit(self, scope,enabled,name, max_read_ops, max_write_ops,max_read_bytes,max_write_bytes):
+        rgwRateLimit_instance = RgwRateLimit()
+        return rgwRateLimit_instance.set_rateLimit(scope, enabled, name, max_read_ops, max_write_ops,max_read_bytes,max_write_bytes)
+
+    
 class RGWRoleEndpoints:
     @staticmethod
     def role_list(_):
