344b99ec55395c3122f8cf1205deb4969f732c2c
Xuehan Xu <xuxuehan@qianxin.com>
2024-12-14 21:26:33 +0800

    crimson/os/seastore: introduce cache_hint_t
    Layers above Cache can use cache_hint_t to notify Cache whether to put
    the extents to Cache::lru.
    
    CEPH_OSD_OP_FLAG_FADVISE_DONTNEED and CEPH_OSD_OP_FLAG_FADVISE_NOCACHE
    will be treated as "no need to put into Cache::lru"
    
    Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

--- src/crimson/os/seastore/transaction.h
+++ src/crimson/os/seastore/transaction.h
@@ -408,12 +408,14 @@ public:
     src_t src,
     journal_seq_t initiated_after,
     on_destruct_func_t&& f,
-    transaction_id_t trans_id
+    transaction_id_t trans_id,
+    cache_hint_t cache_hint
   ) : weak(weak),
       handle(std::move(handle)),
       on_destruct(std::move(f)),
       src(src),
-      trans_id(trans_id)
+      trans_id(trans_id),
+      cache_hint(cache_hint)
   {}
 
   void invalidate_clear_write_set() {
@@ -571,6 +573,10 @@ public:
     return pre_alloc_list;
   }
 
+  cache_hint_t get_cache_hint() const {
+    return cache_hint;
+  }
+
 private:
   friend class Cache;
   friend Ref make_test_transaction();
@@ -669,6 +675,8 @@ private:
   transaction_id_t trans_id = TRANS_ID_NULL;
 
   seastar::lw_shared_ptr<rbm_pending_ool_t> pending_ool;
+
+  cache_hint_t cache_hint = CACHE_HINT_TOUCH;
 };
 using TransactionRef = Transaction::Ref;
 
@@ -681,7 +689,8 @@ inline TransactionRef make_test_transaction() {
     Transaction::src_t::MUTATE,
     JOURNAL_SEQ_NULL,
     [](Transaction&) {},
-    ++next_id
+    ++next_id,
+    CACHE_HINT_TOUCH
   );
 }
 
