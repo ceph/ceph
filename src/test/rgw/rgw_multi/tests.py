8364bb203b011ab4d760d374b0bb78d059193509
Shilpa Jagannath <smanjara@redhat.com>
2024-12-18 14:59:07 -0500

    rgw/trim: restore update_mapping_update in RGWBucketInstanceMetadataHandler::remove - fix testcase
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>

--- src/test/rgw/rgw_multi/tests.py
+++ src/test/rgw/rgw_multi/tests.py
@@ -1733,6 +1733,7 @@ def test_bucket_log_trim_after_delete_bucket_primary_reshard():
     bilog_autotrim(primary.zone, ['--rgw-sync-log-trim-max-buckets', '50'],)
     
     for zonegroup in realm.current_period.zonegroups:
+        zonegroup_conns = ZonegroupConns(zonegroup)
         for zone in zonegroup_conns.zones:
             assert check_bucket_instance_metadata(zone.zone, test_bucket.name)
 
@@ -1796,6 +1797,7 @@ def test_bucket_log_trim_after_delete_bucket_secondary_reshard():
     time.sleep(config.checkpoint_delay)
 
     for zonegroup in realm.current_period.zonegroups:
+        zonegroup_conns = ZonegroupConns(zonegroup)
         for zone in zonegroup_conns.zones:
             assert check_bucket_instance_metadata(zone.zone, test_bucket.name)
 
