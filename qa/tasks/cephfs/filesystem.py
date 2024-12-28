6d4154426b0b0112c4b55744338e5db8900d8614
Kotresh HR <khiremat@redhat.com>
2024-12-29 00:03:24 +0530

    qa/test_backtrace: Validate remote_inode xattr is stored
    Validate 'remote_inode' xattr is stored on the
    referent inode upon hardlink creation. The value
    of the xattr would be the inode number of the
    primary inode.
    
    Fixes: https://tracker.ceph.com/issues/69338
    Signed-off-by: Kotresh HR <khiremat@redhat.com>

--- qa/tasks/cephfs/filesystem.py
+++ qa/tasks/cephfs/filesystem.py
@@ -1304,9 +1304,16 @@ class FilesystemBase(MDSClusterBase):
 
             status = self.status()
 
-    def dencoder(self, obj_type, obj_blob):
-        args = [os.path.join(self._prefix, "ceph-dencoder"), 'type', obj_type, 'import', '-', 'decode', 'dump_json']
-        p = self.mon_manager.controller.run(args=args, stdin=BytesIO(obj_blob), stdout=BytesIO())
+    def dencoder(self, obj_type, obj_blob, skip=0):
+        args = [os.path.join(self._prefix, "ceph-dencoder"), 'type', obj_type]
+        if skip != 0 :
+            args.extend(["skip", str(skip)])
+        args.extend(['import', '-', 'decode', 'dump_json'])
+        try:
+          p = self.mon_manager.controller.run(args=args, stdin=BytesIO(obj_blob), stdout=BytesIO())
+        except Exception:
+            time.sleep(500)
+
         return p.stdout.getvalue()
 
     def rados(self, *args, **kwargs):
@@ -1522,6 +1529,44 @@ class FilesystemBase(MDSClusterBase):
         obj_name = "{0:x}.00000000".format(ino_no)
         args = ["setxattr", obj_name, xattr_name, data]
         self.rados(args, pool=pool)
+
+    def read_meta_inode(self, dir_ino, file_name, pool=None):
+        """
+        Get referent_inode list from the primary file
+        """
+        if pool is None:
+            pool = self.get_metadata_pool_name()
+
+        dirfrag_obj_name = "{0:x}.00000000".format(dir_ino)
+        args=["getomapval", dirfrag_obj_name, file_name+"_head", "-"]
+        try:
+            proc = self.rados(args, pool=pool, stdout=BytesIO())
+        except CommandFailedError as e:
+            log.error(e.__str__())
+            time.sleep(500)
+            raise ObjectNotFound(dirfrag_obj_name)
+
+        obj_blob = proc.stdout.getvalue()
+        return json.loads(self.dencoder("inode_t<std::allocator>", obj_blob, 25).strip())
+
+    def read_remote_inode(self, ino_no, pool=None):
+        """
+        Read the remote_inode xattr from the data pool, return a dict in the
+        format given by inodeno_t::dump, which is something like:
+
+        ::
+
+            rados -p cephfs_data getxattr 100000001f8.00000000 remote_inode > out.bin
+            ceph-dencoder type inodeno_t import out.bin decode dump_json
+
+            {
+                 "val": 1099511627778
+            }
+
+        :param pool: name of pool to read backtrace from.  If omitted, FS must have only
+                     one data pool and that will be used.
+        """
+        return self._read_data_xattr(ino_no, "remote_inode", "inodeno_t", pool)
 
     def read_symlink(self, ino_no, pool=None):
         return self._read_data_xattr(ino_no, "symlink", "string_wrapper", pool)
