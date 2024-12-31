bcb84b3ff5a619b1b3df528b20765198b4d2fb86
Zhang Song <zhangsong02@qianxin.com>
2024-12-31 15:12:30 +0800

    crimson/os/seastore/Onode: adapt new get hint approach
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/crimson/os/seastore/onode.h
+++ src/crimson/os/seastore/onode.h
@@ -54,7 +54,24 @@ class Onode : public boost::intrusive_ref_counter<
   boost::thread_unsafe_counter>
 {
 protected:
-  virtual laddr_t get_hint() const = 0;
+  virtual laddr_hint_t generate_data_hint(
+    std::optional<local_object_id_t> object_id,
+    std::optional<local_clone_id_t> clone_id,
+    extent_len_t block_size) const = 0;
+
+  virtual laddr_hint_t generate_data_clone_hint(
+    local_object_id_t object_id,
+    extent_len_t block_size) const = 0;
+
+  virtual laddr_hint_t generate_metadata_hint(
+    std::optional<local_object_id_t> object_id,
+    std::optional<local_clone_id_t> clone_id,
+    extent_len_t block_size) const = 0;
+
+  virtual laddr_hint_t generate_metadata_clone_hint(
+    local_object_id_t object_id,
+    extent_len_t block_size) const = 0;
+
   const hobject_t hobj;
 
   void validate_root_laddr(laddr_t laddr) const {
@@ -80,11 +97,39 @@ public:
   virtual void clear_object_info(Transaction&) = 0;
   virtual void clear_snapset(Transaction&) = 0;
 
-  laddr_t get_metadata_hint(uint64_t block_size) const {
-    return get_hint();
+  laddr_hint_t get_data_hint(
+    extent_len_t block_size = laddr_t::UNIT_SIZE) const {
+    auto prefix = get_clone_prefix();
+    if (prefix) {
+      return generate_data_hint(
+        prefix->get_local_object_id(),
+        prefix->get_local_clone_id(),
+        block_size);
+    } else {
+      return generate_data_hint(std::nullopt, std::nullopt, block_size);
+    }
   }
-  laddr_t get_data_hint() const {
-    return get_hint();
+  laddr_hint_t get_data_clone_hint(
+    extent_len_t block_size = laddr_t::UNIT_SIZE) const {
+    auto prefix = get_object_prefix();
+    assert(prefix);
+    return generate_data_clone_hint(prefix->get_local_object_id(), block_size);
+  }
+  laddr_hint_t get_metadata_hint(extent_len_t block_size) const {
+    auto prefix = get_clone_prefix();
+    if (prefix) {
+      return generate_metadata_hint(
+        prefix->get_local_object_id(),
+        prefix->get_local_clone_id(),
+        block_size);
+    } else {
+      return generate_metadata_hint(std::nullopt, std::nullopt, block_size);
+    }
+  }
+  laddr_hint_t get_metadata_clone_hint(extent_len_t block_size) const {
+    auto prefix = get_object_prefix();
+    assert(prefix);
+    return generate_metadata_clone_hint(prefix->get_local_object_id(), block_size);
   }
 
   std::optional<laddr_t> get_clone_prefix() const {
