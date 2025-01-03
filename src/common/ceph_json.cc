87fdabf440ae888c3abc49977b1ceb8eb1894184
Jesse F. Williamson <1643380+chardan@users.noreply.github.com>
2025-01-02 17:03:57 -0800

    The file version of parse() is indeed used.
    This is actually called in multiple places.
    
    Signed-off-by: Jesse F. Williamson <jfw@ibm.com>

--- src/common/ceph_json.cc
+++ src/common/ceph_json.cc
@@ -1,19 +1,16 @@
 #include "common/ceph_json.h"
 #include "include/utime.h"
 
-// for testing DELETE ME
-#include <fstream>
-
-#include <memory>
-
-#include <include/types.h>
-
 #include <boost/algorithm/string.hpp>
 #include <boost/tokenizer.hpp>
 #include <boost/lexical_cast.hpp>
+#include <include/types.h>
 
 // Enable boost.json's header-only mode ("https://github.com/boostorg/json?tab=readme-ov-file#header-only"):
 #include <boost/json/src.hpp>
+
+#include <memory>
+#include <fstream>
 
 using std::ifstream;
 using std::pair;
@@ -49,7 +46,7 @@ void JSONObjIter::operator++()
     ++cur;
 }
 
-// IMPORTANT: The returned pointer is inteded as NON-OWNING (i.e. the JSONObjIter is responsible for it):
+// IMPORTANT: The returned pointer is intended as NON-OWNING (i.e. the JSONObjIter is responsible for it):
 JSONObj *JSONObjIter::operator*()
 {
   return cur->second.get();
@@ -156,11 +153,11 @@ void JSONObj::handle_value(boost::json::value v)
  // unknown type is not-an-error
 }
 
-void JSONObj::init(JSONObj *parent, boost::json::value data_in, std::string name_in)
+void JSONObj::init(JSONObj *parent_node, boost::json::value data_in, std::string name_in)
 {
-  name = name_in;
-  parent = parent;
+  parent = parent_node;
   data = data_in;
+  name = name_in;
 
   handle_value(data_in);
 
@@ -220,60 +217,53 @@ void JSONParser::handle_data(const char *s, int len)
 // parse a supplied JSON fragment
 bool JSONParser::parse(const char *buf_, int len)
 {
-  if (!buf_) {
-    set_failure();
+  if (!buf_ || 0 >= len) {
     return false;
   }
 
-  string json_string(buf_, len);
+  std::string_view json_string_view(buf_, len);
 
   std::error_code ec; 
-  data = boost::json::parse(json_string, ec);
+  data = boost::json::parse(json_string_view, ec);
   
   if(ec)
-   set_failure();
+   return false;
 
-  if (success) {
+  // recursively evaluate the result:
+  handle_value(data);
 
-    handle_value(data);
+  if (data.is_object() or data.is_array()) 
+   return true;
 
-    if (!data.is_object() &&
-        !data.is_array()) {
+  if (data.is_string()) {
+   val.set(data.as_string(), true);
+   return true;
+  } 
 
-      if (data.is_string()) {
-        val.set(data.as_string(), true);
-      } else {
-	std::string s = boost::json::serialize(data);
+  // For any other kind of value:
+  std::string s = boost::json::serialize(data);
 
-        if (s.size() == (uint64_t)len) { /* Check if entire string is read */
-          val.set(s, false);
-        } else {
-          set_failure();
-        }
-      }
-    }
-  } else {
-    set_failure();
-  }
+  if (s.size() == static_cast<uint64_t>(len)) { // entire string was read
+    val.set(s, false);
+    return true;
+   }
 
-  return success;
+  // Could not parse and convert:
+  return false; 
 }
 
 // parse the internal json_buffer up to len
 bool JSONParser::parse(int len)
 {
+  std::string_view json_string(std::begin(json_buffer), len + std::begin(json_buffer));
+
   std::error_code ec;
+  if(data = boost::json::parse(json_string, ec); ec)
+   return false;
 
-  string json_string = json_buffer.substr(0, len);
+  handle_value(data);
 
-  data = boost::json::parse(json_string, ec);
-
-  if(ec)
-   set_failure();
-  else
-   handle_value(data);
-
-  return success;
+  return true;
 }
 
 // parse the complete internal json_buffer
@@ -284,17 +274,14 @@ bool JSONParser::parse()
   data = boost::json::parse(json_buffer, ec);
 
   if(ec)
-   set_failure();
+   return false;
 
-  if (success)
-    handle_value(data);
-  else
-    set_failure();
+  handle_value(data);
 
-  return success;
+  return true;
 }
 
-// parse a supplied ifstream, for testing. DELETE ME
+// parse a supplied ifstream:
 bool JSONParser::parse(const char *file_name)
 {
  ifstream is(file_name);
@@ -303,24 +290,11 @@ bool JSONParser::parse(const char *file_name)
  data = boost::json::parse(is, ec);
 
  if(ec)
-  set_failure();
+  return false;
 
- if(success)
-  handle_value(data);
- else
- set_failure();
+ handle_value(data);
 
- return success;
-/*
-  ifstream is(file_name);
-  success = read(is, data);
-  if (success)
-    handle_value(data);
-  else
-    set_failure();
-
-  return success;
-*/
+ return true;
 }
 
 
--- src/common/ceph_json.h
+++ src/common/ceph_json.h
@@ -14,8 +14,6 @@
 
 #include "Formatter.h"
 
-
-
 class JSONObj;
 
 class JSONObjIter {
@@ -29,6 +27,9 @@ public:
   void set(const JSONObjIter::map_iter_t &_cur, const JSONObjIter::map_iter_t &_end);
 
   void operator++();
+
+  // IMPORTANT: The returned pointer is intended as NON-OWNING (i.e. JSONObjIter 
+  // is responsible for it):
   JSONObj *operator*();
 
   bool end() const {
@@ -73,10 +74,9 @@ public:
   virtual ~JSONObj() = default;
 
 public:
-  void init(JSONObj *parent, boost::json::value data_in, std::string name_in);
+  void init(JSONObj *parent_node, boost::json::value data_in, std::string name_in);
 
   std::string& get_name() { return name; }
-  
   data_val& get_data_val() { return val; }
 
   const std::string& get_data() { return val.str; }
@@ -113,7 +113,6 @@ class JSONParser : public JSONObj
 {
   int buf_len = 0;
   std::string json_buffer;
-  bool success = true;
 
 public:
   void handle_data(const char *s, int len);
@@ -124,7 +123,6 @@ public:
   bool parse(const char *file_name);
 
   const char *get_json() { return json_buffer.c_str(); }
-  void set_failure() { success = false; }
 };
 
 void encode_json(const char *name, const JSONObj::data_val& v, ceph::Formatter *f);
@@ -139,7 +137,7 @@ public:
 
   JSONDecoder(ceph::buffer::list& bl) {
     if (!parser.parse(bl.c_str(), bl.length())) {
-      std::cout << "JSONDecoder::err()" << std::endl;
+      std::cout << "JSONDecoder::err()" << std::endl;	// JFW: do we still want this here?
       throw JSONDecoder::err("failed to parse JSON input");
     }
   }
