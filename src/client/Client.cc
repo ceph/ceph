fcbf45c21fc54ed39c5cf868b0e625570f74d4ca
Igor Golikov <igolikov@ibm.com>
2025-01-07 15:40:44 +0000

    fix rebase
    Signed-off-by: Igor Golikov <igolikov@ibm.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -8923,7 +8923,7 @@ int Client::chownat(int dirfd, const char *relpath, uid_t new_uid, gid_t new_gid
       goto out;
     }
 #endif
-    return -CEPHFS_ENOENT;
+    return -ENOENT;
   } else {
     filepath path(relpath);
     r = path_walk(path, &in, perms, !(flags & AT_SYMLINK_NOFOLLOW), 0, dirinode);
@@ -12262,7 +12262,7 @@ int Client::statxat(int dirfd, const char *relpath,
       goto out;
     }
 #endif
-    return -CEPHFS_ENOENT;
+    return -ENOENT;
   } else {
     filepath path(relpath);
     r = path_walk(path, &in, perms, !(flags & AT_SYMLINK_NOFOLLOW), mask, dirinode);
