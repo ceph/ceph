50f5524a507500945916c5ffb2cdcd7e743ee872
Vallari Agrawal <vallari.agrawal@ibm.com>
2025-01-07 19:05:35 +0530

    qa/workunits/nvmeof/basic_tests.sh: fix connect-all assert
    There seems to be change in 'nvme list' json output
    which caused failures in asserts after 'nvme connect-all'
    command.
    
    Signed-off-by: Vallari Agrawal <vallari.agrawal@ibm.com>

--- qa/workunits/nvmeof/fio_test.sh
+++ qa/workunits/nvmeof/fio_test.sh
@@ -34,7 +34,7 @@ done
 
 fio_file=$(mktemp -t nvmeof-fio-XXXX)
 all_drives_list=$(sudo nvme list --output-format=json | 
-    jq -r '.Devices | sort_by(.NameSpace) | .[] | select(.ModelNumber == "Ceph bdev Controller") | .DevicePath')
+    jq -r '.Devices[].Subsystems[] | select(.Controllers | all(.ModelNumber == "Ceph bdev Controller")) | .Namespaces | sort_by(.NSID) | .[] | .NameSpace')
 
 # When the script is passed --start_ns and --end_ns (example: `nvmeof_fio_test.sh --start_ns 1 --end_ns 3`), 
 # then fio runs on namespaces only in the defined range (which is 1 to 3 here). 
