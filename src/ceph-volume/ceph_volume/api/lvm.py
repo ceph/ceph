fa461444b02190f3fda5178cf20c146f3bed430d
Guillaume Abrioux <gabrioux@ibm.com>
2025-01-08 13:40:54 +0000

    ceph-volume: fix type annotation in `objectore`
    This commit addresses some python type annotations errors
    in `objectstore` code.
    
    Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>

--- src/ceph-volume/ceph_volume/api/lvm.py
+++ src/ceph-volume/ceph_volume/api/lvm.py
@@ -835,6 +835,7 @@ class Volume:
         self.lv_name: str = ''
         self.lv_uuid: str = ''
         self.vg_name: str = ''
+        self.lv_tags: Dict[str, Any] = {}
         for k, v in kw.items():
             setattr(self, k, v)
         self.lv_api = kw
