0ab1b8b9ac5c3b949d51c90b949476caaa181206
Casey Bodley <cbodley@redhat.com>
2025-01-08 16:48:24 -0500

    rgw: improved error when listing indexless bucket
    without this change, ListObjects would attempt to list index shard
    objects that don't exist and fail with:
    
    > ERROR: S3 error: 404 (NoSuchKey)
    
    after:
    
    > ERROR: S3 error: 405 (MethodNotAllowed): Indexless buckets cannot be listed
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -3196,6 +3196,13 @@ void RGWListBucket::execute(optional_yield y)
     return;
   }
 
+  if (const auto& current_index = s->bucket->get_info().layout.current_index;
+      current_index.layout.type == rgw::BucketIndexType::Indexless) {
+    s->err.message = "Indexless buckets cannot be listed";
+    op_ret = -ERR_METHOD_NOT_ALLOWED;
+    return;
+  }
+
   if (allow_unordered && !delimiter.empty()) {
     ldpp_dout(this, 0) <<
       "ERROR: unordered bucket listing requested with a delimiter" << dendl;
