76bd3d4b5e74c9fdea80320f4ab8cd973c85b353
Casey Bodley <cbodley@redhat.com>
2025-01-08 16:55:19 -0500

    rgw: reject PutBucketLifecycleConfiguration on indexless buckets
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_op.cc
+++ src/rgw/rgw_op.cc
@@ -6264,6 +6264,13 @@ void RGWPutACLs::execute(optional_yield y)
 
 void RGWPutLC::execute(optional_yield y)
 {
+  if (const auto& current_index = s->bucket->get_info().layout.current_index;
+      current_index.layout.type == rgw::BucketIndexType::Indexless) {
+    s->err.message = "Indexless buckets do not support lifecycle policy";
+    op_ret = -ERR_METHOD_NOT_ALLOWED;
+    return;
+  }
+
   bufferlist bl;
   
   RGWLifecycleConfiguration_S3 config(s->cct);
