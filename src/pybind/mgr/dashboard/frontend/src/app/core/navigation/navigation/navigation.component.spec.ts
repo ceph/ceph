8f65f0e905177b0dd212f54d7799fffd9052d4d2
pujaoshahu <pshahu@redhat.com>
2025-01-09 10:44:43 +0530

    mgr/dashboard: Add RGW topic listing in dashboard
    Fixes: https://tracker.ceph.com/issues/69143
    
    Signed-off-by: pujaoshahu <pshahu@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/core/navigation/navigation/navigation.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/navigation/navigation/navigation.component.spec.ts
@@ -137,8 +137,9 @@ describe('NavigationComponent', () => {
         [
           '.tc_menuitem_rgw',
           '.tc_submenuitem_rgw_daemons',
+          '.tc_submenuitem_rgw_users',
           '.tc_submenuitem_rgw_buckets',
-          '.tc_submenuitem_rgw_users'
+          '.tc_submenuitem_rgw_topics'
         ]
       ]
     ];
@@ -185,8 +186,9 @@ describe('NavigationComponent', () => {
         [
           '.tc_menuitem_rgw',
           '.tc_submenuitem_rgw_daemons',
+          '.tc_submenuitem_rgw_users',
           '.tc_submenuitem_rgw_buckets',
-          '.tc_submenuitem_rgw_users'
+          '.tc_submenuitem_rgw_topics'
         ]
       ]
     ];
@@ -246,8 +248,9 @@ describe('NavigationComponent', () => {
         '.tc_submenuitem_block_iscsi': 'iSCSI',
         '.tc_submenuitem_block_nvme': 'NVMe/TCP',
         '.tc_submenuitem_rgw_overview': 'Overview',
-        '.tc_submenuitem_rgw_buckets': 'Buckets',
         '.tc_submenuitem_rgw_users': 'Users',
+        '.tc_submenuitem_rgw_buckets': 'Buckets',
+        '.tc_submenuitem_rgw_topics': 'Topics',
         '.tc_submenuitem_rgw_multi-site': 'Multi-site',
         '.tc_submenuitem_rgw_daemons': 'Gateways',
         '.tc_submenuitem_rgw_nfs': 'NFS',
