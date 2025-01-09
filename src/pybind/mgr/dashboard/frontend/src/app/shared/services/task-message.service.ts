d12be5e7cbe068a718faae01f2f03e2a9e2ee54a
pujaoshahu <pshahu@redhat.com>
2025-01-09 10:44:43 +0530

    mgr/dashboard: Add RGW topic listing in dashboard
    Fixes: https://tracker.ceph.com/issues/69143
    
    Signed-off-by: Puja Shahu <pshahu@redhat.com>
    Signed-off-by: pujaoshahu <pshahu@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/services/task-message.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/task-message.service.ts
@@ -426,6 +426,9 @@ export class TaskMessageService {
     'smb/standalone/remove': this.newTaskMessage(this.commonOperations.remove, (metadata) =>
       this.smbUsersgroups(metadata)
     ),
+    'rgw/topic/delete': this.newTaskMessage(this.commonOperations.delete, (metadata) =>
+      this.topic(metadata)
+    ),
     // Grafana tasks
     'grafana/dashboards/update': this.newTaskMessage(
       this.commonOperations.update,
@@ -606,6 +609,9 @@ export class TaskMessageService {
     return $localize`SMB users and groups access resource '${metadata.usersGroupsId}'`;
   }
 
+  topic(metadata: any) {
+    return $localize`Topic  '${metadata.name}'`;
+  }
   service(metadata: any) {
     return $localize`service '${metadata.service_name}'`;
   }
