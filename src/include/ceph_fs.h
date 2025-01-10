283f744f8d380e7bdb1727aa5379170eeedb0fba
Kotresh HR <khiremat@redhat.com>
2025-01-10 11:59:48 +0530

    mon: Enable referent inodes by default for testing
    Signed-off-by: Kotresh HR <khiremat@redhat.com>

--- src/include/ceph_fs.h
+++ src/include/ceph_fs.h
@@ -296,7 +296,8 @@ struct ceph_mon_subscribe_ack {
 #define CEPH_MDSMAP_GLOBAL_SNAPREALM             (1<<9)  /* use global snaprealm for hardlink snapshots */
 #define CEPH_MDSMAP_REFERENT_INODES              (1<<10)  /* create referent inode for hardlinks to store backtrace */
 #define CEPH_MDSMAP_DEFAULTS (CEPH_MDSMAP_ALLOW_SNAPS | \
-			      CEPH_MDSMAP_ALLOW_MULTIMDS_SNAPS)
+			      CEPH_MDSMAP_ALLOW_MULTIMDS_SNAPS | \
+			      CEPH_MDSMAP_REFERENT_INODES)
 
 /*
  * mds states
