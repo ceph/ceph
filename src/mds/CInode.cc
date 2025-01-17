anshuman-agarwala/ceph-ci

mds: Mark global snaprealm and send snap trace based on use_global_snaprealm option
Mark the snaprealm global on hardlink creation only if
use_global_snaprealm fs option is set. Don't mark it otherwise.

Also if the fs option use_global_snaprealm is set, then send
snap trace from global snaprealm for hardlinks otherwise
aggregate the snap trace from all the hardlinks and send.

Signed-off-by: Kotresh HR <khiremat@redhat.com>

src/mds/CInode.cc
@@ -3212,6 +3212,21 @@ snapid_t CInode::pick_old_inode(snapid_t snap) const
   return 0;
 }
 
+void CInode::get_related_realms(std::vector<SnapRealm*>& related_realms)
+{
+  const std::vector<uint64_t>& referent_inodes = get_projected_inode()->referent_inodes;
+  dout(20) << __func__ << " referent inodes of inode " << *this << " are " << std::hex << referent_inodes << dendl;
+  for (const auto& ri : referent_inodes) {
+    CInode *cur = mdcache->get_inode(ri);
+    if (!cur) {
+      dout(3) << __func__ << " error: referent inode " << std::hex << ri << " of inode " << *this << " not loaded" << dendl;
+      ceph_abort("get_related_realms: referent inode not loaded");
+    }
+    SnapRealm *cur_realm = cur->find_snaprealm();
+    related_realms.push_back(cur_realm);
+  }
+}
+
 void CInode::open_snaprealm(bool nosplit)
 {
   if (!snaprealm) {

ceph/ceph

src/mds/CInode.cc
@@ -3212,6 +3212,21 @@ snapid_t CInode::pick_old_inode(snapid_t snap) const
   return 0;
 }
 
+void CInode::get_related_realms(std::vector<SnapRealm*>& related_realms)
+{
+  const std::vector<uint64_t>& referent_inodes = get_inode()->referent_inodes;
+  dout(20) << __func__ << " referent inodes of inode " << *this << " are " << std::hex << referent_inodes << dendl;
+  for (const auto& ri : referent_inodes) {
+    CInode *cur = mdcache->get_inode(ri);
+    if (!cur) {
+      dout(3) << __func__ << " error: referent inode " << std::hex << ri << " of inode " << *this << " not loaded" << dendl;
+      ceph_abort("get_related_realms: referent inode not loaded");
+    }
+    SnapRealm *cur_realm = cur->find_snaprealm();
+    related_realms.push_back(cur_realm);
+  }
+}
+
 void CInode::open_snaprealm(bool nosplit)
 {
   if (!snaprealm) {