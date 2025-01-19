9123d698985ce59166117f94d5007315ff8e5658
Nitzan Mordechai <nmordech@redhat.com>
2025-01-19 11:17:54 +0000

    monclient: handle quorum using monmap
    Update the previous implementation to manage quorum get/set operations
    using monmap.
    This approach reduces code complexity and avoids redundant checks.
    Additionally, retrieve the monmap from all monitors, not just the leader.
    
    Signed-off-by: Nitzan Mordechai <nmordech@redhat.com>

--- src/mon/MonClient.h
+++ src/mon/MonClient.h
@@ -48,7 +48,6 @@
 #include "auth/AuthClientHandler.h"
 
 class MMonMap;
-class MMonQuorum;
 class MConfig;
 class MMonGetVersionReply;
 class MMonCommandAck;
@@ -174,6 +173,10 @@ public:
     }
     return agreed_quorum.count(rank) > 0;
   }
+  bool empty() const {
+    std::lock_guard l{quorum_l};
+    return quorums.empty();
+  }
   void recalc_agreed_quorum();
   void add_quorum(const entity_addrvec_t& addrs,
       const version_t epoch,
@@ -190,14 +193,13 @@ class AuxConnections {
 public:
   AuxConnections(std::shared_ptr<MonConnection> conn_)
     : conn(std::move(conn_))
-  { sub.want("quorum_change", 0, 0);}
+  { sub.want("monmap", 0, 0);}
 
   std::shared_ptr<MonConnection> get_con() {
     return conn;
   }
 
   void start_conn(epoch_t epoch, const EntityName& entity_name) {
-    sub.got("quorum", 0);
     conn->start(epoch, entity_name);
   }
 
@@ -503,7 +505,6 @@ private:
   bool ms_handle_refused(Connection *con) override { return false; }
 
   void handle_monmap(MMonMap *m);
-  void handle_mon_quorum(MMonQuorum *m);
   void handle_config(MConfig *m);
 
   void handle_auth(MAuthReply *m);
