44c96e417566ad1f0b11e6558e0be78efeb811d3
Radoslaw Zarzynski <rzarzyns@redhat.com>
2025-01-20 19:31:27 +0000

    crimson/osd, test: turn the EC tests on for crimson
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/test/librados/TestCase.cc
+++ src/test/librados/TestCase.cc
@@ -165,7 +165,6 @@ rados_t RadosTestEC::s_cluster = NULL;
 
 void RadosTestEC::SetUpTestCase()
 {
-  SKIP_IF_CRIMSON();
   auto pool_prefix = fmt::format("{}_", ::testing::UnitTest::GetInstance()->current_test_case()->name()); 
   pool_name = get_temp_pool_name(pool_prefix);
   ASSERT_EQ("", create_one_ec_pool(pool_name, &s_cluster));
@@ -173,13 +172,11 @@ void RadosTestEC::SetUpTestCase()
 
 void RadosTestEC::TearDownTestCase()
 {
-  SKIP_IF_CRIMSON();
   ASSERT_EQ(0, destroy_one_ec_pool(pool_name, &s_cluster));
 }
 
 void RadosTestEC::SetUp()
 {
-  SKIP_IF_CRIMSON();
   cluster = RadosTestEC::s_cluster;
   ASSERT_EQ(0, rados_ioctx_create(cluster, pool_name.c_str(), &ioctx));
   nspace = get_temp_pool_name();
@@ -193,7 +190,6 @@ void RadosTestEC::SetUp()
 
 void RadosTestEC::TearDown()
 {
-  SKIP_IF_CRIMSON();
   if (cleanup) {
     cleanup_default_namespace(ioctx);
     cleanup_namespace(ioctx, nspace);
