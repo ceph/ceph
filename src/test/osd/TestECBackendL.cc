dd4248f69acc7b5c05a7a096b55d159b9d919834
Alex Ainscow <aainscow@uk.ibm.com>
2025-01-31 20:58:28 +0000

    osd: test: Split unittest harness for EC into legacy and new code.
    Signed-off-by: Alex Ainscow <aainscow@uk.ibm.com>

--- src/test/osd/TestECBackendL.cc
+++ src/test/osd/TestECBackendL.cc
@@ -16,11 +16,12 @@
 #include <sstream>
 #include <errno.h>
 #include <signal.h>
-#include "osd/ECCommon.h"
-#include "osd/ECBackend.h"
+#include "osd/ECCommonL.h"
+#include "osd/ECBackendL.h"
 #include "gtest/gtest.h"
 
 using namespace std;
+using namespace ECLegacy;
 
 TEST(ECUtil, stripe_info_t)
 {
@@ -28,7 +29,7 @@ TEST(ECUtil, stripe_info_t)
   const unsigned int k = 4;
   const unsigned int m = 2;
 
-  ECUtil::stripe_info_t s(k, m, swidth);
+  ECUtilL::stripe_info_t s(k, m, swidth);
   ASSERT_EQ(s.get_stripe_width(), swidth);
 
   ASSERT_EQ(s.logical_to_next_chunk_offset(0), 0u);
@@ -94,7 +95,7 @@ TEST(ECUtil, offset_length_is_same_stripe)
   const unsigned int k = 4;
   const unsigned int m = 2;
 
-  ECUtil::stripe_info_t s(k, m, swidth);
+  ECUtilL::stripe_info_t s(k, m, swidth);
   ASSERT_EQ(s.get_stripe_width(), swidth);
   ASSERT_EQ(s.get_chunk_size(), schunk);
 
@@ -170,14 +171,14 @@ TEST(ECCommon, get_min_want_to_read_shards)
   const unsigned int k = 4;
   const unsigned int m = 2;
 
-  ECUtil::stripe_info_t s(k, m, swidth);
+  ECUtilL::stripe_info_t s(k, m, swidth);
   ASSERT_EQ(s.get_stripe_width(), swidth);
   ASSERT_EQ(s.get_chunk_size(), 1024);
 
   // read nothing at the very beginning
   {
     std::set<int> want_to_read;
-    ECCommon::ReadPipeline::get_min_want_to_read_shards(
+    ECCommonL::ReadPipeline::get_min_want_to_read_shards(
       0, 0, s, &want_to_read);
     ASSERT_TRUE(want_to_read == std::set<int>{});
   }
@@ -185,7 +186,7 @@ TEST(ECCommon, get_min_want_to_read_shards)
   // read nothing at the middle (0-sized partial read)
   {
     std::set<int> want_to_read;
-    ECCommon::ReadPipeline::get_min_want_to_read_shards(
+    ECCommonL::ReadPipeline::get_min_want_to_read_shards(
       2048, 0, s, &want_to_read);
     ASSERT_TRUE(want_to_read == std::set<int>{});
   }
@@ -193,7 +194,7 @@ TEST(ECCommon, get_min_want_to_read_shards)
   // read not-so-many (< chunk_size) bytes at the middle (partial read)
   {
     std::set<int> want_to_read;
-    ECCommon::ReadPipeline::get_min_want_to_read_shards(
+    ECCommonL::ReadPipeline::get_min_want_to_read_shards(
       2048, 42, s, &want_to_read);
     ASSERT_TRUE(want_to_read == std::set<int>{2});
   }
@@ -201,7 +202,7 @@ TEST(ECCommon, get_min_want_to_read_shards)
   // read more (> chunk_size) bytes at the middle (partial read)
   {
     std::set<int> want_to_read;
-    ECCommon::ReadPipeline::get_min_want_to_read_shards(
+    ECCommonL::ReadPipeline::get_min_want_to_read_shards(
       1024, 1024+42, s, &want_to_read);
     // extra () due to a language / macro limitation
     ASSERT_TRUE(want_to_read == (std::set<int>{1, 2}));
@@ -210,7 +211,7 @@ TEST(ECCommon, get_min_want_to_read_shards)
   // full stripe except last chunk
   {
     std::set<int> want_to_read;
-    ECCommon::ReadPipeline::get_min_want_to_read_shards(
+    ECCommonL::ReadPipeline::get_min_want_to_read_shards(
       0, 3*1024, s, &want_to_read);
     // extra () due to a language / macro limitation
     ASSERT_TRUE(want_to_read == (std::set<int>{0, 1, 2}));
@@ -219,7 +220,7 @@ TEST(ECCommon, get_min_want_to_read_shards)
   // full stripe except 1st chunk
   {
     std::set<int> want_to_read;
-    ECCommon::ReadPipeline::get_min_want_to_read_shards(
+    ECCommonL::ReadPipeline::get_min_want_to_read_shards(
       1024, swidth-1024, s, &want_to_read);
     // extra () due to a language / macro limitation
     ASSERT_TRUE(want_to_read == (std::set<int>{1, 2, 3}));
@@ -228,7 +229,7 @@ TEST(ECCommon, get_min_want_to_read_shards)
   // large, multi-stripe read starting just after 1st chunk
   {
     std::set<int> want_to_read;
-    ECCommon::ReadPipeline::get_min_want_to_read_shards(
+    ECCommonL::ReadPipeline::get_min_want_to_read_shards(
       1024, swidth*42, s, &want_to_read);
     // extra () due to a language / macro limitation
     ASSERT_TRUE(want_to_read == (std::set<int>{0, 1, 2, 3}));
@@ -237,7 +238,7 @@ TEST(ECCommon, get_min_want_to_read_shards)
   // large read from the beginning
   {
     std::set<int> want_to_read;
-    ECCommon::ReadPipeline::get_min_want_to_read_shards(
+    ECCommonL::ReadPipeline::get_min_want_to_read_shards(
       0, swidth*42, s, &want_to_read);
     // extra () due to a language / macro limitation
     ASSERT_TRUE(want_to_read == (std::set<int>{0, 1, 2, 3}));
@@ -250,7 +251,7 @@ TEST(ECCommon, get_min_want_to_read_shards_bug67087)
   const unsigned int k = 4;
   const unsigned int m = 2;
 
-  ECUtil::stripe_info_t s(k, m, swidth);
+  ECUtilL::stripe_info_t s(k, m, swidth);
   ASSERT_EQ(s.get_stripe_width(), swidth);
   ASSERT_EQ(s.get_chunk_size(), 1024);
 
@@ -259,10 +260,10 @@ TEST(ECCommon, get_min_want_to_read_shards_bug67087)
   // multitple calls with the same want_to_read can happen during
   // multi-region reads.
   {
-    ECCommon::ReadPipeline::get_min_want_to_read_shards(
+    ECCommonL::ReadPipeline::get_min_want_to_read_shards(
       512, 512, s, &want_to_read);
     ASSERT_EQ(want_to_read, std::set<int>{0});
-    ECCommon::ReadPipeline::get_min_want_to_read_shards(
+    ECCommonL::ReadPipeline::get_min_want_to_read_shards(
       512+16*1024, 512, s, &want_to_read);
     ASSERT_EQ(want_to_read, std::set<int>{0});
   }
