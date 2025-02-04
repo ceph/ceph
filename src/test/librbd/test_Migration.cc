49075bc4c28b198e089a56176a2047c20b1d9208
Ramana Raja <rraja@redhat.com>
2025-02-03 19:55:08 -0500

    librbd: remove mirror APIs that change mirror group membership
    Remove mirror APIs, group_image_add() and group_image_remove() that
    are never called as we don't allow adding/removing images to/from a
    mirrored group.
    
    Signed-off-by: Ramana Raja <rraja@redhat.com>

--- src/test/librbd/test_Migration.cc
+++ src/test/librbd/test_Migration.cc
@@ -743,7 +743,7 @@ TEST_F(TestMigration, Group)
 
   ASSERT_EQ(0, librbd::api::Group<>::create(m_ioctx, "123"));
   ASSERT_EQ(0, librbd::api::Group<>::image_add(m_ioctx, "123", m_ioctx,
-                                               m_image_name.c_str(), 0));
+                                               m_image_name.c_str()));
   librbd::group_info_t info;
   ASSERT_EQ(0, librbd::api::Group<>::image_get_group(m_ictx, &info));
 
@@ -755,7 +755,7 @@ TEST_F(TestMigration, Group)
   ASSERT_EQ(info.name, "123");
 
   ASSERT_EQ(0, librbd::api::Group<>::image_remove(m_ioctx, "123", m_ioctx,
-                                                  name.c_str(), 0));
+                                                  name.c_str()));
   ASSERT_EQ(0, librbd::api::Group<>::remove(m_ioctx, "123"));
 }
 
@@ -765,7 +765,7 @@ TEST_F(TestMigration, GroupAbort)
 
   ASSERT_EQ(0, librbd::api::Group<>::create(m_ioctx, "123"));
   ASSERT_EQ(0, librbd::api::Group<>::image_add(m_ioctx, "123", m_ioctx,
-                                               m_image_name.c_str(), 0));
+                                               m_image_name.c_str()));
   librbd::group_info_t info;
   ASSERT_EQ(0, librbd::api::Group<>::image_get_group(m_ictx, &info));
 
@@ -783,7 +783,7 @@ TEST_F(TestMigration, GroupAbort)
   ASSERT_EQ(info.name, "123");
 
   ASSERT_EQ(0, librbd::api::Group<>::image_remove(m_ioctx, "123", m_ioctx,
-                                                  m_image_name.c_str(), 0));
+                                                  m_image_name.c_str()));
   ASSERT_EQ(0, librbd::api::Group<>::remove(m_ioctx, "123"));
 }
 
