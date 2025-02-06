bc09ca0ba2b6f7615ee673964c2b89dd8f599967
N Balachandran <nithya.balachandran@ibm.com>
2025-02-06 20:03:02 +0530

    rbd: rbd group info now displays mirroring info
    The "rbd group info" will now display the mirroring mode, global-id
    and whether primary for mirror enabled groups.
    
    Signed-off-by: N Balachandran <nithya.balachandran@ibm.com>

--- src/librbd/mirror/GroupGetInfoRequest.h
+++ src/librbd/mirror/GroupGetInfoRequest.h
@@ -1,14 +1,14 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:nil -*-
 // vim: ts=8 sw=2 sts=2 expandtab
 
-#ifndef CEPH_LIBRBD_MIRROR_SNAPSHOT_GROUP_GET_INFO_REQUEST_H
-#define CEPH_LIBRBD_MIRROR_SNAPSHOT_GROUP_GET_INFO_REQUEST_H
+#ifndef CEPH_LIBRBD_MIRROR_GROUP_GET_INFO_REQUEST_H
+#define CEPH_LIBRBD_MIRROR_GROUP_GET_INFO_REQUEST_H
 
 #include "include/buffer.h"
 #include "include/types.h"
 #include "include/rados/librados.hpp"
-#include "include/rbd/librbd.hpp"
 #include "librbd/ImageCtx.h"
+#include "librbd/mirror/Types.h"
 #include "cls/rbd/cls_rbd_types.h"
 
 struct Context;
@@ -18,25 +18,27 @@ namespace librbd {
 struct ImageCtx;
 
 namespace mirror {
-namespace snapshot {
 
 template <typename ImageCtxT = librbd::ImageCtx>
 class GroupGetInfoRequest {
 public:
   static GroupGetInfoRequest *create(librados::IoCtx& group_ioctx,
                                      const std::string& group_name,
-                                     mirror_group_info_t *mirror_group_info,
+                                     cls::rbd::MirrorGroup *mirror_group,
+                                     PromotionState *promotion_state,
                                      Context *on_finish) {
-    return new GroupGetInfoRequest(group_ioctx, group_name, mirror_group_info,
-                                   on_finish);
+    return new GroupGetInfoRequest(group_ioctx, group_name, mirror_group,
+                                   promotion_state, on_finish);
   }
 
   GroupGetInfoRequest(librados::IoCtx& group_ioctx,
                       const std::string& group_name,
-                      mirror_group_info_t *mirror_group_info,
+                      cls::rbd::MirrorGroup *mirror_group,
+                      PromotionState *promotion_state,
                       Context *on_finish)
     : m_group_ioctx(group_ioctx), m_group_name(group_name),
-      m_mirror_group_info(mirror_group_info), m_on_finish(on_finish) {
+      m_mirror_group(mirror_group), m_promotion_state(promotion_state),
+      m_on_finish(on_finish) {
   }
 
   void send();
@@ -65,7 +67,8 @@ private:
   librados::IoCtx m_group_ioctx;
   const std::string m_group_name;
   std::string m_group_id;
-  mirror_group_info_t *m_mirror_group_info;
+  cls::rbd::MirrorGroup *m_mirror_group;
+  PromotionState *m_promotion_state;
   std::vector<cls::rbd::GroupSnapshot> m_group_snaps;
   Context *m_on_finish;
 
@@ -83,10 +86,9 @@ private:
   void finish(int r);
 };
 
-} // namespace snapshot
 } // namespace mirror
 } // namespace librbd
 
-extern template class librbd::mirror::snapshot::GroupGetInfoRequest<librbd::ImageCtx>;
+extern template class librbd::mirror::GroupGetInfoRequest<librbd::ImageCtx>;
 
-#endif // CEPH_LIBRBD_MIRROR_SNAPSHOT_GROUP_GET_INFO_REQUEST_H
+#endif // CEPH_LIBRBD_MIRROR_GROUP_GET_INFO_REQUEST_H
