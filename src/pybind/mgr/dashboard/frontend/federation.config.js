9e88bd309f720fea2bd913f169c2191f3b8547b5
Ivo Almeida <ialmeida@redhat.com>
2025-02-12 10:24:17 +0000

    mgr/dashboard: set up dashboard as a app shell
    * Replaced webpack by esbuild
    * Added native-federation packages
    * Set up dashboard to act as an app shell
    
    Fixes: https://tracker.ceph.com/issues/69922
    
    Signed-off-by: Ivo Almeida <ialmeida@redhat.com>

---
+++ src/pybind/mgr/dashboard/frontend/federation.config.js
@@ -0,0 +1,20 @@
+const { withNativeFederation, shareAll } = require('@angular-architects/native-federation/config');
+
+module.exports = withNativeFederation({
+
+  shared: {
+    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+  },
+
+  skip: [
+    'rxjs/ajax',
+    'rxjs/fetch',
+    'rxjs/testing',
+    'rxjs/webSocket',
+    // Add further packages you don't need at runtime
+  ]
+
+  // Please read our FAQ about sharing libs:
+  // https://shorturl.at/jmzH0
+  
+});
