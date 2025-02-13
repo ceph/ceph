768fe6d9b8259602c6f92302286664815f44c339
Samuel Just <sjust@redhat.com>
2025-02-13 03:54:28 +0000

    test/osd/TestMClockScheduler: create_item should pass prio < cutoff
    Cutoff is set to 12, so let's pass something < 12 rather than 12.
    Comments in some tests suggest that the intent is for create_item
    to create things in the mclock queue rather than the high_queue.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/test/osd/TestMClockScheduler.cc
+++ src/test/osd/TestMClockScheduler.cc
@@ -93,7 +93,7 @@ OpSchedulerItem create_item(
   return OpSchedulerItem(
     std::make_unique<mClockSchedulerTest::MockDmclockItem>(
       std::forward<Args>(args)...),
-    12, 12,
+    12, 1,
     utime_t(), owner, e);
 }
 
