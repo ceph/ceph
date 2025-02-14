e12400d4d875ede4241f96736deb448a74121fe0
John Mulligan <jmulligan@redhat.com>
2025-02-14 14:50:42 -0500

    script/build-with-container: add a common packages target
    Add a `packages` target to build-with-container.py that requests a build
    of packages, whatever package type is native to the distro selected.
    For example `./src/script/build-with-container.py -d ubuntu22.04 -e
    packages` will automatically select a deb packages build where
    `./src/script/build-with-container.py -d centos9 -e packages` will
    trigger rpm packages to be built. The underlying package-type specific
    targets remain unchanged.
    
    Signed-off-by: John Mulligan <jmulligan@redhat.com>
    (cherry picked from commit 37b7d509c59348ae11badd6673cb49ce9ce303fa)

--- src/script/build-with-container.py
+++ src/script/build-with-container.py
@@ -178,6 +178,12 @@ class DistroKind(StrEnum):
         return cls.aliases()[value]
 
     @classmethod
+    def uses_rpmbuild(cls):
+        # right now this is the same as uses_dnf, but perhaps not always
+        # let's be specific in our interface
+        return cls.uses_dnf()  # but lazy in the implementation
+
+    @classmethod
     def aliases(cls):
         return {
             str(cls.CENTOS10): cls.CENTOS10,
