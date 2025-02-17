7ef164ebcbd6bc961eccb3d77096d25e7f50a3ba
Matan Breizman <mbreizma@redhat.com>
2025-02-17 10:08:11 +0000

    crimson: fix -Wmissing-template-arg-list-after-template-kw
    See: https://github.com/llvm/llvm-project/commit/f46d1463b835560d90ad3ac02b63c771e4ebe566
    ```
    common/interruptible_future.h:1182:16: error: a template argument list is expected after a name prefixed by the template keyword [-Wmissing-template-arg-list-after-template-kw]
     1182 |     ).template handle_interruption(std::move(efunc));
          |                ^
    1 error generated.
    ```
    
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>

--- src/crimson/os/seastore/onode_manager/staged-fltree/stages/stage.h
+++ src/crimson/os/seastore/onode_manager/staged-fltree/stages/stage.h
@@ -1153,7 +1153,7 @@ struct staged {
       return insert_size(key, value);
     } else {
       assert(stage < STAGE);
-      return NXT_STAGE_T::template insert_size_at(stage, key, value);
+      return NXT_STAGE_T::insert_size_at(stage, key, value);
     }
   }
 
@@ -1406,7 +1406,7 @@ struct staged {
         return container_t::insert_at(
             mut, container, key, value, 0, _insert_size, p_left_bound);
       } else {
-        auto range = container_t::template insert_prefix_at(
+        auto range = container_t::insert_prefix_at(
             mut, container, key, 0, _insert_size, p_left_bound);
         return NXT_STAGE_T::template insert_new<KT>(mut, range, key, value);
       }
