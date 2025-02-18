b9947bacdfd3cbedcf7a6b93af471c40f3a9c65b
Kotresh HR <khiremat@redhat.com>
2025-02-18 16:47:53 +0530

    mds: Hardlink referent inode plumbing work
    The linkage_t struct changes:
      - Add referent_ino(inodeno_t) and referent_inode(CInode *) to the
        linkage_t. These two fields becomes the core and identifies the
        linkage as referent remote.
      - Add 'is_referent_remote()' to check if it's referent remote
        and modify 'is_remote()', 'is_null()' check accordingly.
      - Add functions to access these fields.
    
    Add the following new functions to link referent inode.
      1. CDentry::push_projected_linkage - with referent_inode
      2. CDir::link_null_referent_inode
      3. CDir::link_referent_inode
    
    Modify the following existing functions to link/unlink referent inode
      1. CDentry::link_remote
      2. CDentry::unlink_remote
      3. CDentry::pop_projected_linakge
      4. CDentry::CDentry
      5. CDir::add_remote_dentry
      6. CDir::link_inode_work
      7. CDir::unlink_inode_work
    
    Fixes: https://tracker.ceph.com/issues/54205
    Signed-off-by: Kotresh HR <khiremat@redhat.com>

--- src/mds/CDentry.h
+++ src/mds/CDentry.h
@@ -80,20 +80,26 @@ public:
     CInode *inode = nullptr;
     inodeno_t remote_ino = 0;
     unsigned char remote_d_type = 0;
+    CInode *referent_inode = nullptr;
+    inodeno_t referent_ino = 0;
     
     linkage_t() {}
 
     // dentry type is primary || remote || null
     // inode ptr is required for primary, optional for remote, undefined for null
     bool is_primary() const { return remote_ino == 0 && inode != 0; }
-    bool is_remote() const { return remote_ino > 0; }
-    bool is_null() const { return remote_ino == 0 && inode == 0; }
+    bool is_remote() const { return remote_ino > 0 && referent_inode == nullptr && referent_ino == 0; }
+    bool is_null() const { return remote_ino == 0 && inode == 0 && referent_ino == 0 && referent_inode == nullptr; }
+    bool is_referent_remote() const {return remote_ino > 0 && referent_ino != 0 && referent_inode != nullptr;}
 
     CInode *get_inode() { return inode; }
     const CInode *get_inode() const { return inode; }
     inodeno_t get_remote_ino() const { return remote_ino; }
     unsigned char get_remote_d_type() const { return remote_d_type; }
     std::string get_remote_d_type_string() const;
+    CInode *get_referent_inode() { return referent_inode; }
+    const CInode *get_referent_inode() const { return referent_inode; }
+    inodeno_t get_referent_ino() const { return referent_ino; }
 
     void set_remote(inodeno_t ino, unsigned char d_type) {
       remote_ino = ino;
@@ -137,8 +143,8 @@ public:
   {}
   CDentry(std::string_view n, __u32 h,
           mempool::mds_co::string alternate_name,
-          inodeno_t ino, unsigned char dt,
-	  snapid_t f, snapid_t l) :
+          inodeno_t ino, inodeno_t referent_ino,
+	  unsigned char dt, snapid_t f, snapid_t l) :
     hash(h),
     first(f), last(l),
     item_dirty(this),
@@ -149,6 +155,7 @@ public:
   {
     linkage.remote_ino = ino;
     linkage.remote_d_type = dt;
+    linkage.referent_ino = referent_ino;
   }
 
   ~CDentry() override {
@@ -210,6 +217,7 @@ public:
     p->remote_d_type = d_type;
   }
   void push_projected_linkage(CInode *inode); 
+  void push_projected_linkage(CInode *referent_inode, inodeno_t remote_ino, inodeno_t referent_ino);
   linkage_t *pop_projected_linkage();
 
   bool is_projected() const { return !projected.empty(); }
@@ -257,7 +265,7 @@ public:
   int get_num_dir_auth_pins() const;
   
   // remote links
-  void link_remote(linkage_t *dnl, CInode *in);
+  void link_remote(linkage_t *dnl, CInode *remote_in, CInode *ref_in=nullptr);
   void unlink_remote(linkage_t *dnl);
   
   // copy cons
