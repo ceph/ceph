9cdbb389c1726a872f7d12cf3383d1e2ca4313c1
Oshry Avraham <oshry.dev@gmail.com>
2025-02-18 13:25:19 +0200

    rgw/lua: Update background thread to handle config changes
    Ensure Lua background thread updates memory and runtime limits when config changes.
    - Added setters/getters for max memory and runtime limits in lua_state_guard.
    - Implemented dynamic updates for memory and runtime limits in the background thread.
    - Reset Lua state when memory limit require restart.
    - Added a memory leak check after Lua cleanup to ensure proper resource deallocation.
    
    Signed-off-by: Oshry Avraham <oshry.dev@gmail.com>

--- src/rgw/rgw_lua_utils.h
+++ src/rgw/rgw_lua_utils.h
@@ -68,8 +68,9 @@ inline void unsetglobal(lua_State* L, const char* name)
 void stack_dump(lua_State* L);
 
 class lua_state_guard {
-  const std::size_t max_memory;
-  const std::chrono::milliseconds max_runtime;
+  std::size_t max_memory;
+  std::size_t mem_in_use;
+  std::chrono::milliseconds max_runtime;
   ceph::real_clock::time_point start_time;
   const DoutPrefixProvider* const dpp;
   lua_State* const state;
@@ -83,6 +84,13 @@ class lua_state_guard {
   ~lua_state_guard();
   lua_State* get() { return state; }
   void reset_start_time() { start_time = ceph::real_clock::now(); }
+
+  std::size_t get_max_memory() const { return max_memory; }
+  std::size_t get_mem_in_use() const { return mem_in_use; }
+  std::chrono::milliseconds get_max_runtime() const { return max_runtime; }
+  bool set_max_memory(std::size_t _max_memory);
+  void set_mem_in_use(std::size_t _mem_in_use);
+  void set_max_runtime(std::uint64_t _max_runtime);
 };
 
 int dostring(lua_State* L, const char* str);
