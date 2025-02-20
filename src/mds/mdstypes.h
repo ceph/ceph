98edb020538e383258edf0fae3bea4969d6152a0
Venky Shankar <vshankar@redhat.com>
2025-02-20 13:07:17 +0000

    mds: add blockdiff operation support
    Fixes: http://tracker.ceph.com/issues/69791
    Signed-off-by: Venky Shankar <vshankar@redhat.com>
    (cherry picked from commit 8cc05b172551826eac5b03f086cc872df9584837)
    
     Conflicts:
    	src/common/ceph_strings.cc
    	src/common/options/mds.yaml.in
    	src/mds/mdstypes.cc
    	src/mds/mdstypes.h
    
    Missing EstimatedReplayTime structure in squid and some features.

--- src/mds/mdstypes.h
+++ src/mds/mdstypes.h
@@ -1029,4 +1029,17 @@ inline bool operator==(const MDSCacheObjectInfo& l, const MDSCacheObjectInfo& r)
 }
 WRITE_CLASS_ENCODER(MDSCacheObjectInfo)
 
+struct BlockDiff {
+  int rval;
+  uint64_t scan_idx;
+  interval_set<uint64_t> blocks;
+
+  void encode(ceph::buffer::list& bl) const;
+  void decode(ceph::buffer::list::const_iterator& p);
+  void dump(ceph::Formatter *f) const;
+  static void generate_test_instances(std::list<BlockDiff*>& ls);
+  void print(std::ostream& out) const;
+};
+WRITE_CLASS_ENCODER(BlockDiff);
+
 #endif
