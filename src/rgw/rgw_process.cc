374ddbc0e8767feda1d596517451293371211cc5
kchheda3 <kchheda3@bloomberg.net>
2025-02-20 13:53:44 -0500

    rgw/log: Fix crash during shutdown with ops-log enable.
    Ensure the env.olog member variable is destructed at last, once all the frontends have stopped accepting new connections and have finished processing the existing connections.
    
    Signed-off-by: kchheda3 <kchheda3@bloomberg.net>

--- src/rgw/rgw_process.cc
+++ src/rgw/rgw_process.cc
@@ -342,7 +342,8 @@ int process_request(const RGWProcessEnv& penv,
           "WARNING: failed to execute pre request script. "
           "error: " << rc << dendl;
       } else {
-        rc = rgw::lua::request::execute(driver, rest, penv.olog, s, op, script);
+        rc = rgw::lua::request::execute(driver, rest, penv.olog.get(), s, op,
+                                        script);
         if (rc < 0) {
           ldpp_dout(op, 5) <<
             "WARNING: failed to execute pre request script. "
@@ -440,7 +441,8 @@ done:
           "WARNING: failed to read post request script. "
           "error: " << rc << dendl;
       } else {
-        rc = rgw::lua::request::execute(driver, rest, penv.olog, s, op, script);
+        rc = rgw::lua::request::execute(driver, rest, penv.olog.get(), s, op,
+                                        script);
         if (rc < 0) {
           ldpp_dout(op, 5) <<
             "WARNING: failed to execute post request script. "
@@ -459,7 +461,7 @@ done:
     perfcounter->inc(l_rgw_qactive, -1);
   }
   if (should_log) {
-    rgw_log_op(rest, s, op, penv.olog);
+    rgw_log_op(rest, s, op, penv.olog.get());
   }
 
   if (op) {
