3bb6add86714fbf2fa81d8f10bd51740728c4ce1
Seena Fallah <seenafallah@gmail.com>
2025-02-21 00:57:25 +0100

    rgw: support s3ReplicateTags perm on destination bucket for replication
    Check for tag replication permission on destination bucket, so if
    there was an explicit deny, donot include tags in the replicated
    object.
    
    Signed-off-by: Seena Fallah <seenafallah@gmail.com>
    (cherry picked from commit 3fb1671520d62ce707ebc15e8f7874540b7e2aaa)

--- src/rgw/driver/rados/rgw_rados.cc
+++ src/rgw/driver/rados/rgw_rados.cc
@@ -4319,7 +4319,8 @@ int RGWRados::fetch_remote_obj(RGWObjectCtx& dest_obj_ctx,
                const rgw_obj& stat_dest_obj,
                std::optional<rgw_zone_set_entry> source_trace_entry,
                rgw_zone_set *zones_trace,
-               std::optional<uint64_t>* bytes_transferred)
+               std::optional<uint64_t>* bytes_transferred,
+               bool keep_tags)
 {
   /* source is in a different zonegroup, copy from there */
 
@@ -4598,6 +4599,10 @@ int RGWRados::fetch_remote_obj(RGWObjectCtx& dest_obj_ctx,
     set_copy_attrs(cb.get_attrs(), attrs, attrs_mod);
   } else {
     attrs = cb.get_attrs();
+  }
+
+  if (!keep_tags) {
+    attrs.erase(RGW_ATTR_TAGS);
   }
 
   if (copy_if_newer) {
