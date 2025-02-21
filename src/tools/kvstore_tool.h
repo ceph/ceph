71d5e90c44b2c1b0a3865c900e2d84e92e16160b
Max Kellermann <max.kellermann@ionos.com>
2025-02-21 15:50:24 +0100

    tools/kvstore_tool: make load_bluestore() `private`
    This method is only used internally.
    
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/tools/kvstore_tool.h
+++ src/tools/kvstore_tool.h
@@ -47,7 +47,6 @@ public:
             bool read_only,
 	    bool need_open_db = true,
 	    bool need_stats = false);
-  int load_bluestore(const std::string& path, bool read_only, bool need_open_db);
   uint32_t traverse(const std::string& prefix,
                     const bool do_crc,
                     const bool do_value_dump,
@@ -80,4 +79,7 @@ public:
 
   int print_stats() const;
   int build_size_histogram(const std::string& prefix) const;
+
+private:
+  int load_bluestore(const std::string& path, bool read_only, bool need_open_db);
 };
