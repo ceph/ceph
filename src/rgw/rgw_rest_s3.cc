a686297a1a4ebb3239d1969c890a1636f2220249
Yehuda Sadeh <ysadehwe@ibm.com>
2025-02-24 16:31:42 -0500

    auth: propagate ceph context to encrypt/decrypt
    Signed-off-by: Yehuda Sadeh <ysadehwe@ibm.com>
    Change-Id: 3999706c-f5a7-4ffc-bafe-db1853bdda00
    (cherry picked from commit 7a3dbaf63b851f78a97e7e60d2841832dbad5dc0)

--- src/rgw/rgw_rest_s3.cc
+++ src/rgw/rgw_rest_s3.cc
@@ -7355,7 +7355,7 @@ rgw::auth::s3::STSEngine::get_session_token(const DoutPrefixProvider* dpp, const
   buffer::list en_input, dec_output;
   en_input = buffer::list::static_from_string(decodedSessionToken);
 
-  ret = keyhandler->decrypt(en_input, dec_output, &error);
+  ret = keyhandler->decrypt(cct, en_input, dec_output, &error);
   if (ret < 0) {
     ldpp_dout(dpp, 0) << "ERROR: Decryption failed: " << error << dendl;
     return -EPERM;
