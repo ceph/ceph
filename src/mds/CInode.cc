8ecc7cf9ccef21cc7a945e381bdd515b1ab89616
Kotresh HR <khiremat@redhat.com>
2025-02-25 02:52:42 +0530

    multimds: Consistent view of referent inode list
    Encode/Decode referent inode list with CEPH_LOCK_ILINK
    lock along with ctime and nlink. This solves the consistent
    view of the referent inode list to all it's inode replica mdses.
    
    Fixes: https://tracker.ceph.com/issues/54205
    Signed-off-by: Kotresh HR <khiremat@redhat.com>
    (cherry picked from commit 0ed8c2a2b1c106754fb7a1ddb35d39b7c48bcd28)

--- src/mds/CInode.cc
+++ src/mds/CInode.cc
@@ -1731,10 +1731,11 @@ void CInode::decode_lock_iauth(bufferlist::const_iterator& p)
 
 void CInode::encode_lock_ilink(bufferlist& bl)
 {
-  ENCODE_START(1, 1, bl);
+  ENCODE_START(2, 1, bl);
   encode(get_inode()->version, bl);
   encode(get_inode()->ctime, bl);
   encode(get_inode()->nlink, bl);
+  encode(get_inode()->referent_inodes, bl);
   ENCODE_FINISH(bl);
 }
 
@@ -1748,6 +1749,8 @@ void CInode::decode_lock_ilink(bufferlist::const_iterator& p)
   decode(tm, p);
   if (_inode->ctime < tm) _inode->ctime = tm;
   decode(_inode->nlink, p);
+  if (struct_v >= 2)
+    decode(_inode->referent_inodes, p);
   DECODE_FINISH(p);
   reset_inode(std::move(_inode));
 }
