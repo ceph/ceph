c86da768ce1a8ea40c97747d4b7fa65c827cd76e
Ramana Raja <rraja@redhat.com>
2025-02-28 16:49:27 -0500

    librbd/api: set `image_snap_name` as empty string for mirror gp snap
    The member image snapshots of a mirror group snap do not share a
    common name unlike those of a user group snap. So set the
    `image_snap_name` to an empty string.
    
    Signed-off-by: Ramana Raja <rraja@redhat.com>

--- src/librbd/api/Group.cc
+++ src/librbd/api/Group.cc
@@ -260,7 +260,8 @@ int GroupSnapshot_to_group_snap_info2(
     group_snap->namespace_type = RBD_GROUP_SNAP_NAMESPACE_TYPE_USER;
   }
 
-  if (!image_snaps.empty()) {
+  if (!image_snaps.empty() &&
+      group_snap->namespace_type == RBD_GROUP_SNAP_NAMESPACE_TYPE_USER) {
     group_snap->image_snap_name = calc_ind_image_snap_name(
         group_ioctx.get_id(), group_id, cls_group_snap.id);
   }
