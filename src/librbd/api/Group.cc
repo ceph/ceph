c51163b61802200de6c188f3e04dc28e07897e7a
Ramana Raja <rraja@redhat.com>
2025-02-28 16:19:03 -0500

    librbd/api: set group snap's namespace type to mirror
    ... for a mirror group snap.
    
    Signed-off-by: Ramana Raja <rraja@redhat.com>

--- src/librbd/api/Group.cc
+++ src/librbd/api/Group.cc
@@ -256,7 +256,9 @@ int GroupSnapshot_to_group_snap_info2(
   group_snap->id = cls_group_snap.id;
   group_snap->name = cls_group_snap.name;
   group_snap->state = static_cast<group_snap_state_t>(cls_group_snap.state);
-  group_snap->namespace_type = RBD_GROUP_SNAP_NAMESPACE_TYPE_USER;
+  group_snap->namespace_type = static_cast<group_snap_namespace_type_t>(
+    cls::rbd::get_group_snap_namespace_type(cls_group_snap.snapshot_namespace));
+
   if (!image_snaps.empty()) {
     group_snap->image_snap_name = calc_ind_image_snap_name(
         group_ioctx.get_id(), group_id, cls_group_snap.id);
