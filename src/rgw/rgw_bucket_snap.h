99d7686a7056b836eaed70b894fc4a2b5bb4667f
Yehuda Sadeh <ysadehwe@ibm.com>
2025-02-28 11:33:51 -0500

    rgw: snap remove initial work
    Signed-off-by: Yehuda Sadeh <ysadehwe@ibm.com>

--- src/rgw/rgw_bucket_snap.h
+++ src/rgw/rgw_bucket_snap.h
@@ -18,6 +18,7 @@ class RGWBucketSnapMgr
   rgw_bucket_snap_id cur_snap{rgw_bucket_snap_id::SNAP_MIN};
 
   std::map<rgw_bucket_snap_id, rgw_bucket_snap> snaps;
+  std::map<rgw_bucket_snap_id, rgw_bucket_snap> rm_snaps;
 
   std::map<std::string, rgw_bucket_snap_id> names_to_ids;
 
@@ -29,6 +30,7 @@ public:
     encode(enabled, bl);
     encode(cur_snap, bl);
     encode(snaps, bl);
+    encode(rm_snaps, bl);
     encode(names_to_ids, bl);
     ENCODE_FINISH(bl);
   }
@@ -38,6 +40,7 @@ public:
     decode(enabled, bl);
     decode(cur_snap, bl);
     decode(snaps, bl);
+    decode(rm_snaps, bl);
     decode(names_to_ids, bl);
     DECODE_FINISH(bl);
   }
@@ -49,6 +52,24 @@ public:
   }
 
   int create_snap(const rgw_bucket_snap_info& info);
+  int remove_snap(rgw_bucket_snap_id snap_id);
+
+  /* check if there is a live snapshot in the range
+   * this is used to check if an object still needs to
+   * exist, when min would be the snapshot where it was
+   * created and max would be the one it was removed
+   */
+  bool live_snapshot_at_range(rgw_bucket_snap_id min, rgw_bucket_snap_id max) const;
+
+  bool find_snap(const std::string& snap_name, rgw_bucket_snap_id *snap_id) const {
+    auto iter = names_to_ids.find(snap_name);
+    if (iter == names_to_ids.end()) {
+      return false;
+    }
+
+    *snap_id = iter->second;
+    return true;
+  }
 
   const std::map<rgw_bucket_snap_id, rgw_bucket_snap>& get_snaps() const {
     return snaps;
