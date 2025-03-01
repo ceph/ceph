d4241c483dd051ee2cca84a2627e60087f79de72
Soumya Koduri <skoduri@redhat.com>
2025-03-01 12:35:51 +0530

    rgw/cloudrestore: Add Restore support from Glacier/Tape cloud endpoints
    Unlike regular S3 cloud services, restoring objects from S3/Tape or AWS Glacier services
    would require special handling. We need to first restore the object using Glacier
    RestoreObject API and then download it using GET.
    https://docs.aws.amazon.com/cli/latest/reference/s3api/restore-object.html
    
    This PR adds that support for "Expedited" tier retrieval type. That means the
    restore would be quick and the object can be downloaded soon.
    
    TODO: "Standard" tier-type support. Need to handle the case where in restore from
    cloud endpoint could take a longer time and need to be monitored periodically
    in the background.
    
    Signed-off-by: Soumya Koduri <skoduri@redhat.com>

--- src/rgw/driver/rados/rgw_lc_tier.h
+++ src/rgw/driver/rados/rgw_lc_tier.h
@@ -57,3 +57,18 @@ int rgw_cloud_tier_get_object(RGWLCCloudTierCtx& tier_ctx, bool head,
                          real_time* pset_mtime, std::string& etag,
                          uint64_t& accounted_size, rgw::sal::Attrs& attrs,
                          void* cb);
+int rgw_cloud_tier_restore_object(RGWLCCloudTierCtx& tier_ctx,
+                         std::map<std::string, std::string>& headers,
+                         real_time* pset_mtime, std::string& etag,
+                         uint64_t& accounted_size, rgw::sal::Attrs& attrs,
+                  			 std::optional<uint64_t> days,
+                         RGWZoneGroupTierS3Glacier& glacier_params,
+                         void* cb);
+
+int cloud_tier_restore(const DoutPrefixProvider *dpp,
+                       RGWRESTConn& dest_conn, const rgw_obj& dest_obj,
+                       std::optional<uint64_t> days,
+                       RGWZoneGroupTierS3Glacier& glacier_params);
+
+bool is_restore_in_progress(const DoutPrefixProvider *dpp,
+                            std::map<std::string, std::string>& headers);
