11a0ddb2138cc5ae84d624a09eb9b4de7e435372
Zhang Song <zhangsong02@qianxin.com>
2025-03-01 18:59:19 +0800

    crimson/os/seastore: refactor LBAMapping and BackrefMapping
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/crimson/os/seastore/lba_mapping.h
+++ src/crimson/os/seastore/lba_mapping.h
@@ -10,125 +10,198 @@
 
 namespace crimson::os::seastore {
 
-class LBAMapping;
-using LBAMappingRef = std::unique_ptr<LBAMapping>;
-
-class LogicalCachedExtent;
+namespace lba_manager::btree {
+class BtreeLBAManager;
+}
 
 class LBAMapping {
 public:
-  LBAMapping(op_context_t ctx)
-    : ctx(ctx) {}
-  LBAMapping(
-    op_context_t ctx,
-    CachedExtentRef parent,
-    uint16_t pos,
-    pladdr_t value,
-    extent_len_t len,
-    fixed_kv_node_meta_t<laddr_t> meta)
-    : ctx(ctx),
-      parent(parent),
-      value(value),
-      len(len),
-      range(meta),
-      pos(pos)
+  LBAMapping() = default;
+  LBAMapping(LBACursorRef physical, LBACursorRef indirect)
+      : physical_cursor(std::move(physical)),
+	indirect_cursor(std::move(indirect))
   {}
 
-  CachedExtentRef get_parent() {
-    return parent;
+  static LBAMapping create_physical(LBACursorRef iter) {
+    return LBAMapping(std::move(iter), nullptr);
   }
 
-  uint16_t get_pos() const {
-    return pos;
+  static LBAMapping create_indirect(LBACursorRef iter) {
+    return LBAMapping(nullptr, std::move(iter));
   }
 
-  extent_len_t get_length() const {
-    ceph_assert(range.end > range.begin);
-    return len;
+  LBAMapping(const LBAMapping &) = delete;
+  LBAMapping(LBAMapping &&) = default;
+
+  LBAMapping &operator=(const LBAMapping &) = delete;
+  LBAMapping &operator=(LBAMapping &&) = default;
+
+  ~LBAMapping() = default;
+
+  bool is_null() const {
+    return !physical_cursor && !indirect_cursor;
   }
 
-  paddr_t get_val() const {
-    return value.get_paddr();
+  bool is_indirect() const {
+    assert(!is_null());
+    return (bool)indirect_cursor;
   }
 
-  virtual laddr_t get_key() const {
-    return range.begin;
+  bool is_valid() const {
+    assert(!is_null());
+    return physical_cursor->is_valid()
+	&& (!is_indirect() || indirect_cursor->is_valid());
   }
-
-  bool has_been_invalidated() const {
-    return parent->has_been_invalidated();
-  }
-
-  bool is_parent_viewable() const {
-    ceph_assert(parent);
-    auto [viewable, state] = parent->is_viewable_by_trans(
-      ctx.trans.get_trans_id());
-    if (state == CachedExtent::viewable_state_t::stable_retired) {
-      assert(ctx.trans.is_retired(parent->get_paddr(), parent->get_length()));
-    }
-    return viewable;
-  }
-  bool is_parent_valid() const {
-    ceph_assert(parent);
-    return parent->is_valid();
-  }
-
-  virtual void maybe_fix_pos() = 0;
-  virtual bool parent_modified() const = 0;
-  virtual uint32_t get_checksum() const = 0;
-
-  // An lba pin may be indirect, see comments in lba_manager/btree/btree_lba_manager.h
-  virtual bool is_indirect() const = 0;
-  virtual laddr_t get_intermediate_key() const = 0;
-  virtual laddr_t get_intermediate_base() const = 0;
-  virtual extent_len_t get_intermediate_length() const = 0;
-  // The start offset of the pin, must be 0 if the pin is not indirect
-  virtual extent_len_t get_intermediate_offset() const = 0;
-
-  virtual get_child_ret_t<lba_manager::btree::LBALeafNode, LogicalChildNode>
-  get_logical_extent(Transaction &t) = 0;
-
-  void link_child(LogicalChildNode *c) {
-    ceph_assert(child_pos);
-    child_pos->link_child(c);
-  }
-  virtual LBAMappingRef refresh_with_pending_parent() = 0;
 
   // For reserved mappings, the return values are
   // undefined although it won't crash
-  virtual bool is_stable() const = 0;
-  virtual bool is_data_stable() const = 0;
-  virtual bool is_clone() const = 0;
+  bool is_stable() const;
+  bool is_data_stable() const;
+  bool is_clone() const {
+    assert(!is_null());
+    return physical_cursor->val.refcount > 1;
+  }
   bool is_zero_reserved() const {
+    assert(!is_null());
     return !get_val().is_real();
   }
 
-  LBAMappingRef duplicate() const;
+  extent_len_t get_length() const {
+    assert(!is_null());
+    if (is_indirect()) {
+      return indirect_cursor->val.len;
+    } else {
+      return physical_cursor->val.len;
+    }
+  }
 
-  virtual ~LBAMapping() {}
-protected:
-  virtual LBAMappingRef _duplicate(op_context_t) const = 0;
+  paddr_t get_val() const {
+    assert(!is_null());
+    return physical_cursor->val.pladdr.get_paddr();
+  }
 
-  op_context_t ctx;
-  CachedExtentRef parent;
+  uint32_t get_checksum() const {
+    assert(!is_null());
+    return physical_cursor->val.checksum;
+  }
 
-  pladdr_t value;
-  extent_len_t len = 0;
-  fixed_kv_node_meta_t<laddr_t> range;
-  uint16_t pos = std::numeric_limits<uint16_t>::max();
+  laddr_t get_key() const {
+    assert(!is_null());
+    if (is_indirect()) {
+      return indirect_cursor->key;
+    } else {
+      return physical_cursor->key;
+    }
+  }
 
-  std::optional<child_pos_t<
-    lba_manager::btree::LBALeafNode>> child_pos = std::nullopt;
+  // An lba pin may be indirect, see comments in lba_manager/btree/btree_lba_manager.h
+  laddr_t get_intermediate_key() const {
+    assert(!is_null());
+    assert(is_indirect());
+    return indirect_cursor->val.pladdr.get_laddr();
+  }
+  laddr_t get_intermediate_base() const {
+    assert(!is_null());
+    assert(is_indirect());
+    return physical_cursor->key;
+  }
+  extent_len_t get_intermediate_length() const {
+    assert(!is_null());
+    assert(is_indirect());
+    return physical_cursor->val.len;
+  }
+  // The start offset of the indirect cursor related to physical cursor
+  extent_len_t get_intermediate_offset() const {
+    assert(!is_null());
+    assert(is_indirect());
+    return get_intermediate_base().get_byte_distance<
+      extent_len_t>(get_intermediate_key());
+  }
+
+  get_child_ret_t<lba_manager::btree::LBALeafNode, LogicalChildNode>
+  get_logical_extent(Transaction &t);
+
+  void link_child(LogicalChildNode &extent) {
+    using lba_manager::btree::LBALeafNode;
+    auto leaf = physical_cursor->parent->cast<LBALeafNode>();
+    assert(leaf->is_stable());
+    leaf->link_child(&extent, physical_cursor->pos);
+  }
+
+  LBAMapping duplicate() const {
+    assert(!is_null());
+    auto dup_iter = [](const LBACursorRef &iter) -> LBACursorRef {
+      if (iter) {
+	return std::make_unique<LBACursor>(*iter);
+      } else {
+	return nullptr;
+      }
+    };
+    return LBAMapping(dup_iter(physical_cursor), dup_iter(indirect_cursor));
+  }
+
+private:
+  // Only allow BtreeLBAManager use these two methods
+  // FIXME: remove them
+
+  friend class lba_manager::btree::BtreeLBAManager;
+
+  void make_indirect(LBACursorRef cursor) {
+    assert(physical_cursor);
+    assert(!indirect_cursor);
+    indirect_cursor = std::move(cursor);
+  }
+
+  void link_physical(LBACursorRef cursor) {
+    assert(!physical_cursor);
+    assert(indirect_cursor);
+    physical_cursor = std::move(cursor);
+  }
+
+private:
+  // To support cloning, there are two kinds of lba mappings:
+  //    1. physical lba mapping: the pladdr in the value of which is the paddr of
+  //       the corresponding extent;
+  //    2. indirect lba mapping: the pladdr in the value of which is an laddr pointing
+  //       to the physical lba mapping that's pointing to the actual paddr of the
+  //       extent being searched;
+  //
+  // Accordingly, LBAMapping may also work under two modes: indirect or direct
+  //    1. BtreeLBAMappings that come from quering an indirect lba mapping in the lba tree
+  //       are indirect;
+  //    2. BtreeLBAMappings that come from quering a physical lba mapping in the lba tree
+  //       are direct.
+  //
+  // For direct LBAMappings, there are two important properties:
+  //    1. key: the laddr of the lba mapping being queried;
+  //    2. paddr: the paddr recorded in the value of the lba mapping being queried.
+  // For indirect LBAMappings, LBAMapping has three important properties:
+  //    1. key: the laddr key of the lba entry being queried;
+  //    2. intermediate_key: the laddr within the scope of the physical lba mapping
+  //       that the current indirect lba mapping points to; although an indirect mapping
+  //       points to the start of the physical lba mapping, it may change to other
+  //       laddr after remap
+  //    3. intermediate_base: the laddr key of the physical lba mapping, intermediate_key
+  //       and intermediate_base should be the same when doing cloning
+  //    4. intermediate_offset: intermediate_key - intermediate_base
+  //    5. intermediate_length: the length of the actual physical lba mapping
+  //    6. paddr: the paddr recorded in the physical lba mapping pointed to by the
+  //       indirect lba mapping being queried;
+  //
+  // NOTE THAT, for direct LBAMappings, their intermediate_keys are the same as
+  // their keys.
+  LBACursorRef physical_cursor;
+  LBACursorRef indirect_cursor;
 };
 
 std::ostream &operator<<(std::ostream &out, const LBAMapping &rhs);
-using lba_pin_list_t = std::list<LBAMappingRef>;
+using lba_mapping_list_t = std::list<LBAMapping>;
 
-std::ostream &operator<<(std::ostream &out, const lba_pin_list_t &rhs);
+std::ostream &operator<<(std::ostream &out, const lba_mapping_list_t &rhs);
 
 } // namespace crimson::os::seastore
 
 #if FMT_VERSION >= 90000
 template <> struct fmt::formatter<crimson::os::seastore::LBAMapping> : fmt::ostream_formatter {};
-template <> struct fmt::formatter<crimson::os::seastore::lba_pin_list_t> : fmt::ostream_formatter {};
+template <> struct fmt::formatter<crimson::os::seastore::lba_mapping_list_t> : fmt::ostream_formatter {};
 #endif
