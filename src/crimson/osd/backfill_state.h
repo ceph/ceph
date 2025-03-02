f87804b9f680cb41cebbdbf37760824c647441da
Xuehan Xu <xuxuehan@qianxin.com>
2025-03-02 13:10:38 +0800

    Revert "Merge pull request #60883 from xxhdx1985126/wip-crimson-backfill-throttle"
    This reverts commit 14463aa7a493dc6903d18af960aac24e8ab8153b, reversing
    changes made to 7892d0dd7d594248a8aa6abd81f8f3e018e338ad.
    
    Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

--- src/crimson/osd/backfill_state.h
+++ src/crimson/osd/backfill_state.h
@@ -62,8 +62,6 @@ struct BackfillState {
   struct SuspendBackfill : sc::event<SuspendBackfill> {
   };
 
-  struct ThrottleAcquired : sc::event<ThrottleAcquired> {
-  };
 private:
   // internal events
   struct RequestPrimaryScanning : sc::event<RequestPrimaryScanning> {
@@ -262,7 +260,6 @@ public:
       sc::transition<RequestDone, Done>,
       sc::custom_reaction<SuspendBackfill>,
       sc::custom_reaction<Triggered>,
-      sc::transition<ThrottleAcquired, Enqueuing>,
       sc::transition<sc::event_base, Crashed>>;
     explicit Waiting(my_context);
     sc::result react(ObjectPushed);
