3a6b6f5232cb437ba0b2566cbabff867a7972b9c
Nizamudeen A <nia@redhat.com>
2025-03-04 10:39:43 +0530

    cephadm: change default release to tentacle
    when bootstrapping
    ```
    Mar 04 04:39:10 ceph-node-00 cloud-init[988]: Ceph version: ceph version 20.0.0-41-g1bc1c5e8 (1bc1c5e839b8b2d689f3e665a7a444b5d6c7b240) tentacle (dev)
    Mar 04 04:39:10 ceph-node-00 cloud-init[988]: Error: Container release tentacle != cephadm release squid; please use matching version of cephadm (pass --allow-mismatched-release to continue anyway)
    ```
    
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- src/cephadm/cephadmlib/constants.py
+++ src/cephadm/cephadmlib/constants.py
@@ -3,7 +3,7 @@
 # Default container images -----------------------------------------------------
 DEFAULT_IMAGE = 'quay.ceph.io/ceph-ci/ceph:main'
 DEFAULT_IMAGE_IS_MAIN = True
-DEFAULT_IMAGE_RELEASE = 'squid'
+DEFAULT_IMAGE_RELEASE = 'tentacle'
 DEFAULT_REGISTRY = 'quay.io'  # normalize unqualified digests to this
 # ------------------------------------------------------------------------------
 
