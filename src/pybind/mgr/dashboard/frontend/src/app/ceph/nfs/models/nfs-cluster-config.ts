055ddd218e6c221485c549cb707855db6ebe5a3f
Achintk1491 <achintk1491@gmail.com>
2025-03-05 20:22:16 +0530

    mgr/dashboard: NFS Rate Limit for enabling disabling QOS for Cluster and Export
    Fixes: https://tracker.ceph.com/issues/69839
    
    Signed-off-by: Achint Kaur <ackaur@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/nfs/models/nfs-cluster-config.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/nfs/models/nfs-cluster-config.ts
@@ -1,22 +1,44 @@
-export interface NFSClusterTableModel {
+export interface NFSCluster {
   name: string;
-  host: string[];
-  ip_port: string[];
-  virtual_ip_Port: string;
+  virtual_ip: number;
+  port: number;
+  backend: NFSBackend[];
 }
-
-export interface Backend {
+export interface NFSBackend {
   hostname: string;
   ip: string;
   port: number;
 }
 
-export interface Entity {
-  virtual_ip: number;
-  port: number;
-  backend: Backend[];
+export interface NFSBwIopConfig {
+  cluster_id?: string;
+  qos_type?: string;
+  max_export_write_bw?: number;
+  max_export_read_bw?: number;
+  max_client_write_bw?: number;
+  max_client_read_bw?: number;
+  max_export_combined_bw?: number;
+  max_client_combined_bw?: number;
+  disable_qos?: boolean;
+  enable_qos?: boolean;
+  enable_bw_control?: boolean;
+  combined_rw_bw_control?: boolean;
+  pseudo_path?: string;
 }
 
-export interface NFSClusterListConfig {
-  [name: string]: Entity;
+export enum QOSType {
+  PerShare = 'PerShare',
+  PerClient = 'PerClient',
+  PerSharePerClient = 'PerShare_PerClient'
+}
+
+export interface QOSTypeItem {
+  key: string;
+  value: string;
+  help: string;
+}
+
+export interface bwTypeItem {
+  value: string;
+  help: string;
 }
