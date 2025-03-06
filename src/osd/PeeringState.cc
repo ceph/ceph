8edebb3b5c74b1cf0ad622f2b3f2b480c73076c4
Bill Scales <bill_scales@uk.ibm.com>
2025-03-06 13:48:49 +0000

    osd: PeeringState.cc: Fix misleading comment in find_best_info
    The description of find_best_info said that it prefers a newer
    last_update. While this is true for replica pools, for erasure
    coded pools the function does the exact opposite finiding the
    oldest last_update
    
    Signed-off-by: Bill Scales <bill_scales@uk.ibm.com>

--- src/osd/PeeringState.cc
+++ src/osd/PeeringState.cc
@@ -1557,7 +1557,7 @@ void PeeringState::reject_reservation()
  * find_best_info
  *
  * Returns an iterator to the best info in infos sorted by:
- *  1) Prefer newer last_update
+ *  1) Prefer newer (oldest for ec_pool) last_update
  *  2) Prefer longer tail if it brings another info into contiguity
  *  3) Prefer current primary
  */
@@ -2574,8 +2574,9 @@ bool PeeringState::choose_acting(pg_shard_t &auth_log_shard_id,
 	ceph_assert(want_backfill.empty());
 	vector<int> empty;
 	pl->queue_want_pg_temp(empty);
-      } else
+      } else {
 	pl->queue_want_pg_temp(want);
+      }
     }
     return false;
   }
