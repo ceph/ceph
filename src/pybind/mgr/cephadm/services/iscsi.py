52e1e8d83f534ed07d491f8d43a60fcdc3bda61f
Redouane Kachach <rkachach@ibm.com>
2025-03-07 09:55:08 +0100

    mgr/cephadm: adapting iscsi service to use the new cert mgmt
    Signed-off-by: Redouane Kachach <rkachach@ibm.com>

--- src/pybind/mgr/cephadm/services/iscsi.py
+++ src/pybind/mgr/cephadm/services/iscsi.py
@@ -52,6 +52,7 @@ class IscsiService(CephService):
     def prepare_create(self, daemon_spec: CephadmDaemonDeploySpec) -> CephadmDaemonDeploySpec:
         assert self.TYPE == daemon_spec.daemon_type
 
+        super().prepare_create(daemon_spec)
         spec = cast(IscsiServiceSpec, self.mgr.spec_store[daemon_spec.service_name].spec)
         igw_id = daemon_spec.daemon_id
 
@@ -62,26 +63,18 @@ class IscsiService(CephService):
                                               'mgr', 'allow command "service status"',
                                               'osd', 'allow rwx'])
 
-        if spec.ssl_cert:
-            if isinstance(spec.ssl_cert, list):
-                cert_data = '\n'.join(spec.ssl_cert)
-            else:
-                cert_data = spec.ssl_cert
+        ssl_cert, ssl_key = self.get_certificates(daemon_spec)
+        if ssl_cert:
             ret, out, err = self.mgr.check_mon_command({
                 'prefix': 'config-key set',
                 'key': f'iscsi/{utils.name_to_config_section("iscsi")}.{igw_id}/iscsi-gateway.crt',
-                'val': cert_data,
+                'val': ssl_cert,
             })
-
-        if spec.ssl_key:
-            if isinstance(spec.ssl_key, list):
-                key_data = '\n'.join(spec.ssl_key)
-            else:
-                key_data = spec.ssl_key
+        if ssl_key:
             ret, out, err = self.mgr.check_mon_command({
                 'prefix': 'config-key set',
                 'key': f'iscsi/{utils.name_to_config_section("iscsi")}.{igw_id}/iscsi-gateway.key',
-                'val': key_data,
+                'val': ssl_key,
             })
 
         trusted_ip_list = get_trusted_ips(self.mgr, spec)
