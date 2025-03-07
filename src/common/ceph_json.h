f229a28b5b076534486652485d35f3e8668fc87c
Jesse F. Williamson <jfw@ibm.com>
2025-03-06 17:37:53 -0800

    Add copyright notice, some whitespace adjustment.
    Signed-off-by: Jesse F. Williamson <jfw@ibm.com>

--- src/common/ceph_json.h
+++ src/common/ceph_json.h
@@ -1,3 +1,18 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab ft=cpp
+
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2025 International Business Machines Corp.
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+*/
+
 #ifndef CEPH_JSON_H
 #define CEPH_JSON_H
 
