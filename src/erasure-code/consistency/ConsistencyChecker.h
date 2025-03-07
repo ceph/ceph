b95561ce2c723700529e10a752684c98abc60334
Connor Fawcett <connorfa@uk.ibm.com>
2025-03-07 12:45:20 +0000

    Consistency checker class with 'main' loop and command line utility

---
+++ src/erasure-code/consistency/ConsistencyChecker.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <boost/asio/io_context.hpp>
+#include <boost/program_options.hpp>
+
+#include "librados/librados_asio.h"
+
+#include "global/global_init.h"
+#include "global/global_context.h"
+
+#include "Pool.h"
+#include "ECReader.h"
+#include "RadosCommands.h"
+#include "ECEncoder.h"
+
+#define dout_context g_ceph_context
+
+namespace ceph {
+  namespace consistency {
+    typedef std::pair<std::string, bool> ConsistencyCheckResult;
+    class ConsistencyChecker {
+      protected:
+        librados::Rados& rados;
+        boost::asio::io_context& asio;
+        ceph::consistency::ECReader reader;
+        ceph::consistency::RadosCommands commands;
+        ceph::consistency::Pool pool;
+        bool buffers_match(const bufferlist& b1, const bufferlist& b2);
+        std::vector<ReadResult>* get_read_results();
+        std::pair<bufferlist, bufferlist> split_data_and_parity(const bufferlist& read, 
+                                                                ErasureCodeProfile profile);
+
+      public:
+        ConsistencyChecker(librados::Rados& rados,
+                           boost::asio::io_context& asio,
+                           const std::string& pool_name);
+        void queue_ec_read(Read read);
+        bool check_object_consistency(const bufferlist& inbl, int stripe_unit);
+        void print_results(std::vector<ConsistencyCheckResult> results, std::ostream& out);
+        void single_read_and_check_consistency(const std::string& oid,
+                                               int block_size,
+                                               int offset,
+                                               int length,
+                                               int stripe_unit);
+    };
+  }
+}
