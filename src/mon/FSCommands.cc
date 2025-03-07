2c6ebd0bfed328187cb0687979c0e3918b4338f3
Kotresh HR <khiremat@redhat.com>
2025-03-07 16:06:42 +0530

    mds: Add fs option use_global_snaprealm
    The fs option use_global_snaprealm is introduced
    to handle upgrade scenario of referent inode feature [1]
    This option is enabled be default which exhibits
    the existing behaviour i.e. snapshots will use
    global snaprealm. This option is used along with
    'allow_referent_inodes' on the new filesystems to
    disable global snaprealm.
    
    [1] https://tracker.ceph.com/issues/54205
    
    Fixes: https://tracker.ceph.com/issues/69336
    Signed-off-by: Kotresh HR <khiremat@redhat.com>

--- src/mon/FSCommands.cc
+++ src/mon/FSCommands.cc
@@ -620,6 +620,28 @@ int FileSystemCommandHandler::set_val(Monitor *mon, FSMap& fsmap, MonOpRequestRe
         });
 	ss << "Enabled creation of referent inodes for hardlinks to store backtrace";
       }
+    } else if (var == "use_global_snaprealm") {
+      bool use_global_snaprealm = false;
+      int r = parse_bool(val, &use_global_snaprealm, ss);
+      if (r != 0) {
+        return r;
+      }
+
+      if (!use_global_snaprealm) {
+        modify_filesystem(fsmap, fsv,
+            [](auto&& fs)
+        {
+          fs.get_mds_map().clear_global_snaprealm();
+        });
+	ss << "Disable global snaprealm for hardlink snapshot";
+      } else {
+        modify_filesystem(fsmap, fsv,
+            [](auto&& fs)
+        {
+          fs.get_mds_map().set_global_snaprealm();
+        });
+	ss << "Use global snaprealm for hardlink snapshot";
+      }
     } else if (var == "allow_dirfrags") {
         ss << "Directory fragmentation is now permanently enabled."
            << " This command is DEPRECATED and will be REMOVED from future releases.";
