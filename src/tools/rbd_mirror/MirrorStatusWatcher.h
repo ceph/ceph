c5b45d3d97e1796e1d86b0b57b94e117edd3a462
Prasanna Kumar Kalever <prasanna.kalever@redhat.com>
2025-03-11 15:17:41 +0530

    rbd-mirror: fix MirrorStatusWatcher
    * moved the internal functions scope to private
    * use m_on_start_finish to save the init time Context and use later,
      instead of passing it in various functions
    
    Signed-off-by: Prasanna Kumar Kalever <prasanna.kalever@redhat.com>

--- src/tools/rbd_mirror/MirrorStatusWatcher.h
+++ src/tools/rbd_mirror/MirrorStatusWatcher.h
@@ -30,11 +30,16 @@ public:
   ~MirrorStatusWatcher() override;
 
   void init(Context *on_finish);
-  void remove_down_image_status(Context *on_finish);
-  void handle_remove_down_image_status(int r, Context *on_finish);
-  void remove_down_group_status(Context *on_finish);
-  void handle_remove_down_group_status(int r, Context *on_finish);
   void shut_down(Context *on_finish);
+
+private:
+  mutable ceph::mutex m_lock;
+  Context* m_on_start_finish = nullptr;
+
+  void remove_down_image_status();
+  void handle_remove_down_image_status(int r);
+  void remove_down_group_status();
+  void handle_remove_down_group_status(int r);
 
 protected:
   void handle_notify(uint64_t notify_id, uint64_t handle,
