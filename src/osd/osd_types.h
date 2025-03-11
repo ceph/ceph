d73179181ba65412a1c91232cd78fc6e1f7f1cfd
Alex Ainscow <aainscow@uk.ibm.com>
2025-03-11 10:59:13 +0000

    osd: Add EC_OPTIMIZATIONS flag on and add ability to switch it on.
    This is the basic command. On its own, this does nothing functional, except set
    a flag which can be queried.
    
    Signed-off-by: Alex Ainscow <aainscow@uk.ibm.com>

--- src/osd/osd_types.h
+++ src/osd/osd_types.h
@@ -1298,6 +1298,7 @@ struct pg_pool_t {
     // Pool features are restricted to those supported by crimson-osd.
     // Note, does not prohibit being created on classic osd.
     FLAG_CRIMSON = 1<<18,
+    FLAG_EC_OPTIMIZATIONS = 1<<19, // enable optimizations, once enabled, cannot be disabled
   };
 
   static const char *get_flag_name(uint64_t f) {
@@ -1321,6 +1322,7 @@ struct pg_pool_t {
     case FLAG_EIO: return "eio";
     case FLAG_BULK: return "bulk";
     case FLAG_CRIMSON: return "crimson";
+    case FLAG_EC_OPTIMIZATIONS: return "ec_optimizations";
     default: return "???";
     }
   }
@@ -1377,6 +1379,8 @@ struct pg_pool_t {
       return FLAG_BULK;
     if (name == "crimson")
       return FLAG_CRIMSON;
+    if (name == "ec_optimizations")
+      return FLAG_EC_OPTIMIZATIONS;
     return 0;
   }
 
@@ -1789,6 +1793,10 @@ public:
 
   bool allows_ecoverwrites() const {
     return has_flag(FLAG_EC_OVERWRITES);
+  }
+
+  bool allows_ecoptimizations() const {
+    return has_flag(FLAG_EC_OPTIMIZATIONS);
   }
 
   bool is_crimson() const {
