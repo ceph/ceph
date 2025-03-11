97c43c728dc8e702ce19bc14f6aa9a4790c8415b
Nitzan Mordechai <nmordech@redhat.com>
2025-03-11 15:37:07 +0000

    OSDMonitor: exclude destroyed OSDs from "ceph node ls" output
    After destroying an OSD, the command "ceph node ls" still listed the destroyed OSD,
    causing confusion. This fix ensures that the status of the OSD is checked before
    adding it to the output, aligning its behavior with "ceph osd tree," which already
    displays the correct status.
    
    Fixes: https://tracker.ceph.com/issues/70400
    Signed-off-by: Nitzan Mordechai <nmordech@redhat.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -2198,6 +2198,9 @@ void OSDMonitor::print_nodes(Formatter *f)
       // not likely though
       continue;
     }
+    if (osdmap.is_destroyed(osd)) {
+      continue;
+    }
     osds[hostname->second].push_back(osd);
   }
 
