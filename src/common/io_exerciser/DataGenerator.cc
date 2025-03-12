5d6d0a765919911c4d21cf39d894039ce2a452af
Alex Ainscow <aainscow@uk.ibm.com>
2025-03-12 19:26:43 +0000

    test: Add miscompare debug info in io_sequencer
    Output debug indicating the object/pool that the miscompare occured to aid debug.
    
    Signed-off-by: Alex Ainscow <aainscow@uk.ibm.com>

--- src/common/io_exerciser/DataGenerator.cc
+++ src/common/io_exerciser/DataGenerator.cc
@@ -226,6 +226,8 @@ bool HeaderedSeededRandomGenerator::validate(bufferlist& bufferlist,
   }
 
   if (!invalid_block_offsets.empty()) {
+    dout(0) << "Miscompare for read of " << m_model.get_oid() <<
+      " offset=" << offset << " length=" << length << dendl;
     printDebugInformationForOffsets(offset, invalid_block_offsets, bufferlist);
   }
 
