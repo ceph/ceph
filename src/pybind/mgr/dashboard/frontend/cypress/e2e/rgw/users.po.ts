78a988f35dbb3daab8f1c75001d5dbcc92ae6528
Aashish Sharma <Aashish.Sharma1@ibm.com>
2025-03-13 17:33:54 +0530

    mgr/dashboard: Fix rgw user key modal not shown
    Fixes: https://tracker.ceph.com/issues/70450
    
    Signed-off-by: Aashish Sharma <aasharma@redhat.com>

--- src/pybind/mgr/dashboard/frontend/cypress/e2e/rgw/users.po.ts
+++ src/pybind/mgr/dashboard/frontend/cypress/e2e/rgw/users.po.ts
@@ -155,4 +155,15 @@ export class UsersPageHelper extends PageHelper {
     this.navigateTo();
     this.delete(tenant + '$' + uname, null, null, true, false, false, true);
   }
+
+  checkUserKeys(user_name: string) {
+    this.getExpandCollapseElement(user_name).should('be.visible').click();
+    cy.get('cd-table').contains('td', user_name).click();
+    cy.get('cd-rgw-user-details cd-table').eq(0).first().click();
+    cy.get("[aria-label='Show']").should('exist').click();
+    cy.get('input#user').should('exist');
+    cy.get('input#access_key').should('exist');
+    cy.get('input#secret_key').should('exist');
+    cy.get('cds-modal').should('exist');
+  }
 }
