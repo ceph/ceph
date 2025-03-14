09a07366f6759515999a3af39c5ecf93f49fd2b7
Yehuda Sadeh <ysadehwe@ibm.com>
2025-03-14 14:30:30 -0400

    rgw: snap create REST API
    Can be used with the aws cli tool, e.g.:
    
    $ aws s3api --endpoint http://crestview:8000 create-bucket-snapshot \
                --bucket buck  --snap-conf 'Name=mysnap,Description=...'
    
    Signed-off-by: Yehuda Sadeh <ysadehwe@ibm.com>

--- src/rgw/rgw_rest_snap.cc
+++ src/rgw/rgw_rest_snap.cc
@@ -52,3 +52,131 @@ void RGWListBucketSnapshots_ObjStore_S3::send_response()
   rgw_flush_formatter_and_reset(s, s->formatter);
 }
 
+int RGWCreateBucketSnapshot_ObjStore_S3::verify_permission(optional_yield y)
+{
+  auto [has_s3_existing_tag, has_s3_resource_tag] = rgw_check_policy_condition(this, s, false);
+  if (has_s3_resource_tag)
+    rgw_iam_add_buckettags(this, s);
+
+  if (!verify_bucket_permission(this, s, rgw::IAM::s3CreateBucketSnapshot)) {
+    return -EACCES;
+  }
+
+  return 0;
+}
+
+void RGWCreateBucketSnapshot_ObjStore_S3::pre_exec()
+{
+  rgw_bucket_object_pre_exec(s);
+}
+
+struct RGWCreateBucketSnapshot_S3
+{
+  std::string name;
+  std::string desc;
+
+  void decode_xml(XMLObj *obj) {
+    RGWXMLDecoder::decode_xml("Name", name, obj, true);
+    RGWXMLDecoder::decode_xml("Description", desc, obj, false);
+  }
+};
+
+
+int RGWCreateBucketSnapshot_ObjStore_S3::get_params(optional_yield y)
+{
+  RGWXMLParser parser;
+
+  if (!parser.init()){
+    return -EINVAL;
+  }
+
+  const auto max_size = s->cct->_conf->rgw_max_put_param_size;
+  int r = 0;
+  bufferlist data;
+
+  std::tie(r, data) = read_all_input(s, max_size, false);
+
+  if (r < 0)
+    return r;
+
+  if (!parser.parse(data.c_str(), data.length(), 1)) {
+    return -ERR_MALFORMED_XML;
+  }
+
+  RGWCreateBucketSnapshot_S3 conf;
+  try {
+    RGWXMLDecoder::decode_xml("SnapshotConfiguration", conf, &parser);
+  } catch (RGWXMLDecoder::err& err) {
+
+    ldpp_dout(this, 5) << "Malformed tagging request: " << err << dendl;
+    return -ERR_MALFORMED_XML;
+  }
+
+  snap_name = conf.name;
+  desc = conf.desc;
+
+  if (snap_name.empty()) {
+    ldpp_dout(this, 5) << "Missing Name parameter" << dendl;
+    return -EINVAL;
+  }
+
+  return 0;
+}
+
+void RGWCreateBucketSnapshot_ObjStore_S3::execute(optional_yield y)
+{
+  if (!s->bucket_exists) {
+    op_ret = -ERR_NO_SUCH_BUCKET;
+    return;
+  }
+
+  op_ret = get_params(y);
+  if (op_ret < 0) {
+    return;
+  }
+
+  auto& snap_mgr = s->bucket->get_info().local.snap_mgr;
+  if (!snap_mgr.is_enabled()) {
+    op_ret = -EINVAL;
+    s->err.message = "Snapshots are not enabled for this bucket";
+    return;
+  }
+  snap.info.name = snap_name;
+  snap.info.description = desc;
+  snap.info.creation_time = real_clock::now();
+
+  rgw_bucket_snap_id snap_id;
+
+  op_ret = s->bucket->get_info().local.snap_mgr.create_snap(snap.info, &snap.id);
+  if (op_ret < 0) {
+    if (op_ret == -EEXIST) {
+      op_ret = -ERR_BUCKET_SNAP_EXISTS;
+      s->err.message = "A snapshot with the same name already exists";
+    }
+    ldpp_dout(this, 0) << "ERROR: failed writing bucket instance info: " + cpp_strerror(-op_ret) << dendl;
+    return;
+  }
+  op_ret = s->bucket->put_info(this, false, real_time(), y);
+  if (op_ret < 0) {
+    s->err.message = "Error";
+  }
+}
+
+void RGWCreateBucketSnapshot_ObjStore_S3::send_response()
+{
+  if (op_ret)
+    set_req_state_err(s, op_ret);
+  dump_errno(s);
+  end_header(s, this, to_mime_type(s->format));
+  dump_start(s);
+  if (op_ret < 0) {
+    return;
+  }
+  
+  s->formatter->open_object_section_in_ns("CreateBucketSnapshotResponse", XMLNS_AWS_S3);
+  encode_xml("Snapshot", snap, s->formatter);
+  s->formatter->close_section();
+  rgw_flush_formatter_and_reset(s, s->formatter);
+}
+
+
--- src/rgw/rgw_rest_snap.h
+++ src/rgw/rgw_rest_snap.h
@@ -18,3 +18,23 @@ public:
   const char* name() const override { return "list_bucket_snapshots"; }
 };
 
+class RGWCreateBucketSnapshot_ObjStore_S3 : public RGWRESTOp {
+  std::string snap_name;
+  std::string desc;
+
+  rgw_bucket_snap snap;
+public:
+  RGWCreateBucketSnapshot_ObjStore_S3() {}
+
+  RGWOpType get_type() override { return RGW_OP_CREATE_BUCKET_SNAPSHOT; }
+
+  int verify_permission(optional_yield y) override;
+  void pre_exec() override;
+
+  int get_params(optional_yield y);
+  void execute(optional_yield y) override;
+  void send_response() override;
+
+  const char* name() const override { return "create_bucket_snapshot"; }
+};
+
