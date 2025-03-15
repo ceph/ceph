e5c3eee24bfae61ac7e4daff99a3315d52deaf4d
Ilya Dryomov <idryomov@gmail.com>
2025-03-15 20:42:02 +0100

    qa/workunits/rbd: fix positional argument expansion in create_image()
    Sticking $@ into a string that is supposed to form a command isn't
    right because the string would be broken apart when $@ has more than
    one argument:
    
      If the double-quoted expansion occurs within a word, the expansion of
      the first parameter is joined with the beginning part of the original
      word, and the expansion of the last parameter is joined with the last
      part of the original word.
    
    Resort to $* despite its shortcomings -- given run_cmd() signature it's
    the only practical fixup.  A wrapper such as run_cmd() should really be
    variadic or take an array instead of insisting on a single string.
    
    This unbreaks "TEST: data pool".
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- qa/workunits/rbd/rbd_mirror_helpers.sh
+++ qa/workunits/rbd/rbd_mirror_helpers.sh
@@ -1312,8 +1312,7 @@ create_image()
         shift
     fi
 
-    run_cmd "rbd --cluster ${cluster} create --size ${size} \
-        --image-feature ${RBD_IMAGE_FEATURES} $@ ${pool}/${image}"
+    run_cmd "rbd --cluster ${cluster} create --size ${size} --image-feature ${RBD_IMAGE_FEATURES} $* ${pool}/${image}"
 }
 
 image_create()
@@ -1327,7 +1326,7 @@ image_create()
         shift
     fi
 
-    run_cmd "rbd --cluster ${cluster} create --size ${size} --image-feature ${RBD_IMAGE_FEATURES} $@ ${image_spec}"
+    run_cmd "rbd --cluster ${cluster} create --size ${size} --image-feature ${RBD_IMAGE_FEATURES} $* ${image_spec}"
 }
 
 images_create()
