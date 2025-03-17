ab18530e0997cee9293e72e9428af23e0e4c4d7d
pujaoshahu <pshahu@redhat.com>
2025-03-17 21:47:14 +0530

    mgr/dashboard: When we open an about panel, other options at right top corner is still enabled and if we click on any of them, that page will open in the background,and about panel will not shrink automatically
    Fixes: https://tracker.ceph.com/issues/70500
    
    Signed-off-by: pujaoshahu <pshahu@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/core/navigation/administration/administration.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/navigation/administration/administration.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 import { Permission } from '~/app/shared/models/permissions';
 import { AuthStorageService } from '~/app/shared/services/auth-storage.service';
+import { ModalService } from '~/app/shared/services/modal.service';
 
 @Component({
   selector: 'cd-administration',
@@ -12,9 +13,13 @@ export class AdministrationComponent {
   userPermission: Permission;
   configOptPermission: Permission;
 
-  constructor(private authStorageService: AuthStorageService) {
+  constructor(private authStorageService: AuthStorageService, private modalService: ModalService) {
     const permissions = this.authStorageService.getPermissions();
     this.userPermission = permissions.user;
     this.configOptPermission = permissions.configOpt;
+  }
+
+  closeModal() {
+    this.modalService.dismissAll();
   }
 }
