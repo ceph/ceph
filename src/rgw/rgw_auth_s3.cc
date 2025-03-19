2205c90d299b201029c788b66577af72a03df182
Raja Sharma <rajaramsharma422@gmail.com>
2025-03-20 00:51:49 +0530

    rgw/iam: create customer managed policy
    Managed Policy structure has been suggested by Casey
    
    Signed-off-by: Raja Sharma <rajaramsharma422@gmail.com>
    Signed-off-by: Raja Sharma <raja@ibm.com>

--- src/rgw/rgw_auth_s3.cc
+++ src/rgw/rgw_auth_s3.cc
@@ -537,6 +537,7 @@ bool is_non_s3_op(RGWOpType op_type)
   case RGW_OP_ATTACH_GROUP_POLICY:
   case RGW_OP_DETACH_GROUP_POLICY:
   case RGW_OP_LIST_ATTACHED_GROUP_POLICIES:
+  case RGW_OP_CREATE_POLICY:
     return true;
   default:
     return false;
