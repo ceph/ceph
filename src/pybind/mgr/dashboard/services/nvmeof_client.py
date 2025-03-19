ceph/ceph-ci

mgr/dashboard: introduce "convert_to_model" util
fixes: https://tracker.ceph.com/issues/62705

Signed-off-by: Tomer Haskalovitch <il033030@Tomers-MacBook-Pro.local>

src/pybind/mgr/dashboard/services/nvmeof_client.py
@@ -1,7 +1,7 @@
 import functools
 import logging
 from collections.abc import Iterable
-from typing import Any, Callable, Dict, List, NamedTuple, Optional, Type
+from typing import Any, Callable, Dict, Generator, List, NamedTuple, Optional, Type
 
 from ..exceptions import DashboardException
 from .nvmeof_conf import NvmeofGatewaysConfig
@@ -108,7 +108,7 @@ else:
 
     def map_collection(
         model: Type[NamedTuple],
-        pick: str,
+        pick: str,  # pylint: disable=redefined-outer-name
         finalize: Optional[Callable[[Message, Collection], Collection]] = None,
     ) -> Callable[..., Callable[..., Collection]]:
         def decorator(func: Callable[..., Message]) -> Callable[..., Collection]:
@@ -171,3 +171,107 @@ else:
             func(*args, **kwargs)
 
         return wrapper
+
+    class MaxRecursionDepthError(Exception):
+        pass
+
+    def _convert(value, field_type, depth, max_depth) -> Generator:
+        if depth > max_depth:
+            raise MaxRecursionDepthError(
+                f"Maximum nesting depth of {max_depth} exceeded at depth {depth}.")
+
+        if isinstance(value, dict) and hasattr(field_type, '_fields'):
+            # Lazily create NamedTuple for nested dicts
+            yield from _lazily_create_namedtuple(value, field_type, depth + 1, max_depth)
+        elif isinstance(value, list):
+            # Handle empty lists directly
+            if not value:
+                yield []
+            else:
+                # Lazily process each item in the list based on the expected item type
+                item_type = field_type.__args__[0] if hasattr(field_type, '__args__') else None
+                processed_items = []
+                for v in value:
+                    if item_type:
+                        processed_items.append(next(_convert(v, item_type,
+                                                             depth + 1, max_depth), None))
+                    else:
+                        processed_items.append(v)
+                yield processed_items
+        else:
+            # Yield the value as is for simple types
+            yield value
+
+    def _lazily_create_namedtuple(data: Any, target_type: Type[NamedTuple],
+                                  depth: int, max_depth: int) -> Generator:
+        """ Lazily create NamedTuple from a dict """
+        field_values = {}
+        for field, field_type in zip(target_type._fields,
+                                     target_type.__annotations__.values()):
+            # these conditions are complex since we need to navigate between dicts,
+            # empty dicts and objects
+            if isinstance(data, dict) and data.get(field) is not None:
+                try:
+                    field_values[field] = next(_convert(data.get(field), field_type,
+                                                        depth, max_depth), None)
+                except StopIteration:
+                    return
+            elif hasattr(data, field):
+                try:
+                    field_values[field] = next(_convert(getattr(data, field), field_type,
+                                                        depth, max_depth), None)
+                except StopIteration:
+                    return
+            else:
+                # If the field is missing assign None
+                field_values[field] = None
+
+        namedtuple_instance = target_type(**field_values)  # type: ignore
+        yield namedtuple_instance
+
+    def obj_to_namedtuple(data: Any, target_type: Type[NamedTuple],
+                          max_depth: int = 4) -> NamedTuple:
+        """
+        Convert an object or dict to a NamedTuple, handling nesting and lists lazily.
+        This will raise an error if nesting depth exceeds the max depth (default 4)
+        to avoid bloating the memory in case of mutual references between objects.
+
+        :param data: The input data - object or dictionary
+        :param target_type: The target NamedTuple type
+        :param max_depth: The maximum depth allowed for recursion
+        :return: An instance of the target NamedTuple with fields populated from the JSON
+        """
+
+        if not isinstance(target_type, type) or not hasattr(target_type, '_fields'):
+            raise TypeError("target_type must be a NamedTuple type.")
+        if isinstance(data, list):
+            raise TypeError("data can't be a list.")
+        if data is None:
+            raise TypeError("data can't be None.")
+        namedtuple_values = next(_lazily_create_namedtuple(data, target_type, 1, max_depth))
+        return namedtuple_values
+
+    def convert_to_model(model: Type[NamedTuple]) -> Callable[..., Callable[..., Model]]:
+        def decorator(func: Callable[..., Message]) -> Callable[..., Model]:
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs) -> Model:
+                message = func(*args, **kwargs)
+
+                return obj_to_namedtuple(message, model)._asdict()
+
+            return wrapper
+
+        return decorator
+
+    # pylint: disable-next=redefined-outer-name
+    def pick(field: str, first: bool = False) -> Callable[..., Callable[..., object]]:
+        def decorator(func: Callable[..., Dict]) -> Callable[..., object]:
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs) -> object:
+                model = func(*args, **kwargs)
+                field_to_ret = model[field]
+                if first:
+                    field_to_ret = field_to_ret[0]
+                return field_to_ret
+            return wrapper
+        return decorator

ceph/ceph

src/pybind/mgr/dashboard/services/nvmeof_client.py
@@ -1,7 +1,7 @@
 import functools
 import logging
 from collections.abc import Iterable
-from typing import Any, Callable, Dict, List, NamedTuple, Optional, Type
+from typing import Any, Callable, Dict, Generator, List, NamedTuple, Optional, Type
 
 from ..exceptions import DashboardException
 from .nvmeof_conf import NvmeofGatewaysConfig
@@ -12,7 +12,8 @@ try:
     import grpc  # type: ignore
     import grpc._channel  # type: ignore
     from google.protobuf.message import Message  # type: ignore
-
+    from google.protobuf.json_format import MessageToDict  # type: ignore
+    
     from .proto import gateway_pb2 as pb2  # type: ignore
     from .proto import gateway_pb2_grpc as pb2_grpc  # type: ignore
 except ImportError:
@@ -108,7 +109,7 @@ else:
 
     def map_collection(
         model: Type[NamedTuple],
-        pick: str,
+        pick: str,  # pylint: disable=redefined-outer-name
         finalize: Optional[Callable[[Message, Collection], Collection]] = None,
     ) -> Callable[..., Callable[..., Collection]]:
         def decorator(func: Callable[..., Message]) -> Callable[..., Collection]:
@@ -171,3 +172,107 @@ else:
             func(*args, **kwargs)
 
         return wrapper
+
+    class MaxRecursionDepthError(Exception):
+        pass
+
+    def _convert(value, field_type, depth, max_depth) -> Generator:
+        if depth > max_depth:
+            raise MaxRecursionDepthError(
+                f"Maximum nesting depth of {max_depth} exceeded at depth {depth}.")
+
+        if isinstance(value, dict) and hasattr(field_type, '_fields'):
+            # Lazily create NamedTuple for nested dicts
+            yield from _lazily_create_namedtuple(value, field_type, depth + 1, max_depth)
+        elif isinstance(value, list):
+            # Handle empty lists directly
+            if not value:
+                yield []
+            else:
+                # Lazily process each item in the list based on the expected item type
+                item_type = field_type.__args__[0] if hasattr(field_type, '__args__') else None
+                processed_items = []
+                for v in value:
+                    if item_type:
+                        processed_items.append(next(_convert(v, item_type,
+                                                             depth + 1, max_depth), None))
+                    else:
+                        processed_items.append(v)
+                yield processed_items
+        else:
+            # Yield the value as is for simple types
+            yield value
+
+    def _lazily_create_namedtuple(data: Any, target_type: Type[NamedTuple],
+                                  depth: int, max_depth: int) -> Generator:
+        """ Lazily create NamedTuple from a dict """
+        field_values = {}
+        for field, field_type in zip(target_type._fields,
+                                     target_type.__annotations__.values()):
+            # these conditions are complex since we need to navigate between dicts,
+            # empty dicts and objects
+            if isinstance(data, dict) and data.get(field) is not None:
+                try:
+                    field_values[field] = next(_convert(data.get(field), field_type,
+                                                        depth, max_depth), None)
+                except StopIteration:
+                    return
+            elif hasattr(data, field):
+                try:
+                    field_values[field] = next(_convert(getattr(data, field), field_type,
+                                                        depth, max_depth), None)
+                except StopIteration:
+                    return
+            else:
+                # If the field is missing assign None
+                field_values[field] = None
+
+        namedtuple_instance = target_type(**field_values)  # type: ignore
+        yield namedtuple_instance
+
+    def obj_to_namedtuple(data: Any, target_type: Type[NamedTuple],
+                          max_depth: int = 4) -> NamedTuple:
+        """
+        Convert an object or dict to a NamedTuple, handling nesting and lists lazily.
+        This will raise an error if nesting depth exceeds the max depth (default 4)
+        to avoid bloating the memory in case of mutual references between objects.
+
+        :param data: The input data - object or dictionary
+        :param target_type: The target NamedTuple type
+        :param max_depth: The maximum depth allowed for recursion
+        :return: An instance of the target NamedTuple with fields populated from the JSON
+        """
+
+        if not isinstance(target_type, type) or not hasattr(target_type, '_fields'):
+            raise TypeError("target_type must be a NamedTuple type.")
+        if isinstance(data, list):
+            raise TypeError("data can't be a list.")
+        if data is None:
+            raise TypeError("data can't be None.")
+        namedtuple_values = next(_lazily_create_namedtuple(data, target_type, 1, max_depth))
+        return namedtuple_values
+
+    def convert_to_model(model: Type[NamedTuple]) -> Callable[..., Callable[..., Model]]:
+        def decorator(func: Callable[..., Message]) -> Callable[..., Model]:
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs) -> Model:
+                message = func(*args, **kwargs)
+                msg_dict = MessageToDict(message, including_default_value_fields=True, preserving_proto_field_name=True)
+                return obj_to_namedtuple(msg_dict, model)._asdict()
+
+            return wrapper
+
+        return decorator
+
+    # pylint: disable-next=redefined-outer-name
+    def pick(field: str, first: bool = False) -> Callable[..., Callable[..., object]]:
+        def decorator(func: Callable[..., Dict]) -> Callable[..., object]:
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs) -> object:
+                model = func(*args, **kwargs)
+                field_to_ret = model[field]
+                if first:
+                    field_to_ret = field_to_ret[0]
+                return field_to_ret
+            return wrapper
+        return decorator