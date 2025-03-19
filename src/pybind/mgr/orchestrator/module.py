af32f523833051a59f8b0930b585476724f140d2
Joshua Blanch <joshua.blanch@clyso.com>
2025-03-19 00:46:15 +0000

    mgr/orch: remove unimplemented orch cancel method
    Signed-off-by: Joshua Blanch <joshua.blanch@clyso.com>

--- src/pybind/mgr/orchestrator/module.py
+++ src/pybind/mgr/orchestrator/module.py
@@ -2316,14 +2316,6 @@ Usage:
         self.resume()
         return HandleCommandResult()
 
-    @_cli_write_command('orch cancel')
-    def _cancel(self) -> HandleCommandResult:
-        """
-        Cancel ongoing background operations
-        """
-        self.cancel_completions()
-        return HandleCommandResult()
-
     @_cli_read_command('orch status')
     def _status(self,
                 detail: bool = False,
