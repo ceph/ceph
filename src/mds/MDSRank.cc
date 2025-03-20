2c5737955d1a52d8fe7a364cd2425d75651c43e6
Milind Changire <mchangir@redhat.com>
2025-03-20 22:31:32 +0530

    mds: fix issues with use-after-free in C_Flush_Journal
    Resolved use-after-free issue of ESubtreeMap. The subtreemap event gets
    destroyed after it is submitted to the log.
    
    MDLog::submit_event() now returns a sequence number of the submitted event.
    
    Fixes: https://tracker.ceph.com/issues/69953
    Signed-off-by: Milind Changire <mchangir@redhat.com>

--- src/mds/MDSRank.cc
+++ src/mds/MDSRank.cc
@@ -97,8 +97,7 @@ private:
     // I need to seal off the current segment, and then mark all
     // previous segments for expiry
     auto* sle = mdcache->create_subtree_map();
-    mdlog->submit_entry(sle);
-    seq = sle->get_seq();
+    seq = mdlog->submit_entry(sle);
 
     Context *ctx = new LambdaContext([this](int r) {
         handle_clear_mdlog(r);
