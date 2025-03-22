30126830c2b8c4b6b69b09a510d268ae16871e44
Rishabh Dave <ridave@redhat.com>
2025-03-22 21:06:43 +0530

    qa/cephfs: raise exception when expected progress events are not found
    Method filter_in_only_clone_pevs() looks for progress events with ID
    "mgr-vol-ongoing-clones" or "mgr-vol-total-clones". And when it doesn't
    find either of them, it returns empty dictionary instead of raising
    exception or returning a value indicating this error.
    
    This is problematic because if somehow one of these progress events IDs
    is accidentally and undesirably renamed in the source code, tests won't
    catch this issue. This causes the tests to fail at a much later point
    (which makes it harder to debug) or probably cause tests to run
    successfully without exposing this bug, which is even worse.
    
    Therefore, when neither of these progress IDs are found raise an
    exception and cause the test to abort.
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/tasks/cephfs/test_volumes.py
+++ qa/tasks/cephfs/test_volumes.py
@@ -8532,9 +8532,20 @@ class TestCloneProgressReporter(TestVolumesHelper):
         '''
         clone_pevs = {}
 
+        # Set this variable to true if "mgr-vol-ongoing-clones" or
+        # "mgr-vol-total-clones" were found in the contents of
+        # "progress_events" variable.
+        one_of_the_pevs_occurred = False
         for k, v in progress_events.items():
             if 'mgr-vol-ongoing-clones' in k or 'mgr-vol-total-clones' in k:
                 clone_pevs[k] = v
+                one_of_the_pevs_occurred = True
+
+        if not one_of_the_pevs_occurred:
+            raise RuntimeError('Progress event with ID "mgr-vol-ongoing-clones"'
+                               ' or "mgr-vol-total-clones" was expected in the '
+                               'output of "ceph status" command but neither was'
+                               ' found.')
 
         return clone_pevs
 
