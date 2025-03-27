scantist-ossops-m2/ceph-ci

rgw/rados: remove get_obj_state() overload for follow_olh=true
and just add the follow_olh=true argument to callers

Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/driver/rados/rgw_rados.cc
@@ -4650,7 +4650,9 @@ int RGWRados::copy_obj(RGWObjectCtx& obj_ctx,
   RGWObjState *astate = NULL;
   RGWObjManifest *amanifest = nullptr;
 
-  ret = get_obj_state(dpp, &obj_ctx, src_bucket_info, src_obj, &astate, &amanifest, y);
+  constexpr bool follow_olh = true;
+  ret = get_obj_state(dpp, &obj_ctx, src_bucket_info, src_obj,
+                      &astate, &amanifest, follow_olh, y);
   if (ret < 0) {
     return ret;
   }

ceph/ceph

(cherry picked from commit 60dadf3c8d56eea2a6b2074599404fa74345bcc8)
Conflicts:
	src/rgw/driver/rados/rgw_rados.cc obj_ctx -> src_obj_ctx

src/rgw/driver/rados/rgw_rados.cc
@@ -4445,7 +4445,9 @@ int RGWRados::copy_obj(RGWObjectCtx& src_obj_ctx,
   RGWObjState *astate = NULL;
   RGWObjManifest *amanifest = nullptr;
 
-  ret = get_obj_state(dpp, &src_obj_ctx, src_bucket_info, src_obj, &astate, &amanifest, y);
+  constexpr bool follow_olh = true;
+  ret = get_obj_state(dpp, &src_obj_ctx, src_bucket_info, src_obj,
+                      &astate, &amanifest, follow_olh, y);
   if (ret < 0) {
     return ret;
   }