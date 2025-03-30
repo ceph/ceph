cb227050d7390dc80e7dfe685d64b999928bd855
Ilya Dryomov <idryomov@gmail.com>
2025-03-30 11:09:33 +0200

    qa/workunits/rbd: stop_mirror() should send a given signal just once
    Commit e09f04669053 ("qa/workunits/rbd: mirror group functional tests")
    moved the kill invocation in stop_mirror() from outside of the wait loop
    to inside.  This is wrong because the signal should be sent just once:
    rbd-mirror daemon installs a oneshot handler for SIGINT and SIGTERM and
    a subsequent signal immediately kills the process.
    
    The same commit also erroneously changed RBD_MIRROR_INSTANCES default
    for all rbd_mirror_helpers.sh users instead of just group tests.
    
    This fixes sporadic failures in "TEST: no blocklists".
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- qa/workunits/rbd/rbd_mirror_helpers.sh
+++ qa/workunits/rbd/rbd_mirror_helpers.sh
@@ -84,7 +84,7 @@ else
     exit 1
 fi
 
-RBD_MIRROR_INSTANCES=${RBD_MIRROR_INSTANCES:-1}
+RBD_MIRROR_INSTANCES=${RBD_MIRROR_INSTANCES:-2}
 
 CLUSTER1=cluster1
 CLUSTER2=cluster2
@@ -616,8 +616,8 @@ stop_mirror()
     local pid
     pid=$(cat $(daemon_pid_file "${cluster}") 2>/dev/null) || :
     if [ -n "${pid}" ]; then
+        kill ${sig} ${pid}
         for s in 0 1 2 4 8 16 32; do
-            kill ${sig} ${pid}
             sleep $s
             ps auxww | awk -v pid=${pid} '$2 == pid {print; exit 1}' && break
         done
