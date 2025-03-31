6aa1e210f3659e5c238e80a00d4b7b02e3cff120
Redouane Kachach <rkachach@ibm.com>
2025-03-31 15:02:07 +0200

    mgr/cepahdm: adapting alertmanager service to use the new cert mgmt
    Signed-off-by: Redouane Kachach <rkachach@ibm.com>

--- src/pybind/mgr/cephadm/services/monitoring.py
+++ src/pybind/mgr/cephadm/services/monitoring.py
@@ -278,15 +278,10 @@ class AlertmanagerService(CephadmService):
     USER_CFG_KEY = 'alertmanager/web_user'
     PASS_CFG_KEY = 'alertmanager/web_password'
 
-    def prepare_create(self, daemon_spec: CephadmDaemonDeploySpec) -> CephadmDaemonDeploySpec:
-        assert self.TYPE == daemon_spec.daemon_type
-        daemon_spec.final_config, daemon_spec.deps = self.generate_config(daemon_spec)
-        return daemon_spec
-
     def get_alertmanager_certificates(self, daemon_spec: CephadmDaemonDeploySpec) -> Tuple[str, str]:
-        node_ip = self.mgr.inventory.get_addr(daemon_spec.host)
-        host_fqdn = self.mgr.get_fqdn(daemon_spec.host)
-        cert, key = self.mgr.cert_mgr.generate_cert([host_fqdn, "alertmanager_servers"], node_ip)
+        host_ips = [self.mgr.inventory.get_addr(daemon_spec.host)]
+        host_fqdns = [self.mgr.get_fqdn(daemon_spec.host), 'alertmanager_servers']
+        cert, key = self.get_certificates(daemon_spec, host_ips, host_fqdns)
         return cert, key
 
     @classmethod
