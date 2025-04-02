e25a490d5594f799ec9af86e41b13020fabd7eac
Rishabh Dave <ridave@redhat.com>
2025-04-02 20:55:32 +0530

    mgr/vol: add command to get snapshot path
    Fixes: https://tracker.ceph.com/issues/70815
    Signed-off-by: Rishabh Dave <ridave@redhat.com>
    (cherry picked from commit 50d28992d99fcd67390815aa42f9da8ffaa82575)
    
    Conflicts:
    src/pybind/mgr/volumes/fs/volume.py
    - Line where the original patch makes the change is slightly
      different in main compared to Squid banch, leading to conflict.

--- src/pybind/mgr/volumes/module.py
+++ src/pybind/mgr/volumes/module.py
@@ -390,6 +390,17 @@ class Module(orchestrator.OrchestratorClientMixin, MgrModule):
             'perm': 'rw'
         },
         {
+            'cmd': 'fs subvolume snapshot getpath '
+                   'name=vol_name,type=CephString '
+                   'name=sub_name,type=CephString '
+                   'name=snap_name,type=CephString '
+                   'name=group_name,type=CephString,req=false ',
+            'desc': 'Get path for a snapshot of a CephFS subvolume located in '
+                    'a specific volume, and optionally, in a specific '
+                    'subvolume group',
+            'perm': 'r'
+        },
+        {
             'cmd': 'fs subvolume resize '
                    'name=vol_name,type=CephString '
                    'name=sub_name,type=CephString '
@@ -826,6 +837,13 @@ class Module(orchestrator.OrchestratorClientMixin, MgrModule):
         return self.vc.list_subvolume_snapshots(vol_name=cmd['vol_name'],
                                                 sub_name=cmd['sub_name'],
                                                 group_name=cmd.get('group_name', None))
+
+    @mgr_cmd_wrap
+    def _cmd_fs_subvolume_snapshot_getpath(self, inbuf, cmd):
+        return self.vc.subvolume_snapshot_getpath(vol_name=cmd['vol_name'],
+                                                  sub_name=cmd['sub_name'],
+                                                  snap_name=cmd['snap_name'],
+                                                  group_name=cmd.get('group_name', None))
 
     @mgr_cmd_wrap
     def _cmd_fs_subvolume_resize(self, inbuf, cmd):
