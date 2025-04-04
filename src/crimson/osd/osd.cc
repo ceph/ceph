c4dd06366bfcd3ab20e447455f6d7e63ac33d0e5
Samuel Just <sjust@redhat.com>
2025-04-03 19:12:33 -0700

    crimson: finish_decode() of MOSDRepOp on target core
    Signed-off-by: Samuel Just <sjust@redhat.com>
    (cherry picked from commit bd95adc594908047afd8bb40328ff8dda4488908)

--- src/crimson/osd/osd.cc
+++ src/crimson/osd/osd.cc
@@ -1428,7 +1428,6 @@ seastar::future<> OSD::handle_rep_op(
   crimson::net::ConnectionRef conn,
   Ref<MOSDRepOp> m)
 {
-  m->finish_decode();
   return pg_shard_manager.start_pg_operation<RepRequest>(
     std::move(conn),
     std::move(m)).second;
