ed2bc4110a9b79b4f1ecbb4b5d9e0ba4f8738ace
Redouane Kachach <rkachach@ibm.com>
2025-04-08 12:27:47 +0200

    mgr/cepahdm: adding filtering options for 'orch certmgr key ls' cmd
    Signed-off-by: Redouane Kachach <rkachach@ibm.com>

--- src/pybind/mgr/cephadm/cert_mgr.py
+++ src/pybind/mgr/cephadm/cert_mgr.py
@@ -183,8 +183,6 @@ class CertMgr:
 
         :param service_name: The name of the service.
         """
-        ss_cert_name = self.self_signed_cert(service_name)
-        ss_key_name = self.self_signed_key(service_name)
         self.cert_store.add_entity(self.self_signed_cert(service_name), TLSObjectScope.HOST)
         self.key_store.add_entity(self.self_signed_key(service_name), TLSObjectScope.HOST)
 
@@ -354,10 +352,12 @@ class CertMgr:
 
         return ls
 
-    def key_ls(self) -> Dict:
+    def key_ls(self, include_cephadm_signed: bool = False) -> Dict:
         key_objects: List = self.key_store.list_tlsobjects()
         ls: Dict = {}
         for key_name, key_obj, target in key_objects:
+            if not include_cephadm_signed and self.is_cephadm_signed_entity(key_name):
+                continue
             priv_key_info = get_private_key_info(key_obj.key)
             key_scope = self.get_key_scope(key_name)
             if key_name not in ls:
