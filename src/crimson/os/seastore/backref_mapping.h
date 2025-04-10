500aeb3401c519e6033b943789d719bc98fc4935
Zhang Song <zhangsong02@qianxin.com>
2025-04-10 11:15:11 +0800

    crimson/os/seastore: remove PhysicalNodeMapping and BtreeNodeMapping
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/crimson/os/seastore/backref_mapping.h
+++ src/crimson/os/seastore/backref_mapping.h
@@ -7,15 +7,45 @@
 
 namespace crimson::os::seastore {
 
-class BackrefMapping : public BtreeNodeMapping<paddr_t, laddr_t> {
+class BackrefMapping {
+  op_context_t ctx;
+  CachedExtentRef parent;
+  fixed_kv_node_meta_t<paddr_t> range;
+  laddr_t value;
+  extent_len_t len = 0;
+  uint16_t pos = std::numeric_limits<uint16_t>::max();
   extent_types_t type;
 public:
-  BackrefMapping(op_context_t ctx)
-    : BtreeNodeMapping(ctx) {}
-  template <typename... T>
-  BackrefMapping(extent_types_t type, T&&... t)
-    : BtreeNodeMapping(std::forward<T>(t)...),
-      type(type) {}
+  BackrefMapping(
+    extent_types_t type,
+    op_context_t ctx,
+    CachedExtentRef parent,
+    uint16_t pos,
+    laddr_t value,
+    extent_len_t len,
+    fixed_kv_node_meta_t<paddr_t> meta)
+      : ctx(ctx),
+	parent(parent),
+	range(meta),
+	value(value),
+	len(len),
+	pos(pos),
+	type(type)
+  {}
+
+  extent_len_t get_length() const {
+    ceph_assert(range.end > range.begin);
+    return len;
+  }
+
+  laddr_t get_val() const {
+    return value;
+  }
+
+  paddr_t get_key() const {
+    return range.begin;
+  }
+
   extent_types_t get_type() const {
     return type;
   }
