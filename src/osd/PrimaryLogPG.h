014b3d49639b1e682074039089921eb32b088478
sajibreadd <sajibreadd@gmail.com>
2025-04-11 13:24:00 +0200

    osd: osd code adjustment to remove object with omap hints and rocksdb threshold hint Fixes: https://tracker.ceph.com/issues/70892 Signed-off-by: Md Mahamudur Rahaman Sajib <mahamudur.sajib@croit.io>

--- src/osd/PrimaryLogPG.h
+++ src/osd/PrimaryLogPG.h
@@ -1871,7 +1871,9 @@ private:
   // return true if we're creating a local object, false for a
   // whiteout or no change.
   void maybe_create_new_object(OpContext *ctx, bool ignore_transaction=false);
-  int _delete_oid(OpContext *ctx, bool no_whiteout, bool try_no_whiteout);
+  int _delete_oid(OpContext *ctx, bool no_whiteout, bool try_no_whiteout,
+                  int64_t omap_count_hint = -1,
+                  int64_t db_delete_range_threshold_hint = -1);
   int _rollback_to(OpContext *ctx, OSDOp& op);
   void _do_rollback_to(OpContext *ctx, ObjectContextRef rollback_to,
 				    OSDOp& op);
