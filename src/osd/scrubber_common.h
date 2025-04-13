3c31f48c5764251cca2856751f0d657395710d9a
Ronen Friedman <rfriedma@redhat.com>
2025-04-13 12:53:12 -0500

    osd/scrub: a single counters selection mechanism - step 1
    Following the preceeding PR, the Scrubber now employs
    two methods for selecting the specific subset of performance
    counters to update (the replicated pool set or the EC one).
    The first method is using labeled counters, with 4 optional labels
    (Primary/Replica X Replicated/EC Pool). The second method
    is by naming the specific OSD counters to use in ScrubIoCounterSet
    objects, then selecting the appropriate set based on the pool type.
    
    This commit is the first step on the path to unifying the two
    methods - discarding the use of labeled counters, and only
    naming OSD counters.
    
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- src/osd/scrubber_common.h
+++ src/osd/scrubber_common.h
@@ -292,7 +292,7 @@ struct PgScrubBeListener {
 // defining a specific subset of performance counters. Each of the members
 // is set to (the index of) the corresponding performance counter.
 // Separate sets are used for replicated and erasure-coded pools.
-struct ScrubIoCounterSet {
+struct ScrubCounterSet {
   osd_counter_idx_t getattr_cnt; ///< get_attr calls count
   osd_counter_idx_t stats_cnt;  ///< stats calls count
   osd_counter_idx_t read_cnt;   ///< read calls count
@@ -301,7 +301,9 @@ struct ScrubIoCounterSet {
   osd_counter_idx_t omapgetheader_bytes;  ///< bytes read by omap get header
   osd_counter_idx_t omapget_cnt;  ///< omap get calls count
   osd_counter_idx_t omapget_bytes;  ///< total bytes read by omap get
+  osd_counter_idx_t active_started_cnt; ///< scrubs that got past reservation
 };
+
 }  // namespace Scrub
 
 
