1b03fc01e7a66c969b4e69f3b7fc9207edaf4706
Rishabh Dave <ridave@redhat.com>
2025-04-15 15:13:35 +0530

    mgr/vol: allow opening subvol even when it is cancelled
    Allow opening subvolume for listing it when it's in cancelled state.
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- src/pybind/mgr/volumes/fs/operations/versions/subvolume_v2.py
+++ src/pybind/mgr/volumes/fs/operations/versions/subvolume_v2.py
@@ -291,6 +291,7 @@ class SubvolumeV2(SubvolumeV1):
             }
 
         return {SubvolumeOpType.REMOVE_FORCE,
+                SubvolumeOpType.LIST,
                 SubvolumeOpType.CLONE_CREATE,
                 SubvolumeOpType.CLONE_STATUS,
                 SubvolumeOpType.CLONE_CANCEL,
