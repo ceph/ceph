8d740b1119ab9459bdceb8c84305782f8dd1f06d
Ilya Dryomov <idryomov@gmail.com>
2025-04-16 13:15:19 +0200

    librbd: disallow "rbd trash mv" if image is in a group
    Removing an image that is a member of a group has always been
    disallowed.  However, moving an image that is a member of a group to
    trash is currently allowed and this is deceptive -- the only reason for
    a user to move an image to trash should be the intent to remove it.
    
    More importantly, group APIs operate in terms of image names -- there
    are no corresponding variants that would operate in terms of image IDs.
    For example, even though internally GroupImageSpec struct stores an
    image ID, the public rbd_group_image_info_t struct insists on an image
    name.  When rbd_group_image_list() encounters a trashed member image
    (i.e. one that doesn't have a name), it just fails with ENOENT and no
    listing gets produced at all until the offending image is restored from
    trash.  Something like this can be very hard to debug for an average
    user, so let's make rbd_trash_move() fail with EMLINK the same way as
    rbd_remove() does in this scenario.
    
    The one case where moving a member image to trash makes sense is live
    migration where the source image gets trashed to be almost immediately
    replaced by the destination image as part of preparing migration.
    
    Fixes: https://tracker.ceph.com/issues/71026
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>
    (cherry picked from commit 2ae4a57e160f21201b9dfb4c8adb02f2826c7106)
    
    Conflicts:
    	PendingReleaseNotes [ moved to >=18.2.7 section ]

--- src/tools/rbd/action/Trash.cc
+++ src/tools/rbd/action/Trash.cc
@@ -97,8 +97,15 @@ int execute_move(const po::variables_map &vm,
   librbd::RBD rbd;
   r = rbd.trash_move(io_ctx, image_name.c_str(), dt);
   if (r < 0) {
-    std::cerr << "rbd: deferred delete error: " << cpp_strerror(r)
-              << std::endl;
+    if (r == -EMLINK) {
+      std::cerr << "rbd: error: image belongs to a group"
+                << std::endl
+                << "Remove the image from the group and try again."
+                << std::endl;
+    } else {
+      std::cerr << "rbd: deferred delete error: " << cpp_strerror(r)
+                << std::endl;
+    }
     return r;
   }
 
@@ -162,7 +169,10 @@ int execute_remove(const po::variables_map &vm,
                 << "waiting 30s for the crashed client to timeout."
                 << std::endl;
     } else if (r == -EMLINK) {
-      std::cerr << std::endl
+      // moving to trash an image that belongs to a group is no longer
+      // allowed, this is to handle any image that was trashed earlier
+      std::cerr << "rbd: error: image belongs to a group"
+                << std::endl
                 << "Remove the image from the group and try again."
                 << std::endl;
     } else if (r == -EPERM) {
