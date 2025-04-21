18de76aefa143d50f53c31b6e5b818319ff5af7a
Jesse Williamson <chardan@users.noreply.github.com>
2025-04-21 11:23:31 -0700

    Adjusting settings.
    Signed-off-by: Jesse Williamson <chardan@users.noreply.github.com>

--- src/test/rgw/catch2-rgw-general.cc
+++ src/test/rgw/catch2-rgw-general.cc
@@ -4,11 +4,13 @@
 
 #include "common/reverse.h"
 
-TEST_CASE("check support", "[common]")
+TEST_CASE("JFW check support", "[common]")
 {
+ INFO("JFW catch2 support looks like it's a thing!");
  REQUIRE(true); 	// "it's working!"
 }
 
+/*
 SCENARIO("reverse_bits is reversible") {
  GIVEN("some input") {
      const uint32_t n = 0x00AAAAFF;
@@ -23,3 +25,4 @@ SCENARIO("reverse_bits is reversible") {
  }
 }
 
+ */
