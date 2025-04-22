5ce04f29151d89928605e9782e760f52d900c2ad
Igor Fedotov <igor.fedotov@croit.io>
2025-04-22 11:40:10 +0000

    extblkdev: add thin_nvme plugin
    Signed-off-by: Igor Fedotov <igor.fedotov@croit.io>

---
+++ src/extblkdev/thin_nvme/ExtBlkDevPluginThinNVMe.h
@@ -0,0 +1,26 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph distributed storage system
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ */
+
+#ifndef CEPH_EXT_BLK_DEV_PLUGIN_THIN_NVME_H
+#define CEPH_EXT_BLK_DEV_PLUGIN_THIN_NVME_H
+
+#include "ExtBlkDevThinNVMe.h"
+
+class ExtBlkDevPluginThinNVMe : public ceph::ExtBlkDevPlugin {
+public:
+  explicit ExtBlkDevPluginThinNVMe(CephContext *cct) : ExtBlkDevPlugin(cct) {}
+  int get_required_cap_set(cap_t caps) override;
+  int factory(const std::string& logdevname,
+	      ceph::ExtBlkDevInterfaceRef& ext_blk_dev) override;
+};
+
+#endif
