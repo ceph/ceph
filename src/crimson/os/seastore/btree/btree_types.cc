4a8aecc99557f8d873ac1089763f3881b71d9923
Zhang Song <zhangsong02@qianxin.com>
2025-04-22 16:16:12 +0800

    crimson/os/seastore/btree: introduce BtreeCursor
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>
    (cherry picked from commit 972c139e9c5c1405378495fa9d50b1a314dea618)

--- src/crimson/os/seastore/btree/btree_types.cc
+++ src/crimson/os/seastore/btree/btree_types.cc
@@ -2,6 +2,8 @@
 // vim: ts=8 sw=2 smarttab
 
 #include "crimson/os/seastore/btree/btree_types.h"
+#include "crimson/os/seastore/lba_manager/btree/lba_btree_node.h"
+#include "crimson/os/seastore/backref/backref_tree_node.h"
 
 namespace crimson::os::seastore {
 
@@ -29,4 +31,40 @@ std::ostream& operator<<(std::ostream &out, const backref_map_val_t& val) {
 }
 
 } // namespace backref
+
+namespace {
+template <typename key_t, typename T>
+bool modified_since(T &&extent, uint64_t iter_modifications) {
+  using backref::BackrefLeafNode;
+  using lba_manager::btree::LBALeafNode;
+  if constexpr (std::is_same_v<key_t, laddr_t>) {
+    assert(extent->get_type() == extent_types_t::LADDR_LEAF);
+    auto leaf = extent->template cast<LBALeafNode>();
+    return leaf->modified_since(iter_modifications);
+  } else {
+    assert(extent->get_type() == extent_types_t::BACKREF_LEAF);
+    auto leaf = extent->template cast<BackrefLeafNode>();
+    return leaf->modified_since(iter_modifications);
+  }
+}
+}
+
+template <typename key_t, typename val_t>
+bool BtreeCursor<key_t, val_t>::is_viewable() const {
+  LOG_PREFIX(BtreeCursor::is_viewable());
+  if (!parent->is_valid() ||
+      modified_since<key_t>(parent, modifications)) {
+    return false;
+  }
+
+  auto [viewable, state] = parent->is_viewable_by_trans(ctx.trans);
+  assert(state != CachedExtent::viewable_state_t::invalid);
+  SUBTRACET(seastore_cache, "{} with viewable state {}",
+            ctx.trans, *parent, state);
+  return viewable;
+}
+
+template struct BtreeCursor<laddr_t, lba_manager::btree::lba_map_val_t>;
+template struct BtreeCursor<paddr_t, backref::backref_map_val_t>;
+
 } // namespace crimson::os::seastore
