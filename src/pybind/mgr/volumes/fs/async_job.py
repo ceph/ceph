ceph/ceph-ci

mgr/vol: add helpers to spawn all threads and more threads
Also add log messages for in these helper methods to allow tracking when
and why more threads were spawned.

Signed-off-by: Rishabh Dave <ridave@redhat.com>

src/pybind/mgr/volumes/fs/async_job.py
@@ -136,8 +136,7 @@ class AsyncJobs(threading.Thread):
         self.wakeup_timeout = None
 
         self.threads = []
-        for i in range(self.nr_concurrent_jobs):
-            self.spawn_new_thread(i)
+        self.spawn_all_threads()
         self.start()
 
     def spawn_new_thread(self, suffix):
@@ -147,6 +146,20 @@ class AsyncJobs(threading.Thread):
         t.start()
 
         self.threads.append(t)
+
+    def spawn_all_threads(self):
+        log.info(f'spawning {self.nr_concurrent_jobs} to execute more jobs '
+                  'concurrently')
+        for i in range(self.nr_concurrent_jobs):
+            self.spawn_new_thread(i)
+
+    def spawn_more_threads(self):
+        c = len(self.threads)
+        diff = self.nr_concurrent_jobs - c
+        log.info(f'spawning {diff} threads to execute more jobs concurrently')
+
+        for i in range(c, self.nr_concurrent_jobs):
+            self.spawn_new_thread(i)
 
     def set_wakeup_timeout(self):
         with self.lock:
@@ -170,9 +183,7 @@ class AsyncJobs(threading.Thread):
                     self.cv.notifyAll()
                 elif c < self.nr_concurrent_jobs:
                     # Increase concurrency: create more threads.
-                    log.debug("creating new threads to job increase")
-                    for i in range(c, self.nr_concurrent_jobs):
-                        self.spawn_new_thread(i)
+                    self.spawn_more_threads()
                 self.cv.wait(timeout=5)
 
     def shutdown(self):

ceph/ceph

(cherry picked from commit a9824956d4360862aa9f5e33864984218e0f057d)
Conflicts:
src/pybind/mgr/volumes/fs/async_job.py
- Due to absence of some backport, code around the patch in this commit
  is different on Reef branch compared to main branch, which caused the
  conflict.

src/pybind/mgr/volumes/fs/async_job.py
@@ -125,8 +125,7 @@ class AsyncJobs(threading.Thread):
         self.fs_client = CephfsClient(self.vc.mgr)
 
         self.threads = []
-        for i in range(self.nr_concurrent_jobs):
-            self.spawn_new_thread(i)
+        self.spawn_all_threads()
         self.start()
 
     def spawn_new_thread(self, suffix):
@@ -136,6 +135,20 @@ class AsyncJobs(threading.Thread):
         t.start()
 
         self.threads.append(t)
+
+    def spawn_all_threads(self):
+        log.debug(f'spawning {self.nr_concurrent_jobs} to execute more jobs '
+                  'concurrently')
+        for i in range(self.nr_concurrent_jobs):
+            self.spawn_new_thread(i)
+
+    def spawn_more_threads(self):
+        c = len(self.threads)
+        diff = self.nr_concurrent_jobs - c
+        log.debug(f'spawning {diff} threads to execute more jobs concurrently')
+
+        for i in range(c, self.nr_concurrent_jobs):
+            self.spawn_new_thread(i)
 
     def run(self):
         log.debug("tick thread {} starting".format(self.name))
@@ -148,9 +161,7 @@ class AsyncJobs(threading.Thread):
                     self.cv.notifyAll()
                 elif c < self.nr_concurrent_jobs:
                     # Increase concurrency: create more threads.
-                    log.debug("creating new threads to job increase")
-                    for i in range(c, self.nr_concurrent_jobs):
-                        self.spawn_new_thread(i)
+                    self.spawn_more_threads()
                 self.cv.wait(timeout=5)
 
     def shutdown(self):