1f72f878fe038ceda72b8d9ec8895e4784cb9fa9
Zhang Song <zhangsong02@qianxin.com>
2025-04-24 13:57:04 +0800

    crimson/os/seastore: rename type remap_entry to remap_entry_t
    Signed-off-by: Zhang Song <zhangsong02@qianxin.com>

--- src/test/crimson/seastore/test_transaction_manager.cc
+++ src/test/crimson/seastore/test_transaction_manager.cc
@@ -1097,7 +1097,7 @@ struct transaction_manager_test_t :
     });
   }
 
-  using remap_entry = TransactionManager::remap_entry;
+  using remap_entry_t = TransactionManager::remap_entry_t;
   LBAMappingRef remap_pin(
     test_transaction_t &t,
     LBAMappingRef &&opin,
@@ -1114,7 +1114,7 @@ struct transaction_manager_test_t :
     auto pin = with_trans_intr(*(t.t), [&](auto& trans) {
       return tm->remap_pin<TestBlock>(
         trans, std::move(opin), std::array{
-          remap_entry(new_offset, new_len)}
+          remap_entry_t(new_offset, new_len)}
       ).si_then([](auto ret) {
         return std::move(ret[0]);
       });
@@ -1166,10 +1166,10 @@ struct transaction_manager_test_t :
         t,
         std::move(opin),
         std::array{
-          remap_entry(
+          remap_entry_t(
             0,
             new_offset),
-          remap_entry(
+          remap_entry_t(
             new_offset + new_len,
             o_len - new_offset - new_len)
         }
@@ -1205,7 +1205,7 @@ struct transaction_manager_test_t :
         t,
         std::move(opin),
         std::array{
-          remap_entry(
+          remap_entry_t(
             new_offset + new_len,
             o_len - new_offset - new_len)
         }
@@ -1236,7 +1236,7 @@ struct transaction_manager_test_t :
         t,
         std::move(opin),
         std::array{
-          remap_entry(
+          remap_entry_t(
             0,
             new_offset)
         }
