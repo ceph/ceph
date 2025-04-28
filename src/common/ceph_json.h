a69347120ac9d9f9017fb1fa270efbb32429d4f6
Max Kellermann <max.kellermann@ionos.com>
2025-04-28 09:53:36 +0200

    common/ceph_json: add missing include
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/common/ceph_json.h
+++ src/common/ceph_json.h
@@ -7,6 +7,7 @@
 #include <stdexcept>
 #include <string>
 #include <typeindex>
+#include "include/encoding.h"
 #include <include/types.h>
 #include <boost/container/flat_map.hpp>
 #include <boost/container/flat_set.hpp>
