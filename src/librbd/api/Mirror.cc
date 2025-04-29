2fca6457f9fa9c8d98c1b6d21f59f20a79d3c8d7
Ilya Dryomov <idryomov@gmail.com>
2025-04-29 20:12:42 +0200

    librbd: let Mirror::group_get_info() return ENOENT only if group DNE
    Getting ENOENT when the group doesn't exist is very much desired,
    especially considering the fact that groups can't be opened like images
    are and that group APIs operate in terms of group names.  However, in
    case the group exists but mirroring is disabled, it's better to remain
    consistent with Mirror::image_get_info() and return a partially formed
    struct where state is set to RBD_MIRROR_GROUP_DISABLED -- this is the
    behavior that API users have grown to rely on.
    
    Signed-off-by: Ilya Dryomov <idryomov@gmail.com>

--- src/librbd/api/Mirror.cc
+++ src/librbd/api/Mirror.cc
@@ -582,7 +582,7 @@ struct C_GroupSnapshotCreate : public Context {
   }
 
   void finish(int r) override {
-    if (r < 0 && r != -ENOENT) {
+    if (r < 0) {
       on_finish->complete(r);
       return;
     }
