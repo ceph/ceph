3c335124d8f4478cd9a8f7dfd283bdb7c547c22a
Ivo Almeida <ialmeida@redhat.com>
2025-05-04 15:10:31 +0100

    mgr/dashboard: set up dashboard as a app shell
    * Set up dashboard to act as an app shell/host
    * Added NX libraries to support webpack module federation
    
    Fixes: https://tracker.ceph.com/issues/69922
    Signed-off-by: Ivo Almeida <ialmeida@redhat.com>
    
    fixed unit tests

--- src/pybind/mgr/dashboard/frontend/src/main.ts
+++ src/pybind/mgr/dashboard/frontend/src/main.ts
@@ -1,23 +1,10 @@
-import { ApplicationRef, enableProdMode, isDevMode } from '@angular/core';
-import { enableDebugTools } from '@angular/platform-browser';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { setRemoteDefinitions } from '@nx/angular/mf';
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
-
-if (environment.production) {
-  enableProdMode();
-}
-
-platformBrowserDynamic()
-  .bootstrapModule(AppModule)
-  .then((moduleRef) => {
-    if (isDevMode()) {
-      // source: https://medium.com/@dmitrymogilko/profiling-angular-change-detection-c00605862b9f
-      const applicationRef = moduleRef.injector.get(ApplicationRef);
-      const componentRef = applicationRef.components[0];
-      // allows to run `ng.profiler.timeChangeDetection();`
-      enableDebugTools(componentRef);
-    }
-  })
-  .catch((err) => console.log(err)); // eslint-disable-line no-console
+fetch('/assets/module-federation.manifest.json')
+  .then((res) => res.json())
+  .then(
+    (definitions) =>
+      definitions !== null && definitions !== undefined && setRemoteDefinitions(definitions)
+  )
+  // eslint-disable-next-line no-console
+  .then(() => import('./bootstrap').catch((err) => console.error(err)));
