73a8b2fda1976f553ec474027a3a73a5f6ceb441
Christopher Hoffman <choffman@redhat.com>
2025-05-06 19:02:29 +0000

    client: do not fscrypt encrypt snapshot names
    Snapshot names are visible within the .snap directory
    as dir entries. They can be created by a client that
    has an fscrypt key present and also by the manager who
    does not have any key. While the client with the key
    can create an encrypted name the manager cannot.
    Standardize functionality of these semantics to the
    common of the two.
    
    Signed-off-by: Christopher Hoffman <choffman@redhat.com>

--- src/client/Client.cc
+++ src/client/Client.cc
@@ -14149,8 +14149,6 @@ InodeRef Client::open_snapdir(const InodeRef& diri)
   auto [it, b] = inode_map.try_emplace(vino, nullptr);
   if (b) {
     in = new Inode(this, vino, &diri->layout);
-    in->fscrypt_auth = diri->fscrypt_auth; /* borrow parent fscrypt data */
-    in->fscrypt_ctx = in->init_fscrypt_ctx(fscrypt.get());
     refresh_snapdir_attrs(in, diri.get());
     diri->flags |= I_SNAPDIR_OPEN;
     it->second = in;
