23d15f270acd192fe8aa76918558316a57594ad9
VinayBhaskar-V <vvarada@redhat.com>
2025-05-14 01:55:44 +0530

    rbd-mirror: prevent image deletion if remote image is not primary
    A resync on a mirrored image may incorrectly results in the local
    image being deleted even when the remote image is no longer primary.
    This issue can occur under the following conditions:
    * if  resync is requested on the secondary before the remote image has
      been fully demoted
    * if the demotion of the primary image is not mirrored
      due to the rbd-mirror daemon being offline.
    
    This can be fixed by ensuring that image deletion during a resync is
    only allowed when the remote image is confirmed to be primary.
    
    This commit fixes the issue only for snapshot based mirroring mode
    
    Fixes: https://tracker.ceph.com/issues/70948
    Signed-off-by: VinayBhaskar-V <vvarada@redhat.com>
    (cherry picked from commit e14afbc95a5fb8f5a33e7ea23a035992b966d671)

--- src/tools/rbd_mirror/image_replayer/snapshot/PrepareReplayRequest.cc
+++ src/tools/rbd_mirror/image_replayer/snapshot/PrepareReplayRequest.cc
@@ -58,7 +58,10 @@ void PrepareReplayRequest<I>::handle_load_local_image_meta(int r) {
     return;
   }
 
-  *m_resync_requested = m_state_builder->local_image_meta->resync_requested;
+  if (r >= 0 && m_state_builder->local_image_meta->resync_requested &&
+      m_state_builder->is_remote_primary()) {
+    *m_resync_requested = true;
+  }
   finish(0);
 }
 
