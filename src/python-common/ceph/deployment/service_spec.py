d6ae69026e7e7591c60fcba10d0684c403228f88
Adam King <adking@redhat.com>
2025-05-19 17:48:02 -0400

    python-common/service_spec: add kmip fields to NFS spec
    The KMIP block will take the form
    
    KMIP {
            HOST {
                     addr = kmip-host-1.example.com;
            }
            cert = /etc/ganesha/kmip/cert.pem;
            key = /etc/ganesha/kmip/key.pem;
            ca = /etc/ganesha/kmip/ca.pem;
    }
    
    this commit adds the 4 fields that will populate the block
    
    Signed-off-by: Adam King <adking@redhat.com>

--- src/python-common/ceph/deployment/service_spec.py
+++ src/python-common/ceph/deployment/service_spec.py
@@ -1,3 +1,5 @@
+
+
 import fnmatch
 import os
 import re
@@ -1149,6 +1151,10 @@ class NFSServiceSpec(ServiceSpec):
                  extra_entrypoint_args: Optional[GeneralArgList] = None,
                  idmap_conf: Optional[Dict[str, Dict[str, str]]] = None,
                  custom_configs: Optional[List[CustomConfig]] = None,
+                 kmip_cert: Optional[str] = None,
+                 kmip_key: Optional[str] = None,
+                 kmip_ca_cert: Optional[str] = None,
+                 kmip_host_list: Optional[List[str]] = None,
                  ):
         assert service_type == 'nfs'
         super(NFSServiceSpec, self).__init__(
@@ -1163,6 +1169,10 @@ class NFSServiceSpec(ServiceSpec):
         self.enable_haproxy_protocol = enable_haproxy_protocol
         self.idmap_conf = idmap_conf
         self.enable_nlm = enable_nlm
+        self.kmip_cert = kmip_cert
+        self.kmip_key = kmip_key
+        self.kmip_ca_cert = kmip_ca_cert
+        self.kmip_host_list = kmip_host_list
 
     def get_port_start(self) -> List[int]:
         if self.port:
@@ -1172,6 +1182,21 @@ class NFSServiceSpec(ServiceSpec):
     def rados_config_name(self):
         # type: () -> str
         return 'conf-' + self.service_name()
+
+    def validate(self) -> None:
+        super(NFSServiceSpec, self).validate()
+
+        kmip_field_names = [
+            'kmip_cert',
+            'kmip_key',
+            'kmip_ca_cert',
+            'kmip_host_list'
+        ]
+        kmip_fields = [getattr(self, kmip_field) for kmip_field in kmip_field_names]
+        if any(kmip_fields) and not all(kmip_fields):
+            raise SpecValidationError(
+                f'Either none or all of {kmip_field_names} attrbutes must be set'
+            )
 
 
 yaml.add_representer(NFSServiceSpec, ServiceSpec.yaml_representer)
