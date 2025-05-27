7578d303990f4a0e5706ef32c06c45f8c05313cb
Patrick Donnelly <pdonnell@ibm.com>
2025-05-27 19:25:42 -0400

    auth: cleanup error message formatting
    Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>
    Change-Id: bbf54df2-b913-4273-aac4-7b6ad5b08c09
    (cherry picked from commit cf48247d4a927a8e1d47a5663c52f56b3877820d)
    (cherry picked from commit 89e4b9f3ac0675621a3e172f6cccb0af2c11c381)

--- src/auth/cephx/CephxSessionHandler.cc
+++ src/auth/cephx/CephxSessionHandler.cc
@@ -192,13 +192,13 @@ int CephxSessionHandler::check_message_signature(Message *m)
   if (sig != m->get_footer().sig) {
     // Should have been signed, but signature check failed.  PLR
     if (!(m->get_footer().flags & CEPH_MSG_FOOTER_SIGNED)) {
-      ldout(cct, 0) << "SIGN: MSG " << m->get_seq() << " Sender did not set CEPH_MSG_FOOTER_SIGNED." << dendl;
+      ldout(cct, 0) << "SIGN: MSG seq " << m->get_seq() << " Sender did not set CEPH_MSG_FOOTER_SIGNED." << dendl;
     }
-    ldout(cct, 0) << "SIGN: MSG " << m->get_seq() << " Message signature does not match contents." << dendl;
-    ldout(cct, 0) << "SIGN: MSG " << m->get_seq() << "Signature on message:" << dendl;
-    ldout(cct, 0) << "SIGN: MSG " << m->get_seq() << "    sig: " << m->get_footer().sig << dendl;
-    ldout(cct, 0) << "SIGN: MSG " << m->get_seq() << "Locally calculated signature:" << dendl;
-    ldout(cct, 0) << "SIGN: MSG " << m->get_seq() << "    sig_check:" << sig << dendl;
+    ldout(cct, 0) << "SIGN: MSG seq " << m->get_seq() << " Message signature does not match contents." << dendl;
+    ldout(cct, 0) << "SIGN: MSG seq " << m->get_seq() << " Signature on message:" << dendl;
+    ldout(cct, 0) << "SIGN: MSG seq " << m->get_seq() << "          sig: " << m->get_footer().sig << dendl;
+    ldout(cct, 0) << "SIGN: MSG seq " << m->get_seq() << " Locally calculated signature:" << dendl;
+    ldout(cct, 0) << "SIGN: MSG seq " << m->get_seq() << "    sig_check: " << sig << dendl;
 
     // For the moment, printing an error message to the log and
     // returning failure is sufficient.  In the long term, we should
