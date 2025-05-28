ceph/ceph

commit 09f436938fb1b83ad2f49a1836a8e6aaf336a8ae

node-proxy: make the daemon discover endpoints
Add logic in order to explore the API.
This will allow node-proxy to be compatible with more
hardware.

Fixes: https://tracker.ceph.com/issues/65394

Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>

src/pybind/mgr/cephadm/inventory.py
@@ -1410,7 +1410,7 @@ class HostCache():
 
 
 class NodeProxyCache:
-    def __init__(self, mgr: "CephadmOrchestrator") -> None:
+    def __init__(self, mgr: 'CephadmOrchestrator') -> None:
         self.mgr = mgr
         self.data: Dict[str, Any] = {}
         self.oob: Dict[str, Any] = {}
@@ -1428,7 +1428,7 @@ class NodeProxyCache:
 
             if host not in self.mgr.inventory.keys():
                 # remove entry for host that no longer exists
-                self.mgr.set_store(f"{NODE_PROXY_CACHE_PREFIX}/data/{host}", None)
+                self.mgr.set_store(f'{NODE_PROXY_CACHE_PREFIX}/data/{host}', None)
                 try:
                     self.oob.pop(host)
                     self.data.pop(host)
@@ -1442,15 +1442,15 @@ class NodeProxyCache:
     def save(self,
              host: str = '',
              data: Dict[str, Any] = {}) -> None:
-        self.mgr.set_store(f"{NODE_PROXY_CACHE_PREFIX}/data/{host}", json.dumps(data))
+        self.mgr.set_store(f'{NODE_PROXY_CACHE_PREFIX}/data/{host}', json.dumps(data))
 
     def update_oob(self, host: str, host_oob_info: Dict[str, str]) -> None:
         self.oob[host] = host_oob_info
-        self.mgr.set_store(f"{NODE_PROXY_CACHE_PREFIX}/oob", json.dumps(self.oob))
+        self.mgr.set_store(f'{NODE_PROXY_CACHE_PREFIX}/oob', json.dumps(self.oob))
 
     def update_keyring(self, host: str, key: str) -> None:
         self.keyrings[host] = key
-        self.mgr.set_store(f"{NODE_PROXY_CACHE_PREFIX}/keyrings", json.dumps(self.keyrings))
+        self.mgr.set_store(f'{NODE_PROXY_CACHE_PREFIX}/keyrings', json.dumps(self.keyrings))
 
     def fullreport(self, **kw: Any) -> Dict[str, Any]:
         """
@@ -1500,19 +1500,29 @@ class NodeProxyCache:
         for host in hosts:
             _result[host] = {}
             _result[host]['status'] = {}
+            state: str = ''
             data = self.data[host]
-            for component in data['status'].keys():
-                values = data['status'][component].values()
-                if is_error(values):
-                    state = 'error'
-                elif is_unknown(values):
+            for component, details in data['status'].items():
+                _sys_id_res: List[str] = []
+                for element in details.values():
+                    values = element.values()
+                    if is_error(values):
+                        state = 'error'
+                    elif is_unknown(values) or not values:
+                        state = 'unknown'
+                    else:
+                        state = 'ok'
+                    _sys_id_res.append(state)
+                if any([s == 'unknown' for s in _sys_id_res]):
                     state = 'unknown'
+                elif any([s == 'error' for s in _sys_id_res]):
+                    state = 'error'
                 else:
                     state = 'ok'
                 _result[host]['status'][component] = state
-            _result[host]['sn'] = data['sn']
-            _result[host]['host'] = data['host']
-            _result[host]['firmwares'] = data['firmwares']
+        _result[host]['sn'] = data['sn']
+        _result[host]['host'] = data['host']
+        _result[host]['status']['firmwares'] = data['firmwares']
         return _result
 
     def common(self, endpoint: str, **kw: Any) -> Dict[str, Any]:
@@ -1562,18 +1572,19 @@ class NodeProxyCache:
 
     def get_critical_from_host(self, hostname: str) -> Dict[str, Any]:
         results: Dict[str, Any] = {}
-        for component, data_component in self.data[hostname]['status'].items():
-            if component not in results.keys():
-                results[component] = {}
-            for member, data_member in data_component.items():
-                if component == 'power':
-                    data_member['status']['health'] = 'critical'
-                    data_member['status']['state'] = 'unplugged'
-                if component == 'memory':
-                    data_member['status']['health'] = 'critical'
-                    data_member['status']['state'] = 'errors detected'
-                if data_member['status']['health'].lower() != 'ok':
-                    results[component][member] = data_member
+        for sys_id, component in self.data[hostname]['status'].items():
+            for component_name, data_component in component.items():
+                if component_name not in results.keys():
+                    results[component_name] = {}
+                for member, data_member in data_component.items():
+                    if component_name == 'power':
+                        data_member['status']['health'] = 'critical'
+                        data_member['status']['state'] = 'unplugged'
+                    if component_name == 'memory':
+                        data_member['status']['health'] = 'critical'
+                        data_member['status']['state'] = 'errors detected'
+                    if data_member['status']['health'].lower() != 'ok':
+                        results[component_name][member] = data_member
         return results
 
     def criticals(self, **kw: Any) -> Dict[str, Any]:

src/ceph-node-proxy/ceph_node_proxy/baseredfishsystem.py
@@ -3,17 +3,126 @@ import json
 from ceph_node_proxy.basesystem import BaseSystem
 from ceph_node_proxy.redfish_client import RedFishClient
 from time import sleep
-from ceph_node_proxy.util import get_logger
+from ceph_node_proxy.util import get_logger, to_snake_case
 from typing import Dict, Any, List, Callable, Union
 from urllib.error import HTTPError, URLError
+
+
+class EndpointMgr:
+    NAME: str = 'EndpointMgr'
+
+    def __init__(self,
+                 client: RedFishClient,
+                 prefix: str = RedFishClient.PREFIX) -> None:
+        self.log = get_logger(f'{__name__}:{EndpointMgr.NAME}')
+        self.prefix: str = prefix
+        self.client: RedFishClient = client
+
+    def __getitem__(self, index: str) -> Any:
+        if index in self.__dict__:
+            return self.__dict__[index]
+        else:
+            raise RuntimeError(f'{index} is not a valid endpoint.')
+
+    def init(self) -> None:
+        _error_msg: str = "Can't discover entrypoint(s)"
+        try:
+            _, _data, _ = self.client.query(endpoint=self.prefix)
+            json_data: Dict[str, Any] = json.loads(_data)
+            for k, v in json_data.items():
+                if '@odata.id' in v:
+                    self.log.debug(f'entrypoint found: {to_snake_case(k)} = {v["@odata.id"]}')
+                    _name: str = to_snake_case(k)
+                    _url: str = v['@odata.id']
+                    e = Endpoint(self, _url, self.client)
+                    setattr(self, _name, e)
+            setattr(self, 'session', json_data['Links']['Sessions']['@odata.id'])  # TODO(guits): needs to be fixed
+        except (URLError, KeyError) as e:
+            msg = f'{_error_msg}: {e}'
+            self.log.error(msg)
+            raise RuntimeError
+
+
+class Endpoint:
+    NAME: str = 'Endpoint'
+
+    def __init__(self, url: str, client: RedFishClient) -> None:
+        self.log = get_logger(f'{__name__}:{Endpoint.NAME}')
+        self.url: str = url
+        self.client: RedFishClient = client
+        self.data: Dict[str, Any] = self.get_data()
+        self.id: str = ''
+        self.members_names: List[str] = []
+
+        if self.has_members:
+            self.members_names = self.get_members_names()
+
+        if self.data:
+            try:
+                self.id = self.data['Id']
+            except KeyError:
+                self.id = self.data['@odata.id'].split('/')[-1:]
+        else:
+            self.log.warning(f'No data could be loaded for {self.url}')
+
+    def __getitem__(self, index: str) -> Any:
+        if not getattr(self, index, False):
+            _url: str = f'{self.url}/{index}'
+            setattr(self, index, Endpoint(_url, self.client))
+        return self.__dict__[index]
+
+    def query(self, url: str) -> Dict[str, Any]:
+        data: Dict[str, Any] = {}
+        try:
+            self.log.debug(f'Querying {url}')
+            _, _data, _ = self.client.query(endpoint=url)
+            data = json.loads(_data)
+        except KeyError as e:
+            self.log.error(f'Error while querying {self.url}: {e}')
+        return data
+
+    def get_data(self) -> Dict[str, Any]:
+        return self.query(self.url)
+
+    def get_members_names(self) -> List[str]:
+        result: List[str] = []
+        if self.has_members:
+            for member in self.data['Members']:
+                name: str = member['@odata.id'].split('/')[-1:][0]
+                result.append(name)
+        return result
+
+    def get_name(self, endpoint: str) -> str:
+        return endpoint.split('/')[-1:][0]
+
+    def get_members_endpoints(self) -> Dict[str, str]:
+        members: Dict[str, str] = {}
+        name: str = ''
+        if self.has_members:
+            for member in self.data['Members']:
+                name = self.get_name(member['@odata.id'])
+                members[name] = member['@odata.id']
+        else:
+            name = self.get_name(self.data['@odata.id'])
+            members[name] = self.data['@odata.id']
+
+        return members
+
+    def get_members_data(self) -> Dict[str, Any]:
+        result: Dict[str, Any] = {}
+        if self.has_members:
+            for member, endpoint in self.get_members_endpoints().items():
+                result[member] = self.query(endpoint)
+        return result
+
+    @property
+    def has_members(self) -> bool:
+        return 'Members' in self.data.keys()
 
 
 class BaseRedfishSystem(BaseSystem):
     def __init__(self, **kw: Any) -> None:
         super().__init__(**kw)
-        self.common_endpoints: List[str] = kw.get('common_endpoints', ['/Systems/System.Embedded.1',
-                                                                       '/UpdateService'])
-        self.chassis_endpoint: str = kw.get('chassis_endpoint', '/Chassis/System.Embedded.1')
         self.log = get_logger(__name__)
         self.host: str = kw['host']
         self.port: str = kw['port']
@@ -21,6 +130,7 @@ class BaseRedfishSystem(BaseSystem):
         self.password: str = kw['password']
         # move the following line (class attribute?)
         self.client: RedFishClient = RedFishClient(host=self.host, port=self.port, username=self.username, password=self.password)
+        self.endpoints: EndpointMgr = EndpointMgr(self.client)
         self.log.info(f'redfish system initialization, host: {self.host}, user: {self.username}')
         self.data_ready: bool = False
         self.previous_data: Dict = {}
@@ -48,6 +158,8 @@ class BaseRedfishSystem(BaseSystem):
     def main(self) -> None:
         self.stop = False
         self.client.login()
+        self.endpoints.init()
+
         while not self.stop:
             self.log.debug('waiting for a lock in the update loop.')
             with self.lock:
@@ -100,9 +212,7 @@ class BaseRedfishSystem(BaseSystem):
         return result
 
     def get_members(self, data: Dict[str, Any], path: str) -> List:
-        _path = data[path]['@odata.id']
-        _data = self._get_path(_path)
-        return [self._get_path(member['@odata.id']) for member in _data['Members']]
+        return [self._get_path(member['@odata.id']) for member in data['Members']]
 
     def get_system(self) -> Dict[str, Any]:
         result = {
@@ -117,15 +227,18 @@ class BaseRedfishSystem(BaseSystem):
                 'fans': self.get_fans()
             },
             'firmwares': self.get_firmwares(),
-            'chassis': {'redfish_endpoint': f'/redfish/v1{self.chassis_endpoint}'}  # TODO(guits): not ideal
         }
         return result
 
     def _update_system(self) -> None:
-        for endpoint in self.common_endpoints:
-            result = self.client.get_path(endpoint)
-            _endpoint = endpoint.strip('/').split('/')[0]
-            self._system[_endpoint] = result
+        system_members: Dict[str, Any] = self.endpoints['systems'].get_members_data()
+        update_service_members: Endpoint = self.endpoints['update_service']
+
+        for member, data in system_members.items():
+            self._system[member] = data
+            self._sys[member] = dict()
+
+        self._system[update_service_members.id] = update_service_members.data
 
     def _update_sn(self) -> None:
         raise NotImplementedError()
@@ -196,7 +309,7 @@ class BaseRedfishSystem(BaseSystem):
 
     def set_device_led(self, device: str, data: Dict[str, bool]) -> int:
         try:
-            _, response, status = self.client.query(
+            _, _, status = self.client.query(
                 data=json.dumps(data),
                 method='PATCH',
                 endpoint=self._sys['storage'][device]['redfish_endpoint']
@@ -207,7 +320,7 @@ class BaseRedfishSystem(BaseSystem):
         return status
 
     def get_chassis_led(self) -> Dict[str, Any]:
-        endpoint = f'/redfish/v1/{self.chassis_endpoint}'
+        endpoint = list(self.endpoints['chassis'].get_members_endpoints().values())[0]
         try:
             result = self.client.query(method='GET',
                                        endpoint=endpoint,
@@ -227,10 +340,10 @@ class BaseRedfishSystem(BaseSystem):
         # '{"IndicatorLED": "Lit"}'      -> LocationIndicatorActive = false
         # '{"IndicatorLED": "Blinking"}' -> LocationIndicatorActive = true
         try:
-            _, response, status = self.client.query(
+            _, _, status = self.client.query(
                 data=json.dumps(data),
                 method='PATCH',
-                endpoint=f'/redfish/v1{self.chassis_endpoint}'
+                endpoint=list(self.endpoints['chassis'].get_members_endpoints().values())[0]
             )
         except HTTPError as e:
             self.log.error(f"Couldn't set the ident chassis LED: {e}")
@@ -260,7 +373,7 @@ class BaseRedfishSystem(BaseSystem):
     def create_reboot_job(self, reboot_type: str) -> str:
         data: Dict[str, str] = dict(RebootJobType=reboot_type)
         try:
-            headers, response, status = self.client.query(
+            headers, _, _ = self.client.query(
                 data=json.dumps(data),
                 endpoint=self.create_reboot_job_endpoint
             )
@@ -273,7 +386,7 @@ class BaseRedfishSystem(BaseSystem):
     def schedule_reboot_job(self, job_id: str) -> int:
         data: Dict[str, Union[List[str], str]] = dict(JobArray=[job_id], StartTimeInterval='TIME_NOW')
         try:
-            headers, response, status = self.client.query(
+            _, _, status = self.client.query(
                 data=json.dumps(data),
                 endpoint=self.setup_job_queue_endpoint
             )

src/ceph-node-proxy/ceph_node_proxy/redfish_client.py
@@ -22,9 +22,24 @@ class RedFishClient(BaseClient):
         self.url: str = f'https://{self.host}:{self.port}'
         self.token: str = ''
         self.location: str = ''
+        self.session_service: str = ''
+
+    def sessionservice_discover(self) -> None:
+        _error_msg: str = "Can't discover SessionService url"
+        try:
+            _headers, _data, _status_code = self.query(endpoint=RedFishClient.PREFIX)
+            json_data: Dict[str, Any] = json.loads(_data)
+            self.session_service = json_data['Links']['Sessions']['@odata.id']
+        except (URLError, KeyError) as e:
+            msg = f'{_error_msg}: {e}'
+            self.log.error(msg)
+            raise RuntimeError
 
     def login(self) -> None:
         if not self.is_logged_in():
+            self.log.debug('Discovering SessionService url...')
+            self.sessionservice_discover()
+            self.log.debug(f'SessionService url is {self.session_service}')
             self.log.info('Logging in to '
                           f"{self.url} as '{self.username}'")
             oob_credentials = json.dumps({'UserName': self.username,
@@ -35,7 +50,7 @@ class RedFishClient(BaseClient):
             try:
                 _headers, _data, _status_code = self.query(data=oob_credentials,
                                                            headers=headers,
-                                                           endpoint='/redfish/v1/SessionService/Sessions/')
+                                                           endpoint=self.session_service)
                 if _status_code != 201:
                     self.log.error(f"Can't log in to {self.url} as '{self.username}': {_status_code}")
                     raise RuntimeError
@@ -119,5 +134,5 @@ class RedFishClient(BaseClient):
 
             return response_headers, response_str, response_status
         except (HTTPError, URLError) as e:
-            self.log.debug(f'{e}')
+            self.log.debug(f'endpoint={endpoint} err={e}')
             raise

commit 83fd6ab1343247ba89b1414ea4ce6ccbfc91eae6

node-proxy: address `ceph orch hardware status` cmd
09f43693 introduced a regression.

`ceph orch hardware status` fails when no hostname is passed (global
cluster hw status report).

Typical failure:

```
Error EINVAL: Traceback (most recent call last):
  File "/usr/share/ceph/mgr/mgr_module.py", line 1928, in _handle_command
    return self.handle_command(inbuf, cmd)
  File "/usr/share/ceph/mgr/orchestrator/_interface.py", line 186, in handle_command
    return dispatch[cmd['prefix']].call(self, cmd, inbuf)
  File "/usr/share/ceph/mgr/mgr_module.py", line 527, in call
    return self.func(mgr, **kwargs)
  File "/usr/share/ceph/mgr/orchestrator/_interface.py", line 122, in <lambda>
    wrapper_copy = lambda *l_args, **l_kwargs: wrapper(*l_args, **l_kwargs)  # noqa: E731
  File "/usr/share/ceph/mgr/orchestrator/_interface.py", line 111, in wrapper
    return func(*args, **kwargs)
  File "/usr/share/ceph/mgr/orchestrator/module.py", line 553, in _hardware_status
    row = [k, v['sn']]
KeyError: 'sn'
```

Only the last node in the list has the `sn`, `host` and `firmwares`
information because of the wrong identation.

Fixes: https://tracker.ceph.com/issues/71472

Signed-off-by: Guillaume Abrioux <gabrioux@ibm.com>

src/pybind/mgr/cephadm/inventory.py
@@ -1709,9 +1709,9 @@ class NodeProxyCache:
                 else:
                     state = 'ok'
                 _result[host]['status'][component] = state
-        _result[host]['sn'] = data['sn']
-        _result[host]['host'] = data['host']
-        _result[host]['status']['firmwares'] = data['firmwares']
+            _result[host]['sn'] = data['sn']
+            _result[host]['host'] = data['host']
+            _result[host]['status']['firmwares'] = data['firmwares']
         return _result
 
     def common(self, endpoint: str, **kw: Any) -> Dict[str, Any]: