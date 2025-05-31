d45823e248849044ace066d4f6f8349518ab0d71
Patrick Donnelly <pdonnell@ibm.com>
2025-05-31 19:52:33 -0400

    common/entity_name: cleanup entity_name::type
    This should use the entity_type_t from the msg headers. The only awkwardness is
    that the encode/decode of the type needs to continue using a uint32_t.
    
    Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>
    Change-Id: ff219ed9-480f-4eec-ae90-b133cb9f9f05
    (cherry picked from commit b41e180e1104beaac8bce8348784676b7794a5e9)
    
    Conflicts:
    	src/common/entity_name.cc: encode/decode methods moved to .cc in
                                       main branch
    	src/common/entity_name.h: ditto

--- src/common/entity_name.h
+++ src/common/entity_name.h
@@ -12,8 +12,7 @@
  *
  */
 
-#ifndef CEPH_COMMON_ENTITY_NAME_H
-#define CEPH_COMMON_ENTITY_NAME_H
+#pragma once
 
 #include <string_view>
 
@@ -21,41 +20,37 @@
 
 #include "msg/msg_types.h"
 
+using namespace std::literals::string_view_literals;
+
+
 /* Represents a Ceph entity name.
  *
  * For example, mds.0 is the name of the first metadata server.
  * client
  */
+
 struct EntityName
 {
-  void encode(ceph::buffer::list& bl) const {
-    using ceph::encode;
-    encode(type, bl);
-    encode(id, bl);
-  }
-  void decode(ceph::buffer::list::const_iterator& bl) {
-    using ceph::decode;
-    uint32_t type_;
-    std::string id_;
-    decode(type_, bl);
-    decode(id_, bl);
-    set(type_, id_);
-  }
+  EntityName() = default;
+  explicit EntityName(entity_type_t t) : type(t), id("*"sv) {}
+
+  void encode(ceph::buffer::list& bl) const;
+  void decode(ceph::buffer::list::const_iterator& bl);
   void dump(ceph::Formatter *f) const;
   static void generate_test_instances(std::list<EntityName*>& ls);
   const std::string& to_str() const;
   const char *to_cstr() const;
   bool from_str(std::string_view s);
-  void set(uint32_t type_, std::string_view id_);
+  void set(entity_type_t type_, std::string_view id_);
   int set(std::string_view type_, std::string_view id_);
-  void set_type(uint32_t type_);
+  void set_type(entity_type_t type_);
   int set_type(std::string_view type);
   void set_id(std::string_view id_);
   void set_name(entity_name_t n);
 
   const char* get_type_str() const;
 
-  uint32_t get_type() const { return type; }
+  entity_type_t get_type() const { return type; }
   bool is_osd() const { return get_type() == CEPH_ENTITY_TYPE_OSD; }
   bool is_mgr() const { return get_type() == CEPH_ENTITY_TYPE_MGR; }
   bool is_mds() const { return get_type() == CEPH_ENTITY_TYPE_MDS; }
@@ -67,7 +62,8 @@ struct EntityName
   bool has_default_id() const;
 
   static std::string get_valid_types_as_str();
-  static uint32_t str_to_ceph_entity_type(std::string_view);
+  static entity_type_t str_to_ceph_entity_type(std::string_view);
+  static std::string_view ceph_entity_type_to_str(entity_type_t type);
 
   friend bool operator<(const EntityName& a, const EntityName& b);
   friend std::ostream& operator<<(std::ostream& out, const EntityName& n);
@@ -77,17 +73,9 @@ struct EntityName
   }
 
 private:
-  struct str_to_entity_type_t {
-    uint32_t type;
-    const char *str;
-  };
-  static const std::array<str_to_entity_type_t, 6> STR_TO_ENTITY_TYPE;
-
-  uint32_t type = 0;
+  entity_type_t type = 0;
   std::string id;
   std::string type_id;
 };
 
 WRITE_CLASS_ENCODER(EntityName)
-
-#endif
