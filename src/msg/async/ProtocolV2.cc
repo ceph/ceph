0911072c1b0bbc97c13b5e9a3b9661dc47d4ee4f
Kamoltat Sirivadhna <ksirivad@redhat.com>
2025-06-05 20:14:57 +0000

    ProtocolV2.cc: Add more loggings to functions that needs them
    Fixes: https://tracker.ceph.com/issues/71344
    
    Signed-off-by: Kamoltat Sirivadhna <ksirivad@redhat.com>
    (cherry picked from commit f31caa1d045d40c13978866adb4b39acb25218fe)

--- src/msg/async/ProtocolV2.cc
+++ src/msg/async/ProtocolV2.cc
@@ -1899,6 +1899,7 @@ CtPtr ProtocolV2::handle_auth_done(ceph::bufferlist &payload)
 }
 
 CtPtr ProtocolV2::finish_client_auth() {
+  ldout(cct, 20) << __func__ << dendl;
   if (HAVE_MSGR2_FEATURE(peer_supported_features, COMPRESSION)) {
     return send_compression_request();
   }
@@ -1907,6 +1908,7 @@ CtPtr ProtocolV2::finish_client_auth() {
 }
 
 CtPtr ProtocolV2::finish_server_auth() {
+  ldout(cct, 20) << __func__ << dendl;
   // server had sent AuthDone and client responded with correct pre-auth
   // signature. 
   // We can start conditioanl msgr protocol
@@ -1923,10 +1925,12 @@ CtPtr ProtocolV2::finish_server_auth() {
 
 CtPtr ProtocolV2::start_session_connect() {
   if (!server_cookie) {
+    ldout(cct, 20) << __func__ << " starting a new session" << dendl;
     ceph_assert(connect_seq == 0);
     state = SESSION_CONNECTING;
     return send_client_ident();
   } else {  // reconnecting to previous session
+    ldout(cct, 20) << __func__ << " reconnecting to session" << dendl;
     state = SESSION_RECONNECTING;
     ceph_assert(connect_seq > 0);
     return send_reconnect();
