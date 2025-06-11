31eea561f453b6487c3c0e2ee2ef72028dcd49e7
Xuehan Xu <xuxuehan@qianxin.com>
2025-06-11 15:09:25 +0800

    crimson/os/seastore/lba_mapping: drop the deprecated "duplicate" method
    Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

--- src/crimson/os/seastore/btree/btree_types.h
+++ src/crimson/os/seastore/btree/btree_types.h
@@ -283,9 +283,6 @@ struct LBACursor : BtreeCursor<laddr_t, lba::lba_map_val_t> {
     assert(!is_indirect());
     return val->refcount;
   }
-  std::unique_ptr<LBACursor> duplicate() const {
-    return std::make_unique<LBACursor>(*this);
-  }
 
   using base_ertr = crimson::errorator<
     crimson::ct_error::input_output_error>;
