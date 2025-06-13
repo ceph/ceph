bf111109d61891b295572158de731579c991682d
Marcel Lauhoff <marcel.lauhoff@clyso.com>
2025-06-13 16:45:41 +0200

    rgw: Early Linux process keyring initialization
    To allow RGW threads to share possession over process keyring keys the
    keyring must be created before a child thread adds keys.
    
    Since we only use the process keyring for KMS cache secrets, only
    initialize the keyring if it is enabled on startup.
    
    Signed-off-by: Marcel Lauhoff <marcel.lauhoff@clyso.com>
    On-behalf-of: SAP marcel.lauhoff@sap.com

--- src/rgw/radosgw-admin/radosgw-admin.cc
+++ src/rgw/radosgw-admin/radosgw-admin.cc
@@ -34,6 +34,7 @@ extern "C" {
 #include "common/errno.h"
 #include "common/safe_io.h"
 #include "common/fault_injector.h"
+#include "common/keyring.h"
 
 #include "common/async/blocked_completion.h"
 
@@ -82,6 +83,7 @@ extern "C" {
 #include "rgw_account.h"
 #include "rgw_bucket_logging.h"
 #include "rgw_dedup_cluster.h"
+#include "rgw_kms.h"
 #include "services/svc_sync_modules.h"
 #include "services/svc_cls.h"
 #include "services/svc_bilog_rados.h"
@@ -1303,6 +1305,7 @@ public:
     : driver(_s), pool(pool) {}
   ~StoreDestructor() {
     driver->shutdown();
+    rgw_kms_cleanup(g_ceph_context);
     pool->finish();
     DriverManager::close_storage(driver);
     rgw_http_client_cleanup();
