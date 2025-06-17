2c526da48455c4158ab3c9ef0c2bc06e5975ab9b
Laura Flores <lflores@ibm.com>
2025-06-17 20:45:44 +0000

    (to be squashed) qa/workunits/mgr: improve error handling in workunit
    Signed-off-by: Laura Flores <lflores@ibm.com>

--- qa/workunits/mgr/test_mgr_module_loading_time.sh
+++ qa/workunits/mgr/test_mgr_module_loading_time.sh
@@ -1,25 +1,53 @@
 #!/bin/bash
 
-# Set up orchestrator...
-# This will create CEPHADM_STRAY_HOST warnings, but we just want to be able to run an orch command.
-ceph mgr module enable cephadm
-ceph orch set backend cephadm
+setup_cephadm() {
+    # This will create CEPHADM_STRAY_HOST warnings, but we just want to be able to run an orch command.
+    echo "Enabling cephadm module..."
+    ceph mgr module enable cephadm
+    ceph orch set backend cephadm
+}
 
-# Check cluster status
-ceph -s
+check_cluster_status() {
+    echo "Checking cluster status..."
+    ceph -s
+}
 
-# Delay loading on the balancer module
-ceph config set mgr mgr_module_load_delay_name balancer
+set_balancer_delay() {
+    echo "Setting balancer module load delay..."
+    ceph config set mgr mgr_module_load_delay_name balancer
+    ceph config set mgr mgr_module_load_delay 10000
+}
 
-# Test if an excess loading time is properly handled
-echo "Testing with module load delay of 10000 ms..."
-ceph config set mgr mgr_module_load_delay 10000
+test_loading_time() {
+    echo "Testing with module load delay of 10000 ms..."
+    ceph mgr fail
 
-output=$(ceph mgr fail; ceph orch status 2>&1)
-echo "$output"
-if [[ "$output" == *"Error ENOTSUP: Warning: due to ceph-mgr restart, some PG states may not be up to date"* ]]; then
-    echo "FAIL: Excess loading time was not properly supported."
-    exit 1
-else
-    echo "PASS: Excess loading time was properly supported."
-fi
+    local orch_status_output
+    if ! orch_status_output=$(ceph orch status 2>&1); then
+        echo "FAIL: 'ceph orch status' failed to run:"
+        echo "$orch_status_output"
+        exit 1
+    fi
+
+    echo "$orch_status_output"
+
+    if [[ "$orch_status_output" == *"Backend: cephadm"* ]]; then
+        echo "PASS: Excess loading time was properly supported."
+    elif [[ "$orch_status_output" == *"Error ENOTSUP: Warning: due to ceph-mgr restart, some PG states may not be up to date"* ]]; then
+        echo "FAIL: Excess loading time was not properly supported."
+        exit 1
+    else
+        echo "FAIL: Unexpected error in 'ceph orch status':"
+        echo "$orch_status_output"
+        exit 1
+    fi
+}
+
+main() {
+    setup_cephadm || return 1
+    check_cluster_status || return 1
+    set_balancer_delay || return 1
+    test_loading_time || return 1
+}
+
+main "$@"
