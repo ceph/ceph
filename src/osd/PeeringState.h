49f0667804b65306a8e8bab7e6f245300f8b2cd3
Bill Scales <bill_scales@uk.ibm.com>
2025-06-19 13:41:17 +0100

    osd: EC Optimizations proc_replica_log needs to apply pwlc
    PeeringState::proc_replica_log needs to apply pwlc before
    calling PGLog so that any partial writes that have occurred
    are taken into account when working out where a replica/stray
    has diverged from the primary.
    
    Signed-off-by: Bill Scales <bill_scales@uk.ibm.com>
    (cherry picked from commit 6c3c0a88b68e2548df670dbe9797d54f89259398)

--- src/osd/PeeringState.h
+++ src/osd/PeeringState.h
@@ -1787,7 +1787,7 @@ private:
   void proc_master_log(ObjectStore::Transaction& t, pg_info_t &oinfo,
 		       pg_log_t&& olog, pg_missing_t&& omissing,
 		       pg_shard_t from);
-  void proc_replica_log(pg_info_t &oinfo, const pg_log_t &olog,
+  void proc_replica_log(pg_info_t &oinfo, pg_log_t &olog,
 			pg_missing_t&& omissing, pg_shard_t from);
 
   void calc_min_last_complete_ondisk();
