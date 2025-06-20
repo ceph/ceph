ceph/ceph

commit 52a09f2465b08e74c5b2fba715f41bca809d5acf

common/static_ptr: avoid using std::aligned_storage_t
std::aligned_storage_t was deprecated in C++23, to be prepared for it,
let's use alignas for the same behavior. because the size of `Base`
class is not always power-of-2, while `alignas()` requires an alignment
of power of 2. so we use `std::bit_ceil()` to calculate the minimum
alignment greater or equal to its size.

Signed-off-by: Kefu Chai <tchaikov@gmail.com>

src/common/static_ptr.h
@@ -14,6 +14,7 @@
 
 #pragma once
 
+#include <bit>
 #include <cstddef>
 #include <utility>
 #include <type_traits>
@@ -100,7 +101,9 @@ class static_ptr {
   // difference in semantics between a pointer-to-const and a const
   // pointer.
   //
-  mutable typename std::aligned_storage<Size>::type buf;
+  mutable struct alignas(std::bit_ceil(Size)) {
+    unsigned char data[sizeof(Base)];
+  } buf;
 
 public:
   using element_type = Base;

commit da847802db725fa0200123a99694aee97e93e06a

common/static_ptr: pass an integer to alignas to fix GCC-11 build failure
GCC-11 fails to compile `alignas(std::bit_ceil(Size))` despite std::bit_ceil()
being marked constexpr in libstdc++11. The compiler doesn't recognize it as a
constant expression, while GCC-12+ and Clang-14+ handle it correctly.

Define the alignment value as a separate constexpr variable before passing it
to alignas() to ensure compatibility with GCC-11.

Fixes compilation issue introduced in commit 52a09f24 when std::aligned_storage_t
was replaced with alignas.

Signed-off-by: Kefu Chai <tchaikov@gmail.com>

src/common/static_ptr.h
@@ -101,7 +101,8 @@ class static_ptr {
   // difference in semantics between a pointer-to-const and a const
   // pointer.
   //
-  mutable struct alignas(std::bit_ceil(Size)) {
+  static constexpr std::size_t Alignment = std::bit_ceil(Size);
+  mutable struct alignas(Alignment) {
     unsigned char data[sizeof(Base)];
   } buf;
 