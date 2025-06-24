68e72559773f1eee5ac2c9d1b532ad72556ef63d
Casey Bodley <cbodley@redhat.com>
2025-06-24 13:48:22 -0400

    rgw/s3: reject PutBucket/ObjectAcl for BucketOwnerEnforced
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -352,6 +352,7 @@ inline constexpr const char* RGW_REST_STS_XMLNS =
 #define ERR_PRESIGNED_URL_DISABLED     2224
 #define ERR_AUTHORIZATION        2225 // SNS 403 AuthorizationError
 #define ERR_ILLEGAL_LOCATION_CONSTRAINT_EXCEPTION 2226
+#define ERR_ACLS_NOT_SUPPORTED   2227 // 400 AccessControlListNotSupported
 
 #define ERR_BUSY_RESHARDING      2300 // also in cls_rgw_types.h, don't change!
 #define ERR_NO_SUCH_ENTITY       2301
