0201f86e6939a3d787bea755a48cb4b4254d2f9c
Patrick Donnelly <pdonnell@ibm.com>
2025-06-27 14:38:17 -0400

    mds: allow disabling batch ops
    To address a bug and future ones where batching lookup/getattr does not help
    "kick" the MDS in switching state more quickly (e.g. flushing the MDS journal).
    
    Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>

--- src/mds/Server.h
+++ src/mds/Server.h
@@ -607,6 +607,7 @@ private:
   unsigned delegate_inos_pct = 0;
   uint64_t dir_max_entries = 0;
   int64_t bal_fragment_size_max = 0;
+  bool allow_batched_ops = true;
 
   double inject_rename_corrupt_dentry_first = 0.0;
 
