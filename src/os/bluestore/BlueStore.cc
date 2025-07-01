510dea472092874d350cba07a61ad0deff043328
Adam Kupczyk <akupczyk@ibm.com>
2025-07-01 13:25:38 +0000

    os/bluestore: Add "hidden_recovery_compare" action to CBT
    New command compares 2 recovery modes:
     - legacy
     - new multithreaded
    The command is hidden - it does not show in help.
    Its role is devel & test only.
    
    Signed-off-by: Adam Kupczyk <akupczyk@ibm.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -20929,6 +20929,77 @@ int BlueStore::read_allocation_from_drive_for_bluestore_tool()
   return ret;
 }
 
+int BlueStore::compare_allocation_recovery_for_bluestore_tool()
+{
+  dout(5) << __func__ << dendl;
+  int ret = 0;
+  ret = _open_db_and_around(true, false);
+  if (ret < 0) {
+    return ret;
+  }
+
+  ret = _open_collections();
+  if (ret < 0) {
+    _close_db_and_around();
+    return ret;
+  }
+  auto shutdown_cache = make_scope_guard([&] {
+    _shutdown_cache();
+    _close_db_and_around();
+  });
+
+  SimpleBitmap old_bitmap(cct, (bdev->get_size()/ min_alloc_size));
+  SimpleBitmap mt_bitmap(cct, (bdev->get_size()/ min_alloc_size));
+  utime_t start;
+  utime_t duration;
+  read_alloc_stats_t old_stats = {};
+  read_alloc_stats_t mt_stats = {};
+
+  if (cct->_conf.get_val<uint64_t>("bluestore_allocation_recovery_threads") != 0) {
+    dout(0) << "New recovery start" << dendl;
+    start = ceph_clock_now();
+    ret = read_allocation_from_onodes_mt(&mt_bitmap, mt_stats);
+    duration = ceph_clock_now() - start;
+    dout(0) << "New recovery result=" << ret << " took " << duration << " seconds" << dendl;
+    dout(0) << "New recovery stats=" << std::endl << mt_stats << dendl;
+  }
+
+  dout(0) << "Legacy recovery start" << dendl;
+  start = ceph_clock_now();
+  ret = read_allocation_from_onodes(&old_bitmap, old_stats);
+  duration = ceph_clock_now() - start;
+  dout(0) << "Legacy recovery result=" << ret << " took " << duration << " seconds" << dendl;
+  dout(0) << "Legacy recovery stats=" << std::endl << old_stats << dendl;
+
+  if (old_stats.actual_pool_vstatfs == mt_stats.actual_pool_vstatfs)
+    dout(0) << "FSstats the same." << dendl;
+  else {
+    dout(0) << "FSTATS DIFFERENT !" << dendl;
+    ret = -1;
+  }
+
+  extent_t ext_a;
+  extent_t ext_b;
+  uint64_t offset = 0;
+  do {
+    ext_a = mt_bitmap.get_next_set_extent(offset);
+    ext_b = old_bitmap.get_next_set_extent(offset);
+    if (ext_a != ext_b) {
+      dout(0) << "ALLOCATOR DIFFERENT !, first:" << dendl;
+      dout(0) << ext_a.offset << "~" << ext_a.length << dendl;
+      dout(0) << ext_b.offset << "~" << ext_b.length << dendl;
+      offset = 1;
+      ret = -1;
+      break;
+    }
+    offset = ext_a.offset + ext_a.length;
+  } while (offset != 0);
+  if (offset == 0) {
+    dout(0) << "Allocators the same." << dendl;
+  }
+  return ret;
+}
+
 //---------------------------------------------------------
 Allocator* BlueStore::clone_allocator_without_bluefs(Allocator *src_allocator)
 {
