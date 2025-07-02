19d2d9f9a992bdc1df481e84f1c4bd921519622b
Adam C. Emerson <aemerson@redhat.com>
2025-07-02 13:49:13 -0400

    neorados/cls/fifo: Get rid of `lazy_open`
    Not needed in a bugfix, we can do that later, and I don't want to hold
    this up on reworking tests for it.
    
    Will squash later.
    
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/neorados/cls/fifo.h
+++ src/neorados/cls/fifo.h
@@ -27,8 +27,6 @@
 
 #include "fifo/detail/fifo.h"
 
-#include <atomic>
-#include <boost/asio/associated_immediate_executor.hpp>
 #include <cstdint>
 #include <deque>
 #include <memory>
@@ -39,11 +37,9 @@
 
 #include <fmt/format.h>
 
-#include <boost/asio/append.hpp>
-#include <boost/asio/as_tuple.hpp>
 #include <boost/asio/async_result.hpp>
+#include <boost/asio/consign.hpp>
 #include <boost/asio/co_composed.hpp>
-#include <boost/asio/dispatch.hpp>
 
 #include <boost/system/error_code.hpp>
 #include <boost/system/system_error.hpp>
@@ -83,48 +79,7 @@ public:
 
 private:
 
-  /// For lazy opening, hold the parameters here
-  struct lazy_preopen {
-    /// RADOS handle
-    std::optional<neorados::RADOS> rados;
-    /// Head object
-    neorados::Object obj;
-    /// Locator
-    neorados::IOContext ioc;
-    /// Open specific version only
-    std::optional<rados::cls::fifo::objv> objv;
-    /// Prefix for data objects
-    std::optional<std::string> oid_prefix;
-    /// Fail if the FIFO already exists
-    const bool exclusive = false;
-    /// Maximum size per part
-    const std::uint64_t max_part_size = 0;
-    /// Maximum entry size
-    const std::uint64_t max_entry_size = 0;
-    /// If we are actually creating
-    const bool create = false;
-  };
-
-  /// This makes the interface object larger than I'd like, but the
-  /// alternative is allocating.
-  lazy_preopen preopen;
-
-  std::atomic<std::shared_ptr<detail::FIFOImpl>> impl;
-
-  FIFO(neorados::RADOS rados, neorados::Object obj, neorados::IOContext ioc,
-       std::optional<rados::cls::fifo::objv> objv,
-       std::optional<std::string> oid_prefix, bool exclusive,
-       std::uint64_t max_part_size, std::uint64_t max_entry_size, bool create)
-    : executor(rados.get_executor()),
-      preopen{.rados = std::move(rados),
-	      .obj = std::move(obj),
-	      .ioc = std::move(ioc),
-	      .objv = std::move(objv),
-	      .oid_prefix = std::move(oid_prefix),
-	      .exclusive = exclusive,
-	      .max_part_size = max_part_size,
-	      .max_entry_size = max_entry_size,
-	      .create = create} {}
+  std::shared_ptr<detail::FIFOImpl> impl;
 
   FIFO(std::shared_ptr<detail::FIFOImpl>&& impl)
     : executor(impl->get_executor()), impl(std::move(impl)) {}
@@ -138,70 +93,10 @@ private:
   template<typename CompletionToken>
   auto consign(CompletionToken&& token) {
     return boost::asio::consign(
-      std::forward<CompletionToken>(token), impl.load());
-  }
-
-  /// \brief Create and open a FIFO
-  ///
-  /// \param dpp Prefix provider for debug logging
-  /// \param rados RADOS handle
-  /// \param obj Head object for FIFO
-  /// \param ioc Locator
-  /// \param token Boost.Asio CompletionToken
-  /// \param objv Operation will fail if FIFO exists and is not at this version
-  /// \param oid_prefix Prefix for all objects
-  /// \param exclusive Fail if the FIFO already exists
-  /// \param max_part_size Maximum allowed size of parts
-  /// \param max_entry_size Maximum allowed size of entries
-  ///
-  /// \return A `unique_ptr` to the open FIFO in a way appropriate to
-  /// the completion token.
-  template<boost::asio::completion_token_for<
-    void(boost::system::error_code)> CompletionToken>
-  auto maybe_open(const DoutPrefixProvider* dpp, CompletionToken&& token) {
-    namespace asio = boost::asio;
-    namespace sys = boost::system;
-    auto consigned = consign(std::forward<CompletionToken>(token));
-    return asio::async_initiate<decltype(consigned), void(sys::error_code)>
-      (asio::co_composed<void(sys::error_code)>
-       ([](auto state, const DoutPrefixProvider* dpp, FIFO* f) -> void {
-	 try {
-	   state.throw_if_cancelled(true);
-	   state.reset_cancellation_state(asio::enable_terminal_cancellation());
-
-	   if (f->impl.load()) {
-	     co_return sys::error_code{};
-	   }
-	   auto impl = std::make_shared<detail::FIFOImpl>(*f->preopen.rados,
-							  f->preopen.obj,
-							  f->preopen.ioc);
-
-	   if (f->preopen.create) {
-	     co_await impl->do_create(dpp, f->preopen.objv,
-				      f->preopen.oid_prefix,
-				      f->preopen.exclusive,
-				      f->preopen.max_part_size,
-				      f->preopen.max_entry_size,
-				      asio::deferred);
-	   } else {
-	     co_await impl->do_open(dpp, f->preopen.objv, false, asio::deferred);
-	   }
-	   if (f->impl.load()) {
-	     // We were raced!
-	     impl.reset();
-	   } else {
-	     f->impl.store(std::move(impl));
-	   }
-	   co_return sys::error_code{};
-
-	 } catch (const sys::system_error& e) {
-	   ldpp_dout(dpp, 1) << __PRETTY_FUNCTION__ << ":" << __LINE__
-			     << " failed lazily opening FIFO: " << e.what()
-			     << dendl;
-	   co_return e.code();
-	 }
-       }, get_executor()),
-       consigned, dpp, this);
+      std::forward<CompletionToken>(token),
+      // Even though RADOS holds the executor across operations, we
+      // still need it live between operations.
+      std::make_pair(impl, get_executor()));
   }
 
 public:
@@ -256,28 +151,6 @@ public:
        token, dpp, std::move(rados), std::move(obj), std::move(ioc),
        std::move(objv), probe);
   }
-
-  /// \brief Open an existing FIFO, lazily
-  ///
-  /// \param rados RADOS handle
-  /// \param obj Head object for FIFO
-  /// \param ioc Locator
-  /// \param objv Operation will fail if FIFO is not at this version
-  ///
-  /// \return A `unique_ptr` to a FIFO to be opened at need
-  static auto lazy_open(
-    neorados::RADOS rados,
-    neorados::Object obj,
-    neorados::IOContext ioc,
-    std::optional<rados::cls::fifo::objv> objv = std::nullopt) {
-
-    return std::unique_ptr<FIFO>{
-      new FIFO(std::move(rados), std::move(obj),
-	       std::move(ioc), std::move(objv),
-	       std::nullopt, false, default_max_part_size,
-	       default_max_entry_size, false)};
-  }
-
 
   /// \brief Create and open a FIFO
   ///
@@ -343,36 +216,6 @@ public:
        max_entry_size);
   }
 
-  /// \brief Create and open a FIFO lazily
-  ///
-  /// \param rados RADOS handle
-  /// \param obj Head object for FIFO
-  /// \param ioc Locator
-  /// \param objv Operation will fail if FIFO exists and is not at this version
-  /// \param oid_prefix Prefix for all objects
-  /// \param exclusive Fail if the FIFO already exists
-  /// \param max_part_size Maximum allowed size of parts
-  /// \param max_entry_size Maximum allowed size of entries
-  ///
-  /// \return A `unique_ptr` to the open FIFO in a way appropriate to
-  /// the completion token.
-  static auto lazy_create(
-    neorados::RADOS rados,
-    neorados::Object obj,
-    neorados::IOContext ioc,
-    std::optional<rados::cls::fifo::objv> objv = std::nullopt,
-    std::optional<std::string> oid_prefix = std::nullopt,
-    bool exclusive = false,
-    std::uint64_t max_part_size = default_max_part_size,
-    std::uint64_t max_entry_size = default_max_entry_size) {
-
-    return std::unique_ptr<FIFO>{
-      new FIFO(std::move(rados), std::move(obj),
-	       std::move(ioc), std::move(objv),
-	       std::move(oid_prefix), exclusive,
-	       max_part_size, max_entry_size, true)};
-  }
-
   /// \brief Push entries to the FIFO
   ///
   /// \param dpp Prefix provider for debug logging
@@ -386,32 +229,8 @@ public:
   auto push(const DoutPrefixProvider* dpp,
 	    std::deque<buffer::list> entries,
 	    CompletionToken&& token) {
-    namespace asio = boost::asio;
-    namespace sys = boost::system;
-    namespace buffer = ceph::buffer;
-    auto consigned = asio::consign(std::forward<CompletionToken>(token));
-    return asio::async_initiate<decltype(consigned),
-				void(sys::error_code)>
-      (asio::co_composed<void(sys::error_code)>
-       ([](auto state, const DoutPrefixProvider* dpp,
-	   std::deque<buffer::list> entries, FIFO* f) -> void {
-	 try {
-	   state.throw_if_cancelled(true);
-	   state.reset_cancellation_state(asio::enable_terminal_cancellation());
-	   if (!f->impl.load()) {
-	     co_await f->maybe_open(dpp, asio::deferred);
-	   }
-	   co_return co_await
-	     f->impl.load()->push(dpp, std::move(entries),
-				  asio::as_tuple(asio::deferred));
-	   co_return sys::error_code{};
-	 } catch (const sys::system_error& e) {
-	   ldpp_dout(dpp, -1) << __PRETTY_FUNCTION__ << ":" << __LINE__
-			      << " push failed: " << e.what() << dendl;
-	   co_return e.code();
-	 }
-       }, get_executor()),
-       consigned, dpp, std::move(entries), this);
+    return impl->push(dpp, std::move(entries),
+		      consign(std::forward<CompletionToken>(token)));
   }
 
   /// \brief Push entries to the FIFO
@@ -468,34 +287,8 @@ public:
   auto list(const DoutPrefixProvider* dpp,
 	    std::string markstr, std::span<entry> entries,
 	    CompletionToken&& token) {
-    namespace asio = boost::asio;
-    namespace sys = boost::system;
-    namespace buffer = ceph::buffer;
-    auto consigned = asio::consign(std::forward<CompletionToken>(token));
-    return asio::async_initiate<decltype(consigned),
-				void(sys::error_code, std::span<entry>,
-                                     std::string)>
-      (asio::co_composed<void(sys::error_code,
-			      std::span<entry>,
-			      std::string)>
-       ([](auto state, const DoutPrefixProvider* dpp,
-	   std::string markstr, std::span<entry> entries,
-	   FIFO* f) -> void {
-	 try {
-	   state.throw_if_cancelled(true);
-	   state.reset_cancellation_state(asio::enable_terminal_cancellation());
-
-	   if (!f->impl.load()) {
-	     co_await f->maybe_open(dpp, asio::deferred);
-	   }
-	   co_return co_await
-	     f->impl.load()->list(dpp, std::move(markstr), entries,
-				  asio::as_tuple(asio::deferred));
-	 } catch (const sys::system_error& e) {
-	   co_return {e.code(), std::span<entry>{}, std::string{}};
-	 }
-       }, get_executor()),
-       consigned, dpp, std::move(markstr), std::move(entries), this);
+    return impl->list(dpp, std::move(markstr), entries,
+		      consign(std::forward<CompletionToken>(token)));
   }
 
   /// \brief Trim entries from the FIFO
@@ -513,29 +306,8 @@ public:
   auto trim(const DoutPrefixProvider* dpp,
 	    std::string marker, bool exclusive,
 	    CompletionToken&& token) {
-    namespace asio = boost::asio;
-    namespace sys = boost::system;
-    auto consigned = asio::consign(std::forward<CompletionToken>(token));
-    return asio::async_initiate<decltype(consigned),
-				void(sys::error_code)>
-      (asio::co_composed<void(sys::error_code)>
-       ([](auto state, const DoutPrefixProvider* dpp,
-	   std::string marker, bool exclusive, FIFO* f) -> void {
-	 try {
-	   state.throw_if_cancelled(true);
-	   state.reset_cancellation_state(asio::enable_terminal_cancellation());
-
-	   if (!f->impl.load()) {
-	     co_await f->maybe_open(dpp, asio::deferred);
-	   }
-	   co_return co_await
-	     f->impl.load()->trim(dpp, std::move(marker), exclusive,
-				  asio::as_tuple(asio::deferred));
-	 } catch (const sys::system_error& e) {
-	   co_return e.code();
-	 }
-       }, get_executor()),
-       consigned, dpp, std::move(marker), exclusive, this);
+    return impl->trim(dpp, std::move(marker), exclusive,
+		      consign(std::forward<CompletionToken>(token)));
   }
 
   /// \brief Get information on the last entry
@@ -550,29 +322,8 @@ public:
 	 ceph::real_time)> CompletionToken>
   auto last_entry_info(const DoutPrefixProvider* dpp,
 		       CompletionToken&& token) {
-    namespace asio = boost::asio;
-    namespace sys = boost::system;
-    auto consigned = asio::consign(std::forward<CompletionToken>(token));
-    return asio::async_initiate<
-      decltype(consigned), void(sys::error_code, std::string, ceph::real_time)>
-      (asio::co_composed<void(sys::error_code, std::string, ceph::real_time)>
-       ([](auto state, const DoutPrefixProvider* dpp,
-	   FIFO* f) -> void {
-	 try {
-	   state.throw_if_cancelled(true);
-	   state.reset_cancellation_state(asio::enable_terminal_cancellation());
-
-	   if (!f->impl.load()) {
-	     co_await f->maybe_open(dpp, asio::deferred);
-	   }
-	   co_return co_await
-	     f->impl.load()->last_entry_info(dpp,
-					     asio::as_tuple(asio::deferred));
-	 } catch (const sys::system_error& e) {
-	   co_return {e.code(), std::string{}, ceph::real_time{}};
-	 }
-       }, get_executor()),
-       consigned, dpp, this);
+    return impl->last_entry_info(dpp,
+				 consign(std::forward<CompletionToken>(token)));
   }
 
   static constexpr auto max_list_entries =
