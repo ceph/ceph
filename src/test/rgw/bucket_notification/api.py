a7e9d3e623741b6c018891b87721c97a366535a4
Yuval Lifshitz <ylifshit@ibm.com>
2025-07-03 16:57:39 +0000

    rgw/notifications: test refactoring
    * kafka: pass full broker list to consumer in tests
    * kafka: use ip instead of localhost
    * kafka: make sure topic exists before consumer start
    * kafka: fix zookeeper and broker conf in tests
    * kafka: verify receiver in the test
    * kafka: tests were not running (Fixes: https://tracker.ceph.com/issues/72240)
    * kafka: failover tests were failing (Fixes: https://tracker.ceph.com/issues/71585)
    * simplify basic tests run command
    * v2 migration tests were not running
    * fix failing migration tests
    
    Signed-off-by: Yuval Lifshitz <ylifshit@ibm.com>

--- src/test/rgw/bucket_notification/api.py
+++ src/test/rgw/bucket_notification/api.py
@@ -247,13 +247,19 @@ def delete_all_topics(conn, tenant, cluster):
     if tenant == '':
         topics_result = admin(['topic', 'list'], cluster)
         topics_json = json.loads(topics_result[0])
-        for topic in topics_json:
-            rm_result = admin(['topic', 'rm', '--topic', topic['name']], cluster)
-            print(rm_result)
+        try:
+            for topic in topics_json['topics']:
+                admin(['topic', 'rm', '--topic', topic['name']], cluster)
+        except TypeError:
+            for topic in topics_json:
+                admin(['topic', 'rm', '--topic', topic['name']], cluster)
     else:
         topics_result = admin(['topic', 'list', '--tenant', tenant], cluster)
         topics_json = json.loads(topics_result[0])
-        for topic in topics_json:
-            rm_result = admin(['topic', 'rm', '--tenant', tenant, '--topic', topic['name']], cluster)
-            print(rm_result)
+        try:
+            for topic in topics_json['topics']:
+                admin(['topic', 'rm', '--tenant', tenant, '--topic', topic['name']], cluster)
+        except TypeError:
+            for topic in topics_json:
+                admin(['topic', 'rm', '--tenant', tenant, '--topic', topic['name']], cluster)
 
