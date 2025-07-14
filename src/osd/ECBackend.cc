f17a656ee43974d2267ef4797819cacef33c8e27
Jon Bailey <jonathan.bailey1@ibm.com>
2025-07-14 10:26:48 +0100

    DO NOT MERGE TO MAIN - fix to TestECBackend.cc
    Signed-off-by: Alex Ainscow <aainscow@uk.ibm.com>
    
    DO NOT MERGE TO MAIN
    
    Modifies log_stats to not leak memory. Also moves processing to only happen at log level 20.
    
    Intentionally left unsigned to flag up in GitHub actions if it ends up in main.
    
    Jon
    
    DO NOT MERGE TO MAIN
    
    Modifies ceph_test_rados to also do short IOs as well as longer IOs
    
    Intentionally left unsigned to flag up in GitHub actions if it ends up in main.
    
    Jon
    
    DO NOT MERGE TO MAIN
    
    Makes debug messages more consistent in logging of stats.
    
    Intentionally left unsigned to flag up in GitHub actions if it ends up in main.
    
    Jon
    
    DO NOT MERGE TO MAIN
    
    Tightens logic around reporting of when multiple set OIs are called in a single transaction. Also tidies up interfaces for log_stats function and makes log messages more consistent.
    
    Intentionally left unsigned to flag up in GitHub actions if it ends up in main.
    
    Jon
    
    DO NOT MERGE TO MAIN
    
    Adds debugging to help debug stat mismatches detected by scrubbing.
    
    Intentionally left unsigned to flag up in GitHub actions if it ends up in main.
    
    Jon

--- src/osd/ECBackend.cc
+++ src/osd/ECBackend.cc
@@ -396,7 +396,10 @@ void ECBackend::handle_sub_write(
     ceph_abort_msg("Error inject - OSD down");
   }
   if (!get_parent()->pgb_is_primary())
+  {
     get_parent()->update_stats(op.stats);
+    get_parent()->log_stats(op.soid, op.stats.stats.sum, op.t, false);
+  }
   ObjectStore::Transaction localt;
   if (!op.temp_added.empty()) {
     switcher->add_temp_objs(op.temp_added);
