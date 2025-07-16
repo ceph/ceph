7ac69255970146af94b713a869a66f773f98b524
Shweta Bhosale <Shweta.Bhosale1@ibm.com>
2025-07-16 12:53:05 +0530

    mgr/cephadm: NFSSpec changes to apply cluster level QOS while nfs service creation
    Fixes: https://tracker.ceph.com/issues/69458
    Signed-off-by: Shweta Bhosale <Shweta.Bhosale1@ibm.com>

--- src/pybind/mgr/cephadm/services/cephadmservice.py
+++ src/pybind/mgr/cephadm/services/cephadmservice.py
@@ -834,6 +834,9 @@ class CephadmService(metaclass=ABCMeta):
     def get_blocking_daemon_hosts(self, service_name: str) -> List[HostSpec]:
         return []
 
+    def pre_daemon_service_config(self, spec: ServiceSpec) -> None:
+        return
+
     def has_placement_changed(self, deps: List[str], spec: ServiceSpec) -> bool:
         return False
 
