df9c69f5a992354e3ee8492db7bf9a885d1d4cff
Alex Ainscow <aainscow@uk.ibm.com>
2025-07-22 16:47:11 +0100

    Refactor ECBackend (FIXUP)
    Signed-off-by: Alex Ainscow <aainscow@uk.ibm.com>

--- src/osd/ECBackend.h
+++ src/osd/ECBackend.h
@@ -186,6 +186,14 @@ class ECBackend : public ECCommon {
 
   void kick_reads();
 
+  int _objects_read_sync(
+    const hobject_t &hoid,
+    uint64_t off,
+    uint64_t len,
+    uint32_t op_flags,
+    ceph::buffer::list *bl
+  );
+
   /**
    * Recovery
    *
