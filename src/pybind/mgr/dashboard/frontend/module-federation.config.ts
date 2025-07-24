9489931cbded773ee73a1c1ca74839ee38c65dfe
Afreen Misbah <afreen@ibm.com>
2025-07-24 16:59:10 +0530

    mgr/dashboard: Fix issue with loading localization module
    - this module is only required to be imported from polyfill
    -  nx now loading this module as well breaking CI tests and dashboard `@angular/localize`
    
    Signed-off-by: Afreen Misbah <afreen@ibm.com>

--- src/pybind/mgr/dashboard/frontend/module-federation.config.ts
+++ src/pybind/mgr/dashboard/frontend/module-federation.config.ts
@@ -2,23 +2,13 @@ import { ModuleFederationConfig } from '@nx/module-federation';
 
 const config: ModuleFederationConfig = {
   name: 'ceph-dashboard',
-  /**
-   * To use a remote that does not exist in your current Nx Workspace
-   * You can use the tuple-syntax to define your remote
-   *
-   * remotes: [['my-external-remote', 'https://nx-angular-remote.netlify.app']]
-   *
-   * You _may_ need to add a `remotes.d.ts` file to your `src/` folder declaring the external remote for tsc, with the
-   * following content:
-   *
-   * declare module 'my-external-remote';
-   *
-   */
-  remotes: []
+  remotes: [],
+  shared: (libraryName, sharedConfig) => {
+    if (libraryName === '@angular/localize' || libraryName === '@angular/localize/init') {
+      return false; // do not share these, import directly via polyfills
+    }
+    return sharedConfig;
+  },
 };
 
-/**
- * Nx requires a default export of the config to allow correct resolution of the module federation graph.
- **/
 export default config;
-
