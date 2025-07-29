38196689bb53d8cfba33ab570711566e78abc7fb
Laura Flores <lflores@ibm.com>
2025-07-29 22:46:46 +0000

    mgr, common: issue health error after max retries is exceeded
    During a mgr failover, the active mgr is marked available if:
      1. The mon has chosen a standby to be active
      2. The chosen active mgr has all of its modules initialized
    
    Now that we've improved the criteria for sending the "active" beacon
    by enforcing it to retry initializing mgr modules, we need to account
    for extreme cases in which the modules are stuck loading for a very long
    time, or even indefinitely. In these extreme cases where the modules might
    never initialize, we don't want to delay sending the "active" beacon for
    too long. This can result in blocking other important mgr functionality,
    such as reporting PG availability in the health status. We want
    to avoid sending warnings about PGs being unknown in the health status when
    that's not ultimately the problem.
    
    To account for an exeptionally long module loading time, I added a new
    configurable `mgr_module_load_max_retries`. "Retries" corresponds directly
    with how many "active, starting" beacons we send to the mon to allow the
    modules a little extra time to initialize. If we exceed the max retries
    allowed (in other words, if we send the maximum number of "active, starting"
    beacons allowed), the standby will then proceed to mark itself "available" and
    send the "active" beacon to the mon and unblock other critical mgr functionality.
    
    For added clarity, a health error will be issued at this time, indicating
    which mgr modules got stuck initializing (See src/mgr/PyModuleRegistry.cc). The
    idea is to unblock the rest of the mgr's critical functionality while making it
    clear to Ceph operators that some modules are unusable.
    
    Signed-off-by: Laura Flores <lflores@ibm.com>

--- src/mgr/Mgr.h
+++ src/mgr/Mgr.h
@@ -66,6 +66,7 @@ protected:
 
   bool initialized;
   bool initializing;
+  uint64_t initialization_retries;
 
 public:
   Mgr(MonClient *monc_, const MgrMap& mgrmap,
@@ -75,6 +76,8 @@ public:
   ~Mgr();
 
   bool is_initialized() const {return initialized;}
+  bool exceeded_initialization_retires() const;
+  void update_initialization_retries();
   entity_addrvec_t get_server_addrs() const {
     return server.get_myaddrs();
   }
