880a17e39626d99a0b6cc8259523daa83c72802c
Bill Scales <bill_scales@uk.ibm.com>
2025-07-30 12:41:34 +0100

    osd: Optimized EC present_shards no longer needed
    present_shards is no longer needed in the PG log entry, this has been
    replaced with code in proc_master_log that calculates which shards were
    in the last epoch started and are still present.
    
    Signed-off-by: Bill Scales <bill_scales@uk.ibm.com>

--- src/osd/osd_types.cc
+++ src/osd/osd_types.cc
@@ -5064,7 +5064,8 @@ void pg_log_entry_t::encode(ceph::buffer::list &bl) const
     encode(return_code, bl);
   encode(op_returns, bl);
   encode(written_shards, bl);
-  encode(present_shards, bl);
+  shard_id_set unused;
+  encode(unused, bl);
   ENCODE_FINISH(bl);
 }
 
@@ -5138,7 +5139,8 @@ void pg_log_entry_t::decode(ceph::buffer::list::const_iterator &bl)
   }
   if (struct_v >= 15) {
     decode(written_shards, bl);
-    decode(present_shards, bl);
+    shard_id_set unused;
+    decode(unused, bl);
   }
   DECODE_FINISH(bl);
 }
@@ -5190,7 +5192,6 @@ void pg_log_entry_t::dump(Formatter *f) const
     f->close_section();
   }
   f->dump_stream("written_shards") << written_shards;
-  f->dump_stream("present_shards") << present_shards;
   {
     f->open_object_section("mod_desc");
     mod_desc.dump(f);
