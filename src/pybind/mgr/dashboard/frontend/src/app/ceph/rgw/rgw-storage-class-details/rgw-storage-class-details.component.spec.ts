2a6f6f0b00a0054c1352d93ee8428a6286e52853
Dnyaneshwari <dnyaneshwari@li-9c9fbecc-2d5c-11b2-a85c-e2a7cc8a424f.ibm.com>
2025-08-01 10:00:18 +0530

    mgr/dashboard:RGW- Storage Class ACL Mapping
    Fixes: https://tracker.ceph.com/issues/72362
    
    Signed-off-by: Dnyaneshwari Talwekar <dtalwekar@redhat.com>
    (cherry picked from commit 38b237c8cb12cb77fd29a560c10c7e2225786955)

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-storage-class-details/rgw-storage-class-details.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-storage-class-details/rgw-storage-class-details.component.spec.ts
@@ -1,15 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RgwStorageClassDetailsComponent } from './rgw-storage-class-details.component';
 import { StorageClassDetails } from '../models/rgw-storage-class.model';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from '~/app/shared/shared.module';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { SimpleChange } from '@angular/core';
 
 describe('RgwStorageClassDetailsComponent', () => {
   let component: RgwStorageClassDetailsComponent;
   let fixture: ComponentFixture<RgwStorageClassDetailsComponent>;
+
+  const mockSelection: StorageClassDetails = {
+    access_key: 'TestAccessKey',
+    secret: 'TestSecret',
+    target_path: '/test/path',
+    multipart_min_part_size: 100,
+    multipart_sync_threshold: 200,
+    host_style: 'path',
+    retain_head_object: true,
+    allow_read_through: true,
+    tier_type: 'local',
+    acl_mappings: []
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,6 +37,8 @@ describe('RgwStorageClassDetailsComponent', () => {
 
     fixture = TestBed.createComponent(RgwStorageClassDetailsComponent);
     component = fixture.componentInstance;
+    component.selection = mockSelection;
+
     fixture.detectChanges();
   });
 
@@ -32,19 +47,28 @@ describe('RgwStorageClassDetailsComponent', () => {
   });
 
   it('should update storageDetails when selection input changes', () => {
-    const mockSelection: StorageClassDetails = {
-      access_key: 'TestAccessKey',
-      secret: 'TestSecret',
-      target_path: '/test/path',
-      multipart_min_part_size: 100,
-      multipart_sync_threshold: 200,
-      host_style: 'path',
-      retain_head_object: true,
-      allow_read_through: true,
-      tier_type: 'local'
+    const newSelection: StorageClassDetails = {
+      access_key: 'NewAccessKey',
+      secret: 'NewSecret',
+      target_path: '/new/path',
+      multipart_min_part_size: 500,
+      multipart_sync_threshold: 1000,
+      host_style: 'virtual',
+      retain_head_object: false,
+      allow_read_through: false,
+      tier_type: 'archive',
+      glacier_restore_days: 1,
+      glacier_restore_tier_type: 'standard',
+      placement_targets: '',
+      read_through_restore_days: 7,
+      restore_storage_class: 'restored',
+      zonegroup_name: 'zone1'
     };
-    component.selection = mockSelection;
-    component.ngOnChanges();
-    expect(component.storageDetails).toEqual(mockSelection);
+
+    component.selection = newSelection;
+    component.ngOnChanges({
+      selection: new SimpleChange(null, newSelection, false)
+    });
+    expect(component.storageDetails).toEqual(newSelection);
   });
 });
