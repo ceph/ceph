2abc6abea1ef72b282a35f9d241877b84a92ac08
Jaya Prakash <jayaprakash@ibm.com>
2025-08-04 18:05:12 +0000

    os/bluestore: Move Blob to bluestore namespace in BlueStore_objects
    Signed-off-by: Jaya Prakash <jayaprakash@ibm.com>

---
+++ src/os/bluestore/BlueStore_objects.h
@@ -0,0 +1,198 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2014 Red Hat
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
+#ifndef CEPH_OSD_BLUESTORE_BLUESTORE_OBJECTS_H
+#define CEPH_OSD_BLUESTORE_BLUESTORE_OBJECTS_H
+
+#include <atomic>
+#include <bit>
+#include <mutex>
+#include <condition_variable>
+#include <memory_resource>
+#include <new>
+
+#include "bluestore_types.h"
+#include "BlueStore.h"
+
+namespace bluestore {
+
+  /// in-memory blob metadata and associated cached buffers (if any)
+  struct Blob {
+    MEMPOOL_CLASS_HELPERS();
+
+    std::atomic_int nref = {0};     ///< reference count
+    int16_t id = -1;                ///< id, for spanning blobs only, >= 0
+    int16_t last_encoded_id = -1;   ///< (ephemeral) used during encoding only
+    BlueStore::CollectionRef collection;
+
+    void set_shared_blob(BlueStore::SharedBlobRef sb);
+    Blob(BlueStore::CollectionRef collection) : collection(collection) {}
+  private:
+    BlueStore::SharedBlobRef shared_blob;      ///< shared blob state (if any)
+    mutable bluestore_blob_t blob;  ///< decoded blob metadata
+    /// refs from this shard.  ephemeral if id<0, persisted if spanning.
+    bluestore_blob_use_tracker_t used_in_blob;
+
+  public:
+
+    friend void intrusive_ptr_add_ref(Blob *b) { b->get(); }
+    friend void intrusive_ptr_release(Blob *b) { b->put(); }
+
+    void dump(ceph::Formatter* f) const;
+    friend std::ostream& operator<<(std::ostream& out, const Blob &b);
+    struct printer : public BlueStore::printer {
+      const Blob& blob;
+      uint16_t mode;
+      printer(const Blob& blob, uint16_t mode)
+      :blob(blob), mode(mode) {}
+    };
+    friend std::ostream& operator<<(std::ostream& out, const printer &p);
+    printer print(uint16_t mode) const {
+      return printer(*this, mode);
+    }
+    const bluestore_blob_use_tracker_t& get_blob_use_tracker() const {
+      return used_in_blob;
+    }
+    bluestore_blob_use_tracker_t& dirty_blob_use_tracker() {
+      return used_in_blob;
+    }
+
+    const BlueStore::SharedBlobRef& get_shared_blob() const {
+      return shared_blob;
+    }
+
+    BlueStore::SharedBlobRef& get_dirty_shared_blob() {
+      return shared_blob;
+    }
+
+    bool is_referenced() const {
+      return used_in_blob.is_not_empty();
+    }
+    uint32_t get_referenced_bytes() const {
+      return used_in_blob.get_referenced_bytes();
+    }
+
+    bool is_spanning() const {
+      return id >= 0;
+    }
+
+    bool can_split() {
+      // splitting a BufferSpace writing list is too hard; don't try.
+      return used_in_blob.can_split() &&
+             get_blob().can_split();
+    }
+
+    bool can_merge_blob(const Blob* other, uint32_t& blob_end) const;
+    uint32_t merge_blob(CephContext* cct, Blob* blob_to_dissolve);
+
+    bool can_split_at(uint32_t blob_offset) const {
+      return used_in_blob.can_split_at(blob_offset) &&
+             get_blob().can_split_at(blob_offset);
+    }
+
+    bool can_reuse_blob(uint32_t min_alloc_size,
+			uint32_t target_blob_size,
+			uint32_t b_offset,
+			uint32_t *length0);
+
+    void dup(Blob& o) {
+      o.set_shared_blob(shared_blob);
+      o.blob = blob;
+    }
+    void add_tail(uint32_t new_blob_size, uint32_t min_release_size);
+    void dup(const Blob& from, bool copy_used_in_blob);
+    void copy_from(CephContext* cct, const Blob& from,
+		   uint32_t min_release_size, uint32_t start, uint32_t len);
+    void copy_extents(CephContext* cct, const Blob& from, uint32_t start,
+		      uint32_t pre_len, uint32_t main_len, uint32_t post_len);
+    void copy_extents_over_empty(CephContext* cct, const Blob& from, uint32_t start, uint32_t len);
+
+    inline const bluestore_blob_t& get_blob() const {
+      return blob;
+    }
+    inline bluestore_blob_t& dirty_blob() {
+      return blob;
+    }
+
+    /// get logical references
+    void get_ref(BlueStore::Collection *coll, uint32_t offset, uint32_t length);
+    /// put logical references, and get back any released extents
+    bool put_ref(BlueStore::Collection *coll, uint32_t offset, uint32_t length,
+		 PExtentVector *r);
+    uint32_t put_ref_accumulate(
+      BlueStore::Collection *coll,
+      uint32_t offset,
+      uint32_t length,
+      PExtentVector *released_disk);
+    /// split the blob
+    void split(BlueStore::Collection *coll, uint32_t blob_offset, Blob *o);
+
+    void maybe_prune_tail();
+
+    void get() {
+      ++nref;
+    }
+    void put() {
+      if (nref.load(std::memory_order_acquire) == 1) {
+        delete this;
+        return;
+      }
+      if (--nref == 0)
+	delete this;
+    }
+    bool is_shared_loaded() const;
+    BlueStore::BufferCacheShard* get_cache();
+    uint64_t get_sbid() const;
+    BlueStore::CollectionRef get_collection() const {
+      return collection;
+    }
+
+    ~Blob();
+
+    void bound_encode(
+      size_t& p,
+      uint64_t struct_v,
+      uint64_t sbid,
+      bool include_ref_map) const {
+      denc(blob, p, struct_v);
+      if (blob.is_shared()) {
+        denc(sbid, p);
+      }
+      if (include_ref_map) {
+	used_in_blob.bound_encode(p);
+      }
+    }
+    void encode(
+      ceph::buffer::list::contiguous_appender& p,
+      uint64_t struct_v,
+      uint64_t sbid,
+      bool include_ref_map) const {
+      denc(blob, p, struct_v);
+      if (blob.is_shared()) {
+        denc(sbid, p);
+      }
+      if (include_ref_map) {
+	used_in_blob.encode(p);
+      }
+    }
+    void decode(
+      ceph::buffer::ptr::const_iterator& p,
+      uint64_t struct_v,
+      uint64_t* sbid,
+      bool include_ref_map,
+      BlueStore::Collection *coll);
+  };
+}
+
+#endif
