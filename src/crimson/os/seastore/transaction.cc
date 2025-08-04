c3621dace75475fcddf12fba285101bb6bfdb1ae
Matan Breizman <mbreizma@redhat.com>
2025-08-04 10:28:46 +0000

    crimson/os/seastore/transaction: fix circular dependency
    transaction_interruptor includes transaction and vice verca.
    With the previous commits in this PR, we can not drop transaction
    from including transaction_interruptor.
    
    * move interrupt_cond_t to transaction_interruptor.
    
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>

--- src/crimson/os/seastore/transaction.cc
+++
@@ -1,8 +0,0 @@
-#include "transaction.h"
-#include "crimson/common/interruptible_future.h"
-
-namespace crimson::interruptible {
-template
-thread_local interrupt_cond_t<::crimson::os::seastore::TransactionConflictCondition>
-interrupt_cond<::crimson::os::seastore::TransactionConflictCondition>;
-}
