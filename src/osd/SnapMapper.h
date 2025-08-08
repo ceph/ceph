884d6054545fae2c92203cc4852b651712aa0169
Naveen Naidu <naveennaidu479@gmail.com>
2025-08-08 10:31:13 +0530

    src/osd: replace tl::expected to std::expected
    Fixes: https://tracker.ceph.com/issues/72487
    
    Signed-off-by: Naveen Naidu <naveen.naidu@ibm.com>

--- src/osd/SnapMapper.h
+++ src/osd/SnapMapper.h
@@ -16,6 +16,7 @@
 #define SNAPMAPPER_H
 
 #include <cstring>
+#include <expected>
 #include <map>
 #include <set>
 #include <string>
@@ -273,7 +274,7 @@ private:
     );
 
   /// Get snaps (as an 'object_snaps' object) for oid
-  tl::expected<object_snaps, SnapMapReaderI::result_t> get_snaps_common(
+  std::expected<object_snaps, SnapMapReaderI::result_t> get_snaps_common(
     const hobject_t &hoid) const;
 
   /// \returns vector with the first objects with @snap as a snap
@@ -368,7 +369,7 @@ private:
     MapCacher::Transaction<std::string, ceph::buffer::list> *t);
 
   /// Get snaps for oid - alternative interface
-  tl::expected<std::set<snapid_t>, SnapMapReaderI::result_t> get_snaps(
+  std::expected<std::set<snapid_t>, SnapMapReaderI::result_t> get_snaps(
     const hobject_t &hoid) const final;
 
   /**
@@ -377,7 +378,7 @@ private:
    * Returns snaps for hoid as in get_snaps(), but additionally validates the
    * snap->hobject_t mappings ('SNA_' entries).
    */
-  tl::expected<std::set<snapid_t>, SnapMapReaderI::result_t>
+  std::expected<std::set<snapid_t>, SnapMapReaderI::result_t>
   get_snaps_check_consistency(const hobject_t &hoid) const final;
 };
 WRITE_CLASS_ENCODER(SnapMapper::object_snaps)
