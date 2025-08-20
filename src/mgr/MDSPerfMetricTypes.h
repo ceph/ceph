20b0d965d542e45dd42bed2a9dc92ccaf60ab0d5
Max Kellermann <max.kellermann@ionos.com>
2025-08-20 09:54:14 +0200

    mgr/MDSPerfMetricTypes: include cleanup
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/mgr/MDSPerfMetricTypes.h
+++ src/mgr/MDSPerfMetricTypes.h
@@ -20,7 +20,6 @@
 #include "include/utime.h"
 #include "common/Formatter.h"
 
-#include "mds/mdstypes.h"
 #include "mgr/Types.h"
 
 typedef std::vector<std::string> MDSPerfMetricSubKey; // array of regex match
