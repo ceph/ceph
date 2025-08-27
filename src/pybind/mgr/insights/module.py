ef927260989630844951c0194c66bb67adffea54
Nitzan Mordechai <nmordech@redhat.com>
2025-08-27 10:49:40 +0000

    mgr/insights: fix read-only copy before change
    Fixes: https://tracker.ceph.com/issues/72447
    Signed-off-by: Nitzan Mordechai <nmordec@ibm.com>

--- src/pybind/mgr/insights/module.py
+++ src/pybind/mgr/insights/module.py
@@ -270,8 +270,9 @@ class Module(MgrModule):
         report["config"] = config
         health_check_details.extend(health_details)
 
-        osd_map = self.get("osd_map")
-        del osd_map['pg_temp']
+        osd_map_ro = self.get("osd_map")
+        osd_map = dict(osd_map_ro)
+        osd_map.pop("pg_temp", None)
         self._apply_osd_stats(osd_map)
         report["osd_dump"] = osd_map
 
