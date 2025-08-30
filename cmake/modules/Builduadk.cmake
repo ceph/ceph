a48fef121341cfd073af3e8edf86715df7a9fb47
Dan Mick <dan.mick@redhat.com>
2025-08-30 00:34:55 -0700

    cmake/modules/Builduadk.cmake: fix tabs/spaces
    Signed-off-by: Dan Mick <dan.mick@redhat.com>

--- cmake/modules/Builduadk.cmake
+++ cmake/modules/Builduadk.cmake
@@ -10,22 +10,22 @@ function(build_uadk)
 
     include(ExternalProject)
     ExternalProject_Add(uadk_ext
-	    UPDATE_COMMAND "" # this disables rebuild on each run
-	    GIT_REPOSITORY "https://github.com/ceph/uadk.git"
-            GIT_CONFIG advice.detachedHead=false
-            GIT_TAG 19f650cae960304e3c674992a4c7d5d56a8f4efa
-            SOURCE_DIR "${PROJECT_SOURCE_DIR}/src/uadk"
-            BUILD_IN_SOURCE 1
-            CMAKE_ARGS -DCMAKE_CXX_COMPILER=which g++
-            CONFIGURE_COMMAND ./autogen.sh COMMAND ${configure_cmd}
-            BUILD_COMMAND make
-	    BUILD_BYPRODUCTS ${UADK_WD_LIBRARY} ${UADK_WD_COMP_LIBRARY} ${UADK_WD_ZIP_LIBRARY}
-            INSTALL_COMMAND make install
-            LOG_CONFIGURE ON
-            LOG_BUILD ON
-            LOG_INSTALL ON
-            LOG_MERGED_STDOUTERR ON
-            LOG_OUTPUT_ON_FAILURE ON)
+        UPDATE_COMMAND "" # this disables rebuild on each run
+        GIT_REPOSITORY "https://github.com/ceph/uadk.git"
+        GIT_CONFIG advice.detachedHead=false
+        GIT_TAG 19f650cae960304e3c674992a4c7d5d56a8f4efa
+        SOURCE_DIR "${PROJECT_SOURCE_DIR}/src/uadk"
+        BUILD_IN_SOURCE 1
+        CMAKE_ARGS -DCMAKE_CXX_COMPILER=which g++
+        CONFIGURE_COMMAND ./autogen.sh COMMAND ${configure_cmd}
+        BUILD_COMMAND make
+        BUILD_BYPRODUCTS ${UADK_WD_LIBRARY} ${UADK_WD_COMP_LIBRARY} ${UADK_WD_ZIP_LIBRARY}
+        INSTALL_COMMAND make install
+        LOG_CONFIGURE ON
+        LOG_BUILD ON
+        LOG_INSTALL ON
+        LOG_MERGED_STDOUTERR ON
+        LOG_OUTPUT_ON_FAILURE ON)
 
     ExternalProject_Get_Property(uadk_ext source_dir)
     set(UADK_INCLUDE_DIR ${UADK_INCLUDE_DIR} PARENT_SCOPE)
@@ -40,13 +40,13 @@ function(build_uadk)
     set_target_properties(uadk::uadk PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES ${UADK_INCLUDE_DIR}
         IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-	IMPORTED_LOCATION "${UADK_WD_COMP_LIBRARY}")
+        IMPORTED_LOCATION "${UADK_WD_COMP_LIBRARY}")
     set_target_properties(uadk::uadkwd PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES ${UADK_INCLUDE_DIR}
         IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-	IMPORTED_LOCATION "${UADK_WD_LIBRARY}")
+        IMPORTED_LOCATION "${UADK_WD_LIBRARY}")
     set_target_properties(uadk::uadkzip PROPERTIES
         INTERFACE_INCLUDE_DIRECTORIES ${UADK_INCLUDE_DIR}
         IMPORTED_LINK_INTERFACE_LANGUAGES "C"
-	IMPORTED_LOCATION "${UADK_WD_ZIP_LIBRARY}")
+        IMPORTED_LOCATION "${UADK_WD_ZIP_LIBRARY}")
 endfunction()
