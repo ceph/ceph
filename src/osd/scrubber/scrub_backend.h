6546211b1670cfbc3a23706dc09c59dfc2e1d2c2
Ronen Friedman <rfriedma@redhat.com>
2025-09-02 10:52:48 -0500

    osd/scrub: modify OMAP stats collection
    Replace the separate all-chunk loop with object-by-object
    handling. Use the selected authoritative version of the object
    as the info source.
    
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>

--- src/osd/scrubber/scrub_backend.h
+++ src/osd/scrubber/scrub_backend.h
@@ -435,7 +435,13 @@ class ScrubBackend {
   /// might return error messages to be cluster-logged
   std::optional<std::string> compare_obj_in_maps(const hobject_t& ho);
 
-  void omap_checks();
+  /**
+   * add the OMAP stats for the handled object to the m_omap_stats info.
+   * Also warn if the object has large omap keys or values.
+   */
+  void collect_omap_stats(
+      const hobject_t& ho,
+      const ScrubMap::object& auth_object);
 
   std::optional<auth_and_obj_errs_t> for_empty_auth_list(
     std::list<pg_shard_t>&& auths,
