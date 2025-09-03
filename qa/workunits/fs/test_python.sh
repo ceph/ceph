170d8c3966625ce0b110b5cb035153f7ad807a39
Rishabh Dave <ridave@redhat.com>
2025-09-03 11:48:57 +0530

    DNM: test_cephfs.py: v4
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/workunits/fs/test_python.sh
+++ qa/workunits/fs/test_python.sh
@@ -2,5 +2,5 @@
 
 # Running as root because the filesystem root directory will be
 # owned by uid 0, and that's where we're writing.
-python3 -m pytest -v $(dirname $0)/../../../src/test/pybind/test_cephfs.py -k test_create_and_rm_2000_subdir_levels_close_v3
+python3 -m pytest -v $(dirname $0)/../../../src/test/pybind/test_cephfs.py -k test_create_and_rm_2000_subdir_levels_close_v4
 exit 0
