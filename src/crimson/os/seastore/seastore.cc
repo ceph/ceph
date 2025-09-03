b8be1aa91d3cdff87f2f31a1cbb005750f6df6d1
Chunmei Liu <chunmei.liu@ibm.com>
2025-09-03 04:28:43 +0000

    crimson/common/options: add seastore_enable_shard_change in crimson.yaml.in
    Signed-off-by: Chunmei Liu <chunmei.liu@ibm.com>

--- src/crimson/os/seastore/seastore.cc
+++ src/crimson/os/seastore/seastore.cc
@@ -233,6 +233,13 @@ seastar::future<> SeaStore::get_shard_nums()
       });
     INFO("seastore shard nums {}", shard_nums);
     store_shard_nums = shard_nums;
+    if(!crimson::common::get_conf<bool>("seastore_enable_shard_change")) {
+      INFO("seastore_enable_shard_change is false");
+      if (store_shard_nums != seastar::smp::count) {
+        INFO("seastore shards {} do not match seastar::smp {}", store_shard_nums, seastar::smp::count);
+        ceph_abort_msg("seastore_enable_shard_change is false, seastore shards do not match seastar::smp");
+      }
+    }
     co_return;
   }
 }
