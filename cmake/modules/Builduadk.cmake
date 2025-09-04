db8228b086265ea5dbc9d4a2223845c1066730e9
David Galloway <david.galloway@ibm.com>
2025-09-04 11:17:55 -0400

    uadk: Build with ceph fork (for FORTIFY_SOURCE fix)
    See https://github.com/ceph/ceph/pull/65371
    
    Signed-off-by: David Galloway <david.galloway@ibm.com>
    
    CEPH-BUILD-JOB: ceph-dev-pipeline
    FLAVORS: default
    ARCHS: arm64
    DISTROS: noble

--- cmake/modules/Builduadk.cmake
+++ cmake/modules/Builduadk.cmake
@@ -20,7 +20,7 @@ function(build_uadk)
         UPDATE_COMMAND "" # this disables rebuild on each run
         GIT_REPOSITORY "https://github.com/ceph/uadk.git"
         GIT_CONFIG advice.detachedHead=false
-        GIT_TAG 19f650cae960304e3c674992a4c7d5d56a8f4efa
+        GIT_TAG 8481b582808a252fc29387b84ee319045bc2557b
         SOURCE_DIR "${PROJECT_SOURCE_DIR}/src/uadk"
         BUILD_IN_SOURCE 1
         CMAKE_ARGS -DCMAKE_CXX_COMPILER=which g++
