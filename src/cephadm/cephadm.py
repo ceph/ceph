90f6388781564316fc05ba2f7f24e14c54d4cdb4
Kushal Deb <Kushal.Deb@ibm.com>
2025-09-09 17:32:00 +0530

    mgr/cephadm: configurable per-service stop timeout
    Introduce a termination_grace_period field in service spec to define how long the
    orchestrator should wait for a service to shut down gracefully before forcefully terminating it.
    The value is plumbed mgr -> cephadm and written into 'unit.stop' as 'podman stop -t <N>'
    
    Signed-off-by: Kushal Deb <Kushal.Deb@ibm.com>

--- src/cephadm/cephadm.py
+++ src/cephadm/cephadm.py
@@ -962,6 +962,7 @@ def deploy_daemon(
                     endpoints=endpoints,
                     init_containers=init_containers,
                     sidecars=sidecars,
+                    stop_timeout=getattr(ctx, 'termination_grace_period_seconds', None),
                 )
             else:
                 raise RuntimeError('attempting to deploy a daemon without a container image')
@@ -1028,6 +1029,7 @@ def deploy_daemon_units(
     endpoints: Optional[List[EndPoint]] = None,
     init_containers: Optional[List[InitContainer]] = None,
     sidecars: Optional[List[SidecarContainer]] = None,
+    stop_timeout: Optional[int] = None,
 ) -> None:
     data_dir = ident.data_dir(ctx.data_dir)
     pre_start_commands: List[runscripts.Command] = []
@@ -1061,7 +1063,7 @@ def deploy_daemon_units(
         endpoints=endpoints,
         pre_start_commands=pre_start_commands,
         post_stop_commands=post_stop_commands,
-        timeout=30 if ident.daemon_type == 'osd' else None,
+        timeout=stop_timeout,
     )
 
     # sysctl
@@ -2943,6 +2945,7 @@ def apply_deploy_config_to_ctx(
         if key not in facade.defaults:
             logger.warning('unexpected parameter: %r=%r', key, value)
         setattr(ctx, key, value)
+
     update_default_image(ctx)
     logger.debug('Determined image: %r', ctx.image)
 
@@ -4496,6 +4499,12 @@ def _add_deploy_parser_args(
         action='append',
         default=[],
         help='Additional entrypoint arguments to apply to deamon'
+    )
+    parser_deploy.add_argument(
+        '--termination-grace-period-seconds',
+        type=int,
+        default=None,
+        help='Time in seconds to wait for graceful service shutdown before forcefully killing it'
     )
 
 
