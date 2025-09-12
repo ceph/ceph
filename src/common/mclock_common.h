32944b5d8e0a9a572ff6b7069ba1179e7b242ea0
Max Kellermann <max.kellermann@ionos.com>
2025-09-12 12:42:52 +0200

    common/mclock_common: include cleanup
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/common/mclock_common.h
+++ src/common/mclock_common.h
@@ -16,7 +16,8 @@
 
 #pragma once
 #include "config.h"
-#include "ceph_context.h"
+#include "common/ceph_context.h" // for CephContext
+#include "common/config_proxy.h" // for md_config_obs_t
 #include "include/utime.h"
 #include "dmclock/src/dmclock_server.h"
 #ifndef WITH_CRIMSON
