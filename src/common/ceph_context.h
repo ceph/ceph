ceph/ceph

commit c00a09a254df7ee736ab6cea87d5d435c46e3f91

common/ceph_context.h: Jenkins builder fix: breakpad reserve space
For cases when HAVE_BREAKPAD is off, supply exactly the same space in
CephContext struct.

While it should happen, jenkins seems to link binaries with different variants.

The noticeable artefacts of this misbehaviour are:
	208 - unittest_bluefs (Bus error)
	209 - unittest_bluefs_ex (Failed)
	211 - unittest_bdev (Bus error)

Above mentioned unittests are failing because
ceph_context.h :

  ceph::PluginRegistry *get_plugin_registry() {
    return _plugin_registry;
  }
^ _plugin_registry returned is at !!!offset off by 8 bytes!!! to the location of _plugin_registry as constructed at
ceph_context.cc :

743:   _plugin_registry = new PluginRegistry(this);

This causes fatal error in
src/extblkdev/ExtBlkDevPlugin.cc :

227      auto registry = cct->get_plugin_registry();
228      std::lock_guard l(registry->lock);

Sometimes lock_guard hangs, sometimes lock_guard segfaults.

Signed-off-by: Adam Kupczyk <akupczyk@ibm.com>

src/common/ceph_context.h
@@ -151,6 +151,10 @@ public:
   ceph::logging::Log *_log;
 #ifdef HAVE_BREAKPAD
   std::unique_ptr<google_breakpad::ExceptionHandler> _ex_handler;
+  static_assert(sizeof(std::unique_ptr<google_breakpad::ExceptionHandler>) == sizeof(std::unique_ptr<char>));
+#else
+  // Reserve the space for the case when part of ceph is compiled with and other without HAVE_BREAKPAD
+  [[maybe_unused]] std::unique_ptr<char> _ex_handler;
 #endif
 
   /* init ceph::crypto */

commit 6d60756be757b5c4ac058238c07f94e8431bf4c0

common: remove unused attribute
This resolves an obnoxious compiler warning:

    [79/768] Building CXX object src/mgr/CMakeFiles/ceph-mgr.dir/__/ceph_mgr.cc.o
    In file included from /run/host/home/sepian/ceph/src/global/global_init.h:23,
                     from /run/host/home/sepian/ceph/src/ceph_mgr.cc:27:
    /run/host/home/sepian/ceph/src/common/ceph_context.h:153:42: warning: ‘maybe_unused’ attribute ignored [-Wattributes]
      153 |   [[maybe_unused]] std::unique_ptr<char> _ex_handler;
          |                                          ^~~~~~~~~~~

Fixes: c00a09a254df7ee736ab6cea87d5d435c46e3f91
Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>

src/common/ceph_context.h
@@ -152,7 +152,7 @@ public:
   static_assert(sizeof(std::unique_ptr<google_breakpad::ExceptionHandler>) == sizeof(std::unique_ptr<char>));
 #else
   // Reserve the space for the case when part of ceph is compiled with and other without HAVE_BREAKPAD
-  [[maybe_unused]] std::unique_ptr<char> _ex_handler;
+  std::unique_ptr<char> _ex_handler;
 #endif
 
   /* init ceph::crypto */