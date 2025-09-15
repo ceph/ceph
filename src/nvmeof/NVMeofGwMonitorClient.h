37393a9793622456bf5575de5b4d8060d18e4243
Leonid Chernin <leonidc@il.ibm.com>
2025-09-15 14:04:04 +0300

    nvmeofgw:  beacon diff implementation in the monitor unit tests works and tested new fields beacons            coming according to new schema fixes for failover/failback/gw fast-reboot.
               src/messages/MNVMeofGwBeacon.h: add sequence number
    	   truncate prev_beacon_subsystems on beacon sequence mismatch
    	   do not process OOO map
    
       Signed-off-by: Alexander Indenbaum <aindenba@redhat.com>
    
    nvmeofgw : monitor changes for the beacon-diff feature
         process beacons by beacon-diff new schema
         detect sequence out of order(ooo) condition and handle it
         in case ooo detected send ack to the gw with the expected correct sequence
         skip failovers for some interval when ooo detected
         ignore all becons with incorrect sequences until gw sends expected one
    
    Signed-off-by: Leonid Chernin <leonidc@il.ibm.com>
    
    nvmeofgw: this commit adds upgrade rules for the beacon-diff
    
    Signed-off-by: Leonid Chernin <leonidc@il.ibm.com>
    
    nvmeofgw: CR change: use fmt::format() instead of std::hex stream manipulators
    
    Signed-off-by: Alexander Indenbaum <aindenba@redhat.com>
    
    nvmeofgw: CR change: use BEACON_SUBSYS_VERSION_ENHANCED instead of verson number
    
    Signed-off-by: Leonid Chernin <leonidc@il.ibm.com>
    
    mon: CR change: centralize beacon version constants to eliminate duplication
    
    Signed-off-by: Alexander Indenbaum <aindenba@redhat.com>
    
    Fixes: https://tracker.ceph.com/issues/72394

--- src/nvmeof/NVMeofGwMonitorClient.h
+++ src/nvmeof/NVMeofGwMonitorClient.h
@@ -54,7 +54,9 @@ private:
 
   bool first_beacon = true;
   bool set_group_id = false;
-
+  uint64_t beacon_sequence = 0;
+  BeaconSubsystems prev_beacon_subsystems;
+  uint64_t cluster_features = 0;  // track cluster features for beacon encoding
   // init gw ssl opts
   void init_gw_ssl_opts();
 
