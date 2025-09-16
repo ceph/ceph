f48c4a0f89677d536c9f76c07bead258c680e7d3
Bradley Bishop <bishopbm1@gmail.com>
2025-09-16 12:31:57 -0400

    mgr/cephadm: Add check for virtual_interface_networks
    Added an additional look up for if a match with the ceph networks is not found it will check the virtual_interface_networks list from the config that will enable matching to those networks, enabling dummy IP usage as described in the docs: https://docs.ceph.com/en/reef/cephadm/services/rgw/#selecting-network-interfaces-for-the-virtual-ip.
    
    Fixes: https://tracker.ceph.com/issues/66874
    Signed-off-by: Bradley Bishop <bishopbm1@gmail.com>

--- src/pybind/mgr/cephadm/serve.py
+++ src/pybind/mgr/cephadm/serve.py
@@ -802,6 +802,14 @@ class CephadmServe:
                         )
                         found = True
                         break
+                if not found and spec.virtual_interface_networks:
+                    for subnet, ifaces in self.mgr.cache.networks.get(host, {}).items():
+                        if subnet in spec.virtual_interface_networks:
+                            logger.debug(
+                                f'{subnet} found in virtual_interface_networks list {list(spec.virtual_interface_networks)}'
+                            )
+                            found = True
+                            break
                 if not found:
                     self.log.info(
                         f"Filtered out host {host}: Host has no interface available for VIP: {vip}"
