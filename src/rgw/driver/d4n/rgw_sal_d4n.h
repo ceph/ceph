dddcd7bd9f1143d5660422694dbc77636d3cbec9
Samarah <samarah.uriarte@ibm.com>
2025-09-22 15:16:40 +0000

    rgw/d4n: Implement bucket remove method
    Signed-off-by: Samarah <samarah.uriarte@ibm.com>

--- src/rgw/driver/d4n/rgw_sal_d4n.h
+++ src/rgw/driver/d4n/rgw_sal_d4n.h
@@ -113,6 +113,8 @@ class D4NFilterBucket : public FilterBucket {
     };
     D4NFilterDriver* filter;
     bool cache_request{false};
+    bool return_blocks{false}; // indicates whether dir_blocks should be populated
+    std::vector<rgw::d4n::CacheBlock> dir_blocks; // for use in bucket removal
 
   public:
     D4NFilterBucket(std::unique_ptr<Bucket> _next, D4NFilterDriver* _filter) :
@@ -123,6 +125,8 @@ class D4NFilterBucket : public FilterBucket {
     virtual std::unique_ptr<Object> get_object(const rgw_obj_key& key) override;
     virtual int list(const DoutPrefixProvider* dpp, ListParams& params, int max,
 		   ListResults& results, optional_yield y) override;
+    virtual int remove(const DoutPrefixProvider* dpp, bool delete_children,
+		       optional_yield y) override;
     virtual int create(const DoutPrefixProvider* dpp,
                        const CreateParams& params,
                        optional_yield y) override;
