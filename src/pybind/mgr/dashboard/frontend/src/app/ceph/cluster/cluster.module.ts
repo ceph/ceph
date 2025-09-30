781cf0e0fe135354d9872e029dfc8f7f596ba9b6
Pedro Gonzalez Gomez <pegonzal@ibm.com>
2025-09-30 21:00:35 +0200

    mgr/dashboard: carbonize smb service custom_dns input
    Fixes: https://tracker.ceph.com/issues/73319
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@ibm.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/cluster.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cluster/cluster.module.ts
@@ -83,6 +83,7 @@ import { MultiClusterFormComponent } from './multi-cluster/multi-cluster-form/mu
 import { MultiClusterListComponent } from './multi-cluster/multi-cluster-list/multi-cluster-list.component';
 import { DashboardV3Module } from '../dashboard-v3/dashboard-v3.module';
 import { MultiClusterDetailsComponent } from './multi-cluster/multi-cluster-details/multi-cluster-details.component';
+import { TextLabelListComponent } from '~/app/shared/components/text-label-list/text-label-list.component';
 
 @NgModule({
   imports: [
@@ -115,7 +116,8 @@ import { MultiClusterDetailsComponent } from './multi-cluster/multi-cluster-deta
     ModalModule,
     ListModule,
     ToggletipModule,
-    IconModule
+    IconModule,
+    TextLabelListComponent
   ],
   declarations: [
     MonitorComponent,
