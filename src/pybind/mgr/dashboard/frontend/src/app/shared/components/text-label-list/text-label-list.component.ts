90e1b9d061c1f379c0067689e4ff80453e5bd462
Pedro Gonzalez Gomez <pegonzal@ibm.com>
2025-09-30 16:53:23 +0200

    mgr/dashboard: add text-label-list component
    Introduces new form input component to input  a list of strings based on carbon's cds-text-label component
    
    Fixes: https://tracker.ceph.com/issues/73319
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@ibm.com>
    (cherry picked from commit f19754d04249d05cac59693198fb305572884032)

---
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/text-label-list/text-label-list.component.ts
@@ -0,0 +1,99 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, ReactiveFormsModule } from '@angular/forms';
+import { ButtonModule, GridModule, IconModule, InputModule } from 'carbon-components-angular';
+import { ComponentsModule } from '../components.module';
+
+@Component({
+  selector: 'cd-text-label-list',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    InputModule,
+    IconModule,
+    ButtonModule,
+    GridModule,
+    ComponentsModule
+  ],
+  templateUrl: './text-label-list.component.html',
+  styleUrl: './text-label-list.component.scss',
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: TextLabelListComponent,
+      multi: true
+    }
+  ]
+})
+export class TextLabelListComponent implements ControlValueAccessor {
+  @Input()
+  label: string = '';
+  @Input()
+  helperText: string = '';
+  @Input()
+  placeholder: string = '';
+
+  values: string[] = [''];
+  disabled: boolean = false;
+  touched: boolean = false;
+
+  private onChange: (value: string[]) => void = () => {};
+  private onTouched: () => void = () => {};
+
+  private _isRemovingChar(originalValue: string, value: string): boolean {
+    return originalValue.slice(0, -1) === value;
+  }
+
+  writeValue(value: string[]): void {
+    this.values = value?.length ? [...value, ''] : [''];
+  }
+
+  registerOnChange(onChange: (value: string[]) => void): void {
+    this.onChange = onChange;
+  }
+
+  registerOnTouched(onTouched: () => void): void {
+    this.onTouched = onTouched;
+  }
+
+  setDisabledState(disabled: boolean): void {
+    this.disabled = disabled;
+  }
+
+  onInputChange(index: number, value: string) {
+    const originalValue = this.values[index];
+    this.values[index] = value;
+
+    if (
+      !this._isRemovingChar(originalValue, value) &&
+      index === this.values.length - 1 &&
+      value.trim() !== ''
+    ) {
+      this.values.push('');
+    }
+
+    const nonEmptyValues = this.values.filter((v) => v.trim() !== '');
+    this.onChange(nonEmptyValues.length ? nonEmptyValues : []);
+
+    this.markAsTouched();
+  }
+
+  markAsTouched() {
+    if (!this.touched) {
+      this.onTouched();
+      this.touched = true;
+    }
+  }
+
+  deleteInput(index: number) {
+    this.values.splice(index, 1);
+
+    if (this.values.length === 0) {
+      this.values.push('');
+    }
+
+    const nonEmpty = this.values.filter((v) => v.trim() !== '');
+    this.onChange(nonEmpty.length ? nonEmpty : []);
+  }
+}
