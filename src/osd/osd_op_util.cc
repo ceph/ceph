2e8d1f82f99cacd26a1559c9b25e78f5e87ec3d6
Alex Ainscow <aainscow@uk.ibm.com>
2025-10-03 14:00:10 +0100

    osd: Create EC Direct Read flag and pass through to EC.
    This is in preperation for supporting sparse and sync reads in EC.
    Such ops will only be supported for "balance reads".
    
    Signed-off-by: Alex Ainscow <aainscow@uk.ibm.com>

--- src/osd/osd_op_util.cc
+++ src/osd/osd_op_util.cc
@@ -52,6 +52,9 @@ bool OpInfo::need_skip_promote() const {
 bool OpInfo::allows_returnvec() const {
   return check_rmw(CEPH_OSD_RMW_FLAG_RETURNVEC);
 }
+bool OpInfo::ec_direct_read() const {
+  return check_rmw(CEPH_OSD_RMW_FLAG_EC_DIRECT_READ);
+}
 /**
  * may_read_data()
  * 
@@ -79,6 +82,7 @@ void OpInfo::set_skip_promote() { set_rmw_flags(CEPH_OSD_RMW_FLAG_SKIP_PROMOTE);
 void OpInfo::set_force_rwordered() { set_rmw_flags(CEPH_OSD_RMW_FLAG_RWORDERED); }
 void OpInfo::set_returnvec() { set_rmw_flags(CEPH_OSD_RMW_FLAG_RETURNVEC); }
 void OpInfo::set_read_data() { set_rmw_flags(CEPH_OSD_RMW_FLAG_READ_DATA); }
+void OpInfo::set_ec_direct_read() { set_rmw_flags(CEPH_OSD_RMW_FLAG_EC_DIRECT_READ); }
 
 
 int OpInfo::set_from_op(
