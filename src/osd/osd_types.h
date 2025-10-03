ac8816c970362ad62ba743454fbc8ffa91f35fb0
Alex Ainscow <aainscow@uk.ibm.com>
2025-10-03 13:49:58 +0100

    osd: Introduce pool flag for "split IO" and Plugin flag for "direct read"
    These flags will currently behave as follows:
    
    1. The pool flag is never set, unless by a user with the osd_pool_default_flags
       config option.
    2. The pool flag will be removed for EC pools where the plugin does not support
       direct reads.
    3. Replica pools will never remove the flag.
    
    The intention is to eventually invert this logic and allow split IOs upon
    upgrade to Umberella in this same function.
    
    Signed-off-by: Alex Ainscow <aainscow@uk.ibm.com>

--- src/osd/osd_types.h
+++ src/osd/osd_types.h
@@ -1331,6 +1331,7 @@ struct pg_pool_t {
     // Note, does not prohibit being created on classic osd.
     FLAG_CRIMSON = 1<<18,
     FLAG_EC_OPTIMIZATIONS = 1<<19, // enable optimizations, once enabled, cannot be disabled
+    FLAG_CLIENT_SPLIT_READS = 1<<20, // Optimized EC is permitted to do direct reads.
   };
 
   static const char *get_flag_name(uint64_t f) {
