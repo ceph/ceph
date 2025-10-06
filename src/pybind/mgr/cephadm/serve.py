7067d0f35181deedd24484629011b944b4f61cd2
Timothy Q Nguyen <timqn22@gmail.com>
2025-10-06 14:54:05 -0700

    cephadm: change bootstrap version tracking
    I have removed the CLI commands associated with adding bootstrap
    version and time to the version tracker sqlite table. Instead, I
    have opted to directly call config-key command to set the KV store
    values and to get the KV store values. This will reduce the likelyhood
    that clients may modify the bootstrap information stored since there
    won't be CLI commands showing up on their menus. I have also added a
    new mechanism for checking the status of the bootstrap version. The
    mechanism relies on the variable bootstrap_versioned_stored in the
    main CephadmOrchestrator class in Cephadm. This boolean variable is
    initially false and is changed to true whenever a bootstrap version
    was successfully added to the sqlite table. This mechanism was added
    to allow quick check for if bootstrap version is added which will save
    on overhead since functions that try to add the bootstrap version
    exist in the Cephadm serve loop which is called every 60 seconds.
    
    Signed-off-by: Timothy Q Nguyen <timqn22@gmail.com>

--- src/pybind/mgr/cephadm/serve.py
+++ src/pybind/mgr/cephadm/serve.py
@@ -120,11 +120,11 @@ class CephadmServe:
 
                     self._check_for_moved_osds()
 
-                    if self.mgr.db_ready():
-                        self.mgr.version_tracker.add_bootstrap_cluster_version()
-                    else:
-                        self.mgr.log.debug('Version Tracker, Cluster bootstrap version "' +  self.mgr._version + '" could not be added during serve: mgr db not ready')
-                        self.mgr.log.debug('Version Tracker, note: bootstrap version may already be added')
+                    if not self.mgr.bootstrap_version_stored:
+                        if self.mgr.db_ready():
+                            self.mgr.version_tracker.add_bootstrap_cluster_version()
+                        else:
+                            self.mgr.log.debug('Version Tracker, Cluster bootstrap version "' +  self.mgr._version + '" could not be added during serve: mgr db not ready')
                         
                     if self.mgr.agent_helpers._handle_use_agent_setting():
                         continue
