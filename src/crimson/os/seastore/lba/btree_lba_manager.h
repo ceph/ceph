cc2b231cc8b6305f5f48afe0cc044963698c017b
Samuel Just <sjust@redhat.com>
2025-10-07 14:20:17 -0700

    crimson: replace LBAManager::complete_indirect_lba_mapping with TransactionManager::complete_mapping
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/os/seastore/lba/btree_lba_manager.h
+++ src/crimson/os/seastore/lba/btree_lba_manager.h
@@ -333,10 +333,6 @@ public:
     laddr_t laddr,
     extent_len_t len) final;
 
-  complete_lba_mapping_ret complete_indirect_lba_mapping(
-    Transaction &t,
-    LBAMapping mapping) final;
-
 private:
   Cache &cache;
 
