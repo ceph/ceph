349ec31ba1ec2f2952c13556e9bcfa72070cbd64
Samuel Just <sjust@redhat.com>
2025-10-07 18:47:52 -0700

    crimson/.../transaction_manager: rework _remove in terms of LBACursor interfaces
    Removes the need for _remove_direct_mapping and _remove_indirect_mapping.
    
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/os/seastore/transaction_manager.h
+++ src/crimson/os/seastore/transaction_manager.h
@@ -1366,14 +1366,7 @@ private:
   ref_iertr::future<LBAMapping> _remove(
     Transaction &t,
     LBAMapping mapping);
-  ref_iertr::future<LBAMapping>
-  _remove_indirect_mapping(
-    Transaction &t,
-    LBAMapping mapping);
-  ref_iertr::future<LBAMapping>
-  _remove_direct_mapping(
-    Transaction &t,
-    LBAMapping mapping);
+
   ref_iertr::future<LBAMapping>
   _remove_indirect_mapping_only(
     Transaction &t,
