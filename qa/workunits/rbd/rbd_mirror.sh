49dd48b89ec243a1238d54878e8cf84388a41020
VinayBhaskar-V <vvarada@redhat.com>
2025-10-14 17:17:37 +0530

    rbd-mirror: allow incomplete demote snapshot to sync after rbd-mirror daemon restart
    Currently when the secondary daemon was killed while a demote snapshot was newly created (0% copied)
    or partially synced, the image's promotion state was set to **PROMOTION_STATE_ORPHAN** upon restart.
    This state prevents the demote snapshot sync after restart as bootstrap on secondary fails.
    In this commit we fix this by assigning promotion state to **PROMOTION_STATE_NON_PRIMARY**
    for an image with an **incomplete non-primary demote snapshot**.
    
    Fixes: https://tracker.ceph.com/issues/73528
    Signed-off-by: VinayBhaskar-V <vvarada@redhat.com>

--- qa/workunits/rbd/rbd_mirror.sh
+++ qa/workunits/rbd/rbd_mirror.sh
@@ -798,6 +798,35 @@ if [ -z "${RBD_MIRROR_USE_RBD_MIRROR}" ]; then
   CEPH_ARGS='--id admin' ceph --cluster ${CLUSTER2} osd blocklist ls 2>&1 | grep -q "listed 0 entries"
 fi
 
+if [ "${RBD_MIRROR_MODE}" = "snapshot" ]; then
+  if [ -z "${RBD_MIRROR_USE_RBD_MIRROR}" ]; then
+    testlog "TEST: test partially synced demote snapshot sync after rbd-mirror restart"
+    test_demote_image=test_demote_image
+    create_image_and_enable_mirror ${CLUSTER2} ${POOL} ${test_demote_image} ${RBD_MIRROR_MODE} 10G
+    write_image ${CLUSTER2} ${POOL} ${test_demote_image} 100
+    wait_for_image_replay_stopped ${CLUSTER2} ${POOL} ${test_demote_image}
+    wait_for_image_replay_started ${CLUSTER1} ${POOL} ${test_demote_image}
+    wait_for_replay_complete ${CLUSTER1} ${CLUSTER2} ${POOL} ${POOL} ${test_demote_image}
+    wait_for_replaying_status_in_pool_dir ${CLUSTER1} ${POOL} ${test_demote_image}
+    wait_for_status_in_pool_dir ${CLUSTER2} ${POOL} ${test_demote_image} 'up+stopped'
+    write_image ${CLUSTER2} ${POOL} ${test_demote_image} 2560 4194304
+    demote_image ${CLUSTER2} ${POOL} ${test_demote_image}
+    sleep 1
+    stop_mirrors ${CLUSTER1} -KILL
+    SNAPS=$(get_snaps_json ${CLUSTER1} ${POOL} ${test_demote_image})
+    jq -e '.[-1].namespace["type"] == "mirror" and .[-1].namespace["state"] == "demoted" and .[-1].namespace["complete"] == false' <<< ${SNAPS}
+    start_mirrors ${CLUSTER1}
+    wait_for_status_in_pool_dir ${CLUSTER2} ${POOL} ${test_demote_image} 'up+unknown'
+    wait_for_status_in_pool_dir ${CLUSTER1} ${POOL} ${test_demote_image} 'up+unknown'
+    promote_image ${CLUSTER1} ${POOL} ${test_demote_image}
+    wait_for_image_replay_started ${CLUSTER2} ${POOL} ${test_demote_image}
+    wait_for_status_in_pool_dir ${CLUSTER1} ${POOL} ${test_demote_image} 'up+stopped'
+    wait_for_status_in_pool_dir ${CLUSTER2} ${POOL} ${test_demote_image} 'up+replaying'
+    compare_images ${CLUSTER1} ${CLUSTER2} ${POOL} ${POOL} ${test_demote_image}
+    remove_image_retry ${CLUSTER1} ${POOL} ${test_demote_image}
+  fi
+fi
+
 testlog "TEST: force promote with a user snapshot"
 force_promote_image=test_force_promote_user
 create_image_and_enable_mirror ${CLUSTER2} ${POOL} ${force_promote_image} ${RBD_MIRROR_MODE} 10G
