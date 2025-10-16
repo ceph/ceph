e88be3d050888fc36fcedab2d045872bf5e6029b
Samuel Just <sjust@redhat.com>
2025-10-16 01:26:52 +0000

    crimson/.../lba_manager: convert clone_mapping to use cursors
    Signed-off-by: Samuel Just <sjust@redhat.com>

--- src/crimson/os/seastore/lba_manager.h
+++ src/crimson/os/seastore/lba_manager.h
@@ -91,8 +91,8 @@ public:
     std::vector<LogicalChildNodeRef> ext) = 0;
 
   struct clone_mapping_ret_t {
-    LBAMapping cloned_mapping;
-    LBAMapping orig_mapping;
+    LBACursorRef cloned_mapping;
+    LBACursorRef orig_mapping;
   };
   using clone_mapping_iertr = alloc_extent_iertr;
   using clone_mapping_ret = clone_mapping_iertr::future<clone_mapping_ret_t>;
@@ -101,11 +101,11 @@ public:
    */
   virtual clone_mapping_ret clone_mapping(
     Transaction &t,
-    LBAMapping pos,		// the destined position
-    LBAMapping mapping,		// the mapping to be cloned
+    LBACursorRef pos,		// the destined position
+    LBACursorRef mapping,	// the mapping to be cloned
     laddr_t laddr,		// the new lba key of the cloned mapping
-    extent_len_t offset,	// the offset of the part to be cloned,
-				// relative to the start of the mapping.
+    laddr_t inter_key,	        // offset within mapping of the target of the
+                                // clone
     extent_len_t len,		// the length of the part to be cloned
     bool updateref		// whether to update the refcount of the
 				// direct mapping
