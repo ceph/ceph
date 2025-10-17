7fb2b1a725b6ce44c93fa12b144699a5ed6638dd
Bill Scales <bill_scales@uk.ibm.com>
2025-10-17 17:59:15 +0100

    osd: New interval for pool migration changes
    Start a new interval whenever pool migration starts or
    finishes for all PGs in the pool. Start a new interval
    for a PG when it is added or removed from migrating_pgs.
    
    Signed-off-by: Bill Scales <bill_scales@uk.ibm.com>

--- src/osdc/Objecter.cc
+++ src/osdc/Objecter.cc
@@ -3220,6 +3220,8 @@ int Objecter::_calc_target(op_target_t *t, bool any_change)
 	pi->peering_crush_mandatory_member,
 	t->allows_ecoptimizations,
 	pi->allows_ecoptimizations(),
+	t->migrating_pgs,
+	pi->migrating_pgs,
 	prev_pgid)) {
     force_resend = true;
   }
@@ -3313,6 +3315,7 @@ int Objecter::_calc_target(op_target_t *t, bool any_change)
     t->peering_crush_bucket_barrier = pi->peering_crush_bucket_barrier;
     t->peering_crush_mandatory_member = pi->peering_crush_mandatory_member;
     t->allows_ecoptimizations = pi->allows_ecoptimizations();
+    t->migrating_pgs = pi->migrating_pgs;
     ldout(cct, 10) << __func__ << " "
 		   << " raw pgid " << pgid << " -> actual " << t->actual_pgid
 		   << " acting " << t->acting
