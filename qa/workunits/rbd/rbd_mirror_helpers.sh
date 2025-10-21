9eea869cecc4f16931e43a6f846211bd481a2c33
VinayBhaskar-V <vvarada@redhat.com>
2025-10-21 12:34:40 +0530

    qa/workunits/rbd: moving mirror_image_snapshot to wait_for_replay_complete
    to make wait_for_snapshot_sync_complete work for all cases.
    
    Signed-off-by: VinayBhaskar-V <vvarada@redhat.com>
    (cherry picked from commit c4168074508624bda97fbe374f0c76e5b64c5b6e)

--- qa/workunits/rbd/rbd_mirror_helpers.sh
+++ qa/workunits/rbd/rbd_mirror_helpers.sh
@@ -813,7 +813,6 @@ wait_for_snapshot_sync_complete()
     local status_log=${TEMPDIR}/$(mkfname ${cluster}-${pool}-${image}.status)
     local local_status_log=${TEMPDIR}/$(mkfname ${local_cluster}-${pool}-${image}.status)
 
-    mirror_image_snapshot "${cluster}" "${pool}" "${image}"
     get_newest_mirror_snapshot "${cluster}" "${pool}" "${image}" "${status_log}"
     local snapshot_id=$(xmlstarlet sel -t -v "//snapshot/id" < ${status_log})
 
@@ -842,6 +841,7 @@ wait_for_replay_complete()
     if [ "${RBD_MIRROR_MODE}" = "journal" ]; then
         wait_for_journal_replay_complete ${local_cluster} ${cluster} ${pool} ${image}
     elif [ "${RBD_MIRROR_MODE}" = "snapshot" ]; then
+        mirror_image_snapshot "${cluster}" "${pool}" "${image}"
         wait_for_snapshot_sync_complete ${local_cluster} ${cluster} ${pool} ${image}
     else
         return 1
