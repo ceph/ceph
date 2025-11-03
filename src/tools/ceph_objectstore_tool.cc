85e0081a0023ef5dd725bd639f8f716149cfa26b
Jaya Prakash <jayaprakash@ibm.com>
2025-11-03 19:39:45 +0000

    tools: handle get-attr as read-only ops in ceph_objectstore_tool
    Fixes: https://tracker.ceph.com/issues/73710
    
    Signed-off-by: Jaya Prakash <jayaprakash@ibm.com>

--- src/tools/ceph_objectstore_tool.cc
+++ src/tools/ceph_objectstore_tool.cc
@@ -4000,6 +4000,7 @@ int main(int argc, char **argv)
     op == "get-inc-osdmap" ||
     objcmd == "get-bytes" ||
     objcmd == "get-attrs" ||
+    objcmd == "get-attr" ||
     objcmd == "get-omap" ||
     objcmd == "get-omaphdr" ||
     objcmd == "list-attrs" ||
