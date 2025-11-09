191201140bb3c4413e5ddcf0cbb3c3e7f8d331be
Anshuman <anshuman@ibm.com>
2025-11-09 16:44:32 +0530

    run-make: Added flag for Dashboard, Breakpad and WError Added flags to make Dashboard, Breakpad and WError optional. This should not impact existing pipelines as these options are disabled only when the flags are present. Signed-off-by: Anshuman <anshuman@ibm.com>

--- src/script/run-make.sh
+++ src/script/run-make.sh
@@ -77,6 +77,9 @@ function prepare() {
     if test -f ./install-deps.sh ; then
         ci_debug "Running install-deps.sh"
         INSTALL_EXTRA_PACKAGES="ccache git $which_pkg lvm2"
+        if [ "$(uname -m)" == "ppc64le" ]; then
+            INSTALL_EXTRA_PACKAGES="$INSTALL_EXTRA_PACKAGES openblas-devel gfortran cargo"
+        fi
         $DRY_RUN source ./install-deps.sh || return 1
         trap clean_up_after_myself EXIT
     fi
@@ -120,7 +123,6 @@ EOM
     local c_compiler="${discovered_c_compiler}"
     local cmake_opts
     cmake_opts+=" -DCMAKE_CXX_COMPILER=$cxx_compiler -DCMAKE_C_COMPILER=$c_compiler"
-    cmake_opts+=" -DCMAKE_CXX_FLAGS_DEBUG=-Werror"
     cmake_opts+=" -DENABLE_GIT_VERSION=OFF"
     cmake_opts+=" -DWITH_GTEST_PARALLEL=ON"
     cmake_opts+=" -DWITH_FIO=ON"
@@ -128,6 +130,15 @@ EOM
     cmake_opts+=" -DWITH_GRAFANA=ON"
     cmake_opts+=" -DWITH_SPDK=ON"
     cmake_opts+=" -DWITH_RBD_MIRROR=ON"
+    if [ ! $DISABLE_WERROR ]; then
+        cmake_opts+=" -DCMAKE_CXX_FLAGS_DEBUG=-Werror"
+    fi
+    if [ $WITHOUT_DASHBOARD ]; then
+        cmake_opts+=" -DWITH_MGR_DASHBOARD_FRONTEND=OFF"
+    fi
+    if [ $WITHOUT_BREAKPAD ]; then
+        cmake_opts+=" -DWITH_BREAKPAD=OFF"
+    fi
     if [ $WITH_CRIMSON ]; then
         cmake_opts+=" -DWITH_CRIMSON=ON"
     fi
