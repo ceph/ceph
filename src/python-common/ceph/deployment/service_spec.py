8145c6639ba550faf48e71ccf55c33427b424780
Shweta Bhosale <Shweta.Bhosale1@ibm.com>
2025-11-12 17:20:03 +0530

    mgr/cephadm: Added NFS spec field skip_nfs_notify_on_kmip_update, to skip SIGHUP on kmip update
    Fixes: https://tracker.ceph.com/issues/71551
    Signed-off-by: Shweta Bhosale <Shweta.Bhosale1@ibm.com>

--- src/python-common/ceph/deployment/service_spec.py
+++ src/python-common/ceph/deployment/service_spec.py
@@ -1429,6 +1429,7 @@ class NFSServiceSpec(ServiceSpec):
                  tls_ciphers: Optional[str] = None,
                  colocation_ports: Optional[List[Dict[str, int]]] = None,
                  enable_nfsv3: bool = False,
+                 skip_nfs_notify_on_kmip_update: bool = False,
                  ):
         assert service_type == 'nfs'
         super(NFSServiceSpec, self).__init__(
@@ -1475,8 +1476,10 @@ class NFSServiceSpec(ServiceSpec):
                 elif isinstance(host_obj, dict):
                     self.kmip_host_list.append(host_obj)
                 else:
-                    raise SpecValidationError(f'kmip_host_list contains an invalid element: {host_obj}.')
-
+                    raise SpecValidationError(
+                        f'kmip_host_list contains an invalid element: {host_obj}.'
+                    )
+        self.skip_nfs_notify_on_kmip_update = skip_nfs_notify_on_kmip_update
         # TLS fields
         self.tls_ciphers = tls_ciphers
         self.tls_ktls = tls_ktls
