baa88e9bcb080d9d4efd48650f78d60d01a81d53
Naman Munet <naman.munet@ibm.com>
2025-11-14 19:33:31 +0530

    mgr/dashboard: add footer to side-panel
    Fixes: https://tracker.ceph.com/issues/73788Wq
    
    Signed-off-by: Naman Munet <naman.munet@ibm.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/side-panel/side-panel.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/side-panel/side-panel.component.ts
@@ -1,17 +1,33 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
 
 @Component({
   selector: 'cd-side-panel',
   templateUrl: './side-panel.component.html',
   styleUrl: './side-panel.component.scss'
 })
-export class SidePanelComponent {
+export class SidePanelComponent implements AfterContentInit {
   @Input() expanded = false;
   @Input() headerText = '';
   @Input() overlay = true;
   @Input() size: 'sm' | 'md' | 'lg' = 'lg';
 
   @Output() closed = new EventEmitter<void>();
+  hasFooter = false;
+
+  constructor(private host: ElementRef) {}
+
+  ngAfterContentInit() {
+    // Look for .panel-footer inside projected content
+    const footer = this.host.nativeElement.querySelector('.panel-footer');
+    this.hasFooter = !!footer;
+  }
 
   close() {
     this.closed.emit();
