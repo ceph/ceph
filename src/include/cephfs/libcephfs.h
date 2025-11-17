319cd411943a38ee0f6221cc162c4153732632ea
Rishabh Dave <ridave@redhat.com>
2025-11-17 17:53:56 +0530

    libcephfs: provide API to mutate snapshot metadata
    Fixes: https://tracker.ceph.com/issues/66293
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- src/include/cephfs/libcephfs.h
+++ src/include/cephfs/libcephfs.h
@@ -862,6 +862,20 @@ int ceph_mksnap(struct ceph_mount_info *cmount, const char *path, const char *na
 int ceph_rmsnap(struct ceph_mount_info *cmount, const char *path, const char *name);
 
 /**
+ * Add, update or remove snapshot metadata.
+ *
+ * @param cmount the ceph mount handle to use for making the directory.
+ * @param path the path of the snapshot. This must be either an absolute
+ *        path or a path relative to CWD.
+ * @param mds_key key for the key-value pair in snapshot metadata.
+ * @param mds_val value for the key-value pair in snapshot metadata.
+ * @param op_flag unsigned integer to indicate whether metadata op is create,
+ *        update or remove.
+ */
+int ceph_do_snap_md_op(struct ceph_mount_info* cmount, const char* path,
+  const char* md_key, const char* md_val, unsigned op_flag);
+
+/**
  * Create multiple directories at once.
  *
  * @param cmount the ceph mount handle to use for making the directories.
