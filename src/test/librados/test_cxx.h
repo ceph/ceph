8df0ee40ca035f7f7162127f842e0fc9eed1f28c
Alex Ainscow <aainscow@uk.ibm.com>
2025-11-26 11:05:26 +0000

    test/librados: New tests for split IO
    Signed-off-by: Alex Ainscow <aainscow@uk.ibm.com>

--- src/test/librados/test_cxx.h
+++ src/test/librados/test_cxx.h
@@ -13,6 +13,9 @@ std::string create_one_pool_pp(const std::string &pool_name,
 			       const std::map<std::string, std::string> &config);
 std::string create_one_ec_pool_pp(const std::string &pool_name,
 			    librados::Rados &cluster);
+std::string create_ec_pool_pp(const std::string &pool_name,
+                            librados::Rados &cluster,
+                            bool fast_ec);
 std::string set_allow_ec_overwrites_pp(const std::string &pool_name,
 				       librados::Rados &cluster, bool allow);
 std::string connect_cluster_pp(librados::Rados &cluster);
@@ -20,3 +23,18 @@ std::string connect_cluster_pp(librados::Rados &cluster,
 			       const std::map<std::string, std::string> &config);
 int destroy_one_pool_pp(const std::string &pool_name, librados::Rados &cluster);
 int destroy_one_ec_pool_pp(const std::string &pool_name, librados::Rados &cluster);
+int destroy_ec_pool_pp(const std::string &pool_name, librados::Rados &cluster);
+std::string set_pool_flags_pp(const std::string &pool_name, librados::Rados &cluster, int64_t flags, bool set_not_unset);
+std::string set_split_ops_pp(const std::string &pool_name, librados::Rados &cluster, bool set_not_unset);
+
+// The following are convenient macros for defining test combinations
+// with each of the gtest suites.
+#define INSTANTIATE_TEST_SUITE_P_EC(CLASS) \
+INSTANTIATE_TEST_SUITE_P( CLASS ## ParamCombination, CLASS, \
+::testing::Combine( \
+::testing::Bool(),   /* fast_ec */ \
+::testing::Bool()))  /* split_ops */
+
+#define INSTANTIATE_TEST_SUITE_P_REPLICA(CLASS) \
+INSTANTIATE_TEST_SUITE_P( CLASS ## ParamCombination, CLASS, \
+::testing::Bool()) /* split_ops */
