12afd95ef8f07a0d7110c47205fbb445f0d38d69
Xuehan Xu <xuxuehan@qianxin.com>
2025-12-01 17:44:45 +0800

    crimson/os/seastore/transaction_manager: block client transactions if they conflict with rewriting transactions until the rewriting transactions finishes
    Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

--- src/crimson/os/seastore/cached_extent.cc
+++ src/crimson/os/seastore/cached_extent.cc
@@ -18,6 +18,8 @@ namespace {
   }
 }
 
+SET_SUBSYS(seastore_cache);
+
 namespace crimson::os::seastore {
 
 #ifdef DEBUG_CACHED_EXTENT_REF
@@ -470,6 +472,26 @@ void CachedExtent::new_committer(Transaction &t) {
   assert(prior_instance);
   assert(!prior_instance->committer);
   prior_instance->committer = committer;
+}
+
+void ExtentCommitter::block_trans(Transaction &t) {
+  LOG_PREFIX(ExtentCommitter::block_trans);
+  auto &prior = *extent.prior_instance;
+  for (auto &item : prior.read_transactions) {
+    TRACET("blocking trans {} for rewriting {}",
+      t, item.t->get_trans_id(), *item.ref);
+    item.t->need_wait_rewrite = true;
+  }
+}
+
+void ExtentCommitter::unblock_trans(Transaction &t) {
+  LOG_PREFIX(ExtentCommitter::unblock_trans);
+  auto &prior = *extent.prior_instance;
+  for (auto &item : prior.read_transactions) {
+    TRACET("unblocking trans {} for rewriting {}",
+      t, item.t->get_trans_id(), *item.ref);
+    item.t->need_wait_rewrite = false;
+  }
 }
 
 }
