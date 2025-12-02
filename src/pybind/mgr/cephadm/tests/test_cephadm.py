09c45635cd96ac2bd6aca25b453f4ce432171488
Vivek Maurya <vivek.maurya2@ibm.com>
2025-12-02 00:18:21 -0600

    mgr/cephadm: Add tunable to set log level for ceph-volume This commit fix cephadm testcase fail after change
    Fixes: https://tracker.ceph.com/issues/73725
    
    Signed-off-by: Vivek Maurya <vivek.maurya2@ibm.com>

--- src/pybind/mgr/cephadm/tests/test_cephadm.py
+++ src/pybind/mgr/cephadm/tests/test_cephadm.py
@@ -1131,16 +1131,16 @@ class TestCephadm(object):
 
             _run_cephadm.assert_any_call(
                 'test', 'osd', 'ceph-volume',
-                ['--config-json', '-', '--', 'lvm', 'batch',
+                ['--config-json', '-', '--', '--log-level', 'debug', 'lvm', 'batch',
                     '--no-auto', '/dev/sdb', '--objectstore', 'bluestore', '--yes', '--no-systemd'],
                 env_vars=['CEPH_VOLUME_OSDSPEC_AFFINITY=foo'], error_ok=True,
                 stdin='{"config": "", "keyring": ""}')
             _run_cephadm.assert_any_call(
-                'test', 'osd', 'ceph-volume', ['--', 'lvm', 'list', '--format', 'json'],
+                'test', 'osd', 'ceph-volume', ['--', '--log-level', 'debug', 'lvm', 'list', '--format', 'json'],
                 image='', no_fsid=False, error_ok=False, log_output=True, use_current_daemon_image=False
             )
             _run_cephadm.assert_any_call(
-                'test', 'osd', 'ceph-volume', ['--', 'raw', 'list', '--format', 'json'],
+                'test', 'osd', 'ceph-volume', ['--', '--log-level', 'debug', 'raw', 'list', '--format', 'json'],
                 image='', no_fsid=False, error_ok=False, log_output=True, use_current_daemon_image=False
             )
 
@@ -1176,18 +1176,18 @@ class TestCephadm(object):
 
             _run_cephadm.assert_any_call(
                 'test', 'osd', 'ceph-volume',
-                ['--config-json', '-', '--', 'lvm', 'batch',
+                ['--config-json', '-', '--', '--log-level', 'debug', 'lvm', 'batch',
                     '--no-auto', '/dev/sdb', '--db-devices', '/dev/sdc',
                     '--wal-devices', '/dev/sdd', '--objectstore', 'bluestore', '--yes', '--no-systemd'],
                 env_vars=['CEPH_VOLUME_OSDSPEC_AFFINITY=noncollocated'],
                 error_ok=True, stdin='{"config": "", "keyring": ""}',
             )
             _run_cephadm.assert_any_call(
-                'test', 'osd', 'ceph-volume', ['--', 'lvm', 'list', '--format', 'json'],
+                'test', 'osd', 'ceph-volume', ['--', '--log-level', 'debug', 'lvm', 'list', '--format', 'json'],
                 image='', no_fsid=False, error_ok=False, log_output=True, use_current_daemon_image=False
             )
             _run_cephadm.assert_any_call(
-                'test', 'osd', 'ceph-volume', ['--', 'raw', 'list', '--format', 'json'],
+                'test', 'osd', 'ceph-volume', ['--', '--log-level', 'debug', 'raw', 'list', '--format', 'json'],
                 image='', no_fsid=False, error_ok=False, log_output=True, use_current_daemon_image=False
             )
 
@@ -2333,10 +2333,10 @@ Traceback (most recent call last):
 
             assert _run_cephadm.mock_calls == [
                 mock.call('test', 'osd', 'ceph-volume',
-                          ['--', 'inventory', '--format=json-pretty', '--filter-for-batch'], image='',
+                          ['--', '--log-level', 'debug', 'inventory', '--format=json-pretty', '--filter-for-batch'], image='',
                           no_fsid=False, error_ok=False, log_output=False, use_current_daemon_image=False),
                 mock.call('test', 'osd', 'ceph-volume',
-                          ['--', 'inventory', '--format=json-pretty'], image='',
+                          ['--', '--log-level', 'debug', 'inventory', '--format=json-pretty'], image='',
                           no_fsid=False, error_ok=False, log_output=False, use_current_daemon_image=False),
             ]
 
