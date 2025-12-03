33b3875ead03ec9122171aebdbd81c92edae04c3
Matan Breizman <mbreizma@redhat.com>
2025-12-03 13:02:27 +0000

    crimson/os/seastore: Introduce check_full_extent_integrity helper
    This is an intermediate step to move the crc checks to Cache logic.
    
    Signed-off-by: Matan Breizman <mbreizma@redhat.com>

--- src/crimson/os/seastore/transaction_manager.cc
+++ src/crimson/os/seastore/transaction_manager.cc
@@ -625,6 +625,32 @@ TransactionManager::do_submit_transaction(
   );
 }
 
+void TransactionManager::check_full_extent_integrity(
+  Transaction &t, uint32_t ref_crc, uint32_t pin_crc)
+{
+  LOG_PREFIX(TransactionManager::check_full_extent_integrity);;
+  SUBDEBUGT(seastore_tm,
+    "checksum in the lba tree: 0x{:x}, actual checksum: 0x{:x}",
+    t,
+    pin_crc,
+    ref_crc);
+  bool inconsistent = false;
+  if (full_extent_integrity_check) {
+    inconsistent = (pin_crc != ref_crc);
+  } else { // !full_extent_integrity_check: remapped extent may be skipped
+    inconsistent = !(pin_crc == 0 ||
+                     pin_crc == ref_crc);
+  }
+  if (unlikely(inconsistent)) {
+    SUBERRORT(seastore_tm,
+      "extent checksum inconsistent, recorded: 0x{:x}, actual: 0x{:x}",
+      t,
+      pin_crc,
+      ref_crc);
+      ceph_abort_msg("extent checksum inconsistent");
+  }
+}
+
 seastar::future<> TransactionManager::flush(OrderingHandle &handle)
 {
   LOG_PREFIX(TransactionManager::flush);
