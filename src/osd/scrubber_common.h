5418821f0b6a82d7e3b2d08fd2a667996b19b433
Ronen Friedman <rfriedma@redhat.com>
2025-12-04 08:49:29 -0600

    osd/scrub: support an operator-abort command
    The new explicit command aborts any ongoing scrub of the target PG,
    including operator-initiated scrubs. That additional capability is needed now that
    operator-initiated scrubs are no longer blocked by 'no-scrub' settings.
    
    The scenario we are trying to help the operator with is:
    - an operator issues a set of operator-initiated scrubs (e.g., via a
      script), then realizes the mistake and wants to abort them all.
    The abort command also downgrades the urgency level of the scrub target
    (as otherwise the target would immediately restart, against the operator
    wishes).
    
    This commit implements the changes to the state machine and to the abort
    logic, assuming the operator command was translated into an event.
    
    Signed-off-by: Ronen Friedman <rfriedma@redhat.com>
    (cherry picked from commit d24a02f67a26f1bf8f13efffe2fa213c744f096f)

--- src/osd/scrubber_common.h
+++ src/osd/scrubber_common.h
@@ -216,6 +216,7 @@ enum class delay_cause_t {
   aborted,	    ///< scrub was aborted w/ unspecified reason
   interval,	    ///< the interval had ended mid-scrub
   scrub_params,     ///< the specific scrub type is not allowed
+  operator_abort    ///< operator-requested abort
 };
 }  // namespace Scrub
 
@@ -239,6 +240,7 @@ struct formatter<Scrub::delay_cause_t> : ::fmt::formatter<std::string_view> {
       case aborted:             desc = "aborted"; break;
       case interval:            desc = "interval"; break;
       case scrub_params:        desc = "scrub-mode"; break;
+      case operator_abort:      desc = "operator-abort"; break;
       // better to not have a default case, so that the compiler will warn
     }
     return ::fmt::formatter<string_view>::format(desc, ctx);
