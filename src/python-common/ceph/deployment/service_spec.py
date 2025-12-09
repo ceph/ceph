46e574a616d792a34fbd71c413c2498dbafbb9f5
Alexander Indenbaum <aindenba@redhat.com>
2025-12-09 12:15:14 +0200

    cephadm/nvmeof: enable rbd_with_crc32c by default
    Signed-off-by: Alexander Indenbaum <aindenba@redhat.com>

--- src/python-common/ceph/deployment/service_spec.py
+++ src/python-common/ceph/deployment/service_spec.py
@@ -1713,7 +1713,7 @@ class NvmeofServiceSpec(ServiceSpec):
                  transport_tcp_options: Optional[Dict[str, int]] =
                  {"in_capsule_data_size": 8192, "max_io_qpairs_per_ctrlr": 7},
                  enable_dsa_acceleration: bool = False,
-                 rbd_with_crc32c: bool = False,
+                 rbd_with_crc32c: bool = True,
                  tgt_cmd_extra_args: Optional[str] = None,
                  iobuf_options: Optional[Dict[str, int]] = None,
                  qos_timeslice_in_usecs: Optional[int] = 0,
