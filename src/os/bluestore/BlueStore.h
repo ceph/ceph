4125ad1c65172f0e57668b8145bbfb18816f84f5
Jaya Prakash <jayaprakash@ibm.com>
2025-12-09 16:50:19 +0000

    os/bluestore: separate slow scrub ops from normal slow ops
    This Commit introduces separate slow-op tracking paths:
    
    1. Normal IO slow op queue
    2. Scrub IO slow op queue
    
    This enables the OSD to record slow scrub-originated operations distinctly, without mixing
    them with client IO latency.
    
    Fixes: https://tracker.ceph.com/issues/73990
    
    Signed-off-by: Jaya Prakash <jayaprakash@ibm.com>

--- src/os/bluestore/BlueStore.h
+++ src/os/bluestore/BlueStore.h
@@ -3577,7 +3577,21 @@ public:
     const char* info = "",
     int idx2 = l_bluestore_first);
 
+  inline void log_latency_scrub(const char* name,
+    int idx,
+    const ceph::timespan& l,
+    double lat_threshold,
+    const char* info = "",
+    int idx2 = l_bluestore_first);
+
   inline void log_latency_fn(const char* name,
+    int idx,
+    const ceph::timespan& lat,
+    double lat_threshold,
+    std::function<std::string (const ceph::timespan& lat)> fn,
+    int idx2 = l_bluestore_first);
+
+  inline void log_latency_fn_scrub(const char* name,
     int idx,
     const ceph::timespan& lat,
     double lat_threshold,
@@ -3616,10 +3630,13 @@ private:
   std::string no_per_pg_omap_alert;
   std::string disk_size_mismatch_alert;
   std::string spurious_read_errors_alert;
-  std::queue <ceph::mono_clock::time_point> slow_op_event_queue;
+  std::queue <std::pair<ceph::mono_clock::time_point, bool>> slow_op_event_queue;
+  size_t slow_op_event_count = 0;
+  size_t slow_scrub_op_event_count = 0;
 
-  size_t _trim_slow_op_event_queue(ceph::mono_clock::time_point cur_time);
+  std::pair<size_t, size_t> _trim_slow_op_event_queue(ceph::mono_clock::time_point cur_time);
   void _add_slow_op_event();
+  void _add_slow_scrub_op_event();
   void _log_alerts(osd_alert_list_t& alerts);
   bool _set_compression_alert(bool cmode, const char* s) {
     std::lock_guard l(qlock);
