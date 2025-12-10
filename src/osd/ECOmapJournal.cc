7544415b474d39356b2a96aca6cdd3447c379fcc
Matty Williams <Matty.Williams@ibm.com>
2025-12-10 17:01:39 +0000

    osd: Add copyright headers to new files
    Signed-off-by: Matty Williams <Matty.Williams@ibm.com>

--- src/osd/ECOmapJournal.cc
+++ src/osd/ECOmapJournal.cc
@@ -1,6 +1,17 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:nil -*- 
 // vim: ts=8 sw=2 sts=2 expandtab
 
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2025 IBM
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ */
+
 #include "ECOmapJournal.h"
 
 #include <utility>
