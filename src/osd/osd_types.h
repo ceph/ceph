9890aab56203b10bfe26d291498a45cc806c5ae8
Max Kellermann <max.kellermann@ionos.com>
2025-12-15 14:17:03 +0100

    osd/osd_types: use `requires` instead of `static_assert`
    This allows instantiating the whole class without triggering a
    static_assert failure.
    
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/osd/osd_types.h
+++ src/osd/osd_types.h
@@ -5230,8 +5230,7 @@ public:
     return it->second.need;
   }
 
-  void claim(pg_missing_set&& o) {
-    static_assert(!TrackChanges, "Can't use claim with TrackChanges");
+  void claim(pg_missing_set&& o) requires(!TrackChanges) {
     missing = std::move(o.missing);
     rmissing = std::move(o.rmissing);
   }
