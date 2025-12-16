a3d06bc27720020b1588e1922517677f44c431ea
Alex Ainscow <aainscow@uk.ibm.com>
2025-12-16 14:11:34 +0000

    Fix Object op locks
    Signed-off-by: Alex Ainscow <aainscow@uk.ibm.com>

--- src/osdc/Objecter.cc
+++ src/osdc/Objecter.cc
@@ -2421,13 +2421,16 @@ void Objecter::_op_submit_with_budget(Op *op,
       op->tid = ++last_tid;
     }
     *ptid = op->tid;
-    OSDSession *s;;
+    OSDSession *s;
     int r = _get_session(op->target.osd, &s, sul);
     // The lock has been held since the last calc_target, so it should not
     // be possible for a new map to have appeared.
     ceph_assert(r == 0);
+    unique_lock sl(s->lock);
     _session_op_assign(s, op);
     inflight_ops++;
+    sl.unlock();
+    put_session(s);
   } else {
     _op_submit(op, sul, ptid);
   }
