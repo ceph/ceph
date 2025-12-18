670a8b3b270bd2feff09359d8a964bcc7fbb33bd
Afreen Misbah <afreen@ibm.com>
2025-12-19 00:27:08 +0530

    mgr/dashboard:Added modal for notiifcation area
    Signed-off-by: Afreen Misbah <afreen@ibm.com>

--- src/pybind/mgr/dashboard/frontend/src/app/core/navigation/notification-panel/notification-panel/notification-panel.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/navigation/notification-panel/notification-panel/notification-panel.component.ts
@@ -1,20 +1,26 @@
-import { Component, ElementRef, HostListener } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NotificationService } from '~/app/shared/services/notification.service';
+import { trigger, transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'cd-notification-panel',
   templateUrl: './notification-panel.component.html',
   styleUrls: ['./notification-panel.component.scss'],
-  standalone: false
+  standalone: false,
+  animations: [
+    trigger('panelAnimation', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateY(-38.5rem)' }),
+        animate(
+          '240ms cubic-bezier(0.2, 0, 0.38, 0.9)',
+          style({ opacity: 1, transform: 'translateY(0)' })
+        )
+      ])
+    ])
+  ]
 })
 export class NotificationPanelComponent {
-  constructor(public notificationService: NotificationService, private elementRef: ElementRef) {}
+  @Input() isPanelOpen: boolean = true;
 
-  @HostListener('document:click', ['$event'])
-  handleClickOutside(event: Event) {
-    const clickedInside = this.elementRef.nativeElement.contains(event.target);
-    if (!clickedInside) {
-      this.notificationService.toggleSidebar(false, true);
-    }
-  }
+  constructor(public notificationService: NotificationService) {}
 }
