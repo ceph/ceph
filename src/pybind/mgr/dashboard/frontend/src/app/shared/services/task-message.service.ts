24b7a67e1d7b114af52d0b1fbd6dee17512aaa59
Ankush Behl <cloudbehl@gmail.com>
2025-12-18 23:32:07 +0530

    mgr/dashboard: NFS Cluster create and Delete
    Fixes: https://tracker.ceph.com/issues/75845
    
    Signed-off-by: Dnyaneshwari Talwekar <dtalweka@redhat.com>
    (cherry picked from commit 07f00f57aedaac7973bd73ee430ee5d55e86764c)

--- src/pybind/mgr/dashboard/frontend/src/app/shared/services/task-message.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/task-message.service.ts
@@ -422,6 +422,12 @@ export class TaskMessageService {
     'nfs/create': this.newTaskMessage(this.commonOperations.create, (metadata) =>
       this.nfs(metadata)
     ),
+    'nfs/cluster/create': this.newTaskMessage(this.commonOperations.create, (metadata) =>
+      this.nfsCluster(metadata)
+    ),
+    'nfs/cluster/delete': this.newTaskMessage(this.commonOperations.delete, (metadata) =>
+      this.nfsCluster(metadata)
+    ),
     'nfs/edit': this.newTaskMessage(this.commonOperations.update, (metadata) => this.nfs(metadata)),
     'nfs/delete': this.newTaskMessage(this.commonOperations.delete, (metadata) =>
       this.nfs(metadata)
@@ -640,6 +646,10 @@ export class TaskMessageService {
     return $localize`NFS '${metadata.cluster_id}\:${
       metadata.export_id ? metadata.export_id : metadata.path
     }'`;
+  }
+
+  nfsCluster(metadata: { cluster_id: string }) {
+    return $localize`NFS cluster  '${metadata.cluster_id}'`;
   }
 
   smbCluster(metadata: { cluster_id: string }) {
