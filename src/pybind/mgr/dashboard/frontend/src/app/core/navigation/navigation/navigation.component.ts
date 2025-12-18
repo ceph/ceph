b6f239cc247a19b7a65edfb6aba986fe154735f3
Afreen Misbah <afreen@ibm.com>
2025-12-19 02:34:26 +0530

    mgr/dashboard: Added cdsGrid to header
    Signed-off-by: Afreen Misbah <afreen@ibm.com>
    (cherry picked from commit 5e9336a9aa84de568bb78d25c7b689a5ba02e21b)
    
     Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/core/navigation/notification-panel/notification-header/notification-header.component.html
    
    (rebased conflicts due to the merging of html lint changes)

--- src/pybind/mgr/dashboard/frontend/src/app/core/navigation/navigation/navigation.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/navigation/navigation/navigation.component.ts
@@ -32,7 +32,7 @@ export class NavigationComponent implements OnInit, OnDestroy {
   clusterTokenStatus: object = {};
   summaryData: any;
 
-  isNotifPanelOpen = false;
+  isNotifPanelOpen = true;
   showMenuSidebar = true;
 
   simplebar = {
