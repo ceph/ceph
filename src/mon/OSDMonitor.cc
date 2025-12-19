aa0c8862d20841084918d9b560de4003693a8a20
yulongshi0816 <52553877+yulongshi0816@users.noreply.github.com>
2025-12-19 15:44:32 +0800

    mon/fix: ceph osd pool set fails when setting fingerprint_algorithm or dedup_chunk_algorithm to none, but setting any other string works.
    When setting the deduplication algorithm or the fingerprint algorithm, the value cannot be none, but can be any other value. This bug occurs because any number other than 0 is evaluated as true.
    
    Signed-off-by: yulong Shi <sz.ylshi@evocsv.com>"
    
    Signed-off-by: yulongshi0816 <52553877+yulongshi0816@users.noreply.github.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -9114,7 +9114,7 @@ int OSDMonitor::prepare_command_pool_set(const cmdmap_t& cmdmap,
     } else if (var == "fingerprint_algorithm") {
       if (!unset) {
         auto alg = pg_pool_t::get_fingerprint_from_str(val);
-        if (!alg) {
+        if (alg == (pg_pool_t::fingerprint_t)-1) {
           ss << "unrecognized fingerprint_algorithm '" << val << "'";
 	  return -EINVAL;
         }
@@ -9196,7 +9196,7 @@ int OSDMonitor::prepare_command_pool_set(const cmdmap_t& cmdmap,
     } else if (var == "dedup_chunk_algorithm") {
       if (!unset) {
         auto alg = pg_pool_t::get_dedup_chunk_algorithm_from_str(val);
-        if (!alg) {
+        if (alg == (pg_pool_t::dedup_chunk_algo_t)-1) {
           ss << "unrecognized fingerprint_algorithm '" << val << "'";
 	  return -EINVAL;
         }
