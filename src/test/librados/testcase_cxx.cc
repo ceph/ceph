f75ee25373ae5bd95baf264a41478827ae4ee5ff
Matty Williams <Matty.Williams@ibm.com>
2025-12-23 13:42:37 +0000

    test: Add integration tests for EC Omap operations and recovery
    Fixes: https://tracker.ceph.com/issues/74188
    Signed-off-by: Matty Williams <Matty.Williams@ibm.com>

--- src/test/librados/testcase_cxx.cc
+++ src/test/librados/testcase_cxx.cc
@@ -429,3 +429,184 @@ void RadosTestECPP::set_allow_ec_overwrites()
     std::this_thread::sleep_for(std::chrono::seconds(2));
   }
 }
+
+std::string RadosTestECOptimisedPP::pool_name;
+Rados RadosTestECOptimisedPP::s_cluster;
+
+void RadosTestECOptimisedPP::SetUpTestCase()
+{
+  SKIP_IF_CRIMSON();
+  auto pool_prefix = fmt::format("{}_", ::testing::UnitTest::GetInstance()->current_test_case()->name());
+  pool_name = get_temp_pool_name(pool_prefix);
+  ASSERT_EQ("", create_one_ec_pool_pp(pool_name, s_cluster, true));
+}
+
+void RadosTestECOptimisedPP::TearDownTestCase()
+{
+  SKIP_IF_CRIMSON();
+  ASSERT_EQ(0, destroy_one_ec_pool_pp(pool_name, s_cluster));
+}
+
+void RadosTestECOptimisedPP::SetUp()
+{
+  SKIP_IF_CRIMSON();
+  ASSERT_EQ(0, cluster.ioctx_create(pool_name.c_str(), ioctx));
+  nspace = get_temp_pool_name();
+  ioctx.set_namespace(nspace);
+  bool req;
+  ASSERT_EQ(0, ioctx.pool_requires_alignment2(&req));
+  ASSERT_TRUE(req);
+  ASSERT_EQ(0, ioctx.pool_required_alignment2(&alignment));
+  ASSERT_NE(0U, alignment);
+}
+
+void RadosTestECOptimisedPP::TearDown()
+{
+  SKIP_IF_CRIMSON();
+  if (cleanup) {
+    cleanup_default_namespace(ioctx);
+    cleanup_namespace(ioctx, nspace);
+  }
+  ioctx.close();
+}
+
+void RadosTestECOptimisedPP::turn_balancing_off()
+{
+  int rc;
+  std::ostringstream oss;
+  bufferlist outbl;
+
+  oss.str("");
+  bufferlist inbl_autoscaler;
+  oss << "{\"prefix\": \"osd set\", \"key\": \"noautoscale\"}";
+  rc = cluster.mon_command(oss.str(), std::move(inbl_autoscaler), &outbl, nullptr);
+  EXPECT_EQ(rc, 0);
+
+  oss.str("");
+  oss << "{\"prefix\": \"balancer off\"}";
+  bufferlist inbl_balancer;
+  rc = cluster.mon_command(oss.str(), std::move(inbl_balancer), &outbl, nullptr);
+  EXPECT_EQ(rc, 0);
+}
+
+void RadosTestECOptimisedPP::turn_balancing_on()
+{
+  int rc;
+  std::ostringstream oss;
+  bufferlist outbl;
+
+  oss.str("");
+  bufferlist inbl_autoscaler;
+  oss << "{\"prefix\": \"osd unset\", \"key\": \"noautoscale\"}";
+  rc = cluster.mon_command(oss.str(), std::move(inbl_autoscaler), &outbl, nullptr);
+  EXPECT_EQ(rc, 0);
+
+  oss.str("");
+  oss << "{\"prefix\": \"balancer on\"}";
+  bufferlist inbl_balancer;
+  rc = cluster.mon_command(oss.str(), std::move(inbl_balancer), &outbl, nullptr);
+  EXPECT_EQ(rc, 0);
+}
+
+void RadosTestECOptimisedPP::enable_omap()
+{
+  bufferlist inbl, outbl;
+  std::ostringstream oss;
+  oss << "{\"prefix\": \"osd pool set\", \"pool\": \"" << pool_name
+      << "\", \"var\": \"supports_omap\", \"val\": \"true\"}";
+  int ret = cluster.mon_command(oss.str(), std::move(inbl), &outbl, nullptr);
+  EXPECT_EQ(ret, 0);
+}
+
+int RadosTestECOptimisedPP::request_osd_map(
+    std::string oid,
+    ceph::messaging::osd::OSDMapReply* reply) {
+  bufferlist inbl, outbl;
+  auto formatter = std::make_unique<JSONFormatter>(false);
+  ceph::messaging::osd::OSDMapRequest osdMapRequest{pool_name, oid, nspace};
+  encode_json("OSDMapRequest", osdMapRequest, formatter.get());
+
+  std::ostringstream oss;
+  formatter.get()->flush(oss);
+  int rc = cluster.mon_command(oss.str(), std::move(inbl), &outbl, nullptr);
+  if (rc != 0) {
+    return rc;
+  }
+
+  JSONParser p;
+  bool success = p.parse(outbl.c_str(), outbl.length());
+  if (!success) {
+    return -1;
+  }
+
+  reply->decode_json(&p);
+  return 0;
+}
+
+int RadosTestECOptimisedPP::set_osd_upmap(
+    std::string pgid,
+    std::vector<int> up_osds) {
+  bufferlist inbl, outbl;
+  std::ostringstream oss;
+  oss << "{\"prefix\": \"osd pg-upmap\", \"pgid\": \"" << pgid << "\", \"id\": [";
+  for (size_t i = 0; i < up_osds.size(); i++) {
+    oss << up_osds[i];
+    if (i != up_osds.size() - 1) {
+      oss << ", ";
+    }
+  }
+  oss << "]}";
+  int rc = cluster.mon_command(oss.str(), std::move(inbl), &outbl, nullptr);
+  return rc;
+}
+
+int RadosTestECOptimisedPP::wait_for_upmap(
+    std::string oid,
+    int desired_primary,
+    std::chrono::seconds timeout) {
+  bool upmap_in_effect = false;
+  auto start_time = std::chrono::steady_clock::now();
+  while (!upmap_in_effect && (std::chrono::steady_clock::now() - start_time < timeout)) {
+    ceph::messaging::osd::OSDMapReply reply;
+    int res = request_osd_map(oid, &reply);
+    EXPECT_TRUE(res == 0);
+    std::vector<int> acting_osds = reply.acting;
+    if (!acting_osds.empty() && acting_osds[0] == desired_primary) {
+      print_osd_map("New upmap in effect, acting set: ", acting_osds);
+      upmap_in_effect = true;
+    } else {
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+  }
+  return upmap_in_effect ? 0 : -ETIMEDOUT;
+}
+
+void RadosTestECOptimisedPP::print_osd_map(std::string message, std::vector<int> osd_vec) {
+  std::stringstream out_vec;
+  std::copy(osd_vec.begin(), osd_vec.end(), std::ostream_iterator<int>(out_vec, " "));
+  std::cout << message << out_vec.str().c_str() << std::endl;
+}
+
+void RadosTestECOptimisedPP::check_omap_read(
+    std::string oid,
+    std::string first_omap_key,
+    std::string first_omap_value,
+    int expected_size,
+    int expected_err) {
+  ObjectReadOperation read;
+  int err = 0;
+  std::map<std::string,bufferlist> vals_read{ {"_", {}} };
+  read.omap_get_vals2("", LONG_MAX, &vals_read, nullptr, &err);
+  int ret = ioctx.operate(oid, &read, nullptr);
+  EXPECT_EQ(ret, 0);
+  EXPECT_EQ(err, expected_err);
+  EXPECT_EQ(vals_read.size(), expected_size);
+  if (vals_read.find(first_omap_key) == vals_read.end()) {
+    ADD_FAILURE() << "Missing key " << first_omap_key;
+  } else {
+    bufferlist val_read_bl = vals_read[first_omap_key];
+    std::string val_read;
+    decode(val_read, val_read_bl);
+    EXPECT_EQ(first_omap_value, val_read);
+  }
+}
