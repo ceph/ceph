8bf1d69d6bbf3cebf47f7eafb373e8480d1b8b49
Puja Shahu <pshahu@li-4dbc3fcc-2cf0-11b2-a85c-8cca2743bba1.ibm.com>
2025-12-29 13:27:06 +0530

    mgr/dashboard: NVme-Subsystem list
    Fixes: https://tracker.ceph.com/issues/74284
    
    Signed-off-by:pujaoshahu <pshahu@redhat.com>
    Signed-off-by: Puja Shahu <pshahu@li-4dbc3fcc-2cf0-11b2-a85c-8cca2743bba1.ibm.com>
    Signed-off-by: pujaoshahu <pshahu@redhat.com>
    (cherry picked from commit db4950dc8b72da4127d3a3ffef61594532f00dc3)
    
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-gateway-group/nvmeof-gateway-group.component.ts

---
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-gateway-group/nvmeof-gateway-group.component.ts
@@ -0,0 +1,209 @@
+import { Component, OnInit, TemplateRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import { BehaviorSubject, forkJoin, Observable, of } from 'rxjs';
+import { catchError, map, switchMap, tap } from 'rxjs/operators';
+import { GatewayGroup, NvmeofService } from '~/app/shared/api/nvmeof.service';
+import { ActionLabelsI18n } from '~/app/shared/constants/app.constants';
+import { TableComponent } from '~/app/shared/datatable/table/table.component';
+import { CdTableAction } from '~/app/shared/models/cd-table-action';
+import { CdTableColumn } from '~/app/shared/models/cd-table-column';
+import { CdTableFetchDataContext } from '~/app/shared/models/cd-table-fetch-data-context';
+import { CdTableSelection } from '~/app/shared/models/cd-table-selection';
+import { Permission } from '~/app/shared/models/permissions';
+import { AuthStorageService } from '~/app/shared/services/auth-storage.service';
+import { Icons, IconSize } from '~/app/shared/enum/icons.enum';
+import { NvmeofGatewayGroup } from '~/app/shared/models/nvmeof';
+import { CephServiceSpec } from '~/app/shared/models/service.interface';
+import { ModalCdsService } from '~/app/shared/services/modal-cds.service';
+import { CephServiceService } from '~/app/shared/api/ceph-service.service';
+import { TaskWrapperService } from '~/app/shared/services/task-wrapper.service';
+import { DeleteConfirmationModalComponent } from '~/app/shared/components/delete-confirmation-modal/delete-confirmation-modal.component';
+import { FinishedTask } from '~/app/shared/models/finished-task';
+import { DeletionImpact } from '~/app/shared/enum/delete-confirmation-modal-impact.enum';
+import { NotificationService } from '~/app/shared/services/notification.service';
+import { NotificationType } from '~/app/shared/enum/notification-type.enum';
+
+@Component({
+  selector: 'cd-nvmeof-gateway-group',
+  templateUrl: './nvmeof-gateway-group.component.html',
+  styleUrls: ['./nvmeof-gateway-group.component.scss'],
+  standalone: false,
+  encapsulation: ViewEncapsulation.None
+})
+export class NvmeofGatewayGroupComponent implements OnInit {
+  @ViewChild(TableComponent, { static: true })
+  table: TableComponent;
+
+  @ViewChild('dateTpl', { static: true })
+  dateTpl: TemplateRef<any>;
+
+  @ViewChild('gatewayStatusTpl', { static: true })
+  gatewayStatusTpl: TemplateRef<any>;
+
+  @ViewChild('deleteTpl', { static: true })
+  deleteTpl: TemplateRef<any>;
+
+  permission: Permission;
+  tableActions: CdTableAction[];
+  columns: CdTableColumn[] = [];
+  selection: CdTableSelection = new CdTableSelection();
+  gatewayGroup$: Observable<CephServiceSpec[]>;
+  subject = new BehaviorSubject<CephServiceSpec[]>([]);
+  context: CdTableFetchDataContext;
+  gatewayGroupName: string;
+  subsystemCount: number;
+  gatewayCount: number;
+
+  icons = Icons;
+
+  iconSize = IconSize;
+
+  constructor(
+    public actionLabels: ActionLabelsI18n,
+    private authStorageService: AuthStorageService,
+    private nvmeofService: NvmeofService,
+    public modalService: ModalCdsService,
+    private cephServiceService: CephServiceService,
+    public taskWrapper: TaskWrapperService,
+    private notificationService: NotificationService
+  ) {}
+
+  ngOnInit(): void {
+    this.permission = this.authStorageService.getPermissions().nvmeof;
+
+    this.columns = [
+      {
+        name: $localize`Name`,
+        prop: 'name'
+      },
+      {
+        name: $localize`Gateways`,
+        prop: 'statusCount',
+        cellTemplate: this.gatewayStatusTpl
+      },
+      {
+        name: $localize`Subsystems`,
+        prop: 'subSystemCount'
+      },
+      {
+        name: $localize`Created on`,
+        prop: 'created',
+        cellTemplate: this.dateTpl
+      }
+    ];
+
+    const createAction: CdTableAction = {
+      permission: 'create',
+      icon: Icons.add,
+      name: this.actionLabels.CREATE,
+      canBePrimary: (selection: CdTableSelection) => !selection.hasSelection
+    };
+
+    const deleteAction: CdTableAction = {
+      permission: 'delete',
+      icon: Icons.destroy,
+      click: () => this.deleteGatewayGroupModal(),
+      name: this.actionLabels.DELETE,
+      canBePrimary: (selection: CdTableSelection) => selection.hasMultiSelection
+    };
+    this.tableActions = [createAction, deleteAction];
+    this.gatewayGroup$ = this.subject.pipe(
+      switchMap(() =>
+        this.nvmeofService.listGatewayGroups().pipe(
+          switchMap((gatewayGroups: GatewayGroup[][]) => {
+            const groups = gatewayGroups?.[0] ?? [];
+            if (groups.length === 0) {
+              return of([]);
+            }
+            return forkJoin(
+              groups.map((group: NvmeofGatewayGroup) => {
+                const isRunning = (group.status?.running ?? 0) > 0;
+                const subsystemsObservable = isRunning
+                  ? this.nvmeofService
+                      .listSubsystems(group.spec.group)
+                      .pipe(catchError(() => of([])))
+                  : of([]);
+
+                return subsystemsObservable.pipe(
+                  map((subs) => ({
+                    ...group,
+                    name: group.spec?.group,
+                    statusCount: {
+                      running: group.status?.running ?? 0,
+                      error: (group.status?.size ?? 0) - (group.status?.running ?? 0)
+                    },
+                    subSystemCount: Array.isArray(subs) ? subs.length : 0,
+                    gateWayNode: group.placement?.hosts?.length ?? 0,
+                    created: group.status?.created ? new Date(group.status.created) : null
+                  }))
+                );
+              })
+            );
+          }),
+          catchError((error) => {
+            this.notificationService.show(
+              NotificationType.error,
+              $localize`Unable to fetch Gateway group`,
+              $localize`Gateway group does not exist`
+            );
+            if (error?.preventDefault) {
+              error.preventDefault();
+            }
+            return of([]);
+          })
+        )
+      )
+    );
+  }
+
+  fetchData(): void {
+    this.subject.next([]);
+  }
+
+  updateSelection(selection: CdTableSelection): void {
+    this.selection = selection;
+  }
+
+  deleteGatewayGroupModal() {
+    const selectedGroup = this.selection.first();
+    if (!selectedGroup) {
+      return;
+    }
+    const {
+      service_name: serviceName,
+      spec: { group }
+    } = selectedGroup;
+
+    const disableForm = selectedGroup.subSystemCount > 0 || !group;
+
+    this.modalService.show(DeleteConfirmationModalComponent, {
+      impact: DeletionImpact.high,
+      itemDescription: $localize`gateway group`,
+      bodyTemplate: this.deleteTpl,
+      itemNames: [selectedGroup.spec.group],
+      bodyContext: {
+        disableForm,
+        subsystemCount: selectedGroup.subSystemCount
+      },
+      submitActionObservable: () => {
+        return this.taskWrapper
+          .wrapTaskAroundCall({
+            task: new FinishedTask('nvmeof/gateway/delete', { group: selectedGroup.spec.group }),
+            call: this.cephServiceService.delete(serviceName)
+          })
+          .pipe(
+            tap(() => {
+              this.table.refreshBtn();
+            }),
+            catchError((error) => {
+              this.table.refreshBtn();
+              this.notificationService.show(
+                NotificationType.error,
+                $localize`${`Failed to delete gateway group ${selectedGroup.spec.group}: ${error.message}`}`
+              );
+              return of(null);
+            })
+          );
+      }
+    });
+  }
+}
