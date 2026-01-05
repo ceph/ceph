4b9be377cf32e93b371d58c91fb4a5b6069f1dc9
Ville Ojamo <14869000+bluikko@users.noreply.github.com>
2026-01-05 13:10:45 +0700

    doc: Remove sphinxcontrib-seqdiag Python package from RTD builds
    This is a proactive PR to avoid breaking docs builds when Setuptools 81
    starts to be used in the RTD builds process.
    
    The sphnixcontrib-seqdiag Python package is not compatible with
    Setuptools 81 or later due to use of pkg_resources:
    https://setuptools.pypa.io/en/latest/pkg_resources.html
    
    Setuptools 81 release should be imminent, with the Python deprecation
    warning stating pkg_resources "removal as early as 2025-11-30".
    
    Seqdiag seems to be unmaintained with the latest update at Pypi in
    the year 2021 and also no updates to the seqdiag git repo.
    
    There are no seqdiag directives left in the docs after last seqdiags
    were removed in PR #52308.
    
    Two other options would exist for fixing the situation (see PR for
    discussion) but this seems to be the suitable one.
    
    Signed-off-by: Ville Ojamo <14869000+bluikko@users.noreply.github.com>
    (cherry picked from commit 15481e509b4d644d0644188501d86a4ceda2c039)
    
    Conflicts:
    	doc/conf.py: formatting difference

--- doc/conf.py
+++ doc/conf.py
@@ -128,8 +128,7 @@ extensions = [
     'ceph_confval',
     'sphinxcontrib.mermaid',
     'sphinxcontrib.openapi',
-    'sphinxcontrib.seqdiag',
-    ]
+]
 
 ditaa = shutil.which("ditaa")
 if ditaa is not None:
@@ -237,10 +236,6 @@ for c in pybinds:
 # openapi
 openapi_logger = sphinx.util.logging.getLogger('sphinxcontrib.openapi.openapi30')
 openapi_logger.setLevel(logging.WARNING)
-
-# seqdiag
-seqdiag_antialias = True
-seqdiag_html_image_format = 'SVG'
 
 # ceph_confval
 ceph_confval_imports = glob.glob(os.path.join(top_level,
