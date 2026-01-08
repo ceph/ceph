49749d4f3617959ee4b2c332103f554444098dfa
Adam King <adking@redhat.com>
2026-01-08 14:59:29 -0500

    qa/cephadm: add default container image name base
    Recent runs in the new lab for cephadm jobs are hitting
    
    ```
    2026-01-06T00:47:00.247 ERROR:teuthology.run_tasks:Saw exception from tasks.
    Traceback (most recent call last):
      File "/home/teuthworker/src/git.ceph.com_teuthology_8707f5d0ca0e547efc56a3734c7aa4a4cf45b1f4/teuthology/run_tasks.py", line 112, in run_tasks
        manager.__enter__()
      File "/usr/lib/python3.12/contextlib.py", line 137, in __enter__
        return next(self.gen)
               ^^^^^^^^^^^^^^
      File "/home/teuthworker/src/github.com_ceph_ceph-c_73712ccf0d7b99648a0c0d2ebf766adc13587a45/qa/tasks/cephadm.py", line 1961, in task
        raise Exception("Configuration error occurred. "
    Exception: Configuration error occurred. The 'image' value is undefined for 'cephadm' task. Please provide...
    ```
    
    but it appears the build is still pushing images to quay.ceph.io/ceph-ci/ceph:<sha1>
    which is the image we would be looking for. The bit that raises an exception if
    this isn't set is quite old, and I'm unsure if we still need it or it's safe
    to just assume this naming scheme can be used if an explicit image is
    passed, but this seems worth a try
    
    Fixes: https://tracker.ceph.com/issues/74477
    
    Signed-off-by: Adam King <adking@redhat.com>
    (cherry picked from commit 1b155707a2b463240e5bf1c73f9ef0ade93cd7be)
    (cherry picked from commit 7cd40142fb6af74773beea70a19ee6ee6b1001bc)

--- qa/tasks/cephadm.py
+++ qa/tasks/cephadm.py
@@ -1684,11 +1684,7 @@ def task(ctx, config):
     ref = ctx.config.get("branch", "main")
     if not ctx.ceph[cluster_name].image:
         if not container_image_name:
-            raise Exception("Configuration error occurred. "
-                            "The 'image' value is undefined for 'cephadm' task. "
-                            "Please provide corresponding options in the task's "
-                            "config, task 'overrides', or teuthology 'defaults' "
-                            "section.")
+            container_image_name = 'quay.ceph.io/ceph-ci/ceph'
         sha1 = config.get('sha1')
         flavor = config.get('flavor', 'default')
 
