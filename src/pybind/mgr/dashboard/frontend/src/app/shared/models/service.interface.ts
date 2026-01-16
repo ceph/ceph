0e07869e96e819b7c1dbbe2042c7eb07d884b8a7
Abhishek Desai <abhishek.desai1@ibm.com>
2026-01-16 20:01:54 +0530

    mgr/dashboard : Add Cert Status column to services page fixes : https://tracker.ceph.com/issues/74411 Signed-off-by: Abhishek Desai <abhishek.desai1@ibm.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/models/service.interface.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/models/service.interface.ts
@@ -7,6 +7,27 @@ export interface CephServiceStatus {
   created: Date;
 }
 
+export enum CephCertificateStatus {
+  valid = 'valid',
+  expired = 'expired',
+  expiring = 'expiring',
+  expiringSoon = 'expiring_soon',
+  notConfigured = 'not_configured',
+  invalid = 'invalid'
+}
+
+export interface CephServiceCertificate {
+  cert_name: string;
+  scope: string;
+  requires_certificate: boolean;
+  status: CephCertificateStatus | string;
+  days_to_expiration: number;
+  signed_by: string;
+  has_certificate: boolean;
+  certificate_source: string;
+  expiry_date: string;
+}
+
 // This will become handy when creating arbitrary services
 export interface CephServiceSpec {
   service_name: string;
@@ -14,6 +35,7 @@ export interface CephServiceSpec {
   service_id: string;
   unmanaged: boolean;
   status: CephServiceStatus;
+  certificate?: CephServiceCertificate;
   spec: CephServiceAdditionalSpec;
   placement: CephServicePlacement;
 }
