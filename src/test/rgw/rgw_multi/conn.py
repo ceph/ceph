0cbb542db86e3e0f7b22d57a80e25e31d1f6e701
Shilpa Jagannath <smanjara@redhat.com>
2026-01-20 18:42:30 -0500

    qa/multisite: switch to boto3 in multisite test libraries
    Signed-off-by: Shilpa Jagannath <smanjara@redhat.com>
    (cherry picked from commit c84efd9af5a4e2845745285ada994aad072eb2a0)

--- src/test/rgw/rgw_multi/conn.py
+++ src/test/rgw/rgw_multi/conn.py
@@ -5,8 +5,14 @@ import boto.sts.connection
 import boto3
 from boto.regioninfo import RegionInfo
 
-def get_gateway_connection(gateway, credentials):
+def get_gateway_connection(gateway, credentials, region):
     """ connect to the given gateway """
+    # Always create a new connection to the gateway to ensure each set of credentials gets its own connection
+    conn = boto3.client('s3',
+                        endpoint_url='http://' + gateway.host + ':' + str(gateway.port),
+                        aws_access_key_id=credentials.access_key,
+                        aws_secret_access_key=credentials.secret,
+                        region_name=region)
     if gateway.connection is None:
         gateway.connection = boto.connect_s3(
                 aws_access_key_id = credentials.access_key,
@@ -17,19 +23,16 @@ def get_gateway_connection(gateway, credentials):
                 calling_format = boto.s3.connection.OrdinaryCallingFormat())
     return gateway.connection
 
-def get_gateway_secure_connection(gateway, credentials):
+def get_gateway_secure_connection(gateway, credentials, region):
     """ secure connect to the given gateway """
     if gateway.ssl_port == 0:
         return None
     if gateway.secure_connection is None:
-        gateway.secure_connection = boto.connect_s3(
-            aws_access_key_id = credentials.access_key,
-            aws_secret_access_key = credentials.secret,
-            host = gateway.host,
-            port = gateway.ssl_port,
-            is_secure = True,
-            validate_certs=False,
-            calling_format = boto.s3.connection.OrdinaryCallingFormat())
+        gateway.secure_connection = boto3.client('s3',
+                                                endpoint_url='http://' + gateway.host + ':' + str(gateway.port),
+                                                aws_access_key_id=credentials.access_key,
+                                                aws_secret_access_key=credentials.secret,
+                                                region_name=region)
     return gateway.secure_connection
 
 def get_gateway_iam_connection(gateway, credentials, region):
@@ -59,17 +62,6 @@ def get_gateway_sts_connection(gateway, credentials, region):
                 region_name=region,
                 use_ssl = False)
     return gateway.sts_connection
-
-
-def get_gateway_s3_client(gateway, credentials, region):
-  """ connect to boto3 s3 client api of the given gateway """
-  if gateway.s3_client is None:
-      gateway.s3_client = boto3.client('s3',
-                                        endpoint_url='http://' + gateway.host + ':' + str(gateway.port),
-                                        aws_access_key_id=credentials.access_key,
-                                        aws_secret_access_key=credentials.secret,
-                                        region_name=region)
-  return gateway.s3_client
 
 
 def get_gateway_sns_client(gateway, credentials, region):
