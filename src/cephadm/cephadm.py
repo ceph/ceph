4e13c6cc73ff53fe38a825d4101eb566d1ca4c6c
Redouane Kachach <rkachach@ibm.com>
2026-01-26 14:07:22 +0100

    cephadm: enable ceph_secrets mgr module during bootstrap
    Enable the new ceph_secrets mgr module automatically during cephadm
    bootstrap so secrets functionality is available by default on new
    clusters. Cephadm and dashboard depends on this new module for secrets
    handling.
    
    https://tracker.ceph.com/issues/74562
    
    Signed-off-by: Redouane Kachach <rkachach@ibm.com>

--- src/cephadm/cephadm.py
+++ src/cephadm/cephadm.py
@@ -2789,6 +2789,7 @@ def enable_cephadm_mgr_module(
 ) -> None:
 
     logger.info('Enabling cephadm module...')
+    cli(['mgr', 'module', 'enable', 'ceph_secrets'])
     cli(['mgr', 'module', 'enable', 'cephadm'])
     wait_for_mgr_restart(['cephadm'])
     # https://tracker.ceph.com/issues/67969
