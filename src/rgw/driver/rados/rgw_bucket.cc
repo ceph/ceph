a3509f6944f7db71a5930445037c4264b67252cf
Casey Bodley <cbodley@redhat.com>
2026-02-03 10:19:39 -0500

    rgw: bucket_stats() uses local variable 'index'
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/rgw/driver/rados/rgw_bucket.cc
+++ src/rgw/driver/rados/rgw_bucket.cc
@@ -1620,10 +1620,9 @@ static int bucket_stats(rgw::sal::Driver* driver,
   ::encode_json("explicit_placement", bucket->get_key().explicit_placement, formatter);
   formatter->dump_string("id", bucket->get_bucket_id());
   formatter->dump_string("marker", bucket->get_marker());
-  formatter->dump_stream("index_type") << bucket_info.layout.current_index.layout.type;
-  formatter->dump_int("index_generation", bucket_info.layout.current_index.gen);
-  formatter->dump_int("num_shards",
-		      bucket_info.layout.current_index.layout.normal.num_shards);
+  formatter->dump_stream("index_type") << index.layout.type;
+  formatter->dump_int("index_generation", index.gen);
+  formatter->dump_int("num_shards", index.layout.normal.num_shards);
   formatter->dump_string("reshard_status", to_string(bucket_info.layout.resharding));
   logrecord_ut.gmtime(formatter->dump_stream("judge_reshard_lock_time"));
   formatter->dump_bool("object_lock_enabled", bucket_info.obj_lock_enabled());
