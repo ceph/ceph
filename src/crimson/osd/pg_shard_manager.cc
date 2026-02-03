58601ac8a2e3d0f67abf64dc188bda4eafddd2b9
chunmei liu <chunmei.liu@ibm.com>
2026-02-03 14:29:26 -0800

    crimson/osd/shard_services: get multiple store shards for per local state, and use store index to create pg mapping
    Signed-off-by: chunmei liu <chunmei.liu@ibm.com>

--- src/crimson/osd/pg_shard_manager.cc
+++ src/crimson/osd/pg_shard_manager.cc
@@ -20,19 +20,20 @@ seastar::future<> PGShardManager::load_pgs(crimson::os::FuturizedStore& store)
     return seastar::parallel_for_each(
       colls_cores,
       [this](auto coll_core) {
-        auto[coll, shard_core] = coll_core;
+        auto[coll, shard_core_index] = coll_core;
+        auto[shard_core, store_index] = shard_core_index;
 	spg_t pgid;
 	if (coll.is_pg(&pgid)) {
           return get_pg_to_shard_mapping().get_or_create_pg_mapping(
-            pgid, shard_core
-          ).then([this, pgid] (auto core) {
+            pgid, shard_core, store_index
+          ).then([this, pgid] (auto core_store) {
             return this->with_remote_shard_state(
-              core,
-              [pgid](
+              core_store.first,
+              [pgid, core_store](
 	      PerShardState &per_shard_state,
 	      ShardServices &shard_services) {
 	      return shard_services.load_pg(
-		pgid
+		pgid, core_store.second
 	      ).then([pgid, &per_shard_state](auto &&pg) {
 		logger().info("load_pgs: loaded {}", pgid);
 		return pg->clear_temp_objects(
