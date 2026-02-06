3e2c74ec95c922f7e028f84455d8221130da239e
Raja Sharma <raja@ibm.com>
2026-02-06 10:43:58 +0530

    rgw/iam: list policies
    Signed-off-by: Raja Sharma <raja@ibm.com>

--- src/rgw/rgw_sal_dbstore.cc
+++ src/rgw/rgw_sal_dbstore.cc
@@ -1523,6 +1523,20 @@ int DBStore::delete_customer_managed_policy(const DoutPrefixProvider* dpp,
   return -ENOTSUP;
 }
 
+int DBStore::list_customer_mananged_policies(const DoutPrefixProvider* dpp,
+                          optional_yield y,
+                          std::string_view account_id,
+                          rgw::IAM::Scope scope,
+                          bool only_attached,
+                          std::string_view path_prefix,
+                          rgw::IAM::PolicyUsageFilter policy_usage_filter,
+                          std::string_view marker,
+                          uint32_t max_items,
+                          rgw::IAM::PolicyList& listing)
+{
+  return -ENOTSUP;
+}
+
   std::unique_ptr<Writer> DBStore::get_append_writer(const DoutPrefixProvider *dpp,
 				  optional_yield y,
 				  rgw::sal::Object* obj,
