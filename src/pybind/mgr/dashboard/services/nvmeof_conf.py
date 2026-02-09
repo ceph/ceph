6e34b705a98efa02de3c9e2c0333b0cad265c286
Vallari Agrawal <vallari.agrawal@ibm.com>
2026-02-10 02:58:28 +0530

    src/pybind/mgr: Add nvmeof-top tool
    Add src/pybind/mgr/dashboard/services/nvmeof_top_cli.py
    which adds commands:
    "ceph nvmeof top io <subsystem>"
    "ceph nvmeof top cpu"
    
    This file is moved from the original work in:
    https://github.com/pcuzner/ceph-nvmeof-top
    
    Fixes: https://tracker.ceph.com/issues/64578
    
    Co-authored-by: Paul Cuzner <pcuzner@ibm.com>
    Signed-off-by: Vallari Agrawal <vallari.agrawal@ibm.com>
    (cherry picked from commit 23bb563ed8b3970af0c1824c19444163a5fb62d0)

--- src/pybind/mgr/dashboard/services/nvmeof_conf.py
+++ src/pybind/mgr/dashboard/services/nvmeof_conf.py
@@ -216,3 +216,12 @@ def is_mtls_enabled(service_name: str):
         return orch.services.get(service_name)[0].spec.enable_auth
     except OrchestratorError:
         return False
+
+
+def get_pool_group_name(service_name: str):
+    try:
+        orch = OrchClient.instance()
+        spec = orch.services.get(service_name)[0].spec
+        return (spec.pool, spec.group)
+    except OrchestratorError:
+        return None
