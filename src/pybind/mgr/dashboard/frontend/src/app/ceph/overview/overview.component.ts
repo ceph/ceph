1a3737bc64187eff3041a2b515ccc4ef261cf7f4
Afreen Misbah <afreen@ibm.com>
2026-02-09 12:58:16 +0530

    Added query tital and used capacity data
    Signed-off-by: Afreen Misbah <afreen@ibm.com>
    (cherry picked from commit 621c214d010162e205a1573d1dc1c074b4fb71d1)

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/overview/overview.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/overview/overview.component.ts
@@ -1,6 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { GridModule, TilesModule } from 'carbon-components-angular';
 import { OverviewStorageCardComponent } from './storage-card/overview-storage-card.component';
+import { HealthService } from '~/app/shared/api/health.service';
+import { HealthSnapshotMap } from '~/app/shared/models/health.interface';
+import { RefreshIntervalService } from '~/app/shared/services/refresh-interval.service';
+import { catchError, exhaustMap, takeUntil } from 'rxjs/operators';
+import { EMPTY, Subject } from 'rxjs';
 
 @Component({
   selector: 'cd-overview',
@@ -9,4 +14,32 @@ import { OverviewStorageCardComponent } from './storage-card/overview-storage-ca
   templateUrl: './overview.component.html',
   styleUrl: './overview.component.scss'
 })
-export class OverviewComponent {}
+export class OverviewComponent implements OnInit {
+  totalCapacity: number;
+  usedCapacity: number;
+  private destroy$ = new Subject<void>();
+
+
+  constructor(private healthService: HealthService, private refreshIntervalService: RefreshIntervalService) {}
+
+  ngOnInit(): void {
+    this.refreshIntervalObs(() =>  this.healthService.getHealthSnapshot()).subscribe({
+      next: (healthData: HealthSnapshotMap) => {
+      this.totalCapacity = healthData?.pgmap?.bytes_total;
+      this.usedCapacity = healthData?.pgmap?.bytes_used;
+  }});
+  }
+
+   refreshIntervalObs(fn: Function) {
+      return this.refreshIntervalService.intervalData$.pipe(
+        exhaustMap(() => fn().pipe(catchError(() => EMPTY))),
+        takeUntil(this.destroy$)
+      );
+    }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+}
