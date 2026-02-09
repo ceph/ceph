ff51af416791e34fb95ea7fe7d9ddd89a4790699
Afreen Misbah <afreen@ibm.com>
2026-02-09 12:58:16 +0530

    Added query tital and used capacity data
    Signed-off-by: Afreen Misbah <afreen@ibm.com>
    (cherry picked from commit 621c214d010162e205a1573d1dc1c074b4fb71d1)

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/overview/storage-card/overview-storage-card.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/overview/storage-card/overview-storage-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, Input, OnChanges, ViewEncapsulation } from '@angular/core';
 import {
   CheckboxModule,
   DropdownModule,
@@ -8,6 +8,7 @@ import {
 } from 'carbon-components-angular';
 import { ProductiveCardComponent } from '~/app/shared/components/productive-card/productive-card.component';
 import { MeterChartComponent, MeterChartOptions } from '@carbon/charts-angular';
+import { DimlessBinaryPipe } from '~/app/shared/pipes/dimless-binary.pipe';
 
 const StorageType = {
   ALL: $localize`All`,
@@ -15,6 +16,30 @@ const StorageType = {
   FILE: $localize`File`,
   OBJECT: $localize`Object`
 };
+
+// const Query = {
+//   RAW: {
+//     [StorageType.ALL]: `sum by (application) (ceph_pool_bytes_used * on(pool_id) group_left(instance, name, application) ceph_pool_metadata{application=~"(.*Block.*)|(.*Filesystem.*)|(.*Object.*)|(..*)"})`,
+//     [StorageType.BLOCK]: "",
+//     [StorageType.FILE]: "",
+//     [StorageType.OBJECT]: ""
+//   },
+//   USED: {
+//     [StorageType.ALL]: `sum by (application) (ceph_pool_stored * on(pool_id) group_left(instance, name, application) ceph_pool_metadata{application=~"(.*Block.*)|(.*Filesystem.*)|(.*Object.*)|(..*)"})`,
+//     [StorageType.BLOCK]: "",
+//     [StorageType.FILE]: "",
+//     [StorageType.OBJECT]: ""
+//   }
+// }
+
+/**
+ * 1. Fetch snapshot query -> [pass total and used raw]
+ * 2. Show the usage title -> always fixed
+ * 3. The chart total -> raw total always fixed
+ * 4. Set data for block, file , object, all -> raw, sep queries
+ * 5. Set data for block, file object + replicated -> usable
+ * 6. Dont show what is 0
+ */
 
 @Component({
   selector: 'cd-overview-storage-card',
@@ -32,12 +57,18 @@ const StorageType = {
   styleUrl: './overview-storage-card.component.scss',
   encapsulation: ViewEncapsulation.None
 })
-export class OverviewStorageCardComponent {
+export class OverviewStorageCardComponent implements OnChanges {
+  @Input() total!: number;
+  @Input() used!: number;
+  totalRaw: string;
+  usedRaw: string;
+  totalRawUnit: string;
+  usedRawUnit: string;
   options: MeterChartOptions = {
     height: '45px',
     meter: {
       proportional: {
-        total: 2000,
+        total: null,
         unit: 'GB',
         breakdownFormatter: (_e) => null,
         totalFormatter: (_e) => null
@@ -58,16 +89,15 @@ export class OverviewStorageCardComponent {
   allData = [
     {
       group: StorageType.BLOCK,
-      value: 202
+      value: 100,
     },
     {
       group: StorageType.FILE,
-      value: 654
+      value: 105
     },
     {
       group: StorageType.OBJECT,
-      value: 120
-    }
+      value: 60    }
   ];
   dropdownItems = [
     { content: StorageType.ALL },
@@ -78,6 +108,40 @@ export class OverviewStorageCardComponent {
   isRawCapacity: boolean = true;
   selectedStorageType: string = StorageType.ALL;
   displayData = this.allData;
+
+  constructor(private dimlessBinaryPipe: DimlessBinaryPipe) {}
+
+ngOnChanges(): void {
+  if (this.total == null || this.used == null) return;
+
+  const totalRaw = this.dimlessBinaryPipe.transform(this.total);
+  const usedRaw = this.dimlessBinaryPipe.transform(this.used);
+
+  const [totalValue, totalUnit] = totalRaw.split(/\s+/);
+  const [usedValue, usedUnit] = usedRaw.split(/\s+/);
+
+  const cleanedTotal = Number(totalValue.replace(/,/g, '').trim());
+
+  if (Number.isNaN(cleanedTotal)) return;
+
+  this.totalRaw = totalValue;
+  this.totalRawUnit = totalUnit;
+  this.usedRaw = usedValue;
+  this.usedRawUnit = usedUnit;
+
+  // chart reacts to 'options' and 'data' changes only, hence mandatory to replace whole object
+  this.options = {
+    ...this.options,
+    meter: {
+      ...this.options.meter,
+      proportional: {
+        ...this.options.meter.proportional,
+        total: cleanedTotal,
+        unit: totalUnit
+      }
+    }
+  };
+}
 
   toggleRawCapacity(isChecked: boolean) {
     this.isRawCapacity = isChecked;
