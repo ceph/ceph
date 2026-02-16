d0c5a08849f5d4d3c96c7bb890ed0a119d661861
Sagar Gopale <sagar.gopale@ibm.com>
2026-02-16 18:18:42 +0530

    mgr/dashboard: NVme- Add namespace in subsystem resource page
    Fixes: https://tracker.ceph.com/issues/74338
    
    Signed-off-by: Sagar Gopale <sagar.gopale@ibm.com>
    (cherry picked from commit abc2cd078f434eb6705eaddb3fc2476b280d3dca)

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-namespaces-list/nvmeof-namespaces-list.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-namespaces-list/nvmeof-namespaces-list.component.ts
@@ -109,17 +109,13 @@ export class NvmeofNamespacesListComponent implements OnInit, OnDestroy {
         name: $localize`Expand`,
         permission: 'update',
         icon: Icons.edit,
-        click: () =>
+        click: (row: NvmeofSubsystemNamespace) => {
+          const namespace = row || this.selection.first();
           this.router.navigate(
             [
               {
                 outlets: {
-                  modal: [
-                    URLVerbs.EDIT,
-                    this.selection.first().ns_subsystem_nqn,
-                    'namespace',
-                    this.selection.first().nsid
-                  ]
+                  modal: [URLVerbs.EDIT, namespace.ns_subsystem_nqn, 'namespace', namespace.nsid]
                 }
               }
             ],
@@ -128,7 +124,8 @@ export class NvmeofNamespacesListComponent implements OnInit, OnDestroy {
               queryParams: { group: this.group },
               queryParamsHandling: 'merge'
             }
-          )
+          );
+        }
       },
       {
         name: this.actionLabels.DELETE,
