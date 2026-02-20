92b185260d9409fc5af11e6472ac5d1ffc6ee10f
Timothy Q Nguyen <timqn22@gmail.com>
2026-02-20 11:55:49 -0800

    mgr/cephadm: align to new commands
    Updated the commands to follow the new CephadmCLICommand
    structure. It should exactly the same as before.
    
    Signed-off-by: Timothy Q Nguyen <timqn22@gmail.com>

--- src/pybind/mgr/cephadm/module.py
+++ src/pybind/mgr/cephadm/module.py
@@ -63,8 +63,6 @@ from mgr_module import (
     MonCommandFailed,
     MgrModuleRecoverDB,
     CLIRequiresDB,
-    CLIReadCommand,
-    CLIWriteCommand,
 )
 from mgr_util import build_url
 import orchestrator
@@ -4375,7 +4373,7 @@ Then run the following:
         self.event.set()
 
     @CLIRequiresDB
-    @CLIReadCommand('cephadm get-cluster-version-history')
+    @CephadmCLICommand.Read('cephadm get-cluster-version-history')
     @MgrModuleRecoverDB
     def do_get_cluster_version_history(self) -> Tuple[int, str, str]:
         '''
@@ -4384,7 +4382,7 @@ Then run the following:
         return self.version_tracker.get_cluster_version_history()
 
     @CLIRequiresDB
-    @CLIWriteCommand('cephadm remove-cluster-version-history')
+    @CephadmCLICommand.Write('cephadm remove-cluster-version-history')
     @MgrModuleRecoverDB
     def do_remove_cluster_version_history(self, all: Optional[bool] = False, before: Optional[str] = None, after: Optional[str] = None) -> Tuple[int, str, str]:
         '''
