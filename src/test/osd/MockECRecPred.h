f5debfe6b244675c6f659f99f39fef34b014ba92
Alex Ainscow <aainscow@uk.ibm.com>
2026-02-28 23:58:51 +0000

    Refactor: Extract mock classes from TestPeeringState into separate header files
    Move the following mock classes out of TestPeeringState.cc into their own
    header files. The code is identical - this is a pure refactor with no
    functional changes:
    
    - MockConnection -> MockConnection.h
    - MockLog -> MockLog.h
    - MockECRecPred -> MockECRecPred.h
    - MockECReadPred -> MockECReadPred.h
    - MockPGBackendListener -> MockPGBackendListener.h
    - MockPGBackend -> MockPGBackend.h
    - MockPGLogEntryHandler -> MockPGLogEntryHandler.h
    - MockPeeringListener -> MockPeeringListener.h
    
    TestPeeringState.cc is updated to include the new headers instead of
    defining the classes inline.
    
    Signed-off-by: Alex Ainscow <aainscow@uk.ibm.com>

---
+++ src/test/osd/MockECRecPred.h
@@ -0,0 +1,31 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:nil -*-
+// vim: ts=8 sw=2 sts=2 expandtab
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright (C) 2026 IBM
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
+#pragma once
+
+#include <set>
+#include "osd/PGBackend.h"
+
+// MockECRecPred - simple stub for IsPGRecoverablePredicate
+// Warning - this always returns true. This means we cannot test scenarios
+// where there are too many OSDs down and the PG should be incomplete
+class MockECRecPred : public IsPGRecoverablePredicate {
+ public:
+  MockECRecPred() {}
+
+  bool operator()(const std::set<pg_shard_t> &_have) const override {
+    return true;
+  }
+};
+
