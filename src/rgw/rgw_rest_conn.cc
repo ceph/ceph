475d0213659f61812d37698ecaa4fafd024d0c28
Oguzhan Ozmen <oozmen@bloomberg.net>
2026-03-03 01:05:06 +0000

    rgw/rest: remove unused headers
    Signed-off-by: Oguzhan Ozmen <oozmen@bloomberg.net>

--- src/rgw/rgw_rest_conn.cc
+++ src/rgw/rgw_rest_conn.cc
@@ -1,9 +1,7 @@
 // -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:nil -*-
 // vim: ts=8 sw=2 sts=2 expandtab ft=cpp
 
-#include "rgw_zone.h"
 #include "rgw_rest_conn.h"
-#include "rgw_http_errors.h"
 #include "rgw_sal.h"
 #include "rgw_resolve.h"
 
