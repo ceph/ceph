14042e5333b281c78abce866e34d7c138be4fcb0
Joshua Blanch <joshua.blanch@clyso.com>
2026-03-03 19:37:06 +0000

    os/bluestore: adds assert for device mounted before expand
    Signed-off-by: Joshua Blanch <joshua.blanch@clyso.com>

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -9123,6 +9123,7 @@ int BlueStore::expand_devices(ostream& out)
                 << std::dec << "(" << byte_u_t(size) << ")"
                 << std::endl;
           }
+          ceph_assert(mounted);
           bluefs->expand_device(devid, size, old_size);
         }
       }
