13f71c250c21794aad8b0e024fa7883621036f6f
pujaoshahu <pshahu@redhat.com>
2026-03-04 14:02:54 +0530

    mgr/dashboard: Breadcrumb should allow going back to subsystem tab
    Fixes:  https://tracker.ceph.com/issues/75288
    
    Signed-off-by: pujaoshahu <pshahu@redhat.com>
    (cherry picked from commit 69a7c6bf151a1e1d256b15c4dd1e5c590145005e)
    
     Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/block.module.ts
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-gateway/nvmeof-gateway.component.ts
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-group-form/nvmeof-group-form.component.html
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-namespaces-form/nvmeof-namespaces-form.component.ts
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-namespaces-list/nvmeof-namespaces-list.component.html
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystems-form/nvmeof-subsystems-form.component.ts
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystems/nvmeof-subsystems.component.html
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-tabs/nvmeof-tabs.component.html
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-tabs/nvmeof-tabs.component.spec.ts
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-tabs/nvmeof-tabs.component.ts

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-tabs/nvmeof-tabs.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-tabs/nvmeof-tabs.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+const NVMEOF_PATH = 'block/nvmeof';
+
+enum TABS {
+  gateways = 'gateways',
+  subsystems = 'subsystems',
+  namespaces = 'namespaces'
+}
 
 @Component({
   selector: 'cd-nvmeof-tabs',
   templateUrl: './nvmeof-tabs.component.html',
-  styleUrls: ['./nvmeof-tabs.component.scss']
+  styleUrls: ['./nvmeof-tabs.component.scss'],
+  standalone: false
 })
-export class NvmeofTabsComponent {}
+export class NvmeofTabsComponent implements OnInit {
+  selectedTab: TABS;
+  activeTab: TABS = TABS.gateways;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    const currentPath = this.router.url;
+    this.activeTab = Object.values(TABS).find((tab) => currentPath.includes(tab)) || TABS.gateways;
+  }
+
+  onSelected(tab: TABS) {
+    this.selectedTab = tab;
+    this.router.navigate([`${NVMEOF_PATH}/${tab}`]);
+  }
+
+  public get Tabs(): typeof TABS {
+    return TABS;
+  }
+}
