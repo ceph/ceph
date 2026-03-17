05c8d36149310f5144b82020f1c2c39f39a646a0
Abhishek Desai <abhishek.desai1@ibm.com>
2026-03-17 14:03:21 +0530

    mgr/dashboard : Fix application names in pools form Signed-off-by: Abhishek Desai <abhishek.desai1@ibm.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/pool/pool-form/pool-form-data.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/pool/pool-form/pool-form-data.ts
@@ -10,9 +10,9 @@ export class PoolFormData {
   applications: any;
 
   readonly APP_LABELS: Record<string, string> = {
-    cephfs: 'Filesystem',
-    rbd: 'Block',
-    rgw: 'Object'
+    cephfs: $localize`Filesystem (CephFS)`,
+    rbd: $localize`Block (RBD)`,
+    rgw: $localize`Object (RGW)`
   };
 
   constructor() {
