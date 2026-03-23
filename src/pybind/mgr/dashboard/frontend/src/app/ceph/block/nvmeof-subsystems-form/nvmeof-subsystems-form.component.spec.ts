f7dc3bf4366d005637e5cef9b57a39abcc8a9339
Sagar Gopale <sagar.gopale@ibm.com>
2026-03-23 11:38:44 +0530

    mgr/dashboard: Add Hosts via CSV Upload
    Fixes: https://tracker.ceph.com/issues/75578
    
    Signed-off-by: Sagar Gopale <sagar.gopale@ibm.com>
    (cherry picked from commit de23df56d1047055db2c3602893160513a42ca52)
    
     Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystems-form/nvmeof-subsystem-step-2/nvmeof-subsystem-step-2.component.scss
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystems-form/nvmeof-subsystems-form.component.spec.ts

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystems-form/nvmeof-subsystems-form.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystems-form/nvmeof-subsystems-form.component.spec.ts
@@ -3,9 +3,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ToastrModule } from 'ngx-toastr';
-
 import { NgbActiveModal, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { SharedModule } from '~/app/shared/shared.module';
@@ -17,6 +15,7 @@ import { NvmeofService } from '~/app/shared/api/nvmeof.service';
 import { NvmeofSubsystemsStepOneComponent } from './nvmeof-subsystem-step-1/nvmeof-subsystem-step-1.component';
 import {
   ComboBoxModule,
+  FileUploaderModule,
   GridModule,
   InputModule,
   RadioModule,
@@ -75,6 +74,7 @@ describe('NvmeofSubsystemsFormComponent', () => {
         RadioModule,
         TagModule,
         ToastrModule.forRoot(),
+        FileUploaderModule,
         ComboBoxModule
       ]
     }).compileComponents();
