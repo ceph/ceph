bd41b5e45b3edac3da39f0622204f02d42bad5f7
Aryan-coder06 <aryaniiitian06@gmail.com>
2026-03-24 22:01:08 +0530

    mgr/dashboard: refetch nvmeof listeners when inputs arrive
    Signed-off-by: Aryan-coder06 <aryaniiitian06@gmail.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystems-form/nvmeof-subsystem-step-1/nvmeof-subsystem-step-1.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystems-form/nvmeof-subsystem-step-1/nvmeof-subsystem-step-1.component.spec.ts
@@ -2,6 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SimpleChange } from '@angular/core';
 
 import { ToastrModule } from 'ngx-toastr';
 
@@ -13,6 +14,7 @@ import { NvmeofSubsystemsStepOneComponent } from './nvmeof-subsystem-step-1.comp
 import { FormHelper } from '~/testing/unit-test-helper';
 import { NvmeofService } from '~/app/shared/api/nvmeof.service';
 import { ComboBoxModule, GridModule, InputModule } from 'carbon-components-angular';
+import { ActionLabelsI18n } from '~/app/shared/constants/app.constants';
 
 import { of } from 'rxjs';
 
@@ -50,11 +52,12 @@ describe('NvmeofSubsystemsStepOneComponent', () => {
 
     nvmeofService = TestBed.inject(NvmeofService);
     spyOn(nvmeofService, 'getHostsForGroup').and.returnValue(of([]));
+    spyOn(nvmeofService, 'listListeners').and.returnValue(of([]));
+    component.group = mockGroupName;
     component.ngOnInit();
     form = component.formGroup;
     formHelper = new FormHelper(form);
     fixture.detectChanges();
-    component.group = mockGroupName;
   });
 
   it('should create', () => {
@@ -71,5 +74,71 @@ describe('NvmeofSubsystemsStepOneComponent', () => {
       formHelper.setValue('nqn', 'nqn:2001-07.com.ceph:');
       formHelper.expectError('nqn', 'nqnPattern');
     });
+  });
+
+  it('should fetch hosts when the group input becomes available after init', () => {
+    const lateService = {
+      getHostsForGroup: jest.fn(),
+      listListeners: jest.fn()
+    } as unknown as NvmeofService;
+    const hosts = [{ hostname: 'gw-a', addr: '10.0.0.1' }] as any;
+    const lateComponent = new NvmeofSubsystemsStepOneComponent(
+      new ActionLabelsI18n(),
+      new NgbActiveModal(),
+      lateService
+    );
+
+    (lateService.getHostsForGroup as jest.Mock).mockReturnValue(of(hosts));
+    (lateService.listListeners as jest.Mock).mockReturnValue(of([]));
+
+    lateComponent.ngOnInit();
+    expect(lateService.getHostsForGroup).not.toHaveBeenCalled();
+
+    lateComponent.group = mockGroupName;
+    lateComponent.ngOnChanges({
+      group: new SimpleChange(undefined, mockGroupName, false)
+    });
+
+    expect(lateService.getHostsForGroup).toHaveBeenCalledWith(mockGroupName);
+    expect(lateComponent.hosts).toEqual([{ content: 'gw-a', addr: '10.0.0.1' }]);
+  });
+
+  it('should auto-fetch listeners once listener page inputs are ready', () => {
+    const lateService = {
+      getHostsForGroup: jest.fn(),
+      listListeners: jest.fn()
+    } as unknown as NvmeofService;
+    const hosts = [
+      { hostname: 'gw-a', addr: '10.0.0.1' },
+      { hostname: 'gw-b', addr: '10.0.0.2' }
+    ] as any;
+    const existingListeners = [{ host_name: 'gw-a' }];
+    const lateComponent = new NvmeofSubsystemsStepOneComponent(
+      new ActionLabelsI18n(),
+      new NgbActiveModal(),
+      lateService
+    );
+
+    lateComponent.listenersOnly = true;
+    (lateService.getHostsForGroup as jest.Mock).mockReturnValue(of(hosts));
+    (lateService.listListeners as jest.Mock).mockReturnValue(of(existingListeners));
+
+    lateComponent.ngOnInit();
+    expect(lateService.getHostsForGroup).not.toHaveBeenCalled();
+    expect(lateService.listListeners).not.toHaveBeenCalled();
+
+    lateComponent.group = mockGroupName;
+    lateComponent.subsystemNQN = 'nqn.2001-07.com.ceph:listener-test';
+    lateComponent.ngOnChanges({
+      group: new SimpleChange(undefined, mockGroupName, false),
+      subsystemNQN: new SimpleChange(undefined, lateComponent.subsystemNQN, false)
+    });
+
+    expect(lateService.getHostsForGroup).toHaveBeenCalledWith(mockGroupName);
+    expect(lateService.listListeners).toHaveBeenCalledWith(
+      'nqn.2001-07.com.ceph:listener-test',
+      mockGroupName
+    );
+    expect(lateComponent.hosts).toEqual([{ content: 'gw-b', addr: '10.0.0.2' }]);
   });
 });
