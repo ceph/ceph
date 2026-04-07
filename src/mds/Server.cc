d81d51dbcd49bec38aa44c704589072f0a476234
Christopher Hoffman <choffman@redhat.com>
2026-04-07 16:14:48 +0000

    mds: wait until last block persisted to return
    The implementation of truncating smaller of an fscrypt
    file will require the use of fscrypt last block. This
    process is a multi-step process. There is a race condition
    that libcephfs can hit.
    
    The problem:
    1. Truncate an fscrypt file smaller
    2. Immediately read file
    3. Truncate larger
    4. Read file
    
    The expected contents of the file when step 4 is reached
    is file contents truncated to smaller size and then zero
    padded. The actual contents show the before contents of
    truncating smaller (step 1).
    
    This is due to a race condition due to the mds reporting back
    to the client early before the last block is written persistently.
    In the above example stale data will be read.
    
    Why does this only exist in userspace?
    The stale data read behavior is only present in libcephfs. This is
    to do with how cache invalidation works. The kclient stores data in
    the page cache in encrypted or decrypted form. Alternatively,
    libcephfs may rely on ObjectCacher to cache data and stores it in
    encrypted form.
    
    During the above scenario, the kclient will truncate the decrypted
    data and no read from osd is needed. Alternatively, libcephfs needs
    to invalidate the complete block and will trigger a read from the osds.
    
    Fixes: https://tracker.ceph.com/issues/74883
    Signed-off-by: Christopher Hoffman <choffman@redhat.com>

--- src/mds/Server.cc
+++ src/mds/Server.cc
@@ -5632,6 +5632,7 @@ void Server::handle_client_setattr(const MDRequestRef& mdr)
     pi.inode->time_warp_seq++;   // maybe not a timewarp, but still a serialization point.
   if (mask & CEPH_SETATTR_SIZE) {
     if (truncating_smaller) {
+      mdr->no_early_reply = true;
       pi.inode->truncate(old_size, req->head.args.setattr.size, req->get_data().cbegin());
       le->metablob.add_truncate_start(cur->ino());
     } else {
