6fbd4557718529d25d81a67a03fa14e4036cc67f
Max Kellermann <max.kellermann@ionos.com>
2026-04-20 13:31:43 +0200

    mon/PoolAvailability: un-inline methods to reduce compile times
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/mon/PoolAvailability.h
+++ src/mon/PoolAvailability.h
@@ -20,78 +20,29 @@
 #include <list>
 #include <string>
 
-#include "common/Clock.h" // for ceph_clock_now()
-#include "common/Formatter.h"
-#include "include/encoding_string.h"
+#include "include/encoding.h"
 #include "include/utime.h"
+
+namespace ceph { class Formatter; }
 
 struct PoolAvailability {
   std::string pool_name  = "";
-  utime_t started_at = ceph_clock_now();
+  utime_t started_at;
   uint64_t uptime = 0;
-  utime_t last_uptime = ceph_clock_now();
+  utime_t last_uptime;
   uint64_t downtime = 0;
-  utime_t last_downtime = ceph_clock_now();
+  utime_t last_downtime;
   uint64_t num_failures = 0;
   bool is_avail = true;
 
-  PoolAvailability() {}
+  PoolAvailability();
 
-  void dump(ceph::Formatter *f) const {
-    ceph_assert(f != NULL);
-    f->dump_stream("pool_name") << pool_name;
-    f->dump_stream("started_at") << started_at;
-    f->dump_int("uptime", uptime);
-    f->dump_stream("last_uptime") << last_uptime;
-    f->dump_int("downtime", downtime);
-    f->dump_stream("last_downtime") << last_downtime;
-    f->dump_int("num_failures", num_failures);
-    f->dump_bool("is_avail", is_avail);
-  }
+  void dump(ceph::Formatter *f) const;
 
-  void encode(ceph::buffer::list &bl) const {
-    ENCODE_START(1, 1, bl);
-    encode(pool_name, bl);
-    encode(started_at, bl);
-    encode(uptime, bl);
-    encode(last_uptime, bl);
-    encode(downtime, bl);
-    encode(last_downtime, bl);
-    encode(num_failures, bl);
-    encode(is_avail, bl);
-    ENCODE_FINISH(bl);
-  }
+  void encode(ceph::buffer::list &bl) const;
+  void decode(ceph::buffer::list::const_iterator &p);
 
-  void decode(ceph::buffer::list::const_iterator &p) {
-    DECODE_START(1, p);
-    decode(pool_name, p);
-    decode(started_at, p);
-    decode(uptime, p);
-    decode(last_uptime, p);
-    decode(downtime, p);
-    decode(last_downtime, p);
-    decode(num_failures, p);
-    decode(is_avail, p);
-    DECODE_FINISH(p);
-  }
-
-  static std::list<PoolAvailability> generate_test_instances() {
-    std::list<PoolAvailability> o;
-    o.emplace_back();
-    o.back().started_at = utime_t(123, 456);
-    o.back().last_uptime = utime_t(123, 456);
-    o.back().last_downtime = utime_t(123, 456);
-    o.emplace_back();
-    o.back().pool_name = "foo";
-    o.back().started_at = utime_t(123, 456);
-    o.back().uptime = 100;
-    o.back().last_uptime = utime_t(123, 456);
-    o.back().downtime = 15;
-    o.back().last_downtime = utime_t(123, 456);
-    o.back().num_failures = 2;
-    o.back().is_avail = true;
-    return o;
-  }  
+  static std::list<PoolAvailability> generate_test_instances();
 };
 WRITE_CLASS_ENCODER(PoolAvailability)
 
