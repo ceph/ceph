0f9bb1d81d462559369d6a8fa4b13eab77b676cf
Max Kellermann <max.kellermann@ionos.com>
2026-04-20 14:08:46 +0200

    mon/ProgressEvent: un-inline methods to reduce compile times
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/mon/ProgressEvent.h
+++ src/mon/ProgressEvent.h
@@ -19,47 +19,18 @@
 #include <list>
 #include <string>
 
-#include "include/encoding_string.h"
-#include "common/Formatter.h"
+#include "include/encoding.h"
+
+namespace ceph { class Formatter; }
 
 struct ProgressEvent {
   std::string message;                  ///< event description
   float progress = 0.0f;                  ///< [0..1]
   bool add_to_ceph_s = false;
-  void encode(ceph::buffer::list& bl) const {
-    ENCODE_START(2, 1, bl);
-    encode(message, bl);
-    encode(progress, bl);
-    encode(add_to_ceph_s, bl);
-    ENCODE_FINISH(bl);
-  }
-  void decode(ceph::buffer::list::const_iterator& p) {
-    DECODE_START(2, p);
-    decode(message, p);
-    decode(progress, p);
-    if (struct_v >= 2){
-	decode(add_to_ceph_s, p);
-    } else {
-      if (!message.empty()) {
-	add_to_ceph_s = true;
-      }
-    }
-    DECODE_FINISH(p);
-  }
-  void dump(ceph::Formatter *f) const {
-    f->dump_string("message", message);
-    f->dump_float("progress", progress);
-    f->dump_bool("add_to_ceph_s", add_to_ceph_s);
-  }
-  static std::list<ProgressEvent> generate_test_instances() {
-    std::list<ProgressEvent> o;
-    o.emplace_back();
-    o.emplace_back();
-    o.back().message = "test message";
-    o.back().progress = 0.5;
-    o.back().add_to_ceph_s = true;
-    return o;
-  }
+  void encode(ceph::buffer::list& bl) const;
+  void decode(ceph::buffer::list::const_iterator& p);
+  void dump(ceph::Formatter *f) const;
+  static std::list<ProgressEvent> generate_test_instances();
 };
 WRITE_CLASS_ENCODER(ProgressEvent)
 
