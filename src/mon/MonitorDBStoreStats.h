d8818e787ac1244bcf7cbc3c3e8edd6f7345a672
Max Kellermann <max.kellermann@ionos.com>
2026-04-20 13:25:42 +0200

    mon/MonitorDBStoreStats: un-inline methods to reduce compile times
    Signed-off-by: Max Kellermann <max.kellermann@ionos.com>

--- src/mon/MonitorDBStoreStats.h
+++ src/mon/MonitorDBStoreStats.h
@@ -21,7 +21,8 @@
 
 #include "include/encoding.h"
 #include "include/utime.h"
-#include "common/Formatter.h"
+
+namespace ceph { class Formatter; }
 
 /**
  * monitor db store stats
@@ -40,46 +41,12 @@ struct MonitorDBStoreStats {
     bytes_misc(0)
   {}
 
-  void dump(ceph::Formatter *f) const {
-    ceph_assert(f != NULL);
-    f->dump_int("bytes_total", bytes_total);
-    f->dump_int("bytes_sst", bytes_sst);
-    f->dump_int("bytes_log", bytes_log);
-    f->dump_int("bytes_misc", bytes_misc);
-    f->dump_stream("last_updated") << last_update;
-  }
+  void dump(ceph::Formatter *f) const;
 
-  void encode(ceph::buffer::list &bl) const {
-    ENCODE_START(1, 1, bl);
-    encode(bytes_total, bl);
-    encode(bytes_sst, bl);
-    encode(bytes_log, bl);
-    encode(bytes_misc, bl);
-    encode(last_update, bl);
-    ENCODE_FINISH(bl);
-  }
+  void encode(ceph::buffer::list &bl) const;
+  void decode(ceph::buffer::list::const_iterator &p);
 
-  void decode(ceph::buffer::list::const_iterator &p) {
-    DECODE_START(1, p);
-    decode(bytes_total, p);
-    decode(bytes_sst, p);
-    decode(bytes_log, p);
-    decode(bytes_misc, p);
-    decode(last_update, p);
-    DECODE_FINISH(p);
-  }
-
-  static std::list<MonitorDBStoreStats> generate_test_instances() {
-    std::list<MonitorDBStoreStats> ls;
-    ls.emplace_back();
-    ls.emplace_back();
-    ls.back().bytes_total = 1024*1024;
-    ls.back().bytes_sst = 512*1024;
-    ls.back().bytes_log = 256*1024;
-    ls.back().bytes_misc = 256*1024;
-    ls.back().last_update = utime_t();
-    return ls;
-  }
+  static std::list<MonitorDBStoreStats> generate_test_instances();
 };
 WRITE_CLASS_ENCODER(MonitorDBStoreStats)
 
