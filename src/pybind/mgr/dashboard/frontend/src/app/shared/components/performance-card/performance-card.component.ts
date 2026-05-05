d4fa4fc281b373bea5e680a3142dee0abb94d254
Afreen Misbah <afreen@ibm.com>
2026-05-06 02:35:11 +0530

    mgr/dashboard: Updates to empty state component
    - added state for no storage in empty state component
    - extended the icon component to take into account the scenario of button with icon
    - fix unit tests
    
    Signed-off-by: Afreen Misbah <afreen@ibm.com>
    (cherry picked from commit 2eb19f35e33fbc53c80398dcb0be882e3bd8ff0c)

--- src/pybind/mgr/dashboard/frontend/src/app/shared/components/performance-card/performance-card.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/components/performance-card/performance-card.component.ts
@@ -8,7 +8,7 @@ import {
   computed,
   Input
 } from '@angular/core';
-import { Icons, IconSize } from '~/app/shared/enum/icons.enum';
+import { EMPTY_STATE_IMAGE, Icons, IconSize } from '~/app/shared/enum/icons.enum';
 import { PrometheusService } from '~/app/shared/api/prometheus.service';
 import {
   METRIC_UNIT_MAP,
@@ -18,7 +18,7 @@ import {
 } from '~/app/shared/models/performance-data';
 import { PerformanceCardService } from '../../api/performance-card.service';
 import { DropdownModule, GridModule, LayoutModule, ListItem } from 'carbon-components-angular';
-import { of, Subject, Subscription } from 'rxjs';
+import { Subject, Subscription } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 import { ProductiveCardComponent } from '../productive-card/productive-card.component';
 import { CommonModule } from '@angular/common';
@@ -44,7 +44,8 @@ import { EmptyStateComponent } from '../empty-state/empty-state.component';
   encapsulation: ViewEncapsulation.None
 })
 export class PerformanceCardComponent implements OnInit, OnDestroy {
-  @Input() emptyStateText: string | null = '';
+  @Input() prometheusEmptyState: boolean = false;
+  @Input() storageEmptyState: boolean = false;
 
   chartDataSignal = signal<PerformanceData | null>(null);
   chartDataLengthSignal = computed(() => {
@@ -55,6 +56,7 @@ export class PerformanceCardComponent implements OnInit, OnDestroy {
   metricUnitMap = METRIC_UNIT_MAP;
   icons = Icons;
   iconSize = IconSize;
+  emptyState = EMPTY_STATE_IMAGE;
 
   private destroy$ = new Subject<void>();
 
@@ -95,8 +97,13 @@ export class PerformanceCardComponent implements OnInit, OnDestroy {
 
     this.chartSub?.unsubscribe();
 
-    this.chartSub = this.prometheusService
-      .withPrometheusEnabled(this.performanceCardService.getChartData(time))
+    if (this.storageEmptyState || this.prometheusEmptyState) {
+      this.chartDataSignal.set(null);
+      return;
+    }
+
+    this.chartSub = this.performanceCardService
+      .getChartData(time)
       .pipe(takeUntil(this.destroy$))
       .subscribe((data) => {
         this.chartDataSignal.set(data);
