be0835a76b8332a94675fad9cee8c6f1a14f6abd
Eric Zhang <emzhang@umich.edu>
2026-05-07 13:15:55 -0700

    qa/standalone/crush: Add stretch crush rule generation test for osd pool create Test stretch crush rule generation tests for osd pool create for both EC and replica
    Fixes: https://tracker.ceph.com/issues/76325
    
    Signed-off-by: Eric Zhang <emzhang@ibm.com>

--- qa/standalone/crush/crush-stretch.sh
+++ qa/standalone/crush/crush-stretch.sh
@@ -35,8 +35,7 @@ function TEST_stretch_replicated() {
     ceph osd crush add-bucket dc2 datacenter
     ceph osd crush move dc1 root=default
     ceph osd crush move dc2 root=default
-    ceph osd tree
-    ceph osd crush rule create-stretch-replicated
+
     ceph osd crush rule create-stretch-replicated 2>&1 | grep "Error EINVAL: zone dc1 has only 0 items of type host" || return 1
 
     ceph osd crush add-bucket host1 host
@@ -69,13 +68,13 @@ function TEST_stretch_replicated() {
 
     ceph osd crush rule create-stretch-replicated || return 1
 
-    ceph osd crush rule dump stretch_rule | jq '.steps[1].op' | grep "choose_firstn" || return 1
-    ceph osd crush rule dump stretch_rule | jq '.steps[1].num' | grep "0" || return 1
-    ceph osd crush rule dump stretch_rule | jq '.steps[1].type' | grep "datacenter" || return 1
+    ceph osd crush rule dump stretch_replica_rule | jq '.steps[1].op' | grep "choose_firstn" || return 1
+    ceph osd crush rule dump stretch_replica_rule | jq '.steps[1].num' | grep "0" || return 1
+    ceph osd crush rule dump stretch_replica_rule | jq '.steps[1].type' | grep "datacenter" || return 1
 
-    ceph osd crush rule dump stretch_rule | jq '.steps[2].op' | grep "chooseleaf_firstn" || return 1
-    ceph osd crush rule dump stretch_rule | jq '.steps[2].num' | grep "2" || return 1
-    ceph osd crush rule dump stretch_rule | jq '.steps[2].type' | grep "host" || return 1
+    ceph osd crush rule dump stretch_replica_rule | jq '.steps[2].op' | grep "chooseleaf_firstn" || return 1
+    ceph osd crush rule dump stretch_replica_rule | jq '.steps[2].num' | grep "2" || return 1
+    ceph osd crush rule dump stretch_replica_rule | jq '.steps[2].type' | grep "host" || return 1
 }
 
 function TEST_stretch_ec() {
@@ -149,6 +148,133 @@ function TEST_stretch_ec() {
     ceph osd crush rule dump stretch_erasurecode_rule | jq '.steps[4].op' | grep "chooseleaf_indep" || return 1
     ceph osd crush rule dump stretch_erasurecode_rule | jq '.steps[4].num' | grep "3" || return 1
     ceph osd crush rule dump stretch_erasurecode_rule | jq '.steps[4].type' | grep "host" || return 1
+}
+
+function TEST_pool_create_stretch_ec() {
+    local dir=$1
+    run_mon $dir a || return 1
+    run_osd $dir 0 || return 1
+    run_osd $dir 1 || return 1
+    run_osd $dir 2 || return 1
+    run_osd $dir 3 || return 1
+    run_osd $dir 4 || return 1
+    run_osd $dir 5 || return 1
+
+    ceph osd crush add-bucket dc1 datacenter
+    ceph osd crush add-bucket dc2 datacenter
+    ceph osd crush move dc1 root=default
+    ceph osd crush move dc2 root=default
+
+    ceph osd erasure-code-profile set stretch_ec_profile plugin=jerasure num_zones=2 k=2 m=1 crush-num-osd-failure-domains=2
+    ceph osd pool create data0 erasure stretch_ec_profile 2>&1 | grep "Error EINVAL: zone dc1 has only 0 items of type host" || return 1
+
+    ceph osd crush add-bucket host1 host
+    ceph osd crush move host1 datacenter=dc1
+    ceph osd pool create data0 erasure stretch_ec_profile 2>&1 | grep "Error EINVAL: zone dc1 has only 1 items of type host" || return 1
+
+    ceph osd crush add-bucket host2 host
+    ceph osd crush move host2 datacenter=dc1
+    ceph osd pool create data0 erasure stretch_ec_profile 2>&1 | grep "Error EINVAL: zone dc1 has only 2 items of type host" || return 1
+
+    ceph osd crush add-bucket host3 host
+    ceph osd crush move host3 datacenter=dc1
+    ceph osd pool create data0 erasure stretch_ec_profile
+    ceph osd pool create data0 erasure stretch_ec_profile 2>&1 | grep "Error EINVAL: host host1 has no OSDs" || return 1
+
+    ceph osd crush set osd.0 1.0 host=host1
+    ceph osd pool create data0 erasure stretch_ec_profile 2>&1 | grep "Error EINVAL: host host2 has no OSDs" || return 1
+
+    ceph osd crush set osd.1 1.0 host=host2
+    ceph osd pool create data0 erasure stretch_ec_profile 2>&1 | grep "Error EINVAL: host host3 has no OSDs" || return 1
+
+    ceph osd crush set osd.2 1.0 host=host3
+    ceph osd pool create data0 erasure stretch_ec_profile 2>&1 | grep "Error EINVAL: zone dc2 has only 0 items of type host" || return 1
+
+    ceph osd crush add-bucket host4 host
+    ceph osd crush move host4 datacenter=dc2
+    ceph osd pool create data0 erasure stretch_ec_profile 2>&1 | grep "Error EINVAL: zone dc2 has only 1 items of type host" || return 1
+
+    ceph osd crush add-bucket host5 host
+    ceph osd crush move host5 datacenter=dc2
+    ceph osd pool create data0 erasure stretch_ec_profile 2>&1 | grep "Error EINVAL: zone dc2 has only 2 items of type host" || return 1
+
+    ceph osd crush add-bucket host6 host
+    ceph osd crush move host6 datacenter=dc2
+    ceph osd pool create data0 erasure stretch_ec_profile 2>&1 | grep "Error EINVAL: host host4 has no OSDs" || return 1
+
+    ceph osd crush set osd.3 1.0 host=host4
+    ceph osd pool create data0 erasure stretch_ec_profile 2>&1 | grep "Error EINVAL: host host5 has no OSDs" || return 1
+
+    ceph osd crush set osd.4 1.0 host=host5
+    ceph osd pool create data0 erasure stretch_ec_profile 2>&1 | grep "Error EINVAL: host host6 has no OSDs" || return 1
+
+    ceph osd crush set osd.5 1.0 host=host6
+
+    ceph osd pool create data0 erasure stretch_ec_profile || return 1
+
+    ceph osd crush rule dump data0 | jq '.steps[3].op' | grep "choose_firstn" || return 1
+    ceph osd crush rule dump data0 | jq '.steps[3].num' | grep "0" || return 1
+    ceph osd crush rule dump data0 | jq '.steps[3].type' | grep "datacenter" || return 1
+
+    ceph osd crush rule dump data0 | jq '.steps[4].op' | grep "chooseleaf_indep" || return 1
+    ceph osd crush rule dump data0 | jq '.steps[4].num' | grep "3" || return 1
+    ceph osd crush rule dump data0 | jq '.steps[4].type' | grep "host" || return 1
+}
+
+function TEST_pool_create_stretch_replica() {
+    local dir=$1
+    run_mon $dir a || return 1
+    run_osd $dir 0 || return 1
+    run_osd $dir 1 || return 1
+    run_osd $dir 2 || return 1
+    run_osd $dir 3 || return 1
+    run_osd $dir 4 || return 1
+    run_osd $dir 5 || return 1
+
+    ceph osd crush add-bucket dc1 datacenter
+    ceph osd crush add-bucket dc2 datacenter
+    ceph osd crush move dc1 root=default
+    ceph osd crush move dc2 root=default
+
+    ceph osd pool create data0 --zones=2 2>&1 | grep "Error EINVAL: zone dc1 has only 0 items of type host" || return 1
+
+    ceph osd crush add-bucket host1 host
+    ceph osd crush move host1 datacenter=dc1
+    ceph osd pool create data0 --zones=2 2>&1 | grep "Error EINVAL: zone dc1 has only 1 items of type host" || return 1
+
+    ceph osd crush add-bucket host2 host
+    ceph osd crush move host2 datacenter=dc1
+    ceph osd pool create data0 --zones=2 2>&1 | grep "Error EINVAL: host host1 has no OSDs" || return 1
+
+    ceph osd crush set osd.0 1.0 host=host1
+    ceph osd pool create data0 --zones=2 2>&1 | grep "Error EINVAL: host host2 has no OSDs" || return 1
+
+    ceph osd crush set osd.1 1.0 host=host2
+    ceph osd pool create data0 --zones=2 2>&1 | grep "Error EINVAL: zone dc2 has only 0 items of type host" || return 1
+
+    ceph osd crush add-bucket host3 host
+    ceph osd crush move host3 datacenter=dc2
+    ceph osd pool create data0 --zones=2 2>&1 | grep "Error EINVAL: zone dc2 has only 1 items of type host" || return 1
+
+    ceph osd crush add-bucket host4 host
+    ceph osd crush move host4 datacenter=dc2
+    ceph osd pool create data0 --zones=2 2>&1 | grep "Error EINVAL: host host3 has no OSDs" || return 1
+
+
+    ceph osd crush set osd.2 1.0 host=host3
+    ceph osd pool create data0 --zones=2 2>&1 | grep "Error EINVAL: host host4 has no OSDs" || return 1
+
+    ceph osd crush set osd.3 1.0 host=host4
+
+    ceph osd pool create data0 --zones=2 || return 1
+
+    ceph osd crush rule dump data0 | jq '.steps[1].op' | grep "choose_firstn" || return 1
+    ceph osd crush rule dump data0 | jq '.steps[1].num' | grep "0" || return 1
+    ceph osd crush rule dump data0 | jq '.steps[1].type' | grep "datacenter" || return 1
+
+    ceph osd crush rule dump data0 | jq '.steps[2].op' | grep "chooseleaf_firstn" || return 1
+    ceph osd crush rule dump data0 | jq '.steps[2].num' | grep "2" || return 1
+    ceph osd crush rule dump data0 | jq '.steps[2].type' | grep "host" || return 1
 }
 
 main crush-stretch "$@"
