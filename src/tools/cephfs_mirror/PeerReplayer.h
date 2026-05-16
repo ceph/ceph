9986b41cdee36d36f64daa5838379a9499d611b2
Karthik U S <karthik.u.s1@ibm.com>
2026-05-16 06:20:03 +0530

    tools/cephfs_mirror: update checkpoint status during snapshot sync
    When the sync completes or fails for a checkpointed snapshot, transition
    the status of that checkpoint from CREATED/FAILED to COMPLETE/FAILED in
    the do_sync_snaps() along with the timestamp of the event.
    
    Fixes: https://tracker.ceph.com/issues/73454
    Signed-off-by: Karthik U S <karthik.u.s1@ibm.com>
    (cherry picked from commit 37163436cc87a2be691202bb565c7b5007df026f)

--- src/tools/cephfs_mirror/PeerReplayer.h
+++ src/tools/cephfs_mirror/PeerReplayer.h
@@ -10,6 +10,7 @@
 #include "ServiceDaemon.h"
 #include "Types.h"
 #include "json_spirit/json_spirit.h"
+#include "Checkpoint.h"
 
 #include <set>
 #include <stack>
@@ -758,6 +759,11 @@ private:
 
   int build_snap_map(const std::string &dir_root, std::map<uint64_t, std::string> *snap_map,
                      bool is_remote=false);
+
+  void checkpoint_sync_complete(const std::string &dir_root, uint64_t synced_snap_id,
+                                const std::string &snap_name);
+  void checkpoint_sync_failed(const std::string &dir_root, uint64_t snap_id,
+                              const std::string &snap_name, int err);
 
   int propagate_snap_deletes(const std::string &dir_root, const std::set<std::string> &snaps);
   int propagate_snap_renames(const std::string &dir_root,
