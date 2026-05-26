f8b110c13e9596059b5eb504a7060923eb89bbbc
Adam Kupczyk <akupczyk@ibm.com>
2026-05-26 10:38:17 +0000

    os/bluestore: Collection::split_cache no longer purges cache
    There are 2 cases when Collection::split_cache is used:
    1) Merge Collections.
       It this case we get more elements in dest collection,
       but we do not want to trim by force as it might cause stall.
    2) Split Collection
       Source cache is getting thinner, and dest cache is growing.
       Similarly, trimming dest will cause stall.
    
    Decision:
    It is better to not trim forcibly and relay on gradual trimming
    from MempoolThread or client IO.
    
    Fixes: https://tracker.ceph.com/issues/77269
    
    Signed-off-by: Adam Kupczyk <akupczyk@ibm.com>
    (cherry picked from commit d2f2d866991b356bc4f8c595982abd20e591d7a4)

--- src/os/bluestore/BlueStore.cc
+++ src/os/bluestore/BlueStore.cc
@@ -5455,7 +5455,9 @@ void BlueStore::Collection::split_cache(
       }
     }
   }
-  dest->cache->_trim();
+  // The cache has now more elements.
+  // Trimming right away will cause stalls.
+  // It will get adjusted in MempoolThread
 }
 // =======================================================
 
