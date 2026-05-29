b8e32c1cd03124b09aaeac477c1baa8cbdbc38e3
Bill Scales <bill_scales@uk.ibm.com>
2026-05-29 14:45:30 +0100

    osd: Pool migration must release reservations when preempted
    Pool migration wasn't releasing reservations when the reserve
    was premepted by a repliuca shard. This lead to asserts when
    it tried to resume the migration and reacquire the reservations.
    
    Signed-off-by: Bill Scales <bill_scales@uk.ibm.com>

--- src/osd/PeeringState.cc
+++ src/osd/PeeringState.cc
@@ -6592,6 +6592,7 @@ PeeringState::WaitRemotePoolMigrationReserved::react(const RemotePoolMigrationRe
     retry();
   } else {
     // Tell source PG, it will retry
+    migration_release_reservations();
     pl->on_pool_migration_target_suspended(true);
   }
   return transit<Clean>();
@@ -6621,6 +6622,7 @@ PeeringState::WaitRemotePoolMigrationReserved::react(const RemotePoolMigrationRe
     retry();
   } else {
     // Tell source PG, it will retry
+    migration_release_reservations();
     pl->on_pool_migration_target_suspended(true);
   }
   return transit<Clean>();
