d5e254c6313d3d7bf15b7845fd546d47a29f5ae9
Leonid Chernin <leonidc@il.ibm.com>
2026-06-03 21:51:39 +0300

    nvmeofgw: arm beacon timeouts for GWs in Created state
    Signed-off-by: Leonid Chernin <leonidc@il.ibm.com>

--- src/mon/NVMeofGwMon.h
+++ src/mon/NVMeofGwMon.h
@@ -95,8 +95,7 @@ public:
 private:
   void synchronize_last_beacon();
   void process_gw_down(const NvmeGwId &gw_id,
-     const NvmeGroupKey& group_key, bool &propose_pending,
-     gw_availability_t avail);
+     const NvmeGroupKey& group_key, bool &propose_pending);
   bool get_gw_by_addr(const  entity_addr_t &sub_addr,
        NvmeGwId &gw_id, NvmeGroupKey& group_key);
   epoch_t get_ack_map_epoch(bool gw_created, const NvmeGroupKey& group_key);
