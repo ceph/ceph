75cadff380342ca5b74f1587aa923cbed2a0abe2
AnuragRaut08 <anuragtraut2003@gmail.com>
2026-06-04 12:24:56 +0530

    mgr/telemetry: add dashboard protocol detection metrics
    Fixes: https://tracker.ceph.com/issues/76930
    
    Signed-off-by: AnuragRaut08 <anuragtraut2003@gmail.com>

--- src/pybind/mgr/telemetry/module.py
+++ src/pybind/mgr/telemetry/module.py
@@ -1371,6 +1371,32 @@ class Module(MgrModule):
 
         # NOTE: We do not include the 'device' channel in this report; it is
         # sent to a different endpoint.
+        # -- Dashboard UI metrics --
+        try:
+            import json as _json
+
+            def _read_key(key):
+                r, outb, outs = self.mon_command({
+                    'prefix': 'config-key get',
+                    'key': key
+                })
+                return outb.strip() if r == 0 and outb else None
+
+            report['dashboard'] = {
+                'protocols_enabled': _json.loads(
+                    _read_key(
+                        'mgr/dashboard/ui_metrics/protocols_enabled'
+                    ) or '{}'
+                ),
+            }
+
+        except Exception as e:  # pylint: disable=broad-except
+            self.log.warning(
+                'ui_metrics: failed to attach dashboard section: %s',
+                e
+            )
+
+        # -- End Dashboard UI metrics --
 
         return report
 
