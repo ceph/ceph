5123ffd5e7115648d28f0339ecad85779eefd359
Patrick Donnelly <pdonnell@ibm.com>
2026-06-08 10:33:01 -0400

    mon/MgrStatMonitor: reuse last health check map for each proposal
    Otherwise the ephemeral health checks is cleared for any update
    not triggered by a mgr update.
    
    Change-Id: 42caf65a-e287-435b-bab2-d353a600fbb5
    Fixes: 86c5043d379052dd5f2cb8344fcd95bf7f4f3d50 ("mon: refactor health check map through PaxosMap")
    Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>

--- src/mon/MgrStatMonitor.h
+++ src/mon/MgrStatMonitor.h
@@ -22,6 +22,7 @@ struct Subscription;
 
   // pending commit
   PGMapDigest pending_digest;
+  health_check_map_t pending_health_checks;
   std::map<std::string,ProgressEvent> pending_progress_events;
   ceph::buffer::list pending_service_map_bl;
   std::map<uint64_t, PoolAvailability> pending_pool_availability;
