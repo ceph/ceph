bf2cccb5bfb28aef432a6db32d22617a0cf39b31
Dongdong Tao <tdd21151186@gmail.com>
2026-06-10 15:13:49 +0900

    qa/standalone: fix license header in radostrace.sh to LGPL-2.1
    The test script carried GPL-style boilerplate referencing the
    "GNU Library Public License". Replace it with the standard Ceph
    LGPL-2.1 header so the file can be integrated under the project
    license.
    
    Signed-off-by: Dongdong Tao <dongdong.tao@canonical.com>

--- qa/standalone/ceph-bpf/radostrace.sh
+++ qa/standalone/ceph-bpf/radostrace.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 #
-# Author: Dongdong Tao <tdd21151186@gmail.com>
+# Ceph - scalable distributed file system
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Library Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
-# any later version.
+# Copyright (C) 2026 Dongdong Tao <dongdong.tao@canonical.com>
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Library Public License for more details.
+# This is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 2.1, as published by the Free Software
+# Foundation.  See file COPYING.
 #
 
 source $CEPH_ROOT/qa/standalone/ceph-helpers.sh
