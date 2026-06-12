3fe720dc5e949f174a1f11ee5fc4df61e44a4bc1
Bill Scales <bill_scales@uk.ibm.com>
2026-06-12 13:52:48 +0100

    FIXME: Temp debug for failed delete unmanaged snap
    Add debug for RadosModel.h 1985: ceph_assert(!context->io_ctx.selfmanaged_snap_remove(snap))
    
    The delete managed snap command to the mon is being retried (allowed) but is failing
    with -95 ENOTSUP because the selfmanaged snap flag on the pool isn't set. Add extra
    debug to help find out why.
    
    Signed-off-by: Bill Scales <bill_scales@uk.ibm.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -15624,6 +15624,10 @@ bool OSDMonitor::prepare_pool_op(MonOpRequestRef op)
       return false;
     }
   }
+  dout(0) << "BILL: pool " << m->pool << " flags: " << pool->get_flags_string() << dendl;
+  if (pending_inc.new_pools.count(m->pool)) {
+    dout(0) << "BILL: pending_inc " << m->pool << " flags: " << pending_inc.new_pools[m->pool].get_flags_string() << dendl;
+  }
 
   switch (m->op) {
     case POOL_OP_CREATE_SNAP:
@@ -15651,6 +15655,7 @@ bool OSDMonitor::prepare_pool_op(MonOpRequestRef op)
       // we won't allow removal of an unmanaged snapshot from a pool
       // not in unmanaged snaps mode.
       if (!pool->is_unmanaged_snaps_mode()) {
+        dout(0) << "BILL: failing DELETE_UNMANAGED_SNAP with ENOTSUP" << dendl;
         _pool_op_reply(op, -ENOTSUP, osdmap.get_epoch());
         return false;
       }
