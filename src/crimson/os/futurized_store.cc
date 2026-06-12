a0a9ee73232b21b26a767c6da4d9b00db1c05682
Radoslaw Zarzynski <rzarzyns@redhat.com>
2026-06-12 19:48:05 +0000

    crimson/os: use RelayStore when \!seastore_require_partition_count_match_reactor_count
    Signed-off-by: Radoslaw Zarzynski <rzarzyns@redhat.com>

--- src/crimson/os/futurized_store.cc
+++ src/crimson/os/futurized_store.cc
@@ -9,6 +9,8 @@
 #include "relaystore/relay_store.h"
 #include "seastore/seastore.h"
 
+SET_SUBSYS(osd);
+
 namespace crimson::os {
 
 std::unique_ptr<FuturizedStore>
@@ -16,11 +18,21 @@ FuturizedStore::create(const std::string& type,
                        const std::string& data,
                        const ConfigValues& values)
 {
+  LOG_PREFIX(FuturizedStore::create);
+  const bool same_no_shards =
+    crimson::common::get_conf<bool>("seastore_require_partition_count_match_reactor_count");
+  DEBUG("creating object store: type={}, same_no_shards={}",
+        type, same_no_shards);
   if (type == "cyanstore") {
     using crimson::os::CyanStore;
-    return std::make_unique<CyanStore>(data);
+    if (same_no_shards) {
+      return std::make_unique<CyanStore>(data);
+    } else {
+      return std::make_unique<_RelayStore>(
+        std::make_unique<CyanStore>(data));
+    }
   } else if (type == "seastore") {
-    if(crimson::common::get_conf<bool>("seastore_require_partition_count_match_reactor_count")) {
+    if (same_no_shards) {
       return crimson::os::seastore::make_seastore(data);
     } else {
       using crimson::os::_RelayStore;
@@ -44,7 +56,19 @@ seastar::future<> with_store_do_transaction(
   boost::intrusive_ptr<FuturizedCollection> ch, // TODO: move back to `FuturizedStore::Shard::CollectionRef ch,`
   ceph::os::Transaction&& txn)
 {
-  return crimson::os::_RelayStore::Shard::with_store_do_transaction(
-    static_cast<crimson::os::_RelayStore::Shard&>(shard), ch, std::move(txn));
+  if (crimson::common::get_conf<bool>("seastore_require_partition_count_match_reactor_count")) {
+    std::unique_ptr<Context> on_commit(
+      ceph::os::Transaction::collect_all_contexts(txn));
+    return shard.do_transaction_no_callbacks(
+      std::move(ch), std::move(txn)
+    ).then([on_commit=std::move(on_commit)]() mutable {
+      auto c = on_commit.release();
+      if (c) c->complete(0);
+      return seastar::now();
+    });
+  } else {
+    return crimson::os::_RelayStore::Shard::with_store_do_transaction(
+      static_cast<crimson::os::_RelayStore::Shard&>(shard), ch, std::move(txn));
+  }
 }
 }
