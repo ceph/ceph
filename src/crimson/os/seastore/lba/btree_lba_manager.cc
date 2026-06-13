9a169dbba04237d1cd3995a95cfc8b92da141bd1
Xuehan Xu <xuxuehan@qianxin.com>
2026-06-13 18:25:51 +0800

    crimson/os/seastore/lba: don't update the lba mapping if its child is an initial pending one.
    Because initial pending extents' paddrs are considered newer.
    
    Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

--- src/crimson/os/seastore/lba/btree_lba_manager.cc
+++ src/crimson/os/seastore/lba/btree_lba_manager.cc
@@ -1138,6 +1138,13 @@ void BtreeLBAManager::update_paddr_sync(
   auto btree = get_btree_sync<LBABtree>(c);
   auto iter = btree.lower_bound_sync(c, laddr);
   assert(iter.get_leaf_node()->is_pending());
+  auto child = iter.get_leaf_node()->get_child_sync<LogicalChildNode>(
+    c.trans, c.cache, iter.get_leaf_pos(), iter.get_key());
+  ceph_assert(child);
+  if (child->is_initial_pending()) {
+    return;
+  }
+  ceph_assert(child->is_exist_clean());
   auto cursor = iter.get_cursor(c);
   assert(cursor->get_laddr() == laddr);
   btree.update(
