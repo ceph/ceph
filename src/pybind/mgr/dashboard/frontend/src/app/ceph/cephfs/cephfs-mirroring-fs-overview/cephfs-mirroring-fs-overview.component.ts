4b23888e5ae3b413a2b789a2fae41cc78a41b539
Pedro Gonzalez Gomez <pegonzal@ibm.com>
2026-06-15 12:01:10 +0200

    mgr/dashboard: add cephfs mirroring routes and tabs for mirrored filesystem
    Fixes: https://tracker.ceph.com/issues/77404
    Signed-off-by: Pedro Gonzalez Gomez <pegonzal@ibm.com>

---
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cephfs/cephfs-mirroring-fs-overview/cephfs-mirroring-fs-overview.component.ts
@@ -0,0 +1,11 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+
+
+@Component({
+  selector: 'cd-cephfs-mirroring-fs-overview',
+  templateUrl: './cephfs-mirroring-fs-overview.component.html',
+  styleUrls: ['./cephfs-mirroring-fs-overview.component.scss'],
+  standalone: false,
+  encapsulation: ViewEncapsulation.None
+})
+export class CephfsMirroringFsOverviewComponent {}
