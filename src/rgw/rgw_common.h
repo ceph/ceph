54647d92d90837ac0626812b9cbacc33231ed236
kabicin <37311900+kabicin@users.noreply.github.com>
2026-06-15 23:36:12 -0400

    rgw: expose multi delete objects in Lua
    Signed-off-by: Kirby Chin <Kirby.Chin@ibm.com>

--- src/rgw/rgw_common.h
+++ src/rgw/rgw_common.h
@@ -1338,6 +1338,7 @@ struct req_state : DoutPrefixProvider {
    * overwritten */
   std::unique_ptr<rgw::sal::Bucket> bucket;
   std::unique_ptr<rgw::sal::Object> object;
+  std::vector<std::unique_ptr<rgw::sal::Object>> objects;
   std::string src_tenant_name;
   std::string src_bucket_name;
   std::unique_ptr<rgw::sal::Object> src_object;
