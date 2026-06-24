029724d7ee0925922e389b0e0e24bd4bac7db417
Oguzhan Ozmen <oozmen@bloomberg.net>
2026-06-24 01:48:05 +0000

    rgw/beast: (non-functional) refactor Connection and ConnectionList to header
    Just like rgw_asio_frontend_timer.h, factor out the Connection struct and
    ConnectionList class from the anonymous namespace in rgw_asio_frontend.cc
    to a new header rgw_asio_frontend_connection.h (under namespace rgw::asio).
    
    This is especially needed to get better coverage thru unittesting.
    
    This is non-functional: no logic changes, the types, methods, and behavior
    are identical as before. The frontend .cc uses type aliases to maintain the
    same unqualified names.
    
    Signed-off-by: Oguzhan Ozmen <oozmen@bloomberg.net>
    (cherry picked from commit 222a39366d27aaadf2c96a33b8a29275f233afab)
    Fixes: https://tracker.ceph.com/issues/78306
    Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

--- src/rgw/rgw_asio_frontend.cc
+++ src/rgw/rgw_asio_frontend.cc
@@ -42,6 +42,7 @@
 
 #include "rgw_zone.h"
 
+#include "rgw_asio_frontend_connection.h"
 #include "rgw_asio_frontend_timer.h"
 #include "rgw_dmclock_async_scheduler.h"
 
@@ -55,13 +56,11 @@ namespace http = boost::beast::http;
 namespace ssl = boost::asio::ssl;
 #endif
 
-struct Connection;
-
 using timeout_timer = rgw::basic_timeout_timer<ceph::coarse_mono_clock,
-      boost::asio::any_io_executor, Connection>;
+      boost::asio::any_io_executor, rgw::asio::Connection>;
 
-static constexpr size_t parse_buffer_size = 65536;
-using parse_buffer = boost::beast::flat_static_buffer<parse_buffer_size>;
+static constexpr size_t parse_buffer_size = rgw::asio::parse_buffer_size;
+using parse_buffer = rgw::asio::parse_buffer;
 
 // use mmap/mprotect to allocate 512k coroutine stacks
 auto make_stack_allocator() {
@@ -390,56 +389,8 @@ void handle_connection(boost::asio::io_context& context,
   }
 }
 
-// timeout support requires that connections are reference-counted, because the
-// timeout_handler can outlive the coroutine
-struct Connection : boost::intrusive::list_base_hook<>,
-                    boost::intrusive_ref_counter<Connection>
-{
-  tcp::socket socket;
-  parse_buffer buffer;
-
-  explicit Connection(tcp::socket&& socket) noexcept
-      : socket(std::move(socket)) {}
-
-  void close(boost::system::error_code& ec) {
-    socket.close(ec);
-  }
-
-  tcp::socket& get_socket() { return socket; }
-};
-
-class ConnectionList {
-  using List = boost::intrusive::list<Connection>;
-  List connections;
-  std::mutex mutex;
-
-  void remove(Connection& c) {
-    std::lock_guard lock{mutex};
-    if (c.is_linked()) {
-      connections.erase(List::s_iterator_to(c));
-    }
-  }
- public:
-  class Guard {
-    ConnectionList *list;
-    Connection *conn;
-   public:
-    Guard(ConnectionList *list, Connection *conn) : list(list), conn(conn) {}
-    ~Guard() { list->remove(*conn); }
-  };
-  [[nodiscard]] Guard add(Connection& conn) {
-    std::lock_guard lock{mutex};
-    connections.push_back(conn);
-    return Guard{this, &conn};
-  }
-  void close(boost::system::error_code& ec) {
-    std::lock_guard lock{mutex};
-    for (auto& conn : connections) {
-      conn.socket.close(ec);
-    }
-    connections.clear();
-  }
-};
+using rgw::asio::Connection;
+using rgw::asio::ConnectionList;
 
 namespace dmc = rgw::dmclock;
 class AsioFrontend {
