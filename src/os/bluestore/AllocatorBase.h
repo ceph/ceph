ceph/ceph-ci

os/bluestore: add per-instance new allocator perf counters
Fixes: https://tracker.ceph.com/issues/76936

Signed-off-by: Jaya Prakash <jayaprakash@ibm.com>

src/os/bluestore/AllocatorBase.h
@@ -19,6 +19,15 @@
 #include "bluestore_types.h"
 #include "common/ceph_mutex.h"
 #include "Allocator.h"
+#include "common/perf_counters.h"
+#include "common/perf_counters_collection.h"
+
+enum {
+  l_bluestore_allocator_first = 732300,
+  l_bluestore_allocator_alloc_process_lat,
+  l_bluestore_allocator_lock_wait_lat,
+  l_bluestore_allocator_last
+};
 
 class AllocatorBase : public Allocator {
 protected:
@@ -293,6 +302,27 @@ public:
 private:
   class SocketHook;
   SocketHook* asok_hook = nullptr;
+};
+
+class AllocatorPerf {
+private:
+  CephContext* cct = nullptr;
+protected:
+  PerfCounters *logger = nullptr;
+public:
+   AllocatorPerf() = default;
+  AllocatorPerf(CephContext* cct, std::string_view name)
+    : cct(cct)
+  {
+    if (cct) {
+      _init_logger(std::string(name));
+    }
+  }
+  ~AllocatorPerf() {
+    _shutdown_logger();
+  }
+  void _init_logger(std::string name);
+  void _shutdown_logger();
 };
 
 #endif

ceph/ceph

src/os/bluestore/AllocatorBase.h
@@ -19,6 +19,15 @@
 #include "bluestore_types.h"
 #include "common/ceph_mutex.h"
 #include "Allocator.h"
+#include "common/perf_counters.h"
+#include "common/perf_counters_collection.h"
+
+enum {
+  l_bluestore_allocator_first = 732300,
+  l_bluestore_allocator_alloc_process_lat,
+  l_bluestore_allocator_lock_wait_lat,
+  l_bluestore_allocator_last
+};
 
 class AllocatorBase : public Allocator {
 protected:
@@ -293,6 +302,26 @@ public:
 private:
   class SocketHook;
   SocketHook* asok_hook = nullptr;
+};
+
+class AllocatorPerf {
+private:
+  CephContext* cct = nullptr;
+protected:
+  PerfCounters *logger = nullptr;
+public:
+  AllocatorPerf() = delete;
+  AllocatorPerf(CephContext* cct, std::string_view name)
+    : cct(cct)
+  {
+    _init_logger(name);
+  }
+  ~AllocatorPerf() {
+    _shutdown_logger();
+  }
+private:
+  void _init_logger(std::string_view name);
+  void _shutdown_logger();
 };
 
 #endif