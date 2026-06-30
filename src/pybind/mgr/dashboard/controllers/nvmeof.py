dbf83dd48b108fd95f38ea45769035e89e111b6e
pujashahu <pshahu@redhat.com>
2026-07-01 02:21:53 +0530

    mgr/dashboard: NVMe-oF – Add edit authentication support for subsystem
    Fixes: https://tracker.ceph.com/issues/77840
    
    Signed-off-by: pujaoshahu <pshahu@redhat.com>
    (cherry picked from commit 6c923d6c57884a31750e267a2d11a904430d82dc)
    
     Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-group-form/nvmeof-group-form.component.html
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystem-overview/nvmeof-subsystem-overview.component.html
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystem-overview/nvmeof-subsystem-overview.component.spec.ts
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystem-overview/nvmeof-subsystem-overview.component.ts

--- src/pybind/mgr/dashboard/controllers/nvmeof.py
+++ src/pybind/mgr/dashboard/controllers/nvmeof.py
@@ -544,6 +544,8 @@ else:
                 )
             )
 
+        @Endpoint('PUT', '{nqn}/change_key')
+        @UpdatePermission
         @EndpointDoc(
             "Change subsystem inband authentication key",
             parameters={
