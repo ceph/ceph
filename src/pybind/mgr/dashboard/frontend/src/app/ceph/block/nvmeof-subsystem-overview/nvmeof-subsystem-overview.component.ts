c7ec9a1c61ae48a1df644b237e1d1af822c6b9b2
pujashahu <pshahu@redhat.com>
2026-07-01 02:21:53 +0530

    mgr/dashboard: NVMe-oF – Add edit authentication support for subsystem
    Fixes: https://tracker.ceph.com/issues/77840
    
    Signed-off-by: pujaoshahu <pshahu@redhat.com>
    (cherry picked from commit 6c923d6c57884a31750e267a2d11a904430d82dc)
    
     Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-group-form/nvmeof-group-form.component.html
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystem-overview/nvmeof-subsystem-overview.component.html
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystem-overview/nvmeof-subsystem-overview.component.spec.ts
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystem-overview/nvmeof-subsystem-overview.component.ts
    
    -Kept current group-form tile UI; skipped legacy enable_auth field renames
    Kept template-driven subsystem overview; added Subsystem Type spec coverage only
    Kept existing table filter UI; skipped unrelated custom-filter popover changes
    Kept current service-form mTLS submit path; applied default RBD pool selection for nvmeof
    Skipped CertificateType / pool service_id changes not supported on this branch yet

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystem-overview/nvmeof-subsystem-overview.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-subsystem-overview/nvmeof-subsystem-overview.component.ts
@@ -1,7 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
+
 import { NvmeofService } from '~/app/shared/api/nvmeof.service';
-import { NvmeofSubsystem } from '~/app/shared/models/nvmeof';
+import {
+  NvmeofSubsystem,
+  NvmeofSubsystemInitiator,
+  NO_AUTH,
+  getSubsystemAuthStatus
+} from '~/app/shared/models/nvmeof';
+import { ICON_TYPE } from '~/app/shared/enum/icons.enum';
+import { ModalCdsService } from '~/app/shared/services/modal-cds.service';
+import { NvmeofEditAuthenticationComponent } from '../nvmeof-edit-authentication/nvmeof-edit-authentication.component';
 
 @Component({
   selector: 'cd-nvmeof-subsystem-overview',
@@ -13,8 +23,13 @@ export class NvmeofSubsystemOverviewComponent implements OnInit {
   subsystemNQN: string;
   groupName: string;
   subsystem: NvmeofSubsystem;
+  authStatus = NO_AUTH;
 
-  constructor(private route: ActivatedRoute, private nvmeofService: NvmeofService) {}
+  constructor(
+    private route: ActivatedRoute,
+    private nvmeofService: NvmeofService,
+    private modalService: ModalCdsService
+  ) {}
 
   ngOnInit() {
     this.route.parent?.params.subscribe((params) => {
@@ -34,10 +49,29 @@ export class NvmeofSubsystemOverviewComponent implements OnInit {
   }
 
   fetchSubsystem() {
-    this.nvmeofService
-      .getSubsystem(this.subsystemNQN, this.groupName)
-      .subscribe((subsystem: NvmeofSubsystem) => {
-        this.subsystem = subsystem;
-      });
+    forkJoin({
+      subsystem: this.nvmeofService.getSubsystem(this.subsystemNQN, this.groupName),
+      initiators: this.nvmeofService.getInitiators(this.subsystemNQN, this.groupName)
+    }).subscribe(({ subsystem, initiators }) => {
+      this.subsystem = subsystem as NvmeofSubsystem;
+      const initiatorList = initiators as
+        | NvmeofSubsystemInitiator[]
+        | { hosts?: NvmeofSubsystemInitiator[] };
+      this.authStatus = getSubsystemAuthStatus(this.subsystem, initiatorList);
+    });
+  }
+
+  getAuthStatusIcon(authStatus: string): keyof typeof ICON_TYPE {
+    return authStatus === NO_AUTH ? 'error' : 'success';
+  }
+
+  openEditAuthModal() {
+    const modalRef = this.modalService.show(NvmeofEditAuthenticationComponent, {
+      subsystemNQN: this.subsystemNQN,
+      groupName: this.groupName
+    });
+    if (modalRef?.closeChange) {
+      modalRef.closeChange.subscribe(() => this.fetchSubsystem());
+    }
   }
 }
