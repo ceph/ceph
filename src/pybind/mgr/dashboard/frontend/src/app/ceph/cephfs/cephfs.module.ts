5ed6a46676c28445628c81475e64b666c74bab49
Dnyaneshwari Talwekar <dtalwekar@li-4c4c4544-0038-3510-8056-b5c04f473234.ibm.com>
2026-07-01 18:23:46 +0530

    mgr/dashboard : Step 3 - Review Signed-off-by: Dnyaneshwari Talwekar <dtalweka@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/cephfs/cephfs.module.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/cephfs/cephfs.module.ts
@@ -137,7 +137,6 @@ import FolderIcon16 from '@carbon/icons/es/folder/16';
     CephfsDirectoriesComponent,
     CephfsSubvolumeListComponent,
     CephfsSubvolumeFormComponent,
-    CephfsDirectoriesComponent,
     CephfsSubvolumeGroupComponent,
     CephfsSubvolumegroupFormComponent,
     CephfsSubvolumeSnapshotsListComponent,
