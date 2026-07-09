1fd75275f2a2f41089ac74a67282146c9b2a0659
Shweta Bhosale <Shweta.Bhosale1@ibm.com>
2026-07-09 20:01:45 +0530

    qa: fixed the NFS protocol version in test_nfs.py
    Fixes: https://tracker.ceph.com/issues/78099
    Signed-off-by: Shweta Bhosale <Shweta.Bhosale1@ibm.com>
    (cherry picked from commit 19dea175a36185aa18ef06a3684d35fd6fb535d1)

--- qa/tasks/cephfs/test_nfs.py
+++ qa/tasks/cephfs/test_nfs.py
@@ -55,7 +55,7 @@ class TestNFS(MgrTestCase):
          "squash": "none",
          "security_label": True,
          "protocols": [
-           3, 4
+           4
          ],
          "transports": [
            "TCP"
