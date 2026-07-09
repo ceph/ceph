320a7ed5eead550def835bec892205f09a87b1ef
Tom Sollers <tom.sollers@ibm.com>
2026-07-09 10:37:53 +0100

    osd: fix pool migration target reservation not released on cleanup
    MigratingSource::migration_release_reservations() cancelled the local
    I/O reservation but never released the target PG reservation, leaving
    it held indefinitely. Call pool_migration_release_target_reservation()
    from there, guard it against the no-reservation case, and add the
    missing override specifier.
    
    Signed-off-by: Tom Sollers <tom.sollers@ibm.com>

--- src/osd/PeeringState.cc
+++ src/osd/PeeringState.cc
@@ -6220,6 +6220,7 @@ void PeeringState::MigratingSource::migration_release_reservations()
 {
   DECLARE_LOCALS;
   pl->cancel_local_background_io_reservation();
+  pl->pool_migration_release_target_reservation();
 }
 
 void PeeringState::MigratingSource::suspend_migration()
