c059e675b7257fcfbc8269fa96b5e88c017f3d43
Adam King <adking@redhat.com>
2026-07-09 10:21:14 -0400

    mgr/cephadm: increaase cache timeout time for gather-facts
    This doesn't really need to be run once per minute and
    since adding sysctl info it no longer runs fast enough
    for us to be doing it this often.
    
    Change-Id: 33ce0648-64c9-4a8a-baa0-553a8a011ee0
    Signed-off-by: Adam King <adking@redhat.com>
    (cherry picked from commit a11578b2702c381351d785540c9787d934245e05)
    
    Conflicts:
    	src/pybind/mgr/cephadm/module.py

--- src/pybind/mgr/cephadm/module.py
+++ src/pybind/mgr/cephadm/module.py
@@ -207,7 +207,7 @@ class CephadmOrchestrator(orchestrator.Orchestrator, MgrModule,
         Option(
             'facts_cache_timeout',
             type='secs',
-            default=1 * 60,
+            default=10 * 60,
             desc='seconds to cache host facts data',
         ),
         Option(
