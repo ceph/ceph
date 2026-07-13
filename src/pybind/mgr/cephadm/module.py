18b08545733bce6efd7d8a5a1551f64bc31efdb4
Adam King <adking@redhat.com>
2026-07-13 10:55:52 -0400

    mgr/cephadm: try using new key cipher when creating cephx keys
    Also moved the constants to utils.py to avoid a circular
    dependency bettwen serve and upgrade
    
    Change-Id: 5b47f195-27d4-40e0-b14a-2f3d1bbaeded
    Signed-off-by: Adam King <adking@redhat.com>
    (cherry picked from commit 97ae22593690ca7cba9d58ed8607bd9d4b346663)

--- src/pybind/mgr/cephadm/module.py
+++ src/pybind/mgr/cephadm/module.py
@@ -96,7 +96,7 @@ from .inventory import (
     NodeProxyCache,
     OrchSecretNotFound,
 )
-from .upgrade import CephadmUpgrade, ROTATION_CIPHER
+from .upgrade import CephadmUpgrade
 from .template import TemplateMgr
 from .utils import CEPH_IMAGE_TYPES, RESCHEDULE_FROM_OFFLINE_HOSTS_TYPES, forall_hosts, \
     cephadmNoImage, SpecialHostLabels
@@ -2574,7 +2574,7 @@ Then run the following:
             'prefix': 'auth rotate',
             'entity': daemon_spec.entity_name(),
             'format': 'json',
-            'key_type': ROTATION_CIPHER
+            'key_type': utils.ROTATION_CIPHER
         })
         if rc:
             raise OrchestratorError(
