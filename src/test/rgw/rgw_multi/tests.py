8e15219a7b9259789978c11fa2bf5acb85994ba0
Casey Bodley <cbodley@redhat.com>
2026-07-16 13:14:17 -0400

    tests/rgw/multisite: fix syntax error from boto3 backport
    boto3 backport commit be58ba51efdfb9e9dea41637c73e2c7e454147f5 didn't
    update check_all_buckets_dont_exist():
    
    >     log.critical('zone %s contains bucket %s', zone.zone, b)
    >
    > NameError: name 'zone' is not defined. Did you mean: 'Zone'?
    
    copy the syntax from check_all_buckets_exist() which uses
    zone_conn.zone.name instead of zone.zone
    
    Fixes: https://tracker.ceph.com/issues/77478
    
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- src/test/rgw/rgw_multi/tests.py
+++ src/test/rgw/rgw_multi/tests.py
@@ -540,7 +540,7 @@ def check_all_buckets_dont_exist(zone_conn, buckets):
         except:
             continue
 
-        log.critical('zone %s contains bucket %s', zone.zone, b)
+        log.critical('zone %s contains bucket %s', zone_conn.zone.name, b)
         return False
 
     return True
