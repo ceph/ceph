2c1f1775188d49949617459c40522a28c0a7f2a3
Adam King <adking@redhat.com>
2026-07-16 13:54:19 -0400

    cephadm: fix key rotation for raw OSDs
    These OSDs don't have an LV tied to them and
    won't show up in `ceph-volume lvm list`. We need
    to get the path of the raw device from
    `ceph-volume raw list` and use that when updating
    the osd_key bluestore label
    
    Change-Id: 663be380-ec0a-4e3a-8f33-514289f683dc
    Signed-off-by: Adam King <adking@redhat.com>
    (cherry picked from commit 4a9c2019321cde6335ce5efc7310ef6ec4d14832)
    (cherry picked from commit 3cf41d3970b62d8afdd88190f48e2a09bb80ddde)
    
    [conflict: reef cephadm is pre-cephadmlib; change applied to ported rotation code in src/cephadm/cephadm.py]

--- src/cephadm/cephadm.py
+++ src/cephadm/cephadm.py
@@ -518,6 +518,37 @@ class OSD(object):
         lv_path = ''
         encrypted = False
         osd_lv_data = osd_bluestore_data.get(self.daemon_id, [])
+        if not osd_lv_data:
+            logger.info(
+                'No lv data found for OSD.%s, checking for raw OSD', self.daemon_id
+            )
+            c_v_container = CephContainer(
+                ctx,
+                image=ctx.image,
+                privileged=True,
+                entrypoint='/usr/sbin/ceph-volume',
+                args=[
+                    'raw',
+                    'list',
+                    '--format',
+                    'json',
+                ],
+                volume_mounts=get_container_mounts(
+                    ctx, self.fsid, 'clusterless-ceph-volume', None
+                ),
+            )
+            out, err, ret = call(
+                ctx,
+                c_v_container.run_cmd(),
+                verbosity=CallVerbosity.QUIET_UNLESS_ERROR,
+            )
+            raw_list_data = json.loads(out)
+            for osd_data in raw_list_data.values():
+                if str(osd_data.get('osd_id', -1)) == str(self.daemon_id):
+                    logger.info('Found data for osd.%s in raw list', str(self.daemon_id))
+                    # misnomer since this isn't an LV, but it is the path
+                    # we need to pass to the bluestore tool
+                    lv_path = osd_data.get('device', None)
         if osd_lv_data:
             for dev in osd_lv_data:
                 if dev.get('type') == 'block':
