c521462d1160580c4b84cfa9ea5fa94d10d4f226
Abhishek Desai <abhishek.desai1@ibm.com>
2026-07-20 20:31:51 +0530

    mgr/dashboard : Fix access denied issue for readonly user and user role fixes : https://tracker.ceph.com/issues/78418 Signed-off-by: Abhishek Desai <abhishek.desai1@ibm.com>
    (cherry picked from commit 21e74195578c7ab4983c80efe7f17117aecaa287)
    
     Conflicts:
    	src/pybind/mgr/dashboard/frontend/src/app/ceph/block/mirroring/overview/overview.component.html
    	src/pybind/mgr/dashboard/frontend/src/app/core/navigation/administration/administration.component.html
    	src/pybind/mgr/dashboard/frontend/src/app/shared/services/api-interceptor.service.spec.ts
    	src/pybind/mgr/dashboard/frontend/src/app/shared/services/api-interceptor.service.ts
            - Accpted incoming changes for all files - list issues only

--- src/pybind/mgr/dashboard/frontend/src/app/core/navigation/administration/administration.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/core/navigation/administration/administration.component.spec.ts
@@ -1,5 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
+import { Permission } from '~/app/shared/models/permissions';
 import { SharedModule } from '~/app/shared/shared.module';
 import { configureTestBed } from '~/testing/unit-test-helper';
 import { AdministrationComponent } from './administration.component';
@@ -21,5 +23,35 @@ describe('AdministrationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('settings icon visibility', () => {
+    const setPermissions = (user: string[], configOpt: string[]) => {
+      component.userPermission = new Permission(user);
+      component.configOptPermission = new Permission(configOpt);
+      fixture.detectChanges();
+    };
+
+    const settingsIcon = () => fixture.debugElement.query(By.css('[cdsIcon="settings"]'));
+
+    it('should show the settings icon when user has user.read', () => {
+      setPermissions(['read'], []);
+      expect(settingsIcon()).toBeTruthy();
+    });
+
+    it('should show the settings icon when user has configOpt.read', () => {
+      setPermissions([], ['read']);
+      expect(settingsIcon()).toBeTruthy();
+    });
+
+    it('should show the settings icon when user has both user.read and configOpt.read', () => {
+      setPermissions(['read'], ['read']);
+      expect(settingsIcon()).toBeTruthy();
+    });
+
+    it('should hide the settings icon when user has neither user.read nor configOpt.read', () => {
+      setPermissions([], []);
+      expect(settingsIcon()).toBeFalsy();
+    });
   });
 });
