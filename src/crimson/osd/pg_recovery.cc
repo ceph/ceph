ceph/ceph

commit 60537bdd2f42fb87c9d4a8b62e9e722231a8842b

crimson/osd/pg_recovery: call on_global_recover on the "recover deleted" object when it's not missing on the primary too
Fixes: https://tracker.ceph.com/issues/75574
Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

src/crimson/osd/pg_recovery.cc
@@ -334,9 +334,11 @@ RecoveryBackend::interruptible_future<> PGRecovery::prep_object_replica_deletes(
       trigger,
       pg->get_recovery_backend()->push_delete(soid, need).then_interruptible(
 	[=, this] {
-	object_stat_sum_t stat_diff;
-	stat_diff.num_objects_recovered = 1;
-	on_global_recover(soid, stat_diff, true);
+        if (pg->get_peering_state().get_pg_log().get_missing().is_missing(soid)) {
+          object_stat_sum_t stat_diff;
+          stat_diff.num_objects_recovered = 1;
+          on_global_recover(soid, stat_diff, true);
+        }
 	return seastar::make_ready_future<>();
       })
     );

commit 6dab4b50345fc1cc3c40890f2cd692450392c081

crimson/osd/pg_recovery: call on_global_recover on replica-recover-deleted objects only when it's not missing on the primary too
The issue was introduced by 60537bdd2f42fb87c9d4a8b62e9e722231a8842b
which didn't fix the issue it was supposed to because of the same error
this commit tries to fix.

Fixes: https://tracker-origin.ceph.com/issues/78676
Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

src/crimson/osd/pg_recovery.cc
@@ -348,7 +348,7 @@ RecoveryBackend::interruptible_future<> PGRecovery::prep_object_replica_deletes(
       trigger,
       pg->get_recovery_backend()->push_delete(soid, need).then_interruptible(
 	[=, this] {
-        if (pg->get_peering_state().get_pg_log().get_missing().is_missing(soid)) {
+        if (!pg->get_peering_state().get_pg_log().get_missing().is_missing(soid)) {
           object_stat_sum_t stat_diff;
           stat_diff.num_objects_recovered = 1;
           on_global_recover(soid, stat_diff, true);