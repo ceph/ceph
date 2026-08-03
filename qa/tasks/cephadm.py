f576c2b11dd0cb892a8b6031d464984f07e9d9fe
Patrick Donnelly <pdonnell@ibm.com>
2026-08-03 15:48:19 -0400

    qa/tasks/cephadm: create cluster keyring file
    This mirrors what the ceph task does.
    
    Change-Id: 7b799510-f6b3-4be4-ac79-d2aff61d6ed8
    Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>

--- qa/tasks/cephadm.py
+++ qa/tasks/cephadm.py
@@ -937,6 +937,7 @@ def ceph_bootstrap(ctx, config):
             args=[
                 'sudo', 'rm', '-f',
                 '/etc/ceph/{}.conf'.format(cluster_name),
+                '/etc/ceph/{}.keyring'.format(cluster_name),
                 '/etc/ceph/{}.client.admin.keyring'.format(cluster_name),
             ],
             check_status=False,  # rm-cluster above should have cleaned these up
@@ -1830,14 +1831,17 @@ def distribute_config_and_admin_keyring(ctx, config):
             path='/etc/ceph/{}.client.admin.keyring'.format(cluster_name),
             data=ctx.ceph[cluster_name].admin_keyring,
             sudo=True)
+        keyring = f"/etc/ceph/{cluster_name}.keyring"
+        remote.write_file(
+            path=keyring,
+            data=ctx.ceph[cluster_name].admin_keyring,
+            sudo=True)
+        ctx.ceph[cluster_name].keyring = keyring
     try:
         yield
     finally:
-        ctx.cluster.run(args=[
-            'sudo', 'rm', '-f',
-            '/etc/ceph/{}.conf'.format(cluster_name),
-            '/etc/ceph/{}.client.admin.keyring'.format(cluster_name),
-        ])
+        # ceph_bootstrap cleans up keys/confs
+        pass
 
 
 @contextlib.contextmanager
