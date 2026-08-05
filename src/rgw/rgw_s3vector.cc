79d16d766ba574c74da28bb0a3080746d920df6f
Aadesh Rasal <Aadesh.Rasal@ibm.com>
2026-08-05 21:14:30 +0000

    RGW : Adding put vector implementation
    Signed-off-by: Aadesh Rasal <Aadesh.Rasal@ibm.com>

--- src/rgw/rgw_s3vector.cc
+++ src/rgw/rgw_s3vector.cc
@@ -1478,6 +1478,24 @@ namespace rgw::s3vector {
       throw JSONDecoder::err("policy must be specified and cannot be empty");
     }
     // policy TODO: validate JSON
+    JSONParser parser;
+    if(!parser.parse(policy.c_str(), policy.length())) {
+      throw JSONDecoder::err("policy must be specified and cannot be empty");
+    }
+  }
+
+  int put_vector_bucket_policy(const put_vector_bucket_policy_t& configuration, 
+    rgw::sal::VectorBucket* bucket, DoutPrefixProvider* dpp, optional_yield y) {
+    log_configuration(dpp, "PutVectorBucketPolicy", configuration);
+
+    rgw::sal::Attrs &attrs = bucket->get_attrs();
+    attrs[RGW_ATTR_IAM_POLICY].clear();
+    attrs[RGW_ATTR_IAM_POLICY].append(configuration.policy);
+    int ret = bucket->set_attrs(std::move(attrs));
+    if(ret < 0) {
+      ldpp_dout(dpp, 1)<<"ERROR: PutVectorBucketPolicy failed to store attrs, ret="<< ret << dendl;
+    }
+    return ret;
   }
 
   // get vector bucket policy
