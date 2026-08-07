ceph/ceph

commit e8e47abef6cd7f9de46ce603c675b6bc478eb0e5

mgr/DaemonServer: add heap command to mgr
Ceph-mgr didn't had heap profile collection, the following
commit will add admin socket command to support heap commands:
 dump|start_profiler|stop_profiler|release|get_release_rate|set_release_rate|stats
just like OSD, MON and MDS

Fixes: https://tracker.ceph.com/issues/76639
Signed-off-by: Nitzan Mordechai <nmordech@redhat.com>

src/mgr/DaemonServer.cc
@@ -49,6 +49,7 @@
 #include "common/pick_address.h"
 #include "common/TextTable.h"
 #include "crush/CrushWrapper.h"
+#include "perfglue/heap_profiler.h"
 
 #include <boost/algorithm/string.hpp>
 
@@ -268,6 +269,14 @@ int DaemonServer::init(uint64_t gid, entity_addrvec_t client_addrs)
              "name=filterstr,type=CephString,n=N,req=false",
              asok_hook,
              "show slowest recent ops, sorted by duration");
+  ceph_assert(r == 0);
+  r = admin_socket->register_command(
+    "heap " \
+    "name=heapcmd,type=CephChoices,strings=" \
+    "dump|start_profiler|stop_profiler|release|get_release_rate|set_release_rate|stats " \
+    "name=value,type=CephString,req=false",
+    asok_hook,
+    "show heap usage info (available only if compiled with tcmalloc)");
   ceph_assert(r == 0);
   return 0;
 }
@@ -3858,6 +3867,22 @@ will start to track new ops received afterwards.";
         goto out;
       }
     }
+  } else if (admin_command == "heap") {
+    if (!ceph_using_tcmalloc()) {
+      ss << "not using tcmalloc";
+      ret = -EOPNOTSUPP;
+      goto out;
+    }
+    string heapcmd;
+    cmd_getval(cmdmap, "heapcmd", heapcmd);
+    vector<string> heapcmd_vec;
+    get_str_vec(heapcmd, heapcmd_vec);
+    string value;
+    if (cmd_getval(cmdmap, "value", value)) {
+      heapcmd_vec.push_back(value);
+    }
+    ceph_heap_profiler_handle_command(heapcmd_vec, ss);
+    ret = 0;
   }
   dout(10) << "ret := " << ret << dendl;
   return true;

commit 1cd0421b01cfa962a8ba4ef212b97d1674efd1dc

mgr: drop the duplicate heap asok command from DaemonServer
every ceph-mgr dies with an assert the moment it wins the election:

    asok(...) register_command heap cmddesc heap name=heapcmd,... EEXIST
    src/mgr/DaemonServer.cc: 282: FAILED ceph_assert(r == 0)
     5: (DaemonServer::init(unsigned long, entity_addrvec_t)+0x10ba)
     6: (Mgr::init()+0xc25)

d8dde402b6d and e8e47abef6c both register a "heap" admin socket
command for the mgr: one in MgrStandby::init(), which runs at process
startup, the other in DaemonServer::init(), which runs when the mgr
goes active. each passed CI on its own, and the merge raised no
textual conflict. so nothing noticed that the second
register_command() now returns EEXIST and trips the assert sitting
right next to it, until the ceph-api job hit it with both in main.

keep the MgrStandby registration and drop the DaemonServer one. the
MgrStandby hook lives as long as the process, so "heap" works on a
standby mgr as well as an active one, while the dropped one only
existed after activation. and tcmalloc heap profiling is process-wide
state anyway, it needs nothing from DaemonServer.

Fixes: e8e47abef6cd7f9de46ce603c675b6bc478eb0e5
Signed-off-by: Kefu Chai <k.chai@proxmox.com>

src/mgr/DaemonServer.cc
@@ -49,7 +49,6 @@
 #include "common/pick_address.h"
 #include "common/TextTable.h"
 #include "crush/CrushWrapper.h"
-#include "perfglue/heap_profiler.h"
 
 #include <boost/algorithm/string.hpp>
 
@@ -271,14 +270,6 @@ int DaemonServer::init(uint64_t gid, entity_addrvec_t client_addrs)
              "name=filterstr,type=CephString,n=N,req=false",
              asok_hook,
              "show slowest recent ops, sorted by duration");
-  ceph_assert(r == 0);
-  r = admin_socket->register_command(
-    "heap " \
-    "name=heapcmd,type=CephChoices,strings=" \
-    "dump|start_profiler|stop_profiler|release|get_release_rate|set_release_rate|stats " \
-    "name=value,type=CephString,req=false",
-    asok_hook,
-    "show heap usage info (available only if compiled with tcmalloc)");
   ceph_assert(r == 0);
   return 0;
 }
@@ -3855,22 +3846,6 @@ will start to track new ops received afterwards.";
         goto out;
       }
     }
-  } else if (admin_command == "heap") {
-    if (!ceph_using_tcmalloc()) {
-      ss << "not using tcmalloc";
-      ret = -EOPNOTSUPP;
-      goto out;
-    }
-    string heapcmd;
-    cmd_getval(cmdmap, "heapcmd", heapcmd);
-    vector<string> heapcmd_vec;
-    get_str_vec(heapcmd, heapcmd_vec);
-    string value;
-    if (cmd_getval(cmdmap, "value", value)) {
-      heapcmd_vec.push_back(value);
-    }
-    ceph_heap_profiler_handle_command(heapcmd_vec, ss);
-    ret = 0;
   }
   dout(10) << "ret := " << ret << dendl;
   return true;