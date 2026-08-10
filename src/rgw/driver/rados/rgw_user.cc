ceph/ceph

commit ac8226ad64f4c03bc9ce7d37f1d2aa7d9afa9e24

rgw: update bucket ownership when adding user to account
Signed-off-by: Casey Bodley <cbodley@redhat.com>

src/rgw/driver/rados/rgw_user.cc
@@ -1643,6 +1643,68 @@ static int validate_account_tenant(const DoutPrefixProvider* dpp,
   return 0;
 }
 
+static int adopt_user_bucket(const DoutPrefixProvider* dpp,
+                             optional_yield y,
+                             rgw::sal::Driver* driver,
+                             const rgw_bucket& bucketid,
+                             const rgw_owner& new_owner)
+{
+  // retry in case of racing writes to the bucket instance metadata
+  static constexpr auto max_retries = 10;
+  int tries = 0;
+  int r = 0;
+
+  do {
+    ldpp_dout(dpp, 1) << "adopting bucket " << bucketid << "..." << dendl;
+
+    std::unique_ptr<rgw::sal::Bucket> bucket;
+    r = driver->load_bucket(dpp, bucketid, &bucket, y);
+    if (r < 0) {
+      ldpp_dout(dpp, 1) << "failed to load bucket " << bucketid
+          << ": " << cpp_strerror(r) << dendl;
+      return r;
+    }
+
+    r = bucket->chown(dpp, new_owner, y);
+    if (r < 0) {
+      ldpp_dout(dpp, 1) << "failed to chown bucket " << bucketid
+          << ": " << cpp_strerror(r) << dendl;
+    }
+    ++tries;
+  } while (r == -ECANCELED && tries < max_retries);
+
+  return r;
+}
+
+static int adopt_user_buckets(const DoutPrefixProvider* dpp, optional_yield y,
+                              rgw::sal::Driver* driver, const rgw_user& user,
+                              const rgw_account_id& account_id)
+{
+  const size_t max_chunk = dpp->get_cct()->_conf->rgw_list_buckets_max_chunk;
+  constexpr bool need_stats = false;
+
+  ldpp_dout(dpp, 1) << "adopting all buckets owned by " << user
+      << " into account " << account_id << dendl;
+
+  rgw::sal::BucketList listing;
+  do {
+    int r = driver->list_buckets(dpp, user, user.tenant, listing.next_marker,
+                                 "", max_chunk, need_stats, listing, y);
+    if (r < 0) {
+      return r;
+    }
+
+    for (const auto& ent : listing.buckets) {
+      r = adopt_user_bucket(dpp, y, driver, ent.bucket, account_id);
+      if (r < 0 && r != -ENOENT) {
+        return r;
+      }
+    }
+  } while (!listing.next_marker.empty());
+
+  return 0;
+}
+
 int RGWUser::execute_add(const DoutPrefixProvider *dpp, RGWUserAdminOpState& op_state, std::string *err_msg,
 			 optional_yield y)
 {
@@ -1716,7 +1778,6 @@ int RGWUser::execute_add(const DoutPrefixProvider *dpp, RGWUserAdminOpState& op_
       return ret;
     }
     user_info.account_id = op_state.account_id;
-    // TODO: change account on user's buckets
   }
 
   if (op_state.account_root) {
@@ -2031,7 +2092,13 @@ int RGWUser::execute_modify(const DoutPrefixProvider *dpp, RGWUserAdminOpState& 
         set_err_msg(err_msg, err);
         return ret;
       }
-      // TODO: change account on user's buckets
+      // change account on user's buckets
+      ret = adopt_user_buckets(dpp, y, driver, user_info.user_id,
+                               user_info.account_id);
+      if (ret < 0) {
+        set_err_msg(err_msg, "failed to change ownership of user's buckets");
+        return ret;
+      }
     }
   }
 

commit 15bc7d3b8b1b3045a89deec62ae3e9eed9d42f53

rgw: suspend account buckets when suspending account root
After a user is adopted into an account, buckets are owned by the
account id. user suspend still listed buckets by uid, so the account's
buckets were left enabled. Account root is the owner analogue: list and
set_buckets_enabled against the account id instead. Non-root account
users only get user.suspended and do not suspend account buckets.

Apply the bucket update after account-id / account-root are resolved so
a combined modify still targets the right owner.

Fixes: http://tracker.ceph.com/issues/79320
Fixes: ac8226ad64f4c03bc9ce7d37f1d2aa7d9afa9e24
Signed-off-by: ramin.najarbashi <ramin.najarbashi@gmail.com>

src/rgw/driver/rados/rgw_user.cc
@@ -1687,6 +1687,53 @@ static int adopt_user_buckets(const DoutPrefixProvider* dpp, optional_yield y,
   return 0;
 }
 
+/* Enable or disable buckets owned by this user. Account root users own buckets
+ * under the account id (after adopt_user_buckets), so list that owner instead
+ * of the uid. Non-root account members must not suspend the whole account. */
+static int set_user_bucket_suspended(const DoutPrefixProvider* dpp,
+                                     optional_yield y,
+                                     rgw::sal::Driver* driver,
+                                     const RGWUserInfo& user_info,
+                                     bool suspended,
+                                     std::string* err_msg)
+{
+  if (user_info.user_id.empty()) {
+    set_err_msg(err_msg, "empty user id passed...aborting");
+    return -EINVAL;
+  }
+
+  rgw_owner owner = user_info.user_id;
+  if (user_info.type == TYPE_ROOT && !user_info.account_id.empty()) {
+    owner = user_info.account_id;
+  }
+
+  const size_t max_buckets = dpp->get_cct()->_conf->rgw_list_buckets_max_chunk;
+  rgw::sal::BucketList listing;
+  do {
+    int ret = driver->list_buckets(dpp, owner, user_info.user_id.tenant,
+                                   listing.next_marker, string(),
+                                   max_buckets, false, listing, y);
+    if (ret < 0) {
+      set_err_msg(err_msg, "could not get buckets for owner: " +
+                  to_string(owner));
+      return ret;
+    }
+
+    std::vector<rgw_bucket> bucket_names;
+    for (auto& ent : listing.buckets) {
+      bucket_names.push_back(std::move(ent.bucket));
+    }
+
+    ret = driver->set_buckets_enabled(dpp, bucket_names, !suspended, y);
+    if (ret < 0) {
+      set_err_msg(err_msg, "failed to modify bucket");
+      return ret;
+    }
+  } while (!listing.next_marker.empty());
+
+  return 0;
+}
+
 int RGWUser::execute_add(const DoutPrefixProvider *dpp, RGWUserAdminOpState& op_state, std::string *err_msg,
 			 optional_yield y)
 {
@@ -2042,40 +2089,7 @@ int RGWUser::execute_modify(const DoutPrefixProvider *dpp, RGWUserAdminOpState& 
     user_info.quota.user_quota = op_state.get_user_quota();
 
   if (op_state.has_suspension_op()) {
-    __u8 suspended = op_state.get_suspension_status();
-    user_info.suspended = suspended;
-
-
-    if (user_id.empty()) {
-      set_err_msg(err_msg, "empty user id passed...aborting");
-      return -EINVAL;
-    }
-    std::unique_ptr<rgw::sal::User> user = driver->get_user(user_id);
-
-    size_t max_buckets = dpp->get_cct()->_conf->rgw_list_buckets_max_chunk;
-
-    rgw::sal::BucketList listing;
-    do {
-      ret = driver->list_buckets(dpp, user->get_id(), user->get_tenant(),
-                                 listing.next_marker, string(),
-                                 max_buckets, false, listing, y);
-      if (ret < 0) {
-        set_err_msg(err_msg, "could not get buckets for uid:  " + user_id.to_str());
-        return ret;
-      }
-
-      std::vector<rgw_bucket> bucket_names;
-      for (auto& ent : listing.buckets) {
-        bucket_names.push_back(std::move(ent.bucket));
-      }
-
-      ret = driver->set_buckets_enabled(dpp, bucket_names, !suspended, y);
-      if (ret < 0) {
-        set_err_msg(err_msg, "failed to modify bucket");
-        return ret;
-      }
-
-    } while (!listing.next_marker.empty());
+    user_info.suspended = op_state.get_suspension_status();
   }
 
   if (op_state.mfa_ids_specified) {
@@ -2136,6 +2150,16 @@ int RGWUser::execute_modify(const DoutPrefixProvider *dpp, RGWUserAdminOpState& 
       return -EINVAL;
     }
     user_info.type = op_state.account_root ? TYPE_ROOT : TYPE_RGW;
+  }
+
+  /* Apply bucket suspension after account_id / account-root are resolved so
+   * account root users disable account-owned buckets, not an empty uid list. */
+  if (op_state.has_suspension_op()) {
+    ret = set_user_bucket_suspended(dpp, y, driver, user_info,
+                                    user_info.suspended, err_msg);
+    if (ret < 0) {
+      return ret;
+    }
   }
 
   if (!user_info.account_id.empty()) {