fe8c5f45a9cae204e085ef338701e6e5aa6d4b26
Adam King <adking@redhat.com>
2026-08-16 11:37:59 -0400

    cephadm: Add retries to OSD key rotation
    There may be a delay between sending the OSD SIGTERM
    and the device actually being available for the
    ceph-bluestore-tool to access it to rotate the keyring
    
    Change-Id: 69b9ae64-a553-4df8-b9b2-8a424a76c152
    Signed-off-by: Adam King <adking@redhat.com>
    (cherry picked from commit dc39f525436c470d66ea98ee54692c7b45b917f0)
    
    [conflict: reef cephadm is pre-cephadmlib; change applied to ported rotation code in src/cephadm/cephadm.py]

--- src/cephadm/cephadm.py
+++ src/cephadm/cephadm.py
@@ -605,6 +605,9 @@ echo "$DM_CRYPT_KEY" | cryptsetup luksOpen $LV_PATH $DEV_NAME
                     helper_script_path: '/tmp/cryptsetup_action.sh',
                 },
             )
+            logger.info(
+                f'Opening osd.{self.daemon_id} with crypsetup'
+            )
             out, err, ret = call(
                 ctx,
                 cryptsetup_open_container.run_cmd(),
@@ -634,17 +637,31 @@ echo "$DM_CRYPT_KEY" | cryptsetup luksOpen $LV_PATH $DEV_NAME
             ],
             volume_mounts={'/dev': '/dev'},
         )
-        out, err, ret = call(
-            ctx,
-            bluestore_tool_container.run_cmd(),
-            verbosity=CallVerbosity.QUIET_UNLESS_ERROR,
+        logger.info(
+            f'Rotating osd.{self.daemon_id} key with ceph-bluestore-tool'
         )
-        if ret:
-            raise Error(
-                'Got error rotating osd keyring using ceph-bluestore-tool\n'
-                f'Out:{out}\n'
-                f'Err:{err}'
+        # The OSD may take some time to shutdown fully and make the device
+        # available for the key rotation
+        for i in [2, 5, 10, 30, 0]:
+            if not i:
+                break
+            out, err, ret = call(
+                ctx,
+                bluestore_tool_container.run_cmd(),
+                verbosity=CallVerbosity.VERBOSE,
             )
+            if ret:
+                if not i:
+                    raise Error(
+                        'Got error rotating osd keyring using ceph-bluestore-tool\n'
+                        f'Out:{out}\n'
+                        f'Err:{err}'
+                    )
+                logger.info(f'Got issue rotating osd keyring using ceph-bluestore-tool:\n{out}\n{err}\nRetrying in {i} seconds')
+                time.sleep(i)
+            else:
+                logger.info(f'Successfully rotated osd.{self.daemon_id} keyring')
+                break
 
         if encrypted:
             cryptsetup_close_container = CephContainer(
@@ -671,9 +688,12 @@ echo "$DM_CRYPT_KEY" | cryptsetup luksOpen $LV_PATH $DEV_NAME
                     f'Out:{out}\n'
                     f'Err:{err}'
                 )
+
+        call(ctx, ['systemctl', 'reset-failed', get_unit_name(self.fsid, 'osd', self.daemon_id)],
+             verbosity=CallVerbosity.QUIET_UNLESS_ERROR)
         call(
             ctx,
-            ['systemctl', 'restart', get_unit_name(self.fsid, 'osd', self.daemon_id)],
+            ['systemctl', 'start', get_unit_name(self.fsid, 'osd', self.daemon_id)],
             verbosity=CallVerbosity.QUIET_UNLESS_ERROR,
         )
 
