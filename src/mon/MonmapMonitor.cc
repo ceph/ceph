9024abddbbecfa3914ec970615811660ec2fd6c6
Patrick Donnelly <pdonnell@ibm.com>
2026-08-17 14:48:32 -0400

    mon/MonMap: sort allowed ciphers
    For comparisons.
    
    Change-Id: 23074e79-adf6-4765-ab18-2440eb98adf2
    Signed-off-by: Patrick Donnelly <pdonnell@ibm.com>
    (cherry picked from commit dcaec0a28d0fa0c2571441e513bb6c615fd3bf40)

--- src/mon/MonmapMonitor.cc
+++ src/mon/MonmapMonitor.cc
@@ -1275,6 +1275,7 @@ bool MonmapMonitor::prepare_command(MonOpRequestRef op)
         }
         ciphers.push_back(c);
       }
+      std::sort(ciphers.begin(), ciphers.end());
       pending_map.auth_allowed_ciphers = std::move(ciphers);
     } else if (name == "auth_preferred_cipher") {
       int c = CryptoManager::get_key_type(value);
