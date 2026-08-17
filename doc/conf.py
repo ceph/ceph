6a0c3ccbc9cdd65b2df15ac1271b6ae9eccbcc90
Kefu Chai <k.chai@proxmox.com>
2026-08-17 16:15:32 +0800

    doc: build with Sphinx 8
    The "< 7" cap was there because the RTD theme of the day did not support
    7, which current sphinx_rtd_theme does. Cap below 9 until the
    RemovedInSphinx90Warning deprecations are cleared. Sphinx 8 wants docutils
    >= 0.20 and Python >= 3.10, so drop that pin and raise the readthedocs
    interpreter.
    
    It also wants source_suffix as a mapping, and replaced the "style"
    template variable with the "styles" sequence, so iterate it in the theme
    layout.
    
    Signed-off-by: Kefu Chai <k.chai@proxmox.com>

--- doc/conf.py
+++ doc/conf.py
@@ -92,7 +92,7 @@ html_css_files = ['css/custom.css']
 
 # general configuration
 templates_path = ['_templates']
-source_suffix = '.rst'
+source_suffix = {'.rst': 'restructuredtext'}
 exclude_patterns = ['**/.#*',
                     '**/*~',
                     'start/quick-common.rst',
