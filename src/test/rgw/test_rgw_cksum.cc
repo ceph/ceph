ceph/ceph

commit ee03b5054147afcf6f174efe71e976394ede7715

rgw:cksum: implement crc64nvme and combined 32- and 64-bit CRCs
* internally compensate for at-rest byteswapped crc64 representation (e.g., before combine step)
* generalize Cksum crc api for 32bit and 64bit crcs, other cleanup
* prototype abstract cksum::Combiner workflow
* add support for forward and backward handling of composite vs full object checksums
  by marking the composites and the update flag day
* clean up checksum formatting and checksum type reporting
* add unit tests for Combiner interface
* validate and track requested checksum type (i.e., composite or full), plus
  unit test fixture for combinations of full matrix of cksum types
* doh.  GET/HEAD checksums are in headers
* add crcany license to COPYING
* return ChecksumType as header in GET/HEAD

    Found by Casey in review

* avoid fmt of char* null when no checksum header supplied

    A cksum_hdr_t(nullptr, nullptr) results in this case, and is intended,
    but its components obviously can't be presented to std::format unless
    cast to a safe pointer type.

* fail checksum mismatch with BadDigest

    When uploading an S3 object with an invalid checksum, the return code
    should be BadDigest to mirror more closely the AWS S3 implementation.
    See: https://docs.aws.amazon.com/AmazonS3/latest/userguide/checking-object-integrity.html

    Fixes: https://tracker.ceph.com/issues/70614

    Reported by Alex Wojno <awojno@bloomberg.net>

* fix comparison and display of composite checksums

A string comparision bounds error and a bitmask comparison
    error are fixed.

* fix build on centos9

On gcc(?13?) we can't declare rgw::cksum::FLAG_NONE
    and also rgw::cksum::Cksum::FlAG_NONE.

    SAD!!

* include <variant> invariantly

* fix checksum type return from complete-multipart

    This one is in the XML response

* aieee, don't leak Combiners

   Use unique_ptr to polymorphic type...correctly.

Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>

src/rgw/rgw_cksum.cc
@@ -1,0 +1,157 @@
+// -*- mode:C++; tab-width:8; c-basic-offset:2; indent-tabs-mode:t -*-
+// vim: ts=8 sw=2 smarttab ft=cpp
+
+/*
+ * Ceph - scalable distributed file system
+ *
+ * Copyright contributors to the Ceph project
+ *
+ * This is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software
+ * Foundation.  See file COPYING.
+ *
+ */
+
+#include "rgw_cksum.h"
+#include "rgw_cksum_digest.h"
+#include <cstdint>
+#include <memory>
+#include "rgw_crc_digest.h"
+
+extern "C" {
+#include "madler/crc64nvme.h"
+#include "madler/crc32iso_hdlc.h"
+#include "madler/crc32iscsi.h"
+#include "spdk/crc64.h"
+} // extern "C"
+
+namespace rgw::cksum {
+
+  std::optional<Cksum>
+  combine_crc_cksum(const Cksum& ck1, const Cksum& ck2, uintmax_t len1)
+  {
+    std::optional<rgw::cksum::Cksum> ck3;
+    if ((ck1.type != ck2.type) ||
+	!ck1.crc()) {
+      goto out;
+    }
+
+    switch(ck1.type) {
+    case cksum::Type::crc64nvme:
+      {
+	/* due to AWS (and other) convention, the at-rest
+	 * digest is byteswapped (on LE?);  restore the
+	 * defined byte order before combining */
+	auto cck1 =
+	  rgw::digest::byteswap(std::get<uint64_t>(*ck1.get_crc()));
+	auto cck2 =
+	  rgw::digest::byteswap(std::get<uint64_t>(*ck2.get_crc()));
+	/* madler crcany */
+	auto cck3 = crc64nvme_comb(cck1, cck2, len1);
+	/* and byteswap */
+	cck3 = rgw::digest::byteswap(cck3);
+	/* convert to a Cksum, no ascii armor */
+	ck3 = Cksum(ck1.type, (char*) &cck3, Cksum::CtorStyle::raw);
+      }
+      break;
+    case cksum::Type::crc32:
+    case cksum::Type::crc32c:
+      {
+	uint32_t cck3;
+	auto cck1 =
+	  rgw::digest::byteswap(std::get<uint32_t>(*ck1.get_crc()));
+	auto cck2 =
+	  rgw::digest::byteswap(std::get<uint32_t>(*ck2.get_crc()));
+	/* madler crcany */
+	switch (ck1.type) {
+	case cksum::Type::crc32:
+	  cck3 =  crc32iso_hdlc_comb(cck1, cck2, len1);
+	  break;
+	case cksum::Type::crc32c:
+	  cck3 =  crc32iscsi_comb(cck1, cck2, len1);
+	  break;
+	default:
+	  break;
+	}
+        /* and byteswap */
+	cck3 = rgw::digest::byteswap(cck3);
+	/* convert to a Cksum, no ascii armor */
+	ck3 = Cksum(ck1.type, (char*) &cck3, Cksum::CtorStyle::raw);
+      }
+      break;
+    default:
+      break;
+    };
+
+  out:
+    return ck3;
+  }
+
+  /* the checksum of the final object is a checksum (of the same type,
+   * presumably) of the concatenated checksum bytes of the parts, plus
+   * "-<num-parts>.  See
+   * https://docs.aws.amazon.com/AmazonS3/latest/userguide/checking-object-integrity.html#large-object-checksums
+   */
+  class DigestCombiner : public Combiner
+  {
+    DigestVariant dv;
+    Digest* digest{nullptr};
+
+  public:
+    DigestCombiner(cksum::Type t)
+      : Combiner(t),
+	dv(digest_factory(t)),
+	digest(get_digest(dv))
+    {}
+    virtual void append(const Cksum& cksum, uint64_t ignored) {
+      auto ckr = cksum.raw();
+      digest->Update((unsigned char *)ckr.data(), ckr.length());
+    }
+    virtual Cksum final() {
+      auto cksum = finalize_digest(digest, get_type());
+      cksum.flags |= Cksum::COMPOSITE_MASK;
+      return cksum;
+    }
+  }; /* Digest */
+
+  /* the checksum of the final object is an "ordinary" (full object) checksum,
+   * synthesized from the CRCs of the component checksums
+   */
+  class CRCCombiner : public Combiner
+  {
+    Cksum cksum;
+    bool once{false};
+  public:
+    CRCCombiner(cksum::Type t)
+      : Combiner(t)
+    {}
+    virtual void append(const Cksum& rhs, uint64_t part_size) {
+      if (! once) {
+	// save the first part checksum
+	cksum = rhs;
+	once = true;
+      } else {
+	// combine the next partial checksum into cksum
+	cksum = *combine_crc_cksum(cksum, rhs, part_size);
+      }
+    }
+    virtual Cksum final() {
+      cksum.flags |= Cksum::FULL_OBJECT_MASK;
+      return cksum;
+    }
+  }; /* CRCCombine */
+
+  std::unique_ptr<Combiner> CombinerFactory(cksum::Type t, uint16_t flags)
+  {
+    switch(t) {
+    case cksum::Type::crc32:
+    case cksum::Type::crc32c:
+    case cksum::Type::crc64nvme:
+      return std::unique_ptr<Combiner>(new CRCCombiner(t));
+    default:
+      return std::unique_ptr<Combiner>(new DigestCombiner(t));
+    };
+  }
+
+} // namespace rgw::cksum

src/rgw/rgw_cksum.h
@@ -17,12 +17,15 @@
 #include <boost/algorithm/string/predicate.hpp>
 #include <cstdint>
 #include <cstring>
+#include <memory>
 #include <optional>
 #include <stdint.h>
 #include <string>
 #include <string_view>
 #include <array>
 #include <iterator>
+#include <tuple>
+#include <variant>
 #include <boost/algorithm/string.hpp>
 #include "fmt/format.h"
 #include "common/armor.h"
@@ -48,8 +51,9 @@ namespace rgw { namespace cksum {
       crc64nvme,
   };
 
-  static constexpr uint16_t FLAG_NONE =      0x0000;
-  static constexpr uint16_t FLAG_AWS_CKSUM = 0x0001;
+  static constexpr uint16_t FLAG_NONE =       0x0000;
+  static constexpr uint16_t FLAG_AWS_CKSUM =  0x0001;
+  static constexpr uint16_t FLAG_CRC =        0x0002;
 
   class Desc
   {
@@ -84,30 +88,60 @@ namespace rgw { namespace cksum {
     static constexpr std::array<Desc, 9> checksums =
     {
       Desc(Type::none, "none", 0, FLAG_NONE),
-      Desc(Type::crc32, "crc32", 4, FLAG_AWS_CKSUM),
-      Desc(Type::crc32c, "crc32c", 4, FLAG_AWS_CKSUM),
+      Desc(Type::crc32, "crc32", 4, FLAG_AWS_CKSUM|FLAG_CRC),
+      Desc(Type::crc32c, "crc32c", 4, FLAG_AWS_CKSUM|FLAG_CRC),
       Desc(Type::xxh3, "xxh3", 8, FLAG_NONE),
       Desc(Type::sha1, "sha1", 20, FLAG_AWS_CKSUM),
       Desc(Type::sha256, "sha256", 32, FLAG_AWS_CKSUM),
       Desc(Type::sha512, "sha512", 64, FLAG_NONE),
       Desc(Type::blake3, "blake3", 32, FLAG_NONE),
-      Desc(Type::crc64nvme, "crc64nvme", 8, FLAG_AWS_CKSUM),
+      Desc(Type::crc64nvme, "crc64nvme", 8, FLAG_AWS_CKSUM|FLAG_CRC),
     };
 
     static constexpr uint16_t max_digest_size = 64;
     using value_type = std::array<unsigned char, max_digest_size>;
 
+    static constexpr uint16_t FLAG_CKSUM_NONE =           0x0000;
+    static constexpr uint16_t FLAG_V2 =             0x0001; // struct_v >= 2
+    static constexpr uint16_t FLAG_COMPOSITE =      0x0002;
+    static constexpr uint16_t FLAG_FULL_OBJECT =    0x0004;
+    static constexpr uint16_t FLAG_COMBINED =       0x0008;
+
+    static constexpr uint16_t COMPOSITE_MASK =
+      (FLAG_COMBINED|FLAG_COMPOSITE);
+    static constexpr uint16_t FULL_OBJECT_MASK =
+      (FLAG_COMBINED|FLAG_FULL_OBJECT);
+
     Type type;
     value_type digest;
+    uint16_t flags;
 
-    Cksum(Type _type = Type::none) : type(_type) {}
-    Cksum(Type _type, const char* _armored_text)
-      : type(_type) {
+    enum class CtorStyle : uint8_t
+    {
+      raw = 0,
+      from_armored,
+    };
+
+    Cksum(Type _type = Type::none)
+      : type(_type), flags(FLAG_V2)
+    {}
+
+    Cksum(Type _type, const char* _data, CtorStyle style)
+      : type(_type), flags(FLAG_V2)
+    {
       const auto& ckd = checksums[uint16_t(type)];
+      switch (style) {
+      case CtorStyle::from_armored:
       (void) ceph_unarmor((char*) digest.begin(),
 			  (char*) digest.begin() + ckd.digest_size,
-			  _armored_text,
-			  _armored_text + std::strlen(_armored_text));
+			  _data,
+			  _data + std::strlen(_data));
+      break;
+      case CtorStyle::raw:
+      default:
+	memcpy((char*) digest.data(), (char*) _data, ckd.digest_size);
+	break;
+      };
     }
 
     const char* type_string() const {
@@ -116,6 +150,28 @@ namespace rgw { namespace cksum {
 
     const bool aws() const {
       return (Cksum::checksums[uint16_t(type)]).aws();
+    }
+
+    const bool crc() const {
+      return (Cksum::checksums[uint16_t(type)]).flags & FLAG_CRC;
+    }
+
+    const bool v2() const {
+      return flags & FLAG_V2;
+    }
+
+    const bool composite() const {
+      /* treating COMPOSITE and FULL_OBJECT as flags has issues,
+       * as does not doing it;  note that we cannot rely on crc()
+       * to mean !COMPOSITE/FULL_OBJECT, as CRC32 and CRC32C
+       * were deployed as digest checksums in 2023 (so we must be
+       * prepared to find them on disk) and per AWS can still be
+       * constructed as digests.
+       *
+       * invariant: FLAG_COMPOSITE is a property of /combined checksums/;
+       * it propogates through encode/decode, but we expect only
+       * logical combination/Combiner to set it */
+      return ((flags & COMPOSITE_MASK) == COMPOSITE_MASK);
     }
 
     std::string aws_name() const {
@@ -164,27 +220,63 @@ namespace rgw { namespace cksum {
     }
 
     std::string to_string() const  {
-      std::string hs;
       const auto& ckd = checksums[uint16_t(type)];
       return fmt::format("{{{}}}{}", ckd.name, to_base64());
     }
 
+
+    using crc_type = std::variant<uint32_t, uint64_t>;
+
+    std::optional<crc_type> get_crc() const {
+      std::optional<crc_type> res;
+      const auto& ckd = checksums[uint16_t(type)];
+      if (!crc()) {
+	goto out;
+      }
+      switch(ckd.digest_size) {
+      case 4:
+	{
+	  uint32_t crc;
+	  memcpy(&crc, (char*) digest.data(), sizeof(crc));
+	  res = crc;
+	}
+	break;
+      case 8:
+	{
+	  uint64_t crc;
+	  memcpy(&crc, (char*) digest.data(), sizeof(crc));
+	  res = crc;
+	}
+	break;
+      default:
+	break;
+      }
+    out:
+      return res;
+    }
+
     void encode(buffer::list& bl) const {
       const auto& ckd = checksums[uint16_t(type)];
-      ENCODE_START(1, 1, bl);
+      ENCODE_START(2, 1, bl);
       encode(uint16_t(type), bl);
       encode(ckd.digest_size, bl);
       bl.append((char*)digest.data(), ckd.digest_size);
+      encode(flags, bl);
       ENCODE_FINISH(bl);
     }
 
     void decode(bufferlist::const_iterator& p) {
-      DECODE_START(1, p);
+      DECODE_START(2, p);
       uint16_t tt;
       decode(tt, p);
       type = cksum::Type(tt);
       decode(tt, p); /* <= max_digest_size */
       p.copy(tt, (char*)digest.data());
+      if (struct_v < 2) {
+	flags = 0;
+      } else {
+	decode(flags, p);
+      }
       DECODE_FINISH(p);
     }
   }; /* Cksum */
@@ -192,8 +284,8 @@ namespace rgw { namespace cksum {
 
   static inline const std::optional<rgw::cksum::Cksum> no_cksum{std::nullopt};
 
+  /* XXX would like std::string view */
   static inline std::string to_string(const Type type) {
-    std::string hs;
     const auto& ckd = Cksum::checksums[uint16_t(type)];
     return ckd.name;
   }
@@ -206,6 +298,22 @@ namespace rgw { namespace cksum {
     }
     return Type::none;
   } /* parse_cksum_type */
+
+  static inline uint16_t cksum_flags_of(Type t) {
+    switch(t) {
+    case cksum::Type::none:
+      return Cksum::FLAG_CKSUM_NONE;
+      break;
+    case cksum::Type::crc64nvme:
+    case cksum::Type::crc32:
+    case cksum::Type::crc32c:
+      return Cksum::FLAG_FULL_OBJECT;
+      break;
+    default:
+      break;
+    };
+    return Cksum::FLAG_COMPOSITE;
+  } /* cksum_flags_of */
 
   static inline Type parse_cksum_type_hdr(const std::string_view hdr_name) {
     auto pos = hdr_name.find("x-amz-checksum-", 0);
@@ -224,5 +332,70 @@ namespace rgw { namespace cksum {
     return hdr_name == "x-amz-checksum-algorithm" ||
       parse_cksum_type_hdr(hdr_name) != Type::none;
   } /* is_cksum_hdr */
+
+
+  using PermittedCksumResult
+  = std::tuple<bool, const std::string, const char*>;
+
+  static inline PermittedCksumResult
+  permitted_cksum_algo_and_type(Type type, uint16_t cksum_flags) {
+    if (type == Type::none) {
+      return PermittedCksumResult(true, "", "");
+    }
+    if (cksum_flags & Cksum::FLAG_COMPOSITE) {
+      if (type == Type::crc64nvme) {
+	return PermittedCksumResult(false, to_string(type), "COMPOSITE");
+      }
+      return PermittedCksumResult(true, to_string(type), "COMPOSITE");
+    }
+    /* FULL_OBJECT */
+    const auto& ckd = Cksum::checksums[uint16_t(type)];
+    return
+      PermittedCksumResult(
+	(ckd.flags & cksum::FLAG_CRC), to_string(type), "FULL_OBJECT");
+  }
+
+  std::optional<Cksum>
+  combine_crc_cksum(const Cksum& ck1, const Cksum& ck2, uintmax_t len2);
+
+  /* wrap combine/digest checksums */
+  class Combiner
+  {
+    cksum::Type type;
+  public:
+    Combiner(cksum::Type t)
+      : type(t) {}
+
+    cksum::Type get_type() const { return type; }
+
+    virtual void append(const Cksum& cksum, uint64_t part_size) = 0;
+    virtual Cksum final() = 0;
+    virtual ~Combiner() {}
+  }; /* abstract Combiner */
+
+  /* choose type-correct Combiner */
+  std::unique_ptr<Combiner>
+  CombinerFactory(cksum::Type t, uint16_t flags);
+
+  using ChecksumTypeResult = std::tuple<uint16_t, const char*>;
+
+  static inline ChecksumTypeResult
+  get_checksum_type(const Cksum& cksum, bool is_multipart) {
+    /* non-multipart checksum */
+    if (! is_multipart) {
+      return ChecksumTypeResult(Cksum::FLAG_CKSUM_NONE, "FULL_OBJECT");
+    }
+    /* multipart cksum */
+    if (cksum.v2()) [[likely]] {
+      if (! cksum.composite()) {
+	return ChecksumTypeResult(Cksum::FLAG_CKSUM_NONE, "FULL_OBJECT");
+      }
+      /* composite */
+      /* fall through */
+    }
+    /* cksum predates the 2025 update that introduced CRC combining,
+     * so it's a "composite" checksum regardless of the algorithm */
+    return ChecksumTypeResult(Cksum::FLAG_COMPOSITE, "COMPOSITE");
+  } /* get_checksum_type */
 
 }} /* namespace */

src/rgw/rgw_rest_s3.cc
@@ -509,12 +509,29 @@ int RGWGetObj_ObjStore_S3::send_response_data(bufferlist& bl, off_t bl_ofs,
 	try {
 	  rgw::cksum::Cksum cksum;
 	  decode(cksum, i->second);
-	  if (multipart_parts_count && multipart_parts_count > 0) {
+	  auto cksum_type =
+	    rgw::cksum::get_checksum_type(cksum,
+	      (multipart_parts_count && multipart_parts_count > 0) /* is_multipart */);
+	  if (std::get<0>(cksum_type) == rgw::cksum::Cksum::FLAG_COMPOSITE) {
+	    /* cksum was computed with a digest algorithm, or predates the 2025
+	       update that introduced CRC combining */
+	    ldpp_dout_fmt(this, 16,
+			  "INFO: {} ChecksumMode==ENABLED element-name {} value {}-{}",
+			  __func__, cksum.element_name(), cksum.to_armor(),
+			  *multipart_parts_count);
 	    dump_header(s, cksum.header_name(),
 			fmt::format("{}-{}", cksum.to_armor(), *multipart_parts_count));
 	  } else {
-	    dump_header(s, cksum.header_name(), cksum.to_armor());
+	    /* a full object checksum, if multipart, because the checksum is CRC family */
+	    auto elt_name = cksum.element_name();
+	    auto armored_cksum = cksum.to_armor();
+
+	    ldpp_dout_fmt(this, 16,
+			  "INFO: {} ChecksumMode==ENABLED element-name {} value {}",
+			  __func__, cksum.element_name(), cksum.to_armor());
+	    dump_header(s, cksum.header_name(), armored_cksum);
 	  }
+	  dump_header(s, "x-amz-checksum-type", std::get<1>(cksum_type));
 	}  catch (buffer::error& err) {
 	  ldpp_dout(this, 0) << "ERROR: failed to decode rgw::cksum::Cksum"
 			     << dendl;
@@ -2877,6 +2894,7 @@ void RGWPutObj_ObjStore_S3::send_response()
       dump_header_if_nonempty(s, "x-amz-version-id", version_id);
       dump_header_if_nonempty(s, "x-amz-expiration", expires);
       if (cksum && cksum->aws()) {
+	dump_header(s, "x-amz-checksum-type", "FULL_OBJECT");
 	dump_header(s, cksum->header_name(), cksum->to_armor());
       }
       for (auto &it : crypt_http_responses)
@@ -2885,7 +2903,8 @@ void RGWPutObj_ObjStore_S3::send_response()
       dump_errno(s);
       dump_header_if_nonempty(s, "x-amz-version-id", version_id);
       dump_header_if_nonempty(s, "x-amz-expiration", expires);
-      if (cksum) {
+      if (cksum && cksum->aws()) {
+	dump_header(s, "x-amz-checksum-type", "FULL_OBJECT");
 	dump_header(s, cksum->header_name(), cksum->to_armor());
       }
       end_header(s, this, to_mime_type(s->format));
@@ -4006,12 +4025,20 @@ void RGWGetObjAttrs_ObjStore_S3::send_response()
 	  rgw::cksum::Cksum cksum;
 	  auto bliter = iter->second.cbegin();
 	  cksum.decode(bliter);
-          if (multipart_parts_count && multipart_parts_count > 0) {
+	  auto cksum_type =
+	    rgw::cksum::get_checksum_type(cksum,
+	      (multipart_parts_count && multipart_parts_count > 0) /* is_multipart */);
+	  if (std::get<0>(cksum_type) == rgw::cksum::Cksum::FLAG_COMPOSITE) {
+	    /* cksum was computed with a digest algorithm, or predates the 2025
+	       update that introduced CRC combining */
 	    s->formatter->dump_string(cksum.element_name(),
-		fmt::format("{}-{}", cksum.to_armor(), *multipart_parts_count));
+				      fmt::format("{}-{}", cksum.to_armor(),
+						  *multipart_parts_count));
 	  } else {
+	    /* a full object checksum, if multipart, because the checksum is CRC family */
 	    s->formatter->dump_string(cksum.element_name(), cksum.to_armor());
 	  }
+	  s->formatter->dump_string("ChecksumType", std::get<1>(cksum_type));
 	} catch (buffer::error& err) {
 	  ldpp_dout(this, 0)
 	    << "ERROR: could not decode stored cksum, caught buffer::error" << dendl;
@@ -4053,6 +4080,7 @@ void RGWGetObjAttrs_ObjStore_S3::send_response()
 	      s->formatter->dump_int("PartNumber", part.part_number);
 	      s->formatter->dump_unsigned("Size", part.part_size);
 	      if (part.cksum.type != rgw::cksum::Type::none) {
+		/* parts always have a non-digest checksum */
 		s->formatter->dump_string(part.cksum.element_name(), part.cksum.to_armor());
 	      }
 	      s->formatter->close_section(); /* Part */
@@ -4467,13 +4495,24 @@ int RGWInitMultipart_ObjStore_S3::get_params(optional_yield y)
     return -ERR_INVALID_REQUEST;
   }
 
-  auto algo_hdr = rgw::putobj::cksum_algorithm_hdr(*(s->info.env));
-  if (algo_hdr.second) {
-    cksum_algo = rgw::cksum::parse_cksum_type(algo_hdr.second);
+  /* checksums */
+  auto checksum_type_hdr =
+    s->info.env->get_optional("HTTP_X_AMZ_CHECKSUM_TYPE");
+
+  /* composite or "full object" */
+  cksum_algo = putobj::multipart_cksum_algo(*(s->info.env));
+  cksum_flags = putobj::parse_cksum_flags(cksum_algo, checksum_type_hdr);
+
+  auto aok = cksum::permitted_cksum_algo_and_type(cksum_algo, cksum_flags);
+  if (! std::get<0>(aok)) {
+    ldpp_dout_fmt(this, 5,
+		  "ERROR: {} checksum type {} not compatible with checksum algorithm {}",
+		  __func__, std::get<1>(aok), std::get<2>(aok));
+    return -ERR_INVALID_REQUEST;
   }
 
   return 0;
-}
+} /* RGWInitMultipart_ObjStore_S3::get_params() */
 
 void RGWInitMultipart_ObjStore_S3::send_response()
 {
@@ -4555,8 +4594,11 @@ void RGWCompleteMultipart_ObjStore_S3::send_response()
     s->formatter->dump_string("Bucket", s->bucket_name);
     s->formatter->dump_string("Key", s->object->get_name());
     s->formatter->dump_string("ETag", etag);
-    if (armored_cksum) {
+    if (armored_cksum) [[likely]] {
+      auto cksum_type
+	= rgw::cksum::get_checksum_type(*cksum, true /* is_multipart */);
       s->formatter->dump_string(cksum->element_name(), *armored_cksum);
+      s->formatter->dump_string("ChecksumType", std::get<1>(cksum_type));
     }
     s->formatter->close_section();
     rgw_flush_formatter_and_reset(s, s->formatter);

commit 372886ab7b2bc8424fe2a26fc2b2b461e9b97c4f

rgw: default MPU CRC32/CRC32C checksum-type to COMPOSITE
AWS CreateMultipartUpload treats algorithm-only CRC32/CRC32C as
COMPOSITE; Ceph used FULL_OBJECT and CombinerFactory always
CRC-combined, so clients like minio-go failed CompleteMPU with
BadDigest. Honor COMPOSITE via DigestCombiner, keep FULL_OBJECT
CRC combine, and emit x-amz-checksum-type on CreateMPU.

Per the AWS checksum-type table, CRC32/CRC32C support both COMPOSITE
and FULL_OBJECT, while CRC64NVME is full-object only:
https://docs.aws.amazon.com/AmazonS3/latest/userguide/checking-object-integrity-upload.html#ChecksumTypes-Uploads

Fixes: http://tracker.ceph.com/issues/79572
Fixes: ee03b5054147
Signed-off-by: ramin.najarbashi <ramin.najarbashi@gmail.com>

src/rgw/rgw_cksum.cc
@@ -147,9 +147,16 @@ namespace rgw::cksum {
   std::unique_ptr<Combiner> CombinerFactory(cksum::Type t, uint16_t flags)
   {
     switch(t) {
+    case cksum::Type::crc64nvme:
+      /* full-object only */
+      return std::unique_ptr<Combiner>(new CRCCombiner(t));
     case cksum::Type::crc32:
     case cksum::Type::crc32c:
-    case cksum::Type::crc64nvme:
+      /* Honor multipart checksum-type: COMPOSITE digests part checksums;
+       * FULL_OBJECT (or legacy callers with no type flag) CRC-combines. */
+      if (flags & Cksum::FLAG_COMPOSITE) {
+	return std::unique_ptr<Combiner>(new DigestCombiner(t));
+      }
       return std::unique_ptr<Combiner>(new CRCCombiner(t));
     default:
       return std::unique_ptr<Combiner>(new DigestCombiner(t));

src/rgw/rgw_cksum.h
@@ -314,9 +314,15 @@ namespace rgw { namespace cksum {
       return Cksum::FLAG_CKSUM_NONE;
       break;
     case cksum::Type::crc64nvme:
+      /* AWS: CRC64NVME is full-object only for multipart. */
+      return Cksum::FLAG_FULL_OBJECT;
+      break;
     case cksum::Type::crc32:
     case cksum::Type::crc32c:
-      return Cksum::FLAG_FULL_OBJECT;
+      /* AWS CreateMultipartUpload defaults CRC32/CRC32C to COMPOSITE
+       * when x-amz-checksum-type is omitted. Match that so clients that
+       * send only x-amz-checksum-algorithm (e.g. minio-go) validate. */
+      return Cksum::FLAG_COMPOSITE;
       break;
     default:
       break;

src/rgw/rgw_rest_s3.cc
@@ -4788,6 +4788,11 @@ void RGWInitMultipart_ObjStore_S3::send_response()
   }
   if (cksum_algo != rgw::cksum::Type::none) {
     dump_header(s, "x-amz-checksum-algorithm", to_uc_string(cksum_algo));
+    if (cksum_flags & rgw::cksum::Cksum::FLAG_COMPOSITE) {
+      dump_header(s, "x-amz-checksum-type", "COMPOSITE");
+    } else if (cksum_flags & rgw::cksum::Cksum::FLAG_FULL_OBJECT) {
+      dump_header(s, "x-amz-checksum-type", "FULL_OBJECT");
+    }
   }
   end_header(s, this, to_mime_type(s->format));
   if (op_ret == 0) {

src/test/rgw/test_rgw_cksum.cc
@@ -835,6 +835,73 @@ TEST(RGWCksum, Combiner1)
   }
 } /* Combiner1 */
 
+TEST(RGWCksum, FlagsOfDefaults)
+{
+  /* AWS CreateMultipartUpload: algorithm-only CRC32/CRC32C → COMPOSITE;
+   * CRC64NVME → FULL_OBJECT; cryptographic digests → COMPOSITE. */
+  ASSERT_EQ(cksum_flags_of(cksum::Type::crc32), Cksum::FLAG_COMPOSITE);
+  ASSERT_EQ(cksum_flags_of(cksum::Type::crc32c), Cksum::FLAG_COMPOSITE);
+  ASSERT_EQ(cksum_flags_of(cksum::Type::crc64nvme), Cksum::FLAG_FULL_OBJECT);
+  ASSERT_EQ(cksum_flags_of(cksum::Type::sha256), Cksum::FLAG_COMPOSITE);
+  ASSERT_EQ(cksum_flags_of(cksum::Type::none), Cksum::FLAG_CKSUM_NONE);
+}
+
+TEST(RGWCksum, CRC32C_CombinerComposite)
+{
+  using std::get;
+
+  auto t = cksum::Type::crc32c;
+  auto cksums = mpu_checksum_helper(t, 0);
+  auto& [cksum1, cksum2, cksum3] = cksums;
+
+  /* FULL_OBJECT (or no type flag): CRC-combine equals whole-object digest */
+  auto full = rgw::cksum::CombinerFactory(t, Cksum::FLAG_FULL_OBJECT);
+  ASSERT_TRUE(full);
+  full->append(cksum1, dolor.length());
+  full->append(cksum2, lorem.length());
+  auto cksum_full = full->final();
+  ASSERT_EQ(cksum3.to_armor(), cksum_full.to_armor());
+  ASSERT_TRUE(cksum_full.flags & Cksum::FLAG_FULL_OBJECT);
+
+  /* COMPOSITE: digest of part checksums — must differ from full-object */
+  auto composite = rgw::cksum::CombinerFactory(t, Cksum::FLAG_COMPOSITE);
+  ASSERT_TRUE(composite);
+  composite->append(cksum1, dolor.length());
+  composite->append(cksum2, lorem.length());
+  auto cksum_comp = composite->final();
+  ASSERT_NE(cksum3.to_armor(), cksum_comp.to_armor());
+  ASSERT_TRUE(cksum_comp.composite());
+
+  /* Default algorithm-only flags match COMPOSITE combiner */
+  auto def_flags = cksum_flags_of(t);
+  ASSERT_EQ(def_flags, Cksum::FLAG_COMPOSITE);
+  auto defcmb = rgw::cksum::CombinerFactory(t, def_flags);
+  defcmb->append(cksum1, dolor.length());
+  defcmb->append(cksum2, lorem.length());
+  ASSERT_EQ(cksum_comp.to_armor(), defcmb->final().to_armor());
+}
+
+TEST(RGWCksum, CRC32_CombinerComposite)
+{
+  using std::get;
+
+  auto t = cksum::Type::crc32;
+  auto cksums = mpu_checksum_helper(t, 0);
+  auto& [cksum1, cksum2, cksum3] = cksums;
+
+  auto full = rgw::cksum::CombinerFactory(t, Cksum::FLAG_FULL_OBJECT);
+  full->append(cksum1, dolor.length());
+  full->append(cksum2, lorem.length());
+  ASSERT_EQ(cksum3.to_armor(), full->final().to_armor());
+
+  auto composite = rgw::cksum::CombinerFactory(t, Cksum::FLAG_COMPOSITE);
+  composite->append(cksum1, dolor.length());
+  composite->append(cksum2, lorem.length());
+  auto cksum_comp = composite->final();
+  ASSERT_NE(cksum3.to_armor(), cksum_comp.to_armor());
+  ASSERT_TRUE(cksum_comp.composite());
+}
+
 using cksum_4tuple
     = std::tuple<rgw::cksum::Cksum, rgw::cksum::Cksum, rgw::cksum::Cksum,
 		 rgw::cksum::Cksum>;