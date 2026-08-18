828639c1116b748e33dad1a88c11db7acdb5495e
Jamie Pryde <jamiepry@uk.ibm.com>
2026-08-18 15:44:13 +0100

    mon: Roll back pending_inc on prepare_new_pool failure
        Any error return in prepare_new_pool() after the new pool has been
        inserted into pending_inc.new_pools and pending_inc.new_pool_max has
        been incremented, but before pending_inc.new_pool_names is set, leaves
        pending_inc in an inconsistent state: the pool ID is present in
        new_pools but absent from new_pool_names.
    
        On the next encode_pending() call, apply_incremental() adds the unnamed
        pool to tmp.pools but not to tmp.pool_name. check_health() then
        iterates pools and calls get_pool_name() on the unnamed ID, hitting
        ceph_assert(i != pool_name.end()) and crashing the monitor.
    
        Fix by adding an abort_pool lambda that erases the pool from
        pending_inc.new_pools and decrements pending_inc.new_pool_max, and
        calling it before every error return between the pool allocation and
        the new_pool_names assignment.
    
        Fixes: https://tracker.ceph.com/issues/79574
        Signed-off-by: Jamie Pryde <jamiepry@uk.ibm.com>
        Assisted-by: IBM Bob 2.0.2

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -8430,6 +8430,14 @@ int OSDMonitor::prepare_new_pool(string& name,
   if (-1 == pending_inc.new_pool_max)
     pending_inc.new_pool_max = osdmap.pool_max;
   int64_t pool = ++pending_inc.new_pool_max;
+  // Rollback helper: Any error after this point would result in the pool ID
+  // being present in pending_inc.new_pools but not in new_pool_names. This
+  // would cause the next call to check_health() -> get_pool_name() to crash on
+  // the unnamed pool. Must abort_pool before returning with an error.
+  auto abort_pool = [&] {
+    pending_inc.new_pools.erase(pool);
+    --pending_inc.new_pool_max;
+  };
   pg_pool_t empty;
   pg_pool_t *pi = pending_inc.get_new_pool(pool, &empty);
   pi->create_time = ceph_clock_now();
@@ -8519,6 +8527,7 @@ int OSDMonitor::prepare_new_pool(string& name,
         pi->ec_coding_shard_count = erasure_code->get_coding_chunk_count();
       } else {
         *ss << "get_erasure_code failed: " << tmp.str();
+        abort_pool();
         return -EINVAL;
       }
       pi->erasure_code_profile = erasure_code_profile;
@@ -8555,6 +8564,7 @@ int OSDMonitor::prepare_new_pool(string& name,
       if (auto r = enable_pool_ec_optimizations(*pi, true); !r) {
         // for Crimson - failure is not an option
         *ss << r.error().message;
+        abort_pool();
         return r.error().error;
       }
     } else {
