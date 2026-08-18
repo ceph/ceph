b5adc2b3efbbc06095107e7c1468b40bc0f5334d
Xuehan Xu <xuxuehan@qianxin.com>
2026-08-18 13:37:43 +0800

    crimson/os/seastore/device: all devices of the same OSD share the same magic
    Signed-off-by: Xuehan Xu <xuxuehan@qianxin.com>

--- src/crimson/os/seastore/seastore.cc
+++ src/crimson/os/seastore/seastore.cc
@@ -829,6 +829,7 @@ Device::access_ertr::future<> SeaStore::_mkfs(uuid_d new_osd_fsid)
   ceph_assert(!root.empty());
 
   cache_device_set_t cds;
+  magic_t magic = (magic_t)std::rand();
   for (auto &dev : cache_devices) {
     auto dtype = dev->get_device_type();
     auto btype = dev->get_backend_type();
@@ -849,7 +850,7 @@ Device::access_ertr::future<> SeaStore::_mkfs(uuid_d new_osd_fsid)
     DEBUG("creating primary device");
     co_await dev->mkfs(
       device_config_t::create_data(
-        new_osd_fsid, id, d_type, b_type, cds));
+        new_osd_fsid, id, d_type, b_type, cds, magic));
   }
 
   DEBUG("mounting {} cache_devices", cache_devices.size());
