ceph/ceph

commit 24dc65ef20defe9761036f10527e7c5ccbe057bd

cephadm: Add retries to OSD key rotation
There may be a delay between sending the OSD SIGTERM
and the device actually being available for the
ceph-bluestore-tool to access it to rotate the keyring

Change-Id: 69b9ae64-a553-4df8-b9b2-8a424a76c152
Signed-off-by: Adam King <adking@redhat.com>

src/cephadm/cephadmlib/daemons/ceph.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import json
+import time
 
 from typing import Any, Dict, List, Optional, Tuple, Union
 
@@ -506,6 +507,9 @@ echo "$DM_CRYPT_KEY" | cryptsetup luksOpen $LV_PATH $DEV_NAME
                     helper_script_path: '/tmp/cryptsetup_action.sh',
                 },
             )
+            logger.info(
+                f'Opening osd.{self.identity.daemon_id} with crypsetup'
+            )
             out, err, ret = call(
                 ctx,
                 cryptsetup_open_container.run_cmd(),
@@ -535,17 +539,31 @@ echo "$DM_CRYPT_KEY" | cryptsetup luksOpen $LV_PATH $DEV_NAME
             ],
             volume_mounts={'/dev': '/dev'},
         )
-        out, err, ret = call(
-            ctx,
-            bluestore_tool_container.run_cmd(),
-            verbosity=CallVerbosity.QUIET_UNLESS_ERROR,
+        logger.info(
+            f'Rotating osd.{self.identity.daemon_id} key with ceph-bluestore-tool'
         )
-        if ret:
-            raise Error(
-                'Got error rotating osd keyring using ceph-bluestore-tool\n'
-                f'Out:{out}\n'
-                f'Err:{err}'
+        # The OSD may take some time to shutdown fully and make the device
+        # available for the key rotation
+        for i in [2, 5, 10, 30, 0]:
+            if not i:
+                break
+            out, err, ret = call(
+                ctx,
+                bluestore_tool_container.run_cmd(),
+                verbosity=CallVerbosity.VERBOSE,
             )
+            if ret:
+                if not i:
+                    raise Error(
+                        'Got error rotating osd keyring using ceph-bluestore-tool\n'
+                        f'Out:{out}\n'
+                        f'Err:{err}'
+                    )
+                logger.info(f'Got issue rotating osd keyring using ceph-bluestore-tool:\n{out}\n{err}\nRetrying in {i} seconds')
+                time.sleep(i)
+            else:
+                logger.info(f'Successfully rotated osd.{self.identity.daemon_id} keyring')
+                break
 
         if encrypted:
             cryptsetup_close_container = CephContainer(
@@ -572,11 +590,15 @@ echo "$DM_CRYPT_KEY" | cryptsetup luksOpen $LV_PATH $DEV_NAME
                     f'Out:{out}\n'
                     f'Err:{err}'
                 )
+
+        call(ctx, ['systemctl', 'reset-failed', self.identity.unit_name],
+             verbosity=CallVerbosity.QUIET_UNLESS_ERROR)
         call(
             ctx,
-            ['systemctl', 'restart', self.identity.unit_name],
+            ['systemctl', 'start', self.identity.unit_name],
             verbosity=CallVerbosity.QUIET_UNLESS_ERROR,
         )
+
 
 @register_daemon_form
 class Crash(Ceph):

commit 6b206bc281c05b773b3e24c11b248a45c86ffed4

cephadm: black-format OSD key rotation code in daemons/ceph.py
The OSD keyring rotation changes from the CVE-2025-30156 cephx merge
(c9bc85adec3 "cephadm: restart OSD systemd unit after key rotation",
24dc65ef20d "cephadm: Add retries to OSD key rotation") added lines to
cephadmlib/daemons/ceph.py that are not formatted the way
`tox -e check-black` (black>=23,<24, -l78) expects, so run-tox-cephadm
fails on every PR now that the py3 env passes again:

  would reformat /ceph/src/cephadm/cephadmlib/daemons/ceph.py
  1 file would be reformatted, 50 files would be left unchanged.

This is the output of `tox -e format-black` (black 23.12.1) on that
file; no functional change.

Fixes: 24dc65ef20defe9761036f10527e7c5ccbe057bd
Signed-off-by: David Galloway <david.galloway@ibm.com>
Assisted-by: Claude Fable 5 (claude-fable-5)

src/cephadm/cephadmlib/daemons/ceph.py
@@ -422,7 +422,8 @@ class OSD(Ceph):
         osd_lv_data = osd_bluestore_data.get(self.identity.daemon_id, [])
         if not osd_lv_data:
             logger.info(
-                'No lv data found for OSD.%s, checking for raw OSD', self.identity.daemon_id
+                'No lv data found for OSD.%s, checking for raw OSD',
+                self.identity.daemon_id,
             )
             c_v_container = CephContainer(
                 ctx,
@@ -446,8 +447,13 @@ class OSD(Ceph):
             )
             raw_list_data = json.loads(out)
             for osd_data in raw_list_data.values():
-                if str(osd_data.get('osd_id', -1)) == str(self.identity.daemon_id):
-                    logger.info('Found data for osd.%s in raw list', str(self.identity.daemon_id))
+                if str(osd_data.get('osd_id', -1)) == str(
+                    self.identity.daemon_id
+                ):
+                    logger.info(
+                        'Found data for osd.%s in raw list',
+                        str(self.identity.daemon_id),
+                    )
                     # misnomer since this isn't an LV, but it is the path
                     # we need to pass to the bluestore tool
                     lv_path = osd_data.get('device', None)
@@ -559,10 +565,14 @@ echo "$DM_CRYPT_KEY" | cryptsetup luksOpen $LV_PATH $DEV_NAME
                         f'Out:{out}\n'
                         f'Err:{err}'
                     )
-                logger.info(f'Got issue rotating osd keyring using ceph-bluestore-tool:\n{out}\n{err}\nRetrying in {i} seconds')
+                logger.info(
+                    f'Got issue rotating osd keyring using ceph-bluestore-tool:\n{out}\n{err}\nRetrying in {i} seconds'
+                )
                 time.sleep(i)
             else:
-                logger.info(f'Successfully rotated osd.{self.identity.daemon_id} keyring')
+                logger.info(
+                    f'Successfully rotated osd.{self.identity.daemon_id} keyring'
+                )
                 break
 
         if encrypted:
@@ -591,8 +601,11 @@ echo "$DM_CRYPT_KEY" | cryptsetup luksOpen $LV_PATH $DEV_NAME
                     f'Err:{err}'
                 )
 
-        call(ctx, ['systemctl', 'reset-failed', self.identity.unit_name],
-             verbosity=CallVerbosity.QUIET_UNLESS_ERROR)
+        call(
+            ctx,
+            ['systemctl', 'reset-failed', self.identity.unit_name],
+            verbosity=CallVerbosity.QUIET_UNLESS_ERROR,
+        )
         call(
             ctx,
             ['systemctl', 'start', self.identity.unit_name],