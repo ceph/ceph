2ffed1ff70f89e7aa0cf8e7d9a367f7795f7c48f
Dnyaneshwari Talwekar <dtalwekar@li-4c4c4544-0038-3510-8056-b5c04f473234.ibm.com>
2026-08-20 18:44:30 +0530

    mgr/dashboard: Storage class details view missing location_constraint field
    Fixes: https://tracker.ceph.com/issues/79663
    
    Signed-off-by: Dnyaneshwari Talwekar <dtalweka@redhat.com>
    (cherry picked from commit 62feba4b25c0906879728cb0b6380a696b0faebc)

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/models/rgw-storage-class.model.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/models/rgw-storage-class.model.ts
@@ -50,6 +50,7 @@ export interface StorageClassDetails {
   glacier_restore_tier_type?: string;
   read_through_restore_days?: number;
   restore_storage_class?: string;
+  location_constraint?: string;
   retain_head_object?: boolean;
   acls?: ACL[];
   acl_mappings?: ACL[];
@@ -116,6 +117,7 @@ export interface S3Details {
   storage_class: string;
   target_path: string;
   target_storage_class: string;
+  location_constraint?: string;
   region: string;
   secret: string;
   multipart_min_part_size: number;
@@ -155,6 +157,7 @@ export interface PlacementTarget {
     restore_storage_class?: string;
     read_through_restore_days?: number;
     target_storage_class?: string;
+    location_constraint?: string;
     acls?: ACL[];
   };
   storage_class?: string;
@@ -190,6 +193,7 @@ export interface TextLabels {
   restoreDaysText: string;
   readthroughrestoreDaysText: string;
   restoreStorageClassText: string;
+  locationConstraintText: string;
 }
 
 export const CLOUD_TIER_REQUIRED_FIELDS = [
@@ -277,6 +281,8 @@ export const RESTORE_DAYS_TEXT = $localize`Refers to number of days to the objec
 export const READTHROUGH_RESTORE_DAYS_TEXT = $localize`The days for which objects restored via read-through are retained.`;
 
 export const RESTORE_STORAGE_CLASS_TEXT = $localize`The storage class to which object data is to be restored.`;
+
+export const LOCATION_CONSTRAINT_TEXT = $localize`The region constraint for the target bucket on the remote S3 endpoint. For AWS, set this only if the region is other than US East (us-east-1).`;
 
 export const ZONEGROUP_TEXT = $localize`A Zonegroup is a logical grouping of one or more zones that share the same data
                   and metadata, allowing for multi-site replication and geographic distribution of
