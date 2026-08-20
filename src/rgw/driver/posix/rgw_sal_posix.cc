49b3d62090d7bc1cab52a245c7807a4b4aa337e2
Nithya Balachandran <nithya.balachandran@ibm.com>
2026-08-20 11:30:06 +0000

    rgw/posix: create delete marker if obj does not exist
    Creates a delete marker when delete is called on a versioned bucket
    even if the key does not exist.
    
    Signed-off-by: Nithya Balachandran <nithya.balachandran@ibm.com>

--- src/rgw/driver/posix/rgw_sal_posix.cc
+++ src/rgw/driver/posix/rgw_sal_posix.cc
@@ -2019,6 +2019,8 @@ int VersionedDirectory::remove(const DoutPrefixProvider* dpp, optional_yield y,
     return ret;
 
   if (instance_id.empty()) {
+
+#if 0
     /* Check if directory is empty */
     ret = for_each(dpp, [](const char *n) {
       return -ENOENT;
@@ -2028,6 +2030,7 @@ int VersionedDirectory::remove(const DoutPrefixProvider* dpp, optional_yield y,
       /* We're empty, nuke us */
       return Directory::remove(dpp, y, /*delete_children=*/true, result);
     }
+#endif
 
     /* Add a delete marker */
     std::unique_ptr<File> f;
@@ -4171,6 +4174,7 @@ int POSIXObject::delete_object(const DoutPrefixProvider* dpp,
   }
 
   int ret = stat(dpp);
+/*
   if (ret < 0) {
       if (ret == -ENOENT) {
 	// Nothing to do
@@ -4178,10 +4182,29 @@ int POSIXObject::delete_object(const DoutPrefixProvider* dpp,
       }
       return ret;
   }
-  ret = ent->remove(dpp, y, /*delete_children=*/false, &del_result);
+*/
 
   cls_rgw_obj_key key;
   get_key().get_index_key(&key);
+
+  if (ret == -ENOENT) {
+    if (!versioned() || !key.instance.empty() ) {
+      // Nothing to do
+      return 0;
+    }
+    // A delete marker must be created even if the key does not exist
+    // Create the versioned directory and create a delete marker
+    ret = make_ent(ObjectType::VERSIONED);
+    if (ret < 0) {
+      return ret;
+    }
+    ret = ent->create(dpp, nullptr, false);
+    if (ret < 0) {
+      ldpp_dout(dpp, 0) << "ERROR: could not create " << ent->get_name() << dendl;
+      return ret;
+    }
+  }
+  ret = ent->remove(dpp, y, /*delete_children=*/false, &del_result);
 
   driver->get_bucket_cache()->remove_entry(dpp, b->get_name(), key);
 
