ab148c2c96fb9cf0af3554ee498d19c7993bb577
Nizamudeen A <nia@redhat.com>
2026-08-21 13:49:12 +0530

    mgr/dashboard: bump angular to 20.3.29
    Migrating to new esbuild builder (we were using old webpack builder),
    and this could improve the build times and modernize the application.
    Also pretty restrictive on the angular practices so the build will make
    sure we don't fall short on the best practices.
    
    https://medium.com/@fahrigedik/angular-application-builder-transitioning-from-webpack-to-esbuild-82afa505a077
    
    **ISSUES I FACED**
    
    1. xml2js is a library that relies on node core modules (timers, stream),
    which the old webpack builder auto-polyfilled but esbuild does not and
    that fails the build with
    
    ```
    Could not resolve
      "timers" in node_modules/xml2js/lib/parser.js
    ```
    
    So I removed the xml2js dep and parsed the S3 ACL XML with
    browser-native DOMParser via a new XmlService (which is written with the
    help of claude so I don't miss anything there). Also added tests
    
    2.
    ```
    ✘ [ERROR] Could not resolve "~@ibm/plex/IBM-Plex-Mono/fonts/split/woff2/IBMPlexMono-Light-Cyrillic.woff2" [plugin angular-css-resource]
    
        src/styles.scss:8398:11:
          8398 │   src: url("../node_modules/@carbon/styles/scss/fonts||file:~@ib...
               ╵            ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ```
    
    which is fixed by removing `~` for `@ibm/plex` in styles scss
    
    3. strict module resolution and deep import blockages
    
    ```
    [ERROR] TS2307: Cannot find module 'carbon-components-angular/treeview/tree-node.types' or its corresponding type declarations. [plugin angular-compiler]
    
        src/app/ceph/block/iscsi-target-details/iscsi-target-details.component.ts:3:21:
          3 │ ...{ Node } from 'carbon-components-angular/treeview/tree-node.types';
            ╵                  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ```
    
    I created a new tree-node model in the repo itself and left a clear
    explanation in its start. Also raised an issue to carbon-components
    angular.
    
    Similarly
    ```
    ✘ [ERROR] TS2307: Cannot find module 'chart.js/dist/types/utils' or its corresponding type declarations. [plugin angular-compiler]
    
        node_modules/ng2-charts/lib/ng-charts.provider.d.ts:3:28:
          3 │ import { DeepPartial } from 'chart.js/dist/types/utils';
            ╵                             ~~~~~~~~~~~~~~~~~~~~~~~~~~~
    ```
    
    and added the path to tsconfig.json. The solution is to bump ng2-charts
    to v9+ but not doing it for now..
    https://github.com/valor-software/ng2-charts/issues/1991#issuecomment-3945913113
    
    ```
    ✘ [ERROR] TS2613: Module '".../brace-expansion/dist/esm/index"' has no default export. Did you mean to use 'import { expand } from "..."' instead? [plugin angular-compiler]
    
        src/app/ceph/cluster/hosts/host-form/host-form.component.ts:4:7:
          4 │ import expand from 'brace-expansion';
            ╵        ~~~~~~
    ```
    
    another similar issue
    
    4. had to bump `ng-bootstrap` to 19 which is what angular 20 supports
    
    ```
    ✘ [ERROR] No matching export in "node_modules/@angular/core/fesm2022/core.mjs" for import "AfterRenderPhase"
    
        node_modules/@ng-bootstrap/ng-bootstrap/fesm2022/ng-bootstrap.mjs:2:287:
          2 │ ...ector, afterNextRender, AfterRenderPhase, LOCALE_ID, ViewChild, ...
            ╵                            ~~~~~~~~~~~~~~~~
    
    ✘ [ERROR] No matching export in "node_modules/@angular/core/fesm2022/core.mjs" for import "afterRender"
    
        node_modules/@ng-bootstrap/ng-bootstrap/fesm2022/ng-bootstrap.mjs:2:339:
          2 │ ...ID, ViewChild, forwardRef, afterRender, ApplicationRef, Environm...
            ╵                               ~~~~~~~~~~~
    ```
    
    5. override cypress module to common, cypress is still older
    ```
    TSError: ⨯ Unable to compile TypeScript:
    error TS5095: Option 'bundler' can only be used when 'module' is set to 'preserve' or to 'es2015' or later.
    ```
    
    Fixes: https://tracker.ceph.com/issues/79687
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-resource-page/rgw-bucket-resource-page.component.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/rgw/rgw-bucket-resource-page/rgw-bucket-resource-page.component.ts
@@ -2,7 +2,6 @@ import { ChangeDetectorRef, Component, OnDestroy, OnInit, ViewEncapsulation } fr
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { Subscription } from 'rxjs';
-import * as xml2js from 'xml2js';
 import { ContentSwitcherOption } from 'carbon-components-angular';
 
 import { RgwBucketService } from '~/app/shared/api/rgw-bucket.service';
@@ -11,6 +10,7 @@ import { CdDatePipe } from '~/app/shared/pipes/cd-date.pipe';
 import { RgwBucketReplication } from '../models/rgw-bucket-replication';
 import { RgwRateLimitConfig } from '../models/rgw-rate-limit';
 import { OverviewField } from '~/app/shared/components/resource-overview-card/resource-overview-card.component';
+import { XmlService } from '~/app/shared/services/xml.service';
 
 @Component({
   selector: 'cd-rgw-bucket-resource-page',
@@ -66,7 +66,8 @@ export class RgwBucketResourcePageComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     private rgwBucketService: RgwBucketService,
     private cd: ChangeDetectorRef,
-    private cdDatePipe: CdDatePipe
+    private cdDatePipe: CdDatePipe,
+    private xmlService: XmlService
   ) {}
 
   ngOnInit(): void {
@@ -157,25 +158,21 @@ export class RgwBucketResourcePageComponent implements OnInit, OnDestroy {
   }
 
   parseXmlAcl(xml: any, bucketOwner: string): Record<string, string[]> {
-    const parser = new xml2js.Parser({ explicitArray: false, trim: true });
     const data: Record<string, string[]> = {
       Owner: ['-'],
       AllUsers: ['-'],
       AuthenticatedUsers: ['-']
     };
 
-    parser.parseString(xml, (err, result) => {
-      if (err) return;
-
-      const xmlGrantees = result?.['AccessControlPolicy']?.['AccessControlList']?.['Grant'];
-      if (!xmlGrantees) return;
-
+    const result = this.xmlService.parse(xml);
+    const xmlGrantees = result?.['AccessControlPolicy']?.['AccessControlList']?.['Grant'];
+    if (xmlGrantees) {
       if (Array.isArray(xmlGrantees)) {
         xmlGrantees.forEach((grantee) => this.processGrantee(grantee, bucketOwner, data));
       } else {
         this.processGrantee(xmlGrantees, bucketOwner, data);
       }
-    });
+    }
 
     return data;
   }
