a0799afba0fb02fbe155019859f6790149216188
Nizamudeen A <nia@redhat.com>
2026-08-21 11:30:52 +0530

    tentacle: mgr/dashboard: fix lint errors specific to tentacle
    ran npm run lint after resolving conflicts which caught couple of issues
    and resolved them manually here.
    
    Fixes: https://tracker.ceph.com/issues/78150
    Signed-off-by: Nizamudeen A <nia@redhat.com>

--- src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-edit-host-key-modal/nvmeof-edit-host-key-modal.component.spec.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/ceph/block/nvmeof-edit-host-key-modal/nvmeof-edit-host-key-modal.component.spec.ts
@@ -29,28 +29,26 @@ describe('NvmeofEditHostKeyModalComponent', () => {
     wrapTaskAroundCall: jasmine.createSpy('wrapTaskAroundCall').and.callFake(({ call }) => call)
   };
 
-  beforeEach(
-    waitForAsync(() => {
-      TestBed.configureTestingModule({
-        declarations: [NvmeofEditHostKeyModalComponent],
-        imports: [
-          ReactiveFormsModule,
-          HttpClientTestingModule,
-          RouterTestingModule,
-          SharedModule,
-          ToastrModule.forRoot()
-        ],
-        providers: [
-          { provide: NvmeofService, useValue: nvmeofServiceSpy },
-          { provide: TaskWrapperService, useValue: taskWrapperServiceSpy },
-          { provide: 'subsystemNQN', useValue: mockSubsystemNQN },
-          { provide: 'hostNQN', useValue: mockHostNQN },
-          { provide: 'group', useValue: mockGroup },
-          { provide: 'dhchapKey', useValue: '' }
-        ]
-      }).compileComponents();
-    })
-  );
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [NvmeofEditHostKeyModalComponent],
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        SharedModule,
+        ToastrModule.forRoot()
+      ],
+      providers: [
+        { provide: NvmeofService, useValue: nvmeofServiceSpy },
+        { provide: TaskWrapperService, useValue: taskWrapperServiceSpy },
+        { provide: 'subsystemNQN', useValue: mockSubsystemNQN },
+        { provide: 'hostNQN', useValue: mockHostNQN },
+        { provide: 'group', useValue: mockGroup },
+        { provide: 'dhchapKey', useValue: '' }
+      ]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(NvmeofEditHostKeyModalComponent);
