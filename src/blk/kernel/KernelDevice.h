32c94cd156d6a3be080ef10795619f2a46474aa8
Igor Fedotov <igor.fedotov@croit.io>
2026-08-24 23:21:08 +0300

    blk/kernel: reshuffle member funcs in the header to reflect their proper visibility.
    Signed-off-by: Igor Fedotov <igor.fedotov@croit.io>

--- src/blk/kernel/KernelDevice.h
+++ src/blk/kernel/KernelDevice.h
@@ -97,11 +97,7 @@ private:
   void _aio_thread();
   void _discard_thread(DiscardThread* thr);
   bool _queue_discard(interval_set<uint64_t> &to_release);
-  bool try_discard(interval_set<uint64_t> &to_release,
-                   bool async = true,
-                   bool force = false) override;
-
-  void collect_alerts(osd_alert_list_t& alerts, const std::string& device_name) override;
+  int _discard(uint64_t offset, uint64_t len);
 
   int _aio_start();
   void _aio_stop();
@@ -166,7 +162,12 @@ public:
 		bool buffered,
 		int write_hint = WRITE_LIFE_NOT_SET) override;
   int flush() override;
-  int _discard(uint64_t offset, uint64_t len);
+
+  bool try_discard(interval_set<uint64_t> &to_release,
+                   bool async = true,
+                   bool force = false) override;
+
+  void collect_alerts(osd_alert_list_t& alerts, const std::string& device_name) override;
 
   // for managing buffered readers/writers
   int invalidate_cache(uint64_t off, uint64_t len) override;
