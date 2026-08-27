028bedb102ecf60c47e2f4269de870ed45d36707
chunmei liu <chunmei.liu@ibm.com>
2026-08-27 16:35:11 -0700

    crimson/tools/objectstore: make crimson-objectstore-tool to support operation for omap and snapset
    Signed-off-by: chunmei liu <chunmei.liu@ibm.com>

--- src/crimson/tools/objectstore/objectstore_tool.cc
+++ src/crimson/tools/objectstore/objectstore_tool.cc
@@ -3,13 +3,16 @@
 
 #include <boost/smart_ptr/intrusive_ptr.hpp>
 #include <fmt/format.h>
+#include <sstream>
 
 #include "common/hobject.h"
+#include "common/JSONFormatter.h"
 #include "crimson/os/alienstore/alien_store.h"
 #include "crimson/os/futurized_collection.h"
 #include "crimson/os/futurized_store.h"
 #include "include/expected.hpp"
 #include "objectstore_tool.h"
+#include "osd/osd_types.h"
 #include "seastar/core/future.hh"
 #include "seastar/core/smp.hh"
 
@@ -131,7 +134,7 @@ seastar::future<bool> StoreTool::set_omap(
 {
   return seastar::smp::submit_to(
     shard_id,
-    [this, cid, oid, key, value]() mutable 
+    [this, cid, oid, key, value]() mutable
       -> seastar::future<bool>
   {
     auto coll = co_await store->get_sharded_store().open_collection(cid
@@ -149,6 +152,7 @@ seastar::future<bool> StoreTool::set_omap(
     omap_values[key] = std::move(bl);
     txn.omap_setkeys(cid, oid, omap_values);
     co_await store->get_sharded_store().do_transaction(coll, std::move(txn));
+    co_await store->get_sharded_store().flush(coll);
     co_return true;
   });
 }
@@ -173,10 +177,71 @@ seastar::future<bool> StoreTool::remove_omap(
     ceph::os::Transaction txn;
     txn.omap_rmkey(cid, oid, key);
     co_await store->get_sharded_store().do_transaction(coll, std::move(txn));
+    co_await store->get_sharded_store().flush(coll);
     co_return true;
   });
 }
 
+
+seastar::future<std::string> StoreTool::get_omap_header(
+  const coll_t& cid,
+  const ghobject_t& oid)
+{
+  return seastar::smp::submit_to(
+    shard_id,
+    [this, cid, oid]() -> seastar::future<std::string>
+  {
+    auto coll = co_await store->get_sharded_store().open_collection(cid
+    ).handle_exception([](std::exception_ptr) {
+      return seastar::make_ready_future<FuturizedStore::Shard::CollectionRef>(nullptr);
+    });
+    if (!coll) {
+      fmt::println(std::cerr, "Failed to open collection: collection does not exist");
+      co_return std::string();
+    }
+
+    auto&& header = co_await store->get_sharded_store().omap_get_header(
+      coll, oid).safe_then(
+      [](auto&& header) {
+        return seastar::make_ready_future<ceph::bufferlist>(std::move(header));
+      },
+      crimson::os::FuturizedStore::Shard::get_attr_errorator::all_same_way(
+        [](auto&& error) {
+          return seastar::make_ready_future<ceph::bufferlist>(ceph::bufferlist());
+        }
+      )
+    );
+    co_return header.to_str();
+  });
+}
+
+seastar::future<bool> StoreTool::set_omap_header(
+  const coll_t& cid,
+  const ghobject_t& oid,
+  const std::string& header)
+{
+  return seastar::smp::submit_to(
+    shard_id,
+    [this, cid, oid, header]() mutable
+      -> seastar::future<bool>
+  {
+    auto coll = co_await store->get_sharded_store().open_collection(cid
+    ).handle_exception([](std::exception_ptr) {
+      return seastar::make_ready_future<FuturizedStore::Shard::CollectionRef>(nullptr);
+    });
+    if (!coll) {
+      fmt::println(std::cerr, "Failed to open collection: collection does not exist");
+      co_return false;
+    }
+    ceph::os::Transaction txn;
+    ceph::bufferlist bl;
+    bl.append(header.c_str(), header.length());
+    txn.omap_setheader(cid, oid, bl);
+    co_await store->get_sharded_store().do_transaction(coll, std::move(txn));
+    co_await store->get_sharded_store().flush(coll);
+    co_return true;
+  });
+}
 // Object data operations
 seastar::future<std::string> StoreTool::get_bytes(
   const coll_t& cid,
@@ -195,7 +260,9 @@ seastar::future<std::string> StoreTool::get_bytes(
       co_return std::string();
     }
 
-    auto stat_result = co_await store->get_sharded_store().stat(coll, oid);
+    auto stat_result = co_await store->get_sharded_store().stat(coll, oid).handle_error(
+      crimson::os::FuturizedStore::Shard::stat_ertr::assert_all("stat failed in get_bytes")
+    );
     uint64_t total_size = stat_result.st_size;
 
     if (total_size == 0) {
@@ -207,10 +274,10 @@ seastar::future<std::string> StoreTool::get_bytes(
 
     uint64_t offset = 0;
     const uint64_t max_read = 1024 * 1024; // 1MB chunks
-    
+
     while (offset < total_size) {
       uint64_t len = std::min(max_read, total_size - offset);
-      
+
       auto read_result = co_await store->get_sharded_store().read(coll, oid, offset, len).safe_then(
         [](auto&& bl) -> ceph::bufferlist {
           return std::move(bl);
@@ -259,6 +326,7 @@ seastar::future<bool> StoreTool::set_bytes(
     txn.write(cid, oid, 0, data.length(), bl, 0);
 
     co_await store->get_sharded_store().do_transaction(coll, std::move(txn));
+    co_await store->get_sharded_store().flush(coll);
     co_return true;
   });
 }
@@ -344,13 +412,14 @@ seastar::future<bool> StoreTool::set_attr(
       fmt::println(std::cerr, "Failed to open collection: collection does not exist");
       co_return false;
     }
-    
+
     ceph::os::Transaction txn;
     ceph::bufferlist bl;
     bl.append(value.c_str(), value.length());
     txn.setattr(cid, oid, key, bl);
 
     co_await store->get_sharded_store().do_transaction(coll, std::move(txn));
+    co_await store->get_sharded_store().flush(coll);
     co_return true;
   });
 }
@@ -372,11 +441,12 @@ seastar::future<bool> StoreTool::remove_attr(
       fmt::println(std::cerr, "Failed to open collection: collection does not exist");
       co_return false;
     }
-    
+
     ceph::os::Transaction txn;
     txn.rmattr(cid, oid, key);
 
     co_await store->get_sharded_store().do_transaction(coll, std::move(txn));
+    co_await store->get_sharded_store().flush(coll);
     co_return true;
   });
 }
@@ -400,23 +470,26 @@ seastar::future<bool> StoreTool::remove_object(
       fmt::println(std::cerr, "Failed to open collection: collection does not exist");
       co_return false;
     }
-    
+
     // First check if the object exists
-    try {
-      [[maybe_unused]] auto stat_result = co_await store->get_sharded_store().stat(coll, oid);
-      // Object exists, proceed with removal
-    } catch (const std::exception& e) {
-      fmt::println(std::cerr, "Object {} does not exist or stat failed: {}", oid, e.what());
+    auto stat_result = co_await store->get_sharded_store().stat(coll, oid).handle_error(
+      crimson::os::FuturizedStore::Shard::stat_ertr::all_same_way(
+        []() -> struct stat {
+          return {};  // zero st_nlink signals object-not-found or IO error
+        })
+    );
+    if (stat_result.st_nlink == 0) {
+      fmt::println(std::cerr, "Object {} does not exist or stat failed", oid);
       co_return false;
     }
-    
+
     ceph::os::Transaction txn;
 
     //TODO: Implement proper snapset handling when crimson supports snapshots
     if (all) {
       fmt::println(std::cout, "removeall: removing object {} and all clones", oid);
       txn.remove(cid, oid);
-      
+
     } else {
       fmt::println(std::cout, "remove: removing object {}{}", oid, force ? " (forced)" : "");
 
@@ -427,6 +500,10 @@ seastar::future<bool> StoreTool::remove_object(
     }
 
     co_await store->get_sharded_store().do_transaction(coll, std::move(txn));
+
+    // Flush to ensure the transaction is persisted to disk before unmounting
+    co_await store->get_sharded_store().flush(coll);
+
     fmt::println(std::cout, "Successfully removed object {}", oid);
     co_return true;
   });
@@ -450,13 +527,50 @@ seastar::future<std::string> StoreTool::dump_object_info(
       co_return std::string();
     }
 
-    std::string result = fmt::format("{{\n");
-    result += fmt::format("  \"collection\": \"{}\",\n", cid);
-    result += fmt::format("  \"object\": \"{}\",\n", oid);
-    result += fmt::format("  \"attributes\": {{}}\n");
-    result += fmt::format("}}\n");
+    // Read the object_info attribute
+    ceph::bufferlist attr_bl;
+    bool success = co_await store->get_sharded_store().get_attr(coll, oid, OI_ATTR
+    ).safe_then([&attr_bl](auto&& bl) {
+      attr_bl = std::move(bl);
+      return seastar::make_ready_future<bool>(true);
+    }).handle_error(
+      FuturizedStore::Shard::get_attr_errorator::all_same_way(
+        [](const std::error_code& e) {
+          fmt::println(std::cerr, "Failed to get object info attribute: {}", e.message());
+          return seastar::make_ready_future<bool>(false);
+        }
+      )
+    );
 
-    co_return result;
+    if (!success || attr_bl.length() == 0) {
+      fmt::println(std::cerr, "Object info attribute is empty or not found");
+      co_return std::string();
+    }
+
+    // Decode object_info_t
+    object_info_t oi;
+    auto bp = attr_bl.cbegin();
+    try {
+      decode(oi, bp);
+    } catch (...) {
+      fmt::println(std::cerr, "Failed to decode object info");
+      co_return std::string();
+    }
+
+    // Format output using JSONFormatter
+    std::ostringstream out;
+    ceph::JSONFormatter formatter(true);
+    formatter.open_object_section("obj");
+    formatter.open_object_section("id");
+    oid.dump(&formatter);
+    formatter.close_section();
+    formatter.open_object_section("info");
+    oi.dump(&formatter);
+    formatter.close_section();
+    formatter.close_section();
+    formatter.flush(out);
+
+    co_return out.str();
   });
 }
 
@@ -477,11 +591,12 @@ seastar::future<bool> StoreTool::set_object_size(
       fmt::println(std::cerr, "Failed to open collection: collection does not exist");
       co_return false;
     }
-    
+
     ceph::os::Transaction txn;
     txn.truncate(cid, oid, size);
 
     co_await store->get_sharded_store().do_transaction(coll, std::move(txn));
+    co_await store->get_sharded_store().flush(coll);
     co_return true;
   });
 }
@@ -502,11 +617,163 @@ seastar::future<bool> StoreTool::clear_data_digest(
       fmt::println(std::cerr, "Failed to open collection: collection does not exist");
       co_return false;
     }
-    
+
     ceph::os::Transaction txn;
     txn.rmattr(cid, oid, "data_digest");
-    
+
     co_await store->get_sharded_store().do_transaction(coll, std::move(txn));
+    co_await store->get_sharded_store().flush(coll);
+    co_return true;
+  });
+}
+
+seastar::future<bool> StoreTool::corrupt_info(
+  const coll_t& cid,
+  const ghobject_t& oid)
+{
+  return seastar::smp::submit_to(
+    shard_id,
+    [this, cid, oid]() -> seastar::future<bool>
+  {
+    auto coll = co_await store->get_sharded_store().open_collection(cid
+    ).handle_exception([](std::exception_ptr) {
+      return seastar::make_ready_future<FuturizedStore::Shard::CollectionRef>(nullptr);
+    });
+    if (!coll) {
+      fmt::println(std::cerr, "Failed to open collection: collection does not exist");
+      co_return false;
+    }
+
+    // Read the object_info attribute
+    ceph::bufferlist attr_bl;
+    bool success = co_await store->get_sharded_store().get_attr(coll, oid, OI_ATTR
+    ).safe_then([&attr_bl](auto&& bl) {
+      attr_bl = std::move(bl);
+      return seastar::make_ready_future<bool>(true);
+    }).handle_error(
+      FuturizedStore::Shard::get_attr_errorator::all_same_way(
+        [](const std::error_code& e) {
+          fmt::println(std::cerr, "Failed to get object info attribute: {}", e.message());
+          return seastar::make_ready_future<bool>(false);
+        }
+      )
+    );
+
+    if (!success || attr_bl.length() == 0) {
+      fmt::println(std::cerr, "Object info attribute is empty or not found");
+      co_return false;
+    }
+
+    // Decode object_info_t
+    object_info_t oi;
+    auto bp = attr_bl.cbegin();
+    try {
+      decode(oi, bp);
+    } catch (...) {
+      fmt::println(std::cerr, "Failed to decode object info");
+      co_return false;
+    }
+
+    // Corrupt the object_info by modifying alloc_hint_flags
+    oi.alloc_hint_flags += 0xff;
+
+    // Re-encode and write back
+    ceph::bufferlist new_attr_bl;
+    encode(oi, new_attr_bl, -1);  // using full features
+
+    ceph::os::Transaction txn;
+    txn.setattr(cid, oid, OI_ATTR, new_attr_bl);
+
+    co_await store->get_sharded_store().do_transaction(coll, std::move(txn));
+    co_await store->get_sharded_store().flush(coll);
+    co_return true;
+  });
+}
+
+seastar::future<bool> StoreTool::clear_snapset(
+  const coll_t& cid,
+  const ghobject_t& oid,
+  const std::string& arg)
+{
+  return seastar::smp::submit_to(
+    shard_id,
+    [this, cid, oid, arg]() -> seastar::future<bool>
+  {
+    auto coll = co_await store->get_sharded_store().open_collection(cid
+    ).handle_exception([](std::exception_ptr) {
+      return seastar::make_ready_future<FuturizedStore::Shard::CollectionRef>(nullptr);
+    });
+    if (!coll) {
+      fmt::println(std::cerr, "Failed to open collection: collection does not exist");
+      co_return false;
+    }
+
+    // Read the snapset attribute
+    ceph::bufferlist attr_bl;
+    bool success = co_await store->get_sharded_store().get_attr(coll, oid, SS_ATTR
+    ).safe_then([&attr_bl](auto&& bl) {
+      attr_bl = std::move(bl);
+      return seastar::make_ready_future<bool>(true);
+    }).handle_error(
+      FuturizedStore::Shard::get_attr_errorator::all_same_way(
+        [](const std::error_code& e) {
+          fmt::println(std::cerr, "Failed to get snapset attribute: {}", e.message());
+          return seastar::make_ready_future<bool>(false);
+        }
+      )
+    );
+
+    if (!success || attr_bl.length() == 0) {
+      fmt::println(std::cerr, "Snapset attribute is empty or not found");
+      co_return false;
+    }
+
+    // Decode SnapSet
+    SnapSet snapset;
+    auto bp = attr_bl.cbegin();
+    try {
+      decode(snapset, bp);
+    } catch (...) {
+      fmt::println(std::cerr, "Failed to decode snapset");
+      co_return false;
+    }
+
+    // Selectively clear snapset fields based on arg parameter
+    // This matches the behavior of ceph-objectstore-tool
+
+    // Use "corrupt" to clear entire SnapSet
+    // Use "seq" to just corrupt SnapSet.seq
+    if (arg == "corrupt" || arg == "seq")
+      snapset.seq = 0;
+    // Use "snaps" to just clear SnapSet.clone_snaps
+    if (arg == "corrupt" || arg == "snaps")
+      snapset.clone_snaps.clear();
+    // By default just clear clone, clone_overlap and clone_size
+    std::string clear_arg = arg;
+    if (arg == "corrupt")
+      clear_arg = "";
+    if (clear_arg == "" || clear_arg == "clones")
+      snapset.clones.clear();
+    if (clear_arg == "" || clear_arg == "clone_overlap")
+      snapset.clone_overlap.clear();
+    if (clear_arg == "" || clear_arg == "clone_size")
+      snapset.clone_size.clear();
+    // Break all clone sizes by adding 1
+    if (clear_arg == "size") {
+      for (auto& [snap, size] : snapset.clone_size) {
+        ++size;
+      }
+    }
+
+    // Re-encode and write back
+    ceph::bufferlist new_attr_bl;
+    encode(snapset, new_attr_bl);
+
+    ceph::os::Transaction txn;
+    txn.setattr(cid, oid, SS_ATTR, new_attr_bl);
+
+    co_await store->get_sharded_store().do_transaction(coll, std::move(txn));
+    co_await store->get_sharded_store().flush(coll);
     co_return true;
   });
 }
