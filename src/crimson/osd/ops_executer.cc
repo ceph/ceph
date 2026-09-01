ac3a482d8fc8bdda92d1f62fe1532eaacb0e064a
chunmei liu <chunmei.liu@ibm.com>
2026-08-31 23:01:29 -0700

    crimson/osd: SCRUBLS op and osd-mapper support
    Implement CEPH_OSD_OP_SCRUBLS in crimson for rados list-inconsistent-*.
    PgOpsExecuter stores MOSDOp* and dispatches to PG::do_scrub_ls().
    PG::do_scrub_ls() decodes scrub_ls_arg_t, calls get_store_errors(),
    encodes scrub_ls_result_t into the reply.
    PG::handle_advance_map() triggers on_scrub_schedule_input_change() when
    pool.info changes. Fix PG::should_send_op() async-recovery-target logic.
    
    Signed-off-by: chunmei liu <chunmei.liu@ibm.com>

--- src/crimson/osd/ops_executer.cc
+++ src/crimson/osd/ops_executer.cc
@@ -1541,6 +1541,28 @@ static PG::interruptible_future<> do_pgls_filtered(
   });
 }
 
+static PG::interruptible_future<> do_scrub_ls(
+  const PG& pg,
+  const MOSDOp* m,
+  OSDOp& osd_op)
+{
+  int r = pg.do_scrub_ls(m, &osd_op);
+
+  if (r < 0) {
+    if (r == -EINVAL) {
+      throw crimson::osd::invalid_argument{};
+    } else if (r == -EAGAIN) {
+      throw crimson::osd::error(std::errc::resource_unavailable_try_again);
+    } else if (r == -ENOENT) {
+      throw crimson::osd::object_not_found{};
+    } else {
+      throw crimson::osd::make_error(r);
+    }
+  }
+
+  return seastar::now();
+}
+
 PgOpsExecuter::interruptible_future<>
 PgOpsExecuter::execute_op(OSDOp& osd_op)
 {
@@ -1554,6 +1576,8 @@ PgOpsExecuter::execute_op(OSDOp& osd_op)
     return do_pgnls(pg, nspace, osd_op);
   case CEPH_OSD_OP_PGNLS_FILTER:
     return do_pgnls_filtered(pg, nspace, osd_op);
+  case CEPH_OSD_OP_SCRUBLS:
+    return do_scrub_ls(pg, m, osd_op);
   default:
     logger().warn("unknown op {}", ceph_osd_op_name(op.op));
     throw std::runtime_error(
