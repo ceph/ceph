88dca29d2dd99443826311723db375df96ddca5e
Viliam Durina <viliam.durina@gmail.com>
2026-09-01 10:58:18 +0200

    os/bluestore: Re-introduce truncation in `BlueRocksWritableFile.Close()`
    `rocksdb::WritableFile` assumes that whatever was allocated using `Allocate()`, and was not actually written to, is released when the file is closed (cf. `PosixWritableFile::Close()`). This used to work up to PR #62224 (released in 20.1.0, a tentacle RC), which removed the truncation from `close` and it assumed that `truncate` is called.
    
    This PR adds the truncation back to `BlueRocksWritableFile::Close()`. We cannot bring back the exact pre-62224 code, as it relies on `h->pos`. Instead we flush the file and use `fnode.size`, which contains the on-disk size, including the envelope.
    
    This should fix https://tracker.ceph.com/issues/76256.
    
    I'm not sure why this issue excessively affected clusters with no sharding. From the logs attached to the issue, in the non-sharded case there are many files with only a few kB payload in a 70MB file; in the sharded case the unreleased file tails still exist, but are much smaller.
    
    The new test case unit-tests the new `truncate_unused_allocations()` method, overall correctness of BlueFS operation is covered by other tests already. We also delete `BlueStoreWritableFile::UseDirectIO()`, as it is unused (it doesn't override superclass method).
    
    Fixes: https://tracker.ceph.com/issues/76256
    Signed-off-by: Viliam Durina <viliam.durina@gmail.com>

--- src/os/bluestore/BlueRocksEnv.cc
+++ src/os/bluestore/BlueRocksEnv.cc
@@ -221,6 +221,13 @@ class BlueRocksWritableFile : public rocksdb::WritableFile {
   }
 
   rocksdb::Status Close() override {
+    // RocksDB assumes that when the file is closed, unused allocated
+    // space is truncated. It's not explicitly documented, but compare
+    // PosixWritableFile::Close().
+    int r = fs->truncate_unused(h);
+    if (r < 0) {
+      return err_to_status(r);
+    }
     fs->fsync(h);
     return rocksdb::Status::OK();
   }
@@ -239,12 +246,6 @@ class BlueRocksWritableFile : public rocksdb::WritableFile {
   // and Flush().
   bool IsSyncThreadSafe() const override {
     return true;
-  }
-
-  // Indicates the upper layers if the current WritableFile implementation
-  // uses direct IO.
-  bool UseDirectIO() const {
-    return false;
   }
 
   void SetWriteLifeTimeHint(rocksdb::Env::WriteLifeTimeHint hint) override {
