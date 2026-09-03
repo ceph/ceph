ceph/ceph-ci

rgw/d4n: Remove explicit `use_blocked`
Replace with `rgw::oyc`.

Signed-off-by: Adam C. Emerson <aemerson@redhat.com>

src/rgw/driver/d4n/d4n_directory.cc
@@ -2,8 +2,10 @@
 #include <boost/asio/consign.hpp>
 #include <boost/algorithm/string.hpp>
 #include <memory>
-#include "common/async/blocked_completion.h"
-#include "common/dout.h" 
+#include "common/dout.h"
+
+#include "rgw/yield_completion.h"
+
 #include "d4n_directory.h"
 
 namespace rgw { namespace d4n {
@@ -36,19 +38,6 @@ auto async_exec(std::shared_ptr<connection> conn,
       initiate_exec{std::move(conn)}, token, req, resp);
 }
 
-template <typename... Types>
-void redis_exec(std::shared_ptr<connection> conn,
-                boost::system::error_code& ec,
-                const boost::redis::request& req,
-                boost::redis::response<Types...>& resp, optional_yield y)
-{
-  if (y) {
-    auto yield = y.get_yield_context();
-    async_exec(std::move(conn), req, resp, yield[ec]);
-  } else {
-    async_exec(std::move(conn), req, resp, ceph::async::use_blocked[ec]);
-  }
-}
 
 template <typename... Types>
 void redis_exec_cp(const DoutPrefixProvider* dpp,
@@ -62,12 +51,7 @@ void redis_exec_cp(const DoutPrefixProvider* dpp,
 	std::shared_ptr<connection> conn = pool->acquire(dpp);
 	try {
 
-  		if (y) {
-    		auto yield = y.get_yield_context();
-    		async_exec(conn, req, resp, yield[ec]);
-  		} else {
-    		async_exec(conn, req, resp, ceph::async::use_blocked[ec]);
-  		}
+    		async_exec(conn, req, resp, rgw::oyc(dpp, y, ec));
 	} catch (const std::exception& e) {
 		//release the connection upon exception
     		pool->release(conn);
@@ -77,18 +61,6 @@ void redis_exec_cp(const DoutPrefixProvider* dpp,
 	pool->release(conn);
 }
 
-void redis_exec(std::shared_ptr<connection> conn,
-                boost::system::error_code& ec,
-                const boost::redis::request& req,
-    boost::redis::generic_response& resp, optional_yield y)
-{
-  if (y) {
-    auto yield = y.get_yield_context();
-    async_exec(std::move(conn), req, resp, yield[ec]);
-  } else {
-    async_exec(std::move(conn), req, resp, ceph::async::use_blocked[ec]);
-  }
-}
 
 void redis_exec_cp(const DoutPrefixProvider* dpp,
                 std::shared_ptr<rgw::d4n::RedisPool> pool,
@@ -100,12 +72,7 @@ void redis_exec_cp(const DoutPrefixProvider* dpp,
 	std::shared_ptr<connection> conn = pool->acquire(dpp);
 
 	try {
-  		if (y) {
-    			auto yield = y.get_yield_context();
-    			async_exec(conn, req, resp, yield[ec]);
-  		} else {
-    			async_exec(conn, req, resp, ceph::async::use_blocked[ec]);
-  		}	
+    		async_exec(conn, req, resp, rgw::oyc(dpp, y, ec));
 	} catch (const std::exception& e) {
     			pool->release(conn);
     			throw;
@@ -134,7 +101,7 @@ void redis_exec_connection_pool(const DoutPrefixProvider* dpp,
 {
     if(!redis_pool)[[unlikely]]
     {
-	redis_exec(conn, ec, req, resp, y);
+        async_exec(conn, req, resp, rgw::oyc(dpp, y, ec));
 	ldpp_dout(dpp, 0) << "Directory::" << __func__ << " not using connection-pool, it's using the shared connection " << dendl;
     }
     else[[likely]]
@@ -152,7 +119,7 @@ void redis_exec_connection_pool(const DoutPrefixProvider* dpp,
 {
     if(!redis_pool)[[unlikely]]
     {
-	redis_exec(conn, ec, req, resp, y);
+        async_exec(conn, req, resp, rgw::oyc(dpp, y, ec));
 	ldpp_dout(dpp, 0) << "Directory::" << __func__ << " not using connection-pool, it's using the shared connection " << dendl;
     }
     else[[likely]]
@@ -1275,7 +1242,7 @@ int BlockDirectory::get(const DoutPrefixProvider* dpp, std::vector<CacheBlock>& 
 
   try {
     boost::system::error_code ec;
-    redis_exec(conn, ec, req, resp, y);
+    async_exec(conn, req, resp, rgw::oyc(dpp, y, ec));
 
     if (ec) {
       ldpp_dout(dpp, 0) << "BlockDirectory::" << __func__ << "() ERROR: " << ec.what() << dendl;

src/rgw/driver/d4n/d4n_policy.cc
@@ -1,8 +1,9 @@
 #include "d4n_policy.h"
 
 #include "../../../common/async/yield_context.h"
-#include "common/async/blocked_completion.h"
 #include "common/split.h"
+
+#include "yield_completion.h"
 #include "rgw_perf_counters.h"
 
 namespace rgw { namespace d4n {
@@ -35,19 +36,6 @@ auto async_exec(std::shared_ptr<connection> conn,
       initiate_exec{std::move(conn)}, token, req, resp);
 }
 
-template <typename... Types>
-static inline void redis_exec(std::shared_ptr<connection> conn,
-                boost::system::error_code& ec,
-                const boost::redis::request& req,
-                boost::redis::response<Types...>& resp, optional_yield y)
-{
-  if (y) {
-    auto yield = y.get_yield_context();
-    async_exec(std::move(conn), req, resp, yield[ec]);
-  } else {
-    async_exec(std::move(conn), req, resp, ceph::async::use_blocked[ec]);
-  }
-}
 
 int LFUDAPolicy::init(CephContext* cct, const DoutPrefixProvider* dpp, asio::io_context& io_context, rgw::sal::Driver* _driver) {
   response<int, int, int, int> resp;
@@ -90,7 +78,7 @@ int LFUDAPolicy::init(CephContext* cct, const DoutPrefixProvider* dpp, asio::io_
     req.push("HSETNX", "lfuda", "age", age); /* Only set maximum age if it doesn't exist */
     req.push("EXEC");
   
-    redis_exec(conn, ec, req, resp, y);
+    async_exec(conn, req, resp, rgw::oyc(dpp, y, ec));
 
     if (ec) {
       ldpp_dout(dpp, 0) << "LFUDAPolicy::" << __func__ << "() ERROR: " << ec.what() << dendl;
@@ -115,7 +103,7 @@ int LFUDAPolicy::age_sync(const DoutPrefixProvider* dpp, optional_yield y) {
     request req;
     req.push("HGET", "lfuda", "age");
       
-    redis_exec(conn, ec, req, resp, y);
+    async_exec(conn, req, resp, rgw::oyc(dpp, y, ec));
 
     if (ec) {
       ldpp_dout(dpp, 0) << "LFUDAPolicy::" << __func__ << "() ERROR: " << ec.what() << dendl;
@@ -132,7 +120,7 @@ int LFUDAPolicy::age_sync(const DoutPrefixProvider* dpp, optional_yield y) {
       request req;
       req.push("HSET", "lfuda", "age", age);
 
-      redis_exec(conn, ec, req, ret, y);
+      async_exec(conn, req, ret, rgw::oyc(dpp, y, ec));
 
       if (ec) {
 	ldpp_dout(dpp, 0) << "LFUDAPolicy::" << __func__ << "() ERROR: " << ec.what() << dendl;
@@ -157,7 +145,7 @@ int LFUDAPolicy::local_weight_sync(const DoutPrefixProvider* dpp, optional_yield
       request req;
       req.push("HMGET", "lfuda", "minLocalWeights_sum", "minLocalWeights_size");
 	
-      redis_exec(conn, ec, req, resp, y);
+      async_exec(conn, req, resp, rgw::oyc(dpp, y, ec));
 
       if (ec) {
 	ldpp_dout(dpp, 0) << "LFUDAPolicy::" << __func__ << "() ERROR: " << ec.what() << dendl;
@@ -181,7 +169,7 @@ int LFUDAPolicy::local_weight_sync(const DoutPrefixProvider* dpp, optional_yield
                   "minLocalWeights_size", std::to_string(entries_map.size()), 
                   "minLocalWeights_address", dpp->get_cct()->_conf->rgw_d4n_local_rgw_address);
 
-	redis_exec(conn, ec, req, resp, y);
+	async_exec(conn, req, resp, rgw::oyc(dpp, y, ec));
 
 	if (ec) {
 	  ldpp_dout(dpp, 0) << "LFUDAPolicy::" << __func__ << "() ERROR: " << ec.what() << dendl;
@@ -203,7 +191,7 @@ int LFUDAPolicy::local_weight_sync(const DoutPrefixProvider* dpp, optional_yield
     req.push("HSET", dpp->get_cct()->_conf->rgw_d4n_local_rgw_address, "avgLocalWeight_sum", std::to_string(weightSum), 
               "avgLocalWeight_size", std::to_string(entries_map.size()));
 
-    redis_exec(conn, ec, req, resp, y);
+    async_exec(conn, req, resp, rgw::oyc(dpp, y, ec));
 
     if (ec) {
       ldpp_dout(dpp, 0) << "LFUDAPolicy::" << __func__ << "() ERROR: " << ec.what() << dendl;

ceph/ceph

src/rgw/driver/d4n/d4n_directory.cc
@@ -2,8 +2,10 @@
 #include <boost/asio/consign.hpp>
 #include <boost/algorithm/string.hpp>
 #include <memory>
-#include "common/async/blocked_completion.h"
-#include "common/dout.h" 
+#include "common/dout.h"
+
+#include "rgw/yield_completion.h"
+
 #include "d4n_directory.h"
 
 namespace rgw { namespace d4n {
@@ -36,19 +38,6 @@ auto async_exec(std::shared_ptr<connection> conn,
       initiate_exec{std::move(conn)}, token, req, resp);
 }
 
-template <typename... Types>
-void redis_exec(std::shared_ptr<connection> conn,
-                boost::system::error_code& ec,
-                const boost::redis::request& req,
-                boost::redis::response<Types...>& resp, optional_yield y)
-{
-  if (y) {
-    auto yield = y.get_yield_context();
-    async_exec(std::move(conn), req, resp, yield[ec]);
-  } else {
-    async_exec(std::move(conn), req, resp, ceph::async::use_blocked[ec]);
-  }
-}
 
 template <typename... Types>
 void redis_exec_cp(const DoutPrefixProvider* dpp,
@@ -62,12 +51,7 @@ void redis_exec_cp(const DoutPrefixProvider* dpp,
 	std::shared_ptr<connection> conn = pool->acquire(dpp);
 	try {
 
-  		if (y) {
-    		auto yield = y.get_yield_context();
-    		async_exec(conn, req, resp, yield[ec]);
-  		} else {
-    		async_exec(conn, req, resp, ceph::async::use_blocked[ec]);
-  		}
+    		async_exec(conn, req, resp, rgw::maybe_yield(dpp, y, ec));
 	} catch (const std::exception& e) {
 		//release the connection upon exception
     		pool->release(conn);
@@ -77,18 +61,6 @@ void redis_exec_cp(const DoutPrefixProvider* dpp,
 	pool->release(conn);
 }
 
-void redis_exec(std::shared_ptr<connection> conn,
-                boost::system::error_code& ec,
-                const boost::redis::request& req,
-    boost::redis::generic_response& resp, optional_yield y)
-{
-  if (y) {
-    auto yield = y.get_yield_context();
-    async_exec(std::move(conn), req, resp, yield[ec]);
-  } else {
-    async_exec(std::move(conn), req, resp, ceph::async::use_blocked[ec]);
-  }
-}
 
 void redis_exec_cp(const DoutPrefixProvider* dpp,
                 std::shared_ptr<rgw::d4n::RedisPool> pool,
@@ -100,12 +72,7 @@ void redis_exec_cp(const DoutPrefixProvider* dpp,
 	std::shared_ptr<connection> conn = pool->acquire(dpp);
 
 	try {
-  		if (y) {
-    			auto yield = y.get_yield_context();
-    			async_exec(conn, req, resp, yield[ec]);
-  		} else {
-    			async_exec(conn, req, resp, ceph::async::use_blocked[ec]);
-  		}	
+    		async_exec(conn, req, resp, rgw::maybe_yield(dpp, y, ec));
 	} catch (const std::exception& e) {
     			pool->release(conn);
     			throw;
@@ -134,7 +101,7 @@ void redis_exec_connection_pool(const DoutPrefixProvider* dpp,
 {
     if(!redis_pool)[[unlikely]]
     {
-	redis_exec(conn, ec, req, resp, y);
+        async_exec(conn, req, resp, rgw::maybe_yield(dpp, y, ec));
 	ldpp_dout(dpp, 0) << "Directory::" << __func__ << " not using connection-pool, it's using the shared connection " << dendl;
     }
     else[[likely]]
@@ -152,7 +119,7 @@ void redis_exec_connection_pool(const DoutPrefixProvider* dpp,
 {
     if(!redis_pool)[[unlikely]]
     {
-	redis_exec(conn, ec, req, resp, y);
+        async_exec(conn, req, resp, rgw::maybe_yield(dpp, y, ec));
 	ldpp_dout(dpp, 0) << "Directory::" << __func__ << " not using connection-pool, it's using the shared connection " << dendl;
     }
     else[[likely]]
@@ -1275,7 +1242,7 @@ int BlockDirectory::get(const DoutPrefixProvider* dpp, std::vector<CacheBlock>& 
 
   try {
     boost::system::error_code ec;
-    redis_exec(conn, ec, req, resp, y);
+    async_exec(conn, req, resp, rgw::maybe_yield(dpp, y, ec));
 
     if (ec) {
       ldpp_dout(dpp, 0) << "BlockDirectory::" << __func__ << "() ERROR: " << ec.what() << dendl;

src/rgw/driver/d4n/d4n_policy.cc
@@ -1,8 +1,9 @@
 #include "d4n_policy.h"
 
 #include "../../../common/async/yield_context.h"
-#include "common/async/blocked_completion.h"
 #include "common/split.h"
+
+#include "yield_completion.h"
 #include "rgw_perf_counters.h"
 
 namespace rgw { namespace d4n {
@@ -35,19 +36,6 @@ auto async_exec(std::shared_ptr<connection> conn,
       initiate_exec{std::move(conn)}, token, req, resp);
 }
 
-template <typename... Types>
-static inline void redis_exec(std::shared_ptr<connection> conn,
-                boost::system::error_code& ec,
-                const boost::redis::request& req,
-                boost::redis::response<Types...>& resp, optional_yield y)
-{
-  if (y) {
-    auto yield = y.get_yield_context();
-    async_exec(std::move(conn), req, resp, yield[ec]);
-  } else {
-    async_exec(std::move(conn), req, resp, ceph::async::use_blocked[ec]);
-  }
-}
 
 int LFUDAPolicy::init(CephContext* cct, const DoutPrefixProvider* dpp, asio::io_context& io_context, rgw::sal::Driver* _driver) {
   response<int, int, int, int> resp;
@@ -90,7 +78,7 @@ int LFUDAPolicy::init(CephContext* cct, const DoutPrefixProvider* dpp, asio::io_
     req.push("HSETNX", "lfuda", "age", age); /* Only set maximum age if it doesn't exist */
     req.push("EXEC");
   
-    redis_exec(conn, ec, req, resp, y);
+    async_exec(conn, req, resp, rgw::maybe_yield(dpp, y, ec));
 
     if (ec) {
       ldpp_dout(dpp, 0) << "LFUDAPolicy::" << __func__ << "() ERROR: " << ec.what() << dendl;
@@ -115,7 +103,7 @@ int LFUDAPolicy::age_sync(const DoutPrefixProvider* dpp, optional_yield y) {
     request req;
     req.push("HGET", "lfuda", "age");
       
-    redis_exec(conn, ec, req, resp, y);
+    async_exec(conn, req, resp, rgw::maybe_yield(dpp, y, ec));
 
     if (ec) {
       ldpp_dout(dpp, 0) << "LFUDAPolicy::" << __func__ << "() ERROR: " << ec.what() << dendl;
@@ -132,7 +120,7 @@ int LFUDAPolicy::age_sync(const DoutPrefixProvider* dpp, optional_yield y) {
       request req;
       req.push("HSET", "lfuda", "age", age);
 
-      redis_exec(conn, ec, req, ret, y);
+      async_exec(conn, req, ret, rgw::maybe_yield(dpp, y, ec));
 
       if (ec) {
 	ldpp_dout(dpp, 0) << "LFUDAPolicy::" << __func__ << "() ERROR: " << ec.what() << dendl;
@@ -157,7 +145,7 @@ int LFUDAPolicy::local_weight_sync(const DoutPrefixProvider* dpp, optional_yield
       request req;
       req.push("HMGET", "lfuda", "minLocalWeights_sum", "minLocalWeights_size");
 	
-      redis_exec(conn, ec, req, resp, y);
+      async_exec(conn, req, resp, rgw::maybe_yield(dpp, y, ec));
 
       if (ec) {
 	ldpp_dout(dpp, 0) << "LFUDAPolicy::" << __func__ << "() ERROR: " << ec.what() << dendl;
@@ -181,7 +169,7 @@ int LFUDAPolicy::local_weight_sync(const DoutPrefixProvider* dpp, optional_yield
                   "minLocalWeights_size", std::to_string(entries_map.size()), 
                   "minLocalWeights_address", dpp->get_cct()->_conf->rgw_d4n_local_rgw_address);
 
-	redis_exec(conn, ec, req, resp, y);
+	async_exec(conn, req, resp, rgw::maybe_yield(dpp, y, ec));
 
 	if (ec) {
 	  ldpp_dout(dpp, 0) << "LFUDAPolicy::" << __func__ << "() ERROR: " << ec.what() << dendl;
@@ -203,7 +191,7 @@ int LFUDAPolicy::local_weight_sync(const DoutPrefixProvider* dpp, optional_yield
     req.push("HSET", dpp->get_cct()->_conf->rgw_d4n_local_rgw_address, "avgLocalWeight_sum", std::to_string(weightSum), 
               "avgLocalWeight_size", std::to_string(entries_map.size()));
 
-    redis_exec(conn, ec, req, resp, y);
+    async_exec(conn, req, resp, rgw::maybe_yield(dpp, y, ec));
 
     if (ec) {
       ldpp_dout(dpp, 0) << "LFUDAPolicy::" << __func__ << "() ERROR: " << ec.what() << dendl;