ceph/ceph-ci

mds: gate snapdiff unchanged-span skip on snapflush_pending
When a dentry spans both snapid_prev and snapid, build_snap_diff skips
it without comparing metadata.  If snapflush is still outstanding, that
skip can hide a real change.

Add snapflush_pending() using client_snap_caps on snap inodes and
client_need_snapflush on HEAD for snaps in [snapid_prev, snapid].
When snapflush is pending on an unchanged span, rdlock the filelock and
compare mtime at snapid_prev vs snapid via pick_inode_snap before
skipping.

Pair compare (Path 3) still always rdlocks on equal unlocked metadata
as in the preceding commit.

Fixes: https://tracker.ceph.com/issues/74984
Signed-off-by: Kotresh HR <khiremat@redhat.com>

src/mds/Server.cc
@@ -12332,6 +12332,19 @@ bool Server::build_snap_diff(
     }
   } before;
 
+  auto snapflush_pending = [&](CInode *in) -> bool {
+    if (!in->is_head() && !in->client_snap_caps.empty())
+      return true;
+    CInode *head = in->is_head() ? in : mdcache->get_inode(in->ino());
+    if (!head)
+      return true;
+    for (const auto& p : head->client_need_snapflush) {
+      if (p.first >= snapid_prev && p.first <= snapid && !p.second.empty())
+	return true;
+    }
+    return false;
+  };
+
   auto rdlock_file_mtime = [&](CInode *in, utime_t &mtime) -> bool {
     if (!mds->locker->rdlock_start(&in->filelock, mdr)) {
       dout(10) << __func__ << " waiting for snapflush, deferring readdir_snapdiff on "
@@ -12440,8 +12453,40 @@ bool Server::build_snap_diff(
       }
     } else {
       if (snapid_prev >= dn->first && snapid <= dn->last) {
-	dout(20) << __func__ << " skipping unchanged " << dn->get_name() << " "
-	  << dn->first << "/" << dn->last << dendl;
+	if (!snapflush_pending(in)) {
+	  dout(20) << __func__ << " skipping unchanged " << dn->get_name() << " "
+	    << dn->first << "/" << dn->last << dendl;
+	  continue;
+	}
+	if (before.dn) {
+	  if (!insert_deleted(before)) {
+	    break;
+	  }
+	  before.reset();
+	}
+	CInode *head = in->is_head() ? in : mdcache->get_inode(in->ino());
+	if (!head) {
+	  dout(20) << __func__ << " skipping unchanged " << dn->get_name() << " "
+	    << dn->first << "/" << dn->last << dendl;
+	  continue;
+	}
+	utime_t mtime = in->get_inode()->mtime;
+	if (!rdlock_file_mtime(in, mtime))
+	  return false;
+	CInode *in_prev = mdcache->pick_inode_snap(head, snapid_prev);
+	CInode *in_snap = mdcache->pick_inode_snap(head, snapid);
+	if (in_prev->get_inode()->mtime != in_snap->get_inode()->mtime) {
+	  dout(30) << __func__ << " timestamp changed on unchanged span "
+	    << dn->get_name() << " " << dn->first << "/" << dn->last
+	    << " " << in_prev->get_inode()->mtime << " vs. "
+	    << in_snap->get_inode()->mtime << dendl;
+	  if (!add_result_cb(dn, in, true)) {
+	    break;
+	  }
+	  continue;
+	}
+	dout(20) << __func__ << " skipping unchanged after snapflush check "
+	  << dn->get_name() << " " << dn->first << "/" << dn->last << dendl;
 	continue;
       } else if (snapid_prev < dn->first && snapid > dn->last) {
 	dout(20) << __func__ << " skipping inner modification " << dn->get_name() << " "
@@ -12479,19 +12524,16 @@ bool Server::build_snap_diff(
 		       << dendl;
 	      before.reset();
 	    } else {
-          /*If mtime matches, rdlock the filelock which waits for pending snap flush
-           *so that latest mtime is fetched
-           */
-          if (!rdlock_file_mtime(in, mtime))
-            return false;
-          if (mtime == before.mtime) {
-            dout(30) << __func__ << " timestamp not changed " << dn->get_name() << " "
-                 << dn->first << "/" << dn->last
-                 << " " << mtime
-                 << dendl;
-            before.reset();
-            continue;
-          }
+	      if (!rdlock_file_mtime(in, mtime))
+		return false;
+	      if (mtime == before.mtime) {
+		dout(30) << __func__ << " timestamp not changed " << dn->get_name() << " "
+			 << dn->first << "/" << dn->last
+			 << " " << mtime
+			 << dendl;
+		before.reset();
+		continue;
+	      }
 	      dout(30) << __func__ << " timestamp changed " << dn->get_name() << " "
 		       << dn->first << "/" << dn->last
 		       << " " << before.mtime << " vs. " << mtime

ceph/ceph

src/mds/Server.cc
@@ -12501,6 +12501,18 @@ bool Server::build_snap_diff(
     }
   } before;
 
+  auto snapflush_pending = [&](CInode *in) -> bool {
+    if (!in->is_head() && !in->client_snap_caps.empty())
+      return true;
+    CInode *head = in->is_head() ? in : mdcache->get_inode(in->ino());
+    if (!head)
+      return true;
+    for (const auto& p : head->client_need_snapflush) {
+      if (p.first >= snapid_prev && p.first <= snapid && !p.second.empty())
+	return true;
+    }
+    return false;
+  };
 
   // rdlock filelock so pending snapflush completes before metadata is read.
   auto rdlock_file_start = [&](CInode *in) -> bool {
@@ -12614,8 +12626,42 @@ bool Server::build_snap_diff(
       }
     } else {
       if (snapid_prev >= dn->first && snapid <= dn->last) {
-	dout(20) << __func__ << " skipping unchanged " << dn->get_name() << " "
-	  << dn->first << "/" << dn->last << dendl;
+	if (!snapflush_pending(in)) {
+	  dout(20) << __func__ << " skipping unchanged " << dn->get_name() << " "
+	    << dn->first << "/" << dn->last << dendl;
+	  continue;
+	}
+	if (before.dn) {
+	  if (!insert_deleted(before)) {
+	    break;
+	  }
+	  before.reset();
+	}
+	CInode *head = in->is_head() ? in : mdcache->get_inode(in->ino());
+	if (!head) {
+	  dout(20) << __func__ << " skipping unchanged " << dn->get_name() << " "
+	    << dn->first << "/" << dn->last << dendl;
+	  continue;
+	}
+	unsigned res_mask = 0;
+	if (!rdlock_file_start(in))
+	  return false;
+	CInode *in_prev = mdcache->pick_inode_snap(head, snapid_prev);
+	CInode *in_snap = mdcache->pick_inode_snap(head, snapid);
+	EntryInfo prev_ei{dn, in_prev};
+	bool differs = prev_ei.meta_differs(in_snap, diff_mask, res_mask);
+	rdlock_file_finish(in);
+	if (differs) {
+	  dout(30) << __func__ << " attrs changed on unchanged span "
+	    << dn->get_name() << " " << dn->first << "/" << dn->last
+	    << " result mask: 0x" << std::hex << res_mask << std::dec << dendl;
+	  if (!add_result_cb(dn, in, true)) {
+	    break;
+	  }
+	  continue;
+	}
+	dout(20) << __func__ << " skipping unchanged after snapflush check "
+	  << dn->get_name() << " " << dn->first << "/" << dn->last << dendl;
 	continue;
       } else if (snapid_prev < dn->first && snapid > dn->last) {
 	dout(20) << __func__ << " skipping inner modification " << dn->get_name() << " "
@@ -12651,9 +12697,15 @@ bool Server::build_snap_diff(
 		<< " result mask: 0x" << std::hex << res_mask << std::dec
 		<< dendl;
 	      before.reset();
+	    } else if (!snapflush_pending(in)) {
+	      dout(0) << __func__ << " attrs not changed " << dn->get_name() << " "
+		<< dn->first << "/" << dn->last
+		<< dendl;
+	      before.reset();
+	      continue;
 	    } else {
-	      /* If attrs match, rdlock the filelock which waits for pending snap
-	       * flush so that latest metadata is fetched under the lock.
+	      /* attrs match but snapflush is pending; rdlock so metadata is
+	       * read after writeback completes.
 	       */
 	      if (!rdlock_file_start(in))
 		return false;
@@ -12669,6 +12721,7 @@ bool Server::build_snap_diff(
 	      } else {
 		dout(0) << __func__ << " attrs not changed " << dn->get_name() << " "
 		  << dn->first << "/" << dn->last
+		  << " (after snapflush)"
 		  << dendl;
 		before.reset();
 		continue;