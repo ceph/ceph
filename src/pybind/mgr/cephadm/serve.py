803503bb9d937e6cc6a761630e85a8653d871ae8
Shweta Bhosale <Shweta.Bhosale1@ibm.com>
2026-09-08 20:30:15 +0530

    mgr/cephadm: check ok-to-stop before OSD redeploy/reconfig
    When _check_daemons decides an OSD needs a reconfig or
    redeploy (e.g. extra ceph config changed, deps changed, or monmap
    updated), then before executing an redeploy or reconfig
    on an OSD, consult `osd ok-to-stop` first.  If the cluster says the OSD
    is not safe to stop, skip the action and log a message at INFO level;
    the serve loop will retry on the next iteration, naturally backing off
    until the cluster is healthy enough to proceed.
    
    Fixes: https://tracker.ceph.com/issues/80399
    
    Signed-off-by: Shweta Bhosale <Shweta.Bhosale1@ibm.com>
    (cherry picked from commit 18bb15c2f702a863561dcd1e2d894d9e99fed5b2)

--- src/pybind/mgr/cephadm/serve.py
+++ src/pybind/mgr/cephadm/serve.py
@@ -1143,6 +1143,25 @@ class CephadmServe:
                 if self.mgr.cache.get_scheduled_daemon_action(dd.hostname, dd.name()) == 'redeploy' \
                         and action == 'reconfig':
                     action = 'redeploy'
+                # For OSD redeploy/reconfig that was NOT explicitly scheduled by
+                # the user (i.e. scheduled_action was None), check ok-to-stop
+                # before proceeding.  A redeploy/reconfig stops the OSD; running
+                # it when the cluster cannot afford to lose that OSD risks data
+                # unavailability.  If the OSD is not safe to stop right now,
+                # defer the action to the next serve-loop iteration.
+                if (
+                    dd.daemon_type == 'osd'
+                    and action in ('redeploy', 'reconfig')
+                    and not scheduled_action
+                ):
+                    r = svc_obj.ok_to_stop([dd.daemon_id])
+                    if r.retval:
+                        self.log.info(
+                            'Skipping %s of %s (not ok-to-stop: %s); '
+                            'will retry next serve loop',
+                            action, dd.name(), r.stderr,
+                        )
+                        continue
                 try:
                     daemon_spec = CephadmDaemonDeploySpec.from_daemon_description(dd)
                     self.mgr._daemon_action(daemon_spec, action=action)
