8713d0f4d95a2489e39d0e789ae0b0db0059e6e5
Jamie Pryde <jamiepry@uk.ibm.com>
2026-09-08 13:51:18 +0100

    osd: Block snap trimmer during pool migration wait states
    NotTrimming::react(KickTrim) would transition to Trimming while the PG
    was in PG_STATE_MIGRATION_WAIT or PG_STATE_MIGRATION_TOOFULL, hitting
    the ceph_assert(permit_trim()) in the Trimming constructor. Discard the
    KickTrim event when either migration-wait state is set. The trimmer will
    be restarted by kick_snap_trim() in on_clean() when migration finishes.
    
    Signed-off-by: Jamie Pryde <jamiepry@uk.ibm.com>
    Assisted-by: IBM Bob 2.2.0

--- src/osd/PrimaryLogPG.cc
+++ src/osd/PrimaryLogPG.cc
@@ -17936,8 +17936,10 @@ boost::statechart::result PrimaryLogPG::NotTrimming::react(const KickTrim&)
     return discard_event();
   }
   if (!pg->is_clean() ||
-      pg->snap_trimq.empty()) {
-    ldout(pg->cct, 10) << "NotTrimming not clean or nothing to trim" << dendl;
+      pg->snap_trimq.empty() ||
+      pg->state_test(PG_STATE_MIGRATION_WAIT) ||
+      pg->state_test(PG_STATE_MIGRATION_TOOFULL)) {
+    ldout(pg->cct, 10) << "NotTrimming not permitted to trim" << dendl;
     return discard_event();
   }
   if (pg->is_scrub_queued_or_active()) {
