ceph/ceph

commit f317cef9e3f6255898a67528ab0e421899d5d3f5

mgr/dashboard: update fedora link for dashboard-cephadm-e2e test
The old link points to a directory that now only contains a
README saying

---

ATTENTION
======================================
The contents of this directory have been moved to our archives available at:

http://archives.fedoraproject.org/pub/archive/fedora/

If you are having troubles finding something there please stop by #fedora-admin on irc.freenode.net

---

so I think we either need to start using a newer fedora or point
to the archive link. This commit is currently doing the latter.

Signed-off-by: Adam King <adking@redhat.com>

src/pybind/mgr/dashboard/ci/cephadm/start-cluster.sh
@@ -60,7 +60,7 @@ npm run build ${FRONTEND_BUILD_OPTS} &
 
 cd ${CEPH_DEV_FOLDER}
 : ${VM_IMAGE:='fedora36'}
-: ${VM_IMAGE_URL:='https://download.fedoraproject.org/pub/fedora/linux/releases/36/Cloud/x86_64/images/Fedora-Cloud-Base-36-1.5.x86_64.qcow2'}
+: ${VM_IMAGE_URL:='https://archives.fedoraproject.org/pub/archive/fedora/linux/releases/36/Cloud/x86_64/images/Fedora-Cloud-Base-36-1.5.x86_64.qcow2'}
 kcli download image -p ceph-dashboard -u ${VM_IMAGE_URL} ${VM_IMAGE}
 kcli delete plan -y ceph || true
 # Compile cephadm locally for the shared_ceph_folder to pick it up

commit 248b3ae2241b71468e1798e59c94118f3895d6c0

mgr/dashboard: bump the cephadm e2e VM image to Fedora 44
Fedora 42 is end of life and its cloud image has moved to
archives.fedoraproject.org. download.fedoraproject.org still redirects,
but the mirrors it points at no longer carry the file:

  curl: (22) The requested URL returned error: 404 Not Found
  Image fedora42 not Added because Unable to download indicated image
  ceph-node-00 not deployed because you don't have image fedora42

kcli deploys no VMs, start-cluster.sh sits in its wait loop printing
"VM ceph-node-00 not found" until the job is killed, and Cypress never
starts. The build fails without a single test result [0].

Use Fedora 44. Fedora 43 is still on the mirrors, but it goes end of
life about a month after 45 ships, so it would need bumping again
within months.

Fedora 36 broke the same way in f317cef9e3f ("mgr/dashboard: update
fedora link for dashboard-cephadm-e2e test"). That one repointed at the
archive and said the alternative was moving to a newer Fedora. Take the
newer Fedora this time, since an archived image only ages further.

[0] https://jenkins.ceph.com/job/ceph-dashboard-cephadm-e2e/25435/consoleText

Signed-off-by: Kefu Chai <k.chai@proxmox.com>

src/pybind/mgr/dashboard/ci/cephadm/ceph_cluster.yml
@@ -5,7 +5,7 @@ parameters:
  netmask: 255.255.255.0
  numcpus: 1
  memory: 2048
- image: fedora42
+ image: fedora44
  notify: false
  admin_password: password
  disks:

src/pybind/mgr/dashboard/ci/cephadm/start-cluster.sh
@@ -61,8 +61,8 @@ fi
 npm run build ${FRONTEND_BUILD_OPTS} &
 
 cd ${CEPH_DEV_FOLDER}
-: ${VM_IMAGE:='fedora42'}
-: ${VM_IMAGE_URL:='https://download.fedoraproject.org/pub/fedora/linux/releases/42/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-42-1.1.x86_64.qcow2'}
+: ${VM_IMAGE:='fedora44'}
+: ${VM_IMAGE_URL:='https://download.fedoraproject.org/pub/fedora/linux/releases/44/Cloud/x86_64/images/Fedora-Cloud-Base-Generic-44-1.7.x86_64.qcow2'}
 with_libvirt "kcli download image -p ceph-dashboard -u ${VM_IMAGE_URL} ${VM_IMAGE}"
 with_libvirt "kcli delete plan -y ceph || true"
 # Compile cephadm locally for the shared_ceph_folder to pick it up