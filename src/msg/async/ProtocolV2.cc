de14dcbe60db79fb58f735f69e904d456b8b9866
Kamoltat (Junior) Sirivadhna <ksirivad@redhat.com>
2026-09-16 19:14:19 +0000

    Revert "msg/async: ProtocolV2::send_server_ident update ProtocolV2::global_seq"
    This reverts commit de39ed10bc71f36ad8e034a1e7526cd17473bf70.
    
    Signed-off-by: Kamoltat (Junior) Sirivadhna <ksirivad@redhat.com>
    
    Conflicts:
    	- src/msg/async/ProtocolV2.cc
    	Accepts:
        auto& oq = out_queue[m->get_priority()];
        oq.emplace_back(out_queue_entry_t{is_prepared, std::move(m)});

--- src/msg/async/ProtocolV2.cc
+++ src/msg/async/ProtocolV2.cc
@@ -2978,11 +2978,11 @@ CtPtr ProtocolV2::send_server_ident() {
     flags = flags | CEPH_MSG_CONNECT_LOSSY;
   }
 
-  global_seq = messenger->get_global_seq();
+  uint64_t gs = messenger->get_global_seq();
   auto server_ident = ServerIdentFrame::Encode(
           messenger->get_myaddrs(),
           messenger->get_myname().num(),
-          global_seq,
+          gs,
           connection->policy.features_supported,
           connection->policy.features_required | msgr2_required,
           flags,
@@ -2991,7 +2991,7 @@ CtPtr ProtocolV2::send_server_ident() {
   ldout(cct, 5) << __func__ << " sending identification:"
                 << " addrs=" << messenger->get_myaddrs()
                 << " gid=" << messenger->get_myname().num()
-                << " global_seq=" << global_seq << " features_supported=" << std::hex
+                << " global_seq=" << gs << " features_supported=" << std::hex
                 << connection->policy.features_supported
                 << " features_required="
 		            << (connection->policy.features_required | msgr2_required)
