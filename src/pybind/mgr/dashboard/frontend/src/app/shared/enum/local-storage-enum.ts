2a267459933050ea3fcc81be59a57ddde4f3180a
Afreen Misbah <afreen@ibm.com>
2026-09-17 20:37:42 +0530

    mgr/dashboard: fix typo in AuthStorageService.remove() localStorage key
    user_pwd_expiration_data was used instead of user_pwd_expiration_date,
    causing the password expiration timestamp to never be cleared on logout.
    
    Signed-off-by: Afreen Misbah <afreen.misbah@ibm.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/enum/local-storage-enum.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/enum/local-storage-enum.ts
@@ -1,3 +1,7 @@
 export enum LocalStorage {
-  DASHBOARD_USERNAME = 'dashboard_username'
+  DASHBOARD_USERNAME = 'dashboard_username',
+  DASHBOARD_PERMISSIONS = 'dashboard_permissions',
+  PWD_EXPIRATION_DATE = 'user_pwd_expiration_date',
+  PWD_UPDATE_REQUIRED = 'user_pwd_update_required',
+  SSO = 'sso'
 }
