b3af613031822093772449f01686dfa936997ee0
Yuval Lifshitz <ylifshit@ibm.com>
2026-09-17 11:23:36 +0000

    rgw/logging: logging conf change is not Idempotent
    so, any failure in flushing after the conf was modified
    should be treated as a warning (for possible data loss)
    
    Signed-off-by: Yuval Lifshitz <ylifshit@ibm.com>

--- src/rgw/rgw_rest_bucket_logging.cc
+++ src/rgw/rgw_rest_bucket_logging.cc
@@ -316,22 +316,17 @@ class RGWPutBucketLoggingOp : public RGWDefaultResponseOp {
         if (const auto ret = rgw::bucketlogging::get_bucket_id(
               old_conf->target_bucket, s->bucket_tenant, old_target_bucket_id);
             ret < 0) {
-          ldpp_dout(this, 1) << "ERROR: failed to parse logging bucket '"
-            << old_conf->target_bucket << "', ret = " << ret << dendl;
-          op_ret = ret;
-          return;
-        }
-        if (const auto ret = driver->load_bucket(this, old_target_bucket_id,
+          ldpp_dout(this, 1) << "WARNING: failed to flush pending logging object "
+            << "when updating logging configuration of bucket '" << src_bucket_id
+            << "'. due to invalid bucket name '" << old_conf->target_bucket
+            << "', ret = " << ret << dendl;
+        } else if (const auto ret = driver->load_bucket(this, old_target_bucket_id,
                                                 &old_target_bucket, y);
             ret < 0) {
           ldpp_dout(this, 1) << "WARNING: failed to flush pending logging object "
             << "when updating logging configuration of bucket '" << src_bucket_id
             << "' because original target bucket '" << old_target_bucket_id
             << "' could not be loaded, ret = " << ret << dendl;
-
-          if (-ENOENT != ret) {
-            return;
-          }
         }
       }
 
@@ -343,13 +338,13 @@ class RGWPutBucketLoggingOp : public RGWDefaultResponseOp {
         const auto& flush_target_bucket_id = flush_target_bucket->get_key();
         if (int ret = flush_target_bucket->get_logging_object_name(
               obj_name, old_conf->target_prefix, y, this, nullptr);
-            ret < 0 && ret != -ENOENT) {
-          ldpp_dout(this, 1) << "ERROR: failed to get name of logging object "
+            ret < 0) {
+          ldpp_dout(this, 1) << "WARNING: failed to flush pending logging object "
+            << "when updating logging configuration of bucket '" << src_bucket_id
+            << "' because of failure to get name of logging object "
             << "of logging bucket '" << flush_target_bucket_id
             << "' and prefix '" << old_conf->target_prefix
             << "', ret = " << ret << dendl;
-          op_ret = ret;
-          return;
         }
 
         const auto region = driver->get_zone()->get_zonegroup().get_api_name();
@@ -366,7 +361,7 @@ class RGWPutBucketLoggingOp : public RGWDefaultResponseOp {
               &old_obj); ret < 0) {
           ldpp_dout(this, 1) << "WARNING: failed to flush pending logging object '" << obj_name << "'"
               << " to target bucket '" << flush_target_bucket_id << "'. "
-              << "' when updating logging configuration of bucket '" << src_bucket->get_key() << ". error: " << ret << dendl;
+              << "' when updating logging configuration of bucket '" << src_bucket->get_key() << "', ret = " << ret << dendl;
         } else {
           ldpp_dout(this, 20) << "INFO: flushed pending logging object '" << old_obj
             << "' to target bucket '" << flush_target_bucket_id << "' when updating logging configuration of bucket '"
