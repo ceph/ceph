d47742fcded284658b4f5909e5c265c938d29298
Dongdong Tao <tdd21151186@gmail.com>
2026-09-21 15:09:44 +0900

    qa/standalone/ceph-bpf: drop removed --auth-supported option
    auth_supported no longer exists, so ceph-mon --mkfs fails with
    "too many arguments". Use the auth_*_required options like the other
    standalone tests.
    
    Signed-off-by: Dongdong Tao <dongdong.tao@canonical.com>

--- qa/standalone/ceph-bpf/radostrace.sh
+++ qa/standalone/ceph-bpf/radostrace.sh
@@ -38,7 +38,7 @@ function run() {
 
     export CEPH_MON="127.0.0.1:7220" # git grep '\<7220\>' : there must be only one
     export CEPH_ARGS
-    CEPH_ARGS+="--fsid=$(uuidgen) --auth-supported=none "
+    CEPH_ARGS+="--fsid=$(uuidgen) --auth_cluster_required=none --auth_service_required=none --auth_client_required=none "
     CEPH_ARGS+="--mon-host=$CEPH_MON "
 
     # Check if radostrace binary exists
