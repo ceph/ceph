bd237a75117717bfe869cd9493667ffc400a1adf
Naman Munet <naman.munet@ibm.com>
2026-09-22 18:41:37 +0530

    fix: flaky guard blocking e2e tests run
    Signed-off-by: Naman Munet <naman.munet@ibm.com>

--- src/pybind/mgr/dashboard/frontend/src/app/shared/services/module-status-guard.service.ts
+++ src/pybind/mgr/dashboard/frontend/src/app/shared/services/module-status-guard.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Router } from '@angular/router';
 
 import { of as observableOf } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, map, switchMap } from 'rxjs/operators';
 
 import { MgrModuleService } from '~/app/shared/api/mgr-module.service';
 import { Icons } from '~/app/shared/enum/icons.enum';
@@ -61,48 +61,50 @@ export class ModuleStatusGuardService {
       return observableOf(true);
     }
     const config = route.data['moduleStatusGuardConfig'];
-    let backendCheck = false;
-    if (config.backend && this.authStorageService.getPermissions().configOpt?.read) {
-      this.mgrModuleService.getConfig('orchestrator').subscribe(
-        (resp) => {
-          backendCheck = config.backend === resp['orchestrator'];
-        },
-        () => {
-          this.router.navigate([config.redirectTo]);
-          return observableOf(false);
-        }
-      );
-    }
-    return this.http.get(`ui-api/${config.uiApiPath}/status`).pipe(
-      map((resp: any) => {
-        if (!resp.available && !backendCheck) {
-          this.router.navigate([config.redirectTo || ''], {
-            state: {
-              header: config.header,
-              message: resp.message,
-              section: config.section,
-              section_info: config.section_info,
-              button_name: config.button_name,
-              button_route: config.button_route,
-              button_title: config.button_title,
-              secondary_button_name: config.secondary_button_name,
-              secondary_button_route: config.secondary_button_route,
-              secondary_button_title: config.secondary_button_title,
-              module_name: config.module_name,
-              navigate_to: config.navigate_to,
-              uiConfig: config.uiConfig,
-              uiApiPath: config.uiApiPath,
-              icon: Icons.wrench,
-              component: config.component
+    const backendCheck$ =
+      config.backend && this.authStorageService.getPermissions().configOpt?.read
+        ? this.mgrModuleService.getConfig('orchestrator').pipe(
+            map((resp) => config.backend === resp['orchestrator']),
+            catchError(() => {
+              this.router.navigate([config.redirectTo]);
+              return observableOf(false);
+            })
+          )
+        : observableOf(false);
+    return backendCheck$.pipe(
+      switchMap((backendCheck) =>
+        this.http.get(`ui-api/${config.uiApiPath}/status`).pipe(
+          map((resp: any) => {
+            if (!resp.available && !backendCheck) {
+              this.router.navigate([config.redirectTo || ''], {
+                state: {
+                  header: config.header,
+                  message: resp.message,
+                  section: config.section,
+                  section_info: config.section_info,
+                  button_name: config.button_name,
+                  button_route: config.button_route,
+                  button_title: config.button_title,
+                  secondary_button_name: config.secondary_button_name,
+                  secondary_button_route: config.secondary_button_route,
+                  secondary_button_title: config.secondary_button_title,
+                  module_name: config.module_name,
+                  navigate_to: config.navigate_to,
+                  uiConfig: config.uiConfig,
+                  uiApiPath: config.uiApiPath,
+                  icon: Icons.wrench,
+                  component: config.component
+                }
+              });
             }
-          });
-        }
-        return resp.available;
-      }),
-      catchError(() => {
-        this.router.navigate([config.redirectTo]);
-        return observableOf(false);
-      })
+            return resp.available;
+          }),
+          catchError(() => {
+            this.router.navigate([config.redirectTo]);
+            return observableOf(false);
+          })
+        )
+      )
     );
   }
 }
