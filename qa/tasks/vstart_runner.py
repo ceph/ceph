40d7f727024f01e768abecea4103beebe0cc0000
Rishabh Dave <ridave@redhat.com>
2026-09-24 17:09:42 +0530

    qa/vstart_runner: add extra blank lines in vstart_runnner.log
    Add extra blank lines at beginning and end of a session, this makes it
    easier to distinguish the log entries of one vstart_runnner.py session
    from another.
    
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

--- qa/tasks/vstart_runner.py
+++ qa/tasks/vstart_runner.py
@@ -1347,6 +1347,7 @@ def exec_test():
     opt_exit_on_test_failure = True
     mode = Mode.unittest
 
+    log.info('\n'*10)
     args = sys.argv[1:]
     flags = [a for a in args if a.startswith("-")]
     modules = [a for a in args if not a.startswith("-")]
@@ -1610,8 +1611,12 @@ def exec_test():
         for test, failure in result.failures:
             bad_tests.append(str(test))
 
+        log.info('vstart_runner.py finished running, exiting with 1')
+        log.info('\n'*10)
         sys.exit(-1)
     else:
+        log.info('vstart_runner.py finished running, exiting with 0')
+        log.info('\n'*10)
         sys.exit(0)
 
 def run_configs(configs):
