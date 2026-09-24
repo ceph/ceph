29a8fec230555aaf4f9baa3224a9590201b8c190
Tom Sollers <tom.sollers@ibm.com>
2026-09-24 13:29:55 +0100

    qa/standalone/mon/misc: update for vampire Signed-Off-By: Tom Sollers <tom.sollers@ibm.com>

--- qa/standalone/mon/misc.sh
+++ qa/standalone/mon/misc.sh
@@ -171,7 +171,7 @@ function TEST_mon_features() {
     CEPH_ARGS="--fsid=$fsid --auth_cluster_required=none --auth_service_required=none --auth_client_required=none "
     CEPH_ARGS+="--mon-host=$MONA,$MONB,$MONC "
     CEPH_ARGS+="--mon-debug-no-initial-persistent-features "
-    CEPH_ARGS+="--mon-debug-no-require-umbrella"
+    CEPH_ARGS+="--mon-debug-no-require-vampire"
 
     run_mon $dir a --public-addr $MONA || return 1
     run_mon $dir b --public-addr $MONB || return 1
@@ -206,6 +206,8 @@ function TEST_mon_features() {
     jq_success "$jqinput" "$jqfilter" "tentacle" || return 1
     jqfilter='.features.quorum_mon[]|select(. == "umbrella")'
     jq_success "$jqinput" "$jqfilter" "umbrella" || return 1
+    jqfilter='.features.quorum_mon[]|select(. == "vampire")'
+    jq_success "$jqinput" "$jqfilter" "vampire" || return 1
 
     # monmap must have no persistent features set, because we
     # don't currently have a quorum made out of all the monitors
@@ -243,6 +245,8 @@ function TEST_mon_features() {
     jq_success "$jqinput" "$jqfilter" "tentacle" || return 1
     jqfilter='.all.supported[] | select(. == "umbrella")'
     jq_success "$jqinput" "$jqfilter" "umbrella" || return 1
+    jqfilter='.all.supported[] | select(. == "vampire")'
+    jq_success "$jqinput" "$jqfilter" "vampire" || return 1
 
     # start third monitor
     run_mon $dir c --public-addr $MONC || return 1
@@ -289,6 +293,8 @@ function TEST_mon_features() {
     jq_success "$jqinput" "$jqfilter" "nvmeof_beacon_diff" || return 1
     jqfilter='.monmap.features.persistent[]|select(. == "umbrella")'
     jq_success "$jqinput" "$jqfilter" "umbrella" || return 1
+    jqfilter='.monmap.features.persistent[]|select(. == "vampire")'
+    jq_success "$jqinput" "$jqfilter" "vampire" || return 1
     jqfilter='.monmap.features.persistent | length == 14'
     jq_success "$jqinput" "$jqfilter" || return 1
 
