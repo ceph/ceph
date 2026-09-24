f79cf02227f13f69b017b651789e64cd79d6618f
Rishabh Dave <ridave@redhat.com>
2026-09-24 17:10:18 +0530

    volumes: add helper to convert str and bytes safely and intelligently
    Signed-off-by: Rishabh Dave <ridave@redhat.com>

---
+++ src/pybind/mgr/volumes/fs/utils.py
@@ -0,0 +1,57 @@
+from errno import *
+from uuid import UUID, uuid4
+from os.path import join
+from logging import getLogger
+
+from .exception import VolumeException, InvalidUuidError
+
+
+log = getLogger(__name__)
+
+
+def to_bytes(*args):
+    '''
+    Convert all of args to bytes. Valid types: str, bytes, int, float and bool.
+
+    :rtype: bytes or list of bytes
+    '''
+    newargs = []
+    for var in args:
+
+        var_type = type(var)
+        if var_type is bytes:
+            newargs.append(var)
+        elif var_type is str:
+            newargs.append(var.encode('utf-8'))
+        elif var_type in (int, float, bool):
+            newargs.append(str(var).encode('utf-8'))
+        else:
+            raise VolumeException(EINVAL,
+                                  f'invalid type. {var_type} = {var_type} '
+                                  f'var = {var} args = {args}')
+
+    return newargs if len(newargs) > 1 else newargs[0]
+
+
+def to_str(*args):
+    '''
+    Convert all of args to str. Valid types: str, bytes, int, float and bool.
+
+    :rtype: str or list of str
+    '''
+    newargs = []
+    for var in args:
+        var_type = type(var)
+
+        if var_type is str:
+            newargs.append(var)
+        elif var_type is bytes:
+            newargs.append(var.decode('utf-8'))
+        elif var_type in (int, float, bool):
+            newargs.append(str(var))
+        else:
+            raise VolumeException(EINVAL,
+                                  f'invalid type. var_type = {var_type} '
+                                  f'var = {var} args = {args}')
+
+    return newargs if len(newargs) > 1 else newargs[0]
